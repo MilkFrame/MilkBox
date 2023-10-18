8cab3758e321919518d2108db09612a71bbf3ad9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-10-19 01:40:56 +1100

    Create World.lua

---
+++ Tradelands [198116126]/World.lua
@@ -0,0 +1,44 @@
+data = {
+    ["Regions"] = {
+        ["spawn"] = {198116126, 252919716, "The Grand Isles", Vector3.new(0, 0, 0), 0, {}},
+        ["PFort"] = {420777178, 420777178, "PFort", Vector3.new(-1, 0, -1), 1, {}},
+        ["Main"] = {
+            198116126,
+            252919716,
+            "The Grand Isles",
+            Vector3.new(0, 0, 0),
+            0,
+            {
+                ["Nassau"] = Vector3.new(2175, 0, 10072),
+                ["Harrisburg"] = Vector3.new(4184, 0, -9237),
+                ["Fenwick"] = Vector3.new(1155, 0, 1904),
+                ["Freeport"] = Vector3.new(-1975, 0, 4207),
+                ["Salem"] = Vector3.new(-7360, 0, -1209),
+                ["Key"] = Vector3.new(-8401, 0, 6598),
+                ["Fort Verner"] = Vector3.new(-1969, 0, -6916),
+                ["Perth"] = Vector3.new(-1311, 0, -967),
+                ["Verdantium"] = Vector3.new(-3072, 0, -2803),
+                ["Whitecrest"] = Vector3.new(3749, 0, 3744),
+                ["Blackwind Cove"] = Vector3.new(2576, 0, -3905)
+            }
+        },
+        ["Burkeland"] = {
+            5052112391,
+            6647184514,
+            "Burkeland",
+            Vector3.new(0, 0, -1),
+            1,
+            {["Burkeland"] = Vector3.new(-2836, 0, -4302)}
+        },
+        ["Eastern Sea"] = {
+            6703456505,
+            6703440478,
+            "Eastern Sea",
+            Vector3.new(1, 0, 0),
+            2,
+            {["Corsica"] = Vector3.new(-2938, 0, 4329),
+            ["Weymouth"] = Vector3.new(-8150, 0, -7096)}
+        }
+    },
+    ["GetDistanceBetweenPorts"] = "function: 0x00000000534b7712"
+}
