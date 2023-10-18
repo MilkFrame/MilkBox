93d5e557b5974e0629b1e1e265650d0320f2d1ab
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-10-19 01:48:08 +1100

    Create Recipes.lua

---
+++ Tradelands [198116126]/Recipes.lua
@@ -0,0 +1,130 @@
+data = {
+    {{"CAT_Tools", "Herramientas"}},
+    {{"Axe", 1, "Used for timber-felling.", 0, 12}, {"ANY_METAL", 5}, {"ANY_WOOD", 7}},
+    {{"Pickaxe", 1, "Used for mining.", 0, 12}, {"ANY_METAL", 7}, {"ANY_WOOD", 5}},
+    {{"Torch", 1, "Light source.", 0, 12}, {"ANY_METAL", 1}, {"ANY_WOOD", 10}},
+    {{"Spyglass", 1, "Used to view distant objects.", 600, 16}, {"ANY_METAL", 10}, {"ANY_WOOD", 5}},
+    {{"Fishing Rod", 1, "Used for fishing.", 1000, 15}, {"ANY_METAL", 4}, {"ANY_WOOD", 10}},
+    {{"Paddle", 1, "Used to row.", 1500, 21}, {"ANY_METAL", 1}, {"ANY_WOOD", 20}},
+    {{"CAT_Weapons", "Armas"}},
+    {{"Dagger", 1, "Common Dagger", 0, 8}, {"ANY_METAL", 10}, {"ANY_WOOD", 4}},
+    {{"Short Sword", 1, "Common Sword", 100, 13}, {"ANY_METAL", 15}, {"ANY_WOOD", 5}},
+    {{"Rapier", 1, "High-Quality Sword", 600, 20}, {"ANY_METAL", 20}, {"ANY_WOOD", 5}, {"High-Quality Mold", 1}},
+    {{"Smallsword", 1, "High-Quality Sword", 600, 20}, {"ANY_METAL", 17}, {"ANY_WOOD", 5}, {"High-Quality Mold", 1}},
+    {{"Stiletto", 1, "High-Quality Sword", 600, 20}, {"ANY_METAL", 10}, {"ANY_WOOD", 4}, {"High-Quality Mold", 1}},
+    {{"Tomahawk", 1, "Small Axe", 1000, 14}, {"ANY_METAL", 8}, {"ANY_WOOD", 16}},
+    {{"Sabre", 1, "Fancy Sword", 4100, 25}, {"ANY_METAL", 22}, {"ANY_WOOD", 5}, {"Gold", 6}},
+    {{"Battleaxe", 1, "Large Axe", 6200, 30}, {"ANY_METAL", 16}, {"ANY_WOOD", 16}, {"High-Quality Mold", 1}},
+    {{"CAT_Ranged Weapons", "Armas a Distancia"}},
+    {{"Crossbow", 1, "Crossbow", 2100, 25}, {"ANY_METAL", 12}, {"ANY_WOOD", 10}},
+    {{"Flintlock Pistol", 1, "Common Firearm", 4100, 30}, {"ANY_METAL", 10}, {"ANY_WOOD", 10}, {"Iron", 5}},
+    {
+        {"Musket", 1, "More Powerful Firearm", 6200, 55},
+        {"ANY_METAL", 20},
+        {"ANY_WOOD", 20},
+        {"Iron", 8},
+        {"High-Quality Mold", 2}
+    },
+    {{"CAT_Ammunition", "MuniciÃ³n"}},
+    {{"Round Shot", 10, "Ammunition for cannons. (10 Rounds)", 0, 3}, {"Iron", 4}},
+    {{"Bolt", 10, "Ammunition for crossbows. (10 Rounds)", 1000, 5}, {"Iron", 1}, {"Oak", 5}},
+    {
+        {"Flintlock Ammunition", 10, "Ammunition for flintlock firearms. (10 Rounds)", 2100, 12},
+        {"Iron", 5},
+        {"Gunpowder", 3}
+    },
+    {
+        {"Caplock Ammunition", 10, "Ammunition for caplock firearms. (10 Rounds)", 4100, 15},
+        {"Iron", 8},
+        {"Copper", 2},
+        {"Gunpowder", 3},
+        {"Saltpeter", 1}
+    },
+    {
+        {"Swivel Ammunition", 20, "Ammunition for swivel guns. (20 Rounds)", 4100, 20},
+        {"Iron", 6},
+        {"Gunpowder", 10},
+        {"Steel", 2}
+    },
+    {{"CAT_Crafting", "Estaciones Artesanales"}},
+    {
+        {"Carpentry Station", 1, "Crafting station for making furniture.", 4100, 650},
+        {"ANY_METAL", 100},
+        {"ANY_WOOD", 500},
+        {"Carpenter's Mallet", 1}
+    },
+    {
+        {"Blacksmithing Station", 1, "Crafting station for making weapons and other metal items.", 4100, 620},
+        {"ANY_METAL", 120},
+        {"ANY_WOOD", 250},
+        {"Steel", 200},
+        {"Blacksmith's Hammer", 1}
+    },
+    {{"CAT_Miscellaneous", "Diverso"}},
+    {{"Cast Iron", 1, "Brittle Crafting Material", 600, 3}, {"Iron", 3}},
+    {{"Steel", 1, "Crafting Material", 1000, 4}, {"Iron", 3}, {"Coal", 1}},
+    {
+        {"Moorian Hardwood", 10, "Crafting Material", 10400, 15},
+        {"Spruce", 4},
+        {"Ebony", 3},
+        {"Blood Oak", 3},
+        {"Pwnwood", 3},
+        {"Inyolan Oak", 2}
+    },
+    {
+        {"Lysian Steel", 10, "Crafting Material", 10400, 15},
+        {"Electrosteel", 4},
+        {"Onyx", 3},
+        {"Composition L", 3},
+        {"Salt", 3},
+        {"Pursteel", 2}
+    },
+    {{"Gunpowder", 1, "Crafting Material", 2100, 3}, {"Coal", 2}, {"Saltpeter", 1}},
+    {{"Fish Oil", 1, "Crafting Material", 4100, 5}, {"Tarpon", 3}},
+    {
+        {
+            "Steam Engine Parts",
+            1,
+            "Requirement for Steamships. Premium tokens can be bought with Robux or from other players.",
+            10400,
+            160
+        },
+        {"Steel", 100},
+        {"Copper", 50},
+        {"Premium Token", 30},
+        {"Fish Oil", 20}
+    },
+    {
+        {"Steam Engine Parts (Alternate)", 1, "Requirement for Steamships (Old Recipe)", 0, 2},
+        {"Unfinished Engine Parts", 1},
+        {"Lord Gaben's Blessing [Loyalty]", 1}
+    },
+    {
+        {"Cladifier", 1, "Changes metal of an ironclad to metal used to craft.", 0, 1000},
+        {"Blank Cladifier", 1},
+        {"ANY_METAL", 1000}
+    },
+    {
+        {"Ghostship Hull Kit", 1, "Used to change wood of a ghostship.", 0, 500},
+        {"Ghostship Hull Plans", 1},
+        {"ANY_WOOD", 500},
+        {"Doubloons", 50000}
+    },
+    {{"CAT_Cannons", "CaÃ±ones"}},
+    {{"Swivel Gun", 1, "Swivel Gun", 300, 26}, {"ANY_METAL", 20}, {"ANY_WOOD", 5}},
+    {{"Light Long Gun", 1, "Size 1 Cannon", 0, 56}, {"ANY_METAL", 35}, {"ANY_WOOD", 20}},
+    {{"Light Carronade", 1, "Size 1 Cannon", 0, 36}, {"ANY_METAL", 20}, {"ANY_WOOD", 15}},
+    {{"Light Mortar", 1, "Size 1 Mortar", 300, 61}, {"ANY_METAL", 40}, {"ANY_WOOD", 20}},
+    {{"Medium Long Gun", 1, "Size 2 Cannon", 3000, 96}, {"ANY_METAL", 60}, {"ANY_WOOD", 35}},
+    {{"Medium Carronade", 1, "Size 2 Cannon", 1500, 86}, {"ANY_METAL", 50}, {"ANY_WOOD", 35}},
+    {{"Medium Mortar", 1, "Size 2 Mortar", 3000, 96}, {"ANY_METAL", 60}, {"ANY_WOOD", 35}},
+    {{"Heavy Long Gun", 1, "Size 3 Cannon", 6200, 126}, {"ANY_METAL", 80}, {"ANY_WOOD", 45}},
+    {{"Heavy Carronade", 1, "Size 3 Cannon", 3000, 121}, {"ANY_METAL", 75}, {"ANY_WOOD", 45}},
+    {{"Heavy Mortar", 1, "Size 3 Mortar", 6200, 131}, {"ANY_METAL", 80}, {"ANY_WOOD", 50}},
+    {{"Demeter Long Gun", 1, "Size 4 Turret", 10400, 400}, {"ANY_METAL", 300}, {"ANY_WOOD", 60}},
+    {{"Poseidon Long Gun", 1, "Size 4 Turret", 10400, 450}, {"ANY_METAL", 350}, {"ANY_WOOD", 60}},
+    {{"Neptune Howitzer (Long Gun)", 1, "Size 5 Turret", 10400, 480}, {"ANY_METAL", 380}, {"ANY_WOOD", 60}},
+    {{"Neptune Twin Long Guns", 1, "Size 5 Turret", 10400, 500}, {"ANY_METAL", 400}, {"ANY_WOOD", 60}},
+    {{"Neptune Super-Carronade", 1, "Size 5 Turret", 10400, 525}, {"ANY_METAL", 425}, {"ANY_WOOD", 70}},
+    {{"Mortar Turret", 1, "Size 5 Mortar (Binglehopper Only)", 10400, 525}, {"ANY_METAL", 400}, {"ANY_WOOD", 70}}
+}
