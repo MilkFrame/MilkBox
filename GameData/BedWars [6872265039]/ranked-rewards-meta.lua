28894b32553bac721f5d7270064b8069bd9a598e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-10-25 00:55:32 +1100

    Create ranked-rewards-meta.lua

---
+++ GameData/BedWars [6872265039]/ranked-rewards-meta.lua
@@ -0,0 +1,135 @@
+data = {
+    ["rankSkins"] = {
+        "yuzi_victorious",
+        "nightmare_victorious_davey",
+        "emerald_victorious_davey",
+        "diamond_victorious_davey",
+        "platinum_victorious_davey",
+        "gold_victorious_davey",
+        "barbarian_victorious",
+        "nightmare_victorious_spirit_assassin",
+        "diamond_victorious_spirit_assassin",
+        "platinum_victorious_spirit_assassin",
+        "gold_victorious_spirit_assassin",
+        "hannah_victorious"
+    },
+    ["RankedRewardsMeta"] = {
+        ["RANKED_S4"] = {
+            ["diamond"] = {["titles"] = {"rank_s4_diamond"}, ["rank"] = "diamond"},
+            ["silver"] = {["titles"] = {"rank_s4_silver"}, ["rank"] = "silver"},
+            ["gold"] = {["titles"] = {"rank_s4_gold"}, ["rank"] = "gold"},
+            ["nightmare"] = {
+                ["titles"] = {"rank_s4_nightmare"},
+                ["kitSkins"] = {"yuzi_victorious"},
+                ["rank"] = "nightmare"
+            },
+            ["bronze"] = {["titles"] = {"rank_s4_bronze"}, ["rank"] = "bronze"},
+            ["platinum"] = {["titles"] = {"rank_s4_platinum"}, ["rank"] = "platinum"}
+        },
+        ["RANKED_S7"] = {
+            ["diamond"] = {
+                ["titles"] = {"rank_s7_diamond"},
+                ["kitSkins"] = {"diamond_victorious_davey", "platinum_victorious_davey", "gold_victorious_davey"},
+                ["rank"] = "diamond"
+            },
+            ["silver"] = {["titles"] = {"rank_s7_silver"}, ["rank"] = "silver"},
+            ["gold"] = {["titles"] = {"rank_s7_gold"}, ["kitSkins"] = {"gold_victorious_davey"}, ["rank"] = "gold"},
+            ["nightmare"] = {
+                ["titles"] = {"rank_s7_nightmare"},
+                ["emotes"] = {"nightmare_davey_flex", "nightmare_davey_think"},
+                ["kitSkins"] = {
+                    "nightmare_victorious_davey",
+                    "emerald_victorious_davey",
+                    "diamond_victorious_davey",
+                    "platinum_victorious_davey",
+                    "gold_victorious_davey"
+                },
+                ["rank"] = "nightmare"
+            },
+            ["emerald"] = {
+                ["titles"] = {"rank_s7_emerald"},
+                ["kitSkins"] = {
+                    "emerald_victorious_davey",
+                    "diamond_victorious_davey",
+                    "platinum_victorious_davey",
+                    "gold_victorious_davey"
+                },
+                ["rank"] = "emerald"
+            },
+            ["bronze"] = {["titles"] = {"rank_s7_bronze"}, ["rank"] = "bronze"},
+            ["platinum"] = {
+                ["titles"] = {"rank_s7_platinum"},
+                ["kitSkins"] = {"platinum_victorious_davey", "gold_victorious_davey"},
+                ["rank"] = "platinum"
+            }
+        },
+        ["RANKED_BETA"] = {
+            ["diamond"] = {["titles"] = {"rank_beta_diamond"}, ["rank"] = "diamond"},
+            ["silver"] = {["titles"] = {"rank_beta_silver"}, ["rank"] = "silver"},
+            ["gold"] = {["titles"] = {"rank_beta_gold"}, ["rank"] = "gold"},
+            ["nightmare"] = {
+                ["titles"] = {"rank_beta_nightmare"},
+                ["kitSkins"] = {"barbarian_victorious"},
+                ["emotes"] = {"victorious_beta"},
+                ["rank"] = "nightmare"
+            },
+            ["bronze"] = {["titles"] = {"rank_beta_bronze"}, ["rank"] = "bronze"},
+            ["platinum"] = {["titles"] = {"rank_beta_platinum"}, ["rank"] = "platinum"}
+        },
+        ["RANKED_S6"] = {
+            ["diamond"] = {
+                ["titles"] = {"rank_s6_diamond"},
+                ["kitSkins"] = {
+                    "diamond_victorious_spirit_assassin",
+                    "platinum_victorious_spirit_assassin",
+                    "gold_victorious_spirit_assassin"
+                },
+                ["rank"] = "diamond"
+            },
+            ["silver"] = {["titles"] = {"rank_s6_silver"}, ["rank"] = "silver"},
+            ["gold"] = {
+                ["titles"] = {"rank_s6_gold"},
+                ["kitSkins"] = {"gold_victorious_spirit_assassin"},
+                ["rank"] = "gold"
+            },
+            ["nightmare"] = {
+                ["titles"] = {"rank_s6_nightmare"},
+                ["emotes"] = {"nightmare_season_6", "victorious_celebration_season_6"},
+                ["kitSkins"] = {
+                    "nightmare_victorious_spirit_assassin",
+                    "diamond_victorious_spirit_assassin",
+                    "platinum_victorious_spirit_assassin",
+                    "gold_victorious_spirit_assassin"
+                },
+                ["rank"] = "nightmare"
+            },
+            ["emerald"] = {
+                ["titles"] = {"rank_s6_diamond"},
+                ["kitSkins"] = {
+                    "diamond_victorious_spirit_assassin",
+                    "platinum_victorious_spirit_assassin",
+                    "gold_victorious_spirit_assassin"
+                },
+                ["rank"] = "emerald"
+            },
+            ["bronze"] = {["titles"] = {"rank_s6_bronze"}, ["rank"] = "bronze"},
+            ["platinum"] = {
+                ["titles"] = {"rank_s6_platinum"},
+                ["kitSkins"] = {"platinum_victorious_spirit_assassin", "gold_victorious_spirit_assassin"},
+                ["rank"] = "platinum"
+            }
+        },
+        ["RANKED_S5"] = {
+            ["diamond"] = {["titles"] = {"rank_s5_diamond"}, ["rank"] = "diamond"},
+            ["silver"] = {["titles"] = {"rank_s5_silver"}, ["rank"] = "silver"},
+            ["gold"] = {["titles"] = {"rank_s5_gold"}, ["rank"] = "gold"},
+            ["nightmare"] = {
+                ["titles"] = {"rank_s5_nightmare"},
+                ["kitSkins"] = {"hannah_victorious"},
+                ["rank"] = "nightmare"
+            },
+            ["bronze"] = {["titles"] = {"rank_s5_bronze"}, ["rank"] = "bronze"},
+            ["platinum"] = {["titles"] = {"rank_s5_platinum"}, ["rank"] = "platinum"}
+        }
+    }
+}
