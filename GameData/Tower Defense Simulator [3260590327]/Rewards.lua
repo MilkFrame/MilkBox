cf1e2a8b87667c5c567b5d34846cd880953d7409
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-10-31 00:52:46 +1100

    Create Rewards.lua

---
+++ GameData/Tower Defense Simulator [3260590327]/Rewards.lua
@@ -0,0 +1,109 @@
+data = {
+    ["DEFEAT_NUCLEAR_FALLEN_KING"] = {
+        ["Badge"] = 2124572797,
+        ["Claim"] = "function: 0x00000000d894b8a6",
+        ["Reward"] = "x1 Premium Crate"
+    },
+    ["STRENGTH_IN_NUMBERS"] = {
+        ["Badge"] = 2124572813,
+        ["Claim"] = "function: 0x000000008cf1fe16",
+        ["Reward"] = "100 Exp"
+    },
+    ["APOCALYPSE_RAVE"] = {["Badge"] = 2124572816, ["Claim"] = "function: 0x00000000b014a446", ["Reward"] = "100 Exp"},
+    ["DOUBLE_THE_LANES_DOUBLE_THE_POWER"] = {
+        ["Badge"] = 2124572828,
+        ["Claim"] = "function: 0x000000002ca871b6",
+        ["Reward"] = "x1 Basic Crate"
+    },
+    ["TRIUMPHANT"] = {
+        ["Badge"] = 2124581488,
+        ["Claim"] = "function: 0x000000005b6c01e6",
+        ["Reward"] = "x1 Premium Crate"
+    },
+    ["VERGE_OF_DEATH"] = {
+        ["Badge"] = 2124582922,
+        ["Claim"] = "function: 0x000000006560ed56",
+        ["Reward"] = "x1 Premium Crate"
+    },
+    ["SOLO_PLAYER"] = {
+        ["Badge"] = 2124582925,
+        ["Claim"] = "function: 0x00000000673fa686",
+        ["Reward"] = "x1 Premium Crate"
+    },
+    ["GOLDEN_POWER"] = {["Badge"] = 2124572809, ["Claim"] = "function: 0x00000000659998f6", ["Reward"] = "1000 Coins"},
+    ["DEFEAT_FALLEN_KING"] = {
+        ["Badge"] = 2124572796,
+        ["Claim"] = "function: 0x000000002becde26",
+        ["Reward"] = "250 Exp"
+    },
+    ["DEFEAT_GOLDEN_TITAN"] = {
+        ["Badge"] = 2124572795,
+        ["Claim"] = "function: 0x00000000ba332396",
+        ["Reward"] = "150 Exp"
+    },
+    ["DEFEAT_MOLTEN_BOSS"] = {
+        ["Badge"] = 2124572794,
+        ["Claim"] = "function: 0x00000000ae5517c6",
+        ["Reward"] = "100 Exp"
+    },
+    ["DEFEAT_GRAVE_DIGGER"] = {
+        ["Badge"] = 2124572793,
+        ["Claim"] = "function: 0x00000000de794336",
+        ["Reward"] = "75 Exp"
+    },
+    ["COWBOY"] = {
+        ["Badge"] = 2124474327,
+        ["Claim"] = "function: 0x000000007b208c8e",
+        ["Check"] = "function: 0x000000000b47ef66",
+        ["Reward"] = "Cowboy Tower"
+    },
+    ["CROOK_BOSS"] = {
+        ["Badge"] = 2124475816,
+        ["Claim"] = "function: 0x0000000084ca7806",
+        ["Check"] = "function: 0x000000006dddd6fe",
+        ["Reward"] = "Crook Boss Tower"
+    },
+    ["TURRET"] = {
+        ["Badge"] = 2124477836,
+        ["Claim"] = "function: 0x000000002dac1f9e",
+        ["Check"] = "function: 0x00000000c09a3a76",
+        ["Reward"] = "Turret Tower"
+    },
+    ["MORTAR"] = {
+        ["Badge"] = 2124477837,
+        ["Claim"] = "function: 0x00000000aa7ce83e",
+        ["Check"] = "function: 0x000000004815b116",
+        ["Reward"] = "Mortar Tower"
+    },
+    ["PURSUIT"] = {
+        ["Badge"] = 2124477838,
+        ["Claim"] = "function: 0x000000004fa6acb6",
+        ["Check"] = "function: 0x00000000969baa6e",
+        ["Reward"] = "Pursuit Tower"
+    },
+    ["BEAT_HARDCORE"] = {
+        ["Badge"] = 2124597827,
+        ["Claim"] = "function: 0x000000007d21eae6",
+        ["Reward"] = "x1 Premium Crate"
+    },
+    ["TRIUMPH_HARDCORE"] = {
+        ["Badge"] = 2124629158,
+        ["Claim"] = "function: 0x000000006be30856",
+        ["Reward"] = "x1 Deluxe Crate"
+    },
+    ["METAVERSE_EVENT"] = {
+        ["Badge"] = 2124634239,
+        ["Claim"] = "function: 0x000000008cbebdae",
+        ["Reward"] = "x1 Basic Crate"
+    },
+    ["DEFEAT_FROST_SPIRIT"] = {
+        ["Badge"] = 2124652180,
+        ["Claim"] = "function: 0x000000000c73b84e",
+        ["Reward"] = "400 Exp"
+    },
+    ["DEFEAT_NUCLEAR_MONSTER"] = {
+        ["Badge"] = 2127670181,
+        ["Claim"] = "function: 0x000000005baaeabe",
+        ["Reward"] = "x1 Premium Crate"
+    }
+}
