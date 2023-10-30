6e8b4fbed4a4f754f87d091b712da476e89e5ee5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-10-31 01:25:23 +1100

    Create Totem.lua

---
+++ GameData/Tower Defense Simulator [3260590327]/Totem.lua
@@ -0,0 +1,7 @@
+data = {["Default"] = {["Name"]="Default",["Description"]="The default charm.",["Model"]="Default"},
+["Fallen King"] = {["Name"]="Fallen King",["Description"]="Redeemed from Fallen King plushie.",["Model"]="Fallen King"},
+["Grave Digger"] = {["Name"]="Grave Digger",["Description"]="Redeemed from Grave Digger plushie.",["Model"]="Grave Digger"},
+["Gunslinger"] = {["Name"]="Gunslinger",["Description"]="Redeemed from Gun Slinger plushie.",["Model"]="Gunslinger"},
+["Molten Boss"] = {["Name"]="Molten Boss",["Description"]="Redeemed from Molten Boss plushie.",["Model"]="Molten Boss"},
+["Nuclear Monster"] = {["Name"]="Nuclear Monster",["Description"]="Redeemed from Nuclear Monster plushie.",["Model"]="Nuclear Monster"},
+}
