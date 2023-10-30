c1efb7385ec3c5fbecb00f68b4b152b92fa17fb0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-10-31 00:56:35 +1100

    Create Crate.lua

---
+++ GameData/Tower Defense Simulator [3260590327]/Content/Crate.lua
@@ -0,0 +1,439 @@
+data = {
+    ["Basic"] = {
+        ["Daily"] = {
+            ["CurrencyType"] = "2",
+            ["Prices"] = {["Rarities"] = {["1"] = 1250, ["3"] = 2000, ["2"] = 1500, ["5"] = 50000, ["4"] = 4000}}
+        },
+        ["Description"] = "A common skincrate used to unbox very basic skins.",
+        ["Category"] = "1",
+        ["Price"] = {["Value"] = 500, ["Type"] = "Coins"},
+        ["Preview"] = {
+            ["FieldOfView"] = 50,
+            ["Time"] = 1,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["IsAnimated"] = true,
+            ["CameraOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 5177997959,
+            ["Animation"] = "Animation"
+        },
+        ["Contents"] = {
+            ["Navy"] = {"Ace Pilot", "Accelerator"},
+            ["Blue"] = {
+                "Minigunner",
+                "Pyromancer",
+                "Ranger",
+                "Hunter",
+                "Scout",
+                "Crook Boss",
+                "Sniper",
+                "Demoman",
+                "Soldier"
+            },
+            ["Purple"] = {"Ace Pilot"},
+            ["Survivor"] = {"Scout"},
+            ["Military"] = {"Demoman"},
+            ["Green"] = {"Minigunner", "Commander", "Ace Pilot", "Ranger", "Scout", "Demoman"},
+            ["Ghillie"] = {"Sniper"},
+            ["Yellow"] = {"Ace Pilot", "Demoman"},
+            ["Aerial Ace"] = {"Ace Pilot"},
+            ["Red"] = {
+                "Commander",
+                "Paintballer",
+                "Ace Pilot",
+                "Scout",
+                "Crook Boss",
+                "Sniper",
+                "Soldier",
+                "Accelerator",
+                "Demoman"
+            }
+        }
+    },
+    ["Bunny"] = {
+        ["Contents"] = {
+            ["Bunny"] = {
+                "Minigunner",
+                "Pyromancer",
+                "Commander",
+                "Mortar",
+                "Paintballer",
+                "Ranger",
+                "Scout",
+                "Sniper",
+                "Electroshocker"
+            }
+        },
+        ["Preview"] = {
+            ["FieldOfView"] = 50,
+            ["Time"] = 1,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["CameraOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 4846840633
+        },
+        ["Description"] = "Happy Easter 2020!",
+        ["Category"] = "1"
+    },
+    ["Cold Front"] = {
+        ["Daily"] = {
+            ["CurrencyType"] = "2",
+            ["Prices"] = {["Rarities"] = {["1"] = 3000, ["4"] = 4500, ["3"] = 4000, ["2"] = 3500}}
+        },
+        ["Description"] = "Special delivery from the Arctic! (Skins by MidnightKrystal)",
+        ["Category"] = "1",
+        ["Price"] = {["Value"] = 3000, ["Type"] = "Coins"},
+        ["Preview"] = {
+            ["FieldOfView"] = 20,
+            ["Time"] = 3,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["IsAnimated"] = true,
+            ["CameraOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 5501324337,
+            ["Animation"] = "Animation"
+        },
+        ["Contents"] = {
+            ["Silent"] = {"Sniper"},
+            ["Ice Witch"] = {"Accelerator"},
+            ["Brave Soul"] = {"Sledger"},
+            ["Brisk"] = {"Commander"},
+            ["Frost Hunter"] = {"Scout"},
+            ["Stranded"] = {"Medic"}
+        }
+    },
+    ["Deluxe"] = {
+        ["Daily"] = {
+            ["CurrencyType"] = "4",
+            ["Prices"] = {
+                ["Rarities"] = {["1"] = 1346914696, ["4"] = 1346914794, ["3"] = 1346914768, ["2"] = 1346914737}
+            }
+        },
+        ["Description"] = "A deluxe skincrate used to unbox the highest quality skins.",
+        ["Category"] = "1",
+        ["Price"] = {["Id"] = 1110639180, ["Type"] = "Robux", ["Value"] = 300},
+        ["Preview"] = {
+            ["FieldOfView"] = 50,
+            ["Time"] = 1.15,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["IsAnimated"] = true,
+            ["CameraOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 5853271555,
+            ["Animation"] = "Animation"
+        },
+        ["Contents"] = {
+            ["Neon Rave"] = {"DJ Booth"},
+            ["Gun Gale"] = {"Ranger"},
+            ["Mage"] = {"Accelerator"},
+            ["Maid"] = {"Commander"},
+            ["Huntsman"] = {"Archer"},
+            ["Retired"] = {"Cowboy"},
+            ["Neko"] = {"DJ Booth", "Commander"},
+            ["Cyberpunk"] = {"Cowboy"},
+            ["Ace Pilot"] = {"Militant"},
+            ["Railgunner"] = {"Ranger"},
+            ["Soviet"] = {"Crook Boss"},
+            ["Noir"] = {"Cowboy"},
+            ["Tycoon"] = {"Farm"}
+        }
+    },
+    ["Ducky"] = {
+        ["Daily"] = {
+            ["CurrencyType"] = "2",
+            ["Prices"] = {["Rarities"] = {["1"] = 3000, ["4"] = 4500, ["3"] = 4000, ["2"] = 3500}}
+        },
+        ["Description"] = "Quack! Duck skins introduced in the 2022 'Duck Hunting' season.",
+        ["Category"] = "1",
+        ["Price"] = {["Value"] = 3000, ["Type"] = "Coins"},
+        ["Preview"] = {
+            ["FieldOfView"] = 10,
+            ["Time"] = 1.4,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["IsAnimated"] = true,
+            ["CameraOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 5501324337,
+            ["Animation"] = "Animation"
+        },
+        ["Contents"] = {
+            ["Ducky"] = {
+                "Scout",
+                "Sniper",
+                "Soldier",
+                "Hunter",
+                "Shotgunner",
+                "Militant",
+                "Minigunner",
+                "Commander",
+                "Farm",
+                "Engineer",
+                "Warden",
+                "Accelerator",
+                "Cowboy",
+                "Electroshocker",
+                "Mortar"
+            }
+        }
+    },
+    ["Frost"] = {
+        ["Contents"] = {["Frost"] = {"Minigunner", "Pyromancer", "Commander", "Ranger"}},
+        ["Preview"] = {
+            ["FieldOfView"] = 50,
+            ["Time"] = 1,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["CameraOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 6778699919
+        },
+        ["Description"] = "A chilling wind creeps into the room.",
+        ["Category"] = "1"
+    },
+    ["Golden"] = {
+        ["Daily"] = {
+            ["CurrencyType"] = "2",
+            ["Prices"] = {
+                ["Rarities"] = {["1"] = 100000, ["3"] = 100000, ["2"] = 100000, ["5"] = 100000, ["4"] = 100000},
+                ["Skins"] = {
+                    ["Pyromancer"] = 65000,
+                    ["Minigunner"] = 75000,
+                    ["Scout"] = 55000,
+                    ["Crook Boss"] = 55000,
+                    ["Soldier"] = 50000,
+                    ["Cowboy"] = 70000
+                }
+            }
+        },
+        ["Description"] = "Special skins for: Minigunner, Cowboy, Crook Boss, Pyro, Scout, Soldier.",
+        ["Category"] = "1",
+        ["Price"] = {["Value"] = 50000, ["Type"] = "Coins"},
+        ["Weights"] = {["1"] = 1, ["4"] = 4, ["3"] = 3, ["2"] = 2},
+        ["Contents"] = {["Golden"] = {"Minigunner", "Pyromancer", "Scout", "Crook Boss", "Soldier", "Cowboy"}},
+        ["Preview"] = {
+            ["FieldOfView"] = 50,
+            ["Time"] = 1.8,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["IsAnimated"] = true,
+            ["CameraOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 5177999683,
+            ["Animation"] = "Animation"
+        }
+    },
+    ["Halloween 2019"] = {
+        ["Contents"] = {
+            ["Pumpkin"] = {"Minigunner", "Ace Pilot", "Gladiator", "Rocketeer", "Cowboy"},
+            ["Demon"] = {"Gladiator", "Crook Boss"},
+            ["Gargoyle"] = {"Commander"},
+            ["Scarecrow"] = {"Pyromancer"}
+        },
+        ["Preview"] = {
+            ["FieldOfView"] = 50,
+            ["Time"] = 1,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["CameraOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 5501301558
+        },
+        ["Description"] = "Happy Halloween 2019!",
+        ["Category"] = "1"
+    },
+    ["Lovely"] = {
+        ["Daily"] = {
+            ["CurrencyType"] = "2",
+            ["Prices"] = {["Rarities"] = {["1"] = 4000, ["4"] = 7000, ["3"] = 5500, ["2"] = 4500}}
+        },
+        ["Description"] = "XOXO -Paradoxum Games",
+        ["Category"] = "1",
+        ["Preview"] = {
+            ["FieldOfView"] = 20,
+            ["Time"] = 3,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 0.98480773, 0, 0.173648179, 0, 1, 0, -0.173648179, 0, 0.98480773),
+            ["CameraOffset"] = CFrame.new(0, 0, 10, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 5501324337
+        },
+        ["Contents"] = {
+            ["Valentines"] = {"Commander", "Sniper", "Scout", "Soldier", "Cowboy"},
+            ["Chocolatier"] = {"Militant"},
+            ["Heartbreak"] = {"Engineer"},
+            ["Cupid"] = {"Crook Boss", "Accelerator"}
+        }
+    },
+    ["Party"] = {
+        ["Contents"] = {["Party"] = {"Minigunner", "Scout", "Soldier"}},
+        ["Preview"] = {
+            ["FieldOfView"] = 50,
+            ["Time"] = 1,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["CameraOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 5501380355
+        },
+        ["Description"] = "A time of celebration!",
+        ["Category"] = "1"
+    },
+    ["Pirate"] = {
+        ["Daily"] = {
+            ["CurrencyType"] = "2",
+            ["Prices"] = {["Rarities"] = {["1"] = 3500, ["4"] = 5000, ["3"] = 4500, ["2"] = 4000}}
+        },
+        ["Description"] = "ðŸ´â€â˜ ï¸ Arghhhhhhh! ",
+        ["Category"] = "1",
+        ["Price"] = {["Value"] = 3500, ["Type"] = "Coins"},
+        ["Preview"] = {
+            ["FieldOfView"] = 20,
+            ["Time"] = 2,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["IsAnimated"] = true,
+            ["CameraOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 5501324337,
+            ["Animation"] = "Animation"
+        },
+        ["Contents"] = {
+            ["Pirate"] = {
+                "Demoman",
+                "Commando",
+                "Commander",
+                "Farm",
+                "Mortar",
+                "Hunter",
+                "Crook Boss",
+                "Warden",
+                "Gladiator"
+            }
+        }
+    },
+    ["Premium"] = {
+        ["Daily"] = {
+            ["CurrencyType"] = "4",
+            ["Prices"] = {
+                ["Rarities"] = {["1"] = 1083359574, ["4"] = 1083359655, ["3"] = 1083359629, ["2"] = 1083359603}
+            }
+        },
+        ["Description"] = "A premium skincrate used to unbox more complex skins.",
+        ["Category"] = "1",
+        ["Price"] = {["Id"] = 785567507, ["Type"] = "Robux", ["Value"] = 100},
+        ["Preview"] = {
+            ["FieldOfView"] = 50,
+            ["Time"] = 1.15,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["IsAnimated"] = true,
+            ["CameraOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 5798487154,
+            ["Animation"] = "Animation"
+        },
+        ["Contents"] = {
+            ["Partisan"] = {"Ranger"},
+            ["General"] = {"Commander"},
+            ["XR300"] = {"Turret"},
+            ["Redemption"] = {"Cowboy"},
+            ["Black Ops"] = {"Minigunner", "Ranger", "Scout"},
+            ["Cop"] = {"Cowboy"},
+            ["Hazmat"] = {"Minigunner", "Militant", "Pyromancer", "Electroshocker"},
+            ["Cyber"] = {"Medic"},
+            ["Checker"] = {"Crook Boss"},
+            ["Deep Freeze"] = {"Freezer"},
+            ["Dark Matter"] = {"Ranger", "Rocketeer"},
+            ["Slugger"] = {"Gladiator"},
+            ["Acidic"] = {"Pyromancer"},
+            ["Cold Soldier"] = {"Soldier"},
+            ["Doughboy"] = {"Soldier"},
+            ["Fortress"] = {"Demoman"},
+            ["Victorian"] = {"Commander"},
+            ["Bosanka"] = {"Rocketeer"},
+            ["Heavy"] = {"Minigunner"},
+            ["Bandit"] = {"Cowboy"},
+            ["Arcade"] = {"Farm"},
+            ["Kasodus"] = {"Cowboy"},
+            ["XR500"] = {"Turret"},
+            ["Classic"] = {"Military Base"},
+            ["Galactic"] = {"Commander"},
+            ["Wraith"] = {"Minigunner", "Ranger"},
+            ["Mint Choco"] = {"Freezer"}
+        }
+    },
+    ["Pumpkin"] = {
+        ["Contents"] = {["Pumpkin"] = {"Minigunner", "Ace Pilot", "Gladiator", "Rocketeer", "Cowboy"}},
+        ["Preview"] = {
+            ["FieldOfView"] = 50,
+            ["Time"] = 1,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["CameraOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 5902698900
+        },
+        ["Description"] = "Ahahahaha... HAPPY HALLOWEEN!",
+        ["Category"] = "1"
+    },
+    ["Spooky"] = {
+        ["Contents"] = {
+            ["Spooky"] = {"Crook Boss", "Slasher", "Archer"},
+            ["Demon"] = {"Gladiator", "Crook Boss"},
+            ["Graveyard"] = {"Farm"},
+            ["Ghost"] = {"Minigunner", "Militant", "DJ Booth", "Pyromancer", "Commander", "Electroshocker"}
+        },
+        ["Preview"] = {
+            ["FieldOfView"] = 50,
+            ["Time"] = 1,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["CameraOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 5797457085
+        },
+        ["Description"] = "What could be inside? A trick or a treat?",
+        ["Category"] = "1"
+    },
+    ["Toy"] = {
+        ["Daily"] = {
+            ["CurrencyType"] = "2",
+            ["Prices"] = {["Rarities"] = {["1"] = 850, ["4"] = 1500, ["3"] = 1200, ["2"] = 1000}}
+        },
+        ["Description"] = "Who knew cheap plastic and toy blasters could be so deadly?",
+        ["Category"] = "1",
+        ["Price"] = {["Value"] = 850, ["Type"] = "Coins"},
+        ["Preview"] = {
+            ["FieldOfView"] = 50,
+            ["Time"] = 1,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["IsAnimated"] = true,
+            ["CameraOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 4771364114,
+            ["Animation"] = "Animation"
+        },
+        ["Contents"] = {["Toy"] = {"Minigunner", "Rocketeer", "Soldier"}}
+    },
+    ["Valentines"] = {
+        ["Contents"] = {["Valentines"] = {"Pyromancer", "Ranger", "Electroshocker", "Archer"}},
+        ["Preview"] = {
+            ["FieldOfView"] = 50,
+            ["Time"] = 1,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["CameraOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 5501324337
+        },
+        ["Description"] = "With extra love! <3 (Legacy Skins)",
+        ["Category"] = "1"
+    },
+    ["Vigilante"] = {
+        ["Daily"] = {
+            ["CurrencyType"] = "2",
+            ["Prices"] = {["Rarities"] = {["1"] = 3500, ["4"] = 5000, ["3"] = 4500, ["2"] = 4000}}
+        },
+        ["Description"] = "Bring justice to Cyber City!",
+        ["Category"] = "1",
+        ["Price"] = {["Value"] = 3500, ["Type"] = "Coins"},
+        ["Preview"] = {
+            ["FieldOfView"] = 20,
+            ["Time"] = 2,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["IsAnimated"] = true,
+            ["CameraOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 5501324337,
+            ["Animation"] = "Animation"
+        },
+        ["Contents"] = {
+            ["Vigilante"] = {"Accelerator", "Shotgunner", "Electroshocker", "Gladiator", "Pyromancer", "Mortar"}
+        }
+    },
+    ["Xmas 2019"] = {
+        ["Contents"] = {["Xmas"] = {"Minigunner", "Farm", "Crook Boss", "Rocketeer"}},
+        ["Preview"] = {
+            ["FieldOfView"] = 50,
+            ["Time"] = 1,
+            ["CrateRotation"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["CameraOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["Icon"] = 5501301703
+        },
+        ["Description"] = "Happy Holidays!",
+        ["Category"] = "1"
+    }
+}
