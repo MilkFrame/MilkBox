1f7f3c6ec1730bdd329884570bb298e166f3003f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-10-31 00:51:03 +1100

    Create Events.lua

---
+++ GameData/Tower Defense Simulator [3260590327]/Events.lua
@@ -0,0 +1,372 @@
+data = {
+    ["Halloween"] = {
+        {
+            ["Function"] = "function: 0x0000000079bc88a6",
+            ["Image"] = 5902698900,
+            ["Title"] = "Pumpkin Crate",
+            ["Value"] = 100
+        },
+        {
+            ["Function"] = "function: 0x000000004bcb0c5e",
+            ["Image"] = 5902698900,
+            ["Title"] = "Pumpkin Crate",
+            ["Value"] = 150
+        },
+        {
+            ["Function"] = "function: 0x00000000e12693f6",
+            ["Image"] = 5900548032,
+            ["Title"] = "Coffin Emote",
+            ["Value"] = 225
+        },
+        {
+            ["Function"] = "function: 0x00000000861e376e",
+            ["Image"] = 5797457085,
+            ["Title"] = "Spooky Crate",
+            ["Value"] = 260
+        },
+        {
+            ["Function"] = "function: 0x000000005f639a06",
+            ["Image"] = 5902698900,
+            ["Title"] = "Pumpkin Crate",
+            ["Value"] = 300
+        },
+        {
+            ["Function"] = "function: 0x0000000059c57ebe",
+            ["Image"] = 5797457085,
+            ["Title"] = "Spooky Crate",
+            ["Value"] = 350
+        },
+        {
+            ["Function"] = "function: 0x000000002b78dd56",
+            ["Image"] = 5900547542,
+            ["Title"] = "Demon Swing",
+            ["Value"] = 450
+        },
+        {
+            ["Function"] = "function: 0x00000000f2f439ce",
+            ["Image"] = 5902698900,
+            ["Title"] = "2 Pumpkin Crates",
+            ["Value"] = 500
+        },
+        {
+            ["Function"] = "function: 0x0000000036915466",
+            ["Image"] = 5797457085,
+            ["Title"] = "2 Spooky Crates",
+            ["Value"] = 550
+        },
+        {["Function"] = "function: 0x000000007fdff11e", ["Image"] = 5900547842, ["Title"] = "Couldron", ["Value"] = 650},
+        {
+            ["Function"] = "function: 0x00000000b84a4eb6",
+            ["Image"] = 5797457085,
+            ["Title"] = "2 Spooky Crates",
+            ["Value"] = 700
+        },
+        {
+            ["Function"] = "function: 0x00000000eb2aca2e",
+            ["Image"] = 5797457085,
+            ["Title"] = "3 Spooky Crates",
+            ["Value"] = 850
+        },
+        {
+            ["Function"] = "function: 0x00000000c23ec7c6",
+            ["Image"] = 5797457085,
+            ["Title"] = "4 Spooky Crates",
+            ["Value"] = 1100
+        }
+    },
+    ["Winter"] = {
+        {
+            ["Function"] = "function: 0x000000000a1cb83e",
+            ["Image"] = 5177997959,
+            ["Title"] = "Basic Crate",
+            ["Value"] = 150
+        },
+        {
+            ["Function"] = "function: 0x00000000632013d6",
+            ["Image"] = 6778699919,
+            ["Title"] = "Frost Crate",
+            ["Value"] = 225
+        },
+        {
+            ["Function"] = "function: 0x00000000404c7f4e",
+            ["Image"] = 6778801201,
+            ["Title"] = "Frozen Emote",
+            ["Value"] = 300
+        },
+        {
+            ["Function"] = "function: 0x000000009cbb1ae6",
+            ["Image"] = 5798487154,
+            ["Title"] = "Premium Crate",
+            ["Value"] = 350
+        },
+        {
+            ["Function"] = "function: 0x0000000086b5869e",
+            ["Image"] = 6778699919,
+            ["Title"] = "Frost Crate",
+            ["Value"] = 450
+        },
+        {
+            ["Function"] = "function: 0x00000000feef94fe",
+            ["Image"] = 6779086328,
+            ["Title"] = "Raw Dinner Emote",
+            ["Value"] = 500
+        },
+        {
+            ["Function"] = "function: 0x0000000036043696",
+            ["Image"] = 5798487154,
+            ["Title"] = "Premium Crate",
+            ["Value"] = 550
+        },
+        {
+            ["Function"] = "function: 0x0000000069e09a0e",
+            ["Image"] = 6778699919,
+            ["Title"] = "Frost Crate",
+            ["Value"] = 600
+        },
+        {
+            ["Function"] = "function: 0x00000000844effa6",
+            ["Image"] = 5853271555,
+            ["Title"] = "Deluxe Crate",
+            ["Value"] = 650
+        },
+        {
+            ["Function"] = "function: 0x00000000e0e6635e",
+            ["Image"] = 6778699919,
+            ["Title"] = "Frost Crates",
+            ["Value"] = 700
+        },
+        {
+            ["Function"] = "function: 0x00000000de9a24f6",
+            ["Image"] = 6779256928,
+            ["Title"] = "Recliner Emote",
+            ["Value"] = 850
+        }
+    },
+    ["Solar Eclipse"] = {
+        {
+            ["Function"] = "function: 0x00000000a8dc6d16",
+            ["Image"] = 7972183361,
+            ["Title"] = "Eclipse Scout",
+            ["Value"] = 150,
+            ["Claim"] = "function: 0x00000000d8e6fda6"
+        },
+        {
+            ["Function"] = "function: 0x00000000f529218e",
+            ["Image"] = 7990102631,
+            ["Title"] = "Torchlight Emote",
+            ["Value"] = 225
+        },
+        {
+            ["Function"] = "function: 0x00000000b20aa426",
+            ["Image"] = 5798487154,
+            ["Title"] = "Premium Crate",
+            ["Value"] = 300
+        },
+        {
+            ["Function"] = "function: 0x00000000713e18de",
+            ["Image"] = 7972185166,
+            ["Title"] = "Vampire Slayer Hunter",
+            ["Value"] = 350,
+            ["Claim"] = "function: 0x000000001bc45bce"
+        },
+        {
+            ["Function"] = "function: 0x000000003441ff76",
+            ["Image"] = 7990102059,
+            ["Title"] = "Candy Throw Emote",
+            ["Value"] = 450
+        },
+        {
+            ["Function"] = "function: 0x00000000e8ab13ee",
+            ["Image"] = 7972187295,
+            ["Title"] = "Eclipse Mortar",
+            ["Value"] = 500,
+            ["Claim"] = "function: 0x000000000cb9b116"
+        },
+        {
+            ["Function"] = "function: 0x00000000c598b786",
+            ["Image"] = 5853271555,
+            ["Title"] = "Deluxe Crate",
+            ["Value"] = 550
+        },
+        {
+            ["Function"] = "function: 0x0000000063940b3e",
+            ["Image"] = 7972187960,
+            ["Title"] = "Crusader Minigunner",
+            ["Value"] = 600,
+            ["Claim"] = "function: 0x000000006188e83e"
+        },
+        {
+            ["Function"] = "function: 0x00000000817380d6",
+            ["Image"] = 7972189067,
+            ["Title"] = "Eclipse Ranger",
+            ["Value"] = 650,
+            ["Claim"] = "function: 0x00000000c05b8146"
+        },
+        {
+            ["Function"] = "function: 0x00000000a4fd0c4e",
+            ["Image"] = 7990100957,
+            ["Title"] = "Axe Throw Emote",
+            ["Value"] = 700
+        },
+        {
+            ["Function"] = "function: 0x00000000cf7789e6",
+            ["Image"] = 7972189846,
+            ["Title"] = "Eclipse Accelerator",
+            ["Value"] = 850,
+            ["Claim"] = "function: 0x000000006677aa6e"
+        },
+        {
+            ["Function"] = "function: 0x000000002a0a359e",
+            ["Image"] = 7972188533,
+            ["Title"] = "Eclipse Commander",
+            ["Value"] = 900,
+            ["Claim"] = "function: 0x000000008c72acb6"
+        }
+    },
+    ["Season of Love"] = {
+        {
+            ["Function"] = "function: 0x00000000434eef0e",
+            ["Image"] = 8808640218,
+            ["Title"] = "Valentines Scout",
+            ["Value"] = 100,
+            ["Claim"] = "function: 0x00000000d84deae6"
+        },
+        {
+            ["Function"] = "function: 0x0000000020d272a6",
+            ["Image"] = 8808640816,
+            ["Title"] = "Valentines Soldier",
+            ["Value"] = 200,
+            ["Claim"] = "function: 0x000000002ab5310e"
+        },
+        {
+            ["Function"] = "function: 0x000000000515d65e",
+            ["Image"] = 8808641341,
+            ["Title"] = "Chocolatier Militant",
+            ["Value"] = 300,
+            ["Claim"] = "function: 0x000000000b970856"
+        },
+        {
+            ["Function"] = "function: 0x000000002e0fb9f6",
+            ["Image"] = 8808641872,
+            ["Title"] = "Cupid Crook Boss",
+            ["Value"] = 350,
+            ["Claim"] = "function: 0x0000000032ebe17e"
+        },
+        {
+            ["Function"] = "function: 0x0000000049655d6e",
+            ["Image"] = 8808769324,
+            ["Title"] = "Valentines Cowboy",
+            ["Value"] = 475,
+            ["Claim"] = "function: 0x0000000031ff5b86"
+        },
+        {
+            ["Function"] = "function: 0x00000000bcc0c006",
+            ["Image"] = 8808642288,
+            ["Title"] = "Cupid Accelerator",
+            ["Value"] = 600,
+            ["Claim"] = "function: 0x00000000cee2bdae"
+        }
+    },
+    ["Hunting Season"] = {
+        {
+            ["Function"] = "function: 0x00000000a8c0797e",
+            ["Image"] = 9378102786,
+            ["Title"] = "Ducky Sniper",
+            ["Value"] = 225,
+            ["Claim"] = "function: 0x00000000694c9326"
+        },
+        {
+            ["Function"] = "function: 0x00000000fd631216",
+            ["Image"] = 9378103512,
+            ["Title"] = "Ducky Soldier",
+            ["Value"] = 500,
+            ["Claim"] = "function: 0x00000000c89fb84e"
+        },
+        {
+            ["Function"] = "function: 0x000000003386b68e",
+            ["Image"] = 9378099553,
+            ["Title"] = "Ducky Hunter",
+            ["Value"] = 750,
+            ["Claim"] = "function: 0x000000007b884696"
+        },
+        {
+            ["Function"] = "function: 0x00000000727e0b26",
+            ["Image"] = 9378136278,
+            ["Title"] = "Ducky Militant",
+            ["Value"] = 1050,
+            ["Claim"] = "function: 0x00000000b9faa78e"
+        },
+        {
+            ["Function"] = "function: 0x000000004cf38fde",
+            ["Image"] = 9378102266,
+            ["Title"] = "Ducky Shotgunner",
+            ["Value"] = 1400,
+            ["Claim"] = "function: 0x000000003ae875d6"
+        },
+        {
+            ["Function"] = "function: 0x00000000e8510076",
+            ["Image"] = 9378101436,
+            ["Title"] = "Ducky Minigunner",
+            ["Value"] = 1750,
+            ["Claim"] = "function: 0x000000006f4f189e"
+        },
+        {
+            ["Function"] = "function: 0x000000009d1c84ee",
+            ["Image"] = 9378098303,
+            ["Title"] = "Ducky Commander",
+            ["Value"] = 2000,
+            ["Claim"] = "function: 0x0000000049ba6356"
+        },
+        {
+            ["Function"] = "function: 0x00000000a2083a86",
+            ["Image"] = 9433442088,
+            ["Title"] = "Ducky Farm",
+            ["Value"] = 2500,
+            ["Claim"] = "function: 0x0000000041bf145e"
+        },
+        {
+            ["Function"] = "function: 0x0000000041259e3e",
+            ["Image"] = 9433442546,
+            ["Title"] = "Ducky Engineer",
+            ["Value"] = 3000,
+            ["Claim"] = "function: 0x00000000414890e6"
+        }
+    },
+    ["End of Summer"] = {
+        {
+            ["Function"] = "function: 0x00000000e40c90d6",
+            ["Image"] = 10488006471,
+            ["Title"] = "Beach Scout",
+            ["Value"] = 225,
+            ["Claim"] = "function: 0x000000006bf7770e"
+        },
+        {
+            ["Function"] = "function: 0x00000000df20fc4e",
+            ["Image"] = 10488004920,
+            ["Title"] = "Beach Militant",
+            ["Value"] = 500,
+            ["Claim"] = "function: 0x00000000cadb8656"
+        },
+        {
+            ["Function"] = "function: 0x0000000015c499e6",
+            ["Image"] = 10488003451,
+            ["Title"] = "BBQ Pyromancer",
+            ["Value"] = 750,
+            ["Claim"] = "function: 0x0000000099cea37e"
+        },
+        {
+            ["Function"] = "function: 0x0000000012af259e",
+            ["Image"] = 10488005975,
+            ["Title"] = "Beach Minigunner",
+            ["Value"] = 1050,
+            ["Claim"] = "function: 0x0000000086b05d86"
+        },
+        {
+            ["Function"] = "function: 0x000000003095a236",
+            ["Image"] = 10488007217,
+            ["Title"] = "Lifeguard Commander",
+            ["Value"] = 1400,
+            ["Claim"] = "function: 0x00000000af91ffae"
+        }
+    }
+}
