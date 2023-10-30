134111e536b6bf5002d22ce20c8ad3b71bfce88e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-10-31 00:50:19 +1100

    Create Emotes.lua

---
+++ GameData/Tower Defense Simulator [3260590327]/Emotes.lua
@@ -0,0 +1,402 @@
+data = {
+    ["Flex"] = {
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["JumpPower"] = 0,
+        ["Description"] = "FLEX THOSE MUSCLES!!!",
+        ["WalkSpeed"] = 0,
+        ["Price"] = {["Value"] = 500, ["Type"] = "Coins"},
+        ["Looped"] = false,
+        ["Icon"] = "5058721184",
+        ["Creator"] = "Shapic2"
+    },
+    ["Power Dance"] = {
+        ["Description"] = "Power!",
+        ["Price"] = {["Value"] = 800, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["JumpPower"] = 0,
+        ["WalkSpeed"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Sound"] = "9166993251",
+        ["Icon"] = "4820711723",
+        ["Creator"] = "AltiWyre"
+    },
+    ["Hotline"] = {
+        ["Description"] = "I know when that hotline bling, that can only mean one thing.",
+        ["Price"] = {["Value"] = 1500, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["JumpPower"] = 0,
+        ["WalkSpeed"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = true},
+        ["Sound"] = "4834782022",
+        ["Icon"] = "5069650260",
+        ["Creator"] = "Falosaur"
+    },
+    ["Praisin"] = {
+        ["WalkSpeed"] = 0,
+        ["Sound"] = "4834756022",
+        ["Description"] = "Ya'll mind if I praise the lord?",
+        ["JumpPower"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = true},
+        ["Looped"] = true,
+        ["Icon"] = "5307806059",
+        ["Creator"] = "Falosaur"
+    },
+    ["Cat Dance"] = {
+        ["WalkSpeed"] = 0,
+        ["Sound"] = "9166996669",
+        ["Description"] = "Funni Cat & may mays!",
+        ["JumpPower"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Looped"] = true,
+        ["Icon"] = "5307815051",
+        ["Creator"] = "RostTyme"
+    },
+    ["Mannrobics"] = {
+        ["Description"] = "Keep it moving!",
+        ["Price"] = {["Value"] = 2000, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["JumpPower"] = 0,
+        ["WalkSpeed"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Sound"] = "9166993858",
+        ["Icon"] = "5307808353",
+        ["Creator"] = "RostTyme"
+    },
+    ["Orange Justice"] = {
+        ["WalkSpeed"] = 0,
+        ["Sound"] = "9166995058",
+        ["Description"] = "Its also a great workout.",
+        ["JumpPower"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Looped"] = true,
+        ["Icon"] = "5307807285",
+        ["Creator"] = "Falosaur"
+    },
+    ["Thriller"] = {
+        ["Description"] = "'Cause this is thriller, thriller night",
+        ["Price"] = {["Value"] = 2500, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["JumpPower"] = 0,
+        ["WalkSpeed"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = true},
+        ["Sound"] = "4678640322",
+        ["Icon"] = "5307800783",
+        ["Creator"] = "RostTyme"
+    },
+    ["Revenge"] = {
+        ["WalkSpeed"] = 0,
+        ["Sound"] = "9166994385",
+        ["Description"] = "Creeper, aw man...",
+        ["JumpPower"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = true},
+        ["Looped"] = true,
+        ["Icon"] = "5307803357",
+        ["Creator"] = "Shapic2"
+    },
+    ["Garry's Dance"] = {
+        ["Description"] = "Gmod lol",
+        ["Price"] = {["Value"] = 1000, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["JumpPower"] = 0,
+        ["WalkSpeed"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Sound"] = "9166993681",
+        ["Icon"] = "5307809800",
+        ["Creator"] = "MRePotatoes"
+    },
+    ["Gang Dance"] = {
+        ["Description"] = "Epic joe reference!",
+        ["Price"] = {["Value"] = 5000, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["JumpPower"] = 0,
+        ["WalkSpeed"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Sound"] = "9166994018",
+        ["Icon"] = "5307810974",
+        ["Creator"] = "RostTyme"
+    },
+    ["Smug"] = {
+        ["Description"] = "Peace and tranquility is all I need.",
+        ["Price"] = {["Value"] = 650, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["JumpPower"] = 0,
+        ["WalkSpeed"] = 4,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Sound"] = "9166993097",
+        ["Icon"] = "5307801926",
+        ["Creator"] = "MRePotatoes"
+    },
+    ["Comeback"] = {
+        ["Description"] = "Show them how it's done.",
+        ["Price"] = {["Value"] = 1250, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["JumpPower"] = 0,
+        ["WalkSpeed"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Sound"] = "4634656670",
+        ["Icon"] = "5307813619",
+        ["Creator"] = "AltiWyre"
+    },
+    ["Breakdown"] = {
+        ["WalkSpeed"] = 0,
+        ["Sound"] = "9166991772",
+        ["Description"] = "Goes with any song!",
+        ["JumpPower"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Looped"] = true,
+        ["Icon"] = "5307816251",
+        ["Creator"] = "AltiWyre"
+    },
+    ["Wess"] = {
+        ["Description"] = "This is embarassing. Turn the other way.",
+        ["Price"] = {["Value"] = 1500, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["JumpPower"] = 0,
+        ["WalkSpeed"] = 5,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Sound"] = "4583054488",
+        ["Icon"] = "5307799220",
+        ["Creator"] = "Shapic2"
+    },
+    ["Beggin"] = {
+        ["Description"] = "Day n' nite",
+        ["Price"] = {["Value"] = 1500, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["JumpPower"] = 0,
+        ["WalkSpeed"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Sound"] = "9166995886",
+        ["Icon"] = "5307817352",
+        ["Creator"] = "Falosaur"
+    },
+    ["Epic Backflips"] = {
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["JumpPower"] = 0,
+        ["Description"] = "Round and round and round...",
+        ["WalkSpeed"] = 0,
+        ["Price"] = {["Value"] = 500, ["Type"] = "Coins"},
+        ["Looped"] = false,
+        ["Icon"] = "5307812590",
+        ["Creator"] = "painteduser"
+    },
+    ["Radical"] = {
+        ["Description"] = "That was rad dude!",
+        ["Price"] = {["Value"] = 1500, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["JumpPower"] = 0,
+        ["WalkSpeed"] = 2,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Sound"] = "5283140469",
+        ["Icon"] = "5307804773",
+        ["Creator"] = "RostTyme"
+    },
+    ["Lazy Couch"] = {
+        ["JumpPower"] = 0,
+        ["Description"] = "The best way to get into the Holiday spirit!",
+        ["WalkSpeed"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Looped"] = true,
+        ["Icon"] = "5363077783",
+        ["Creator"] = "MRePotatoes"
+    },
+    ["Firework"] = {
+        ["JumpPower"] = 0,
+        ["Description"] = "Kaboom! A spectacular show!",
+        ["WalkSpeed"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Looped"] = false,
+        ["Icon"] = "5363078008",
+        ["Creator"] = "MRePotatoes"
+    },
+    ["Raw Dinner"] = {
+        ["JumpPower"] = 0,
+        ["Description"] = "I'd like my meat raw please!",
+        ["WalkSpeed"] = 12,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Looped"] = true,
+        ["Icon"] = "6779086328",
+        ["Creator"] = "BelowNatural"
+    },
+    ["Tpose"] = {
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["JumpPower"] = 0,
+        ["Description"] = "Assert dominance.",
+        ["WalkSpeed"] = 4,
+        ["Price"] = {["Value"] = 750, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["Icon"] = "5624146345",
+        ["Creator"] = "BelowNatural"
+    },
+    ["Monster Mash"] = {
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["JumpPower"] = 0,
+        ["Description"] = "Do the monster mash!",
+        ["WalkSpeed"] = 4,
+        ["Price"] = {["Value"] = 300, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["Icon"] = "5624171493",
+        ["Creator"] = "Roblox"
+    },
+    ["Classic Party"] = {
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["JumpPower"] = 0,
+        ["Description"] = "^-^ 2008 ROBLOX!",
+        ["WalkSpeed"] = 4,
+        ["Price"] = {["Value"] = 300, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["Icon"] = "5624186677",
+        ["Creator"] = "Roblox"
+    },
+    ["Rat"] = {
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["JumpPower"] = 0,
+        ["Description"] = "Oh shoot, a rat!",
+        ["WalkSpeed"] = 0,
+        ["Price"] = {["Value"] = 2000, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["Icon"] = "5624215384",
+        ["Creator"] = "MRePotatoes"
+    },
+    ["Boston Breakdance"] = {
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["WalkSpeed"] = 0,
+        ["Description"] = "Boom!! I am a dance machine!",
+        ["JumpPower"] = 0,
+        ["Price"] = {["Value"] = 650, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["Icon"] = "5624228898",
+        ["Creator"] = "RostTyme"
+    },
+    ["Dream Feet"] = {
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["WalkSpeed"] = 0,
+        ["Description"] = "Jumpstyle!",
+        ["JumpPower"] = 0,
+        ["Price"] = {["Value"] = 800, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["Icon"] = "5624240335",
+        ["Creator"] = "Shapic2"
+    },
+    ["Transcendence"] = {
+        ["Description"] = "Transcend reality",
+        ["Price"] = {["Value"] = 1000, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["JumpPower"] = 0,
+        ["WalkSpeed"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Sound"] = "9166996324",
+        ["Icon"] = "5624291950",
+        ["Creator"] = "BelowNatural"
+    },
+    ["Russian"] = {
+        ["JumpPower"] = 0,
+        ["Description"] = "Dance with me comrade!",
+        ["WalkSpeed"] = 5,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Looped"] = true,
+        ["Icon"] = "5624606295",
+        ["Creator"] = "Shapic2"
+    },
+    ["Dab"] = {
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["JumpPower"] = 0,
+        ["Description"] = "Dab on them haters!",
+        ["WalkSpeed"] = 0,
+        ["Price"] = {["Value"] = 500, ["Type"] = "Coins"},
+        ["Looped"] = true,
+        ["Icon"] = "5625252965",
+        ["Creator"] = "Altiwyre"
+    },
+    ["Whip"] = {
+        ["WalkSpeed"] = 0,
+        ["Sound"] = "9166993681",
+        ["Description"] = "Watch me whip",
+        ["JumpPower"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Looped"] = true,
+        ["Icon"] = "5645342307",
+        ["Creator"] = "ZerkomisticDynamic"
+    },
+    ["Demon Swing"] = {
+        ["WalkSpeed"] = 0,
+        ["Sound"] = "5887363589",
+        ["Description"] = "I can be your angle or devil >:3",
+        ["JumpPower"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Looped"] = true,
+        ["Icon"] = "5900547542",
+        ["Creator"] = "BelowNatural"
+    },
+    ["Couldron"] = {
+        ["JumpPower"] = 0,
+        ["Description"] = "Stirring up mischief..",
+        ["WalkSpeed"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Looped"] = true,
+        ["Icon"] = "5900547842",
+        ["Creator"] = "BelowNatural"
+    },
+    ["Coffin"] = {
+        ["JumpPower"] = 0,
+        ["Description"] = "Lol ded",
+        ["WalkSpeed"] = 4,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Looped"] = true,
+        ["Icon"] = "5900548032",
+        ["Creator"] = "BelowNatural"
+    },
+    ["Frozen"] = {
+        ["JumpPower"] = 0,
+        ["Description"] = "You're pretty chill!",
+        ["WalkSpeed"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Looped"] = true,
+        ["Icon"] = "6778801201",
+        ["Creator"] = "BelowNatural"
+    },
+    ["Bloxy Cola"] = {
+        ["JumpPower"] = 0,
+        ["Description"] = "Pick up your trash!!!",
+        ["WalkSpeed"] = 4,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Looped"] = true,
+        ["Icon"] = "5363077783",
+        ["Creator"] = "BelowNatural"
+    },
+    ["Recliner"] = {
+        ["JumpPower"] = 0,
+        ["Description"] = "Sit back and enjoy the view!",
+        ["WalkSpeed"] = 0,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Looped"] = true,
+        ["Icon"] = "6779256928",
+        ["Creator"] = "BelowNatural"
+    },
+    ["Candy Throw"] = {
+        ["JumpPower"] = 0,
+        ["Description"] = "Don't ask where I got the basket!",
+        ["WalkSpeed"] = 6,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Looped"] = true,
+        ["Icon"] = "7990102059",
+        ["Creator"] = "BelowNatural"
+    },
+    ["Axe Throw"] = {
+        ["JumpPower"] = 50,
+        ["Description"] = "It always returns.",
+        ["WalkSpeed"] = 12,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Looped"] = false,
+        ["Icon"] = "7990100957",
+        ["Creator"] = "BelowNatural"
+    },
+    ["Torchlight"] = {
+        ["JumpPower"] = 50,
+        ["Description"] = "A light to bright up your night.",
+        ["WalkSpeed"] = 12,
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Looped"] = true,
+        ["Icon"] = "7990102631",
+        ["Creator"] = "BelowNatural"
+    }
+}
