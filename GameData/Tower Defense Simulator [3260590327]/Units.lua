40152ab6caae4f1f942774c60a494416c23559d4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-10-31 00:54:34 +1100

    Create Units.lua

---
+++ GameData/Tower Defense Simulator [3260590327]/Units.lua
@@ -0,0 +1,58 @@
+data = {["Crook"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=25,["SendTime"]=2,["Health"]=25,["upgrades"]={0,0,0,0,0},["Speed"]=3,["Hidden"]=false,["Cooldown"]=0.3,["Damage"]=1},["Skins"]={"Default"}},
+["Mafia"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=25,["SendTime"]=2,["Health"]=50,["upgrades"]={0,0,0,0,0},["Speed"]=3,["Hidden"]=false,["Cooldown"]=0.15,["Damage"]=3},["Skins"]={"Default"}},
+["Humvee 3"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=30,["SendTime"]=3.5,["Health"]=60,["upgrades"]={0,0,0,0,0},["Speed"]=6,["Hidden"]=false,["Cooldown"]=0.2,["Damage"]=3},["Skins"]={"Classic","Default"}},
+["Monster"] = {["Stats"]={["images"]={},["Price"]=0,["Range"]=10,["SendTime"]=2,["Health"]=400000,["upgrades"]={0,0,0,0,0},["Speed"]=1.2,["Hidden"]=false,["Cooldown"]=0.6,["Damage"]=1500},["Skins"]={"Default"}},
+["Humvee"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=5,["SendTime"]=3.5,["Health"]=25,["upgrades"]={0,0,0,0,0},["Speed"]=6,["Hidden"]=false,["Cooldown"]=0,["Damage"]=0},["Skins"]={"Classic","Default"}},
+["Tank"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=30,["upgrades"]={0,0,0,0,0},["SendTime"]=3.5,["Health"]=400,["Hidden"]=false,["Speed"]=6,["ExplosionDamage"]=25,["Cooldown"]=0.2,["Damage"]=6},["Skins"]={"Classic","Default"}},
+["Railgun Tank"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=30,["upgrades"]={0,0,0,0,0},["SendTime"]=3.5,["Health"]=1200,["Hidden"]=false,["Speed"]=6,["ExplosionDamage"]=60,["Cooldown"]=0.2,["Damage"]=16},["Skins"]={"Classic","Default"}},
+["SWAT Van"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=5,["SendTime"]=3.5,["Health"]=600,["upgrades"]={0,0,0,0,0},["Speed"]=4,["Hidden"]=false,["Cooldown"]=20,["Damage"]=1},["Skins"]={"Default"}},
+["XR700"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=32,["SendTime"]=3.5,["Health"]=500,["upgrades"]={0,0,0,0,0},["Speed"]=4,["Hidden"]=false,["Cooldown"]=0.1,["Damage"]=8},["Skins"]={"Default"}},
+["XR900"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=32,["SendTime"]=3.5,["Health"]=1500,["upgrades"]={0,0,0,0,0},["Speed"]=4,["Hidden"]=false,["Cooldown"]=0.05,["Damage"]=10},["Skins"]={"Default"}},
+["Mark5"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=100,["SendTime"]=3.5,["Health"]=5000,["upgrades"]={0,0,0,0,0},["Speed"]=3,["Hidden"]=false,["Cooldown"]=0.05,["Damage"]=20},["Skins"]={"Default"}},
+["Golden Crook"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=25,["SendTime"]=2,["Health"]=25,["upgrades"]={0,0,0,0,0},["Speed"]=4,["Hidden"]=false,["Cooldown"]=0.3,["Damage"]=2},["Skins"]={"Default"}},
+["Golden Mafia"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=25,["SendTime"]=2,["Health"]=50,["upgrades"]={0,0,0,0,0},["Speed"]=4,["Hidden"]=false,["Cooldown"]=0.15,["Damage"]=5},["Skins"]={"Default"}},
+["XR600"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=38,["SendTime"]=3.5,["Health"]=100,["upgrades"]={0,0,0,0,0},["Speed"]=4,["Hidden"]=false,["Cooldown"]=0.4,["Damage"]=8},["Skins"]={"Default"}},
+["XR901"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=32,["SendTime"]=3.5,["Health"]=4500,["upgrades"]={0,0,0,0,0},["Speed"]=4,["Hidden"]=false,["Cooldown"]=0.05,["Damage"]=15},["Skins"]={"Default"}},
+["Jeep"] = {["Stats"]={["images"]={},["Price"]=0,["Range"]=30,["SendTime"]=3.5,["Health"]=50,["upgrades"]={0,0,0,0,0},["Speed"]=5,["Hidden"]=false,["Cooldown"]=0.12,["Damage"]=3},["Skins"]={}},
+["XR800"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=32,["SendTime"]=3.5,["Health"]=500,["upgrades"]={0,0,0,0,0},["Speed"]=4,["Hidden"]=false,["Cooldown"]=0.1,["Damage"]=8},["Skins"]={"Default"}},
+["XR600Rocket"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=38,["SendTime"]=3.5,["Health"]=100,["upgrades"]={0,0,0,0,0},["Speed"]=4,["Hidden"]=false,["Cooldown"]=0.4,["Damage"]=8},["Skins"]={"Default"}},
+["Tank2"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=30,["upgrades"]={0,0,0,0,0},["SendTime"]=3.5,["Health"]=400,["Hidden"]=false,["Speed"]=6,["ExplosionDamage"]=25,["Cooldown"]=0.2,["Damage"]=6},["Skins"]={}},
+["Railgun Tank2"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=30,["upgrades"]={0,0,0,0,0},["SendTime"]=3.5,["Health"]=1200,["Hidden"]=false,["Speed"]=6,["ExplosionDamage"]=60,["Cooldown"]=0.2,["Damage"]=16},["Skins"]={}},
+["Truck 3"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=30,["SendTime"]=3.5,["Health"]=60,["upgrades"]={0,0,0,0,0},["Speed"]=6,["Hidden"]=false,["Cooldown"]=0.2,["Damage"]=3},["Skins"]={}},
+["Sentry4"] = {["Stats"]={["Price"]=0,["ExplosionDamage"]=15,["Range"]=22,["ScrapCost"]=100,["Hidden"]=false,["SendTime"]=1.75,["Health"]=120,["Lifetime"]=60,["Speed"]=1.5,["IgnoreCollisionDamage"]=true,["Cooldown"]=0.12,["Damage"]=12},["Skins"]={"Default"}},
+["Truck 2"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=5,["SendTime"]=3.5,["Health"]=60,["upgrades"]={0,0,0,0,0},["Speed"]=6,["Hidden"]=false,["Cooldown"]=0,["Damage"]=0},["Skins"]={}},
+["Sentry1"] = {["Stats"]={["Price"]=0,["Range"]=18,["Hidden"]=false,["ScrapCost"]=10,["SendTime"]=1.25,["Health"]=20,["Lifetime"]=20,["Speed"]=1.25,["IgnoreCollisionDamage"]=true,["Cooldown"]=0.5,["Damage"]=1},["Skins"]={"Default"}},
+["Sentry2"] = {["Stats"]={["Price"]=0,["Range"]=20,["Hidden"]=false,["ScrapCost"]=20,["SendTime"]=1.5,["Health"]=40,["Lifetime"]=30,["Speed"]=1.25,["IgnoreCollisionDamage"]=true,["Cooldown"]=0.25,["Damage"]=2},["Skins"]={"Default"}},
+["Sentry3"] = {["Stats"]={["Price"]=0,["Range"]=20,["Hidden"]=false,["ScrapCost"]=50,["SendTime"]=1.75,["Health"]=60,["Lifetime"]=45,["Speed"]=1.25,["IgnoreCollisionDamage"]=true,["Cooldown"]=0.16,["Damage"]=5},["Skins"]={"Default"}},
+["Mark3"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=25,["SendTime"]=3.5,["Health"]=1200,["upgrades"]={0,0,0,0,0},["Speed"]=3,["Hidden"]=false,["Cooldown"]=0.08,["Damage"]=10},["Skins"]={"Default"}},
+["Mark2"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=25,["SendTime"]=3.5,["Health"]=750,["upgrades"]={0,0,0,0,0},["Speed"]=3,["Hidden"]=false,["Cooldown"]=0.15,["Damage"]=12},["Skins"]={"Default"}},
+["Mark4"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=100,["SendTime"]=3.5,["Health"]=2000,["upgrades"]={0,0,0,0,0},["Speed"]=3,["Hidden"]=false,["Cooldown"]=0.05,["Damage"]=10},["Skins"]={"Default"}},
+["Mark1"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=30,["SendTime"]=3.5,["Health"]=400,["upgrades"]={0,0,0,0,0},["Speed"]=3,["Hidden"]=false,["Cooldown"]=0.2,["Damage"]=8},["Skins"]={"Default"}},
+["Mark1Rocket"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=30,["SendTime"]=3.5,["Health"]=400,["upgrades"]={0,0,0,0,0},["Speed"]=3,["Hidden"]=false,["Cooldown"]=0.2,["Damage"]=8},["Skins"]={"Default"}},
+["Soldier"] = {["Stats"]={["Price"]=0,["SendTime"]=2,["Health"]=25,["Hidden"]=false,["Speed"]=4,["Range"]=25,["Cooldown"]=0.2,["Damage"]=1},["Skins"]={"Default"}},
+["Citizen 1"] = {["Stats"]={["images"]={},["Price"]=0,["Range"]=8,["SendTime"]=2,["Health"]=135,["upgrades"]={0,0,0,0,0},["Speed"]=3,["Hidden"]=false,["Cooldown"]=1,["Damage"]=60},["Skins"]={"Default"}},
+["Citizen 2"] = {["Stats"]={["images"]={},["Price"]=0,["Range"]=5,["SendTime"]=2,["Health"]=100,["upgrades"]={0,0,0,0,0},["Speed"]=5,["Hidden"]=false,["Cooldown"]=0.3,["Damage"]=35},["Skins"]={"Default"}},
+["TestTruck"] = {["Stats"]={["images"]={},["Price"]=0,["Range"]=30,["SendTime"]=3.5,["Health"]=60,["upgrades"]={0,0,0,0,0},["Speed"]=6,["Hidden"]=false,["Cooldown"]=0.12,["Damage"]=3},["Skins"]={}},
+["Guard3"] = {["Stats"]={["upgrades"]={0,0,0,0,0},["Price"]=0,["Range"]=25,["SendTime"]=2,["Health"]=50,["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Speed"]=4,["Hidden"]=false,["Cooldown"]=0.2,["Damage"]=2},["Skins"]={"Default"}},
+["Guard2"] = {["Stats"]={["upgrades"]={0,0,0,0,0},["Price"]=0,["Range"]=20,["SendTime"]=2,["Health"]=10,["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Speed"]=4,["Hidden"]=false,["Cooldown"]=0.5,["Damage"]=2},["Skins"]={"Default"}},
+["Guard1"] = {["Stats"]={["upgrades"]={0,0,0,0,0},["Price"]=0,["Range"]=20,["SendTime"]=2,["Health"]=10,["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Speed"]=4,["Hidden"]=false,["Cooldown"]=1,["Damage"]=2},["Skins"]={"Default"}},
+["Tank3"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=30,["upgrades"]={0,0,0,0,0},["SendTime"]=3.5,["Health"]=400,["Hidden"]=false,["Speed"]=6,["ExplosionDamage"]=25,["Cooldown"]=0.2,["Damage"]=6},["Skins"]={}},
+["Humvee 2"] = {["Stats"]={["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Price"]=0,["Range"]=5,["SendTime"]=3.5,["Health"]=60,["upgrades"]={0,0,0,0,0},["Speed"]=6,["Hidden"]=false,["Cooldown"]=0,["Damage"]=0},["Skins"]={"Classic","Default"}},
+["GoldenGuard1"] = {["Stats"]={["upgrades"]={0,0,0,0,0},["Price"]=0,["Range"]=20,["SendTime"]=2,["Health"]=10,["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Speed"]=4,["Hidden"]=false,["Cooldown"]=0.6,["Damage"]=2},["Skins"]={"Default"}},
+["GoldenGuard2"] = {["Stats"]={["upgrades"]={0,0,0,0,0},["Price"]=0,["Range"]=20,["SendTime"]=2,["Health"]=10,["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Speed"]=4,["Hidden"]=false,["Cooldown"]=0.3,["Damage"]=3},["Skins"]={"Default"}},
+["GoldenGuard3"] = {["Stats"]={["upgrades"]={0,0,0,0,0},["Price"]=0,["Range"]=25,["SendTime"]=2,["Health"]=50,["images"]={3280081327,3284939244,3284940162,3284941120,3284943100},["Speed"]=4,["Hidden"]=false,["Cooldown"]=0.15,["Damage"]=4},["Skins"]={"Default"}},
+}
+out="{"
+for i,v in pairs(game:GetService("ReplicatedStorage").Assets.Units:GetChildren()) do
+   -- if v:FindFirstChild("Data") then
+    line2 = require(v.Stats)
+    local sk
+    if v:FindFirstChild("Skins") then
+     sk=v.Skins:GetChildren() else sk={} end
+    table.sort(sk,function(s1,s2) return s1.Name<s2.Name end)
+    out=out.."[\""..v.Name.."\"] = {[\"Stats\"]="..table_to_string(line2)..",[\"Skins\"]="..table_to_string(sk).."},\n"
+   -- end
+end out=out.."}"
+--local copy = table_to_string(out)
+wait(1)
+setclipboard(out) print("Done")
