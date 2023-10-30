ce3e3cddb162fa47b32522ed3b57369cf25ed993
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-10-31 00:49:38 +1100

    Create Crates.lua

---
+++ GameData/Tower Defense Simulator [3260590327]/Crates.lua
@@ -0,0 +1,291 @@
+data = {
+    ["Basic"] = {
+        ["Description"] = "A common skincrate used to unbox very basic skins.",
+        ["Price"] = {["Value"] = 1000, ["Type"] = "Coins"},
+        ["Percentages"] = {["Rare"] = "20%", ["Uncommon"] = "40%", ["Common"] = "50%"},
+        ["Rarities"] = {["Rare"] = 0, ["Uncommon"] = 0.2, ["Common"] = 0.5},
+        ["Skins"] = {
+            ["Navy"] = "Uncommon",
+            ["Blue"] = "Common",
+            ["Purple"] = "Common",
+            ["Survivor"] = "Uncommon",
+            ["Green"] = "Common",
+            ["Ghillie"] = "Uncommon",
+            ["Yellow"] = "Common",
+            ["Aerial Ace"] = "Rare",
+            ["Red"] = "Common"
+        },
+        ["Display"] = {["Time"] = 1, ["Zoom"] = 2},
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Icon"] = 5177997959,
+        ["Duplicates"] = true
+    },
+    ["Premium"] = {
+        ["Description"] = "A premium skincrate used to unbox more complex skins.",
+        ["Price"] = {["Value"] = 100, ["Type"] = "Robux"},
+        ["Percentages"] = {["Rare"] = "20%", ["Legendary"] = "10%", ["Uncommon"] = "30%", ["Common"] = "40%"},
+        ["Rarities"] = {["Rare"] = 0.25, ["Legendary"] = 0.00001, ["Uncommon"] = 0.5, ["Common"] = 0.75},
+        ["Skins"] = {
+            ["Partisan"] = "Common",
+            ["General"] = "Common",
+            ["XR300"] = "Uncommon",
+            ["Redemption"] = "Legendary",
+            ["Black Ops"] = "Common",
+            ["Cop"] = "Uncommon",
+            ["Hazmat"] = "Rare",
+            ["Cyber"] = "Rare",
+            ["Checker"] = "Uncommon",
+            ["Pirate"] = "Common",
+            ["Dark Matter"] = "Legendary",
+            ["Slugger"] = "Rare",
+            ["Kasodus"] = "Rare,",
+            ["Cold Soldier"] = "Common",
+            ["Doughboy"] = "Common",
+            ["Victorian"] = "Common",
+            ["Galactic"] = "Rare",
+            ["Classic"] = "Common",
+            ["Heavy"] = "Common",
+            ["Arcade"] = "Rare",
+            ["Mint Choco"] = "Uncommon",
+            ["Deep Freeze"] = "Uncommon",
+            ["XR500"] = "Legendary",
+            ["Bandit"] = "Uncommon",
+            ["Bosanka"] = "Common",
+            ["Wraith"] = "Rare",
+            ["Acidic"] = "Common"
+        },
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Display"] = {["Time"] = 1.15},
+        ["Id"] = 785567507,
+        ["Icon"] = 5798487154,
+        ["Duplicates"] = false
+    },
+    ["Golden"] = {
+        ["Description"] = "Special skins for: Minigunner, Cowboy, Crook Boss, Pyro, Scout, Soldier.",
+        ["Price"] = {["Value"] = 50000, ["Type"] = "Coins"},
+        ["Percentages"] = {["Legendary"] = "100%"},
+        ["Rarities"] = {["Legendary"] = 0},
+        ["Skins"] = {["Golden"] = "Legendary"},
+        ["Display"] = {["Time"] = 1.8},
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Icon"] = 5177999683,
+        ["Duplicates"] = false
+    },
+    ["Toy"] = {
+        ["Description"] = "Who knew cheap plastic and toy blasters could be so deadly?",
+        ["Price"] = {["Value"] = 3500, ["Type"] = "Coins"},
+        ["Percentages"] = {["Uncommon"] = "100%"},
+        ["Rarities"] = {["Uncommon"] = 0},
+        ["Skins"] = {["Toy"] = "Uncommon"},
+        ["Display"] = {["Time"] = 1, ["Zoom"] = 2},
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Icon"] = 4771364114,
+        ["Duplicates"] = true
+    },
+    ["Bunny"] = {
+        ["Rarities"] = {["Rare"] = 0},
+        ["Skins"] = {["Bunny"] = "Rare"},
+        ["Description"] = "Happy Easter 2020!",
+        ["Percentages"] = {["Rare"] = "100%"},
+        ["Display"] = {["Time"] = 1, ["Zoom"] = 2},
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Icon"] = 4846840633,
+        ["Duplicates"] = false
+    },
+    ["Halloween 2019"] = {
+        ["Rarities"] = {["Rare"] = 0},
+        ["Skins"] = {
+            ["Pumpkin"] = "Rare",
+            ["Demon"] = "Rare",
+            ["Hallow Punk"] = "Rare",
+            ["Witch"] = "Rare",
+            ["Gargoyle"] = "Rare",
+            ["Scarecrow"] = "Rare"
+        },
+        ["Description"] = "Happy Halloween 2019!",
+        ["Percentages"] = {["Rare"] = "100%"},
+        ["Display"] = {["Time"] = 1, ["Zoom"] = 2},
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Icon"] = 5501301558,
+        ["Duplicates"] = false
+    },
+    ["Xmas 2019"] = {
+        ["Rarities"] = {["Rare"] = 0},
+        ["Skins"] = {["Xmas"] = "Rare"},
+        ["Description"] = "Happy Holidays!",
+        ["Percentages"] = {["Rare"] = "100%"},
+        ["Display"] = {["Time"] = 1, ["Zoom"] = 2},
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Icon"] = 5501301703,
+        ["Duplicates"] = false
+    },
+    ["Valentines"] = {
+        ["Rarities"] = {["Rare"] = 0},
+        ["Skins"] = {["Valentines"] = "Rare"},
+        ["Description"] = "Happy Valentines 2020!",
+        ["Percentages"] = {["Rare"] = "100%"},
+        ["Display"] = {["Time"] = 1, ["Zoom"] = 2},
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Icon"] = 5501324337,
+        ["Duplicates"] = false
+    },
+    ["Party"] = {
+        ["Rarities"] = {["Rare"] = 0},
+        ["Skins"] = {["Party"] = "Rare"},
+        ["Description"] = "A time of celebration!",
+        ["Percentages"] = {["Rare"] = "100%"},
+        ["Display"] = {["Time"] = 1, ["Zoom"] = 2},
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Icon"] = 5501380355,
+        ["Duplicates"] = false
+    },
+    ["Spooky"] = {
+        ["Rarities"] = {["Rare"] = 0},
+        ["Skins"] = {["Spooky"] = "Rare", ["Demon"] = "Rare", ["Graveyard"] = "Rare", ["Ghost"] = "Rare"},
+        ["Description"] = "What could be inside? A trick or a treat?",
+        ["Percentages"] = {["Rare"] = "100%"},
+        ["Display"] = {["Time"] = 1, ["Zoom"] = 2},
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Icon"] = 5797457085,
+        ["Duplicates"] = false
+    },
+    ["Deluxe"] = {
+        ["Description"] = "A deluxe skincrate used to unbox the highest quality skins.",
+        ["Price"] = {["Value"] = 300, ["Type"] = "Robux"},
+        ["Percentages"] = {["Rare"] = "20%", ["Legendary"] = "10%", ["Uncommon"] = "30%", ["Common"] = "40%"},
+        ["Rarities"] = {["Rare"] = 0.25, ["Legendary"] = 0.00001, ["Uncommon"] = 0.5, ["Common"] = 0.75},
+        ["Skins"] = {
+            ["Huntsman"] = "Legendary",
+            ["Noir"] = "Legendary",
+            ["Gun Gale"] = "Legendary",
+            ["Mage"] = "Legendary",
+            ["Maid"] = "Legendary",
+            ["Tycoon"] = "Legendary",
+            ["Retired"] = "Legendary",
+            ["Railgunner"] = "Legendary",
+            ["Cyberpunk"] = "Legendary",
+            ["Neon Rave"] = "Legendary",
+            ["Ace Pilot"] = "Legendary",
+            ["Soviet"] = "Legendary",
+            ["Slayer"] = "Legendary",
+            ["Neko"] = "Legendary"
+        },
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Display"] = {["Time"] = 1.15, ["Zoom"] = 2},
+        ["Id"] = 1110639180,
+        ["Icon"] = 5853271555,
+        ["Duplicates"] = false
+    },
+    ["Pumpkin"] = {
+        ["Rarities"] = {["Rare"] = 0},
+        ["Skins"] = {["Pumpkin"] = "Rare"},
+        ["Description"] = "Ahahahaha... HAPPY HALLOWEEN!",
+        ["Percentages"] = {["Rare"] = "100%"},
+        ["Display"] = {["Time"] = 1, ["Zoom"] = 2},
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Icon"] = 5902698900,
+        ["Duplicates"] = false
+    },
+    ["Frost"] = {
+        ["Rarities"] = {["Rare"] = 0},
+        ["Skins"] = {["Frost"] = "Rare"},
+        ["Description"] = "A chilling wind creeps into the room.",
+        ["Percentages"] = {["Rare"] = "100%"},
+        ["Display"] = {["Time"] = 1, ["Zoom"] = 2},
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Icon"] = 6778699919,
+        ["Duplicates"] = false
+    },
+    ["Vigilante"] = {
+        ["Description"] = "A premium skincrate used to unbox more complex skins.",
+        ["Price"] = {["Value"] = 100, ["Type"] = "Robux"},
+        ["Percentages"] = {["Rare"] = "20%", ["Legendary"] = "10%", ["Uncommon"] = "30%", ["Common"] = "40%"},
+        ["Rarities"] = {["Rare"] = 0.25, ["Legendary"] = 0.00001, ["Uncommon"] = 0.5, ["Common"] = 0.75},
+        ["Skins"] = {
+            ["Partisan"] = "Common",
+            ["General"] = "Common",
+            ["XR300"] = "Uncommon",
+            ["Redemption"] = "Legendary",
+            ["Black Ops"] = "Common",
+            ["Cop"] = "Uncommon",
+            ["Hazmat"] = "Rare",
+            ["Cyber"] = "Rare",
+            ["Checker"] = "Uncommon",
+            ["Pirate"] = "Common",
+            ["Dark Matter"] = "Legendary",
+            ["Slugger"] = "Rare",
+            ["Kasodus"] = "Rare,",
+            ["Cold Soldier"] = "Common",
+            ["Doughboy"] = "Common",
+            ["Victorian"] = "Common",
+            ["Galactic"] = "Rare",
+            ["Classic"] = "Common",
+            ["Heavy"] = "Common",
+            ["Arcade"] = "Rare",
+            ["Mint Choco"] = "Uncommon",
+            ["Deep Freeze"] = "Uncommon",
+            ["XR500"] = "Legendary",
+            ["Bandit"] = "Uncommon",
+            ["Bosanka"] = "Common",
+            ["Wraith"] = "Rare",
+            ["Acidic"] = "Common"
+        },
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Display"] = {["Time"] = 1.15},
+        ["Id"] = 785567507,
+        ["Icon"] = 5798487154,
+        ["Duplicates"] = false
+    },
+    ["Pirate"] = {
+        ["Description"] = "A premium skincrate used to unbox more complex skins.",
+        ["Price"] = {["Value"] = 100, ["Type"] = "Robux"},
+        ["Percentages"] = {["Rare"] = "20%", ["Legendary"] = "10%", ["Uncommon"] = "30%", ["Common"] = "40%"},
+        ["Rarities"] = {["Rare"] = 0.25, ["Legendary"] = 0.00001, ["Uncommon"] = 0.5, ["Common"] = 0.75},
+        ["Skins"] = {
+            ["Partisan"] = "Common",
+            ["General"] = "Common",
+            ["XR300"] = "Uncommon",
+            ["Redemption"] = "Legendary",
+            ["Black Ops"] = "Common",
+            ["Cop"] = "Uncommon",
+            ["Hazmat"] = "Rare",
+            ["Cyber"] = "Rare",
+            ["Checker"] = "Uncommon",
+            ["Pirate"] = "Common",
+            ["Dark Matter"] = "Legendary",
+            ["Slugger"] = "Rare",
+            ["Kasodus"] = "Rare,",
+            ["Cold Soldier"] = "Common",
+            ["Doughboy"] = "Common",
+            ["Victorian"] = "Common",
+            ["Galactic"] = "Rare",
+            ["Classic"] = "Common",
+            ["Heavy"] = "Common",
+            ["Arcade"] = "Rare",
+            ["Mint Choco"] = "Uncommon",
+            ["Deep Freeze"] = "Uncommon",
+            ["XR500"] = "Legendary",
+            ["Bandit"] = "Uncommon",
+            ["Bosanka"] = "Common",
+            ["Wraith"] = "Rare",
+            ["Acidic"] = "Common"
+        },
+        ["Status"] = {["Sale"] = false, ["New"] = false},
+        ["Display"] = {["Time"] = 1.15},
+        ["Id"] = 785567507,
+        ["Icon"] = 5798487154,
+        ["Duplicates"] = false
+    }
+}
+out = "{"
+for i, v in pairs(game:GetService("ReplicatedStorage").Assets.Events:GetChildren()) do
+    -- if v:FindFirstChild("Data") then
+    line1 = require(v)
+    out = out .. '["' .. v.Name .. '"] = ' .. table_to_string(line1) .. ",\n"
+    -- end
+end
+out = out .. "}"
+--local copy = table_to_string(out)
+wait(1)
+setclipboard(out)
+print("Done")
