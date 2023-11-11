26a8bab268531ef2a44f257b1d451e54ef18fbf9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 18:44:41 +1100

    Create FreeGifts.lua

---
+++ GameData/Pet Simulator X [6284583030]/FreeGifts.lua
@@ -0,0 +1,153 @@
+data = {
+    {
+        ["rewardNum"] = {1, 1},
+        ["masteryXP"] = 125,
+        ["waitTime"] = 300,
+        ["rewards"] = {{"Coins", 100}, {"Diamonds", 50}},
+        ["icon"] = "rbxassetid://9009590481",
+        ["rare"] = false
+    },
+    {
+        ["rewardNum"] = {1, 2},
+        ["masteryXP"] = 175,
+        ["waitTime"] = 600,
+        ["rewards"] = {{"Coins", 100}, {"Diamonds", 50}, {"Boosts", 20}},
+        ["icon"] = "rbxassetid://9009590481",
+        ["rare"] = false
+    },
+    {
+        ["rewardNum"] = {1, 2},
+        ["masteryXP"] = 200,
+        ["waitTime"] = 900,
+        ["rewards"] = {{"Coins", 90}, {"Diamonds", 60}, {"Boosts", 25}},
+        ["icon"] = "rbxassetid://9009590481",
+        ["rare"] = false
+    },
+    {
+        ["rewardNum"] = {2, 2},
+        ["masteryXP"] = 250,
+        ["waitTime"] = 1200,
+        ["rewards"] = {
+            {"Coins", 90},
+            {"Diamonds", 60},
+            {"Boosts", 25},
+            {"CoinsJumbo", 7.5},
+            {"DiamondsJumbo", 5},
+            {"BoostsRare", 5}
+        },
+        ["icon"] = "rbxassetid://8978530123",
+        ["rare"] = false
+    },
+    {
+        ["rewardNum"] = {2, 3},
+        ["masteryXP"] = 350,
+        ["waitTime"] = 1800,
+        ["rewards"] = {
+            {"Coins", 90},
+            {"Diamonds", 60},
+            {"Boosts", 30},
+            {"CoinsJumbo", 9},
+            {"DiamondsJumbo", 6.5},
+            {"BoostsRare", 6.5}
+        },
+        ["icon"] = "rbxassetid://8978530123",
+        ["rare"] = false
+    },
+    {
+        ["rewardNum"] = {3, 3},
+        ["masteryXP"] = 400,
+        ["waitTime"] = 2400,
+        ["rewards"] = {
+            {"Coins", 90},
+            {"Diamonds", 60},
+            {"Boosts", 30},
+            {"CoinsJumbo", 10},
+            {"DiamondsJumbo", 7.5},
+            {"BoostsRare", 7.5}
+        },
+        ["icon"] = "rbxassetid://8978530123",
+        ["rare"] = false
+    },
+    {
+        ["rewardNum"] = {3, 3},
+        ["masteryXP"] = 500,
+        ["waitTime"] = 3000,
+        ["rewards"] = {
+            {"Coins", 90},
+            {"Diamonds", 60},
+            {"Boosts", 30},
+            {"CoinsJumbo", 12.5},
+            {"DiamondsJumbo", 10},
+            {"BoostsRare", 10}
+        },
+        ["icon"] = "rbxassetid://8978530123",
+        ["rare"] = false
+    },
+    {
+        ["rewardNum"] = {3, 4},
+        ["masteryXP"] = 600,
+        ["waitTime"] = 3600,
+        ["rewards"] = {
+            {"Coins", 90},
+            {"Diamonds", 60},
+            {"Boosts", 30},
+            {"CoinsJumbo", 12.5},
+            {"DiamondsJumbo", 10},
+            {"BoostsRare", 10}
+        },
+        ["icon"] = "rbxassetid://9009611254",
+        ["rare"] = false
+    },
+    {
+        ["rewardNum"] = {3, 4},
+        ["masteryXP"] = 700,
+        ["waitTime"] = 4500,
+        ["rewards"] = {
+            {"Coins", 80},
+            {"Diamonds", 60},
+            {"Boosts", 30},
+            {"CoinsJumbo", 20},
+            {"DiamondsJumbo", 15},
+            {"BoostsRare", 12.5}
+        },
+        ["icon"] = "rbxassetid://9009611254",
+        ["rare"] = false
+    },
+    {
+        ["rewardNum"] = {4, 4},
+        ["masteryXP"] = 900,
+        ["waitTime"] = 5400,
+        ["rewards"] = {{"Diamonds", 60}, {"Boosts", 25}, {"CoinsJumbo", 20}, {"DiamondsJumbo", 15}, {"BoostsRare", 15}},
+        ["icon"] = "rbxassetid://9009611254",
+        ["rare"] = false
+    },
+    {
+        ["rewardNum"] = {5, 5},
+        ["masteryXP"] = 1400,
+        ["waitTime"] = 7200,
+        ["rewards"] = {
+            {"Diamonds", 30},
+            {"Boosts", 15},
+            {"CoinsJumbo", 20},
+            {"DiamondsJumbo", 15},
+            {"BoostsRare", 10},
+            {"HugePet", 0.005}
+        },
+        ["icon"] = "rbxassetid://8343895468",
+        ["rare"] = true
+    },
+    {
+        ["rewardNum"] = {6, 6},
+        ["masteryXP"] = 2000,
+        ["waitTime"] = 10800,
+        ["rewards"] = {
+            {"Diamonds", 30},
+            {"CoinsJumbo", 20},
+            {"DiamondsJumbo", 15},
+            {"BoostsRare", 10},
+            {"HugePet", 0.05}
+        },
+        ["icon"] = "rbxassetid://8343895468",
+        ["rare"] = true
+    }
+}
