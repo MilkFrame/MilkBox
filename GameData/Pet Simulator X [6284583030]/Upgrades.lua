e5c987c92dbe69b2f1bb033551437ac63e944c64
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 18:50:30 +1100

    Create Upgrades.lua

---
+++ GameData/Pet Simulator X [6284583030]/Upgrades.lua
@@ -0,0 +1,144 @@
+data = {
+    ["Lucky Block Huge Pets"] = {
+        ["prices"] = {10000000, 50000000, 250000000, 750000000, 2250000000},
+        ["currency"] = "Lucky Coins",
+        ["title"] = "Lucky Block Huge Chance",
+        ["icon"] = "rbxassetid://7878226819",
+        ["isLuckyBlockEvent"] = true
+    },
+    ["Orb Pickup Distance"] = {
+        ["prices"] = {1250, 3500, 6750, 13500, 20000},
+        ["currency"] = "Diamonds",
+        ["title"] = "Orbs Reach",
+        ["icon"] = "rbxassetid://7056089728"
+    },
+    ["Lucky Block Boosters"] = {
+        ["prices"] = {10000000, 40000000, 140000000, 400000000, 900000000},
+        ["currency"] = "Lucky Coins",
+        ["title"] = "More Boosters",
+        ["icon"] = "rbxassetid://13415040808",
+        ["isLuckyBlockEvent"] = true
+    },
+    ["Yeet Egg Price"] = {
+        ["prices"] = {200, 300, 400, 500, 750, 1000, 2000, 3000, 4000, 5000},
+        ["currency"] = "Yeet Orbs",
+        ["title"] = "Better Yeet Egg Prices",
+        ["icon"] = "rbxassetid://14153188040",
+        ["isYeetEvent"] = true
+    },
+    ["Pet Strength"] = {
+        ["prices"] = {1750, 5000, 13250, 40000, 100000},
+        ["currency"] = "Diamonds",
+        ["title"] = "Pet Strength",
+        ["icon"] = "rbxassetid://7056090073"
+    },
+    ["Halloween Mythical Pets"] = {
+        ["prices"] = {8000000, 32500000, 115000000, 375000000, 1000000000},
+        ["currency"] = "Halloween Candy",
+        ["title"] = "Mythical Pets Chance",
+        ["icon"] = "rbxassetid://7878226819",
+        ["isHalloweenEvent"] = true
+    },
+    ["Halloween More Candy"] = {
+        ["prices"] = {2500000, 12000000, 47500000, 175000000, 450000000},
+        ["currency"] = "Halloween Candy",
+        ["title"] = "More Candy",
+        ["icon"] = "rbxassetid://7878227066",
+        ["isHalloweenEvent"] = true
+    },
+    ["Lucky Block Pet Strength"] = {
+        ["prices"] = {4000000, 26000000, 80000000, 300000000, 500000000},
+        ["currency"] = "Lucky Coins",
+        ["title"] = "Lucky Block Pet Strength",
+        ["icon"] = "rbxassetid://7056090073",
+        ["isLuckyBlockEvent"] = true
+    },
+    ["Yeet Egg Luck"] = {
+        ["prices"] = {50, 100, 150, 250, 500, 750, 1000, 2500, 5000, 7500},
+        ["currency"] = "Yeet Orbs",
+        ["title"] = "Higher Yeet Egg Luck",
+        ["icon"] = "rbxassetid://7878226819",
+        ["isYeetEvent"] = true
+    },
+    ["Halloween Better Enchants"] = {
+        ["prices"] = {1250000, 4750000, 12500000, 35000000, 70000000},
+        ["currency"] = "Halloween Candy",
+        ["title"] = "Better Re-Enchants",
+        ["icon"] = "rbxassetid://7878227199",
+        ["isHalloweenEvent"] = true
+    },
+    ["Lucky Block More Diamonds"] = {
+        ["prices"] = {5000000, 20000000, 70000000, 200000000, 450000000},
+        ["currency"] = "Lucky Coins",
+        ["title"] = "More Lucky Block Diamonds",
+        ["icon"] = "rbxassetid://7056089501",
+        ["isLuckyBlockEvent"] = true
+    },
+    ["Halloween Pet Storage"] = {
+        ["prices"] = {500000, 1250000, 4500000, 11000000, 35000000},
+        ["currency"] = "Halloween Candy",
+        ["title"] = "More Storage",
+        ["icon"] = "rbxassetid://7878226631",
+        ["isHalloweenEvent"] = true
+    },
+    ["More Diamonds"] = {
+        ["prices"] = {2500, 10000, 30000, 80000, 150000},
+        ["currency"] = "Diamonds",
+        ["title"] = "More Diamonds",
+        ["icon"] = "rbxassetid://7056089501"
+    },
+    ["Lucky Block More Lucky Coins"] = {
+        ["prices"] = {5000000, 30000000, 100000000, 300000000, 750000000},
+        ["currency"] = "Lucky Coins",
+        ["title"] = "More Lucky Coins",
+        ["icon"] = "rbxassetid://13415110719",
+        ["isLuckyBlockEvent"] = true
+    },
+    ["Yeet Critical Chance"] = {
+        ["prices"] = {50, 300, 500, 1000, 2000, 3000, 4000, 5000, 7500, 9000},
+        ["currency"] = "Yeet Orbs",
+        ["title"] = "Critical Yeet Chance",
+        ["icon"] = "rbxassetid://7056090073",
+        ["isYeetEvent"] = true
+    },
+    ["Yeet Orb Power"] = {
+        ["prices"] = {200, 300, 500, 750, 1000, 1500, 3000, 5000, 7500, 10000},
+        ["currency"] = "Yeet Orbs",
+        ["title"] = "More Strength from Orbs",
+        ["icon"] = "rbxassetid://7056089832",
+        ["isYeetEvent"] = true
+    },
+    ["Yeet Orb Pickup Distance"] = {
+        ["prices"] = {50, 100, 150, 200, 300, 500, 750, 1000, 2000, 3000},
+        ["currency"] = "Yeet Orbs",
+        ["title"] = "Yeet Orbs Reach",
+        ["icon"] = "rbxassetid://7056089728",
+        ["isYeetEvent"] = true
+    },
+    ["Pet Storage"] = {
+        ["prices"] = {3000, 5000, 7000, 11000, 15000},
+        ["currency"] = "Diamonds",
+        ["title"] = "More Storage",
+        ["icon"] = "rbxassetid://7056089976"
+    },
+    ["Halloween More Diamonds"] = {
+        ["prices"] = {2000000, 9500000, 32500000, 90000000, 175000000},
+        ["currency"] = "Halloween Candy",
+        ["title"] = "More Diamonds",
+        ["icon"] = "rbxassetid://7056089501",
+        ["isHalloweenEvent"] = true
+    },
+    ["Halloween Legendary Pets"] = {
+        ["prices"] = {1750000, 6000000, 25000000, 110000000, 300000000},
+        ["currency"] = "Halloween Candy",
+        ["title"] = "Legendary Pets Chance",
+        ["icon"] = "rbxassetid://7878226947",
+        ["isHalloweenEvent"] = true
+    },
+    ["Pet Walkspeed"] = {
+        ["prices"] = {750, 2500, 7000, 15000, 30000},
+        ["currency"] = "Diamonds",
+        ["title"] = "Pet Speed",
+        ["icon"] = "rbxassetid://7056089832"
+    }
+}
