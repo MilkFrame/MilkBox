609eaa6ab10e33addf52b7c94ffc6664406e74d2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 18:44:56 +1100

    Create Fruits.lua

---
+++ GameData/Pet Simulator X [6284583030]/Fruits.lua
@@ -0,0 +1,62 @@
+data = {
+    ["Rainbow"] = {
+        ["Bonus"] = "function: 0x00000000b1ebce73",
+        ["BonusName"] = "All",
+        ["Name"] = "Rainbow",
+        ["HardCap"] = 200,
+        ["DecayRate"] = 180,
+        ["Coin"] = "Rainbow Fruit",
+        ["Icon"] = "rbxassetid://7786012670",
+        ["DisplayName"] = "Rainbow"
+    },
+    ["Pineapple"] = {
+        ["Bonus"] = "function: 0x000000005854f52b",
+        ["BonusName"] = "XP",
+        ["Name"] = "Pineapple",
+        ["HardCap"] = 200,
+        ["DecayRate"] = 180,
+        ["Coin"] = "Pineapple",
+        ["Icon"] = "rbxassetid://7786012670",
+        ["DisplayName"] = "Pineapple"
+    },
+    ["Apple"] = {
+        ["Bonus"] = "function: 0x000000008630a38b",
+        ["BonusName"] = "Damage",
+        ["Name"] = "Apple",
+        ["HardCap"] = 200,
+        ["DecayRate"] = 180,
+        ["Coin"] = "Apple",
+        ["Icon"] = "rbxassetid://7786012670",
+        ["DisplayName"] = "Apple"
+    },
+    ["Pear"] = {
+        ["Bonus"] = "function: 0x00000000f753b36b",
+        ["BonusName"] = "Coins",
+        ["Name"] = "Pear",
+        ["HardCap"] = 200,
+        ["DecayRate"] = 180,
+        ["Coin"] = "Pear",
+        ["Icon"] = "rbxassetid://7786012670",
+        ["DisplayName"] = "Pear"
+    },
+    ["Orange"] = {
+        ["Bonus"] = "function: 0x00000000e015bcc3",
+        ["BonusName"] = "Diamonds",
+        ["Name"] = "Orange",
+        ["HardCap"] = 200,
+        ["DecayRate"] = 180,
+        ["Coin"] = "Orange",
+        ["Icon"] = "rbxassetid://7786012670",
+        ["DisplayName"] = "Orange"
+    },
+    ["Banana"] = {
+        ["Bonus"] = "function: 0x000000008f908b9b",
+        ["BonusName"] = "Luck",
+        ["Name"] = "Banana",
+        ["HardCap"] = 200,
+        ["DecayRate"] = 180,
+        ["Coin"] = "Banana",
+        ["Icon"] = "rbxassetid://7786012670",
+        ["DisplayName"] = "Banana"
+    }
+}
