e87dcf185b339681a47799713ca53124f2d752f6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 18:44:17 +1100

    Create Events.lua

---
+++ GameData/Pet Simulator X [6284583030]/Events.lua
@@ -0,0 +1,230 @@
+data = {
+    ["Summer"] = {
+        ["Hard"] = {
+            {
+                ["Name"] = "Hard-1",
+                ["Coin"] = {["Pattern"] = "Diamond", ["World"] = "Diamond"},
+                ["Description"] = "Break {Amount} Diamond{s} in the Diamond Mine!",
+                ["Amount"] = 600
+            },
+            {["Enchant"] = {}, ["Name"] = "Hard-2", ["Amount"] = 300},
+            {["FreeGifts"] = {}, ["Name"] = "Hard-3", ["Amount"] = 3},
+            {["Name"] = "Hard-4", ["Coin"] = {["Pattern"] = "Crate"}, ["Amount"] = 300},
+            {["Name"] = "Hard-5", ["Coin"] = {["Pattern"] = "Chest"}, ["Amount"] = 250},
+            {
+                ["Name"] = "Hard-6",
+                ["Amount"] = 300,
+                ["Coin"] = {["Pattern"] = "Safe"},
+                ["IgnoreForFirstSelection"] = true
+            },
+            {
+                ["Name"] = "Hard-7",
+                ["Amount"] = 250,
+                ["Coin"] = {["Pattern"] = "Vault"},
+                ["IgnoreForFirstSelection"] = true
+            },
+            {["Egg"] = {}, ["Name"] = "Hard-8", ["Amount"] = 175},
+            {
+                ["Golden"] = {["SuccessRequired"] = true, ["MinimumChance"] = 13},
+                ["Amount"] = 10,
+                ["Name"] = "Hard-9",
+                ["IgnoreForFirstSelection"] = true
+            },
+            {
+                ["Rainbow"] = {["SuccessRequired"] = true, ["MinimumChance"] = 13},
+                ["Amount"] = 5,
+                ["Name"] = "Hard-10",
+                ["IgnoreForFirstSelection"] = true
+            },
+            {
+                ["Amount"] = 3,
+                ["DarkMatter"] = {["ClaimRequired"] = true},
+                ["Name"] = "Hard-11",
+                ["IgnoreForFirstSelection"] = true
+            },
+            {
+                ["Egg"] = {["Rarity"] = "Mythical"},
+                ["Amount"] = 5,
+                ["Name"] = "Hard-12",
+                ["IgnoreForFirstSelection"] = true
+            },
+            {["Amount"] = 3, ["Merchant"] = {}, ["Name"] = "Hard-13", ["IgnoreForFirstSelection"] = true},
+            {
+                ["ServerBoost"] = {["BoostType"] = "Triple Coins"},
+                ["Amount"] = 3,
+                ["Name"] = "Hard-14",
+                ["IgnoreForFirstSelection"] = true
+            },
+            {
+                ["ServerBoost"] = {["BoostType"] = "Triple Damage"},
+                ["Amount"] = 3,
+                ["Name"] = "Hard-15",
+                ["IgnoreForFirstSelection"] = true
+            },
+            {
+                ["ServerBoost"] = {["BoostType"] = "Super Lucky"},
+                ["Amount"] = 3,
+                ["Name"] = "Hard-16",
+                ["IgnoreForFirstSelection"] = true
+            },
+            {
+                ["Name"] = "Hard-17",
+                ["Amount"] = 10,
+                ["TradingBooth"] = {["Sell"] = true},
+                ["IgnoreForFirstSelection"] = true
+            },
+            {["Fuse"] = {}, ["Name"] = "Hard-18", ["Amount"] = 10},
+            {["Name"] = "Hard-19", ["Coin"] = {}, ["Amount"] = 400},
+            {
+                ["Name"] = "Hard-20",
+                ["Coin"] = {["Pattern"] = {"Orange", "Apple", "Banana", "Pear", "Pineapple", "Rainbow Fruit"}},
+                ["Description"] = "Break {Amount} Fruit{s}!",
+                ["Amount"] = 10
+            },
+            {["Name"] = "Hard-21", ["Coin"] = {["Pattern"] = {"Pineapple"}}, ["Amount"] = 5},
+            {["Name"] = "Hard-22", ["Coin"] = {["Pattern"] = {"Rainbow Fruit"}}, ["Amount"] = 5}
+        },
+        ["Easy"] = {
+            {["Name"] = "Easy-1", ["Coin"] = {["Pattern"] = "Coin"}, ["Amount"] = 100},
+            {["Name"] = "Easy-2", ["Coin"] = {["Pattern"] = "Crate"}, ["Amount"] = 75},
+            {["Name"] = "Easy-3", ["Coin"] = {["Pattern"] = "Present"}, ["Amount"] = 75},
+            {["Name"] = "Easy-4", ["Coin"] = {["Pattern"] = "Chest"}, ["Amount"] = 40},
+            {
+                ["Name"] = "Easy-5",
+                ["Amount"] = 150,
+                ["Coin"] = {["Pattern"] = "Diamond"},
+                ["IgnoreForFirstSelection"] = true
+            },
+            {
+                ["Name"] = "Easy-6",
+                ["Amount"] = 50,
+                ["Coin"] = {["Pattern"] = "Safe"},
+                ["IgnoreForFirstSelection"] = true
+            },
+            {
+                ["Name"] = "Easy-7",
+                ["Amount"] = 50,
+                ["Coin"] = {["Pattern"] = "Vault"},
+                ["IgnoreForFirstSelection"] = true
+            },
+            {["Egg"] = {["World"] = "Spawn"}, ["Name"] = "Easy-8", ["Amount"] = 50},
+            {
+                ["Egg"] = {["World"] = "Fantasy"},
+                ["Amount"] = 50,
+                ["Name"] = "Easy-9",
+                ["IgnoreForFirstSelection"] = true
+            },
+            {["Egg"] = {["World"] = "Tech"}, ["Amount"] = 50, ["Name"] = "Easy-10", ["IgnoreForFirstSelection"] = true},
+            {
+                ["Egg"] = {["World"] = "Axolotl Ocean"},
+                ["Amount"] = 50,
+                ["Name"] = "Easy-11",
+                ["IgnoreForFirstSelection"] = true
+            },
+            {["Egg"] = {["World"] = "Pixel"}, ["Amount"] = 50, ["Name"] = "Easy-12", ["IgnoreForFirstSelection"] = true},
+            {["Egg"] = {["World"] = "Cat"}, ["Amount"] = 50, ["Name"] = "Easy-13", ["IgnoreForFirstSelection"] = true},
+            {
+                ["Egg"] = {["World"] = "Doodle"},
+                ["Amount"] = 50,
+                ["Name"] = "Easy-14",
+                ["IgnoreForFirstSelection"] = true
+            },
+            {
+                ["Egg"] = {["World"] = "Kawaii"},
+                ["Amount"] = 50,
+                ["Name"] = "Easy-15",
+                ["IgnoreForFirstSelection"] = true
+            },
+            {["Egg"] = {["World"] = "Dog"}, ["Amount"] = 50, ["Name"] = "Easy-16", ["IgnoreForFirstSelection"] = true},
+            {
+                ["Name"] = "Easy-17",
+                ["Amount"] = 5,
+                ["Daycare"] = {["ClaimRequired"] = false},
+                ["IgnoreForFirstSelection"] = true
+            },
+            {["Golden"] = {["SuccessRequired"] = true}, ["Name"] = "Easy-18", ["Amount"] = 20},
+            {["Rainbow"] = {["SuccessRequired"] = true}, ["Name"] = "Easy-19", ["Amount"] = 10},
+            {
+                ["Name"] = "Easy-20",
+                ["Amount"] = 1,
+                ["TradingBooth"] = {["Sell"] = true},
+                ["IgnoreForFirstSelection"] = true
+            },
+            {["Enchant"] = {}, ["Name"] = "Easy-21", ["Amount"] = 20},
+            {
+                ["Egg"] = {["Rarity"] = "Legendary"},
+                ["Amount"] = 20,
+                ["Name"] = "Easy-22",
+                ["IgnoreForFirstSelection"] = true
+            },
+            {["FreeGifts"] = {}, ["Name"] = "Easy-23", ["Amount"] = 1},
+            {["Trade"] = {}, ["Name"] = "Easy-24", ["Amount"] = 1},
+            {["Fuse"] = {}, ["Name"] = "Easy-25", ["Amount"] = 1},
+            {["Name"] = "Easy-26", ["Coin"] = {}, ["Amount"] = 100},
+            {
+                ["Name"] = "Easy-27",
+                ["Coin"] = {["Pattern"] = {"Orange", "Apple", "Banana", "Pear", "Pineapple", "Rainbow Fruit"}},
+                ["Description"] = "Break {Amount} Fruit{s}!",
+                ["Amount"] = 3
+            }
+        },
+        ["Medium"] = {
+            {["Golden"] = {["SuccessRequired"] = true}, ["Name"] = "Medium-1", ["Amount"] = 30},
+            {["Rainbow"] = {["SuccessRequired"] = true}, ["Name"] = "Medium-2", ["Amount"] = 10},
+            {["Enchant"] = {}, ["Name"] = "Medium-3", ["Amount"] = 100},
+            {["FreeGifts"] = {}, ["Name"] = "Medium-4", ["Amount"] = 2},
+            {["Trade"] = {}, ["Name"] = "Medium-5", ["Amount"] = 3},
+            {["Name"] = "Medium-6", ["Coin"] = {["Pattern"] = "Crate"}, ["Amount"] = 150},
+            {["Name"] = "Medium-7", ["Coin"] = {["Pattern"] = "Chest"}, ["Amount"] = 100},
+            {["Name"] = "Medium-8", ["Coin"] = {["Pattern"] = "Safe"}, ["Amount"] = 125},
+            {["Name"] = "Medium-9", ["Coin"] = {["Pattern"] = "Vault"}, ["Amount"] = 100},
+            {["Amount"] = 1, ["Merchant"] = {}, ["Name"] = "Medium-10", ["IgnoreForFirstSelection"] = true},
+            {
+                ["Name"] = "Medium-11",
+                ["Amount"] = 5,
+                ["TradingBooth"] = {["Sell"] = true},
+                ["IgnoreForFirstSelection"] = true
+            },
+            {
+                ["Egg"] = {["Rarity"] = "Legendary"},
+                ["Amount"] = 100,
+                ["Name"] = "Medium-12",
+                ["IgnoreForFirstSelection"] = true
+            },
+            {
+                ["Amount"] = 1,
+                ["DarkMatter"] = {["ClaimRequired"] = true},
+                ["Name"] = "Medium-13",
+                ["IgnoreForFirstSelection"] = true
+            },
+            {
+                ["ServerBoost"] = {["BoostType"] = "Triple Coins"},
+                ["Amount"] = 1,
+                ["Name"] = "Medium-14",
+                ["IgnoreForFirstSelection"] = true
+            },
+            {
+                ["ServerBoost"] = {["BoostType"] = "Triple Damage"},
+                ["Amount"] = 1,
+                ["Name"] = "Medium-15",
+                ["IgnoreForFirstSelection"] = true
+            },
+            {
+                ["ServerBoost"] = {["BoostType"] = "Super Lucky"},
+                ["Amount"] = 1,
+                ["Name"] = "Medium-16",
+                ["IgnoreForFirstSelection"] = true
+            },
+            {["Fuse"] = {}, ["Name"] = "Medium-17", ["Amount"] = 5},
+            {["Name"] = "Medium-18", ["Coin"] = {}, ["Amount"] = 200},
+            {
+                ["Name"] = "Medium-19",
+                ["Coin"] = {["Pattern"] = {"Orange", "Apple", "Banana", "Pear", "Pineapple", "Rainbow Fruit"}},
+                ["Description"] = "Break {Amount} Fruit{s}!",
+                ["Amount"] = 5
+            },
+            {["Name"] = "Medium-20", ["Coin"] = {["Pattern"] = {"Banana"}}, ["Amount"] = 3},
+            {["Name"] = "Medium-21", ["Coin"] = {["Pattern"] = {"Apple"}}, ["Amount"] = 3}
+        }
+    }
+}
