6b163efb52987b5272cb385113f7da70407d5eb5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 18:46:18 +1100

    Create Mastery.lua

---
+++ GameData/Pet Simulator X [6284583030]/Mastery.lua
@@ -0,0 +1,305 @@
+data = {
+    ["Mastery - Eggs"] = {
+        ["Eggs"] = {
+            ["enabled"] = true,
+            ["title"] = "Eggs",
+            ["perks"] = {
+                {["desc"] = "Better luck!", ["level"] = 15},
+                {["desc"] = [[<font color="#5bff3e">10%</font> cheaper eggs!]], ["level"] = 35},
+                {["desc"] = "Even better luck!", ["level"] = 50},
+                {["desc"] = [[<font color="#5bff3e">20%</font> cheaper eggs!]], ["level"] = 75},
+                {["desc"] = [[Chance to hatch <font color="#ffbd37">Golden Pets</font>!]], ["level"] = 99}
+            },
+            ["maxLvl"] = 99,
+            ["sort"] = 1,
+            ["icon"] = "rbxassetid://8990634927",
+            ["xpMult"] = 1
+        }
+    },
+    ["Mastery - Golden Eggs"] = {
+        ["Golden Eggs"] = {
+            ["enabled"] = true,
+            ["title"] = "Golden Eggs",
+            ["perks"] = {
+                {["desc"] = "Better luck!", ["level"] = 15},
+                {["desc"] = [[<font color="#5bff3e">5%</font> cheaper golden eggs!]], ["level"] = 35},
+                {["desc"] = "Even better luck!", ["level"] = 50},
+                {["desc"] = [[<font color="#5bff3e">15%</font> cheaper golden eggs!]], ["level"] = 75},
+                {["desc"] = [[Chance to hatch <font color="#ffbd37">Rainbow Pets</font>!]], ["level"] = 99}
+            },
+            ["maxLvl"] = 99,
+            ["sort"] = 1,
+            ["icon"] = "rbxassetid://8990635029",
+            ["xpMult"] = 1
+        }
+    },
+    ["Mastery - Boosts"] = {
+        ["Boosts"] = {
+            ["enabled"] = true,
+            ["title"] = "Boosts",
+            ["perks"] = {
+                {
+                    ["desc"] = [[<font color="#ff81f9">Triple Damage</font> boost lasts <font color="#5bff3e">25%</font> longer!]],
+                    ["level"] = 30
+                },
+                {
+                    ["desc"] = [[<font color="#ff81f9">Triple Coins</font> boost lasts <font color="#5bff3e">25%</font> longer!]],
+                    ["level"] = 50
+                },
+                {["desc"] = [[All boosts last <font color="#5bff3e">25%</font> longer!]], ["level"] = 75},
+                {["desc"] = [[All boosts last <font color="#5bff3e">50%</font> longer!]], ["level"] = 99}
+            },
+            ["maxLvl"] = 99,
+            ["sort"] = 5,
+            ["icon"] = "rbxassetid://8997529686",
+            ["xpMult"] = 1
+        }
+    },
+    ["Mastery - Coin Piles"] = {
+        ["Coin Piles"] = {
+            ["enabled"] = true,
+            ["title"] = "Coin Piles",
+            ["perks"] = {
+                {["desc"] = [[Coin Piles reward <font color="#5bff3e">25%</font> more!]], ["level"] = 15},
+                {["desc"] = [[Coin Piles break <font color="#5bff3e">50%</font> faster!]], ["level"] = 35},
+                {["desc"] = [[Coin Piles drop more <font color="#ffbd37">lootbags</font>!]], ["level"] = 50},
+                {["desc"] = [[Coin Piles reward <font color="#5bff3e">100%</font> more!]], ["level"] = 75},
+                {["desc"] = [[Coin Piles reward <font color="#5bff3e">200%</font> more!]], ["level"] = 99}
+            },
+            ["maxLvl"] = 99,
+            ["sort"] = 10,
+            ["icon"] = "rbxassetid://8990634337",
+            ["xpMult"] = 1
+        }
+    },
+    ["Mastery - Chests"] = {
+        ["Chests"] = {
+            ["enabled"] = true,
+            ["title"] = "Chests",
+            ["perks"] = {
+                {["desc"] = [[Chests reward <font color="#5bff3e">15%</font> more!]], ["level"] = 15},
+                {["desc"] = [[Chests break <font color="#5bff3e">25%</font> faster!]], ["level"] = 35},
+                {["desc"] = [[Chests drop more <font color="#ffbd37">lootbags</font>!]], ["level"] = 50},
+                {["desc"] = [[Chests reward <font color="#5bff3e">50%</font> more!]], ["level"] = 75},
+                {["desc"] = [[Chests reward <font color="#5bff3e">100%</font> more!]], ["level"] = 99}
+            },
+            ["maxLvl"] = 99,
+            ["sort"] = 14,
+            ["icon"] = "rbxassetid://8990634176",
+            ["xpMult"] = 1
+        }
+    },
+    ["Mastery - Presents"] = {
+        ["Presents"] = {
+            ["enabled"] = true,
+            ["title"] = "Presents",
+            ["perks"] = {
+                {["desc"] = [[Presents reward <font color="#5bff3e">15%</font> more!]], ["level"] = 15},
+                {["desc"] = [[Presents break <font color="#5bff3e">35%</font> faster!]], ["level"] = 35},
+                {["desc"] = [[Presents drop more <font color="#ffbd37">lootbags</font>!]], ["level"] = 50},
+                {["desc"] = [[Presents reward <font color="#5bff3e">60%</font> more!]], ["level"] = 75},
+                {["desc"] = [[Presents reward <font color="#5bff3e">100%</font> more!]], ["level"] = 99}
+            },
+            ["maxLvl"] = 99,
+            ["sort"] = 12,
+            ["icon"] = "rbxassetid://8993525554",
+            ["xpMult"] = 1
+        }
+    },
+    ["Mastery - Crates"] = {
+        ["Crates"] = {
+            ["enabled"] = true,
+            ["title"] = "Crates",
+            ["perks"] = {
+                {["desc"] = [[Crates reward <font color="#5bff3e">25%</font> more!]], ["level"] = 15},
+                {["desc"] = [[Crates break <font color="#5bff3e">40%</font> faster!]], ["level"] = 35},
+                {["desc"] = [[Crates drop more <font color="#ffbd37">lootbags</font>!]], ["level"] = 50},
+                {["desc"] = [[Crates reward <font color="#5bff3e">75%</font> more!]], ["level"] = 75},
+                {["desc"] = [[Crates reward <font color="#5bff3e">125%</font> more!]], ["level"] = 99}
+            },
+            ["maxLvl"] = 99,
+            ["sort"] = 11,
+            ["icon"] = "rbxassetid://8990639413",
+            ["xpMult"] = 1
+        }
+    },
+    ["Mastery - Vaults & Safes"] = {
+        ["VaultsAndSafes"] = {
+            ["enabled"] = true,
+            ["title"] = "Vaults & Safes",
+            ["perks"] = {
+                {["desc"] = [[Vaults & Safes reward <font color="#5bff3e">15%</font> more!]], ["level"] = 15},
+                {["desc"] = [[Vaults & Safes break <font color="#5bff3e">35%</font> faster!]], ["level"] = 35},
+                {["desc"] = [[Vaults & Safes drop more <font color="#ffbd37">lootbags</font>!]], ["level"] = 50},
+                {["desc"] = [[Vaults & Safes reward <font color="#5bff3e">60%</font> more!]], ["level"] = 75},
+                {["desc"] = [[Vaults & Safes reward <font color="#5bff3e">100%</font> more!]], ["level"] = 99}
+            },
+            ["maxLvl"] = 99,
+            ["sort"] = 13,
+            ["icon"] = "rbxassetid://8990634777",
+            ["xpMult"] = 1
+        }
+    },
+    ["Mastery - Lootbags"] = {
+        ["Lootbags"] = {
+            ["enabled"] = true,
+            ["title"] = "Lootbags",
+            ["perks"] = {
+                {["desc"] = [[Lootbags reward <font color="#5bff3e">25%</font> more coins!]], ["level"] = 15},
+                {["desc"] = "Better lootbags appear more often!", ["level"] = 35},
+                {["desc"] = [[Lootbags reward <font color="#5bff3e">35%</font> more diamonds!]], ["level"] = 50},
+                {["desc"] = [[Unlock <font color="#ffbd37">Mini Gifts</font>!]], ["level"] = 75},
+                {["desc"] = [[Chance to drop <font color="#ff81f9">double</font> lootbags!]], ["level"] = 99}
+            },
+            ["maxLvl"] = 99,
+            ["sort"] = 7,
+            ["icon"] = "rbxassetid://8990635175",
+            ["xpMult"] = 1
+        }
+    },
+    ["Mastery - Diamond Piles"] = {
+        ["Diamond Piles"] = {
+            ["enabled"] = true,
+            ["title"] = "Diamond Piles",
+            ["perks"] = {
+                {["desc"] = [[Diamond Piles reward <font color="#5bff3e">20%</font> more!]], ["level"] = 25},
+                {["desc"] = [[Diamond Piles break <font color="#5bff3e">50%</font> faster!]], ["level"] = 50},
+                {["desc"] = [[Diamond Piles reward <font color="#5bff3e">100%</font> more!]], ["level"] = 75},
+                {["desc"] = [[Diamond Piles reward <font color="#5bff3e">200%</font> more!]], ["level"] = 99}
+            },
+            ["maxLvl"] = 99,
+            ["sort"] = 15,
+            ["icon"] = "rbxassetid://8990639527",
+            ["xpMult"] = 1
+        }
+    },
+    ["Mastery - Free Gifts"] = {
+        ["Free Gifts"] = {
+            ["enabled"] = true,
+            ["title"] = "Free Gifts",
+            ["perks"] = {
+                {
+                    ["desc"] = [[Free Gifts have very small extra luck to hatch a <font color="#ffbd37">Huge Pet</font>!]],
+                    ["level"] = 30
+                },
+                {
+                    ["desc"] = [[Free Gifts have a little more luck to hatch a <font color="#ffbd37">Huge Pet</font>!]],
+                    ["level"] = 75
+                },
+                {
+                    ["desc"] = [[Free Gifts have even better luck to hatch a <font color="#ffbd37">Huge Pet</font>!]],
+                    ["level"] = 99
+                }
+            },
+            ["maxLvl"] = 99,
+            ["sort"] = 6,
+            ["icon"] = "rbxassetid://9003556008",
+            ["xpMult"] = 2
+        }
+    },
+    ["Mastery - Dark Matter"] = {
+        ["Dark Matter"] = {
+            ["enabled"] = true,
+            ["title"] = "Dark Matter",
+            ["perks"] = {
+                {["desc"] = [[Conversion time is <font color="#5bff3e">5%</font> faster!]], ["level"] = 30},
+                {["desc"] = [[Conversion time is <font color="#5bff3e">10%</font> faster!]], ["level"] = 75},
+                {["desc"] = [[Conversion time is <font color="#5bff3e">20%</font> faster!]], ["level"] = 99}
+            },
+            ["maxLvl"] = 99,
+            ["sort"] = 44,
+            ["icon"] = "rbxassetid://9003568230",
+            ["xpMult"] = 2
+        }
+    },
+    ["Mastery - Fusing"] = {
+        ["Fusing"] = {
+            ["enabled"] = true,
+            ["title"] = "Fusing",
+            ["perks"] = {
+                {["desc"] = [[Fusing has <font color="#5bff3e">0.2%</font> chance to make a shiny!]], ["level"] = 30},
+                {["desc"] = [[Fusing has <font color="#ffbd37">0.5%</font> chance to make a shiny!]], ["level"] = 75},
+                {["desc"] = [[Fusing has <font color="#ff81f9">1%</font> chance to make a shiny!]], ["level"] = 99}
+            },
+            ["maxLvl"] = 99,
+            ["sort"] = 41,
+            ["icon"] = "rbxassetid://9003571910",
+            ["xpMult"] = 2
+        }
+    },
+    ["Mastery - Converting"] = {
+        ["Converting"] = {
+            ["enabled"] = true,
+            ["title"] = "Converting",
+            ["perks"] = {{["desc"] = [[Unlock <font color="#ffbd37">Steampunk Hoverboard</font>!]], ["level"] = 99}},
+            ["maxLvl"] = 99,
+            ["sort"] = 40,
+            ["icon"] = "rbxassetid://9003575074",
+            ["xpMult"] = 1
+        }
+    },
+    ["Mastery - Enchanting"] = {
+        ["Enchanting"] = {
+            ["enabled"] = true,
+            ["title"] = "Enchanting",
+            ["perks"] = {
+                {["desc"] = [[<font color="#ff81f9">Unique</font> Enchantments are more common!]], ["level"] = 35},
+                {["desc"] = [[<font color="#ff81f9">Unique</font> Enchantments are even more common!]], ["level"] = 75},
+                {["desc"] = [[Enchantments cost <font color="#5bff3e">25%</font> less!]], ["level"] = 99}
+            },
+            ["maxLvl"] = 99,
+            ["sort"] = 43,
+            ["icon"] = "rbxassetid://9003576350",
+            ["xpMult"] = 1
+        }
+    },
+    ["Mastery - Diamond Mine"] = {
+        ["Diamond Mine"] = {
+            ["enabled"] = true,
+            ["title"] = "Diamond Mine",
+            ["perks"] = {
+                {["desc"] = [[<font color="#5bff3e">+20%</font> Diamond Mine drops!]], ["level"] = 15},
+                {["desc"] = [[<font color="#5bff3e">+40%</font> Diamond Mine pet damage!]], ["level"] = 35},
+                {["desc"] = [[<font color="#5bff3e">+40%</font> Diamond Mine drops!]], ["level"] = 50},
+                {["desc"] = [[<font color="#ffbd37">+100%</font> Diamond Mine pet damage!]], ["level"] = 75},
+                {["desc"] = [[<font color="#ff81f9">+80%</font> Diamond Mine drops!]], ["level"] = 99}
+            },
+            ["maxLvl"] = 99,
+            ["sort"] = -1,
+            ["icon"] = "rbxassetid://13210223917",
+            ["xpMult"] = 1
+        }
+    },
+    ["Mastery - Daycare"] = {
+        ["Daycare"] = {
+            ["enabled"] = true,
+            ["title"] = "Daycare",
+            ["perks"] = {
+                {["desc"] = [[Daycare earns <font color="#5bff3e">10%</font> more coins!]], ["level"] = 15},
+                {["desc"] = [[Daycare earns <font color="#5bff3e">25%</font> more boosters!]], ["level"] = 35},
+                {["desc"] = [[Daycare earns <font color="#ffbd37">35%</font> more coins!]], ["level"] = 50},
+                {["desc"] = [[Daycare earns <font color="#ffbd37">50%</font> more diamonds!]], ["level"] = 75},
+                {["desc"] = [[Daycare has better luck to get a <font color="#ff81f9">Huge Pet</font>!]], ["level"] = 99}
+            },
+            ["maxLvl"] = 99,
+            ["sort"] = -1,
+            ["icon"] = "rbxassetid://13210223994",
+            ["xpMult"] = 1
+        }
+    },
+    ["Mastery - Fruits"] = {
+        ["Fruits"] = {
+            ["enabled"] = true,
+            ["title"] = "Fruits",
+            ["perks"] = {
+                {["desc"] = [[Fruits last <font color="#5bff3e">20%</font> longer!]], ["level"] = 35},
+                {["desc"] = [[Fruits last <font color="#5bff3e">50%</font> longer!]], ["level"] = 75},
+                {["desc"] = [[Fruits give <font color="#5bff3e">30%</font> more multipliers!]], ["level"] = 99}
+            },
+            ["maxLvl"] = 99,
+            ["sort"] = -1,
+            ["icon"] = "rbxassetid://13280523436",
+            ["xpMult"] = 1
+        }
+    }
+}
