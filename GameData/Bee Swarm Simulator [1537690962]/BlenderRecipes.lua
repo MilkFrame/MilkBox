c4f369d8a2a2da56e76f36cf593901f5bd152622
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 19:12:45 +1100

    Create BlenderRecipes.lua

---
+++ GameData/Bee Swarm Simulator [1537690962]/BlenderRecipes.lua
@@ -0,0 +1,205 @@
+data = {
+    ["HardWax"] = {
+        ["Requirements"] = {},
+        ["Ingredients"] = {
+            {["Type"] = "SoftWax", ["Amount"] = 3, ["Category"] = "Eggs"},
+            {["Type"] = "Enzymes", ["Amount"] = 3, ["Category"] = "Eggs"},
+            {["Type"] = "Bitterberry", ["Amount"] = 33, ["Category"] = "Eggs"},
+            {["Type"] = "RoyalJelly", ["Amount"] = 33, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["Glitter"] = {
+        ["Requirements"] = {{["Type"] = "Earn Badges", ["Amount"] = 3, ["Set"] = "Pineapple"}},
+        ["Ingredients"] = {
+            {["Type"] = "MoonCharm", ["Amount"] = 25, ["Category"] = "Eggs"},
+            {["Type"] = "MagicBean", ["Amount"] = 1, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["Oil"] = {
+        ["Requirements"] = {{["Type"] = "Earn Badges", ["Amount"] = 3, ["Set"] = "Ability"}},
+        ["Ingredients"] = {
+            {["Type"] = "SunflowerSeed", ["Amount"] = 50, ["Category"] = "Eggs"},
+            {["Type"] = "RoyalJelly", ["Amount"] = 10, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["Gumdrops"] = {
+        ["Requirements"] = {{["Type"] = "Earn Badges", ["Amount"] = 3, ["Set"] = "Pineapple"}},
+        ["Ingredients"] = {
+            {["Type"] = "Blueberry", ["Amount"] = 3, ["Category"] = "Eggs"},
+            {["Type"] = "Strawberry", ["Amount"] = 3, ["Category"] = "Eggs"},
+            {["Type"] = "Pineapple", ["Amount"] = 3, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 10
+    },
+    ["FieldDice"] = {
+        ["Requirements"] = {},
+        ["Ingredients"] = {
+            {["Type"] = "SoftWax", ["Amount"] = 1, ["Category"] = "Eggs"},
+            {["Type"] = "Whirligig", ["Amount"] = 1, ["Category"] = "Eggs"},
+            {["Type"] = "RedExtract", ["Amount"] = 1, ["Category"] = "Eggs"},
+            {["Type"] = "BlueExtract", ["Amount"] = 1, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["Turpentine"] = {
+        ["Requirements"] = {},
+        ["Ingredients"] = {
+            {["Type"] = "SuperSmoothie", ["Amount"] = 10, ["Category"] = "Eggs"},
+            {["Type"] = "CausticWax", ["Amount"] = 10, ["Category"] = "Eggs"},
+            {["Type"] = "StarJelly", ["Amount"] = 100, ["Category"] = "Eggs"},
+            {["Type"] = "Honeysuckle", ["Amount"] = 1000, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["SuperSmoothie"] = {
+        ["Requirements"] = {},
+        ["Ingredients"] = {
+            {["Type"] = "Neonberry", ["Amount"] = 3, ["Category"] = "Eggs"},
+            {["Type"] = "StarJelly", ["Amount"] = 3, ["Category"] = "Eggs"},
+            {["Type"] = "PurplePotion", ["Amount"] = 3, ["Category"] = "Eggs"},
+            {["Type"] = "TropicalDrink", ["Amount"] = 6, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["LoadedDice"] = {
+        ["Requirements"] = {},
+        ["Ingredients"] = {
+            {["Type"] = "SmoothDice", ["Amount"] = 3, ["Category"] = "Eggs"},
+            {["Type"] = "HardWax", ["Amount"] = 3, ["Category"] = "Eggs"},
+            {["Type"] = "Oil", ["Amount"] = 3, ["Category"] = "Eggs"},
+            {["Type"] = "Glue", ["Amount"] = 1, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["BlueExtract"] = {
+        ["Requirements"] = {{["Type"] = "Earn Badges", ["Amount"] = 3, ["Set"] = "Blue Flower"}},
+        ["Ingredients"] = {
+            {["Type"] = "Blueberry", ["Amount"] = 50, ["Category"] = "Eggs"},
+            {["Type"] = "RoyalJelly", ["Amount"] = 10, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["SmoothDice"] = {
+        ["Requirements"] = {},
+        ["Ingredients"] = {
+            {["Type"] = "FieldDice", ["Amount"] = 3, ["Category"] = "Eggs"},
+            {["Type"] = "SoftWax", ["Amount"] = 3, ["Category"] = "Eggs"},
+            {["Type"] = "Whirligig", ["Amount"] = 3, ["Category"] = "Eggs"},
+            {["Type"] = "Oil", ["Amount"] = 3, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["StarJelly"] = {
+        ["Requirements"] = {{["Type"] = "Earn Badges", ["Amount"] = 5, ["Set"] = "Clover"}},
+        ["Ingredients"] = {
+            {["Type"] = "RoyalJelly", ["Amount"] = 100, ["Category"] = "Eggs"},
+            {["Type"] = "Glitter", ["Amount"] = 3, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["Glue"] = {
+        ["Requirements"] = {{["Type"] = "Earn Badges", ["Amount"] = 3, ["Set"] = "Goo"}},
+        ["Ingredients"] = {
+            {["Type"] = "Gumdrops", ["Amount"] = 50, ["Category"] = "Eggs"},
+            {["Type"] = "RoyalJelly", ["Amount"] = 10, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["RedExtract"] = {
+        ["Requirements"] = {{["Type"] = "Earn Badges", ["Amount"] = 3, ["Set"] = "Mushroom"}},
+        ["Ingredients"] = {
+            {["Type"] = "Strawberry", ["Amount"] = 50, ["Category"] = "Eggs"},
+            {["Type"] = "RoyalJelly", ["Amount"] = 10, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["PurplePotion"] = {
+        ["Requirements"] = {},
+        ["Ingredients"] = {
+            {["Type"] = "Neonberry", ["Amount"] = 3, ["Category"] = "Eggs"},
+            {["Type"] = "RedExtract", ["Amount"] = 3, ["Category"] = "Eggs"},
+            {["Type"] = "BlueExtract", ["Amount"] = 3, ["Category"] = "Eggs"},
+            {["Type"] = "Glue", ["Amount"] = 3, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["CausticWax"] = {
+        ["Requirements"] = {},
+        ["Ingredients"] = {
+            {["Type"] = "HardWax", ["Amount"] = 5, ["Category"] = "Eggs"},
+            {["Type"] = "Enzymes", ["Amount"] = 5, ["Category"] = "Eggs"},
+            {["Type"] = "Neonberry", ["Amount"] = 25, ["Category"] = "Eggs"},
+            {["Type"] = "RoyalJelly", ["Amount"] = 5252, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["TropicalDrink"] = {
+        ["Requirements"] = {},
+        ["Ingredients"] = {
+            {["Type"] = "Coconut", ["Amount"] = 10, ["Category"] = "Eggs"},
+            {["Type"] = "Enzymes", ["Amount"] = 2, ["Category"] = "Eggs"},
+            {["Type"] = "Oil", ["Amount"] = 2, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["SwirledWax"] = {
+        ["Requirements"] = {},
+        ["Ingredients"] = {
+            {["Type"] = "HardWax", ["Amount"] = 3, ["Category"] = "Eggs"},
+            {["Type"] = "SoftWax", ["Amount"] = 9, ["Category"] = "Eggs"},
+            {["Type"] = "PurplePotion", ["Amount"] = 6, ["Category"] = "Eggs"},
+            {["Type"] = "RoyalJelly", ["Amount"] = 3333, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["SoftWax"] = {
+        ["Requirements"] = {},
+        ["Ingredients"] = {
+            {["Type"] = "Honeysuckle", ["Amount"] = 5, ["Category"] = "Eggs"},
+            {["Type"] = "Oil", ["Amount"] = 1, ["Category"] = "Eggs"},
+            {["Type"] = "Enzymes", ["Amount"] = 1, ["Category"] = "Eggs"},
+            {["Type"] = "RoyalJelly", ["Amount"] = 10, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["Enzymes"] = {
+        ["Requirements"] = {{["Type"] = "Earn Badges", ["Amount"] = 3, ["Set"] = "Battle"}},
+        ["Ingredients"] = {
+            {["Type"] = "Pineapple", ["Amount"] = 50, ["Category"] = "Eggs"},
+            {["Type"] = "RoyalJelly", ["Amount"] = 10, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 1
+    },
+    ["MoonCharm"] = {
+        ["Requirements"] = {{["Type"] = "Earn Badges", ["Amount"] = 3, ["Set"] = "Pineapple"}},
+        ["Ingredients"] = {
+            {["Type"] = "Pineapple", ["Amount"] = 5, ["Category"] = "Eggs"},
+            {["Type"] = "Gumdrops", ["Amount"] = 5, ["Category"] = "Eggs"},
+            {["Type"] = "RoyalJelly", ["Amount"] = 1, ["Category"] = "Eggs"}
+        },
+        ["Time"] = 300,
+        ["AutoCompletePerTicket"] = 10
+    }
+}
