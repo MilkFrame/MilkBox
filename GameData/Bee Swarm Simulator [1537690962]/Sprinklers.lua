774abd3600e6f11a3c88eb6a99d7b9eb80c32359
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 19:24:33 +1100

    Create Sprinklers.lua

---
+++ GameData/Bee Swarm Simulator [1537690962]/Sprinklers.lua
@@ -0,0 +1,92 @@
+data = {
+    ["Silver Soakers"] = {
+        ["Radius"] = 7,
+        ["Description"] = [[Two sprinklers which fire faster!
+
+Count: 2
+Radius: 7
+Power: 7
+Rate: 4.5 Seconds
+Setup: 5 Seconds]],
+        ["Power"] = 7,
+        ["Requirements"] = {{["Type"] = "Earn Badges", ["Tier"] = 2, ["Amount"] = 10}},
+        ["Rate"] = 4.5,
+        ["Count"] = 2,
+        ["Model"] = "Silver Soakers",
+        ["Name"] = "Silver Soakers",
+        ["ActivationDelay"] = 5,
+        ["Cost"] = 22222222
+    },
+    ["The Supreme Saturator"] = {
+        ["Radius"] = 16,
+        ["Description"] = [[The ultimate sprinkler. Nobody knows how it works or where it came from.
+
+Count: 1
+Radius: 16
+Power: 10
+Rate: 1 Seconds
+Setup: 3 Seconds]],
+        ["Power"] = 10,
+        ["Requirements"] = {{["Type"] = "Earn Badges", ["Tier"] = 5, ["Amount"] = 10}},
+        ["Rate"] = 1,
+        ["Count"] = 1,
+        ["Model"] = "The Supreme Saturator",
+        ["Name"] = "The Supreme Saturator",
+        ["ActivationDelay"] = 3,
+        ["Cost"] = 55555555555
+    },
+    ["Golden Gushers"] = {
+        ["Radius"] = 8,
+        ["Description"] = [[Three gorgeous sprinklers with enormous range.
+
+Count: 3
+Radius: 8
+Power: 8
+Rate: 4.5 Seconds
+Setup: 5 Seconds]],
+        ["Power"] = 8,
+        ["Requirements"] = {{["Type"] = "Earn Badges", ["Tier"] = 3, ["Amount"] = 10}},
+        ["Rate"] = 4.5,
+        ["Count"] = 3,
+        ["Model"] = "Golden Gushers",
+        ["Name"] = "Golden Gushers",
+        ["ActivationDelay"] = 5,
+        ["Cost"] = 333333333
+    },
+    ["Basic Sprinkler"] = {
+        ["Radius"] = 7,
+        ["Description"] = [[When planted in the ground, causes nearby flowers to regrow faster!
+
+Count: 1
+Radius: 7
+Power: 7
+Rate: 5 Seconds
+Setup: 6 Seconds]],
+        ["Power"] = 7,
+        ["Requirements"] = {{["Type"] = "Earn Badges", ["Tier"] = 1, ["Amount"] = 10}},
+        ["Rate"] = 5,
+        ["Count"] = 1,
+        ["Model"] = "Basic Sprinkler",
+        ["Name"] = "Basic Sprinkler",
+        ["ActivationDelay"] = 6,
+        ["Cost"] = 1111111
+    },
+    ["Diamond Drenchers"] = {
+        ["Radius"] = 8,
+        ["Description"] = [[Four enhanced sprinklers made of durable artificial diamond!
+
+Count: 4
+Radius: 8
+Power: 9
+Rate: 4 Seconds
+Setup: 4 Seconds]],
+        ["Power"] = 9,
+        ["Requirements"] = {{["Type"] = "Earn Badges", ["Tier"] = 4, ["Amount"] = 10}},
+        ["Rate"] = 4,
+        ["Count"] = 4,
+        ["Model"] = "Diamond Drenchers",
+        ["Name"] = "Diamond Drenchers",
+        ["ActivationDelay"] = 4,
+        ["Cost"] = 4444444444
+    }
+}
