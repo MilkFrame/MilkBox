f18eca3dd92ceb6730cc5753713d79c50427e223
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 18:45:12 +1100

    Create Gamepasses.lua

---
+++ GameData/Pet Simulator X [6284583030]/Gamepasses.lua
@@ -0,0 +1,17 @@
+data = {
+    ["Lucky"] = {["ID"] = 21583760},
+    ["Secret Hunter"] = {["ID"] = 122535467},
+    ["Teleport"] = {["ID"] = 18674296},
+    ["Mythical Hunter"] = {["ID"] = 21641016},
+    ["Shiny Hunter"] = {["ID"] = 109685917},
+    ["Hoverboard"] = {["ID"] = 18674298},
+    ["Magnet"] = {["ID"] = 26398305},
+    ["Triple Egg Open"] = {["ID"] = 18674307},
+    ["VIP"] = {["ID"] = 18674305},
+    ["Magic Eggs"] = {["ID"] = 22596039},
+    ["Auto Hatch"] = {["ID"] = 21176989},
+    ["Pet Storage"] = {["ID"] = 18674317},
+    ["Skip Egg Open"] = {["ID"] = 18674321},
+    ["Super Pet Storage"] = {["ID"] = 18829757},
+    ["8 Pets Equipped"] = {["ID"] = 18674288}
+}
