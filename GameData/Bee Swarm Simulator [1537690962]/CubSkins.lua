43de7d04d2865352d0f29ea51ce617d860b3de86
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 18:59:06 +1100

    Create CubSkins.lua

---
+++ GameData/Bee Swarm Simulator [1537690962]/CubSkins.lua
@@ -0,0 +1,17 @@
+data = {{["Description"]=[[A precious baby Black Bear!
+
+This is the default skin given to all Cub Buddy owners.]],["Image"]="http://www.roblox.com/asset/?id=4519210639",["Name"]="Black",["DescTextScaled"]=false},{["Description"]=[[A slightly chubby cub that got into the Royal Jelly.
+
+Rewarded after completing 300 Brown Bear Quests!]],["Image"]="http://www.roblox.com/asset/?id=4972076494",["Name"]="Brown",["DescTextScaled"]=false},{["Description"]=[[They forgot a few drives when constructing this Robo Bear.
+
+Rewarded after beating round 25 of Robo Bear's Challenge or collecting 250,000 Cogs.]],["Image"]="rbxassetid://12854797283",["Name"]="Robo",["DescTextScaled"]=true},{["Image"]="http://www.roblox.com/asset/?id=4519210980",["Name"]="Noob",["DisplayTag"]="Limited Edition",["Description"]=[[This little noob doesn't know the first thing about running a shop.
+
+This was included in the Cub Buddy Launch Pack.]],["DescTextScaled"]=true},{["Image"]="http://www.roblox.com/asset/?id=4519211194",["Name"]="Bee",["DisplayTag"]="Limited Edition",["Description"]=[[A young polar bear going through an identity crisis.
+
+ This was the final reward of Bee Bear's 2019 quests.]],["DescTextScaled"]=false},{["Image"]="rbxassetid://6128761647",["Name"]="Gingerbread",["DisplayTag"]="Limited Edition",["Description"]=[[A cub made of cookies, brought to life by Honeyday Magic!
+
+ Purchased in Bee Bear's 2020 Catalog.]],["DescTextScaled"]=false},{["Image"]="rbxassetid://8930099751",["Name"]="Snow",["DisplayTag"]="Limited Edition",["Description"]=[[This cruel cub was carved from a Lvl 20 Snowbear's snowball.
+
+This was the final reward in Bee Bear's March 2022 quests.]],["DescTextScaled"]=false},{["Image"]="rbxassetid://12854798207",["Name"]="Peppermint Robo",["DisplayTag"]="Limited Edition",["Description"]=[[A clockwork cub composed of cogs and candycanes.
+
+This was the final reward in Bee Bear's April 2023 quests.]],["DescTextScaled"]=false}}
