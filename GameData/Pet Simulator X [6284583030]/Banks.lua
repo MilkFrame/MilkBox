012e5250b7eb634a914d12dfe8ca2fc9c8d4d3c0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 18:41:46 +1100

    Create Banks.lua

---
+++ GameData/Pet Simulator X [6284583030]/Banks.lua
@@ -0,0 +1,82 @@
+data = {
+    {
+        ["Cost"] = 7500000,
+        ["MaxDiamonds"] = 10000000000,
+        ["Title"] = "Starter Account",
+        ["Tier"] = 1,
+        ["ProductId"] = 0,
+        ["Interest"] = 0,
+        ["PetSlots"] = 40,
+        ["MaxPlayers"] = 1
+    },
+    {
+        ["Cost"] = 40000000,
+        ["MaxDiamonds"] = 50000000000,
+        ["Title"] = "Basic Account",
+        ["Tier"] = 2,
+        ["ProductId"] = 1228140504,
+        ["Interest"] = 0.1,
+        ["PetSlots"] = 95,
+        ["MaxPlayers"] = 2
+    },
+    {
+        ["Cost"] = 200000000,
+        ["MaxDiamonds"] = 300000000000,
+        ["Title"] = "Plus Account",
+        ["Tier"] = 3,
+        ["ProductId"] = 1228140516,
+        ["Interest"] = 0.175,
+        ["PetSlots"] = 200,
+        ["MaxPlayers"] = 3
+    },
+    {
+        ["Cost"] = 800000000,
+        ["MaxDiamonds"] = 1500000000000,
+        ["Title"] = "Deluxe Account",
+        ["Tier"] = 4,
+        ["ProductId"] = 1228140531,
+        ["Interest"] = 0.25,
+        ["PetSlots"] = 500,
+        ["MaxPlayers"] = 4
+    },
+    {
+        ["Cost"] = 3500000000,
+        ["MaxDiamonds"] = 5000000000000,
+        ["Title"] = "Premier Account",
+        ["Tier"] = 5,
+        ["ProductId"] = 1228140544,
+        ["Interest"] = 0.4,
+        ["PetSlots"] = 950,
+        ["MaxPlayers"] = 5
+    },
+    {
+        ["Cost"] = 15000000000,
+        ["MaxDiamonds"] = 10000000000000,
+        ["Title"] = "Gold Account",
+        ["Tier"] = 6,
+        ["ProductId"] = 1228140560,
+        ["Interest"] = 0.55,
+        ["PetSlots"] = 1400,
+        ["MaxPlayers"] = 5
+    },
+    {
+        ["Cost"] = 50000000000,
+        ["MaxDiamonds"] = 25000000000000,
+        ["Title"] = "Diamond Account",
+        ["Tier"] = 7,
+        ["ProductId"] = 1228140571,
+        ["Interest"] = 0.7,
+        ["PetSlots"] = 2500,
+        ["MaxPlayers"] = 6
+    },
+    {
+        ["Cost"] = 125000000000,
+        ["MaxDiamonds"] = 100000000000000,
+        ["Title"] = "Sapphire Account",
+        ["Tier"] = 8,
+        ["ProductId"] = 0,
+        ["Interest"] = 0.85,
+        ["PetSlots"] = 3500,
+        ["MaxPlayers"] = 6
+    }
+}
