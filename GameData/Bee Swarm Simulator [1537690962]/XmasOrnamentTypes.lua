22594287a8eddaacb8b9abf4efbf45982fc6f99b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 19:25:09 +1100

    Create XmasOrnamentTypes.lua

---
+++ GameData/Bee Swarm Simulator [1537690962]/XmasOrnamentTypes.lua
@@ -0,0 +1,407 @@
+data = {
+    ["Conical Flask"] = {
+        ["Name"] = "Conical Flask",
+        ["Icon"] = "http://www.roblox.com/asset/?id=4520882363",
+        ["Source"] = "Science Bear",
+        ["Slot"] = 5,
+        ["ID"] = 5,
+        ["Buffs"] = {
+            {["Value"] = 0.25, ["Type"] = "BaseProductionRate", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.25,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Zone"] = "Pineapple Patch"}
+            },
+            {["Value"] = 0.25, ["Type"] = "ConversionAtHive", ["RBCDisable"] = true, ["Op"] = "Add"}
+        },
+        ["Quest"] = "Science Bear's Ornament",
+        ["Desc"] = "A small replica of an Erlenmeyer flask - a glass vessel used in precise laboratory experiments."
+    },
+    ["Stuffed Heart"] = {
+        ["Name"] = "Stuffed Heart",
+        ["Icon"] = "http://www.roblox.com/asset/?id=4520882231",
+        ["Source"] = "Mother Bear",
+        ["Slot"] = 2,
+        ["ID"] = 2,
+        ["Buffs"] = {
+            {["Value"] = 0.25, ["Type"] = "BaseProductionRate", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.25,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Zone"] = "Strawberry Field"}
+            },
+            {["Value"] = 0.1, ["Type"] = "BeePollenBonus", ["RBCDisable"] = true, ["Op"] = "Add"}
+        },
+        ["Quest"] = "Mother Bear's Ornament",
+        ["Desc"] = "A silky heart with lacy frills, representing the bond between you and your bees."
+    },
+    ["Bread Climp"] = {
+        ["Source"] = "Bubble Bee Man",
+        ["Slot"] = 16,
+        ["SourceHidden"] = true,
+        ["Desc"] = "A small device used to keep plastic bread bags closed.",
+        ["OrnamentSuffix"] = "",
+        ["Name"] = "Bread Climp",
+        ["ID"] = 15,
+        ["Buffs"] = {
+            {["Value"] = 0.1, ["Type"] = "ContainerSpaceMultiplier", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.1,
+                ["RBCDisable"] = true,
+                ["Type"] = "PollenBonus",
+                ["Params"] = {["Tag"] = "Bubble"}
+            },
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.1,
+                ["RBCDisable"] = true,
+                ["Type"] = "PollenConversion",
+                ["Params"] = {["Tag"] = "Bee Gather"}
+            },
+            {["Value"] = 0.05, ["Type"] = "SuperCritPower", ["RBCDisable"] = true, ["Op"] = "Add"}
+        },
+        ["Quest"] = "B.B.M.'s Ornament",
+        ["Icon"] = "http://www.roblox.com/asset/?id=4557591762"
+    },
+    ["Memory Match"] = {
+        ["Name"] = "Memory Match",
+        ["Icon"] = "http://www.roblox.com/asset/?id=4520882387",
+        ["Source"] = "Spirit Bear",
+        ["Slot"] = 9,
+        ["ID"] = 9,
+        ["Buffs"] = {
+            {["Value"] = 0.25, ["Type"] = "BaseProductionRate", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.25,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Zone"] = "Stump Field"}
+            },
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.05,
+                ["RBCDisable"] = true,
+                ["Type"] = "PollenBonus",
+                ["Params"] = {["Tag"] = "Bee Ability"}
+            }
+        },
+        ["Quest"] = "Spirit Bear's Ornament",
+        ["Desc"] = "A glass ornament given to the top 100 Memory Match players in 1983."
+    },
+    ["Gummy Globe"] = {
+        ["Source"] = "Gummy Bear",
+        ["Slot"] = 15,
+        ["SourceHidden"] = true,
+        ["Desc"] = "A figurine of the ideal planet covered entirely in goo.",
+        ["OrnamentSuffix"] = "",
+        ["Name"] = "Gummy Globe",
+        ["ID"] = 14,
+        ["Buffs"] = {
+            {["Value"] = 0.1, ["Type"] = "ContainerSpaceMultiplier", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.25,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Zone"] = "Coconut Field"}
+            },
+            {["Value"] = 0.1, ["Type"] = "GooAmount", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {["Value"] = 0.01, ["Type"] = "SuperCritChance", ["RBCDisable"] = true, ["Op"] = "Add"}
+        },
+        ["Quest"] = "GummyBearXmas21",
+        ["Icon"] = "rbxassetid://6318423823"
+    },
+    ["Electric Candle"] = {
+        ["Name"] = "Electric Candle",
+        ["Icon"] = "http://www.roblox.com/asset/?id=4520882345",
+        ["Source"] = "Gifted Riley Bee",
+        ["Slot"] = 11,
+        ["ID"] = 10,
+        ["Buffs"] = {
+            {["Value"] = 0.25, ["Type"] = "BaseProductionRate", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.25,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Zone"] = "Mushroom Field"}
+            },
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.25,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Zone"] = "Rose Field"}
+            },
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.1,
+                ["RBCDisable"] = true,
+                ["Type"] = "PollenBonus",
+                ["Params"] = {["Tag"] = "Flame"}
+            }
+        },
+        ["Quest"] = "Riley Bee's Ornament",
+        ["Desc"] = "A light-up replica of a holiday candle. It's powered by a single AA battery."
+    },
+    ["Snowman"] = {
+        ["Name"] = "Snowman",
+        ["Icon"] = "http://www.roblox.com/asset/?id=4520882546",
+        ["Source"] = "Gifted Bucko Bee",
+        ["Slot"] = 12,
+        ["ID"] = 11,
+        ["Buffs"] = {
+            {["Value"] = 0.25, ["Type"] = "BaseProductionRate", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.25,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Zone"] = "Blue Flower Field"}
+            },
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.25,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Zone"] = "Pine Tree Forest"}
+            },
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.1,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Color"] = "Blue"}
+            }
+        },
+        ["Quest"] = "Bucko Bee's Ornament",
+        ["Desc"] = "A felt snow man with a face resembling Frosty Bee."
+    },
+    ["Candy Cane Reindeer"] = {
+        ["Name"] = "Candy Cane Reindeer",
+        ["Icon"] = "http://www.roblox.com/asset/?id=4520882258",
+        ["Source"] = "Onett",
+        ["Slot"] = 8,
+        ["ID"] = 8,
+        ["Buffs"] = {
+            {["Value"] = 0.25, ["Type"] = "BaseProductionRate", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.25,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Zone"] = "Mountain Top Field"}
+            },
+            {["Value"] = 0.2, ["Type"] = "CollectorPollenMultiplier", ["RBCDisable"] = true, ["Op"] = "Add"}
+        },
+        ["Quest"] = "Onett's Ornament",
+        ["Desc"] = "A peppermint candy cane with plastic googly-eyes and antlers made of pipe-cleaners."
+    },
+    ["Royal Jelly"] = {
+        ["Name"] = "Royal Jelly",
+        ["Icon"] = "http://www.roblox.com/asset/?id=4520882195",
+        ["Source"] = "Brown Bear",
+        ["Slot"] = 3,
+        ["ID"] = 3,
+        ["Buffs"] = {
+            {["Value"] = 0.25, ["Type"] = "BaseProductionRate", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.25,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Zone"] = "Clover Field"}
+            },
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.2,
+                ["RBCDisable"] = true,
+                ["Type"] = "PollenBonus",
+                ["Params"] = {["Tag"] = "Bomb"}
+            }
+        },
+        ["Quest"] = "Brown Bear's Ornament",
+        ["Desc"] = "A plastic replica of a Royal Jelly jar with Brown Bear's face on it."
+    },
+    ["Honey Jar"] = {
+        ["Source"] = "Black Bear",
+        ["Slot"] = 1,
+        ["Buffs2019"] = {
+            {["Value"] = 0.1, ["Type"] = "ContainerSpaceMultiplier", ["Op"] = "Add"},
+            {["Value"] = 0.05, ["Type"] = "PollenValue", ["Op"] = "Add"},
+            {["Value"] = 1.25, ["Type"] = "PollenBonus", ["Op"] = "Mul", ["Params"] = {["Zone"] = "Sunflower Field"}}
+        },
+        ["Desc"] = "A replica of an expensive jar of artisanal honey.",
+        ["Name"] = "Honey Jar",
+        ["ID"] = 1,
+        ["Buffs"] = {
+            {["Value"] = 0.25, ["Type"] = "BaseProductionRate", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.25,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Zone"] = "Sunflower Field"}
+            },
+            {["Value"] = 0.05, ["Type"] = "PollenValue", ["RBCDisable"] = true, ["Op"] = "Add"}
+        },
+        ["Quest"] = "BlackBearXmas21",
+        ["Icon"] = "http://www.roblox.com/asset/?id=4520882165"
+    },
+    ["Bag Of Honey"] = {
+        ["Source"] = "Honey Bee",
+        ["Slot"] = 14,
+        ["SourceHidden"] = true,
+        ["Desc"] = "A sandwich bag filled with honey. A very happy Honey Bee floats inside.",
+        ["OrnamentSuffix"] = "",
+        ["Name"] = "Bag Of Honey",
+        ["ID"] = 13,
+        ["Buffs"] = {
+            {["Value"] = 0.25, ["Type"] = "BaseProductionRate", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {["Value"] = 0.05, ["Type"] = "MarkDurationMultiplier", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {["Value"] = 0.1, ["Type"] = "HoneyFromTokens", ["RBCDisable"] = true, ["Op"] = "Add"}
+        },
+        ["Quest"] = "Honey BeeXmas21",
+        ["Icon"] = "rbxassetid://6318424884"
+    },
+    ["Partridge In A Stick Bug"] = {
+        ["Name"] = "Partridge In A Stick Bug",
+        ["Icon"] = "http://www.roblox.com/asset/?id=4520882414",
+        ["Source"] = "Stick Bug",
+        ["Slot"] = 13,
+        ["ID"] = 12,
+        ["Buffs"] = {
+            {["Value"] = 0.25, ["Type"] = "BaseProductionRate", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.25,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Zone"] = "Cactus Field"}
+            },
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.25,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Zone"] = "Pepper Patch"}
+            },
+            {["Value"] = 0.05, ["Type"] = "PollenBonus", ["RBCDisable"] = true, ["Op"] = "Add"}
+        },
+        ["Quest"] = "Stick Bug's Ornament",
+        ["Desc"] = [[A clearly hand-crafted ornament representing the first of the "12 Days of Beesmas".]]
+    },
+    ["Potpourri"] = {
+        ["Name"] = "Potpourri",
+        ["Icon"] = "rbxassetid://8420613753",
+        ["Source"] = "Dapper Bear",
+        ["Slot"] = 7,
+        ["ID"] = 7,
+        ["Buffs"] = {
+            {["Value"] = 0.25, ["Type"] = "BaseProductionRate", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.25,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Zone"] = "Dandelion Field"}
+            },
+            {["Value"] = 0.05, ["Type"] = "NectarMultiplier", ["RBCDisable"] = true, ["Op"] = "Add"}
+        },
+        ["Quest"] = "DapperBearXmas21",
+        ["Desc"] = "A sheer mesh pouch webbed with decorative wire, filled with fragrant dried petals, leaves and pinecones."
+    },
+    ["Digital Pet"] = {
+        ["Source"] = "Robo Bear",
+        ["Slot"] = 10,
+        ["SourceHidden"] = true,
+        ["Desc"] = "A holiday edition of a virtual keychain pet. It's got a low resolution LCD screen and only 3 buttons.",
+        ["OrnamentSuffix"] = "",
+        ["Name"] = "Digital Pet",
+        ["ID"] = 16,
+        ["Buffs"] = {
+            {["Value"] = 0.1, ["Type"] = "ContainerSpaceMultiplier", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.25,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Zone"] = "Spider Field"}
+            },
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.1,
+                ["RBCDisable"] = true,
+                ["Type"] = "PollenBonus",
+                ["Params"] = {["Tag"] = "Event Bee Ability"}
+            },
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.05,
+                ["RBCDisable"] = true,
+                ["Type"] = "PollenBonus",
+                ["Params"] = {["Tag"] = "Duped Ability"}
+            }
+        },
+        ["Quest"] = "RoboBearXmas22",
+        ["Icon"] = "rbxassetid://12922267914"
+    },
+    ["Boxing Glove"] = {
+        ["Name"] = "Boxing Glove",
+        ["Icon"] = "http://www.roblox.com/asset/?id=4520882298",
+        ["Source"] = "Panda Bear",
+        ["Slot"] = 4,
+        ["ID"] = 4,
+        ["Buffs"] = {
+            {["Value"] = 0.25, ["Type"] = "BaseProductionRate", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.25,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Zone"] = "Bamboo Field"}
+            },
+            {
+                ["Op"] = "Add",
+                ["KeepRBC"] = true,
+                ["RBCDisable"] = true,
+                ["Value"] = 0.05,
+                ["Type"] = "BeeAttackMultiplier"
+            }
+        },
+        ["Quest"] = "Panda Bear's Ornament",
+        ["Desc"] = [[A miniture boxing glove with the word "POW" engraved on it.]]
+    },
+    ["Cast Iron Skillet"] = {
+        ["Name"] = "Cast Iron Skillet",
+        ["Icon"] = "http://www.roblox.com/asset/?id=4520882322",
+        ["Source"] = "Polar Bear",
+        ["Slot"] = 6,
+        ["ID"] = 6,
+        ["Buffs"] = {
+            {["Value"] = 0.25, ["Type"] = "BaseProductionRate", ["RBCDisable"] = true, ["Op"] = "Add"},
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.25,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Zone"] = "Pumpkin Patch"}
+            },
+            {
+                ["Op"] = "Add",
+                ["Value"] = 0.1,
+                ["RBCDisable"] = true,
+                ["Type"] = "ContainerSpaceMultiplier",
+                ["Params"] = {["Color"] = "White"}
+            }
+        },
+        ["Quest"] = "Polar Bear's Ornament",
+        ["Desc"] = "A miniature cast iron skillet - a type of pan that's naturally non-stick and great for browning."
+    }
+}
