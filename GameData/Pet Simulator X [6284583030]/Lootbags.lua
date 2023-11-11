9c3f325e05cfaec8e50a9c0334fbcee32d1a6cab
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 18:45:57 +1100

    Create Lootbags.lua

---
+++ GameData/Pet Simulator X [6284583030]/Lootbags.lua
@@ -0,0 +1,196 @@
+data = {
+    ["Mini Comet Diamonds 3"] = {
+        ["masteryXP"] = 200,
+        ["model"] = "Giftbox",
+        ["orbs"] = {40, 80},
+        ["isRare"] = true,
+        ["rewards"] = {{"Diamonds", 50, 1666.6666666666667}, {"Boost", 10, 3}},
+        ["weight"] = 0
+    },
+    ["HugePet"] = {
+        ["masteryXP"] = 450,
+        ["model"] = "HugeSnowmanGiftbox",
+        ["orbs"] = {60, 65},
+        ["isRare"] = true,
+        ["rewards"] = {{"HugePet", 100, 1}},
+        ["weight"] = 0
+    },
+    ["Massive Comet Diamonds 3"] = {
+        ["masteryXP"] = 400,
+        ["model"] = "Giftbox",
+        ["orbs"] = {40, 80},
+        ["isRare"] = true,
+        ["rewards"] = {{"Diamonds", 50, 10000}, {"Boost", 10, 15}},
+        ["weight"] = 0
+    },
+    ["Diamond Bag Tier 2"] = {
+        ["masteryXP"] = 5,
+        ["rewards"] = {{"Diamonds", 100, 10000}},
+        ["diamondMine"] = true,
+        ["model"] = "Diamond Bag Tier 2",
+        ["orbs"] = {10, 20},
+        ["isRare"] = false,
+        ["staticCurrency"] = true,
+        ["weight"] = 0
+    },
+    ["Mini Giftbox"] = {
+        ["masteryXP"] = 400,
+        ["model"] = "Mini Giftbox",
+        ["orbs"] = {60, 65},
+        ["isRare"] = true,
+        ["rewards"] = {{"Boost", 100, 1}},
+        ["weight"] = 1.5
+    },
+    ["Giftbox"] = {
+        ["masteryXP"] = 450,
+        ["model"] = "Giftbox",
+        ["orbs"] = {60, 65},
+        ["isRare"] = true,
+        ["rewards"] = {{"Boost", 100, 2}},
+        ["weight"] = 1
+    },
+    ["Best"] = {
+        ["masteryXP"] = 200,
+        ["model"] = "Best",
+        ["orbs"] = {60, 65},
+        ["isRare"] = true,
+        ["rewards"] = {{"Coins", 50, 15}, {"Diamonds", 75, 18}, {"Boost", 2.5, 2}},
+        ["weight"] = 2.5
+    },
+    ["Small"] = {
+        ["masteryXP"] = 75,
+        ["model"] = "Small",
+        ["orbs"] = {16, 19},
+        ["isRare"] = false,
+        ["rewards"] = {{"Coins", 100, 1}, {"Diamonds", 60, 1}, {"Boost", 0.15, 1}},
+        ["weight"] = 100
+    },
+    ["FreeGiftsDiamonds"] = {
+        ["masteryXP"] = 25,
+        ["model"] = "Giftbox",
+        ["orbs"] = {60, 65},
+        ["isRare"] = true,
+        ["rewards"] = {{"Diamonds", 50, 13.5}},
+        ["weight"] = 0
+    },
+    ["FreeGiftsCoinsJUMBO"] = {
+        ["masteryXP"] = 75,
+        ["model"] = "Good",
+        ["orbs"] = {28, 32},
+        ["isRare"] = true,
+        ["rewards"] = {{"Coins", 50, 14}},
+        ["weight"] = 0
+    },
+    ["FreeGiftsDiamondsJUMBO"] = {
+        ["masteryXP"] = 75,
+        ["model"] = "Giftbox",
+        ["orbs"] = {60, 65},
+        ["isRare"] = true,
+        ["rewards"] = {{"Diamonds", 50, 52.5}},
+        ["weight"] = 0
+    },
+    ["Massive Comet Diamonds 2"] = {
+        ["masteryXP"] = 200,
+        ["model"] = "Giftbox",
+        ["orbs"] = {20, 40},
+        ["isRare"] = true,
+        ["rewards"] = {{"Diamonds", 50, 4000}, {"Boost", 10, 8}},
+        ["weight"] = 0
+    },
+    ["Diamond Bag Tier 4"] = {
+        ["masteryXP"] = 10,
+        ["rewards"] = {{"Diamonds", 100, 1000000}},
+        ["diamondMine"] = true,
+        ["model"] = "Diamond Bag Tier 4",
+        ["orbs"] = {20, 40},
+        ["isRare"] = true,
+        ["staticCurrency"] = true,
+        ["weight"] = 0
+    },
+    ["Good"] = {
+        ["masteryXP"] = 125,
+        ["model"] = "Good",
+        ["orbs"] = {28, 32},
+        ["isRare"] = false,
+        ["rewards"] = {{"Coins", 50, 2.5}, {"Diamonds", 50, 2.5}, {"Boost", 0.225, 1}},
+        ["weight"] = 20
+    },
+    ["Scavenger Diamonds"] = {
+        ["masteryXP"] = 25,
+        ["model"] = "Giftbox",
+        ["orbs"] = {60, 65},
+        ["isRare"] = true,
+        ["rewards"] = {{"Diamonds", 50, 6500}},
+        ["weight"] = 0
+    },
+    ["ChristmasEgg"] = {
+        ["masteryXP"] = 450,
+        ["model"] = "Mini Giftbox",
+        ["orbs"] = {60, 65},
+        ["isRare"] = true,
+        ["rewards"] = {{"ChristmasEgg", 100, 1}},
+        ["weight"] = 0
+    },
+    ["Diamond Bag Tier 1"] = {
+        ["masteryXP"] = 3,
+        ["rewards"] = {{"Diamonds", 100, 2500}},
+        ["diamondMine"] = true,
+        ["model"] = "Diamond Bag Tier 1",
+        ["orbs"] = {5, 10},
+        ["isRare"] = false,
+        ["staticCurrency"] = true,
+        ["weight"] = 0
+    },
+    ["Diamond Bag Tier 5"] = {
+        ["masteryXP"] = 20,
+        ["rewards"] = {{"Diamonds", 100, 10000000}},
+        ["diamondMine"] = true,
+        ["model"] = "Diamond Bag Tier 5",
+        ["orbs"] = {25, 50},
+        ["isRare"] = true,
+        ["staticCurrency"] = true,
+        ["weight"] = 0
+    },
+    ["Massive Comet Diamonds 1"] = {
+        ["masteryXP"] = 100,
+        ["model"] = "Giftbox",
+        ["orbs"] = {10, 20},
+        ["isRare"] = true,
+        ["rewards"] = {{"Diamonds", 50, 1000}, {"Boost", 10, 4}},
+        ["weight"] = 0
+    },
+    ["Mini Comet Diamonds 2"] = {
+        ["masteryXP"] = 100,
+        ["model"] = "Giftbox",
+        ["orbs"] = {20, 40},
+        ["isRare"] = true,
+        ["rewards"] = {{"Diamonds", 50, 666.6666666666666}, {"Boost", 10, 1}},
+        ["weight"] = 0
+    },
+    ["Mini Comet Diamonds 1"] = {
+        ["masteryXP"] = 50,
+        ["model"] = "Giftbox",
+        ["orbs"] = {10, 20},
+        ["isRare"] = true,
+        ["rewards"] = {{"Diamonds", 50, 166.66666666666666}, {"Boost", 10, 1}},
+        ["weight"] = 0
+    },
+    ["FreeGiftsCoins"] = {
+        ["masteryXP"] = 25,
+        ["model"] = "Good",
+        ["orbs"] = {28, 32},
+        ["isRare"] = true,
+        ["rewards"] = {{"Coins", 50, 6}},
+        ["weight"] = 0
+    },
+    ["Diamond Bag Tier 3"] = {
+        ["masteryXP"] = 8,
+        ["rewards"] = {{"Diamonds", 100, 100000}},
+        ["diamondMine"] = true,
+        ["model"] = "Diamond Bag Tier 3",
+        ["orbs"] = {15, 30},
+        ["isRare"] = false,
+        ["staticCurrency"] = true,
+        ["weight"] = 0
+    }
+}
