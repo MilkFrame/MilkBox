ffa3e0495a06c0380289baa0ebcc39dea6bad1c6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 19:07:21 +1100

    Create BeeStatMods.lua

---
+++ GameData/Bee Swarm Simulator [1537690962]/BeeStatMods.lua
@@ -0,0 +1,275 @@
+data = {
+    ["LevelBondReqs"] = {0,10,50,250,1000,5000,20000,80000,350000,800000,2000000,4000000,8000000,15000000,30000000,150000000,600000000,2500000000,10000000000,25000000000,500000000000,5000000000000,100000000000000,1000000000000000,10000000000000000},
+    ["CriticalPower"] = {
+        ["Op"] = "Add",
+        ["Name"] = "CriticalPower",
+        ["Tag"] = "CRP",
+        ["Desc"] = "function: 0x00000000d70f0935",
+        ["Base"] = 0,
+        ["DisplayName"] = "Critical Power"
+    },
+    ["FlamePollen"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "FlamePollen",
+        ["Tag"] = "FLP",
+        ["Desc"] = "function: 0x00000000ce8f6d1d",
+        ["Base"] = 1,
+        ["DisplayName"] = "Flames Pollen"
+    },
+    ["AttackMul"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "AttackMul",
+        ["Tag"] = "AM",
+        ["Desc"] = "function: 0x0000000060f23045",
+        ["Base"] = 1,
+        ["DisplayName"] = "Attack"
+    },
+    ["GatherMul"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "GatherMul",
+        ["Tag"] = "GM",
+        ["Desc"] = "function: 0x000000000d36df6d",
+        ["Base"] = 1,
+        ["DisplayName"] = "Gather Amount"
+    },
+    ["MovespeedMul"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "MovespeedMul",
+        ["Tag"] = "MM",
+        ["Desc"] = "function: 0x000000006e0c7775",
+        ["Base"] = 1,
+        ["DisplayName"] = "Movespeed"
+    },
+    ["SuperCritChance"] = {
+        ["Op"] = "Add",
+        ["Name"] = "SuperCritChance",
+        ["Tag"] = "SCC",
+        ["Desc"] = "function: 0x00000000ac949715",
+        ["Base"] = 0,
+        ["DisplayName"] = "Super-Crit Chance"
+    },
+    ["GatherBlueAdd"] = {
+        ["Op"] = "Add",
+        ["Name"] = "GatherBlueAdd",
+        ["Tag"] = "GBA",
+        ["Desc"] = "function: 0x00000000ddde0975",
+        ["Base"] = 0,
+        ["DisplayName"] = "Blue Gather Amount"
+    },
+    ["GatherBlueMul"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "GatherBlueMul",
+        ["Tag"] = "GBM",
+        ["Desc"] = "function: 0x00000000d15dae7d",
+        ["Base"] = 1,
+        ["DisplayName"] = "Blue Gather Amount"
+    },
+    ["SuperCritPower"] = {
+        ["Op"] = "Add",
+        ["Name"] = "SuperCritPower",
+        ["Tag"] = "SCP",
+        ["Desc"] = "function: 0x0000000032c34965",
+        ["Base"] = 0,
+        ["DisplayName"] = "Super-Crit Power"
+    },
+    ["GatherWhiteAdd"] = {
+        ["Op"] = "Add",
+        ["Name"] = "GatherWhiteAdd",
+        ["Tag"] = "GWA",
+        ["Desc"] = "function: 0x0000000023044985",
+        ["Base"] = 0,
+        ["DisplayName"] = "White Gather Amount"
+    },
+    ["BubblePollen"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "BubblePollen",
+        ["Tag"] = "BUP",
+        ["Desc"] = "function: 0x000000008dd6c565",
+        ["Base"] = 1,
+        ["DisplayName"] = "Bubble Pollen"
+    },
+    ["ConvertMul"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "ConvertMul",
+        ["Tag"] = "CM",
+        ["Desc"] = "function: 0x00000000e9f2700d",
+        ["Base"] = 1,
+        ["DisplayName"] = "Convert Amount"
+    },
+    ["GatherPollinationChance"] = {
+        ["Op"] = "Add",
+        ["Name"] = "GatherPollinationChance",
+        ["Tag"] = "GPC",
+        ["Desc"] = "function: 0x000000006f68c8a5",
+        ["Base"] = 0,
+        ["DisplayName"] = "Gather Pollination Chance"
+    },
+    ["MarkDur"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "MarkDur",
+        ["Tag"] = "MD",
+        ["Desc"] = "function: 0x0000000041bf2355",
+        ["Base"] = 1,
+        ["DisplayName"] = "Mark Duration"
+    },
+    ["AbilityPollen"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "AbilityPollen",
+        ["Tag"] = "ABP",
+        ["Desc"] = "function: 0x00000000ec40ca5d",
+        ["Base"] = 1,
+        ["DisplayName"] = "Ability Pollen"
+    },
+    ["MovespeedAdd"] = {
+        ["Op"] = "Add",
+        ["Name"] = "MovespeedAdd",
+        ["Tag"] = "MA",
+        ["Desc"] = "function: 0x0000000086498895",
+        ["Base"] = 0,
+        ["DisplayName"] = "Movespeed"
+    },
+    ["EnergyAdd"] = {
+        ["Op"] = "Add",
+        ["Name"] = "EnergyAdd",
+        ["Tag"] = "EA",
+        ["Desc"] = "function: 0x0000000025914fe5",
+        ["Base"] = 0,
+        ["DisplayName"] = "Energy"
+    },
+    ["CriticalChance"] = {
+        ["Op"] = "Add",
+        ["Name"] = "CriticalChance",
+        ["Tag"] = "CRC",
+        ["Desc"] = "function: 0x00000000dfea3705",
+        ["Base"] = 0,
+        ["DisplayName"] = "Critical Chance"
+    },
+    ["FlameChance"] = {
+        ["Op"] = "Add",
+        ["Name"] = "FlameChance",
+        ["Tag"] = "FLC",
+        ["Desc"] = "function: 0x00000000fae68bd5",
+        ["Base"] = 0,
+        ["DisplayName"] = "Gathering Flames Chance"
+    },
+    ["ConvertAtHiveAdd"] = {
+        ["Op"] = "Add",
+        ["Name"] = "ConvertAtHiveAdd",
+        ["Tag"] = "CAHA",
+        ["Desc"] = "function: 0x000000004b6d08b5",
+        ["Base"] = 0,
+        ["DisplayName"] = "Convert At Hive"
+    },
+    ["GatherRedMul"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "GatherRedMul",
+        ["Tag"] = "GRM",
+        ["Desc"] = "function: 0x0000000009297e85",
+        ["Base"] = 1,
+        ["DisplayName"] = "Red Gather Amount"
+    },
+    ["BubbleChance"] = {
+        ["Op"] = "Add",
+        ["Name"] = "BubbleChance",
+        ["Tag"] = "BUC",
+        ["Desc"] = "function: 0x000000005453c8c5",
+        ["Base"] = 0,
+        ["DisplayName"] = "Gathering Bubbles Chance"
+    },
+    ["RedBombPollen"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "RedBombPollen",
+        ["Tag"] = "BPR",
+        ["Desc"] = "function: 0x00000000d211a7ad",
+        ["Base"] = 1,
+        ["DisplayName"] = "Red Bomb Pollen"
+    },
+    ["BombPollen"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "BombPollen",
+        ["Tag"] = "BP",
+        ["Desc"] = "function: 0x000000006ccad035",
+        ["Base"] = 1,
+        ["DisplayName"] = "Bomb Pollen"
+    },
+    ["BuzzBombPollen"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "BuzzBombPollen",
+        ["Tag"] = "BPZ",
+        ["Desc"] = "function: 0x0000000088a7053d",
+        ["Base"] = 1,
+        ["DisplayName"] = "Buzz Bomb Pollen"
+    },
+    ["BlueBombPollen"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "BlueBombPollen",
+        ["Tag"] = "BPB",
+        ["Desc"] = "function: 0x00000000627bda25",
+        ["Base"] = 1,
+        ["DisplayName"] = "Blue Bomb Pollen"
+    },
+    ["AbilityRate"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "AbilityRate",
+        ["Tag"] = "AR",
+        ["Desc"] = "function: 0x00000000c3acabcd",
+        ["Base"] = 1,
+        ["DisplayName"] = "Ability Rate"
+    },
+    ["GatherWhiteMul"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "GatherWhiteMul",
+        ["Tag"] = "GWM",
+        ["Desc"] = "function: 0x000000006bd28495",
+        ["Base"] = 1,
+        ["DisplayName"] = "White Gather Amount"
+    },
+    ["AttackAdd"] = {
+        ["Op"] = "Add",
+        ["Name"] = "AttackAdd",
+        ["Tag"] = "AA",
+        ["Desc"] = "function: 0x00000000fee6c845",
+        ["Base"] = 0,
+        ["DisplayName"] = "Attack"
+    },
+    ["ConvertAtHiveMul"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "ConvertAtHiveMul",
+        ["Tag"] = "CAHM",
+        ["Desc"] = "function: 0x000000001319d19d",
+        ["Base"] = 1,
+        ["DisplayName"] = "Convert At Hive"
+    },
+    ["EnergyMul"] = {
+        ["Op"] = "Mul",
+        ["Name"] = "EnergyMul",
+        ["Tag"] = "EM",
+        ["Desc"] = "function: 0x00000000522126c5",
+        ["Base"] = 1,
+        ["DisplayName"] = "Energy"
+    },
+    ["ConvertAdd"] = {
+        ["Op"] = "Add",
+        ["Name"] = "ConvertAdd",
+        ["Tag"] = "CA",
+        ["Desc"] = "function: 0x000000000d138855",
+        ["Base"] = 0,
+        ["DisplayName"] = "Convert Amount"
+    },
+    ["GatherAdd"] = {
+        ["Op"] = "Add",
+        ["Name"] = "GatherAdd",
+        ["Tag"] = "GA",
+        ["Desc"] = "function: 0x00000000d3cfc825",
+        ["Base"] = 0,
+        ["DisplayName"] = "Gather Amount"
+    },
+    ["GatherRedAdd"] = {
+        ["Op"] = "Add",
+        ["Name"] = "GatherRedAdd",
+        ["Tag"] = "GRA",
+        ["Desc"] = "function: 0x00000000b6840bf5",
+        ["Base"] = 0,
+        ["DisplayName"] = "Red Gather Amount"
+    }
+}
