9cb3d5d7f9cc45c28b69f3a69af8556add2f054b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 19:03:07 +1100

    Create Backpacks.lua

---
+++ GameData/Bee Swarm Simulator [1537690962]/Backpacks.lua
@@ -0,0 +1,44 @@
+data = {
+    ["Canister"] = {
+        ["Capacity"] = 10000,
+        ["Description"] = "A high-tech container. Holds up to 10,000 pollen.",
+        ["Cost"] = 22000
+    },
+    ["Compressor"] = {
+        ["Capacity"] = 50000,
+        ["Description"] = "A machine which packs pollen down to increase storage capacity to 50,000.",
+        ["Cost"] = 160000
+    },
+    ["Jar"] = {["Capacity"] = 750, ["Description"] = "A durable plastic jar. Holds up to 750 pollen.", ["Cost"] = 650},
+    ["Elite Barrel"] = {
+        ["Capacity"] = 100000,
+        ["Description"] = "The worlds most advanced barrel. Holds up to 100,000 pollen.",
+        ["Cost"] = 650000
+    },
+    ["Porcelain Port-O-Hive"] = {
+        ["Capacity"] = 500000,
+        ["Description"] = "The rarest and most precious Port-O-Hive in the world. Can store up to 500,000 pollen.",
+        ["Cost"] = 250000000
+    },
+    ["Port-O-Hive"] = {
+        ["Capacity"] = 150000,
+        ["Description"] = "A miniature hive you can wear on your back! Can store up to 150,000 pollen.",
+        ["Cost"] = 1250000
+    },
+    ["Debug-Bag"] = {
+        ["Capacity"] = 500000,
+        ["Description"] = "A miniature hive you can wear on your back! Can store up to 150,000 pollen."
+    },
+    ["Mega-Jug"] = {
+        ["Capacity"] = 24000,
+        ["Description"] = "A humongous jug! Holds up to 24,000 pollen.",
+        ["Cost"] = 50000
+    },
+    ["Pouch"] = {["Capacity"] = 200, ["Description"] = "A small bag. Holds up to 200 pollen.", ["Cost"] = 0},
+    ["Backpack"] = {
+        ["Capacity"] = 3500,
+        ["Description"] = "A heavy-duty backpack. Holds up to 3,500 pollen.",
+        ["Cost"] = 5500
+    },
+    ["None"] = {["Capacity"] = 0, ["Description"] = "No container.", ["Cost"] = 0}
+}
