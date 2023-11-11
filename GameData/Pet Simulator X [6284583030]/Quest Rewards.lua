9bee94ebccb3f91a87257f9e31d1bf19dddd982c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 18:49:28 +1100

    Create Quest Rewards.lua

---
+++ GameData/Pet Simulator X [6284583030]/Quest Rewards.lua
@@ -0,0 +1,37 @@
+data = {
+    ["Medium Diamond Pack"] = {
+        ["Amount"] = 1,
+        ["Currency"] = {["Diamonds"] = 6000000000},
+        ["Name"] = "Medium Diamond Pack",
+        ["Cost"] = 2000
+    },
+    ["Booster Pack"] = {
+        ["Amount"] = 1,
+        ["Boosts"] = {["Triple Coins"] = 65, ["Ultra Lucky"] = 35, ["Super Lucky"] = 85, ["Triple Damage"] = 65},
+        ["Name"] = "Booster Pack",
+        ["Cost"] = 150
+    },
+    ["Pet Equip"] = {["PetEquips"] = 1, ["Stock"] = 1, ["Cost"] = 2500, ["Amount"] = 1, ["Name"] = "Pet Equip"},
+    ["Small Diamond Pack"] = {
+        ["Amount"] = 1,
+        ["Currency"] = {["Diamonds"] = 1250000000},
+        ["Name"] = "Small Diamond Pack",
+        ["Cost"] = 500
+    },
+    ["Quest Egg"] = {["Amount"] = 1, ["Name"] = "Quest Egg", ["Pet"] = {["id"] = "1026"}, ["Cost"] = 400},
+    ["Large Diamond Pack"] = {
+        ["Amount"] = 1,
+        ["Currency"] = {["Diamonds"] = 25000000000},
+        ["Name"] = "Large Diamond Pack",
+        ["Cost"] = 5000
+    },
+    ["Pet Storage"] = {["Stock"] = 3, ["Cost"] = 200, ["Amount"] = 1, ["PetSlots"] = 10, ["Name"] = "Pet Storage"},
+    ["Quest Booth"] = {["Stock"] = 1, ["Cost"] = 400, ["Amount"] = 1, ["Name"] = "Quest Booth", ["Booth"] = "M10 Booth"},
+    ["Quest Hoverboard"] = {
+        ["Stock"] = 1,
+        ["Cost"] = 1000,
+        ["Amount"] = 1,
+        ["Hoverboard"] = "M-10",
+        ["Name"] = "Quest Hoverboard"
+    }
+}
