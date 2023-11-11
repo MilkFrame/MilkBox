02ffc7e9077c2f925f07fe4da906349d4673da04
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 18:50:09 +1100

    Create Ranks.lua

---
+++ GameData/Pet Simulator X [6284583030]/Ranks.lua
@@ -0,0 +1,205 @@
+data = {
+    ["Hero"] = {
+        ["needed"] = 163840000,
+        ["title"] = "Hero",
+        ["productId"] = 1192856065,
+        ["rewardCooldown"] = 28800,
+        ["rewards"] = {{"Diamonds", 11000}, {"Super Lucky Boost", 1}}
+    },
+    ["Godlike"] = {
+        ["needed"] = 419430400000,
+        ["title"] = "Godlike",
+        ["productId"] = 1192856066,
+        ["rewardCooldown"] = 18000,
+        ["rewards"] = {
+            {"Diamonds", 45000},
+            {"Triple Coins Boost", 1},
+            {"Super Lucky Boost", 2},
+            {"Triple Damage Boost", 1}
+        }
+    },
+    ["Pro"] = {
+        ["needed"] = 5000000,
+        ["title"] = "Pro",
+        ["productId"] = 1192856067,
+        ["rewardCooldown"] = 28800,
+        ["rewards"] = {{"Diamonds", 6500}}
+    },
+    ["Legend"] = {
+        ["needed"] = 10485760000,
+        ["title"] = "Legend",
+        ["productId"] = 1192856064,
+        ["rewardCooldown"] = 21600,
+        ["rewards"] = {{"Diamonds", 25000}, {"Super Lucky Boost", 1}, {"Triple Damage Boost", 1}}
+    },
+    ["Elite"] = {
+        ["needed"] = 25000000,
+        ["title"] = "Elite",
+        ["productId"] = 1192856065,
+        ["rewardCooldown"] = 28800,
+        ["rewards"] = {{"Diamonds", 8500}}
+    },
+    ["Insane Hacker"] = {
+        ["needed"] = 13743895347200000,
+        ["title"] = "Insane Hacker",
+        ["productId"] = 1195031603,
+        ["rewardCooldown"] = 16200,
+        ["rewards"] = {
+            {"Diamonds", 125000},
+            {"Triple Coins Boost", 2},
+            {"Super Lucky Boost", 2},
+            {"Triple Damage Boost", 2},
+            {"Ultra Lucky Boost", 1}
+        }
+    },
+    ["Basic"] = {
+        ["needed"] = 1000000,
+        ["title"] = "Basic",
+        ["productId"] = 1192856067,
+        ["rewardCooldown"] = 28800,
+        ["rewards"] = {{"Diamonds", 5000}}
+    },
+    ["Immortal"] = {
+        ["needed"] = 52428800000,
+        ["title"] = "Immortal",
+        ["productId"] = 1192856066,
+        ["rewardCooldown"] = 18000,
+        ["rewards"] = {
+            {"Diamonds", 32500},
+            {"Triple Coins Boost", 1},
+            {"Super Lucky Boost", 1},
+            {"Triple Damage Boost", 1}
+        }
+    },
+    ["Starter"] = {
+        ["needed"] = 200000,
+        ["title"] = "Starter",
+        ["productId"] = 1192856067,
+        ["rewardCooldown"] = 0,
+        ["rewards"] = {}
+    },
+    ["The Best"] = {
+        ["needed"] = 879609302220800000,
+        ["title"] = "THE BEST",
+        ["productId"] = 1195031603,
+        ["rewardCooldown"] = 14400,
+        ["rewards"] = {
+            {"Diamonds", 200000},
+            {"Triple Coins Boost", 2},
+            {"Super Lucky Boost", 2},
+            {"Triple Damage Boost", 2},
+            {"Ultra Lucky Boost", 2}
+        }
+    },
+    ["PET CELESTIAL"] = {
+        ["needed"] = 0,
+        ["title"] = "PET CELESTIAL",
+        ["productId"] = 0,
+        ["rewardCooldown"] = 12600,
+        ["rewards"] = {
+            {"Diamonds", 1250000},
+            {"Triple Coins Boost", 4},
+            {"Super Lucky Boost", 3},
+            {"Triple Damage Boost", 3},
+            {"Ultra Lucky Boost", 3}
+        }
+    },
+    ["Hacker"] = {
+        ["needed"] = 429496729600000,
+        ["title"] = "Hacker",
+        ["productId"] = 1195031603,
+        ["rewardCooldown"] = 18000,
+        ["rewards"] = {
+            {"Diamonds", 85000},
+            {"Triple Coins Boost", 1},
+            {"Super Lucky Boost", 2},
+            {"Triple Damage Boost", 2},
+            {"Ultra Lucky Boost", 1}
+        }
+    },
+    ["CREATOR OF PETS"] = {
+        ["needed"] = 1.4757395258967641e+25,
+        ["title"] = "CREATOR OF PETS",
+        ["productId"] = 1199177128,
+        ["rewardCooldown"] = 12600,
+        ["rewards"] = {
+            {"Diamonds", 1000000},
+            {"Triple Coins Boost", 3},
+            {"Super Lucky Boost", 3},
+            {"Triple Damage Boost", 3},
+            {"Ultra Lucky Boost", 3}
+        }
+    },
+    ["Master"] = {
+        ["needed"] = 2621440000,
+        ["title"] = "Master",
+        ["productId"] = 1192856064,
+        ["rewardCooldown"] = 28800,
+        ["rewards"] = {{"Diamonds", 20000}, {"Super Lucky Boost", 1}, {"Triple Damage Boost", 1}}
+    },
+    ["Noob"] = {
+        ["needed"] = 40000,
+        ["title"] = "Noob",
+        ["productId"] = 1192856067,
+        ["rewardCooldown"] = 0,
+        ["rewards"] = {}
+    },
+    ["PET OVERLORD"] = {
+        ["needed"] = 4.611686018427388e+23,
+        ["title"] = "PET OVERLORD",
+        ["productId"] = 1199177128,
+        ["rewardCooldown"] = 12600,
+        ["rewards"] = {
+            {"Diamonds", 750000},
+            {"Triple Coins Boost", 3},
+            {"Super Lucky Boost", 3},
+            {"Triple Damage Boost", 2},
+            {"Ultra Lucky Boost", 2}
+        }
+    },
+    ["Champion"] = {
+        ["needed"] = 655360000,
+        ["title"] = "Champion",
+        ["productId"] = 1192856065,
+        ["rewardCooldown"] = 28800,
+        ["rewards"] = {{"Diamonds", 15000}, {"Super Lucky Boost", 1}}
+    },
+    ["The Very Best"] = {
+        ["needed"] = 112589990684262400000,
+        ["title"] = "THE VERY BEST",
+        ["productId"] = 1199177128,
+        ["rewardCooldown"] = 14400,
+        ["rewards"] = {
+            {"Diamonds", 325000},
+            {"Triple Coins Boost", 2},
+            {"Super Lucky Boost", 2},
+            {"Triple Damage Boost", 2},
+            {"Ultra Lucky Boost", 2}
+        }
+    },
+    ["Impossible"] = {
+        ["needed"] = 13421772800000,
+        ["title"] = "Impossible",
+        ["productId"] = 1192856066,
+        ["rewardCooldown"] = 18000,
+        ["rewards"] = {
+            {"Diamonds", 60000},
+            {"Triple Coins Boost", 1},
+            {"Super Lucky Boost", 2},
+            {"Triple Damage Boost", 2}
+        }
+    },
+    ["PET GOD"] = {
+        ["needed"] = 7.205759403792794e+21,
+        ["title"] = "PET GOD",
+        ["productId"] = 1199177128,
+        ["rewardCooldown"] = 12600,
+        ["rewards"] = {
+            {"Diamonds", 500000},
+            {"Triple Coins Boost", 3},
+            {"Super Lucky Boost", 2},
+            {"Triple Damage Boost", 2},
+            {"Ultra Lucky Boost", 2}
+        }
+    }
+}
