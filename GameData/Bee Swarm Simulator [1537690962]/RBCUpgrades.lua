cda2262d419a18fed082377e31f958888724b96b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 19:23:56 +1100

    Create RBCUpgrades.lua

---
+++ GameData/Bee Swarm Simulator [1537690962]/RBCUpgrades.lua
@@ -0,0 +1,14 @@
+data = {
+    ["RARITY_CHANCES_MAX"] = {["Epic"] = 25, ["Legendary"] = 10, ["Rare"] = 50, ["Common"] = 60},
+    ["Init"] = "function: 0x000000003236332c",
+    ["GetAbilityDefs"] = "function: 0x00000000e323bd0c",
+    ["MAX_INFLATION"] = 0.25,
+    ["GetUpgrades"] = "function: 0x000000002d29c20c",
+    ["RARITY_TO_COST"] = {["Epic"] = {12, 14}, ["Legendary"] = {15, 18}, ["Rare"] = {9, 11}, ["Common"] = {6, 8}},
+    ["NUM_RARITIES"] = 4,
+    ["SelectBuffs"] = "function: 0x00000000cad27a3c",
+    ["RARITIES"] = {"Common", "Rare", "Epic", "Legendary"},
+    ["RARITY_CHANCES_MIN"] = {["Epic"] = 10, ["Legendary"] = 1, ["Rare"] = 30, ["Common"] = 80},
+    ["GetBuffDefs"] = "function: 0x0000000096d5c364",
+    ["RARITY_NAME_TO_INDEX"] = {["Epic"] = 3, ["Legendary"] = 4, ["Rare"] = 2, ["Common"] = 1}
+}
