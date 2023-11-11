493b8bb4381075833f48bc13635ff8cae7d5bdc7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 19:24:49 +1100

    Create WaxTypes.lua

---
+++ GameData/Bee Swarm Simulator [1537690962]/WaxTypes.lua
@@ -0,0 +1,65 @@
+data = {
+    ["ByID"] = "function: 0x000000006c1e2e84",
+    ["GetType"] = "function: 0x00000000d13bf3a4",
+    ["Get"] = "function: 0x00000000ba1c986c",
+    ["TypeByID"] = {
+        {["Name"] = "Soft", ["Upgrades"] = 1, ["ID"] = 1, ["Icon"] = "rbxassetid://8277778300", ["SuccessRate"] = 1},
+        {["Name"] = "Hard", ["Upgrades"] = 2, ["ID"] = 2, ["Icon"] = "rbxassetid://8277780065", ["SuccessRate"] = 0.6},
+        {
+            ["Name"] = "Caustic",
+            ["Upgrades"] = 4,
+            ["ID"] = 3,
+            ["SuccessRate"] = 0.25,
+            ["Icon"] = "rbxassetid://8277781666",
+            ["DestroyOnFail"] = true
+        },
+        {
+            ["RerollStats"] = true,
+            ["Name"] = "Swirled",
+            ["Upgrade"] = 0,
+            ["ID"] = 4,
+            ["Icon"] = "rbxassetid://8277783113",
+            ["SuccessRate"] = 1
+        },
+        {["Name"] = "Debug", ["Upgrades"] = 4, ["ID"] = 5, ["Icon"] = "rbxassetid://8277781666", ["SuccessRate"] = 1}
+    },
+    ["Types"] = {
+        ["Hard"] = {
+            ["Name"] = "Hard",
+            ["Upgrades"] = 2,
+            ["ID"] = 2,
+            ["Icon"] = "rbxassetid://8277780065",
+            ["SuccessRate"] = 0.6
+        },
+        ["Debug"] = {
+            ["Name"] = "Debug",
+            ["Upgrades"] = 4,
+            ["ID"] = 5,
+            ["Icon"] = "rbxassetid://8277781666",
+            ["SuccessRate"] = 1
+        },
+        ["Swirled"] = {
+            ["RerollStats"] = true,
+            ["Name"] = "Swirled",
+            ["Upgrade"] = 0,
+            ["ID"] = 4,
+            ["Icon"] = "rbxassetid://8277783113",
+            ["SuccessRate"] = 1
+        },
+        ["Caustic"] = {
+            ["Name"] = "Caustic",
+            ["Upgrades"] = 4,
+            ["ID"] = 3,
+            ["SuccessRate"] = 0.25,
+            ["Icon"] = "rbxassetid://8277781666",
+            ["DestroyOnFail"] = true
+        },
+        ["Soft"] = {
+            ["Name"] = "Soft",
+            ["Upgrades"] = 1,
+            ["ID"] = 1,
+            ["Icon"] = "rbxassetid://8277778300",
+            ["SuccessRate"] = 1
+        }
+    }
+}
