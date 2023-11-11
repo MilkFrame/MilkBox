2f3957a7706d0c0b8e63d9cb171c67f3711c4ff7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-11 18:41:09 +1100

    Create Backpack.lua

---
+++ GameData/Pet Simulator X [6284583030]/Backpack.lua
@@ -0,0 +1,5 @@
+data = {["Snowball Launcher"] = {["Snowball Launcher"]={["hasTheInMessage"]=true,["name"]="Snowball Launcher",["rarity"]="Legendary",["stackable"]=false,["icon"]="rbxassetid://4525213346",["desc"]="Capable of hitting targets & popping balloons!"}},
+["Firework Launcher"] = {["Firework Launcher"]={["hasTheInMessage"]=true,["name"]="Firework Launcher",["rarity"]="Legendary",["stackable"]=false,["icon"]="rbxassetid://4525213346",["desc"]="Capable of shooting fireworks!"}},
+["Paintball Launcher"] = {["Paintball Launcher"]={["hasTheInMessage"]=true,["name"]="Paintball Launcher",["rarity"]="Legendary",["stackable"]=false,["icon"]="rbxassetid://4525213346",["desc"]="Capable of shooting and painting players"}},
+["Water Launcher"] = {["Water Launcher"]={["hasTheInMessage"]=true,["name"]="Water Launcher",["rarity"]="Legendary",["stackable"]=false,["icon"]="rbxassetid://4525213346",["desc"]="Capable of shooting and drenching players"}},
+}
