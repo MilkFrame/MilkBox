e4f512f431a27602516c33bc473ff4dd73c902bb
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-14 00:31:44 +1100

    Create Pirate Clash [6161747775].lua

---
+++ Leaderboards/Pirate Clash [6161747775].lua
@@ -0,0 +1,47 @@
+--15 October 2023 9:00pm
+data = {
+    ["Coins"] = {
+        {1, "Ongjming", "88579134"},
+        {2, "Danishlady83", "3257011771"},
+        {3, "juswortell", "2895512714"},
+        {4, "Incredible4Emu", "2886830436"},
+        {5, "Oceaviolet", "3472141"},
+        {6, "NJCecil", "139814709"},
+        {7, "JedimasterFish", "419866633"},
+        {8, "hardboy112", "1741037770"},
+        {9, "TackleBus", "2783024362"}
+    },
+    ["Kills"] = {
+        {1, "juswortell", "2895512714"},
+        {2, "Incredible4Emu", "2886830436"},
+        {3, "Oceaviolet", "3472141"},
+        {4, "Danishlady83", "3257011771"},
+        {5, "NJCecil", "139814709"},
+        {6, "Ongjming", "88579134"},
+        {7, "JedimasterFish", "419866633"},
+        {8, "GreensIee", "165048217"},
+        {9, "ShadowyAura", "35767333"}
+    },
+    Levels = {
+        {1, "juswortell", 330, "2895512714"},
+        {2, "Danishlady83", 326, "3257011771"},
+        {3, "Ongjming", 287, "88579134"},
+        {4, "Oceaviolet", 282, "3472141"},
+        {5, "Incredible4Emu", 260, "2886830436"},
+        {6, "JedimasterFish", 177, "419866633"},
+        {7, "hardboy112", 118, "1741037770"},
+        {8, "Glenniek", 117, "3358959606"},
+        {9, "NJCecil", 117, "139814709"}
+    },
+    ["DailyQuests"] = {
+        {1, "Ongjming", "88579134"},
+        {2, "juswortell", 811, "2895512714"},
+        {3, "Incredible4Emu", 683, "2886830436"},
+        {4, "Danishlady83", 555, "3257011771"},
+        {5, "JedimasterFish", 504, "419866633"},
+        {6, "Oceaviolet", 391, "3472141"},
+        {7, "Glenniek", 236, "3358959606"},
+        {8, "NJCecil", 236, "139814709"},
+        {9, "Joey14584", 204, "16609377"}
+    }
+}
