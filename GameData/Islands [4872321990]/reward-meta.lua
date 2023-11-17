43b7910e22ec8d80efd21113980507bfb001ce85
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:27:15 +1100

    Create reward-meta.lua

---
+++ GameData/Islands [4872321990]/reward-meta.lua
@@ -0,0 +1,13 @@
+data = {
+    ["RewardKey"] = {["TRAVELLING_MECHANIC"] = 1, ["TRAVELLING_TOTEM"] = 0},
+    ["RewardMeta"] = {
+        {
+            ["rewardKey"] = 1,
+            ["toolRewards"] = {
+                {["amount"] = 8, ["toolName"] = "conveyorForward"},
+                {["amount"] = 1, ["toolName"] = "chestMediumIndustrial"}
+            }
+        },
+        {["rewardKey"] = 0, ["toolRewards"] = {{["amount"] = 1, ["toolName"] = "totemIron"}}}
+    }
+}
