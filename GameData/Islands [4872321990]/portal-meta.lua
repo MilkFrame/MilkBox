75cebcf97248d616f14a7739223aabcce70fcdf0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:26:07 +1100

    Create portal-meta.lua

---
+++ GameData/Islands [4872321990]/portal-meta.lua
@@ -0,0 +1,229 @@
+data = {
+    ["IslandPortalMeta"] = {
+        ["slime-hub"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "hub-slime",
+            ["id"] = "slime-hub",
+            ["island"] = "slime",
+            ["spawnCFrame"] = CFrame.new(
+                700.096008,
+                198.593994,
+                -216.589996,
+                -0.906307757,
+                0,
+                0.42261833,
+                0,
+                1,
+                0,
+                -0.42261833,
+                0,
+                -0.906307757
+            )
+        },
+        ["butterfly_festival-hub"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "hub-festival",
+            ["id"] = "butterfly_festival-hub",
+            ["island"] = "butterfly_festival",
+            ["spawnCFrame"] = CFrame.new(-3596.7771, 119.278, -2578.01294, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["hub-maple"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "maple-hub",
+            ["id"] = "hub-maple",
+            ["island"] = "hub",
+            ["spawnCFrame"] = CFrame.new(714.570984, 44.5040016, -631.359009, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["spirit-slime"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "slime-spirit",
+            ["id"] = "spirit-slime",
+            ["island"] = "spirit",
+            ["spawnCFrame"] = CFrame.new(34.3149986, 290.803986, 856.348999, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["mapleTravel"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "maple",
+            ["id"] = "mapleTravel",
+            ["island"] = "hub",
+            ["spawnCFrame"] = CFrame.new(-814.995972, 246.748993, -718.96698, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["farming_festival-hub"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "hub-festival",
+            ["id"] = "farming_festival-hub",
+            ["island"] = "farming_festival",
+            ["spawnCFrame"] = CFrame.new(
+                -2265.81909,
+                192.645996,
+                -682.112,
+                -4.37113883e-08,
+                0,
+                1,
+                0,
+                1,
+                0,
+                -1,
+                0,
+                -4.37113883e-08
+            )
+        },
+        ["wizard-desert"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "desert-wizard",
+            ["id"] = "wizard-desert",
+            ["island"] = "wizard",
+            ["spawnCFrame"] = CFrame.new(1493.30005, 336.781006, -877.151978, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["buffalkor-diamond_mine"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "diamond_mine-buffalkor",
+            ["id"] = "buffalkor-diamond_mine",
+            ["island"] = "buffalkor",
+            ["spawnCFrame"] = CFrame.new(1577.92297, 209.658005, 123.737999, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["pirateTravelHub"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "pirateTravel",
+            ["id"] = "pirateTravelHub",
+            ["island"] = "pirate",
+            ["spawnCFrame"] = CFrame.new(-284.975006, 365.919006, -2000.05396, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["diamond_mine-buffalkor"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "buffalkor-diamond_mine",
+            ["id"] = "diamond_mine-buffalkor",
+            ["island"] = "diamond_mine",
+            ["spawnCFrame"] = CFrame.new(2480.63989, 237.328995, 885.411987, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["buffalkor-slime"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "slime-buffalkor",
+            ["id"] = "buffalkor-slime",
+            ["island"] = "buffalkor",
+            ["spawnCFrame"] = CFrame.new(1291.22302, 389.753998, 150.356003, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["pirateTravel"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "pirate",
+            ["id"] = "pirateTravel",
+            ["island"] = "hub",
+            ["spawnCFrame"] = CFrame.new(-59.4819984, 36.5900002, -660.14801, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["maple-hub"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "hub-maple",
+            ["id"] = "maple-hub",
+            ["island"] = "maple",
+            ["spawnCFrame"] = CFrame.new(-814.995972, 246.748993, -718.96698, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["buffalkor-wizard"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "wizard-buffalkor",
+            ["id"] = "buffalkor-wizard",
+            ["island"] = "buffalkor",
+            ["spawnCFrame"] = CFrame.new(1700.61206, 442.075012, -193.042007, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["underworld"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "underworld",
+            ["id"] = "underworld",
+            ["island"] = "underworld",
+            ["spawnCFrame"] = CFrame.new(-8221.9043, 284.5, -652.117004, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["mapleTravelHub"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "maple",
+            ["id"] = "mapleTravelHub",
+            ["island"] = "maple",
+            ["spawnCFrame"] = CFrame.new(-814.995972, 246.748993, -718.96698, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["fishing_festival-hub"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "hub-festival",
+            ["id"] = "fishing_festival-hub",
+            ["island"] = "fishing_festival",
+            ["spawnCFrame"] = CFrame.new(
+                -1850.95801,
+                52.4480019,
+                -1615.27197,
+                0.633197784,
+                -0.099586539,
+                0.767556608,
+                -0,
+                0.991688073,
+                0.128666416,
+                -0.773990154,
+                -0.0814712867,
+                0.627934575
+            )
+        },
+        ["wizard-buffalkor"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "buffalkor-wizard",
+            ["id"] = "wizard-buffalkor",
+            ["island"] = "wizard",
+            ["spawnCFrame"] = CFrame.new(1497.53296, 335.48999, -700.820984, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["slime-buffalkor"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "buffalkor-slime",
+            ["id"] = "slime-buffalkor",
+            ["island"] = "slime",
+            ["spawnCFrame"] = CFrame.new(878.237976, 178.065994, 11.4890003, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["slime-spirit"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "spirit-slime",
+            ["id"] = "slime-spirit",
+            ["island"] = "slime",
+            ["spawnCFrame"] = CFrame.new(681.187012, 181.598999, -141.673004, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["desert-wizard"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "wizard-desert",
+            ["id"] = "desert-wizard",
+            ["island"] = "desert",
+            ["spawnCFrame"] = CFrame.new(1112.92603, 283.309998, -1738.02698, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        },
+        ["hub-festival"] = {
+            ["id"] = "hub-festival",
+            ["type"] = 1,
+            ["island"] = "hub",
+            ["spawnCFrame"] = CFrame.new(
+                247.738007,
+                30.3780003,
+                -782.59198,
+                -4.37113883e-08,
+                0,
+                1,
+                0,
+                1,
+                0,
+                -1,
+                0,
+                -4.37113883e-08
+            )
+        },
+        ["hub-slime"] = {
+            ["type"] = 0,
+            ["destinationPortal"] = "slime-hub",
+            ["spawnCFrame"] = CFrame.new(
+                166.531006,
+                33,
+                -739.296997,
+                0.707106769,
+                0,
+                -0.707106769,
+                0,
+                1,
+                0,
+                0.707106769,
+                0,
+                0.707106769
+            ),
+            ["island"] = "hub",
+            ["id"] = "hub-slime"
+        }
+    }
+}
