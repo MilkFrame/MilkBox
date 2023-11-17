0f57f554f95d4ced08e3b011c641ef45100a4c1a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:13:36 +1100

    Create potion-speed-meta.lua

---
+++ GameData/Islands [4872321990]/potion-speed-meta.lua
@@ -0,0 +1,16 @@
+data = {
+    ["PotionSpeedMultiplier"] = {
+        ["broom"] = 0.5,
+        ["duck"] = 0.25,
+        ["yak"] = 0.25,
+        ["sweet_rice_dessert_speed"] = 0.3,
+        ["hoverboard"] = 0.65,
+        ["potato_duck_egg_scramble_speed"] = 0.4,
+        ["vulture"] = 0.5,
+        ["slowed"] = -0.3,
+        ["chili_pepper_speed"] = 0.25,
+        ["horse"] = 0.75,
+        ["witch_broom"] = 0.65,
+        ["stun"] = -0.75
+    }
+}
