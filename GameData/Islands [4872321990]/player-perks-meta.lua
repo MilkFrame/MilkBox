ae6d4d052f6131982a35cc94a696a9f72a146c41
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:12:47 +1100

    Create player-perks-meta.lua

---
+++ GameData/Islands [4872321990]/player-perks-meta.lua
@@ -0,0 +1,97 @@
+data = {
+    ["PlayerPerkType"] = {
+        ["UTILITY"] = "UTILITY",
+        ["DEFENSE"] = "DEFENSE",
+        ["DEVELOPER"] = "DEVELOPER",
+        ["OFFENSE"] = "OFFENSE"
+    },
+    ["PlayerPerkUpdateType"] = {["REMOVED"] = "REMOVED", ["ADDED"] = "ADDED"},
+    ["convertPerkTypeToNumber"] = "function: 0x0000000050b56716",
+    ["PlayerPerkMeta"] = {
+        ["EMPOWERED"] = {
+            ["perkType"] = "OFFENSE",
+            ["description"] = "Deal 25% more damage.",
+            ["amount"] = 0.25,
+            ["gameTypes"] = {"snowball_fight"},
+            ["duration"] = 22,
+            ["icon"] = "rbxassetid://11817701997",
+            ["displayName"] = "Empowered"
+        },
+        ["DEV_HASTE"] = {
+            ["perkType"] = "DEVELOPER",
+            ["description"] = "Gotta Go Fast.",
+            ["amount"] = 4,
+            ["duration"] = 120,
+            ["icon"] = "rbxassetid://11817702031",
+            ["displayName"] = "Developer Haste"
+        },
+        ["HEALER"] = {
+            ["perkType"] = "DEFENSE",
+            ["description"] = "Hitting enemies will trigger aoe heal for teammates in range.",
+            ["amount"] = 0.12,
+            ["gameTypes"] = {"snowball_fight"},
+            ["duration"] = 16,
+            ["icon"] = "rbxassetid://11817702056",
+            ["displayName"] = "Healer"
+        },
+        ["HASTE"] = {
+            ["perkType"] = "UTILITY",
+            ["description"] = "Gain increased movement speed.",
+            ["amount"] = 0.65,
+            ["gameTypes"] = {"snowball_fight"},
+            ["duration"] = 18,
+            ["icon"] = "rbxassetid://11817702031",
+            ["displayName"] = "Haste"
+        },
+        ["MISFORTUNE"] = {
+            ["perkType"] = "UTILITY",
+            ["description"] = "Enemies hit will receive a debuff that increases incoming damage to them.",
+            ["gameTypes"] = {"snowball_fight"},
+            ["duration"] = 20,
+            ["icon"] = "rbxassetid://11817702117",
+            ["displayName"] = "Misfortune"
+        },
+        ["ADRENALINE"] = {
+            ["perkType"] = "OFFENSE",
+            ["description"] = "The lower your health is, the more damage you'll deal.",
+            ["amount"] = 1.5,
+            ["gameTypes"] = {"snowball_fight"},
+            ["duration"] = 18,
+            ["icon"] = "rbxassetid://11817701954",
+            ["displayName"] = "Adrenaline"
+        },
+        ["LEADER"] = {
+            ["perkType"] = "DEFENSE",
+            ["description"] = "Nearby teammates will take 15% less damage.",
+            ["amount"] = 0.12,
+            ["gameTypes"] = {"snowball_fight"},
+            ["duration"] = 21,
+            ["icon"] = "rbxassetid://11817702086",
+            ["displayName"] = "Leader"
+        },
+        ["DEV_DAMAGE"] = {
+            ["perkType"] = "DEVELOPER",
+            ["description"] = "Deal 1000% more damage",
+            ["amount"] = 10,
+            ["duration"] = 120,
+            ["icon"] = "rbxassetid://11817701997",
+            ["displayName"] = "Developer Empowered"
+        }
+    },
+    ["PlayerPerk"] = {
+        ["DEV_DAMAGE"] = "DEV_DAMAGE",
+        ["HEALER"] = "HEALER",
+        ["DEV_HASTE"] = "DEV_HASTE",
+        ["HASTE"] = "HASTE",
+        ["MISFORTUNE"] = "MISFORTUNE",
+        ["ADRENALINE"] = "ADRENALINE",
+        ["LEADER"] = "LEADER",
+        ["EMPOWERED"] = "EMPOWERED"
+    },
+    ["PlayerPerkTypeColors"] = {
+        ["UTILITY"] = Color3.fromRGB(255, 191, 0),
+        ["DEFENSE"] = Color3.fromRGB(100, 149, 237),
+        ["DEVELOPER"] = Color3.fromRGB(166, 247, 215),
+        ["OFFENSE"] = Color3.fromRGB(164, 42, 4)
+    }
+}
