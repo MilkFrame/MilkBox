aadd53a8fdd2be63f9221a2dd4b8a7c18c54c056
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:27:32 +1100

    Create season-meta.lua

---
+++ GameData/Islands [4872321990]/season-meta.lua
@@ -0,0 +1,8 @@
+data = {
+    ["SeasonMeta"] = {
+        ["winter"] = {["displayName"] = "Winter"},
+        ["fall"] = {["displayName"] = "Fall"},
+        ["spring"] = {["displayName"] = "Spring"},
+        ["summer"] = {["displayName"] = "Summer"}
+    }
+}
