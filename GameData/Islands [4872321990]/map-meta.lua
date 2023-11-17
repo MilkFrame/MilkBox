f000dab95358a70738b6992ae72a41dec840c2c2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:24:23 +1100

    Create map-meta.lua

---
+++ GameData/Islands [4872321990]/map-meta.lua
@@ -0,0 +1,250 @@
+data = {
+    ["encodePositionWithIsland"] = "function: 0x00000000a6f80126",
+    ["LootableMapMetaKeys"] = {"buffalkor", "maple", "desert", "pirate", "spirit", "hub", "slime", "wizard"},
+    ["MapMetaValues"] = {
+        {
+            ["possibleLoot"] = {
+                ["objects"] = {{["weight"] = 1, ["toolName"] = "treasureChestBuffalkor"}},
+                ["maxElementWeight"] = 1,
+                ["cumulativeWeight"] = 1
+            },
+            ["mapAsset"] = "rbxassetid://9379678130",
+            ["islandLocation"] = "Buffalkor Island",
+            ["origin"] = Vector3.new(1537, 564, 85.5),
+            ["id"] = "buffalkor",
+            ["weight"] = 1,
+            ["pixelStudRatio"] = 1,
+            ["size"] = "906, 867"
+        },
+        {
+            ["possibleLoot"] = {
+                ["objects"] = {{["weight"] = 1, ["toolName"] = "treasureChestMaple"}},
+                ["maxElementWeight"] = 1,
+                ["cumulativeWeight"] = 1
+            },
+            ["mapAsset"] = "rbxassetid://9439235619",
+            ["islandLocation"] = "Maple Island",
+            ["origin"] = Vector3.new(-1019, 431, -1029),
+            ["id"] = "maple",
+            ["weight"] = 1,
+            ["pixelStudRatio"] = 0.8,
+            ["size"] = "880, 659"
+        },
+        {
+            ["possibleLoot"] = {
+                ["objects"] = {{["weight"] = 1, ["toolName"] = "treasureChestDesert"}},
+                ["maxElementWeight"] = 1,
+                ["cumulativeWeight"] = 1
+            },
+            ["mapAsset"] = "rbxassetid://9379678669",
+            ["islandLocation"] = "Desert Island",
+            ["origin"] = Vector3.new(844.5, 374, -1924.5),
+            ["id"] = "desert",
+            ["weight"] = 1,
+            ["pixelStudRatio"] = 1,
+            ["size"] = "915, 935"
+        },
+        {
+            ["islandLocation"] = "The Underworld",
+            ["origin"] = Vector3.new(0, 0, 0),
+            ["id"] = "underworld",
+            ["weight"] = 1,
+            ["pixelStudRatio"] = 1,
+            ["size"] = "0, 0"
+        },
+        {
+            ["possibleLoot"] = {
+                ["objects"] = {{["weight"] = 1, ["toolName"] = "treasureChest"}},
+                ["maxElementWeight"] = 1,
+                ["cumulativeWeight"] = 1
+            },
+            ["mapAsset"] = "rbxassetid://9439236215",
+            ["islandLocation"] = "Pirate Island",
+            ["origin"] = Vector3.new(-352, 659, -2500),
+            ["id"] = "pirate",
+            ["weight"] = 2,
+            ["pixelStudRatio"] = 0.65,
+            ["size"] = "883, 945"
+        },
+        {
+            ["possibleLoot"] = {
+                ["objects"] = {{["weight"] = 1, ["toolName"] = "treasureChestSpirit"}},
+                ["maxElementWeight"] = 1,
+                ["cumulativeWeight"] = 1
+            },
+            ["mapAsset"] = "rbxassetid://9379679455",
+            ["islandLocation"] = "Spirit Island",
+            ["origin"] = Vector3.new(-303, 564, 946),
+            ["id"] = "spirit",
+            ["weight"] = 1,
+            ["pixelStudRatio"] = 1,
+            ["size"] = "792, 878"
+        },
+        {
+            ["possibleLoot"] = {
+                ["objects"] = {{["weight"] = 1, ["toolName"] = "treasureChest"}},
+                ["maxElementWeight"] = 1,
+                ["cumulativeWeight"] = 1
+            },
+            ["mapAsset"] = "rbxassetid://10162085841",
+            ["islandLocation"] = "The Hub",
+            ["origin"] = Vector3.new(288, 436, -884.5),
+            ["id"] = "hub",
+            ["weight"] = 2,
+            ["pixelStudRatio"] = 0.5,
+            ["size"] = "739, 769"
+        },
+        {
+            ["possibleLoot"] = {
+                ["objects"] = {{["weight"] = 1, ["toolName"] = "treasureChestSlime"}},
+                ["maxElementWeight"] = 1,
+                ["cumulativeWeight"] = 1
+            },
+            ["mapAsset"] = "rbxassetid://10162086307",
+            ["islandLocation"] = "Slime Island",
+            ["origin"] = Vector3.new(702, 319, 264),
+            ["id"] = "slime",
+            ["weight"] = 1,
+            ["pixelStudRatio"] = 0.5,
+            ["size"] = "368, 723"
+        },
+        {
+            ["possibleLoot"] = {
+                ["objects"] = {{["weight"] = 1, ["toolName"] = "treasureChestWizard"}},
+                ["maxElementWeight"] = 1,
+                ["cumulativeWeight"] = 1
+            },
+            ["mapAsset"] = "rbxassetid://9379679690",
+            ["islandLocation"] = "Wizard Island",
+            ["origin"] = Vector3.new(1752.5, 668, -886),
+            ["id"] = "wizard",
+            ["weight"] = 1,
+            ["pixelStudRatio"] = 1.25,
+            ["size"] = "886, 903"
+        }
+    },
+    ["MapMeta"] = {
+        ["buffalkor"] = {
+            ["possibleLoot"] = {
+                ["objects"] = {{["weight"] = 1, ["toolName"] = "treasureChestBuffalkor"}},
+                ["maxElementWeight"] = 1,
+                ["cumulativeWeight"] = 1
+            },
+            ["mapAsset"] = "rbxassetid://9379678130",
+            ["islandLocation"] = "Buffalkor Island",
+            ["origin"] = Vector3.new(1537, 564, 85.5),
+            ["id"] = "buffalkor",
+            ["weight"] = 1,
+            ["pixelStudRatio"] = 1,
+            ["size"] = "906, 867"
+        },
+        ["maple"] = {
+            ["possibleLoot"] = {
+                ["objects"] = {{["weight"] = 1, ["toolName"] = "treasureChestMaple"}},
+                ["maxElementWeight"] = 1,
+                ["cumulativeWeight"] = 1
+            },
+            ["mapAsset"] = "rbxassetid://9439235619",
+            ["islandLocation"] = "Maple Island",
+            ["origin"] = Vector3.new(-1019, 431, -1029),
+            ["id"] = "maple",
+            ["weight"] = 1,
+            ["pixelStudRatio"] = 0.8,
+            ["size"] = "880, 659"
+        },
+        ["desert"] = {
+            ["possibleLoot"] = {
+                ["objects"] = {{["weight"] = 1, ["toolName"] = "treasureChestDesert"}},
+                ["maxElementWeight"] = 1,
+                ["cumulativeWeight"] = 1
+            },
+            ["mapAsset"] = "rbxassetid://9379678669",
+            ["islandLocation"] = "Desert Island",
+            ["origin"] = Vector3.new(844.5, 374, -1924.5),
+            ["id"] = "desert",
+            ["weight"] = 1,
+            ["pixelStudRatio"] = 1,
+            ["size"] = "915, 935"
+        },
+        ["underworld"] = {
+            ["islandLocation"] = "The Underworld",
+            ["origin"] = Vector3.new(0, 0, 0),
+            ["id"] = "underworld",
+            ["weight"] = 1,
+            ["pixelStudRatio"] = 1,
+            ["size"] = "0, 0"
+        },
+        ["pirate"] = {
+            ["possibleLoot"] = {
+                ["objects"] = {{["weight"] = 1, ["toolName"] = "treasureChest"}},
+                ["maxElementWeight"] = 1,
+                ["cumulativeWeight"] = 1
+            },
+            ["mapAsset"] = "rbxassetid://9439236215",
+            ["islandLocation"] = "Pirate Island",
+            ["origin"] = Vector3.new(-352, 659, -2500),
+            ["id"] = "pirate",
+            ["weight"] = 2,
+            ["pixelStudRatio"] = 0.65,
+            ["size"] = "883, 945"
+        },
+        ["spirit"] = {
+            ["possibleLoot"] = {
+                ["objects"] = {{["weight"] = 1, ["toolName"] = "treasureChestSpirit"}},
+                ["maxElementWeight"] = 1,
+                ["cumulativeWeight"] = 1
+            },
+            ["mapAsset"] = "rbxassetid://9379679455",
+            ["islandLocation"] = "Spirit Island",
+            ["origin"] = Vector3.new(-303, 564, 946),
+            ["id"] = "spirit",
+            ["weight"] = 1,
+            ["pixelStudRatio"] = 1,
+            ["size"] = "792, 878"
+        },
+        ["hub"] = {
+            ["possibleLoot"] = {
+                ["objects"] = {{["weight"] = 1, ["toolName"] = "treasureChest"}},
+                ["maxElementWeight"] = 1,
+                ["cumulativeWeight"] = 1
+            },
+            ["mapAsset"] = "rbxassetid://10162085841",
+            ["islandLocation"] = "The Hub",
+            ["origin"] = Vector3.new(288, 436, -884.5),
+            ["id"] = "hub",
+            ["weight"] = 2,
+            ["pixelStudRatio"] = 0.5,
+            ["size"] = "739, 769"
+        },
+        ["slime"] = {
+            ["possibleLoot"] = {
+                ["objects"] = {{["weight"] = 1, ["toolName"] = "treasureChestSlime"}},
+                ["maxElementWeight"] = 1,
+                ["cumulativeWeight"] = 1
+            },
+            ["mapAsset"] = "rbxassetid://10162086307",
+            ["islandLocation"] = "Slime Island",
+            ["origin"] = Vector3.new(702, 319, 264),
+            ["id"] = "slime",
+            ["weight"] = 1,
+            ["pixelStudRatio"] = 0.5,
+            ["size"] = "368, 723"
+        },
+        ["wizard"] = {
+            ["possibleLoot"] = {
+                ["objects"] = {{["weight"] = 1, ["toolName"] = "treasureChestWizard"}},
+                ["maxElementWeight"] = 1,
+                ["cumulativeWeight"] = 1
+            },
+            ["mapAsset"] = "rbxassetid://9379679690",
+            ["islandLocation"] = "Wizard Island",
+            ["origin"] = Vector3.new(1752.5, 668, -886),
+            ["id"] = "wizard",
+            ["weight"] = 1,
+            ["pixelStudRatio"] = 1.25,
+            ["size"] = "886, 903"
+        }
+    },
+    ["decodePositionAndIsland"] = "function: 0x000000007c5c881e",
+    ["MapMetaKeys"] = {"buffalkor", "maple", "desert", "underworld", "pirate", "spirit", "hub", "slime", "wizard"}
+}
