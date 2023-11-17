f76fe0c7e685d91cd0ae620d7f45757e02d56a76
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:23:05 +1100

    Create scavenge-meta.lua

---
+++ GameData/Islands [4872321990]/scavenge-meta.lua
@@ -0,0 +1,42 @@
+data = {
+    ["ScavengeItems"] = {
+        ["Entries"] = {
+            {["itemName"] = {"stone"}, ["title"] = "Stone", ["amount"] = 5},
+            {
+                ["itemName"] = {
+                    "fishSalmon",
+                    "fishGrouper",
+                    "fishEel",
+                    "fishCarp",
+                    "fishTrout",
+                    "fishSwordfish",
+                    "fishCatfish",
+                    "fishTalapia",
+                    "fishRedSnapper",
+                    "fishClownfish"
+                },
+                ["title"] = "Fish",
+                ["amount"] = 5
+            },
+            {["itemName"] = {"bread"}, ["title"] = "Bread", ["amount"] = 2},
+            {
+                ["itemName"] = {"slimeBallGreen", "slimeBallBlue", "slimeBallPink"},
+                ["title"] = "Slimeballs",
+                ["amount"] = 10
+            },
+            {
+                ["itemName"] = {
+                    "marinePlant1",
+                    "marinePlant2",
+                    "marinePlant3",
+                    "marinePlant4",
+                    "marinePlant5",
+                    "marinePlant6"
+                },
+                ["title"] = "Coral",
+                ["amount"] = 5
+            },
+            {["itemName"] = {"tomatoHarvested"}, ["title"] = "Tomato", ["amount"] = 1}
+        }
+    }
+}
