0094a80bf6aefa7a69129b6a19b2ccc9ddcc76f3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:19:03 +1100

    Create deeds-meta.lua

---
+++ GameData/Islands [4872321990]/deeds-meta.lua
@@ -0,0 +1,28 @@
+data = {
+    ["DeedMeta"] = {
+        ["oilResourceDeed"] = {
+            ["id"] = "oilResourceDeed",
+            ["name"] = "Oil Resource Deed",
+            ["visualToolName"] = "barrelOil",
+            ["deposit"] = {["id"] = "oilDeposit1"}
+        },
+        ["copperResourceDeed"] = {
+            ["id"] = "copperResourceDeed",
+            ["name"] = "Copper Resource Deed",
+            ["visualToolName"] = "copperOre",
+            ["deposit"] = {["id"] = "copperDeposit1"}
+        },
+        ["goldResourceDeed"] = {
+            ["id"] = "goldResourceDeed",
+            ["name"] = "Gold Resource Deed",
+            ["visualToolName"] = "goldOre",
+            ["deposit"] = {["id"] = "goldDeposit1"}
+        },
+        ["oilResourceDeed2"] = {
+            ["id"] = "oilResourceDeed2",
+            ["name"] = "Large Oil Resource Deed",
+            ["visualToolName"] = "barrelOil",
+            ["deposit"] = {["id"] = "oilDeposit2"}
+        }
+    }
+}
