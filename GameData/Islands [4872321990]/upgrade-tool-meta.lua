b202a0a4197ca7272950de0d2ee8c21f3cd81508
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:29:04 +1100

    Create upgrade-tool-meta.lua

---
+++ GameData/Islands [4872321990]/upgrade-tool-meta.lua
@@ -0,0 +1,52 @@
+data = {
+    ["UpgradeToolMeta"] = {
+        ["upgradePickaxeGilded"] = {
+            ["triggerCost"] = {
+                {["amount"] = 10, ["toolName"] = "diamond"},
+                {["amount"] = 1, ["toolName"] = "gildedSteelPickaxe"}
+            },
+            ["oldTool"] = "gildedSteelPickaxe",
+            ["actionText"] = "Diamond Pickaxe",
+            ["requiresTriggerCost"] = true,
+            ["canBeTriggered"] = "function: 0x00000000c6f7ebe6",
+            ["upgradedTool"] = "diamondPickaxe",
+            ["onTrigger"] = "function: 0x000000002f52102e"
+        },
+        ["upgradeSwordIron"] = {
+            ["triggerCost"] = {
+                {["amount"] = 15, ["toolName"] = "diamond"},
+                {["amount"] = 1, ["toolName"] = "ironSword"}
+            },
+            ["oldTool"] = "ironSword",
+            ["actionText"] = "Diamond Great Sword",
+            ["requiresTriggerCost"] = true,
+            ["canBeTriggered"] = "function: 0x00000000947dcc06",
+            ["upgradedTool"] = "diamondSword2",
+            ["onTrigger"] = "function: 0x000000008da0534e"
+        },
+        ["upgradeSwordWood"] = {
+            ["triggerCost"] = {
+                {["amount"] = 30, ["toolName"] = "ironOre"},
+                {["amount"] = 1, ["toolName"] = "swordWood"}
+            },
+            ["oldTool"] = "swordWood",
+            ["actionText"] = "Iron Sword",
+            ["requiresTriggerCost"] = true,
+            ["canBeTriggered"] = "function: 0x0000000069bcebb6",
+            ["upgradedTool"] = "ironSword",
+            ["onTrigger"] = "function: 0x00000000b15963de"
+        },
+        ["upgradePickaxeIron"] = {
+            ["triggerCost"] = {
+                {["amount"] = 25, ["toolName"] = "ironOre"},
+                {["amount"] = 1, ["toolName"] = "ironPickaxe"}
+            },
+            ["oldTool"] = "ironPickaxe",
+            ["actionText"] = "Gilded Steel Pickaxe",
+            ["requiresTriggerCost"] = true,
+            ["canBeTriggered"] = "function: 0x00000000178a8b56",
+            ["upgradedTool"] = "gildedSteelPickaxe",
+            ["onTrigger"] = "function: 0x000000001fcf037e"
+        }
+    }
+}
