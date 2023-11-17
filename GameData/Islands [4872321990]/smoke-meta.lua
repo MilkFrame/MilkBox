da51998c658cf12c75ebe652d85d780b37496fc3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:16:57 +1100

    Create smoke-meta.lua

---
+++ GameData/Islands [4872321990]/smoke-meta.lua
@@ -0,0 +1,21 @@
+data = {
+    ["SmokeEffectId"] = {["PoisonSmoke"] = "poisonSmoke", ["VoidSmoke"] = "voidSmoke"},
+    ["SmokeMetadata"] = {
+        ["poisonSmoke"] = {
+            ["resistanceEffects"] = {["toxin_resistance"] = 0.25},
+            ["effectModelName"] = "poisonSmoke",
+            ["id"] = "poisonSmoke",
+            ["collectionServiceTags"] = {"poison-smoke"},
+            ["damageRate"] = 1,
+            ["damage"] = 10
+        },
+        ["voidSmoke"] = {
+            ["resistanceEffects"] = {["smoke_resistance"] = 0.25},
+            ["effectModelName"] = "voidSmoke",
+            ["id"] = "voidSmoke",
+            ["collectionServiceTags"] = {"void-smoke"},
+            ["damageRate"] = 1,
+            ["damage"] = 7
+        }
+    }
+}
