aa620db048652358d5154237cda0e600974c0af1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:25:13 +1100

    Create mount-meta.lua

---
+++ GameData/Islands [4872321990]/mount-meta.lua
@@ -0,0 +1,46 @@
+data = {
+    ["MountMeta"] = {
+        ["witchBroom"] = {
+            ["mountDescription"] = "+65% movement speed [Limited Offer]",
+            ["potionEffects"] = {"witch_broom"},
+            ["playerAnimation"] = "rbxassetid://11157452238",
+            ["mountType"] = "witchBroom",
+            ["encodeId"] = 0,
+            ["mountSeatOffset"] = Vector3.new(0, 0.75, 0.25),
+            ["hipHeightOffset"] = 4,
+            ["displayName"] = "Witch's Broom"
+        },
+        ["broom"] = {
+            ["mountDescription"] = "+50% movement speed",
+            ["potionEffects"] = {"broom"},
+            ["playerAnimation"] = "rbxassetid://11157452238",
+            ["mountType"] = "broom",
+            ["encodeId"] = 1,
+            ["mountSeatOffset"] = Vector3.new(0, 0.5, 0.25),
+            ["hipHeightOffset"] = 4,
+            ["displayName"] = "Broom"
+        }
+    },
+    ["MountEncodeMeta"] = {
+        {
+            ["mountDescription"] = "+50% movement speed",
+            ["potionEffects"] = {"broom"},
+            ["playerAnimation"] = "rbxassetid://11157452238",
+            ["mountType"] = "broom",
+            ["encodeId"] = 1,
+            ["mountSeatOffset"] = Vector3.new(0, 0.5, 0.25),
+            ["hipHeightOffset"] = 4,
+            ["displayName"] = "Broom"
+        },
+        {
+            ["mountDescription"] = "+65% movement speed [Limited Offer]",
+            ["potionEffects"] = {"witch_broom"},
+            ["playerAnimation"] = "rbxassetid://11157452238",
+            ["mountType"] = "witchBroom",
+            ["encodeId"] = 0,
+            ["mountSeatOffset"] = Vector3.new(0, 0.75, 0.25),
+            ["hipHeightOffset"] = 4,
+            ["displayName"] = "Witch's Broom"
+        }
+    }
+}
