77f4eb39d5463352b5473d74f311eafd5cfabd2f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:26:56 +1100

    Create quest-series-meta.lua

---
+++ GameData/Islands [4872321990]/quest-series-meta.lua
@@ -0,0 +1,53 @@
+data = {
+    ["QuestSeriesId"] = {
+        ["VoidIsland"] = "void",
+        ["Christmas2022"] = "christmas2022",
+        ["AnniversaryYear3"] = "anniversaryYear3"
+    },
+    ["QuestSeriesMeta"] = {
+        ["void"] = {
+            ["islandId"] = {"void", "desert"},
+            ["title"] = "The Void Isles",
+            ["id"] = "void",
+            ["priority"] = 0,
+            ["colourScheme"] = Color3.fromRGB(128, 94, 175)
+        },
+        ["christmas2022"] = {
+            ["islandId"] = {
+                "slime",
+                "buffalkor",
+                "diamond_mine",
+                "maple",
+                "pirate",
+                "wizard",
+                "desert",
+                "void",
+                "underworld",
+                "hub"
+            },
+            ["title"] = "A Very Merry Day!",
+            ["id"] = "christmas2022",
+            ["priority"] = 0,
+            ["colourScheme"] = Color3.fromRGB(186, 48, 41)
+        },
+        ["anniversaryYear3"] = {
+            ["islandId"] = {
+                "slime",
+                "buffalkor",
+                "diamond_mine",
+                "maple",
+                "pirate",
+                "wizard",
+                "desert",
+                "void",
+                "underworld",
+                "hub"
+            },
+            ["title"] = "3 Years of Islands!",
+            ["id"] = "anniversaryYear3",
+            ["priority"] = 0,
+            ["colourScheme"] = Color3.fromRGB(252, 41, 224)
+        }
+    },
+    ["QuestSeriesPriority"] = {["Normal"] = 5, ["High"] = 0, ["Low"] = 10}
+}
