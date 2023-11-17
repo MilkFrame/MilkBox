ad840e3abe365082857023b0b52250e6d7c73455
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:28:05 +1100

    Create tool-meta-types.lua

---
+++ GameData/Islands [4872321990]/tool-meta-types.lua
@@ -0,0 +1,31 @@
+data = {
+    ["ThrowableType"] = {["SNOWBALL"] = "SNOWBALL"},
+    ["BowType"] = {["LONG"] = "LONG", ["SHORT"] = "SHORT", ["CROSSBOW"] = "CROSSBOW"},
+    ["EventExclusiveRefund"] = {["Coins"] = {["key"] = "type", ["type"] = "Coins"}},
+    ["ToolCategory"] = {
+        ["UTILITY"] = 4,
+        ["MISC"] = 3,
+        ["WORKBENCH"] = 6,
+        ["CROP"] = 1,
+        ["BLOCK"] = 0,
+        ["FOOD"] = 8,
+        ["FUN"] = 7,
+        ["FACTORY"] = 5,
+        ["TOOL"] = 2
+    },
+    ["ArrowTypeName"] = {["LARGE"] = "Long Arrow", ["SMALL"] = "Short Arrow", ["BOLT"] = "Crossbow Bolt"},
+    ["DamageFalloffs"] = {["LONG"] = 55, ["CROSSBOW"] = 45, ["SHORT"] = 35, ["SNOWBALL"] = 45},
+    ["ToolMagicItemAttackKind"] = {
+        ["Projectile"] = {["key"] = "type", ["type"] = "Projectile"},
+        ["WeightedRandomProjectile"] = {["key"] = "type", ["type"] = "WeightedRandomProjectile"}
+    },
+    ["DefaultAttackRates"] = {["Light_Melee"] = 0.5, ["Archery"] = 0.55, ["Heavy_Melee"] = 0.5, ["Magic"] = 0.14},
+    ["MagicType"] = {["STAFF"] = "STAFF", ["SPELLBOOK"] = "SPELLBOOK"},
+    ["WeaponType"] = {
+        ["HEAVY_MELEE"] = "Heavy_Melee",
+        ["LIGHT_MELEE"] = "Light_Melee",
+        ["ARCHERY"] = "Archery",
+        ["MAGIC"] = "Magic"
+    },
+    ["ArrowType"] = {["LONG"] = "LARGE", ["SHORT"] = "SMALL", ["BOLT"] = "BOLT"}
+}
