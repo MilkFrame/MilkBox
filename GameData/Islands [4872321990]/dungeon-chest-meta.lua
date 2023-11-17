ffe54942670885d5db2fd2afaad8fbcf6704eafd
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:28:32 +1100

    Create dungeon-chest-meta.lua

---
+++ GameData/Islands [4872321990]/dungeon-chest-meta.lua
@@ -0,0 +1,12 @@
+data = {
+    ["DungeonChestMeta"] = {
+        ["slimeDungeonChest2"] = {
+            ["rewards"] = {{["amount"] = "15 25 ", ["reward"] = "dungeonCoin"}},
+            ["actionText"] = "Open Chest"
+        },
+        ["slimeDungeonChest1"] = {
+            ["rewards"] = {{["amount"] = "15 25 ", ["reward"] = "dungeonCoin"}},
+            ["actionText"] = "Open Chest"
+        }
+    }
+}
