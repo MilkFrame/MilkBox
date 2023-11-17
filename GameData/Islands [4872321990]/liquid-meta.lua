beeec4ea1481a0d3f0001b7799b534e877713999
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:23:58 +1100

    Create liquid-meta.lua

---
+++ GameData/Islands [4872321990]/liquid-meta.lua
@@ -0,0 +1,30 @@
+data = {
+    ["LiquidMeta"] = {
+        ["oil"] = {
+            ["color"] = Color3.fromRGB(8, 8, 8),
+            ["transparency"] = 0,
+            ["liquidType"] = "oil",
+            ["liquidTool"] = "fuelOil"
+        },
+        ["sapMaple"] = {
+            ["transparency"] = 0.2,
+            ["liquidType"] = "sapMaple",
+            ["color"] = Color3.fromRGB(255, 224, 94),
+            ["material"] = Enum.Material.Glass,
+            ["liquidTool"] = "sapMaple"
+        },
+        ["petroleum"] = {
+            ["color"] = Color3.fromRGB(248, 217, 109),
+            ["transparency"] = 0,
+            ["liquidType"] = "petroleum",
+            ["liquidTool"] = "fuelPetroleum"
+        },
+        ["mapleSyrup"] = {
+            ["transparency"] = 0.1,
+            ["liquidType"] = "mapleSyrup",
+            ["color"] = Color3.fromRGB(197, 115, 18),
+            ["material"] = Enum.Material.Glass,
+            ["liquidTool"] = "mapleSyrup"
+        }
+    }
+}
