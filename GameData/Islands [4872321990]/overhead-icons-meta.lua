29889d60202e2a1a9f0f078b96261286cf2bc1e1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:25:31 +1100

    Create overhead-icons-meta.lua

---
+++ GameData/Islands [4872321990]/overhead-icons-meta.lua
@@ -0,0 +1,10 @@
+data = {
+    ["PermissionRankIcon"] = {"BUILDER", "OWNER"},
+    ["OverheadIconsMeta"] = {
+        ["BUILDER"] = {["icon"] = "Builder", ["color"] = Color3.fromRGB(61, 255, 74)},
+        ["OWNER"] = {["icon"] = "Owner", ["color"] = Color3.fromRGB(255, 194, 61)},
+        ["PVP"] = {["icon"] = "PVP", ["color"] = Color3.fromRGB(201, 59, 23)}
+    },
+    ["OverheadIcons"] = {["BUILDER"] = "BUILDER", ["OWNER"] = "OWNER", ["PVP"] = "PVP"},
+    ["OverheadIconVisibility"] = {["OTHERS"] = "OTHERS", ["ALL"] = "ALL"}
+}
