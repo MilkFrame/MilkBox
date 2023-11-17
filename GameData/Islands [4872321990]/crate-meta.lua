18efaf5449107a77fc0e438c5c2fec64bfd92c35
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:18:47 +1100

    Create crate-meta.lua

---
+++ GameData/Islands [4872321990]/crate-meta.lua
@@ -0,0 +1,258 @@
+data = {
+    ["CrateMeta"] = {
+        ["CAULDRON_PET"] = {
+            ["objects"] = {
+                {["amount"] = "10 25 ", ["weight"] = 30, ["rarity"] = "COMMON", ["toolName"] = "pumpkinSeeds"},
+                {["amount"] = "5 6 ", ["weight"] = 30, ["rarity"] = "COMMON", ["toolName"] = "cobweb"},
+                {["amount"] = "20 30 ", ["weight"] = 25, ["rarity"] = "COMMON", ["toolName"] = "pumpkinHarvested"},
+                {["amount"] = "5 10 ", ["weight"] = 20, ["rarity"] = "COMMON", ["toolName"] = "jackOLantern"},
+                {["weight"] = 8, ["rarity"] = "UNCOMMON", ["toolName"] = "furnitureCandyBasket"},
+                {["weight"] = 8, ["rarity"] = "UNCOMMON", ["toolName"] = "furnitureGravestone"},
+                {["weight"] = 8, ["rarity"] = "UNCOMMON", ["toolName"] = "furnitureLanternHalloween"},
+                {["weight"] = 8, ["rarity"] = "UNCOMMON", ["toolName"] = "furniturePumpkinCat"},
+                {["weight"] = 8, ["rarity"] = "UNCOMMON", ["toolName"] = "furnitureLanternGhost"},
+                {["weight"] = 8, ["rarity"] = "UNCOMMON", ["toolName"] = "furniturePumpkinSpooky"},
+                {["weight"] = 5, ["rarity"] = "LEGENDARY", ["toolName"] = "trophyHalloween"},
+                {["weight"] = 3, ["rarity"] = "RARE", ["toolName"] = "furniturePumpkinBed"},
+                {["weight"] = 0.5, ["rarity"] = "MYTHIC", ["toolName"] = "pumpkinHammer"},
+                {["weight"] = 0.3, ["rarity"] = "MYTHIC", ["toolName"] = "spawnEggPumpkinBoss"}
+            },
+            ["maxElementWeight"] = 0,
+            ["cumulativeWeight"] = 161.8
+        },
+        ["SERPENT_EGG"] = {
+            ["objects"] = {
+                {["amount"] = "10 30 ", ["weight"] = 50, ["rarity"] = "COMMON", ["toolName"] = "amethystCrystal"},
+                {["amount"] = "10 30 ", ["weight"] = 10, ["rarity"] = "UNCOMMON", ["toolName"] = "voidBossToken"},
+                {["amount"] = "2 3 ", ["weight"] = 5, ["rarity"] = "RARE", ["toolName"] = "fishVoid"},
+                {["weight"] = 3, ["rarity"] = "RARE", ["toolName"] = "voidMattockHilt"},
+                {["weight"] = 0.8, ["rarity"] = "RARE", ["toolName"] = "voidParasiteSeeds"},
+                {["weight"] = 0.2, ["rarity"] = "MYTHIC", ["toolName"] = "spawnEggVoidSerpent"}
+            },
+            ["maxElementWeight"] = 0,
+            ["cumulativeWeight"] = 69
+        },
+        ["TREASURE_CHEST_SLIME"] = {
+            ["objects"] = {
+                {["weight"] = 0.7, ["rarity"] = "COMMON", ["toolName"] = "stickyGearGreen"},
+                {["weight"] = 0.25, ["rarity"] = "UNCOMMON", ["toolName"] = "stickyGearBlue"},
+                {["weight"] = 0.05, ["rarity"] = "MYTHIC", ["toolName"] = "stickyGearPink"}
+            },
+            ["maxElementWeight"] = 0,
+            ["cumulativeWeight"] = 1
+        },
+        ["CAULDRON_2022"] = {
+            ["objects"] = {
+                {["amount"] = "7 8 ", ["weight"] = 25, ["rarity"] = "UNCOMMON", ["toolName"] = "furnitureCandyBasket1"},
+                {["amount"] = "4 6 ", ["weight"] = 20, ["rarity"] = "COMMON", ["toolName"] = "ghost2"},
+                {["amount"] = "4 6 ", ["weight"] = 20, ["rarity"] = "COMMON", ["toolName"] = "ghost1"},
+                {["amount"] = "4 6 ", ["weight"] = 20, ["rarity"] = "COMMON", ["toolName"] = "ghost3"},
+                {["amount"] = "7 8 ", ["weight"] = 20, ["rarity"] = "COMMON", ["toolName"] = "ghostGroup"},
+                {["weight"] = 1, ["rarity"] = "MYTHIC", ["toolName"] = "reaperScythe"},
+                {["weight"] = 1, ["rarity"] = "MYTHIC", ["toolName"] = "reaperCrossbow"},
+                {["weight"] = 0.8, ["rarity"] = "LEGENDARY", ["toolName"] = "halloweenEventTrophy2022"},
+                {["weight"] = 0.6, ["rarity"] = "MYTHIC", ["toolName"] = "spawnEggSpider"}
+            },
+            ["maxElementWeight"] = 0,
+            ["cumulativeWeight"] = 108.39999999999999
+        },
+        ["TREASURE_CHEST_BUFFALKOR"] = {
+            ["objects"] = {
+                {["amount"] = "20 50 ", ["weight"] = 0.6, ["rarity"] = "COMMON", ["toolName"] = "iron"},
+                {["amount"] = "15 25 ", ["weight"] = 0.2, ["rarity"] = "UNCOMMON", ["toolName"] = "gold"},
+                {["amount"] = "10 20 ", ["weight"] = 0.1, ["rarity"] = "RARE", ["toolName"] = "electrite"},
+                {["weight"] = 0.05, ["rarity"] = "LEGENDARY", ["toolName"] = "ironCrystal"},
+                {["weight"] = 0.04, ["rarity"] = "LEGENDARY", ["toolName"] = "goldCrystal"},
+                {["weight"] = 0.01, ["rarity"] = "MYTHIC", ["toolName"] = "buffalkorCrystal"}
+            },
+            ["maxElementWeight"] = 0,
+            ["cumulativeWeight"] = 1
+        },
+        ["TREASURE_CHEST_DESERT"] = {
+            ["objects"] = {
+                {["weight"] = 0.6, ["rarity"] = "COMMON", ["toolName"] = "skorpClawGold"},
+                {["weight"] = 0.2, ["rarity"] = "UNCOMMON", ["toolName"] = "cactusSeeds"},
+                {["weight"] = 0.1, ["rarity"] = "LEGENDARY", ["toolName"] = "skorpClawRuby"},
+                {["weight"] = 0.05, ["rarity"] = "MYTHIC", ["toolName"] = "skorpStingerRuby"},
+                {["weight"] = 0.05, ["rarity"] = "MYTHIC", ["toolName"] = "skorpStingerRuby"}
+            },
+            ["maxElementWeight"] = 0,
+            ["cumulativeWeight"] = 1
+        },
+        ["TREASURE_CHEST_PET"] = {
+            ["objects"] = {
+                {["weight"] = 50, ["rarity"] = "UNCOMMON", ["toolName"] = "crystalizedObsidian"},
+                {["weight"] = 50, ["rarity"] = "UNCOMMON", ["toolName"] = "fishShark"},
+                {["weight"] = 50, ["rarity"] = "COMMON", ["toolName"] = "fishPuffer"},
+                {["weight"] = 50, ["rarity"] = "COMMON", ["toolName"] = "furnitureBarrel"},
+                {["weight"] = 40, ["rarity"] = "UNCOMMON", ["toolName"] = "furniturePirateWheel"},
+                {["weight"] = 40, ["rarity"] = "UNCOMMON", ["toolName"] = "furniturePirateLamp"},
+                {["weight"] = 40, ["rarity"] = "UNCOMMON", ["toolName"] = "furniturePirateChandelier"},
+                {["weight"] = 7, ["rarity"] = "MYTHIC", ["toolName"] = "furniturePirateChair"},
+                {["weight"] = 6, ["rarity"] = "RARE", ["toolName"] = "furniturePirateGlobe"},
+                {["weight"] = 5, ["rarity"] = "LEGENDARY", ["toolName"] = "furniturePirateTable"},
+                {["weight"] = 4, ["rarity"] = "MYTHIC", ["toolName"] = "furniturePirateShip"},
+                {["weight"] = 2, ["rarity"] = "LEGENDARY", ["toolName"] = "furnitureCannon"},
+                {["weight"] = 1, ["rarity"] = "MYTHIC", ["toolName"] = "captainsRapier"}
+            },
+            ["maxElementWeight"] = 0,
+            ["cumulativeWeight"] = 345
+        },
+        ["TREASURE_CHEST_MAPLE"] = {
+            ["objects"] = {
+                {["amount"] = "15 25 ", ["weight"] = 0.6, ["rarity"] = "COMMON", ["toolName"] = "mapleSyrupBottle"},
+                {["weight"] = 0.2, ["rarity"] = "UNCOMMON", ["toolName"] = "saplingMaple"},
+                {["weight"] = 0.1, ["rarity"] = "LEGENDARY", ["toolName"] = "furnitureCornucopia"},
+                {["weight"] = 0.08, ["rarity"] = "LEGENDARY", ["toolName"] = "spawnEggTurkeyT1"},
+                {["weight"] = 0.02, ["rarity"] = "MYTHIC", ["toolName"] = "flowerFhanhorn"}
+            },
+            ["maxElementWeight"] = 0,
+            ["cumulativeWeight"] = 1
+        },
+        ["DUNGEON_CHEST"] = {
+            ["objects"] = {
+                {["amount"] = "5 10 ", ["weight"] = 10, ["rarity"] = "COMMON", ["toolName"] = "slimeBallGreen"},
+                {["amount"] = "3 5 ", ["weight"] = 8, ["rarity"] = "COMMON", ["toolName"] = "stickyGearGreen"},
+                {["amount"] = "2 7 ", ["weight"] = 7, ["rarity"] = "COMMON", ["toolName"] = "slimeKingToken"},
+                {["amount"] = "5 8 ", ["weight"] = 5, ["rarity"] = "UNCOMMON", ["toolName"] = "dungeonCoin"},
+                {["amount"] = "2 5 ", ["weight"] = 5, ["rarity"] = "COMMON", ["toolName"] = "slimeQueenToken"},
+                {["weight"] = 2, ["rarity"] = "UNCOMMON", ["toolName"] = "primordialSkullStatue"},
+                {["weight"] = 2, ["rarity"] = "UNCOMMON", ["toolName"] = "queenCrownStatue"},
+                {["weight"] = 2, ["rarity"] = "UNCOMMON", ["toolName"] = "warriorStatue"},
+                {["weight"] = 2, ["rarity"] = "UNCOMMON", ["toolName"] = "kingCrownStatue"},
+                {["weight"] = 0.6, ["rarity"] = "RARE", ["toolName"] = "smallBrazierGold"},
+                {["weight"] = 0.6, ["rarity"] = "RARE", ["toolName"] = "tallBrazierGold"},
+                {["weight"] = 0.3, ["rarity"] = "RARE", ["toolName"] = "slimeMural3Gold"},
+                {["weight"] = 0.3, ["rarity"] = "RARE", ["toolName"] = "slimeMural2Gold"},
+                {["weight"] = 0.3, ["rarity"] = "RARE", ["toolName"] = "slimeMural1Gold"},
+                {["weight"] = 0.3, ["rarity"] = "RARE", ["toolName"] = "slimeMural4Gold"},
+                {["weight"] = 0.3, ["rarity"] = "RARE", ["toolName"] = "slimeMural5Gold"},
+                {["weight"] = 0.04, ["rarity"] = "MYTHIC", ["toolName"] = "primordialPlushie"},
+                {["weight"] = 0.008, ["rarity"] = "LEGENDARY", ["toolName"] = "primordialStatue"}
+            },
+            ["maxElementWeight"] = 0,
+            ["cumulativeWeight"] = 45.74799999999999
+        },
+        ["CAULDRON"] = {
+            ["objects"] = {
+                {["amount"] = "10 25 ", ["weight"] = 30, ["rarity"] = "COMMON", ["toolName"] = "pumpkinSeeds"},
+                {["amount"] = "5 6 ", ["weight"] = 30, ["rarity"] = "COMMON", ["toolName"] = "cobweb"},
+                {["amount"] = "20 30 ", ["weight"] = 25, ["rarity"] = "COMMON", ["toolName"] = "pumpkinHarvested"},
+                {["amount"] = "5 10 ", ["weight"] = 20, ["rarity"] = "COMMON", ["toolName"] = "jackOLantern"},
+                {["weight"] = 8, ["rarity"] = "UNCOMMON", ["toolName"] = "furniturePumpkinSpooky"},
+                {["weight"] = 8, ["rarity"] = "UNCOMMON", ["toolName"] = "furnitureLanternGhost"},
+                {["weight"] = 8, ["rarity"] = "UNCOMMON", ["toolName"] = "furniturePumpkinCat"},
+                {["weight"] = 8, ["rarity"] = "UNCOMMON", ["toolName"] = "furnitureLanternHalloween"},
+                {["weight"] = 8, ["rarity"] = "UNCOMMON", ["toolName"] = "furnitureGravestone"},
+                {["weight"] = 8, ["rarity"] = "UNCOMMON", ["toolName"] = "furnitureCandyBasket"},
+                {["weight"] = 5, ["rarity"] = "LEGENDARY", ["toolName"] = "trophyHalloween"},
+                {["weight"] = 3, ["rarity"] = "RARE", ["toolName"] = "furniturePumpkinBed"},
+                {["weight"] = 0.5, ["rarity"] = "MYTHIC", ["toolName"] = "pumpkinHammer"}
+            },
+            ["maxElementWeight"] = 0,
+            ["cumulativeWeight"] = 161.5
+        },
+        ["TREASURE_CHEST"] = {
+            ["objects"] = {
+                {["weight"] = 50, ["rarity"] = "UNCOMMON", ["toolName"] = "crystalizedObsidian"},
+                {["weight"] = 50, ["rarity"] = "COMMON", ["toolName"] = "furnitureBarrel"},
+                {["weight"] = 50, ["rarity"] = "COMMON", ["toolName"] = "fishPuffer"},
+                {["weight"] = 50, ["rarity"] = "UNCOMMON", ["toolName"] = "fishShark"},
+                {["weight"] = 40, ["rarity"] = "UNCOMMON", ["toolName"] = "furniturePirateChandelier"},
+                {["weight"] = 40, ["rarity"] = "UNCOMMON", ["toolName"] = "furniturePirateWheel"},
+                {["weight"] = 40, ["rarity"] = "UNCOMMON", ["toolName"] = "furniturePirateLamp"},
+                {["weight"] = 7, ["rarity"] = "MYTHIC", ["toolName"] = "furniturePirateChair"},
+                {["weight"] = 6, ["rarity"] = "RARE", ["toolName"] = "furniturePirateGlobe"},
+                {["weight"] = 5, ["rarity"] = "LEGENDARY", ["toolName"] = "furniturePirateTable"},
+                {["weight"] = 4, ["rarity"] = "MYTHIC", ["toolName"] = "furniturePirateShip"},
+                {["weight"] = 2, ["rarity"] = "LEGENDARY", ["toolName"] = "furnitureCannon"},
+                {["weight"] = 1, ["rarity"] = "MYTHIC", ["toolName"] = "captainsRapier"}
+            },
+            ["maxElementWeight"] = 0,
+            ["cumulativeWeight"] = 345
+        },
+        ["DRAGON_EGG_PET"] = {
+            ["objects"] = {
+                {["amount"] = "20 40 ", ["weight"] = 50, ["rarity"] = "COMMON", ["toolName"] = "fishInfernal"},
+                {
+                    ["amount"] = "250 1000 ",
+                    ["weight"] = 25,
+                    ["rarity"] = "COMMON",
+                    ["toolName"] = "underworldDragonToken"
+                },
+                {["amount"] = "10 30 ", ["weight"] = 25, ["rarity"] = "COMMON", ["toolName"] = "opal"},
+                {["weight"] = 6, ["rarity"] = "RARE", ["toolName"] = "opalAxeHilt"},
+                {["weight"] = 5, ["rarity"] = "RARE", ["toolName"] = "infernalFlame"},
+                {["weight"] = 5, ["rarity"] = "RARE", ["toolName"] = "opalPickaxeHilt"},
+                {["weight"] = 4, ["rarity"] = "MYTHIC", ["toolName"] = "opalSwordHilt"},
+                {["weight"] = 0.3, ["rarity"] = "MYTHIC", ["toolName"] = "spawnEggDragon"}
+            },
+            ["maxElementWeight"] = 0,
+            ["cumulativeWeight"] = 120.3
+        },
+        ["DRAGON_EGG"] = {
+            ["objects"] = {
+                {["amount"] = "20 40 ", ["weight"] = 50, ["rarity"] = "COMMON", ["toolName"] = "fishInfernal"},
+                {
+                    ["amount"] = "250 1000 ",
+                    ["weight"] = 25,
+                    ["rarity"] = "COMMON",
+                    ["toolName"] = "underworldDragonToken"
+                },
+                {["amount"] = "10 30 ", ["weight"] = 25, ["rarity"] = "COMMON", ["toolName"] = "opal"},
+                {["weight"] = 6, ["rarity"] = "RARE", ["toolName"] = "opalAxeHilt"},
+                {["weight"] = 5, ["rarity"] = "RARE", ["toolName"] = "infernalFlame"},
+                {["weight"] = 5, ["rarity"] = "RARE", ["toolName"] = "opalPickaxeHilt"},
+                {["weight"] = 4, ["rarity"] = "MYTHIC", ["toolName"] = "opalSwordHilt"}
+            },
+            ["maxElementWeight"] = 0,
+            ["cumulativeWeight"] = 120
+        },
+        ["TREASURE_CHEST_SPIRIT"] = {
+            ["objects"] = {
+                {["amount"] = "2 6 ", ["weight"] = 0.7, ["rarity"] = "COMMON", ["toolName"] = "spiritSeeds"},
+                {["weight"] = 0.25, ["rarity"] = "UNCOMMON", ["toolName"] = "furnitureSpiritLantern"},
+                {["weight"] = 0.04, ["rarity"] = "RARE", ["toolName"] = "furnitureSpiritLightHanging"},
+                {["weight"] = 0.01, ["rarity"] = "MYTHIC", ["toolName"] = "furnitureSpiritStatue"}
+            },
+            ["maxElementWeight"] = 0,
+            ["cumulativeWeight"] = 1
+        },
+        ["TREASURE_CHEST_WIZARD"] = {
+            ["objects"] = {
+                {["amount"] = "45 75 ", ["weight"] = 0.7, ["rarity"] = "COMMON", ["toolName"] = "potionBottle"},
+                {["weight"] = 0.09, ["rarity"] = "RARE", ["toolName"] = "pageRed"},
+                {["weight"] = 0.09, ["rarity"] = "RARE", ["toolName"] = "pageBlue"},
+                {["weight"] = 0.09, ["rarity"] = "RARE", ["toolName"] = "pageGreen"},
+                {["weight"] = 0.03, ["rarity"] = "LEGENDARY", ["toolName"] = "spellbook"},
+                {["weight"] = 0.005, ["rarity"] = "MYTHIC", ["toolName"] = "spellbookAquamarine"}
+            },
+            ["maxElementWeight"] = 0,
+            ["cumulativeWeight"] = 1.005
+        }
+    },
+    ["CrateEntryRarity"] = {
+        ["UNCOMMON"] = "UNCOMMON",
+        ["COMMON"] = "COMMON",
+        ["MYTHIC"] = "MYTHIC",
+        ["RARE"] = "RARE",
+        ["LEGENDARY"] = "LEGENDARY"
+    },
+    ["CrateType"] = {
+        ["CAULDRON_PET"] = "CAULDRON_PET",
+        ["TREASURE_CHEST_BUFFALKOR"] = "TREASURE_CHEST_BUFFALKOR",
+        ["TREASURE_CHEST_SLIME"] = "TREASURE_CHEST_SLIME",
+        ["CAULDRON_2022"] = "CAULDRON_2022",
+        ["SERPENT_EGG"] = "SERPENT_EGG",
+        ["TREASURE_CHEST_DESERT"] = "TREASURE_CHEST_DESERT",
+        ["TREASURE_CHEST_PET"] = "TREASURE_CHEST_PET",
+        ["TREASURE_CHEST_MAPLE"] = "TREASURE_CHEST_MAPLE",
+        ["DUNGEON_CHEST"] = "DUNGEON_CHEST",
+        ["CAULDRON"] = "CAULDRON",
+        ["TREASURE_CHEST"] = "TREASURE_CHEST",
+        ["DRAGON_EGG_PET"] = "DRAGON_EGG_PET",
+        ["DRAGON_EGG"] = "DRAGON_EGG",
+        ["TREASURE_CHEST_SPIRIT"] = "TREASURE_CHEST_SPIRIT",
+        ["TREASURE_CHEST_WIZARD"] = "TREASURE_CHEST_WIZARD"
+    }
+}
