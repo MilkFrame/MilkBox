621ddcd78333017a215540dea79f95eb076df2ee
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:22:31 +1100

    Create island-meta.lua

---
+++ GameData/Islands [4872321990]/island-meta.lua
@@ -0,0 +1,409 @@
+data = {
+    ["IslandPriority"] = {["Low"] = 1, ["High"] = 10, ["Medium"] = 5},
+    ["LocalTeleportId"] = {
+        ["YELLOW"] = "yellow",
+        ["CYAN"] = "cyan",
+        ["RED"] = "red",
+        ["BLUE"] = "blue",
+        ["PINK"] = "pink",
+        ["BLACK"] = "black",
+        ["PURPLE"] = "purple",
+        ["GREEN"] = "green",
+        ["LIME"] = "lime",
+        ["ORANGE"] = "orange"
+    },
+    ["IslandMeta"] = {
+        ["desert"] = {
+            ["wildIsland"] = true,
+            ["boss"] = {
+                {
+                    ["entity"] = "desertBoss",
+                    ["playerTeleportCFrame"] = CFrame.new(
+                        648.677979,
+                        309.350006,
+                        -2058.83008,
+                        1,
+                        0,
+                        0,
+                        0,
+                        1,
+                        0,
+                        0,
+                        0,
+                        1
+                    ),
+                    ["icon"] = {["Image"] = "rbxassetid://9374888881"},
+                    ["displayName"] = "Temple of Bhaa"
+                }
+            },
+            ["islandRadius"] = 650,
+            ["originCFrame"] = CFrame.new(877, 291, -1828, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["spawnCFrame"] = CFrame.new(1112.92603, 283.309998, -1738.02698, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["placeId"] = 12815054798,
+            ["icon"] = {["Image"] = "rbxassetid://10529562216"},
+            ["displayName"] = "The Azarathian Desert"
+        },
+        ["pirate"] = {
+            ["wildIsland"] = true,
+            ["originCFrame"] = CFrame.new(-253.227005, 412.584991, -2291.06299, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["spawnCFrame"] = CFrame.new(-284.975006, 365.919006, -2000.05396, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["placeId"] = 9501318975,
+            ["icon"] = {["Image"] = "rbxassetid://9314156828"},
+            ["displayName"] = "Pirate Island"
+        },
+        ["hub"] = {
+            ["wildIsland"] = false,
+            ["boss"] = {
+                {
+                    ["entity"] = "cletusHalloween",
+                    ["playerTeleportCFrame"] = CFrame.new(
+                        708.934998,
+                        63.8670006,
+                        -501.464996,
+                        1,
+                        0,
+                        0,
+                        0,
+                        1,
+                        0,
+                        0,
+                        0,
+                        1
+                    ),
+                    ["eventDateRange"] = {["fromDate"] = "1665385200000", ["toDate"] = "1667433600000"},
+                    ["displayName"] = "Cletus"
+                }
+            },
+            ["islandRadius"] = 3000,
+            ["originCFrame"] = CFrame.new(317.768005, 22.7380009, -1057.45898, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["spawnCFrame"] = CFrame.new(
+                317.768005,
+                22.7380009,
+                -1057.45898,
+                -0.598604321,
+                0,
+                0.801044881,
+                0,
+                1,
+                0,
+                -0.801044881,
+                0,
+                -0.598604321
+            ),
+            ["placeId"] = 5899156129,
+            ["icon"] = {["Image"] = "rbxassetid://9497998664"},
+            ["displayName"] = "Hub"
+        },
+        ["fhanhorn"] = {
+            ["boss"] = {
+                {
+                    ["entity"] = "deerBoss",
+                    ["playerTeleportCFrame"] = CFrame.new(
+                        -1053.44897,
+                        287.600006,
+                        -1124.38501,
+                        1,
+                        0,
+                        0,
+                        0,
+                        1,
+                        0,
+                        0,
+                        0,
+                        1
+                    ),
+                    ["icon"] = {["Image"] = "rbxassetid://9502115239"},
+                    ["displayName"] = "Fhanhorn's Island"
+                }
+            },
+            ["islandRadius"] = 550,
+            ["originCFrame"] = CFrame.new(-1200.34204, 293.665009, -1112.58301, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["spawnCFrame"] = CFrame.new(-991.880981, 272.893005, -1136.00305, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["placeId"] = 9501318975,
+            ["wildIsland"] = true,
+            ["displayName"] = "Fhanhorn Island"
+        },
+        ["user_island"] = {
+            ["islandRadius"] = 1000,
+            ["wildIsland"] = false,
+            ["placeId"] = 4872321990,
+            ["icon"] = {["Image"] = "rbxassetid://9314157010"},
+            ["displayName"] = "My Island"
+        },
+        ["buffalkor"] = {
+            ["wildIsland"] = true,
+            ["islandRadius"] = 600,
+            ["originCFrame"] = CFrame.new(1528, 392, 74, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["spawnCFrame"] = CFrame.new(1291.22302, 389.753998, 150.356003, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["placeId"] = 9501318975,
+            ["icon"] = {["Image"] = "rbxassetid://9314154794"},
+            ["displayName"] = "Buffalkor Island"
+        },
+        ["game_mode"] = {
+            ["displayName"] = "Match-made Game",
+            ["originCFrame"] = CFrame.new(500, 500, 500, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["islandRadius"] = 1000,
+            ["spawnCFrame"] = CFrame.new(500, 500, 500, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["placeId"] = 11838346571,
+            ["icon"] = {["Image"] = "rbxassetid://13252378129"},
+            ["wildIsland"] = false
+        },
+        ["diamond_mine"] = {
+            ["wildIsland"] = true,
+            ["boss"] = {
+                {
+                    ["entity"] = "golem",
+                    ["playerTeleportCFrame"] = CFrame.new(2896.35596, 286.737, 1174.43799, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+                    ["icon"] = {["Image"] = "rbxassetid://9374889153"},
+                    ["displayName"] = "Kor's Arena"
+                }
+            },
+            ["islandRadius"] = 1000,
+            ["originCFrame"] = CFrame.new(2855.01489, 285.490997, 1176.495, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["spawnCFrame"] = CFrame.new(2480.63989, 237.328995, 885.411987, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["placeId"] = 9501318975,
+            ["icon"] = {["Image"] = "rbxassetid://9314155367"},
+            ["displayName"] = "Diamond Mines"
+        },
+        ["farming_festival"] = {
+            ["wildIsland"] = true,
+            ["festival"] = "farming",
+            ["priority"] = 10,
+            ["originCFrame"] = CFrame.new(-2839.5, 195.858994, -685, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["spawnCFrame"] = CFrame.new(
+                -2265.81909,
+                192.645996,
+                -682.112,
+                -4.37113883e-08,
+                0,
+                1,
+                0,
+                1,
+                0,
+                -1,
+                0,
+                -4.37113883e-08
+            ),
+            ["placeId"] = 7176435327,
+            ["islandRadius"] = 1000,
+            ["displayName"] = "Farming Festival"
+        },
+        ["fishing_festival"] = {
+            ["festival"] = "fishing",
+            ["islandRadius"] = 1000,
+            ["spawnCFrame"] = CFrame.new(
+                -1850.95801,
+                52.4480019,
+                -1615.27197,
+                0.633197784,
+                -0.099586539,
+                0.767556608,
+                -0,
+                0.991688073,
+                0.128666416,
+                -0.773990154,
+                -0.0814712867,
+                0.627934575
+            ),
+            ["placeId"] = 7176435327,
+            ["displayName"] = "Fishing Festival",
+            ["originCFrame"] = CFrame.new(-1850.95801, 52.4480019, -1615.27197, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["priority"] = 10,
+            ["icon"] = {["Image"] = "rbxassetid://10301057624"},
+            ["wildIsland"] = true
+        },
+        ["butterfly_festival"] = {
+            ["festival"] = "butterfly",
+            ["islandRadius"] = 1000,
+            ["spawnCFrame"] = CFrame.new(-3596.7771, 119.278, -2578.01294, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["placeId"] = 7176435327,
+            ["displayName"] = "Butterfly Festival",
+            ["originCFrame"] = CFrame.new(-3596.24707, 119.278, -2600.90405, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["priority"] = 10,
+            ["icon"] = {["Image"] = "rbxassetid://10801722014"},
+            ["wildIsland"] = true
+        },
+        ["maple"] = {
+            ["wildIsland"] = true,
+            ["originCFrame"] = CFrame.new(-855.88501, 247.998993, -947.965027, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["spawnCFrame"] = CFrame.new(-814.995972, 246.748993, -718.96698, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["placeId"] = 9501318975,
+            ["icon"] = {["Image"] = "rbxassetid://9374995691"},
+            ["displayName"] = "Maple Island"
+        },
+        ["spirit"] = {
+            ["wildIsland"] = true,
+            ["islandRadius"] = 625,
+            ["originCFrame"] = CFrame.new(-326.820007, 266.690002, 864.679993, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["spawnCFrame"] = CFrame.new(34.3149986, 290.803986, 856.348999, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["placeId"] = 9501318975,
+            ["icon"] = {["Image"] = "rbxassetid://9314158004"},
+            ["displayName"] = "Spirit Island"
+        },
+        ["slime"] = {
+            ["wildIsland"] = true,
+            ["boss"] = {
+                {
+                    ["entity"] = "slimeKing",
+                    ["playerTeleportCFrame"] = CFrame.new(730.51001, 169.464005, 120.380997, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+                    ["icon"] = {["Image"] = "rbxassetid://9374795456"},
+                    ["displayName"] = "The Slime King"
+                },
+                {
+                    ["entity"] = "slimeQueen",
+                    ["playerTeleportCFrame"] = CFrame.new(
+                        710.473999,
+                        220.022995,
+                        496.653015,
+                        -0.883877456,
+                        0,
+                        0.467718512,
+                        0,
+                        1,
+                        0,
+                        -0.467718512,
+                        0,
+                        -0.883877456
+                    ),
+                    ["icon"] = {["Image"] = "rbxassetid://10080059190"},
+                    ["displayName"] = "The Slime Queen"
+                }
+            },
+            ["islandRadius"] = 800,
+            ["originCFrame"] = CFrame.new(726.192017, 168.104996, 213.084, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["spawnCFrame"] = CFrame.new(
+                700.096008,
+                198.593994,
+                -216.589996,
+                -0.906307757,
+                0,
+                0.42261833,
+                0,
+                1,
+                0,
+                -0.42261833,
+                0,
+                -0.906307757
+            ),
+            ["placeId"] = 9501318975,
+            ["icon"] = {["Image"] = "rbxassetid://9374996177"},
+            ["displayName"] = "Slime Island"
+        },
+        ["void"] = {
+            ["wildIsland"] = true,
+            ["boss"] = {
+                {
+                    ["entity"] = "voidSerpent",
+                    ["playerTeleportCFrame"] = CFrame.new(
+                        -10230.0117,
+                        287.592987,
+                        9406.75391,
+                        1,
+                        0,
+                        0,
+                        0,
+                        1,
+                        0,
+                        0,
+                        0,
+                        1
+                    ),
+                    ["icon"] = {["Image"] = "rbxassetid://10580115418"},
+                    ["displayName"] = "The Serpent's Hold"
+                }
+            },
+            ["islandRadius"] = 2000,
+            ["originCFrame"] = CFrame.new(-10000, 100, 10000, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["spawnCFrame"] = CFrame.new(-10167.9648, 110.334, 10298.7168, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["placeId"] = 10529772199,
+            ["icon"] = {["Image"] = "rbxassetid://10552806261"},
+            ["displayName"] = "Void Isles"
+        },
+        ["underworld"] = {
+            ["wildIsland"] = true,
+            ["boss"] = {
+                {
+                    ["entity"] = "magmaDragon",
+                    ["playerTeleportCFrame"] = CFrame.new(-9193.95508, 404.436005, -1400.25, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+                    ["icon"] = {["Image"] = "rbxassetid://9374889558"},
+                    ["displayName"] = "The Dragon's Arena"
+                }
+            },
+            ["islandRadius"] = 1500,
+            ["originCFrame"] = CFrame.new(-8809.52441, 374.657013, -711.703003, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["spawnCFrame"] = CFrame.new(-8221.9043, 284.5, -652.117004, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["placeId"] = 7456800858,
+            ["icon"] = {["Image"] = "rbxassetid://10529529010"},
+            ["displayName"] = "Underworld"
+        },
+        ["wizard"] = {
+            ["wildIsland"] = true,
+            ["boss"] = {
+                {
+                    ["entity"] = "wizardBoss",
+                    ["playerTeleportCFrame"] = CFrame.new(
+                        1810.03796,
+                        412.544006,
+                        -945.494019,
+                        1,
+                        0,
+                        0,
+                        0,
+                        1,
+                        0,
+                        0,
+                        0,
+                        1
+                    ),
+                    ["icon"] = {["Image"] = "rbxassetid://9374889795"},
+                    ["displayName"] = "The Wizard Boss"
+                }
+            },
+            ["islandRadius"] = 450,
+            ["originCFrame"] = CFrame.new(1680, 433.834015, -840, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["spawnCFrame"] = CFrame.new(1497.53296, 335.48999, -700.820984, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["placeId"] = 9501318975,
+            ["icon"] = {["Image"] = "rbxassetid://9314158677"},
+            ["displayName"] = "Wizard Island"
+        },
+        ["pvp"] = {
+            ["placeId"] = 5980696036,
+            ["originCFrame"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["displayName"] = "PvP Island"
+        }
+    },
+    ["isIslandId"] = "function: 0x000000008a56af9e",
+    ["IsIslandId"] = "function: 0x0000000001141496",
+    ["LOCAL_COLOR_MAP"] = {
+        ["blue"] = Color3.fromRGB(5, 0, 255),
+        ["cyan"] = Color3.fromRGB(0, 255, 242),
+        ["pink"] = Color3.fromRGB(255, 0, 222),
+        ["purple"] = Color3.fromRGB(112, 0, 255),
+        ["black"] = Color3.fromRGB(0, 0, 0),
+        ["green"] = Color3.fromRGB(0, 125, 10),
+        ["lime"] = Color3.fromRGB(10, 255, 0),
+        ["yellow"] = Color3.fromRGB(255, 250, 0),
+        ["orange"] = Color3.fromRGB(255, 122, 0),
+        ["red"] = Color3.fromRGB(255, 0, 0)
+    },
+    ["IslandId"] = {
+        ["SLIME"] = "slime",
+        ["FARMING_FESTIVAL"] = "farming_festival",
+        ["MAPLE"] = "maple",
+        ["DIAMOND_MINE"] = "diamond_mine",
+        ["USER_ISLAND"] = "user_island",
+        ["HUB"] = "hub",
+        ["BUFFALKOR"] = "buffalkor",
+        ["FISHING_FESTIVAL"] = "fishing_festival",
+        ["DESERT"] = "desert",
+        ["WIZARD"] = "wizard",
+        ["BUTTERFLY_FESTIVAL"] = "butterfly_festival",
+        ["PIRATE"] = "pirate",
+        ["PVP"] = "pvp",
+        ["GAME_MODE"] = "game_mode",
+        ["VOID"] = "void",
+        ["FHANHORN"] = "fhanhorn",
+        ["SPIRIT"] = "spirit",
+        ["UNDERWORLD"] = "underworld"
+    }
+}
