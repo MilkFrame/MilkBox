847da9671ccc4a574b53f1d9d61c9efcca00a15a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:27:48 +1100

    Create survey-meta.lua

---
+++ GameData/Islands [4872321990]/survey-meta.lua
@@ -0,0 +1,90 @@
+data = {
+    ["SurveyMeta"] = {
+        ["general-survey-2"] = {
+            ["id"] = "general-survey-2",
+            ["survey"] = {
+                {
+                    ["fieldId"] = "favourite-first",
+                    ["title"] = "What is your favorite play-style for islands?",
+                    ["uniqueValue"] = true,
+                    ["options"] = {
+                        {
+                            ["id"] = "economy-trading",
+                            ["text"] = "Economy & Trading",
+                            ["description"] = "You like the idea of growing your money through trading, shopping and selling."
+                        },
+                        {
+                            ["id"] = "factories-farming",
+                            ["text"] = "Factories & Farming",
+                            ["description"] = "You like the idea of building big farms and automating them through factories."
+                        },
+                        {
+                            ["id"] = "building",
+                            ["text"] = "Building",
+                            ["description"] = "You like the idea of being creative and building cool structures on your island."
+                        },
+                        {
+                            ["id"] = "collecting-scavenging",
+                            ["text"] = "Collecting & Scavenging",
+                            ["description"] = "You like the idea of growing and displaying your collection of items."
+                        },
+                        {
+                            ["id"] = "combat-exploration",
+                            ["text"] = "Combat & Exploration",
+                            ["description"] = "You like the idea of fighting creatures and bosses and exploring different islands."
+                        },
+                        {
+                            ["id"] = "other",
+                            ["text"] = "Other",
+                            ["description"] = "You like to play a different way that isn't listed here. Let us know!",
+                            ["priority"] = 5
+                        }
+                    },
+                    ["type"] = "single-choice"
+                },
+                {
+                    ["fieldId"] = "favourite-second",
+                    ["title"] = "What is your second favorite play-style for islands?",
+                    ["uniqueValue"] = true,
+                    ["options"] = {
+                        {
+                            ["id"] = "economy-trading",
+                            ["text"] = "Economy & Trading",
+                            ["description"] = "You like the idea of growing your money through trading, shopping and selling."
+                        },
+                        {
+                            ["id"] = "factories-farming",
+                            ["text"] = "Factories & Farming",
+                            ["description"] = "You like the idea of building big farms and automating them through factories."
+                        },
+                        {
+                            ["id"] = "building",
+                            ["text"] = "Building",
+                            ["description"] = "You like the idea of being creative and building cool structures on your island."
+                        },
+                        {
+                            ["id"] = "collecting-scavenging",
+                            ["text"] = "Collecting & Scavenging",
+                            ["description"] = "You like the idea of growing and displaying your collection of items."
+                        },
+                        {
+                            ["id"] = "combat-exploration",
+                            ["text"] = "Combat & Exploration",
+                            ["description"] = "You like the idea of fighting creatures and bosses and exploring different islands."
+                        },
+                        {
+                            ["id"] = "other",
+                            ["text"] = "Other",
+                            ["description"] = "You like to play a different way that isn't listed here. Let us know!",
+                            ["priority"] = 5
+                        }
+                    },
+                    ["type"] = "single-choice"
+                },
+                {["type"] = "submit", ["title"] = "Thanks for your feeback!", ["body"] = "function: 0x00000000565e6336"}
+            },
+            ["title"] = "Islands Survey",
+            ["rewardCoins"] = 200
+        }
+    }
+}
