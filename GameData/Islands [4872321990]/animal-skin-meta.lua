eb98509e0e2dbc87415445031ebd0b298f1813fd
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-11-17 22:29:52 +1100

    Create animal-skin-meta.lua

---
+++ GameData/Islands [4872321990]/animal-skin-meta.lua
@@ -0,0 +1,28 @@
+data = {
+    ["AnimalSkinMeta"] = {
+        ["chicken_tuxedo"] = {
+            ["entityType"] = "chicken",
+            ["title"] = "Tuxedo",
+            ["textureId"] = "rbxassetid://6447723664"
+        },
+        ["sheep_default"] = {["entityType"] = "sheep", ["starter"] = true, ["textureId"] = "rbxassetid://6280122752"},
+        ["sheep_birthday"] = {
+            ["entityType"] = "sheep",
+            ["title"] = "Birthday",
+            ["textureId"] = "rbxassetid://6414175193"
+        },
+        ["cow_default"] = {["entityType"] = "cow", ["starter"] = true, ["textureId"] = "rbxassetid://6280126590"},
+        ["chicken_default"] = {
+            ["entityType"] = "chicken",
+            ["starter"] = true,
+            ["textureId"] = "rbxassetid://6445511003"
+        },
+        ["cow_lunar_2021"] = {
+            ["entityType"] = "cow",
+            ["title"] = "Lunar New Year",
+            ["textureId"] = "rbxassetid://6381320424"
+        },
+        ["pig_default"] = {["entityType"] = "pig", ["starter"] = true, ["textureId"] = "rbxassetid://6324199566"},
+        ["pig_bank"] = {["entityType"] = "pig", ["title"] = "Piggy Bank", ["textureId"] = "rbxassetid://6722810379"}
+    }
+}
