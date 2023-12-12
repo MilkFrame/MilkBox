789c91db239c69886b8ff11743f127002cd97954
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-13 01:06:29 +1100

    Create MiscValues.lua

---
+++ GameData/Blade Ball [13772394625]/MiscValues.lua
@@ -0,0 +1,337 @@
+data = {
+    ["Misc"] = {
+        ["Sakura"] = {
+            ["Sakura Normal"] = {["Tree"] = {["Uproots"] = {["Upz"] = {["Value"] = 1}}}},
+            ["Sakura Red"] = {["Tree"] = {["Uproots"] = {["Upz"] = {["Value"] = 2}}}},
+            ["Sakura Green"] = {["Tree"] = {["Uproots"] = {["Upz"] = {["Value"] = 2}}}},
+            ["Sakura Yellow"] = {["Tree"] = {["Uproots"] = {["Upz"] = {["Value"] = 2}}}},
+            ["Sakura Purple"] = {["Tree"] = {["Uproots"] = {["Upz"] = {["Value"] = 2}}}},
+            ["Sakura Blue"] = {["Tree"] = {["Uproots"] = {["Upz"] = {["Value"] = 2}}}}
+        },
+        ["Swords"] = {
+            ["Normal"] = {
+                ["Emerald Warden Blade"] = {["KatanaMesh"] = {["Description"] = "A strange sword from another world."}},
+                ["Dark Blade"] = {["KatanaMesh"] = {["Description"] = "Hey, It's pretty shiny for being so dark."}},
+                ["Kitchen Knife"] = {["KatanaMesh"] = {["Description"] = "Your standard kitchen knife."}},
+                ["Wooden Hammer"] = {["KatanaMesh"] = {["Description"] = "Why would they make it wooden?"}},
+                ["Base Sword"] = {["KatanaMesh"] = {["Description"] = "The starter katana."}},
+                ["Pickaxe"] = {["KatanaMesh"] = {["Description"] = "A basic pickaxe."}},
+                ["Battle Axe"] = {["KatanaMesh"] = {["Description"] = "A classic battle axe."}},
+                ["Linked Sword"] = {["KatanaMesh"] = {["Description"] = "Classic roblox sword."}},
+                ["Melon Slice"] = {["KatanaMesh"] = {["Description"] = "Watermelon on a stick? This isnt a sword..."}},
+                ["Rapier"] = {["KatanaMesh"] = {["Description"] = "A simple rapier."}},
+                ["Tanto"] = {["KatanaMesh"] = {["Description"] = "A basic tanto."}},
+                ["Baseball Bat"] = {["KatanaMesh"] = {["Description"] = "A regular baseball bat."}},
+                ["Stick"] = {["KatanaMesh"] = {["Description"] = "A sturdy stick from a tree."}},
+                ["Massive Lolipop"] = {["KatanaMesh"] = {["Description"] = "Why is this lolipop so large?"}},
+                ["Bone"] = {["KatanaMesh"] = {["Description"] = "FLOOR BONE."}}
+            },
+            ["Unique"] = {
+                ["Molten Greatblade"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "A molten sword crafted by the finest smiths in the earth's core"
+                    }
+                },
+                ["Hotdog Sword"] = {
+                    ["KatanaMesh"] = {["Description"] = "Do something about it. (Obtained via code, it's expired now)."}
+                },
+                ["OLD_Hellfire Blade Level 3"] = {["KatanaMesh"] = {["Description"] = "Max evolution."}},
+                ["Wind Thorn"] = {
+                    ["KatanaMesh"] = {["Description"] = "A roblox classic. Lights on fire when equipped."}
+                },
+                ["Hellfire Blade Level 1"] = {["KatanaMesh"] = {["Description"] = "Reach 3 wins to evolve."}},
+                ["Hellfire Blade Level 3"] = {["KatanaMesh"] = {["Description"] = "Max evolution."}},
+                ["Crescendo"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "(OBTAINED VIA WHEEL) Crescendo, the soul stealer. Lights on fire and has custom animations."
+                    }
+                },
+                ["Tsunami Blade"] = {
+                    ["KatanaMesh"] = {["Description"] = "A roblox classic. Lights on fire when equipped."}
+                },
+                ["Ocean Blade"] = {
+                    ["KatanaMesh"] = {["Description"] = "A roblox classic. Lights on fire when equipped."}
+                },
+                ["princ2"] = {["KatanaMesh"] = {["Description"] = "A sword once wielded by the mother nature herself."}},
+                ["Eternum Galepiercer"] = {
+                    ["KatanaMesh"] = {["Description"] = "A roblox classic. Lights on fire when equipped."}
+                },
+                ["giveable champ"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "A scythe worthy of the greatest. Awarded to the Top 10 players on the leaderboard."
+                    }
+                },
+                ["Nothing"] = {["KatanaMesh"] = {["Description"] = "Nothing at all"}},
+                ["Pumpkin PieBlade"] = {["KatanaMesh"] = {["Description"] = "A forgotten sword. (Obtained Via Code)"}},
+                ["Party Popper"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "A lively blade that bursts into action with the exuberance of celebration, ensuring every encounter is an event to remember."
+                    }
+                },
+                ["Noob"] = {["KatanaMesh"] = {["Description"] = "Sword"}},
+                ["Godsaber"] = {["KatanaMesh"] = {["Description"] = "Sword"}},
+                ["Cyber Thorn"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "Pretty neat trident. I mean Spear. I mean Thorn. I mean Fork..?"
+                    }
+                },
+                ["Borealis"] = {["KatanaMesh"] = {["Description"] = "Sword"}},
+                ["Skib"] = {["KatanaMesh"] = {["Description"] = "Destroyer of worlds, Conqueror of galaxies..."}},
+                ["OLD_Hellfire Blade Level 1"] = {["KatanaMesh"] = {["Description"] = "Reach 3 wins to evolve."}},
+                ["Remnant Sword"] = {["KatanaMesh"] = {["Description"] = "A forgotten sword. (Obtained Via Code)"}},
+                ["Prince Blade2"] = {
+                    ["KatanaMesh"] = {["Description"] = "A sword once wielded by the mother nature herself."}
+                },
+                ["OLD_Hellfire Blade Level 2"] = {["KatanaMesh"] = {["Description"] = "Reach 7 wins to evolve."}},
+                ["Thunder Blade"] = {
+                    ["KatanaMesh"] = {["Description"] = "A roblox classic. Lights on fire when equipped."}
+                },
+                ["HardRockStick"] = {
+                    ["Handle"] = {["OriginalSize"] = Vector3.new(1, 1, 1), ["AvatarPartScaleType"] = "Classic"},
+                    ["KatanaMesh"] = {["Description"] = "A hard rock stick from a hard rock tree"}
+                },
+                ["OLDLESS_Hellfire Blade Level 3"] = {["KatanaMesh"] = {["Description"] = "Max evolution."}},
+                ["Apex Blade"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "Said to be crafted with 100 generations of the finest blacksmiths... Only wieldable by the most skilled warriors. Awarded to Rank #1"
+                    }
+                },
+                ["Ball on a Stick"] = {["KatanaMesh"] = {["Description"] = "A sturdy stick from a tree."}},
+                ["Dual Elemental Masterblade"] = {
+                    ["blade"] = {["kmesh"] = {["Description"] = "A roblox classic. Lights on fire when equipped."}},
+                    ["KatanaMesh"] = {["Description"] = "A roblox classic. Lights on fire when equipped."},
+                    ["blade1"] = {["KMesh"] = {["Description"] = "A roblox classic. Lights on fire when equipped."}}
+                },
+                ["Midas Thorn"] = {
+                    ["KatanaMesh"] = {["Description"] = "A roblox classic. Lights on fire when equipped."}
+                },
+                ["Naturic Cutlass"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "A cutlass sewn from a forest's essence. Obtained via code (It's expired now)."
+                    },
+                    ["sword"] = {["Description"] = "FLOOR BONE."}
+                },
+                ["Bobber"] = {["KatanaMesh"] = {["Description"] = "Meant for Boba"}},
+                ["Vishram"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "A blade for fallen warriors. Awarded to the Top 10 players on the leaderboard and kept after ranking is lost."
+                    }
+                },
+                ["Pillar"] = {["KatanaMesh"] = {["Description"] = "The starter katana."}},
+                ["Elemental Masterblade"] = {
+                    ["KatanaMesh"] = {["Description"] = "A roblox classic. Lights on fire when equipped."}
+                },
+                ["The Nooblade"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "The most noobulant of all blades... Obtained by using a code (it's expired now)."
+                    }
+                },
+                ["Bowtie Blade"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "An elegant blade that strikes with the precision and finesse of a gentleman's fashion, ensuring every duel ends in a polished finish."
+                    }
+                },
+                ["Eternal Piercer"] = {
+                    ["KatanaMesh"] = {["Description"] = "A roblox classic. Lights on fire when equipped."}
+                },
+                ["Champion Scythe"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "A scythe worthy of the greatest. Awarded to the Top 10 players on the leaderboard."
+                    }
+                },
+                ["Flowing Fists"] = {
+                    ["KatanaMesh"] = {["Description"] = "Come with special animations. Awarded by the wheel."}
+                },
+                ["Hellfire Blade Level 2"] = {["KatanaMesh"] = {["Description"] = "Reach 7 wins to evolve."}},
+                ["RAPIER_PLACEHOLDER"] = {["KatanaMesh"] = {["Description"] = "Sword"}},
+                ["Play Sword"] = {
+                    ["KatanaMesh"] = {["Description"] = "A roblox classic. Lights on fire when equipped."}
+                },
+                ["Essence Cleaver"] = {
+                    ["KatanaMesh"] = {["Description"] = "A sword once wielded by the mother nature herself."}
+                },
+                ["OLDLESS_Hellfire Blade Level 1"] = {["KatanaMesh"] = {["Description"] = "Reach 3 wins to evolve."}},
+                ["giveable apex"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "Said to be crafted with 100 generations of the finest blacksmiths... Only wieldable by the most skilled warriors. Awarded to Rank #1"
+                    }
+                },
+                ["Shattered Sword"] = {
+                    ["KatanaMesh"] = {["Description"] = "A serrated blade tattered with an aura of vengeance."}
+                },
+                ["Anime Sword"] = {["KatanaMesh"] = {["Description"] = "ANIME SWORD!!!!"}},
+                ["OLDLESS_Hellfire Blade Level 2"] = {["KatanaMesh"] = {["Description"] = "Reach 7 wins to evolve."}},
+                ["Festive Eclipse Blade"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "A sword that marries the joy of celebration with the mysterious allure of cosmic shadows, ensuring each swing brings both light and darkness."
+                    }
+                },
+                ["Voltage"] = {["KatanaMesh"] = {["Description"] = "A roblox classic. Lights on fire when equipped."}}
+            },
+            ["Legendary"] = {
+                ["Thunderous Hammer"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "A hammer once utilized by a god. Eminates lightning when equipped."
+                    }
+                },
+                ["Solans"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "A sword created by the god of order. Eminates his holy flames."
+                    }
+                },
+                ["Venomshank"] = {
+                    ["KatanaMesh"] = {["Description"] = "A roblox classic. Eminates a venomous aura when equipped."}
+                },
+                ["Soul Scythe"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "It is said to have absorbed billions of souls. Eminates a purple aura."
+                    }
+                },
+                ["Musara"] = {["KatanaMesh"] = {["Description"] = "Lightning buzz zbzab! Glows red."}},
+                ["Cyber Blade"] = {
+                    ["KatanaMesh"] = {["Description"] = "A legendary blade once wielded by a Cyborg. Glows purple."}
+                },
+                ["Solarus"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "Divine markings shimmer over this fine axe. Emits golden energy and glows."
+                    }
+                },
+                ["Darkheart"] = {
+                    ["KatanaMesh"] = {["Description"] = "A roblox classic. Eminates darkness when equipped."}
+                },
+                ["Firebrand"] = {["KatanaMesh"] = {["Description"] = "A roblox classic. Lights on fire when equipped."}},
+                ["Trident"] = {["KatanaMesh"] = {["Description"] = "It never seems to dry. Emits water and glows."}},
+                ["Illumina"] = {
+                    ["KatanaMesh"] = {["Description"] = "A roblox classic. Glows blue-purple when equipped"}
+                },
+                ["Holy Axe"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "A pride-driven axe, almost as if it has a will of its own. Lights on fire when equipped."
+                    }
+                },
+                ["Ghostwalker"] = {
+                    ["KatanaMesh"] = {["Description"] = "A roblox classic. Eminates a ghastly aura when equipped."}
+                },
+                ["Windforce"] = {
+                    ["KatanaMesh"] = {["Description"] = "A roblox classic. Glows blue-green when equipped."}
+                },
+                ["Holy Sword"] = {["KatanaMesh"] = {["Description"] = "A cosmic blade... Perhaps of divine origin?"}},
+                ["Zoru"] = {
+                    ["Handle"] = {["AvatarPartScaleType"] = "Classic"},
+                    ["KatanaMesh"] = {
+                        ["Description"] = "A dark blade with a strange energy imbued into it. Glows green."
+                    }
+                },
+                ["Aurorum"] = {["KatanaMesh"] = {["Description"] = "Only revealed to the master. Glows blue."}}
+            },
+            ["Limited"] = {
+                ["Hallow's Edge"] = {
+                    ["KatanaMesh"] = {["Description"] = "A scythe born from fiery wrath. Awarded as limited #6"}
+                },
+                ["Wavelight Greatblade"] = {
+                    ["KatanaMesh"] = {["Description"] = "The pride of an Atlantian king. Awarded as Limited #4"}
+                },
+                ["Oni Claws"] = {["KatanaMesh"] = {["Description"] = "Bladed fists of fury. Awarded as Limited #2"}},
+                ["Cyber Scythe"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "A scythe once wielded by the Prince of the Future. Awarded as Limited #3"
+                    }
+                },
+                ["Prince Blade"] = {
+                    ["KatanaMesh"] = {
+                        ["Description"] = "A sword wielded by a great elven warrior. Awarded as limited #5"
+                    }
+                },
+                ["Empyrean Greatblade"] = {
+                    ["KatanaMesh"] = {["Description"] = "Defender of the Heavens. Awarded as Limited #1"}
+                }
+            },
+            ["Rare"] = {
+                ["Frying Pan"] = {["KatanaMesh"] = {["Description"] = "Fryan Pam Frying Pan..."}},
+                ["Spark Sword"] = {["KatanaMesh"] = {["Description"] = "Wibble Wobble zap zap sword..."}},
+                ["Rockxcalibur"] = {["KatanaMesh"] = {["Description"] = "The sword in the stone."}},
+                ["Bonsai Tree"] = {["KatanaMesh"] = {["Description"] = "How is it still alive..?"}},
+                ["Blocky Sword"] = {["KatanaMesh"] = {["Description"] = "This sword is so highly detailed... Wow..."}},
+                ["Vengeful Dagger"] = {
+                    ["KatanaMesh"] = {["Description"] = "A dagger once wielded by a vengeful demon."}
+                },
+                ["Plunger"] = {["KatanaMesh"] = {["Description"] = "Destroyer of worlds, Conqueror of galaxies..."}},
+                ["Spearbeard"] = {["KatanaMesh"] = {["Description"] = "Whitespeard..?? Bear Bead..."}},
+                ["Etherium Dagger"] = {["KatanaMesh"] = {["Description"] = "Green... Shiny... Dagler..."}},
+                ["Red Saber"] = {["KatanaMesh"] = {["Description"] = "Glowy saber, Glows red."}},
+                ["Executive Greatsword"] = {
+                    ["KatanaMesh"] = {["Description"] = "A greatsword once used to execute strong foes."}
+                },
+                ["Glowy Bone"] = {
+                    ["outerbone"] = {["Description"] = "FLOOR BONE."},
+                    ["KatanaMesh"] = {["Description"] = "Why is this bone glowing?"}
+                },
+                ["Rifle"] = {["KatanaMesh"] = {["Description"] = "Doesn't look like a sword to me."}},
+                ["Dragonslayer Blade"] = {
+                    ["KatanaMesh"] = {["Description"] = "Doesn't look like it slayed a dragon to me."}
+                },
+                ["Comically Large Spoon"] = {["KatanaMesh"] = {["Description"] = "Spoon of DEATH..."}}
+            }
+        },
+        ["Whirlwinds"] = {
+            ["ServerInfo"] = {
+                ["CurrentCoinLimited"] = "-",
+                ["ServerVersion"] = "1.5",
+                ["Region"] = "None",
+                ["CurrentRobuxLimited"] = "-"
+            }
+        },
+        ["refreshTime"] = 0.001,
+        ["Platform"] = {["Value"] = ""}
+    },
+    ["Shared"] = {
+        ["Time"] = {["ServerTime"] = 1698062463},
+        ["ViewportUtility"] = {
+            ["Rig"] = {
+                ["Animate"] = {
+                    ["idle"] = {["Animation2"] = {["Weight"] = 1}, ["Animation1"] = {["Weight"] = 9}},
+                    ["ScaleDampeningPercent"] = 1
+                }
+            }
+        }
+    },
+    ["Upgrades"] = {
+        ["Thunder Dash"] = 0,
+        ["Phase Bypass"] = 0,
+        ["Reaper"] = 0,
+        ["Infinity"] = 0,
+        ["Super Jump"] = 0,
+        ["Quad Jump"] = 0,
+        ["Freeze"] = 0,
+        ["Platform"] = 0,
+        ["Wind Cloak"] = 0,
+        ["Waypoint"] = 0,
+        ["Dash"] = 0,
+        ["Telekinesis"] = 0,
+        ["Raging Deflection"] = 0,
+        ["Blink"] = 0,
+        ["Forcefield"] = 0,
+        ["Swap"] = 0,
+        ["Pull"] = 0,
+        ["Phantom"] = 0,
+        ["Shadow Step"] = 0,
+        ["Invisibility"] = 0
+    },
+    ["Gacha"] = {
+        ["HalloweenNPC"] = {
+            ["Meshes/1 (7)"] = {
+                ["Handle"] = {
+                    ["OriginalSize"] = Vector3.new(3.2666678428649902, 6.3456950187683105, 4.326160907745361),
+                    ["AvatarPartScaleType"] = "ProportionsNormal"
+                }
+            }
+        }
+    },
+    ["TopbarPlusReference"] = "Icon",
+    ["ServerInfo"] = {
+        ["CurrentCoinLimited"] = "-",
+        ["ServerVersion"] = "1.5.5",
+        ["Region"] = "New South Wales, AU",
+        ["CurrentRobuxLimited"] = "-"
+    }
+}
