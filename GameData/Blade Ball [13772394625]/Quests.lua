bc43a1ab20295e10157192900deb9954f9d79300
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-13 00:59:14 +1100

    Create Quests.lua

---
+++ GameData/Blade Ball [13772394625]/Quests.lua
@@ -0,0 +1,56 @@
+data = {
+    ["Kill10"] = {
+        ["rewards"] = {{["amount"] = 35, ["type"] = "Credits"}},
+        ["text"] = "Kill %d Players!",
+        ["stat"] = "Kills",
+        ["goal"] = 10
+    },
+    ["Win3"] = {
+        ["rewards"] = {{["amount"] = 35, ["type"] = "Credits"}},
+        ["text"] = "Win %d Solo Matches",
+        ["stat"] = "Wins",
+        ["goal"] = 3
+    },
+    ["Parry180"] = {
+        ["rewards"] = {{["amount"] = 95, ["type"] = "Credits"}},
+        ["text"] = "Parry The Ball %d Times",
+        ["stat"] = "Parries",
+        ["goal"] = 180
+    },
+    ["Parry60"] = {
+        ["rewards"] = {{["amount"] = 35, ["type"] = "Credits"}},
+        ["text"] = "Parry The Ball %d Times!",
+        ["stat"] = "Parries",
+        ["goal"] = 60
+    },
+    ["Kill20"] = {
+        ["rewards"] = {{["amount"] = 65, ["type"] = "Credits"}},
+        ["text"] = "Kill %d Players!",
+        ["stat"] = "Kills",
+        ["goal"] = 20
+    },
+    ["Parry120"] = {
+        ["rewards"] = {{["amount"] = 65, ["type"] = "Credits"}},
+        ["text"] = "Parry The Ball %d Times!",
+        ["stat"] = "Parries",
+        ["goal"] = 120
+    },
+    ["Win10"] = {
+        ["rewards"] = {{["amount"] = 95, ["type"] = "Credits"}},
+        ["text"] = "Win %d Solo Matches",
+        ["stat"] = "Wins",
+        ["goal"] = 10
+    },
+    ["Win5"] = {
+        ["rewards"] = {{["amount"] = 65, ["type"] = "Credits"}},
+        ["text"] = "Win %d Solo Matches",
+        ["stat"] = "Wins",
+        ["goal"] = 5
+    },
+    ["Kill30"] = {
+        ["rewards"] = {{["amount"] = 95, ["type"] = "Credits"}},
+        ["text"] = "Kill %d Players!",
+        ["stat"] = "Kills",
+        ["goal"] = 30
+    }
+}
