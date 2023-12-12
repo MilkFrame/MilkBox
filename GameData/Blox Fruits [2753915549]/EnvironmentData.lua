f95387aebbccb7024ddf4b74c4d721512e49e3fb
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-13 00:31:16 +1100

    Create EnvironmentData.lua

---
+++ GameData/Blox Fruits [2753915549]/EnvironmentData.lua
@@ -0,0 +1,8 @@
+data = {
+    ["MIN_RENDER_DISTANCE"] = 2000,
+    ["DEBUG_RENDER_DISTANCE"] = false,
+    ["DEBUG_WEATHER_REGION"] = false,
+    ["mapGroups"] = "function: 0x00000000953ba171",
+    ["groups"] = {"Island", "Sea", "SeaEvent"},
+    ["DEBUG_BOUNDS"] = false
+}
