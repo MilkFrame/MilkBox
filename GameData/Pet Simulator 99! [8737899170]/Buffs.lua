9769e34d0ed5765dec3a22e0bdfd861fc316c8b8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-12 19:27:17 +1100

    Create Buffs.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/Buffs.lua
@@ -0,0 +1,26 @@
+data = {
+    ["Toy Ball"] = {
+        ["_script"] = "Buff | Toy Ball",
+        ["AssociatedItemID"] = "Toy Ball",
+        ["AssociatedItemClass"] = "Misc",
+        ["Length"] = 300,
+        ["_id"] = "Toy Ball",
+        ["DisplayName"] = "Toy Ball"
+    },
+    ["Toy Bone"] = {
+        ["_script"] = "Buff | Toy Bone",
+        ["AssociatedItemID"] = "Toy Bone",
+        ["AssociatedItemClass"] = "Misc",
+        ["Length"] = 300,
+        ["_id"] = "Toy Bone",
+        ["DisplayName"] = "Toy Bone"
+    },
+    ["Squeaky Toy"] = {
+        ["_script"] = "Buff | Squeaky Toy",
+        ["AssociatedItemID"] = "Squeaky Toy",
+        ["AssociatedItemClass"] = "Misc",
+        ["Length"] = 600,
+        ["_id"] = "Squeaky Toy",
+        ["DisplayName"] = "Squeaky Toy"
+    }
+}
