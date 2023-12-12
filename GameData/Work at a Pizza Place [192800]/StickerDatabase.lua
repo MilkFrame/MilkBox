5a121be218cfe01458479ac37e26d31fb7de1511
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-13 01:20:27 +1100

    Create StickerDatabase.lua

---
+++ GameData/Work at a Pizza Place [192800]/StickerDatabase.lua
@@ -0,0 +1,191 @@
+data = {
+    {
+        ["SizeAdjustment"] = 1.2,
+        ["Index"] = 1,
+        ["Rarity"] = "Common",
+        ["ImageId"] = 2312210323,
+        ["Name"] = "Just A Brick"
+    },
+    {["Rarity"] = "Common", ["ImageId"] = 2268778681, ["Index"] = 2, ["Name"] = "The Dew"},
+    {
+        ["SizeAdjustment"] = 1.2,
+        ["Index"] = 3,
+        ["Rarity"] = "Common",
+        ["ImageId"] = 2268781486,
+        ["Name"] = "All Fresh Ingredients"
+    },
+    {["SizeAdjustment"] = 1.2, ["Index"] = 4, ["Rarity"] = "Common", ["ImageId"] = 2324558038, ["Name"] = "Your Order?"},
+    {
+        ["SizeAdjustment"] = 1.2,
+        ["Index"] = 5,
+        ["Rarity"] = "Common",
+        ["ImageId"] = 2312413208,
+        ["Name"] = "Confident Cook"
+    },
+    {
+        ["SizeAdjustment"] = 1,
+        ["Index"] = 6,
+        ["Rarity"] = "Common",
+        ["ImageId"] = 2268772858,
+        ["Name"] = "Delighted Girl"
+    },
+    {["SizeAdjustment"] = 1.1, ["Index"] = 7, ["Rarity"] = "Common", ["ImageId"] = 2268772855, ["Name"] = "Builderman"},
+    {["Rarity"] = "Common", ["ImageId"] = 2268772859, ["Index"] = 8, ["Name"] = "Maniacal Manager"},
+    {
+        ["SizeAdjustment"] = 1.4,
+        ["Index"] = 9,
+        ["Rarity"] = "Common",
+        ["ImageId"] = 2268772860,
+        ["Name"] = "Delivery Vehicle"
+    },
+    {["Rarity"] = "Common", ["ImageId"] = 2268774615, ["Index"] = 10, ["Name"] = "Blissful Noob"},
+    {["Rarity"] = "Common", ["ImageId"] = 2268776285, ["Index"] = 11, ["Name"] = "Happy lil Guest"},
+    {["Rarity"] = "Common", ["ImageId"] = 2268776995, ["Index"] = 12, ["Name"] = "Sad Bacon"},
+    {["Rarity"] = "Common", ["ImageId"] = 2268776994, ["Index"] = 13, ["Name"] = "Sad Girl"},
+    {["Rarity"] = "Common", ["ImageId"] = 2268776992, ["Index"] = 14, ["Name"] = "Zombie"},
+    {
+        ["SizeAdjustment"] = 1.2,
+        ["Index"] = 15,
+        ["Rarity"] = "Common",
+        ["ImageId"] = 2268779556,
+        ["Name"] = "Delivery Moped"
+    },
+    {["Rarity"] = "Common", ["ImageId"] = 2268779554, ["Index"] = 16, ["Name"] = "Doing the Robot"},
+    {["Rarity"] = "Common", ["ImageId"] = 2312211032, ["Index"] = 17, ["Name"] = "Employee Plaque"},
+    {
+        ["SizeAdjustment"] = 1.1,
+        ["Index"] = 18,
+        ["Rarity"] = "Common",
+        ["ImageId"] = 2312211444,
+        ["Name"] = "Roblox Logo"
+    },
+    {
+        ["SizeAdjustment"] = 1.1,
+        ["Index"] = 19,
+        ["Rarity"] = "Common",
+        ["ImageId"] = 2312210844,
+        ["Name"] = "2008 Customer"
+    },
+    {["Rarity"] = "Common", ["ImageId"] = 2324557660, ["Index"] = 20, ["Name"] = "Pizza Cat"},
+    {["Rarity"] = "Uncommon", ["ImageId"] = 2268772287, ["Index"] = 21, ["Name"] = "Noob Confusion"},
+    {["Rarity"] = "Uncommon", ["ImageId"] = 2268772288, ["Index"] = 22, ["Name"] = "A Pizza Pie!"},
+    {
+        ["SizeAdjustment"] = 1.2,
+        ["Index"] = 23,
+        ["Rarity"] = "Uncommon",
+        ["ImageId"] = 2268772861,
+        ["Name"] = "Delivery Dude"
+    },
+    {["Rarity"] = "Uncommon", ["ImageId"] = 2268774621, ["Index"] = 24, ["Name"] = "Rookie Cook"},
+    {["Rarity"] = "Uncommon", ["ImageId"] = 2268774614, ["Index"] = 25, ["Name"] = "Stressed Supplier"},
+    {
+        ["SizeAdjustment"] = 1.1,
+        ["Index"] = 26,
+        ["Rarity"] = "Uncommon",
+        ["ImageId"] = 2268776284,
+        ["Name"] = "Leftovers"
+    },
+    {
+        ["SizeAdjustment"] = 1.2,
+        ["Index"] = 27,
+        ["Rarity"] = "Uncommon",
+        ["ImageId"] = 2273620567,
+        ["Name"] = "Good Duck"
+    },
+    {
+        ["SizeAdjustment"] = 1.1,
+        ["Index"] = 28,
+        ["Rarity"] = "Uncommon",
+        ["ImageId"] = 2268776998,
+        ["Name"] = "Floor Pizza"
+    },
+    {["Rarity"] = "Uncommon", ["ImageId"] = 2312211657, ["Index"] = 29, ["Name"] = "Rest in TIX"},
+    {["Rarity"] = "Uncommon", ["ImageId"] = 2268776993, ["Index"] = 30, ["Name"] = "Jaded Mascot"},
+    {["Rarity"] = "Uncommon", ["ImageId"] = 2268779579, ["Index"] = 31, ["Name"] = "Stuffed Noob"},
+    {
+        ["SizeAdjustment"] = 1.6,
+        ["Index"] = 32,
+        ["Rarity"] = "Uncommon",
+        ["ImageId"] = 2312211209,
+        ["Name"] = "Kitchen Chaos"
+    },
+    {
+        ["Index"] = 33,
+        ["Rarity"] = "Uncommon",
+        ["Name"] = "You got mail!",
+        ["ImageId"] = 2268781485,
+        ["OutlineImage"] = 2304573048
+    },
+    {
+        ["SizeAdjustment"] = 1.5,
+        ["Index"] = 34,
+        ["Rarity"] = "Uncommon",
+        ["ImageId"] = 2324556952,
+        ["Name"] = "Getting Ready"
+    },
+    {
+        ["SizeAdjustment"] = 1.5,
+        ["Index"] = 35,
+        ["Rarity"] = "Uncommon",
+        ["ImageId"] = 2324557234,
+        ["Name"] = "Supplier Disaster"
+    },
+    {
+        ["SizeAdjustment"] = 1.6,
+        ["Index"] = 36,
+        ["Rarity"] = "Rare",
+        ["ImageId"] = 2268780598,
+        ["Name"] = "The Builder Bros"
+    },
+    {["Rarity"] = "Rare", ["ImageId"] = 2268774616, ["Index"] = 37, ["Name"] = "The LMaDer"},
+    {["SizeAdjustment"] = 1.1, ["Index"] = 38, ["Rarity"] = "Rare", ["ImageId"] = 2268774612, ["Name"] = "Shedletsky"},
+    {["Rarity"] = "Rare", ["ImageId"] = 2268776307, ["Index"] = 39, ["Name"] = "Abomination"},
+    {
+        ["SizeAdjustment"] = 1.4,
+        ["Index"] = 40,
+        ["Rarity"] = "Rare",
+        ["ImageId"] = 2268778652,
+        ["Name"] = "Grand Theft Pizza"
+    },
+    {["Rarity"] = "Rare", ["ImageId"] = 2268778649, ["Index"] = 41, ["Name"] = "Almighty Linked Sword"},
+    {["SizeAdjustment"] = 1, ["Index"] = 42, ["Rarity"] = "Rare", ["ImageId"] = 2268776304, ["Name"] = "####"},
+    {["Rarity"] = "Rare", ["ImageId"] = 2268778651, ["Index"] = 43, ["Name"] = "NOOB!"},
+    {["SizeAdjustment"] = 1, ["Index"] = 44, ["Rarity"] = "Rare", ["ImageId"] = 2268776287, ["Name"] = "OOF!"},
+    {["SizeAdjustment"] = 1.1, ["Index"] = 45, ["Rarity"] = "Rare", ["ImageId"] = 2312404201, ["Name"] = "Robuck"},
+    {
+        ["SizeAdjustment"] = 1.3,
+        ["Index"] = 46,
+        ["Rarity"] = "Legendary",
+        ["ImageId"] = 2268778653,
+        ["Name"] = "Golden Pizza"
+    },
+    {
+        ["SizeAdjustment"] = 1.3,
+        ["Index"] = 47,
+        ["Rarity"] = "Legendary",
+        ["ImageId"] = 2268779581,
+        ["Name"] = "Neon Pizza"
+    },
+    {
+        ["SizeAdjustment"] = 1.5,
+        ["Index"] = 48,
+        ["Rarity"] = "Legendary",
+        ["ImageId"] = 2268778683,
+        ["Name"] = "Blazing Pizza"
+    },
+    {
+        ["SizeAdjustment"] = 1.3,
+        ["Index"] = 49,
+        ["Rarity"] = "Legendary",
+        ["ImageId"] = 2268776999,
+        ["Name"] = "Evil Duck"
+    },
+    {
+        ["SizeAdjustment"] = 2,
+        ["Index"] = 50,
+        ["Rarity"] = "Legendary",
+        ["ImageId"] = 2324557482,
+        ["Name"] = "God of Pizza"
+    },
+    ["SizeAdjustment"] = 1.1
+}
