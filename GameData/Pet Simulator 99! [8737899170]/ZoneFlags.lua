0d6fd8ee6a506a7dc09278e9b0aa08c974e3611d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-12 19:43:18 +1100

    Create ZoneFlags.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/ZoneFlags.lua
@@ -0,0 +1,134 @@
+data = {
+    ["Coins Flag"] = {
+        ["Rarity"] = {
+            ["RarityNumber"] = 2,
+            ["Lootbag"] = "Rare",
+            ["_id"] = "Rare",
+            ["Color"] = Color3.fromRGB(113, 255, 62),
+            ["DisplayName"] = "Rare",
+            ["Message"] = "function: 0x00000000c1d7973d",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "2 | Rare"
+        },
+        ["_id"] = "Coins Flag",
+        ["Color"] = Color3.fromRGB(255, 255, 127),
+        ["Duration"] = 300,
+        ["Desc"] = "Place in an area to increase Coins you earn by 50%! Lasts 5 minutes!",
+        ["_script"] = "ZoneFlag | Coins Flag",
+        ["Name"] = "Coins Flag",
+        ["Model"] = "Model",
+        ["Icon"] = "rbxassetid://15000809958"
+    },
+    ["Fortune Flag"] = {
+        ["Rarity"] = {
+            ["RarityNumber"] = 4,
+            ["Lootbag"] = "Legendary",
+            ["_id"] = "Legendary",
+            ["Color"] = Color3.fromRGB(255, 133, 34),
+            ["DisplayName"] = "Legendary",
+            ["Message"] = "function: 0x000000002294486d",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "4 | Legendary"
+        },
+        ["_id"] = "Fortune Flag",
+        ["Color"] = Color3.fromRGB(170, 85, 255),
+        ["Duration"] = 300,
+        ["Desc"] = "Place in an area to increase Coins by 50% and Diamonds by 2x! Lasts 5 minutes!",
+        ["_script"] = "ZoneFlag | Fortune Flag",
+        ["Name"] = "Fortune Flag",
+        ["Model"] = "Model",
+        ["Icon"] = "rbxassetid://15000810242"
+    },
+    ["Hasty Flag"] = {
+        ["Rarity"] = {
+            ["RarityNumber"] = 3,
+            ["Lootbag"] = "Epic",
+            ["_id"] = "Epic",
+            ["Color"] = Color3.fromRGB(41, 226, 255),
+            ["DisplayName"] = "Epic",
+            ["Message"] = "function: 0x000000002c36424d",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "3 | Epic"
+        },
+        ["_id"] = "Hasty Flag",
+        ["Color"] = Color3.fromRGB(152, 8, 255),
+        ["Duration"] = 300,
+        ["Desc"] = "Place in an area to super-charge pets! Lasts 5 minutes!",
+        ["_script"] = "ZoneFlag | Hasty Flag",
+        ["Name"] = "Hasty Flag",
+        ["Model"] = "Model",
+        ["Icon"] = "rbxassetid://15000810356"
+    },
+    ["Magnet Flag"] = {
+        ["Rarity"] = {
+            ["RarityNumber"] = 2,
+            ["Lootbag"] = "Rare",
+            ["_id"] = "Rare",
+            ["Color"] = Color3.fromRGB(113, 255, 62),
+            ["DisplayName"] = "Rare",
+            ["Message"] = "function: 0x00000000c1d7973d",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "2 | Rare"
+        },
+        ["_id"] = "Magnet Flag",
+        ["Color"] = Color3.fromRGB(255, 0, 0),
+        ["Duration"] = 300,
+        ["Desc"] = "A portable magnet that can be placed in any area! Lasts 5 minutes!",
+        ["_script"] = "ZoneFlag | Magnet Flag",
+        ["Name"] = "Magnet Flag",
+        ["Model"] = "Model",
+        ["Icon"] = "rbxassetid://15000810137"
+    },
+    ["Diamonds Flag"] = {
+        ["Rarity"] = {
+            ["RarityNumber"] = 3,
+            ["Lootbag"] = "Epic",
+            ["_id"] = "Epic",
+            ["Color"] = Color3.fromRGB(41, 226, 255),
+            ["DisplayName"] = "Epic",
+            ["Message"] = "function: 0x000000002c36424d",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "3 | Epic"
+        },
+        ["_id"] = "Diamonds Flag",
+        ["Color"] = Color3.fromRGB(0, 255, 255),
+        ["Duration"] = 300,
+        ["Desc"] = "Place in an area to increase Diamond breakables by 2x! Lasts 5 minutes!",
+        ["_script"] = "ZoneFlag | Diamonds Flag",
+        ["Name"] = "Diamonds Flag",
+        ["Model"] = "Model",
+        ["Icon"] = "rbxassetid://15000810493"
+    },
+    ["Exotic Treasure Flag"] = {
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0x0000000016b12f1d",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["_id"] = "Exotic Treasure Flag",
+        ["Color"] = Color3.fromRGB(255, 255, 127),
+        ["Duration"] = 300,
+        ["Desc"] = "When placed in an area, drops increase SUBSTANTIALLY! Lasts 5 minutes!",
+        ["_script"] = "ZoneFlag | Exotic Treasure Flag",
+        ["Name"] = "Exotic Treasure Flag",
+        ["Model"] = "Model",
+        ["Icon"] = "rbxassetid://15011948755"
+    }
+}
