beb6ca180a936e50e13438546a6de8323ec3609b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-12 19:42:40 +1100

    Create Rarity.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/Rarity.lua
@@ -0,0 +1,86 @@
+data = {
+    ["Exclusive"] = {
+        ["RarityNumber"] = 999,
+        ["Lootbag"] = "Exotic",
+        ["_id"] = "Exclusive",
+        ["Color"] = Color3.fromRGB(173, 79, 255),
+        ["DisplayName"] = "Exclusive",
+        ["Message"] = "function: 0x000000002340c490",
+        ["ItemSlot"] = "ItemSlot",
+        ["Gradient"] = "Gradient",
+        ["Announce"] = true,
+        ["_script"] = "Exclusive"
+    },
+    ["Legendary"] = {
+        ["RarityNumber"] = 4,
+        ["Lootbag"] = "Legendary",
+        ["_id"] = "Legendary",
+        ["Color"] = Color3.fromRGB(255, 133, 34),
+        ["DisplayName"] = "Legendary",
+        ["Message"] = "function: 0x0000000074198758",
+        ["ItemSlot"] = "ItemSlot",
+        ["Gradient"] = "Gradient",
+        ["Announce"] = false,
+        ["_script"] = "4 | Legendary"
+    },
+    ["Basic"] = {
+        ["RarityNumber"] = 1,
+        ["Lootbag"] = "Basic",
+        ["_id"] = "Basic",
+        ["Color"] = Color3.fromRGB(148, 145, 167),
+        ["DisplayName"] = "Basic",
+        ["Message"] = "function: 0x00000000f87e77e8",
+        ["ItemSlot"] = "ItemSlot",
+        ["Gradient"] = "Gradient",
+        ["Announce"] = false,
+        ["_script"] = "1 | Basic"
+    },
+    ["Epic"] = {
+        ["RarityNumber"] = 3,
+        ["Lootbag"] = "Epic",
+        ["_id"] = "Epic",
+        ["Color"] = Color3.fromRGB(41, 226, 255),
+        ["DisplayName"] = "Epic",
+        ["Message"] = "function: 0x0000000002ea8d78",
+        ["ItemSlot"] = "ItemSlot",
+        ["Gradient"] = "Gradient",
+        ["Announce"] = false,
+        ["_script"] = "3 | Epic"
+    },
+    ["Exotic"] = {
+        ["RarityNumber"] = 6,
+        ["Lootbag"] = "Exotic",
+        ["_id"] = "Exotic",
+        ["Color"] = Color3.fromRGB(255, 29, 251),
+        ["DisplayName"] = "Exotic",
+        ["Message"] = "function: 0x0000000045f837a8",
+        ["ItemSlot"] = "ItemSlot",
+        ["Gradient"] = "Gradient",
+        ["Announce"] = true,
+        ["_script"] = "6 | Exotic"
+    },
+    ["Mythical"] = {
+        ["RarityNumber"] = 5,
+        ["Lootbag"] = "Mythical",
+        ["_id"] = "Mythical",
+        ["Color"] = Color3.fromRGB(255, 43, 100),
+        ["DisplayName"] = "Mythical",
+        ["Message"] = "function: 0x000000001a57c9b0",
+        ["ItemSlot"] = "ItemSlot",
+        ["Gradient"] = "Gradient",
+        ["Announce"] = false,
+        ["_script"] = "5 | Mythical"
+    },
+    ["Rare"] = {
+        ["RarityNumber"] = 2,
+        ["Lootbag"] = "Rare",
+        ["_id"] = "Rare",
+        ["Color"] = Color3.fromRGB(113, 255, 62),
+        ["DisplayName"] = "Rare",
+        ["Message"] = "function: 0x0000000038e82a88",
+        ["ItemSlot"] = "ItemSlot",
+        ["Gradient"] = "Gradient",
+        ["Announce"] = false,
+        ["_script"] = "2 | Rare"
+    }
+}
