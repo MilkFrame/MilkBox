9c4560bb910a6daf0a492e30827d6ba919bd7272
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-13 01:05:25 +1100

    Create UniverseIds.lua

---
+++ GameData/Blade Ball [13772394625]/UniverseIds.lua
@@ -0,0 +1,27 @@
+data = {
+    ["Ranked"] = {
+        ["PlaceId"] = 14915220621,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["Accessible"] = "function: 0x0000000062b29a5e"
+    },
+    ["Pro"] = {
+        ["PlaceId"] = 14732610803,
+        ["RejectionStatus"] = "You need at least 50 Wins!",
+        ["Accessible"] = "function: 0x00000000ce9711ce"
+    },
+    ["Default"] = {
+        ["PlaceId"] = 13772394625,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["Accessible"] = "function: 0x0000000062b29a5e"
+    },
+    ["Voice"] = {
+        ["PlaceId"] = 15131065025,
+        ["RejectionStatus"] = "You do not have Voice Chat (13+) enabled!",
+        ["Accessible"] = "function: 0x00000000962c9aa6"
+    },
+    ["AFK"] = {
+        ["PlaceId"] = 14368557094,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["Accessible"] = "function: 0x0000000062b29a5e"
+    }
+}
