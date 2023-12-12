831305d51b429135a10ab98d18635c57f120b36c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-12 19:26:03 +1100

    Create Currency.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/Currency.lua
@@ -0,0 +1,160 @@
+data = {
+    ["Digsite"] = {
+        ["_script"] = "Currency | Digsite",
+        ["_id"] = "Digsite",
+        ["Rarity"] = {
+            ["RarityNumber"] = 2,
+            ["Lootbag"] = "Rare",
+            ["_id"] = "Rare",
+            ["Color"] = Color3.fromRGB(113, 255, 62),
+            ["DisplayName"] = "Rare",
+            ["Message"] = "function: 0x0000000028d97bcc",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "2 | Rare"
+        },
+        ["Tradable"] = false,
+        ["Tiers"] = {
+            {
+                ["orbImage"] = "rbxassetid://15466421456",
+                ["imageOutline"] = "rbxassetid://15238661657",
+                ["isBottom"] = true,
+                ["Order"] = 1,
+                ["rainData"] = {["LightEmission"] = 0.2},
+                ["value"] = 1,
+                ["tinyImage"] = "rbxassetid://15258328192",
+                ["tierName"] = "Digsite Tokens",
+                ["textColor"] = Color3.fromRGB(255, 255, 255)
+            }
+        },
+        ["MaxAmount"] = 999999999,
+        ["Desc"] = "Digsite Tokens!",
+        ["DisplayName"] = "Digsite Tokens"
+    },
+    ["Diamonds"] = {
+        ["_script"] = "Currency | Diamonds",
+        ["_id"] = "Diamonds",
+        ["Rarity"] = {
+            ["RarityNumber"] = 2,
+            ["Lootbag"] = "Rare",
+            ["_id"] = "Rare",
+            ["Color"] = Color3.fromRGB(113, 255, 62),
+            ["DisplayName"] = "Rare",
+            ["Message"] = "function: 0x0000000028d97bcc",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "2 | Rare"
+        },
+        ["Tradable"] = true,
+        ["Tiers"] = {
+            {
+                ["orbImage"] = "rbxassetid://15466421090",
+                ["imageOutline"] = "rbxassetid://14867116353",
+                ["isBottom"] = true,
+                ["Order"] = 1,
+                ["rainData"] = {["LightEmission"] = 0.2},
+                ["value"] = 1,
+                ["tinyImage"] = "rbxassetid://15258327857",
+                ["tierName"] = "Diamonds",
+                ["textColor"] = Color3.fromRGB(129, 253, 255)
+            }
+        },
+        ["MaxAmount"] = 999999999,
+        ["Desc"] = "Diamonds!",
+        ["DisplayName"] = "Diamonds"
+    },
+    ["Fishing"] = {
+        ["_script"] = "Currency | Fishing",
+        ["_id"] = "Fishing",
+        ["Rarity"] = {
+            ["RarityNumber"] = 2,
+            ["Lootbag"] = "Rare",
+            ["_id"] = "Rare",
+            ["Color"] = Color3.fromRGB(113, 255, 62),
+            ["DisplayName"] = "Rare",
+            ["Message"] = "function: 0x0000000028d97bcc",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "2 | Rare"
+        },
+        ["Tradable"] = false,
+        ["Tiers"] = {
+            {
+                ["orbImage"] = "rbxassetid://15466421230",
+                ["imageOutline"] = "rbxassetid://15238661515",
+                ["isBottom"] = true,
+                ["Order"] = 1,
+                ["rainData"] = {["LightEmission"] = 0.2},
+                ["value"] = 1,
+                ["tinyImage"] = "rbxassetid://15258327975",
+                ["tierName"] = "Fishing Tokens",
+                ["textColor"] = Color3.fromRGB(255, 255, 255)
+            }
+        },
+        ["MaxAmount"] = 999999999,
+        ["Desc"] = "Fishing Tokens!",
+        ["DisplayName"] = "Fishing Tokens"
+    },
+    ["Coins"] = {
+        ["_script"] = "Currency | Coins",
+        ["_id"] = "Coins",
+        ["Rarity"] = {
+            ["RarityNumber"] = 1,
+            ["Lootbag"] = "Basic",
+            ["_id"] = "Basic",
+            ["Color"] = Color3.fromRGB(148, 145, 167),
+            ["DisplayName"] = "Basic",
+            ["Message"] = "function: 0x000000008ed26eac",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "1 | Basic"
+        },
+        ["Tradable"] = false,
+        ["Tiers"] = {
+            {
+                ["orbImage"] = "rbxassetid://15466375741",
+                ["imageOutline"] = "rbxassetid://14867116080",
+                ["Order"] = 1,
+                ["isBottom"] = true,
+                ["value"] = 1,
+                ["tierName"] = "Gold Coins",
+                ["tinyImage"] = "rbxassetid://15258327621",
+                ["textColor"] = Color3.fromRGB(255, 252, 219)
+            },
+            {
+                ["orbImage"] = "rbxassetid://15466420899",
+                ["imageOutline"] = "rbxassetid://14867116225",
+                ["Order"] = 2,
+                ["value"] = 1000000,
+                ["tierName"] = "Gold Bars",
+                ["tinyImage"] = "rbxassetid://15258327761",
+                ["textColor"] = Color3.fromRGB(255, 248, 155)
+            },
+            {
+                ["orbImage"] = "rbxassetid://15466420621",
+                ["imageOutline"] = "rbxassetid://14867115964",
+                ["Order"] = 3,
+                ["value"] = 1000000000000,
+                ["tierName"] = "Platinum Coins",
+                ["tinyImage"] = "rbxassetid://15258327405",
+                ["textColor"] = Color3.fromRGB(247, 255, 254)
+            },
+            {
+                ["orbImage"] = "rbxassetid://15466420768",
+                ["imageOutline"] = "rbxassetid://14867115795",
+                ["Order"] = 4,
+                ["value"] = 1000000000000000000,
+                ["tierName"] = "Platinum Bars",
+                ["tinyImage"] = "rbxassetid://15258327518",
+                ["textColor"] = Color3.fromRGB(213, 255, 247)
+            }
+        },
+        ["MaxAmount"] = 2.e+24,
+        ["Desc"] = "Gold!",
+        ["DisplayName"] = "Coins"
+    }
+}
