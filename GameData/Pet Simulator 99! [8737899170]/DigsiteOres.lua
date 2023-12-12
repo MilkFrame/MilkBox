d731fd3f4b4192a476f7c098e17d8891583387c9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-12 19:44:38 +1100

    Create DigsiteOres.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/DigsiteOres.lua
@@ -0,0 +1,81 @@
+data = {
+    ["Iron"] = {
+        ["_script"] = "DigsiteOre | Iron",
+        ["_id"] = "Iron",
+        ["Part"] = "Part",
+        ["Multiplier"] = 3,
+        ["DisplayName"] = "Iron"
+    },
+    ["Diamond"] = {
+        ["_script"] = "DigsiteOre | Diamond",
+        ["Loot"] = {
+            {
+                ["_caches"] = {},
+                ["entries"] = {
+                    {
+                        ["Value"] = {
+                            ["_caches"] = {},
+                            ["entries"] = {
+                                {
+                                    ["Value"] = {
+                                        ["_data"] = {["id"] = "Diamonds", ["_am"] = 50},
+                                        ["_stackKey"] = [[{"id":"Diamonds"}]],
+                                        ["_exactStackKey"] = [[{"id":"Diamonds"}]],
+                                        ["_maxAmount"] = 999999999
+                                    },
+                                    ["Weight"] = 5
+                                },
+                                {
+                                    ["Value"] = {
+                                        ["_data"] = {["id"] = "Diamonds", ["_am"] = 30},
+                                        ["_stackKey"] = [[{"id":"Diamonds"}]],
+                                        ["_exactStackKey"] = [[{"id":"Diamonds"}]],
+                                        ["_maxAmount"] = 999999999
+                                    },
+                                    ["Weight"] = 15
+                                },
+                                {
+                                    ["Value"] = {
+                                        ["_data"] = {["id"] = "Diamonds", ["_am"] = 20},
+                                        ["_stackKey"] = [[{"id":"Diamonds"}]],
+                                        ["_exactStackKey"] = [[{"id":"Diamonds"}]],
+                                        ["_maxAmount"] = 999999999
+                                    },
+                                    ["Weight"] = 30
+                                },
+                                {
+                                    ["Value"] = {
+                                        ["_data"] = {["id"] = "Diamonds", ["_am"] = 10},
+                                        ["_stackKey"] = [[{"id":"Diamonds"}]],
+                                        ["_exactStackKey"] = [[{"id":"Diamonds"}]],
+                                        ["_maxAmount"] = 999999999
+                                    },
+                                    ["Weight"] = 50
+                                }
+                            }
+                        },
+                        ["Weight"] = 10
+                    }
+                }
+            }
+        },
+        ["_id"] = "Diamond",
+        ["Part"] = "Part",
+        ["Multiplier"] = 8,
+        ["DisplayName"] = "Diamond"
+    },
+    ["Coal"] = {
+        ["_script"] = "DigsiteOre | Coal",
+        ["_id"] = "Coal",
+        ["Part"] = "Part",
+        ["Multiplier"] = 2,
+        ["DisplayName"] = "Coal"
+    },
+    ["Gold"] = {
+        ["_script"] = "DigsiteOre | Gold",
+        ["_id"] = "Gold",
+        ["Part"] = "Part",
+        ["Multiplier"] = 4,
+        ["DisplayName"] = "Gold"
+    }
+}
