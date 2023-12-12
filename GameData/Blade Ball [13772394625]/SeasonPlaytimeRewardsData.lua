068d6326e16bd079d78fbd97fa8c2490070283d4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-13 01:05:41 +1100

    Create SeasonPlaytimeRewardsData.lua

---
+++ GameData/Blade Ball [13772394625]/SeasonPlaytimeRewardsData.lua
@@ -0,0 +1,7 @@
+data = {
+    {["Reward"] = {["CurrencyName"] = "Pumpkins", ["Amount"] = 25}, ["Seconds"] = 300},
+    {["Reward"] = {["CurrencyName"] = "Pumpkins", ["Amount"] = 25}, ["Seconds"] = 1200},
+    {["Reward"] = {["CurrencyName"] = "Pumpkins", ["Amount"] = 50}, ["Seconds"] = 3600},
+    {["Reward"] = {["CurrencyName"] = "Pumpkins", ["Amount"] = 75}, ["Seconds"] = 5400},
+    {["Reward"] = {["CurrencyName"] = "Pumpkins", ["Amount"] = 100}, ["Seconds"] = 10800}
+}
