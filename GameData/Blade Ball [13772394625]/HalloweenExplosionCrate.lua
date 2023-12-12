de6551515fcc18c77e083fb4d27696835291f13a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-13 01:04:28 +1100

    Create HalloweenExplosionCrate.lua

---
+++ GameData/Blade Ball [13772394625]/HalloweenExplosionCrate.lua
@@ -0,0 +1,85 @@
+data = {
+    ["SpinTable"] = "function: 0x00000000aefdc45e",
+    ["Rewards"] = {
+        {
+            ["ItemName"] = "Portal To Hell",
+            ["Rarity"] = 23,
+            ["Amount"] = 1,
+            ["Icon"] = "rbxassetid://15050028397",
+            ["AwardFunctionName"] = "AddExplosion",
+            ["AwardFunctionArguments"] = {"Portal To Hell", false}
+        },
+        {
+            ["ItemName"] = "Hungry Shadows",
+            ["Rarity"] = 19,
+            ["Amount"] = 1,
+            ["Icon"] = "rbxassetid://15050029747",
+            ["AwardFunctionName"] = "AddExplosion",
+            ["AwardFunctionArguments"] = {"Hungry Shadows", false}
+        },
+        {
+            ["ItemName"] = "Evil Teleportation",
+            ["Rarity"] = 15.85,
+            ["Amount"] = 1,
+            ["Icon"] = "rbxassetid://15050031425",
+            ["AwardFunctionName"] = "AddExplosion",
+            ["AwardFunctionArguments"] = {"Evil Teleportation", false}
+        },
+        {
+            ["ItemName"] = "Evil Spike Pit",
+            ["Rarity"] = 13,
+            ["Amount"] = 1,
+            ["Icon"] = "rbxassetid://15050032257",
+            ["AwardFunctionName"] = "AddExplosion",
+            ["AwardFunctionArguments"] = {"Evil Spike Pit", false}
+        },
+        {
+            ["ItemName"] = "Poison Bomb",
+            ["Rarity"] = 10.5,
+            ["Amount"] = 1,
+            ["Icon"] = "rbxassetid://15050032974",
+            ["AwardFunctionName"] = "AddExplosion",
+            ["AwardFunctionArguments"] = {"Poison Bomb", false}
+        },
+        {
+            ["ItemName"] = "Ghost Grenade",
+            ["Rarity"] = 9,
+            ["Amount"] = 1,
+            ["Icon"] = "rbxassetid://15050034005",
+            ["AwardFunctionName"] = "AddExplosion",
+            ["AwardFunctionArguments"] = {"Ghost Grenade", false}
+        },
+        {
+            ["ItemName"] = "Purgatory Flames",
+            ["Rarity"] = 6,
+            ["Amount"] = 1,
+            ["Icon"] = "rbxassetid://15050035010",
+            ["AwardFunctionName"] = "AddExplosion",
+            ["AwardFunctionArguments"] = {"Purgatory Flames", false}
+        },
+        {
+            ["ItemName"] = "Pumpkin Fire",
+            ["Rarity"] = 2.5,
+            ["Amount"] = 1,
+            ["Icon"] = "rbxassetid://15050038136",
+            ["AwardFunctionName"] = "AddExplosion",
+            ["AwardFunctionArguments"] = {"Pumpkin Fire", false}
+        },
+        {
+            ["ItemName"] = "Exorcist Combustion",
+            ["Rarity"] = 1,
+            ["Amount"] = 1,
+            ["Icon"] = "rbxassetid://15050036477",
+            ["AwardFunctionName"] = "AddExplosion",
+            ["AwardFunctionArguments"] = {"Exorcist Combustion", false}
+        },
+        {
+            ["ItemName"] = "Hell Dragon Beam",
+            ["Rarity"] = 0.15,
+            ["Amount"] = 1,
+            ["Icon"] = "http://www.roblox.com/asset/?id=15060139517",
+            ["AwardFunctionName"] = "AddExplosion",
+            ["AwardFunctionArguments"] = {"Hell Dragon Beam", false}
+        }
+    }
+}
