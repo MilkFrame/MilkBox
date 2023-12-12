c0e343a908edd17434f47259d50e4157159d719f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-12 19:26:17 +1100

    Create Charms.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/Charms.lua
@@ -0,0 +1,80 @@
+data = {
+    ["Royalty"] = {
+        ["Rarity"] = "function: 0x000000000bd5f25c",
+        ["Power"] = "function: 0x00000000a3023934",
+        ["DisplayName"] = "function: 0x00000000e043498c",
+        ["BaseTier"] = 1,
+        ["_script"] = "Charm | Royalty",
+        ["_id"] = "Royalty",
+        ["MaxTier"] = 1,
+        ["Icon"] = "rbxassetid://15286764793",
+        ["Desc"] = "function: 0x00000000bacf04ac"
+    },
+    ["Diamonds"] = {
+        ["Rarity"] = "function: 0x000000005b3777cc",
+        ["Power"] = "function: 0x00000000132a2a44",
+        ["DisplayName"] = "function: 0x0000000008016edc",
+        ["BaseTier"] = 1,
+        ["_script"] = "Charm | Diamonds",
+        ["_id"] = "Diamonds",
+        ["MaxTier"] = 1,
+        ["Icon"] = "rbxassetid://15185586450",
+        ["Desc"] = "function: 0x00000000aa9f1adc"
+    },
+    ["Glittering"] = {
+        ["DiminishPowerThreshold"] = 0.25,
+        ["Rarity"] = "function: 0x00000000e14504a4",
+        ["Power"] = "function: 0x00000000d598203c",
+        ["DisplayName"] = "function: 0x00000000dd06871c",
+        ["BaseTier"] = 1,
+        ["_script"] = "Charm | Glittering",
+        ["_id"] = "Glittering",
+        ["MaxTier"] = 1,
+        ["Icon"] = "rbxassetid://15219668902",
+        ["Desc"] = "function: 0x000000001a6ffbd4"
+    },
+    ["Agility"] = {
+        ["Rarity"] = "function: 0x00000000582a6f14",
+        ["Power"] = "function: 0x000000001e9d928c",
+        ["DisplayName"] = "function: 0x00000000b581347c",
+        ["BaseTier"] = 1,
+        ["_script"] = "Charm | Agility",
+        ["_id"] = "Agility",
+        ["MaxTier"] = 1,
+        ["Icon"] = "rbxassetid://15185586077",
+        ["Desc"] = "function: 0x0000000067c0b204"
+    },
+    ["Bonus"] = {
+        ["Rarity"] = "function: 0x000000007c60206c",
+        ["Power"] = "function: 0x000000007a1d7c24",
+        ["DisplayName"] = "function: 0x000000005f3fd2ac",
+        ["BaseTier"] = 1,
+        ["_script"] = "Charm | Bonus",
+        ["_id"] = "Bonus",
+        ["MaxTier"] = 1,
+        ["Icon"] = "rbxassetid://15185586240",
+        ["Desc"] = "function: 0x00000000704da8bc"
+    },
+    ["Strength"] = {
+        ["Rarity"] = "function: 0x00000000deb51e84",
+        ["Power"] = "function: 0x0000000014b8211c",
+        ["DisplayName"] = "function: 0x00000000de8061bc",
+        ["BaseTier"] = 1,
+        ["_script"] = "Charm | Strength",
+        ["_id"] = "Strength",
+        ["MaxTier"] = 1,
+        ["Icon"] = "rbxassetid://15185586607",
+        ["Desc"] = "function: 0x00000000ee02c474"
+    },
+    ["Coins"] = {
+        ["Rarity"] = "function: 0x00000000b846e8e4",
+        ["Power"] = "function: 0x00000000c693977c",
+        ["DisplayName"] = "function: 0x000000002ac0e44c",
+        ["BaseTier"] = 1,
+        ["_script"] = "Charm | Coins",
+        ["_id"] = "Coins",
+        ["MaxTier"] = 1,
+        ["Icon"] = "rbxassetid://15185586825",
+        ["Desc"] = "function: 0x000000002f5fb194"
+    }
+}
