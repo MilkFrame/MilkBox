454e5b7cffee46ace9d34e35662de800765ed0db
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-13 01:00:44 +1100

    Create AbilityIcons.lua

---
+++ GameData/Blade Ball [13772394625]/AbilityIcons.lua
@@ -0,0 +1,23 @@
+data = {
+    ["Thunder Dash"] = "rbxassetid://14520169908",
+    ["Rapture"] = "rbxassetid://14722857062",
+    ["Phantom"] = "rbxassetid://15059341639",
+    ["Infinity"] = "rbxassetid://14852723022",
+    ["Super Jump"] = "rbxassetid://14021421360",
+    ["Quad Jump"] = "rbxassetid://15060531535",
+    ["Freeze"] = "rbxassetid://14852741510",
+    ["Swap"] = "rbxassetid://14919629746",
+    ["Wind Cloak"] = "rbxassetid://14775984681",
+    ["Waypoint"] = "rbxassetid://14846350280",
+    ["Dash"] = "rbxassetid://13735989240",
+    ["Platform"] = "rbxassetid://14377836406",
+    ["Telekinesis"] = "rbxassetid://14520967276",
+    ["Phase Bypass"] = "rbxassetid://14776121201",
+    ["Blink"] = "rbxassetid://15059338710",
+    ["Forcefield"] = "rbxassetid://14521165734",
+    ["Raging Deflection"] = "rbxassetid://14521129740",
+    ["Pull"] = "rbxassetid://14787894058",
+    ["Reaper"] = "rbxassetid://14970058573",
+    ["Shadow Step"] = "rbxassetid://14021534179",
+    ["Invisibility"] = "rbxassetid://14032499286"
+}
