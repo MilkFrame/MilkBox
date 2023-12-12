c5c47aeb878c67fcb6d62210685e5952ce8de0e9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-13 00:34:58 +1100

    Create SpinWheel.lua

---
+++ GameData/Blade Ball [13772394625]/SpinWheel.lua
@@ -0,0 +1,57 @@
+data = {
+    ["SpinWheelRewards"] = {
+        {
+            ["wheelText"] = "x2 Coins",
+            ["name"] = "twoTimes",
+            ["bottomText"] = "15m",
+            ["chance"] = 24,
+            ["displayText"] = "2x coins for 15 minutes",
+            ["icon"] = "rbxassetid://14737693993",
+            ["rewardText"] = "x2 Coins"
+        },
+        {
+            ["name"] = "crescendo",
+            ["bottomText"] = "CUSTOM VFX",
+            ["chance"] = 5,
+            ["displayText"] = "Crescendo",
+            ["icon"] = "rbxassetid://14744808828",
+            ["rewardText"] = "Crescendo"
+        },
+        {
+            ["wheelText"] = "Coins",
+            ["name"] = "fiveHundredCoins",
+            ["bottomText"] = "500",
+            ["chance"] = 13,
+            ["displayText"] = "five hundred coins",
+            ["icon"] = "rbxassetid://14737377983",
+            ["rewardText"] = "500 Coins"
+        },
+        {
+            ["wheelText"] = "Coins",
+            ["name"] = "hundredCoins",
+            ["bottomText"] = "250",
+            ["chance"] = 29,
+            ["displayText"] = "two hundred and fifty coins",
+            ["icon"] = "rbxassetid://14737356590",
+            ["rewardText"] = "250 Coins"
+        },
+        {
+            ["name"] = "rapture",
+            ["bottomText"] = "ABILITY",
+            ["chance"] = 2,
+            ["displayText"] = "Rapture",
+            ["icon"] = "rbxassetid://14737436876",
+            ["rewardText"] = "Rapture"
+        },
+        {
+            ["wheelText"] = "Instant Spin",
+            ["name"] = "instantSpin",
+            ["bottomText"] = "30m",
+            ["chance"] = 27,
+            ["displayText"] = "instant spin for 30 minutes",
+            ["icon"] = "rbxassetid://14737617340",
+            ["rewardText"] = "Instant Spin"
+        }
+    },
+    ["SpinWheelSettings"] = {["MAX_COIN_SPINS"] = 3, ["COIN_SPIN_COOLDOWN"] = 86400, ["COIN_SPIN_PRICE"] = 3000}
+}
