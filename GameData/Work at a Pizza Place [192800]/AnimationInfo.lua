4c5437ff6b306d32effcbea4d10f9d62ece71648
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-13 01:20:48 +1100

    Create AnimationInfo.lua

---
+++ GameData/Work at a Pizza Place [192800]/AnimationInfo.lua
@@ -0,0 +1,21 @@
+data = {
+    ["ColorShortcuts"] = {
+        ["Blue"] = Color3.fromRGB(102, 102, 255),
+        ["Red"] = Color3.fromRGB(255, 102, 102),
+        ["Black"] = Color3.fromRGB(0, 0, 0),
+        ["Green"] = Color3.fromRGB(102, 255, 102),
+        ["Cyan"] = Color3.fromRGB(102, 216, 255),
+        ["Yellow"] = Color3.fromRGB(255, 229, 51),
+        ["Orange"] = Color3.fromRGB(255, 127, 51),
+        ["White"] = Color3.fromRGB(255, 255, 255)
+    },
+    ["New"] = "function: 0x000000005bc7d343",
+    ["ContinueOverflow"] = "function: 0x0000000064776ecb",
+    ["ImageShortcuts"] = {
+        ["Despicable"] = 711674643,
+        ["Sad"] = 947900188,
+        ["Thinking"] = 955646496,
+        ["Happy"] = 414889555,
+        ["Eggplant"] = 639588687
+    }
+}
