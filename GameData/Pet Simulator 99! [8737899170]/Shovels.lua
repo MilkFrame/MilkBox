2bdd480989d4a29bb813968bcc20d1ed2d091fea
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-12 19:45:18 +1100

    Create Shovels.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/Shovels.lua
@@ -0,0 +1,60 @@
+data = {
+    ["Bluesteel Shovel"] = {
+        ["_script"] = "Shovel | Bluesteel Shovel",
+        ["_id"] = "Bluesteel Shovel",
+        ["AssociatedItemID"] = "Bluesteel Shovel",
+        ["Model"] = "Model",
+        ["MerchantSalePrice"] = 12500,
+        ["Desc"] = "Bluesteel cuts through solid rock easily!",
+        ["Icon"] = "rbxassetid://15246628696",
+        ["DisplayName"] = "Bluesteel Shovel"
+    },
+    ["Flimsy Shovel"] = {
+        ["_script"] = "Shovel | Flimsy Shovel",
+        ["_id"] = "Flimsy Shovel",
+        ["AssociatedItemID"] = "Flimsy Shovel",
+        ["Model"] = "Model",
+        ["Desc"] = "Really flimsy. Doesn't dig very fast.",
+        ["Icon"] = "rbxassetid://15246628328",
+        ["DisplayName"] = "Flimsy Shovel"
+    },
+    ["Pro Shovel"] = {
+        ["_script"] = "Shovel | Pro Shovel",
+        ["_id"] = "Pro Shovel",
+        ["AssociatedItemID"] = "Pro Shovel",
+        ["Model"] = "Model",
+        ["MerchantSalePrice"] = 250000,
+        ["Desc"] = "For professionals only! Digs super fast!",
+        ["Icon"] = "rbxassetid://15501008283",
+        ["DisplayName"] = "Pro Shovel"
+    },
+    ["Normal Shovel"] = {
+        ["_script"] = "Shovel | Normal Shovel",
+        ["_id"] = "Normal Shovel",
+        ["AssociatedItemID"] = "Normal Shovel",
+        ["Model"] = "Model",
+        ["MerchantSalePrice"] = 2000,
+        ["Desc"] = "It's a shovel. It digs!",
+        ["Icon"] = "rbxassetid://15246628205",
+        ["DisplayName"] = "Normal Shovel"
+    },
+    ["Sharp Shovel"] = {
+        ["_script"] = "Shovel | Sharp Shovel",
+        ["_id"] = "Sharp Shovel",
+        ["AssociatedItemID"] = "Sharp Shovel",
+        ["Model"] = "Model",
+        ["MerchantSalePrice"] = 55000,
+        ["Desc"] = "Sharpened really carefully. Digs really fast!",
+        ["Icon"] = "rbxassetid://15246628099",
+        ["DisplayName"] = "Sharp Shovel"
+    },
+    ["Golden Shovel"] = {
+        ["_script"] = "Shovel | Golden Shovel",
+        ["AssociatedItemID"] = "Golden Shovel",
+        ["Model"] = "Model",
+        ["_id"] = "Golden Shovel",
+        ["Desc"] = "Shiny! Digs insanely fast!",
+        ["Icon"] = "rbxassetid://15491866587",
+        ["DisplayName"] = "Golden Shovel"
+    }
+}
