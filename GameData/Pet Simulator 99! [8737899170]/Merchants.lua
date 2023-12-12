2b65dbdc97a59ba81aa4ab3b56bb367ab61f9fe1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-12 19:40:41 +1100

    Create Merchants.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/Merchants.lua
@@ -0,0 +1,18 @@
+data = {
+    ["RegularMerchant"] = {
+        ["_script"] = "RegularMerchant",
+        ["MachineName"] = "RegularMerchant",
+        ["_id"] = "RegularMerchant",
+        ["GetOffers"] = "function: 0x00000000d1922858",
+        ["RefreshRate"] = 1800,
+        ["DisplayName"] = "Items Merchant"
+    },
+    ["AdvancedMerchant"] = {
+        ["_script"] = "AdvancedMerchant",
+        ["MachineName"] = "AdvancedMerchant",
+        ["_id"] = "AdvancedMerchant",
+        ["GetOffers"] = "function: 0x000000006319eaa0",
+        ["RefreshRate"] = 3600,
+        ["DisplayName"] = "Advanced Merchant"
+    }
+}
