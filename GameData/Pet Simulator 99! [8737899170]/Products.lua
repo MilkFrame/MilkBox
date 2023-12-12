79c16c14097f4b3de5524ca94f5f75d95ef32f0d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-12 19:26:36 +1100

    Create Products.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/Products.lua
@@ -0,0 +1,290 @@
+data = {
+    ["Enchant Slot 3"] = {
+        ["_script"] = "Product | Enchant Slot 3",
+        ["_id"] = "Enchant Slot 3",
+        ["Callback"] = "function: 0x0000000000c167fc",
+        ["ProductId"] = 1647763864,
+        ["Desc"] = "Get an extra enchant slot!",
+        ["ClientTest"] = "function: 0x000000009be31b24",
+        ["Icon"] = "rbxassetid://7599295493",
+        ["DisplayName"] = "Enchant Slot 3"
+    },
+    ["Medium Diamond Pack"] = {
+        ["_script"] = "Product | Medium Diamond Pack",
+        ["_id"] = "Medium Diamond Pack",
+        ["Callback"] = "function: 0x0000000037bf39ac",
+        ["ProductId"] = 1666573574,
+        ["Desc"] = "Purchase 50,000 Diamonds!",
+        ["ClientTest"] = "function: 0x00000000b319faf4",
+        ["Icon"] = "rbxassetid://7599295493",
+        ["DisplayName"] = "Medium Diamond Pack"
+    },
+    ["Expert Starter Pack"] = {
+        ["ItemDisplays"] = {
+            {["Item"] = {["_data"] = {["id"] = "Exclusive Egg 17"}}, ["Amount"] = 3},
+            {["Item"] = {["_data"] = {["id"] = "Diamonds", ["_am"] = 30000}}},
+            {["Item"] = {["_data"] = {["id"] = "Lightning", ["tn"] = 1}}},
+            {["Item"] = {["_data"] = {["id"] = "Coins", ["_am"] = 6, ["tn"] = 5}}},
+            {["Item"] = {["_data"] = {["id"] = "Damage", ["_am"] = 6, ["tn"] = 5}}}
+        },
+        ["_id"] = "Expert Starter Pack",
+        ["ClientTest"] = "function: 0x000000001a6a353c",
+        ["Callback"] = "function: 0x00000000171a0134",
+        ["DisplayName"] = "Expert Starter Pack",
+        ["_script"] = "Product | Expert Starter Pack",
+        ["ProductId"] = 1648424261,
+        ["Icon"] = "rbxassetid://7599295493",
+        ["Desc"] = "Get a massive head start and receive an exclusive hoverboard, booth, enchants, potions and more!"
+    },
+    ["Neon Twilight Booth"] = {
+        ["_script"] = "Product | Neon Twilight Booth",
+        ["ItemDisplays"] = {{["Item"] = {["_data"] = {["id"] = "Neon Twilight"}}}},
+        ["_id"] = "Neon Twilight Booth",
+        ["ProductId"] = 1681635105,
+        ["Callback"] = "function: 0x00000000fae3d7e4",
+        ["Desc"] = "",
+        ["Icon"] = "rbxassetid://7599295493",
+        ["DisplayName"] = "Neon Twilight Booth"
+    },
+    ["Huge Machine 2"] = {
+        ["_script"] = "Product | Huge Machine 2",
+        ["_id"] = "Huge Machine 2",
+        ["ProductId"] = 1665959824,
+        ["Callback"] = "function: 0x00000000dd99cf14",
+        ["Desc"] = "Redeem Tier 2 Huge Machine Conversion",
+        ["Icon"] = "rbxassetid://7599295493",
+        ["DisplayName"] = "Huge Machine 2"
+    },
+    ["10 Exclusive Egg"] = {
+        ["_script"] = "Product | 10 Exclusive Egg",
+        ["_id"] = "10 Exclusive Egg",
+        ["Callback"] = "function: 0x0000000056bb716c",
+        ["ProductId"] = 1674573396,
+        ["Desc"] = "Buy 10 Exclusive Egg!",
+        ["ClientTest"] = "function: 0x000000005f2fad24",
+        ["Icon"] = "rbxassetid://15163625617",
+        ["DisplayName"] = "10 Exclusive Eggs"
+    },
+    ["Lucky Block Enchant"] = {
+        ["_script"] = "Product | Lucky Block Enchant",
+        ["ItemDisplays"] = {{["Item"] = {["_data"] = {["id"] = "Lucky Block", ["tn"] = 1}}}},
+        ["_id"] = "Lucky Block Enchant",
+        ["ProductId"] = 1674740376,
+        ["Callback"] = "function: 0x00000000b9818f6c",
+        ["Desc"] = "",
+        ["Icon"] = "rbxassetid://15257718885",
+        ["DisplayName"] = "Lucky Block"
+    },
+    ["Enchant Slot 2"] = {
+        ["_script"] = "Product | Enchant Slot 2",
+        ["_id"] = "Enchant Slot 2",
+        ["Callback"] = "function: 0x000000009a40b35c",
+        ["ProductId"] = 1578347131,
+        ["Desc"] = "Get an extra enchant slot!",
+        ["ClientTest"] = "function: 0x000000000f677004",
+        ["Icon"] = "rbxassetid://7599295493",
+        ["DisplayName"] = "Enchant Slot 2"
+    },
+    ["Balloon Hoverboard"] = {
+        ["_script"] = "Product | Balloon Hoverboard",
+        ["ItemDisplays"] = {{["Item"] = {["_data"] = {["id"] = "Balloon"}}}},
+        ["_id"] = "Balloon Hoverboard",
+        ["ProductId"] = 1683039053,
+        ["Callback"] = "function: 0x0000000032745df4",
+        ["Desc"] = "",
+        ["Icon"] = "rbxassetid://15280246134",
+        ["DisplayName"] = "Balloon Hoverboard"
+    },
+    ["High Tech Hoverboard"] = {
+        ["_script"] = "Product | High Tech Hoverboard",
+        ["ItemDisplays"] = {{["Item"] = {["_data"] = {["id"] = "High Tech"}}}},
+        ["_id"] = "High Tech Hoverboard",
+        ["ProductId"] = 1683039434,
+        ["Callback"] = "function: 0x00000000ce41dfdc",
+        ["Desc"] = "",
+        ["Icon"] = "rbxassetid://14910757075",
+        ["DisplayName"] = "High-Tech Hoverboard"
+    },
+    ["Fire Dragon Hoverboard"] = {
+        ["_script"] = "Product | Fire Dragon Hoverboard",
+        ["ItemDisplays"] = {{["Item"] = {["_data"] = {["id"] = "Fire Dragon"}}}},
+        ["_id"] = "Fire Dragon Hoverboard",
+        ["ProductId"] = 1683039656,
+        ["Callback"] = "function: 0x00000000d7a71654",
+        ["Desc"] = "",
+        ["Icon"] = "rbxassetid://15164150883",
+        ["DisplayName"] = "Fire Dragon Hoverboard"
+    },
+    ["Tiny Diamond Pack"] = {
+        ["_script"] = "Product | Tiny Diamond Pack",
+        ["_id"] = "Tiny Diamond Pack",
+        ["Callback"] = "function: 0x00000000e0db9284",
+        ["ProductId"] = 1666571931,
+        ["Desc"] = "Purchase 12,000 Diamonds!",
+        ["ClientTest"] = "function: 0x000000008b39914c",
+        ["Icon"] = "rbxassetid://7599295493",
+        ["DisplayName"] = "Tiny Diamond Pack"
+    },
+    ["Clan Voucher"] = {
+        ["_script"] = "Product | Clan Voucher",
+        ["ItemDisplays"] = {{["Item"] = {["_data"] = {["id"] = "Clan Voucher"}}}},
+        ["_id"] = "Clan Voucher",
+        ["ProductId"] = 1682471489,
+        ["Callback"] = "function: 0x00000000140d4944",
+        ["Desc"] = "",
+        ["Icon"] = "",
+        ["DisplayName"] = "Clan Voucher"
+    },
+    ["Booth Slot Voucher"] = {
+        ["_script"] = "Product | Booth Slot Voucher",
+        ["ItemDisplays"] = {{["Item"] = {["_data"] = {["id"] = "Booth Slot Voucher"}}}},
+        ["_id"] = "Booth Slot Voucher",
+        ["ProductId"] = 1683043466,
+        ["Callback"] = "function: 0x000000000f1449bc",
+        ["Desc"] = "",
+        ["Icon"] = "",
+        ["DisplayName"] = "Booth Slot Voucher"
+    },
+    ["Chest Mimic Enchant"] = {
+        ["_script"] = "Product | Chest Mimic Enchant",
+        ["ItemDisplays"] = {{["Item"] = {["_data"] = {["id"] = "Chest Mimic", ["tn"] = 1}}}},
+        ["_id"] = "Chest Mimic Enchant",
+        ["ProductId"] = 1674746599,
+        ["Callback"] = "function: 0x00000000e18a80b4",
+        ["Desc"] = "",
+        ["Icon"] = "rbxassetid://15257719276",
+        ["DisplayName"] = "Chest Mimic"
+    },
+    ["Exclusive Egg"] = {
+        ["_script"] = "Product | Exclusive Egg",
+        ["_id"] = "Exclusive Egg",
+        ["Callback"] = "function: 0x000000000bda7294",
+        ["ProductId"] = 1674572193,
+        ["Desc"] = "Buy 1 Exclusive Egg!",
+        ["ClientTest"] = "function: 0x00000000d401cf0c",
+        ["Icon"] = "rbxassetid://15163625617",
+        ["DisplayName"] = "Exclusive Egg"
+    },
+    ["Spinny Wheel Ticket"] = {
+        ["_script"] = "Product | Spinny Wheel Ticket",
+        ["ItemDisplays"] = {{["Item"] = {["_data"] = {["id"] = "Spinny Wheel Ticket", ["_am"] = 5}}}},
+        ["_id"] = "Spinny Wheel Ticket",
+        ["ProductId"] = 1682471258,
+        ["Callback"] = "function: 0x000000009fba32cc",
+        ["Desc"] = "",
+        ["Icon"] = "",
+        ["DisplayName"] = "Spinny Wheel Ticket"
+    },
+    ["100 Exclusive Egg"] = {
+        ["_script"] = "Product | 100 Exclusive Egg",
+        ["_id"] = "100 Exclusive Egg",
+        ["Callback"] = "function: 0x000000007dfae744",
+        ["ProductId"] = 1674573497,
+        ["Desc"] = "Buy 100 Exclusive Egg!",
+        ["ClientTest"] = "function: 0x000000006ae3b2b4",
+        ["Icon"] = "rbxassetid://15163625617",
+        ["DisplayName"] = "100 Exclusive Eggs"
+    },
+    ["Starter Pack"] = {
+        ["ItemDisplays"] = {
+            {["Item"] = {["_data"] = {["id"] = "Exclusive Egg 17"}}},
+            {["Item"] = {["_data"] = {["id"] = "Damage", ["_am"] = 2, ["tn"] = 5}}},
+            {["Item"] = {["_data"] = {["id"] = "Cat"}}},
+            {["Item"] = {["_data"] = {["id"] = "Magnet", ["tn"] = 3}}},
+            {["Item"] = {["_data"] = {["id"] = "Diamonds", ["_am"] = 5000}}}
+        },
+        ["_id"] = "Starter Pack",
+        ["ClientTest"] = "function: 0x00000000741e8644",
+        ["Callback"] = "function: 0x00000000d4e46034",
+        ["DisplayName"] = "Starter Pack",
+        ["_script"] = "Product | Starter Pack",
+        ["ProductId"] = 1648407160,
+        ["Icon"] = "rbxassetid://7599295493",
+        ["Desc"] = "Get a head start and receive an exclusive hoverboard, booth, enchants, potions and more!"
+    },
+    ["Massive Diamond Pack"] = {
+        ["_script"] = "Product | Massive Diamond Pack",
+        ["_id"] = "Massive Diamond Pack",
+        ["Callback"] = "function: 0x0000000084e6761c",
+        ["ProductId"] = 1666574226,
+        ["Desc"] = "Purchase 500,000 Diamonds!",
+        ["ClientTest"] = "function: 0x000000002ddab9c4",
+        ["Icon"] = "rbxassetid://7599295493",
+        ["DisplayName"] = "Massive Diamond Pack"
+    },
+    ["Enchant Slot 1"] = {
+        ["_script"] = "Product | Enchant Slot 1",
+        ["_id"] = "Enchant Slot 1",
+        ["Callback"] = "function: 0x00000000cfc3c83c",
+        ["ProductId"] = 1578342463,
+        ["Desc"] = "Get an extra enchant slot!",
+        ["ClientTest"] = "function: 0x0000000064e03b64",
+        ["Icon"] = "rbxassetid://7599295493",
+        ["DisplayName"] = "Enchant Slot 1"
+    },
+    ["Large Diamond Pack"] = {
+        ["_script"] = "Product | Large Diamond Pack",
+        ["_id"] = "Large Diamond Pack",
+        ["Callback"] = "function: 0x00000000479ac654",
+        ["ProductId"] = 1666573884,
+        ["Desc"] = "Purchase 240,000 Diamonds!",
+        ["ClientTest"] = "function: 0x00000000e179237c",
+        ["Icon"] = "rbxassetid://7599295493",
+        ["DisplayName"] = "Large Diamond Pack"
+    },
+    ["3 Exclusive Egg"] = {
+        ["_script"] = "Product | 3 Exclusive Egg",
+        ["_id"] = "3 Exclusive Egg",
+        ["Callback"] = "function: 0x0000000034664b84",
+        ["ProductId"] = 1674572681,
+        ["Desc"] = "Buy 3 Exclusive Egg!",
+        ["ClientTest"] = "function: 0x00000000bef4fe1c",
+        ["Icon"] = "rbxassetid://15163625617",
+        ["DisplayName"] = "3 Exclusive Eggs"
+    },
+    ["Massive Potion Bundle"] = {
+        ["_script"] = "Product | Massive Potion Bundle",
+        ["ItemDisplays"] = {
+            {["Item"] = {["_data"] = {["id"] = "Coins", ["_am"] = 3, ["tn"] = 5}}},
+            {["Item"] = {["_data"] = {["id"] = "Damage", ["_am"] = 3, ["tn"] = 5}}},
+            {["Item"] = {["_data"] = {["id"] = "Lucky", ["_am"] = 3, ["tn"] = 5}}},
+            {["Item"] = {["_data"] = {["id"] = "Treasure Hunter", ["_am"] = 3, ["tn"] = 5}}},
+            {["Item"] = {["_data"] = {["id"] = "Diamonds", ["_am"] = 3, ["tn"] = 3}}}
+        },
+        ["_id"] = "Massive Potion Bundle",
+        ["ProductId"] = 1674721564,
+        ["Callback"] = "function: 0x00000000baef2f8c",
+        ["Desc"] = "Get a head start and receive an exclusive hoverboard, booth, enchants, potions and more!",
+        ["Icon"] = "rbxassetid://7599295493",
+        ["DisplayName"] = "Massive Potion Bundle"
+    },
+    ["Glitched Booth"] = {
+        ["_script"] = "Product | Glitched Booth",
+        ["ItemDisplays"] = {{["Item"] = {["_data"] = {["id"] = "Glitched"}}}},
+        ["_id"] = "Glitched Booth",
+        ["ProductId"] = 1681640424,
+        ["Callback"] = "function: 0x00000000eaefe55c",
+        ["Desc"] = "",
+        ["Icon"] = "rbxassetid://7599295493",
+        ["DisplayName"] = "Glitched Booth"
+    },
+    ["Fireworks Enchant"] = {
+        ["_script"] = "Product | Fireworks Enchant",
+        ["ItemDisplays"] = {{["Item"] = {["_data"] = {["id"] = "Fireworks", ["tn"] = 1}}}},
+        ["_id"] = "Fireworks Enchant",
+        ["ProductId"] = 1674740229,
+        ["Callback"] = "function: 0x000000003da3c8ec",
+        ["Desc"] = "",
+        ["Icon"] = "rbxassetid://15257719053",
+        ["DisplayName"] = "Fireworks"
+    },
+    ["Huge Machine 1"] = {
+        ["_script"] = "Product | Huge Machine 1",
+        ["_id"] = "Huge Machine 1",
+        ["ProductId"] = 1665959624,
+        ["Callback"] = "function: 0x00000000b810006c",
+        ["Desc"] = "Redeem Tier 1 Huge Machine Conversion",
+        ["Icon"] = "rbxassetid://7599295493",
+        ["DisplayName"] = "Huge Machine 1"
+    }
+}
