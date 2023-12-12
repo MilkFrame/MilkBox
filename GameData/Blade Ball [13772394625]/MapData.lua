1326d85987624e4df86923eee36c1afa34ff5df9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-13 01:06:07 +1100

    Create MapData.lua

---
+++ GameData/Blade Ball [13772394625]/MapData.lua
@@ -0,0 +1,114 @@
+data = {
+    ["Olympus"] = {
+        ["Image"] = "rbxassetid://15122465790",
+        ["MinBallHeight"] = 116.703,
+        ["Id"] = 5,
+        ["DistanceToSpawnFrom"] = 100,
+        ["HoverImage"] = "rbxassetid://15122465534",
+        ["DisplayName"] = "Olympus"
+    },
+    ["RingOfFire"] = {
+        ["Image"] = "rbxassetid://15122132021",
+        ["MinBallHeight"] = 5,
+        ["Id"] = 4,
+        ["DistanceToSpawnFrom"] = 125,
+        ["HoverImage"] = "rbxassetid://15122131610",
+        ["DisplayName"] = "Ring Of Fire"
+    },
+    ["Height_Fight"] = {
+        ["Image"] = "rbxassetid://15122466321",
+        ["MinBallHeight"] = 116.703,
+        ["Id"] = 2,
+        ["DistanceToSpawnFrom"] = 125,
+        ["HoverImage"] = "rbxassetid://15122466060",
+        ["DisplayName"] = "Height Fight"
+    },
+    ["Desert"] = {
+        ["Image"] = "rbxassetid://15122129270",
+        ["MinBallHeight"] = 5.75,
+        ["Id"] = 6,
+        ["DistanceToSpawnFrom"] = 125,
+        ["HoverImage"] = "rbxassetid://15122129068",
+        ["DisplayName"] = "Desert"
+    },
+    ["Birdcage"] = {
+        ["Image"] = "rbxassetid://15122127708",
+        ["MinBallHeight"] = 89,
+        ["Id"] = 1,
+        ["DistanceToSpawnFrom"] = 125,
+        ["HoverImage"] = "rbxassetid://15122127399",
+        ["DisplayName"] = "Birdcage"
+    },
+    ["Halloween_Graveyard"] = {
+        ["Image"] = "rbxassetid://15122130764",
+        ["MinBallHeight"] = 109.5,
+        ["Id"] = 13,
+        ["DistanceToSpawnFrom"] = 125,
+        ["HoverImage"] = "rbxassetid://15122130539",
+        ["DisplayName"] = "Graveyard"
+    },
+    ["Halloween_Colloseum"] = {
+        ["Image"] = "rbxassetid://15122130326",
+        ["MinBallHeight"] = 14.4,
+        ["Id"] = 14,
+        ["DistanceToSpawnFrom"] = 125,
+        ["HoverImage"] = "rbxassetid://15122130029",
+        ["DisplayName"] = "Colloseum"
+    },
+    ["Quarry"] = {
+        ["Image"] = "rbxassetid://15122466949",
+        ["MinBallHeight"] = 5.75,
+        ["Id"] = 12,
+        ["DistanceToSpawnFrom"] = 125,
+        ["HoverImage"] = "rbxassetid://15122466607",
+        ["DisplayName"] = "Quarry"
+    },
+    ["Classic_V2"] = {
+        ["Image"] = "rbxassetid://15122128747",
+        ["MinBallHeight"] = 110.7,
+        ["Id"] = 10,
+        ["DistanceToSpawnFrom"] = 125,
+        ["HoverImage"] = "rbxassetid://15122128523",
+        ["DisplayName"] = "Classic V2"
+    },
+    ["Grassy_Classic"] = {
+        ["Image"] = "rbxassetid://15122129728",
+        ["MinBallHeight"] = 14.4,
+        ["Id"] = 7,
+        ["DistanceToSpawnFrom"] = 125,
+        ["HoverImage"] = "rbxassetid://15122129483",
+        ["DisplayName"] = "Grassy Classic"
+    },
+    ["Red_Sands"] = {
+        ["Image"] = "rbxassetid://15122131284",
+        ["MinBallHeight"] = 3.8,
+        ["Id"] = 9,
+        ["DistanceToSpawnFrom"] = 125,
+        ["HoverImage"] = "rbxassetid://15122130989",
+        ["DisplayName"] = "Red Sands"
+    },
+    ["Arena"] = {
+        ["Image"] = "rbxassetid://15122127147",
+        ["MinBallHeight"] = 10,
+        ["Id"] = 3,
+        ["DistanceToSpawnFrom"] = 125,
+        ["HoverImage"] = "rbxassetid://15122126711",
+        ["DisplayName"] = "Arena"
+    },
+    ["Classic"] = {
+        ["Image"] = "rbxassetid://15122128274",
+        ["MinBallHeight"] = 27,
+        ["Id"] = 8,
+        ["DistanceToSpawnFrom"] = 125,
+        ["HoverImage"] = "rbxassetid://15122128067",
+        ["DisplayName"] = "Classic"
+    },
+    ["SakuraTemple"] = {
+        ["Image"] = "rbxassetid://15122132585",
+        ["MinBallHeight"] = 14.4,
+        ["Id"] = 11,
+        ["DistanceToSpawnFrom"] = 125,
+        ["HoverImage"] = "rbxassetid://15122132376",
+        ["DisplayName"] = "Sakura Temple"
+    }
+}
