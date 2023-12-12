bd9886eec3402e01e33d3fa7361269ed2903c217
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-13 00:58:44 +1100

    Create RankedSeasonData.lua

---
+++ GameData/Blade Ball [13772394625]/RankedSeasonData.lua
@@ -0,0 +1,8 @@
+data = {
+    ["CurrentSeason"] = 1,
+    ["Seasons"] = {{["EndTime"] = "1698760800000", ["StartTime"] = "1696122000000"}},
+    ["Modes"] = {
+        ["FFA"] = {["PlayersPerTeam"] = 1, ["MaxTeams"] = 8, ["DisplayName"] = "FFA"},
+        ["Duo"] = {["PlayersPerTeam"] = 2, ["MaxTeams"] = 8, ["DisplayName"] = "2v2 Duos"}
+    }
+}
