dec75892ae9e6e310b0bf1749ae88776389201ed
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-12 19:33:40 +1100

    Create Rebirths.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/Rebirths.lua
@@ -0,0 +1,75 @@
+data = {
+    ["1"] = {
+        ["_id"] = "1",
+        ["StrengthPowerBoost"] = 75,
+        ["DisplayName"] = "Rebirth One",
+        ["_script"] = "Rebirth | 1",
+        ["BoostDesc"] = "+75% Pet Damage",
+        ["ZoneNumberRequired"] = 25,
+        ["ItemRewards"] = {
+            {["_data"] = {["id"] = "Diamonds", ["_am"] = 10000}},
+            {["_data"] = {["id"] = "Castle Key"}},
+            {["_data"] = {["id"] = "Basic Coin Jar"}},
+            {["_data"] = {["id"] = "Giant Coin Jar"}},
+            {["_data"] = {["id"] = "Comet"}},
+            {["_data"] = {["id"] = "TNT", ["_am"] = 5}},
+            {["_data"] = {["id"] = "TNT Crate"}}
+        },
+        ["RebirthNumber"] = 1,
+        ["RebirthUnlocks"] = {
+            {
+                ["Title"] = "Teleport",
+                ["Icon"] = "rbxassetid://15048276980",
+                ["Desc"] = "Travel anywhere by portable cannon!"
+            },
+            {
+                ["Title"] = "+75% Pet Strength",
+                ["GuiTitle"] = "Strength",
+                ["Icon"] = "rbxassetid://15257082607",
+                ["Desc"] = "Pets are 75% stronger, permanently!"
+            },
+            {
+                ["Title"] = "Castle Key",
+                ["Icon"] = "rbxassetid://15055553528",
+                ["Desc"] = "Access to Trading Plaza, Spinny Wheel, and more!"
+            },
+            {["Title"] = "Rewards", ["Icon"] = "rbxassetid://15048278140", ["Desc"] = "A bunch of rewards!"}
+        }
+    },
+    ["2"] = {
+        ["_id"] = "2",
+        ["StrengthPowerBoost"] = 75,
+        ["DisplayName"] = "Rebirth Two",
+        ["RebirthCallback"] = "function: 0x000000002465c350",
+        ["BoostDesc"] = "+150% Pet Damage",
+        ["ZoneNumberRequired"] = 50,
+        ["ItemRewards"] = {
+            {["_data"] = {["id"] = "Diamonds", ["_am"] = 25000}},
+            {["_data"] = {["id"] = "Nametag"}},
+            {["_data"] = {["id"] = "Basic Coin Jar", ["_am"] = 2}},
+            {["_data"] = {["id"] = "Giant Coin Jar", ["_am"] = 2}},
+            {["_data"] = {["id"] = "Comet", ["_am"] = 2}},
+            {["_data"] = {["id"] = "Toy Bone", ["_am"] = 2}},
+            {["_data"] = {["id"] = "Squeaky Toy", ["_am"] = 2}},
+            {["_data"] = {["id"] = "Toy Ball", ["_am"] = 2}},
+            {["_data"] = {["id"] = "Hasty Flag", ["_am"] = 2}},
+            {["_data"] = {["id"] = "Magnet Flag", ["_am"] = 2}},
+            {["_data"] = {["id"] = "TNT", ["_am"] = 5}},
+            {["_data"] = {["id"] = "TNT Crate"}}
+        },
+        ["RebirthNumber"] = 2,
+        ["_script"] = "Rebirth | 2",
+        ["RebirthUnlocks"] = {
+            {["Title"] = "Clans", ["Icon"] = "rbxassetid://15048278534", ["Desc"] = "Join clans and earn clan rewards!"},
+            {["Title"] = "Auto Hatch", ["Icon"] = "rbxassetid://15209657961", ["Desc"] = "Hatch eggs faster!"},
+            {
+                ["Title"] = "+75% Pet Strength",
+                ["GuiTitle"] = "Strength",
+                ["Icon"] = "rbxassetid://15257082607",
+                ["Desc"] = "Pets are 75% stronger, permanently!"
+            },
+            {["Title"] = "Active Huge Pets", ["Icon"] = "rbxassetid://14976374906", ["Desc"] = "Can hatch huge pets!"},
+            {["Title"] = "Rewards", ["Icon"] = "rbxassetid://15055562843", ["Desc"] = "A bunch of rewards!"}
+        }
+    }
+}
