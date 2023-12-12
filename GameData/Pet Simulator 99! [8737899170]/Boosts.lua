85b5a1ebcd3a870de6d6bc14688c602ea407b675
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-12 19:42:52 +1100

    Create Boosts.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/Boosts.lua
@@ -0,0 +1,9 @@
+data = {
+    ["Friends"] = {
+        ["_script"] = "Boost | Friends",
+        ["_id"] = "Friends",
+        ["MaximumPercent"] = 100,
+        ["Icon"] = "rbxassetid://15210563507",
+        ["DisplayName"] = "Friends Boost"
+    }
+}
