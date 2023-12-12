a28e61f9fb488837d381b5f9c64f23fc477a04f5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-13 00:35:24 +1100

    Create RankData.lua

---
+++ GameData/Blade Ball [13772394625]/RankData.lua
@@ -0,0 +1,140 @@
+data = {
+    ["GetNextRank"] = "function: 0x000000004070c7e6",
+    ["RankList"] = {
+        {
+            ["Name"] = "Rookie",
+            ["MinimumElo"] = 0,
+            ["Icon"] = "rbxassetid://14916629523",
+            ["MaximumElo"] = 1999,
+            ["TextColor"] = Color3.fromRGB(91, 174, 102)
+        },
+        {
+            ["Name"] = "Bronze",
+            ["MinimumElo"] = 2000,
+            ["Reward"] = {{["CurrencyName"] = "Coins", ["Icon"] = "rbxassetid://15038384377", ["Amount"] = 75}},
+            ["Icon"] = "rbxassetid://14916630678",
+            ["MaximumElo"] = 2999,
+            ["TextColor"] = Color3.fromRGB(200, 132, 93)
+        },
+        {
+            ["Name"] = "Silver",
+            ["MinimumElo"] = 3000,
+            ["Reward"] = {{["CurrencyName"] = "Coins", ["Icon"] = "rbxassetid://15038384377", ["Amount"] = 100}},
+            ["Icon"] = "rbxassetid://14916629343",
+            ["MaximumElo"] = 3999,
+            ["TextColor"] = Color3.fromRGB(161, 175, 181)
+        },
+        {
+            ["Name"] = "Gold",
+            ["MinimumElo"] = 4000,
+            ["Reward"] = {{["CurrencyName"] = "Coins", ["Icon"] = "rbxassetid://15038384377", ["Amount"] = 200}},
+            ["Icon"] = "rbxassetid://14916630089",
+            ["MaximumElo"] = 4999,
+            ["TextColor"] = Color3.fromRGB(236, 194, 39)
+        },
+        {
+            ["Name"] = "Platinum",
+            ["MinimumElo"] = 5000,
+            ["Reward"] = {{["CurrencyName"] = "Coins", ["Icon"] = "rbxassetid://15038384377", ["Amount"] = 375}},
+            ["Icon"] = "rbxassetid://14916629671",
+            ["MaximumElo"] = 5999,
+            ["TextColor"] = Color3.fromRGB(228, 242, 248)
+        },
+        {
+            ["Name"] = "Diamond",
+            ["MinimumElo"] = 6000,
+            ["Reward"] = {{["CurrencyName"] = "Coins", ["Icon"] = "rbxassetid://15038384377", ["Amount"] = 750}},
+            ["Icon"] = "rbxassetid://14916630252",
+            ["MaximumElo"] = 6999,
+            ["TextColor"] = Color3.fromRGB(83, 231, 255)
+        },
+        {
+            ["Name"] = "???",
+            ["MinimumElo"] = 7000,
+            ["Reward"] = {{["CurrencyName"] = "Coins", ["Icon"] = "rbxassetid://15038384377", ["Amount"] = 1500}},
+            ["Icon"] = "rbxassetid://14916629907",
+            ["MaximumElo"] = 7999,
+            ["TextColor"] = Color3.fromRGB(255, 82, 255)
+        },
+        {
+            ["Name"] = "Champion",
+            ["MinimumElo"] = 8000,
+            ["Reward"] = {
+                {["CurrencyName"] = "Coins", ["Icon"] = "rbxassetid://15038384377", ["Amount"] = 2000},
+                {["SwordName"] = "Cyber Cleaveblade", ["Icon"] = "rbxassetid://15106177980"}
+            },
+            ["Icon"] = "rbxassetid://14916630439",
+            ["MaximumElo"] = "inf",
+            ["TextColor"] = Color3.fromRGB(0, 124, 215)
+        }
+    },
+    ["GetRank"] = "function: 0x0000000064437b3e",
+    ["Ranks"] = {
+        ["Platinum"] = {
+            ["Name"] = "Platinum",
+            ["MinimumElo"] = 5000,
+            ["Reward"] = {{["CurrencyName"] = "Coins", ["Icon"] = "rbxassetid://15038384377", ["Amount"] = 375}},
+            ["Icon"] = "rbxassetid://14916629671",
+            ["MaximumElo"] = 5999,
+            ["TextColor"] = Color3.fromRGB(228, 242, 248)
+        },
+        ["Diamond"] = {
+            ["Name"] = "Diamond",
+            ["MinimumElo"] = 6000,
+            ["Reward"] = {{["CurrencyName"] = "Coins", ["Icon"] = "rbxassetid://15038384377", ["Amount"] = 750}},
+            ["Icon"] = "rbxassetid://14916630252",
+            ["MaximumElo"] = 6999,
+            ["TextColor"] = Color3.fromRGB(83, 231, 255)
+        },
+        ["Silver"] = {
+            ["Name"] = "Silver",
+            ["MinimumElo"] = 3000,
+            ["Reward"] = {{["CurrencyName"] = "Coins", ["Icon"] = "rbxassetid://15038384377", ["Amount"] = 100}},
+            ["Icon"] = "rbxassetid://14916629343",
+            ["MaximumElo"] = 3999,
+            ["TextColor"] = Color3.fromRGB(161, 175, 181)
+        },
+        ["Gold"] = {
+            ["Name"] = "Gold",
+            ["MinimumElo"] = 4000,
+            ["Reward"] = {{["CurrencyName"] = "Coins", ["Icon"] = "rbxassetid://15038384377", ["Amount"] = 200}},
+            ["Icon"] = "rbxassetid://14916630089",
+            ["MaximumElo"] = 4999,
+            ["TextColor"] = Color3.fromRGB(236, 194, 39)
+        },
+        ["???"] = {
+            ["Name"] = "???",
+            ["MinimumElo"] = 7000,
+            ["Reward"] = {{["CurrencyName"] = "Coins", ["Icon"] = "rbxassetid://15038384377", ["Amount"] = 1500}},
+            ["Icon"] = "rbxassetid://14916629907",
+            ["MaximumElo"] = 7999,
+            ["TextColor"] = Color3.fromRGB(255, 82, 255)
+        },
+        ["Champion"] = {
+            ["Name"] = "Champion",
+            ["MinimumElo"] = 8000,
+            ["Reward"] = {
+                {["CurrencyName"] = "Coins", ["Icon"] = "rbxassetid://15038384377", ["Amount"] = 2000},
+                {["SwordName"] = "Cyber Cleaveblade", ["Icon"] = "rbxassetid://15106177980"}
+            },
+            ["Icon"] = "rbxassetid://14916630439",
+            ["MaximumElo"] = "inf",
+            ["TextColor"] = Color3.fromRGB(0, 124, 215)
+        },
+        ["Bronze"] = {
+            ["Name"] = "Bronze",
+            ["MinimumElo"] = 2000,
+            ["Reward"] = {{["CurrencyName"] = "Coins", ["Icon"] = "rbxassetid://15038384377", ["Amount"] = 75}},
+            ["Icon"] = "rbxassetid://14916630678",
+            ["MaximumElo"] = 2999,
+            ["TextColor"] = Color3.fromRGB(200, 132, 93)
+        },
+        ["Rookie"] = {
+            ["Name"] = "Rookie",
+            ["MinimumElo"] = 0,
+            ["Icon"] = "rbxassetid://14916629523",
+            ["MaximumElo"] = 1999,
+            ["TextColor"] = Color3.fromRGB(91, 174, 102)
+        }
+    }
+}
