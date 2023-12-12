8354c10dcfc6bf61c26d289dcf8dbb973671f413
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-13 00:34:20 +1100

    Create Packs.lua

---
+++ GameData/Blade Ball [13772394625]/Packs.lua
@@ -0,0 +1,111 @@
+data = {
+    ["UltimateBlade"] = {
+        ["PlayTime"] = 240,
+        ["SubPacks"] = {
+            ["Skill"] = {
+                ["PurchaseLimit"] = 3,
+                ["Rewards"] = {
+                    {
+                        ["Type"] = "CrateKey",
+                        ["Name"] = "PremiumExplosion",
+                        ["Amount"] = 1,
+                        ["Icon"] = "rbxassetid://14798401525",
+                        ["DisplayName"] = "OP Explosion Crate"
+                    },
+                    {
+                        ["DisplayName"] = "30K",
+                        ["Type"] = "Coin",
+                        ["Icon"] = "rbxassetid://14849045938",
+                        ["Amount"] = 30000
+                    },
+                    {
+                        ["Alternative"] = {
+                            ["Type"] = "CrateKey",
+                            ["Name"] = "Sword",
+                            ["Amount"] = 10,
+                            ["Icon"] = "rbxassetid://14798399577",
+                            ["DisplayName"] = "Sword Crate"
+                        },
+                        ["Type"] = "Sword",
+                        ["Name"] = "Molten Greatblade",
+                        ["Icon"] = "rbxassetid://14841466149"
+                    },
+                    {
+                        ["Alternative"] = {
+                            ["Type"] = "CrateKey",
+                            ["Name"] = "Explosion",
+                            ["Amount"] = 10,
+                            ["Icon"] = "rbxassetid://14798401525",
+                            ["DisplayName"] = "Explosion Crate"
+                        },
+                        ["Type"] = "Explosion",
+                        ["Name"] = "Arctic Blast",
+                        ["Icon"] = "rbxassetid://14852855399"
+                    },
+                    {
+                        ["Icon"] = "rbxassetid://14847396112",
+                        ["Type"] = "Skill",
+                        ["Name"] = "Waypoint",
+                        ["DisplayName"] = "Waypoint Ability"
+                    }
+                },
+                ["ProductId"] = 1649003949
+            },
+            ["Basic"] = {
+                ["PurchaseLimit"] = 3,
+                ["Rewards"] = {
+                    {
+                        ["Type"] = "CrateKey",
+                        ["Name"] = "PremiumExplosion",
+                        ["Amount"] = 1,
+                        ["Icon"] = "rbxassetid://14798401525",
+                        ["DisplayName"] = "OP Explosion Crate"
+                    },
+                    {
+                        ["DisplayName"] = "14.5K",
+                        ["Type"] = "Coin",
+                        ["Icon"] = "rbxassetid://14849044347",
+                        ["Amount"] = 14500
+                    },
+                    {
+                        ["Type"] = "CrateKey",
+                        ["Name"] = "PremiumSword",
+                        ["Amount"] = 1,
+                        ["Icon"] = "rbxassetid://14798399577",
+                        ["DisplayName"] = "OP Sword Crate"
+                    },
+                    {
+                        ["Alternative"] = {
+                            ["Type"] = "CrateKey",
+                            ["Name"] = "Explosion",
+                            ["Amount"] = 3,
+                            ["Icon"] = "rbxassetid://14798401525",
+                            ["DisplayName"] = "Explosion Crate"
+                        },
+                        ["Type"] = "Explosion",
+                        ["Name"] = "Runic Blast",
+                        ["Icon"] = "rbxassetid://14852855175"
+                    },
+                    {
+                        ["Alternative"] = {
+                            ["Type"] = "CrateKey",
+                            ["Name"] = "Sword",
+                            ["Amount"] = 3,
+                            ["Icon"] = "rbxassetid://14798399577",
+                            ["DisplayName"] = "Sword Crate"
+                        },
+                        ["Type"] = "Sword",
+                        ["Name"] = "Essence Cleaver",
+                        ["Icon"] = "rbxassetid://14841466009"
+                    }
+                },
+                ["ProductId"] = 1649003823
+            }
+        },
+        ["ExpireDate"] = "1696078800000",
+        ["Bonus"] = {
+            {["Type"] = "Skill", ["Name"] = "Infinity", ["Icon"] = "rbxassetid://14847396284"},
+            {["Type"] = "Sword", ["Name"] = "Shattered Sword", ["Icon"] = "rbxassetid://14841466262"}
+        }
+    }
+}
