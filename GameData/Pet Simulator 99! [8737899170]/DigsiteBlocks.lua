c622005e5180e4c23770791a2ce2e2d67c402806
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-12 19:45:34 +1100

    Create DigsiteBlocks.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/DigsiteBlocks.lua
@@ -0,0 +1,260 @@
+data = {
+    ["Stone"] = {
+        ["_script"] = "DigsiteBlock | Stone",
+        ["Health"] = 3,
+        ["_id"] = "Stone",
+        ["Part"] = "Part",
+        ["Loot"] = {
+            {
+                ["_caches"] = {},
+                ["entries"] = {
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 100},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 5
+                    },
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 60},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 15
+                    },
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 40},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 30
+                    },
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 20},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 50
+                    }
+                }
+            }
+        },
+        ["DisplayName"] = "Stone"
+    },
+    ["Dirt"] = {
+        ["_script"] = "DigsiteBlock | Dirt",
+        ["Health"] = 1,
+        ["_id"] = "Dirt",
+        ["Part"] = "Part",
+        ["Loot"] = {
+            {
+                ["_caches"] = {},
+                ["entries"] = {
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 50},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 5
+                    },
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 30},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 15
+                    },
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 20},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 30
+                    },
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 10},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 50
+                    }
+                }
+            }
+        },
+        ["DisplayName"] = "Dirt"
+    },
+    ["Deep Stone"] = {
+        ["_script"] = "DigsiteBlock | Deep Stone",
+        ["Health"] = 3,
+        ["_id"] = "Deep Stone",
+        ["Part"] = "Part",
+        ["Loot"] = {
+            {
+                ["_caches"] = {},
+                ["entries"] = {
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 900},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 5
+                    },
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 540},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 15
+                    },
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 360},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 30
+                    },
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 180},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 50
+                    }
+                }
+            }
+        },
+        ["DisplayName"] = "Stone"
+    },
+    ["Earth Core"] = {
+        ["_script"] = "DigsiteBlock | Earth Core",
+        ["Health"] = 3,
+        ["_id"] = "Earth Core",
+        ["Part"] = "Part",
+        ["Loot"] = {
+            {
+                ["_caches"] = {},
+                ["entries"] = {
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 2700},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 5
+                    },
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 1620},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 15
+                    },
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 1080},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 30
+                    },
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 540},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 50
+                    }
+                }
+            }
+        },
+        ["DisplayName"] = "Stone"
+    },
+    ["Bedrock"] = {
+        ["_script"] = "DigsiteBlock | Bedrock",
+        ["Health"] = inf,
+        ["_id"] = "Bedrock",
+        ["Part"] = "Part",
+        ["DigsiteCurrency"] = 0,
+        ["DisplayName"] = "Bedrock"
+    },
+    ["Hard Stone"] = {
+        ["_script"] = "DigsiteBlock | Hard Stone",
+        ["Health"] = 3,
+        ["_id"] = "Hard Stone",
+        ["Part"] = "Part",
+        ["Loot"] = {
+            {
+                ["_caches"] = {},
+                ["entries"] = {
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 300},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 5
+                    },
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 180},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 15
+                    },
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 120},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 30
+                    },
+                    {
+                        ["Value"] = {
+                            ["_data"] = {["id"] = "Digsite", ["_am"] = 60},
+                            ["_stackKey"] = [[{"id":"Digsite"}]],
+                            ["_exactStackKey"] = [[{"id":"Digsite"}]],
+                            ["_maxAmount"] = 999999999
+                        },
+                        ["Weight"] = 50
+                    }
+                }
+            }
+        },
+        ["DisplayName"] = "Stone"
+    }
+}
