a7671729b33a23406847a16076cf69ec400ca8be
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-12 19:41:43 +1100

    Create RandomEvents.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/RandomEvents.lua
@@ -0,0 +1,32 @@
+data = {
+    ["Comet"] = {
+        ["BreakingRequirement"] = 5,
+        ["Color"] = Color3.fromRGB(44, 255, 255),
+        ["Duration"] = 600,
+        ["AllowInZones"] = true,
+        ["_id"] = "Comet",
+        ["_script"] = "RandomEvent | Comet",
+        ["AllowInInstances"] = false,
+        ["Name"] = "Comet",
+        ["PlaytimeRequirement"] = 5,
+        ["Chance"] = 50,
+        ["AreaWhitelist"] = {["Main_Magma"] = true, ["Main"] = true, ["Main_Ice"] = true},
+        ["AllowMultiple"] = true,
+        ["Icon"] = "rbxassetid://15000811043"
+    },
+    ["CoinJar"] = {
+        ["BreakingRequirement"] = 5,
+        ["Color"] = Color3.fromRGB(255, 220, 19),
+        ["Duration"] = 600,
+        ["AllowInZones"] = true,
+        ["_id"] = "CoinJar",
+        ["_script"] = "RandomEvent | CoinJar",
+        ["AllowInInstances"] = false,
+        ["Name"] = "Coin Jar",
+        ["PlaytimeRequirement"] = 5,
+        ["Chance"] = 50,
+        ["AreaWhitelist"] = {["Main_Magma"] = true, ["Main"] = true, ["Main_Ice"] = true},
+        ["AllowMultiple"] = true,
+        ["Icon"] = "rbxassetid://15000811448"
+    }
+}
