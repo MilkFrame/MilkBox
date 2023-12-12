2d5f62b6b121482c54b6411604e04064a9901bf9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-12 19:26:58 +1100

    Create Gamepasses.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/Gamepasses.lua
@@ -0,0 +1,82 @@
+data = {
+    ["Lucky"] = {
+        ["_script"] = "Gamepass | Lucky",
+        ["ProductId"] = 205379487,
+        ["_id"] = "Lucky",
+        ["Desc"] = "Increases your egg luck by 200%!",
+        ["Icon"] = "rbxassetid://15210431837",
+        ["DisplayName"] = "Lucky"
+    },
+    ["Ultra Lucky"] = {
+        ["_script"] = "Gamepass | Ultra Lucky",
+        ["ProductId"] = 257803774,
+        ["_id"] = "Ultra Lucky",
+        ["Desc"] = "Increases your egg luck by 500%!",
+        ["Icon"] = "rbxassetid://15210432026",
+        ["DisplayName"] = "Ultra Lucky"
+    },
+    ["Magic Eggs"] = {
+        ["_script"] = "Gamepass | Magic Eggs",
+        ["ProductId"] = 258567677,
+        ["_id"] = "Magic Eggs",
+        ["Desc"] = "Eggs have a chance to hatch golden or even rainbow pets! ðŸ‘‘ðŸŒˆ",
+        ["Icon"] = "rbxassetid://15210432223",
+        ["DisplayName"] = "Magic Eggs"
+    },
+    ["Extra Daycare Slots"] = {
+        ["_script"] = "Gamepass | Extra Daycare Slots",
+        ["ProductId"] = 651611000,
+        ["_id"] = "Extra Daycare Slots",
+        ["Desc"] = "Increases your Daycare slots by 10!",
+        ["Icon"] = "rbxassetid://7599295493",
+        ["DisplayName"] = "Extra Daycare Slots"
+    },
+    ["Huge Hunter"] = {
+        ["_script"] = "Gamepass | Huge Hunter",
+        ["ProductId"] = 264808140,
+        ["_id"] = "Huge Hunter",
+        ["Desc"] = "Increases the odds of hatching active huge pets by 2500%!",
+        ["Icon"] = "rbxassetid://15210432366",
+        ["DisplayName"] = "Huge Hunter"
+    },
+    ["15 Pets Equipped"] = {
+        ["_script"] = "Gamepass | 15 Pets Equipped",
+        ["ProductId"] = 259437976,
+        ["_id"] = "15 Pets Equipped",
+        ["Desc"] = "Increase your equipped pets by 15!",
+        ["Icon"] = "rbxassetid://7599295493",
+        ["DisplayName"] = "15 Pets Equipped"
+    },
+    ["15 Extra Eggs"] = {
+        ["_script"] = "Gamepass | 15 Extra Eggs",
+        ["ProductId"] = 655859720,
+        ["_id"] = "15 Extra Eggs",
+        ["Desc"] = "Increase your maximum hatch by 15!",
+        ["Icon"] = "rbxassetid://7599295493",
+        ["DisplayName"] = "15 Extra Eggs"
+    },
+    ["Auto Tapper"] = {
+        ["_script"] = "Gamepass | Auto Tapper",
+        ["ProductId"] = 265324265,
+        ["_id"] = "Auto Tapper",
+        ["Desc"] = "Automatically tap any breakable you hover!",
+        ["Icon"] = "rbxassetid://7599295493",
+        ["DisplayName"] = "Auto Tapper"
+    },
+    ["Auto Farm"] = {
+        ["_script"] = "Gamepass | Auto Farm",
+        ["ProductId"] = 265320491,
+        ["_id"] = "Auto Farm",
+        ["Desc"] = "Lock your pets to an area even when you're not around!",
+        ["Icon"] = "rbxassetid://7599295493",
+        ["DisplayName"] = "Display Name"
+    },
+    ["VIP"] = {
+        ["_script"] = "Gamepass | VIP",
+        ["ProductId"] = 257811346,
+        ["_id"] = "VIP",
+        ["Desc"] = "Range of benefits including a VIP-only area, daily rewards, chat tag, and more!",
+        ["Icon"] = "rbxassetid://15210431644",
+        ["DisplayName"] = "VIP"
+    }
+}
