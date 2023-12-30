ed76fd4aeb13e9d7877dd69d1c00fd26719249ce
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-30 15:57:12 +1100

    Create DailyLogin.lua

---
+++ GameData/Doodle World [5116869569]/DailyLogin.lua
@@ -0,0 +1,9 @@
+data = {
+    {["Amount"] = 5000, ["Type"] = "Cash"},
+    {["Amount"] = 150, ["Type"] = "Gems"},
+    {["Amount"] = 7500, ["Type"] = "Cash"},
+    {["Amount"] = 200, ["Type"] = "Gems"},
+    {["Amount"] = 10000, ["Type"] = "Cash"},
+    {["Type"] = "Item", ["Item"] = "Level-Up Cube", ["Amount"] = 1},
+    {["Type"] = "Item", ["Item"] = "Roulette Ticket", ["Amount"] = 1}
+}
