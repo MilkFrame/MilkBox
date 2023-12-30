e84cdfdca31a9230f94d01eff3b1fe9189757ed9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-30 15:59:29 +1100

    Create GemShopProducts.lua

---
+++ GameData/Doodle World [5116869569]/GemShopProducts.lua
@@ -0,0 +1,93 @@
+data = {
+    {
+        ["HeldItem"] = "Bionotic Rune",
+        ["Type"] = "Gems",
+        ["Description"] = "Purchase this to get a Bionotic with its NEW runestone!",
+        ["Showcase"] = "Bionotic-A",
+        ["Line"] = "Bionotic",
+        ["Item"] = "Doodle",
+        ["Skin"] = 0,
+        ["Cost"] = 5000,
+        ["Name"] = "Bionotic-A",
+        ["New"] = true
+    },
+    {
+        ["HeldItem"] = "Lumiline Rune",
+        ["Type"] = "Gems",
+        ["Description"] = "Purchase this to get a Lumiline with its NEW runestone!",
+        ["Showcase"] = "Lumiline-A",
+        ["Line"] = "Lumiline",
+        ["Item"] = "Doodle",
+        ["Skin"] = 0,
+        ["Cost"] = 5000,
+        ["Name"] = "Lumiline-A",
+        ["New"] = true
+    },
+    {
+        ["Type"] = "Gems",
+        ["Description"] = "Purchase this to get a Electrician Riptorvent. This skin is only obtainable with gems.",
+        ["Showcase"] = "Riptorvent",
+        ["Line"] = "Riptorvent",
+        ["Item"] = "Doodle",
+        ["Skin"] = 7,
+        ["Premium"] = true,
+        ["Cost"] = 4600,
+        ["Name"] = "Electrician Riptorvent"
+    },
+    {
+        ["Type"] = "Gems",
+        ["Description"] = "Purchase this to get a Digital Metalytra!",
+        ["Showcase"] = "Metalytra",
+        ["Line"] = "Metalytra",
+        ["Item"] = "Doodle",
+        ["Skin"] = 18,
+        ["Cost"] = 3600,
+        ["Name"] = "Digital Metalytra",
+        ["New"] = true
+    },
+    {
+        ["Type"] = "Gems",
+        ["Description"] = "Purchase this to get a Donut Banautilus!",
+        ["Showcase"] = "Banautilus",
+        ["Line"] = "Banautilus",
+        ["Item"] = "Doodle",
+        ["Skin"] = 3,
+        ["Cost"] = 3600,
+        ["Name"] = "Donut Banautilus",
+        ["New"] = true
+    },
+    {
+        ["Type"] = "Gems",
+        ["Description"] = "Purchase this to get a Strawberry Shortcake Jellupy!",
+        ["Showcase"] = "Jellupy",
+        ["Line"] = "Jellupy",
+        ["Item"] = "Doodle",
+        ["Skin"] = 26,
+        ["Cost"] = 3600,
+        ["Name"] = "Strawberry Shortcake Jellupy",
+        ["New"] = true
+    },
+    {
+        ["Type"] = "Gems",
+        ["Description"] = "Purchase this to get a Cyberpunk Tasmarauder!",
+        ["Showcase"] = "Hollihare",
+        ["Line"] = "Hollihare",
+        ["Item"] = "Doodle",
+        ["Skin"] = 15,
+        ["Cost"] = 3600,
+        ["Name"] = "Cozy Hollihare",
+        ["New"] = true
+    },
+    {
+        ["Type"] = "Gems",
+        ["Description"] = "Purchase this to get a Nerd Finwick!",
+        ["Showcase"] = "Finwick",
+        ["Line"] = "Finwick",
+        ["Item"] = "Doodle",
+        ["Skin"] = 3,
+        ["Cost"] = 3600,
+        ["Name"] = "Nerd Finwick",
+        ["New"] = true
+    },
+    ["CurrentWeek"] = 53
+}
