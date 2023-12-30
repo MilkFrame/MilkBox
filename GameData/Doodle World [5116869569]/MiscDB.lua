23c2e9ad4aadb6c68a4e750e3240a869e90e0977
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-30 16:03:01 +1100

    Create MiscDB.lua

---
+++ GameData/Doodle World [5116869569]/MiscDB.lua
@@ -0,0 +1,389 @@
+data = {
+    ["GetRank"] = "function: 0x000000004aea56d9",
+    ["Ranks"] = {
+        {
+            ["ColorString"] = "205, 127, 50",
+            ["Name"] = "Bronze",
+            ["Color"] = Color3.fromRGB(205, 127, 50),
+            ["Image"] = "http://www.roblox.com/asset/?id=14214782957",
+            ["Max"] = 1099
+        },
+        {
+            ["ColorString"] = "170,169,173",
+            ["Name"] = "Silver",
+            ["Color"] = Color3.fromRGB(170, 169, 173),
+            ["Image"] = "http://www.roblox.com/asset/?id=14214813726",
+            ["Max"] = 1249
+        },
+        {
+            ["ColorString"] = "255,215,0",
+            ["Name"] = "Gold",
+            ["Color"] = Color3.fromRGB(255, 215, 0),
+            ["Image"] = "http://www.roblox.com/asset/?id=14214869300",
+            ["Max"] = 1399
+        },
+        {
+            ["ColorString"] = "229, 228, 226",
+            ["Name"] = "Platinum",
+            ["Color"] = Color3.fromRGB(229, 228, 226),
+            ["Image"] = "http://www.roblox.com/asset/?id=14215553621",
+            ["Max"] = 1599
+        },
+        {
+            ["ColorString"] = "185,242,255",
+            ["Name"] = "Diamond",
+            ["Color"] = Color3.fromRGB(185, 242, 255),
+            ["Image"] = "http://www.roblox.com/asset/?id=14215670660",
+            ["Max"] = 1799
+        },
+        {
+            ["ColorString"] = "218,112,214",
+            ["Name"] = "Master",
+            ["Color"] = Color3.fromRGB(61, 21, 133),
+            ["Image"] = "http://www.roblox.com/asset/?id=14226750675",
+            ["Max"] = 1999
+        },
+        {
+            ["ColorString"] = "255, 170, 255",
+            ["Name"] = "Mythic",
+            ["Color"] = Color3.fromRGB(255, 170, 255),
+            ["Image"] = "http://www.roblox.com/asset/?id=14216870604",
+            ["Max"] = 1000000000
+        }
+    },
+    ["Rarities"] = {
+        ["Common"] = Color3.fromRGB(255, 255, 255),
+        ["Mythic"] = Color3.fromRGB(255, 85, 0),
+        ["Uncommon"] = Color3.fromRGB(0, 150, 0),
+        ["Rare"] = Color3.fromRGB(0, 85, 255),
+        ["Legendary"] = Color3.fromRGB(255, 85, 0),
+        ["Epic"] = Color3.fromRGB(170, 85, 255),
+        ["RouletteCommon"] = Color3.fromRGB(200, 200, 200),
+        ["DevColor"] = Color3.fromRGB(255, 170, 255),
+        ["Unique"] = Color3.fromRGB(255, 85, 127)
+    },
+    ["Tints"] = {
+        {["Name"] = "Red", ["Color"] = Color3.fromRGB(255, 0, 0)},
+        {["Name"] = "Green", ["Color"] = Color3.fromRGB(0, 255, 0)},
+        {["Name"] = "Orange", ["Color"] = Color3.fromRGB(255, 85, 0)},
+        {["Name"] = "Yellow", ["Color"] = Color3.fromRGB(255, 255, 0)},
+        {["Name"] = "Blue", ["Color"] = Color3.fromRGB(0, 85, 255)},
+        {["Name"] = "Cyan", ["Color"] = Color3.fromRGB(85, 255, 255)},
+        {["Name"] = "Teal", ["Color"] = Color3.fromRGB(0, 128, 128)},
+        {["Name"] = "Black", ["Color"] = Color3.fromRGB(36, 36, 36)},
+        {["Name"] = "Purple", ["Color"] = Color3.fromRGB(85, 0, 127)},
+        {["Name"] = "Dark Green", ["Color"] = Color3.fromRGB(0, 108, 0)},
+        {["Name"] = "Pink", ["Color"] = Color3.fromRGB(255, 85, 255)},
+        {["Name"] = "Brown", ["Color"] = Color3.fromRGB(139, 69, 19)},
+        {["Name"] = "Navy Blue", ["Color"] = Color3.fromRGB(0, 71, 114)},
+        {["Name"] = "Gold", ["Color"] = Color3.fromRGB(212, 175, 55)},
+        {
+            ["Color"] = Color3.fromRGB(76, 33, 0),
+            ["ColorSequence"] = {Color3.fromRGB(254, 94, 35), Color3.fromRGB(30, 30, 30), Color3.fromRGB(254, 94, 35)},
+            ["Name"] = "Halloween",
+            ["Animation"] = "DiagonalMovement"
+        },
+        {
+            ["Color"] = Color3.fromRGB(232, 209, 163),
+            ["ColorSequence"] = {
+                Color3.fromRGB(0, 255, 0),
+                Color3.fromRGB(0, 255, 0),
+                Color3.fromRGB(255, 0, 0),
+                Color3.fromRGB(255, 0, 0)
+            },
+            ["Name"] = "Christmas",
+            ["Animation"] = "Movement"
+        },
+        {["Name"] = "Lime", ["Color"] = Color3.fromRGB(175, 255, 0)},
+        {["Name"] = "Maroon", ["Color"] = Color3.fromRGB(128, 0, 43)},
+        {["Name"] = "Violet", ["Color"] = Color3.fromRGB(175, 0, 255)},
+        {["Name"] = "Indigo", ["Color"] = Color3.fromRGB(85, 0, 255)},
+        {["Name"] = "Rose", ["Color"] = Color3.fromRGB(255, 0, 85)},
+        {["Name"] = "Sky Blue", ["Color"] = Color3.fromRGB(0, 175, 255)},
+        {
+            ["Color"] = Color3.fromRGB(0, 254, 170),
+            ["ColorSequence"] = {Color3.fromRGB(0, 254, 170), Color3.fromRGB(0, 225, 254), Color3.fromRGB(251, 0, 254)},
+            ["Name"] = "Easter 2023",
+            ["Animation"] = "VerticalStatic"
+        },
+        {
+            ["Color"] = Color3.fromRGB(252, 222, 108),
+            ["ColorSequence"] = {
+                Color3.fromRGB(252, 222, 108),
+                Color3.fromRGB(252, 181, 108),
+                Color3.fromRGB(252, 71, 59)
+            },
+            ["Name"] = "Sunset",
+            ["Animation"] = "VerticalStatic"
+        },
+        {
+            ["Color"] = Color3.fromRGB(252, 222, 108),
+            ["ColorSequence"] = {
+                Color3.fromRGB(255, 0, 0),
+                Color3.fromRGB(255, 85, 0),
+                Color3.fromRGB(255, 255, 0),
+                Color3.fromRGB(0, 148, 196),
+                Color3.fromRGB(0, 0, 255),
+                Color3.fromRGB(170, 0, 255)
+            },
+            ["Name"] = "Pride",
+            ["Animation"] = "VerticalMovement"
+        },
+        {
+            ["Color"] = Color3.fromRGB(253, 76, 71),
+            ["ColorSequence"] = {
+                Color3.fromRGB(255, 0, 0),
+                Color3.fromRGB(253, 76, 71),
+                Color3.fromRGB(56, 95, 223),
+                Color3.fromRGB(0, 85, 255)
+            },
+            ["Name"] = "July",
+            ["Animation"] = "VerticalStatic"
+        },
+        {
+            ["Color"] = Color3.fromRGB(145, 53, 184),
+            ["ColorSequence"] = {
+                Color3.fromRGB(145, 53, 184),
+                Color3.fromRGB(217, 47, 109),
+                Color3.fromRGB(57, 147, 177),
+                Color3.fromRGB(89, 47, 255),
+                Color3.fromRGB(99, 44, 216)
+            },
+            ["Name"] = "Roulette",
+            ["Animation"] = "VerticalStatic"
+        },
+        {
+            ["Color"] = Color3.fromRGB(145, 53, 184),
+            ["ColorSequence"] = {
+                Color3.fromRGB(145, 53, 184),
+                Color3.fromRGB(217, 47, 109),
+                Color3.fromRGB(57, 147, 177),
+                Color3.fromRGB(89, 47, 255),
+                Color3.fromRGB(99, 44, 216)
+            },
+            ["Name"] = "Animated Roulette",
+            ["Animation"] = "VerticalMovement"
+        },
+        {["Name"] = "No Tint", ["Color"] = Color3.fromRGB(255, 255, 255)}
+    },
+    ["Animations"] = {
+        ["WakeUpCall"] = {["Id"] = "7199000883"},
+        ["Point"] = {["Id"] = "507770453"},
+        ["DefaultThrow"] = {["Id"] = "4638452918", ["Wait"] = 0.25},
+        ["BunnyIdle"] = {["Id"] = "13039412160"},
+        ["MaleLeaning"] = {["Id"] = "8628215977"},
+        ["Shrug"] = {["Id"] = "8661325787"},
+        ["DefaultIdle"] = {["Id"] = "507766388"},
+        ["Bow"] = {["Id"] = "7349183903"},
+        ["Laugh"] = {["Id"] = "507770818"},
+        ["Stomp"] = {["Id"] = "9215533108"},
+        ["Teleport"] = {["Id"] = "8817342624", ["Wait"] = 0.4},
+        ["Skipping"] = {["Id"] = "9913186564"},
+        ["Facepalm"] = {["Id"] = "9930628209"},
+        ["DeskSlam"] = {["Id"] = "8598576387"},
+        ["OffenseIdle"] = {["Id"] = "8628271295"},
+        ["GetHit"] = {["Id"] = "9241585505"},
+        ["HeadWallSmash"] = {["Id"] = "9930288676", ["Wait"] = 0.3},
+        ["Laughter"] = {["Id"] = "507770818"},
+        ["Dance3"] = {["Id"] = "507777268"},
+        ["ToughIdle"] = {["Id"] = "10019469261"},
+        ["DefaultWalk"] = {["Id"] = "913402848"},
+        ["Up_Toss"] = {["Id"] = "04642283256", ["Wait"] = 0.4},
+        ["KluIdle"] = {["Id"] = "616158929"},
+        ["JumpShock"] = {["Id"] = "9913157882"},
+        ["Orange"] = {["Id"] = "8958552633"},
+        ["Sitting"] = {["Id"] = "8625804079"},
+        ["BreathingIdle"] = {["Id"] = "8628295891"},
+        ["SadIdle"] = {["Id"] = "9903105236"},
+        ["RoundhouseKick"] = {["Id"] = "9241548874"},
+        ["Monkey"] = {["Id"] = "3333499508"},
+        ["BubblyIdle"] = {["Id"] = "910004836"},
+        ["Wiggle"] = {["Id"] = "8958514615"},
+        ["LDance"] = {["Id"] = "8958562672"},
+        ["Dab"] = {["Id"] = "8958542262"},
+        ["Nod"] = {["Id"] = "7349343242"},
+        ["Wave"] = {["Id"] = "507770239"},
+        ["Default"] = {["Id"] = "8958529742"},
+        ["Knocked"] = {["Id"] = "8758876802"},
+        ["VampireIdle"] = {["Id"] = "1083445855"},
+        ["LeaningWall2"] = {["Id"] = "9903046013"},
+        ["Dance2"] = {["Id"] = "507776043"},
+        ["Dance1"] = {["Id"] = "507771019"}
+    },
+    ["NewUGC"] = {14067368888, 14016096474, 14016080374},
+    ["StatChangeColors"] = {
+        ["mix"] = Color3.fromRGB(177, 176, 190),
+        ["eva"] = Color3.fromRGB(63, 191, 146),
+        ["atk"] = Color3.fromRGB(237, 187, 67),
+        ["def"] = Color3.fromRGB(157, 254, 136),
+        ["acc"] = Color3.fromRGB(157, 254, 136),
+        ["spe"] = Color3.fromRGB(63, 191, 146),
+        ["matk"] = Color3.fromRGB(255, 182, 44),
+        ["mdef"] = Color3.fromRGB(149, 222, 131)
+    },
+    ["UGC"] = {
+        14016080374,
+        14067368888,
+        14016096474,
+        12865404842,
+        12865423642,
+        12865436197,
+        12870311694,
+        13001127882,
+        13001158324,
+        12870471410,
+        12870352446,
+        12870335835,
+        12855396167,
+        12793565859,
+        12855344137,
+        12855336549,
+        12855356348,
+        12870345409,
+        13001038703,
+        12870332185,
+        12783804309,
+        12865400755,
+        12790210267,
+        12793383087,
+        12783781842,
+        12792655861,
+        12870306337
+    },
+    ["BattleBGs"] = {
+        ["Route1"] = "http://www.roblox.com/asset/?id=7291014922",
+        ["LakewoodLake"] = "http://www.roblox.com/asset/?id=8634687658",
+        ["GenericIndoors"] = "http://www.roblox.com/asset/?id=8631714430"
+    },
+    ["Capsules"] = {
+        ["Shadow"] = {
+            ["Color3"] = Color3.fromRGB(182, 50, 230),
+            ["Open"] = "http://www.roblox.com/asset/?id=13918356949",
+            ["Closed"] = "http://www.roblox.com/asset/?id=13918356087"
+        },
+        ["Dense"] = {
+            ["Color3"] = Color3.fromRGB(51, 49, 49),
+            ["Open"] = "http://www.roblox.com/asset/?id=10006320670",
+            ["Closed"] = "http://www.roblox.com/asset/?id=10006319585"
+        },
+        ["Viper"] = {
+            ["Color3"] = Color3.fromRGB(93, 195, 37),
+            ["Open"] = "http://www.roblox.com/asset/?id=13918353209",
+            ["Closed"] = "http://www.roblox.com/asset/?id=13918352074"
+        },
+        ["Polkadot"] = {
+            ["Color3"] = Color3.fromRGB(224, 82, 86),
+            ["Open"] = "http://www.roblox.com/asset/?id=10006345996",
+            ["Closed"] = "http://www.roblox.com/asset/?id=10006347567"
+        },
+        ["Chain"] = {
+            ["Color3"] = Color3.fromRGB(25, 25, 25),
+            ["Open"] = "http://www.roblox.com/asset/?id=12355938757",
+            ["Closed"] = "http://www.roblox.com/asset/?id=12355938757"
+        },
+        ["Basic"] = {
+            ["Color3"] = Color3.fromRGB(85, 255, 0),
+            ["Open"] = "http://www.roblox.com/asset/?id=5723542507",
+            ["Closed"] = "http://www.roblox.com/asset/?id=5723541748"
+        },
+        ["Santa"] = {
+            ["Color3"] = Color3.fromRGB(255, 66, 66),
+            ["Open"] = "http://www.roblox.com/asset/?id=11803618675",
+            ["Closed"] = "http://www.roblox.com/asset/?id=11803618177"
+        },
+        ["Harvest"] = {
+            ["Color3"] = Color3.fromRGB(166, 104, 72),
+            ["Open"] = "http://www.roblox.com/asset/?id=13918390175",
+            ["Closed"] = "http://www.roblox.com/asset/?id=13918395023"
+        },
+        ["Runic"] = {
+            ["Color3"] = Color3.fromRGB(255, 255, 255),
+            ["Open"] = "http://www.roblox.com/asset/?id=13134974981",
+            ["Closed"] = "http://www.roblox.com/asset/?id=13134973630"
+        },
+        ["Striped"] = {
+            ["Color3"] = Color3.fromRGB(84, 118, 215),
+            ["Open"] = "http://www.roblox.com/asset/?id=6493789323",
+            ["Closed"] = "http://www.roblox.com/asset/?id=6493789086"
+        },
+        ["Winged"] = {
+            ["Color3"] = Color3.fromRGB(33, 17, 54),
+            ["Open"] = "http://www.roblox.com/asset/?id=11323889485",
+            ["Closed"] = "http://www.roblox.com/asset/?id=11323888899"
+        },
+        ["Blizzard"] = {
+            ["Color3"] = Color3.fromRGB(255, 255, 255),
+            ["Open"] = "http://www.roblox.com/asset/?id=13918407901",
+            ["Closed"] = "http://www.roblox.com/asset/?id=13918406852"
+        },
+        ["Heart"] = {
+            ["Color3"] = Color3.fromRGB(255, 111, 113),
+            ["Open"] = "http://www.roblox.com/asset/?id=10006573313",
+            ["Closed"] = "http://www.roblox.com/asset/?id=10006572771"
+        },
+        ["Cauldron"] = {
+            ["Color3"] = Color3.fromRGB(174, 212, 186),
+            ["Open"] = "http://www.roblox.com/asset/?id=11323982051",
+            ["Closed"] = "http://www.roblox.com/asset/?id=11323981188"
+        },
+        ["Medical"] = {
+            ["Color3"] = Color3.fromRGB(137, 203, 255),
+            ["Open"] = "http://www.roblox.com/asset/?id=12369008112",
+            ["Closed"] = "http://www.roblox.com/asset/?id=12369006386"
+        },
+        ["Witch"] = {
+            ["Color3"] = Color3.fromRGB(45, 92, 86),
+            ["Open"] = "http://www.roblox.com/asset/?id=11324048764",
+            ["Closed"] = "http://www.roblox.com/asset/?id=11324048459"
+        },
+        ["Gold"] = {
+            ["Color3"] = Color3.fromRGB(255, 224, 79),
+            ["Open"] = "http://www.roblox.com/asset/?id=9195089437",
+            ["Closed"] = "http://www.roblox.com/asset/?id=9195090072"
+        }
+    },
+    ["TintList"] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 22},
+    ["MaxTints"] = 20,
+    ["Premium"] = {500, 700},
+    ["PartyGuis"] = {
+        ["Status"] = "http://www.roblox.com/asset/?id=5872549668",
+        ["Empty"] = "http://www.roblox.com/asset/?id=5872550604",
+        ["Healthy"] = "http://www.roblox.com/asset/?id=5872548123",
+        ["Fainted"] = "http://www.roblox.com/asset/?id=5872549094"
+    },
+    ["GenderIcons"] = {
+        ["U"] = {["Color"] = Color3.fromRGB(0, 0, 0), ["Image"] = ""},
+        ["M"] = {["Color"] = Color3.fromRGB(0, 170, 255), ["Image"] = "http://www.roblox.com/asset/?id=5686176593"},
+        ["F"] = {["Color"] = Color3.fromRGB(255, 170, 255), ["Image"] = "http://www.roblox.com/asset/?id=5686175334"}
+    },
+    ["StatusInfo"] = {
+        ["Rage"] = {["Icon"] = "http://www.roblox.com/asset/?id=5734893117", ["Color"] = Color3.fromRGB(255, 0, 0)},
+        ["Cursed"] = {["Icon"] = "http://www.roblox.com/asset/?id=5827308527", ["Color"] = Color3.fromRGB(111, 22, 163)},
+        ["Poison"] = {
+            ["Icon"] = "http://www.roblox.com/asset/?id=5734231647",
+            ["Color"] = Color3.fromRGB(194, 136, 228)
+        },
+        ["Marked"] = {
+            ["Icon"] = "http://www.roblox.com/asset/?id=5756539629",
+            ["Color"] = Color3.fromRGB(255, 255, 255)
+        },
+        ["Diseased"] = {
+            ["Icon"] = "http://www.roblox.com/asset/?id=5827307624",
+            ["Color"] = Color3.fromRGB(201, 206, 60)
+        },
+        ["Vulnerable"] = {
+            ["Icon"] = "http://www.roblox.com/asset/?id=5756615804",
+            ["Color"] = Color3.fromRGB(177, 177, 179)
+        },
+        ["Paralysis"] = {
+            ["Icon"] = "http://www.roblox.com/asset/?id=5719886562",
+            ["Color"] = Color3.fromRGB(255, 223, 0)
+        },
+        ["Sleep"] = {["Icon"] = "http://www.roblox.com/asset/?id=5784445369", ["Color"] = Color3.fromRGB(227, 212, 97)},
+        ["Burn"] = {["Icon"] = "http://www.roblox.com/asset/?id=5756553617", ["Color"] = Color3.fromRGB(207, 90, 24)},
+        ["Frozen"] = {
+            ["Icon"] = "http://www.roblox.com/asset/?id=5756785249",
+            ["Color"] = Color3.fromRGB(113, 205, 227)
+        }
+    }
+}
