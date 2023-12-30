2ee21665c63aa0647438433ea857eb54438ae2ff
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-30 15:33:53 +1100

    Create Locations.lua

---
+++ GameData/Doodle World [5116869569]/Locations.lua
@@ -0,0 +1,75 @@
+data = {
+    {
+        ["Image"] = "rbxassetid://10717784007",
+        ["Name"] = "Sketchvale",
+        ["Chunk"] = "001_Chunk",
+        ["Subway"] = "SketchvaleSubway",
+        ["Requirement"] = "93",
+        ["Part"] = "SubwayExit"
+    },
+    {
+        ["Image"] = "rbxassetid://10732644952",
+        ["Name"] = "Lakewood Town",
+        ["Chunk"] = "007_Lakewood",
+        ["Subway"] = "LakewoodSubway",
+        ["Requirement"] = "93",
+        ["Part"] = "SubwayExit"
+    },
+    {
+        ["Image"] = "rbxassetid://10717784728",
+        ["Name"] = "The Crossroads",
+        ["Chunk"] = "017_Crossroads",
+        ["Subway"] = "CrossroadsSubway",
+        ["Requirement"] = "93",
+        ["Part"] = "SubwayExit"
+    },
+    {
+        ["Image"] = "rbxassetid://10717783073",
+        ["Name"] = "von Sweetsville",
+        ["Chunk"] = "025_Sweetsville",
+        ["Subway"] = "SweetsvilleSubway",
+        ["Requirement"] = "93",
+        ["Part"] = "SubwayExit"
+    },
+    {
+        ["Image"] = "rbxassetid://13040389786",
+        ["Name"] = "Chef's Valley",
+        ["Chunk"] = "030_ChefsValley",
+        ["Subway"] = "ChefsValleySubway",
+        ["Requirement"] = "93",
+        ["Part"] = "SubwayExit"
+    },
+    {["Chunk"] = "Teleport", ["Image"] = "rbxassetid://10717783551", ["Name"] = "Social Park", ["Requirement"] = "93"},
+    {
+        ["Image"] = "rbxassetid://11602879091",
+        ["Name"] = "DoodleCo Academy",
+        ["Chunk"] = "033_Academy",
+        ["Subway"] = "AcademySubway",
+        ["Requirement"] = "144",
+        ["Part"] = "SubwayExit"
+    },
+    {
+        ["Image"] = "rbxassetid://13040385486",
+        ["Name"] = "DoodleCo Printing Facility",
+        ["Chunk"] = "041_Printer",
+        ["Subway"] = "PrinterSubway",
+        ["Requirement"] = "181",
+        ["Part"] = "SubwayExit"
+    },
+    {
+        ["Image"] = "rbxassetid://13039421620",
+        ["Name"] = "Runic Island",
+        ["Chunk"] = "044_RunicIsland",
+        ["Subway"] = "RunicIslandSubway",
+        ["Requirement"] = "182",
+        ["Part"] = "SubwayExit"
+    },
+    {
+        ["Image"] = "rbxassetid://13679852094",
+        ["Name"] = "The Oasis",
+        ["Chunk"] = "045_Oasis",
+        ["Subway"] = "OasisSubway",
+        ["Requirement"] = "204",
+        ["Part"] = "SubwayExit"
+    }
+}
