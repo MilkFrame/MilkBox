c2f07cb8a70dbacf1a2bd08c45c0646c9c44e61a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-30 15:59:03 +1100

    Create GameCards.lua

---
+++ GameData/Doodle World [5116869569]/GameCards.lua
@@ -0,0 +1,14 @@
+data = {
+    ["1"] = {["Stats"] = {3, 2, 4, 3}, ["Name"] = "Pupskey", ["Rarity"] = "Uncommon"},
+    ["3"] = {["Stats"] = {4, 4, 2, 2}, ["Name"] = "Tabbolt", ["Rarity"] = "Uncommon"},
+    ["2"] = {["Stats"] = {5, 2, 3, 2}, ["Name"] = "Vipember", ["Rarity"] = "Uncommon"},
+    ["5"] = {["Stats"] = {2, 3, 2, 2}, ["Name"] = "Borbo", ["Rarity"] = "Common"},
+    ["4"] = {["Stats"] = {1, 3, 4, 4}, ["Name"] = "Skrappey", ["Rarity"] = "Uncommon"},
+    ["7"] = {["Stats"] = {2, 2, 3, 2}, ["Name"] = "Statikeet", ["Rarity"] = "Common"},
+    ["6"] = {["Stats"] = {3, 1, 2, 3}, ["Name"] = "Rosebug", ["Rarity"] = "Common"},
+    ["9"] = {["Stats"] = {4, 1, 1, 3}, ["Name"] = "Bunsweet", ["Rarity"] = "Common"},
+    ["8"] = {["Stats"] = {1, 1, 2, 5}, ["Name"] = "Nibblen", ["Rarity"] = "Common"},
+    ["12"] = {["Stats"] = {4, 4, 4, 4}, ["Name"] = "Louis", ["Rarity"] = "Rare"},
+    ["11"] = {["Stats"] = {4, 5, 4, 3}, ["Name"] = "Partybug", ["Rarity"] = "Rare"},
+    ["10"] = {["Stats"] = {1, 4, 3, 1}, ["Name"] = "Plipo", ["Rarity"] = "Common"}
+}
