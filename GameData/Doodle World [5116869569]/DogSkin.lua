ba99d26c49a868bb0d6e59ab7a70ccaf96256a5d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-30 15:54:49 +1100

    Create DogSkin.lua

---
+++ GameData/Doodle World [5116869569]/DogSkin.lua
@@ -0,0 +1,191 @@
+data = {
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14016061545",
+        ["Name"] = "beachgoer",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14016062738",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14016062283",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14016052488"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14592433355",
+        ["Name"] = "cat",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14592437877",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14592443041",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14592431391"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14592476471",
+        ["Name"] = "inversion",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14592423389",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14592428137",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14592473484"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14593117169",
+        ["Name"] = "idol",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14593121739",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14593119564",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14593091761"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14597651207",
+        ["Name"] = "mechanicalhorror",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14597650856",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14597651047",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14597651389"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14597681346",
+        ["Name"] = "tricky",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14597680189",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14597678981",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14597677743"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14603506520",
+        ["Name"] = "qummoe",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14603489831",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14603502676",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14603481031"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14604299475",
+        ["Name"] = "soul gatherer",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14604303001",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14604301537",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14604298071"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14605574207",
+        ["Name"] = "corn",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14605567861",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14605563213",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14605572681"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14605584942",
+        ["Name"] = "fruitjvice",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14605594567",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14605592150",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14605583071"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14606361546",
+        ["Name"] = "eldritch",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14606366631",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14606363442",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14606359820"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14606413572",
+        ["Name"] = "wish_car",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14606416757",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14606415280",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14606412440"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14607473815",
+        ["Name"] = "bug",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14607479179",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14607475717",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14607471299"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14617885608",
+        ["Name"] = "bartholomew",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14617893998",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14617887089",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14617876985"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14635684761",
+        ["Name"] = "maid",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14635688983",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14635686758",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14635682122"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14636275362",
+        ["Name"] = "ceresora",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14636279582",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14636277345",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14636273581"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14636286785",
+        ["Name"] = "stuck in toilet",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14636290793",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14636288969",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14636285225"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14638921177",
+        ["Name"] = "hot dawg",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14638923863",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14638922322",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14638919958"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14638932241",
+        ["Name"] = "exo-prime",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14638933730",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14638931169",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14638929739"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14640330931",
+        ["Name"] = "chef",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14640333667",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14640331981",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14640329930"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14640657379",
+        ["Name"] = "pretty",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14640654765",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14640656160",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14640653688"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14652893570",
+        ["Name"] = "fisherman",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14652899573",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14652898192",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14652917300"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14720648659",
+        ["Name"] = "miaumage",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14720650059",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14720650059",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14720648659"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14720637952",
+        ["Name"] = "the third dimension",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14720640769",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14720639559",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14720636561"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14720642771",
+        ["Name"] = "louie",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14720647128",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14720645339",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14720643905"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14754194017",
+        ["Name"] = "sword logic",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14754195303",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14754194700",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14754193383"
+    },
+    {
+        ["SFSprite"] = "http://www.roblox.com/asset/?id=14992836908",
+        ["Name"] = "mindspun invocation",
+        ["SBSprite"] = "http://www.roblox.com/asset/?id=14992850773",
+        ["BackSprite"] = "http://www.roblox.com/asset/?id=14992849118",
+        ["FrontSprite"] = "http://www.roblox.com/asset/?id=14992832712"
+    }
+}
