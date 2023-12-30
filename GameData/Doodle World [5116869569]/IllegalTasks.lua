4ea7faebeaec1887092a12eec0dccf96b16b38d6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-30 16:02:12 +1100

    Create IllegalTasks.lua

---
+++ GameData/Doodle World [5116869569]/IllegalTasks.lua
@@ -0,0 +1,10 @@
+data = {
+    ["Kibara"] = true,
+    ["Marshore"] = true,
+    ["Chronos"] = true,
+    ["Wydling"] = true,
+    ["Kidere"] = true,
+    ["Shyce"] = true,
+    ["Shelldo"] = true,
+    ["Caramellow"] = true
+}
