6029a635e897abf6f57071e3f72b095f4b68587c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-30 16:04:57 +1100

    Create Ranked.lua

---
+++ GameData/Doodle World [5116869569]/Ranked.lua
@@ -0,0 +1,13 @@
+data = {
+    ["Season"] = {
+        ["Desc"] = "For the first official season, season 1 will have minimal restrictions.",
+        ["Usage"] = "S1R",
+        ["SpeciesClause"] = true,
+        ["Title"] = "Ranked: Season 1",
+        ["Ban"] = {"Chronos"},
+        ["Ruleset"] = "Casual",
+        ["Format"] = "Singles",
+        ["Level"] = "Level 50",
+        ["ItemClause"] = true
+    }
+}
