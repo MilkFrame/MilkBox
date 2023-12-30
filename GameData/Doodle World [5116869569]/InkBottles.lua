ccc8e7557b41d68d79024b00e2de63907fc30810
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-30 16:02:27 +1100

    Create InkBottles.lua

---
+++ GameData/Doodle World [5116869569]/InkBottles.lua
@@ -0,0 +1,122 @@
+data = {
+    ["Gold Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(212, 175, 55),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237528934",
+        ["TintId"] = 14,
+        ["Tint"] = "Gold"
+    },
+    ["Blue Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(0, 85, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237523543",
+        ["TintId"] = 5,
+        ["Tint"] = "Blue"
+    },
+    ["Pink Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(255, 85, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237535047",
+        ["TintId"] = 11,
+        ["Tint"] = "Pink"
+    },
+    ["Maroon Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(128, 0, 43),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237514638",
+        ["TintId"] = 18,
+        ["Tint"] = "Maroon"
+    },
+    ["Red Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(255, 0, 0),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237517092",
+        ["TintId"] = 1,
+        ["Tint"] = "Red"
+    },
+    ["Black Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(36, 36, 36),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237522137",
+        ["TintId"] = 8,
+        ["Tint"] = "Black"
+    },
+    ["Navy Blue Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(0, 71, 114),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237532953",
+        ["TintId"] = 13,
+        ["Tint"] = "Navy Blue"
+    },
+    ["Yellow Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(255, 255, 0),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237543007",
+        ["TintId"] = 4,
+        ["Tint"] = "Yellow"
+    },
+    ["Dark Green Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(0, 108, 0),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237527505",
+        ["TintId"] = 10,
+        ["Tint"] = "Dark Green"
+    },
+    ["Lime Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(175, 255, 0),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237531786",
+        ["TintId"] = 17,
+        ["Tint"] = "Lime"
+    },
+    ["Violet Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(175, 0, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237542002",
+        ["TintId"] = 19,
+        ["Tint"] = "Violet"
+    },
+    ["Cyan Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(85, 255, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237526040",
+        ["TintId"] = 6,
+        ["Tint"] = "Cyan"
+    },
+    ["Purple Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(85, 0, 127),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237536031",
+        ["TintId"] = 9,
+        ["Tint"] = "Purple"
+    },
+    ["Orange Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(255, 85, 0),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237534386",
+        ["TintId"] = 3,
+        ["Tint"] = "Orange"
+    },
+    ["Sky Blue Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(0, 175, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237539485",
+        ["TintId"] = 22,
+        ["Tint"] = "Sky Blue"
+    },
+    ["Green Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(0, 255, 0),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237529769",
+        ["TintId"] = 2,
+        ["Tint"] = "Green"
+    },
+    ["Indigo Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(85, 0, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237530671",
+        ["TintId"] = 20,
+        ["Tint"] = "Indigo"
+    },
+    ["Brown Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(139, 69, 19),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237524885",
+        ["TintId"] = 12,
+        ["Tint"] = "Brown"
+    },
+    ["Rose Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(255, 0, 85),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237538683",
+        ["TintId"] = 21,
+        ["Tint"] = "Rose"
+    },
+    ["Teal Ink Bottle"] = {
+        ["Color"] = Color3.fromRGB(0, 128, 128),
+        ["Image"] = "http://www.roblox.com/asset/?id=12237541162",
+        ["TintId"] = 7,
+        ["Tint"] = "Teal"
+    }
+}
