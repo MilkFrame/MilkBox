91fc692729f2abbf51f36a841c09243c12a79698
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-30 15:55:23 +1100

    Create Animations.lua

---
+++ GameData/Doodle World [5116869569]/Animations.lua
@@ -0,0 +1,15 @@
+data = {
+    {["Id"] = 507770818, ["Name"] = "Laughter"},
+    {["Id"] = 507770239, ["Name"] = "Wave"},
+    {["Id"] = 507770453, ["Name"] = "Point"},
+    {["Id"] = 507771019, ["Name"] = "Dance1"},
+    {["Id"] = 507776043, ["Name"] = "Dance2"},
+    {["Id"] = 507777268, ["Name"] = "Dance3"},
+    {["Id"] = 8958514615, ["Name"] = "Wiggle"},
+    {["Id"] = 8958529742, ["Name"] = "Default"},
+    {["Id"] = 8958542262, ["Name"] = "Dab"},
+    {["Id"] = 8958552633, ["Name"] = "Orange"},
+    {["Id"] = 8958562672, ["Name"] = "LDance"},
+    {["Id"] = 7199000883, ["Name"] = "WakeUpCall"},
+    {["Id"] = 3333499508, ["Name"] = "Monkey"}
+}
