976083431c91a2ca92d75642dcd13bfac6baf7a6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-30 15:33:24 +1100

    Create Equipment.lua

---
+++ GameData/Doodle World [5116869569]/Equipment.lua
@@ -0,0 +1,396 @@
+--AllEquipment()
+data = {
+    ["Helmet"] = {
+        ["21"] = {
+            ["Info"] = "+10 magical defense, +5 speed",
+            ["Image"] = "http://www.roblox.com/asset/?id=13465470381",
+            ["Name"] = "Tinfoil Hat",
+            ["Bonus"] = {["spe"] = 5, ["mdef"] = 10}
+        },
+        ["20"] = {
+            ["Info"] = "+10 defense, +3 magical defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=13465345714",
+            ["Name"] = "Aviator Helmet",
+            ["Bonus"] = {["mdef"] = 3, ["def"] = 10}
+        },
+        ["19"] = {
+            ["Info"] = "+10 attack, +10 magical attack, -10 defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=13261130997",
+            ["Name"] = "Scuba Goggles",
+            ["Bonus"] = {["matk"] = 10, ["atk"] = 10, ["def"] = -10}
+        },
+        ["18"] = {
+            ["Info"] = "+10 attack, +5 speed",
+            ["Image"] = "http://www.roblox.com/asset/?id=13259348855",
+            ["Name"] = "Decorated Hairclip",
+            ["Bonus"] = {["atk"] = 10, ["spe"] = 5}
+        },
+        ["14"] = {
+            ["Info"] = "+15 speed, +10 attack, -20 hp, -20 defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=11468464861",
+            ["Name"] = "Candy Headphones",
+            ["Bonus"] = {["def"] = -20, ["hp"] = -20, ["atk"] = 10, ["spe"] = 15}
+        },
+        ["1"] = {
+            ["Info"] = "+15 speed, -20 attack, -20 defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=8483540389",
+            ["Name"] = "Magnificent Tophat",
+            ["Bonus"] = {["spe"] = 15, ["atk"] = -20, ["def"] = -20}
+        },
+        ["15"] = {
+            ["Info"] = "+5 magical attack, +15 speed, -10 hp, -10 magical defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=12498318021",
+            ["Name"] = "Pale Sunbonnet",
+            ["Bonus"] = {["mdef"] = -10, ["hp"] = -10, ["matk"] = 5, ["spe"] = 15}
+        },
+        ["3"] = {
+            ["Info"] = "+10 attack, +5 defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=8687430499",
+            ["Name"] = "Doodad",
+            ["Bonus"] = {["atk"] = 10, ["def"] = 5}
+        },
+        ["2"] = {
+            ["Info"] = "+5 attack, +5 defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=7325464936",
+            ["Name"] = "Helmet of Nobodom",
+            ["Bonus"] = {["atk"] = 5, ["def"] = 5}
+        },
+        ["5"] = {
+            ["Info"] = "+10 speed",
+            ["Image"] = "http://www.roblox.com/asset/?id=9301998780",
+            ["Name"] = "Head Leaf",
+            ["Bonus"] = {["spe"] = 10}
+        },
+        ["4"] = {
+            ["Info"] = "+6 defense, +6 magical defense, +6 speed, -10 coolness",
+            ["Image"] = "http://www.roblox.com/asset/?id=9238585380",
+            ["Name"] = "Bowlcut",
+            ["Bonus"] = {["def"] = 5, ["spe"] = 6, ["mdef"] = 6}
+        },
+        ["7"] = {
+            ["Info"] = "+15 hp, -5 defense, +25 coolness",
+            ["Image"] = "http://www.roblox.com/asset/?id=9425372450",
+            ["Name"] = "Epic Shades",
+            ["Bonus"] = {["hp"] = 15, ["def"] = -5}
+        },
+        ["6"] = {
+            ["Info"] = "+8 attack, +8 magical defense, -8 magical attack",
+            ["Image"] = "http://www.roblox.com/asset/?id=9173973390",
+            ["Name"] = "Feathered Hat",
+            ["Bonus"] = {["matk"] = -8, ["atk"] = 8, ["mdef"] = 8}
+        },
+        ["9"] = {
+            ["Info"] = "+8 to every stat.",
+            ["Image"] = "http://www.roblox.com/asset/?id=9545077768",
+            ["Name"] = "NPC Buff",
+            ["Bonus"] = {["atk"] = 8, ["def"] = 8, ["hp"] = 8, ["spe"] = 8, ["matk"] = 8, ["mdef"] = 8}
+        },
+        ["8"] = {
+            ["Info"] = "+8 hp, +8 magical attack",
+            ["Image"] = "http://www.roblox.com/asset/?id=9545079365",
+            ["Name"] = "Glummish Cap",
+            ["Bonus"] = {["hp"] = 8, ["matk"] = 8}
+        },
+        ["16"] = {
+            ["Info"] = "+13 defense, -3 speed",
+            ["Image"] = "http://www.roblox.com/asset/?id=12497544585",
+            ["Name"] = "Eggshell Helmet",
+            ["Bonus"] = {["spe"] = -3, ["def"] = 13}
+        },
+        ["17"] = {
+            ["Info"] = "+15 magic attack, -8 speed",
+            ["Image"] = "http://www.roblox.com/asset/?id=12647523457",
+            ["Name"] = "Drenched Sunhat",
+            ["Bonus"] = {["matk"] = 15, ["spe"] = -8}
+        },
+        ["13"] = {
+            ["Info"] = "+16 attack, -6 hp",
+            ["Image"] = "http://www.roblox.com/asset/?id=11005198033",
+            ["Name"] = "Smoldering Hood",
+            ["Bonus"] = {["atk"] = 16, ["hp"] = -6}
+        },
+        ["12"] = {
+            ["Image"] = "http://www.roblox.com/asset/?id=10780122804",
+            ["Name"] = "Marshmallow Fedora",
+            ["SpecificBonus"] = {
+                ["Partybug-A"] = {["matk"] = 20, ["atk"] = 20, ["spe"] = 10},
+                ["Partybug"] = {["matk"] = 20, ["atk"] = 20, ["spe"] = 10}
+            },
+            ["SpecificInfo"] = {
+                ["Partybug-A"] = "+20 attack, +20 magical attack, +10 speed",
+                ["Partybug"] = "+20 attack, +20 magical attack, +10 speed"
+            },
+            ["Info"] = "+10 magical attack, +5 magical defense",
+            ["Bonus"] = {["matk"] = 10, ["mdef"] = 5}
+        },
+        ["11"] = {
+            ["Info"] = "+15 hp, -5 attack",
+            ["Image"] = "http://www.roblox.com/asset/?id=10653724468",
+            ["Name"] = "Horned Headband",
+            ["Bonus"] = {["hp"] = 15, ["atk"] = -5}
+        },
+        ["10"] = {
+            ["Info"] = "+10 def, +10 magical attack, -8 speed",
+            ["Image"] = "http://www.roblox.com/asset/?id=10010213743",
+            ["Name"] = "Theatre Mask",
+            ["Bonus"] = {["spe"] = -8, ["matk"] = 10, ["def"] = 10}
+        }
+    },
+    ["Amulet"] = {
+        ["22"] = {
+            ["Info"] = "+10 attack, +10 defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=13260481118",
+            ["Name"] = "Toxic Vial",
+            ["Bonus"] = {["atk"] = 10, ["def"] = 10}
+        },
+        ["21"] = {
+            ["Info"] = "+4 to every stat except speed, -14 speed",
+            ["Image"] = "http://www.roblox.com/asset/?id=13260141650",
+            ["Name"] = "Geode Pendant",
+            ["Bonus"] = {["atk"] = 4, ["def"] = 4, ["spe"] = -14, ["matk"] = 4, ["hp"] = 4, ["mdef"] = 4}
+        },
+        ["20"] = {
+            ["Image"] = "http://www.roblox.com/asset/?id=12727033455",
+            ["Name"] = "Dark Brooch",
+            ["SpecificInfo"] = {["Grimantle"] = "+12 magical attack, changes type"},
+            ["Info"] = "+12 magical attack",
+            ["Bonus"] = {["matk"] = 12}
+        },
+        ["19"] = {
+            ["Info"] = "+5 speed, +7 magic attack",
+            ["Image"] = "http://www.roblox.com/asset/?id=12725636711",
+            ["Name"] = "Helpful Lantern",
+            ["Bonus"] = {["matk"] = 7, ["spe"] = 5}
+        },
+        ["18"] = {
+            ["Info"] = "+5 attack, +5 defense, +5 magical attack, +5 magical defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=12498508884",
+            ["Name"] = "DIY Pin",
+            ["Bonus"] = {["atk"] = 5, ["mdef"] = 5, ["matk"] = 5, ["def"] = 5}
+        },
+        ["14"] = {
+            ["Info"] = "+10 hp, +5 attack, +5 defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=11720453020",
+            ["Name"] = "Ornate Necklace",
+            ["Bonus"] = {["atk"] = 5, ["hp"] = 10, ["def"] = 5}
+        },
+        ["1"] = {
+            ["Info"] = "+10 hp, +5 magical attack, +5 magical defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=8483367595",
+            ["Name"] = "Sapphire Amulet",
+            ["Bonus"] = {["matk"] = 5, ["hp"] = 10, ["mdef"] = 5}
+        },
+        ["15"] = {
+            ["Info"] = "+20 magical defense, -10 attack",
+            ["Image"] = "http://www.roblox.com/asset/?id=11781167577",
+            ["Name"] = "Festive Bow",
+            ["Bonus"] = {["atk"] = -10, ["mdef"] = 20}
+        },
+        ["3"] = {
+            ["Info"] = "-10 to all stats.",
+            ["Image"] = "http://www.roblox.com/asset/?id=8689055978",
+            ["Name"] = "Scrap Paper",
+            ["Bonus"] = {["atk"] = -10, ["def"] = -10, ["hp"] = -10, ["spe"] = -10, ["matk"] = -10, ["mdef"] = -10}
+        },
+        ["2"] = {
+            ["Info"] = "+5 speed, +8 defense, -5 attack",
+            ["Image"] = "http://www.roblox.com/asset/?id=5786118191",
+            ["Name"] = "Mini Camera",
+            ["Bonus"] = {["spe"] = 5, ["atk"] = -5, ["def"] = 8}
+        },
+        ["5"] = {
+            ["Info"] = "+15 magical defense, +5 speed",
+            ["Image"] = "http://www.roblox.com/asset/?id=9301934076",
+            ["Name"] = "Crystal Shard",
+            ["Bonus"] = {["mdef"] = 15, ["spe"] = 5}
+        },
+        ["4"] = {
+            ["Info"] = "+10 speed, -10 defense, -10 magical defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=8687505607",
+            ["Name"] = "Whatchamacallit",
+            ["Bonus"] = {["mdef"] = -10, ["def"] = -10, ["spe"] = 10}
+        },
+        ["7"] = {
+            ["Info"] = "+8 hp, +8 speed",
+            ["Image"] = "http://www.roblox.com/asset/?id=9744050624",
+            ["Name"] = "Juniper Necklace",
+            ["Bonus"] = {["hp"] = 8, ["spe"] = 8}
+        },
+        ["6"] = {
+            ["Info"] = "+10 hp, +8 attack, +10 coolness",
+            ["Image"] = "http://www.roblox.com/asset/?id=9425373504",
+            ["Name"] = "Swag Juice",
+            ["Bonus"] = {["hp"] = 10, ["atk"] = 8}
+        },
+        ["9"] = {
+            ["Info"] = "+7 hp, +7 magical attack, +7 magical defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=10010062898",
+            ["Name"] = "Rock Candy",
+            ["Bonus"] = {["matk"] = 7, ["hp"] = 7, ["mdef"] = 7}
+        },
+        ["8"] = {
+            ["Info"] = "+8 to every stat.",
+            ["Image"] = "http://www.roblox.com/asset/?id=9545077768",
+            ["Name"] = "NPC Buff",
+            ["Bonus"] = {["atk"] = 8, ["def"] = 8, ["hp"] = 8, ["spe"] = 8, ["matk"] = 8, ["mdef"] = 8}
+        },
+        ["16"] = {
+            ["Info"] = "+10 speed, +5 attack, -15 hp, -5 magical defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=11891438067",
+            ["Name"] = "Dusty Bandana",
+            ["Bonus"] = {["mdef"] = -5, ["hp"] = -15, ["atk"] = 5, ["spe"] = 10}
+        },
+        ["17"] = {
+            ["Info"] = "+7 magic attack, +10 speed, -15 hp, -7 defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=12500367031",
+            ["Name"] = "Sandstone Necklace",
+            ["Bonus"] = {["def"] = -7, ["hp"] = -15, ["matk"] = 7, ["spe"] = 10}
+        },
+        ["13"] = {
+            ["Info"] = "+7 magic attack, +7 attack, +7 hp",
+            ["Image"] = "http://www.roblox.com/asset/?id=11569566124",
+            ["Name"] = "Sticky Pendant",
+            ["Bonus"] = {["hp"] = 7, ["atk"] = 7, ["matk"] = 7}
+        },
+        ["12"] = {
+            ["Info"] = "+10 hp, +10 defense, +10 magic defense, -10 attack, -10 magic attack",
+            ["Image"] = "http://www.roblox.com/asset/?id=10742604427",
+            ["Name"] = "Vial of Tears",
+            ["Bonus"] = {["atk"] = -10, ["def"] = 10, ["matk"] = -10, ["hp"] = 10, ["mdef"] = 10}
+        },
+        ["11"] = {
+            ["Info"] = "+6 to every stat except speed, -20 speed",
+            ["Image"] = "http://www.roblox.com/asset/?id=10663714155",
+            ["Name"] = "Stone Necklace",
+            ["Bonus"] = {["atk"] = 6, ["def"] = 6, ["spe"] = -20, ["matk"] = 6, ["hp"] = 6, ["mdef"] = 6}
+        },
+        ["10"] = {
+            ["Info"] = "+7 attack, +5 speed",
+            ["Image"] = "http://www.roblox.com/asset/?id=10663715484",
+            ["Name"] = "Ruby Pendant",
+            ["Bonus"] = {["atk"] = 7, ["spe"] = 5}
+        }
+    },
+    ["Artifact"] = {
+        ["20"] = {
+            ["Info"] = "+15 hp",
+            ["Image"] = "http://www.roblox.com/asset/?id=13686526771",
+            ["Name"] = "Pure Tear",
+            ["Bonus"] = {["hp"] = 15}
+        },
+        ["19"] = {
+            ["Info"] = "+15 attack, -15 magical attack",
+            ["Image"] = "http://www.roblox.com/asset/?id=13260142732",
+            ["Name"] = "Chewed Up Bone",
+            ["Bonus"] = {["atk"] = 15, ["matk"] = -15}
+        },
+        ["18"] = {
+            ["Info"] = "+15 speed, -5 hp",
+            ["Image"] = "http://www.roblox.com/asset/?id=13259347880",
+            ["Name"] = "Fluffy Feather",
+            ["Bonus"] = {["hp"] = -5, ["spe"] = 15}
+        },
+        ["14"] = {
+            ["Info"] = "+12 magic attack, -12 attack",
+            ["Image"] = "http://www.roblox.com/asset/?id=11569928487",
+            ["Name"] = "Luminous Orb",
+            ["Bonus"] = {["matk"] = 12, ["atk"] = -12}
+        },
+        ["1"] = {
+            ["Info"] = "+10 magical attack, +10 attack",
+            ["Image"] = "http://www.roblox.com/asset/?id=8481005395",
+            ["Name"] = "Oakwood Staff",
+            ["Bonus"] = {["matk"] = 10, ["atk"] = 10}
+        },
+        ["15"] = {
+            ["Info"] = "-10 to all stats.",
+            ["Image"] = "http://www.roblox.com/asset/?id=11768111812",
+            ["Name"] = "Coal",
+            ["Bonus"] = {["atk"] = -10, ["def"] = -10, ["hp"] = -10, ["spe"] = -10, ["matk"] = -10, ["mdef"] = -10}
+        },
+        ["3"] = {
+            ["Info"] = "+7 attack, +7 speed, -7 defense ",
+            ["Image"] = "http://www.roblox.com/asset/?id=8686617055",
+            ["Name"] = "Computer Mouse",
+            ["Bonus"] = {["def"] = -7, ["atk"] = 7, ["spe"] = 7}
+        },
+        ["2"] = {
+            ["Info"] = "+8 attack, +5 speed",
+            ["Image"] = "http://www.roblox.com/asset/?id=8480797559",
+            ["Name"] = "Decorative Katana",
+            ["Bonus"] = {["atk"] = 8, ["spe"] = 5}
+        },
+        ["5"] = {
+            ["Info"] = "+20 defense, +20 magical defense, -20 attack, -20 magical attack",
+            ["Image"] = "http://www.roblox.com/asset/?id=8736098817",
+            ["Name"] = "Beach Ball",
+            ["Bonus"] = {["matk"] = -20, ["mdef"] = 20, ["atk"] = -20, ["def"] = 20}
+        },
+        ["4"] = {
+            ["Info"] = "+20 speed, -20 hp",
+            ["Image"] = "http://www.roblox.com/asset/?id=8480796750",
+            ["Name"] = "Amber Trapped Fly",
+            ["Bonus"] = {["hp"] = -20, ["spe"] = 20}
+        },
+        ["7"] = {
+            ["Info"] = "+5 attack, +10 defense, -5 magic attack, -10 magic defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=9302035220",
+            ["Name"] = "Sad Pebblett",
+            ["Bonus"] = {["mdef"] = -10, ["matk"] = 5, ["atk"] = 5, ["def"] = 10}
+        },
+        ["6"] = {
+            ["Info"] = "+15 attack, +15 magical attack, -15 speed",
+            ["Image"] = "http://www.roblox.com/asset/?id=8687432224",
+            ["Name"] = "Thingamajig",
+            ["Bonus"] = {["matk"] = 15, ["atk"] = 15, ["spe"] = -15}
+        },
+        ["9"] = {
+            ["Info"] = "+8 to every stat.",
+            ["Image"] = "http://www.roblox.com/asset/?id=9545077768",
+            ["Name"] = "NPC Buff",
+            ["Bonus"] = {["atk"] = 8, ["def"] = 8, ["hp"] = 8, ["spe"] = 8, ["matk"] = 8, ["mdef"] = 8}
+        },
+        ["8"] = {
+            ["Info"] = "+4 to every stat.",
+            ["Image"] = "http://www.roblox.com/asset/?id=9545077768",
+            ["Name"] = "Faunsprout Tail",
+            ["Bonus"] = {["atk"] = 4, ["def"] = 4, ["hp"] = 4, ["spe"] = 4, ["matk"] = 4, ["mdef"] = 4}
+        },
+        ["16"] = {
+            ["Info"] = "+8 magic attack, +5 defense, +5 magical defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=11883921041",
+            ["Name"] = "Broken Clay",
+            ["Bonus"] = {["mdef"] = 5, ["matk"] = 8, ["def"] = 5}
+        },
+        ["17"] = {
+            ["Info"] = "+10 attack, +7 speed, -10 defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=12497546707",
+            ["Name"] = "Potted Cactus",
+            ["Bonus"] = {["spe"] = 7, ["atk"] = 10, ["def"] = -10}
+        },
+        ["13"] = {
+            ["Info"] = "+10 speed",
+            ["Image"] = "http://www.roblox.com/asset/?id=10005744431",
+            ["Name"] = "White Chocolate",
+            ["Bonus"] = {["spe"] = 10}
+        },
+        ["12"] = {
+            ["Info"] = "+7 attack, +7 speed, +7 magical attack, -7 hp",
+            ["Image"] = "http://www.roblox.com/asset/?id=10660636761",
+            ["Name"] = "Scrapped Cardboard",
+            ["Bonus"] = {["hp"] = -7, ["matk"] = 7, ["atk"] = 7, ["spe"] = 7}
+        },
+        ["11"] = {
+            ["Info"] = "+10 defense, +10 magic defense",
+            ["Image"] = "http://www.roblox.com/asset/?id=10651561194",
+            ["Name"] = "Pizza Slice",
+            ["Bonus"] = {["mdef"] = 10, ["def"] = 10}
+        },
+        ["10"] = {
+            ["Info"] = "+15 magical attack, -10 hp",
+            ["Image"] = "http://www.roblox.com/asset/?id=10009787169",
+            ["Name"] = "Stick",
+            ["Bonus"] = {["matk"] = 15, ["hp"] = -10}
+        }
+    }
+}
