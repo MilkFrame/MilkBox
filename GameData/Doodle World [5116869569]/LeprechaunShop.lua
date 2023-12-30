da5a64f6224b96cdd144420eedf3b189f207c1cb
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-30 16:02:43 +1100

    Create LeprechaunShop.lua

---
+++ GameData/Doodle World [5116869569]/LeprechaunShop.lua
@@ -0,0 +1,10 @@
+data = {
+    ["PattyDay"] = {["Cost"] = 10, ["Name"] = "Mount", ["Showcase"] = "8"},
+    ["Moyai"] = {["Cost"] = 1, ["Skin"] = 13, ["Name"] = "Moyai", ["Showcase"] = "Squed"},
+    ["Leazar"] = {["Cost"] = 1, ["Skin"] = 11, ["Name"] = "Leazar", ["Showcase"] = "Leapo"},
+    ["Fluppy"] = {["Cost"] = 1, ["Skin"] = 4, ["Name"] = "Fluppy", ["Showcase"] = "Fluppy"},
+    ["Gomutt"] = {["Cost"] = 1, ["Skin"] = 14, ["Name"] = "Gomutt", ["Showcase"] = "Muttish"},
+    ["Pompaboar"] = {["Cost"] = 1, ["Skin"] = 11, ["Name"] = "Pompaboar", ["Showcase"] = "Wiglet"},
+    ["Girasera"] = {["Cost"] = 1, ["Skin"] = 13, ["Name"] = "Girasera", ["Showcase"] = "Dewaffe"},
+    ["Caramellow"] = {["Cost"] = 1, ["Skin"] = 11, ["Name"] = "Caramellow", ["Showcase"] = "Shmellow"}
+}
