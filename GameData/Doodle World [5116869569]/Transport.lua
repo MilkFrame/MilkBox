105d0a8398ac70e694f66816128240e76086bdf5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-30 15:43:06 +1100

    Create Transport.lua

---
+++ GameData/Doodle World [5116869569]/Transport.lua
@@ -0,0 +1,117 @@
+data = {
+    {["Name"] = "NONE", ["Image"] = ""},
+    ["14"] = {
+        ["Type"] = "RainbowSurfboard",
+        ["Image"] = "rbxassetid://14016477253",
+        ["Name"] = "Swirl Surfboard",
+        ["Model"] = "SwirlSurfboard"
+    },
+    ["15"] = {
+        ["Type"] = "RainbowSurfboard",
+        ["Image"] = "rbxassetid://14016477108",
+        ["Name"] = "Beach Surfboard",
+        ["Model"] = "BeachSurfboard"
+    },
+    ["1"] = {
+        ["Image"] = "rbxassetid://12000437109",
+        ["Name"] = "Red Bike",
+        ["Model"] = "RedBike",
+        ["Animations"] = {["Idle"] = 11593080468, ["Movement"] = 11593083652},
+        ["Type"] = "BasicBike"
+    },
+    ["0"] = {["Name"] = "NONE", ["Image"] = ""},
+    ["3"] = {
+        ["Image"] = "rbxassetid://12000436967",
+        ["Name"] = "Yellow Bike",
+        ["Model"] = "YellowBike",
+        ["Price"] = 800,
+        ["Animations"] = {["Idle"] = 11593080468, ["Movement"] = 11593083652},
+        ["Type"] = "BasicBike"
+    },
+    ["2"] = {
+        ["Image"] = "rbxassetid://12000438150",
+        ["Name"] = "Blue Bike",
+        ["Model"] = "BlueBike",
+        ["Price"] = 800,
+        ["Animations"] = {["Idle"] = 11593080468, ["Movement"] = 11593083652},
+        ["Type"] = "BasicBike"
+    },
+    ["5"] = {
+        ["Image"] = "rbxassetid://12000437392",
+        ["Name"] = "Sled",
+        ["Model"] = "Sled",
+        ["Animations"] = {["Idle"] = {11906458735}, ["Movement"] = {11906431770, 11906442560, 11906449489}},
+        ["Type"] = "Sleigh"
+    },
+    ["4"] = {
+        ["Image"] = "rbxassetid://12000437548",
+        ["Name"] = "Green Bike",
+        ["Model"] = "GreenBike",
+        ["Price"] = 800,
+        ["Animations"] = {["Idle"] = 11593080468, ["Movement"] = 11593083652},
+        ["Type"] = "BasicBike"
+    },
+    ["7"] = {
+        ["Type"] = "2023Bike",
+        ["Image"] = "rbxassetid://12000439128",
+        ["Name"] = "2023 New Years Bike",
+        ["Model"] = "2023Bike"
+    },
+    ["6"] = {
+        ["Image"] = "rbxassetid://12000437797",
+        ["Name"] = "Golden Sled",
+        ["Model"] = "GoldenSled",
+        ["Animations"] = {["Idle"] = {11906458735}, ["Movement"] = {11906431770, 11906442560, 11906449489}},
+        ["Type"] = "Sleigh"
+    },
+    ["9"] = {
+        ["Type"] = "EggModel",
+        ["Image"] = "rbxassetid://13155292360",
+        ["Name"] = "Easter Egg",
+        ["Model"] = "EggModel"
+    },
+    ["8"] = {
+        ["Image"] = "rbxassetid://12843990145",
+        ["Name"] = "Lucky Nimbus",
+        ["Model"] = "PattyDay",
+        ["Animations"] = {["Idle"] = {12844000391}, ["Movement"] = {12844001503, 12844002772, 12844003650}},
+        ["Type"] = "PattyDay"
+    },
+    ["16"] = {
+        ["Type"] = "BalloonAnimal",
+        ["Image"] = "rbxassetid://14016632387",
+        ["Name"] = "King Plipo Balloon",
+        ["Model"] = "KingPlipo"
+    },
+    ["17"] = {
+        ["Type"] = "Skateboard",
+        ["Image"] = "rbxassetid://14389937068",
+        ["Name"] = "Golden Skateboard",
+        ["Model"] = "GoldSkateboard"
+    },
+    ["13"] = {
+        ["Type"] = "RainbowSurfboard",
+        ["Image"] = "rbxassetid://14016477456",
+        ["Name"] = "Palm Surfboard",
+        ["Model"] = "PalmSurfboard"
+    },
+    ["12"] = {
+        ["Type"] = "RainbowSurfboard",
+        ["Image"] = "rbxassetid://14016477623",
+        ["Name"] = "Wood Surfboard",
+        ["Model"] = "WoodSurfboard"
+    },
+    ["11"] = {
+        ["Image"] = "rbxassetid://13820836909",
+        ["Name"] = "Skateboard",
+        ["Model"] = "StandardSkateboard",
+        ["Price"] = 1400,
+        ["Type"] = "Skateboard"
+    },
+    ["10"] = {
+        ["Type"] = "RainbowSurfboard",
+        ["Image"] = "rbxassetid://13709117869",
+        ["Name"] = "Rainbow Surfboard",
+        ["Model"] = "RainbowSurfboard"
+    }
+}
