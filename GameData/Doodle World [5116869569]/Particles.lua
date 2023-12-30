e7b8129241f2fbafe52259ab3b860a3b39e8bf34
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2023-12-30 16:04:12 +1100

    Create Particles.lua

---
+++ GameData/Doodle World [5116869569]/Particles.lua
@@ -0,0 +1,15 @@
+data = {
+    ["1"] = {["Name"] = "Arcana", ["Image"] = "rbxassetid://12437431684"},
+    ["0"] = {["Name"] = "NONE", ["Image"] = "rbxassetid://12437499241"},
+    ["3"] = {["Name"] = "Hopes And Dreams", ["Image"] = "rbxassetid://12437431396"},
+    ["2"] = {["Name"] = "Rainy Day", ["Image"] = "rbxassetid://12437431264"},
+    ["5"] = {["Name"] = "Halloween Confetti", ["Image"] = "rbxassetid://12437431523"},
+    ["4"] = {["Name"] = "Confetti", ["Image"] = "rbxassetid://12437431611"},
+    ["7"] = {["Name"] = "Halloween Rain", ["Image"] = "rbxassetid://12437431462"},
+    ["6"] = {["Name"] = "Void Magic", ["Image"] = "rbxassetid://12437431101"},
+    ["9"] = {["Name"] = "Star Rain", ["Image"] = "rbxassetid://12437431176"},
+    ["8"] = {["Name"] = "Winter Storm", ["Image"] = "rbxassetid://12437431028"},
+    ["12"] = {["Name"] = "Doomsday", ["Image"] = "rbxassetid://14333416906"},
+    ["11"] = {["Name"] = "Plipo Portal", ["Image"] = "rbxassetid://14333286454"},
+    ["10"] = {["Name"] = "Star Rain", ["Image"] = "rbxassetid://12437431176"}
+}
