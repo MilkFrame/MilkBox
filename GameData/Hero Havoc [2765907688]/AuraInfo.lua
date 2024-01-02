1140182a112196825bbf943c9aef2e87d32d5d9b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-03 00:39:21 +1100

    Create AuraInfo.lua

---
+++ GameData/Hero Havoc [2765907688]/AuraInfo.lua
@@ -0,0 +1,415 @@
+data = {
+    ["The Wildcard Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=393533339",
+        ["Description"] = "Grants The Wildcard",
+        ["Ability"] = "The Wildcard",
+        ["Rarity"] = "Mythic"
+    },
+    ["Haste I Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(212, 222, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=332655294",
+        ["Description"] = "Grants Haste I Ability.",
+        ["Ability"] = "Haste I",
+        ["Rarity"] = "Epic"
+    },
+    ["Instructor Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 0),
+        ["Image"] = "http://www.roblox.com/asset/?id=695499751",
+        ["Description"] = "Grants Instructor.",
+        ["Ability"] = "Instructor",
+        ["Rarity"] = "Uncommon"
+    },
+    ["Speed Boost Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(235, 135, 19),
+        ["Image"] = "http://www.roblox.com/asset/?id=242842525",
+        ["Description"] = "Grants Speed Boost",
+        ["Ability"] = "Speed Boost",
+        ["Rarity"] = "Mythic"
+    },
+    ["1K Gem Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "rbxassetid://2585309951",
+        ["Description"] = "Gem Aura",
+        ["Ability"] = "1,000 Gems",
+        ["Rarity"] = "Mythic"
+    },
+    ["One For Owol Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "rbxassetid://2723673456",
+        ["Description"] = "Grants One for Owol Ability.",
+        ["Ability"] = "One for Owol",
+        ["Rarity"] = "Common"
+    },
+    ["Strong Armor Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=163851582",
+        ["Description"] = "Grants Strong Armor Ability.",
+        ["Ability"] = "Strong Armor",
+        ["Rarity"] = "Rare"
+    },
+    ["Haste II Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(212, 222, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=332655294",
+        ["Description"] = "Grants Haste II Ability.",
+        ["Ability"] = "Haste II",
+        ["Rarity"] = "Mythic"
+    },
+    ["Burn I Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 177, 98),
+        ["Image"] = "http://www.roblox.com/asset/?id=258126401",
+        ["Description"] = "Grants Burn I Ability.",
+        ["Ability"] = "Burn I",
+        ["Rarity"] = "Common"
+    },
+    ["Bleed I Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 0, 0),
+        ["Image"] = "http://www.roblox.com/asset/?id=241685484",
+        ["Description"] = "Grants Bleed I Ability.",
+        ["Ability"] = "Bleed I",
+        ["Rarity"] = "Common"
+    },
+    ["Depletion III Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(116, 82, 220),
+        ["Image"] = "rbxassetid://1084974286",
+        ["Description"] = "This hero can only attack 3 times per battle.",
+        ["Ability"] = "Depletion III",
+        ["Rarity"] = "Epic"
+    },
+    ["Star Protection Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 0),
+        ["Image"] = "http://www.roblox.com/asset/?id=55569290",
+        ["Description"] = "Makes it so instead of dropping a star, it consumes this instead..",
+        ["Ability"] = "Star Protection",
+        ["Rarity"] = "Mythic"
+    },
+    ["Stun II Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 0),
+        ["Image"] = "http://www.roblox.com/asset/?id=243098098",
+        ["Description"] = "Grants Stun II Ability.",
+        ["Ability"] = "Stun II",
+        ["Rarity"] = "Rare"
+    },
+    ["Resurgence Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(0, 255, 0),
+        ["Image"] = "http://www.roblox.com/asset/?id=243728104",
+        ["Description"] = "Grants Resurgence",
+        ["Ability"] = "Resurgence",
+        ["Rarity"] = "Rare"
+    },
+    ["Trainer Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(0, 85, 127),
+        ["Image"] = "http://www.roblox.com/asset/?id=695499751",
+        ["Description"] = "Grants Trainer.",
+        ["Ability"] = "Trainer",
+        ["Rarity"] = "Mythic"
+    },
+    ["Taco Boost Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "rbxassetid://4972353882",
+        ["Description"] = "Taco Boost Aura",
+        ["Ability"] = "Taco Boost",
+        ["Rarity"] = "Epic"
+    },
+    ["Conflagration Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=242986608",
+        ["Description"] = "Grants Conflagration Ability.",
+        ["Ability"] = "Conflagration",
+        ["Rarity"] = "Mythic"
+    },
+    ["Lifeleech Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 0, 0),
+        ["Image"] = "http://www.roblox.com/asset/?id=242856226",
+        ["Description"] = "Grants Lifeleech",
+        ["Ability"] = "Lifeleech",
+        ["Rarity"] = "Rare"
+    },
+    ["Charm I Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=400125192",
+        ["Description"] = "Grants Charm I Ability.",
+        ["Ability"] = "Charm I",
+        ["Rarity"] = "Rare"
+    },
+    ["Drip Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 0, 0),
+        ["Image"] = "rbxassetid://3945693196",
+        ["Description"] = "Drip Aura",
+        ["Ability"] = "Drip",
+        ["Rarity"] = "Mythic"
+    },
+    ["VIP Gold Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(2254, 180, 0),
+        ["Image"] = "rbxassetid://5776852759",
+        ["Description"] = "VIP Gold Aura",
+        ["Ability"] = "VIP Gold",
+        ["Rarity"] = "Mythic"
+    },
+    ["Lucky Gold Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=4975726292",
+        ["Description"] = "Grants Lucky Gold",
+        ["Ability"] = "Lucky Gold",
+        ["Rarity"] = "Mythic"
+    },
+    ["Regen II Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(155, 255, 105),
+        ["Image"] = "rbxassetid://52756150",
+        ["Description"] = "Grants Regen II Ability.",
+        ["Ability"] = "Regen II",
+        ["Rarity"] = "Epic"
+    },
+    ["Silence I Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(170, 170, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=244221535",
+        ["Description"] = "Grants Silence I Ability.",
+        ["Ability"] = "Silence I",
+        ["Rarity"] = "Common"
+    },
+    ["Regen I Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(155, 255, 105),
+        ["Image"] = "rbxassetid://52756150",
+        ["Description"] = "Grants Regen I Ability.",
+        ["Ability"] = "Regen I",
+        ["Rarity"] = "Uncommon"
+    },
+    ["Pacifist Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "rbxassetid://1274171132",
+        ["Description"] = "Grants Pacifist Ability.",
+        ["Ability"] = "Pacifist",
+        ["Rarity"] = "Common"
+    },
+    ["Degeneration Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(110, 140, 255),
+        ["Image"] = "rbxassetid://5803571093",
+        ["Description"] = "Degeneration Aura",
+        ["Ability"] = "Degeneration",
+        ["Rarity"] = "Rare"
+    },
+    ["Candy Jackpot Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "rbxassetid://5729059520",
+        ["Description"] = "Candy Jackpot Aura",
+        ["Ability"] = "Candy Jackpot",
+        ["Rarity"] = "Mythic"
+    },
+    ["Depletion I Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(60, 50, 90),
+        ["Image"] = "rbxassetid://1084974286",
+        ["Description"] = "This hero can only attack 15 times per battle.",
+        ["Ability"] = "Depletion I",
+        ["Rarity"] = "Uncommon"
+    },
+    ["Slow II Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(0, 170, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=249902641",
+        ["Description"] = "Grants Slow II Ability.",
+        ["Ability"] = "Slow II",
+        ["Rarity"] = "Rare"
+    },
+    ["Level 1 Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(0, 170, 255),
+        ["Image"] = "rbxassetid://5604875827",
+        ["Description"] = "Level 1 Aura",
+        ["Ability"] = "Level 1",
+        ["Rarity"] = "Epic"
+    },
+    ["Poison I Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(170, 0, 127),
+        ["Image"] = "http://www.roblox.com/asset/?id=243664672",
+        ["Description"] = "Grants Poison I Ability.",
+        ["Ability"] = "Poison I",
+        ["Rarity"] = "Common"
+    },
+    ["Decay Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(100, 0, 0),
+        ["Image"] = "http://www.roblox.com/asset/?id=867619398",
+        ["Description"] = "This hero does 70% more damage, but lose 5% of their max health every time they attack.",
+        ["Ability"] = "Decay",
+        ["Rarity"] = "Epic"
+    },
+    ["Nefarious Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=242995411",
+        ["Description"] = "Grants Nefarious Ability.",
+        ["Ability"] = "Nefarious",
+        ["Rarity"] = "Uncommon"
+    },
+    ["Blind II Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(97, 97, 97),
+        ["Image"] = "http://www.roblox.com/asset/?id=37332909",
+        ["Description"] = "Grants Blind II Ability.",
+        ["Ability"] = "Blind II",
+        ["Rarity"] = "Rare"
+    },
+    ["Sturdy Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 170, 127),
+        ["Image"] = "http://www.roblox.com/asset/?id=37332909",
+        ["Description"] = "Grants Sturdy",
+        ["Ability"] = "Sturdy",
+        ["Rarity"] = "Rare"
+    },
+    ["Natural Hunter Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "rbxassetid://459811994",
+        ["Description"] = "Grants Natural Hunter",
+        ["Ability"] = "Natural Hunter",
+        ["Rarity"] = "Epic"
+    },
+    ["Secret Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(0, 0, 0),
+        ["Image"] = "rbxassetid://0",
+        ["Description"] = "Secret Aura",
+        ["Ability"] = "Secret",
+        ["Rarity"] = "Mythic"
+    },
+    ["Stun I Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(83, 129, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=243098098",
+        ["Description"] = "Grants Stun I Ability.",
+        ["Ability"] = "Stun I",
+        ["Rarity"] = "Common"
+    },
+    ["Oppressive Taunt Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "rbxassetid://678262230",
+        ["Description"] = "Grants Oppressive Taunt",
+        ["Ability"] = "Oppressive Taunt",
+        ["Rarity"] = "Common"
+    },
+    ["Bleed II Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 0, 0),
+        ["Image"] = "http://www.roblox.com/asset/?id=241685484",
+        ["Description"] = "Grants Bleed I Ability.",
+        ["Ability"] = "Bleed II",
+        ["Rarity"] = "Rare"
+    },
+    ["Sharpshooter Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "rbxassetid://1432882432",
+        ["Description"] = "Grants Sharpshooter Ability.",
+        ["Ability"] = "Sharpshooter",
+        ["Rarity"] = "Mythic"
+    },
+    ["Feeding Frenzy Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(52, 52, 52),
+        ["Image"] = "http://www.roblox.com/asset/?id=242223105",
+        ["Description"] = "Grants Freeding Frenzy",
+        ["Ability"] = "Feeding Frenzy",
+        ["Rarity"] = "Epic"
+    },
+    ["No Exp Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=4467193037",
+        ["Description"] = "Grants Lifeleech",
+        ["Ability"] = "No Exp",
+        ["Rarity"] = "Common"
+    },
+    ["Owol Training Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "rbxassetid://2723673456",
+        ["Description"] = "Grants Owol Training Ability.",
+        ["Ability"] = "Owol Training",
+        ["Rarity"] = "Epic"
+    },
+    ["Less Damage Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=2085411302",
+        ["Description"] = "Grants Lifeleech",
+        ["Ability"] = "Less Damage",
+        ["Rarity"] = "Uncommon"
+    },
+    ["Treasure Finder Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "rbxassetid://2763006590",
+        ["Description"] = "Grants Treasure Finder Ability.",
+        ["Ability"] = "Treasure Finder",
+        ["Rarity"] = "Epic"
+    },
+    ["Sneaky Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 0, 0),
+        ["Image"] = "http://www.roblox.com/asset/?id=243664672",
+        ["Description"] = "Grants Sneaky Ability.",
+        ["Ability"] = "Sneaky",
+        ["Rarity"] = "Rare"
+    },
+    ["Pierce I Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(213, 255, 0),
+        ["Image"] = "http://www.roblox.com/asset/?id=243043229",
+        ["Description"] = "Grants Pierce I Ability.",
+        ["Ability"] = "Pierce I",
+        ["Rarity"] = "Uncommon"
+    },
+    ["Silence III Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(170, 170, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=244221535",
+        ["Description"] = "Grants Silence III Ability.",
+        ["Ability"] = "Silence III",
+        ["Rarity"] = "Epic"
+    },
+    ["Waterproof Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(130, 136, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=242997264",
+        ["Description"] = "Grants Waterproof Ability.",
+        ["Ability"] = "Waterproof",
+        ["Rarity"] = "Epic"
+    },
+    ["Wiki Contributor Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(0, 85, 255),
+        ["Image"] = "rbxassetid://5777147495",
+        ["Description"] = "Wiki Contributor Aura",
+        ["Ability"] = "Wiki Contributor",
+        ["Rarity"] = "Mythic"
+    },
+    ["Evasive Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(24, 255, 278),
+        ["Image"] = "http://www.roblox.com/asset/?id=284872207",
+        ["Description"] = "Grants Evasive Ability.",
+        ["Ability"] = "Evasive",
+        ["Rarity"] = "Common"
+    },
+    ["Earth Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=3102286960",
+        ["Description"] = "Makes this Hero Earth type.",
+        ["Ability"] = "Earth",
+        ["Rarity"] = "Epic"
+    },
+    ["Alpha Tester Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(170, 0, 0),
+        ["Image"] = "http://www.roblox.com/asset/?id=4642591399",
+        ["Description"] = "Grants the Alpha Tester ability, which increases item drops, experience gain, victory gold, and the chance to drop gold bars.",
+        ["Ability"] = "Alpha Tester",
+        ["Rarity"] = "Mythic"
+    },
+    ["Blind I Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(97, 97, 97),
+        ["Image"] = "http://www.roblox.com/asset/?id=37332909",
+        ["Description"] = "Grants Blind I Ability.",
+        ["Ability"] = "Blind I",
+        ["Rarity"] = "Rare"
+    },
+    ["Depletion II Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(101, 81, 160),
+        ["Image"] = "rbxassetid://1084974286",
+        ["Description"] = "This hero can only attack 8 times per battle.",
+        ["Ability"] = "Depletion II",
+        ["Rarity"] = "Rare"
+    },
+    ["Poison II Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(170, 0, 127),
+        ["Image"] = "http://www.roblox.com/asset/?id=243664672",
+        ["Description"] = "Grants Poison II Ability.",
+        ["Ability"] = "Poison II",
+        ["Rarity"] = "Rare"
+    },
+    ["Slow I Aura"] = {
+        ["ImageColor3"] = Color3.fromRGB(255, 230, 255),
+        ["Image"] = "http://www.roblox.com/asset/?id=249902641",
+        ["Description"] = "Grants Slow I Ability.",
+        ["Ability"] = "Slow I",
+        ["Rarity"] = "Common"
+    }
+}
