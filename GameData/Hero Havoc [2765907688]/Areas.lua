5dcd0e588be562561846d42173ea692d872e8ec8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-03 00:38:51 +1100

    Create Areas.lua

---
+++ GameData/Hero Havoc [2765907688]/Areas.lua
@@ -0,0 +1,179 @@
+data = {
+    {
+        ["BackgroundMusic"] = "rbxassetid://2762631051",
+        ["Name"] = "The Town",
+        ["Location"] = {["CFrame"] = CFrame.new(579.809998, 4.14099979, 75.6999969, 1, 0, 0, 0, 1, 0, 0, 0, 1)},
+        ["ListButton"] = "rbxassetid://2880724082",
+        ["AreaImage"] = "rbxassetid://2756616886",
+        ["Description"] = "This Town is one of the last safe havens of this world."
+    },
+    {
+        ["BackgroundMusic"] = "rbxassetid://2762631418",
+        ["Location"] = {
+            ["CFrame"] = CFrame.new(
+                596.584778,
+                -43.9964905,
+                -2639.59277,
+                0.999604702,
+                3.87762702e-06,
+                -0.0281145535,
+                -6.07051516e-06,
+                1,
+                -7.79127658e-05,
+                0.0281145535,
+                7.80526243e-05,
+                0.999604702
+            )
+        },
+        ["Name"] = "The Forest",
+        ["RecommendedLevel"] = {["Min"] = 1, ["Max"] = 15},
+        ["ListButton"] = "rbxassetid://2692058306",
+        ["AreaImage"] = "rbxassetid://2762942817",
+        ["Description"] = "Find Cassidy in the forest so you can help her take back the forest from Craig.",
+        ["ForceCamera"] = CFrame.new(
+            597.682983,
+            -40.2496834,
+            -2625.47192,
+            0.999946177,
+            -0.00186379487,
+            0.0102099515,
+            0,
+            0.983743489,
+            0.179579303,
+            -0.0103786727,
+            -0.179569632,
+            0.983690441
+        )
+    },
+    {
+        ["Description"] = "Kentucky Smith is one of the strongest fighters around and he offered to train you. You would be a fool to not take up his offer.",
+        ["AreaImage"] = "rbxassetid://2919986153",
+        ["NoAccess"] = "You need to help Cassidy defeat Craig in the forest before you can access this area.",
+        ["Requirement"] = "Forest Badge",
+        ["Name"] = "The Desert",
+        ["BackgroundMusic"] = "rbxassetid://3015995355",
+        ["ListButton"] = "rbxassetid://2692057797",
+        ["Location"] = {
+            ["CFrame"] = CFrame.new(
+                9990.70605,
+                294.500275,
+                10105.1406,
+                0.881843626,
+                1.7564e-08,
+                -0.471541792,
+                1.66617902e-08,
+                1,
+                6.84077293e-08,
+                0.471541792,
+                -6.81816701e-08,
+                0.881843626
+            )
+        },
+        ["RecommendedLevel"] = {["Min"] = 15, ["Max"] = 30},
+        ["ForceCamera"] = CFrame.new(
+            9975.46973,
+            301.357574,
+            10112.4375,
+            0.556482315,
+            0.283022791,
+            -0.781169355,
+            0,
+            0.940194368,
+            0.340638548,
+            0.830859542,
+            -0.189559326,
+            0.523201466
+        )
+    },
+    {
+        ["Description"] = "The King doesn't allow anyone into his castle gates. Find Norman, one of the King's top knights, and ask what you can do to speak to the King.",
+        ["AreaImage"] = "http://www.roblox.com/asset/?id=4115118631",
+        ["NoAccess"] = "You must help Kentucky Smith with his problems in the Desert to gain access to this area.",
+        ["Requirement"] = "Desert Badge",
+        ["Name"] = "The Bamboo Forest",
+        ["BackgroundMusic"] = "rbxassetid://4105371099",
+        ["ListButton"] = "http://www.roblox.com/asset/?id=3124690004",
+        ["RecommendedLevel"] = {["Min"] = 25, ["Max"] = 50},
+        ["Location"] = {
+            ["CFrame"] = CFrame.new(
+                7921.32178,
+                11.3158646,
+                -457.588989,
+                -0.61317724,
+                0.120119505,
+                -0.780759275,
+                -0,
+                0.988371253,
+                0.152060509,
+                0.789945424,
+                0.093240045,
+                -0.606046677
+            )
+        },
+        ["ForceCamera"] = CFrame.new(
+            7918.78076,
+            14.4239426,
+            -463.228851,
+            -0.729222238,
+            0.214805037,
+            -0.649687409,
+            -1.49011612e-08,
+            0.94945097,
+            0.313915372,
+            0.684276938,
+            0.228914082,
+            -0.692360759
+        )
+    },
+    {
+        ["Description"] = "Test your strength against strong opponents in endless waves! The farther you get, the bigger the rewards!",
+        ["AreaImage"] = "http://www.roblox.com/asset/?id=4973367994",
+        ["NoAccess"] = "Help Kentucky Smith with his tasks to get access to the Battle Coliseum!",
+        ["Requirement"] = "Desert Badge",
+        ["Name"] = "Battle Coliseum",
+        ["BackgroundMusic"] = "rbxassetid://4973187100",
+        ["ListButton"] = "http://www.roblox.com/asset/?id=4973367994",
+        ["RecommendedLevel"] = {["Min"] = 20, ["Max"] = 1000},
+        ["Location"] = {
+            ["CFrame"] = CFrame.new(
+                49965.1367,
+                6.82110214,
+                -582.857971,
+                -0.075566113,
+                0.107031107,
+                -0.991379857,
+                -0,
+                0.994222581,
+                0.107337996,
+                0.997140884,
+                0.00811111555,
+                -0.0751295313
+            )
+        },
+        ["ForceCamera"] = CFrame.new(
+            49952.5977,
+            6.99686146,
+            -583.189453,
+            -0.026237838,
+            0.0229011271,
+            -0.999393404,
+            0,
+            0.99973768,
+            0.0229090136,
+            0.999655783,
+            0.000601082982,
+            -0.0262309518
+        )
+    },
+    {
+        ["Description"] = "Battle unique foes and obtain exclusive heroes!",
+        ["AreaImage"] = "rbxassetid://14792559949",
+        ["Requirement"] = "Unlocked Arena",
+        ["Name"] = "The Arena",
+        ["NoAccess"] = "Resolve all incidents in the Bamboo Forest, then speak to the mysterious man in The Town to unlock this area.",
+        ["ListButton"] = "rbxassetid://14792399893",
+        ["BackgroundMusic"] = "rbxassetid://172313730",
+        ["Location"] = {["CFrame"] = CFrame.new(154.190002, 19.5, 1106.46997, 1, 0, 0, 0, 1, 0, 0, 0, 1)},
+        ["RecommendedLevel"] = {["Min"] = 100, ["Max"] = 1000}
+    }
+}
