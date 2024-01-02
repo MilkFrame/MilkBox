7109236456ec433045c77aca631ee941fd86922a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-03 00:39:41 +1100

    Create BPShopData.lua

---
+++ GameData/Hero Havoc [2765907688]/BPShopData.lua
@@ -0,0 +1,45 @@
+data = {
+    ["TacoShop"] = {
+        {
+            ["Price"] = 10,
+            ["Description"] = "This Churro Sword packs a punch!",
+            ["Item"] = "Weapon",
+            ["Amount"] = "ChurroSword"
+        },
+        {
+            ["Price"] = 20,
+            ["Description"] = "A miniature taco truck of your own! Launches spicy tacos at your enemies!",
+            ["Item"] = "Weapon",
+            ["Amount"] = "TacoLauncher"
+        },
+        {["Price"] = 30, ["Item"] = "Soul", ["Amount"] = "Maraca Master"},
+        {["Price"] = 50, ["Item"] = "Aura", ["Amount"] = "Taco Boost Aura"}
+    },
+    ["BPShop"] = {
+        {["Price"] = 1, ["Item"] = "Gold", ["Amount"] = 20000},
+        {["Price"] = 2, ["Item"] = "Soul", ["Amount"] = "Guest"},
+        {["Price"] = 4, ["Item"] = "Soul", ["Amount"] = "Watermelon Shark"},
+        {
+            ["Price"] = 6,
+            ["Description"] = "Give them a taste of freedom!",
+            ["Item"] = "Weapon",
+            ["Amount"] = "AmericanSplender"
+        },
+        {["Price"] = 8, ["Item"] = "Soul", ["Amount"] = "Private First Class Soldier"},
+        {["Price"] = 10, ["Item"] = "Aura", ["Amount"] = "Oppressive Taunt Aura"},
+        {["Price"] = 16, ["Item"] = "Soul", ["Amount"] = "Mime"},
+        {["Price"] = 15, ["Item"] = "Aura", ["Amount"] = "Less Damage Aura"},
+        {["Price"] = 25, ["Item"] = "Aura", ["Amount"] = "Resurgence Aura"},
+        {["Price"] = 25, ["Item"] = "Aura", ["Amount"] = "Lifeleech Aura"},
+        {["Price"] = 30, ["Item"] = "Aura", ["Amount"] = "No Exp Aura"},
+        {["Price"] = 32, ["Item"] = "Soul", ["Amount"] = "builderman"},
+        {["Price"] = 50, ["Item"] = "Aura", ["Amount"] = "Sturdy Aura"},
+        {["Price"] = 100, ["Item"] = "Aura", ["Amount"] = "Feeding Frenzy Aura"},
+        {["Price"] = 100, ["Item"] = "Aura", ["Amount"] = "Natural Hunter Aura"},
+        {["Price"] = 80, ["Item"] = "Aura", ["Amount"] = "Star Protection Aura"},
+        {["Price"] = 150, ["Item"] = "Aura", ["Amount"] = "Decay Aura"},
+        {["Price"] = 200, ["Item"] = "Aura", ["Amount"] = "Degeneration Aura"},
+        {["Price"] = 250, ["Item"] = "Aura", ["Amount"] = "Speed Boost Aura"},
+        {["Price"] = 250, ["Item"] = "Aura", ["Amount"] = "The Wildcard Aura"}
+    }
+}
