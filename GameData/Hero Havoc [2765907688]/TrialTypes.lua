52cfc1d891282decd742243946f90d3e8c18c317
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-03 00:41:03 +1100

    Create TrialTypes.lua

---
+++ GameData/Hero Havoc [2765907688]/TrialTypes.lua
@@ -0,0 +1,7 @@
+data = {
+    {
+        ["Trial"] = "Classic",
+        ["Description"] = "Take on the Timed Trials in it's purest form! No special abilities or restrictions on the enemy.",
+        ["AllowedTypes"] = {"All"}
+    }
+}
