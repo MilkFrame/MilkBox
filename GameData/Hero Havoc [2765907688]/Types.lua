601685d88ac137ce038608ba79f7729a75b6c7c9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-03 00:41:18 +1100

    Create Types.lua

---
+++ GameData/Hero Havoc [2765907688]/Types.lua
@@ -0,0 +1,46 @@
+data = {
+    ["Weaknesses"] = {
+        ["Mirage"] = {"Time", "Fire"},
+        ["Wind"] = {"Earth", "Time", "Space", "Mirage"},
+        ["Plant"] = {"Fire", "Wind", "Time", "Space", "Mirage"},
+        ["Money"] = {"Fire", "Holy"},
+        ["Evil"] = {"Wind"},
+        ["Timeless"] = {"Mirage", "Evil", "Water"},
+        ["Water"] = {"Wind", "Plant", "Space", "Mirage"},
+        ["Elementless"] = {},
+        ["Time"] = {"Space"},
+        ["Space"] = {"Mirage", "Earth"},
+        ["Fire"] = {"Water", "Earth", "Time", "Space"},
+        ["Holy"] = {"Plant"},
+        ["Earth"] = {"Plant", "Water", "Time", "Mirage"}
+    },
+    ["Strengths"] = {
+        ["Mirage"] = {"Water", "Earth", "Wind", "Plant", "Space"},
+        ["Wind"] = {"Water", "Plant", "Earth"},
+        ["Plant"] = {"Water", "Earth", "Holy"},
+        ["Money"] = {"Time", "Mirage", "Space", "Evil"},
+        ["Evil"] = {"Holy", "Wind"},
+        ["Timeless"] = {"Time", "Space", "Holy", "Elementless"},
+        ["Water"] = {"Fire", "Earth", "Time"},
+        ["Elementless"] = {"Evil", "Holy"},
+        ["Time"] = {"Fire", "Earth", "Wind", "Plant", "Mirage"},
+        ["Space"] = {"Fire", "Water", "Wind", "Plant", "Time"},
+        ["Fire"] = {"Wind", "Mirage", "Plant", "Evil", "Money"},
+        ["Holy"] = {"Evil", "Water", "Money"},
+        ["Earth"] = {"Fire", "Space", "Elementless"}
+    },
+    ["List"] = {
+        ["Mirage"] = true,
+        ["Wind"] = true,
+        ["Plant"] = true,
+        ["Evil"] = true,
+        ["Timeless"] = true,
+        ["Water"] = true,
+        ["Elementless"] = true,
+        ["Time"] = true,
+        ["Space"] = true,
+        ["Fire"] = true,
+        ["Holy"] = true,
+        ["Earth"] = true
+    }
+}
