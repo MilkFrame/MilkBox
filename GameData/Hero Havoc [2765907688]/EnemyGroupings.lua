ffe60f89a5cacfc3254c3a8e0941e39ce8ecbf96
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-03 00:40:08 +1100

    Create EnemyGroupings.lua

---
+++ GameData/Hero Havoc [2765907688]/EnemyGroupings.lua
@@ -0,0 +1,236 @@
+data = {
+    ["Alien Commander"] = {
+        {["CharName"] = "Alien Grunt", ["Chance"] = 60},
+        {["CharName"] = "Alien Commander", ["Chance"] = 99},
+        {["CharName"] = "Scoobis", ["Chance"] = 100},
+        ["Arena"] = "SpaceGrid",
+        ["Music"] = "rbxassetid://1845698630"
+    },
+    ["Mush Trooper"] = {
+        {["CharName"] = "Mush Trooper", ["Chance"] = 50},
+        {["CharName"] = "Combatree", ["Chance"] = 65},
+        {["CharName"] = "Green Slime", ["Chance"] = 80},
+        {["CharName"] = "Snail", ["Chance"] = 100},
+        ["Arena"] = "ForestGrid"
+    },
+    ["Samurai Gorilla"] = {
+        {["CharName"] = "Giant Dragonfly", ["Chance"] = 30},
+        {["CharName"] = "Panda", ["Chance"] = 80},
+        {["CharName"] = "Bamboo Guardian", ["Chance"] = 100},
+        ["Arena"] = "BambooGrid",
+        ["Music"] = "rbxassetid://2763924620"
+    },
+    ["Green Slime"] = {{["CharName"] = "Green Slime", ["Chance"] = 100}, ["Arena"] = "ForestGrid"},
+    ["Panda"] = {
+        {["CharName"] = "Bamboozler", ["Chance"] = 50},
+        {["CharName"] = "Panda", ["Chance"] = 80},
+        {["CharName"] = "Giant Dragonfly", ["Chance"] = 95},
+        {["CharName"] = "Ninja Koala", ["Chance"] = 98},
+        {["CharName"] = "Bamboo Guardian", ["Chance"] = 100},
+        ["Arena"] = "BambooGrid",
+        ["Music"] = "rbxassetid://4105372434"
+    },
+    ["Bamboozler"] = {
+        {["CharName"] = "Bamboozler", ["Chance"] = 50},
+        {["CharName"] = "Panda", ["Chance"] = 80},
+        {["CharName"] = "Giant Dragonfly", ["Chance"] = 95},
+        {["CharName"] = "Ninja Koala", ["Chance"] = 98},
+        {["CharName"] = "Bamboo Guardian", ["Chance"] = 100},
+        ["Arena"] = "BambooGrid",
+        ["Music"] = "rbxassetid://4105372434"
+    },
+    ["Owol"] = {
+        {["CharName"] = "Owol", ["Chance"] = 70},
+        {["CharName"] = "Swag Owol", ["Chance"] = 95},
+        {["CharName"] = "Pink Owol", ["Chance"] = 100},
+        ["Arena"] = "ForestGrid"
+    },
+    ["Evilowol"] = {
+        {["CharName"] = "Owol", ["Chance"] = 70},
+        {["CharName"] = "Howoly", ["Chance"] = 90},
+        {["CharName"] = "Swag Owol", ["Chance"] = 91},
+        {["CharName"] = "Pink Owol", ["Chance"] = 100},
+        ["Arena"] = "ForestGrid"
+    },
+    ["Armedadillo"] = {
+        {["CharName"] = "Armedadillo", ["Chance"] = 80},
+        {["CharName"] = "Mumlet", ["Chance"] = 100},
+        ["Music"] = "rbxassetid://3016010756",
+        ["Arena"] = "DesertGrid"
+    },
+    ["Bandit Owol"] = {
+        {["CharName"] = "Bandit Owol", ["Chance"] = 68},
+        {["CharName"] = "Giant Dragonfly", ["Chance"] = 93},
+        {["CharName"] = "Bamboozler", ["Chance"] = 98},
+        {["CharName"] = "Bamboo Guardian", ["Chance"] = 100},
+        ["Arena"] = "BambooGrid",
+        ["Music"] = "rbxassetid://4105372434"
+    },
+    ["Cobrasp"] = {
+        {["CharName"] = "Peasant Mummy", ["Chance"] = 70},
+        {["CharName"] = "Ostigon", ["Chance"] = 77},
+        {["CharName"] = "Royal Mummy", ["Chance"] = 100},
+        ["Music"] = "rbxassetid://2763924620",
+        ["Arena"] = "DesertGrid"
+    },
+    ["Kappa"] = {
+        {["CharName"] = "Bamboo Guardian", ["Chance"] = 10},
+        {["CharName"] = "Kappa", ["Chance"] = 65},
+        {["CharName"] = "Giant Dragonfly", ["Chance"] = 100},
+        ["Arena"] = "BambooGrid",
+        ["Music"] = "rbxassetid://4105372434"
+    },
+    ["Peasant Mummy"] = {
+        {["CharName"] = "Mumlet", ["Chance"] = 20},
+        {["CharName"] = "Peasant Mummy", ["Chance"] = 75},
+        {["CharName"] = "Royal Mummy", ["Chance"] = 100},
+        ["Music"] = "rbxassetid://3016010756",
+        ["Arena"] = "DesertGrid"
+    },
+    ["Swag Owol"] = {
+        {["CharName"] = "Owol", ["Chance"] = 60},
+        {["CharName"] = "Swag Owol", ["Chance"] = 95},
+        {["CharName"] = "Pink Owol", ["Chance"] = 100},
+        ["Arena"] = "ForestGrid"
+    },
+    ["Pig Knight"] = {
+        {["CharName"] = "Pig Knight", ["Chance"] = 70},
+        {["CharName"] = "Bandit Owol", ["Chance"] = 100},
+        ["Arena"] = "BambooGrid",
+        ["Music"] = "rbxassetid://4105372434"
+    },
+    ["Craig the Evil Bandit"] = {{["CharName"] = "Bandit", ["Chance"] = 100}, ["Arena"] = "ForestGrid"},
+    ["Melon Bird"] = {
+        {["CharName"] = "Melon Bird", ["Chance"] = 70},
+        {["CharName"] = "Kumaneko", ["Chance"] = 80},
+        {["CharName"] = "Giant Dragonfly", ["Chance"] = 98},
+        {["CharName"] = "Bamboo Guardian", ["Chance"] = 100},
+        ["Arena"] = "BambooGrid",
+        ["Music"] = "rbxassetid://4105372434"
+    },
+    ["Bandit"] = {
+        {["CharName"] = "Bandit", ["Chance"] = 75},
+        {["CharName"] = "Rogue Bandit", ["Chance"] = 100},
+        ["Completed_Cassidy's Vengeance"] = {
+            {["CharName"] = "Bandit", ["Chance"] = 65},
+            {["CharName"] = "Rogue Bandit", ["Chance"] = 85},
+            {["CharName"] = "Craig the Bandit", ["Chance"] = 100},
+            ["Arena"] = "ForestGrid"
+        },
+        ["Cassidy's Request"] = {
+            {["CharName"] = "Bandit", ["Chance"] = 70},
+            {["CharName"] = "Rogue Bandit", ["Chance"] = 85},
+            {["CharName"] = "Gun Bandit", ["Chance"] = 100},
+            ["Arena"] = "ForestGrid"
+        },
+        ["Arena"] = "ForestGrid"
+    },
+    ["Owol Father"] = {
+        {["CharName"] = "Owol", ["Chance"] = 70},
+        {["CharName"] = "Swag Owol", ["Chance"] = 90},
+        {["CharName"] = "Evilowol", ["Chance"] = 95},
+        {["CharName"] = "Pink Owol", ["Chance"] = 100},
+        ["Music"] = "rbxassetid://2763924620",
+        ["Arena"] = "ForestGrid"
+    },
+    ["Needling"] = {
+        {["CharName"] = "Needling", ["Chance"] = 70},
+        {["CharName"] = "Prickles", ["Chance"] = 100},
+        ["Music"] = "rbxassetid://3016010756",
+        ["Arena"] = "DesertGrid"
+    },
+    ["Rogue Bandit"] = {
+        {["CharName"] = "Bandit", ["Chance"] = 75},
+        {["CharName"] = "Rogue Bandit", ["Chance"] = 100},
+        ["Arena"] = "ForestGrid"
+    },
+    ["Boulder Ogre"] = {
+        {["CharName"] = "Bandit", ["Chance"] = 80},
+        {["CharName"] = "Rogue Bandit", ["Chance"] = 95},
+        {["CharName"] = "Boulder Ogre", ["Chance"] = 100},
+        ["Arena"] = "ForestGrid"
+    },
+    ["Prickles"] = {
+        {["CharName"] = "Needling", ["Chance"] = 50},
+        {["CharName"] = "Prickles", ["Chance"] = 100},
+        ["Music"] = "rbxassetid://3016010756",
+        ["Arena"] = "DesertGrid"
+    },
+    ["The Lone Wanderer"] = {
+        {["CharName"] = "Pig Knight", ["Chance"] = 80},
+        {["CharName"] = "Bandit Owol", ["Chance"] = 100},
+        ["Arena"] = "BambooGrid",
+        ["Music"] = "rbxassetid://4105372434"
+    },
+    ["Kumaneko"] = {
+        {["CharName"] = "Kumaneko", ["Chance"] = 50},
+        {["CharName"] = "Bamboozler", ["Chance"] = 80},
+        {["CharName"] = "Bandit Owol", ["Chance"] = 95},
+        {["CharName"] = "Giant Dragonfly", ["Chance"] = 100},
+        ["Arena"] = "BambooGrid",
+        ["Music"] = "rbxassetid://4105372434"
+    },
+    ["Mumlet"] = {
+        {["CharName"] = "Mumlet", ["Chance"] = 50},
+        {["CharName"] = "Peasant Mummy", ["Chance"] = 75},
+        {["CharName"] = "Royal Mummy", ["Chance"] = 100},
+        ["Music"] = "rbxassetid://3016010756",
+        ["Arena"] = "DesertGrid"
+    },
+    ["Ninja Koala"] = {
+        {["CharName"] = "Ninja Koala", ["Chance"] = 90},
+        {["CharName"] = "Giant Dragonfly", ["Chance"] = 100},
+        ["Arena"] = "BambooGrid",
+        ["Music"] = "rbxassetid://4105372434"
+    },
+    ["Combatree"] = {
+        {["CharName"] = "Combatree", ["Chance"] = 50},
+        {["CharName"] = "Mush Trooper", ["Chance"] = 65},
+        {["CharName"] = "Green Slime", ["Chance"] = 75},
+        {["CharName"] = "Snail", ["Chance"] = 100},
+        ["Arena"] = "ForestGrid"
+    },
+    ["Bamboo Guardian"] = {
+        {["CharName"] = "Bamboozler", ["Chance"] = 85},
+        {["CharName"] = "Giant Dragonfly", ["Chance"] = 90},
+        {["CharName"] = "Kumaneko", ["Chance"] = 98},
+        {["CharName"] = "Bamboo Guardian", ["Chance"] = 100},
+        ["Arena"] = "BambooGrid",
+        ["Music"] = "rbxassetid://4105372434"
+    },
+    ["Finwick"] = {
+        {["CharName"] = "Finwick", ["Chance"] = 75},
+        {["CharName"] = "Giant Dragonfly", ["Chance"] = 100},
+        ["Arena"] = "BambooGrid",
+        ["Music"] = "rbxassetid://4105372434"
+    },
+    ["Alien Grunt"] = {
+        {["CharName"] = "Alien Grunt", ["Chance"] = 80},
+        {["CharName"] = "Alien Commander", ["Chance"] = 100},
+        ["Arena"] = "SpaceGrid",
+        ["Music"] = "rbxassetid://1845698630"
+    },
+    ["Kenducky"] = {
+        {["CharName"] = "Kenducky", ["Chance"] = 100},
+        ["Music"] = "rbxassetid://3016010756",
+        ["Arena"] = "DesertGrid"
+    },
+    ["BossFightGrids"] = {
+        ["PumpkingeistArenaFight"] = "TownGrid",
+        ["Craig"] = "ForestGrid",
+        ["KentuckyFight"] = "DesertGrid",
+        ["BanditQuest"] = "TownGrid",
+        ["KorbloxMageArenaFight_TheArena"] = "IceGrid",
+        ["OneThreeFight"] = "SpaceGrid2",
+        ["TutorialFight"] = "TownGrid"
+    },
+    ["Owol Mother"] = {
+        {["CharName"] = "Owol", ["Chance"] = 70},
+        {["CharName"] = "Swag Owol", ["Chance"] = 90},
+        {["CharName"] = "Evilowol", ["Chance"] = 95},
+        {["CharName"] = "Pink Owol", ["Chance"] = 100},
+        ["Music"] = "rbxassetid://2763924620",
+        ["Arena"] = "ForestGrid"
+    },
+    ["Glubbie"] = {{["CharName"] = "Snail", ["Chance"] = 100}, ["Arena"] = "ForestGrid"}
+}
