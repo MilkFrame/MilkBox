85988fe0b7ba7a233eaf662fcbff67a7da8c1adb
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-03 23:37:07 +1100

    Create Boxes.lua

---
+++ GameData/Miner's Haven [258258996]/Boxes.lua
@@ -0,0 +1,144 @@
+data = {
+    ["Luxury"] = {
+        ["LuxChance"] = 120,
+        ["ContraMulti"] = 0,
+        ["Description"] = "An incredibly hard to obtain box that contains some of the finest items in the game.",
+        ["ThumbnailId"] = 5140907462,
+        ["BoxColor"] = Color3.fromRGB(255, 238, 175),
+        ["CrystalMulti"] = 10,
+        ["ShopChance"] = 5,
+        ["VintageChance"] = 12
+    },
+    ["Easter"] = {
+        ["BoxColor"] = Color3.fromRGB(56, 253, 178),
+        ["ContraMulti"] = 0,
+        ["Description"] = "Contains limited-edition Easter themed items.",
+        ["ThumbnailId"] = 6652258288,
+        ["ShopMulti"] = 0,
+        ["CrystalMulti"] = 25,
+        ["ShopChance"] = 0,
+        ["VintageChance"] = 3
+    },
+    ["Regular"] = {
+        ["BoxColor"] = Color3.fromRGB(200, 150, 100),
+        ["ContraMulti"] = 0,
+        ["Description"] = "A box that contains a random item inside. Open it up!",
+        ["ThumbnailId"] = 5140907803,
+        ["ShopMulti"] = 1,
+        ["CrystalMulti"] = 2,
+        ["ShopChance"] = 1,
+        ["VintageChance"] = 3
+    },
+    ["Spectral"] = {
+        ["LuxChance"] = 25,
+        ["ContraMulti"] = 1,
+        ["Description"] = "A rare box that can only be obtained through supernatural means.",
+        ["ThumbnailId"] = 5140907887,
+        ["BoxColor"] = Color3.fromRGB(21, 225, 223),
+        ["CrystalMulti"] = 2,
+        ["ShopChance"] = 1,
+        ["VintageChance"] = 40
+    },
+    ["Cake Raffle"] = {
+        ["BoxColor"] = Color3.fromRGB(253, 63, 219),
+        ["ContraMulti"] = 0,
+        ["Description"] = "Celebrating 6 years of Miner's Haven. Contains limited-edition birthday themed items.",
+        ["ThumbnailId"] = 6652258583,
+        ["ShopMulti"] = 0,
+        ["CrystalMulti"] = 0,
+        ["ShopChance"] = 0,
+        ["VintageChance"] = 0
+    },
+    ["Twitch"] = {
+        ["BoxColor"] = Color3.fromRGB(119, 74, 255),
+        ["ContraMulti"] = 1,
+        ["Description"] = "Contains limited-edition twitch themed items.",
+        ["ThumbnailId"] = 5140907979,
+        ["ShopMulti"] = 1,
+        ["CrystalMulti"] = 1,
+        ["ShopChance"] = 1,
+        ["VintageChance"] = 26
+    },
+    ["Pumpkin"] = {
+        ["BoxColor"] = Color3.fromRGB(255, 108, 34),
+        ["ContraMulti"] = 1,
+        ["Description"] = "A spooky box containing Halloween items new and old!",
+        ["ThumbnailId"] = 5140907652,
+        ["ShopMulti"] = 1,
+        ["CrystalMulti"] = 1,
+        ["ShopChance"] = 1,
+        ["VintageChance"] = 20
+    },
+    ["Heavenly"] = {
+        ["Description"] = "Could it be..?",
+        ["ShopChance"] = 0,
+        ["ContraMulti"] = 0,
+        ["AllVintages"] = 0,
+        ["ThumbnailId"] = 5140907242,
+        ["LuxChance"] = 0,
+        ["CrystalMulti"] = 0,
+        ["BoxColor"] = Color3.fromRGB(192, 51, 197),
+        ["VintageChance"] = 500
+    },
+    ["Birthday"] = {
+        ["BoxColor"] = Color3.fromRGB(253, 43, 117),
+        ["ContraMulti"] = 0,
+        ["Description"] = "Celebrating 5 years of Miner's Haven.",
+        ["ThumbnailId"] = 5140907021,
+        ["ShopMulti"] = 0,
+        ["CrystalMulti"] = 25,
+        ["ShopChance"] = 0,
+        ["VintageChance"] = 3
+    },
+    ["Unreal"] = {
+        ["LuxChance"] = 5,
+        ["ContraMulti"] = 0,
+        ["Description"] = "A rare box that a chance to turn into something else...",
+        ["ThumbnailId"] = 5140908051,
+        ["BoxColor"] = Color3.fromRGB(194, 38, 255),
+        ["CrystalMulti"] = 10,
+        ["ShopChance"] = 5,
+        ["VintageChance"] = 10
+    },
+    ["Red-Banded"] = {
+        ["LuxChance"] = 75,
+        ["ContraMulti"] = 200,
+        ["Description"] = "A shady box that is highly likely to contain Contraband or Luxery items.",
+        ["ThumbnailId"] = 5140907725,
+        ["BoxColor"] = Color3.fromRGB(85, 29, 30),
+        ["CrystalMulti"] = 25,
+        ["ShopChance"] = 25,
+        ["VintageChance"] = 18
+    },
+    ["Magnificent"] = {
+        ["Description"] = "The most powerful box in existence. How did you get this?",
+        ["BoxColor"] = Color3.fromRGB(0, 255, 247),
+        ["ContraMulti"] = 150,
+        ["AllVintages"] = 0,
+        ["ThumbnailId"] = 5140907561,
+        ["LuxChance"] = 300,
+        ["CrystalMulti"] = 0,
+        ["ShopChance"] = 0,
+        ["VintageChance"] = 320
+    },
+    ["Festive"] = {
+        ["BoxColor"] = Color3.fromRGB(144, 255, 170),
+        ["ContraMulti"] = 1,
+        ["Description"] = "The most wonderful box of the year! Contains limited-edition holiday themed items.",
+        ["ThumbnailId"] = 5140907136,
+        ["ShopMulti"] = 1,
+        ["CrystalMulti"] = 1,
+        ["ShopChance"] = 1,
+        ["VintageChance"] = 26
+    },
+    ["Inferno"] = {
+        ["LuxChance"] = 10,
+        ["ContraMulti"] = 0,
+        ["Description"] = "A box as fiery as it is rare. Contains some of the best items in the game.",
+        ["ThumbnailId"] = 5140907368,
+        ["BoxColor"] = Color3.fromRGB(239, 73, 76),
+        ["CrystalMulti"] = 30,
+        ["ShopChance"] = 12,
+        ["VintageChance"] = 22
+    }
+}
