04d34fb45587823bcdd08c45816aea5777737dc9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-06 23:53:52 +1100

    Create LotteryLibrary.lua

---
+++ GameData/Base Raiders [1696916806]/LotteryLibrary.lua
@@ -0,0 +1,62 @@
+data = {
+    ["RarityOrder"] = {"COMMON", "RARE", "VERY RARE", "EXTRAORDINARY", "UNKNOWN", "GODLY"},
+    ["CratePrices"] = {
+        ["Rainbow Crate"] = 45,
+        ["Legendary Crate"] = 65,
+        ["Base Crate"] = 12,
+        ["Godly Crate"] = 90,
+        ["Dark Matter Crate"] = 35,
+        ["Blazing Crate"] = 25
+    },
+    ["GetItems"] = "function: 0x0000000097cf7df4",
+    ["CR"] = {
+        ["Rainbow Crate"] = {
+            ["EXTRAORDINARY"] = 40,
+            ["COMMON"] = 5,
+            ["RARE"] = 15,
+            ["VERY RARE"] = 20,
+            ["GODLY"] = 5,
+            ["UNKNOWN"] = 15
+        },
+        ["Legendary Crate"] = {
+            ["EXTRAORDINARY"] = 28,
+            ["COMMON"] = 2,
+            ["RARE"] = 5,
+            ["VERY RARE"] = 15,
+            ["GODLY"] = 10,
+            ["UNKNOWN"] = 40
+        },
+        ["Base Crate"] = {
+            ["EXTRAORDINARY"] = 2,
+            ["COMMON"] = 80,
+            ["RARE"] = 15,
+            ["VERY RARE"] = 3,
+            ["GODLY"] = 0,
+            ["UNKNOWN"] = 1
+        },
+        ["Godly Crate"] = {
+            ["EXTRAORDINARY"] = 25,
+            ["COMMON"] = 0,
+            ["RARE"] = 1,
+            ["VERY RARE"] = 2,
+            ["GODLY"] = 40,
+            ["UNKNOWN"] = 32
+        },
+        ["Dark Matter Crate"] = {
+            ["EXTRAORDINARY"] = 12,
+            ["COMMON"] = 10,
+            ["RARE"] = 20,
+            ["VERY RARE"] = 50,
+            ["GODLY"] = 3,
+            ["UNKNOWN"] = 5
+        },
+        ["Blazing Crate"] = {
+            ["EXTRAORDINARY"] = 3,
+            ["COMMON"] = 30,
+            ["RARE"] = 60,
+            ["VERY RARE"] = 5,
+            ["GODLY"] = 1,
+            ["UNKNOWN"] = 2
+        }
+    }
+}
