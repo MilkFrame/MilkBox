15d9ea1bd6cf670ab1fcc8e200d185c0cea3e48a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-06 23:56:00 +1100

    Create Rebirth.lua

---
+++ GameData/Base Raiders [1696916806]/Rebirth.lua
@@ -0,0 +1,34 @@
+data = {
+    ["getUpgradeRewards"] = "function: 0x0000000062bd5b3c",
+    ["getCurrentDailyRewards"] = "function: 0x00000000d0c21564",
+    ["upgradePrices"] = {50000},
+    ["calculateUpgradePrice"] = "function: 0x0000000016b89dd4",
+    ["dailyRewards"] = {{{"BB", 1, "rbxassetid://12121306317", "BB"}}},
+    ["changePlayerLevel"] = "function: 0x00000000b9be468c",
+    ["upgradePricesBB"] = {100},
+    ["oneTimeReward"] = {
+        {{"Pistol", 1, "rbxassetid://2575610837", "UNLOCK"}},
+        {{"Flintlock", 1, "rbxassetid://2575610666", "UNLOCK"}, {"Katana", 1, "rbxassetid://2575586589", "UNLOCK"}},
+        {{"Mauser .96", 1, "rbxassetid://2575551952", "UNLOCK"}, {"Scythe", 1, "rbxassetid://2575594734", "UNLOCK"}},
+        {{"Revolver", 1, "rbxassetid://2575552346", "UNLOCK"}, {"Beast Claws", 1, "rbxassetid://2575520512", "UNLOCK"}},
+        {
+            {"Pump Shotgun", 1, "rbxassetid://2575587251", "UNLOCK"},
+            {"Bowie Knife", 1, "rbxassetid://2575587112", "UNLOCK"}
+        },
+        {{"Danger Mace", 1, "rbxassetid://2575596227", "UNLOCK"}},
+        {{"Sniper", 1, "rbxassetid://2575560803", "UNLOCK"}},
+        {{"Riptide", 1, "rbxassetid://2575577946", "UNLOCK"}},
+        {{"Burst Rifle", 1, "rbxassetid://2575569586", "UNLOCK"}},
+        {{"Skull Gladius", 1, "rbxassetid://2575618116", "UNLOCK"}},
+        {{"Assault Laser", 1, "rbxassetid://2575603360", "UNLOCK"}},
+        {{"Blazing Crate", 1, "rbxassetid://12250401205", "UNCOMMON CRATE"}},
+        {{"Devil Fork", 1, "rbxassetid://2575569767", "UNLOCK"}},
+        {{"Boson Blaster", 1, "rbxassetid://2575586954", "UNLOCK"}},
+        {{"Dark Matter Crate", 1, "rbxassetid://12250402176", "RARE CRATE"}},
+        {{"Rainbow Crate", 1, "rbxassetid://12250400892", "EPIC CRATE"}},
+        {{"Legendary Crate", 1, "rbxassetid://12250401478", "LEGENDARY CRATE"}},
+        {{"Godly Crate", 1, "rbxassetid://12250401675", "GODLY CRATE"}},
+        {{"Rocket Launcher", 1, "rbxassetid://2575603183", "UNLOCK"}},
+        {"NIL"}
+    }
+}
