151e94ab88aeb31f4d6fb8b43b5767d79e97a091
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-06 23:53:33 +1100

    Create DataContainers.lua

---
+++ GameData/Base Raiders [1696916806]/DataContainers.lua
@@ -0,0 +1,43 @@
+data = {
+    ["RarityOrder"] = {"COMMON", "RARE", "VERY RARE", "EXTRAORDINARY", "UNKNOWN", "GODLY"},
+    ["RarityBGs"] = {
+        ["EXTRAORDINARY"] = Color3.fromRGB(199, 0, 234),
+        ["COMMON"] = Color3.fromRGB(222, 222, 222),
+        ["RARE"] = Color3.fromRGB(93, 255, 6),
+        ["VERY RARE"] = Color3.fromRGB(0, 85, 255),
+        ["GODLY"] = Color3.fromRGB(255, 0, 0),
+        ["UNKNOWN"] = Color3.fromRGB(255, 255, 0)
+    },
+    ["CratePrices"] = {
+        ["Rainbow Crate"] = 45,
+        ["Legendary Crate"] = 65,
+        ["Base Crate"] = 12,
+        ["Godly Crate"] = 90,
+        ["Dark Matter Crate"] = 35,
+        ["Blazing Crate"] = 25
+    },
+    ["RarityValues"] = {
+        ["EXTRAORDINARY"] = 4,
+        ["COMMON"] = 1,
+        ["RARE"] = 2,
+        ["VERY RARE"] = 3,
+        ["GODLY"] = 6,
+        ["UNKNOWN"] = 5
+    },
+    ["RewardWaits"] = {
+        ["Reward2"] = 1800,
+        ["Reward4"] = 7200,
+        ["Reward5"] = 18000,
+        ["Reward3"] = 3600,
+        ["Reward1"] = 600
+    },
+    ["ShowRarityBGs"] = {
+        ["EXTRAORDINARY"] = Color3.fromRGB(199, 0, 234),
+        ["COMMON"] = Color3.fromRGB(255, 255, 255),
+        ["RARE"] = Color3.fromRGB(93, 255, 6),
+        ["VERY RARE"] = Color3.fromRGB(0, 85, 255),
+        ["GODLY"] = Color3.fromRGB(255, 0, 0),
+        ["UNKNOWN"] = Color3.fromRGB(255, 255, 0)
+    },
+    ["BasicTrails"] = {["Green"] = true, ["Yellow"] = true, ["Blue"] = true, ["Red"] = true}
+}
