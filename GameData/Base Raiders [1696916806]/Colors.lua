941b5842761ec01c327eed687b7ddd54396b10ea
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-07 01:14:48 +1100

    Create Colors.lua

---
+++ GameData/Base Raiders [1696916806]/Colors.lua
@@ -0,0 +1,14 @@
+data = {
+    ["1"] = {["ColorName"] = "Green", ["ColorValue"] = Color3.fromRGB(75, 151, 75)},
+    ["3"] = {["ColorName"] = "Blue", ["ColorValue"] = Color3.fromRGB(13, 105, 172)},
+    ["2"] = {["ColorName"] = "Red", ["ColorValue"] = Color3.fromRGB(196, 40, 28)},
+    ["5"] = {["ColorName"] = "Pink", ["ColorValue"] = Color3.fromRGB(255, 152, 220)},
+    ["4"] = {["ColorName"] = "Yellow", ["ColorValue"] = Color3.fromRGB(255, 255, 0)},
+    ["7"] = {["ColorName"] = "White", ["ColorValue"] = Color3.fromRGB(237, 234, 234)},
+    ["6"] = {["ColorName"] = "Black", ["ColorValue"] = Color3.fromRGB(27, 42, 53)},
+    ["9"] = {["ColorName"] = "Purple", ["ColorValue"] = Color3.fromRGB(107, 50, 124)},
+    ["8"] = {["ColorName"] = "Brown", ["ColorValue"] = Color3.fromRGB(124, 92, 70)},
+    ["12"] = {["ColorName"] = "Toothpaste", ["ColorValue"] = Color3.fromRGB(0, 255, 255)},
+    ["11"] = {["ColorName"] = "Light blue", ["ColorValue"] = Color3.fromRGB(180, 210, 228)},
+    ["10"] = {["ColorName"] = "Orange", ["ColorValue"] = Color3.fromRGB(226, 155, 64)}
+}
