0e14a1d50be625d79d1d8de145cb52db502137c1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-07 01:09:02 +1100

    Create SongPlaylistOLD.lua

---
+++ GameData/Base Raiders [1696916806]/SongPlaylistOLD.lua
@@ -0,0 +1,36 @@
+data = {
+    ["Upbeat"] = {
+        ["Growing Motivation"] = "1839799999",
+        ["Heaven"] = "1841505136",
+        ["Sunrise Workout"] = "1837324500",
+        ["Up in the Sky"] = "1842047173",
+        ["Upbeat Summer"] = "1839744484"
+    },
+    ["Default"] = {
+        ["Risky Plan"] = "12964028845",
+        ["Escape the Reactor"] = "12964028558",
+        ["Battle March"] = "12964028352",
+        ["Facing Destiny"] = "12964028004",
+        ["Perfect Engineering"] = "12964027576"
+    },
+    ["Intense"] = {
+        ["Battleforce"] = "1835324730",
+        ["Battle is On"] = "1839961023",
+        ["Into Battle"] = "1837226019",
+        ["Empire at War"] = "1839417407"
+    },
+    ["Casual"] = {
+        ["Castle in the Sky"] = "1839805746",
+        ["Medieval Castle"] = "1839906422",
+        ["Medieval Market"] = "1847223666",
+        ["March"] = "1841118108",
+        ["Moscow"] = "1837168840"
+    },
+    ["Cartoon"] = {
+        ["Castle Level"] = "1837768398",
+        ["Old Cartoon"] = "1838660966",
+        ["Cartoon Capers"] = "1841647421",
+        ["Medieval Fantasy"] = "1836102180",
+        ["Cartoon Brigade"] = "1847236007"
+    }
+}
