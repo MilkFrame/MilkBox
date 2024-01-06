1219341392328336e83f4984b63a7ce0ac5a9279
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-07 01:13:48 +1100

    Create RemovedItems.lua

---
+++ GameData/Base Raiders [1696916806]/RemovedItems.lua
@@ -0,0 +1,179 @@
+data = {
+    ["Cannon"] = {
+        ["Type"] = "Shop",
+        ["Description"] = "Use the control pad to aim and shoot the cannon.",
+        ["Price"] = 30000,
+        ["Genre"] = "Protection",
+        ["CustomColorPart"] = {["Value"] = "Main"},
+        ["PartCount"] = 29,
+        ["Model"] = {
+            ["BoulderScript"] = {["OwnerName"] = ""},
+            ["CannonRotator"] = {},
+            ["BaseItemInteraction"] = {
+                ["LocalAnimation"] = {
+                    ["Offset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+                    ["Duration"] = 0,
+                    ["Point"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+                }
+            },
+            ["Cannon"] = {
+                ["BarrelRotator"] = {},
+                ["Barrel"] = {
+                    ["Pivot"] = {},
+                    ["Control"] = {
+                        ["Model"] = {["Part"] = {}},
+                        ["Buttons"] = {["Up"] = {}, ["Left"] = {}, ["Fire"] = {}, ["Down"] = {}, ["Right"] = {}}
+                    }
+                }
+            }
+        },
+        ["ItemLimit"] = 4,
+        ["DestroyHP"] = 475,
+        ["Thumbnail"] = "59430545",
+        ["ItemId"] = 146
+    },
+    ["Arm Cannon"] = {
+        ["Arm Cannon"] = {["Animations"] = {}, ["Handle"] = {}},
+        ["Type"] = "Guns",
+        ["Description"] = "Testing 101112",
+        ["WeaponId"] = 571,
+        ["Price"] = 10,
+        ["Thumbnail"] = "128593330",
+        ["Currency"] = "Cash"
+    },
+    ["Lasers"] = {
+        ["Type"] = "Chest",
+        ["Description"] = "Kills enemies who make contact.",
+        ["Price"] = 0,
+        ["Genre"] = "Protection",
+        ["PartCount"] = 3,
+        ["Model"] = {},
+        ["Rarity"] = "RARE",
+        ["DestroyHP"] = 500,
+        ["Thumbnail"] = 2043513676,
+        ["ItemId"] = 96
+    },
+    ["Hyperlaser"] = {["Handle"] = {}},
+    ["Model"] = {
+        ["Neon Flat"] = {
+            ["Thumbnail"] = 2942543438,
+            ["Type"] = "Basic",
+            ["PartCount"] = 2,
+            ["Model"] = {["ColorPart"] = {}},
+            ["Price"] = 10,
+            ["DestroyHP"] = 200,
+            ["Genre"] = "Basic",
+            ["ItemId"] = 173
+        },
+        ["Neon Plate"] = {
+            ["Type"] = "Basic",
+            ["Price"] = 10,
+            ["Genre"] = "Basic",
+            ["PartCount"] = 2,
+            ["Model"] = {["ColorPart"] = {["Part"] = {}}},
+            ["Hitbox"] = {},
+            ["DestroyHP"] = 220,
+            ["Thumbnail"] = "rbxassetid://2893280740",
+            ["ItemId"] = 116
+        },
+        ["Neon Wedge"] = {
+            ["Type"] = "Basic",
+            ["Price"] = 10,
+            ["Genre"] = "Basic",
+            ["PartCount"] = 2,
+            ["Model"] = {["ColorPart"] = {["Wedge"] = {}}},
+            ["Hitbox"] = {},
+            ["DestroyHP"] = 180,
+            ["Thumbnail"] = "rbxassetid://2893137489",
+            ["ItemId"] = 18
+        },
+        ["Neon Wall"] = {
+            ["Type"] = "Basic",
+            ["Price"] = 10,
+            ["Genre"] = "Basic",
+            ["PartCount"] = 2,
+            ["Model"] = {["ColorPart"] = {["Part"] = {}}},
+            ["Hitbox"] = {},
+            ["DestroyHP"] = 220,
+            ["Thumbnail"] = "rbxassetid://2893267367",
+            ["ItemId"] = 17
+        },
+        ["Neon Corner Wedge"] = {
+            ["Thumbnail"] = "rbxassetid://2893254098",
+            ["Type"] = "Basic",
+            ["PartCount"] = 2,
+            ["Model"] = {["ColorPart"] = {["CornerWedge"] = {}}},
+            ["Price"] = 10,
+            ["DestroyHP"] = 180,
+            ["Genre"] = "Basic",
+            ["ItemId"] = 129
+        },
+        ["Neon Pyramid"] = {
+            ["Type"] = "Basic",
+            ["Price"] = 10,
+            ["Genre"] = "Basic",
+            ["PartCount"] = 2,
+            ["Model"] = {["ColorPart"] = {}},
+            ["Hitbox"] = {},
+            ["DestroyHP"] = 180,
+            ["Thumbnail"] = 2942530781,
+            ["ItemId"] = 182
+        },
+        ["Neon Block"] = {
+            ["Type"] = "Basic",
+            ["Price"] = 10,
+            ["Genre"] = "Basic",
+            ["PartCount"] = 2,
+            ["Model"] = {["ColorPart"] = {["Part"] = {}}},
+            ["Hitbox"] = {},
+            ["DestroyHP"] = 180,
+            ["Thumbnail"] = "rbxassetid://2893150644",
+            ["ItemId"] = 16
+        }
+    },
+    ["Skins"] = {
+        ["Confetti"] = {
+            ["Id"] = 4,
+            ["Texture"] = 19452574,
+            ["Thumbnail"] = 19452574,
+            ["AwardCondition"] = "Get 50 kills to unlock!"
+        },
+        ["Zebra"] = {["Id"] = 3, ["Rarity"] = "RARE", ["Thumbnail"] = 113813212, ["Texture"] = 113813212},
+        ["GoldBUX"] = {["Id"] = 5, ["BUXPrice"] = 50, ["Thumbnail"] = 2560444019, ["Texture"] = 2560435862},
+        ["Gold"] = {["Id"] = 2, ["RobuxPrice"] = 0, ["Thumbnail"] = 2560444019, ["Texture"] = 2560435862}
+    },
+    ["BathClick"] = {
+        ["Type"] = "Shop",
+        ["Description"] = "Select the taps to fill and empty the bath.",
+        ["Price"] = 4000,
+        ["Genre"] = "Furniture",
+        ["PartCount"] = 7,
+        ["Model"] = {["ColorPart"] = {}, ["InvisC"] = {}, ["Model"] = {["Model"] = {}}},
+        ["DestroyHP"] = 330,
+        ["Thumbnail"] = "59430545",
+        ["ItemId"] = 67
+    },
+    ["Starblox Latte"] = {
+        ["Type"] = "Other",
+        ["Description"] = "Testing 123",
+        ["WeaponId"] = 505,
+        ["Price"] = 61,
+        ["PowerupEffect"] = "SpeedyLatte",
+        ["Currency"] = "Cash",
+        ["Custom"] = 1,
+        ["Thumbnail"] = "2575520777",
+        ["Starblox Latte"] = {["Script"] = {}, ["Handle"] = {}},
+        ["Rarity"] = "EXTRAORDINARY"
+    },
+    ["Attack Doge"] = {
+        ["Type"] = "Other",
+        ["Description"] = "Testing 123",
+        ["WeaponId"] = 515,
+        ["Price"] = 99,
+        ["Currency"] = "Cash",
+        ["Custom"] = 1,
+        ["Rarity"] = "UNKNOWN",
+        ["Thumbnail"] = "2575513094",
+        ["Attack Doge"] = {["Script"] = {["AIScript"] = {}}, ["Handle"] = {}}
+    }
+}
