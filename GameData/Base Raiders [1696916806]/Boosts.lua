acbc4f55d22fc03ad8a117c6a99ce3a27567899c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-07 01:14:33 +1100

    Create Boosts.lua

---
+++ GameData/Base Raiders [1696916806]/Boosts.lua
@@ -0,0 +1,41 @@
+data = {
+    ["Low Gravity"] = {
+        ["Custom"] = 3,
+        ["Description"] = "Gives you 33 extra jump power.",
+        ["Rarity"] = "VERY RARE",
+        ["Duration"] = 15,
+        ["Thumbnail"] = 12427519744,
+        ["Changes"] = {["JumpPower"] = 33}
+    },
+    ["Speed Increase"] = {
+        ["Custom"] = 5,
+        ["Description"] = "Gives you 3 extra speed.",
+        ["Rarity"] = "VERY RARE",
+        ["Duration"] = 15,
+        ["Thumbnail"] = 12327883891,
+        ["Changes"] = {["WalkSpeed"] = 3}
+    },
+    ["Double Cash"] = {
+        ["Custom"] = 1,
+        ["Rarity"] = "EXTRAORDINARY",
+        ["Duration"] = 15,
+        ["Thumbnail"] = 12327884826,
+        ["Description"] = "Gives you double cash."
+    },
+    ["Mini Juggernaut"] = {
+        ["Custom"] = 2,
+        ["Description"] = "Gives you 30 extra health and 3 extra speed.",
+        ["Rarity"] = "EXTRAORDINARY",
+        ["Duration"] = 15,
+        ["Thumbnail"] = 12327884390,
+        ["Changes"] = {["Health"] = 30, ["WalkSpeed"] = 3}
+    },
+    ["Health Increase"] = {
+        ["Custom"] = 4,
+        ["Description"] = "Gives you 20 extra health.",
+        ["Rarity"] = "VERY RARE",
+        ["Duration"] = 15,
+        ["Thumbnail"] = 12327884592,
+        ["Changes"] = {["Health"] = 20}
+    }
+}
