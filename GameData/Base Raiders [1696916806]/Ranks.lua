7f6794e3fc98a453679b332ed0e3cfb640e8f17c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-06 23:54:55 +1100

    Create Ranks.lua

---
+++ GameData/Base Raiders [1696916806]/Ranks.lua
@@ -0,0 +1,56 @@
+data = {
+    ["Nuclear"] = {
+        ["BadgeId"] = 2069010784,
+        ["InnerColor"] = Color3.fromRGB(0, 213, 17),
+        ["KillsRequired"] = 10000,
+        ["OuterColor"] = Color3.fromRGB(0, 44, 2)
+    },
+    ["Legend"] = {
+        ["BadgeId"] = 1697008648,
+        ["InnerColor"] = Color3.fromRGB(158, 94, 3),
+        ["KillsRequired"] = 2500,
+        ["OuterColor"] = Color3.fromRGB(65, 52, 3)
+    },
+    ["Expert"] = {
+        ["BadgeId"] = 1697007964,
+        ["InnerColor"] = Color3.fromRGB(16, 16, 216),
+        ["KillsRequired"] = 500,
+        ["OuterColor"] = Color3.fromRGB(5, 5, 77)
+    },
+    ["Unstoppable"] = {
+        ["BadgeId"] = 2069011384,
+        ["InnerColor"] = Color3.fromRGB(243, 243, 243),
+        ["KillsRequired"] = 50000,
+        ["OuterColor"] = Color3.fromRGB(27, 27, 27)
+    },
+    ["Professional"] = {
+        ["BadgeId"] = 1697007680,
+        ["InnerColor"] = Color3.fromRGB(217, 13, 17),
+        ["KillsRequired"] = 100,
+        ["OuterColor"] = Color3.fromRGB(66, 0, 0)
+    },
+    ["Skilled"] = {
+        ["BadgeId"] = 1697007366,
+        ["InnerColor"] = Color3.fromRGB(77, 0, 193),
+        ["KillsRequired"] = 50,
+        ["OuterColor"] = Color3.fromRGB(68, 9, 94)
+    },
+    ["Fearless"] = {
+        ["BadgeId"] = 2069011139,
+        ["InnerColor"] = Color3.fromRGB(192, 0, 198),
+        ["KillsRequired"] = 25000,
+        ["OuterColor"] = Color3.fromRGB(80, 11, 97)
+    },
+    ["Catastrophic"] = {
+        ["BadgeId"] = 2069010199,
+        ["InnerColor"] = Color3.fromRGB(244, 228, 0),
+        ["KillsRequired"] = 5000,
+        ["OuterColor"] = Color3.fromRGB(36, 36, 36)
+    },
+    ["Master"] = {
+        ["BadgeId"] = 1697008186,
+        ["InnerColor"] = Color3.fromRGB(158, 94, 3),
+        ["KillsRequired"] = 1000,
+        ["OuterColor"] = Color3.fromRGB(65, 52, 3)
+    }
+}
