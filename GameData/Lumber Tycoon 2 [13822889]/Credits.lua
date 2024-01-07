9f1d5a9a7f6e8934b634834c738c1a09b43616b0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-08 03:07:11 +1100

    Create Credits.lua

---
+++ GameData/Lumber Tycoon 2 [13822889]/Credits.lua
@@ -0,0 +1,49 @@
+data = {
+    {["credits"] = {"Josh Sheldon - jshel.co", ""}, ["heading"] = "Game design / development"},
+    {["credits"] = {"RandomFlowz", ""}, ["heading"] = "Icon / thumbnail graphics"},
+    {["credits"] = {"Kevin MacLeod - incompetech.com", "Frank Nora - mcs.franknora.com", ""}, ["heading"] = "Music"},
+    {
+        ["credits"] = {"Mike Koenig - soundbible.com", "Roblox user Nexx", "Roblox user DTF", "", ""},
+        ["heading"] = "Sound Effects"
+    },
+    {
+        ["credits"] = {
+            "Unikarnz",
+            "Jac16king",
+            "Fjalon",
+            "Trainsparency",
+            "Jerricks",
+            "SMALLSOPE42",
+            "Mehdamorphis",
+            "Jimkulus",
+            "Ewnceladus",
+            "Smatchet",
+            "ElectroTechnologies",
+            "Tri_angulum",
+            "RandomFlowz",
+            "Axivele",
+            "TC1010tiago",
+            "",
+            ""
+        },
+        ["heading"] = "With retexture and model contributions by:"
+    },
+    {
+        ["credits"] = {"NitroretroOfficial - Romanian", "@iniciantinroblox (Mr_Tower) - Portugese"},
+        ["heading"] = "Volunteer translations by:"
+    },
+    {
+        ["credits"] = {
+            "EpikYummeh",
+            "SatansNoob",
+            "CeIesti",
+            "man2222222222",
+            "Khanovich",
+            "Quenty",
+            "Rukiryo",
+            "Gusmanak",
+            "Zolarketh"
+        },
+        ["heading"] = "Special thanks to the following Roblox users:"
+    }
+}
