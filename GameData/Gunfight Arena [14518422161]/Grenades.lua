6f2ce4c0d6ab9d56bae6a073afe0fe0eb24a627f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-08 03:32:57 +1100

    Create Grenades.lua

---
+++ GameData/Gunfight Arena [14518422161]/Grenades.lua
@@ -0,0 +1,12 @@
+data = {
+    ["M68 Impact"] = {["Radius"] = 30, ["Desc"] = "An M67 frag with an electric fuse that detonates on impact"},
+    ["Bauble"] = {["Radius"] = 35, ["Limited"] = true, ["Desc"] = "A very festive explosive device"},
+    ["Holy Hand Grenade"] = {
+        ["Radius"] = 50,
+        ["Desc"] = "Lobbest thou thy Holy Hand Grenade of Antioc towards thy foe. Who being not in my sight, shall snuff it."
+    },
+    ["M67 HE"] = {["Radius"] = 40, ["Desc"] = "A high-explosive fragmentation grenade with a large blast radius"},
+    ["M26 Frag"] = {["Radius"] = 30, ["Desc"] = "A basic fragmentation grenade with a 5 second fuse"},
+    ["M34 Willy Pete"] = {["Radius"] = 30, ["Desc"] = "An infantry smoke grenade"},
+    ["V40 Mini"] = {["Radius"] = 25, ["Desc"] = "A small grenade with a small blast radius allows you to carry more"}
+}
