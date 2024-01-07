33be8a952f49581fd896bb6f49e8e01ab534e131
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-08 03:05:51 +1100

    Create RegionInfo.lua

---
+++ GameData/Lumber Tycoon 2 [13822889]/RegionInfo.lua
@@ -0,0 +1,16 @@
+data = {
+    ["Region_Swamp"] = {["MaxTreeCount"] = 100, ["TreeCount"] = 100},
+    ["Region_Mountainside"] = {["MaxTreeCount"] = 120, ["TreeCount"] = 119},
+    ["Parts"] = {["MaxTreeCount"] = 0, ["TreeCount"] = 1},
+    ["Region_Snow"] = {["MaxTreeCount"] = 90, ["TreeCount"] = 90},
+    ["Region_Tropics"] = {["MaxTreeCount"] = 80, ["TreeCount"] = 68},
+    ["Region_MazeCave"] = {["MaxTreeCount"] = 12, ["TreeCount"] = 2},
+    ["Main"] = {["MaxTreeCount"] = 35, ["TreeCount"] = 21},
+    ["Region_LittleMeadow"] = {["MaxTreeCount"] = 15, ["TreeCount"] = 15},
+    ["Region_SnowPeak"] = {["MaxTreeCount"] = 22, ["TreeCount"] = 17},
+    ["Region_Main"] = {["MaxTreeCount"] = 250, ["TreeCount"] = 185},
+    ["Region_Plains"] = {["MaxTreeCount"] = 110, ["TreeCount"] = 110},
+    ["Region_Underbridge"] = {["MaxTreeCount"] = 1, ["TreeCount"] = 1},
+    ["SeaIsland"] = {["TreeCount"] = 30, ["MaxTreeCount"] = 30},
+    ["Region_Volcano"] = {["TreeCount"] = 10, ["MaxTreeCount"] = 10}
+}
