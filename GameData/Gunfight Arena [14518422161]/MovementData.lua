fd01190f2759d5fc67bdb8c06f93ba23606ef532
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-08 03:23:01 +1100

    Create MovementData.lua

---
+++ GameData/Gunfight Arena [14518422161]/MovementData.lua
@@ -0,0 +1,9 @@
+data = {
+    ["JumpBoost"] = 0.6,
+    ["Acceleration"] = 0.1,
+    ["RunSpeed"] = 28,
+    ["CanDoubleSprint"] = true,
+    ["DamageSlowdown"] = 0.9,
+    ["WalkSpeed"] = 12,
+    ["SlideSpeed"] = 0.8
+}
