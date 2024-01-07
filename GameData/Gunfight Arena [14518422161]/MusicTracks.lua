9769608b879f563afdcfd583452618f7f1d1071d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-08 03:34:38 +1100

    Create MusicTracks.lua

---
+++ GameData/Gunfight Arena [14518422161]/MusicTracks.lua
@@ -0,0 +1,49 @@
+data = {
+    ["break and enter"] = "rbxassetid://9045194867",
+    ["Last"] = {
+        ["Special Agent Undercover B"] = "rbxassetid://1847858801",
+        ["Army Of Failure A"] = "rbxassetid://1847789693",
+        ["Pulse B"] = "rbxassetid://9039873957",
+        ["Dive Bombing"] = "rbxassetid://1843200666",
+        ["menacing shadows a"] = "rbxassetid://9044041042",
+        ["Bad Mission A"] = "rbxassetid://1847858822"
+    },
+    ["dark city lights"] = "rbxassetid://1842872594",
+    ["aliens among us a"] = "rbxassetid://9044042022",
+    ["mighty warrior a"] = "rbxassetid://1848179238",
+    ["Mid"] = {
+        ["Weird Suspense"] = "rbxassetid://1847503346",
+        ["The Thrill Of Action B"] = "rbxassetid://1847731470",
+        ["intense operation"] = "rbxassetid://1836708140",
+        ["Thrilling Moments C"] = "rbxassetid://1847683399",
+        ["Under Arrest"] = "rbxassetid://1847406142",
+        ["Scary Beat A"] = "rbxassetid://1847763927",
+        ["Electronic Tension"] = "rbxassetid://1847693202",
+        ["Evil Forces A"] = "rbxassetid://9038849759"
+    },
+    ["End"] = {
+        ["Fat Claws 8 Second Sting"] = "rbxassetid://1842592490",
+        ["Battle Plans D"] = "rbxassetid://1845848077",
+        ["Family Man 6 Second Sting"] = "rbxassetid://1842629963",
+        ["Rock Meets Trap G"] = "rbxassetid://9043943033",
+        ["Rush The Stage E"] = "rbxassetid://1845837803",
+        ["The Greater Of Two Evils 6 Second Sting"] = "rbxassetid://1842580227",
+        ["Train Hard Play Harder E"] = "rbxassetid://1845824349"
+    },
+    ["cyber attack a"] = "rbxassetid://9046236939",
+    ["steel shifter"] = "rbxassetid://9045165774",
+    ["siren rage"] = "rbxassetid://1842702273",
+    ["derailed a"] = "rbxassetid://1848162372",
+    ["combined forces"] = "rbxassetid://1844980507",
+    ["true grit"] = "rbxassetid://1836826139",
+    ["Spawn"] = {
+        ["Smiling Skulls (15 sec)"] = "rbxassetid://1837449306",
+        ["Ra's Al-Ghul 12 Second Link"] = "rbxassetid://1842562671",
+        ["Halloween Nightmare C"] = "rbxassetid://9043923969",
+        ["Knuckle Buster F"] = "rbxassetid://9043953099",
+        ["Rock This House F"] = "rbxassetid://9043944549",
+        ["Mechanical Warrior Short"] = "rbxassetid://1837460626",
+        ["Hurt Like Hell C"] = "rbxassetid://9043926391",
+        ["Lock and Load Short"] = "rbxassetid://1837460710"
+    }
+}
