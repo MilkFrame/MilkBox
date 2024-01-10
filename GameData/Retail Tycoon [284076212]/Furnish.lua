27dfc9fec4ed19ce27a1428210a1f31dbf4cfcfe
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-11 00:58:18 +1100

    Create Furnish.lua

---
+++ GameData/Retail Tycoon [284076212]/Furnish.lua
@@ -0,0 +1,106 @@
+data = {
+    ["Management"] = {
+        ["Desk"] = {
+            ["Price"] = 1500,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A desk with all the equipment your managers need for managing things"
+        },
+        ["StorageRack"] = {
+            ["Price"] = 500,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A large rack for holding and distributing all your extra inventory."
+        }
+    },
+    ["Decoration"] = {
+        ["CenterPlant"] = {
+            ["Price"] = 100,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A decorative potted plant (Centered on the tile)"
+        },
+        ["Tree"] = {["Price"] = 150, ["GamepadSelect"] = {}, ["Desc"] = "A decorative potted tree"},
+        ["MoneyTree"] = {
+            ["Price"] = 2000,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A decorative plant that periodically drops money!"
+        },
+        ["SidePlant"] = {
+            ["Price"] = 100,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A decorative potted plant (Placed to one side of the tile)"
+        },
+        ["Statue"] = {
+            ["Price"] = 0,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A solid gold statue of the store owner. Thanks for being a beta tester!"
+        },
+        ["CornerPlant"] = {
+            ["Price"] = 100,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A decorative potted plant (Placed on the corner of the tile)"
+        },
+        ["Fountain"] = {
+            ["Price"] = 400,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A water feature to entertain your customers"
+        },
+        ["Bench"] = {["Price"] = 300, ["GamepadSelect"] = {}, ["Desc"] = "(Currently nonfunctional)"}
+    },
+    ["CustomerService"] = {
+        ["NewRegister"] = {
+            ["Price"] = 1200,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "Customers line up here to pay for things. Must be manned by a player or cashier."
+        },
+        ["SelfCheckout"] = {
+            ["Price"] = 2000,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "While slower than a normal register, the self checkout takes up less space and doesn't require a cashier."
+        }
+    },
+    ["Special"] = {
+        ["Statue"] = {
+            ["Price"] = 0,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A solid gold statue of the store owner. Earned by being a beta tester."
+        },
+        ["MoneyTree"] = {
+            ["Price"] = 2000,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A tree that periodically drops money! Multiple can be earned by selling 10, 20, 50, etc. of every item."
+        }
+    },
+    ["DisplayItems"] = {
+        ["ClothingRack"] = {
+            ["Price"] = 700,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "The superior choice in clothing display"
+        },
+        ["DisplayTable"] = {
+            ["Price"] = 700,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A fancy table well-suited for displaying the newest tech."
+        },
+        ["HatRack"] = {["Price"] = 500, ["GamepadSelect"] = {}, ["Desc"] = "A stylish hat display device"},
+        ["DisplayCase"] = {
+            ["Price"] = 800,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A fancy yet secure case for displaying expensive things"
+        },
+        ["GameShelves"] = {
+            ["Price"] = 600,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "Modified shelving unit for displaying books and such."
+        },
+        ["Freezer"] = {
+            ["Price"] = 800,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "Chilly container for displaying cool stuff."
+        },
+        ["Shelves"] = {
+            ["Price"] = 500,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "Your average shelving unit, great for putting things on."
+        },
+        ["Rack"] = {["Price"] = 400, ["GamepadSelect"] = {}, ["Desc"] = "Cheap, rugged, practical."}
+    }
+}
