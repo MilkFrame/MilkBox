56775ef12ddaaae80783d354646b4e42953fd2ee
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-11 01:38:03 +1100

    Create Trainers.lua

---
+++ GameData/Project Polaro [15557641005]/Trainers.lua
@@ -0,0 +1,410 @@
+data = {
+    {"Water Trainer", "Water2", 20000, {{"Tapu Fini", 100, false}}},
+    {"Water Trainer", "Water3", 20000, {{"Dracovish", 100, false}}},
+    {"Darkness Trainer", "Darkness3", 20000, {{"Hydreigon", 100, false}}},
+    {"Darkness Trainer", "Darkness4", 20000, {{"Absol", 100, false}}},
+    {"Darkness Trainer", "Darkness2", 20000, {{"Scrafty", 100, false}}},
+    {"Water Trainer", "Water1", 20000, {{"Feraligatr", 100, false}}},
+    {"Water Trainer", "Water4", 20000, {{"Poliwhirl", 100, false}}},
+    {"Darkness Trainer", "Darkness1", 20000, {{"Zarude", 100, false}}},
+    {"Lass", "Sally", 500, {{"Nidoran-F", 13, false}, {"Rattata", 11, false}}},
+    {"Youngster", "Calvin", 500, {{"Spearow", 14, false}}},
+    {"Youngster", "Ben", 500, {{"Ekans", 13, false}, {"Rattata", 11, false}}},
+    {"Lass", "Robin", 500, {{"Jigglypuff", 14, false}}},
+    {"Youngster", "Simon", 500, {{"Nidoran-M", 8, false}}},
+    {"Camper", "Junior", 500, {{"Diglett", 11, false}, {"Sandshrew", 11, false}}},
+    {"Bug Catcher", "Aiden", 500, {{"Kakuna", 10, false}, {"Metapod", 10, false}}},
+    {"Youngster", "Albert", 500, {{"Sandshrew", 8, false}}},
+    {"Camper", "Lukas", 750, {{"Nidorino", 17, false}, {"Sandshrew", 16, false}}},
+    {"Youngster", "Johnny", 750, {{"Mankey", 7, false}}},
+    {"Youngster", "Jason", 500, {{"Rattata", 7, false}}},
+    {"Youngster", "Danny", 500, {{"Pidgey", 4, false}, {"Pidgey", 4, false}}},
+    {"Hiker", "Johnson", 750, {{"Geodude", 18, false}, {"Machop", 18, false}}},
+    {"Lass", "Amy", 750, {{"Goldeen", 17, false}}},
+    {"Camper", "Zackary", 500, {{"Arbok", 22, false}, {"Sandslash", 22, false}}},
+    {"Camper", "Michael", 500, {{"Dugtrio", 26, false}, {"Seadra", 26, false}}},
+    {"Camper", "Louie", 500, {{"Drowzee", 26, false}}},
+    {"Youngster", "Rex", 750, {{"Magnemite", 20, false}, {"Voltorb", 20, false}}},
+    {"Bug Catcher", "Aiden", 500, {{"Kakuna", 10, false}, {"Metapod", 10, false}}},
+    {"Youngster", "Johnny", 750, {{"Mankey", 7, false}}},
+    {"Camper", "Lukas", 750, {{"Nidorino", 17, false}, {"Sandshrew", 16, false}}},
+    {"Camper", "Junior", 500, {{"Diglett", 11, false}, {"Sandshrew", 11, false}}},
+    {"Youngster", "Albert", 500, {{"Sandshrew", 8, false}}},
+    {"Youngster", "Danny", 500, {{"Pidgey", 4, false}, {"Pidgey", 4, false}}},
+    {"Youngster", "Jason", 500, {{"Rattata", 7, false}}},
+    {"Youngster", "Simon", 500, {{"Nidoran-M", 8, false}}},
+    {"Lass", "Robin", 500, {{"Jigglypuff", 14, false}}},
+    {"Youngster", "Ben", 500, {{"Ekans", 13, false}, {"Rattata", 11, false}}},
+    {"Lass", "Sally", 500, {{"Nidoran-F", 13, false}, {"Rattata", 11, false}}},
+    {"Youngster", "Calvin", 500, {{"Spearow", 14, false}}},
+    {"Rocket Grunt", "Yoshi", 750, {{"Kabutops", 84, false}, {"Omastar", 84, false}}},
+    {"Rocket Grunt", "Luigi", 750, {{"Muk", 82, false}, {"Muk", 82, false}, {"Muk", 82, false}}},
+    {"Lass", "Nancey", 700, {{"Pikachu", 16, false}, {"Rattata", 16, false}}},
+    {"Rocket Grunt", "Qwerty", 750, {{"Persian", 83, false}, {"Weezing", 83, false}}},
+    {
+        "Brock",
+        "1st Leader",
+        10000,
+        {
+            {"Lycanroc Midnight", 100, true},
+            {"Rampardos", 100, true},
+            {"Regirock", 100, true},
+            {"Rhyperior", 100, true},
+            {"Terrakion", 100, true},
+            {"Tyrantrum", 100, true}
+        }
+    },
+    {
+        "Misty",
+        "2nd Leader",
+        20000,
+        {
+            {"Gyarados", 100, true},
+            {"Kyogre-Primal", 100, true},
+            {"Seismitoad", 100, true},
+            {"Suicune", 100, true},
+            {"Swampert", 100, true},
+            {"Tapu Fini", 100, true}
+        }
+    },
+    {
+        "Surge",
+        "3rd Leader",
+        30000,
+        {
+            {"Ampharos", 100, true},
+            {"Manectric", 100, true},
+            {"Toxtricity", 100, true},
+            {"Vikavolt", 100, true},
+            {"Xurkitree", 100, true},
+            {"Zeraora", 100, true}
+        }
+    },
+    {
+        "Erika",
+        "4th Leader",
+        40000,
+        {
+            {"Kartana", 100, true},
+            {"Lurantis", 100, true},
+            {"Sceptile", 100, true},
+            {"Tangrowth", 100, true},
+            {"Virizion", 100, true},
+            {"Zarude", 100, true}
+        }
+    },
+    {
+        "Koga",
+        "5th Leader",
+        50000,
+        {
+            {"Dragalge", 100, true},
+            {"Gengar", 100, true},
+            {"Naganadel", 100, true},
+            {"Nihilego", 100, true},
+            {"Salazzle", 100, true},
+            {"Toxicroak", 100, true}
+        }
+    },
+    {
+        "Sabrina",
+        "6th Leader",
+        60000,
+        {
+            {"Deoxys-Normal", 100, true},
+            {"Gardevoir", 100, true},
+            {"Hatterene", 100, true},
+            {"Metagross", 100, true},
+            {"Mewtwo", 100, true},
+            {"Necrozma", 100, true}
+        }
+    },
+    {
+        "Blaine",
+        "7th Leader",
+        70000,
+        {
+            {"Marowak-Alola", 100, true},
+            {"Blacephalon", 100, true},
+            {"Cinderace", 100, true},
+            {"Heatran", 100, true},
+            {"Moltres", 100, true},
+            {"Groudon-Primal", 100, true}
+        }
+    },
+    {
+        "Pop",
+        "Pop",
+        20000,
+        {
+            {"Marshadow", 100, false},
+            {"Milotic", 100, false},
+            {"Rotom-Wash", 100, false},
+            {"Shaymin-Sky", 100, false},
+            {"Tyranitar", 100, false},
+            {"Volcarona", 100, true}
+        }
+    },
+    {
+        "Tah",
+        "Tah",
+        20000,
+        {
+            {"Beedrill", 100, false},
+            {"Mewtwo", 100, false},
+            {"Muk", 100, false},
+            {"Nidoking", 100, false},
+            {"Palossand", 100, false},
+            {"Venusaur", 100, false}
+        }
+    },
+    {
+        "Jay",
+        "Jay",
+        20000,
+        {
+            {"Arcanine", 100, false},
+            {"Drampa", 100, false},
+            {"Hydreigon", 100, false},
+            {"Mimikyu-Disguised", 100, false},
+            {"Rotom-Mow", 100, false},
+            {"Tyranitar", 100, false}
+        }
+    },
+    {
+        "Dumb Cat",
+        "Dumb Cat",
+        20000,
+        {
+            {"Espeon", 100, false},
+            {"Incineroar", 100, false},
+            {"Luxray", 100, true},
+            {"Mew", 100, false},
+            {"Persian", 100, false},
+            {"Torracat", 100, false}
+        }
+    },
+    {
+        "Leppy",
+        "Leppy",
+        20000,
+        {
+            {"Cinderace", 100, true},
+            {"Espeon", 100, false},
+            {"Hatterene", 100, true},
+            {"Lapras", 100, false},
+            {"Mimikyu-Disguised", 100, true},
+            {"Xerneas", 100, false}
+        }
+    },
+    {
+        "Space",
+        "Space",
+        20000,
+        {
+            {"Buizel", 100, false},
+            {"Eevee", 100, true},
+            {"Mareep", 100, true},
+            {"Milotic", 100, false},
+            {"Pikachu", 100, false},
+            {"Zygarde", 100, false}
+        }
+    },
+    {
+        "Priest",
+        "Igloo",
+        2000,
+        {
+            {"Absol", 89, false},
+            {"Decidueye", 84, false},
+            {"Dragapult", 85, false},
+            {"Runerigus", 82, false},
+            {"Umbreon", 86, false},
+            {"Yveltal", 94, false}
+        }
+    },
+    {
+        "Priest",
+        "Igloo",
+        2000,
+        {
+            {"Guzzlord", 91, false},
+            {"Hydreigon", 86, false},
+            {"Incineroar", 87, false},
+            {"Krookodile", 88, true},
+            {"Pangoro", 89, false},
+            {"Tyranitar", 90, true}
+        }
+    },
+    {
+        "Priest",
+        "Igloo",
+        2000,
+        {
+            {"Blacephalon", 93, false},
+            {"Chandelure", 91, false},
+            {"Dusknoir", 89, false},
+            {"Houndoom", 87, false},
+            {"Obstagoon", 85, false},
+            {"Scrafty", 86, false}
+        }
+    },
+    {"Youngster", "Zachary", 750, {{"Pidgey", 16, false}, {"Pidgey", 16, false}, {"Pidgey", 16, false}}},
+    {
+        "Priest",
+        "Igloo",
+        2000,
+        {
+            {"Marowak-Alola", 91, false},
+            {"Darkrai", 93, false},
+            {"Dhelmise", 88, false},
+            {"Palossand", 86, false},
+            {"Sableye", 90, false},
+            {"Spiritomb", 85, false}
+        }
+    },
+    {"Lass", "Isabelle", 700, {{"Butterfree", 20, false}}},
+    {"Youngster", "Jeff", 750, {{"Raticate", 16, false}, {"Rattata", 16, false}}},
+    {"Camper", "Michael", 500, {{"Dugtrio", 26, false}, {"Seadra", 26, false}}},
+    {"Camper", "Louie", 500, {{"Drowzee", 26, false}}},
+    {"Camper", "Zackary", 500, {{"Arbok", 22, false}, {"Sandslash", 22, false}}},
+    {"Fisherman", "Joe", 750, {{"Machop", 22, false}, {"Poliwag", 22, false}, {"Poliwhirl", 22, false}}},
+    {"Rocket Grunt", "Mario", 750, {{"Nidorina", 79, false}, {"Nidorino", 79, false}}},
+    {"Fisherman", "Oscar", 750, {{"Machop", 22, false}, {"Poliwag", 22, false}, {"Poliwhirl", 22, false}}},
+    {
+        "Fisherman",
+        "Kevin",
+        750,
+        {
+            {"Gyarados", 25, false},
+            {"Magikarp", 14, false},
+            {"Magikarp", 14, false},
+            {"Magikarp", 14, false},
+            {"Magikarp", 14, false},
+            {"Magikarp", 14, false}
+        }
+    },
+    {"Rocket Grunt", "Tyler", 750, {{"Raticate", 26, false}}},
+    {"Rocket Grunt", "Peach", 750, {{"Flygon", 81, false}, {"Vibrava", 79, false}}},
+    {
+        "Rocket Grunt",
+        "D'Marcus",
+        750,
+        {
+            {"Voltorb", 10, false},
+            {"Voltorb", 12, false},
+            {"Voltorb", 14, false},
+            {"Voltorb", 16, false},
+            {"Voltorb", 23, false}
+        }
+    },
+    {"Rocket Grunt", "Maximillion", 750, {{"Graveler", 25, false}, {"Nidorino", 25, false}}},
+    {"Gentleman", "David", 1200, {{"Arcanine", 30, false}}},
+    {"Youngster", "Rex", 750, {{"Magnemite", 20, false}, {"Voltorb", 20, false}}},
+    {"Lass", "Amy", 750, {{"Goldeen", 17, false}}},
+    {"Lass", "Zoey", 600, {{"Clefairy", 27, false}, {"Wigglytuff", 30, false}}},
+    {"Black Belt", "Ip Man", 2000, {{"Poliwrath", 55, false}, {"Primeape", 49, false}}},
+    {"Hiker", "Johnson", 750, {{"Geodude", 18, false}, {"Machop", 18, false}}},
+    {"Hiker", "Grylls", 1200, {{"Hitmonlee", 58, false}, {"Scyther", 56, false}}},
+    {"Scientist", "Beckard", 500, {{"Golbat", 41, false}, {"Zubat", 40, false}, {"Zubat", 39, false}}},
+    {"Gentleman", "Moneybanks", 6000, {{"Gengar", 53, false}, {"Hypno", 53, false}}},
+    {"Hiker", "Hayden", 1200, {{"Pinsir", 60, false}, {"Poliwrath", 62, false}}},
+    {"Hiker", "Pizza", 1200, {{"Hitmonchan", 56, false}, {"Primeape", 58, false}}},
+    {"Gentleman", "Artemis", 5000, {{"Arcanine", 55, false}, {"Persian", 49, false}, {"Rapidash", 53, false}}},
+    {"Hiker", "Tomas", 1200, {{"Hitmonchan", 53, false}, {"Hitmonlee", 53, false}, {"Machoke", 53, false}}},
+    {
+        "Fisherman",
+        "Karp",
+        750,
+        {{"Magikarp", 60, false}, {"Magikarp", 70, false}, {"Magikarp", 80, false}, {"Magikarp", 90, false}}
+    },
+    {"Scientist", "Lance Jr.", 500, {{"Dragonite", 35, false}, {"Venomoth", 42, false}}},
+    {"Black Belt", "Igloo", 2000, {{"Machamp", 52, false}, {"Machoke", 46, false}}},
+    {"Fisherman", "Robin", 750, {{"Poliwrath", 47, false}}},
+    {
+        "Fisherman",
+        "Wayne",
+        750,
+        {{"Cloyster", 42, false}, {"Gyarados", 42, false}, {"Seaking", 42, false}, {"Starmie", 42, false}}
+    },
+    {"Gentleman", "Gates", 2000, {{"Meowth", 45, false}, {"Persian", 47, false}}},
+    {"Scientist", "Harry", 500, {{"Pinsir", 40, false}, {"Scyther", 41, true}}},
+    {
+        "Hiker",
+        "Golem",
+        500,
+        {
+            {"Geodude", 5, false},
+            {"Geodude", 10, false},
+            {"Geodude", 15, false},
+            {"Geodude", 25, false},
+            {"Geodude", 20, false},
+            {"Graveler", 40, false}
+        }
+    },
+    {"Camper", "Elliot", 500, {{"Dratini", 40, false}, {"Exeggcute", 40, false}}},
+    {"Hiker", "Buffalo", 500, {{"Rhyhorn", 42, false}, {"Rhyhorn", 42, false}}},
+    {"Scientist", "Jake", 500, {{"Electrode", 42, false}, {"Ninetales", 44, false}}},
+    {"Scientist", "Fuji", 1200, {{"Mewtwo", 21, false}}},
+    {"Camper", "C.K.", 500, {{"Raichu", 45, false}, {"Vulpix", 44, false}}},
+    {"Camper", "Pietro", 500, {{"Poliwhirl", 40, false}, {"Raticate", 38, false}}},
+    {"Scientist", "Fredward", 500, {{"Kangaskhan", 47, false}}},
+    {"Scientist", "Bradley", 500, {{"Snorlax", 40, false}, {"Snorlax", 40, false}}},
+    {"Scientist", "Irwin", 500, {{"Electrode", 42, false}, {"Electrode", 42, false}, {"Electrode", 42, false}}},
+    {"Hiker", "Hank", 500, {{"Onix", 42, false}, {"Onix", 42, false}}},
+    {"Camper", "Kid", 500, {{"Arbok", 43, false}, {"Primeape", 43, false}}},
+    {"Scientist", "Beggar", 500, {{"Magneton", 45, false}}},
+    {
+        "Lass",
+        "Iggy",
+        700,
+        {{"Chansey", 42, false}, {"Clefable", 42, false}, {"Snorlax", 42, false}, {"Wigglytuff", 42, false}}
+    },
+    {"Camper", "Quincy", 750, {{"Ivysaur", 40, false}, {"Pidgeotto", 38, false}}},
+    {
+        "Camper",
+        "Ken",
+        750,
+        {{"Eevee", 35, false}, {"Flareon", 42, false}, {"Jolteon", 42, false}, {"Vaporeon", 42, false}}
+    },
+    {"Hiker", "Big Joey", 500, {{"Kangaskhan", 44, false}}},
+    {"Hiker", "Howard", 500, {{"Kabutops", 43, false}, {"Omastar", 43, false}}},
+    {"Lass", "Zoey", 600, {{"Clefairy", 27, false}, {"Wigglytuff", 30, false}}},
+    {"Scientist", "Ed", 2000, {{"Hypno", 40, false}}},
+    {"Gentleman", "Richard", 1500, {{"Growlithe", 35, false}, {"Raticate", 33, false}}},
+    {"Scientist", "Bill", 1000, {{"Electrode", 33, false}, {"Raichu", 35, false}}},
+    {"Hiker", "Thor", 700, {{"Graveler", 30, false}, {"Machoke", 31, false}, {"Sandslash", 32, false}}},
+    {
+        "Scientist",
+        "Genesis",
+        1500,
+        {{"Electrode", 64, false}, {"Magnezone", 60, false}, {"Metang", 62, false}, {"Lucario", 65, false}}
+    },
+    {"Hiker", "Tyson", 1200, {{"Machamp", 60, false}, {"Primeape", 58, false}}},
+    {"Bug Catcher", "Aiden", 500, {{"Beedrill", 33, false}, {"Butterfree", 33, false}, {"Parasect", 35, false}}},
+    {"Lass", "Charlotte", 500, {{"Hatterene", 56, false}}},
+    {"Lass", "Nancy", 500, {{"Gallade", 56, false}, {"Gardevoir", 56, false}}},
+    {"Lass", "Nancy", 500, {{"Gallade", 56, false}, {"Gardevoir", 56, false}}},
+    {"Lass", "Jessica", 500, {{"Reuniclus", 55, false}, {"Espeon", 55, false}, {"Hypno", 55, false}}},
+    {"Scientist", "Joshua", 500, {{"Grimer", 35, false}, {"Electrode", 35, false}}},
+    {"Rocket Grunt", "David", 2000, {{"Raticate", 25, false}, {"Golbat", 25, false}, {"Zubat", 25, false}}},
+    {"Rocket Grunt", "Andrei", 2000, {{"Machop", 26, false}, {"Drowzee", 29, false}}},
+    {"Rocket Grunt", "Corny", 2000, {{"Machoke", 29, false}}},
+    {"Rocket Grunt", "Tyrone", 2000, {{"Raticate", 36, false}, {"Weezing", 35, false}}},
+    {"Rocket Grunt", "Robin", 2000, {{"Arbok", 33, false}}},
+    {"Rocket Grunt", "Bob", 2000, {{"Sandslash", 33, false}}},
+    {"Rocket Grunt", "Rares", 2000, {{"Hypno", 28, false}, {"Raticate", 28, false}}},
+    {"Rocket Grunt", "Jose", 2000, {{"Ekans", 28, false}}},
+    {"Rocket Grunt", "Trent", 2000, {{"Zubat", 29, false}, {"Cubone", 29, false}}},
+    {"Rocket Grunt", "Nigel", 2000, {{"Arbok", 35, false}, {"Golbat", 35, false}}},
+    {"Rocket Grunt", "Tony", 2000, {{"Raticate", 36, false}, {"Weezing", 35, false}}},
+    {"Rocket Grunt", "Cody", 2000, {{"Sandslash", 33, false}}},
+    {"Rocket Grunt", "Rodney", 2000, {{"Arbok", 33, false}}},
+    {"Scientist", "Mickey", 2000, {{"Grimer", 26, false}, {"Weezing", 26, false}, {"Koffing", 26, false}}},
+    {"Scientist", "Gigi", 2000, {{"Voltorb", 28, false}, {"Magnemite", 28, false}}},
+    {"Scientist", "Calin", 2000, {{"Weezing", 29, false}}},
+    {"Scientist", "Dumb Cat", 2000, {{"Electrode", 33, false}}},
+    {"Scientist", "Taylor", 2000, {{"Muk", 33, false}}},
+    {"Scientist", "Beau", 2000, {{"Mr. Mime", 30, false}, {"Kadabra", 30, false}}},
+    {"Scientist", "Jay", 2000, {{"Magneton", 35, false}}}
+}
