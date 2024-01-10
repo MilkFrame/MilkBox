fc3bbaf6679ff2da2a22bbcb4985878bb16ddd56
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-11 00:58:03 +1100

    Create Build.lua

---
+++ GameData/Retail Tycoon [284076212]/Build.lua
@@ -0,0 +1,95 @@
+data = {
+    ["Flooring"] = {
+        ["Wood"] = {["Price"] = 300, ["GamepadSelect"] = {}, ["Desc"] = "Hard wood flooring for upper class stores"},
+        ["Marble"] = {
+            ["Price"] = 500,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "Extra expensive marble used in only the fanciest of stores"
+        },
+        ["Concrete"] = {
+            ["Price"] = 100,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A slab of concrete as cheap as it is boring"
+        },
+        ["Carpet"] = {["Price"] = 200, ["GamepadSelect"] = {}, ["Desc"] = "A good middle ground between cheap and nice"},
+        ["Stone"] = {
+            ["Price"] = 200,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "Works great for outdoor paths if you're into that"
+        }
+    },
+    ["Ceilings"] = {
+        ["LargeSign"] = {
+            ["Price"] = 300,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A large sign with several fields and an aisle number"
+        },
+        ["SmallSign"] = {
+            ["Price"] = 250,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A short sign with one message that can fit on short ceilings"
+        },
+        ["Options"] = {["20"] = {["GamepadPointer"] = {}}, ["12"] = {["GamepadPointer"] = {}}},
+        ["LargeLight"] = {
+            ["Price"] = 350,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A large light, more suited to large stores with high ceilings. Having one on each tile isn't necessary."
+        },
+        ["Speaker"] = {
+            ["Price"] = 350,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A ceiling speaker for playing music in your store"
+        },
+        ["Default"] = {
+            ["Price"] = 200,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A bare ceiling with nothing interesting on it"
+        },
+        ["SmallLight"] = {
+            ["Price"] = 250,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A small light, great for small stores. Recommended to have one on each ceiling tile."
+        },
+        ["Skylight"] = {
+            ["Price"] = 300,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A window in the ceiling for letting light in"
+        }
+    },
+    ["Walls"] = {
+        ["HalfGlass"] = {
+            ["Price"] = 150,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "The top half of a tall glass wall, good for a transition between ceiling heights"
+        },
+        ["Options"] = {
+            ["14"] = {["GamepadPointer"] = {}},
+            ["12"] = {["GamepadPointer"] = {}},
+            ["22"] = {["GamepadPointer"] = {}},
+            ["20"] = {["GamepadPointer"] = {}}
+        },
+        ["DoubleDoor"] = {["Price"] = 300, ["GamepadSelect"] = {}, ["Desc"] = "A set of glass double doors"},
+        ["SingleDoor"] = {
+            ["Price"] = 250,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A bland wooden door, good for employee-only areas"
+        },
+        ["Window"] = {["Price"] = 250, ["GamepadSelect"] = {}, ["Desc"] = "A wall that you can see through!"},
+        ["GlassWall"] = {
+            ["Price"] = 300,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A classy full glass wall, goes great with skylights"
+        },
+        ["GlassDoor"] = {
+            ["Price"] = 350,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "A set of glass double doors to match a full glass wall"
+        },
+        ["Wall"] = {["Price"] = 200, ["GamepadSelect"] = {}, ["Desc"] = "There's nothing special about this wall"},
+        ["HalfWall"] = {
+            ["Price"] = 100,
+            ["GamepadSelect"] = {},
+            ["Desc"] = "The top half of a tall wall, good for a transition between ceiling heights"
+        }
+    }
+}
