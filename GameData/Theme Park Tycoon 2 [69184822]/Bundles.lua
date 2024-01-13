fc60f2c0231a76b707d1dd0f0a9045fe8a8ca3e4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-13 15:32:23 +1100

    Create Bundles.lua

---
+++ GameData/Theme Park Tycoon 2 [69184822]/Bundles.lua
@@ -0,0 +1,32 @@
+data = {
+    ["fromId"] = {
+        {["id"] = 1, ["name"] = "JuniorCaterpillar", ["cost"] = 2000},
+        {["id"] = 2, ["name"] = "JuniorDragon", ["cost"] = 3000},
+        {["id"] = 3, ["name"] = "JuniorPlane", ["cost"] = 3000},
+        {["id"] = 4, ["name"] = "CarouselEaster", ["cost"] = 2500},
+        {["id"] = 5, ["name"] = "JumperOctopus", ["cost"] = 3000},
+        {["id"] = 6, ["name"] = "MotorcycleSidecar", ["cost"] = 2250},
+        {["id"] = 7, ["name"] = "TumbleTrainCaterpillar", ["cost"] = 3500},
+        {["id"] = 8, ["name"] = "TumbleTrainCanopy", ["cost"] = 2000},
+        {["id"] = 9, ["name"] = "CarRoof", ["cost"] = 1500},
+        {["id"] = 10, ["name"] = "CarVintageSports", ["cost"] = 2500},
+        {["id"] = 11, ["name"] = "CarPolice", ["cost"] = 3000},
+        {["id"] = 12, ["name"] = "JuniorSnake", ["cost"] = 2500},
+        {["id"] = 13, ["name"] = "JuniorShark", ["cost"] = 2500}
+    },
+    ["fromName"] = {
+        ["CarouselEaster"] = {["id"] = 4, ["name"] = "CarouselEaster", ["cost"] = 2500},
+        ["JumperOctopus"] = {["id"] = 5, ["name"] = "JumperOctopus", ["cost"] = 3000},
+        ["CarPolice"] = {["id"] = 11, ["name"] = "CarPolice", ["cost"] = 3000},
+        ["JuniorCaterpillar"] = {["id"] = 1, ["name"] = "JuniorCaterpillar", ["cost"] = 2000},
+        ["JuniorShark"] = {["id"] = 13, ["name"] = "JuniorShark", ["cost"] = 2500},
+        ["JuniorSnake"] = {["id"] = 12, ["name"] = "JuniorSnake", ["cost"] = 2500},
+        ["JuniorDragon"] = {["id"] = 2, ["name"] = "JuniorDragon", ["cost"] = 3000},
+        ["CarVintageSports"] = {["id"] = 10, ["name"] = "CarVintageSports", ["cost"] = 2500},
+        ["CarRoof"] = {["id"] = 9, ["name"] = "CarRoof", ["cost"] = 1500},
+        ["TumbleTrainCanopy"] = {["id"] = 8, ["name"] = "TumbleTrainCanopy", ["cost"] = 2000},
+        ["MotorcycleSidecar"] = {["id"] = 6, ["name"] = "MotorcycleSidecar", ["cost"] = 2250},
+        ["TumbleTrainCaterpillar"] = {["id"] = 7, ["name"] = "TumbleTrainCaterpillar", ["cost"] = 3500},
+        ["JuniorPlane"] = {["id"] = 3, ["name"] = "JuniorPlane", ["cost"] = 3000}
+    }
+}
