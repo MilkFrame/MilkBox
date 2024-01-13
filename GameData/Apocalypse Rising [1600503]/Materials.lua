cedb000a5fcfd743afa55875b61d541130a7498b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-13 15:29:08 +1100

    Create Materials.lua

---
+++ GameData/Apocalypse Rising [1600503]/Materials.lua
@@ -0,0 +1,84 @@
+data = {
+    ["Fireplace"] = {["Head"] = {["Lit"] = 0}},
+    ["BloodBag"] = {
+        ["ObjectID"] = "aPPaSBaBAA",
+        ["Bound"] = -0.1600000113248825,
+        ["LootCF"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+        ["Despawn"] = 300
+    },
+    ["LargeCrateOpen"] = {
+        ["Head"] = {
+            ["storage"] = {
+                ["slot10"] = 0,
+                ["slot40"] = 0,
+                ["slot39"] = 0,
+                ["slot7"] = 0,
+                ["slot12"] = 0,
+                ["slot8"] = 0,
+                ["slot11"] = 0,
+                ["slot21"] = 0,
+                ["slot30"] = 0,
+                ["slot29"] = 0,
+                ["slot19"] = 0,
+                ["slot4"] = 0,
+                ["slot9"] = 0,
+                ["slot20"] = 0,
+                ["slot27"] = 0,
+                ["slot31"] = 0,
+                ["slot35"] = 0,
+                ["slot3"] = 0,
+                ["slot18"] = 0,
+                ["slot28"] = 0,
+                ["slot2"] = 0,
+                ["slot5"] = 0,
+                ["slot36"] = 0,
+                ["slot32"] = 0,
+                ["slot23"] = 0,
+                ["slot33"] = 0,
+                ["slot14"] = 0,
+                ["slot17"] = 0,
+                ["slot6"] = 0,
+                ["slot13"] = 0,
+                ["slot15"] = 0,
+                ["slot22"] = 0,
+                ["slot26"] = 0,
+                ["slot34"] = 0,
+                ["slot37"] = 0,
+                ["slot1"] = 0,
+                ["slot38"] = 0,
+                ["slot25"] = 0,
+                ["slot24"] = 0,
+                ["slot16"] = 0
+            }
+        }
+    },
+    ["VS50Placed"] = {["Arming"] = 12, ["Owner"] = ""},
+    ["TM46Placed"] = {["Arming"] = 12, ["Owner"] = ""},
+    ["C4Placed"] = {["Arming"] = 12, ["Owner"] = ""},
+    ["SmallCrateOpen"] = {
+        ["Head"] = {
+            ["storage"] = {
+                ["slot10"] = 0,
+                ["slot5"] = 0,
+                ["slot7"] = 0,
+                ["slot12"] = 0,
+                ["slot8"] = 0,
+                ["slot3"] = 0,
+                ["slot11"] = 0,
+                ["slot6"] = 0,
+                ["slot14"] = 0,
+                ["slot17"] = 0,
+                ["slot4"] = 0,
+                ["slot9"] = 0,
+                ["slot13"] = 0,
+                ["slot2"] = 0,
+                ["slot19"] = 0,
+                ["slot1"] = 0,
+                ["slot15"] = 0,
+                ["slot20"] = 0,
+                ["slot18"] = 0,
+                ["slot16"] = 0
+            }
+        }
+    }
+}
