b536c45d2b864f3c8d441382366e15488d125c39
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-13 15:33:08 +1100

    Create CarryList.lua

---
+++ GameData/Theme Park Tycoon 2 [69184822]/CarryList.lua
@@ -0,0 +1,234 @@
+data = {
+    ["fromId"] = {
+        {["name"] = "Fries", ["onEmpty"] = "FriesEmpty", ["id"] = 1, ["isConsuming"] = true, ["model"] = "Fries"},
+        {["id"] = 2, ["isTrash"] = true, ["name"] = "FriesEmpty", ["model"] = "FriesEmpty"},
+        {["name"] = "Popcorn", ["onEmpty"] = "PopcornEmpty", ["id"] = 3, ["isConsuming"] = true, ["model"] = "Popcorn"},
+        {["id"] = 4, ["isTrash"] = true, ["name"] = "PopcornEmpty", ["model"] = "PopcornEmpty"},
+        {
+            ["name"] = "HotDrink",
+            ["onEmpty"] = "HotDrinkEmpty",
+            ["id"] = 5,
+            ["isConsuming"] = true,
+            ["model"] = "HotDrink"
+        },
+        {["id"] = 6, ["isTrash"] = true, ["name"] = "HotDrinkEmpty", ["model"] = "HotDrink"},
+        {
+            ["name"] = "SoftDrink",
+            ["onEmpty"] = "SoftDrinkEmpty",
+            ["id"] = 7,
+            ["isConsuming"] = true,
+            ["model"] = "SoftDrink"
+        },
+        {["id"] = 8, ["isTrash"] = true, ["name"] = "SoftDrinkEmpty", ["model"] = "SoftDrink"},
+        {
+            ["carryAnimation"] = "function: 0x00000000c5deba9a",
+            ["name"] = "Balloon1",
+            ["model"] = "Balloon1",
+            ["id"] = 9,
+            ["canRelease"] = true,
+            ["isHolding"] = true
+        },
+        {
+            ["carryAnimation"] = "function: 0x00000000c5deba9a",
+            ["name"] = "Balloon2",
+            ["model"] = "Balloon2",
+            ["id"] = 10,
+            ["canRelease"] = true,
+            ["isHolding"] = true
+        },
+        {
+            ["carryAnimation"] = "function: 0x00000000c5deba9a",
+            ["name"] = "Balloon3",
+            ["model"] = "Balloon3",
+            ["id"] = 11,
+            ["canRelease"] = true,
+            ["isHolding"] = true
+        },
+        {
+            ["carryAnimation"] = "function: 0x00000000c5deba9a",
+            ["name"] = "Balloon4",
+            ["model"] = "Balloon4",
+            ["id"] = 12,
+            ["canRelease"] = true,
+            ["isHolding"] = true
+        },
+        {["name"] = "Pizza1", ["onEmpty"] = "Pizza2", ["id"] = 13, ["isConsuming"] = true, ["model"] = "Pizza1"},
+        {["id"] = 14, ["isTrash"] = true, ["name"] = "Pizza2", ["model"] = "Pizza2"},
+        {
+            ["name"] = "IceCream1",
+            ["onEmpty"] = "IceCream3",
+            ["id"] = 15,
+            ["isConsuming"] = true,
+            ["model"] = "IceCream1"
+        },
+        {
+            ["name"] = "IceCream2",
+            ["onEmpty"] = "IceCream3",
+            ["id"] = 16,
+            ["isConsuming"] = true,
+            ["model"] = "IceCream2"
+        },
+        {["id"] = 17, ["isConsuming"] = true, ["name"] = "IceCream3", ["model"] = "IceCream3"},
+        {
+            ["name"] = "IceCream4",
+            ["onEmpty"] = "IceCream5",
+            ["id"] = 18,
+            ["isConsuming"] = true,
+            ["model"] = "IceCream4"
+        },
+        {["id"] = 19, ["isTrash"] = true, ["name"] = "IceCream5", ["model"] = "IceCream5"},
+        {["name"] = "Slushy1", ["onEmpty"] = "Slushy2", ["id"] = 20, ["isConsuming"] = true, ["model"] = "Slushy1"},
+        {["id"] = 21, ["isTrash"] = true, ["name"] = "Slushy2", ["model"] = "Slushy2"},
+        {
+            ["name"] = "CandyFloss1",
+            ["onEmpty"] = "CandyFloss2",
+            ["id"] = 22,
+            ["isConsuming"] = true,
+            ["model"] = "CandyFloss1"
+        },
+        {["id"] = 23, ["isTrash"] = true, ["name"] = "CandyFloss2", ["model"] = "CandyFloss2"},
+        {["name"] = "Burger1", ["onEmpty"] = "Burger2", ["id"] = 24, ["isConsuming"] = true, ["model"] = "Burger1"},
+        {["id"] = 25, ["isTrash"] = true, ["name"] = "Burger2", ["model"] = "Burger2"},
+        {["name"] = "Fruit1", ["onEmpty"] = "Fruit2", ["id"] = 26, ["isConsuming"] = true, ["model"] = "Fruit1"},
+        {["id"] = 27, ["isTrash"] = true, ["name"] = "Fruit2", ["model"] = "Fruit2"},
+        {["name"] = "Fruit3", ["onEmpty"] = "Fruit4", ["id"] = 28, ["isConsuming"] = true, ["model"] = "Fruit3"},
+        {["id"] = 29, ["isTrash"] = true, ["name"] = "Fruit4", ["model"] = "Fruit4"}
+    },
+    ["fromName"] = {
+        ["CandyFloss1"] = {
+            ["name"] = "CandyFloss1",
+            ["onEmpty"] = "CandyFloss2",
+            ["id"] = 22,
+            ["isConsuming"] = true,
+            ["model"] = "CandyFloss1"
+        },
+        ["Balloon4"] = {
+            ["carryAnimation"] = "function: 0x00000000c5deba9a",
+            ["name"] = "Balloon4",
+            ["model"] = "Balloon4",
+            ["id"] = 12,
+            ["canRelease"] = true,
+            ["isHolding"] = true
+        },
+        ["SoftDrinkEmpty"] = {["id"] = 8, ["isTrash"] = true, ["name"] = "SoftDrinkEmpty", ["model"] = "SoftDrink"},
+        ["Fruit1"] = {
+            ["name"] = "Fruit1",
+            ["onEmpty"] = "Fruit2",
+            ["id"] = 26,
+            ["isConsuming"] = true,
+            ["model"] = "Fruit1"
+        },
+        ["Balloon3"] = {
+            ["carryAnimation"] = "function: 0x00000000c5deba9a",
+            ["name"] = "Balloon3",
+            ["model"] = "Balloon3",
+            ["id"] = 11,
+            ["canRelease"] = true,
+            ["isHolding"] = true
+        },
+        ["Fries"] = {
+            ["name"] = "Fries",
+            ["onEmpty"] = "FriesEmpty",
+            ["id"] = 1,
+            ["isConsuming"] = true,
+            ["model"] = "Fries"
+        },
+        ["Popcorn"] = {
+            ["name"] = "Popcorn",
+            ["onEmpty"] = "PopcornEmpty",
+            ["id"] = 3,
+            ["isConsuming"] = true,
+            ["model"] = "Popcorn"
+        },
+        ["IceCream1"] = {
+            ["name"] = "IceCream1",
+            ["onEmpty"] = "IceCream3",
+            ["id"] = 15,
+            ["isConsuming"] = true,
+            ["model"] = "IceCream1"
+        },
+        ["Balloon2"] = {
+            ["carryAnimation"] = "function: 0x00000000c5deba9a",
+            ["name"] = "Balloon2",
+            ["model"] = "Balloon2",
+            ["id"] = 10,
+            ["canRelease"] = true,
+            ["isHolding"] = true
+        },
+        ["IceCream5"] = {["id"] = 19, ["isTrash"] = true, ["name"] = "IceCream5", ["model"] = "IceCream5"},
+        ["FriesEmpty"] = {["id"] = 2, ["isTrash"] = true, ["name"] = "FriesEmpty", ["model"] = "FriesEmpty"},
+        ["Fruit4"] = {["id"] = 29, ["isTrash"] = true, ["name"] = "Fruit4", ["model"] = "Fruit4"},
+        ["Fruit3"] = {
+            ["name"] = "Fruit3",
+            ["onEmpty"] = "Fruit4",
+            ["id"] = 28,
+            ["isConsuming"] = true,
+            ["model"] = "Fruit3"
+        },
+        ["IceCream3"] = {["id"] = 17, ["isConsuming"] = true, ["name"] = "IceCream3", ["model"] = "IceCream3"},
+        ["SoftDrink"] = {
+            ["name"] = "SoftDrink",
+            ["onEmpty"] = "SoftDrinkEmpty",
+            ["id"] = 7,
+            ["isConsuming"] = true,
+            ["model"] = "SoftDrink"
+        },
+        ["Balloon1"] = {
+            ["carryAnimation"] = "function: 0x00000000c5deba9a",
+            ["name"] = "Balloon1",
+            ["model"] = "Balloon1",
+            ["id"] = 9,
+            ["canRelease"] = true,
+            ["isHolding"] = true
+        },
+        ["Pizza2"] = {["id"] = 14, ["isTrash"] = true, ["name"] = "Pizza2", ["model"] = "Pizza2"},
+        ["Burger2"] = {["id"] = 25, ["isTrash"] = true, ["name"] = "Burger2", ["model"] = "Burger2"},
+        ["Burger1"] = {
+            ["name"] = "Burger1",
+            ["onEmpty"] = "Burger2",
+            ["id"] = 24,
+            ["isConsuming"] = true,
+            ["model"] = "Burger1"
+        },
+        ["CandyFloss2"] = {["id"] = 23, ["isTrash"] = true, ["name"] = "CandyFloss2", ["model"] = "CandyFloss2"},
+        ["Slushy1"] = {
+            ["name"] = "Slushy1",
+            ["onEmpty"] = "Slushy2",
+            ["id"] = 20,
+            ["isConsuming"] = true,
+            ["model"] = "Slushy1"
+        },
+        ["HotDrinkEmpty"] = {["id"] = 6, ["isTrash"] = true, ["name"] = "HotDrinkEmpty", ["model"] = "HotDrink"},
+        ["IceCream4"] = {
+            ["name"] = "IceCream4",
+            ["onEmpty"] = "IceCream5",
+            ["id"] = 18,
+            ["isConsuming"] = true,
+            ["model"] = "IceCream4"
+        },
+        ["PopcornEmpty"] = {["id"] = 4, ["isTrash"] = true, ["name"] = "PopcornEmpty", ["model"] = "PopcornEmpty"},
+        ["HotDrink"] = {
+            ["name"] = "HotDrink",
+            ["onEmpty"] = "HotDrinkEmpty",
+            ["id"] = 5,
+            ["isConsuming"] = true,
+            ["model"] = "HotDrink"
+        },
+        ["Slushy2"] = {["id"] = 21, ["isTrash"] = true, ["name"] = "Slushy2", ["model"] = "Slushy2"},
+        ["IceCream2"] = {
+            ["name"] = "IceCream2",
+            ["onEmpty"] = "IceCream3",
+            ["id"] = 16,
+            ["isConsuming"] = true,
+            ["model"] = "IceCream2"
+        },
+        ["Fruit2"] = {["id"] = 27, ["isTrash"] = true, ["name"] = "Fruit2", ["model"] = "Fruit2"},
+        ["Pizza1"] = {
+            ["name"] = "Pizza1",
+            ["onEmpty"] = "Pizza2",
+            ["id"] = 13,
+            ["isConsuming"] = true,
+            ["model"] = "Pizza1"
+        }
+    }
+}
