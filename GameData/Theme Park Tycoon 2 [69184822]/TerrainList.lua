fec925f087784bcb525f8514634be11c7b91a367
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-13 16:05:53 +1100

    Create TerrainList.lua

---
+++ GameData/Theme Park Tycoon 2 [69184822]/TerrainList.lua
@@ -0,0 +1,309 @@
+data = {
+    ["terrain"] = {
+        {
+            ["brickcolor"] = BrickColor.new("Bright green"),
+            ["transparency"] = 0,
+            ["name"] = "Grass",
+            ["tkey"] = "ui_material_grass",
+            ["id"] = 1,
+            ["material"] = Enum.Material.Grass,
+            ["imageIdx"] = 0,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Nougat"),
+            ["transparency"] = 0,
+            ["name"] = "Sand",
+            ["tkey"] = "ui_material_sand",
+            ["id"] = 2,
+            ["material"] = Enum.Material.Sand,
+            ["imageIdx"] = 2,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Dark stone grey"),
+            ["transparency"] = 0,
+            ["name"] = "Rock",
+            ["tkey"] = "ui_material_rock",
+            ["id"] = 3,
+            ["material"] = Enum.Material.Slate,
+            ["imageIdx"] = 1,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Brown"),
+            ["transparency"] = 0,
+            ["name"] = "Brown rock",
+            ["tkey"] = "ui_terrain_brownrock",
+            ["id"] = 4,
+            ["material"] = Enum.Material.Slate,
+            ["imageIdx"] = 1,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Institutional white"),
+            ["transparency"] = 0,
+            ["name"] = "Snow",
+            ["tkey"] = "ui_terrain_snow",
+            ["id"] = 5,
+            ["material"] = Enum.Material.Sand,
+            ["imageIdx"] = 2,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Pearl"),
+            ["transparency"] = 0,
+            ["name"] = "Ice",
+            ["tkey"] = "ui_material_ice",
+            ["id"] = 6,
+            ["material"] = Enum.Material.Ice,
+            ["imageIdx"] = 6,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Mid gray"),
+            ["transparency"] = 0,
+            ["name"] = "Concrete",
+            ["tkey"] = "ui_material_concrete",
+            ["id"] = 7,
+            ["material"] = Enum.Material.Concrete,
+            ["imageIdx"] = 3,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Cashmere"),
+            ["transparency"] = 0,
+            ["name"] = "Granite",
+            ["tkey"] = "ui_material_granite",
+            ["id"] = 8,
+            ["material"] = Enum.Material.Granite,
+            ["imageIdx"] = 10,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Dark stone grey"),
+            ["transparency"] = 0,
+            ["name"] = "Black granite",
+            ["tkey"] = "ui_terrain_blackgranite",
+            ["id"] = 9,
+            ["material"] = Enum.Material.Granite,
+            ["imageIdx"] = 10,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Cork"),
+            ["transparency"] = 0,
+            ["name"] = "Dry grass",
+            ["tkey"] = "ui_terrain_drygrass",
+            ["id"] = 10,
+            ["material"] = Enum.Material.Grass,
+            ["imageIdx"] = 0,
+            ["reflectance"] = 0
+        }
+    },
+    ["fromName"] = {
+        ["Grass"] = {
+            ["brickcolor"] = BrickColor.new("Bright green"),
+            ["transparency"] = 0,
+            ["name"] = "Grass",
+            ["tkey"] = "ui_material_grass",
+            ["id"] = 1,
+            ["material"] = Enum.Material.Grass,
+            ["imageIdx"] = 0,
+            ["reflectance"] = 0
+        },
+        ["Rock"] = {
+            ["brickcolor"] = BrickColor.new("Dark stone grey"),
+            ["transparency"] = 0,
+            ["name"] = "Rock",
+            ["tkey"] = "ui_material_rock",
+            ["id"] = 3,
+            ["material"] = Enum.Material.Slate,
+            ["imageIdx"] = 1,
+            ["reflectance"] = 0
+        },
+        ["Sand"] = {
+            ["brickcolor"] = BrickColor.new("Nougat"),
+            ["transparency"] = 0,
+            ["name"] = "Sand",
+            ["tkey"] = "ui_material_sand",
+            ["id"] = 2,
+            ["material"] = Enum.Material.Sand,
+            ["imageIdx"] = 2,
+            ["reflectance"] = 0
+        },
+        ["Dry grass"] = {
+            ["brickcolor"] = BrickColor.new("Cork"),
+            ["transparency"] = 0,
+            ["name"] = "Dry grass",
+            ["tkey"] = "ui_terrain_drygrass",
+            ["id"] = 10,
+            ["material"] = Enum.Material.Grass,
+            ["imageIdx"] = 0,
+            ["reflectance"] = 0
+        },
+        ["Black granite"] = {
+            ["brickcolor"] = BrickColor.new("Dark stone grey"),
+            ["transparency"] = 0,
+            ["name"] = "Black granite",
+            ["tkey"] = "ui_terrain_blackgranite",
+            ["id"] = 9,
+            ["material"] = Enum.Material.Granite,
+            ["imageIdx"] = 10,
+            ["reflectance"] = 0
+        },
+        ["Granite"] = {
+            ["brickcolor"] = BrickColor.new("Cashmere"),
+            ["transparency"] = 0,
+            ["name"] = "Granite",
+            ["tkey"] = "ui_material_granite",
+            ["id"] = 8,
+            ["material"] = Enum.Material.Granite,
+            ["imageIdx"] = 10,
+            ["reflectance"] = 0
+        },
+        ["Concrete"] = {
+            ["brickcolor"] = BrickColor.new("Mid gray"),
+            ["transparency"] = 0,
+            ["name"] = "Concrete",
+            ["tkey"] = "ui_material_concrete",
+            ["id"] = 7,
+            ["material"] = Enum.Material.Concrete,
+            ["imageIdx"] = 3,
+            ["reflectance"] = 0
+        },
+        ["Ice"] = {
+            ["brickcolor"] = BrickColor.new("Pearl"),
+            ["transparency"] = 0,
+            ["name"] = "Ice",
+            ["tkey"] = "ui_material_ice",
+            ["id"] = 6,
+            ["material"] = Enum.Material.Ice,
+            ["imageIdx"] = 6,
+            ["reflectance"] = 0
+        },
+        ["Brown rock"] = {
+            ["brickcolor"] = BrickColor.new("Brown"),
+            ["transparency"] = 0,
+            ["name"] = "Brown rock",
+            ["tkey"] = "ui_terrain_brownrock",
+            ["id"] = 4,
+            ["material"] = Enum.Material.Slate,
+            ["imageIdx"] = 1,
+            ["reflectance"] = 0
+        },
+        ["Snow"] = {
+            ["brickcolor"] = BrickColor.new("Institutional white"),
+            ["transparency"] = 0,
+            ["name"] = "Snow",
+            ["tkey"] = "ui_terrain_snow",
+            ["id"] = 5,
+            ["material"] = Enum.Material.Sand,
+            ["imageIdx"] = 2,
+            ["reflectance"] = 0
+        }
+    },
+    ["paintPart"] = "function: 0x00000000d6c80da2",
+    ["fromId"] = {
+        {
+            ["brickcolor"] = BrickColor.new("Bright green"),
+            ["transparency"] = 0,
+            ["name"] = "Grass",
+            ["tkey"] = "ui_material_grass",
+            ["id"] = 1,
+            ["material"] = Enum.Material.Grass,
+            ["imageIdx"] = 0,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Nougat"),
+            ["transparency"] = 0,
+            ["name"] = "Sand",
+            ["tkey"] = "ui_material_sand",
+            ["id"] = 2,
+            ["material"] = Enum.Material.Sand,
+            ["imageIdx"] = 2,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Dark stone grey"),
+            ["transparency"] = 0,
+            ["name"] = "Rock",
+            ["tkey"] = "ui_material_rock",
+            ["id"] = 3,
+            ["material"] = Enum.Material.Slate,
+            ["imageIdx"] = 1,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Brown"),
+            ["transparency"] = 0,
+            ["name"] = "Brown rock",
+            ["tkey"] = "ui_terrain_brownrock",
+            ["id"] = 4,
+            ["material"] = Enum.Material.Slate,
+            ["imageIdx"] = 1,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Institutional white"),
+            ["transparency"] = 0,
+            ["name"] = "Snow",
+            ["tkey"] = "ui_terrain_snow",
+            ["id"] = 5,
+            ["material"] = Enum.Material.Sand,
+            ["imageIdx"] = 2,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Pearl"),
+            ["transparency"] = 0,
+            ["name"] = "Ice",
+            ["tkey"] = "ui_material_ice",
+            ["id"] = 6,
+            ["material"] = Enum.Material.Ice,
+            ["imageIdx"] = 6,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Mid gray"),
+            ["transparency"] = 0,
+            ["name"] = "Concrete",
+            ["tkey"] = "ui_material_concrete",
+            ["id"] = 7,
+            ["material"] = Enum.Material.Concrete,
+            ["imageIdx"] = 3,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Cashmere"),
+            ["transparency"] = 0,
+            ["name"] = "Granite",
+            ["tkey"] = "ui_material_granite",
+            ["id"] = 8,
+            ["material"] = Enum.Material.Granite,
+            ["imageIdx"] = 10,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Dark stone grey"),
+            ["transparency"] = 0,
+            ["name"] = "Black granite",
+            ["tkey"] = "ui_terrain_blackgranite",
+            ["id"] = 9,
+            ["material"] = Enum.Material.Granite,
+            ["imageIdx"] = 10,
+            ["reflectance"] = 0
+        },
+        {
+            ["brickcolor"] = BrickColor.new("Cork"),
+            ["transparency"] = 0,
+            ["name"] = "Dry grass",
+            ["tkey"] = "ui_terrain_drygrass",
+            ["id"] = 10,
+            ["material"] = Enum.Material.Grass,
+            ["imageIdx"] = 0,
+            ["reflectance"] = 0
+        }
+    }
+}
