b72e372b855279831d964ab67ea2a152fdbef0c5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-13 15:26:54 +1100

    Create haircolors.lua

---
+++ GameData/Apocalypse Rising [1600503]/haircolors.lua
@@ -0,0 +1,18 @@
+data = {
+    ["1"] = Color3.fromRGB(255, 255, 255),
+    ["2"] = Color3.fromRGB(255, 215, 125),
+    ["3"] = Color3.fromRGB(200, 150, 100),
+    ["4"] = Color3.fromRGB(150, 100, 63),
+    ["5"] = Color3.fromRGB(90, 50, 25),
+    ["6"] = Color3.fromRGB(255, 100, 50),
+    ["7"] = Color3.fromRGB(175, 75, 50),
+    ["8"] = Color3.fromRGB(50, 38, 38),
+    ["9"] = Color3.fromRGB(255, 0, 0),
+    ["10"] = Color3.fromRGB(0, 255, 0)
+    ["11"] = Color3.fromRGB(0, 0, 255),
+    ["12"] = Color3.fromRGB(200, 25, 200),
+    ["13"] = Color3.fromRGB(255, 25, 100),
+    ["14"] = Color3.fromRGB(0, 0, 77),
+    ["15"] = Color3.fromRGB(100, 150, 75),
+    ["16"] = Color3.fromRGB(0, 0, 0),
+}
