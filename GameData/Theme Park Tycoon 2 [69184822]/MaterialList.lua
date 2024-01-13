4948bea512861959dfd620cae4cd63b1795dd640
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-13 16:02:56 +1100

    Create MaterialList.lua

---
+++ GameData/Theme Park Tycoon 2 [69184822]/MaterialList.lua
@@ -0,0 +1,212 @@
+data = {
+    ["getMaterialIcon"] = "function: 0x000000001b052a8a",
+    ["getMaterialName"] = "function: 0x000000002c2053c2",
+    ["materials"] = {
+        {
+            ["vname"] = "Plastic",
+            ["id"] = 1,
+            ["material"] = Enum.Material.Plastic,
+            ["icon"] = 18,
+            ["tkey"] = "ui_material_plastic"
+        },
+        {
+            ["vname"] = "Metal",
+            ["id"] = 2,
+            ["material"] = Enum.Material.Metal,
+            ["icon"] = 7,
+            ["tkey"] = "ui_material_metal"
+        },
+        {
+            ["vname"] = "Wood",
+            ["id"] = 3,
+            ["material"] = Enum.Material.Wood,
+            ["icon"] = 13,
+            ["tkey"] = "ui_material_wood"
+        },
+        {
+            ["vname"] = "Wooden planks",
+            ["id"] = 4,
+            ["material"] = Enum.Material.WoodPlanks,
+            ["icon"] = 15,
+            ["tkey"] = "ui_material_woodenplanks"
+        },
+        {
+            ["vname"] = "Brick",
+            ["id"] = 5,
+            ["material"] = Enum.Material.Brick,
+            ["icon"] = 21,
+            ["tkey"] = "ui_material_brick"
+        },
+        {
+            ["vname"] = "Fabric",
+            ["id"] = 6,
+            ["material"] = Enum.Material.Fabric,
+            ["icon"] = 3,
+            ["tkey"] = "ui_material_fabric"
+        },
+        {
+            ["vname"] = "Concrete",
+            ["id"] = 7,
+            ["material"] = Enum.Material.Concrete,
+            ["icon"] = 1,
+            ["tkey"] = "ui_material_concrete"
+        },
+        {
+            ["vname"] = "Rock",
+            ["id"] = 8,
+            ["material"] = Enum.Material.Slate,
+            ["icon"] = 12,
+            ["tkey"] = "ui_material_rock"
+        },
+        {
+            ["vname"] = "Metal plates",
+            ["id"] = 9,
+            ["material"] = Enum.Material.DiamondPlate,
+            ["icon"] = 2,
+            ["tkey"] = "ui_material_metalplates"
+        },
+        {
+            ["vname"] = "Metal (corroded)",
+            ["id"] = 10,
+            ["material"] = Enum.Material.CorrodedMetal,
+            ["icon"] = 9,
+            ["tkey"] = "ui_material_metalcorroded"
+        },
+        {
+            ["vname"] = "Grass",
+            ["id"] = 11,
+            ["material"] = Enum.Material.Grass,
+            ["icon"] = 5,
+            ["tkey"] = "ui_material_grass"
+        },
+        {
+            ["vname"] = "Sand",
+            ["id"] = 12,
+            ["material"] = Enum.Material.Sand,
+            ["icon"] = 10,
+            ["tkey"] = "ui_material_sand"
+        },
+        {["vname"] = "Ice", ["id"] = 13, ["material"] = Enum.Material.Ice, ["icon"] = 19, ["tkey"] = "ui_material_ice"},
+        {
+            ["vname"] = "Foil",
+            ["id"] = 14,
+            ["material"] = Enum.Material.Foil,
+            ["icon"] = 17,
+            ["tkey"] = "ui_material_foil"
+        },
+        {
+            ["vname"] = "Granite",
+            ["id"] = 15,
+            ["material"] = Enum.Material.Granite,
+            ["icon"] = 20,
+            ["tkey"] = "ui_material_granite"
+        },
+        {
+            ["vname"] = "Marble",
+            ["id"] = 16,
+            ["material"] = Enum.Material.Marble,
+            ["icon"] = 6,
+            ["tkey"] = "ui_material_marble"
+        },
+        {
+            ["vname"] = "Cobblestone",
+            ["id"] = 17,
+            ["material"] = Enum.Material.Cobblestone,
+            ["icon"] = 0,
+            ["tkey"] = "ui_material_cobblestone"
+        },
+        {
+            ["vname"] = "Pebble",
+            ["id"] = 18,
+            ["material"] = Enum.Material.Pebble,
+            ["icon"] = 8,
+            ["tkey"] = "ui_material_pebble"
+        },
+        {
+            ["vname"] = "Rock (rough)",
+            ["id"] = 19,
+            ["material"] = Enum.Material.Rock,
+            ["icon"] = 23,
+            ["tkey"] = "ui_material_rockrough"
+        },
+        {
+            ["vname"] = "Ground",
+            ["id"] = 20,
+            ["material"] = Enum.Material.Ground,
+            ["icon"] = 28,
+            ["tkey"] = "ui_material_ground"
+        },
+        {
+            ["vname"] = "Asphalt",
+            ["id"] = 21,
+            ["material"] = Enum.Material.Asphalt,
+            ["icon"] = 29,
+            ["tkey"] = "ui_material_asphalt"
+        },
+        {
+            ["vname"] = "Snow",
+            ["id"] = 22,
+            ["material"] = Enum.Material.Snow,
+            ["icon"] = 34,
+            ["tkey"] = "ui_material_snow"
+        },
+        {
+            ["vname"] = "Pavement",
+            ["id"] = 23,
+            ["material"] = Enum.Material.Pavement,
+            ["icon"] = 24,
+            ["tkey"] = "ui_material_pavement"
+        },
+        {["vname"] = "Mud", ["id"] = 24, ["material"] = Enum.Material.Mud, ["icon"] = 35, ["tkey"] = "ui_material_mud"},
+        {
+            ["vname"] = "Leafy grass",
+            ["id"] = 25,
+            ["material"] = Enum.Material.LeafyGrass,
+            ["icon"] = 25,
+            ["tkey"] = "ui_material_leafygrass"
+        },
+        {
+            ["vname"] = "Sandstone",
+            ["id"] = 26,
+            ["material"] = Enum.Material.Sandstone,
+            ["icon"] = 33,
+            ["tkey"] = "ui_material_sandstone"
+        },
+        {
+            ["vname"] = "Limestone",
+            ["id"] = 27,
+            ["material"] = Enum.Material.Limestone,
+            ["icon"] = 32,
+            ["tkey"] = "ui_material_limestone"
+        },
+        {
+            ["vname"] = "Salt",
+            ["id"] = 28,
+            ["material"] = Enum.Material.Salt,
+            ["icon"] = 31,
+            ["tkey"] = "ui_material_salt"
+        },
+        {
+            ["vname"] = "Glacier",
+            ["id"] = 29,
+            ["material"] = Enum.Material.Glacier,
+            ["icon"] = 27,
+            ["tkey"] = "ui_material_glacier"
+        },
+        {
+            ["vname"] = "Basalt",
+            ["id"] = 30,
+            ["material"] = Enum.Material.Basalt,
+            ["icon"] = 30,
+            ["tkey"] = "ui_material_basalt"
+        },
+        {
+            ["vname"] = "Cracked lava",
+            ["id"] = 31,
+            ["material"] = Enum.Material.CrackedLava,
+            ["icon"] = 26,
+            ["tkey"] = "ui_material_crackedlava"
+        }
+    },
+    ["getDropdownList"] = "function: 0x00000000b345511a"
+}
