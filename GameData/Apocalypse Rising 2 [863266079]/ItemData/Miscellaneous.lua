3a118021c655d7eef5d63792e18b2fdbfecd1e90
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-18 02:54:25 +1100

    Create Miscellaneous.lua

---
+++ GameData/Apocalypse Rising 2 [863266079]/ItemData/Miscellaneous.lua
@@ -0,0 +1,278 @@
+data = {
+    ["Road Flare"] = {
+        ["InventoryPickupSound"] = "Fruits 01 Pickup",
+        ["Description"] = "A pyrotechnic stick that, when activated, produces a temporary bright light and intense heat.",
+        ["UtilityType"] = "Flare",
+        ["CanLockMouse"] = false,
+        ["InventoryDropSound"] = "Fruits 01 Drop",
+        ["CanSlotAsUtility"] = false,
+        ["AudibleRange"] = 30,
+        ["RealName"] = "Road Flare",
+        ["LoopSound"] = "Action.Flare Loop",
+        ["IconData"] = {
+            ["ModelSource"] = "Loot",
+            ["Stages"] = {
+                ["Grid"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 25,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {
+                        ["Y"] = -0.015,
+                        ["X"] = 0,
+                        ["Roll"] = -1.58816,
+                        ["Z"] = -5.19,
+                        ["Yaw"] = -2.462634,
+                        ["Pitch"] = -0.027502
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["LightDirection"] = Vector3.new(0.5966249704360962, -0.6124150156974792, -0.5186390280723572),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19)
+                },
+                ["Dynamic"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 25,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {
+                        ["Y"] = -0.565,
+                        ["X"] = 0,
+                        ["Roll"] = -1.58816,
+                        ["Z"] = -2.75,
+                        ["Yaw"] = -2.462634,
+                        ["Pitch"] = -0.027502
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["LightDirection"] = Vector3.new(0.5966249704360962, -0.6124150156974792, -0.5186390280723572),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19)
+                }
+            }
+        },
+        ["Type"] = "Utility",
+        ["IconRotationDirection"] = 1,
+        ["LightSound"] = "Action.Flare Start",
+        ["GridSize"] = {1, 2},
+        ["UtilityMode"] = "Deployable",
+        ["SubType"] = "Chemlight",
+        ["DisplayName"] = "Road Flare",
+        ["IconLabelText"] = "Flare",
+        ["CanEquipInVehicles"] = false,
+        ["CanUseInInventory"] = true,
+        ["CanTurnCharacter"] = false,
+        ["CanEquipInHands"] = false,
+        ["CanSlotInHotbar"] = false,
+        ["IconBackdropAppearance"] = "Orange Utility",
+        ["LightLength"] = 480
+    },
+    ["Red Chemlight"] = {
+        ["InventoryPickupSound"] = "Fruits 01 Pickup",
+        ["Description"] = [[A light stick containing a base catalyst and hydrogen peroxide. 
+
+When snapped, the chemical reaction temporarily produces a small amount of light.]],
+        ["BreakSound"] = "Action.Chemlight",
+        ["UtilityType"] = "Chemlight",
+        ["CanLockMouse"] = false,
+        ["InventoryDropSound"] = "Fruits 01 Drop",
+        ["CanSlotAsUtility"] = false,
+        ["AudibleRange"] = 20,
+        ["RealName"] = "Red Chemlight",
+        ["ScreenTint"] = {
+            ["TintColor"] = Color3.fromRGB(255, 242, 243),
+            ["Brightness"] = 0.01,
+            ["Saturation"] = 0,
+            ["Contrast"] = 0
+        },
+        ["BodyGlowConfig"] = {["Color"] = Color3.fromRGB(255, 0, 0), ["Brightness"] = 1, ["Range"] = 15},
+        ["Type"] = "Utility",
+        ["IconRotationDirection"] = 1,
+        ["IconData"] = {
+            ["ModelSource"] = "Loot",
+            ["Stages"] = {
+                ["Grid"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 12.5,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {
+                        ["Y"] = -0.004,
+                        ["X"] = -0.01,
+                        ["Roll"] = -1.60955,
+                        ["Z"] = -5.77,
+                        ["Yaw"] = -1.955754,
+                        ["Pitch"] = -0.012809
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["LightDirection"] = Vector3.new(0.46870899200439453, -0.05694299936294556, -0.8815150260925293),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19)
+                },
+                ["Dynamic"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 12.5,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {
+                        ["Y"] = -0.264,
+                        ["X"] = -0.01,
+                        ["Roll"] = -1.60955,
+                        ["Z"] = -3.46,
+                        ["Yaw"] = -1.955754,
+                        ["Pitch"] = -0.012809
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["LightDirection"] = Vector3.new(0.46870899200439453, -0.05694299936294556, -0.8815150260925293),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19)
+                }
+            }
+        },
+        ["GridSize"] = {1, 2},
+        ["UtilityMode"] = "Deployable",
+        ["SubType"] = "Chemlight",
+        ["DisplayName"] = "Red Chemlight",
+        ["IconLabelText"] = "CLight",
+        ["CanEquipInVehicles"] = false,
+        ["CanUseInInventory"] = true,
+        ["CanTurnCharacter"] = false,
+        ["CanEquipInHands"] = false,
+        ["CanSlotInHotbar"] = false,
+        ["IconBackdropAppearance"] = "Orange Utility",
+        ["LightLength"] = 600
+    },
+    ["Blue Chemlight"] = {
+        ["InventoryPickupSound"] = "Fruits 01 Pickup",
+        ["Description"] = [[A light stick containing a base catalyst and hydrogen peroxide. 
+
+When snapped, the chemical reaction temporarily produces a small amount of light.]],
+        ["BreakSound"] = "Action.Chemlight",
+        ["UtilityType"] = "Chemlight",
+        ["CanLockMouse"] = false,
+        ["InventoryDropSound"] = "Fruits 01 Drop",
+        ["CanSlotAsUtility"] = false,
+        ["AudibleRange"] = 20,
+        ["RealName"] = "Blue Chemlight",
+        ["ScreenTint"] = {
+            ["TintColor"] = Color3.fromRGB(243, 250, 255),
+            ["Brightness"] = 0.01,
+            ["Saturation"] = 0,
+            ["Contrast"] = 0
+        },
+        ["BodyGlowConfig"] = {["Color"] = Color3.fromRGB(47, 127, 255), ["Brightness"] = 1, ["Range"] = 15},
+        ["Type"] = "Utility",
+        ["IconRotationDirection"] = 1,
+        ["IconData"] = {
+            ["ModelSource"] = "Loot",
+            ["Stages"] = {
+                ["Grid"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 12.5,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {
+                        ["Y"] = -0.004,
+                        ["X"] = -0.01,
+                        ["Roll"] = -1.60955,
+                        ["Z"] = -5.77,
+                        ["Yaw"] = -1.955754,
+                        ["Pitch"] = -0.012809
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["LightDirection"] = Vector3.new(0.46870899200439453, -0.05694299936294556, -0.8815150260925293),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19)
+                },
+                ["Dynamic"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 12.5,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {
+                        ["Y"] = -0.264,
+                        ["X"] = -0.01,
+                        ["Roll"] = -1.60955,
+                        ["Z"] = -3.46,
+                        ["Yaw"] = -1.955754,
+                        ["Pitch"] = -0.012809
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["LightDirection"] = Vector3.new(0.46870899200439453, -0.05694299936294556, -0.8815150260925293),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19)
+                }
+            }
+        },
+        ["GridSize"] = {1, 2},
+        ["UtilityMode"] = "Deployable",
+        ["SubType"] = "Chemlight",
+        ["DisplayName"] = "Blue Chemlight",
+        ["IconLabelText"] = "CLight",
+        ["CanEquipInVehicles"] = false,
+        ["CanUseInInventory"] = true,
+        ["CanTurnCharacter"] = false,
+        ["CanEquipInHands"] = false,
+        ["CanSlotInHotbar"] = false,
+        ["IconBackdropAppearance"] = "Orange Utility",
+        ["LightLength"] = 600
+    },
+    ["Green Chemlight"] = {
+        ["InventoryPickupSound"] = "Fruits 01 Pickup",
+        ["Description"] = [[A light stick containing a base catalyst and hydrogen peroxide. 
+
+When snapped, the chemical reaction temporarily produces a small amount of light.]],
+        ["BreakSound"] = "Action.Chemlight",
+        ["UtilityType"] = "Chemlight",
+        ["CanLockMouse"] = false,
+        ["InventoryDropSound"] = "Fruits 01 Drop",
+        ["CanSlotAsUtility"] = false,
+        ["AudibleRange"] = 20,
+        ["RealName"] = "Green Chemlight",
+        ["ScreenTint"] = {
+            ["TintColor"] = Color3.fromRGB(242, 255, 239),
+            ["Brightness"] = 0.01,
+            ["Saturation"] = 0,
+            ["Contrast"] = 0
+        },
+        ["BodyGlowConfig"] = {["Color"] = Color3.fromRGB(153, 255, 20), ["Brightness"] = 1, ["Range"] = 15},
+        ["Type"] = "Utility",
+        ["IconRotationDirection"] = 1,
+        ["IconData"] = {
+            ["ModelSource"] = "Loot",
+            ["Stages"] = {
+                ["Grid"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 12.5,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {
+                        ["Y"] = -0.004,
+                        ["X"] = -0.01,
+                        ["Roll"] = -1.60955,
+                        ["Z"] = -5.77,
+                        ["Yaw"] = -1.955754,
+                        ["Pitch"] = -0.012809
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["LightDirection"] = Vector3.new(0.46870899200439453, -0.05694299936294556, -0.8815150260925293),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19)
+                },
+                ["Dynamic"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 12.5,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {
+                        ["Y"] = -0.264,
+                        ["X"] = -0.01,
+                        ["Roll"] = -1.60955,
+                        ["Z"] = -3.46,
+                        ["Yaw"] = -1.955754,
+                        ["Pitch"] = -0.012809
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["LightDirection"] = Vector3.new(0.46870899200439453, -0.05694299936294556, -0.8815150260925293),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19)
+                }
+            }
+        },
+        ["GridSize"] = {1, 2},
+        ["UtilityMode"] = "Deployable",
+        ["SubType"] = "Chemlight",
+        ["DisplayName"] = "Green Chemlight",
+        ["IconLabelText"] = "CLight",
+        ["CanEquipInVehicles"] = false,
+        ["CanUseInInventory"] = true,
+        ["CanTurnCharacter"] = false,
+        ["CanEquipInHands"] = false,
+        ["CanSlotInHotbar"] = false,
+        ["IconBackdropAppearance"] = "Orange Utility",
+        ["LightLength"] = 600
+    }
+}
