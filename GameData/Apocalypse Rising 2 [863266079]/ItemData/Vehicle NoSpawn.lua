11474806100c4605681c040219350bb0949d5533
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-18 02:55:20 +1100

    Create Vehicle NoSpawn.lua

---
+++ GameData/Apocalypse Rising 2 [863266079]/ItemData/Vehicle NoSpawn.lua
@@ -0,0 +1,342 @@
+data = {
+    ["Acceleration Upgrade Kit"] = {
+        ["Type"] = "RepairTool",
+        ["Description"] = "",
+        ["GridSize"] = {},
+        ["IconData"] = {
+            ["ModelSource"] = "Loot",
+            ["Stages"] = {
+                ["Creator"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 15,
+                    ["Ambient"] = Color3.fromRGB(117, 129, 149),
+                    ["ModelCFrame"] = {
+                        ["Y"] = 0.06,
+                        ["X"] = -0.14,
+                        ["Roll"] = 0,
+                        ["Z"] = -8.64,
+                        ["Yaw"] = -2.583087,
+                        ["Pitch"] = 0.000001
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 247, 230),
+                    ["LightDirection"] = Vector3.new(-0.5037329792976379, -0.7690020203590393, -0.3935590088367462),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19)
+                },
+                ["Dynamic"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 70,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {["Y"] = 0, ["X"] = 0, ["Roll"] = 0, ["Z"] = -3, ["Yaw"] = 0, ["Pitch"] = 0},
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19),
+                    ["LightDirection"] = Vector3.new(-1, -1, -1)
+                },
+                ["Equip"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 25,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {
+                        ["Y"] = -0.01,
+                        ["X"] = -0.119565,
+                        ["Roll"] = -0.211121,
+                        ["Z"] = -4.449785,
+                        ["Yaw"] = -2.451928,
+                        ["Pitch"] = -0.161979
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19),
+                    ["LightDirection"] = Vector3.new(0.693198025226593, -0.5485029816627502, -0.4675680100917816)
+                },
+                ["Hotbar"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 70,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {["Y"] = 0, ["X"] = 0, ["Roll"] = 0, ["Z"] = -3, ["Yaw"] = 0, ["Pitch"] = 0},
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19),
+                    ["LightDirection"] = Vector3.new(-1, -1, -1)
+                },
+                ["Grid"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 25,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {
+                        ["Y"] = -0.01,
+                        ["X"] = -0.119565,
+                        ["Roll"] = -0.211121,
+                        ["Z"] = -4.449785,
+                        ["Yaw"] = -2.451928,
+                        ["Pitch"] = -0.161979
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19),
+                    ["LightDirection"] = Vector3.new(0.693198025226593, -0.5485029816627502, -0.4675680100917816)
+                }
+            }
+        },
+        ["DisplayName"] = "Acceleration Upgrade Kit",
+        ["IconLabelText"] = "",
+        ["QualityName"] = "",
+        ["RepairSlot"] = "",
+        ["IconRotationDirection"] = 0,
+        ["QualityTier"] = 1,
+        ["RealName"] = "Acceleration Upgrade Kit",
+        ["IconBackdropAppearance"] = "Default",
+        ["RepairPercent"] = 0
+    },
+    ["Top Speed Upgrade Kit"] = {
+        ["Type"] = "RepairTool",
+        ["Description"] = "",
+        ["GridSize"] = {},
+        ["IconData"] = {
+            ["ModelSource"] = "Loot",
+            ["Stages"] = {
+                ["Creator"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 15,
+                    ["Ambient"] = Color3.fromRGB(117, 129, 149),
+                    ["ModelCFrame"] = {
+                        ["Y"] = 0.06,
+                        ["X"] = -0.14,
+                        ["Roll"] = 0,
+                        ["Z"] = -8.64,
+                        ["Yaw"] = -2.583087,
+                        ["Pitch"] = 0.000001
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 247, 230),
+                    ["LightDirection"] = Vector3.new(-0.5037329792976379, -0.7690020203590393, -0.3935590088367462),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19)
+                },
+                ["Dynamic"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 70,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {["Y"] = 0, ["X"] = 0, ["Roll"] = 0, ["Z"] = -3, ["Yaw"] = 0, ["Pitch"] = 0},
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19),
+                    ["LightDirection"] = Vector3.new(-1, -1, -1)
+                },
+                ["Equip"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 25,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {
+                        ["Y"] = -0.01,
+                        ["X"] = -0.119565,
+                        ["Roll"] = -0.211121,
+                        ["Z"] = -4.449785,
+                        ["Yaw"] = -2.451928,
+                        ["Pitch"] = -0.161979
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19),
+                    ["LightDirection"] = Vector3.new(0.693198025226593, -0.5485029816627502, -0.4675680100917816)
+                },
+                ["Hotbar"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 70,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {["Y"] = 0, ["X"] = 0, ["Roll"] = 0, ["Z"] = -3, ["Yaw"] = 0, ["Pitch"] = 0},
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19),
+                    ["LightDirection"] = Vector3.new(-1, -1, -1)
+                },
+                ["Grid"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 25,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {
+                        ["Y"] = -0.01,
+                        ["X"] = -0.119565,
+                        ["Roll"] = -0.211121,
+                        ["Z"] = -4.449785,
+                        ["Yaw"] = -2.451928,
+                        ["Pitch"] = -0.161979
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19),
+                    ["LightDirection"] = Vector3.new(0.693198025226593, -0.5485029816627502, -0.4675680100917816)
+                }
+            }
+        },
+        ["DisplayName"] = "Top Speed Upgrade Kit",
+        ["IconLabelText"] = "",
+        ["QualityName"] = "",
+        ["RepairSlot"] = "",
+        ["IconRotationDirection"] = 0,
+        ["QualityTier"] = 1,
+        ["RealName"] = "Top Speed Upgrade Kit",
+        ["IconBackdropAppearance"] = "Default",
+        ["RepairPercent"] = 0
+    },
+    ["Engine Boost  Upgrade Kit"] = {
+        ["Type"] = "RepairTool",
+        ["Description"] = "",
+        ["GridSize"] = {},
+        ["IconData"] = {
+            ["ModelSource"] = "Loot",
+            ["Stages"] = {
+                ["Creator"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 15,
+                    ["Ambient"] = Color3.fromRGB(117, 129, 149),
+                    ["ModelCFrame"] = {
+                        ["Y"] = 0.06,
+                        ["X"] = -0.14,
+                        ["Roll"] = 0,
+                        ["Z"] = -8.64,
+                        ["Yaw"] = -2.583087,
+                        ["Pitch"] = 0.000001
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 247, 230),
+                    ["LightDirection"] = Vector3.new(-0.5037329792976379, -0.7690020203590393, -0.3935590088367462),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19)
+                },
+                ["Dynamic"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 70,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {["Y"] = 0, ["X"] = 0, ["Roll"] = 0, ["Z"] = -3, ["Yaw"] = 0, ["Pitch"] = 0},
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19),
+                    ["LightDirection"] = Vector3.new(-1, -1, -1)
+                },
+                ["Equip"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 25,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {
+                        ["Y"] = -0.01,
+                        ["X"] = -0.119565,
+                        ["Roll"] = -0.211121,
+                        ["Z"] = -4.449785,
+                        ["Yaw"] = -2.451928,
+                        ["Pitch"] = -0.161979
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19),
+                    ["LightDirection"] = Vector3.new(0.693198025226593, -0.5485029816627502, -0.4675680100917816)
+                },
+                ["Hotbar"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 70,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {["Y"] = 0, ["X"] = 0, ["Roll"] = 0, ["Z"] = -3, ["Yaw"] = 0, ["Pitch"] = 0},
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19),
+                    ["LightDirection"] = Vector3.new(-1, -1, -1)
+                },
+                ["Grid"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 25,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {
+                        ["Y"] = -0.01,
+                        ["X"] = -0.119565,
+                        ["Roll"] = -0.211121,
+                        ["Z"] = -4.449785,
+                        ["Yaw"] = -2.451928,
+                        ["Pitch"] = -0.161979
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19),
+                    ["LightDirection"] = Vector3.new(0.693198025226593, -0.5485029816627502, -0.4675680100917816)
+                }
+            }
+        },
+        ["DisplayName"] = "Engine Boost Upgrade Kit",
+        ["IconLabelText"] = "",
+        ["QualityName"] = "",
+        ["RepairSlot"] = "",
+        ["IconRotationDirection"] = 0,
+        ["QualityTier"] = 1,
+        ["RealName"] = "Engine Boost  Upgrade Kit",
+        ["IconBackdropAppearance"] = "Default",
+        ["RepairPercent"] = 0
+    },
+    ["Torque Upgrade Kit"] = {
+        ["Type"] = "RepairTool",
+        ["Description"] = "",
+        ["GridSize"] = {},
+        ["IconData"] = {
+            ["ModelSource"] = "Loot",
+            ["Stages"] = {
+                ["Creator"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 15,
+                    ["Ambient"] = Color3.fromRGB(117, 129, 149),
+                    ["ModelCFrame"] = {
+                        ["Y"] = 0.06,
+                        ["X"] = -0.14,
+                        ["Roll"] = 0,
+                        ["Z"] = -8.64,
+                        ["Yaw"] = -2.583087,
+                        ["Pitch"] = 0.000001
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 247, 230),
+                    ["LightDirection"] = Vector3.new(-0.5037329792976379, -0.7690020203590393, -0.3935590088367462),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19)
+                },
+                ["Dynamic"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 70,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {["Y"] = 0, ["X"] = 0, ["Roll"] = 0, ["Z"] = -3, ["Yaw"] = 0, ["Pitch"] = 0},
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19),
+                    ["LightDirection"] = Vector3.new(-1, -1, -1)
+                },
+                ["Equip"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 25,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {
+                        ["Y"] = -0.01,
+                        ["X"] = -0.119565,
+                        ["Roll"] = -0.211121,
+                        ["Z"] = -4.449785,
+                        ["Yaw"] = -2.451928,
+                        ["Pitch"] = -0.161979
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19),
+                    ["LightDirection"] = Vector3.new(0.693198025226593, -0.5485029816627502, -0.4675680100917816)
+                },
+                ["Hotbar"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 70,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {["Y"] = 0, ["X"] = 0, ["Roll"] = 0, ["Z"] = -3, ["Yaw"] = 0, ["Pitch"] = 0},
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19),
+                    ["LightDirection"] = Vector3.new(-1, -1, -1)
+                },
+                ["Grid"] = {
+                    ["ImageColor3"] = Color3.fromRGB(255, 255, 255),
+                    ["FieldOfView"] = 25,
+                    ["Ambient"] = Color3.fromRGB(255, 255, 255),
+                    ["ModelCFrame"] = {
+                        ["Y"] = -0.01,
+                        ["X"] = -0.119565,
+                        ["Roll"] = -0.211121,
+                        ["Z"] = -4.449785,
+                        ["Yaw"] = -2.451928,
+                        ["Pitch"] = -0.161979
+                    },
+                    ["LightColor"] = Color3.fromRGB(255, 255, 255),
+                    ["BackgroundColor3"] = Color3.fromRGB(19, 19, 19),
+                    ["LightDirection"] = Vector3.new(0.693198025226593, -0.5485029816627502, -0.4675680100917816)
+                }
+            }
+        },
+        ["DisplayName"] = "Torque Upgrade Kit",
+        ["IconLabelText"] = "",
+        ["QualityName"] = "",
+        ["RepairSlot"] = "",
+        ["IconRotationDirection"] = 0,
+        ["QualityTier"] = 1,
+        ["RealName"] = "Torque Upgrade Kit",
+        ["IconBackdropAppearance"] = "Default",
+        ["RepairPercent"] = 0
+    }
+}
