57a1dfc6bc5f79d7dbea17b6500152979bfdd81c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-18 02:05:43 +1100

    Create Animations.lua

---
+++ GameData/Apocalypse Rising [863266079]/Animations.lua
@@ -0,0 +1,123 @@
+data = {
+    ["Falling Old"] = {["Forwards"] = "rbxassetid://574175243", ["Backwards"] = "rbxassetid://574175243"},
+    ["Crouching"] = {
+        ["BackwardsLeft"] = "rbxassetid://665031163",
+        ["Backwards"] = "rbxassetid://665029883",
+        ["ForwardsRight"] = "rbxassetid://665026158",
+        ["Forwards"] = "rbxassetid://665025121",
+        ["BackwardsRight"] = "rbxassetid://665028564",
+        ["Right"] = "rbxassetid://665027469",
+        ["Left"] = "rbxassetid://665032095",
+        ["ForwardsLeft"] = "rbxassetid://665032930"
+    },
+    ["Sitting"] = {["Forwards"] = "rbxassetid://4535664230", ["Backwards"] = "rbxassetid://4535664230"},
+    ["Zombies"] = {
+        ["Rage"] = "rbxassetid://7316069066",
+        ["Idle"] = "rbxassetid://8044634867",
+        ["Idle Agro"] = "rbxassetid://8044636769",
+        ["Idle Look 1"] = "rbxassetid://8044656070",
+        ["Trip"] = "rbxassetid://6056547338",
+        ["Confused"] = "rbxassetid://7316134406",
+        ["Vault"] = "rbxassetid://6056562479",
+        ["Stumble"] = "rbxassetid://3972162024",
+        ["Running"] = "rbxassetid://8044696131",
+        ["Walking"] = "rbxassetid://8044672721",
+        ["Idle Look 2"] = "rbxassetid://8044662124",
+        ["Stun"] = "rbxassetid://2872034870",
+        ["Vault Low"] = "rbxassetid://7746886138",
+        ["Attack 1"] = "rbxassetid://8044087621",
+        ["Attack 2"] = "rbxassetid://8044633189"
+    },
+    ["Crawling"] = {
+        ["BackwardsLeft"] = "rbxassetid://713803401",
+        ["Backwards"] = "rbxassetid://713803066",
+        ["ForwardsRight"] = "rbxassetid://713800706",
+        ["Forwards"] = "rbxassetid://713799890",
+        ["BackwardsRight"] = "rbxassetid://713802534",
+        ["Right"] = "rbxassetid://713801855",
+        ["Left"] = "rbxassetid://713834610",
+        ["ForwardsLeft"] = "rbxassetid://713835155"
+    },
+    ["Melees"] = {
+        ["One Handed Chop"] = "rbxassetid://2857856883",
+        ["One Handed Slash"] = "rbxassetid://2995267122",
+        ["Shove"] = "rbxassetid://5887311112",
+        ["One Handed Stab"] = "rbxassetid://2857859768",
+        ["Two Handed Chop"] = "rbxassetid://2995268391"
+    },
+    ["Swimming"] = {
+        ["BackwardsLeft"] = "rbxassetid://1267954312",
+        ["Backwards"] = "rbxassetid://1267949674",
+        ["ForwardsRight"] = "rbxassetid://1267967800",
+        ["Forwards"] = "rbxassetid://1267961625",
+        ["BackwardsRight"] = "rbxassetid://1267956543",
+        ["Right"] = "rbxassetid://1267972596",
+        ["Left"] = "rbxassetid://1267971712",
+        ["ForwardsLeft"] = "rbxassetid://1267966623"
+    },
+    ["Falling"] = {["Forwards"] = "rbxassetid://6539236191", ["Backwards"] = "rbxassetid://6539236191"},
+    ["Running"] = {
+        ["BackwardsLeft"] = "rbxassetid://612434058",
+        ["Backwards"] = "rbxassetid://612419340",
+        ["ForwardsRight"] = "rbxassetid://612398488",
+        ["Forwards"] = "rbxassetid://612393879",
+        ["BackwardsRight"] = "rbxassetid://612409280",
+        ["Right"] = "rbxassetid://612403009",
+        ["Left"] = "rbxassetid://612442604",
+        ["ForwardsLeft"] = "rbxassetid://612447082"
+    },
+    ["Walking"] = {
+        ["BackwardsLeft"] = "rbxassetid://612335580",
+        ["Backwards"] = "rbxassetid://612332475",
+        ["ForwardsRight"] = "rbxassetid://612351549",
+        ["Forwards"] = "rbxassetid://612316728",
+        ["BackwardsRight"] = "rbxassetid://612338853",
+        ["Right"] = "rbxassetid://612322606",
+        ["Left"] = "rbxassetid://612327125",
+        ["ForwardsLeft"] = "rbxassetid://612671233"
+    },
+    ["Death"] = {
+        ["Standing Forwards"] = "rbxassetid://3074776675",
+        ["Standing Backwards"] = "rbxassetid://3074778311",
+        ["Crouch Backwards"] = "rbxassetid://3074781193",
+        ["Crouch Forwards"] = "rbxassetid://3074782579"
+    },
+    ["Boxing"] = {
+        ["Box Left Jab"] = "rbxassetid://2980654856",
+        ["Box Block"] = "rbxassetid://2980658303",
+        ["Box Stagger"] = "rbxassetid://3068863609",
+        ["Box Right Jab"] = "rbxassetid://2980653024",
+        ["Box Kick"] = "rbxassetid://3068866003",
+        ["Box Idle"] = "rbxassetid://2980656305"
+    },
+    ["Idle"] = {
+        ["Crouching"] = "rbxassetid://574173951",
+        ["Sitting"] = "rbxassetid://4535664230",
+        ["Standing"] = "rbxassetid://574172168",
+        ["Crawling"] = "rbxassetid://713835520",
+        ["Swimming"] = "rbxassetid://1267970543"
+    },
+    ["Actions"] = {
+        ["VaultHigh"] = "rbxassetid://5897359294",
+        ["Military Health Booster"] = "rbxassetid://2376714102",
+        ["Inventory Search"] = "rbxassetid://4043835168",
+        ["Eat"] = "rbxassetid://2490928804",
+        ["Flashlight Hold"] = "rbxassetid://6593662175",
+        ["Military Stamina Booster"] = "rbxassetid://2376720284",
+        ["Drink"] = "rbxassetid://2490886301",
+        ["Jump"] = "rbxassetid://574175001",
+        ["Lantern Hold"] = "rbxassetid://6593639960",
+        ["Small MedKit"] = "rbxassetid://2376721436",
+        ["Military MedKit"] = "rbxassetid://2376715464",
+        ["Military Flashlight Hold"] = "rbxassetid://6593669645",
+        ["Running Jump"] = "rbxassetid://6510489705",
+        ["Fall Impact"] = "rbxassetid://2340876467",
+        ["Stamina Booster"] = "rbxassetid://5450656856",
+        ["Binoculars Hold"] = "rbxassetid://7525192933",
+        ["Health Booster"] = "rbxassetid://5450645848",
+        ["Running Jump Old"] = "rbxassetid://5857654740",
+        ["Large MedKit"] = "rbxassetid://2376709277",
+        ["VaultLow"] = "rbxassetid://5860219463"
+    },
+    ["Climbing"] = {["Forwards"] = "rbxassetid://959064614", ["Backwards"] = "rbxassetid://2981705868"}
+}
