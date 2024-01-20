1ad3f4d2f9b8923e611c80bd2ed74f63998efd9a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-20 23:45:59 +1100

    Create Trees.lua

---
+++ GameData/Medieval Warfare [109814071]/Trees.lua
@@ -0,0 +1,18 @@
+data = {
+    "blockwood",
+    "cursedwood",
+    "bloodwood",
+    "whitewood",
+    "defiled wood",
+    "fractal wood",
+    "frostwood",
+    "umbrawood",
+    "plaguewood",
+    "fablewood",
+    "elderwood",
+    "rotwood",
+    "zenwood",
+    "deadwood",
+    "redwood",
+    "wanwood"
+}
