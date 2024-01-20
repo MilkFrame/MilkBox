50853dffad4dd715a8f7def9beaeb46077a10106
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-20 23:51:14 +1100

    Create ProductData.lua

---
+++ GameData/Dungeon Quest [2414851778]/ProductData.lua
@@ -0,0 +1,78 @@
+data = {
+    ["Get"] = "function: 0x00000000cbb078f4",
+    ["Products"] = {
+        ["Cosmetics"] = {
+            ["armors"] = {["Shadow Spirit"] = {["id"] = 1340264213}, ["Bartender"] = {["id"] = 1340265192}},
+            ["weapons"] = {["Shadow Daggers"] = {["id"] = 1340264304}, ["Root Beer"] = {["id"] = 1340264331}},
+            ["enchants"] = {["Jade Pulse"] = {["id"] = 1340264106}, ["Golden Burst"] = {["id"] = 1340264162}}
+        },
+        ["XPBoosts"] = {
+            ["2hour"] = {["id"] = 1013086878, ["timeToAdd"] = 7200},
+            ["1hour"] = {["id"] = 1013086750, ["timeToAdd"] = 3600},
+            ["4hour"] = {["id"] = 1013087251, ["timeToAdd"] = 14400}
+        },
+        ["GamePasses"] = {
+            ["FreeStatResets"] = {["id"] = 6093005},
+            ["VIP"] = {["id"] = 6028667},
+            ["ExtraItem"] = {["id"] = 6092999},
+            ["DoubleGold"] = {["id"] = 6092989}
+        },
+        ["Gold"] = {
+            ["tier5"] = {["id"] = 1231085335},
+            ["tier2"] = {["id"] = 1231085313},
+            ["tier1"] = {["id"] = 1231085305},
+            ["tier3"] = {["id"] = 1231085320},
+            ["tier4"] = {["id"] = 1231085324}
+        },
+        ["OniEvent"] = {
+            ["tier5"] = {["id"] = 1613657205, ["currencyToGive"] = 2500},
+            ["tier2"] = {["id"] = 1613656406, ["currencyToGive"] = 250},
+            ["tier1"] = {["id"] = 1613656181, ["currencyToGive"] = 100},
+            ["tier3"] = {["id"] = 1613656626, ["currencyToGive"] = 500},
+            ["tier4"] = {["id"] = 1613656914, ["currencyToGive"] = 1000}
+        },
+        ["RotatingCosmetics"] = {
+            ["1"] = {
+                ["armors"] = {
+                    ["epic"] = {["id"] = 1182451037},
+                    ["legendary"] = {["id"] = 1182451070},
+                    ["uncommon"] = {["id"] = 1182450983},
+                    ["rare"] = {["id"] = 1182451005}
+                },
+                ["weapons"] = {
+                    ["epic"] = {["id"] = 1182451218},
+                    ["legendary"] = {["id"] = 1182451235},
+                    ["uncommon"] = {["id"] = 1182451174},
+                    ["rare"] = {["id"] = 1182451203}
+                },
+                ["enchants"] = {
+                    ["epic"] = {["id"] = 1182450894},
+                    ["legendary"] = {["id"] = 1182450916},
+                    ["uncommon"] = {["id"] = 1182450819},
+                    ["rare"] = {["id"] = 1182450852}
+                }
+            },
+            ["2"] = {
+                ["armors"] = {
+                    ["epic"] = {["id"] = 1182919011},
+                    ["legendary"] = {["id"] = 1182919027},
+                    ["uncommon"] = {["id"] = 1182918924},
+                    ["rare"] = {["id"] = 1182918970}
+                },
+                ["weapons"] = {
+                    ["epic"] = {["id"] = 1182918883},
+                    ["legendary"] = {["id"] = 1182918901},
+                    ["uncommon"] = {["id"] = 1182918836},
+                    ["rare"] = {["id"] = 1182918863}
+                },
+                ["enchants"] = {
+                    ["epic"] = {["id"] = 1182918771},
+                    ["legendary"] = {["id"] = 1182918801},
+                    ["uncommon"] = {["id"] = 1182918731},
+                    ["rare"] = {["id"] = 1182918751}
+                }
+            }
+        },
+        ["DailyRewardRefresh"] = {["id"] = 1013113157}
+    }
+}
