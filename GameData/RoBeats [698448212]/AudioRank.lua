f23ed235807304c487d6f396efa58419a1d58596
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-24 16:47:05 +1100

    Create AudioRank.lua

---
+++ GameData/RoBeats [698448212]/AudioRank.lua
@@ -0,0 +1,21 @@
+data = {
+    ["rank_value_to_accuracy"] = "function: 0x00000000401cae63",
+    ["get_rank_value_icon"] = "function: 0x00000000d85fb9e3",
+    ["contains_index"] = "function: 0x000000007dc8fef3",
+    ["index_to_rank_value"] = "function: 0x00000000ac6d6ee3",
+    ["Value"] = {
+        ["RankF"] = 0,
+        ["RankC"] = 1,
+        ["RankB"] = 2,
+        ["RankA"] = 3,
+        ["RankAPlus"] = 4
+        ["RankD"] = 5,
+        ["RankDPlus"] = 6,
+        ["RankCPlus"] = 7,
+        ["RankBPlus"] = 8,
+        ["RankS"] = 9,
+    },
+    ["get_rank_value_name"] = "function: 0x00000000667aa74b",
+    ["rank_value_to_index"] = "function: 0x000000001dd6a03b",
+    ["accuracy_to_rank_value"] = "function: 0x000000005cb645fb"
+}
