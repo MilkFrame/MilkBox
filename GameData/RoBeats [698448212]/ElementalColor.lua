7b8c453104a941a314b34657b004959713e65bd4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-24 16:26:55 +1100

    Create ElementalColor.lua

---
+++ GameData/RoBeats [698448212]/ElementalColor.lua
@@ -0,0 +1,14 @@
+data = {
+    ["Blue"] = 0,
+    ["Green"] = 1,
+    ["Orange"] = 2,
+    ["Purple"] = 3,
+    ["Red"] = 4,
+    ["color_to_textimage"] = "function: 0x0000000005a652f3",
+    ["color_to_name"] = "function: 0x0000000030530bc3",
+    ["color_to_color3"] = "function: 0x00000000f273c81b",
+    ["render_color_section"] = "function: 0x000000000a2265db",
+    ["color_to_iconimage"] = "function: 0x0000000079beff6b",
+    ["colors_list"] = "function: 0x00000000acec3903",
+    ["color_none"] = "function: 0x00000000a779d50b"
+}
