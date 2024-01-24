5b5264089302f90ff9f9832b0e8e4cd2c0373ab1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-24 16:37:44 +1100

    Create DayEventInfo.lua

---
+++ GameData/RoBeats [698448212]/DayEventInfo.lua
@@ -0,0 +1,23 @@
+data = {
+    ["ActiveEvent"] = {["new"] = "function: 0x00000000719ba1b3"},
+    ["EventType"] = {
+        ["VIPSale"] = 0,
+        ["CoinSale"] = 1,
+        ["StarSale"] = 2,
+        ["PackSale"] = 3
+        ["RBChallenge"] = 4,
+        ["RBSwordHunt"] = 5,
+        ["SongLaunch"] = 6,
+        ["HolidayEvent"] = 7,
+        ["ArtistEvent"] = 8,
+        ["HalloweenCountdown"] = 9,
+        ["UpgradeSale"] = 10,
+        ["CraftSale"] = 11,
+        ["MiniSale"] = 12,
+    },
+    ["EventList"] = {
+        ["from_table"] = "function: 0x000000005199e1fb",
+        ["default_list"] = "function: 0x00000000356f9d23",
+        ["new"] = "function: 0x00000000281d64fb"
+    }
+}
