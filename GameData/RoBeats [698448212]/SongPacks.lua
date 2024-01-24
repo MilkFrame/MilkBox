2f4952ea468f693ab0523907d25ae6193973fa7f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-24 16:38:13 +1100

    Create SongPacks.lua

---
+++ GameData/RoBeats [698448212]/SongPacks.lua
@@ -0,0 +1,160 @@
+local v4 = game.ReplicatedStorage
+local v5 = {}
+local v6 = {}
+v4.Name = "RoBeats Top Hits Pack 1"
+v4.SongKeys = {40, 217, 497, 498, 228, 229, 846, 847}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171193846"
+v6[1] = v4
+v4.Name = "RoBeats Top Hits Pack 2"
+v4.SongKeys = {359, 360, 195, 196, 128, 129, 187, 188}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171208327"
+v6[2] = v4
+v4.Name = "RoBeats Top Hits Pack 3"
+v4.SongKeys = {35, 214, 329, 330, 70, 71, 58, 59}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171209347"
+v6[3] = v4
+v4.Name = "Monstercat Song Pack 1"
+v4.SongKeys = {515, 516, 517, 518, 523, 524, 531, 532}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171209575"
+v6[4] = v4
+v4.Name = "Friday Night Funkin Song Pack 1"
+v4.SongKeys = {42, 218, 271, 272, 50, 215, 52, 84}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171211049"
+v6[5] = v4
+v4.Name = "Fusq Song Pack"
+v4.SongKeys = {128, 129, 343, 344, 357, 358, 469, 470}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171211331"
+v6[6] = v4
+v4.Name = "coda Song Pack"
+v4.SongKeys = {2, 79, 37, 211, 28, 53, 65, 66}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6527672805"
+v6[7] = v4
+v4.Name = "Creo Song Pack 1"
+v4.SongKeys = {22, 38, 102, 103, 137, 138, 162, 163}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171219740"
+v6[8] = v4
+v4.Name = "Camellia Song Pack 1"
+v4.SongKeys = {6, 7, 23, 36, 35, 214, 131, 132}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171220049"
+v6[9] = v4
+v4.Name = "Camellia Song Pack 2"
+v4.SongKeys = {170, 171, 176, 177, 325, 326, 421, 422}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171220295"
+v6[10] = v4
+v4.Name = "Hinkik Song Pack"
+v4.SongKeys = {3, 39, 234, 235, 251, 252, 261, 262}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171220573"
+v6[11] = v4
+v4.Name = "Nash Song Pack"
+v4.SongKeys = {1, 56, 10, 13, 14, 19, 155, 156}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171224392"
+v6[12] = v4
+v4.Name = "LeaF Song Pack"
+v4.SongKeys = {187, 188, 197, 198, 25, 224, 226, 227}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171224745"
+v6[13] = v4
+v4.Name = "Chroma Song Pack"
+v4.SongKeys = {265, 266, 8, 20, 12, 17, 41, 60}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171225199"
+v6[14] = v4
+v4.Name = "RiraN Song Pack"
+v4.SongKeys = {29, 221, 47, 48, 116, 117, 329, 330}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171225606"
+v6[15] = v4
+v4.Name = "tv room Song Pack"
+v4.SongKeys = {377, 378, 87, 88, 149, 150, 431, 432}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171193846"
+v6[16] = v4
+v4.Name = "Dimrain47 Song Pack"
+v4.SongKeys = {63, 64, 124, 125, 365, 366, 407, 408}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171208327"
+v6[17] = v4
+v4.Name = "Geoxor Song Pack"
+v4.SongKeys = {106, 108, 130, 107, 279, 280, 303, 304}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171209347"
+v6[18] = v4
+v4.Name = "Kurokotei Song Pack"
+v4.SongKeys = {301, 302, 219, 46, 164, 165, 168, 169}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171209575"
+v6[19] = v4
+v4.Name = "Monstercat Song Pack 2"
+v4.SongKeys = {305, 306, 311, 312, 345, 346, 347, 348}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171211049"
+v6[20] = v4
+v4.Name = "Creo Song Pack 2"
+v4.SongKeys = {178, 179, 184, 185, 191, 192, 209, 210}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171211331"
+v6[21] = v4
+v4.Name = "BSlick Song Pack"
+v4.SongKeys = {82, 83, 111, 112, 139, 140, 415, 416}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6527672805"
+v6[22] = v4
+v4.Name = "Se-U-Ra Song Pack 1"
+v4.SongKeys = {91, 92, 96, 97, 104, 105, 143, 144}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171219740"
+v6[23] = v4
+v4.Name = "Se-U-Ra Song Pack 2"
+v4.SongKeys = {147, 148, 159, 160, 182, 183, 199, 200}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171220049"
+v6[24] = v4
+v4.Name = "Friday Night Funkin Song Pack 2"
+v4.SongKeys = {571, 572, 573, 574, 575, 576, 579, 580}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171220295"
+v6[25] = v4
+v4.Name = "ARForest Song Pack"
+v4.SongKeys = {605, 606, 607, 608, 609, 610, 617, 618}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171220573"
+v6[26] = v4
+v4.Name = "Silentroom Song Pack"
+v4.SongKeys = {651, 652, 653, 654, 657, 658, 666, 667}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171224392"
+v6[27] = v4
+v4.Name = "Friday Night Funkin Song Pack 3"
+v4.SongKeys = {577, 578, 589, 590, 591, 592, 597, 598}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171224745"
+v6[28] = v4
+v4.Name = "Monstercat Song Pack 3"
+v4.SongKeys = {449, 450, 467, 468, 413, 414, 287, 288}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171225199"
+v6[29] = v4
+v4.Name = "Camellia Song Pack 3"
+v4.SongKeys = {435, 436, 437, 438, 451, 452, 487, 488}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171225606"
+v6[30] = v4
+v4.Name = "garlagan Song Pack"
+v4.SongKeys = {684, 685, 686, 687, 688, 689, 698, 699}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171193846"
+v6[31] = v4
+v4.Name = "naruto2413 Song Pack"
+v4.SongKeys = {748, 749, 760, 761, 764, 765, 924, 925}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171225199"
+v6[32] = v4
+v4.Name = "Halloween Song Pack"
+v4.SongKeys = {799, 800, 802, 803, 805, 806, 75, 76}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171224745"
+v6[33] = v4
+v4.Name = "xi Song Pack"
+v4.SongKeys = {838, 839, 855, 856, 863, 864, 852, 853}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171224392"
+v6[34] = v4
+v4.Name = "James Landino Song Pack"
+v4.SongKeys = {867, 868, 870, 871, 875, 876, 883, 884}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171220573"
+v6[35] = v4
+v4.Name = "F-777 Song Pack"
+v4.SongKeys = {900, 901, 904, 905, 911, 912, 921, 922}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171220573"
+v6[36] = v4
+v4.Name = "dark cat Song Pack"
+v4.SongKeys = {974, 975, 988, 989, 992, 993, 986, 987}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171220573"
+v6[37] = v4
+v4.Name = "Xmas Song Pack"
+v4.SongKeys = {999, 1000, 1009, 1010, 1014, 1015, 1029, 1030}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171220573"
+v6[38] = v4
+v4.Name = "Tobu Song Pack"
+v4.SongKeys = {58, 59, 1071, 1072, 1066, 1067, 1053, 1054}
+v4.CoverImg = "http://www.roblox.com/asset/?id=6171220573"
+v6[39] = v4
+local u7 = v4()
