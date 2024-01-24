8fd2e7ea002f4a7437cea46fbd8f5f52aff90858
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-24 16:32:30 +1100

    Create ColorTierMaterial.lua

---
+++ GameData/RoBeats [698448212]/ColorTierMaterial.lua
@@ -0,0 +1,10 @@
+data = {
+    ["get_color_and_tier_for_material_id"] = "function: 0x00000000e53e347b",
+    ["gold_token_id"] = "function: 0x000000003a77a55b",
+    ["get_invalid_material_id"] = "function: 0x00000000ea4c0b0b",
+    ["get_dict"] = "function: 0x00000000c5c411a3",
+    ["get_token_id_set"] = "function: 0x00000000b8f0235b",
+    ["silver_token_id"] = "function: 0x00000000446e0053",
+    ["brass_token_id"] = "function: 0x00000000de53317b",
+    ["color_and_tier_to_material_id"] = "function: 0x00000000f28c5f33"
+}
