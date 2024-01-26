be10c0fd773dccb3a8d0ea73858bd377bae5993e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:33:22 +1100

    Create LeaderboardRewardsModule.lua

---
+++ GameData/Elemental Dungeons [10515146389]/LeaderboardRewardsModule.lua
@@ -0,0 +1,63 @@
+data = {
+    ["DamageDone"] = {
+        {
+            ["Place"] = 1,
+            ["Type"] = "Top",
+            ["Rewards"] = {
+                {["Type"] = "Currencies", ["Base"] = "Gems", ["Amount"] = 10000},
+                {["ItemType"] = "Titles", ["Type"] = "Titles", ["Base"] = "Master", ["Amount"] = 1}
+            }
+        },
+        {
+            ["Place"] = 10,
+            ["Type"] = "Top",
+            ["Rewards"] = {
+                {["ItemType"] = "Weapons", ["Type"] = "Cosmetics", ["Base"] = "Violet Battleaxe", ["Amount"] = 1},
+                {["Type"] = "Currencies", ["Base"] = "Gems", ["Amount"] = 5000},
+                {["ItemType"] = "Titles", ["Type"] = "Titles", ["Base"] = "Fan", ["Amount"] = 1}
+            }
+        },
+        {
+            ["Place"] = 100,
+            ["Type"] = "Top",
+            ["Rewards"] = {
+                {["ItemType"] = "Weapons", ["Type"] = "Cosmetics", ["Base"] = "Glacial Deathadder", ["Amount"] = 1},
+                {["Type"] = "Currencies", ["Base"] = "Gems", ["Amount"] = 2000},
+                {["ItemType"] = "Titles", ["Type"] = "Titles", ["Base"] = "TheChampion", ["Amount"] = 1}
+            }
+        },
+        {
+            ["Place"] = 1000,
+            ["Type"] = "Top",
+            ["Rewards"] = {
+                {["ItemType"] = "Weapons", ["Type"] = "Cosmetics", ["Base"] = "Lavawood Spear", ["Amount"] = 1},
+                {["Type"] = "Currencies", ["Base"] = "Gems", ["Amount"] = 1000}
+            }
+        },
+        {
+            ["Place"] = 2000,
+            ["Type"] = "Top",
+            ["Rewards"] = {{["Type"] = "Currencies", ["Base"] = "Gems", ["Amount"] = 500}}
+        },
+        {
+            ["Place"] = 0.5,
+            ["Type"] = "Percentile",
+            ["Rewards"] = {{["Type"] = "Currencies", ["Base"] = "Gems", ["Amount"] = 250}}
+        },
+        {
+            ["Place"] = 1,
+            ["Type"] = "Percentile",
+            ["Rewards"] = {{["Type"] = "Currencies", ["Base"] = "Gems", ["Amount"] = 150}}
+        },
+        {
+            ["Place"] = 5,
+            ["Type"] = "Percentile",
+            ["Rewards"] = {{["Type"] = "Currencies", ["Base"] = "Gems", ["Amount"] = 100}}
+        },
+        {
+            ["Place"] = 25,
+            ["Type"] = "Percentile",
+            ["Rewards"] = {{["Type"] = "Currencies", ["Base"] = "Gems", ["Amount"] = 50}}
+        }
+    }
+}
