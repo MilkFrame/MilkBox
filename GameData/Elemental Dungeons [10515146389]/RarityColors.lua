15b2f157caa1277b26c7538fff2970e31b94f600
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:35:16 +1100

    Create RarityColors.lua

---
+++ GameData/Elemental Dungeons [10515146389]/RarityColors.lua
@@ -0,0 +1,10 @@
+data = {
+    ["Misc"] = Color3.fromRGB(117, 117, 117),
+    ["Legendary"] = Color3.fromRGB(255, 246, 116),
+    ["Secret"] = Color3.fromRGB(84, 0, 126),
+    ["Common"] = Color3.fromRGB(255, 239, 246),
+    ["Epic"] = Color3.fromRGB(230, 126, 255),
+    ["Mythic"] = Color3.fromRGB(255, 0, 4),
+    ["Uncommon"] = Color3.fromRGB(110, 255, 107),
+    ["Rare"] = Color3.fromRGB(110, 240, 255)
+}
