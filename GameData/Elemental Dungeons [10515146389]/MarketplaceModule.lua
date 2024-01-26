dfe6c0a3dd2c1439c75ffe9fdb5f810113068e3f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:33:52 +1100

    Create MarketplaceModule.lua

---
+++ GameData/Elemental Dungeons [10515146389]/MarketplaceModule.lua
@@ -0,0 +1,159 @@
+data = {
+    ["ActiveProducts"] = {
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Amount"] = 10},
+            ["RewardFunction"] = "PurchaseTraitRerolls"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {},
+            ["RewardFunction"] = "PaidReroll"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "Gamepass",
+            ["ExtraData"] = {},
+            ["RewardFunction"] = "Award2xLoot"
+        },
+        {["RunRewardFunctionOnJoin"] = false, ["Type"] = "Gamepass", ["ExtraData"] = {}, ["RewardFunction"] = ""},
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Base"] = "SuperLucky", ["Amount"] = 1},
+            ["RewardFunction"] = "AddStoredBoosts"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Base"] = "2xExp", ["Amount"] = 86400},
+            ["RewardFunction"] = "AddRegBoosts"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Name"] = "Phantom Case", ["Amount"] = 10},
+            ["RewardFunction"] = "BuyCase"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {},
+            ["RewardFunction"] = "RespawnPlayer"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Base"] = "2xExp", ["Amount"] = 43200},
+            ["RewardFunction"] = "AddRegBoosts"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Base"] = "2xExp", ["Amount"] = 21600},
+            ["RewardFunction"] = "AddRegBoosts"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Name"] = "Phantom Case", ["Amount"] = 1},
+            ["RewardFunction"] = "BuyCase"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {},
+            ["RewardFunction"] = "RefundSkillPoints"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Base"] = "2xExp", ["Amount"] = 3600},
+            ["RewardFunction"] = "AddRegBoosts"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Amount"] = 50},
+            ["RewardFunction"] = "PurchaseGems"
+        },
+        {["RunRewardFunctionOnJoin"] = false, ["Type"] = "Gamepass", ["ExtraData"] = {}, ["RewardFunction"] = ""},
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Base"] = "UltraLuckyBannerBoost", ["Amount"] = 1},
+            ["RewardFunction"] = "AddBoosts"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "Gamepass",
+            ["ExtraData"] = {["Amount"] = 50},
+            ["RewardFunction"] = "AwardStorage"
+        },
+        {["RunRewardFunctionOnJoin"] = false, ["Type"] = "Gamepass", ["ExtraData"] = {}, ["RewardFunction"] = ""},
+        {["RunRewardFunctionOnJoin"] = false, ["Type"] = "Gamepass", ["ExtraData"] = {}, ["RewardFunction"] = ""},
+        {["RunRewardFunctionOnJoin"] = true, ["Type"] = "Gamepass", ["ExtraData"] = {}, ["RewardFunction"] = "AwardVIP"},
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {},
+            ["RewardFunction"] = "PurchaseStarterPack"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Base"] = "SuperLuckyBannerBoost", ["Amount"] = 1},
+            ["RewardFunction"] = "AddBoosts"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Base"] = "UltraLucky", ["Amount"] = 1},
+            ["RewardFunction"] = "AddStoredBoosts"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Amount"] = 925},
+            ["RewardFunction"] = "PurchaseGems"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Amount"] = 250},
+            ["RewardFunction"] = "PurchaseGems"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Name"] = "Phantom Case", ["Amount"] = 3},
+            ["RewardFunction"] = "BuyCase"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Amount"] = 475},
+            ["RewardFunction"] = "PurchaseGems"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Amount"] = 2000},
+            ["RewardFunction"] = "PurchaseGems"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Base"] = "2xExp", ["Amount"] = 900},
+            ["RewardFunction"] = "AddRegBoosts"
+        },
+        {
+            ["RunRewardFunctionOnJoin"] = false,
+            ["Type"] = "DevProduct",
+            ["ExtraData"] = {["Amount"] = 1},
+            ["RewardFunction"] = "PurchaseTraitRerolls"
+        }
+    }
+}
