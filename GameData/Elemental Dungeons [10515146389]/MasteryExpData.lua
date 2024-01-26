0adf085f50f9c1d026cd4c1f1fa8b403a1a06753
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:34:07 +1100

    Create MasteryExpData.lua

---
+++ GameData/Elemental Dungeons [10515146389]/MasteryExpData.lua
@@ -0,0 +1,12 @@
+data = {
+    ["GetVisualExp"] = "function: 0x0000000032b9ccd0",
+    ["BuffPerLevel"] = 0.05,
+    ["GetLevelFromExp"] = "function: 0x000000008134cef8",
+    ["GetLevelBuff"] = "function: 0x00000000f9130500",
+    ["GetVisualExpRequired"] = "function: 0x000000004e602a90",
+    ["GetSkillPoints"] = "function: 0x000000008b1e75f8",
+    ["GetExpRequired"] = "function: 0x000000005d45f048",
+    ["GetExpBuff"] = "function: 0x00000000537e5568",
+    ["RateIncrease"] = 2.7,
+    ["BaseItemExp"] = 250
+}
