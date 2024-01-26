479222d668ba7a33de83c8b29b6369e8ea0daea9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:39:18 +1100

    Create UniverseModule.lua

---
+++ GameData/Elemental Dungeons [10515146389]/UniverseModule.lua
@@ -0,0 +1,12 @@
+data = {
+    ["IsAFK"] = "function: 0x0000000036f33e40",
+    ["IsGame"] = "function: 0x000000008e5ffc38",
+    ["IsLobby"] = "function: 0x000000006234b6f0",
+    ["PVPIds"] = {["Prod"] = 14013060539, ["Testing"] = 14638460051},
+    ["GameIds"] = {["Prod"] = 10771129745, ["Testing"] = 14001943864},
+    ["Find"] = "function: 0x00000000f3c5d988",
+    ["AFKIds"] = {["Prod"] = 13735670606, ["Testing"] = 14001945026},
+    ["IsPVP"] = "function: 0x000000005a9041a8",
+    ["LobbyIds"] = {["Prod"] = 10515146389, ["Testing"] = 14001922359},
+    ["GetCurrentPlaceData"] = "function: 0x0000000081b1b190"
+}
