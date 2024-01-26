d7b833bf01d66074cf9e8429ee0fcc7ce3989724
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:30:15 +1100

    Create CodesSettings.lua

---
+++ GameData/Elemental Dungeons [10515146389]/CodesSettings.lua
@@ -0,0 +1,19 @@
+data = {
+    ["NegativeResponseColor"] = Color3.fromRGB(255, 84, 86),
+    ["Responses"] = {
+        ["Valid"] = "Code redeemed",
+        ["Error"] = "Something errored!",
+        ["SoldOut"] = "Ran out of copies",
+        ["Invalid"] = "This is not a code",
+        ["Expired"] = "Expired",
+        ["EmptyField"] = "No code was entered",
+        ["Redeemed"] = "Already used",
+        ["TooFast"] = "Please slow down"
+    },
+    ["CopiesRefeshTime"] = 60,
+    ["PositiveResponseColor"] = Color3.fromRGB(152, 255, 133),
+    ["CaseSensitive"] = false,
+    ["IgnoreIfEmpty"] = true,
+    ["DebounceTime"] = 3,
+    ["ExpirationDateShown"] = true
+}
