f0b8f437bb11e42624c771d37dd0328ff16328a9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:34:24 +1100

    Create MaterialsModule.lua

---
+++ GameData/Elemental Dungeons [10515146389]/MaterialsModule.lua
@@ -0,0 +1,141 @@
+data = {
+    ["Atlantis Shard"] = {
+        ["Icon"] = "rbxassetid://13750790390",
+        ["Rarity"] = "Legendary",
+        ["Name"] = "Atlantis Shard",
+        ["Desc"] = "An Atlantis shard."
+    },
+    ["Frog Legs"] = {
+        ["Icon"] = "rbxassetid://12830967573",
+        ["Rarity"] = "Epic",
+        ["Name"] = "Frog Legs",
+        ["Desc"] = "Freshly harvested frog legs known for their incredible jumping abilities."
+    },
+    ["Yeti Fur"] = {
+        ["Icon"] = "rbxassetid://12830970871",
+        ["Rarity"] = "Legendary",
+        ["Name"] = "Yeti Fur",
+        ["Desc"] = "Thick and warm fur obtained from a legendary creature known as the Yeti."
+    },
+    ["Obsidian"] = {
+        ["Icon"] = "rbxassetid://12830974941",
+        ["Rarity"] = "Legendary",
+        ["Name"] = "Obsidian",
+        ["Desc"] = "A dark and glossy volcanic glass formed from rapidly cooled lava."
+    },
+    ["Coal"] = {
+        ["Icon"] = "rbxassetid://12699368304",
+        ["Rarity"] = "Uncommon",
+        ["Name"] = "Coal",
+        ["Desc"] = "A common and essential resource."
+    },
+    ["Sandstone"] = {
+        ["Icon"] = "rbxassetid://12830991922",
+        ["Rarity"] = "Uncommon",
+        ["Name"] = "Sandstone",
+        ["Desc"] = "A sedimentary rock formed from compressed sand particles."
+    },
+    ["Ruby"] = {
+        ["Icon"] = "rbxassetid://12830985436",
+        ["Rarity"] = "Legendary",
+        ["Name"] = "Ruby",
+        ["Desc"] = "A brilliant red gemstone associated with fire and passion."
+    },
+    ["Amethyst"] = {
+        ["Icon"] = "rbxassetid://12830949896",
+        ["Rarity"] = "Epic",
+        ["Name"] = "Amethyst",
+        ["Desc"] = "A mystical gemstone imbued with magical properties"
+    },
+    ["Evil Amulet"] = {
+        ["Icon"] = "rbxassetid://12830962613",
+        ["Rarity"] = "Legendary",
+        ["Name"] = "Evil Amulet",
+        ["Desc"] = "A cursed amulet with dark energy pulsating through it."
+    },
+    ["Jungle Shard"] = {
+        ["Icon"] = "rbxassetid://13750779932",
+        ["Rarity"] = "Legendary",
+        ["Name"] = "Jungle Shard",
+        ["Desc"] = "A jungle shard."
+    },
+    ["Diamond"] = {
+        ["Icon"] = "rbxassetid://12830959984",
+        ["Rarity"] = "Legendary",
+        ["Name"] = "Diamond",
+        ["Desc"] = "A rare and valuable gemstone coveted by adventurers and jewelers alike."
+    },
+    ["Obsidian Amulet"] = {
+        ["Icon"] = "rbxassetid://12830977770",
+        ["Rarity"] = "Mythic",
+        ["Name"] = "Obsidian Amulet",
+        ["Desc"] = "An amulet crafted from obsidian, infused with protective enchantments."
+    },
+    ["Lunar Shard"] = {
+        ["Icon"] = "rbxassetid://14676534795",
+        ["Rarity"] = "Epic",
+        ["Name"] = "Lunar Shard",
+        ["Desc"] = "A Lunar Shard."
+    },
+    ["Volcanic Shard"] = {
+        ["Icon"] = "rbxassetid://13750799513",
+        ["Rarity"] = "Legendary",
+        ["Name"] = "Volcanic Shard",
+        ["Desc"] = "A volcanic shard."
+    },
+    ["Evil Drive"] = {
+        ["Name"] = "Evil Drive",
+        ["Unsellable"] = true,
+        ["Rarity"] = "Epic",
+        ["Icon"] = "rbxassetid://14694720098",
+        ["Desc"] = "An Evil Drive."
+    },
+    ["Vines"] = {
+        ["Icon"] = "rbxassetid://12830996316",
+        ["Rarity"] = "Uncommon",
+        ["Name"] = "Vines",
+        ["Desc"] = "Strong and flexible vines obtained from lush jungles."
+    },
+    ["Saber Tooth"] = {
+        ["Icon"] = "rbxassetid://12830988159",
+        ["Rarity"] = "Epic",
+        ["Name"] = "Saber Tooth",
+        ["Desc"] = "A fearsome curved canine tooth from an extinct saber-toothed creature."
+    },
+    ["Cactus"] = {
+        ["Icon"] = "rbxassetid://12830957115",
+        ["Rarity"] = "Epic",
+        ["Name"] = "Cactus",
+        ["Desc"] = "A prickly plant found in arid environments."
+    },
+    ["Prismarine"] = {
+        ["Icon"] = "rbxassetid://12830980360",
+        ["Rarity"] = "Epic",
+        ["Name"] = "Prismarine",
+        ["Desc"] = "A shimmering blue mineral found deep underwater."
+    },
+    ["Scrap Metal"] = {
+        ["Icon"] = "rbxassetid://12709836134",
+        ["Rarity"] = "Common",
+        ["Name"] = "Scrap Metal",
+        ["Desc"] = "Discarded pieces of metal salvaged from various sources."
+    },
+    ["Corrupted Shard"] = {
+        ["Icon"] = "rbxassetid://13588166823",
+        ["Rarity"] = "Mythic",
+        ["Name"] = "Corrupted Shard",
+        ["Desc"] = "A corrupted shard with corruption pulsating through it."
+    },
+    ["Ice Shard"] = {
+        ["Icon"] = "rbxassetid://13750786801",
+        ["Rarity"] = "Legendary",
+        ["Name"] = "Ice Shard",
+        ["Desc"] = "An ice shard."
+    },
+    ["Sand Shard"] = {
+        ["Icon"] = "rbxassetid://13750697149",
+        ["Rarity"] = "Legendary",
+        ["Name"] = "Sand Shard",
+        ["Desc"] = "A sand shard."
+    }
+}
