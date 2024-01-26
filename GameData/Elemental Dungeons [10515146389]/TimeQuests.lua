ed8205e07fc1ff93198900f470010c245c75511a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:38:02 +1100

    Create TimeQuests.lua

---
+++ GameData/Elemental Dungeons [10515146389]/TimeQuests.lua
@@ -0,0 +1,372 @@
+data = {
+    ["Weekly"] = {
+        ["KillMobsInSpecificDungeon"] = {
+            ["Stages"] = {
+                {
+                    ["Progress"] = 4000,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 350
+                },
+                {
+                    ["Progress"] = 6500,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 450
+                },
+                {
+                    ["Progress"] = 13000,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 500
+                }
+            },
+            ["CustomData"] = {},
+            ["Title"] = "Slayer",
+            ["Desc"] = "Kill %s enemies in %s"
+        },
+        ["ClearWavesInfiniteModeInSpecificDungeon"] = {
+            ["Stages"] = {
+                {
+                    ["Progress"] = 5,
+                    ["Identifier"] = 15,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 350
+                },
+                {
+                    ["Progress"] = 5,
+                    ["Identifier"] = 30,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 450
+                },
+                {
+                    ["Progress"] = 5,
+                    ["Identifier"] = 45,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 600
+                }
+            },
+            ["CustomData"] = {},
+            ["Title"] = "Wave Master",
+            ["Desc"] = "Clear %s waves in %s Infinite Mode"
+        },
+        ["KillMobs"] = {
+            ["Stages"] = {
+                {
+                    ["Progress"] = 6000,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 350
+                },
+                {
+                    ["Progress"] = 7500,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 450
+                },
+                {
+                    ["Progress"] = 15000,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 500
+                }
+            },
+            ["Title"] = "Slayer",
+            ["Desc"] = "Kill %s enemies"
+        },
+        ["ClearDungeonsWithFriends"] = {
+            ["Stages"] = {
+                {
+                    ["Progress"] = 25,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 350
+                },
+                {
+                    ["Progress"] = 50,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 450
+                },
+                {
+                    ["Progress"] = 100,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 700
+                }
+            },
+            ["Title"] = "Social Dungeon Master",
+            ["Desc"] = "Clear %s Dungeons with a friend"
+        },
+        ["ClearWavesInfiniteMode"] = {
+            ["Stages"] = {
+                {
+                    ["Progress"] = 5,
+                    ["Identifier"] = 15,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 350
+                },
+                {
+                    ["Progress"] = 5,
+                    ["Identifier"] = 30,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 450
+                },
+                {
+                    ["Progress"] = 5,
+                    ["Identifier"] = 45,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 600
+                }
+            },
+            ["CustomData"] = {},
+            ["Title"] = "Wave Master",
+            ["Desc"] = "Clear %s waves in Infinite Mode"
+        },
+        ["ClearDungeons"] = {
+            ["Stages"] = {
+                {
+                    ["Progress"] = 25,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 350
+                },
+                {
+                    ["Progress"] = 50,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 450
+                },
+                {
+                    ["Progress"] = 125,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 500
+                }
+            },
+            ["Title"] = "Dungeon Master",
+            ["Desc"] = "Clear %s Dungeons"
+        },
+        ["KillMobsWithSword"] = {
+            ["Stages"] = {
+                {
+                    ["Progress"] = 4500,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 350
+                },
+                {
+                    ["Progress"] = 5500,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 450
+                },
+                {
+                    ["Progress"] = 8000,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 500
+                }
+            },
+            ["Title"] = "Swordsman",
+            ["Desc"] = "Kill %s enemies with a weapon"
+        },
+        ["KillMobsWithElement"] = {
+            ["Stages"] = {
+                {
+                    ["Progress"] = 4500,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 350
+                },
+                {
+                    ["Progress"] = 5500,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 450
+                },
+                {
+                    ["Progress"] = 8000,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 500
+                }
+            },
+            ["Title"] = "Wizard",
+            ["Desc"] = "Kill %s enemies with an element"
+        }
+    },
+    ["Daily"] = {
+        ["KillMobs"] = {
+            ["Stages"] = {
+                {
+                    ["Progress"] = 100,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 20
+                },
+                {
+                    ["Progress"] = 300,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 30
+                },
+                {
+                    ["Progress"] = 500,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 50
+                }
+            },
+            ["Title"] = "Slayer",
+            ["Desc"] = "Kill %s enemies"
+        },
+        ["ClearWavesInfiniteMode"] = {
+            ["Stages"] = {
+                {
+                    ["Progress"] = 1,
+                    ["Identifier"] = 10,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 30
+                },
+                {
+                    ["Progress"] = 1,
+                    ["Identifier"] = 15,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 40
+                },
+                {
+                    ["Progress"] = 1,
+                    ["Identifier"] = 30,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 60
+                }
+            },
+            ["CustomData"] = {},
+            ["Title"] = "Wave Master",
+            ["Desc"] = "Clear %s waves in Infinite Mode"
+        },
+        ["ClearWavesInfiniteModeInSpecificDungeon"] = {
+            ["Stages"] = {
+                {
+                    ["Progress"] = 1,
+                    ["Identifier"] = 10,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 40
+                },
+                {
+                    ["Progress"] = 1,
+                    ["Identifier"] = 15,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 60
+                },
+                {
+                    ["Progress"] = 1,
+                    ["Identifier"] = 30,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 100
+                }
+            },
+            ["CustomData"] = {},
+            ["Title"] = "Wave Master",
+            ["Desc"] = "Clear %s waves in %s Infinite Mode"
+        },
+        ["KillMobsWithSword"] = {
+            ["Stages"] = {
+                {
+                    ["Progress"] = 140,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 20
+                },
+                {
+                    ["Progress"] = 250,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 30
+                },
+                {
+                    ["Progress"] = 400,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 50
+                }
+            },
+            ["Title"] = "Swordsman",
+            ["Desc"] = "Kill %s enemies with a weapon"
+        },
+        ["ClearDungeonsWithFriends"] = {
+            ["Stages"] = {
+                {
+                    ["Progress"] = 5,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 20
+                },
+                {
+                    ["Progress"] = 10,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 40
+                },
+                {
+                    ["Progress"] = 20,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 70
+                }
+            },
+            ["Title"] = "Social Dungeon Master",
+            ["Desc"] = "Clear %s Dungeons with a friend"
+        },
+        ["DailyQuests"] = {
+            ["CantRoll"] = true,
+            ["Stages"] = {
+                {
+                    ["Progress"] = 4,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "200 Beta Exp", ["Amount"] = 200}},
+                    ["Reward"] = 400
+                }
+            },
+            ["Title"] = "Daily Quests Completionist",
+            ["Desc"] = "Complete %s Daily Quests"
+        },
+        ["ClearDungeons"] = {
+            ["Stages"] = {
+                {
+                    ["Progress"] = 5,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 20
+                },
+                {
+                    ["Progress"] = 10,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 40
+                },
+                {
+                    ["Progress"] = 20,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 70
+                }
+            },
+            ["Title"] = "Dungeon Master",
+            ["Desc"] = "Clear %s Dungeons"
+        },
+        ["KillMobsInSpecificDungeon"] = {
+            ["Stages"] = {
+                {
+                    ["Progress"] = 100,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 40
+                },
+                {
+                    ["Progress"] = 300,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 60
+                },
+                {
+                    ["Progress"] = 500,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 80
+                }
+            },
+            ["CustomData"] = {},
+            ["Title"] = "Slayer",
+            ["Desc"] = "Kill %s enemies in %s"
+        },
+        ["KillMobsWithElement"] = {
+            ["Stages"] = {
+                {
+                    ["Progress"] = 80,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 20
+                },
+                {
+                    ["Progress"] = 225,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 30
+                },
+                {
+                    ["Progress"] = 300,
+                    ["OtherRewards"] = {{["Type"] = "SeasonExp", ["Title"] = "50 Beta Exp", ["Amount"] = 50}},
+                    ["Reward"] = 50
+                }
+            },
+            ["Title"] = "Wizard",
+            ["Desc"] = "Kill %s enemies with an element"
+        }
+    }
+}
