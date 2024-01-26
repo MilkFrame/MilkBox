7b4e9788ee1135281d2157d4ba153932e63bdb72
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:18:43 +1100

    Create CaseModule.lua

---
+++ GameData/Elemental Dungeons [10515146389]/CaseModule.lua
@@ -0,0 +1,22 @@
+data = {
+    ["ShowCase"] = "function: 0x000000000d061060",
+    ["Phantom Case"] = {
+        ["Name"] = "Phantom Case",
+        ["Drops"] = {
+            ["Sapphire Battleaxe"] = {["Chance"] = 10, ["Type"] = "Weapons", ["Base"] = "Sapphire Battleaxe"},
+            ["Beta Scythe"] = {["Chance"] = 5, ["Type"] = "Weapons", ["Base"] = "Beta Scythe"},
+            ["Iridescent Frostbite"] = {["Chance"] = 20, ["Type"] = "Weapons", ["Base"] = "Iridescent Frostbite"},
+            ["Red Leggings"] = {["Chance"] = 2, ["Type"] = "Armors", ["Base"] = "Red Leggings"},
+            ["Beta Hammer"] = {["Chance"] = 7, ["Type"] = "Weapons", ["Base"] = "Beta Hammer"},
+            ["Beta Daggers"] = {["Chance"] = 5, ["Type"] = "Weapons", ["Base"] = "Beta Daggers"},
+            ["Blazebringer"] = {["Chance"] = 10, ["Type"] = "Weapons", ["Base"] = "Blazebringer"},
+            ["Ivory Sandslash"] = {["Chance"] = 30, ["Type"] = "Weapons", ["Base"] = "Ivory Sandslash"},
+            ["Beta Battleaxe"] = {["Chance"] = 7, ["Type"] = "Weapons", ["Base"] = "Beta Battleaxe"},
+            ["Red Chestplate"] = {["Chance"] = 2, ["Type"] = "Armors", ["Base"] = "Red Chestplate"},
+            ["Red Helmet"] = {["Chance"] = 2, ["Type"] = "Armors", ["Base"] = "Red Helmet"}
+        },
+        ["Rarity"] = "Legendary",
+        ["Icon"] = "rbxassetid://14982688594",
+        ["CaseImage"] = "rbxassetid://14982688594"
+    }
+}
