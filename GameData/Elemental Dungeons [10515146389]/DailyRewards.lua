d900ee53f264da606e153402de1f5d9b5211e337
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:31:04 +1100

    Create DailyRewards.lua

---
+++ GameData/Elemental Dungeons [10515146389]/DailyRewards.lua
@@ -0,0 +1,9 @@
+data = {
+    {["Amount"] = 50, ["Icon"] = "rbxassetid://13425059084", ["Reward"] = "Gems"},
+    {["Amount"] = 150, ["Icon"] = "rbxassetid://13425059084", ["Reward"] = "Gems"},
+    {["Amount"] = 150, ["Icon"] = "rbxassetid://13425059084", ["Reward"] = "Gems"},
+    {["Amount"] = 150, ["Icon"] = "rbxassetid://13425059084", ["Reward"] = "Gems"},
+    {["Amount"] = 200, ["Icon"] = "rbxassetid://13425059084", ["Reward"] = "Gems"},
+    {["Amount"] = 250, ["Icon"] = "rbxassetid://13425059084", ["Reward"] = "Gems"},
+    {["SecondaryReward"] = "Boost", ["Amount"] = 300, ["Icon"] = "rbxassetid://13425059084", ["Reward"] = "Gems"}
+}
