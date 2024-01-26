a3eb727f937117affb5087fdc583eea624c2c40e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:18:27 +1100

    Create Boosts.lua

---
+++ GameData/Elemental Dungeons [10515146389]/Boosts.lua
@@ -0,0 +1,51 @@
+data = {
+    ["UltraLuckyBannerBoost"] = {
+        ["ActualTitle"] = "Ultra Lucky",
+        ["Type"] = "BannerLuck",
+        ["Title"] = "Ultra Lucky (x%s Banner)",
+        ["Length"] = 1,
+        ["Icon"] = "rbxassetid://13703535830",
+        ["Boost"] = 0.75
+    },
+    ["2xExp"] = {
+        ["ActualTitle"] = "2x Exp",
+        ["Type"] = "Exp",
+        ["Title"] = "2x Exp",
+        ["DontShow"] = true,
+        ["Boost"] = 1,
+        ["Icon"] = "rbxassetid://13703535830",
+        ["NormalBoost"] = true
+    },
+    ["SuperLuckyBannerBoost"] = {
+        ["ActualTitle"] = "Super Lucky",
+        ["Type"] = "BannerLuck",
+        ["Title"] = "Super Lucky (x%s Banner)",
+        ["Length"] = 1,
+        ["Icon"] = "rbxassetid://13588559357",
+        ["Boost"] = 0.25
+    },
+    ["DamageBoost50"] = {
+        ["ActualTitle"] = "+50% Damage",
+        ["Type"] = "Damage",
+        ["Title"] = "+50% Damage",
+        ["Boost"] = 0.5,
+        ["Icon"] = "rbxassetid://14682347680",
+        ["NormalBoost"] = true
+    },
+    ["LootLuck50"] = {
+        ["ActualTitle"] = "+50% Loot Luck",
+        ["Type"] = "LootLuck",
+        ["Title"] = "+50% Loot Luck",
+        ["Boost"] = 0.5,
+        ["Icon"] = "rbxassetid://14682613343",
+        ["NormalBoost"] = true
+    },
+    ["GoldBoost50"] = {
+        ["ActualTitle"] = "+50% Gold",
+        ["Type"] = "Gold",
+        ["Title"] = "+50% Gold",
+        ["Boost"] = 0.5,
+        ["Icon"] = "rbxassetid://14682353483",
+        ["NormalBoost"] = true
+    }
+}
