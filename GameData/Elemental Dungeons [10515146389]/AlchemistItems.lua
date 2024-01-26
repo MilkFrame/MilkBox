055bad1104049be36199d9dad4aa62825afbcb74
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:17:55 +1100

    Create AlchemistItems.lua

---
+++ GameData/Elemental Dungeons [10515146389]/AlchemistItems.lua
@@ -0,0 +1,7 @@
+data = {
+    ["Items"] = {
+        ["DamageBoost50"] = {["Price"] = 4, ["Title"] = "+50% Damage"},
+        ["LootLuck50"] = {["Price"] = 10, ["Title"] = "+50% Loot Luck"},
+        ["GoldBoost50"] = {["Price"] = 6, ["Title"] = "+50% Gold"}
+    }
+}
