9c0fddf057845a24e902f000da7351733866acdb
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:39:35 +1100

    Create WeaponsModule.lua

---
+++ GameData/Elemental Dungeons [10515146389]/WeaponsModule.lua
@@ -0,0 +1,525 @@
+data = {
+    ["Winter's Breath"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Winter's Breath",
+        ["Model"] = "Winter's Breath",
+        ["Rarity"] = "Rare",
+        ["Icon"] = "rbxassetid://14581538176",
+        ["Damage"] = 95
+    },
+    ["Solarflare Cutlass"] = {
+        ["ZoneRating"] = 4,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Solarflare Cutlass",
+        ["Model"] = "Solarflare Cutlass",
+        ["Rarity"] = "Rare",
+        ["Icon"] = "rbxassetid://14993877243",
+        ["Damage"] = 130
+    },
+    ["Nature Axe"] = {
+        ["ZoneRating"] = 2,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Nature Axe",
+        ["Model"] = "Nature Axe",
+        ["WeaponCooldown"] = 0.8,
+        ["Rarity"] = "Rare",
+        ["Icon"] = "rbxassetid://14742977747",
+        ["Damage"] = 35
+    },
+    ["Gold Sword"] = {
+        ["ZoneRating"] = 1,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Gold Sword",
+        ["SalvageCost"] = {["Amount"] = 1, ["Material"] = "Sand Shard"},
+        ["Model"] = "Gold Sword",
+        ["Rarity"] = "Epic",
+        ["Icon"] = "rbxassetid://12938860309",
+        ["Damage"] = 30
+    },
+    ["Poseidon's Trident"] = {
+        ["ZoneRating"] = 4,
+        ["Rarity"] = "Mythic",
+        ["Animations"] = {"rbxassetid://14987581634", "rbxassetid://10891053151"},
+        ["PowerType"] = "Physical",
+        ["Name"] = "Poseidon's Trident",
+        ["Model"] = "Poseidon's Trident",
+        ["IdleAnim"] = "rbxassetid://14988554532",
+        ["Abilities"] = "Poseidon's Trident",
+        ["Icon"] = "rbxassetid://14988105775",
+        ["Damage"] = 250
+    },
+    ["Gold Longsword"] = {
+        ["ZoneRating"] = 2,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Gold Longsword",
+        ["Model"] = "Gold Longsword",
+        ["Rarity"] = "Rare",
+        ["Icon"] = "rbxassetid://12938873924",
+        ["Damage"] = 31
+    },
+    ["Viridescent Cutlass"] = {
+        ["ZoneRating"] = 4,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Viridescent Cutlass",
+        ["Model"] = "Viridescent Cutlass",
+        ["Rarity"] = "Rare",
+        ["Icon"] = "rbxassetid://14993876840",
+        ["Damage"] = 95
+    },
+    ["Bronze Sword"] = {
+        ["ZoneRating"] = 1,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Bronze Sword",
+        ["Model"] = "Bronze Sword",
+        ["Rarity"] = "Uncommon",
+        ["Icon"] = "rbxassetid://12938851884",
+        ["Damage"] = 13
+    },
+    ["Wooden Staff"] = {
+        ["PowerType"] = "Magic",
+        ["Name"] = [[Wooden Staff"]],
+        ["Model"] = "Wooden Sword",
+        ["Rarity"] = "Common",
+        ["Icon"] = "http://www.roblox.com/asset/?id=12522835901",
+        ["Damage"] = 0
+    },
+    ["Chilly Sword"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Chilly Sword",
+        ["Model"] = "Chilly Sword",
+        ["Rarity"] = "Rare",
+        ["Icon"] = "rbxassetid://14581714508",
+        ["Damage"] = 75
+    },
+    ["Beta Battleaxe"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Beta Battleaxe",
+        ["Model"] = "Beta Battleaxe",
+        ["Rarity"] = "Mythic",
+        ["Icon"] = "rbxassetid://14875386672",
+        ["Damage"] = 1
+    },
+    ["Sapphire Battleaxe"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Sapphire Battleaxe",
+        ["Model"] = "Sapphire Battleaxe",
+        ["Rarity"] = "Legendary",
+        ["Icon"] = "rbxassetid://15002344697",
+        ["Damage"] = 1
+    },
+    ["Broadsword"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Broadsword",
+        ["Model"] = "Broadsword",
+        ["Rarity"] = "Rare",
+        ["Icon"] = "rbxassetid://14581710611",
+        ["Damage"] = 60
+    },
+    ["Bone Sword"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Bone Sword",
+        ["Model"] = "Bone Sword",
+        ["Rarity"] = "Rare",
+        ["Icon"] = "rbxassetid://14581707841",
+        ["Damage"] = 80
+    },
+    ["Silvermist Cutlass"] = {
+        ["ZoneRating"] = 4,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Silvermist Cutlass",
+        ["Model"] = "Silvermist Cutlass",
+        ["Rarity"] = "Rare",
+        ["Icon"] = "rbxassetid://15028868175",
+        ["Damage"] = 95
+    },
+    ["Dual Daggers"] = {
+        ["ZoneRating"] = 1,
+        ["Rarity"] = "Rare",
+        ["DualWield"] = true,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Dual Daggers",
+        ["Model"] = "Dual Daggers",
+        ["WeaponCooldown"] = 0.4,
+        ["WeaponLength"] = 0.2,
+        ["Icon"] = "rbxassetid://14742989396",
+        ["Damage"] = 24
+    },
+    ["Vine Sword"] = {
+        ["ZoneRating"] = 2,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Vine Sword",
+        ["Model"] = "Vine Sword",
+        ["Rarity"] = "Uncommon",
+        ["Icon"] = "rbxassetid://12938876445",
+        ["Damage"] = 24
+    },
+    ["Lavawood Spear"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Lavawood Spear",
+        ["Model"] = "Lavawood Spear",
+        ["Rarity"] = "Mythic",
+        ["Icon"] = "rbxassetid://14581704547",
+        ["Damage"] = 1
+    },
+    ["Basic Cutlass"] = {
+        ["ZoneRating"] = 2,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Basic Cutlass",
+        ["Model"] = "Basic Cutlass",
+        ["Rarity"] = "Rare",
+        ["Icon"] = "rbxassetid://12938870490",
+        ["Damage"] = 30
+    },
+    ["Beta Daggers"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Beta Daggers",
+        ["Model"] = "Beta Daggers",
+        ["DualWield"] = true,
+        ["Rarity"] = "Mythic",
+        ["Icon"] = "rbxassetid://14875347250",
+        ["Damage"] = 1
+    },
+    ["Diamond Sword"] = {
+        ["ZoneRating"] = 2,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Diamond Sword",
+        ["Model"] = "Diamond Sword",
+        ["Rarity"] = "Epic",
+        ["Icon"] = "rbxassetid://12938872375",
+        ["Damage"] = 33
+    },
+    ["Pheonix Blade"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Phoenix Blade",
+        ["Model"] = "Pheonix Blade",
+        ["Rarity"] = "Rare",
+        ["Icon"] = "rbxassetid://14581538015",
+        ["Damage"] = 65
+    },
+    ["Primal Sword"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Primal Sword",
+        ["Model"] = "Primal Sword",
+        ["Rarity"] = "Epic",
+        ["Icon"] = "rbxassetid://14581537258",
+        ["Damage"] = 105
+    },
+    ["Lightning Katana"] = {
+        ["ZoneRating"] = 3,
+        ["Rarity"] = "Mythic",
+        ["PowerType"] = "Physical",
+        ["Name"] = "Lightning Katana",
+        ["Model"] = "Lightning Katana",
+        ["IdleAnim"] = "rbxassetid://14988768265",
+        ["Abilities"] = "Lightning Katana",
+        ["Icon"] = "rbxassetid://15002955728",
+        ["Damage"] = 190
+    },
+    ["Iron Longsword"] = {
+        ["ZoneRating"] = 1,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Iron Longsword",
+        ["Model"] = "Iron Longsword",
+        ["Rarity"] = "Rare",
+        ["Icon"] = "rbxassetid://12938857735",
+        ["Damage"] = 20
+    },
+    ["Overseer's Edge"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Overseer's Edge",
+        ["Model"] = "Overseer's Edge",
+        ["Rarity"] = "Epic",
+        ["Icon"] = "rbxassetid://15002344841",
+        ["Damage"] = 1
+    },
+    ["Blazebringer"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Blazebringer",
+        ["Model"] = "Blazebringer",
+        ["Rarity"] = "Epic",
+        ["Icon"] = "rbxassetid://15002345141",
+        ["Damage"] = 1
+    },
+    ["Obsidian Edge"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Obsidian Edge",
+        ["Model"] = "Obsidian Edge",
+        ["Rarity"] = "Epic",
+        ["Icon"] = "rbxassetid://15002345001",
+        ["Damage"] = 1
+    },
+    ["Bronze Longsword"] = {
+        ["ZoneRating"] = 1,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Bronze Longsword",
+        ["Model"] = "Bronze Longsword",
+        ["Rarity"] = "Uncommon",
+        ["Icon"] = "rbxassetid://12938854809",
+        ["Damage"] = 16
+    },
+    ["Seraphim's Edge"] = {
+        ["ZoneRating"] = 4,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Seraphim's Edge",
+        ["Model"] = "Seraphim's Edge",
+        ["Rarity"] = "Legendary",
+        ["Icon"] = "rbxassetid://14993857272",
+        ["Damage"] = 180
+    },
+    ["Neptunian Spear"] = {
+        ["ZoneRating"] = 4,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Neptunian Spear",
+        ["Model"] = "Neptunian Spear",
+        ["Rarity"] = "Legendary",
+        ["Icon"] = "rbxassetid://14993895107",
+        ["Damage"] = 160
+    },
+    ["Serpent's Blade"] = {
+        ["ZoneRating"] = 4,
+        ["Rarity"] = "Legendary",
+        ["Animations"] = {"rbxassetid://14995876369", "rbxassetid://14996063352", "rbxassetid://14996094668"},
+        ["PowerType"] = "Physical",
+        ["Name"] = "Serpent's Blade",
+        ["Model"] = "Serpent's Blade",
+        ["WeaponLength"] = 0.3,
+        ["WeaponCooldown"] = 0.6,
+        ["Icon"] = "rbxassetid://14993877637",
+        ["Damage"] = 200
+    },
+    ["Twilight Luster"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Twilight Luster",
+        ["Model"] = "Twilight Luster",
+        ["Rarity"] = "Legendary",
+        ["Icon"] = "rbxassetid://14581537478",
+        ["Damage"] = 140
+    },
+    ["Kraken's Abyssblade"] = {
+        ["ZoneRating"] = 4,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Kraken's Abyssblade",
+        ["Model"] = "Kraken's Abyssblade",
+        ["Rarity"] = "Epic",
+        ["Icon"] = "rbxassetid://14993878254",
+        ["Damage"] = 140
+    },
+    ["Beta Scythe"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Beta Scythe",
+        ["Model"] = "Beta Scythe",
+        ["Rarity"] = "Mythic",
+        ["Icon"] = "rbxassetid://14875344004",
+        ["Damage"] = 1
+    },
+    ["Voidblade"] = {
+        ["ZoneRating"] = 2,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Voidblade",
+        ["Model"] = "Voidblade",
+        ["Rarity"] = "Legendary",
+        ["Icon"] = "rbxassetid://14581537018",
+        ["Damage"] = 60
+    },
+    ["Fusion Blade"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Fusion Blade",
+        ["Model"] = "Fusion Blade",
+        ["ExtraM1Function"] = "Fusion",
+        ["Rarity"] = "Legendary",
+        ["Icon"] = "rbxassetid://13969098961",
+        ["Damage"] = 110
+    },
+    ["Menta Blade"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Menta Blade V1",
+        ["Model"] = "Menta Blade",
+        ["ExtraM1Function"] = "Elemental",
+        ["Rarity"] = "Mythic",
+        ["Icon"] = "rbxassetid://13969098796",
+        ["Damage"] = 170
+    },
+    ["Glacial Battleaxe"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Glacial Battleaxe",
+        ["Model"] = "Glacial Battleaxe",
+        ["Rarity"] = "Mythic",
+        ["Icon"] = "rbxassetid://14581720466",
+        ["Damage"] = 57
+    },
+    ["Beta Deathadder"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Beta Deathadder",
+        ["Model"] = "Beta Deathadder",
+        ["Rarity"] = "Mythic",
+        ["Icon"] = "rbxassetid://14762369405",
+        ["Damage"] = 1
+    },
+    ["Beta Axe"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Beta Deathadder",
+        ["Model"] = "Beta Axe",
+        ["Rarity"] = "Mythic",
+        ["Icon"] = "rbxassetid://14768424709",
+        ["Damage"] = 1
+    },
+    ["Glacial Deathadder"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Glacial Deathadder",
+        ["Model"] = "Glacial Deathadder",
+        ["Rarity"] = "Mythic",
+        ["Icon"] = "rbxassetid://14581734582",
+        ["Damage"] = 1
+    },
+    ["Wooden Sword"] = {
+        ["ZoneRating"] = 1,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Wooden Sword",
+        ["Model"] = "Wooden Sword",
+        ["Rarity"] = "Common",
+        ["Icon"] = "http://www.roblox.com/asset/?id=12522835901",
+        ["Damage"] = 6
+    },
+    ["Ruby Sword"] = {
+        ["ZoneRating"] = 2,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Ruby Sword",
+        ["Model"] = "Ruby Sword",
+        ["Rarity"] = "Legendary",
+        ["Icon"] = "rbxassetid://14629276213",
+        ["Damage"] = 42
+    },
+    ["Violet Battleaxe"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Violet Battleaxe",
+        ["Model"] = "Violet Battleaxe",
+        ["DualWield"] = true,
+        ["Rarity"] = "Mythic",
+        ["Icon"] = "rbxassetid://14581538393",
+        ["Damage"] = 1
+    },
+    ["Iridescent Frostbite"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Iridescent Frostbite",
+        ["Model"] = "Iridescent Frostbite",
+        ["Rarity"] = "Legendary",
+        ["Icon"] = "rbxassetid://14581537863",
+        ["Damage"] = 53
+    },
+    ["Steel Deathadder"] = {
+        ["ZoneRating"] = 2,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Steel Deathadder",
+        ["Model"] = "Steel Deathadder",
+        ["Rarity"] = "Uncommon",
+        ["Icon"] = "rbxassetid://14742956678",
+        ["Damage"] = 26
+    },
+    ["Ivory Sandslash"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Ivory Sandslash",
+        ["Model"] = "Ivory Sandslash",
+        ["Rarity"] = "Legendary",
+        ["Icon"] = "rbxassetid://14581537664",
+        ["Damage"] = 51
+    },
+    ["Grimshade Katana"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Grimshade Katana",
+        ["Model"] = "Grimshade Katana",
+        ["Rarity"] = "Legendary",
+        ["Icon"] = "rbxassetid://14581739290",
+        ["Damage"] = 125
+    },
+    ["Beta Hammer"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Beta Hammer",
+        ["Model"] = "Beta Hammer",
+        ["Rarity"] = "Mythic",
+        ["Icon"] = "rbxassetid://14875369811",
+        ["Damage"] = 1
+    },
+    ["Steelwave Sword"] = {
+        ["ZoneRating"] = 4,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Steelwave Sword",
+        ["Model"] = "Steelwave Sword",
+        ["Rarity"] = "Rare",
+        ["Icon"] = "rbxassetid://14993877067",
+        ["Damage"] = 115
+    },
+    ["Stone Hammer"] = {
+        ["ZoneRating"] = 1,
+        ["Rarity"] = "Uncommon",
+        ["PowerType"] = "Physical",
+        ["Name"] = "Stone Hammer",
+        ["Model"] = "Stone Hammer",
+        ["WeaponLength"] = 0.4,
+        ["WeaponCooldown"] = 1,
+        ["Icon"] = "rbxassetid://14743001007",
+        ["Damage"] = 18
+    },
+    ["Seafarer's Giltblade"] = {
+        ["ZoneRating"] = 4,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Seafarer's Giltblade",
+        ["Model"] = "Seafarer's Giltblade",
+        ["Rarity"] = "Rare",
+        ["Icon"] = "rbxassetid://14993877862",
+        ["Damage"] = 105
+    },
+    ["Emberheart"] = {
+        ["ZoneRating"] = 2,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Emberheart",
+        ["Model"] = "Emberheart",
+        ["Rarity"] = "Legendary",
+        ["Icon"] = "rbxassetid://14581718160",
+        ["Damage"] = 80
+    },
+    ["Voidreaver Sickle"] = {
+        ["ZoneRating"] = 3,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Voidreaver Sickle",
+        ["Model"] = "Voidreaver Sickle",
+        ["DualWield"] = true,
+        ["Rarity"] = "Mythic",
+        ["Icon"] = "rbxassetid://15002344606",
+        ["Damage"] = 1
+    },
+    ["Wooden Club"] = {
+        ["ZoneRating"] = 1,
+        ["PowerType"] = "Physical",
+        ["Name"] = "Wooden Club",
+        ["Model"] = "Wooden Club",
+        ["Rarity"] = "Uncommon",
+        ["Icon"] = "rbxassetid://12938845119",
+        ["Damage"] = 10
+    }
+}
