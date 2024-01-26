7770af6a50cccf00bd0a1ef82e52e99f6343d062
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:38:21 +1100

    Create Tips.lua

---
+++ GameData/Elemental Dungeons [10515146389]/Tips.lua
@@ -0,0 +1,12 @@
+data = {
+    "Tired of random people inviting you to parties? Change your invite settings in the settings menu.",
+    "Got extra loot you don't need? Sell them in the lobby and upgrade your main gear.",
+    "Looking for a certain item? Purchase it from the merchant.",
+    "Want an extra challenge? Try out infinite mode!",
+    "Want an extra challenge? Try out challenges!",
+    "Want to hide your armor, show accessories when equipping armor or switch your package to blocky to fit in armor? Check out all these options within your inventory",
+    "When you consume an element you gain strengths/weaknesses depending on enemies elements",
+    "Want a new element? Go to the summons area and roll for an element.",
+    "Items are too weak? Upgrade them within your inventory!",
+    "VIP users get an extra daily quest."
+}
