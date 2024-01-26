1829f477a57714a35cac2e76664041de6c073f15
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:30:49 +1100

    Create CurrenciesModule.lua

---
+++ GameData/Elemental Dungeons [10515146389]/CurrenciesModule.lua
@@ -0,0 +1,14 @@
+data = {
+    ["Gems"] = {
+        ["Icon"] = "rbxassetid://13420690268",
+        ["Rarity"] = "Legendary",
+        ["Name"] = "Gems",
+        ["Desc"] = "A Gems Currency"
+    },
+    ["Gold"] = {
+        ["Icon"] = "rbxassetid://12780701014",
+        ["Rarity"] = "Legendary",
+        ["Name"] = "Gold",
+        ["Desc"] = "A Gold Currency"
+    }
+}
