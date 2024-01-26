0b724fa6529fdd44204b9523d1bb6fbb5ef7a491
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:35:59 +1100

    Create SalvageModule.lua

---
+++ GameData/Elemental Dungeons [10515146389]/SalvageModule.lua
@@ -0,0 +1,14 @@
+data = {
+    ["CalculateCost"] = "function: 0x0000000093675f58",
+    ["DefaultCosts"] = {
+        ["Exclusive"] = {["Amount"] = 5, ["Material"] = "Scrap Metal"},
+        ["Common"] = {["Amount"] = 5, ["Material"] = "Scrap Metal"},
+        ["Mythic"] = {["Amount"] = 5, ["Material"] = "Scrap Metal"},
+        ["Uncommon"] = {["Amount"] = 5, ["Material"] = "Scrap Metal"},
+        ["Rare"] = {["Amount"] = 5, ["Material"] = "Scrap Metal"},
+        ["Legendary"] = {["Amount"] = 5, ["Material"] = "Scrap Metal"},
+        ["Epic"] = {["Amount"] = 5, ["Material"] = "Scrap Metal"},
+        ["Misc"] = {["Amount"] = 5, ["Material"] = "Scrap Metal"},
+        ["Secret"] = {["Amount"] = 5, ["Material"] = "Scrap Metal"}
+    }
+}
