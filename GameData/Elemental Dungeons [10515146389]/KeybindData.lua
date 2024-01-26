dfd949c35007a5dc4f1f1c759cfe72eba76b00fa
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:33:01 +1100

    Create KeybindData.lua

---
+++ GameData/Elemental Dungeons [10515146389]/KeybindData.lua
@@ -0,0 +1,7 @@
+data = {
+    {Enum.KeyCode.F, Enum.KeyCode.ButtonR2},
+    {Enum.KeyCode.R, Enum.KeyCode.ButtonL2},
+    {Enum.KeyCode.C, Enum.KeyCode.DPadLeft},
+    {Enum.KeyCode.V, Enum.KeyCode.DPadRight},
+    {Enum.KeyCode.G, Enum.KeyCode.ButtonY}
+}
