69e8af4bcf8827c4db86190296424c6192f72c6c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:29:09 +1100

    Create CharacterStates.lua

---
+++ GameData/Elemental Dungeons [10515146389]/CharacterStates.lua
@@ -0,0 +1,49 @@
+data = {
+    ["Freezing"] = {
+        ["Text"] = "You are currently freezing...",
+        ["Title"] = "Freezing",
+        ["Icon"] = "rbxassetid://13910260753"
+    },
+    ["Poisoned"] = {["Text"] = "You have been poisoned!", ["Title"] = "Poisoned", ["Icon"] = "rbxassetid://13868848187"},
+    ["EyeTrapped"] = {
+        ["Icon"] = "rbxassetid://13976659645",
+        ["Text"] = "You are currently being pulled by the eye of the dragon!",
+        ["Title"] = "Eye_Trapped",
+        ["Stunned"] = true
+    },
+    ["Harmed"] = {
+        ["Text"] = "You have been harmed by an unknown source?",
+        ["Title"] = "Harmed",
+        ["Icon"] = "rbxassetid://13868980851"
+    },
+    ["Trapped"] = {
+        ["Icon"] = "rbxassetid://13868424421",
+        ["Text"] = "You have been trapped by <USERNAME>!",
+        ["Title"] = "Trapped",
+        ["Stunned"] = true
+    },
+    ["Burning"] = {["Text"] = "You're burning!", ["Title"] = "Burning", ["Icon"] = "rbxassetid://13868844788"},
+    ["Slowed"] = {
+        ["Text"] = "You are currently being slowed down...",
+        ["Title"] = "Slowed",
+        ["Icon"] = "rbxassetid://13868785482"
+    },
+    ["VineTrapped"] = {
+        ["Title"] = "Vine_Trapped",
+        ["Stunned"] = true,
+        ["Text"] = "You aare currently trapped by Vines!",
+        ["Icon"] = "rbxassetid://13910252337"
+    },
+    ["GalaxyTrapped"] = {
+        ["Icon"] = "rbxassetid://14128677513",
+        ["Text"] = "You are currently being pulled by a galacial force!",
+        ["Title"] = "Galaxy_Trapped",
+        ["Stunned"] = true
+    },
+    ["Frozen"] = {
+        ["Title"] = "Frozen",
+        ["Stunned"] = true,
+        ["Text"] = "You are currently frozen!",
+        ["Icon"] = "rbxassetid://13868383551"
+    }
+}
