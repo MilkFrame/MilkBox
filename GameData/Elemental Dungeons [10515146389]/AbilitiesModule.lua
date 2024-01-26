d63caa3a9eae47d9928f1c281e379db3b9a4ce38
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:17:34 +1100

    Create AbilitiesModule.lua

---
+++ GameData/Elemental Dungeons [10515146389]/AbilitiesModule.lua
@@ -0,0 +1,26 @@
+data = {
+    ["Dash"] = {
+        ["Module"] = "Dash",
+        ["PowerType"] = "Magic",
+        ["Name"] = "Dash",
+        ["Rarity"] = "Common",
+        ["Cooldown"] = 0.8,
+        ["Damage"] = 0
+    },
+    ["Jump"] = {
+        ["Module"] = "Jump",
+        ["PowerType"] = "Magic",
+        ["Name"] = "Jump",
+        ["Rarity"] = "Common",
+        ["Cooldown"] = 0.8,
+        ["Damage"] = 0
+    },
+    ["DashV2"] = {
+        ["Module"] = "DashV2",
+        ["PowerType"] = "Magic",
+        ["Name"] = "DashV2",
+        ["Rarity"] = "Common",
+        ["Cooldown"] = 0.8,
+        ["Damage"] = 0
+    }
+}
