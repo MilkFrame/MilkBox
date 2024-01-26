758fac1a90d319b231f5aba6c22dbdab4a956049
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:37:30 +1100

    Create SetBonuses.lua

---
+++ GameData/Elemental Dungeons [10515146389]/SetBonuses.lua
@@ -0,0 +1,292 @@
+data = {
+    ["SteelArmorSet"] = {
+        ["SetEquipped"] = "function: 0x00000000bedec4c8",
+        ["Type"] = "Health",
+        ["Title"] = "Steel Set",
+        ["SetDequipped"] = "function: 0x000000001a386ab0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(139, 139, 139),
+        ["Desc"] = "Increased health when set is equipped"
+    },
+    ["TribalArmorSet"] = {
+        ["SetEquipped"] = "function: 0x0000000001abb7e8",
+        ["Type"] = "Magic",
+        ["Title"] = "Tribal Set",
+        ["SetDequipped"] = "function: 0x00000000fa9763d0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(170, 85, 0),
+        ["Desc"] = "Increased magic damage when set is equipped"
+    },
+    ["Iron"] = {
+        ["SetEquipped"] = "function: 0x00000000bedec4c8",
+        ["Type"] = "Health",
+        ["Title"] = "Iron Set",
+        ["SetDequipped"] = "function: 0x000000001a386ab0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(176, 176, 176),
+        ["Desc"] = "Increased health when set is equipped"
+    },
+    ["GoldArmorSet"] = {
+        ["SetEquipped"] = "function: 0x00000000bedec4c8",
+        ["Type"] = "Health",
+        ["Title"] = "Gold Set",
+        ["SetDequipped"] = "function: 0x000000001a386ab0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(255, 170, 0),
+        ["Desc"] = "Increased health when set is equipped"
+    },
+    ["Wooden"] = {
+        ["SetEquipped"] = "function: 0x00000000bedec4c8",
+        ["Type"] = "Health",
+        ["Title"] = "Wooden Set",
+        ["SetDequipped"] = "function: 0x000000001a386ab0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(190, 135, 85),
+        ["Desc"] = "Increased health when set is equipped"
+    },
+    ["Overlord"] = {
+        ["SetEquipped"] = "function: 0x00000000bedec4c8",
+        ["Type"] = "Health",
+        ["Title"] = "Overlord Armor Set",
+        ["SetDequipped"] = "function: 0x000000001a386ab0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(255, 0, 0),
+        ["Desc"] = "Increased health when set is equipped"
+    },
+    ["TurtleArmorSet"] = {
+        ["SetEquipped"] = "function: 0x00000000f2692f78",
+        ["Type"] = "Health",
+        ["Title"] = "Turtle Set",
+        ["SetDequipped"] = "function: 0x00000000afcecb80",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(85, 170, 0),
+        ["Desc"] = "Increased health when set is equipped"
+    },
+    ["YetiArmorSet"] = {
+        ["SetEquipped"] = "function: 0x00000000bedec4c8",
+        ["Type"] = "Health",
+        ["Title"] = "Yeti Armor Set",
+        ["SetDequipped"] = "function: 0x000000001a386ab0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(171, 114, 0),
+        ["Desc"] = "Increased health when set is equipped"
+    },
+    ["SunArmorSet"] = {
+        ["SetEquipped"] = "function: 0x00000000bedec4c8",
+        ["Type"] = "Health",
+        ["Title"] = "Sun armor Set",
+        ["SetDequipped"] = "function: 0x000000001a386ab0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(126, 126, 126),
+        ["Desc"] = "Increased health when set is equipped"
+    },
+    ["Nightshade"] = {
+        ["SetEquipped"] = "function: 0x0000000001abb7e8",
+        ["Type"] = "Magic",
+        ["Title"] = "Nightshade Armor Set",
+        ["SetDequipped"] = "function: 0x00000000fa9763d0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(255, 0, 255),
+        ["Desc"] = "Increased magic damage when set is equipped"
+    },
+    ["Coral"] = {
+        ["SetEquipped"] = "function: 0x0000000001abb7e8",
+        ["Type"] = "Magic",
+        ["Title"] = "Coral Armor Set",
+        ["SetDequipped"] = "function: 0x00000000fa9763d0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(255, 170, 255),
+        ["Desc"] = "Increased magic damage when set is equipped"
+    },
+    ["SylvanSet"] = {
+        ["SetEquipped"] = "function: 0x0000000001abb7e8",
+        ["Type"] = "Magic",
+        ["Title"] = "Sylvan Set",
+        ["SetDequipped"] = "function: 0x00000000fa9763d0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(0, 250, 146),
+        ["Desc"] = "Increased magic damage when set is equipped"
+    },
+    ["RubyArmorSet"] = {
+        ["SetEquipped"] = "function: 0x00000000bedec4c8",
+        ["Type"] = "Health",
+        ["Title"] = "Ruby Armor Set",
+        ["SetDequipped"] = "function: 0x000000001a386ab0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(148, 17, 0),
+        ["Desc"] = "Increased health when set is equipped"
+    },
+    ["DiamondArmorSet"] = {
+        ["SetEquipped"] = "function: 0x00000000bedec4c8",
+        ["Type"] = "Health",
+        ["Title"] = "Diamond Set",
+        ["SetDequipped"] = "function: 0x000000001a386ab0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(85, 255, 255),
+        ["Desc"] = "Increased health when set is equipped"
+    },
+    ["Celestial"] = {
+        ["SetEquipped"] = "function: 0x0000000001abb7e8",
+        ["Type"] = "Magic",
+        ["Title"] = "Celestial Armor Set",
+        ["SetDequipped"] = "function: 0x00000000fa9763d0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(85, 170, 255),
+        ["Desc"] = "Increased magic damage when set is equipped"
+    },
+    ["Rainbowshade"] = {
+        ["SetEquipped"] = "function: 0x0000000001abb7e8",
+        ["Type"] = "Magic",
+        ["Title"] = "Rainbowshade Armor Set",
+        ["SetDequipped"] = "function: 0x00000000fa9763d0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(255, 0, 0),
+        ["Desc"] = "Increased magic damage when set is equipped"
+    },
+    ["AzureArmor"] = {
+        ["SetEquipped"] = "function: 0x0000000001abb7e8",
+        ["Type"] = "Magic",
+        ["Title"] = "Azure Armor Set",
+        ["SetDequipped"] = "function: 0x00000000fa9763d0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(4, 51, 255),
+        ["Desc"] = "Increased magic damage when set is equipped"
+    },
+    ["FrostweaverArmor"] = {
+        ["SetEquipped"] = "function: 0x0000000001abb7e8",
+        ["Type"] = "Magic",
+        ["Title"] = "Frostweaver Armor Set",
+        ["SetDequipped"] = "function: 0x00000000fa9763d0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(85, 255, 255),
+        ["Desc"] = "Increased magic damage when set is equipped"
+    },
+    ["VinewoodArmorSet"] = {
+        ["SetEquipped"] = "function: 0x0000000001abb7e8",
+        ["Type"] = "Magic",
+        ["Title"] = "Vinewood Set",
+        ["SetDequipped"] = "function: 0x00000000fa9763d0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(85, 255, 0),
+        ["Desc"] = "Increased magic damage when set is equipped"
+    },
+    ["CypherArmor"] = {
+        ["SetEquipped"] = "function: 0x0000000001abb7e8",
+        ["Type"] = "Magic",
+        ["Title"] = "Cypher Set",
+        ["SetDequipped"] = "function: 0x00000000fa9763d0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(148, 23, 81),
+        ["Desc"] = "Increased magic damage when set is equipped"
+    },
+    ["SpectreArmorSet"] = {
+        ["SetEquipped"] = "function: 0x0000000001abb7e8",
+        ["Type"] = "Magic",
+        ["Title"] = "Spectre Set",
+        ["SetDequipped"] = "function: 0x00000000fa9763d0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(85, 255, 255),
+        ["Desc"] = "Increased magic damage when set is equipped"
+    },
+    ["BasicMageArmor"] = {
+        ["SetEquipped"] = "function: 0x0000000001abb7e8",
+        ["Type"] = "Magic",
+        ["Title"] = "Basic Mage Set",
+        ["SetDequipped"] = "function: 0x00000000fa9763d0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(190, 135, 85),
+        ["Desc"] = "Increased magic damage when set is equipped"
+    },
+    ["Aureate"] = {
+        ["SetEquipped"] = "function: 0x00000000bedec4c8",
+        ["Type"] = "Health",
+        ["Title"] = "Aureate Set",
+        ["SetDequipped"] = "function: 0x000000001a386ab0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(255, 147, 0),
+        ["Desc"] = "Increased health when set is equipped"
+    },
+    ["Venomous"] = {
+        ["SetEquipped"] = "function: 0x00000000bedec4c8",
+        ["Type"] = "Health",
+        ["Title"] = "Venomous Armor Set",
+        ["SetDequipped"] = "function: 0x000000001a386ab0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(0, 85, 0),
+        ["Desc"] = "Increased health when set is equipped"
+    },
+    ["Neonshroud"] = {
+        ["SetEquipped"] = "function: 0x00000000bedec4c8",
+        ["Type"] = "Health",
+        ["Title"] = "Neonshroud Armor Set",
+        ["SetDequipped"] = "function: 0x000000001a386ab0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(85, 170, 127),
+        ["Desc"] = "Increased health when set is equipped"
+    },
+    ["Bronze"] = {
+        ["SetEquipped"] = "function: 0x00000000bedec4c8",
+        ["Type"] = "Health",
+        ["Title"] = "Bronze Set",
+        ["SetDequipped"] = "function: 0x000000001a386ab0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(255, 170, 127),
+        ["Desc"] = "Increased health when set is equipped"
+    },
+    ["ViridianArmorSet"] = {
+        ["SetEquipped"] = "function: 0x00000000bedec4c8",
+        ["Type"] = "Health",
+        ["Title"] = "Viridian Armor Set",
+        ["SetDequipped"] = "function: 0x000000001a386ab0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(0, 145, 147),
+        ["Desc"] = "Increased health when set is equipped"
+    },
+    ["GlacialArmorSet"] = {
+        ["SetEquipped"] = "function: 0x0000000001abb7e8",
+        ["Type"] = "Health",
+        ["Title"] = "Glacial Set",
+        ["SetDequipped"] = "function: 0x0000000001abb7e8",
+        ["AmtNeeded"] = 2,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(85, 255, 255),
+        ["Desc"] = "Increased magic damage when set is equipped"
+    },
+    ["Wavecrest"] = {
+        ["SetEquipped"] = "function: 0x00000000bedec4c8",
+        ["Type"] = "Health",
+        ["Title"] = "Wavecrest Armor Set",
+        ["SetDequipped"] = "function: 0x000000001a386ab0",
+        ["AmtNeeded"] = 3,
+        ["IndividualBonus"] = 0.025,
+        ["TitleColor"] = Color3.fromRGB(85, 255, 255),
+        ["Desc"] = "Increased health when set is equipped"
+    }
+}
