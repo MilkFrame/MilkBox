d62286e604a94df43c206c33fb8c97b6ceefce15
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:32:22 +1100

    Create Gamepasses.lua

---
+++ GameData/Elemental Dungeons [10515146389]/Gamepasses.lua
@@ -0,0 +1,9 @@
+data = {
+    ["Lucky"] = {["Product"] = 182857423},
+    ["2xElementMastery"] = {["Product"] = 182823087},
+    ["ExtraStorage"] = {["Product"] = 182857492},
+    ["2xGold"] = {["Product"] = 182837469},
+    ["VIP"] = {["Product"] = 182857523},
+    ["2xLoot"] = {["Product"] = 182857119},
+    ["2xExp"] = {["Product"] = 182836186}
+}
