0d781c3b1741230b34bb3742b2137d0de305f1b4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:35:45 +1100

    Create ReforgeEnchantsList.lua

---
+++ GameData/Elemental Dungeons [10515146389]/ReforgeEnchantsList.lua
@@ -0,0 +1,81 @@
+data = {
+    ["Weapons"] = {
+        ["Furious"] = {
+            ["Color"] = Color3.fromRGB(255, 140, 0),
+            ["Buffs"] = {["Knockback"] = 0.3, ["Damage"] = 0.2},
+            ["Weight"] = 14
+        },
+        ["Precise"] = {
+            ["Color"] = Color3.fromRGB(0, 128, 128),
+            ["Buffs"] = {["Crit"] = 0.2, ["Knockback"] = 0.1},
+            ["Weight"] = 18
+        },
+        ["Mythical"] = {
+            ["Color"] = Color3.fromRGB(255, 20, 147),
+            ["Buffs"] = {["Knockback"] = 0.3, ["Crit"] = 0.3, ["Damage"] = 0.4},
+            ["Weight"] = 3
+        },
+        ["Dangerous"] = {["Color"] = Color3.fromRGB(255, 0, 0), ["Buffs"] = {["Damage"] = 0.1}, ["Weight"] = 20},
+        ["Swift"] = {["Color"] = Color3.fromRGB(0, 255, 0), ["Buffs"] = {["Crit"] = 0.3}, ["Weight"] = 15},
+        ["Mystical"] = {
+            ["Color"] = Color3.fromRGB(186, 85, 211),
+            ["Buffs"] = {["Crit"] = 0.3, ["Damage"] = 0.1},
+            ["Weight"] = 6
+        },
+        ["Hasty"] = {
+            ["Color"] = Color3.fromRGB(255, 255, 0),
+            ["Buffs"] = {["Knockback"] = 0.1, ["Crit"] = 0.2, ["Damage"] = 0.1},
+            ["Weight"] = 11
+        },
+        ["Superior"] = {
+            ["Color"] = Color3.fromRGB(173, 216, 230),
+            ["Buffs"] = {["Knockback"] = 0.1, ["Crit"] = 0.1, ["Damage"] = 0.2},
+            ["Weight"] = 10
+        },
+        ["Savage"] = {
+            ["Color"] = Color3.fromRGB(139, 0, 0),
+            ["Buffs"] = {["Knockback"] = -0.1, ["Crit"] = -0.1, ["Damage"] = 0.1},
+            ["Weight"] = 8
+        },
+        ["Godly"] = {
+            ["Color"] = Color3.fromRGB(0, 0, 255),
+            ["Buffs"] = {["Knockback"] = 0.2, ["Crit"] = 0.2, ["Damage"] = 0.5},
+            ["Weight"] = 1
+        },
+        ["Vicious"] = {
+            ["Color"] = Color3.fromRGB(160, 82, 45),
+            ["Buffs"] = {["Crit"] = -0.2, ["Damage"] = 0.4},
+            ["Weight"] = 9
+        },
+        ["Legendary"] = {
+            ["Color"] = Color3.fromRGB(255, 215, 0),
+            ["Buffs"] = {["Knockback"] = 0.1, ["Crit"] = 0.1, ["Damage"] = 0.2},
+            ["Weight"] = 5
+        },
+        ["Relentless"] = {
+            ["Color"] = Color3.fromRGB(50, 205, 50),
+            ["Buffs"] = {["Knockback"] = 0.2, ["Crit"] = 0.2, ["Damage"] = 0.2},
+            ["Weight"] = 13
+        },
+        ["Frenzied"] = {
+            ["Color"] = Color3.fromRGB(255, 69, 0),
+            ["Buffs"] = {["Knockback"] = 0.2, ["Crit"] = 0.2, ["Damage"] = 0.3},
+            ["Weight"] = 7
+        },
+        ["Ruthless"] = {
+            ["Color"] = Color3.fromRGB(0, 0, 139),
+            ["Buffs"] = {["Knockback"] = 0.2, ["Crit"] = 0.1, ["Damage"] = 0.3},
+            ["Weight"] = 8
+        },
+        ["Murderous"] = {
+            ["Color"] = Color3.fromRGB(128, 0, 128),
+            ["Buffs"] = {["Knockback"] = -0.3, ["Crit"] = -0.1, ["Damage"] = -0.1},
+            ["Weight"] = 12
+        },
+        ["Cruel"] = {
+            ["Color"] = Color3.fromRGB(128, 0, 0),
+            ["Buffs"] = {["Knockback"] = 0.2, ["Damage"] = 0.3},
+            ["Weight"] = 12
+        }
+    }
+}
