b8ae1451ece286638aab7730b1c62804ef71a872
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:30:33 +1100

    Create CraftingRecipes.lua

---
+++ GameData/Elemental Dungeons [10515146389]/CraftingRecipes.lua
@@ -0,0 +1,71 @@
+data = {
+    {
+        ["Before"] = {
+            {["Type"] = "Materials", ["Base"] = "Diamond", ["Quantity"] = 8},
+            {["Type"] = "Materials", ["Base"] = "Vines", ["Quantity"] = 3},
+            {["Type"] = "Currencies", ["Base"] = "Gold", ["Quantity"] = 4000}
+        },
+        ["After"] = {{["Type"] = "Armors", ["Base"] = "Diamond Chestplate", ["Quantity"] = 1}}
+    },
+    {
+        ["Before"] = {
+            {["Type"] = "Materials", ["Base"] = "Diamond", ["Quantity"] = 6},
+            {["Type"] = "Materials", ["Base"] = "Vines", ["Quantity"] = 3},
+            {["Type"] = "Currencies", ["Base"] = "Gold", ["Quantity"] = 4000}
+        },
+        ["After"] = {{["Type"] = "Armors", ["Base"] = "Diamond Helmet", ["Quantity"] = 1}}
+    },
+    {
+        ["Before"] = {
+            {["Type"] = "Materials", ["Base"] = "Diamond", ["Quantity"] = 6},
+            {["Type"] = "Materials", ["Base"] = "Vines", ["Quantity"] = 3},
+            {["Type"] = "Currencies", ["Base"] = "Gold", ["Quantity"] = 4000}
+        },
+        ["After"] = {{["Type"] = "Armors", ["Base"] = "Diamond Leggings", ["Quantity"] = 1}}
+    },
+    {
+        ["Before"] = {
+            {["Type"] = "Materials", ["Base"] = "Scrap Metal", ["Quantity"] = 150},
+            {["Type"] = "Orbs", ["Base"] = "Dragon", ["Quantity"] = 1},
+            {["Type"] = "Materials", ["Base"] = "Diamond", ["Quantity"] = 30},
+            {["Type"] = "Currencies", ["Base"] = "Gold", ["Quantity"] = 100000}
+        },
+        ["After"] = {{["Type"] = "Weapons", ["Base"] = "Fusion Blade", ["Quantity"] = 1}}
+    },
+    {
+        ["Before"] = {
+            {["Type"] = "Orbs", ["Base"] = "Water", ["Quantity"] = 2},
+            {["Type"] = "Orbs", ["Base"] = "Fire", ["Quantity"] = 2},
+            {["Type"] = "Orbs", ["Base"] = "Earth", ["Quantity"] = 2},
+            {["Type"] = "Orbs", ["Base"] = "Air", ["Quantity"] = 2}
+        },
+        ["After"] = {{["Type"] = "Relics", ["Base"] = "Arcane Necklace", ["Quantity"] = 1}}
+    },
+    {
+        ["Before"] = {
+            {["Type"] = "Weapons", ["Base"] = "Fusion Blade", ["Quantity"] = 3},
+            {["Type"] = "Relics", ["Base"] = "Arcane Necklace", ["Quantity"] = 3},
+            {["Type"] = "Orbs", ["Base"] = "Galaxy", ["Quantity"] = 1},
+            {["Type"] = "Currencies", ["Base"] = "Gold", ["Quantity"] = 1000000}
+        },
+        ["After"] = {{["Type"] = "Weapons", ["Base"] = "Menta Blade", ["Quantity"] = 1}}
+    },
+    {
+        ["Before"] = {
+            {["Type"] = "Materials", ["Base"] = "Scrap Metal", ["Quantity"] = 150},
+            {["Type"] = "Weapons", ["Base"] = "Grimshade Katana", ["Quantity"] = 1},
+            {["Type"] = "Orbs", ["Base"] = "Lightning", ["Quantity"] = 8},
+            {["Type"] = "Currencies", ["Base"] = "Gold", ["Quantity"] = 100000}
+        },
+        ["After"] = {{["Type"] = "Weapons", ["Base"] = "Lightning Katana", ["Quantity"] = 1}}
+    },
+    {
+        ["Before"] = {
+            {["Type"] = "Materials", ["Base"] = "Yeti Fur", ["Quantity"] = 3},
+            {["Type"] = "Materials", ["Base"] = "Lunar Shard", ["Quantity"] = 40},
+            {["Type"] = "Orbs", ["Base"] = "Reaper", ["Quantity"] = 1},
+            {["Type"] = "Currencies", ["Base"] = "Gold", ["Quantity"] = 500000}
+        },
+        ["After"] = {{["Type"] = "Relics", ["Base"] = "Reaper's Ring", ["Quantity"] = 1}}
+    }
+}
