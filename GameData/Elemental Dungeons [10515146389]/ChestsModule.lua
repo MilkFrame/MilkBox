7a6d81550df4eb88649f6d179db59bdf7cd2cb67
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:29:32 +1100

    Create ChestsModule.lua

---
+++ GameData/Elemental Dungeons [10515146389]/ChestsModule.lua
@@ -0,0 +1,11 @@
+data = {
+    ["Tiers"] = {
+        {["Price"] = 500, ["Space"] = 5},
+        {["Price"] = 3000, ["Space"] = 15},
+        {["Price"] = 6000, ["Space"] = 25},
+        {["Price"] = 20000, ["Space"] = 40},
+        {["Price"] = 0, ["Space"] = 0}
+    },
+    ["GetTierData"] = "function: 0x00000000b4943ce0",
+    ["GetSlotsBeingUsed"] = "function: 0x00000000b2c2b640"
+}
