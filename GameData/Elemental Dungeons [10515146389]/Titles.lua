3db49dcda9b02eb8c73f854737826566725e9f7f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-27 00:38:45 +1100

    Create Titles.lua

---
+++ GameData/Elemental Dungeons [10515146389]/Titles.lua
@@ -0,0 +1,199 @@
+data = {
+    ["TitleTags"] = {
+        ["TouchGrass"] = {
+            ["Order"] = 23,
+            ["Title"] = "Touch Grass",
+            ["Base"] = "TouchGrass",
+            ["Desc"] = "Log in a year for a row"
+        },
+        ["TheBountiful"] = {
+            ["Order"] = 19,
+            ["Title"] = "Bountiful",
+            ["Base"] = "TheBountiful",
+            ["Desc"] = "Obtained by gifting 50,000 or more robux worth of products"
+        },
+        ["TheChampion"] = {
+            ["Title"] = "Champ",
+            ["Order"] = 1,
+            ["Rarity"] = "Legendary",
+            ["Base"] = "TheChampion",
+            ["Desc"] = "Obtained by becoming a #100 player"
+        },
+        ["TheRich"] = {
+            ["Order"] = 6,
+            ["Title"] = "Rich",
+            ["Base"] = "TheRich",
+            ["Desc"] = "Obtained by earning 10,000,000 Gold"
+        },
+        ["TheGodOfLightning"] = {
+            ["Order"] = 12,
+            ["Title"] = "God of Lightning",
+            ["Base"] = "TheGodOfLightning",
+            ["Desc"] = "Obtained by reaching level 100 on the Lighting element"
+        },
+        ["TheVisionary"] = {["Order"] = 8, ["Base"] = "TheVisionary", ["Title"] = "Visionary"},
+        ["TheZues"] = {
+            ["Order"] = 13,
+            ["Title"] = "Zeus",
+            ["Base"] = "TheZues",
+            ["Desc"] = "Obtained by reaching level 100 on the Lighting element"
+        },
+        ["MostBeautifulLooking"] = {
+            ["Title"] = "Most Beautiful Looking",
+            ["Order"] = 28,
+            ["Hidden"] = true,
+            ["Base"] = "MostBeautifulLooking",
+            ["Desc"] = "Avaliable to Moose and only Moose."
+        },
+        ["TheInnovator"] = {["Order"] = 3, ["Base"] = "TheInnovator", ["Title"] = "Innovator"},
+        ["TheMillionaire"] = {
+            ["Order"] = 27,
+            ["Title"] = "Millionaire",
+            ["Base"] = "TheMillionaire",
+            ["Desc"] = "Obtained by earning 1,000,000 Gold"
+        },
+        ["TheAchiever"] = {["Order"] = 7, ["Base"] = "TheAchiever", ["Title"] = "Achiever"},
+        ["TheHero"] = {
+            ["Order"] = 11,
+            ["Title"] = "Hero",
+            ["Base"] = "TheHero",
+            ["Desc"] = "Obtained by killing 100,000 Mobs"
+        },
+        ["GrimReaper"] = {
+            ["Order"] = 17,
+            ["Title"] = "Grim Reaper",
+            ["Base"] = "GrimReaper",
+            ["Desc"] = "Obtained by consuming the Reaper element"
+        },
+        ["TheExplorer"] = {["Order"] = 2, ["Base"] = "TheExplorer", ["Title"] = "Explorer"},
+        ["TheGenerous"] = {
+            ["Order"] = 18,
+            ["Title"] = "Generous",
+            ["Base"] = "TheGenerous",
+            ["Desc"] = "Obtained by gifting 5,000 or more robux worth of products"
+        },
+        ["BETA"] = {
+            ["Order"] = 24,
+            ["Title"] = "BETA",
+            ["Base"] = "BETA",
+            ["Desc"] = "Avaliable in the beta cosmetics pass"
+        },
+        ["TheMastermind"] = {
+            ["Order"] = 10,
+            ["Title"] = "Mastermind",
+            ["Base"] = "TheMastermind",
+            ["Desc"] = "Obtained by unlocking Dash V2"
+        },
+        ["TheMentor"] = {["Order"] = 21, ["Base"] = "TheMentor", ["Title"] = "Mentor"},
+        ["OG"] = {["Order"] = 26, ["Title"] = "OG", ["Base"] = "OG", ["Desc"] = "Avaliable in the beta cosmetics pass"},
+        ["TheStrategist"] = {
+            ["Order"] = 15,
+            ["Title"] = "Strategist",
+            ["Base"] = "TheStrategist",
+            ["Desc"] = "Obtained by dealing 10,000,000 damage"
+        },
+        ["TheAdventurer"] = {["Order"] = 4, ["Base"] = "TheAdventurer", ["Title"] = "Adventurer"},
+        ["TheEmpowerer"] = {
+            ["Order"] = 20,
+            ["Title"] = "Empowerer",
+            ["Base"] = "TheEmpowerer",
+            ["Desc"] = "Obtained by killing 1,000,000 Mobs"
+        },
+        ["Master"] = {
+            ["Title"] = "Master",
+            ["Order"] = 22,
+            ["Rarity"] = "Legendary",
+            ["Base"] = "Master",
+            ["Desc"] = "Obtained by becoming a #1 player"
+        },
+        ["Î²"] = {["Order"] = 25, ["Title"] = "Î²", ["Base"] = "Î²", ["Desc"] = "Avaliable in the beta cosmetics pass"},
+        ["TheLeader"] = {
+            ["Order"] = 16,
+            ["Title"] = "Leader",
+            ["Base"] = "TheLeader",
+            ["Desc"] = "Obtained by reaching Level 100"
+        },
+        ["TheFamous"] = {
+            ["Order"] = 14,
+            ["Title"] = "Famous",
+            ["Base"] = "TheFamous",
+            ["Desc"] = "Obtained by meeting the developers"
+        },
+        ["Fan"] = {["Order"] = 22, ["Title"] = "Fan", ["Base"] = "Fan", ["Desc"] = "Obtained by becoming a #10 player"},
+        ["ThePunisher"] = {
+            ["Order"] = 9,
+            ["Title"] = "Punisher",
+            ["Base"] = "ThePunisher",
+            ["Desc"] = "Obtained by reaching level 130 on the Lighting element"
+        },
+        ["TheForce"] = {
+            ["Order"] = 5,
+            ["Title"] = "The Force",
+            ["Base"] = "TheForce",
+            ["Desc"] = "Obtained by reaching level 150 with the gravity element"
+        }
+    },
+    ["Colors"] = {
+        {["Color"] = Color3.fromRGB(255, 255, 255), ["Type"] = "Default", ["Title"] = "Default", ["ObtainAmount"] = 1},
+        {
+            ["Color"] = Color3.fromRGB(255, 170, 0),
+            ["Type"] = "BabyOrangeColor",
+            ["Title"] = "Baby Orange",
+            ["ObtainAmount"] = 5
+        },
+        {
+            ["Color"] = Color3.fromRGB(255, 108, 108),
+            ["Type"] = "CoralPinkColor",
+            ["Title"] = "Coral Pink",
+            ["ObtainAmount"] = 10
+        },
+        {
+            ["Color"] = Color3.fromRGB(0, 102, 204),
+            ["Type"] = "OceanBlueColor",
+            ["Title"] = "Ocean Blue",
+            ["ObtainAmount"] = 15
+        },
+        {
+            ["Color"] = Color3.fromRGB(0, 204, 102),
+            ["Type"] = "EmeraldGreenColor",
+            ["Title"] = "Emerald Green",
+            ["ObtainAmount"] = 20
+        },
+        {
+            ["Color"] = Color3.fromRGB(128, 0, 255),
+            ["Type"] = "VioletPurpleColor",
+            ["Title"] = "Violet Purple",
+            ["ObtainAmount"] = 25
+        },
+        {
+            ["Color"] = Color3.fromRGB(255, 51, 51),
+            ["Type"] = "SunsetRedColor",
+            ["Title"] = "Sunset Red",
+            ["ObtainAmount"] = 30
+        },
+        {
+            ["Color"] = Color3.fromRGB(0, 187, 170),
+            ["Type"] = "TurquoiseBlueColor",
+            ["Title"] = "Turquoise Blue",
+            ["ObtainAmount"] = 35
+        },
+        {
+            ["Color"] = Color3.fromRGB(255, 204, 0),
+            ["Type"] = "GoldenYellowColor",
+            ["Title"] = "Golden Yellow",
+            ["ObtainAmount"] = 35
+        },
+        {
+            ["Color"] = Color3.fromRGB(192, 192, 192),
+            ["Type"] = "SilverGrayColor",
+            ["Title"] = "Silver Gray",
+            ["ObtainAmount"] = 40
+        },
+        {
+            ["Color"] = Color3.fromRGB(0, 0, 128),
+            ["Type"] = "RoyalBlueColor",
+            ["Title"] = "Royal Blue",
+            ["ObtainAmount"] = 45
+        }
+    }
+}
