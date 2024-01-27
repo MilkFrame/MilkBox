0b3e22a249285cc9eb0c185dab9eb5c1aad7d0d7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-28 03:01:47 +1100

    Create AnimationConstants.lua

---
+++ GameData/Murder Party [5611648039]/AnimationConstants.lua
@@ -0,0 +1,13 @@
+data = {
+    ["KnifeAttackAnimationName"] = "Slash",
+    ["KnifeThrowAfterAttackTime"] = 0.36333333333333334,
+    ["KnifeThrowAnimationName"] = "Lunge",
+    ["KnifeAttackAfterThrowTime"] = 0.52,
+    ["KnifeThrowAttackSpeed"] = 2,
+    ["KnifeAttackStartTime"] = 0.12,
+    ["KnifeThrowTime"] = 1.04,
+    ["KnifeThrowStartTime"] = 0.4,
+    ["TapScreenTime"] = 0.75,
+    ["KnifeAttackTime"] = 0.7266666666666667,
+    ["KnifeAttackSpeed"] = 1.5
+}
