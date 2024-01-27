5f57e7cee2638123f80770e038a739eb64e6deda
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-28 03:02:23 +1100

    Create Currencies.lua

---
+++ GameData/Murder Party [5611648039]/Currencies.lua
@@ -0,0 +1,24 @@
+data = {
+    ["IsCurrency"] = "function: 0x00000000d66c74b1",
+    ["Names"] = {
+        ["HardCurrency"] = "Gems",
+        ["robux"] = "Robux",
+        ["GachaToken"] = "Gacha Token",
+        ["SoftCurrency"] = "Coins"
+    },
+    ["DataTags"] = {
+        ["HardCurrency"] = "HardCurrency",
+        ["GachaToken"] = "GachaTokens",
+        ["SoftCurrency"] = "SoftCurrency"
+    },
+    ["RobuxKey"] = "robux",
+    ["Icons"] = {
+        ["HardCurrency"] = "rbxassetid://6525399831",
+        ["robux"] = "rbxassetid://6647092565",
+        ["GachaToken"] = "rbxassetid://5980659143",
+        ["SoftCurrency"] = "rbxassetid://6525399890"
+    },
+    ["HardCurrencyKey"] = "HardCurrency",
+    ["SoftCurrencyKey"] = "SoftCurrency",
+    ["GachaTokensKey"] = "GachaToken"
+}
