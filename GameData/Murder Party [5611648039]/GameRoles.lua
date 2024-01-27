4f56dbd8d3e7bd87ebf8ae31d96e3197c20410b7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-28 03:03:58 +1100

    Create GameRoles.lua

---
+++ GameData/Murder Party [5611648039]/GameRoles.lua
@@ -0,0 +1,87 @@
+data = {
+    ["LayoutOrder"] = {1, 1, 3, 3, 4, 1, 2},
+    ["TeamKeys"] = {1, 2, 5, 1, 4, 3, 2},
+    ["DESKTOP_HELP_TEXT"] = {
+        [[Click or F - Attack
+E - Throw]],
+        "Click or F - Shoot",
+        "Click or F - Shoot",
+        [[Keep your laptop out to hack
+E - Teleport]],
+        [[Click or F - Attack
+E - Throw]],
+        "Keep your Magnifying Glass out to scan people."
+    },
+    ["HackerKey"] = 5,
+    ["Names"] = {"Murderer", "Sheriff", "Innocent", "Hunter", "Hacker", "Party Planner", "Detective"},
+    ["AssignmentAudio"] = {
+        "YouAreMurderer",
+        "YouAreSheriff",
+        "YouAreInnocent",
+        "YouAreHunter",
+        "YouAreHacker",
+        "YouArePartyPlanner",
+        "YouAreDetective"
+    },
+    ["ItemFolders"] = {"Knives", "Guns", "Bows", "Laptops", "PartyHat", "MagnifyingGlasses"},
+    ["Keys"] = {1, 2, 3, 4, 5, 6, 7},
+    ["ROLE_WEAPON_INCOMING"] = {
+        "Get ready to murder in:",
+        "Save the people in:",
+        "Match starts in:",
+        "Punch some people in:",
+        "Hacking the mainframe in:",
+        "Party starts in:",
+        "Sleuthing starts in:"
+    },
+    ["MurdererKey"] = 1,
+    ["Emoji"] = {"ðŸ˜ˆ", "ðŸ¤ ", "ðŸ™‚", "ðŸ¹", "ðŸ’»", "ðŸ¥³", "ðŸ”"},
+    ["RoleNameToKey"] = {
+        ["Party"] = 6,
+        ["Detective"] = 7,
+        ["Sheriff"] = 2,
+        ["Hacker"] = 5,
+        ["Murderer"] = 1,
+        ["Hunter"] = 4,
+        ["Innocent"] = 3
+    },
+    ["DetectiveKey"] = 7,
+    ["MYSTERY_EMOJI"] = "ðŸ˜³",
+    ["HunterKey"] = 4,
+    ["DEAD_EMOJI"] = "ðŸ’€",
+    ["RoleInventoryTypes"] = {"Knife", "Gun", "HunterBow", "Laptop", "PartyHat", "DetectiveGlass"},
+    ["SheriffKey"] = 2,
+    ["PartyKey"] = 6,
+    ["Colors"] = {
+        Color3.fromRGB(255, 0, 0),
+        Color3.fromRGB(0, 0, 255),
+        Color3.fromRGB(0, 217, 255),
+        Color3.fromRGB(204, 0, 0),
+        Color3.fromRGB(110, 175, 0),
+        Color3.fromRGB(255, 49, 215),
+        Color3.fromRGB(66, 133, 244)
+    },
+    ["IsThreat"] = {true, true, false, false, true, true, false},
+    ["RoleKeyToName"] = {"Murderer", "Sheriff", "Innocent", "Hunter", "Hacker", "Party", "Detective"},
+    ["InnocentKey"] = 3,
+    ["GOAL_HELP_TEXT"] = {
+        "Murder everyone!",
+        "Hunt down the Murderer!",
+        "Try to survive, and look for loose tools!",
+        "Help the Murderer hunt other players!",
+        "Stay alive and finish your hack!",
+        "Put a party hat on everyone!",
+        "Help the Sheriff find the Murderer!"
+    },
+    ["MOBILE_HELP_TEXT"] = {
+        [[Press "Attack" - Attack
+Tap Screen - Throw]],
+        "Tap Screen - Shoot",
+        "Tap Screen - Shoot",
+        [[Keep your laptop out to hack
+Tap "Teleport" - Teleport]],
+        [[Press "Attack" - Attack
+Tap Screen - Throw]],
+        "Keep your Magnifying Glass out to scan people."
+    }
+}
