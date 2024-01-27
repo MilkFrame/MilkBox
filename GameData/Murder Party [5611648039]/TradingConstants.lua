96494dfd05857c424d2bc7ea6ec20b08da31e8ea
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-28 03:04:43 +1100

    Create TradingConstants.lua

---
+++ GameData/Murder Party [5611648039]/TradingConstants.lua
@@ -0,0 +1,16 @@
+data = {
+    ["STATE_DONE"] = 2,
+    ["PAID_ITEM_TRADING_NOT_ALLOWED"] = "paid_item_trading_not_allowed",
+    ["ACCEPT_NONE"] = 2,
+    ["STATE_CONFIRMING"] = 1,
+    ["ACCEPT_FRIENDS"] = 1,
+    ["REASON_PLAYER_CANCELLED"] = "player_cancelled",
+    ["STATE_PICK_ITEMS"] = 0,
+    ["TARGET_CANNOT_TRADE"] = "target_cannot_trade",
+    ["REASON_POLICYSERVICE_NOT_LOADED"] = "policyservice_not_loaded",
+    ["REASON_PLAYER_BUSY"] = "player_busy",
+    ["PLAYER_SETTING_BLOCKED"] = "player_setting_blocked",
+    ["ACCEPT_ALL"] = 0,
+    ["DATA_STORE_ERROR"] = "data_store_error",
+    ["REASON_PLAYER_LEFT"] = "player_left"
+}
