021925d319e2f735dc376fbbbf4b264225259658
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-28 03:03:26 +1100

    Create ErrorCodes.lua

---
+++ GameData/Murder Party [5611648039]/ErrorCodes.lua
@@ -0,0 +1,9 @@
+data = {
+    ["Success"] = 0,
+    ["RewardCodes"] = {
+        ["InvalidCode"] = 1,
+        ["CodeExpired"] = 2,
+        ["SaveNotLoaded"] = 3,
+        ["AlreadyRedeemed"] = 4
+    }
+}
