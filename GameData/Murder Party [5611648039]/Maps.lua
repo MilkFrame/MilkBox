2a519bfbcd4bc2412167b252a84b1fa5f3dcdb31
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-28 03:04:12 +1100

    Create Maps.lua

---
+++ GameData/Murder Party [5611648039]/Maps.lua
@@ -0,0 +1,14 @@
+data = {
+    {["display"] = "Lumberyard", ["image"] = "rbxassetid://5172529602", ["name"] = "Lumberyard"},
+    {["display"] = "The Lab", ["image"] = "rbxassetid://5172262128", ["name"] = "TheLab"},
+    {["display"] = "Magic School", ["image"] = "rbxassetid://5172529798", ["name"] = "MagicSchool"},
+    {["display"] = "Clockwork", ["image"] = "rbxassetid://5172529124", ["name"] = "Clockwork"},
+    {["display"] = "Box Park", ["image"] = "rbxassetid://5191715649", ["name"] = "BoxPark"},
+    {["display"] = "Murderer Mansion", ["image"] = "rbxassetid://6502963771", ["name"] = "VampireMansion"},
+    {["display"] = "Party Castle", ["image"] = "rbxassetid://6503000628", ["name"] = "FairyCastle"},
+    {["display"] = "Hacker's Lair", ["image"] = "rbxassetid://6502998776", ["name"] = "McNug"},
+    {["display"] = "Sheriff's Farm", ["image"] = "rbxassetid://6502999852", ["name"] = "RainbowFarm"},
+    {["display"] = "Party Studio", ["image"] = "rbxassetid://6652116821", ["name"] = "PartyStudio"},
+    {["display"] = "Robot Factory", ["image"] = "rbxassetid://7807457458", ["name"] = "RobotFactory"},
+    {["display"] = "My Amazing Map", ["image"] = "", ["name"] = "TestArea"}
+}
