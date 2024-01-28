f1f8e26a631ef3ea8cb87fe03aa8789f2541d9d6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-28 16:16:33 +1100

    Create Recipes.lua

---
+++ GameData/Assassin! [379614936]/Recipes.lua
@@ -0,0 +1,24 @@
+--GetRecipes()
+data = {
+    {"Unicorn", {{"Dark Blade", 1}, {"Rainbow", 1}}, "ddddddddd"},
+    {"Dark Horse", {{"Unicorn", 1}, {"Dark Blade", 2}, {"Doombringer", 2}}, "ddddddddddd"},
+    {"Steampunk", {{"Striker", 1}, {"Proton", 3}}, "ff"},
+    {"Armor Eye", {{"Cosmic Eye", 3}, {"Red", 1}, {"Damascus", 1}, {"Roughed", 1}, {"Edged", 1}}, "dddddd"},
+    {"Kunai", {{"Synergy", 1}, {"Striker", 1}, {"Fury", 1}}, "gg"},
+    {"Runic Blade", {{"Void", 1}, {"Dark Age", 1}, {"Ancient Steel", 1}}, "ii"},
+    {"Wendigo", {{"Dark Horse", 1}, {"Obsidian Lord", 1}, {"Duality", 1}}, "ddddddddddddd"},
+    {"Dark Age", {{"Onyx", 1}, {"Shadow", 1}}, "i"},
+    {"Flamethrower", {{"Energy Slicer", 1}, {"Proton", 1}, {"Flamebrand", 1}, {"Epic Red", 1}}, "jj"},
+    {"Imp", {{"Fang", 1}, {"Kunai", 1}}, "hhhh"},
+    {"Starlight", {{"Faerie", 1}, {"Lava Blade", 1}, {"Emerald Knight", 1}}, "eee"},
+    {"Rainbow Saber", {{"Elegant Saber", 1}, {"Starlight", 1}, {"Rainbow", 1}}, "eeee"},
+    {"Rainbow",{{"Red", 1}, {"Orange", 1}, {"Yellow", 1}, {"Green", 1}, {"Blue", 1}, {"Purple", 1}, {"Pink", 1}},"dddddddd"},
+    {"Striker", {{"Gilded", 1}, {"Lightning", 1}, {"Volt", 1}}, "f"},
+    {"Duality", {{"Nemesis", 3}, {"Noble Dagger", 3}}, "fff"},
+    {"Elemental Scimitar", {{"Fire Scimitar", 1}, {"Water Scimitar", 1}, {"Blossom Scimitar", 1}}, "ee"},
+    {"Jade Maiden", {{"Enchanted Jade", 1}, {"Steel Maiden", 1}, {"Chaos Axe", 1}}, "k"},
+    {"Void", {{"Wrath", 1}, {"Bandit", 1}}, "h"},
+    {"Energy Slicer", {{"Kunai", 1}, {"Epic Blue", 1}, {"Ice Lord", 1}}, "j"},
+    {"Fang", {{"Spider", 1}, {"Dark Claw", 1}}, "hhh"},
+    {"Vine", {{"Runic Blade", 1}, {"Elemental Scimitar", 1}, {"Jade Maiden", 1}}, "kk"}
+}
