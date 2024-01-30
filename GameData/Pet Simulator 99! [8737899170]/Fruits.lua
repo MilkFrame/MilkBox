32b38e4a97c3b016fa5d5cc8c64e4087b6ad6aa3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-31 01:55:36 +1100

    Update Fruits.lua

--- GameData/Pet Simulator 99! [8737899170]/Fruits.lua
+++ GameData/Pet Simulator 99! [8737899170]/Fruits.lua
@@ -1,4 +1,47 @@
 data = {
+    ["Candycane"] = {
+        ["Rarity"] = {
+            ["RarityNumber"] = 3,
+            ["Lootbag"] = "Epic",
+            ["_id"] = "Epic",
+            ["Color"] = Color3.fromRGB(41, 226, 255),
+            ["DisplayName"] = "Epic",
+            ["Message"] = "function: 0x00000000962d710c",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "3 | Epic"
+        },
+        ["_id"] = "Candycane",
+        ["Duration"] = 300,
+        ["DisplayName"] = "Candycane",
+        ["_script"] = "Fruit | Candycane",
+        ["IgnoreFruitMachine"] = true,
+        ["Boost"] = {{["Amount"] = 1.5, ["Type"] = "Drops"}},
+        ["Icon"] = "rbxassetid://15636388090",
+        ["Desc"] = [[Christmas snack for your pets! Increases chance for drops by 1.5%! \n(Stacks!)]]
+    },
+    ["Apple"] = {
+        ["_script"] = "Fruit | Apple",
+        ["Boost"] = {{["Amount"] = 1.5, ["Type"] = "Damage"}},
+        ["Rarity"] = {
+            ["RarityNumber"] = 2,
+            ["Lootbag"] = "Rare",
+            ["_id"] = "Rare",
+            ["Color"] = Color3.fromRGB(113, 255, 62),
+            ["DisplayName"] = "Rare",
+            ["Message"] = "function: 0x00000000a25a89dc",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "2 | Rare"
+        },
+        ["_id"] = "Apple",
+        ["Desc"] = [[Tasty snack for your pets! Increases pet damage by 1.5%! \n(Stacks!)]],
+        ["Duration"] = 300,
+        ["Icon"] = "rbxassetid://15030757644",
+        ["DisplayName"] = "Apple"
+    },
     ["Rainbow"] = {
         ["_script"] = "Fruit | Rainbow",
         ["Boost"] = {
@@ -13,7 +56,7 @@ data = {
             ["_id"] = "Legendary",
             ["Color"] = Color3.fromRGB(255, 133, 34),
             ["DisplayName"] = "Legendary",
-            ["Message"] = "function: 0x0000000074198758",
+            ["Message"] = "function: 0x00000000cd315c2c",
             ["ItemSlot"] = "ItemSlot",
             ["Gradient"] = "Gradient",
             ["Announce"] = false,
@@ -25,27 +68,6 @@ data = {
         ["Icon"] = "rbxassetid://15030756988",
         ["DisplayName"] = "Rainbow Fruit"
     },
-    ["Apple"] = {
-        ["_script"] = "Fruit | Apple",
-        ["Boost"] = {{["Amount"] = 1.5, ["Type"] = "Damage"}},
-        ["Rarity"] = {
-            ["RarityNumber"] = 2,
-            ["Lootbag"] = "Rare",
-            ["_id"] = "Rare",
-            ["Color"] = Color3.fromRGB(113, 255, 62),
-            ["DisplayName"] = "Rare",
-            ["Message"] = "function: 0x0000000038e82a88",
-            ["ItemSlot"] = "ItemSlot",
-            ["Gradient"] = "Gradient",
-            ["Announce"] = false,
-            ["_script"] = "2 | Rare"
-        },
-        ["_id"] = "Apple",
-        ["Desc"] = [[Tasty snack for your pets! Increases pet damage by 1.5%! \n(Stacks!)]],
-        ["Duration"] = 300,
-        ["Icon"] = "rbxassetid://15030757644",
-        ["DisplayName"] = "Apple"
-    },
     ["Pineapple"] = {
         ["_script"] = "Fruit | Pineapple",
         ["Boost"] = {{["Amount"] = 1, ["Type"] = "Drops"}},
@@ -55,7 +77,7 @@ data = {
             ["_id"] = "Rare",
             ["Color"] = Color3.fromRGB(113, 255, 62),
             ["DisplayName"] = "Rare",
-            ["Message"] = "function: 0x0000000038e82a88",
+            ["Message"] = "function: 0x00000000a25a89dc",
             ["ItemSlot"] = "ItemSlot",
             ["Gradient"] = "Gradient",
             ["Announce"] = false,
@@ -76,7 +98,7 @@ data = {
             ["_id"] = "Rare",
             ["Color"] = Color3.fromRGB(113, 255, 62),
             ["DisplayName"] = "Rare",
-            ["Message"] = "function: 0x0000000038e82a88",
+            ["Message"] = "function: 0x00000000a25a89dc",
             ["ItemSlot"] = "ItemSlot",
             ["Gradient"] = "Gradient",
             ["Announce"] = false,
@@ -97,7 +119,7 @@ data = {
             ["_id"] = "Rare",
             ["Color"] = Color3.fromRGB(113, 255, 62),
             ["DisplayName"] = "Rare",
-            ["Message"] = "function: 0x0000000038e82a88",
+            ["Message"] = "function: 0x00000000a25a89dc",
             ["ItemSlot"] = "ItemSlot",
             ["Gradient"] = "Gradient",
             ["Announce"] = false,
