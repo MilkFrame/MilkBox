febc8bbe00e28e719444e861951998d9459760eb
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-31 02:06:27 +1100

    Create GuildBattles.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/GuildBattles.lua
@@ -0,0 +1,80 @@
+data = {
+    ["Christmas2023"] = {
+        ["_script"] = "Christmas2023",
+        ["FinishTime"] = 1703869200,
+        ["Title"] = "Festive Clan Battle",
+        ["_id"] = "Christmas2023",
+        ["StartTime"] = 1702746000,
+        ["Rewards"] = {
+            ["Bronze"] = {{["_data"] = {["id"] = "Present"}}},
+            ["Silver"] = {
+                {["_data"] = {["id"] = "Red Present"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}}
+            },
+            ["Gold"] = {
+                {["_data"] = {["id"] = "Huge Present Cat"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}}
+            }
+        }
+    },
+    ["IndexBattle"] = {
+        ["_script"] = "IndexBattle",
+        ["FinishTime"] = 1707498000,
+        ["Title"] = "Pet Collection Battle",
+        ["_id"] = "IndexBattle",
+        ["StartTime"] = 1705770000,
+        ["Rewards"] = {
+            ["Bronze"] = {{["_data"] = {["id"] = "M10"}}},
+            ["Silver"] = {
+                {["_data"] = {["id"] = "Pegasus"}},
+                {["_data"] = {["id"] = "Exclusive Egg 20"}},
+                {["_data"] = {["id"] = "Exclusive Egg 20"}},
+                {["_data"] = {["id"] = "Exclusive Egg 20"}}
+            },
+            ["Gold"] = {
+                {["_data"] = {["id"] = "Huge Sensei Penguin"}},
+                {["_data"] = {["id"] = "Exclusive Egg 20"}},
+                {["_data"] = {["id"] = "Exclusive Egg 20"}},
+                {["_data"] = {["id"] = "Exclusive Egg 20"}},
+                {["_data"] = {["id"] = "Exclusive Egg 20"}},
+                {["_data"] = {["id"] = "Exclusive Egg 20"}},
+                {["_data"] = {["id"] = "Exclusive Egg 20"}},
+                {["_data"] = {["id"] = "Exclusive Egg 20"}}
+            }
+        }
+    },
+    ["DecemberActiveHugePets"] = {
+        ["_script"] = "DecemberActiveHugePets",
+        ["FinishTime"] = 1705683600,
+        ["Title"] = "New Year's Clan Battle",
+        ["_id"] = "DecemberActiveHugePets",
+        ["StartTime"] = 1703955600,
+        ["Rewards"] = {
+            ["Bronze"] = {{["_data"] = {["id"] = "Cupcake"}}},
+            ["Silver"] = {
+                {["_data"] = {["id"] = "Blobfish"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}}
+            },
+            ["Gold"] = {
+                {["_data"] = {["id"] = "Huge Owl"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}},
+                {["_data"] = {["id"] = "Exclusive Egg 18"}}
+            }
+        }
+    }
+}
