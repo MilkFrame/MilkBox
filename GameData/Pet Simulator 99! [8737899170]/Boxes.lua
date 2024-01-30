6728d5de6989c6ff8d75ab2be8584bda2ab528e0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-31 02:04:47 +1100

    Create Boxes.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/Boxes.lua
@@ -0,0 +1,32 @@
+data = {
+    ["Small"] = {
+        ["_script"] = "Box | Small",
+        ["Rarity"] = {
+            ["RarityNumber"] = 4,
+            ["Lootbag"] = "Legendary",
+            ["_id"] = "Legendary",
+            ["Color"] = Color3.fromRGB(255, 133, 34),
+            ["DisplayName"] = "Legendary",
+            ["Message"] = "function: 0x0000000040b6e07b",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "4 | Legendary"
+        },
+        ["_id"] = "Small",
+        ["Capacity"] = 100,
+        ["Desc"] = "Store items & pets!",
+        ["Icons"] = {
+            {["Color"] = Color3.fromRGB(226, 184, 126), ["Name"] = "Brown", ["Icon"] = "rbxassetid://15977548594"},
+            {["Color"] = Color3.fromRGB(255, 65, 65), ["Name"] = "Red", ["Icon"] = "rbxassetid://15977547828"},
+            {["Color"] = Color3.fromRGB(255, 155, 55), ["Name"] = "Orange", ["Icon"] = "rbxassetid://15977548298"},
+            {["Color"] = Color3.fromRGB(255, 255, 55), ["Name"] = "Yellow", ["Icon"] = "rbxassetid://15977547685"},
+            {["Color"] = Color3.fromRGB(115, 255, 45), ["Name"] = "Green", ["Icon"] = "rbxassetid://15977548487"},
+            {["Color"] = Color3.fromRGB(65, 255, 255), ["Name"] = "Aqua", ["Icon"] = "rbxassetid://15977548852"},
+            {["Color"] = Color3.fromRGB(65, 65, 255), ["Name"] = "Blue", ["Icon"] = "rbxassetid://15977548711"},
+            {["Color"] = Color3.fromRGB(155, 55, 255), ["Name"] = "Purple", ["Icon"] = "rbxassetid://15977547973"},
+            {["Color"] = Color3.fromRGB(255, 55, 255), ["Name"] = "Pink", ["Icon"] = "rbxassetid://15977548126"}
+        },
+        ["DisplayName"] = "Small Box"
+    }
+}
