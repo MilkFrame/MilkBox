010478b96af97086a712387f27f97cec4b06f414
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-31 02:04:26 +1100

    Create Ornaments.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/Ornaments.lua
@@ -0,0 +1,122 @@
+data = {
+    ["Pink Christmas Ornament"] = {
+        ["_script"] = "Pink Christmas Ornament",
+        ["Rarity"] = {
+            ["RarityNumber"] = 5,
+            ["Lootbag"] = "Mythical",
+            ["_id"] = "Mythical",
+            ["Color"] = Color3.fromRGB(255, 43, 100),
+            ["DisplayName"] = "Mythical",
+            ["Message"] = "function: 0x000000004162f913",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "5 | Mythical"
+        },
+        ["Model"] = "Model",
+        ["_id"] = "Pink Christmas Ornament",
+        ["Desc"] = "Place on a tree in Snowman Town and leave a special message!",
+        ["Icon"] = "rbxassetid://15628075235",
+        ["DisplayName"] = "Pink Christmas Ornament"
+    },
+    ["Silver Christmas Ornament"] = {
+        ["_script"] = "Silver Christmas Ornament",
+        ["Rarity"] = {
+            ["RarityNumber"] = 5,
+            ["Lootbag"] = "Mythical",
+            ["_id"] = "Mythical",
+            ["Color"] = Color3.fromRGB(255, 43, 100),
+            ["DisplayName"] = "Mythical",
+            ["Message"] = "function: 0x000000004162f913",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "5 | Mythical"
+        },
+        ["Model"] = "Model",
+        ["_id"] = "Silver Christmas Ornament",
+        ["Desc"] = "Place on a tree in Snowman Town and leave a special message!",
+        ["Icon"] = "rbxassetid://15628075059",
+        ["DisplayName"] = "Silver Christmas Ornament"
+    },
+    ["Gold Christmas Ornament"] = {
+        ["_script"] = "Gold Christmas Ornament",
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0x00000000bb5c2a6b",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["Model"] = "Model",
+        ["_id"] = "Gold Christmas Ornament",
+        ["Desc"] = "Place on a tree in Snowman Town and leave a special message!",
+        ["Icon"] = "rbxassetid://15621569759",
+        ["DisplayName"] = "Gold Christmas Ornament"
+    },
+    ["Red Christmas Ornament"] = {
+        ["_script"] = "Red Christmas Ornament",
+        ["Rarity"] = {
+            ["RarityNumber"] = 5,
+            ["Lootbag"] = "Mythical",
+            ["_id"] = "Mythical",
+            ["Color"] = Color3.fromRGB(255, 43, 100),
+            ["DisplayName"] = "Mythical",
+            ["Message"] = "function: 0x000000004162f913",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "5 | Mythical"
+        },
+        ["Model"] = "Model",
+        ["_id"] = "Red Christmas Ornament",
+        ["Desc"] = "Place on a tree in Snowman Town and leave a special message!",
+        ["Icon"] = "rbxassetid://15621569543",
+        ["DisplayName"] = "Red Christmas Ornament"
+    },
+    ["Blue Christmas Ornament"] = {
+        ["_script"] = "Blue Christmas Ornament",
+        ["Rarity"] = {
+            ["RarityNumber"] = 5,
+            ["Lootbag"] = "Mythical",
+            ["_id"] = "Mythical",
+            ["Color"] = Color3.fromRGB(255, 43, 100),
+            ["DisplayName"] = "Mythical",
+            ["Message"] = "function: 0x000000004162f913",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "5 | Mythical"
+        },
+        ["Model"] = "Model",
+        ["_id"] = "Blue Christmas Ornament",
+        ["Desc"] = "Place on a tree in Snowman Town and leave a special message!",
+        ["Icon"] = "rbxassetid://15628075380",
+        ["DisplayName"] = "Blue Christmas Ornament"
+    },
+    ["Green Christmas Ornament"] = {
+        ["_script"] = "Green Christmas Ornament",
+        ["Rarity"] = {
+            ["RarityNumber"] = 5,
+            ["Lootbag"] = "Mythical",
+            ["_id"] = "Mythical",
+            ["Color"] = Color3.fromRGB(255, 43, 100),
+            ["DisplayName"] = "Mythical",
+            ["Message"] = "function: 0x000000004162f913",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "5 | Mythical"
+        },
+        ["Model"] = "Model",
+        ["_id"] = "Green Christmas Ornament",
+        ["Desc"] = "Place on a tree in Snowman Town and leave a special message!",
+        ["Icon"] = "rbxassetid://15621569679",
+        ["DisplayName"] = "Green Christmas Ornament"
+    }
+}
