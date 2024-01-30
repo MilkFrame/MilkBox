35a7c3fc6ad8fc322d645440d3148d62580cb772
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-31 01:47:44 +1100

    Create MagicOrbs.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/MagicOrbs.lua
@@ -0,0 +1,19 @@
+data = {
+    ["Magic Orb"] = {
+        ["ImpactVolume"] = 0.3,
+        ["ShootSounds"] = {"rbxassetid://15980447241", "rbxassetid://15980447103", "rbxassetid://15980446378"},
+        ["DamageMultiplier"] = 5,
+        ["_id"] = "Magic Orb",
+        ["ShootVolume"] = 0.15,
+        ["ImpactPitch"] = 1,
+        ["FireRate"] = 1,
+        ["_script"] = "MagicOrb | Magic Orb",
+        ["ProjectileSpeed"] = 30,
+        ["ImpactSounds"] = {"rbxassetid://15980446586", "rbxassetid://15980446961", "rbxassetid://15980446756"},
+        ["Model"] = "Model",
+        ["ShootPitch"] = 1.4,
+        ["ProjectileMinimumDuration"] = 0.5,
+        ["Lifetime"] = 10,
+        ["ProjectileModel"] = "Projectile"
+    }
+}
