e0f5cb9ca5887600e21b1e717777f233811f7dc4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-31 01:45:54 +1100

    Create WateringCans.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/WateringCans.lua
@@ -0,0 +1,22 @@
+data = {
+    ["Watering Can"] = {
+        ["_script"] = "WateringCan | Watering Can",
+        ["_id"] = "Watering Can",
+        ["AssociatedItemID"] = "Watering Can",
+        ["Model"] = "Model",
+        ["PlantTimeMultiplierDuration"] = 1200,
+        ["PlantTimeMultiplier"] = 5,
+        ["Icon"] = "rbxassetid://15555104581",
+        ["DisplayName"] = "Watering Can"
+    },
+    ["Golden Watering Can"] = {
+        ["_script"] = "WateringCan | Golden Watering Can",
+        ["_id"] = "Golden Watering Can",
+        ["AssociatedItemID"] = "Golden Watering Can",
+        ["Model"] = "Model",
+        ["PlantTimeMultiplierDuration"] = 3600,
+        ["PlantTimeMultiplier"] = 5,
+        ["Icon"] = "rbxassetid://15555104643",
+        ["DisplayName"] = "Golden Watering Can"
+    }
+}
