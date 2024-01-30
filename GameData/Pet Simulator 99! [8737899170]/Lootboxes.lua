b13c48f8290e349d5bc0e410a8645cfff64f40ad
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-31 02:07:57 +1100

    Create Lootboxes.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/Lootboxes.lua
@@ -0,0 +1,116 @@
+data = {
+    ["Medium Christmas Present"] = {
+        ["_script"] = "Medium Christmas Present",
+        ["Rarity"] = {
+            ["RarityNumber"] = 3,
+            ["Lootbag"] = "Epic",
+            ["_id"] = "Epic",
+            ["Color"] = Color3.fromRGB(41, 226, 255),
+            ["DisplayName"] = "Epic",
+            ["Message"] = "function: 0x000000008ca3bb5b",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "3 | Epic"
+        },
+        ["_id"] = "Medium Christmas Present",
+        ["Desc"] = "Open for a bunch of goodies!",
+        ["Icon"] = "rbxassetid://15635476705",
+        ["DisplayName"] = "Medium Christmas Present"
+    },
+    ["Small Christmas Present"] = {
+        ["_script"] = "Small Christmas Present",
+        ["Rarity"] = {
+            ["RarityNumber"] = 2,
+            ["Lootbag"] = "Rare",
+            ["_id"] = "Rare",
+            ["Color"] = Color3.fromRGB(113, 255, 62),
+            ["DisplayName"] = "Rare",
+            ["Message"] = "function: 0x00000000c268f24b",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "2 | Rare"
+        },
+        ["_id"] = "Small Christmas Present",
+        ["Desc"] = "Open for goodies!",
+        ["Icon"] = "rbxassetid://15635476827",
+        ["DisplayName"] = "Small Christmas Present"
+    },
+    ["Titanic Christmas Present"] = {
+        ["_script"] = "Titanic Christmas Present",
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0x00000000bb5c2a6b",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["_id"] = "Titanic Christmas Present",
+        ["Desc"] = "TITANIC present with sooo many goodies! Includes chance for a Titanic & Huge pet!",
+        ["Icon"] = "rbxassetid://15635476408",
+        ["DisplayName"] = "Titanic Christmas Present"
+    },
+    ["Large Christmas Present"] = {
+        ["_script"] = "Large Christmas Present",
+        ["Rarity"] = {
+            ["RarityNumber"] = 4,
+            ["Lootbag"] = "Legendary",
+            ["_id"] = "Legendary",
+            ["Color"] = Color3.fromRGB(255, 133, 34),
+            ["DisplayName"] = "Legendary",
+            ["Message"] = "function: 0x0000000040b6e07b",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "4 | Legendary"
+        },
+        ["_id"] = "Large Christmas Present",
+        ["Desc"] = "Big present with lots of goodies!",
+        ["Icon"] = "rbxassetid://15635476600",
+        ["DisplayName"] = "Large Christmas Present"
+    },
+    ["2024 New Years Gift"] = {
+        ["_script"] = "2024 New Years Gift",
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0x00000000bb5c2a6b",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["_id"] = "2024 New Years Gift",
+        ["Desc"] = "Special gift for attending the 2024 New Year's Party! Chance for Huge Party Crown Ducky!",
+        ["Icon"] = "rbxassetid://15800024082",
+        ["DisplayName"] = "2024 New Year's Gift"
+    },
+    ["X-Large Christmas Present"] = {
+        ["_script"] = "X-Large Christmas Present",
+        ["Rarity"] = {
+            ["RarityNumber"] = 5,
+            ["Lootbag"] = "Mythical",
+            ["_id"] = "Mythical",
+            ["Color"] = Color3.fromRGB(255, 43, 100),
+            ["DisplayName"] = "Mythical",
+            ["Message"] = "function: 0x000000004162f913",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "5 | Mythical"
+        },
+        ["_id"] = "X-Large Christmas Present",
+        ["Desc"] = "Huge present with a ton of goodies!",
+        ["Icon"] = "rbxassetid://15635476534",
+        ["DisplayName"] = "X-Large Christmas Present"
+    }
+}
