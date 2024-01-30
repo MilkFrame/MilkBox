d5326880e9245b2e7b2144b7a686c427ff88acfc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-31 02:05:59 +1100

    Create EventGoals.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/EventGoals.lua
@@ -0,0 +1,124 @@
+data = {
+    ["Christmas2023"] = {
+        ["_script"] = "Christmas2023",
+        ["Sets"] = {
+            {
+                ["Rewards"] = {{["_data"] = {["id"] = "Small Christmas Present", ["_am"] = 6}}},
+                ["Goals"] = {{["GoalType"] = 1, ["Amount"] = 200, ["BreakableType"] = "Candy Cane"}},
+                ["TimeOffset"] = 0
+            },
+            {
+                ["Rewards"] = {{["_data"] = {["id"] = "Small Christmas Present", ["_am"] = 12}}},
+                ["Goals"] = {{["GoalType"] = 1, ["Amount"] = 2500}},
+                ["TimeOffset"] = 86400
+            },
+            {
+                ["Rewards"] = {{["_data"] = {["id"] = "Christmas"}}},
+                ["Goals"] = {{["GoalType"] = 20, ["Amount"] = 750}},
+                ["TimeOffset"] = 172800
+            },
+            {
+                ["Rewards"] = {{["_data"] = {["id"] = "Medium Christmas Present", ["_am"] = 4}}},
+                ["Goals"] = {{["GoalType"] = 43, ["Amount"] = 2}},
+                ["TimeOffset"] = 259200
+            },
+            {
+                ["Rewards"] = {{["_data"] = {["id"] = "Large Christmas Present"}}},
+                ["Goals"] = {{["GoalType"] = 1, ["Amount"] = 300, ["BreakableType"] = "Candy Cane"}},
+                ["TimeOffset"] = 345600
+            },
+            {
+                ["Rewards"] = {{["_data"] = {["id"] = "Large Christmas Present", ["_am"] = 2}}},
+                ["Goals"] = {{["GoalType"] = 21, ["Amount"] = 3000}},
+                ["TimeOffset"] = 432000
+            },
+            {
+                ["Rewards"] = {{["_data"] = {["id"] = "Large Christmas Present", ["_am"] = 3}}},
+                ["Goals"] = {{["GoalType"] = 21, ["Amount"] = 4500}},
+                ["TimeOffset"] = 518400
+            },
+            {
+                ["Rewards"] = {{["_data"] = {["id"] = "Rudolph"}}},
+                ["Goals"] = {{["GoalType"] = 20, ["Amount"] = 1000}},
+                ["TimeOffset"] = 604800
+            },
+            {
+                ["Rewards"] = {{["_data"] = {["id"] = "Medium Christmas Present", ["_am"] = 17}}},
+                ["Goals"] = {{["GoalType"] = 1, ["Amount"] = 325, ["BreakableType"] = "Candy Cane"}},
+                ["TimeOffset"] = 691200
+            },
+            {
+                ["Rewards"] = {{["_data"] = {["id"] = "Large Christmas Present", ["_am"] = 4}}},
+                ["Goals"] = {{["GoalType"] = 20, ["Amount"] = 1000}},
+                ["TimeOffset"] = 777600
+            },
+            {
+                ["Rewards"] = {{["_data"] = {["id"] = "X-Large Christmas Present"}}},
+                ["Goals"] = {{["GoalType"] = 21, ["Amount"] = 5000}},
+                ["TimeOffset"] = 864000
+            },
+            {
+                ["Rewards"] = {{["_data"] = {["id"] = "X-Large Christmas Present", ["_am"] = 2}}},
+                ["Goals"] = {{["GoalType"] = 43, ["Amount"] = 3}},
+                ["TimeOffset"] = 950400
+            },
+            {
+                ["Rewards"] = {{["_data"] = {["id"] = "X-Large Christmas Present", ["_am"] = 3}}},
+                ["Goals"] = {{["GoalType"] = 1, ["Amount"] = 400, ["BreakableType"] = "Candy Cane"}},
+                ["TimeOffset"] = 1036800
+            },
+            {
+                ["Rewards"] = {{["_data"] = {["id"] = "Titanic Christmas Present"}}},
+                ["Goals"] = {{["GoalType"] = 21, ["Amount"] = 5500}},
+                ["TimeOffset"] = 1123200
+            }
+        },
+        ["_id"] = "Christmas2023",
+        ["EventBuffer"] = 86400,
+        ["EventStart"] = 1702746000,
+        ["EventLength"] = 1209600,
+        ["GoalTypes"] = {
+            ["GOLD_PET"] = 4,
+            ["BEST_EGG"] = 20,
+            ["USE_POTION"] = 34,
+            ["BUY_POTION"] = 10,
+            ["BREAKABLE"] = 1,
+            ["CURRENT_BREAKABLE"] = 21,
+            ["ATLANTIS"] = 24,
+            ["FUSE_PETS"] = 19,
+            ["USE_FLAG"] = 33,
+            ["PET"] = 2,
+            ["DIAMOND_BREAKABLE"] = 9,
+            ["EGG"] = 3,
+            ["FREE_GIFT"] = 18,
+            ["MINI_CHEST"] = 8,
+            ["BEST_RAINBOW_PET"] = 41,
+            ["PYRAMID_OBBY"] = 28,
+            ["MINEFIELD"] = 23,
+            ["BEST_COMET"] = 38,
+            ["SLED_RACE"] = 36,
+            ["COLLECT_POTION"] = 14,
+            ["FISHING"] = 26,
+            ["RAINBOW_PET"] = 5,
+            ["GINGERBREAD_CHEST"] = 43,
+            ["UPGRADE_POTION"] = 12,
+            ["BEST_GOLD_PET"] = 40,
+            ["HATCH_RARE_PET"] = 42,
+            ["BUY_ENCHANT"] = 11,
+            ["BEST_MINI_CHEST"] = 39,
+            ["ZONE"] = 6,
+            ["BEST_COIN_JAR"] = 37,
+            ["DIGSITE"] = 25,
+            ["COIN_JAR"] = 31,
+            ["CHEST_RUSH"] = 30,
+            ["JUNGLE_OBBY"] = 29,
+            ["ICE_OBBY"] = 27,
+            ["SPAWN_OBBY"] = 22,
+            ["COLLECT_ENCHANT"] = 15,
+            ["UPGRADE_ENCHANT"] = 13,
+            ["COMET"] = 32,
+            ["CURRENCY"] = 7,
+            ["USE_FRUIT"] = 35
+        }
+    }
+}
