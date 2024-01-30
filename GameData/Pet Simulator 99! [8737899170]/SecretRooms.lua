36d2f11347c686317b0bdd85378ee6225529d2bd
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-31 02:07:31 +1100

    Create SecretRooms.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/SecretRooms.lua
@@ -0,0 +1,29 @@
+data = {
+    ["Chest Roulette"] = {
+        ["_script"] = "SecretRoom | Chest Roulette",
+        ["UnlockAnimation"] = "function: 0x0000000018806083",
+        ["InstanceId"] = "ChestRoulette",
+        ["_id"] = "Chest Roulette",
+        ["CloseDoor"] = "function: 0x00000000bf0aaae3",
+        ["RequiredZone"] = "Wild West",
+        ["DisplayName"] = "Chest Roulette"
+    },
+    ["Treehouse Merchant"] = {
+        ["_script"] = "SecretRoom | Treehouse Merchant",
+        ["UnlockAnimation"] = "function: 0x000000001afef7d3",
+        ["InstanceId"] = "TreehouseMerchant",
+        ["_id"] = "Treehouse Merchant",
+        ["CloseDoor"] = "function: 0x00000000cee06543",
+        ["RequiredZone"] = "Enchanted Forest",
+        ["DisplayName"] = "Treehouse Merchant"
+    },
+    ["Crystal Key Castle"] = {
+        ["_script"] = "SecretRoom | Crystal Key Castle",
+        ["UnlockAnimation"] = "function: 0x000000009ac3ab6b",
+        ["InstanceId"] = "CrystalKeyCastle",
+        ["_id"] = "Crystal Key Castle",
+        ["CloseDoor"] = "function: 0x00000000d7db2bb3",
+        ["RequiredZone"] = "Fairy Castle",
+        ["DisplayName"] = "Crystal Key Castle"
+    }
+}
