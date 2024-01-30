6895c5aa578b6ae5864158cb10ae8d967ce85fef
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-01-31 01:47:13 +1100

    Update Buffs.lua

--- GameData/Pet Simulator 99! [8737899170]/Buffs.lua
+++ GameData/Pet Simulator 99! [8737899170]/Buffs.lua
@@ -1,12 +1,4 @@
 data = {
-    ["Toy Ball"] = {
-        ["_script"] = "Buff | Toy Ball",
-        ["AssociatedItemID"] = "Toy Ball",
-        ["AssociatedItemClass"] = "Misc",
-        ["Length"] = 300,
-        ["_id"] = "Toy Ball",
-        ["DisplayName"] = "Toy Ball"
-    },
     ["Toy Bone"] = {
         ["_script"] = "Buff | Toy Bone",
         ["AssociatedItemID"] = "Toy Bone",
@@ -15,6 +7,22 @@ data = {
         ["_id"] = "Toy Bone",
         ["DisplayName"] = "Toy Bone"
     },
+    ["Christmas Cookie"] = {
+        ["_script"] = "Buff | Christmas Cookie",
+        ["AssociatedItemID"] = "Christmas Cookie",
+        ["AssociatedItemClass"] = "Misc",
+        ["Length"] = 300,
+        ["_id"] = "Christmas Cookie",
+        ["DisplayName"] = "Christmas Cookie"
+    },
+    ["Toy Ball"] = {
+        ["_script"] = "Buff | Toy Ball",
+        ["AssociatedItemID"] = "Toy Ball",
+        ["AssociatedItemClass"] = "Misc",
+        ["Length"] = 300,
+        ["_id"] = "Toy Ball",
+        ["DisplayName"] = "Toy Ball"
+    },
     ["Squeaky Toy"] = {
         ["_script"] = "Buff | Squeaky Toy",
         ["AssociatedItemID"] = "Squeaky Toy",
@@ -22,5 +30,13 @@ data = {
         ["Length"] = 600,
         ["_id"] = "Squeaky Toy",
         ["DisplayName"] = "Squeaky Toy"
+    },
+    ["Hot Cocoa"] = {
+        ["_script"] = "Buff | Hot Cocoa",
+        ["AssociatedItemID"] = "Hot Cocoa",
+        ["AssociatedItemClass"] = "Misc",
+        ["Length"] = 300,
+        ["_id"] = "Hot Cocoa",
+        ["DisplayName"] = "Hot Cocoa"
     }
 }
