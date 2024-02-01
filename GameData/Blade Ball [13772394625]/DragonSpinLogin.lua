486f44080d198523a0356c37e85ea1b7580e9a6a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:32:31 +1100

    Create DragonSpinLogin.lua

---
+++ GameData/Blade Ball [13772394625]/DragonSpinLogin.lua
@@ -0,0 +1,17 @@
+data = {
+    ["End"] = "1705766400000",
+    ["Start"] = "1705161600000",
+    ["StreakHoursRewards"] = {
+        {["Value"] = 2, ["Type"] = "DragonRolls", ["Icon"] = "rbxassetid://15943477115", ["DisplayName"] = "x2 Spins"},
+        {["Value"] = 10, ["Type"] = "DragonRolls", ["Icon"] = "rbxassetid://15943477115", ["DisplayName"] = "x10 Spins"},
+        {["Value"] = 4, ["Type"] = "DragonRolls", ["Icon"] = "rbxassetid://15943477115", ["DisplayName"] = "x4 Spins"},
+        {["Value"] = 7, ["Type"] = "DragonRolls", ["Icon"] = "rbxassetid://15943477115", ["DisplayName"] = "x7 Spins"}
+    },
+    ["HourlyReward"] = {
+        ["Value"] = 1,
+        ["Type"] = "DragonRolls",
+        ["Icon"] = "rbxassetid://15943477115",
+        ["DisplayName"] = "x1 Spins"
+    },
+    ["DebugTimeOffset"] = 0
+}
