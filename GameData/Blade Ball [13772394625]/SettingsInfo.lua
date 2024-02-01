1ee01e9cb72a0237805f67fa958a94658f5251ae
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:12:56 +1100

    Create SettingsInfo.lua

---
+++ GameData/Blade Ball [13772394625]/SettingsInfo.lua
@@ -0,0 +1,75 @@
+data = {
+    ["Misc"] = {
+        ["LayoutOrder"] = 8,
+        ["AbilityButtonPosition"] = {
+            ["LayoutOrder"] = 19,
+            ["Current"] = {["Y"] = 0.51, ["X"] = 0.9},
+            ["Default"] = {["Y"] = 0.51, ["X"] = 0.9},
+            ["DoNotDisplay"] = true
+        },
+        ["BlockButtonScale"] = {["LayoutOrder"] = 18, ["Current"] = 1, ["Default"] = 1, ["DoNotDisplay"] = true},
+        ["VR Parry Sensitivity"] = {["Max"] = 100, ["Current"] = 50, ["Default"] = 50, ["LayoutOrder"] = 15},
+        ["Duel Requests"] = {["Enabled"] = true, ["LayoutOrder"] = 10},
+        ["SwordSkinsRandomizer"] = {
+            ["LayoutOrder"] = 23,
+            ["Current"] = false,
+            ["Default"] = false,
+            ["UseFavorites"] = false,
+            ["DoNotDisplay"] = true
+        },
+        ["Low Graphics"] = {["Enabled"] = false, ["LayoutOrder"] = 9},
+        ["Tap Screen To Block"] = {["Enabled"] = true, ["LayoutOrder"] = 14},
+        ["Server Region"] = {["LayoutOrder"] = 21},
+        ["BlockButtonPosition"] = {
+            ["LayoutOrder"] = 17,
+            ["Current"] = {["Y"] = 0.3, ["X"] = 0.9},
+            ["Default"] = {["Y"] = 0.3, ["X"] = 0.9},
+            ["DoNotDisplay"] = true
+        },
+        ["VR Hand Switch Sensitivity"] = {["Max"] = 100, ["Current"] = 50, ["Default"] = 50, ["LayoutOrder"] = 16},
+        ["Country Flag"] = {["Enabled"] = true, ["LayoutOrder"] = 12, ["DoNotDisplay"] = true},
+        ["ExplosionSkinsRandomizer"] = {
+            ["LayoutOrder"] = 24,
+            ["Current"] = false,
+            ["Default"] = false,
+            ["UseFavorites"] = false,
+            ["DoNotDisplay"] = true
+        },
+        ["AbilitiesRandomizer"] = {
+            ["LayoutOrder"] = 25,
+            ["Current"] = false,
+            ["Default"] = false,
+            ["UseFavorites"] = false,
+            ["DoNotDisplay"] = true
+        },
+        ["Hide UI During Match"] = {["Enabled"] = false, ["LayoutOrder"] = 11},
+        ["AbilityButtonScale"] = {["LayoutOrder"] = 20, ["Current"] = 1, ["Default"] = 1, ["DoNotDisplay"] = true},
+        ["Server Version"] = {["LayoutOrder"] = 22}
+    },
+    ["Keybinds"] = {
+        ["Ability"] = {
+            ["LayoutOrder"] = 6,
+            ["PC"] = {["Bind2"] = "", ["Bind1"] = ""},
+            ["Default"] = {["Console"] = "ButtonX", ["PC"] = "Q"},
+            ["Console"] = {["Bind2"] = "", ["Bind1"] = ""}
+        },
+        ["Block"] = {
+            ["LayoutOrder"] = 5,
+            ["PC"] = {["Bind2"] = "", ["Bind1"] = "MouseButton1"},
+            ["Default"] = {["Console"] = "ButtonR1", ["PC"] = "F"},
+            ["Console"] = {["Bind2"] = "", ["Bind1"] = ""}
+        },
+        ["Emote"] = {
+            ["LayoutOrder"] = 7,
+            ["PC"] = {["Bind2"] = "", ["Bind1"] = "R"},
+            ["Default"] = {["Console"] = "ButtonL1", ["PC"] = "R"},
+            ["Console"] = {["Bind2"] = "", ["Bind1"] = "ButtonL1"}
+        },
+        ["LayoutOrder"] = 4
+    },
+    ["Volume"] = {
+        ["LayoutOrder"] = 1,
+        ["Music"] = {["Max"] = 100, ["Current"] = 50, ["Default"] = 50, ["LayoutOrder"] = 2},
+        ["SFX"] = {["Max"] = 100, ["Current"] = 50, ["Default"] = 50, ["LayoutOrder"] = 3}
+    }
+}
