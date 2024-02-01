8f19a1b4461d1260b36b9dbb380b9a0cc30744de
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:20:58 +1100

    Create ElementalRewards.lua

---
+++ GameData/Blade Ball [13772394625]/ElementalRewards.lua
@@ -0,0 +1,91 @@
+data = {
+    ["Water"] = {
+        ["Items"] = {
+            {
+                ["Chance"] = 5,
+                ["Item"] = {["Sword"] = "Ocean Blade"},
+                ["SwordIcon"] = "Ocean Blade",
+                ["Rarity"] = "Premium"
+            },
+            {["Chance"] = 15, ["Item"] = {["Emote"] = "Emote22"}, ["Rarity"] = "Rare", ["EmoteIcon"] = "Emote22"},
+            {
+                ["Rarity"] = "Common",
+                ["Chance"] = 40,
+                ["Item"] = {["Credits"] = 200},
+                ["Icon"] = "Credits",
+                ["Value"] = 200
+            },
+            {
+                ["Name"] = "Premium Sword Crate",
+                ["Chance"] = 40,
+                ["Item"] = {["CrateKeys"] = {"PremiumSword"}},
+                ["Icon"] = "PremiumSwordCrate",
+                ["Rarity"] = "Common"
+            }
+        },
+        ["BuyBatchProductId"] = 1671034481,
+        ["BuyProductId"] = 1671034374,
+        ["BuyGift"] = 1673406181,
+        ["UpgradedSword"] = "Tsunami Blade"
+    },
+    ["MegaReward"] = {["Sword"] = "Dual Elemental Masterblade"},
+    ["Lightning"] = {
+        ["Items"] = {
+            {
+                ["Chance"] = 5,
+                ["Item"] = {["Sword"] = "Thunder Blade"},
+                ["SwordIcon"] = "Thunder Blade",
+                ["Rarity"] = "Premium"
+            },
+            {["Chance"] = 15, ["Item"] = {["Emote"] = "Emote23"}, ["Rarity"] = "Rare", ["EmoteIcon"] = "Emote23"},
+            {
+                ["Rarity"] = "Common",
+                ["Chance"] = 40,
+                ["Item"] = {["Credits"] = 200},
+                ["Icon"] = "Credits",
+                ["Value"] = 200
+            },
+            {
+                ["Name"] = "Premium Explosion Crate",
+                ["Chance"] = 40,
+                ["Item"] = {["CrateKeys"] = {"PremiumExplosion"}},
+                ["Icon"] = "PremiumExplosionCrate",
+                ["Rarity"] = "Common"
+            }
+        },
+        ["BuyBatchProductId"] = 1671034294,
+        ["BuyProductId"] = 1671034247,
+        ["BuyGift"] = 1673405841,
+        ["UpgradedSword"] = "Voltage"
+    },
+    ["Wind"] = {
+        ["Items"] = {
+            {
+                ["Chance"] = 5,
+                ["Item"] = {["Sword"] = "Wind Thorn"},
+                ["SwordIcon"] = "Wind Thorn",
+                ["Rarity"] = "Premium"
+            },
+            {["Chance"] = 15, ["Item"] = {["Emote"] = "Emote21"}, ["Rarity"] = "Rare", ["EmoteIcon"] = "Emote21"},
+            {
+                ["Rarity"] = "Common",
+                ["Chance"] = 40,
+                ["Item"] = {["Credits"] = 200},
+                ["Icon"] = "Credits",
+                ["Value"] = 200
+            },
+            {
+                ["Name"] = "Premium Sword Crate",
+                ["Chance"] = 40,
+                ["Item"] = {["CrateKeys"] = {"PremiumSword"}},
+                ["Icon"] = "PremiumSwordCrate",
+                ["Rarity"] = "Common"
+            }
+        },
+        ["BuyBatchProductId"] = 1671034186,
+        ["BuyProductId"] = 1671034074,
+        ["BuyGift"] = 1673406032,
+        ["UpgradedSword"] = "Eternum Galepiercer"
+    },
+    ["NormalReward"] = {["Sword"] = "Elemental Masterblade"}
+}
