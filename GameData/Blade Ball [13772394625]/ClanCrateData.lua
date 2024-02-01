6cf366897f77f711ac41a4766ddf79ecb8ece44b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:39:58 +1100

    Create ClanCrateData.lua

---
+++ GameData/Blade Ball [13772394625]/ClanCrateData.lua
@@ -0,0 +1,44 @@
+data = {
+    ["crateLevels"] = {
+        {
+            ["SimpleReward"] = {["Crowns"] = 1, ["Credits"] = 100},
+            ["cost"] = 400,
+            ["icon"] = "rbxassetid://15594796502",
+            ["level"] = 1,
+            ["purchasesRequired"] = 2,
+            ["productId"] = 1702230766
+        },
+        {
+            ["SimpleReward"] = {["Crowns"] = 10, ["Credits"] = 200},
+            ["cost"] = 800,
+            ["icon"] = "rbxassetid://15594799981",
+            ["level"] = 2,
+            ["purchasesRequired"] = 4,
+            ["productId"] = 1702230851
+        },
+        {
+            ["SimpleReward"] = {["Crowns"] = 10, ["ClanSwordChest"] = 1},
+            ["cost"] = 1700,
+            ["icon"] = "rbxassetid://15594801468",
+            ["level"] = 3,
+            ["purchasesRequired"] = 8,
+            ["productId"] = 1702230966
+        },
+        {
+            ["SimpleReward"] = {["Crowns"] = 10, ["ClanSwordChest"] = 3},
+            ["cost"] = 4000,
+            ["icon"] = "rbxassetid://15594803283",
+            ["level"] = 4,
+            ["purchasesRequired"] = 15,
+            ["productId"] = 1702231112
+        },
+        {
+            ["SimpleReward"] = {["Crowns"] = 10, ["ClanSwordChest"] = 10},
+            ["cost"] = 8000,
+            ["icon"] = "rbxassetid://15594804778",
+            ["level"] = 5,
+            ["purchasesRequired"] = 20,
+            ["productId"] = 1702231207
+        }
+    }
+}
