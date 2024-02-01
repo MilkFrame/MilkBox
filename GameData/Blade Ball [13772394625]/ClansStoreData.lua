93ef5a186bb482d1c25358f4bf2dff39dc9c3767
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:41:45 +1100

    Create ClansStoreData.lua

---
+++ GameData/Blade Ball [13772394625]/ClansStoreData.lua
@@ -0,0 +1,120 @@
+data = {
+    ["Items"] = {
+        ["SwordClan_2"] = {
+            ["Type"] = "Permament",
+            ["Limit"] = 1,
+            ["Description"] = "This scythe conquered the world",
+            ["Price"] = 3000,
+            ["Rarity"] = "Legendary",
+            ["ClanLevel"] = 6,
+            ["Reward"] = {
+                ["Value"] = "Reckoner Scythe",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://15581256664",
+                ["DisplayName"] = "Reckoner Scythe"
+            }
+        },
+        ["ClanSwordChest"] = {
+            ["Type"] = "Weekly",
+            ["Limit"] = 1,
+            ["Description"] = "Special chest with exclusive swords",
+            ["Price"] = 5000,
+            ["Rarity"] = "Legendary",
+            ["ClanLevel"] = 3,
+            ["Reward"] = {
+                ["Type"] = "Chest",
+                ["Amount"] = 1,
+                ["Value"] = "Clan Sword",
+                ["Icon"] = "rbxassetid://15587888647",
+                ["DisplayName"] = "Clan Sword Chest"
+            }
+        },
+        ["SwordClan_1"] = {
+            ["Type"] = "Permament",
+            ["Limit"] = 1,
+            ["Description"] = "Sharp sword, be careful when using.",
+            ["Price"] = 1200,
+            ["Rarity"] = "Epic",
+            ["ClanLevel"] = 3,
+            ["Reward"] = {
+                ["Value"] = "Royal Sharpened Sword",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://15582396004",
+                ["DisplayName"] = "Royal Sharpened Sword"
+            }
+        },
+        ["ClanPoints_5000"] = {
+            ["Type"] = "Permament",
+            ["Limit"] = 2,
+            ["Description"] = "5,000 Clan Points",
+            ["Price"] = 10000,
+            ["Rarity"] = "Legendary",
+            ["ClanLevel"] = 3,
+            ["Reward"] = {
+                ["Value"] = 5000,
+                ["Type"] = "ClanPoints",
+                ["Icon"] = "rbxassetid://15612091195",
+                ["DisplayName"] = "5,000 Clan Points"
+            }
+        },
+        ["SciFiCrate"] = {
+            ["Type"] = "Weekly",
+            ["Limit"] = 50,
+            ["Description"] = "Get a free spin in the SCI-FI roll",
+            ["Price"] = 80,
+            ["Rarity"] = "Epic",
+            ["ClanLevel"] = 3,
+            ["Reward"] = {
+                ["Value"] = 1,
+                ["Type"] = "SciFiWheelSpins",
+                ["Icon"] = "rbxassetid://15455896861",
+                ["DisplayName"] = "x1 Sci-Fi Spin"
+            }
+        },
+        ["Waypoint"] = {
+            ["Type"] = "Weekly",
+            ["Limit"] = 1,
+            ["Description"] = "Place a sword marker on the ground. You can then re-use the ability to teleport to the sword marker.",
+            ["Price"] = 1500,
+            ["Rarity"] = "Common",
+            ["ClanLevel"] = 5,
+            ["Reward"] = {
+                ["Value"] = "Waypoint",
+                ["Type"] = "Ability",
+                ["Icon"] = "rbxassetid://14846350280",
+                ["DisplayName"] = "Waypoint"
+            }
+        },
+        ["PremiumSwordCrate"] = {
+            ["Type"] = "Weekly",
+            ["Limit"] = 40,
+            ["Description"] = " A premium crate that contains cool swords.",
+            ["Price"] = 15,
+            ["Rarity"] = "Common",
+            ["ClanLevel"] = 3,
+            ["Reward"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 1,
+                ["Value"] = "PremiumSword",
+                ["Icon"] = "rbxassetid://16039967646",
+                ["DisplayName"] = "Premium Sword Crate"
+            }
+        },
+        ["WinterGacha"] = {
+            ["Type"] = "Weekly",
+            ["Limit"] = 8,
+            ["Description"] = "Get a free roll in the Christmas Spin!",
+            ["Price"] = 55,
+            ["Rarity"] = "Epic",
+            ["ClanLevel"] = 3,
+            ["Reward"] = {
+                ["Value"] = 1,
+                ["Type"] = "GachaSpins",
+                ["Icon"] = "rbxassetid://16100521042",
+                ["DisplayName"] = "x1 Christmas Spin"
+            }
+        }
+    },
+    ["RefreshTimes"] = {["Weekly"] = 604800},
+    ["MinClanLevel"] = 3
+}
