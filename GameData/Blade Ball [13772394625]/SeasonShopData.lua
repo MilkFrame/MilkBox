03a9533f77ce8dfc29c977efe2e493db6a754929
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:28:22 +1100

    Update SeasonShopData.lua

--- GameData/Blade Ball [13772394625]/SeasonShopData.lua
+++ GameData/Blade Ball [13772394625]/SeasonShopData.lua
@@ -1,117 +1,63 @@
 data = {
     ["Item11"] = {
-        ["Currency"] = "Pumpkins",
+        ["Currency"] = "Balloons",
         ["ItemID"] = "Item11",
+        ["Price"] = 7000,
+        ["Chance"] = 0.03,
+        ["Discounts"] = {},
+        ["Icon"] = "rbxassetid://16136376184",
+        ["Reward"] = {
+            ["Value"] = "New Year's Spear",
+            ["Type"] = "SwordSkin",
+            ["Name"] = "New Year's Spear",
+            ["Award"] = "function: 0x000000000e9bfb2b"
+        }
+    },
+    ["Item10"] = {
+        ["Currency"] = "Balloons",
+        ["ItemID"] = "Item10",
         ["Price"] = 50,
-        ["Chance"] = 5,
+        ["Chance"] = 5.25,
         ["Discounts"] = {},
         ["Icon"] = "rbxassetid://14737693993",
         ["Reward"] = {
             ["Value"] = 1800,
             ["Type"] = "Boost",
             ["Name"] = "2x Coins (30 min)",
-            ["Award"] = "function: 0x000000006c0c747e"
-        }
-    },
-    ["Item10"] = {
-        ["Currency"] = "Pumpkins",
-        ["ItemID"] = "Item10",
-        ["Price"] = 50,
-        ["Chance"] = 5,
-        ["Discounts"] = {},
-        ["Icon"] = "rbxassetid://15051835513",
-        ["Reward"] = {
-            ["Value"] = 1,
-            ["Type"] = "SeasonPassTier",
-            ["Name"] = "+1 Season Pass Tier",
-            ["Award"] = "function: 0x000000006f9faf7e"
+            ["Award"] = "function: 0x0000000043061263"
         }
     },
     ["Item8"] = {
-        ["Currency"] = "Pumpkins",
+        ["Currency"] = "Balloons",
         ["ItemID"] = "Item8",
         ["Price"] = 150,
-        ["Chance"] = 5,
+        ["Chance"] = 5.25,
         ["Discounts"] = {},
-        ["Icon"] = "rbxassetid://15049303003",
+        ["Icon"] = "rbxassetid://16039967646",
         ["Reward"] = {
-            ["Value"] = "PremiumExplosion",
+            ["Value"] = "PremiumSword",
             ["Type"] = "Crate",
-            ["GetName"] = "function: 0x0000000009de76e6",
-            ["Award"] = "function: 0x00000000dbedd296"
-        }
-    },
-    ["Item14"] = {
-        ["Price"] = 799,
-        ["Currency"] = "Robux",
-        ["ItemID"] = "Item14",
-        ["ProductId"] = 1667316701,
-        ["Chance"] = 0.1,
-        ["Discounts"] = {
-            {["Chance"] = 4, ["Price"] = 400, ["ProductId"] = 1667316499},
-            {["Chance"] = 26, ["Price"] = 499, ["ProductId"] = 1667316567},
-            {["Chance"] = 40, ["Price"] = 699, ["ProductId"] = 1667316650},
-            {["Chance"] = 30, ["Price"] = 799, ["ProductId"] = 1667316701}
-        },
-        ["Icon"] = "rbxassetid://15055505690",
-        ["Reward"] = {
-            ["Value"] = "Emote12",
-            ["Type"] = "Emote",
-            ["Name"] = "Emote12 Emote",
-            ["Award"] = "function: 0x000000000b19e2f6"
+            ["GetName"] = "function: 0x00000000a34262bb",
+            ["Award"] = "function: 0x000000004d4aa46b"
         }
     },
     ["Item3"] = {
-        ["Currency"] = "Pumpkins",
+        ["Currency"] = "Balloons",
         ["ItemID"] = "Item3",
-        ["Price"] = 30,
-        ["Chance"] = 18,
-        ["Discounts"] = {},
-        ["Icon"] = "rbxassetid://15038384377",
-        ["Reward"] = {
-            ["Value"] = 250,
-            ["Type"] = "Coins",
-            ["Name"] = "+250 Coins",
-            ["Award"] = "function: 0x00000000b637f2ce"
-        }
-    },
-    ["Item4"] = {
-        ["Currency"] = "Pumpkins",
-        ["ItemID"] = "Item4",
         ["Price"] = 60,
-        ["Chance"] = 22.77,
+        ["Chance"] = 18,
         ["Discounts"] = {},
         ["Icon"] = "rbxassetid://15038384377",
         ["Reward"] = {
             ["Value"] = 500,
             ["Type"] = "Coins",
             ["Name"] = "+500 Coins",
-            ["Award"] = "function: 0x00000000a5bfb3c6"
+            ["Award"] = "function: 0x00000000f544eb4b"
         }
     },
-    ["Item2"] = {
-        ["Currency"] = "Pumpkins",
-        ["ItemID"] = "Item2",
-        ["Price"] = 600,
-        ["Chance"] = 4,
-        ["Discounts"] = {
-            {["Chance"] = 20, ["Price"] = 400},
-            {["Chance"] = 20, ["Price"] = 450},
-            {["Chance"] = 20, ["Price"] = 500},
-            {["Chance"] = 20, ["Price"] = 550},
-            {["Chance"] = 20, ["Price"] = 600}
-        },
-        ["Icon"] = "rbxassetid://15113645202",
-        ["Reward"] = {
-            ["Value"] = 1,
-            ["Type"] = "Gacha",
-            ["Name"] = "+1 Halloween Spin",
-            ["Award"] = "function: 0x0000000098b7f196"
-        }
-    },
-    ["Item5"] = {
-        ["Currency"] = "Pumpkins",
-        ["ItemID"] = "Item5",
+    ["Item4"] = {
+        ["Currency"] = "Balloons",
+        ["ItemID"] = "Item4",
         ["Price"] = 100,
         ["Chance"] = 5,
         ["Discounts"] = {},
@@ -120,12 +66,26 @@ data = {
             ["Value"] = 1,
             ["Type"] = "WheelSpins",
             ["Name"] = "+1 Wheel Spin",
-            ["Award"] = "function: 0x000000005122cfc6"
+            ["Award"] = "function: 0x0000000069c1ee9b"
         }
     },
-    ["Item6"] = {
-        ["Currency"] = "Pumpkins",
-        ["ItemID"] = "Item6",
+    ["Item2"] = {
+        ["Currency"] = "Balloons",
+        ["ItemID"] = "Item2",
+        ["Price"] = 30,
+        ["Chance"] = 21.12,
+        ["Discounts"] = {},
+        ["Icon"] = "rbxassetid://15038384377",
+        ["Reward"] = {
+            ["Value"] = 250,
+            ["Type"] = "Coins",
+            ["Name"] = "+250 Coins",
+            ["Award"] = "function: 0x000000002c2712b3"
+        }
+    },
+    ["Item5"] = {
+        ["Currency"] = "Balloons",
+        ["ItemID"] = "Item5",
         ["Price"] = 120,
         ["Chance"] = 5,
         ["Discounts"] = {},
@@ -134,40 +94,12 @@ data = {
             ["Value"] = 1000,
             ["Type"] = "Coins",
             ["Name"] = "+1000 Coins",
-            ["Award"] = "function: 0x000000001c8712be"
+            ["Award"] = "function: 0x0000000093a07b23"
         }
     },
-    ["Item12"] = {
-        ["Currency"] = "Pumpkins",
-        ["ItemID"] = "Item12",
-        ["Price"] = 7000,
-        ["Chance"] = 0.03,
-        ["Discounts"] = {},
-        ["Icon"] = "rbxassetid://15055510663",
-        ["Reward"] = {
-            ["Value"] = "Spooky Sightblade",
-            ["Type"] = "SwordSkin",
-            ["Name"] = "Spooky Sightblade",
-            ["Award"] = "function: 0x000000004050f8a6"
-        }
-    },
-    ["Item9"] = {
-        ["Currency"] = "Pumpkins",
-        ["ItemID"] = "Item9",
-        ["Price"] = 150,
-        ["Chance"] = 5,
-        ["Discounts"] = {},
-        ["Icon"] = "rbxassetid://15049301853",
-        ["Reward"] = {
-            ["Value"] = "PremiumSword",
-            ["Type"] = "Crate",
-            ["GetName"] = "function: 0x00000000bdf2ad86",
-            ["Award"] = "function: 0x0000000027836fb6"
-        }
-    },
-    ["Item7"] = {
-        ["Currency"] = "Pumpkins",
-        ["ItemID"] = "Item7",
+    ["Item6"] = {
+        ["Currency"] = "Balloons",
+        ["ItemID"] = "Item6",
         ["Price"] = 180,
         ["Chance"] = 5,
         ["Discounts"] = {},
@@ -176,12 +108,12 @@ data = {
             ["Value"] = 1500,
             ["Type"] = "Coins",
             ["Name"] = "+1500 Coins",
-            ["Award"] = "function: 0x00000000efced676"
+            ["Award"] = "function: 0x00000000380fe39b"
         }
     },
-    ["Item13"] = {
-        ["Currency"] = "Pumpkins",
-        ["ItemID"] = "Item13",
+    ["Item12"] = {
+        ["Currency"] = "Balloons",
+        ["ItemID"] = "Item12",
         ["Price"] = 150,
         ["Chance"] = 3.1,
         ["Discounts"] = {},
@@ -190,21 +122,63 @@ data = {
             ["Value"] = 1,
             ["Type"] = "UpdateGift",
             ["Name"] = "+1 Update Gift",
-            ["Award"] = "function: 0x00000000cedb9256"
+            ["Award"] = "function: 0x0000000056de142b"
+        }
+    },
+    ["Item9"] = {
+        ["Currency"] = "Balloons",
+        ["ItemID"] = "Item9",
+        ["Price"] = 50,
+        ["Chance"] = 5,
+        ["Discounts"] = {},
+        ["Icon"] = "rbxassetid://16127729716",
+        ["Reward"] = {
+            ["Value"] = 1,
+            ["Type"] = "SeasonPassTier",
+            ["Name"] = "+1 Season Pass Tier",
+            ["Award"] = "function: 0x0000000057083cf3"
+        }
+    },
+    ["Item7"] = {
+        ["Currency"] = "Balloons",
+        ["ItemID"] = "Item7",
+        ["Price"] = 150,
+        ["Chance"] = 5.25,
+        ["Discounts"] = {},
+        ["Icon"] = "rbxassetid://15049303003",
+        ["Reward"] = {
+            ["Value"] = "PremiumExplosion",
+            ["Type"] = "Crate",
+            ["GetName"] = "function: 0x0000000005d8b08b",
+            ["Award"] = "function: 0x00000000017920d3"
+        }
+    },
+    ["Item13"] = {
+        ["Currency"] = "Balloons",
+        ["ItemID"] = "Item13",
+        ["Price"] = 150,
+        ["Chance"] = 4,
+        ["Discounts"] = {},
+        ["Icon"] = "rbxassetid://16039967646",
+        ["Reward"] = {
+            ["Value"] = "PremiumSword",
+            ["Type"] = "Crate",
+            ["GetName"] = "function: 0x00000000ccec56eb",
+            ["Award"] = "function: 0x00000000b4163983"
         }
     },
     ["Item1"] = {
-        ["Currency"] = "Pumpkins",
+        ["Currency"] = "Balloons",
         ["ItemID"] = "Item1",
         ["Price"] = 100,
-        ["Chance"] = 17,
+        ["Chance"] = 18,
         ["Discounts"] = {},
-        ["Icon"] = "rbxassetid://15047413867",
+        ["Icon"] = "rbxassetid://16130315794",
         ["Reward"] = {
-            ["Value"] = "HalloweenExplosion",
+            ["Value"] = "NewYearExplosion",
             ["Type"] = "Crate",
-            ["GetName"] = "function: 0x00000000a8a82e5e",
-            ["Award"] = "function: 0x0000000033205e56"
+            ["GetName"] = "function: 0x000000009f93039b",
+            ["Award"] = "function: 0x000000002695923b"
         }
     }
 }
