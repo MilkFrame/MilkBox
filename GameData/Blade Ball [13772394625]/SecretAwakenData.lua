78c9d2cfaef97db8ec4e96139ee9c44669136d54
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:42:46 +1100

    Create SecretAwakenData.lua

---
+++ GameData/Blade Ball [13772394625]/SecretAwakenData.lua
@@ -0,0 +1,62 @@
+data = {
+    ["GetSecretRate"] = "function: 0x00000000aee8b023",
+    ["Requirement"] = 1500,
+    ["Void Hammer"] = {
+        ["Awakened"] = {
+            ["Value"] = "Awakened Void Hammer",
+            ["Type"] = "Sword",
+            ["Icon"] = "rbxassetid://16103232777",
+            ["DisplayName"] = "Awakened Void Hammer"
+        },
+        ["Regular"] = {
+            ["Value"] = "Void Hammer",
+            ["Type"] = "Sword",
+            ["Icon"] = "rbxassetid://16103231392",
+            ["DisplayName"] = "Void Hammer"
+        }
+    },
+    ["Righteous Blade"] = {
+        ["Awakened"] = {
+            ["Value"] = "Awakened Righteous Blade",
+            ["Type"] = "Sword",
+            ["Icon"] = "rbxassetid://16103227301",
+            ["DisplayName"] = "Awakened Righteous Blade"
+        },
+        ["Regular"] = {
+            ["Value"] = "Righteous Blade",
+            ["Type"] = "Sword",
+            ["Icon"] = "rbxassetid://16103226361",
+            ["DisplayName"] = "Righteous Blade"
+        }
+    },
+    ["Emperor's Axe"] = {
+        ["Awakened"] = {
+            ["Value"] = "Awakened Emperor's Axe",
+            ["Type"] = "Sword",
+            ["Icon"] = "rbxassetid://16103229820",
+            ["DisplayName"] = "Awakened Emperor's Axe"
+        },
+        ["Regular"] = {
+            ["Value"] = "Emperor's Axe",
+            ["Type"] = "Sword",
+            ["Icon"] = "rbxassetid://16103229036",
+            ["DisplayName"] = "Emperor's Axe"
+        }
+    },
+    ["Titan's Gleam"] = {
+        ["Awakened"] = {
+            ["Value"] = "Awakened Titan's Gleam",
+            ["Type"] = "Sword",
+            ["Icon"] = "rbxassetid://16132471653",
+            ["DisplayName"] = "Awakened Titan's Gleam"
+        },
+        ["Regular"] = {
+            ["Value"] = "Titan's Gleam",
+            ["Type"] = "Sword",
+            ["Icon"] = "rbxassetid://16132471055",
+            ["DisplayName"] = "Titan's Gleam"
+        }
+    },
+    ["NormalOdds"] = 0.0002,
+    ["PremiumOdds"] = 0.008
+}
