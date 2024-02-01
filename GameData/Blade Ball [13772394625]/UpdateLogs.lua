459765873b14ba6fe4dda8697c6419089841a9c5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:43:26 +1100

    Create UpdateLogs.lua

---
+++ GameData/Blade Ball [13772394625]/UpdateLogs.lua
@@ -0,0 +1,29 @@
+data = {
+    {
+        ["Subtext"] = [[<font color="rgb(255, 0, 0)">(Limited time only)</font>]],
+        ["Title"] = "New Year's Season!",
+        ["ItemIcons"] = {"rbxassetid://16125258147", "rbxassetid://16136299818", "rbxassetid://16136287074"},
+        ["Date"] = "1706374800000",
+        ["Description"] = [[		👀 Brand New Blade Ball update is out now!\n		🔑 Gold Machine!\n		⭐ New Years Battlepass!\n		⌛ Time Hole Ability!\n		🎁 Futuristic Hourly Rewards\n		❓ Secret Swords\n		🛡️ Clan additions\n		🌟 Clan Roles\n		📋 Clan Manager\n		🤑 Sci-Fi Spin\n		👀 Invite Friend Reward\n		💥 No Ability Ranked Random Reward\n		]],
+        ["Thumbnail"] = "rbxassetid://15937853529",
+        ["SubDescription"] = "New Year's Season Pass & new ability: Time Hole! New Secret rarity swords & explosions in crates, new Gold Machine, Futuristic Spins, & clan features!"
+    },
+    {
+        ["Subtext"] = [[<font color="rgb(255, 0, 0)"></font>]],
+        ["Title"] = "Infection Mode & Banana Event!",
+        ["ItemIcons"] = {},
+        ["Date"] = "1705770000000",
+        ["Description"] = [[		• Infection limited time mode!\n		• New ability: Absolute Confidence!\n		• Banana Event - earn Banana-themed items!\n		• New live event phase!\n		• Explosion favoriting!\n		• Random shuffle!\n		]],
+        ["Thumbnail"] = "",
+        ["SubDescription"] = ""
+    },
+    {
+        ["Subtext"] = [[<font color="rgb(255, 0, 0)"></font>]],
+        ["Title"] = "The Sentinel returns...",
+        ["ItemIcons"] = {},
+        ["Date"] = "1705165200000",
+        ["Description"] = [[		• New live event!\n		• FREE hourly spin rewards!\n		• New ability: Martyrdom!\n		• Ranked No Ability game mode!\n		• Clan settings & Clan management!\n		• New spin wheel sword: Gilded Glory!\n		]],
+        ["Thumbnail"] = "",
+        ["SubDescription"] = ""
+    }
+}
