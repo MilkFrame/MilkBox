75ed090cd44f7164e48806c3e4848174f61a51c2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:16:22 +1100

    Create GeolocationWhitelist.lua

---
+++ GameData/Blade Ball [13772394625]/GeolocationWhitelist.lua
@@ -0,0 +1,6 @@
+data = {
+    ["Anime Sword"] = {["JP"] = true},
+    ["ClanPointsShop"] = {["CA"] = true, ["US"] = true},
+    ["GamePassDisplay"] = {["PH"] = true, ["TH"] = true, ["VN"] = true, ["ID"] = true, ["KH"] = true},
+    ["DailyLogin"] = {["CA"] = true}
+}
