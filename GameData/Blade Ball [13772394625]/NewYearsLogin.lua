a358f85a79a6bb6c71bfbfd64d9bf58b752a8361
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:34:32 +1100

    Create NewYearsLogin.lua

---
+++ GameData/Blade Ball [13772394625]/NewYearsLogin.lua
@@ -0,0 +1,22 @@
+data = {
+    ["End"] = "1704646800000",
+    ["Start"] = "1704038400000",
+    ["StreakHoursRewards"] = {
+        {["Value"] = 2, ["Type"] = "NewYearSpins", ["Icon"] = "rbxassetid://15785934484", ["DisplayName"] = "x2 Spins"},
+        {
+            ["Value"] = 10,
+            ["Type"] = "NewYearSpins",
+            ["Icon"] = "rbxassetid://15785934484",
+            ["DisplayName"] = "x10 Spins"
+        },
+        {["Value"] = 4, ["Type"] = "NewYearSpins", ["Icon"] = "rbxassetid://15785934484", ["DisplayName"] = "x4 Spins"},
+        {["Value"] = 7, ["Type"] = "NewYearSpins", ["Icon"] = "rbxassetid://15785934484", ["DisplayName"] = "x7 Spins"}
+    },
+    ["HourlyReward"] = {
+        ["Value"] = 1,
+        ["Type"] = "NewYearSpins",
+        ["Icon"] = "rbxassetid://15785934484",
+        ["DisplayName"] = "x1 Spins"
+    },
+    ["DebugTimeOffset"] = 0
+}
