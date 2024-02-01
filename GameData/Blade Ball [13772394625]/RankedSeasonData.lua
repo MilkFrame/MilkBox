81ae79f5b13bdea254a4732e9ea8ca96c296d019
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:24:42 +1100

    Update RankedSeasonData.lua

--- GameData/Blade Ball [13772394625]/RankedSeasonData.lua
+++ GameData/Blade Ball [13772394625]/RankedSeasonData.lua
@@ -1,8 +1,116 @@
 data = {
-    ["CurrentSeason"] = 1,
-    ["Seasons"] = {{["EndTime"] = "1698760800000", ["StartTime"] = "1696122000000"}},
+    ["GetCurrentSeason"] = "function: 0x00000000ec2b4c53",
+    ["Seasons"] = {
+        ["Normal"] = {
+            {["EndTime"] = "1699722000000", ["StartTime"] = "1696122000000"},
+            {["EndTime"] = "1703610000000", ["StartTime"] = "1699722000000"},
+            {["EndTime"] = "1706374800000", ["StartTime"] = "1703610000000"},
+            {["EndTime"] = "1710003600000", ["StartTime"] = "1706374800000"}
+        },
+        ["NoAbility"] = {
+            {["EndTime"] = "1706374800000", ["StartTime"] = "1704906000000"},
+            {["EndTime"] = "1710003600000", ["StartTime"] = "1706374800000"}
+        }
+    },
+    ["GetRankedType"] = "function: 0x000000005fee0a4b",
+    ["Rewards"] = {
+        ["Normal"] = {
+            ["1"] = {
+                {
+                    ["Rewards"] = {{["Icon"] = "rbxassetid://14923456813", ["Sword"] = "Ranked Season 1 Top 1 Sword"}},
+                    ["Rank"] = 1
+                },
+                {
+                    ["Rewards"] = {{["Icon"] = "rbxassetid://14923474986", ["Sword"] = "Ranked Season 1 Top 50 Sword"}},
+                    ["Rank"] = 50
+                },
+                {
+                    ["Rewards"] = {{["Icon"] = "rbxassetid://14923478749", ["Sword"] = "Ranked Season 1 Top 200 Sword"}},
+                    ["Rank"] = 200
+                }
+            },
+            ["4"] = {
+                {
+                    ["Rewards"] = {{["Icon"] = "rbxassetid://16138991986", ["Sword"] = "Ranked Season 4 Top 1 Sword"}},
+                    ["Rank"] = 1
+                },
+                {
+                    ["Rewards"] = {{["Icon"] = "rbxassetid://16138996452", ["Sword"] = "Ranked Season 4 Top 25 Sword"}},
+                    ["Rank"] = 25
+                },
+                {
+                    ["Rewards"] = {{["Icon"] = "rbxassetid://16138999325", ["Sword"] = "Ranked Season 4 Top 100 Sword"}},
+                    ["Rank"] = 100
+                }
+            },
+            ["3"] = {
+                {
+                    ["Rewards"] = {{["Icon"] = "rbxassetid://15759882708", ["Sword"] = "Ranked Season 3 Top 1 Sword"}},
+                    ["Rank"] = 1
+                },
+                {
+                    ["Rewards"] = {{["Icon"] = "rbxassetid://15759882472", ["Sword"] = "Ranked Season 3 Top 50 Sword"}},
+                    ["Rank"] = 50
+                },
+                {
+                    ["Rewards"] = {{["Icon"] = "rbxassetid://15759882253", ["Sword"] = "Ranked Season 3 Top 200 Sword"}},
+                    ["Rank"] = 200
+                }
+            },
+            ["2"] = {
+                {
+                    ["Rewards"] = {{["Icon"] = "rbxassetid://15308721554", ["Sword"] = "Ranked Season 2 Top 1 Sword"}},
+                    ["Rank"] = 1
+                },
+                {
+                    ["Rewards"] = {{["Icon"] = "rbxassetid://15308721702", ["Sword"] = "Ranked Season 2 Top 50 Sword"}},
+                    ["Rank"] = 50
+                },
+                {
+                    ["Rewards"] = {{["Icon"] = "rbxassetid://15308721826", ["Sword"] = "Ranked Season 2 Top 200 Sword"}},
+                    ["Rank"] = 200
+                }
+            }
+        },
+        ["NoAbility"] = {
+            ["1"] = {
+                {
+                    ["Rewards"] = {
+                        {
+                            ["RandomAbilities"] = {"Infinity", "Death Slash", "Dribble"},
+                            ["Icon"] = "rbxassetid://15962769255"
+                        }
+                    },
+                    ["Rank"] = 50
+                }
+            },
+            ["2"] = {
+                {
+                    ["Rewards"] = {
+                        {["Icon"] = "rbxassetid://16138991986", ["Sword"] = "Ranked NA Season 2 Top 1 Sword"}
+                    },
+                    ["Rank"] = 1
+                },
+                {
+                    ["Rewards"] = {
+                        {["Icon"] = "rbxassetid://16138996452", ["Sword"] = "Ranked NA Season 2 Top 25 Sword"}
+                    },
+                    ["Rank"] = 25
+                },
+                {
+                    ["Rewards"] = {
+                        {["Icon"] = "rbxassetid://16138999325", ["Sword"] = "Ranked NA Season 2 Top 100 Sword"}
+                    },
+                    ["Rank"] = 100
+                }
+            }
+        }
+    },
+    ["MaxMatchHistory"] = 50,
     ["Modes"] = {
         ["FFA"] = {["PlayersPerTeam"] = 1, ["MaxTeams"] = 8, ["DisplayName"] = "FFA"},
         ["Duo"] = {["PlayersPerTeam"] = 2, ["MaxTeams"] = 8, ["DisplayName"] = "2v2 Duos"}
-    }
+    },
+    ["RankedTypes"] = {"Normal", "NoAbility"},
+    ["SeasonChanged"] = "Signal Event"
 }
