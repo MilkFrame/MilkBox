88737b1260cab778b664c57301446bfe6c61ac48
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:41:30 +1100

    Create ClansRankData.lua

---
+++ GameData/Blade Ball [13772394625]/ClansRankData.lua
@@ -0,0 +1,31 @@
+data = {
+    ["DefaultPermissions"] = {["Admin"] = 1, ["Co-Owner"] = 383},
+    ["ReplacedLegacyRoles"] = {["Underboss"] = "Admin"},
+    ["getPermissionBitfield"] = "function: 0x00000000b07dbaeb",
+    ["Ranks"] = {"Member", "Admin", "Co-Owner", "Owner"},
+    ["getPermissionsBitfield"] = "function: 0x00000000e63912f3",
+    ["hasPermissionFor"] = "function: 0x00000000e2b5f633",
+    ["Permissions"] = {
+        ["INVITE_MEMBERS"] = 0,
+        ["CHANGE_BADGE"] = 1,
+        ["CHANGE_PERMISSIONS"] = 7,
+        ["MANAGE_APPLICATIONS"] = 2,
+        ["POST_SHOUT"] = 4,
+        ["CHANGE_REQUIREMENTS"] = 9,
+        ["CHANGE_DESCRIPTION"] = 8,
+        ["VIEW_AUDIT_LOGS"] = 5,
+        ["VIEW_PERMISSIONS"] = 6,
+        ["SET_RECOMMENDED_UPGRADE"] = 3
+    },
+    ["RanksColor"] = {
+        ["Co-Owner"] = Color3.fromRGB(0, 255, 51),
+        ["Admin"] = Color3.fromRGB(255, 56, 56),
+        ["Member"] = Color3.fromRGB(180, 180, 180),
+        ["Underboss"] = Color3.fromRGB(255, 56, 56),
+        ["Owner"] = Color3.fromRGB(255, 200, 0)
+    },
+    ["isValidPermissionsBitfield"] = "function: 0x00000000fe8aa14b",
+    ["getBitfieldPermissions"] = "function: 0x00000000890166c3",
+    ["getRank"] = "function: 0x00000000d809785b",
+    ["hasPermission"] = "function: 0x0000000054f474db"
+}
