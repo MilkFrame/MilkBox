f2001d10ad6ebbfa51f0b26fcaeb124caefd69cc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:15:43 +1100

    Create CratesContent.lua

---
+++ GameData/Blade Ball [13772394625]/CratesContent.lua
@@ -0,0 +1,366 @@
+data = {
+    ["SerpentLiveEventCrate"] = {
+        {
+            ["Chance"] = 100,
+            ["Item"] = {
+                ["Lootbox"] = {
+                    {["Item"] = {["Explosions"] = {"Serpent's Rage"}}, ["Chance"] = 30.5},
+                    {["Item"] = {["Swords"] = {"Serpent's Lance"}}, ["Chance"] = 31},
+                    {["Item"] = {["Emotes"] = {"Emote35"}}, ["Chance"] = 23},
+                    {["Item"] = {["Swords"] = {"Serpent's Fang"}}, ["Chance"] = 10.5},
+                    {["Item"] = {["Abilities"] = {"Serpent Shadow Clone"}}, ["Chance"] = 5}
+                }
+            }
+        }
+    },
+    ["SmallChestOfCoins"] = {
+        {
+            ["Chance"] = 80,
+            ["Icon"] = "rbxassetid://14713794582",
+            ["Item"] = {["Lootbox"] = {{["Item"] = {["Coins"] = {"100Coins"}}, ["Chance"] = 1}}}
+        },
+        {
+            ["Chance"] = 15,
+            ["Icon"] = "rbxassetid://14713779371",
+            ["Item"] = {["Lootbox"] = {{["Item"] = {["Coins"] = {"250Coins"}}, ["Chance"] = 1}}}
+        },
+        {
+            ["Chance"] = 4,
+            ["Icon"] = "rbxassetid://14713785287",
+            ["Item"] = {["Lootbox"] = {{["Item"] = {["Coins"] = {"350Coins"}}, ["Chance"] = 1}}}
+        },
+        {
+            ["Chance"] = 1,
+            ["Icon"] = "rbxassetid://14713787523",
+            ["Item"] = {["Lootbox"] = {{["Item"] = {["Coins"] = {"500Coins"}}, ["Chance"] = 1}}}
+        }
+    },
+    ["HalloweenSwordCrate"] = {
+        {
+            ["Chance"] = 100,
+            ["Item"] = {
+                ["Lootbox"] = {
+                    {["Chance"] = 4, ["Item"] = {["Swords"] = {"Hellfire Shadowslicer"}}},
+                    {["Chance"] = 23, ["Item"] = {["Swords"] = {"Bloodgazer's Edge"}}},
+                    {["Chance"] = 39, ["Item"] = {["Swords"] = {"Demonblade"}}},
+                    {["Chance"] = 27, ["Item"] = {["Swords"] = {"Witch's Broomstick"}}},
+                    {["Chance"] = 6, ["Item"] = {["Swords"] = {"Dreadbone Greataxe"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Staff of The Accursed"}}}
+                }
+            }
+        }
+    },
+    ["PremiumSwordCrate"] = {
+        {
+            ["Chance"] = 10,
+            ["Item"] = {
+                ["Lootbox"] = {
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Musara"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Aurorum"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Cyber Blade"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Trident"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Holy Sword"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Excalibur"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Darkheart"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Firebrand"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Zoru"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Ghostwalker"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Windforce"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Illumina"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Solans"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Solarus"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Thunderous Hammer"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Venomshank"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Holy Axe"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Void Sword"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Dune Cleaver"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Soul Scythe"}}}
+                }
+            }
+        },
+        {
+            ["Chance"] = 90,
+            ["Item"] = {
+                ["Lootbox"] = {
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Plunger"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Frying Pan"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Rifle"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Rockxcalibur"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Blocky Sword"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Spearbeard"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Dragonslayer Blade"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Executive Greatsword"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Etherium Dagger"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Bonsai Tree"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Comically Large Spoon"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Glowy Bone"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Red Saber"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Spark Sword"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Vengeful Dagger"}}}
+                }
+            }
+        }
+    },
+    ["SecretExplosionCrate"] = {
+        {["Chance"] = 50, ["Item"] = {["Lootbox"] = {{["Item"] = {["Explosions"] = {"L Explosion"}}, ["Chance"] = 1}}}},
+        {["Chance"] = 50, ["Item"] = {["Lootbox"] = {{["Item"] = {["Explosions"] = {"Disco Ball"}}, ["Chance"] = 1}}}}
+    },
+    ["SecretSwordCrate"] = {
+        {
+            ["Chance"] = 33,
+            ["Icon"] = "rbxassetid://16103231392",
+            ["Item"] = {["Lootbox"] = {{["Item"] = {["Swords"] = {"Void Hammer"}}, ["Chance"] = 1}}}
+        },
+        {
+            ["Chance"] = 33,
+            ["Icon"] = "rbxassetid://16103226361",
+            ["Item"] = {["Lootbox"] = {{["Item"] = {["Swords"] = {"Righteous Blade"}}, ["Chance"] = 1}}}
+        },
+        {
+            ["Chance"] = 33,
+            ["Icon"] = "rbxassetid://16103229036",
+            ["Item"] = {["Lootbox"] = {{["Item"] = {["Swords"] = {"Emperor's Axe"}}, ["Chance"] = 1}}}
+        }
+    },
+    ["BigChestOfCoins"] = {
+        {
+            ["Chance"] = 70,
+            ["Icon"] = "rbxassetid://14713797174",
+            ["Item"] = {["Lootbox"] = {{["Item"] = {["Coins"] = {"250Coins"}}, ["Chance"] = 1}}}
+        },
+        {
+            ["Chance"] = 20,
+            ["Icon"] = "rbxassetid://14713794582",
+            ["Item"] = {["Lootbox"] = {{["Item"] = {["Coins"] = {"500Coins"}}, ["Chance"] = 1}}}
+        },
+        {
+            ["Chance"] = 8,
+            ["Icon"] = "rbxassetid://14713779371",
+            ["Item"] = {["Lootbox"] = {{["Item"] = {["Coins"] = {"750Coins"}}, ["Chance"] = 1}}}
+        },
+        {
+            ["Chance"] = 1.9,
+            ["Icon"] = "rbxassetid://14713785287",
+            ["Item"] = {["Lootbox"] = {{["Item"] = {["Coins"] = {"1000Coins"}}, ["Chance"] = 1}}}
+        },
+        {
+            ["Chance"] = 0.1,
+            ["Icon"] = "rbxassetid://14713787523",
+            ["Item"] = {["Lootbox"] = {{["Item"] = {["Coins"] = {"1500Coins"}}, ["Chance"] = 1}}}
+        }
+    },
+    ["NormalSwordCrate"] = {
+        {
+            ["Chance"] = 1,
+            ["Item"] = {
+                ["Lootbox"] = {
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Musara"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Aurorum"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Cyber Blade"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Trident"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Holy Sword"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Excalibur"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Darkheart"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Firebrand"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Zoru"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Ghostwalker"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Windforce"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Illumina"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Solans"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Solarus"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Thunderous Hammer"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Venomshank"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Holy Axe"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Void Sword"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Dune Cleaver"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Soul Scythe"}}}
+                }
+            }
+        },
+        {
+            ["Chance"] = 10,
+            ["Item"] = {
+                ["Lootbox"] = {
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Plunger"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Frying Pan"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Rifle"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Rockxcalibur"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Blocky Sword"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Spearbeard"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Dragonslayer Blade"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Executive Greatsword"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Etherium Dagger"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Bonsai Tree"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Comically Large Spoon"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Glowy Bone"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Red Saber"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Spark Sword"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Vengeful Dagger"}}}
+                }
+            }
+        },
+        {
+            ["Chance"] = 89,
+            ["Item"] = {
+                ["Lootbox"] = {
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Battle Axe"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Wooden Hammer"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Emerald Warden Blade"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Tanto"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Stick"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Massive Lolipop"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Pickaxe"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Rapier"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Baseball Bat"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Melon Slice"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Dark Blade"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Kitchen Knife"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Bone"}}}
+                }
+            }
+        }
+    },
+    ["LiveEventCrate"] = {
+        {
+            ["Chance"] = 100,
+            ["Item"] = {
+                ["Lootbox"] = {
+                    {["Chance"] = 3, ["Item"] = {["Swords"] = {"Serpent’s Shiv"}}},
+                    {["Chance"] = 1, ["Item"] = {["Swords"] = {"Serpent’s Sickle"}}}
+                }
+            }
+        }
+    },
+    ["NormalExplosionCrate"] = {
+        {
+            ["Chance"] = 3,
+            ["Item"] = {
+                ["Lootbox"] = {
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Blackhole Blue"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Blackhole Green"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Blackhole Normal"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Blackhole Red"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Blackhole White"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Blackhole Yellow"}}}
+                }
+            }
+        },
+        {
+            ["Chance"] = 10,
+            ["Item"] = {
+                ["Lootbox"] = {
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Sakura Blue"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Sakura Green"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Sakura Normal"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Sakura Purple"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Sakura Red"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Sakura Yellow"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Waterblast Black"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Waterblast Green"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Waterblast Normal"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Waterblast Purple"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Waterblast Red"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Waterblast Yellow"}}}
+                }
+            }
+        },
+        {
+            ["Chance"] = 87,
+            ["Item"] = {
+                ["Lootbox"] = {
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Explosion Blue"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Explosion Green"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Explosion Normal"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Explosion Purple"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Explosion Red"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Explosion White"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Lightning Green"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Lightning Normal"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Lightning Purple"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Lightning Red"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Lightning White"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Lightning Yellow"}}}
+                }
+            }
+        }
+    },
+    ["AncientSwordChest"] = {
+        {
+            ["Chance"] = 75,
+            ["Icon"] = "rbxassetid://15316186440",
+            ["Item"] = {["Lootbox"] = {{["Item"] = {["Swords"] = {"Ancient Cutlass"}}, ["Chance"] = 1}}}
+        },
+        {
+            ["Chance"] = 20,
+            ["Icon"] = "rbxassetid://15316201428",
+            ["Item"] = {["Lootbox"] = {{["Item"] = {["Swords"] = {"Ancient Spear"}}, ["Chance"] = 1}}}
+        },
+        {
+            ["Chance"] = 4,
+            ["Icon"] = "rbxassetid://15316213854",
+            ["Item"] = {["Lootbox"] = {{["Item"] = {["Swords"] = {"Great Axe"}}, ["Chance"] = 1}}}
+        },
+        {
+            ["Chance"] = 0.9,
+            ["Icon"] = "rbxassetid://15316216519",
+            ["Item"] = {["Lootbox"] = {{["Item"] = {["Swords"] = {"Hallberd"}}, ["Chance"] = 1}}}
+        },
+        {
+            ["Chance"] = 0.1,
+            ["Icon"] = "rbxassetid://15316219581",
+            ["Item"] = {["Lootbox"] = {{["Item"] = {["Swords"] = {"Runic Wrecker"}}, ["Chance"] = 1}}}
+        }
+    },
+    ["ChristmasSwordCrate"] = {
+        {
+            ["Chance"] = 100,
+            ["Item"] = {
+                ["Lootbox"] = {
+                    {["Chance"] = 0.2, ["Item"] = {["Swords"] = {"Snowman Scythe"}}},
+                    {["Chance"] = 12, ["Item"] = {["Swords"] = {"Blizzard Blade"}}},
+                    {["Chance"] = 18, ["Item"] = {["Swords"] = {"North Pole Enchanter"}}},
+                    {["Chance"] = 24, ["Item"] = {["Swords"] = {"Yuletide Saber"}}},
+                    {["Chance"] = 2, ["Item"] = {["Swords"] = {"Santa's Slicer"}}},
+                    {["Chance"] = 8, ["Item"] = {["Swords"] = {"Evergreen Edge"}}},
+                    {["Chance"] = 31, ["Item"] = {["Swords"] = {"Gingerbread Guardian"}}},
+                    {["Chance"] = 4.5, ["Item"] = {["Swords"] = {"Holiday Herald"}}},
+                    {["Chance"] = 0.5, ["Item"] = {["Swords"] = {"Christmas Scythe"}}}
+                }
+            }
+        }
+    },
+    ["PremiumExplosionCrate"] = {
+        {
+            ["Chance"] = 20,
+            ["Item"] = {
+                ["Lootbox"] = {
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Blackhole Blue"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Blackhole Green"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Blackhole Normal"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Blackhole Red"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Blackhole White"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Blackhole Yellow"}}}
+                }
+            }
+        },
+        {
+            ["Chance"] = 80,
+            ["Item"] = {
+                ["Lootbox"] = {
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Sakura Blue"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Sakura Green"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Sakura Normal"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Sakura Purple"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Sakura Red"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Sakura Yellow"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Waterblast Black"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Waterblast Green"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Waterblast Normal"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Waterblast Purple"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Waterblast Red"}}},
+                    {["Chance"] = 1, ["Item"] = {["Explosions"] = {"Waterblast Yellow"}}}
+                }
+            }
+        }
+    }
+}
