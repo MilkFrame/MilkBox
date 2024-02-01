923cd838cfc2d8e9b6a30e40a819e936b89f2686
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:39:18 +1100

    Create GamePasses.lua

---
+++ GameData/Blade Ball [13772394625]/GamePasses.lua
@@ -0,0 +1,29 @@
+data = {
+    {
+        ["GiftName"] = "Instant Spin",
+        ["Name"] = "FastUnbox",
+        ["ProductId"] = 229765926,
+        ["ItemType"] = "GamePass",
+        ["Description"] = "Loot crates now unbox instantly",
+        ["Icon"] = "rbxassetid://14713792391",
+        ["DisplayName"] = "Instant Spin"
+    },
+    {
+        ["GiftName"] = "VIP",
+        ["Name"] = "VIP",
+        ["ProductId"] = 223367086,
+        ["ItemType"] = "GamePass",
+        ["Description"] = [[		-Special "Whitehole" effect\n		-Custom sword slash color\n		-One-time-bonus of 2000c\n		-VIP chat tag\n		]],
+        ["Icon"] = "rbxassetid://14713793356",
+        ["DisplayName"] = "VIP"
+    },
+    {
+        ["GiftName"] = "Double Coins",
+        ["Name"] = "2xCoins",
+        ["ProductId"] = 226785981,
+        ["Description"] = "Receive twice the amount of coins you'd get for normally participating permanently.",
+        ["ItemType"] = "GamePass",
+        ["Icon"] = "rbxassetid://14713789478",
+        ["DisplayName"] = "Double Coins"
+    }
+}
