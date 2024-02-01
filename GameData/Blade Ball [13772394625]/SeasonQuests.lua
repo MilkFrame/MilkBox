8af1f8ec35220250d6488185167b74e5cc9b20be
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:28:00 +1100

    Update SeasonQuests.lua

--- GameData/Blade Ball [13772394625]/SeasonQuests.lua
+++ GameData/Blade Ball [13772394625]/SeasonQuests.lua
@@ -1,24 +1,6 @@
 data = {
     {
         ["Currency"] = "XP",
-        ["DisplayName"] = "Log in to the game",
-        ["Guaranteed"] = true,
-        ["Reward"] = 25,
-        ["CompletionArguments"] = {"Daily_Login", 1},
-        ["Icon"] = "rbxassetid://15046295869",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["DisplayName"] = "Play with 1 friend",
-        ["Reward"] = 50,
-        ["CompletionArguments"] = {"Friend_Login", 1},
-        ["Icon"] = "rbxassetid://15046295767",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Guaranteed"] = true,
         ["Reward"] = 100,
         ["CompletionArguments"] = {"Round_Won", 1},
         ["DisplayName"] = "Win 1 game",
@@ -26,238 +8,145 @@ data = {
     },
     {
         ["Currency"] = "XP",
-        ["Guaranteed"] = true,
         ["Reward"] = 100,
-        ["CompletionArguments"] = {"Round_Won", 2, "2t"},
-        ["DisplayName"] = "Win a 2 team gamemode, 2 times",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["DisplayName"] = "Play for 30 minutes",
-        ["Reward"] = 50,
-        ["CompletionArguments"] = {"Play_Time", 30},
-        ["Icon"] = "rbxassetid://15046295496",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["DisplayName"] = "Play for 90 minutes",
-        ["Reward"] = 100,
-        ["CompletionArguments"] = {"Play_Time", 90},
-        ["Icon"] = "rbxassetid://15046295496",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["DisplayName"] = "Parry the ball 60 times",
-        ["Reward"] = 50,
-        ["CompletionArguments"] = {"Parry_Ball", 60},
-        ["Icon"] = "rbxassetid://15046295651",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["DisplayName"] = "Parry the ball 120 times",
-        ["Reward"] = 75,
-        ["CompletionArguments"] = {"Parry_Ball", 120},
-        ["Icon"] = "rbxassetid://15046295651",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 125,
-        ["CompletionArguments"] = {"Kill_Player", 30},
-        ["DisplayName"] = "Eliminate 30 players",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 100,
-        ["CompletionArguments"] = {"Round_Kill_Count", 5},
-        ["DisplayName"] = "Eliminate 5 enemies in a single match",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["DisplayName"] = "Parry the ball 100 times in Ranked mode",
-        ["Reward"] = 75,
-        ["CompletionArguments"] = {"Parry_Ball", 100, "Ranked"},
-        ["Icon"] = "rbxassetid://15022099579",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 100,
-        ["CompletionArguments"] = {"Kill_Player", 5, "Halloween_Map"},
-        ["DisplayName"] = "Eliminate 5 players on the Halloween maps",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["DisplayName"] = "Play 20 Ranked mode matches",
-        ["Reward"] = 50,
-        ["CompletionArguments"] = {"Play_Round", 20, "Ranked"},
-        ["Icon"] = "rbxassetid://15022099579",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 100,
-        ["CompletionArguments"] = {"Kill_Player", 20, "Pro"},
-        ["DisplayName"] = "Eliminate 20 players in pro servers",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 125,
-        ["CompletionArguments"] = {"Kill_Player", 25, "Pro"},
-        ["DisplayName"] = "Eliminate 25 players in pro servers",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["DisplayName"] = "Eliminate 20 players in Ranked mode",
-        ["Reward"] = 125,
-        ["CompletionArguments"] = {"Kill_Player", 20, "Ranked"},
-        ["Icon"] = "rbxassetid://15022099579",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 50,
-        ["CompletionArguments"] = {"Round_Won", 1, "4t"},
-        ["DisplayName"] = "Win a 4 team gamemode",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 50,
-        ["CompletionArguments"] = {"Play_Round", 15},
-        ["DisplayName"] = "Play 15 matches",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 125,
-        ["CompletionArguments"] = {"Spend_Credits", 5000},
-        ["DisplayName"] = "Spend 5,000 Coins",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 80,
-        ["CompletionArguments"] = {"Use_Ability", 120, "Dash"},
-        ["DisplayName"] = "Use Dash Ability 120 times",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 75,
-        ["CompletionArguments"] = {"Chatted", 20},
-        ["DisplayName"] = "Send 20 chat messages",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 60,
-        ["CompletionArguments"] = {"Top5Alive", 5},
-        ["DisplayName"] = "Be one of the last five players alive, 5 times.",
-        ["QuestType"] = "Daily"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 125,
         ["CompletionArguments"] = {"Round_Won", 2},
         ["DisplayName"] = "Win 2 games",
         ["QuestType"] = "Daily"
     },
     {
         ["Currency"] = "XP",
+        ["DisplayName"] = "Play 15 Ranked mode matches",
         ["Reward"] = 50,
-        ["CompletionArguments"] = {"Kill_Player", 5, "2t"},
-        ["DisplayName"] = "Eliminate 5 people in 2 team mode",
+        ["CompletionArguments"] = {"Play_Round", 15, "Ranked"},
+        ["Icon"] = "rbxassetid://15022099579",
         ["QuestType"] = "Daily"
     },
     {
         ["Currency"] = "XP",
-        ["Reward"] = 50,
-        ["CompletionArguments"] = {"Kill_Player", 5, "4t"},
-        ["DisplayName"] = "Eliminate 5 people in 4 team mode",
+        ["Reward"] = 60,
+        ["CompletionArguments"] = {"Top5Alive", 1},
+        ["DisplayName"] = "Score Top 5 in any mode",
         ["QuestType"] = "Daily"
     },
     {
         ["Currency"] = "XP",
         ["Disabled"] = true,
-        ["Reward"] = 100,
-        ["CompletionArguments"] = {"Invite_To_Game", 5},
-        ["DisplayName"] = "Send 5 game invites to your friends!",
+        ["Reward"] = 60,
+        ["CompletionArguments"] = {"Top5Alive", 1},
+        ["DisplayName"] = "Score Top 5 in any mode",
         ["QuestType"] = "Daily"
     },
     {
         ["Currency"] = "XP",
-        ["Reward"] = 250,
-        ["CompletionArguments"] = {"Round_Kill_Count", 6, "Normal"},
-        ["DisplayName"] = "Eliminate 6 enemies in a single match in Normal mode",
-        ["QuestType"] = "Season"
+        ["Reward"] = 60,
+        ["CompletionArguments"] = {"Top2Alive", 1},
+        ["DisplayName"] = "Score Top 2 in any mode",
+        ["QuestType"] = "Daily"
     },
     {
         ["Currency"] = "XP",
-        ["DisplayName"] = "Hit the ball 1,000 times in Ranked mode",
-        ["Reward"] = 500,
-        ["CompletionArguments"] = {"Parry_Ball", 1000, "Ranked"},
-        ["Icon"] = "rbxassetid://15022099579",
-        ["QuestType"] = "Season"
+        ["Reward"] = 200,
+        ["DisplayName"] = "Play for 60 minutes",
+        ["Premium"] = true,
+        ["CompletionArguments"] = {"Play_Time", 60},
+        ["Icon"] = "rbxassetid://15046295496",
+        ["QuestType"] = "Daily"
     },
     {
         ["Currency"] = "XP",
-        ["Disabled"] = true,
-        ["Reward"] = 100,
-        ["CompletionArguments"] = {"Obstacle_Jump", 50},
-        ["DisplayName"] = "Jump over 50 obstacles",
-        ["QuestType"] = "Season"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 750,
-        ["CompletionArguments"] = {"Kill_Player", 1500, "Normal"},
-        ["DisplayName"] = "Eliminate 1500 people in Normal mode",
-        ["QuestType"] = "Season"
-    },
-    {
-        ["Currency"] = "XP",
-        ["DisplayName"] = "Eliminate 500 people in Ranked mode",
-        ["Reward"] = 750,
-        ["CompletionArguments"] = {"Kill_Player", 500, "Ranked"},
-        ["Icon"] = "rbxassetid://15022099579",
-        ["QuestType"] = "Season"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 250,
-        ["CompletionArguments"] = {"Double_Jump", 800, "Normal"},
-        ["DisplayName"] = "Double-jump 800 times in Normal mode",
-        ["QuestType"] = "Season"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 400,
-        ["CompletionArguments"] = {"Round_Kill_Streak", 7, 5},
-        ["DisplayName"] = "Get 5 eliminations 7 matches in a row",
-        ["QuestType"] = "Season"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 100,
-        ["CompletionArguments"] = {"Match_Time", 200, "Normal"},
-        ["DisplayName"] = "Survive for a total of 200 minutes in Normal mode",
+        ["DisplayName"] = "Play for 120 minutes",
+        ["Reward"] = 50,
+        ["CompletionArguments"] = {"Play_Time", 120},
+        ["Icon"] = "rbxassetid://15046295496",
         ["QuestType"] = "Season"
     },
     {
         ["Currency"] = "XP",
         ["Reward"] = 75,
-        ["CompletionArguments"] = {"Use_Sword_In_Match", 10, "Normal"},
-        ["DisplayName"] = "Play with 10 different swords in Normal mode",
+        ["CompletionArguments"] = {"Use_Sword_In_Match", 10},
+        ["DisplayName"] = "Play with 10 different swords in any mode",
+        ["QuestType"] = "Season"
+    },
+    {
+        ["Currency"] = "XP",
+        ["Reward"] = 50,
+        ["CompletionArguments"] = {"Round_Kill_Count", 2},
+        ["DisplayName"] = "Eliminate 2 enemies in a single match",
+        ["QuestType"] = "Season"
+    },
+    {
+        ["Currency"] = "XP",
+        ["Reward"] = 100,
+        ["CompletionArguments"] = {"Round_Kill_Count", 4},
+        ["DisplayName"] = "Eliminate 4 enemies in a single match",
+        ["QuestType"] = "Season"
+    },
+    {
+        ["Currency"] = "XP",
+        ["Reward"] = 100,
+        ["CompletionArguments"] = {"Match_Time", 200},
+        ["DisplayName"] = "Survive for 200 total minutes",
+        ["QuestType"] = "Season"
+    },
+    {
+        ["Currency"] = "XP",
+        ["DisplayName"] = "Play for 240 total minutes",
+        ["Reward"] = 100,
+        ["CompletionArguments"] = {"Play_Time", 240},
+        ["Icon"] = "rbxassetid://15046295496",
+        ["QuestType"] = "Season"
+    },
+    {
+        ["Currency"] = "XP",
+        ["DisplayName"] = "Play for 360 total minutes",
+        ["Reward"] = 200,
+        ["CompletionArguments"] = {"Play_Time", 360},
+        ["Icon"] = "rbxassetid://15046295496",
+        ["QuestType"] = "Season"
+    },
+    {
+        ["Currency"] = "XP",
+        ["Reward"] = 200,
+        ["Premium"] = true,
+        ["CompletionArguments"] = {"Match_Time", 500},
+        ["DisplayName"] = "Survive for 500 total minutes",
+        ["QuestType"] = "Season"
+    },
+    {
+        ["Currency"] = "XP",
+        ["Reward"] = 250,
+        ["CompletionArguments"] = {"Round_Kill_Count", 6},
+        ["DisplayName"] = "Eliminate 6 enemies in a single match",
+        ["QuestType"] = "Season"
+    },
+    {
+        ["Currency"] = "XP",
+        ["Reward"] = 250,
+        ["CompletionArguments"] = {"Double_Jump", 800},
+        ["DisplayName"] = "Double-jump 800 times in any mode",
+        ["QuestType"] = "Season"
+    },
+    {
+        ["Currency"] = "XP",
+        ["Reward"] = 300,
+        ["CompletionArguments"] = {"Studs_Moved", 50000},
+        ["DisplayName"] = "Move 50,000 studs in any mode",
+        ["QuestType"] = "Season"
+    },
+    {
+        ["Currency"] = "XP",
+        ["DisplayName"] = "Play for 600 total minutes",
+        ["Reward"] = 300,
+        ["CompletionArguments"] = {"Play_Time", 600},
+        ["Icon"] = "rbxassetid://15046295496",
+        ["QuestType"] = "Season"
+    },
+    {
+        ["Currency"] = "XP",
+        ["DisplayName"] = "Win 3 games in a row in any mode",
+        ["Reward"] = 400,
+        ["CompletionArguments"] = {"Win_Streak", 3},
+        ["Icon"] = "rbxassetid://15022099579",
         ["QuestType"] = "Season"
     },
     {
@@ -269,9 +158,18 @@ data = {
     },
     {
         ["Currency"] = "XP",
-        ["Reward"] = 1000,
-        ["CompletionArguments"] = {"Gift_Robux_Item", 1},
-        ["DisplayName"] = "Gift a GamePass or any Robux item",
+        ["DisplayName"] = "Play for 960 total minutes",
+        ["Reward"] = 400,
+        ["CompletionArguments"] = {"Play_Time", 960},
+        ["Icon"] = "rbxassetid://15046295496",
+        ["QuestType"] = "Season"
+    },
+    {
+        ["Currency"] = "XP",
+        ["DisplayName"] = "Parry the ball 1,000 times",
+        ["Reward"] = 500,
+        ["CompletionArguments"] = {"Parry_Ball", 1000},
+        ["Icon"] = "rbxassetid://15046295651",
         ["QuestType"] = "Season"
     },
     {
@@ -290,82 +188,10 @@ data = {
     },
     {
         ["Currency"] = "XP",
-        ["Reward"] = 300,
-        ["CompletionArguments"] = {"Studs_Moved", 50000},
-        ["DisplayName"] = "Move 50,000 studs",
-        ["QuestType"] = "Season"
-    },
-    {
-        ["Currency"] = "XP",
-        ["DisplayName"] = "Win 3 games in a row in Ranked mode",
-        ["Reward"] = 400,
-        ["CompletionArguments"] = {"Ranked_Win_Streak", 3},
-        ["Icon"] = "rbxassetid://15022099579",
-        ["QuestType"] = "Season"
-    },
-    {
-        ["Currency"] = "XP",
-        ["DisplayName"] = "Spend a total of 60 minutes in the game during the season",
-        ["Reward"] = 50,
-        ["CompletionArguments"] = {"Play_Time", 60},
-        ["Icon"] = "rbxassetid://15046295496",
-        ["QuestType"] = "Season"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 100,
-        ["CompletionArguments"] = {"Play_Time", 240},
-        ["DisplayName"] = "Spend a total of 240 minutes in the game during the season",
-        ["QuestType"] = "Season"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 200,
-        ["CompletionArguments"] = {"Play_Time", 360},
-        ["DisplayName"] = "Spend a total of 360 minutes in the game during the season",
-        ["QuestType"] = "Season"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 300,
-        ["CompletionArguments"] = {"Play_Time", 600},
-        ["DisplayName"] = "Spend a total of 600 minutes in the game during the season",
-        ["QuestType"] = "Season"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 400,
-        ["CompletionArguments"] = {"Play_Time", 960},
-        ["DisplayName"] = "Spend a total of 960 minutes in the game during the season",
-        ["QuestType"] = "Season"
-    },
-    {
-        ["Currency"] = "XP",
+        ["DisplayName"] = "Play for 1200 total minutes",
         ["Reward"] = 500,
         ["CompletionArguments"] = {"Play_Time", 1200},
-        ["DisplayName"] = "Spend a total of 1200 minutes in the game during the season",
-        ["QuestType"] = "Season"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 600,
-        ["CompletionArguments"] = {"Play_Time", 1800},
-        ["DisplayName"] = "Spend a total of 1800 minutes in the game during the season",
-        ["QuestType"] = "Season"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Reward"] = 750,
-        ["CompletionArguments"] = {"Play_Time", 2160},
-        ["DisplayName"] = "Spend a total of 2160 minutes in the game during the season",
-        ["QuestType"] = "Season"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Disabled"] = true,
-        ["Reward"] = 500,
-        ["CompletionArguments"] = {"Invite_Friend", 3},
-        ["DisplayName"] = "Invite 3 friends to play Blade Ball for the first time",
+        ["Icon"] = "rbxassetid://15046295496",
         ["QuestType"] = "Season"
     },
     {
@@ -378,27 +204,41 @@ data = {
     },
     {
         ["Currency"] = "XP",
-        ["Reward"] = 200,
-        ["DisplayName"] = "Play for 60 minutes",
+        ["Reward"] = 500,
         ["Premium"] = true,
-        ["CompletionArguments"] = {"Play_Time", 60},
+        ["CompletionArguments"] = {"Kill_Player", 50, "Glacier Shard"},
+        ["DisplayName"] = "Eliminate 50 players with the Glacier Shard!",
+        ["QuestType"] = "Season"
+    },
+    {
+        ["Currency"] = "XP",
+        ["DisplayName"] = "Play for 1800 total minutes",
+        ["Reward"] = 600,
+        ["CompletionArguments"] = {"Play_Time", 1800},
         ["Icon"] = "rbxassetid://15046295496",
-        ["QuestType"] = "Daily"
+        ["QuestType"] = "Season"
     },
     {
         ["Currency"] = "XP",
-        ["Reward"] = 150,
-        ["Premium"] = true,
-        ["CompletionArguments"] = {"Gift_Robux_Item", 1},
-        ["DisplayName"] = "Gift any robux item",
-        ["QuestType"] = "Daily"
+        ["Reward"] = 750,
+        ["CompletionArguments"] = {"Kill_Player", 1500},
+        ["DisplayName"] = "Eliminate 1500 players in any mode",
+        ["QuestType"] = "Season"
     },
     {
         ["Currency"] = "XP",
-        ["Reward"] = 200,
-        ["Premium"] = true,
-        ["CompletionArguments"] = {"Match_Time", 500},
-        ["DisplayName"] = "Survive for a total of 500 minutes",
+        ["DisplayName"] = "Eliminate 500 players in Ranked mode",
+        ["Reward"] = 750,
+        ["CompletionArguments"] = {"Kill_Player", 500, "Ranked"},
+        ["Icon"] = "rbxassetid://15022099579",
+        ["QuestType"] = "Season"
+    },
+    {
+        ["Currency"] = "XP",
+        ["DisplayName"] = "Play for 2160 total minutes",
+        ["Reward"] = 750,
+        ["CompletionArguments"] = {"Play_Time", 2160},
+        ["Icon"] = "rbxassetid://15046295496",
         ["QuestType"] = "Season"
     },
     {
@@ -411,28 +251,18 @@ data = {
     },
     {
         ["Currency"] = "XP",
-        ["Reward"] = 500,
-        ["Premium"] = true,
-        ["CompletionArguments"] = {"Kill_Player", 50, "Pumpkin VineCrusher"},
-        ["DisplayName"] = "Eliminate 50 players with the Pumpkin VineCrusher!",
-        ["QuestType"] = "Season"
-    },
-    {
-        ["Currency"] = "XP",
-        ["Disabled"] = true,
-        ["Reward"] = 1000,
-        ["Premium"] = true,
-        ["CompletionArguments"] = {"Invite_Friend", 5},
-        ["DisplayName"] = "Invite 5 friends to play Blade Ball for the first time",
-        ["QuestType"] = "Season"
-    },
-    {
-        ["Currency"] = "XP",
         ["Reward"] = 800,
-        ["DisplayName"] = "Use Phantom Ability 50 times",
+        ["DisplayName"] = "Use Time Hole 50 times",
         ["Premium"] = true,
-        ["CompletionArguments"] = {"Use_Ability", 50, "Phantom"},
-        ["Icon"] = "rbxassetid://15059341639",
+        ["CompletionArguments"] = {"Use_Ability", 50, "Time Hole"},
+        ["Icon"] = "rbxassetid://16125258147",
+        ["QuestType"] = "Season"
+    },
+    {
+        ["Currency"] = "XP",
+        ["Reward"] = 1000,
+        ["CompletionArguments"] = {"Gift_Robux_Item", 1},
+        ["DisplayName"] = "Gift a GamePass or any Robux item",
         ["QuestType"] = "Season"
     }
 }
