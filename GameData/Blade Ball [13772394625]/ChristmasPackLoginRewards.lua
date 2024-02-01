21ad3166d5754cd58335b6e11cf920c1de3aae20
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:20:35 +1100

    Create ChristmasPackLoginRewards.lua

---
+++ GameData/Blade Ball [13772394625]/ChristmasPackLoginRewards.lua
@@ -0,0 +1,60 @@
+data = {
+    ["EndTime"] = "1703865600000",
+    ["StartTime"] = "1703433600000",
+    ["Rewards"] = {
+        ["Day1"] = {
+            ["Reward2"] = {
+                ["Value"] = "Emote53",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://15964613010",
+                ["DisplayName"] = "Candy Cane Strike Emote"
+            },
+            ["Reward3"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 2,
+                ["Value"] = "PremiumSword",
+                ["Icon"] = "rbxassetid://16039967646",
+                ["DisplayName"] = "Premium Sword"
+            },
+            ["Expiration"] = 0,
+            ["Reward1"] = {
+                ["Value"] = 500,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713797174",
+                ["DisplayName"] = "500 Coins"
+            }
+        },
+        ["Day3"] = {
+            ["Expiration"] = 86400,
+            ["Reward1"] = {
+                ["Value"] = 1500,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713779371",
+                ["DisplayName"] = "1500 Coins"
+            }
+        },
+        ["Day4"] = {
+            ["Expiration"] = 86400,
+            ["Reward1"] = {
+                ["Value"] = 2000,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713785287",
+                ["DisplayName"] = "2000 Coins"
+            }
+        },
+        ["Day5"] = {
+            ["Expiration"] = 86400,
+            ["Reward1"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 4,
+                ["Value"] = "PremiumSword",
+                ["Icon"] = "rbxassetid://16039967646",
+                ["DisplayName"] = "Premium Sword"
+            }
+        },
+        ["Day2"] = {
+            ["Expiration"] = 86400,
+            ["Reward1"] = {["Value"] = 1000, ["Type"] = "Coins", ["DisplayName"] = "1000 Coins"}
+        }
+    }
+}
