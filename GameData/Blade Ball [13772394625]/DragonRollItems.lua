25e691da93a12151fd5cbdf7c205cae0170f8c53
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:34:11 +1100

    Create DragonRollItems.lua

---
+++ GameData/Blade Ball [13772394625]/DragonRollItems.lua
@@ -0,0 +1,192 @@
+data = {
+    ["SpinTable"] = "function: 0x00000000dd4c5263",
+    ["RewardPool"] = {
+        {
+            ["FixedIconSize"] = "{0.899999976, 0}, {0.899999976, 0}",
+            ["Reward"] = {
+                ["Value"] = "Fire Katana",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://15941937788",
+                ["DisplayName"] = "Fire Katana"
+            },
+            ["Rarity"] = "Premium",
+            ["Probability"] = 0.01
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = 200,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713779371",
+                ["DisplayName"] = "200 Coins"
+            },
+            ["Rarity"] = "Premium",
+            ["Probability"] = 17.7
+        },
+        {
+            ["AbilityTrial"] = {
+                ["DurationShown"] = 259200,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Rapture",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://14722857062",
+                    ["DisplayName"] = "Rapture Free Trial"
+                }
+            },
+            ["Reward"] = {
+                ["Value"] = 500,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713785287",
+                ["DisplayName"] = "500 Coins"
+            },
+            ["Rarity"] = "Premium",
+            ["Probability"] = 14.75
+        },
+        {
+            ["AbilityTrial"] = {
+                ["DurationShown"] = 259200,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Dribble",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://15705640503",
+                    ["DisplayName"] = "Dribble Free Trial"
+                }
+            },
+            ["Reward"] = {
+                ["Value"] = 750,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713787523",
+                ["DisplayName"] = "750 Coins"
+            },
+            ["Rarity"] = "Premium",
+            ["Probability"] = 9.5
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Emote48",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://15778705775",
+                ["DisplayName"] = "Ballin' Emote"
+            },
+            ["Rarity"] = "Premium",
+            ["Probability"] = 9.5
+        },
+        {
+            ["AbilityTrial"] = {
+                ["FixedIconSize"] = "{0.899999976, 0}, {0.899999976, 0}",
+                ["DurationShown"] = 259200,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Titan Blade",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://15460700443",
+                    ["DisplayName"] = "Titan Blade Free Trial"
+                }
+            },
+            ["Reward"] = {
+                ["Value"] = 100,
+                ["Type"] = "SeasonPassCurrency",
+                ["Icon"] = "rbxassetid://16123086920",
+                ["DisplayName"] = "100 Balloons"
+            },
+            ["Rarity"] = "Super Rare",
+            ["Probability"] = 6.35
+        },
+        {
+            ["AbilityTrial"] = {
+                ["DurationShown"] = 259200,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Quantum Arena",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://15519219917",
+                    ["DisplayName"] = "Quantum Arena Free Trial"
+                }
+            },
+            ["Reward"] = {
+                ["Value"] = 200,
+                ["Type"] = "SeasonPassCurrency",
+                ["Icon"] = "rbxassetid://16123086920",
+                ["DisplayName"] = "200 Balloons"
+            },
+            ["Rarity"] = "Super Rare",
+            ["Probability"] = 1.6
+        },
+        {
+            ["AbilityTrial"] = {
+                ["DurationShown"] = 259200,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Infinity",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://14852723022",
+                    ["DisplayName"] = "Infinity Free Trial"
+                }
+            },
+            ["Reward"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 1,
+                ["Value"] = "PremiumExplosion",
+                ["Icon"] = "rbxassetid://15049303003",
+                ["DisplayName"] = "Premium Explosion Crate"
+            },
+            ["Rarity"] = "Super Rare",
+            ["Probability"] = 4.75
+        },
+        {
+            ["Reward"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 1,
+                ["Value"] = "PremiumSword",
+                ["Icon"] = "rbxassetid://16039967646",
+                ["DisplayName"] = "Premium Sword Crate"
+            },
+            ["Rarity"] = "Super Rare",
+            ["Probability"] = 4.75
+        },
+        {
+            ["Reward"] = {
+                ["Type"] = "Boost",
+                ["Value"] = "Coins2x",
+                ["Duration"] = 900,
+                ["Icon"] = "rbxassetid://15289112140",
+                ["DisplayName"] = "2x Coins - 15 Minutes"
+            },
+            ["Rarity"] = "Super Rare",
+            ["Probability"] = 1.6
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = 1,
+                ["Type"] = "WheelSpin",
+                ["Icon"] = "rbxassetid://15051770001",
+                ["DisplayName"] = "x1 Wheel Spin"
+            },
+            ["Rarity"] = "Ultra Rare",
+            ["Probability"] = 1.6
+        },
+        {
+            ["Reward"] = {
+                ["Type"] = "Boost",
+                ["Value"] = "Coins2x",
+                ["Duration"] = 1800,
+                ["Icon"] = "rbxassetid://15289112140",
+                ["DisplayName"] = "2x Coins - 30 Minutes"
+            },
+            ["Rarity"] = "Ultra Rare",
+            ["Probability"] = 1.6
+        },
+        {
+            ["FixedIconSize"] = "{1, 0}, {1, 0}",
+            ["Reward"] = {
+                ["Value"] = "Dual Fire Katana",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://15941938480",
+                ["DisplayName"] = "Dual Fire Katana"
+            },
+            ["Rarity"] = "Ultra Rare",
+            ["Probability"] = 0.00375
+        }
+    }
+}
