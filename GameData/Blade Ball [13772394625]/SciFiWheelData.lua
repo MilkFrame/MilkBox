bdd7a72e7fc6e21ce84d461e502b0679c5fe823a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:43:00 +1100

    Create SciFiWheelData.lua

---
+++ GameData/Blade Ball [13772394625]/SciFiWheelData.lua
@@ -0,0 +1,299 @@
+data = {
+    ["Outer"] = {
+        ["Item2"] = {
+            ["Angle"] = 48,
+            ["Reward"] = {
+                ["Value"] = "Futuristic Explosion",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://16130094172",
+                ["DisplayName"] = "Futuristic Explosion Explosion"
+            },
+            ["Probability"] = 15
+        },
+        ["Item5"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1706626800,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Continuity Zero",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://15797061567",
+                    ["DisplayName"] = "Continuity Zero Free Trial"
+                }
+            },
+            ["Angle"] = 180,
+            ["Reward"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 1,
+                ["Value"] = "PremiumExplosion",
+                ["Icon"] = "rbxassetid://15049303003",
+                ["DisplayName"] = "Premium Explosion Crate"
+            },
+            ["Probability"] = 10
+        },
+        ["Item6"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1706626800,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Serpent Shadow Clone",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://15508130858",
+                    ["DisplayName"] = "Serpent Shadow Clone Free Trial"
+                }
+            },
+            ["Angle"] = 228,
+            ["Reward"] = {
+                ["Value"] = 1,
+                ["Type"] = "WheelSpin",
+                ["Icon"] = "rbxassetid://15051770001",
+                ["DisplayName"] = "x1 Wheel Spin"
+            },
+            ["Probability"] = 10
+        },
+        ["Item7"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1706626800,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Infinity",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://14852723022",
+                    ["DisplayName"] = "Infinity Free Trial"
+                }
+            },
+            ["Angle"] = 270,
+            ["Reward"] = {
+                ["Value"] = 100,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713797174",
+                ["DisplayName"] = "100 Coins"
+            },
+            ["Probability"] = 8
+        },
+        ["Item8"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1706626800,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Quantum Arena",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://15519219917",
+                    ["DisplayName"] = "Quantum Arena Free Trial"
+                }
+            },
+            ["Angle"] = 312,
+            ["Reward"] = {
+                ["Value"] = 250,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713797174",
+                ["DisplayName"] = "250 Coins"
+            },
+            ["Probability"] = 2
+        },
+        ["Item4"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1706626800,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Titan Blade",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://15460700443",
+                    ["DisplayName"] = "Titan Blade Free Trial"
+                }
+            },
+            ["Angle"] = 132,
+            ["Reward"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 2,
+                ["Value"] = "NewYearExplosion",
+                ["Icon"] = "rbxassetid://16130315794",
+                ["DisplayName"] = "New Years Explosion Crate"
+            },
+            ["Probability"] = 20
+        },
+        ["Item3"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1706626800,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Phantom",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://15059341639",
+                    ["DisplayName"] = "Phantom Free Trial"
+                }
+            },
+            ["Angle"] = 90,
+            ["Reward"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 1,
+                ["Value"] = "PremiumSword",
+                ["Icon"] = "rbxassetid://15049301853",
+                ["DisplayName"] = "Premium Sword Crate"
+            },
+            ["Probability"] = 15
+        },
+        ["Item1"] = {["Lucky"] = true, ["Advance"] = true, ["Angle"] = 0, ["IconId"] = "", ["Probability"] = 25}
+    },
+    ["GuaranteedReward"] = {
+        ["Value"] = "Golden Gauntlets",
+        ["Type"] = "Sword",
+        ["Icon"] = "rbxassetid://16136726684",
+        ["DisplayName"] = "Golden Gauntlets"
+    },
+    ["DebugTimeOffset"] = 0,
+    ["HourlyReward"] = {
+        ["Value"] = 1,
+        ["Type"] = "SciFiWheelSpins",
+        ["Icon"] = "rbxassetid://16128936098",
+        ["DisplayName"] = "x1 Spins"
+    },
+    ["End"] = "1706976000000",
+    ["Start"] = "1706198400000",
+    ["Inner"] = {
+        ["Item2"] = {
+            ["Angle"] = 90,
+            ["Reward"] = {
+                ["Value"] = "Emote117",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://16115548043",
+                ["DisplayName"] = "Pom Pom Emote"
+            },
+            ["Probability"] = 40
+        },
+        ["Item4"] = {
+            ["Angle"] = 270,
+            ["Reward"] = {
+                ["Value"] = 800,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713787523",
+                ["DisplayName"] = "800 Coins"
+            },
+            ["Probability"] = 10
+        },
+        ["Item3"] = {
+            ["Angle"] = 180,
+            ["Reward"] = {
+                ["Value"] = 700,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713785287",
+                ["DisplayName"] = "700 Coins"
+            },
+            ["Probability"] = 40
+        },
+        ["Item1"] = {["IconId"] = "", ["Angle"] = 0, ["Advance"] = true, ["Probability"] = 10}
+    },
+    ["Middle"] = {
+        ["Item2"] = {
+            ["Angle"] = 62,
+            ["Reward"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 1,
+                ["Value"] = "PremiumSword",
+                ["Icon"] = "rbxassetid://15049301853",
+                ["DisplayName"] = "Premium Sword Crate"
+            },
+            ["Probability"] = 40
+        },
+        ["Item5"] = {
+            ["Angle"] = 242.5,
+            ["Reward"] = {
+                ["Value"] = 400,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713779371",
+                ["DisplayName"] = "400 Coins"
+            },
+            ["Probability"] = 20
+        },
+        ["Item6"] = {
+            ["Angle"] = 297.5,
+            ["Reward"] = {
+                ["Value"] = 500,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713785287",
+                ["DisplayName"] = "500 Coins"
+            },
+            ["Probability"] = 15
+        },
+        ["Item4"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1706626800,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Dribble",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://15705640503",
+                    ["DisplayName"] = "Dribble Free Trial"
+                }
+            },
+            ["Angle"] = 180,
+            ["Reward"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 1,
+                ["Value"] = "PremiumExplosion",
+                ["Icon"] = "rbxassetid://15049303003",
+                ["DisplayName"] = "Premium Explosion Crate"
+            },
+            ["Probability"] = 5
+        },
+        ["Item3"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1706626800,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Calming Deflection",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://15572578968",
+                    ["DisplayName"] = "Calming Deflection Free Trial"
+                }
+            },
+            ["Angle"] = 118,
+            ["Reward"] = {
+                ["Value"] = 1,
+                ["Type"] = "SciFiWheelSpins",
+                ["Icon"] = "rbxassetid://15455896861",
+                ["DisplayName"] = "x1 Sci-Fi Spin"
+            },
+            ["Probability"] = 5
+        },
+        ["Item1"] = {["IconId"] = "", ["Angle"] = 0, ["Advance"] = true, ["Probability"] = 15}
+    },
+    ["StreakHoursRewards"] = {
+        {
+            ["TimeStamp"] = 3600,
+            ["Reward"] = {
+                ["Value"] = 1,
+                ["Type"] = "SciFiWheelSpins",
+                ["Icon"] = "rbxassetid://16128936098",
+                ["DisplayName"] = "x1 Spins"
+            }
+        },
+        {
+            ["TimeStamp"] = 7200,
+            ["Reward"] = {
+                ["Value"] = 2,
+                ["Type"] = "SciFiWheelSpins",
+                ["Icon"] = "rbxassetid://16128936098",
+                ["DisplayName"] = "x2 Spins"
+            }
+        },
+        {
+            ["TimeStamp"] = 10800,
+            ["Reward"] = {
+                ["Value"] = 4,
+                ["Type"] = "SciFiWheelSpins",
+                ["Icon"] = "rbxassetid://16128936098",
+                ["DisplayName"] = "x4 Spins"
+            }
+        },
+        {
+            ["TimeStamp"] = 14400,
+            ["Reward"] = {
+                ["Value"] = 8,
+                ["Type"] = "SciFiWheelSpins",
+                ["Icon"] = "rbxassetid://16128936098",
+                ["DisplayName"] = "x8 Spins"
+            }
+        }
+    }
+}
