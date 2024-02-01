e7e34fa55203591f746e82cea713ed61e816684d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:21:19 +1100

    Create ElementalQuests.lua

---
+++ GameData/Blade Ball [13772394625]/ElementalQuests.lua
@@ -0,0 +1,26 @@
+data = {
+    ["Water"] = {
+        {["Requirement"] = 2400, ["IsPlaytimeBased"] = true, ["Reward"] = 1},
+        {["Requirement"] = 3600, ["IsPlaytimeBased"] = true, ["Reward"] = 1},
+        {["Requirement"] = 5400, ["IsPlaytimeBased"] = true, ["Reward"] = 2},
+        {["Requirement"] = 7200, ["IsPlaytimeBased"] = true, ["Reward"] = 3},
+        {["Requirement"] = 10800, ["IsPlaytimeBased"] = true, ["Reward"] = 3}
+    },
+    ["Lightning"] = {
+        {["Reward"] = 1, ["Requirement"] = 25},
+        {["Reward"] = 1, ["Requirement"] = 50},
+        {["Reward"] = 2, ["Requirement"] = 100},
+        {["Reward"] = 3, ["Requirement"] = 150},
+        {["Reward"] = 3, ["Requirement"] = 250}
+    },
+    ["Wind"] = {
+        {["Reward"] = 1, ["Requirement"] = 3},
+        {["Reward"] = 1, ["Requirement"] = 10},
+        {["Reward"] = 2, ["Requirement"] = 25},
+        {["Reward"] = 3, ["Requirement"] = 50},
+        {["Reward"] = 3, ["Requirement"] = 100}
+    },
+    ["LightningDescription"] = "Players Killed",
+    ["WaterDescription"] = "Play Time",
+    ["WindDescription"] = "Wins"
+}
