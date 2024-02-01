f0b3de23393028c52fe00ebd27576b99c31f8020
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:43:41 +1100

    Create SuperCoinsRollData.lua

---
+++ GameData/Blade Ball [13772394625]/SuperCoinsRollData.lua
@@ -0,0 +1,204 @@
+data = {
+    ["Claims"] = {
+        {
+            ["Spins"] = 10,
+            ["Sound"] = "win_01",
+            ["Reward"] = {
+                ["Type"] = "Crate",
+                ["Value"] = "BigChestOfCoins",
+                ["CrateType"] = "Coins",
+                ["Icon"] = "rbxassetid://15326928319",
+                ["DisplayName"] = "Big Chest of Coins"
+            }
+        },
+        {
+            ["Spins"] = 25,
+            ["Reward"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 2,
+                ["Value"] = "PremiumExplosion",
+                ["Icon"] = "rbxassetid://15049303003",
+                ["DisplayName"] = "Premium Explosion Crate"
+            }
+        },
+        {
+            ["Spins"] = 50,
+            ["Reward"] = {
+                ["Rewards"] = {
+                    {
+                        ["Id"] = "Inferno Axe",
+                        ["Condition"] = "function: 0x00000000bbf884db",
+                        ["Reward"] = {
+                            ["Value"] = "Inferno Axe",
+                            ["Type"] = "Sword",
+                            ["Icon"] = "rbxassetid://16131565762",
+                            ["DisplayName"] = "Inferno Axe"
+                        }
+                    },
+                    {
+                        ["Id"] = "Premium Sword Crate",
+                        ["Condition"] = true,
+                        ["Reward"] = {
+                            ["Type"] = "CrateKey",
+                            ["Amount"] = 2,
+                            ["Value"] = "PremiumSword",
+                            ["Icon"] = "rbxassetid://16039967646",
+                            ["DisplayName"] = "Premium Sword Crate"
+                        }
+                    }
+                },
+                ["Type"] = "ConditionRewards"
+            }
+        }
+    },
+    ["ColorTypes"] = {["Explosion"] = Color3.fromRGB(241, 43, 255), ["Sword"] = Color3.fromRGB(255, 163, 34)},
+    ["Id"] = 1,
+    ["MinToClaim"] = 10,
+    ["MaxToClaim"] = 50,
+    ["Prices"] = {1000, 8000},
+    ["Limit"] = 20,
+    ["Weighted"] = {0.35, 2.15, 5, 3.5, 4, 10, 4, 7, 12, 15, 20, 10, 2, 5},
+    ["Spin"] = {
+        {
+            ["Color"] = Color3.fromRGB(0, 0, 0),
+            ["StrokeColor"] = Color3.fromRGB(255, 255, 255),
+            ["Chance"] = 0.35,
+            ["Reward"] = {
+                ["Value"] = "Titan's Gleam",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://16132471055",
+                ["DisplayName"] = "Titan's Gleam"
+            }
+        },
+        {
+            ["Color"] = Color3.fromRGB(255, 62, 62),
+            ["Chance"] = 2.15,
+            ["Reward"] = {
+                ["Value"] = "Sunfire Sword",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://16132579598",
+                ["DisplayName"] = "Sunfire Sword"
+            }
+        },
+        {
+            ["Color"] = Color3.fromRGB(255, 62, 62),
+            ["Chance"] = 5,
+            ["Reward"] = {
+                ["Value"] = "Blaze Blade",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://16132577801",
+                ["DisplayName"] = "Blaze Blade"
+            }
+        },
+        {
+            ["Color"] = Color3.fromRGB(129, 255, 101),
+            ["Chance"] = 3.5,
+            ["Reward"] = {
+                ["Value"] = "Gold Burst",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://16130074456",
+                ["DisplayName"] = "Gold Burst Explosion"
+            }
+        },
+        {
+            ["Color"] = Color3.fromRGB(124, 255, 251),
+            ["Chance"] = 4,
+            ["Reward"] = {
+                ["Value"] = "Emote119",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://16127720016",
+                ["DisplayName"] = "Moon Walk Emote"
+            }
+        },
+        {
+            ["Color"] = Color3.fromRGB(124, 255, 251),
+            ["Chance"] = 10,
+            ["Reward"] = {
+                ["Value"] = "Emote120",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://16127719694",
+                ["DisplayName"] = "Ballerina Dance Emote"
+            }
+        },
+        {
+            ["Color"] = Color3.fromRGB(255, 255, 69),
+            ["Chance"] = 4,
+            ["Reward"] = {
+                ["Value"] = 2000,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713787523",
+                ["DisplayName"] = "2000 Coins"
+            }
+        },
+        {
+            ["Color"] = Color3.fromRGB(255, 255, 69),
+            ["Chance"] = 7,
+            ["Reward"] = {
+                ["Value"] = 1000,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713785287",
+                ["DisplayName"] = "1000 Coins"
+            }
+        },
+        {
+            ["Color"] = Color3.fromRGB(255, 255, 69),
+            ["Chance"] = 12,
+            ["Reward"] = {
+                ["Value"] = 500,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713779371",
+                ["DisplayName"] = "500 Coins"
+            }
+        },
+        {
+            ["Color"] = Color3.fromRGB(255, 255, 69),
+            ["Chance"] = 15,
+            ["Reward"] = {
+                ["Value"] = 400,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713794582",
+                ["DisplayName"] = "400 Coins"
+            }
+        },
+        {
+            ["Color"] = Color3.fromRGB(255, 255, 69),
+            ["Chance"] = 20,
+            ["Reward"] = {
+                ["Value"] = 200,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713797174",
+                ["DisplayName"] = "200 Coins"
+            }
+        },
+        {
+            ["Color"] = Color3.fromRGB(255, 164, 74),
+            ["Chance"] = 10,
+            ["Reward"] = {
+                ["Value"] = 1,
+                ["Type"] = "SciFiWheelSpins",
+                ["Icon"] = "rbxassetid://16128936098",
+                ["DisplayName"] = "x1 Spins"
+            }
+        },
+        {
+            ["Color"] = Color3.fromRGB(255, 164, 74),
+            ["Chance"] = 2,
+            ["Reward"] = {
+                ["Value"] = 1,
+                ["Type"] = "WheelSpin",
+                ["Icon"] = "rbxassetid://15051770001",
+                ["DisplayName"] = "x1 Wheel Spin"
+            }
+        },
+        {
+            ["Color"] = Color3.fromRGB(255, 57, 255),
+            ["Chance"] = 5,
+            ["Reward"] = {
+                ["Value"] = 30,
+                ["Type"] = "SeasonPassCurrency",
+                ["Icon"] = "rbxassetid://16123086920",
+                ["DisplayName"] = "30 Balloons"
+            }
+        }
+    }
+}
