dce612c55e4730cfd6fbfe355ef6b8de0d2c6dbf
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:30:05 +1100

    Create SpecialTrainingData.lua

---
+++ GameData/Blade Ball [13772394625]/SpecialTrainingData.lua
@@ -0,0 +1,187 @@
+data = {
+    ["SPECIAL_DAILY_LOGIN_VERSION"] = 9,
+    ["MAXEVENTTIME"] = "1705771137",
+    ["CURRENT_RESETS_AVAILABLE"] = 1,
+    ["QuestToDiff"] = {1, 2, 2, 3},
+    ["Products"] = {
+        {["Id"] = 1676261676, ["Price"] = 99, ["Amount"] = 1},
+        {["Id"] = 1676261918, ["Price"] = 199, ["Amount"] = 1},
+        {["Id"] = 1676261963, ["Price"] = 299, ["Amount"] = 1},
+        {["Id"] = 1676262004, ["Price"] = 399, ["Amount"] = 1},
+        {["Id"] = 1676256463, ["Price"] = 1, ["Amount"] = 1}
+    },
+    ["AMOUNTOFQUESTS"] = 4,
+    ["Gifts"] = {
+        {["Id"] = 1676262354, ["Price"] = 999, ["Amount"] = 2},
+        {["Id"] = 1676262500, ["Price"] = 2999, ["Amount"] = 10},
+        {["Id"] = 1676262558, ["Price"] = 9999, ["Amount"] = 100}
+    },
+    ["CURRENT_TRAINING_QUEST"] = 6,
+    ["SpecialTrainingRewards"] = {
+        {["Required"] = 20, ["RewardType"] = "Coins", ["Icon"] = "rbxassetid://15038384377", ["Amount"] = 150},
+        {
+            ["Required"] = 40,
+            ["RewardType"] = "PremiumExplosionCrate",
+            ["Icon"] = "rbxassetid://15049303003",
+            ["Amount"] = 1
+        },
+        {
+            ["RewardType"] = "Emote",
+            ["Required"] = 60,
+            ["RewardStat"] = "Emote75",
+            ["Icon"] = "rbxassetid://15862986776",
+            ["Replacement"] = {
+                ["Required"] = 60,
+                ["RewardType"] = "Coins",
+                ["Icon"] = "rbxassetid://15038384377",
+                ["Amount"] = 500
+            }
+        },
+        {
+            ["RewardType"] = "Boost",
+            ["Required"] = 80,
+            ["RewardStat"] = "Coins",
+            ["Icon"] = "rbxassetid://14737693993",
+            ["Duration"] = 1800
+        },
+        {
+            ["RewardType"] = "Ability",
+            ["Required"] = 100,
+            ["RewardStat"] = "Continuity Zero",
+            ["Icon"] = "rbxassetid://15797061567",
+            ["Replacement"] = {
+                ["RewardStat"] = "Aether Katana",
+                ["RewardType"] = "Sword",
+                ["Icon"] = "rbxassetid://15861551107",
+                ["Required"] = 100
+            }
+        }
+    },
+    ["SpecialTrainingQuests"] = {
+        {
+            {
+                ["Currency"] = "XP",
+                ["Amount"] = 3,
+                ["CompletionArguments"] = {"Play_Round", 8},
+                ["DisplayName"] = "Play 8 matches",
+                ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["Amount"] = 3,
+                ["CompletionArguments"] = {"Kill_Player", 5, "2Teams"},
+                ["DisplayName"] = "Eliminate 5 people in 2 team mode",
+                ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["Amount"] = 3,
+                ["CompletionArguments"] = {"Parry_Ball", 60},
+                ["DisplayName"] = "Parry the ball 60 times",
+                ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["Amount"] = 3,
+                ["CompletionArguments"] = {"Play_Time", 1800},
+                ["DisplayName"] = "Play for 30 minutes",
+                ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["Amount"] = 3,
+                ["CompletionArguments"] = {"Friend_Login", 1},
+                ["DisplayName"] = "Play with 1 friend",
+                ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["Amount"] = 3,
+                ["CompletionArguments"] = {"Play_Duel", 4},
+                ["DisplayName"] = "Play in 4 Duels",
+                ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["Amount"] = 3,
+                ["CompletionArguments"] = {"Join_Clan", 1},
+                ["DisplayName"] = "Join or create a clan",
+                ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["Amount"] = 3,
+                ["CompletionArguments"] = {"Play_Round", 8, "LTM"},
+                ["DisplayName"] = "Play 8 Infected Limited Mode Matches",
+                ["QuestType"] = "Daily"
+            }
+        },
+        {
+            {
+                ["Currency"] = "XP",
+                ["Amount"] = 3,
+                ["CompletionArguments"] = {"Kill_Player", 7},
+                ["DisplayName"] = "Eliminate 7 people in any mode",
+                ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["Amount"] = 5,
+                ["CompletionArguments"] = {"Parry_Ball", 100},
+                ["DisplayName"] = "Parry the ball 100 times",
+                ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["Amount"] = 5,
+                ["CompletionArguments"] = {"Parry_Ball", 250},
+                ["DisplayName"] = "Parry the ball 250 times",
+                ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["Amount"] = 5,
+                ["CompletionArguments"] = {"Win_Duel", 2},
+                ["DisplayName"] = "Win at least 2 Duels",
+                ["QuestType"] = "Daily"
+            }
+        },
+        {
+            {
+                ["Currency"] = "XP",
+                ["Amount"] = 8,
+                ["CompletionArguments"] = {"Play_Time", 5400},
+                ["DisplayName"] = "Play for 90 minutes",
+                ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["Amount"] = 8,
+                ["CompletionArguments"] = {"Round_Kill_Count_Specific", 5},
+                ["DisplayName"] = "Eliminate 5 enemies in a single match 5 times",
+                ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["Amount"] = 8,
+                ["CompletionArguments"] = {"Round_Won", 1},
+                ["DisplayName"] = "Win 1 game",
+                ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["Amount"] = 8,
+                ["CompletionArguments"] = {"Round_Won", 2, "2Teams"},
+                ["DisplayName"] = "Win 2 Team Mode 2 Times",
+                ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["Amount"] = 8,
+                ["CompletionArguments"] = {"Round_Won", 1, "Dash"},
+                ["DisplayName"] = "Win 1 game using dash ability",
+                ["QuestType"] = "Daily"
+            }
+        }
+    }
+}
