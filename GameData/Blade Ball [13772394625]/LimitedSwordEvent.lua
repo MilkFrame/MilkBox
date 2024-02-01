e02bffa108bc4323ed3f0c3363d769989d77e973
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:30:23 +1100

    Create LimitedSwordEvent.lua

---
+++ GameData/Blade Ball [13772394625]/LimitedSwordEvent.lua
@@ -0,0 +1,121 @@
+data = {
+    ["Events"] = {
+        ["SciFi"] = {
+            ["PromptText"] = "Buy Sci-Fi Swords",
+            ["Swords"] = {
+                ["Single"] = {["GiftId"] = 0, ["SwordName"] = "Plasma Blaster", ["SwordIcon"] = "", ["ProductId"] = 0},
+                ["Dual"] = {["GiftId"] = 0, ["SwordName"] = "Plasma Blasters", ["SwordIcon"] = "", ["ProductId"] = 0},
+                ["Better"] = {["GiftId"] = 0, ["SwordName"] = "Laser Twinblade", ["SwordIcon"] = "", ["ProductId"] = 0}
+            },
+            ["Duration"] = {["Min"] = 1700330400, ["Max"] = 1702051200},
+            ["HideGiftButton"] = true,
+            ["NoLimitedSwordUI"] = true
+        },
+        ["EtherBlade"] = {
+            ["Swords"] = {
+                ["Single"] = {
+                    ["GiftId"] = 1681075983,
+                    ["SwordIcon"] = "rbxassetid://15255441533",
+                    ["ProductId"] = 1681082566,
+                    ["Upgrade"] = {
+                        ["SuccessChance"] = 0.02,
+                        ["SwordName"] = "Aurum Etherius",
+                        ["SwordIcon"] = "rbxassetid://15256626064",
+                        ["UpgradeSpinIds"] = {["Spin10"] = 1681372141, ["Spin1"] = 1681371858}
+                    },
+                    ["SwordName"] = "Ether Blade"
+                },
+                ["Dual"] = {
+                    ["GiftId"] = 1681076133,
+                    ["SwordIcon"] = "rbxassetid://15255436534",
+                    ["ProductId"] = 1681082693,
+                    ["Upgrade"] = {
+                        ["SuccessChance"] = 0.02,
+                        ["SwordName"] = "Dual Aurum Etherius",
+                        ["SwordIcon"] = "rbxassetid://15256629402",
+                        ["UpgradeSpinIds"] = {["Spin10"] = 1681372797, ["Spin1"] = 1681372677}
+                    },
+                    ["SwordName"] = "Dual Ether Blade"
+                }
+            },
+            ["Duration"] = {["Min"] = 1699000000, ["Max"] = 1699714800}
+        },
+        ["Shield"] = {
+            ["UseShowroom"] = true,
+            ["Duration"] = {["Min"] = 1699714800, ["Max"] = 1700330400},
+            ["Swords"] = {
+                ["Single"] = {
+                    ["GiftId"] = 1685471782,
+                    ["SwordName"] = "Vanguard Shield",
+                    ["SwordIcon"] = "rbxassetid://15318862124",
+                    ["ProductId"] = 1685471391
+                },
+                ["Better"] = {
+                    ["GiftId"] = 1685472174,
+                    ["SwordName"] = "Empyrean Fortress",
+                    ["SwordIcon"] = "rbxassetid://15318864052",
+                    ["ProductId"] = 1685472082
+                }
+            }
+        },
+        ["SwordPacks"] = {
+            ["UseShowroom"] = true,
+            ["PromptText"] = "Buy Limited Swords",
+            ["Swords"] = {
+                ["Single"] = {
+                    ["GiftId"] = 0,
+                    ["SwordName"] = "Aetherial Azure Reckoner",
+                    ["SwordIcon"] = "",
+                    ["ProductId"] = 0
+                },
+                ["Better"] = {
+                    ["GiftId"] = 0,
+                    ["SwordName"] = "Dual Aetherial Azure Reckoner",
+                    ["SwordIcon"] = "",
+                    ["ProductId"] = 0
+                }
+            },
+            ["Duration"] = {["Min"] = 0, ["Max"] = 0},
+            ["HideGiftButton"] = true,
+            ["NoLimitedSwordUI"] = true
+        },
+        ["NebulaLightning"] = {
+            ["UseShowroom"] = true,
+            ["Duration"] = {["Min"] = 1699714800, ["Max"] = 1705075200},
+            ["Swords"] = {
+                ["Single"] = {
+                    ["GiftId"] = 1701417480,
+                    ["SwordIcon"] = "rbxassetid://15580262293",
+                    ["ProductId"] = 1701417403,
+                    ["Price"] = "399",
+                    ["SwordName"] = "Nebula's Lightning"
+                },
+                ["Better"] = {
+                    ["GiftId"] = 1701417712,
+                    ["SwordIcon"] = "rbxassetid://15580262580",
+                    ["ProductId"] = 1701417621,
+                    ["Price"] = "3499",
+                    ["SwordName"] = "Dual Nebula's Lightning"
+                }
+            }
+        },
+        ["Nebula"] = {
+            ["Swords"] = {
+                ["Single"] = {
+                    ["GiftId"] = 1676651366,
+                    ["SwordName"] = "Nebula Scythe",
+                    ["SwordIcon"] = "rbxassetid://15188448407",
+                    ["ProductId"] = 1676650561
+                },
+                ["Dual"] = {
+                    ["GiftId"] = 1676651251,
+                    ["SwordName"] = "Dual Nebula Scythe",
+                    ["SwordIcon"] = "rbxassetid://15146215320",
+                    ["ProductId"] = 1676650712
+                }
+            },
+            ["Duration"] = {["Min"] = 0, ["Max"] = 1699000000}
+        }
+    },
+    ["Active"] = "SwordPacks"
+}
