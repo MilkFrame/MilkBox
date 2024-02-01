265e60ea2cd914b4d6a5d79eefa7d89fe00d0090
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:19:06 +1100

    Create PlaytimeRewardsInfo.lua

---
+++ GameData/Blade Ball [13772394625]/PlaytimeRewardsInfo.lua
@@ -0,0 +1,60 @@
+data = {
+    ["Rewards"] = {
+        {
+            ["Reward"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 1,
+                ["Value"] = "Sword",
+                ["Icon"] = "rbxassetid://15049301853",
+                ["DisplayName"] = "Sword Crate"
+            },
+            ["Duration"] = 300
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = 100,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713797174",
+                ["DisplayName"] = "100 Coins"
+            },
+            ["Duration"] = 600
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = 1,
+                ["Type"] = "WheelSpin",
+                ["Icon"] = "rbxassetid://15051770001",
+                ["DisplayName"] = "x1 Wheel Spin"
+            },
+            ["Duration"] = 900
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = 150,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713794582",
+                ["DisplayName"] = "150 Coins"
+            },
+            ["Duration"] = 1200
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = 1,
+                ["Type"] = "WheelSpin",
+                ["Icon"] = "rbxassetid://15051770001",
+                ["DisplayName"] = "x1 Wheel Spin"
+            },
+            ["Duration"] = 1500
+        },
+        {
+            ["Reward"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 1,
+                ["Value"] = "PremiumExplosion",
+                ["Icon"] = "rbxassetid://15049303003",
+                ["DisplayName"] = "Premium Explosion Crate"
+            },
+            ["Duration"] = 1800
+        }
+    }
+}
