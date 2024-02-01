99057c20778027c7a25b6828a3a1454dfcb95e0a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:31:49 +1100

    Create GachaRaceRewards.lua

---
+++ GameData/Blade Ball [13772394625]/GachaRaceRewards.lua
@@ -0,0 +1,46 @@
+data = {
+    {
+        ["Rewards"] = {
+            {
+                ["Value"] = "Venom Blade",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://16136764863",
+                ["DisplayName"] = "Venom Blade"
+            }
+        },
+        ["Rank"] = 1
+    },
+    {
+        ["Rewards"] = {
+            {
+                ["Value"] = "Resolution Blade",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://16136398662",
+                ["DisplayName"] = "Resolution Blade"
+            }
+        },
+        ["Rank"] = 5
+    },
+    {
+        ["Rewards"] = {
+            {
+                ["Value"] = 50,
+                ["Type"] = "GachaSpins",
+                ["Icon"] = "rbxassetid://16100520533",
+                ["DisplayName"] = "x50 New Year Spin"
+            }
+        },
+        ["Rank"] = 10
+    },
+    {
+        ["Rewards"] = {
+            {
+                ["Value"] = 25,
+                ["Type"] = "GachaSpins",
+                ["Icon"] = "rbxassetid://16100521042",
+                ["DisplayName"] = "x25 New Year Spin"
+            }
+        },
+        ["Rank"] = 50
+    }
+}
