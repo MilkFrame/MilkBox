25302081fe1ea9a0d1e726666592a32626282fbe
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:19:50 +1100

    Create VIPPlusInfo.lua

---
+++ GameData/Blade Ball [13772394625]/VIPPlusInfo.lua
@@ -0,0 +1,139 @@
+data = {
+    ["MonthlyRewards"] = {
+        {
+            {
+                {
+                    ["Value"] = "Emote26",
+                    ["Type"] = "Emote",
+                    ["Icon"] = "rbxassetid://15386185741",
+                    ["DisplayName"] = "Sitting Emote"
+                },
+                {
+                    ["Type"] = "CrateKey",
+                    ["Amount"] = 1,
+                    ["Value"] = "PremiumSword",
+                    ["Icon"] = "rbxassetid://16039967646",
+                    ["DisplayName"] = "Premium Sword Crate"
+                }
+            },
+            {
+                {
+                    ["Value"] = "Runic Wrecker",
+                    ["Type"] = "Sword",
+                    ["Icon"] = "rbxassetid://15316219581",
+                    ["DisplayName"] = "Runic Wrecker"
+                },
+                {
+                    ["Value"] = "Emote27",
+                    ["Type"] = "Emote",
+                    ["Icon"] = "rbxassetid://15386190890",
+                    ["DisplayName"] = "Cheering Emote"
+                }
+            },
+            {
+                {
+                    ["Value"] = "Golden Reserve",
+                    ["Type"] = "Explosion",
+                    ["Icon"] = "rbxassetid://15528770333",
+                    ["DisplayName"] = "Golden Reserve Explosion"
+                },
+                {
+                    ["Value"] = "VIP Cutlass",
+                    ["Type"] = "Sword",
+                    ["Icon"] = "rbxassetid://15385009806",
+                    ["DisplayName"] = "VIP Cutlass"
+                }
+            },
+            {
+                {
+                    ["Value"] = "VIP Sword",
+                    ["Type"] = "Sword",
+                    ["Icon"] = "rbxassetid://15385004580",
+                    ["DisplayName"] = "VIP Sword"
+                },
+                {
+                    ["Value"] = "VIP Explosion",
+                    ["Type"] = "Explosion",
+                    ["Icon"] = "rbxassetid://15373017669",
+                    ["DisplayName"] = "VIP Explosion Explosion"
+                }
+            }
+        },
+        {
+            {
+                {
+                    ["Value"] = "Emote47",
+                    ["Type"] = "Emote",
+                    ["Icon"] = "rbxassetid://15620519624",
+                    ["DisplayName"] = "Jiggy Emote"
+                },
+                {
+                    ["Type"] = "CrateKey",
+                    ["Amount"] = 1,
+                    ["Value"] = "PremiumSword",
+                    ["Icon"] = "rbxassetid://16039967646",
+                    ["DisplayName"] = "Premium Sword Crate"
+                }
+            },
+            {
+                {
+                    ["Value"] = "Festive Sword",
+                    ["Type"] = "Sword",
+                    ["Icon"] = "rbxassetid://15652859692",
+                    ["DisplayName"] = "Festive Sword"
+                },
+                {
+                    ["Type"] = "CrateKey",
+                    ["Amount"] = 1,
+                    ["Value"] = "PremiumExplosion",
+                    ["Icon"] = "rbxassetid://15049303003",
+                    ["DisplayName"] = "Premium Explosion Crate"
+                }
+            },
+            {
+                {
+                    ["Value"] = "Parasol",
+                    ["Type"] = "Sword",
+                    ["Icon"] = "rbxassetid://15961551173",
+                    ["DisplayName"] = "Parasol"
+                },
+                {
+                    ["Value"] = "Emote100",
+                    ["Type"] = "Emote",
+                    ["Icon"] = "rbxassetid://15961417216",
+                    ["DisplayName"] = "Splits Emote"
+                }
+            },
+            {
+                {
+                    ["Value"] = "Christmas Present",
+                    ["Type"] = "Explosion",
+                    ["Icon"] = "rbxassetid://15652859535",
+                    ["DisplayName"] = "Christmas Present Explosion"
+                },
+                {
+                    ["Value"] = "Crowned Ending",
+                    ["Type"] = "Explosion",
+                    ["Icon"] = "rbxassetid://15901790349",
+                    ["DisplayName"] = "Crowned Ending Explosion"
+                }
+            }
+        }
+    },
+    ["Instant"] = {
+        {["Value"] = 1500, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713779371", ["DisplayName"] = "1500 Coins"},
+        {
+            ["Value"] = "Blackhole Whitehole",
+            ["Type"] = "Explosion",
+            ["Icon"] = "rbxassetid://14204582096",
+            ["DisplayName"] = "Blackhole Whitehole Explosion"
+        },
+        {
+            ["Type"] = "CrateKey",
+            ["Amount"] = 3,
+            ["Value"] = "PremiumExplosion",
+            ["Icon"] = "rbxassetid://15049303003",
+            ["DisplayName"] = "Premium Explosion Crate"
+        }
+    }
+}
