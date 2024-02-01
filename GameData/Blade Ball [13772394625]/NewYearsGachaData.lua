e09f7cd79f359fcc3b1fb926e636e0073b0b6bea
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:32:16 +1100

    Create NewYearsGachaData.lua

---
+++ GameData/Blade Ball [13772394625]/NewYearsGachaData.lua
@@ -0,0 +1,27 @@
+data = {
+    ["Prices"] = {
+        ["SpecialDiscount"] = {
+            ["Price"] = 49,
+            ["RewardInfo"] = {
+                ["Value"] = 1,
+                ["Type"] = "DragonSpins",
+                ["Icon"] = "",
+                ["DisplayName"] = "1 Dragon Spins"
+            },
+            ["ProductId"] = 1
+        }
+    },
+    ["Paths"] = {
+        ["ENTIRE_DATA"] = {"ChineseNewYearsGachaData"},
+        ["DiscountSpinStamp"] = {"ChineseNewYearsGachaData", "DiscountSpinStamp"},
+        ["DragonSpins"] = {"ChineseNewYearsGachaData", "DragonSpins"},
+        ["GachaSpinHistory"] = {"ChineseNewYearsGachaData", "GachaSpinHistory"},
+        ["MigrationVersion"] = {"ChineseNewYearsGachaData", "MigrationVersion"}
+    },
+    ["GetIsSpinDiscounted"] = "function: 0x000000008e57cb4b",
+    ["GetPriceFromProductId"] = "function: 0x0000000095e169f3",
+    ["StartDate"] = "1706611249910",
+    ["Rewards"] = {},
+    ["MigrationVersion"] = 1,
+    ["EndDate"] = "1733979900000"
+}
