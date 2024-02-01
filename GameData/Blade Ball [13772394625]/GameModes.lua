c965f5a6891d363feb02011ac88ce34e59bba0ba
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:30:39 +1100

    Create GameModes.lua

---
+++ GameData/Blade Ball [13772394625]/GameModes.lua
@@ -0,0 +1,10 @@
+data = {
+    ["Chances"] = {{["FFA"] = 100}, {["Duo"] = 0, ["2Teams"] = 100}, {["Randomizer"] = 0, ["4Teams"] = 100}},
+    ["Modes"] = {
+        ["Duo"] = {["DisabledInTraining"] = true, ["DisplayName"] = "Duos"},
+        ["Randomizer"] = {["DisplayName"] = "Randomizer"},
+        ["FFA"] = {["DisplayName"] = "Classic"},
+        ["2Teams"] = {["DisplayName"] = "2 Teams"},
+        ["4Teams"] = {["DisplayName"] = "4 Teams"}
+    }
+}
