b2a678ebb44c7d7131800dd3bd93a0bd88293169
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:13:25 +1100

    Create BoostsInfo.lua

---
+++ GameData/Blade Ball [13772394625]/BoostsInfo.lua
@@ -0,0 +1,6 @@
+data = {
+    ["Boosts"] = {
+        ["Coins2x"] = {["Stacks"] = true, ["Icon"] = "rbxassetid://15289112140", ["DisplayName"] = "2x Coins"},
+        ["InstantSpin"] = {["Stacks"] = true, ["Icon"] = "rbxassetid://15289128167", ["DisplayName"] = "Instant Spin"}
+    }
+}
