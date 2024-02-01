3fc26819906ab9e001071b821d92e8eb7e404a22
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:40:45 +1100

    Create ClanBattlepassQuests.lua

---
+++ GameData/Blade Ball [13772394625]/ClanBattlepassQuests.lua
@@ -0,0 +1,79 @@
+data = {
+    ["SpinWheelSantaMode"] = {
+        ["IncrementArguments"] = {"Spin_LTM_Wheel"},
+        ["Type"] = "Daily",
+        ["Goal"] = 1,
+        ["Description"] = "Spin the wheel from Infected Limited Mode",
+        ["Reward"] = 100
+    },
+    ["WinTwoGames"] = {
+        ["IncrementArguments"] = {"Round_Won"},
+        ["Type"] = "Daily",
+        ["Goal"] = 2,
+        ["Description"] = "Win 2 games",
+        ["Reward"] = 100
+    },
+    ["CompleteRankedDuoRounds"] = {
+        ["IncrementArguments"] = {"Play_Round", "Duo", "Ranked"},
+        ["Type"] = "Daily",
+        ["Goal"] = 2,
+        ["Description"] = "Complete 2 rounds in Ranked Duo",
+        ["Reward"] = 100
+    },
+    ["PlayRankedModeMatches"] = {
+        ["IncrementArguments"] = {"Play_Round", "Ranked"},
+        ["Type"] = "Daily",
+        ["Goal"] = 10,
+        ["Description"] = "Play 10 Ranked mode matches",
+        ["Reward"] = 100
+    },
+    ["PlayWithClanMember"] = {
+        ["IncrementArguments"] = {"Play_With_Clan_Member"},
+        ["Type"] = "Daily",
+        ["Goal"] = 1,
+        ["Description"] = "Play with a clan member",
+        ["Reward"] = 100
+    },
+    ["EliminateEnemiesInRankedMode"] = {
+        ["IncrementArguments"] = {"Kill_Player", "Ranked"},
+        ["Type"] = "Daily",
+        ["Goal"] = 4,
+        ["Description"] = "Eliminate 4 enemies in Ranked mode match",
+        ["Reward"] = 100
+    },
+    ["KillPlayersInAnyGameMode"] = {
+        ["IncrementArguments"] = {"Kill_Player"},
+        ["Type"] = "Daily",
+        ["Goal"] = 10,
+        ["Description"] = "Eliminate 10 players in any gamemode",
+        ["Reward"] = 100
+    },
+    ["SurviveTotalMinutes"] = {
+        ["IncrementArguments"] = {"Survive_For"},
+        ["Type"] = "Daily",
+        ["Goal"] = 50,
+        ["Description"] = "Survive for 50 total minutes",
+        ["Reward"] = 100
+    },
+    ["PlayRoundsSantaMode"] = {
+        ["IncrementArguments"] = {"Play_Round", "LTM"},
+        ["Type"] = "Daily",
+        ["Goal"] = 5,
+        ["Description"] = "Play 5 matches in the Infected Limited Mode",
+        ["Reward"] = 100
+    },
+    ["PlayDifferentAbilities"] = {
+        ["IncrementArguments"] = {"Play_Round_With_Diff_Ability"},
+        ["Type"] = "Daily",
+        ["Goal"] = 5,
+        ["Description"] = "Play with 5 different abilities in any gamemode",
+        ["Reward"] = 100
+    },
+    ["WinSantaMode"] = {
+        ["IncrementArguments"] = {"Round_Won", "LTM"},
+        ["Type"] = "Daily",
+        ["Goal"] = 2,
+        ["Description"] = "Win twice in the Infected Limited Mode",
+        ["Reward"] = 100
+    }
+}
