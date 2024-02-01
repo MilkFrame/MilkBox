60d6bb0496f1a4a7fd40e97f570d766679bf3c1d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:31:00 +1100

    Create BananaKillRewards.lua

---
+++ GameData/Blade Ball [13772394625]/BananaKillRewards.lua
@@ -0,0 +1,71 @@
+data = {
+    ["Rewards"] = {
+        {
+            ["Item"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 1,
+                ["Value"] = "PremiumExplosion",
+                ["Icon"] = "rbxassetid://15049303003",
+                ["DisplayName"] = "Premium Explosion Crate"
+            },
+            ["Requirement"] = 20
+        },
+        {
+            ["Item"] = {
+                ["Value"] = 1000,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713794582",
+                ["DisplayName"] = "1000 Coins"
+            },
+            ["Requirement"] = 50
+        },
+        {
+            ["Item"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 1,
+                ["Value"] = "PremiumSword",
+                ["Icon"] = "rbxassetid://16039967646",
+                ["DisplayName"] = "Premium Sword Crate"
+            },
+            ["Requirement"] = 100
+        },
+        {
+            ["Item"] = {
+                ["Value"] = "Emote105",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://16044136015",
+                ["DisplayName"] = "Monkey Taunt Emote"
+            },
+            ["Requirement"] = 200
+        },
+        {
+            ["Item"] = {
+                ["Value"] = "Banana Aura",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://16044553874",
+                ["DisplayName"] = "Banana Aura Explosion"
+            },
+            ["Requirement"] = 350
+        },
+        {
+            ["Item"] = {
+                ["Value"] = "Banana Sword",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://16027136120",
+                ["DisplayName"] = "Banana Sword"
+            },
+            ["Requirement"] = 750
+        },
+        {
+            ["Requirement"] = 1500,
+            ["Item"] = {
+                ["Value"] = "Emote109",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://16044137355",
+                ["DisplayName"] = "Twirl n' Peel Emote"
+            },
+            ["Hide"] = true
+        }
+    },
+    ["End"] = "1706932800000"
+}
