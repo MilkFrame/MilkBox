4def317615045447bcc0dd1adfaa143e0b6879a1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:31:33 +1100

    Create Subscriptions.lua

---
+++ GameData/Blade Ball [13772394625]/Subscriptions.lua
@@ -0,0 +1,30 @@
+data = {
+    ["VIPPlus"] = {
+        ["RobuxEquivalent"] = 999,
+        ["TestServerID"] = "EXP-6025518231484891237",
+        ["ProductPrice"] = 1199,
+        ["Duration"] = 2592000,
+        ["DisplayName"] = "VIP+",
+        ["Deactivate"] = "function: 0x0000000084d8fe9b",
+        ["Name"] = "VIPPlus",
+        ["ProductId"] = 1688955853,
+        ["ID"] = "EXP-3779330262602350693",
+        ["Revoke"] = "function: 0x000000002c5ee2e3",
+        ["Activate"] = "function: 0x00000000ff333df3",
+        ["SubscriptionPrice"] = "$4.99"
+    },
+    ["ClanPointsMonthly"] = {
+        ["RobuxEquivalent"] = 499,
+        ["TestServerID"] = "EXP-3286897663245287638",
+        ["ProductPrice"] = 699,
+        ["Duration"] = 2592000,
+        ["DisplayName"] = "Clan Points Monthly Pass",
+        ["Deactivate"] = "function: 0x00000000cc111a6b",
+        ["Name"] = "ClanPointsMonthly",
+        ["ProductId"] = 1708274980,
+        ["ID"] = "EXP-7934027470910259309",
+        ["Revoke"] = "function: 0x00000000b63a0b63",
+        ["Activate"] = "function: 0x00000000b067682b",
+        ["SubscriptionPrice"] = "-"
+    }
+}
