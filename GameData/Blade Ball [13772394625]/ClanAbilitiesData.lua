259b6a5fed10a14c4911f008a958a3d1fbe3cffc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:40:13 +1100

    Create ClanAbilitiesData.lua

---
+++ GameData/Blade Ball [13772394625]/ClanAbilitiesData.lua
@@ -0,0 +1,14 @@
+data = {
+    ["checkRequirementsFor"] = "function: 0x0000000073e4b133",
+    ["Abilities"] = {
+        ["Calming Deflection"] = {
+            ["Upgrades"] = {["Welfare"] = 6, ["CoinEarning"] = 6, ["Luck"] = 4, ["Size"] = 5},
+            ["Level"] = 2
+        },
+        ["Quantum Arena"] = {
+            ["Upgrades"] = {["Welfare"] = 7, ["CoinEarning"] = 7, ["Luck"] = 6, ["Size"] = 7},
+            ["Level"] = 2
+        }
+    },
+    ["getClanAbilities"] = "function: 0x000000009a8732cb"
+}
