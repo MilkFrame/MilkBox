dbbc311e672bb4ce1c364bd85c682231b828d0d6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:32:02 +1100

    Create RankedPenaltyData.lua

---
+++ GameData/Blade Ball [13772394625]/RankedPenaltyData.lua
@@ -0,0 +1,14 @@
+data = {
+    ["NonPunishablePenalty"] = {["MatchInProgress"] = true},
+    ["GetListType"] = "function: 0x000000009ce9cbd3",
+    ["GracePeriodAmount"] = 3,
+    ["CurrentRankedPenaltyDataVersion"] = 4,
+    ["AMOUNT_OF_GAMES_FOR_CLEAR"] = 30,
+    ["PunishablePenaltyReason"] = {
+        ["Warning"] = false,
+        ["Adandoning"] = true,
+        ["MatchInProgress"] = false,
+        ["AFK"] = true
+    },
+    ["RESET_TIME_FOR_CLEAR"] = 604800
+}
