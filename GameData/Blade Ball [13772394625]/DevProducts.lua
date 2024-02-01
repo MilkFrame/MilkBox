65ac714ff19c6f5906dbed51a45655e7fa8a2e7f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:38:58 +1100

    Create DevProducts.lua

---
+++ GameData/Blade Ball [13772394625]/DevProducts.lua
@@ -0,0 +1,52 @@
+data = {
+    {
+        ["CoinReward"] = 1000,
+        ["TitleText"] = "Small",
+        ["Name"] = "SmallCoins",
+        ["ProductId"] = 1599945740,
+        ["Description"] = "Receive a small amount of coins.",
+        ["ItemType"] = "DevProduct",
+        ["Icon"] = "rbxassetid://14713797174",
+        ["DisplayName"] = "Small Coin Pack"
+    },
+    {
+        ["CoinReward"] = 3750,
+        ["TitleText"] = "Medium",
+        ["Name"] = "MedCoins",
+        ["ProductId"] = 1599946040,
+        ["Description"] = "Receive a medium amount of coins.",
+        ["ItemType"] = "DevProduct",
+        ["Icon"] = "rbxassetid://14713794582",
+        ["DisplayName"] = "Medium Coin Pack"
+    },
+    {
+        ["CoinReward"] = 8000,
+        ["TitleText"] = "Big",
+        ["Name"] = "BigCoins",
+        ["ProductId"] = 1599944759,
+        ["Description"] = "Receive a big amount of coins.",
+        ["ItemType"] = "DevProduct",
+        ["Icon"] = "rbxassetid://14713779371",
+        ["DisplayName"] = "Big Coin Pack"
+    },
+    {
+        ["CoinReward"] = 14500,
+        ["TitleText"] = "Huge",
+        ["Name"] = "HugeCoins",
+        ["ProductId"] = 1599946789,
+        ["Description"] = "Receive a huge amount of coins.",
+        ["ItemType"] = "DevProduct",
+        ["Icon"] = "rbxassetid://14713785287",
+        ["DisplayName"] = "Huge Coin Pack"
+    },
+    {
+        ["CoinReward"] = 24000,
+        ["TitleText"] = "Massive",
+        ["Name"] = "MassiveCoins",
+        ["ProductId"] = 1599946917,
+        ["Description"] = "Receive a massive  amount of coins.",
+        ["ItemType"] = "DevProduct",
+        ["Icon"] = "rbxassetid://14713787523",
+        ["DisplayName"] = "Massive Coin Pack"
+    }
+}
