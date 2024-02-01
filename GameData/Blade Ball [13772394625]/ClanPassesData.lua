9d3569219a1284b4c7e520c4de3d750bb7341ae4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:42:15 +1100

    Create ClanPassesData.lua

---
+++ GameData/Blade Ball [13772394625]/ClanPassesData.lua
@@ -0,0 +1,58 @@
+data = {
+    ["Rewards"] = {
+        ["Monthly"] = {
+            ["FirstSubscriptionBonus"] = {
+                {
+                    ["Value"] = 90,
+                    ["Type"] = "ClanPoints",
+                    ["Icon"] = "rbxassetid://15636184218",
+                    ["DisplayName"] = "90 Clan Points"
+                }
+            },
+            ["Total"] = 6400,
+            ["DaysDuration"] = 30,
+            ["ProductId"] = 1708274980,
+            ["Extra"] = 10,
+            ["Instant"] = {
+                {
+                    ["Value"] = 400,
+                    ["Type"] = "ClanPoints",
+                    ["Icon"] = "rbxassetid://15636248286",
+                    ["DisplayName"] = "400 Clan Points"
+                }
+            },
+            ["Daily"] = {
+                {
+                    ["Value"] = 200,
+                    ["Type"] = "ClanPoints",
+                    ["Icon"] = "rbxassetid://15636248286",
+                    ["DisplayName"] = "200 Clan Points"
+                }
+            },
+            ["TotalIcon"] = "rbxassetid://15636184218"
+        },
+        ["Weekly"] = {
+            ["Total"] = 900,
+            ["DaysDuration"] = 7,
+            ["ProductId"] = 1708274791,
+            ["Extra"] = 3,
+            ["Instant"] = {
+                {
+                    ["Value"] = 200,
+                    ["Type"] = "ClanPoints",
+                    ["Icon"] = "rbxassetid://15636248286",
+                    ["DisplayName"] = "200 Clan Points"
+                }
+            },
+            ["Daily"] = {
+                {
+                    ["Value"] = 100,
+                    ["Type"] = "ClanPoints",
+                    ["Icon"] = "rbxassetid://15636248286",
+                    ["DisplayName"] = "100 Clan Points"
+                }
+            },
+            ["TotalIcon"] = "rbxassetid://15636184218"
+        }
+    }
+}
