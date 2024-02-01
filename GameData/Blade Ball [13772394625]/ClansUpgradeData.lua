f504f0330654d02eee4150b883beb83d2f9dc618
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:41:59 +1100

    Create ClansUpgradeData.lua

---
+++ GameData/Blade Ball [13772394625]/ClansUpgradeData.lua
@@ -0,0 +1,170 @@
+data = {
+    ["getUpgrade"] = "function: 0x0000000084474673",
+    ["MaxCrownsDonates"] = 1,
+    ["getWelfareClaim"] = "function: 0x00000000992d0bf3",
+    ["Upgrades"] = {
+        ["Welfare"] = {
+            ["Levels"] = {
+                {
+                    ["GP"] = 900,
+                    ["XP"] = 0,
+                    ["Rewards"] = {
+                        {
+                            ["Value"] = 100,
+                            ["Type"] = "Coins",
+                            ["Icon"] = "rbxassetid://14713797174",
+                            ["DisplayName"] = "100 Coins"
+                        }
+                    }
+                },
+                {
+                    ["GP"] = 1200,
+                    ["XP"] = 2000,
+                    ["Rewards"] = {
+                        {
+                            ["Value"] = 100,
+                            ["Type"] = "Coins",
+                            ["Icon"] = "rbxassetid://14713797174",
+                            ["DisplayName"] = "100 Coins"
+                        }
+                    }
+                },
+                {
+                    ["GP"] = 900,
+                    ["XP"] = 4500,
+                    ["Rewards"] = {
+                        {
+                            ["Value"] = 100,
+                            ["Type"] = "Coins",
+                            ["Icon"] = "rbxassetid://14713797174",
+                            ["DisplayName"] = "100 Coins"
+                        }
+                    }
+                },
+                {
+                    ["GP"] = 1200,
+                    ["XP"] = 12000,
+                    ["Rewards"] = {
+                        {
+                            ["Value"] = 10,
+                            ["Type"] = "Crowns",
+                            ["Icon"] = "rbxassetid://15579450524",
+                            ["DisplayName"] = "10 Crowns"
+                        }
+                    }
+                },
+                {
+                    ["GP"] = 1800,
+                    ["XP"] = 25000,
+                    ["Rewards"] = {
+                        {
+                            ["Value"] = 15,
+                            ["Type"] = "Crowns",
+                            ["Icon"] = "rbxassetid://15579450524",
+                            ["DisplayName"] = "15 Crowns"
+                        }
+                    }
+                },
+                {
+                    ["GP"] = 3600,
+                    ["XP"] = 65000,
+                    ["Rewards"] = {
+                        {
+                            ["Value"] = 20,
+                            ["Type"] = "Crowns",
+                            ["Icon"] = "rbxassetid://15579450524",
+                            ["DisplayName"] = "20 Crowns"
+                        }
+                    }
+                },
+                {
+                    ["GP"] = 1500,
+                    ["XP"] = 180000,
+                    ["Rewards"] = {
+                        {
+                            ["Value"] = 25,
+                            ["Type"] = "Crowns",
+                            ["Icon"] = "rbxassetid://15579450524",
+                            ["DisplayName"] = "25 Crowns"
+                        }
+                    }
+                },
+                {
+                    ["GP"] = 1800,
+                    ["XP"] = 250000,
+                    ["Rewards"] = {
+                        {
+                            ["Value"] = 30,
+                            ["Type"] = "Crowns",
+                            ["Icon"] = "rbxassetid://15579450524",
+                            ["DisplayName"] = "30 Crowns"
+                        },
+                        {
+                            ["Type"] = "Chest",
+                            ["Amount"] = 1,
+                            ["Value"] = "Clan Sword",
+                            ["Icon"] = "",
+                            ["DisplayName"] = "Clan Sword Chest"
+                        }
+                    }
+                }
+            },
+            ["Description"] = "All clan members can claim a reward at %s and has to be claimed within a certain time period. This upgrade increases the grace period for that time.",
+            ["IconBackground"] = "rbxassetid://15518687236",
+            ["Icon"] = "rbxassetid://15593970264",
+            ["DisplayName"] = "Welfare"
+        },
+        ["CoinEarning"] = {
+            ["Levels"] = {
+                {["XP"] = 0, ["Value"] = 0},
+                {["XP"] = 800, ["Value"] = 0.01},
+                {["XP"] = 1500, ["Value"] = 0.02},
+                {["XP"] = 4500, ["Value"] = 0.025},
+                {["XP"] = 16000, ["Value"] = 0.03},
+                {["XP"] = 29000, ["Value"] = 0.05},
+                {["XP"] = 85000, ["Value"] = 0.1},
+                {["XP"] = 200000, ["Value"] = 0.15}
+            },
+            ["Description"] = "Increases coin multiplier for all alliance members!",
+            ["IconBackground"] = "rbxassetid://15518226758",
+            ["Icon"] = "rbxassetid://15594219221",
+            ["DisplayName"] = "Coin Earning"
+        },
+        ["Luck"] = {
+            ["Levels"] = {
+                {["XP"] = 0, ["Value"] = 0},
+                {["XP"] = 4000, ["Value"] = 0.005},
+                {["XP"] = 15000, ["Value"] = 0.015},
+                {["XP"] = 50000, ["Value"] = 0.025},
+                {["XP"] = 180000, ["Value"] = 0.04},
+                {["XP"] = 400000, ["Value"] = 0.05},
+                {["XP"] = 900000, ["Value"] = 0.07},
+                {["XP"] = 1500000, ["Value"] = 0.1}
+            },
+            ["Description"] = "Increases the luck chances for getting anything rare in Blade Ball, for all of the clan!",
+            ["IconBackground"] = "rbxassetid://15518687330",
+            ["Icon"] = "rbxassetid://15592780889",
+            ["DisplayName"] = "Luck"
+        },
+        ["Size"] = {
+            ["Levels"] = {
+                {["XP"] = 0, ["Value"] = 12},
+                {["XP"] = 2000, ["Value"] = 20},
+                {["XP"] = 4500, ["Value"] = 25},
+                {["XP"] = 20000, ["Value"] = 30},
+                {["XP"] = 75000, ["Value"] = 35},
+                {["XP"] = 200000, ["Value"] = 40},
+                {["XP"] = 450000, ["Value"] = 45},
+                {["XP"] = 1000000, ["Value"] = 50}
+            },
+            ["Description"] = "Increases the amount of members that can be in your clan!",
+            ["IconBackground"] = "rbxassetid://15592780082",
+            ["Icon"] = "rbxassetid://15594226000",
+            ["DisplayName"] = "Size"
+        }
+    },
+    ["getClanUpgrade"] = "function: 0x0000000033d2156b",
+    ["MinClanPointsDonate"] = 1,
+    ["CrownsResetTime"] = 1800,
+    ["CrownsPerDonate"] = 100
+}
