24a7f1c4b1c7912cc0921fb2339da831a332d3fc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:39:42 +1100

    Create ClanActivityData.lua

---
+++ GameData/Blade Ball [13772394625]/ClanActivityData.lua
@@ -0,0 +1,226 @@
+data = {
+    ["GetQuestInformationForWeekly"] = "function: 0x000000002d781e23",
+    ["getTodayJoinLimit"] = "function: 0x00000000d35d0dfb",
+    ["RefreshAllQuestsWeekly"] = 1700717634,
+    ["WeeklyMilestones"] = {{["Crowns"] = 60}, {["Crowns"] = 65}, {["Crowns"] = 75}, {["Crowns"] = 70}},
+    ["GetQuestInformationForDaily"] = "function: 0x00000000dfa57ceb",
+    ["RefreshAllQuests"] = 1700645992,
+    ["getActivityFromTime"] = "function: 0x000000006dd8cbd3",
+    ["WeeklyMissions"] = {
+        ["Place_Top_In_Ranked"] = {
+            ["SimpleReward"] = {["Crowns"] = 20, ["ActivityPoints"] = 20},
+            ["MaxProgress"] = 5,
+            ["ProgressAction"] = "Place_Top_In_Ranked",
+            ["QuestKey"] = "Place_Top_In_Ranked",
+            ["DisplayName"] = "Place top 1 in ranked 5+ times"
+        },
+        ["Gain_Elo"] = {
+            ["SimpleReward"] = {["Crowns"] = 20, ["ActivityPoints"] = 20},
+            ["ProgressFunction"] = "function: 0x0000000091300f63",
+            ["MaxProgress"] = 500,
+            ["ProgressAction"] = "Gain_Elo",
+            ["QuestKey"] = "Gain_Elo",
+            ["DisplayName"] = "Gain 500+ ELO in Ranked"
+        },
+        ["Win_Match"] = {
+            ["SimpleReward"] = {["Crowns"] = 30, ["ActivityPoints"] = 30},
+            ["StaticOnly"] = true,
+            ["MaxProgress"] = 10,
+            ["ProgressAction"] = "Win_Match",
+            ["QuestKey"] = "Win_Match",
+            ["DisplayName"] = "Win 10 matches"
+        },
+        ["Play_Map"] = {
+            ["SimpleReward"] = {["Crowns"] = 20, ["ActivityPoints"] = 20},
+            ["MaxProgress"] = 10,
+            ["ProgressFunction"] = "function: 0x000000004cc6438b",
+            ["IntializeFunction"] = "function: 0x0000000013ce203b",
+            ["ProgressAction"] = "Play_Map",
+            ["QuestKey"] = "Play_Map",
+            ["DisplayName"] = "Play on 10+ different maps"
+        },
+        ["Gift_To_Clan_Mate"] = {
+            ["SimpleReward"] = {["Crowns"] = 20, ["ActivityPoints"] = 20},
+            ["MaxProgress"] = 1,
+            ["ProgressAction"] = "Gift_To_Clan_Mate",
+            ["QuestKey"] = "Gift_To_Clan_Mate",
+            ["DisplayName"] = "Gift a Robux item/pass to a clan mate"
+        },
+        ["Play_With_Clan_Mates"] = {
+            ["SimpleReward"] = {["Crowns"] = 20, ["ActivityPoints"] = 20},
+            ["ProgressFunction"] = "function: 0x00000000d0019b13",
+            ["MaxProgress"] = 1,
+            ["ProgressAction"] = "Play_With_Clan_Member",
+            ["QuestKey"] = "Play_With_Clan_Mates",
+            ["DisplayName"] = "Play with 2+ clan mates in one game"
+        },
+        ["Get_Kills_Pro_Server"] = {
+            ["SimpleReward"] = {["Crowns"] = 20, ["ActivityPoints"] = 20},
+            ["ProgressFunction"] = "function: 0x0000000062bd30bb",
+            ["MaxProgress"] = 100,
+            ["ProgressAction"] = "Eliminate",
+            ["DisplayName"] = "Get 100 elims in pro lobbies"
+        },
+        ["Unbox_Legendary_Skins"] = {
+            ["SimpleReward"] = {["Crowns"] = 20, ["ActivityPoints"] = 20},
+            ["MaxProgress"] = 3,
+            ["ProgressAction"] = "Unbox_Legendary_Skin",
+            ["QuestKey"] = "Unbox_Legendary_Skins",
+            ["DisplayName"] = "Unbox 3 legendary skins from the sword or explosion crate."
+        },
+        ["Play_Ranked_Duo_With_Clan_Mate"] = {
+            ["SimpleReward"] = {["Crowns"] = 20, ["ActivityPoints"] = 20},
+            ["MaxProgress"] = 25,
+            ["ProgressAction"] = "Play_Ranked_Duo_With_Clan_Member",
+            ["QuestKey"] = "Play_Ranked_Duo_With_Clan_Mate",
+            ["DisplayName"] = "Play 25 ranked duo matches with a clan mate"
+        },
+        ["Play_BladeBall"] = {
+            ["SimpleReward"] = {["Crowns"] = 20, ["ActivityPoints"] = 25},
+            ["StaticOnly"] = true,
+            ["IntializeFunction"] = "function: 0x0000000054ac3633",
+            ["ProgressFunction"] = "function: 0x0000000056ba02eb",
+            ["MaxProgress"] = 300,
+            ["ProgressAction"] = "Play_Time",
+            ["QuestKey"] = "Play_BladeBall",
+            ["DisplayName"] = "Play Blade Ball for 300 minutes"
+        },
+        ["Login_To_BladeBall"] = {
+            ["SimpleReward"] = {["Crowns"] = 20, ["ActivityPoints"] = 20},
+            ["StaticOnly"] = true,
+            ["MaxProgress"] = 5,
+            ["ProgressAction"] = "Login",
+            ["QuestKey"] = "Login_To_BladeBall",
+            ["DisplayName"] = "Log in to Blade Ball on 5 different days"
+        },
+        ["Earn_Credit"] = {
+            ["SimpleReward"] = {["Crowns"] = 20, ["ActivityPoints"] = 20},
+            ["ProgressFunction"] = "function: 0x000000002e08bec3",
+            ["MaxProgress"] = 7000,
+            ["ProgressAction"] = "Match_Spend_Coins",
+            ["QuestKey"] = "Earn_Credit",
+            ["DisplayName"] = "Earn 7,000 coins in any game mode"
+        }
+    },
+    ["getListFromTime"] = "function: 0x00000000bee491cb",
+    ["ClanProgressionLevels"] = {
+        {["Amount"] = 4300},
+        {["Amount"] = 13700},
+        {["Amount"] = 24400},
+        {["Amount"] = 37400},
+        {["Amount"] = 125900},
+        {["Amount"] = 157400}
+    },
+    ["DailyMissions"] = {
+        ["Eliminate_With_Clan_Member"] = {
+            ["SimpleReward"] = {["Crowns"] = 5, ["ActivityPoints"] = 10},
+            ["ProgressFunction"] = "function: 0x000000001ad55333",
+            ["MaxProgress"] = 25,
+            ["ProgressAction"] = "Eliminate",
+            ["QuestKey"] = "Eliminate_With_Clan_Member",
+            ["DisplayName"] = "Eliminate 25 players with a clan mate in your game"
+        },
+        ["Play_With_Clan_Member"] = {
+            ["SimpleReward"] = {["Crowns"] = 5, ["ActivityPoints"] = 10},
+            ["ProgressFunction"] = "function: 0x0000000075d989ab",
+            ["MaxProgress"] = 1,
+            ["ProgressAction"] = "Play_With_Clan_Member",
+            ["QuestKey"] = "Play_With_Clan_Member",
+            ["DisplayName"] = "Play with 1+ clan members"
+        },
+        ["Eliminate_Clan_Member"] = {
+            ["SimpleReward"] = {["Crowns"] = 5, ["ActivityPoints"] = 10},
+            ["ProgressFunction"] = "function: 0x00000000afa0359b",
+            ["MaxProgress"] = 3,
+            ["ProgressAction"] = "EliminateVictim",
+            ["QuestKey"] = "Eliminate_Clan_Member",
+            ["DisplayName"] = "Eliminate 3 clan members in any game mode"
+        },
+        ["Eliminate_Different_Clan"] = {
+            ["SimpleReward"] = {["Crowns"] = 5, ["ActivityPoints"] = 10},
+            ["ProgressFunction"] = "function: 0x000000000289e68b",
+            ["MaxProgress"] = 5,
+            ["ProgressAction"] = "Eliminate_Different_Clan",
+            ["QuestKey"] = "Eliminate_Different_Clan",
+            ["DisplayName"] = "Eliminate 5 players from a Different clan"
+        },
+        ["Use_Same_Sword_As_Clan_Member"] = {
+            ["SimpleReward"] = {["Crowns"] = 5, ["ActivityPoints"] = 10},
+            ["MaxProgress"] = 1,
+            ["ProgressAction"] = "Use_Same_Sword_As_Clan_Member",
+            ["QuestKey"] = "Use_Same_Sword_As_Clan_Member",
+            ["DisplayName"] = "Use the same sword as your clan mate"
+        },
+        ["Win_Duel_Round"] = {
+            ["SimpleReward"] = {["Crowns"] = 5, ["ActivityPoints"] = 10},
+            ["MaxProgress"] = 3,
+            ["ProgressAction"] = "Win_Duel_Flawless",
+            ["QuestKey"] = "Win_Duel_Round",
+            ["DisplayName"] = "Win 3 duels without losing a single round"
+        },
+        ["Eliminate_With_Dash"] = {
+            ["SimpleReward"] = {["Crowns"] = 5, ["ActivityPoints"] = 10},
+            ["ProgressFunction"] = "function: 0x00000000875e4fe3",
+            ["MaxProgress"] = 5,
+            ["ProgressAction"] = "Kill_Streak_Increment",
+            ["QuestKey"] = "Eliminate_With_Dash",
+            ["DisplayName"] = "Eliminate 5 players in one game using dash"
+        },
+        ["Play_Ranked_Duo_With_Clan_Member"] = {
+            ["SimpleReward"] = {["Crowns"] = 5, ["ActivityPoints"] = 10},
+            ["MaxProgress"] = 5,
+            ["ProgressAction"] = "Play_Ranked_Duo_With_Clan_Member",
+            ["QuestKey"] = "Play_Ranked_Duo_With_Clan_Member",
+            ["DisplayName"] = "Play 5 Ranked Duos games with a clan mate"
+        },
+        ["Win_With_Clan_Member"] = {
+            ["SimpleReward"] = {["Crowns"] = 5, ["ActivityPoints"] = 10},
+            ["MaxProgress"] = 3,
+            ["ProgressAction"] = "Win_With_Clan_Member",
+            ["QuestKey"] = "Win_With_Clan_Member",
+            ["DisplayName"] = "Win 3 games playing with someone in your clan"
+        },
+        ["Place_Top_In_Ranked"] = {
+            ["SimpleReward"] = {["Crowns"] = 5, ["ActivityPoints"] = 10},
+            ["MaxProgress"] = 1,
+            ["ProgressAction"] = "Place_Top_In_Ranked",
+            ["QuestKey"] = "Place_Top_In_Ranked",
+            ["DisplayName"] = "Place Top in Ranked"
+        },
+        ["Gain_Elo"] = {
+            ["SimpleReward"] = {["Crowns"] = 5, ["ActivityPoints"] = 10},
+            ["ProgressFunction"] = "function: 0x000000009a266d5b",
+            ["MaxProgress"] = 100,
+            ["ProgressAction"] = "Gain_Elo",
+            ["QuestKey"] = "Gain_Elo",
+            ["DisplayName"] = "Gain 100 Ranked ELO"
+        },
+        ["Play_BladeBall"] = {
+            ["StaticOnly"] = true,
+            ["CustomReward"] = {["RewardFunction"] = "function: 0x000000001bd9dd83"},
+            ["ProgressFunction"] = "function: 0x000000005a93c44b",
+            ["IntializeFunction"] = "function: 0x000000005cf47513",
+            ["QuestKey"] = "Play_BladeBall",
+            ["ProgressAction"] = "Play_Time"
+        },
+        ["Login_To_BladeBall"] = {
+            ["SimpleReward"] = {["Crowns"] = 5, ["ActivityPoints"] = 10},
+            ["StaticOnly"] = true,
+            ["MaxProgress"] = 1,
+            ["ProgressAction"] = "Login",
+            ["QuestKey"] = "Login_To_BladeBall",
+            ["DisplayName"] = "Login to Blade Ball"
+        },
+        ["Play_In_Training_Mode"] = {
+            ["SimpleReward"] = {["Crowns"] = 5, ["ActivityPoints"] = 10},
+            ["ProgressFunction"] = "function: 0x000000007d6de403",
+            ["MaxProgress"] = 600,
+            ["ProgressAction"] = "Play_In_Training_Mode",
+            ["QuestKey"] = "Play_In_Training_Mode",
+            ["DisplayName"] = "Play training mode for 10 minutes"
+        }
+    },
+    ["getWeekNumber"] = "function: 0x0000000042df9bf3",
+    ["ClanWeeklyMilestones"] = {{["Crowns"] = 20}, {["Crowns"] = 15}, {["Crowns"] = 30}},
+    ["MaxActivityPointsTrack"] = 200
+}
