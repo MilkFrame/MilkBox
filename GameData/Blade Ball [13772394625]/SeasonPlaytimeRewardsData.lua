a163f0c798e6990a5fa09af50f1d18614c549c1d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:28:40 +1100

    Update SeasonPlaytimeRewardsData.lua

--- GameData/Blade Ball [13772394625]/SeasonPlaytimeRewardsData.lua
+++ GameData/Blade Ball [13772394625]/SeasonPlaytimeRewardsData.lua
@@ -1,7 +1,7 @@
 data = {
-    {["Reward"] = {["CurrencyName"] = "Pumpkins", ["Amount"] = 25}, ["Seconds"] = 300},
-    {["Reward"] = {["CurrencyName"] = "Pumpkins", ["Amount"] = 25}, ["Seconds"] = 1200},
-    {["Reward"] = {["CurrencyName"] = "Pumpkins", ["Amount"] = 50}, ["Seconds"] = 3600},
-    {["Reward"] = {["CurrencyName"] = "Pumpkins", ["Amount"] = 75}, ["Seconds"] = 5400},
-    {["Reward"] = {["CurrencyName"] = "Pumpkins", ["Amount"] = 100}, ["Seconds"] = 10800}
+    {["Reward"] = {["CurrencyName"] = "Balloons", ["Amount"] = 25}, ["Seconds"] = 300},
+    {["Reward"] = {["CurrencyName"] = "Balloons", ["Amount"] = 25}, ["Seconds"] = 1200},
+    {["Reward"] = {["CurrencyName"] = "Balloons", ["Amount"] = 50}, ["Seconds"] = 3600},
+    {["Reward"] = {["CurrencyName"] = "Balloons", ["Amount"] = 75}, ["Seconds"] = 5400},
+    {["Reward"] = {["CurrencyName"] = "Balloons", ["Amount"] = 100}, ["Seconds"] = 10800}
 }
