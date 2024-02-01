60cfda76b41428557d0d509cb35e792436b8af92
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-01 15:18:15 +1100

    Create DailyLoginInfo.lua

---
+++ GameData/Blade Ball [13772394625]/DailyLoginInfo.lua
@@ -0,0 +1,280 @@
+data = {
+    ["Legacy"] = {
+        ["Rewards"] = {
+            {["Credits"] = 60},
+            {["Credits"] = 180},
+            {["Credits"] = 360},
+            {["Credits"] = 520},
+            {["SwordSkins"] = {"Holy Sword"}}
+        }
+    },
+    ["D30"] = {
+        ["Rewards"] = {
+            {["Value"] = 30, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713797174", ["DisplayName"] = "30 Coins"},
+            {["Value"] = 40, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713797174", ["DisplayName"] = "40 Coins"},
+            {["Value"] = 50, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713797174", ["DisplayName"] = "50 Coins"},
+            {["Value"] = 100, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713794582", ["DisplayName"] = "100 Coins"},
+            {["Value"] = 150, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713794582", ["DisplayName"] = "150 Coins"},
+            {["Value"] = 250, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713779371", ["DisplayName"] = "250 Coins"},
+            {
+                ["Value"] = "Gladiator Sword",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://15316360055",
+                ["DisplayName"] = "Gladiator Sword"
+            },
+            {["Value"] = 80, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713797174", ["DisplayName"] = "80 Coins"},
+            {["Value"] = 150, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713797174", ["DisplayName"] = "150 Coins"},
+            {["Value"] = 200, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713794582", ["DisplayName"] = "200 Coins"},
+            {["Value"] = 300, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713779371", ["DisplayName"] = "300 Coins"},
+            {["Value"] = 500, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713779371", ["DisplayName"] = "500 Coins"},
+            {
+                ["Type"] = "Boost",
+                ["Value"] = "Coins2x",
+                ["Duration"] = 1800,
+                ["Icon"] = "rbxassetid://15289112140",
+                ["DisplayName"] = "2x Coins - 30 Minutes"
+            },
+            {
+                ["Type"] = "Crate",
+                ["Value"] = "BigChestOfCoins",
+                ["CrateType"] = "Coins",
+                ["Icon"] = "rbxassetid://15326928319",
+                ["DisplayName"] = "Big Chest of Coins"
+            },
+            {["Value"] = 100, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713779371", ["DisplayName"] = "100 Coins"},
+            {["Value"] = 200, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713779371", ["DisplayName"] = "200 Coins"},
+            {["Value"] = 300, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713785287", ["DisplayName"] = "300 Coins"},
+            {["Value"] = 400, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713785287", ["DisplayName"] = "400 Coins"},
+            {["Value"] = 500, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713785287", ["DisplayName"] = "500 Coins"},
+            {
+                ["Value"] = 1000,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713787523",
+                ["DisplayName"] = "1000 Coins"
+            },
+            {
+                ["Value"] = "Emote24",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://15317518516",
+                ["DisplayName"] = "Dance Moves Emote"
+            }
+        },
+        ["Cooldown"] = 72000,
+        ["ConsecutiveRewards"] = {
+            {
+                ["Value"] = "Void Sword",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://6034407076",
+                ["DisplayName"] = "Void Sword"
+            },
+            {
+                ["Value"] = "Dune Cleaver",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://6034407076",
+                ["DisplayName"] = "Dune Cleaver"
+            },
+            {
+                ["Value"] = "Ice Explosion",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://15376057384",
+                ["DisplayName"] = "Ice Explosion Explosion"
+            }
+        }
+    },
+    ["Longterm"] = {
+        ["Cooldown"] = 72000,
+        ["Rewards"] = {
+            {["Value"] = 30, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713797174", ["DisplayName"] = "30 Coins"},
+            {["Value"] = 40, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713797174", ["DisplayName"] = "40 Coins"},
+            {["Value"] = 50, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713797174", ["DisplayName"] = "50 Coins"},
+            {["Value"] = 100, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713794582", ["DisplayName"] = "100 Coins"},
+            {["Value"] = 150, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713794582", ["DisplayName"] = "150 Coins"},
+            {["Value"] = 250, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713779371", ["DisplayName"] = "250 Coins"},
+            {
+                ["Value"] = "Gladiator Sword",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://15316360055",
+                ["DisplayName"] = "Gladiator Sword"
+            },
+            {["Value"] = 80, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713797174", ["DisplayName"] = "80 Coins"},
+            {["Value"] = 150, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713797174", ["DisplayName"] = "150 Coins"},
+            {["Value"] = 200, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713794582", ["DisplayName"] = "200 Coins"},
+            {["Value"] = 300, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713779371", ["DisplayName"] = "300 Coins"},
+            {["Value"] = 500, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713779371", ["DisplayName"] = "500 Coins"},
+            {
+                ["Type"] = "Boost",
+                ["Value"] = "Coins2x",
+                ["Duration"] = 1800,
+                ["Icon"] = "rbxassetid://15289112140",
+                ["DisplayName"] = "2x Coins - 30 Minutes"
+            },
+            {
+                ["Type"] = "Crate",
+                ["Value"] = "BigChestOfCoins",
+                ["CrateType"] = "Coins",
+                ["Icon"] = "rbxassetid://15326928319",
+                ["DisplayName"] = "Big Chest of Coins"
+            },
+            {["Value"] = 100, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713779371", ["DisplayName"] = "100 Coins"},
+            {["Value"] = 200, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713779371", ["DisplayName"] = "200 Coins"},
+            {["Value"] = 300, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713785287", ["DisplayName"] = "300 Coins"},
+            {["Value"] = 400, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713785287", ["DisplayName"] = "400 Coins"},
+            {["Value"] = 500, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713785287", ["DisplayName"] = "500 Coins"},
+            {
+                ["Value"] = 1000,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713787523",
+                ["DisplayName"] = "1000 Coins"
+            },
+            {
+                ["Value"] = "Emote24",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://15317518516",
+                ["DisplayName"] = "Dance Moves Emote"
+            },
+            {
+                ["Type"] = "Crate",
+                ["Value"] = "SmallChestOfCoins",
+                ["CrateType"] = "Coins",
+                ["Icon"] = "rbxassetid://15326924950",
+                ["DisplayName"] = "Small Chest of Coins"
+            },
+            {
+                ["Type"] = "Boost",
+                ["Value"] = "Coins2x",
+                ["Duration"] = 3600,
+                ["Icon"] = "rbxassetid://15289112140",
+                ["DisplayName"] = "2x Coins - 60 Minutes"
+            },
+            {["Value"] = 300, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713785287", ["DisplayName"] = "300 Coins"},
+            {["Value"] = 500, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713785287", ["DisplayName"] = "500 Coins"},
+            {["Value"] = 750, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713787523", ["DisplayName"] = "750 Coins"},
+            {
+                ["Value"] = "Rainbow Explosion",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://15316881227",
+                ["DisplayName"] = "Rainbow Explosion Explosion"
+            },
+            {
+                ["Type"] = "Crate",
+                ["Value"] = "BigChestOfCoins",
+                ["CrateType"] = "Coins",
+                ["Icon"] = "rbxassetid://15326928319",
+                ["DisplayName"] = "Big Chest of Coins"
+            },
+            {["Value"] = 100, ["Type"] = "Coins", ["Icon"] = "rbxassetid://14713779371", ["DisplayName"] = "100 Coins"},
+            {
+                ["Value"] = {
+                    {
+                        {
+                            ["Type"] = "Crate",
+                            ["Value"] = "AncientSwordChest",
+                            ["CrateType"] = "Sword",
+                            ["DisplayName"] = "rbxassetid://15326916699"
+                        }
+                    },
+                    {
+                        {
+                            ["Value"] = 500,
+                            ["Type"] = "Coins",
+                            ["Icon"] = "rbxassetid://14713785287",
+                            ["DisplayName"] = "500 Coins"
+                        }
+                    },
+                    {
+                        {
+                            ["Value"] = 500,
+                            ["Type"] = "Coins",
+                            ["Icon"] = "rbxassetid://14713785287",
+                            ["DisplayName"] = "500 Coins"
+                        }
+                    },
+                    {
+                        {
+                            ["Value"] = 500,
+                            ["Type"] = "Coins",
+                            ["Icon"] = "rbxassetid://14713785287",
+                            ["DisplayName"] = "500 Coins"
+                        }
+                    },
+                    {
+                        {
+                            ["Value"] = 500,
+                            ["Type"] = "Coins",
+                            ["Icon"] = "rbxassetid://14713785287",
+                            ["DisplayName"] = "500 Coins"
+                        }
+                    }
+                },
+                ["Type"] = "MonthVariant",
+                ["DisplayName"] = "None"
+            }
+        }
+    },
+    ["DailyLogin"] = {
+        ["Rewards"] = {
+            {
+                ["Rewards"] = {
+                    {
+                        ["Value"] = 175,
+                        ["Type"] = "Coins",
+                        ["Icon"] = "rbxassetid://14713797174",
+                        ["DisplayName"] = "175 Coins"
+                    },
+                    {
+                        ["Value"] = "Holy Axe",
+                        ["Type"] = "Sword",
+                        ["Icon"] = "rbxassetid://6034407076",
+                        ["DisplayName"] = "Holy Axe"
+                    }
+                }
+            },
+            {
+                ["Rewards"] = {
+                    {
+                        ["Value"] = 425,
+                        ["Type"] = "Coins",
+                        ["Icon"] = "rbxassetid://14713794582",
+                        ["DisplayName"] = "425 Coins"
+                    }
+                }
+            },
+            {
+                ["Rewards"] = {
+                    {
+                        ["Value"] = 700,
+                        ["Type"] = "Coins",
+                        ["Icon"] = "rbxassetid://14713794582",
+                        ["DisplayName"] = "700 Coins"
+                    }
+                }
+            },
+            {
+                ["Rewards"] = {
+                    {
+                        ["Value"] = 1300,
+                        ["Type"] = "Coins",
+                        ["Icon"] = "rbxassetid://14713779371",
+                        ["DisplayName"] = "1300 Coins"
+                    },
+                    {
+                        ["Value"] = "Holy Sword",
+                        ["Type"] = "Sword",
+                        ["Icon"] = "rbxassetid://15016587129",
+                        ["DisplayName"] = "Holy Sword"
+                    }
+                }
+            },
+            {
+                ["Rewards"] = {
+                    {
+                        ["Value"] = 100,
+                        ["Type"] = "Coins",
+                        ["Icon"] = "rbxassetid://14713797174",
+                        ["DisplayName"] = "100 Coins"
+                    }
+                }
+            }
+        }
+    }
+}
