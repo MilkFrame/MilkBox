891b1f49d48e24a1eb2b1aa99c225ad98e98047b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-02 17:05:42 +1100

    Create rank-meta.lua

---
+++ GameData/BedWars [6872265039]/rank-meta.lua
@@ -0,0 +1,193 @@
+data = {
+    ["RankMeta"] = {
+        {
+            ["image"] = "rbxassetid://7904293227",
+            ["name"] = "Bronze 2",
+            ["tier"] = "bronze",
+            ["color"] = Color3.fromRGB(140, 74, 47),
+            ["prevRank"] = 0,
+            ["nextRank"] = 2,
+            ["imageSize"] = "{0.800000012, 0}, {0.800000012, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://7904293227",
+            ["name"] = "Bronze 3",
+            ["tier"] = "bronze",
+            ["color"] = Color3.fromRGB(140, 74, 47),
+            ["prevRank"] = 1,
+            ["nextRank"] = 3,
+            ["imageSize"] = "{0.800000012, 0}, {0.800000012, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://7904293227",
+            ["name"] = "Bronze 4",
+            ["tier"] = "bronze",
+            ["color"] = Color3.fromRGB(140, 74, 47),
+            ["prevRank"] = 2,
+            ["nextRank"] = 4,
+            ["imageSize"] = "{0.800000012, 0}, {0.800000012, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://7904292748",
+            ["name"] = "Silver 1",
+            ["tier"] = "silver",
+            ["color"] = Color3.fromRGB(207, 207, 207),
+            ["prevRank"] = 3,
+            ["nextRank"] = 5,
+            ["imageSize"] = "{0.800000012, 0}, {0.800000012, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://7904292748",
+            ["name"] = "Silver 2",
+            ["tier"] = "silver",
+            ["color"] = Color3.fromRGB(207, 207, 207),
+            ["prevRank"] = 4,
+            ["nextRank"] = 6,
+            ["imageSize"] = "{0.800000012, 0}, {0.800000012, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://7904292748",
+            ["name"] = "Silver 3",
+            ["tier"] = "silver",
+            ["color"] = Color3.fromRGB(207, 207, 207),
+            ["prevRank"] = 5,
+            ["nextRank"] = 7,
+            ["imageSize"] = "{0.800000012, 0}, {0.800000012, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://7904292748",
+            ["name"] = "Silver 4",
+            ["tier"] = "silver",
+            ["color"] = Color3.fromRGB(207, 207, 207),
+            ["prevRank"] = 6,
+            ["nextRank"] = 8,
+            ["imageSize"] = "{0.800000012, 0}, {0.800000012, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://7904293006",
+            ["name"] = "Gold 1",
+            ["tier"] = "gold",
+            ["color"] = Color3.fromRGB(253, 228, 0),
+            ["prevRank"] = 7,
+            ["nextRank"] = 9,
+            ["imageSize"] = "{0.850000024, 0}, {0.850000024, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://7904293006",
+            ["name"] = "Gold 2",
+            ["tier"] = "gold",
+            ["color"] = Color3.fromRGB(253, 228, 0),
+            ["prevRank"] = 8,
+            ["nextRank"] = 10,
+            ["imageSize"] = "{0.850000024, 0}, {0.850000024, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://7904293006",
+            ["name"] = "Gold 3",
+            ["tier"] = "gold",
+            ["color"] = Color3.fromRGB(253, 228, 0),
+            ["prevRank"] = 9,
+            ["nextRank"] = 11,
+            ["imageSize"] = "{0.850000024, 0}, {0.850000024, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://7904293006",
+            ["name"] = "Gold 4",
+            ["tier"] = "gold",
+            ["color"] = Color3.fromRGB(253, 228, 0),
+            ["prevRank"] = 10,
+            ["nextRank"] = 12,
+            ["imageSize"] = "{0.850000024, 0}, {0.850000024, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://7904292835",
+            ["name"] = "Platinum 1",
+            ["tier"] = "platinum",
+            ["color"] = Color3.fromRGB(0, 253, 192),
+            ["prevRank"] = 11,
+            ["nextRank"] = 13,
+            ["imageSize"] = "{0.899999976, 0}, {0.899999976, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://7904292835",
+            ["name"] = "Platinum 2",
+            ["tier"] = "platinum",
+            ["color"] = Color3.fromRGB(0, 253, 192),
+            ["prevRank"] = 12,
+            ["nextRank"] = 14,
+            ["imageSize"] = "{0.899999976, 0}, {0.899999976, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://7904292835",
+            ["name"] = "Platinum 3",
+            ["tier"] = "platinum",
+            ["color"] = Color3.fromRGB(0, 253, 192),
+            ["prevRank"] = 13,
+            ["nextRank"] = 15,
+            ["imageSize"] = "{0.899999976, 0}, {0.899999976, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://7904292835",
+            ["name"] = "Platinum 4",
+            ["tier"] = "platinum",
+            ["color"] = Color3.fromRGB(0, 253, 192),
+            ["prevRank"] = 14,
+            ["nextRank"] = 16,
+            ["imageSize"] = "{0.899999976, 0}, {0.899999976, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://7904293114",
+            ["name"] = "Diamond 1",
+            ["tier"] = "diamond",
+            ["color"] = Color3.fromRGB(53, 243, 255),
+            ["prevRank"] = 15,
+            ["nextRank"] = 17,
+            ["imageSize"] = "{1, 0}, {1, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://7904293114",
+            ["name"] = "Diamond 2",
+            ["tier"] = "diamond",
+            ["color"] = Color3.fromRGB(53, 243, 255),
+            ["prevRank"] = 16,
+            ["nextRank"] = 18,
+            ["imageSize"] = "{1, 0}, {1, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://7904293114",
+            ["name"] = "Diamond 3",
+            ["tier"] = "diamond",
+            ["color"] = Color3.fromRGB(53, 243, 255),
+            ["prevRank"] = 17,
+            ["nextRank"] = 19,
+            ["imageSize"] = "{1, 0}, {1, 0}"
+        },
+        {
+            ["image"] = "rbxassetid://12599231807",
+            ["name"] = "EMERALD",
+            ["tier"] = "emerald",
+            ["color"] = Color3.fromRGB(8, 199, 0),
+            ["imageSize"] = "{1, 0}, {1, 0}",
+            ["prevRank"] = 18,
+            ["noRPLimit"] = true
+        },
+        {
+            ["image"] = "rbxassetid://7904292926",
+            ["name"] = "NIGHTMARE",
+            ["tier"] = "nightmare",
+            ["color"] = Color3.fromRGB(87, 0, 173),
+            ["noInstantDemote"] = true,
+            ["imageSize"] = "{1, 0}, {1, 0}",
+            ["noRPLimit"] = true
+        },
+        {
+            ["image"] = "rbxassetid://7904293227",
+            ["name"] = "Bronze 1",
+            ["tier"] = "bronze",
+            ["color"] = Color3.fromRGB(140, 74, 47),
+            ["nextRank"] = 1,
+            ["imageSize"] = "{0.800000012, 0}, {0.800000012, 0}"
+        }
+    },
+    ["getRankColor"] = "function: 0x00000000c3acf71b"
+}
