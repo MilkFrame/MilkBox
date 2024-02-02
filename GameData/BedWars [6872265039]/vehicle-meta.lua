330d98d17e43e8e716162651ff1f192f60acada3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-02 17:16:06 +1100

    Create vehicle-meta.lua

---
+++ GameData/BedWars [6872265039]/vehicle-meta.lua
@@ -0,0 +1,11 @@
+data = {
+    ["getVehicleMeta"] = "function: 0x00000000ef1a41c3",
+    ["VehicleMeta"] = {
+        ["attack_helicopter"] = {["name"] = "Attack Helicopter", ["helicopter"] = {["attack"] = true}},
+        ["minicopter"] = {["name"] = "Minicopter", ["helicopter"] = {["attack"] = false}},
+        ["flying_broom"] = {["name"] = "Flying Broom", ["flying_broom"] = {}},
+        ["flying_cloud"] = {["name"] = "Flying Cloud", ["flying_cloud"] = {["attack"] = true}},
+        ["hot_air_balloon"] = {["name"] = "Hot Air Balloon", ["hot_air_balloon"] = {["attack"] = true}},
+        ["ufo"] = {["name"] = "UFO", ["ufo"] = {["attack"] = true}}
+    }
+}
