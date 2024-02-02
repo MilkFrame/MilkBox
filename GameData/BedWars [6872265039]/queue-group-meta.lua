66c5009421c44e19ffdf95723857939308c7e3f7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-02 16:46:06 +1100

    Create queue-group-meta.lua

---
+++ GameData/BedWars [6872265039]/queue-group-meta.lua
@@ -0,0 +1,373 @@
+data = {
+    ["QueueMenuDisplayType"] = {["FULL"] = "full", ["SQUARE"] = "square"},
+    ["getQueueGroupMeta"] = "function: 0x0000000099331c8b",
+    ["QueueGroupMeta"] = {
+        ["ctf"] = {
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(43, 51, 249),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}}},
+                ["backgroundImage"] = "rbxassetid://11108528694",
+                ["backgroundColor"] = Color3.fromRGB(134, 64, 223),
+                ["icon"] = "rbxassetid://11108528694",
+                ["displayType"] = "square"
+            },
+            ["subtitle"] = "Teams of 12",
+            ["title"] = "CTF",
+            ["queues"] = {{["queueType"] = "flag_capture", ["title"] = "(Teams of 12)"}}
+        },
+        ["bedwars_30v30"] = {
+            ["subtitle"] = "FUN ",
+            ["title"] = "30v30",
+            ["npcDisplay"] = {["title"] = "🎉 30v30", ["subtitle"] = [[<font color="rgb(85,255,85)">FUN!</font>]]},
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(0, 86, 165),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}}},
+                ["backgroundImage"] = "rbxassetid://11150313115",
+                ["backgroundColor"] = Color3.fromRGB(69, 182, 235),
+                ["icon"] = "rbxassetid://11150313115",
+                ["displayType"] = "square"
+            },
+            ["queues"] = {{["queueType"] = "bedwars_20v20", ["title"] = "(Teams of 30)"}},
+            ["description"] = [[🛌 Protect your base. Once it's gone, you can no longer respawn!\n\n💎 Gather resources to purchase items and team upgrades\n\n🏹 Destroy the enemy bed and eliminate players to win the game!\n\n]]
+        },
+        ["winter"] = {
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(18, 158, 255),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}}},
+                ["backgroundImage"] = "rbxassetid://11825851588",
+                ["backgroundColor"] = Color3.fromRGB(42, 242, 255),
+                ["icon"] = "rbxassetid://11825851588",
+                ["displayType"] = "square"
+            },
+            ["subtitle"] = "MINIGAMES",
+            ["title"] = "MINIGAME MOUNTAIN",
+            ["queues"] = {{["queueType"] = "winter_event", ["title"] = "MINIGAMES"}}
+        },
+        ["infected"] = {
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(0, 0, 0),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}}},
+                ["backgroundImage"] = "rbxassetid://11527394782",
+                ["backgroundColor"] = Color3.fromRGB(36, 205, 63),
+                ["icon"] = "rbxassetid://11527394782",
+                ["displayType"] = "square"
+            },
+            ["subtitle"] = "SURVIVE",
+            ["title"] = "INFECTED",
+            ["queues"] = {{["queueType"] = "infected", ["title"] = "SURVIVE"}}
+        },
+        ["gun_game"] = {
+            ["subtitle"] = "FFA",
+            ["title"] = "GUN GAME",
+            ["npcDisplay"] = {["title"] = "GUN GAME", ["subtitle"] = [[<font color="rgb(85,255,85)">FFA LTM</font>]]},
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(43, 51, 249),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}}},
+                ["backgroundImage"] = "rbxassetid://10977265283",
+                ["backgroundColor"] = Color3.fromRGB(134, 64, 223),
+                ["icon"] = "rbxassetid://10977265283",
+                ["displayType"] = "square"
+            },
+            ["description"] = [[🏎️ Race against your enemies through a series of weapons in this fast paced FFA mode. \n🗡️ Each kill grants you a new weapon.  Be the first player to progress through all 15 weapons to win!\n\n]],
+            ["queues"] = {{["queueType"] = "gun_game", ["title"] = "FFA"}}
+        },
+        ["halloween"] = {
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(0, 0, 0),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}, {"🔑", "Requires 1 Halloween LTM Key"}}},
+                ["backgroundImage"] = "rbxassetid://11331145133",
+                ["backgroundColor"] = Color3.fromRGB(36, 205, 63),
+                ["icon"] = "rbxassetid://11331144984",
+                ["displayType"] = "square"
+            },
+            ["subtitle"] = "EVENT",
+            ["title"] = "CRYPT'S COVEN",
+            ["queues"] = {{["queueType"] = "halloween_event", ["title"] = "(Teams of 4)"}}
+        },
+        ["hyper_gen"] = {
+            ["subtitle"] = "SQUADS",
+            ["title"] = "HYPER GEN",
+            ["npcDisplay"] = {["title"] = "💎 HYPER GEN", ["subtitle"] = [[<font color="rgb(85,255,85)">SQUADS</font>]]},
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(0, 86, 165),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}}},
+                ["backgroundImage"] = "rbxassetid://6850538161",
+                ["backgroundColor"] = Color3.fromRGB(69, 182, 235),
+                ["icon"] = "rbxassetid://6850538161",
+                ["displayType"] = "square"
+            },
+            ["queues"] = {{["queueType"] = "hyper_gen_to4", ["title"] = "HYPER GEN"}},
+            ["description"] = [[🤝 BedWars squads but faster\n💎 3x generator speed & forge experience\n🌟 Extreme team upgrades]]
+        },
+        ["bedwars_lucky_block"] = {
+            ["queues"] = {
+                {["queueType"] = "bedwars_lucky_block", ["title"] = "Squads"},
+                {["queueType"] = "bedwars_lucky_block_to2", ["title"] = "Doubles"}
+            },
+            ["title"] = "LUCKY BLOCK",
+            ["npcDisplay"] = {["title"] = "✨ Lucky Block", ["subtitle"] = [[<font color="rgb(85,255,85)">FUN!</font>]]},
+            ["menuDisplay"] = {
+                ["gradientColor"] = Color3.fromRGB(249, 194, 43),
+                ["helperIcons"] = {["text"] = {{"🔥", "Popular"}}},
+                ["backgroundImage"] = "rbxassetid://11109724236",
+                ["backgroundColor"] = Color3.fromRGB(154, 89, 89),
+                ["icon"] = "rbxassetid://11109720105",
+                ["displayType"] = "full"
+            },
+            ["description"] = [[✨ Break lucky blocks for a chance at surprising rewards and sometimes deadly encounters.\n\n🪂 Look out for Lucky Air Drops that periodically fall from the sky for a better chance of high-tier rewards.\n\n]]
+        },
+        ["skull_drop"] = {
+            ["subtitle"] = "FFA PVP",
+            ["title"] = "SKULL DROP",
+            ["npcDisplay"] = {
+                ["title"] = "💀 SKULL DROP",
+                ["subtitle"] = [[<font color="rgb(85,255,85)">⚔️ FFA PVP</font>]]
+            },
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(0, 86, 165),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}}},
+                ["backgroundImage"] = "rbxassetid://10730792783",
+                ["backgroundColor"] = Color3.fromRGB(69, 182, 235),
+                ["icon"] = "rbxassetid://13421692306",
+                ["displayType"] = "square"
+            },
+            ["queues"] = {{["queueType"] = "skull_drop", ["title"] = "SKULL DROP"}},
+            ["description"] = [[🛌 Kill players to drop their skulls!\n💎 Turn in skulls at designated drop points, first to 20 wins!\n]]
+        },
+        ["ranked"] = {
+            ["menuDisplay"] = {
+                ["layoutOrder"] = 1,
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(255, 248, 88),
+                ["backgroundImage"] = "rbxassetid://15372938656",
+                ["backgroundColor"] = Color3.fromRGB(236, 70, 48),
+                ["icon"] = "rbxassetid://7904292926",
+                ["displayType"] = "square"
+            },
+            ["subtitle"] = "5v5 ",
+            ["title"] = "RANKED",
+            ["queues"] = {{["queueType"] = "bedwars_ranked_s9", ["title"] = "5v5"}}
+        },
+        ["bridge_duel"] = {
+            ["subtitle"] = "1v1",
+            ["title"] = "BRIDGE BATTLE",
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(255, 140, 0),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}}},
+                ["backgroundImage"] = "rbxassetid://10993361352",
+                ["backgroundColor"] = Color3.fromRGB(50, 50, 255),
+                ["icon"] = "rbxassetid://10993361352",
+                ["displayType"] = "square"
+            },
+            ["queues"] = {{["queueType"] = "bridge_duel", ["title"] = "BRIDGE BATTLE"}},
+            ["description"] = "🌉 Conquer the bridge, clash with your opponent, and score touchdowns in their base to emerge victorious!"
+        },
+        ["tnt_wars"] = {
+            ["subtitle"] = "LTM",
+            ["title"] = "TNT WARS",
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(0, 86, 165),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}}},
+                ["backgroundImage"] = "rbxassetid://11104071738",
+                ["backgroundColor"] = Color3.fromRGB(69, 182, 235),
+                ["icon"] = "rbxassetid://11104071738",
+                ["displayType"] = "square"
+            },
+            ["queues"] = {{["queueType"] = "tnt_wars", ["title"] = "TNT WARS"}},
+            ["description"] = ""
+        },
+        ["winstreak"] = {
+            ["subtitle"] = "1v1",
+            ["title"] = "WINSTREAK",
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(255, 248, 88),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}}},
+                ["backgroundImage"] = "rbxassetid://8268259203",
+                ["backgroundColor"] = Color3.fromRGB(236, 70, 48),
+                ["icon"] = "rbxassetid://8268259203",
+                ["displayType"] = "square"
+            },
+            ["queues"] = {{["queueType"] = "winstreak_1v1", ["title"] = "WINSTREAK 1v1"}},
+            ["description"] = "🔥 Fortune favors the bold! In this BedWars variant you must 1v1 players on the same winstreak."
+        },
+        ["no_build_bedwars"] = {
+            ["subtitle"] = "SQUADS",
+            ["title"] = "NO BUILD",
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(0, 86, 165),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}}},
+                ["backgroundImage"] = "rbxassetid://7884372987",
+                ["backgroundColor"] = Color3.fromRGB(69, 182, 235),
+                ["icon"] = "rbxassetid://7884372987",
+                ["displayType"] = "square"
+            },
+            ["queues"] = {{["queueType"] = "bedwars_nb4", ["title"] = "NO BUILD BEDWARS"}},
+            ["description"] = "BedWars but no building!"
+        },
+        ["og_bedwars"] = {
+            ["subtitle"] = "SQUADS",
+            ["title"] = "OG BEDWARS",
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(0, 86, 165),
+                ["helperIcons"] = {["text"] = {{"🛏️", "Old School"}}},
+                ["backgroundImage"] = "rbxassetid://6875481413",
+                ["backgroundColor"] = Color3.fromRGB(69, 182, 235),
+                ["icon"] = "rbxassetid://6875481413",
+                ["displayType"] = "square"
+            },
+            ["queues"] = {{["queueType"] = "bedwars_og_to4", ["title"] = "OG BEDWARS"}},
+            ["description"] = "No kits, no season mechanics, old maps."
+        },
+        ["all_same_kit"] = {
+            ["subtitle"] = "SQUADS",
+            ["title"] = "ALL SAME KIT",
+            ["npcDisplay"] = {
+                ["title"] = "⚔️ ALL SAME KIT",
+                ["subtitle"] = [[<font color="rgb(85,255,85)">SQUADS</font>]]
+            },
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(0, 86, 165),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}}},
+                ["backgroundImage"] = "rbxassetid://6850538161",
+                ["backgroundColor"] = Color3.fromRGB(69, 182, 235),
+                ["icon"] = "rbxassetid://6850538161",
+                ["displayType"] = "square"
+            },
+            ["queues"] = {{["queueType"] = "all_same_kit_to4", ["title"] = "ONE FOR ALL"}},
+            ["description"] = [[🤝 Everyone plays the same kit\n🌟 The kit rotates every hour]]
+        },
+        ["jugg-v2"] = {
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(255, 0, 0),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}}},
+                ["backgroundImage"] = "rbxassetid://7051149237",
+                ["backgroundColor"] = Color3.fromRGB(255, 186, 9),
+                ["icon"] = "rbxassetid://9166207628",
+                ["displayType"] = "square"
+            },
+            ["subtitle"] = "RAGE",
+            ["title"] = "JUGGERNAUT",
+            ["queues"] = {{["queueType"] = "juggernaut_v2", ["title"] = "RAGE"}}
+        },
+        ["sky_wars"] = {
+            ["menuDisplay"] = {
+                ["layoutOrder"] = 2,
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(108, 249, 43),
+                ["backgroundImage"] = "rbxassetid://11108575630",
+                ["backgroundColor"] = Color3.fromRGB(89, 150, 154),
+                ["icon"] = "rbxassetid://11108575630",
+                ["displayType"] = "square"
+            },
+            ["subtitle"] = "Teams of 2 ",
+            ["title"] = "SKY WARS",
+            ["queues"] = {
+                {["queueType"] = "skywars_to4", ["title"] = "Squads"},
+                {["queueType"] = "skywars_to2", ["title"] = "Doubles"}
+            }
+        },
+        ["block_hunt"] = {
+            ["subtitle"] = "HIDE AND SEEK",
+            ["title"] = "BLOCK HUNT",
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(255, 248, 88),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}}},
+                ["backgroundImage"] = "rbxassetid://14983595388",
+                ["backgroundColor"] = Color3.fromRGB(236, 70, 48),
+                ["icon"] = "rbxassetid://14983595388",
+                ["displayType"] = "square"
+            },
+            ["queues"] = {{["queueType"] = "block_hunt", ["title"] = "BLOCK HUNT"}},
+            ["description"] = [[🧐 Blend in as a block or hunt down hidden players!\n⏲️ Survive until the end of the game or eliminate all hiders to win!]]
+        },
+        ["skywars_royale"] = {
+            ["subtitle"] = "TRIOS",
+            ["title"] = "SW ROYALE ",
+            ["npcDisplay"] = {
+                ["title"] = "⏰ SKYWARS ROYALE",
+                ["subtitle"] = [[<font color="rgb(85,255,85)">TRIOS</font>]]
+            },
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(0, 86, 165),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}}},
+                ["backgroundImage"] = "rbxassetid://11108575630",
+                ["backgroundColor"] = Color3.fromRGB(69, 182, 235),
+                ["icon"] = "rbxassetid://11108575630",
+                ["displayType"] = "square"
+            },
+            ["queues"] = {{["queueType"] = "skywars_royale", ["title"] = "SKYWARS ROYALE"}},
+            ["description"] = [[🎮 72 players. Teams of 3.\n🏆 Win this Skywars variant to unlock the Royale badge!]]
+        },
+        ["murderer"] = {
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(249, 194, 43),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}}},
+                ["backgroundImage"] = "rbxassetid://12738559407",
+                ["backgroundColor"] = Color3.fromRGB(154, 89, 89),
+                ["icon"] = "rbxassetid://12738559407",
+                ["displayType"] = "square"
+            },
+            ["subtitle"] = "MURDER",
+            ["title"] = "Murder in Bedtown",
+            ["queues"] = {
+                {["queueType"] = "murder_game", ["title"] = "MURDER"},
+                {["queueType"] = "murder_game_vc", ["title"] = "VOICE CHAT ONLY"}
+            }
+        },
+        ["bedwars_classic"] = {
+            ["queues"] = {
+                {["queueType"] = "bedwars_to4", ["title"] = "Squads"},
+                {["queueType"] = "bedwars_to2", ["title"] = "Doubles"},
+                {["queueType"] = "bedwars_to1", ["title"] = "Solos"},
+                {["queueType"] = "bedwars_5v5", ["title"] = "5v5"},
+                {["queueType"] = "bedwars_20v20", ["title"] = "30v30"},
+                {["queueType"] = "bedwars_voice_chat", ["title"] = "Voice Chat "}
+            },
+            ["title"] = "CLASSIC",
+            ["npcDisplay"] = {
+                ["title"] = "⚔️ BedWars Classic",
+                ["subtitle"] = [[<font color="rgb(85,255,85)">POPULAR!!</font>]]
+            },
+            ["menuDisplay"] = {
+                ["helperIcons"] = {["text"] = {{"🔥", "Popular"}}},
+                ["backgroundImage"] = "rbxassetid://11104064080",
+                ["backgroundColor"] = Color3.fromRGB(154, 89, 89),
+                ["icon"] = "rbxassetid://11104064080",
+                ["displayType"] = "full"
+            },
+            ["description"] = [[🛌 Protect your base. Once it's gone, you can no longer respawn!\n\n💎 Gather resources to purchase items and team upgrades\n\n🏹 Destroy enemy beds and eliminate players to win the game!\n\n]]
+        },
+        ["duels"] = {
+            ["menuDisplay"] = {
+                ["instantQueue"] = true,
+                ["gradientColor"] = Color3.fromRGB(249, 43, 68),
+                ["helperIcons"] = {["text"] = {{"⏰", "Limited Time Mode"}}},
+                ["backgroundImage"] = "rbxassetid://9155467645",
+                ["backgroundColor"] = Color3.fromRGB(61, 61, 61),
+                ["icon"] = "rbxassetid://9155467645",
+                ["displayType"] = "square"
+            },
+            ["subtitle"] = "2 v 2 ",
+            ["title"] = "DUELS",
+            ["queues"] = {{["queueType"] = "bedwars_duels", ["title"] = "2v2"}}
+        }
+    }
+}
