793c0a14aa4e2f1fda4fed0905c148eba77cde57
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-02 16:51:28 +1100

    Create win-effect-meta.lua

---
+++ GameData/BedWars [6872265039]/win-effect-meta.lua
@@ -0,0 +1,7 @@
+data = {
+    ["WinEffectMeta"] = {
+        ["train"] = {["name"] = "Choo Choo Champion", ["image"] = "rbxassetid://15716142989"},
+        ["juggernaut_barbarian"] = {["name"] = "Juggernaut Barbarian", ["image"] = "rbxassetid://14559884081"},
+        ["none"] = {["name"] = "None"}
+    }
+}
