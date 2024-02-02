56beff52bf1562a485bddefab463cc25076f606b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-02 17:12:14 +1100

    Create easter-2023.lua

---
+++ GameData/BedWars [6872265039]/easter-2023.lua
@@ -0,0 +1,206 @@
+data = {
+    ["easter-2023-eggs"] = {
+        ["EasterEgg"] = {
+            ["SKY_EGG"] = "sky_egg",
+            ["ENCHANT_EGG"] = "enchant_egg",
+            ["HALLOWEEN_EGG"] = "halloween_egg",
+            ["LARGE_EGG"] = "large_egg",
+            ["DIAMOND_EGG"] = "diamond_egg",
+            ["TESLA_EGG"] = "tesla_egg",
+            ["VOID_EGG"] = "void_egg",
+            ["TITAN_EGG"] = "titan_egg",
+            ["LOBBY_EGG"] = "lobby_egg",
+            ["SNOWBALL_EGG"] = "snowball_egg",
+            ["BED_EGG"] = "bed_egg",
+            ["EMERALD_EGG"] = "emerald_egg",
+            ["BETRAYAL_EGG"] = "betrayal_egg",
+            ["EASY_GAMES_EGG"] = "easy_games_egg",
+            ["HONK_EGG"] = "honk_egg",
+            ["SWORD_EGG"] = "sword_egg",
+            ["GG_EGG"] = "gg_egg",
+            ["LUCKYBLOCK_EGG"] = "luckyblock_egg"
+        },
+        ["EasterEggMeta"] = {
+            ["gg_egg"] = {["order"] = 14, ["image"] = "rbxassetid://13031415107", ["name"] = "GG Egg", ["rarity"] = 3},
+            ["betrayal_egg"] = {
+                ["order"] = 16,
+                ["image"] = "rbxassetid://13031416198",
+                ["name"] = "Betrayal Egg",
+                ["rarity"] = 4
+            },
+            ["honk_egg"] = {
+                ["order"] = 13,
+                ["image"] = "rbxassetid://13031414787",
+                ["name"] = "Honk Egg",
+                ["rarity"] = 3
+            },
+            ["enchant_egg"] = {
+                ["order"] = 3,
+                ["image"] = "rbxassetid://13031415263",
+                ["name"] = "Enchant Egg",
+                ["rarity"] = 0
+            },
+            ["titan_egg"] = {
+                ["order"] = 7,
+                ["image"] = "rbxassetid://13031412799",
+                ["name"] = "Titan Egg",
+                ["rarity"] = 2
+            },
+            ["sky_egg"] = {["order"] = 12, ["image"] = "rbxassetid://13031413739", ["name"] = "Sky Egg", ["rarity"] = 3},
+            ["luckyblock_egg"] = {
+                ["order"] = 10,
+                ["image"] = "rbxassetid://13031414038",
+                ["name"] = "Lucky Block Egg",
+                ["rarity"] = 2
+            },
+            ["halloween_egg"] = {
+                ["order"] = 17,
+                ["image"] = "rbxassetid://13031414958",
+                ["name"] = "Halloween Egg",
+                ["rarity"] = 4
+            },
+            ["large_egg"] = {
+                ["order"] = 8,
+                ["image"] = "rbxassetid://13031414534",
+                ["name"] = "Large Egg",
+                ["rarity"] = 2
+            },
+            ["diamond_egg"] = {
+                ["order"] = 1,
+                ["image"] = "rbxassetid://13031415937",
+                ["name"] = "Diamond Egg",
+                ["rarity"] = 0
+            },
+            ["easy_games_egg"] = {
+                ["order"] = 18,
+                ["image"] = "rbxassetid://13031415708",
+                ["name"] = "Easy Games Egg",
+                ["rarity"] = 4
+            },
+            ["lobby_egg"] = {
+                ["order"] = 11,
+                ["image"] = "rbxassetid://13031414319",
+                ["name"] = "Lobby Egg",
+                ["rarity"] = 2
+            },
+            ["tesla_egg"] = {
+                ["order"] = 9,
+                ["image"] = "rbxassetid://13031413120",
+                ["name"] = "Tesla Egg",
+                ["rarity"] = 3
+            },
+            ["sword_egg"] = {
+                ["order"] = 4,
+                ["image"] = "rbxassetid://13031413351",
+                ["name"] = "Sword Egg",
+                ["rarity"] = 0
+            },
+            ["emerald_egg"] = {
+                ["order"] = 2,
+                ["image"] = "rbxassetid://13031415391",
+                ["name"] = "Emerald Egg",
+                ["rarity"] = 0
+            },
+            ["bed_egg"] = {["order"] = 5, ["image"] = "rbxassetid://13031416379", ["name"] = "Bed Egg", ["rarity"] = 1},
+            ["snowball_egg"] = {
+                ["order"] = 15,
+                ["image"] = "rbxassetid://13031413528",
+                ["name"] = "Snow Egg",
+                ["rarity"] = 4
+            },
+            ["void_egg"] = {
+                ["order"] = 6,
+                ["image"] = "rbxassetid://13031412419",
+                ["name"] = "Void Egg",
+                ["rarity"] = 1
+            }
+        },
+        ["EasterEggRarity"] = {["EASY_MYSTERY"] = 3, ["MEDIUM"] = 1, ["EASY"] = 0, ["HARD"] = 2, ["HARD_MYSTERY"] = 4}
+    },
+    ["easter-2023-milestones"] = {
+        ["Easter2023Reward"] = {
+            ["EASTER_TITLE_2"] = 6,
+            ["EASTER_EMOTE"] = 0,
+            ["SHOP_ITEM"] = 5,
+            ["EASTER_EMOTE_2"] = 3,
+            ["BADGE"] = 4,
+            ["EASTER_KIT_SKIN"] = 2,
+            ["EASTER_TITLE_1"] = 1
+        },
+        ["Easter2023MilestoneMeta"] = {
+            ["milestone1"] = {
+                ["milestoneColor"] = Color3.fromRGB(59, 227, 94),
+                ["requiredText"] = {["postText"] = " EGGS"},
+                ["rewards"] = {1},
+                ["numRequired"] = 4
+            },
+            ["milestone3"] = {
+                ["milestoneColor"] = Color3.fromRGB(102, 36, 224),
+                ["requiredText"] = {["postText"] = " EGGS"},
+                ["rewards"] = {3},
+                ["numRequired"] = 12
+            },
+            ["milestone5"] = {
+                ["milestoneColor"] = Color3.fromRGB(255, 59, 59),
+                ["requiredText"] = {["postText"] = " EGGS"},
+                ["rewards"] = {6, 5},
+                ["numRequired"] = 18
+            },
+            ["milestone2"] = {
+                ["milestoneColor"] = Color3.fromRGB(95, 169, 255),
+                ["requiredText"] = {["postText"] = " EGGS"},
+                ["rewards"] = {0},
+                ["numRequired"] = 8
+            },
+            ["milestone4"] = {
+                ["milestoneColor"] = Color3.fromRGB(235, 191, 87),
+                ["requiredText"] = {["postText"] = " EGGS"},
+                ["rewards"] = {4, 2},
+                ["numRequired"] = 16
+            }
+        },
+        ["Easter2023RewardMeta"] = {
+            {
+                ["name"] = "🥚 Egg Hunt 2023 🐇",
+                ["display"] = {
+                    ["rewardTypeText"] = "Title",
+                    ["text"] = [[<stroke color="rgb(36,36,36)" thickness="2"><font>Egg Hunt 2023</font></stroke>]]
+                }
+            },
+            {
+                ["name"] = "Exeggutor Hannah",
+                ["display"] = {["rewardTypeText"] = "Kit Skin", ["image"] = "rbxassetid://13034479411"}
+            },
+            {
+                ["name"] = "Easter Amy Murderous",
+                ["display"] = {["rewardTypeText"] = "Emote", ["image"] = "rbxassetid://13030624582"}
+            },
+            {
+                ["name"] = "Egg Hunt 2023 Badge",
+                ["display"] = {["rewardTypeText"] = "Badge", ["image"] = "rbxassetid://13031413351"}
+            },
+            {
+                ["name"] = "Egg Launcher",
+                ["display"] = {["rewardTypeText"] = "Shop Item Unlock", ["image"] = "rbxassetid://13033176844"}
+            },
+            {
+                ["name"] = "EGGCELLENT",
+                ["display"] = {
+                    ["rewardTypeText"] = "Title",
+                    ["text"] = [[<stroke color="rgb(255,255,255)" thickness="3"><font color="rgb(255,184,82)">🥚 EGG-CELLENT</font></stroke>]]
+                }
+            },
+            {
+                ["name"] = "Easter Star Collector Choco Bunny",
+                ["display"] = {["rewardTypeText"] = "Emote", ["image"] = "rbxassetid://13030627055"}
+            }
+        }
+    },
+    ["easter-2023-egg-util"] = {
+        ["EasterEgg2023Util"] = {
+            ["TOTAL_EGGS"] = 18,
+            ["getNumEggsFound"] = "function: 0x00000000d827f5bb",
+            ["collectedEmeraldEgg"] = "function: 0x0000000015a34253"
+        }
+    }
+}
