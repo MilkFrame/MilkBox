654aac5e1bcf673648cfa937af5f2c8b8fe67bd2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-02 17:06:35 +1100

    Create recipe-meta.lua

---
+++ GameData/BedWars [6872265039]/recipe-meta.lua
@@ -0,0 +1,30 @@
+data = {
+    ["recipes"] = {
+        ["poison_splash_potion"] = {
+            ["timeToCraft"] = 5,
+            ["ingredients"] = {"thorns", "mushrooms", "mushrooms"},
+            ["result"] = "poison_splash_potion"
+        },
+        ["heal_splash_potion"] = {
+            ["timeToCraft"] = 5,
+            ["ingredients"] = {"wild_flower", "wild_flower", "wild_flower"},
+            ["result"] = "heal_splash_potion"
+        },
+        ["sleep_splash_potion"] = {
+            ["timeToCraft"] = 4,
+            ["ingredients"] = {"mushrooms", "mushrooms", "mushrooms"},
+            ["result"] = "sleep_splash_potion"
+        },
+        ["time_bomb_potion"] = {
+            ["timeToCraft"] = 3,
+            ["ingredients"] = {"emerald_block", "emerald_block", "emerald_block"},
+            ["result"] = "time_bomb_potion"
+        },
+        ["big_shield"] = {
+            ["timeToCraft"] = 7,
+            ["ingredients"] = {"thorns", "thorns", "wild_flower"},
+            ["result"] = "big_shield"
+        }
+    },
+    ["getRecipeMeta"] = "function: 0x0000000085c30f4b"
+}
