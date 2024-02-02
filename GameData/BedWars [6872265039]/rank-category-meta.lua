5db02a07a870b78a0bfae8a727d749b05b385fbc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-02 17:05:16 +1100

    Create rank-category-meta.lua

---
+++ GameData/BedWars [6872265039]/rank-category-meta.lua
@@ -0,0 +1,11 @@
+data = {
+    ["RankCategoryMeta"] = {
+        ["RANKED_BETA"] = {["queueType"] = "bedwars_ranked_beta", ["leaderboard"] = "RankPoints"},
+        ["RANKED_S4"] = {["queueType"] = "bedwars_ranked_s4", ["leaderboard"] = "RankPoints"},
+        ["RANKED_S5"] = {["queueType"] = "bedwars_ranked_s5", ["leaderboard"] = "RankPoints_S5"},
+        ["RANKED_S6"] = {["queueType"] = "bedwars_ranked_s6", ["leaderboard"] = "RankPoints_S6_2"},
+        ["RANKED_S7"] = {["queueType"] = "bedwars_ranked_s7", ["leaderboard"] = "RankPoints_S7"}
+        ["RANKED_S8"] = {["queueType"] = "bedwars_ranked_s8", ["leaderboard"] = "RankPoints_S8"},
+        ["RANKED_S9"] = {["queueType"] = "bedwars_ranked_s9", ["leaderboard"] = "RankPoints_S9"},
+    }
+}
