df8e51c49b6373643ed57b0c9a3cd73b20d77817
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-02 17:03:46 +1100

    Create mount-meta.lua

---
+++ GameData/BedWars [6872265039]/mount-meta.lua
@@ -0,0 +1,24 @@
+data = {
+    ["MountMeta"] = {
+        ["dino"] = {
+            ["animations"] = {["walk"] = 170, ["fall"] = 171, ["idle"] = 175, ["jump"] = 172},
+            ["displayName"] = "Dino",
+            ["controlBoneCallback"] = "function: 0x00000000068aede3",
+            ["model"] = "dino"
+        },
+        ["dodo_bird"] = {
+            ["animations"] = {["walk"] = 44, ["fall"] = 46, ["idle"] = 43, ["jump"] = 47},
+            ["displayName"] = "Dodo Bird",
+            ["controlBoneCallback"] = "function: 0x000000007649685b",
+            ["model"] = "dodo_bird"
+        },
+        ["elk"] = {
+            ["playerMountOffset"] = Vector3.new(0, -1.75, 1),
+            ["model"] = "elk",
+            ["animations"] = {["walk"] = 176, ["fall"] = 177, ["idle"] = 179, ["jump"] = 178},
+            ["mountHipYOffset"] = 2,
+            ["controlBoneCallback"] = "function: 0x00000000f17f816b",
+            ["displayName"] = "Elk"
+        }
+    }
+}
