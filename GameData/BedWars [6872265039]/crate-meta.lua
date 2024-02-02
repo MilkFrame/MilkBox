bfdd988058d2ecde4d1e130f907240ccf10751f3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-02 17:06:56 +1100

    Create crate-meta.lua

---
+++ GameData/BedWars [6872265039]/crate-meta.lua
@@ -0,0 +1,115 @@
+data = {
+    ["CrateMeta"] = {
+        ["murder_crate"] = {
+            ["items"] = {
+                {["rarity"] = 1, ["itemType"] = "sleep_splash_potion", ["amount"] = 1},
+                {["rarity"] = 1, ["itemType"] = "pie", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "wood_sword", ["amount"] = 1},
+                {
+                    ["extraItems"] = {{["itemType"] = "arrow", ["amount"] = 8}},
+                    ["rarity"] = 2,
+                    ["itemType"] = "wood_bow",
+                    ["amount"] = 1
+                }
+            },
+            ["image"] = "",
+            ["model"] = "RewardCrate",
+            ["raritySettings"] = {
+                {["rarity"] = 0, ["value"] = 0.38},
+                {["rarity"] = 1, ["value"] = 0.35},
+                {["rarity"] = 2, ["value"] = 0.2},
+                {["rarity"] = 3, ["value"] = 0.05},
+                {["rarity"] = 4, ["value"] = 0.02}
+            },
+            ["displayName"] = "Mystery Crate"
+        },
+        ["diamond_lucky_crate"] = {
+            ["consumableRequirement"] = "diamond_lucky_crate",
+            ["image"] = "rbxassetid://15883494822",
+            ["model"] = "DiamondLuckyCrate",
+            ["raritySettings"] = {
+                {["rarity"] = 0, ["value"] = 0.2},
+                {["rarity"] = 1, ["value"] = 0.275},
+                {["rarity"] = 2, ["value"] = 0.35},
+                {["rarity"] = 3, ["value"] = 0.15},
+                {["rarity"] = 4, ["value"] = 0.025}
+            },
+            ["items"] = {
+                {["rarity"] = 0, ["itemType"] = "yuzi_brb", ["amount"] = 1},
+                {["rarity"] = 0, ["itemType"] = "hats_off", ["amount"] = 1},
+                {["rarity"] = 0, ["itemType"] = "hmm_ok", ["amount"] = 1},
+                {["rarity"] = 0, ["itemType"] = "beekeeper_gift", ["amount"] = 1},
+                {["rarity"] = 0, ["itemType"] = "kaliyah_pumped_up", ["amount"] = 1},
+                {["rarity"] = 1, ["itemType"] = "alchemist_evil", ["amount"] = 1},
+                {["rarity"] = 1, ["itemType"] = "conqueror_fireball", ["amount"] = 1},
+                {["rarity"] = 1, ["itemType"] = "all_good", ["amount"] = 1},
+                {["rarity"] = 1, ["itemType"] = "bomb_emoji", ["amount"] = 1},
+                {["rarity"] = 1, ["itemType"] = "destroyer", ["amount"] = 1},
+                {["rarity"] = 4, ["itemType"] = "legend", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "flying_backpack", ["amount"] = 1},
+                {["rarity"] = 3, ["itemType"] = "mythic_gauntlets", ["amount"] = 1},
+                {["rarity"] = 4, ["itemType"] = "dino_mount", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "kit_rental_fighter_7", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "kit_rental_defender_7", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "kit_rental_economy_7", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "kit_rental_movement_7", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "kit_rental_support_7", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "kit_rental_tank_7", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "kit_rental_ranged_7", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "kit_rental_destroyer_7", ["amount"] = 1},
+                {["rarity"] = 3, ["itemType"] = "kit_rental_limited_7", ["amount"] = 1},
+                {["rarity"] = 4, ["itemType"] = "kit_rental_all_7", ["amount"] = 1},
+                {["rarity"] = 0, ["itemType"] = "battle_pass_1_5x", ["amount"] = 1},
+                {["rarity"] = 1, ["itemType"] = "battle_pass_2x", ["amount"] = 1},
+                {["rarity"] = 3, ["itemType"] = "battle_pass_3x", ["amount"] = 1},
+                {["rarity"] = 1, ["itemType"] = "auto_complete_daily_mission_ticket", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "auto_complete_weekly_mission_ticket", ["amount"] = 1}
+            },
+            ["displayName"] = "Diamond Lucky Crate"
+        },
+        ["level_up_crate"] = {
+            ["consumableRequirement"] = "level_up_crate",
+            ["image"] = "rbxassetid://12500112438",
+            ["model"] = "RewardCrate",
+            ["raritySettings"] = {
+                {["rarity"] = 0, ["value"] = 0.38},
+                {["rarity"] = 1, ["value"] = 0.35},
+                {["rarity"] = 2, ["value"] = 0.2},
+                {["rarity"] = 3, ["value"] = 0.065},
+                {["rarity"] = 4, ["value"] = 0.005}
+            },
+            ["items"] = {
+                {["rarity"] = 0, ["itemType"] = "yuzi_brb", ["amount"] = 1},
+                {["rarity"] = 0, ["itemType"] = "hats_off", ["amount"] = 1},
+                {["rarity"] = 0, ["itemType"] = "hmm_ok", ["amount"] = 1},
+                {["rarity"] = 0, ["itemType"] = "beekeeper_gift", ["amount"] = 1},
+                {["rarity"] = 0, ["itemType"] = "kaliyah_pumped_up", ["amount"] = 1},
+                {["rarity"] = 1, ["itemType"] = "alchemist_evil", ["amount"] = 1},
+                {["rarity"] = 1, ["itemType"] = "conqueror_fireball", ["amount"] = 1},
+                {["rarity"] = 1, ["itemType"] = "all_good", ["amount"] = 1},
+                {["rarity"] = 1, ["itemType"] = "bomb_emoji", ["amount"] = 1},
+                {["rarity"] = 1, ["itemType"] = "destroyer", ["amount"] = 1},
+                {["rarity"] = 4, ["itemType"] = "legend", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "flying_backpack", ["amount"] = 1},
+                {["rarity"] = 3, ["itemType"] = "mythic_gauntlets", ["amount"] = 1},
+                {["rarity"] = 4, ["itemType"] = "dino_mount", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "kit_rental_fighter_7", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "kit_rental_defender_7", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "kit_rental_economy_7", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "kit_rental_movement_7", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "kit_rental_support_7", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "kit_rental_tank_7", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "kit_rental_ranged_7", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "kit_rental_destroyer_7", ["amount"] = 1},
+                {["rarity"] = 3, ["itemType"] = "kit_rental_limited_7", ["amount"] = 1},
+                {["rarity"] = 4, ["itemType"] = "kit_rental_all_7", ["amount"] = 1},
+                {["rarity"] = 0, ["itemType"] = "battle_pass_1_5x", ["amount"] = 1},
+                {["rarity"] = 1, ["itemType"] = "battle_pass_2x", ["amount"] = 1},
+                {["rarity"] = 3, ["itemType"] = "battle_pass_3x", ["amount"] = 1},
+                {["rarity"] = 1, ["itemType"] = "auto_complete_daily_mission_ticket", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "auto_complete_weekly_mission_ticket", ["amount"] = 1}
+            },
+            ["displayName"] = "Gold Lucky Crate"
+        }
+    }
+}
