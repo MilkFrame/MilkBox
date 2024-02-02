138283acea5c11bf446ff53ea4c7cf0ca11bbf80
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-02 16:46:59 +1100

    Create leaderboard-meta.lua

---
+++ GameData/BedWars [6872265039]/leaderboard-meta.lua
@@ -0,0 +1,23 @@
+data = {
+    ["LEADERBOARD_DISABLED"] = false,
+    ["LEADERBOARD_DEFAULT_BOARD"] = "RankPoints_S9",
+    ["LEADERBOARD_REFRESH_INTERVAL"] = 300,
+    ["LeaderboardMeta"] = {
+        ["RankPoints_S9"] = {
+            ["description"] = "Players with the highest Rank Points",
+            ["statName"] = "rank",
+            ["metric"] = "RP",
+            ["aggregationType"] = "last",
+            ["maxUsersDisplayed"] = 200,
+            ["displayName"] = "Ranked Season 9"
+        },
+        ["Wins"] = {
+            ["description"] = "Players with the highest number of wins this month",
+            ["statName"] = "wins",
+            ["metric"] = "wins",
+            ["aggregationType"] = "sum",
+            ["maxUsersDisplayed"] = 100,
+            ["displayName"] = "Monthly Wins"
+        }
+    }
+}
