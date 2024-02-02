b27011b8c8f06d6ed99826f84243a1febb7c8b8d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-02 17:03:15 +1100

    Create match-level-meta.lua

---
+++ GameData/BedWars [6872265039]/match-level-meta.lua
@@ -0,0 +1,75 @@
+data = {
+    ["getMatchLevelMeta"] = "function: 0x00000000ddbabcc3",
+    ["MatchExperienceFactors"] = {
+        ["INCREASE_PER_LEVEL_BELOW"] = 0.2,
+        ["REDUCTION_PER_LEVEL_ABOVE"] = {0, 0.16, 0.32, 0.48, 0.6}
+    },
+    ["MatchLevelMeta"] = {
+        {
+            ["forgePointsAwarded"] = 2,
+            ["experienceRequired"] = 100,
+            ["experienceAwardedForAssist"] = 105,
+            ["experienceAwardedForKill"] = 215
+        },
+        {
+            ["forgePointsAwarded"] = 3,
+            ["experienceRequired"] = 200,
+            ["experienceAwardedForAssist"] = 174.79999999999998,
+            ["experienceAwardedForKill"] = 262.2
+        },
+        {
+            ["forgePointsAwarded"] = 3,
+            ["experienceRequired"] = 300,
+            ["experienceAwardedForAssist"] = 285.2,
+            ["experienceAwardedForKill"] = 331.2
+        },
+        {
+            ["forgePointsAwarded"] = 4,
+            ["experienceRequired"] = 400,
+            ["experienceAwardedForAssist"] = 361.2,
+            ["experienceAwardedForKill"] = 365.40000000000003
+        },
+        {
+            ["forgePointsAwarded"] = 4,
+            ["experienceRequired"] = 500,
+            ["experienceAwardedForAssist"] = 418,
+            ["experienceAwardedForKill"] = 387.59999999999997
+        },
+        {
+            ["forgePointsAwarded"] = 5,
+            ["experienceRequired"] = 600,
+            ["experienceAwardedForAssist"] = 455.59999999999997,
+            ["experienceAwardedForKill"] = 397.8
+        },
+        {
+            ["forgePointsAwarded"] = 5,
+            ["experienceRequired"] = 700,
+            ["experienceAwardedForAssist"] = 605.1999999999999,
+            ["experienceAwardedForKill"] = 523.6
+        },
+        {
+            ["forgePointsAwarded"] = 6,
+            ["experienceRequired"] = 800,
+            ["experienceAwardedForAssist"] = 615,
+            ["experienceAwardedForKill"] = 588
+        },
+        {
+            ["forgePointsAwarded"] = 6,
+            ["experienceRequired"] = 900,
+            ["experienceAwardedForAssist"] = 772.5,
+            ["experienceAwardedForKill"] = 729
+        },
+        {
+            ["forgePointsAwarded"] = 8,
+            ["experienceRequired"] = 1000,
+            ["experienceAwardedForAssist"] = 885,
+            ["experienceAwardedForKill"] = 885
+        },
+        {
+            ["forgePointsAwarded"] = 0,
+            ["experienceRequired"] = 0,
+            ["experienceAwardedForAssist"] = 70,
+            ["experienceAwardedForKill"] = 105
+        }
+    }
+}
