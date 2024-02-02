cbbfe28f09b5fc10c29a741d6fbf24c7aefa6fa1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-02 16:49:15 +1100

    Create lobby-gadget-meta.lua

---
+++ GameData/BedWars [6872265039]/lobby-gadget-meta.lua
@@ -0,0 +1,204 @@
+data = {
+    ["LobbyGadgetMeta"] = {
+        ["flying_backpack"] = {
+            ["items"] = {{["itemType"] = "flying_backpack", ["amount"] = 1}},
+            ["image"] = "rbxassetid://13630754419",
+            ["name"] = "Flying Backpack"
+        },
+        ["snow_ball"] = {
+            ["items"] = {{["refillable"] = true, ["itemType"] = "snowball", ["amount"] = 3}},
+            ["battlepassSeason"] = "season3",
+            ["name"] = "Snow Ball",
+            ["unlockedInBattlePass"] = true
+        },
+        ["cutlass_ghost"] = {
+            ["items"] = {{["itemType"] = "cutlass_ghost", ["amount"] = 1}},
+            ["image"] = "rbxassetid://10729541018",
+            ["name"] = "Ghost Cutlass"
+        },
+        ["mythic_great_hammer"] = {
+            ["items"] = {{["itemType"] = "mythic_great_hammer", ["amount"] = 1}},
+            ["image"] = "rbxassetid://13832631998",
+            ["name"] = "Paragon"
+        },
+        ["toy_hammer"] = {["name"] = "Toy Hammer", ["items"] = {{["itemType"] = "toy_hammer", ["amount"] = 1}}},
+        ["jump_boots"] = {
+            ["items"] = {{["itemType"] = "jump_boots"}},
+            ["battlepassSeason"] = "season3",
+            ["name"] = "Jump Boots",
+            ["unlockedInBattlePass"] = true
+        },
+        ["mythic_dagger"] = {
+            ["items"] = {{["itemType"] = "mythic_dagger", ["amount"] = 1}},
+            ["image"] = "rbxassetid://13832903272",
+            ["name"] = "Deathbloom"
+        },
+        ["tennis_racket"] = {
+            ["items"] = {
+                {["itemType"] = "tennis_racket", ["amount"] = 1},
+                {["refillCooldown"] = 8, ["refillable"] = true, ["itemType"] = "tennis_ball", ["amount"] = 4}
+            },
+            ["image"] = "rbxassetid://10392204924",
+            ["name"] = "Tennis Racket"
+        },
+        ["grappling_hook"] = {
+            ["name"] = "Grappling Hook",
+            ["items"] = {{["itemType"] = "grappling_hook", ["amount"] = 1}}
+        },
+        ["raven"] = {
+            ["items"] = {{["refillable"] = true, ["itemType"] = "raven", ["amount"] = 1}},
+            ["battlepassSeason"] = "season4",
+            ["name"] = "Raven",
+            ["unlockedInBattlePass"] = true
+        },
+        ["laser_sword"] = {
+            ["items"] = {{["itemType"] = "laser_sword", ["amount"] = 1}},
+            ["image"] = "rbxassetid://11775182286",
+            ["name"] = "Laser Sword"
+        },
+        ["necromancer_staff"] = {
+            ["items"] = {{["itemType"] = "necromancer_staff", ["amount"] = 1}},
+            ["image"] = "rbxassetid://11350214469",
+            ["name"] = "Necromancer Staff"
+        },
+        ["rainbow_staff"] = {
+            ["items"] = {{["itemType"] = "rainbow_staff", ["amount"] = 1}},
+            ["image"] = "rbxassetid://12813669578",
+            ["name"] = "Rainbow Staff"
+        },
+        ["mythic_gauntlets"] = {
+            ["items"] = {{["itemType"] = "mythic_gauntlets", ["amount"] = 1}},
+            ["image"] = "rbxassetid://14839096268",
+            ["name"] = "Warfists"
+        },
+        ["elk_mount"] = {["mount"] = "elk", ["image"] = "rbxassetid://15716032069", ["name"] = "Elk Mount"},
+        ["void_sword"] = {["name"] = "Void Sword", ["items"] = {{["itemType"] = "void_sword", ["amount"] = 1}}},
+        ["dodo_bird"] = {["mount"] = "dodo_bird", ["image"] = "rbxassetid://11149949979", ["name"] = "Dodo Bird"},
+        ["none"] = {["name"] = "None"},
+        ["jade_hammer"] = {
+            ["items"] = {{["itemType"] = "jade_hammer"}},
+            ["battlepassSeason"] = "season4",
+            ["name"] = "Jade Hammer",
+            ["unlockedInBattlePass"] = true
+        },
+        ["FIREWORK_ROCKET_LAUNCHER"] = {
+            ["items"] = {
+                {["itemType"] = "firework_rocket_launcher", ["amount"] = 1},
+                {
+                    ["refillCooldown"] = 8,
+                    ["refillable"] = true,
+                    ["itemType"] = "firework_rocket_missile",
+                    ["amount"] = 2
+                }
+            },
+            ["image"] = "rbxassetid://15798141956",
+            ["name"] = "Firework Rocket Launcher"
+        },
+        ["snowball_launcher"] = {
+            ["items"] = {
+                {["itemType"] = "snowball_launcher", ["amount"] = 1},
+                {["refillCooldown"] = 8, ["refillable"] = true, ["itemType"] = "snowball", ["amount"] = 4}
+            },
+            ["image"] = "rbxassetid://15628201582",
+            ["name"] = "Snowball Launcher"
+        },
+        ["hunters_echo"] = {
+            ["items"] = {
+                {["refillCooldown"] = 10, ["refillable"] = true, ["itemType"] = "hunters_echo", ["amount"] = 1}
+            },
+            ["image"] = "rbxassetid://14978481226",
+            ["name"] = "Hunter's Echo"
+        },
+        ["blunderbuss"] = {
+            ["items"] = {{["itemType"] = "blunderbuss", ["amount"] = 1}},
+            ["image"] = "rbxassetid://10722841562",
+            ["name"] = "Blunderbuss"
+        },
+        ["flying_broom"] = {
+            ["items"] = {{["refillable"] = true, ["itemType"] = "flying_broom_deploy", ["amount"] = 1}},
+            ["image"] = "rbxassetid://15115405598",
+            ["name"] = "Flying Broom"
+        },
+        ["rageblade"] = {
+            ["items"] = {{["itemType"] = "rageblade"}},
+            ["battlepassSeason"] = "season3",
+            ["name"] = "Rageblade",
+            ["unlockedInBattlePass"] = true
+        },
+        ["frosty_hammer"] = {
+            ["items"] = {{["itemType"] = "frosty_hammer", ["amount"] = 1}},
+            ["image"] = "rbxassetid://11831565831",
+            ["name"] = "Frosty Hammer"
+        },
+        ["can_of_beans"] = {
+            ["items"] = {{["refillCooldown"] = 8, ["refillable"] = true, ["itemType"] = "can_of_beans", ["amount"] = 1}},
+            ["image"] = "rbxassetid://13918757728",
+            ["name"] = "Can of Beans"
+        },
+        ["headhunter"] = {
+            ["items"] = {
+                {["itemType"] = "headhunter", ["amount"] = 1},
+                {["refillCooldown"] = 8, ["refillable"] = true, ["itemType"] = "arrow", ["amount"] = 4}
+            },
+            ["image"] = "rbxassetid://13421692306",
+            ["name"] = "Headhunter"
+        },
+        ["grenade_launcher"] = {
+            ["name"] = "Grenade Launcher",
+            ["items"] = {
+                {["itemType"] = "grenade_launcher", ["amount"] = 1},
+                {["refillCooldown"] = 8, ["refillable"] = true, ["itemType"] = "stun_grenade", ["amount"] = 5}
+            }
+        },
+        ["light_sword"] = {["name"] = "Light Sword", ["items"] = {{["itemType"] = "light_sword", ["amount"] = 1}}},
+        ["freiya_ice_sword"] = {
+            ["items"] = {{["itemType"] = "ice_sword"}},
+            ["battlepassSeason"] = "season4",
+            ["name"] = "Freiya Ice Sword",
+            ["unlockedInBattlePass"] = true
+        },
+        ["leather_helmet"] = {
+            ["items"] = {{["itemType"] = "leather_helmet", ["amount"] = 1}},
+            ["image"] = "rbxassetid://6855466216",
+            ["name"] = "Leather Helmet"
+        },
+        ["glitch_guitar"] = {
+            ["items"] = {{["itemType"] = "glitch_guitar", ["amount"] = 1}},
+            ["image"] = "rbxassetid://12509567989",
+            ["name"] = "Glitch Guitar"
+        },
+        ["rocket_belt"] = {["name"] = "Rocket Belt", ["items"] = {{["itemType"] = "rocket_belt", ["amount"] = 1}}},
+        ["scythe"] = {["name"] = "Scythe", ["items"] = {{["itemType"] = "scythe", ["amount"] = 1}}},
+        ["dino_mount"] = {["mount"] = "dino", ["image"] = "rbxassetid://9855535867", ["name"] = "Dino Mount"},
+        ["impulse_gun"] = {
+            ["items"] = {{["itemType"] = "impulse_gun", ["amount"] = 1}},
+            ["image"] = "rbxassetid://13629029360",
+            ["name"] = "Impulse Gun"
+        },
+        ["VOID_AXE"] = {
+            ["items"] = {{["itemType"] = "void_axe", ["amount"] = 1}},
+            ["image"] = "rbxassetid://8322058718",
+            ["name"] = "Void Axe"
+        },
+        ["barbarian_helmet"] = {
+            ["items"] = {{["itemType"] = "barbarian_helmet", ["amount"] = 1}},
+            ["image"] = "rbxassetid://14559460074",
+            ["name"] = "Barbarian Helmet"
+        },
+        ["twirl_blade"] = {["name"] = "Twirl Blade", ["items"] = {{["itemType"] = "twirlblade", ["amount"] = 1}}},
+        ["hang_glider"] = {
+            ["name"] = "Hang Glider",
+            ["items"] = {{["refillable"] = true, ["itemType"] = "hang_glider", ["amount"] = 1}}
+        },
+        ["infernal_saber"] = {
+            ["name"] = "Infernal Saber",
+            ["items"] = {{["itemType"] = "infernal_saber", ["amount"] = 1}}
+        },
+        ["speed_boots"] = {
+            ["items"] = {{["itemType"] = "speed_boots"}},
+            ["battlepassSeason"] = "season3",
+            ["name"] = "Speed Boots",
+            ["unlockedInBattlePass"] = true
+        }
+    }
+}
