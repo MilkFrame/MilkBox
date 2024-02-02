794dbfef5e25521bd055f585036bd248eab7114e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-02 16:34:26 +1100

    Create bundle-meta.lua

---
+++ GameData/BedWars [6872265039]/bundle-meta.lua
@@ -0,0 +1,154 @@
+data = {
+    ["BundleMeta"] = {
+        ["holiyday_bundle_2022"] = {
+            ["limitedTime"] = true,
+            ["image"] = "rbxassetid://12501229731",
+            ["gamepassId"] = 113770382,
+            ["rewards"] = {
+                ["kits"] = {"seahorse", "miner", "bigman"},
+                ["titles"] = {"iced_out", "bing_chilling"},
+                ["lobbyGadgets"] = {"dodo_bird"},
+                ["kitSkins"] = {"miner_winter", "bigman_christmas"},
+                ["killEffects"] = {"ribbon", "iceblock"}
+            },
+            ["price"] = 1499,
+            ["devProductId"] = 1349265434,
+            ["kitShopBundle"] = {
+                ["disabled"] = true,
+                ["image"] = "rbxassetid://11902803137",
+                ["name"] = "Holiday Bundle 2022",
+                ["description"] = [[🐟 Sheila kit (Limited)\n🧊 Winter Miner skin + Miner kit\n🎄 Christmas ElderTree skin + ElderTree kit\n🪢 Wrapped Up kill effect (Limited)\n🧊 Ice Block kill effect (Limited)\n🦤 Dodo bird mount lobby gadget (Limited)\n🏷️ “Iced Out” & “Bing Chilling 🥶🍧” lobby titles (Limited)]]
+            },
+            ["name"] = "Holiday Bundle 2022"
+        },
+        ["holiday_bundle_2023"] = {
+            ["limitedTime"] = true,
+            ["image"] = "rbxassetid://15715067677",
+            ["name"] = "Holiday Bundle 2023",
+            ["rewards"] = {
+                ["kits"] = {"elk_master", "lumen", "ember", "flower_bee"},
+                ["titles"] = {"blizzard", "present_emoji"},
+                ["lobbyGadgets"] = {"elk_mount"},
+                ["kitSkins"] = {"flower_bee_frost_queen", "ember_krampus"},
+                ["winEffects"] = {"train"},
+                ["killEffects"] = {"elk"}
+            },
+            ["price"] = 1499,
+            ["devProductId"] = 1711787818,
+            ["kitShopBundle"] = {
+                ["disabled"] = true,
+                ["image"] = "rbxassetid://15715067677",
+                ["name"] = "Holiday Bundle 2023",
+                ["description"] = [[⛄ Sigrid kit (Limited)\n🔔 Krampus Ember skin + Ember kit + Lumen kit (Not Limited)\n❄️ Frost Queen Lyla skin + Lyla kit (Not Limited)\n💥 Antler Strike kill effect (Limited)\n🚂 Train win effect (Limited)\n🎠 Elk mount lobby gadget (Limited)\n🏷️ “Blizzard” & “🎁” lobby titles (Limited)]]
+            },
+            ["gamepassId"] = 675791713
+        },
+        ["ember_lumen_kit_bundle_perm"] = {
+            ["image"] = "rbxassetid://9630018755",
+            ["name"] = "Ember & Lumen Kit Bundle",
+            ["rewards"] = {["kits"] = {"ember", "lumen"}},
+            ["price"] = 799,
+            ["devProductId"] = 1267394254,
+            ["kitShopBundle"] = {
+                ["image"] = "rbxassetid://9630018755",
+                ["name"] = "Ember & Lumen Kit Bundle",
+                ["description"] = [[This bundle unlocks the following:\n☀️ <b>LUMEN (Kit)</b>\n🔥 <b>EMBER (Kit)</b>]]
+            },
+            ["gamepassId"] = 47400080
+        },
+        ["lunarBundle2022"] = {
+            ["image"] = "rbxassetid://8666432622",
+            ["name"] = "Lunar Bundle 2022",
+            ["price"] = 999,
+            ["devProductId"] = 1235079279,
+            ["rewards"] = {
+                ["titles"] = {"Year of the Tiger", "Spring Festival", "Lunear New Year!"},
+                ["emotes"] = {"lunar_tiger_builder", "vulcan_lantern", "lny_2022", "red_envelope"},
+                ["kitSkins"] = {"dasher_tiger"},
+                ["kits"] = {"dasher"},
+                ["killEffects"] = {"lantern"}
+            },
+            ["gamepassId"] = 27969090
+        },
+        ["ember_lumen_kit_bundle"] = {
+            ["kitShopBundle"] = {
+                ["disabled"] = true,
+                ["image"] = "rbxassetid://9630018755",
+                ["name"] = "Ember & Lumen Kit Bundle",
+                ["description"] = [[Purchase this limited time bundle to unlock the following:\n☀️ <b>LUMEN (Kit)</b>\n🔥 <b>EMBER (Kit)</b>\n- Unlocks limited lobby titles: <font color="rgb(140, 5, 56)"><b>Infernal 🔥</b></font> & <font color="rgb(242, 209, 0)"><b>Light 🔅</b></font>\n- Unlocks limited time lobby gadgets: Infernal Saber & Light Sword\n]]
+            },
+            ["image"] = "rbxassetid://9630018755",
+            ["name"] = "Ember & Lumen Kit Bundle",
+            ["rewards"] = {
+                ["kits"] = {"ember", "lumen"},
+                ["titles"] = {"infernal", "light"},
+                ["lobbyGadgets"] = {"infernal_saber", "light_sword"}
+            },
+            ["price"] = 999,
+            ["devProductId"] = 1263740791,
+            ["limitedTime"] = true,
+            ["gamepassId"] = 44594629
+        },
+        ["holidayBundle2021"] = {
+            ["image"] = "rbxassetid://12501229731",
+            ["gamepassId"] = 26343736,
+            ["price"] = 1499,
+            ["devProductId"] = 1230368615,
+            ["rewards"] = {
+                ["kits"] = {"axolotl", "barbarian", "regent"},
+                ["lobbyGadgets"] = {"hang_glider"},
+                ["kitSkins"] = {"axolotl_reindeer", "barbarian_deep_void"},
+                ["killEffects"] = {"abduction", "coffin"}
+            },
+            ["name"] = "Holiday Bundle 2021"
+        },
+        ["minerBundle"] = {
+            ["image"] = "rbxassetid://8936489137",
+            ["name"] = "Miner Bundle",
+            ["price"] = 899,
+            ["devProductId"] = 1240859867,
+            ["rewards"] = {
+                ["titles"] = {"miners_of_the_galaxy", "pickaxe", "mining_away"},
+                ["kits"] = {"miner"},
+                ["kitSkins"] = {"miner_space"}
+            },
+            ["gamepassId"] = 29973548
+        },
+        ["evelynn_bundle"] = {
+            ["kitShopBundle"] = {
+                ["disabled"] = true,
+                ["image"] = "rbxassetid://10406984466",
+                ["name"] = "Evelynn Bundle",
+                ["description"] = [[This bundle unlocks the following:\n🗡️ <b>Evelynn (Kit)</b>\n🌈 <b>Prismatic Evelynn (Kit Skin)</b>]]
+            },
+            ["image"] = "rbxassetid://10406984466",
+            ["name"] = "Evelynn Bundle",
+            ["rewards"] = {["kits"] = {"spirit_assassin"}, ["kitSkins"] = {"spirit_assassin_prismatic"}},
+            ["price"] = 999,
+            ["devProductId"] = 1291660453,
+            ["limitedTime"] = true,
+            ["gamepassId"] = 67052293
+        },
+        ["hannah_bundle"] = {
+            ["kitShopBundle"] = {
+                ["disabled"] = true,
+                ["image"] = "rbxassetid://10726577232",
+                ["name"] = "Hannah Bundle",
+                ["description"] = [[This bundle unlocks the following:\n🏴‍☠️ <b>Hannah (Kit)</b>\n👻 <b>Ghost Hannah (Kit Skin)</b>\n🏴‍☠️ <b>Corsair (Title)</b>\n🗡️ <b>Ghost Cutlass (Lobby Gadget)</b>\n🗡️ <b>Ghosted Hannah (Emote)</b>]]
+            },
+            ["image"] = "rbxassetid://10726577232",
+            ["name"] = "Hannah Bundle",
+            ["rewards"] = {
+                ["kits"] = {"hannah"},
+        		["emotes"] = {"hannah_ghosted"},
+                ["titles"] = {"corsair"},
+                ["kitSkins"] = {"hannah_ghost"},
+                ["lobbyGadgets"] = {"cutlass_ghost"}
+            },
+            ["price"] = 999,
+            ["devProductId"] = 1305893097,
+            ["limitedTime"] = true,
+            ["gamepassId"] = 78679462
+        }
+    }
+}
