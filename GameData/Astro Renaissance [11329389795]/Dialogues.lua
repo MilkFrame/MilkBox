471ebd42ea8e55dfaa41e73cdedecc1bde3173f7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-05 00:54:54 +1100

    Create Dialogues.lua

---
+++ GameData/Astro Renaissance [11329389795]/Dialogues.lua
@@ -0,0 +1,132 @@
+data = {
+    ["DahliaEND"] = {
+        {
+            ["Name"] = "Dahlia",
+            ["Text"] = [[You found all of the batteries! <font face="PermanentMarker" size="20" color="rgb(135,255,240)">THANK YOU SO MUCH!!!</font> Please feel free to work here whenever you need some <font size="20" face="Kalam" color="rgb(207,143,255)">Moonstones</font>!]]
+        }
+    },
+    ["LNYEND"] = {
+        {
+            ["Name"] = "Yutao",
+            ["Text"] = "YOU FINALLY GOT THEM!!! Phew... What a relief! Thank you so much Astral, I really appreciate it. Have a happy Lunar New Year and to a joyous festivity!"
+        }
+    },
+    ["Moonlight"] = {
+        {
+            ["Choices"] = {{["Text"] = "I'm lost!", ["Follow"] = true, ["Next"] = 2}},
+            ["Text"] = [[Hello Astral! What can I help you with in this <font size="20" face="FredokaOne" color="rgb(235,141,255)">GORGEOUS</font> day?]],
+            ["Name"] = "Moonlight"
+        },
+        {
+            ["Name"] = "Moonlight",
+            ["Text"] = [[Nothing to worry about! I'm <font face="PermanentMarker" color="rgb(255,128,241)" size="25">Moonlight,</font> your astral companion! Let me introduce you the basics of <font color="rgb(148,225,255)" size="20" face="Creepster">Astro Renaissance</font>]]
+        }
+    },
+    ["Saphire"] = {
+        {
+            ["Choices"] = {{["Text"] = "I'll help you look for them.", ["Follow"] = true, ["Next"] = 2}},
+            ["Text"] = [[*sigh* My friends and I decided to meet tomorrow at the <font face="Arcade" color="rgb(198,139,255)" size="20">Arcade</font>, *sniff* and I decided to buy some things from the <font color="rgb(164,255,162)" size="20" face="SciFi">Astro Line!</font> But on the way back... *sniff* EVERYTHING GOT <u>LOST</u>! 😭😪 *sigh* I was really excited but since I lost all of my things, my day got <font size="20" face="SpecialElite" color="rgb(255,159,159)"><uc>ruined!</uc></font> I have looked for hours... BUT I CAN'T FIND THEM! 😔]],
+            ["Name"] = "Saphire"
+        },
+        {
+            ["Name"] = "Saphire",
+            ["Text"] = [[YOU WOULD ACTUALLY DO THAT FOR ME?! I'LL BE FOREVER <font color="rgb(162,186,255)" face="IndieFlower" size="25">GRATEFUL!</font>]]
+        }
+    },
+    ["SaphireEND"] = {
+        {
+            ["Name"] = "Saphire",
+            ["Text"] = [[OH MY GOD you found all of the items?! Thank you so much for finding them! Here, take these <font size="20" face="PermanentMarker" color="rgb(231,168,255)"><uc>exclusive headphones</uc></font>, I won't need them anymore! 🤩]]
+        }
+    },
+    ["VenusEND"] = {
+        {
+            ["Name"] = "Venus",
+            ["Text"] = [[THANK YOU SO MUCH FOR FINDING MY CAT! Here, have a <font size="20" face="PermanentMarker" color="rgb(168,126,255)">plushie of my cat</font> I knitted a long long time ago, I hope you <b><font color="rgb(255,248,165)">like it</font></b>! 💗]]
+        }
+    },
+    ["Venus"] = {
+        {["Text"] = "Hey, is everything okay? You look worried...", ["Name"] = "You", ["Next"] = 2},
+        {
+            ["Choices"] = {
+                {["Text"] = "I can help you find him! What does he look like?", ["Follow"] = true, ["Next"] = 3}
+            },
+            ["Text"] = [[Well... My cat went <u>missing</u>. He was on my lap and he suddenly <font size="20" color="rgb(255,174,174)" face="FredokaOne">ran away</font> and I lost him. I don't know what to do and I cannot manage to find anyone who can help me find him. I've tried searching him myself but I can't seem to find him anywhere 🙁]],
+            ["Name"] = "Venus"
+        },
+        {
+            ["Name"] = "Venus",
+            ["Text"] = [[Thank you so so much, you're <uc><font color="rgb(255,248,165)" face="Arcade">amazing!</font></uc> He has <font color="rgb(159,155,154)"><b>grey</b></font> fur and a <font color="rgb(200,169,255)">purple</font> collar with a planet keychain attached to it. He also has the ability to <b><font face="Kalam" color="rgb(167,241,252)">fly</font></b>, so maybe he must've gotten himself stuck in a tree or somewhere very high up. I'm just worried he's alone and <font face="Creepster" size="20" color="rgb(120,104,252)">scared</font> without me. I really hope you can find him.]]
+        }
+    },
+    ["LNY"] = {
+        {
+            ["Choices"] = {
+                {["Text"] = "Uhm... yeah sure kid, do you know where they were?", ["Follow"] = true, ["Next"] = 2}
+            },
+            ["Text"] = [[Hey... <font size="18" face="Creepster" color="rgb(255,156,156)">HEY</font>!! You there!! Can you please help me find my <font color="rgb(255,248,166)" face="PatrickHand" size="25">hongbaos???</font> I lost them all... Please help me!]],
+            ["Name"] = "Yutao"
+        },
+        {
+            ["Text"] = "THANK YOU SO MUCH!!!! Ok ok uhm... uhm... AH shoot... I don't remember.... I was just running around with a friend and the next thing I know... they are gone!!!",
+            ["Name"] = "Yutao",
+            ["Next"] = 3
+        },
+        {["Name"] = "Yutao", ["Text"] = "Argh I don't know... just help me find them!!"}
+    },
+    ["DrFluffkins"] = {
+        {["Text"] = "Hello... Do you need anything?", ["Name"] = "You", ["Next"] = 2},
+        {
+            ["Choices"] = {{["Text"] = "I can help you find the tools and materials!", ["Follow"] = true, ["Next"] = 3}},
+            ["Text"] = [[Hello fellow Astral... I'm trying to gather some <font size="20" face="IndieFlower" color="rgb(200,155,255)">Moonstones</font> to buy a new laboratory, mine just <font face="Creepster" size="20" color="rgb(255,210,166)">EXPLODED</font> after trying a new experiment and I <u><b>NEED</b></u> a new one. I cannot repair it myself because some <font face="FredokaOne" size="18" color="rgb(144,210,255)"><b>Robots</b></font> stole my tools and materials 😭]],
+            ["Name"] = "Dr Fluffkins"
+        },
+        {
+            ["Name"] = "Dr Fluffkins",
+            ["Text"] = [[<font face="FredokaOne" size="20" color="rgb(255,240,168)">Really?!</font> I beg you to find them! I really need my laboratory <font size="18" face="Creepster" color="rgb(255,165,156)">BACK.</font> 😪]]
+        }
+    },
+    ["Dahlia"] = {
+        {
+            ["Text"] = [[Hello! May I have some <font face="IndieFlower" size="25" color="rgb(136,255,134)"><b>Ice cream</b></font> please? 😋]],
+            ["Name"] = "You",
+            ["Next"] = 2
+        },
+        {
+            ["Choices"] = {{["Text"] = "I can help you find the batteries!", ["Follow"] = true, ["Next"] = 3}},
+            ["Text"] = [[I'm afraid not sweetheart 😓... Someone stole my <font size="20" face="IndieFlower" color="rgb(255,140,246)">Magical Batteries</font> and now my Ice Cream Machine <font size="20" face="Creepster" color="rgb(255,162,162)">doesn't work</font>!!! I bet it was that <font face="PermanentMarker" color="rgb(199,255,162)" size="20">crazy scientist</font>! He always wants new material to work with... 😒]],
+            ["Name"] = "Dahlia"
+        },
+        {
+            ["Name"] = "Dahlia",
+            ["Text"] = [[That would be <font color="rgb(255,234,149)" face="IndieFlower" size="25"><uc>amazing!!!</uc></font> I hope you can find all of them or else I might have to stop selling Ice Cream <font size="20" color="rgb(255,141,141)" face="Creepster">FOREVER!</font> 😭]]
+        }
+    },
+    ["AstralRobot"] = {
+        {
+            ["Text"] = [[Oh, hey, where are we? After going to sleep I appeared in a spaceship with many more people, it had a kind of <b><font color="rgb(148,255,60)">portal</font></b> that made us appear on the shiny platforms...]],
+            ["Name"] = "You",
+            ["Next"] = 2
+        },
+        {
+            ["Text"] = [[So it looks like you're new... Welcome to <b><font face="FredokaOne" color="rgb(30,227,255)">Citadel of Cosmos!</font></b> The city that brings magic to an <b><font face="Arcade" color="rgb(255,172,254)">entire galaxy!</font></b>]],
+            ["Name"] = "AstroBot",
+            ["Next"] = 3
+        },
+        {
+            ["Text"] = [[<font face="Creepster" size="16" color="rgb(56,248,255)"><b>CITADEL OF COSMOS?</b></font> I remember this place very differently, the Heart of Citadel is the only thing that is the same.... But where is <b><font color="rgb(211,193,255)">Moonlight</font></b>? Maybe she can tell us what happened.]],
+            ["Name"] = "You",
+            ["Next"] = 4
+        },
+        {
+            ["Name"] = "AstroBot",
+            ["Text"] = [[<b>WHAT ARE YOU SAYING!</b> 😂 This place has always been like this! It's been <b>centuries of crescent moons</b> and everything is still the same, maybe a new building or a new visitor but everything else is the same! <font color="rgb(255,23,238)" face="Highway"><b><stroke color="rgb(161,161,161)">The Heart of Citadel</stroke></b></font> has always remained with us since the Moon created the Child of the Moon, Moonlight, in its maximum splendor, since that moment the love of each person in citadel has been in charge of growing the <b><font color="rgb(255,248,103)">glow of the heart</font></b>]]
+        }
+    },
+    ["DrFluffkinsEND"] = {
+        {
+            ["Name"] = "Dr Fluffkins",
+            ["Text"] = [[ARE THOSE MY TOOLS?! Thank you <font size="20" color="rgb(255,240,168)" face="FredokaOne">SOSOSO MUCH!!!!!!</font> Now I can finally work again 😎 Don't be afraid to come to my <font size="20" face="Arcade" color="rgb(156,166,255)">laboratory</font>, you can help me make new <font color="rgb(242,147,255)"><b><u>potions</u></b></font> if u desire! 🧪]]
+        }
+    }
+}
