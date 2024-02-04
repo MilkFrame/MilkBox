171e3d8e76c3fd39b67274d56be175a8efe22f8c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-05 00:54:11 +1100

    Create CodeModule.lua

---
+++ GameData/Astro Renaissance [11329389795]/CodeModule.lua
@@ -0,0 +1,8 @@
+data = {
+    ["#BeAstro"] = {["Reward"] = 2500, ["Type"] = "Tickets"},
+    ["LAGstro"] = {["Reward"] = 250, ["Type"] = "Tickets"},
+    ["AR1YEAR"] = {["Reward"] = 5000, ["Type"] = "Tickets"},
+    ["Cyberlove2024"] = {["Reward"] = 2500, ["Type"] = "Moonstones"},
+    ["Kicked"] = {["Reward"] = 150, ["Type"] = "Tickets"},
+    ["Glitched"] = {["Reward"] = 500, ["Type"] = "Tickets"}
+}
