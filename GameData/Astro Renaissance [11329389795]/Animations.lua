25234b3a94e10f9c13cc292f1e0dcea2806f2b4a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-05 00:51:37 +1100

    Create Animations.lua

---
+++ GameData/Astro Renaissance [11329389795]/Animations.lua
@@ -0,0 +1,164 @@
+data = {
+    ["Robot"] = {
+        ["FallAnimation"] = 619521521,
+        ["ClimbAnimation"] = 619521311,
+        ["IdleAnimation"] = 619521748,
+        ["WalkAnimation"] = 619522849,
+        ["SwimAnimation"] = 619522642,
+        ["JumpAnimation"] = 619522088,
+        ["RunAnimation"] = 619522386
+    },
+    ["Rthro"] = {
+        ["FallAnimation"] = 2510233257,
+        ["ClimbAnimation"] = 2510230574,
+        ["IdleAnimation"] = 2510235063,
+        ["WalkAnimation"] = 2510242378,
+        ["SwimAnimation"] = 2510240941,
+        ["JumpAnimation"] = 2510236649,
+        ["RunAnimation"] = 2510238627
+    },
+    ["Levitation"] = {
+        ["FallAnimation"] = 619541867,
+        ["ClimbAnimation"] = 619541458,
+        ["IdleAnimation"] = 619542203,
+        ["WalkAnimation"] = 619544080,
+        ["SwimAnimation"] = 619543721,
+        ["JumpAnimation"] = 619542888,
+        ["RunAnimation"] = 619543231
+    },
+    ["Werewolf"] = {
+        ["FallAnimation"] = 1113742092,
+        ["ClimbAnimation"] = 1113754738,
+        ["IdleAnimation"] = 1113752682,
+        ["WalkAnimation"] = 1113751657,
+        ["SwimAnimation"] = 1113752975,
+        ["JumpAnimation"] = 1113752285,
+        ["RunAnimation"] = 1113750642
+    },
+    ["Knight"] = {
+        ["FallAnimation"] = 734326679,
+        ["ClimbAnimation"] = 734329002,
+        ["IdleAnimation"] = 734327140,
+        ["WalkAnimation"] = 734326330,
+        ["SwimAnimation"] = 734327363,
+        ["JumpAnimation"] = 734326930,
+        ["RunAnimation"] = 734325948
+    },
+    ["Pirate"] = {
+        ["FallAnimation"] = 837024147,
+        ["ClimbAnimation"] = 837025325,
+        ["IdleAnimation"] = 837024662,
+        ["WalkAnimation"] = 837023892,
+        ["SwimAnimation"] = 837025054,
+        ["JumpAnimation"] = 837024350,
+        ["RunAnimation"] = 837023444
+    },
+    ["Oldschool"] = {
+        ["FallAnimation"] = 5319914476,
+        ["ClimbAnimation"] = 5319931619,
+        ["IdleAnimation"] = 5319922112,
+        ["WalkAnimation"] = 5319909330,
+        ["SwimAnimation"] = 5319927054,
+        ["JumpAnimation"] = 5319917561,
+        ["RunAnimation"] = 5319900634
+    },
+    ["Astronaut"] = {
+        ["FallAnimation"] = 1090132063,
+        ["ClimbAnimation"] = 1090134016,
+        ["IdleAnimation"] = 1090133099,
+        ["WalkAnimation"] = 1090131576,
+        ["SwimAnimation"] = 1090133583,
+        ["JumpAnimation"] = 1090132507,
+        ["RunAnimation"] = 1090130630
+    },
+    ["Cartoony"] = {
+        ["FallAnimation"] = 837010685,
+        ["ClimbAnimation"] = 837013990,
+        ["IdleAnimation"] = 837011741,
+        ["WalkAnimation"] = 837010234,
+        ["SwimAnimation"] = 837012509,
+        ["JumpAnimation"] = 837011171,
+        ["RunAnimation"] = 837009922
+    },
+    ["Ninja"] = {
+        ["FallAnimation"] = 658831500,
+        ["ClimbAnimation"] = 658833139,
+        ["IdleAnimation"] = 658832408,
+        ["WalkAnimation"] = 658831143,
+        ["SwimAnimation"] = 658832807,
+        ["JumpAnimation"] = 658832070,
+        ["RunAnimation"] = 658830056
+    },
+    ["Mage"] = {
+        ["FallAnimation"] = 754636589,
+        ["ClimbAnimation"] = 754639239,
+        ["IdleAnimation"] = 754637456,
+        ["WalkAnimation"] = 754636298,
+        ["SwimAnimation"] = 754638471,
+        ["JumpAnimation"] = 754637084,
+        ["RunAnimation"] = 754635032
+    },
+    ["Elder"] = {
+        ["FallAnimation"] = 892267521,
+        ["ClimbAnimation"] = 892269341,
+        ["IdleAnimation"] = 892268340,
+        ["WalkAnimation"] = 892267099,
+        ["SwimAnimation"] = 892268710,
+        ["JumpAnimation"] = 892267917,
+        ["RunAnimation"] = 892265784
+    },
+    ["Bubbly"] = {
+        ["FallAnimation"] = 1018552770,
+        ["ClimbAnimation"] = 1018554668,
+        ["IdleAnimation"] = 1018553897,
+        ["WalkAnimation"] = 1018549681,
+        ["SwimAnimation"] = 1018554245,
+        ["JumpAnimation"] = 1018553240,
+        ["RunAnimation"] = 1018548665
+    },
+    ["Vampire"] = {
+        ["FallAnimation"] = 1113742092,
+        ["ClimbAnimation"] = 1113743239,
+        ["IdleAnimation"] = 1113742618,
+        ["WalkAnimation"] = 1113741192,
+        ["SwimAnimation"] = 1113742944,
+        ["JumpAnimation"] = 1113742359,
+        ["RunAnimation"] = 1113740510
+    },
+    ["Toy"] = {
+        ["FallAnimation"] = 973768058,
+        ["ClimbAnimation"] = 973773170,
+        ["IdleAnimation"] = 973771666,
+        ["WalkAnimation"] = 973767371,
+        ["SwimAnimation"] = 973772659,
+        ["JumpAnimation"] = 973770652,
+        ["RunAnimation"] = 973766674
+    },
+    ["Wheelchair"] = {
+        ["FallAnimation"] = 15872580089,
+        ["ClimbAnimation"] = 15872570543,
+        ["IdleAnimation"] = 15872513227,
+        ["WalkAnimation"] = 15872533584,
+        ["SwimAnimation"] = 5319927054,
+        ["JumpAnimation"] = 15872576372,
+        ["RunAnimation"] = 15871887589
+    },
+    ["Superhero"] = {
+        ["FallAnimation"] = 619527817,
+        ["ClimbAnimation"] = 619527470,
+        ["IdleAnimation"] = 619528125,
+        ["WalkAnimation"] = 619529601,
+        ["SwimAnimation"] = 619529095,
+        ["JumpAnimation"] = 619528412,
+        ["RunAnimation"] = 619528716
+    },
+    ["Stylish"] = {
+        ["FallAnimation"] = 619511417,
+        ["ClimbAnimation"] = 619509955,
+        ["IdleAnimation"] = 619511648,
+        ["WalkAnimation"] = 619512767,
+        ["SwimAnimation"] = 619512450,
+        ["JumpAnimation"] = 619511974,
+        ["RunAnimation"] = 619512153
+    }
+}
