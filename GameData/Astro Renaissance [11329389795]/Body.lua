af91e189927890770c61a37e02851d846f43d3a7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-05 00:51:56 +1100

    Create Body.lua

---
+++ GameData/Astro Renaissance [11329389795]/Body.lua
@@ -0,0 +1,18 @@
+data = {
+    ["Woman"] = {
+        ["RightArm"] = "86499698",
+        ["LeftArm"] = "86499716",
+        ["Image"] = "rbxassetid://14206656840",
+        ["Torso"] = "86499666",
+        ["RightLeg"] = "86499793",
+        ["LeftLeg"] = "86499753"
+    },
+    ["ROBLOX Boy"] = {
+        ["RightArm"] = "376531012",
+        ["LeftArm"] = "376530220",
+        ["Image"] = "rbxassetid://12554146357",
+        ["Torso"] = "376532000",
+        ["RightLeg"] = "376531703",
+        ["LeftLeg"] = "376531300"
+    }
+}
