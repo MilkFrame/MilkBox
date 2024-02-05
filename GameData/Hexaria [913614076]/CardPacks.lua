8a1c6daae55a594e8051a49d2d09f3a689418fc7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-06 00:41:13 +1100

    Create CardPacks.lua

---
+++ GameData/Hexaria [913614076]/CardPacks.lua
@@ -0,0 +1,118 @@
+data = {
+    ["Mystic Wisdom"] = {
+        ["CardsPerPack"] = 3,
+        ["Cards"] = {
+            {
+                "Wisp",
+                "Wand Zap",
+                "Magician's Secret",
+                "Wand Heal",
+                "Insight",
+                "Contact Heal",
+                "Dispel",
+                "Aura Pierce",
+                "Panic",
+                "Conjure",
+                "Spell Backfire",
+                "Cursed Rune",
+                "Mystery Hat",
+                "Regenerative Pulse",
+                "Blessed Rune",
+                "Strength Duality",
+                "Corrupt"
+            },
+            {
+                "Zoink",
+                "Flame Blockade",
+                "Huge Blessing",
+                "Arcane Blast",
+                "Divine Grace",
+                "Ignite",
+                "Chain Lightning",
+                "Enlighten",
+                "Body Cleanse",
+                "Guided Strike",
+                "Barrier",
+                "Dazzle"
+            },
+            {"Meditation", "Teleport", "Holy Ground", "High Cognition", "Hocus Pocus"},
+            {"Horn of the Unicorn"}
+        }
+    },
+    ["Blazing Inferno"] = {
+        ["CardsPerPack"] = 3,
+        ["Cards"] = {
+            {
+                "Burning Cleanse",
+                "Torch",
+                "Combust",
+                "Scorch",
+                "Campfire",
+                "Lantern Throw",
+                "Hot Sauce",
+                "Flaming Uppercut",
+                "Drought",
+                "Fizzle",
+                "Melt",
+                "Firepower",
+                "Burning Staff",
+                "Spicy Concoction",
+                "Will-o'-the-Wisp",
+                "Hot Hands"
+            },
+            {
+                "Reignite",
+                "Inferno Blast",
+                "Singe",
+                "Fire Breath",
+                "Coruscate",
+                "Burning Mask",
+                "Blazing Short Sword",
+                "Hot Coals",
+                "Swelter",
+                "Scorching Sauna"
+            },
+            {"Pyrocycle", "Cauterize", "Transfer Flame", "Meteor Strike", "Fire Axe"},
+            {"Detonate"}
+        }
+    },
+    ["Genesis"] = {
+        ["CardsPerPack"] = 3,
+        ["Cards"] = {
+            {
+                "Smack",
+                "Attack Buff",
+                "Attack Debuff",
+                "Defense Buff",
+                "Defense Debuff",
+                "Banana Peel",
+                "Ember",
+                "Pollute",
+                "Ant Hill",
+                "Stupify",
+                "Afternoon Delight",
+                "Backhand Smack",
+                "Scratch",
+                "Karaoke",
+                "Mask of Uncertainty",
+                "Antidote",
+                "Extinguish",
+                "Rest and Recovery",
+                "Beauty Sleep",
+                "Really Loud Noise",
+                "Rubber Duck"
+            },
+            {
+                "The Ole' One-Two",
+                "Mask of Truth",
+                "Papercut",
+                "Mask of Calamity",
+                "Masquerade",
+                "Blue Elixir",
+                "Smokescreen"
+            },
+            {"Purge", "Blinding Flash", "Mask of Courage"},
+            {"Core Infliction"}
+        }
+    }
+}
