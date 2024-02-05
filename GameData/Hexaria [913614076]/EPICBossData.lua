76af503c6ebbd596c7dd9ff0893d40b9613c4c56
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-06 00:43:03 +1100

    Create EPICBossData.lua

---
+++ GameData/Hexaria [913614076]/EPICBossData.lua
@@ -0,0 +1,19 @@
+data = {
+    ["NumEPICBossRaids"] = 2,
+    ["WeeklyRaids"] = {
+        {
+            ["RaidLocation"] = "the crypt in Cambris",
+            ["BossName"] = "Gonaku",
+            ["Normal_Place"] = "Cambris.Crypt",
+            ["Pronouns"] = {"he", "him"},
+            ["EPIC_Place"] = "EPIC.Crypt"
+        },
+        {
+            ["RaidLocation"] = "the den of thieves",
+            ["BossName"] = "King of Thieves",
+            ["Normal_Place"] = "Cambris.DenOfThieves",
+            ["Pronouns"] = {"he", "him"},
+            ["EPIC_Place"] = "EPIC.DenOfThieves"
+        }
+    }
+}
