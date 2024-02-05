28b17c11553bb55a09cf7194068f486d054e718f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-06 00:42:41 +1100

    Create DealData.lua

---
+++ GameData/Hexaria [913614076]/DealData.lua
@@ -0,0 +1,113 @@
+data = {
+    ["DailyDeals"] = {
+        {
+            ["Id"] = 99001,
+            ["Contents"] = {{["Type"] = "Item", ["ItemType"] = "clothes_beast_hunter_yellow", ["Quantity"] = 1}},
+            ["Value"] = 300
+        },
+        {
+            ["Id"] = 99002,
+            ["Contents"] = {{["Type"] = "Currency", ["Quantity"] = 800, ["Currency"] = "Gold"}},
+            ["Value"] = 300
+        },
+        {
+            ["Id"] = 99003,
+            ["Contents"] = {
+                {["Type"] = "Item", ["ItemType"] = "pack_mystic_wisdom", ["Quantity"] = 3},
+                {["Type"] = "Item", ["ItemType"] = "pack_genesis", ["Quantity"] = 3},
+                {["Type"] = "Item", ["ItemType"] = "pack_blazing_inferno", ["Quantity"] = 3}
+            },
+            ["Value"] = 275
+        },
+        {
+            ["Id"] = 99004,
+            ["Contents"] = {{["Type"] = "Item", ["ItemType"] = "teleport_stone", ["Quantity"] = 5}},
+            ["Value"] = 250
+        },
+        {
+            ["Id"] = 99005,
+            ["Contents"] = {{["Type"] = "Item", ["ItemType"] = "clothes_beast_hunter_purple", ["Quantity"] = 1}},
+            ["Value"] = 400
+        },
+        {
+            ["Id"] = 99006,
+            ["Contents"] = {{["Type"] = "Item", ["ItemType"] = "epic_sigil_of_courage", ["Quantity"] = 3}},
+            ["Value"] = 150
+        },
+        {
+            ["Id"] = 99007,
+            ["Contents"] = {{["Type"] = "Item", ["ItemType"] = "key_mysterious_crate", ["Quantity"] = 3}},
+            ["Value"] = 250
+        },
+        {
+            ["Id"] = 99008,
+            ["Contents"] = {
+                {["Type"] = "Item", ["ItemType"] = "boost_small_gold", ["Quantity"] = 3},
+                {["Type"] = "Item", ["ItemType"] = "boost_small_experience", ["Quantity"] = 3},
+                {["Type"] = "Item", ["ItemType"] = "boost_small_speed", ["Quantity"] = 3},
+                {["Type"] = "Item", ["ItemType"] = "boost_small_heal", ["Quantity"] = 3}
+            },
+            ["Value"] = 250
+        },
+        {
+            ["Id"] = 99009,
+            ["Contents"] = {{["Type"] = "Item", ["ItemType"] = "clothes_beast_hunter_green", ["Quantity"] = 1}},
+            ["Value"] = 400
+        },
+        {
+            ["Id"] = 99010,
+            ["Contents"] = {
+                {["Type"] = "Item", ["ItemType"] = "pack_mystic_wisdom", ["Quantity"] = 3},
+                {["Type"] = "Item", ["ItemType"] = "pack_genesis", ["Quantity"] = 3},
+                {["Type"] = "Item", ["ItemType"] = "pack_blazing_inferno", ["Quantity"] = 3}
+            },
+            ["Value"] = 275
+        },
+        {
+            ["Id"] = 99011,
+            ["Contents"] = {{["Type"] = "Item", ["ItemType"] = "clothes_beast_hunter_red", ["Quantity"] = 1}},
+            ["Value"] = 400
+        },
+        {
+            ["Id"] = 99012,
+            ["Contents"] = {
+                {["Type"] = "Currency", ["Quantity"] = 500, ["Currency"] = "Gold"},
+                {["Type"] = "Item", ["ItemType"] = "health_potion", ["Quantity"] = 5}
+            },
+            ["Value"] = 250
+        },
+        {
+            ["Id"] = 99013,
+            ["Contents"] = {{["Type"] = "Item", ["ItemType"] = "clothes_beast_hunter_blue", ["Quantity"] = 1}},
+            ["Value"] = 400
+        },
+        {
+            ["Id"] = 99014,
+            ["Contents"] = {{["Type"] = "Item", ["ItemType"] = "nether_token", ["Quantity"] = 5}},
+            ["Value"] = 300
+        }
+    },
+    ["NumDailyDeals"] = 14,
+    ["WeeklyDeal"] = {
+        ["Id"] = 6,
+        ["Contents"] = {
+            {["Type"] = "Currency", ["Quantity"] = 1000, ["Currency"] = "Gold"},
+            {["Type"] = "Currency", ["Quantity"] = 750, ["Currency"] = "Hexons"},
+            {["Type"] = "Item", ["ItemType"] = "enchanted_power_infusion_crystal", ["Quantity"] = 50},
+            {["Type"] = "Item", ["ItemType"] = "epic_sigil_of_courage", ["Quantity"] = 3},
+            {["Type"] = "Item", ["ItemType"] = "clothes_lonnie_ric", ["Quantity"] = 1}
+        },
+        ["Value"] = 2000
+    },
+    ["MonthlyDeal"] = {
+        ["Id"] = 55006,
+        ["Contents"] = {
+            {["Type"] = "Currency", ["Quantity"] = 5000, ["Currency"] = "Gold"},
+            {["Type"] = "Currency", ["Quantity"] = 2000, ["Currency"] = "Hexons"},
+            {["Type"] = "Item", ["ItemType"] = "enchanted_power_infusion_crystal", ["Quantity"] = 250},
+            {["Type"] = "Item", ["ItemType"] = "epic_sigil_of_courage", ["Quantity"] = 15},
+            {["Type"] = "Item", ["ItemType"] = "clothes_lost_lithian", ["Quantity"] = 1}
+        },
+        ["Value"] = 6500
+    }
+}
