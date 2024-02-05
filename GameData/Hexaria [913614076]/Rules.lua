6c8dc766b55673bce62d81ac163c49a1cdf1b576
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-06 00:53:33 +1100

    Create Rules.lua

---
+++ GameData/Hexaria [913614076]/Rules.lua
@@ -0,0 +1,37 @@
+data = {
+    {
+        ["Description"] = "This player is either harassing, manipulating, swearing, bullying or trolling me or another player in the server.",
+        ["Name"] = "Harassment",
+        ["Reason"] = "Harassing other players"
+    },
+    {
+        ["Description"] = "This player is impersonating me or another player in the server.",
+        ["Name"] = "Impersonation",
+        ["Reason"] = "Impersonating other players"
+    },
+    {
+        ["Description"] = "This player is talking about inappropriate topics",
+        ["Name"] = "Inappropriate",
+        ["Reason"] = "Talking about inappropriate topics"
+    },
+    {
+        ["Description"] = "This player repeats the same line of text over and over again and fills the chat.",
+        ["Name"] = "Spamming",
+        ["Reason"] = "Spamming the chat"
+    },
+    {
+        ["Description"] = "This player lies about how a card can be obtained or what it does in battle.",
+        ["Name"] = "Scamming",
+        ["Reason"] = "Scamming other players"
+    },
+    {
+        ["Description"] = "This player is hacking or cheating or doing something that is impossible without the use of external programs.",
+        ["Name"] = "Exploiting",
+        ["Reason"] = "Exploiting"
+    },
+    {
+        ["Description"] = "This player did something else that you think is breaking the rules.",
+        ["Name"] = "Other",
+        ["Reason"] = "Breaking a rule (not specified)"
+    }
+}
