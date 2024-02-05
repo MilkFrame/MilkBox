ad3e28c079319d600712e62e39a0c331a7f08ef2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-06 00:50:02 +1100

    Create NPCChanceEncounter.lua

---
+++ GameData/Hexaria [913614076]/NPCChanceEncounter.lua
@@ -0,0 +1,66 @@
+data = {
+    ["List"] = {
+        ["Cambris.Shack"] = "function: 0x00000000ad1d9ac5",
+        ["Nether.CupidOutpost"] = {
+            ["Start"] = {
+                ["NPCTypes"] = {127, 128},
+                ["Count"] = 2,
+                ["Chance"] = 1,
+                ["NPCAppearance"] = {"Nether/CupidGoon", "Nether/CupidCaster"},
+                ["Team"] = 2
+            },
+            ["During"] = {
+                ["Rounds"] = 20,
+                ["Count"] = 1,
+                ["Chance"] = 0.1,
+                ["NPCAppearance"] = {
+                    "Nether/CupidScout",
+                    "Nether/EyeofCupid",
+                    "Nether/Heartbreaker",
+                    "Nether/CupidGoon",
+                    "Nether/CupidCaster"
+                },
+                ["NPCTypes"] = {126, 123, 125, 127, 128},
+                ["Team"] = 2
+            }
+        },
+        ["Cambris.DrownedPass"] = {
+            ["During"] = {
+                ["Rounds"] = 30,
+                ["Count"] = 1,
+                ["NPCAppearance"] = {
+                    "Cambris/ZombiePirate",
+                    "Cambris/ZombieRanger",
+                    "Cambris/ZombieMercenary",
+                    "Cambris/ZombieMage",
+                    "Cambris/ZombieAdventurer",
+                    "Cambris/ZombieGuard",
+                    "Summoned/SummonedZombie"
+                },
+                ["Chance"] = 0.05,
+                ["NPCTypes"] = {157, 158, 159, 160, 161, 162, 165},
+                ["Edge"] = true,
+                ["Team"] = 2
+            }
+        },
+        ["Olbrek.AeroPeaks"] = "function: 0x0000000059fd852d"
+    },
+    ["Wanted"] = {
+        ["314562989"] = true,
+        ["1279207488"] = true,
+        ["331868591"] = true,
+        ["16186071"] = true,
+        ["72425862"] = true,
+        ["311770480"] = true,
+        ["238935943"] = true,
+        ["442947355"] = true,
+        ["56130004"] = true,
+        ["496856664"] = true,
+        ["128561079"] = true,
+        ["1143432799"] = true,
+        ["22817284"] = true,
+        ["1410838441"] = true,
+        ["1531801585"] = true,
+        ["112765820"] = true
+    }
+}
