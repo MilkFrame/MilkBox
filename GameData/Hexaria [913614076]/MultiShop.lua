f649504c54173946d9cac0e2dbe6aee9fa40b9c7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-06 00:49:22 +1100

    Create MultiShop.lua

---
+++ GameData/Hexaria [913614076]/MultiShop.lua
@@ -0,0 +1,298 @@
+data = {
+    ["epic_cosmetic_multishop"] = {
+        ["Currency"] = 196,
+        ["CurrencyType"] = 2,
+        ["Offers"] = {
+            {["Price"] = 60, ["Offer"] = {{["ItemType"] = 207, ["Count"] = 1}}},
+            {["Price"] = 60, ["Offer"] = {{["ItemType"] = 208, ["Count"] = 1}}},
+            {["Price"] = 60, ["Offer"] = {{["ItemType"] = 209, ["Count"] = 1}}},
+            {["Price"] = 100, ["Offer"] = {{["ItemType"] = 210, ["Count"] = 1}}},
+            {["Price"] = 100, ["Offer"] = {{["ItemType"] = 211, ["Count"] = 1}}},
+            {["Price"] = 100, ["Offer"] = {{["ItemType"] = 212, ["Count"] = 1}}},
+            {["Price"] = 250, ["Offer"] = {{["ItemType"] = 202, ["Count"] = 1}}},
+            {["Price"] = 250, ["Offer"] = {{["ItemType"] = 203, ["Count"] = 1}}},
+            {["Price"] = 250, ["Offer"] = {{["ItemType"] = 204, ["Count"] = 1}}},
+            {["Price"] = 250, ["Offer"] = {{["ItemType"] = 197, ["Count"] = 1}}},
+            {["Price"] = 250, ["Offer"] = {{["ItemType"] = 198, ["Count"] = 1}}},
+            {["Price"] = 250, ["Offer"] = {{["ItemType"] = 199, ["Count"] = 1}}}
+        }
+    },
+    ["snowville_booster_multishop"] = {
+        ["Currency"] = "Gold",
+        ["CurrencyType"] = 0,
+        ["Offers"] = {
+            {["Price"] = 400, ["Offer"] = {{["ItemType"] = 360, ["Count"] = 1}}},
+            {["Price"] = 4000, ["Offer"] = {{["ItemType"] = 360, ["Count"] = 10}}}
+        }
+    },
+    ["kapak_multishop"] = {
+        ["Currency"] = "Gold",
+        ["CurrencyType"] = 0,
+        ["Offers"] = {
+            {["Price"] = 500, ["Offer"] = {{["ItemType"] = 368, ["Count"] = 1}}},
+            {["Price"] = 5000, ["Offer"] = {{["ItemType"] = 368, ["Count"] = 10}}},
+            {["Price"] = 10000, ["Offer"] = {{["ItemType"] = 368, ["Count"] = 20}}}
+        }
+    },
+    ["golden_outfit_multishop"] = {
+        ["Currency"] = "Gold",
+        ["CurrencyType"] = 0,
+        ["Offers"] = {{["Price"] = 500000, ["Offer"] = {{["ItemType"] = 274, ["Count"] = 1}}}}
+    },
+    ["epic_card_multishop"] = {
+        ["Currency"] = 196,
+        ["CurrencyType"] = 2,
+        ["Offers"] = {
+            {["Price"] = 9, ["Offer"] = {{["CardName"] = "Poisoned Spikes", ["Count"] = 1}}},
+            {["Price"] = 12, ["Offer"] = {{["CardName"] = "Radiant Indemnity", ["Count"] = 1}}},
+            {["Price"] = 15, ["Offer"] = {{["CardName"] = "Plated Sword", ["Count"] = 1}}},
+            {["Price"] = 16, ["Offer"] = {{["CardName"] = "Axe Slash", ["Count"] = 1}}},
+            {["Price"] = 18, ["Offer"] = {{["CardName"] = "Deep Blight", ["Count"] = 1}}},
+            {["Price"] = 24, ["Offer"] = {{["CardName"] = "Crippling Shot", ["Count"] = 1}}},
+            {["Price"] = 29, ["Offer"] = {{["CardName"] = "Woozy Whirl", ["Count"] = 1}}},
+            {["Price"] = 32, ["Offer"] = {{["CardName"] = "Rotten Claw", ["Count"] = 1}}},
+            {["Price"] = 35, ["Offer"] = {{["CardName"] = "Foul Breath", ["Count"] = 1}}},
+            {["Price"] = 42, ["Offer"] = {{["CardName"] = "Arms Race", ["Count"] = 1}}},
+            {["Price"] = 47, ["Offer"] = {{["CardName"] = "Toxic Coat", ["Count"] = 1}}}
+        }
+    },
+    ["golden_gift_multishop"] = {
+        ["Currency"] = "Gold",
+        ["CurrencyType"] = 0,
+        ["Offers"] = {
+            {["Price"] = 500000, ["Offer"] = {{["ItemType"] = 271, ["Count"] = 1}}},
+            {["Price"] = 1500, ["Offer"] = {{["ItemType"] = 270, ["Count"] = 1}}},
+            {["Price"] = 15000, ["Offer"] = {{["ItemType"] = 270, ["Count"] = 10}}}
+        }
+    },
+    ["skham_multishop"] = {
+        ["Currency"] = "Hexons",
+        ["CurrencyType"] = 1,
+        ["Offers"] = {
+            {["Price"] = 55, ["Offer"] = {{["CardName"] = "Glognut", ["Count"] = 1}}},
+            {["Price"] = 4500, ["Offer"] = {{["CardName"] = "Golden Glognut", ["Count"] = 1}}}
+        }
+    },
+    ["merc_multishop"] = {
+        ["Currency"] = "Gold",
+        ["CurrencyType"] = 0,
+        ["Offers"] = {
+            {["Price"] = 5000, ["Offer"] = {{["CardName"] = "Mercenary", ["Count"] = 1}}},
+            {["Price"] = 5000, ["Offer"] = {{["CardName"] = "Hired Wizard", ["Count"] = 1}}},
+            {["Price"] = 100, ["Offer"] = {{["CardName"] = "Scimitar", ["Count"] = 1}}},
+            {["Price"] = 2500, ["Offer"] = {{["ItemType"] = 42, ["Count"] = 1}}}
+        }
+    },
+    ["kishuf_event_multishop"] = {
+        ["Currency"] = 223,
+        ["CurrencyType"] = 2,
+        ["Offers"] = {
+            {["Price"] = 12, ["Offer"] = {{["CardName"] = "Kishuf Ward", ["Count"] = 1}}},
+            {["Price"] = 27, ["Offer"] = {{["CardName"] = "Unstable Corruption", ["Count"] = 1}}},
+            {["Price"] = 32, ["Offer"] = {{["CardName"] = "Kishuf Shrike", ["Count"] = 1}}},
+            {["Price"] = 44, ["Offer"] = {{["CardName"] = "Unstable Blast", ["Count"] = 1}}},
+            {["Price"] = 49, ["Offer"] = {{["CardName"] = "Neurotic Decoction", ["Count"] = 1}}},
+            {["Price"] = 55, ["Offer"] = {{["CardName"] = "Ancient Prayer", ["Count"] = 1}}},
+            {["Price"] = 72, ["Offer"] = {{["ItemType"] = 154, ["Count"] = 1}}},
+            {["Price"] = 250, ["Offer"] = {{["ItemType"] = 268, ["Count"] = 1}}}
+        }
+    },
+    ["nether_multishop"] = {
+        ["Currency"] = 50,
+        ["CurrencyType"] = 2,
+        ["Offers"] = {
+            {["Price"] = 3, ["Offer"] = {{["CardName"] = "Berserker Stance", ["Count"] = 1}}},
+            {["Price"] = 5, ["Offer"] = {{["ItemType"] = 98, ["Count"] = 1}}},
+            {
+                ["Price"] = 10,
+                ["Offer"] = {
+                    {["ItemType"] = 122, ["Count"] = 2},
+                    {["ItemType"] = 123, ["Count"] = 2},
+                    {["ItemType"] = 124, ["Count"] = 2},
+                    {["ItemType"] = 125, ["Count"] = 2}
+                }
+            },
+            {["Price"] = 12, ["Offer"] = {{["CardName"] = "Linked Hearts", ["Count"] = 1}}},
+            {
+                ["Price"] = 20,
+                ["Offer"] = {
+                    {["ItemType"] = 18, ["Count"] = 1},
+                    {["ItemType"] = 6, ["Count"] = 2},
+                    {["ItemType"] = 14, ["Count"] = 2}
+                }
+            },
+            {["Price"] = 30, ["Offer"] = {{["CardName"] = "Demon Lore", ["Count"] = 1}}},
+            {["Price"] = 45, ["Offer"] = {{["CardName"] = "Agonized Greed", ["Count"] = 1}}},
+            {["Price"] = 200, ["Offer"] = {{["CardName"] = "Ancient Power", ["Count"] = 1}}},
+            {["Price"] = 500, ["Offer"] = {{["ItemType"] = 21, ["Count"] = 1}}},
+            {["Price"] = 800, ["Offer"] = {{["ItemType"] = 119, ["Count"] = 1}}},
+            {["Price"] = 1000, ["Offer"] = {{["ItemType"] = 129, ["Count"] = 1}}}
+        }
+    },
+    ["blacksmith_multishop"] = {
+        ["Currency"] = "Gold",
+        ["CurrencyType"] = 0,
+        ["Offers"] = {
+            {["Price"] = 15, ["Offer"] = {{["CardName"] = "Rusty Nails", ["Count"] = 1}}},
+            {["Price"] = 200, ["Offer"] = {{["CardName"] = "Cambrian Shield", ["Count"] = 1}}},
+            {["Price"] = 200, ["Offer"] = {{["CardName"] = "Fire Arrow", ["Count"] = 1}}},
+            {["Price"] = 500, ["Offer"] = {{["CardName"] = "Mace Smash", ["Count"] = 1}}},
+            {["Price"] = 750, ["Offer"] = {{["CardName"] = "Blacksmith's Expertise", ["Count"] = 1}}}
+        }
+    },
+    ["banditoutfit_multishop"] = {
+        ["Currency"] = 35,
+        ["CurrencyType"] = 2,
+        ["Offers"] = {
+            {["Price"] = 1, ["Offer"] = {{["CardName"] = "Bow and Arrow", ["Count"] = 1}}},
+            {["Price"] = 1, ["Offer"] = {{["CardName"] = "Wooden Club Slam", ["Count"] = 1}}},
+            {["Price"] = 1, ["Offer"] = {{["CardName"] = "Rusty Knife", ["Count"] = 1}}},
+            {["Price"] = 1, ["Offer"] = {{["CardName"] = "Bear Trap", ["Count"] = 1}}},
+            {["Price"] = 1, ["Offer"] = {{["CardName"] = "Tripwire", ["Count"] = 1}}},
+            {["Price"] = 1, ["Offer"] = {{["CardName"] = "Dash", ["Count"] = 1}}},
+            {["Price"] = 1, ["Offer"] = {{["CardName"] = "Sharp Instincts", ["Count"] = 1}}}
+        }
+    },
+    ["kapak_kandyshop_cards"] = {
+        ["Currency"] = 214,
+        ["CurrencyType"] = 2,
+        ["Offers"] = {
+            {["Price"] = 38, ["Offer"] = {{["CardName"] = "Trick or Treat", ["Count"] = 1}}},
+            {["Price"] = 48, ["Offer"] = {{["CardName"] = "Agonized Greed", ["Count"] = 1}}},
+            {["Price"] = 70, ["Offer"] = {{["CardName"] = "Agony Seal", ["Count"] = 1}}},
+            {["Price"] = 80, ["Offer"] = {{["CardName"] = "Ancient Power", ["Count"] = 1}}},
+            {["Price"] = 89, ["Offer"] = {{["CardName"] = "Agonizing Transformation", ["Count"] = 1}}},
+            {["Price"] = 90, ["Offer"] = {{["CardName"] = "Hired Pumpkinfool", ["Count"] = 1}}},
+            {["Price"] = 98, ["Offer"] = {{["CardName"] = "Twilight Zone", ["Count"] = 1}}}
+        }
+    },
+    ["ashley_sagespark_multishop"] = {
+        ["Currency"] = 300,
+        ["CurrencyType"] = 2,
+        ["Offers"] = {
+            {["Price"] = 4, ["Offer"] = {{["CardName"] = "Astral Bastion", ["Count"] = 1}}},
+            {["Price"] = 6, ["Offer"] = {{["CardName"] = "Astral Blessing", ["Count"] = 1}}},
+            {["Price"] = 100, ["Offer"] = {{["ItemType"] = 302, ["Count"] = 1}}}
+        }
+    },
+    ["empty_spellbook_multishop"] = {
+        ["Currency"] = "Gold",
+        ["CurrencyType"] = 0,
+        ["Offers"] = {{["Price"] = 5000, ["Offer"] = {{["ItemType"] = 343, ["Count"] = 1}}}}
+    },
+    ["olbrek_chess_multishop"] = {
+        ["Currency"] = 356,
+        ["CurrencyType"] = 2,
+        ["Offers"] = {
+            {["Price"] = 4, ["Offer"] = {{["CardName"] = "Pawn's Gambit", ["Count"] = 1}}},
+            {["Price"] = 5, ["Offer"] = {{["CardName"] = "Knight's Leap", ["Count"] = 1}}},
+            {["Price"] = 5, ["Offer"] = {{["CardName"] = "Bishop's Benevolence", ["Count"] = 1}}},
+            {["Price"] = 6, ["Offer"] = {{["CardName"] = "Rook's Maneuver", ["Count"] = 1}}},
+            {["Price"] = 7, ["Offer"] = {{["CardName"] = "King's Governance", ["Count"] = 1}}},
+            {["Price"] = 8, ["Offer"] = {{["CardName"] = "Queen's Terror", ["Count"] = 1}}}
+        }
+    },
+    ["kapak_kandyshop_items"] = {
+        ["Currency"] = 214,
+        ["CurrencyType"] = 2,
+        ["Offers"] = {
+            {["Price"] = 35, ["Offer"] = {{["ItemType"] = 57, ["Count"] = 1}}},
+            {["Price"] = 40, ["Offer"] = {{["ItemType"] = 55, ["Count"] = 1}}},
+            {["Price"] = 60, ["Offer"] = {{["ItemType"] = 132, ["Count"] = 1}}},
+            {["Price"] = 80, ["Offer"] = {{["ItemType"] = 373, ["Count"] = 1}}},
+            {["Price"] = 100, ["Offer"] = {{["ItemType"] = 133, ["Count"] = 1}}},
+            {["Price"] = 150, ["Offer"] = {{["ItemType"] = 56, ["Count"] = 1}}},
+            {["Price"] = 350, ["Offer"] = {{["ItemType"] = 215, ["Count"] = 1}}}
+        }
+    },
+    ["olbrek_newbie_multishop"] = {
+        ["Currency"] = "Gold",
+        ["CurrencyType"] = 0,
+        ["Offers"] = {
+            {["Price"] = 50, ["Offer"] = {{["CardName"] = "Inferno Wand", ["Count"] = 1}}},
+            {["Price"] = 100, ["Offer"] = {{["CardName"] = "Duplicate Inscription", ["Count"] = 1}}},
+            {["Price"] = 300, ["Offer"] = {{["CardName"] = "Student's Tome", ["Count"] = 1}}},
+            {["Price"] = 850, ["Offer"] = {{["CardName"] = "Conjure Rune of Fortitude", ["Count"] = 1}}}
+        }
+    },
+    ["newbie_multishop"] = {
+        ["Currency"] = "Gold",
+        ["CurrencyType"] = 0,
+        ["Offers"] = {
+            {["Price"] = 60, ["Offer"] = {{["CardName"] = "Antidote", ["Count"] = 1}}},
+            {["Price"] = 60, ["Offer"] = {{["CardName"] = "Smack", ["Count"] = 1}}},
+            {["Price"] = 60, ["Offer"] = {{["CardName"] = "Backhand Smack", ["Count"] = 1}}},
+            {["Price"] = 60, ["Offer"] = {{["CardName"] = "Uppercut", ["Count"] = 1}}},
+            {["Price"] = 65, ["Offer"] = {{["CardName"] = "Ember", ["Count"] = 1}}},
+            {["Price"] = 65, ["Offer"] = {{["CardName"] = "Ant Hill", ["Count"] = 1}}},
+            {["Price"] = 75, ["Offer"] = {{["CardName"] = "Wooden Sword", ["Count"] = 1}}},
+            {["Price"] = 100, ["Offer"] = {{["CardName"] = "Scimitar", ["Count"] = 1}}},
+            {["Price"] = 100, ["Offer"] = {{["CardName"] = "Defense Buff", ["Count"] = 1}}},
+            {["Price"] = 100, ["Offer"] = {{["CardName"] = "Attack Debuff", ["Count"] = 1}}}
+        }
+    },
+    ["snow_multishop"] = {
+        ["Currency"] = "Gold",
+        ["CurrencyType"] = 0,
+        ["Offers"] = {
+            {
+                ["Price"] = 150,
+                ["Offer"] = {
+                    {["CardName"] = "Spell Backfire", ["Count"] = 2},
+                    {["CardName"] = "Stupify", ["Count"] = 1}
+                }
+            },
+            {
+                ["Price"] = 250,
+                ["Offer"] = {
+                    {["CardName"] = "Beauty Sleep", ["Count"] = 2},
+                    {["CardName"] = "Snowball Throw", ["Count"] = 1}
+                }
+            },
+            {["Price"] = 300, ["Offer"] = {{["CardName"] = "Icicle Stab", ["Count"] = 1}}},
+            {
+                ["Price"] = 350,
+                ["Offer"] = {{["CardName"] = "Scimitar", ["Count"] = 1}, {["CardName"] = "Antidote", ["Count"] = 2}}
+            },
+            {
+                ["Price"] = 480,
+                ["Offer"] = {{["CardName"] = "Ice Breath", ["Count"] = 1}, {["CardName"] = "Dash", ["Count"] = 2}}
+            },
+            {
+                ["Price"] = 700,
+                ["Offer"] = {
+                    {["CardName"] = "Bear Trap", ["Count"] = 1},
+                    {["CardName"] = "Rusty Knife", ["Count"] = 2},
+                    {["CardName"] = "Uppercut", ["Count"] = 3}
+                }
+            },
+            {
+                ["Price"] = 1000,
+                ["Offer"] = {
+                    {["CardName"] = "Aura Pierce", ["Count"] = 2},
+                    {["CardName"] = "Afternoon Delight", ["Count"] = 3}
+                }
+            },
+            {
+                ["Price"] = 1500,
+                ["Offer"] = {
+                    {["CardName"] = "Blinding Flash", ["Count"] = 1},
+                    {["CardName"] = "Karaoke", ["Count"] = 2},
+                    {["CardName"] = "Tranquilizer", ["Count"] = 2},
+                    {["CardName"] = "Beauty Sleep", ["Count"] = 5}
+                }
+            },
+            {
+                ["Price"] = 2000,
+                ["Offer"] = {{["CardName"] = "Meditation", ["Count"] = 1}, {["CardName"] = "Sugar Rush", ["Count"] = 2}}
+            },
+            {["Price"] = 4000, ["Offer"] = {{["ItemType"] = 94, ["Count"] = 1}}},
+            {["Price"] = 4000, ["Offer"] = {{["ItemType"] = 95, ["Count"] = 1}}}
+        }
+    },
+    ["lean_multishop"] = {
+        ["Currency"] = "Gold",
+        ["CurrencyType"] = 0,
+        ["Offers"] = {{["Price"] = 150, ["Offer"] = {{["CardName"] = "Mystery Elixir", ["Count"] = 1}}}}
+    }
+}
