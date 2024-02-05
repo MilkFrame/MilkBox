5a2b197af67e464b65eea503b04414ff782b6231
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-06 00:42:24 +1100

    Create Changelogs.lua

---
+++ GameData/Hexaria [913614076]/Changelogs.lua
@@ -0,0 +1,26 @@
+data = {
+    ["Notes"] = [[	
+October 11th, 2023.
+-- Kapak is here --
+
+Kapak is back!! You can find the Kapak portal in the Nether Hub.
+
+NEW=
+- [Haunted Jab]
+- [Spirit Flame] ;Reskin
+- 2 new [Haunted Knight] colors
+- [Trick or Treat Bag(2023]
+
+CHANGES=
+- Targets with [Freeze] and [Frostbite] now take half of the Frostbite's damage as DoT.
+- Retired the old [Haunted Knight] colors and replaced them with the new colors.
+- [Halloween Spookifier] should now correctly give options to reskin the following; Shield Bash, Lantern Throw, Dynamite, Fireball, Blue Elixir, Scimitar, Arcane Blast, Graveyard, Zealot's Manuscript, and Meteor Strike.
+- Updated the King Kandy card shop.
+- [World Tear] has a new updated pool of mobs. ;they were randomized ;)
+- [Avenged PumpkinFolk] No longer trades you [Hellfire], will now trade you [Spirit Flame] instead.
+- Other bugfixes and changes
+- Join our offical Discord server to see Balance Changes
+]],
+    ["UpdateVersion"] = "1.05",
+    ["UpdateDate"] = "October 2023"
+}
