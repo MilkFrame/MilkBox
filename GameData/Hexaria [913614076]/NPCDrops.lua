c17b5ea17f987f8ffc2c1071c6e2466e1c600dca
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-06 00:50:39 +1100

    Create NPCDrops.lua

---
+++ GameData/Hexaria [913614076]/NPCDrops.lua
@@ -0,0 +1,284 @@
+data = {
+    {
+        ["Cards"] = {
+            ["Bow and Arrow"] = 0.02,
+            ["Dash"] = 0.02,
+            ["Wooden Club Slam"] = 0.03,
+            ["Bear Trap"] = 0.01,
+            ["Rusty Knife"] = 0.03
+        },
+        ["Items"] = {0.03}
+    },
+    {
+        ["Cards"] = {["Venomshank"] = 0.02, ["Poison Dagger"] = 0.08, ["Toxic Knife"] = 0.04},
+        ["Items"] = {0.06, {1, 0.9, 0.8, 0.5, 0.3, 0.1}, 0.12}
+    },
+    {
+        ["Cards"] = {
+            ["Cursed Spot"] = 0.2,
+            ["Shadow Trade"] = 0.1,
+            ["Soul Blast"] = 0.04,
+            ["Soul Missile"] = 0.1,
+            ["Earth Totem"] = 0.08,
+            ["Tremor"] = 0.02
+        },
+        ["Items"] = {0.06, {1, 0.99, 0.89, 0.8, 0.77, 0.63, 0.5, 0.4, 0.3, 0.1}}
+    },
+    {["Items"] = {{1, 0.6, 0.5, 0.2}, {1, 1, 1, 1, 0.7, 0.6, 0.5, 0.3}}},
+    {["Cards"] = {["Dynamite"] = 0.03, ["Tripwire"] = 0.04, ["Sharp Instincts"] = 0.06}},
+    {["Cards"] = {["Grave Rave"] = 0.05, ["Wild Roar"] = 0.05}, ["Items"] = {0.15}},
+    {
+        ["Cards"] = {
+            ["Bow and Arrow"] = 0.04,
+            ["Rock Throw"] = 0.3,
+            ["Dash"] = 0.04,
+            ["Wooden Club Slam"] = 0.06,
+            ["Bear Trap"] = 0.02,
+            ["Rusty Knife"] = 0.06
+        },
+        ["Items"] = {{0.7, 0.5, 0.4, 0.1}}
+    },
+    {["Cards"] = {["Enlighten"] = 0.08}, ["Items"] = {0.01}},
+    {
+        ["Cards"] = {
+            ["Grave Rave"] = 0.01,
+            ["Wild Roar"] = 0.01,
+            ["Toxic Knife"] = 0.01,
+            ["Scimitar"] = 0.01,
+            ["Bow and Arrow"] = 0.01,
+            ["Sourbloom Flask"] = 0.05,
+            ["Zombify"] = 0.05,
+            ["Rusty Knife"] = 0.01,
+            ["Shadow Trade"] = 0.01,
+            ["Wooden Club Slam"] = 0.01,
+            ["Rock Smash"] = 0.01,
+            ["Soul Blast"] = 0.01,
+            ["Soul Missile"] = 0.01,
+            ["Poison Dagger"] = 0.01,
+            ["Iron Broadsword"] = 0.01
+        },
+        ["Items"] = {0.15}
+    },
+    {["Items"] = {0.02}},
+    {["Items"] = {0.2, 1}},
+    {["Cards"] = {["Piercing Gaze"] = 0.1}, ["Items"] = {{1, 1, 1, 0.5, 0.3}}},
+    {["Cards"] = {["Duplicate Inscription"] = 0.05}},
+    {["Items"] = {{1, 0.6, 0.3}}},
+    {["Cards"] = {["Snowman"] = 0.02}, ["Items"] = {{1, 0.5}, 0.35}},
+    {["Cards"] = {["Egg Relic"] = {1, 1}}},
+    {
+        ["Cards"] = {["Dynamite"] = 0.06, ["Tripwire"] = 0.08, ["Sharp Instincts"] = 0.12},
+        ["Items"] = {{0.7, 0.5, 0.4, 0.1}}
+    },
+    {["Cards"] = {["Ember"] = 0.1, ["Inferno Wand"] = 0.08, ["Heatwave"] = 0.04, ["Blaze Rune"] = 0.05}},
+    {["Cards"] = {["Graveyard"] = 0.15, ["Decaying Ray"] = 0.07}},
+    {["Cards"] = {["Noxious Gas"] = 0.25, ["Wild Roar"] = 0.1}, ["Items"] = {{0.75, 0.25, 0.1}, {0.01, 0.01, 0.01}}},
+    {["Cards"] = {["Agony Seal"] = 0.01, ["Demon Lore"] = 0.05}, ["Items"] = {0.02, 0.06, 0.04, 0.03, 0.02, 1, 0.08}},
+    {["Cards"] = {["Extinguish"] = 0.2}, ["Items"] = {0.3}},
+    {["Cards"] = {["Vampire's Claw"] = 0.3, ["Psychic Wave"] = 0.1}},
+    {["Items"] = {{0.5, 0.2}}},
+    {
+        ["Cards"] = {
+            ["Fireball"] = 0.07,
+            ["Arcane Ward"] = 0.04,
+            ["Lightning Bolt"] = 0.05,
+            ["Arcane Transmutation"] = 0.03,
+            ["Chain Lightning"] = 0.07,
+            ["Arcane Blast"] = 0.06
+        }
+    },
+    {["Cards"] = {["Extinguish"] = 0.2}, ["Items"] = {0.05}},
+    {["Cards"] = {["Rock Smash"] = 0.02}},
+    {
+        ["Cards"] = {
+            ["Fireball"] = 0.1,
+            ["Darksteel Longblade"] = 0.01,
+            ["Chain Lightning"] = 0.05,
+            ["Lightning Bolt"] = 0.05
+        },
+        ["Items"] = {0.01}
+    },
+    {["Cards"] = {["Protection Seal"] = 0.04}, ["Items"] = {0.08, 0.03, 0.11}},
+    {["Cards"] = {["Karaoke"] = 0.3}, ["Items"] = {0.1}},
+    {["Cards"] = {["Claw Impale"] = 0.05}},
+    {["Cards"] = {["Extinguish"] = 0.2}, ["Items"] = {0.05}},
+    {["Cards"] = {["Claw Impale"] = 0.1}, ["Items"] = {0.25}},
+    {["Items"] = {0.03}},
+    {["Cards"] = {["Egg Relic"] = {1, 1, 1, 1, 1}}},
+    {["Cards"] = {["Fireball"] = 0.3, ["Singe"] = 0.1}},
+    {["Cards"] = {["Bow and Arrow"] = 0.02}, ["Items"] = {0.01}},
+    {["Cards"] = {["Heartwarming Arrow"] = 0.01}, ["Items"] = {{1}, {1, 0.5, 0.2}}},
+    {["Cards"] = {["Egg Relic"] = 0.3}},
+    {["Items"] = {1}},
+    {["Items"] = {0.3}},
+    {["Cards"] = {["Papercut"] = 0.2, ["Stargazer's Eye"] = 0.01}},
+    {["Cards"] = {["Fireball"] = 0.3, ["Singe"] = 0.1}},
+    {["Cards"] = {["Rusty Knife"] = 0.02, ["Rusty Nails"] = 0.1}, ["Items"] = {0.01}},
+    {["Cards"] = {["Cursed Spot"] = 0.1}},
+    {["Cards"] = {["Pumpkinfool's Zap"] = 0.05, ["Pumpkinfool's Heal"] = 0.05}, ["Items"] = {1, 0.06}},
+    {["Cards"] = {["Cold Feet"] = 0.05, ["Hot Cocoa"] = 0.05}, ["Items"] = {0.2}},
+    {["Cards"] = {["Fireball"] = 0.05, ["Chain Lightning"] = 0.03, ["Lightning Bolt"] = 0.03}},
+    {["Items"] = {{1, 0.2}, {1, 0.7, 0.6, 0.5, 0.3}}},
+    {["Cards"] = {["Extinguish"] = 0.2}, ["Items"] = {0.05}},
+    {["Cards"] = {["Cursed Spot"] = 0.02}, ["Items"] = {{1, 0.85, 0.7, 0.62, 0.5, 0.24, 0.1}}},
+    {["Cards"] = {["Royal Claymore"] = 0.2}, ["Items"] = {0.3}},
+    {["Items"] = {0.35, 0.15}},
+    {["Cards"] = {["Fireball"] = 0.03, ["Chain Lightning"] = 0.01, ["Lightning Bolt"] = 0.01}},
+    {["Cards"] = {["Rock Throw"] = 0.25}},
+    {["Cards"] = {["Karaoke"] = 0.5}, ["Items"] = {0.2}},
+    {["Cards"] = {["Karaoke"] = 0.2}, ["Items"] = {0.1}},
+    {
+        ["Cards"] = {
+            ["Cursed Spot"] = 0.1,
+            ["Shadow Trade"] = 0.05,
+            ["Soul Blast"] = 0.02,
+            ["Soul Missile"] = 0.05,
+            ["Earth Totem"] = 0.04,
+            ["Tremor"] = 0.01
+        },
+        ["Items"] = {0.03}
+    },
+    {["Cards"] = {["Trick or Treat"] = 0.02}, ["Items"] = {0.01, 0.01, 0.02, 0.02, 0.4, {1, 0.15, 0.05}}},
+    {["Cards"] = {["Egg Relic"] = 1}},
+    {["Cards"] = {["Jack o' Lantern Throw"] = 0.02}, ["Items"] = {0.2}},
+    {["Items"] = {0.2}},
+    {["Cards"] = {["Egg Relic"] = {1, 1, 1, 1, 1}}},
+    {
+        ["Cards"] = {
+            ["Fireball"] = 0.13,
+            ["Soothing Flame"] = 0.14,
+            ["Ignition"] = 0.15,
+            ["Ignite"] = 0.6,
+            ["Micro Meteors"] = 0.07,
+            ["Overheat"] = 0.07,
+            ["Meteor Strike"] = 0.06,
+            ["Invoke Fire Spirit"] = 0.11
+        },
+        ["Items"] = {0.08, 0.04}
+    },
+    {["Cards"] = {["Mushroom Growth"] = 0.05, ["Shadow Trade"] = 0.05}, ["Items"] = {0.15}},
+    {["Cards"] = {["Rock Smash"] = 1}, ["Items"] = {0.6}},
+    {["Cards"] = {["Soul Blast"] = 0.4}, ["Items"] = {0.6}},
+    {},
+    {["Cards"] = {["Burning Pledge"] = 0.02}, ["Items"] = {0.2}},
+    {["Cards"] = {["Soothing Flame"] = 0.05, ["Inferno Blast"] = 0.02, ["Heatwave"] = 0.06, ["Ignite"] = 0.1}},
+    {["Cards"] = {["Trick or Treat"] = 0.02}, ["Items"] = {0.01, {0.04, 0.03}, 0.2}},
+    {["Cards"] = {["Vampire's Claw"] = 0.03, ["Vampiric Brew"] = 0.05, ["Vampiric Mindflay"] = 0.01}, ["Items"] = {0.1}},
+    {
+        ["Cards"] = {
+            ["Grave Rave"] = 0.02,
+            ["Catalyst"] = 0.05,
+            ["Perish"] = 0.05,
+            ["Vigor Mortis"] = 0.05,
+            ["Necromancy"] = 0.02
+        }
+    },
+    {["Cards"] = {["Panic"] = 0.6}, ["Items"] = {0.06}},
+    {
+        ["Cards"] = {["Egg Relic"] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}},
+        ["Items"] = {0.04}
+    },
+    {["Cards"] = {["Invoke Fire Spirit"] = 0.1, ["Fireball"] = 0.2}, ["Items"] = {0.03}},
+    {["Cards"] = {["Eternal Flame"] = 0.01}},
+    {["Cards"] = {["Dizzy Swing"] = 0.05, ["Seafarer's Tonic"] = 0.05}, ["Items"] = {0.15}},
+    {["Cards"] = {["Cambrian Shield"] = 0.02, ["Shield Bash"] = 0.1}, ["Items"] = {0.08}},
+    {["Cards"] = {["Heartseeker"] = 0.02}, ["Items"] = {0.01}},
+    {["Cards"] = {["Soul Missile"] = 0.01, ["Rock Smash"] = 0.02, ["Shadow Trade"] = 0.01}},
+    {["Cards"] = {["High Cognition"] = 0.01, ["Ramparts"] = 0.01}, ["Items"] = {0.01}},
+    {["Cards"] = {["Fireball"] = 0.2}, ["Items"] = {0.3}},
+    {["Items"] = {{1, 0.5}}},
+    {["Cards"] = {["Karaoke"] = 0.4, ["Haunted Scythe"] = 1}, ["Items"] = {0.3}},
+    {["Cards"] = {["Karaoke"] = 1, ["Rapier Thrust"] = 1}, ["Items"] = {{1, 1, 0.6, 0.2, 0.1}}},
+    {["Cards"] = {["Snowball"] = 0.05, ["Enlighten"] = 0.08, ["Arcane Blast"] = 0.05}, ["Items"] = {}},
+    {["Cards"] = {["The Ole' One-Two"] = 0.06}, ["Items"] = {0.01}},
+    {["Cards"] = {["Pumpkin Bomb"] = 0.02}, ["Items"] = {0.2}},
+    {["Cards"] = {["Fireball"] = 0.1}, ["Items"] = {0.01}},
+    {
+        ["Cards"] = {
+            ["Inferno Blast"] = 0.04,
+            ["Ignition"] = 0.02,
+            ["Overheat"] = 0.03,
+            ["Invoke Fire Spirit"] = 0.01,
+            ["Micro Meteors"] = 0.03
+        }
+    },
+    {["Cards"] = {["Chain Link"] = 0.4}, ["Items"] = {0.2}},
+    {["Cards"] = {["Burning Pledge"] = 0.01}},
+    {["Items"] = {{1, 0.15, 0.07}}},
+    {["Cards"] = {["The Ole' One-Two"] = 0.1}, ["Items"] = {0.01}},
+    {["Cards"] = {["Ice Tipped Arrow"] = 0.07, ["Blue Elixir"] = 0.04, ["Dynamite"] = 0.04}, ["Items"] = {}},
+    {["Cards"] = {["Scimitar"] = 0.05}, ["Items"] = {0.15}},
+    {["Cards"] = {["Heartseeker"] = 0.4}, ["Items"] = {0.1}},
+    {["Items"] = {{1, 0.3}}},
+    {["Cards"] = {["Smokescreen"] = 0.08}, ["Items"] = {0.01}},
+    {["Cards"] = {["Cursed Spot"] = 0.02}, ["Items"] = {{1, 0.85, 0.7, 0.62, 0.5, 0.24, 0.1}}},
+    {["Items"] = {0.2, {1, 0.5}}},
+    {["Cards"] = {["Rock Throw"] = 1}, ["Items"] = {0.1}},
+    {["Cards"] = {["Regenerative Pulse"] = 0.12}, ["Items"] = {0.01}},
+    {["Cards"] = {["Karaoke"] = 0.1}, ["Items"] = {0.01}},
+    {["Cards"] = {["Karaoke"] = 0.7}, ["Items"] = {0.1}},
+    {["Cards"] = {["Venomshank"] = 0.01, ["Poison Dagger"] = 0.04, ["Toxic Knife"] = 0.02}, ["Items"] = {0.03, 0.06}},
+    {["Cards"] = {["Rock Smash"] = 0.02, ["Astral Surge"] = 0.03}, ["Items"] = {0.2}},
+    {["Items"] = {{0.5, 0.2}, {1, 1, 1, 0.7, 0.6, 0.5, 0.3}}},
+    {["Cards"] = {["Soul Blast"] = 0.5}, ["Items"] = {{1, 0.3}}},
+    {["Cards"] = {["Afternoon Delight"] = 0.05}, ["Items"] = {{1, 0.5, 0.25, 0.1}}},
+    {["Cards"] = {["Dash"] = 0.1, ["Shadow Trade"] = 0.01}, ["Items"] = {0.01}},
+    {["Cards"] = {["Rest and Recovery"] = 0.5}, ["Items"] = {0.15, {1, 0.2}}},
+    {["Cards"] = {["Ember"] = 0.1, ["Micro Meteors"] = 0.03, ["Heatwave"] = 0.05, ["Dancing Flame"] = 0.03}},
+    {["Cards"] = {["Flame Body"] = 0.06, ["Kindling Fury"] = 0.08, ["Burst Flare"] = 0.05}},
+    {["Cards"] = {["Manipulate the Eternal"] = 0.05, ["Eternal Cleansing"] = 0.07}, ["Items"] = {0.08}},
+    {["Cards"] = {["Vampire's Claw"] = 0.03, ["Vampiric Brew"] = 0.05}, ["Items"] = {}},
+    {["Cards"] = {["Reckless Frenzy"] = 0.03}},
+    {["Cards"] = {["Hunter's Mark"] = 0.07, ["Lantern Throw"] = 0.4, ["Chain Link"] = 0.15}},
+    {["Cards"] = {["Cursed Spot"] = 0.01}},
+    {["Cards"] = {["Piercing Gaze"] = 0.15, ["Obsessive Gaze"] = 0.1}, ["Items"] = {{1, 1, 0.5, 0.15}}},
+    {["Items"] = {0.02}},
+    {
+        ["Cards"] = {["Poison Dagger"] = 0.02, ["Rusty Knife"] = 0.05, ["Toxic Knife"] = 0.02},
+        ["Items"] = {0.2, 0.03, 0.01, 0.05, 0.02}
+    },
+    {["Cards"] = {["Vampire's Claw"] = 0.15, ["Vampiric Brew"] = 0.15}, ["Items"] = {0.01, 0.05}},
+    {["Cards"] = {["Dynamite"] = 0.05}, ["Items"] = {0.15}},
+    {["Cards"] = {["Bow and Arrow"] = 0.1, ["Blue Elixir"] = 0.1, ["Exploding Arrow"] = 0.03}, ["Items"] = {}},
+    {["Cards"] = {["Wisp"] = 1}},
+    {["Items"] = {0.01, {0.1}}},
+    {["Items"] = {{1, 0.2, 0.15}}},
+    {["Items"] = {0.5, 0.3}},
+    {
+        ["Cards"] = {
+            ["Rest and Recovery"] = 0.02,
+            ["Snowball Throw"] = 0.02,
+            ["Blue Elixir"] = 0.02,
+            ["Small Blessing"] = 0.02
+        },
+        ["Items"] = {}
+    },
+    {
+        ["Cards"] = {
+            ["Rune of Arcane Power"] = 0.05,
+            ["Conjure Rune of Fortitude"] = 0.01,
+            ["Arcane Ward"] = 0.04,
+            ["Meditation"] = 0.02
+        }
+    },
+    {
+        ["Cards"] = {
+            ["Zealot's Manuscript"] = 0.03,
+            ["Arcane Ward"] = 0.05,
+            ["Channel Arcane Barrage"] = 0.01,
+            ["Arcane Transmutation"] = 0.09,
+            ["Coffee"] = 0.05
+        }
+    },
+    {["Cards"] = {["Graveyard"] = 0.05, ["Royal Claymore"] = 0.03}, ["Items"] = {0.06}},
+    {["Cards"] = {["Heartwarming Arrow"] = 0.01}, ["Items"] = {{1}, {1, 0.5, 0.2}}},
+    {["Cards"] = {["Wild Roar"] = 0.06, ["Regrowth"] = 0.03, ["Tremor"] = 0.04}, ["Items"] = {0.05, 0.06, 0.4}},
+    {["Cards"] = {["Poison Dagger"] = 0.01}, ["Items"] = {0.01}},
+    {["Items"] = {0.03}},
+    {["Items"] = {{1, 0.55, 0.25, 0.05}}},
+    {["Cards"] = {["Wisp"] = 1}},
+    {["Cards"] = {["Rapier Thrust"] = 0.07, ["Iron Broadsword"] = 0.05}, ["Items"] = {0.05}},
+    {["Cards"] = {}, ["Items"] = {0.12, 1, 0.06}},
+    {["Cards"] = {["Strong Gust"] = 0.03}},
+    {["Items"] = {0.35}}
+}
