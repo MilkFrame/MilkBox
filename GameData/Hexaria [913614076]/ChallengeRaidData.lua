d50300620b89476a0ce7b621b24c6df6dc44499f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-06 00:41:36 +1100

    Create ChallengeRaidData.lua

---
+++ GameData/Hexaria [913614076]/ChallengeRaidData.lua
@@ -0,0 +1,63 @@
+data = {
+    ["Cambris.SwampCave"] = {
+        ["PlaceName"] = "Swamp Caverns",
+        ["Name"] = "Challenge.SwampCave",
+        ["ID"] = 14871876622,
+        ["Point"] = "BossCleared_MonstorsitySwampCave",
+        ["Description"] = [[The gravelord said the quality was better. The graveling said quantity was better and proceeded to fuse multiple bodies together.
+The zombies raised were only able to achieve a single goal with their combined physiques, to destroy.]],
+        ["Objective"] = "After 1 round [Zombified Monstrosity] has increased [Health]."
+    },
+    ["KishufBoss"] = {
+        ["PlaceName"] = "Kishuf's Curse",
+        ["Name"] = "Challenge.KishufBoss",
+        ["ID"] = 14847071414,
+        ["Point"] = "BossCleared_DraunKishuf",
+        ["Description"] = "The recently awakened Draun still has some Magisteel left from building the Golem and is using them to make mercenaries!",
+        ["Objective"] = "Every 3 rounds, a hostile [Draunforged Mercenary] will appear on a random available tile."
+    },
+    ["Arcovia.SnowvilleTower"] = {
+        ["PlaceName"] = "Snowvile Fortress",
+        ["Name"] = "Challenge.SnowvilleTower",
+        ["ID"] = 14847140012,
+        ["Point"] = "BossCleared_NicoleRinSnowville",
+        ["Description"] = "While Snowville is gradually put under her control, she looks at the Arcovian Knight she conjured from the staff given by The Birdman.",
+        ["Objective"] = "[Nicole Rin] fights alongside an [Arcovian Knight]. Some enemies have been removed from the raid."
+    },
+    ["Nether.DesecratedCliffs"] = {
+        ["PlaceName"] = "Cupid's Cliffs",
+        ["Name"] = "Challenge.DesecratedCliffs",
+        ["ID"] = 14847730221,
+        ["Point"] = "BossCleared_LovelostCliffs",
+        ["Description"] = [[His neverending love for Cupid has spread throughout the area, corrupting the island and feeding him energy.
+The nourishment received from this grants him increased capabilities.]],
+        ["Objective"] = [[After 1 round [Lovelost Mercenary] is [Freeze Immune] for 14 turns, gains a permanent +35% [Attack Up] and has increased [Health]. 
+[Channel Arcane Barrage], [Rubber Duck], [Staff of Azure] and [Astral Blessing] are banned.
+Card and Item drops are guaranteed.]]
+    },
+    ["Olbrek.HeadmastersOffice"] = {
+        ["PlaceName"] = "Roku's Office",
+        ["Name"] = "Challenge.HeadmastersOffice",
+        ["ID"] = 14847492709,
+        ["Point"] = "BossCleared_PyromancyRoku",
+        ["Description"] = [[While Roku is discussing what to do after the fire in the academy, an outsider barges into his office...
+He roars: "CONSIDER YOURSELF, E X P E L L E D".]],
+        ["Objective"] = "[Headmaster Roku] fights alongside 2 [Elite Pyromancers]."
+    },
+    ["Olbrek.SkeletonRaid"] = {
+        ["PlaceName"] = "Olbrek Catacombs",
+        ["Name"] = "Challenge.SkeletonRaid",
+        ["ID"] = 14847602493,
+        ["Point"] = "BossCleared_SkeletonKingCrypt",
+        ["Description"] = "The Dark Operative has turned his allies into Undead and used more advanced materials!",
+        ["Objective"] = "More elite enemies will appear. [Skeleton King] fights alongside 3 [Dark Skeletons]."
+    },
+    ["Olbrek.SedrianRaid"] = {
+        ["PlaceName"] = "Arcane Mentor Camp",
+        ["Name"] = "Challenge.SedrianRaid",
+        ["ID"] = 14846980456,
+        ["Point"] = "BossCleared_SedrianMentorCamp",
+        ["Description"] = "Sedrian challenges you to to a more difficult fight!",
+        ["Objective"] = "[Sedrian, The Arcane Mentor] fights alongside an [Arcane Conjurer] and 2 [Elite Mages]."
+    }
+}
