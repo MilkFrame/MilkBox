8b41b5baccfb2b5d9a199d85cf96dcb3ab26126d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-06 00:53:10 +1100

    Create Purchases.lua

---
+++ GameData/Hexaria [913614076]/Purchases.lua
@@ -0,0 +1,40 @@
+data = {
+    ["Enchant"] = {["EnchantPack"] = {["Cost"] = 20, ["Currency"] = "Hexons"}},
+    ["Hexons"] = {
+        ["Tier5"] = 138453447,
+        ["Tier2"] = 138328867,
+        ["Tier1"] = 138328752,
+        ["Tier3"] = 138329073,
+        ["Tier4"] = 138329277
+    },
+    ["Items"] = {
+        ["clothes_bandit_leader"] = {["Hexons"] = {["Cost"] = 750, ["Currency"] = "Hexons"}},
+        ["clothes_cambrian_guard"] = {["Hexons"] = {["Cost"] = 500, ["Currency"] = "Hexons"}},
+        ["teleport_stone"] = {["Hexons"] = {["Cost"] = 50, ["Currency"] = "Hexons"}},
+        ["server_shout"] = {["Hexons"] = {["Cost"] = 25, ["Currency"] = "Hexons"}},
+        ["health_potion"] = {["Hexons"] = {["Cost"] = 25, ["Currency"] = "Hexons"}},
+        ["clothes_simple_mage"] = {["Gold"] = {["Cost"] = 1750, ["Currency"] = "Gold"}},
+        ["clothes_bone_general"] = {["Hexons"] = {["Cost"] = 750, ["Currency"] = "Hexons"}},
+        ["clothes_adventurer_clothes"] = {["Gold"] = {["Cost"] = 1500, ["Currency"] = "Gold"}},
+        ["clothes_adept_mage"] = {["Hexons"] = {["Cost"] = 500, ["Currency"] = "Hexons"}},
+        ["clothes_aurmire_spellcaster"] = {["Hexons"] = {["Cost"] = 500, ["Currency"] = "Hexons"}},
+        ["healing_shrine_shard"] = {["Hexons"] = {["Cost"] = 50, ["Currency"] = "Hexons"}},
+        ["clothes_basic_royal"] = {["Hexons"] = {["Cost"] = 3000, ["Currency"] = "Hexons"}},
+        ["clothes_sampson"] = {["Hexons"] = {["Cost"] = 15000, ["Currency"] = "Hexons"}},
+        ["epic_sigil_of_courage"] = {["Hexons"] = {["Cost"] = 50, ["Currency"] = "Hexons"}}
+    },
+    ["Membership"] = 988539601,
+    ["CardPacks"] = {
+        ["Mystic Wisdom"] = {["Cost"] = 250, ["Currency"] = "Gold"},
+        ["Blazing Inferno"] = {["Cost"] = 350, ["Currency"] = "Gold"},
+        ["Genesis"] = {["Cost"] = 150, ["Currency"] = "Gold"}
+    },
+    ["Boosts"] = {
+        ["Speed"] = {["Cost"] = 75, ["Currency"] = "Hexons"},
+        ["Heal"] = {["Cost"] = 75, ["Currency"] = "Hexons"},
+        ["Experience"] = {["Cost"] = 75, ["Currency"] = "Hexons"},
+        ["Gold"] = {["Cost"] = 75, ["Currency"] = "Hexons"}
+    },
+    ["TimedDeals"] = {["DailyDeal"] = 137962961, ["WeeklyDeal"] = 137963230, ["MonthlyDeal"] = 137963288},
+    ["StarterPack"] = 167217652
+}
