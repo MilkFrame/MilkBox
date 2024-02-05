2d5cc5aa95b9be264f50a10141348e3c66312f8e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-06 00:40:56 +1100

    Create CardFestivizer.lua

---
+++ GameData/Hexaria [913614076]/CardFestivizer.lua
@@ -0,0 +1,112 @@
+data = {
+    ["Dragon Crystal"] = {},
+    ["Halloween Spookifier"] = {
+        ["Haunted Hallowed Blast trade"] = {
+            ["Get"] = {{["CardName"] = "Haunted Hallowed Blast", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Arcane Blast", ["Count"] = 1}}
+        },
+        ["Pumpkin Bomb trade"] = {
+            ["Get"] = {{["CardName"] = "Pumpkin Bomb", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Dynamite", ["Count"] = 1}}
+        },
+        ["Haunted Pumpkin trade"] = {
+            ["Get"] = {{["CardName"] = "Haunted Pumpkin", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Graveyard", ["Count"] = 1}}
+        },
+        ["Dullhans Calamity trade"] = {
+            ["Get"] = {{["CardName"] = "Dullahan's Calamity", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Meteor Strike", ["Count"] = 1}}
+        },
+        ["Sanguine Wisdom trade"] = {
+            ["Get"] = {{["CardName"] = "Sanguine Wisdom", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Zealot's Manuscript", ["Count"] = 1}}
+        },
+        ["Haunted Scythe trade"] = {
+            ["Get"] = {{["CardName"] = "Haunted Scythe", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Scimitar", ["Count"] = 1}}
+        },
+        ["Pumpkin Bomb Fling trade"] = {
+            ["Get"] = {{["CardName"] = "Pumpkin Bomb Fling", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Fireball", ["Count"] = 1}}
+        },
+        ["Jack o Lantern trade"] = {
+            ["Get"] = {{["CardName"] = "Jack o' Lantern Throw", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Lantern Throw", ["Count"] = 1}}
+        },
+        ["Haunted Pumpkin Elixir trade"] = {
+            ["Get"] = {{["CardName"] = "Haunted Pumpkin Elixir", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Blue Elixir", ["Count"] = 1}}
+        },
+        ["Haunted Tombstone Bash trade"] = {
+            ["Get"] = {{["CardName"] = "Haunted Tombstone Bash", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Shield Bash", ["Count"] = 1}}
+        }
+    },
+    ["Valentine's Festivizer"] = {
+        ["Cupid's Mercenary trade"] = {
+            ["Get"] = {{["CardName"] = "Cupid's Mercenary", ["Count"] = 1}},
+            ["Give"] = {
+                {["CardName"] = "Mercenary", ["Count"] = 1},
+                {["CardName"] = "Festive Mercenary", ["Count"] = 1}
+            }
+        },
+        ["Love Bow"] = {
+            ["Get"] = {{["CardName"] = "Yew Lovebow", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Yew Longbow", ["Count"] = 1}}
+        }
+    },
+    ["Holiday Festivizer"] = {
+        ["Festive Bardiche Chop trade"] = {
+            ["Get"] = {{["CardName"] = "Festive Bardiche Pop", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Bardiche Chop", ["Count"] = 1}}
+        },
+        ["Festive Wooden Sword trade"] = {
+            ["Get"] = {{["CardName"] = "Festive Toy Sword", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Wooden Sword", ["Count"] = 1}}
+        },
+        ["Festive VS trade"] = {
+            ["Get"] = {{["CardName"] = "Festive Candyshank", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Venomshank", ["Count"] = 1}}
+        },
+        ["Festive Pie Throw trade"] = {
+            ["Get"] = {{["CardName"] = "Festive Pudding Throw", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Pie Throw", ["Count"] = 1}}
+        },
+        ["Festive Wooden Club trade"] = {
+            ["Get"] = {{["CardName"] = "Festive Gift Slam", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Wooden Club Slam", ["Count"] = 1}}
+        },
+        ["Festive Scimitar trade"] = {
+            ["Get"] = {{["CardName"] = "Festive Scimi-Star", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Scimitar", ["Count"] = 1}, {["CardName"] = "Rune Scimitar", ["Count"] = 1}}
+        },
+        ["Festive Core trade"] = {
+            ["Get"] = {{["CardName"] = "Festive Merry Infliction", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Core Infliction", ["Count"] = 1}}
+        },
+        ["Festive Hot Cocoa trade"] = {
+            ["Get"] = {{["CardName"] = "Festive Loaded Cocoa", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Hot Cocoa", ["Count"] = 1}}
+        },
+        ["Festive Chopper Crash trade"] = {
+            ["Get"] = {{["CardName"] = "Festive Tree Crash"}},
+            ["Give"] = {{["CardName"] = "Chopper Crash", ["Count"] = 1}}
+        },
+        ["Festive DS trade"] = {
+            ["Get"] = {{["CardName"] = "Festive Darkwood Pineblade", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Darksteel Longblade", ["Count"] = 1}}
+        },
+        ["Festive Azure Staff trade"] = {
+            ["Get"] = {{["CardName"] = "Festive Staff of Saint Nick", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Staff of Azure", ["Count"] = 1}}
+        },
+        ["Festive Toxic Knife trade"] = {
+            ["Get"] = {{["CardName"] = "Festive Peppermint Poke"}},
+            ["Give"] = {{["CardName"] = "Toxic Knife", ["Count"] = 1}}
+        },
+        ["Festive Shield Bash trade"] = {
+            ["Get"] = {{["CardName"] = "Festive Cookie Bash", ["Count"] = 1}},
+            ["Give"] = {{["CardName"] = "Shield Bash", ["Count"] = 1}}
+        }
+    }
+}
