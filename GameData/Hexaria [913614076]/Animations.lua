e50ed398228107677179b63ccc56d47ed3bc6682
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-06 00:36:23 +1100

    Create Animations.lua

---
+++ GameData/Hexaria [913614076]/Animations.lua
@@ -0,0 +1,252 @@
+data = {
+    ["Classes"] = {
+        ["Warrior"] = {
+            ["CombatIdle"] = "rbxassetid://507767714",
+            ["Run"] = "rbxassetid://507767714",
+            ["WorldIdle"] = "rbxassetid://507767714"
+        },
+        ["Mage"] = {
+            ["CombatIdle"] = "rbxassetid://507767714",
+            ["Run"] = "rbxassetid://507767714",
+            ["WorldIdle"] = "rbxassetid://507767714"
+        },
+        ["Hunter"] = {
+            ["CombatIdle"] = "rbxassetid://507767714",
+            ["Run"] = "rbxassetid://507767714",
+            ["WorldIdle"] = "rbxassetid://507767714"
+        }
+    },
+    ["Idle"] = "rbxassetid://938226757",
+    ["Combatants"] = {
+        ["_default"] = {
+            ["Status"] = {
+                ["SleepStart"] = "rbxassetid://911788771",
+                ["SleepIdle"] = "rbxassetid://911790090",
+                ["SleepEnd"] = "rbxassetid://911791550",
+                ["StunIdle"] = "rbxassetid://911788012"
+            },
+            ["TakeDamage"] = "rbxassetid://924240611",
+            ["Draw"] = "rbxassetid://897330925",
+            ["Death"] = "rbxassetid://950050630"
+        },
+        ["Mage"] = {
+            ["CombatIdle"] = "rbxassetid://902070676",
+            ["WorldIdle"] = "rbxassetid://897337732",
+            ["Movement"] = "rbxassetid://890801706"
+        },
+        ["Hunter"] = {["Idle"] = "rbxassetid://912093318"}
+    },
+    ["Conversation"] = {
+        ["Gesticulate"] = "rbxassetid://930064538",
+        ["Shrug"] = "rbxassetid://930063639",
+        ["Nod"] = "rbxassetid://930069537",
+        ["ShakingHead"] = "rbxassetid://930061973",
+        ["Laugh"] = "rbxassetid://930060591"
+    },
+    ["Creatures"] = {
+        ["Host"] = {
+            ["Idle"] = "rbxassetid://11796153119",
+            ["AssistantDie"] = "rbxassetid://11800271609",
+            ["AssistantIdle"] = "rbxassetid://11800184465",
+            ["Skill"] = "rbxassetid://11796157444",
+            ["Revive"] = "rbxassetid://11796155632",
+            ["Attack"] = "rbxassetid://11796140107"
+        },
+        ["EyeofCupid"] = {
+            ["FastFire"] = "rbxassetid://12330199264",
+            ["Idle"] = "rbxassetid://12330208698",
+            ["Death"] = "rbxassetid://12330203557",
+            ["SmallCharge"] = "rbxassetid://12330235426",
+            ["WingCharge"] = "rbxassetid://12330221130",
+            ["LongCharge"] = "rbxassetid://12330226702",
+            ["Move"] = "rbxassetid://12330190722"
+        },
+        ["Cannon"] = {
+            ["CannonIdle"] = "rbxassetid://14399200334",
+            ["CannonFire"] = "rbxassetid://14398801998",
+            ["CannonDamage"] = "rbxassetid://14399203394",
+            ["CannonMove"] = "rbxassetid://14398816609",
+            ["CannonAim"] = "rbxassetid://14398798896"
+        },
+        ["MusicMen"] = {
+            ["SpectreDeath"] = "rbxassetid://11180212399",
+            ["ActorIdle"] = "rbxassetid://11154967188",
+            ["WDeath"] = "rbxassetid://12015811630",
+            ["SingerDeath"] = "rbxassetid://11154968492",
+            ["DeathKnees"] = "rbxassetid://11000273336",
+            ["MagusAttack"] = "rbxassetid://11588361134",
+            ["SaxophoneDeath"] = "rbxassetid://10974334683",
+            ["TriangleDeath"] = "rbxassetid://10974340081",
+            ["WAttack"] = "rbxassetid://12015809095",
+            ["ActorDeath"] = "rbxassetid://11154965172",
+            ["zIdle"] = "rbxassetid://13516136937",
+            ["Idle"] = "rbxassetid://10921101664",
+            ["SaxophoneIdle"] = "rbxassetid://10974336990",
+            ["SpectreMove"] = "rbxassetid://11180131681",
+            ["SpectreIdle"] = "rbxassetid://11180053571",
+            ["WIdle"] = "rbxassetid://12015813104",
+            ["WSkill"] = "rbxassetid://12015814785",
+            ["MagusDeath"] = "rbxassetid://11588358387",
+            ["TriangleIdle"] = "rbxassetid://10974344140",
+            ["SpectreTakeDamage"] = "rbxassetid://11180098838",
+            ["SingerIdle"] = "rbxassetid://11154979335",
+            ["MagusSteal"] = "rbxassetid://11588363892",
+            ["ViolinDeath"] = "rbxassetid://10974346516",
+            ["ViolinIdle"] = "rbxassetid://10974348643",
+            ["Walk"] = "rbxassetid://10921111375"
+        },
+        ["Dummy"] = {
+            ["WheelIdle"] = "rbxassetid://11869287170",
+            ["WheelDie"] = "rbxassetid://11869284523",
+            ["WheelAttack"] = "rbxassetid://11865452716",
+            ["WheelMove"] = "rbxassetid://11749271586"
+        },
+        ["EtherealElemental"] = {
+            ["ChargedAttack"] = "rbxassetid://8194368187",
+            ["Heartseeker"] = "rbxassetid://3470142685",
+            ["Claw"] = "rbxassetid://8194275345",
+            ["Attack"] = "rbxassetid://3457275706"
+        },
+        ["EyeMass"] = {
+            ["AttackFastCharge"] = "rbxassetid://14874901348",
+            ["AttackSmooth"] = "rbxassetid://14874921547",
+            ["AttackStutter"] = "rbxassetid://14874882447",
+            ["AttackElastic"] = "rbxassetid://14874871944",
+            ["Idle"] = "rbxassetid://14874911323",
+            ["Move"] = "rbxassetid://14874908035",
+            ["AttackSlow"] = "rbxassetid://14874879952",
+            ["AttackStutterCharge"] = "rbxassetid://14874926554",
+            ["Protect"] = "rbxassetid://14874914477",
+            ["Death"] = "rbxassetid://14874886665",
+            ["WingAttack"] = "rbxassetid://14890552960",
+            ["AttackFast"] = "rbxassetid://14874875771",
+            ["Revive"] = "rbxassetid://14879863352",
+            ["Attack"] = "rbxassetid://14874866865"
+        },
+        ["KishufGolem"] = {
+            ["BrutalStrike"] = "rbxassetid://3580021951",
+            ["Idle"] = "rbxassetid://3577422537",
+            ["ArmCannon"] = "rbxassetid://3581279784",
+            ["Energize"] = "rbxassetid://3577898342",
+            ["Spellcast"] = "rbxassetid://3580051689",
+            ["Deathbeam"] = "rbxassetid://3577325359",
+            ["Walk"] = "rbxassetid://3577396272"
+        }
+    },
+    ["Run"] = "rbxassetid://507767714",
+    ["Cards"] = {
+        ["Trap"] = {
+            ["BananaThrow"] = "rbxassetid://924248229",
+            ["TripwireTrip"] = "rbxassetid://963810947",
+            ["BananaSlip"] = "rbxassetid://897325304"
+        },
+        ["Generic"] = {
+            ["UseEquip Mirrored"] = "rbxassetid://13957233047",
+            ["EquipRecovery"] = "rbxassetid://966210774",
+            ["UseFront"] = "rbxassetid://915122315",
+            ["UseEquip"] = "rbxassetid://924223165"
+        },
+        ["Effect"] = {
+            ["JetCrash"] = "rbxassetid://9865151135",
+            ["HocusPocus"] = "rbxassetid://1464633845",
+            ["DonMask"] = "rbxassetid://926207531",
+            ["StaffThrustUp"] = "rbxassetid://1464639980",
+            ["StaffPrayer"] = "rbxassetid://8194537764",
+            ["Eat"] = "rbxassetid://4480928687",
+            ["Fall"] = "rbxassetid://11434816596",
+            ["Karaoke"] = "rbxassetid://928324648",
+            ["chestpat"] = "rbxassetid://13202677593",
+            ["Stomp"] = "rbxassetid://6015390145",
+            ["Miasma"] = "rbxassetid://5925610191",
+            ["HeavyStomp"] = "rbxassetid://14022804699",
+            ["Action Begin"] = "rbxassetid://9464238372",
+            ["Drink"] = "rbxassetid://936463448",
+            ["ActorBlink"] = "rbxassetid://11155268739",
+            ["JetLand"] = "rbxassetid://9729260207",
+            ["Meditate"] = "rbxassetid://1469700358",
+            ["BoxerStance"] = "rbxassetid://9464240529",
+            ["Action End"] = "rbxassetid://9464239309"
+        },
+        ["Attack"] = {
+            ["SaxophoneSkillEnd"] = "rbxassetid://10974382337",
+            ["TriangleAttack"] = "rbxassetid://10977213980",
+            ["1HandFire"] = "rbxassetid://9356300702",
+            ["OneTwoPunch2"] = "rbxassetid://897390245",
+            ["Smack"] = "rbxassetid://924220564",
+            ["SaxophoneSkillBegin"] = "rbxassetid://10974384133",
+            ["Gun"] = "rbxassetid://11931597541",
+            ["HunterMark"] = "rbxassetid://9464233639",
+            ["everfrostaxe"] = "rbxassetid://13202683946",
+            ["Flop"] = "rbxassetid://1430629793",
+            ["FireAxe"] = "rbxassetid://12799091578",
+            ["Jab Combo"] = "rbxassetid://9464235324",
+            ["Slingshot"] = "rbxassetid://961805029",
+            ["VerticalBowshot"] = "rbxassetid://981153488",
+            ["CrossbowModern"] = "rbxassetid://9662276119",
+            ["StaffPanic"] = "rbxassetid://1464644135",
+            ["Venomshank"] = "rbxassetid://1587158748",
+            ["PirateAttack"] = "rbxassetid://13505148708",
+            ["SwordSlam"] = "rbxassetid://10058351037",
+            ["BucklerStrike"] = "rbxassetid://6169493700",
+            ["FireStrike"] = "rbxassetid://3457881541",
+            ["BarrageRight"] = "rbxassetid://11001488489",
+            ["RockSmashSpecial"] = "rbxassetid://951580476",
+            ["AetherialStrike"] = "rbxassetid://965942706",
+            ["SpectreSing"] = "rbxassetid://11180217513",
+            ["OneTwoPunch1"] = "rbxassetid://897386760",
+            ["StaffBackfire"] = "rbxassetid://1464638438",
+            ["Ice Breaker"] = "rbxassetid://11748961346",
+            ["BarrageLeft"] = "rbxassetid://11001483053",
+            ["Stargazer"] = "rbxassetid://6327817511",
+            ["Backhand Smack Mirrored"] = "rbxassetid://13957226877",
+            ["Scimitar"] = "rbxassetid://1533991431",
+            ["Bowshot"] = "rbxassetid://962510665",
+            ["SpectreAttack"] = "rbxassetid://11180219841",
+            ["tridentthrust"] = "rbxassetid://13202689325",
+            ["WeaponThrow"] = "rbxassetid://13962906158",
+            ["HeavySpin"] = "rbxassetid://6011210240",
+            ["Rushdown"] = "rbxassetid://9464236629",
+            ["SaxophoneSkillIdle"] = "rbxassetid://10974376993",
+            ["WandFlick"] = "rbxassetid://964045185",
+            ["Greatsword"] = "rbxassetid://953524050",
+            ["ViolinAttack"] = "rbxassetid://10977214764",
+            ["BigSwing"] = "rbxassetid://10974372617",
+            ["Shotgun"] = "rbxassetid://9662277404",
+            ["WoodenSwordSpecial"] = "rbxassetid://953042385",
+            ["KnifeJab"] = "rbxassetid://963818914",
+            ["Claymore"] = "rbxassetid://6689734313",
+            ["SingerAttack"] = "rbxassetid://11155265960",
+            ["Butcher"] = "rbxassetid://13962138730",
+            ["Blowdart"] = "rbxassetid://963815102",
+            ["Scratch Mirrored"] = "rbxassetid://13957229450",
+            ["BagHit"] = "rbxassetid://13748208871",
+            ["Uppercut"] = "rbxassetid://953215302",
+            ["duelistrapier"] = "rbxassetid://13202679736",
+            ["Flintlock"] = "rbxassetid://13505171402",
+            ["zombiebite"] = "rbxassetid://13505157045",
+            ["Eat2"] = "rbxassetid://13202671734",
+            ["Chuck"] = "rbxassetid://14156723843",
+            ["FlameLance"] = "rbxassetid://11114648942",
+            ["OneTwoPunch1Recovery"] = "rbxassetid://936513192",
+            ["Rifle30"] = "rbxassetid://11278621475",
+            ["Breath"] = "rbxassetid://4437818710",
+            ["WoodenSword"] = "rbxassetid://953529094",
+            ["toepoke"] = "rbxassetid://13202686766",
+            ["brutalize"] = "rbxassetid://13202674188",
+            ["RockSmash"] = "rbxassetid://953526637",
+            ["Scratch"] = "rbxassetid://925356866",
+            ["ShieldBash"] = "rbxassetid://952943277",
+            ["AuraPierce"] = "rbxassetid://1464636283",
+            ["BackhandSmack"] = "rbxassetid://925355259",
+            ["ActorSing"] = "rbxassetid://11155264484",
+            ["ActorAttack"] = "rbxassetid://11155261553"
+        },
+        ["Heal"] = {["UnicornHorn"] = "rbxassetid://966132439"},
+        ["Buff"] = {
+            ["BasicBuffTarget"] = "rbxassetid://913536861",
+            ["BasicBuffSelf"] = "rbxassetid://913525676",
+            ["Roar"] = "rbxassetid://4437822857"
+        },
+        ["Stance"] = {["CounterAction"] = "rbxassetid://601668086", ["CounterStance"] = "rbxassetid://601668086"}
+    }
+}
