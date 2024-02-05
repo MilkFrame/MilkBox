488d2e4d8407de18b3cf231e7d1b47b11e392eb6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-06 00:55:08 +1100

    Create Trades.lua

---
+++ GameData/Hexaria [913614076]/Trades.lua
@@ -0,0 +1,187 @@
+data = {
+    ["kishuf_trade_pure_energy"] = {["Take"] = {["Items"] = {1, 1}}, ["Give"] = {["Items"] = {1}}},
+    ["infinipie_trade"] = {
+        ["Take"] = {
+            ["Cards"] = {
+                ["Egg Relic"] = 50,
+                ["Summon Pie"] = 10,
+                ["Pie Throw"] = 20,
+                ["Pie Swarm"] = 4,
+                ["Tasty Pie"] = 20
+            }
+        },
+        ["Give"] = {["Cards"] = {["Infini-Pie"] = 1}}
+    },
+    ["steel_sabre_trade"] = {["Take"] = {["Items"] = {2}}, ["Give"] = {["Cards"] = {["Eternal Sabre"] = 1}}},
+    ["aurmire_matt_6"] = {
+        ["Take"] = {["Cards"] = {["Mushroom Growth"] = 1, ["Mystery Elixir"] = 4}, ["Items"] = {10}},
+        ["Give"] = {["Cards"] = {["Sourbloom Flask"] = 1}}
+    },
+    ["monsterhunting_bandit_clothes_betrugenian_scout"] = {["Take"] = {["Items"] = {150}}, ["Give"] = {["Items"] = {1}}},
+    ["halloween_trade_1"] = {
+        ["Take"] = {["Cards"] = {["Blue Elixir"] = 15, ["Antidote"] = 25}},
+        ["Give"] = {["Items"] = {1}}
+    },
+    ["blacksmith_trade_iron"] = {["Take"] = {["Items"] = {5, 1}}, ["Give"] = {["Cards"] = {["Iron Broadsword"] = 1}}},
+    ["simple_pyromancer_trade"] = {
+        ["Take"] = {
+            ["Cards"] = {
+                ["Ember"] = 12,
+                ["Inferno Blast"] = 3,
+                ["Blaze Rune"] = 5,
+                ["Invoke Fire Spirit"] = 1,
+                ["Meteor Strike"] = 1,
+                ["Heatwave"] = 3,
+                ["Ignite"] = 8,
+                ["Torch"] = 3,
+                ["Inferno Wand"] = 6,
+                ["Fire Axe"] = 1,
+                ["Blazing Short Sword"] = 2
+            },
+            ["Items"] = {1}
+        },
+        ["Give"] = {["Items"] = {1}}
+    },
+    ["halloween_trade_clothes_haunted_knight_blackred"] = {
+        ["Take"] = {["Items"] = {70, 3, 1, 7, 1, 7, 5, 14}},
+        ["Give"] = {["Items"] = {1}}
+    },
+    ["halloween_trade_clothes_haunted_knight_blackblue"] = {
+        ["Take"] = {["Items"] = {70, 3, 1, 7, 1, 7, 5, 14}},
+        ["Give"] = {["Items"] = {1}}
+    },
+    ["shieldbash_trade"] = {
+        ["Take"] = {["Cards"] = {["Wooden Club Slam"] = 3, ["Bow and Arrow"] = 1, ["Defense Buff"] = 5}},
+        ["Give"] = {["Cards"] = {["Shield Bash"] = 1}}
+    },
+    ["pilfered_trade_2"] = {["Take"] = {["Items"] = {4}}, ["Give"] = {["Cards"] = {["Darksteel Dagger"] = 1}}},
+    ["halloween_trade_clothes_haunted_knight_blackorange"] = {
+        ["Take"] = {["Items"] = {70, 3, 1, 7, 1, 7, 5, 14}},
+        ["Give"] = {["Items"] = {1}}
+    },
+    ["worlds_colliding_2"] = {
+        ["Take"] = {["Cards"] = {["Magician's Secret"] = 1}, ["Items"] = {40, 1, 27}},
+        ["Give"] = {["Cards"] = {["Decaying Ground"] = 1}}
+    },
+    ["monsterhunting_bandit_7"] = {
+        ["Take"] = {["Cards"] = {["Magician's Secret"] = 1, ["Blessed Rune"] = 1}, ["Items"] = {15, 1, 3, 5}},
+        ["Give"] = {["Cards"] = {["Vampiric Goblet"] = 1}}
+    },
+    ["kishufoutfit_trade"] = {["Take"] = {["Items"] = {100, 1}}, ["Give"] = {["Items"] = {1}}},
+    ["guard_summon_trade"] = {
+        ["Take"] = {["Cards"] = {["Mercenary"] = 1, ["Shield Bash"] = 3}},
+        ["Give"] = {["Cards"] = {["Call Guard"] = 1}}
+    },
+    ["eternal_trade"] = {
+        ["Take"] = {["Cards"] = {["Fireball"] = 3, ["Magician's Secret"] = 10, ["Ember"] = 4, ["Ignite"] = 3}},
+        ["Give"] = {["Cards"] = {["Eternal Flame"] = 1}}
+    },
+    ["monsterhunting_bandit_6"] = {
+        ["Take"] = {["Cards"] = {["Dash"] = 3, ["Magician's Secret"] = 1}, ["Items"] = {15}},
+        ["Give"] = {["Cards"] = {["Haste"] = 1}}
+    },
+    ["stargazer_trade"] = {["Take"] = {["Items"] = {1000}}, ["Give"] = {["Items"] = {1}}},
+    ["and_hellfire_trade"] = {["Take"] = {["Items"] = {1, 1}}, ["Give"] = {["Cards"] = {["Hellfire"] = 1}}},
+    ["aurmire_matt_1"] = {
+        ["Take"] = {["Cards"] = {["Pollute"] = 5}, ["Items"] = {8}},
+        ["Give"] = {["Cards"] = {["Mutate: Pestilence"] = 1}}
+    },
+    ["aurmire_matt_3"] = {
+        ["Take"] = {["Cards"] = {["Ancient Prayer"] = 5}, ["Items"] = {8}},
+        ["Give"] = {["Cards"] = {["Mutate: Health"] = 1}}
+    },
+    ["aurmire_matt_5"] = {
+        ["Take"] = {["Cards"] = {["Attack Buff"] = 5}, ["Items"] = {10}},
+        ["Give"] = {["Cards"] = {["Mutate: Death"] = 1}}
+    },
+    ["rocktrow_trade"] = {["Take"] = {["Cards"] = {["Rock Throw"] = 4}}, ["Give"] = {["Cards"] = {["Slingshot"] = 1}}},
+    ["astral_blast_trade"] = {
+        ["Take"] = {["Cards"] = {["Arcane Blast"] = 1, ["Student's Tome"] = 2}, ["Items"] = {30}},
+        ["Give"] = {["Cards"] = {["Astral Blast"] = 1}}
+    },
+    ["aurmire_matt_4"] = {
+        ["Take"] = {["Cards"] = {["Defense Buff"] = 5}, ["Items"] = {10}},
+        ["Give"] = {["Cards"] = {["Mutate: War"] = 1}}
+    },
+    ["aurmire_matt_7"] = {
+        ["Take"] = {["Cards"] = {["Necromancy"] = 1}, ["Items"] = {25}},
+        ["Give"] = {["Cards"] = {["Zombify"] = 1}}
+    },
+    ["monsterhunting_bandit_5"] = {
+        ["Take"] = {
+            ["Cards"] = {
+                ["Mask of Truth"] = 2,
+                ["Sanguine Wisdom"] = 2,
+                ["Blessed Rune"] = 5,
+                ["Magician's Secret"] = 5,
+                ["Core Infliction"] = 1,
+                ["Meditation"] = 2
+            },
+            ["Items"] = {100, 100, 100, 400, 100, 100, 100, 100}
+        },
+        ["Give"] = {["Cards"] = {["Cardcyclopedia"] = 1}}
+    },
+    ["halloween_trade_pages_hellfire"] = {
+        ["Take"] = {["Cards"] = {["Lantern Throw"] = 4}, ["Items"] = {4, 8, 12, 16}},
+        ["Give"] = {["Cards"] = {["Spirit Flame"] = 1}}
+    },
+    ["pommel_trade"] = {["Take"] = {["Items"] = {20}}, ["Give"] = {["Items"] = {1}}},
+    ["halloween_trade_pages_agonyseal"] = {
+        ["Take"] = {["Items"] = {1, 1, 1, 1}},
+        ["Give"] = {["Cards"] = {["Agony Seal"] = 1}}
+    },
+    ["ramparts_trade"] = {["Take"] = {["Items"] = {5}}, ["Give"] = {["Cards"] = {["Ramparts"] = 1}}},
+    ["protectorate_trade"] = {
+        ["Take"] = {["Cards"] = {["Holy Ground"] = 1, ["Defense Buff"] = 5, ["Barrier"] = 3}},
+        ["Give"] = {["Cards"] = {["Protectorate"] = 1}}
+    },
+    ["halloween_trade_clothes_haunted_knight_purplegreen"] = {
+        ["Take"] = {["Items"] = {70, 3, 1, 7, 1, 7, 5, 14}},
+        ["Give"] = {["Items"] = {1}}
+    },
+    ["bardiche_trade"] = {
+        ["Take"] = {["Cards"] = {["Wooden Club Slam"] = 3, ["Rusty Knife"] = 3, ["Bow and Arrow"] = 2}},
+        ["Give"] = {["Cards"] = {["Bardiche Chop"] = 1}}
+    },
+    ["monsterhunting_bandit_1"] = {
+        ["Take"] = {["Cards"] = {["Vampire's Claw"] = 1, ["Contact Heal"] = 3}, ["Items"] = {15}},
+        ["Give"] = {["Cards"] = {["Claw Impale"] = 1}}
+    },
+    ["aurmire_matt_2"] = {
+        ["Take"] = {["Cards"] = {["Sugar Rush"] = 5}, ["Items"] = {8}},
+        ["Give"] = {["Cards"] = {["Mutate: Famine"] = 1}}
+    },
+    ["vile_graveyard_trade"] = {
+        ["Take"] = {["Cards"] = {["Graveyard"] = 1}, ["Items"] = {150}},
+        ["Give"] = {["Cards"] = {["Vile Graveyard"] = 1}}
+    },
+    ["worlds_colliding_1"] = {
+        ["Take"] = {["Cards"] = {["Afternoon Delight"] = 1, ["Magician's Secret"] = 1}, ["Items"] = {1, 1, 35}},
+        ["Give"] = {["Cards"] = {["Kishuf Drain"] = 1}}
+    },
+    ["monsterhunting_bandit_2"] = {
+        ["Take"] = {["Cards"] = {["Scimitar"] = 3, ["Eternal Flame"] = 1}, ["Items"] = {30}},
+        ["Give"] = {["Cards"] = {["Silver Blade"] = 1}}
+    },
+    ["monsterhunting_bandit_8"] = {
+        ["Take"] = {
+            ["Cards"] = {["Defense Debuff"] = 2, ["Attack Buff"] = 2, ["Magician's Secret"] = 2},
+            ["Items"] = {15}
+        },
+        ["Give"] = {["Cards"] = {["Vampiric Rejuvenation"] = 1}}
+    },
+    ["monsterhunting_bandit_4"] = {
+        ["Take"] = {["Cards"] = {["Vampiric Mindflay"] = 2}, ["Items"] = {99999999999, 20}},
+        ["Give"] = {["Cards"] = {["Psychic Wave"] = 1}}
+    },
+    ["monsterhunting_bandit_3"] = {
+        ["Take"] = {["Cards"] = {["Shadow Trade"] = 3, ["Chain Lightning"] = 1, ["Pollute"] = 3}, ["Items"] = {15}},
+        ["Give"] = {["Cards"] = {["Chain Link"] = 1}}
+    },
+    ["coldshoulder_trade"] = {
+        ["Take"] = {["Cards"] = {["Cold Feet"] = 2, ["Chain Lightning"] = 3}, ["Items"] = {5}},
+        ["Give"] = {["Cards"] = {["Cold Shoulder"] = 1}}
+    },
+    ["disarm_trade"] = {["Take"] = {["Items"] = {15}}, ["Give"] = {["Cards"] = {["Disarm"] = 1}}},
+    ["pilfered_trade_1"] = {["Take"] = {["Items"] = {6}}, ["Give"] = {["Cards"] = {["Darksteel Longblade"] = 1}}}
+}
