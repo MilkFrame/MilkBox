f89cc4cc39f71f248d328f5d8008e40516985939
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-06 00:27:00 +1100

    Add files via upload

--- GameData/Dungeon Quest [2414851778]/ProductData.lua
+++ GameData/Dungeon Quest [2414851778]/ProductData.lua
@@ -1,35 +1,23 @@
 data = {
-    ["Get"] = "function: 0x00000000cbb078f4",
+    ["Giftable"] = {
+        ["Cosmetics"] = true,
+        ["XPBoosts"] = true,
+        ["SnowflakeBundles"] = true,
+        ["RotatingCosmetics"] = true
+    },
     ["Products"] = {
-        ["Cosmetics"] = {
-            ["armors"] = {["Shadow Spirit"] = {["id"] = 1340264213}, ["Bartender"] = {["id"] = 1340265192}},
-            ["weapons"] = {["Shadow Daggers"] = {["id"] = 1340264304}, ["Root Beer"] = {["id"] = 1340264331}},
-            ["enchants"] = {["Jade Pulse"] = {["id"] = 1340264106}, ["Golden Burst"] = {["id"] = 1340264162}}
-        },
-        ["XPBoosts"] = {
-            ["2hour"] = {["id"] = 1013086878, ["timeToAdd"] = 7200},
-            ["1hour"] = {["id"] = 1013086750, ["timeToAdd"] = 3600},
-            ["4hour"] = {["id"] = 1013087251, ["timeToAdd"] = 14400}
+        ["HalloweenBundles"] = {
+            ["Legendary"] = {["id"] = 1675638374},
+            ["Common"] = {["id"] = 1675638372},
+            ["Epic"] = {["id"] = 1675638376},
+            ["Uncommon"] = {["id"] = 1675638368},
+            ["Rare"] = {["id"] = 1675638371}
         },
         ["GamePasses"] = {
             ["FreeStatResets"] = {["id"] = 6093005},
             ["VIP"] = {["id"] = 6028667},
             ["ExtraItem"] = {["id"] = 6092999},
             ["DoubleGold"] = {["id"] = 6092989}
-        },
-        ["Gold"] = {
-            ["tier5"] = {["id"] = 1231085335},
-            ["tier2"] = {["id"] = 1231085313},
-            ["tier1"] = {["id"] = 1231085305},
-            ["tier3"] = {["id"] = 1231085320},
-            ["tier4"] = {["id"] = 1231085324}
-        },
-        ["OniEvent"] = {
-            ["tier5"] = {["id"] = 1613657205, ["currencyToGive"] = 2500},
-            ["tier2"] = {["id"] = 1613656406, ["currencyToGive"] = 250},
-            ["tier1"] = {["id"] = 1613656181, ["currencyToGive"] = 100},
-            ["tier3"] = {["id"] = 1613656626, ["currencyToGive"] = 500},
-            ["tier4"] = {["id"] = 1613656914, ["currencyToGive"] = 1000}
         },
         ["RotatingCosmetics"] = {
             ["1"] = {
@@ -73,6 +61,62 @@ data = {
                 }
             }
         },
-        ["DailyRewardRefresh"] = {["id"] = 1013113157}
-    }
+        ["SnowflakeBundles"] = {
+            ["Tier5"] = {["id"] = 1691470671, ["currencyToGive"] = 20000},
+            ["Tier2"] = {["id"] = 1691470674, ["currencyToGive"] = 3000},
+            ["Tier1"] = {["id"] = 1691470670, ["currencyToGive"] = 1200},
+            ["Tier3"] = {["id"] = 1691470672, ["currencyToGive"] = 7000},
+            ["Tier4"] = {["id"] = 1691470673, ["currencyToGive"] = 12000}
+        },
+        ["Cosmetics"] = {
+            ["armors"] = {["Draconic Deity"] = {["id"] = 1667168394}, ["Bartender"] = {["id"] = 1340265192}},
+            ["weapons"] = {["Draconic Naginata"] = {["id"] = 1667168849}, ["Root Beer"] = {["id"] = 1340264331}},
+            ["enchants"] = {["Jade Pulse"] = {["id"] = 1340264106}, ["Golden Burst"] = {["id"] = 1340264162}}
+        },
+        ["XPBoosts"] = {
+            ["2hour"] = {["id"] = 1013086878, ["timeToAdd"] = 7200},
+            ["1hour"] = {["id"] = 1013086750, ["timeToAdd"] = 3600},
+            ["4hour"] = {["id"] = 1013087251, ["timeToAdd"] = 14400}
+        },
+        ["DailyRewardRefresh"] = {["id"] = 1013113157},
+        ["Gold"] = {
+            ["tier5"] = {["id"] = 1231085335},
+            ["tier2"] = {["id"] = 1231085313},
+            ["tier1"] = {["id"] = 1231085305},
+            ["tier3"] = {["id"] = 1231085320},
+            ["tier4"] = {["id"] = 1231085324}
+        },
+        ["OniEvent"] = {
+            ["Tier5"] = {["id"] = 1613657205, ["currencyToGive"] = 10000},
+            ["Tier2"] = {["id"] = 1613656406, ["currencyToGive"] = 625},
+            ["Tier1"] = {["id"] = 1613656181, ["currencyToGive"] = 250},
+            ["Tier3"] = {["id"] = 1613656626, ["currencyToGive"] = 1250},
+            ["Tier4"] = {["id"] = 1613656914, ["currencyToGive"] = 3000}
+        },
+        ["HalloweenCosmetics"] = {
+            ["Demonic Pumpkin Reaver"] = {["id"] = 1675638391, ["Type"] = "Weapon"},
+            ["Arch Devil"] = {["id"] = 1675638383, ["Type"] = "Armor"},
+            ["Creepy Crawler"] = {["id"] = 1675638377, ["Type"] = "Title"},
+            ["Wicked Scarecrow"] = {["id"] = 1675638373, ["Type"] = "Armor"},
+            ["Cropslash Sickle"] = {["id"] = 1675638375, ["Type"] = "Weapon"},
+            ["Infernal Trident"] = {["id"] = 1675638385, ["Type"] = "Weapon"},
+            ["Soul Lantern Mace"] = {["id"] = 1675638380, ["Type"] = "Weapon"},
+            ["Halloween 2023"] = {["id"] = 1675632144, ["Type"] = "Title"},
+            ["Grave Robber"] = {["id"] = 1675638381, ["Type"] = "Title"},
+            ["Aspect of Death"] = {["id"] = 1675638390, ["Type"] = "Armor"},
+            ["Undead Pumpkin Lord"] = {["id"] = 1675638394, ["Type"] = "Armor"},
+            ["Plague Pumpkin Lord"] = {["id"] = 1675638392, ["Type"] = "Armor"},
+            ["Demonic Pumpkin Lord"] = {["id"] = 1675638395, ["Type"] = "Armor"},
+            ["Blighted Pumpkin Scythe"] = {["id"] = 1675638389, ["Type"] = "Weapon"},
+            ["Broom of Bewitchment"] = {["id"] = 1675638384, ["Type"] = "Weapon"},
+            ["Necromantic Pumpkin Staff"] = {["id"] = 1675638393, ["Type"] = "Weapon"},
+            ["Dark Sorceress"] = {["id"] = 1675638386, ["Type"] = "Armor"},
+            ["Undead Witch"] = {["id"] = 1675638382, ["Type"] = "Armor"},
+            ["Grim Reaper"] = {["id"] = 1675638387, ["Type"] = "Title"},
+            ["Death's Grasp Scythe"] = {["id"] = 1675638398, ["Type"] = "Weapon"}
+        },
+        ["AutofillExchange"] = {["id"] = 1675634438},
+        ["ClaimMissedDay"] = {["id"] = 1696523275}
+    },
+    ["Get"] = "function: 0x00000000d1a0016f"
 }
