2842ecd245ca82c17a5f6fb112c67f2e8949bc21
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-11 02:00:46 +1100

    Create Quests.lua

---
+++ GameData/Breadwinners [6900606609]/Quests.lua
@@ -0,0 +1,20 @@
+data = {
+    ["DailyQuests"] = {
+        {["MissionValueNeeded"] = 30, ["MissionReward"] = 100, ["MissionName"] = "Play this game for 30 minutes."},
+        {["MissionValueNeeded"] = 10, ["MissionReward"] = 30, ["MissionName"] = "Play this game for 10 minutes."},
+        {["MissionValueNeeded"] = 10000, ["MissionReward"] = 1000, ["MissionName"] = "Get to $10,000."},
+        {["MissionValueNeeded"] = 5000, ["MissionReward"] = 500, ["MissionName"] = "Get to $5,000."},
+        {["MissionValueNeeded"] = 60, ["MissionReward"] = 25, ["MissionName"] = "Stand still for one minute."},
+        {["MissionValueNeeded"] = 5, ["MissionReward"] = 100, ["MissionName"] = "Find 5 apples in the map."},
+        {["MissionValueNeeded"] = 5, ["MissionReward"] = 100, ["MissionName"] = "Find 5 bushes in the map."}
+    },
+    ["SideQuests"] = {
+        {["MissionValueNeeded"] = 30, ["MissionReward"] = 100, ["MissionName"] = "Play this game for 30 minutes."},
+        {["MissionValueNeeded"] = 20, ["MissionReward"] = 30, ["MissionName"] = "Play this game for 20 minutes."},
+        {["MissionValueNeeded"] = 10000, ["MissionReward"] = 1000, ["MissionName"] = "Get to $10,000."},
+        {["MissionValueNeeded"] = 5000, ["MissionReward"] = 500, ["MissionName"] = "Get to $5,000."},
+        {["MissionValueNeeded"] = 60, ["MissionReward"] = 25, ["MissionName"] = "Stand still for one minute."},
+        {["MissionValueNeeded"] = 5, ["MissionReward"] = 100, ["MissionName"] = "Find 5 apples in the map."},
+        {["MissionValueNeeded"] = 5, ["MissionReward"] = 100, ["MissionName"] = "Find 5 bushes in the map."}
+    }
+}
