f1e4848f33eddc709eae6d804afd1144ac1dc3c4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-11 01:57:56 +1100

    Create Tutorial.lua

---
+++ GameData/Breadwinners [6900606609]/Tutorial.lua
@@ -0,0 +1,13 @@
+local data = {}
+data.Stage1 = {Page1 = {"Hello there, welcome to Breadwinners!", "The game where you run your own bakery!", ""}, Page2 = {"Lets start by building a small bakery.", "Claim 1 of the empty plots to build a bakery.", ""}}
+data.Stage2 = {Page1 = {"Congratulations!", "You now own your own bakery!", "Time to make some bread."}, Page2 = {"Don't worry I'll teach you the ropes of running your bakery.", "Lets start by buying a grain field.", "Buy a grain field next to your store."}}
+data.Stage3 = {Page1 = {"You'll have to be patient, grain takes some time to grow.", "Come back in about 40 seconds.", ""}, Page2 = {"Let's get you some equipment while we wait.", "Go to the Tool Shop.", "And then buy a Starter Sickle and Starter Bag."}}
+data.Stage4 = {Page1 = {"Your grain has finished growing.", "It is time to harvest!", ""}, Page2 = {"Equip your sickle and use it on the grain field.", "", ""}}
+data.Stage5 = {Page1 = {"What a wonderful harvest!", "We can make plenty of bread with that!", ""}, Page2 = {"Go to the kitchen inside your bakery to start cooking.", "", ""}}
+data.Stage6 = {Page1 = {"First lets make some space in your bag for other ingredients.", "Click on the X button in the ingredient list on the right of your screen.", "This will remove 1 grain from your inventory."}}
+data.Stage7 = {Page1 = {"Great, now bread is very simple to make.", "All you need is 3 grain and 1 water.", "You already have the grain so go to the sink to grab some water."}}
+data.Stage8 = {Page1 = {"Now mix the grain and the water to make Dough.", "", ""}}
+data.Stage9 = {Page1 = {"Now for the last step go to the oven and select Bread.", "", ""}}
+data.Stage10 = {Page1 = {"Well done!", "That bread looks delicious!", ""}, Page2 = {"Let's put it on a storage rack in the front of the store.", "So customers can see what you are selling.", ""}}
+data.Stage11 = {Page1 = {"Your first customer arrived!", "Go to the customer and press E to take their order.", ""}, Page2 = {"To give something to a customer it has to be in your inventory.", "You can grab the bread of the racks by clicking on it.", "Then you can give it to your customer."}}
+data.Stage12 = {Page1 = {"Well done!", "I think you'll be fine from here on.", "Have fun playing Bread Winners!"}}
