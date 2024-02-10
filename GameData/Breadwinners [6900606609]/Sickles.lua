25d7bbc812818d47c7049d85b592ee8042891eee
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-11 02:15:16 +1100

    Create Sickles.lua

---
+++ GameData/Breadwinners [6900606609]/Sickles.lua
@@ -0,0 +1,78 @@
+data = {
+    ["Sickles"] = {
+        {
+            ["Name"] = "Starter sickle",
+            ["Icon"] = "http://www.roblox.com/asset/?id=7365859398",
+            ["Price"] = 0,
+            ["Multiplier"] = 1,
+            ["Tool"] = "Sickle",
+            ["Description"] = "X1"
+        },
+        {
+            ["Name"] = "Sparkling Sickle",
+            ["Icon"] = "http://www.roblox.com/asset/?id=7365859892",
+            ["Price"] = 260,
+            ["Multiplier"] = 2,
+            ["Tool"] = "Sparkling Sickle",
+            ["Description"] = "X2"
+        },
+        {
+            ["Name"] = "Shining Sickle",
+            ["Icon"] = "http://www.roblox.com/asset/?id=7365860246",
+            ["Price"] = 2100,
+            ["Multiplier"] = 3,
+            ["Tool"] = "Shining Sickle",
+            ["Description"] = "X3"
+        },
+        {
+            ["Name"] = "Godly Sickle",
+            ["Icon"] = "http://www.roblox.com/asset/?id=7365860598",
+            ["Price"] = 8500,
+            ["Multiplier"] = 4,
+            ["Tool"] = "Godly Sickle",
+            ["Description"] = "X4"
+        },
+        {
+            ["Name"] = "Demonic Sickle",
+            ["Icon"] = "http://www.roblox.com/asset/?id=7420369171",
+            ["Price"] = 22000,
+            ["Multiplier"] = 5,
+            ["Tool"] = "Demonic Sickle",
+            ["Description"] = "X5"
+        },
+        {
+            ["Name"] = "Water Sickle",
+            ["Icon"] = "http://www.roblox.com/asset/?id=7478774709",
+            ["Price"] = 48000,
+            ["Multiplier"] = 6,
+            ["Tool"] = "Water Sickle",
+            ["Description"] = "X6"
+        },
+        {
+            ["Name"] = "Phoenix Sickle",
+            ["Icon"] = "http://www.roblox.com/asset/?id=7478765622",
+            ["Price"] = 480000,
+            ["Multiplier"] = 7,
+            ["Tool"] = "Phoenix Sickle",
+            ["Description"] = "X7"
+        },
+        {
+            ["Name"] = "Void Sickle",
+            ["Icon"] = "http://www.roblox.com/asset/?id=7478773460",
+            ["Price"] = 1400000,
+            ["Multiplier"] = 8,
+            ["Tool"] = "Void Sickle",
+            ["Description"] = "X8"
+        }
+    },
+    ["Stats"] = {
+        ["Godly Sickle"] = {["Range"] = 6, ["Multiplier"] = 4},
+        ["Void Sickle"] = {["Range"] = 11, ["Multiplier"] = 8},
+        ["Phoenix Sickle"] = {["Range"] = 10, ["Multiplier"] = 7},
+        ["Sparkling Sickle"] = {["Multiplier"] = 2, ["Range"] = 6},
+        ["Water Sickle"] = {["Range"] = 9, ["Multiplier"] = 6},
+        ["Shining Sickle"] = {["Range"] = 6, ["Multiplier"] = 3},
+        ["Demonic Sickle"] = {["Range"] = 8, ["Multiplier"] = 5},
+        ["Sickle"] = {["Range"] = 6, ["Multiplier"] = 1}
+    }
+}
