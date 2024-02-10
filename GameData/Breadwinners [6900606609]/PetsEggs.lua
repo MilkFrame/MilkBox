dda4c3d93a3b0a020f4242d3d32236cba6ebbaf6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-11 02:16:58 +1100

    Create PetsEggs.lua

---
+++ GameData/Breadwinners [6900606609]/PetsEggs.lua
@@ -0,0 +1,424 @@
+data = {
+    ["ChristmasR$Pet"] = {
+        ["ProductID"] = 1713797008,
+        ["Currency"] = "R$",
+        ["Pets"] = {
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "Gingerbread Bear",
+                ["Rarity"] = 1,
+                ["Multiplier"] = 16,
+                ["Secret"] = false
+            }
+        },
+        ["Cost"] = 399
+    },
+    ["FallR$Pet"] = {
+        ["ProductID"] = 1666084948,
+        ["Currency"] = "R$",
+        ["Pets"] = {
+            {["Type"] = "Normal", ["Name"] = "PumpkinMacaroon", ["Rarity"] = 1, ["Multiplier"] = 16, ["Secret"] = false}
+        },
+        ["Cost"] = 199
+    },
+    ["Ghost"] = {
+        ["Currency"] = "GhostCoins",
+        ["Pets"] = {
+            {["Type"] = "Normal", ["Name"] = "Candy Ghost", ["Rarity"] = 2, ["Multiplier"] = 30, ["Secret"] = false},
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "Witch Kitten Cupcake",
+                ["Rarity"] = 10,
+                ["Multiplier"] = 20,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "Boo Berry Ghost",
+                ["Rarity"] = 50,
+                ["Multiplier"] = 10,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "Zombie Lamb Cake",
+                ["Rarity"] = 200,
+                ["Multiplier"] = 8,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "Choco Ghost Witch",
+                ["Rarity"] = 500,
+                ["Multiplier"] = 7,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "Marshmellow Ghost Witch",
+                ["Rarity"] = 1000,
+                ["Multiplier"] = 6,
+                ["Secret"] = false
+            }
+        },
+        ["Cost"] = 50
+    },
+    ["WinterR$Egg"] = {
+        ["ProductID"] = 1696622210,
+        ["Currency"] = "R$",
+        ["Pets"] = {
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "Frosted Bat Cookie",
+                ["Rarity"] = 1,
+                ["Multiplier"] = 24,
+                ["Secret"] = false
+            }
+        },
+        ["Cost"] = 199
+    },
+    ["ChocoEasterEgg"] = {
+        ["ProductID"] = 1515448549,
+        ["Currency"] = "R$",
+        ["Pets"] = {{["Rarity"] = 100, ["Type"] = "Normal", ["Name"] = "Choco Chick", ["Secret"] = false}},
+        ["Cost"] = 199
+    },
+    ["Pets"] = {
+        ["Currency"] = "Gems",
+        ["Pets"] = {
+            {["Rarity"] = 2, ["Type"] = "Normal", ["Name"] = "Bee", ["Secret"] = false},
+            {["Rarity"] = 10, ["Type"] = "Normal", ["Name"] = "Monkey", ["Secret"] = false},
+            {["Rarity"] = 50, ["Type"] = "Normal", ["Name"] = "Duck", ["Secret"] = false},
+            {["Rarity"] = 200, ["Type"] = "Normal", ["Name"] = "Fox", ["Secret"] = false},
+            {["Rarity"] = 500, ["Type"] = "Normal", ["Name"] = "Dog", ["Secret"] = false},
+            {["Rarity"] = 1000, ["Type"] = "Normal", ["Name"] = "Cat", ["Secret"] = false}
+        },
+        ["Cost"] = 100
+    },
+    ["Halloween"] = {
+        ["Currency"] = "PumpkinCoins",
+        ["Pets"] = {
+            {["Rarity"] = 2, ["Type"] = "Normal", ["Name"] = "Demon", ["Secret"] = false},
+            {["Rarity"] = 10, ["Type"] = "Normal", ["Name"] = "Light demon", ["Secret"] = false},
+            {["Rarity"] = 50, ["Type"] = "Normal", ["Name"] = "Bat", ["Secret"] = false},
+            {["Rarity"] = 200, ["Type"] = "Normal", ["Name"] = "Candy mage", ["Secret"] = false},
+            {["Rarity"] = 500, ["Type"] = "Normal", ["Name"] = "Pumpkin", ["Secret"] = false},
+            {["Rarity"] = 1000, ["Type"] = "Normal", ["Name"] = "Turkey", ["Secret"] = false}
+        },
+        ["Cost"] = 500
+    },
+    ["WinterEgg"] = {
+        ["Currency"] = "Snowflakes",
+        ["Pets"] = {
+            {["Type"] = "Normal", ["Name"] = "Snowy Lamb Cake", ["Rarity"] = 2, ["Multiplier"] = 12, ["Secret"] = false},
+            {["Type"] = "Normal", ["Name"] = "Cookie Bat", ["Rarity"] = 10, ["Multiplier"] = 8, ["Secret"] = false},
+            {["Type"] = "Normal", ["Name"] = "Pig Croissant", ["Rarity"] = 50, ["Multiplier"] = 5, ["Secret"] = false},
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "Cardinal Macaroon",
+                ["Rarity"] = 200,
+                ["Multiplier"] = 4,
+                ["Secret"] = false
+            },
+            {["Type"] = "Normal", ["Name"] = "Winter Kitten", ["Rarity"] = 500, ["Multiplier"] = 3, ["Secret"] = false},
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "Frosty Kitten",
+                ["Rarity"] = 1000,
+                ["Multiplier"] = 2.5,
+                ["Secret"] = false
+            }
+        },
+        ["Cost"] = 500
+    },
+    ["Pets2"] = {
+        ["Currency"] = "Gems",
+        ["Pets"] = {
+            {["Rarity"] = 2, ["Type"] = "Normal", ["Name"] = "Dragon", ["Secret"] = false},
+            {["Rarity"] = 10, ["Type"] = "Normal", ["Name"] = "Sea monster", ["Secret"] = false},
+            {["Rarity"] = 50, ["Type"] = "Normal", ["Name"] = "Scorpion", ["Secret"] = false},
+            {["Rarity"] = 200, ["Type"] = "Normal", ["Name"] = "Jellyfish", ["Secret"] = false},
+            {["Rarity"] = 500, ["Type"] = "Normal", ["Name"] = "Shark", ["Secret"] = false},
+            {["Rarity"] = 1000, ["Type"] = "Normal", ["Name"] = "Crab", ["Secret"] = false}
+        },
+        ["Cost"] = 10000
+    },
+    ["FallPets"] = {
+        ["Currency"] = "PumpkinCoins",
+        ["Pets"] = {
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "PrettyPumpkinCheesecakeBear",
+                ["Rarity"] = 2,
+                ["Multiplier"] = 12,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "PumpkinCheesecakeBear",
+                ["Rarity"] = 10,
+                ["Multiplier"] = 8,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "PrettyCaramelAppleTurkey",
+                ["Rarity"] = 50,
+                ["Multiplier"] = 5,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "CaramelAppleTurkey",
+                ["Rarity"] = 200,
+                ["Multiplier"] = 4,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "PrettyPumpkinPieTurtle",
+                ["Rarity"] = 500,
+                ["Multiplier"] = 3,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "PumpkinPieTurtle",
+                ["Rarity"] = 1000,
+                ["Multiplier"] = 2.5,
+                ["Secret"] = false
+            }
+        },
+        ["Cost"] = 500
+    },
+    ["Pets1New"] = {
+        ["Currency"] = "Coins",
+        ["Pets"] = {
+            {["Type"] = "Normal", ["Name"] = "PrettyCorgiLoaf", ["Rarity"] = 2, ["Multiplier"] = 4, ["Secret"] = false},
+            {["Type"] = "Normal", ["Name"] = "ChefCorgiLoaf", ["Rarity"] = 10, ["Multiplier"] = 3, ["Secret"] = false},
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "PrettyCupcakeKitty",
+                ["Rarity"] = 50,
+                ["Multiplier"] = 2.5,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "ChefCupcakeKitty",
+                ["Rarity"] = 200,
+                ["Multiplier"] = 2,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "PrettyMouseBun",
+                ["Rarity"] = 500,
+                ["Multiplier"] = 1.5,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "ChefMouseBun",
+                ["Rarity"] = 1000,
+                ["Multiplier"] = 1.2,
+                ["Secret"] = false
+            }
+        },
+        ["Cost"] = 1000
+    },
+    ["Valentine"] = {
+        ["Currency"] = "Hearts",
+        ["Pets"] = {
+            {["Rarity"] = 2, ["Type"] = "Normal", ["Name"] = "Chocolate Box", ["Secret"] = false},
+            {["Rarity"] = 10, ["Type"] = "Normal", ["Name"] = "Sweetheart", ["Secret"] = false},
+            {["Rarity"] = 50, ["Type"] = "Normal", ["Name"] = "Sweet Cake", ["Secret"] = false},
+            {["Rarity"] = 200, ["Type"] = "Normal", ["Name"] = "Milk To My Cookie", ["Secret"] = false},
+            {["Rarity"] = 500, ["Type"] = "Normal", ["Name"] = "Choco Cherries", ["Secret"] = false},
+            {["Rarity"] = 1000, ["Type"] = "Normal", ["Name"] = "Choco Strawberry", ["Secret"] = false}
+        },
+        ["Cost"] = 500
+    },
+    ["Staff"] = {
+        ["Currency"] = "Gems",
+        ["Pets"] = {
+            {["Rarity"] = 2, ["Type"] = "Normal", ["Name"] = "Jersito4", ["Secret"] = false},
+            {["Rarity"] = 10, ["Type"] = "Normal", ["Name"] = "MichyAsg", ["Secret"] = false},
+            {["Rarity"] = 50, ["Type"] = "Normal", ["Name"] = "TheRealNcMaster", ["Secret"] = false},
+            {["Rarity"] = 200, ["Type"] = "Normal", ["Name"] = "Caiteas", ["Secret"] = false},
+            {["Rarity"] = 500, ["Type"] = "Normal", ["Name"] = "Nesraie", ["Secret"] = false},
+            {["Rarity"] = 1000, ["Type"] = "Normal", ["Name"] = "Witheredfoxyxmangle2", ["Secret"] = false}
+        },
+        ["Cost"] = 1000
+    },
+    ["Pets2New"] = {
+        ["Currency"] = "Coins",
+        ["Pets"] = {
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "ChefMacaroonChick",
+                ["Rarity"] = 2,
+                ["Multiplier"] = 12,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "PrettyMacaroonChick",
+                ["Rarity"] = 10,
+                ["Multiplier"] = 8,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "ChefSwirlCakeDeer",
+                ["Rarity"] = 50,
+                ["Multiplier"] = 5,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "PrettySwirlCakeDeer",
+                ["Rarity"] = 200,
+                ["Multiplier"] = 4,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "ChocoBunnyDoughnut",
+                ["Rarity"] = 500,
+                ["Multiplier"] = 3,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "StrawberryBunnyDoughnut",
+                ["Rarity"] = 1000,
+                ["Multiplier"] = 2.5,
+                ["Secret"] = false
+            }
+        },
+        ["Cost"] = 10000
+    },
+    ["HalloweenR$Pet"] = {
+        ["ProductID"] = 1666085380,
+        ["Currency"] = "R$",
+        ["Pets"] = {
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "PumpkinMacaroonWitch",
+                ["Rarity"] = 1,
+                ["Multiplier"] = 24,
+                ["Secret"] = false
+            }
+        },
+        ["Cost"] = 399
+    },
+    ["EasterEgg"] = {
+        ["Currency"] = "ChocoEggs",
+        ["Pets"] = {
+            {["Rarity"] = 2, ["Type"] = "Normal", ["Name"] = "Easter Chick", ["Secret"] = false},
+            {["Rarity"] = 10, ["Type"] = "Normal", ["Name"] = "Easter Chicken", ["Secret"] = false},
+            {["Rarity"] = 50, ["Type"] = "Normal", ["Name"] = "Choco Chicken", ["Secret"] = false},
+            {["Rarity"] = 200, ["Type"] = "Normal", ["Name"] = "Easter Lamb", ["Secret"] = false},
+            {["Rarity"] = 500, ["Type"] = "Normal", ["Name"] = "Easter Bunny", ["Secret"] = false},
+            {["Rarity"] = 1000, ["Type"] = "Normal", ["Name"] = "Choco Bunny", ["Secret"] = false}
+        },
+        ["Cost"] = 500
+    },
+    ["ChristmasPets"] = {
+        ["Currency"] = "SnowCoins",
+        ["Pets"] = {
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "Christmas Roll Deer",
+                ["Rarity"] = 2,
+                ["Multiplier"] = 30,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "Jolly Lamb Cake",
+                ["Rarity"] = 10,
+                ["Multiplier"] = 20,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "Jolly Axolotl Cookie",
+                ["Rarity"] = 50,
+                ["Multiplier"] = 10,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "Jolly Macaron Frog",
+                ["Rarity"] = 200,
+                ["Multiplier"] = 8,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "Jolly Mouse Bun",
+                ["Rarity"] = 500,
+                ["Multiplier"] = 7,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "Jolly Cinnamon Deer",
+                ["Rarity"] = 1000,
+                ["Multiplier"] = 6,
+                ["Secret"] = false
+            }
+        },
+        ["Cost"] = 500
+    },
+    ["HalloweenNew"] = {
+        ["Currency"] = "PumpkinCoins",
+        ["Pets"] = {
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "HalloweenCorgiLoaf",
+                ["Rarity"] = 2,
+                ["Multiplier"] = 24,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "HalloweenMacaroonChick",
+                ["Rarity"] = 10,
+                ["Multiplier"] = 16,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "HalloweenCheeseCakeBear",
+                ["Rarity"] = 50,
+                ["Multiplier"] = 7,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "HalloweenCupcakeKitten",
+                ["Rarity"] = 200,
+                ["Multiplier"] = 6,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "HalloweenBunnyDoughnut",
+                ["Rarity"] = 500,
+                ["Multiplier"] = 5,
+                ["Secret"] = false
+            },
+            {
+                ["Type"] = "Normal",
+                ["Name"] = "HalloweenChocolateAppleTurkey",
+                ["Rarity"] = 1000,
+                ["Multiplier"] = 4,
+                ["Secret"] = false
+            }
+        },
+        ["Cost"] = 800
+    }
+}
