bdf1a8f78f7119d3a4ad1d05b7022cbad8a3e676
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-11 02:02:32 +1100

    Create Enchantments.lua

---
+++ GameData/Breadwinners [6900606609]/Enchantments.lua
@@ -0,0 +1,8 @@
+data = {
+    {
+        ["Price"] = 500000,
+        ["Icon"] = "http://www.roblox.com/asset/?id=279544753",
+        ["Name"] = "Water",
+        ["Description"] = "Regrow your crops 2x faster"
+    }
+}
