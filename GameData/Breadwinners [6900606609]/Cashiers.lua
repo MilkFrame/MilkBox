e3a3e393e9a7bee5bd5c1780273490b54ef9ebca
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-11 01:58:23 +1100

    Create Cashiers.lua

---
+++ GameData/Breadwinners [6900606609]/Cashiers.lua
@@ -0,0 +1,6 @@
+data = {
+    {["Price"] = 100, ["Efficiency"] = 1, ["Name"] = "Jersito", ["Model"] = "jersito4"},
+    {["Price"] = 10000, ["Efficiency"] = 1.2, ["Name"] = "Carolyn", ["Model"] = "sailormoon_sao1025"},
+    {["Price"] = 1000000, ["Efficiency"] = 1.5, ["Name"] = "Michy", ["Model"] = "MichyAsg"},
+    {["Price"] = 10000000, ["Efficiency"] = 2, ["Name"] = "Ed", ["Model"] = "Ed"}
+}
