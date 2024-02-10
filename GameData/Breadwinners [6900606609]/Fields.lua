605638fb9238077163c0a2cfb6f3c1af1bb7349e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-11 02:12:25 +1100

    Create Fields.lua

---
+++ GameData/Breadwinners [6900606609]/Fields.lua
@@ -0,0 +1,44 @@
+data = {
+    {
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Grain"},
+        {["Price"] = 50, ["Multiplier"] = 1, ["Name"] = "Sugar cane"},
+        {["Price"] = 10, ["Multiplier"] = 1, ["Name"] = "Ginger"},
+        {["Price"] = 10, ["Multiplier"] = 1, ["Name"] = "Pumpkin"},
+        {["Price"] = 10, ["Multiplier"] = 1, ["Name"] = "Carrot"},
+        {["Price"] = 10, ["Multiplier"] = 1, ["Name"] = "Peppermint"}
+    },
+    {
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Grain Field"},
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Sugar Field"},
+        {["Price"] = 180, ["Multiplier"] = 1, ["Name"] = "Grain Field"},
+        {["Price"] = 500, ["Multiplier"] = 1, ["Name"] = "Strawberry Field"},
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Ginger"},
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Pumpkin"},
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Carrot"},
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Peppermint"}
+    },
+    {
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Grain Field"},
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Sugar Field"},
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Grain Field"},
+        {["Price"] = 830, ["Multiplier"] = 1, ["Name"] = "Strawberry Field"},
+        {["Price"] = 9400, ["Multiplier"] = 1, ["Name"] = "Rose Field"},
+        {["Price"] = 430000, ["Multiplier"] = 1, ["Name"] = "Tangerine Field"},
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Ginger"},
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Pumpkin"},
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Carrot"},
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Peppermint"}
+    },
+    {
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Grain Field"},
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Sugar Field"},
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Grain Field"},
+        {["Price"] = 830, ["Multiplier"] = 1, ["Name"] = "Strawberry Field"},
+        {["Price"] = 9400, ["Multiplier"] = 1, ["Name"] = "Rose Field"},
+        {["Price"] = 430000, ["Multiplier"] = 1, ["Name"] = "Tangerine Field"},
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Ginger"},
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Pumpkin"},
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Carrot"},
+        {["Price"] = 0, ["Multiplier"] = 1, ["Name"] = "Peppermint"}
+    }
+}
