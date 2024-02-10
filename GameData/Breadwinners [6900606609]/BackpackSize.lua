14f88c81399f3edaf25b7b3025d2c6878c536208
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-11 02:02:19 +1100

    Create BackpackSize.lua

---
+++ GameData/Breadwinners [6900606609]/BackpackSize.lua
@@ -0,0 +1,82 @@
+data = {
+    {
+        ["Name"] = "Starter Bag",
+        ["Model"] = "Starter Bag",
+        ["Price"] = 0,
+        ["Description"] = "10 Slots",
+        ["Icon"] = "http://www.roblox.com/asset/?id=7365795260",
+        ["Size"] = 10
+    },
+    {
+        ["Name"] = "Beginner Bag",
+        ["Model"] = "Beginner Bag",
+        ["Price"] = 20,
+        ["Description"] = "15 Slots.",
+        ["Icon"] = "http://www.roblox.com/asset/?id=7365795260",
+        ["Size"] = 15
+    },
+    {
+        ["Name"] = "Sparkling Bag",
+        ["Model"] = "Sparkling Bag",
+        ["Price"] = 250,
+        ["Description"] = "35 Slots",
+        ["Icon"] = "http://www.roblox.com/asset/?id=7365795785",
+        ["Size"] = 35
+    },
+    {
+        ["Name"] = "Shining Bag",
+        ["Model"] = "Shining Bag",
+        ["Price"] = 2800,
+        ["Description"] = "70 Slots",
+        ["Icon"] = "http://www.roblox.com/asset/?id=7365796226",
+        ["Size"] = 70
+    },
+    {
+        ["Name"] = "Godly Bag",
+        ["Model"] = "Godly Bag",
+        ["Price"] = 5100,
+        ["Description"] = "100 Slots",
+        ["Icon"] = "http://www.roblox.com/asset/?id=7365796594",
+        ["Size"] = 100
+    },
+    {
+        ["Name"] = "Demonic Bag",
+        ["Model"] = "Demonic Bag",
+        ["Price"] = 32000,
+        ["Description"] = "150 Slots",
+        ["Icon"] = "http://www.roblox.com/asset/?id=7420367051",
+        ["Size"] = 150
+    },
+    {
+        ["Name"] = "Water Bag",
+        ["Model"] = "Water Bag",
+        ["Price"] = 94000,
+        ["Description"] = "200 Slots",
+        ["Icon"] = "http://www.roblox.com/asset/?id=7478774175",
+        ["Size"] = 200
+    },
+    {
+        ["Name"] = "Phoenix Bag",
+        ["Model"] = "Phoenix Bag",
+        ["Price"] = 160000,
+        ["Description"] = "300 Slots",
+        ["Icon"] = "http://www.roblox.com/asset/?id=7478764031",
+        ["Size"] = 300
+    },
+    {
+        ["Name"] = "Void Bag",
+        ["Model"] = "Void Bag",
+        ["Price"] = 280000,
+        ["Description"] = "400 Slots",
+        ["Icon"] = "http://www.roblox.com/asset/?id=7478772824",
+        ["Size"] = 400
+    },
+    {
+        ["Name"] = "Halloween Bag",
+        ["Model"] = "HalloweenBag",
+        ["Price"] = false,
+        ["Description"] = "500 Slots",
+        ["Icon"] = "rbxassetid://1073429497",
+        ["Size"] = 500
+    }
+}
