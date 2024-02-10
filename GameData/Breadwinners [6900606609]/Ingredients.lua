43148d9fb9554986272c5abb774f64b8768bd85c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-11 02:12:47 +1100

    Create Ingredients.lua

---
+++ GameData/Breadwinners [6900606609]/Ingredients.lua
@@ -0,0 +1,51 @@
+data = {
+    {
+        ["ItemName"] = "Chocolate",
+        ["Name"] = "Chocolate Bar",
+        ["Price"] = 10,
+        ["Icon"] = "http://www.roblox.com/asset/?id=11832757468",
+        ["Description"] = "For Chocolate Glaze."
+    },
+    {
+        ["ItemName"] = "Vanille",
+        ["Name"] = "Vanille Powder",
+        ["Price"] = 10,
+        ["Icon"] = "http://www.roblox.com/asset/?id=11832757726",
+        ["Description"] = "For Vanille Glaze."
+    },
+    {
+        ["ItemName"] = "Whipped Cream",
+        ["Name"] = "Whipped Cream",
+        ["Price"] = 100,
+        ["Icon"] = "http://www.roblox.com/asset/?id=11832757890",
+        ["Description"] = ""
+    },
+    {
+        ["ItemName"] = "Candle",
+        ["Name"] = "Candle",
+        ["Price"] = 100,
+        ["Icon"] = "http://www.roblox.com/asset/?id=11832757622",
+        ["Description"] = ""
+    },
+    {
+        ["ItemName"] = "Banana",
+        ["Name"] = "Banana",
+        ["Price"] = 100,
+        ["Icon"] = "rbxassetid://14997394111",
+        ["Description"] = ""
+    },
+    {
+        ["ItemName"] = "Cinnamon",
+        ["Name"] = "Cinnamon",
+        ["Price"] = 1000,
+        ["Icon"] = "rbxassetid://15189251396",
+        ["Description"] = ""
+    },
+    {
+        ["ItemName"] = "Vinegar",
+        ["Name"] = "Vinegar",
+        ["Price"] = 1000,
+        ["Icon"] = "rbxassetid://15269731058",
+        ["Description"] = ""
+    }
+}
