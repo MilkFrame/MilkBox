460bd3391f57b00d8e8e24e682b838e85e3a91dd
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-14 01:32:02 +1100

    Create Sabotage.lua

---
+++ GameData/Tower Heroes [4646477729]/Sabotage.lua
@@ -0,0 +1,363 @@
+data = {
+    ["Attacks"] = {
+        {
+            ["Noise"] = 1841390909,
+            ["Image"] = 7112594148,
+            ["Name"] = "Mirror",
+            ["Positive"] = true,
+            ["ID"] = "mirrorSab",
+            ["RunFunction"] = "function: 0x00000000ac4b5519",
+            ["Cost"] = 100,
+            ["Desc"] = "Use the last sabotage that the opposing team used."
+        },
+        {
+            ["Image"] = 7147727505,
+            ["Name"] = "Spawn Enemies",
+            ["Cost"] = 70,
+            ["ID"] = "spawnEnemy",
+            ["RunFunction"] = "function: 0x00000000ca5ba211",
+            ["Desc"] = "Spawn a few Random Enemies from the current wave."
+        },
+        {
+            ["Image"] = 12958474287,
+            ["Name"] = "Troublemaker Egg",
+            ["Cost"] = 70,
+            ["ID"] = "egg_Troublemaker",
+            ["RunFunction"] = "function: 0x000000007d22bee1",
+            ["Check"] = "function: 0x000000001764a459",
+            ["Desc"] = "Spawn a Troublemaker Egg."
+        },
+        {
+            ["Image"] = 15164483829,
+            ["Name"] = "Imp",
+            ["Cost"] = 130,
+            ["ID"] = "bounty_Imp",
+            ["RunFunction"] = "function: 0x000000004a2137f1",
+            ["Check"] = "function: 0x0000000055e33b89",
+            ["Desc"] = "Spawn an Imp."
+        },
+        {
+            ["Image"] = 10007216646,
+            ["Name"] = "Mini Boss",
+            ["Cost"] = 200,
+            ["ID"] = "miniBossT",
+            ["RunFunction"] = "function: 0x0000000065bb7041",
+            ["Check"] = "function: 0x00000000174b9f69",
+            ["Desc"] = "Spawn a random mini boss for the opposing team."
+        },
+        {
+            ["Image"] = 7106153455,
+            ["Name"] = "Bombie",
+            ["Cost"] = 80,
+            ["ID"] = "bombRush",
+            ["RunFunction"] = "function: 0x0000000091dd03b1",
+            ["Check"] = "function: 0x00000000dbcce7f9",
+            ["Desc"] = "Spawn a few Bombies for the opposing team."
+        },
+        {
+            ["Image"] = 7112341915,
+            ["Name"] = "Tornado",
+            ["Cost"] = 100,
+            ["ID"] = "tornado",
+            ["RunFunction"] = "function: 0x000000005dc1eca1",
+            ["Check"] = "function: 0x000000009fc22e89",
+            ["Desc"] = "Spawn a Tornado for the opposing team that will wreck everything in its path."
+        },
+        {
+            ["Image"] = 7101599070,
+            ["Name"] = "Fast Enemies",
+            ["Cost"] = 90,
+            ["ID"] = "speedEnemies",
+            ["RunFunction"] = "function: 0x0000000013c184d9",
+            ["Desc"] = "Increase the speed of enemies for the opposing team."
+        },
+        {
+            ["Image"] = 7102456875,
+            ["Name"] = "Healing",
+            ["Cost"] = 80,
+            ["ID"] = "regenEnemy",
+            ["RunFunction"] = "function: 0x00000000cb6f2b79",
+            ["Desc"] = "Enemies on the opposing team will slowly regain health for 20 seconds."
+        },
+        {
+            ["Image"] = 7112552556,
+            ["Name"] = "Burning",
+            ["Cost"] = 80,
+            ["ID"] = "burnFriend",
+            ["RunFunction"] = "function: 0x00000000db250e01",
+            ["Desc"] = "Friendly Summons on the opposing team will catch on fire for 20 seconds."
+        },
+        {
+            ["Image"] = 7107402965,
+            ["Name"] = "Freezing",
+            ["Cost"] = 100,
+            ["ID"] = "freezeFriend",
+            ["RunFunction"] = "function: 0x00000000fadb1569",
+            ["Desc"] = "Friendly Summons on the opposing team will be frozen for 10 seconds."
+        },
+        {
+            ["LocalFunction"] = "function: 0x000000008d4a17d9",
+            ["Image"] = 7102058035,
+            ["Name"] = "Splatter",
+            ["Cost"] = 110,
+            ["ID"] = "paintSplat",
+            ["RunFunction"] = "function: 0x0000000025dea551",
+            ["Noise"] = 1845105232,
+            ["Desc"] = "Cover the screens of the opposing team with paint for 20 seconds!"
+        },
+        {
+            ["Noise"] = 10337055816,
+            ["Cost"] = 110,
+            ["Desc"] = "Scare the other team.",
+            ["Image"] = 13180481350,
+            ["Name"] = "Jumpscare",
+            ["LocalFunction"] = "function: 0x0000000098f9be09",
+            ["ID"] = "jumpscare_Doors",
+            ["HideNotif"] = true,
+            ["RunFunction"] = "function: 0x00000000913c1901",
+            ["Check"] = "function: 0x0000000047cf3e29"
+        },
+        {
+            ["Image"] = 7105915325,
+            ["Name"] = "Move Hero",
+            ["Cost"] = 110,
+            ["ID"] = "positionSwap",
+            ["RunFunction"] = "function: 0x00000000ea4fe039",
+            ["Desc"] = "Swap the places of two Heroes on the opposing team."
+        },
+        {
+            ["Image"] = 7112734682,
+            ["Name"] = "Taxes",
+            ["Cost"] = 40,
+            ["ID"] = "taxCat",
+            ["RunFunction"] = "function: 0x0000000095d925d1",
+            ["Check"] = "function: 0x000000006f48b2c9",
+            ["Desc"] = "Next wave, Lemonade Cats on the opposing team will give your team half of the mana they profited."
+        },
+        {
+            ["Image"] = 10007215854,
+            ["Name"] = "Stun Immunity",
+            ["Cost"] = 120,
+            ["ID"] = "stumBreak",
+            ["RunFunction"] = "function: 0x000000008d56dd99",
+            ["Desc"] = "The Heroes on your team will become immune to Stun for 60s."
+        },
+        {
+            ["Image"] = 7105945562,
+            ["Name"] = "Less Damage",
+            ["Cost"] = 70,
+            ["ID"] = "lessDamage",
+            ["RunFunction"] = "function: 0x000000002978aee1",
+            ["Desc"] = "Decrease the Damage of Heroes on the opposing team  for 20 seconds."
+        },
+        {
+            ["Image"] = 7107373545,
+            ["Name"] = "More Delay",
+            ["Cost"] = 70,
+            ["ID"] = "moreRate",
+            ["RunFunction"] = "function: 0x00000000c44e4499",
+            ["Desc"] = "Increase the Delay of Heroes on the opposing team  for 20 seconds."
+        },
+        {
+            ["Image"] = 7105983619,
+            ["Name"] = "Less Range",
+            ["Cost"] = 60,
+            ["ID"] = "lessRange",
+            ["RunFunction"] = "function: 0x00000000b65df791",
+            ["Desc"] = "Decrease the Range of Heroes on the opposing team for 20 seconds."
+        },
+        {
+            ["Image"] = 7106031951,
+            ["Name"] = "Downgrade",
+            ["Cost"] = 120,
+            ["ID"] = "downGrade",
+            ["RunFunction"] = "function: 0x00000000674c1be9",
+            ["Desc"] = "Decrease the level of one hero for the opposing team."
+        },
+        {
+            ["LocalFunction"] = "function: 0x00000000f2d29b79",
+            ["Image"] = 7107339570,
+            ["Name"] = "Eliminate",
+            ["Cost"] = 100,
+            ["ID"] = "removeHero",
+            ["RunFunction"] = "function: 0x00000000453d3fc1",
+            ["Noise"] = 1837715911,
+            ["Desc"] = "Remove one hero on the opposing team."
+        },
+        {
+            ["Image"] = 7105879383,
+            ["Name"] = "More HP",
+            ["Cost"] = 70,
+            ["ID"] = "extraHPE",
+            ["RunFunction"] = "function: 0x0000000038d07031",
+            ["Desc"] = "Increase the health of enemies on the opposing team."
+        },
+        {
+            ["Image"] = 7106084906,
+            ["Name"] = "Stun",
+            ["Cost"] = 130,
+            ["ID"] = "stunHero",
+            ["RunFunction"] = "function: 0x000000007ef3c909",
+            ["Noise"] = 1837715911,
+            ["Desc"] = "Stun all Heroes on the opposing team for 5 seconds."
+        },
+        {
+            ["Image"] = 7106116538,
+            ["Name"] = "Cost Up",
+            ["Cost"] = 80,
+            ["ID"] = "moreUpgrade",
+            ["RunFunction"] = "function: 0x00000000f8d30d59",
+            ["Desc"] = "Increase the price of upgrades on the opposing team for 20 seconds."
+        },
+        {
+            ["LocalFunction"] = "function: 0x00000000ddc73431",
+            ["Image"] = 7106256355,
+            ["Name"] = "Pop-ups",
+            ["Cost"] = 100,
+            ["ID"] = "lemonAds",
+            ["RunFunction"] = "function: 0x00000000c2113481",
+            ["Noise"] = 1846251238,
+            ["Desc"] = "Cover the screens of the opposing team with pop-ups which can only be closed manually!"
+        },
+        {
+            ["Noise"] = 1846251247,
+            ["Cost"] = 90,
+            ["Desc"] = "Increase the Max HP of the Base and restore the health of your team.",
+            ["Image"] = 7106429966,
+            ["Name"] = "Extra HP",
+            ["ID"] = "baseHP",
+            ["RunFunction"] = "function: 0x00000000393689f1",
+            ["Check"] = "function: 0x000000007fb0adb9",
+            ["Positive"] = true
+        },
+        {
+            ["Noise"] = 1846251247,
+            ["Positive"] = true,
+            ["Desc"] = "Restore the health of your Base for your team.",
+            ["Image"] = 7112761776,
+            ["Name"] = "Base Heal",
+            ["ID"] = "baseHeal",
+            ["RunFunction"] = "function: 0x00000000b417d861",
+            ["Check"] = "function: 0x00000000c3977149",
+            ["Cost"] = 70
+        },
+        {
+            ["Image"] = 7106630177,
+            ["Name"] = "Cooldown",
+            ["Cost"] = 60,
+            ["ID"] = "cooldown",
+            ["RunFunction"] = "function: 0x00000000c1f65e11",
+            ["Check"] = "function: 0x0000000077f70119",
+            ["Desc"] = "Force all Heroes that have abilities on the opposing team to enter their cooldown."
+        },
+        {
+            ["Image"] = 7106897911,
+            ["Name"] = "Spaced",
+            ["Cost"] = 90,
+            ["ID"] = "spaceUp",
+            ["RunFunction"] = "function: 0x00000000b76cc2a1",
+            ["Desc"] = "Heroes on the opposing team will take up more space when being placed, this will last until the game ends."
+        },
+        {
+            ["Check"] = "function: 0x000000009fb2d049",
+            ["Image"] = 7106589794,
+            ["Name"] = "Vengeful",
+            ["Cost"] = 130,
+            ["ID"] = "ghostedEne",
+            ["RunFunction"] = "function: 0x0000000076f0e331",
+            ["Noise"] = 1844115779,
+            ["Desc"] = "Until the end of the next wave, enemies on the opposing team will respawn as a ghost upon death."
+        },
+        {
+            ["Image"] = 7106324859,
+            ["Name"] = "Skip Wave",
+            ["Cost"] = 20,
+            ["ID"] = "skipWave",
+            ["RunFunction"] = "function: 0x000000002e088441",
+            ["Check"] = "function: 0x00000000eb98f369",
+            ["Desc"] = "Start the next wave before your team or the opposing team have defeated all the enemies."
+        },
+        {
+            ["Check"] = "function: 0x000000004fb9abf9",
+            ["Image"] = 7163410049,
+            ["Name"] = "Hero Limit",
+            ["Cost"] = 110,
+            ["ID"] = "heroLimit",
+            ["RunFunction"] = "function: 0x00000000ad05cfb1",
+            ["Noise"] = 1846251238,
+            ["Desc"] = "Increase the overall Hero limit for both teams."
+        },
+        {
+            ["LocalFunction"] = "function: 0x000000004002e0a1",
+            ["Image"] = 7107249453,
+            ["Name"] = "Fling",
+            ["Cost"] = 10,
+            ["ID"] = "flingPlayer",
+            ["RunFunction"] = "function: 0x00000000eb840a89",
+            ["Noise"] = 1836245036,
+            ["Desc"] = "Players on the opposing team will be launched in random direction!"
+        },
+        {
+            ["LocalFunction"] = "function: 0x000000004dbe8e39",
+            ["Image"] = 7112645482,
+            ["Name"] = "FOV Change",
+            ["Cost"] = 20,
+            ["ID"] = "fovPlayer",
+            ["RunFunction"] = "function: 0x00000000c0839a29",
+            ["Noise"] = 1836245036,
+            ["Desc"] = "Players on the opposing team will have their field of view changed for 20 seconds!"
+        },
+        {
+            ["LocalFunction"] = "function: 0x00000000c10c2d71",
+            ["Image"] = 7107219542,
+            ["Name"] = "Explode",
+            ["Cost"] = 30,
+            ["ID"] = "bombPlayer",
+            ["RunFunction"] = "function: 0x000000009e024501",
+            ["Noise"] = 1846902656,
+            ["Desc"] = "Players on the opposing team will explode and stun Heroes near them!"
+        },
+        {
+            ["LocalFunction"] = "function: 0x0000000028bff779",
+            ["Image"] = 7112301657,
+            ["Name"] = "Mines",
+            ["Cost"] = 30,
+            ["ID"] = "minePlayer",
+            ["RunFunction"] = "function: 0x00000000ac9e3fe9",
+            ["Noise"] = 1846902656,
+            ["Desc"] = "Deploy a mine that will explode when stepped on by a player from the opposing team!"
+        },
+        {
+            ["LocalFunction"] = "function: 0x00000000c29e8d09",
+            ["Image"] = 7112688700,
+            ["Name"] = "Tutoro",
+            ["Cost"] = 90,
+            ["ID"] = "tutoroKill",
+            ["RunFunction"] = "function: 0x00000000f30f6431",
+            ["Noise"] = 1304824213,
+            ["Desc"] = "Teach players on the opposing team a valuable lesson!"
+        },
+        {
+            ["Check"] = "function: 0x000000006870b221",
+            ["Image"] = 7107300653,
+            ["Name"] = "Shrink",
+            ["Cost"] = 50,
+            ["ID"] = "shrinkPlayer",
+            ["RunFunction"] = "function: 0x00000000b6716959",
+            ["Noise"] = 1836245036,
+            ["Desc"] = "Players on the opposing team will shrink for 40 seconds!"
+        },
+        {
+            ["Check"] = "function: 0x00000000c36e01d1",
+            ["Image"] = 7107286557,
+            ["Name"] = "Heavy",
+            ["Cost"] = 60,
+            ["ID"] = "noJump",
+            ["RunFunction"] = "function: 0x00000000399d59a9",
+            ["Noise"] = 1836245036,
+            ["Desc"] = "Players on the opposing cannot jump for 40 seconds!"
+        }
+    },
+    ["GetTeamMessages"] = "function: 0x00000000fb2b64f1",
+    ["GetSabotage"] = "function: 0x0000000055748341"
+}
