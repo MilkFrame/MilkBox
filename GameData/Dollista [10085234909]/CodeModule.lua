c635d3af9fde0a1e4af138857f24e2c1cf02a763
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-14 00:49:27 +1100

    Create CodeModule.lua

---
+++ GameData/Dollista [10085234909]/CodeModule.lua
@@ -0,0 +1,21 @@
+data = {
+    ["trfgtr"] = {
+        ["Type"] = "Cash",
+        ["MSG"] = "How did you know?!?! Here is 1000$ DONT TELL ANYONE.",
+        ["Reward"] = "1000"
+    },
+    ["ftegtfdf"] = {["Type"] = "Cash", ["MSG"] = "Last word queen!", ["Reward"] = "0"},
+    ["gfefd"] = {["Type"] = "Cash", ["MSG"] = "Nothing is cuter than Hello Kitty!", ["Reward"] = "5000"},
+    ["DOLLISTAGRAM"] = {
+        ["Type"] = "Item",
+        ["MSG"] = "You will look like a true fan with this top!",
+        ["Reward"] = "Follower Top"
+    },
+    ["tfgfgtf"] = {["Type"] = "Cash", ["MSG"] = "Ho, Ho, Ho! This code gives you: 5000 Coins!", ["Reward"] = "5000"},
+    ["DOLLISTA"] = {["Type"] = "Cash", ["MSG"] = "Welcome to Dollista! You recived 5000$", ["Reward"] = 5000},
+    ["GLOSS ANGELES"] = {["Type"] = "Cash", ["MSG"] = "Welcome to Gloss Angeles!", ["Reward"] = "1000"},
+    ["asdghafg"] = {["Type"] = "Item", ["MSG"] = "Unlocked Rare Galaxy Winter Top", ["Reward"] = "Galaxy Winter Top"},
+    ["trrfgtfr"] = {["Type"] = "Cash", ["MSG"] = "You've gotten 2500 stlyists!", ["Reward"] = "5000"},
+    ["SCHOOL"] = {["Type"] = "Cash", ["MSG"] = "Check Out the new school!", ["Reward"] = "1000"},
+    ["gvdsfgr"] = {["Type"] = "Cash", ["MSG"] = "YOU GOTTEN 10K COINS SLAYY!", ["Reward"] = "10000"}
+}
