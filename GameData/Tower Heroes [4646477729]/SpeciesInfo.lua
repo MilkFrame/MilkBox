c438cdbdb54d21ded9ed6f29de412444ed76643d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-14 01:30:31 +1100

    Create SpeciesInfo.lua

---
+++ GameData/Tower Heroes [4646477729]/SpeciesInfo.lua
@@ -0,0 +1,150 @@
+data = {
+    ["GetEventData"] = "function: 0x000000009828c2d9",
+    ["Species"] = {
+        {
+            ["MapInfo"] = {["Map"] = "CastleTown"},
+            ["Species"] = "Slime",
+            ["Image"] = 7240399443,
+            ["Desc"] = "Enemies that are common to come by! The most common type in the game!"
+        },
+        {
+            ["MapInfo"] = {["Map"] = "PiratePanic"},
+            ["Species"] = "Goblin",
+            ["Image"] = 7240031245,
+            ["Desc"] = "There's a lot of these guys everywhere, they do most heavy duty jobs!"
+        },
+        {
+            ["MapInfo"] = {["Map"] = "SilentSanctuary"},
+            ["Species"] = "Plant",
+            ["Image"] = 7240284631,
+            ["Desc"] = "Enemies made out of natural elements and materials!"
+        },
+        {
+            ["MapInfo"] = {["Map"] = "AlienAttack"},
+            ["Species"] = "Alien",
+            ["Image"] = 7239727867,
+            ["Desc"] = "Visitors from another planet commonly found on Alien Attack."
+        },
+        {
+            ["MapInfo"] = {["Map"] = "BleakBarrens"},
+            ["Species"] = "Undead",
+            ["Image"] = 7240429675,
+            ["Desc"] = "Ghouls and ghosts here to haunt you! Watch out for these spooky enemies!"
+        },
+        {
+            ["MapInfo"] = {["Map"] = "VolcanoValley"},
+            ["Species"] = "Golem",
+            ["Image"] = 7240068462,
+            ["Desc"] = "All of the rock-like enemies that come from the ground."
+        },
+        {
+            ["MapInfo"] = {["Map"] = "FranticForest"},
+            ["Species"] = "Animal",
+            ["Image"] = 7239765507,
+            ["Desc"] = "Animals that come in all shapes and sizes!"
+        },
+        {
+            ["MapInfo"] = {["Map"] = "RadiantReef"},
+            ["Species"] = "Aquatic",
+            ["Image"] = 7658471733,
+            ["Desc"] = "Marine Life that has come from the sea!"
+        },
+        {
+            ["MapInfo"] = {["Map"] = "BeehiveBlitz"},
+            ["Species"] = "Bug",
+            ["Image"] = 7239789443,
+            ["Desc"] = "Everything ranging from Bees, Spiders, and other creepy crawlers!"
+        },
+        {
+            ["MapInfo"] = {["Map"] = "CloudyCatastrophe"},
+            ["Species"] = "Element",
+            ["Image"] = 7239916907,
+            ["Desc"] = "Enemies made out of different elements not normally found in nature."
+        },
+        {
+            ["MapInfo"] = {["Map"] = "CorporateChaos"},
+            ["Species"] = "People",
+            ["Image"] = 7240130771,
+            ["Desc"] = "Enemies that resemble humanoid type creatures."
+        },
+        {
+            ["MapInfo"] = {["Map"] = "GrandGarage"},
+            ["Species"] = "Object",
+            ["Image"] = 7240219394,
+            ["Desc"] = "Everyday objects that have somehow gained sentience!"
+        },
+        {
+            ["MapInfo"] = {["Map"] = "FacilityFrenzy"},
+            ["Species"] = "Robot",
+            ["Image"] = 7240347682,
+            ["Desc"] = "Machines of all types composed of parts, electricity, and advanced engineering."
+        },
+        {
+            ["MapInfo"] = {["Map"] = "SacredShrine"},
+            ["Species"] = "Reaper",
+            ["Image"] = 7240315693,
+            ["Desc"] = "Tough enemies seeking your defeat. Keepers of undead enemies."
+        },
+        {
+            ["MapInfo"] = {["Map"] = "1Random"},
+            ["Species"] = "Mimic",
+            ["Image"] = 7147534047,
+            ["Desc"] = "Rare enemies that have a small chance of appearing, defeat them for a special reward!"
+        },
+        {
+            ["Image"] = 7239874732,
+            ["EventEnemy"] = "Halloween",
+            ["MapInfo"] = {["Map"] = "HalloweenHavoc"},
+            ["EventCurrency"] = true,
+            ["Desc"] = "Every creepy and spooky enemy seen during Halloween!"
+        },
+        {
+            ["Image"] = 7239827961,
+            ["EventEnemy"] = "Christmas",
+            ["MapInfo"] = {["Map"] = "ToyTakeover"},
+            ["EventCurrency"] = true,
+            ["Desc"] = "All types of festive enemies seen during the holidays!"
+        },
+        {
+            ["Image"] = 7240096603,
+            ["EventEnemy"] = "Easter",
+            ["MapInfo"] = {["Map"] = "EasterIsla"},
+            ["EventCurrency"] = true,
+            ["Desc"] = "All of the Easter enemies seen during Easter!"
+        },
+        {
+            ["EventEnemy"] = "Metaverse",
+            ["Image"] = 7240172031,
+            ["MapInfo"] = {["Map"] = "MetaverseMadness"},
+            ["Desc"] = "Enemies seen all across the Metaverse and are after the Champions!"
+        },
+        {
+            ["Image"] = 13180481350,
+            ["EventEnemy"] = "Doors",
+            ["MapInfo"] = {["Map"] = "DoorsMap"},
+            ["EventCurrency"] = true,
+            ["Desc"] = "Entities seen in the game Doors by LSplash!"
+        },
+        {
+            ["EventEnemy"] = "Oddport",
+            ["Image"] = 7240244008,
+            ["MapInfo"] = {["Map"] = "Oddport Academy"},
+            ["Desc"] = "Characters seen in the Oddport Academy webcomic by Degnut!"
+        },
+        {
+            ["EventEnemy"] = "Robot64",
+            ["Image"] = 7240376683,
+            ["MapInfo"] = {["Map"] = "KnoddyResort"},
+            ["Desc"] = "Enemies seen in Knoddy's Resort from Robot 64 by zKevin!"
+        },
+        {
+            ["Image"] = 15800566335,
+            ["EventEnemy"] = "Roliday",
+            ["MapInfo"] = {["Map"] = "RolidayRumble"},
+            ["Hidden"] = true,
+            ["Desc"] = "Enemies seen in Roliday Rumble by Roliday!"
+        }
+    },
+    ["GetEnemies"] = "function: 0x00000000875ce1a9",
+    ["GetSpeciesData"] = "function: 0x000000007e66f091"
+}
