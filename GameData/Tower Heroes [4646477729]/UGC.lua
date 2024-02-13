1d0fe692160fd9f16a89fdc40f034199cbc0d9d4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-14 00:56:20 +1100

    Create UGC.lua

---
+++ GameData/Tower Heroes [4646477729]/UGC.lua
@@ -0,0 +1,34 @@
+data = {
+    ["ID"] = {
+        8919553114,
+        8919531951,
+        8919581408,
+        8919490052,
+        8919336599,
+        8919447077,
+        8919389997,
+        9058323640,
+        9058343261,
+        9058353246,
+        9058400602,
+        9121972929,
+        9363043236,
+        9363058777
+    },
+    ["Name"] = {
+        "Husk Mask",
+        "Stump Mask",
+        "CAT Charm",
+        "Chirp Friend",
+        "Synth Mask",
+        "Cherry Friend",
+        "Byte Egg",
+        "Catware Head",
+        "Cacti Friend",
+        "Tutoro Friend",
+        "Lemonade Cat Friend",
+        "Sus Noob",
+        "Wuffle",
+        "Friendship Necklace"
+    }
+}
