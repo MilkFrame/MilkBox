e8d7b8a35ba9d1a057c749e6d876b360f3e675d6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-14 01:31:24 +1100

    Create DeckQuest.lua

---
+++ GameData/Tower Heroes [4646477729]/DeckQuest.lua
@@ -0,0 +1,110 @@
+data = {
+    ["GetDeck"] = "function: 0x00000000ec3458e1",
+    ["Decks"] = {
+        {["ID"] = "Holiday Heroes", ["Heroes"] = {"Bunny", "Hayes", "Wafer"}, ["Reward"] = 35},
+        {["ID"] = "Enemy Area", ["Heroes"] = {"Scientist", "Slime King", "Beetrice", "Oddport"}, ["Reward"] = 40},
+        {["ID"] = "Chain Link", ["Heroes"] = {"Hotdog Frank", "Volt"}, ["Reward"] = 35},
+        {["ID"] = "Magic Trick", ["Heroes"] = {"Maitake", "Bunny", "Jester"}, ["Reward"] = 40},
+        {["ID"] = "Speed of Sound", ["Heroes"] = {"Yasuke", "Sparks Kilowatt", "Beebo"}, ["Reward"] = 40},
+        {["ID"] = "Picnic", ["Heroes"] = {"Beetrice", "Voca", "Stella"}, ["Reward"] = 45},
+        {["ID"] = "Race Day", ["Heroes"] = {"Kart Kid", "Beebo"}, ["Reward"] = 30},
+        {["ID"] = "Night Rider", ["Heroes"] = {"Kart Kid", "Volt", "Lemonade Cat"}, ["Reward"] = 40},
+        {["ID"] = "Probably Evil", ["Heroes"] = {"Beetrice", "Fracture", "Quinn", "Branch"}, ["Reward"] = 40},
+        {["ID"] = "Maniacs", ["Heroes"] = {"Byte", "Volt", "Jester", "Hayes", "Spectre"}, ["Reward"] = 45},
+        {["ID"] = "Cool Guys", ["Heroes"] = {"Branch", "Chef", "Hotdog Frank", "Keith"}, ["Reward"] = 40},
+        {
+            ["ID"] = "Too Smol",
+            ["Heroes"] = {"Stella", "Wafer", "Soda Pop", "Hotdog Frank", "Discount Dog"},
+            ["Reward"] = 60
+        },
+        {
+            ["ID"] = "Pipsqueaks",
+            ["Heroes"] = {"Dumpster Child", "Soda Pop", "Balloon Pal", "Keith", "Discount Dog"},
+            ["Reward"] = 55
+        },
+        {["ID"] = "Clutz", ["Heroes"] = {"Voca", "Kart Kid", "Maitake", "Jester"}, ["Reward"] = 40},
+        {["ID"] = "Bad to Good", ["Heroes"] = {"Wafer", "Branch", "Fracture", "Slime King", "Jester"}, ["Reward"] = 55},
+        {["ID"] = "Byte in Space", ["Heroes"] = {"Byte", "Sparks Kilowatt"}, ["Reward"] = 40},
+        {["ID"] = "Collab Heroes", ["Heroes"] = {"Beebo", "Oddport", "Sparks Kilowatt"}, ["Reward"] = 45},
+        {["ID"] = "Nature", ["Heroes"] = {"Branch", "Bunny", "Quinn"}, ["Reward"] = 50},
+        {["ID"] = "Magic Users", ["Heroes"] = {"Wizard", "Fracture", "Branch", "Beetrice"}, ["Reward"] = 55},
+        {
+            ["ID"] = "Petting Zoo",
+            ["Heroes"] = {"Keith", "Bunny", "Soda Pop", "Discount Dog", "Lemonade Cat"},
+            ["Reward"] = 75
+        },
+        {["ID"] = "Bake Off", ["Heroes"] = {"Chef", "Dumpster Child", "Hotdog Frank", "Wafer"}, ["Reward"] = 70},
+        {["ID"] = "Area Damage", ["Heroes"] = {"Branch", "Hotdog Frank", "Keith", "Soda Pop", "Voca"}, ["Reward"] = 65},
+        {["ID"] = "Blaster", ["Heroes"] = {"Sparks Kilowatt", "Scientist", "Beebo"}, ["Reward"] = 50},
+        {["ID"] = "The Swarm", ["Heroes"] = {"Stella", "Beetrice", "Hayes", "Maitake"}, ["Reward"] = 70},
+        {["ID"] = "Alleyway", ["Heroes"] = {"Soda Pop", "Dumpster Child"}, ["Reward"] = 45},
+        {["ID"] = "Boom Squad", ["Heroes"] = {"Bunny", "Soda Pop", "Sparks Kilowatt", "Stella"}, ["Reward"] = 65},
+        {["ID"] = "Art Class", ["Heroes"] = {"Stella", "Keith", "Oddport", "Voca"}, ["Reward"] = 65},
+        {["ID"] = "Cat and Mouse", ["Heroes"] = {"Lemonade Cat", "Soda Pop"}, ["Reward"] = 25},
+        {["ID"] = "Ghost Buster", ["Heroes"] = {"Scientist", "Byte", "Spectre", "Lure"}, ["Reward"] = 50},
+        {["ID"] = "Ghosts", ["Heroes"] = {"Spectre", "Lure"}, ["Reward"] = 30},
+        {["ID"] = "Resident", ["Heroes"] = {"Dumpster Child", "Lure", "Lemonade Cat"}, ["Reward"] = 35},
+        {["ID"] = "Drain", ["Heroes"] = {"Lure", "Volt", "Scientist"}, ["Reward"] = 35},
+        {["ID"] = "Halloween", ["Heroes"] = {"Lure", "Hayes"}, ["Reward"] = 30},
+        {["ID"] = "The Lab", ["Heroes"] = {"Scientist", "Byte"}, ["Reward"] = 30},
+        {
+            ["ID"] = "Status Effects",
+            ["Heroes"] = {"Dumpster Child", "Maitake", "Yasuke", "Slime King", "Jester"},
+            ["Reward"] = 65
+        },
+        {["ID"] = "Ranged", ["Heroes"] = {"Beebo", "Branch", "Voca", "Quinn", "Balloon Pal"}, ["Reward"] = 60},
+        {["ID"] = "Target Practice", ["Heroes"] = {"Quinn", "Soda Pop", "Balloon Pal"}, ["Reward"] = 35},
+        {["ID"] = "Slow and Steady", ["Heroes"] = {"Quinn", "Chef", "Voca"}, ["Reward"] = 30},
+        {["ID"] = "BIG RADIUS", ["Heroes"] = {"Balloon Pal", "Voca"}, ["Reward"] = 25},
+        {["ID"] = "Healthy Slimes", ["Heroes"] = {"Maitake", "Slime King"}, ["Reward"] = 25},
+        {["ID"] = "Hack 'n' Slash", ["Heroes"] = {"Byte", "Chef", "Yasuke"}, ["Reward"] = 30},
+        {["ID"] = "Art Show", ["Heroes"] = {"Lemonade Cat", "Stella"}, ["Reward"] = 25},
+        {["ID"] = "Rockstars", ["Heroes"] = {"Spectre", "Voca"}, ["Reward"] = 25},
+        {["ID"] = "Stunt man", ["Heroes"] = {"Kart Kid", "Chef"}, ["Reward"] = 25},
+        {["ID"] = "Witchery", ["Heroes"] = {"Hayes", "Wizard", "Maitake"}, ["Reward"] = 40},
+        {["ID"] = "Crop Circle", ["Heroes"] = {"Hayes", "Beebo"}, ["Reward"] = 30},
+        {["ID"] = "Baked", ["Heroes"] = {"Wafer", "Dumpster Child"}, ["Reward"] = 25},
+        {["ID"] = "Airborne", ["Heroes"] = {"Hayes", "Balloon Pal", "Sparks Kilowatt"}, ["Reward"] = 45},
+        {["ID"] = "Wizards", ["Heroes"] = {"Wizard", "Oddport"}, ["Reward"] = 30},
+        {["ID"] = "This is fine", ["Heroes"] = {"Dumpster Child", "Discount Dog"}, ["Reward"] = 30},
+        {
+            ["ID"] = "All Damage?",
+            ["Heroes"] = {"Scientist", "Branch", "Chef", "Fracture", "Sparks Kilowatt"},
+            ["Reward"] = 65
+        },
+        {
+            ["ID"] = "Too many Heroes",
+            ["Heroes"] = {"Wizard", "Bunny", "Yasuke", "Hotdog Frank", "Chef"},
+            ["Reward"] = 50
+        },
+        {["ID"] = "Underdogs", ["Heroes"] = {"Wizard", "Keith", "Bunny", "Volt"}, ["Reward"] = 60},
+        {["ID"] = "Shocker", ["Heroes"] = {"Byte", "Volt"}, ["Reward"] = 30},
+        {["ID"] = "BIG DAMAGE", ["Heroes"] = {"Spectre", "Chef"}, ["Reward"] = 25},
+        {["ID"] = "Going Green", ["Heroes"] = {"Quinn", "Branch"}, ["Reward"] = 25},
+        {["ID"] = "Ultimate Buff", ["Heroes"] = {"Spectre", "Balloon Pal"}, ["Reward"] = 25},
+        {["ID"] = "Blimp", ["Heroes"] = {"Beebo", "Balloon Pal"}, ["Reward"] = 25},
+        {["ID"] = "Undead", ["Heroes"] = {"Fracture", "Maitake", "Volt", "Spectre"}, ["Reward"] = 60},
+        {["ID"] = "Glasses Gang", ["Heroes"] = {"Wizard", "Chef", "Scientist", "Sparks Kilowatt"}, ["Reward"] = 40},
+        {
+            ["ID"] = "Close Ranged",
+            ["Heroes"] = {"Yasuke", "Chef", "Soda Pop", "Hotdog Frank", "Spectre"},
+            ["Reward"] = 60
+        },
+        {["ID"] = "Robots", ["Heroes"] = {"Beebo", "Volt", "Sparks Kilowatt", "Scientist", "Byte"}, ["Reward"] = 65},
+        {["ID"] = "Summoner", ["Heroes"] = {"Slime King", "Oddport", "Kart Kid", "Jester", "Maitake"}, ["Reward"] = 60},
+        {["ID"] = "Let it Burn", ["Heroes"] = {"Slime King", "Dumpster Child"}, ["Reward"] = 25},
+        {
+            ["ID"] = "Classic",
+            ["Heroes"] = {"Wizard", "Chef", "Dumpster Child", "Scientist", "Lemonade Cat"},
+            ["Reward"] = 50
+        },
+        {
+            ["ID"] = "Supporter",
+            ["Heroes"] = {"Byte", "Maitake", "Discount Dog", "Balloon Pal", "Spectre"},
+            ["Reward"] = 80
+        },
+        {["ID"] = "Business Partners", ["Heroes"] = {"Lemonade Cat", "Discount Dog"}, ["Reward"] = 30},
+        {["ID"] = "Anti-Deck", ["Heroes"] = {"Quinn", "Keith", "Beetrice", "Stella", "Maitake"}, ["Reward"] = 100}
+    },
+    ["GetAvaliable"] = "function: 0x00000000a3725389"
+}
