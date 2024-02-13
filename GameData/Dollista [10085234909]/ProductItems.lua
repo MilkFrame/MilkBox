f0c360a21fc355da0ec47264c03cc4bccc20ff96
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-14 00:49:46 +1100

    Create ProductItems.lua

---
+++ GameData/Dollista [10085234909]/ProductItems.lua
@@ -0,0 +1,282 @@
+data = {
+    {
+        ["Items"] = {"Spring Skirt"},
+        ["ImageID"] = 12958832573,
+        ["Name"] = "Spring Skirt",
+        ["Status"] = true,
+        ["RobuxPrice"] = 50
+    },
+    {
+        ["Items"] = {"Cupid Corset", "Cupid Heels", "Cupid Skirt", "Cupid Choker", "Cupid Necklaces", "Cupid Wings"},
+        ["ImageID"] = 12557758670,
+        ["Name"] = "Cupid Set",
+        ["Status"] = true,
+        ["RobuxPrice"] = 500
+    },
+    {
+        ["Items"] = {"HeartBreaker Top"},
+        ["ImageID"] = 13747962769,
+        ["Name"] = " HeartBreaker Top!",
+        ["Status"] = true,
+        ["RobuxPrice"] = 149
+    },
+    {
+        ["Items"] = {"Gothic Skirt"},
+        ["ImageID"] = 13041377706,
+        ["Name"] = "Gothic Skirt",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"Gothic Top"},
+        ["ImageID"] = 13041374837,
+        ["Name"] = "Gothic Top",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"Sasha Sunshine Pants"},
+        ["ImageID"] = 13223189233,
+        ["Name"] = "Sasha Sunshine Pants",
+        ["Status"] = true,
+        ["RobuxPrice"] = 500
+    },
+    {
+        ["Items"] = {"Sugar Skirt"},
+        ["ImageID"] = 13222384797,
+        ["Name"] = "Sugar Skirt",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"Elf Skirt"},
+        ["ImageID"] = 15505256564,
+        ["Name"] = "Elf Skirt",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"HeartStopper Corset"},
+        ["ImageID"] = 13223019447,
+        ["Name"] = "HeartStopper Corset",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"SuckerPuncher Top"},
+        ["ImageID"] = 12407685887,
+        ["Name"] = "SuckerPuncher Top",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"Barbras Hat"},
+        ["ImageID"] = 15505520671,
+        ["Name"] = "Barbras Hat",
+        ["Status"] = true,
+        ["RobuxPrice"] = 500
+    },
+    {
+        ["Items"] = {"Sugar Corset"},
+        ["ImageID"] = 13222386765,
+        ["Name"] = "Sugar Corset",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"Twinkle Belt Dress"},
+        ["ImageID"] = 15411009895,
+        ["Name"] = "Twinkle Belt Dress",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"Elf Sleeves"},
+        ["ImageID"] = 15505519026,
+        ["Name"] = "Elf Sleeves",
+        ["Status"] = true,
+        ["RobuxPrice"] = 100
+    },
+    {
+        ["Items"] = {"Spring Sleeves"},
+        ["ImageID"] = 12958913589,
+        ["Name"] = "Spring Sleeves",
+        ["Status"] = true,
+        ["RobuxPrice"] = 50
+    },
+    {
+        ["Items"] = {"Mistletoe Skirt"},
+        ["ImageID"] = 15430216488,
+        ["Name"] = "Mistletoe Skirt",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"HeartStopper Heels"},
+        ["ImageID"] = 13222397660,
+        ["Name"] = "HeartStopper Heels",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"SuckerPuncher Skirt"},
+        ["ImageID"] = 12407698020,
+        ["Name"] = "SuckerPuncher Skirt",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"Scene Skirt"},
+        ["ImageID"] = 13747984449,
+        ["Name"] = " Scene Skirt!",
+        ["Status"] = true,
+        ["RobuxPrice"] = 199
+    },
+    {
+        ["Items"] = {"SuckerPuncher Boots"},
+        ["ImageID"] = 12407697037,
+        ["Name"] = "SuckerPuncher Boots",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"Kitty Heels"},
+        ["ImageID"] = 13747961513,
+        ["Name"] = " Kitty Heels!",
+        ["Status"] = true,
+        ["RobuxPrice"] = 99
+    },
+    {
+        ["Items"] = {"Mistletoe Sleeves"},
+        ["ImageID"] = 15430243056,
+        ["Name"] = "Mistletoe Sleeves",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"Sugar Bag"},
+        ["ImageID"] = 13222389177,
+        ["Name"] = "Sugar Bag",
+        ["Status"] = true,
+        ["RobuxPrice"] = 20
+    },
+    {
+        ["Items"] = {"Snow Buddy Hat"},
+        ["ImageID"] = 15504979303,
+        ["Name"] = "Snow Buddy Hat",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"Elf Corset"},
+        ["ImageID"] = 15505255124,
+        ["Name"] = "Elf Corset",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"Gothic Sleeves"},
+        ["ImageID"] = 13104782002,
+        ["Name"] = "Gothic Sleeves",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"Elf Heels"},
+        ["ImageID"] = 15505253231,
+        ["Name"] = "Elf Heels",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"SuckerPuncher Boots", "SuckerPuncher Top", "SuckerPuncher Skirt", "SuckerPuncher Sleeves"},
+        ["ImageID"] = 12407683953,
+        ["Name"] = "SuckerPuncher set",
+        ["Status"] = true,
+        ["RobuxPrice"] = 500
+    },
+    {
+        ["Items"] = {"HeartStopper Skirt"},
+        ["ImageID"] = 13222395131,
+        ["Name"] = "HeartStopper Skirt",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"Cozy Cocoa Boots"},
+        ["ImageID"] = 15505521908,
+        ["Name"] = "Cozy Cocoa Boots",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"SuckerPuncher Sleeves"},
+        ["ImageID"] = 12407696333,
+        ["Name"] = "SuckerPuncher Sleeves",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"Sugar Heels", "Sugar Corset", "Sugar Skirt", "Sugar Bag", "Sugar Sleeves"},
+        ["ImageID"] = 12400186485,
+        ["Name"] = "Sugar Set",
+        ["Status"] = true,
+        ["RobuxPrice"] = 500
+    },
+    {
+        ["Items"] = {"Furr Boots"},
+        ["ImageID"] = 13747958193,
+        ["Name"] = " Furr Boots!",
+        ["Status"] = true,
+        ["RobuxPrice"] = 99
+    },
+    {
+        ["Items"] = {"Trendy MiniSkirt"},
+        ["ImageID"] = 13747958736,
+        ["Name"] = " Trendy MiniSkirt!",
+        ["Status"] = true,
+        ["RobuxPrice"] = 199
+    },
+    {
+        ["Items"] = {"Summerless Top"},
+        ["ImageID"] = 13747957607,
+        ["Name"] = " Summerless Top!",
+        ["Status"] = true,
+        ["RobuxPrice"] = 149
+    },
+    {
+        ["Items"] = {"Mistletoe Corset"},
+        ["ImageID"] = 15430213081,
+        ["Name"] = "Mistletoe Corset",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"Sasha Sunshine Top"},
+        ["ImageID"] = 13041377706,
+        ["Name"] = "Sasha Sunshine Top",
+        ["Status"] = true,
+        ["RobuxPrice"] = 500
+    },
+    {
+        ["Items"] = {"Sugar Heels"},
+        ["ImageID"] = 13222367623,
+        ["Name"] = "Sugar Heels",
+        ["Status"] = true,
+        ["RobuxPrice"] = 150
+    },
+    {
+        ["Items"] = {"Star Gloves"},
+        ["ImageID"] = 13041377706,
+        ["Name"] = "Star Gloves",
+        ["Status"] = true,
+        ["RobuxPrice"] = 50
+    },
+    {
+        ["Items"] = {"Springness Top"},
+        ["ImageID"] = 12958354759,
+        ["Name"] = "Springness Top",
+        ["Status"] = true,
+        ["RobuxPrice"] = 50
+    }
+}
