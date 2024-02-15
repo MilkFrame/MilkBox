65915f217c9336a8e10ce28b96daa6f9c7a2ed9a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-15 18:29:22 +1100

    Create CapsuleData.lua

---
+++ GameData/Chibi Crossing [5005984649]/CapsuleData.lua
@@ -0,0 +1,70 @@
+data = {
+    ["Basic Capsule"] = {
+        ["Currency"] = "Munny",
+        ["Name"] = "Basic Capsule",
+        ["RollChance"] = {["SSR"] = 0, ["UR"] = 0, ["R"] = 0.2, ["N"] = 0.8, ["SR"] = 0},
+        ["Price"] = 1000,
+        ["Pool"] = {}
+    },
+    ["Normie Capsule"] = {
+        ["Currency"] = "Munny",
+        ["Name"] = "Normie Capsule",
+        ["RollChance"] = {["SSR"] = 0, ["UR"] = 0, ["R"] = 0.409, ["N"] = 0.6902, ["SR"] = 0.0308},
+        ["Price"] = 35000,
+        ["Pool"] = {}
+    },
+    ["Weeblet Capsule"] = {
+        ["Currency"] = "Munny",
+        ["Name"] = "Weeblet Capsule",
+        ["RollChance"] = {["SSR"] = 0.001, ["UR"] = 0, ["R"] = 0.45, ["N"] = 0.449, ["SR"] = 0.1},
+        ["Price"] = 100000,
+        ["Pool"] = {}
+    },
+    ["Senpai Capsule"] = {
+        ["Currency"] = "Munny",
+        ["Name"] = "Senpai Capsule",
+        ["RollChance"] = {["SSR"] = 0.002, ["UR"] = 0, ["R"] = 0.5, ["N"] = 0.248, ["SR"] = 0.25},
+        ["Price"] = 3200000,
+        ["Pool"] = {}
+    },
+    ["Otaku Capsule"] = {
+        ["Currency"] = "Munny",
+        ["Name"] = "Otaku Capsule",
+        ["RollChance"] = {["SSR"] = 0.001, ["UR"] = 0, ["R"] = 0.5, ["N"] = 0.18, ["SR"] = 0.25},
+        ["Price"] = 1550000,
+        ["Pool"] = {}
+    },
+    ["Goshujin-sama Capsule"] = {
+        ["Currency"] = "Munny",
+        ["Name"] = "Goshujin-sama Capsule",
+        ["RollChance"] = {["SSR"] = 0.005, ["UR"] = 0.001, ["R"] = 0.5814, ["N"] = 0.1, ["SR"] = 0.3},
+        ["Price"] = 375000000,
+        ["Pool"] = {}
+    },
+    ["Spooky Capsule"] = {
+        ["Currency"] = "ArcadeTickets",
+        ["Name"] = "Spooky Capsule",
+        ["RollChance"] = {["SSR"] = 0.15, ["UR"] = 0.025, ["R"] = 0.375, ["N"] = 0, ["SR"] = 0.45},
+        ["Price"] = 1031,
+        ["Pool"] = {}
+    },
+    ["Super Spooky Capsule"] = {
+        ["Currency"] = "ArcadeTickets",
+        ["Name"] = "Super Spooky Capsule",
+        ["RollChance"] = {["SSR"] = 0.4, ["UR"] = 0.08, ["R"] = 0.17, ["N"] = 0, ["SR"] = 0.35},
+        ["Price"] = 10310,
+        ["Pool"] = {}
+    },
+    ["Kami-sama Capsule"] = {
+        ["Currency"] = "Robux",
+        ["Name"] = "Kami-sama Capsule",
+        ["RollChance"] = {["SSR"] = 0.35, ["UR"] = 0.15, ["R"] = 0, ["N"] = 0, ["SR"] = 0.5},
+        ["Price"] = 201,
+        ["_productid"] = 1023292661,
+        ["Pool"] = {}
+    }
+    --["Daily Capsule"]={["Currency"]="Robux",["Name"]="Onii's Daily Special",["RollChance"]={["SSR"]=0.02,["UR"]=0.01,["R"]=0.35,["N"]=0.27,["SR"]=0.25},["Price"]=69,["_productid"]=0,["Pool"]={}},
+    --["Debug Capsule"]={["Currency"]="Munny",["Name"]="Debug Capsule",["RollChance"]={["SSR"]=0.5,["UR"]=0.5,["R"]=0,["N"]=0,["SR"]=0},["Price"]=1,["Pool"]={["SSR"]={"Yuri_1","Sayori_1","Natsuki_1"},["UR"]={"Monika_1"}}},
+    --["Test Capsule"]={["Price"]=500000,["Currency"]="Robux",["Pool"]={},["RollChance"]={["SSR"]=0,["UR"]=0,["R"]=0,["N"]=0,["SR"]=0}},
+    --["Weekly Capsule"]={["Currency"]="Robux",["Name"]="Weekly SR+ Lock",["RollChance"]={["SSR"]=0.335,["UR"]=0.015,["R"]=0,["N"]=0,["SR"]=0.5},["Price"]=519,["_productid"]=0,["Pool"]={}},
+}
