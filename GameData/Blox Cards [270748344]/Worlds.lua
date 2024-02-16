9266ea8bc0b7eab2f119871fb892fe370de07dbf
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-17 02:26:22 +1100

    Create Worlds.lua

---
+++ GameData/Blox Cards [270748344]/Worlds.lua
@@ -0,0 +1,15 @@
+data = {
+    {
+        ["Subtitle"] = [[<stroke color="#000000" joins="miter" thickness="2" transparency="0.25">Take control of the faction leaders on their quest to take over the power of immortality in this <b>Roguelike</b> gamemode!</stroke>]],
+        ["Name"] = "City Trek",
+        ["Time"] = "427 AD",
+        ["Run"] = "City Trek",
+        ["Image"] = "rbxassetid://14347125208"
+    },
+    {
+        ["Time"] = "???",
+        ["Subtitle"] = [[<stroke color="#000000" joins="miter" thickness="2" transparency="0.25">Explore some worlds created in the past!</stroke>]],
+        ["Name"] = "Legacy Worlds",
+        ["Image"] = "rbxassetid://14380903304"
+    }
+}
