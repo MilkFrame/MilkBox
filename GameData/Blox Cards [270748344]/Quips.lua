e42d1390cd9bfaa983baadca821a9d91c783d132
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-17 02:24:53 +1100

    Create Quips.lua

---
+++ GameData/Blox Cards [270748344]/Quips.lua
@@ -0,0 +1,9 @@
+data = {
+    ["Permafrost Storm"] = "Lost to some cold water?",
+    ["Unwanted Trend"] = "Wow! Why did you do that?",
+    ["Fatal Discovery"] = "Losing to AI, huh?",
+    ["InceptionTime"] = "Imagine losing to viable life gain!",
+    ["Broken Dreams"] = "Curiosity killed the cat, huh?",
+    ["Priestess Zanzel, the Promised One"] = "Touched by the priestess of perfection?",
+    ["Eye of Heaven"] = "You have proven the errors of democracy!"
+}
