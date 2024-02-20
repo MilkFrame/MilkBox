0a0f27317c0e090c2680b5ff104fc99d5ea24d9d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-20 17:44:44 +1100

    Create TimePlayedRewards.lua

---
+++ GameData/Kohau Hibachi [4375619868]/TimePlayedRewards.lua
@@ -0,0 +1,22 @@
+data = {
+    {["MinutesRequired"] = 300, ["Name"] = "5 minutes", ["Reward"] = {["Amount"] = 1, ["RewardType"] = "Sushi Rolls"}},
+    {["MinutesRequired"] = 600, ["Name"] = "10 minutes", ["Reward"] = {["Amount"] = 2, ["RewardType"] = "Sushi Rolls"}},
+    {["MinutesRequired"] = 1200, ["Name"] = "20 minutes", ["Reward"] = {["Amount"] = 2, ["RewardType"] = "Sushi Rolls"}},
+    {["MinutesRequired"] = 2100, ["Name"] = "35 minutes", ["Reward"] = {["Amount"] = 3, ["RewardType"] = "Sushi Rolls"}},
+    {["MinutesRequired"] = 3600, ["Name"] = "1 hour", ["Reward"] = {["Amount"] = 3, ["RewardType"] = "Sushi Rolls"}},
+    {
+        ["MinutesRequired"] = 5400,
+        ["Name"] = "1 hour & 30 minutes",
+        ["Reward"] = {["Amount"] = 4, ["RewardType"] = "Sushi Rolls"}
+    },
+    {
+        ["MinutesRequired"] = 7200,
+        ["Name"] = "2 hours",
+        ["Reward"] = {["Amount"] = 1, ["RewardType"] = "DailyRewardCrate"}
+    },
+    {
+        ["MinutesRequired"] = 9000,
+        ["Name"] = "2 hours & 30 minutes",
+        ["Reward"] = {["Amount"] = 1, ["RewardType"] = "DailyRewardCrate"}
+    }
+}
