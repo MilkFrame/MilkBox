42de9934f5f71e485cb84bd28fe7897382978359
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-20 17:42:26 +1100

    Create Nametags.lua

---
+++ GameData/Kohau Hibachi [4375619868]/Nametags.lua
@@ -0,0 +1,277 @@
+data = {
+    ["NametagInfo"] = {
+        ["Electric"] = {
+            ["ParticleImage"] = "rbxthumb://type=Asset&id=13501874775&w=420&h=420",
+            ["Name"] = "Electric",
+            ["Colors"] = {Color3.fromRGB(19, 74, 255), Color3.fromRGB(98, 195, 255)}
+        },
+        ["Sunset"] = {
+            ["ParticleImage"] = "rbxassetid://13491277553",
+            ["Name"] = "Warm Sunset",
+            ["Colors"] = {Color3.fromRGB(250, 134, 7), Color3.fromRGB(252, 176, 51), Color3.fromRGB(254, 228, 105)}
+        },
+        ["Starry"] = {
+            ["ParticleImage"] = "rbxassetid://13491276688",
+            ["Name"] = "Starry Night",
+            ["Colors"] = {Color3.fromRGB(127, 197, 220), Color3.fromRGB(72, 137, 200), Color3.fromRGB(23, 54, 121)}
+        },
+        ["Easter"] = {
+            ["ParticleImage"] = "rbxassetid://13491267899",
+            ["HideInShop"] = true,
+            ["Name"] = "Easter",
+            ["Colors"] = {
+                Color3.fromRGB(148, 200, 255),
+                Color3.fromRGB(251, 190, 255),
+                Color3.fromRGB(183, 255, 179),
+                Color3.fromRGB(255, 243, 149)
+            }
+        },
+        ["Blue"] = {["Name"] = "Blue", ["Colors"] = {Color3.fromRGB(0, 0, 255)}},
+        ["Rich"] = {
+            ["ParticleImage"] = "rbxthumb://type=Asset&id=13781410723&w=420&h=420",
+            ["HideInShop"] = true,
+            ["Name"] = "Rich",
+            ["Colors"] = {Color3.fromRGB(35, 173, 0), Color3.fromRGB(176, 255, 175)}
+        },
+        ["Pink"] = {["Name"] = "Pink", ["Colors"] = {Color3.fromRGB(255, 170, 255)}},
+        ["Green"] = {["Name"] = "Green", ["Colors"] = {Color3.fromRGB(0, 255, 0)}},
+        ["Firework"] = {
+            ["ParticleImage"] = "rbxthumb://type=Asset&id=13875009821&w=420&h=420",
+            ["HideInShop"] = true,
+            ["Name"] = "Firework",
+            ["Colors"] = {
+                Color3.fromRGB(255, 0, 0),
+                Color3.fromRGB(255, 255, 255),
+                Color3.fromRGB(43, 54, 255),
+                Color3.fromRGB(255, 255, 255)
+            }
+        },
+        ["Fire"] = {
+            ["ParticleImage"] = "rbxthumb://type=Asset&id=13501875981&w=420&h=420",
+            ["Name"] = "Fire",
+            ["Colors"] = {Color3.fromRGB(255, 0, 0), Color3.fromRGB(255, 106, 0), Color3.fromRGB(255, 238, 0)}
+        },
+        ["Tropical"] = {
+            ["ParticleImage"] = "rbxassetid://13491279236",
+            ["Name"] = "Tropical",
+            ["Colors"] = {
+                Color3.fromRGB(255, 244, 173),
+                Color3.fromRGB(106, 186, 149),
+                Color3.fromRGB(70, 163, 163),
+                Color3.fromRGB(64, 79, 137)
+            }
+        },
+        ["IceCream"] = {
+            ["ParticleImage"] = "rbxthumb://type=Asset&id=13499650367&w=420&h=420",
+            ["Name"] = "Ice Cream",
+            ["Colors"] = {Color3.fromRGB(194, 153, 255), Color3.fromRGB(255, 114, 116)}
+        },
+        ["Autumn"] = {
+            ["ParticleImage"] = "rbxassetid://13491266852",
+            ["Name"] = "Autumn",
+            ["Colors"] = {
+                Color3.fromRGB(163, 75, 75),
+                Color3.fromRGB(213, 115, 61),
+                Color3.fromRGB(239, 184, 56),
+                Color3.fromRGB(248, 217, 109),
+                Color3.fromRGB(52, 142, 64)
+            }
+        },
+        ["Spring"] = {
+            ["ParticleImage"] = "rbxassetid://13491275958",
+            ["HideInShop"] = true,
+            ["Name"] = "Spring",
+            ["Colors"] = {Color3.fromRGB(221, 94, 243), Color3.fromRGB(92, 176, 255), Color3.fromRGB(255, 181, 229)}
+        },
+        ["Ghost"] = {
+            ["ParticleImage"] = "rbxthumb://type=Asset&id=14796465088&w=420&h=420",
+            ["HideInShop"] = true,
+            ["Name"] = "Ghost",
+            ["Colors"] = {Color3.fromRGB(255, 255, 255), Color3.fromRGB(135, 79, 255)}
+        },
+        ["Purple"] = {["Name"] = "Purple", ["Colors"] = {Color3.fromRGB(85, 0, 127)}},
+        ["Starfish"] = {
+            ["ParticleImage"] = "rbxthumb://type=Asset&id=13558851605&w=420&h=420",
+            ["HideInShop"] = true,
+            ["Name"] = "Starfish",
+            ["Colors"] = {Color3.fromRGB(255, 162, 255), Color3.fromRGB(255, 64, 64), Color3.fromRGB(255, 157, 0)}
+        },
+        ["Halloween"] = {
+            ["ParticleImage"] = "rbxassetid://13491269718",
+            ["HideInShop"] = true,
+            ["Name"] = "Halloween",
+            ["Colors"] = {Color3.fromRGB(0, 0, 0), Color3.fromRGB(136, 0, 226), Color3.fromRGB(255, 136, 0)}
+        },
+        ["Space"] = {
+            ["ParticleImage"] = "rbxassetid://13491275312",
+            ["Name"] = "Space",
+            ["Colors"] = {
+                Color3.fromRGB(27, 42, 53),
+                Color3.fromRGB(33, 84, 185),
+                Color3.fromRGB(123, 47, 123),
+                Color3.fromRGB(255, 102, 204)
+            }
+        },
+        ["Orange"] = {["Name"] = "Orange", ["Colors"] = {Color3.fromRGB(255, 85, 0)}},
+        ["Red"] = {["Name"] = "Red", ["Colors"] = {Color3.fromRGB(255, 0, 0)}},
+        ["SciFi"] = {
+            ["ParticleImage"] = "rbxassetid://13491273978",
+            ["Name"] = "Sci-Fi",
+            ["Colors"] = {Color3.fromRGB(0, 16, 176), Color3.fromRGB(13, 105, 172), Color3.fromRGB(4, 175, 236)}
+        },
+        ["Pride"] = {
+            ["ParticleImage"] = "rbxthumb://type=Asset&id=13501973817&w=420&h=420",
+            ["Name"] = "Pride",
+            ["Colors"] = {
+                Color3.fromRGB(255, 93, 96),
+                Color3.fromRGB(255, 152, 83),
+                Color3.fromRGB(253, 255, 120),
+                Color3.fromRGB(148, 255, 148),
+                Color3.fromRGB(143, 201, 255),
+                Color3.fromRGB(250, 179, 255)
+            }
+        },
+        ["Shell"] = {
+            ["ParticleImage"] = "rbxthumb://type=Asset&id=13499653421&w=420&h=420",
+            ["Name"] = "Shell",
+            ["Colors"] = {Color3.fromRGB(255, 207, 111), Color3.fromRGB(255, 248, 144)}
+        },
+        ["Rainbow"] = {
+            ["HideInShop"] = true,
+            ["Name"] = "Rainbow",
+            ["Colors"] = {
+                Color3.fromRGB(255, 0, 4),
+                Color3.fromRGB(255, 255, 20),
+                Color3.fromRGB(0, 255, 0),
+                Color3.fromRGB(0, 255, 255),
+                Color3.fromRGB(255, 0, 255)
+            }
+        },
+        ["Royalty"] = {
+            ["ParticleImage"] = "rbxthumb://type=Asset&id=13499651693&w=420&h=420",
+            ["Name"] = "Royalty",
+            ["Colors"] = {Color3.fromRGB(255, 150, 2), Color3.fromRGB(255, 239, 64)}
+        },
+        ["Witch"] = {
+            ["ParticleImage"] = "rbxthumb://type=Asset&id=14796453878&w=420&h=420",
+            ["HideInShop"] = true,
+            ["Name"] = "Witch",
+            ["Colors"] = {Color3.fromRGB(113, 0, 226), Color3.fromRGB(255, 119, 0)}
+        },
+        ["Bonfire"] = {
+            ["ParticleImage"] = "rbxthumb://type=Asset&id=14596767037&w=420&h=420",
+            ["HideInShop"] = true,
+            ["Name"] = "Bonfire",
+            ["Colors"] = {
+                Color3.fromRGB(255, 0, 0),
+                Color3.fromRGB(255, 85, 0),
+                Color3.fromRGB(239, 147, 0),
+                Color3.fromRGB(248, 219, 0)
+            }
+        },
+        ["Cactus"] = {
+            ["ParticleImage"] = "rbxthumb://type=Asset&id=14156780878&w=420&h=420",
+            ["HideInShop"] = true,
+            ["Name"] = "Cactus",
+            ["Colors"] = {Color3.fromRGB(61, 162, 86), Color3.fromRGB(162, 255, 201)}
+        },
+        ["Sakura"] = {
+            ["ParticleImage"] = "rbxassetid://13491272397",
+            ["HideInShop"] = true,
+            ["Name"] = "Sakura",
+            ["Colors"] = {Color3.fromRGB(255, 175, 247), Color3.fromRGB(172, 133, 255)}
+        },
+        ["Black"] = {["Name"] = "Black", ["Colors"] = {Color3.fromRGB(27, 42, 53)}},
+        ["Heart"] = {
+            ["ParticleImage"] = "rbxassetid://13491270358",
+            ["Name"] = "Heart",
+            ["Colors"] = {
+                Color3.fromRGB(255, 89, 89),
+                Color3.fromRGB(255, 102, 204),
+                Color3.fromRGB(212, 144, 189),
+                Color3.fromRGB(231, 231, 236)
+            }
+        },
+        ["2023 Awards"] = {
+            ["ParticleImage"] = "rbxthumb://type=Asset&id=14024631734&w=420&h=420",
+            ["HideInShop"] = true,
+            ["Name"] = "2023 Awards",
+            ["Colors"] = {Color3.fromRGB(255, 185, 8), Color3.fromRGB(255, 255, 160)}
+        },
+        ["Yellow"] = {["Name"] = "Yellow", ["Colors"] = {Color3.fromRGB(255, 255, 0)}},
+        ["Holiday"] = {
+            ["ParticleImage"] = "rbxassetid://13491271226",
+            ["HideInShop"] = true,
+            ["Name"] = "Holiday",
+            ["Colors"] = {Color3.fromRGB(255, 0, 0), Color3.fromRGB(0, 255, 0)}
+        },
+        ["Snowy"] = {
+            ["ParticleImage"] = "rbxassetid://13491274690",
+            ["Name"] = "Snowy",
+            ["Colors"] = {Color3.fromRGB(13, 105, 172), Color3.fromRGB(4, 175, 236), Color3.fromRGB(248, 248, 248)}
+        }
+    },
+    ["NametagList"] = {
+        ["Electric"] = "Electric",
+        ["Sunset"] = "Sunset",
+        ["Starry"] = "Starry",
+        ["Easter"] = "Easter",
+        ["Blue"] = "Blue",
+        ["Rich"] = "Rich",
+        ["Pink"] = "Pink",
+        ["Custom"] = "Custom",
+        ["Green"] = "Green",
+        ["Firework"] = "Firework",
+        ["Fire"] = "Fire",
+        ["Tropical"] = "Tropical",
+        ["IceCream"] = "IceCream",
+        ["Autumn"] = "Autumn",
+        ["Spring"] = "Spring",
+        ["Ghost"] = "Ghost",
+        ["Purple"] = "Purple",
+        ["Starfish"] = "Starfish",
+        ["Halloween"] = "Halloween",
+        ["Space"] = "Space",
+        ["Orange"] = "Orange",
+        ["Red"] = "Red",
+        ["SciFi"] = "SciFi",
+        ["Pride"] = "Pride",
+        ["Shell"] = "Shell",
+        ["Rainbow"] = "Rainbow",
+        ["Royalty"] = "Royalty",
+        ["Witch"] = "Witch",
+        ["Bonfire"] = "Bonfire",
+        ["Cactus"] = "Cactus",
+        ["Sakura"] = "Sakura",
+        ["Black"] = "Black",
+        ["Heart"] = "Heart",
+        ["2023 Awards"] = "2023 Awards",
+        ["Yellow"] = "Yellow",
+        ["Holiday"] = "Holiday",
+        ["Snowy"] = "Snowy"
+    },
+    ["NametagShopItems"] = {
+        ["Autumn"] = 50,
+        ["Sunset"] = 50,
+        ["Starry"] = 50,
+        ["Purple"] = 25,
+        ["Royalty"] = 50,
+        ["Space"] = 50,
+        ["Orange"] = 25,
+        ["Red"] = 25,
+        ["SciFi"] = 50,
+        ["Blue"] = 25,
+        ["Pride"] = 50,
+        ["Shell"] = 50,
+        ["Snowy"] = 50,
+        ["Pink"] = 25,
+        ["Fire"] = 50,
+        ["Electric"] = 50,
+        ["Black"] = 25,
+        ["Green"] = 25,
+        ["Heart"] = 50,
+        ["Yellow"] = 25,
+        ["Tropical"] = 50,
+        ["IceCream"] = 50
+    }
+}
