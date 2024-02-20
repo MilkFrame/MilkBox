c89ddfa27e5fe78040ef6a85feb8010d5c1c002a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-20 17:46:39 +1100

    Create DailyRewardCrates.lua

---
+++ GameData/Kohau Hibachi [4375619868]/DailyRewardCrates.lua
@@ -0,0 +1,35 @@
+data = {
+    ["StarterPack"] = {
+        ["Rarities"] = {["Uncommon"] = 0.2, ["Common"] = 0.8},
+        ["Name"] = "Starter Pack",
+        ["OpenSound"] = "ChestOpenDefault",
+        ["Price"] = 0,
+        ["Contents"] = {
+            ["Uncommon"] = {"Smoke", "Sparks", "Wind", "Caution Tape", "Evil", "Blessed"},
+            ["Common"] = {
+                "Robux",
+                "Sushi",
+                "Stars",
+                "Hearts",
+                "Grass",
+                "Energy",
+                "Smoke",
+                "Snow",
+                "Red",
+                "Orange",
+                "Green",
+                "Blue",
+                "Indigo",
+                "Violet",
+                "Black",
+                "White",
+                "SushiTrail",
+                "HamburgerTrail",
+                "Pizza",
+                "RobuxTrail"
+            }
+        },
+        ["Icon"] = "rbxassetid://846009620",
+        ["Crate"] = "StarterCrate"
+    }
+}
