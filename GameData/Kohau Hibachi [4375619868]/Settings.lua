d799dbdc73d7ca272433d1e6c261d2915f0739f2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-20 17:44:08 +1100

    Create Settings.lua

---
+++ GameData/Kohau Hibachi [4375619868]/Settings.lua
@@ -0,0 +1,113 @@
+data = {
+    ["SettingsInfo"] = {
+        ["Tipping"] = {
+            ["Type"] = "Staff",
+            ["Name"] = "Tipping",
+            ["LRRestricted"] = true,
+            ["Trigger"] = {["RunOnServer"] = false, ["Trigger"] = "function: 0x00000000e3fd066d"},
+            ["ToggleType"] = "Button",
+            ["Description"] = "Manage your tip settings."
+        },
+        ["FastCookingEnabled"] = {
+            ["Type"] = "Staff",
+            ["Name"] = "Faster Cooking Enabled",
+            ["GamepassRestricted"] = 59159441,
+            ["Trigger"] = {["RunOnServer"] = false, ["Trigger"] = "function: 0x000000008db44be5"},
+            ["ToggleType"] = "Toggle",
+            ["Description"] = "Enable/disable your Faster Cooking gamepass."
+        },
+        ["AutoAFK"] = {
+            ["Type"] = "Other",
+            ["Name"] = "Auto AFK",
+            ["Trigger"] = {["RunOnServer"] = false, ["Trigger"] = "function: 0x000000001797a97d"},
+            ["ToggleType"] = "Toggle",
+            ["Description"] = "Show you as AFK when switching windows."
+        },
+        ["SFXVolume"] = {
+            ["Type"] = "Audio",
+            ["Name"] = "SFX Volume",
+            ["Trigger"] = {["RunOnServer"] = false, ["Trigger"] = "function: 0x00000000be0e72e5"},
+            ["ToggleType"] = "Slider",
+            ["Description"] = "Adjust the SFX volume."
+        },
+        ["RestaurantAmbienceVolume"] = {
+            ["Type"] = "Audio",
+            ["Name"] = "Ambience Volume",
+            ["Trigger"] = {["RunOnServer"] = false, ["Trigger"] = "function: 0x0000000028364a9d"},
+            ["ToggleType"] = "Slider",
+            ["Description"] = "Adjust the talking ambience volume."
+        },
+        ["MinigameMovement"] = {
+            ["Type"] = "Staff",
+            ["Name"] = "Disable Movement",
+            ["Trigger"] = {["RunOnServer"] = false, ["Trigger"] = "function: 0x00000000b1cdaead"},
+            ["ToggleType"] = "Toggle",
+            ["Description"] = "Disable emote animations during cooking."
+        },
+        ["EquipUniform"] = {
+            ["MRRestricted"] = true,
+            ["Type"] = "Staff",
+            ["Name"] = "Equip Uniform",
+            ["Trigger"] = {["RunOnServer"] = false, ["Trigger"] = "function: 0x00000000b660b10d"},
+            ["ToggleType"] = "Toggle",
+            ["Description"] = "Toggle your uniform behind the counter."
+        },
+        ["DisableNPCVoice"] = {
+            ["Type"] = "Audio",
+            ["Name"] = "Disable NPC Voices",
+            ["Trigger"] = {["RunOnServer"] = false, ["Trigger"] = "function: 0x00000000b5c93b45"},
+            ["ToggleType"] = "Toggle",
+            ["Description"] = "Disable the voices of the NPC customers."
+        },
+        ["LowGraphics"] = {
+            ["Type"] = "Other",
+            ["Name"] = "Low Graphics Mode",
+            ["Trigger"] = {["RunOnServer"] = false, ["Trigger"] = "function: 0x00000000d08837d5"},
+            ["ToggleType"] = "Toggle",
+            ["Description"] = "Improve performance by hiding unnecessary parts."
+        },
+        ["DisableInteractions"] = {
+            ["Type"] = "Other",
+            ["Name"] = "Disable Interactions",
+            ["Trigger"] = {["RunOnServer"] = false, ["Trigger"] = "function: 0x000000005b1daeb5"},
+            ["ToggleType"] = "Toggle",
+            ["Description"] = "Hide interactions throughout the game."
+        },
+        ["DisableCosmetics"] = {
+            ["Type"] = "Other",
+            ["Name"] = "Disable Cosmetics",
+            ["Trigger"] = {["RunOnServer"] = false, ["Trigger"] = "function: 0x00000000631499e5"},
+            ["ToggleType"] = "Toggle",
+            ["Description"] = "Hide particles and trails on all players."
+        },
+        ["ExtraSpeed"] = {
+            ["Type"] = "Other",
+            ["Name"] = "Extra Speed",
+            ["Trigger"] = {["RunOnServer"] = true, ["Trigger"] = "function: 0x00000000ec345bbd"},
+            ["ToggleType"] = "Toggle",
+            ["Description"] = "Makes your character walk faster."
+        },
+        ["MusicVolume"] = {
+            ["Type"] = "Audio",
+            ["Name"] = "Music Volume",
+            ["Trigger"] = {["RunOnServer"] = false, ["Trigger"] = "function: 0x00000000f65b0935"},
+            ["ToggleType"] = "Slider",
+            ["Description"] = "Adjust the music volume."
+        }
+    },
+    ["SettingsList"] = {
+        ["Tipping"] = "Tipping",
+        ["FastCookingEnabled"] = "FastCookingEnabled",
+        ["AutoAFK"] = "AutoAFK",
+        ["SFXVolume"] = "SFXVolume",
+        ["RestaurantAmbienceVolume"] = "RestaurantAmbienceVolume",
+        ["MinigameMovement"] = "MinigameMovement",
+        ["EquipUniform"] = "EquipUniform",
+        ["DisableNPCVoice"] = "DisableNPCVoice",
+        ["LowGraphics"] = "LowGraphics",
+        ["DisableInteractions"] = "DisableInteractions",
+        ["DisableCosmetics"] = "DisableCosmetics",
+        ["ExtraSpeed"] = "ExtraSpeed",
+        ["MusicVolume"] = "MusicVolume"
+    }
+}
