996eea2a63f9123d561336ec5f5128df5db1fb5a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-20 17:46:25 +1100

    Create VehicleInfo.lua

---
+++ GameData/Kohau Hibachi [4375619868]/VehicleInfo.lua
@@ -0,0 +1,60 @@
+data = {
+    ["Sleigh"] = {
+        ["ViewportOffset"] = CFrame.new(1.34000003, 0, -10, -4.37113883e-08, 0, -1, 0, 1, 0, 1, 0, -4.37113883e-08),
+        ["Name"] = "Sleigh",
+        ["Model"] = "Sleigh"
+    },
+    ["Broomstick"] = {
+        ["ViewportOffset"] = CFrame.new(
+            1,
+            0.100000001,
+            -3,
+            -4.37113883e-08,
+            0,
+            -1,
+            -0.642787576,
+            0.766044438,
+            2.80971371e-08,
+            0.766044438,
+            0.642787576,
+            -3.34848664e-08
+        ),
+        ["Name"] = "Broomstick",
+        ["Model"] = "Broomstick"
+    },
+    ["Jetski"] = {
+        ["ViewportOffset"] = CFrame.new(0, -1.20000005, -12, -4.37113883e-08, 0, -1, 0, 1, 0, 1, 0, -4.37113883e-08),
+        ["Model"] = "Jetski",
+        ["Name"] = "Jetski",
+        ["Aquatic"] = true
+    },
+    ["Hoverboard"] = {
+        ["ViewportOffset"] = CFrame.new(
+            0,
+            0.200000003,
+            -7,
+            1.91068547e-15,
+            4.37113883e-08,
+            1,
+            0.98480773,
+            -0.173648223,
+            7.59040297e-09,
+            0.173648223,
+            0.98480773,
+            -4.30473115e-08
+        ),
+        ["Name"] = "Hoverboard",
+        ["Model"] = "Hoverboard"
+    },
+    ["HotAirBalloon"] = {
+        ["ViewportOffset"] = CFrame.new(0, -10, -30, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+        ["Name"] = "Balloon",
+        ["Model"] = "HotAirBalloon"
+    },
+    ["Yacht"] = {
+        ["ViewportOffset"] = CFrame.new(-7, -10, -75, -4.37113883e-08, 0, -1, 0, 1, 0, 1, 0, -4.37113883e-08),
+        ["Model"] = "Yacht",
+        ["Name"] = "Yacht",
+        ["Aquatic"] = true
+    }
+}
