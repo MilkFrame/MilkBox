5f2064a77dcab7269e96b79d55fc47acbbf4b464
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-20 17:43:44 +1100

    Create Quests.lua

---
+++ GameData/Kohau Hibachi [4375619868]/Quests.lua
@@ -0,0 +1,132 @@
+data = {
+    ["Everyone_Quests"] = {
+        {
+            ["RequiredAmount"] = 5,
+            ["Description"] = "Order or get handed to 5 times.",
+            ["Name"] = "Five Stars",
+            ["Reward"] = {["Amount"] = 3, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["RequiredAmount"] = 1,
+            ["Description"] = "Order a random food on the menu.",
+            ["Name"] = "Hungry",
+            ["Reward"] = {["Amount"] = 1, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["RequiredAmount"] = 3,
+            ["Description"] = "Use the sushi conveyer belt 3 times.",
+            ["Name"] = "Sushi Lover",
+            ["Reward"] = {["Amount"] = 1, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["RequiredAmount"] = 5,
+            ["Description"] = "Successfully catch 5 fish.",
+            ["Name"] = "Fish Fear Me",
+            ["Reward"] = {["Amount"] = 2, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["Name"] = "Marathon Runner",
+            ["DescriptionCallback"] = "function: 0x0000000061ccda2d",
+            ["Description"] = "Walk %d studs.",
+            ["Reward"] = {["Amount"] = 2, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["RequiredAmount"] = 5,
+            ["Description"] = "Have 5 different foods in your inventory.",
+            ["Name"] = "Full of Food",
+            ["Reward"] = {["Amount"] = 3, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["Name"] = "Time Master",
+            ["DescriptionCallback"] = "function: 0x000000000524b465",
+            ["Description"] = "Play for %d minutes.",
+            ["Reward"] = {["Amount"] = 2, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["RequiredAmount"] = 1,
+            ["Description"] = "Invite or play with one friend.",
+            ["Name"] = "Partier",
+            ["Reward"] = {["Amount"] = 2, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["RequiredAmount"] = 1,
+            ["Description"] = "See a Vice President+ in-game.",
+            ["Name"] = "Meet a Corporate",
+            ["Reward"] = {["Amount"] = 3, ["Type"] = "Sushi Rolls"}
+        }
+    },
+    ["Chef_Quests"] = {
+        {
+            ["RequiredAmount"] = 5,
+            ["Description"] = "Mop 5 spills.",
+            ["Name"] = "Shiny Floors",
+            ["Reward"] = {["Amount"] = 2, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["RequiredAmount"] = 5,
+            ["Description"] = "Serve 5 NPC customers.",
+            ["Name"] = "No Interaction",
+            ["Reward"] = {["Amount"] = 3, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["RequiredAmount"] = 1,
+            ["Description"] = "Have an NPC customer leave from impatience.",
+            ["Name"] = "Bad Service",
+            ["Reward"] = {["Amount"] = 1, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["RequiredAmount"] = 1,
+            ["Description"] = "Successfully serve one NPC customer.",
+            ["Name"] = "Satisfied",
+            ["Reward"] = {["Amount"] = 1, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["RequiredAmount"] = 2,
+            ["Description"] = "Use 2 different tricks.",
+            ["Name"] = "Tricky",
+            ["Reward"] = {["Amount"] = 1, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["RequiredAmount"] = 5,
+            ["Description"] = "Cook 5 foods.",
+            ["Name"] = "Pro Chef",
+            ["Reward"] = {["Amount"] = 3, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["RequiredAmount"] = 3,
+            ["Description"] = "Grab 3 drinks.",
+            ["Name"] = "Drink Star",
+            ["Reward"] = {["Amount"] = 2, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["RequiredAmount"] = 2,
+            ["Description"] = "Make 2 appetizers.",
+            ["Name"] = "Appetizer Elite",
+            ["Reward"] = {["Amount"] = 2, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["RequiredAmount"] = 2,
+            ["Description"] = "Make 2 entrees.",
+            ["Name"] = "Entree Legend",
+            ["Reward"] = {["Amount"] = 2, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["RequiredAmount"] = 2,
+            ["Description"] = "Make 2 desserts.",
+            ["Name"] = "Dessert Hero",
+            ["Reward"] = {["Amount"] = 2, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["RequiredAmount"] = 5,
+            ["Description"] = "Hand 5 items to real customers",
+            ["Name"] = "Tip?",
+            ["Reward"] = {["Amount"] = 3, ["Type"] = "Sushi Rolls"}
+        },
+        {
+            ["RequiredAmount"] = 1,
+            ["Description"] = "Finish an NPC customer order in 30 secs.",
+            ["Name"] = "Great Service",
+            ["Reward"] = {["Amount"] = 3, ["Type"] = "Sushi Rolls"}
+        }
+    }
+}
