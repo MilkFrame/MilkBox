43257e931022290c0d81137871d4abce220bb9ab
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-20 17:46:09 +1100

    Create Uniforms.lua

---
+++ GameData/Kohau Hibachi [4375619868]/Uniforms.lua
@@ -0,0 +1,106 @@
+data = {
+    ["UniformInfo"] = {
+        ["Spooky"] = {
+            ["ShirtTemplate"] = "http://www.roblox.com/asset/?id=14932079958",
+            ["Name"] = "Spooky",
+            ["PantsTemplate"] = "http://www.roblox.com/asset/?id=14932081928"
+        },
+        ["Astronaut"] = {
+            ["ShirtTemplate"] = "http://www.roblox.com/asset/?id=6599081592",
+            ["Name"] = "Astronaut",
+            ["PantsTemplate"] = "http://www.roblox.com/asset/?id=6599099975"
+        },
+        ["Scarecrow"] = {
+            ["ShirtTemplate"] = "http://www.roblox.com/asset/?id=14671960705",
+            ["Name"] = "Scarecrow",
+            ["PantsTemplate"] = "http://www.roblox.com/asset/?id=14671963986"
+        },
+        ["Cowboy"] = {
+            ["ShirtTemplate"] = "http://www.roblox.com/asset/?id=14326811731",
+            ["Name"] = "Cowboy",
+            ["PantsTemplate"] = "http://www.roblox.com/asset/?id=14326815741"
+        },
+        ["Superhero"] = {
+            ["ShirtTemplate"] = "http://www.roblox.com/asset/?id=10638233981",
+            ["Name"] = "Superhero",
+            ["PantsTemplate"] = "http://www.roblox.com/asset/?id=10638236073"
+        },
+        ["Fireworks"] = {
+            ["ShirtTemplate"] = "http://www.roblox.com/asset/?id=13923072616",
+            ["Name"] = "Fireworks",
+            ["PantsTemplate"] = "http://www.roblox.com/asset/?id=13923076386"
+        },
+        ["Alien"] = {
+            ["ShirtTemplate"] = "http://www.roblox.com/asset/?id=6599084357",
+            ["Name"] = "Alien",
+            ["PantsTemplate"] = "http://www.roblox.com/asset/?id=6599101081"
+        },
+        ["Royalty"] = {
+            ["ShirtTemplate"] = "http://www.roblox.com/asset/?id=6599087664",
+            ["Name"] = "Royalty",
+            ["PantsTemplate"] = "http://www.roblox.com/asset/?id=6599103289"
+        },
+        ["VIP"] = {
+            ["ShirtTemplate"] = "http://www.roblox.com/asset/?id=10569744851",
+            ["Name"] = "VIP",
+            ["PantsTemplate"] = "http://www.roblox.com/asset/?id=10569746279"
+        },
+        ["Default"] = {
+            ["ShirtTemplate"] = "http://www.roblox.com/asset/?id=4580545897",
+            ["Name"] = "Default",
+            ["PantsTemplate"] = "http://www.roblox.com/asset/?id=4698036177"
+        },
+        ["Elf"] = {
+            ["ShirtTemplate"] = "http://www.roblox.com/asset/?id=8201133493",
+            ["Name"] = "Elf",
+            ["PantsTemplate"] = "http://www.roblox.com/asset/?id=8201134973"
+        },
+        ["Zombie"] = {
+            ["ShirtTemplate"] = "http://www.roblox.com/asset/?id=6599088860",
+            ["Name"] = "Zombie",
+            ["PantsTemplate"] = "http://www.roblox.com/asset/?id=6599104676"
+        },
+        ["Kimono"] = {
+            ["ShirtTemplate"] = "http://www.roblox.com/asset/?id=12510463263",
+            ["Name"] = "Kimono",
+            ["PantsTemplate"] = "http://www.roblox.com/asset/?id=12510464878"
+        },
+        ["Tropical"] = {
+            ["ShirtTemplate"] = "http://www.roblox.com/asset/?id=13641972283",
+            ["Name"] = "Tropical",
+            ["PantsTemplate"] = "http://www.roblox.com/asset/?id=13641976856"
+        },
+        ["Angel"] = {
+            ["ShirtTemplate"] = "http://www.roblox.com/asset/?id=6599085774",
+            ["Name"] = "Angel",
+            ["PantsTemplate"] = "http://www.roblox.com/asset/?id=6599102154"
+        }
+    },
+    ["UniformShop"] = {
+        ["Kimono"] = 50,
+        ["Angel"] = 50,
+        ["Default"] = 5,
+        ["Elf"] = 50,
+        ["Zombie"] = 50,
+        ["Superhero"] = 50,
+        ["Astronaut"] = 50,
+        ["Alien"] = 50
+    },
+    ["UniformList"] = {
+        ["Spooky"] = "Spooky",
+        ["Astronaut"] = "Astronaut",
+        ["Scarecrow"] = "Scarecrow",
+        ["Cowboy"] = "Cowboy",
+        ["Superhero"] = "Superhero",
+        ["Fireworks"] = "Fireworks",
+        ["Alien"] = "Alien",
+        ["Royalty"] = "Royalty",
+        ["VIP"] = "VIP",
+        ["Default"] = "Default",
+        ["Elf"] = "Elf",
+        ["Zombie"] = "Zombie",
+        ["Kimono"] = "Kimono",
+        ["Tropical"] = "Tropical",
+        ["Angel"] = "Angel"
+    }
+}
