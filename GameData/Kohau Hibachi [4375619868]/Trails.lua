490f5dff166a98d4b1dab4bc249fe42ca05dba6e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-20 17:45:16 +1100

    Create Trails.lua

---
+++ GameData/Kohau Hibachi [4375619868]/Trails.lua
@@ -0,0 +1,294 @@
+data = {
+    ["TrailCrates"] = {
+        ["Space"] = {
+            ["Rarities"] = {["Rare"] = 0.15, ["Legendary"] = 0.05, ["Uncommon"] = 0.3, ["Common"] = 0.5},
+            ["Name"] = "Space",
+            ["OpenSound"] = "ChestOpenDefault",
+            ["Price"] = 25,
+            ["Contents"] = {
+                ["Rare"] = {"Rocket", "Planet"},
+                ["Legendary"] = {"Comet"},
+                ["Uncommon"] = {"Galaxy", "Asteroid"},
+                ["Common"] = {"Star", "Meteor", "Moon"}
+            },
+            ["Icon"] = "rbxassetid://846009620",
+            ["Crate"] = "StarterCrate"
+        },
+        ["Colorful"] = {
+            ["Rarities"] = {["Rare"] = 0.15, ["Common"] = 0.85},
+            ["Name"] = "Colorful",
+            ["OpenSound"] = "ChestOpenDefault",
+            ["Price"] = 10,
+            ["Contents"] = {
+                ["Rare"] = {"Rainbow"},
+                ["Common"] = {"Red", "Orange", "Green", "Blue", "Indigo", "Violet", "Black", "White"}
+            },
+            ["Icon"] = "rbxassetid://846009620",
+            ["Crate"] = "StarterCrate"
+        }
+    },
+    ["TrailInfo"] = {
+        ["Blessed"] = {
+            ["Trail"] = "Blessed",
+            ["Image"] = "rbxassetid://6419650446",
+            ["Name"] = "Blessed",
+            ["Description"] = ""
+        },
+        ["RobuxTrail"] = {
+            ["Trail"] = "Robux",
+            ["Image"] = "rbxthumb://type=Asset&id=10023748539&w=420&h=420",
+            ["Name"] = "Robux",
+            ["Description"] = ""
+        },
+        ["Desert"] = {
+            ["TrailGap"] = 2,
+            ["Image"] = "rbxthumb://type=Asset&id=14195453842&w=420&h=420",
+            ["Name"] = "Desert",
+            ["Trail"] = "Desert",
+            ["Description"] = ""
+        },
+        ["Moon"] = {
+            ["TrailGap"] = 2,
+            ["Image"] = "rbxthumb://type=Asset&id=12876705439&w=420&h=420",
+            ["Name"] = "Moon",
+            ["Trail"] = "Moon",
+            ["Description"] = ""
+        },
+        ["Planet"] = {
+            ["TrailGap"] = 2,
+            ["Image"] = "rbxthumb://type=Asset&id=12876706437&w=420&h=420",
+            ["Name"] = "Planet",
+            ["Trail"] = "Planet",
+            ["Description"] = ""
+        },
+        ["Star"] = {
+            ["TrailGap"] = 2,
+            ["Image"] = "rbxthumb://type=Asset&id=12876708094&w=420&h=420",
+            ["Name"] = "Star",
+            ["Trail"] = "Star",
+            ["Description"] = ""
+        },
+        ["Doge"] = {
+            ["Trail"] = "Doge",
+            ["Image"] = "rbxthumb://type=Asset&id=10682188840&w=420&h=420",
+            ["Name"] = "Doge",
+            ["Description"] = ""
+        },
+        ["Blue"] = {["Trail"] = "Blue", ["Image"] = "rbxassetid://37856113", ["Name"] = "Blue", ["Description"] = ""},
+        ["Pizza"] = {
+            ["Trail"] = "Pizza",
+            ["Image"] = "rbxthumb://type=Asset&id=10023594935&w=420&h=420",
+            ["Name"] = "Pizza",
+            ["Description"] = ""
+        },
+        ["Caution Tape"] = {
+            ["Trail"] = "Caution Tape",
+            ["Image"] = "rbxthumb://type=Asset&id=10016078099&w=420&h=420",
+            ["Name"] = "Caution Tape",
+            ["Description"] = ""
+        },
+        ["StarsTrail"] = {
+            ["Trail"] = "Stars",
+            ["Image"] = "rbxthumb://type=Asset&id=10023718435&w=420&h=420",
+            ["Name"] = "Stars",
+            ["Description"] = ""
+        },
+        ["Indigo"] = {
+            ["Trail"] = "Indigo",
+            ["Image"] = "rbxassetid://48375058",
+            ["Name"] = "Indigo",
+            ["Description"] = ""
+        },
+        ["Green"] = {
+            ["Trail"] = "Green",
+            ["Image"] = "rbxassetid://486362106",
+            ["Name"] = "Green",
+            ["Description"] = ""
+        },
+        ["Asteroid"] = {
+            ["TrailGap"] = 2,
+            ["Image"] = "rbxthumb://type=Asset&id=12876701067&w=420&h=420",
+            ["Name"] = "Asteroid",
+            ["Trail"] = "Asteroid",
+            ["Description"] = ""
+        },
+        ["Orange Fireworks"] = {
+            ["Trail"] = "OrangeFirework",
+            ["Image"] = "rbxthumb://type=Asset&id=10016159042&w=420&h=420",
+            ["Name"] = "Orange Firework",
+            ["Description"] = ""
+        },
+        ["Patriotic Gradient"] = {
+            ["Trail"] = "Patriotic",
+            ["Image"] = "rbxthumb://type=Asset&id=7042687560&w=420&h=420",
+            ["Name"] = "Patriotic Gradient",
+            ["Description"] = ""
+        },
+        ["Evil"] = {["Trail"] = "Evil", ["Image"] = "rbxassetid://67599364", ["Name"] = "Evil", ["Description"] = ""},
+        ["White Fireworks"] = {
+            ["Trail"] = "WhiteFirework",
+            ["Image"] = "rbxthumb://type=Asset&id=10016158269&w=420&h=420",
+            ["Name"] = "White Firework",
+            ["Description"] = ""
+        },
+        ["Comet"] = {
+            ["TrailGap"] = 2,
+            ["Image"] = "rbxthumb://type=Asset&id=12876701794&w=420&h=420",
+            ["Name"] = "Comet",
+            ["Trail"] = "Comet",
+            ["Description"] = ""
+        },
+        ["Meteor"] = {
+            ["TrailGap"] = 2,
+            ["Image"] = "rbxthumb://type=Asset&id=12876704167&w=420&h=420",
+            ["Name"] = "Meteor",
+            ["Trail"] = "Meteor",
+            ["Description"] = ""
+        },
+        ["Orange"] = {
+            ["Trail"] = "Orange",
+            ["Image"] = "rbxassetid://5944839314",
+            ["Name"] = "Orange",
+            ["Description"] = ""
+        },
+        ["Red"] = {["Trail"] = "Red", ["Image"] = "rbxassetid://12491809", ["Name"] = "Red", ["Description"] = ""},
+        ["Spooky"] = {
+            ["TrailGap"] = 2,
+            ["Image"] = "rbxthumb://type=Asset&id=14944909705&w=420&h=420",
+            ["Name"] = "Spooky",
+            ["Trail"] = "Spooky",
+            ["Description"] = ""
+        },
+        ["Sakura"] = {
+            ["TrailGap"] = 2,
+            ["Image"] = "rbxthumb://type=Asset&id=13354538829&w=420&h=420",
+            ["Name"] = "Sakura",
+            ["Trail"] = "Sakura",
+            ["Description"] = ""
+        },
+        ["Autumn"] = {
+            ["TrailGap"] = 2,
+            ["Image"] = "rbxthumb://type=Asset&id=14660924625&w=420&h=420",
+            ["Name"] = "Autumn",
+            ["Trail"] = "Autumn",
+            ["Description"] = ""
+        },
+        ["HamburgerTrail"] = {
+            ["Trail"] = "Hamburger",
+            ["Image"] = "rbxthumb://type=Asset&id=10023581039&w=420&h=420",
+            ["Name"] = "Hamburger",
+            ["Description"] = ""
+        },
+        ["Rainbow"] = {
+            ["Trail"] = "Rainbow",
+            ["Image"] = "rbxassetid://260882104",
+            ["Name"] = "Rainbow",
+            ["Description"] = ""
+        },
+        ["Firework"] = {
+            ["TrailGap"] = 2,
+            ["Image"] = "rbxthumb://type=Asset&id=13913883734&w=420&h=420",
+            ["Name"] = "Firework",
+            ["Trail"] = "Firework",
+            ["Description"] = ""
+        },
+        ["Waves"] = {
+            ["TrailGap"] = 1.25,
+            ["Image"] = "rbxthumb://type=Asset&id=13567138484&w=420&h=420",
+            ["Name"] = "Waves",
+            ["Trail"] = "Waves",
+            ["Description"] = ""
+        },
+        ["Patriotic Fireworks"] = {
+            ["Trail"] = "PatrioticFirework",
+            ["Image"] = "rbxthumb://type=Asset&id=10016161412&w=420&h=420",
+            ["Name"] = "Patriotic Fireworks",
+            ["Description"] = ""
+        },
+        ["Rocket"] = {
+            ["TrailGap"] = 2,
+            ["Image"] = "rbxthumb://type=Asset&id=12876707329&w=420&h=420",
+            ["Name"] = "Rocket",
+            ["Trail"] = "Rocket",
+            ["Description"] = ""
+        },
+        ["Violet"] = {
+            ["Trail"] = "Violet",
+            ["Image"] = "rbxassetid://4923451716",
+            ["Name"] = "Violet",
+            ["Description"] = ""
+        },
+        ["Black"] = {["Trail"] = "Black", ["Image"] = "rbxassetid://25901058", ["Name"] = "Black", ["Description"] = ""},
+        ["Galaxy"] = {
+            ["TrailGap"] = 2,
+            ["Image"] = "rbxthumb://type=Asset&id=12876702779&w=420&h=420",
+            ["Name"] = "Galaxy",
+            ["Trail"] = "Galaxy",
+            ["Description"] = ""
+        },
+        ["Yellow Fireworks"] = {
+            ["Trail"] = "YellowFirework",
+            ["Image"] = "rbxthumb://type=Asset&id=10016157388&w=420&h=420",
+            ["Name"] = "Yellow Firework",
+            ["Description"] = ""
+        },
+        ["Blue Fireworks"] = {
+            ["Trail"] = "BlueFirework",
+            ["Image"] = "rbxthumb://type=Asset&id=10016163315&w=420&h=420",
+            ["Name"] = "Blue Fireworks",
+            ["Description"] = ""
+        },
+        ["SushiTrail"] = {
+            ["Trail"] = "Sushi",
+            ["Image"] = "rbxthumb://type=Asset&id=10023489969&w=420&h=420",
+            ["Name"] = "Sushi",
+            ["Description"] = ""
+        },
+        ["White"] = {
+            ["Trail"] = "White",
+            ["Image"] = "rbxassetid://103461041",
+            ["Name"] = "White",
+            ["Description"] = ""
+        }
+    },
+    ["TrailsList"] = {
+        ["Blessed"] = "Blessed",
+        ["YellowFirework"] = "Yellow Fireworks",
+        ["RobuxTrail"] = "RobuxTrail",
+        ["Desert"] = "Desert",
+        ["OrangeFirework"] = "Orange Fireworks",
+        ["Moon"] = "Moon",
+        ["Planet"] = "Planet",
+        ["Star"] = "Star",
+        ["Doge"] = "Doge",
+        ["Patriotic"] = "Patriotic Gradient",
+        ["Blue"] = "Blue",
+        ["Pizza"] = "Pizza",
+        ["Caution Tape"] = "Caution Tape",
+        ["StarsTrail"] = "StarsTrail",
+        ["Indigo"] = "Indigo",
+        ["Green"] = "Green",
+        ["WhiteFirework"] = "White Fireworks",
+        ["BlueFirework"] = "Blue Fireworks",
+        ["Galaxy"] = "Galaxy",
+        ["Evil"] = "Evil",
+        ["Comet"] = "Comet",
+        ["Meteor"] = "Meteor",
+        ["PatrioticFirework"] = "Patriotic Fireworks",
+        ["Orange"] = "Orange",
+        ["Red"] = "Red",
+        ["Sakura"] = "Sakura",
+        ["HamburgerTrail"] = "HamburgerTrail",
+        ["Rainbow"] = "Rainbow",
+        ["Spooky"] = "Spooky",
+        ["Autumn"] = "Autumn",
+        ["Firework"] = "Firework",
+        ["Violet"] = "Violet",
+        ["Black"] = "Black",
+        ["Waves"] = "Waves",
+        ["Rocket"] = "Rocket",
+        ["Asteroid"] = "Asteroid",
+        ["SushiTrail"] = "SushiTrail",
+        ["White"] = "White"
+    }
+}
