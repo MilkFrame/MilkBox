263c223e9a4346bf64b37f54372fcd271b9bd242
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-20 17:40:55 +1100

    Create Levels.lua

---
+++ GameData/Kohau Hibachi [4375619868]/Levels.lua
@@ -0,0 +1,43 @@
+data = {
+    {
+        ["ItemName"] = "Nametag",
+        ["Award"] = "function: 0x000000007d67c865",
+        ["Gamepass"] = true,
+        ["XP"] = 50,
+        ["Image"] = ""
+    },
+    {
+        ["Award"] = "function: 0x00000000b9a3eef5",
+        ["ItemName"] = "10 Rolls",
+        ["XP"] = 170,
+        ["Image"] = "http://www.roblox.com/asset/?id=10024770508"
+    },
+    {
+        ["ItemName"] = "Trail",
+        ["Award"] = "function: 0x000000006e9f4385",
+        ["Gamepass"] = true,
+        ["XP"] = 390,
+        ["Image"] = "rbxthumb://type=Asset&id=14944909705&w=420&h=420"
+    },
+    {
+        ["Award"] = "function: 0x000000006372e205",
+        ["ItemName"] = "Particle",
+        ["XP"] = 640,
+        ["Image"] = "rbxthumb://type=Asset&id=14952890781&w=420&h=420"
+    },
+    {
+        ["ItemName"] = "Uniform",
+        ["Award"] = "function: 0x0000000040283da5",
+        ["Image"] = "",
+        ["Gamepass"] = true,
+        ["XP"] = 1000,
+        ["UniformName"] = "Spooky"
+    },
+    {
+        ["ItemName"] = "Trick",
+        ["Award"] = "function: 0x000000007cc1d595",
+        ["TrickModule"] = {["DoTrick"] = "function: 0x00000000ad890355"},
+        ["XP"] = 1350,
+        ["Image"] = "rbxthumb://type=Asset&id=13368533985&w=420&h=420"
+    }
+}
