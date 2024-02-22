3ed5548f04e3c60e5f96d186d54b5f64ba6b0036
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:16:12 +1100

    Create Gifts.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/DataModules/Gifts.lua
@@ -0,0 +1,31 @@
+data = {
+    ["GiftData"] = {
+        ["Christmas2022Gift"] = {
+            ["enabled"] = false,
+            ["expiryTime"] = 1672056000,
+            ["openingTime"] = 1671915600,
+            ["expiryTimeDev"] = 1670775686,
+            ["claimText"] = [[The gift contained <font color="#2e9bf2">2x boost</font> and  <font color="#2e9bf2">rare christmas furniture items</font>. Enjoy!]],
+            ["openingTimeDev"] = 1670689286
+        },
+        ["BillionGift"] = {
+            ["enabled"] = false,
+            ["expiryTime"] = 1685536233,
+            ["openingTime"] = 0,
+            ["expiryTimeDev"] = 1685536233,
+            ["claimText"] = [[The gift contained <font color="#2e9bf2">2x boost</font>. Enjoy!]],
+            ["openingTimeDev"] = 0
+        },
+        ["Summer2023Gift"] = {
+            ["enabled"] = false,
+            ["expiryTime"] = 1688644800,
+            ["expiryTimeDev"] = 1688644800,
+            ["openingTimeDev"] = 0,
+            ["openingTime"] = 0,
+            ["claimText"] = [[The gift contained <font color="#2e9bf2">2x boost</font>. Enjoy!]],
+            ["giftText"] = "Summer Gift",
+            ["strokeColor"] = Color3.fromRGB(31, 127, 203),
+            ["texture"] = "http://www.roblox.com/asset/?id=1237427"
+        }
+    }
+}
