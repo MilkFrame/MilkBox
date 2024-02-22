2f3f4b9bff3cf0d71a15ab3834b34fc68e80cd9a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:09:04 +1100

    Create Brand.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/DataModules/Brand.lua
@@ -0,0 +1,18 @@
+data = {
+    ["LogoFoods"] = {
+        "rbxassetid://13756376540",
+        "rbxassetid://13897954577",
+        "rbxassetid://14309316130",
+        "rbxassetid://13897952773",
+        "rbxassetid://13756377563",
+        "rbxassetid://13897954015",
+        "rbxassetid://13897953285",
+        "rbxassetid://13756377159"
+    },
+    ["BrandFoods"] = {"BombPop", "BombPop", "BombPop", "BombPop", "BombPop", "BombPop", "BombPop", "BombPop", "BombPop"},
+    ["BrandData"] = {["BombPop"] = {["logo"] = "rbxassetid://13029536817"}},
+    ["BrandFurniture"] = {
+        ["BombPopCarryOver"] = {["brand"] = "BombPop", ["expired"] = true},
+        ["BombPopFurniture"] = {["brand"] = "BombPop", ["expired"] = true}
+    }
+}
