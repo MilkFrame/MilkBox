cf1031046ac2a717b0e4aa5dfb9648fceeb6d440
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:29:10 +1100

    Create Materials.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/Materials.lua
@@ -0,0 +1,52 @@
+data = {
+    ["getImage"] = "function: 0x0000000049c52542",
+    ["get"] = "function: 0x0000000092c4dc7a",
+    ["list"] = {
+        ["a"] = {["offset"] = "0, 0", ["material"] = Enum.Material.Brick, ["name"] = "Brick", ["layout"] = 6},
+        ["c"] = {["offset"] = "2, 0", ["material"] = Enum.Material.Concrete, ["name"] = "Concrete", ["layout"] = 4},
+        ["b"] = {["offset"] = "1, 0", ["material"] = Enum.Material.Cobblestone, ["name"] = "Cobble", ["layout"] = 8},
+        ["e"] = {["offset"] = "4, 0", ["material"] = Enum.Material.Marble, ["name"] = "Marble", ["layout"] = 4},
+        ["d"] = {["offset"] = "3, 0", ["material"] = Enum.Material.Fabric, ["name"] = "Fabric", ["layout"] = 4},
+        ["g"] = {["offset"] = "1, 1", ["material"] = Enum.Material.WoodPlanks, ["name"] = "Wood Planks", ["layout"] = 2},
+        ["f"] = {["offset"] = "4, 3", ["material"] = Enum.Material.Wood, ["name"] = "Wood", ["layout"] = 3},
+        ["i"] = {["offset"] = "5, 0", ["material"] = Enum.Material.Slate, ["name"] = "Slate", ["layout"] = 9},
+        ["h"] = {["offset"] = "2, 1", ["material"] = Enum.Material.Plastic, ["name"] = "Plastic", ["layout"] = 1},
+        ["k"] = {
+            ["offset"] = "2, 3",
+            ["material"] = Enum.Material.DiamondPlate,
+            ["name"] = "Diamond Plate",
+            ["layout"] = 11
+        },
+        ["j"] = {["offset"] = "4, 1", ["material"] = Enum.Material.Metal, ["name"] = "Metal", ["layout"] = 10},
+        ["m"] = {["offset"] = "0, 3", ["material"] = Enum.Material.Sand, ["name"] = "Sand", ["layout"] = 13},
+        ["l"] = {["offset"] = "3, 3", ["material"] = Enum.Material.Granite, ["name"] = "Granite", ["layout"] = 12},
+        ["o"] = {
+            ["offset"] = "0, 2",
+            ["name"] = "Tread",
+            ["variant"] = "OldDiamondPlate",
+            ["material"] = Enum.Material.DiamondPlate,
+            ["layout"] = 15
+        },
+        ["n"] = {
+            ["offset"] = "0, 1",
+            ["name"] = "Oak",
+            ["variant"] = "OldWood",
+            ["material"] = Enum.Material.Wood,
+            ["layout"] = 14
+        },
+        ["q"] = {
+            ["offset"] = "3, 1",
+            ["name"] = "Rock",
+            ["variant"] = "OldSlate",
+            ["material"] = Enum.Material.Slate,
+            ["layout"] = 17
+        },
+        ["p"] = {
+            ["offset"] = "1, 3",
+            ["name"] = "Dark Granite",
+            ["variant"] = "OldGranite",
+            ["material"] = Enum.Material.Granite,
+            ["layout"] = 16
+        }
+    }
+}
