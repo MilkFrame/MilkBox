ba0e9a9ed4907f77088976ad9427b1e142c9f91f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:09:32 +1100

    Create Cosmetics.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/DataModules/Cosmetics.lua
@@ -0,0 +1,346 @@
+data = {
+    ["PlateData"] = {
+        ["Flat"] = {
+            ["offset"] = "0, 2",
+            ["image"] = 14779467779,
+            ["rarity"] = "Uncommon",
+            ["displayName"] = "Flat Plate"
+        },
+        ["Bamboo"] = {
+            ["offset"] = "4, 2",
+            ["image"] = 14779467779,
+            ["rarity"] = "Rare",
+            ["displayName"] = "Bamboo Plate"
+        },
+        ["FancyGold"] = {
+            ["offset"] = "2, 1",
+            ["image"] = 14779467779,
+            ["rarity"] = "SuperRare",
+            ["displayName"] = "Fancy Gold Plate"
+        },
+        ["FlatSilver"] = {
+            ["offset"] = "1, 2",
+            ["image"] = 14779467779,
+            ["rarity"] = "Rare",
+            ["displayName"] = "Flat Silver Plate"
+        },
+        ["LightWood"] = {
+            ["offset"] = "3, 0",
+            ["image"] = 14779467779,
+            ["rarity"] = "Common",
+            ["displayName"] = "Light Wood Plate"
+        },
+        ["SmoothWood"] = {
+            ["offset"] = "5, 0",
+            ["image"] = 14779467779,
+            ["rarity"] = "Common",
+            ["displayName"] = "Smooth Wood Plate"
+        },
+        ["Slate"] = {
+            ["offset"] = "5, 1",
+            ["image"] = 14779467779,
+            ["rarity"] = "Common",
+            ["displayName"] = "Slate Plate"
+        },
+        ["DarkWood"] = {
+            ["offset"] = "1, 0",
+            ["image"] = 14779467779,
+            ["rarity"] = "Common",
+            ["displayName"] = "Dark Wood Plate"
+        },
+        ["Diamond"] = {
+            ["offset"] = "5, 2",
+            ["image"] = 14779467779,
+            ["rarity"] = "SuperRare",
+            ["displayName"] = "Diamond Plate"
+        },
+        ["FlatGold"] = {
+            ["offset"] = "1, 1",
+            ["image"] = 14779467779,
+            ["rarity"] = "SuperRare",
+            ["displayName"] = "Flat Gold Plate"
+        },
+        ["Standard"] = {
+            ["offset"] = "2, 3",
+            ["image"] = 14779467779,
+            ["rarity"] = "Common",
+            ["displayName"] = "Standard Plate"
+        },
+        ["Redwood"] = {
+            ["offset"] = "1, 3",
+            ["image"] = 14779467779,
+            ["rarity"] = "Rare",
+            ["displayName"] = "Redwood Plate"
+        },
+        ["MatteBlack"] = {
+            ["offset"] = "3, 1",
+            ["image"] = 14779467779,
+            ["rarity"] = "Rare",
+            ["displayName"] = "Matte Black Plate"
+        },
+        ["Pink"] = {
+            ["offset"] = "0, 3",
+            ["image"] = 14779467779,
+            ["rarity"] = "SuperRare",
+            ["displayName"] = "Pink Plate"
+        },
+        ["Ceramic"] = {
+            ["offset"] = "0, 0",
+            ["image"] = 14779467779,
+            ["rarity"] = "Common",
+            ["displayName"] = "Ceramic Plate"
+        },
+        ["Stoneware"] = {
+            ["offset"] = "4, 1",
+            ["image"] = 14779467779,
+            ["rarity"] = "Uncommon",
+            ["displayName"] = "Stoneware Plate"
+        },
+        ["Marble"] = {
+            ["offset"] = "4, 0",
+            ["image"] = 14779467779,
+            ["rarity"] = "SuperRare",
+            ["displayName"] = "Marble Plate"
+        },
+        ["Metal"] = {
+            ["offset"] = "3, 2",
+            ["image"] = 14779467779,
+            ["rarity"] = "Uncommon",
+            ["displayName"] = "Metal Plate"
+        },
+        ["Emerald"] = {
+            ["offset"] = "2, 0",
+            ["image"] = 14779467779,
+            ["rarity"] = "Rare",
+            ["displayName"] = "Emerald Plate"
+        },
+        ["Fancy"] = {
+            ["offset"] = "0, 1",
+            ["image"] = 14779467779,
+            ["rarity"] = "Uncommon",
+            ["displayName"] = "Fancy Plate"
+        },
+        ["FlatWood"] = {
+            ["offset"] = "2, 2",
+            ["image"] = 14779467779,
+            ["rarity"] = "Uncommon",
+            ["displayName"] = "Flat Wood Plate"
+        }
+    },
+    ["RarityData"] = {
+        ["SuperRare"] = {
+            ["chance"] = 0.1,
+            ["color"] = Color3.fromRGB(170, 85, 244),
+            ["layoutOrder"] = 4,
+            ["displayName"] = "Super Rare"
+        },
+        ["Rare"] = {
+            ["chance"] = 0.25,
+            ["color"] = Color3.fromRGB(240, 69, 69),
+            ["layoutOrder"] = 3,
+            ["displayName"] = "Rare"
+        },
+        ["Uncommon"] = {
+            ["chance"] = 0.29,
+            ["color"] = Color3.fromRGB(236, 109, 18),
+            ["layoutOrder"] = 2,
+            ["displayName"] = "Uncommon"
+        },
+        ["Common"] = {
+            ["chance"] = 0.36,
+            ["color"] = Color3.fromRGB(46, 155, 242),
+            ["layoutOrder"] = 1,
+            ["displayName"] = "Common"
+        }
+    },
+    ["MenuData"] = {
+        ["Wood"] = {
+            ["offset"] = "3, 7",
+            ["image"] = 14779467779,
+            ["rarity"] = "Uncommon",
+            ["displayName"] = "Wooden Menu"
+        },
+        ["Silver"] = {
+            ["offset"] = "1, 7",
+            ["image"] = 14779467779,
+            ["rarity"] = "Rare",
+            ["displayName"] = "Silver Menu"
+        },
+        ["Gold"] = {
+            ["offset"] = "5, 6",
+            ["image"] = 14779467779,
+            ["rarity"] = "SuperRare",
+            ["displayName"] = "Gold Menu"
+        },
+        ["Standard"] = {
+            ["offset"] = "2, 7",
+            ["image"] = 14779467779,
+            ["rarity"] = "Common",
+            ["displayName"] = "Red Menu"
+        },
+        ["Blue"] = {["offset"] = "4, 6", ["image"] = 14779467779, ["rarity"] = "Common", ["displayName"] = "Blue Menu"},
+        ["MatteBlack"] = {
+            ["offset"] = "0, 7",
+            ["image"] = 14779467779,
+            ["rarity"] = "Rare",
+            ["displayName"] = "Matte Black Menu"
+        },
+        ["Antique"] = {
+            ["offset"] = "3, 6",
+            ["image"] = 14779467779,
+            ["rarity"] = "Uncommon",
+            ["displayName"] = "Antique Menu"
+        }
+    },
+    ["BowlData"] = {
+        ["DarkWood"] = {
+            ["offset"] = "5, 3",
+            ["image"] = 14779467779,
+            ["rarity"] = "Common",
+            ["displayName"] = "Dark Wood Bowl"
+        },
+        ["Silver"] = {
+            ["offset"] = "2, 4",
+            ["image"] = 14779467779,
+            ["rarity"] = "Rare",
+            ["displayName"] = "Silver Bowl"
+        },
+        ["Standard"] = {
+            ["offset"] = "3, 3",
+            ["image"] = 14779467779,
+            ["rarity"] = "Common",
+            ["displayName"] = "Standard Bowl"
+        },
+        ["Redwood"] = {
+            ["offset"] = "0, 5",
+            ["image"] = 14779467779,
+            ["rarity"] = "Rare",
+            ["displayName"] = "Redwood Bowl"
+        },
+        ["MatteBlack"] = {
+            ["offset"] = "4, 4",
+            ["image"] = 14779467779,
+            ["rarity"] = "Rare",
+            ["displayName"] = "Matte Black Bowl"
+        },
+        ["Pink"] = {
+            ["offset"] = "5, 4",
+            ["image"] = 14779467779,
+            ["rarity"] = "SuperRare",
+            ["displayName"] = "Pink Bowl"
+        },
+        ["Ceramic"] = {
+            ["offset"] = "4, 3",
+            ["image"] = 14779467779,
+            ["rarity"] = "Common",
+            ["displayName"] = "Ceramic Bowl"
+        },
+        ["Gold"] = {
+            ["offset"] = "1, 4",
+            ["image"] = 14779467779,
+            ["rarity"] = "SuperRare",
+            ["displayName"] = "Gold Bowl"
+        },
+        ["Emerald"] = {
+            ["offset"] = "0, 4",
+            ["image"] = 14779467779,
+            ["rarity"] = "Rare",
+            ["displayName"] = "Emerald Bowl"
+        },
+        ["Wood"] = {
+            ["offset"] = "3, 4",
+            ["image"] = 14779467779,
+            ["rarity"] = "Uncommon",
+            ["displayName"] = "Wood Bowl"
+        }
+    },
+    ["CutleryData"] = {
+        ["Wood"] = {
+            ["offset"] = "1, 6",
+            ["image"] = 14779467779,
+            ["rarity"] = "Common",
+            ["displayName"] = "Wooden Utensils"
+        },
+        ["Steel"] = {
+            ["offset"] = "0, 6",
+            ["image"] = 14779467779,
+            ["rarity"] = "Uncommon",
+            ["displayName"] = "Steel Utensils"
+        },
+        ["Silver"] = {
+            ["offset"] = "5, 5",
+            ["image"] = 14779467779,
+            ["rarity"] = "Rare",
+            ["displayName"] = "Silver Utensils"
+        },
+        ["Gold"] = {
+            ["offset"] = "1, 5",
+            ["image"] = 14779467779,
+            ["rarity"] = "SuperRare",
+            ["displayName"] = "Gold Utensils"
+        },
+        ["Standard"] = {
+            ["offset"] = "4, 5",
+            ["image"] = 14779467779,
+            ["rarity"] = "Common",
+            ["displayName"] = "Standard Utensils"
+        },
+        ["Antique"] = {
+            ["offset"] = "2, 6",
+            ["image"] = 14779467779,
+            ["rarity"] = "Rare",
+            ["displayName"] = "Antique Utensils"
+        },
+        ["MatteBlack"] = {
+            ["offset"] = "3, 5",
+            ["image"] = 14779467779,
+            ["rarity"] = "Uncommon",
+            ["displayName"] = "Matte Black Utensils"
+        },
+        ["Marble"] = {
+            ["offset"] = "2, 5",
+            ["image"] = 14779467779,
+            ["rarity"] = "SuperRare",
+            ["displayName"] = "Marble Utensils"
+        }
+    },
+    ["TrayData"] = {
+        ["Wood"] = {["offset"] = "6, 2", ["image"] = 14779467779, ["rarity"] = "Common", ["displayName"] = "Wood Tray"},
+        ["Marble"] = {
+            ["offset"] = "6, 6",
+            ["image"] = 14779467779,
+            ["rarity"] = "Rare",
+            ["displayName"] = "Marble Tray"
+        },
+        ["Gold"] = {
+            ["offset"] = "6, 0",
+            ["image"] = 14779467779,
+            ["rarity"] = "SuperRare",
+            ["displayName"] = "Gold Tray"
+        },
+        ["Standard"] = {
+            ["offset"] = "6, 3",
+            ["image"] = 14779467779,
+            ["rarity"] = "Common",
+            ["displayName"] = "Standard Tray"
+        },
+        ["Emerald"] = {
+            ["offset"] = "6, 5",
+            ["image"] = 14779467779,
+            ["rarity"] = "Rare",
+            ["displayName"] = "Emerald Tray"
+        },
+        ["Hexagon"] = {
+            ["offset"] = "6, 4",
+            ["image"] = 14779467779,
+            ["rarity"] = "Uncommon",
+            ["displayName"] = "Hexagon Tray"
+        },
+        ["Metal"] = {
+            ["offset"] = "6, 1",
+            ["image"] = 14779467779,
+            ["rarity"] = "Uncommon",
+            ["displayName"] = "Metal Tray"
+        }
+    }
+}
