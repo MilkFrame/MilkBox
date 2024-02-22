c472d00bd246d654c49ae7cbed468ff4ddd5d9b2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:19:13 +1100

    Create Tycoon.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/DataModules/Tycoon.lua
@@ -0,0 +1,16 @@
+data = {
+    ["SaveFileIDs"] = {
+        {["letter"] = "a", ["premium"] = false},
+        {["letter"] = "b", ["premium"] = false},
+        {["letter"] = "c", ["premium"] = false},
+        {["letter"] = "d", ["premium"] = false},
+        {["letter"] = "e", ["premium"] = false},
+        {["letter"] = "f", ["premium"] = false},
+        {["letter"] = "g", ["premium"] = true},
+        {["letter"] = "h", ["premium"] = true},
+        {["letter"] = "i", ["premium"] = true},
+        {["letter"] = "j", ["premium"] = true},
+        {["letter"] = "k", ["premium"] = true},
+        {["letter"] = "l", ["premium"] = true}
+    }
+}
