87ff2627626bd6417d5d50bbbf3569e8891f9d72
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:35:25 +1100

    Create Toys.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/Toys.lua
@@ -0,0 +1,15 @@
+data = {
+    ["a"] = {["Price"] = 5, ["unlockId"] = 1, ["Name"] = "Beach Ball", ["Model"] = "BeachBall"},
+    ["b"] = {["Price"] = 10, ["unlockId"] = 5, ["Name"] = "Bunny Plushie", ["Model"] = "Bunny"},
+    ["c"] = {["Price"] = 10, ["unlockId"] = 6, ["Name"] = "Dino Plushie", ["Model"] = "Dino"},
+    ["d"] = {["Price"] = 5, ["unlockId"] = 2, ["Name"] = "Pinwheel Headband", ["Model"] = "Headband"},
+    ["e"] = {["Price"] = 10, ["unlockId"] = 7, ["Name"] = "Noob Sunglasses", ["Model"] = "NoobSunglasses"},
+    ["f"] = {["Price"] = 15, ["unlockId"] = 10, ["Name"] = "Puppy Plushie", ["Model"] = "Puppy"},
+    ["g"] = {["Price"] = 5, ["unlockId"] = 3, ["Name"] = "Space Sunglasses", ["Model"] = "SpaceSunglasses"},
+    ["h"] = {["Price"] = 10, ["unlockId"] = 8, ["Name"] = "Water Gun", ["Model"] = "Watergun"},
+    ["i"] = {["Price"] = 15, ["unlockId"] = 11, ["Name"] = "Ninja Set", ["Model"] = "NinjaSet"},
+    ["j"] = {["Price"] = 5, ["unlockId"] = 4, ["Name"] = "Normal Sunglasses", ["Model"] = "NormalSunglasses"},
+    ["k"] = {["Price"] = 15, ["unlockId"] = 12, ["Name"] = "Police Car", ["Model"] = "PoliceCar"},
+    ["l"] = {["Price"] = 10, ["unlockId"] = 9, ["Name"] = "Toy Car", ["Model"] = "RedCar"}
+    ["m"] = {["Price"] = 15, ["unlockId"] = 13, ["Name"] = "Toy Rover Car", ["Model"] = "Rover"},
+}
