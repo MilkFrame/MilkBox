5467ae0bc3033a2d601f466c60b97c79c7513a89
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:24:45 +1100

    Create Christmas.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/Christmas.lua
@@ -0,0 +1,34 @@
+data = {
+    ["Prizes"] = {
+        {
+            ["Offset"] = "0, 0",
+            ["Img"] = "https://www.roblox.com/asset-thumbnail/image?assetId=4484629207&width=420&height=420&format=png",
+            ["Name"] = "Candy Cane",
+            ["Size"] = "0, 0"
+        },
+        {
+            ["Offset"] = "128, 256",
+            ["Img"] = "http://www.roblox.com/asset/?id=2837097712",
+            ["Name"] = "Cookies",
+            ["Size"] = "128, 128"
+        },
+        {
+            ["Offset"] = "0, 0",
+            ["Img"] = "http://www.roblox.com/asset/?id=2837097712",
+            ["Name"] = "Marshmallows",
+            ["Size"] = "128, 128"
+        },
+        {
+            ["Offset"] = "384, 384",
+            ["Img"] = "http://www.roblox.com/asset/?id=2837097712",
+            ["Name"] = "Chocolate",
+            ["Size"] = "128, 128"
+        }
+    },
+    ["Elfs"] = {
+        ["Pink"] = {["Color"] = Color3.fromRGB(255, 96, 234), ["LeastFavorite"] = 4, ["FavoriteFood"] = 3},
+        ["Green"] = {["Color"] = Color3.fromRGB(155, 255, 119), ["LeastFavorite"] = 3, ["FavoriteFood"] = 4},
+        ["Blue"] = {["Color"] = Color3.fromRGB(116, 179, 255), ["LeastFavorite"] = 1, ["FavoriteFood"] = 2},
+        ["Red"] = {["Color"] = Color3.fromRGB(255, 85, 102), ["LeastFavorite"] = 2, ["FavoriteFood"] = 1}
+    }
+}
