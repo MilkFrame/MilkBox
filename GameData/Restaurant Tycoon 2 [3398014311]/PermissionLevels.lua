f7eb17721e01f116106ced48a56d49c60828ec91
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:30:15 +1100

    Create PermissionLevels.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/PermissionLevels.lua
@@ -0,0 +1,6 @@
+data = {
+    ["a"] = {["Spend"] = false, ["Build"] = true},
+    ["b"] = {["Spend"] = false, ["Build"] = false},
+    ["c"] = {["Spend"] = true, ["Build"] = true},
+    ["d"] = {["Spend"] = true, ["Build"] = false}
+}
