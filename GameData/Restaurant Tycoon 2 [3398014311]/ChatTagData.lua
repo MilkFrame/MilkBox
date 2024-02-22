63fe4eda75d682a3d05cad330a0becd3125a4184
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:24:12 +1100

    Create ChatTagData.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/ChatTagData.lua
@@ -0,0 +1,5 @@
+data = {
+    ["Beta"] = {["color"] = Color3.fromRGB(0, 255, 255), ["text"] = "Beta", ["priority"] = 5},
+    ["Tester"] = {["color"] = Color3.fromRGB(255, 94, 242), ["text"] = "Tester", ["priority"] = 6},
+    ["Developer"] = {["color"] = Color3.fromRGB(255, 230, 102), ["text"] = "Developer", ["priority"] = 10}
+}
