57d0ef620c915e5a75f03c9ea36af8b2dbc217ad
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:17:29 +1100

    Create Pet.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/DataModules/Pet.lua
@@ -0,0 +1,107 @@
+data = {
+    ["PetData"] = {
+        ["Grapes"] = {
+            ["offset"] = "5, 0",
+            ["image"] = 14646043669,
+            ["rarity"] = "Uncommon",
+            ["model"] = "Grapes",
+            ["available"] = true,
+            ["effectArgument"] = 258,
+            ["effect"] = "Delicacy",
+            ["displayName"] = "Grapes"
+        },
+        ["Pizza"] = {
+            ["offset"] = "1, 0",
+            ["image"] = 14646043669,
+            ["rarity"] = "Uncommon",
+            ["model"] = "Pizza",
+            ["effectArgument"] = "Italy",
+            ["effect"] = "Gourmet",
+            ["displayName"] = "Pizza"
+        },
+        ["Burger"] = {
+            ["offset"] = "3, 0",
+            ["image"] = 14646043669,
+            ["rarity"] = "Common",
+            ["model"] = "Burger",
+            ["avatarItemId"] = 13266752694,
+            ["effectArgument"] = "UnitedStates",
+            ["effect"] = "Gourmet",
+            ["displayName"] = "Burger"
+        },
+        ["Taco"] = {
+            ["offset"] = "0, 0",
+            ["image"] = 14646043669,
+            ["rarity"] = "SuperRare",
+            ["model"] = "Taco",
+            ["effectArgument"] = "Mexico",
+            ["effect"] = "Gourmet",
+            ["displayName"] = "Taco"
+        },
+        ["Watermelon"] = {
+            ["offset"] = "0, 1",
+            ["image"] = 14646043669,
+            ["rarity"] = "Rare",
+            ["model"] = "Watermelon",
+            ["available"] = true,
+            ["effectArgument"] = 7,
+            ["effect"] = "Popular",
+            ["displayName"] = "Watermelon"
+        },
+        ["IcePop"] = {
+            ["offset"] = "4, 0",
+            ["image"] = 14646043669,
+            ["rarity"] = "Rare",
+            ["model"] = "IcePop",
+            ["effectArgument"] = 30,
+            ["effect"] = "Delicacy",
+            ["displayName"] = "Ice Pop"
+        },
+        ["Apple"] = {
+            ["offset"] = "2, 0",
+            ["image"] = 14646043669,
+            ["rarity"] = "Common",
+            ["model"] = "Apple",
+            ["avatarItemId"] = 13266756618,
+            ["effectArgument"] = 256,
+            ["effect"] = "Delicacy",
+            ["displayName"] = "Apple"
+        },
+        ["Coconut"] = {
+            ["offset"] = "1, 1",
+            ["image"] = 14646043669,
+            ["rarity"] = "SuperRare",
+            ["model"] = "Coconut",
+            ["available"] = true,
+            ["effectArgument"] = 20,
+            ["effect"] = "Vitality",
+            ["displayName"] = "Coconut"
+        },
+        ["Pineapple"] = {
+            ["offset"] = "6, 0",
+            ["image"] = 14646043669,
+            ["rarity"] = "Uncommon",
+            ["model"] = "Pineapple",
+            ["available"] = true,
+            ["effectArgument"] = 5,
+            ["effect"] = "Popular",
+            ["displayName"] = "Pineapple"
+        },
+        ["Banana"] = {
+            ["offset"] = "7, 0",
+            ["image"] = 14646043669,
+            ["rarity"] = "Common",
+            ["model"] = "Banana",
+            ["available"] = true,
+            ["effectArgument"] = 10,
+            ["effect"] = "Vitality",
+            ["displayName"] = "Banana"
+        }
+    },
+    ["PetEffectData"] = {
+        ["Gourmet"] = {["getDescription"] = "function: 0x0000000080a53efa", ["displayName"] = "Gourmet"},
+        ["Vitality"] = {["getDescription"] = "function: 0x000000000eef2682", ["displayName"] = "Vitality"},
+        ["Delicacy"] = {["getDescription"] = "function: 0x000000005dd7d5b2", ["displayName"] = "Delicacy"},
+        ["Popular"] = {["getDescription"] = "function: 0x00000000c2c790fa", ["displayName"] = "Popular"}
+    }
+}
