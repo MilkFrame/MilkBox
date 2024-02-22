19606957f515d30fe1c4565d06a8d69dd79a96d4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:42:22 +1100

    Create Vehicles.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/Vehicles.lua
@@ -0,0 +1,79 @@
+data = {
+    main = {
+        ["a"] = {
+            ["Offset"] = "0, 0",
+            ["Image"] = "rbxassetid://5306136250",
+            ["Name"] = "Sports Car",
+            ["Price"] = 50,
+            ["Type"] = "Car",
+            ["Body"] = "SportsBody"
+        },
+        ["h"] = {
+            ["Offset"] = "0, 0",
+            ["Image"] = "rbxassetid://7897089600",
+            ["Name"] = "Food Truck",
+            ["Body"] = "FoodTruck",
+            ["Price"] = 40,
+            ["Type"] = "Car",
+            ["Hidden"] = true,
+            ["Size"] = "128, 128"
+        },
+        ["c"] = {
+            ["Offset"] = "0, 256",
+            ["Image"] = "rbxassetid://5306136250",
+            ["Name"] = "Jeep",
+            ["Price"] = 30,
+            ["Type"] = "Car",
+            ["Body"] = "JeepBody"
+        },
+        ["b"] = {
+            ["Offset"] = "256, 0",
+            ["Image"] = "rbxassetid://5306136250",
+            ["Name"] = "SUV",
+            ["Price"] = 40,
+            ["Type"] = "Car",
+            ["Body"] = "SUVBody"
+        },
+        ["e"] = {
+            ["Offset"] = "512, 0",
+            ["Image"] = "rbxassetid://5306136250",
+            ["Name"] = "Coupe",
+            ["Price"] = 40,
+            ["DriveThruEnabled"] = true,
+            ["Type"] = "Car",
+            ["Body"] = "PorscheBody"
+        },
+        ["d"] = {
+            ["Offset"] = "256, 256",
+            ["Image"] = "rbxassetid://5306136250",
+            ["Name"] = "Hatchback Car",
+            ["Price"] = 40,
+            ["Type"] = "Car",
+            ["Body"] = "MiniBody"
+        },
+        ["g"] = {
+            ["Offset"] = "512, 256",
+            ["Image"] = "rbxassetid://5306136250",
+            ["Name"] = "Moped",
+            ["Price"] = 40,
+            ["Type"] = "Bike",
+            ["Hidden"] = true,
+            ["Body"] = "Moped"
+        },
+        ["f"] = {
+            ["Offset"] = "768, 0",
+            ["Image"] = "rbxassetid://5306136250",
+            ["Name"] = "Sedan Sport",
+            ["Price"] = 40,
+            ["DriveThruEnabled"] = true,
+            ["Type"] = "Car",
+            ["Body"] = "MustangBody"
+        }
+    },
+    CarStats = {
+        ["Moped"] = {["reverseSpeed"] = 50},
+        ["MustangBody"] = {["maxSpeed"] = 80},
+        ["SportsBody"] = {["maxSpeed"] = 60},
+        ["PorscheBody"] = {["maxSpeed"] = 80}
+    }
+}
