0955c466b88e4a37d748009a1f0033a7f7117e0a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:31:36 +1100

    Create PlacementControlData.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/PlacementControlData.lua
@@ -0,0 +1,8 @@
+data = {
+    ["CopyColor"] = {["computerControl"] = "Ctrl + Click", ["description"] = "Copy Color"},
+    ["Cancel"] = {["computerControl"] = "E", ["gamepadControl"] = "B", ["description"] = "Cancel"},
+    ["Place"] = {["computerControl"] = "Click", ["gamepadControl"] = "RT", ["description"] = "Place Item"},
+    ["Rotate"] = {["computerControl"] = "R", ["gamepadControl"] = "X", ["description"] = "Rotate Item"},
+    ["PickItem"] = {["computerControl"] = "Q", ["description"] = "Pick Item"},
+    ["ChangeGrid"] = {["computerControl"] = "G", ["description"] = "Change Placement Grid"}
+}
