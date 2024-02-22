023f2f9fe8eea7dc182ce606ff4f647922fae47a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:15:54 +1100

    Create Furniture.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/DataModules/Furniture.lua
@@ -0,0 +1,480 @@
+data = {
+    ["CanEditMaterial"] = {["check"] = "function: 0x00000000c86b2e02"},
+    ["FurnitureAllowedInEggs"] = {
+        ["Set2"] = true,
+        ["Exclusive"] = true,
+        ["General"] = true,
+        ["Set1"] = true,
+        ["NewRoofs"] = true,
+        ["Set15"] = true,
+        ["Set5"] = true,
+        ["Set17"] = true,
+        ["Set3"] = true,
+        ["Set9"] = true,
+        ["Windows"] = true,
+        ["Regular"] = true,
+        ["NatureItems"] = true,
+        ["Stairs"] = true,
+        ["Set6"] = true,
+        ["Doors"] = true,
+        ["Set16"] = true,
+        ["JapanBasic"] = true,
+        ["EKEAOld"] = true,
+        ["Set14"] = true,
+        ["Set4"] = true,
+        ["Set19"] = true,
+        ["Set18"] = true,
+        ["Set13"] = true,
+        ["Set12"] = true,
+        ["Set7"] = true,
+        ["Set10"] = true,
+        ["Set11"] = true,
+        ["Set8"] = true
+    },
+    ["DoorEntranceSize"] = {
+        ["get"] = "function: 0x000000006ea86b4a",
+        ["sizes"] = {["Single"] = 0, ["Double"] = 2},
+        ["list"] = {0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2}
+    },
+    ["SpawnModel"] = {
+        ["addObjectId"] = "function: 0x000000006242352a",
+        ["getModelFromLine"] = "function: 0x000000007a979ba2",
+        ["getModelFromId"] = "function: 0x00000000d114978a",
+        ["getOriginalModelFromLine"] = "function: 0x000000008036e21a",
+        ["getOriginalModelFromId"] = "function: 0x000000001557006a"
+    },
+    ["LockedFolders"] = {
+        ["TipJarUpgrade"] = {["Locked"] = true, ["LockedByDefault"] = true, ["Message"] = "Requires Upgrade: Tip Jars"},
+        ["ChristmasPrizes"] = {
+            ["Message"] = "Requires Pack: Christmas",
+            ["Pack"] = 3,
+            ["Locked"] = true,
+            ["LockedByDefault"] = true,
+            ["Reason"] = "Pack"
+        },
+        ["SciFiPack"] = {
+            ["Message"] = "Requires Pack: Sci-Fi",
+            ["Pack"] = 12,
+            ["Locked"] = true,
+            ["LockedByDefault"] = true,
+            ["Reason"] = "Pack"
+        },
+        ["Luigi1"] = {
+            ["Locked"] = true,
+            ["LockedByDefault"] = true,
+            ["HiddenInChina"] = true,
+            ["Message"] = "Requires Quest: Luigi's Pt. 1"
+        },
+        ["EventReward3"] = {["Locked"] = true, ["LockedByDefault"] = true, ["Message"] = "Play Roblox Halloween 2021"},
+        ["AquariumPack"] = {
+            ["Message"] = "Requires Pack: Aquarium",
+            ["Pack"] = 11,
+            ["Locked"] = true,
+            ["LockedByDefault"] = true,
+            ["Reason"] = "Pack"
+        },
+        ["Halloween"] = {
+            ["Message"] = "Requires Pack: Halloween",
+            ["Pack"] = 1,
+            ["Locked"] = true,
+            ["LockedByDefault"] = true,
+            ["Reason"] = "Pack"
+        },
+        ["EKEAUpstairs"] = {["Locked"] = false, ["LockedByDefault"] = false},
+        ["Outdoor"] = {
+            ["Message"] = "Requires Pack: Outdoor",
+            ["Pack"] = 2,
+            ["Locked"] = true,
+            ["LockedByDefault"] = true,
+            ["Reason"] = "Pack"
+        },
+        ["EventReward2"] = {["Locked"] = true, ["LockedByDefault"] = true, ["Message"] = "Play Roblox Halloween 2021"},
+        ["Christmas"] = {
+            ["Message"] = "Requires Pack: Christmas",
+            ["Pack"] = 3,
+            ["Locked"] = true,
+            ["LockedByDefault"] = true,
+            ["Reason"] = "Pack"
+        },
+        ["EventReward1"] = {["Locked"] = true, ["LockedByDefault"] = true, ["Message"] = "Play Roblox Halloween 2021"},
+        ["JapanPack"] = {
+            ["Message"] = "Requires Pack: Japanese",
+            ["Pack"] = 4,
+            ["Locked"] = true,
+            ["LockedByDefault"] = true,
+            ["Reason"] = "Pack"
+        },
+        ["VintagePack"] = {
+            ["Message"] = "Requires Pack: Vintage",
+            ["Pack"] = 10,
+            ["Locked"] = true,
+            ["LockedByDefault"] = true,
+            ["Reason"] = "Pack"
+        },
+        ["RestaurantKit2"] = {["Locked"] = false, ["LockedByDefault"] = false},
+        ["ChristmasPack2021"] = {
+            ["Message"] = "Requires Pack: Christmas (Rare)",
+            ["Pack"] = 7,
+            ["Locked"] = true,
+            ["LockedByDefault"] = true,
+            ["Reason"] = "Gift"
+        },
+        ["RestaurantKit5"] = {["Locked"] = false, ["LockedByDefault"] = false},
+        ["RestaurantKit4"] = {["Locked"] = false, ["LockedByDefault"] = false},
+        ["WesternPack"] = {
+            ["Message"] = "Requires Pack: Western",
+            ["Pack"] = 6,
+            ["Locked"] = true,
+            ["LockedByDefault"] = true,
+            ["Reason"] = "Pack"
+        },
+        ["ConstructionStore"] = {["Locked"] = false, ["LockedByDefault"] = false},
+        ["Luigi2"] = {
+            ["Locked"] = true,
+            ["LockedByDefault"] = true,
+            ["HiddenInChina"] = true,
+            ["Message"] = "Requires Quest: Luigi's Pt. 2"
+        },
+        ["MedievalPack"] = {
+            ["Message"] = "Requires Pack: Medieval",
+            ["Pack"] = 5,
+            ["Locked"] = true,
+            ["LockedByDefault"] = true,
+            ["Reason"] = "Pack"
+        },
+        ["BabyChairUpgrade"] = {
+            ["Locked"] = true,
+            ["LockedByDefault"] = true,
+            ["Message"] = "Requires Upgrade: Baby Chairs"
+        },
+        ["Invisible"] = {["Locked"] = true, ["LockedByDefault"] = true, ["Message"] = "Not available"},
+        ["RestaurantKit3"] = {["Locked"] = false, ["LockedByDefault"] = false},
+        ["FarmPack"] = {
+            ["Message"] = "Requires Pack: Farm",
+            ["Pack"] = 8,
+            ["Locked"] = true,
+            ["LockedByDefault"] = true,
+            ["Reason"] = "Pack"
+        },
+        ["RestaurantKit1"] = {["Locked"] = false, ["LockedByDefault"] = false},
+        ["DeliveryUpgrade"] = {
+            ["Locked"] = true,
+            ["LockedByDefault"] = true,
+            ["Message"] = "Requires Upgrade: Delivery"
+        },
+        ["Furniture"] = {["Locked"] = false, ["LockedByDefault"] = false}
+    },
+    ["Classes"] = {
+        ["MaxItems"] = {
+            ["Roof"] = 100,
+            ["Wall"] = 200,
+            ["Firework"] = 30,
+            ["TableTop"] = 100,
+            ["Statue"] = 8,
+            ["FoodDisplay"] = 5
+        },
+        ["Properties"] = {
+            {
+                ["GridSize"] = 4,
+                ["GetParent"] = "function: 0x00000000af73672a",
+                ["Collisions"] = true,
+                ["CoordinateType"] = "Normal",
+                ["GetTargetFilter"] = "function: 0x000000000d536f62",
+                ["YMode"] = "Normal",
+                ["ModuleName"] = "BuildObject",
+                ["Rotatable"] = true,
+                ["TallyFolder"] = "function: 0x000000000d84cbca"
+            },
+            {
+                ["ModuleName"] = "BuildObject",
+                ["Rotatable"] = true,
+                ["GetTargetFilter"] = "function: 0x00000000c442bbb2",
+                ["YMode"] = "Normal",
+                ["GridSize"] = 4,
+                ["GetParent"] = "function: 0x00000000703c10aa",
+                ["Collisions"] = true,
+                ["CoordinateType"] = "Wall"
+            },
+            {
+                ["ModuleName"] = "BuildObject",
+                ["Rotatable"] = true,
+                ["GetTargetFilter"] = "function: 0x000000005cbd1242",
+                ["YMode"] = "Normal",
+                ["GridSize"] = 4,
+                ["GetParent"] = "function: 0x0000000008614c3a",
+                ["Collisions"] = true,
+                ["CoordinateType"] = "Wall"
+            },
+            {
+                ["GridSize"] = 4,
+                ["GetParent"] = "function: 0x00000000f4c84552",
+                ["Collisions"] = true,
+                ["CoordinateType"] = "Roof",
+                ["CollisionOffset"] = 200,
+                ["GetTargetFilter"] = "function: 0x00000000a533f472",
+                ["YMode"] = "Roof",
+                ["ModuleName"] = "BuildObject",
+                ["Rotatable"] = true
+            },
+            {
+                ["GridSize"] = 1,
+                ["GetParent"] = "function: 0x00000000b2211dd2",
+                ["Collisions"] = false,
+                ["CoordinateType"] = "None",
+                ["GetTargetFilter"] = "function: 0x0000000069dceffa",
+                ["YMode"] = "Increment",
+                ["ModuleName"] = "BuildWallObject",
+                ["Rotatable"] = true,
+                ["TallyFolder"] = "function: 0x00000000acaa345a"
+            },
+            {
+                ["GridSize"] = 1,
+                ["GetParent"] = "function: 0x000000003df5c782",
+                ["Collisions"] = false,
+                ["CoordinateType"] = "None",
+                ["GetTargetFilter"] = "function: 0x0000000001883eca",
+                ["YMode"] = "RoofItem",
+                ["ModuleName"] = "BuildObject",
+                ["Rotatable"] = true,
+                ["TallyFolder"] = "function: 0x0000000008afc0ca"
+            },
+            {
+                ["GridSize"] = 0.5,
+                ["GetParent"] = "function: 0x00000000a11e8b42",
+                ["Collisions"] = false,
+                ["CoordinateType"] = "None",
+                ["GetTargetFilter"] = "function: 0x0000000055a79512",
+                ["YMode"] = "Decimal",
+                ["ModuleName"] = "BuildSmallObject",
+                ["Rotatable"] = true,
+                ["TallyFolder"] = "function: 0x0000000002643d1a"
+            },
+            {
+                ["GridSize"] = 2,
+                ["GetParent"] = "function: 0x00000000ea2179a2",
+                ["Collisions"] = true,
+                ["CoordinateType"] = "None",
+                ["CollisionOffset"] = 199,
+                ["GetTargetFilter"] = "function: 0x000000003e019c2a",
+                ["YMode"] = "Normal",
+                ["ModuleName"] = "BuildObject",
+                ["Rotatable"] = true
+            },
+            {
+                ["GridSize"] = 4,
+                ["GetParent"] = "function: 0x00000000b68d527a",
+                ["Collisions"] = true,
+                ["CoordinateType"] = "None",
+                ["CollisionOffset"] = 0.25,
+                ["GetTargetFilter"] = "function: 0x0000000002e44232",
+                ["YMode"] = "Normal",
+                ["ModuleName"] = "BuildObject",
+                ["Rotatable"] = true
+            },
+            {
+                ["ModuleName"] = "BuildObject",
+                ["Rotatable"] = true,
+                ["GetTargetFilter"] = "function: 0x00000000d6da2bba",
+                ["YMode"] = "Normal",
+                ["GridSize"] = 4,
+                ["GetParent"] = "function: 0x0000000037fc7432",
+                ["Collisions"] = true,
+                ["CoordinateType"] = "Normal"
+            },
+            {
+                ["Rotatable"] = false,
+                ["ModuleName"] = "BuildCustomWall",
+                ["GetTargetFilter"] = "function: 0x000000006e75c30a",
+                ["YMode"] = "Normal",
+                ["GridSize"] = 4,
+                ["GetParent"] = "function: 0x000000009aa893c2",
+                ["Collisions"] = true,
+                ["CoordinateType"] = "None"
+            },
+            {
+                ["BoundaryFunction"] = "function: 0x00000000a6835dea",
+                ["GridSize"] = 4,
+                ["GetParent"] = "function: 0x00000000339ee8d2",
+                ["Collisions"] = false,
+                ["CoordinateType"] = "None",
+                ["GetTargetFilter"] = "function: 0x0000000007b7919a",
+                ["YMode"] = "Normal",
+                ["ModuleName"] = "BuildObject",
+                ["Rotatable"] = true
+            },
+            {
+                ["ModuleName"] = "BuildObject",
+                ["Rotatable"] = true,
+                ["GetTargetFilter"] = "function: 0x000000002df0638a",
+                ["YMode"] = "NewRoof",
+                ["GridSize"] = 4,
+                ["GetParent"] = "function: 0x000000009f14266a",
+                ["Collisions"] = true,
+                ["CoordinateType"] = "Roof"
+            },
+            {
+                ["ModuleName"] = "BuildObject",
+                ["Rotatable"] = false,
+                ["GetTargetFilter"] = "function: 0x00000000239998da",
+                ["YMode"] = "Ground",
+                ["GridSize"] = 8,
+                ["GetParent"] = "function: 0x0000000047209682",
+                ["Collisions"] = false,
+                ["CoordinateType"] = "None"
+            }
+        },
+        ["Id"] = {
+            ["SmallItem"] = 7,
+            ["WallItem"] = 5,
+            ["SingleDoor"] = 3,
+            ["SingleWall"] = 2,
+            ["RoofItem"] = 6,
+            ["Ground"] = 14,
+            ["Config"] = 12,
+            ["Object"] = 1,
+            ["NewRoof"] = 13,
+            ["SingleRoof"] = 4,
+            ["SurfaceObject"] = 10,
+            ["Pillar"] = 8,
+            ["AlternateObject"] = 9
+        }
+    },
+    ["HighlightedFurniture"] = {
+        {["layoutOrder"] = -4, ["highlightText"] = "Basics"},
+        {["highlightText"] = "Code"},
+        {["highlightText"] = "Code"},
+        {["layoutOrder"] = -2, ["highlightText"] = "Basics"},
+        {["layoutOrder"] = -1, ["highlightText"] = "Basics"},
+        {["layoutOrder"] = -3, ["highlightText"] = "Basics"},
+        {["highlightText"] = "Code"},
+        {["highlightText"] = "Code"},
+        {["highlightText"] = "Event"},
+        {["highlightText"] = "Code"}
+    },
+    ["HiddenFolders"] = {["Valentines"] = true, ["Invisible"] = true, ["NewYears"] = true, ["Easter"] = true},
+    ["CannotAddToInventory"] = {true, true},
+    ["Categories"] = {
+        ["Categories"] = {
+            ["Special"] = {
+                ["SciFi"] = "Sci-Fi Pack",
+                ["Beach"] = "Beach Pack",
+                ["Japanese"] = "Japanese Pack",
+                ["Vintage"] = "Vintage Pack",
+                ["Medieval"] = "Medieval Pack",
+                ["Configuration"] = "Configuration",
+                ["Aquarium"] = "Aquarium Pack",
+                ["Valentines"] = "Valentine's",
+                ["Halloween"] = "Halloween",
+                ["Farm"] = "Farm Pack",
+                ["Outdoor"] = "Outdoor Pack",
+                ["Western"] = "Western Pack",
+                ["Christmas"] = "Christmas",
+                ["Quests"] = "Quests",
+                ["NewYears"] = "New Year"
+            },
+            ["Construction"] = {
+                ["Stairs"] = "Stairs",
+                ["Pillars"] = "Pillars",
+                ["Roofs"] = "Roofs",
+                ["Doors"] = "Doors",
+                ["Walls"] = "Walls",
+                ["Fences"] = "Fences",
+                ["Flooring"] = "Flooring",
+                ["Ground"] = "Ground",
+                ["Other"] = "Other"
+            },
+            ["Decorations"] = {
+                ["Lights"] = "Lights",
+                ["Misc"] = "Misc",
+                ["Tech"] = "Electronics",
+                ["Art"] = "Art",
+                ["Plants"] = "Nature",
+                ["BombPop"] = "Bomb Pop",
+                ["Shelves"] = "Shelves",
+                ["Signs"] = "Signs",
+                ["Cookery"] = "Cookery",
+                ["New"] = "New!"
+            },
+            ["Restaurant"] = {
+                ["Bathroom"] = "Bathroom",
+                ["Tables"] = "Tables",
+                ["Kitchens"] = "Kitchen",
+                ["Chairs"] = "Chairs"
+            }
+        },
+        ["SubCategoryInformation"] = {
+            ["Aquarium Pack"] = {["hidden"] = true},
+            ["Roofs"] = {["availableEnvironments"] = {"Restaurant"}},
+            ["Configuration"] = {["availableEnvironments"] = {"Restaurant"}},
+            ["Outdoor Pack"] = {["hidden"] = true},
+            ["Halloween"] = {["hidden"] = true, ["color"] = Color3.fromRGB(230, 144, 40)},
+            ["Home"] = {["availableEnvironments"] = {"Home"}},
+            ["Bomb Pop"] = {["top"] = true, ["branded"] = true, ["hidden"] = true, ["displayName"] = "Bomb Pop (AD)"},
+            ["Christmas"] = {
+                ["color"] = Color3.fromRGB(255, 92, 103),
+                ["hidden"] = true,
+                ["textColor"] = Color3.fromRGB(255, 255, 255)
+            },
+            ["Ground"] = {["hidden"] = true, ["availableEnvironments"] = {"Restaurant"}},
+            ["Stairs"] = {["availableEnvironments"] = {"Restaurant"}},
+            ["Valentine's"] = {["hidden"] = true},
+            ["New!"] = {["top"] = true, ["hidden"] = false, ["color"] = Color3.fromRGB(172, 237, 255)},
+            ["Medieval Pack"] = {["hidden"] = true},
+            ["Beach Pack"] = {["hidden"] = true},
+            ["Farm Pack"] = {["hidden"] = true},
+            ["Western Pack"] = {["hidden"] = true},
+            ["Flooring"] = {["availableEnvironments"] = {"Restaurant"}},
+            ["Japanese Pack"] = {["hidden"] = true},
+            ["New Year"] = {["hidden"] = true}
+        },
+        ["LayoutOrder"] = {
+            ["Aquarium Pack"] = 41,
+            ["Art"] = 9,
+            ["Shelves"] = 10,
+            ["Restaurant"] = -5,
+            ["Outdoor Pack"] = 37,
+            ["Fences"] = 23,
+            ["Decorations"] = 5,
+            ["Bathroom"] = -1,
+            ["Bomb Pop"] = -7,
+            ["Valentine's"] = 52,
+            ["Construction"] = 14,
+            ["Sports"] = 12,
+            ["Kitchen"] = -2,
+            ["Inventory"] = -10,
+            ["Misc"] = 13,
+            ["Roofs"] = 21,
+            ["Configuration"] = 34,
+            ["Nature"] = 7,
+            ["Halloween"] = 50,
+            ["Home"] = -8,
+            ["Other"] = 25,
+            ["Doors"] = 20,
+            ["Christmas"] = 51,
+            ["Japanese Pack"] = 36,
+            ["Medieval Pack"] = 38,
+            ["Lights"] = 6,
+            ["Electronics"] = 11,
+            ["New!"] = -9,
+            ["Tables"] = -4,
+            ["Quests"] = 33,
+            ["Special"] = 32,
+            ["Cookery"] = 9,
+            ["Chairs"] = -3,
+            ["Beach Pack"] = 40,
+            ["Stairs"] = 24,
+            ["Pillars"] = 23,
+            ["Flooring"] = 22,
+            ["Western Pack"] = 39,
+            ["Signs"] = 8,
+            ["Walls"] = 19,
+            ["New Year"] = 53
+        },
+        ["CategoryInformation"] = {
+            ["Special"] = {},
+            ["Construction"] = {},
+            ["Decorations"] = {},
+            ["Restaurant"] = {["openByDefault"] = true}
+        }
+    }
+}
