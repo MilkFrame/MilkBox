99656fba781fb4dd8c008cf3f524c143d38b041d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:34:30 +1100

    Create Textures.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/Textures.lua
@@ -0,0 +1,61 @@
+data = {
+    ["a"] = {["layout"] = 10, ["t"] = "rbxassetid://1433438832", ["name"] = "Pattern", ["size"] = 2},
+    ["c"] = {["layout"] = 12, ["t"] = "rbxassetid://291169891", ["name"] = "Ornate", ["size"] = 4},
+    ["b"] = {["layout"] = 5, ["t"] = "rbxassetid://2852608818", ["name"] = "Bricks 1", ["size"] = 4},
+    ["d"] = {["layout"] = 3, ["t"] = "rbxassetid://1513910198", ["name"] = "Wood 3", ["size"] = 4},
+    ["g"] = {["layout"] = 15, ["t"] = "rbxassetid://92068989", ["name"] = "Checked", ["size"] = 4},
+    ["f"] = {["layout"] = 13, ["t"] = "rbxassetid://240352561", ["name"] = "Tiles 1", ["size"] = 4},
+    ["h"] = {["layout"] = 14, ["t"] = "rbxassetid://748527930", ["name"] = "Tiles 2", ["size"] = 4},
+    ["k"] = {["layout"] = 16, ["t"] = "rbxassetid://2335774404", ["name"] = "Funky Wood", ["size"] = 4},
+    ["m"] = {
+        ["name"] = "Bricks 2",
+        ["layout"] = 6,
+        ["t"] = "rbxassetid://3807523935",
+        ["material"] = Enum.Material.Brick,
+        ["size"] = 4
+    },
+    ["l"] = {["layout"] = 9, ["t"] = "rbxassetid://1553880873", ["name"] = "Cobble 2", ["size"] = 4},
+    ["o"] = {
+        ["name"] = "Concrete",
+        ["layout"] = 4,
+        ["t"] = "rbxassetid://3807525794",
+        ["material"] = Enum.Material.Concrete,
+        ["size"] = 4
+    },
+    ["n"] = {
+        ["name"] = "Cobble 1",
+        ["layout"] = 8,
+        ["t"] = "rbxassetid://3807525224",
+        ["material"] = Enum.Material.Cobblestone,
+        ["size"] = 4
+    },
+    ["q"] = {
+        ["name"] = "Marble",
+        ["layout"] = 4,
+        ["t"] = "rbxassetid://3807526610",
+        ["material"] = Enum.Material.Marble,
+        ["size"] = 4
+    },
+    ["p"] = {
+        ["name"] = "Carpet",
+        ["layout"] = 4,
+        ["t"] = "rbxassetid://3807526187",
+        ["material"] = Enum.Material.Fabric,
+        ["size"] = 4
+    },
+    ["s"] = {
+        ["name"] = "Wood 1",
+        ["layout"] = 1,
+        ["t"] = "rbxassetid://3807527417",
+        ["material"] = Enum.Material.WoodPlanks,
+        ["size"] = 4
+    },
+    ["r"] = {
+        ["name"] = "Wood 2",
+        ["layout"] = 2,
+        ["t"] = "rbxassetid://3807527054",
+        ["material"] = Enum.Material.Wood,
+        ["size"] = 4
+    },
+    ["z"] = {["layout"] = 0, ["t"] = "", ["name"] = "Plastic", ["size"] = 4}
+}
