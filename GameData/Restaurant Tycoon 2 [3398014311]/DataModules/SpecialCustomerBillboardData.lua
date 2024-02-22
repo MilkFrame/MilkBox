1072791cb0b9cc85af4c3ad700f37bf900112529
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:14:46 +1100

    Create SpecialCustomerBillboardData.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/DataModules/SpecialCustomerBillboardData.lua
@@ -0,0 +1,27 @@
+data = {
+    ["Celebrity"] = {
+        ["Text"] = "Celebrity",
+        ["StrokeColor3"] = Color3.fromRGB(25, 185, 38),
+        ["BackgroundColor3"] = Color3.fromRGB(40, 230, 56)
+    },
+    ["Birthday"] = {
+        ["Text"] = "Birthday",
+        ["StrokeColor3"] = Color3.fromRGB(167, 39, 47),
+        ["BackgroundColor3"] = Color3.fromRGB(240, 56, 66)
+    },
+    ["Influencer"] = {
+        ["Text"] = "Influencer",
+        ["StrokeColor3"] = Color3.fromRGB(167, 39, 47),
+        ["BackgroundColor3"] = Color3.fromRGB(240, 56, 66)
+    },
+    ["Critic"] = {
+        ["Text"] = "VIP Customer",
+        ["StrokeColor3"] = Color3.fromRGB(176, 138, 42),
+        ["BackgroundColor3"] = Color3.fromRGB(240, 182, 48)
+    },
+    ["President"] = {
+        ["Text"] = "President",
+        ["StrokeColor3"] = Color3.fromRGB(33, 33, 33),
+        ["BackgroundColor3"] = Color3.fromRGB(44, 44, 44)
+    }
+}
