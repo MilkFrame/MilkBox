533142220831fe5de36dc9fbedd4ad6f0eaf7737
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:29:41 +1100

    Create PackData.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/PackData.lua
@@ -0,0 +1,131 @@
+data = {
+    ["list"] = {
+        {
+            ["ForSale"] = true,
+            ["MusicRadioId"] = 20,
+            ["Discounted"] = true,
+            ["Price"] = 15,
+            ["BuildName"] = "Halloween",
+            ["DiscountText"] = "Event",
+            ["Name"] = "Halloween",
+            ["Description"] = "A spook-tacular pack containing furniture and music from the Halloween season!",
+            ["EnableBuildModeSubCategory"] = "Halloween",
+            ["Icon"] = "rbxassetid://639455634",
+            ["PackName"] = "HalloweenPack"
+        },
+        {
+            ["ForSale"] = true,
+            ["MusicRadioId"] = 10,
+            ["Price"] = 30,
+            ["BuildName"] = "Outdoor",
+            ["Name"] = "Outdoor",
+            ["Description"] = "Unlocks 14 exclusive furniture items, perfect for an outdoor restaurant!",
+            ["EnableBuildModeSubCategory"] = "Outdoor Pack",
+            ["Icon"] = "rbxassetid://88667513",
+            ["PackName"] = "OutdoorPack"
+        },
+        {
+            ["ForSale"] = false,
+            ["FoodCountryName"] = "Christmas",
+            ["MusicRadioId"] = 11,
+            ["Price"] = 20,
+            ["BuildName"] = "Christmas",
+            ["Name"] = "Christmas",
+            ["Description"] = "Bring some festive joy to your restaurant with exclusive furniture, food and more!",
+            ["EnableBuildModeSubCategory"] = "Christmas",
+            ["FoodSheet"] = 7,
+            ["Icon"] = "rbxassetid://583829772",
+            ["PackName"] = "ChristmasPack"
+        },
+        {
+            ["ForSale"] = true,
+            ["Description"] = "Contains a music pack and 20 furniture items to give your restaurant an oriental feel!",
+            ["Name"] = "Japanese",
+            ["EnableBuildModeSubCategory"] = "Japanese Pack",
+            ["Price"] = 16,
+            ["BuildName"] = "JapanPack",
+            ["Icon"] = "rbxassetid://10924020045",
+            ["MusicRadioId"] = 12
+        },
+        {
+            ["ForSale"] = true,
+            ["MusicRadioId"] = 18,
+            ["Price"] = 26,
+            ["BuildName"] = "MedievalPack",
+            ["Name"] = "Medieval",
+            ["Description"] = "Make your restaurant more historical with medieval music and 10 furniture items!",
+            ["EnableBuildModeSubCategory"] = "Medieval Pack",
+            ["Icon"] = "rbxassetid://7227135838",
+            ["PackName"] = "MedievalPack"
+        },
+        {
+            ["ForSale"] = true,
+            ["MusicRadioId"] = 19,
+            ["Price"] = 24,
+            ["BuildName"] = "WesternPack",
+            ["Name"] = "Western",
+            ["Description"] = "Unlocks 15 furniture items and a music pack to turn your restaurant into the wild west!",
+            ["EnableBuildModeSubCategory"] = "Western Pack",
+            ["Icon"] = "rbxassetid://7439358834",
+            ["PackName"] = "WesternPack"
+        },
+        {
+            ["ForSale"] = false,
+            ["Name"] = "Christmas (Rare)",
+            ["Description"] = "Add description here",
+            ["Price"] = 20,
+            ["EnableBuildModeSubCategory"] = "Christmas",
+            ["Icon"] = "rbxassetid://6468523528",
+            ["BuildName"] = "ChristmasPack2021"
+        },
+        {
+            ["ForSale"] = true,
+            ["Name"] = "Farm",
+            ["Description"] = "Contains 25 furniture items, great for building a farm-themed cafe!",
+            ["Price"] = 20,
+            ["EnableBuildModeSubCategory"] = "Farm Pack",
+            ["Icon"] = "rbxassetid://8467579037",
+            ["BuildName"] = "FarmPack"
+        },
+        {
+            ["ForSale"] = true,
+            ["Description"] = "Give your restaurant the feel of a tropical beach bar! Contains 16 furniture items.",
+            ["Discounted"] = false,
+            ["Price"] = 20,
+            ["BuildName"] = "BeachPack",
+            ["DiscountText"] = "Summer Event",
+            ["Name"] = "Beach",
+            ["Icon"] = "rbxassetid://10924015799",
+            ["EnableBuildModeSubCategory"] = "Beach Pack"
+        },
+        {
+            ["ForSale"] = true,
+            ["Name"] = "Vintage",
+            ["Description"] = "Unlocks 13 luxury and vintage items for giving your restaurant some glamour.",
+            ["Price"] = 15,
+            ["EnableBuildModeSubCategory"] = "Vintage Pack",
+            ["Icon"] = "rbxassetid://12870942739",
+            ["BuildName"] = "VintagePack"
+        },
+        {
+            ["ForSale"] = true,
+            ["Name"] = "Aquarium",
+            ["Description"] = "Unlocks glass aquarium kitchen items so you can cook with fishy companions!",
+            ["Price"] = 20,
+            ["EnableBuildModeSubCategory"] = "Aquarium Pack",
+            ["Icon"] = "rbxassetid://13626866117",
+            ["BuildName"] = "AquariumPack"
+        },
+        {
+            ["ForSale"] = true,
+            ["Description"] = "Step into the future and give your restaurant sci-fi flair. Contains 26 items.",
+            ["Discounted"] = false,
+            ["Price"] = 20,
+            ["BuildName"] = "SciFiPack",
+            ["DiscountText"] = "New!",
+            ["Name"] = "Sci-Fi",
+            ["Icon"] = "rbxassetid://14633686678",
+            ["EnableBuildModeSubCategory"] = "Sci-Fi Pack"
+        }
+    }
+}
