28e4cb086f0b78af636b31e1a8e5c36b7006970e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:17:09 +1100

    Create Levels.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/DataModules/Levels.lua
@@ -0,0 +1,17 @@
+data = {
+    ["XPTaskData"] = {
+        ["DishesCooked"] = {["displayName"] = "Dishes Cooked"},
+        ["MealsDelivered"] = {["displayName"] = "Meals Delivered"},
+        ["DishesCleaned"] = {["displayName"] = "Dishes Cleaned"},
+        ["CustomersSeated"] = {["displayName"] = "Customers Seated"},
+        ["DishesServed"] = {["displayName"] = "Dishes Served"},
+        ["DishesCollected"] = {["displayName"] = "Dishes Collected"},
+        ["OrdersTaken"] = {["displayName"] = "Orders Taken"}
+    },
+    ["DayRankData"] = {
+        ["Good"] = {["color"] = Color3.fromRGB(172, 172, 172), ["image"] = "rbxassetid://3883975940"},
+        ["Excellent"] = {["color"] = Color3.fromRGB(255, 220, 20), ["image"] = "rbxassetid://3437951747"},
+        ["Great"] = {["color"] = Color3.fromRGB(255, 220, 20), ["image"] = "rbxassetid://3883975940"},
+        ["Poor"] = {["color"] = Color3.fromRGB(202, 122, 42), ["image"] = "rbxassetid://3883975940"}
+    }
+}
