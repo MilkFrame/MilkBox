ec3b5ee2c2b24ee9345c6b64c696f3ec71f663ac
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:16:56 +1100

    Create Leaderboard.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/DataModules/Leaderboard.lua
@@ -0,0 +1,9 @@
+data = {
+    ["LeaderboardData"] = {
+        ["Likes"] = {["isCumulative"] = false, ["dataStoreName"] = "GlobalLikes"},
+        ["BombPopFurniture"] = {["isCumulative"] = false, ["dataStoreName"] = "GlobalBombPopFurnitureInRestaurant"},
+        ["BombPopsServed"] = {["isCumulative"] = true, ["dataStoreName"] = "GlobalBombPopsServed"},
+        ["Days"] = {["isCumulative"] = false, ["dataStoreName"] = "GlobalDays"},
+        ["XP"] = {["isCumulative"] = false, ["dataStoreName"] = "GlobalXP"}
+    }
+}
