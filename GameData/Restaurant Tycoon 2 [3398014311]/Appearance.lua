ab89ad0f5083fc214cc7ab246fd84122d04b5e4b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:23:37 +1100

    Create Appearance.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/Appearance.lua
@@ -0,0 +1,151 @@
+data = {
+    ["Front"] = {
+        {["Name"] = "None"},
+        {["Name"] = "Apron", ["Model"] = "Apron"},
+        {["Name"] = "Black Apron", ["Model"] = "BlackApron"},
+        {["Name"] = "Blue Fabric Apron", ["Model"] = "BlueFabricApron"},
+        {["Name"] = "Brown Apron", ["Model"] = "BrownApron"},
+        {["Name"] = "Gold Apron", ["Model"] = "GoldApron"},
+        {["Name"] = "Gray Apron", ["Model"] = "GrayApron"},
+        {["Name"] = "Red Fabric Apron", ["Model"] = "RedFabricApron"}
+    },
+    ["Skin"] = {
+        {["Name"] = "Face 1", ["Decal"] = "rbxassetid://28999175"},
+        {["Name"] = "Face 2", ["Decal"] = "rbxassetid://209713384"},
+        {["Name"] = "Face 3", ["Decal"] = "rbxassetid://416829065"},
+        {["Name"] = "Face 4", ["Decal"] = "rbxassetid://226216895"},
+        {["Name"] = "Face 5", ["Decal"] = "rbxassetid://280987381"},
+        {["Name"] = "Face 6", ["Decal"] = "rbxassetid://26018945"},
+        {["Name"] = "Face 7", ["Decal"] = "rbxassetid://494290547"},
+        {["Name"] = "Face 8", ["Decal"] = "rbxassetid://31117192"},
+        {["Name"] = "Face 9", ["Decal"] = "rbxassetid://7074856"},
+        {["Name"] = "Face 10", ["Decal"] = "rbxassetid://7505989"},
+        {["Name"] = "Face 11", ["Decal"] = "rbxassetid://28878210"},
+        {["Name"] = "Face 12", ["Decal"] = "rbxassetid://22626010"},
+        {["Name"] = "Face 13", ["Decal"] = "rbxassetid://158044781"},
+        {["Name"] = "Face 14", ["Decal"] = "rbxassetid://735617084"},
+        {["Name"] = "Face 15", ["Decal"] = "rbxassetid://2939916389"},
+        {["Name"] = "Face 16", ["Decal"] = "rbxassetid://642467680"},
+        {["Name"] = "Face 17", ["Decal"] = "rbxassetid://710945165"},
+        {["Name"] = "Face 18", ["Decal"] = "rbxassetid://1911639035"},
+        {["Name"] = "Face 20", ["Decal"] = "rbxassetid://111183124"}
+    },
+    ["Hats"] = {
+        {["Name"] = "None"},
+        {["Name"] = "Chef Hat", ["Model"] = "ChefHat"},
+        {["Name"] = "Flat Hat", ["Model"] = "ChefAccessory"},
+        {["Name"] = "Pastry Hat", ["Model"] = "LilChef"},
+        {["Name"] = "All Star Hat", ["Model"] = "AllStarChef"},
+        {["Name"] = "Black Hat", ["Model"] = "Black Chef Hat"},
+        {["Name"] = "Pink Baker Hat", ["Model"] = "Pink Baker Hat"},
+        {["Name"] = "Strawberry Hat", ["Model"] = "Strawberry Chef"},
+        {["Name"] = "Sushi Hat", ["Model"] = "Sushi Chef Hat"},
+        {["Name"] = "Yellow Hat", ["Model"] = "Yellow Chef Hat"}
+    },
+    ["Face"] = {
+        {["Name"] = "Face 1", ["Decal"] = "rbxassetid://28999175"},
+        {["Name"] = "Face 2", ["Decal"] = "rbxassetid://209713384"},
+        {["Name"] = "Face 3", ["Decal"] = "rbxassetid://416829065"},
+        {["Name"] = "Face 4", ["Decal"] = "rbxassetid://226216895"},
+        {["Name"] = "Face 5", ["Decal"] = "rbxassetid://280987381"},
+        {["Name"] = "Face 6", ["Decal"] = "rbxassetid://26018945"},
+        {["Name"] = "Face 7", ["Decal"] = "rbxassetid://494290547"},
+        {["Name"] = "Face 8", ["Decal"] = "rbxassetid://31117192"},
+        {["Name"] = "Face 9", ["Decal"] = "rbxassetid://7074856"},
+        {["Name"] = "Face 10", ["Decal"] = "rbxassetid://7505989"},
+        {["Name"] = "Face 11", ["Decal"] = "rbxassetid://28878210"},
+        {["Name"] = "Face 12", ["Decal"] = "rbxassetid://22626010"},
+        {["Name"] = "Face 13", ["Decal"] = "rbxassetid://158044781"},
+        {["Name"] = "Face 14", ["Decal"] = "rbxassetid://735617084"},
+        {["Name"] = "Face 15", ["Decal"] = "rbxassetid://2939916389"},
+        {["Name"] = "Face 16", ["Decal"] = "rbxassetid://642467680"},
+        {["Name"] = "Face 17", ["Decal"] = "rbxassetid://710945165"},
+        {["Name"] = "Face 18", ["Decal"] = "rbxassetid://1911639035"},
+        {["Name"] = "Face 20", ["Decal"] = "rbxassetid://111183124"}
+    },
+    ["Clothing"] = {
+        {
+            ["NoColorChange"] = "http://www.roblox.com/asset/?id=4626332593",
+            ["Name"] = "Clothing 1",
+            ["ColorChange"] = "http://www.roblox.com/asset/?id=4626332718"
+        },
+        {
+            ["NoColorChange"] = "http://www.roblox.com/asset/?id=4626321151",
+            ["Name"] = "Clothing 2",
+            ["ColorChange"] = "http://www.roblox.com/asset/?id=4626321283"
+        },
+        {
+            ["NoColorChange"] = "http://www.roblox.com/asset/?id=4626347111",
+            ["Name"] = "Clothing 3",
+            ["ColorChange"] = "http://www.roblox.com/asset/?id=4626347212"
+        },
+        {
+            ["NoColorChange"] = "http://www.roblox.com/asset/?id=4626748897",
+            ["Name"] = "Clothing 4",
+            ["ColorChange"] = "http://www.roblox.com/asset/?id=4626749071"
+        },
+        {
+            ["NoColorChange"] = "http://www.roblox.com/asset/?id=4626765935",
+            ["Name"] = "Clothing 5",
+            ["ColorChange"] = "http://www.roblox.com/asset/?id=4626766117"
+        },
+        {
+            ["NoColorChange"] = "http://www.roblox.com/asset/?id=4799008558",
+            ["Name"] = "Clothing 6",
+            ["ColorChange"] = "http://www.roblox.com/asset/?id=4799008396"
+        },
+        {
+            ["NoColorChange"] = "http://www.roblox.com/asset/?id=4799031516",
+            ["Name"] = "Clothing 7",
+            ["ColorChange"] = "http://www.roblox.com/asset/?id=4799031342"
+        },
+        {
+            ["NoColorChange"] = "http://www.roblox.com/asset/?id=4799531146",
+            ["Name"] = "Clothing 8",
+            ["ColorChange"] = "http://www.roblox.com/asset/?id=4799082846"
+        },
+        {
+            ["NoColorChange"] = "http://www.roblox.com/asset/?id=4799511258",
+            ["Name"] = "Clothing 9",
+            ["ColorChange"] = "http://www.roblox.com/asset/?id=4799511078"
+        },
+        {
+            ["NoColorChange"] = "http://www.roblox.com/asset/?id=4817053936",
+            ["Name"] = "Clothing 10",
+            ["ColorChange"] = "http://www.roblox.com/asset/?id=4817105106"
+        }
+    },
+    ["Hair"] = {
+        {["Name"] = "No hair"},
+        {["Male"] = false, ["Name"] = "Long brown", ["Model"] = "Americasweetheart"},
+        {["Male"] = false, ["Name"] = "Long blond", ["Model"] = "GirlAnimeHair_Blonde"},
+        {["Male"] = false, ["Name"] = "Amber bangs", ["Model"] = "LongHair"},
+        {["Male"] = false, ["Name"] = "Straight red", ["Model"] = "LongHairHeadBand"},
+        {["Male"] = false, ["Name"] = "Straight blond", ["Model"] = "LongHairHeadBandBlonde"},
+        {["Male"] = false, ["Name"] = "Straight black", ["Model"] = "LongHairHeadBandBlack"},
+        {["Male"] = false, ["Name"] = "Straight brown", ["Model"] = "LongHairHeadBandBrown"},
+        {["Male"] = true, ["Name"] = "Messy", ["Model"] = "MessyHair"},
+        {["Male"] = true, ["Name"] = "Neat black", ["Model"] = "BroodingBlack"},
+        {["Male"] = true, ["Name"] = "Pointy black", ["Model"] = "BoyNerdHair"},
+        {["Male"] = true, ["Name"] = "Short brown", ["Model"] = "NerdHair"},
+        {["Male"] = true, ["Name"] = "Curly blond", ["Model"] = "Kent"},
+        {["Male"] = false, ["Name"] = "Blond bun", ["Model"] = "HairAda"},
+        {["Male"] = false, ["Name"] = "Pink ponytail", ["Model"] = "HairAnnie"},
+        {["Male"] = true, ["Name"] = "Gelled brown", ["Model"] = "HairArthur"},
+        {["Male"] = true, ["Name"] = "Slick brown", ["Model"] = "HairBarney"},
+        {["Male"] = false, ["Name"] = "Brown bun", ["Model"] = "HairBill"},
+        {["Male"] = false, ["Name"] = "Violet headband", ["Model"] = "HairCassie"},
+        {["Male"] = true, ["Name"] = "Red glasses", ["Model"] = "HairChad"},
+        {["Male"] = false, ["Name"] = "Tidy auburn", ["Model"] = "HairCheryl"},
+        {["Male"] = false, ["Name"] = "Green pigtails", ["Model"] = "HairClove"},
+        {["Male"] = true, ["Name"] = "Sideswept blue", ["Model"] = "HairCora"},
+        {["Male"] = false, ["Name"] = "Large black", ["Model"] = "HairDaisy"},
+        {["Male"] = true, ["Name"] = "Pointed brown", ["Model"] = "HairDerek"},
+        {["Male"] = true, ["Name"] = "Gelled blond", ["Model"] = "HairDrake"},
+        {["Male"] = true, ["Name"] = "Fancy brown", ["Model"] = "HairDwight"},
+        {["Male"] = true, ["Name"] = "Messy red", ["Model"] = "HairHenry"},
+        {["Male"] = true, ["Name"] = "Bacon", ["Model"] = "HairHoward"},
+        {["Male"] = true, ["Name"] = "Spiky purple", ["Model"] = "HairJacob"},
+        {["Male"] = true, ["Name"] = "Spiky brown", ["Model"] = "HairJaden"}
+    }
+}
