e0497297db91e69bfe8bda7c76b55da205869d3b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:41:41 +1100

    Create Upgrades.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/Upgrades.lua
@@ -0,0 +1,208 @@
+data = {
+    ["a"] = {
+        ["offset"] = "0, 0",
+        ["description"] = "A bigger plot with much more space to decorate!",
+        ["price"] = 600,
+        ["img"] = "rbxassetid://3539217379",
+        ["robuxPrice"] = 120,
+        ["name"] = "Expanded Plot",
+        ["dependency"] = "",
+        ["productId"] = 1340091606,
+        ["enabled"] = true
+    },
+    ["b"] = {
+        ["offset"] = "0, 0",
+        ["description"] = "Even more space to expand your restaurant into.",
+        ["price"] = 1200,
+        ["img"] = "rbxassetid://3539217379",
+        ["robuxPrice"] = 240,
+        ["name"] = "Expanded Plot 2",
+        ["dependency"] = "a",
+        ["productId"] = 1340092464,
+        ["enabled"] = true
+    },
+    ["e"] = {
+        ["offset"] = "3, 0",
+        ["name"] = "Second Floor",
+        ["dependency"] = "b",
+        ["price"] = 2500,
+        ["img"] = "rbxassetid://3539217379",
+        ["enabled"] = false,
+        ["description"] = "It's time to take your restaurant to another level!"
+    },
+    ["g"] = {
+        ["offset"] = "1, 1",
+        ["name"] = "Parties",
+        ["dependency"] = "",
+        ["price"] = 200,
+        ["img"] = "rbxassetid://3539217379",
+        ["enabled"] = false,
+        ["description"] = "Host birthday parties and earn money by serving cake."
+    },
+    ["f"] = {
+        ["offset"] = "0, 1",
+        ["description"] = "Allows you to carry multiple plates at once.",
+        ["price"] = 100,
+        ["img"] = "rbxassetid://3539217379",
+        ["robuxPrice"] = 20,
+        ["name"] = "Waiter Trays",
+        ["dependency"] = "",
+        ["productId"] = 1340092700,
+        ["enabled"] = true
+    },
+    ["i"] = {
+        ["offset"] = "3, 1",
+        ["description"] = "50% of customers will make payments automatically, so you don't need to pick up the cash.",
+        ["price"] = 600,
+        ["img"] = "rbxassetid://3539217379",
+        ["robuxPrice"] = 120,
+        ["name"] = "Card Payments",
+        ["dependency"] = "h",
+        ["productId"] = 1340092996,
+        ["enabled"] = true
+    },
+    ["h"] = {
+        ["offset"] = "2, 1",
+        ["description"] = "Unlocks the tip jar item in build mode, giving you some spare change.",
+        ["price"] = 250,
+        ["img"] = "rbxassetid://3539217379",
+        ["robuxPrice"] = 50,
+        ["name"] = "Tip Jars",
+        ["dependency"] = "",
+        ["productId"] = 1340092851,
+        ["enabled"] = true
+    },
+    ["k"] = {
+        ["offset"] = "0, 2",
+        ["description"] = "Creates some large advertisements for your restaurant.",
+        ["price"] = 1200,
+        ["img"] = "rbxassetid://3539217379",
+        ["robuxPrice"] = 240,
+        ["name"] = "Advanced Advertising",
+        ["dependency"] = "j",
+        ["productId"] = 1340093351,
+        ["enabled"] = true
+    },
+    ["j"] = {
+        ["offset"] = "0, 2",
+        ["description"] = "Creates some small advertisements and increases your customer levels.",
+        ["price"] = 800,
+        ["img"] = "rbxassetid://3539217379",
+        ["robuxPrice"] = 160,
+        ["name"] = "Basic Advertising",
+        ["dependency"] = "",
+        ["productId"] = 1340093183,
+        ["enabled"] = true
+    },
+    ["m"] = {
+        ["offset"] = "0, 0",
+        ["description"] = "Even more space to expand your restaurant into.",
+        ["price"] = 1600,
+        ["img"] = "rbxassetid://3539217379",
+        ["robuxPrice"] = 320,
+        ["name"] = "Expanded Plot 3",
+        ["dependency"] = "b",
+        ["productId"] = 1340093509,
+        ["enabled"] = true
+    },
+    ["l"] = {
+        ["offset"] = "2, 2",
+        ["name"] = "Children's Menus",
+        ["dependency"] = "",
+        ["price"] = 400,
+        ["img"] = "rbxassetid://3539217379",
+        ["enabled"] = false,
+        ["description"] = "Families will enter your restaurant."
+    },
+    ["o"] = {
+        ["offset"] = "2, 0",
+        ["description"] = "Adds 4 blocks of space outdoors at the back of your restaurant.",
+        ["price"] = 2000,
+        ["img"] = "rbxassetid://3539217379",
+        ["robuxPrice"] = 400,
+        ["name"] = "Outdoor Expansion",
+        ["dependency"] = "m",
+        ["productId"] = 1340093785,
+        ["enabled"] = true
+    },
+    ["n"] = {
+        ["offset"] = "3, 0",
+        ["description"] = "Take your restaurant to another level with this upgrade.",
+        ["price"] = 8000,
+        ["img"] = "rbxassetid://3539217379",
+        ["robuxPrice"] = 1600,
+        ["name"] = "Second Floor",
+        ["dependency"] = "m",
+        ["productId"] = 1340093680,
+        ["enabled"] = true
+    },
+    ["q"] = {
+        ["offset"] = "0, 0",
+        ["description"] = "Deliver meals across the map!",
+        ["price"] = 3500,
+        ["img"] = "rbxassetid://5218022711",
+        ["robuxPrice"] = 700,
+        ["name"] = "Delivery",
+        ["dependency"] = "m",
+        ["upgradeEditData"] = {["editType"] = "Toggle"},
+        ["productId"] = 1340094075,
+        ["enabled"] = true
+    },
+    ["p"] = {
+        ["offset"] = "0, 0",
+        ["description"] = "Creates a road around your restaurant, allowing customers to order from their car!",
+        ["price"] = 3200,
+        ["img"] = "rbxassetid://3611806544",
+        ["robuxPrice"] = 640,
+        ["name"] = "Drive Thru",
+        ["dependency"] = "m",
+        ["productId"] = 1340093975,
+        ["upgradeEditData"] = {["editType"] = "List", ["data"] = {"Large", "Small", "Off"}},
+        ["enabled"] = true,
+        ["size"] = "512, 512"
+    },
+    ["s"] = {
+        ["offset"] = "0, 0",
+        ["description"] = "A food truck to drive around the map and serve customers from.",
+        ["price"] = 2500,
+        ["img"] = "rbxassetid://7897089600",
+        ["robuxPrice"] = 500,
+        ["name"] = "Food Truck",
+        ["dependency"] = "j",
+        ["productId"] = 1340094373,
+        ["enabled"] = true
+    },
+    ["r"] = {
+        ["offset"] = "0, 0",
+        ["description"] = "Even more space to expand your restaurant into.",
+        ["price"] = 16000,
+        ["img"] = "rbxassetid://3539217379",
+        ["robuxPrice"] = 3200,
+        ["name"] = "MEGA Plot",
+        ["dependency"] = "o",
+        ["productId"] = 1340094242,
+        ["enabled"] = true
+    },
+    ["u"] = {
+        ["offset"] = "3, 2",
+        ["description"] = "Customers will start leaving reviews on the rating board outside the restaurant!",
+        ["price"] = 2000,
+        ["img"] = "rbxassetid://3539217379",
+        ["robuxPrice"] = 400,
+        ["name"] = "Restaurant Reviews",
+        ["dependency"] = "k",
+        ["productId"] = 1569217309,
+        ["enabled"] = true
+    },
+    ["t"] = {
+        ["offset"] = "1, 2",
+        ["description"] = "A stack of baby chairs, so customers can bring along their babies!",
+        ["price"] = 600,
+        ["img"] = "rbxassetid://3539217379",
+        ["robuxPrice"] = 120,
+        ["name"] = "Baby Chairs",
+        ["dependency"] = "i",
+        ["productId"] = 1378296702,
+        ["enabled"] = true
+    }
+}
