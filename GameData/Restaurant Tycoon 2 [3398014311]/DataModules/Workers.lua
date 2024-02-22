a18f5db1d897fa7dd0f5078d01240f99ae938dcd
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:19:57 +1100

    Create Workers.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/DataModules/Workers.lua
@@ -0,0 +1,20 @@
+data = {
+    ["DefaultHair"] = {["Operator"] = 10, ["Driver"] = 28, ["Chef"] = 9, ["Waiter"] = 9},
+    ["WorkerIcons"] = {
+        ["Driver"] = "rbxassetid://8350881375",
+        ["Waiter"] = "rbxassetid://8347642618",
+        ["Manager"] = "rbxassetid://8350881283",
+        ["Chef"] = "rbxassetid://8351294931",
+        ["Operator"] = "rbxassetid://8350881375"
+    },
+    ["WorkerSlotPrices"] = {
+        ["Operator"] = {500, 700},
+        ["Driver"] = {500},
+        ["Chef"] = {200, 500, 1000, 1500, 2000, 2500, 3000, 1200, 1200, 1200, 1200, 1200},
+        ["Waiter"] = {200, 500, 1000, 1500, 2000, 2500, 3000, 1200, 1200, 1200, 1200, 1200}
+    },
+    ["MaximumWorkerAmount"] = {
+        ["standard"] = {["Operator"] = 2, ["Driver"] = 1, ["Chef"] = 7, ["Waiter"] = 7},
+        ["gamepass"] = {["Operator"] = 2, ["Driver"] = 1, ["Chef"] = 12, ["Waiter"] = 12}
+    }
+}
