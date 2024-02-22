a91c6a6385a0ce83d5888ffd787b3ebfa912ca66
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:14:00 +1100

    Create Credits.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/DataModules/Credits.lua
@@ -0,0 +1,12 @@
+data = {
+    ["CreditData"] = {
+        ["Ultraw"] = {["layoutOrder"] = 1, ["userId"] = 13042801, ["description"] = "@UltrawRbx-Lead Developer"}
+        ["Zynkuru"] = {["layoutOrder"] = 2, ["userId"] = 88324279, ["description"] = "@ZynkuruRBLX-Lead Builder"},
+        ["Duck_Ify"] = {["layoutOrder"] = 3, ["userId"] = 52880156, ["description"] = "@IfyDuck-Model Developer, etc."},
+        ["EGOTISMS"] = {["layoutOrder"] = 4, ["userId"] = 45907101, ["description"] = "@EGOTISMS_RBLX-UI Design"},
+        ["Chloroplast"] = {["layoutOrder"] = 5, ["userId"] = 96330968, ["description"] = "Customers"},
+        ["DerpyMcDerpell"] = { ["layoutOrder"] = 6, ["userId"] = 47232097, ["description"] = "@DerpyMcDerpell-3D Models, etc."},
+        ["BosOfRoblox"] = {["layoutOrder"] = 7, ["userId"] = 65888899, ["description"] = "@BosOfRoblox-UI Design"},
+        ["AtomixKing"] = {["layoutOrder"] = 8, ["userId"] = 23838736, ["description"] = "@Atomix_King-3D Models"},
+    }
+}
