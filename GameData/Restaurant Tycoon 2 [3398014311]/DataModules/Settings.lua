fcad62002da94e195b58875c3ab49f748598e05c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:18:52 +1100

    Create Settings.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/DataModules/Settings.lua
@@ -0,0 +1,40 @@
+data = {
+    ["ToggleSettingsData"] = {
+        ["DayCycle"] = {["layoutOrder"] = 11, ["text"] = "Day Cycle"},
+        ["DetailedFood"] = {["layoutOrder"] = 12, ["text"] = "Detailed Food", ["debounce"] = 1},
+        ["MapDecorations"] = {["layoutOrder"] = 13, ["text"] = "Map Decorations"},
+        ["SeatButtons"] = {
+            ["layoutOrder"] = 5,
+            ["subtitle"] = "Sit on seats in your restaurant",
+            ["offByDefault"] = true,
+            ["debounce"] = 1,
+            ["text"] = "Seat buttons"
+        },
+        ["AutoNavigate"] = {
+            ["layoutOrder"] = 2,
+            ["text"] = "Auto-Navigate",
+            ["offByDefault"] = true,
+            ["subtitle"] = "Automatically navigate to customers"
+        },
+        ["NPCAnimations"] = {["layoutOrder"] = 14, ["text"] = "NPC Animations"},
+        ["TwentyFourHourClock"] = {["layoutOrder"] = 1, ["text"] = "12-Hour Clock"},
+        ["RealisticNPCs"] = {["layoutOrder"] = 15, ["text"] = "Realistic NPCs", ["debounce"] = 1},
+        ["Waypoints"] = {
+            ["layoutOrder"] = 4,
+            ["text"] = "Outdoor Waypoints",
+            ["subtitle"] = "Waypoints for Home and Restaurant"
+        },
+        ["Shadows"] = {["layoutOrder"] = 16, ["text"] = "Shadows", ["debounce"] = 1},
+        ["WorkerLabels"] = {["layoutOrder"] = 17, ["text"] = "Worker Labels"},
+        ["AutoCharacterSize"] = {
+            ["layoutOrder"] = 3,
+            ["text"] = "Auto character size",
+            ["subtitle"] = "Automatically fix character size when cooking"
+        }
+    },
+    ["VolumeSettingsData"] = {
+        ["SoundEffectsVolume"] = {["layoutOrder"] = -7, ["text"] = "Sound Effects Volume"},
+        ["OutdoorMusicVolume"] = {["layoutOrder"] = -9, ["text"] = "Outdoor Music Volume"},
+        ["IndoorMusicVolume"] = {["layoutOrder"] = -8, ["text"] = "Indoor Music Volume"}
+    }
+}
