3fe712af31835fbe504bd63da0bf2a2261ffc47a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:16:25 +1100

    Create Homes.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/DataModules/Homes.lua
@@ -0,0 +1,54 @@
+data = {
+    ["HomeData"] = {
+        ["LargeHome"] = {
+            ["numRooms"] = 3,
+            ["price"] = 1800,
+            ["displayName"] = "Large Home",
+            ["homePropsVisible"] = false,
+            ["defaultSettings"] = {
+                ["RowRoofMaterial"] = "f",
+                ["RowWallpaper"] = "U",
+                ["RowFloorMaterial"] = "d",
+                ["RowFloor"] = "a",
+                ["RowRoof"] = "o"
+            },
+            ["saveFileExtension"] = "b",
+            ["ownedByDefault"] = false,
+            ["interiorName"] = "LargeHome",
+            ["exteriorName"] = "LargeHouse"
+        },
+        ["BasicHouse"] = {
+            ["homePropsVisible"] = false,
+            ["interiorName"] = "BasicHouse",
+            ["numRooms"] = 1,
+            ["defaultSettings"] = {
+                ["RowRoofMaterial"] = "f",
+                ["RowWallpaper"] = "U",
+                ["RowFloorMaterial"] = "d",
+                ["RowFloor"] = "a",
+                ["RowRoof"] = "o"
+            },
+            ["saveFileExtension"] = "",
+            ["ownedByDefault"] = true,
+            ["exteriorName"] = "BasicHouse",
+            ["displayName"] = "Basic Home"
+        },
+        ["MediumHome"] = {
+            ["numRooms"] = 2,
+            ["price"] = 1000,
+            ["displayName"] = "Medium Home",
+            ["homePropsVisible"] = false,
+            ["defaultSettings"] = {
+                ["RowRoofMaterial"] = "f",
+                ["RowWallpaper"] = "U",
+                ["RowFloorMaterial"] = "d",
+                ["RowFloor"] = "a",
+                ["RowRoof"] = "o"
+            },
+            ["saveFileExtension"] = "a",
+            ["ownedByDefault"] = false,
+            ["interiorName"] = "MediumHome",
+            ["exteriorName"] = "MediumHouse"
+        }
+    }
+}
