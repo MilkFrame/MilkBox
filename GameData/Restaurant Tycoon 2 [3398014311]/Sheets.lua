7407ac557ed518c04f726f5ac1fbc345f1178d92
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-22 17:34:11 +1100

    Create Sheets.lua

---
+++ GameData/Restaurant Tycoon 2 [3398014311]/Sheets.lua
@@ -0,0 +1,11 @@
+data = {
+    "rbxassetid://3174618126",
+    "rbxassetid://3302120261",
+    "rbxassetid://3591551120",
+    "rbxassetid://3656129665",
+    "rbxassetid://14983915999",
+    "rbxassetid://13341737389",
+    "rbxassetid://4539408008",
+    "rbxassetid://12731359694",
+    "rbxassetid://13341740520"
+}
