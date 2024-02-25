df299bad57c72757469260f5c260de72af9a35c9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-25 17:02:32 +1100

    Create CatalogInformation.lua

---
+++ GameData/Club Stellar [7890640623]/CatalogInformation.lua
@@ -0,0 +1,229 @@
+data = {
+    ["BlacklistedAccessories"] = {1055299},
+    ["LayeredClothing"] = {
+        Enum.AccessoryType.DressSkirt,
+        Enum.AccessoryType.Shirt,
+        Enum.AccessoryType.TShirt,
+        Enum.AccessoryType.Shorts,
+        Enum.AccessoryType.Sweater,
+        Enum.AccessoryType.Pants,
+        Enum.AccessoryType.Jacket,
+        Enum.AccessoryType.RightShoe,
+        Enum.AccessoryType.LeftShoe
+    },
+    ["AllowedAssetTypes"] = {2, 8, 11, 12, 18, 41, 42, 43, 44, 45, 46, 47, 58, 64, 65, 66, 67, 68, 69, 70, 71, 72},
+    ["SingleAccessoryTypes"] = {"Shirt", "Pants", "GraphicTShirt", "Face"},
+    ["AssetTypes"] = {
+        {"Pants", false},
+        {"FaceAccessory", true},
+        {"Shirt", false},
+        {"Face", false},
+        {"HatAccessory", true},
+        {"NeckAccessory", true},
+        {"FaceAccessory", true},
+        {"WaistAccessory", true},
+        {"FrontAccessory", true},
+        {"HatAccessory", true},
+        {"BackAccessory", true},
+        {"HairAccessory", true},
+        {"ShouldersAccessory", true}
+    },
+    ["CustomAccessories"] = {
+        ["Blackpinkvalk"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://7958032637"
+        },
+        ["stixxntricks"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238777836"},
+        ["Tired Pink"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://6676201546"},
+        ["Orange Glowing Eyes"] = {
+            ["Type"] = "Face",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://10468502196"
+        },
+        ["Knight of the Federation"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://9986877684"
+        },
+        ["Grace"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://6676206467"},
+        ["Orangevalk"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://10468765911"
+        },
+        ["Werewolves Flame"] = {
+            ["Type"] = "Face",
+            ["Creator"] = "@PurplePotatOwO",
+            ["Image"] = "rbxassetid://7583431316"
+        },
+        ["Green Invasion"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://6676200168"},
+        ["ItsLakiPlays"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238774215"},
+        ["Light Pink Sparkle Time Fedora"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://9986873386"
+        },
+        ["catpaws04"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238780152"},
+        ["BongoLad"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238771466"},
+        ["Jordan (RH Cheerleader)"] = {
+            ["Type"] = "Face",
+            ["Creator"] = "@PurplePotatOwO",
+            ["Image"] = "rbxassetid://7432301390"
+        },
+        ["Blue Glowing Eyes"] = {
+            ["Type"] = "Face",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://10468493314"
+        },
+        ["Princess of the Federation"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://9986875618"
+        },
+        ["Purple Horns of Pwnage"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://10468766914"
+        },
+        ["Octavia"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238776311"},
+        ["Blue Horns of Pwnage"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://10468764989"
+        },
+        ["Rainbow0bsessed"] = {
+            ["Type"] = "Face",
+            ["Creator"] = "@PurplePotatOwO",
+            ["Image"] = "rbxassetid://7238774908"
+        },
+        ["Jade"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://6676271783"},
+        ["Pinkwork Shades"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://9157875250"
+        },
+        ["veev520"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238779244"},
+        ["Queen of the Federation"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://10468766404"
+        },
+        ["Stacey"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://6676205862"},
+        ["Pinkwork Headphones"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://9157879072"
+        },
+        ["PerpetualStevo"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238774430"},
+        ["Pink Glowing Eyes"] = {
+            ["Type"] = "Face",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://10468460796"
+        },
+        ["Crimson Horns of Pwnage"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://9986866640"
+        },
+        ["Mugalo"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238927428"},
+        ["Purple Glowing Eyes"] = {
+            ["Type"] = "Face",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://10468474652"
+        },
+        ["Dan"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238777354"},
+        ["gemastone123456"] = {
+            ["Type"] = "Face",
+            ["Creator"] = "@PurplePotatOwO",
+            ["Image"] = "rbxassetid://7238774045"
+        },
+        ["Pink DaVinci Copter"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://10468766119"
+        },
+        ["Burning Spider"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7583432011"},
+        ["Surprised Rainbow"] = {
+            ["Type"] = "Face",
+            ["Creator"] = "@PurplePotatOwO",
+            ["Image"] = "rbxassetid://6676204268"
+        },
+        ["alfredobake"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238778554"},
+        ["Wildfire Vampire"] = {
+            ["Type"] = "Face",
+            ["Creator"] = "@PurplePotatOwO",
+            ["Image"] = "rbxassetid://7583433182"
+        },
+        ["Green Horns of Pwnage"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://10468765284"
+        },
+        ["Warm Spirit"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7583436475"},
+        ["Pink Dominus"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://7958375014"
+        },
+        ["cybernova"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238777011"},
+        ["BlackRedvalk"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://14687811454"
+        },
+        ["Sneaky Purple"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://6676279267"},
+        ["PurplePotatOwO"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238775233"},
+        ["Sad Blue"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://6676279776"},
+        ["Midnight Purple Sparkle Time Fedora"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://10468765639"
+        },
+        ["CyclopsArtss"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238770599"},
+        ["QueenChroma"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238779893"},
+        ["Sarah"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238775766"},
+        ["Piper (RH Cheerleader)"] = {
+            ["Type"] = "Face",
+            ["Creator"] = "@PurplePotatOwO",
+            ["Image"] = "rbxassetid://7432301087"
+        },
+        ["Angry Red"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://6676205040"},
+        ["Ophelia (RH Cheerleader)"] = {
+            ["Type"] = "Face",
+            ["Creator"] = "@PurplePotatOwO",
+            ["Image"] = "rbxassetid://7432300680"
+        },
+        ["Pinkvalk"] = {["Type"] = "Accessory", ["Creator"] = "Stellar Studios", ["Image"] = "rbxassetid://9157885471"},
+        ["MissValeriiee's Saphire Gaze"] = {
+            ["Type"] = "Face",
+            ["Creator"] = "@PurplePotatOwO",
+            ["Image"] = "rbxassetid://7238774602"
+        },
+        ["CosmosPhyral"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238771674"},
+        ["K4YTO"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238773716"},
+        ["LeahAshe"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238776592"},
+        ["Mermaid"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://6676212699"},
+        ["Inferno Ghost"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7583432554"},
+        ["Forest Pixie"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://6676257898"},
+        ["JamJamJoo"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238785416"},
+        ["Gemma"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://6676213281"},
+        ["AntiqueDestiny"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238770321"},
+        ["Purple DaVinci Copter"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://10468766680"
+        },
+        ["Pink Fiery Horns"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://9984673942"
+        },
+        ["Twinyurd"] = {["Type"] = "Face", ["Creator"] = "@PurplePotatOwO", ["Image"] = "rbxassetid://7238778967"},
+        ["Pink Void Star"] = {
+            ["Type"] = "Accessory",
+            ["Creator"] = "Stellar Studios",
+            ["Image"] = "rbxassetid://9157891468"
+        }
+    }
+}
