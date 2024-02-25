2629895391fef4c70574f798eec3cd2648e0e8ac
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-25 17:04:56 +1100

    Create DailyWheel.lua

---
+++ GameData/Club Stellar [7890640623]/DailyWheel.lua
@@ -0,0 +1,25 @@
+data = {
+    ["Items"] = {
+        {["Rarity"] = 0, ["Sound"] = "LevelRocket"},
+        {["Rarity"] = 1, ["Sound"] = "Ghost"},
+        {["Rarity"] = 2, ["Sound"] = "SuperRare"},
+        {["Rarity"] = 2, ["Sound"] = "Future"},
+        {["Rarity"] = 2, ["Sound"] = "Rare"},
+        {["Rarity"] = 2, ["Sound"] = "Ghost"},
+        {["Rarity"] = 1, ["Sound"] = "XPMultiplier"},
+        {["Rarity"] = 2, ["Sound"] = "Console"},
+        {["Rarity"] = 3, ["Sound"] = "Arcane"},
+        {["Rarity"] = 3, ["Sound"] = "SmallCoins"},
+        {["Rarity"] = 0, ["Sound"] = "LargeCoins"},
+        {["Rarity"] = 3, ["Sound"] = "Common"}
+    },
+    ["Spin"] = {
+        ["degreeOfFreedom"] = 14,
+        ["RarityDelay"] = {2, 6, 15},
+        ["secondsBetweenSpins"] = 21600,
+        ["WindupTime"] = 4,
+        ["fullRotations"] = 3,
+        ["rotationTime"] = {["min"] = 8, ["max"] = 8},
+        ["totalChoices"] = 12
+    }
+}
