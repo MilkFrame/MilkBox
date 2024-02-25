3e10e76b4f1ec7aefdb931c446295b90219ffedf
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-25 16:58:43 +1100

    Create DailyRewardsData.lua

---
+++ GameData/Club Stellar [7890640623]/DailyRewardsData.lua
@@ -0,0 +1,11 @@
+data = {
+    ["LoginStreaks"] = {
+        {["Reward"] = {500}, ["Type"] = "Coins"},
+        {["Reward"] = {650}, ["Type"] = "Coins"},
+        {["Reward"] = {800}, ["Type"] = "Coins"},
+        {["Reward"] = {1000}, ["Type"] = "Coins"},
+        {["Reward"] = {1150}, ["Type"] = "Coins"},
+        {["Reward"] = {1250}, ["Type"] = "Coins"},
+        {["Reward"] = {1500}, ["Type"] = "Coins"}
+    }
+}
