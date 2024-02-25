3433141b9306cf09d02ce5911583fd12584360dc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-25 16:59:37 +1100

    Create LeaderboardData.lua

---
+++ GameData/Club Stellar [7890640623]/LeaderboardData.lua
@@ -0,0 +1,24 @@
+data = {
+    ["RankColors"] = {
+        Color3.fromRGB(144, 65, 255),
+        Color3.fromRGB(155, 53, 175),
+        Color3.fromRGB(213, 93, 193),
+        Color3.fromRGB(0, 211, 204),
+        Color3.fromRGB(139, 225, 167),
+        Color3.fromRGB(150, 150, 150)
+    },
+    ["MannequinTags"] = {
+        ["Likes"] = "â˜… HIGHEST LIKES â˜…",
+        ["Levels"] = "â˜… HIGHEST LEVEL â˜…",
+        ["Donations"] = "â˜… TOP DONATOR â˜…"
+    },
+    ["SaveBlacklist"] = {73797397},
+    ["RefreshTime"] = 90,
+    ["LeaderboardTypes"] = {"Likes", "Donations", "Levels"},
+    ["PageSize"] = 100,
+    ["DataPatterns"] = {
+        ["Likes"] = "<b>%s%s</font></b> Likes",
+        ["Levels"] = "Level <b>%s%s</font></b>",
+        ["Donations"] = "<b>%sR$%s</font></b> Donated"
+    }
+}
