80cd1d51ec2df4824eb2fd6a68ec4a513f41cf36
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-25 17:04:37 +1100

    Create StellarGiftData.lua

---
+++ GameData/Club Stellar [7890640623]/StellarGiftData.lua
@@ -0,0 +1,131 @@
+data = {
+    ["PresetOrder"] = {"StellarRegularPurple"},
+    ["UniqueGiftReward"] = "Bouquet",
+    ["GiftRewards"] = {"Rose", "Cupid's Scepter", "Heartslayer"},
+    ["UniqueGiftThreshold"] = 20,
+    ["DupeCompensation"] = {0, 2000, 6500},
+    ["AppearancePresets"] = {
+        ["ValentinesFancyRed"] = {
+            ["MessageFont"] = "Font { Family = rbxasset://fonts/families/Nunito.json, Weight = Bold, Style = Normal }",
+            ["CardTitleText"] = "happy valentine's day",
+            ["InnerCardImage"] = "rbxassetid://12606059598",
+            ["CardPrimaryColor"] = Color3.fromRGB(255, 105, 125),
+            ["CardDropShadowColor"] = Color3.fromRGB(255, 73, 98),
+            ["CardSenderBorder"] = "rbxassetid://12462561860",
+            ["CardFontBold"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Bold, Style = Normal }",
+            ["TitleCardImage"] = "rbxassetid://12606059598",
+            ["CardFont"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Regular, Style = Normal }"
+        },
+        ["ValentinesBasicPink"] = {
+            ["MessageFont"] = "Font { Family = rbxasset://fonts/families/Nunito.json, Weight = Bold, Style = Normal }",
+            ["CardTitleText"] = "happy valentine's day",
+            ["InnerCardImage"] = "rbxassetid://12450355738",
+            ["CardPrimaryColor"] = Color3.fromRGB(255, 0, 127),
+            ["CardDropShadowColor"] = Color3.fromRGB(255, 67, 156),
+            ["CardSenderBorder"] = "rbxassetid://12462561860",
+            ["CardFontBold"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Bold, Style = Normal }",
+            ["TitleCardImage"] = "rbxassetid://12437046233",
+            ["CardFont"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Regular, Style = Normal }"
+        },
+        ["ValentinesFancyOrange"] = {
+            ["MessageFont"] = "Font { Family = rbxasset://fonts/families/Nunito.json, Weight = Bold, Style = Normal }",
+            ["CardTitleText"] = "happy valentine's day",
+            ["InnerCardImage"] = "rbxassetid://12606060484",
+            ["CardPrimaryColor"] = Color3.fromRGB(255, 199, 130),
+            ["CardDropShadowColor"] = Color3.fromRGB(255, 184, 95),
+            ["CardSenderBorder"] = "rbxassetid://12462561860",
+            ["CardFontBold"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Bold, Style = Normal }",
+            ["TitleCardImage"] = "rbxassetid://12606060484",
+            ["CardFont"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Regular, Style = Normal }"
+        },
+        ["ValentinesFancyPink"] = {
+            ["MessageFont"] = "Font { Family = rbxasset://fonts/families/Nunito.json, Weight = Bold, Style = Normal }",
+            ["CardTitleText"] = "happy valentine's day",
+            ["InnerCardImage"] = "rbxassetid://12606059827",
+            ["CardPrimaryColor"] = Color3.fromRGB(255, 55, 205),
+            ["CardDropShadowColor"] = Color3.fromRGB(255, 99, 215),
+            ["CardSenderBorder"] = "rbxassetid://12462561860",
+            ["CardFontBold"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Bold, Style = Normal }",
+            ["TitleCardImage"] = "rbxassetid://12606059827",
+            ["CardFont"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Regular, Style = Normal }"
+        },
+        ["StellarRegularPurple"] = {
+            ["MessageFont"] = "Font { Family = rbxasset://fonts/families/Nunito.json, Weight = Bold, Style = Normal }",
+            ["CardTitleText"] = "you're super stellar",
+            ["InnerCardImage"] = "rbxassetid://12606060769",
+            ["CardPrimaryColor"] = Color3.fromRGB(162, 115, 255),
+            ["CardDropShadowColor"] = Color3.fromRGB(150, 94, 255),
+            ["CardSenderBorder"] = "rbxassetid://12462561860",
+            ["CardFontBold"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Bold, Style = Normal }",
+            ["TitleCardImage"] = "rbxassetid://12606060769",
+            ["CardFont"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Regular, Style = Normal }"
+        },
+        ["EasterRegular"] = {
+            ["MessageFont"] = "Font { Family = rbxasset://fonts/families/Nunito.json, Weight = Bold, Style = Normal }",
+            ["CardTitleText"] = "happy easter",
+            ["InnerCardImage"] = "rbxassetid://13055914743",
+            ["CardPrimaryColor"] = Color3.fromRGB(231, 122, 250),
+            ["CardDropShadowColor"] = Color3.fromRGB(231, 122, 250),
+            ["CardSenderBorder"] = "rbxassetid://13055960633",
+            ["CardFontBold"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Bold, Style = Normal }",
+            ["TitleCardImage"] = "rbxassetid://13055914743",
+            ["CardFont"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Regular, Style = Normal }"
+        },
+        ["ValentinesFancyTurquoise"] = {
+            ["MessageFont"] = "Font { Family = rbxasset://fonts/families/Nunito.json, Weight = Bold, Style = Normal }",
+            ["CardTitleText"] = "happy valentine's day",
+            ["InnerCardImage"] = "rbxassetid://12606061226",
+            ["CardPrimaryColor"] = Color3.fromRGB(90, 255, 244),
+            ["CardDropShadowColor"] = Color3.fromRGB(41, 255, 242),
+            ["CardSenderBorder"] = "rbxassetid://12462561860",
+            ["CardFontBold"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Bold, Style = Normal }",
+            ["TitleCardImage"] = "rbxassetid://12606061226",
+            ["CardFont"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Regular, Style = Normal }"
+        },
+        ["ValentinesFancyBlue"] = {
+            ["MessageFont"] = "Font { Family = rbxasset://fonts/families/Nunito.json, Weight = Bold, Style = Normal }",
+            ["CardTitleText"] = "happy valentine's day",
+            ["InnerCardImage"] = "rbxassetid://12606060151",
+            ["CardPrimaryColor"] = Color3.fromRGB(115, 171, 255),
+            ["CardDropShadowColor"] = Color3.fromRGB(95, 158, 255),
+            ["CardSenderBorder"] = "rbxassetid://12462561860",
+            ["CardFontBold"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Bold, Style = Normal }",
+            ["TitleCardImage"] = "rbxassetid://12606060151",
+            ["CardFont"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Regular, Style = Normal }"
+        },
+        ["ValentinesFancyPurple"] = {
+            ["MessageFont"] = "Font { Family = rbxasset://fonts/families/Nunito.json, Weight = Bold, Style = Normal }",
+            ["CardTitleText"] = "happy valentine's day",
+            ["InnerCardImage"] = "rbxassetid://12606060769",
+            ["CardPrimaryColor"] = Color3.fromRGB(162, 115, 255),
+            ["CardDropShadowColor"] = Color3.fromRGB(150, 94, 255),
+            ["CardSenderBorder"] = "rbxassetid://12462561860",
+            ["CardFontBold"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Bold, Style = Normal }",
+            ["TitleCardImage"] = "rbxassetid://12606060769",
+            ["CardFont"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Regular, Style = Normal }"
+        },
+        ["ValentinesBasicBlue"] = {
+            ["MessageFont"] = "Font { Family = rbxasset://fonts/families/Nunito.json, Weight = Bold, Style = Normal }",
+            ["CardTitleText"] = "happy valentine's day",
+            ["InnerCardImage"] = "rbxassetid://12605953063",
+            ["CardPrimaryColor"] = Color3.fromRGB(30, 184, 255),
+            ["CardDropShadowColor"] = Color3.fromRGB(75, 183, 255),
+            ["CardSenderBorder"] = "rbxassetid://12462561860",
+            ["CardFontBold"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Bold, Style = Normal }",
+            ["TitleCardImage"] = "rbxassetid://12605941488",
+            ["CardFont"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Regular, Style = Normal }"
+        },
+        ["StPatricksBasic"] = {
+            ["MessageFont"] = "Font { Family = rbxasset://fonts/families/Nunito.json, Weight = Bold, Style = Normal }",
+            ["CardTitleText"] = "happy st. patrick's day",
+            ["InnerCardImage"] = "rbxassetid://12745467327",
+            ["CardPrimaryColor"] = Color3.fromRGB(0, 255, 106),
+            ["CardDropShadowColor"] = Color3.fromRGB(136, 255, 215),
+            ["CardSenderBorder"] = "rbxassetid://12745552278",
+            ["CardFontBold"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Bold, Style = Normal }",
+            ["TitleCardImage"] = "rbxassetid://12745467327",
+            ["CardFont"] = "Font { Family = rbxasset://fonts/families/IndieFlower.json, Weight = Regular, Style = Normal }"
+        }
+    },
+    ["DataTiers"] = {"PremiumGiftUnlocked", "BasicGiftUnlocked"}
+}
