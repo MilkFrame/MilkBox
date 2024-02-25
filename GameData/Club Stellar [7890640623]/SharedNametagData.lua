4ab2fe88e0401835f0fb8b024296991eec245942
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-25 17:04:12 +1100

    Create SharedNametagData.lua

---
+++ GameData/Club Stellar [7890640623]/SharedNametagData.lua
@@ -0,0 +1,17 @@
+data = {
+    ["MilestoneData"] = {
+        {["Threshold"] = 10000000, ["BoldLevel"] = true, ["Symbol"] = "â˜…"},
+        {["Threshold"] = 5000000, ["BoldLevel"] = true, ["Symbol"] = "â˜†"},
+        {["Threshold"] = 3000000, ["BoldLevel"] = true, ["Symbol"] = "â™¥"},
+        {["Threshold"] = 1000000, ["BoldLevel"] = true, ["Symbol"] = "â˜…"},
+        {["Threshold"] = 500000, ["BoldLevel"] = true, ["Symbol"] = "â™¢"},
+        {["Threshold"] = 300000, ["BoldLevel"] = true, ["Symbol"] = "â™¥"},
+        {["Threshold"] = 150000, ["BoldLevel"] = true, ["Symbol"] = "â˜…"},
+        {["Threshold"] = 75000, ["BoldLevel"] = true, ["Symbol"] = "â™¥"},
+        {["Threshold"] = 30000, ["BoldLevel"] = false, ["Symbol"] = "â™¡"},
+        {["Threshold"] = 15000, ["BoldLevel"] = false, ["Symbol"] = "â™¢"},
+        {["Threshold"] = 5000, ["BoldLevel"] = false, ["Symbol"] = ""},
+        {["Threshold"] = 0, ["BoldLevel"] = false, ["Symbol"] = ""}
+    },
+    ["GetDataForLevel"] = "function: 0x00000000ab32340b"
+}
