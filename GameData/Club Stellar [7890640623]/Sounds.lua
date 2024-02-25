d383eecbd5b17bf707c548cbe6779c069d968564
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-25 17:13:23 +1100

    Create Sounds.lua

---
+++ GameData/Club Stellar [7890640623]/Sounds.lua
@@ -0,0 +1,71 @@
+data = {
+    ["SpinWheel"] = {
+        ["Effects"] = {["LeverSound"] = "rbxassetid://14714160244"},
+        ["Crowd"] = {
+            ["CrowdT2"] = "rbxassetid://14688387992",
+            ["CrowdT3"] = "rbxassetid://14688386081",
+            ["CrowdT1"] = "rbxassetid://14688389893"
+        },
+        ["Ticks"] = {["DefaultTick"] = "rbxassetid://14688394392", ["TickAccent"] = "rbxassetid://14714159354"},
+        ["RewardSounds"] = {
+            ["LargeCoins"] = "rbxassetid://14688396962",
+            ["MediumCoins"] = "rbxassetid://14688396353",
+            ["XPMultiplier"] = "rbxassetid://14688397900",
+            ["Common"] = "rbxassetid://14877036794",
+            ["LevelRocket"] = "rbxassetid://14688399120",
+            ["Uncommon"] = "rbxassetid://14688392110",
+            ["SuperRare"] = "rbxassetid://14688395377",
+            ["Ghost"] = "rbxassetid://14410012825",
+            ["Arcane"] = "rbxassetid://14332990850",
+            ["Future"] = "rbxassetid://14679217446",
+            ["Console"] = "rbxassetid://9048375769",
+            ["SmallCoins"] = "rbxassetid://14417108619",
+            ["Rare"] = "rbxassetid://14688395004"
+        },
+        ["SlotRisers"] = {
+            ["star_b_01"] = "rbxassetid://14689677573",
+            ["star_a_01"] = "rbxassetid://14689677640",
+            ["star_a_04"] = "rbxassetid://14689677167",
+            ["star_b_04"] = "rbxassetid://14689677312",
+            ["star_b_02"] = "rbxassetid://14689677387",
+            ["star_b_03"] = "rbxassetid://14689677235",
+            ["star_a_03"] = "rbxassetid://14689677518",
+            ["star_a_02"] = "rbxassetid://14689677457"
+        }
+    },
+    ["Shop"] = {
+        ["Shop"] = {["Open01"] = "rbxassetid://14333403778", ["Close01"] = "rbxassetid://14333402909"},
+        ["Purchase"] = {
+            ["Purchase04"] = "rbxassetid://14173327886",
+            ["Purchase03"] = "rbxassetid://14173324714",
+            ["Purchase02"] = "rbxassetid://14173329719",
+            ["Purchase06"] = "rbxassetid://14332990850",
+            ["Purchase07"] = "rbxassetid://14417108619",
+            ["Purchase05"] = "rbxassetid://14333671844",
+            ["Purchase01"] = "rbxassetid://14173326835"
+        },
+        ["Category"] = {
+            ["Plushies"] = "rbxassetid://9125994778",
+            ["Food"] = "rbxassetid://14325633959",
+            ["Limited"] = "rbxassetid://4940109913",
+            ["Coins"] = "rbxassetid://14325642443",
+            ["Concert"] = "rbxassetid://14325640869",
+            ["Passes"] = "rbxassetid://14332991673",
+            ["DressUp"] = "rbxassetid://6636232274",
+            ["Wands"] = "rbxassetid://6691605753",
+            ["Weapons"] = "rbxassetid://14325637413",
+            ["Music"] = "rbxassetid://14325639138",
+            ["Props"] = "rbxassetid://14325635812"
+        },
+        ["Popup"] = {["TicketsInformation"] = "rbxassetid://14332160155"},
+        ["Click"] = {
+            ["Click04"] = "rbxassetid://14173322804",
+            ["Click02"] = "rbxassetid://14173322025",
+            ["Click03"] = "rbxassetid://14173323840",
+            ["Click01"] = "rbxassetid://14325646109",
+            ["Click05"] = "rbxassetid://14173320211"
+        },
+        ["Error"] = {["Error02"] = "rbxassetid://14173321118", ["Error01"] = "rbxassetid://14173318678"},
+        ["Coins"] = {["Coin01"] = "rbxassetid://14173314919", ["Coin02"] = "rbxassetid://14325644116"}
+    }
+}
