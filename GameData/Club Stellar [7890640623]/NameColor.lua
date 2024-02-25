d19a9c3493cba74791ca3569d2461eec098bea1d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-25 17:02:51 +1100

    Create NameColor.lua

---
+++ GameData/Club Stellar [7890640623]/NameColor.lua
@@ -0,0 +1,14 @@
+data = {
+    ["GetNameValue"] = "function: 0x0000000092563f4b",
+    ["GetNameColor"] = "function: 0x00000000f3598c13",
+    ["ColorMatrix"] = {
+        Color3.fromRGB(253, 41, 67),
+        Color3.fromRGB(1, 162, 255),
+        Color3.fromRGB(2, 184, 87),
+        Color3.fromRGB(107, 50, 124),
+        Color3.fromRGB(218, 133, 65),
+        Color3.fromRGB(245, 205, 48),
+        Color3.fromRGB(232, 186, 200),
+        Color3.fromRGB(215, 197, 154)
+    }
+}
