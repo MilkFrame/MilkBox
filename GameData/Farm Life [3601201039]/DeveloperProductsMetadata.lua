7912e30961cd9c579a7f3b79632ea97ab1723d7c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-27 22:47:59 +1100

    Create DeveloperProductsMetadata.lua

---
+++ GameData/Farm Life [3601201039]/DeveloperProductsMetadata.lua
@@ -0,0 +1,93 @@
+data = {
+    ["gold25000"] = {
+        ["buyPrice"] = 1800,
+        ["name"] = "25000 Gold",
+        ["productId"] = 665414836,
+        ["id"] = "gold25000",
+        ["devProductId"] = 656284507,
+        ["store"] = true,
+        ["textureId"] = "rbxassetid://3662127387"
+    },
+    ["gold250"] = {
+        ["order"] = 2,
+        ["buyPrice"] = 25,
+        ["name"] = "250 Gold",
+        ["productId"] = 665410459,
+        ["id"] = "gold250",
+        ["devProductId"] = 656189122,
+        ["store"] = true,
+        ["textureId"] = "rbxassetid://3662126969"
+    },
+    ["gold1000"] = {
+        ["buyPrice"] = 100,
+        ["name"] = "1000 Gold",
+        ["productId"] = 665412662,
+        ["id"] = "gold1000",
+        ["devProductId"] = 655924086,
+        ["store"] = true,
+        ["textureId"] = "rbxassetid://3662127055"
+    },
+    ["egg1"] = {
+        ["name"] = "Egg 1",
+        ["productId"] = 706699345,
+        ["id"] = "egg1",
+        ["devProductId"] = 706700621,
+        ["buyPrice"] = 75,
+        ["textureId"] = "rbxassetid://3764038593"
+    },
+    ["gold5000"] = {
+        ["description"] = "Best Seller",
+        ["buyPrice"] = 450,
+        ["name"] = "5000 Gold",
+        ["productId"] = 665413147,
+        ["id"] = "gold5000",
+        ["devProductId"] = 656283755,
+        ["store"] = true,
+        ["textureId"] = "rbxassetid://3662127143"
+    },
+    ["egg2"] = {
+        ["name"] = "Egg 2",
+        ["productId"] = 706699909,
+        ["id"] = "egg2",
+        ["devProductId"] = 706701000,
+        ["buyPrice"] = 175,
+        ["textureId"] = "rbxassetid://3764038393"
+    },
+    ["gold100000"] = {
+        ["buyPrice"] = 6000,
+        ["name"] = "100000 Gold",
+        ["productId"] = 665415278,
+        ["id"] = "gold100000",
+        ["devProductId"] = 656284731,
+        ["store"] = true,
+        ["textureId"] = "rbxassetid://3662127461"
+    },
+    ["rain"] = {
+        ["description"] = "Server Rain",
+        ["id"] = "rain",
+        ["textureId"] = "rbxassetid://3662055018",
+        ["name"] = "Rain Dance",
+        ["productId"] = 671597824,
+        ["order"] = 1,
+        ["devProductId"] = 667499592,
+        ["store"] = true,
+        ["buyPrice"] = 25
+    },
+    ["gold10000"] = {
+        ["buyPrice"] = 800,
+        ["name"] = "10000 Gold",
+        ["productId"] = 665413624,
+        ["id"] = "gold10000",
+        ["devProductId"] = 656284346,
+        ["store"] = true,
+        ["textureId"] = "rbxassetid://3662127214"
+    },
+    ["premiumFurniture"] = {
+        ["name"] = "Premium Furniture",
+        ["productId"] = 745115205,
+        ["id"] = "premiumFurniture",
+        ["devProductId"] = 745114518,
+        ["buyPrice"] = 399,
+        ["textureId"] = "rbxassetid://3878803594"
+    }
+}
