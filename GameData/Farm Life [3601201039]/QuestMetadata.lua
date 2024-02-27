3b666ec55bbba88856c84f9eb8203ea3edcb7c3c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-27 22:49:49 +1100

    Create QuestMetadata.lua

---
+++ GameData/Farm Life [3601201039]/QuestMetadata.lua
@@ -0,0 +1,35 @@
+data = {
+    ["fishing"] = {
+        ["fish2"] = {["id"] = "fish2", ["items"] = {["Red Crab"] = 6, ["Blue Crab"] = 3}, ["order"] = 2},
+        ["fish4"] = {["id"] = "fish4", ["items"] = {["Clam"] = 10, ["Squid"] = 10}, ["order"] = 5},
+        ["goldRod"] = {["id"] = "goldRod", ["items"] = {["Flatfish"] = 10, ["Clam"] = 7, ["Squid"] = 7}, ["order"] = 6},
+        ["fish3"] = {["id"] = "fish3", ["items"] = {["Red Snapper"] = 5, ["Blue Crab"] = 5}, ["order"] = 4},
+        ["fiberglassRod"] = {
+            ["id"] = "fiberglassRod",
+            ["items"] = {["Red Snapper"] = 5, ["Blue Crab"] = 2, ["Flatfish"] = 3},
+            ["order"] = 3
+        },
+        ["fish1"] = {["id"] = "fish1", ["items"] = {["Catfish"] = 7}, ["order"] = 1}
+    },
+    ["lumberjack"] = {
+        ["goldAxe"] = {["id"] = "goldAxe", ["items"] = {["Maple Wood"] = 200, ["Wood"] = 100}, ["order"] = 6},
+        ["ironAxe"] = {["id"] = "ironAxe", ["items"] = {["Wood"] = 200}, ["order"] = 3},
+        ["wood2"] = {["id"] = "wood2", ["items"] = {["Wood"] = 100}, ["order"] = 2},
+        ["wood6"] = {
+            ["id"] = "wood6",
+            ["items"] = {["Wood"] = 200, ["Maple Wood"] = 100, ["Aspen Wood"] = 150},
+            ["order"] = 8
+        },
+        ["wood8"] = {["id"] = "wood7", ["items"] = {["Enchanted Wood"] = 5000}, ["order"] = 11},
+        ["wood1"] = {["id"] = "wood1", ["items"] = {["Wood"] = 50}, ["order"] = 1},
+        ["diamondAxe"] = {
+            ["id"] = "diamondAxe",
+            ["items"] = {["Wood"] = 300, ["Maple Wood"] = 200, ["Aspen Wood"] = 200},
+            ["order"] = 9
+        },
+        ["wood5"] = {["id"] = "wood5", ["items"] = {["Aspen Wood"] = 100, ["Maple Wood"] = 50}, ["order"] = 7},
+        ["wood7"] = {["id"] = "wood7", ["items"] = {["Enchanted Wood"] = 1000}, ["order"] = 10},
+        ["wood4"] = {["id"] = "wood4", ["items"] = {["Maple Wood"] = 75, ["Wood"] = 100}, ["order"] = 5},
+        ["wood3"] = {["id"] = "wood3", ["items"] = {["Maple Wood"] = 100, ["Wood"] = 25}, ["order"] = 4}
+    }
+}
