9c2f568272b8cdededfde967ca850112678303ce
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-27 22:46:51 +1100

    Create TagMetadata.lua

---
+++ GameData/Farm Life [3601201039]/TagMetadata.lua
@@ -0,0 +1,18 @@
+data = {
+    ["admin"] = {
+        ["tagColor"] = "Really red",
+        ["chatColor"] = "Really red",
+        ["id"] = "admin",
+        ["nameColor"] = "Really red",
+        ["headNameTag"] = true,
+        ["tag"] = "Admin"
+    },
+    ["vip"] = {
+        ["tagColor"] = "New Yeller",
+        ["chatColor"] = "New Yeller",
+        ["id"] = "vip",
+        ["nameColor"] = "New Yeller",
+        ["headNameTag"] = true,
+        ["tag"] = "VIP"
+    }
+}
