3348f9229749aa192bd3406efacae873a5ed7d5b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-27 22:48:27 +1100

    Create ForagingMetadata.lua

---
+++ GameData/Farm Life [3601201039]/ForagingMetadata.lua
@@ -0,0 +1,48 @@
+data = {
+    ["Grass"] = {
+        ["offset"] = 0.5,
+        ["randomReward"] = true,
+        ["id"] = "Grass",
+        ["rewards"] = {
+            ["Carrot Seeds"] = {["rewardId"] = "Carrot Seeds", ["dropChance"] = 0.65},
+            ["Onion Seeds"] = {["rewardId"] = "Onion Seeds", ["dropChance"] = 0.55},
+            ["Wheat Seeds"] = {["rewardId"] = "Wheat Seeds", ["dropChance"] = 0.4}
+        },
+        ["renewable"] = false,
+        ["spawnChance"] = 1
+    },
+    ["Blackberry Bush"] = {
+        ["offset"] = 2,
+        ["renewableAge"] = 100,
+        ["renewable"] = true,
+        ["id"] = "Blackberry Bush",
+        ["bush"] = true,
+        ["rewardId"] = "Blackberry",
+        ["spawnChance"] = 0.4
+    },
+    ["Wildberry Bush"] = {
+        ["offset"] = 2,
+        ["renewableAge"] = 60,
+        ["renewable"] = true,
+        ["id"] = "Wildberry Bush",
+        ["bush"] = true,
+        ["rewardId"] = "Wild Berry",
+        ["spawnChance"] = 1
+    },
+    ["Brown Mushroom"] = {
+        ["offset"] = 0,
+        ["renewable"] = false,
+        ["id"] = "Brown Mushroom",
+        ["mushroom"] = true,
+        ["rewardId"] = "Brown Mushroom",
+        ["spawnChance"] = 0.5
+    },
+    ["Red Mushroom"] = {
+        ["offset"] = 0,
+        ["renewable"] = false,
+        ["id"] = "Red Mushroom",
+        ["mushroom"] = true,
+        ["rewardId"] = "Red Mushroom",
+        ["spawnChance"] = 0.25
+    }
+}
