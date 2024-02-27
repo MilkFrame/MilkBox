3cc4b0ccac6d0311ddc327e1caeedebf4a6046b0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-27 22:51:04 +1100

    Create UpgradeMetadata.lua

---
+++ GameData/Farm Life [3601201039]/UpgradeMetadata.lua
@@ -0,0 +1,119 @@
+data = {
+    ["automaticHarvester"] = {
+        ["name"] = "Automatic Harvester",
+        ["id"] = "automaticHarvester",
+        ["buyPrice"] = 50000,
+        ["description"] = "Automatic Harvester",
+        ["textureId"] = "rbxassetid://3878803509"
+    },
+    ["oilMaker"] = {
+        ["name"] = "Oil Maker",
+        ["buyItems"] = {["Aspen Plank"] = 600},
+        ["id"] = "oilMaker",
+        ["buyPrice"] = 70000,
+        ["description"] = "Culinary Oils",
+        ["textureId"] = "rbxassetid://3692940657"
+    },
+    ["house2"] = {
+        ["name"] = "House Upgrade",
+        ["id"] = "house2",
+        ["buyPrice"] = 200000,
+        ["description"] = "Expand Farm House",
+        ["textureId"] = "rbxassetid://3878803594"
+    },
+    ["sawmill"] = {
+        ["name"] = "Sawmill",
+        ["buyItems"] = {["Wood"] = 125},
+        ["id"] = "sawmill",
+        ["buyPrice"] = 2000,
+        ["description"] = "Wooden Planks",
+        ["textureId"] = "rbxassetid://3699203307"
+    },
+    ["statue"] = {
+        ["name"] = "Statue of Excellence",
+        ["id"] = "statue",
+        ["buyPrice"] = 250000,
+        ["description"] = "Decoration",
+        ["textureId"] = "rbxassetid://3662127622"
+    },
+    ["plot2"] = {
+        ["name"] = "Expand Farmland",
+        ["buyItems"] = {["Maple Plank"] = 500},
+        ["id"] = "plot2",
+        ["description"] = "More Crop Fields",
+        ["buyPrice"] = 5000,
+        ["textureId"] = "rbxassetid://3603588984"
+    },
+    ["automaticHarvester3"] = {
+        ["name"] = "Automatic Harvester 3",
+        ["description"] = "Automatic Harvester 3",
+        ["id"] = "automaticHarvester3",
+        ["buyPrice"] = 400000,
+        ["requiredUpgrade"] = "plot3",
+        ["textureId"] = "rbxassetid://3878803509"
+    },
+    ["automaticHarvester2"] = {
+        ["name"] = "Automatic Harvester 2",
+        ["description"] = "Automatic Harvester 2",
+        ["id"] = "automaticHarvester2",
+        ["buyPrice"] = 150000,
+        ["requiredUpgrade"] = "plot2",
+        ["textureId"] = "rbxassetid://3878803509"
+    },
+    ["churner"] = {
+        ["name"] = "Churner",
+        ["buyItems"] = {["Maple Plank"] = 500},
+        ["id"] = "churner",
+        ["buyPrice"] = 20000,
+        ["description"] = "Dairy and Egg Products",
+        ["textureId"] = "rbxassetid://3710541182"
+    },
+    ["smallMetalTrough"] = {
+        ["name"] = "Metal Wheat Feeder",
+        ["id"] = "smallMetalTrough",
+        ["buyPrice"] = 15000,
+        ["description"] = "Larger Wheat Capacity",
+        ["textureId"] = "rbxassetid://3603601951"
+    },
+    ["plot3"] = {
+        ["buyPrice"] = 50000,
+        ["name"] = "Expand Farmland",
+        ["buyItems"] = {["Maple Plank"] = 500},
+        ["id"] = "plot3",
+        ["description"] = "More Crop Fields",
+        ["requiredUpgrade"] = "plot2",
+        ["textureId"] = "rbxassetid://3607814819"
+    },
+    ["largeMetalTrough"] = {
+        ["name"] = "Metal Carrot Feeder",
+        ["description"] = "Larger Carrot Capacity",
+        ["id"] = "largeMetalTrough",
+        ["buyPrice"] = 20000,
+        ["requiredUpgrade"] = "largeAnimalPen",
+        ["textureId"] = "rbxassetid://3677674439"
+    },
+    ["preservesBarrel"] = {
+        ["name"] = "Preserves Barrel",
+        ["buyItems"] = {["Maple Plank"] = 700},
+        ["id"] = "preservesBarrel",
+        ["buyPrice"] = 12000,
+        ["description"] = "Mixed Crop Products",
+        ["textureId"] = "rbxassetid://3699364525"
+    },
+    ["windmill"] = {
+        ["name"] = "Windmill",
+        ["buyItems"] = {["Wood Plank"] = 300},
+        ["id"] = "windmill",
+        ["buyPrice"] = 3000,
+        ["description"] = "Flour and Grains",
+        ["textureId"] = "rbxassetid://3710949213"
+    },
+    ["largeAnimalPen"] = {
+        ["name"] = "Large Animal Pen",
+        ["buyItems"] = {["Aspen Plank"] = 600},
+        ["id"] = "largeAnimalPen",
+        ["buyPrice"] = 10000,
+        ["description"] = "Unlocks Large Animals",
+        ["textureId"] = "rbxassetid://3603591810"
+    }
+}
