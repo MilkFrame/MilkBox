7c6291d02d3e8526170ac8d4119d52a1ff5d2c45
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-27 22:50:33 +1100

    Create TreeMetadata.lua

---
+++ GameData/Farm Life [3601201039]/TreeMetadata.lua
@@ -0,0 +1,34 @@
+data = {
+    ["Enchanted"] = {
+        ["health"] = 1000,
+        ["chopStrengthRequirement"] = 4,
+        ["id"] = "Enchanted",
+        ["woodPerTree"] = 20,
+        ["colors"] = {Color3.fromRGB(255, 56, 56), Color3.fromRGB(255, 89, 89), Color3.fromRGB(255, 74, 58)},
+        ["reward"] = "Enchanted Wood"
+    },
+    ["Aspen"] = {
+        ["health"] = 240,
+        ["chopStrengthRequirement"] = 3,
+        ["id"] = "Aspen",
+        ["woodPerTree"] = 6,
+        ["colors"] = {Color3.fromRGB(254, 192, 24), Color3.fromRGB(231, 195, 15), Color3.fromRGB(255, 223, 62)},
+        ["reward"] = "Aspen Wood"
+    },
+    ["Maple"] = {
+        ["health"] = 240,
+        ["chopStrengthRequirement"] = 2,
+        ["id"] = "Maple",
+        ["woodPerTree"] = 8,
+        ["colors"] = {Color3.fromRGB(255, 56, 56), Color3.fromRGB(255, 89, 89), Color3.fromRGB(255, 74, 58)},
+        ["reward"] = "Maple Wood"
+    },
+    ["Basic"] = {
+        ["health"] = 100,
+        ["chopStrengthRequirement"] = 1,
+        ["id"] = "Basic",
+        ["woodPerTree"] = 5,
+        ["colors"] = {Color3.fromRGB(91, 154, 76), Color3.fromRGB(75, 127, 63), Color3.fromRGB(58, 154, 62)},
+        ["reward"] = "Wood"
+    }
+}
