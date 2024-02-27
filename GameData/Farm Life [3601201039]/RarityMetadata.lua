f909c7f296fb000f9efaf5fbc4185733ef3db3d1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-27 22:50:05 +1100

    Create RarityMetadata.lua

---
+++ GameData/Farm Life [3601201039]/RarityMetadata.lua
@@ -0,0 +1,6 @@
+data = {
+    ["mythical"] = {["id"] = "mythical", ["title"] = "MYTHICAL", ["color"] = Color3.fromRGB(255, 20, 147)},
+    ["legendary"] = {["id"] = "legendary", ["title"] = "LEGENDARY", ["color"] = Color3.fromRGB(255, 255, 0)},
+    ["rare"] = {["id"] = "rare", ["title"] = "RARE", ["color"] = Color3.fromRGB(30, 144, 255)},
+    ["common"] = {["id"] = "common", ["title"] = "COMMON", ["color"] = Color3.fromRGB(62, 37, 42)}
+}
