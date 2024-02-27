789fc39af0c9694577532c5c523ee19455625344
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-27 22:50:18 +1100

    Create RollMetadata.lua

---
+++ GameData/Farm Life [3601201039]/RollMetadata.lua
@@ -0,0 +1,71 @@
+data = {
+    ["qualityFurniture"] = {
+        ["category"] = "furniture",
+        ["title"] = "Quality Furniture Box",
+        ["buyPriceGold"] = 5000,
+        ["id"] = "qualityFurniture",
+        ["description"] = "55% rare, 34% Legendary, 11% Mythical",
+        ["order"] = 2,
+        ["textureId"] = "rbxassetid://3940117510"
+    },
+    ["largePen"] = {
+        ["category"] = "animals",
+        ["title"] = "Large Egg",
+        ["buyPriceGold"] = 10000,
+        ["id"] = "largePen",
+        ["description"] = "60% Common, 39% Rare, 1% Legendary",
+        ["order"] = 2,
+        ["textureId"] = "rbxassetid://3764038593"
+    },
+    ["premiumFurniture"] = {
+        ["description"] = "60% Legendary, 40% Mythical",
+        ["id"] = "premiumFurniture",
+        ["developerProduct"] = 745115205,
+        ["buyPriceRobux"] = 139,
+        ["textureId"] = "rbxassetid://3940117730",
+        ["title"] = "Premium Furniture Box",
+        ["category"] = "furniture",
+        ["order"] = 3,
+        ["developerProductDev"] = 745114518
+    },
+    ["cheapFurniture"] = {
+        ["category"] = "furniture",
+        ["title"] = "Cheap Furniture Box",
+        ["buyPriceGold"] = 800,
+        ["id"] = "cheapFurniture",
+        ["description"] = "30% Rare, 17% Legendary, 1% Mythical",
+        ["order"] = 1,
+        ["textureId"] = "rbxassetid://3940117092"
+    },
+    ["largePenLucky"] = {
+        ["description"] = "56% Rare, 40% Legendary, 4% Mythical",
+        ["id"] = "largePenLucky",
+        ["developerProduct"] = 706699909,
+        ["buyPriceRobux"] = 175,
+        ["textureId"] = "rbxassetid://3764038513",
+        ["title"] = "Lucky Large Egg",
+        ["category"] = "animals",
+        ["order"] = 4,
+        ["developerProductDev"] = 706701000
+    },
+    ["smallPen"] = {
+        ["category"] = "animals",
+        ["title"] = "Small Egg",
+        ["buyPriceGold"] = 1000,
+        ["id"] = "smallPen",
+        ["description"] = "60% Common, 39% Rare, 1% Legendary",
+        ["order"] = 1,
+        ["textureId"] = "rbxassetid://3764038662"
+    },
+    ["smallPenLucky"] = {
+        ["description"] = "56% Rare, 40% Legendary, 4% Mythical",
+        ["id"] = "smallPenLucky",
+        ["developerProduct"] = 706699345,
+        ["buyPriceRobux"] = 75,
+        ["textureId"] = "rbxassetid://3764038393",
+        ["title"] = "Lucky Small Egg",
+        ["category"] = "animals",
+        ["order"] = 3,
+        ["developerProductDev"] = 706700621
+    }
+}
