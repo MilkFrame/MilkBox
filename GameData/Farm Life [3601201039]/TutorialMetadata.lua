69e07cd0c5d59899340e54e93426560078998790
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-27 22:50:48 +1100

    Create TutorialMetadata.lua

---
+++ GameData/Farm Life [3601201039]/TutorialMetadata.lua
@@ -0,0 +1,79 @@
+data = {
+    ["caughtFish"] = {
+        ["id"] = "caughtFish",
+        ["message"] = "Nice catch! Fish can be sold for money or sacrificed to the mysterious fishing elder shrine. It'll be worth it to see what the fishing elder can offer."
+    },
+    ["harvestCrops"] = {
+        ["id"] = "harvestCrops",
+        ["nextTutorial"] = "sellCrops",
+        ["message"] = "Perfect. Once they are fully grown harvest by clicking without any item in hand."
+    },
+    ["goFish"] = {
+        ["id"] = "goFish",
+        ["nextTutorial"] = "goneFishing",
+        ["message"] = "Go to any pond or river and click the water source with your fishing rod. You'll start bringing home tasty fish in no time."
+    },
+    ["plantSeeds"] = {
+        ["id"] = "plantSeeds",
+        ["nextTutorial"] = "waterSeeds",
+        ["message"] = "Welcome to Farm Life. Plant your wheat seeds on the dirt plot."
+    },
+    ["openStore"] = {
+        ["id"] = "openStore",
+        ["nextTutorial"] = "explore",
+        ["message"] = "You're on your way to becoming a successful farmer. Use the store button in the left panel to shop for more seeds."
+    },
+    ["fillWateringCan"] = {
+        ["id"] = "fillWateringCan",
+        ["message"] = "Looks like you're out of water. Fill up your watering at a pond or river."
+    },
+    ["explore"] = {
+        ["id"] = "explore",
+        ["message"] = "Legend says the wilderness is filled with ancient spirits. It's worth exploring sometime. Just don't get lost."
+    },
+    ["goLumberjackShrine"] = {
+        ["id"] = "goLumberjackShrine",
+        ["message"] = "Nice job! Wood can be used for upgrades or sacrificed to the mysterious lumberjack elder shrine. The lumberjack elder may reward you generously."
+    },
+    ["fillLargeAnimalTrough"] = {
+        ["id"] = "fillLargeAnimalTrough",
+        ["message"] = "Large animals eat carrots. Make sure to keep their feeder full."
+    },
+    ["sellCrops"] = {
+        ["id"] = "sellCrops",
+        ["nextTutorial"] = "openStore",
+        ["message"] = "You sell crops at the sell bin. Just hold them in your hand and click on the bin."
+    },
+    ["goneFishing"] = {
+        ["id"] = "goneFishing",
+        ["nextTutorial"] = "caughtFish",
+        ["message"] = [[You've succesfully started fishing. As a wise man once said "Be patient and calm... for no one can catch fish in anger" ]]
+    },
+    ["waterSeeds"] = {
+        ["id"] = "waterSeeds",
+        ["nextTutorial"] = "harvestCrops",
+        ["message"] = "Great! Now water the seeds using your Watering Can."
+    },
+    ["treeChopped"] = {
+        ["id"] = "treeChopped",
+        ["nextTutorial"] = "goLumberjackShrine",
+        ["message"] = "This is a basic tree. Every chop will get you one piece of wood. More valuable trees require a stronger axes."
+    },
+    ["upgradeableTools"] = {
+        ["id"] = "upgradeableTools",
+        ["message"] = "Your sacrifice has been made. The more you sacrifice the better rewards you'll earn. Legend says you may even be able to receive better tools."
+    },
+    ["chopTrees"] = {
+        ["id"] = "chopTrees",
+        ["nextTutorial"] = "treeChopped",
+        ["message"] = "Now that you've bought an axe you can chop basic trees outside your plot to obtain wood."
+    },
+    ["renewableCrops"] = {
+        ["id"] = "renewableCrops",
+        ["message"] = "Renewable crops take longer to grow but never have to be replanted. Remove them using a shovel from the store."
+    },
+    ["fillSmallAnimalTrough"] = {
+        ["id"] = "fillSmallAnimalTrough",
+        ["message"] = "It's important to keep animals happy. Supply wheat in the trough and pet them regularly."
+    }
+}
