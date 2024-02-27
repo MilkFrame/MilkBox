f5e12aca13061feb8daee9ac6677490d698ced91
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-27 22:46:33 +1100

    Create AutomationMetadata.lua

---
+++ GameData/Farm Life [3601201039]/AutomationMetadata.lua
@@ -0,0 +1,32 @@
+data = {
+    ["automaticHarvester"] = {
+        ["cooldown"] = 0.4,
+        ["inventorySlots"] = 12,
+        ["modelName"] = "AutomaticHarvester",
+        ["fuelType"] = "Low Grade Fuel",
+        ["id"] = "automaticHarvester",
+        ["inventorySpace"] = 100,
+        ["strength"] = 1,
+        ["title"] = "Automatic Harvester"
+    },
+    ["automaticHarvester3"] = {
+        ["cooldown"] = 0.4,
+        ["inventorySlots"] = 16,
+        ["modelName"] = "AutomaticHarvester3",
+        ["fuelType"] = "Low Grade Fuel",
+        ["id"] = "automaticHarvester3",
+        ["inventorySpace"] = 200,
+        ["strength"] = 3,
+        ["title"] = "Automatic Harvester 3"
+    },
+    ["automaticHarvester2"] = {
+        ["cooldown"] = 0.4,
+        ["inventorySlots"] = 12,
+        ["modelName"] = "AutomaticHarvester2",
+        ["fuelType"] = "Low Grade Fuel",
+        ["id"] = "automaticHarvester2",
+        ["inventorySpace"] = 100,
+        ["strength"] = 2,
+        ["title"] = "Automatic Harvester 2"
+    }
+}
