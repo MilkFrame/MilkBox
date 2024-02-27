77ddb36d993b8fd207ebb3c0c007a252b7e09610
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-27 22:49:00 +1100

    Create GamePassesMetadata.lua

---
+++ GameData/Farm Life [3601201039]/GamePassesMetadata.lua
@@ -0,0 +1,45 @@
+data = {
+    ["vip"] = {
+        ["description"] = "VIP Perks",
+        ["buyPrice"] = 600,
+        ["name"] = "VIP",
+        ["productId"] = 6991270,
+        ["id"] = "vip",
+        ["devProductId"] = 6968607,
+        ["perk"] = true,
+        ["textureId"] = "rbxassetid://3662127544"
+    },
+    ["lumberjack"] = {
+        ["itemName"] = "Enchanted Axe",
+        ["description"] = "Best Axe",
+        ["id"] = "lumberjack",
+        ["textureId"] = "rbxassetid://3659044384",
+        ["name"] = "Enchanted Axe",
+        ["productId"] = 7006240,
+        ["devProductId"] = 7003469,
+        ["item"] = true,
+        ["buyPrice"] = 400
+    },
+    ["fisherman"] = {
+        ["itemName"] = "Enchanted Rod",
+        ["description"] = "Best Rod",
+        ["id"] = "fisherman",
+        ["textureId"] = "rbxassetid://3661964163",
+        ["name"] = "Enchanted Rod",
+        ["productId"] = 6991268,
+        ["devProductId"] = 6968256,
+        ["item"] = true,
+        ["buyPrice"] = 400
+    },
+    ["infiniteWateringCan"] = {
+        ["itemName"] = "Enchanted Can",
+        ["description"] = "Infinite Water",
+        ["id"] = "infiniteWateringCan",
+        ["textureId"] = "rbxassetid://3661964275",
+        ["name"] = "Infinite Water Can",
+        ["productId"] = 6991259,
+        ["devProductId"] = 6968205,
+        ["item"] = true,
+        ["buyPrice"] = 400
+    }
+}
