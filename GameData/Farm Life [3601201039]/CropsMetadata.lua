941db721f7c1ea7e3993ec0d8c3e91ce2ab255b4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-27 22:47:06 +1100

    Create CropsMetadata.lua

---
+++ GameData/Farm Life [3601201039]/CropsMetadata.lua
@@ -0,0 +1,40 @@
+data = {
+    ["Wheat"] = {["maxAge"] = 100, ["harvestReward"] = "Wheat"},
+    ["Corn"] = {
+        ["renewableAge"] = 350,
+        ["maxAge"] = 900,
+        ["harvestReward"] = "Corn",
+        ["renewable"] = true,
+        ["amountPerHarvest"] = 2
+    },
+    ["Onion"] = {["maxAge"] = 340, ["harvestReward"] = "Onion"},
+    ["Grapes"] = {
+        ["renewableAge"] = 1200,
+        ["maxAge"] = 900,
+        ["harvestReward"] = "Grapes",
+        ["renewable"] = true,
+        ["amountPerHarvest"] = 3
+    },
+    ["Tomato"] = {["harvestReward"] = "Tomato", ["maxAge"] = 1500, ["amountPerHarvest"] = 4},
+    ["Watermelon"] = {["maxAge"] = 900, ["harvestReward"] = "Watermelon"},
+    ["Carrot"] = {["maxAge"] = 350, ["harvestReward"] = "Carrot"},
+    ["Cucumber"] = {["harvestReward"] = "Cucumber", ["maxAge"] = 1700, ["amountPerHarvest"] = 4},
+    ["Turnip"] = {["maxAge"] = 1800, ["harvestReward"] = "Turnip"},
+    ["Strawberry"] = {
+        ["renewableAge"] = 370,
+        ["maxAge"] = 1200,
+        ["harvestReward"] = "Strawberry",
+        ["renewable"] = true,
+        ["amountPerHarvest"] = 6
+    },
+    ["Pumpkin"] = {["maxAge"] = 800, ["harvestReward"] = "Pumpkin"},
+    ["Chilli"] = {
+        ["renewableAge"] = 350,
+        ["maxAge"] = 1400,
+        ["harvestReward"] = "Chilli",
+        ["renewable"] = true,
+        ["amountPerHarvest"] = 2
+    },
+    ["Potato"] = {["maxAge"] = 500, ["harvestReward"] = "Potato"},
+    ["Pineapple"] = {["maxAge"] = 2400, ["harvestReward"] = "Pineapple"}
+}
