55f64f1c17acf4dadd4f38ee3a889e8721730a95
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-02-27 22:45:55 +1100

    Create AnimalMetadata.lua

---
+++ GameData/Farm Life [3601201039]/AnimalMetadata.lua
@@ -0,0 +1,107 @@
+data = {
+    ["chicken"] = {
+        ["name"] = "Chicken",
+        ["id"] = "chicken",
+        ["description"] = "Lays Eggs",
+        ["buyPrice"] = 500,
+        ["textureId"] = "rbxassetid://3603591427"
+    },
+    ["duck"] = {
+        ["name"] = "Duck",
+        ["id"] = "duck",
+        ["description"] = "Lays Duck Eggs",
+        ["buyPrice"] = 500,
+        ["textureId"] = "rbxassetid://3778589876"
+    },
+    ["fox"] = {
+        ["name"] = "Fox",
+        ["id"] = "fox",
+        ["description"] = "Sharpens Teeth",
+        ["buyPrice"] = 500,
+        ["textureId"] = "rbxassetid://3778590005"
+    },
+    ["goat"] = {
+        ["name"] = "Goat",
+        ["id"] = "goat",
+        ["description"] = "Produces Goat Milk",
+        ["buyPrice"] = 500,
+        ["textureId"] = "rbxassetid://3778606444"
+    },
+    ["rabbit"] = {
+        ["name"] = "Rabbit",
+        ["id"] = "rabbit",
+        ["description"] = "Produces Goat Milk",
+        ["buyPrice"] = 500,
+        ["textureId"] = "rbxassetid://3778590412"
+    },
+    ["sheep"] = {
+        ["name"] = "Sheep",
+        ["description"] = "Produces Wool",
+        ["id"] = "sheep",
+        ["buyPrice"] = 2500,
+        ["requiredUpgrade"] = "largeAnimalPen",
+        ["textureId"] = "rbxassetid://3603599063"
+    },
+    ["turtle"] = {
+        ["name"] = "Turtle",
+        ["id"] = "turtle",
+        ["description"] = "Produces Goat Milk",
+        ["buyPrice"] = 2500,
+        ["textureId"] = "rbxassetid://3778590537"
+    },
+    ["pig"] = {
+        ["name"] = "Pig",
+        ["id"] = "pig",
+        ["description"] = "Finds Truffles",
+        ["buyPrice"] = 500,
+        ["textureId"] = "rbxassetid://3603596144"
+    },
+    ["moose"] = {
+        ["name"] = "Moose",
+        ["description"] = "Produces Milk",
+        ["id"] = "moose",
+        ["buyPrice"] = 2500,
+        ["requiredUpgrade"] = "largeAnimalPen",
+        ["textureId"] = "rbxassetid://3778590287"
+    },
+    ["bear"] = {
+        ["name"] = "Bear",
+        ["description"] = "Produces Milk",
+        ["id"] = "bear",
+        ["buyPrice"] = 15000,
+        ["requiredUpgrade"] = "largeAnimalPen",
+        ["textureId"] = "rbxassetid://3778589645"
+    },
+    ["alligator"] = {
+        ["name"] = "Alligator",
+        ["description"] = "Produces Milk",
+        ["id"] = "alligator",
+        ["buyPrice"] = 2500,
+        ["requiredUpgrade"] = "largeAnimalPen",
+        ["textureId"] = "rbxassetid://3778589349"
+    },
+    ["alpaca"] = {
+        ["name"] = "Alpaca",
+        ["buyPrice"] = 2500,
+        ["id"] = "alpaca",
+        ["description"] = "Produces Thick Wool",
+        ["requiredUpgrade"] = "largeAnimalPen",
+        ["textureId"] = "rbxassetid://3778589504"
+    },
+    ["deer"] = {
+        ["name"] = "Deer",
+        ["description"] = "Produces Milk",
+        ["id"] = "deer",
+        ["buyPrice"] = 2500,
+        ["requiredUpgrade"] = "largeAnimalPen",
+        ["textureId"] = "rbxassetid://3778589759"
+    },
+    ["cow"] = {
+        ["name"] = "Cow",
+        ["description"] = "Produces Milk",
+        ["id"] = "cow",
+        ["buyPrice"] = 2500,
+        ["requiredUpgrade"] = "largeAnimalPen",
+        ["textureId"] = "rbxassetid://3603591810"
+    }
+}
