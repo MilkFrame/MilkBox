eb6df56078c4a2f0674b56763ff296c4b512c2f3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-10 21:52:02 +1100

    Create GearPerks.lua

---
+++ GameData/World ⁄ ⁄ Zero [2727067538]/GearPerks.lua
@@ -0,0 +1,265 @@
+data = {
+    ["ResistFrost"] = {
+        ["DisplayDesc"] = "Chance to dodge a Frost status infliction",
+        ["Name"] = "ResistFrost",
+        ["StatRange"] = {0.3, 0.9},
+        ["ID"] = 10,
+        ["DisplayName"] = "Resist Frost"
+    },
+    ["Glass"] = {
+        ["DisplayDesc"] = "Increased max critical attack, but -35% health",
+        ["ArmorOnly"] = true,
+        ["Name"] = "Glass",
+        ["StatRange"] = {0.3, 1},
+        ["Tier5Perk"] = true,
+        ["ID"] = 24,
+        ["DisplayName"] = "Glass"
+    },
+    ["MasterThief"] = {
+        ["DisplayDesc"] = "Gain additional movement speed for 2.5 seconds, after destroying an enemy",
+        ["ArmorOnly"] = true,
+        ["Name"] = "MasterThief",
+        ["StatRange"] = {0.1, 0.35},
+        ["Tier5Perk"] = true,
+        ["ID"] = 25,
+        ["DisplayName"] = "Master Thief"
+    },
+    ["Fortress"] = {
+        ["DisplayDesc"] = "Increased defense, but -20% walkspeed",
+        ["ArmorOnly"] = true,
+        ["Name"] = "Fortress",
+        ["StatRange"] = {0.1, 0.35},
+        ["Tier5Perk"] = true,
+        ["ID"] = 20,
+        ["DisplayName"] = "Fortress"
+    },
+    ["Destruction"] = {
+        ["DisplayDesc"] = "Upon death, your character self destructs dealing massive damage",
+        ["ArmorOnly"] = true,
+        ["Name"] = "Destruction",
+        ["ID"] = 30,
+        ["StatRange"] = {0.1, 0.5},
+        ["Tier5Perk"] = true,
+        ["WorldOrderID"] = 10,
+        ["DisplayName"] = "Self Destruct"
+    },
+    ["RoughSkin"] = {
+        ["DisplayDesc"] = "Chance to inflict damage back from getting hit",
+        ["ArmorOnly"] = true,
+        ["Name"] = "RoughSkin",
+        ["StatRange"] = {0.04, 0.09},
+        ["ID"] = 14,
+        ["DisplayName"] = "Rough Skin"
+    },
+    ["ResistPoison"] = {
+        ["DisplayDesc"] = "Chance to dodge a Poison status infliction",
+        ["Name"] = "ResistPoison",
+        ["StatRange"] = {0.3, 0.9},
+        ["ID"] = 9,
+        ["DisplayName"] = "Resist Poison"
+    },
+    ["ResistKnockdown"] = {
+        ["DisplayDesc"] = "Chance to dodge a Knockdown status infliction",
+        ["Name"] = "ResistKnockdown",
+        ["StatRange"] = {0.3, 0.9},
+        ["ID"] = 11,
+        ["DisplayName"] = "Resist Knockdown"
+    },
+    ["LifeDrain"] = {
+        ["DisplayDesc"] = "You gain this percent back as health relative to the damage you are dealing",
+        ["Name"] = "LifeDrain",
+        ["StatRange"] = {0.02, 0.06},
+        ["ID"] = 18,
+        ["WeaponOnly"] = true,
+        ["DisplayName"] = "Life Drain"
+    },
+    ["BonusWalkspeed"] = {
+        ["DisplayDesc"] = "Grants a faster walk speed",
+        ["Name"] = "BonusWalkspeed",
+        ["StatRange"] = {0.02, 0.05},
+        ["ID"] = 7,
+        ["LimitPetRange"] = true,
+        ["DisplayName"] = "Bonus Walkspeed"
+    },
+    ["ResistBurn"] = {
+        ["DisplayDesc"] = "Chance to dodge a Burn status infliction",
+        ["Name"] = "ResistBurn",
+        ["StatRange"] = {0.3, 0.9},
+        ["ID"] = 8,
+        ["DisplayName"] = "Resist Burn"
+    },
+    ["PetFoodDrop"] = {
+        ["DisplayDesc"] = "Increased chance to get pet food from enemies",
+        ["Name"] = "PetFoodDrop",
+        ["StatRange"] = {0.02, 0.08},
+        ["ID"] = 2,
+        ["DisplayName"] = "Bonus Pet Food Chance"
+    },
+    ["DodgeChance"] = {
+        ["DisplayDesc"] = "Chance to dodge any attack from an enemy",
+        ["ArmorOnly"] = true,
+        ["Name"] = "DodgeChance",
+        ["StatRange"] = {0.05, 0.2},
+        ["ID"] = 13,
+        ["DisplayName"] = "Untouchable"
+    },
+    ["EliteBoss"] = {
+        ["DisplayDesc"] = "Increased damage against elite monsters",
+        ["PetOnly"] = true,
+        ["WeaponOnly"] = true,
+        ["DisplayName"] = "Elite's Boss",
+        ["Name"] = "EliteBoss",
+        ["StatRange"] = {0.1, 0.3},
+        ["Tier5Perk"] = true,
+        ["PetStatRange"] = {0.05, 0.1},
+        ["ID"] = 22
+    },
+    ["TestTier5"] = {
+        ["DisplayDesc"] = "Increased damage against bosses and minibosses",
+        ["PetOnly"] = true,
+        ["WeaponOnly"] = true,
+        ["DisplayName"] = "Boss of the Boss",
+        ["Name"] = "TestTier5",
+        ["StatRange"] = {0.1, 0.3},
+        ["Tier5Perk"] = true,
+        ["PetStatRange"] = {0.05, 0.1},
+        ["ID"] = 12
+    },
+    ["Vampiric"] = {
+        ["DisplayDesc"] = "Regenerate a portion of max health and gain critical strike chance on your first basic attack",
+        ["PetOnly"] = true,
+        ["WorldOrderID"] = 10,
+        ["WeaponOnly"] = true,
+        ["DisplayName"] = "Vampiric",
+        ["Name"] = "Vampiric",
+        ["StatRange"] = {0.02, 0.1},
+        ["Tier5Perk"] = true,
+        ["PetStatRange"] = {0.02, 0.05},
+        ["ID"] = 26
+    },
+    ["UltCharge"] = {
+        ["DisplayDesc"] = "Grants more ultimate energy from enemies",
+        ["Name"] = "UltCharge",
+        ["StatRange"] = {0.05, 0.15},
+        ["ID"] = 4,
+        ["DisplayName"] = "Bonus Ult. Charge"
+    },
+    ["Ferocious"] = {
+        ["DisplayDesc"] = "Increased damage when under 50% health.",
+        ["PetOnly"] = true,
+        ["WorldOrderID"] = 10,
+        ["WeaponOnly"] = true,
+        ["DisplayName"] = "Ferocious",
+        ["Name"] = "Ferocious",
+        ["StatRange"] = {0.2, 0.4},
+        ["Tier5Perk"] = true,
+        ["PetStatRange"] = {0.05, 0.15},
+        ["ID"] = 27
+    },
+    ["Aggro"] = {
+        ["NegativeActive"] = true,
+        ["Name"] = "Aggro",
+        ["StatRange"] = {0.15, 0.35},
+        ["ID"] = 3,
+        ["DisplayDesc"] = "Improves how much you attract enemies from attacking",
+        ["DisplayName"] = "Shifted Aggro"
+    },
+    ["Elemental"] = {
+        ["DisplayDesc"] = "Reduce damage taken from status effects",
+        ["ID"] = 28,
+        ["Name"] = "Elemental",
+        ["PetOnly"] = true,
+        ["StatRange"] = {0.1, 0.4},
+        ["WeaponOnly"] = true,
+        ["PetStatRange"] = {0.05, 0.1},
+        ["DisplayName"] = "Elemental Resistance"
+    },
+    ["BurnChance"] = {
+        ["DisplayDesc"] = "Chance to inflict a Burn status on an enemy",
+        ["Name"] = "BurnChance",
+        ["StatRange"] = {0.02, 0.07},
+        ["ID"] = 16,
+        ["WeaponOnly"] = true,
+        ["DisplayName"] = "Burn Chance"
+    },
+    ["DamageReduction"] = {
+        ["DisplayDesc"] = "Reduces overall received damage",
+        ["ArmorOnly"] = true,
+        ["Name"] = "DamageReduction",
+        ["StatRange"] = {0.02, 0.08},
+        ["ID"] = 15,
+        ["DisplayName"] = "Damage Reduction"
+    },
+    ["CritStack"] = {
+        ["DisplayDesc"] = "Increases crit chance by attacking enemies",
+        ["Name"] = "CritStack",
+        ["StatRange"] = {0.05, 0.15},
+        ["ID"] = 17,
+        ["WeaponOnly"] = true,
+        ["DisplayName"] = "Crit Stack"
+    },
+    ["PoisonThorns"] = {
+        ["DisplayDesc"] = "Chance to inflict Poison back from getting hit",
+        ["ArmorOnly"] = true,
+        ["Name"] = "PoisonThorns",
+        ["StatRange"] = {0.05, 0.2},
+        ["ID"] = 29,
+        ["DisplayName"] = "Poisonous Thorns"
+    },
+    ["BonusRegen"] = {
+        ["DisplayDesc"] = "Grants more health gained from regenerating",
+        ["Name"] = "BonusRegen",
+        ["StatRange"] = {0.01, 0.05},
+        ["ID"] = 23,
+        ["DisplayName"] = "Bonus Health Regen"
+    },
+    ["GoldDrop"] = {
+        ["DisplayDesc"] = "Grants more gold from enemies",
+        ["Name"] = "GoldDrop",
+        ["StatRange"] = {0.05, 0.15},
+        ["ID"] = 1,
+        ["DisplayName"] = "Bonus Gold"
+    },
+    ["MobBoss"] = {
+        ["DisplayDesc"] = "Increased damage against normal monsters",
+        ["PetOnly"] = true,
+        ["WeaponOnly"] = true,
+        ["DisplayName"] = "Mob Boss",
+        ["Name"] = "MobBoss",
+        ["StatRange"] = {0.1, 0.3},
+        ["Tier5Perk"] = true,
+        ["PetStatRange"] = {0.05, 0.1},
+        ["ID"] = 21
+    },
+    ["OpeningStrike"] = {
+        ["DisplayDesc"] = "Gain increased crit chance against monsters above 75% health",
+        ["Name"] = "OpeningStrike",
+        ["ID"] = 19,
+        ["StatRange"] = {0.1, 0.25},
+        ["Tier5Perk"] = true,
+        ["WeaponOnly"] = true,
+        ["DisplayName"] = "Opening Strike"
+    },
+    ["BonusAttack"] = {
+        ["DisplayDesc"] = "Grants more attack power",
+        ["PetOnly"] = true,
+        ["WeaponOnly"] = true,
+        ["DisplayName"] = "Attack UP",
+        ["ExcludeDuplicate"] = true,
+        ["Name"] = "BonusAttack",
+        ["StatRange"] = {0.02, 0.08},
+        ["PetStatRange"] = {0.01, 0.05},
+        ["ID"] = 6
+    },
+    ["BonusHP"] = {
+        ["DisplayDesc"] = "Grants more health",
+        ["ArmorOnly"] = true,
+        ["PetOnly"] = true,
+        ["DisplayName"] = "HP UP",
+        ["ExcludeDuplicate"] = true,
+        ["Name"] = "BonusHP",
+        ["StatRange"] = {0.05, 0.12},
+        ["PetStatRange"] = {0.01, 0.07},
+        ["ID"] = 5
+    }
+}
