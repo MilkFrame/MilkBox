b13fb1f574271965091d7c035e772154386236ca
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-10 21:57:09 +1100

    Create Products.lua

---
+++ GameData/World ⁄ ⁄ Zero [2727067538]/Products.lua
@@ -0,0 +1,25 @@
+data = {
+    ["Gamepasses"] = {
+        ["VIP"] = {["OwnsGamepass"] = "function: 0x00000000bc0444ed", ["LiveID"] = 8136226},
+        ["ExtraBankStorage"] = {
+            ["LiveID"] = 74987904,
+            ["DevID"] = 75170705,
+            ["OwnsGamepass"] = "function: 0x000000001fecabd5"
+        },
+        ["AdvancedCustomization"] = {["LiveID"] = 6137247},
+        ["Incubator"] = {["OwnsGamepass"] = "function: 0x00000000897905bd", ["LiveID"] = 8461083},
+        ["MoreOutfitSlots"] = {
+            ["LiveID"] = 245383174,
+            ["DevID"] = 244771016,
+            ["OwnsGamepass"] = "function: 0x000000009652cd4d"
+        },
+        ["ExtraItemDrop"] = {["OwnsGamepass"] = "function: 0x000000002a9360a5", ["LiveID"] = 8136250}
+    },
+    ["DeveloperProducts"] = {
+        ["EXP3Hour"] = {["DevID"] = 1081563643, ["LiveID"] = 1087420134},
+        ["StarterPack"] = {["DevID"] = 1081335012, ["LiveID"] = 1087419801},
+        ["EXP1Hour"] = {["DevID"] = 1081563246, ["LiveID"] = 1087420107},
+        ["SkipToEndgame"] = {["DevID"] = 1081262971, ["LiveID"] = 1087419884},
+        ["EXP6Hour"] = {["DevID"] = 1081563832, ["LiveID"] = 1087420145}
+    }
+}
