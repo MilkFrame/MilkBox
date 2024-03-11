78fca38e69a141dca501ca2468de74c31d82723b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-11 23:18:50 +1100

    Create DailyQuests.lua

---
+++ GameData/Untitled Boxing Game [13621938427]/DailyQuests.lua
@@ -0,0 +1,153 @@
+data = {
+    ["Random"] = {
+        ["CASH I"] = {
+            ["Reward"] = {["Amount"] = 3, ["Type"] = "Spins"},
+            ["Type"] = "CashEarned",
+            ["Description"] = "Earn 5000 cash.",
+            ["Amount"] = 5000
+        },
+        ["CASH II"] = {
+            ["Reward"] = {["Amount"] = 7, ["Type"] = "Spins"},
+            ["Type"] = "CashEarned",
+            ["Description"] = "Earn 10,000 cash.",
+            ["Amount"] = 10000
+        },
+        ["STREAK II"] = {
+            ["Reward"] = {["Amount"] = 8, ["Type"] = "Spins"},
+            ["Type"] = "Streak",
+            ["Description"] = "Win 10 matches in a row without losing.",
+            ["Amount"] = 10
+        },
+        ["KNOCKDOWNS IV"] = {
+            ["Reward"] = {["Amount"] = 10, ["Type"] = "Spins"},
+            ["Type"] = "Knockdowns",
+            ["Description"] = "Get 100 Knockdowns.",
+            ["Amount"] = 100
+        },
+        ["SPIN I"] = {
+            ["Reward"] = {["Amount"] = 2, ["Type"] = "Spins"},
+            ["Type"] = "Spins",
+            ["Description"] = "Spin for a new style 10 times.",
+            ["Amount"] = 10
+        },
+        ["COUNTERS II"] = {
+            ["Reward"] = {["Amount"] = 3, ["Type"] = "Spins"},
+            ["Type"] = "Counters",
+            ["Description"] = "Land 25 counters.",
+            ["Amount"] = 25
+        },
+        ["COUNTERS IV"] = {
+            ["Reward"] = {["Amount"] = 10000, ["Type"] = "Cash"},
+            ["Type"] = "Counters",
+            ["Description"] = "Land 50 counters.",
+            ["Amount"] = 50
+        },
+        ["COUNTERS III"] = {
+            ["Reward"] = {["Amount"] = 5000, ["Type"] = "Cash"},
+            ["Type"] = "Counters",
+            ["Description"] = "Land 50 counters.",
+            ["Amount"] = 50
+        },
+        ["COUNTERS I"] = {
+            ["Reward"] = {["Amount"] = 1, ["Type"] = "Spins"},
+            ["Type"] = "Counters",
+            ["Description"] = "Land 10 counters.",
+            ["Amount"] = 1
+        },
+        ["LANDED I"] = {
+            ["Reward"] = {["Amount"] = 5000, ["Type"] = "Cash"},
+            ["Type"] = "PunchesLanded",
+            ["Description"] = "Land 1000 punches.",
+            ["Amount"] = 1000
+        },
+        ["PERFECT DODGE II"] = {
+            ["Reward"] = {["Amount"] = 5000, ["Type"] = "Cash"},
+            ["Type"] = "PerfectDodges",
+            ["Description"] = "Perfect dodge 25 times.",
+            ["Amount"] = 25
+        },
+        ["DEFENDED III"] = {
+            ["Reward"] = {["Amount"] = 10000, ["Type"] = "Cash"},
+            ["Type"] = "Defended",
+            ["Description"] = "Defend 1500 punches.",
+            ["Amount"] = 1500
+        },
+        ["LANDED II"] = {
+            ["Reward"] = {["Amount"] = 8, ["Type"] = "Spins"},
+            ["Type"] = "PunchesLanded",
+            ["Description"] = "Land 2500 punches.",
+            ["Amount"] = 2500
+        },
+        ["DEFENDED II"] = {
+            ["Reward"] = {["Amount"] = 9, ["Type"] = "Spins"},
+            ["Type"] = "Defended",
+            ["Description"] = "Defend 1500 punches.",
+            ["Amount"] = 1500
+        },
+        ["PERFECT DODGE I"] = {
+            ["Reward"] = {["Amount"] = 3, ["Type"] = "Spins"},
+            ["Type"] = "PerfectDodges",
+            ["Description"] = "Perfect dodge 10 times.",
+            ["Amount"] = 10
+        },
+        ["DEFENDED I"] = {
+            ["Reward"] = {["Amount"] = 3, ["Type"] = "Spins"},
+            ["Type"] = "Defended",
+            ["Description"] = "Defend 500 punches.",
+            ["Amount"] = 500
+        },
+        ["STREAK I"] = {
+            ["Reward"] = {["Amount"] = 3, ["Type"] = "Spins"},
+            ["Type"] = "Streak",
+            ["Description"] = "Win 3 matches in a row without losing.",
+            ["Amount"] = 3
+        },
+        ["WINS I"] = {
+            ["Reward"] = {["Amount"] = 2, ["Type"] = "Spins"},
+            ["Type"] = "Wins",
+            ["Description"] = "Win 5 matches.",
+            ["Amount"] = 5
+        },
+        ["ULTIMATES I"] = {
+            ["Reward"] = {["Amount"] = 3000, ["Type"] = "Cash"},
+            ["Type"] = "UltimatesLanded",
+            ["Description"] = "Land 5 Ultimate attacks.",
+            ["Amount"] = 5
+        },
+        ["ULTIMATES II"] = {
+            ["Reward"] = {["Amount"] = 3, ["Type"] = "Spins"},
+            ["Type"] = "UltimatesLanded",
+            ["Description"] = "Land 10 Ultimate attacks.",
+            ["Amount"] = 10
+        },
+        ["WINS II"] = {
+            ["Reward"] = {["Amount"] = 6, ["Type"] = "Spins"},
+            ["Type"] = "Wins",
+            ["Description"] = "Win 15 matches.",
+            ["Amount"] = 15
+        }
+    },
+    ["Guaranteed"] = {
+        ["KNOCKDOWNS II"] = {
+            ["Type"] = "Knockdowns",
+            ["Description"] = "Get 20 Knockdowns.",
+            ["Amount"] = 20,
+            ["Guaranteed"] = true,
+            ["Reward"] = {["Amount"] = 3, ["Type"] = "Spins"}
+        },
+        ["KNOCKDOWNS III"] = {
+            ["Type"] = "Knockdowns",
+            ["Description"] = "Get 50 Knockdowns.",
+            ["Amount"] = 50,
+            ["Guaranteed"] = true,
+            ["Reward"] = {["Amount"] = 6, ["Type"] = "Spins"}
+        },
+        ["KNOCKDOWNS I"] = {
+            ["Type"] = "Knockdowns",
+            ["Description"] = "Get 5 Knockdowns.",
+            ["Amount"] = 5,
+            ["Guaranteed"] = true,
+            ["Reward"] = {["Amount"] = 1, ["Type"] = "Spins"}
+        }
+    }
+}
