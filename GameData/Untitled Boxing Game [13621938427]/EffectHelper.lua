7add5da83184446076d8461dcb6fd2a9d7248182
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-11 23:18:01 +1100

    Create EffectHelper.lua

---
+++ GameData/Untitled Boxing Game [13621938427]/EffectHelper.lua
@@ -0,0 +1,221 @@
+data = {
+    ["Entrances"] = {
+        ["Default"] = {
+            ["Name"] = "Default",
+            ["Animations"] = {
+                ["CharacterVariation"] = 14339807328,
+                ["Character"] = 14329267744,
+                ["Camera"] = 14331219047
+            },
+            ["HasMetadata"] = true,
+            ["Use"] = "function: 0x00000000ac863787",
+            ["Metadata"] = {
+                ["HasVariation"] = true,
+                ["Time"] = 4,
+                ["Price"] = 0,
+                ["Description"] = "The standard ring walk.",
+                ["Rarity"] = "Uncommon",
+                ["DisplayName"] = "Basic"
+            }
+        },
+        ["Bear"] = {
+            ["Name"] = "Bear",
+            ["Animations"] = {["Camera"] = 14390826673, ["Character"] = 14390803208},
+            ["HasMetadata"] = true,
+            ["Use"] = "function: 0x0000000054e44307",
+            ["Metadata"] = {
+                ["Rarity"] = "Mythic",
+                ["Price"] = 20000,
+                ["Time"] = 8.4,
+                ["Description"] = "Show off the head of the bear you defeated.",
+                ["DisplayName"] = "Bear"
+            }
+        },
+        ["Carried"] = {
+            ["Preload"] = "function: 0x0000000026288107",
+            ["Name"] = "Carried",
+            ["Animations"] = {["Character"] = 14390983065, ["Friend"] = 14390957315, ["Camera"] = 14390986206},
+            ["HasMetadata"] = true,
+            ["Use"] = "function: 0x000000002895d187",
+            ["Metadata"] = {
+                ["Rarity"] = "Mythic",
+                ["Price"] = 25000,
+                ["Time"] = 5.833333333333334,
+                ["Description"] = "Get carried on the shoulders of one of your friends.",
+                ["DisplayName"] = "Carried"
+            }
+        },
+        ["Throne"] = {
+            ["Preload"] = "function: 0x0000000014cb3ecf",
+            ["Name"] = "Throne",
+            ["Animations"] = {
+                ["Camera"] = 14341823544,
+                ["Character"] = 14342287363,
+                ["Left"] = 14342767779,
+                ["Right"] = 14342771991
+            },
+            ["HasMetadata"] = true,
+            ["Use"] = "function: 0x000000001330b977",
+            ["Metadata"] = {
+                ["Rarity"] = "Legendary",
+                ["Price"] = 99999,
+                ["Time"] = 9,
+                ["Description"] = "Get carried on a gold throne by 4 of your friends.",
+                ["DisplayName"] = "The Throne"
+            }
+        },
+        ["Stoic"] = {
+            ["Name"] = "Stoic",
+            ["Animations"] = {["Camera"] = 14351486304, ["Character"] = 14351477412},
+            ["HasMetadata"] = true,
+            ["Use"] = "function: 0x000000000647f087",
+            ["Metadata"] = {
+                ["Rarity"] = "Rare",
+                ["Price"] = 2000,
+                ["Time"] = 6.666666666666667,
+                ["Description"] = "You're not here to play around.",
+                ["DisplayName"] = "Basic"
+            }
+        },
+        ["Billionaire"] = {
+            ["Name"] = "Billionaire",
+            ["Animations"] = {["Camera"] = 14350660939, ["Character"] = 14350353310},
+            ["HasMetadata"] = true,
+            ["Use"] = "function: 0x00000000fbe76007",
+            ["Metadata"] = {
+                ["Rarity"] = "Mythic",
+                ["Price"] = 30000,
+                ["Time"] = 6.3,
+                ["Description"] = "Strut like a billionaire and make it rain.",
+                ["DisplayName"] = "Basic"
+            }
+        }
+    },
+    ["Knockdowns"] = {
+        ["Fling"] = {
+            ["Main"] = "function: 0x000000008fa19707",
+            ["DelayTime"] = 8,
+            ["Animations"] = {
+                ["Start"] = 16648553087,
+                ["Loop"] = 16648493072,
+                ["KO"] = 16649713083,
+                ["End"] = 16648665342
+            },
+            ["KO"] = "function: 0x00000000aaa8d127",
+            ["Use"] = "function: 0x00000000c012ce97"
+        },
+        ["8Bit"] = {
+            ["Main"] = "function: 0x0000000014e4f207",
+            ["DelayTime"] = 8,
+            ["Animations"] = {
+                ["Start"] = 16660377173,
+                ["Loop"] = 16660406457,
+                ["KO"] = 16662188706,
+                ["End"] = 16660353264
+            },
+            ["KO"] = "function: 0x0000000040910a07",
+            ["Use"] = "function: 0x0000000005ab982f"
+        },
+        ["Crafted"] = {
+            ["Main"] = "function: 0x000000001f63bcc7",
+            ["DelayTime"] = 8,
+            ["Animations"] = {["KO"] = 16632140256, ["Regular"] = 16639576416},
+            ["KO"] = "function: 0x00000000922955c7",
+            ["Use"] = "function: 0x00000000ca41984f"
+        },
+        ["Default"] = {
+            ["Animations"] = {
+                ["StartBody"] = 13633987968,
+                ["Loop"] = 13633987885,
+                ["End"] = 13633987926,
+                ["Start"] = 13633987842,
+                ["EndBody"] = 13633988074,
+                ["LoopBody"] = 13633988018
+            },
+            ["Use"] = "function: 0x0000000076e85907",
+            ["DelayTime"] = 8
+        }
+    },
+    ["Ultimates"] = {
+        ["Hitman"] = {
+            ["Animations"] = {["Victim"] = 13633643044, ["Character"] = 13633643142, ["Camera"] = 13633642900},
+            ["Use"] = "function: 0x00000000511e78b7"
+        },
+        ["Takamura"] = {
+            ["Animations"] = {["Victim"] = 13863260227, ["Character"] = 13863241980, ["Camera"] = 13863301078},
+            ["Use"] = "function: 0x00000000a10eb967"
+        },
+        ["Ghost"] = {
+            ["Animations"] = {["Victim"] = 14476924948, ["Character"] = 14476915136, ["Camera"] = 14476939570},
+            ["Use"] = "function: 0x00000000daa67007"
+        },
+        ["Date"] = {
+            ["Animations"] = {["Victim"] = 13913383554, ["Character"] = 13913257624, ["Camera"] = 13913485128},
+            ["Use"] = "function: 0x0000000063880587"
+        },
+        ["Hawk"] = {
+            ["Animations"] = {["Victim"] = 14155829549, ["Character"] = 14155921358, ["Camera"] = 14156013880},
+            ["Use"] = "function: 0x00000000bde7b107"
+        },
+        ["Sendo"] = {
+            ["Animations"] = {["Victim"] = 13653220063, ["Character"] = 13674991776, ["Camera"] = 13653250649},
+            ["Use"] = "function: 0x00000000b178eae7"
+        },
+        ["Basic"] = {
+            ["Animations"] = {["Victim"] = 13675509899, ["Character"] = 13675505052, ["Camera"] = 13675613432},
+            ["Use"] = "function: 0x00000000e874c327"
+        },
+        ["Saeki"] = {
+            ["Animations"] = {["Victim"] = 14000573433, ["Character"] = 14000549120, ["Camera"] = 14000612333},
+            ["Use"] = "function: 0x00000000d736b997"
+        },
+        ["LongGuard"] = {
+            ["Animations"] = {["Victim"] = 13674193999, ["Character"] = 13674180071, ["Camera"] = 13674217034},
+            ["Use"] = "function: 0x000000001b7dd607"
+        },
+        ["Charge"] = {
+            ["Animations"] = {["Victim"] = 16042735222, ["Character"] = 16042303622, ["Camera"] = 16042356517},
+            ["Use"] = "function: 0x00000000d8be8f07"
+        },
+        ["Freedom"] = {
+            ["Animations"] = {["Victim"] = 15644936251, ["Character"] = 15644927506, ["Camera"] = 15644945168},
+            ["Use"] = "function: 0x00000000be5d3c07"
+        },
+        ["Shotgun"] = {
+            ["Animations"] = {["Victim"] = 15464133732, ["Character"] = 15464123854, ["Camera"] = 15464146719},
+            ["Use"] = "function: 0x00000000047cf507"
+        },
+        ["Turtle"] = {
+            ["Animations"] = {["Victim"] = 15381025723, ["Character"] = 15380989343, ["Camera"] = 15381039211},
+            ["Use"] = "function: 0x00000000b79f6347"
+        },
+        ["Volk"] = {
+            ["Animations"] = {["Victim"] = 14090391953, ["Character"] = 14090389345, ["Camera"] = 14090376621},
+            ["Use"] = "function: 0x00000000f7b51d87"
+        },
+        ["Miyata"] = {
+            ["Animations"] = {["Victim"] = 13682527848, ["Character"] = 13682508488, ["Camera"] = 13682553454},
+            ["Use"] = "function: 0x0000000006080067"
+        },
+        ["IronFist"] = {
+            ["Animations"] = {["Victim"] = 15187266014, ["Character"] = 15187207937, ["Camera"] = 15187278475},
+            ["Use"] = "function: 0x00000000ea5b9607"
+        },
+        ["Bullet"] = {
+            ["Animations"] = {["Victim"] = 14849513622, ["Character"] = 14849520023, ["Camera"] = 14849530947},
+            ["Use"] = "function: 0x0000000073b81307"
+        },
+        ["Kimura"] = {
+            ["Animations"] = {["Victim"] = 14543480925, ["Character"] = 14543473653, ["Camera"] = 14543550973},
+            ["Use"] = "function: 0x000000001a1296a7"
+        },
+        ["Aoki"] = {
+            ["Animations"] = {["Victim"] = 13815810035, ["Character"] = 13815799083, ["Camera"] = 13815819808},
+            ["Use"] = "function: 0x00000000af8b2cc7"
+        },
+        ["Ippo"] = {
+            ["Animations"] = {["Victim"] = 13633643307, ["Character"] = 13676909167, ["Camera"] = 13633643211},
+            ["Use"] = "function: 0x000000009677f847"
+        }
+    }
+}
