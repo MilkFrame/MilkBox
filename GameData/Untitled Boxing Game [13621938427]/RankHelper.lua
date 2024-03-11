584848470884f5ec3f3f981f627f3650cb415ec0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-11 23:15:53 +1100

    Create RankHelper.lua

---
+++ GameData/Untitled Boxing Game [13621938427]/RankHelper.lua
@@ -0,0 +1,134 @@
+data = {
+    ["RankInfo"] = {
+        ["Underdog"] = {
+            ["Subtier"] = "2",
+            ["Title"] = "Underdog",
+            ["Tier"] = "Bronze",
+            ["Reward"] = {["Amount"] = 3, ["Type"] = "Spins"},
+            ["Rating"] = 2000,
+            ["ImageId"] = 14257245023,
+            ["Quality"] = 4
+        },
+        ["Prospect"] = {
+            ["Subtier"] = "1",
+            ["Title"] = "Prospect",
+            ["Tier"] = "Silver",
+            ["Reward"] = {["Amount"] = 2500, ["Type"] = "Cash"},
+            ["Rating"] = 6000,
+            ["ImageId"] = 14257247997,
+            ["Quality"] = 6
+        },
+        ["National"] = {
+            ["Subtier"] = "2",
+            ["Tier"] = "Gold",
+            ["Reward"] = {["Amount"] = 10, ["Type"] = "Spins"},
+            ["Rating"] = 12500,
+            ["ImageId"] = 14257262053,
+            ["Quality"] = 8
+        },
+        ["Undisputed Champion"] = {
+            ["Subtier"] = "3",
+            ["Tier"] = "Champion",
+            ["Reward"] = {
+                ["Type"] = "Gloves",
+                ["GloveName"] = "ChampionRanked",
+                ["GloveDisplayName"] = "CHAMPION GLOVES"
+            },
+            ["Rating"] = 24250,
+            ["ImageId"] = 14257270241,
+            ["Quality"] = 13
+        },
+        ["World Ranked"] = {
+            ["Subtier"] = "1",
+            ["Tier"] = "Diamond",
+            ["Reward"] = {["Amount"] = 15000, ["Type"] = "Cash"},
+            ["Rating"] = 16500,
+            ["ImageId"] = 14257267120,
+            ["Quality"] = 9
+        },
+        ["Unified Champion"] = {
+            ["Subtier"] = "2",
+            ["Tier"] = "Champion",
+            ["Reward"] = {["Amount"] = 50, ["Type"] = "Spins"},
+            ["Rating"] = 23000,
+            ["ImageId"] = 14292791435,
+            ["Quality"] = 12
+        },
+        ["Mandatory Challenger"] = {
+            ["Subtier"] = "2",
+            ["Tier"] = "Diamond",
+            ["Reward"] = {["Amount"] = 15, ["Type"] = "Spins"},
+            ["Rating"] = 18000,
+            ["ImageId"] = 14257267843,
+            ["Quality"] = 9
+        },
+        ["Gatekeeper"] = {
+            ["Subtier"] = "3",
+            ["Tier"] = "Bronze",
+            ["Reward"] = {["Type"] = "Gloves", ["GloveName"] = "BronzeRanked", ["GloveDisplayName"] = "BRONZE GLOVES"},
+            ["Rating"] = 4000,
+            ["ImageId"] = 14257247028,
+            ["Quality"] = 5
+        },
+        ["World Champion"] = {
+            ["Subtier"] = "1",
+            ["Tier"] = "Champion",
+            ["Reward"] = {["Amount"] = 35000, ["Type"] = "Cash"},
+            ["Rating"] = 21500,
+            ["ImageId"] = 14257269017,
+            ["Quality"] = 10
+        },
+        ["Interim Champion"] = {
+            ["Subtier"] = "3",
+            ["Tier"] = "Diamond",
+            ["Reward"] = {["Type"] = "Gloves", ["GloveName"] = "DiamondRanked", ["GloveDisplayName"] = "DIAMOND GLOVES"},
+            ["Rating"] = 20000,
+            ["ImageId"] = 14257268405,
+            ["Quality"] = 10
+        },
+        ["Contender"] = {
+            ["Subtier"] = "3",
+            ["Title"] = "Contender",
+            ["Tier"] = "Silver",
+            ["Reward"] = {["Type"] = "Gloves", ["GloveName"] = "SilverRanked", ["GloveDisplayName"] = "SILVER GLOVES"},
+            ["Rating"] = 9500,
+            ["ImageId"] = 14257251305,
+            ["Quality"] = 7
+        },
+        ["Prodigy"] = {
+            ["Subtier"] = "1",
+            ["Title"] = "Prodigy",
+            ["Tier"] = "Gold",
+            ["Reward"] = {["Amount"] = 7500, ["Type"] = "Cash"},
+            ["Rating"] = 11000,
+            ["ImageId"] = 14257256465,
+            ["Quality"] = 7
+        },
+        ["Continental"] = {
+            ["Subtier"] = "3",
+            ["Tier"] = "Gold",
+            ["Reward"] = {["Type"] = "Gloves", ["GloveName"] = "GoldRanked", ["GloveDisplayName"] = "GOLD GLOVES"},
+            ["Rating"] = 14000,
+            ["ImageId"] = 14257264734,
+            ["Quality"] = 8
+        },
+        ["Challenger"] = {
+            ["Subtier"] = "2",
+            ["Tier"] = "Silver",
+            ["Reward"] = {["Amount"] = 5, ["Type"] = "Spins"},
+            ["Rating"] = 8000,
+            ["ImageId"] = 14257250577,
+            ["Quality"] = 6
+        },
+        ["Journeyman"] = {
+            ["Subtier"] = "1",
+            ["Tier"] = "Bronze",
+            ["Reward"] = {["Amount"] = 500, ["Type"] = "Cash"},
+            ["Rating"] = 1000,
+            ["ImageId"] = 14257236784,
+            ["Quality"] = 3
+        }
+    },
+    ["GetRankInfoFromRating"] = "function: 0x000000002ca8247f",
+    ["GetNextRankInfoFromRating"] = "function: 0x00000000fb67db77"
+}
