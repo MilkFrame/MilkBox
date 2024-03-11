28dab24d8b10f51387813f44cd744821f3b8bff3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-11 23:19:02 +1100

    Create Gloves.lua

---
+++ GameData/Untitled Boxing Game [13621938427]/Gloves.lua
@@ -0,0 +1,138 @@
+data = {
+    ["Corruption"] = {["Metadata"] = {["Limited"] = true, ["Rarity"] = "Legendary", ["DisplayName"] = "Corruption"}},
+    ["Checkered"] = {["Metadata"] = {["Rarity"] = "Uncommon", ["DisplayName"] = "Checkered"}},
+    ["Desert"] = {["Metadata"] = {["Rarity"] = "Uncommon", ["DisplayName"] = "Desert"}},
+    ["Tiger"] = {["Metadata"] = {["Rarity"] = "Rare", ["DisplayName"] = "Tiger"}},
+    ["Socks"] = {["Metadata"] = {["Limited"] = true, ["Rarity"] = "Uncommon", ["DisplayName"] = "Socks"}},
+    ["Three"] = {
+        ["Metadata"] = {["Price"] = 21, ["SellPrice"] = 10000, ["Rarity"] = "Uncommon", ["DisplayName"] = "Magenta"}
+    },
+    ["Star"] = {["Metadata"] = {["NotPurchasable"] = true, ["Rarity"] = "Mythic", ["DisplayName"] = "Star"}},
+    ["Caution"] = {["Metadata"] = {["Rarity"] = "Uncommon", ["DisplayName"] = "Caution"}},
+    ["Krampus"] = {["Metadata"] = {["Limited"] = true, ["Rarity"] = "Legendary", ["DisplayName"] = "Krampus"}},
+    ["Classic"] = {["Metadata"] = {["Rarity"] = "Uncommon", ["DisplayName"] = "Classic"}},
+    ["Chimney"] = {["Metadata"] = {["Limited"] = true, ["Rarity"] = "Uncommon", ["DisplayName"] = "Chimney"}},
+    ["Emerald"] = {["Metadata"] = {["Rarity"] = "Rare", ["DisplayName"] = "Emerald"}},
+    ["Five"] = {
+        ["Metadata"] = {["Price"] = 500, ["SellPrice"] = 10000, ["Rarity"] = "Uncommon", ["DisplayName"] = "Blue"}
+    },
+    ["Cake"] = {["Metadata"] = {["Limited"] = true, ["Rarity"] = "Rare", ["DisplayName"] = "Cake"}},
+    ["Flowers"] = {["Metadata"] = {["Rarity"] = "Mythic", ["DisplayName"] = "Flowers"}},
+    ["Skull"] = {["Metadata"] = {["NotPurchasable"] = true, ["Rarity"] = "Mythic", ["DisplayName"] = "Skull"}},
+    ["Two"] = {
+        ["Metadata"] = {["Price"] = 23, ["SellPrice"] = 10000, ["Rarity"] = "Uncommon", ["DisplayName"] = "Green"}
+    },
+    ["Goo"] = {["Metadata"] = {["Rarity"] = "Uncommon", ["DisplayName"] = "Goo"}},
+    ["Bandages"] = {["Metadata"] = {["NotPurchasable"] = true, ["Rarity"] = "Legendary", ["DisplayName"] = "Bandages"}},
+    ["Quartz"] = {["Metadata"] = {["Rarity"] = "Rare", ["DisplayName"] = "Quartz"}},
+    ["Diamond"] = {["Metadata"] = {["Rarity"] = "Legendary", ["DisplayName"] = "Diamond"}},
+    ["Opal"] = {["Metadata"] = {["Rarity"] = "Uncommon", ["DisplayName"] = "Opal"}},
+    ["ChampionRanked"] = {
+        ["Metadata"] = {
+            ["CannotSell"] = true,
+            ["IsTradeable"] = false,
+            ["Rarity"] = "Legendary",
+            ["DisplayName"] = "Champion"
+        }
+    },
+    ["SilverRanked"] = {
+        ["Metadata"] = {
+            ["CannotSell"] = true,
+            ["IsTradeable"] = false,
+            ["Rarity"] = "Mythic",
+            ["DisplayName"] = "Patched Silver"
+        }
+    },
+    ["Six"] = {
+        ["Metadata"] = {["Price"] = 1500, ["SellPrice"] = 15000, ["Rarity"] = "Mythic", ["DisplayName"] = "White"}
+    },
+    ["Default"] = {
+        ["Metadata"] = {["Price"] = 500, ["SellPrice"] = 5000, ["Rarity"] = "Uncommon", ["DisplayName"] = "Default"}
+    },
+    ["Cloth"] = {["Metadata"] = {["Rarity"] = "Uncommon", ["DisplayName"] = "Cloth"}},
+    ["BronzeRanked"] = {
+        ["Metadata"] = {
+            ["CannotSell"] = true,
+            ["IsTradeable"] = false,
+            ["Rarity"] = "Rare",
+            ["DisplayName"] = "Torn Bronze"
+        }
+    },
+    ["October"] = {["Metadata"] = {["Rarity"] = "Uncommon", ["DisplayName"] = "October"}},
+    ["Sleek"] = {["Metadata"] = {["Rarity"] = "Uncommon", ["DisplayName"] = "Sleek"}},
+    ["Four"] = {
+        ["Metadata"] = {["Price"] = 500, ["SellPrice"] = 10000, ["Rarity"] = "Uncommon", ["DisplayName"] = "Yellow"}
+    },
+    ["Poison"] = {["Metadata"] = {["Rarity"] = "Uncommon", ["DisplayName"] = "Poison"}},
+    ["Gift"] = {["Metadata"] = {["Limited"] = true, ["Rarity"] = "Mythic", ["DisplayName"] = "Gift"}},
+    ["Fade"] = {["Metadata"] = {["Rarity"] = "Rare", ["DisplayName"] = "Fade"}},
+    ["Warped"] = {["Metadata"] = {["Limited"] = true, ["Rarity"] = "Mythic", ["DisplayName"] = "Warped"}},
+    ["GoldRanked"] = {
+        ["Metadata"] = {
+            ["CannotSell"] = true,
+            ["IsTradeable"] = false,
+            ["Rarity"] = "Mythic",
+            ["DisplayName"] = "Fused Gold"
+        }
+    },
+    ["Joker"] = {["Metadata"] = {["Limited"] = true, ["Rarity"] = "Mythic", ["DisplayName"] = "Joker"}},
+    ["Holly"] = {["Metadata"] = {["Limited"] = true, ["Rarity"] = "Uncommon", ["DisplayName"] = "Holly"}},
+    ["One"] = {
+        ["Metadata"] = {["Price"] = 500, ["SellPrice"] = 12000, ["Rarity"] = "Mythic", ["DisplayName"] = "Black"}
+    },
+    ["NewRed"] = {
+        ["Metadata"] = {["SellPrice"] = 100000, ["Rarity"] = "Uncommon", ["DisplayName"] = "[BETA] New Default"}
+    },
+    ["Flame"] = {["Metadata"] = {["Rarity"] = "Uncommon", ["DisplayName"] = "Flame"}},
+    ["Spikes"] = {["Metadata"] = {["NotPurchasable"] = true, ["Rarity"] = "Mythic", ["DisplayName"] = "Spikes"}},
+    ["NeonOni"] = {["Metadata"] = {["Rarity"] = "Mythic", ["DisplayName"] = "Neon Oni"}},
+    ["Candycane"] = {["Metadata"] = {["Limited"] = true, ["Rarity"] = "Rare", ["DisplayName"] = "Candycane"}},
+    ["Convict"] = {["Metadata"] = {["Rarity"] = "Rare", ["DisplayName"] = "Convict"}},
+    ["Moth"] = {["Metadata"] = {["Limited"] = true, ["Rarity"] = "Rare", ["DisplayName"] = "Moth"}},
+    ["Jungle"] = {["Metadata"] = {["Rarity"] = "Uncommon", ["DisplayName"] = "Jungle"}},
+    ["Firecracker"] = {["Metadata"] = {["Limited"] = true, ["Rarity"] = "Rare", ["DisplayName"] = "Firecracker"}},
+    ["Snowflake"] = {["Metadata"] = {["Limited"] = true, ["Rarity"] = "Uncommon", ["DisplayName"] = "Snowflake"}},
+    ["Dragon"] = {
+        ["Metadata"] = {["NotPurchasable"] = true, ["Rarity"] = "Legendary", ["DisplayName"] = "Dragon Scales"}
+    },
+    ["Obsidian"] = {["Metadata"] = {["Rarity"] = "Mythic", ["DisplayName"] = "Obsidian"}},
+    ["Splash"] = {["Metadata"] = {["NotPurchasable"] = true, ["Rarity"] = "Rare", ["DisplayName"] = "Splash"}},
+    ["King"] = {["Metadata"] = {["NotPurchasable"] = true, ["Rarity"] = "Mythic", ["DisplayName"] = "King"}},
+    ["DiamondRanked"] = {
+        ["Metadata"] = {
+            ["CannotSell"] = true,
+            ["IsTradeable"] = false,
+            ["Rarity"] = "Legendary",
+            ["DisplayName"] = "Ultra Diamond"
+        }
+    },
+    ["Waves"] = {["Metadata"] = {["Rarity"] = "Uncommon", ["DisplayName"] = "Waves"}},
+    ["Beowulf"] = {
+        ["Metadata"] = {
+            ["SellPrice"] = 50000,
+            ["NotPurchasable"] = true,
+            ["Rarity"] = "Legendary",
+            ["DisplayName"] = "Beowulf"
+        }
+    },
+    ["Crystal"] = {["Metadata"] = {["Rarity"] = "Rare", ["DisplayName"] = "Crystal"}},
+    ["SciFi"] = {["Metadata"] = {["NotPurchasable"] = true, ["Rarity"] = "Rare", ["DisplayName"] = "SciFi"}},
+    ["Spider"] = {["Metadata"] = {["Limited"] = true, ["Rarity"] = "Rare", ["DisplayName"] = "Spider"}},
+    ["Carbon"] = {["Metadata"] = {["Rarity"] = "Uncommon", ["DisplayName"] = "Carbon"}},
+    ["NewBrown"] = {
+        ["Metadata"] = {["SellPrice"] = 200000, ["Rarity"] = "Legendary", ["DisplayName"] = "[BETA] Carbon"}
+    },
+    ["Knitting"] = {["Metadata"] = {["Limited"] = true, ["Rarity"] = "Uncommon", ["DisplayName"] = "Knitting"}},
+    ["Turbulence"] = {["Metadata"] = {["Rarity"] = "Rare", ["DisplayName"] = "Turbulence"}},
+    ["Stitch"] = {
+        ["Metadata"] = {["NotPurchasable"] = true, ["Rarity"] = "Uncommon", ["DisplayName"] = "Purple Stitch"}
+    },
+    ["NewBlack"] = {["Metadata"] = {["SellPrice"] = 150000, ["Rarity"] = "Rare", ["DisplayName"] = "[BETA] Sleek"}},
+    ["Bismuth"] = {["Metadata"] = {["Rarity"] = "Mythic", ["DisplayName"] = "Bismuth"}},
+    ["Splatter"] = {["Metadata"] = {["Rarity"] = "Uncommon", ["DisplayName"] = "Splatter"}},
+    ["Bloody"] = {["Metadata"] = {["Rarity"] = "Rare", ["DisplayName"] = "Bloody"}},
+    ["Camo"] = {["Metadata"] = {["NotPurchasable"] = true, ["Rarity"] = "Uncommon", ["DisplayName"] = "Camo"}},
+    ["Checkers"] = {["Metadata"] = {["NotPurchasable"] = true, ["Rarity"] = "Uncommon", ["DisplayName"] = "Checkers"}},
+    ["Haunted"] = {["Metadata"] = {["Limited"] = true, ["Rarity"] = "Rare", ["DisplayName"] = "Haunted"}},
+    ["Bats"] = {["Metadata"] = {["Limited"] = true, ["Rarity"] = "Mythic", ["DisplayName"] = "Bats"}}
+}
