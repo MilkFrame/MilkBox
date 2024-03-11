14a61677338188af76926e1d4571377c5aaedfd6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-11 23:18:40 +1100

    Create Emotes.lua

---
+++ GameData/Untitled Boxing Game [13621938427]/Emotes.lua
@@ -0,0 +1,360 @@
+data = {
+    ["FridayNight"] = {
+        ["Use"] = "function: 0x000000002c5c8167",
+        ["Metadata"] = {
+            ["Rarity"] = "Rare",
+            ["MainId"] = 15837814629,
+            ["Name"] = "FridayNight",
+            ["DisplayName"] = "Friday Night"
+        }
+    },
+    ["VirtualInsanity"] = {
+        ["Use"] = "function: 0x000000006b34a5b7",
+        ["Metadata"] = {
+            ["Name"] = "VirtualInsanity",
+            ["LoopId"] = 15569981501,
+            ["Rarity"] = "Mythic",
+            ["MainId"] = 15569973306,
+            ["DisplayName"] = "Virtual Insanity"
+        }
+    },
+    ["Penguin"] = {
+        ["Use"] = "function: 0x000000008ba9b287",
+        ["Metadata"] = {
+            ["Rarity"] = "Rare",
+            ["MainId"] = 15851112795,
+            ["Name"] = "Penguin",
+            ["DisplayName"] = "Penguin"
+        }
+    },
+    ["OneArmPushup"] = {
+        ["Use"] = "function: 0x0000000024c48367",
+        ["Metadata"] = {
+            ["StartId"] = 15852958283,
+            ["Name"] = "OneArmPushup",
+            ["Rarity"] = "Rare",
+            ["MainId"] = 15852963601,
+            ["DisplayName"] = "One-Arm Pushup"
+        }
+    },
+    ["Karaoke"] = {
+        ["Use"] = "function: 0x00000000adddf127",
+        ["Metadata"] = {
+            ["Rarity"] = "Rare",
+            ["MainId"] = 15570949446,
+            ["Name"] = "Karaoke",
+            ["DisplayName"] = "Karaoke"
+        }
+    },
+    ["Laugh"] = {
+        ["Use"] = "function: 0x00000000ab3844f7",
+        ["Metadata"] = {
+            ["Name"] = "Laugh",
+            ["Rarity"] = "Rare",
+            ["MainId"] = 15581407864,
+            ["Toxicity"] = 2,
+            ["DisplayName"] = "Laugh"
+        }
+    },
+    ["SoldierOfDance"] = {
+        ["Use"] = "function: 0x00000000cbaf8547",
+        ["Metadata"] = {
+            ["StartId"] = 15838117241,
+            ["Name"] = "SoldierOfDance",
+            ["Rarity"] = "Rare",
+            ["MainId"] = 15838119382,
+            ["DisplayName"] = "Soldier of Dance"
+        }
+    },
+    ["Kickback"] = {
+        ["Use"] = "function: 0x00000000fec25fa7",
+        ["Metadata"] = {
+            ["Rarity"] = "Rare",
+            ["MainId"] = 15851171997,
+            ["Name"] = "Kickback",
+            ["DisplayName"] = "Kickback"
+        }
+    },
+    ["Phone"] = {
+        ["Use"] = "function: 0x00000000a35174e7",
+        ["Metadata"] = {
+            ["FOVId"] = 15864271539,
+            ["Name"] = "Phone",
+            ["Rarity"] = "Legendary",
+            ["CameraId"] = 15864157061,
+            ["MainId"] = 15851579084,
+            ["DisplayName"] = "Phone Summon"
+        }
+    },
+    ["TPose"] = {
+        ["Use"] = "function: 0x000000001dd2be37",
+        ["Metadata"] = {["Rarity"] = "Rare", ["MainId"] = 15864463830, ["Name"] = "TPose", ["DisplayName"] = "T-Pose"}
+    },
+    ["Akuma"] = {
+        ["Use"] = "function: 0x0000000087ade347",
+        ["Metadata"] = {
+            ["StartId"] = 15582700649,
+            ["Name"] = "Akuma",
+            ["Rarity"] = "Rare",
+            ["MainId"] = 15582703234,
+            ["DisplayName"] = "Akuma"
+        }
+    },
+    ["Facepalm"] = {
+        ["Use"] = "function: 0x00000000db3c0c07",
+        ["Metadata"] = {
+            ["Name"] = "Facepalm",
+            ["Rarity"] = "Rare",
+            ["MainId"] = 15578091501,
+            ["Toxicity"] = 1,
+            ["DisplayName"] = "Facepalm"
+        }
+    },
+    ["Hakari"] = {
+        ["Use"] = "function: 0x000000006c4f6517",
+        ["Metadata"] = {
+            ["Name"] = "Hakari",
+            ["Rarity"] = "Mythic",
+            ["MainId"] = 15853182525,
+            ["Toxicity"] = 1,
+            ["DisplayName"] = "Hakari"
+        }
+    },
+    ["BetterCall"] = {
+        ["Use"] = "function: 0x00000000d454aa07",
+        ["Metadata"] = {
+            ["Name"] = "BetterCall",
+            ["LoopId"] = 15582401530,
+            ["Rarity"] = "Mythic",
+            ["MainId"] = 15582398415,
+            ["DisplayName"] = "Better Call"
+        }
+    },
+    ["Chair"] = {
+        ["Use"] = "function: 0x0000000037fe2a57",
+        ["Metadata"] = {
+            ["Rarity"] = "Mythic",
+            ["MainId"] = 15564818855,
+            ["Name"] = "Chair",
+            ["DisplayName"] = "Motivation Chair"
+        }
+    },
+    ["Whistle"] = {
+        ["Use"] = "function: 0x00000000cc2871e7",
+        ["Metadata"] = {
+            ["Name"] = "Whistle",
+            ["LoopId"] = 15562514749,
+            ["Rarity"] = "Mythic",
+            ["MainId"] = 15562348008,
+            ["DisplayName"] = "Whistle"
+        }
+    },
+    ["PointUp"] = {
+        ["Use"] = "function: 0x00000000210ef4f7",
+        ["Metadata"] = {
+            ["Rarity"] = "Rare",
+            ["MainId"] = 15563486740,
+            ["Name"] = "PointUp",
+            ["DisplayName"] = "Raise Hand"
+        }
+    },
+    ["Cinderella"] = {
+        ["Use"] = "function: 0x00000000e74dd067",
+        ["Metadata"] = {
+            ["StartId"] = 15862648223,
+            ["Name"] = "Cinderella",
+            ["Rarity"] = "Mythic",
+            ["MainId"] = 15862653618,
+            ["DisplayName"] = "24-Hour Cinderella"
+        }
+    },
+    ["TakeYourTime"] = {
+        ["Use"] = "function: 0x000000002402b627",
+        ["Metadata"] = {
+            ["Name"] = "TakeYourTime",
+            ["Rarity"] = "Rare",
+            ["MainId"] = 15862059432,
+            ["Toxicity"] = 2,
+            ["DisplayName"] = "Take Your Time"
+        }
+    },
+    ["Slickback"] = {
+        ["Use"] = "function: 0x00000000fb701077",
+        ["Metadata"] = {
+            ["Rarity"] = "Rare",
+            ["MainId"] = 16300039798,
+            ["Name"] = "Slickback",
+            ["DisplayName"] = "Slickback"
+        }
+    },
+    ["Cookout"] = {
+        ["Use"] = "function: 0x00000000f04c0687",
+        ["Metadata"] = {
+            ["Rarity"] = "Rare",
+            ["MainId"] = 15851435247,
+            ["Name"] = "Cookout",
+            ["DisplayName"] = "Cookout"
+        }
+    },
+    ["Inspect"] = {
+        ["Use"] = "function: 0x000000003b3df337",
+        ["Metadata"] = {
+            ["StartId"] = 15557002222,
+            ["Name"] = "Inspect",
+            ["Rarity"] = "Rare",
+            ["MainId"] = 15557017812,
+            ["DisplayName"] = "Inspect"
+        }
+    },
+    ["Shakedown"] = {
+        ["Use"] = "function: 0x00000000057ca307",
+        ["Metadata"] = {
+            ["Rarity"] = "Rare",
+            ["MainId"] = 16300075476,
+            ["Name"] = "Shakedown",
+            ["DisplayName"] = "Shakedown"
+        }
+    },
+    ["TakeTheL"] = {
+        ["Use"] = "function: 0x00000000b19b2767",
+        ["Metadata"] = {
+            ["Name"] = "TakeTheL",
+            ["Rarity"] = "Mythic",
+            ["MainId"] = 15556521774,
+            ["Toxicity"] = 2,
+            ["DisplayName"] = "Take the L"
+        }
+    },
+    ["Shadowboxing"] = {
+        ["Use"] = "function: 0x00000000617123b7",
+        ["Metadata"] = {
+            ["Rarity"] = "Rare",
+            ["MainId"] = 15590031538,
+            ["Name"] = "Shadowboxing",
+            ["DisplayName"] = "Shadowboxing"
+        }
+    },
+    ["WindUp"] = {
+        ["Use"] = "function: 0x00000000533f5887",
+        ["Metadata"] = {["Rarity"] = "Rare", ["MainId"] = 15563717483, ["Name"] = "WindUp", ["DisplayName"] = "Wind Up"}
+    },
+    ["Guitar"] = {
+        ["Use"] = "function: 0x00000000302589a7",
+        ["Metadata"] = {
+            ["Name"] = "Guitar",
+            ["Rarity"] = "Legendary",
+            ["LoopId"] = 13633642843,
+            ["Toxicity"] = 2,
+            ["MainId"] = 15578484225,
+            ["DisplayName"] = "Guitar"
+        }
+    },
+    ["Lookaway"] = {
+        ["Use"] = "function: 0x00000000399f1cc7",
+        ["Metadata"] = {
+            ["Rarity"] = "Rare",
+            ["MainId"] = 15557390767,
+            ["Name"] = "Lookaway",
+            ["DisplayName"] = "Lookaway"
+        }
+    },
+    ["PBJT"] = {
+        ["Use"] = "function: 0x00000000c2daa8c7",
+        ["Metadata"] = {["Rarity"] = "Rare", ["MainId"] = 16561261755, ["Name"] = "PBJT", ["DisplayName"] = "PBJT"}
+    },
+    ["Sturdy"] = {
+        ["Use"] = "function: 0x0000000025356a67",
+        ["Metadata"] = {["Rarity"] = "Rare", ["MainId"] = 16300044316, ["Name"] = "Sturdy", ["DisplayName"] = "Sturdy"}
+    },
+    ["Gear2"] = {
+        ["Use"] = "function: 0x0000000072d48407",
+        ["Metadata"] = {
+            ["StartId"] = 15582802070,
+            ["Name"] = "Gear2",
+            ["Rarity"] = "Mythic",
+            ["MainId"] = 15582807030,
+            ["DisplayName"] = "Gear Second"
+        }
+    },
+    ["UltimateApology"] = {
+        ["Use"] = "function: 0x00000000e2c9f047",
+        ["Metadata"] = {
+            ["Rarity"] = "Mythic",
+            ["MainId"] = 15837323254,
+            ["Name"] = "UltimateApology",
+            ["DisplayName"] = "Ultimate Apology"
+        }
+    },
+    ["Caged"] = {
+        ["Use"] = "function: 0x00000000d5568ce7",
+        ["Metadata"] = {["Rarity"] = "Rare", ["MainId"] = 15590331778, ["Name"] = "Caged", ["DisplayName"] = "Caged"}
+    },
+    ["Geddan"] = {
+        ["Use"] = "function: 0x00000000212fe4a7",
+        ["Metadata"] = {
+            ["Rarity"] = "Mythic",
+            ["MainId"] = 16561483484,
+            ["Name"] = "Geddan",
+            ["DisplayName"] = "Geddan"
+        }
+    },
+    ["Default"] = {
+        ["Use"] = "function: 0x00000000bef9eb77",
+        ["Metadata"] = {
+            ["Rarity"] = "Uncommon",
+            ["MainId"] = 15582137915,
+            ["Name"] = "Default",
+            ["DisplayName"] = "Standing Bow"
+        }
+    },
+    ["Heart"] = {
+        ["Use"] = "function: 0x000000000071ff97",
+        ["Metadata"] = {
+            ["StartId"] = 15583155107,
+            ["Name"] = "Heart",
+            ["Rarity"] = "Rare",
+            ["MainId"] = 15583160823,
+            ["DisplayName"] = "Heart"
+        }
+    },
+    ["Lagtrain"] = {
+        ["Use"] = "function: 0x00000000f202b5c7",
+        ["Metadata"] = {
+            ["Rarity"] = "Mythic",
+            ["MainId"] = 15591140210,
+            ["Name"] = "Lagtrain",
+            ["DisplayName"] = "Lagtrain"
+        }
+    },
+    ["Torture"] = {
+        ["Use"] = "function: 0x000000004541f077",
+        ["Metadata"] = {
+            ["Rarity"] = "Mythic",
+            ["MainId"] = 15851270373,
+            ["Name"] = "Torture",
+            ["DisplayName"] = "Torture"
+        }
+    },
+    ["Seahorse"] = {
+        ["Use"] = "function: 0x00000000ab828a27",
+        ["Metadata"] = {
+            ["Name"] = "Seahorse",
+            ["Rarity"] = "Mythic",
+            ["VisualId"] = 15850154564,
+            ["SeahorseId"] = 15850643062,
+            ["MainId"] = 15850178624,
+            ["DisplayName"] = "Seahorse"
+        }
+    },
+    ["DeathNote"] = {
+        ["Use"] = "function: 0x0000000076cb4ad7",
+        ["Metadata"] = {
+            ["BookId"] = 15839205398,
+            ["Name"] = "DeathNote",
+            ["Rarity"] = "Mythic",
+            ["MainId"] = 15839006721,
+            ["Toxicity"] = 1,
+            ["DisplayName"] = "Death Note"
+        }
+    }
+}
