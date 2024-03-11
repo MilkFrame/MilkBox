61a5c9e68cc5cfd353c9663f1decac77f5acc388
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-11 23:09:31 +1100

    Create Mermaid.lua

---
+++ GameData/Royale High [735030788]/Mermaid.lua
@@ -0,0 +1,362 @@
+data = {
+    ["Elegant Sapphire Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 3250,
+        ["Alias"] = "Elegant Sapphire Mermaid",
+        ["Icon"] = "rbxassetid://1091062093",
+        ["Unlisted"] = true
+    },
+    ["Purest Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 2500,
+        ["Alias"] = "Purest Merman",
+        ["Icon"] = "rbxassetid://1091053446",
+        ["Unlisted"] = true
+    },
+    ["Cotton Candy Coral Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 9000,
+        ["Alias"] = "Cotton Candy Coral Merman",
+        ["Icon"] = "rbxassetid://1091066320",
+        ["Unlisted"] = true
+    },
+    ["Barbie Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 5500,
+        ["Alias"] = "Barbie Mermaid",
+        ["Icon"] = "rbxassetid://1091061815",
+        ["Unlisted"] = true
+    },
+    ["Amythest Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 500,
+        ["Alias"] = "Amythest Mermaid",
+        ["Icon"] = "rbxassetid://1091061537",
+        ["Unlisted"] = true
+    },
+    ["Evil and Nice Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 8000,
+        ["Alias"] = "Evil and Nice Mermaid",
+        ["Icon"] = "rbxassetid://1091065005",
+        ["Unlisted"] = true
+    },
+    ["Popular Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 10000,
+        ["Alias"] = "Popular Merman",
+        ["Icon"] = "rbxassetid://1091053648",
+        ["Unlisted"] = true
+    },
+    ["Darkest Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 8500,
+        ["Alias"] = "Darkest Mermaid",
+        ["Icon"] = "rbxassetid://1091064779",
+        ["Unlisted"] = true
+    },
+    ["Passionate Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 3500,
+        ["Alias"] = "Passionate Merman",
+        ["Icon"] = "rbxassetid://1091064369",
+        ["Unlisted"] = true
+    },
+    ["Shy Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 4000,
+        ["Alias"] = "Shy Merman",
+        ["Icon"] = "rbxassetid://1091053772",
+        ["Unlisted"] = true
+    },
+    ["Darkest Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 8500,
+        ["Alias"] = "Darkest Merman",
+        ["Icon"] = "rbxassetid://1091064779",
+        ["Unlisted"] = true
+    },
+    ["Urchin Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 800,
+        ["Alias"] = "Urchin Mermaid",
+        ["Icon"] = "rbxassetid://1091061023",
+        ["Unlisted"] = true
+    },
+    ["Neon Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 12000,
+        ["Alias"] = "Neon Mermaid",
+        ["Icon"] = "rbxassetid://1091066210",
+        ["Unlisted"] = true
+    },
+    ["Pond Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 1500,
+        ["Alias"] = "Pond Merman",
+        ["Icon"] = "rbxassetid://1091064369",
+        ["Unlisted"] = true
+    },
+    ["Purest Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 2500,
+        ["Alias"] = "Purest Mermaid",
+        ["Icon"] = "rbxassetid://1091053446",
+        ["Unlisted"] = true
+    },
+    ["Fire Rubies Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 4000,
+        ["Alias"] = "Fire Rubies Merman",
+        ["Icon"] = "rbxassetid://1091054106",
+        ["Unlisted"] = true
+    },
+    ["Amythest Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 500,
+        ["Alias"] = "Amythest Merman",
+        ["Icon"] = "rbxassetid://1091061537",
+        ["Unlisted"] = true
+    },
+    ["Royal of the Ocean Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 4500,
+        ["Alias"] = "Royal of the Ocean Merman",
+        ["Icon"] = "rbxassetid://1091063298",
+        ["Unlisted"] = true
+    },
+    ["Barbie Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 5500,
+        ["Alias"] = "Barbie Merman",
+        ["Icon"] = "rbxassetid://1091061815",
+        ["Unlisted"] = true
+    },
+    ["Candy Sweet Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 3500,
+        ["Alias"] = "Candy Sweet Mermaid",
+        ["Icon"] = "rbxassetid://1091053944",
+        ["Unlisted"] = true
+    },
+    ["Blue World Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 5500,
+        ["Alias"] = "Blue World Merman",
+        ["Icon"] = "rbxassetid://1091061198",
+        ["Unlisted"] = true
+    },
+    ["Fire Rubies Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 4000,
+        ["Alias"] = "Fire Rubies Mermaid",
+        ["Icon"] = "rbxassetid://1091054106",
+        ["Unlisted"] = true
+    },
+    ["Evil and Nice Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 8000,
+        ["Alias"] = "Evil and Nice Merman",
+        ["Icon"] = "rbxassetid://1091065005",
+        ["Unlisted"] = true
+    },
+    ["Nature Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 300,
+        ["Alias"] = "Nature Mermaid",
+        ["Icon"] = "rbxassetid://1091059030",
+        ["Unlisted"] = true
+    },
+    ["Candy Sweet Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 3500,
+        ["Alias"] = "Candy Sweet Merman",
+        ["Icon"] = "rbxassetid://1091053944",
+        ["Unlisted"] = true
+    },
+    ["Passionate Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 3500,
+        ["Alias"] = "Passionate Mermaid",
+        ["Icon"] = "rbxassetid://1091058499",
+        ["Unlisted"] = true
+    },
+    ["Popular Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 10000,
+        ["Alias"] = "Popular Mermaid",
+        ["Icon"] = "rbxassetid://1091053648",
+        ["Unlisted"] = true
+    },
+    ["Littlest Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 7500,
+        ["Alias"] = "Littlest Mermaid",
+        ["Icon"] = "rbxassetid://1091063770",
+        ["Unlisted"] = true
+    },
+    ["Fiercest Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 2500,
+        ["Alias"] = "Fiercest Mermaid",
+        ["Icon"] = "rbxassetid://1091064916",
+        ["Unlisted"] = true
+    },
+    ["Fiercest Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 2500,
+        ["Alias"] = "Fiercest Merman",
+        ["Icon"] = "rbxassetid://1091064916",
+        ["Unlisted"] = true
+    },
+    ["Neon Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 12000,
+        ["Alias"] = "Neon Merman",
+        ["Icon"] = "rbxassetid://1091066210",
+        ["Unlisted"] = true
+    },
+    ["Nature Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 300,
+        ["Alias"] = "Nature Merman",
+        ["Icon"] = "rbxassetid://1091059030",
+        ["Unlisted"] = true
+    },
+    ["Shy Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 4000,
+        ["Alias"] = "Shy Mermaid",
+        ["Icon"] = "rbxassetid://1091053772",
+        ["Unlisted"] = true
+    },
+    ["Elegant Sapphire Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 3250,
+        ["Alias"] = "Elegant Sapphire Merman",
+        ["Icon"] = "rbxassetid://1091062093",
+        ["Unlisted"] = true
+    },
+    ["Pond Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 1500,
+        ["Alias"] = "Pond Mermaid",
+        ["Icon"] = "rbxassetid://1091064369",
+        ["Unlisted"] = true
+    },
+    ["Littlest Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 7500,
+        ["Alias"] = "Littlest Merman",
+        ["Icon"] = "rbxassetid://1091063770",
+        ["Unlisted"] = true
+    },
+    ["Urchin Merman"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 800,
+        ["Alias"] = "Urchin Merman",
+        ["Icon"] = "rbxassetid://1091061023",
+        ["Unlisted"] = true
+    },
+    ["Royal of the Ocean Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 4500,
+        ["Alias"] = "Royal of the Ocean Mermaid",
+        ["Icon"] = "rbxassetid://1091063298",
+        ["Unlisted"] = true
+    },
+    ["Cotton Candy Coral Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 9000,
+        ["Alias"] = "Cotton Candy Coral Mermaid",
+        ["Icon"] = "rbxassetid://1091066320",
+        ["Unlisted"] = true
+    },
+    ["Blue World Mermaid"] = {
+        ["NotTradable"] = true,
+        ["NumColorGroups"] = 0,
+        ["category"] = "Mermaid",
+        ["Price"] = 5500,
+        ["Alias"] = "Blue World Mermaid",
+        ["Icon"] = "rbxassetid://1091061198",
+        ["Unlisted"] = true
+    }
+}
