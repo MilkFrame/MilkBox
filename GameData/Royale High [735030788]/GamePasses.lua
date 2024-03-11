5b8a5b3ab8f1017740d65cc93de3a6bbf84dd3db
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-11 23:12:25 +1100

    Create GamePasses.lua

---
+++ GameData/Royale High [735030788]/GamePasses.lua
@@ -0,0 +1,59 @@
+data = {
+    ["ExtraDiamonds4x"] = {
+        ["Price"] = 1649,
+        ["PassId"] = 3457593,
+        ["Description"] = "Get FOUR times as many diamonds when you level up + collect diamonds in Royale High & the lobby! (1200 instead of 300 when leveling up) *If you own both the 2x and 4x pass, you get 6x, 1800 diamonds every level*",
+        ["DisplayName"] = "4x More Diamonds on Level Up!"
+    },
+    ["CrystalBall"] = {
+        ["PassId"] = 6316501,
+        ["Description"] = "Look into your Crystal Ball🔮 and see ANYWHERE on campus without actually being there! Specially created with Photo Editors in mind, this pass gives you a free camera/vision to capture impossible angles you couldn't reach before. INSTRUCTIONS: NOT FOR MOBILE/IPAD/XBOX. Do Shift+P to activate/exit. Use WASD to move the cam. Hold Shift+W to slow down for accuracy. Have fun!",
+        ["Price"] = 300,
+        ["CheckImmediately"] = true,
+        ["DisplayName"] = "🔮Crystal Ball Power🔮"
+    },
+    ["ProfileStickers"] = {
+        ["Price"] = 50,
+        ["PassId"] = 10111433,
+        ["Description"] = "18 themed Sticker Packs with 176 different fun stickers (and growing) to decorate your Profile Journal with!",
+        ["DisplayName"] = "Sticker Packs Pass!"
+    },
+    ["FlyonEarth"] = {
+        ["Retired"] = true,
+        ["Description"] = "Only the most powerful of creatures do not let a silly blue and green planet hinder their flight abilities! You might even be able to reach the Princess Tower..",
+        ["Price"] = 80,
+        ["PassId"] = 5350675,
+        ["DisplayName"] = "Fly on Earth!"
+    },
+    ["ExtraDiamonds"] = {
+        ["Price"] = 749,
+        ["PassId"] = 3455446,
+        ["Description"] = "Get twice as many diamonds when you level up + collect diamonds in Royale High & the lobby! (600 instead of 300 when leveling up) *If you own both the 2x and 4x pass, you get 6x, 1800 diamonds every level*",
+        ["DisplayName"] = "2x More Diamonds on Level Up!"
+    },
+    ["LockerPaint"] = {
+        ["Price"] = 289,
+        ["PassId"] = 3457412,
+        ["Description"] = "Show off how unique you are with a custom-colored roleplay name, colorful profile, and paint your locker as many times as you want! NEW: Paint your phone case and keyboard keys!",
+        ["DisplayName"] = "Paintbrush Pass!"
+    },
+    ["HairPass"] = {
+        ["Price"] = 50,
+        ["PassId"] = 4097864,
+        ["Description"] = "Recolor your hair with 90+ new colors, including ombres, highlights, rainbow shades, and more! I am constantly adding to this pass to put more fun color shades in!",
+        ["DisplayName"] = "Specialty Hair Color Pass"
+    },
+    ["FlySpeed"] = {
+        ["Price"] = 249,
+        ["PassId"] = 3436552,
+        ["Description"] = "Unlock a meter which you can use to freely adjust your flight speed!",
+        ["DisplayName"] = "Faster Flight + Custom Speeds!"
+    },
+    ["FabricPass"] = {
+        ["PassId"] = 5585682,
+        ["Description"] = "Unlock 88+ more fabric designs for your skirts/heels with this pass! Fabrics range from velvety textures, animal prints, pretty plaids, space and ocean scenery, pizza, and many others!",
+        ["Price"] = 50,
+        ["CheckImmediately"] = true,
+        ["DisplayName"] = "Special Fabric Designs✂"
+    }
+}
