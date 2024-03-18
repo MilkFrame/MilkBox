548a8fd06fb6f59ebd5020e206b5edebbbf1354e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:11:04 +1100

    Create SpinToWinModule.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/SpinToWinModule.lua
@@ -0,0 +1,18 @@
+data = {
+    ["TIME_TO_STOP"] = 5.333333333333334,
+    ["SPIN_SLOWDOWN"] = 0.3,
+    ["DIST_TO_STOP"] = 4.2666666666666675,
+    ["GetHalfDay"] = "function: 0x00000000ffaa3938",
+    ["SPIN_VELOCITY"] = 1.6,
+    ["Rewards"] = {
+        {"Boost", "2xLuck", 3600},
+        {"Boost", "3xShinyChance", 3600},
+        {"Boost", "2xXP", 3600},
+        {"Potion", "EnchantMax"},
+        {"Potion", "Level1"},
+        {"Potion", "LevelMax"},
+        {"Potion", "Enchant1"},
+        {"Boost", "2xHatchSpeed", 3600}
+    },
+    ["GetTimeUntilNextHalfDay"] = "function: 0x000000003773cfd0"
+}
