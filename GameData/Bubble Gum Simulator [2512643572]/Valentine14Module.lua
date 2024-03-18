2509a566da419d8d72547039e8761d1a0e6c11cc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:12:59 +1100

    Create Valentine14Module.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/Valentine14Module.lua
@@ -0,0 +1,24 @@
+data = {
+    ["GetBaseData"] = "function: 0x000000008b1a5b58",
+    ["UnlockAllId"] = 1150545415,
+    ["UnlockOneId"] = 1150545348,
+    ["GetDayStringForIndex"] = "function: 0x00000000cc9579c0",
+    ["GetDayIndex"] = "function: 0x000000008d322070",
+    ["Days"] = {
+        {["Reward"] = {"Potion", "Level1"}, ["Challenge"] = {"Eggs", 750}},
+        {["Reward"] = {"Potion", "Enchant1"}, ["Challenge"] = {"Bubbles", 10000000}},
+        {["Reward"] = {"Boost", "2xLuck", 5400}, ["Challenge"] = {"Legendaries", 10}},
+        {["Reward"] = {"Boost", "2xHatchSpeed", 5400}, ["Challenge"] = {"Gems", 750000000}},
+        {["Reward"] = {"Potion", "Level1"}, ["Challenge"] = {"Bubbles", 75000000}},
+        {["Reward"] = {"Potion", "Enchant1"}, ["Challenge"] = {"Eggs", 2000}},
+        {["Reward"] = {"Boost", "2xHatchSpeed", 7200}, ["Challenge"] = {"Epics", 750}},
+        {["Reward"] = {"Boost", "2xLuck", 7200}, ["Challenge"] = {"Bubbles", 250000000}},
+        {["Reward"] = {"Boost", "2xHatchSpeed", 14400}, ["Challenge"] = {"Magma", 1250000000}},
+        {["Reward"] = {"Boost", "2xLuck", 14400}, ["Challenge"] = {"Bubbles", 1000000000}},
+        {["Reward"] = {"Potion", "EnchantMax"}, ["Challenge"] = {"Eggs", 4000}},
+        {["Reward"] = {"Potion", "ShadowMax"}, ["Challenge"] = {"Shells", 4000000000}},
+        {["Reward"] = {"Potion", "EnchantBubbles"}, ["Challenge"] = {"Legendaries", 75}},
+        {["Reward"] = {"Pet", "Bruhette"}, ["Challenge"] = {"Pearls", 7500000000}}
+    },
+    ["GetLastFinishedDay"] = "function: 0x00000000ac969c48"
+}
