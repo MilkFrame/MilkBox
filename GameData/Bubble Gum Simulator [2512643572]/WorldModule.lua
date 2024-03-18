2f9e4dbdf7c42b5b55ec26e85ff6c867a3d356ec
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:08:26 +1100

    Create WorldModule.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/WorldModule.lua
@@ -0,0 +1,9 @@
+data = {
+    ["Rainbow Land"] = {"Coins", 1500000000},
+    ["Mystic Forest"] = {"Coins", 4000000000},
+    ["Candy Land"] = {"Coins", 2500000},
+    ["Underworld"] = {"Coins", 2000000000},
+    ["Atlantis"] = {"Coins", 20000000},
+    ["Beach World"] = {"Coins", 150000000},
+    ["Toy Land"] = {"Coins", 50000000}
+}
