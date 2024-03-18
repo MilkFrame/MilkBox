254d204a602093bfb14a0c6d55e510c24b3d696c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:11:51 +1100

    Create SpringShopModule.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/SpringShopModule.lua
@@ -0,0 +1,10 @@
+data = {
+    {["Type"] = "Boost", ["Name"] = "2xLuck", ["Cost"] = 30000000, ["Time"] = 3600, ["Limit"] = 30},
+    {["Type"] = "Boost", ["Name"] = "2xHatchSpeed", ["Cost"] = 45000000, ["Time"] = 3600, ["Limit"] = 30},
+    {["Limit"] = 24, ["Type"] = "Potion", ["Name"] = "Level1", ["Cost"] = 60000000},
+    {["Limit"] = 24, ["Type"] = "Potion", ["Name"] = "Enchant1", ["Cost"] = 75000000},
+    {["Limit"] = 6, ["Type"] = "Potion", ["Name"] = "LevelMax", ["Cost"] = 8000000000},
+    {["Limit"] = 6, ["Type"] = "Potion", ["Name"] = "EnchantMax", ["Cost"] = 8000000000},
+    {["Limit"] = 1, ["Type"] = "Title", ["Name"] = "Spring Grinder", ["Cost"] = 250000000000},
+    {["Limit"] = 1, ["Type"] = "Pet", ["Name"] = "Spring Spirit", ["Cost"] = 750000000000}
+}
