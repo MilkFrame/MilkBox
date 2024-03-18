bfb04b13c466b21e1537cc44e8fb69c3f21ca2ff
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:10:47 +1100

    Create StPatricksModule.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/StPatricksModule.lua
@@ -0,0 +1,24 @@
+data = {
+    ["GetBaseData"] = "function: 0x00000000a8534130",
+    ["UnlockAllId"] = 964813898,
+    ["UnlockOneId"] = 964813874,
+    ["GetDayStringForIndex"] = "function: 0x0000000024fd1b28",
+    ["GetDayIndex"] = "function: 0x0000000049893c88",
+    ["Days"] = {
+        {["Reward"] = {"Potion", "Level1"}, ["Challenge"] = {"Eggs", 2000}},
+        {["Reward"] = {"Potion", "Enchant1"}, ["Challenge"] = {"Bubbles", 10000000}},
+        {["Reward"] = {"Boost", "2xLuck", 1800}, ["Challenge"] = {"Legendaries", 50}},
+        {["Reward"] = {"Boost", "2xHatchSpeed", 1800}, ["Challenge"] = {"Blocks", 750000000}},
+        {["Reward"] = {"Potion", "Level1"}, ["Challenge"] = {"Bubbles", 75000000}},
+        {["Reward"] = {"Potion", "Enchant1"}, ["Challenge"] = {"Eggs", 5000}},
+        {["Reward"] = {"Boost", "2xLuck", 7200}, ["Challenge"] = {"Epics", 750}},
+        {["Reward"] = {"Boost", "2xHatchSpeed", 7200}, ["Challenge"] = {"Bubbles", 250000000}},
+        {["Reward"] = {"Boost", "2xLuck", 14400}, ["Challenge"] = {"Candy", 1250000000}},
+        {["Reward"] = {"Boost", "2xHatchSpeed", 21600}, ["Challenge"] = {"Bubbles", 1000000000}},
+        {["Reward"] = {"Boost", "2xLuck", 21600}, ["Challenge"] = {"Eggs", 10000}},
+        {["Reward"] = {"Potion", "LevelMax"}, ["Challenge"] = {"Shells", 4000000000}},
+        {["Reward"] = {"Potion", "EnchantMax"}, ["Challenge"] = {"Legendaries", 250}},
+        {["Reward"] = {"Pet", "Spirit of Wealth"}, ["Challenge"] = {"Pearls", 7500000000}}
+    },
+    ["GetLastFinishedDay"] = "function: 0x00000000cb479840"
+}
