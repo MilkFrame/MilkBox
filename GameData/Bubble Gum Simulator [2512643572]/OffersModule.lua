c5c9c6722dd276dd8e25b77fd1fefb6ba56ebc44
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:06:41 +1100

    Create OffersModule.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/OffersModule.lua
@@ -0,0 +1,10 @@
+data = {
+    {["Name"] = "Frosted Darkness", ["Product"] = 1230389028},
+    {["Name"] = "Atlantis Guardian", ["Product"] = 513125306},
+    {["Name"] = "Sea Urchin", ["Product"] = 482984617},
+    {["Name"] = "Toxic Elemental", ["Product"] = 462083354},
+    {["Name"] = "Brick Shark", ["Product"] = 462170136},
+    {["Name"] = "Wind Up Fox", ["Product"] = 462170507},
+    {["Name"] = "Candy Winged Hydra", ["Product"] = 443896019},
+    {["Name"] = "Inferno Dragon", ["Product"] = 422501406}
+}
