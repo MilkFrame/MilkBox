e5237b97867e5561805e490bd2caa13ef0dfe427
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:24:38 +1100

    Create Achievements.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/Achievements.lua
@@ -0,0 +1,54 @@
+data = {
+    ["Bubbles Blown"] = {
+        ["GemStart"] = 34,
+        ["_shf"] = -3,
+        ["Index"] = 1,
+        ["_mul"] = 15000,
+        ["_add"] = -50.2,
+        ["Desc"] = "Blow %s bubble%s",
+        ["Name"] = "Bubble Blower",
+        ["_div"] = 24.3,
+        ["CoinsPerGem"] = 7,
+        ["Base"] = 30,
+        ["Reward"] = 0.5
+    },
+    ["Coins Collected"] = {
+        ["GemStart"] = 25,
+        ["_shf"] = -2,
+        ["Index"] = 2,
+        ["_mul"] = 7.9,
+        ["_add"] = 4.2,
+        ["Desc"] = "Collect %s coin%s",
+        ["Name"] = "Coin Collector",
+        ["_div"] = 10,
+        ["CoinsPerGem"] = 4,
+        ["Base"] = 5,
+        ["Reward"] = 3
+    },
+    ["Gems Collected"] = {
+        ["GemStart"] = 1,
+        ["_shf"] = -2.5,
+        ["Index"] = 3,
+        ["_mul"] = 8.6,
+        ["_add"] = 2.9,
+        ["Desc"] = "Collect %s gem%s",
+        ["Name"] = "Gem Collector",
+        ["_div"] = 6,
+        ["CoinsPerGem"] = 2,
+        ["Base"] = 3,
+        ["Reward"] = 10
+    },
+    ["Eggs Hatched"] = {
+        ["GemStart"] = 9,
+        ["_shf"] = -2.5,
+        ["Index"] = 4,
+        ["_mul"] = 7.6,
+        ["_add"] = 0.9,
+        ["Desc"] = "Hatch %s egg%s",
+        ["Name"] = "Egg Hatcher",
+        ["_div"] = 4,
+        ["CoinsPerGem"] = 4,
+        ["Base"] = 1,
+        ["Reward"] = 100
+    }
+}
