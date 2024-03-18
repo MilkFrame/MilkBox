5e29bcb9302fcc24a4db01310429100589dbae3e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:26:08 +1100

    Create ShardService.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/ShardService.lua
@@ -0,0 +1,36 @@
+data = {
+    ["Shop"] = {
+        {["Type"] = "Boost", ["Name"] = "2xLuck", ["Cost"] = 4000, ["Time"] = 3600, ["Limit"] = 10},
+        {["Type"] = "Boost", ["Name"] = "2xHatchSpeed", ["Cost"] = 4000, ["Time"] = 3600, ["Limit"] = 10},
+        {["Type"] = "Boost", ["Name"] = "3xMythicChance", ["Cost"] = 4000, ["Time"] = 3600, ["Limit"] = 10},
+        {["Limit"] = 30, ["Type"] = "Potion", ["Name"] = "Shadow", ["Cost"] = 1500}
+    },
+    ["GetRandomQuest"] = "function: 0x0000000049fdd150",
+    ["IncrementQuest"] = "function: 0x00000000cedc7668",
+    ["GetWeekIndex"] = "function: 0x00000000516b97a8",
+    ["GetQuestData"] = "function: 0x000000005fd6c1f8",
+    ["Quests"] = {
+        ["Hard"] = {
+            {"Eggs", 5000, 10000},
+            {"Legendaries", 25, 4000},
+            {"Bubbles", 1000000000, 4500},
+            {"Gems", 30000000000, 10000},
+            {"Currency", 10000000000, 7500}
+        },
+        ["Medium"] = {
+            {"Eggs", 750, 700},
+            {"Legendaries", 10, 150},
+            {"Bubbles", 250000000, 350},
+            {"Gems", 3000000000, 650},
+            {"Currency", 1000000000, 450}
+        },
+        ["Easy"] = {
+            {"Eggs", 200, 80},
+            {"Legendaries", 5, 60},
+            {"Bubbles", 75000000, 75},
+            {"Gems", 400000000, 50},
+            {"Currency", 500000000, 55}
+        }
+    },
+    ["GetBaseShopData"] = "function: 0x00000000c0b96ed0"
+}
