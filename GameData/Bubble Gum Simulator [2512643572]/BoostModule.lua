a371439cd0d22c317553d4b09aad506c7e97144b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:09:11 +1100

    Create BoostModule.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/BoostModule.lua
@@ -0,0 +1,98 @@
+data = {
+    ["2xCandy"] = {
+        ["Product2"] = 466922230,
+        ["Product1"] = 466922135,
+        ["Description"] = "2x Candy from pickups and chests!"
+    },
+    ["2xTreats"] = {
+        ["Order"] = 0,
+        ["Product1"] = 1096565778,
+        ["Description"] = "2x Treats during the event!",
+        ["Product2"] = 1096566077
+    },
+    ["3xShinyChance"] = {
+        ["Order"] = 4,
+        ["Product1"] = 466937119,
+        ["Description"] = "3x Chance to hatch a shiny pet!",
+        ["Product2"] = 466937240
+    },
+    ["2xPearls"] = {
+        ["Product2"] = 512684158,
+        ["Product1"] = 512683807,
+        ["Description"] = "2x Pearls from pickups and chests!"
+    },
+    ["2xLollipops"] = {
+        ["Order"] = 0,
+        ["Product1"] = 1212630707,
+        ["Description"] = "2x Lollipops during the event!",
+        ["Product2"] = 1212630889
+    },
+    ["2xStars"] = {
+        ["Product2"] = 579626971,
+        ["Product1"] = 579626409,
+        ["Description"] = "2x Stars from pickups and chests!"
+    },
+    ["2xSilver"] = {
+        ["Order"] = 0,
+        ["Product1"] = 1155839853,
+        ["Description"] = "2x Silver during the event!",
+        ["Product2"] = 1155839901
+    },
+    ["2xBlocks"] = {
+        ["Product2"] = 466921793,
+        ["Product1"] = 466908394,
+        ["Description"] = "2x Blocks from pickups and chests!"
+    },
+    ["2xMagma"] = {
+        ["Product2"] = 784791372,
+        ["Product1"] = 784788753,
+        ["Description"] = "2x Magma from pickups and chests!"
+    },
+    ["2xShells"] = {
+        ["Product2"] = 482526550,
+        ["Product1"] = 482526320,
+        ["Description"] = "2x Shells from pickups and chests!"
+    },
+    ["2xSnowflakes"] = {
+        ["Order"] = 0,
+        ["Product1"] = 1230291114,
+        ["Description"] = "2x Snowflakes during the event!",
+        ["Product2"] = 1230291134
+    },
+    ["2xLuck"] = {
+        ["Order"] = 1,
+        ["Product1"] = 466935363,
+        ["Description"] = "2x Luck from Eggs and Crates!",
+        ["Product2"] = 466935590
+    },
+    ["3xMythicChance"] = {
+        ["Order"] = 3,
+        ["Product1"] = 979439886,
+        ["Description"] = "3x Chance to hatch a mythic variant!",
+        ["Product2"] = 979440048
+    },
+    ["2xHatchSpeed"] = {
+        ["Order"] = 2,
+        ["Product1"] = 466935965,
+        ["Description"] = "Open Eggs 2x faster!",
+        ["Product2"] = 466936102
+    },
+    ["2xCrystals"] = {
+        ["Order"] = 6,
+        ["Product1"] = 967427090,
+        ["Description"] = "2x Crystals from pickups and chests!",
+        ["Product2"] = 967427127
+    },
+    ["2xXP"] = {
+        ["Order"] = 5,
+        ["Product1"] = 968481420,
+        ["Description"] = "2x XP from pickups and chests!",
+        ["Product2"] = 968481469
+    },
+    ["2xBells"] = {
+        ["Order"] = 0,
+        ["Product1"] = 1121231200,
+        ["Description"] = "2x Bells during the event!",
+        ["Product2"] = 1121231329
+    }
+}
