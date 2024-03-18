491061d366710a2248b9c204b5eed0ec1d26e5f3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:12:03 +1100

    Create CarnivalShopModule.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/CarnivalShopModule.lua
@@ -0,0 +1,10 @@
+data = {
+    {["Type"] = "Boost", ["Name"] = "2xLuck", ["Cost"] = 30000000, ["Time"] = 3600, ["Limit"] = 60},
+    {["Type"] = "Boost", ["Name"] = "2xHatchSpeed", ["Cost"] = 45000000, ["Time"] = 3600, ["Limit"] = 60},
+    {["Limit"] = 48, ["Type"] = "Potion", ["Name"] = "Enchant1", ["Cost"] = 75000000},
+    {["Limit"] = 30, ["Type"] = "Potion", ["Name"] = "Shadow", ["Cost"] = 2000000000},
+    {["Limit"] = 12, ["Type"] = "Potion", ["Name"] = "LevelMax", ["Cost"] = 8000000000},
+    {["Limit"] = 12, ["Type"] = "Potion", ["Name"] = "EnchantMax", ["Cost"] = 8000000000},
+    {["Limit"] = 1, ["Type"] = "Pet", ["Name"] = "Unicorn Plushie", ["Cost"] = 250000000000},
+    {["Limit"] = 1, ["Type"] = "Pet", ["Name"] = "LOL", ["Cost"] = 1000000000000}
+}
