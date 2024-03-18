d304e37cea995d591378c36a16b7b5d56e075444
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:12:36 +1100

    Create FallShopModule.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/FallShopModule.lua
@@ -0,0 +1,9 @@
+data = {
+    {["Type"] = "Boost", ["Name"] = "2xLuck", ["Cost"] = 5000000000, ["Time"] = 3600, ["Limit"] = 30},
+    {["Type"] = "Boost", ["Name"] = "2xHatchSpeed", ["Cost"] = 4500000000, ["Time"] = 3600, ["Limit"] = 30},
+    {["Limit"] = 48, ["Type"] = "Potion", ["Name"] = "Enchant1", ["Cost"] = 250000000},
+    {["Limit"] = 30, ["Type"] = "Potion", ["Name"] = "Shadow", ["Cost"] = 7500000000},
+    {["Limit"] = 12, ["Type"] = "Potion", ["Name"] = "LevelMax", ["Cost"] = 3000000000},
+    {["Limit"] = 12, ["Type"] = "Potion", ["Name"] = "EnchantMax", ["Cost"] = 4000000000},
+    {["Limit"] = 1, ["Type"] = "Title", ["Name"] = "Autumn Wizard", ["Cost"] = 1000000000000}
+}
