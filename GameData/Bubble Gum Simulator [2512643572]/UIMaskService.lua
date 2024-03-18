71cf9cad595be6556425a253a4487fb014d5b180
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:26:42 +1100

    Create UIMaskService.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/UIMaskService.lua
@@ -0,0 +1,96 @@
+data = {
+    ["Mystic Forest"] = {
+        ["Other"] = Color3.fromRGB(0, 150, 204),
+        ["PurchaseFrame"] = {["CrystalsButton"] = {["Visible"] = true}, ["CoinsButton"] = {["Visible"] = false}},
+        ["Other2"] = Color3.fromRGB(94, 243, 194),
+        ["Primary"] = Color3.fromRGB(69, 255, 218),
+        ["StatsFrame"] = {["Crystals"] = {["Visible"] = true}, ["Coins"] = {["Visible"] = false}},
+        ["Secondary"] = Color3.fromRGB(0, 255, 162)
+    },
+    ["Beach World"] = {
+        ["Other"] = Color3.fromRGB(0, 150, 204),
+        ["PurchaseFrame"] = {["ShellsButton"] = {["Visible"] = true}, ["CoinsButton"] = {["Visible"] = false}},
+        ["Other2"] = Color3.fromRGB(94, 243, 194),
+        ["Primary"] = Color3.fromRGB(69, 255, 218),
+        ["StatsFrame"] = {["Shells"] = {["Visible"] = true}, ["Coins"] = {["Visible"] = false}},
+        ["Secondary"] = Color3.fromRGB(0, 255, 162)
+    },
+    ["Rainbow Land"] = {
+        ["Other"] = Color3.fromRGB(159, 35, 35),
+        ["PurchaseFrame"] = {["StarsButton"] = {["Visible"] = true}, ["CoinsButton"] = {["Visible"] = false}},
+        ["Other2"] = Color3.fromRGB(159, 75, 75),
+        ["Primary"] = Color3.fromRGB(240, 99, 99),
+        ["StatsFrame"] = {["Stars"] = {["Visible"] = true}, ["Coins"] = {["Visible"] = false}},
+        ["Secondary"] = Color3.fromRGB(213, 141, 142)
+    },
+    ["Underworld"] = {
+        ["Other"] = Color3.fromRGB(27, 42, 53),
+        ["PurchaseFrame"] = {["MagmaButton"] = {["Visible"] = true}, ["CoinsButton"] = {["Visible"] = false}},
+        ["Other2"] = Color3.fromRGB(170, 85, 10),
+        ["Primary"] = Color3.fromRGB(255, 85, 0),
+        ["StatsFrame"] = {["Magma"] = {["Visible"] = true}, ["Coins"] = {["Visible"] = false}},
+        ["Secondary"] = Color3.fromRGB(255, 12, 0)
+    },
+    ["Candy Land"] = {
+        ["Other"] = Color3.fromRGB(0, 150, 204),
+        ["PurchaseFrame"] = {["CoinsButton"] = {["Visible"] = false}, ["CandyButton"] = {["Visible"] = true}},
+        ["Other2"] = Color3.fromRGB(94, 243, 194),
+        ["Primary"] = Color3.fromRGB(69, 255, 218),
+        ["StatsFrame"] = {["Candy"] = {["Visible"] = true}, ["Coins"] = {["Visible"] = false}},
+        ["Secondary"] = Color3.fromRGB(0, 255, 162)
+    },
+    ["Atlantis"] = {
+        ["Other"] = Color3.fromRGB(0, 150, 204),
+        ["PurchaseFrame"] = {["CoinsButton"] = {["Visible"] = false}, ["PearlsButton"] = {["Visible"] = true}},
+        ["Other2"] = Color3.fromRGB(94, 243, 194),
+        ["Primary"] = Color3.fromRGB(69, 255, 218),
+        ["StatsFrame"] = {["Pearls"] = {["Visible"] = true}, ["Coins"] = {["Visible"] = false}},
+        ["Secondary"] = Color3.fromRGB(0, 255, 162)
+    },
+    ["Christmas"] = {
+        ["Other"] = Color3.fromRGB(125, 204, 137),
+        ["PurchaseFrame"] = {["CandyCanesButton"] = {["Visible"] = true}, ["CoinsButton"] = {["Visible"] = false}},
+        ["Other2"] = Color3.fromRGB(243, 102, 133),
+        ["Primary"] = Color3.fromRGB(0, 255, 106),
+        ["StatsFrame"] = {["CandyCanes"] = {["Visible"] = true}, ["Coins"] = {["Visible"] = false}},
+        ["Secondary"] = Color3.fromRGB(50, 159, 53)
+    },
+    ["Toy Land"] = {
+        ["Other"] = Color3.fromRGB(159, 35, 35),
+        ["PurchaseFrame"] = {["CoinsButton"] = {["Visible"] = false}, ["BlocksButton"] = {["Visible"] = true}},
+        ["Other2"] = Color3.fromRGB(159, 75, 75),
+        ["Primary"] = Color3.fromRGB(240, 99, 99),
+        ["StatsFrame"] = {["Blocks"] = {["Visible"] = true}, ["Coins"] = {["Visible"] = false}},
+        ["Secondary"] = Color3.fromRGB(213, 141, 142)
+    },
+    ["Overworld"] = {
+        ["Other"] = Color3.fromRGB(100, 134, 159),
+        ["PurchaseFrame"] = {
+            ["CoinsButton"] = {["Visible"] = true},
+            ["CandyButton"] = {["Visible"] = false},
+            ["ShellsButton"] = {["Visible"] = false},
+            ["BlocksButton"] = {["Visible"] = false}
+        },
+        ["Other2"] = Color3.fromRGB(62, 97, 255),
+        ["Primary"] = Color3.fromRGB(0, 179, 255),
+        ["StatsFrame"] = {
+            ["Snowflakes"] = {["Visible"] = true},
+            ["Pearls"] = {["Visible"] = false},
+            ["Silver"] = {["Visible"] = false},
+            ["Lollipops"] = {["Visible"] = false},
+            ["Flowers"] = {["Visible"] = false},
+            ["Candy"] = {["Visible"] = false},
+            ["Magma"] = {["Visible"] = false},
+            ["Coins"] = {["Visible"] = true},
+            ["Stars"] = {["Visible"] = false},
+            ["Blocks"] = {["Visible"] = false},
+            ["Tokens"] = {["Visible"] = false},
+            ["Crystals"] = {["Visible"] = false},
+            ["Presents"] = {["Visible"] = false},
+            ["Rainbows"] = {["Visible"] = false},
+            ["Tickets"] = {["Visible"] = false},
+            ["Shells"] = {["Visible"] = false}
+        },
+        ["Secondary"] = Color3.fromRGB(43, 103, 159)
+    }
+}
