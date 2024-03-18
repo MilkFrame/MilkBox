848d0f7ed03230ecdaa43c0f6d0d2bab036c01eb
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:12:49 +1100

    Create ChristmasShopModule.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/ChristmasShopModule.lua
@@ -0,0 +1,10 @@
+data = {
+    {["Type"] = "Boost", ["Name"] = "2xLuck", ["Cost"] = 60000000, ["Time"] = 3600, ["Limit"] = 40},
+    {["Type"] = "Boost", ["Name"] = "2xHatchSpeed", ["Cost"] = 85000000, ["Time"] = 3600, ["Limit"] = 40},
+    {["Limit"] = 64, ["Type"] = "Potion", ["Name"] = "Enchant1", ["Cost"] = 100000000},
+    {["Limit"] = 40, ["Type"] = "Potion", ["Name"] = "Shadow", ["Cost"] = 2250000000},
+    {["Limit"] = 16, ["Type"] = "Potion", ["Name"] = "LevelMax", ["Cost"] = 10000000000},
+    {["Limit"] = 16, ["Type"] = "Potion", ["Name"] = "EnchantMax", ["Cost"] = 10000000000},
+    {["Limit"] = 1, ["Type"] = "Title", ["Name"] = "Blizzard Wizard", ["Cost"] = 750000000000},
+    {["Limit"] = 2, ["Type"] = "Pet", ["Name"] = "Fairy in a Bottle", ["Cost"] = 2000000000000}
+}
