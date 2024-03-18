970831158cbdd1be4706180475decfbe639d7274
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:23:46 +1100

    Create Halloween21ShopModule.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/Halloween21ShopModule.lua
@@ -0,0 +1,10 @@
+data = {
+    {["Type"] = "Boost", ["Name"] = "2xLuck", ["Cost"] = 60000000, ["Time"] = 3600, ["Limit"] = 10},
+    {["Type"] = "Boost", ["Name"] = "2xHatchSpeed", ["Cost"] = 85000000, ["Time"] = 3600, ["Limit"] = 10},
+    {["Limit"] = 16, ["Type"] = "Potion", ["Name"] = "Enchant1", ["Cost"] = 100000000},
+    {["Limit"] = 10, ["Type"] = "Potion", ["Name"] = "Shadow", ["Cost"] = 2250000000},
+    {["Limit"] = 4, ["Type"] = "Potion", ["Name"] = "LevelMax", ["Cost"] = 10000000000},
+    {["Limit"] = 4, ["Type"] = "Potion", ["Name"] = "EnchantMax", ["Cost"] = 10000000000},
+    {["Limit"] = 1, ["Type"] = "Title", ["Name"] = "Spooky", ["Cost"] = 750000000000},
+    {["Limit"] = 1, ["Type"] = "Pet", ["Name"] = "Haunted Spirit", ["Cost"] = 1500000000000}
+}
