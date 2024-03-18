232ae22d94c3bc54f511962dfd465bbe1b1a67f6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:07:05 +1100

    Create CrateModule.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/CrateModule.lua
@@ -0,0 +1,175 @@
+data = {
+    ["Ocean Crate"] = {
+        ["Rarities"] = {
+            {"Eyepatch", 35.92},
+            {"Sea Bucket", 35},
+            {"Snorkel", 23},
+            {"Commander", 4},
+            {"Deep Diver", 2},
+            {"Triton Crown", 0.055},
+            {"Sea King", 0.025}
+        },
+        ["Cost"] = {"Pearls", 150000}
+    },
+    ["Secret Crate"] = {
+        ["Rarities"] = {{"Happy Hat", 99.41}, {"Bacon Hair", 0.49}, {"Eye Poppers", 0.099}, {"Clout Goggles", 0.001}},
+        ["Cost"] = {"Candy", 100000}
+    },
+    ["Headphone Crate"] = {
+        ["Rarities"] = {
+            {"Red Headphones", 45},
+            {"Blue Headphones", 38},
+            {"8-Bit Headphones", 15.985},
+            {"Workclock Headphones", 0.75},
+            {"Clockwork Headphones", 0.25},
+            {"Golden Headphones", 0.01},
+            {"Workclock Ultimate", 0.005}
+        },
+        ["Cost"] = {"Blocks", 200000}
+    },
+    ["Basic Crate"] = {
+        ["Rarities"] = {{"Spy Hat", 35}, {"Fedora", 30}, {"Shades", 20}, {"Gold Crown", 10}, {"Timeless Top Hat", 5}},
+        ["Cost"] = {"Candy", 500}
+    },
+    ["Products"] = {["Rarities"] = {}},
+    ["Classic Crate"] = {
+        ["Rarities"] = {
+            {"Rubber Ducky", 45},
+            {"Sword Pack", 38},
+            {"Bucket", 15.985},
+            {"Cone", 0.75},
+            {"Bunny Ears", 0.25},
+            {"Sparky Domino", 0.01},
+            {"Flaming Headphones", 0.005}
+        },
+        ["Cost"] = {"Candy", 225000}
+    },
+    ["Water Crate"] = {
+        ["Rarities"] = {
+            {"Water Hat", 46.4849},
+            {"Blue Hat", 40},
+            {"Blue Hood", 12},
+            {"Blue Horns", 1},
+            {"Blue Top Hat", 0.5},
+            {"Blue Circlet", 0.01},
+            {"sircfenner", 0.005},
+            {"ObscureEntity", 0.0001}
+        },
+        ["Ignore"] = {"ObscureEntity"},
+        ["Cost"] = {"Shells", 200000}
+    },
+    ["Adurite Crate"] = {
+        ["Rarities"] = {
+            {"Adurite Bowler", 40},
+            {"Adurite Fedora", 31},
+            {"Adurite Crown", 22},
+            {"Adurite Shades", 5},
+            {"Adurite Bucket", 1.9},
+            {"Adurite Antlers", 0.075},
+            {"Adurite Top Hat", 0.025}
+        },
+        ["Cost"] = {"Blocks", 150000}
+    },
+    ["Christmas Crate"] = {
+        ["Rarities"] = {
+            {"Star Top", 58.94},
+            {"Ornamental Antlers", 25},
+            {"Chimney Santa", 10},
+            {"Gingerbread Scarf", 4},
+            {"Santa Cop", 2},
+            {"Diamond Circlet", 0.05},
+            {"Mr. Snowman's Top Hat", 0.005},
+            {"Santa Paws", 0.0001}
+        },
+        ["Ignore"] = {"Santa Paws"},
+        ["Cost"] = {"Coins", 10000000}
+    },
+    ["Autumn Crate"] = {
+        ["Rarities"] = {
+            {"Leaf Glasses", 46.5},
+            {"Leaf Ninja", 30},
+            {"Leaf Wizard", 14},
+            {"Leaf Crown", 9.5},
+            {"Autumn Antlers", 0.01},
+            {"Autumn Tophat", 0.0055},
+            {"Vibe Check", 0.00005}
+        },
+        ["Ignore"] = {"Vibe Check"},
+        ["Cost"] = {"Coins", 3500000}
+    },
+    ["50th Update Crate"] = {
+        ["Rarities"] = {
+            {"Pretty Bow", 46.4849},
+            {"Bear Mask", 30},
+            {"Blue Pirate Hat", 14},
+            {"Yeehaw", 9.5},
+            {"Relaxed", 0.01},
+            {"MLG", 0.0055},
+            {"Sylently's Hat", 0.00005}
+        },
+        ["Ignore"] = {"Sylently's Hat"},
+        ["Cost"] = {"Coins", 2250000}
+    },
+    ["Underwater Crate"] = {
+        ["Rarities"] = {
+            {"Fisherman", 46.485},
+            {"Ocean Guard", 40},
+            {"Ocean Sorcerer", 12},
+            {"Shell Crown", 1},
+            {"Sea Prince", 0.5},
+            {"Atlantis Tiara", 0.01},
+            {"Poseidon", 0.005}
+        },
+        ["Cost"] = {"Pearls", 200000}
+    },
+    ["Epic Crate"] = {
+        ["Rarities"] = {
+            {"Bandit", 40},
+            {"Pirate Captain", 31},
+            {"Wizard", 22},
+            {"Silver Antlers", 5},
+            {"Gold Antlers", 1.9},
+            {"Frost Horns", 0.075},
+            {"Fire Horns", 0.025}
+        },
+        ["Cost"] = {"Candy", 150000}
+    },
+    ["Winged Crate"] = {
+        ["Rarities"] = {
+            {"Wings", 47.8},
+            {"Bluesteel Wings", 31.1},
+            {"Dragon Wings", 20.08},
+            {"Interstellar Wings", 1.003},
+            {"Fire Wings", 0.01},
+            {"Godly Wings", 0.006},
+            {"Frost Wings", 0.001}
+        },
+        ["Cost"] = {"Candy", 200000}
+    },
+    ["Beach Crate"] = {
+        ["Rarities"] = {
+            {"Sun Visor", 35.92},
+            {"Sun Hat", 35},
+            {"Beach Ball", 23},
+            {"Sun Fedora", 4},
+            {"Sand Wizard", 2},
+            {"Yellow Top Hat", 0.055},
+            {"Red Crown", 0.025}
+        },
+        ["Cost"] = {"Shells", 150000}
+    },
+    ["Sparkle Crate"] = {
+        ["Rarities"] = {
+            {"White Sparkle Time", 26},
+            {"Gold Sparkle Time", 19},
+            {"Blue Sparkle Time", 17},
+            {"Green Sparkle Time", 15},
+            {"Purple Sparkle Time", 12},
+            {"Red Sparkle Time", 6.8},
+            {"Sparkle Cone", 3.6},
+            {"Sparkle Valkyrie", 0.5},
+            {"Sparkle Top Hat", 0.1}
+        },
+        ["Cost"] = {"Candy", 55000}
+    }
+}
