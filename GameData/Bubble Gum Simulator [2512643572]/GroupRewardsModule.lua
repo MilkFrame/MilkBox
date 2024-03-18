cf99877d61f8946c8bca324c449da614c30d8da3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:09:56 +1100

    Create GroupRewardsModule.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/GroupRewardsModule.lua
@@ -0,0 +1,13 @@
+data = {
+    {"2xBlocks", 1200},
+    {"2xHatchSpeed", 1200},
+    {"2xLuck", 1200},
+    {"3xShinyChance", 1200},
+    {"2xCandy", 1200},
+    {"2xShells", 1200},
+    {"2xPearls", 1200},
+    {"2xStars", 1200},
+    {"2xMagma", 1200},
+    {"2xCrystals", 1200},
+    {"2xXP", 1200}
+}
