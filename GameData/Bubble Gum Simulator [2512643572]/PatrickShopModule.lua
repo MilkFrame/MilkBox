d5121e998943e5092f5b6eefca0bb952f92d7b28
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:10:36 +1100

    Create PatrickShopModule.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/PatrickShopModule.lua
@@ -0,0 +1,11 @@
+data = {
+    {["Type"] = "Boost", ["Name"] = "2xLuck", ["Cost"] = 30000000, ["Time"] = 3600, ["Limit"] = 20},
+    {["Type"] = "Boost", ["Name"] = "2xHatchSpeed", ["Cost"] = 45000000, ["Time"] = 3600, ["Limit"] = 20},
+    {["Limit"] = 16, ["Type"] = "Potion", ["Name"] = "Level1", ["Cost"] = 60000000},
+    {["Limit"] = 16, ["Type"] = "Potion", ["Name"] = "Enchant1", ["Cost"] = 75000000},
+    {["Limit"] = 4, ["Type"] = "Potion", ["Name"] = "LevelMax", ["Cost"] = 8000000000},
+    {["Limit"] = 4, ["Type"] = "Potion", ["Name"] = "EnchantMax", ["Cost"] = 8000000000},
+    {["Limit"] = 1, ["Type"] = "Pet", ["Name"] = "Lucky Tophat", ["Cost"] = 50000000000},
+    {["Limit"] = 2, ["Type"] = "Hat", ["Name"] = "Lucky Clout Goggles", ["Cost"] = 8250000000},
+    {["Limit"] = 1, ["Type"] = "Pet", ["Name"] = "Toxic Demon", ["Cost"] = 250000000000}
+}
