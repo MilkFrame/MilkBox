b4c6f5f359b57363d4b98013d909e5c66f650885
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:06:52 +1100

    Create Patrick21ShopModule.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/Patrick21ShopModule.lua
@@ -0,0 +1,10 @@
+data = {
+    {["Type"] = "Boost", ["Name"] = "2xLuck", ["Cost"] = 60000000, ["Time"] = 3600, ["Limit"] = 30},
+    {["Type"] = "Boost", ["Name"] = "2xHatchSpeed", ["Cost"] = 85000000, ["Time"] = 3600, ["Limit"] = 30},
+    {["Limit"] = 48, ["Type"] = "Potion", ["Name"] = "Enchant1", ["Cost"] = 100000000},
+    {["Limit"] = 30, ["Type"] = "Potion", ["Name"] = "Shadow", ["Cost"] = 2250000000},
+    {["Limit"] = 12, ["Type"] = "Potion", ["Name"] = "LevelMax", ["Cost"] = 10000000000},
+    {["Limit"] = 12, ["Type"] = "Potion", ["Name"] = "EnchantMax", ["Cost"] = 10000000000},
+    {["Limit"] = 1, ["Type"] = "Title", ["Name"] = "Luck Master", ["Cost"] = 750000000000},
+    {["Limit"] = 2, ["Type"] = "Pet", ["Name"] = "Gold Overseer", ["Cost"] = 1500000000000}
+}
