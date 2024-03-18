49326108cdf606cf1398780821f9c214036d5179
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:26:18 +1100

    Create CarnivalPassService.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/CarnivalPassService.lua
@@ -0,0 +1,51 @@
+data = {
+    ["GetBaseData"] = "function: 0x000000001b4e5bd8",
+    ["Challenges"] = {
+        ["Hard"] = {
+            {"Eggs", 5000, 110},
+            {"Bubbles", 1000000000, 85},
+            {"Gems", 30000000000, 100},
+            {"Currency", 10000000000, 80}
+        },
+        ["Medium"] = {
+            {"Eggs", 750, 35},
+            {"Legendaries", 10, 15},
+            {"Bubbles", 350000000, 20},
+            {"Gems", 3000000000, 35},
+            {"Currency", 1000000000, 25}
+        },
+        ["Easy"] = {
+            {"Eggs", 250, 10},
+            {"Legendaries", 5, 10},
+            {"Bubbles", 45000000, 5},
+            {"Gems", 400000000, 5},
+            {"Currency", 500000000, 5}
+        }
+    },
+    ["RewardTemplate"] = "RewardTemplate",
+    ["GetChallengeData"] = "function: 0x000000009a5bd2e0",
+    ["Rewards"] = {
+        {["Type"] = "Boost", ["Name"] = "2xLuck", ["Time"] = 3600},
+        {["Type"] = "Pet", ["Name"] = "Pink Lollipop", ["Desc"] = "Great work, have a Lollipop!"},
+        {["Name"] = "Shadow", ["Type"] = "Potion"},
+        {["Type"] = "Boost", ["Name"] = "2xLuck", ["Time"] = 7200},
+        {["Type"] = "Boost", ["Name"] = "2xHatchSpeed", ["Time"] = 7200},
+        {["Name"] = "Shadow", ["Type"] = "Potion"},
+        {["Type"] = "Pet", ["Name"] = "Sugar Heart", ["Desc"] = "You can never go wrong with a free pet!"},
+        {["Name"] = "LevelMax", ["Type"] = "Potion"},
+        {["Name"] = "EnchantMax", ["Type"] = "Potion"},
+        {["Type"] = "Pet", ["Name"] = "Ocelot", ["Desc"] = "Heard this is a rare pet. Take it, you've earned it!"},
+        {["Name"] = "EnchantMax", ["Type"] = "Potion"},
+        {["Type"] = "Boost", ["Name"] = "2xLuck", ["Time"] = 18000},
+        {["Type"] = "Boost", ["Name"] = "2xHatchSpeed", ["Time"] = 18000},
+        {["Type"] = "Title", ["Name"] = "Carnival Pro", ["Desc"] = "Flex on your friends with this exclusive title."},
+        {
+            ["Type"] = "Pet",
+            ["Name"] = "Rabbit Plushie",
+            ["Desc"] = "Congrats, you have completed the carnival pass. Here's a special plushie, just for you!"
+        }
+    },
+    ["XPPerLevel"] = 100,
+    ["GetRandomChallenge"] = "function: 0x000000007f3ac8c8",
+    ["IncrementChallenge"] = "function: 0x00000000787d0278"
+}
