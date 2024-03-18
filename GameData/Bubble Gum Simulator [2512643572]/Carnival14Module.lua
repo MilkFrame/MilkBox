42737a868be6ec06d75f5ef30400de658e925d7d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:11:40 +1100

    Create Carnival14Module.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/Carnival14Module.lua
@@ -0,0 +1,24 @@
+data = {
+    ["GetBaseData"] = "function: 0x00000000323f3d08",
+    ["UnlockAllId"] = 1055138023,
+    ["UnlockOneId"] = 1055137857,
+    ["GetDayStringForIndex"] = "function: 0x000000001e04d1b0",
+    ["GetDayIndex"] = "function: 0x00000000049e98c0",
+    ["Days"] = {
+        {["Reward"] = {"Potion", "Level1"}, ["Challenge"] = {"Eggs", 750}},
+        {["Reward"] = {"Potion", "Enchant1"}, ["Challenge"] = {"Bubbles", 10000000}},
+        {["Reward"] = {"Boost", "2xLuck", 5400}, ["Challenge"] = {"Legendaries", 10}},
+        {["Reward"] = {"Boost", "2xHatchSpeed", 5400}, ["Challenge"] = {"Gems", 750000000}},
+        {["Reward"] = {"Potion", "Level1"}, ["Challenge"] = {"Bubbles", 75000000}},
+        {["Reward"] = {"Potion", "Enchant1"}, ["Challenge"] = {"Eggs", 2000}},
+        {["Reward"] = {"Boost", "2xHatchSpeed", 7200}, ["Challenge"] = {"Epics", 750}},
+        {["Reward"] = {"Boost", "2xLuck", 7200}, ["Challenge"] = {"Bubbles", 250000000}},
+        {["Reward"] = {"Boost", "2xHatchSpeed", 14400}, ["Challenge"] = {"Magma", 1250000000}},
+        {["Reward"] = {"Boost", "2xLuck", 14400}, ["Challenge"] = {"Bubbles", 1000000000}},
+        {["Reward"] = {"Pet", "Prismatic Paragon"}, ["Challenge"] = {"Eggs", 4000}},
+        {["Reward"] = {"Potion", "LevelMax"}, ["Challenge"] = {"Shells", 4000000000}},
+        {["Reward"] = {"Potion", "EnchantMax"}, ["Challenge"] = {"Legendaries", 75}},
+        {["Reward"] = {"Pet", "Queen Kitty"}, ["Challenge"] = {"Pearls", 7500000000}}
+    },
+    ["GetLastFinishedDay"] = "function: 0x0000000035e2e3b8"
+}
