a43fe342ac3dec197f15f0e9250233d1784bce63
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-03-18 21:10:24 +1100

    Create SpinToWinSpookyModule.lua

---
+++ GameData/Bubble Gum Simulator [2512643572]/SpinToWinSpookyModule.lua
@@ -0,0 +1,18 @@
+data = {
+    ["TIME_TO_STOP"] = 5.333333333333334,
+    ["SPIN_SLOWDOWN"] = 0.3,
+    ["DIST_TO_STOP"] = 4.2666666666666675,
+    ["GetHalfDay"] = "function: 0x000000006f0f31e8",
+    ["SPIN_VELOCITY"] = 1.6,
+    ["Rewards"] = {
+        {"Boost", "2xLuck", 10800},
+        {"Pet", "Mythic Astral Sprite"},
+        {"Potion", "EnchantMax"},
+        {"Pet", "Astral Sprite"},
+        {"Potion", "LevelMax"},
+        {"Potion", "Shadow"},
+        {"Boost", "2xHatchSpeed", 10800},
+        {"Boost", "2xTreats", 10800}
+    },
+    ["GetTimeUntilNextHalfDay"] = "function: 0x00000000675d7628"
+}
