2309517104c6cce725078425a2d10344dffbd93c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 03:01:50 +1000

    Create SettingToolTips.lua

---
+++ GameData/Sol's RNG [15532962292]/SettingToolTips.lua
@@ -0,0 +1,6 @@
+data = {
+    ["SkipAuraEffect"] = "If the rarity of the drawn aura is below the set value, the cutscene will be skipped.",
+    ["AutoEquipRarity"] = "If the rarity of the drawn aura exceeds the set value, the game automatically presses the equip button. You can enable/disable this by checking the checkbox next to it.",
+    ["EquipWarningRarity"] = "If the rarity of the currently equipped aura exceeds the set value, an equip warning window will appear when you click/press the equip button. You can enable/disable this by checking the checkbox next to it.",
+    ["SkipWarningRarity"] = "If the rarity of the drawn aura exceeds the set value, a skip warning window will appear when you click/press the skip button. You can enable/disable this by checking the checkbox next to it."
+}
