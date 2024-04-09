0b6dd36adafe7fa036ff73cf62d4d37c37cacbcb
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 03:00:30 +1000

    Create ToolEffects.lua

---
+++ GameData/Sol's RNG [15532962292]/ToolEffects.lua
@@ -0,0 +1,14 @@
+data = {
+    ["Luck Glove"] = {["Equipped"] = {"Basic Luck"}},
+    ["Solar Device"] = {["Equipped"] = {"Solar Ignite"}},
+    ["Subzero Device"] = {["Equipped"] = {"Subzero"}},
+    ["Gravitational Device"] = {["Equipped"] = {"Unstable Gravity"}},
+    ["Jackpot Gauntlet"] = {["Equipped"] = {"Jackpot"}},
+    ["Windstorm Device"] = {["Equipped"] = {"Galestorm"}},
+    ["Exo Gauntlet"] = {["Equipped"] = {"Exo Enhancement"}},
+    ["Lucky Potion"] = {["Activated"] = {["Buffs"] = {{["Duration"] = 60, ["BuffName"] = "Lucky", ["Stack"] = 1}}}},
+    ["Speed Potion"] = {["Activated"] = {["Buffs"] = {{["Duration"] = 30, ["BuffName"] = "QuickRoll", ["Stack"] = 1}}}},
+    ["Lunar Device"] = {["Equipped"] = {"Lunar Shift"}},
+    ["Galactic Device"] = {["Equipped"] = {"Cosmic Enhancement"}},
+    ["Eclipse Device"] = {["Equipped"] = {"Solar Ignite", "Lunar Shift"}}
+}
