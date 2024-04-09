bd4193721d68140e8a9e0d4b0a69171c29558349
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 03:02:22 +1000

    Create TagOptions.lua

---
+++ GameData/Sol's RNG [15532962292]/TagOptions.lua
@@ -0,0 +1,106 @@
+data = {
+    ["Take a break"] = {
+        ["GradientName"] = "None",
+        ["ZIndex"] = 11,
+        ["ImageId"] = 16776634409,
+        ["Name"] = "Take a break"
+    },
+    ["Something went wrong"] = {
+        ["GradientName"] = "GlitchGradient",
+        ["ZIndex"] = 22,
+        ["ImageId"] = 16776783582,
+        ["Name"] = "Something went wrong"
+    },
+    ["A sincere person"] = {
+        ["GradientName"] = "Roll10000",
+        ["ZIndex"] = 19,
+        ["ImageId"] = 16952038885,
+        ["Name"] = "A sincere person"
+    },
+    ["I can't stop playing this"] = {
+        ["GradientName"] = "Circles",
+        ["ZIndex"] = 12,
+        ["ImageId"] = 16776654801,
+        ["Name"] = "I can't stop playing this"
+    },
+    ["Breakthrough"] = {
+        ["GradientName"] = "None",
+        ["ZIndex"] = 15,
+        ["ImageId"] = 16776743577,
+        ["Name"] = "Breakthrough"
+    },
+    ["When is payday???"] = {
+        ["GradientName"] = "DailyQuestMaster",
+        ["ZIndex"] = 20,
+        ["ImageId"] = 16952041276,
+        ["Name"] = "When is payday???"
+    },
+    ["I just started Sol's RNG"] = {
+        ["GradientName"] = "None",
+        ["ZIndex"] = 1,
+        ["ImageId"] = 16776465635,
+        ["Name"] = "I just started Sol's RNG"
+    },
+    ["Roll, Eat, Sleep, Repeat"] = {
+        ["GradientName"] = "Digital",
+        ["ZIndex"] = 5,
+        ["ImageId"] = 16776545768,
+        ["Name"] = "Roll, Eat, Sleep, Repeat"
+    },
+    ["Finished work for today"] = {
+        ["GradientName"] = "None",
+        ["ZIndex"] = 17,
+        ["ImageId"] = 16951986723,
+        ["Name"] = "Finished work for today"
+    },
+    ["Good job this week too"] = {
+        ["GradientName"] = "Roll100",
+        ["ZIndex"] = 18,
+        ["ImageId"] = 16952031113,
+        ["Name"] = "Good job this week too"
+    },
+    ["In-dev"] = {["GradientName"] = "Digital", ["ZIndex"] = 10, ["ImageId"] = 16776803348, ["Name"] = "In-dev"},
+    ["Waste of time"] = {
+        ["GradientName"] = "Diamonds",
+        ["ZIndex"] = 13,
+        ["ImageId"] = 16776706541,
+        ["Name"] = "Waste of time"
+    },
+    ["I'm addicted to Sol's RNG"] = {
+        ["GradientName"] = "Roll1000",
+        ["ZIndex"] = 3,
+        ["ImageId"] = 16776486242,
+        ["Name"] = "I'm addicted to Sol's RNG"
+    },
+    ["A little bit of rolls"] = {
+        ["GradientName"] = "Roll100",
+        ["ZIndex"] = 2,
+        ["ImageId"] = 16776456110,
+        ["Name"] = "A little bit of rolls"
+    },
+    ["Would You Leave? / Nah I'd Roll"] = {
+        ["GradientName"] = "Roll10000",
+        ["ZIndex"] = 4,
+        ["ImageId"] = 16776530949,
+        ["Name"] = "Would You Leave? / Nah I'd Roll"
+    },
+    ["Spotted the Sol"] = {
+        ["GradientName"] = "Sol",
+        ["ZIndex"] = 21,
+        ["ImageId"] = 16776775881,
+        ["Name"] = "Spotted the Sol"
+    },
+    ["Break the limit"] = {
+        ["GradientName"] = "Digital",
+        ["ZIndex"] = 16,
+        ["ImageId"] = 16776759140,
+        ["Name"] = "Break the limit"
+    },
+    ["Touch the grass"] = {
+        ["GradientName"] = "Digital",
+        ["ZIndex"] = 14,
+        ["ImageId"] = 16776721677,
+        ["Name"] = "Touch the grass"
+    },
+    ["None"] = {["Name"] = "None"}
+}
