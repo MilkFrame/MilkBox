98be5cb78dc3173fe2b20b65b4240ff9d45cff85
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 02:59:19 +1000

    Create RecipesDB.lua

---
+++ GameData/Sol's RNG [15532962292]/RecipesDB.lua
@@ -0,0 +1,134 @@
+data = {
+    ["Luck Glove"] = {
+        3,
+        ["OneTimeCraft"] = true,
+        ["Type"] = "Tool",
+        ["Ingredients"] = {["Gear Basing"] = 1, ["Divinus"] = 2, ["Crystallized"] = 1, ["Rare"] = 3},
+        ["Desc"] = [[A glove with a really simple structure. \nHolding this will increase the luck for +25%.]]
+    },
+    ["Solar Device"] = {
+        5,
+        ["Type"] = "Tool",
+        ["Ingredients"] = {["Gear Basing"] = 1, ["Solar"] = 1, ["Divinus"] = 1, ["Rare"] = 1},
+        ["Desc"] = [[The heat straightens your spirit. \nHolding this will increase the luck for +50%.]]
+    },
+    ["Subzero Device"] = {
+        11,
+        ["OneTimeCraft"] = true,
+        ["Type"] = "Tool",
+        ["DoServerMessage"] = true,
+        ["Ingredients"] = {
+            ["Gear Basing"] = 5,
+            ["Permafrost"] = 2,
+            ["Crystallized"] = 600,
+            ["Aquatic"] = 2,
+            ["Glacier"] = 60,
+            ["Sidereum"] = 10,
+            ["Magnetic"] = 20,
+            ["Precious"] = 40
+        },
+        ["Desc"] = [[The device even freezes your soul.\nHolding this will increase the luck for +150% and decrease the rolling cooltime for -30%.]]
+    },
+    ["Gravitational Device"] = {
+        12,
+        ["OneTimeCraft"] = true,
+        ["Type"] = "Tool",
+        ["DoServerMessage"] = true,
+        ["Ingredients"] = {
+            ["Gravitational"] = 1,
+            ["BOUNDED"] = 3,
+            ["Exotic"] = 5,
+            ["Magnetic"] = 75,
+            ["Diaboli"] = 152,
+            ["Gear Basing"] = 15,
+            ["Sidereum"] = 31,
+            ["Nautilus"] = 5,
+            ["Precious"] = 152
+        },
+        ["Desc"] = [[The device provides you unstable gravity.\nBonus roll luck is increases by 6x]]
+    },
+    ["Eclipse"] = {
+        6,
+        ["DoServerMessage"] = true,
+        ["Type"] = "Aura",
+        ["Ingredients"] = {["Divinus"] = 1, ["Solar"] = 1, ["Lunar"] = 1},
+        ["Desc"] = "A big, great being that we cannot interfere with."
+    },
+    ["Windstorm Device"] = {
+        13,
+        ["OneTimeCraft"] = true,
+        ["Type"] = "Tool",
+        ["Ingredients"] = {
+            ["Gear Basing"] = 5,
+            ["Wind"] = 25,
+            ["Stormal"] = 1,
+            ["Aquatic"] = 1,
+            ["Sidereum"] = 4,
+            ["Precious"] = 12
+        },
+        ["Desc"] = [[The storm winds around you.\nHolding this will increase the luck for +115% and decrease the rolling cooltime for -25%.]]
+    },
+    ["Gear Basing"] = {
+        2,
+        ["Type"] = "Tool",
+        ["Ingredients"] = {["Good"] = 1, ["Rare"] = 1, ["Uncommon"] = 1, ["Common"] = 1},
+        ["Desc"] = "Gear basing is very important item for base crafting."
+    },
+    ["Gilded Coin"] = {14, ["Type"] = "Tool", ["Ingredients"] = {["Gilded"] = 1}, ["Desc"] = "That's shiny one!"},
+    ["Galactic Device"] = {
+        10,
+        ["OneTimeCraft"] = true,
+        ["Type"] = "Tool",
+        ["DoServerMessage"] = true,
+        ["Ingredients"] = {
+            ["Galaxy"] = 1,
+            ["Eclipse Device"] = 1,
+            ["Sapphire"] = 100,
+            ["Solar"] = 15,
+            ["Magnetic"] = 62,
+            ["Diaboli"] = 80,
+            ["Gear Basing"] = 25,
+            ["Comet"] = 3,
+            ["Lunar"] = 15
+        },
+        ["Desc"] = [[The device is composed with the power of galaxy.\nHolding this will increase the luck for +250% and decrease the rolling cooltime for -25%.]]
+    },
+    ["Exo Gauntlet"] = {
+        9,
+        ["OneTimeCraft"] = true,
+        ["Type"] = "Tool",
+        ["DoServerMessage"] = true,
+        ["Ingredients"] = {
+            ["Gear Basing"] = 3,
+            ["Gilded"] = 3,
+            ["Precious"] = 2,
+            ["Undead"] = 1,
+            ["Exotic"] = 1,
+            ["Magnetic"] = 2,
+            ["Sidereum"] = 1
+        },
+        ["Desc"] = [[Exo power enhances you.\nHolding this will increase the luck for +100% and decrease the rolling cooltime for -20%.]]
+    },
+    ["Lunar Device"] = {
+        4,
+        ["Type"] = "Tool",
+        ["Ingredients"] = {["Gear Basing"] = 1, ["Rare"] = 1, ["Divinus"] = 1, ["Lunar"] = 1},
+        ["Desc"] = [[Stillness clears your mind. \nHolding this will decrease the rolling cooltime for -15%.]]
+    },
+    ["Jackpot Gauntlet"] = {
+        8,
+        ["OneTimeCraft"] = true,
+        ["Type"] = "Tool",
+        ["DoServerMessage"] = true,
+        ["Ingredients"] = {["Gear Basing"] = 7, ["Jackpot"] = 77, ["Gilded"] = 77, ["Rare"] = 777},
+        ["Desc"] = [[Always bet on yourself.\nHolding this will increase the luck for +77% and decrease the rolling cooltime for -7%. get 77 cash when you hit the jackpot.]]
+    },
+    ["Eclipse Device"] = {
+        7,
+        ["OneTimeCraft"] = true,
+        ["Type"] = "Tool",
+        ["DoServerMessage"] = true,
+        ["Ingredients"] = {["Lunar Device"] = 1, ["Solar Device"] = 1, ["Eclipse"] = 1},
+        ["Desc"] = [[The faint light of the eclipse surrounds you.\nHolding this will increase the luck for +50% and decrease the rolling cooltime for -15%.]]
+    }
+}
