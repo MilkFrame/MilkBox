9cf9ca1d25c09bdbf844b246d0696c5d4f7a958d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 03:02:05 +1000

    Create RollSettings.lua

---
+++ GameData/Sol's RNG [15532962292]/RollSettings.lua
@@ -0,0 +1,14 @@
+data = {
+    ["Contexts"] = {
+        "There is still cooldown time remaining.",
+        "Roll Succeed.",
+        "Roll sequence is already in progress.",
+        "The player has not made a choice yet."
+    },
+    ["ROLL_DELAY_START"] = 0.1,
+    ["ROLL_DELAY_END"] = 0.3,
+    ["ROLL_COOLDOWN"] = 1,
+    ["BONUS_STACK"] = 10,
+    ["ROLL_DELAY_ICREASE"] = 0.025,
+    ["Headers"] = {"Forbidden", "Success", "Forbidden", "Forbidden"}
+}
