eb4c00720e6fad3d7ee5e578f07e32b91da0173f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 02:41:51 +1000

    Update SharedNametagData.lua

--- GameData/Club Stellar [7890640623]/SharedNametagData.lua
+++ GameData/Club Stellar [7890640623]/SharedNametagData.lua
@@ -1,17 +1,17 @@
 data = {
     ["MilestoneData"] = {
-        {["Threshold"] = 10000000, ["BoldLevel"] = true, ["Symbol"] = "â˜…"},
-        {["Threshold"] = 5000000, ["BoldLevel"] = true, ["Symbol"] = "â˜†"},
-        {["Threshold"] = 3000000, ["BoldLevel"] = true, ["Symbol"] = "â™¥"},
-        {["Threshold"] = 1000000, ["BoldLevel"] = true, ["Symbol"] = "â˜…"},
-        {["Threshold"] = 500000, ["BoldLevel"] = true, ["Symbol"] = "â™¢"},
-        {["Threshold"] = 300000, ["BoldLevel"] = true, ["Symbol"] = "â™¥"},
-        {["Threshold"] = 150000, ["BoldLevel"] = true, ["Symbol"] = "â˜…"},
-        {["Threshold"] = 75000, ["BoldLevel"] = true, ["Symbol"] = "â™¥"},
-        {["Threshold"] = 30000, ["BoldLevel"] = false, ["Symbol"] = "â™¡"},
-        {["Threshold"] = 15000, ["BoldLevel"] = false, ["Symbol"] = "â™¢"},
+        {["Threshold"] = 10000000, ["BoldLevel"] = true, ["Symbol"] = "★"},
+        {["Threshold"] = 5000000, ["BoldLevel"] = true, ["Symbol"] = "☆"},
+        {["Threshold"] = 3000000, ["BoldLevel"] = true, ["Symbol"] = "♥"},
+        {["Threshold"] = 1000000, ["BoldLevel"] = true, ["Symbol"] = "★"},
+        {["Threshold"] = 500000, ["BoldLevel"] = true, ["Symbol"] = "♢"},
+        {["Threshold"] = 300000, ["BoldLevel"] = true, ["Symbol"] = "♥"},
+        {["Threshold"] = 150000, ["BoldLevel"] = true, ["Symbol"] = "★"},
+        {["Threshold"] = 75000, ["BoldLevel"] = true, ["Symbol"] = "♥"},
+        {["Threshold"] = 30000, ["BoldLevel"] = false, ["Symbol"] = "♡"},
+        {["Threshold"] = 15000, ["BoldLevel"] = false, ["Symbol"] = "♢"},
         {["Threshold"] = 5000, ["BoldLevel"] = false, ["Symbol"] = ""},
         {["Threshold"] = 0, ["BoldLevel"] = false, ["Symbol"] = ""}
     },
-    ["GetDataForLevel"] = "function: 0x00000000ab32340b"
+    ["GetDataForLevel"] = "function: 0x00000000512218b9"
 }
