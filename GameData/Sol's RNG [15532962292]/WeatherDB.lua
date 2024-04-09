f8a566c92a7f70192204c58415bd221c748adce0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 02:49:04 +1000

    Create WeatherDB.lua

---
+++ GameData/Sol's RNG [15532962292]/WeatherDB.lua
@@ -0,0 +1,9 @@
+data = {
+    {["Value"] = "Normal", ["Duration"] = 1, ["Rarity"] = 2},
+    {["Value"] = "IsWindy", ["Duration"] = 120, ["Name"] = "Windy", ["Rarity"] = 500},
+    {["Value"] = "IsRainy", ["Duration"] = 120, ["Name"] = "Rainy", ["Rarity"] = 750},
+    {["Value"] = "IsSnowy", ["Duration"] = 120, ["Name"] = "Snowy", ["Rarity"] = 750},
+    {["EventRarity"] = 1000, ["Name"] = "Starfall", ["Value"] = "IsStarfall", ["Duration"] = 600, ["Rarity"] = 6000},
+    {["Value"] = "IsNull", ["Duration"] = 99, ["Name"] = "Null", ["Rarity"] = 33333},
+    {["Unobtainable"] = true, ["Name"] = "Glitched", ["Value"] = "IsGlitched", ["Duration"] = 164, ["Rarity"] = inf}
+}
