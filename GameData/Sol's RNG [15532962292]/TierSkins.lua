00ec35f0167ea03baaefa4e131d958df3286802b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 03:03:50 +1000

    Create TierSkins.lua

---
+++ GameData/Sol's RNG [15532962292]/TierSkins.lua
@@ -0,0 +1,73 @@
+data = {
+    ["Rage"] = {["Heated"] = {["Rarity"] = 1, ["CreatorId"] = 419860256}},
+    ["BOUNDED"] = {["UNBOUND"] = {["Rarity"] = 1}},
+    ["Impeached"] = {
+        ["impeach"] = {
+            ["LimitedTimestamp"] = 1714143600,
+            ["Rarity"] = 1,
+            ["Description"] = [=[im peach
+
+Aura dedicated to April Fool's Day. You can get it until [April 26, 2024 11:00 AM]]=]
+        }
+    },
+    ["Divinus"] = {["Love"] = {["Rarity"] = 0, ["Unobtainable"] = true}},
+    ["Exotic"] = {
+        ["APEX"] = {
+            ["AudioInfo"] = {["Name"] = "Panic Track", ["Author"] = "Remi Gallego"},
+            ["CreatorId"] = 419860256,
+            ["Rarity"] = 1
+        }
+    },
+    ["Magnetic"] = {["Reverse Polarity"] = {["Rarity"] = 1, ["CreatorId"] = 500806314}},
+    ["Diaboli"] = {["Void"] = {["Rarity"] = 1, ["CreatorId"] = 419860256}},
+    ["Undefined"] = {
+        ["Defined"] = {
+            ["LimitedTimestamp"] = 1714143600,
+            ["Rarity"] = 1,
+            ["Description"] = "Aura dedicated to April Fool's Day. You can get it until [April 26, 2024 11:00 AM]"
+        }
+    },
+    [":Flushed:"] = {
+        ["Heart Eye"] = {["Rarity"] = 0, ["Unobtainable"] = true},
+        ["Lobotomy"] = {["CreatorId"] = 198120391, ["Rarity"] = 1, ["ConceptId"] = 1616246197}
+    },
+    ["Celestial"] = {["Cupid"] = {["Rarity"] = 0, ["Unobtainable"] = true}},
+    ["Arcane"] = {
+        ["Legacy"] = {["Rarity"] = 1, ["CreatorId"] = 419860256},
+        ["Dark"] = {["Rarity"] = 2, ["CreatorId"] = 419860256}
+    },
+    ["Undead"] = {["Devil"] = {["Rarity"] = 1, ["CreatorId"] = 419860256}},
+    ["Chromatic"] = {
+        ["Exotic"] = {
+            ["Unobtainable"] = true,
+            ["CreatorId"] = 3536675145,
+            ["Rarity"] = 0,
+            ["AudioInfo"] = {["Name"] = "Cheatreal", ["Author"] = "t+pazolite"},
+            ["Description"] = "Inside of hollow and dark place."
+        },
+        ["Kromat1k"] = {
+            ["AudioInfo"] = {["Name"] = "The Great Strategy", ["Author"] = "badliz"},
+            ["LimitedTimestamp"] = 1714143600,
+            ["Rarity"] = 0.4,
+            ["Description"] = "Aura dedicated to April Fool's Day. You can get it until [April 26, 2024 11:00 AM]"
+        },
+        ["Genesis"] = {
+            ["AudioInfo"] = {["Name"] = "Something Different (Feat. Talurre) [Remastered]", ["Author"] = "Derpcat"},
+            ["Rarity"] = 1,
+            ["Description"] = [[Defining the beginning and end of the world.
+
+<b><font transparency=".75">WAKE
+UP
+FROM
+AWAY</font></b>]]
+        }
+    },
+    ["Aquatic"] = {["Flame"] = {["Rarity"] = 1, ["CreatorId"] = 1390696522}},
+    ["Lunar"] = {
+        ["Full Moon"] = {
+            ["AudioInfo"] = {["Name"] = "Shiny night", ["Author"] = "Kandera"},
+            ["CreatorId"] = 198120391,
+            ["Rarity"] = 1
+        }
+    }
+}
