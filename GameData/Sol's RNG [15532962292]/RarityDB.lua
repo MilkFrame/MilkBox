e748c8ad66070f2f413b7189d5d046829384b4eb
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 02:48:03 +1000

    Create RarityDB.lua

---
+++ GameData/Sol's RNG [15532962292]/RarityDB.lua
@@ -0,0 +1,244 @@
+data = {
+    {["Value"] = "Common", ["Rarity"] = 2},
+    {["Value"] = "Bleeding", ["Rarity"] = 4444},
+    {["Value"] = "Uncommon", ["Rarity"] = 4},
+    {["Value"] = "Good", ["SkinChance"] = 4000000, ["Rarity"] = 5},
+    {["Value"] = "Natural", ["Rarity"] = 8},
+    {["Value"] = "Rare", ["Rarity"] = 16},
+    {["Value"] = "Divinus", ["Rarity"] = 32},
+    {["Value"] = "Diaboli", ["Rarity"] = 1004},
+    {["Value"] = "Crystallized", ["Rarity"] = 64},
+    {["Value"] = "Rage", ["Rarity"] = 128},
+    {["Value"] = "Topaz", ["Rarity"] = 150},
+    {["Value"] = "Glacier", ["Rarity"] = 256, ["UnlockBy"] = "IsSnowy"},
+    {["Value"] = "Permafrost", ["Rarity"] = 24500, ["UnlockBy"] = "IsSnowy"},
+    {["Value"] = "Wind", ["CreatorId"] = 361208413, ["Rarity"] = 300, ["UnlockBy"] = "IsWindy"},
+    {["Value"] = "Ruby", ["Rarity"] = 350},
+    {["Value"] = "Emerald", ["Rarity"] = 500},
+    {["Value"] = "Gilded", ["Rarity"] = 512},
+    {["Value"] = "Jackpot", ["Rarity"] = 777},
+    {["Value"] = "Sapphire", ["Rarity"] = 800},
+    {["Value"] = "Aquamarine", ["Rarity"] = 900},
+    {["Value"] = "Precious", ["Rarity"] = 1024},
+    {["Value"] = "Magnetic", ["SkinChance"] = 0.2, ["Rarity"] = 2048},
+    {["Value"] = "Sidereum", ["Rarity"] = 4096},
+    {["Value"] = ":Flushed:", ["SkinChance"] = 10, ["Rarity"] = 6900},
+    {["Value"] = "Undead", ["SkinChance"] = 10, ["Rarity"] = 10000},
+    {["Value"] = "Stormal", ["Rarity"] = 30000, ["UnlockBy"] = "IsRainy"},
+    {["Value"] = "Lightning", ["CreatorId"] = 361208413, ["Rarity"] = 40000, ["Unobtainable"] = true},
+    {["Value"] = "Aquatic", ["Rarity"] = 40000},
+    {["Value"] = "Nautilus", ["CreatorId"] = 1450080989, ["Rarity"] = 70000},
+    {
+        ["Value"] = "Exotic",
+        ["SkinChance"] = 0.2,
+        ["Rarity"] = 99999,
+        ["AudioInfo"] = {["Name"] = "Harvest", ["Author"] = "ImCold"}
+    },
+    {
+        ["Value"] = "Jade",
+        ["Rarity"] = 125000,
+        ["AudioInfo"] = {["Name"] = "Stickerbrush Symphony", ["Author"] = "Donkey Kong County 2 OST"}
+    },
+    {
+        ["Value"] = "BOUNDED",
+        ["SkinChance"] = 10,
+        ["Rarity"] = 200000,
+        ["AudioInfo"] = {["Name"] = "Brooding Tension (b)", ["Author"] = "KPM Music"}
+    },
+    {
+        ["CreatorId"] = 419860256,
+        ["Rarity"] = 350000,
+        ["Value"] = "Celestial",
+        ["AudioInfo"] = {["Name"] = "Cycle to the Stars (a 30)", ["Author"] = "KPM Music"},
+        ["ConceptId"] = 83216827
+    },
+    {
+        ["Value"] = "Kyawthuite",
+        ["Rarity"] = 850000,
+        ["AudioInfo"] = {["Name"] = "Forest Interlude", ["Author"] = "Donkey Kong County 2 OST"}
+    },
+    {
+        ["Value"] = "Arcane",
+        ["SkinChance"] = 10,
+        ["Rarity"] = 1000000,
+        ["AudioInfo"] = {["Name"] = "Catching a Killer", ["Author"] = "The Box"}
+    },
+    {
+        ["Value"] = "Gravitational",
+        ["CreatorId"] = 1450080989,
+        ["Rarity"] = 2000000,
+        ["AudioInfo"] = {["Name"] = "Gravity Rainbow (a 60)", ["Author"] = "KPM Music"}
+    },
+    {
+        ["Value"] = "Virtual",
+        ["Rarity"] = 2500000,
+        ["AudioInfo"] = {["Name"] = "Virtual Utopia A", ["Author"] = "Sonoton Vanguard"}
+    },
+    {
+        ["CreatorId"] = 1087607753,
+        ["Rarity"] = 3000000,
+        ["Value"] = "Sailor",
+        ["AudioInfo"] = {["Name"] = "Encounter the Storm", ["Author"] = "Kandera"},
+        ["HipHeight"] = 2.5,
+        ["UnlockBy"] = "IsRainy"
+    },
+    {
+        ["Value"] = "Poseidon",
+        ["CreatorId"] = 1631971926,
+        ["Rarity"] = 3800000,
+        ["AudioInfo"] = {["Name"] = "Sea Bloom (a 30)", ["Author"] = "Roblox"}
+    },
+    {
+        ["Unobtainable"] = true,
+        ["CreatorId"] = 26139676,
+        ["Rarity"] = 5000000,
+        ["Value"] = "Therma",
+        ["AudioInfo"] = {["Name"] = "Blood Code", ["Author"] = "Le Castle Vania"},
+        ["IsHidden"] = true
+    },
+    {
+        ["Value"] = "Hades",
+        ["CreatorId"] = 1450080989,
+        ["Rarity"] = 6666666,
+        ["AudioInfo"] = {["Name"] = "Hordes of Hades (No Choir Version)", ["Author"] = "Liquid Cinema"}
+    },
+    {
+        ["CreatorId"] = 361208413,
+        ["Rarity"] = 7500000,
+        ["Value"] = "Hyper-Volt",
+        ["AudioInfo"] = {["Name"] = "Titan Dune (Archangel)", ["Author"] = "Two Steps From Hell"},
+        ["HipHeight"] = 0.5
+    },
+    {
+        ["SaveToFirst"] = true,
+        ["CreatorId"] = 1448645016,
+        ["Rarity"] = 12210110,
+        ["Value"] = "Glitch",
+        ["AudioInfo"] = {["Name"] = "Meteoric Impact", ["Author"] = "Kyutatsuki"},
+        ["UnlockBy"] = "IsGlitched"
+    },
+    {
+        ["Rarity"] = 20000000,
+        ["Value"] = "Chromatic",
+        ["SkinChance"] = 20.0000002,
+        ["AudioInfo"] = {["Name"] = "Panorama", ["Author"] = "Fishisuso"},
+        ["HipHeight"] = 1
+    },
+    {
+        ["Value"] = "Ethereal",
+        ["Rarity"] = 35000000,
+        ["AudioInfo"] = {["Name"] = "Singularity", ["Author"] = "Sergey Kolosov"}
+    },
+    {
+        ["Value"] = "Matrix",
+        ["Rarity"] = 50000000,
+        ["AudioInfo"] = {["Name"] = "Arcology On Permafrost", ["Author"] = "Camellia"}
+    },
+    {
+        ["Value"] = "AbyssalHunter",
+        ["AudioInfo"] = {["Name"] = "Treasures Within The Abomination", ["Author"] = "DM DOKURO"},
+        ["Rarity"] = 100000000,
+        ["UnlockBy"] = "IsRainy"
+    },
+    {
+        ["Value"] = "Impeached",
+        ["SkinChance"] = 50,
+        ["Rarity"] = 200000000,
+        ["AudioInfo"] = {["Name"] = "Threats of The Ocean Floor", ["Author"] = "DM DOKURO"}
+    },
+    {
+        ["Value"] = "Archangel",
+        ["CreatorId"] = 584488747,
+        ["Rarity"] = 250000000,
+        ["AudioInfo"] = {["Name"] = "Vani, The Blood Cleric", ["Author"] = "Dutonic"}
+    },
+    {
+        ["CreatorId"] = 3836821815,
+        ["Rarity"] = 5000,
+        ["Value"] = "Starlight",
+        ["AudioInfo"] = {["Name"] = "Galaxies", ["Author"] = "Bruton"},
+        ["UnlockBy"] = "IsStarfall"
+    },
+    {
+        ["CreatorId"] = 215403410,
+        ["Rarity"] = 12000,
+        ["Value"] = "Comet",
+        ["ConceptId"] = 215403410,
+        ["AudioInfo"] = {["Name"] = "Galaxies", ["Author"] = "Bruton"},
+        ["UnlockBy"] = "IsStarfall"
+    },
+    {
+        ["Value"] = "Galaxy",
+        ["AudioInfo"] = {["Name"] = "Andromeda Galaxy", ["Author"] = "Cezame"},
+        ["Rarity"] = 500000,
+        ["UnlockBy"] = "IsStarfall"
+    },
+    {
+        ["CreatorId"] = 561741827,
+        ["Rarity"] = 1000000,
+        ["Value"] = "Starscourge",
+        ["AudioInfo"] = {["Name"] = "Tech Anxiety", ["Author"] = "APM Music"},
+        ["UnlockBy"] = "IsStarfall"
+    },
+    {["Value"] = "Lunar", ["Rarity"] = 5000, ["UnlockBy"] = "IsNight"},
+    {
+        ["Rarity"] = 600000,
+        ["Value"] = "Twilight",
+        ["AudioInfo"] = {["Name"] = "New Horizon", ["Author"] = "Protostar"},
+        ["HipHeight"] = 1,
+        ["UnlockBy"] = "IsNight"
+    },
+    {["Value"] = "Solar", ["Rarity"] = 5000, ["UnlockBy"] = "IsDay"},
+    {["Value"] = "Undefined", ["SkinChance"] = 50, ["Rarity"] = 1111, ["UnlockBy"] = "IsNull"},
+    {["Value"] = "Eclipse", ["Rarity"] = 30000, ["Craftable"] = true},
+    {
+        ["IsHidden"] = true,
+        ["Rarity"] = inf,
+        ["Value"] = "Sovereign",
+        ["AudioInfo"] = {["Name"] = "Floor Realization Theme", ["Author"] = "Studio EIM"},
+        ["OwnerId"] = 419860256,
+        ["Unobtainable"] = true
+    },
+    {
+        ["IsHidden"] = true,
+        ["Rarity"] = inf,
+        ["Value"] = "Sol",
+        ["AudioInfo"] = {["Name"] = "Artificial Snow", ["Author"] = "Camellia"},
+        ["OwnerId"] = 419860256,
+        ["Unobtainable"] = true
+    },
+    {
+        ["IsHidden"] = true,
+        ["Rarity"] = inf,
+        ["AudioInfo"] = {["Name"] = "HALL", ["Author"] = "Frums"},
+        ["Value"] = "Abomination",
+        ["Unobtainable"] = true,
+        ["OwnerId"] = 361208413,
+        ["HipHeight"] = 2
+    },
+    {
+        ["IsHidden"] = true,
+        ["Rarity"] = inf,
+        ["AudioInfo"] = {["Name"] = "LAYERS", ["Author"] = "KORAII"},
+        ["Value"] = "LAYERS",
+        ["CameraOffset"] = Vector3.new(0, 6, 0),
+        ["OwnerId"] = 361208413,
+        ["Unobtainable"] = true
+    },
+    {
+        ["IsHidden"] = true,
+        ["Rarity"] = inf,
+        ["Value"] = "Kyawthuite : Iridescent Memory",
+        ["AudioInfo"] = {["Name"] = "Forest Interlude", ["Author"] = "Donkey Kong County 2 OST"},
+        ["Unobtainable"] = true
+    },
+    {
+        ["IsHidden"] = true,
+        ["CreatorId"] = 1913463034,
+        ["Rarity"] = inf,
+        ["Value"] = "Blossom",
+        ["AudioInfo"] = {["Name"] = "Inevitability", ["Author"] = "Tido Kang"},
+        ["Unobtainable"] = true
+    },
+    {["IsHidden"] = true, ["Rarity"] = inf, ["Value"] = "Stardust", ["Unobtainable"] = true, ["HipHeight"] = 1}
+}
