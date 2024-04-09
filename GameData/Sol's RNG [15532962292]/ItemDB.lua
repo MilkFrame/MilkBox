4631e9008b85f1ddf2bc1cc748dd874cd7041727
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 02:59:35 +1000

    Create ItemDB.lua

---
+++ GameData/Sol's RNG [15532962292]/ItemDB.lua
@@ -0,0 +1,262 @@
+data = {
+    ["Universe Potion I"] = {
+        ["Description"] = "It contains the mystery of the universe.",
+        ["Use"] = "function: 0x00000000dfd8a7b0",
+        ["PreviewModel"] = "Universe Potion I",
+        ["Expendable"] = true,
+        ["Sound"] = {["Play"] = "function: 0x000000007132fc80"},
+        ["TextColor"] = Color3.fromRGB(51, 88, 130)
+    },
+    ["Huge Fortune Potion I"] = {
+        ["Description"] = "Those who take this potion will be blessed with good luck.",
+        ["Use"] = "function: 0x0000000057541b20",
+        ["PreviewModel"] = "Fortune Potion I",
+        ["Expendable"] = true,
+        ["Sound"] = {["Play"] = "function: 0x000000007132fc80"},
+        ["TextColor"] = Color3.fromRGB(105, 255, 122)
+    },
+    ["Word's hat"] = {
+        ["PreviewModel"] = "Word's hat",
+        ["Description"] = "word is not toto",
+        ["TextColor"] = Color3.fromRGB(0, 170, 255)
+    },
+    ["Lucky Potion"] = {
+        ["Description"] = "Lucky Potion.",
+        ["Use"] = "function: 0x00000000a0eae518",
+        ["PreviewModel"] = "Lucky Potion",
+        ["Expendable"] = true,
+        ["Sound"] = {["Play"] = "function: 0x000000007132fc80"},
+        ["TextColor"] = Color3.fromRGB(114, 191, 94)
+    },
+    ["Subspace Tripmine"] = {
+        ["GearType"] = "Right",
+        ["PlayAnimation"] = true,
+        ["Description"] = "Powerful.",
+        ["PreviewModel"] = "SubspaceTripmine",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 0, 212)
+    },
+    ["dwjk pillow"] = {
+        ["GearType"] = "Right",
+        ["PlayAnimation"] = true,
+        ["Description"] = ".",
+        ["PreviewModel"] = "DwjkPillow",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(178, 228, 255)
+    },
+    ["Lunar Device"] = {
+        ["GearType"] = "Right",
+        ["Description"] = [[Stillness clears your mind. \nHolding this will decrease the rolling cooltime for -15%.]],
+        ["PreviewModel"] = "Lunar Device",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(125, 121, 255)
+    },
+    ["Haste Potion II"] = {
+        ["Description"] = "The power of the wind speeds you up.",
+        ["Use"] = "function: 0x00000000ec235360",
+        ["PreviewModel"] = "Haste Potion II",
+        ["Expendable"] = true,
+        ["Sound"] = {["Play"] = "function: 0x000000007132fc80"},
+        ["TextColor"] = Color3.fromRGB(4, 175, 236)
+    },
+    ["Coin"] = {
+        ["Description"] = "Coin.",
+        ["Use"] = "function: 0x000000009000ecd0",
+        ["PreviewModel"] = "Coin",
+        ["Expendable"] = true,
+        ["Sound"] = "Money",
+        ["TextColor"] = Color3.fromRGB(245, 205, 48)
+    },
+    ["Gilded Coin"] = {
+        ["Description"] = "That's shiny one!",
+        ["Use"] = "function: 0x00000000c196e4a8",
+        ["PreviewModel"] = "Gilded Coin",
+        ["Expendable"] = true,
+        ["Sound"] = "Money",
+        ["TextColor"] = Color3.fromRGB(239, 184, 56)
+    },
+    ["Exo Gauntlet"] = {
+        ["GearType"] = "Right",
+        ["Description"] = [[Exo power enhances you.\nHolding this will increase the luck for +100% and decrease the rolling cooltime for -20%.]],
+        ["PreviewModel"] = "Exo Gauntlet",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(114, 201, 255)
+    },
+    ["test item(left)"] = {
+        ["GearType"] = "Left",
+        ["Description"] = "I love sun, moon, and star.",
+        ["PreviewModel"] = "test item",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(0, 170, 255)
+    },
+    ["Haste Potion I"] = {
+        ["Description"] = "The power of the wind speeds you up.",
+        ["Use"] = "function: 0x00000000253ebea8",
+        ["PreviewModel"] = "Haste Potion I",
+        ["Expendable"] = true,
+        ["Sound"] = {["Play"] = "function: 0x000000007132fc80"},
+        ["TextColor"] = Color3.fromRGB(128, 187, 219)
+    },
+    ["Fortune Potion I"] = {
+        ["Description"] = "Those who take this potion will be blessed with good luck.",
+        ["Use"] = "function: 0x00000000f95fae00",
+        ["PreviewModel"] = "Fortune Potion I",
+        ["Expendable"] = true,
+        ["Sound"] = {["Play"] = "function: 0x000000007132fc80"},
+        ["TextColor"] = Color3.fromRGB(105, 255, 122)
+    },
+    ["Solar Device"] = {
+        ["GearType"] = "Right",
+        ["Description"] = [[The heat straightens your spirit. \nHolding this will increase the luck for +50%.]],
+        ["PreviewModel"] = "Solar Device",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 170, 110)
+    },
+    ["XC' Signature"] = {
+        ["GearType"] = "Right",
+        ["PlayAnimation"] = true,
+        ["Description"] = "...",
+        ["PreviewModel"] = "Signature",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 255, 255)
+    },
+    ["test item"] = {
+        ["GearType"] = "Right",
+        ["PlayAnimation"] = true,
+        ["Description"] = "I love sun, moon, and star.",
+        ["PreviewModel"] = "test item",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(0, 170, 255)
+    },
+    ["Gear Basing"] = {
+        ["PreviewModel"] = "Gear Basing",
+        ["Description"] = "Gear basing is very important item for base crafting.",
+        ["TextColor"] = Color3.fromRGB(91, 93, 105)
+    },
+    ["shoney pillow"] = {
+        ["GearType"] = "Right",
+        ["PlayAnimation"] = true,
+        ["Description"] = "The end of a Samsung engineer. This might give you some luck....",
+        ["PreviewModel"] = "ShoneyPillow",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(61, 139, 255)
+    },
+    ["Heavenly Potion II"] = {
+        ["Description"] = "Those who take this potion will be heavenly blessed.",
+        ["Use"] = "function: 0x0000000059cb9488",
+        ["PreviewModel"] = "Heavenly Potion II",
+        ["Expendable"] = true,
+        ["Sound"] = {["Play"] = "function: 0x000000007132fc80"},
+        ["TextColor"] = Color3.fromRGB(255, 152, 220)
+    },
+    ["Luck Glove"] = {
+        ["GearType"] = "Right",
+        ["Description"] = [[A glove with a really simple structure. \nHolding this will increase the luck for +25%.]],
+        ["PreviewModel"] = "Luck Glove",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(91, 93, 105)
+    },
+    ["Adele Doll"] = {
+        ["GearType"] = "Right",
+        ["PlayAnimation"] = true,
+        ["Description"] = "...",
+        ["PreviewModel"] = "Adele",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 255, 255)
+    },
+    ["Heavenly Potion I"] = {
+        ["Description"] = "Those who take this potion will be heavenly blessed.",
+        ["Use"] = "function: 0x0000000046912f50",
+        ["PreviewModel"] = "Heavenly Potion I",
+        ["Expendable"] = true,
+        ["Sound"] = {["Play"] = "function: 0x000000007132fc80"},
+        ["TextColor"] = Color3.fromRGB(224, 178, 208)
+    },
+    ["MandooMon Doll"] = {
+        ["GearType"] = "Right",
+        ["PlayAnimation"] = true,
+        ["Description"] = "...",
+        ["PreviewModel"] = "MandooDoll",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 255, 255)
+    },
+    ["Jackpot Gauntlet"] = {
+        ["GearType"] = "Left",
+        ["Description"] = [[Always bet on yourself.\nHolding this will increase the luck for +77% and decrease the rolling cooltime for -7%. get 77 cash when you hit the jackpot.]],
+        ["PreviewModel"] = "Jackpot Gauntlet",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(239, 184, 56)
+    },
+    ["Haste Potion III"] = {
+        ["Description"] = "The power of the wind speeds you up.",
+        ["Use"] = "function: 0x00000000cba875f8",
+        ["PreviewModel"] = "Haste Potion III",
+        ["Expendable"] = true,
+        ["Sound"] = {["Play"] = "function: 0x000000007132fc80"},
+        ["TextColor"] = Color3.fromRGB(9, 137, 207)
+    },
+    ["Windstorm Device"] = {
+        ["GearType"] = "Right",
+        ["Description"] = [[The storm winds around you.\nHolding this will increase the luck for +115% and decrease the rolling cooltime for -25%.]],
+        ["PreviewModel"] = "Windstorm Device",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(93, 164, 165)
+    },
+    ["Fortune Potion III"] = {
+        ["Description"] = "Those who take this potion will be blessed with good luck.",
+        ["Use"] = "function: 0x000000007ab5d6b0",
+        ["PreviewModel"] = "Fortune Potion III",
+        ["Expendable"] = true,
+        ["Sound"] = {["Play"] = "function: 0x000000007132fc80"},
+        ["TextColor"] = Color3.fromRGB(44, 101, 29)
+    },
+    ["Stella's star"] = {
+        ["PreviewModel"] = "StellaStar",
+        ["Description"] = "The star Stella lost.",
+        ["TextColor"] = Color3.fromRGB(224, 255, 166)
+    },
+    ["Fortune Potion II"] = {
+        ["Description"] = "Those who take this potion will be blessed with good luck.",
+        ["Use"] = "function: 0x000000003f8af258",
+        ["PreviewModel"] = "Fortune Potion II",
+        ["Expendable"] = true,
+        ["Sound"] = {["Play"] = "function: 0x000000007132fc80"},
+        ["TextColor"] = Color3.fromRGB(52, 142, 64)
+    },
+    ["Speed Potion"] = {
+        ["Description"] = "Speed Potion.",
+        ["Use"] = "function: 0x00000000023e2dc8",
+        ["PreviewModel"] = "Speed Potion",
+        ["Expendable"] = true,
+        ["Sound"] = {["Play"] = "function: 0x000000007132fc80"},
+        ["TextColor"] = Color3.fromRGB(110, 153, 202)
+    },
+    ["Gravitational Device"] = {
+        ["GearType"] = "Left",
+        ["Description"] = [[The device provides you unstable gravity.\nBonus roll luck is increases by 6x]],
+        ["PreviewModel"] = "Gravitational Device",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(98, 37, 209)
+    },
+    ["Subzero Device"] = {
+        ["GearType"] = "Right",
+        ["Description"] = [[The device even freezes your soul.\nHolding this will increase the luck for +150% and decrease the rolling cooltime for -30%.]],
+        ["PreviewModel"] = "Subzero Device",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(110, 153, 202)
+    },
+    ["Galactic Device"] = {
+        ["GearType"] = "Right",
+        ["Description"] = [[The device is composed with the power of galaxy.\nHolding this will increase the luck for +250% and decrease the rolling cooltime for -30%.]],
+        ["PreviewModel"] = "Galactic Device",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(140, 91, 159)
+    },
+    ["Eclipse Device"] = {
+        ["GearType"] = "Right",
+        ["Description"] = [[The faint light of the eclipse surrounds you.\nHolding this will increase the luck for +50% and decrease the rolling cooltime for -15%.]],
+        ["PreviewModel"] = "Eclipse Device",
+        ["Equipable"] = true,
+        ["TextColor"] = Color3.fromRGB(213, 115, 61)
+    }
+}
