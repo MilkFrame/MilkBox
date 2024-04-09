086e7cecb92480529b7379aaf2135960c0cf6408
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 02:51:32 +1000

    Create Descriptions.lua

---
+++ GameData/Sol's RNG [15532962292]/Descriptions.lua
@@ -0,0 +1,37 @@
+data = {
+    ["Kyawthuite"] = [[Long, long ago, there was a 'boy' who stayed alone in the empty space of a deep cave. He is still waiting for the day when he can sit on the moon together with the girl who suddently left when he was young , somewhere in the forest and talk again, <font color="rgb(157, 133, 255)">once in a thousand years, in the deepest part of the forest.</font>]],
+    ["BOUNDED"] = [[It's too dangerous.
+Although it was sealed by an <font color="rgb(255, 255, 112)">Ancient Power</font> hundreds of thousands of years ago, its consciousness appears to be <font color="rgb(255, 0, 0)">alive</font>.]],
+    ["Exotic"] = [[<font color="rgb(255,0,0)">No one</font> knows where it originated or how <font color="rgb(85, 0, 255)">old</font> it is.]],
+    ["Celestial"] = "The divine, complex being",
+    ["Lightning"] = [[[Community aura]
+Made by xcvbwer123]],
+    ["Arcane"] = [[People couldn't understand it. 
+But they used to call it a disaster. 
+People's such vigilance may be a good act, but it considered their boundaries a pathetic act. 
+It was too powerful.]],
+    ["LAYERS"] = "i want to beat this map ngl",
+    ["Nautilus"] = [[[Community aura]
+Made by UnworldlyDice]],
+    ["Lunar"] = [[It was made with <font color="rgb(183, 135, 255)">Moonlight</font> by an <font color="rgb(213, 165, 255)">Unknown being</font> on a quiet night.]],
+    ["Gravitational"] = [[A gravitational anomaly beyond human comprehension. 
+Thought to originate from the same matter which makes up the mysterious "Great Attractor".]],
+    ["Matrix"] = "Nobody can touch, watch or feel this.",
+    ["Ethereal"] = "In a country where the vast majority of people attend church, there is a deep-rooted belief in powers, either from 'God' or from the ‘dark side’. Their god, is believed a spirit or being that has great power, strength, knowledge, etc., and that can affect nature and the lives of people. Someone will get this, and be an absolute and powerful being...",
+    ["LUNATIC"] = [[<font color="rgb(211, 34, 255)">A dazzling, incomplete one</font> that has variable form. Anyone who encounter this becomes <font color="rgb(211, 34, 255)">frantic.</font>]],
+    ["AbyssalHunter"] = [[<font color="rgb(66, 116, 255)">A Mysterious Hunter who has been roaming under the depths of the ocean floor, slaughtering all deep sea creatures without mercy.</font>]],
+    ["Solar"] = [[It was made with <font color="rgb(255, 142, 62)">Sunshine</font> by an <font color="rgb(255, 166, 102)">Unknown being</font> on a bright day.]],
+    ["Sol?"] = [[<font color="rgb(255, 0, 102)">A strange thing created by a group that seeks chaos to oppose sol.
+It's actually stronger than sol, but it's horribly unstable and has a very foolish power that could be the key to the destruction of the world.
+sol and this are still fighting and probably will be forever.</font>]],
+    ["Impeached"] = [[<font color="rgb(91, 58, 255)">Purple, The Colour of Royalty, the Monarch haven been stripped from his title of Humanities Protector from the Shadows. 
+What is left for our fallen hero?</font>]],
+    ["Twilight"] = [[A jewel that shines brightly <font color="rgb(157, 133, 255)">once in a thousand years, in the deepest part of the forest.</font> No one has ever seen how bright that light is. According to rumors, the moon gives gifts to those who look at the gem with a pure heart without any evil...]],
+    ["Eclipse"] = "A big, great being that we cannot interfere with.",
+    ["Therma"] = "A figure of light and heat, the epitome of energy that never meets the darkness, and in the darkest of moments, they did shine the brightest. Their thermal abilities were described to be comparable to the sparks of creation, Therma.",
+    ["Sol"] = [[<font color="rgb(128, 225, 255)">This is called the worldview itself. It is stronger than everything in this world combined and has outstanding power. It is owned by one person who is at the top among the many engineers who created the world.</font>]],
+    ["Hyper-Volt"] = "Special thanks to @HailFirz for making assets for this aura",
+    ["Chromatic"] = [["Feel my unstoppable beats!"]],
+    ["Undefined"] = [[<font color="rgb(0,0,0)">Its too dark here..</font>]],
+    ["Galaxy"] = [[It came from the <font color="rgb(131, 120, 255)">Incredibly vast universe</font> itself.]]
+}
