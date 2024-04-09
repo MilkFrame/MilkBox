ff8163348f8bb4a0fbbb48f871bd145534e19941
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 03:00:01 +1000

    Create WeatherDB2.lua

---
+++ GameData/Sol's RNG [15532962292]/WeatherDB2.lua
@@ -0,0 +1,149 @@
+data = {
+    ["IsWindy"] = {
+        ["Grass"] = Color3.fromRGB(40, 127, 71),
+        ["Leaves"] = Color3.fromRGB(75, 151, 75),
+        ["Floor"] = Color3.fromRGB(75, 151, 75),
+        ["Underground"] = Color3.fromRGB(86, 66, 54),
+        ["Woods"] = Color3.fromRGB(97, 82, 46),
+        ["AtmosphereGoal"] = {
+            ["Density"] = 0.3,
+            ["Decay"] = Color3.fromRGB(106, 112, 125),
+            ["Color"] = Color3.fromRGB(199, 199, 199),
+            ["Glare"] = 0,
+            ["Haze"] = 0,
+            ["Offset"] = 0.25
+        },
+        ["LightGrass"] = Color3.fromRGB(75, 151, 75),
+        ["Rock"] = Color3.fromRGB(100, 100, 100),
+        ["Mountain"] = Color3.fromRGB(86, 66, 54)
+    },
+    ["IsRainy"] = {
+        ["Grass"] = Color3.fromRGB(40, 127, 71),
+        ["Leaves"] = Color3.fromRGB(75, 151, 75),
+        ["Floor"] = Color3.fromRGB(75, 151, 75),
+        ["Underground"] = Color3.fromRGB(86, 66, 54),
+        ["Woods"] = Color3.fromRGB(97, 82, 46),
+        ["Brightness"] = 0,
+        ["FadeInfo"] = "Time:2 DelayTime:0 RepeatCount:0 Reverses:False EasingDirection:In EasingStyle:Quart",
+        ["AtmosphereGoal"] = {
+            ["Density"] = 0.6,
+            ["Decay"] = Color3.fromRGB(36, 33, 79),
+            ["Color"] = Color3.fromRGB(86, 86, 86),
+            ["Glare"] = 0,
+            ["Haze"] = 10,
+            ["Offset"] = 0.2
+        },
+        ["LightGrass"] = Color3.fromRGB(75, 151, 75),
+        ["Rock"] = Color3.fromRGB(100, 100, 100),
+        ["Mountain"] = Color3.fromRGB(86, 66, 54)
+    },
+    ["IsSnowy"] = {
+        ["Grass"] = Color3.fromRGB(193, 221, 255),
+        ["Leaves"] = Color3.fromRGB(219, 238, 255),
+        ["Floor"] = Color3.fromRGB(219, 238, 255),
+        ["Underground"] = Color3.fromRGB(86, 66, 54),
+        ["Woods"] = Color3.fromRGB(97, 82, 46),
+        ["AtmosphereGoal"] = {
+            ["Density"] = 0.7,
+            ["Decay"] = Color3.fromRGB(106, 112, 125),
+            ["Color"] = Color3.fromRGB(201, 229, 255),
+            ["Glare"] = 0,
+            ["Haze"] = 0,
+            ["Offset"] = 0.25
+        },
+        ["LightGrass"] = Color3.fromRGB(219, 238, 255),
+        ["Rock"] = Color3.fromRGB(171, 187, 193),
+        ["Mountain"] = Color3.fromRGB(86, 66, 54)
+    },
+    ["IsNull"] = {
+        ["Grass"] = Color3.fromRGB(62, 62, 62),
+        ["Leaves"] = Color3.fromRGB(72, 72, 72),
+        ["Floor"] = Color3.fromRGB(18, 18, 18),
+        ["Underground"] = Color3.fromRGB(0, 0, 0),
+        ["Woods"] = Color3.fromRGB(226, 226, 226),
+        ["AtmosphereGoal"] = {
+            ["Density"] = 0.5,
+            ["Decay"] = Color3.fromRGB(255, 255, 255),
+            ["Color"] = Color3.fromRGB(0, 0, 0),
+            ["Glare"] = 1,
+            ["Haze"] = 5,
+            ["Offset"] = 0.25
+        },
+        ["LightGrass"] = Color3.fromRGB(38, 38, 38),
+        ["Rock"] = Color3.fromRGB(255, 255, 255),
+        ["Mountain"] = Color3.fromRGB(0, 0, 0)
+    },
+    ["IsNight"] = {
+        ["Grass"] = Color3.fromRGB(40, 127, 71),
+        ["Leaves"] = Color3.fromRGB(75, 151, 75),
+        ["Floor"] = Color3.fromRGB(75, 151, 75),
+        ["Underground"] = Color3.fromRGB(86, 66, 54),
+        ["Woods"] = Color3.fromRGB(97, 82, 46),
+        ["AtmosphereGoal"] = {
+            ["Density"] = 0.3,
+            ["Decay"] = Color3.fromRGB(106, 112, 125),
+            ["Color"] = Color3.fromRGB(199, 199, 199),
+            ["Glare"] = 0,
+            ["Haze"] = 0,
+            ["Offset"] = 0.25
+        },
+        ["LightGrass"] = Color3.fromRGB(75, 151, 75),
+        ["Rock"] = Color3.fromRGB(100, 100, 100),
+        ["Mountain"] = Color3.fromRGB(86, 66, 54)
+    },
+    ["IsStarfall"] = {
+        ["Grass"] = Color3.fromRGB(80, 147, 255),
+        ["Leaves"] = Color3.fromRGB(80, 147, 255),
+        ["Floor"] = Color3.fromRGB(15, 16, 18),
+        ["Underground"] = Color3.fromRGB(0, 0, 0),
+        ["Woods"] = Color3.fromRGB(37, 43, 49),
+        ["AtmosphereGoal"] = {
+            ["Density"] = 0.5,
+            ["Decay"] = Color3.fromRGB(106, 112, 125),
+            ["Color"] = Color3.fromRGB(43, 111, 199),
+            ["Glare"] = 0,
+            ["Haze"] = 5,
+            ["Offset"] = 0.25
+        },
+        ["LightGrass"] = Color3.fromRGB(37, 43, 49),
+        ["Rock"] = Color3.fromRGB(38, 72, 184),
+        ["Mountain"] = Color3.fromRGB(0, 0, 0)
+    },
+    ["IsDay"] = {
+        ["Grass"] = Color3.fromRGB(40, 127, 71),
+        ["Leaves"] = Color3.fromRGB(75, 151, 75),
+        ["Floor"] = Color3.fromRGB(75, 151, 75),
+        ["Underground"] = Color3.fromRGB(86, 66, 54),
+        ["Woods"] = Color3.fromRGB(97, 82, 46),
+        ["AtmosphereGoal"] = {
+            ["Density"] = 0.3,
+            ["Decay"] = Color3.fromRGB(106, 112, 125),
+            ["Color"] = Color3.fromRGB(199, 199, 199),
+            ["Glare"] = 0,
+            ["Haze"] = 0,
+            ["Offset"] = 0.25
+        },
+        ["LightGrass"] = Color3.fromRGB(75, 151, 75),
+        ["Rock"] = Color3.fromRGB(100, 100, 100),
+        ["Mountain"] = Color3.fromRGB(86, 66, 54)
+    },
+    ["IsGlitched"] = {
+        ["Grass"] = Color3.fromRGB(62, 62, 62),
+        ["Leaves"] = Color3.fromRGB(70, 162, 148),
+        ["Floor"] = Color3.fromRGB(18, 10, 10),
+        ["Underground"] = Color3.fromRGB(106, 106, 106),
+        ["Woods"] = Color3.fromRGB(58, 58, 58),
+        ["FadeInfo"] = "Time:0.01 DelayTime:0 RepeatCount:0 Reverses:False EasingDirection:In EasingStyle:Quart",
+        ["AtmosphereGoal"] = {
+            ["Density"] = 0.546,
+            ["Decay"] = Color3.fromRGB(0, 0, 0),
+            ["Color"] = Color3.fromRGB(28, 44, 45),
+            ["Glare"] = 0,
+            ["Haze"] = 10,
+            ["Offset"] = 1
+        },
+        ["LightGrass"] = Color3.fromRGB(63, 30, 30),
+        ["Rock"] = Color3.fromRGB(73, 128, 136),
+        ["Mountain"] = Color3.fromRGB(28, 49, 48)
+    }
+}
