f89511e9998d433f0dde157ab920958cc9513425
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:40:16 +1000

    Create Bosses.lua

---
+++ GameData/Pet Catchers! [16510724413]/Bosses.lua
@@ -0,0 +1,67 @@
+data = {
+    ["the-kraken"] = {
+        ["Region"] = "Sunset Shores",
+        ["Rewards"] = {
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Wildberry", ["Amount"] = 1000}, ["Chance"] = 30},
+            {["Item"] = {["Type"] = "CaptureCube", ["Name"] = "Epic", ["Amount"] = 5}, ["Chance"] = 20.5},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Mystery Egg", ["Amount"] = 75}, ["Chance"] = 14.5},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Coin Elixir", ["Amount"] = 2}, ["Chance"] = 10},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Lucky Elixir", ["Amount"] = 2}, ["Chance"] = 7},
+            {["Item"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 100}, ["Chance"] = 5},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Elite Mystery Egg", ["Amount"] = 75}, ["Chance"] = 5},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "RandomRune", ["Amount"] = 1}, ["Chance"] = 3},
+            {["Item"] = {["Type"] = "CaptureCube", ["Name"] = "Legendary", ["Amount"] = 3}, ["Chance"] = 2},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Respawn Tome", ["Amount"] = 1}, ["Chance"] = 2},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Knowledge Scroll", ["Amount"] = 1}, ["Chance"] = 0.5},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Golden Cherry", ["Amount"] = 1}, ["Chance"] = 0.5},
+            {
+                ["Item"] = {["Color"] = "Default", ["Type"] = "Mount", ["Name"] = "Comet Board", ["Amount"] = 1},
+                ["Chance"] = 0.05
+            },
+            {["Item"] = {["Type"] = "Pet", ["Name"] = "Kraken", ["Amount"] = 1}, ["Chance"] = 0.01}
+        },
+        ["DisplayName"] = "The Kraken",
+        ["HealthScaleDivisor"] = 7,
+        ["Guaranteed"] = {
+            {["Amount"] = 350000, ["Type"] = "XP"},
+            {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 450},
+            {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 12000}
+        },
+        ["Requirement"] = 200,
+        ["Cooldown"] = 2700,
+        ["RewardAmount"] = 1,
+        ["MaxHealth"] = 48000,
+        ["Icon"] = "rbxassetid://16285150340"
+    },
+    ["king-slime"] = {
+        ["Region"] = "Auburn Woods",
+        ["Rewards"] = {
+            {["Item"] = {["Type"] = "CaptureCube", ["Name"] = "Rare", ["Amount"] = 15}, ["Chance"] = 26},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Wildberry", ["Amount"] = 750}, ["Chance"] = 22},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Mystery Egg", ["Amount"] = 50}, ["Chance"] = 20},
+            {["Item"] = {["Type"] = "CaptureCube", ["Name"] = "Epic", ["Amount"] = 5}, ["Chance"] = 13.8},
+            {["Item"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 100}, ["Chance"] = 5},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Lucky Elixir", ["Amount"] = 1}, ["Chance"] = 5},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Elite Mystery Egg", ["Amount"] = 50}, ["Chance"] = 5},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Timeful Tome", ["Amount"] = 1}, ["Chance"] = 2},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "RandomRune", ["Amount"] = 1}, ["Chance"] = 1},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Golden Cherry", ["Amount"] = 1}, ["Chance"] = 0.2},
+            {
+                ["Item"] = {["Color"] = "Default", ["Type"] = "Mount", ["Name"] = "Pool Ball", ["Amount"] = 1},
+                ["Chance"] = 0.08
+            },
+            {["Item"] = {["Type"] = "Pet", ["Name"] = "Royal Slime", ["Amount"] = 1}, ["Chance"] = 0.02}
+        },
+        ["DisplayName"] = "King Slime",
+        ["HealthScaleDivisor"] = 5.5,
+        ["Guaranteed"] = {
+            {["Amount"] = 75000, ["Type"] = "XP"},
+            {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 3500}
+        },
+        ["Requirement"] = 10,
+        ["Cooldown"] = 1200,
+        ["RewardAmount"] = 1,
+        ["MaxHealth"] = 6000,
+        ["Icon"] = "rbxassetid://16271063633"
+    }
+}
