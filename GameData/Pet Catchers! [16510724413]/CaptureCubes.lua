696ae073cae2f3f4d6f7640a6aae60429cfab5c4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:35:14 +1000

    Create CaptureCubes.lua

---
+++ GameData/Pet Catchers! [16510724413]/CaptureCubes.lua
@@ -0,0 +1,32 @@
+data = {
+    ["Legendary"] = {
+        ["ShortDescription"] = "Captures Legendary pets.",
+        ["LayoutOrder"] = 4,
+        ["Rarity"] = "Legendary",
+        ["Description"] = "A capture cube used to catch Legendary pets."
+    },
+    ["Common"] = {
+        ["ShortDescription"] = "Captures Common pets.",
+        ["LayoutOrder"] = 1,
+        ["Rarity"] = "Common",
+        ["Description"] = "A capture cube used to catch Common pets."
+    },
+    ["Epic"] = {
+        ["ShortDescription"] = "Captures Epic pets.",
+        ["LayoutOrder"] = 3,
+        ["Rarity"] = "Epic",
+        ["Description"] = "A capture cube used to catch Epic pets."
+    },
+    ["Perfect"] = {
+        ["ShortDescription"] = "Captures any pet.",
+        ["LayoutOrder"] = 5,
+        ["Rarity"] = "Secret",
+        ["Description"] = "Captures any pet with the first attempt."
+    },
+    ["Rare"] = {
+        ["ShortDescription"] = "Captures Rare pets.",
+        ["LayoutOrder"] = 2,
+        ["Rarity"] = "Rare",
+        ["Description"] = "A capture cube used to catch Rare pets."
+    }
+}
