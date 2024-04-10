11d5bf3c63ad3f327f69ca208f98bc9ddc9771c3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:35:44 +1000

    Create Gamepasses.lua

---
+++ GameData/Pet Catchers! [16510724413]/Gamepasses.lua
@@ -0,0 +1,74 @@
+data = {
+    ["Triple Hatch"] = {
+        ["LayoutOrder"] = 3,
+        ["Image"] = "rbxassetid://16457005808",
+        ["Description"] = "Allows you to hatch 3 Eggs at once!",
+        ["Cost"] = 749,
+        ["Id"] = 722265788,
+        ["Gradient"] = "0 1 0.807843 0.32549 0 1 1 0.533333 0 0 "
+    },
+    ["Super AFK"] = {
+        ["LayoutOrder"] = 4,
+        ["Image"] = "rbxassetid://16397622148",
+        ["Description"] = "Instantly unlocks all AFK Upgrades as well as Auto Fishing and more!",
+        ["Cost"] = 1599,
+        ["Id"] = 722504606,
+        ["Gradient"] = "0 0 0.607843 0.0588235 0 1 0 0 0 0 "
+    },
+    ["VIP"] = {
+        ["LayoutOrder"] = 0,
+        ["Image"] = "rbxassetid://16394902310",
+        ["Description"] = "Gives you an exclusive Pet, Mount, Title, and +25% Bonus Coins and Pet XP!",
+        ["Cost"] = 349,
+        ["Id"] = 722785092,
+        ["Gradient"] = "0 1 1 0 0 1 1 0.588235 0 0 "
+    },
+    ["+100 Storage"] = {
+        ["LayoutOrder"] = 9,
+        ["Image"] = "rbxassetid://16493715969",
+        ["Description"] = "Increases the amount of pets you can store by +100!",
+        ["Cost"] = 79,
+        ["Id"] = 722671220,
+        ["Gradient"] = "0 1 0.784314 0.490196 0 1 1 0.784314 0.490196 0 "
+    },
+    ["+300 Storage"] = {
+        ["LayoutOrder"] = 8,
+        ["Image"] = "rbxassetid://16493677122",
+        ["Description"] = "Increases the amount of pets you can store by +300!",
+        ["Cost"] = 249,
+        ["Id"] = 722336777,
+        ["Gradient"] = "0 0.588235 0.392157 1 0 1 1 0.588235 0.901961 0 "
+    },
+    ["+550 Storage"] = {
+        ["LayoutOrder"] = 7,
+        ["Image"] = "rbxassetid://16457103901",
+        ["Description"] = "Increases the amount of pets you can store by +550!",
+        ["Cost"] = 649,
+        ["Id"] = 722095895,
+        ["Gradient"] = "0 0.196078 0.862745 1 0 1 0.235294 1 0.490196 0 "
+    },
+    ["Lucky Cubes"] = {
+        ["LayoutOrder"] = 6,
+        ["Image"] = "rbxassetid://16397027766",
+        ["Description"] = "All cubes are 2x as likely to successfully capture a pet!",
+        ["Cost"] = 549,
+        ["Id"] = 722173933,
+        ["Gradient"] = "0 0.254902 1 0.352941 0 1 0.686275 1 0.333333 0 "
+    },
+    ["Lucky Eggs"] = {
+        ["LayoutOrder"] = 5,
+        ["Image"] = "rbxassetid://16397000368",
+        ["Description"] = "2x Luck when hatching Legendary and Secret Pets from Eggs!",
+        ["Cost"] = 749,
+        ["Id"] = 722573505,
+        ["Gradient"] = "0 0.470588 0.901961 1 0 1 0.156863 0.705882 1 0 "
+    },
+    ["More Pets!"] = {
+        ["LayoutOrder"] = 1,
+        ["Image"] = "rbxassetid://16396891565",
+        ["Description"] = "Gives you an extra +3 Pets Equipped slots!",
+        ["Cost"] = 999,
+        ["Id"] = 722555495,
+        ["Gradient"] = "0 1 0.45098 0.901961 0 1 0.196078 1 0.705882 0 "
+    }
+}
