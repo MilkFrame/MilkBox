12ea8edfff892c431c856ed139fd73b7f2ab04c9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:32:12 +1000

    Create Constants.lua

---
+++ GameData/Pet Catchers! [16510724413]/Constants.lua
@@ -0,0 +1,144 @@
+data = {
+    ["CatchRateFactor"] = 2,
+    ["ServerEventCooldown"] = 1,
+    ["TooltipTagOrder"] = {
+        ["Normal"] = 5,
+        ["Animated"] = 2,
+        ["Shiny"] = 4,
+        ["Fusion"] = 1,
+        ["Unique"] = 3,
+        ["Secret"] = 0
+    },
+    ["FishingCastTime"] = 4,
+    ["EasyLegendaryThreshold"] = 0.1,
+    ["RenderDistance"] = {["Low"] = 1, ["High"] = 3, ["Ultra"] = 5, ["Medium"] = 2},
+    ["BaseHealth"] = 100,
+    ["QuestIndexUnlock"] = "skater-started",
+    ["PityBaseAttempts"] = 2,
+    ["EggHatchDurations"] = {["Legendary"] = 1200, ["Common"] = 30, ["Epic"] = 300, ["Secret"] = 3600, ["Rare"] = 60},
+    ["MinigameSpecialRewardColor"] = Color3.fromRGB(180, 105, 255),
+    ["BasePetInventory"] = 15,
+    ["RarityOrder"] = {["Legendary"] = 4, ["Common"] = 1, ["Epic"] = 3, ["Secret"] = 5, ["Rare"] = 2},
+    ["ButtonSmallColorConfig"] = {
+        ["Color"] = Color3.fromRGB(255, 255, 255),
+        ["TweenInfo"] = "Time:0.1 DelayTime:0 RepeatCount:0 Reverses:False EasingDirection:Out EasingStyle:Quint",
+        ["Enter"] = Color3.fromRGB(255, 255, 255),
+        ["Press"] = Color3.fromRGB(190, 230, 255)
+    },
+    ["BossMaxDrops"] = 25,
+    ["MountModifierColors"] = {"Red", "Orange", "Yellow", "Green", "Blue", "Purple"},
+    ["ShinyColor"] = Color3.fromRGB(255, 204, 0),
+    ["SailorRepeatableId"] = "sailor-repeatable",
+    ["BossMusic"] = "rbxassetid://16269383792",
+    ["PetShinyStatMultiplier"] = 2,
+    ["BasePetsEquipped"] = 2,
+    ["SailorRepeatableUnlock"] = "sailor-diamond",
+    ["ButtonInactiveColorConfig"] = {
+        ["Color"] = Color3.fromRGB(160, 176, 188),
+        ["TweenInfo"] = "Time:0.1 DelayTime:0 RepeatCount:0 Reverses:False EasingDirection:Out EasingStyle:Quint",
+        ["Enter"] = Color3.fromRGB(180, 199, 212),
+        ["Press"] = Color3.fromRGB(240, 183, 40)
+    },
+    ["PetCatchCooldown"] = 3,
+    ["XP"] = {["Color"] = Color3.fromRGB(24, 147, 255), ["Image"] = "rbxassetid://16168689114"},
+    ["MountModifierColorValues"] = {
+        ["Blue"] = Color3.fromRGB(50, 118, 255),
+        ["Green"] = Color3.fromRGB(50, 255, 50),
+        ["Purple"] = Color3.fromRGB(187, 50, 255),
+        ["Yellow"] = Color3.fromRGB(255, 255, 50),
+        ["Orange"] = Color3.fromRGB(255, 135, 50),
+        ["Red"] = Color3.fromRGB(255, 50, 50)
+    },
+    ["PetShinyCharmSlotIncrease"] = 1,
+    ["PityMaxMultiplier"] = 4,
+    ["AFKPassName"] = "Super AFK",
+    ["PetStatScaling"] = {["Legendary"] = 2, ["Common"] = 1.25, ["Epic"] = 1.75, ["Secret"] = 2, ["Rare"] = 1.5},
+    ["PetMaximumInteractionDistance"] = 30,
+    ["BaseWalkSpeed"] = 20,
+    ["ServerEventDuration"] = 900,
+    ["RobuxEmoji"] = "",
+    ["QuestCraftingUnlock"] = "blacksmith-showdown",
+    ["BossRespawnItem"] = "Respawn Tome",
+    ["RichTextColor"] = {
+        ["Purple"] = Color3.fromRGB(205, 143, 255),
+        ["Pink"] = Color3.fromRGB(255, 170, 247),
+        ["Blue"] = Color3.fromRGB(133, 141, 255),
+        ["Green"] = Color3.fromRGB(151, 255, 163),
+        ["Cyan"] = Color3.fromRGB(156, 247, 255),
+        ["Yellow"] = Color3.fromRGB(255, 245, 105),
+        ["Orange"] = Color3.fromRGB(255, 144, 89),
+        ["Red"] = Color3.fromRGB(255, 97, 108)
+    },
+    ["PetCritMultiplier"] = 2,
+    ["ShinySpawnChance"] = 0.05,
+    ["BruhBountyUnlock"] = "bruh-bounties",
+    ["ButtonLightColorConfig"] = {
+        ["Color"] = Color3.fromRGB(156, 224, 243),
+        ["TweenInfo"] = "Time:0.1 DelayTime:0 RepeatCount:0 Reverses:False EasingDirection:Out EasingStyle:Quint",
+        ["Enter"] = Color3.fromRGB(164, 235, 255),
+        ["Press"] = Color3.fromRGB(255, 215, 29)
+    },
+    ["PetOffsetDistance"] = 5,
+    ["DefaultCraftingSlots"] = 1,
+    ["PetMaximumLevel"] = {["Legendary"] = 25, ["Common"] = 25, ["Epic"] = 25, ["Secret"] = 25, ["Rare"] = 25},
+    ["ChatTagsAttribute"] = "ChatTags",
+    ["MaxCraftingSlots"] = 3,
+    ["CloseButtonConfig"] = {
+        ["Color"] = Color3.fromRGB(255, 70, 70),
+        ["TweenInfo"] = "Time:0.1 DelayTime:0 RepeatCount:0 Reverses:False EasingDirection:Out EasingStyle:Quint",
+        ["Enter"] = Color3.fromRGB(255, 93, 93),
+        ["Press"] = Color3.fromRGB(255, 215, 29)
+    },
+    ["ConfirmButtonConfig"] = {
+        ["Color"] = Color3.fromRGB(67, 234, 55),
+        ["TweenInfo"] = "Time:0.1 DelayTime:0 RepeatCount:0 Reverses:False EasingDirection:Out EasingStyle:Quint",
+        ["Enter"] = Color3.fromRGB(93, 255, 93),
+        ["Press"] = Color3.fromRGB(255, 215, 29)
+    },
+    ["AFKConfigRarity"] = {"any", "common", "rare", "epic", "low-tier-legendary", "high-tier-legendary", "secret"},
+    ["ChanceForMountColor"] = 10,
+    ["ExtraCraftingSlots"] = {
+        {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 1250},
+        {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 35000}
+    },
+    ["PetCharmSlots"] = {["Secret"] = 2, ["Legendary"] = 2},
+    ["ButtonActivatedColorConfig"] = {
+        ["Color"] = Color3.fromRGB(255, 215, 29),
+        ["TweenInfo"] = "Time:0.1 DelayTime:0 RepeatCount:0 Reverses:False EasingDirection:Out EasingStyle:Quint",
+        ["Press"] = Color3.fromRGB(240, 183, 40)
+    },
+    ["MaxCraftingAmount"] = 999,
+    ["BruhBountyId"] = "bruh-bounty",
+    ["ToolbarSlots"] = 6,
+    ["MaxGoldenTickets"] = 7,
+    ["PityLuckRarityMaxRequirement"] = {["Legendary"] = 16, ["Common"] = 2, ["Epic"] = 8, ["Secret"] = 16, ["Rare"] = 4},
+    ["PityExponent"] = 2,
+    ["FriendshipBuff"] = 1.05,
+    ["PurpleButtonConfig"] = {
+        ["Color"] = Color3.fromRGB(251, 121, 255),
+        ["TweenInfo"] = "Time:0.1 DelayTime:0 RepeatCount:0 Reverses:False EasingDirection:Out EasingStyle:Quint",
+        ["Enter"] = Color3.fromRGB(255, 166, 221),
+        ["Press"] = Color3.fromRGB(255, 215, 29)
+    },
+    ["PityMultiplierIncrease"] = 0.2,
+    ["PetMaximumOwnerDistance"] = 200,
+    ["ButtonColorConfig"] = {
+        ["Color"] = Color3.fromRGB(0, 166, 236),
+        ["TweenInfo"] = "Time:0.1 DelayTime:0 RepeatCount:0 Reverses:False EasingDirection:Out EasingStyle:Quint",
+        ["Enter"] = Color3.fromRGB(77, 220, 236),
+        ["Press"] = Color3.fromRGB(236, 236, 52)
+    },
+    ["FeaturedEndTime"] = "1709856000000",
+    ["PetCritChance"] = 0.25,
+    ["AFKRegionSlots"] = 4,
+    ["HealthBarColors"] = {["Default"] = Color3.fromRGB(0, 234, 255), ["Low"] = Color3.fromRGB(255, 44, 44)},
+    ["RarityXPRequired"] = {
+        ["EasyLegendary"] = 5000000,
+        ["Legendary"] = 30000000,
+        ["Common"] = 250000,
+        ["Epic"] = 1200000,
+        ["Secret"] = 250000000,
+        ["Rare"] = 500000
+    },
+    ["TimeToRegen"] = 5
+}
