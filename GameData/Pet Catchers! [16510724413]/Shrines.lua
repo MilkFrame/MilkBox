21a35aa17d8a0fad4387db335468a51367454881
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:36:56 +1000

    Create Shrines.lua

---
+++ GameData/Pet Catchers! [16510724413]/Shrines.lua
@@ -0,0 +1,84 @@
+data = {
+    ["egg"] = {
+        ["Pool"] = {
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Mystery Egg", ["Amount"] = 30}, ["Chance"] = 65},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Elite Mystery Egg", ["Amount"] = 30}, ["Chance"] = 35}
+        },
+        ["Region"] = "Sunset Shores",
+        ["Amount"] = 10,
+        ["Cooldown"] = 3600,
+        ["DisplayName"] = "Egg"
+    },
+    ["radioactive"] = {
+        ["Pool"] = {
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Radioactive Jelly", ["Amount"] = 10}, ["Chance"] = 95},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Voiding Scroll", ["Amount"] = 1}, ["Chance"] = 5}
+        },
+        ["Region"] = "Gloomy Grotto",
+        ["Amount"] = 8,
+        ["Cooldown"] = 3600,
+        ["DisplayName"] = "Radioactive"
+    },
+    ["cherry"] = {
+        ["Pool"] = {{["Item"] = {["Type"] = "Treat", ["Name"] = "Golden Cherry", ["Amount"] = 2}, ["Chance"] = 100}},
+        ["Region"] = "Magma Basin",
+        ["Amount"] = 1,
+        ["Cooldown"] = 43200,
+        ["DisplayName"] = "Cherry"
+    },
+    ["berry"] = {
+        ["Pool"] = {{["Item"] = {["Type"] = "Treat", ["Name"] = "Wildberry", ["Amount"] = 350}, ["Chance"] = 100}},
+        ["Region"] = "Mellow Meadows",
+        ["Amount"] = 1,
+        ["Cooldown"] = 600,
+        ["DisplayName"] = "Berry"
+    },
+    ["gem"] = {
+        ["Pool"] = {{["Item"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 350}, ["Chance"] = 100}},
+        ["Region"] = "Sunset Shores",
+        ["Amount"] = 1,
+        ["Cooldown"] = 3600,
+        ["DisplayName"] = "Gem"
+    },
+    ["rune"] = {
+        ["Pool"] = {
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Seafarer Rune", ["Amount"] = 1}, ["Chance"] = 3.5},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Fatal Strike Rune", ["Amount"] = 1}, ["Chance"] = 1},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Lifesteal Rune", ["Amount"] = 1}, ["Chance"] = 15},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Shrine Rune", ["Amount"] = 1}, ["Chance"] = 15},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Deadly Rune", ["Amount"] = 1}, ["Chance"] = 20},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Looting Rune", ["Amount"] = 1}, ["Chance"] = 20},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Critical Hit Rune", ["Amount"] = 1}, ["Chance"] = 1},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "High Roller Rune", ["Amount"] = 1}, ["Chance"] = 1},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Gamer Rune", ["Amount"] = 1}, ["Chance"] = 1}
+        },
+        ["Region"] = "Magma Basin",
+        ["Amount"] = 2,
+        ["Cooldown"] = 7200,
+        ["DisplayName"] = "Rune"
+    },
+    ["ticket"] = {
+        ["Pool"] = {{["Item"] = {["Amount"] = 2, ["Type"] = "GoldenTicket"}, ["Chance"] = 100}},
+        ["Region"] = "Dusty Dunes",
+        ["Amount"] = 1,
+        ["Cooldown"] = 3600,
+        ["DisplayName"] = "Ticket"
+    },
+    ["cube"] = {
+        ["Pool"] = {{["Item"] = {["Type"] = "CaptureCube", ["Name"] = "Rare", ["Amount"] = 20}, ["Chance"] = 90}},
+        ["Region"] = "Mellow Meadows",
+        ["Amount"] = 1,
+        ["Cooldown"] = 600,
+        ["DisplayName"] = "Cube"
+    },
+    ["better-cube"] = {
+        ["Pool"] = {
+            {["Item"] = {["Type"] = "CaptureCube", ["Name"] = "Epic", ["Amount"] = 20}, ["Chance"] = 50},
+            {["Item"] = {["Type"] = "CaptureCube", ["Name"] = "Legendary", ["Amount"] = 1}, ["Chance"] = 50}
+        },
+        ["Region"] = "Gloomy Grotto",
+        ["Amount"] = 5,
+        ["Cooldown"] = 3600,
+        ["DisplayName"] = "Super Cube"
+    }
+}
