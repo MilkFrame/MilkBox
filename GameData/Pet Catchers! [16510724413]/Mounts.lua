6c839613c461e55af31fcb2940e27881151b9252
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:39:32 +1000

    Create Mounts.lua

---
+++ GameData/Pet Catchers! [16510724413]/Mounts.lua
@@ -0,0 +1,242 @@
+data = {
+    ["Capsule"] = {
+        ["SpeedMultiplier"] = 1.5,
+        ["Type"] = "Ball",
+        ["Description"] = "This should help to get around a bit faster.",
+        ["Viewport"] = {
+            ["Scale"] = 0.85,
+            ["FOV"] = 1,
+            ["CFrame"] = CFrame.new(
+                0,
+                0,
+                0,
+                -0.766044497,
+                0,
+                -0.642787576,
+                -0.454519451,
+                0.707106769,
+                0.54167527,
+                0.454519451,
+                0.707106769,
+                -0.54167527
+            )
+        },
+        ["HasColor"] = true,
+        ["LayoutOrder"] = 2,
+        ["Rarity"] = "Rare",
+        ["Offset"] = Vector3.new(0, -0.5, 0)
+    },
+    ["Pool Ball"] = {
+        ["SpeedMultiplier"] = 3,
+        ["Type"] = "Ball",
+        ["Description"] = "Works on any surface, not just pool tables.",
+        ["Viewport"] = {
+            ["Scale"] = 0.85,
+            ["FOV"] = 1,
+            ["CFrame"] = CFrame.new(
+                0,
+                0,
+                0,
+                -0.766044497,
+                0,
+                -0.642787576,
+                -0.454519451,
+                0.707106769,
+                0.54167527,
+                0.454519451,
+                0.707106769,
+                -0.54167527
+            )
+        },
+        ["HasColor"] = true,
+        ["LayoutOrder"] = 4,
+        ["Rarity"] = "Secret",
+        ["Offset"] = Vector3.new(0, 2, 0)
+    },
+    ["Hoverboard"] = {
+        ["SpeedMultiplier"] = 2,
+        ["Type"] = "Hoverboard",
+        ["Description"] = "A basic hoverboard.",
+        ["Viewport"] = {
+            ["Scale"] = 1,
+            ["FOV"] = 1,
+            ["CFrame"] = CFrame.new(
+                0,
+                0,
+                0,
+                0.586824059,
+                -0.492403835,
+                0.642787576,
+                0.790662169,
+                0.51965636,
+                -0.323744386,
+                -0.174615651,
+                0.698208928,
+                0.694272041
+            )
+        },
+        ["LayoutOrder"] = 1,
+        ["AnimationId"] = "rbxassetid://16547816407",
+        ["Rarity"] = "Common"
+    },
+    ["Boombox"] = {
+        ["SpeedMultiplier"] = 3.5,
+        ["Type"] = "Hoverboard",
+        ["Description"] = "Plays the beat of the region you're in.",
+        ["Viewport"] = {
+            ["Scale"] = 1,
+            ["FOV"] = 1,
+            ["CFrame"] = CFrame.new(
+                0,
+                0,
+                0,
+                0.586824059,
+                -0.492403835,
+                0.642787576,
+                0.790662169,
+                0.51965636,
+                -0.323744386,
+                -0.174615651,
+                0.698208928,
+                0.694272041
+            )
+        },
+        ["HasColor"] = true,
+        ["LayoutOrder"] = 6,
+        ["AnimationId"] = "rbxassetid://16547816407",
+        ["Rarity"] = "Secret"
+    },
+    ["Ray"] = {
+        ["SpeedMultiplier"] = 3.5,
+        ["Type"] = "Hoverboard",
+        ["Description"] = "A hoverboard modeled after the majestic stingray. Swift and sleek.",
+        ["Viewport"] = {
+            ["Scale"] = 1,
+            ["FOV"] = 1,
+            ["CFrame"] = CFrame.new(
+                0,
+                0,
+                0,
+                0.586824059,
+                -0.492403835,
+                0.642787576,
+                0.790662169,
+                0.51965636,
+                -0.323744386,
+                -0.174615651,
+                0.698208928,
+                0.694272041
+            )
+        },
+        ["HasColor"] = true,
+        ["LayoutOrder"] = 7,
+        ["AnimationId"] = "rbxassetid://16547816407",
+        ["Rarity"] = "Secret"
+    },
+    ["Golden Jetpack"] = {
+        ["SpeedMultiplier"] = 3,
+        ["Type"] = "Jetpack",
+        ["Description"] = "Fly around in style with this Golden Jetpack.",
+        ["Viewport"] = {
+            ["Scale"] = 0.9,
+            ["FOV"] = 1,
+            ["CFrame"] = CFrame.new(
+                0,
+                0,
+                0,
+                0.707106769,
+                0,
+                -0.707106769,
+                -0.298836231,
+                0.906307757,
+                -0.298836231,
+                0.640856326,
+                0.42261827,
+                0.640856326
+            )
+        },
+        ["LayoutOrder"] = 3,
+        ["AnimationId"] = "rbxassetid://16547829356",
+        ["Rarity"] = "Legendary"
+    },
+    ["Comet Board"] = {
+        ["SpeedMultiplier"] = 3.5,
+        ["Type"] = "Hoverboard",
+        ["Description"] = "Take a cosmic journey on this board, with swirling nebula colors.",
+        ["Viewport"] = {
+            ["Scale"] = 1,
+            ["FOV"] = 1,
+            ["CFrame"] = CFrame.new(
+                0,
+                0,
+                0,
+                0.586824059,
+                -0.492403835,
+                0.642787576,
+                0.790662169,
+                0.51965636,
+                -0.323744386,
+                -0.174615651,
+                0.698208928,
+                0.694272041
+            )
+        },
+        ["HasColor"] = true,
+        ["LayoutOrder"] = 4,
+        ["AnimationId"] = "rbxassetid://16547816407",
+        ["Rarity"] = "Secret"
+    },
+    ["Jetpack"] = {
+        ["SpeedMultiplier"] = 3.5,
+        ["Type"] = "Jetpack",
+        ["Description"] = "Jet around with this hi-tech personal flying device.",
+        ["Viewport"] = {
+            ["Scale"] = 0.9,
+            ["FOV"] = 1,
+            ["CFrame"] = CFrame.new(
+                0,
+                0,
+                0,
+                0.707106769,
+                0,
+                -0.707106769,
+                -0.298836231,
+                0.906307757,
+                -0.298836231,
+                0.640856326,
+                0.42261827,
+                0.640856326
+            )
+        },
+        ["HasColor"] = true,
+        ["LayoutOrder"] = 5,
+        ["AnimationId"] = "rbxassetid://16547829356",
+        ["Rarity"] = "Secret"
+    },
+    ["Prismatic Hoverboard"] = {
+        ["SpeedMultiplier"] = 3.5,
+        ["Type"] = "Hoverboard",
+        ["Description"] = "A hoverboard that shimmers in all colors imaginable.",
+        ["Viewport"] = {
+            ["Scale"] = 1,
+            ["FOV"] = 1,
+            ["CFrame"] = CFrame.new(
+                0,
+                0,
+                0,
+                0.586824059,
+                -0.492403835,
+                0.642787576,
+                0.790662169,
+                0.51965636,
+                -0.323744386,
+                -0.174615651,
+                0.698208928,
+                0.694272041
+            )
+        },
+        ["LayoutOrder"] = 5,
+        ["AnimationId"] = "rbxassetid://16547816407",
+        ["Rarity"] = "Secret"
+    }
+}
