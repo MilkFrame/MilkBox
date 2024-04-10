64a04a1519377c008053d4af1a5f2484278b92f2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:36:23 +1000

    Create Chests.lua

---
+++ GameData/Pet Catchers! [16510724413]/Chests.lua
@@ -0,0 +1,121 @@
+data = {
+    ["beach-island"] = {
+        ["Loot"] = {
+            {["Type"] = "Powerup", ["Name"] = "Timeful Tome", ["Amount"] = 1},
+            {["Type"] = "Treat", ["Name"] = "Seafarer Rune", ["Amount"] = 1},
+            {["Type"] = "Treat", ["Name"] = "Knowledge Scroll", ["Amount"] = 5}
+        },
+        ["Region"] = "Sunset Shores"
+    },
+    ["dunes-tower"] = {
+        ["Loot"] = {
+            {["Type"] = "Powerup", ["Name"] = "Timeful Tome", ["Amount"] = 1},
+            {["Amount"] = 1, ["Type"] = "GoldenTicket"}
+        },
+        ["Region"] = "Dusty Dunes"
+    },
+    ["grotto-cave"] = {
+        ["Loot"] = {
+            {["Type"] = "Treat", ["Name"] = "Looting Rune", ["Amount"] = 2},
+            {["Type"] = "Treat", ["Name"] = "Deadly Rune", ["Amount"] = 2},
+            {["Type"] = "Treat", ["Name"] = "Mystery Scroll", ["Amount"] = 2}
+        },
+        ["Region"] = "Gloomy Grotto"
+    },
+    ["magma-ladder"] = {
+        ["Loot"] = {
+            {["Type"] = "Treat", ["Name"] = "Wildberry", ["Amount"] = 1000},
+            {["Type"] = "Powerup", ["Name"] = "Respawn Tome", ["Amount"] = 1},
+            {["Type"] = "Treat", ["Name"] = "Radioactive Jelly", ["Amount"] = 350}
+        },
+        ["Region"] = "Magma Basin"
+    },
+    ["woods-blacksmith"] = {
+        ["Loot"] = {
+            {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 1200},
+            {["Type"] = "CaptureCube", ["Name"] = "Epic", ["Amount"] = 1},
+            {["Type"] = "Powerup", ["Name"] = "Respawn Tome", ["Amount"] = 1}
+        },
+        ["Region"] = "Auburn Woods"
+    },
+    ["dunes-house"] = {
+        ["Loot"] = {
+            {["Type"] = "Treat", ["Name"] = "Golden Cherry", ["Amount"] = 2},
+            {["Type"] = "Powerup", ["Name"] = "Prismatic Sundae", ["Amount"] = 1}
+        },
+        ["Region"] = "Dusty Dunes"
+    },
+    ["woods-logs"] = {
+        ["Loot"] = {{["Type"] = "Treat", ["Name"] = "Wildberry", ["Amount"] = 1200}},
+        ["Region"] = "Auburn Woods"
+    },
+    ["meadows-island"] = {
+        ["Loot"] = {
+            {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 450},
+            {["Type"] = "CaptureCube", ["Name"] = "Rare", ["Amount"] = 3},
+            {["Type"] = "CaptureCube", ["Name"] = "Epic", ["Amount"] = 1}
+        },
+        ["Region"] = "Mellow Meadows"
+    },
+    ["magma-peak"] = {
+        ["Loot"] = {
+            {["Type"] = "Treat", ["Name"] = "Golden Cherry", ["Amount"] = 5},
+            {["Type"] = "Powerup", ["Name"] = "Timeful Tome", ["Amount"] = 1},
+            {["Type"] = "Powerup", ["Name"] = "Respawn Tome", ["Amount"] = 1},
+            {["Amount"] = 1, ["Type"] = "GoldenTicket"}
+        },
+        ["Region"] = "Magma Basin"
+    },
+    ["frosty-ladder"] = {
+        ["Loot"] = {
+            {["Type"] = "Powerup", ["Name"] = "Coin Elixir", ["Amount"] = 1},
+            {["Type"] = "Powerup", ["Name"] = "Lucky Elixir", ["Amount"] = 1},
+            {["Type"] = "CaptureCube", ["Name"] = "Epic", ["Amount"] = 5}
+        },
+        ["Region"] = "Frosty Peaks"
+    },
+    ["dunes-crates"] = {
+        ["Loot"] = {
+            {["Type"] = "Powerup", ["Name"] = "Respawn Tome", ["Amount"] = 1},
+            {["Amount"] = 1, ["Type"] = "GoldenTicket"}
+        },
+        ["Region"] = "Dusty Dunes"
+    },
+    ["beach-edge"] = {
+        ["Loot"] = {
+            {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 250},
+            {["Type"] = "Treat", ["Name"] = "Seafarer Rune", ["Amount"] = 1}
+        },
+        ["Region"] = "Sunset Shores"
+    },
+    ["frosty-top"] = {
+        ["Loot"] = {
+            {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 2000},
+            {["Type"] = "Powerup", ["Name"] = "Respawn Tome", ["Amount"] = 1},
+            {["Type"] = "CaptureCube", ["Name"] = "Legendary", ["Amount"] = 1}
+        },
+        ["Region"] = "Frosty Peaks"
+    },
+    ["park-hedge"] = {
+        ["Loot"] = {
+            {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 300},
+            {["Type"] = "Treat", ["Name"] = "Wildberry", ["Amount"] = 15}
+        },
+        ["Region"] = "Pet Park"
+    },
+    ["park-mountain"] = {
+        ["Loot"] = {
+            {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 550},
+            {["Type"] = "CaptureCube", ["Name"] = "Rare", ["Amount"] = 20},
+            {["Type"] = "Treat", ["Name"] = "Wildberry", ["Amount"] = 35}
+        },
+        ["Region"] = "Pet Park"
+    },
+    ["meadows-waterfall"] = {
+        ["Loot"] = {
+            {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 1200},
+            {["Type"] = "CaptureCube", ["Name"] = "Rare", ["Amount"] = 10}
+        },
+        ["Region"] = "Mellow Meadows"
+    }
+}
