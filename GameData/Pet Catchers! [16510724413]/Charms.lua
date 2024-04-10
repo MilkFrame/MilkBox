449d74868486c93a0ec8acbfaa42e394a8122e46
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:32:29 +1000

    Create Charms.lua

---
+++ GameData/Pet Catchers! [16510724413]/Charms.lua
@@ -0,0 +1,74 @@
+data = {
+    ["Seafarer"] = {
+        ["LayoutOrder"] = 5,
+        ["Emoji"] = "🐟",
+        ["Description"] = "MULTIPLIER more luck to catch Legendary fish.",
+        ["Weight"] = 3.5,
+        ["Color"] = Color3.fromRGB(173, 225, 250),
+        ["Extra"] = {["Multiplier"] = {1.1, 1.15, 1.2, 1.4, 1.75}}
+    },
+    ["Fatal Strike"] = {
+        ["LayoutOrder"] = 2,
+        ["Emoji"] = "💀",
+        ["Description"] = "Increases pets damage by MULTIPLIER to Bosses below 50% Health.",
+        ["Weight"] = 1,
+        ["Color"] = Color3.fromRGB(26, 255, 0),
+        ["Extra"] = {["Multiplier"] = {1.05, 1.1, 1.15, 1.25, 1.5}}
+    },
+    ["Lifesteal"] = {
+        ["LayoutOrder"] = 9,
+        ["Emoji"] = "❤️",
+        ["Description"] = "Gain INCREASE Health when this pet deals damage.",
+        ["Weight"] = 15,
+        ["Color"] = Color3.fromRGB(232, 26, 74),
+        ["Extra"] = {["Increase"] = {5, 10, 15, 20, 25}}
+    },
+    ["Shrine"] = {
+        ["LayoutOrder"] = 6,
+        ["Emoji"] = "💫",
+        ["Description"] = [[CHANCE <font color="#fff569">chance</font> to increase drops from shrines by INCREASE.]],
+        ["Weight"] = 15,
+        ["Color"] = Color3.fromRGB(225, 232, 159),
+        ["Extra"] = {["Chance"] = {5, 7, 10, 13, 18}, ["Increase"] = {1, 1, 1, 1, 1}}
+    },
+    ["Deadly"] = {
+        ["LayoutOrder"] = 7,
+        ["Emoji"] = "⚔️",
+        ["Description"] = "Increases pet's damage by MULTIPLIER.",
+        ["Weight"] = 20,
+        ["Color"] = Color3.fromRGB(220, 64, 66),
+        ["Extra"] = {["Multiplier"] = {1.1, 1.15, 1.25, 1.5, 2}}
+    },
+    ["Looting"] = {
+        ["LayoutOrder"] = 8,
+        ["Emoji"] = "💰",
+        ["Description"] = "Increases pet's currency by MULTIPLIER.",
+        ["Weight"] = 20,
+        ["Color"] = Color3.fromRGB(232, 174, 82),
+        ["Extra"] = {["Multiplier"] = {1.1, 1.15, 1.25, 1.5, 2}}
+    },
+    ["Critical Hit"] = {
+        ["LayoutOrder"] = 1,
+        ["Emoji"] = "🎯",
+        ["Description"] = "Increases your Critical Multiplier by +MULTIPLIER.",
+        ["Weight"] = 1,
+        ["Color"] = Color3.fromRGB(51, 211, 255),
+        ["Extra"] = {["Multiplier"] = {1.1, 1.15, 1.25, 1.4, 1.75}}
+    },
+    ["High Roller"] = {
+        ["LayoutOrder"] = 4,
+        ["Emoji"] = "🎲",
+        ["Description"] = [[CHANCE <font color="#fff569">chance</font> to get INCREASE Boss Drops.]],
+        ["Weight"] = 1,
+        ["Color"] = Color3.fromRGB(226, 102, 145),
+        ["Extra"] = {["Chance"] = {5, 7, 14, 19, 25}, ["Increase"] = {1, 1, 1, 1, 1}}
+    },
+    ["Gamer"] = {
+        ["LayoutOrder"] = 3,
+        ["Emoji"] = "🕹️",
+        ["Description"] = "While this pet plays a Minigame you're MULTIPLIER more likely to get Legendary.",
+        ["Weight"] = 1,
+        ["Color"] = Color3.fromRGB(151, 125, 255),
+        ["Extra"] = {["Multiplier"] = {1.05, 1.1, 1.15, 1.2, 1.25}}
+    }
+}
