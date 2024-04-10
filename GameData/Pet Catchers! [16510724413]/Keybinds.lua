5c34fbe1a4d145cd798e2aa6b621992ad7e2ba7d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:31:26 +1000

    Create Keybinds.lua

---
+++ GameData/Pet Catchers! [16510724413]/Keybinds.lua
@@ -0,0 +1,27 @@
+data = {
+    ["Inventory"] = {["Button"] = "Button", ["Keybind"] = Enum.KeyCode.F},
+    ["EquipBest"] = {["Button"] = "Button", ["KeybindTip"] = "Keybind", ["Keybind"] = Enum.KeyCode.B},
+    ["QuickThrow4"] = {
+        ["KeybindTip"] = "Keybind",
+        ["Condition"] = "function: 0x000000005343068e",
+        ["Keybind"] = Enum.KeyCode.Four
+    },
+    ["QuickThrow2"] = {
+        ["KeybindTip"] = "Keybind",
+        ["Condition"] = "function: 0x000000005343068e",
+        ["Keybind"] = Enum.KeyCode.Two
+    },
+    ["Crafting"] = {["Button"] = "Button", ["Keybind"] = Enum.KeyCode.C},
+    ["QuickThrow1"] = {
+        ["KeybindTip"] = "Keybind",
+        ["Condition"] = "function: 0x000000005343068e",
+        ["Keybind"] = Enum.KeyCode.One
+    },
+    ["Index"] = {["Button"] = "Button", ["Keybind"] = Enum.KeyCode.Q},
+    ["QuickThrow3"] = {
+        ["KeybindTip"] = "Keybind",
+        ["Condition"] = "function: 0x000000005343068e",
+        ["Keybind"] = Enum.KeyCode.Three
+    },
+    ["AFK"] = {["Button"] = "Button", ["Keybind"] = Enum.KeyCode.T}
+}
