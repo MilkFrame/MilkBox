4682a94f8260e0465616cc36cd0d9baa4261253a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:34:58 +1000

    Create Events.lua

---
+++ GameData/Pet Catchers! [16510724413]/Events.lua
@@ -0,0 +1,58 @@
+data = {
+    ["Boss Rush"] = {
+        ["Emoji"] = "⚔️",
+        ["Description"] = "All bosses have INCREASE more drops!",
+        ["TextColor3"] = Color3.fromRGB(214, 64, 255),
+        ["Extra"] = {["Increase"] = 4},
+        ["HasRegion"] = false
+    },
+    ["Treasure"] = {
+        ["Emoji"] = "💰",
+        ["Description"] = "Earn MULTIPLIER more Coins!",
+        ["TextColor3"] = Color3.fromRGB(255, 239, 116),
+        ["Extra"] = {["Multiplier"] = 2},
+        ["HasRegion"] = false
+    },
+    ["Fortune"] = {
+        ["Emoji"] = "💎",
+        ["Description"] = "Earn MULTIPLIER more Gems!",
+        ["TextColor3"] = Color3.fromRGB(85, 190, 255),
+        ["Extra"] = {["Multiplier"] = 2},
+        ["HasRegion"] = false
+    },
+    ["Shiny Hunt"] = {
+        ["Emoji"] = "✨",
+        ["Description"] = "Pets in REGION are MULTIPLIER more likely to spawn as Shiny.",
+        ["TextColor3"] = Color3.fromRGB(255, 191, 0),
+        ["Extra"] = {["Multiplier"] = 4},
+        ["HasRegion"] = true
+    },
+    ["Gamer"] = {
+        ["Emoji"] = "🕹️",
+        ["Description"] = "MULTIPLIER more likely to get Legendary drops from Minigames!",
+        ["TextColor3"] = Color3.fromRGB(151, 125, 255),
+        ["Extra"] = {["Multiplier"] = 1.25},
+        ["HasRegion"] = false
+    },
+    ["Lucky"] = {
+        ["Emoji"] = "☘️",
+        ["Description"] = "Increased Legendary Pet chances on Cubes and Eggs by MULTIPLIER!",
+        ["TextColor3"] = Color3.fromRGB(109, 255, 101),
+        ["Extra"] = {["Multiplier"] = 2},
+        ["HasRegion"] = false
+    },
+    ["Master Chef"] = {
+        ["Emoji"] = "🍰",
+        ["Description"] = "Crafting speed increased by MULTIPLIER!",
+        ["TextColor3"] = Color3.fromRGB(255, 197, 139),
+        ["Extra"] = {["Multiplier"] = 4},
+        ["HasRegion"] = false
+    },
+    ["Quick Fishing"] = {
+        ["Emoji"] = "🎣",
+        ["Description"] = "Catch fish MULTIPLIER faster in REGION!",
+        ["TextColor3"] = Color3.fromRGB(140, 192, 255),
+        ["Extra"] = {["Multiplier"] = 2},
+        ["HasRegion"] = true
+    }
+}
