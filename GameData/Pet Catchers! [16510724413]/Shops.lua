ee826c73660d06f3744ebdd41a9acd0958f819a1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:33:05 +1000

    Create Shops.lua

---
+++ GameData/Pet Catchers! [16510724413]/Shops.lua
@@ -0,0 +1,308 @@
+data = {
+    ["the-blackmarket"] = {
+        ["RandomItems"] = {
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 100000000},
+                ["Stock"] = 15,
+                ["Product"] = {["Type"] = "CaptureCube", ["Name"] = "Legendary", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 150},
+                ["Stock"] = 15,
+                ["Product"] = {["Type"] = "CaptureCube", ["Name"] = "Legendary", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 190},
+                ["Stock"] = 4,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Knowledge Scroll", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 20000000},
+                ["Stock"] = 4,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Knowledge Scroll", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 1500},
+                ["Stock"] = 2,
+                ["Product"] = {["Type"] = "Powerup", ["Name"] = "Respawn Tome", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 900000000},
+                ["Stock"] = 2,
+                ["Product"] = {["Type"] = "Powerup", ["Name"] = "Respawn Tome", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 1500},
+                ["Stock"] = 2,
+                ["Product"] = {["Type"] = "Powerup", ["Name"] = "Timeful Tome", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 500000000},
+                ["Stock"] = 2,
+                ["Product"] = {["Type"] = "Powerup", ["Name"] = "Timeful Tome", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 1000000},
+                ["Stock"] = 15,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Voiding Scroll", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 50},
+                ["Stock"] = 15,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Voiding Scroll", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 1000000},
+                ["Stock"] = 15,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Mystery Scroll", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 90},
+                ["Stock"] = 15,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Mystery Scroll", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 2000000000},
+                ["Stock"] = 2,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Golden Cherry", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 3500},
+                ["Stock"] = 3,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Golden Cherry", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 20000000000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Powerup", ["Name"] = "Prismatic Tome", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 40000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Powerup", ["Name"] = "Prismatic Tome", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 25000000000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Prismatic Scroll", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 50000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Prismatic Scroll", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 9000000000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Powerup", ["Name"] = "Prismatic Sundae", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 12500},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Powerup", ["Name"] = "Prismatic Sundae", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 1450},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Seafarer Rune", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 2860000000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Seafarer Rune", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 5000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Fatal Strike Rune", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 10000000000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Fatal Strike Rune", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 350},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Lifesteal Rune", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 670000000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Lifesteal Rune", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 350},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Shrine Rune", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 670000000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Shrine Rune", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 250},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Deadly Rune", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 500000000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Deadly Rune", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 250},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Looting Rune", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 500000000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Looting Rune", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 5000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Critical Hit Rune", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 10000000000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Critical Hit Rune", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 5000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "High Roller Rune", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 10000000000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "High Roller Rune", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 5000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Gamer Rune", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 10000000000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Gamer Rune", ["Amount"] = 1}
+            }
+        },
+        ["RerollDuration"] = 7200,
+        ["Region"] = "Magma Basin",
+        ["PermanentItems"] = {},
+        ["NotificationName"] = "Blackmarket",
+        ["SlotCount"] = 3,
+        ["DisplayName"] = "Blackmarket"
+    },
+    ["auburn-shop"] = {
+        ["RandomItems"] = {},
+        ["RerollDuration"] = 1200,
+        ["Region"] = "Auburn Woods",
+        ["PermanentItems"] = {
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 550},
+                ["Stock"] = 25,
+                ["Product"] = {["Type"] = "CaptureCube", ["Name"] = "Rare", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 2000},
+                ["Stock"] = 5,
+                ["Product"] = {["Type"] = "CaptureCube", ["Name"] = "Epic", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 1500},
+                ["Stock"] = 20,
+                ["Product"] = {["Type"] = "Powerup", ["Name"] = "Mystery Egg", ["Amount"] = 1}
+            }
+        },
+        ["NotificationName"] = "Merchant",
+        ["SlotCount"] = 3,
+        ["DisplayName"] = "Merchant"
+    },
+    ["magic-shop"] = {
+        ["RandomItems"] = {},
+        ["RerollDuration"] = 3600,
+        ["Region"] = "Gloomy Grotto",
+        ["PermanentItems"] = {
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 1000000},
+                ["Stock"] = 15,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Radioactive Jelly", ["Amount"] = 5}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 2000000},
+                ["Stock"] = 2,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Voiding Scroll", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 3000000},
+                ["Stock"] = 2,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Mystery Scroll", ["Amount"] = 1}
+            }
+        },
+        ["NotificationName"] = "Magic Shop",
+        ["SlotCount"] = 3,
+        ["DisplayName"] = "Magic Shop"
+    },
+    ["gem-trader"] = {
+        ["RandomItems"] = {
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 150},
+                ["Stock"] = 2,
+                ["Product"] = {["Type"] = "Powerup", ["Name"] = "Lucky Elixir", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 350},
+                ["Stock"] = 2,
+                ["Product"] = {["Type"] = "Powerup", ["Name"] = "Sea Elixir", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 220},
+                ["Stock"] = 4,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Knowledge Scroll", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 1000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Powerup", ["Name"] = "Respawn Tome", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 1000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Powerup", ["Name"] = "Timeful Tome", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 2500},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Treat", ["Name"] = "Golden Cherry", ["Amount"] = 1}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 950},
+                ["Stock"] = 2,
+                ["Product"] = {["Amount"] = 1, ["Type"] = "GoldenTicket"}
+            },
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 10000},
+                ["Stock"] = 1,
+                ["Product"] = {["Type"] = "Powerup", ["Name"] = "Prismatic Sundae", ["Amount"] = 1}
+            }
+        },
+        ["RerollDuration"] = 3600,
+        ["Region"] = "Sunset Shores",
+        ["PermanentItems"] = {
+            {
+                ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 250},
+                ["Stock"] = 10,
+                ["Product"] = {["Type"] = "CaptureCube", ["Name"] = "Legendary", ["Amount"] = 1}
+            }
+        },
+        ["NotificationName"] = "Gem Trader",
+        ["SlotCount"] = 3,
+        ["DisplayName"] = "Gem Trader"
+    }
+}
