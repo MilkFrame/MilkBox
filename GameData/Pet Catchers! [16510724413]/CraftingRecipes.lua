7df7e31ba6d710486bf46eb36048a5a63c7ff8fc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:37:17 +1000

    Create CraftingRecipes.lua

---
+++ GameData/Pet Catchers! [16510724413]/CraftingRecipes.lua
@@ -0,0 +1,71 @@
+data = {
+    ["sea-elixir"] = {
+        ["LayoutOrder"] = 7,
+        ["Region"] = "Dusty Dunes",
+        ["Duration"] = 60,
+        ["Ingredients"] = {
+            {["Type"] = "Powerup", ["Name"] = "Coin Elixir", ["Amount"] = 5},
+            {["Type"] = "Powerup", ["Name"] = "XP Elixir", ["Amount"] = 5},
+            {["Type"] = "Powerup", ["Name"] = "Lucky Elixir", ["Amount"] = 1},
+            {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 100}
+        },
+        ["Product"] = {["Type"] = "Powerup", ["Name"] = "Sea Elixir", ["Amount"] = 1}
+    },
+    ["legendary-cube"] = {
+        ["LayoutOrder"] = 2,
+        ["Region"] = "Sunset Shores",
+        ["Duration"] = 60,
+        ["Ingredients"] = {
+            {["Type"] = "CaptureCube", ["Name"] = "Epic", ["Amount"] = 30},
+            {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 500000}
+        },
+        ["Product"] = {["Type"] = "CaptureCube", ["Name"] = "Legendary", ["Amount"] = 1}
+    },
+    ["elite-mystery-egg"] = {
+        ["LayoutOrder"] = 4,
+        ["Region"] = "Sunset Shores",
+        ["Duration"] = 3,
+        ["Ingredients"] = {
+            {["Type"] = "Powerup", ["Name"] = "Mystery Egg", ["Amount"] = 5},
+            {["Type"] = "CaptureCube", ["Name"] = "Rare", ["Amount"] = 5},
+            {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 50000}
+        },
+        ["Product"] = {["Type"] = "Powerup", ["Name"] = "Elite Mystery Egg", ["Amount"] = 1}
+    },
+    ["xp-elixir"] = {
+        ["LayoutOrder"] = 6,
+        ["Region"] = "Frosty Peaks",
+        ["Duration"] = 60,
+        ["Ingredients"] = {{["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 20000}},
+        ["Product"] = {["Type"] = "Powerup", ["Name"] = "XP Elixir", ["Amount"] = 1}
+    },
+    ["epic-cube"] = {
+        ["LayoutOrder"] = 1,
+        ["Duration"] = 10,
+        ["Ingredients"] = {
+            {["Type"] = "CaptureCube", ["Name"] = "Rare", ["Amount"] = 10},
+            {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 8000}
+        },
+        ["Product"] = {["Type"] = "CaptureCube", ["Name"] = "Epic", ["Amount"] = 1}
+    },
+    ["coin-elixir"] = {
+        ["LayoutOrder"] = 5,
+        ["Region"] = "Frosty Peaks",
+        ["Duration"] = 60,
+        ["Ingredients"] = {{["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 20000}},
+        ["Product"] = {["Type"] = "Powerup", ["Name"] = "Coin Elixir", ["Amount"] = 1}
+    },
+    ["mystery-egg"] = {
+        ["LayoutOrder"] = 3,
+        ["Region"] = "Frosty Peaks",
+        ["Duration"] = 1,
+        ["Ingredients"] = {{["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 4000}},
+        ["Product"] = {["Type"] = "Powerup", ["Name"] = "Mystery Egg", ["Amount"] = 1}
+    },
+    ["rare-cube"] = {
+        ["LayoutOrder"] = 0,
+        ["Duration"] = 1,
+        ["Ingredients"] = {{["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 950}},
+        ["Product"] = {["Type"] = "CaptureCube", ["Name"] = "Rare", ["Amount"] = 1}
+    }
+}
