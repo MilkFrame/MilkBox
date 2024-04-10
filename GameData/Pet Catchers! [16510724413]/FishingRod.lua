563a5a940473ee376352fa20cc61b7be048ba453
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:37:35 +1000

    Create FishingRod.lua

---
+++ GameData/Pet Catchers! [16510724413]/FishingRod.lua
@@ -0,0 +1,42 @@
+data = {
+    {
+        ["Name"] = "Basic Rod",
+        ["Extra"] = {["LootChance"] = 10},
+        ["Color"] = Color3.fromRGB(178, 222, 255),
+        ["Description"] = "A starter rod for catching fish Common, Rare, and Epic.",
+        ["Quest"] = "sailor-arrival",
+        ["Rarity"] = "Common"
+    },
+    {
+        ["Name"] = "Copper Rod",
+        ["Extra"] = {["LootChance"] = 20},
+        ["Color"] = Color3.fromRGB(255, 186, 146),
+        ["Description"] = "Enhanced with a sharp hook, ideal for treasure seeking.",
+        ["Quest"] = "sailor-copper",
+        ["Rarity"] = "Rare"
+    },
+    {
+        ["Name"] = "Silver Rod",
+        ["Extra"] = {["Speed"] = 1.1, ["Legendary"] = true, ["LootChance"] = 45},
+        ["Color"] = Color3.fromRGB(143, 165, 180),
+        ["Description"] = "A line strong enough to catch a Legendary Fish.",
+        ["Quest"] = "sailor-silver",
+        ["Rarity"] = "Epic"
+    },
+    {
+        ["Name"] = "Gold Rod",
+        ["Extra"] = {["Speed"] = 1.25, ["Legendary"] = true, ["Luck"] = 1.1, ["LootChance"] = 65},
+        ["Color"] = Color3.fromRGB(255, 223, 96),
+        ["Description"] = "A golden line which favors fortune for rare finds.",
+        ["Quest"] = "sailor-gold",
+        ["Rarity"] = "Epic"
+    },
+    {
+        ["Name"] = "Diamond Rod",
+        ["Extra"] = {["Speed"] = 1.5, ["Legendary"] = true, ["Luck"] = 1.25, ["LootChance"] = 85},
+        ["Color"] = Color3.fromRGB(5, 213, 255),
+        ["Description"] = "A rod gifted to a Master Angler, offering excellent speed and luck chances.",
+        ["Quest"] = "sailor-diamond",
+        ["Rarity"] = "Legendary"
+    }
+}
