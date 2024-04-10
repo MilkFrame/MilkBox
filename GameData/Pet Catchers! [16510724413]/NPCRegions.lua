bdb39291e2189a3f7cee84d7ef246b71b88dff0c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:40:01 +1000

    Create NPCRegions.lua

---
+++ GameData/Pet Catchers! [16510724413]/NPCRegions.lua
@@ -0,0 +1,14 @@
+data = {
+    ["The Blacksmith"] = "Auburn Woods",
+    ["Ancient Moai"] = "Magma Basin",
+    ["Catching Guide"] = "Pet Park",
+    ["Archeologist"] = "Dusty Dunes",
+    ["Sir Locksley"] = "Magma Basin",
+    ["Magma Fiend"] = "Frosty Peaks",
+    ["Old Sailor"] = "Sunset Shores",
+    ["Dance Champion"] = "Gloomy Grotto",
+    ["Phony Hawk"] = "Mellow Meadows",
+    ["Bruh"] = "Frosty Peaks",
+    ["Mayor Bling"] = "Frosty Peaks",
+    ["Ski Patrol"] = "Frosty Peaks"
+}
