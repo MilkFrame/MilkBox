d19b11157551118f20129b496abe9ef083adbd14
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:39:00 +1000

    Create SirLocksley.lua

---
+++ GameData/Pet Catchers! [16510724413]/SirLocksley.lua
@@ -0,0 +1,60 @@
+data = {
+    {
+        [[<font color="#9cf7ff">Good gracious</font>! Is it customary where you're from to wake nobility from their rest? A lock must maintain its shine after all!]],
+        [[Where are my manners, let me introduce myself. I am <font color="#fff569">Sir Locksley</font>, the illustrious descendant of <font color="#9cf7ff">Sir Locksington the IV</font>!]],
+        [[Quite a notable lineage for one in <font color="#fff569">my predicament</font>, don't you think?]],
+        "Ah, forgive me, where was I? Sometimes I get lost in the sound of my own charming voice!",
+        [[<font color="#9cf7ff">A wizard</font>, lacking in both humor and reason, decided to <font color="#fff569">lock me here for eternity</font>. A great overreaction if you ask me.]],
+        [[He was mumbling a <font color="#cd8fff">riddle</font> to himself. You'd think someone of his 'vast' wisdom would recall that locks have the best ears for eavesdropping!]],
+        "It seems memory isn't among his magical talents!",
+        [[The <font color="#cd8fff">wizard's riddle</font> went something like this:]],
+        [[Locked forever, a curse to be,
+A hidden key shall set you free.
+<font color="#9cf7ff">Clues hidden</font>, all across the land.
+The first awaits you under a <font color="#fff569">cross in sand</font>.]],
+        "Quite the riddle it was! If you manage to unravel its mysteries, do swing by and enlighten me. Not like I'm going anywhere ha!"
+    },
+    {
+        [[In an autumn forest where the <font color="#fff569">furnace burns bright</font>,
+<font color="#9cf7ff">On a pile logs</font>, just out of plain sight.]]
+    },
+    {
+        [[At journey's start, <font color="#fff569">where champions</font> stand tall,
+Find me on the <font color="#9cf7ff">fountains waterfall</font>.]]
+    },
+    {
+        [[Where the <font color="#fff569">air is crisp</font> and the land is serene,
+A clue lay on top of the <font color="#97ffa3">roof colored green</font>.]]
+    },
+    {
+        [[On the <font color="#fff569">desert island</font>, where <font color="#9cf7ff">a pyramid resides</font>,
+Walk through it to find where the clue hides.]]
+    },
+    {
+        [[In the land of <font color="#cd8fff">purple water</font> and tress of fungi,
+At the <font color="#9cf7ff">end of the cave</font>, where crystal meets sky.]]
+    },
+    {
+        [[In the meadows by the <font color="#9cf7ff">Capybara Crew</font>, in the <font color="#fff569">Skate Park</font> you'll find your next clue.]]
+    },
+    {
+        [[Where <font color="#fff569">ground is hot</font>, the <font color="#9cf7ff">castle</font> stands tall,
+Climb the spire and find the key once and for all.]]
+    },
+    {[[You found the key! Return to <font color="#fff569">Sir Locksley</font> to break the curse.]]},
+    {
+        [[<font color="#ff616c">MWAHAHAHAHAH! YOU FOOL!</font> You actually believed me???]],
+        [[Ah, <font color="#fff569">I'm pulling your leg</font>! After all these years stuck here, I had to seize the opportunity. Thanks to your help <font color="#9cf7ff">I'm free</font>!]],
+        "...Yet, upon reflection, do I truly wish to depart? Over the years, this spot and I have become rather inseparable.",
+        "Maybe I'll stay just a smidge longer. After all, where else would I find such a cozy door?",
+        [[<font color="#fff569">Thanks for the help traveler</font>! I hope you find what you seek beyond this door.]]
+    },
+    ["BeginningRiddle"] = {
+        [[The <font color="#cd8fff">wizard's riddle</font> went something like this:]],
+        [[Locked forever, a curse to be,
+A hidden key shall set you free.
+<font color="#9cf7ff">Clues hidden</font>, all across the land.
+The first awaits you under a <font color="#fff569">cross in sand</font>.]],
+        "Quite the riddle it was! If you manage to unravel its mysteries, do swing by and enlighten me. Not like I'm going anywhere ha!"
+    }
+}
