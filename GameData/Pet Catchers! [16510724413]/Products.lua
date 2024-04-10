9668983bee863bf28941b08cc3169ad57b04d184
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:41:59 +1000

    Create Products.lua

---
+++ GameData/Pet Catchers! [16510724413]/Products.lua
@@ -0,0 +1,180 @@
+data = {
+    ["ticket-2"] = {
+        ["Id"] = 1764653238,
+        ["Type"] = "Product",
+        ["Item"] = {["Amount"] = 4, ["Type"] = "GoldenTicket"},
+        ["Cost"] = 299
+    },
+    ["guaranteed-legendary"] = {
+        ["Id"] = 1764653240,
+        ["Type"] = "Product",
+        ["Image"] = "rbxassetid://16347647978",
+        ["Cost"] = 179
+    },
+    ["guaranteed-secret"] = {
+        ["Id"] = 1764653241,
+        ["Type"] = "Product",
+        ["Image"] = "rbxassetid://16347699989",
+        ["Cost"] = 1499
+    },
+    ["gems-1"] = {
+        ["Id"] = 1764653234,
+        ["Type"] = "Product",
+        ["Item"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 250},
+        ["Cost"] = 25
+    },
+    ["gems-3"] = {
+        ["Id"] = 1764653232,
+        ["Type"] = "Product",
+        ["Item"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 1000},
+        ["Cost"] = 100
+    },
+    ["gems-5"] = {
+        ["Id"] = 1764653230,
+        ["Type"] = "Product",
+        ["Item"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 5000},
+        ["Cost"] = 425
+    },
+    ["featured-3"] = {
+        ["Id"] = 1764653253,
+        ["Type"] = "Featured",
+        ["Pet"] = {["Name"] = "The Overlord", ["Type"] = "Pet"},
+        ["Cost"] = 2999
+    },
+    ["ticket-3"] = {
+        ["Id"] = 1764653239,
+        ["Type"] = "Product",
+        ["Item"] = {["Amount"] = 7, ["Type"] = "GoldenTicket"},
+        ["Cost"] = 399
+    },
+    ["ultra-luck"] = {
+        ["Type"] = "Boost",
+        ["Cost"] = 199,
+        ["Id"] = 1764653236,
+        ["Length"] = 600,
+        ["Buffs"] = {"Ultra Lucky"}
+    },
+    ["featured-2"] = {
+        ["Id"] = 1764653252,
+        ["Type"] = "Featured",
+        ["Pet"] = {["Name"] = "Dowodle", ["Type"] = "Pet"},
+        ["Cost"] = 729
+    },
+    ["prismatic-bundle"] = {
+        ["EndsAt"] = "1710374400000",
+        ["Type"] = "Bundle",
+        ["Stock"] = 3,
+        ["Cost"] = 3499,
+        ["Id"] = 1764653250,
+        ["LayoutOrder"] = 0,
+        ["Items"] = {
+            {["Type"] = "Powerup", ["Name"] = "Prismatic Mystery Egg", ["Amount"] = 3},
+            {["Type"] = "Powerup", ["Name"] = "Prismatic Sundae", ["Amount"] = 4},
+            {["Type"] = "Powerup", ["Name"] = "Prismatic Tome", ["Amount"] = 3},
+            {["Type"] = "Treat", ["Name"] = "Prismatic Scroll", ["Amount"] = 2},
+            {["Type"] = "Powerup", ["Name"] = "Prismatic Elixir", ["Amount"] = 2}
+        }
+    },
+    ["golden-bundle"] = {
+        ["EndsAt"] = "1710374400000",
+        ["Type"] = "Bundle",
+        ["Stock"] = 5,
+        ["Cost"] = 1199,
+        ["Id"] = 1764653249,
+        ["LayoutOrder"] = 1,
+        ["Items"] = {
+            {["Type"] = "CaptureCube", ["Name"] = "Perfect", ["Amount"] = 1},
+            {["Type"] = "Treat", ["Name"] = "Golden Cherry", ["Amount"] = 20},
+            {["Type"] = "Treat", ["Name"] = "Looting Rune", ["Amount"] = 4},
+            {["Amount"] = 3, ["Type"] = "GoldenTicket"},
+            {["Type"] = "Powerup", ["Name"] = "Coin Elixir", ["Amount"] = 20}
+        }
+    },
+    ["vip-bundle"] = {
+        ["Items"] = {
+            {["Type"] = "Pet", ["Name"] = "Capybaruh", ["Amount"] = 1},
+            {["Color"] = "Default", ["Type"] = "Mount", ["Name"] = "Golden Jetpack", ["Amount"] = 1}
+        },
+        ["Type"] = "Bundle",
+        ["Stock"] = 1,
+        ["Cost"] = 349,
+        ["Id"] = 722785092,
+        ["LayoutOrder"] = 0,
+        ["HideFromShop"] = true
+    },
+    ["gems-2"] = {
+        ["Id"] = 1764653233,
+        ["Type"] = "Product",
+        ["Item"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 500},
+        ["Cost"] = 50
+    },
+    ["skip-crafting"] = {["Id"] = 1764653242, ["Type"] = "Product", ["Cost"] = 74},
+    ["catchers-bundle"] = {
+        ["EndsAt"] = "1711756800000",
+        ["Type"] = "Bundle",
+        ["Stock"] = 10,
+        ["Cost"] = 299,
+        ["Id"] = 1764653248,
+        ["LayoutOrder"] = 3,
+        ["Items"] = {
+            {["Type"] = "Powerup", ["Name"] = "Elite Mystery Egg", ["Amount"] = 30},
+            {["Type"] = "CaptureCube", ["Name"] = "Legendary", ["Amount"] = 15},
+            {["Type"] = "CaptureCube", ["Name"] = "Epic", ["Amount"] = 15},
+            {["Type"] = "Powerup", ["Name"] = "Lucky Elixir", ["Amount"] = 15}
+        }
+    },
+    ["fishing-bundle"] = {
+        ["Items"] = {
+            {["Type"] = "Powerup", ["Name"] = "Sea Elixir", ["Amount"] = 5},
+            {["Type"] = "Treat", ["Name"] = "Seafarer Rune", ["Amount"] = 5}
+        },
+        ["Type"] = "Bundle",
+        ["Stock"] = 1,
+        ["Cost"] = 599,
+        ["Id"] = 1764653247,
+        ["LayoutOrder"] = 0,
+        ["HideFromShop"] = true
+    },
+    ["magic-bundle"] = {
+        ["EndsAt"] = "1711756800000",
+        ["Type"] = "Bundle",
+        ["Stock"] = 10,
+        ["Cost"] = 499,
+        ["Id"] = 1764653255,
+        ["LayoutOrder"] = 2,
+        ["Items"] = {
+            {["Type"] = "Powerup", ["Name"] = "Respawn Tome", ["Amount"] = 2},
+            {["Type"] = "Powerup", ["Name"] = "Timeful Tome", ["Amount"] = 2},
+            {["Type"] = "Treat", ["Name"] = "Knowledge Scroll", ["Amount"] = 20},
+            {["Type"] = "Treat", ["Name"] = "Mystery Scroll", ["Amount"] = 20},
+            {["Type"] = "Treat", ["Name"] = "Voiding Scroll", ["Amount"] = 20},
+            {["Type"] = "Treat", ["Name"] = "Radioactive Jelly", ["Amount"] = 500}
+        }
+    },
+    ["restock-shop"] = {["Id"] = 1764653245, ["Type"] = "Product", ["Cost"] = 74},
+    ["featured-1"] = {
+        ["Id"] = 1764653251,
+        ["Type"] = "Featured",
+        ["Pet"] = {["Name"] = "Toilet Doggy", ["Type"] = "Pet"},
+        ["Cost"] = 299
+    },
+    ["ticket-1"] = {
+        ["Id"] = 1764653237,
+        ["Type"] = "Product",
+        ["Item"] = {["Amount"] = 2, ["Type"] = "GoldenTicket"},
+        ["Cost"] = 149
+    },
+    ["gems-4"] = {
+        ["Id"] = 1764653231,
+        ["Type"] = "Product",
+        ["Item"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 2000},
+        ["Cost"] = 200
+    },
+    ["gems-6"] = {
+        ["Id"] = 1764653228,
+        ["Type"] = "Product",
+        ["Item"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 10000},
+        ["Cost"] = 750
+    },
+    ["respawn-boss"] = {["Id"] = 1764653246, ["Type"] = "Product", ["Cost"] = 129}
+}
