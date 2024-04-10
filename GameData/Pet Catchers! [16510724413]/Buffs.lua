a04e349b7361eba01233bdba7f38886a0cf1be8f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:36:39 +1000

    Create Buffs.lua

---
+++ GameData/Pet Catchers! [16510724413]/Buffs.lua
@@ -0,0 +1,92 @@
+data = {
+    ["Experienced"] = {
+        ["LayoutOrder"] = 2,
+        ["Emoji"] = "🌟",
+        ["Description"] = "Increases your XP multiplier by MULTIPLIER.",
+        ["Color"] = Color3.fromRGB(24, 147, 255),
+        ["Extra"] = {["Multiplier"] = 1.5},
+        ["Stat"] = "XP",
+        ["Image"] = "rbxassetid://16168689114"
+    },
+    ["Fortune"] = {
+        ["LayoutOrder"] = 3,
+        ["Emoji"] = "💎",
+        ["Description"] = "Increases your Gem Multiplier by MULTIPLIER.",
+        ["Color"] = Color3.fromRGB(49, 179, 226),
+        ["Extra"] = {["Multiplier"] = 2},
+        ["Stat"] = "Gems",
+        ["Image"] = "rbxassetid://16144607993"
+    },
+    ["Feeling Lucky"] = {
+        ["LayoutOrder"] = 4,
+        ["Emoji"] = "☘️",
+        ["Description"] = "Increases Legendary Pet chance from Cubes and Eggs by MULTIPLIER.",
+        ["Color"] = Color3.fromRGB(89, 226, 65),
+        ["Extra"] = {["Multiplier"] = 1.35},
+        ["Stat"] = "Luck",
+        ["Image"] = "rbxassetid://16112168470"
+    },
+    ["Super Roll"] = {
+        ["LayoutOrder"] = 10,
+        ["Emoji"] = "🎲🎲",
+        ["Description"] = "Increases Boss and Enemy drops by INCREASE.",
+        ["Color"] = Color3.fromRGB(226, 202, 18),
+        ["Extra"] = {["Increase"] = 4},
+        ["Stat"] = "Drops",
+        ["Image"] = "rbxassetid://16112226479"
+    },
+    ["Treasure"] = {
+        ["LayoutOrder"] = 1,
+        ["Emoji"] = "💰",
+        ["Description"] = "Increases your Coin Multiplier by MULTIPLIER.",
+        ["Color"] = Color3.fromRGB(226, 185, 21),
+        ["Extra"] = {["Multiplier"] = 1.25},
+        ["Stat"] = "Coins",
+        ["Image"] = "rbxassetid://12352867717"
+    },
+    ["Lucky Roll"] = {
+        ["LayoutOrder"] = 9,
+        ["Emoji"] = "🎲",
+        ["Description"] = "Increases Boss and Enemy drops by INCREASE.",
+        ["Color"] = Color3.fromRGB(226, 69, 58),
+        ["Extra"] = {["Increase"] = 1},
+        ["Stat"] = "Drops",
+        ["Image"] = "rbxassetid://16112223596"
+    },
+    ["Stopwatch"] = {
+        ["LayoutOrder"] = 8,
+        ["Emoji"] = "🕒",
+        ["Description"] = "Speeds up the duration of timers by MULTIPLIER.",
+        ["Color"] = Color3.fromRGB(205, 214, 226),
+        ["Extra"] = {["Multiplier"] = 3},
+        ["Stat"] = "Timer",
+        ["Image"] = "rbxassetid://16347466404"
+    },
+    ["Ocean's Blessing"] = {
+        ["LayoutOrder"] = 7,
+        ["Emoji"] = "🌊",
+        ["Description"] = "Increases chance of Legendary Bonus Loot and Fish by MULTIPLIER.",
+        ["Color"] = Color3.fromRGB(110, 162, 226),
+        ["Extra"] = {["Multiplier"] = 1.2},
+        ["Stat"] = "FishBonus",
+        ["Image"] = "rbxassetid://15883666209"
+    },
+    ["Super Lucky"] = {
+        ["LayoutOrder"] = 5,
+        ["Emoji"] = "🌟",
+        ["Description"] = "Increases Legendary Pet chance from Cubes and Eggs by MULTIPLIER.",
+        ["Color"] = Color3.fromRGB(55, 226, 29),
+        ["Extra"] = {["Multiplier"] = 2.25},
+        ["Stat"] = "Luck",
+        ["Image"] = "rbxassetid://16112219861"
+    },
+    ["Ultra Lucky"] = {
+        ["LayoutOrder"] = 6,
+        ["Emoji"] = "🌈",
+        ["Description"] = "Grants every buff in the game as well as an additional MULTIPLIER Luck!",
+        ["Color"] = Color3.fromRGB(255, 0, 0),
+        ["Extra"] = {["Multiplier"] = 2.5},
+        ["Stat"] = "Luck",
+        ["Image"] = "rbxassetid://16388382676"
+    }
+}
