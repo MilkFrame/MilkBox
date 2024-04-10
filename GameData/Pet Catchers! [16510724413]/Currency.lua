28e3579d3b9e9b6f93e824afc768afbeeced7133
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:35:29 +1000

    Create Currency.lua

---
+++ GameData/Pet Catchers! [16510724413]/Currency.lua
@@ -0,0 +1,14 @@
+data = {
+    ["Gems"] = {
+        ["Color"] = Color3.fromRGB(235, 82, 255),
+        ["Image"] = "rbxassetid://16144607993",
+        ["Prefix"] = "",
+        ["LayoutOrder"] = 2
+    },
+    ["Coins"] = {
+        ["Color"] = Color3.fromRGB(255, 222, 56),
+        ["Image"] = "rbxassetid://12352867717",
+        ["Prefix"] = "$",
+        ["LayoutOrder"] = 1
+    }
+}
