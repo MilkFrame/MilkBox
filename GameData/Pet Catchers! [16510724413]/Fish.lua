28b760427f255bd4193d1626fc6f4a58cd8631bb
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:37:51 +1000

    Create Fish.lua

---
+++ GameData/Pet Catchers! [16510724413]/Fish.lua
@@ -0,0 +1,170 @@
+data = {
+    ["Leaf Fish"] = {
+        ["Value"] = {["Amount"] = 50, ["Currency"] = "Coins"},
+        ["Description"] = "An expert in camouflage, this fish uses its leaf-like fins to blend in among the leaves.",
+        ["Rarity"] = "Rare",
+        ["Footer"] = "Auburn Woods"
+    },
+    ["Sturgeon"] = {
+        ["Value"] = {["Amount"] = 50, ["Currency"] = "Coins"},
+        ["Description"] = "A majestic and old fish that commands respect with its presence.",
+        ["Rarity"] = "Rare",
+        ["Footer"] = "Frosty Peaks"
+    },
+    ["Bass"] = {
+        ["Value"] = {["Amount"] = 50, ["Currency"] = "Coins"},
+        ["Description"] = "A larger fish known for its strength.",
+        ["Rarity"] = "Rare",
+        ["Footer"] = "Pet Park and Mellow Meadows"
+    },
+    ["Eel"] = {
+        ["Value"] = {["Amount"] = 8, ["Currency"] = "Gems"},
+        ["Description"] = "A slippery creature with a shocking personality.",
+        ["Rarity"] = "Epic",
+        ["Footer"] = "Auburn Woods"
+    },
+    ["Rock Fish"] = {
+        ["Value"] = {["Amount"] = 70, ["Currency"] = "Coins"},
+        ["Description"] = "Often overlooked, this fish blends into the rocky seafloor.",
+        ["Rarity"] = "Common",
+        ["Footer"] = "Gloomy Grotto"
+    },
+    ["Blobfish"] = {
+        ["Value"] = {["Amount"] = 700, ["Currency"] = "Gems"},
+        ["Description"] = "A gelatinous blob with a permanent frown.",
+        ["Rarity"] = "Legendary",
+        ["Footer"] = "Magma Basin"
+    },
+    ["Lava Eel"] = {
+        ["Value"] = {["Amount"] = 20, ["Currency"] = "Gems"},
+        ["Description"] = "Thriving in scorching temperatures, this eel is as fiery as the environment it lives in.",
+        ["Rarity"] = "Epic",
+        ["Footer"] = "Magma Basin"
+    },
+    ["Void Fish"] = {
+        ["Value"] = {["Amount"] = 100, ["Currency"] = "Coins"},
+        ["Description"] = "An abyssal fish made of void, it's as dark as the deep sea trenches it inhabits.",
+        ["Rarity"] = "Rare",
+        ["Footer"] = "Magma Basin"
+    },
+    ["Tuna"] = {
+        ["Value"] = {["Amount"] = 10, ["Currency"] = "Gems"},
+        ["Description"] = "This fish uses litter as a clever disguise.",
+        ["Rarity"] = "Epic",
+        ["Footer"] = "Sunset Shores"
+    },
+    ["Axolotl"] = {
+        ["Value"] = {["Amount"] = 40, ["Currency"] = "Gems"},
+        ["Description"] = "Everyone's favorite underwater salamander.",
+        ["Rarity"] = "Legendary",
+        ["Footer"] = "Auburn Woods"
+    },
+    ["Angler Fish"] = {
+        ["Value"] = {["Amount"] = 400, ["Currency"] = "Gems"},
+        ["Description"] = "With a lure as bright as its teeth are sharp, this fish is almost alien.",
+        ["Rarity"] = "Legendary",
+        ["Footer"] = "Gloomy Grotto"
+    },
+    ["Pyramid Fish"] = {
+        ["Value"] = {["Amount"] = 240, ["Currency"] = "Gems"},
+        ["Description"] = "Often found staring at its own reflection, wondering if it's a fish or a misplaced artifact.",
+        ["Rarity"] = "Legendary",
+        ["Footer"] = "Dusty Dunes"
+    },
+    ["Cat Fish"] = {
+        ["Value"] = {["Amount"] = 30, ["Currency"] = "Gems"},
+        ["Description"] = "Part cat, part fish. Known to occasionally purr when caught.",
+        ["Rarity"] = "Legendary",
+        ["Footer"] = "Pet Park and Mellow Meadows"
+    },
+    ["Ancient Fish"] = {
+        ["Value"] = {["Amount"] = 75, ["Currency"] = "Coins"},
+        ["Description"] = "Reminiscent of a time long past, this skeletal fish is a living fossil.",
+        ["Rarity"] = "Rare",
+        ["Footer"] = "Dusty Dunes"
+    },
+    ["Piranha"] = {
+        ["Value"] = {["Amount"] = 16, ["Currency"] = "Gems"},
+        ["Description"] = "A fierce fish known for its sharp teeth and powerful jaws.",
+        ["Rarity"] = "Epic",
+        ["Footer"] = "Gloomy Grotto"
+    },
+    ["Lionfish"] = {
+        ["Value"] = {["Amount"] = 65, ["Currency"] = "Coins"},
+        ["Description"] = "Strikingly patterned with venomous spines.",
+        ["Rarity"] = "Rare",
+        ["Footer"] = "Sunset Shores"
+    },
+    ["Lucky Fish"] = {
+        ["Value"] = {["Amount"] = 4, ["Currency"] = "Gems"},
+        ["Description"] = "Highly sought-after by alchemists for brewing lucky potions.",
+        ["Rarity"] = "Epic",
+        ["Footer"] = "Pet Park and Mellow Meadows"
+    },
+    ["Sword Fish"] = {
+        ["Value"] = {["Amount"] = 10, ["Currency"] = "Gems"},
+        ["Description"] = "Yes, literally a sword as a fish.",
+        ["Rarity"] = "Epic",
+        ["Footer"] = "Frosty Peaks"
+    },
+    ["Sand Ray"] = {
+        ["Value"] = {["Amount"] = 14, ["Currency"] = "Gems"},
+        ["Description"] = "Blending seamlessly with the sandy ocean floor, this fish is a master of disguise.",
+        ["Rarity"] = "Epic",
+        ["Footer"] = "Dusty Dunes"
+    },
+    ["Trout"] = {
+        ["Value"] = {["Amount"] = 50, ["Currency"] = "Coins"},
+        ["Description"] = "A common fish that enjoys ponds and rivers.",
+        ["Rarity"] = "Common",
+        ["Footer"] = "Pet Park and Mellow Meadows"
+    },
+    ["Cactus Fish"] = {
+        ["Value"] = {["Amount"] = 60, ["Currency"] = "Coins"},
+        ["Description"] = "A spiny fish that thrives in hot waters, often mistaken for floating desert debris.",
+        ["Rarity"] = "Common",
+        ["Footer"] = "Dusty Dunes"
+    },
+    ["Pufferfish"] = {
+        ["Value"] = {["Amount"] = 110, ["Currency"] = "Gems"},
+        ["Description"] = "Aeugh.",
+        ["Rarity"] = "Legendary",
+        ["Footer"] = "Sunset Shores"
+    },
+    ["Fire Trout"] = {
+        ["Value"] = {["Amount"] = 90, ["Currency"] = "Coins"},
+        ["Description"] = "With a body shaped like fire, this fish is a living flame.",
+        ["Rarity"] = "Common",
+        ["Footer"] = "Magma Basin"
+    },
+    ["Bruhfish"] = {
+        ["Value"] = {["Amount"] = 80, ["Currency"] = "Gems"},
+        ["Description"] = "Said to be the first ancestor of the Bruh.",
+        ["Rarity"] = "Legendary",
+        ["Footer"] = "Frosty Peaks"
+    },
+    ["Discus"] = {
+        ["Value"] = {["Amount"] = 50, ["Currency"] = "Coins"},
+        ["Description"] = "A beautifully shaped fish with a wide, circular body and vibrant colors.",
+        ["Rarity"] = "Common",
+        ["Footer"] = "Sunset Shores"
+    },
+    ["Mutant Fish"] = {
+        ["Value"] = {["Amount"] = 85, ["Currency"] = "Coins"},
+        ["Description"] = "Radiating an eerie glow, this multi-eyed fish is the result of poluted waters.",
+        ["Rarity"] = "Rare",
+        ["Footer"] = "Gloomy Grotto"
+    },
+    ["Koi"] = {
+        ["Value"] = {["Amount"] = 50, ["Currency"] = "Coins"},
+        ["Description"] = "Often found gracefully gliding in ponds, they're like living artwork.",
+        ["Rarity"] = "Common",
+        ["Footer"] = "Auburn Woods"
+    },
+    ["Ice Fish"] = {
+        ["Value"] = {["Amount"] = 50, ["Currency"] = "Coins"},
+        ["Description"] = "Don't be fooled, this fish uses ice as a defense mechanism.",
+        ["Rarity"] = "Common",
+        ["Footer"] = "Frosty Peaks"
+    }
+}
