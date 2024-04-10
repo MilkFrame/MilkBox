ad8ade2427ab41bf850ffccd8de84fcc29dd8ed0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:40:47 +1000

    Create IndexRewards.lua

---
+++ GameData/Pet Catchers! [16510724413]/IndexRewards.lua
@@ -0,0 +1,142 @@
+data = {
+    {["Rewards"] = {{["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 500}}, ["Amount"] = 5},
+    {
+        ["Rewards"] = {
+            {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 2500},
+            {["Type"] = "Powerup", ["Name"] = "Coin Elixir", ["Amount"] = 1},
+            {["Type"] = "Powerup", ["Name"] = "XP Elixir", ["Amount"] = 1}
+        },
+        ["Amount"] = 10
+    },
+    {
+        ["Rewards"] = {
+            {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 250},
+            {["Type"] = "Powerup", ["Name"] = "Elite Mystery Egg", ["Amount"] = 50},
+            {["Type"] = "Powerup", ["Name"] = "Respawn Tome", ["Amount"] = 1},
+            {["Amount"] = 1, ["Type"] = "GoldenTicket"}
+        },
+        ["Amount"] = 20
+    },
+    {
+        ["Rewards"] = {
+            {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 1000},
+            {["Type"] = "Powerup", ["Name"] = "Timeful Tome", ["Amount"] = 1},
+            {["Amount"] = 2, ["Type"] = "GoldenTicket"},
+            {["Type"] = "Treat", ["Name"] = "Golden Cherry", ["Amount"] = 2}
+        },
+        ["Amount"] = 50
+    },
+    {
+        ["Rewards"] = {
+            {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 2500},
+            {["Type"] = "CaptureCube", ["Name"] = "Legendary", ["Amount"] = 25},
+            {["Type"] = "Powerup", ["Name"] = "Respawn Tome", ["Amount"] = 2},
+            {["Type"] = "Powerup", ["Name"] = "Prismatic Sundae", ["Amount"] = 1}
+        },
+        ["Amount"] = 75
+    },
+    {
+        ["Rewards"] = {
+            {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 5000},
+            {["Type"] = "Treat", ["Name"] = "Golden Cherry", ["Amount"] = 5},
+            {["Type"] = "Powerup", ["Name"] = "Prismatic Tome", ["Amount"] = 1},
+            {["Type"] = "Pet", ["Name"] = "Classic Trio", ["Amount"] = 1}
+        },
+        ["Amount"] = 100
+    },
+    {
+        ["Rewards"] = {
+            {["Type"] = "CaptureCube", ["Name"] = "Legendary", ["Amount"] = 10},
+            {["Type"] = "Powerup", ["Name"] = "Lucky Elixir", ["Amount"] = 1}
+        },
+        ["Rarity"] = "Legendary",
+        ["Amount"] = 1
+    },
+    {
+        ["Rewards"] = {
+            {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 300},
+            {["Type"] = "Powerup", ["Name"] = "Elite Mystery Egg", ["Amount"] = 50},
+            {["Type"] = "Powerup", ["Name"] = "Coin Elixir", ["Amount"] = 2},
+            {["Type"] = "Powerup", ["Name"] = "XP Elixir", ["Amount"] = 2}
+        },
+        ["Rarity"] = "Legendary",
+        ["Amount"] = 3
+    },
+    {
+        ["Rewards"] = {
+            {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 550},
+            {["Type"] = "Powerup", ["Name"] = "Lucky Elixir", ["Amount"] = 2},
+            {["Type"] = "Powerup", ["Name"] = "Respawn Tome", ["Amount"] = 1},
+            {["Amount"] = 1, ["Type"] = "GoldenTicket"}
+        },
+        ["Rarity"] = "Legendary",
+        ["Amount"] = 5
+    },
+    {
+        ["Rewards"] = {
+            {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 1000},
+            {["Type"] = "CaptureCube", ["Name"] = "Legendary", ["Amount"] = 25},
+            {["Type"] = "Powerup", ["Name"] = "Timeful Tome", ["Amount"] = 2},
+            {["Type"] = "Powerup", ["Name"] = "Respawn Tome", ["Amount"] = 2},
+            {["Amount"] = 2, ["Type"] = "GoldenTicket"}
+        },
+        ["Rarity"] = "Legendary",
+        ["Amount"] = 10
+    },
+    {
+        ["Shiny"] = true,
+        ["Rewards"] = {
+            {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 75},
+            {["Amount"] = 1, ["Type"] = "GoldenTicket"}
+        },
+        ["Amount"] = 1
+    },
+    {
+        ["Shiny"] = true,
+        ["Rewards"] = {
+            {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 250},
+            {["Type"] = "Powerup", ["Name"] = "Timeful Tome", ["Amount"] = 1},
+            {["Type"] = "Powerup", ["Name"] = "Respawn Tome", ["Amount"] = 1}
+        },
+        ["Amount"] = 5
+    },
+    {
+        ["Shiny"] = true,
+        ["Rewards"] = {
+            {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 550},
+            {["Type"] = "Powerup", ["Name"] = "Timeful Tome", ["Amount"] = 3},
+            {["Type"] = "Powerup", ["Name"] = "Respawn Tome", ["Amount"] = 3},
+            {["Amount"] = 3, ["Type"] = "GoldenTicket"}
+        },
+        ["Amount"] = 10
+    },
+    {
+        ["Shiny"] = true,
+        ["Rewards"] = {
+            {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 1500},
+            {["Type"] = "Treat", ["Name"] = "Golden Cherry", ["Amount"] = 3},
+            {["Type"] = "Powerup", ["Name"] = "Prismatic Sundae", ["Amount"] = 1}
+        },
+        ["Amount"] = 20
+    },
+    {
+        ["Shiny"] = true,
+        ["Rewards"] = {
+            {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 8000},
+            {["Type"] = "Treat", ["Name"] = "Golden Cherry", ["Amount"] = 10},
+            {["Type"] = "Powerup", ["Name"] = "Prismatic Tome", ["Amount"] = 1},
+            {["Type"] = "Treat", ["Name"] = "Prismatic Scroll", ["Amount"] = 1},
+            {["Type"] = "Powerup", ["Name"] = "Prismatic Elixir", ["Amount"] = 1}
+        },
+        ["Amount"] = 40
+    },
+    {
+        ["Shiny"] = true,
+        ["Rewards"] = {
+            {["Type"] = "Treat", ["Name"] = "Golden Cherry", ["Amount"] = 20},
+            {["Type"] = "Powerup", ["Name"] = "Prismatic Elixir", ["Amount"] = 2},
+            {["Type"] = "Pet", ["Name"] = "The OwOlord", ["Amount"] = 1}
+        },
+        ["Amount"] = 55
+    }
+}
