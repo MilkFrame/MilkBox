455225ea67a282c8d74c721e69afd1f04fc645c3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:32:46 +1000

    Create Treats.lua

---
+++ GameData/Pet Catchers! [16510724413]/Treats.lua
@@ -0,0 +1,111 @@
+data = {
+    ["Deadly Rune"] = {
+        ["ShortDescription"] = "Increases pet's damage by MULTIPLIER. (Pet must have an empty Charm Slot)",
+        ["Description"] = [[Increases pet's damage by MULTIPLIER.\n\nPet must have an empty Charm Slot to use.]],
+        ["Extra"] = {["Multiplier"] = 1.1},
+        ["Rarity"] = "Legendary",
+        ["LayoutOrder"] = 7
+    },
+    ["Mystery Scroll"] = {
+        ["LayoutOrder"] = 8,
+        ["Extra"] = {["Chance"] = 1},
+        ["Description"] = "Rerolls all pet charms.",
+        ["Rarity"] = "Epic"
+    },
+    ["Seafarer Rune"] = {
+        ["ShortDescription"] = "MULTIPLIER more luck to catch Legendary fish. (Pet must have an empty Charm Slot)",
+        ["Description"] = [[MULTIPLIER more luck to catch Legendary fish.\n\nPet must have an empty Charm Slot to use.]],
+        ["Extra"] = {["Multiplier"] = 1.1},
+        ["Rarity"] = "Legendary",
+        ["LayoutOrder"] = 5
+    },
+    ["Golden Cherry"] = {
+        ["ShortDescription"] = [[Has a CHANCE <font color="#fff569">chance</font> to turn pets shiny.]],
+        ["Description"] = [[CHANCE <font color="#fff569">chance</font> of converting a pet to a shiny.]],
+        ["Extra"] = {["Chance"] = 3.5},
+        ["LayoutOrder"] = 9,
+        ["MultiUse"] = true,
+        ["Rarity"] = "Secret"
+    },
+    ["Critical Hit Rune"] = {
+        ["ShortDescription"] = "Increases your Critical Multiplier by +MULTIPLIER. (Pet must have an empty Charm Slot)",
+        ["Description"] = [[Increases your Critical Multiplier by +MULTIPLIER.\n\nPet must have an empty Charm Slot to use.]],
+        ["Extra"] = {["Multiplier"] = 1.1},
+        ["Rarity"] = "Legendary",
+        ["LayoutOrder"] = 1
+    },
+    ["Knowledge Scroll"] = {
+        ["LayoutOrder"] = 7,
+        ["Extra"] = {["Increase"] = 1},
+        ["Description"] = [[INCREASE <font color="#9cf7ff">Level</font> when fed to a pet.]],
+        ["Rarity"] = "Epic"
+    },
+    ["High Roller Rune"] = {
+        ["ShortDescription"] = [[CHANCE <font color="#fff569">chance</font> to get INCREASE Boss Drops. (Pet must have an empty Charm Slot)]],
+        ["Description"] = [[CHANCE <font color="#fff569">chance</font> to get INCREASE Boss Drops.\n\nPet must have an empty Charm Slot to use.]],
+        ["Extra"] = {["Chance"] = 5, ["Increase"] = 1},
+        ["Rarity"] = "Legendary",
+        ["LayoutOrder"] = 4
+    },
+    ["Radioactive Jelly"] = {
+        ["LayoutOrder"] = 5,
+        ["Description"] = [[CHANCE <font color="#fff569">chance</font> of adding a random charm to a pet.]],
+        ["Extra"] = {["Chance"] = 2},
+        ["MultiUse"] = true,
+        ["Rarity"] = "Rare"
+    },
+    ["Fatal Strike Rune"] = {
+        ["ShortDescription"] = "Increases pets damage by MULTIPLIER to Bosses below 50% Health. (Pet must have an empty Charm Slot)",
+        ["Description"] = [[Increases pets damage by MULTIPLIER to Bosses below 50% Health.\n\nPet must have an empty Charm Slot to use.]],
+        ["Extra"] = {["Multiplier"] = 1.05},
+        ["Rarity"] = "Legendary",
+        ["LayoutOrder"] = 2
+    },
+    ["Shrine Rune"] = {
+        ["ShortDescription"] = [[CHANCE <font color="#fff569">chance</font> to increase drops from shrines by INCREASE. (Pet must have an empty Charm Slot)]],
+        ["Description"] = [[CHANCE <font color="#fff569">chance</font> to increase drops from shrines by INCREASE.\n\nPet must have an empty Charm Slot to use.]],
+        ["Extra"] = {["Chance"] = 5, ["Increase"] = 1},
+        ["Rarity"] = "Legendary",
+        ["LayoutOrder"] = 6
+    },
+    ["Lifesteal Rune"] = {
+        ["ShortDescription"] = "Gain INCREASE Health when this pet deals damage. (Pet must have an empty Charm Slot)",
+        ["Description"] = [[Gain INCREASE Health when this pet deals damage.\n\nPet must have an empty Charm Slot to use.]],
+        ["Extra"] = {["Increase"] = 5},
+        ["Rarity"] = "Legendary",
+        ["LayoutOrder"] = 9
+    },
+    ["Voiding Scroll"] = {
+        ["LayoutOrder"] = 6,
+        ["Extra"] = {},
+        ["Description"] = "Removes the lowest charm from a pet.",
+        ["Rarity"] = "Rare"
+    },
+    ["Looting Rune"] = {
+        ["ShortDescription"] = "Increases pet's currency by MULTIPLIER. (Pet must have an empty Charm Slot)",
+        ["Description"] = [[Increases pet's currency by MULTIPLIER.\n\nPet must have an empty Charm Slot to use.]],
+        ["Extra"] = {["Multiplier"] = 1.1},
+        ["Rarity"] = "Legendary",
+        ["LayoutOrder"] = 8
+    },
+    ["Gamer Rune"] = {
+        ["ShortDescription"] = "While this pet plays a Minigame you're MULTIPLIER more likely to get Legendary. (Pet must have an empty Charm Slot)",
+        ["Description"] = [[While this pet plays a Minigame you're MULTIPLIER more likely to get Legendary.\n\nPet must have an empty Charm Slot to use.]],
+        ["Extra"] = {["Multiplier"] = 1.05},
+        ["Rarity"] = "Legendary",
+        ["LayoutOrder"] = 3
+    },
+    ["Prismatic Scroll"] = {
+        ["LayoutOrder"] = 10,
+        ["Extra"] = {},
+        ["Description"] = "Sets all Level 4 charms on a pet to Level 5.",
+        ["Rarity"] = "Secret"
+    },
+    ["Wildberry"] = {
+        ["LayoutOrder"] = 1,
+        ["Description"] = [[INCREASE <font color="#9cf7ff">XP</font> when fed to a pet.]],
+        ["Extra"] = {["Increase"] = 100},
+        ["MultiUse"] = true,
+        ["Rarity"] = "Common"
+    }
+}
