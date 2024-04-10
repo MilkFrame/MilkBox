3938919cf9b265fc0ec5b26bed922fa653de0771
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:36:09 +1000

    Create Powerups.lua

---
+++ GameData/Pet Catchers! [16510724413]/Powerups.lua
@@ -0,0 +1,113 @@
+data = {
+    ["Elite Mystery Egg"] = {
+        ["LayoutOrder"] = 5,
+        ["Rarity"] = "Epic",
+        ["ShortDescription"] = "Hatches into Epic and Legendary pets.",
+        ["Cooldown"] = 0,
+        ["Description"] = "Hatches into an Epic pet or above from the region it's used in."
+    },
+    ["Mystery Egg"] = {
+        ["LayoutOrder"] = 3,
+        ["Rarity"] = "Rare",
+        ["ShortDescription"] = "Hatches into Common to Epic pets.",
+        ["Cooldown"] = 0,
+        ["Description"] = "Hatches into a random Common, Rare, or Epic pet from the region it's used in."
+    },
+    ["Respawn Tome"] = {
+        ["LayoutOrder"] = 8,
+        ["NotConsumable"] = true,
+        ["Rarity"] = "Legendary",
+        ["ShortDescription"] = "Resurrects all bosses.",
+        ["Cooldown"] = 300,
+        ["Description"] = "A mystical tome with the power to resurrect bosses."
+    },
+    ["Prismatic Tome"] = {
+        ["LayoutOrder"] = 10,
+        ["Rarity"] = "Secret",
+        ["ShortDescription"] = "Respawns bosses, resets shrines, and restocks shops.",
+        ["Cooldown"] = 0,
+        ["Description"] = "A tome that respawns all bosses, resets all shrines, and rerolls all shops."
+    },
+    ["Coin Elixir"] = {
+        ["LayoutOrder"] = 2,
+        ["Rarity"] = "Common",
+        ["Buffs"] = {"Treasure"},
+        ["ShortDescription"] = "Temporarily increases your currency multiplier.",
+        ["Duration"] = 300,
+        ["Cooldown"] = 0,
+        ["Description"] = "A magical liquid that enhances your fortunes."
+    },
+    ["Lucky Elixir"] = {
+        ["LayoutOrder"] = 4,
+        ["Rarity"] = "Epic",
+        ["Buffs"] = {"Feeling Lucky", "Lucky Roll"},
+        ["ShortDescription"] = "Temporarily increases your luck.",
+        ["Duration"] = 300,
+        ["Cooldown"] = 0,
+        ["Description"] = "This potion is said to turn the tides of luck in your favor."
+    },
+    ["Prismatic Mystery Egg"] = {
+        ["LayoutOrder"] = 11,
+        ["Rarity"] = "Secret",
+        ["Extra"] = {["Chance"] = 5},
+        ["ShortDescription"] = "Hatches into Legendary pets and possibly Secrets pets.",
+        ["Cooldown"] = 0,
+        ["Description"] = [[Only hatches into <font color="#9cf7ff">Legendary Pets</font> from the region it's used in. Has a CHANCE <font color="#fff569">chance</font> of being <font color="#ffaaf7">Secret</font>.]]
+    },
+    ["Timeful Tome"] = {
+        ["LayoutOrder"] = 7,
+        ["Rarity"] = "Legendary",
+        ["Buffs"] = {"Stopwatch"},
+        ["ShortDescription"] = "Temporarily speeds up all timers.",
+        ["Duration"] = 1200,
+        ["Cooldown"] = 0,
+        ["Description"] = "A special book filled with secrets that bend the very fabric of time."
+    },
+    ["Prismatic Elixir"] = {
+        ["LayoutOrder"] = 10,
+        ["Rarity"] = "Secret",
+        ["Buffs"] = {
+            "Ultra Lucky",
+            "Super Lucky",
+            "Feeling Lucky",
+            "Super Roll",
+            "Lucky Roll",
+            "Fortune",
+            "Stopwatch",
+            "Ocean's Blessing",
+            "Treasure",
+            "Experienced"
+        },
+        ["ShortDescription"] = "It feels very lucky...",
+        ["Duration"] = 600,
+        ["Cooldown"] = 0,
+        ["Description"] = "A secret dev item..."
+    },
+    ["Prismatic Sundae"] = {
+        ["LayoutOrder"] = 9,
+        ["Rarity"] = "Secret",
+        ["Buffs"] = {"Super Roll", "Super Lucky", "Fortune"},
+        ["ShortDescription"] = "It feels very lucky...",
+        ["Duration"] = 1200,
+        ["Cooldown"] = 0,
+        ["Description"] = "A colorful sundae that feels very lucky... Who knows what fortunes it might bring?"
+    },
+    ["Sea Elixir"] = {
+        ["LayoutOrder"] = 6,
+        ["Rarity"] = "Legendary",
+        ["Buffs"] = {"Ocean's Blessing"},
+        ["ShortDescription"] = "Temporarily increases your fishing luck.",
+        ["Duration"] = 300,
+        ["Cooldown"] = 0,
+        ["Description"] = "A gift from the Sea that temporarily increases fishing luck."
+    },
+    ["XP Elixir"] = {
+        ["LayoutOrder"] = 1,
+        ["Rarity"] = "Common",
+        ["Buffs"] = {"Experienced"},
+        ["ShortDescription"] = "Temporarily increases your XP multiplier.",
+        ["Duration"] = 300,
+        ["Cooldown"] = 0,
+        ["Description"] = "A magical liquid that enhances your experience."
+    }
+}
