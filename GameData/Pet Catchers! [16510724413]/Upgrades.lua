e76ca48421ba2fd1ddb207a28cabf5572c7e992a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:39:50 +1000

    Create Upgrades.lua

---
+++ GameData/Pet Catchers! [16510724413]/Upgrades.lua
@@ -0,0 +1,218 @@
+data = {
+    ["luck-1"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251614242", ["Active"] = "rbxassetid://16251613216"},
+        ["Description"] = "🍀 +MULTIPLIER Luck",
+        ["Region"] = "Magma Basin",
+        ["Extra"] = {["Multiplier"] = 1.1},
+        ["Stat"] = "Luck",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 40000000000}
+    },
+    ["health-1"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251603608", ["Active"] = "rbxassetid://16251602926"},
+        ["Description"] = "❤️ INCREASE Health",
+        ["Region"] = "Auburn Woods",
+        ["Extra"] = {["Increase"] = 50},
+        ["Stat"] = "Health",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 20000}
+    },
+    ["team-2"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16306472608", ["Active"] = "rbxassetid://16251723749"},
+        ["Description"] = "Pet Teams INCREASE",
+        ["Region"] = "Gloomy Grotto",
+        ["Extra"] = {["Increase"] = 2},
+        ["Stat"] = "Pet Team",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 700}
+    },
+    ["inventory-2"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251583861", ["Active"] = "rbxassetid://16251582719"},
+        ["Description"] = "📦 INCREASE Pet Storage",
+        ["Region"] = "Mellow Meadows",
+        ["Extra"] = {["Increase"] = 20},
+        ["Stat"] = "Inventory",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 4000}
+    },
+    ["equipped-1"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251591196", ["Active"] = "rbxassetid://16251586201"},
+        ["Description"] = "🐶 INCREASE Pets Equipped",
+        ["Region"] = "Mellow Meadows",
+        ["Extra"] = {["Increase"] = 1},
+        ["Stat"] = "Pet Equip",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 3000}
+    },
+    ["inventory-3"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251583861", ["Active"] = "rbxassetid://16251582719"},
+        ["Description"] = "📦 INCREASE Pet Storage",
+        ["Region"] = "Auburn Woods",
+        ["Extra"] = {["Increase"] = 30},
+        ["Stat"] = "Inventory",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 45000}
+    },
+    ["equipped-2"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251591196", ["Active"] = "rbxassetid://16251586201"},
+        ["Description"] = "🐶 INCREASE Pets Equipped",
+        ["Region"] = "Mellow Meadows",
+        ["Extra"] = {["Increase"] = 1},
+        ["Stat"] = "Pet Equip",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 100000}
+    },
+    ["egg-hatch-1"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16433573730", ["Active"] = "rbxassetid://16433572708"},
+        ["Description"] = "Egg Hatch INCREASE",
+        ["Region"] = "Magma Basin",
+        ["Extra"] = {["Increase"] = 1},
+        ["Stat"] = "Egg",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 95000}
+    },
+    ["health-4"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251603608", ["Active"] = "rbxassetid://16251602926"},
+        ["Description"] = "❤️ INCREASE Health",
+        ["Region"] = "Gloomy Grotto",
+        ["Extra"] = {["Increase"] = 400},
+        ["Stat"] = "Health",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 40000000}
+    },
+    ["coins-2"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251596612", ["Active"] = "rbxassetid://16251595536"},
+        ["Description"] = "💰 +MULTIPLIER Coin Multiplier",
+        ["Region"] = "Frosty Peaks",
+        ["Extra"] = {["Multiplier"] = 1.25},
+        ["Stat"] = "Coins",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 190000}
+    },
+    ["team-1"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16306472608", ["Active"] = "rbxassetid://16251723749"},
+        ["Description"] = "Pet Teams INCREASE",
+        ["Region"] = "Dusty Dunes",
+        ["Extra"] = {["Increase"] = 2},
+        ["Stat"] = "Pet Team",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 15000000}
+    },
+    ["inventory-5"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251583861", ["Active"] = "rbxassetid://16251582719"},
+        ["Description"] = "📦 INCREASE Pet Storage",
+        ["Region"] = "Gloomy Grotto",
+        ["Extra"] = {["Increase"] = 100},
+        ["Stat"] = "Inventory",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 250000000}
+    },
+    ["auto-rejoin"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251671421", ["Active"] = "rbxassetid://16251670649"},
+        ["Description"] = "Automatic Rejoin",
+        ["Region"] = "Magma Basin",
+        ["Extra"] = {["Increase"] = 1},
+        ["Stat"] = "Auto Rejoin",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 25000}
+    },
+    ["auto-afk"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251528195", ["Active"] = "rbxassetid://16251526901"},
+        ["Description"] = "Auto AFK",
+        ["Region"] = "Dusty Dunes",
+        ["Extra"] = {["Increase"] = 1},
+        ["Stat"] = "Auto AFK",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 7500}
+    },
+    ["damage-1"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251608020", ["Active"] = "rbxassetid://16251606636"},
+        ["Description"] = "💪 +MULTIPLIER Damage",
+        ["Region"] = "Auburn Woods",
+        ["Extra"] = {["Multiplier"] = 1.1},
+        ["Stat"] = "Damage",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 25000}
+    },
+    ["damage-3"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251608020", ["Active"] = "rbxassetid://16251606636"},
+        ["Description"] = "💪 +MULTIPLIER Damage",
+        ["Region"] = "Magma Basin",
+        ["Extra"] = {["Multiplier"] = 1.25},
+        ["Stat"] = "Damage",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 2000000000}
+    },
+    ["drops-1"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251621116", ["Active"] = "rbxassetid://16251620263"},
+        ["Description"] = "⚔️ INCREASE Mob Drops",
+        ["Region"] = "Frosty Peaks",
+        ["Extra"] = {["Increase"] = 1},
+        ["Stat"] = "Drops",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 350000}
+    },
+    ["coins-3"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251596612", ["Active"] = "rbxassetid://16251595536"},
+        ["Description"] = "💰 +MULTIPLIER Coin Multiplier",
+        ["Region"] = "Sunset Shores",
+        ["Extra"] = {["Multiplier"] = 1.5},
+        ["Stat"] = "Coins",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 2000000}
+    },
+    ["coins-1"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251596612", ["Active"] = "rbxassetid://16251595536"},
+        ["Description"] = "💰 +MULTIPLIER Coin Multiplier",
+        ["Region"] = "Mellow Meadows",
+        ["Extra"] = {["Multiplier"] = 1.25},
+        ["Stat"] = "Coins",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 3500}
+    },
+    ["auto-delete"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251626571", ["Active"] = "rbxassetid://16251625574"},
+        ["Description"] = "Auto Delete",
+        ["Region"] = "Frosty Peaks",
+        ["Extra"] = {["Increase"] = 1},
+        ["Stat"] = "Auto Delete",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 175000}
+    },
+    ["inventory-1"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251583861", ["Active"] = "rbxassetid://16251582719"},
+        ["Description"] = "📦 INCREASE Pet Storage",
+        ["Region"] = "Pet Park",
+        ["Extra"] = {["Increase"] = 10},
+        ["Stat"] = "Inventory",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 250}
+    },
+    ["health-2"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251603608", ["Active"] = "rbxassetid://16251602926"},
+        ["Description"] = "❤️ INCREASE Health",
+        ["Region"] = "Auburn Woods",
+        ["Extra"] = {["Increase"] = 50},
+        ["Stat"] = "Health",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 35000}
+    },
+    ["damage-2"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251608020", ["Active"] = "rbxassetid://16251606636"},
+        ["Description"] = "💪 +MULTIPLIER Damage",
+        ["Region"] = "Sunset Shores",
+        ["Extra"] = {["Multiplier"] = 1.25},
+        ["Stat"] = "Damage",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 650000}
+    },
+    ["auto-fish"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251632885", ["Active"] = "rbxassetid://16251700080"},
+        ["Description"] = "Auto Fish",
+        ["Region"] = "Sunset Shores",
+        ["Extra"] = {["Increase"] = 1},
+        ["Stat"] = "Auto Fish",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 5000}
+    },
+    ["health-3"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251603608", ["Active"] = "rbxassetid://16251602926"},
+        ["Description"] = "❤️ INCREASE Health",
+        ["Region"] = "Sunset Shores",
+        ["Extra"] = {["Increase"] = 200},
+        ["Stat"] = "Health",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 300000}
+    },
+    ["team-3"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16306472608", ["Active"] = "rbxassetid://16251723749"},
+        ["Description"] = "Pet Teams INCREASE",
+        ["Region"] = "Magma Basin",
+        ["Extra"] = {["Increase"] = 2},
+        ["Stat"] = "Pet Team",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Gems", ["Amount"] = 3000}
+    },
+    ["inventory-4"] = {
+        ["Image"] = {["Inactive"] = "rbxassetid://16251583861", ["Active"] = "rbxassetid://16251582719"},
+        ["Description"] = "📦 INCREASE Pet Storage",
+        ["Region"] = "Dusty Dunes",
+        ["Extra"] = {["Increase"] = 70},
+        ["Stat"] = "Inventory",
+        ["Cost"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 40000000}
+    }
+}
