0b27b7ba2d2f3c9696399977d9d3f0ae6a7f2d57
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:39:15 +1000

    Create Enemies.lua

---
+++ GameData/Pet Catchers! [16510724413]/Enemies.lua
@@ -0,0 +1,165 @@
+data = {
+    ["Molten Crab"] = {
+        ["LayoutOrder"] = 10,
+        ["LootAmount"] = "4 4 ",
+        ["Health"] = 7000,
+        ["Model"] = "Molten Crab",
+        ["Loot"] = {
+            {["Item"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 800}, ["Chance"] = 70},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Wildberry", ["Amount"] = 20}, ["Chance"] = 26},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Elite Mystery Egg", ["Amount"] = 4}, ["Chance"] = 4}
+        },
+        ["Module"] = "Crab",
+        ["XP"] = 5500,
+        ["Damage"] = 7
+    },
+    ["Prismatic Slime"] = {
+        ["LayoutOrder"] = 3,
+        ["LootAmount"] = "5 8 ",
+        ["Health"] = 550,
+        ["Model"] = "Prismatic Slime",
+        ["Loot"] = {
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Elite Mystery Egg", ["Amount"] = 100}, ["Chance"] = 43},
+            {["Item"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 500}, ["Chance"] = 39},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Coin Elixir", ["Amount"] = 1}, ["Chance"] = 5},
+            {["Item"] = {["Type"] = "CaptureCube", ["Name"] = "Legendary", ["Amount"] = 1}, ["Chance"] = 5},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Respawn Tome", ["Amount"] = 1}, ["Chance"] = 1},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Timeful Tome", ["Amount"] = 1}, ["Chance"] = 1},
+            {["Item"] = {["Amount"] = 1, ["Type"] = "GoldenTicket"}, ["Chance"] = 1},
+            {
+                ["Item"] = {
+                    ["Color"] = "Default",
+                    ["Type"] = "Mount",
+                    ["Name"] = "Prismatic Hoverboard",
+                    ["Amount"] = 1
+                },
+                ["Chance"] = 0.1
+            },
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Prismatic Sundae", ["Amount"] = 1}, ["Chance"] = 0.1},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Prismatic Tome", ["Amount"] = 1}, ["Chance"] = 0.1}
+        },
+        ["Module"] = "Slime",
+        ["XP"] = 15000,
+        ["Damage"] = 20
+    },
+    ["Mutant Slime"] = {
+        ["LayoutOrder"] = 9,
+        ["LootAmount"] = "4 4 ",
+        ["Health"] = 6000,
+        ["Model"] = "Mutant Slime",
+        ["Loot"] = {
+            {["Item"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 550}, ["Chance"] = 66},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Wildberry", ["Amount"] = 5}, ["Chance"] = 27},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Radioactive Jelly", ["Amount"] = 5}, ["Chance"] = 5},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Voiding Scroll", ["Amount"] = 1}, ["Chance"] = 1},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Mystery Scroll", ["Amount"] = 1}, ["Chance"] = 1}
+        },
+        ["Module"] = "Slime",
+        ["XP"] = 4100,
+        ["Damage"] = 10
+    },
+    ["Scorpion"] = {
+        ["LayoutOrder"] = 8,
+        ["LootAmount"] = "4 4 ",
+        ["Health"] = 5500,
+        ["Model"] = "Scorpion",
+        ["Loot"] = {
+            {["Item"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 450}, ["Chance"] = 68},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Wildberry", ["Amount"] = 20}, ["Chance"] = 15},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Mystery Egg", ["Amount"] = 20}, ["Chance"] = 10},
+            {["Item"] = {["Type"] = "CaptureCube", ["Name"] = "Rare", ["Amount"] = 2}, ["Chance"] = 5},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Elite Mystery Egg", ["Amount"] = 3}, ["Chance"] = 2}
+        },
+        ["Module"] = "Scorpion",
+        ["XP"] = 5000,
+        ["Damage"] = 40
+    },
+    ["Snowman"] = {
+        ["LayoutOrder"] = 4,
+        ["LootAmount"] = "3 4 ",
+        ["Health"] = 1100,
+        ["Model"] = "Snowman",
+        ["Loot"] = {
+            {["Item"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 140}, ["Chance"] = 65},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Wildberry", ["Amount"] = 7}, ["Chance"] = 35}
+        },
+        ["Module"] = "Snowman",
+        ["XP"] = 4000,
+        ["Damage"] = 7
+    },
+    ["King Crab"] = {
+        ["LayoutOrder"] = 7,
+        ["LootAmount"] = "5 8 ",
+        ["Health"] = 7500,
+        ["Model"] = "King Crab",
+        ["Loot"] = {
+            {["Item"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 3000}, ["Chance"] = 45},
+            {["Item"] = {["Type"] = "CaptureCube", ["Name"] = "Rare", ["Amount"] = 20}, ["Chance"] = 20},
+            {["Item"] = {["Type"] = "CaptureCube", ["Name"] = "Epic", ["Amount"] = 10}, ["Chance"] = 10},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Mystery Egg", ["Amount"] = 40}, ["Chance"] = 9},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Wildberry", ["Amount"] = 15}, ["Chance"] = 8},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Elite Mystery Egg", ["Amount"] = 5}, ["Chance"] = 5},
+            {["Item"] = {["Type"] = "CaptureCube", ["Name"] = "Legendary", ["Amount"] = 1}, ["Chance"] = 2},
+            {["Item"] = {["Amount"] = 1, ["Type"] = "GoldenTicket"}, ["Chance"] = 1}
+        },
+        ["Module"] = "Crab",
+        ["XP"] = 40000,
+        ["Damage"] = 25
+    },
+    ["Crab"] = {
+        ["LayoutOrder"] = 6,
+        ["LootAmount"] = "4 4 ",
+        ["Health"] = 4000,
+        ["Model"] = "Crab",
+        ["Loot"] = {
+            {["Item"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 300}, ["Chance"] = 70},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Wildberry", ["Amount"] = 20}, ["Chance"] = 10},
+            {["Item"] = {["Type"] = "CaptureCube", ["Name"] = "Rare", ["Amount"] = 5}, ["Chance"] = 10},
+            {["Item"] = {["Type"] = "Powerup", ["Name"] = "Mystery Egg", ["Amount"] = 10}, ["Chance"] = 10}
+        },
+        ["Module"] = "Crab",
+        ["XP"] = 5000,
+        ["Damage"] = 20
+    },
+    ["Armored Snowman"] = {
+        ["LayoutOrder"] = 5,
+        ["LootAmount"] = "4 4 ",
+        ["Health"] = 2900,
+        ["Model"] = "Armored Snowman",
+        ["Loot"] = {
+            {["Item"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 220}, ["Chance"] = 70},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Wildberry", ["Amount"] = 14}, ["Chance"] = 25},
+            {["Item"] = {["Type"] = "CaptureCube", ["Name"] = "Rare", ["Amount"] = 2}, ["Chance"] = 5}
+        },
+        ["Module"] = "Snowman",
+        ["XP"] = 4500,
+        ["Damage"] = 16
+    },
+    ["Slime"] = {
+        ["LayoutOrder"] = 1,
+        ["LootAmount"] = "3 3 ",
+        ["Health"] = 120,
+        ["Model"] = "Slime",
+        ["Loot"] = {
+            {["Item"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 90}, ["Chance"] = 50},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Wildberry", ["Amount"] = 5}, ["Chance"] = 50}
+        },
+        ["Module"] = "Slime",
+        ["XP"] = 3500,
+        ["Damage"] = 3
+    },
+    ["Mega Slime"] = {
+        ["LayoutOrder"] = 2,
+        ["LootAmount"] = "3 3 ",
+        ["Health"] = 320,
+        ["Model"] = "Mega Slime",
+        ["Loot"] = {
+            {["Item"] = {["Type"] = "Currency", ["Currency"] = "Coins", ["Amount"] = 220}, ["Chance"] = 50},
+            {["Item"] = {["Type"] = "Treat", ["Name"] = "Wildberry", ["Amount"] = 20}, ["Chance"] = 39},
+            {["Item"] = {["Type"] = "CaptureCube", ["Name"] = "Rare", ["Amount"] = 1}, ["Chance"] = 11}
+        },
+        ["Module"] = "Slime",
+        ["XP"] = 5500,
+        ["Damage"] = 7
+    }
+}
