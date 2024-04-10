802953bd79918cbab59ebc03d36519240a0f8a43
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:42:43 +1000

    Create ChatTags.lua

---
+++ GameData/Pet Catchers! [16510724413]/ChatTags.lua
@@ -0,0 +1,7 @@
+data = {
+    ["kraken-champ"] = {["Color"] = Color3.fromRGB(255, 135, 135), ["Text"] = "🦑 #1"},
+    ["slime-champ"] = {["Color"] = Color3.fromRGB(177, 177, 177), ["Text"] = "⚔ #1"},
+    ["fish-champ"] = {["Color"] = Color3.fromRGB(7, 189, 255), ["Text"] = "🐟 #1"},
+    ["catch-champ"] = {["Color"] = Color3.fromRGB(255, 108, 35), ["Text"] = "🐶 #1"},
+    ["vip"] = {["Color"] = Color3.fromRGB(255, 172, 7), ["Text"] = "👑 VIP"}
+}
