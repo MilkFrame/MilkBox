f243c7b808c1cdeac796e780ef3de1e53c56bb8b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-10 14:34:35 +1000

    Create Options.lua

---
+++ GameData/Pet Catchers! [16510724413]/Options.lua
@@ -0,0 +1,81 @@
+data = {
+    ["Sound Effects"] = {
+        ["LayoutOrder"] = 4,
+        ["Type"] = "Slider",
+        ["Data"] = {["Increment"] = 5, ["Default"] = 100, ["Range"] = "0 100 "},
+        ["Category"] = "Sound"
+    },
+    ["UI Scale"] = {
+        ["LayoutOrder"] = 1,
+        ["Type"] = "Slider",
+        ["Data"] = {["Increment"] = 5, ["Default"] = 100, ["Range"] = "75 150 "},
+        ["Category"] = "General"
+    },
+    ["Render Distance"] = {
+        ["LayoutOrder"] = 0,
+        ["Type"] = "Dropdown",
+        ["Data"] = {["Default"] = "High", ["Options"] = {"Low", "Medium", "High", "Ultra"}},
+        ["Category"] = "General"
+    },
+    ["Disable Player Eggs"] = {
+        ["LayoutOrder"] = 4,
+        ["Type"] = "Switch",
+        ["Data"] = {["Default"] = false},
+        ["Category"] = "Pets"
+    },
+    ["Stats"] = {["LayoutOrder"] = 1, ["Type"] = "Switch", ["Data"] = {["Default"] = false}, ["Category"] = "Debug"},
+    ["Visual Effects"] = {
+        ["LayoutOrder"] = 2,
+        ["Type"] = "Switch",
+        ["Data"] = {["Default"] = true},
+        ["Category"] = "General"
+    },
+    ["Hide Others Pets"] = {
+        ["LayoutOrder"] = 3,
+        ["Type"] = "Switch",
+        ["Data"] = {["Default"] = false},
+        ["Category"] = "Pets"
+    },
+    ["Master Volume"] = {
+        ["LayoutOrder"] = 2,
+        ["Type"] = "Slider",
+        ["Data"] = {["Increment"] = 5, ["Default"] = 100, ["Range"] = "0 100 "},
+        ["Category"] = "Sound"
+    },
+    ["Pet Animations"] = {
+        ["LayoutOrder"] = 0,
+        ["Type"] = "Switch",
+        ["Data"] = {["Default"] = true},
+        ["Category"] = "Pets"
+    },
+    ["Button Click"] = {
+        ["LayoutOrder"] = 1,
+        ["Type"] = "Switch",
+        ["Data"] = {["Default"] = true},
+        ["Category"] = "Sound"
+    },
+    ["Performance"] = {
+        ["LayoutOrder"] = 1,
+        ["Type"] = "Switch",
+        ["Data"] = {["Default"] = false},
+        ["Category"] = "Debug"
+    },
+    ["Music"] = {
+        ["LayoutOrder"] = 3,
+        ["Type"] = "Slider",
+        ["Data"] = {["Increment"] = 5, ["Default"] = 75, ["Range"] = "0 100 "},
+        ["Category"] = "Sound"
+    },
+    ["Hide Your Pets"] = {
+        ["LayoutOrder"] = 2,
+        ["Type"] = "Switch",
+        ["Data"] = {["Default"] = false},
+        ["Category"] = "Pets"
+    },
+    ["Easy Legendary Alerts"] = {
+        ["LayoutOrder"] = 1,
+        ["Type"] = "Switch",
+        ["Data"] = {["Default"] = true},
+        ["Category"] = "Pets"
+    }
+}
