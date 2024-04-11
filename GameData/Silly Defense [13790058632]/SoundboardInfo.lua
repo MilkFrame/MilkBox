b8639dc2b98fce72ed8c61ccba4f5ff96be8de6c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-11 16:19:09 +1000

    Create SoundboardInfo.lua

---
+++ GameData/Silly Defense [13790058632]/SoundboardInfo.lua
@@ -0,0 +1,9 @@
+data = {
+    ["Penetrator"] = {["Confetti"] = {["Price"] = 424, ["PriceType"] = "Coins"}},
+    ["Firecracker"] = {["Confetti"] = {["Price"] = 224, ["PriceType"] = "Coins"}},
+    ["Quarterer"] = {["HiTech"] = {["Price"] = 955, ["PriceType"] = "Coins"}},
+    ["Focus"] = {
+        ["Confetti"] = {["Price"] = 624, ["PriceType"] = "Coins"},
+        ["Italian"] = {["Price"] = 1333, ["PriceType"] = "Coins"}
+    }
+}
