50301d9922b334d446479728e15836b4c6fadb9f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-11 16:19:31 +1000

    Create Difficulties.lua

---
+++ GameData/Silly Defense [13790058632]/Difficulties.lua
@@ -0,0 +1,292 @@
+data = {
+    ["Medium"] = {
+        ["Waves"] = {
+            {["Enemies"] = {"Afflicted_4"}, ["CashReward"] = 0, ["Time"] = 15, ["SkipIn"] = 3},
+            {["Enemies"] = {"Afflicted_6"}, ["CashReward"] = 150, ["Time"] = 15, ["SkipIn"] = 3},
+            {["Enemies"] = {"Apace_4", "Afflicted_4"}, ["CashReward"] = 170, ["Time"] = 20, ["SkipIn"] = 3},
+            {
+                ["Time"] = 25,
+                ["Spawnrate"] = 2,
+                ["CashReward"] = 200,
+                ["SkipIn"] = 5,
+                ["Enemies"] = {"Apace_5", "Afflicted_3"}
+            },
+            {["Enemies"] = {"Dulled_4", "Apace_2"}, ["CashReward"] = 220, ["Time"] = 25, ["SkipIn"] = 5},
+            {
+                ["Time"] = 30,
+                ["Spawnrate"] = 1,
+                ["CashReward"] = 250,
+                ["SkipIn"] = 7,
+                ["Enemies"] = {"Dulled_4", "Wait(1)", "Apace_3_Spawnrate:0.75"}
+            },
+            {["Enemies"] = {"Quick_4_Spawnrate:0.5", "Dulled_4"}, ["CashReward"] = 260, ["Time"] = 15, ["SkipIn"] = 3},
+            {
+                ["Enemies"] = {"Broken Boss", "Afflicted_6", "Dulled_4"},
+                ["CashReward"] = 270,
+                ["Time"] = 30,
+                ["SkipIn"] = 5
+            },
+            {
+                ["Enemies"] = {"Broken Boss_1_Immunity:Shield50", "Apace_5_Spawnrate:0.2_Immunity:Defense5"},
+                ["CashReward"] = 350,
+                ["Time"] = 120,
+                ["SkipIn"] = 15
+            },
+            {
+                ["Enemies"] = {"Wasted Energy_3_Spawnrate:0.75", "Broken Boss"},
+                ["CashReward"] = 360,
+                ["Time"] = 100,
+                ["SkipIn"] = 15
+            },
+            {["Enemies"] = {"Nimble Boss", "Wasted Energy_3"}, ["CashReward"] = 550, ["Time"] = 80, ["SkipIn"] = 20},
+            {["Enemies"] = {"Broken Boss", "Wasted Energy_3"}, ["CashReward"] = 450, ["Time"] = 100, ["SkipIn"] = 35},
+            {
+                ["Enemies"] = {"Unnoticeable Boss", "Unnoticeable_6_Immunity:Shield5", "Afflicted_4_Spawnrate:0.5"},
+                ["CashReward"] = 750,
+                ["Time"] = 140,
+                ["SkipIn"] = 30
+            },
+            {
+                ["Enemies"] = {"Nimble Boss", "Broken Boss_2_Immunity:Shield5", "Wasted Energy_4_Spawnrate:0.75"},
+                ["CashReward"] = 600,
+                ["Time"] = 200,
+                ["SkipIn"] = 40
+            },
+            {
+                ["Enemies"] = {"Thunder Boss", "Wasted Energy_4_Spawnrate:0.75"},
+                ["CashReward"] = 460,
+                ["Time"] = 150,
+                ["SkipIn"] = 30
+            },
+            {
+                ["Enemies"] = {
+                    "Nimble Boss_2",
+                    "Thunder Boss",
+                    "Wasted Energy_2_Spawnrate:0.75",
+                    "Unnoticeable_10_Immunity:Shield15"
+                },
+                ["CashReward"] = 465,
+                ["Time"] = 160,
+                ["SkipIn"] = 40
+            },
+            {
+                ["Enemies"] = {"Thunder Boss_Immunity:Shield150", "Wasted Energy_4_Spawnrate:0.75"},
+                ["CashReward"] = 440,
+                ["Time"] = 250,
+                ["SkipIn"] = 40
+            },
+            {
+                ["Enemies"] = {"Thundering Cloud", "Wasted Energy_8_Spawnrate:0.75", "Thunder Boss_Immunity:Shield200"},
+                ["CashReward"] = 450,
+                ["Time"] = 160,
+                ["SkipIn"] = 60
+            },
+            {
+                ["Enemies"] = {
+                    "Wasted Energy_8_Spawnrate:0.75",
+                    "Thunder Boss_Immunity:Shield200",
+                    "Lightning Dasher_2_Spawnrate:0.5"
+                },
+                ["CashReward"] = 460,
+                ["Time"] = 150,
+                ["SkipIn"] = 50
+            },
+            {
+                ["Enemies"] = {
+                    "Droplet_5_Immunity:Shield25",
+                    "Stormful_Immunity:Boss",
+                    "Thundering Cloud_1",
+                    "Thunder_2"
+                },
+                ["CashReward"] = 470,
+                ["Time"] = 140,
+                ["SkipIn"] = 40
+            },
+            {
+                ["Enemies"] = {
+                    "Droplet_8_Immunity:Shield25",
+                    "Thundering Cloud_2_Immunity:Shield150",
+                    "Thunder_2",
+                    "Lightning Dasher_4_Spawnrate:0.5"
+                },
+                ["CashReward"] = 480,
+                ["Time"] = 150,
+                ["SkipIn"] = 30
+            },
+            {
+                ["Enemies"] = {"Droplet_6_Immunity:Shield25", "Thundering Cloud_2_Immunity:Shield150", "Thunder_2"},
+                ["CashReward"] = 500,
+                ["Time"] = 180,
+                ["SkipIn"] = 60
+            },
+            {
+                ["Enemies"] = {
+                    "Droplet_4_Immunity:Shield25",
+                    "Lightning Boss",
+                    "Thundering Cloud_1_Immunity:Shield150",
+                    "Thunder_1_Immunity:Shield50"
+                },
+                ["CashReward"] = 510,
+                ["Time"] = 190,
+                ["SkipIn"] = 70
+            },
+            {
+                ["Enemies"] = {"Lightning Boss_1", "Stormful", "Thundering Cloud_2"},
+                ["CashReward"] = 520,
+                ["Time"] = 190,
+                ["SkipIn"] = 55
+            },
+            {
+                ["Enemies"] = {"Lightning Boss", "Lightning_10_Spawnrate:0.2"},
+                ["CashReward"] = 530,
+                ["Time"] = 120,
+                ["SkipIn"] = 45
+            },
+            {
+                ["Enemies"] = {"Stormful", "Thundering Cloud", "Lightning Boss_2", "Lightning_2_Spawnrate:0.2"},
+                ["CashReward"] = 540,
+                ["Time"] = 140,
+                ["SkipIn"] = 50
+            },
+            {
+                ["Enemies"] = {
+                    "Wasted Energy_10_Immunity:Shield50",
+                    "Thunder Boss_1_Immunity:Shield500",
+                    "Thundering Cloud",
+                    "Lightning Boss_2",
+                    "Thundering Cloud"
+                },
+                ["CashReward"] = 550,
+                ["Time"] = 150,
+                ["SkipIn"] = 55
+            },
+            {
+                ["Enemies"] = {
+                    "Lightning Boss",
+                    "Lightning_4_Spawnrate:0.2",
+                    "Harbinger_2_Spawnrate:2_Immunity:Boss",
+                    "Wait(4)",
+                    "Thundering Cloud_2_Spawnrate:2",
+                    "Lightning Dasher_1_Spawnrate:0.5"
+                },
+                ["CashReward"] = 560,
+                ["Time"] = 300,
+                ["SkipIn"] = 80
+            },
+            {
+                ["Enemies"] = {
+                    "Thundering Cloud",
+                    "Stormful",
+                    "Wait(5)",
+                    "Lightning Boss",
+                    "Thunder Boss",
+                    "Wasted Energy"
+                },
+                ["CashReward"] = 600,
+                ["Time"] = 170,
+                ["SkipIn"] = 55
+            },
+            {
+                ["Enemies"] = {
+                    "Harbinger_1_Spawnrate:1_Immunity:Shield500",
+                    "Stormful_Immunity:Shield250",
+                    "Lightning_5_Spawnrate:0.5",
+                    "Vociferous_Immunity:Boss",
+                    "Thundering Cloud_2_Immunity:Shield250",
+                    "Lightning Boss_3_Spawnrate:0.75",
+                    "Thunder Boss_6_Spawnrate:1.5"
+                },
+                ["CashReward"] = 650,
+                ["Time"] = "Inf",
+                ["SkipIn"] = 99999
+            }
+        }
+    },
+    ["Easy"] = {
+        ["Waves"] = {
+            {["Enemies"] = {"Drab_4"}, ["CashReward"] = 0, ["Time"] = 15, ["SkipIn"] = 3},
+            {["Enemies"] = {"Drab_6"}, ["CashReward"] = 70, ["Time"] = 15, ["SkipIn"] = 3},
+            {["Enemies"] = {"Quick_4", "Drab_4"}, ["CashReward"] = 75, ["Time"] = 20, ["SkipIn"] = 3},
+            {["Time"] = 25, ["Spawnrate"] = 2, ["CashReward"] = 85, ["SkipIn"] = 5, ["Enemies"] = {"Trampled_4"}},
+            {["Enemies"] = {"Trampled_4"}, ["CashReward"] = 130, ["Time"] = 25, ["SkipIn"] = 5},
+            {
+                ["Time"] = 30,
+                ["Spawnrate"] = 1,
+                ["CashReward"] = 150,
+                ["SkipIn"] = 7,
+                ["Enemies"] = {"Trampled_4", "Wait(1)", "Quick_3_Spawnrate:0.75"}
+            },
+            {["Enemies"] = {"Quick_10_Spawnrate:0.5"}, ["CashReward"] = 200, ["Time"] = 15, ["SkipIn"] = 3},
+            {
+                ["Enemies"] = {"Drab_10_Spawnrate:0.5_Immunity:Defense10"},
+                ["CashReward"] = 300,
+                ["Time"] = 25,
+                ["SkipIn"] = 5
+            },
+            {["Enemies"] = {"Drabby Boss", "Trampled_4"}, ["CashReward"] = 350, ["Time"] = 60, ["SkipIn"] = 15},
+            {
+                ["Enemies"] = {"Drabby Boss", "Quick_10_Spawnrate:0.2_Immunity:Defense25"},
+                ["CashReward"] = 360,
+                ["Time"] = 60,
+                ["SkipIn"] = 15
+            },
+            {["Enemies"] = {"Unnoticeable_5"}, ["CashReward"] = 400, ["Time"] = 20, ["SkipIn"] = 8},
+            {
+                ["Enemies"] = {"Drabby Boss_2_Immunity:Defense5", "Unnoticeable_4"},
+                ["CashReward"] = 450,
+                ["Time"] = 70,
+                ["SkipIn"] = 20
+            },
+            {["Enemies"] = {"Quick Boss"}, ["CashReward"] = 475, ["Time"] = 30, ["SkipIn"] = 10},
+            {
+                ["Time"] = 60,
+                ["Spawnrate"] = 0.75,
+                ["CashReward"] = 500,
+                ["SkipIn"] = 15,
+                ["Enemies"] = {"Drabby Boss", "Quick Boss", "Drab_10", "Quick_5"}
+            },
+            {
+                ["Time"] = 60,
+                ["Spawnrate"] = 0.75,
+                ["CashReward"] = 500,
+                ["SkipIn"] = 20,
+                ["Enemies"] = {"Acid Waste_5", "Drabby Boss", "Trampled_10"}
+            },
+            {
+                ["Time"] = 90,
+                ["Spawnrate"] = 0.75,
+                ["CashReward"] = 500,
+                ["SkipIn"] = 30,
+                ["Enemies"] = {"Quick Boss", "Acid Waste_10", "Trampled_2"}
+            },
+            {
+                ["Time"] = 80,
+                ["Spawnrate"] = 0.75,
+                ["CashReward"] = 500,
+                ["SkipIn"] = 25,
+                ["Enemies"] = {"Acid Boss", "Unnoticeable_10_Immunity:Defense25", "Acid Waste_5", "Quick_5"}
+            },
+            {
+                ["Time"] = 75,
+                ["Spawnrate"] = 0.75,
+                ["CashReward"] = 750,
+                ["SkipIn"] = 25,
+                ["Enemies"] = {"Acid Boss_2", "Quick Boss_3_Spawnrate:0.5", "Drab_5", "Acid Waste_10"}
+            },
+            {
+                ["Time"] = 120,
+                ["Spawnrate"] = 0.75,
+                ["CashReward"] = 500,
+                ["SkipIn"] = 45,
+                ["Enemies"] = {"Acid Boss_2", "Quick Boss_1", "Acid Bot_3", "Acid Waste_5", "Trampled_10_Spawnrate:0.5"}
+            },
+            {
+                ["Time"] = "Inf",
+                ["Spawnrate"] = 0.75,
+                ["CashReward"] = 1250,
+                ["SkipIn"] = 9999,
+                ["Enemies"] = {"Quick Boss", "Drab_15", "Wait(5)", "Acid Spitter", "Acid Bot_5_Immunity:Defense15"}
+            }
+        }
+    }
+}
