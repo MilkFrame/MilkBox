a2ef670aabc79bd6a616a8bb485bf30d50095e21
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-11 16:18:16 +1000

    Create TowerInfo.lua

---
+++ GameData/Silly Defense [13790058632]/TowerInfo.lua
@@ -0,0 +1,94 @@
+data = {
+    ["Stasis"] = {
+        ["Price"] = 3750,
+        ["Level"] = 25,
+        ["Description"] = "A Ground Support Tower. Stasis Stops the Enemies dead on path in the Forcefield Radius. Stasis Duration scales with Enemy Health",
+        ["PriceType"] = "Coins"
+    },
+    ["Planeswalker"] = {
+        ["Price"] = 0,
+        ["LayoutOrder"] = 7500,
+        ["Description"] = "A Cliff Single Target Tower. Damage scales with distance between the Target and the Tower.",
+        ["PriceType"] = "Quest"
+    },
+    ["Firecracker"] = {
+        ["Price"] = 325,
+        ["Description"] = "A Burst Explosive Tower. Short Explosion Radius with an explosion delay and burst of firecracks, for a decent price",
+        ["PriceType"] = "Coins"
+    },
+    ["Darter"] = {
+        ["Price"] = 0,
+        ["Description"] = "A Cheap Ground Tower. The Start Tower of the game, certainly a weak one, compensated by its price.",
+        ["PriceType"] = "Coins"
+    },
+    ["Samurai"] = {
+        ["Price"] = 1850,
+        ["Description"] = "A Short Range Melee Tower. Decent Damage in short bursts for a cheap price. Good early game solution.",
+        ["PriceType"] = "Coins"
+    },
+    ["Broadcaster"] = {
+        ["Price"] = 3500,
+        ["Level"] = 20,
+        ["Description"] = "A Support Cliff Tower. Gives towers Range and Discount Buffs over long range.",
+        ["PriceType"] = "Coins"
+    },
+    ["Plantation"] = {
+        ["Price"] = 2400,
+        ["Description"] = "Generic Economy Cash-Over-Wave Tower. Nothing special.",
+        ["PriceType"] = "Coins"
+    },
+    ["Enforcement Base"] = {
+        ["Price"] = 6500,
+        ["Level"] = 30,
+        ["Description"] = "A Spawner Tower. Units Vary from Armed Enforcers to Annihilation Device Bots",
+        ["PriceType"] = "Coins"
+    },
+    ["Glowup"] = {
+        ["Price"] = 4000,
+        ["Description"] = "A Support Ground Tower. Gives towers Firerate Buffs which gradually fade with range.",
+        ["PriceType"] = "Coins"
+    },
+    ["Spark"] = {
+        ["Price"] = 5000,
+        ["Description"] = "An Incredible Tower. Deals Monstrous Burst-Chain Damage over Great Range, at the cost of Firerate and Price. Chaining has significant Damage Falloff",
+        ["PriceType"] = "Coins"
+    },
+    ["Trawler"] = {
+        ["Price"] = 3750,
+        ["Description"] = "An Economy Water Tower. Catches fish every few seconds with fluctuating prices during the beginning of the wave. Able to control Weather Forecast with the Ability",
+        ["PriceType"] = "Coins"
+    },
+    ["Penetrator"] = {
+        ["Price"] = 950,
+        ["Description"] = "A Long Range Elevated Tower. Decent damage on impact, with piercing ability and damage falloff after each pierce.",
+        ["PriceType"] = "Coins"
+    },
+    ["Quarterer"] = {
+        ["Price"] = 450,
+        ["Description"] = "A Burst Tower. Deals incredible damage over short period of time, with cooldown inbetween.",
+        ["PriceType"] = "Coins"
+    },
+    ["Ruptor"] = {
+        ["LayoutOrder"] = 3502,
+        ["Description"] = "A Burst Explosive Tower. Deals short burts finishing every with an APM Explosion, dealing massive damage.",
+        ["Price"] = 0,
+        ["Level"] = 25,
+        ["PriceType"] = "Coins"
+    },
+    ["Orbit"] = {
+        ["Price"] = 10000,
+        ["Level"] = 50,
+        ["Description"] = "A Devastating Ground Tower. Fires a beam of absolute annihilation which shreds everything on it's path. Gains a Shield and Strike Ability",
+        ["PriceType"] = "Coins"
+    },
+    ["Raider"] = {
+        ["Price"] = 6250,
+        ["Description"] = "A Ground-Air Tower. Deals Devastating Damage in Flight Waves. Gains NUKE Ability at the Last Level",
+        ["PriceType"] = "Coins"
+    },
+    ["Focus"] = {
+        ["Price"] = 2300,
+        ["Description"] = "A Fast Firerate Tower. Low damage at a medium cost with fast firerate. Dealing Double Damage in close proximity.",
+        ["PriceType"] = "Coins"
+    }
+}
