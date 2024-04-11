d16bacfc46952aba17824823fc4e6cefcc3dd307
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-11 16:17:45 +1000

    Create UnitStats.lua

---
+++ GameData/Silly Defense [13790058632]/UnitStats.lua
@@ -0,0 +1,42 @@
+data = {
+    ["APM-Bot"] = {
+        ["RocketSpeed"] = 75,
+        ["HitboxRadius"] = 1.75,
+        ["Range"] = 45,
+        ["Firerate"] = 0.1,
+        ["Functionality"] = "Default",
+        ["Health"] = 1500,
+        ["ExplosionFirerate"] = 1.5,
+        ["Speed"] = 2,
+        ["ExplosionRadius"] = 3,
+        ["ExplosionDamage"] = 75,
+        ["Damage"] = 18
+    },
+    ["Enforcer M"] = {
+        ["Firerate"] = 0.85,
+        ["Health"] = 35,
+        ["Functionality"] = "Default_StopWhenAttack",
+        ["Speed"] = 3.75,
+        ["HitboxRadius"] = 0.65,
+        ["Range"] = 2.5,
+        ["Damage"] = 35
+    },
+    ["Turret Truck"] = {
+        ["Firerate"] = 0.15,
+        ["Health"] = 250,
+        ["Functionality"] = "Default",
+        ["Speed"] = 3.5,
+        ["HitboxRadius"] = 1.75,
+        ["Range"] = 40,
+        ["Damage"] = 6
+    },
+    ["Enforcer"] = {
+        ["Firerate"] = 1,
+        ["Health"] = 15,
+        ["Functionality"] = "Default_StopWhenAttack",
+        ["Speed"] = 2.5,
+        ["HitboxRadius"] = 0.75,
+        ["Range"] = 12,
+        ["Damage"] = 6
+    }
+}
