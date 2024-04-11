f7beeececccbdb5ff619f995a82802a28b7918af
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-11 16:20:01 +1000

    Create Dialogues.lua

---
+++ GameData/Silly Defense [13790058632]/Dialogues.lua
@@ -0,0 +1,62 @@
+data = {
+    ["EasyMode"] = {
+        ["Waves"] = {
+            {
+                {
+                    ["Emotion"] = "HatHold",
+                    ["Character"] = "Mr. Silly",
+                    ["Text"] = "Oh welcome to our silly little experience, or should I say, GAME? Unfortunately, you are the one responsible for defending the base today."
+                },
+                {
+                    ["Emotion"] = "Shocked",
+                    ["Character"] = "Mr. Silly",
+                    ["Text"] = "So I guess you have no choice now other than defending Haha!"
+                }
+            },
+            {
+                {
+                    ["Emotion"] = "Shocked",
+                    ["Character"] = "Mr. Silly",
+                    ["Text"] = "Looks like quite quick enemies are coming, I mean the obvious solution is to get Stronger Towers. But if you have a better idea go ahead Haha!"
+                }
+            },
+            {
+                {
+                    ["Emotion"] = "Shocked",
+                    ["Character"] = "Mr. Silly",
+                    ["Text"] = "Uh oh. Looks like the mad scientist responsible for Acid Creatures prior is coming on the field himself!"
+                },
+                {
+                    ["Emotion"] = "Shocked",
+                    ["Character"] = "Mr. Silly",
+                    ["Text"] = "You're on your own now haha! See you in lobby with either a trophy or a skull!"
+                }
+            }
+        }
+    },
+    ["MediumMode"] = {
+        ["Waves"] = {
+            {
+                {
+                    ["Emotion"] = "HatHold",
+                    ["Character"] = "Mr. Silly",
+                    ["Text"] = "Wooo! Trying something harder today? I love such challenged individuals! Be sure that you would need a BIT better towers."
+                }
+            },
+            {
+                {
+                    ["Emotion"] = "Shocked",
+                    ["Character"] = "Mr. Silly",
+                    ["Text"] = "What can I say about this. A typical cliche at this point with quicker enemies having higher speeds but less health. You already know how to defend"
+                }
+            },
+            {
+                {
+                    ["Emotion"] = "Shocked",
+                    ["Character"] = "Mr. Silly",
+                    ["Text"] = "Wow, so far only stronger versions of Easy Mode Enemies. Doesn't it seem a bit boring? Surely they've got something new on the field."
+                }
+            }
+        }
+    }
+}
