fea5e2b5a4753ba131e9433a290c2f2bc8aee692
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-27 03:27:55 +1000

    Create Badges.lua

---
+++ GameData/Super Striker League [3360853050]/Badges.lua
@@ -0,0 +1,350 @@
+data = {
+    ["HighestBadgeId"] = 35,
+    ["Table"] = {
+        ["35"] = {
+            ["Description"] = "Block an opponent's Super Strike and prevent it from scoring.",
+            ["BadgeId"] = 2125148957,
+            ["ImageId"] = "rbxassetid://9008412888",
+            ["InGameStatId"] = "SuperStrikeDenials",
+            ["Name"] = "Super Strike Denial",
+            ["TitleId"] = "31",
+            ["CashReward"] = 1500,
+            ["InGameStatRequirement"] = 1,
+            ["InGameTask"] = true
+        },
+        ["29"] = {
+            ["InGameTask"] = true,
+            ["Name"] = "Blowout",
+            ["TitleId"] = "29",
+            ["BadgeId"] = 2125148660,
+            ["CashReward"] = 750,
+            ["ImageId"] = "rbxassetid://9008413364",
+            ["Description"] = "Win a game by 5 or more points."
+        },
+        ["1"] = {
+            ["BadgeId"] = 2125146989,
+            ["ImageId"] = "rbxassetid://9008602353",
+            ["Name"] = "Welcome to Super Striker League!",
+            ["Description"] = "Join the game for the first time."
+        },
+        ["3"] = {
+            ["CashReward"] = 250,
+            ["Name"] = "First Win",
+            ["Description"] = "Win your first game.",
+            ["BadgeId"] = 2125147126,
+            ["StatId"] = "Wins",
+            ["ImageId"] = "rbxassetid://9007850315",
+            ["StatRequirement"] = 1
+        },
+        ["2"] = {
+            ["Name"] = "It's Party Time",
+            ["TitleId"] = "37",
+            ["BadgeId"] = 2125147091,
+            ["CashReward"] = 250,
+            ["ImageId"] = "rbxassetid://9008412662",
+            ["Description"] = "Complete a game with a party of 2 or more people."
+        },
+        ["5"] = {
+            ["CashReward"] = 2500,
+            ["Description"] = "Win 100 games.",
+            ["Name"] = "Triumphant",
+            ["TitleId"] = "3",
+            ["BadgeId"] = 2125147196,
+            ["StatId"] = "Wins",
+            ["ImageId"] = "rbxassetid://9007849899",
+            ["StatRequirement"] = 100
+        },
+        ["4"] = {
+            ["CashReward"] = 500,
+            ["Description"] = "Win 10 games.",
+            ["Name"] = "Winner",
+            ["TitleId"] = "2",
+            ["BadgeId"] = 2125147172,
+            ["StatId"] = "Wins",
+            ["ImageId"] = "rbxassetid://9007850100",
+            ["StatRequirement"] = 10
+        },
+        ["7"] = {
+            ["LevelRequirement"] = 10,
+            ["Name"] = "Rookie",
+            ["TitleId"] = "5",
+            ["BadgeId"] = 2125147382,
+            ["CashReward"] = 1000,
+            ["ImageId"] = "rbxassetid://9007851645",
+            ["Description"] = "Reach player level 10."
+        },
+        ["6"] = {
+            ["CashReward"] = 10000,
+            ["Description"] = "Win 1,000 games.",
+            ["Name"] = "Victorious",
+            ["TitleId"] = "4",
+            ["BadgeId"] = 2125147231,
+            ["StatId"] = "Wins",
+            ["ImageId"] = "rbxassetid://9007849285",
+            ["StatRequirement"] = 1000
+        },
+        ["9"] = {
+            ["LevelRequirement"] = 50,
+            ["Name"] = "Experienced",
+            ["TitleId"] = "7",
+            ["BadgeId"] = 2125147442,
+            ["CashReward"] = 3000,
+            ["ImageId"] = "rbxassetid://9007851245",
+            ["Description"] = "Reach player level 50."
+        },
+        ["8"] = {
+            ["LevelRequirement"] = 25,
+            ["Name"] = "Up-and-Coming",
+            ["TitleId"] = "6",
+            ["BadgeId"] = 2125147418,
+            ["CashReward"] = 2000,
+            ["ImageId"] = "rbxassetid://9007851458",
+            ["Description"] = "Reach player level 25."
+        },
+        ["27"] = {
+            ["Description"] = "Score 3 goals in one game.",
+            ["BadgeId"] = 2125148568,
+            ["ImageId"] = "rbxassetid://9008413736",
+            ["InGameStatId"] = "Goals",
+            ["Name"] = "Hat Trick",
+            ["TitleId"] = "25",
+            ["CashReward"] = 1000,
+            ["InGameStatRequirement"] = 3,
+            ["InGameTask"] = true
+        },
+        ["17"] = {
+            ["CashReward"] = 2500,
+            ["Description"] = "Score 500 assists.",
+            ["Name"] = "Tactician",
+            ["TitleId"] = "15",
+            ["BadgeId"] = 2125147707,
+            ["StatId"] = "Assists",
+            ["ImageId"] = "rbxassetid://9007852448",
+            ["StatRequirement"] = 500
+        },
+        ["13"] = {
+            ["CashReward"] = 1000,
+            ["Description"] = "Score 50 goals.",
+            ["Name"] = "Scoring Pro",
+            ["TitleId"] = "11",
+            ["BadgeId"] = 2125147596,
+            ["StatId"] = "Goals",
+            ["ImageId"] = "rbxassetid://9007852152",
+            ["StatRequirement"] = 50
+        },
+        ["21"] = {
+            ["Description"] = "Perform a Super Strike by fully charging a shot while your Super is active.",
+            ["BadgeId"] = 2125148194,
+            ["ImageId"] = "rbxassetid://9008415234",
+            ["InGameStatId"] = "SuperStrikes",
+            ["Name"] = "Super Striker",
+            ["TitleId"] = "36",
+            ["CashReward"] = 500,
+            ["InGameStatRequirement"] = 1,
+            ["InGameTask"] = true
+        },
+        ["11"] = {
+            ["LevelRequirement"] = 250,
+            ["Name"] = "Expert Striker",
+            ["TitleId"] = "9",
+            ["BadgeId"] = 2125147531,
+            ["CashReward"] = 10000,
+            ["ImageId"] = "rbxassetid://9007850806",
+            ["Description"] = "Reach player level 250."
+        },
+        ["23"] = {
+            ["Description"] = "Perform 7 successful passes in one game.",
+            ["BadgeId"] = 2125148297,
+            ["ImageId"] = "rbxassetid://9008416510",
+            ["InGameStatId"] = "Passes",
+            ["Name"] = "Pass Player",
+            ["TitleId"] = "19",
+            ["CashReward"] = 1000,
+            ["InGameStatRequirement"] = 7,
+            ["InGameTask"] = true
+        },
+        ["34"] = {
+            ["InGameTask"] = true,
+            ["Name"] = "KABOOM!",
+            ["TitleId"] = "30",
+            ["BadgeId"] = 2125148936,
+            ["CashReward"] = 500,
+            ["ImageId"] = "rbxassetid://9008413241",
+            ["Description"] = "Blow up 3 opposing players with one Bomb explosion."
+        },
+        ["28"] = {
+            ["Description"] = "Score 3 assists in one game.",
+            ["BadgeId"] = 2125148598,
+            ["ImageId"] = "rbxassetid://9008413541",
+            ["InGameStatId"] = "Assists",
+            ["Name"] = "Helping Hand",
+            ["TitleId"] = "26",
+            ["CashReward"] = 1000,
+            ["InGameStatRequirement"] = 3,
+            ["InGameTask"] = true
+        },
+        ["33"] = {
+            ["InGameTask"] = true,
+            ["Name"] = "Walk It In",
+            ["TitleId"] = "28",
+            ["BadgeId"] = 2125148885,
+            ["CashReward"] = 1500,
+            ["ImageId"] = "rbxassetid://9008412459",
+            ["Description"] = "Score by walking straight into the net with the ball."
+        },
+        ["32"] = {
+            ["Description"] = "Score a Power-Up Goal by knocking the ball into the net with a Power-Up.",
+            ["BadgeId"] = 2125148811,
+            ["ImageId"] = "rbxassetid://9008413086",
+            ["InGameStatId"] = "PowerUpGoals",
+            ["Name"] = "Creative Scorer",
+            ["TitleId"] = "27",
+            ["CashReward"] = 1500,
+            ["InGameStatRequirement"] = 1,
+            ["InGameTask"] = true
+        },
+        ["31"] = {
+            ["InGameTask"] = true,
+            ["Name"] = "Goalkeeper Interference",
+            ["TitleId"] = "22",
+            ["BadgeId"] = 2125148737,
+            ["CashReward"] = 1000,
+            ["ImageId"] = "rbxassetid://9008414293",
+            ["Description"] = "Stun the opposing goalkeeper with a Power-Up as a teammate scores a goal."
+        },
+        ["30"] = {
+            ["InGameTask"] = true,
+            ["Name"] = "Total Dominance",
+            ["TitleId"] = "24",
+            ["BadgeId"] = 2125148702,
+            ["CashReward"] = 1500,
+            ["ImageId"] = "rbxassetid://9008413901",
+            ["Description"] = "Achieve a score of at least 1500 in one game."
+        },
+        ["19"] = {
+            ["Description"] = "Score the winning goal during Sudden Death.",
+            ["BadgeId"] = 2125147916,
+            ["ImageId"] = "rbxassetid://9008415871",
+            ["InGameStatId"] = "SuddenDeathGoals",
+            ["Name"] = "Game-Winning Goal",
+            ["TitleId"] = "17",
+            ["CashReward"] = 2000,
+            ["InGameStatRequirement"] = 1,
+            ["InGameTask"] = true
+        },
+        ["18"] = {
+            ["CashReward"] = 10000,
+            ["Description"] = "Score 5,000 assists.",
+            ["Name"] = "Ultimate Playmaker",
+            ["TitleId"] = "16",
+            ["BadgeId"] = 2125147807,
+            ["StatId"] = "Assists",
+            ["ImageId"] = "rbxassetid://9007852296",
+            ["StatRequirement"] = 5000
+        },
+        ["25"] = {
+            ["Description"] = "Get 7 Power-Up hits against players in one game.",
+            ["BadgeId"] = 2125148475,
+            ["ImageId"] = "rbxassetid://9008414509",
+            ["InGameStatId"] = "PowerUpHits",
+            ["Name"] = "Power-Up Destruction",
+            ["TitleId"] = "21",
+            ["CashReward"] = 1000,
+            ["InGameStatRequirement"] = 7,
+            ["InGameTask"] = true
+        },
+        ["15"] = {
+            ["CashReward"] = 10000,
+            ["Description"] = "Score 5,000 goals.",
+            ["Name"] = "Sharpshooter",
+            ["TitleId"] = "13",
+            ["BadgeId"] = 2125147638,
+            ["StatId"] = "Goals",
+            ["ImageId"] = "rbxassetid://9007851832",
+            ["StatRequirement"] = 5000
+        },
+        ["14"] = {
+            ["CashReward"] = 2500,
+            ["Description"] = "Score 500 goals.",
+            ["Name"] = "Hotshot",
+            ["TitleId"] = "12",
+            ["BadgeId"] = 2125147618,
+            ["StatId"] = "Goals",
+            ["ImageId"] = "rbxassetid://9007852048",
+            ["StatRequirement"] = 500
+        },
+        ["24"] = {
+            ["Description"] = "Block 3 shots in one game.",
+            ["BadgeId"] = 2125148411,
+            ["ImageId"] = "rbxassetid://9008414735",
+            ["InGameStatId"] = "BlockedShots",
+            ["Name"] = "Take One for the Team",
+            ["TitleId"] = "20",
+            ["CashReward"] = 1000,
+            ["InGameStatRequirement"] = 3,
+            ["InGameTask"] = true
+        },
+        ["16"] = {
+            ["CashReward"] = 1000,
+            ["Description"] = "Score 50 assists.",
+            ["Name"] = "Team Player",
+            ["TitleId"] = "14",
+            ["BadgeId"] = 2125147668,
+            ["StatId"] = "Assists",
+            ["ImageId"] = "rbxassetid://9007852641",
+            ["StatRequirement"] = 50
+        },
+        ["26"] = {
+            ["Description"] = "Perform 3 Super Strikes in one game.",
+            ["BadgeId"] = 2125148533,
+            ["ImageId"] = "rbxassetid://9008414121",
+            ["InGameStatId"] = "SuperStrikes",
+            ["Name"] = "Super Strike Maniac",
+            ["TitleId"] = "23",
+            ["CashReward"] = 1500,
+            ["InGameStatRequirement"] = 3,
+            ["InGameTask"] = true
+        },
+        ["20"] = {
+            ["Description"] = "Perform a Skillshot by fully charging a regular shot.",
+            ["BadgeId"] = 2125148157,
+            ["ImageId"] = "rbxassetid://9008415561",
+            ["InGameStatId"] = "Skillshots",
+            ["Name"] = "All Skill",
+            ["TitleId"] = "38",
+            ["CashReward"] = 250,
+            ["InGameStatRequirement"] = 1,
+            ["InGameTask"] = true
+        },
+        ["12"] = {
+            ["LevelRequirement"] = 1000,
+            ["Name"] = "Master of the Pitch",
+            ["TitleId"] = "10",
+            ["BadgeId"] = 2125147556,
+            ["CashReward"] = 25000,
+            ["ImageId"] = "rbxassetid://9007850568",
+            ["Description"] = "Reach player level 1000."
+        },
+        ["22"] = {
+            ["Description"] = "Perform 7 steals in one game.",
+            ["BadgeId"] = 2125148259,
+            ["ImageId"] = "rbxassetid://9008414992",
+            ["InGameStatId"] = "Steals",
+            ["Name"] = "It Takes a Thief",
+            ["TitleId"] = "18",
+            ["CashReward"] = 1000,
+            ["InGameStatRequirement"] = 7,
+            ["InGameTask"] = true
+        },
+        ["10"] = {
+            ["LevelRequirement"] = 100,
+            ["Name"] = "Big Leaguer",
+            ["TitleId"] = "8",
+            ["BadgeId"] = 2125147470,
+            ["CashReward"] = 5000,
+            ["ImageId"] = "rbxassetid://9007850976",
+            ["Description"] = "Reach player level 100."
+        }
+    },
+    ["Init"] = "function: 0x0000000077db782e"
+}
