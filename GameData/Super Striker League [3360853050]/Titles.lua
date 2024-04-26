5740303c8b66b211b119bb52ebd941c10cbcf918
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-27 03:26:47 +1000

    Create Titles.lua

---
+++ GameData/Super Striker League [3360853050]/Titles.lua
@@ -0,0 +1,76 @@
+data = {
+    ["Table"] = {
+        ["41"] = {["Name"] = "Clubs Tester"},
+        ["37"] = {["Name"] = "Party Animal"},
+        ["35"] = {
+            ["Name"] = "#{n} Highest Level Player",
+            ["TextColor3"] = Color3.fromRGB(255, 226, 0),
+            ["TextStrokeColor3"] = Color3.fromRGB(132, 75, 0),
+            ["ParameterOverrideFunction"] = "function: 0xc8901ba9c4f72acf",
+            ["ConditionFunction"] = "function: 0xc9248e9d96569d8f",
+            ["GetSubstitutionsFunction"] = "function: 0xcd29bf16b7117a6f"
+        },
+        ["39"] = {
+            ["Name"] = "#{n} Ranked Player",
+            ["TextColor3"] = Color3.fromRGB(255, 226, 0),
+            ["TextStrokeColor3"] = Color3.fromRGB(132, 75, 0),
+            ["ParameterOverrideFunction"] = "function: 0x6b76af8ad0e9433f",
+            ["ConditionFunction"] = "function: 0x8e1da9899148c82f",
+            ["GetSubstitutionsFunction"] = "function: 0x6dbd34dfb09b91bf"
+        },
+        ["29"] = {["Name"] = "Relentless"},
+        ["1"] = {["Name"] = "All-Star", ["ConditionFunction"] = "function: 0x14278362a51daeef"},
+        ["3"] = {["Name"] = "Triumphant"},
+        ["2"] = {["Name"] = "Winner"},
+        ["5"] = {["Name"] = "Rookie"},
+        ["4"] = {["Name"] = "Victorious"},
+        ["7"] = {["Name"] = "Experienced"},
+        ["6"] = {["Name"] = "Up-and-Coming"},
+        ["9"] = {["Name"] = "Expert Striker"},
+        ["8"] = {["Name"] = "Big Leaguer"},
+        ["27"] = {["Name"] = "Calculator"},
+        ["17"] = {["Name"] = "Clutch Player"},
+        ["13"] = {["Name"] = "Sharpshooter"},
+        ["21"] = {["Name"] = "Destroyer"},
+        ["11"] = {["Name"] = "Scorer"},
+        ["23"] = {["Name"] = "Electrifying"},
+        ["40"] = {["Name"] = "Ranked Tester"},
+        ["36"] = {["Name"] = "Super Striker"},
+        ["34"] = {
+            ["TextStrokeColor3"] = Color3.fromRGB(132, 75, 0),
+            ["GetSubstitutionsFunction"] = "function: 0xbbf68de5864f848f",
+            ["Name"] = "Top 10 Weekly Player x{n}",
+            ["TextColor3"] = Color3.fromRGB(255, 226, 0)
+        },
+        ["28"] = {["Name"] = "Sneaky"},
+        ["38"] = {["Name"] = "Charged Up"},
+        ["33"] = {
+            ["TextStrokeColor3"] = Color3.fromRGB(132, 75, 0),
+            ["GetSubstitutionsFunction"] = "function: 0x7e6b2af18f31567f",
+            ["Name"] = "Player of the Day x{n}",
+            ["TextColor3"] = Color3.fromRGB(255, 226, 0)
+        },
+        ["32"] = {
+            ["TextStrokeColorSequence"] = "0 0.529412 0.156863 0.156863 0 0.502496 0.596078 0.239216 0.239216 0 1 0.529412 0.156863 0.156863 0 ",
+            ["AnimationTime"] = 1.6,
+            ["Name"] = "Developer",
+            ["TextColorSequence"] = "0 1 0.298039 0.298039 0 0.505824 1 0.560784 0.560784 0 1 1 0.298039 0.298039 0 "
+        },
+        ["31"] = {["Name"] = "Savior"},
+        ["30"] = {["Name"] = "Explosive"},
+        ["19"] = {["Name"] = "Selfless"},
+        ["18"] = {["Name"] = "Thief"},
+        ["25"] = {["Name"] = "Trickster"},
+        ["15"] = {["Name"] = "Tactician"},
+        ["14"] = {["Name"] = "Team Player"},
+        ["24"] = {["Name"] = "High Scorer"},
+        ["16"] = {["Name"] = "Ultimate Playmaker"},
+        ["26"] = {["Name"] = "Helping Hand"},
+        ["20"] = {["Name"] = "Sacrificial"},
+        ["12"] = {["Name"] = "Hotshot"},
+        ["22"] = {["Name"] = "Stunning"},
+        ["10"] = {["Name"] = "Master of the Pitch"}
+    },
+    ["ApplyTitleToTextLabel"] = "function: 0x87c8082f5ee96a2f",
+    ["Init"] = "function: 0xb2f27b45c69b982f"
+}
