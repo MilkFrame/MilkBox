db5953acc8adaf25f6867af4ccbb663c666f9a3e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-29 03:23:42 +1000

    Create Currencies.lua

---
+++ GameData/Treasure Quest [2960777560]/Currencies.lua
@@ -0,0 +1,32 @@
+data = {
+    ["DailyTokens"] = {
+        ["Color"] = Color3.fromRGB(148, 99, 255),
+        ["Image"] = "rbxassetid://13408495744",
+        ["CanBePurchasedWithRobux"] = false
+    },
+    ["Gold"] = {
+        ["Color"] = Color3.fromRGB(255, 215, 0),
+        ["Image"] = "rbxassetid://10977426063",
+        ["CanBePurchasedWithRobux"] = true
+    },
+    ["QuestTokens"] = {
+        ["Color"] = Color3.fromRGB(255, 189, 134),
+        ["Image"] = "rbxassetid://7159017671",
+        ["CanBePurchasedWithRobux"] = false
+    },
+    ["GiftTokens"] = {
+        ["Color"] = Color3.fromRGB(255, 93, 204),
+        ["Image"] = "rbxassetid://11090071565",
+        ["CanBePurchasedWithRobux"] = true
+    },
+    ["HolidayCurrency"] = {
+        ["Color"] = Color3.fromRGB(130, 255, 47),
+        ["Image"] = "rbxassetid://12860733552",
+        ["CanBePurchasedWithRobux"] = true
+    },
+    ["Glory"] = {
+        ["Color"] = Color3.fromRGB(255, 188, 19),
+        ["Image"] = "rbxassetid://12736640594",
+        ["CanBePurchasedWithRobux"] = false
+    }
+}
