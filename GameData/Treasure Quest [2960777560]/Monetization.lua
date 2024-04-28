fc9ca0d98dae863cab6a1ad6bbb754400427b79a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-29 03:14:46 +1000

    Create Monetization.lua

---
+++ GameData/Treasure Quest [2960777560]/Monetization.lua
@@ -0,0 +1,206 @@
+data = {
+    ["Potions"] = {
+        {538470749, "Damage Potion", 2},
+        {538472098, "Unique Potion", 2},
+        {538471477, "Gold Potion", 2},
+        {538471137, "Health Potion", 2},
+        {538471917, "XP Potion", 2},
+        {538471656, "Luck Potion", 2}
+    },
+    ["Products"] = {
+        ["ReclaimAdventCalendarRewards"] = 1691490371,
+        ["SkipAdventCalendarQuest"] = 1691490023,
+        ["TreasurePassRestart"] = 1316152898,
+        ["Effect"] = 529344961,
+        ["BankSpace"] = 950269281,
+        ["LoadoutSpace"] = 1505681654,
+        ["StarterPack"] = 1317865664,
+        ["RefreshDailyShop"] = 1688886530,
+        ["BankSpace50"] = 1326447816,
+        ["InventorySpace"] = 499101729,
+        ["TreasurePassGold"] = 1267978007,
+        ["TreasurePassSkip"] = 1268032042,
+        ["InventorySpace50"] = 1233898116,
+        ["HolidayCurrencyRain"] = 794573312,
+        ["Revive"] = 520928377,
+        ["CosmeticChest"] = 1203634222
+    },
+    ["Gifting"] = {
+        {
+            1316712481,
+            1267978007,
+            "TreasurePassGold",
+            "Treasure Pass Gold",
+            "rbxassetid://10975249705",
+            "Treasure Pass Gold"
+        },
+        {1316712910, 752486176, "HolidayCurrency", "%s  worth of %s", 1},
+        {1316712945, 752488303, "HolidayCurrency", "%s  worth of %s", 2},
+        {1316712979, 752488829, "HolidayCurrency", "%s  worth of %s", 3},
+        {1316713010, 752489907, "HolidayCurrency", "%s  worth of %s", 4},
+        {1316713091, 752496783, "HolidayCurrency", "%s  worth of %s", 5},
+        {1316713043, 752492633, "HolidayCurrency", "%s  worth of %s", 6},
+        {1326445232, 1233898116, "InventorySpace", "50 Backpack Space", "rbxassetid://2972486600", "+50 Backpack Space"},
+        {1326445386, 1326447816, "BankSpace", "50 Bank Space", "rbxassetid://11289896564", "+50 Bank Space"},
+        {1672357213, 1505681654, "LoadoutSpace", "1 Loadout Slot", "rbxassetid://12889474376", "+1 Loadout Slot"},
+        {1673319349, 1122019901, "QuestSkips10", "10 Quest Skips", "rbxassetid://15136398759", "+10 Quest Skips"},
+        {1673322236, 1150307416, "QuestSkips50", "50 Quest Skips", "rbxassetid://15136398759", "+50 Quest Skips"},
+        {1316709955, 6131826, "Gamepass", "VIP", "rbxassetid://10976137398", "VIP"},
+        {1316710492, 6131800, "Gamepass", "DualWield", "rbxassetid://10976138215", "Dual Wield"},
+        {1316710703, 6131794, "Gamepass", "ExtraDrop", "rbxassetid://10976138014", "Extra Item Drop"},
+        {1316710567, 6682911, "Gamepass", "TrueNinja", "rbxassetid://10976137480", "True Ninja"},
+        {1316710633, 6399367, "Gamepass", "ExtraLife", "rbxassetid://10976137810", "Extra Life"},
+        {1316725112, 6432679, "Gamepass", "BackpackSlots", "rbxassetid://10976139176", "+50 Backpack Slots"},
+        {1316711204, 18659733, "Gamepass", "BackpackSlotsPlus", "rbxassetid://10976138986", "+100 Backpack Slots"},
+        {1316711279, 18659738, "Gamepass", "BackpackSlotsPlusPlus", "rbxassetid://10976138986", "+250 Backpack Slots"},
+        {1316710801, 6389964, "Gamepass", "DoubleGold", "rbxassetid://10976138760", "x2 Gold"},
+        {1316710882, 6389974, "Gamepass", "DoubleXP", "rbxassetid://10976138329", "x2 XP"},
+        {1316710950, 6682913, "Gamepass", "DoubleLuck", "rbxassetid://10976138606", "x2 Luck"},
+        {1316711423, 8086968, "Gamepass", "DoubleQuest", "rbxassetid://10976138495", "x2 Quest Rewards"},
+        {1316711029, 24698860, "Gamepass", "ExtraQuest", "rbxassetid://10976137603", "Extra Quest"},
+        {1316711499, 24786287, "Gamepass", "Customize", "rbxassetid://10976138877", "Customize"},
+        {1316712641, 526356973, "Gold", "%s  worth of Gold", "rbxassetid://10975618611", 1},
+        {1316712683, 526357153, "Gold", "%s  worth of Gold", "rbxassetid://10975618882", 2},
+        {1316712719, 526357408, "Gold", "%s  worth of Gold", "rbxassetid://10975619000", 3},
+        {1316712767, 526358032, "Gold", "%s  worth of Gold", "rbxassetid://10975618772", 4},
+        {1316712805, 526359809, "Gold", "%s  worth of Gold", "rbxassetid://10975619207", 5},
+        {1316712853, 526360152, "Gold", "%s  worth of Gold", "rbxassetid://3149743294", 6}
+    },
+    ["Packs"] = {
+        ["A"] = {
+            ["ProductID"] = 1158791925,
+            ["ExtraItems"] = {
+                {"Ability Scroll", {["Rarity"] = "Ultimate", ["Quantity"] = 3}},
+                {"Power Scroll", {["Rarity"] = "Ultimate", ["Quantity"] = 3}},
+                {"Upgrade Scroll", {["Rarity"] = "Ultimate", ["Quantity"] = 3}},
+                {"Luck Potion", {["Quantity"] = 3, ["Duration"] = 1}},
+                {"Speed Potion", {["Quantity"] = 3, ["Duration"] = 1}}
+            },
+            ["Name"] = "Ultimate",
+            ["Items"] = {"Punishment"}
+        },
+        ["C"] = {["ProductID"] = 1219043145, ["Name"] = "Evening", ["Items"] = {"Evening Blade", "Evening Staff"}},
+        ["B"] = {["ProductID"] = 1158791992, ["Name"] = "Stem", ["Items"] = {"Stemspike", "Stemwing"}}
+    },
+    ["TreasurePassSkips"] = {{1268032042, 1}, {1271481145, 5}, {1271481168, 10}, {1271481187, 20}},
+    ["FlashSales"] = {
+        ["Wooden Bow"] = {
+            ["MaxGoldPurchases"] = 1,
+            ["Gold5RobuxMultiplier"] = 10,
+            ["Item"] = "Wooden Bow",
+            ["AlwaysAvailable"] = true
+        },
+        ["Cegglestial"] = {
+            ["AppearTime"] = 1712721600,
+            ["MaxGoldPurchases"] = 1,
+            ["MaxHolidayCurrencyPurchases"] = 1,
+            ["HolidayCurrency"] = 250000,
+            ["Item"] = "Cegglestial",
+            ["ProductID"] = 1201656701,
+            ["StartTime"] = 1712980800,
+            ["AlwaysAvailable"] = false,
+            ["Duration"] = 86400
+        },
+        ["Dreggonic"] = {
+            ["AppearTime"] = 1715140800,
+            ["MaxGoldPurchases"] = 1,
+            ["MaxHolidayCurrencyPurchases"] = 1,
+            ["HolidayCurrency"] = 500000,
+            ["Item"] = "Dreggonic",
+            ["ProductID"] = 1219389342,
+            ["StartTime"] = 1715400000,
+            ["AlwaysAvailable"] = false,
+            ["Duration"] = 86400
+        },
+        ["Egglightened Bow"] = {
+            ["AppearTime"] = 1713931200,
+            ["MaxGoldPurchases"] = 1,
+            ["MaxHolidayCurrencyPurchases"] = 1,
+            ["HolidayCurrency"] = 375000,
+            ["Item"] = "Egglightened Bow",
+            ["ProductID"] = 1219389161,
+            ["StartTime"] = 1714190400,
+            ["AlwaysAvailable"] = false,
+            ["Duration"] = 86400
+        }
+    },
+    ["QuestSkips"] = {
+        {1122019067, 1},
+        {1122019184, 5},
+        {1122019901, 10},
+        {1122020283, 25},
+        {1150307416, 50},
+        {1688875153, 150}
+    },
+    ["PackOrder"] = {"A", "B", "C"},
+    ["Cosmetics"] = {
+        {959613862, "Mythical"},
+        {959613869, "Mythical"},
+        {959613887, "Legendary"},
+        {959613900, "Legendary"},
+        {959613908, "Legendary"},
+        {959613917, "Rare"},
+        {959613931, "Rare"},
+        {959613939, "Rare"}
+    },
+    ["Subscriptions"] = {
+        ["Extra Item Drop"] = "EXP-7425107362707144806",
+        ["True Quester"] = "EXP-8691319178185670777",
+        ["Ultimate Potion"] = "EXP-1884909406987747443"
+    },
+    ["PrizeWheelSpins"] = {
+        {965134569, 1},
+        {1690239132, 5},
+        {1690239480, 10},
+        {1690239740, 20},
+        {1690239875, 40},
+        {1690240118, 100}
+    },
+    ["Gold"] = {
+        {526356973, 1},
+        {526357153, 4.2},
+        {526357408, 17.6},
+        {526358032, 73.6},
+        {526359809, 307.2},
+        {526360152, 1280}
+    },
+    ["Gamepasses"] = {
+        ["DoubleGold"] = 6389964,
+        ["ExtraDrop"] = 6131794,
+        ["ExtraLife"] = 6399367,
+        ["ExtraQuest"] = 24698860,
+        ["DualWield"] = 6131800,
+        ["BackpackSlotsPlusPlus"] = 18659738,
+        ["BackpackSlotsPlus"] = 18659733,
+        ["VIP"] = 6131826,
+        ["BackpackSlots"] = 6432679,
+        ["Customize"] = 24786287,
+        ["TrueNinja"] = 6682911,
+        ["DoubleLuck"] = 6682913,
+        ["DoubleQuest"] = 8086968,
+        ["DoubleXP"] = 6389974
+    },
+    ["Scrolls"] = {
+        {974014650, "Ability Scroll", "Ultimate"},
+        {974014627, "Ability Scroll", "Mythical"},
+        {974014597, "Ability Scroll", "Legendary"},
+        {974014565, "Ability Scroll", "Rare"},
+        {974014536, "Ability Scroll", "Common"},
+        {974014372, "Power Scroll", "Ultimate", 1219872993},
+        {974014345, "Power Scroll", "Mythical"},
+        {974014285, "Power Scroll", "Legendary"},
+        {974014236, "Power Scroll", "Rare"},
+        {974013897, "Power Scroll", "Common"}
+    },
+    ["HolidayCurrency"] = {
+        {752486176, 1000},
+        {752488303, 5000},
+        {752488829, 10000},
+        {752489907, 25000},
+        {752496783, 50000},
+        {752492633, 100000},
+        {1208086835, 200000},
+        {1316206510, 400000}
+    },
+    ["BackpackSpace"] = {["BackpackSlotsPlus"] = 100, ["BackpackSlots"] = 50, ["BackpackSlotsPlusPlus"] = 250}
+}
