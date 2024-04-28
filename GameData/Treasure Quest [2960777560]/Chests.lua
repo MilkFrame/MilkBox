3c5714c5b65092ccf34cdf8243eb64665467521f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-29 03:35:36 +1000

    Create Chests.lua

---
+++ GameData/Treasure Quest [2960777560]/Chests.lua
@@ -0,0 +1,208 @@
+data = {
+    ["Lucky Stocking"] = {
+        ["Contents"] = {"Slush", "Lump of Coal", "Naughty"},
+        ["Chances"] = {["Unique"] = 10, ["Common"] = 89.9, ["Ultimate"] = 0.1}
+    },
+    ["Molten Chest"] = {
+        ["Contents"] = {
+            "Eruption",
+            "Essence",
+            "Molten Axe",
+            "Sharpfire",
+            "Torchflame",
+            "Fierce Fire",
+            "Bright Fire",
+            "Hot Sword",
+            "Sunny Staff",
+            "Staff Torch",
+            "Fireplace Poker",
+            "Hot Edge",
+            "Flame Torch",
+            "Chilli",
+            "Cherry Bomb"
+        },
+        ["Chances"] = {["Legendary"] = 8.9, ["Mythical"] = 1, ["Common"] = 55, ["Rare"] = 35, ["Ultimate"] = 0.1}
+    },
+    ["Victory Chest"] = {
+        ["Contents"] = {
+            "Victory Bow",
+            "Victory Sickle",
+            "Victory Hammer",
+            "Victory Katana",
+            "Victory Striker",
+            "Victory Axe",
+            "Victory Spinners",
+            "Victory Mace",
+            "Victory Zapper",
+            "Victory Caster",
+            "Victory Staff",
+            "Victory Summoner"
+        },
+        ["Chances"] = {["Ultimate"] = 100}
+    },
+    ["Champion Chest"] = {
+        ["Contents"] = {
+            "Champion Bow",
+            "Champion Striker",
+            "Champion Shuriken",
+            "Champion Glaive",
+            "Champion Mace",
+            "Champion Zapper",
+            "Champion Caster",
+            "Champion Orbstaff",
+            "Champion Summoner"
+        },
+        ["Chances"] = {["Ultimate"] = 100}
+    },
+    ["Wave Chest"] = {
+        ["Contents"] = {
+            "Bowmerang",
+            "Webbed Scythe",
+            "Swirly Trident",
+            "Taur Blade",
+            "Dual Guardian Blade",
+            "Orb Scepter",
+            "Plain Dagger",
+            "Zapper Blade",
+            "Amethyst Caster",
+            "Magmorb Staff",
+            "Curvy Sword",
+            "Crossblade",
+            "Toy Cutlass",
+            "Toy Staff",
+            "Antler Staff"
+        },
+        ["Chances"] = {["Legendary"] = 8.9, ["Mythical"] = 1, ["Common"] = 55, ["Rare"] = 35, ["Ultimate"] = 0.1}
+    },
+    ["+Template"] = {
+        ["Contents"] = {},
+        ["Chances"] = {["Legendary"] = 8.9, ["Mythical"] = 1, ["Common"] = 55, ["Rare"] = 35, ["Ultimate"] = 0.1}
+    },
+    ["Magic Chest"] = {
+        ["Contents"] = {
+            "Leviathan",
+            "Perception",
+            "Vind",
+            "Shatterstaff",
+            "Motley",
+            "Skydepth",
+            "Precision Axe",
+            "Aqua Blade",
+            "Captivate",
+            "Ancient Staff",
+            "Noble Slicer",
+            "Pure Slasher",
+            "Yellow Caster",
+            "Crestaff",
+            "Vile Slicer",
+            "Monochete",
+            "Harmful Orbstaff"
+        },
+        ["Chances"] = {["Legendary"] = 8.9, ["Mythical"] = 1, ["Common"] = 55, ["Rare"] = 35, ["Ultimate"] = 0.1}
+    },
+    ["Spooky Chest"] = {
+        ["Contents"] = {
+            "Boneblaze",
+            "Eerie Katana",
+            "Creepy Crawler",
+            "Death Slasher",
+            "Sinister Balloon",
+            "Toxic Vial",
+            "Crow Cage",
+            "Ancient Cup",
+            "Ancient Message",
+            "Record Player",
+            "Coping Saw",
+            "Scalpel",
+            "Ghostly Jar",
+            "Deadly Lantern",
+            "Diabolical Book",
+            "Dusk"
+        },
+        ["Chances"] = {
+            ["Legendary"] = 8.9,
+            ["Mythical"] = 1,
+            ["Unique"] = 10,
+            ["Common"] = 45,
+            ["Rare"] = 35,
+            ["Ultimate"] = 0.1
+        }
+    },
+    ["Festive Chest"] = {
+        ["Contents"] = {
+            "Wreath Splitter",
+            "Swordsmas",
+            "Wreath Caster",
+            "Staffsmas",
+            "Crescent Moon Scythe",
+            "Star Blade",
+            "Duowreath",
+            "Minty Caster",
+            "Cyber Blade",
+            "Engraved Sword",
+            "Crescent Moon Staff",
+            "Star Staff",
+            "Caged Staff",
+            "Pointy Staff",
+            "Snowy Tree",
+            "Snow Shovel",
+            "Ice Pick",
+            "Nutcracker Spear",
+            "Lantern",
+            "Peppermint Candy",
+            "Royal Trumpet",
+            "Nutcracker",
+            "Big Bag",
+            "Drum Set",
+            "Celebration Cracker",
+            "Festive Lights",
+            "Minty Firework",
+            "Big Mitten",
+            "Warm Tea",
+            "Plate of Cookies",
+            "Milk Carton",
+            "Reindeer Envelope"
+        },
+        ["Chances"] = {["Legendary"] = 8.9, ["Mythical"] = 1, ["Common"] = 55, ["Rare"] = 35, ["Ultimate"] = 0.1}
+    },
+    ["Daring Chest"] = {
+        ["Contents"] = {
+            "Wisdom",
+            "Tradition",
+            "Pipa",
+            "Balanced Kama",
+            "Fan of Balance",
+            "Unsealed Scroll",
+            "Wooden Katana",
+            "Bamboo Sword",
+            "Kanabo",
+            "Brush",
+            "Red Nunchucks",
+            "Chopsticks",
+            "Baton",
+            "Gourd Bottle",
+            "Bamboo Wand"
+        },
+        ["Chances"] = {["Legendary"] = 8.9, ["Mythical"] = 1, ["Common"] = 55, ["Rare"] = 35, ["Ultimate"] = 0.1}
+    },
+    ["Aquatic Chest"] = {
+        ["Contents"] = {
+            "Floodra",
+            "Seaweed Slicer",
+            "Floodcast",
+            "Jellyfisher",
+            "Fishing Rod",
+            "Canister",
+            "Naval Mine",
+            "Lifeguard Float",
+            "Water Vase",
+            "Bottled Message",
+            "Spyglass",
+            "Paddle",
+            "Clam Shell",
+            "Starfish",
+            "Ship Wheel"
+        },
+        ["Chances"] = {["Legendary"] = 8.9, ["Mythical"] = 1, ["Common"] = 55, ["Rare"] = 35, ["Ultimate"] = 0.1}
+    }
+}
