bf26b44aedee6396b2135476119ec89434dd9629
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-29 03:23:55 +1000

    Create PromosLibrary.lua

---
+++ GameData/Treasure Quest [2960777560]/PromosLibrary.lua
@@ -0,0 +1,38 @@
+data = {
+    ["Order"] = {
+        {
+            ["AssetId"] = 7243871815,
+            ["Name"] = "Series 11 Mystery Figure",
+            ["AssetIcon"] = "rbxassetid://9611947050",
+            ["Rewards"] = {{["Name"] = "Wildfire", ["ItemInfo"] = {["Level"] = 1}}},
+            ["Description"] = "Redeem a Treasure Quest Series 11 Mystery Figure Toy Code to claim this reward!",
+            ["AssetName"] = "Hat of Wizardry"
+        },
+        {
+            ["AssetId"] = 8664418297,
+            ["Name"] = "Series 12 Mystery Figure",
+            ["AssetIcon"] = "rbxassetid://10975856530",
+            ["Rewards"] = {{["Name"] = "Apocalypse", ["ItemInfo"] = {["Level"] = 1}}},
+            ["Description"] = "Redeem a Treasure Quest Series 12 Mystery Figure Toy Code to claim this reward!",
+            ["AssetName"] = "Obsidian Headband"
+        }
+    },
+    ["Promos"] = {
+        ["Series 11 Mystery Figure"] = {
+            ["AssetId"] = 7243871815,
+            ["Name"] = "Series 11 Mystery Figure",
+            ["AssetIcon"] = "rbxassetid://9611947050",
+            ["Rewards"] = {{["Name"] = "Wildfire", ["ItemInfo"] = {["Level"] = 1}}},
+            ["Description"] = "Redeem a Treasure Quest Series 11 Mystery Figure Toy Code to claim this reward!",
+            ["AssetName"] = "Hat of Wizardry"
+        },
+        ["Series 12 Mystery Figure"] = {
+            ["AssetId"] = 8664418297,
+            ["Name"] = "Series 12 Mystery Figure",
+            ["AssetIcon"] = "rbxassetid://10975856530",
+            ["Rewards"] = {{["Name"] = "Apocalypse", ["ItemInfo"] = {["Level"] = 1}}},
+            ["Description"] = "Redeem a Treasure Quest Series 12 Mystery Figure Toy Code to claim this reward!",
+            ["AssetName"] = "Obsidian Headband"
+        }
+    }
+}
