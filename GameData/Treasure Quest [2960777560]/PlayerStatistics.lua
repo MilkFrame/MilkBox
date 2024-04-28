3e42185d145b65859f6a051d00b3d50ba3958d84
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-29 03:15:01 +1000

    Create PlayerStatistics.lua

---
+++ GameData/Treasure Quest [2960777560]/PlayerStatistics.lua
@@ -0,0 +1,426 @@
+data = {
+    ["Order"] = {
+        "Stats Time Played",
+        "Stats Deaths",
+        "Stats Monster Kills",
+        "Stats Miniboss Kills",
+        "Stats Boss Kills",
+        "Stats Elite Boss Kills",
+        "Stats Damage Dealt",
+        "Stats Healing Given",
+        "Stats Healing Received",
+        "Stats Medkits Used",
+        "Stats Chests Opened",
+        "Stats Quests Completed",
+        "Stats Items Bought",
+        "Stats Items Crafted",
+        "Stats Items Sold",
+        "Stats Sell Profits",
+        "Stats Prize Wheels",
+        "Stats Daily Rewards",
+        "Stats Real Daily Rewards",
+        "Stats Upgrades",
+        "Stats Found Common",
+        "Stats Found Rare",
+        "Stats Found Legendary",
+        "Stats Found Mythical",
+        "Stats Found Ultimate",
+        "Stats Pass Completions",
+        "Stats Pass Quests Completed",
+        "Stats Pass Rewards Claimed",
+        "Stats Pass Points Earned",
+        "Stats Waves Completed",
+        "Stats Highest Wave Completed",
+        "Stats Glory Earned",
+        "Stats Weekly Top Player",
+        "Stats Weekly #1 Player",
+        "Stats Daily Tokens Earned",
+        "Stats Summons",
+        "Stats Daily Dungeon Easy",
+        "Stats Daily Dungeon Hard",
+        "Stats Daily Dungeon Impossible",
+        "Stats Dungeon Ancient Jungle",
+        "Stats Dungeon Coral Kingdom",
+        "Stats Dungeon Crystal Cave",
+        "Stats Dungeon Sacred Sands",
+        "Stats Dungeon Candy Land",
+        "Stats Dungeon Sinister Sewers",
+        "Stats Dungeon Magical Mushrooms",
+        "Stats Dungeon Space Base",
+        "Stats Dungeon Daring Dojo",
+        "Stats Dungeon Cryptic Castle",
+        "Stats Dungeon Santa's Workshop",
+        "Stats Dungeon Ice Palace",
+        "Stats Dungeon Frenzy Factory",
+        "Stats Dungeon Fiery Fortress",
+        "Stats Dungeon Danger Depths",
+        "Stats Dungeon Poison Prison",
+        "Stats Dungeon Smog Sanctum",
+        "Stats Dungeon Missing Map",
+        "Stats Dungeon Brightest Beach",
+        "Stats Dungeon Spookiest Mansion",
+        "Stats Dungeon Flower Field"
+    },
+    ["Info"] = {
+        ["Stats Dungeon Ice Palace"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Ice Palace",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Ice Palace Clears"
+        },
+        ["Stats Dungeon Magical Mushrooms"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Magical Mushrooms",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Magical Mushrooms Clears"
+        },
+        ["Stats Pass Quests Completed"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Pass Quests Completed",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Pass Quests Completed"
+        },
+        ["Stats Glory Earned"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Glory Earned",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Glory Earned"
+        },
+        ["Stats Dungeon Sacred Sands"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Sacred Sands",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Sacred Sands Clears"
+        },
+        ["Stats Daily Dungeon Hard"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Daily Dungeon Hard",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Hard Daily Dungeons"
+        },
+        ["Stats Daily Dungeon Easy"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Daily Dungeon Easy",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Easy Daily Dungeons"
+        },
+        ["Stats Real Daily Rewards"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Real Daily Rewards",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Daily Rewards"
+        },
+        ["Stats Pass Rewards Claimed"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Pass Rewards Claimed",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Pass Rewards Claimed"
+        },
+        ["Stats Time Played"] = {
+            ["ValueTostring"] = "function: 0xce9362cd2eaf8508",
+            ["Name"] = "Stats Time Played",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Time Played"
+        },
+        ["Stats Daily Tokens Earned"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Daily Tokens Earned",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Daily Tokens Earned"
+        },
+        ["Stats Dungeon Space Base"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Space Base",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Space Base Clears"
+        },
+        ["Stats Sell Profits"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Sell Profits",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Sell Profits"
+        },
+        ["Stats Weekly Top Player"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Weekly Top Player",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Weekly Competition Wins"
+        },
+        ["Stats Healing Received"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Healing Received",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Healing Received"
+        },
+        ["Stats Dungeon Brightest Beach"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Brightest Beach",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Brightest Beach Clears"
+        },
+        ["Stats Dungeon Crystal Cave"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Crystal Cave",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Crystal Cave Clears"
+        },
+        ["Stats Items Bought"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Items Bought",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Items Bought"
+        },
+        ["Stats Quests Completed"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Quests Completed",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Quests Completed"
+        },
+        ["Stats Medkits Used"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Medkits Used",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Medkits Used"
+        },
+        ["Stats Chests Opened"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Chests Opened",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Treasure Chests Opened"
+        },
+        ["Stats Upgrades"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Upgrades",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Upgrades"
+        },
+        ["Stats Waves Completed"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Waves Completed",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Waves Completed"
+        },
+        ["Stats Prize Wheels"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Prize Wheels",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Prize Wheels"
+        },
+        ["Stats Daily Rewards"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Daily Rewards",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Treasure Rewards"
+        },
+        ["Stats Dungeon Ancient Jungle"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Ancient Jungle",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Ancient Jungle Clears"
+        },
+        ["Stats Boss Kills"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Boss Kills",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Bosses Defeated"
+        },
+        ["Stats Dungeon Missing Map"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Missing Map",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Missing Map Clears"
+        },
+        ["Stats Dungeon Smog Sanctum"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Smog Sanctum",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Smog Sanctum Clears"
+        },
+        ["Stats Dungeon Poison Prison"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Poison Prison",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Poison Prison Clears"
+        },
+        ["Stats Damage Dealt"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Damage Dealt",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Damage Dealt"
+        },
+        ["Stats Dungeon Danger Depths"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Danger Depths",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Danger Depths Clears"
+        },
+        ["Stats Found Legendary"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Found Legendary",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Legendaries Found"
+        },
+        ["Stats Weekly #1 Player"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Weekly #1 Player",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "#1 Weekly Competitions"
+        },
+        ["Stats Dungeon Fiery Fortress"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Fiery Fortress",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Fiery Fortress Clears"
+        },
+        ["Stats Items Crafted"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Items Crafted",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Items Crafted"
+        },
+        ["Stats Dungeon Frenzy Factory"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Frenzy Factory",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Frenzy Factory Clears"
+        },
+        ["Stats Found Common"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Found Common",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Commons Found"
+        },
+        ["Stats Dungeon Santa's Workshop"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Santa's Workshop",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Santa's Workshop Clears"
+        },
+        ["Stats Pass Completions"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Pass Completions",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Treasure Passes Completed"
+        },
+        ["Stats Summons"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Summons",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Summons"
+        },
+        ["Stats Highest Wave Completed"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Highest Wave Completed",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Highest Wave Completed"
+        },
+        ["Stats Dungeon Cryptic Castle"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Cryptic Castle",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Cryptic Castle Clears"
+        },
+        ["Stats Elite Boss Kills"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Elite Boss Kills",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Elite Bosses Defeated"
+        },
+        ["Stats Found Rare"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Found Rare",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Rares Found"
+        },
+        ["Stats Monster Kills"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Monster Kills",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Monsters Defeated"
+        },
+        ["Stats Dungeon Spookiest Mansion"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Spookiest Mansion",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Spookiest Mansion Clears"
+        },
+        ["Stats Dungeon Sinister Sewers"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Sinister Sewers",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Sinister Sewers Clears"
+        },
+        ["Stats Items Sold"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Items Sold",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Items Sold"
+        },
+        ["Stats Pass Points Earned"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Pass Points Earned",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Pass Points Earned"
+        },
+        ["Stats Found Mythical"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Found Mythical",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Mythicals Found"
+        },
+        ["Stats Dungeon Coral Kingdom"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Coral Kingdom",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Coral Kingdom Clears"
+        },
+        ["Stats Dungeon Candy Land"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Candy Land",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Candy Land Clears"
+        },
+        ["Stats Found Ultimate"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Found Ultimate",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Ultimates Found"
+        },
+        ["Stats Daily Dungeon Impossible"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Daily Dungeon Impossible",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Impossible Daily Dungeons"
+        },
+        ["Stats Healing Given"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Healing Given",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Healing Given"
+        },
+        ["Stats Miniboss Kills"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Miniboss Kills",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Minibosses Defeated"
+        },
+        ["Stats Dungeon Daring Dojo"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Daring Dojo",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Daring Dojo Clears"
+        },
+        ["Stats Dungeon Flower Field"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Dungeon Flower Field",
+            ["OnlyShowAboveZero"] = true,
+            ["DisplayName"] = "Flower Field Clears"
+        },
+        ["Stats Deaths"] = {
+            ["ValueTostring"] = "function: 0xe86420de744bd638",
+            ["Name"] = "Stats Deaths",
+            ["OnlyShowAboveZero"] = false,
+            ["DisplayName"] = "Deaths"
+        }
+    }
+}
