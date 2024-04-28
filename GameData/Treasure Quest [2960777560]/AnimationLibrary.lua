9b677b2b0a797ea77ec832cfecc90cc75718d180
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-29 03:13:53 +1000

    Create AnimationLibrary.lua

---
+++ GameData/Treasure Quest [2960777560]/AnimationLibrary.lua
@@ -0,0 +1,241 @@
+data = {
+    ["Idle"] = {
+        ["Reaper"] = {
+            ["Reference"] = "Cryo Sickle",
+            ["Default"] = "rbxassetid://11713200279",
+            ["DualWield"] = "rbxassetid://11713233659"
+        },
+        ["Sword"] = {
+            ["Reference"] = "Wooden Sword",
+            ["Default"] = "rbxassetid://2964789432",
+            ["DualWield"] = "rbxassetid://2964789432"
+        },
+        ["Eternal"] = {
+            ["Reference"] = "Eternal",
+            ["Default"] = "rbxassetid://3135627601",
+            ["DualWield"] = "rbxassetid://3135627601"
+        },
+        ["Dagger"] = {["Reference"] = "Sai", ["DualWield"] = "rbxassetid://3138235468"},
+        ["SwordScythe"] = {
+            ["DualWield"] = "rbxassetid://2967374250",
+            ["Reference"] = "Red Energy Blade",
+            ["Default"] = "rbxassetid://2964789432",
+            ["DisplayName"] = "Energy Sword"
+        },
+        ["LargeBow"] = {
+            ["DualWield"] = "rbxassetid://12133119022",
+            ["Reference"] = "Omega Bow",
+            ["Default"] = "rbxassetid://12133119022",
+            ["DisplayName"] = "Large Bow"
+        },
+        ["Bow"] = {
+            ["Reference"] = "Wooden Bow",
+            ["Default"] = "rbxassetid://12122342983",
+            ["DualWield"] = "rbxassetid://12122342983"
+        },
+        ["Lance"] = {
+            ["Reference"] = "Golden Lance",
+            ["Default"] = "rbxassetid://4634643286",
+            ["DualWield"] = "rbxassetid://4634761391"
+        },
+        ["SwordModified"] = {
+            ["DualWield"] = "rbxassetid://6909338099",
+            ["Reference"] = "Wooden Sword",
+            ["Default"] = "rbxassetid://6909338099",
+            ["DisplayName"] = "Sword Modified"
+        },
+        ["Balloon"] = {
+            ["Reference"] = "Balloon",
+            ["Default"] = "rbxassetid://3307907939",
+            ["DualWield"] = "rbxassetid://3307916660"
+        },
+        ["Katana"] = {
+            ["Reference"] = "Katana",
+            ["Default"] = "rbxassetid://2964789432",
+            ["DualWield"] = "rbxassetid://2967374250"
+        },
+        ["Cannon"] = {
+            ["Reference"] = "Death Cannon",
+            ["Default"] = "rbxassetid://6653141613",
+            ["DualWield"] = "rbxassetid://6655994130"
+        },
+        ["Scythe"] = {
+            ["Reference"] = "Alpha",
+            ["Default"] = "rbxassetid://2964817561",
+            ["DualWield"] = "rbxassetid://2967374250"
+        },
+        ["Hammer"] = {
+            ["Reference"] = "Ban Hammer",
+            ["Default"] = "rbxassetid://2964817561",
+            ["DualWield"] = "rbxassetid://2967374250"
+        },
+        ["Launcher"] = {
+            ["Reference"] = "Influencer Egg Launcher",
+            ["Default"] = "rbxassetid://4850868963",
+            ["DualWield"] = "rbxassetid://4850874467"
+        },
+        ["DualSpin"] = {
+            ["DualWield"] = "rbxassetid://6520269235",
+            ["Reference"] = "Wisdom",
+            ["Default"] = "rbxassetid://4748824870",
+            ["DisplayName"] = "Dual Spin"
+        },
+        ["Boulder"] = {
+            ["Reference"] = "Boulder",
+            ["Default"] = "rbxassetid://3145002007",
+            ["DualWield"] = "rbxassetid://3169191396"
+        },
+        ["Dragging"] = {
+            ["Reference"] = "Grimitar",
+            ["Default"] = "rbxassetid://4748670286",
+            ["DualWield"] = "rbxassetid://4748714528"
+        },
+        ["DualScythe"] = {
+            ["DualWield"] = "rbxassetid://4459726323",
+            ["Reference"] = "Wintertide",
+            ["Default"] = "rbxassetid://4459688396",
+            ["DisplayName"] = "Dual Scythe"
+        },
+        ["AnimeKatana"] = {
+            ["DualWield"] = "rbxassetid://3260045692",
+            ["Reference"] = "Katana",
+            ["Default"] = "rbxassetid://2964789432",
+            ["DisplayName"] = "Better Katana"
+        },
+        ["None"] = {["Reference"] = "Wooden Sword"}
+    },
+    ["Attack"] = {
+        ["DualTitan"] = {
+            ["DualWield"] = {"rbxassetid://4748882428"},
+            ["Reference"] = "Corrupter",
+            ["Default"] = {"rbxassetid://4748879528", "rbxassetid://4748871679", "rbxassetid://4748876916"},
+            ["DisplayName"] = "Dual Titan"
+        },
+        ["SpinSword"] = {
+            ["DualWield"] = {"rbxassetid://2967465279", "rbxassetid://2967465279", "rbxassetid://3073682700"},
+            ["Reference"] = "Wooden Sword",
+            ["Default"] = {"rbxassetid://3073682700"},
+            ["DisplayName"] = "Sword Spin"
+        },
+        ["LargeBow"] = {
+            ["DualWield"] = {"rbxassetid://12133088269"},
+            ["Reference"] = "Omega Bow",
+            ["Default"] = {"rbxassetid://12133088269"},
+            ["DisplayName"] = "Large Bow"
+        },
+        ["Sword"] = {
+            ["Reference"] = "Wooden Sword",
+            ["Default"] = {"rbxassetid://2963581776", "rbxassetid://2972801568", "rbxassetid://3073682700"},
+            ["DualWield"] = {"rbxassetid://2972801568", "rbxassetid://3073802882", "rbxassetid://2967465279"}
+        },
+        ["Eternal"] = {
+            ["Reference"] = "Eternal",
+            ["Default"] = {"rbxassetid://3135637376", "rbxassetid://3135687188", "rbxassetid://3135667666"},
+            ["DualWield"] = {
+                "rbxassetid://3135637376",
+                "rbxassetid://3135642140",
+                "rbxassetid://3135687188",
+                "rbxassetid://3135667666"
+            }
+        },
+        ["Spear"] = {
+            ["Reference"] = "Naginata",
+            ["Default"] = {"rbxassetid://2972801568", "rbxassetid://3073754807", "rbxassetid://3073754807"},
+            ["DualWield"] = {"rbxassetid://2972801568", "rbxassetid://3073802882", "rbxassetid://2967465279"}
+        },
+        ["Cast"] = {
+            ["Reference"] = "Wooden Wand",
+            ["Default"] = {"rbxassetid://2972801568"},
+            ["DualWield"] = {"rbxassetid://2967465279"}
+        },
+        ["Bow"] = {
+            ["Reference"] = "Wooden Bow",
+            ["Default"] = {"rbxassetid://12131744037"},
+            ["DualWield"] = {"rbxassetid://12131744037"}
+        },
+        ["Gauntlet"] = {
+            ["Reference"] = "Gingerclaw",
+            ["Default"] = {"rbxassetid://7943990551", "rbxassetid://2972801568", "rbxassetid://7943990551"},
+            ["DualWield"] = {"rbxassetid://7943990551", "rbxassetid://7944071063", "rbxassetid://7944118431"}
+        },
+        ["CastModified"] = {
+            ["Reference"] = "Table Tennis Paddle",
+            ["Default"] = {"rbxassetid://13603099530"},
+            ["DualWield"] = {"rbxassetid://13603120217"}
+        },
+        ["Lance"] = {
+            ["Reference"] = "Golden Lance",
+            ["Default"] = {"rbxassetid://4634689055"},
+            ["DualWield"] = {"rbxassetid://4634756826"}
+        },
+        ["SwordBackwards"] = {
+            ["DualWield"] = {"rbxassetid://4748743094"},
+            ["Reference"] = "Wooden Sword",
+            ["Default"] = {"rbxassetid://4748695989", "rbxassetid://4748700226", "rbxassetid://4748702664"},
+            ["DisplayName"] = "Sword Backwards"
+        },
+        ["EnergySword"] = {
+            ["DualWield"] = {"rbxassetid://2972801568", "rbxassetid://3073802882", "rbxassetid://2967465279"},
+            ["Reference"] = "Wildfire",
+            ["Default"] = {"rbxassetid://2972801568", "rbxassetid://9743732918", "rbxassetid://9743732918"},
+            ["DisplayName"] = "Energy Sword"
+        },
+        ["Katana"] = {
+            ["Reference"] = "Katana",
+            ["Default"] = {"rbxassetid://3244372962", "rbxassetid://3244392723"},
+            ["DualWield"] = {"rbxassetid://3244449294", "rbxassetid://3244496438", "rbxassetid://2967465279"}
+        },
+        ["Zap"] = {
+            ["Reference"] = "Aqua Zapper",
+            ["Default"] = {"rbxassetid://3073754807"},
+            ["DualWield"] = {"rbxassetid://3073754807"}
+        },
+        ["Launcher"] = {
+            ["Reference"] = "Influencer Egg Launcher",
+            ["Default"] = {"rbxassetid://4859367595"},
+            ["DualWield"] = {"rbxassetid://4859379342"}
+        },
+        ["Hammer"] = {
+            ["Reference"] = "Ban Hammer",
+            ["Default"] = {"rbxassetid://2976003518"},
+            ["DualWield"] = {"rbxassetid://2976033363"}
+        },
+        ["Shuriken"] = {
+            ["Reference"] = "Wisdom",
+            ["Default"] = {"rbxassetid://5139001611"},
+            ["DualWield"] = {"rbxassetid://7219869849"}
+        },
+        ["Cannon"] = {
+            ["Reference"] = "Death Cannon",
+            ["Default"] = {"rbxassetid://6655643878", "rbxassetid://6655653541", "rbxassetid://6685001690"},
+            ["DualWield"] = {"rbxassetid://6656033473", "rbxassetid://2967465279"}
+        },
+        ["Boulder"] = {
+            ["Reference"] = "Boulder",
+            ["Default"] = {"rbxassetid://3145024443"},
+            ["DualWield"] = {"rbxassetid://3145024443"}
+        },
+        ["Scythe"] = {
+            ["Reference"] = "Ocean Scythe",
+            ["Default"] = {"rbxassetid://2963581776", "rbxassetid://2972801568", "rbxassetid://3073682700"},
+            ["DualWield"] = {"rbxassetid://2967521340", "rbxassetid://2967521340", "rbxassetid://2967465279"}
+        },
+        ["Handcast"] = {
+            ["DualWield"] = {"rbxassetid://7221101784", "rbxassetid://7221113660"},
+            ["Reference"] = "Perception",
+            ["Default"] = {"rbxassetid://7221101784", "rbxassetid://7221113660"},
+            ["DisplayName"] = "Hand Cast"
+        },
+        ["DualScythe"] = {
+            ["DualWield"] = {"rbxassetid://4459704975"},
+            ["Reference"] = "Wintertide",
+            ["Default"] = {"rbxassetid://4459704975"},
+            ["DisplayName"] = "Dual Scythe"
+        },
+        ["Handsaw"] = {
+            ["Reference"] = "Handsaw",
+            ["Default"] = {"rbxassetid://3245184681", "rbxassetid://3245184681", "rbxassetid://3244372962"},
+            ["DualWield"] = {"rbxassetid://3245212081", "rbxassetid://3245212081", "rbxassetid://2967465279"}
+        }
+    }
+}
