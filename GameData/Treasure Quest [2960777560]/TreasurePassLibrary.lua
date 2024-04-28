949417ee92268441b5d65552c528e012eb379be5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-29 03:24:11 +1000

    Create TreasurePassLibrary.lua

---
+++ GameData/Treasure Quest [2960777560]/TreasurePassLibrary.lua
@@ -0,0 +1,368 @@
+data = {
+    ["QuestOrder"] = {
+        "Peppermint Miniboss Kills",
+        "Blaze Miniboss Kills",
+        "Crystal Cave Dungeon",
+        "Medkits",
+        "Frost Miniboss Kills",
+        "Food Cart",
+        "Miniboss Kills",
+        "Playtime",
+        "Sinister Sewers Dungeon",
+        "Complete Quests",
+        "Monster Kills",
+        "Use Potions",
+        "Boss Kills",
+        "Ability Scroll",
+        "Greenback Miniboss Kills",
+        "Wave Defense Waves",
+        "Teamwork",
+        "Wave Defense Bosses",
+        "Candy Land Dungeon Monster Kills",
+        "Melee Monster Kills",
+        "Coral Kingdom Dungeon Monster Kills",
+        "Rename Item",
+        "Flawless",
+        "Voltage Miniboss Kills",
+        "Sinister Sewers Dungeon Monster Kills",
+        "Obsidian Miniboss Kills",
+        "Horseman Miniboss Kills",
+        "Level Up",
+        "Seller",
+        "Ancient Jungle Dungeon Monster Kills",
+        "Shopper",
+        "Sacred Sands Dungeon Monster Kills",
+        "Star Guardian Miniboss Kills",
+        "Charity",
+        "Miniboss Master",
+        "Elite Boss Kills",
+        "Blacksmith",
+        "Ranged Monster Kills",
+        "Coral Kingdom Dungeon",
+        "Witch Miniboss Kills",
+        "Mission Impossible",
+        "Prize Wheel"
+    },
+    ["QuestsPerWeek"] = 6,
+    ["DailyQuestOrder"] = {"Daily Boss Kills", "Daily Monster Kills"},
+    ["NumRewards"] = 46,
+    ["Duration"] = 4838400,
+    ["NumWeeks"] = 7,
+    ["Rewards"] = {
+        {
+            ["Free"] = {["Name"] = "Gold Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}},
+            ["Gold"] = {
+                ["Name"] = "Sporewand",
+                ["ItemInfo"] = {
+                    ["EvolutionQuest"] = {
+                        ["Goal"] = 5,
+                        ["Name"] = "Complete Quests",
+                        ["Reward"] = {
+                            "Sporecast",
+                            {
+                                ["EvolutionQuest"] = {
+                                    ["Goal"] = 25,
+                                    ["Name"] = "Complete Quests",
+                                    ["Reward"] = {
+                                        "Sporestaff",
+                                        {
+                                            ["EvolutionQuest"] = {
+                                                ["Goal"] = 100,
+                                                ["Name"] = "Complete Quests",
+                                                ["Reward"] = {
+                                                    "Sporesummon",
+                                                    {
+                                                        ["EvolutionQuest"] = {
+                                                            ["Goal"] = 250,
+                                                            ["Name"] = "Complete Quests",
+                                                            ["Reward"] = {"Bioluminescence", {["Level"] = 1}}
+                                                        },
+                                                        ["Level"] = 1
+                                                    }
+                                                }
+                                            },
+                                            ["Level"] = 1
+                                        }
+                                    }
+                                },
+                                ["Level"] = 1
+                            }
+                        }
+                    },
+                    ["Level"] = 1
+                }
+            }
+        },
+        {["Gold"] = {["Name"] = "XP Perk 1", ["ItemInfo"] = {["Level"] = 1}}},
+        {
+            ["Free"] = {["Name"] = "Storage", ["ItemInfo"] = {["Level"] = 1, ["Quantity"] = 5}},
+            ["Gold"] = {["Name"] = "Gold Perk 1", ["ItemInfo"] = {["Level"] = 1}}
+        },
+        {["Gold"] = {["Name"] = "Luck Perk 1", ["ItemInfo"] = {["Level"] = 1}}},
+        {
+            ["Free"] = {["Name"] = "Speed Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}},
+            ["Gold"] = {["Name"] = "Discount Perk 1", ["ItemInfo"] = {["Level"] = 1}}
+        },
+        {["Gold"] = {["Name"] = "Speed Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}}},
+        {
+            ["Free"] = {["Name"] = "QuestTokens", ["ItemInfo"] = {["Level"] = 1, ["Quantity"] = 25}},
+            ["Gold"] = {["Name"] = "Storage", ["ItemInfo"] = {["Level"] = 1, ["Quantity"] = 5}}
+        },
+        {["Gold"] = {["Name"] = "Power Scroll", ["ItemInfo"] = {["Rarity"] = "Ultimate", ["Level"] = 1}}},
+        {
+            ["Free"] = {["Name"] = "XP Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}},
+            ["Gold"] = {["Name"] = "Jump Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}}
+        },
+        {["Gold"] = {["Name"] = "Luck Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}}},
+        {
+            ["Free"] = {["Name"] = "Ability Scroll", ["ItemInfo"] = {["Rarity"] = "Ultimate", ["Level"] = 1}},
+            ["Gold"] = {["Name"] = "Upgrade Scroll", ["ItemInfo"] = {["Rarity"] = "Ultimate", ["Level"] = 1}}
+        },
+        {["Gold"] = {["Name"] = "Level", ["ItemInfo"] = {["Level"] = 1, ["Quantity"] = 1}}},
+        {
+            ["Free"] = {["Name"] = "Luck Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}},
+            ["Gold"] = {["Name"] = "Effect Chest", ["ItemInfo"] = {["Level"] = 1}}
+        },
+        {["Gold"] = {["Name"] = "Cosmetic Chest", ["ItemInfo"] = {["Level"] = 1}}},
+        {
+            ["Free"] = {["Name"] = "Nametag", ["ItemInfo"] = {["Level"] = 1}},
+            ["Gold"] = {["Name"] = "XP Perk 2", ["ItemInfo"] = {["Level"] = 1}}
+        },
+        {["Gold"] = {["Name"] = "Gold Perk 2", ["ItemInfo"] = {["Level"] = 1}}},
+        {
+            ["Free"] = {["Name"] = "Level", ["ItemInfo"] = {["Level"] = 1, ["Quantity"] = 1}},
+            ["Gold"] = {["Name"] = "Luck Perk 2", ["ItemInfo"] = {["Level"] = 1}}
+        },
+        {["Gold"] = {["Name"] = "Discount Perk 2", ["ItemInfo"] = {["Level"] = 1}}},
+        {
+            ["Free"] = {["Name"] = "Effect Chest", ["ItemInfo"] = {["Level"] = 1}},
+            ["Gold"] = {["Name"] = "XP Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}}
+        },
+        {["Gold"] = {["Name"] = "Luck Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}}},
+        {
+            ["Free"] = {["Name"] = "Unique Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}},
+            ["Gold"] = {["Name"] = "Gold Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}}
+        },
+        {["Gold"] = {["Name"] = "Power Scroll", ["ItemInfo"] = {["Rarity"] = "Ultimate", ["Level"] = 1}}},
+        {
+            ["Free"] = {["Name"] = "QuestTokens", ["ItemInfo"] = {["Level"] = 1, ["Quantity"] = 25}},
+            ["Gold"] = {["Name"] = "Power Scroll", ["ItemInfo"] = {["Rarity"] = "Ultimate", ["Level"] = 1}}
+        },
+        {["Gold"] = {["Name"] = "Power Scroll", ["ItemInfo"] = {["Rarity"] = "Ultimate", ["Level"] = 1}}},
+        {
+            ["Free"] = {["Name"] = "Speed Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}},
+            ["Gold"] = {["Name"] = "Storage", ["ItemInfo"] = {["Level"] = 1, ["Quantity"] = 5}}
+        },
+        {["Gold"] = {["Name"] = "Nametag", ["ItemInfo"] = {["Level"] = 1}}},
+        {
+            ["Free"] = {["Name"] = "Jump Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}},
+            ["Gold"] = {["Name"] = "Speed Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}}
+        },
+        {["Gold"] = {["Name"] = "XP Perk 3", ["ItemInfo"] = {["Level"] = 1}}},
+        {
+            ["Free"] = {["Name"] = "BankStorage", ["ItemInfo"] = {["Level"] = 1, ["Quantity"] = 10}},
+            ["Gold"] = {["Name"] = "Gold Perk 3", ["ItemInfo"] = {["Level"] = 1}}
+        },
+        {["Gold"] = {["Name"] = "Luck Perk 3", ["ItemInfo"] = {["Level"] = 1}}},
+        {
+            ["Free"] = {["Name"] = "Storage", ["ItemInfo"] = {["Level"] = 1, ["Quantity"] = 5}},
+            ["Gold"] = {["Name"] = "Discount Perk 3", ["ItemInfo"] = {["Level"] = 1}}
+        },
+        {["Gold"] = {["Name"] = "Luck Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}}},
+        {
+            ["Free"] = {["Name"] = "Ability Scroll", ["ItemInfo"] = {["Rarity"] = "Ultimate", ["Level"] = 1}},
+            ["Gold"] = {["Name"] = "Ability Scroll", ["ItemInfo"] = {["Rarity"] = "Ultimate", ["Level"] = 1}}
+        },
+        {["Gold"] = {["Name"] = "XP Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}}},
+        {
+            ["Free"] = {["Name"] = "Power Scroll", ["ItemInfo"] = {["Rarity"] = "Ultimate", ["Level"] = 1}},
+            ["Gold"] = {["Name"] = "Power Scroll", ["ItemInfo"] = {["Rarity"] = "Ultimate", ["Level"] = 1}}
+        },
+        {["Gold"] = {["Name"] = "Gold Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}}},
+        {
+            ["Free"] = {["Name"] = "Upgrade Scroll", ["ItemInfo"] = {["Rarity"] = "Ultimate", ["Level"] = 1}},
+            ["Gold"] = {["Name"] = "Upgrade Scroll", ["ItemInfo"] = {["Rarity"] = "Ultimate", ["Level"] = 1}}
+        },
+        {["Gold"] = {["Name"] = "Speed Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}}},
+        {
+            ["Free"] = {["Name"] = "Level", ["ItemInfo"] = {["Level"] = 1, ["Quantity"] = 1}},
+            ["Gold"] = {["Name"] = "Level", ["ItemInfo"] = {["Level"] = 1, ["Quantity"] = 1}}
+        },
+        {["Gold"] = {["Name"] = "Speed Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}}},
+        {["Gold"] = {["Name"] = "Super Health Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}}},
+        {["Gold"] = {["Name"] = "Super Damage Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}}},
+        {["Gold"] = {["Name"] = "Super XP Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}}},
+        {["Gold"] = {["Name"] = "Super Gold Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}}},
+        {["Gold"] = {["Name"] = "Super Luck Potion", ["ItemInfo"] = {["Level"] = 1, ["Duration"] = 0.25}}},
+        {
+            ["Free"] = {["Name"] = "Sakura", ["ItemInfo"] = {["Level"] = 1}},
+            ["Gold"] = {["Name"] = "Rabid Axe", ["ItemInfo"] = {["Level"] = 1}}
+        }
+    },
+    ["_quests_rng"] = "Random",
+    ["DailyQuestReward"] = 15,
+    ["Version"] = 10,
+    ["_rewards_rng"] = "Random",
+    ["TotalPointsForCompletion"] = 1840,
+    ["StartTime"] = 1711684800,
+    ["Quests"] = {
+        ["Peppermint Miniboss Kills"] = {
+            ["Goal"] = 100,
+            ["StartTime"] = 1711684800,
+            ["Name"] = "Peppermint Miniboss Kills",
+            ["Reward"] = 65
+        },
+        ["Seller"] = {["Goal"] = 30, ["StartTime"] = 1714104000, ["Name"] = "Seller", ["Reward"] = 85},
+        ["Food Cart"] = {["Goal"] = 1, ["StartTime"] = 1711684800, ["Name"] = "Food Cart", ["Reward"] = 60},
+        ["Complete Quests"] = {["Goal"] = 15, ["StartTime"] = 1712289600, ["Name"] = "Complete Quests", ["Reward"] = 55},
+        ["Blaze Miniboss Kills"] = {
+            ["Goal"] = 60,
+            ["StartTime"] = 1711684800,
+            ["Name"] = "Blaze Miniboss Kills",
+            ["Reward"] = 45
+        },
+        ["Miniboss Kills"] = {["Goal"] = 70, ["StartTime"] = 1712289600, ["Name"] = "Miniboss Kills", ["Reward"] = 60},
+        ["Melee Monster Kills"] = {
+            ["Goal"] = 1150,
+            ["StartTime"] = 1713499200,
+            ["Name"] = "Melee Monster Kills",
+            ["Reward"] = 65
+        },
+        ["Wave Defense Bosses"] = {
+            ["Goal"] = 30,
+            ["StartTime"] = 1712894400,
+            ["Name"] = "Wave Defense Bosses",
+            ["Reward"] = 60
+        },
+        ["Use Potions"] = {["Goal"] = 10, ["StartTime"] = 1712289600, ["Name"] = "Use Potions", ["Reward"] = 55},
+        ["Ability Scroll"] = {["Goal"] = 1, ["StartTime"] = 1712894400, ["Name"] = "Ability Scroll", ["Reward"] = 75},
+        ["Wave Defense Waves"] = {
+            ["Goal"] = 270,
+            ["StartTime"] = 1712894400,
+            ["Name"] = "Wave Defense Waves",
+            ["Reward"] = 75
+        },
+        ["Frost Miniboss Kills"] = {
+            ["Goal"] = 60,
+            ["StartTime"] = 1711684800,
+            ["Name"] = "Frost Miniboss Kills",
+            ["Reward"] = 60
+        },
+        ["Boss Kills"] = {["Goal"] = 30, ["StartTime"] = 1712894400, ["Name"] = "Boss Kills", ["Reward"] = 70},
+        ["Shopper"] = {["Goal"] = 10, ["StartTime"] = 1714708800, ["Name"] = "Shopper", ["Reward"] = 70},
+        ["Monster Kills"] = {["Goal"] = 1500, ["StartTime"] = 1712289600, ["Name"] = "Monster Kills", ["Reward"] = 50},
+        ["Horseman Miniboss Kills"] = {
+            ["Goal"] = 60,
+            ["StartTime"] = 1714104000,
+            ["Name"] = "Horseman Miniboss Kills",
+            ["Reward"] = 85
+        },
+        ["Obsidian Miniboss Kills"] = {
+            ["Goal"] = 70,
+            ["StartTime"] = 1714104000,
+            ["Name"] = "Obsidian Miniboss Kills",
+            ["Reward"] = 55
+        },
+        ["Candy Land Dungeon Monster Kills"] = {
+            ["Goal"] = 750,
+            ["StartTime"] = 1713499200,
+            ["Name"] = "Candy Land Dungeon Monster Kills",
+            ["Reward"] = 70
+        },
+        ["Prize Wheel"] = {["Goal"] = 4, ["StartTime"] = 1715313600, ["Name"] = "Prize Wheel", ["Reward"] = 65},
+        ["Elite Boss Kills"] = {
+            ["Goal"] = 10,
+            ["StartTime"] = 1714708800,
+            ["Name"] = "Elite Boss Kills",
+            ["Reward"] = 75
+        },
+        ["Coral Kingdom Dungeon"] = {
+            ["Goal"] = 20,
+            ["StartTime"] = 1715313600,
+            ["Name"] = "Coral Kingdom Dungeon",
+            ["Reward"] = 85
+        },
+        ["Mission Impossible"] = {
+            ["Goal"] = 10,
+            ["StartTime"] = 1715313600,
+            ["Name"] = "Mission Impossible",
+            ["Reward"] = 90
+        },
+        ["Level Up"] = {["Goal"] = 1, ["StartTime"] = 1714104000, ["Name"] = "Level Up", ["Reward"] = 65},
+        ["Playtime"] = {["Goal"] = 110, ["StartTime"] = 1712289600, ["Name"] = "Playtime", ["Reward"] = 60},
+        ["Sinister Sewers Dungeon Monster Kills"] = {
+            ["Goal"] = 950,
+            ["StartTime"] = 1714104000,
+            ["Name"] = "Sinister Sewers Dungeon Monster Kills",
+            ["Reward"] = 60
+        },
+        ["Ranged Monster Kills"] = {
+            ["Goal"] = 1200,
+            ["StartTime"] = 1715313600,
+            ["Name"] = "Ranged Monster Kills",
+            ["Reward"] = 75
+        },
+        ["Coral Kingdom Dungeon Monster Kills"] = {
+            ["Goal"] = 1000,
+            ["StartTime"] = 1713499200,
+            ["Name"] = "Coral Kingdom Dungeon Monster Kills",
+            ["Reward"] = 70
+        },
+        ["Sinister Sewers Dungeon"] = {
+            ["Goal"] = 30,
+            ["StartTime"] = 1712289600,
+            ["Name"] = "Sinister Sewers Dungeon",
+            ["Reward"] = 70
+        },
+        ["Blacksmith"] = {["Goal"] = 60, ["StartTime"] = 1715313600, ["Name"] = "Blacksmith", ["Reward"] = 65},
+        ["Miniboss Master"] = {["Goal"] = 3, ["StartTime"] = 1714708800, ["Name"] = "Miniboss Master", ["Reward"] = 60},
+        ["Charity"] = {["Goal"] = 1, ["StartTime"] = 1714708800, ["Name"] = "Charity", ["Reward"] = 65},
+        ["Star Guardian Miniboss Kills"] = {
+            ["Goal"] = 70,
+            ["StartTime"] = 1714708800,
+            ["Name"] = "Star Guardian Miniboss Kills",
+            ["Reward"] = 70
+        },
+        ["Voltage Miniboss Kills"] = {
+            ["Goal"] = 80,
+            ["StartTime"] = 1713499200,
+            ["Name"] = "Voltage Miniboss Kills",
+            ["Reward"] = 55
+        },
+        ["Sacred Sands Dungeon Monster Kills"] = {
+            ["Goal"] = 750,
+            ["StartTime"] = 1714708800,
+            ["Name"] = "Sacred Sands Dungeon Monster Kills",
+            ["Reward"] = 65
+        },
+        ["Ancient Jungle Dungeon Monster Kills"] = {
+            ["Goal"] = 1000,
+            ["StartTime"] = 1714104000,
+            ["Name"] = "Ancient Jungle Dungeon Monster Kills",
+            ["Reward"] = 55
+        },
+        ["Medkits"] = {["Goal"] = 200, ["StartTime"] = 1711684800, ["Name"] = "Medkits", ["Reward"] = 60},
+        ["Rename Item"] = {["Goal"] = 1, ["StartTime"] = 1713499200, ["Name"] = "Rename Item", ["Reward"] = 50},
+        ["Witch Miniboss Kills"] = {
+            ["Goal"] = 90,
+            ["StartTime"] = 1715313600,
+            ["Name"] = "Witch Miniboss Kills",
+            ["Reward"] = 90
+        },
+        ["Flawless"] = {["Goal"] = 10, ["StartTime"] = 1713499200, ["Name"] = "Flawless", ["Reward"] = 60},
+        ["Greenback Miniboss Kills"] = {
+            ["Goal"] = 90,
+            ["StartTime"] = 1712894400,
+            ["Name"] = "Greenback Miniboss Kills",
+            ["Reward"] = 55
+        },
+        ["Crystal Cave Dungeon"] = {
+            ["Goal"] = 20,
+            ["StartTime"] = 1711684800,
+            ["Name"] = "Crystal Cave Dungeon",
+            ["Reward"] = 35
+        },
+        ["Teamwork"] = {["Goal"] = 23, ["StartTime"] = 1712894400, ["Name"] = "Teamwork", ["Reward"] = 45}
+    },
+    ["PointsPerReward"] = 40
+}
