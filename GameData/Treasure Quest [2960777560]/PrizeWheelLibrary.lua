80cb53212e75b193d6b8f8990b7264b34644f705
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-29 03:19:23 +1000

    Create PrizeWheelLibrary.lua

---
+++ GameData/Treasure Quest [2960777560]/PrizeWheelLibrary.lua
@@ -0,0 +1,184 @@
+data = {
+    ["Prizes"] = {
+        {
+            ["Type"] = "Item",
+            ["Index"] = 1,
+            ["Rarity"] = "Ultimate",
+            ["Color"] = Color3.fromRGB(0, 255, 255),
+            ["Image"] = "rbxassetid://16912211351",
+            ["Name"] = "Bunny Bow",
+            ["Reward"] = "function: 0x5c153eed8915e348"
+        },
+        {
+            ["Type"] = "Gold",
+            ["Index"] = 2,
+            ["Color"] = Color3.fromRGB(255, 215, 0),
+            ["Image"] = "rbxassetid://15394537919",
+            ["Reward"] = "function: 0xaa72d90925ab73c8"
+        },
+        {
+            ["Type"] = "Potion",
+            ["Index"] = 3,
+            ["Rarity"] = "Unique",
+            ["Color"] = Color3.fromRGB(255, 127, 0),
+            ["Image"] = "rbxassetid://6813278386",
+            ["Name"] = "Random Potion1",
+            ["Reward"] = "function: 0x748ad5d07a47d908"
+        },
+        {
+            ["Type"] = "Item",
+            ["Index"] = 4,
+            ["Rarity"] = "Rare",
+            ["Color"] = Color3.fromRGB(0, 125, 255),
+            ["Image"] = "rbxassetid://16912221116",
+            ["Name"] = "Mossy Slasher",
+            ["Reward"] = "function: 0xca1bc5ed4c415ce8"
+        },
+        {
+            ["Type"] = "Item",
+            ["Index"] = 5,
+            ["Rarity"] = "Mythical",
+            ["Color"] = Color3.fromRGB(255, 215, 0),
+            ["Image"] = "rbxassetid://16912213686",
+            ["Name"] = "Cosmic Egg Bow",
+            ["Reward"] = "function: 0x6942d9bb2558d048"
+        },
+        {
+            ["Type"] = "Item",
+            ["Index"] = 6,
+            ["Rarity"] = "Common",
+            ["Color"] = Color3.fromRGB(100, 255, 50),
+            ["Image"] = "rbxassetid://16912216207",
+            ["Name"] = "Handmade Blade",
+            ["Reward"] = "function: 0x0cedb079fb321b28"
+        },
+        {
+            ["Type"] = "Effect",
+            ["Index"] = 7,
+            ["Rarity"] = "Unique",
+            ["Color"] = Color3.fromRGB(255, 127, 0),
+            ["Image"] = "rbxassetid://6813278452",
+            ["Name"] = "Random Effect1",
+            ["Reward"] = "function: 0x6e06992e10dd8788"
+        },
+        {
+            ["Type"] = "Gold",
+            ["Index"] = 8,
+            ["Color"] = Color3.fromRGB(255, 215, 0),
+            ["Image"] = "rbxassetid://15394537603",
+            ["Reward"] = "function: 0xc33574e2ac99e408"
+        },
+        {
+            ["Type"] = "Item",
+            ["Index"] = 9,
+            ["Rarity"] = "Legendary",
+            ["Color"] = Color3.fromRGB(255, 25, 25),
+            ["Image"] = "rbxassetid://16912213550",
+            ["Name"] = "Cosmic Egg Caster",
+            ["Reward"] = "function: 0xc1be629459ff1268"
+        },
+        {
+            ["Type"] = "Item",
+            ["Index"] = 10,
+            ["Rarity"] = "Rare",
+            ["Color"] = Color3.fromRGB(0, 125, 255),
+            ["Image"] = "rbxassetid://16912218897",
+            ["Name"] = "Lavender Scent",
+            ["Reward"] = "function: 0x6cd64cc3449eaec8"
+        },
+        {
+            ["Type"] = "Effect",
+            ["Index"] = 11,
+            ["Rarity"] = "Unique",
+            ["Color"] = Color3.fromRGB(255, 127, 0),
+            ["Image"] = "rbxassetid://6813278452",
+            ["Name"] = "Random Effect2",
+            ["Reward"] = "function: 0x8a124b112f3848a8"
+        },
+        {
+            ["Type"] = "Item",
+            ["Index"] = 12,
+            ["Rarity"] = "Common",
+            ["Color"] = Color3.fromRGB(100, 255, 50),
+            ["Image"] = "rbxassetid://16912224398",
+            ["Name"] = "Tulip Orbstaff",
+            ["Reward"] = "function: 0x69ba26649a933408"
+        },
+        {
+            ["Type"] = "Potion",
+            ["Index"] = 13,
+            ["Rarity"] = "Unique",
+            ["Color"] = Color3.fromRGB(255, 127, 0),
+            ["Image"] = "rbxassetid://6813278386",
+            ["Name"] = "Random Potion2",
+            ["Reward"] = "function: 0xd4da1bbaf075a1e8"
+        },
+        {
+            ["Type"] = "Gold",
+            ["Index"] = 14,
+            ["Color"] = Color3.fromRGB(255, 215, 0),
+            ["Image"] = "rbxassetid://15394538448",
+            ["Reward"] = "function: 0x8e085c6f9020ae68"
+        },
+        {
+            ["Type"] = "Item",
+            ["Index"] = 15,
+            ["Rarity"] = "Common",
+            ["Color"] = Color3.fromRGB(100, 255, 50),
+            ["Image"] = "rbxassetid://16912224531",
+            ["Name"] = "Tulip Glaive",
+            ["Reward"] = "function: 0x97ffa3dedddb91a8"
+        },
+        {
+            ["Type"] = "Gold",
+            ["Index"] = 16,
+            ["Color"] = Color3.fromRGB(255, 215, 0),
+            ["Image"] = "rbxassetid://15394538165",
+            ["Reward"] = "function: 0x8c2b1e592a9f3cb8"
+        }
+    },
+    ["StatsInfo"] = {
+        ["Level"] = {
+            ["Color"] = Color3.fromRGB(0, 150, 255),
+            ["Image"] = {"rbxassetid://6366210200"},
+            ["Reward"] = "function: 0xf8d7e67060d19088"
+        },
+        ["Gold"] = {
+            ["Color"] = Color3.fromRGB(255, 215, 0),
+            ["Image"] = {
+                "rbxassetid://15394537919",
+                "rbxassetid://15394537603",
+                "rbxassetid://15394538165",
+                "rbxassetid://15394538448"
+            },
+            ["Reward"] = "function: 0x2246c7673e9c0cf8"
+        }
+    },
+    ["ItemPrizeNames"] = {
+        "Bunny Bow",
+        "Mossy Slasher",
+        "Cosmic Egg Bow",
+        "Handmade Blade",
+        "Cosmic Egg Caster",
+        "Lavender Scent",
+        "Tulip Orbstaff",
+        "Tulip Glaive"
+    },
+    ["DropChances"] = {
+        ["Legendary"] = 2.4001,
+        ["Mythical"] = 0.18,
+        ["Common"] = 73.3898,
+        ["Rare"] = 24,
+        ["Ultimate"] = 0.0301
+    },
+    ["RarityPrizes"] = {
+        ["Common3"] = "Tulip Orbstaff",
+        ["Legendary"] = "Cosmic Egg Caster",
+        ["Common2"] = "Tulip Glaive",
+        ["Mythical"] = "Cosmic Egg Bow",
+        ["Common1"] = "Handmade Blade",
+        ["Rare2"] = "Lavender Scent",
+        ["Rare1"] = "Mossy Slasher",
+        ["Ultimate"] = "Bunny Bow"
+    }
+}
