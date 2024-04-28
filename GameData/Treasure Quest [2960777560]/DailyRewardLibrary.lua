d93da47bafb27b87a35bbd49b33b3576497448ed
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-04-29 03:14:33 +1000

    Create DailyRewardLibrary.lua

---
+++ GameData/Treasure Quest [2960777560]/DailyRewardLibrary.lua
@@ -0,0 +1,166 @@
+data = {
+    ["StaticRewards"] = {
+        {
+            {
+                ["Color"] = Color3.fromRGB(255, 215, 0),
+                ["Image"] = "rbxassetid://15394537919",
+                ["Reward"] = "function: 0x8ecc1d9ceec4afa8"
+            },
+            {
+                ["Color"] = Color3.fromRGB(255, 127, 0),
+                ["Image"] = "rbxassetid://6813278386",
+                ["Reward"] = "function: 0x491108165b7c78f8"
+            },
+            {
+                ["Color"] = Color3.fromRGB(255, 215, 0),
+                ["Image"] = "rbxassetid://15394537603",
+                ["Reward"] = "function: 0xda5a8a128ff70b98"
+            },
+            {
+                ["Color"] = Color3.fromRGB(255, 127, 0),
+                ["Image"] = "rbxassetid://6813278452",
+                ["Reward"] = "function: 0xe9e104bd09e3b2a8"
+            },
+            {
+                ["Color"] = Color3.fromRGB(255, 215, 0),
+                ["Image"] = "rbxassetid://15394538165",
+                ["Reward"] = "function: 0xf19767680ea69688"
+            },
+            {
+                ["Color"] = Color3.fromRGB(0, 150, 255),
+                ["Image"] = "rbxassetid://6366210200",
+                ["Reward"] = "function: 0x8f0bd32f8d7a3278"
+            }
+        },
+        {
+            {
+                ["Color"] = Color3.fromRGB(255, 215, 0),
+                ["Image"] = "rbxassetid://15394537919",
+                ["Reward"] = "function: 0x8ecc1d9ceec4afa8"
+            },
+            {
+                ["Color"] = Color3.fromRGB(255, 127, 0),
+                ["Image"] = "rbxassetid://6813278386",
+                ["Reward"] = "function: 0x491108165b7c78f8"
+            },
+            {
+                ["Color"] = Color3.fromRGB(255, 215, 0),
+                ["Image"] = "rbxassetid://15394537603",
+                ["Reward"] = "function: 0xda5a8a128ff70b98"
+            },
+            {
+                ["Color"] = Color3.fromRGB(255, 127, 0),
+                ["Image"] = "rbxassetid://6813278452",
+                ["Reward"] = "function: 0xe9e104bd09e3b2a8"
+            },
+            {
+                ["Color"] = Color3.fromRGB(255, 215, 0),
+                ["Image"] = "rbxassetid://15394538165",
+                ["Reward"] = "function: 0xf19767680ea69688"
+            },
+            {
+                ["Color"] = Color3.fromRGB(0, 150, 255),
+                ["Image"] = "rbxassetid://6366210200",
+                ["Reward"] = "function: 0x8f0bd32f8d7a3278"
+            }
+        },
+        {
+            {
+                ["Color"] = Color3.fromRGB(255, 215, 0),
+                ["Image"] = "rbxassetid://15394537919",
+                ["Reward"] = "function: 0x8ecc1d9ceec4afa8"
+            },
+            {
+                ["Color"] = Color3.fromRGB(255, 127, 0),
+                ["Image"] = "rbxassetid://6813278386",
+                ["Reward"] = "function: 0x491108165b7c78f8"
+            },
+            {
+                ["Color"] = Color3.fromRGB(255, 215, 0),
+                ["Image"] = "rbxassetid://15394537603",
+                ["Reward"] = "function: 0xda5a8a128ff70b98"
+            },
+            {
+                ["Color"] = Color3.fromRGB(255, 127, 0),
+                ["Image"] = "rbxassetid://6813278452",
+                ["Reward"] = "function: 0xe9e104bd09e3b2a8"
+            },
+            {
+                ["Color"] = Color3.fromRGB(255, 215, 0),
+                ["Image"] = "rbxassetid://15394538165",
+                ["Reward"] = "function: 0xf19767680ea69688"
+            },
+            {
+                ["Color"] = Color3.fromRGB(0, 150, 255),
+                ["Image"] = "rbxassetid://6366210200",
+                ["Reward"] = "function: 0x8f0bd32f8d7a3278"
+            }
+        },
+        {
+            {
+                ["Color"] = Color3.fromRGB(255, 215, 0),
+                ["Image"] = "rbxassetid://15394537919",
+                ["Reward"] = "function: 0x8ecc1d9ceec4afa8"
+            },
+            {
+                ["Color"] = Color3.fromRGB(255, 127, 0),
+                ["Image"] = "rbxassetid://6813278386",
+                ["Reward"] = "function: 0x491108165b7c78f8"
+            },
+            {
+                ["Color"] = Color3.fromRGB(255, 215, 0),
+                ["Image"] = "rbxassetid://15394537603",
+                ["Reward"] = "function: 0xda5a8a128ff70b98"
+            },
+            {
+                ["Color"] = Color3.fromRGB(255, 127, 0),
+                ["Image"] = "rbxassetid://6813278452",
+                ["Reward"] = "function: 0xe9e104bd09e3b2a8"
+            },
+            {
+                ["Color"] = Color3.fromRGB(255, 215, 0),
+                ["Image"] = "rbxassetid://15394538165",
+                ["Reward"] = "function: 0xf19767680ea69688"
+            },
+            {
+                ["Color"] = Color3.fromRGB(0, 150, 255),
+                ["Image"] = "rbxassetid://6366210200",
+                ["Reward"] = "function: 0x8f0bd32f8d7a3278"
+            }
+        }
+    },
+    ["DynamicRewards"] = {
+        {
+            "North Feather",
+            "Earth Axe",
+            "Fire Dagger",
+            "Navy Katars",
+            "Staff of Focus",
+            "Amethyst Bow",
+            "Amethyst Dagger",
+            "Amethyst Wand"
+        },
+        {
+            "Midnight Glaive",
+            "Infernal Slicer",
+            "Energy Falchion",
+            "Atomic",
+            "Black Hole Caster",
+            "Radioactive Bow",
+            "Radioactive Slasher",
+            "Radioactive Lamp"
+        },
+        {
+            "Ascent",
+            "Space Claymore",
+            "Sci Spear",
+            "Subatomic",
+            "Crystal Fang Staff",
+            "Mineral Bow",
+            "Mineral Slasher",
+            "Mineral Zapper"
+        },
+        {"Darkness", "Arachnid", "Monument", "Gravetender", "Fuchsia", "Mana Bow", "Voltaxe", "Wizard's Spellbook"}
+    },
+    ["NUM_WEEKS"] = 4
+}
