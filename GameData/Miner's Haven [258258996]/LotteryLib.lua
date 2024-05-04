6130574d6260275cc353b07d59b82e85e5d3ee98
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-05-04 22:03:47 +1000

    Create LotteryLib.lua

---
+++ GameData/Miner's Haven [258258996]/LotteryLib.lua
@@ -0,0 +1,220 @@
+data = {
+    ["Stats"] = {
+        ["Luxury"] = {
+            ["Description"] = "An incredibly hard to obtain box that contains some of the finest items in the game.",
+            ["ThumbnailId"] = 5140907462,
+            ["TwitchModifier"] = 1,
+            ["BoxColor"] = Color3.fromRGB(255, 238, 175),
+            ["Rates"] = {
+                ["Luxury"] = 40,
+                ["Shop"] = 0,
+                ["Vintage"] = 3,
+                ["Premium"] = 55,
+                ["Exotic"] = 2,
+                ["Contraband"] = 0
+            }
+        },
+        ["Cake Raffle"] = {
+            ["Description"] = "Daily Cake Raffle box. Contains limited-edition birthday themed items.",
+            ["ThumbnailId"] = 6652258583,
+            ["TwitchModifier"] = 1,
+            ["BoxColor"] = Color3.fromRGB(253, 63, 219),
+            ["Rates"] = {
+                ["Luxury"] = 0,
+                ["Shop"] = 0,
+                ["Cake"] = 100,
+                ["Premium"] = 0,
+                ["Exotic"] = 0,
+                ["Contraband"] = 0,
+                ["Vintage"] = 0
+            }
+        },
+        ["Festive"] = {
+            ["Description"] = "The most wonderful box of the year! Contains limited-edition holiday themed items.",
+            ["ThumbnailId"] = 5140907136,
+            ["Relic"] = true,
+            ["TwitchModifier"] = 1,
+            ["BoxColor"] = Color3.fromRGB(144, 255, 170),
+            ["Rates"] = {
+                ["Luxury"] = 5,
+                ["Shop"] = 0,
+                ["Vintage"] = 3,
+                ["Premium"] = 40,
+                ["Exotic"] = 2,
+                ["Holiday"] = 50,
+                ["Contraband"] = 0
+            }
+        },
+        ["Spectral"] = {
+            ["Description"] = "A rare box that can only be obtained through supernatural means.",
+            ["ThumbnailId"] = 5140907887,
+            ["Relic"] = true,
+            ["TwitchModifier"] = 1,
+            ["BoxColor"] = Color3.fromRGB(21, 225, 223),
+            ["Rates"] = {
+                ["Luxury"] = 10,
+                ["Shop"] = 0,
+                ["Vintage"] = 5,
+                ["Premium"] = 75,
+                ["Exotic"] = 5,
+                ["Contraband"] = 5
+            }
+        },
+        ["Unreal"] = {
+            ["Description"] = "A rare box that a chance to turn into something else...",
+            ["ThumbnailId"] = 5140908051,
+            ["TwitchModifier"] = 1,
+            ["BoxColor"] = Color3.fromRGB(194, 38, 255),
+            ["Rates"] = {
+                ["Luxury"] = 5,
+                ["Shop"] = 0,
+                ["Vintage"] = 2,
+                ["Premium"] = 91,
+                ["Exotic"] = 2,
+                ["Contraband"] = 0
+            }
+        },
+        ["Regular"] = {
+            ["Description"] = "A box that contains a random item inside. Open it up!",
+            ["ThumbnailId"] = 5140907803,
+            ["TwitchModifier"] = 1,
+            ["BoxColor"] = Color3.fromRGB(170, 117, 86),
+            ["Rates"] = {
+                ["Luxury"] = 0,
+                ["Shop"] = 0,
+                ["Vintage"] = 1,
+                ["Premium"] = 98,
+                ["Exotic"] = 1,
+                ["Contraband"] = 0
+            }
+        },
+        ["Pumpkin"] = {
+            ["Description"] = "A spooky box containing Halloween items new and old!",
+            ["ThumbnailId"] = 5140907652,
+            ["Relic"] = true,
+            ["TwitchModifier"] = 1,
+            ["BoxColor"] = Color3.fromRGB(144, 255, 170),
+            ["Rates"] = {
+                ["Luxury"] = 0,
+                ["Halloween"] = 50,
+                ["Vintage"] = 3,
+                ["Premium"] = 40,
+                ["Exotic"] = 2,
+                ["Shop"] = 0,
+                ["Contraband"] = 5
+            }
+        },
+        ["Heavenly"] = {
+            ["Description"] = "Could it be..?",
+            ["ThumbnailId"] = 5140907242,
+            ["Relic"] = true,
+            ["TwitchModifier"] = 1,
+            ["BoxColor"] = Color3.fromRGB(192, 51, 197),
+            ["Rates"] = {
+                ["Luxury"] = 0,
+                ["Shop"] = 0,
+                ["Vintage"] = 50,
+                ["Premium"] = 0,
+                ["Exotic"] = 50,
+                ["Contraband"] = 0
+            }
+        },
+        ["Birthday"] = {
+            ["Description"] = "Celebrating 5 years of Miner's Haven. Contains limited-edition birthday themed items.",
+            ["ThumbnailId"] = 5140907021,
+            ["TwitchModifier"] = 1,
+            ["BoxColor"] = Color3.fromRGB(253, 43, 117),
+            ["Rates"] = {
+                ["Luxury"] = 5,
+                ["Shop"] = 0,
+                ["Birthday"] = 50,
+                ["Vintage"] = 3,
+                ["Premium"] = 40,
+                ["Exotic"] = 2,
+                ["Contraband"] = 0
+            }
+        },
+        ["Easter"] = {
+            ["Description"] = "Contains limited-edition Easter themed items.",
+            ["ThumbnailId"] = 6652258288,
+            ["TwitchModifier"] = 1,
+            ["BoxColor"] = Color3.fromRGB(56, 253, 178),
+            ["Rates"] = {
+                ["Luxury"] = 5,
+                ["Easter"] = 50,
+                ["Vintage"] = 3,
+                ["Premium"] = 40,
+                ["Exotic"] = 2,
+                ["Shop"] = 0,
+                ["Contraband"] = 0
+            }
+        },
+        ["Red-Banded"] = {
+            ["Description"] = "A shady box that is highly likely to contain Contraband or Luxery items.",
+            ["ThumbnailId"] = 5140907725,
+            ["Relic"] = true,
+            ["TwitchModifier"] = 1,
+            ["BoxColor"] = Color3.fromRGB(85, 29, 30),
+            ["Rates"] = {
+                ["Luxury"] = 0,
+                ["Shop"] = 0,
+                ["Vintage"] = 3,
+                ["Premium"] = 65,
+                ["Exotic"] = 2,
+                ["Contraband"] = 30
+            }
+        },
+        ["Magnificent"] = {
+            ["Description"] = "The most powerful box in existence. How did you get this?",
+            ["ThumbnailId"] = 5140907561,
+            ["TwitchModifier"] = 1,
+            ["BoxColor"] = Color3.fromRGB(0, 255, 247),
+            ["Rates"] = {
+                ["Luxury"] = 5,
+                ["Shop"] = 0,
+                ["Vintage"] = 40,
+                ["Premium"] = 25,
+                ["Exotic"] = 20,
+                ["Contraband"] = 0
+            }
+        },
+        ["Twitch"] = {
+            ["Description"] = "Contains limited-edition twitch themed items.",
+            ["ThumbnailId"] = 5140907979,
+            ["TwitchModifier"] = 1,
+            ["BoxColor"] = Color3.fromRGB(119, 74, 255),
+            ["Rates"] = {
+                ["Luxury"] = 5,
+                ["Shop"] = 0,
+                ["Vintage"] = 3,
+                ["Premium"] = 50,
+                ["Exotic"] = 2,
+                ["Twitch"] = 40,
+                ["Contraband"] = 0
+            }
+        },
+        ["Inferno"] = {
+            ["Description"] = "A box as fiery as it is rare. Contains some of the best items in the game.",
+            ["ThumbnailId"] = 5140907368,
+            ["Relic"] = true,
+            ["TwitchModifier"] = 1,
+            ["BoxColor"] = Color3.fromRGB(239, 73, 76),
+            ["Rates"] = {
+                ["Luxury"] = 5,
+                ["Shop"] = 0,
+                ["Vintage"] = 3,
+                ["Premium"] = 90,
+                ["Exotic"] = 2,
+                ["Contraband"] = 0
+            }
+        }
+    },
+    ["AwardClover"] = "function: 0x5ef5f36f541bc2e5",
+    ["GetBoxInfo"] = "function: 0x3dae45533b200bb5",
+    ["Award"] = "function: 0x2fbbe5bc876e5cd5",
+    ["Simulate"] = "function: 0x020100069ad57e15",
+    ["RelicChance"] = 200,
+    ["DropTable"] = "function: 0x3b4fbf4ca0c6d7f5",
+    ["Run"] = "function: 0x9984fb1758ab85d5",
+    ["UpdatedBoxData"] = true
+}
