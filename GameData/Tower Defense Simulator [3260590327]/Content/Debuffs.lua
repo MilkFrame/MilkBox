5a12433e2c008c88cdbd161564fe056c2c5aaaed
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-05-04 22:35:49 +1000

    Create Debuffs.lua

---
+++ GameData/Tower Defense Simulator [3260590327]/Content/Debuffs.lua
@@ -0,0 +1,103 @@
+data = {
+    ["PosionDebuff"] = {
+        ["Destroy"] = "function: 0x7e941ff029161f45",
+        ["OnServerTick"] = "function: 0x9a5aa892a71f60e5",
+        ["DefaultParameters"] = {["DefenseMelt"] = 0, ["Slowness"] = 10, ["PosionDamage"] = 0, ["PosionTickRate"] = 0.5},
+        ["CanApply"] = "function: 0x179379af5ecf8335",
+        ["OnClientTick"] = "function: 0x37de122da9034b55",
+        ["Score"] = "function: 0xd7e505aaebb3c895",
+        ["Create"] = "function: 0x0936f1f6c8877c55",
+        ["DebuffType"] = "2"
+    },
+    ["ConfuseDebuff"] = {
+        ["Destroy"] = "function: 0xdde5b565482b38a5",
+        ["OnServerTick"] = "function: 0x67aed5d725180795",
+        ["DefaultParameters"] = {},
+        ["CanApply"] = "function: 0x7744f9cc6f85b4d5",
+        ["OnClientTick"] = "function: 0x6419f1119cd5f775",
+        ["Score"] = "function: 0x576fc58efc7864b5",
+        ["Create"] = "function: 0x9129a04b8122cc35",
+        ["DebuffType"] = "6"
+    },
+    ["BurnDebuff"] = {
+        ["Destroy"] = "function: 0x919bc1b5c4db44d5",
+        ["OnServerTick"] = "function: 0x83bb10a6d8b17435",
+        ["DefaultParameters"] = {["BurnDamage"] = 3, ["BurnTickRate"] = 1},
+        ["CanApply"] = "function: 0xfd1e12567c743fe5",
+        ["OnClientTick"] = "function: 0xe6c615fc46e27bb5",
+        ["Score"] = "function: 0x269371f1e3a8a315",
+        ["Create"] = "function: 0xe13e703e73da0825",
+        ["DebuffType"] = "1"
+    },
+    ["ShockDebuff"] = {
+        ["Destroy"] = "function: 0xaafdf952004befb5",
+        ["OnServerTick"] = "function: 0xf75bdd0f2a8c2fd5",
+        ["DefaultParameters"] = {},
+        ["CanApply"] = "function: 0xd696d549bf5b6fb5",
+        ["OnClientTick"] = "function: 0xe42e295299b3ee75",
+        ["Score"] = "function: 0x946dc98ba916af15",
+        ["Create"] = "function: 0x7a8c5210f39fb5d5",
+        ["DebuffType"] = "6"
+    },
+    ["StunDebuff"] = {
+        ["Destroy"] = "function: 0x1f568f343f4c6475",
+        ["OnServerTick"] = "function: 0x4973dc861e2f07f5",
+        ["DefaultParameters"] = {},
+        ["CanApply"] = "function: 0x817eca43925a4755",
+        ["OnClientTick"] = "function: 0x1466d144eef0d615",
+        ["Score"] = "function: 0xc461c07d36979735",
+        ["Create"] = "function: 0x0a8afe69fab1f175",
+        ["DebuffType"] = "7"
+    },
+    ["ScaleDebuff"] = {
+        ["Destroy"] = "function: 0xbcedc73ec978f835",
+        ["OnServerTick"] = "function: 0x74243d3a18f22ad5",
+        ["DefaultParameters"] = {["HealthModifier"] = 1.5, ["SizeModifier"] = 1.5, ["SpeedModifier"] = 0.75},
+        ["CanApply"] = "function: 0x0ceefbc6aeebbed5",
+        ["OnClientTick"] = "function: 0x7796ad388819ed75",
+        ["Score"] = "function: 0x673ea1b5c9bd62b5",
+        ["Create"] = "function: 0x24fd15a2d9395da5",
+        ["DebuffType"] = "8"
+    },
+    ["FreezeDebuff"] = {
+        ["Destroy"] = "function: 0x3be4348ecda42175",
+        ["OnServerTick"] = "function: 0x9e65006a7ef99a95",
+        ["DefaultParameters"] = {},
+        ["CanApply"] = "function: 0xcdc5c8436a161975",
+        ["OnClientTick"] = "function: 0x4c937a69cda1dd35",
+        ["Score"] = "function: 0x714d8d6f0a0c2275",
+        ["Create"] = "function: 0x40576d8103457ed5",
+        ["DebuffType"] = "5"
+    },
+    ["StingDebuff"] = {
+        ["Destroy"] = "function: 0xad3efb5370954e95",
+        ["OnServerTick"] = "function: 0xda8dd06114d0a615",
+        ["DefaultParameters"] = {["StingTickRate"] = 1, ["StingDamage"] = 1},
+        ["CanApply"] = "function: 0xe9352976cd6755b5",
+        ["OnClientTick"] = "function: 0xf420d1751f33a5d5",
+        ["Score"] = "function: 0x26bea570749a1515",
+        ["Create"] = "function: 0x6445ec23f4f275d5",
+        ["DebuffType"] = "3"
+    },
+    ["SlownessDebuff"] = {
+        ["Destroy"] = "function: 0x15d01579c4ce2b85",
+        ["OnServerTick"] = "function: 0x56d9959fdbc4d015",
+        ["DefaultParameters"] = {["SpeedModifier"] = 0.75},
+        ["CanApply"] = "function: 0x369410580e8a89f5",
+        ["OnClientTick"] = "function: 0x84e1055d56f168b5",
+        ["Score"] = "function: 0x342faa1afe5f4155",
+        ["Create"] = "function: 0x3dd0477dcc9e0335",
+        ["DebuffType"] = "8"
+    },
+    ["FrostDebuff"] = {
+        ["Destroy"] = "function: 0xe4d7ec4cc0d34cf5",
+        ["OnServerTick"] = "function: 0xd115c615a91b5b85",
+        ["Score"] = "function: 0xd7dd35dcdec32915",
+        ["Create"] = "function: 0x444109df0691f895",
+        ["PreApply"] = "function: 0x69aeebd8f61f55e5",
+        ["DefaultParameters"] = {},
+        ["CanApply"] = "function: 0xa560744f65feaa95",
+        ["OnClientTick"] = "function: 0x07e791a24a1e61b5",
+        ["DebuffType"] = "11"
+    }
+}
