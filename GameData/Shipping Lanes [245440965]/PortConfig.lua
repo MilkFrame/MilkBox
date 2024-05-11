5364156715018684db2ec7a362e47d8d18cddc82
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-05-12 02:50:11 +1000

    Create PortConfig.lua

---
+++ GameData/Shipping Lanes [245440965]/PortConfig.lua
@@ -0,0 +1,106 @@
+data = {
+    ["Emerald Harbor"] = {
+        ["PortConfig"] = {
+            ["ProfitFactor"] = 1,
+            ["ImageID"] = 6984493596,
+            ["Description"] = "A harbor situated in the desert, which happens to be sitting on the largest known deposits of oil in the game, know for its cheap and affordable oil.",
+            ["MaxShipSize"] = 1000000
+        }
+    },
+    ["Norfolk Port"] = {
+        ["PortConfig"] = {
+            ["ProfitFactor"] = 0.95,
+            ["ImageID"] = 9280339838,
+            ["Description"] = "This port can be a nightmare to navigate into, situated on a island far away surrounded by many rocks and boulders. This port also happens to have a small oil refinery which can process oil into fuel.",
+            ["MaxShipSize"] = 1000000
+        }
+    },
+    ["Woodside Inlet"] = {
+        ["PortConfig"] = {
+            ["ProfitFactor"] = 1.3,
+            ["ImageID"] = 6258578614,
+            ["Description"] = "",
+            ["MaxShipSize"] = 50000
+        }
+    },
+    ["Bayview Port"] = {
+        ["PortConfig"] = {
+            ["ProfitFactor"] = 1.3,
+            ["ImageID"] = 6258578614,
+            ["Description"] = "",
+            ["MaxShipSize"] = 51000
+        }
+    },
+    ["Rockfall Port"] = {
+        ["PortConfig"] = {
+            ["ProfitFactor"] = 1,
+            ["ImageID"] = 8901010489,
+            ["Description"] = "A rather boring port due to its location and its accessibility from other major ports.",
+            ["MaxShipSize"] = 1000000
+        }
+    },
+    ["Stanley Harbor"] = {
+        ["PortConfig"] = {
+            ["ProfitFactor"] = 1,
+            ["ImageID"] = 8901010341,
+            ["Description"] = "This port is built around a small harbour which can make it tough to access for bigger ships. It also features a canal entrance which can mean that this port can get busy sometimes. ",
+            ["MaxShipSize"] = 1000000
+        }
+    },
+    ["Lakeview Docks"] = {
+        ["PortConfig"] = {
+            ["ProfitFactor"] = 1.1,
+            ["ImageID"] = 6258578614,
+            ["Description"] = "",
+            ["MaxShipSize"] = 157000
+        }
+    },
+    ["Newport"] = {
+        ["PortConfig"] = {
+            ["ProfitFactor"] = 1,
+            ["ImageID"] = 9807450141,
+            ["Description"] = "A small port that has nothing too special about it. This port used to have a limitless supply of oil until it suddenly all ran out and had to start importing Oil.",
+            ["MaxShipSize"] = 1000000
+        }
+    },
+    ["Jamestown"] = {
+        ["PortConfig"] = {
+            ["ProfitFactor"] = 1.1,
+            ["ImageID"] = 6258578614,
+            ["Description"] = "",
+            ["MaxShipSize"] = 157000
+        }
+    },
+    ["Davenport"] = {
+        ["PortConfig"] = {
+            ["ProfitFactor"] = 1,
+            ["ImageID"] = 6421191360,
+            ["Description"] = "An extremely cold port, that is always covered in ice and snow all year round. This port is also rich in gas, with some of the lowest gas prices in the game.",
+            ["MaxShipSize"] = 1000000
+        }
+    },
+    ["Long Island"] = {
+        ["PortConfig"] = {
+            ["ProfitFactor"] = 1,
+            ["ImageID"] = 8037578936,
+            ["Description"] = "A port situated on a very long island which gives it its name. A relatively big port given the size of the island.",
+            ["MaxShipSize"] = 1000000
+        }
+    },
+    ["Ocean Fall Port"] = {
+        ["PortConfig"] = {
+            ["ProfitFactor"] = 0.87,
+            ["ImageID"] = 12120225227,
+            ["Description"] = "A tropical port located between 2 active Volcanos being the largest mountains in Shipping Lanes. Formally Ocean Fall Port was a small port, but has grown over the years with the threat of a eruption always imminent. Port does not the best paying loads per NM out there.",
+            ["MaxShipSize"] = 1000000
+        }
+    },
+    ["Newhaven Harbor"] = {
+        ["PortConfig"] = {
+            ["ProfitFactor"] = 0.95,
+            ["ImageID"] = 6984493448,
+            ["Description"] = "One of the largest ports in the game, this port is also extremely popular with merchant ships due to its high profitability. Also features a refinery which can refine oil into fuel. ",
+            ["MaxShipSize"] = 1000000
+        }
+    }
+}
