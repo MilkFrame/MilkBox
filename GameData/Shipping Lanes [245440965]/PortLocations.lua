20062e5bcbec0429f4ba175be26ff1a87d286667
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-05-12 02:48:57 +1000

    Create PortLocations.lua

---
+++ GameData/Shipping Lanes [245440965]/PortLocations.lua
@@ -0,0 +1,29 @@
+data = {
+    ["CalculateDistance"] = "function: 0x49c0098f9485ce6e",
+    ["GetPortInfo"] = "function: 0x8f0269b12264435e",
+    ["PortOffsets"] = {["15485663462"] = Vector3.new(0, 0, -120000)},
+    ["CenterPoint"] = "245440965",
+    ["CalculateDistanceNM"] = "function: 0x82e0044090dbf1be",
+    ["Ports"] = {
+        ["245440965"] = {
+            ["Emerald Harbor"] = {["DWT"] = 1000000, ["ProfitFactor"] = 1, ["Pos"] = Vector3.new(11602, 0, -20453)},
+            ["Norfolk Port"] = {["DWT"] = 1000000, ["ProfitFactor"] = 0.95, ["Pos"] = Vector3.new(22222, 0, 25189)},
+            ["Davenport"] = {["DWT"] = 1000000, ["ProfitFactor"] = 1, ["Pos"] = Vector3.new(-4756, 0, 23165)},
+            ["Bayview Port"] = {["DWT"] = 51000, ["ProfitFactor"] = 1.3, ["Pos"] = Vector3.new(25426, 0, -3509)},
+            ["Ocean Fall Port"] = {["DWT"] = 1000000, ["ProfitFactor"] = 0.87, ["Pos"] = Vector3.new(16455, 0, -45080)},
+            ["Stanley Harbor"] = {["DWT"] = 1000000, ["ProfitFactor"] = 1, ["Pos"] = Vector3.new(-8, 0, 6975)},
+            ["Newport"] = {["DWT"] = 1000000, ["ProfitFactor"] = 1, ["Pos"] = Vector3.new(7062, 0, 13864)},
+            ["Lakeview Docks"] = {["DWT"] = 157000, ["ProfitFactor"] = 1.1, ["Pos"] = Vector3.new(7243, 0, 830)},
+            ["Jamestown"] = {["DWT"] = 157000, ["ProfitFactor"] = 1.1, ["Pos"] = Vector3.new(-7789, 0, 7084)},
+            ["Woodside Inlet"] = {["DWT"] = 50000, ["ProfitFactor"] = 1.3, ["Pos"] = Vector3.new(-33910, 0, -2300)},
+            ["Long Island"] = {["DWT"] = 1000000, ["ProfitFactor"] = 1, ["Pos"] = Vector3.new(14512, 0, 3960)},
+            ["Newhaven Harbor"] = {["DWT"] = 1000000, ["ProfitFactor"] = 0.95, ["Pos"] = Vector3.new(-23936, 0, -1930)},
+            ["Rockfall Port"] = {["DWT"] = 1000000, ["ProfitFactor"] = 1, ["Pos"] = Vector3.new(-4758, 0, 1774)}
+        },
+        ["15485663462"] = {
+            ["Wilmington Cove"] = {["DWT"] = 1000000, ["ProfitFactor"] = 1, ["Pos"] = Vector3.new(7155, 0, 768)},
+            ["Midway Harbor"] = {["DWT"] = 1000000, ["ProfitFactor"] = 1, ["Pos"] = Vector3.new(-40232, 0, 54234)}
+        }
+    },
+    ["GetAllPorts"] = "function: 0x224f5874d1c2c4ce"
+}
