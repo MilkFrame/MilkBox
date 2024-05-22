1f8bae75cad2079760044c0dca9c9acd8a735e93
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-05-23 01:56:16 +1000

    Create LevelMilestones.lua

---
+++ GameData/Club Iris [2228128339]/LevelMilestones.lua
@@ -0,0 +1,52 @@
+data = {
+    {
+        ["Font"] = Enum.Font.SourceSans,
+        ["StrokeTransparency"] = 0.75,
+        ["StrokeColor"] = Color3.fromRGB(0, 0, 0),
+        ["Level"] = 0,
+        ["UseGradient"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 255, 255)
+    },
+    {
+        ["Font"] = Enum.Font.SourceSans,
+        ["StrokeTransparency"] = 0.75,
+        ["StrokeColor"] = Color3.fromRGB(0, 0, 0),
+        ["Level"] = 500,
+        ["TextColor"] = Color3.fromRGB(217, 123, 255)
+    },
+    {
+        ["Font"] = Enum.Font.Bangers,
+        ["StrokeTransparency"] = 0.75,
+        ["StrokeColor"] = Color3.fromRGB(0, 0, 0),
+        ["Level"] = 100000,
+        ["TextColor"] = Color3.fromRGB(255, 255, 255)
+    },
+    {
+        ["Font"] = Enum.Font.Bangers,
+        ["StrokeTransparency"] = 0,
+        ["StrokeColor"] = Color3.fromRGB(179, 179, 179),
+        ["Level"] = 50000,
+        ["TextColor"] = Color3.fromRGB(0, 0, 0)
+    },
+    {
+        ["Font"] = Enum.Font.SourceSans,
+        ["StrokeTransparency"] = 0.75,
+        ["StrokeColor"] = Color3.fromRGB(0, 0, 0),
+        ["Level"] = 5000,
+        ["TextColor"] = Color3.fromRGB(69, 255, 49)
+    },
+    {
+        ["Font"] = Enum.Font.SourceSans,
+        ["StrokeTransparency"] = 0.75,
+        ["StrokeColor"] = Color3.fromRGB(0, 0, 0),
+        ["Level"] = 1000,
+        ["TextColor"] = Color3.fromRGB(120, 232, 255)
+    },
+    {
+        ["Font"] = Enum.Font.SourceSansSemibold,
+        ["StrokeTransparency"] = 0.75,
+        ["StrokeColor"] = Color3.fromRGB(0, 0, 0),
+        ["Level"] = 10000,
+        ["TextColor"] = Color3.fromRGB(255, 0, 0)
+    }
+}
