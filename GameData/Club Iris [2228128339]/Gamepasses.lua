376369f2aec5db4985f89fb85ed1a6490ec3541c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-05-23 01:58:31 +1000

    Create Gamepasses.lua

---
+++ GameData/Club Iris [2228128339]/Gamepasses.lua
@@ -0,0 +1,292 @@
+data = {
+    {
+        ["Description"] = "Gain exclusive access to our upstairs lounge and other perks with our VIP gamepass!",
+        ["MarketInfo"] = {
+            ["AssetTypeId"] = 0,
+            ["Description"] = [[🏅 Wanna be the VIP of the club?
+😊 Now you can be with the VIP gamepass. Owning this proves you're the best of the best, and you're given a special VIP loft to oversee the club with an outstanding overhead view. (More features coming soon)
+
+Join Infamous Productions to be part of the world!
+https://www.roblox.com/My/Groups.aspx?gid=4360726]],
+            ["IsNew"] = false,
+            ["Updated"] = "2019-09-23T04:38:50.293Z",
+            ["AssetId"] = 0,
+            ["ProductId"] = 368493737,
+            ["MinimumMembershipLevel"] = 0,
+            ["Created"] = "2018-09-03T21:46:13.693Z",
+            ["ProductType"] = "Game Pass",
+            ["IsLimited"] = false,
+            ["Sales"] = 0,
+            ["IconImageAssetId"] = 4819964918,
+            ["IsPublicDomain"] = false,
+            ["PriceInRobux"] = 75,
+            ["Name"] = "VIP",
+            ["IsForSale"] = true,
+            ["Creator"] = {
+                ["Id"] = 721113956,
+                ["CreatorType"] = "Group",
+                ["Name"] = "Infamous Productions",
+                ["CreatorTargetId"] = 4360726
+            },
+            ["TargetId"] = 5084124,
+            ["IsLimitedUnique"] = false
+        },
+        ["PayInRobux"] = true,
+        ["Name"] = "VIP",
+        ["GamepassId"] = 5084124
+    },
+    {
+        ["Description"] = "Gain exclusive access to the Club Iris lounge located below the dance floor!",
+        ["MarketInfo"] = {
+            ["AssetTypeId"] = 0,
+            ["Description"] = [[🥇 Wanna have a special room dedicated to you and your friends?
+😊 Now you can with the Lounge Access gamepass. It gives you access to the room under the dance floor! The game owner is seen under here often which gives you a high chance of meeting/talking to him!
+
+Join Infamous Productions to be part of the world!
+https://www.roblox.com/My/Groups.aspx?gid=4360726]],
+            ["IsNew"] = false,
+            ["Updated"] = "2019-09-23T04:39:51.267Z",
+            ["AssetId"] = 0,
+            ["ProductId"] = 370496808,
+            ["MinimumMembershipLevel"] = 0,
+            ["Created"] = "2018-09-06T03:06:35.89Z",
+            ["ProductType"] = "Game Pass",
+            ["IsLimited"] = false,
+            ["Sales"] = 0,
+            ["IconImageAssetId"] = 4819735973,
+            ["IsPublicDomain"] = false,
+            ["PriceInRobux"] = 85,
+            ["Name"] = "Lounge Access",
+            ["IsForSale"] = true,
+            ["Creator"] = {
+                ["Id"] = 721113956,
+                ["CreatorType"] = "Group",
+                ["Name"] = "Infamous Productions",
+                ["CreatorTargetId"] = 4360726
+            },
+            ["TargetId"] = 5093275,
+            ["IsLimitedUnique"] = false
+        },
+        ["PayInRobux"] = true,
+        ["Name"] = "Lounge Access",
+        ["GamepassId"] = 5093275
+    },
+    {
+        ["Description"] = "Customize your character's nametag with custom emojis and colors!",
+        ["MarketInfo"] = {
+            ["AssetTypeId"] = 0,
+            ["Description"] = [[🌟 Want your tag to stand out?
+😊 Now you can! With the Tag Customs pass you'll be able to add emojis to your name and change your level & name color to preset colors, and even a custom color wheel for infinite customization!]],
+            ["IsNew"] = false,
+            ["Updated"] = "2021-04-04T19:43:20.76Z",
+            ["AssetId"] = 0,
+            ["ProductId"] = 1148216393,
+            ["MinimumMembershipLevel"] = 0,
+            ["Created"] = "2021-02-04T09:34:37.35Z",
+            ["ProductType"] = "Game Pass",
+            ["IsLimited"] = false,
+            ["Sales"] = 0,
+            ["IconImageAssetId"] = 6615944822,
+            ["IsPublicDomain"] = false,
+            ["PriceInRobux"] = 199,
+            ["Name"] = "Tag Customs",
+            ["IsForSale"] = true,
+            ["Creator"] = {
+                ["Id"] = 721113956,
+                ["CreatorType"] = "Group",
+                ["Name"] = "Infamous Productions",
+                ["CreatorTargetId"] = 4360726
+            },
+            ["TargetId"] = 14656878,
+            ["IsLimitedUnique"] = false
+        },
+        ["PayInRobux"] = true,
+        ["Name"] = "Tag Customs",
+        ["GamepassId"] = 14656878
+    },
+    {
+        ["Description"] = "Stand out from the crowd with Club Iris' customizable glow bands!",
+        ["MarketInfo"] = {
+            ["AssetTypeId"] = 0,
+            ["Description"] = [[⭐ Wanna be the brightest person around?
+😊 Now you can be with the exclusive glow bands gamepass. It lets you chose from EVERY color you could imagine, along with a custom RGB input for the crafty few looking for even more customization.
+
+🌌 Join Infamous Productions to be part of the world!
+https://www.roblox.com/My/Groups.aspx?gid=4360726]],
+            ["IsNew"] = false,
+            ["Updated"] = "2020-07-17T21:45:39.603Z",
+            ["AssetId"] = 0,
+            ["ProductId"] = 367598915,
+            ["MinimumMembershipLevel"] = 0,
+            ["Created"] = "2018-09-02T22:37:07.897Z",
+            ["ProductType"] = "Game Pass",
+            ["IsLimited"] = false,
+            ["Sales"] = 0,
+            ["IconImageAssetId"] = 5363721483,
+            ["IsPublicDomain"] = false,
+            ["PriceInRobux"] = 200,
+            ["Name"] = "Glow Bands",
+            ["IsForSale"] = true,
+            ["Creator"] = {
+                ["Id"] = 721113956,
+                ["CreatorType"] = "Group",
+                ["Name"] = "Infamous Productions",
+                ["CreatorTargetId"] = 4360726
+            },
+            ["TargetId"] = 5078977,
+            ["IsLimitedUnique"] = false
+        },
+        ["PayInRobux"] = true,
+        ["Name"] = "Glow Bands",
+        ["GamepassId"] = 5078977
+    },
+    {
+        ["Description"] = "Level up three times as fast as you normally would!",
+        ["MarketInfo"] = {
+            ["AssetTypeId"] = 0,
+            ["Description"] = [[⏫Want to level up THREE times as fast?
+🎖️ Now you can with this gamepass, you'll be able to reach the higher tiers of the levels leaderboard easier and it'll upgrade your level rate from 1x to 3x instantly!]],
+            ["IsNew"] = false,
+            ["Updated"] = "2021-02-02T10:44:58.54Z",
+            ["AssetId"] = 0,
+            ["ProductId"] = 1147729633,
+            ["MinimumMembershipLevel"] = 0,
+            ["Created"] = "2021-02-02T09:51:45.923Z",
+            ["ProductType"] = "Game Pass",
+            ["IsLimited"] = false,
+            ["Sales"] = 0,
+            ["IconImageAssetId"] = 6335104441,
+            ["IsPublicDomain"] = false,
+            ["PriceInRobux"] = 299,
+            ["Name"] = "Triple Level Rate",
+            ["IsForSale"] = true,
+            ["Creator"] = {
+                ["Id"] = 721113956,
+                ["CreatorType"] = "Group",
+                ["Name"] = "Infamous Productions",
+                ["CreatorTargetId"] = 4360726
+            },
+            ["TargetId"] = 14604373,
+            ["IsLimitedUnique"] = false
+        },
+        ["PayInRobux"] = true,
+        ["Name"] = "Triple Level Rate",
+        ["GamepassId"] = 14604373
+    },
+    {
+        ["Description"] = "Want to be able to party with your friends without disruptance? Try out our hotel rooms!",
+        ["MarketInfo"] = {
+            ["AssetTypeId"] = 0,
+            ["Description"] = [[🏨 Want a hotel of your own? 
+😊 Want to be able to party with your friends? Purchasing this game-pass will allow access to one hotel room of YOUR choice! Have a blast and bring whoever you want, with you inside of the hotel!
+
+🌌 Join Infamous Productions to be part of the world!
+https://www.roblox.com/My/Groups.aspx?gid=4360726]],
+            ["IsNew"] = false,
+            ["Updated"] = "2019-09-23T04:39:29.327Z",
+            ["AssetId"] = 0,
+            ["ProductId"] = 367758494,
+            ["MinimumMembershipLevel"] = 0,
+            ["Created"] = "2018-09-03T02:02:27.047Z",
+            ["ProductType"] = "Game Pass",
+            ["IsLimited"] = false,
+            ["Sales"] = 0,
+            ["IconImageAssetId"] = 4819962826,
+            ["IsPublicDomain"] = false,
+            ["PriceInRobux"] = 325,
+            ["Name"] = "Iris Hotel",
+            ["IsForSale"] = true,
+            ["Creator"] = {
+                ["Id"] = 721113956,
+                ["CreatorType"] = "Group",
+                ["Name"] = "Infamous Productions",
+                ["CreatorTargetId"] = 4360726
+            },
+            ["TargetId"] = 5079672,
+            ["IsLimitedUnique"] = false
+        },
+        ["PayInRobux"] = true,
+        ["Name"] = "Iris Hotel",
+        ["GamepassId"] = 5079672
+    },
+    {
+        ["Description"] = "Earn gold tier rewards for every daily login!",
+        ["MarketInfo"] = {
+            ["AssetTypeId"] = 0,
+            ["Description"] = [[💰 Wanna be apart of the 1% in Club Iris and reap the rewards?
+😊 Well now you can, this time without worrying about finances because this gamepass grants you a $60 daily login bonus (Upgraded from the standard $15 and $30)]],
+            ["IsNew"] = false,
+            ["Updated"] = "2020-01-27T21:50:46.203Z",
+            ["AssetId"] = 0,
+            ["ProductId"] = 577612987,
+            ["MinimumMembershipLevel"] = 0,
+            ["Created"] = "2019-06-21T09:13:03.5Z",
+            ["ProductType"] = "Game Pass",
+            ["IsLimited"] = false,
+            ["Sales"] = 0,
+            ["IconImageAssetId"] = 4820055123,
+            ["IsPublicDomain"] = false,
+            ["PriceInRobux"] = 349,
+            ["Name"] = "Gold Tier Login Bonus",
+            ["IsForSale"] = true,
+            ["Creator"] = {
+                ["Id"] = 721113956,
+                ["CreatorType"] = "Group",
+                ["Name"] = "Infamous Productions",
+                ["CreatorTargetId"] = 4360726
+            },
+            ["TargetId"] = 6622716,
+            ["IsLimitedUnique"] = false
+        },
+        ["PayInRobux"] = true,
+        ["Name"] = "Gold Tier Login Bonus",
+        ["GamepassId"] = 6622716
+    },
+    {
+        ["Description"] = "Use exclusive and fun commands to improve your experience!",
+        ["MarketInfo"] = {
+            ["AssetTypeId"] = 0,
+            ["Description"] = [[⭐ Iris Commands, fun, useful, and easy to use.
+⚠️ NOTE: These are NOT admin commands, they are just fun/useful player commands.
+
+📜!cmds - View this commands list in-game
+🎃 !headless / !unheadless - Gives/removes a headless head from your character.
+🦿 !korblox / !unkorblox - Gives/removes a korblox leg from your character.
+🔄 !re - Refreshes your character.
+🎭 !char (name) / !unchar - Chars you into the specified player.
+🎩 !hat (id) - Gives your player the specified hat.
+❌ !removehats - Removes all accessories
+😋 !face (id) - Gives your player the specified face
+👕 !shirt (id) - Gives your player the specified shirt
+🩳 !pants (id) - Gives your player the specified pants
+🔥 !fire (color) / !unfire - Gives your player fire
+✨!sparkles (color) / !unsparkles - Gives your player sparkles
+⬆️!scale (0.4 - 1.5) - Sets your avatar scale to the specified number]],
+            ["IsNew"] = false,
+            ["Updated"] = "2021-03-08T23:49:26.503Z",
+            ["AssetId"] = 0,
+            ["ProductId"] = 1156630935,
+            ["MinimumMembershipLevel"] = 0,
+            ["Created"] = "2021-03-08T23:04:41.407Z",
+            ["ProductType"] = "Game Pass",
+            ["IsLimited"] = false,
+            ["Sales"] = 0,
+            ["IconImageAssetId"] = 6493057600,
+            ["IsPublicDomain"] = false,
+            ["PriceInRobux"] = 499,
+            ["Name"] = "Iris Commands",
+            ["IsForSale"] = true,
+            ["Creator"] = {
+                ["Id"] = 721113956,
+                ["CreatorType"] = "Group",
+                ["Name"] = "Infamous Productions",
+                ["CreatorTargetId"] = 4360726
+            },
+            ["TargetId"] = 15630361,
+            ["IsLimitedUnique"] = false
+        },
+        ["PayInRobux"] = true,
+        ["Name"] = "Iris Commands",
+        ["GamepassId"] = 15630361
+    }
+}
