cbf4f8c75561edb110fb4ae239b3752ce8130f75
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-05-23 01:59:17 +1000

    Create Settings.lua

---
+++ GameData/Club Iris [2228128339]/Settings.lua
@@ -0,0 +1,26 @@
+data = {
+    ["Club Music"] = {
+        ["Enabled"] = true,
+        ["Description"] = "Toggle this option to enable club music.",
+        ["Name"] = "Club Music",
+        ["ClientRan"] = true
+    },
+    ["Youtuber Mode"] = {
+        ["Enabled"] = false,
+        ["Description"] = "Enabling Youtuber Mode will toggle off certain sensitive details and other information in regards to player names.",
+        ["Name"] = "Youtuber Mode",
+        ["ClientRan"] = true
+    },
+    ["Low Graphics"] = {
+        ["Enabled"] = false,
+        ["Description"] = "Toggling this option will disable high-end device detailing.",
+        ["Name"] = "Low Graphics",
+        ["ClientRan"] = true
+    },
+    ["Interface Sounds"] = {
+        ["Enabled"] = true,
+        ["Description"] = "Toggle this option to enable interface sounds.",
+        ["Name"] = "Interface Sounds",
+        ["ClientRan"] = true
+    }
+}
