1ea00940fbbb1560ef27331afad69ef44a4cec67
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-05-23 01:59:40 +1000

    Create Items.lua

---
+++ GameData/Club Iris [2228128339]/Items.lua
@@ -0,0 +1,541 @@
+--game.ReplicatedStorage:FindFirstChild("GetItems",true):InvokeServer()
+data = {
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Go above and beyond, floating around the map with this balloon!",
+        ["IsEquippable"] = true,
+        ["Name"] = "Balloon",
+        ["Price"] = 699,
+        ["Id"] = 1
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Drink your bloxy cola before it gets too flat!",
+        ["IsEquippable"] = true,
+        ["Name"] = "Bloxy Cola",
+        ["Price"] = 29,
+        ["Id"] = 2
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Served fresh out of the Club Iris grillhouse!",
+        ["IsEquippable"] = true,
+        ["Name"] = "Burger",
+        ["Price"] = 59,
+        ["Id"] = 3
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Ribbit! Ribbit!",
+        ["IsEquippable"] = true,
+        ["Name"] = "Froggy Plush",
+        ["Price"] = 199,
+        ["Id"] = 4
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Emerse yourself with some neon light action!",
+        ["IsEquippable"] = true,
+        ["Name"] = "Glow Sticks",
+        ["Price"] = 2199,
+        ["Id"] = 5
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "The ultimate flex item to buy in Club Iris..",
+        ["IsEquippable"] = true,
+        ["Name"] = "Golden Hoverboard",
+        ["Price"] = 3899,
+        ["Id"] = 6
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Try not to ride it like a skateboard!",
+        ["IsEquippable"] = true,
+        ["Name"] = "Hoverboard",
+        ["Price"] = 2499,
+        ["Id"] = 7
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Go under the radar and spook your friends!",
+        ["IsEquippable"] = true,
+        ["Name"] = "Invisible Cape",
+        ["Price"] = 999,
+        ["Id"] = 8
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "For when you have a groggy morning.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Latte",
+        ["Price"] = 69,
+        ["Id"] = 9
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Panda! Panda! Panda! Panda!",
+        ["IsEquippable"] = true,
+        ["Name"] = "Panda Friend",
+        ["Price"] = 149,
+        ["Id"] = 10
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "I... AM... SPEED...",
+        ["IsEquippable"] = true,
+        ["Name"] = "Speed Coil",
+        ["Price"] = 499,
+        ["Id"] = 11
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "It doesn't talk and it's not friends with Mark Wahlberg.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Teddy Bear",
+        ["Price"] = 149,
+        ["Id"] = 12
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "I got beans, greens, potatoes, tomatoes..",
+        ["IsEquippable"] = true,
+        ["Name"] = "Turkey Leg",
+        ["Price"] = 59,
+        ["Id"] = 13
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Murica'",
+        ["IsEquippable"] = true,
+        ["Name"] = "American Flag",
+        ["Price"] = 49,
+        ["Id"] = 14
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Welcome to Jurassic Park!",
+        ["IsEquippable"] = true,
+        ["Name"] = "Blue Dino Plush",
+        ["Price"] = 249,
+        ["Id"] = 15
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "The cure to everything.. chocolate milk.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Chocolate Milk",
+        ["Price"] = 49,
+        ["Id"] = 16
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Hey, look! It's King Harold from Shrek!",
+        ["IsEquippable"] = true,
+        ["Name"] = "Green Froggy Plush",
+        ["Price"] = 99,
+        ["Id"] = 17
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Brain freeze.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Iris Slushie",
+        ["Price"] = 59,
+        ["Id"] = 18
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Spooky season is upon us.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Jack O' Lantern",
+        ["Price"] = 249,
+        ["Id"] = 19
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "An exclusive Pride Month item.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Pride Flag",
+        ["Price"] = 49,
+        ["Id"] = 20
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Don't let King Neptune make a burger!",
+        ["IsEquippable"] = true,
+        ["Name"] = "Shadow Trident",
+        ["Price"] = 999,
+        ["Id"] = 21
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Then you're left in the dust, unless I stuck by ya",
+        ["IsEquippable"] = true,
+        ["Name"] = "Sunflower Wand",
+        ["Price"] = 99,
+        ["Id"] = 22
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "If Smokey the Bear was a plushie.",
+        ["IsEquippable"] = true,
+        ["Name"] = "American Bear",
+        ["Price"] = 299,
+        ["Id"] = 23
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Celebrate Club Iris' anniversary!",
+        ["IsEquippable"] = true,
+        ["Name"] = "Birthday Balloon",
+        ["Id"] = 24,
+        ["OnSale"] = false
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Who knows? Maybe you'll summon the Easter Bunny.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Bunny Wand",
+        ["Price"] = 199,
+        ["Id"] = 25
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Elongated peppermint stick",
+        ["IsEquippable"] = true,
+        ["Name"] = "Candy Cane",
+        ["Price"] = 199,
+        ["Id"] = 26
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "You can't go wrong with an adorable hatchling.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Chick Plushie",
+        ["Price"] = 299,
+        ["Id"] = 27
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "An exclusive St. Patrick's Day item.",
+        ["Name"] = "Clover Wand",
+        ["IsEquippable"] = true,
+        ["Price"] = 299,
+        ["Id"] = 28,
+        ["OnSale"] = false
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "An exclusive Easter item.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Easter Bunny Plushie",
+        ["Price"] = 399,
+        ["Id"] = 29
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "An exclusive Valentine's Day item.",
+        ["Name"] = "Heart Balloon",
+        ["IsEquippable"] = true,
+        ["Price"] = 499,
+        ["Id"] = 30,
+        ["OnSale"] = false
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Just own the night like the 4th of July!",
+        ["IsEquippable"] = true,
+        ["Name"] = "July Sparkler",
+        ["Price"] = 99,
+        ["Id"] = 31
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "An exclusive Pride Month item.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Pride Wand",
+        ["Id"] = 32,
+        ["OnSale"] = false
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "An exclusive Pride Month item.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Rainbow Plush",
+        ["Id"] = 33,
+        ["OnSale"] = false
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Roses are red, violets are blue, Club Iris is the best, and so are you!",
+        ["Name"] = "Rose",
+        ["IsEquippable"] = true,
+        ["Price"] = 249,
+        ["Id"] = 34,
+        ["OnSale"] = false
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "An exclusive St. Patrick's Day item.",
+        ["Name"] = "Shamrock Shake",
+        ["IsEquippable"] = true,
+        ["Price"] = 199,
+        ["Id"] = 35,
+        ["OnSale"] = false
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "An exclusive St. Patrick's Day item.",
+        ["Name"] = "St. Patrick's Bear",
+        ["IsEquippable"] = true,
+        ["Price"] = 399,
+        ["Id"] = 36,
+        ["OnSale"] = false
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "An exclusive Fourth of July item.",
+        ["IsEquippable"] = true,
+        ["Name"] = "USA Wand",
+        ["Price"] = 149,
+        ["Id"] = 37
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "An exclusive Pride Month item.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Unicorn Plush",
+        ["Id"] = 38,
+        ["OnSale"] = false
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "An exclusive Valentine's Day item.",
+        ["Name"] = "Valentines Bear",
+        ["IsEquippable"] = true,
+        ["Price"] = 499,
+        ["Id"] = 39,
+        ["OnSale"] = false
+    },
+    {
+        ["Type"] = "Soda",
+        ["Description"] = "For a limited time, this magical beverage gives your character a random particle effect. Make a statement among your peers! This item can be used indefinitely, but it will not save when you exit the game.",
+        ["Price"] = 25,
+        ["Name"] = "Effect Soda",
+        ["Id"] = 40
+    },
+    {
+        ["Type"] = "Soda",
+        ["Description"] = "For a limited period, this energizing beverage gives your character a significant gravity reduction. Is it true that you're in space? This item can be used indefinitely, but it will not save when you exit the game.",
+        ["Price"] = 40,
+        ["Name"] = "Gravity Soda",
+        ["Id"] = 41
+    },
+    {
+        ["Type"] = "Soda",
+        ["Description"] = "This ordinary drink accomplishes nothing, but it is refreshing. Anyone for a Club Iris cola? This item can be used indefinitely, but it will not save when you exit the game.",
+        ["Price"] = 5,
+        ["Name"] = "Regular Soda",
+        ["Id"] = 42
+    },
+    {
+        ["Type"] = "Soda",
+        ["Description"] = "For a limited time, this strong-smelling drink gives your character a significant speed boost. This item can be used indefinitely, but it will not save when you exit the game.",
+        ["Price"] = 30,
+        ["Name"] = "Speed Soda",
+        ["Id"] = 43
+    },
+    {
+        ["Type"] = "Soda",
+        ["Description"] = "For a limited time, this violent drink gives your character an uncontrollable spin. Now is not the time to get too dizzy! This item can be used indefinitely, but it will not save when you exit the game.",
+        ["Price"] = 20,
+        ["Name"] = "Spin Soda",
+        ["Id"] = 44
+    },
+    {
+        ["Type"] = "Soda",
+        ["Description"] = "For a limited time, this potent drink offers your character teleporting skills. To warp to a location near you, click anywhere! This item can be used indefinitely, but it will not save when you exit the game.",
+        ["Price"] = 75,
+        ["Name"] = "Teleport Soda",
+        ["Id"] = 45
+    },
+    {
+        ["Type"] = "Soda",
+        ["Description"] = "For a brief time, this strange-looking drink turns your character into a zombie. No one should be eaten! This item can be used indefinitely, but it will not save when you exit the game. (This beverage prevents you from infecting others.)",
+        ["Price"] = 40,
+        ["Name"] = "Zombie Soda",
+        ["Id"] = 46
+    },
+    {["Type"] = "Sword", ["Name"] = "Aqua", ["Price"] = 2249, ["Id"] = 47, ["IsEquippable"] = true},
+    {["Type"] = "Sword", ["Name"] = "Blue Energy Sword", ["Price"] = 1249, ["Id"] = 48, ["IsEquippable"] = true},
+    {["Type"] = "Sword", ["Name"] = "Blue Katana", ["Price"] = 75, ["Id"] = 49, ["IsEquippable"] = true},
+    {["Type"] = "Sword", ["Name"] = "Crescendo", ["Price"] = 2249, ["Id"] = 50, ["IsEquippable"] = true},
+    {["Type"] = "Sword", ["Name"] = "Darkheart", ["Price"] = 499, ["Id"] = 51, ["IsEquippable"] = true},
+    {["Type"] = "Sword", ["Name"] = "Golden Katana", ["Price"] = 299, ["Id"] = 52, ["IsEquippable"] = true},
+    {["Type"] = "Sword", ["Name"] = "Green Katana", ["Price"] = 75, ["Id"] = 53, ["IsEquippable"] = true},
+    {["Type"] = "Sword", ["Name"] = "Illumina", ["Price"] = 499, ["Id"] = 54, ["IsEquippable"] = true},
+    {["Type"] = "Sword", ["Name"] = "Noctis", ["Price"] = 2249, ["Id"] = 55, ["IsEquippable"] = true},
+    {["Type"] = "Sword", ["Name"] = "Red Energy Sword", ["Price"] = 1249, ["Id"] = 56, ["IsEquippable"] = true},
+    {["Type"] = "Sword", ["Name"] = "Red Katana", ["Price"] = 75, ["Id"] = 57, ["IsEquippable"] = true},
+    {
+        ["Type"] = "Gear",
+        ["OnSale"] = false,
+        ["Name"] = "AFK Sign",
+        ["IsEquippable"] = true,
+        ["IsSpecialItem"] = true,
+        ["Id"] = 58
+    },
+    {
+        ["Type"] = "Gear",
+        ["OnSale"] = false,
+        ["Name"] = "Influencer Star",
+        ["IsEquippable"] = true,
+        ["IsSpecialItem"] = true,
+        ["Id"] = 59
+    },
+    {
+        ["Type"] = "Gear",
+        ["OnSale"] = false,
+        ["Name"] = "Youtuber Plush",
+        ["IsEquippable"] = true,
+        ["IsSpecialItem"] = true,
+        ["Id"] = 60
+    },
+    {
+        ["Type"] = "Gear",
+        ["OnSale"] = false,
+        ["Name"] = "Iris Bear",
+        ["IsEquippable"] = true,
+        ["IsSpecialItem"] = true,
+        ["Id"] = 61
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "A delicious hot chocolate.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Hot Chocolate",
+        ["Price"] = 249,
+        ["Id"] = 62
+    },
+    {
+        ["Type"] = "Gear",
+        ["OnSale"] = false,
+        ["Name"] = "Super Speed Coil",
+        ["IsEquippable"] = true,
+        ["IsSpecialItem"] = true,
+        ["Id"] = 63
+    },
+    {
+        ["Type"] = "Gear",
+        ["OnSale"] = false,
+        ["Name"] = "Steampunk Glove",
+        ["IsEquippable"] = true,
+        ["IsSpecialItem"] = true,
+        ["Id"] = 64
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "A flag of Ireland.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Irish Flag",
+        ["Price"] = 49,
+        ["Id"] = 65
+    },
+    {
+        ["Type"] = "Gear",
+        ["OnSale"] = false,
+        ["Name"] = "Laser Pointers",
+        ["IsEquippable"] = true,
+        ["IsSpecialItem"] = true,
+        ["Id"] = 66
+    },
+    {
+        ["Type"] = "Gear",
+        ["OnSale"] = false,
+        ["Name"] = "Decal Sign",
+        ["IsEquippable"] = true,
+        ["IsSpecialItem"] = true,
+        ["Id"] = 67
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Get a guitar for your character to rock on!",
+        ["IsEquippable"] = true,
+        ["Name"] = "Acoustic Guitar",
+        ["Price"] = 799,
+        ["Id"] = 68
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Brain freeze.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Blueberry Slushie",
+        ["Price"] = 79,
+        ["Id"] = 69
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Popcorn! Get 'ya popcorn here folks!",
+        ["IsEquippable"] = true,
+        ["Name"] = "Popcorn",
+        ["Price"] = 59,
+        ["Id"] = 70
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Are you gonna finish that... croissant?",
+        ["IsEquippable"] = true,
+        ["Name"] = "Croissant",
+        ["Price"] = 59,
+        ["Id"] = 71
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "A box of macarons.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Macaron Box",
+        ["Price"] = 59,
+        ["Id"] = 72
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "With a hint of vanilla.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Vanilla Milkshake",
+        ["Price"] = 79,
+        ["Id"] = 73
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "With a hint of chocolate.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Chocolate Milkshake",
+        ["Price"] = 79,
+        ["Id"] = 74
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "MIC UP!",
+        ["IsEquippable"] = true,
+        ["Name"] = "Microphone",
+        ["Price"] = 299,
+        ["Id"] = 75
+    },
+    {
+        ["Type"] = "Gear",
+        ["Description"] = "Your emo friend.",
+        ["IsEquippable"] = true,
+        ["Name"] = "Emo Bear",
+        ["Price"] = 149,
+        ["Id"] = 76
+    },
+    {
+        ["Type"] = "Gear",
+        ["OnSale"] = false,
+        ["Name"] = "Kasha",
+        ["IsEquippable"] = true,
+        ["IsSpecialItem"] = true,
+        ["Id"] = 77
+    }
+}
