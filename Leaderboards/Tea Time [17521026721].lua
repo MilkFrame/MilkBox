4b675fa597bfd20c01a825d0e09ddb5ed4b3a2b8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-10 01:01:36 +1000

    Create Tea Time [17521026721].lua

---
+++ Leaderboards/Tea Time [17521026721].lua
@@ -0,0 +1,124 @@
+--7 June 2024 4:00pm
+data = {
+    ["All-Time Points"] = {
+        {1, "zvvlt", "111.28k", 128732325},
+        {2, "Accrelic", "48.25k", 662158304},
+        {3, "zeroshirome555", "41.30k", 4586702754},
+        {4, "ttunknownN", "36.84k", 4599996397},
+        {5, "He_beeee", "35.44k", 4913031010},
+        {6, "antisocietys", "33.67k", 3134801146},
+        {7, "Aito_kun2451", "31.76k", 2821724894},
+        {8, "Nyanuiu", "31.06k", 4010500598},
+        {9, "l1lyl0pez", "29.00k", 1443323867},
+        {10, "IamMellowie", "27.51k", 2427700319},
+        {11, "wiulo0m", "27.42k", 3713327860},
+        {12, "marshmallowwyy", "23.89k", 501835684},
+        {13, "nekocat1817", "22.42k", 106155372},
+        {14, "CosmicGlaceon", "18.53k", 504881763},
+        {15, "nyazeu", "18.23k", 1302637611},
+        {16, "Tempeyra", "17.73k", 118470331},
+        {17, "Leigha0408", "16.75k", 2042050726},
+        {18, "Milkkzways", "16.21k", 566243515},
+        {19, "urlocalmonke06", "16.17k", 3031049574},
+        {20, "ScarlettVonHex", "15.99k", 794240470},
+        {21, "dotmrjosh", "15.51k", 5183556237},
+        {22, "slimey136", "15.35k", 474578218},
+        {23, "afloralmess", "14.96k", 1279354973},
+        {24, "HeavensThanatos", "14.76k", 1026825501},
+        {25, "Camilanesas_com", "13.45k", 3622052035},
+        {26, "legendAce_Ess", "13.38k", 1297277518},
+        {27, "2000mad201B", "13.16k", 204800166},
+        {28, "kageharashui", "13.12k", 262729486},
+        {29, "Sharumaine", "13.02k", 64644044},
+        {30, "BazanAshrafi", "12.98k", 5112674336},
+        {31, "iiPotatoCone", "12.91k", 332700194},
+        {32, "ThePrismCube", "12.79k", 1143942367},
+        {33, "animllover1237", "12.59k", 2010430001},
+        {34, "kimmyprincessheart12", "12.53k", 327744095},
+        {35, "RobloxIzDaBest23", "12.52k", 4946615520},
+        {36, "TETEJIHAN", "12.29k", 2891537800},
+        {37, "Slayalldaypenny", "11.99k", 5264509480},
+        {38, "MathematicMatt2021", "11.98k", 1483591171},
+        {39, "o_Gyo", "11.89k", 109834001},
+        {40, "0xTaliyahh", "11.88k", 131407258},
+        {41, "Joelisstillhere", "11.81k", 5376119715},
+        {42, "Ralynn_0717", "11.57k", 6082426029},
+        {43, "crayonpudding", "11.45k", 200526143},
+        {44, "kellys_0311", "11.34k", 1529983950},
+        {45, "eggyeetaway", "11.16k", 656166913},
+        {46, "6789coolcat", "11.14k", 364713133},
+        {47, "Noceur_Star", "10.95k", 673454838},
+        {48, "HaoKeAii", "10.90k", 281684964},
+        {49, "EchoingForever", "10.81k", 4105053033},
+        {50, "moogoopooloo", "10.71k", 540649384}
+    },
+    ["All-Time Donations"] = {
+        {1, "PointMelon", "7.00k", 1903573421},
+        {2, "GabbyTheConqueror", "5.10k", 105712382},
+        {3, "DestroyerCam", "5.00k", 20052361},
+        {4, "Chahier", "5.00k", 330759580},
+        {5, "strayrew", "1.50k", 91329898},
+        {6, "KelvinBlues1", "1.40k", 8820949},
+        {7, "K4TSUK1XV", "1.10k", 1943942773},
+        {8, "kobecide", "800", 724416230},
+        {9, "Starnamics", "500", 259773550},
+        {10, "hiryoto_ex", "250", 2511517656},
+        {11, "boxyofcookies", "200", 1665939205},
+        {12, "Gunn_Wunn", "200", 3041069356},
+        {13, "WhisperedCeir", "150", 2951547646},
+        {14, "Del_Txco", "150", 589393918},
+        {15, "xXDark_BoudaXx", "150", 5321160174},
+        {16, "AsteriaNoon", "150", 3463647239},
+        {17, "coolman210007", "125", 55034265},
+        {18, "StinkALotW", "100", 5648633248},
+        {19, "andr3en87", "100", 168777271},
+        {20, "NotMysticTV", "100", 2217617558},
+        {21, "ILuvbobaXx_Xx", "100", 4724873463},
+        {22, "Enzlink", "100", 1610028685},
+        {23, "UriYeon", "100", 496754459},
+        {24, "Soldemedianoche96", "100", 4039352289},
+        {25, "atombla", "75", 119053677},
+        {26, "LyxYT12", "75", 1235861336},
+        {27, "mjrayyyy", "50", 196958292},
+        {28, "Leigha0408", "50", 2042050726},
+        {29, "Whiter_girl0311", "50", 1826660598},
+        {30, "smhhmaddieee", "50", 1582187642},
+        {31, "Kayyuhuh", "50", 2901732248},
+        {32, "treehousetapes", "50", 2217798589},
+        {33, "VaIerisu", "50", 1503944234},
+        {34, "Verppz", "50", 5068390796},
+        {35, "casarin135", "50", 1029727060},
+        {36, "vlrntn", "50", 3224220331},
+        {37, "SI33pykid", "50", 609472975},
+        {38, "DJ_MadameMachina", "50", 663672572},
+        {39, "Jsmith19699", "25", 5773482997},
+        {40, "jhopexu", "25", 79176654},
+        {41, "elizxa_San", "25", 1411369355},
+        {42, "cienna_kitty", "25", 872545022},
+        {43, "oWaterDuck", "25", 661142635},
+        {44, "FlyingUnicorn2018", "25", 474431631},
+        {45, "DudaxBalofinha", "25", 1186525865},
+        {46, "sadie8368", "25", 474862537},
+        {47, "m3rcurxy", "25", 174114197},
+        {48, "Miko22_U", "25", 2582631524},
+        {49, "Ryleelove156", "25", 2549837891},
+        {50, "SwitchToIncognito", "25", 2566375769}
+    }
+}
+o="{"
+function get(board,title)
+p={}
+for i=1,50 do
+v = game.Players.LocalPlayer.PlayerGui[board].Frame.ScrollingFrame["UserRank"..tostring(i)]
+id="" dd,d2=string.find(v.ImageLabel.Image,"%d+") if dd~=nil then
+id=tonumber(string.sub(v.ImageLabel.Image,dd,d2)) end
+ss = string.find(v.Username.Text,"%s+")
+username = string.sub(v.Username.Text,ss+1)
+table.insert(p,{i,username,v.Amount.Text,id})
+end
+table.sort(p,function(a,b) return a[1]<b[1] end)
+o = o .."[\""..title.."\"]="..table_to_string(p)..",\n"
+end
+get("PointsLeaderboard","All-Time Points")
+get("DonationLeaderboard","All-Time Donations")
+o=o.."}" setclipboard(o) print("Done")
