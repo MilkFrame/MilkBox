78c034fa19f22f698fae3cfff8a2144a3d643785
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-10 01:00:46 +1000

    Create Bangkok Dinner [16076105670].lua

---
+++ Leaderboards/Bangkok Dinner [16076105670].lua
@@ -0,0 +1,123 @@
+--7 June 2024 3:00pm
+data = {
+    ["Top Points"] = {
+        {1, "helloguys_TH", "1.02M", 1470268719},
+        {2, "BERSERK_1154", "581.68K", 5370704735},
+        {3, "NongTodjaa", "501.2K", 3262612878},
+        {4, "kantong1280", "455.82K", 2668822078},
+        {5, "NeilHorain", "435.65K", 1158261642},
+        {6, "OmboTheGreat", "424.05K", 491123369},
+        {7, "AzDreem1", "322.43K", 1705140797},
+        {8, "Aito_kun2451", "321.61K", 2821724894},
+        {9, "DullerBerry", "314.22K", 3197731087},
+        {10, "FrozenQueen644", "256.40K", 3302081062},
+        {11, "BazanAshrafi", "255.17K", 5112674336},
+        {12, "dreemuhz", "236.16K", 95562699},
+        {13, "Honda_da", "235.11K", 2340246189},
+        {14, "FrancescaGoFree69", "221.54K", 5231107250},
+        {15, "eieihahaz", "210.32K", 4059226336},
+        {16, "JameKyol", "208.70K", 510716152},
+        {17, "DevilJoyce10", "208.15K", 3194422162},
+        {18, "OLDCATZZZZ", "203.19K", 3978286924},
+        {19, "JR0GGY", "202.2K", 4384686271},
+        {20, "jamjamj8", "198.63K", 1038582429},
+        {21, "Xxx_GodGU", "191.45K", 2612990937},
+        {22, "traumagrl", "181.91K", 16270378},
+        {23, "yuttba_21", "181.00K", 4306890249},
+        {24, "Minky8970", "167.57K", 2757329191},
+        {25, "Donut_ls21", "162.07K", 2928610673},
+        {26, "Aikajung", "158.91K", 412925471},
+        {27, "Pavonrat_704", "156.55K", 4239055034},
+        {28, "AjirosUn002", "154.09K", 4224114480},
+        {29, "kpgpgamerz_1TH", "153.48K", 2509594537},
+        {30, "CelestialWixch", "152.7K", 4704440864},
+        {31, "glowinthedarkk_22", "147.26K", 5821716441},
+        {32, "WhiskerPlot", "142.62K", 608383342},
+        {33, "awes0mejz", "141.27K", 4842455912},
+        {34, "2000mad201B", "140K", 204800166},
+        {35, "kookop555", "138.51K", 290825050},
+        {36, "xxquix_x", "132.65K", 4163980527},
+        {37, "itmytann", "126.4K", 2835954406},
+        {38, "Areeyanee27", "120.2K", 1527046290},
+        {39, "rxiserae09", "118.42K", 2411961925},
+        {40, "aerinkilla", "115.65K", 2926199282},
+        {41, "Mikaelamasinag", "113.03K", 3234973374},
+        {42, "fistggez", "110.29K", 465706008},
+        {43, "Ploynarak55555", "109.79K", 5830649937},
+        {44, "Lorgung", "107.56K", 5783825683},
+        {45, "armysutart", "107.27K", 1273073447},
+        {46, "Zirawich", "105.96K", 1032774725},
+        {47, "zioyachan", "102.23K", 3849199109},
+        {48, "Venomskullie", "94.60K", 2871292900},
+        {49, "UDie405", "94.18K", 1047685001},
+        {50, "may250925", "94.15K", 3071445847}
+    },
+    ["Top Donators"] = {
+        {1, "Milkytillys", "45K", 140375977},
+        {2, "myusernameXD_01", "42.6K", 861393812},
+        {3, "N4Animation", "32.5K", 116174560},
+        {4, "MaoKuma_Z", "30K", 1387629860},
+        {5, "yerinetta", "20K", 62715994},
+        {6, "Q3Prototype", "12.5K", 283813432},
+        {7, "artsterm2", "6.27K", 1840908734},
+        {8, "Auuddy", "5K", 712699855},
+        {9, "notmks", "2.8K", 527459619},
+        {10, "Nm1pickme", "2.5K", 3370652723},
+        {11, "YoYoYo_rahh23", "2.5K", 6077151368},
+        {12, "agra_mane", "2.5K", 1075487231},
+        {13, "Studdious", "1.12K", 2659686253},
+        {14, "AlkalinElemental", "1K", 186469086},
+        {15, "CeruleanPisces", "700", 514807981},
+        {16, "LemonsOnTheChain", "625", 5225119330},
+        {17, "sakuya_GOD", "600", 2605699943},
+        {18, "Polyraris", "550", 759098623},
+        {19, "prampram", "525", 116762901},
+        {20, "Snowfoxau2221", "500", 1609763353},
+        {21, "Chxmei", "500", 1917581866},
+        {22, "hazelmasterclaw", "500", 3159371351},
+        {23, "MinniSupar", "500", 19190198},
+        {24, "Darkmatterwinston1", "325", 3331096171},
+        {25, "XDO027", "300", 2617712951},
+        {26, "Lady_I3lackRose", "275", 842288800},
+        {27, "firstggezso", "275", 3110410726},
+        {28, "P2013rem", "225", 2822269019},
+        {29, "Osaki_Fei", "200", 3013387505},
+        {30, "kalasho_thesecond", "200", 1580306176},
+        {31, "pondfary", "200", 117754186},
+        {32, "ashenyze", "200", 116233087},
+        {33, "Ukhita", "200", 100623578},
+        {34, "starshine0017", "200", 2316751980},
+        {35, "thitiphan999", "200", 1654705717},
+        {36, "abhishekshahi", "175", 86673747},
+        {37, "rino08163", "150", 4980987817},
+        {38, "Mosto_100", "150", 2761647894},
+        {39, "Coliw2345", "150", 2703095261},
+        {40, "xkfn007", "150", 4623672467},
+        {41, "x_PingH1gHz", "125", 842203843},
+        {42, "ItzMarlowReal", "125", 4299872739},
+        {43, "yosh1lnwza007", "125", 5277900014},
+        {44, "Sakusa_nami", "125", 2712098947},
+        {45, "Alicimavr", "125", 1934224255},
+        {46, "HenryEmily1955", "125", 133358911},
+        {47, "TenGamer259", "125", 153893591},
+        {48, "Thomas_Kitcat", "125", 3144973975},
+        {49, "TaffOat47", "125", 812462289},
+        {50, "Yono_Yamashi", "125", 112993529}
+    }
+}
+o="{"
+function get(board,title)
+p={}
+for i,v in pairs(game.Players.LocalPlayer.PlayerGui[board].Frame.List:GetChildren()) do
+if v.Name=="PlayerInfo" then
+id="" dd,d2=string.find(v.PlayerImage.Image,"%d+") if dd~=nil then
+id=tonumber(string.sub(v.PlayerImage.Image,dd,d2)) end
+table.insert(p,{tonumber(v.Rank.Text),v.Player.Text,v.Score.Text,id})
+end
+end
+table.sort(p,function(a,b) return a[1]<b[1] end)
+o = o .."[\""..title.."\"]="..table_to_string(p)..",\n"
+end
+get("Leaderboard","Top Points")
+get("Donators","Top Donators")
+o=o.."}" setclipboard(o) print("Done")
