6082027d9f0adefe05aaf0184ee0eaf2de16b5b9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-12 17:33:39 +1000

    Create VIPProducts.lua

---
+++ GameData/Dress To Impress [15101393044]/VIPProducts.lua
@@ -0,0 +1,9 @@
+data = {
+    ["Enums"] = {["Perm"] = 2, ["Monthly"] = 3, ["NotEnoughProps"] = 4, ["CanPurchase"] = 1},
+    ["Products"] = {
+        ["PermGift"] = {["Display"] = "VIP Permanent Gamepass Gift", ["ID"] = 1778174288},
+        ["MonthlyPass"] = {["Display"] = "VIP Monthly Subscription", ["ID"] = 1706713030},
+        ["PermGamepass"] = {["Display"] = "VIP Permanent Gamepass", ["ID"] = 647191068},
+        ["MonthlyGift"] = {["Display"] = "VIP Monthly Subscription Gift", ["ID"] = 1778174609}
+    }
+}
