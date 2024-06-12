a1ba265326b30ed6b02a6c05320e2f8c7e352d1f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-12 17:59:50 +1000

    Create Fanart.lua

---
+++ GameData/In Plain Sight 2 [2901172949]/Fanart.lua
@@ -0,0 +1,146 @@
+data = {
+    {"rbxgameasset://Images/chads", "ionutflameblade66"},
+    {"rbxgameasset://Images/IPS2_Fanart", "RealBlobTheBob"},
+    {"rbxgameasset://Images/ips2", "CRAZYDIAMONDDOO"},
+    {"rbxgameasset://Images/IMG_20191014_150114402", "Delvauxite"},
+    {"rbxassetid://7419460730", "FearlessMartinLOL"},
+    {"rbxassetid://9360402744", "raptorr23"},
+    {"rbxassetid://9369944664", "3210brian"},
+    {"rbxassetid://9377170768", "Jeffuse"},
+    {"rbxassetid://9380615371", "stammy26789"},
+    {"rbxassetid://9380638053", "OraCOjaVA"},
+    {"rbxassetid://9398086271", "stammy26789"},
+    {"rbxassetid://9398109688", "WenTingGamingYT"},
+    {"rbxassetid://9415428615", "3210brian"},
+    {"rbxassetid://9415437064", "OraCOjaVA"},
+    {"rbxassetid://9415451429", "SawdustSnifferx1"},
+    {"rbxassetid://9424354478", "raptorr23"},
+    {"rbxassetid://9424369671", "Jeffuse"},
+    {"rbxassetid://9469223999", "SawdustSnifferx1"},
+    {"rbxassetid://9520516030", "nautrikal"},
+    {"rbxassetid://9605986477", "fakerflakey"},
+    {"rbxassetid://9623026985", "fakerflakey"},
+    {"rbxassetid://9657849569", "fakerflakey"},
+    {"rbxassetid://9657875986", "raainby"},
+    {"rbxassetid://9666157587", "fakerflakey"},
+    {"rbxassetid://9726188746", "fakerflakey"},
+    {"rbxassetid://9726209281", "fakerflakey"},
+    {"rbxassetid://9726217500", "dankest_ac"},
+    {"rbxassetid://9726236567", "karta_animation12"},
+    {"rbxassetid://9765674601", "fakerflakey"},
+    {"rbxassetid://9765690415", "Tiventous"},
+    {"rbxassetid://9945257401", "GuyCalledPogger"},
+    {"rbxassetid://10059919214", "Hx_ru"},
+    {"rbxassetid://10059982525", "NoticeTheSenpai23232"},
+    {"rbxassetid://10157065057", "tigerluv615"},
+    {"rbxassetid://10157103199", "Hx_ru"},
+    {"rbxassetid://10235789504", "RavenAlphaRay"},
+    {"rbxassetid://10317963643", "chirstien0409"},
+    {"rbxassetid://10317997217", "fueledbydiesel"},
+    {"rbxassetid://11437085567", "alt3zt"},
+    {"rbxassetid://11437097079", "DiamondGoldAndSilver"},
+    {"rbxassetid://11437115672", "DiamondGoldAndSilver"},
+    {"rbxassetid://11437130478", "OakeyYT"},
+    {"rbxassetid://11437171852", "zh0ng_li"},
+    {"rbxassetid://11437184421", "cupidivityy"},
+    {"rbxassetid://11620949992", "Temen_Sesad"},
+    {"rbxassetid://12759136435", "fakerflakey"},
+    {"rbxassetid://12759161894", "XxSoftaspillowxX"},
+    {"rbxassetid://12759179326", "BoneyMachete"},
+    {"rbxassetid://12759195044", "BoneyMachete"},
+    {"rbxassetid://12759209529", "BoneyMachete"},
+    {"rbxassetid://12759232237", "stammy26789"},
+    {"rbxassetid://12759260423", "SpaceInvader08"},
+    {"rbxassetid://12759286044", "NekoTechno"},
+    {"rbxassetid://12759300053", "Temen_Sesad"},
+    {"rbxassetid://12759337167", "ryuchaii"},
+    {"rbxassetid://12759373343", "apolI3r"},
+    {"rbxassetid://12759457445", "Temen_Sesad"},
+    {"rbxassetid://12759479742", "stammy26789"},
+    {"rbxassetid://12759528960", "V3X3DACT10NS"},
+    {"rbxassetid://12759566473", "RavenAlphaRay"},
+    {"rbxassetid://12759621853", "apolI3r"},
+    {"rbxassetid://12759689587", "Iord_ozymandias"},
+    {"rbxassetid://12760552626", "0Kawaiika0"},
+    {"rbxassetid://12760564898", "dexverity"},
+    {"rbxassetid://12760578167", "MM77UU_GAMING"},
+    {"rbxassetid://12760589652", "IrishPixel"},
+    {"rbxassetid://12760609854", "Temen_Sesad"},
+    {"rbxassetid://12760624869", "aria33133"},
+    {"rbxassetid://12760636412", "Koiiffy"},
+    {"rbxassetid://12760644701", "Koiiffy"},
+    {"rbxassetid://12760655850", "Enxssi"},
+    {"rbxassetid://13811380745", "ArthurKinq"},
+    {"rbxassetid://13811459216", "sasukatchi"},
+    {"rbxassetid://13811485511", "emo8bit"},
+    {"rbxassetid://13811511524", "Its_AndrewTheReal"},
+    {"rbxassetid://13811555257", "FloweryItsFluffy123"},
+    {"rbxassetid://14862433580", "Schemmeboop"},
+    {"rbxassetid://14862475478", "MonstahMash"},
+    {"rbxassetid://14862498982", "Smileylittlebottle"},
+    {"rbxassetid://14862533874", "Galaxy5Coyote5YT"},
+    {"rbxassetid://14862607132", "LucidlyMae"},
+    {"rbxassetid://14862636715", "Its_AndrewTheReal"},
+    {"rbxassetid://14862848495", "Razaphanta"},
+    {"rbxassetid://14862447843", "Tobiroe"},
+    {"rbxassetid://14862544154", "BulbaGame"},
+    {"rbxassetid://14862568667", "hugs4leigh"},
+    {"rbxassetid://14862588586", "Bagelmuffinz"},
+    {"rbxassetid://14862623556", "Its_AndrewTheReal"},
+    {"rbxassetid://14862659391", "Smileylittlebottle"}
+}
+--- getting the fanart
+x={} for i,v in pairs(workspace.Lobby.Geometry:GetChildren()) do
+if v.Name=="Fanart" then table.insert(x,{v.Background.Visual.Display.Texture,v.Credit.Display.Front.Text})
+        end end
+local copy = table_to_string(x) wait(1)
+setclipboard(copy) print("Done")
+
+--- cleaning and sorting raw tables (combine them all beforehand)
+local fanartRaw = {}
+function r(d)
+    local s = {}
+    for i = #d, 1, -1 do
+        local t = d[i]
+        local r = t[1]
+        if s[r] then
+            table.remove(d, i)
+        else
+            s[r] = true
+        end
+    end
+end
+function f(e)
+    local o = "{"
+    for n, v in pairs(e) do
+        if type(n) == "string" then
+            o = o .. '["' .. n .. '"]='
+        end
+        if type(v) == "table" then
+            o = o .. f(v)
+        elseif type(v) == "boolean" then
+            o = o .. tostring(v)
+        else
+            o = o .. '"' .. v .. '"'
+        end
+        o = o .. ","
+    end
+    if o ~= "" then
+        o = o:sub(1, o:len() - 1)
+    end
+    return o .. "}"
+end
+function e(r)
+    local n = tonumber(r:match("%d+"))
+    return n
+end
+
+function s(d)
+    table.sort(d,function(a,b)
+                local nA=e(a[1])
+                local nB=e(b[1])
+                if nA~=nil and nB~=nil then
+                return nA<nB end end) end
+r(fanartRaw)
+s(fanartRaw)
+print(f(fanartRaw))
