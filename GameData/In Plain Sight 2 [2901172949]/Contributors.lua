ccccc48220f2bcd9a2cf3d68fea6bd55cad7e9e4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-12 17:43:51 +1000

    Create Contributors.lua

---
+++ GameData/In Plain Sight 2 [2901172949]/Contributors.lua
@@ -0,0 +1,52 @@
+data = {
+    ["Thieves"] = {
+        {["Username"] = "TwistedGlyphs", ["Content"] = {60, 61}},
+        {["Username"] = "IckyNico77", ["Content"] = {58, 60, 71, 73, 79}},
+        {["Username"] = "minderkat", ["Content"] = {54}},
+        {["Username"] = "matinchito", ["Content"] = {70}},
+        {["Username"] = "GameGuruGreenGryphon", ["Content"] = {72, 79, 80}, ["ExtraContent"] = {78}},
+        {["Username"] = "ejczebra", ["Content"] = {61, 63}},
+        {["Username"] = "MoonSans_MoonDawn", ["Content"] = {63}},
+        {["Username"] = "Novumflame", ["Content"] = {64}},
+        {["Username"] = "Greeism", ["Content"] = {82}},
+        {["Username"] = "Uti_ba", ["Content"] = {62}},
+        {["Username"] = "Hypertxt", ["Content"] = {13, 17, 22, 64, 65, 69, 73, 74, 77, 80, 82}, ["ExtraContent"] = {78}},
+        {["Username"] = "sasukatchi", ["Content"] = {64, 31}},
+        {["Username"] = "TutanHello616", ["ExtraContent"] = {81}},
+        {["Username"] = "dragonslayerhiccup", ["Content"] = {48, 49, 51}},
+        {["Username"] = "3210brian", ["Content"] = {}, ["ExtraContent"] = {78}},
+        {["Username"] = "RoxN_Roll", ["Content"] = {82}},
+        {["Username"] = "LemonRune", ["Content"] = {68}},
+        {["Username"] = "Kezonate", ["Content"] = {66}}
+    },
+    ["Misc"] = {{["Username"] = "apolI3r"}},
+    ["Maps"] = {
+        {["Username"] = "Piratulus", ["Content"] = {"PoliceStation", "Farm"}},
+        {["Username"] = "GameGuruGreenGryphon", ["Content"] = {"Restaurant"}},
+        {["Username"] = "RoxN_Roll", ["Content"] = {"Complex"}},
+        {["Username"] = "3210brian", ["Content"] = {"Restaurant"}}
+    },
+    ["Moneybags"] = {
+        {["Username"] = "Greeism", ["Content"] = {6, 8, 25}},
+        {["Username"] = "Temen_Sesad", ["Content"] = {31}},
+        {["Username"] = "TinyPaperFestival", ["Content"] = {33}},
+        {["Username"] = "Hypertxt", ["Content"] = {21}},
+        {["Username"] = "Arceusdon", ["Content"] = {10, 11, 12, 13, 14, 15, 16, 17, 23, 27, 28, 34}},
+        {["Username"] = "3210brian", ["Content"] = {9, 19, 20, 22, 24, 26, 30}},
+        {["Username"] = "RoxN_Roll", ["Content"] = {2, 3, 4, 5, 7, 18, 29}},
+        {["Username"] = "akifthebro", ["Content"] = {32}},
+        {["Username"] = "Trolol_john", ["Content"] = {31}}
+    },
+    ["Cameras"] = {
+        {["Username"] = "Greeism", ["Content"] = {3, 5, 13, 15, 18, 35}},
+        {["Username"] = "Smeltamatron2000", ["Content"] = {24}},
+        {["Username"] = "minderkat", ["Content"] = {21, 22}},
+        {["Username"] = "Arceusdon", ["Content"] = {6, 23, 30}, ["ExtraContent"] = {32}},
+        {["Username"] = "Pixol_333", ["Content"] = {27}},
+        {["Username"] = "Knifemasterxxx", ["Content"] = {28}},
+        {["Username"] = "3210brian", ["Content"] = {29, 31}},
+        {["Username"] = "RoxN_Roll", ["Content"] = {35}},
+        {["Username"] = "lawin123457", ["Content"] = {28}},
+        {["Username"] = "CodeRederuption", ["Content"] = {23}}
+    }
+}
