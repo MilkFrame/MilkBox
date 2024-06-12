7e52dba54dc482489b8217ff986b52ddebf0db95
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-12 17:45:17 +1000

    Create Missions.lua

---
+++ GameData/In Plain Sight 2 [2901172949]/Missions.lua
@@ -0,0 +1,85 @@
+data = {
+    ["Get"] = "function: 0x22d660d757f1f11b",
+    ["List"] = {
+        {
+            ["Action"] = "Play 5 rounds",
+            ["TimeFrame"] = 86400,
+            ["Id"] = 1,
+            ["Target"] = 5,
+            ["Stat"] = "RoundsPlayed",
+            ["Reward"] = 10000
+        },
+        {
+            ["Action"] = "Play 50 rounds",
+            ["TimeFrame"] = 345600,
+            ["Id"] = 2,
+            ["Target"] = 50,
+            ["Stat"] = "RoundsPlayed",
+            ["Reward"] = 80000
+        },
+        {
+            ["Action"] = "Revive 10 thieves",
+            ["TimeFrame"] = 86400,
+            ["Id"] = 3,
+            ["Target"] = 10,
+            ["Stat"] = "ThievesRevived",
+            ["Reward"] = 5000
+        },
+        {
+            ["Action"] = "Down 10 thieves",
+            ["TimeFrame"] = 86400,
+            ["Id"] = 4,
+            ["Target"] = 10,
+            ["Stat"] = "ThievesDowned",
+            ["Reward"] = 5000
+        },
+        {
+            ["Action"] = "Steal 100 items",
+            ["TimeFrame"] = 86400,
+            ["Id"] = 5,
+            ["Target"] = 100,
+            ["Stat"] = "ItemsStolen",
+            ["Reward"] = 10000
+        },
+        {
+            ["Action"] = "Steal 800 items",
+            ["TimeFrame"] = 172800,
+            ["Id"] = 6,
+            ["Target"] = 800,
+            ["Stat"] = "ItemsStolen",
+            ["Reward"] = 30000
+        },
+        {
+            ["Action"] = "Deposit loot 20 times",
+            ["TimeFrame"] = 86400,
+            ["Id"] = 7,
+            ["Target"] = 20,
+            ["Stat"] = "TimesDeposited",
+            ["Reward"] = 5000
+        },
+        {
+            ["Action"] = "Use 20 abilities",
+            ["TimeFrame"] = 86400,
+            ["Id"] = 8,
+            ["Target"] = 20,
+            ["Stat"] = "AbilitiesUsed",
+            ["Reward"] = 2500
+        },
+        {
+            ["Action"] = "Steal 5 golden items",
+            ["TimeFrame"] = 86400,
+            ["Id"] = 9,
+            ["Target"] = 5,
+            ["Stat"] = "GoldenItemsStolen",
+            ["Reward"] = 6000
+        },
+        {
+            ["Action"] = "Steal 5 special items",
+            ["TimeFrame"] = 86400,
+            ["Id"] = 10,
+            ["Target"] = 5,
+            ["Stat"] = "SpecialItemsStolen",
+            ["Reward"] = 7000
+        }
+    }
+}
