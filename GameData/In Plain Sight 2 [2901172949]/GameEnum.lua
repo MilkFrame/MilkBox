849a7a641ad1272009dc64f49d3f89e596ca5c39
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-12 17:44:30 +1000

    Create GameEnum.lua

---
+++ GameData/In Plain Sight 2 [2901172949]/GameEnum.lua
@@ -0,0 +1,68 @@
+data = {
+    ["ThiefAppearanceSource"] = {["NPC"] = 0, ["Player"] = 1, ["ActiveNPC"] = 2},
+    ["ReviveEvent"] = {["Downed"] = 0, ["Revived"] = 1},
+    ["VotingType"] = {["Map"] = 0, ["RoundType"] = 2, ["Entrance"] = 1},
+    ["HoldAbilityState"] = {["Start"] = 0, ["End"] = 1},
+    ["ServerType"] = {["Private"] = 1, ["Public"] = 0, ["Reserved"] = 2},
+    ["Shop"] = {["BlackMarket"] = 2, ["Thief"] = 0, ["Camera"] = 1},
+    ["InputType"] = {["Touch"] = 1, ["Gamepad"] = 2, ["MouseKeyboard"] = 0},
+    ["DamageCalculationState"] = {["Cancel"] = 1, ["Pass"] = 0, ["PassNoDamage"] = 2},
+    ["Team"] = {["Thieves"] = 1, ["Cameras"] = 2, ["None"] = 0},
+    ["LeaderboardType"] = {["Data"] = 1, ["Offline"] = 0},
+    ["GameEndStatus"] = {["Despawn"] = 1, ["Panic"] = 2, ["End"] = 0},
+    ["GameStartStatus"] = {["Round"] = 1, ["Cutscene"] = 0},
+    ["Rarity"] = {
+        ["Exclusive"] = 7,
+        ["Legendary"] = 4,
+        ["Default"] = 0,
+        ["Event"] = 6,
+        ["Epic"] = 3,
+        ["Outlawed"] = 5,
+        ["Uncommon"] = 1,
+        ["Rare"] = 2
+    },
+    ["GameWarning"] = {["Clear"] = 3, ["CameraSwitched"] = 2, ["Idle"] = 0, ["AFK"] = 1},
+    ["TutorialState"] = {
+        ["ThiefPrompt"] = 1,
+        ["ThiefIntro"] = 0,
+        ["CameraPrompt"] = 4,
+        ["CameraPresentation"] = 5,
+        ["CameraIntro"] = 3,
+        ["ThiefPresentation"] = 2
+    },
+    ["CharacterSkin"] = {["Normal"] = 0, ["Emerald"] = 2, ["Ruby"] = 3, ["Gold"] = 1},
+    ["TeamComposition"] = {["Normal"] = 0, ["ThiefOnly"] = 1},
+    ["HitregStatus"] = {
+        ["PlayerNoHeat"] = 4,
+        ["Player"] = 2,
+        ["PlayerReload"] = 3,
+        ["NPC"] = 1,
+        ["InteractedReload"] = 6,
+        ["Interacted"] = 5,
+        ["None"] = 0
+    },
+    ["RoundType"] = {
+        ["Normal"] = 1,
+        ["Overtime"] = 5,
+        ["Anomalous"] = 4,
+        ["Funny"] = 8,
+        ["Spooky"] = 2,
+        ["PresentHeist"] = 10,
+        ["HauntedSchool"] = 9,
+        ["Stacked"] = 7,
+        ["DoubleTeaming"] = 6,
+        ["BananaBonanza"] = 3,
+        ["None"] = 0
+    },
+    ["HeldObjectState"] = {
+        ["Normal"] = 1,
+        ["NormalWithAbilities"] = 2,
+        ["NoDepositWithAbilities"] = 4,
+        ["NoDeposit"] = 3,
+        ["None"] = 0
+    },
+    ["Permissions"] = {["Internal"] = 2, ["Default"] = 0, ["Admin"] = 1},
+    ["CooldownUpdateState"] = {["Add"] = 2, ["Update"] = 1, ["Ignore"] = 0},
+    ["AnimationState"] = {["Start"] = 0, ["Stop"] = 1},
+    ["InteractionCompletionState"] = {["Normal"] = 0, ["SkipSetInteracting"] = 1}
+}
