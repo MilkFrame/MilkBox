af4afe50489311ffa64edc0af49038c17d8e95c7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-12 17:45:34 +1000

    Create RoundTypes.lua

---
+++ GameData/In Plain Sight 2 [2901172949]/RoundTypes.lua
@@ -0,0 +1,33 @@
+data = {
+    {["Thumbnail"] = "", ["Flair"] = "ERROR", ["DisplayName"] = "Normal Round"},
+    {["Thumbnail"] = "", ["Flair"] = "ERROR", ["DisplayName"] = "Spooky Round"},
+    {
+        ["Thumbnail"] = "rbxassetid://14208938360",
+        ["Flair"] = "Banana Bonanza Round!!",
+        ["DisplayName"] = "Banana Bonanza Round"
+    },
+    {["Thumbnail"] = "rbxassetid://14208428234", ["Flair"] = "Anomalous Round...", ["DisplayName"] = "Anomalous Round"},
+    {["Thumbnail"] = "rbxassetid://14209266335", ["Flair"] = "Overtime Round!", ["DisplayName"] = "Overtime Round"},
+    {
+        ["Thumbnail"] = "rbxassetid://14209574960",
+        ["Flair"] = "Double Teaming Round!",
+        ["DisplayName"] = "Double Teaming Round"
+    },
+    {["Thumbnail"] = "rbxassetid://14209499458", ["Flair"] = "Stacked Round!", ["DisplayName"] = "Stacked Round"},
+    {["Thumbnail"] = "rbxassetid://16949635862", ["Flair"] = "Funny Round", ["DisplayName"] = "Funny Round"},
+    {
+        ["IsEvent"] = true,
+        ["EscapeSFXName"] = "Thunder",
+        ["Flair"] = "Welcome back to school...",
+        ["EndingSubtitle"] = "That was intense...",
+        ["Thumbnail"] = "rbxassetid://15068191358",
+        ["DisplayName"] = "Visit Haunted School"
+    },
+    {
+        ["IsEvent"] = true,
+        ["Thumbnail"] = "rbxassetid://15595075614",
+        ["Flair"] = "It's time for a present heist!",
+        ["DisplayName"] = "Present Heist"
+    },
+    {["Thumbnail"] = "", ["Flair"] = "ERROR", ["DisplayName"] = "ERROR"}
+}
