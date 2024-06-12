561acdf44baf2a0c05e4d8371c569d03d93135a4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-12 17:35:21 +1000

    Create PosePacks.lua

---
+++ GameData/Dress To Impress [15101393044]/PosePacks.lua
@@ -0,0 +1,10 @@
+data = {
+    ["Pop Culture Pose Pack"] = {["Price"] = 1800, ["Icon"] = "rbxassetid://17186576001"},
+    ["K-Pop Pose Pack"] = {["Price"] = 4500, ["Icon"] = "rbxassetid://17514148701"},
+    ["Fantasy Pose Pack"] = {["Price"] = 1000, ["Icon"] = "rbxassetid://17067974177"},
+    ["Supermodel Pose Pack"] = {["Price"] = 3200, ["Icon"] = "rbxassetid://17067577235"},
+    ["Sci-Fi Pose Pack"] = {["Price"] = 2000, ["Icon"] = "rbxassetid://17190530494"},
+    ["Roblox Classic Pose Pack"] = {["Price"] = 999999999999999, ["Icon"] = "rbxassetid://17579389160"},
+    ["Kawaii Pose Pack"] = {["Price"] = 4000, ["Icon"] = "rbxassetid://17058902638"},
+    ["Crazy Pose Pack"] = {["Price"] = 2000, ["Icon"] = "rbxassetid://17186565505"}
+}
