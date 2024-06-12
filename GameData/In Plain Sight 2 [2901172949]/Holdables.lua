15c77dc7c34f412d6c797688cc280c34b231af65
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-12 17:44:43 +1000

    Create Holdables.lua

---
+++ GameData/In Plain Sight 2 [2901172949]/Holdables.lua
@@ -0,0 +1,61 @@
+data = {
+    ["Candle"] = {
+        ["OnHoldState"] = 3,
+        ["UserCanDrop"] = true,
+        ["Flair"] = "Place this at the ritual!",
+        ["DisplayName"] = "Candle"
+    },
+    ["SuperBanana"] = {
+        ["OnHoldState"] = 1,
+        ["Flair"] = "Quickly deposit the Super Banana!",
+        ["DisplayName"] = "Super Banana"
+    },
+    ["Fuse"] = {
+        ["OnHoldState"] = 3,
+        ["UserCanDrop"] = true,
+        ["Flair"] = "Put this in the fusebox!",
+        ["DisplayName"] = "Fuse"
+    },
+    ["Ingredient"] = {
+        ["OnHoldState"] = 3,
+        ["UserCanDrop"] = true,
+        ["Flair"] = "This goes in the cooking pot!",
+        ["DisplayName"] = "Ingredient"
+    },
+    ["HotCoal"] = {
+        ["OnHoldState"] = 1,
+        ["Flair"] = "Quickly pass the coal on or deposit it!",
+        ["DisplayName"] = "Hot Coal"
+    },
+    ["Tape"] = {
+        ["OnHoldState"] = 3,
+        ["UserCanDrop"] = true,
+        ["Flair"] = "Patch up the boiler pipes!",
+        ["DisplayName"] = "Tape"
+    },
+    ["EnergyCell"] = {
+        ["OnHoldState"] = 3,
+        ["UserCanDrop"] = true,
+        ["Flair"] = "Walk into an item to energize it!",
+        ["DisplayName"] = "Energy Cell"
+    },
+    ["Item"] = {
+        ["SkipNotif"] = true,
+        ["OnHoldState"] = 2,
+        ["Flair"] = "You can deposit this extra item.",
+        ["DisplayName"] = "Item"
+    },
+    ["NPC"] = {
+        ["SkipNotif"] = true,
+        ["OnHoldState"] = 4,
+        ["Flair"] = "Use the ability again to drop this NPC.",
+        ["DisplayName"] = "NPC"
+    },
+    ["Present"] = {["OnHoldState"] = 1, ["Flair"] = "Bring this back to the truck!", ["DisplayName"] = "Present"},
+    ["GoldenSeed"] = {
+        ["OnHoldState"] = 3,
+        ["UserCanDrop"] = true,
+        ["Flair"] = "Plant the seed in the greenhouse!",
+        ["DisplayName"] = "Golden Seed"
+    }
+}
