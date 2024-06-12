fc65844b7b52c3550b8dc6e80496141bcde443e7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-12 17:44:08 +1000

    Create Cosmetics.lua

---
+++ GameData/In Plain Sight 2 [2901172949]/Cosmetics.lua
@@ -0,0 +1,356 @@
+data = {
+    ["Prices"] = {
+        ["Moneybags"] = {["Small"] = 40000, ["Large"] = 85000},
+        ["LaserModules"] = {["Small"] = 50000, ["Large"] = 95000}
+    },
+    ["LaserModules"] = {
+        {
+            ["NeverSold"] = true,
+            ["Name"] = "Red",
+            ["Id"] = 1,
+            ["Color"] = Color3.fromRGB(255, 100, 100),
+            ["Description"] = "From out of the box, the standard laser color."
+        },
+        {
+            ["NeverSold"] = true,
+            ["Name"] = "Rainbow",
+            ["Id"] = 2,
+            ["Gradient"] = "rbxassetid://12117508719",
+            ["Description"] = "Obtained through the Rainbow Lasers pass."
+        },
+        {
+            ["Id"] = 3,
+            ["Color"] = Color3.fromRGB(100, 255, 100),
+            ["Name"] = "Green",
+            ["Description"] = "An alternative laser color, produced illegally through studies of the standard red module."
+        },
+        {
+            ["Id"] = 4,
+            ["Color"] = Color3.fromRGB(100, 100, 255),
+            ["Name"] = "Blue",
+            ["Description"] = "An alternative laser color, produced illegally through studies of the standard red module."
+        },
+        {
+            ["Id"] = 5,
+            ["Color"] = Color3.fromRGB(255, 255, 100),
+            ["Name"] = "Yellow",
+            ["Description"] = "An alternative laser color, produced illegally through studies of the standard red module."
+        },
+        {
+            ["Id"] = 6,
+            ["Color"] = Color3.fromRGB(255, 100, 255),
+            ["Name"] = "Magenta",
+            ["Description"] = "An alternative laser color, produced illegally through studies of the standard red module."
+        },
+        {
+            ["Id"] = 7,
+            ["Color"] = Color3.fromRGB(100, 255, 255),
+            ["Name"] = "Cyan",
+            ["Description"] = "An alternative laser color, produced illegally through studies of the standard red module."
+        },
+        {
+            ["Id"] = 8,
+            ["Color"] = Color3.fromRGB(255, 255, 255),
+            ["Name"] = "White",
+            ["Description"] = "An alternative laser color, produced illegally through studies of the standard red module."
+        },
+        {
+            ["Id"] = 9,
+            ["Color"] = Color3.fromRGB(0, 0, 0),
+            ["Name"] = "Black",
+            ["Description"] = "An alternative laser color, produced illegally through studies of the standard red module."
+        },
+        {
+            ["Id"] = 10,
+            ["Color"] = Color3.fromRGB(163, 58, 255),
+            ["Name"] = "Purple",
+            ["Description"] = "An alternative laser color, produced illegally through studies of the standard red module."
+        },
+        {
+            ["Id"] = 11,
+            ["Gradient"] = "rbxassetid://12127176541",
+            ["Name"] = "Solar",
+            ["Description"] = "Blinding the baddies with solar power is the way to go!"
+        },
+        {
+            ["Id"] = 12,
+            ["Gradient"] = "rbxassetid://12158223615",
+            ["Name"] = "Golden Focus",
+            ["Description"] = "Focus on what matters here. It's the gold they want!"
+        },
+        {
+            ["Id"] = 13,
+            ["Color"] = Color3.fromRGB(255, 140, 53),
+            ["Name"] = "Orange",
+            ["Description"] = "An alternative laser color, produced illegally through studies of the standard red module."
+        },
+        {
+            ["Id"] = 14,
+            ["Color"] = Color3.fromRGB(110, 110, 110),
+            ["Name"] = "Silver",
+            ["Description"] = "An alternative laser color, produced illegally through studies of the standard red module."
+        },
+        {
+            ["Id"] = 15,
+            ["Gradient"] = "rbxassetid://12157824718",
+            ["Name"] = "Vaporwave",
+            ["Description"] = "A E S T H E T I C"
+        },
+        {
+            ["Id"] = 16,
+            ["Gradient"] = "rbxassetid://12127269937",
+            ["Name"] = "Red Focus",
+            ["Description"] = "Target acquired."
+        },
+        {
+            ["Id"] = 17,
+            ["Pattern"] = "rbxassetid://12127281315",
+            ["Name"] = "Candy Cane",
+            ["Description"] = "The thieves are in for a jolly treat!"
+        },
+        {
+            ["Id"] = 18,
+            ["Gradient"] = "rbxassetid://12157537441",
+            ["Name"] = "Gumdrops",
+            ["Description"] = "Mmm... sweet!"
+        },
+        {
+            ["Id"] = 19,
+            ["Gradient"] = "rbxassetid://12779121005",
+            ["Name"] = "Scorched",
+            ["Description"] = "Legend has it, this was a standard red module before being burnt in a warehouse fire."
+        },
+        {
+            ["Id"] = 20,
+            ["Gradient"] = "rbxassetid://12779121520",
+            ["Name"] = "Freezeflame",
+            ["Description"] = "Pack a colorful punch with a mix of fire and ice!"
+        },
+        {
+            ["Id"] = 21,
+            ["Pattern"] = "rbxassetid://12779121394",
+            ["Name"] = "Nineties",
+            ["Description"] = "The thieves will be running."
+        },
+        {
+            ["Id"] = 22,
+            ["Pattern"] = "rbxassetid://12779121183",
+            ["Name"] = "RGB",
+            ["Description"] = "Things are looking a little 255,255,255 in here!"
+        },
+        {
+            ["Id"] = 23,
+            ["Gradient"] = "rbxassetid://12779379460",
+            ["Name"] = "Magic",
+            ["Description"] = "A magical approach to your camera wizardry nonsense."
+        },
+        {
+            ["Id"] = 24,
+            ["Gradient"] = "rbxassetid://12827544192",
+            ["Name"] = "Toxic",
+            ["Description"] = "Powering your camera with toxic sludge."
+        },
+        {["Id"] = 25, ["Gradient"] = "rbxassetid://12827539885", ["Name"] = "Green Focus"},
+        {["Id"] = 26, ["Gradient"] = "rbxassetid://12827538240", ["Name"] = "Cyan Focus"},
+        {
+            ["Id"] = 27,
+            ["Pattern"] = "rbxassetid://12827536799",
+            ["Name"] = "Buzzy Bee",
+            ["Description"] = "buzz bzzzz bzz"
+        },
+        {
+            ["Id"] = 28,
+            ["Pattern"] = "rbxassetid://12827543016",
+            ["Name"] = "Monochrome",
+            ["Description"] = "Colors are overrated anyways."
+        },
+        {
+            ["Id"] = 29,
+            ["Gradient"] = "rbxassetid://13408913807",
+            ["Name"] = "Green Energy",
+            ["Description"] = "Endorse eco-friendly tech while sweeping the premises clean."
+        },
+        {
+            ["Id"] = 30,
+            ["Gradient"] = "rbxassetid://13408914139",
+            ["Name"] = "Royal",
+            ["Description"] = "All hail the future of camera technology!"
+        },
+        {
+            ["Id"] = 31,
+            ["Gradient"] = "rbxassetid://13408914924",
+            ["Name"] = "Valentine",
+            ["Description"] = "Who will be yours? A thief, quite possibly."
+        },
+        {
+            ["Id"] = 32,
+            ["Gradient"] = "rbxassetid://13408913245",
+            ["Name"] = "Frost",
+            ["Description"] = "This spells a freezing doom to those who meet this laser."
+        },
+        {
+            ["NeverSold"] = true,
+            ["Name"] = "Ace Pride",
+            ["Id"] = 33,
+            ["BMSource"] = "rbxassetid://13560071024",
+            ["Pattern"] = "rbxassetid://13611842376",
+            ["Description"] = "Part of the Pride Laser Modules Pack pass."
+        },
+        {
+            ["NeverSold"] = true,
+            ["Name"] = "Agender Pride",
+            ["Id"] = 34,
+            ["BMSource"] = "rbxassetid://13560071819",
+            ["Pattern"] = "rbxassetid://13611843954",
+            ["Description"] = "Part of the Pride Laser Modules Pack pass."
+        },
+        {
+            ["NeverSold"] = true,
+            ["Name"] = "Aro Pride",
+            ["Id"] = 35,
+            ["BMSource"] = "rbxassetid://13560073258",
+            ["Pattern"] = "rbxassetid://13611845448",
+            ["Description"] = "Part of the Pride Laser Modules Pack pass."
+        },
+        {
+            ["NeverSold"] = true,
+            ["Name"] = "Bi Pride",
+            ["Id"] = 36,
+            ["BMSource"] = "rbxassetid://13560075146",
+            ["Pattern"] = "rbxassetid://13611846625",
+            ["Description"] = "Part of the Pride Laser Modules Pack pass."
+        },
+        {
+            ["NeverSold"] = true,
+            ["Name"] = "Gay Pride",
+            ["Id"] = 37,
+            ["BMSource"] = "rbxassetid://13560076657",
+            ["Pattern"] = "rbxassetid://13611847516",
+            ["Description"] = "Part of the Pride Laser Modules Pack pass."
+        },
+        {
+            ["NeverSold"] = true,
+            ["Name"] = "Lesbian Pride",
+            ["Id"] = 38,
+            ["BMSource"] = "rbxassetid://13560078591",
+            ["Pattern"] = "rbxassetid://13611849102",
+            ["Description"] = "Part of the Pride Laser Modules Pack pass."
+        },
+        {
+            ["NeverSold"] = true,
+            ["Name"] = "MLM Pride",
+            ["Id"] = 39,
+            ["BMSource"] = "rbxassetid://13560079167",
+            ["Pattern"] = "rbxassetid://13611849549",
+            ["Description"] = "Part of the Pride Laser Modules Pack pass."
+        },
+        {
+            ["NeverSold"] = true,
+            ["Name"] = "Pan Pride",
+            ["Id"] = 40,
+            ["BMSource"] = "rbxassetid://13560083377",
+            ["Pattern"] = "rbxassetid://13611850997",
+            ["Description"] = "Part of the Pride Laser Modules Pack pass."
+        },
+        {
+            ["NeverSold"] = true,
+            ["Name"] = "Transgender Pride",
+            ["Id"] = 41,
+            ["BMSource"] = "rbxassetid://13560084050",
+            ["Pattern"] = "rbxassetid://13611851791",
+            ["Description"] = "Part of the Pride Laser Modules Pack pass."
+        },
+        {
+            ["NeverSold"] = true,
+            ["Name"] = "Genderfluid Pride",
+            ["Id"] = 42,
+            ["BMSource"] = "rbxassetid://13560075858",
+            ["Pattern"] = "rbxassetid://13611848495",
+            ["Description"] = "Part of the Pride Laser Modules Pack pass."
+        },
+        {
+            ["NeverSold"] = true,
+            ["Name"] = "Nonbinary Pride",
+            ["Id"] = 43,
+            ["BMSource"] = "rbxassetid://13560081839",
+            ["Pattern"] = "rbxassetid://13611850510",
+            ["Description"] = "Part of the Pride Laser Modules Pack pass."
+        },
+        {
+            ["Id"] = 44,
+            ["Pattern"] = "rbxassetid://14910491231",
+            ["Name"] = "Jack-o'-Lantern",
+            ["Description"] = "This will scare them off for sure... Purchased during Halloween."
+        },
+        {
+            ["Id"] = 45,
+            ["Pattern"] = "rbxassetid://14910510508",
+            ["Name"] = "Candy Corn",
+            ["Description"] = "A sweet, scary delight! Purchased during Halloween."
+        },
+        {
+            ["Id"] = 46,
+            ["Pattern"] = "rbxassetid://14910530840",
+            ["Name"] = "Trick or Treat",
+            ["Description"] = "Oho! A laser module for all the ghouls, goblins, and ghosts alike! Purchased during Halloween."
+        }
+    },
+    ["Moneybags"] = {
+        {
+            ["Id"] = 1,
+            ["NeverSold"] = true,
+            ["Name"] = "Sack",
+            ["Description"] = "A well-rounded and simple bag for all your thieving needs; the classic moneybag - timeless."
+        },
+        {
+            ["Id"] = 2,
+            ["Name"] = "Reinforced Sack",
+            ["Description"] = "A well-rounded, albeit sturdier, bag for even more thieving needs!"
+        },
+        {["Id"] = 3, ["Name"] = "Ornament", ["Description"] = "Who knew they made them this big?"},
+        {["Id"] = 4, ["Name"] = "Garbage Bag", ["Description"] = "Thrash, steal, dispose."},
+        {["Id"] = 5, ["Special"] = true, ["Name"] = "The Cube", ["Description"] = "The Cube. It yearns."},
+        {["Id"] = 6, ["Name"] = "Bomb", ["Description"] = "Handle with care."},
+        {["Id"] = 7, ["Name"] = "Present", ["Description"] = "Give the gift of stolen goods!"},
+        {
+            ["Id"] = 8,
+            ["Name"] = "Potion",
+            ["Description"] = "Held within the strongest flask out there... for all things science!"
+        },
+        {["Id"] = 9, ["Name"] = "Piggy Bank", ["Description"] = "Holds more than just coins."},
+        {["Id"] = 10, ["Name"] = "Fishbowl", ["Description"] = "Rumors claim these fish came from San Diego."},
+        {["Id"] = 11, ["Name"] = "Snowglobe", ["Description"] = "Shake for a fun time!"},
+        {["Id"] = 12, ["Name"] = "Gumball Machine", ["Description"] = "Have you tried to shake the machine instead?"},
+        {["Id"] = 13, ["Name"] = "Suitcase", ["Description"] = "Safe travels!"},
+        {["Id"] = 14, ["Special"] = true, ["Name"] = "Bucket of Chicken", ["Description"] = "om nom nom"},
+        {["Id"] = 15, ["Name"] = "Yarn", ["Description"] = "Buy this bag and meowmewoememwoemwmeowmewoew"},
+        {["Id"] = 16, ["Special"] = true, ["Name"] = "LOLBAG", ["Description"] = "LOLWUT? LOLWHY? LOL bag!"},
+        {["Id"] = 17, ["Name"] = "Cow", ["Description"] = "Moo?"},
+        {["Id"] = 18, ["Special"] = true, ["Name"] = "Lightbulb", ["Description"] = "It glows as bright as the sun."},
+        {["Id"] = 19, ["Name"] = "Dufflebag", ["Description"] = "Sleek and stealthy, nobody will suspect the wiser!"},
+        {["Id"] = 20, ["Special"] = true, ["Name"] = "Explosive Barrel", ["Description"] = "Maybe not the best idea."},
+        {
+            ["Id"] = 21,
+            ["Special"] = true,
+            ["Name"] = "Gyroscope",
+            ["Description"] = "Your bank account will always be in balance. This moneybag always stays upright!"
+        },
+        {["Id"] = 22, ["Name"] = "Weight", ["Description"] = "Lift with your legs, not your back."},
+        {["Id"] = 23, ["Name"] = "Gourd", ["Description"] = "All natural!"},
+        {["Id"] = 24, ["Name"] = "Barrel", ["Description"] = "If it ain't broke, don't fix it."},
+        {["Id"] = 25, ["Name"] = "Mooring Buoy", ["Description"] = "Straight from the docks!"},
+        {["Id"] = 26, ["Name"] = "Backpack", ["Description"] = "It's hard to beat the trusty ol' backpack."},
+        {["Id"] = 27, ["Name"] = "Satchel", ["Description"] = "For your assorted parcels and stolen goods."},
+        {["Id"] = 28, ["Name"] = "Purse", ["Description"] = "Stunning and classy."},
+        {["Id"] = 29, ["Name"] = "Cooler", ["Description"] = "I be coolin brah IDC bout nun."},
+        {["Id"] = 30, ["Name"] = "Break Time", ["Description"] = "A worker's best friend."},
+        {["Id"] = 31, ["Name"] = "Burger", ["Description"] = "can i haz chezburger plz mmm chezburger"},
+        {["Id"] = 32, ["Name"] = "Gun Case", ["Description"] = "Keep your weapons close, but your valuables closer."},
+        {
+            ["Id"] = 33,
+            ["Name"] = "Mug of Tea",
+            ["Description"] = [[Though it may be a duffel bag, we can confirm it is full of tea.\n\n\nslosh slosh slosh]]
+        },
+        {["Id"] = 34, ["Special"] = true, ["Name"] = "Dragon Lantern", ["Description"] = "Happy Lunar New Year!"}
+    }
+}
