04d67725aae137291cc57d0646b31764a1912344
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-12 17:46:36 +1000

    Create Music.lua

---
+++ GameData/In Plain Sight 2 [2901172949]/Music.lua
@@ -0,0 +1,40 @@
+data = {
+    ["ThiefShop"] = {["Trick or Treat"] = "rbxassetid://1837467234"},
+    ["CameraShop"] = {["Old Corridor"] = "rbxassetid://1839807434"},
+    ["ThiefRound_HauntedSchool"] = {
+        ["The Mysterious House (Underscore)"] = "rbxassetid://1838645878",
+        ["Ghost Ride a"] = "rbxassetid://1841601655"
+    },
+    ["CameraRound_Overtime"] = {["Funster"] = "rbxassetid://9041893774"},
+    ["ThiefRound_Overtime"] = {["Ska Mansions"] = "rbxassetid://1836267402"},
+    ["ThiefRound"] = {
+        ["Halloween March"] = "rbxassetid://1838591268",
+        ["Funster"] = "rbxassetid://1839940567",
+        ["Adventure"] = "rbxassetid://1839806888"
+    },
+    ["CameraRound_BananaBonanza"] = {["New"] = "rbxassetid://1838140215"},
+    ["BlackMarketShop"] = {["Another Enigma"] = "rbxassetid://1839629725"},
+    ["Club"] = {["Other"] = "rbxassetid://1837200896"},
+    ["CameraRound"] = {
+        ["Night of the Ghouls"] = "rbxassetid://1837467198",
+        ["The Wooden Pa"] = "rbxassetid://1837233558"
+    },
+    ["ThiefIntro"] = {
+        ["Smooth but UneasyJazz"] = "rbxassetid://1847724293",
+        ["Jazzy IntrigueNEW"] = "rbxassetid://1839795063"
+    },
+    ["Unused"] = {
+        ["Jazzy DreamingNEW"] = "rbxassetid://1839332161",
+        ["Jazzin' (B)"] = "rbxassetid://1846255800",
+        ["Sunday Afternoon"] = "rbxassetid://1844332898"
+    },
+    ["CameraIntro"] = {["Jazz Deluxe"] = "rbxassetid://1839222173"},
+    ["Lobby"] = {
+        ["Draculas castle"] = "rbxassetid://1839807734",
+        ["Halloween Alien"] = "rbxassetid://1836009584",
+        ["Halloween Night"] = "rbxassetid://1838592691",
+        ["Prom Fright c"] = "rbxassetid://1841602939",
+        ["Halloween music"] = "rbxassetid://1839807778"
+    },
+    ["ThiefRound_BananaBonanza"] = {["New"] = "rbxassetid://1839280267"}
+}
