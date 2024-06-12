72f6ab4821d5a1f975a91a8a8fc2c175b17be6e5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-12 17:34:24 +1000

    Create CodeSystem.lua

---
+++ GameData/Dress To Impress [15101393044]/CodeSystem.lua
@@ -0,0 +1,30 @@
+data = {
+    ["ItemRenders"] = {
+        ["Lana Rae's Shorts"] = "rbxassetid://16534055365",
+        ["Lana Rae's Top"] = "rbxassetid://16534053706",
+        ["Lashes"] = "rbxassetid://17216953410",
+        ["Lana Rae's Bow"] = "rbxassetid://16510173129",
+        ["SWEETHEART Dress"] = "rbxassetid://16716563842",
+        ["^^Iconic Dress"] = "rbxassetid://17585219755",
+        ["Juicy Pants Leah Ashe"] = "rbxassetid://16560774578",
+        ["^^Bunny Slippers"] = "rbxassetid://17444500107",
+        ["Lana Rae's Leg Warmers"] = "rbxassetid://16534587235",
+        ["Miss Tekk's Bag"] = "rbxassetid://16510153836",
+        ["- Hair IBella"] = "rbxassetid://17428656113",
+        ["Juicy Top Leah Ashe"] = "rbxassetid://16560777015",
+        ["LH Boots"] = "rbxassetid://16583993790",
+        ["Elegant Pearl Necklace"] = "rbxassetid://17216644108"
+    },
+    ["Codes"] = {
+        ["SUBM15CY"] = {"Elegant Pearl Necklace", "Lashes"},
+        ["TEKKYOOZ"] = {"Miss Tekk's Bag"},
+        ["LANA"] = {"Lana Rae's Shorts", "Lana Rae's Top", "Lana Rae's Leg Warmers"},
+        ["LANABOW"] = {"Lana Rae's Bow"},
+        ["ASHLEYBUNNI"] = {"^^Bunny Slippers"},
+        ["LABOOTS"] = {"LH Boots"},
+        ["SWEETHEART"] = {"SWEETHEART Dress"},
+        ["IBELLASLAY"] = {"- Hair IBella"},
+        ["REWARD4CLASS1C"] = {"^^Iconic Dress"},
+        ["LEAHASHE"] = {"Juicy Top Leah Ashe", "Juicy Pants Leah Ashe"}
+    }
+}
