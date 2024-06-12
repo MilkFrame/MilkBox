6347efa75ac615f8ca02d8476092ce74af4accbe
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-12 17:46:17 +1000

    Create Animations.lua

---
+++ GameData/In Plain Sight 2 [2901172949]/Animations.lua
@@ -0,0 +1,139 @@
+data = {
+    ["Scenes"] = {
+        ["Typing"] = "rbxassetid://10333778868",
+        ["Lean"] = "rbxassetid://10326021661",
+        ["Sit"] = "rbxassetid://10326063681",
+        ["BulkBuy_Open"] = "rbxassetid://12550558670",
+        ["Salute"] = "rbxassetid://10345933264",
+        ["Shops"] = {
+            ["Salute2"] = "rbxassetid://10890167654",
+            ["ThiefEnter"] = "rbxassetid://10822862883",
+            ["Dealer1"] = {
+                ["HoldCards"] = "rbxassetid://10955438734",
+                ["CardsLookAround"] = "rbxassetid://10955442093",
+                ["CollectCards"] = "rbxassetid://10955455565",
+                ["PlaceCards"] = "rbxassetid://10955436517",
+                ["NoCardsIdle"] = "rbxassetid://10955441153",
+                ["GrabCard"] = "rbxassetid://10955431404",
+                ["DropCards"] = "rbxassetid://10955437765"
+            },
+            ["CameraEnter"] = "rbxassetid://10822584574",
+            ["Keeper"] = {
+                ["Idle"] = "rbxassetid://12136024487",
+                ["Sleepy"] = "rbxassetid://12136027659",
+                ["Crisis"] = "rbxassetid://12136031802",
+                ["Sitting"] = "rbxassetid://12136030723",
+                ["Wait"] = "rbxassetid://12136026243"
+            },
+            ["Character"] = {
+                ["LookAround"] = "rbxassetid://10906406961",
+                ["TapFoot"] = "rbxassetid://10906400788",
+                ["Examine"] = "rbxassetid://10906392171",
+                ["SwingArms"] = "rbxassetid://10906403744",
+                ["Yawn"] = "rbxassetid://10906398345",
+                ["CheckTime"] = "rbxassetid://10906396082",
+                ["Stretch2"] = "rbxassetid://10906393929",
+                ["CharacterIdle"] = "rbxassetid://10906380270",
+                ["Stretch"] = "rbxassetid://10906402180",
+                ["Clean"] = "rbxassetid://10906390245"
+            },
+            ["Typing2"] = "rbxassetid://10889872643",
+            ["Dealer2"] = {
+                ["HoldCards_D2"] = "rbxassetid://10955438734",
+                ["DropCards2"] = "rbxassetid://10955502041",
+                ["NoCardsIdle2"] = "rbxassetid://10955503333"
+            },
+            ["Typing3"] = "rbxassetid://10889886647",
+            ["Talking2"] = "rbxassetid://10890254406",
+            ["Clipboard2"] = "rbxassetid://10889902571",
+            ["Talking"] = "rbxassetid://10890244312"
+        },
+        ["Chief"] = {
+            ["SpinBoard"] = "rbxassetid://10347188398",
+            ["Shrug"] = "rbxassetid://10347694972",
+            ["DrawLines"] = "rbxassetid://10348079106",
+            ["Wave"] = "rbxassetid://10348245624",
+            ["StickFigure"] = "rbxassetid://10347333736",
+            ["StickFigurePoint"] = "rbxassetid://10347455947"
+        },
+        ["Clipboard"] = "rbxassetid://10333768441",
+        ["Godfather"] = {
+            ["Laugh"] = "rbxassetid://10346102399",
+            ["PickupStick"] = "rbxassetid://10338550286",
+            ["Point"] = "rbxassetid://10346142328",
+            ["Pointer1"] = "rbxassetid://10338763084",
+            ["Pointer2"] = "rbxassetid://10346299414",
+            ["ProjectorOn"] = "rbxassetid://10337844318",
+            ["Flail"] = "rbxassetid://10346329337"
+        },
+        ["Dances"] = {
+            ["1"] = "rbxassetid://507776043",
+            ["0"] = "rbxassetid://507771019",
+            ["2"] = "rbxassetid://507777268"
+        },
+        ["HoldSign"] = "rbxassetid://10141912065",
+        ["CameraEntrance"] = "rbxassetid://10299151956"
+    },
+    ["Thief"] = {
+        ["Interactions"] = {
+            ["Downed"] = "rbxassetid://10391782251",
+            ["GetUp"] = "rbxassetid://10152836908",
+            ["PettingInteraction"] = "rbxassetid://14203240174",
+            ["Die"] = "rbxassetid://10152853592",
+            ["Vault"] = "rbxassetid://10089960166",
+            ["BananaStation"] = "rbxassetid://12896561393",
+            ["HoldDie"] = "rbxassetid://10152864292",
+            ["BreakableInteraction"] = "rbxassetid://14732070237",
+            ["Barrier"] = "rbxassetid://12473881581",
+            ["Revive"] = "rbxassetid://10152873276",
+            ["Deposit"] = "rbxassetid://12570784131"
+        },
+        ["Special"] = {
+            ["Holdable_Ingredient"] = "rbxassetid://14195400808",
+            ["Holdable_Box"] = "rbxassetid://13714532052",
+            ["Holdable_HotCoal"] = "rbxassetid://11792635141",
+            ["Holdable_NPC"] = "rbxassetid://12861319751",
+            ["Holdable_EnergyCell"] = "rbxassetid://13737825649",
+            ["Holdable_Item"] = "rbxassetid://12861319751",
+            ["Holdable_SuperBanana"] = "rbxassetid://12861319751",
+            ["Holdable_GoldenSeed"] = "rbxassetid://14195400808"
+        },
+        ["Abilities"] = {
+            ["Bandages_Use"] = "rbxassetid://10420209737",
+            ["Dig"] = "rbxassetid://13396834098",
+            ["BananaPeel_Place"] = "rbxassetid://12142756505",
+            ["BubbleShield"] = "rbxassetid://10432333009",
+            ["FailedExperiment"] = "rbxassetid://10433807942",
+            ["Consume"] = "rbxassetid://14988285483",
+            ["Ink"] = "rbxassetid://10433542847",
+            ["Howl"] = "rbxassetid://10442115403",
+            ["SuckBlood"] = "rbxassetid://11532306055",
+            ["MoldEarth_Remove"] = "rbxassetid://12143294595",
+            ["Robbery"] = "rbxassetid://10434192816",
+            ["Smokebomb_Throw3"] = "rbxassetid://12653000091",
+            ["MoldEarth_Place"] = "rbxassetid://12143352450",
+            ["Teleport"] = "rbxassetid://10421491793",
+            ["Necromancy"] = "rbxassetid://13197202408",
+            ["Salvation"] = "rbxassetid://12638150920",
+            ["Smokebomb_Throw2"] = "rbxassetid://12652961033",
+            ["Zoom_Flail"] = "rbxassetid://10420941598",
+            ["TurtlePower"] = "rbxassetid://10433261182",
+            ["Snitch"] = "rbxassetid://11217683351",
+            ["Blacksmith"] = "rbxassetid://11216302835",
+            ["HoHoHo"] = "rbxassetid://10423429518",
+            ["HealingAura"] = "rbxassetid://10422653339",
+            ["Greed"] = "rbxassetid://10431224107",
+            ["Smokebomb_Throw1"] = "rbxassetid://12652936873",
+            ["OneLastRep_Toss"] = "rbxassetid://13835017971"
+        },
+        ["Stealing"] = {
+            ["Light_Down_2"] = "rbxassetid://10089978556",
+            ["Light_Down_1"] = "rbxassetid://10089977220",
+            ["Light_Up_1"] = "rbxassetid://10089974640",
+            ["Light_2"] = "rbxassetid://10089973611",
+            ["Light_1"] = "rbxassetid://10089970645",
+            ["Light_Up_2"] = "rbxassetid://10089975700",
+            ["Heavy"] = "rbxassetid://10089969410"
+        }
+    }
+}
