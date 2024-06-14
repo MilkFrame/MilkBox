dea9e34b8b3419961ab57a7687ad4909b3807ff1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-14 22:14:13 +1000

    Create EnemyBuffs.lua

---
+++ GameData/Tower Defense X [9503261072]/EnemyBuffs.lua
@@ -0,0 +1,17 @@
+data = {
+    ["ResurrectedJohnRageBuff"] = {
+        ["Type"] = "MovementSpeed",
+        ["Time"] = 9,
+        ["IsDebuff"] = false,
+        ["CustomVisualEffectName"] = "ResurrectedRageModeBuff",
+        ["Percentage"] = 1.6
+    },
+    ["ResurrectedJohnRageSelfBuff"] = {
+        ["Type"] = "MovementSpeed",
+        ["Time"] = 10,
+        ["IsDebuff"] = false,
+        ["OverrideMovementAnimationName"] = "MoveFast",
+        ["NoVisualEffect"] = true,
+        ["Percentage"] = 0.2
+    }
+}
