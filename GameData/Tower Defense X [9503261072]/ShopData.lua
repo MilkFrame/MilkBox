019f8e8d83adc0f4d3dea1f17a263da0ddd779ac
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-14 22:08:49 +1000

    Create ShopData.lua

---
+++ GameData/Tower Defense X [9503261072]/ShopData.lua
@@ -0,0 +1,301 @@
+data = {
+    ["Products"] = {
+        ["JuggernautEradicator"] = {
+            ["ShopCardOverrideSize"] = "{0.899999976, 0}, {0.899999976, 0}",
+            ["RenderImage"] = "rbxassetid://17228174127",
+            ["ShopCardOverrideAspectRatio"] = 0.76,
+            ["Price"] = 0,
+            ["Image"] = "rbxassetid://17228174785",
+            ["MinLevel"] = 0,
+            ["PriceInRobux"] = 150,
+            ["ProductId"] = 1812225162,
+            ["ExpectedSkin"] = "Juggernaut_Eradicator",
+            ["IsRobuxOnly"] = true,
+            ["New"] = true
+        },
+        ["DealerOperator"] = {
+            ["ShopCardOverrideSize"] = "{0.899999976, 0}, {0.899999976, 0}",
+            ["RenderImage"] = "rbxassetid://16281574500",
+            ["ShopCardOverrideAspectRatio"] = 0.76,
+            ["Price"] = 0,
+            ["Image"] = "rbxassetid://16281574205",
+            ["MinLevel"] = 0,
+            ["PriceInRobux"] = 125,
+            ["ProductId"] = 1748747509,
+            ["ExpectedSkin"] = "Operator_Dealer",
+            ["OverrideLayoutOrder"] = -4,
+            ["IsRobuxOnly"] = true
+        },
+        ["RCUJuggernaut"] = {
+            ["ShopCardOverrideSize"] = "{0.899999976, 0}, {0.899999976, 0}",
+            ["RenderImage"] = "rbxassetid://16281574664",
+            ["ShopCardOverrideAspectRatio"] = 0.76,
+            ["Price"] = 0,
+            ["Image"] = "rbxassetid://16281574048",
+            ["MinLevel"] = 0,
+            ["PriceInRobux"] = 175,
+            ["ProductId"] = 1746131721,
+            ["ExpectedSkin"] = "Juggernaut_RCU",
+            ["OverrideLayoutOrder"] = -5,
+            ["IsRobuxOnly"] = true
+        },
+        ["TBOperator"] = {
+            ["ShopCardOverrideSize"] = "{0.899999976, 0}, {0.899999976, 0}",
+            ["RenderImage"] = "rbxassetid://15603232016",
+            ["ShopCardOverrideAspectRatio"] = 0.76,
+            ["Price"] = 0,
+            ["Image"] = "rbxassetid://15604140150",
+            ["MinLevel"] = 0,
+            ["PriceInRobux"] = 100,
+            ["ProductId"] = 1702723032,
+            ["ExpectedSkin"] = "Operator_Tower Battles",
+            ["IsRobuxOnly"] = true
+        },
+        ["TBSniper"] = {
+            ["ShopCardOverrideSize"] = "{0.899999976, 0}, {0.899999976, 0}",
+            ["RenderImage"] = "rbxassetid://15603806237",
+            ["ShopCardOverrideAspectRatio"] = 0.76,
+            ["Price"] = 0,
+            ["Image"] = "rbxassetid://15604140029",
+            ["MinLevel"] = 0,
+            ["PriceInRobux"] = 100,
+            ["ProductId"] = 1702722959,
+            ["ExpectedSkin"] = "Sniper_Tower Battles",
+            ["IsRobuxOnly"] = true
+        },
+        ["GoldenJuggernautGoldenCommando"] = {
+            ["ShopCardOverrideSize"] = "{0.899999976, 0}, {0.899999976, 0}",
+            ["RenderImage"] = "rbxassetid://17213996336",
+            ["ShopCardOverrideAspectRatio"] = 0.76,
+            ["Price"] = 0,
+            ["Image"] = "rbxassetid://17228176402",
+            ["MinLevel"] = 0,
+            ["PriceInRobux"] = 225,
+            ["ProductId"] = 1812224574,
+            ["ExpectedSkin"] = "Golden Juggernaut_Golden Commando",
+            ["IsRobuxOnly"] = true,
+            ["New"] = true
+        },
+        ["JohnEradicator"] = {
+            ["ShopCardOverrideSize"] = "{0.899999976, 0}, {0.899999976, 0}",
+            ["RenderImage"] = "rbxassetid://17228175846",
+            ["ShopCardOverrideAspectRatio"] = 0.76,
+            ["Price"] = 0,
+            ["Image"] = "rbxassetid://17228175333",
+            ["MinLevel"] = 0,
+            ["PriceInRobux"] = 125,
+            ["ProductId"] = 1812224930,
+            ["ExpectedSkin"] = "John_Eradicator",
+            ["IsRobuxOnly"] = true,
+            ["New"] = true
+        },
+        ["TBMissileTrooper"] = {
+            ["ShopCardOverrideSize"] = "{0.899999976, 0}, {0.899999976, 0}",
+            ["RenderImage"] = "rbxassetid://15573422868",
+            ["ShopCardOverrideAspectRatio"] = 0.76,
+            ["Price"] = 0,
+            ["Image"] = "rbxassetid://15604139702",
+            ["MinLevel"] = 0,
+            ["PriceInRobux"] = 100,
+            ["ProductId"] = 1702723103,
+            ["ExpectedSkin"] = "Missile Trooper_Tower Battles",
+            ["IsRobuxOnly"] = true
+        },
+        ["TBJuggernaut"] = {
+            ["ShopCardOverrideSize"] = "{0.899999976, 0}, {0.899999976, 0}",
+            ["RenderImage"] = "rbxassetid://15573430409",
+            ["ShopCardOverrideAspectRatio"] = 0.76,
+            ["Price"] = 0,
+            ["Image"] = "rbxassetid://15604139078",
+            ["MinLevel"] = 0,
+            ["PriceInRobux"] = 100,
+            ["ProductId"] = 1701193783,
+            ["ExpectedSkin"] = "Juggernaut_Tower Battles",
+            ["IsRobuxOnly"] = true
+        },
+        ["TBEDJ"] = {
+            ["ShopCardOverrideSize"] = "{0.899999976, 0}, {0.899999976, 0}",
+            ["RenderImage"] = "rbxassetid://15558310526",
+            ["ShopCardOverrideAspectRatio"] = 0.76,
+            ["Price"] = 0,
+            ["Image"] = "rbxassetid://15604139880",
+            ["MinLevel"] = 0,
+            ["PriceInRobux"] = 325,
+            ["ProductId"] = 1701193889,
+            ["ExpectedSkin"] = "EDJ_Tower Battles",
+            ["IsRobuxOnly"] = true
+        }
+    },
+    ["PowerUps"] = {
+        ["HealingField"] = {
+            ["LayoutOrder"] = 8,
+            ["MainImage"] = "rbxassetid://17190997326",
+            ["MinLevel"] = 0,
+            ["SinglePrice"] = 850,
+            ["MiniImage"] = "rbxassetid://17191076768",
+            ["FivePriceInRobux"] = 110,
+            ["FiveProductId"] = 1810447012
+        },
+        ["ReconDrone"] = {
+            ["LayoutOrder"] = 5,
+            ["MainImage"] = "rbxassetid://17190996292",
+            ["MinLevel"] = 0,
+            ["SinglePrice"] = 750,
+            ["MiniImage"] = "rbxassetid://17191075655",
+            ["FivePriceInRobux"] = 100,
+            ["FiveProductId"] = 1810446478
+        },
+        ["CashCrate"] = {
+            ["LayoutOrder"] = 6,
+            ["MainImage"] = "rbxassetid://17190997589",
+            ["MinLevel"] = 0,
+            ["SinglePrice"] = 1000,
+            ["MiniImage"] = "rbxassetid://17191077072",
+            ["FivePriceInRobux"] = 125,
+            ["FiveProductId"] = 1810446617
+        },
+        ["HealthCrate"] = {
+            ["LayoutOrder"] = 7,
+            ["MainImage"] = "rbxassetid://17190997001",
+            ["MinLevel"] = 0,
+            ["SinglePrice"] = 1000,
+            ["MiniImage"] = "rbxassetid://17191076158",
+            ["FivePriceInRobux"] = 125,
+            ["FiveProductId"] = 1810446834
+        },
+        ["NuclearMissile"] = {
+            ["LayoutOrder"] = 4,
+            ["MainImage"] = "rbxassetid://17308259360",
+            ["MinLevel"] = 0,
+            ["SinglePrice"] = 50000,
+            ["MiniImage"] = "rbxassetid://17308249244",
+            ["SingleProductId"] = 1816281467,
+            ["SinglePriceInRobux"] = 1000
+        },
+        ["RaptorStorm"] = {
+            ["LayoutOrder"] = 3,
+            ["MainImage"] = "rbxassetid://17190996650",
+            ["MinLevel"] = 0,
+            ["SinglePrice"] = 5000,
+            ["MiniImage"] = "rbxassetid://17191075823",
+            ["FivePriceInRobux"] = 450,
+            ["FiveProductId"] = 1810446325
+        },
+        ["AC130Support"] = {
+            ["LayoutOrder"] = 1,
+            ["MainImage"] = "rbxassetid://17190998147",
+            ["MinLevel"] = 0,
+            ["SinglePrice"] = 3500,
+            ["MiniImage"] = "rbxassetid://17191077509",
+            ["FivePriceInRobux"] = 325,
+            ["FiveProductId"] = 1810445873
+        },
+        ["A10Strike"] = {
+            ["LayoutOrder"] = 0,
+            ["MainImage"] = "rbxassetid://17190998443",
+            ["MinLevel"] = 0,
+            ["SinglePrice"] = 2000,
+            ["MiniImage"] = "rbxassetid://17191077678",
+            ["FivePriceInRobux"] = 225,
+            ["FiveProductId"] = 1810445660
+        },
+        ["AirStrike"] = {
+            ["LayoutOrder"] = 2,
+            ["MainImage"] = "rbxassetid://17190997844",
+            ["MinLevel"] = 0,
+            ["SinglePrice"] = 1250,
+            ["MiniImage"] = "rbxassetid://17191077315",
+            ["FivePriceInRobux"] = 150,
+            ["FiveProductId"] = 1810446026
+        }
+    },
+    ["Emotes"] = {
+        ["MonsterMash"] = {["Price"] = 2000, ["Image"] = "", ["MinLevel"] = 0},
+        ["StockShuffle"] = {["Price"] = 1000, ["Image"] = "", ["MinLevel"] = 0},
+        ["ChainsawDance"] = {["Price"] = 1000, ["Image"] = "", ["MinLevel"] = 0},
+        ["Sturdy"] = {["Price"] = 1000, ["Image"] = "", ["MinLevel"] = 0},
+        ["SherbertDance"] = {["Price"] = 1000, ["Image"] = "", ["MinLevel"] = 0}
+    },
+    ["Towers"] = {
+        ["Cryo Blaster"] = {["ViewportName"] = "Cryo Blaster", ["MinLevel"] = 0, ["Price"] = 2000},
+        ["Cryo Ranger"] = {
+            ["MinLevel"] = 0,
+            ["ProductId"] = 1711785582,
+            ["ViewportName"] = "Cryo Ranger",
+            ["PriceInRobux"] = 300,
+            ["EventPurchase"] = true,
+            ["Price"] = 0
+        },
+        ["Juggernaut"] = {["ViewportName"] = "Juggernaut", ["MinLevel"] = 0, ["Price"] = 13250},
+        ["Shotgunner"] = {["ViewportName"] = "Shotgunner", ["MinLevel"] = 0, ["Price"] = 450},
+        ["EDJ"] = {["ViewportName"] = "EDJ", ["MinLevel"] = 0, ["Price"] = 3200},
+        ["Golden Juggernaut"] = {["ViewportName"] = "Golden Juggernaut", ["MinLevel"] = 0, ["Price"] = 85000},
+        ["Warship"] = {
+            ["MinLevel"] = 100,
+            ["ProductId"] = 1725456708,
+            ["ViewportName"] = "Warship",
+            ["PriceInRobux"] = 4950,
+            ["Price"] = 75000
+        },
+        ["Missile Trooper"] = {["ViewportName"] = "Missile Trooper", ["MinLevel"] = 0, ["Price"] = 2000},
+        ["Ranger"] = {["ViewportName"] = "Ranger", ["MinLevel"] = 0, ["Price"] = 0},
+        ["Artillery"] = {
+            ["MinLevel"] = 64,
+            ["ProductId"] = 1691241834,
+            ["ViewportName"] = "Artillery",
+            ["PriceInRobux"] = 1750,
+            ["Price"] = 8750
+        },
+        ["John"] = {["ViewportName"] = "John", ["MinLevel"] = 0, ["Price"] = 4500},
+        ["Golden Ranger"] = {["ViewportName"] = "Golden Ranger", ["MinLevel"] = 0, ["Price"] = 38500},
+        ["Barracks"] = {["ViewportName"] = "Barracks", ["MinLevel"] = 0, ["Price"] = 2350},
+        ["Laser Gunner"] = {
+            ["MinLevel"] = 35,
+            ["ProductId"] = 1691366905,
+            ["ViewportName"] = "Laser Gunner",
+            ["PriceInRobux"] = 1000,
+            ["Price"] = 5500
+        },
+        ["XWM Turret"] = {
+            ["MinLevel"] = 60,
+            ["ProductId"] = 1812273030,
+            ["ViewportName"] = "XWM Turret",
+            ["PriceInRobux"] = 2500,
+            ["Price"] = 32000
+        },
+        ["Operator"] = {["ViewportName"] = "Operator", ["MinLevel"] = 0, ["Price"] = 1250},
+        ["Patrol Boat"] = {["ViewportName"] = "Patrol Boat", ["MinLevel"] = 0, ["Price"] = 2150},
+        ["Toxicnator"] = {["ViewportName"] = "Toxicnator", ["MinLevel"] = 0, ["Price"] = 6500},
+        ["Grenadier"] = {["ViewportName"] = "Grenadier", ["MinLevel"] = 0, ["Price"] = 2750},
+        ["Farm"] = {["ViewportName"] = "Farm", ["MinLevel"] = 0, ["Price"] = 2200},
+        ["Railgunner"] = {["ViewportName"] = "Railgunner", ["MinLevel"] = 0, ["Price"] = 15000},
+        ["Armored Factory"] = {
+            ["MinLevel"] = 50,
+            ["ProductId"] = 1770565748,
+            ["ViewportName"] = "Armored Factory",
+            ["PriceInRobux"] = 1500,
+            ["Price"] = 12000
+        },
+        ["Sniper"] = {
+            ["MinLevel"] = 15,
+            ["ProductId"] = 1669970778,
+            ["ViewportName"] = "Sniper",
+            ["PriceInRobux"] = 350,
+            ["Price"] = 1650
+        },
+        ["Mine Layer"] = {["ViewportName"] = "Mine Layer", ["MinLevel"] = 0, ["Price"] = 1450}
+    },
+    ["Crates"] = {
+        ["BasicCrate"] = {
+            ["ShopCardOverrideSize"] = "{0.949999988, 0}, {0.949999988, 0}",
+            ["ShopCardOverrideAspectRatio"] = 1,
+            ["Price"] = 200,
+            ["RenderImageOverridePosition"] = "{0.5, 0}, {0.449999988, 0}",
+            ["Image"] = "rbxassetid://15604139479",
+            ["ShopCardOverridePosition"] = "{0.5, 0}, {0.449999988, 0}",
+            ["RenderImageOverrideAspectRatio"] = 1,
+            ["MinLevel"] = 0,
+            ["RenderImageOverrideSize"] = "{1, 0}, {0.699999988, 0}"
+        }
+    }
+}
