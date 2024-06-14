c8ee224106563550690d1b7a0646cc1cfbbeb928
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-14 22:39:31 +1000

    Create DailyMissions.lua

---
+++ GameData/Evade [9872472334]/DailyMissions.lua
@@ -0,0 +1,80 @@
+data = {
+    ["NoUtilities"] = {
+        ["Type"] = "NoUtilities",
+        ["Name"] = "No Utilities",
+        ["Rewards"] = {["Tokens"] = 200, ["Points"] = 15},
+        ["Requirement"] = 2,
+        ["Title"] = "Win <font color='rgb(200,200,255)'>2</font> matches without any utilities"
+    },
+    ["WinGames"] = {
+        ["Type"] = "Revives",
+        ["Name"] = "Win Games",
+        ["Rewards"] = {["Tokens"] = 300, ["Points"] = 20},
+        ["Requirement"] = 3,
+        ["Title"] = "Win <font color='rgb(200,200,255)'>3</font> games without dying"
+    },
+    ["PlayGames"] = {
+        ["Type"] = "Revives",
+        ["Name"] = "Play Games",
+        ["Rewards"] = {["Experience"] = 50, ["Points"] = 10},
+        ["Requirement"] = 6,
+        ["Title"] = "Play <font color='rgb(200,200,255)'>6</font> games"
+    },
+    ["Deployables"] = {
+        ["Type"] = "SpecialRound",
+        ["Name"] = "Deployables",
+        ["Rewards"] = {["Experience"] = 40, ["Points"] = 10},
+        ["Requirement"] = 6,
+        ["Title"] = "Build <font color='rgb(200,200,255)'>6</font> deployables"
+    },
+    ["Run"] = {
+        ["Type"] = "Distance",
+        ["Name"] = "Run",
+        ["OnlyAlertUponCompletion"] = true,
+        ["Rewards"] = {["Experience"] = 45, ["Points"] = 10},
+        ["Requirement"] = 5000,
+        ["Title"] = "Run for <font color='rgb(200,200,255)'>5000</font> studs"
+    },
+    ["SurviveSpecialRound"] = {
+        ["Type"] = "SpecialRound",
+        ["Name"] = "Special Round",
+        ["Rewards"] = {["Tokens"] = 250, ["Points"] = 15},
+        ["Requirement"] = 2,
+        ["Title"] = "Survive <font color='rgb(200,200,255)'>2</font> special rounds"
+    },
+    ["NoFlashlight"] = {
+        ["Type"] = "NoUtilities",
+        ["Name"] = "No Flashlight",
+        ["Rewards"] = {["Experience"] = 40, ["Points"] = 5},
+        ["Requirement"] = 2,
+        ["Title"] = "Win <font color='rgb(200,200,255)'>2</font> matches without any flashlights"
+    },
+    ["NoUsables"] = {
+        ["Type"] = "NoUsables",
+        ["Name"] = "No Usables",
+        ["Rewards"] = {["Experience"] = 40, ["Points"] = 10},
+        ["Requirement"] = 2,
+        ["Title"] = "Win <font color='rgb(200,200,255)'>2</font> matches without any usables/deployables"
+    },
+    ["Usables"] = {
+        ["Type"] = "Usables",
+        ["Name"] = "Usables",
+        ["Rewards"] = {["Experience"] = 40, ["Points"] = 10},
+        ["Requirement"] = 4,
+        ["Title"] = "Use <font color='rgb(200,200,255)'>4</font> consumables"
+    },
+    ["Revive"] = {
+        ["Type"] = "Revives",
+        ["Name"] = "Revive",
+        ["Rewards"] = {["Experience"] = 45, ["Points"] = 15},
+        ["Requirement"] = 10,
+        ["Title"] = "Revive <font color='rgb(200,200,255)'>10</font> people"
+    },
+    ["Objectives"] = {
+        ["Type"] = "Objectives",
+        ["Name"] = "Objectives",
+        ["Rewards"] = {["Experience"] = 75, ["Points"] = 20},
+        ["Requirement"] = 3,
+        ["Title"] = "Complete <font color='rgb(200,200,255)'>3</font> map objectives"
+    }
+}
