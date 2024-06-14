659b785aa024df1439dae9c0fd368edce0402abc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-14 22:08:26 +1000

    Create MapData.lua

---
+++ GameData/Tower Defense X [9503261072]/MapData.lua
@@ -0,0 +1,95 @@
+data = {
+    ["Chalet"] = {
+        ["Difficulty"] = "Normal",
+        ["Image"] = "http://www.roblox.com/asset/?id=15728751031",
+        ["Creator"] = "archmaestros"
+    },
+    ["Moon Outpost"] = {["Difficulty"] = "Easy", ["Image"] = "rbxassetid://15638522013", ["Creator"] = "V_eeer"},
+    ["Assembly Line"] = {
+        ["Difficulty"] = "Normal",
+        ["Image"] = "rbxassetid://17224097562",
+        ["Creator"] = "archmaestros"
+    },
+    ["Santa's Stronghold"] = {
+        ["Difficulty"] = "Easy",
+        ["IsEventOnlyMap"] = true,
+        ["Creator"] = "archmaestros",
+        ["Image"] = "rbxassetid://15657682478"
+    },
+    ["Research Base"] = {
+        ["Difficulty"] = "Simple",
+        ["Image"] = "rbxassetid://17310102408",
+        ["Creator"] = "archmaestros"
+    },
+    ["Scorched Passage"] = {
+        ["Difficulty"] = "Hard",
+        ["Image"] = "rbxassetid://17224095196",
+        ["Creator"] = "archmaestros"
+    },
+    ["Blade Works"] = {["Difficulty"] = "Impossible", ["Image"] = "rbxassetid://17221050497", ["Creator"] = "rikaforge"},
+    ["Obscure Island"] = {["Difficulty"] = "Hard", ["Image"] = "rbxassetid://14762424172", ["Creator"] = "V_eeer"},
+    ["Singularity"] = {["Difficulty"] = "Easy", ["Image"] = "rbxassetid://17224114428", ["Creator"] = "archmaestros"},
+    ["Treasure Cove"] = {["Difficulty"] = "Easy", ["Image"] = "rbxassetid://16283237864", ["Creator"] = "rikaforge"},
+    ["Danger Beach"] = {
+        ["Difficulty"] = "Normal",
+        ["Image"] = "rbxassetid://12381096186",
+        ["Creator"] = "ExtremelyHugeBadger"
+    },
+    ["Winter Fort"] = {["Difficulty"] = "Hard", ["Image"] = "rbxassetid://13747204507", ["Creator"] = "V_eeer"},
+    ["Limbo"] = {["Difficulty"] = "Hard", ["Image"] = "rbxassetid://17224114862", ["Creator"] = "rikaforge"},
+    ["Blox Out"] = {
+        ["Difficulty"] = "Easy",
+        ["Image"] = "rbxassetid://12381095833",
+        ["Creator"] = "ExtremelyHugeBadger",
+        ["DisplayName"] = "Blox Out"
+    },
+    ["Unforgiving Winter"] = {
+        ["Difficulty"] = "Hard",
+        ["Image"] = "rbxassetid://14005165044",
+        ["Creator"] = "ExtremelyHugeBadger"
+    },
+    ["Ancient Sky Island"] = {
+        ["Difficulty"] = "Simple",
+        ["Image"] = "rbxassetid://14762424368",
+        ["Creator"] = "epicdjgameking"
+    },
+    ["Purgatory"] = {["Difficulty"] = "Normal", ["Image"] = "rbxassetid://17224115263", ["Creator"] = "rikaforge"},
+    ["Military Harbor"] = {["Difficulty"] = "Normal", ["Image"] = "rbxassetid://12968961228", ["Creator"] = "Scrufus"},
+    ["Carrier"] = {["Difficulty"] = "Hard", ["Image"] = "rbxassetid://16283385109", ["Creator"] = "archmaestros"},
+    ["Volcanic Mishap"] = {["Difficulty"] = "Hard", ["Image"] = "rbxassetid://14762424541", ["Creator"] = "V_eeer"},
+    ["Fort Summit"] = {
+        ["Difficulty"] = "Easy",
+        ["Image"] = "http://www.roblox.com/asset/?id=15728752874",
+        ["Creator"] = "rikaforge"
+    },
+    ["SFOTH"] = {["Difficulty"] = "Hard", ["Image"] = "rbxassetid://12381095407", ["Creator"] = "ExtremelyHugeBadger"},
+    ["Vinland"] = {
+        ["Difficulty"] = "Normal",
+        ["Image"] = "http://www.roblox.com/asset/?id=15728754287",
+        ["Creator"] = "rikaforge"
+    },
+    ["Misleading Pond"] = {
+        ["Difficulty"] = "Hard",
+        ["Image"] = "rbxassetid://15360246041",
+        ["Creator"] = "archmaestros"
+    },
+    ["Vapor City"] = {["Difficulty"] = "Normal", ["Image"] = "rbxassetid://15360245792", ["Creator"] = "V_eeer"},
+    ["Oil Rig"] = {["Difficulty"] = "Simple", ["Image"] = "rbxassetid://12381094725", ["Creator"] = "V_eeer"},
+    ["Secret Forest"] = {["Difficulty"] = "Normal", ["Image"] = "rbxassetid://14005150270", ["Creator"] = "V_eeer"},
+    ["Cow Annoyance"] = {
+        ["Difficulty"] = "Normal",
+        ["Image"] = "rbxassetid://15360245910",
+        ["Creator"] = "archmaestros"
+    },
+    ["Sorrows Harbor"] = {
+        ["Difficulty"] = "Hard",
+        ["Image"] = "http://www.roblox.com/asset/?id=17224089112",
+        ["Creator"] = "rikaforge"
+    },
+    ["Deserted Island"] = {
+        ["Difficulty"] = "Impossible",
+        ["Image"] = "rbxassetid://14762424065",
+        ["Creator"] = "JOHN ROBLOX"
+    },
+    ["Apocalypse"] = {["Difficulty"] = "Easy", ["Image"] = "rbxassetid://12968961477", ["Creator"] = "V_eeer"}
+}
