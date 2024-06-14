3be94eaa59993b67beadc3935b36f25a196c3c38
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-14 22:09:50 +1000

    Create SkinData.lua

---
+++ GameData/Tower Defense X [9503261072]/SkinData.lua
@@ -0,0 +1,250 @@
+--game.ReplicatedStorage.SkinData.SkinTypes:Destroy()
+data = {
+    ["SkinUtilities"] = {
+        ["ValidateTowerSkinCombination"] = "function: 0xec71f6cc79592975",
+        ["GetAllSkinsForTower"] = "function: 0x587dfcfda05b0185",
+        ["GetSkinData"] = "function: 0xd765ccebcad92d95"
+    },
+    ["Skins"] = {
+        ["Artillery"] = {
+            ["MeshPartSkins"] = {},
+            ["ColorSkins"] = {
+                {["Name"] = "ChristmasV2", ["DisplayName"] = "Christmas"},
+                {["Name"] = "Beige"},
+                {["Name"] = "Black"},
+                {["Name"] = "Brown"},
+                {["Name"] = "Cyan"},
+                {["Name"] = "Deep Blue"},
+                {["Name"] = "Green"},
+                {["Name"] = "Light Blue"},
+                {["Name"] = "Light Pink"},
+                {["Name"] = "Orange"},
+                {["Name"] = "Pink"},
+                {["Name"] = "Purple"},
+                {["Name"] = "Red"},
+                {["Name"] = "White"},
+                {["Name"] = "Yellow"}
+            },
+            ["ModelSkins"] = {}
+        },
+        ["Laser Gunner"] = {
+            ["MeshPartSkins"] = {{["Name"] = "Christmas"}, {["Name"] = "Toxic"}},
+            ["ColorSkins"] = {
+                {["Name"] = "Beige"},
+                {["Name"] = "Black"},
+                {["Name"] = "Brown"},
+                {["Name"] = "Cyan"},
+                {["Name"] = "Deep Blue"},
+                {["Name"] = "Forest Green"},
+                {["Name"] = "Green"},
+                {["Name"] = "Light Blue"},
+                {["Name"] = "Light Pink"},
+                {["Name"] = "Orange"},
+                {["Name"] = "Pink"},
+                {["Name"] = "Purple"},
+                {["Name"] = "Red"},
+                {["Name"] = "White"},
+                {["Name"] = "Yellow"}
+            },
+            ["ModelSkins"] = {{["Name"] = "Retro"}}
+        },
+        ["John"] = {
+            ["MeshPartSkins"] = {{["Name"] = "Valentine's", ["DisplayName"] = "Jane"}, {["Name"] = "Eradicator"}},
+            ["ColorSkins"] = {},
+            ["ModelSkins"] = {}
+        },
+        ["Grenadier"] = {["MeshPartSkins"] = {}, ["ColorSkins"] = {{["Name"] = "Valentine's"}}, ["ModelSkins"] = {}},
+        ["Barracks"] = {
+            ["MeshPartSkins"] = {},
+            ["ColorSkins"] = {
+                {["Name"] = "Beige"},
+                {["Name"] = "Black"},
+                {["Name"] = "Brown"},
+                {["Name"] = "Cyan"},
+                {["Name"] = "Deep Blue"},
+                {["Name"] = "Forest Green"},
+                {["Name"] = "Green"},
+                {["Name"] = "Light Blue"},
+                {["Name"] = "Light Pink"},
+                {["Name"] = "Orange"},
+                {["Name"] = "Pink"},
+                {["Name"] = "Purple"},
+                {["Name"] = "Red"},
+                {["Name"] = "White"},
+                {["Name"] = "Yellow"}
+            },
+            ["ModelSkins"] = {}
+        },
+        ["Juggernaut"] = {
+            ["MeshPartSkins"] = {},
+            ["ColorSkins"] = {},
+            ["ModelSkins"] = {
+                {["Name"] = "Tower Battles"},
+                {["Name"] = "Ghost"},
+                {["Name"] = "RCU"},
+                {["Name"] = "WWI"},
+                {["Name"] = "Eradicator"}
+            }
+        },
+        ["Sniper"] = {
+            ["MeshPartSkins"] = {{["Name"] = "Neko"}, {["Name"] = "Toxic"}},
+            ["ColorSkins"] = {
+                {["Name"] = "Digital Urban Camo"},
+                {["Name"] = "Woodlands Camo"},
+                {["Name"] = "Digital Desert Camo"},
+                {["Name"] = "Forest Camo"},
+                {["Name"] = "Valentine's"},
+                {["Name"] = "Beige"},
+                {["Name"] = "Black"},
+                {["Name"] = "Brown"},
+                {["Name"] = "Cyan"},
+                {["Name"] = "Deep Blue"},
+                {["Name"] = "Forest Green"},
+                {["Name"] = "Green"},
+                {["Name"] = "Light Blue"},
+                {["Name"] = "Light Pink"},
+                {["Name"] = "Orange"},
+                {["Name"] = "Pink"},
+                {["Name"] = "Purple"},
+                {["Name"] = "Red"},
+                {["Name"] = "White"},
+                {["Name"] = "Yellow"}
+            },
+            ["ModelSkins"] = {{["Name"] = "Tower Battles"}}
+        },
+        ["Operator"] = {
+            ["MeshPartSkins"] = {{["Name"] = "Catgirl"}, {["Name"] = "Christmas"}},
+            ["ColorSkins"] = {
+                {["Name"] = "Beige"},
+                {["Name"] = "Black"},
+                {["Name"] = "Brown"},
+                {["Name"] = "Cyan"},
+                {["Name"] = "Deep Blue"},
+                {["Name"] = "Forest Green"},
+                {["Name"] = "Green"},
+                {["Name"] = "Light Blue"},
+                {["Name"] = "Light Pink"},
+                {["Name"] = "Orange"},
+                {["Name"] = "Pink"},
+                {["Name"] = "Purple"},
+                {["Name"] = "Red"},
+                {["Name"] = "White"},
+                {["Name"] = "Yellow"}
+            },
+            ["ModelSkins"] = {{["Name"] = "Tower Battles"}, {["Name"] = "Dealer"}, {["Name"] = "Valentine's"}}
+        },
+        ["Railgunner"] = {
+            ["MeshPartSkins"] = {},
+            ["ColorSkins"] = {
+                {["Name"] = "Digital Urban Camo"},
+                {["Name"] = "Woodlands Camo"},
+                {["Name"] = "Digital Desert Camo"},
+                {["Name"] = "Forest Camo"},
+                {["Name"] = "Valentine's"}
+            },
+            ["ModelSkins"] = {}
+        },
+        ["EDJ"] = {
+            ["MeshPartSkins"] = {},
+            ["ColorSkins"] = {
+                {["Name"] = "Valentine's"},
+                {["Name"] = "Beige"},
+                {["Name"] = "Black"},
+                {["Name"] = "Brown"},
+                {["Name"] = "Cyan"},
+                {["Name"] = "Deep Blue"},
+                {["Name"] = "Forest Green"},
+                {["Name"] = "Green"},
+                {["Name"] = "Light Blue"},
+                {["Name"] = "Light Pink"},
+                {["Name"] = "Orange"},
+                {["Name"] = "Pink"},
+                {["Name"] = "Purple"},
+                {["Name"] = "Red"},
+                {["Name"] = "White"},
+                {["Name"] = "Yellow"}
+            },
+            ["ModelSkins"] = {{["Name"] = "Catgirl"}, {["Name"] = "Tower Battles"}, {["Name"] = "Cyber Neko"}}
+        },
+        ["Golden Juggernaut"] = {
+            ["MeshPartSkins"] = {},
+            ["ColorSkins"] = {},
+            ["ModelSkins"] = {{["Name"] = "Golden Commando"}}
+        },
+        ["Patrol Boat"] = {
+            ["MeshPartSkins"] = {{["Name"] = "Christmas"}},
+            ["ColorSkins"] = {
+                {["Name"] = "Beige"},
+                {["Name"] = "Black"},
+                {["Name"] = "Brown"},
+                {["Name"] = "Cyan"},
+                {["Name"] = "Deep Blue"},
+                {["Name"] = "Forest Green"},
+                {["Name"] = "Green"},
+                {["Name"] = "Light Blue"},
+                {["Name"] = "Light Pink"},
+                {["Name"] = "Orange"},
+                {["Name"] = "Pink"},
+                {["Name"] = "Purple"},
+                {["Name"] = "Red"},
+                {["Name"] = "White"},
+                {["Name"] = "Yellow"}
+            },
+            ["ModelSkins"] = {}
+        },
+        ["Missile Trooper"] = {
+            ["MeshPartSkins"] = {},
+            ["ColorSkins"] = {{["Name"] = "Valentine's"}},
+            ["ModelSkins"] = {{["Name"] = "Tower Battles"}}
+        },
+        ["Ranger"] = {
+            ["MeshPartSkins"] = {{["Name"] = "WWII"}, {["Name"] = "Christmas"}},
+            ["ColorSkins"] = {},
+            ["ModelSkins"] = {}
+        },
+        ["Mine Layer"] = {
+            ["MeshPartSkins"] = {{["Name"] = "Christmas"}, {["Name"] = "Valentine's"}},
+            ["ColorSkins"] = {
+                {["Name"] = "Beige"},
+                {["Name"] = "Black"},
+                {["Name"] = "Brown"},
+                {["Name"] = "Cyan"},
+                {["Name"] = "Deep Blue"},
+                {["Name"] = "Forest Green"},
+                {["Name"] = "Green"},
+                {["Name"] = "Light Blue"},
+                {["Name"] = "Light Pink"},
+                {["Name"] = "Orange"},
+                {["Name"] = "Pink"},
+                {["Name"] = "Purple"},
+                {["Name"] = "Red"},
+                {["Name"] = "White"},
+                {["Name"] = "Yellow"}
+            },
+            ["ModelSkins"] = {}
+        },
+        ["Farm"] = {
+            ["MeshPartSkins"] = {{["Name"] = "Christmas"}},
+            ["ColorSkins"] = {
+                {["Name"] = "Beige"},
+                {["Name"] = "Black"},
+                {["Name"] = "Brown"},
+                {["Name"] = "Cyan"},
+                {["Name"] = "Deep Blue"},
+                {["Name"] = "Forest Green"},
+                {["Name"] = "Green"},
+                {["Name"] = "Light Blue"},
+                {["Name"] = "Light Pink"},
+                {["Name"] = "Orange"},
+                {["Name"] = "Pink"},
+                {["Name"] = "Purple"},
+                {["Name"] = "Red"},
+                {["Name"] = "Yellow"}
+            },
+            ["ModelSkins"] = {}
+        }
+    },
+    ["SkinEnums"] = {
+        ["SkinTypes"] = {["ColorSkin"] = "ColorSkin", ["ModelSkin"] = "ModelSkin", ["MeshPartSkin"] = "MeshPartSkin"}
+    }
+}
