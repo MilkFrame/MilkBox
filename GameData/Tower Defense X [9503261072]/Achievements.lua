44dd7aaf3ec904aa4339fcc55b2df37245f0e026
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-14 22:09:25 +1000

    Create Achievements.lua

---
+++ GameData/Tower Defense X [9503261072]/Achievements.lua
@@ -0,0 +1,382 @@
+data = {
+    ["KillRevivedPlagueDoctor2"] = {
+        ["ProgressNeeded"] = 6,
+        ["Name"] = "KillRevivedPlagueDoctor2",
+        ["ReadProgressFromDataPath"] = "BossKillMap>Revived Plague Doctor",
+        ["_Category"] = "Bosses",
+        ["Rewards"] = {{["Amount"] = 1250, ["Type"] = "Gold"}, {["Amount"] = 2750, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 16
+    },
+    ["Level1"] = {
+        ["_Category"] = "Level",
+        ["ProgressNeeded"] = 29400,
+        ["Name"] = "Level1",
+        ["ReadProgressFromDataPath"] = "XP",
+        ["BadgeId"] = 2153897183,
+        ["Rewards"] = {{["Amount"] = 100, ["Type"] = "Gold"}, {["Amount"] = 200, ["Type"] = "XP"}},
+        ["ConvertProgressToLevel"] = true,
+        ["PriorityInCategory"] = 1
+    },
+    ["KillZombies3"] = {
+        ["ProgressNeeded"] = 100000,
+        ["Name"] = "KillZombies3",
+        ["ReadProgressFromDataPath"] = "Kills",
+        ["_Category"] = "Kills",
+        ["Rewards"] = {{["Amount"] = 400, ["Type"] = "Gold"}, {["Amount"] = 1000, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 10
+    },
+    ["KillZombies2"] = {
+        ["ProgressNeeded"] = 10000,
+        ["Name"] = "KillZombies2",
+        ["ReadProgressFromDataPath"] = "Kills",
+        ["_Category"] = "Kills",
+        ["Rewards"] = {{["Amount"] = 225, ["Type"] = "Gold"}, {["Amount"] = 400, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 5
+    },
+    ["KillPlagueDoctor3"] = {
+        ["ProgressNeeded"] = 13,
+        ["Name"] = "KillPlagueDoctor3",
+        ["ReadProgressFromDataPath"] = "BossKillMap>Plague Doctor",
+        ["_Category"] = "Bosses",
+        ["Rewards"] = {{["Amount"] = 750, ["Type"] = "Gold"}, {["Amount"] = 2000, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 7
+    },
+    ["Win200"] = {
+        ["ProgressNeeded"] = 200,
+        ["Name"] = "Win200",
+        ["ReadProgressFromDataPath"] = "Wins",
+        ["_Category"] = "Wins",
+        ["Rewards"] = {{["Amount"] = 500, ["Type"] = "Gold"}, {["Amount"] = 800, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 10
+    },
+    ["KillEradicator2"] = {
+        ["ProgressNeeded"] = 5,
+        ["Name"] = "KillEradicator2",
+        ["ReadProgressFromDataPath"] = "BossKillMap>Eradicator",
+        ["_Category"] = "Bosses",
+        ["Rewards"] = {{["Amount"] = 650, ["Type"] = "Gold"}, {["Amount"] = 1700, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 11
+    },
+    ["Level8"] = {
+        ["_Category"] = "Level",
+        ["ProgressNeeded"] = 4935373,
+        ["Name"] = "Level8",
+        ["ReadProgressFromDataPath"] = "XP",
+        ["BadgeId"] = 2153897296,
+        ["Rewards"] = {{["Amount"] = 3000, ["Type"] = "Gold"}, {["Amount"] = 4000, ["Type"] = "XP"}},
+        ["ConvertProgressToLevel"] = true,
+        ["PriorityInCategory"] = 35
+    },
+    ["Time9"] = {
+        ["ProgressNeeded"] = 31536000,
+        ["Name"] = "Time9",
+        ["ReadProgressFromDataPath"] = "GameplayTime",
+        ["_Category"] = "TimePlayed",
+        ["Rewards"] = {{["Amount"] = 3500, ["Type"] = "Gold"}, {["Amount"] = 25000, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 40,
+        ["DisplayDivisor"] = 3600
+    },
+    ["KillPredator"] = {
+        ["ProgressNeeded"] = 1,
+        ["Name"] = "KillPredator",
+        ["ReadProgressFromDataPath"] = "BossKillMap>Predator",
+        ["_Category"] = "Bosses",
+        ["Rewards"] = {{["Amount"] = 175, ["Type"] = "Gold"}, {["Amount"] = 350, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 8
+    },
+    ["Win1"] = {
+        ["ProgressNeeded"] = 1,
+        ["Name"] = "Win1",
+        ["ReadProgressFromDataPath"] = "Wins",
+        ["_Category"] = "Wins",
+        ["Rewards"] = {{["Amount"] = 100, ["Type"] = "Gold"}, {["Amount"] = 200, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 1
+    },
+    ["Time5"] = {
+        ["ProgressNeeded"] = 180000,
+        ["Name"] = "Time5",
+        ["ReadProgressFromDataPath"] = "GameplayTime",
+        ["_Category"] = "TimePlayed",
+        ["Rewards"] = {{["Amount"] = 500, ["Type"] = "Gold"}, {["Amount"] = 300, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 20,
+        ["DisplayDivisor"] = 3600
+    },
+    ["Join"] = {
+        ["ProgressNeeded"] = 1,
+        ["Name"] = "Join",
+        ["_Category"] = "Base",
+        ["PriorityInCategory"] = 1,
+        ["Rewards"] = {{["Amount"] = 50, ["Type"] = "Gold"}, {["Amount"] = 100, ["Type"] = "XP"}}
+    },
+    ["Level3"] = {
+        ["_Category"] = "Level",
+        ["ProgressNeeded"] = 194450,
+        ["Name"] = "Level3",
+        ["ReadProgressFromDataPath"] = "XP",
+        ["BadgeId"] = 2153897206,
+        ["Rewards"] = {{["Amount"] = 375, ["Type"] = "Gold"}, {["Amount"] = 800, ["Type"] = "XP"}},
+        ["ConvertProgressToLevel"] = true,
+        ["PriorityInCategory"] = 10
+    },
+    ["Time6"] = {
+        ["ProgressNeeded"] = 360000,
+        ["Name"] = "Time6",
+        ["ReadProgressFromDataPath"] = "GameplayTime",
+        ["_Category"] = "TimePlayed",
+        ["Rewards"] = {{["Amount"] = 1000, ["Type"] = "Gold"}, {["Amount"] = 600, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 25,
+        ["DisplayDivisor"] = 3600
+    },
+    ["Time8"] = {
+        ["ProgressNeeded"] = 2592000,
+        ["Name"] = "Time8",
+        ["ReadProgressFromDataPath"] = "GameplayTime",
+        ["_Category"] = "TimePlayed",
+        ["Rewards"] = {{["Amount"] = 2500, ["Type"] = "Gold"}, {["Amount"] = 5000, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 35,
+        ["DisplayDivisor"] = 3600
+    },
+    ["FirstTowerAbility"] = {
+        ["ProgressNeeded"] = 1,
+        ["Name"] = "FirstTowerAbility",
+        ["ReadProgressFromDataPath"] = "TowerAbilitiesUsed",
+        ["_Category"] = "Gameplay",
+        ["Rewards"] = {{["Amount"] = 50, ["Type"] = "Gold"}, {["Amount"] = 50, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 5
+    },
+    ["KillPredator2"] = {
+        ["ProgressNeeded"] = 10,
+        ["Name"] = "KillPredator2",
+        ["ReadProgressFromDataPath"] = "BossKillMap>Predator",
+        ["_Category"] = "Bosses",
+        ["Rewards"] = {{["Amount"] = 1000, ["Type"] = "Gold"}, {["Amount"] = 2000, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 9
+    },
+    ["KillEradicator"] = {
+        ["ProgressNeeded"] = 1,
+        ["Name"] = "KillEradicator",
+        ["ReadProgressFromDataPath"] = "BossKillMap>Eradicator",
+        ["BadgeId"] = 2153897112,
+        ["Rewards"] = {{["Amount"] = 400, ["Type"] = "Gold"}, {["Amount"] = 1250, ["Type"] = "XP"}},
+        ["_Category"] = "Bosses",
+        ["PriorityInCategory"] = 10
+    },
+    ["Time4"] = {
+        ["ProgressNeeded"] = 90000,
+        ["Name"] = "Time4",
+        ["ReadProgressFromDataPath"] = "GameplayTime",
+        ["_Category"] = "TimePlayed",
+        ["Rewards"] = {{["Amount"] = 250, ["Type"] = "Gold"}, {["Amount"] = 150, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 15,
+        ["DisplayDivisor"] = 3600
+    },
+    ["KillZombies1"] = {
+        ["ProgressNeeded"] = 1000,
+        ["Name"] = "KillZombies1",
+        ["ReadProgressFromDataPath"] = "Kills",
+        ["_Category"] = "Kills",
+        ["Rewards"] = {{["Amount"] = 75, ["Type"] = "Gold"}, {["Amount"] = 50, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 1
+    },
+    ["KillZombies4"] = {
+        ["ProgressNeeded"] = 1000000,
+        ["Name"] = "KillZombies4",
+        ["ReadProgressFromDataPath"] = "Kills",
+        ["_Category"] = "Kills",
+        ["Rewards"] = {{["Amount"] = 1250, ["Type"] = "Gold"}, {["Amount"] = 3000, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 15
+    },
+    ["Level5"] = {
+        ["_Category"] = "Level",
+        ["ProgressNeeded"] = 990768,
+        ["Name"] = "Level5",
+        ["ReadProgressFromDataPath"] = "XP",
+        ["BadgeId"] = 2153897263,
+        ["Rewards"] = {{["Amount"] = 1500, ["Type"] = "Gold"}, {["Amount"] = 1600, ["Type"] = "XP"}},
+        ["ConvertProgressToLevel"] = true,
+        ["PriorityInCategory"] = 20
+    },
+    ["Level7"] = {
+        ["_Category"] = "Level",
+        ["ProgressNeeded"] = 2716628,
+        ["Name"] = "Level7",
+        ["ReadProgressFromDataPath"] = "XP",
+        ["BadgeId"] = 2153897280,
+        ["Rewards"] = {{["Amount"] = 2000, ["Type"] = "Gold"}, {["Amount"] = 3200, ["Type"] = "XP"}},
+        ["ConvertProgressToLevel"] = true,
+        ["PriorityInCategory"] = 30
+    },
+    ["Level6"] = {
+        ["_Category"] = "Level",
+        ["ProgressNeeded"] = 1346951,
+        ["Name"] = "Level6",
+        ["ReadProgressFromDataPath"] = "XP",
+        ["BadgeId"] = 2153897272,
+        ["Rewards"] = {{["Amount"] = 1850, ["Type"] = "Gold"}, {["Amount"] = 2400, ["Type"] = "XP"}},
+        ["ConvertProgressToLevel"] = true,
+        ["PriorityInCategory"] = 25
+    },
+    ["Level2"] = {
+        ["_Category"] = "Level",
+        ["ProgressNeeded"] = 95150,
+        ["Name"] = "Level2",
+        ["ReadProgressFromDataPath"] = "XP",
+        ["BadgeId"] = 2153897191,
+        ["Rewards"] = {{["Amount"] = 225, ["Type"] = "Gold"}, {["Amount"] = 400, ["Type"] = "XP"}},
+        ["ConvertProgressToLevel"] = true,
+        ["PriorityInCategory"] = 5
+    },
+    ["Level4"] = {
+        ["_Category"] = "Level",
+        ["ProgressNeeded"] = 435892,
+        ["Name"] = "Level4",
+        ["ReadProgressFromDataPath"] = "XP",
+        ["BadgeId"] = 2153897247,
+        ["Rewards"] = {{["Amount"] = 1000, ["Type"] = "Gold"}, {["Amount"] = 1200, ["Type"] = "XP"}},
+        ["ConvertProgressToLevel"] = true,
+        ["PriorityInCategory"] = 15
+    },
+    ["FirstHardMap"] = {
+        ["ProgressNeeded"] = 1,
+        ["Name"] = "FirstHardMap",
+        ["ReadProgressFromDataPath"] = "WinsPerMapDifficulty>Hard",
+        ["_Category"] = "Gameplay",
+        ["Rewards"] = {{["Amount"] = 225, ["Type"] = "Gold"}, {["Amount"] = 450, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 15
+    },
+    ["KillRevivedPlagueDoctor"] = {
+        ["ProgressNeeded"] = 1,
+        ["Name"] = "KillRevivedPlagueDoctor",
+        ["ReadProgressFromDataPath"] = "BossKillMap>Revived Plague Doctor",
+        ["BadgeId"] = 3416298406790691,
+        ["Rewards"] = {{["Amount"] = 550, ["Type"] = "Gold"}, {["Amount"] = 2000, ["Type"] = "XP"}},
+        ["_Category"] = "Bosses",
+        ["PriorityInCategory"] = 15
+    },
+    ["KillEradicatorMK2"] = {
+        ["ProgressNeeded"] = 1,
+        ["Name"] = "KillEradicatorMK2",
+        ["ReadProgressFromDataPath"] = "BossKillMap>EradicatorMK2",
+        ["BadgeId"] = 2153897366,
+        ["Rewards"] = {{["Amount"] = 1000, ["Type"] = "Gold"}, {["Amount"] = 3500, ["Type"] = "XP"}},
+        ["_Category"] = "Bosses",
+        ["PriorityInCategory"] = 20
+    },
+    ["KillZombies5"] = {
+        ["ProgressNeeded"] = 10000000,
+        ["Name"] = "KillZombies5",
+        ["ReadProgressFromDataPath"] = "Kills",
+        ["_Category"] = "Kills",
+        ["Rewards"] = {{["Amount"] = 2000, ["Type"] = "Gold"}, {["Amount"] = 10000, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 20
+    },
+    ["KillPlagueDoctor2"] = {
+        ["ProgressNeeded"] = 6,
+        ["Name"] = "KillPlagueDoctor2",
+        ["ReadProgressFromDataPath"] = "BossKillMap>Plague Doctor",
+        ["_Category"] = "Bosses",
+        ["Rewards"] = {{["Amount"] = 500, ["Type"] = "Gold"}, {["Amount"] = 1000, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 5
+    },
+    ["Time2"] = {
+        ["ProgressNeeded"] = 18000,
+        ["Name"] = "Time2",
+        ["ReadProgressFromDataPath"] = "GameplayTime",
+        ["_Category"] = "TimePlayed",
+        ["Rewards"] = {{["Amount"] = 100, ["Type"] = "Gold"}, {["Amount"] = 75, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 5,
+        ["DisplayDivisor"] = 3600
+    },
+    ["Win1000"] = {
+        ["ProgressNeeded"] = 1000,
+        ["Name"] = "Win1000",
+        ["ReadProgressFromDataPath"] = "Wins",
+        ["_Category"] = "Wins",
+        ["Rewards"] = {{["Amount"] = 4500, ["Type"] = "Gold"}, {["Amount"] = 10000, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 20
+    },
+    ["AbilitySpammer"] = {
+        ["ProgressNeeded"] = 100,
+        ["Name"] = "AbilitySpammer",
+        ["ReadProgressFromDataPath"] = "TowerAbilitiesUsed",
+        ["_Category"] = "Gameplay",
+        ["Rewards"] = {{["Amount"] = 50, ["Type"] = "Gold"}, {["Amount"] = 50, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 10
+    },
+    ["FirstImpossibleMap"] = {
+        ["ProgressNeeded"] = 1,
+        ["Name"] = "FirstImpossibleMap",
+        ["ReadProgressFromDataPath"] = "WinsPerMapDifficulty>Impossible",
+        ["_Category"] = "Gameplay",
+        ["Rewards"] = {{["Amount"] = 300, ["Type"] = "Gold"}, {["Amount"] = 800, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 20
+    },
+    ["KillPlagueDoctor"] = {
+        ["ProgressNeeded"] = 1,
+        ["Name"] = "KillPlagueDoctor",
+        ["ReadProgressFromDataPath"] = "BossKillMap>Plague Doctor",
+        ["BadgeId"] = 2153897158,
+        ["Rewards"] = {{["Amount"] = 275, ["Type"] = "Gold"}, {["Amount"] = 350, ["Type"] = "XP"}},
+        ["_Category"] = "Bosses",
+        ["PriorityInCategory"] = 1
+    },
+    ["Win500"] = {
+        ["ProgressNeeded"] = 500,
+        ["Name"] = "Win500",
+        ["ReadProgressFromDataPath"] = "Wins",
+        ["_Category"] = "Wins",
+        ["Rewards"] = {{["Amount"] = 1500, ["Type"] = "Gold"}, {["Amount"] = 2000, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 15
+    },
+    ["Win5"] = {
+        ["ProgressNeeded"] = 5,
+        ["Name"] = "Win5",
+        ["ReadProgressFromDataPath"] = "Wins",
+        ["_Category"] = "Wins",
+        ["Rewards"] = {{["Amount"] = 150, ["Type"] = "Gold"}, {["Amount"] = 250, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 2
+    },
+    ["Time7"] = {
+        ["ProgressNeeded"] = 604800,
+        ["Name"] = "Time7",
+        ["ReadProgressFromDataPath"] = "GameplayTime",
+        ["_Category"] = "TimePlayed",
+        ["Rewards"] = {{["Amount"] = 1500, ["Type"] = "Gold"}, {["Amount"] = 2250, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 30,
+        ["DisplayDivisor"] = 3600
+    },
+    ["Win50"] = {
+        ["ProgressNeeded"] = 50,
+        ["Name"] = "Win50",
+        ["ReadProgressFromDataPath"] = "Wins",
+        ["_Category"] = "Wins",
+        ["Rewards"] = {{["Amount"] = 250, ["Type"] = "Gold"}, {["Amount"] = 400, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 5
+    },
+    ["Time1"] = {
+        ["ProgressNeeded"] = 3600,
+        ["Name"] = "Time1",
+        ["ReadProgressFromDataPath"] = "GameplayTime",
+        ["_Category"] = "TimePlayed",
+        ["Rewards"] = {{["Amount"] = 75, ["Type"] = "Gold"}, {["Amount"] = 75, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 1,
+        ["DisplayDivisor"] = 3600
+    },
+    ["Time3"] = {
+        ["ProgressNeeded"] = 36000,
+        ["Name"] = "Time3",
+        ["ReadProgressFromDataPath"] = "GameplayTime",
+        ["_Category"] = "TimePlayed",
+        ["Rewards"] = {{["Amount"] = 175, ["Type"] = "Gold"}, {["Amount"] = 100, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 10,
+        ["DisplayDivisor"] = 3600
+    },
+    ["FirstTower"] = {
+        ["ProgressNeeded"] = 1,
+        ["Name"] = "FirstTower",
+        ["ReadProgressFromDataPath"] = "TowersPlaced",
+        ["_Category"] = "Gameplay",
+        ["Rewards"] = {{["Amount"] = 50, ["Type"] = "Gold"}, {["Amount"] = 75, ["Type"] = "XP"}},
+        ["PriorityInCategory"] = 1
+    }
+}
