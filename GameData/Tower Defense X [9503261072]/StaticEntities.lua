c68a10734ffab00858d1e983d31da2f36141c672
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-14 22:11:13 +1000

    Create StaticEntities.lua

---
+++ GameData/Tower Defense X [9503261072]/StaticEntities.lua
@@ -0,0 +1,21 @@
+ata = {
+    ["Configs"] = {
+        ["Mine"] = {
+            ["ExplosionDelay"] = 0.05,
+            ["Radius"] = 0.1,
+            ["VariableExplosionEffects"] = true,
+            ["RotationOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, -4.37113883e-08, -1, 0, 1, -4.37113883e-08),
+            ["AnimationTimeScale"] = 1,
+            ["VariableModel"] = true,
+            ["ExplosionEffectNames"] = {"MineExplosion"},
+            ["NoAnimations"] = true
+        },
+        ["BombardierC4"] = {
+            ["Radius"] = 0.1,
+            ["RotationOffset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, -4.37113883e-08, -1, 0, 1, -4.37113883e-08),
+            ["AnimationTimeScale"] = 1,
+            ["ExplosionEffectNames"] = {"BombardierC4Explosion"},
+            ["NoAnimations"] = true
+        }
+    }
+}
