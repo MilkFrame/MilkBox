1a9f1bd37fe445e18e67a7c1be4f1e8a8c1b5ad9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-15 21:10:40 +1000

    Create Actions.lua

---
+++ GameData/Anime Defenders [17017769292]/Actions.lua
@@ -0,0 +1,506 @@
+data = {
+    ["EMOTE_STC_STOP"] = {
+        ["hasBinds"] = true,
+        ["name"] = "Ya4InXHEPPx3La",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xa67cdd21c60ed8d5",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0xff93cc6ca8234365"
+    },
+    ["UNIT_SELL"] = {
+        ["hasBinds"] = false,
+        ["name"] = "CHtJPxyw6",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x962166d1dbc70965",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x36e9932cfbf7b045"
+    },
+    ["INSPECT"] = {
+        ["hasBinds"] = true,
+        ["name"] = "hGNiPxJ",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x77b97b3441b56e85",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x86859a10a4923725"
+    },
+    ["LEVEL_UP"] = {
+        ["hasBinds"] = true,
+        ["name"] = "ujk8HoW0",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xa1eeeae3003bd055",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x182ae18882018625"
+    },
+    ["USE_DASH"] = {
+        ["hasBinds"] = true,
+        ["name"] = "VKZIlTSY",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x82cd12659562d195",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x0d7c73eb2f9421a5"
+    },
+    ["CREATE_GRID_PROMPT"] = {
+        ["hasBinds"] = true,
+        ["name"] = "81bs6qGnAYcc6CGm3o",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x57cea2c45f6d9375",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x4ae439c5177d7d85"
+    },
+    ["REQUEST_UPDATE_TITLE"] = {
+        ["hasBinds"] = false,
+        ["name"] = "1wuVGst5oh8F0hhFkIwc",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x8a1d8f811dfc2935",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x8864e62e8a8a24a5"
+    },
+    ["START_MATCH"] = {
+        ["hasBinds"] = false,
+        ["name"] = "H0g2DeepnX8",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x826985b117c7dac5",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x23f070f01dc99685"
+    },
+    ["LIKE_REWARD_CLAIMED"] = {
+        ["hasBinds"] = true,
+        ["name"] = "rTAhkqKvZC9qWyeGPoA",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x097a96747c9733c5",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x854480c675be9405"
+    },
+    ["UNIT_UNEQUIP_ALL"] = {
+        ["hasBinds"] = false,
+        ["name"] = "gLEAvoVkbcL9kAQY",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x938cf88da4e03335",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x59358af8a40888a5"
+    },
+    ["EMOTE_EQUIP_TOGGLE"] = {
+        ["hasBinds"] = false,
+        ["name"] = "9yNIHoC3QPX8KwdSct",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x8abc78e12d0e6f95",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0xd54500cbed2cfae5"
+    },
+    ["UNIT_SAVE_TEAM"] = {
+        ["hasBinds"] = false,
+        ["name"] = "DyzIhve0nRLlfU",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x92909310ca46bba5",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x5cca72053e638cc5"
+    },
+    ["UNIT_FORCE_DESELECT"] = {
+        ["hasBinds"] = true,
+        ["name"] = "xfTedsdhWLJFRkWj6Qa",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xeaba4e956b76f505",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x39a327e6b932c125"
+    },
+    ["POTION_BUY"] = {
+        ["hasBinds"] = false,
+        ["name"] = "LXKHys7AHV",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xaa9ca240a69896f5",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x2652ef4740741025"
+    },
+    ["EMOTE_CTS_STOP"] = {
+        ["hasBinds"] = false,
+        ["name"] = "e4svNdikmnOj1a",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xe3486eef7cc84a05",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x89e59576d119af05"
+    },
+    ["DISPLAY_NOTIFICATION"] = {
+        ["hasBinds"] = true,
+        ["name"] = "SNV7bwyD5XOYvTm2Nzkg",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xb8ffdcd7aee8d145",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x653676b0098bf0e5"
+    },
+    ["UNIT_PLACE"] = {
+        ["hasBinds"] = true,
+        ["name"] = "dGmrMrDNFL",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x116bc9458d4eb5f5",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x9ff2af41f2c10da5"
+    },
+    ["UNIT_TOGGLE_ACTIVE"] = {
+        ["hasBinds"] = true,
+        ["name"] = "j7LV00lX088yDLsjvh",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x09d25c2de61792a5",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x92fc14cc51783265"
+    },
+    ["QUEST_CLAIM"] = {
+        ["hasBinds"] = false,
+        ["name"] = "WEIZarCAJ7k",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xabfbd763b8093a55",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x331fda502dbd3a65"
+    },
+    ["LOCK_CONTROLS"] = {
+        ["hasBinds"] = true,
+        ["name"] = "kWpNEZtxjac1G",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x0112a11dbadfce95",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x5879e2dad3e28a45"
+    },
+    ["RAID_OPEN_SHOP"] = {
+        ["hasBinds"] = true,
+        ["name"] = "dOh55dZyoEHcRn",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x10ca8f84306e1635",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0xb653e3531874b3c5"
+    },
+    ["UNIT_LOAD_TEAM"] = {
+        ["hasBinds"] = false,
+        ["name"] = "mDtaHPmWJDWbUT",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x3df88953e26fd245",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x4e5c521a8b176185"
+    },
+    ["AUTO_DELETE_RARITY"] = {
+        ["hasBinds"] = false,
+        ["name"] = "3gISGOb0Gsnv95Ov6q",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x53004037f57a0ee5",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x7a95293b7c9f4b45"
+    },
+    ["INSPECT_CLOSE"] = {
+        ["hasBinds"] = true,
+        ["name"] = "gJK2nzbe20pPB",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x208d9c6c2bfc31d5",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x6cef94bb6bc1a605"
+    },
+    ["XP_TOGGLE_VISIBLE"] = {
+        ["hasBinds"] = true,
+        ["name"] = "FkbRJIp5rlGg5aDJ9",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x6a42f97cdff42fe5",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x1939725eb2b10085"
+    },
+    ["LEAVE_MATCH"] = {
+        ["hasBinds"] = false,
+        ["name"] = "7ZNeqy7UGFm",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x9ba35c5241211f65",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0xc7835ed2d745fc45"
+    },
+    ["EMOTE_DELETE"] = {
+        ["hasBinds"] = false,
+        ["name"] = "G0rm6bgTiq40",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x4b245b03cfbd1bb5",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x3c0237dd0be245a5"
+    },
+    ["CHANGE_MATCH_DATA"] = {
+        ["hasBinds"] = false,
+        ["name"] = "QfESLHiZtapd0l4cC",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xbcc8ea5069f43745",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0xf78bb9ff9ad67785"
+    },
+    ["REWARD_NOTIFICATION"] = {
+        ["hasBinds"] = true,
+        ["name"] = "wxZA7eI4tB3CQHBe8uX",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xc02f47a394d97315",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x53a0c16a78cee6c5"
+    },
+    ["JOIN_MATCH"] = {
+        ["hasBinds"] = false,
+        ["name"] = "fAung68qBQ",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xccb421a01b0cf515",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0xa9358b1e698965e5"
+    },
+    ["TRACK_EVENT"] = {
+        ["hasBinds"] = false,
+        ["name"] = "hOCDiaIL2OW",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xdd9014f020529d25",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x00a0a430b9431bc5"
+    },
+    ["RANK_FREEZE_STAT"] = {
+        ["hasBinds"] = true,
+        ["name"] = "WCXZeZoxwmF6fGGF",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x50af310376ab31b5",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x84273e858ae2b6e5"
+    },
+    ["SELL_ITEM"] = {
+        ["hasBinds"] = false,
+        ["name"] = "iznLLEF9z",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x2dd5f840c493ad75",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x9a375f9af60f8925"
+    },
+    ["BUY_ITEM"] = {
+        ["hasBinds"] = false,
+        ["name"] = "vQlSbRln",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x7c280530aad3f485",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0xdc4a0145476c3c05"
+    },
+    ["UNIT_LOCK"] = {
+        ["hasBinds"] = false,
+        ["name"] = "ZwOmgmxhB",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x2c0432e072f057d5",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x46c3700454a99165"
+    },
+    ["LOCK_CAMERA"] = {
+        ["hasBinds"] = true,
+        ["name"] = "iPxDw6Ihk2o",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x599f6a3f3b7a70e5",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x4d01850be0ae9c65"
+    },
+    ["EMOTE_CTS_PLAY"] = {
+        ["hasBinds"] = false,
+        ["name"] = "gYdW9cMDY5cvfi",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x1c20eb51b7331465",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0xaf54feb5e234e245"
+    },
+    ["TOGGLE_PROXIMITY_LOCK"] = {
+        ["hasBinds"] = true,
+        ["name"] = "h3b29aRZDBz9AQgMoyehk",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xf939319527ebd005",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0xecb894bb761c3c85"
+    },
+    ["UNIT_TOGGLE_COSMETIC"] = {
+        ["hasBinds"] = false,
+        ["name"] = "gfsUaWL6T3gPyEqyxUMw",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x0cc46581f872fc35",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0xf1d87693b0c3eaa5"
+    },
+    ["DELETE_ITEM"] = {
+        ["hasBinds"] = false,
+        ["name"] = "nGCL6Rrv5bQ",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x3c6830f1c3ad4dc5",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0xba67cce903fef885"
+    },
+    ["EMOTE_STC_PLAY"] = {
+        ["hasBinds"] = true,
+        ["name"] = "y4QTCTVrU622C1",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x66a8b011af46cf85",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x4505366a59bd5e05"
+    },
+    ["UNIT_EQUIP_BEST"] = {
+        ["hasBinds"] = false,
+        ["name"] = "c82BS7NZHmcWep6",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x0fb7c72161145495",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x7cee1d2e92ddc4e5"
+    },
+    ["PRINT_TO_CONSOLE"] = {
+        ["hasBinds"] = true,
+        ["name"] = "V2F8nyUfIkyKasnT",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x5741137201c41be5",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x50c89f55793b2865"
+    },
+    ["GAME_MODE_SELECTED_CTS"] = {
+        ["hasBinds"] = false,
+        ["name"] = "HbzJqOEuyZ54jsHGrS0nrF",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x1fd04e110fddf4a5",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0xb50926ac2d3ad2c5"
+    },
+    ["EQUIP_TITLE"] = {
+        ["hasBinds"] = false,
+        ["name"] = "TUMl1SjhNIG",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x2f54b2c11595b3f5",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0xcf84486a7c972225"
+    },
+    ["TELEPORT_PLAYER"] = {
+        ["hasBinds"] = true,
+        ["name"] = "9biz6g27hyCRsAv",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x991106ed526291a5",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0xa6f0e5b1e048b385"
+    },
+    ["RESET_KEYBINDS"] = {
+        ["hasBinds"] = false,
+        ["name"] = "ZdZIQHHlfigq2J",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xbb7923b598a170c5",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x12aea8c675af1a85"
+    },
+    ["EMOTE_UNEQUIP_ALL"] = {
+        ["hasBinds"] = false,
+        ["name"] = "rZ6LiHkniQwXBWdQP",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xff38a392d36e5f05",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x2071bb13ce40ad05"
+    },
+    ["CREATE_MATCH"] = {
+        ["hasBinds"] = false,
+        ["name"] = "JP8mGSgpxXto",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x2ee479a75c28ed55",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x9ae6ba49df0c1865"
+    },
+    ["UI_TOGGLE_TOOLBAR"] = {
+        ["hasBinds"] = true,
+        ["name"] = "Jf4H3wGpLaIdU4jYS",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x22e257ad543d5355",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x7f2a5de168413405"
+    },
+    ["FRAMEWORK_GLOBAL_CHAT_ANNOUNCEMENT"] = {
+        ["hasBinds"] = true,
+        ["name"] = "VsrRIf9kkCiJTRYaJp1GyLt5pymUsi2P4S",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xc73f366eff01a815",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0xcf44af73373310e5"
+    },
+    ["TOGGLE_BLUR"] = {
+        ["hasBinds"] = true,
+        ["name"] = "bMtixbockU3",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x771cf8066bbd28b5",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0xbb143693bc0fe7e5"
+    },
+    ["UNIT_DELETE"] = {
+        ["hasBinds"] = false,
+        ["name"] = "bGZQwpXvloF",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x5d4196f398f835e5",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x136e2f8668a88d45"
+    },
+    ["SET_GIFT_RECIPIENT_ID"] = {
+        ["hasBinds"] = false,
+        ["name"] = "TALjBOV4u0lFJjD2Vwvxh",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x4da58e0f82a716b5",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0xa5d105c439ab73a5"
+    },
+    ["EMOTE_PLAY"] = {
+        ["hasBinds"] = true,
+        ["name"] = "IMOv5TtjJt",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xb69f44c688807875",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x72983931d5586f25"
+    },
+    ["UNIT_EQUIP_UNEQUIP"] = {
+        ["hasBinds"] = false,
+        ["name"] = "vW1nZ20X0Gd9WM0oUz",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xcdb49ce64727be15",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x30d8e1b303c257e5"
+    },
+    ["POTION_ACTIVATE"] = {
+        ["hasBinds"] = false,
+        ["name"] = "XrBreiU5HHRp1yR",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xda50a3b27ddc9225",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0xf9557799929db9c5"
+    },
+    ["RAID_CONFIRM_JOIN"] = {
+        ["hasBinds"] = false,
+        ["name"] = "jQ9WKdgjaqVsmOZUV",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x520b555e33e49675",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x032fc27fe13ecb25"
+    },
+    ["UNIT_REORDER"] = {
+        ["hasBinds"] = false,
+        ["name"] = "RfJsj1KsrKI2",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x7a3bcef2f5aeb185",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x74a9735db023d205"
+    },
+    ["REQUEST_PITY"] = {
+        ["hasBinds"] = false,
+        ["name"] = "cuxgqywhprAv",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xa24347262e6672d5",
+        ["isReplicated"] = true,
+        ["Connect"] = "function: 0x2e3ce4bb4770ef65"
+    },
+    ["CREATE_PROMPT"] = {
+        ["hasBinds"] = true,
+        ["name"] = "FuJkE8BfmSCC4",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xd81ead6f9819b725",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0xec6fab0be7406fa5"
+    },
+    ["RAID_CONFIRMATION_PROMPT"] = {
+        ["hasBinds"] = true,
+        ["name"] = "8YvIq0KQxgylLLYXY0wODlia",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0xe8e180dc0d371265",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x58ba37b5e0260705"
+    },
+    ["UI_CALL"] = {
+        ["hasBinds"] = true,
+        ["name"] = "ig8Of2Z",
+        ["FireClient"] = "function: 0x3ca15a2aa8835045",
+        ["Fire"] = "function: 0x916d1fbd49a370f5",
+        ["isReplicated"] = false,
+        ["Connect"] = "function: 0x7fd23affd884de45"
+    }
+}
