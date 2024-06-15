721af0c93b1dd0ba408bdc3303c1c7e7fbddc959
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-15 21:19:22 +1000

    Create LightingExport.lua

---
+++ GameData/Anime Defenders [17017769292]/LightingExport.lua
@@ -0,0 +1,44 @@
+data = {
+    ["ColorCorrectionEffect"] = {
+        ["Enabled"] = true,
+        ["Saturation"] = 0.30000001192092896,
+        ["Contrast"] = 0.20000000298023224,
+        ["TintColor"] = Color3.fromRGB(255, 255, 255),
+        ["Brightness"] = -0.10000000149011612
+    },
+    ["Lighting"] = {
+        ["ClockTime"] = 12.199999809265137,
+        ["ColorShift_Bottom"] = Color3.fromRGB(219, 112, 255),
+        ["FogColor"] = Color3.fromRGB(192, 192, 192),
+        ["FogEnd"] = 100000,
+        ["TimeOfDay"] = "12:12:00",
+        ["ShadowSoftness"] = 0.20000000298023224,
+        ["EnvironmentDiffuseScale"] = 0.6579999923706055,
+        ["EnvironmentSpecularScale"] = 0.289000004529953,
+        ["GlobalShadows"] = true,
+        ["FogStart"] = 0,
+        ["Ambient"] = Color3.fromRGB(106, 133, 208),
+        ["OutdoorAmbient"] = Color3.fromRGB(103, 133, 203),
+        ["GeographicLatitude"] = 46.11954879760742,
+        ["Brightness"] = 1.2999999523162842,
+        ["ColorShift_Top"] = Color3.fromRGB(196, 128, 255),
+        ["ExposureCompensation"] = 0.1599999964237213
+    },
+    ["Atmosphere"] = {
+        ["Density"] = 0.41999998688697815,
+        ["Offset"] = 0,
+        ["Color"] = Color3.fromRGB(83, 226, 255),
+        ["Decay"] = Color3.fromRGB(27, 44, 92),
+        ["Haze"] = 2.2200000286102295,
+        ["Glare"] = 0
+    },
+    ["DepthOfFieldEffect"] = {
+        ["Enabled"] = true,
+        ["FarIntensity"] = 1,
+        ["FocusDistance"] = 104,
+        ["InFocusRadius"] = 55,
+        ["NearIntensity"] = 0
+    },
+    ["BloomEffect"] = {["Threshold"] = 1.2799999713897705, ["Intensity"] = 1, ["Size"] = 16},
+    ["BlurEffect"] = {["Enabled"] = true, ["Size"] = 1}
+}
