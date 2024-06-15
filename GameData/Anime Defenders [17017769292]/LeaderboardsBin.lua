0c82563e0c7d3c6bdc0eb93c20570ebe63c1e106
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-15 21:19:09 +1000

    Create LeaderboardsBin.lua

---
+++ GameData/Anime Defenders [17017769292]/LeaderboardsBin.lua
@@ -0,0 +1,44 @@
+data = {
+    ["MobCityInfinite"] = {
+        ["AllTimeFunc"] = "function: 0x1d0f9c36aa998625",
+        ["Variants"] = {"AllTime"},
+        ["CountryVariants"] = {}
+    },
+    ["Robux"] = {["AllTimeFunc"] = "function: 0xed03ad25344520a5", ["Variants"] = {"AllTime"}, ["CountryVariants"] = {}},
+    ["PlayerLevel"] = {
+        ["AllTimeFunc"] = "function: 0x558ba56517b90905",
+        ["Variants"] = {"AllTime"},
+        ["CountryVariants"] = {}
+    },
+    ["Gems"] = {["AllTimeFunc"] = "function: 0xb408189917ccd5e5", ["Variants"] = {"AllTime"}, ["CountryVariants"] = {}},
+    ["TotalPower"] = {
+        ["AllTimeFunc"] = "function: 0xccbe43008f7f2225",
+        ["Variants"] = {"AllTime", "Weekly"},
+        ["CountryVariants"] = {}
+    },
+    ["CursedAcademyInfinite"] = {
+        ["AllTimeFunc"] = "function: 0x7bc9b876b27540f5",
+        ["Variants"] = {"AllTime"},
+        ["CountryVariants"] = {}
+    },
+    ["Kills"] = {
+        ["AllTimeFunc"] = "function: 0x17430b19d72b9df5",
+        ["Variants"] = {"AllTime", "Weekly"},
+        ["CountryVariants"] = {}
+    },
+    ["UnderwaterTempleInfinite"] = {
+        ["AllTimeFunc"] = "function: 0xe4394ee39b67ad95",
+        ["Variants"] = {"AllTime"},
+        ["CountryVariants"] = {}
+    },
+    ["BluePlanetInfinite"] = {
+        ["AllTimeFunc"] = "function: 0x59f3177e15b20315",
+        ["Variants"] = {"AllTime"},
+        ["CountryVariants"] = {}
+    },
+    ["FooshaVillageInfinite"] = {
+        ["AllTimeFunc"] = "function: 0x47c926bfe149cea5",
+        ["Variants"] = {"AllTime"},
+        ["CountryVariants"] = {}
+    }
+}
