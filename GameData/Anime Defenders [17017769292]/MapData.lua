bd1da922aa5634a43c71a90c38980646d5ff04df
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-15 21:16:19 +1000

    Create MapData.lua

---
+++ GameData/Anime Defenders [17017769292]/MapData.lua
@@ -0,0 +1,84 @@
+data = {
+    ["CursedAcademy"] = {
+        ["Chapters"] = {
+            "CursedAcademy_Chapter1",
+            "CursedAcademy_Chapter2",
+            "CursedAcademy_Chapter3",
+            "CursedAcademy_Chapter4",
+            "CursedAcademy_Chapter5",
+            "CursedAcademy_Chapter6",
+            "CursedAcademy_Infinite"
+        },
+        ["Index"] = 3,
+        ["IsStoryMode"] = true,
+        ["ImageId"] = "rbxassetid://17534341034",
+        ["DisplayName"] = "Cursed Academy"
+    },
+    ["MobCity"] = {
+        ["Chapters"] = {
+            "MobCity_Chapter1",
+            "MobCity_Chapter2",
+            "MobCity_Chapter3",
+            "MobCity_Chapter4",
+            "MobCity_Chapter5",
+            "MobCity_Chapter6",
+            "MobCity_Infinite"
+        },
+        ["Index"] = 2,
+        ["IsStoryMode"] = true,
+        ["ImageId"] = "rbxassetid://17534340205",
+        ["DisplayName"] = "Mob City"
+    },
+    ["BluePlanet"] = {
+        ["Chapters"] = {
+            "BluePlanet_Chapter1",
+            "BluePlanet_Chapter2",
+            "BluePlanet_Chapter3",
+            "BluePlanet_Chapter4",
+            "BluePlanet_Chapter5",
+            "BluePlanet_Chapter6",
+            "BluePlanet_Infinite"
+        },
+        ["Index"] = 4,
+        ["IsStoryMode"] = true,
+        ["ImageId"] = "rbxassetid://17690526241",
+        ["DisplayName"] = "Blue Planet"
+    },
+    ["UnderwaterTemple"] = {
+        ["Chapters"] = {
+            "UnderwaterTemple_Chapter1",
+            "UnderwaterTemple_Chapter2",
+            "UnderwaterTemple_Chapter3",
+            "UnderwaterTemple_Chapter4",
+            "UnderwaterTemple_Chapter5",
+            "UnderwaterTemple_Chapter6",
+            "UnderwaterTemple_Infinite"
+        },
+        ["Index"] = 5,
+        ["IsStoryMode"] = true,
+        ["ImageId"] = "rbxassetid://17690525831",
+        ["DisplayName"] = "Underwater Temple"
+    },
+    ["FooshaVillage"] = {
+        ["Chapters"] = {
+            "FooshaVillage_Chapter1",
+            "FooshaVillage_Chapter2",
+            "FooshaVillage_Chapter3",
+            "FooshaVillage_Chapter4",
+            "FooshaVillage_Chapter5",
+            "FooshaVillage_Chapter6",
+            "FooshaVillage_Infinite"
+        },
+        ["Index"] = 1,
+        ["IsStoryMode"] = true,
+        ["ImageId"] = "rbxassetid://17534340660",
+        ["DisplayName"] = "Windmill Village"
+    },
+    ["CursedDungeon"] = {
+        ["Chapters"] = {"CursedDungeon_Chapter1"},
+        ["Index"] = 999,
+        ["IsStoryMode"] = false,
+        ["ImageId"] = "rbxassetid://17690526578",
+        ["DisplayName"] = "Cursed Dungeon"
+    }
+}
