c5e86e22f370812412d911b0d1bb262850eeb8ad
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-15 21:17:02 +1000

    Create ClanRankData.lua

---
+++ GameData/Anime Defenders [17017769292]/ClanRankData.lua
@@ -0,0 +1,7 @@
+data = {
+    {["Color"] = Color3.fromRGB(255, 16, 20), ["Description"] = "Owner of the Guild", ["DisplayName"] = "Owner"},
+    {["Color"] = Color3.fromRGB(200, 65, 255), ["Description"] = "Banker of the Guild", ["DisplayName"] = "Banker"},
+    {["Color"] = Color3.fromRGB(0, 110, 255), ["Description"] = "Moderates the Guild", ["DisplayName"] = "Officer"},
+    {["Color"] = Color3.fromRGB(66, 255, 96), ["Description"] = "General Member", ["DisplayName"] = "Member"},
+    {["Color"] = Color3.fromRGB(175, 175, 175), ["Description"] = "Lowest rank", ["DisplayName"] = "Novice"}
+}
