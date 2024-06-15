f4c6b0d71db9667cb1deb4f0f62ba9085db3ddf3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-15 21:18:28 +1000

    Create PotionData.lua

---
+++ GameData/Anime Defenders [17017769292]/PotionData.lua
@@ -0,0 +1,26 @@
+data = {
+    ["Super Lucky"] = {
+        ["LayoutOrder"] = 2,
+        ["Description"] = "function: 0x4eb6f90d65808c85",
+        ["Resumable"] = true,
+        ["Color"] = {Color3.fromRGB(249, 255, 58), Color3.fromRGB(255, 188, 0)},
+        ["Multi"] = 3,
+        ["ShopId"] = 1829425662,
+        ["Currency"] = "Robux",
+        ["Amount"] = 1,
+        ["Price"] = 149,
+        ["Duration"] = 3600
+    },
+    ["Lucky"] = {
+        ["LayoutOrder"] = 1,
+        ["Description"] = "function: 0x6a67ff8fb2f9d1c5",
+        ["Resumable"] = true,
+        ["Color"] = {Color3.fromRGB(155, 255, 94), Color3.fromRGB(25, 255, 0)},
+        ["Multi"] = 2,
+        ["ShopId"] = 1829425661,
+        ["Currency"] = "Robux",
+        ["Amount"] = 1,
+        ["Price"] = 99,
+        ["Duration"] = 3600
+    }
+}
