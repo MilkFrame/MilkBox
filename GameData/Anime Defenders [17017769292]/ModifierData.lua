2bd4faa7bf65a91fdecb7cbc74744b6f176737b6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-15 21:15:54 +1000

    Create ModifierData.lua

---
+++ GameData/Anime Defenders [17017769292]/ModifierData.lua
@@ -0,0 +1,8 @@
+data = {
+    ["ShortRange"] = {["Description"] = "All units lowered range", ["DisplayName"] = "Short Range"},
+    ["TankEnemies"] = {["Description"] = "All enemies 2x health", ["DisplayName"] = "Tank Enemies"},
+    ["HighCost"] = {["Description"] = "All units increased cost", ["DisplayName"] = "High Cost"},
+    ["FastEnemies"] = {["Description"] = "All enemies 2x speed", ["DisplayName"] = "Fast Enemies"},
+    ["RegenEnemies"] = {["Description"] = "All enemies regen health", ["DisplayName"] = "Regen Enemies"},
+    ["ShieldEnemies"] = {["Description"] = "All enemies +1 shield", ["DisplayName"] = "Shield Enemies"}
+}
