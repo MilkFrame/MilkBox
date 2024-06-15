a402a5709c3a84ad979e56b52b491096c3ee6fd6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-15 21:16:50 +1000

    Create Settings.lua

---
+++ GameData/Anime Defenders [17017769292]/Settings.lua
@@ -0,0 +1,98 @@
+data = {
+    ["Auto Sell Deletes Shinies"] = {
+        ["Name"] = "Auto Sell Shinies",
+        ["Position"] = 71,
+        ["OffFunction"] = "function: 0x90fd4fdc04007a55",
+        ["OnFunction"] = "function: 0x67a06af13cd9e445",
+        ["Prefab"] = "SettingsButton",
+        ["Get"] = "function: 0xcb096b59b4cbe485"
+    },
+    ["Unit Visibility"] = {
+        ["Name"] = "Unit Visibility",
+        ["Position"] = 50,
+        ["States"] = {
+            {["Name"] = "All Units", ["Function"] = "function: 0xf2b67caed217c285"},
+            {["Name"] = "Your Units", ["Function"] = "function: 0xa7cbbb41fa998415"}
+        },
+        ["Prefab"] = "SettingsButton",
+        ["Get"] = "function: 0xd80149dac0529345"
+    },
+    ["Low Quality"] = {
+        ["Name"] = "Low Quality",
+        ["Position"] = 10,
+        ["OffFunction"] = "function: 0x6436c9453dfca245",
+        ["OnFunction"] = "function: 0xf02164a5c7f43c65",
+        ["Prefab"] = "SettingsButton",
+        ["Get"] = "function: 0xae3e6356594229c5"
+    },
+    ["Mute Music"] = {
+        ["Name"] = "Mute Music",
+        ["Position"] = 11,
+        ["OffFunction"] = "function: 0x3206d1ffc3364545",
+        ["OnFunction"] = "function: 0x5f43a8ce18579e75",
+        ["Prefab"] = "SettingsButton",
+        ["Get"] = "function: 0x7c1d08c9922dc645"
+    },
+    ["Vibrations"] = {
+        ["Name"] = "Vibrations",
+        ["Position"] = 13,
+        ["OffFunction"] = "function: 0x42c0e7c3fcadc5c5",
+        ["OnFunction"] = "function: 0x4d49ed40d1657305",
+        ["Prefab"] = "SettingsButton",
+        ["Get"] = "function: 0xab0ef66c2142cee5"
+    },
+    ["Camera Shake"] = {
+        ["Name"] = "Camera Shake",
+        ["Position"] = 12,
+        ["OffFunction"] = "function: 0x1970b43f8bb40b35",
+        ["OnFunction"] = "function: 0x3c3df4af43baf0a5",
+        ["Prefab"] = "SettingsButton",
+        ["Get"] = "function: 0x478510b9a69fbb25"
+    },
+    ["TradesEnabled"] = {
+        ["Name"] = "Receive Trade Requests",
+        ["Position"] = 60,
+        ["States"] = {
+            {["Function"] = "function: 0x819dfc75d683d6f5", ["Name"] = "On", ["Color"] = Color3.fromRGB(54, 255, 0)},
+            {["Name"] = "Friends", ["Function"] = "function: 0x5c5d3d78fe29b1c5"},
+            {["Function"] = "function: 0xff188667c7363ad5", ["Name"] = "Off", ["Color"] = Color3.fromRGB(255, 0, 4)}
+        },
+        ["Prefab"] = "SettingsButton",
+        ["Get"] = "function: 0xa7ef9dd665dab705"
+    },
+    ["Confirm Trait Rerolls"] = {
+        ["Name"] = "Confirm Trait Rerolls",
+        ["Position"] = 70,
+        ["OffFunction"] = "function: 0x2a6611f2954f6f75",
+        ["OnFunction"] = "function: 0x313498cbedfd1765",
+        ["Prefab"] = "SettingsButton",
+        ["Get"] = "function: 0x5e96e7f4b1060265"
+    },
+    ["Auto Skip Wave"] = {
+        ["Name"] = "Auto Skip Wave",
+        ["Position"] = 31,
+        ["OffFunction"] = "function: 0x4df163db21695095",
+        ["OnFunction"] = "function: 0xbdd7fbd75a17bd85",
+        ["Prefab"] = "SettingsButton",
+        ["Get"] = "function: 0x8949488418c38a05"
+    },
+    ["Skills Enabled"] = {
+        ["Name"] = "Skill VFX Enabled",
+        ["Position"] = 33,
+        ["OffFunction"] = "function: 0xb684d121702a4cb5",
+        ["OnFunction"] = "function: 0x23c62a468fb40325",
+        ["Prefab"] = "SettingsButton",
+        ["Get"] = "function: 0x2297deb0058ef6a5"
+    },
+    ["Unit Info Position"] = {
+        ["Name"] = "Unit Info Position",
+        ["Position"] = 32,
+        ["States"] = {
+            {["Name"] = "World", ["Function"] = "function: 0xacaf16c93519f035"},
+            {["Name"] = "Left", ["Function"] = "function: 0x2f638dba5d8f6985"},
+            {["Name"] = "Right", ["Function"] = "function: 0x6a266b1365fce715"}
+        },
+        ["Prefab"] = "SettingsButton",
+        ["Get"] = "function: 0x14a6d274c81167e5"
+    }
+}
