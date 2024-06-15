b23ad97238cffe5d38b6124d684f23c377924bac
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-15 21:11:22 +1000

    Create EmoteData.lua

---
+++ GameData/Anime Defenders [17017769292]/EmoteData.lua
@@ -0,0 +1,115 @@
+data = {
+    ["Tidy"] = {
+        ["LockMovement"] = true,
+        ["Rarity"] = 1,
+        ["NonTradeable"] = true,
+        ["AnimationId"] = 17391519638,
+        ["DisplayName"] = "Tidy"
+    },
+    ["L Dance"] = {
+        ["LockMovement"] = true,
+        ["Rarity"] = 1,
+        ["NonTradeable"] = true,
+        ["AnimationId"] = 17391510278,
+        ["DisplayName"] = "L Dance"
+    },
+    ["Lethal"] = {
+        ["LockMovement"] = true,
+        ["Rarity"] = 1,
+        ["NonTradeable"] = true,
+        ["AnimationId"] = 17391511917,
+        ["DisplayName"] = "Lethal"
+    },
+    ["Vibin"] = {
+        ["LockMovement"] = true,
+        ["Rarity"] = 1,
+        ["NonTradeable"] = true,
+        ["AnimationId"] = 17391521934,
+        ["DisplayName"] = "Vibin"
+    },
+    ["Infinite Gaze"] = {
+        ["LockMovement"] = true,
+        ["Rarity"] = 1,
+        ["StartupId"] = 17513590101,
+        ["NonTradeable"] = true,
+        ["AnimationId"] = 17391496732,
+        ["DisplayName"] = "Infinite Gaze"
+    },
+    ["Floss"] = {
+        ["LockMovement"] = true,
+        ["Rarity"] = 1,
+        ["NonTradeable"] = true,
+        ["AnimationId"] = 17030681462,
+        ["DisplayName"] = "Floss"
+    },
+    ["Japanese Dance"] = {
+        ["LockMovement"] = true,
+        ["Rarity"] = 1,
+        ["NonTradeable"] = true,
+        ["AnimationId"] = 17391506690,
+        ["DisplayName"] = "Glitched Dance"
+    },
+    ["Electroshuffle"] = {
+        ["LockMovement"] = true,
+        ["Rarity"] = 1,
+        ["NonTradeable"] = true,
+        ["AnimationId"] = 17391524225,
+        ["DisplayName"] = "Electroshuffle"
+    },
+    ["Get gone"] = {
+        ["LockMovement"] = true,
+        ["Rarity"] = 1,
+        ["NonTradeable"] = true,
+        ["AnimationId"] = 17391494128,
+        ["DisplayName"] = "Get gone"
+    },
+    ["Chika Dance"] = {
+        ["LockMovement"] = true,
+        ["Rarity"] = 1,
+        ["NonTradeable"] = true,
+        ["AnimationId"] = 17391489437,
+        ["DisplayName"] = "Cute Dance"
+    },
+    ["Justice"] = {
+        ["LockMovement"] = true,
+        ["Rarity"] = 1,
+        ["NonTradeable"] = true,
+        ["AnimationId"] = 17513595926,
+        ["DisplayName"] = "Justice"
+    },
+    ["Lucked Out"] = {
+        ["LockMovement"] = true,
+        ["Rarity"] = 1,
+        ["NonTradeable"] = true,
+        ["AnimationId"] = 17391504289,
+        ["DisplayName"] = "Lucked Out"
+    },
+    ["Kick"] = {
+        ["LockMovement"] = true,
+        ["Rarity"] = 1,
+        ["NonTradeable"] = true,
+        ["AnimationId"] = 17391508651,
+        ["DisplayName"] = "Kick"
+    },
+    ["Boogie Down"] = {
+        ["LockMovement"] = true,
+        ["Rarity"] = 1,
+        ["NonTradeable"] = true,
+        ["AnimationId"] = 17391485235,
+        ["DisplayName"] = "Boogie Down"
+    },
+    ["Griddy"] = {
+        ["LockMovement"] = true,
+        ["Rarity"] = 1,
+        ["NonTradeable"] = true,
+        ["AnimationId"] = 17391502185,
+        ["DisplayName"] = "Griddy"
+    },
+    ["Rizz"] = {
+        ["LockMovement"] = true,
+        ["Rarity"] = 1,
+        ["NonTradeable"] = true,
+        ["AnimationId"] = 17391516727,
+        ["DisplayName"] = "Rizz"
+    }
+}
