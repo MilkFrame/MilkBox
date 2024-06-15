8463c3e30d4bac85909ecd798313915f895726ce
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-15 21:17:42 +1000

    Create TraitData.lua

---
+++ GameData/Anime Defenders [17017769292]/TraitData.lua
@@ -0,0 +1,154 @@
+data = {
+    ["Almighty 1"] = {
+        ["LayoutOrder"] = 19,
+        ["Type"] = "Almighty",
+        ["Description"] = "+280% damage, 15% cooldown, +10% range. 1 placement cap",
+        ["Chance"] = 0.1,
+        ["Level"] = 1,
+        ["Modifiers"] = {["Range"] = 10, ["Cooldown"] = -15, ["Cap"] = 1, ["Damage"] = 280}
+    },
+    ["Brawler 2"] = {
+        ["LayoutOrder"] = 2,
+        ["Type"] = "Brawler",
+        ["Description"] = "Increases damage by 12.5%",
+        ["Chance"] = 7.8999999999999995,
+        ["Level"] = 2,
+        ["Modifiers"] = {["Damage"] = 12.5}
+    },
+    ["Critical 2"] = {
+        ["LayoutOrder"] = 11,
+        ["Type"] = "Critical",
+        ["Description"] = "Increases crit chance by 25%",
+        ["Chance"] = 6.1,
+        ["Level"] = 2,
+        ["Modifiers"] = {["CritChance"] = 25}
+    },
+    ["Critical 1"] = {
+        ["LayoutOrder"] = 10,
+        ["Type"] = "Critical",
+        ["Description"] = "Increases crit chance by 20%",
+        ["Chance"] = 6.15,
+        ["Level"] = 1,
+        ["Modifiers"] = {["CritChance"] = 20}
+    },
+    ["Critical 3"] = {
+        ["LayoutOrder"] = 12,
+        ["Type"] = "Critical",
+        ["Description"] = "Increases crit chance by 30%",
+        ["Chance"] = 6,
+        ["Level"] = 3,
+        ["Modifiers"] = {["CritChance"] = 30}
+    },
+    ["Requiem 1"] = {
+        ["LayoutOrder"] = 18,
+        ["Type"] = "Requiem",
+        ["Description"] = "+20% damage, 10% cooldown, +20% range",
+        ["Chance"] = 0.2,
+        ["Level"] = 1,
+        ["Modifiers"] = {["Range"] = 20, ["Cooldown"] = -10, ["Damage"] = 20}
+    },
+    ["Bullseye 1"] = {
+        ["LayoutOrder"] = 14,
+        ["Type"] = "Bullseye",
+        ["Description"] = "Increases range by 25%",
+        ["Chance"] = 2.5,
+        ["Level"] = 1,
+        ["Modifiers"] = {["Range"] = 25}
+    },
+    ["Sonic 1"] = {
+        ["LayoutOrder"] = 16,
+        ["Type"] = "Sonic",
+        ["Description"] = "Decreases cooldown by -20%",
+        ["Chance"] = 1,
+        ["Level"] = 1,
+        ["Modifiers"] = {["Cooldown"] = -20}
+    },
+    ["Swiftness 2"] = {
+        ["LayoutOrder"] = 5,
+        ["Type"] = "Swiftness",
+        ["Description"] = "Reduces cooldown by -9%",
+        ["Chance"] = 7,
+        ["Level"] = 2,
+        ["Modifiers"] = {["Cooldown"] = -9}
+    },
+    ["Brawler 1"] = {
+        ["LayoutOrder"] = 1,
+        ["Type"] = "Brawler",
+        ["Description"] = "Increases damage by 10%",
+        ["Chance"] = 7.95,
+        ["Level"] = 1,
+        ["Modifiers"] = {["Damage"] = 10}
+    },
+    ["Hunter 2"] = {
+        ["LayoutOrder"] = 8,
+        ["Type"] = "Hunter",
+        ["Description"] = "Increases range by 12.5%",
+        ["Chance"] = 7,
+        ["Level"] = 2,
+        ["Modifiers"] = {["Range"] = 12.5}
+    },
+    ["Prodigy 1"] = {
+        ["LayoutOrder"] = 13,
+        ["Type"] = "Prodigy",
+        ["Description"] = "Increases XP gain by 50%",
+        ["Chance"] = 10,
+        ["Level"] = 1,
+        ["Modifiers"] = {["XpBoost"] = 50}
+    },
+    ["Hunter 1"] = {
+        ["LayoutOrder"] = 7,
+        ["Type"] = "Hunter",
+        ["Description"] = "Increases range by 10%",
+        ["Chance"] = 7,
+        ["Level"] = 1,
+        ["Modifiers"] = {["Range"] = 10}
+    },
+    ["Swiftness 3"] = {
+        ["LayoutOrder"] = 6,
+        ["Type"] = "Swiftness",
+        ["Description"] = "Reduces cooldown by -12.5%",
+        ["Chance"] = 7,
+        ["Level"] = 3,
+        ["Modifiers"] = {["Cooldown"] = -12.5}
+    },
+    ["Swiftness 1"] = {
+        ["LayoutOrder"] = 4,
+        ["Type"] = "Swiftness",
+        ["Description"] = "Reduces cooldown by -6%",
+        ["Chance"] = 7,
+        ["Level"] = 1,
+        ["Modifiers"] = {["Cooldown"] = -6}
+    },
+    ["Precision 1"] = {
+        ["LayoutOrder"] = 17,
+        ["Type"] = "Precision",
+        ["Description"] = "+30% crit chance, +30% crit damage",
+        ["Chance"] = 0.8,
+        ["Level"] = 1,
+        ["Modifiers"] = {["CritMult"] = 30, ["CritChance"] = 30}
+    },
+    ["Midas Touch 1"] = {
+        ["LayoutOrder"] = 15,
+        ["Type"] = "Midas Touch",
+        ["Description"] = "Increases farm money earned by 15%",
+        ["Chance"] = 1.5,
+        ["Level"] = 1,
+        ["Modifiers"] = {["Farm"] = 15}
+    },
+    ["Brawler 3"] = {
+        ["LayoutOrder"] = 3,
+        ["Type"] = "Brawler",
+        ["Description"] = "Increases damage by 15%",
+        ["Chance"] = 7.8,
+        ["Level"] = 3,
+        ["Modifiers"] = {["Damage"] = 15}
+    },
+    ["Hunter 3"] = {
+        ["LayoutOrder"] = 9,
+        ["Type"] = "Hunter",
+        ["Description"] = "Increases range by 15%",
+        ["Chance"] = 7,
+        ["Level"] = 3,
+        ["Modifiers"] = {["Range"] = 15}
+    }
+}
