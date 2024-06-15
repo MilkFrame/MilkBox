ada20870b0fc2cbbf3d8b9839388aaf903eaeb13
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-15 21:18:08 +1000

    Create TitleData.lua

---
+++ GameData/Anime Defenders [17017769292]/TitleData.lua
@@ -0,0 +1,83 @@
+data = {
+    ["Planner"] = {
+        ["Index"] = 95,
+        ["Category"] = "Staff",
+        ["Gradient"] = {Color3.fromRGB(249, 255, 140), Color3.fromRGB(255, 184, 105)},
+        ["Condition"] = "function: 0x47eee523ec60c305",
+        ["Description"] = "For Anime Defenders planners!"
+    },
+    ["CC"] = {
+        ["Index"] = 94,
+        ["Category"] = "Staff",
+        ["Gradient"] = {Color3.fromRGB(253, 119, 0), Color3.fromRGB(249, 253, 0)},
+        ["Condition"] = "function: 0xc4b765e35a237be5",
+        ["Description"] = "For Content Creators of Anime Defenders!"
+    },
+    ["Owner"] = {
+        ["Index"] = 100,
+        ["Category"] = "Staff",
+        ["ChatColor"] = Color3.fromRGB(255, 144, 144),
+        ["Gradient"] = {Color3.fromRGB(255, 29, 0), Color3.fromRGB(255, 0, 0)},
+        ["Condition"] = "function: 0x17bb01b6b8f58725",
+        ["Description"] = "Owner of Anime Defenders"
+    },
+    ["Tester"] = {
+        ["Index"] = 93,
+        ["Category"] = "Staff",
+        ["Gradient"] = {Color3.fromRGB(144, 235, 255), Color3.fromRGB(161, 246, 255)},
+        ["Condition"] = "function: 0x5c1fe962a0e79245",
+        ["Description"] = "For Anime Defenders testers!"
+    },
+    ["Early Access"] = {
+        ["Index"] = 92,
+        ["Category"] = "Staff",
+        ["Gradient"] = {Color3.fromRGB(144, 235, 255), Color3.fromRGB(161, 246, 255)},
+        ["Condition"] = "function: 0xac776eadba5bdc25",
+        ["Description"] = "For Anime Defenders early access users!"
+    },
+    ["VIP"] = {
+        ["Index"] = 91,
+        ["ChatColor"] = Color3.fromRGB(255, 249, 181),
+        ["Gradient"] = {Color3.fromRGB(255, 225, 0), Color3.fromRGB(255, 179, 0)},
+        ["Condition"] = "function: 0x3b08f42dad906d85",
+        ["Description"] = "Buy the VIP Gamepass to access!"
+    },
+    ["Admin"] = {
+        ["Index"] = 97,
+        ["Category"] = "Staff",
+        ["ChatColor"] = Color3.fromRGB(255, 184, 253),
+        ["Gradient"] = {Color3.fromRGB(179, 0, 255), Color3.fromRGB(255, 0, 251)},
+        ["Condition"] = "function: 0xae3ddfe8cbe915c5",
+        ["Description"] = "For Admins!"
+    },
+    ["Chai"] = {
+        ["Index"] = 99,
+        ["Category"] = "Staff",
+        ["ChatColor"] = Color3.fromRGB(255, 144, 144),
+        ["Gradient"] = {Color3.fromRGB(0, 0, 0), Color3.fromRGB(255, 0, 0)},
+        ["Condition"] = "function: 0xa36ac6158351e885",
+        ["Description"] = "Unique to the Head Manager of Anime Defenders."
+    },
+    ["Dev"] = {
+        ["Index"] = 98,
+        ["Category"] = "Staff",
+        ["ChatColor"] = Color3.fromRGB(165, 242, 255),
+        ["Gradient"] = {Color3.fromRGB(0, 26, 255), Color3.fromRGB(0, 114, 255)},
+        ["Condition"] = "function: 0x39619c69a31d3665",
+        ["Description"] = "For developers of Anime Defenders!"
+    },
+    ["Staff"] = {
+        ["Index"] = 96,
+        ["Category"] = "Staff",
+        ["ChatColor"] = Color3.fromRGB(156, 246, 255),
+        ["Gradient"] = {Color3.fromRGB(0, 221, 255), Color3.fromRGB(0, 174, 255)},
+        ["Condition"] = "function: 0xdae618ab73a51aa5",
+        ["Description"] = "For Staff!"
+    },
+    ["None"] = {
+        ["Condition"] = "function: 0xa7844668d1f40165",
+        ["Gradient"] = {Color3.fromRGB(255, 255, 255), Color3.fromRGB(255, 255, 255)},
+        ["Index"] = -999,
+        ["Description"] = "No Title"
+    }
+}
