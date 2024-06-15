ec4111fb7f8e214adfb2e59c72b8b834168208a8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-15 21:18:39 +1000

    Create RaidData.lua

---
+++ GameData/Anime Defenders [17017769292]/RaidData.lua
@@ -0,0 +1,7 @@
+data = {
+    ["Raid1"] = {
+        ["Chapter"] = "CursedDungeon_Chapter1",
+        ["DisplayName"] = "Cursed Dungeon",
+        ["Cost"] = {["Meta"] = {"Gold", 3000}, ["RewardType"] = "Currency"}
+    }
+}
