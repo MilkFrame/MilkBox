efe18b04f6fc98ad4619348ca1329b66bdfb5cbe
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-15 21:15:05 +1000

    Create RankData.lua

---
+++ GameData/Anime Defenders [17017769292]/RankData.lua
@@ -0,0 +1,100 @@
+data = {
+    ["S"] = {
+        ["MaxChance"] = 6,
+        ["HexColor"] = "#FFFFFF",
+        ["Chance"] = 0.5,
+        ["Priority"] = 1,
+        ["Modifiers"] = {["Range"] = 10, ["Cooldown"] = -10, ["Damage"] = 20}
+    },
+    ["A+"] = {
+        ["MaxChance"] = 8,
+        ["HexColor"] = "#FFFFFF",
+        ["Chance"] = 1,
+        ["Priority"] = 2,
+        ["Modifiers"] = {["Range"] = 9.75, ["Cooldown"] = -9.75, ["Damage"] = 19.6}
+    },
+    ["A"] = {
+        ["MaxChance"] = 10,
+        ["HexColor"] = "#FFFFFF",
+        ["Chance"] = 1.5,
+        ["Priority"] = 3,
+        ["Modifiers"] = {["Range"] = 9.4, ["Cooldown"] = -9.4, ["Damage"] = 18.8}
+    },
+    ["A-"] = {
+        ["MaxChance"] = 12,
+        ["HexColor"] = "#FFFFFF",
+        ["Chance"] = 2,
+        ["Priority"] = 4,
+        ["Modifiers"] = {["Range"] = 9, ["Cooldown"] = -9, ["Damage"] = 18.1}
+    },
+    ["B+"] = {
+        ["MaxChance"] = 13,
+        ["HexColor"] = "#FFFFFF",
+        ["Chance"] = 4,
+        ["Priority"] = 5,
+        ["Modifiers"] = {["Range"] = 8.6, ["Cooldown"] = -8.6, ["Damage"] = 17.4}
+    },
+    ["B"] = {
+        ["MaxChance"] = 15,
+        ["HexColor"] = "#FFFFFF",
+        ["Chance"] = 6,
+        ["Priority"] = 6,
+        ["Modifiers"] = {["Range"] = 7.9, ["Cooldown"] = -7.9, ["Damage"] = 15.9}
+    },
+    ["B-"] = {
+        ["MaxChance"] = 17,
+        ["HexColor"] = "#FFFFFF",
+        ["Chance"] = 7.5,
+        ["Priority"] = 7,
+        ["Modifiers"] = {["Range"] = 6.8, ["Cooldown"] = -6.8, ["Damage"] = 13.6}
+    },
+    ["C+"] = {
+        ["MaxChance"] = 19,
+        ["HexColor"] = "#FFFFFF",
+        ["Chance"] = 9,
+        ["Priority"] = 8,
+        ["Modifiers"] = {["Range"] = 5.6, ["Cooldown"] = -5.6, ["Damage"] = 11.2}
+    },
+    ["C"] = {
+        ["MaxChance"] = 0,
+        ["HexColor"] = "#FFFFFF",
+        ["Chance"] = 10,
+        ["Priority"] = 9,
+        ["Modifiers"] = {["Range"] = 4, ["Cooldown"] = -4, ["Damage"] = 8.2}
+    },
+    ["C-"] = {
+        ["MaxChance"] = 0,
+        ["HexColor"] = "#FFFFFF",
+        ["Chance"] = 11,
+        ["Priority"] = 10,
+        ["Modifiers"] = {["Range"] = 2.5, ["Cooldown"] = -2.5, ["Damage"] = 4.7}
+    },
+    ["D+"] = {
+        ["MaxChance"] = 0,
+        ["HexColor"] = "#FFFFFF",
+        ["Chance"] = 16,
+        ["Priority"] = 11,
+        ["Modifiers"] = {["Range"] = 0.8, ["Cooldown"] = -0.8, ["Damage"] = 1.5}
+    },
+    ["D"] = {
+        ["MaxChance"] = 0,
+        ["HexColor"] = "#FFFFFF",
+        ["Chance"] = 12,
+        ["Priority"] = 12,
+        ["Modifiers"] = {["Range"] = -1, ["Cooldown"] = 1, ["Damage"] = -1.5}
+    },
+    ["E"] = {
+        ["MaxChance"] = 0,
+        ["HexColor"] = "#FFFFFF",
+        ["Chance"] = 10.5,
+        ["Priority"] = 13,
+        ["Modifiers"] = {["Range"] = -3.4, ["Cooldown"] = 3.4, ["Damage"] = -4.8}
+    },
+    ["F"] = {
+        ["MaxChance"] = 0,
+        ["HexColor"] = "#FFFFFF",
+        ["Chance"] = 9,
+        ["Priority"] = 14,
+        ["Modifiers"] = {["Range"] = -7.4, ["Cooldown"] = 7.4, ["Damage"] = -8.3}
+    }
+}
