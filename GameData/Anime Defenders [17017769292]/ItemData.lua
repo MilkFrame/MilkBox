905855935f6eec61ea222e16f67721b83b43ac40
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-15 21:12:31 +1000

    Create ItemData.lua

---
+++ GameData/Anime Defenders [17017769292]/ItemData.lua
@@ -0,0 +1,418 @@
+data = {
+    ["Fire Dragon Soul"] = {
+        ["StackSize"] = 100,
+        ["Description"] = "Be careful, it's hot... Used to evolve Flame Dragon King.",
+        ["CraftingRecipe"] = {
+            ["Star Rift (Blue)"] = 6,
+            ["Star Rift (Yellow)"] = 14,
+            ["Star Rift (Green)"] = 6,
+            ["Star Rift (Rainbow)"] = 1,
+            ["Star Rift (Purple)"] = 5
+        },
+        ["CraftPrice"] = 28000,
+        ["CraftCurrency"] = "Gold",
+        ["Consumable"] = true,
+        ["Category"] = "Unique",
+        ["Rarity"] = 5,
+        ["Craftable"] = true,
+        ["NonTradeable"] = true
+    },
+    ["Star Rift (Purple)"] = {
+        ["StackSize"] = 100,
+        ["Consumable"] = true,
+        ["Description"] = "Purple variant of star rift. Used as crafting material.",
+        ["Category"] = "Star",
+        ["Craftable"] = false,
+        ["Rarity"] = 4,
+        ["MaxOwned"] = 100,
+        ["NonTradeable"] = false
+    },
+    ["Alien Fruit"] = {
+        ["StackSize"] = 1000,
+        ["Description"] = "(+200xp) Obtained from Blue Planet.",
+        ["Craftable"] = false,
+        ["UsageData"] = {200},
+        ["Consumable"] = true,
+        ["Category"] = "Food",
+        ["Rarity"] = 1,
+        ["UsageType"] = "XPFood",
+        ["NonTradeable"] = true
+    },
+    ["Risky Dice"] = {
+        ["StackSize"] = 100,
+        ["Consumable"] = true,
+        ["Description"] = "Rerolls the rank of a unit",
+        ["Category"] = "Misc",
+        ["Craftable"] = false,
+        ["Rarity"] = 5,
+        ["NonTradeable"] = false
+    },
+    ["Star Rift (Yellow)"] = {
+        ["StackSize"] = 100,
+        ["Consumable"] = true,
+        ["Description"] = "Yellow variant of star rift. Used as crafting material.",
+        ["Category"] = "Star",
+        ["Craftable"] = false,
+        ["Rarity"] = 3,
+        ["MaxOwned"] = 100,
+        ["NonTradeable"] = false
+    },
+    ["Cursed Soul"] = {
+        ["StackSize"] = 100,
+        ["Description"] = "Truly Cursed. Used to evolve The Cursed Knight.",
+        ["CraftingRecipe"] = {
+            ["Star Rift (Blue)"] = 14,
+            ["Star Rift (Red)"] = 14,
+            ["Star Rift (Yellow)"] = 35,
+            ["Star Rift (Green)"] = 13,
+            ["Star Rift (Rainbow)"] = 3
+        },
+        ["CraftPrice"] = 38000,
+        ["CraftCurrency"] = "Gold",
+        ["Consumable"] = true,
+        ["Category"] = "Unique",
+        ["Rarity"] = 5,
+        ["Craftable"] = true,
+        ["NonTradeable"] = true
+    },
+    ["Underwater Chest"] = {
+        ["StackSize"] = 1000,
+        ["Description"] = "(+250xp) Obtained from Underwater Temple.",
+        ["Craftable"] = false,
+        ["UsageData"] = {250},
+        ["Consumable"] = true,
+        ["Category"] = "Food",
+        ["Rarity"] = 1,
+        ["UsageType"] = "XPFood",
+        ["NonTradeable"] = true
+    },
+    ["Trait Crystal"] = {
+        ["StackSize"] = 1000,
+        ["Consumable"] = true,
+        ["Description"] = "Rerolls a trait.",
+        ["Category"] = "Misc",
+        ["Craftable"] = false,
+        ["Rarity"] = 3,
+        ["NonTradeable"] = false
+    },
+    ["Cursed Finger"] = {
+        ["StackSize"] = 100,
+        ["Description"] = "Ugh, a finger? Used to evolve Curse Prince.",
+        ["CraftingRecipe"] = {
+            ["Star Rift (Purple)"] = 5,
+            ["Star Rift (Red)"] = 6,
+            ["Star Rift (Yellow)"] = 14,
+            ["Star Rift (Green)"] = 6,
+            ["Star Rift (Rainbow)"] = 1
+        },
+        ["CraftPrice"] = 14000,
+        ["CraftCurrency"] = "Gold",
+        ["Consumable"] = true,
+        ["Category"] = "Unique",
+        ["Rarity"] = 5,
+        ["Craftable"] = true,
+        ["NonTradeable"] = true
+    },
+    ["Sky Splitting Bow"] = {
+        ["StackSize"] = 100,
+        ["Description"] = "The legendary bow. Used to evolve Sharpshooter.",
+        ["CraftingRecipe"] = {
+            ["Star Rift (Blue)"] = 7,
+            ["Star Rift (Red)"] = 5,
+            ["Star Rift (Yellow)"] = 15,
+            ["Star Rift (Green)"] = 6,
+            ["Star Rift (Rainbow)"] = 1
+        },
+        ["CraftPrice"] = 13000,
+        ["CraftCurrency"] = "Gold",
+        ["Consumable"] = true,
+        ["Category"] = "Unique",
+        ["Rarity"] = 5,
+        ["Craftable"] = true,
+        ["NonTradeable"] = true
+    },
+    ["Frost Bind"] = {
+        ["StackSize"] = 100,
+        ["Consumable"] = true,
+        ["Description"] = "Freezes a rank of a unit during reroll",
+        ["Category"] = "Misc",
+        ["Craftable"] = false,
+        ["Rarity"] = 3,
+        ["NonTradeable"] = false
+    },
+    ["Blue Soul"] = {
+        ["StackSize"] = 100,
+        ["Description"] = "The soul of the Alpha Ant... Used to evolve Ant King.",
+        ["CraftingRecipe"] = {
+            ["Star Rift (Blue)"] = 13,
+            ["Star Rift (Red)"] = 10,
+            ["Star Rift (Yellow)"] = 32,
+            ["Star Rift (Green)"] = 12,
+            ["Star Rift (Rainbow)"] = 1
+        },
+        ["CraftPrice"] = 26500,
+        ["CraftCurrency"] = "Gold",
+        ["Consumable"] = true,
+        ["Category"] = "Unique",
+        ["Rarity"] = 5,
+        ["Craftable"] = true,
+        ["NonTradeable"] = true
+    },
+    ["Meat"] = {
+        ["StackSize"] = 1000,
+        ["Description"] = "(+50xp) Obtained from Windmill Village",
+        ["Craftable"] = false,
+        ["UsageData"] = {50},
+        ["Consumable"] = true,
+        ["Category"] = "Food",
+        ["Rarity"] = 1,
+        ["UsageType"] = "XPFood",
+        ["NonTradeable"] = true
+    },
+    ["Energy Crystal"] = {
+        ["StackSize"] = 1000,
+        ["Description"] = "(+100xp) Obtained from Mob City",
+        ["Craftable"] = false,
+        ["UsageData"] = {100},
+        ["Consumable"] = true,
+        ["Category"] = "Food",
+        ["Rarity"] = 1,
+        ["UsageType"] = "XPFood",
+        ["NonTradeable"] = true
+    },
+    ["Star Rift (Green)"] = {
+        ["StackSize"] = 100,
+        ["Consumable"] = true,
+        ["Description"] = "Green variant of star rift. Used as crafting material.",
+        ["Category"] = "Star",
+        ["Craftable"] = false,
+        ["Rarity"] = 4,
+        ["MaxOwned"] = 100,
+        ["NonTradeable"] = false
+    },
+    ["Celestial Sword"] = {
+        ["StackSize"] = 100,
+        ["Description"] = "A legendary blade with ancient enchantments. Used to evolve the Warrior Queen.",
+        ["CraftingRecipe"] = {
+            ["Star Rift (Blue)"] = 8,
+            ["Star Rift (Red)"] = 6,
+            ["Star Rift (Yellow)"] = 30,
+            ["Star Rift (Rainbow)"] = 2,
+            ["Star Rift (Purple)"] = 7
+        },
+        ["CraftPrice"] = 30000,
+        ["CraftCurrency"] = "Gold",
+        ["Consumable"] = true,
+        ["Category"] = "Unique",
+        ["Rarity"] = 5,
+        ["Craftable"] = true,
+        ["NonTradeable"] = true
+    },
+    ["Star Rift (Blue)"] = {
+        ["StackSize"] = 100,
+        ["Consumable"] = true,
+        ["Description"] = "Blue variant of star rift. Used as crafting material.",
+        ["Category"] = "Star",
+        ["Craftable"] = false,
+        ["Rarity"] = 4,
+        ["MaxOwned"] = 100,
+        ["NonTradeable"] = false
+    },
+    ["Star Rift (Red)"] = {
+        ["StackSize"] = 100,
+        ["Consumable"] = true,
+        ["Description"] = "Red variant of star rift. Used as crafting material.",
+        ["Category"] = "Star",
+        ["Craftable"] = false,
+        ["Rarity"] = 4,
+        ["MaxOwned"] = 100,
+        ["NonTradeable"] = false
+    },
+    ["Shadow Eye"] = {
+        ["StackSize"] = 100,
+        ["Description"] = "The eye of Shadow Hokage... Used to evolve Thunder Shinobi.",
+        ["CraftingRecipe"] = {
+            ["Star Rift (Blue)"] = 6,
+            ["Star Rift (Red)"] = 6,
+            ["Star Rift (Yellow)"] = 15,
+            ["Star Rift (Rainbow)"] = 1,
+            ["Star Rift (Purple)"] = 4
+        },
+        ["CraftPrice"] = 13000,
+        ["CraftCurrency"] = "Gold",
+        ["Consumable"] = true,
+        ["Category"] = "Unique",
+        ["Rarity"] = 5,
+        ["Craftable"] = true,
+        ["NonTradeable"] = true
+    },
+    ["Demon Greatsword"] = {
+        ["StackSize"] = 100,
+        ["Description"] = "A demonic blade. Used to evolve The Gamer.",
+        ["CraftingRecipe"] = {
+            ["Star Rift (Blue)"] = 9,
+            ["Star Rift (Red)"] = 9,
+            ["Star Rift (Yellow)"] = 35,
+            ["Star Rift (Green)"] = 9,
+            ["Star Rift (Rainbow)"] = 3,
+            ["Star Rift (Purple)"] = 9
+        },
+        ["CraftPrice"] = 33000,
+        ["CraftCurrency"] = "Gold",
+        ["Consumable"] = true,
+        ["Category"] = "Unique",
+        ["Rarity"] = 5,
+        ["Craftable"] = true,
+        ["NonTradeable"] = true
+    },
+    ["Roulette Machine"] = {
+        ["StackSize"] = 100,
+        ["Description"] = "Try your luck? Used to evolve Chance Taker.",
+        ["CraftingRecipe"] = {
+            ["Star Rift (Blue)"] = 10,
+            ["Star Rift (Red)"] = 10,
+            ["Star Rift (Yellow)"] = 35,
+            ["Star Rift (Green)"] = 8,
+            ["Star Rift (Rainbow)"] = 3,
+            ["Star Rift (Purple)"] = 8
+        },
+        ["CraftPrice"] = 35000,
+        ["CraftCurrency"] = "Gold",
+        ["Consumable"] = true,
+        ["Category"] = "Unique",
+        ["Rarity"] = 5,
+        ["Craftable"] = true,
+        ["NonTradeable"] = true
+    },
+    ["Otherworldly Gauntlet"] = {
+        ["StackSize"] = 100,
+        ["Consumable"] = true,
+        ["Description"] = "The weapon of destruction. Used to evolve Bear King.",
+        ["Category"] = "Unique",
+        ["Craftable"] = false,
+        ["Rarity"] = 5,
+        ["NonTradeable"] = true
+    },
+    ["Raid Shards (Red)"] = {
+        ["StackSize"] = 1000,
+        ["Consumable"] = true,
+        ["Description"] = "Obtained from the Statue Dungeon Raid. Use it to buy items from raid shop",
+        ["Category"] = "Misc",
+        ["Craftable"] = false,
+        ["Rarity"] = 5,
+        ["NonTradeable"] = true
+    },
+    ["Marine's Hat"] = {
+        ["StackSize"] = 100,
+        ["Description"] = "A marine's honour. Used to evolve Carp.",
+        ["CraftingRecipe"] = {
+            ["Star Rift (Blue)"] = 13,
+            ["Star Rift (Red)"] = 12,
+            ["Star Rift (Yellow)"] = 30,
+            ["Star Rift (Rainbow)"] = 2,
+            ["Star Rift (Purple)"] = 10
+        },
+        ["CraftPrice"] = 25000,
+        ["CraftCurrency"] = "Gold",
+        ["Consumable"] = true,
+        ["Category"] = "Unique",
+        ["Rarity"] = 5,
+        ["Craftable"] = true,
+        ["NonTradeable"] = true
+    },
+    ["Skull Mask"] = {
+        ["StackSize"] = 100,
+        ["Description"] = "💀... Used to evolve Skull Warrior.",
+        ["CraftingRecipe"] = {
+            ["Star Rift (Blue)"] = 6,
+            ["Star Rift (Red)"] = 6,
+            ["Star Rift (Yellow)"] = 28,
+            ["Star Rift (Green)"] = 4,
+            ["Star Rift (Rainbow)"] = 2
+        },
+        ["CraftPrice"] = 25000,
+        ["CraftCurrency"] = "Gold",
+        ["Consumable"] = true,
+        ["Category"] = "Unique",
+        ["Rarity"] = 5,
+        ["Craftable"] = true,
+        ["NonTradeable"] = true
+    },
+    ["Flame Grimoire"] = {
+        ["StackSize"] = 100,
+        ["Description"] = "Teachings left by a master. Used to evolve Elf Wizardess.",
+        ["CraftingRecipe"] = {
+            ["Star Rift (Blue)"] = 3,
+            ["Star Rift (Red)"] = 3,
+            ["Star Rift (Yellow)"] = 10,
+            ["Star Rift (Green)"] = 3,
+            ["Star Rift (Rainbow)"] = 1,
+            ["Star Rift (Purple)"] = 3
+        },
+        ["CraftPrice"] = 8500,
+        ["CraftCurrency"] = "Gold",
+        ["Consumable"] = true,
+        ["Category"] = "Unique",
+        ["Rarity"] = 5,
+        ["Craftable"] = true,
+        ["NonTradeable"] = true
+    },
+    ["Esper's Spirit"] = {
+        ["StackSize"] = 100,
+        ["Description"] = "Is it a spirit? No, it's more. Used to evolve Esper.",
+        ["CraftingRecipe"] = {
+            ["Star Rift (Blue)"] = 7,
+            ["Star Rift (Red)"] = 7,
+            ["Star Rift (Yellow)"] = 25,
+            ["Star Rift (Green)"] = 6,
+            ["Star Rift (Rainbow)"] = 2,
+            ["Star Rift (Purple)"] = 5
+        },
+        ["CraftPrice"] = 28000,
+        ["CraftCurrency"] = "Gold",
+        ["Consumable"] = true,
+        ["Category"] = "Unique",
+        ["Rarity"] = 5,
+        ["Craftable"] = true,
+        ["NonTradeable"] = true
+    },
+    ["Star Rift (Rainbow)"] = {
+        ["StackSize"] = 100,
+        ["Consumable"] = true,
+        ["Description"] = "Rainbow variant of star rift. Used as crafting material.",
+        ["Category"] = "Star",
+        ["Craftable"] = false,
+        ["Rarity"] = 5,
+        ["MaxOwned"] = 100,
+        ["NonTradeable"] = false
+    },
+    ["Cursed Object"] = {
+        ["StackSize"] = 1000,
+        ["Description"] = "(+150xp) Obtained from Cursed Academy",
+        ["Craftable"] = false,
+        ["UsageData"] = {150},
+        ["Consumable"] = true,
+        ["Category"] = "Food",
+        ["Rarity"] = 1,
+        ["UsageType"] = "XPFood",
+        ["NonTradeable"] = true
+    },
+    ["Donut of Prosperity"] = {
+        ["StackSize"] = 100,
+        ["Description"] = "First bite tastes like heaven, second one takes you there. Used to evolve Donut Warrior.",
+        ["CraftingRecipe"] = {
+            ["Star Rift (Blue)"] = 4,
+            ["Star Rift (Red)"] = 4,
+            ["Star Rift (Yellow)"] = 11,
+            ["Star Rift (Rainbow)"] = 1,
+            ["Star Rift (Purple)"] = 4
+        },
+        ["CraftPrice"] = 9000,
+        ["CraftCurrency"] = "Gold",
+        ["Consumable"] = true,
+        ["Category"] = "Unique",
+        ["Rarity"] = 5,
+        ["Craftable"] = true,
+        ["NonTradeable"] = true
+    }
+}
