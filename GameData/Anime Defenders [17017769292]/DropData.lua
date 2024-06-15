0faf04cb9c35115e38bd03959f95fbb14fb6bb77
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-15 21:15:39 +1000

    Create DropData.lua

---
+++ GameData/Anime Defenders [17017769292]/DropData.lua
@@ -0,0 +1,205 @@
+data = {
+    ["DailyQuest"] = {
+        ["IsChallengeDrop"] = false,
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Risky Dice", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Gems", 100}, ["RewardType"] = "Currency"}}},
+            {["Chance"] = 25, ["Rewards"] = {{["Meta"] = {"Gems", 100}, ["RewardType"] = "Currency"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Energy Crystal", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Meat", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Cursed Object", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}}
+        }
+    },
+    ["Rankrewards"] = {
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Risky Dice", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 25, ["Rewards"] = {{["Meta"] = {"Risky Dice", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Frost Bind", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 25, ["Rewards"] = {{["Meta"] = {"Frost Bind", 1}, ["RewardType"] = "Item"}}}
+        },
+        ["IsChallengeDrop"] = true,
+        ["ChallengeDropChance"] = 10
+    },
+    ["ExtraGold"] = {
+        ["Pool"] = {{["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Gold", 500}, ["RewardType"] = "Currency"}}}},
+        ["IsChallengeDrop"] = true,
+        ["ChallengeDropChance"] = 10
+    },
+    ["FooshaVillageInfinite"] = {
+        ["IsChallengeDrop"] = false,
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Meat", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 40, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}}
+        }
+    },
+    ["CursedAcademy"] = {
+        ["IsChallengeDrop"] = false,
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Gems", 30}, ["RewardType"] = "Currency"}}},
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Cursed Object", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Cursed Object", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 25, ["Rewards"] = {{["Meta"] = {"Cursed Object", 2}, ["RewardType"] = "Item"}}}
+        }
+    },
+    ["WeeklyAssortedQuest"] = {
+        ["IsChallengeDrop"] = false,
+        ["Pool"] = {
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Energy Crystal", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Meat", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Cursed Object", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Risky Dice", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Gold", 500}, ["RewardType"] = "Currency"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Gems", 100}, ["RewardType"] = "Currency"}}}
+        }
+    },
+    ["TraitCrystal"] = {
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 25, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}}
+        },
+        ["IsChallengeDrop"] = true,
+        ["ChallengeDropChance"] = 15
+    },
+    ["ExtraGems"] = {
+        ["Pool"] = {{["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Gems", 100}, ["RewardType"] = "Currency"}}}},
+        ["IsChallengeDrop"] = true,
+        ["ChallengeDropChance"] = 10
+    },
+    ["CursedDungeonRaid"] = {
+        ["IsChallengeDrop"] = false,
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Raid Shards (Red)", 30}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Raid Shards (Red)", 5}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Raid Shards (Red)", 5}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Raid Shards (Red)", 5}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Raid Shards (Red)", 5}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Gems", 50}, ["RewardType"] = "Currency"}}}
+        }
+    },
+    ["UnderwaterTemple"] = {
+        ["IsChallengeDrop"] = false,
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Gems", 30}, ["RewardType"] = "Currency"}}},
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Underwater Chest", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Underwater Chest", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 25, ["Rewards"] = {{["Meta"] = {"Underwater Chest", 2}, ["RewardType"] = "Item"}}}
+        }
+    },
+    ["Foosha"] = {
+        ["IsChallengeDrop"] = false,
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Gems", 30}, ["RewardType"] = "Currency"}}},
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Meat", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Meat", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 25, ["Rewards"] = {{["Meta"] = {"Meat", 2}, ["RewardType"] = "Item"}}}
+        }
+    },
+    ["BluePlanet"] = {
+        ["IsChallengeDrop"] = false,
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Gems", 30}, ["RewardType"] = "Currency"}}},
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Alien Fruit", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Alien Fruit", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 25, ["Rewards"] = {{["Meta"] = {"Alien Fruit", 2}, ["RewardType"] = "Item"}}}
+        }
+    },
+    ["BluePlanetInfinite"] = {
+        ["IsChallengeDrop"] = false,
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Alien Fruit", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 60, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 45, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 35, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}}
+        }
+    },
+    ["StarRiftRandom"] = {
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Star Rift (Yellow)", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Star Rift (Yellow)", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 25, ["Rewards"] = {{["Meta"] = {"Star Rift (Yellow)", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Star Rift (Red)", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Star Rift (Blue)", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Star Rift (Green)", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Star Rift (Purple)", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 5, ["Rewards"] = {{["Meta"] = {"Star Rift (Rainbow)", 1}, ["RewardType"] = "Item"}}}
+        },
+        ["IsChallengeDrop"] = true,
+        ["ChallengeDropChance"] = 50
+    },
+    ["VIPQuest"] = {
+        ["IsChallengeDrop"] = false,
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Risky Dice", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Gems", 50}, ["RewardType"] = "Currency"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Energy Crystal", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Meat", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Cursed Object", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 25, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 25, ["Rewards"] = {{["Meta"] = {"Risky Dice", 1}, ["RewardType"] = "Item"}}}
+        }
+    },
+    ["UnderwaterTempleInfinite"] = {
+        ["IsChallengeDrop"] = false,
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Underwater Chest", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 60, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 55, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 45, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}}
+        }
+    },
+    ["MobCity"] = {
+        ["IsChallengeDrop"] = false,
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Gems", 30}, ["RewardType"] = "Currency"}}},
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Energy Crystal", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Energy Crystal", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 25, ["Rewards"] = {{["Meta"] = {"Energy Crystal", 2}, ["RewardType"] = "Item"}}}
+        }
+    },
+    ["WeeklyQuest"] = {
+        ["IsChallengeDrop"] = false,
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 4}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Risky Dice", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Gems", 200}, ["RewardType"] = "Currency"}}},
+            {["Chance"] = 25, ["Rewards"] = {{["Meta"] = {"Gems", 300}, ["RewardType"] = "Currency"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Gold", 1000}, ["RewardType"] = "Currency"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Risky Dice", 1}, ["RewardType"] = "Item"}}}
+        }
+    },
+    ["StarRiftRainbow"] = {
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Star Rift (Rainbow)", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 5, ["Rewards"] = {{["Meta"] = {"Star Rift (Rainbow)", 1}, ["RewardType"] = "Item"}}}
+        },
+        ["IsChallengeDrop"] = true,
+        ["ChallengeDropChance"] = 5
+    },
+    ["MobCityInfinite"] = {
+        ["IsChallengeDrop"] = false,
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Energy Crystal", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 50, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 30, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}}
+        }
+    },
+    ["CursedAcademyInfinite"] = {
+        ["IsChallengeDrop"] = false,
+        ["Pool"] = {
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Cursed Object", 2}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 100, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 60, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 35, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}},
+            {["Chance"] = 25, ["Rewards"] = {{["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"}}}
+        }
+    }
+}
