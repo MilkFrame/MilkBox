d21787b3de284202ccbd834ca0fa102bc8b90fa9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-15 21:18:52 +1000

    Create RaidShopData.lua

---
+++ GameData/Anime Defenders [17017769292]/RaidShopData.lua
@@ -0,0 +1,49 @@
+data = {
+    ["Raid1"] = {
+        ["Pool"] = {
+            {
+                ["MaxDiscount"] = 20,
+                ["Quantity"] = 5,
+                ["Item"] = {["Meta"] = {"Gems", 30}, ["RewardType"] = "Currency"},
+                ["Cost"] = 25,
+                ["MinDiscount"] = 0
+            },
+            {
+                ["MaxDiscount"] = 20,
+                ["Quantity"] = 3,
+                ["Item"] = {["Meta"] = {"Trait Crystal", 1}, ["RewardType"] = "Item"},
+                ["Cost"] = 15,
+                ["MinDiscount"] = 0
+            },
+            {
+                ["MaxDiscount"] = 20,
+                ["Quantity"] = 3,
+                ["Item"] = {["Meta"] = {"Risky Dice", 1}, ["RewardType"] = "Item"},
+                ["Cost"] = 25,
+                ["MinDiscount"] = 0
+            },
+            {
+                ["MaxDiscount"] = 20,
+                ["Quantity"] = 3,
+                ["Item"] = {["Meta"] = {"Frost Bind", 1}, ["RewardType"] = "Item"},
+                ["Cost"] = 20,
+                ["MinDiscount"] = 0
+            },
+            {
+                ["MaxDiscount"] = 30,
+                ["Quantity"] = 1,
+                ["Item"] = {["Meta"] = {"Bear King"}, ["RewardType"] = "Unit"},
+                ["Cost"] = 2000,
+                ["MinDiscount"] = 0
+            },
+            {
+                ["MaxDiscount"] = 30,
+                ["Quantity"] = 1,
+                ["Item"] = {["Meta"] = {"Otherworldly Gauntlet", 1}, ["RewardType"] = "Item"},
+                ["Cost"] = 1000,
+                ["MinDiscount"] = 0
+            }
+        },
+        ["ShopCurrencyItem"] = "Raid Shards (Red)"
+    }
+}
