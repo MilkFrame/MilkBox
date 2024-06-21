4173338c282a8ed1868e6d873054a436f2c33ee9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:50:45 +1000

    Create Chests.lua

---
+++ GameData/Treasure Hunt Simulator [1345139196]/Chests.lua
@@ -0,0 +1,254 @@
+data = {
+    ["Rainbow Chest"] = {
+        ["Health"] = 1000000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 1},
+        ["Rainbow"] = {["RubyReward"] = 1093, ["Mat"] = "Rainbow Chest", ["Health"] = 1000000, ["Reward"] = 69172552},
+        ["Rubies"] = {["Max"] = 1500, ["Min"] = 500},
+        ["SpawnChance"] = 3,
+        ["Coins"] = {["Max"] = 75000000, ["Min"] = 25000000}
+    },
+    ["Diamond Chest"] = {
+        ["Diamond"] = {["Mat"] = "Diamond Chest", ["RubyReward"] = 27, ["Health"] = 625, ["Reward"] = 30113},
+        ["Health"] = 625,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 100},
+        ["Rubies"] = {["Max"] = 40, ["Min"] = 0},
+        ["Coins"] = {["Max"] = 37500, ["Min"] = 12500},
+        ["SpawnChance"] = 15
+    },
+    ["Rare Chest"] = {
+        ["Health"] = 25,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 5},
+        ["Rare"] = {["RubyReward"] = 8, ["Mat"] = "Rare Chest", ["Health"] = 25, ["Reward"] = 794},
+        ["Rubies"] = {["Max"] = 10, ["Min"] = 0},
+        ["SpawnChance"] = 50,
+        ["Coins"] = {["Max"] = 1500, ["Min"] = 500}
+    },
+    ["Common Chest"] = {
+        ["Health"] = 1,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 1},
+        ["Common"] = {["Mat"] = "Common Chest", ["RubyReward"] = 5, ["Health"] = 1, ["Reward"] = 236},
+        ["Rubies"] = {["Max"] = 5, ["Min"] = 0},
+        ["SpawnChance"] = 100,
+        ["Coins"] = {["Max"] = 300, ["Min"] = 100}
+    },
+    ["Ruby Chest"] = {
+        ["Ruby"] = {["Mat"] = "Ruby Chest", ["RubyReward"] = 9, ["Health"] = 25, ["Reward"] = 699},
+        ["Health"] = 25,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 5},
+        ["Rubies"] = {["Max"] = 11, ["Min"] = 1},
+        ["Coins"] = {["Max"] = 1600, ["Min"] = 600},
+        ["SpawnChance"] = 35
+    },
+    ["Amethyst Chest"] = {
+        ["Health"] = 100000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 300},
+        ["Amethyst"] = {["RubyReward"] = 67, ["Mat"] = "Amethyst Chest", ["Health"] = 100000, ["Reward"] = 3750632},
+        ["Rubies"] = {["Max"] = 150, ["Min"] = 50},
+        ["Coins"] = {["Max"] = 7500000, ["Min"] = 2500000},
+        ["SpawnChance"] = 5
+    },
+    ["Atlantis Chest"] = {
+        ["Health"] = 200,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 100},
+        ["Atlantis"] = {["Mat"] = "Atlantis Chest", ["RubyReward"] = 7, ["Health"] = 200, ["Reward"] = 9161},
+        ["Rubies"] = {["Max"] = 30, ["Min"] = 0},
+        ["SpawnChance"] = 20,
+        ["Coins"] = {["Max"] = 15000, ["Min"] = 5000}
+    },
+    ["Pumpkin Chest"] = {
+        ["Pumpkin"] = {["RubyReward"] = 415, ["Mat"] = "Pumpkin Chest", ["Health"] = 15000, ["Reward"] = 3184982},
+        ["Health"] = 15000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 150},
+        ["Rubies"] = {["Max"] = 480, ["Min"] = 180},
+        ["SpawnChance"] = 40,
+        ["Coins"] = {["Max"] = 3375000, ["Min"] = 1125000}
+    },
+    ["Epic Chest"] = {
+        ["Health"] = 125,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 25},
+        ["Epic"] = {["RubyReward"] = 19, ["Mat"] = "Epic Chest", ["Health"] = 125, ["Reward"] = 6271},
+        ["Rubies"] = {["Max"] = 20, ["Min"] = 0},
+        ["SpawnChance"] = 25,
+        ["Coins"] = {["Max"] = 7500, ["Min"] = 2500}
+    },
+    ["Stone Chest"] = {
+        ["Stone"] = {["RubyReward"] = 52, ["Mat"] = "Stone Chest", ["Health"] = 1000, ["Reward"] = 201329},
+        ["Health"] = 1000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 50},
+        ["Rubies"] = {["Max"] = 100, ["Min"] = 50},
+        ["SpawnChance"] = 10,
+        ["Coins"] = {["Max"] = 300000, ["Min"] = 100000}
+    },
+    ["Jackpot Chest"] = {
+        ["Health"] = 10000000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 777},
+        ["Rubies"] = {["Max"] = 5000, ["Min"] = 2500},
+        ["SpawnChance"] = 0.001,
+        ["Coins"] = {["Max"] = 600000000, ["Min"] = 200000000}
+    },
+    ["Elite Chest"] = {
+        ["Health"] = 150000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 1000},
+        ["Rubies"] = {["Max"] = 450, ["Min"] = 225},
+        ["SpawnChance"] = 1,
+        ["Coins"] = {["Max"] = 7500000, ["Min"] = 2500000}
+    },
+    ["Sacred Chest"] = {
+        ["Health"] = 400000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 2500},
+        ["Rubies"] = {["Max"] = 640, ["Min"] = 320},
+        ["SpawnChance"] = 0.01,
+        ["Coins"] = {["Max"] = 30000000, ["Min"] = 10000000}
+    },
+    ["Magma Chest"] = {
+        ["Health"] = 50000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 500},
+        ["SpawnChance"] = 5,
+        ["Rubies"] = {["Max"] = 350, ["Min"] = 150},
+        ["Magma"] = {["Mat"] = "Magma Chest", ["RubyReward"] = 186, ["Health"] = 50000, ["Reward"] = 5133759},
+        ["Coins"] = {["Max"] = 6000000, ["Min"] = 1000000}
+    },
+    ["Hell Chest"] = {
+        ["Hell"] = {["RubyReward"] = 666, ["Mat"] = "Hell Chest", ["Health"] = 666, ["Reward"] = 0},
+        ["Health"] = 666,
+        ["SpawnDepth"] = {["Max"] = 666, ["Min"] = 666},
+        ["Rubies"] = {["Max"] = 666, ["Min"] = 666},
+        ["SpawnChance"] = 1,
+        ["Coins"] = {["Max"] = 0, ["Min"] = 0}
+    },
+    ["Golden Chest"] = {
+        ["Golden"] = {["Mat"] = "Golden Chest", ["RubyReward"] = 109, ["Health"] = 6250, ["Reward"] = 215237},
+        ["Health"] = 6250,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 150},
+        ["Rubies"] = {["Max"] = 160, ["Min"] = 20},
+        ["SpawnChance"] = 10,
+        ["Coins"] = {["Max"] = 375000, ["Min"] = 125000}
+    },
+    ["Snow Chest"] = {
+        ["Health"] = 2000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 250},
+        ["Snow"] = {["RubyReward"] = 179, ["Mat"] = "Snow Chest", ["Health"] = 2000, ["Reward"] = 1031724},
+        ["Rubies"] = {["Max"] = 200, ["Min"] = 100},
+        ["SpawnChance"] = 5,
+        ["Coins"] = {["Max"] = 1500000, ["Min"] = 500000}
+    },
+    ["Police Chest"] = {
+        ["Health"] = 50000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 911},
+        ["Police"] = {["RubyReward"] = 174, ["Mat"] = "Police Chest", ["Health"] = 50000, ["Reward"] = 1015258},
+        ["Rubies"] = {["Max"] = 200, ["Min"] = 100},
+        ["SpawnChance"] = 7,
+        ["Coins"] = {["Max"] = 3000000, ["Min"] = 1000000}
+    },
+    ["Mermaid Chest"] = {
+        ["Health"] = 100000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 500},
+        ["Atlantis"] = {["Mat"] = "Mermaid Chest", ["RubyReward"] = 359, ["Health"] = 100000, ["Reward"] = 4858127},
+        ["Rubies"] = {["Max"] = 400, ["Min"] = 200},
+        ["SpawnChance"] = 5,
+        ["Coins"] = {["Max"] = 6000000, ["Min"] = 2000000}
+    },
+    ["Shadow Chest"] = {
+        ["Health"] = 75000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 2500},
+        ["Rubies"] = {["Max"] = 320, ["Min"] = 160},
+        ["SpawnChance"] = 1,
+        ["Coins"] = {["Max"] = 4687500, ["Min"] = 1562500}
+    },
+    ["Alien Chest"] = {
+        ["Health"] = 5000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 100},
+        ["Alien"] = {["RubyReward"] = 23, ["Mat"] = "Alien Chest", ["Health"] = 5000, ["Reward"] = 121148},
+        ["Rubies"] = {["Max"] = 40, ["Min"] = 20},
+        ["SpawnChance"] = 5,
+        ["Coins"] = {["Max"] = 300000, ["Min"] = 100000}
+    },
+    ["Emerald Chest"] = {
+        ["Health"] = 3125,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 625},
+        ["Emerald"] = {["RubyReward"] = 13, ["Mat"] = "Emerald Chest", ["Health"] = 3125, ["Reward"] = 69390},
+        ["Rubies"] = {["Max"] = 80, ["Min"] = 0},
+        ["SpawnChance"] = 10,
+        ["Coins"] = {["Max"] = 187500, ["Min"] = 62500}
+    },
+    ["Mars Chest"] = {
+        ["Mars"] = {["Mat"] = "Mars Chest", ["RubyReward"] = 591, ["Health"] = 125, ["Reward"] = 5311441},
+        ["Health"] = 125,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 200},
+        ["Rubies"] = {["Max"] = 600, ["Min"] = 200},
+        ["SpawnChance"] = 15,
+        ["Coins"] = {["Max"] = 9000000, ["Min"] = 3000000}
+    },
+    ["Astronaut Chest"] = {
+        ["Health"] = 125,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 50},
+        ["Astronaut"] = {["RubyReward"] = 5, ["Mat"] = "Astronaut Chest", ["Health"] = 125, ["Reward"] = 21224},
+        ["Rubies"] = {["Max"] = 10, ["Min"] = 5},
+        ["SpawnChance"] = 15,
+        ["Coins"] = {["Max"] = 30000, ["Min"] = 10000}
+    },
+    ["Knight's Chest"] = {
+        ["Knight's"] = {["Mat"] = "Knight's Chest", ["RubyReward"] = 193, ["Health"] = 50000, ["Reward"] = 2143052},
+        ["Health"] = 50000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 500},
+        ["Rubies"] = {["Max"] = 200, ["Min"] = 100},
+        ["SpawnChance"] = 5,
+        ["Coins"] = {["Max"] = 3000000, ["Min"] = 1000000}
+    },
+    ["Mythical Chest"] = {
+        ["Health"] = 16000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 1000},
+        ["Mythical"] = {["RubyReward"] = 97, ["Mat"] = "Mythical Chest", ["Health"] = 16000, ["Reward"] = 342591},
+        ["Rubies"] = {["Max"] = 160, ["Min"] = 80},
+        ["SpawnChance"] = 5,
+        ["Coins"] = {["Max"] = 937500, ["Min"] = 312500}
+    },
+    ["Prisoner Chest"] = {
+        ["Health"] = 10000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 150},
+        ["SpawnChance"] = 15,
+        ["Rubies"] = {["Max"] = 160, ["Min"] = 40},
+        ["Prisoner"] = {["RubyReward"] = 62, ["Mat"] = "Prisoner Chest", ["Health"] = 10000, ["Reward"] = 408788},
+        ["Coins"] = {["Max"] = 750000, ["Min"] = 250000}
+    },
+    ["Dice Chest"] = {
+        ["Dice"] = {["Mat"] = "Dice Chest", ["RubyReward"] = 48, ["Health"] = 777, ["Reward"] = 2330010},
+        ["Health"] = 777,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 50},
+        ["Rubies"] = {["Max"] = 77, ["Min"] = 7},
+        ["SpawnChance"] = 7,
+        ["Coins"] = {["Max"] = 7777777, ["Min"] = 777777}
+    },
+    ["Spooky Chest"] = {
+        ["Spooky"] = {["RubyReward"] = 861, ["Mat"] = "Spooky Chest", ["Health"] = 150000, ["Reward"] = 13859730},
+        ["Health"] = 150000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 400},
+        ["Rubies"] = {["Max"] = 1200, ["Min"] = 400},
+        ["SpawnChance"] = 15,
+        ["Coins"] = {["Max"] = 27000000, ["Min"] = 9000000}
+    },
+    ["Trident Chest"] = {
+        ["Trident"] = {["RubyReward"] = 27, ["Mat"] = "Trident Chest", ["Health"] = 5000, ["Reward"] = 247985},
+        ["Health"] = 5000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 250},
+        ["Rubies"] = {["Max"] = 40, ["Min"] = 20},
+        ["SpawnChance"] = 10,
+        ["Coins"] = {["Max"] = 300000, ["Min"] = 100000}
+    },
+    ["Legendary Chest"] = {
+        ["Legendary"] = {["RubyReward"] = 22, ["Mat"] = "Legendary Chest", ["Health"] = 625, ["Reward"] = 17246},
+        ["Health"] = 625,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 125},
+        ["Rubies"] = {["Max"] = 40, ["Min"] = 0},
+        ["SpawnChance"] = 15,
+        ["Coins"] = {["Max"] = 37500, ["Min"] = 12500}
+    },
+    ["Space Chest"] = {
+        ["Health"] = 15000,
+        ["SpawnDepth"] = {["Max"] = 99999999999, ["Min"] = 500},
+        ["Space"] = {["Mat"] = "Space Chest", ["RubyReward"] = 260, ["Health"] = 15000, ["Reward"] = 1056684},
+        ["Rubies"] = {["Max"] = 360, ["Min"] = 60},
+        ["SpawnChance"] = 30,
+        ["Coins"] = {["Max"] = 1125000, ["Min"] = 375000}
+    }
+}
