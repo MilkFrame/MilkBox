1596e100a00433a191ad1080916fce617216a7e7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:21:19 +1000

    Create monetization.lua

---
+++ GameData/Gym League [17450551531]/monetization.lua
@@ -0,0 +1,387 @@
+data = {
+    ["products"] = {
+        ["1841412934"] = "function: 0xb2d614da324b7971",
+        ["1841412931"] = "function: 0xb05f3f67230aa521",
+        ["1841412932"] = "function: 0xbe24abe814b6ce91",
+        ["1841738362"] = "function: 0xecb1e6c517223e01",
+        ["1841356711"] = "function: 0x2bbc6d6d68233761",
+        ["1841714352"] = "function: 0x0af77e2db5d3ee61",
+        ["1841412930"] = "function: 0xcf4cd62cc0805441",
+        ["1841412928"] = "function: 0x37077af6f6f81eb1",
+        ["1841738361"] = "function: 0x8123d48cc908bf01",
+        ["1841738359"] = "function: 0x76b69afd42f48fe1",
+        ["1841408115"] = "function: 0xea5011c4bb41ade1",
+        ["1841412935"] = "function: 0x2bf8f1a4514fd1d1",
+        ["1841714590"] = "function: 0xe1bdefc0f6d40d21",
+        ["1841408118"] = "function: 0xbd17ac73f25a6021",
+        ["1841738363"] = "function: 0xc60a4991ebb20e21",
+        ["1841738360"] = "function: 0x90c6902708630ca1",
+        ["1841716587"] = "function: 0x48635f7327a588e1",
+        ["1842377656"] = "function: 0xe7de3dc5309f8f61",
+        ["1841738358"] = "function: 0x3f7c467105e5df41",
+        ["1842381205"] = "function: 0x6fb8f5eb6b36ff81",
+        ["1841713914"] = "function: 0xe2adb4c3d84a5c41",
+        ["1841412933"] = "function: 0x3cae687b0594e601",
+        ["1841408117"] = "function: 0x12a2c5a99ac2ddc1",
+        ["1841714685"] = "function: 0x5dd71739562f5ec1",
+        ["1841738356"] = "function: 0x15f1635b6d870fa1",
+        ["1841714447"] = "function: 0x8d5ac79f41717081",
+        ["1841823662"] = "function: 0x4837a5d3c6be4f51",
+        ["1841738366"] = "function: 0xdecc604f5213dfc1",
+        ["1841408116"] = "function: 0x4a06291693c43901",
+        ["1841408113"] = "function: 0x524ccea964b929a1"
+    },
+    ["packs"] = {
+        {
+            ["name"] = "Starter Pack",
+            ["productId"] = 1841738362,
+            ["discountedPrice"] = 349,
+            ["content"] = {
+                {
+                    ["display"] = "+10 Rerolls",
+                    ["type"] = "rerolls",
+                    ["icon"] = "rbxassetid://17632254273",
+                    ["amount"] = 10
+                },
+                {
+                    ["type"] = "cash",
+                    ["amount"] = 20000,
+                    ["display"] = "+$20,000",
+                    ["icon"] = "rbxassetid://17701524578",
+                    ["color"] = Color3.fromRGB(255, 242, 98)
+                },
+                {
+                    ["display"] = "+1 Every Potion",
+                    ["type"] = "allPotion",
+                    ["icon"] = "rbxassetid://17701088509",
+                    ["amount"] = 1
+                }
+            },
+            ["normalPrice"] = 670
+        },
+        {
+            ["bgColor"] = Color3.fromRGB(255, 204, 0),
+            ["name"] = "Premium Pack",
+            ["productId"] = 1841738360,
+            ["discountedPrice"] = 1049,
+            ["normalPrice"] = 1800,
+            ["content"] = {
+                {
+                    ["display"] = "+25 Rerolls",
+                    ["type"] = "rerolls",
+                    ["icon"] = "rbxassetid://17632254273",
+                    ["amount"] = 25
+                },
+                {
+                    ["type"] = "cash",
+                    ["amount"] = 50000,
+                    ["display"] = "+$50,000",
+                    ["icon"] = "rbxassetid://17701524578",
+                    ["color"] = Color3.fromRGB(255, 242, 98)
+                },
+                {
+                    ["display"] = "+3 Every Potion",
+                    ["type"] = "allPotion",
+                    ["icon"] = "rbxassetid://17701088509",
+                    ["amount"] = 3
+                }
+            },
+            ["textColor"] = Color3.fromRGB(223, 223, 85)
+        },
+        {
+            ["bgColor"] = Color3.fromRGB(41, 219, 255),
+            ["name"] = "Mega Potion Pack",
+            ["productId"] = 1841738358,
+            ["discountedPrice"] = 1470,
+            ["normalPrice"] = 2940,
+            ["content"] = {
+                {
+                    ["display"] = "+10 Every Potion",
+                    ["type"] = "allPotion",
+                    ["icon"] = "rbxassetid://17701088509",
+                    ["amount"] = 10
+                }
+            },
+            ["textColor"] = Color3.fromRGB(37, 197, 229)
+        },
+        {
+            ["bgColor"] = Color3.fromRGB(206, 255, 249),
+            ["name"] = "Spin Pack",
+            ["productId"] = 1842377656,
+            ["discountedPrice"] = 1249,
+            ["normalPrice"] = 2139,
+            ["content"] = {
+                {
+                    ["display"] = "+50 Rerolls",
+                    ["type"] = "rerolls",
+                    ["icon"] = "rbxassetid://17632254273",
+                    ["amount"] = 50
+                },
+                {
+                    ["type"] = "rerolls",
+                    ["amount"] = 10,
+                    ["display"] = "+10 Premium Rerolls",
+                    ["iconColor"] = Color3.fromRGB(255, 137, 34),
+                    ["icon"] = "rbxassetid://17632254273",
+                    ["premium"] = true
+                }
+            },
+            ["textColor"] = Color3.fromRGB(164, 203, 198)
+        },
+        {
+            ["bgColor"] = Color3.fromRGB(146, 255, 242),
+            ["name"] = "Advanced Spin Pack",
+            ["productId"] = 1842381205,
+            ["discountedPrice"] = 1749,
+            ["normalPrice"] = 3089,
+            ["content"] = {
+                {
+                    ["type"] = "poseRerolls",
+                    ["amount"] = 50,
+                    ["display"] = "+50 Pose Rerolls",
+                    ["icon"] = "rbxassetid://17441465403",
+                    ["premium"] = true
+                },
+                {
+                    ["display"] = "+50 Rerolls",
+                    ["type"] = "rerolls",
+                    ["icon"] = "rbxassetid://17632254273",
+                    ["amount"] = 50
+                },
+                {
+                    ["type"] = "rerolls",
+                    ["amount"] = 10,
+                    ["display"] = "+10 Premium Rerolls",
+                    ["iconColor"] = Color3.fromRGB(255, 178, 83),
+                    ["icon"] = "rbxassetid://17632254273",
+                    ["premium"] = true
+                }
+            },
+            ["textColor"] = Color3.fromRGB(133, 203, 192)
+        },
+        {
+            ["bgColor"] = Color3.fromRGB(169, 255, 71),
+            ["name"] = "Monster Pack",
+            ["productId"] = 1841738363,
+            ["discountedPrice"] = 1149,
+            ["normalPrice"] = 1648,
+            ["content"] = {
+                {
+                    ["display"] = "Intelligent Trainer",
+                    ["type"] = "gamepass",
+                    ["icon"] = "rbxassetid://17703499211",
+                    ["gamepass"] = "IntelligentTrainer"
+                },
+                {
+                    ["display"] = "Mutated Genes",
+                    ["type"] = "gamepass",
+                    ["icon"] = "rbxassetid://17703349783",
+                    ["gamepass"] = "MutatedGenes"
+                }
+            },
+            ["textColor"] = Color3.fromRGB(78, 118, 33)
+        },
+        {
+            ["bgColor"] = Color3.fromRGB(255, 240, 71),
+            ["name"] = "All In One Pack",
+            ["productId"] = 1841738366,
+            ["discountedPrice"] = 999,
+            ["normalPrice"] = 1440,
+            ["content"] = {
+                {
+                    ["display"] = "Pumped",
+                    ["type"] = "gamepass",
+                    ["icon"] = "rbxassetid://17703327047",
+                    ["gamepass"] = "Pumped"
+                },
+                {
+                    ["display"] = "Muscle Man",
+                    ["type"] = "gamepass",
+                    ["icon"] = "rbxassetid://17703336128",
+                    ["gamepass"] = "MuscleMan"
+                },
+                {
+                    ["display"] = "Mutated Genes",
+                    ["type"] = "gamepass",
+                    ["icon"] = "rbxassetid://17703349783",
+                    ["gamepass"] = "MutatedGenes"
+                }
+            },
+            ["textColor"] = Color3.fromRGB(223, 210, 62)
+        },
+        {
+            ["bgColor"] = Color3.fromRGB(70, 190, 255),
+            ["name"] = "Intelligent Pack",
+            ["productId"] = 1841738359,
+            ["discountedPrice"] = 630,
+            ["normalPrice"] = 900,
+            ["content"] = {
+                {
+                    ["display"] = "Intelligent Trainer",
+                    ["type"] = "gamepass",
+                    ["icon"] = "rbxassetid://17703499211",
+                    ["gamepass"] = "IntelligentTrainer"
+                },
+                {
+                    ["display"] = "Galaxy Weight",
+                    ["type"] = "gamepass",
+                    ["icon"] = "rbxassetid://17703290595",
+                    ["gamepass"] = "GalaxyWeight"
+                }
+            },
+            ["textColor"] = Color3.fromRGB(54, 147, 197)
+        },
+        {
+            ["bgColor"] = Color3.fromRGB(255, 202, 111),
+            ["name"] = "Auto Farmer Pack",
+            ["productId"] = 1841738361,
+            ["discountedPrice"] = 449,
+            ["normalPrice"] = 640,
+            ["content"] = {
+                {
+                    ["display"] = "Auto Train",
+                    ["type"] = "gamepass",
+                    ["icon"] = "rbxassetid://17703282644",
+                    ["gamepass"] = "AutoTrain"
+                },
+                {
+                    ["display"] = "Pumped",
+                    ["type"] = "gamepass",
+                    ["icon"] = "rbxassetid://17703327047",
+                    ["gamepass"] = "Pumped"
+                }
+            },
+            ["textColor"] = Color3.fromRGB(225, 176, 98)
+        },
+        {
+            ["bgColor"] = Color3.fromRGB(255, 240, 71),
+            ["name"] = "Filthy Rich Pack",
+            ["productId"] = 1841738356,
+            ["discountedPrice"] = 799,
+            ["normalPrice"] = 1370,
+            ["content"] = {
+                {
+                    ["display"] = "VIP",
+                    ["type"] = "gamepass",
+                    ["icon"] = "rbxassetid://17703391722",
+                    ["gamepass"] = "VIP"
+                },
+                {
+                    ["display"] = "Extra Cash",
+                    ["type"] = "gamepass",
+                    ["icon"] = "rbxassetid://17703322246",
+                    ["gamepass"] = "ExtraCash"
+                },
+                {
+                    ["type"] = "cash",
+                    ["amount"] = 25000,
+                    ["display"] = "+$25,000",
+                    ["icon"] = "rbxassetid://17701524578",
+                    ["color"] = Color3.fromRGB(255, 242, 98)
+                }
+            },
+            ["textColor"] = Color3.fromRGB(223, 210, 62)
+        }
+    },
+    ["ids"] = {
+        ["Skip"] = "836602023",
+        ["Pumped"] = "827642460",
+        ["AutoTrain"] = "827336789",
+        ["ExtraCash"] = "827693167",
+        ["GalaxyWeight"] = "827889103",
+        ["VIP"] = "827799028",
+        ["StaminaMaster"] = "832192282",
+        ["IntelligentTrainer"] = "827851007",
+        ["MutatedGenes"] = "827708085",
+        ["MuscleMan"] = "827896038",
+        ["HyperPump"] = "836353993",
+        ["Lucky"] = "827963005"
+    },
+    ["rerolls"] = {"1841713914", "1841714352", "1841714447", "1841714590", "1841714685"},
+    ["passes"] = {
+        ["827693167"] = {
+            ["onPurchase"] = "function: 0x30eeba748f0b7e81",
+            ["order"] = 2,
+            ["onJoin"] = "function: 0xc44bf3b7ac82ec61",
+            ["attribute"] = "ExtraCash",
+            ["product"] = 1849790731
+        },
+        ["827336789"] = {
+            ["onPurchase"] = "function: 0x1bbbd2988e175cc1",
+            ["order"] = 3,
+            ["onJoin"] = "function: 0x646884228f79ef21",
+            ["attribute"] = "AutoTrain",
+            ["product"] = 1849790739
+        },
+        ["827963005"] = {
+            ["onPurchase"] = "function: 0xba2eec50cc681d41",
+            ["order"] = 5,
+            ["onJoin"] = "function: 0xcaa0558e509b2ea1",
+            ["attribute"] = "Lucky",
+            ["product"] = 1849790730
+        },
+        ["827799028"] = {
+            ["onPurchase"] = "function: 0x852420aea47dbff1",
+            ["order"] = 1,
+            ["onJoin"] = "function: 0x79850be1a03e5e41",
+            ["attribute"] = "VIP",
+            ["product"] = 1849790729
+        },
+        ["827896038"] = {
+            ["onPurchase"] = "function: 0x9ddfe330b353d7a1",
+            ["order"] = 8,
+            ["onJoin"] = "function: 0xf08b2782165851e1",
+            ["attribute"] = "MuscleMan",
+            ["product"] = 1849790732
+        },
+        ["836602023"] = {
+            ["onPurchase"] = "function: 0xf59b7884731ff981",
+            ["order"] = 6,
+            ["onJoin"] = "function: 0x6245862acbe56961",
+            ["attribute"] = "Skip",
+            ["product"] = 1850408479
+        },
+        ["832192282"] = {
+            ["onPurchase"] = "function: 0x37043d6c714b7c81",
+            ["order"] = 11,
+            ["onJoin"] = "function: 0x9f5e74de24bdca61",
+            ["attribute"] = "StaminaMaster",
+            ["product"] = 1849790726
+        },
+        ["827642460"] = {
+            ["onPurchase"] = "function: 0xa5590f682a2d11c1",
+            ["order"] = 7,
+            ["onJoin"] = "function: 0x093f3737e2a66c21",
+            ["attribute"] = "Pumped",
+            ["product"] = 1849790734
+        },
+        ["827889103"] = {
+            ["onPurchase"] = "function: 0x6376013fed31a001",
+            ["order"] = 4,
+            ["onJoin"] = "function: 0xb31cd94da98eaee1",
+            ["attribute"] = "GalaxyWeight",
+            ["product"] = 1849790738
+        },
+        ["827851007"] = {
+            ["onPurchase"] = "function: 0xf7e0ae8027d61841",
+            ["order"] = 10,
+            ["onJoin"] = "function: 0xa811987e176ecda1",
+            ["attribute"] = "IntelligentTrainer",
+            ["product"] = 1849790728
+        },
+        ["836353993"] = {
+            ["onPurchase"] = "function: 0x466849e8ba4102c1",
+            ["order"] = 12,
+            ["onJoin"] = "function: 0xeecae182c5c4c121",
+            ["attribute"] = "HyperPump",
+            ["product"] = 1850408719
+        },
+        ["827708085"] = {
+            ["onPurchase"] = "function: 0x4247e7b183b5ef11",
+            ["order"] = 9,
+            ["onJoin"] = "function: 0x444f5e2c06f5bd01",
+            ["attribute"] = "MutatedGenes",
+            ["product"] = 1849790727
+        }
+    }
+}
