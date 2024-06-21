a5c742a9ce3e967cd8effcac2b69edf9b0004713
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:19:00 +1000

    Create dailyGift.lua

---
+++ GameData/Gym League [17450551531]/dailyGift.lua
@@ -0,0 +1,9 @@
+data = {
+    {["value"] = 250, ["image"] = "rbxassetid://17701691468", ["type"] = "cash", ["reward"] = "Cash"},
+    {["value"] = 1, ["image"] = "rbxassetid://17654121583", ["type"] = "Power Potion", ["reward"] = "Boost"},
+    {["value"] = 5, ["image"] = "rbxassetid://17735246415", ["type"] = "spin", ["reward"] = "Spin"},
+    {["value"] = 1, ["image"] = "rbxassetid://17654122529", ["type"] = "Money Potion", ["reward"] = "Boost"},
+    {["value"] = 10, ["image"] = "rbxassetid://17735246415", ["type"] = "spin", ["reward"] = "Spin"},
+    {["value"] = 1, ["image"] = "rbxassetid://17654121877", ["type"] = "Mega Potion", ["reward"] = "Boost"},
+    {["value"] = 1, ["image"] = "rbxassetid://17735246150", ["type"] = "premiumSpin", ["reward"] = "Spin"}
+}
