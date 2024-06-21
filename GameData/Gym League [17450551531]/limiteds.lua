6c5c0d38e6946f06a2203d9201341e1c3ea20153
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:20:52 +1000

    Create limiteds.lua

---
+++ GameData/Gym League [17450551531]/limiteds.lua
@@ -0,0 +1,9 @@
+data = {
+    ["1841356711"] = {
+        ["type"] = "GalaxyPack",
+        ["key"] = "GalaxyPack_01",
+        ["productId"] = 1841356711,
+        ["limit"] = 1,
+        ["stock"] = 5000
+    }
+}
