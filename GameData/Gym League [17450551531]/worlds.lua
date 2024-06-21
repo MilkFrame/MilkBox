e305dab4dccd053b37c2fa2a18ace73ab1c94b3c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:20:39 +1000

    Create worlds.lua

---
+++ GameData/Gym League [17450551531]/worlds.lua
@@ -0,0 +1,104 @@
+data = {
+    {
+        ["image"] = "rbxassetid://17739151800",
+        ["color"] = Color3.fromRGB(255, 201, 135),
+        ["display"] = "Beach GYM",
+        ["spin_price"] = 1500,
+        ["place"] = 17450551531,
+        ["price"] = 0
+    },
+    {
+        ["image"] = "rbxassetid://17739153186",
+        ["color"] = Color3.fromRGB(147, 199, 52),
+        ["display"] = "Underground GYM",
+        ["spin_price"] = 2000,
+        ["place"] = 17593437943,
+        ["price"] = 1000
+    },
+    {
+        ["image"] = "rbxassetid://17739152795",
+        ["color"] = Color3.fromRGB(255, 137, 107),
+        ["display"] = "The GYM",
+        ["spin_price"] = 2500,
+        ["place"] = 17593439780,
+        ["price"] = 10000
+    },
+    {
+        ["image"] = "rbxassetid://17739152361",
+        ["color"] = Color3.fromRGB(255, 245, 107),
+        ["display"] = "Golden GYM",
+        ["spin_price"] = 3000,
+        ["place"] = 17593439630,
+        ["price"] = 50000
+    },
+    {
+        ["image"] = "rbxassetid://17739151430",
+        ["color"] = Color3.fromRGB(119, 201, 255),
+        ["display"] = "Cyber GYM",
+        ["spin_price"] = 4500,
+        ["place"] = 17593441072,
+        ["price"] = 100000
+    },
+    {
+        ["image"] = "rbxassetid://17882974902",
+        ["color"] = Color3.fromRGB(255, 156, 69),
+        ["display"] = "Gravity GYM",
+        ["spin_price"] = 6000,
+        ["place"] = 17883015202,
+        ["price"] = 500000
+    },
+    ["Gravity GYM"] = {
+        ["image"] = "rbxassetid://17882974902",
+        ["order"] = 6,
+        ["color"] = Color3.fromRGB(255, 156, 69),
+        ["display"] = "Gravity GYM",
+        ["spin_price"] = 6000,
+        ["place"] = 17883015202,
+        ["price"] = 500000
+    },
+    ["Golden GYM"] = {
+        ["image"] = "rbxassetid://17739152361",
+        ["order"] = 4,
+        ["color"] = Color3.fromRGB(255, 245, 107),
+        ["display"] = "Golden GYM",
+        ["spin_price"] = 3000,
+        ["place"] = 17593439630,
+        ["price"] = 50000
+    },
+    ["Beach GYM"] = {
+        ["image"] = "rbxassetid://17739151800",
+        ["order"] = 1,
+        ["color"] = Color3.fromRGB(255, 201, 135),
+        ["display"] = "Beach GYM",
+        ["spin_price"] = 1500,
+        ["place"] = 17450551531,
+        ["price"] = 0
+    },
+    ["The GYM"] = {
+        ["image"] = "rbxassetid://17739152795",
+        ["order"] = 3,
+        ["color"] = Color3.fromRGB(255, 137, 107),
+        ["display"] = "The GYM",
+        ["spin_price"] = 2500,
+        ["place"] = 17593439780,
+        ["price"] = 10000
+    },
+    ["Cyber GYM"] = {
+        ["image"] = "rbxassetid://17739151430",
+        ["order"] = 5,
+        ["color"] = Color3.fromRGB(119, 201, 255),
+        ["display"] = "Cyber GYM",
+        ["spin_price"] = 4500,
+        ["place"] = 17593441072,
+        ["price"] = 100000
+    },
+    ["Underground GYM"] = {
+        ["image"] = "rbxassetid://17739153186",
+        ["order"] = 2,
+        ["color"] = Color3.fromRGB(147, 199, 52),
+        ["display"] = "Underground GYM",
+        ["spin_price"] = 2000,
+        ["place"] = 17593437943,
+        ["price"] = 1000
+    }
+}
