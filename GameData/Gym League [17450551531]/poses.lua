12064ae1427db6f30cfe507287de3ad895ed0290
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:19:12 +1000

    Create poses.lua

---
+++ GameData/Gym League [17450551531]/poses.lua
@@ -0,0 +1,16 @@
+data = {
+    ["Arm Up"] = {["display"] = "Arm Up", ["image"] = 17734920277, ["rarity"] = "Common"},
+    ["Godlike"] = {["display"] = "Godlike", ["image"] = 17705391271, ["rarity"] = "Exclusive"},
+    ["Front Lat Spread"] = {["display"] = "Front Lat Spread", ["image"] = 17734919695, ["rarity"] = "Legendary"},
+    ["The Kneel"] = {["display"] = "The Kneel", ["image"] = 17734919907, ["rarity"] = "Mythical"},
+    ["Victory"] = {["display"] = "Victory", ["image"] = 17734918835, ["rarity"] = "Rare"},
+    ["Side Chest"] = {["display"] = "Side Chest", ["image"] = 17734918974, ["rarity"] = "Rare"},
+    ["Most Muscular"] = {["display"] = "Most Muscular", ["image"] = 17734919530, ["rarity"] = "Mythical"},
+    ["Front Lat And Bicep"] = {["display"] = "Front Lat And Bicep", ["image"] = 17734920077, ["rarity"] = "Common"},
+    ["Aduket"] = {["display"] = "Aduket", ["image"] = 17734920428, ["rarity"] = "Epic"},
+    ["Three Quarter Back Biceps"] = {
+        ["display"] = "Three Quarter Back Biceps",
+        ["image"] = 17734919200,
+        ["rarity"] = "Legendary"
+    }
+}
