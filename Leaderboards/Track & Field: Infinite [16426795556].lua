c9148607c082c94a91b758d1dd7c31fce71455b4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 15:46:20 +1000

    Create Track & Field: Infinite [16426795556].lua

---
+++ Leaderboards/Track & Field: Infinite [16426795556].lua
@@ -0,0 +1,166 @@
+--20 June 2024 4:00pm
+data = {
+    ["Time Played"] = {
+        {1, "Rue_Key", "8067"},
+        {2, "wonPece", "7249"},
+        {3, "handsomegirl773", "7124"},
+        {4, "shinisplotting", "6509"},
+        {5, "1kmanman0", "6177"},
+        {6, "ilovefruitsyay", "6003"},
+        {7, "Crashout556n", "5858"},
+        {8, "Kiontejackson1111098", "5313"},
+        {9, "duckyvxk", "5307"},
+        {10, "z6vba", "5294"}
+    },
+    ["60M"] = {
+        {1, "vonloir", "00:05:71"},
+        {2, "DataVerxx", "00:05:71"},
+        {3, "Da1_Travis", "00:05:71"},
+        {4, "edits_hasgoodaim111", "00:05:71"},
+        {5, "11ghxsst", "00:05:71"},
+        {6, "Akazaori", "00:05:71"},
+        {7, "Cr1m1nalOnTop", "00:05:72"},
+        {8, "adebisi88", "00:05:73"},
+        {9, "Nixobixo0191", "00:05:73"},
+        {10, "jaaayb1132", "00:05:73"}
+    },
+    ["100M"] = {
+        {1, "contraent", "00:09:01"},
+        {2, "khoablox293", "00:09:01"},
+        {3, "Octoberismybabyx", "00:09:01"},
+        {4, "Dark0n0E", "00:09:01"},
+        {5, "Crvzzyzq", "00:09:01"}
+    },
+    ["200M"] = {
+        {1, "cooldudeawsome36", "00:19:01"},
+        {2, "giop210_alt1", "00:19:01"},
+        {3, "JamesWINS1234", "00:19:01"},
+        {4, "LuhhXi", "00:19:01"},
+        {5, "headpatsfromhim", "00:19:01"},
+        {6, "xaltilty", "00:19:01"},
+        {7, "edits_hasgoodaim111", "00:19:01"},
+        {8, "killedhezi", "00:19:01"},
+        {9, "MelindaGarcia17", "00:19:01"},
+        {10, "vMufasa", "00:19:01"}
+    },
+    ["300M"] = {
+        {1, "djyner", "00:29:51"},
+        {2, "moneygreenshadow", "00:29:51"},
+        {3, "lighteronbread", "00:29:52"},
+        {4, "cooldudeawsome36", "00:29:53"},
+        {5, "sh5ow", "00:29:53"},
+        {6, "Godfather23000", "00:29:53"},
+        {7, "AspiratorkingonYT", "00:29:54"},
+        {8, "DTBClothingHolder", "00:29:54"},
+        {9, "arickmnop", "00:29:54"},
+        {10, "sigurets", "00:29:56"}
+    },
+    ["400M"] = {
+        {1, "SemonDemonz", "00:40:01"},
+        {2, "QuintupleHyperNova", "00:40:01"},
+        {3, "jotley", "00:40:01"},
+        {4, "MelindaGarcia17", "00:40:01"},
+        {5, "TTVEnjoy", "00:40:01"},
+        {6, "JamesWINS1234", "00:40:01"},
+        {7, "cooldudeawsome36", "00:40:03"},
+        {8, "Dark0n0E", "00:40:03"},
+        {9, "taebackupifhacke", "00:40:03"},
+        {10, "jordanthegoat_lol", "00:40:04"}
+    },
+    ["800M"] = {
+        {1, "Chickencutletslol", "01:35:02"},
+        {2, "contraent", "01:35:04"},
+        {3, "cuwivos", "01:35:04"},
+        {4, "terkzy_3", "01:35:07"},
+        {5, "5qv46u1fk5ldym94cf", "01:35:07"},
+        {6, "ONGDGIKLL", "01:35:07"},
+        {7, "24k_DNGL", "01:35:09"},
+        {8, "d6ner", "01:35:09"},
+        {9, "XSheLoveGxdX", "01:35:09"},
+        {10, "Pecutura", "01:35:09"}
+    },
+    ["1600M"] = {
+        {1, "Stuhple", "03:15:02"},
+        {2, "recks98", "03:15:02"},
+        {3, "rellyrichkid243", "03:15:02"},
+        {4, "C0O_P", "03:15:03"},
+        {5, "cuwivos", "03:15:05"},
+        {6, "clIzxxm", "03:15:07"},
+        {7, "DreamyFazos", "03:15:07"},
+        {8, "jotley", "03:15:07"},
+        {9, "5qv46u1fk5ldym94cf", "03:15:09"},
+        {10, "Maqx_0", "03:15:10"}
+    },
+    ["110MH"] = {
+        {1, "SlapBattles_Org", "00:10:01"},
+        {2, "sigurets", "00:10:01"},
+        {3, "GlxbxlWr", "00:10:01"},
+        {4, "QuintupleHyperNova", "00:10:01"},
+        {5, "Dark0n0E", "00:10:01"},
+        {6, "Crvzzyzq", "00:10:01"},
+        {7, "jerc1ano", "00:10:01"},
+        {8, "sh5ow", "00:10:03"},
+        {9, "Poopster3724", "00:10:03"},
+        {10, "edits_hasgoodaim111", "00:10:03"}
+    },
+    ["400MH"] = {
+        {1, "SemonDemonz", "00:40:01"},
+        {2, "rellyrichkid243", "00:40:04"},
+        {3, "Poopster3724", "00:40:06"},
+        {4, "liveroftheolive", "00:40:06"},
+        {5, "Crvzzyzq", "00:40:06"},
+        {6, "FreeRwil3842", "00:40:06"},
+        {7, "DreamyFazos", "00:40:08"},
+        {8, "Truthninja40", "00:40:11"},
+        {9, "taxdeception", "00:40:13"},
+        {10, "Dark0n0E", "00:40:16"}
+    },
+    ["LJ"] = {
+        {1, "Uruglysike0710", "20.45"},
+        {2, "ilikegaby1", "19.98"},
+        {3, "ethaniscrazy65", "19.98"},
+        {4, "qDodgeV4", "19.98"},
+        {5, "princepaulb", "19.98"},
+        {6, "Sxbrin3", "19.98"},
+        {7, "sp5der_w3b", "19.98"},
+        {8, "Baconhairs914", "19.98"},
+        {9, "KaramaHasPain", "19.98"},
+        {10, "X_ZombieKiller", "19.98"}
+    },
+    ["HJ"] = {
+        {1, "a9b51", "5.48"},
+        {2, "Tyetonix", "5.38"},
+        {3, "peruna_prkl", "5.2"},
+        {4, "Stuhple", "5.16"},
+        {5, "ticketstohisdownfall", "5.09"},
+        {6, "adebisi88", "5.09"},
+        {7, "jotley", "4.96"},
+        {8, "jelly3277", "4.88"},
+        {9, "jahx224", "4.87"},
+        {10, "DJtheDJisfun", "4.83"}
+    }
+}
+o="{"
+function get(board,title)
+p={}
+for i,v in pairs(workspace.GlobalLeaderboards[board].SurfaceGui.Frame:GetChildren()) do
+if v.Name=="BarFrame" then
+table.insert(p,{tonumber(string.sub(v.TopRank.Text,2)),v.TopUser.Text,v.TopValue.Text})
+end
+end
+table.sort(p,function(a,b) return a[1]<b[1] end)
+o = o .."[\""..title.."\"]="..table_to_string(p)..",\n"
+end
+get("TimePlayedBoard","Time Played")
+get("60MBoard","60M")
+get("100MBoard","100M")
+get("200MBoard","200M")
+get("300MBoard","300M")
+get("400MBoard","400M")
+get("800MBoard","800M")
+get("1600MBoard","1600M")
+get("110MHBoard","110MH")
+get("400MHBoard","400MH")
+get("LJBoard","LJ")
+get("HJBoard","HJ")
+o=o.."}" setclipboard(o) print("Done")
