a5a49d1ff3ed9011b1331ca43c0f47d0f6feb15d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:50:21 +1000

    Create Shovels.lua

---
+++ GameData/Treasure Hunt Simulator [1345139196]/Shovels.lua
@@ -0,0 +1,546 @@
+data = {
+    ["Great Sword"] = {
+        ["Great Sword"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 7000,
+                ["DigRange"] = 50,
+                ["AttackLength"] = 0.15
+            }
+        },
+        ["Island"] = "Medieval",
+        ["Price"] = 650000000000
+    },
+    ["Golden Metal Detector"] = {
+        ["Price"] = 3000000000,
+        ["Golden Metal Detector"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "AutoDig",
+                ["BlockDamage"] = 3500,
+                ["AttackLength"] = 0.25,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Dual Drills"] = {
+        ["Price"] = 500000000,
+        ["Dual Drills"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "AutoDig",
+                ["BlockDamage"] = 2000,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Medium Shovel"] = {
+        ["Price"] = 2100,
+        ["Medium Shovel"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 5,
+                ["AttackLength"] = 1.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Lazer Gun"] = {
+        ["Price"] = 500000000000,
+        ["Island"] = "Toy Land",
+        ["Lazer Gun"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 6750,
+                ["DigRange"] = 50,
+                ["AttackLength"] = 0.15
+            }
+        }
+    },
+    ["Dual Scoops"] = {
+        ["Price"] = 22000000,
+        ["Dual Scoops"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 750,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Easter Basket"] = {
+        ["Price"] = 250000000,
+        ["Easter Basket"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 2000,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Small Shovel"] = {
+        ["Price"] = 600,
+        ["Small Shovel"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 4,
+                ["AttackLength"] = 1.5,
+                ["DigRange"] = 40
+            }
+        }
+    },
+    ["Magical Map"] = {
+        ["Price"] = 5000000000,
+        ["Island"] = "Pirate",
+        ["Magical Map"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "AutoDig",
+                ["BlockDamage"] = 4900,
+                ["AttackLength"] = 0.25,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Grenade"] = {
+        ["Price"] = 1500000000,
+        ["Grenade"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "Bomb",
+                ["BlockDamage"] = 5000,
+                ["AttackLength"] = 0.25,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Drill"] = {
+        ["Price"] = 45000000,
+        ["Drill"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "AutoDig",
+                ["BlockDamage"] = 1000,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Big Scooper"] = {
+        ["Price"] = 24000,
+        ["Big Scooper"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 10,
+                ["AttackLength"] = 1,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Magnifying Glass"] = {
+        ["Price"] = 1000000000,
+        ["Magnifying Glass"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "AutoDig",
+                ["BlockDamage"] = 3000,
+                ["AttackLength"] = 0.25,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Large Shovel"] = {
+        ["Price"] = 8800,
+        ["Large Shovel"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 7,
+                ["AttackLength"] = 1.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["C4"] = {
+        ["Price"] = 1500000,
+        ["C4"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "Bomb",
+                ["BlockDamage"] = 150,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Metal Detector"] = {
+        ["Price"] = 500000,
+        ["Metal Detector"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "AutoDig",
+                ["BlockDamage"] = 100,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Firework"] = {
+        ["Price"] = 80000000000,
+        ["Firework"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "Bomb",
+                ["BlockDamage"] = 6000,
+                ["AttackLength"] = 0.25,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Candy Hammer"] = {
+        ["Price"] = 20000000,
+        ["Island"] = "Candy",
+        ["Candy Hammer"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 800,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Giant Shovel"] = {
+        ["Price"] = 250000,
+        ["Giant Shovel"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 75,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Golden Fork"] = {
+        ["Price"] = 125000000000,
+        ["Golden Fork"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 6150,
+                ["AttackLength"] = 0.15,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Lollipop"] = {
+        ["Price"] = 200000,
+        ["Island"] = "Candy",
+        ["Lollipop"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 100,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Sword"] = {
+        ["Price"] = 5000000000,
+        ["Sword"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 5000,
+                ["DigRange"] = 50,
+                ["AttackLength"] = 0.2
+            }
+        },
+        ["Island"] = "Medieval"
+    },
+    ["Jack Hammer"] = {
+        ["Price"] = 3000000,
+        ["Jack Hammer"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "AutoDig",
+                ["BlockDamage"] = 250,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Nuke"] = {
+        ["Price"] = 100000000,
+        ["Nuke"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "Bomb",
+                ["BlockDamage"] = 2000,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Golden Nuke"] = {
+        ["Price"] = 300000000,
+        ["Golden Nuke"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "Bomb",
+                ["BlockDamage"] = 4000,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Golden Bucket"] = {
+        ["Price"] = 30000000,
+        ["Golden Bucket"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 1000,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 30
+            }
+        },
+        ["Island"] = "Pirate"
+    },
+    ["Police Baton"] = {
+        ["Price"] = 800000000000,
+        ["Island"] = "Prison",
+        ["Police Baton"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 7350,
+                ["DigRange"] = 50,
+                ["AttackLength"] = 0.15
+            }
+        }
+    },
+    ["Alien Lazer Gun"] = {
+        ["Alien Lazer Gun"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 7250,
+                ["DigRange"] = 50,
+                ["AttackLength"] = 0.15
+            }
+        },
+        ["Island"] = "Mars",
+        ["Price"] = 700000000000
+    },
+    ["Toy Shovel"] = {
+        ["Price"] = 250,
+        ["Toy Shovel"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 3,
+                ["AttackLength"] = 2,
+                ["DigRange"] = 30
+            }
+        }
+    },
+    ["Trident"] = {
+        ["Trident"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 6500,
+                ["DigRange"] = 50,
+                ["AttackLength"] = 0.15
+            }
+        },
+        ["Island"] = "Atlantis",
+        ["Price"] = 300000000000
+    },
+    ["Hoover"] = {
+        ["Price"] = 100000000000,
+        ["Hoover"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "AutoDig",
+                ["BlockDamage"] = 6000,
+                ["AttackLength"] = 0.15,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Dynamite"] = {
+        ["Price"] = 185000,
+        ["Dynamite"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "Bomb",
+                ["BlockDamage"] = 50,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Double Big Scooper"] = {
+        ["Price"] = 20000000000,
+        ["Double Big Scooper"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 5500,
+                ["AttackLength"] = 0.15,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Scythe"] = {
+        ["Price"] = 99,
+        ["Scythe"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "AutoDig",
+                ["BlockDamage"] = 10000,
+                ["AttackLength"] = 0.125,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Rake"] = {
+        ["Price"] = 40000,
+        ["Rake"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 15,
+                ["AttackLength"] = 1,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Black Hole"] = {
+        ["Price"] = 999999999,
+        ["Black Hole"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "AutoDig",
+                ["BlockDamage"] = 5000,
+                ["AttackLength"] = 0.15,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Spiked Shovel"] = {
+        ["Price"] = 6000000000,
+        ["Island"] = "Dino",
+        ["Spiked Shovel"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 5000,
+                ["AttackLength"] = 0.25,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Pickaxe"] = {
+        ["Price"] = 25000000,
+        ["Island"] = "Dino",
+        ["Pickaxe"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 900,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Lightning Bolt"] = {
+        ["Price"] = 999999999,
+        ["Lightning Bolt"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "AutoDig",
+                ["BlockDamage"] = 3500,
+                ["AttackLength"] = 0.3,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Golden Spoon"] = {
+        ["Price"] = 10000000,
+        ["Golden Spoon"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 500,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Spade"] = {
+        ["Price"] = 100,
+        ["Spade"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 2,
+                ["AttackLength"] = 2,
+                ["DigRange"] = 30
+            }
+        }
+    },
+    ["Bucket"] = {
+        ["Price"] = 0,
+        ["Bucket"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 1,
+                ["AttackLength"] = 2,
+                ["DigRange"] = 30
+            }
+        }
+    },
+    ["Chisel"] = {
+        ["Price"] = 300000,
+        ["Island"] = "Dino",
+        ["Chisel"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 125,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Lawn Mower"] = {
+        ["Price"] = 250000000000,
+        ["Lawn Mower"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "AutoDig",
+                ["BlockDamage"] = 6500,
+                ["AttackLength"] = 0.15,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Flaming Axe"] = {
+        ["Flaming Axe"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 7500,
+                ["DigRange"] = 50,
+                ["AttackLength"] = 0.15
+            }
+        },
+        ["Island"] = "Volcano",
+        ["Price"] = 1000000000000
+    },
+    ["Magical Message Bottle"] = {
+        ["Price"] = 400000,
+        ["Island"] = "Pirate",
+        ["Magical Message Bottle"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 150,
+                ["AttackLength"] = 0.5,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Claw Scooper"] = {
+        ["Price"] = 4000000000,
+        ["Island"] = "Candy",
+        ["Claw Scooper"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 4750,
+                ["AttackLength"] = 0.25,
+                ["DigRange"] = 50
+            }
+        }
+    },
+    ["Vacuum"] = {
+        ["Price"] = 65000,
+        ["Vacuum"] = {
+            ["Configurations"] = {
+                ["ToolType"] = "NormalDig",
+                ["BlockDamage"] = 20,
+                ["AttackLength"] = 1,
+                ["DigRange"] = 50
+            }
+        }
+    }
+}
