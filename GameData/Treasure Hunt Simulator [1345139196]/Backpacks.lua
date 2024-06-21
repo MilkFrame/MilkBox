11ffb53d08541a6b45e4d40bdf8174a5308e5076
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:50:10 +1000

    Create Backpacks.lua

---
+++ GameData/Treasure Hunt Simulator [1345139196]/Backpacks.lua
@@ -0,0 +1,227 @@
+data = {
+    ["Medium Bag"] = {
+        ["Price"] = 375,
+        ["Storage"] = 40,
+        ["Medium Bag"] = {
+            ["Part"] = {["OriginalSize"] = Vector3.new(0.6400007605552673, 0.4399995803833008, 0.7500001192092896)},
+            ["SandParticle"] = {
+                ["OriginalSize"] = Vector3.new(0.20000000298023224, 0.20000000298023224, 0.20000000298023224)
+            },
+            ["Color"] = {["OriginalSize"] = Vector3.new(0.12869548797607422, 0.3700016140937805, 0.45043668150901794)},
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}
+        }
+    },
+    ["Present"] = {["Storage"] = 2000000, ["Price"] = 20000000000},
+    ["Starterpack"] = {
+        ["Price"] = 0,
+        ["Storage"] = 10,
+        ["Starterpack"] = {
+            ["Color"] = {["OriginalSize"] = Vector3.new(1.4599990844726562, 1.549999475479126, 1.200000286102295)},
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)},
+            ["SandParticle"] = {
+                ["OriginalSize"] = Vector3.new(0.20000000298023224, 0.20000000298023224, 0.20000000298023224)
+            }
+        }
+    },
+    ["Bucket O"] = {["Price"] = 250000000, ["Island"] = "Pirate", ["Storage"] = 350000},
+    ["Rainbow Rock"] = {
+        ["Price"] = 100000000000,
+        ["Storage"] = 30000000,
+        ["Rainbow Rock"] = {
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}
+        }
+    },
+    ["Large Canister"] = {
+        ["Price"] = 8000000,
+        ["Storage"] = 25000,
+        ["Large Canister"] = {
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}
+        }
+    },
+    ["Sand Vault"] = {
+        ["Price"] = 350000,
+        ["Storage"] = 5000,
+        ["Sand Vault"] = {
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)},
+            ["SandParticle"] = {
+                ["OriginalSize"] = Vector3.new(0.20000000298023224, 0.20000000298023224, 0.20000000298023224)
+            }
+        }
+    },
+    ["Humongous Canister"] = {
+        ["Price"] = 250000000,
+        ["Storage"] = 200000,
+        ["Humongous Canister"] = {
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}
+        }
+    },
+    ["Sand Safe"] = {
+        ["Price"] = 150000,
+        ["Sand Safe"] = {
+            ["Part"] = {["OriginalSize"] = Vector3.new(1.700001835823059, 2.299999952316284, 0.899999737739563)},
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)},
+            ["Color"] = {["OriginalSize"] = Vector3.new(1.900001883506775, 2.3999998569488525, 0.7999997138977051)},
+            ["SandParticle"] = {
+                ["OriginalSize"] = Vector3.new(0.20000000298023224, 0.20000000298023224, 0.20000000298023224)
+            }
+        },
+        ["Storage"] = 2500
+    },
+    ["Ice Cream Sandwich"] = {["Price"] = 5000000, ["Island"] = "Candy", ["Storage"] = 25000},
+    ["Rubix Cube"] = {["Price"] = 50000000000, ["Island"] = "Toy Land", ["Storage"] = 10000000},
+    ["Chest"] = {
+        ["Price"] = 1000000000,
+        ["Storage"] = 500000,
+        ["Chest"] = {["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}}
+    },
+    ["Magical Fanny Pack"] = {
+        ["Price"] = 100000000,
+        ["Magical Fanny Pack"] = {
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}
+        },
+        ["Storage"] = 100000
+    },
+    ["Giant Magical Fanny Pack"] = {
+        ["Price"] = 500000000,
+        ["Storage"] = 300000,
+        ["Giant Magical Fanny Pack"] = {
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}
+        }
+    },
+    ["XL Bag"] = {
+        ["Price"] = 3150,
+        ["Storage"] = 250,
+        ["XL Bag"] = {
+            ["Color"] = {["OriginalSize"] = Vector3.new(0.6000001430511475, 0.3100021481513977, 0.37000057101249695)},
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)},
+            ["SandParticle"] = {
+                ["OriginalSize"] = Vector3.new(0.20000000298023224, 0.20000000298023224, 0.20000000298023224)
+            }
+        }
+    },
+    ["Golden Chest"] = {["Price"] = 1500000000, ["Island"] = "Pirate", ["Storage"] = 900000},
+    ["Shield"] = {["Price"] = 70000000000, ["Island"] = "Medieval", ["Storage"] = 15000000},
+    ["Candy Bucket"] = {["Price"] = 200000000, ["Island"] = "Candy", ["Storage"] = 200000},
+    ["Large Bag"] = {
+        ["Price"] = 900,
+        ["Storage"] = 140,
+        ["Large Bag"] = {
+            ["Color"] = {["OriginalSize"] = Vector3.new(0.6278274655342102, 0.37999963760375977, 0.10739259421825409)},
+            ["SandParticle"] = {
+                ["OriginalSize"] = Vector3.new(0.20000000298023224, 0.20000000298023224, 0.20000000298023224)
+            },
+            ["Part"] = {["OriginalSize"] = Vector3.new(2.2000014781951904, 1.6000001430511475, 0.6999996900558472)},
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}
+        }
+    },
+    ["XXL Bag"] = {
+        ["Price"] = 13200,
+        ["XXL Bag"] = {
+            ["Color"] = {["OriginalSize"] = Vector3.new(2.089998960494995, 0.3100021481513977, 0.43000054359436035)},
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)},
+            ["SandParticle"] = {
+                ["OriginalSize"] = Vector3.new(0.20000000298023224, 0.20000000298023224, 0.20000000298023224)
+            }
+        },
+        ["Storage"] = 480
+    },
+    ["Giant Safe"] = {["Price"] = 2500000000, ["Storage"] = 1000000},
+    ["Duffle Bag"] = {
+        ["Price"] = 12000000,
+        ["Storage"] = 35000,
+        ["Duffle Bag"] = {["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}}
+    },
+    ["Small Bag"] = {
+        ["Price"] = 150,
+        ["Storage"] = 25,
+        ["Small Bag"] = {
+            ["Color"] = {["OriginalSize"] = Vector3.new(1.5900020599365234, 2.0699987411499023, 0.6700000762939453)},
+            ["SandParticle"] = {
+                ["OriginalSize"] = Vector3.new(0.20000000298023224, 0.20000000298023224, 0.20000000298023224)
+            },
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}
+        }
+    },
+    ["Popcorn Bucket"] = {["Price"] = 5000000000, ["Storage"] = 750000},
+    ["Double Humongous Canister"] = {
+        ["Price"] = 80000000000,
+        ["Double Humongous Canister"] = {
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}
+        },
+        ["Storage"] = 20000000
+    },
+    ["Empty Crate"] = {["Price"] = 7500000, ["Island"] = "Pirate", ["Storage"] = 50000},
+    ["Giant Canister"] = {
+        ["Price"] = 48000000,
+        ["Storage"] = 75000,
+        ["Giant Canister"] = {
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}
+        }
+    },
+    ["SuperStorage™"] = {
+        ["Price"] = 36000,
+        ["Storage"] = 1000,
+        ["SuperStorage™"] = {
+            ["Part"] = {["OriginalSize"] = Vector3.new(1.920001745223999, 0.2999999225139618, 0.9599997401237488)},
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)},
+            ["Color"] = {["OriginalSize"] = Vector3.new(0.11478259414434433, 0.3300018012523651, 0.40174129605293274)},
+            ["SandParticle"] = {
+                ["OriginalSize"] = Vector3.new(0.20000000298023224, 0.20000000298023224, 0.20000000298023224)
+            }
+        }
+    },
+    ["Dual Canister"] = {
+        ["Price"] = 24000000,
+        ["Dual Canister"] = {
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}
+        },
+        ["Storage"] = 50000
+    },
+    ["Egg Preserver"] = {["Price"] = 2000000000, ["Island"] = "Dino", ["Storage"] = 1200000},
+    ["Hiking Pack"] = {["Price"] = 10000000, ["Island"] = "Dino", ["Storage"] = 100000},
+    ["Medium Canister"] = {
+        ["Price"] = 4000000,
+        ["Storage"] = 15000,
+        ["Medium Canister"] = {
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}
+        }
+    },
+    ["SuperStorage™ 3"] = {
+        ["Price"] = 700000,
+        ["SuperStorage™ 3"] = {
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}
+        },
+        ["Storage"] = 7500
+    },
+    ["Infinite"] = {
+        ["Price"] = 1000000000,
+        ["Infinite"] = {["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}},
+        ["Storage"] = 9223372036854775000
+    },
+    ["Candy Bag"] = {["Price"] = 1250000000, ["Island"] = "Candy", ["Storage"] = 700000},
+    ["Dinosaur Cage"] = {["Price"] = 330000000, ["Island"] = "Dino", ["Storage"] = 500000},
+    ["SuperStorage™ 2"] = {
+        ["Price"] = 75000,
+        ["Storage"] = 1500,
+        ["SuperStorage™ 2"] = {
+            ["Part"] = {["OriginalSize"] = Vector3.new(1.920001745223999, 0.2999999225139618, 0.9599997401237488)},
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)},
+            ["Color"] = {["OriginalSize"] = Vector3.new(0.11478259414434433, 0.3300018012523651, 0.40174129605293274)},
+            ["SandParticle"] = {
+                ["OriginalSize"] = Vector3.new(0.20000000298023224, 0.20000000298023224, 0.20000000298023224)
+            }
+        }
+    },
+    ["Small Canister"] = {
+        ["Price"] = 1500000,
+        ["Storage"] = 10000,
+        ["Small Canister"] = {
+            ["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}
+        }
+    },
+    ["Brief Case"] = {
+        ["Price"] = 750000000,
+        ["Storage"] = 400000,
+        ["Brief Case"] = {["Counter"] = {["OriginalSize"] = Vector3.new(1.2000000476837158, 0.5, 0.05000000074505806)}}
+    }
+}
