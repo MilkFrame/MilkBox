b0a68be3f02f38702716a939de315f2311a53f1d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:18:19 +1000

    Create rarities.lua

---
+++ GameData/Gym League [17450551531]/rarities.lua
@@ -0,0 +1,8 @@
+data = {
+    ["Common"] = {["order"] = 1, ["color"] = Color3.fromRGB(190, 190, 190)},
+    ["Rare"] = {["order"] = 2, ["color"] = Color3.fromRGB(93, 155, 255)},
+    ["Epic"] = {["order"] = 3, ["color"] = Color3.fromRGB(177, 99, 255)},
+    ["Legendary"] = {["order"] = 4, ["color"] = Color3.fromRGB(255, 161, 53)},
+    ["Mythical"] = {["order"] = 5, ["color"] = Color3.fromRGB(255, 47, 47)},
+    ["Exclusive"] = {["order"] = 6, ["gradient"] = "Exclusive", ["color"] = Color3.fromRGB(217, 0, 255)},
+}
