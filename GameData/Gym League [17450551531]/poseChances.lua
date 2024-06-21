67b6506dc416d5c0188470be5294e31fc7a17dbc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:19:24 +1000

    Create poseChances.lua

---
+++ GameData/Gym League [17450551531]/poseChances.lua
@@ -0,0 +1,10 @@
+data = {
+    ["rarities"] = {["Legendary"] = 1, ["Common"] = 63.9, ["Epic"] = 10, ["Mythical"] = 0.1, ["Rare"] = 25},
+    ["poses"] = {
+        ["Legendary"] = {["Front Lat Spread"] = 1, ["Front Lat And Bicep"] = 1, ["Three Quarter Back Biceps"] = 1},
+        ["Common"] = {["Arm Up"] = 1},
+        ["Epic"] = {["Aduket"] = 1},
+        ["Mythical"] = {["The Kneel"] = 1, ["Most Muscular"] = 1},
+        ["Rare"] = {["Side Chest"] = 1, ["Victory"] = 1}
+    }
+}
