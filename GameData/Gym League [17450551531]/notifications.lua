af597ff56b79bb1179df76b7ed116d79cc8a6b66
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:19:49 +1000

    Create notifications.lua

---
+++ GameData/Gym League [17450551531]/notifications.lua
@@ -0,0 +1,135 @@
+data = {
+    ["pose_equip_err"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "You don't have enough money!",
+        ["sound"] = "Error"
+    },
+    ["cot"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "You need to continue on tutorial to join a competition!",
+        ["sound"] = "Error"
+    },
+    ["nem"] = {["color"] = Color3.fromRGB(255, 50, 50), ["text"] = "Not enough money!", ["sound"] = "Error"},
+    ["nae"] = {["color"] = Color3.fromRGB(255, 50, 50), ["text"] = "You can't use this right now!", ["sound"] = "Error"},
+    ["code_format_wrong"] = {
+        ["color"] = Color3.fromRGB(255, 75, 75),
+        ["text"] = "Code format wrong!",
+        ["sound"] = "Error"
+    },
+    ["rate_2_hours"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "You can only send feedbacks every 2 hours!",
+        ["sound"] = "Error"
+    },
+    ["nep"] = {["color"] = Color3.fromRGB(255, 50, 50), ["text"] = "Not enough power!", ["sound"] = "Error"},
+    ["like_rewards"] = {
+        ["color"] = Color3.fromRGB(50, 255, 50),
+        ["text"] = "Successfully redeemed the rewards!",
+        ["sound"] = "Sucess2"
+    },
+    ["daily_award"] = {
+        ["color"] = Color3.fromRGB(50, 255, 50),
+        ["text"] = "You have received your daily reward!",
+        ["sound"] = "Success2"
+    },
+    ["cca"] = {["color"] = Color3.fromRGB(255, 50, 50), ["text"] = "Complete current body alter!", ["sound"] = "Error"},
+    ["code_not_exist_err"] = {
+        ["color"] = Color3.fromRGB(255, 75, 75),
+        ["text"] = "Code does not exist!",
+        ["sound"] = "Error"
+    },
+    ["code_success_redeem"] = {
+        ["color"] = Color3.fromRGB(0, 255, 0),
+        ["text"] = "Successfully redeemded!",
+        ["sound"] = "Sucess2"
+    },
+    ["dp"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "You need to continue on tutorial to occupy this!",
+        ["sound"] = "Error"
+    },
+    ["aura_enough_spin"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "You don't have enough spins!",
+        ["sound"] = "Error"
+    },
+    ["code_is_too_short"] = {
+        ["color"] = Color3.fromRGB(255, 75, 75),
+        ["text"] = "Code is too short!",
+        ["sound"] = "Error"
+    },
+    ["daily_already"] = {
+        ["color"] = Color3.fromRGB(50, 255, 50),
+        ["text"] = "Daily reward already claimed.",
+        ["sound"] = "Success2"
+    },
+    ["limit"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "The limit for this item is %s!",
+        ["sound"] = "Error"
+    },
+    ["powerups_not_enought_cash"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "Not enough cash to buy this powerup!",
+        ["sound"] = "Error"
+    },
+    ["code_already_redeemed"] = {
+        ["color"] = Color3.fromRGB(255, 75, 75),
+        ["text"] = "Code already redeemed!",
+        ["sound"] = "Error"
+    },
+    ["rate_error"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "An error occured while sending!",
+        ["sound"] = "Error"
+    },
+    ["aura_already"] = {
+        ["color"] = Color3.fromRGB(255, 135, 37),
+        ["text"] = "You already have this aura :(",
+        ["sound"] = "Error"
+    },
+    ["pose_spin_err"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "You don't have enough spins!",
+        ["sound"] = "Error"
+    },
+    ["code_player_data_err"] = {
+        ["color"] = Color3.fromRGB(255, 75, 75),
+        ["text"] = "Player data not found!",
+        ["sound"] = "Error"
+    },
+    ["code_expired_err"] = {["color"] = Color3.fromRGB(255, 75, 75), ["text"] = "Code expired!", ["sound"] = "Error"},
+    ["tfp"] = {["color"] = Color3.fromRGB(50, 255, 50), ["text"] = "Thanks for purchasing!", ["sound"] = "Sucess2"},
+    ["powerups_max_buyable_limit"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "You have reached the maximum limit (5) of buyable powerups!",
+        ["sound"] = "Error"
+    },
+    ["aura_enough_money"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "You don't have enough money!",
+        ["sound"] = "Error"
+    },
+    ["powerups_cannot_buyable"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "This powerup cannot be bought!",
+        ["sound"] = "Error"
+    },
+    ["pose_enough_money"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "You don't have enough money!",
+        ["sound"] = "Error"
+    },
+    ["pose_enough_spin"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "You don't have enough spins!",
+        ["sound"] = "Error"
+    },
+    ["pfy"] = {["color"] = Color3.fromRGB(50, 255, 50), ["text"] = "%s purchased for you!", ["sound"] = "Sucess2"},
+    ["uw"] = {["color"] = Color3.fromRGB(255, 50, 50), ["text"] = "Unlock next world!", ["sound"] = "Error"},
+    ["powerups_unlock_world"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "You haven't unlocked the required world to use this powerup!",
+        ["sound"] = "Error"
+    }
+}
