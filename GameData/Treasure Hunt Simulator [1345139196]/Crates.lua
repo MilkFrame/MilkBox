cde944c55d22fb269c4cb09a837315f7cddce6a0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:51:08 +1000

    Create Crates.lua

---
+++ GameData/Treasure Hunt Simulator [1345139196]/Crates.lua
@@ -0,0 +1,86 @@
+data = {
+    ["Tier2Hat"] = {
+        ["ProductID"] = 175485501,
+        ["CoinsCost"] = 250000,
+        ["CaseName"] = "Tier 2 Hat Crate",
+        ["PercentOdds"] = {["1"] = 49, ["3"] = 15, ["2"] = 30, ["5"] = 1, ["4"] = 5, ["7"] = 0, ["6"] = 0}
+    },
+    ["Tier4Hat"] = {
+        ["ProductID"] = 175486103,
+        ["PercentOdds"] = {["1"] = 0, ["3"] = 25, ["2"] = 54.5, ["5"] = 5, ["4"] = 15, ["7"] = 0, ["6"] = 0.5},
+        ["CaseName"] = "Tier 4 Hat Crate",
+        ["RubyCost"] = 250
+    },
+    ["Tier2"] = {
+        ["ProductID"] = 175485501,
+        ["CoinsCost"] = 50000,
+        ["CaseName"] = "Tier 2 Skin Crate",
+        ["PercentOdds"] = {["1"] = 49, ["3"] = 15, ["2"] = 30, ["5"] = 1, ["4"] = 5, ["7"] = 0, ["6"] = 0}
+    },
+    ["Tier1Hat"] = {
+        ["ProductID"] = 175485078,
+        ["CoinsCost"] = 5000,
+        ["CaseName"] = "Tier 1 Hat Crate",
+        ["PercentOdds"] = {["1"] = 70, ["3"] = 7, ["2"] = 20, ["5"] = 0.1, ["4"] = 2.9, ["7"] = 0, ["6"] = 0}
+    },
+    ["Tier3"] = {
+        ["ProductID"] = 175485820,
+        ["CoinsCost"] = 1000000,
+        ["CaseName"] = "Tier 3 Skin Crate",
+        ["PercentOdds"] = {["1"] = 23, ["3"] = 20, ["2"] = 45, ["5"] = 1.9, ["4"] = 10, ["7"] = 0, ["6"] = 0.1}
+    },
+    ["Tier1Halloween"] = {
+        ["CandyCost"] = 50,
+        ["ProductID"] = 175487202,
+        ["CaseName"] = "Pumpkin Hat Crate",
+        ["PercentOdds"] = {["1"] = 23, ["3"] = 20, ["2"] = 45, ["5"] = 1.9, ["4"] = 10, ["7"] = 0, ["6"] = 0.1}
+    },
+    ["Tier2Halloween"] = {
+        ["CandyCost"] = 500,
+        ["ProductID"] = 175487202,
+        ["CaseName"] = "Death Hat Crate",
+        ["PercentOdds"] = {["1"] = 0, ["3"] = 20, ["2"] = 10, ["5"] = 25, ["4"] = 40, ["7"] = 1, ["6"] = 5}
+    },
+    ["Tier5"] = {
+        ["ProductID"] = 175486921,
+        ["PercentOdds"] = {["1"] = 0, ["3"] = 36.5, ["2"] = 30, ["5"] = 7.5, ["4"] = 25, ["7"] = 0, ["6"] = 1},
+        ["CaseName"] = "Tier 5 Skin Crate",
+        ["RubyCost"] = 100
+    },
+    ["Tier6Hat"] = {
+        ["ProductID"] = 175487202,
+        ["PercentOdds"] = {["1"] = 0, ["3"] = 20, ["2"] = 10, ["5"] = 25, ["4"] = 40, ["7"] = 1, ["6"] = 5},
+        ["CaseName"] = "Sacred Hat Crate",
+        ["RubyCost"] = 1500
+    },
+    ["Tier5Hat"] = {
+        ["ProductID"] = 175486921,
+        ["PercentOdds"] = {["1"] = 0, ["3"] = 36.5, ["2"] = 30, ["5"] = 7.5, ["4"] = 25, ["7"] = 0, ["6"] = 1},
+        ["CaseName"] = "Tier 5 Hat Crate",
+        ["RubyCost"] = 500
+    },
+    ["Tier1"] = {
+        ["ProductID"] = 175485078,
+        ["CoinsCost"] = 1000,
+        ["CaseName"] = "Tier 1 Skin Crate",
+        ["PercentOdds"] = {["1"] = 70, ["3"] = 7, ["2"] = 20, ["5"] = 0.1, ["4"] = 2.9, ["7"] = 0, ["6"] = 0}
+    },
+    ["Tier3Hat"] = {
+        ["ProductID"] = 175485820,
+        ["CoinsCost"] = 5000000,
+        ["CaseName"] = "Tier 3 Hat Crate",
+        ["PercentOdds"] = {["1"] = 23, ["3"] = 20, ["2"] = 45, ["5"] = 1.9, ["4"] = 10, ["7"] = 0, ["6"] = 0.1}
+    },
+    ["Tier4"] = {
+        ["ProductID"] = 175486103,
+        ["PercentOdds"] = {["1"] = 0, ["3"] = 25, ["2"] = 54.5, ["5"] = 5, ["4"] = 15, ["7"] = 0, ["6"] = 0.5},
+        ["CaseName"] = "Tier 4 Skin Crate",
+        ["RubyCost"] = 50
+    },
+    ["Tier6"] = {
+        ["ProductID"] = 175487202,
+        ["PercentOdds"] = {["1"] = 0, ["3"] = 20, ["2"] = 10, ["5"] = 25, ["4"] = 40, ["7"] = 0, ["6"] = 5},
+        ["CaseName"] = "Sacred Skin Crate",
+        ["RubyCost"] = 300
+    }
+}
