05618be2a00670c9cf38915f7f472ca76354b9d0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:22:04 +1000

    Create logs.lua

---
+++ GameData/Gym League [17450551531]/logs.lua
@@ -0,0 +1,44 @@
+data = {
+    {
+        ["logs"] = {
+            "New GYM (Gravity GYM)",
+            "New body alters",
+            "New power ups",
+            "Now stamina drain is lowered 25% from all the equipments over 4th GYM",
+            "Reroll prices are higher in higher GYMs",
+            "Each body alters makes your muscle sizes bigger",
+            "Gifting",
+            "New gamepasses (Hyper Pump and Skip)",
+            "Bug fixes",
+            "Added new codes (Check discord for more info)"
+        },
+        ["mainUpdate"] = true,
+        ["header"] = "UPDATE 1",
+        ["thumbnail"] = "rbxassetid://17885733489"
+    },
+    {
+        ["logs"] = {
+            "Now stamina drain is lowered 5% from all the equipments",
+            "Fixed being inside with other people in podium (?)",
+            "Fixed can't open quests on topbar (?)",
+            "Disabled layered clothes at other GYMs",
+            "Added new codes (Check discord for more info)"
+        },
+        ["mainUpdate"] = false,
+        ["header"] = "v1.2 Log",
+        ["thumbnail"] = "rbxassetid://17698119228"
+    },
+    {
+        ["logs"] = {
+            [[Added an option to change your appearance for <font color="#a0ff61">'Mutated Genes'</font> and <font color="#82b4ff">'Muscle Man'</font> passes]],
+            [[Added friend boost <font color="#a3ff95">(5% per friend, max 25%)</font>]],
+            [[Added new pass <font color="#ffff2f">"Stamina Master"</font>]],
+            "Pack UI doesn't pop up on every join (only pop ups when new pack appeared)",
+            "Fixed Power Ups not coming after purchasing",
+            "Some bug fixes"
+        },
+        ["mainUpdate"] = false,
+        ["header"] = "v1.1 Log",
+        ["thumbnail"] = "rbxassetid://17786274272"
+    }
+}
