11f1bafda55c6c54746dae2511cb7b8b2b1298de
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:18:33 +1000

    Create muscles.lua

---
+++ GameData/Gym League [17450551531]/muscles.lua
@@ -0,0 +1,16 @@
+data = {
+    ["Triceps"] = {["id"] = 17462090126, ["image"] = "rbxassetid://17462090126"},
+    ["Abs"] = {["id"] = 17462471412, ["image"] = "rbxassetid://17462471412"},
+    ["Back"] = {["id"] = 17462471197, ["image"] = "rbxassetid://17462471197"},
+    ["Chest"] = {["id"] = 17462471022, ["image"] = "rbxassetid://17462471022"},
+    ["Calves"] = {["id"] = 17462748139, ["image"] = "rbxassetid://17462748139"},
+    ["Legs"] = {["id"] = 17462747943, ["image"] = "rbxassetid://17462747943"},
+    ["Stamina"] = {
+        ["id"] = 17462257192,
+        ["image"] = "rbxassetid://17462257192",
+        ["color"] = Color3.fromRGB(113, 167, 255)
+    },
+    ["Forearm"] = {["id"] = 17462765667, ["image"] = "rbxassetid://17462765667"},
+    ["Shoulder"] = {["id"] = 17462090354, ["image"] = "rbxassetid://17462090354"},
+    ["Biceps"] = {["id"] = 17462090624, ["image"] = "rbxassetid://17462090624"}
+}
