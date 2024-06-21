bd0c742034fe8556062e468c74dbcab483e18b89
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:16:53 +1000

    Create auras.lua

---
+++ GameData/Gym League [17450551531]/auras.lua
@@ -0,0 +1,166 @@
+data = {
+    ["crimsonfog"] = {
+        ["image"] = "rbxassetid://17735773607",
+        ["rarity"] = "Common",
+        ["display"] = "Crimson Fog",
+        ["multiplier"] = 1.05,
+        ["color"] = Color3.fromRGB(179, 7, 7)
+    },
+    ["galaxy"] = {
+        ["image"] = "rbxassetid://17704659000",
+        ["rarity"] = "Exclusive",
+        ["display"] = "Galaxy",
+        ["multiplier"] = 2.5,
+        ["body"] = {
+            ["glowHair"] = true,
+            ["hairColor"] = Color3.fromRGB(165, 165, 255),
+            ["color"] = Color3.fromRGB(30, 29, 56),
+            ["material"] = Enum.Material.Neon,
+            ["pantsColor"] = Color3.fromRGB(30, 29, 56),
+            ["pantsMaterial"] = Enum.Material.Neon
+        }
+    },
+    ["yellowfog"] = {
+        ["image"] = "rbxassetid://17735773607",
+        ["rarity"] = "Common",
+        ["display"] = "Yellow Fog",
+        ["multiplier"] = 1.05,
+        ["color"] = Color3.fromRGB(255, 255, 30)
+    },
+    ["curse"] = {
+        ["display"] = "Curse",
+        ["image"] = "rbxassetid://17735747030",
+        ["rarity"] = "Rare",
+        ["multiplier"] = 1.25
+    },
+    ["purple"] = {
+        ["display"] = "Purple",
+        ["image"] = "rbxassetid://17735746120",
+        ["rarity"] = "Rare",
+        ["multiplier"] = 1.15
+    },
+    ["tealfog"] = {
+        ["image"] = "rbxassetid://17735773607",
+        ["rarity"] = "Common",
+        ["display"] = "Teal Fog",
+        ["multiplier"] = 1.05,
+        ["color"] = Color3.fromRGB(94, 255, 255)
+    },
+    ["blackfog"] = {
+        ["image"] = "rbxassetid://17735773607",
+        ["rarity"] = "Common",
+        ["display"] = "Black Fog",
+        ["multiplier"] = 1.05,
+        ["color"] = Color3.fromRGB(0, 0, 0)
+    },
+    ["orange"] = {
+        ["image"] = "rbxassetid://17735213808",
+        ["rarity"] = "Common",
+        ["display"] = "Orange Aura",
+        ["multiplier"] = 1.05,
+        ["color"] = Color3.fromRGB(255, 155, 32)
+    },
+    ["red"] = {
+        ["image"] = "rbxassetid://17735213808",
+        ["rarity"] = "Common",
+        ["display"] = "Red Aura",
+        ["multiplier"] = 1.05,
+        ["color"] = Color3.fromRGB(255, 17, 17)
+    },
+    ["rage"] = {
+        ["display"] = "Rage",
+        ["image"] = "rbxassetid://17735745900",
+        ["rarity"] = "Rare",
+        ["multiplier"] = 1.15
+    },
+    ["vip"] = {
+        ["display"] = "VIP Aura",
+        ["image"] = "rbxassetid://17735747293",
+        ["rarity"] = "Exclusive",
+        ["multiplier"] = 1.5
+    },
+    ["psycho"] = {
+        ["image"] = "rbxassetid://17735746400",
+        ["rarity"] = "Mythical",
+        ["display"] = "Psycho",
+        ["multiplier"] = 2,
+        ["body"] = {["hairColor"] = Color3.fromRGB(0, 0, 0)}
+    },
+    ["ui"] = {
+        ["image"] = "rbxassetid://17735745233",
+        ["rarity"] = "Mythical",
+        ["display"] = "Ultra Instinct",
+        ["multiplier"] = 2,
+        ["body"] = {["glowHair"] = true, ["hairColor"] = Color3.fromRGB(300, 310, 322)}
+    },
+    ["ssjb"] = {
+        ["image"] = "rbxassetid://17735745448",
+        ["rarity"] = "Legendary",
+        ["display"] = "Super Saiyan Blue",
+        ["multiplier"] = 1.8,
+        ["body"] = {["glowHair"] = true, ["hairColor"] = Color3.fromRGB(98, 236, 238)}
+    },
+    ["slime"] = {
+        ["display"] = "Slime",
+        ["image"] = "rbxassetid://17735745759",
+        ["rarity"] = "Legendary",
+        ["multiplier"] = 1.8
+    },
+    ["pink"] = {
+        ["image"] = "rbxassetid://17735213808",
+        ["rarity"] = "Common",
+        ["display"] = "Pink Aura",
+        ["multiplier"] = 1.05,
+        ["color"] = Color3.fromRGB(255, 160, 217)
+    },
+    ["esper"] = {
+        ["display"] = "Esper",
+        ["image"] = "rbxassetid://17735746822",
+        ["rarity"] = "Epic",
+        ["multiplier"] = 1.4
+    },
+    ["ssjg"] = {
+        ["image"] = "rbxassetid://17735745308",
+        ["rarity"] = "Legendary",
+        ["display"] = "Super Saiyan God",
+        ["multiplier"] = 1.7,
+        ["body"] = {["hairColor"] = Color3.fromRGB(1000, 400, 200)}
+    },
+    ["default"] = {
+        ["image"] = "rbxassetid://17735213808",
+        ["rarity"] = "Common",
+        ["display"] = "Default",
+        ["gradient"] = "default",
+        ["multiplier"] = 1,
+        ["color"] = Color3.fromRGB(255, 255, 255)
+    },
+    ["green"] = {
+        ["image"] = "rbxassetid://17735213808",
+        ["rarity"] = "Common",
+        ["display"] = "Green Aura",
+        ["multiplier"] = 1.05,
+        ["color"] = Color3.fromRGB(145, 255, 19)
+    },
+    ["dark"] = {
+        ["image"] = "rbxassetid://17735746944",
+        ["rarity"] = "Epic",
+        ["display"] = "Dark",
+        ["multiplier"] = 1.5,
+        ["body"] = {["hairColor"] = Color3.fromRGB(0, 0, 0)}
+    },
+    ["ssj"] = {
+        ["image"] = "rbxassetid://17735745651",
+        ["rarity"] = "Epic",
+        ["display"] = "Super Saiyan",
+        ["multiplier"] = 1.4,
+        ["body"] = {["glowHair"] = true, ["hairColor"] = Color3.fromRGB(255, 238, 66)}
+    },
+    ["nen"] = {["display"] = "Nen", ["image"] = "rbxassetid://17735746556", ["rarity"] = "Rare", ["multiplier"] = 1.2},
+    ["fog"] = {
+        ["image"] = "rbxassetid://17735773607",
+        ["rarity"] = "Common",
+        ["display"] = "Fog",
+        ["multiplier"] = 1.05,
+        ["color"] = Color3.fromRGB(255, 255, 255)
+    }
+}
