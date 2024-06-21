314f2b5d22a6fb37c55626c1d6de7e921a2746ef
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:21:04 +1000

    Create settings.lua

---
+++ GameData/Gym League [17450551531]/settings.lua
@@ -0,0 +1,56 @@
+data = {
+    {["value"] = "Titles", ["image"] = "rbxassetid://17713735819", ["name"] = "Titles", ["type"] = "button"},
+    {
+        ["image"] = "rbxassetid://17713732527",
+        ["name"] = "Music",
+        ["value"] = "0 1 ",
+        ["default"] = 1,
+        ["step"] = 0.1,
+        ["type"] = "range"
+    },
+    {
+        ["image"] = "rbxassetid://17490255379",
+        ["name"] = "SFX",
+        ["value"] = "0 1 ",
+        ["default"] = 1,
+        ["step"] = 0.1,
+        ["type"] = "range"
+    },
+    {
+        ["options"] = {
+            {["display"] = "On", ["background"] = Color3.fromRGB(85, 255, 0)},
+            {["display"] = "Off", ["background"] = Color3.fromRGB(255, 0, 0)}
+        },
+        ["image"] = "rbxassetid://17713741030",
+        ["name"] = "Pop-up",
+        ["type"] = "boolean"
+    },
+    {
+        ["options"] = {
+            {["display"] = "Off", ["background"] = Color3.fromRGB(255, 0, 0)},
+            {["display"] = "On", ["background"] = Color3.fromRGB(85, 255, 0)}
+        },
+        ["image"] = "rbxassetid://17628725664",
+        ["name"] = "Show My Aura",
+        ["type"] = "boolean"
+    },
+    {
+        ["options"] = {
+            {["display"] = "On", ["background"] = Color3.fromRGB(85, 255, 0)},
+            {["display"] = "Off", ["background"] = Color3.fromRGB(255, 0, 0)}
+        },
+        ["image"] = "rbxassetid://17628725664",
+        ["name"] = "Show Others Aura",
+        ["type"] = "boolean"
+    },
+    {
+        ["image"] = "rbxassetid://17703349783",
+        ["name"] = "Bigger Muscles",
+        ["gamepass"] = {"MutatedGenes", "MuscleMan"},
+        ["options"] = {
+            {["display"] = "Off", ["background"] = Color3.fromRGB(255, 0, 0)},
+            {["display"] = "On", ["background"] = Color3.fromRGB(85, 255, 0)}
+        },
+        ["type"] = "boolean"
+    }
+}
