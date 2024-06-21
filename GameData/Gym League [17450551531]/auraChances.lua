9768bea98260ec1c654b2bd434c365a7cd75e3e8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:17:12 +1000

    Create auraChances.lua

---
+++ GameData/Gym League [17450551531]/auraChances.lua
@@ -0,0 +1,22 @@
+data = {
+    ["rarities"] = {["Legendary"] = 1, ["Common"] = 63.9, ["Epic"] = 10, ["Mythical"] = 0.1, ["Rare"] = 25},
+    ["auras"] = {
+        ["Legendary"] = {["ssjb"] = 1, ["ssjg"] = 1, ["slime"] = 1},
+        ["Common"] = {
+            ["crimsonfog"] = 1,
+            ["yellowfog"] = 1,
+            ["pink"] = 1,
+            ["tealfog"] = 1,
+            ["default"] = 1,
+            ["green"] = 1,
+            ["blackfog"] = 1,
+            ["fog"] = 1,
+            ["orange"] = 1,
+            ["red"] = 1
+        },
+        ["Epic"] = {["esper"] = 1, ["ssj"] = 1, ["dark"] = 1},
+        ["Mythical"] = {["ui"] = 1, ["psycho"] = 1},
+        ["Rare"] = {["rage"] = 1, ["curse"] = 1, ["nen"] = 1, ["purple"] = 1}
+    },
+    ["premiumRarities"] = {["Legendary"] = 10, ["Common"] = 0, ["Epic"] = 50, ["Mythical"] = 1, ["Rare"] = 39}
+}
