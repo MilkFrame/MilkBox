3e3669e29bece0ea06379a43b79c8e1af6365397
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-21 16:36:08 +1000

    Create Gamepasses.lua

---
+++ GameData/Tower Heroes [4646477729]/Gamepasses.lua
@@ -0,0 +1,301 @@
+data = {
+    {
+        ["Image"] = 5036807964,
+        ["Title"] = "Custom Music",
+        ["Cost"] = "100",
+        ["Owned"] = "Select Spectre in the Heroes tab and click the music icon!",
+        ["ID"] = 8875549,
+        ["Desc"] = "Play tunes while you have Spectre equipped!"
+    },
+    {
+        ["HasPass"] = "function: 0xeaff2368a5c88243",
+        ["Image"] = 5872291923,
+        ["Title"] = "Sandbox Mode",
+        ["Cost"] = "500",
+        ["Owned"] = "Create a Server and then cycle through the gamemodes! You also received 4 modifiers based on Sandbox!",
+        ["HelpMenu"] = {"SandboxHelp"},
+        ["ID"] = 12444124,
+        ["Desc"] = "Spawn any enemy and place as many heroes as you want in this 4 player server! You also receive 4 modifiers based on Sandbox!"
+    },
+    {
+        ["Image"] = 6783202341,
+        ["Title"] = "More Mods",
+        ["Cost"] = "200",
+        ["Owned"] = "You have 3 more slots to equip mods!",
+        ["HasPass"] = "function: 0xd40a6e43618ff053",
+        ["ID"] = 17642674,
+        ["Desc"] = "You can equip 3 more mods than normal!"
+    },
+    {
+        ["Image"] = 5197814616,
+        ["Title"] = "Shoulder Hero",
+        ["Cost"] = "150",
+        ["Owned"] = "In the Heroes tab, click on the smiley face to set the Hero on your shoulder!",
+        ["ID"] = 10323010,
+        ["Desc"] = "Show off your favorite Hero on your shoulder!"
+    },
+    {
+        ["Image"] = 4904030990,
+        ["Title"] = "100 Coins",
+        ["Cost"] = "80",
+        ["ID"] = 973943728,
+        ["Coins"] = 100,
+        ["Desc"] = "100 Coins"
+    },
+    {
+        ["Image"] = 4904307680,
+        ["Title"] = "250 Coins",
+        ["Cost"] = "200",
+        ["ID"] = 974021509,
+        ["Coins"] = 250,
+        ["Desc"] = "250 Coins"
+    },
+    {
+        ["Image"] = 4904307935,
+        ["Title"] = "500 Coins",
+        ["Cost"] = "400",
+        ["ID"] = 974022548,
+        ["Coins"] = 500,
+        ["Desc"] = "500 Coins"
+    },
+    {
+        ["Image"] = 5334499631,
+        ["Title"] = "1000 Coins",
+        ["Cost"] = "770",
+        ["ID"] = 1031353026,
+        ["Coins"] = 1000,
+        ["Desc"] = "1000 Coins"
+    },
+    {
+        ["Image"] = 5334500400,
+        ["Title"] = "2500 Coins",
+        ["Cost"] = "1900",
+        ["ID"] = 1031353924,
+        ["Coins"] = 2500,
+        ["Desc"] = "2500 Coins"
+    },
+    {
+        ["Image"] = 5334501246,
+        ["Title"] = "5000 Coins",
+        ["Cost"] = "3500",
+        ["ID"] = 1031354339,
+        ["Coins"] = 5000,
+        ["Desc"] = "5000 Coins"
+    },
+    {
+        ["Image"] = 5334501246,
+        ["Title"] = "New Quests",
+        ["Cost"] = "50",
+        ["ID"] = 1084032023,
+        ["RefreshQuests"] = true,
+        ["Hidden"] = true,
+        ["Desc"] = "new quests"
+    },
+    {
+        ["Image"] = 5334501246,
+        ["Title"] = "Buy Normal Advent",
+        ["Cost"] = "75",
+        ["ID"] = 1223056211,
+        ["BuyAdvent"] = true,
+        ["Hidden"] = true,
+        ["Desc"] = "normal advent"
+    },
+    {
+        ["Image"] = 5334501246,
+        ["Title"] = "Buy Special Advent",
+        ["Cost"] = "250",
+        ["ID"] = 1223056423,
+        ["BuyAdvent"] = true,
+        ["Hidden"] = true,
+        ["Desc"] = "special advent"
+    },
+    {
+        ["Image"] = 5334501246,
+        ["Title"] = "Buy Small Advent",
+        ["Cost"] = "30",
+        ["ID"] = 1223103971,
+        ["BuyAdvent"] = true,
+        ["Hidden"] = true,
+        ["Desc"] = "small advent"
+    },
+    {
+        ["Image"] = 11317790700,
+        ["Title"] = "Buy Rank Up",
+        ["Cost"] = "100",
+        ["ID"] = 1328874807,
+        ["BuyRankUp"] = true,
+        ["Hidden"] = true,
+        ["Desc"] = "battle pass"
+    },
+    {
+        ["Hidden"] = true,
+        ["Image"] = 5838577312,
+        ["Title"] = "50 Candy",
+        ["Cost"] = "60",
+        ["ID"] = 1110535048,
+        ["Coins"] = 50,
+        ["EventCurrency"] = true,
+        ["Desc"] = "50 Candy"
+    },
+    {
+        ["Hidden"] = true,
+        ["Image"] = 5838578080,
+        ["Title"] = "100 Candy",
+        ["Cost"] = "110",
+        ["ID"] = 1110535613,
+        ["Coins"] = 100,
+        ["EventCurrency"] = true,
+        ["Desc"] = "100 Candy"
+    },
+    {
+        ["Hidden"] = true,
+        ["Image"] = 5838578627,
+        ["Title"] = "500 Candy",
+        ["Cost"] = "500",
+        ["ID"] = 1110535945,
+        ["Coins"] = 500,
+        ["EventCurrency"] = true,
+        ["Desc"] = "500 Candy"
+    },
+    {
+        ["Hidden"] = true,
+        ["Image"] = 6106590022,
+        ["Title"] = "50 Cookies",
+        ["Cost"] = "60",
+        ["ID"] = 1110535048,
+        ["Coins"] = 50,
+        ["EventCurrency"] = true,
+        ["Desc"] = "50 Gingerbread"
+    },
+    {
+        ["Hidden"] = true,
+        ["Image"] = 6106590975,
+        ["Title"] = "100 Cookies",
+        ["Cost"] = "110",
+        ["ID"] = 1110535613,
+        ["Coins"] = 100,
+        ["EventCurrency"] = true,
+        ["Desc"] = "100 Gingerbread"
+    },
+    {
+        ["Hidden"] = true,
+        ["Image"] = 6106592560,
+        ["Title"] = "500 Cookies",
+        ["Cost"] = "500",
+        ["ID"] = 1110535945,
+        ["Coins"] = 500,
+        ["EventCurrency"] = true,
+        ["Desc"] = "500 Gingerbread"
+    },
+    {
+        ["Hidden"] = true,
+        ["Image"] = 6586705115,
+        ["Title"] = "50 Eggs",
+        ["Cost"] = "60",
+        ["ID"] = 1110535048,
+        ["Coins"] = 50,
+        ["EventCurrency"] = true,
+        ["Desc"] = "50 Eggs"
+    },
+    {
+        ["Hidden"] = true,
+        ["Image"] = 6586706664,
+        ["Title"] = "100 Eggs",
+        ["Cost"] = "110",
+        ["ID"] = 1110535613,
+        ["Coins"] = 100,
+        ["EventCurrency"] = true,
+        ["Desc"] = "100 Eggs"
+    },
+    {
+        ["Hidden"] = true,
+        ["Image"] = 6586708268,
+        ["Title"] = "500 Eggs",
+        ["Cost"] = "500",
+        ["ID"] = 1110535945,
+        ["Coins"] = 500,
+        ["EventCurrency"] = true,
+        ["Desc"] = "500 Eggs"
+    },
+    {
+        ["Image"] = 5197948076,
+        ["Title"] = "Bunny Hero",
+        ["Cost"] = "500",
+        ["Owned"] = "You got the Bunny and a Pink Egg Skin!",
+        ["Rewards"] = {["Troop"] = "Bunny", ["Skin"] = "4878667130"},
+        ["ID"] = 8992765,
+        ["Desc"] = "A Hero only avaliable during the Easter Event, comes with a rare Pink Egg Skin for all Heroes!"
+    },
+    {
+        ["Image"] = 5874835913,
+        ["Title"] = "Scarecrow Hero",
+        ["Cost"] = "500",
+        ["Owned"] = "You got Hayes and the Purple Hatsy Skin!",
+        ["Rewards"] = {["Troop"] = "Hayes", ["Skin"] = "hatsyHayes"},
+        ["ID"] = 12453784,
+        ["Desc"] = "A Hero only avaliable during the Halloween Event, comes with a rare Purple Hatsy Skin for Hayes!"
+    },
+    {
+        ["ID"] = 13527297,
+        ["Image"] = 6128397343,
+        ["Title"] = "Wafer Hero",
+        ["Cost"] = "500",
+        ["Owned"] = "You got Wafer and the PB&J skin!",
+        ["Rewards"] = {["Troop"] = "Wafer", ["Skin"] = "pbjWafer"},
+        ["EventCurrency"] = true,
+        ["Desc"] = "A Hero only avaliable during the Christmas Event, comes with a rare PB&J skin for Wafer!"
+    },
+    {
+        ["EventCurrency"] = true,
+        ["Image"] = 12958551983,
+        ["Title"] = "Pixel-Egg Launcher",
+        ["Cost"] = 2000,
+        ["ID"] = 159868432,
+        ["Rewards"] = {
+            ["Followers"] = {"egg_Launcher", "Wuffle", "Locker"},
+            ["Stickers"] = {13049024023},
+            ["Coins"] = 50
+        },
+        ["Hidden"] = true,
+        ["Desc"] = "Morph into Wuffle and give out the Pixel Egg to everyone!"
+    },
+    {
+        ["EventCurrency"] = true,
+        ["Image"] = 7789791237,
+        ["Title"] = "Minitreat Bundle",
+        ["Cost"] = 75,
+        ["ID"] = 24602887,
+        ["Rewards"] = {["Stickers"] = {7596292718}, ["Modifiers"] = {"stormyNight", "pumpreapBoss"}, ["Coins"] = 50},
+        ["Hidden"] = true,
+        ["Desc"] = "Includes the Roblox Pumpkin Sticker, Stormy Night Mod, and Pumpkin Reaper Mod!"
+    },
+    {
+        ["EventCurrency"] = true,
+        ["Image"] = 7789792495,
+        ["Title"] = "Bigtreat Bundle",
+        ["Cost"] = 250,
+        ["ID"] = 24603129,
+        ["Rewards"] = {["Stickers"] = {7596228906}, ["Coins"] = 100},
+        ["Hidden"] = true,
+        ["Desc"] = "Includes the Bat Boy Sticker, Pumpkin Kid Follower, and Pumpkin Reaper Follower!"
+    },
+    {
+        ["EventCurrency"] = true,
+        ["Image"] = 7789793365,
+        ["Title"] = "Megatreat Bundle",
+        ["Cost"] = 500,
+        ["ID"] = 24603137,
+        ["Rewards"] = {["Stickers"] = {7594203751}, ["Skin"] = {"7594046773", "halloweenOddport"}, ["Coins"] = 200},
+        ["Hidden"] = true,
+        ["Desc"] = "Includes the Skeleton Bloxikin Sticker, Trick or Treaters Skin, and the Global Skele-Suit Skin!"
+    },
+    {
+        ["Image"] = 16976476178,
+        ["Title"] = "April 1st Skins",
+        ["Cost"] = 1000,
+        ["ID"] = 768209442,
+        ["Rewards"] = {["Skin"] = {"gunWiz", "r6_Hero1"}, ["Coins"] = 0},
+        ["Hidden"] = true,
+        ["Desc"] = "Includes the Hero Battles and Gun Wizard Skin!"
+    }
+}
