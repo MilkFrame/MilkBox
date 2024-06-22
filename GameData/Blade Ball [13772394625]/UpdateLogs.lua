453cc64c76ddc33d265136af25920d5c4eeabe7c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-22 22:42:10 +1000

    Update UpdateLogs.lua

--- GameData/Blade Ball [13772394625]/UpdateLogs.lua
+++ GameData/Blade Ball [13772394625]/UpdateLogs.lua
@@ -1,29 +1,128 @@
 data = {
     {
-        ["Subtext"] = [[<font color="rgb(255, 0, 0)">(Limited time only)</font>]],
-        ["Title"] = "New Year's Season!",
-        ["ItemIcons"] = {"rbxassetid://16125258147", "rbxassetid://16136299818", "rbxassetid://16136287074"},
-        ["Date"] = "1706374800000",
-        ["Description"] = [[		👀 Brand New Blade Ball update is out now!\n		🔑 Gold Machine!\n		⭐ New Years Battlepass!\n		⌛ Time Hole Ability!\n		🎁 Futuristic Hourly Rewards\n		❓ Secret Swords\n		🛡️ Clan additions\n		🌟 Clan Roles\n		📋 Clan Manager\n		🤑 Sci-Fi Spin\n		👀 Invite Friend Reward\n		💥 No Ability Ranked Random Reward\n		]],
-        ["Thumbnail"] = "rbxassetid://15937853529",
-        ["SubDescription"] = "New Year's Season Pass & new ability: Time Hole! New Secret rarity swords & explosions in crates, new Gold Machine, Futuristic Spins, & clan features!"
+        ["Subtext"] = [[<font color="rgb(255, 0, 0)">LIMITED TIME ONLY</font>]],
+        ["Title"] = "👼 Rebirth Limited Time Mode!",
+        ["ItemIcons"] = {"rbxassetid://17864515991", "rbxassetid://17858882844", "rbxassetid://17862232505"},
+        ["Date"] = "1718470800000",
+        ["Description"] = [[		👥 Mythical DUO Tournament!\n		🇧🇷 Brazil Event\n		🪤 Bladetrap\n		💫 New Spin Wheel Rewards\n		🔎 Server Selector\n		🔑 GLOBAL 2x Luck (AFK WORLD)\n		😇 Resurrection Pack\n		🏝️ 1 New Map\n		❓ 6 Secrets\n		🗡️ 30 Swords\n		💥 13 Explosions\n		🕺 5 Emotes\n		🐛 Bug Fixes\n        ]],
+        ["Thumbnail"] = "rbxassetid://17864860477",
+        ["SubDescription"] = "Rebirth Gamemode, go to server selector to play now!"
     },
     {
-        ["Subtext"] = [[<font color="rgb(255, 0, 0)"></font>]],
-        ["Title"] = "Infection Mode & Banana Event!",
-        ["ItemIcons"] = {},
-        ["Date"] = "1705770000000",
-        ["Description"] = [[		• Infection limited time mode!\n		• New ability: Absolute Confidence!\n		• Banana Event - earn Banana-themed items!\n		• New live event phase!\n		• Explosion favoriting!\n		• Random shuffle!\n		]],
-        ["Thumbnail"] = "",
-        ["SubDescription"] = ""
+        ["Subtext"] = [[<font color="rgb(255, 0, 0)">LIMITED TIME ONLY</font>]],
+        ["Title"] = "🐲 Dragon LTM!",
+        ["ItemIcons"] = {"rbxassetid://17773288207", "rbxassetid://17777099660", "rbxassetid://17773288435"},
+        ["Date"] = "1717866000000",
+        ["Description"] = [[		🐲 Dragon LTM\n		🔥🧊 Fire & Ice Spins\n		🚫 No Ability Gamemode\n		🔑 GLOBAL 2x Luck (AFK WORLD)\n		⚡ Abyssal Pack\n		❓ 1 Secret & 1 RNG Emote\n		🗡️ 23 Swords\n		💥 9 Explosions\n		🕺 12 Emotes\n		🐛 Bug Fixes\n        ]],
+        ["Thumbnail"] = "rbxassetid://0",
+        ["SubDescription"] = "Dragon LTM!"
     },
     {
-        ["Subtext"] = [[<font color="rgb(255, 0, 0)"></font>]],
-        ["Title"] = "The Sentinel returns...",
-        ["ItemIcons"] = {},
-        ["Date"] = "1705165200000",
-        ["Description"] = [[		• New live event!\n		• FREE hourly spin rewards!\n		• New ability: Martyrdom!\n		• Ranked No Ability game mode!\n		• Clan settings & Clan management!\n		• New spin wheel sword: Gilded Glory!\n		]],
+        ["Subtext"] = [[<font color="rgb(255, 0, 0)">LIMITED TIME ONLY</font>]],
+        ["Title"] = "🧙‍🧹 Wizards VS Witches!",
+        ["ItemIcons"] = {"rbxassetid://17683139090", "rbxassetid://17594836798", "rbxassetid://17693752806"},
+        ["Date"] = "1717261200000",
+        ["Description"] = [[		👑 Royal Merchant\n		🕵️ NEW Bounty Ability\n		💪 Ability Training Quests\n		🔑 GLOBAL 2x Luck (AFK WORLD)\n		🌙 Eclipse Pack\n		📝 New Update Rewards\n		❓ 1 Secret & 1 RNG Emote\n		🗡️ 23 Swords\n		💥 13 Explosions\n		🕺 12 Emotes\n		🐛 Bug Fixes\n        ]],
+        ["Thumbnail"] = "rbxassetid://17676181458",
+        ["SubDescription"] = "F2P Ability Quests!"
+    },
+    {
+        ["Subtext"] = [[<font color="rgb(255, 0, 0)">LIMITED TIME ONLY</font>]],
+        ["Title"] = "✨ Wheel Rewards!",
+        ["ItemIcons"] = {"rbxassetid://17602196227", "rbxassetid://17606707767", "rbxassetid://17605886112"},
+        ["Date"] = "1716656400000",
+        ["Description"] = [[		🤩 New F2P Wheel Rewards \n		🔑 GLOBAL 2x Luck (AFK WORLD)\n		🌈 Chroma Pack (LIMITED STOCK)\n		🟣 Runic Pack\n		❓ 2 RNG Emotes\n		🗡️ 10 Swords\n		💥 5 Explosions\n		🕺 6 Emotes\n		🐛 Bug Fixes\n        ]],
+        ["Thumbnail"] = "rbxassetid://17604388908",
+        ["SubDescription"] = "New F2P Wheel Rewards!"
+    },
+    {
+        ["Subtext"] = [[<font color="rgb(255, 0, 0)">LIMITED TIME ONLY</font>]],
+        ["Title"] = "⌛ ROBLOX Classic Event!",
+        ["ItemIcons"] = {"rbxassetid://17447273269", "rbxassetid://17524787969", "rbxassetid://17452957112"},
+        ["Date"] = "1716483600000",
+        ["Description"] = [[		👥 Encrypted Clone Ability \n		🕵️ 1x1x1x1 Boss Fight! \n		🔑 Hacker Crate\n		🌀 Ability Spin!\n		🛒 Shop\n		🗓️ Daily Rewards\n		🗡️ 10 Swords\n		💥 6 Explosions\n		🕺 8 Emotes\n		🐛 Bug Fixes\n        ]],
+        ["Thumbnail"] = "rbxassetid://17488279385",
+        ["SubDescription"] = "ROBLOX Classic Event is here!"
+    },
+    {
+        ["Subtext"] = [[<font color="rgb(255, 0, 0)">Choose an ability to spin for!</font>]],
+        ["Title"] = "✨ Mythical Battlepass!",
+        ["ItemIcons"] = {"rbxassetid://17528545536", "rbxassetid://17527245332", "rbxassetid://17534029490"},
+        ["Date"] = "1716051600000",
+        ["Description"] = [[		🐲 Mythical Battlepass \n		✨ Spin for Death Slash, Dribble, Time Hole, & Singularity!\n		🧙TRIO Mage's Rampage\n		❄️ Hourly Yeti Spin\n		🥶 Frigid Pack\n		🗡️ 44 Swords\n		💥 19 Explosions\n		🕺 21 Emotes\n		📲 Mobile UI Fixes\n		🐛 Bug Fixes\n        ]],
         ["Thumbnail"] = "",
-        ["SubDescription"] = ""
+        ["SubDescription"] = "New Mythical Battlepass!"
+    },
+    {
+        ["Subtext"] = [[<font color="rgb(255, 0, 0)">Dungeon's Crafting!</font>]],
+        ["Title"] = "🛠️ Dungeon Crafting!",
+        ["ItemIcons"] = {"rbxassetid://17441024563", "rbxassetid://17451958188", "rbxassetid://17451902634"},
+        ["Date"] = "1715446800000",
+        ["Description"] = [[		🛠️ Dungeon Crafting\n		🚨 New Coins Ability\n		🍀 2x Luck in AFK World & Battlepass Spins\n		⚔️ 3v3 & No Ability Duels\n		🏝️ 1 New Map\n		🗡️ 18 Swords\n		❓ 3 Secrets\n		💥 4 Explosions\n		🕺 5 Emotes\n		🐛 Bug Fixes\n        ]],
+        ["Thumbnail"] = "rbxassetid://17452479204",
+        ["SubDescription"] = "2x Luck Ends in 48h!"
+    },
+    {
+        ["Subtext"] = [[<font color="rgb(255, 0, 0)">GOOD VS EVIL EVENT!</font>]],
+        ["Title"] = "😇😈 Good vs Evil Event",
+        ["ItemIcons"] = {"rbxassetid://17370472635", "rbxassetid://17378422011", "rbxassetid://17375979282"},
+        ["Date"] = "1714842000000",
+        ["Description"] = [[		💯 F2P Katana Spins\n		🐲 Dungeons QoL\n		🔥 4 Dungeon Abilities Added \n		📅 Daily Login\n		🏝️ 1 New Map\n		🗡️ 14 Swords\n		❓ 1 Secret\n		💥 2 Explosions\n		🕺 3 Emotes\n		🐛 Bug Fixes\n        ]],
+        ["Thumbnail"] = "rbxassetid://17371631481",
+        ["SubDescription"] = "New Unique Triple Katanas!"
+    },
+    {
+        ["Subtext"] = [[<font color="rgb(255, 0, 0)">DUNGEONS!</font>]],
+        ["Title"] = "🐲 Dungeons!",
+        ["ItemIcons"] = {"rbxassetid://17299532193", "rbxassetid://17300652097", "rbxassetid://17299459102"},
+        ["Date"] = "1714237200000",
+        ["Description"] = [[		🐉 New Dungeon's Mode\n		🍀 NEW ABILITY: Lucky\n		🤖 Cyber Crate (Coins!)\n		👋 Welcome Back Rewards\n		⚔️ New Ranked Season\n		🗡️ 40 swords\n		💥 14 Explosions\n		🕺 12 Emotes\n		🐛 Bug Fixes\n        ]],
+        ["Thumbnail"] = "rbxassetid://15522190869",
+        ["SubDescription"] = "Check out our new Dungeon's Mode!"
+    },
+    {
+        ["Subtext"] = [[<font color="rgb(255, 0, 0)">LIMITED TIME</font>]],
+        ["Title"] = "🐸 Frog Event (& Frog Sword)!",
+        ["ItemIcons"] = {"rbxassetid://17220457823", "rbxassetid://17219391627", "rbxassetid://17218833703"},
+        ["Date"] = "1713632400000",
+        ["Description"] = [[		🐸 Frog Sword!\n		🛡️ New Clan Battlepass\n		✨ Try Limited Pack Items\n		😴 Try AFK World Emotes\n		🛡️ Transfer Clan Ownership\n		🗡️ 6 swords\n		💥 2 Explosions\n		🕺 5 Emotes\n		🔴 New AFK Emotes\n		🐛 Bug Fixes\n        ]],
+        ["Thumbnail"] = "rbxassetid://17220631914",
+        ["SubDescription"] = "Check out the new Clan Battlepass!"
+    },
+    {
+        ["Subtext"] = [[<font color="rgb(255, 0, 0)">LIMITED TIME</font>]],
+        ["Title"] = "New Battlepass!",
+        ["ItemIcons"] = {"rbxassetid://17123745084", "rbxassetid://17124812285", "rbxassetid://17110342188"},
+        ["Date"] = "1713027600000",
+        ["Description"] = [[		👼😈 Good vs Evil Battlepass!\n		⚔️ DUO Reaper Event\n		✨ TWO New Abilities\n		😴 AFK World Improvement\n		🔔 Notification Rewards\n		🛡️ Clan Improvements\n		🔢 Limited Quantity Swords\n		🗡️ 36 swords\n		💥 16 Explosions\n		🕺 21 Emotes\n		❓ 1 Secret\n		🏝️ 1 New Map (ANCIENT WAYPOINT)\n		🐛 Bug Fixes\n        ]],
+        ["Thumbnail"] = "rbxassetid://17128058166",
+        ["SubDescription"] = "Check out the new Battlepass!"
+    },
+    {
+        ["Subtext"] = [[<font color="rgb(255, 0, 0)">LIMITED TIME</font>]],
+        ["Title"] = "New Limited Time Mode!",
+        ["ItemIcons"] = {"rbxassetid://17039412238", "rbxassetid://17027200630", "rbxassetid://17027844396"},
+        ["Date"] = "1712422800000",
+        ["Description"] = [[		⚔️ 50 Player LTM!\n		✨ Lobby Training Practice\n		📦 Merchant\n		🌍 New Elemental Character (NEBULA)\n		⚔️ Training Mode Abilities FREE\n		😈 Demonic Pack\n		💰 Coin Ability Trials\n		😴 AFK World with Friends\n		🗡️ 19 swords\n		💥 12 Explosions\n		🕺 12 Emotes\n		❓ 1 Secret\n		🐛 Bug Fixes\n        ]],
+        ["Thumbnail"] = "rbxassetid://17038301015",
+        ["SubDescription"] = "Play our new Battle Royale LTM!"
+    },
+    {
+        ["Subtext"] = [[<font color="rgb(255, 0, 0)">LIMITED TIME</font>]],
+        ["Title"] = "Easter Event",
+        ["ItemIcons"] = {"rbxassetid://16931929247", "rbxassetid://16930088541", "rbxassetid://16934291121"},
+        ["Date"] = "1711818000000",
+        ["Description"] = [[		🔥 F2P Limited Time Easter Event!\n		✨ Egg Hunt\n		🐰 NEW F2P Ability\n		💫 Easter Wheel\n		⚔️ Training Mode Changes\n		📦 Easter Bundle\n		👀 Emotes Owned QoL\n		🌸  Blossom Pack\n		🗡️ 22 swords\n		💥 7 Explosions\n		🕺 11 Emotes\n		❓ 1 Secret\n		🐛 Bug Fixes\n        ]],
+        ["Thumbnail"] = "rbxassetid://16937685102",
+        ["SubDescription"] = "Check out our one of kind Easter Bunny sword!"
+    },
+    {
+        ["Subtext"] = [[<font color="rgb(255, 0, 0)">Live Event</font>]],
+        ["Title"] = "Galactic Ogre - Boss Fight",
+        ["ItemIcons"] = {"rbxassetid://16844075159", "rbxassetid://16846177246", "rbxassetid://16827044346"},
+        ["Date"] = "1711213200000",
+        ["Description"] = [[		🔥 Godzilla x Kong Event!\n		🪐 Zero Gravity LTM\n		🗡️ Live Event\n		✍️ Training Mode Update\n		👀 New F2P Ability: Qi-Charge\n		🏃‍♂️ Emote while parrying\n		😴  AFK World QoL\n		✨ Stellar Pack\n		🗡️ 21 Swords\n		💥 8 Explosions\n		🕺 10 Emotes\n		❓ 2 Secrets\n		🐛 Bug Fixes\n        ]],
+        ["Thumbnail"] = "rbxassetid://16846836086",
+        ["SubDescription"] = "Join the live event every 30 minutes to end the Galactic Ogre's wrath! Check out the Godzilla x Kong event and complete all the quests to earn Kong's Axe."
     }
 }
