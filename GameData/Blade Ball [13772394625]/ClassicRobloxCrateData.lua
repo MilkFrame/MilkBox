e6c591a38fc36dd4d03a26379a003167c6481ca6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-22 22:40:13 +1000

    Create ClassicRobloxCrateData.lua

---
+++ GameData/Blade Ball [13772394625]/ClassicRobloxCrateData.lua
@@ -0,0 +1,85 @@
+data = {
+    ["DailyLoginStreaks"] = {
+        {["Streak"] = 1, ["Tickets"] = 1},
+        {["Streak"] = 3, ["Tickets"] = 3},
+        {["Streak"] = 5, ["Tickets"] = 5},
+        {["Streak"] = 7, ["Tickets"] = 7}
+    },
+    ["TimeLength"] = "1717200000000",
+    ["SpinTable"] = "function: 0xeb13b559c18e1350",
+    ["RewardPool"] = {
+        {
+            ["Reward"] = {
+                ["Value"] = "Redcliff Claymore",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17527765794",
+                ["DisplayName"] = "Redcliff Claymore"
+            },
+            ["Probability"] = 30
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Gothic Squire’s Sword",
+                ["Type"] = "Sword",
+                ["DisplayName"] = "Gothic Squire’s Sword"
+            },
+            ["Probability"] = 15
+        },
+        {
+            ["Reward"] = {["Value"] = "Wind’s Breath", ["Type"] = "Sword", ["DisplayName"] = "Wind’s Breath"},
+            ["Probability"] = 5
+        },
+        {
+            ["Reward"] = {["Value"] = "Firebrand’s Fury", ["Type"] = "Sword", ["DisplayName"] = "Firebrand’s Fury"},
+            ["Probability"] = 1
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Block Breaker",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://17452957112",
+                ["DisplayName"] = "Block Breaker Explosion"
+            },
+            ["Probability"] = 25
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Glitch Bomb",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://17452996145",
+                ["DisplayName"] = "Glitch Bomb Explosion"
+            },
+            ["Probability"] = 15
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Emote317",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://17488230114",
+                ["DisplayName"] = "Back in Time Emote"
+            },
+            ["Probability"] = 6
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Emote318",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://17488238034",
+                ["DisplayName"] = "Go, Kitty, Go Emote"
+            },
+            ["Probability"] = 2.9
+        },
+        {
+            ["Reward"] = {["Value"] = "Periastron’s Glory", ["Type"] = "Sword", ["DisplayName"] = "Periastron’s Glory"},
+            ["Replacement"] = {
+                ["Reward"] = {
+                    ["Value"] = "RIP",
+                    ["Type"] = "Explosion",
+                    ["Icon"] = "rbxassetid://16497865249",
+                    ["DisplayName"] = "RIP Explosion"
+                }
+            },
+            ["Probability"] = 0.05
+        }
+    }
+}
