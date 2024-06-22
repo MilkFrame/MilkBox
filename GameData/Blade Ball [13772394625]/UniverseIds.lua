892b049775556552b62797a1af13bd67ee428604
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-22 22:39:54 +1000

    Update UniverseIds.lua

--- GameData/Blade Ball [13772394625]/UniverseIds.lua
+++ GameData/Blade Ball [13772394625]/UniverseIds.lua
@@ -1,27 +1,158 @@
 data = {
-    ["Ranked"] = {
-        ["PlaceId"] = 14915220621,
+    ["LTM"] = {
+        ["Accessible"] = "function: 0xdafc9ded3ab4a210",
+        ["PlaceId"] = 15552588346,
         ["RejectionStatus"] = "Failed to teleport!",
-        ["Accessible"] = "function: 0x0000000062b29a5e"
+        ["CanInvite"] = true
+    },
+    ["Tutorial"] = {
+        ["CanInvite"] = false,
+        ["RequiredPlayers"] = 1,
+        ["PlaceId"] = 16044264830,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["Accessible"] = "function: 0x55aa8d800ec05f90"
     },
     ["Pro"] = {
+        ["Accessible"] = "function: 0xc3c998a3d785f770",
         ["PlaceId"] = 14732610803,
         ["RejectionStatus"] = "You need at least 50 Wins!",
-        ["Accessible"] = "function: 0x00000000ce9711ce"
+        ["CanInvite"] = true
     },
-    ["Default"] = {
-        ["PlaceId"] = 13772394625,
+    ["BossFight"] = {
+        ["Accessible"] = "function: 0xbd2b27602ffe3730",
+        ["PlaceId"] = 15185247558,
         ["RejectionStatus"] = "Failed to teleport!",
-        ["Accessible"] = "function: 0x0000000062b29a5e"
+        ["CanInvite"] = false
     },
     ["Voice"] = {
+        ["Accessible"] = "function: 0xcb00202e6b4d8d00",
         ["PlaceId"] = 15131065025,
         ["RejectionStatus"] = "You do not have Voice Chat (13+) enabled!",
-        ["Accessible"] = "function: 0x00000000962c9aa6"
+        ["CanInvite"] = true
+    },
+    ["Duel"] = {
+        ["Accessible"] = "function: 0xb9175bc0532c4ed0",
+        ["PlaceId"] = 15144787112,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["CanInvite"] = "function: 0x05e0fec04ebced50"
+    },
+    ["TradingPlaza"] = {
+        ["Accessible"] = "function: 0x55aa8d800ec05f90",
+        ["PlaceId"] = 0,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["CanInvite"] = false
+    },
+    ["TournamentMatches"] = {
+        ["Accessible"] = "function: 0x55aa8d800ec05f90",
+        ["PlaceId"] = 16331596518,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["CanInvite"] = false
+    },
+    ["RBBattles"] = {
+        ["Accessible"] = "function: 0xce7672e029764670",
+        ["PlaceId"] = 0,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["CanInvite"] = false
     },
     ["AFK"] = {
+        ["Accessible"] = "function: 0xce7672e029764670",
         ["PlaceId"] = 14368557094,
         ["RejectionStatus"] = "Failed to teleport!",
-        ["Accessible"] = "function: 0x0000000062b29a5e"
+        ["CanInvite"] = true
+    },
+    ["DungeonsMatch"] = {
+        ["Accessible"] = "function: 0x55aa8d800ec05f90",
+        ["PlaceId"] = 16456370330,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["CanInvite"] = false
+    },
+    ["Ranked"] = {
+        ["CanInvite"] = true,
+        ["AssociatedPlaces"] = {"RankedMatches", "Ranked"},
+        ["PlaceId"] = 14915220621,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["Accessible"] = "function: 0xce7672e029764670"
+    },
+    ["TournamentDuo"] = {
+        ["Accessible"] = "function: 0x55aa8d800ec05f90",
+        ["PlaceId"] = 17757592456,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["CanInvite"] = false
+    },
+    ["ClanWar"] = {
+        ["Accessible"] = "function: 0x55aa8d800ec05f90",
+        ["PlaceId"] = 16331598816,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["CanInvite"] = false
+    },
+    ["MobileServers"] = {
+        ["Accessible"] = "function: 0x55aa8d800ec05f90",
+        ["PlaceId"] = 15509350986,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["CanInvite"] = true
+    },
+    ["TournamentLobby"] = {
+        ["Accessible"] = "function: 0xce7672e029764670",
+        ["PlaceId"] = 16331595046,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["CanInvite"] = true
+    },
+    ["RankedNoAbility"] = {
+        ["CanInvite"] = true,
+        ["AssociatedPlaces"] = {"RankedMatchesNoAbility", "RankedNoAbility"},
+        ["PlaceId"] = 15582821022,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["Accessible"] = "function: 0xce7672e029764670"
+    },
+    ["DungeonsLobby"] = {
+        ["Accessible"] = "function: 0xce7672e029764670",
+        ["PlaceId"] = 16581648071,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["CanInvite"] = true
+    },
+    ["NewPlayerLobbies"] = {
+        ["Accessible"] = "function: 0x55aa8d800ec05f90",
+        ["PlaceId"] = 16281300371,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["CanInvite"] = false
+    },
+    ["Default"] = {
+        ["Accessible"] = "function: 0xce7672e029764670",
+        ["PlaceId"] = 13772394625,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["CanInvite"] = true
+    },
+    ["IntermediatePlayerLobbies"] = {
+        ["Accessible"] = "function: 0x55aa8d800ec05f90",
+        ["PlaceId"] = 16331600459,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["CanInvite"] = false
+    },
+    ["Elemental"] = {
+        ["Accessible"] = "function: 0x6dedc5e0a986e5f0",
+        ["PlaceId"] = 15517169103,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["CanInvite"] = true
+    },
+    ["MultiplayerTraining"] = {
+        ["CanInvite"] = true,
+        ["RequiredPlayers"] = 1,
+        ["PlaceId"] = 15234596844,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["Accessible"] = "function: 0xce7672e029764670"
+    },
+    ["RankedMatchesNoAbility"] = {
+        ["CanInvite"] = false,
+        ["AssociatedPlaces"] = {"RankedMatchesNoAbility", "RankedNoAbility"},
+        ["PlaceId"] = 15582823307,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["Accessible"] = "function: 0xce7672e029764670"
+    },
+    ["RankedMatches"] = {
+        ["CanInvite"] = false,
+        ["AssociatedPlaces"] = {"RankedMatches", "Ranked"},
+        ["PlaceId"] = 15264892126,
+        ["RejectionStatus"] = "Failed to teleport!",
+        ["Accessible"] = "function: 0xce7672e029764670"
     }
 }
