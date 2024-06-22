f88b8c1299af12cdd6281419fb0485c4168d7d7b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-22 22:34:07 +1000

    Update GachaItemsData.lua

--- GameData/Blade Ball [13772394625]/GachaItemsData.lua
+++ GameData/Blade Ball [13772394625]/GachaItemsData.lua
@@ -1,126 +1,170 @@
 data = {
     ["BottomRewards"] = {
         {
-            ["ItemName"] = "Magic Token",
-            ["Rarity"] = "Blue",
-            ["Amount"] = 1,
-            ["Icon"] = "rbxassetid://16130315578",
-            ["AwardFunctionArguments"] = {},
-            ["AwardFunctionName"] = "nil",
-            ["Probability"] = 3
-        },
-        {
-            ["ItemName"] = "New Year Explosion Crate",
+            ["ItemName"] = " 300 Coins",
             ["Rarity"] = "Green",
             ["Amount"] = 1,
-            ["Icon"] = "rbxassetid://16130315794",
-            ["AwardFunctionArguments"] = {"NewYearExplosion", 1},
-            ["AwardFunctionName"] = "AddCrateKeys",
-            ["Probability"] = 10
-        },
-        {
-            ["ItemName"] = "Premium Explosion Skin Crate",
-            ["Rarity"] = "Green",
-            ["Amount"] = 1,
-            ["Icon"] = "rbxassetid://15049564874",
-            ["AwardFunctionArguments"] = {"PremiumExplosion", 1},
-            ["AwardFunctionName"] = "AddCrateKeys",
-            ["Probability"] = 23
+            ["Icon"] = "rbxassetid://15013634268",
+            ["AwardFunctionArguments"] = {300, true},
+            ["AwardFunctionName"] = "AddCredits",
+            ["Probability"] = 30
         },
         {
             ["ItemName"] = "Premium Weapon Skin Crate",
             ["Rarity"] = "Green",
             ["Amount"] = 1,
-            ["Icon"] = "rbxassetid://15049562836",
+            ["Icon"] = "rbxassetid://16039967646",
             ["AwardFunctionArguments"] = {"PremiumSword", 1},
             ["AwardFunctionName"] = "AddCrateKeys",
             ["Probability"] = 28
         },
         {
-            ["ItemName"] = " 300 Coins",
+            ["ItemName"] = "Magic Token",
+            ["Rarity"] = "Blue",
+            ["Amount"] = 1,
+            ["Icon"] = "rbxassetid://17127313069",
+            ["AwardFunctionArguments"] = {},
+            ["AwardFunctionName"] = "nil",
+            ["Probability"] = 3
+        },
+        {
+            ["ItemName"] = "Premium Explosion Skin Crate",
             ["Rarity"] = "Green",
             ["Amount"] = 1,
-            ["Icon"] = "http://www.roblox.com/asset/?id=15013634268",
-            ["AwardFunctionArguments"] = {300},
-            ["AwardFunctionName"] = "AddCredits",
-            ["Probability"] = 30
+            ["Icon"] = "rbxassetid://15049303003",
+            ["AwardFunctionArguments"] = {"PremiumExplosion", 1},
+            ["AwardFunctionName"] = "AddCrateKeys",
+            ["Probability"] = 23
+        },
+        {
+            ["ItemName"] = "Facepalm",
+            ["Rarity"] = "Green",
+            ["Amount"] = 1,
+            ["Icon"] = "rbxassetid://17515583106",
+            ["AwardFunctionArguments"] = {"Emote325", true},
+            ["AwardFunctionName"] = "AddEmote",
+            ["Probability"] = 10
         }
     },
-    ["MagicTokenImage"] = "rbxassetid://16125257882",
-    ["SpinTable"] = "function: 0x00000000f659e313",
-    ["SecondItemsToCombine"] = {"New Year's Axe", "New Year's Slasher", "NewYearInfinityToken"},
-    ["UpgradedCombinedItemIcon"] = "rbxassetid://16125258147",
+    ["MagicTokenImage"] = "rbxassetid://17127295866",
+    ["CheckIfPlayersOwnsBestPrizeAbility"] = "function: 0x673182eb6ee65f40",
+    ["GachaTokenName"] = "EternalInfinityToken",
+    ["AbilitiesRewards"] = {
+        ["Singularity"] = {
+            ["AbilityTokenIcon"] = "rbxassetid://17528477826",
+            ["UpgradeTokenIcon"] = "rbxassetid://17529427702",
+            ["ChestIcon"] = "rbxassetid://16685498782",
+            ["Requirements"] = {"Galactic Emperor", "Galactic Harbinger", "SingularityToken"},
+            ["DisplayName"] = "Singularity"
+        },
+        ["Dribble"] = {
+            ["AbilityTokenIcon"] = "rbxassetid://17516032792",
+            ["UpgradeTokenIcon"] = "rbxassetid://17523603847",
+            ["ChestIcon"] = "rbxassetid://15730586130",
+            ["Requirements"] = {"Christmas Blade", "Winged Blade", "DribbleToken"},
+            ["DisplayName"] = "Dribble"
+        },
+        ["DeathSlash"] = {
+            ["AbilityTokenIcon"] = "rbxassetid://17516032502",
+            ["UpgradeTokenIcon"] = "rbxassetid://17523625496",
+            ["ChestIcon"] = "rbxassetid://15456915547",
+            ["Requirements"] = {"Ice Dagger", "Glacial Scythe", "DeathSlashToken"},
+            ["DisplayName"] = "Death Slash"
+        },
+        ["TimeHole"] = {
+            ["AbilityTokenIcon"] = "rbxassetid://17516033085",
+            ["UpgradeTokenIcon"] = "rbxassetid://17523602041",
+            ["ChestIcon"] = "rbxassetid://16130315578",
+            ["Requirements"] = {"New Year's Axe", "New Year's Slasher", "TimeHoleToken"},
+            ["DisplayName"] = "Time Hole"
+        }
+    },
+    ["GetAbilityToken"] = "function: 0xc24860ab0c835270",
+    ["ItemsToCombine"] = {"Griffon's Clasp", "Phoenix's Edge", "Infinity"},
+    ["Probabilities"] = {},
+    ["SpinTable"] = "function: 0xee469491361090b0",
     ["TopRewards"] = {
         {
-            ["AwardFunctionName"] = "Level2",
-            ["ItemName"] = "Time Hole",
-            ["Icon"] = "rbxassetid://16125258147",
-            ["Probability"] = 0
-        },
-        {
-            ["ItemName"] = "Time Hole",
-            ["AwardFunctionName"] = "AddAbility",
-            ["First"] = true,
-            ["Icon"] = "rbxassetid://16125258147",
-            ["Probability"] = 0
-        },
-        {
-            ["AwardFunctionName"] = "Token",
-            ["ItemName"] = "Magic Token",
-            ["Icon"] = "rbxassetid://16125257882",
-            ["Probability"] = 5
-        },
-        {
-            ["ItemName"] = "Infinity",
+            ["GetCustomReward"] = "function: 0xb60183e521fb7950",
+            ["ItemName"] = "???",
             ["Rarity"] = "Yellow",
             ["Amount"] = 1,
-            ["Icon"] = "rbxassetid://14852723022",
-            ["AwardFunctionArguments"] = {"Infinity", false},
-            ["AwardFunctionName"] = "AddAbility",
-            ["Probability"] = 5
-        },
-        {
-            ["ItemName"] = "New Year's Slasher",
-            ["Rarity"] = "Yellow",
-            ["Amount"] = 1,
-            ["Icon"] = "rbxassetid://16136299818",
-            ["AwardFunctionArguments"] = {"New Year's Slasher"},
-            ["AwardFunctionName"] = "AddSwordSkin",
+            ["Icon"] = "rbxasset://textures/ui/GuiImagePlaceholder.png",
+            ["AwardFunctionArguments"] = {},
+            ["AwardFunctionName"] = "nil",
             ["Probability"] = 19
         },
         {
-            ["ItemName"] = "Hand Stand",
+            ["GetCustomReward"] = "function: 0x2ff0c7474db4c0b0",
+            ["ItemName"] = "???",
             ["Rarity"] = "Yellow",
             ["Amount"] = 1,
-            ["Icon"] = "rbxassetid://16123062388",
-            ["AwardFunctionArguments"] = {"Emote116"},
+            ["Icon"] = "rbxasset://textures/ui/GuiImagePlaceholder.png",
+            ["AwardFunctionArguments"] = {},
+            ["AwardFunctionName"] = "nil",
+            ["Probability"] = 42
+        },
+        {
+            ["ItemName"] = "Phoenix Flight",
+            ["Rarity"] = "Yellow",
+            ["Amount"] = 1,
+            ["Icon"] = "rbxassetid://17515578486",
+            ["AwardFunctionArguments"] = {"Emote326", true},
             ["AwardFunctionName"] = "AddEmote",
             ["Probability"] = 34
         },
         {
-            ["ItemName"] = "New Year's Axe",
+            ["ItemName"] = "Ability Token",
             ["Rarity"] = "Yellow",
             ["Amount"] = 1,
-            ["Icon"] = "rbxassetid://16136333847",
-            ["AwardFunctionArguments"] = {"New Year's Axe"},
-            ["AwardFunctionName"] = "AddSwordSkin",
-            ["Probability"] = 42
+            ["Icon"] = "rbxassetid://14852723022",
+            ["AwardFunctionArguments"] = {},
+            ["AwardFunctionName"] = "nil",
+            ["Probability"] = 5
         }
     },
-    ["GachaTokenName"] = "NewYearInfinityToken",
-    ["GachaTokenDisplayName"] = "New Year's Token",
-    ["RewardCombinedItem"] = "function: 0x0000000084f8f12b",
-    ["MagicTokenData"] = {
-        ["ItemName"] = "NewYearInfinityToken",
-        ["Rarity"] = "Yellow",
-        ["Amount"] = 1,
-        ["Icon"] = "rbxassetid://16125257882",
-        ["Probability"] = 5
+    ["RewardCombinedItem"] = "function: 0xbfed7206199ea450",
+    ["GetAbilityRewardData"] = "function: 0x6fa0c049caaca660",
+    ["GachaTokens"] = {
+        ["Singularity"] = {
+            ["ItemName"] = "SingularityToken",
+            ["Rarity"] = "Yellow",
+            ["Amount"] = 1,
+            ["DisplayName"] = "Singularity Token",
+            ["UpgradeIcon"] = "rbxassetid://17529427702",
+            ["Icon"] = "rbxassetid://17528477826",
+            ["Probability"] = 5
+        },
+        ["DeathSlash"] = {
+            ["ItemName"] = "DeathSlashToken",
+            ["Rarity"] = "Yellow",
+            ["Amount"] = 1,
+            ["DisplayName"] = "Death Slash Token",
+            ["UpgradeIcon"] = "rbxassetid://17523625496",
+            ["Icon"] = "rbxassetid://17516032502",
+            ["Probability"] = 5
+        },
+        ["Dribble"] = {
+            ["ItemName"] = "DribbleToken",
+            ["Rarity"] = "Yellow",
+            ["Amount"] = 1,
+            ["DisplayName"] = "Dribble Token",
+            ["UpgradeIcon"] = "rbxassetid://17523603847",
+            ["Icon"] = "rbxassetid://17516032792",
+            ["Probability"] = 5
+        },
+        ["TimeHole"] = {
+            ["ItemName"] = "TimeHoleToken",
+            ["Rarity"] = "Yellow",
+            ["Amount"] = 1,
+            ["DisplayName"] = "Time Hole Token",
+            ["UpgradeIcon"] = "rbxassetid://17523602041",
+            ["Icon"] = "rbxassetid://17516033085",
+            ["Probability"] = 5
+        }
     },
-    ["CheckIfPlayerOwnsPhantom"] = "function: 0x00000000f97640a3",
-    ["ItemsToCombine"] = {"New Year's Axe", "New Year's Slasher", "Infinity"},
-    ["CheckIfPlayerOwnsOriginalInfinity"] = "function: 0x00000000f7ec50fb",
-    ["CombinedItemId"] = "Time Hole",
-    ["Probabilities"] = {},
-    ["CombinedItemIcon"] = "rbxassetid://16125258147"
+    ["CheckIfPlayerOwnsOriginalInfinity"] = "function: 0x97012211066af810",
+    ["CombinedItemId"] = "Slash of Duality",
+    ["UpgradedCombinedItemIcon"] = "rbxassetid://17128058166",
+    ["CombinedItemIcon"] = "rbxassetid://17128015725"
 }
