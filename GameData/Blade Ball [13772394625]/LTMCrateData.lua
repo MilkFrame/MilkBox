397ca39f8ac52ca1bd8cd0cac57143d110c93ee2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-22 22:48:43 +1000

    Create LTMCrateData.lua

---
+++ GameData/Blade Ball [13772394625]/LTMCrateData.lua
@@ -0,0 +1,113 @@
+data = {
+    ["DailyLoginStreaks"] = {
+        {["Streak"] = 1, ["Tickets"] = 1},
+        {["Streak"] = 2, ["Tickets"] = 2},
+        {["Streak"] = 3, ["Tickets"] = 3},
+        {["Streak"] = 4, ["Tickets"] = 4}
+    },
+    ["RewardPool"] = {
+        {
+            ["Reward"] = {
+                ["Value"] = "Traitor's Blade",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17673694225",
+                ["DisplayName"] = "Traitor's Blade"
+            },
+            ["ReplionPath"] = {"SwordSkins", "Unlocked", "Traitor's Blade"},
+            ["Probability"] = 30
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Scale Blade",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17673694502",
+                ["DisplayName"] = "Scale Blade"
+            },
+            ["ReplionPath"] = {"SwordSkins", "Unlocked", "Scale Blade"},
+            ["Probability"] = 15
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Abyssal Fang",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17673696562",
+                ["DisplayName"] = "Abyssal Fang"
+            },
+            ["ReplionPath"] = {"SwordSkins", "Unlocked", "Abyssal Fang"},
+            ["Probability"] = 5
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Ember's Rage",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17673695517",
+                ["DisplayName"] = "Ember's Rage"
+            },
+            ["ReplionPath"] = {"SwordSkins", "Unlocked", "Ember's Rage"},
+            ["Probability"] = 1
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Molten Eruption",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://17673329963",
+                ["DisplayName"] = "Molten Eruption Explosion"
+            },
+            ["ReplionPath"] = {"ExplosionSkins", "Unlocked", "Molten Eruption"},
+            ["Probability"] = 25
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Infernal Blast",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://17693758193",
+                ["DisplayName"] = "Infernal Blast Explosion"
+            },
+            ["ReplionPath"] = {"ExplosionSkins", "Unlocked", "Infernal Blast"},
+            ["Probability"] = 15
+        },
+        {
+            ["ReplionPathFindValue"] = "Emote359",
+            ["Reward"] = {
+                ["Value"] = "Emote359",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://17673543973",
+                ["DisplayName"] = "Uncovered Emote"
+            },
+            ["ReplionPath"] = {"Emotes", "Unlocked"},
+            ["Probability"] = 6
+        },
+        {
+            ["ReplionPathFindValue"] = "Emote360",
+            ["Reward"] = {
+                ["Value"] = "Emote360",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://17673584301",
+                ["DisplayName"] = "Dragon Beast Emote"
+            },
+            ["ReplionPath"] = {"Emotes", "Unlocked"},
+            ["Probability"] = 2.9
+        },
+        {
+            ["Replacement"] = {
+                ["ReplionPath"] = {"ExplosionSkins", "Unlocked", "Ferocitus’ Awakening"},
+                ["Reward"] = {
+                    ["Value"] = "Ferocitus’ Awakening",
+                    ["Type"] = "Explosion",
+                    ["Icon"] = "rbxassetid://17673252317",
+                    ["DisplayName"] = "Ferocitus’ Awakening Explosion"
+                }
+            },
+            ["Reward"] = {
+                ["Value"] = "Bane of Ferocity",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17673696086",
+                ["DisplayName"] = "Bane of Ferocity"
+            },
+            ["ReplionPath"] = {"SwordSkins", "Unlocked", "Bane of Ferocity"},
+            ["Probability"] = 0.05
+        }
+    },
+    ["SpinTable"] = "function: 0x69d85bfab7d71070",
+    ["TimeLength"] = "1719072000000"
+}
