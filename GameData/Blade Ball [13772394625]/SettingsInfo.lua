75c43384bb6699bf13c71f6de24aae1147e8163e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-22 22:33:48 +1000

    Update SettingsInfo.lua

--- GameData/Blade Ball [13772394625]/SettingsInfo.lua
+++ GameData/Blade Ball [13772394625]/SettingsInfo.lua
@@ -1,32 +1,65 @@
 data = {
     ["Misc"] = {
         ["LayoutOrder"] = 8,
-        ["AbilityButtonPosition"] = {
-            ["LayoutOrder"] = 19,
-            ["Current"] = {["Y"] = 0.51, ["X"] = 0.9},
-            ["Default"] = {["Y"] = 0.51, ["X"] = 0.9},
+        ["Server Version"] = {["LayoutOrder"] = 22},
+        ["Ability1TabletButtonPosition"] = {
+            ["LayoutOrder"] = 17,
+            ["Current"] = {["Y"] = 0.87, ["X"] = 0.718},
+            ["Default"] = {["Y"] = 0.87, ["X"] = 0.718},
             ["DoNotDisplay"] = true
         },
-        ["BlockButtonScale"] = {["LayoutOrder"] = 18, ["Current"] = 1, ["Default"] = 1, ["DoNotDisplay"] = true},
-        ["VR Parry Sensitivity"] = {["Max"] = 100, ["Current"] = 50, ["Default"] = 50, ["LayoutOrder"] = 15},
-        ["Duel Requests"] = {["Enabled"] = true, ["LayoutOrder"] = 10},
-        ["SwordSkinsRandomizer"] = {
-            ["LayoutOrder"] = 23,
+        ["Ability2TabletButtonScale"] = {
+            ["LayoutOrder"] = 20,
+            ["Current"] = 1,
+            ["Default"] = 1,
+            ["DoNotDisplay"] = true
+        },
+        ["Ability3MobileButtonPosition"] = {
+            ["LayoutOrder"] = 17,
+            ["Current"] = {["Y"] = 0.651, ["X"] = 0.859},
+            ["Default"] = {["Y"] = 0.651, ["X"] = 0.859},
+            ["DoNotDisplay"] = true
+        },
+        ["Ability2MobileButtonPosition"] = {
+            ["LayoutOrder"] = 17,
+            ["Current"] = {["Y"] = 0.72, ["X"] = 0.778},
+            ["Default"] = {["Y"] = 0.72, ["X"] = 0.778},
+            ["DoNotDisplay"] = true
+        },
+        ["Ability1MobileButtonPosition"] = {
+            ["LayoutOrder"] = 17,
+            ["Current"] = {["Y"] = 0.879, ["X"] = 0.778},
+            ["Default"] = {["Y"] = 0.879, ["X"] = 0.778},
+            ["DoNotDisplay"] = true
+        },
+        ["Weapon VFX"] = {["Enabled"] = true, ["LayoutOrder"] = 9},
+        ["AbilitiesRandomizer"] = {
+            ["LayoutOrder"] = 25,
             ["Current"] = false,
             ["Default"] = false,
             ["UseFavorites"] = false,
             ["DoNotDisplay"] = true
         },
-        ["Low Graphics"] = {["Enabled"] = false, ["LayoutOrder"] = 9},
-        ["Tap Screen To Block"] = {["Enabled"] = true, ["LayoutOrder"] = 14},
-        ["Server Region"] = {["LayoutOrder"] = 21},
+        ["Night Mode"] = {["Enabled"] = false, ["LayoutOrder"] = 9},
+        ["Ability2TabletButtonPosition"] = {
+            ["LayoutOrder"] = 17,
+            ["Current"] = {["Y"] = 0.739, ["X"] = 0.718},
+            ["Default"] = {["Y"] = 0.739, ["X"] = 0.718},
+            ["DoNotDisplay"] = true
+        },
+        ["Ability4MobileButtonScale"] = {
+            ["LayoutOrder"] = 20,
+            ["Current"] = 1,
+            ["Default"] = 1,
+            ["DoNotDisplay"] = true
+        },
+        ["Highlight Players"] = {["Enabled"] = false, ["LayoutOrder"] = 9},
         ["BlockButtonPosition"] = {
             ["LayoutOrder"] = 17,
             ["Current"] = {["Y"] = 0.3, ["X"] = 0.9},
             ["Default"] = {["Y"] = 0.3, ["X"] = 0.9},
             ["DoNotDisplay"] = true
         },
-        ["VR Hand Switch Sensitivity"] = {["Max"] = 100, ["Current"] = 50, ["Default"] = 50, ["LayoutOrder"] = 16},
         ["Country Flag"] = {["Enabled"] = true, ["LayoutOrder"] = 12, ["DoNotDisplay"] = true},
         ["ExplosionSkinsRandomizer"] = {
             ["LayoutOrder"] = 24,
@@ -35,22 +68,104 @@ data = {
             ["UseFavorites"] = false,
             ["DoNotDisplay"] = true
         },
-        ["AbilitiesRandomizer"] = {
-            ["LayoutOrder"] = 25,
+        ["Ability2MobileButtonScale"] = {
+            ["LayoutOrder"] = 20,
+            ["Current"] = 1,
+            ["Default"] = 1,
+            ["DoNotDisplay"] = true
+        },
+        ["Ability1TabletButtonScale"] = {
+            ["LayoutOrder"] = 20,
+            ["Current"] = 1,
+            ["Default"] = 1,
+            ["DoNotDisplay"] = true
+        },
+        ["AbilityButtonPosition"] = {
+            ["LayoutOrder"] = 19,
+            ["Current"] = {["Y"] = 0.51, ["X"] = 0.9},
+            ["Default"] = {["Y"] = 0.51, ["X"] = 0.9},
+            ["DoNotDisplay"] = true
+        },
+        ["Ability4MobileButtonPosition"] = {
+            ["LayoutOrder"] = 17,
+            ["Current"] = {["Y"] = 0.651, ["X"] = 0.935},
+            ["Default"] = {["Y"] = 0.651, ["X"] = 0.935},
+            ["DoNotDisplay"] = true
+        },
+        ["BlockButtonScale"] = {["LayoutOrder"] = 18, ["Current"] = 1, ["Default"] = 1, ["DoNotDisplay"] = true},
+        ["VR Parry Sensitivity"] = {["Max"] = 100, ["Current"] = 50, ["Default"] = 50, ["LayoutOrder"] = 15},
+        ["Duel Requests"] = {["Enabled"] = true, ["LayoutOrder"] = 10},
+        ["Ability3TabletButtonScale"] = {
+            ["LayoutOrder"] = 20,
+            ["Current"] = 1,
+            ["Default"] = 1,
+            ["DoNotDisplay"] = true
+        },
+        ["Low Graphics"] = {["Enabled"] = false, ["LayoutOrder"] = 9},
+        ["Tap Screen To Block"] = {["Enabled"] = true, ["LayoutOrder"] = 14},
+        ["Ability4TabletButtonScale"] = {
+            ["LayoutOrder"] = 20,
+            ["Current"] = 1,
+            ["Default"] = 1,
+            ["DoNotDisplay"] = true
+        },
+        ["SwordSkinsRandomizer"] = {
+            ["LayoutOrder"] = 23,
             ["Current"] = false,
             ["Default"] = false,
             ["UseFavorites"] = false,
             ["DoNotDisplay"] = true
         },
-        ["Hide UI During Match"] = {["Enabled"] = false, ["LayoutOrder"] = 11},
+        ["Server Region"] = {["LayoutOrder"] = 21},
         ["AbilityButtonScale"] = {["LayoutOrder"] = 20, ["Current"] = 1, ["Default"] = 1, ["DoNotDisplay"] = true},
-        ["Server Version"] = {["LayoutOrder"] = 22}
+        ["VR Hand Switch Sensitivity"] = {["Max"] = 100, ["Current"] = 50, ["Default"] = 50, ["LayoutOrder"] = 16},
+        ["Ability3MobileButtonScale"] = {
+            ["LayoutOrder"] = 20,
+            ["Current"] = 1,
+            ["Default"] = 1,
+            ["DoNotDisplay"] = true
+        },
+        ["Ability4TabletButtonPosition"] = {
+            ["LayoutOrder"] = 17,
+            ["Current"] = {["Y"] = 0.641, ["X"] = 0.903},
+            ["Default"] = {["Y"] = 0.641, ["X"] = 0.903},
+            ["DoNotDisplay"] = true
+        },
+        ["Shift Lock on Match"] = {["Enabled"] = true, ["LayoutOrder"] = 11},
+        ["Hide UI During Match"] = {["Enabled"] = false, ["LayoutOrder"] = 11},
+        ["Ability1MobileButtonScale"] = {
+            ["LayoutOrder"] = 20,
+            ["Current"] = 1,
+            ["Default"] = 1,
+            ["DoNotDisplay"] = true
+        },
+        ["Ability3TabletButtonPosition"] = {
+            ["LayoutOrder"] = 17,
+            ["Current"] = {["Y"] = 0.641, ["X"] = 0.815},
+            ["Default"] = {["Y"] = 0.641, ["X"] = 0.815},
+            ["DoNotDisplay"] = true
+        }
     },
     ["Keybinds"] = {
+        ["LayoutOrder"] = 4,
+        ["Emote"] = {
+            ["LayoutOrder"] = 8,
+            ["PC"] = {["Bind2"] = "", ["Bind1"] = "R"},
+            ["Default"] = {["Console"] = "ButtonL1", ["PC"] = "R"},
+            ["Console"] = {["Bind2"] = "", ["Bind1"] = "ButtonL1"}
+        },
+        ["Ability1"] = {
+            ["LayoutOrder"] = 7,
+            ["PC"] = {["Bind2"] = "", ["Bind1"] = ""},
+            ["Default"] = {["Console"] = "ButtonL2", ["PC"] = "Q"},
+            ["Elemental"] = true,
+            ["Console"] = {["Bind2"] = "", ["Bind1"] = ""}
+        },
         ["Ability"] = {
             ["LayoutOrder"] = 6,
             ["PC"] = {["Bind2"] = "", ["Bind1"] = ""},
             ["Default"] = {["Console"] = "ButtonX", ["PC"] = "Q"},
+            ["Elemental"] = false,
             ["Console"] = {["Bind2"] = "", ["Bind1"] = ""}
         },
         ["Block"] = {
@@ -59,13 +174,20 @@ data = {
             ["Default"] = {["Console"] = "ButtonR1", ["PC"] = "F"},
             ["Console"] = {["Bind2"] = "", ["Bind1"] = ""}
         },
-        ["Emote"] = {
+        ["Ability2"] = {
             ["LayoutOrder"] = 7,
-            ["PC"] = {["Bind2"] = "", ["Bind1"] = "R"},
-            ["Default"] = {["Console"] = "ButtonL1", ["PC"] = "R"},
-            ["Console"] = {["Bind2"] = "", ["Bind1"] = "ButtonL1"}
+            ["PC"] = {["Bind2"] = "", ["Bind1"] = ""},
+            ["Default"] = {["Console"] = "ButtonR2", ["PC"] = "E"},
+            ["Elemental"] = true,
+            ["Console"] = {["Bind2"] = "", ["Bind1"] = ""}
         },
-        ["LayoutOrder"] = 4
+        ["Ability3"] = {
+            ["LayoutOrder"] = 7,
+            ["PC"] = {["Bind2"] = "", ["Bind1"] = ""},
+            ["Default"] = {["Console"] = "DPadDown", ["PC"] = "G"},
+            ["Elemental"] = true,
+            ["Console"] = {["Bind2"] = "", ["Bind1"] = ""}
+        }
     },
     ["Volume"] = {
         ["LayoutOrder"] = 1,
