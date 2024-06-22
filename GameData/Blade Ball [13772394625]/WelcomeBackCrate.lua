b642494c421021353c75a3fd4d1aa9c5226d650e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-22 22:49:22 +1000

    Create WelcomeBackCrate.lua

---
+++ GameData/Blade Ball [13772394625]/WelcomeBackCrate.lua
@@ -0,0 +1,78 @@
+data = {
+    ["Rewards"] = {
+        {
+            ["Chance"] = 25,
+            ["Reward"] = {
+                ["Value"] = "Rekindled Gladius",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17301853449",
+                ["DisplayName"] = "Rekindled Gladius"
+            }
+        },
+        {
+            ["Chance"] = 20,
+            ["Reward"] = {
+                ["Value"] = "Emote285",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://17299327961",
+                ["DisplayName"] = "It Is What It Is Emote"
+            }
+        },
+        {
+            ["Chance"] = 16,
+            ["Reward"] = {
+                ["Value"] = "Emote284",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://17299332141",
+                ["DisplayName"] = "Judging Emote"
+            }
+        },
+        {
+            ["Chance"] = 15,
+            ["Reward"] = {
+                ["Value"] = "Blade of Exile",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17301851928",
+                ["DisplayName"] = "Blade of Exile"
+            }
+        },
+        {
+            ["Chance"] = 10,
+            ["Reward"] = {
+                ["Value"] = "The Pendulum",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17301854221",
+                ["DisplayName"] = "The Pendulum"
+            }
+        },
+        {
+            ["Chance"] = 10,
+            ["Reward"] = {
+                ["Value"] = "Reverse Implosion",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://17299375048",
+                ["DisplayName"] = "Reverse Implosion Explosion"
+            }
+        },
+        {
+            ["Chance"] = 3,
+            ["Reward"] = {
+                ["Value"] = "Phantom Vortex",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://17299390388",
+                ["DisplayName"] = "Phantom Vortex Explosion"
+            }
+        },
+        {
+            ["Chance"] = 1,
+            ["Reward"] = {
+                ["Value"] = "The Covenant",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17301854020",
+                ["DisplayName"] = "The Covenant"
+            }
+        }
+    },
+    ["SpinTable"] = "function: 0x95e8374051e701b0",
+    ["Cost"] = 150
+}
