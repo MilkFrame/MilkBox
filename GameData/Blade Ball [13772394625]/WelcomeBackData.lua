fcdebb738f3fa332343c6e75930e002c4fecfc75
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-22 22:49:06 +1000

    Create WelcomeBackData.lua

---
+++ GameData/Blade Ball [13772394625]/WelcomeBackData.lua
@@ -0,0 +1,187 @@
+data = {
+    ["GetSelectionCrateRewards"] = "function: 0x656fd6d31bf29710",
+    ["WelcomeBackReturnCoinShop"] = {
+        {
+            ["Reward"] = {
+                ["Value"] = "Lament's Blade",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17301853029",
+                ["DisplayName"] = "Lament's Blade"
+            },
+            ["ReplionPath"] = {"SwordSkins", "Unlocked", "Lament's Blade"},
+            ["Cost"] = 200
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Whispering Katana",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17301942435",
+                ["DisplayName"] = "Whispering Katana"
+            },
+            ["ReplionPath"] = {"SwordSkins", "Unlocked", "Whispering Katana"},
+            ["Cost"] = 500
+        },
+        {
+            ["ReplionPathFindValue"] = "Emote289",
+            ["Reward"] = {
+                ["Value"] = "Emote289",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://17300984034",
+                ["DisplayName"] = "Heartfelt Dance Emote"
+            },
+            ["ReplionPath"] = {"Emotes", "Unlocked"},
+            ["Cost"] = 125
+        },
+        {
+            ["ReplionPathFindValue"] = "Emote286",
+            ["Reward"] = {
+                ["Value"] = "Emote286",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://17299243448",
+                ["DisplayName"] = "Shuffle Dance Emote"
+            },
+            ["ReplionPath"] = {"Emotes", "Unlocked"},
+            ["Cost"] = 350
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Corrupted Duality",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://17299458888",
+                ["DisplayName"] = "Corrupted Duality Explosion"
+            },
+            ["ReplionPath"] = {"ExplosionSkins", "Unlocked", "Corrupted Duality"},
+            ["Cost"] = 100
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Aurora's Blast",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://17299459102",
+                ["DisplayName"] = "Aurora's Blast Explosion"
+            },
+            ["ReplionPath"] = {"ExplosionSkins", "Unlocked", "Aurora's Blast"},
+            ["Cost"] = 300
+        }
+    },
+    ["MilestoneFinalRewards"] = {
+        {
+            ["Value"] = "Frigidus' Starforge",
+            ["Type"] = "Sword",
+            ["Icon"] = "rbxassetid://17301852500",
+            ["DisplayName"] = "Frigidus' Starforge"
+        },
+        {
+            ["Value"] = "Hyper Starforge",
+            ["Type"] = "Sword",
+            ["Icon"] = "rbxassetid://17301852222",
+            ["DisplayName"] = "Hyper Starforge"
+        },
+        {
+            ["Value"] = "Inferno's Starforge",
+            ["Type"] = "Sword",
+            ["Icon"] = "rbxassetid://17301852757",
+            ["DisplayName"] = "Inferno's Starforge"
+        }
+    },
+    ["MaxMilestoneXP"] = 250,
+    ["WelcomeBackMilestones"] = {
+        {
+            ["XP"] = 50,
+            ["Reward"] = {
+                ["Value"] = 150,
+                ["Type"] = "ReturnCoins",
+                ["Icon"] = "rbxassetid://17286763390",
+                ["DisplayName"] = "150 Return Coins"
+            }
+        },
+        {
+            ["XP"] = 100,
+            ["Reward"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 1,
+                ["Value"] = "PremiumSword",
+                ["Icon"] = "rbxassetid://16039967646",
+                ["DisplayName"] = "Premium Sword Crate"
+            }
+        },
+        {
+            ["XP"] = 150,
+            ["Reward"] = {
+                ["Value"] = 350,
+                ["Type"] = "ReturnCoins",
+                ["Icon"] = "rbxassetid://17286763390",
+                ["DisplayName"] = "350 Return Coins"
+            }
+        },
+        {
+            ["XP"] = 200,
+            ["Reward"] = {
+                ["Value"] = "Shadow Saber",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17301853712",
+                ["DisplayName"] = "Shadow Saber"
+            }
+        },
+        {
+            ["XP"] = 250,
+            ["Reward"] = {
+                ["Value"] = "Frigidus' Starforge",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17301852500",
+                ["DisplayName"] = "Frigidus' Starforge"
+            }
+        }
+    },
+    ["WelcomeBackDailyRewards"] = {
+        {
+            ["Reward"] = {
+                ["Value"] = "Persistence Blade",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17300302206",
+                ["DisplayName"] = "Persistence Blade"
+            }
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = 250,
+                ["Type"] = "ReturnCoins",
+                ["Icon"] = "rbxassetid://17286763390",
+                ["DisplayName"] = "250 Return Coins"
+            }
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Emote288",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://17299164637",
+                ["DisplayName"] = "Go Crazy Emote"
+            }
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Quantum Fracture",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://17299184903",
+                ["DisplayName"] = "Quantum Fracture Explosion"
+            }
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = 500,
+                ["Type"] = "ReturnCoins",
+                ["Icon"] = "rbxassetid://17286763390",
+                ["DisplayName"] = "500 Return Coins"
+            }
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Wind Slicer",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17300301516",
+                ["DisplayName"] = "Wind Slicer"
+            }
+        },
+        {["Reward"] = "SWORD_SELECTION_CRATE"}
+    }
+}
