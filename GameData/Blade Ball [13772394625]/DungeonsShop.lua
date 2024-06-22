503d68b60cdb580828cf0f00e226c792fb1dc479
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-22 22:49:51 +1000

    Create DungeonsShop.lua

---
+++ GameData/Blade Ball [13772394625]/DungeonsShop.lua
@@ -0,0 +1,107 @@
+data = {
+    ["Emotes"] = {
+        {
+            ["Reward"] = {
+                ["Value"] = "Emote277",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://17196391897",
+                ["DisplayName"] = "Sarcastic Clapping Emote"
+            },
+            ["Cost"] = 250
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Emote295",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://17300946550",
+                ["DisplayName"] = "Sleeper Build Emote"
+            },
+            ["Cost"] = 500
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Emote294",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://17300954529",
+                ["DisplayName"] = "Pyromaniac Emote"
+            },
+            ["Cost"] = 1000
+        }
+    },
+    ["Explosions"] = {
+        {
+            ["Reward"] = {
+                ["Value"] = "Cannon Fodder",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://17182956966",
+                ["DisplayName"] = "Cannon Fodder Explosion"
+            },
+            ["Cost"] = 500
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Ancient Curse",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://17182957140",
+                ["DisplayName"] = "Ancient Curse Explosion"
+            },
+            ["Cost"] = 750
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Hazardous Omen",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://17182957481",
+                ["DisplayName"] = "Hazardous Omen Explosion"
+            },
+            ["Cost"] = 1500
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Sentinel's Fury",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://17182957643",
+                ["DisplayName"] = "Sentinel's Fury Explosion"
+            },
+            ["Cost"] = 5000
+        }
+    },
+    ["Swords"] = {
+        {
+            ["Reward"] = {
+                ["Value"] = "Voidheart Piercer",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17196732122",
+                ["DisplayName"] = "Voidheart Piercer"
+            },
+            ["Cost"] = 500
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Griffin's Claw",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17196970831",
+                ["DisplayName"] = "Griffin's Claw"
+            },
+            ["Cost"] = 1000
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Cerberus",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17196734158",
+                ["DisplayName"] = "Cerberus"
+            },
+            ["Cost"] = 2500
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Serpent's Wraithblade",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17196990934",
+                ["DisplayName"] = "Serpent's Wraithblade"
+            },
+            ["Cost"] = 10000
+        }
+    }
+}
