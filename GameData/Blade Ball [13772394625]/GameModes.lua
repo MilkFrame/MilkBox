af732e966dabb846d793a24b2fcb98bdb075ac51
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-22 22:38:24 +1000

    Update GameModes.lua

--- GameData/Blade Ball [13772394625]/GameModes.lua
+++ GameData/Blade Ball [13772394625]/GameModes.lua
@@ -1,10 +1,14 @@
 data = {
-    ["Chances"] = {{["FFA"] = 100}, {["Duo"] = 0, ["2Teams"] = 100}, {["Randomizer"] = 0, ["4Teams"] = 100}},
+    ["Chances"] = {{["FFA"] = 100}, {["NoAbilityFFA"] = 0, ["2Teams"] = 100}, {["Randomizer"] = 0, ["4Teams"] = 100}},
     ["Modes"] = {
-        ["Duo"] = {["DisabledInTraining"] = true, ["DisplayName"] = "Duos"},
-        ["Randomizer"] = {["DisplayName"] = "Randomizer"},
+        ["Randomizer"] = {["DisabledInTraining"] = true, ["DisplayName"] = "Randomizer"},
+        ["RobloxClassic"] = {["DisabledInTraining"] = true, ["DisplayName"] = "1x1x1x1"},
         ["FFA"] = {["DisplayName"] = "Classic"},
+        ["BonusLifeFFA"] = {["DisabledInTraining"] = true, ["DisplayName"] = "2 Lives"},
+        ["Dragon"] = {["DisabledInTraining"] = true, ["DisplayName"] = "Dragon"},
+        ["NoAbilityFFA"] = {["DisabledInTraining"] = true, ["DisplayName"] = "No Abilities"},
         ["2Teams"] = {["DisplayName"] = "2 Teams"},
+        ["Duo"] = {["DisabledInTraining"] = true, ["DisplayName"] = "Duos"},
         ["4Teams"] = {["DisplayName"] = "4 Teams"}
     }
 }
