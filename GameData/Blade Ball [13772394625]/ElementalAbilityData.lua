dc890d921947e04c69543d19e0003bbea5db393a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-22 22:41:50 +1000

    Create ElementalAbilityData.lua

---
+++ GameData/Blade Ball [13772394625]/ElementalAbilityData.lua
@@ -0,0 +1,429 @@
+data = {
+    ["GetAbilityData"] = "function: 0x845d5713f7626430",
+    ["_elementalAbilityData"] = {
+        ["Water"] = {
+            ["Water"] = {
+                ["Type"] = "PASSIVE",
+                ["Description"] = "Increase movement speed by 2.5% per second while moving (up to 50%), user is slowed down when they parry the ball",
+                ["Order"] = 4,
+                ["Level"] = 50,
+                ["Icon"] = "rbxassetid://16180205851",
+                ["Passive"] = true
+            },
+            ["Geyser"] = {
+                ["Type"] = "MOVEMENT",
+                ["Description"] = "Use a geyser to jump high into the air.",
+                ["Order"] = 1,
+                ["Level"] = 0,
+                ["Icon"] = "http://www.roblox.com/asset/?id=16169200059"
+            },
+            ["Tidal Wave"] = {
+                ["Type"] = "DAMAGE",
+                ["Description"] = "Ride a wave of water, pushing enemies away upon impact",
+                ["Order"] = 3,
+                ["Level"] = 20,
+                ["Icon"] = "http://www.roblox.com/asset/?id=16169199837"
+            },
+            ["Shark Bite"] = {
+                ["Type"] = "ENVIRONMENT",
+                ["Description"] = "Summon a shark to attack other players",
+                ["Order"] = 2,
+                ["Level"] = 10,
+                ["Icon"] = "http://www.roblox.com/asset/?id=16169200312"
+            }
+        },
+        ["Electric"] = {
+            ["Bolt"] = {
+                ["Type"] = "",
+                ["Description"] = "Teleport forward at the speed of light.",
+                ["Order"] = 1,
+                ["Level"] = 0,
+                ["Icon"] = "rbxassetid://16197075529"
+            },
+            ["Static Shock"] = {
+                ["Type"] = "PASSIVE",
+                ["Description"] = "Unleash a lightning aura that slowly damages nearby players",
+                ["Order"] = 4,
+                ["Level"] = 50,
+                ["Icon"] = "rbxassetid://16197074798",
+                ["Passive"] = true
+            },
+            ["Spark Beam"] = {
+                ["Type"] = "",
+                ["Description"] = "Shoot an electric beam to zap opponents",
+                ["Order"] = 2,
+                ["Level"] = 10,
+                ["Icon"] = "rbxassetid://16197074995"
+            },
+            ["Smite"] = {
+                ["Type"] = "",
+                ["Description"] = "Teleport forward, unleashing a lightning strike that stuns any nearby players",
+                ["Order"] = 3,
+                ["Level"] = 20,
+                ["Icon"] = "rbxassetid://16197075270"
+            }
+        },
+        ["Nebula"] = {
+            ["Gravitation"] = {
+                ["Type"] = "PASSIVE",
+                ["Description"] = "Gain a speed boost when close to players",
+                ["Order"] = "4",
+                ["Level"] = 50,
+                ["Icon"] = "rbxassetid://17032058469",
+                ["Passive"] = true
+            },
+            ["Shooting Star"] = {
+                ["Type"] = "",
+                ["Description"] = "Shoot a star that explodes, dealing damage to anyone within its range!",
+                ["Order"] = 3,
+                ["Level"] = 20,
+                ["Icon"] = "rbxassetid://17031212090"
+            },
+            ["Astral Projection"] = {
+                ["Type"] = "",
+                ["Description"] = "Harness the powers of the cosmos!",
+                ["Order"] = "1",
+                ["Level"] = 0,
+                ["Icon"] = "rbxassetid://17032010734"
+            },
+            ["Black Hole"] = {
+                ["Type"] = "",
+                ["Description"] = "Suck in nearby players, slowing them down and disabling their abilities",
+                ["Order"] = "2",
+                ["Level"] = 10,
+                ["Icon"] = "rbxassetid://17031212328"
+            }
+        },
+        ["Dragon"] = {
+            ["Draconic Leap"] = {
+                ["Type"] = "",
+                ["Description"] = "Leap into the air and slam into your opponents",
+                ["Order"] = "2",
+                ["Level"] = 10,
+                ["Icon"] = "rbxassetid://16303481816"
+            },
+            ["Dragon Guardian"] = {
+                ["Type"] = "",
+                ["Description"] = "Summon three dragon spirits that protect you from getting hit by the ball",
+                ["Order"] = 3,
+                ["Level"] = 20,
+                ["Icon"] = "rbxassetid://16303481968"
+            },
+            ["Fire Breath"] = {
+                ["Type"] = "PASSIVE",
+                ["Description"] = "Build up fire charges over time that empower your next parry",
+                ["Order"] = "4",
+                ["Level"] = 50,
+                ["Icon"] = "rbxassetid://16303482096",
+                ["Passive"] = true
+            },
+            ["Dragon Mount"] = {
+                ["Type"] = "",
+                ["Description"] = "Soar through the skies on a dragon",
+                ["Order"] = "1",
+                ["Level"] = 0,
+                ["Icon"] = "rbxassetid://16303481619"
+            }
+        },
+        ["Fire"] = {
+            ["Fireball"] = {
+                ["Type"] = "",
+                ["Description"] = "Launch a fireball at other players",
+                ["Order"] = "2",
+                ["Level"] = 10,
+                ["Icon"] = "rbxassetid://16169199611"
+            },
+            ["Fire Tact"] = {
+                ["Type"] = "",
+                ["Description"] = "Increase the curve of the ball",
+                ["Order"] = "4",
+                ["Level"] = 50,
+                ["Icon"] = "rbxassetid://16180194638",
+                ["Passive"] = true
+            },
+            ["Raging Flames"] = {
+                ["Type"] = "",
+                ["Description"] = "Collect souls by eliminating other players, to empower your parry when attacking the ball",
+                ["Order"] = 3,
+                ["Level"] = 20,
+                ["Icon"] = "rbxassetid://16197052086"
+            },
+            ["Fire Dash"] = {
+                ["Type"] = "",
+                ["Description"] = "Dash forward with a fire trail",
+                ["Order"] = "1",
+                ["Level"] = 0,
+                ["Icon"] = "rbxassetid://16169223031"
+            }
+        },
+        ["Earth"] = {
+            ["Sunbloom"] = {
+                ["Type"] = "PASSIVE",
+                ["Description"] = "Spawn a sun from eliminated players to regain 20% of health",
+                ["Order"] = 4,
+                ["Level"] = 50,
+                ["Icon"] = "rbxassetid://16207126011",
+                ["Passive"] = true
+            },
+            ["Sprout"] = {
+                ["Type"] = "",
+                ["Description"] = "Teleport up to 3 times, sprouting from the ground.",
+                ["Order"] = 1,
+                ["Level"] = 0,
+                ["Icon"] = "rbxassetid://16207562066"
+            },
+            ["Root Grasp"] = {
+                ["Type"] = "",
+                ["Description"] = "Use roots to pull the ball toward you, powering it up for a powerful strike",
+                ["Order"] = 3,
+                ["Level"] = 20,
+                ["Icon"] = "rbxassetid://16207125594"
+            },
+            ["Root Rake"] = {
+                ["Type"] = "",
+                ["Description"] = "Unleash sharp roots that damage players and disable their moves.",
+                ["Order"] = 2,
+                ["Level"] = 10,
+                ["Icon"] = "rbxassetid://16207125808"
+            }
+        },
+        ["Ice"] = {
+            ["Permafrost Bolt"] = {
+                ["Type"] = "",
+                ["Description"] = "Shoot a beam that freezes players for 2 seconds, disabling their abilities",
+                ["Order"] = 2,
+                ["Level"] = 10,
+                ["Icon"] = "rbxassetid://16186497815"
+            },
+            ["Frostcone Surge"] = {
+                ["Type"] = "",
+                ["Description"] = "Unleash a wave of ice, any players caught in the wave are pushed back and affected by the ice",
+                ["Order"] = 3,
+                ["Level"] = 20,
+                ["Icon"] = "rbxassetid://16186508519"
+            },
+            ["Frostzone"] = {
+                ["Type"] = "MOVEMENT",
+                ["Description"] = "Use icy wind to increase your speed and jump",
+                ["Order"] = 1,
+                ["Level"] = 0,
+                ["Icon"] = "rbxassetid://16186501354"
+            },
+            ["Frostbite"] = {
+                ["Type"] = "PASSIVE",
+                ["Description"] = "Unleash an Ice Aura that slows down close players",
+                ["Order"] = 4,
+                ["Level"] = 50,
+                ["Icon"] = "rbxassetid://16186492515",
+                ["Passive"] = true
+            }
+        }
+    },
+    ["Start"] = "function: 0xa009779fecf567a0",
+    ["GetAbilitiesForType"] = "function: 0x3f396193348c2c50",
+    ["_elementalAbilityIndex"] = {
+        ["Permafrost Bolt"] = {
+            ["Type"] = "",
+            ["Description"] = "Shoot a beam that freezes players for 2 seconds, disabling their abilities",
+            ["Order"] = 2,
+            ["Level"] = 10,
+            ["Icon"] = "rbxassetid://16186497815"
+        },
+        ["Frostcone Surge"] = {
+            ["Type"] = "",
+            ["Description"] = "Unleash a wave of ice, any players caught in the wave are pushed back and affected by the ice",
+            ["Order"] = 3,
+            ["Level"] = 20,
+            ["Icon"] = "rbxassetid://16186508519"
+        },
+        ["Geyser"] = {
+            ["Type"] = "MOVEMENT",
+            ["Description"] = "Use a geyser to jump high into the air.",
+            ["Order"] = 1,
+            ["Level"] = 0,
+            ["Icon"] = "http://www.roblox.com/asset/?id=16169200059"
+        },
+        ["Root Rake"] = {
+            ["Type"] = "",
+            ["Description"] = "Unleash sharp roots that damage players and disable their moves.",
+            ["Order"] = 2,
+            ["Level"] = 10,
+            ["Icon"] = "rbxassetid://16207125808"
+        },
+        ["Astral Projection"] = {
+            ["Type"] = "",
+            ["Description"] = "Harness the powers of the cosmos!",
+            ["Order"] = "1",
+            ["Level"] = 0,
+            ["Icon"] = "rbxassetid://17032010734"
+        },
+        ["Water"] = {
+            ["Type"] = "PASSIVE",
+            ["Description"] = "Increase movement speed by 2.5% per second while moving (up to 50%), user is slowed down when they parry the ball",
+            ["Order"] = 4,
+            ["Level"] = 50,
+            ["Icon"] = "rbxassetid://16180205851",
+            ["Passive"] = true
+        },
+        ["Shooting Star"] = {
+            ["Type"] = "",
+            ["Description"] = "Shoot a star that explodes, dealing damage to anyone within its range!",
+            ["Order"] = 3,
+            ["Level"] = 20,
+            ["Icon"] = "rbxassetid://17031212090"
+        },
+        ["Tidal Wave"] = {
+            ["Type"] = "DAMAGE",
+            ["Description"] = "Ride a wave of water, pushing enemies away upon impact",
+            ["Order"] = 3,
+            ["Level"] = 20,
+            ["Icon"] = "http://www.roblox.com/asset/?id=16169199837"
+        },
+        ["Static Shock"] = {
+            ["Type"] = "PASSIVE",
+            ["Description"] = "Unleash a lightning aura that slowly damages nearby players",
+            ["Order"] = 4,
+            ["Level"] = 50,
+            ["Icon"] = "rbxassetid://16197074798",
+            ["Passive"] = true
+        },
+        ["Frostbite"] = {
+            ["Type"] = "PASSIVE",
+            ["Description"] = "Unleash an Ice Aura that slows down close players",
+            ["Order"] = 4,
+            ["Level"] = 50,
+            ["Icon"] = "rbxassetid://16186492515",
+            ["Passive"] = true
+        },
+        ["Gravitation"] = {
+            ["Type"] = "PASSIVE",
+            ["Description"] = "Gain a speed boost when close to players",
+            ["Order"] = "4",
+            ["Level"] = 50,
+            ["Icon"] = "rbxassetid://17032058469",
+            ["Passive"] = true
+        },
+        ["Bolt"] = {
+            ["Type"] = "",
+            ["Description"] = "Teleport forward at the speed of light.",
+            ["Order"] = 1,
+            ["Level"] = 0,
+            ["Icon"] = "rbxassetid://16197075529"
+        },
+        ["Black Hole"] = {
+            ["Type"] = "",
+            ["Description"] = "Suck in nearby players, slowing them down and disabling their abilities",
+            ["Order"] = "2",
+            ["Level"] = 10,
+            ["Icon"] = "rbxassetid://17031212328"
+        },
+        ["Draconic Leap"] = {
+            ["Type"] = "",
+            ["Description"] = "Leap into the air and slam into your opponents",
+            ["Order"] = "2",
+            ["Level"] = 10,
+            ["Icon"] = "rbxassetid://16303481816"
+        },
+        ["Sunbloom"] = {
+            ["Type"] = "PASSIVE",
+            ["Description"] = "Spawn a sun from eliminated players to regain 20% of health",
+            ["Order"] = 4,
+            ["Level"] = 50,
+            ["Icon"] = "rbxassetid://16207126011",
+            ["Passive"] = true
+        },
+        ["Dragon Mount"] = {
+            ["Type"] = "",
+            ["Description"] = "Soar through the skies on a dragon",
+            ["Order"] = "1",
+            ["Level"] = 0,
+            ["Icon"] = "rbxassetid://16303481619"
+        },
+        ["Fire Dash"] = {
+            ["Type"] = "",
+            ["Description"] = "Dash forward with a fire trail",
+            ["Order"] = "1",
+            ["Level"] = 0,
+            ["Icon"] = "rbxassetid://16169223031"
+        },
+        ["Frostzone"] = {
+            ["Type"] = "MOVEMENT",
+            ["Description"] = "Use icy wind to increase your speed and jump",
+            ["Order"] = 1,
+            ["Level"] = 0,
+            ["Icon"] = "rbxassetid://16186501354"
+        },
+        ["Raging Flames"] = {
+            ["Type"] = "",
+            ["Description"] = "Collect souls by eliminating other players, to empower your parry when attacking the ball",
+            ["Order"] = 3,
+            ["Level"] = 20,
+            ["Icon"] = "rbxassetid://16197052086"
+        },
+        ["Spark Beam"] = {
+            ["Type"] = "",
+            ["Description"] = "Shoot an electric beam to zap opponents",
+            ["Order"] = 2,
+            ["Level"] = 10,
+            ["Icon"] = "rbxassetid://16197074995"
+        },
+        ["Dragon Guardian"] = {
+            ["Type"] = "",
+            ["Description"] = "Summon three dragon spirits that protect you from getting hit by the ball",
+            ["Order"] = 3,
+            ["Level"] = 20,
+            ["Icon"] = "rbxassetid://16303481968"
+        },
+        ["Sprout"] = {
+            ["Type"] = "",
+            ["Description"] = "Teleport up to 3 times, sprouting from the ground.",
+            ["Order"] = 1,
+            ["Level"] = 0,
+            ["Icon"] = "rbxassetid://16207562066"
+        },
+        ["Fire Breath"] = {
+            ["Type"] = "PASSIVE",
+            ["Description"] = "Build up fire charges over time that empower your next parry",
+            ["Order"] = "4",
+            ["Level"] = 50,
+            ["Icon"] = "rbxassetid://16303482096",
+            ["Passive"] = true
+        },
+        ["Smite"] = {
+            ["Type"] = "",
+            ["Description"] = "Teleport forward, unleashing a lightning strike that stuns any nearby players",
+            ["Order"] = 3,
+            ["Level"] = 20,
+            ["Icon"] = "rbxassetid://16197075270"
+        },
+        ["Root Grasp"] = {
+            ["Type"] = "",
+            ["Description"] = "Use roots to pull the ball toward you, powering it up for a powerful strike",
+            ["Order"] = 3,
+            ["Level"] = 20,
+            ["Icon"] = "rbxassetid://16207125594"
+        },
+        ["Fireball"] = {
+            ["Type"] = "",
+            ["Description"] = "Launch a fireball at other players",
+            ["Order"] = "2",
+            ["Level"] = 10,
+            ["Icon"] = "rbxassetid://16169199611"
+        },
+        ["Fire Tact"] = {
+            ["Type"] = "",
+            ["Description"] = "Increase the curve of the ball",
+            ["Order"] = "4",
+            ["Level"] = 50,
+            ["Icon"] = "rbxassetid://16180194638",
+            ["Passive"] = true
+        },
+        ["Shark Bite"] = {
+            ["Type"] = "ENVIRONMENT",
+            ["Description"] = "Summon a shark to attack other players",
+            ["Order"] = 2,
+            ["Level"] = 10,
+            ["Icon"] = "http://www.roblox.com/asset/?id=16169200312"
+        }
+    }
+}
