ac42d28e799d88012396fa2ec547c0ff58c37267
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-22 22:50:14 +1000

    Create DungeonsCrafting.lua

---
+++ GameData/Blade Ball [13772394625]/DungeonsCrafting.lua
@@ -0,0 +1,100 @@
+data = {
+    ["Ingredients"] = {
+        ["ParticleJar"] = {["Image"] = "rbxassetid://17440891757", ["DisplayName"] = "Particle Jar"},
+        ["IronBar"] = {["Image"] = "rbxassetid://17440888881", ["DisplayName"] = "Iron Bar"},
+        ["Scroll"] = {["Image"] = "rbxassetid://17440887487", ["DisplayName"] = "Scroll"},
+        ["RubyGem"] = {["Image"] = "rbxassetid://17440890292", ["DisplayName"] = "Ruby Gem"},
+        ["Skull"] = {["Image"] = "rbxassetid://17440881722", ["DisplayName"] = "Skull"},
+        ["MagicalOrb"] = {["Image"] = "rbxassetid://17440884917", ["DisplayName"] = "Magical Orb"}
+    },
+    ["Recipes"] = {
+        ["Emote_307"] = {
+            ["Reward"] = {
+                ["Value"] = "Emote307",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://17441024391",
+                ["DisplayName"] = "Serpent's Chosen Emote"
+            },
+            ["Image"] = "rbxassetid://17441024391",
+            ["Ingredients"] = {["Scroll"] = 2, ["MagicalOrb"] = 3},
+            ["DisplayName"] = "Serpent's Chosen"
+        },
+        ["BladeofEras"] = {
+            ["Reward"] = {
+                ["Value"] = "Blade of Eras",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17442688121",
+                ["DisplayName"] = "Blade of Eras"
+            },
+            ["Image"] = "rbxassetid://17442688121",
+            ["Ingredients"] = {["ParticleJar"] = 5, ["RubyGem"] = 7, ["IronBar"] = 10},
+            ["DisplayName"] = "Blade of Eras"
+        },
+        ["Frostwraith"] = {
+            ["Reward"] = {
+                ["Value"] = "Frostwraith",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17440990904",
+                ["DisplayName"] = "Frostwraith"
+            },
+            ["Image"] = "rbxassetid://17440990904",
+            ["Ingredients"] = {["ParticleJar"] = 3, ["RubyGem"] = 5, ["IronBar"] = 7},
+            ["DisplayName"] = "Frostwraith"
+        },
+        ["Emote_308"] = {
+            ["Reward"] = {
+                ["Value"] = "Emote308",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://17441024563",
+                ["DisplayName"] = "Ancient Mark Emote"
+            },
+            ["Image"] = "rbxassetid://17441024563",
+            ["Ingredients"] = {["Scroll"] = 3, ["IronBar"] = 2, ["MagicalOrb"] = 5},
+            ["DisplayName"] = "Ancient Mark"
+        },
+        ["BriarBlade"] = {
+            ["Reward"] = {
+                ["Value"] = "Briar's Blade",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17440991340",
+                ["DisplayName"] = "Briar's Blade"
+            },
+            ["Image"] = "rbxassetid://17440991340",
+            ["Ingredients"] = {["ParticleJar"] = 1, ["RubyGem"] = 2, ["IronBar"] = 5, ["MagicalOrb"] = 3},
+            ["DisplayName"] = "Briar's Blade"
+        },
+        ["NatureFang"] = {
+            ["Reward"] = {
+                ["Value"] = "Nature's Fang",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://17440990192",
+                ["DisplayName"] = "Nature's Fang"
+            },
+            ["Image"] = "rbxassetid://17440990192",
+            ["Ingredients"] = {["IronBar"] = 5, ["MagicalOrb"] = 5},
+            ["DisplayName"] = "Nature's Fang"
+        },
+        ["MotherNature"] = {
+            ["Reward"] = {
+                ["Value"] = "Mother Nature",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://17440953142",
+                ["DisplayName"] = "Mother Nature Explosion"
+            },
+            ["Image"] = "rbxassetid://17440953142",
+            ["Ingredients"] = {["Skull"] = 3, ["MagicalOrb"] = 1},
+            ["DisplayName"] = "Mother Nature"
+        },
+        ["MidnightBloom"] = {
+            ["Reward"] = {
+                ["Value"] = "Midnight Bloom",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://17440953337",
+                ["DisplayName"] = "Midnight Bloom Explosion"
+            },
+            ["Image"] = "rbxassetid://17440953337",
+            ["Ingredients"] = {["Skull"] = 5, ["MagicalOrb"] = 2},
+            ["DisplayName"] = "Midnight Bloom"
+        }
+    }
+}
