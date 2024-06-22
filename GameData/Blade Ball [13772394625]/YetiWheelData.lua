e981b8642a4c08d48848930f52f7751e6a320519
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-22 22:38:02 +1000

    Create YetiWheelData.lua

---
+++ GameData/Blade Ball [13772394625]/YetiWheelData.lua
@@ -0,0 +1,334 @@
+data = {
+    ["Products"] = {["Spinx1"] = 1829338555, ["Spinx1First"] = 1829338544},
+    ["GuaranteedReward"] = {
+        ["Value"] = "Frosted Gleam",
+        ["Type"] = "Sword",
+        ["Icon"] = "rbxassetid://17533754507",
+        ["DisplayName"] = "Frosted Gleam"
+    },
+    ["StreakHoursRewards"] = {
+        {
+            ["TimeStamp"] = 3600,
+            ["Reward"] = {
+                ["Value"] = 1,
+                ["Type"] = "YetiWheelSpins",
+                ["Icon"] = "rbxassetid://17534344343",
+                ["DisplayName"] = "x1 Yeti Spins"
+            }
+        },
+        {
+            ["TimeStamp"] = 7200,
+            ["Reward"] = {
+                ["Value"] = 2,
+                ["Type"] = "YetiWheelSpins",
+                ["Icon"] = "rbxassetid://17534344343",
+                ["DisplayName"] = "x2 Yeti Spins"
+            }
+        },
+        {
+            ["TimeStamp"] = 10800,
+            ["Reward"] = {
+                ["Value"] = 4,
+                ["Type"] = "YetiWheelSpins",
+                ["Icon"] = "rbxassetid://17534344343",
+                ["DisplayName"] = "x4 Yeti Spins"
+            }
+        },
+        {
+            ["TimeStamp"] = 14400,
+            ["Reward"] = {
+                ["Value"] = 8,
+                ["Type"] = "YetiWheelSpins",
+                ["Icon"] = "rbxassetid://17534344343",
+                ["DisplayName"] = "x8 Yeti Spins"
+            }
+        }
+    },
+    ["HourlyReward"] = {
+        ["Value"] = 1,
+        ["Type"] = "YetiWheelSpins",
+        ["Icon"] = "rbxassetid://17534344343",
+        ["DisplayName"] = "x1 Yeti Spins"
+    },
+    ["Outer"] = {
+        ["Item2"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1717520400,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Death Slash",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://15448381140",
+                    ["DisplayName"] = "Death Slash Trial (15m)"
+                }
+            },
+            ["Angle"] = 45,
+            ["Reward"] = {
+                ["Value"] = "Blizzard Smash",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://17530546783",
+                ["DisplayName"] = "Blizzard Smash Explosion"
+            },
+            ["Probability"] = 15
+        },
+        ["Item5"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1717520400,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Continuity Zero",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://15797061567",
+                    ["DisplayName"] = "Continuity Zero Trial (15m)"
+                }
+            },
+            ["Angle"] = 225,
+            ["Reward"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 1,
+                ["Value"] = "PremiumExplosion",
+                ["Icon"] = "rbxassetid://15049303003",
+                ["DisplayName"] = "Premium Explosion Crate"
+            },
+            ["Probability"] = 10
+        },
+        ["Item6"] = {["Lucky"] = true, ["Advance"] = true, ["Angle"] = 179, ["IconId"] = "", ["Probability"] = 25},
+        ["Item7"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1717520400,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Infinity",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://14852723022",
+                    ["DisplayName"] = "Infinity Trial (15m)"
+                }
+            },
+            ["Angle"] = 269,
+            ["Reward"] = {
+                ["Value"] = 1000,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713779371",
+                ["DisplayName"] = "1,000 Coins"
+            },
+            ["Probability"] = 8
+        },
+        ["Item8"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1717520400,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Quantum Arena",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://15519219917",
+                    ["DisplayName"] = "Quantum Arena Trial (15m)"
+                }
+            },
+            ["Angle"] = 317,
+            ["Reward"] = {
+                ["Value"] = 2500,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713785287",
+                ["DisplayName"] = "2,500 Coins"
+            },
+            ["Probability"] = 2
+        },
+        ["Item4"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1717520400,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Titan Blade",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://15460700443",
+                    ["DisplayName"] = "Titan Blade Trial (15m)"
+                }
+            },
+            ["Angle"] = 133.5,
+            ["Reward"] = {
+                ["Value"] = 1,
+                ["Type"] = "YetiWheelSpins",
+                ["Icon"] = "rbxassetid://17534344343",
+                ["DisplayName"] = "x1 Yeti Spins"
+            },
+            ["Probability"] = 15
+        },
+        ["Item3"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1717520400,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Phantom",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://15059341639",
+                    ["DisplayName"] = "Phantom Trial (15m)"
+                }
+            },
+            ["Angle"] = 89,
+            ["Reward"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 1,
+                ["Value"] = "PremiumSword",
+                ["Icon"] = "rbxassetid://15049301853",
+                ["DisplayName"] = "Premium Sword Crate"
+            },
+            ["Probability"] = 15
+        },
+        ["Item1"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1717520400,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Serpent Shadow Clone",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://15508130858",
+                    ["DisplayName"] = "Serpent Shadow Clone Trial (15m)"
+                }
+            },
+            ["Angle"] = 3.5,
+            ["Reward"] = {
+                ["Value"] = 500,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713794582",
+                ["DisplayName"] = "500 Coins"
+            },
+            ["Probability"] = 10
+        }
+    },
+    ["Inner"] = {
+        ["Item2"] = {
+            ["Angle"] = 56,
+            ["Reward"] = {
+                ["Value"] = "Emote338",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://17528044807",
+                ["DisplayName"] = "Chilly Hideout Emote"
+            },
+            ["Probability"] = 40
+        },
+        ["Item4"] = {["IconId"] = "", ["Angle"] = 235, ["Advance"] = true, ["Probability"] = 5},
+        ["Item3"] = {
+            ["Angle"] = 144,
+            ["Reward"] = {
+                ["Value"] = 150,
+                ["Type"] = "SeasonPassCurrency",
+                ["Icon"] = "rbxassetid://17515245060",
+                ["DisplayName"] = "150 Crystals"
+            },
+            ["Probability"] = 40
+        },
+        ["Item1"] = {
+            ["Angle"] = -33,
+            ["Reward"] = {
+                ["Value"] = "Arctic Fury",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://17528048112",
+                ["DisplayName"] = "Arctic Fury Explosion"
+            },
+            ["Probability"] = 15
+        }
+    },
+    ["End"] = "1716656400000",
+    ["Start"] = "1716033600000",
+    ["GuaranteedReplacementProbabilty"] = 1,
+    ["Middle"] = {
+        ["Item3"] = {["IconId"] = "", ["Angle"] = 56, ["Advance"] = true, ["Probability"] = 15},
+        ["Item5"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1717520400,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Bunny Leap",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://16931437185",
+                    ["DisplayName"] = "Bunny Leap Trial (15m)"
+                }
+            },
+            ["Angle"] = 235,
+            ["Reward"] = {
+                ["Value"] = 200,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713797174",
+                ["DisplayName"] = "200 Coins"
+            },
+            ["Probability"] = 10
+        },
+        ["Item6"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1717520400,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Singularity",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://16680650414",
+                    ["DisplayName"] = "Singularity Trial (15m)"
+                }
+            },
+            ["Angle"] = 300,
+            ["Reward"] = {
+                ["Value"] = 50,
+                ["Type"] = "SeasonPassCurrency",
+                ["Icon"] = "rbxassetid://17515245060",
+                ["DisplayName"] = "50 Crystals"
+            },
+            ["Probability"] = 13
+        },
+        ["Item1"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1717520400,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Slash of Duality",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://17128015725",
+                    ["DisplayName"] = "Slash of Duality Trial (15m)"
+                }
+            },
+            ["Angle"] = 2,
+            ["Reward"] = {
+                ["Value"] = 2500,
+                ["Type"] = "Coins",
+                ["Icon"] = "rbxassetid://14713785287",
+                ["DisplayName"] = "2,500 Coins"
+            },
+            ["Probability"] = 5
+        },
+        ["Item2"] = {
+            ["Angle"] = 118,
+            ["Reward"] = {
+                ["Type"] = "CrateKey",
+                ["Amount"] = 1,
+                ["Value"] = "PremiumSword",
+                ["Icon"] = "rbxassetid://15049301853",
+                ["DisplayName"] = "Premium Sword Crate"
+            },
+            ["Probability"] = 45
+        },
+        ["Item4"] = {
+            ["AbilityTrial"] = {
+                ["ExpireTime"] = 1717520400,
+                ["Reward"] = {
+                    ["Type"] = "AbilityFreeTrial",
+                    ["Value"] = "Dribble",
+                    ["Duration"] = 900,
+                    ["Icon"] = "rbxassetid://15705640503",
+                    ["DisplayName"] = "Dribble Trial (15m)"
+                }
+            },
+            ["Angle"] = 180,
+            ["Reward"] = {
+                ["Value"] = 50,
+                ["Type"] = "SeasonPassCurrency",
+                ["Icon"] = "rbxassetid://17515245060",
+                ["DisplayName"] = "50 Crystals"
+            },
+            ["Probability"] = 12
+        }
+    },
+    ["GuaranteedReplacement"] = {
+        ["Value"] = "Dual Frosted Gleam",
+        ["Type"] = "Sword",
+        ["Icon"] = "rbxassetid://17533757509",
+        ["DisplayName"] = "Dual Frosted Gleam"
+    }
+}
