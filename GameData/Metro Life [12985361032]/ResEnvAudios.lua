7eddd6f3e42b0ee779441fce6140818832fec0fd
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:52:57 +1000

    Create ResEnvAudios.lua

---
+++ GameData/Metro Life [12985361032]/ResEnvAudios.lua
@@ -0,0 +1,94 @@
+data = {
+    ["AirportTerminal"] = {
+        ["Clear"] = "Departure_Hall",
+        ["Priority"] = 2,
+        ["AreaRunningSound"] = "",
+        ["Rain"] = "Departure_Hall",
+        ["Cloudy"] = "Departure_Hall",
+        ["RegionPart"] = "AirportTerminal"
+    },
+    ["StarlightClub"] = {
+        ["Clear"] = "StarlightClub",
+        ["Priority"] = 2,
+        ["AreaRunningSound"] = "",
+        ["Rain"] = "StarlightClub",
+        ["Cloudy"] = "StarlightClub",
+        ["RegionPart"] = "StarlightClub"
+    },
+    ["Base_aboveground"] = {
+        ["Clear"] = "Environment_City_New",
+        ["Priority"] = 1,
+        ["AreaRunningSound"] = "",
+        ["Rain"] = "Environment_City_New",
+        ["Cloudy"] = "Environment_City_New",
+        ["RegionPart"] = "Base_aboveground"
+    },
+    ["AirportPlane"] = {
+        ["Clear"] = "Airport",
+        ["Priority"] = 2,
+        ["AreaRunningSound"] = "",
+        ["Rain"] = "Airport",
+        ["Cloudy"] = "Airport",
+        ["RegionPart"] = "AirportPlane"
+    },
+    ["Spa"] = {
+        ["Clear"] = "",
+        ["Priority"] = 2,
+        ["AreaRunningSound"] = "",
+        ["Rain"] = "",
+        ["Cloudy"] = "",
+        ["RegionPart"] = "Spa"
+    },
+    ["MetroMall"] = {
+        ["Clear"] = "",
+        ["Priority"] = 2,
+        ["AreaRunningSound"] = "",
+        ["Rain"] = "",
+        ["Cloudy"] = "",
+        ["RegionPart"] = "MetroMall"
+    },
+    ["Base_aboveground_night"] = {
+        ["Clear"] = "Environment_City_Night",
+        ["Priority"] = 1,
+        ["AreaRunningSound"] = "",
+        ["Rain"] = "Environment_City_Night",
+        ["Cloudy"] = "Environment_City_Night",
+        ["RegionPart"] = "Base_aboveground_night"
+    },
+    ["Island_Airport"] = {
+        ["Clear"] = "Departure_Hall",
+        ["Priority"] = 2,
+        ["AreaRunningSound"] = "",
+        ["Rain"] = "Departure_Hall",
+        ["Cloudy"] = "Departure_Hall",
+        ["RegionPart"] = "Island_Airport"
+    },
+    ["Apartment"] = {
+        ["Clear"] = "",
+        ["Priority"] = 2,
+        ["AreaRunningSound"] = "",
+        ["Rain"] = "",
+        ["Cloudy"] = "",
+        ["RegionPart"] = "Apartment"
+    },
+    ["__index"] = {
+        "Base_aboveground",
+        "Base_aboveground_night",
+        "Base_underground",
+        "Apartment",
+        "Spa",
+        "MetroMall",
+        "AirportPlane",
+        "AirportTerminal",
+        "Island_Airport",
+        "StarlightClub"
+    },
+    ["Base_underground"] = {
+        ["Clear"] = "",
+        ["Priority"] = 1,
+        ["AreaRunningSound"] = "",
+        ["Rain"] = "",
+        ["Cloudy"] = "",
+        ["RegionPart"] = "Base_underground"
+    }
+}
