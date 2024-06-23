c76b93b5dc2b3c22b034ad6505689d3505dc240a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:48:04 +1000

    Create ResApartmentRoomType.lua

---
+++ GameData/Metro Life [12985361032]/ResApartmentRoomType.lua
@@ -0,0 +1,58 @@
+data = {
+    ["Apartment_Room_01"] = {
+        ["RoomType"] = "topiaPrimary",
+        ["Sort"] = 1,
+        ["Id"] = "Apartment_Room_01",
+        ["NameLoc"] = "",
+        ["Icon"] = "rbxassetid://10301276831",
+        ["IsNew"] = ""
+    },
+    ["__index"] = {
+        "Apartment_Room_01",
+        "Apartment_VProom",
+        "Apartment_SSroom",
+        "topiaRest",
+        "Apartment_VProom_02",
+        "Apartment_VProom_03"
+    },
+    ["Apartment_VProom"] = {
+        ["RoomType"] = "topiaAdvanced",
+        ["Sort"] = 3,
+        ["Id"] = "Apartment_VProom",
+        ["NameLoc"] = "K_APARTMENT_CHECKIN_TYPE_1",
+        ["Icon"] = "rbxassetid://10301276494",
+        ["IsNew"] = ""
+    },
+    ["Apartment_VProom_03"] = {
+        ["RoomType"] = "topiaAdvanced",
+        ["Sort"] = 1,
+        ["Id"] = "Apartment_VProom_03",
+        ["NameLoc"] = "K_APARTMENT_CHECKIN_TYPE_3",
+        ["Icon"] = "rbxassetid://11895430789",
+        ["IsNew"] = "2022-12-30"
+    },
+    ["topiaRest"] = {
+        ["RoomType"] = "topiaRest",
+        ["Sort"] = 1,
+        ["Id"] = "topiaRest",
+        ["NameLoc"] = "",
+        ["Icon"] = "rbxassetid://10302742204",
+        ["IsNew"] = ""
+    },
+    ["Apartment_VProom_02"] = {
+        ["RoomType"] = "topiaAdvanced",
+        ["Sort"] = 2,
+        ["Id"] = "Apartment_VProom_02",
+        ["NameLoc"] = "K_APARTMENT_CHECKIN_TYPE_2",
+        ["Icon"] = "rbxassetid://10909064176",
+        ["IsNew"] = "2022-9-23"
+    },
+    ["Apartment_SSroom"] = {
+        ["RoomType"] = "topiaPass",
+        ["Sort"] = 1,
+        ["Id"] = "Apartment_SSroom",
+        ["NameLoc"] = "",
+        ["Icon"] = "rbxassetid://10302348093",
+        ["IsNew"] = ""
+    }
+}
