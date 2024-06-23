218668f3ebef8c57c681ea3933300afb9810dbd1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:57:14 +1000

    Create ResFrame.lua

---
+++ GameData/Metro Life [12985361032]/ResFrame.lua
@@ -0,0 +1,158 @@
+data = {
+    ["Frame_2"] = {
+        ["Bg"] = "rbxassetid://14036201529",
+        ["MinSize"] = 0.3,
+        ["Sort"] = 98,
+        ["MaxSize"] = 0.9,
+        ["Id"] = "Frame_2",
+        ["NameLoc"] = "K_SELFIE_TYPE2",
+        ["Icon"] = "rbxassetid://13972700097",
+        ["New"] = ""
+    },
+    ["Frame_8"] = {
+        ["Bg"] = "rbxassetid://15745224340",
+        ["MinSize"] = 0.3,
+        ["Sort"] = 92,
+        ["MaxSize"] = 0.9,
+        ["Id"] = "Frame_8",
+        ["NameLoc"] = "K_SELFIE_TYPE8",
+        ["Icon"] = "rbxassetid://15765953121",
+        ["New"] = ""
+    },
+    ["Frame_9"] = {
+        ["Bg"] = "rbxassetid://15932821993",
+        ["MinSize"] = 0.3,
+        ["Sort"] = 91,
+        ["MaxSize"] = 0.9,
+        ["Id"] = "Frame_9",
+        ["NameLoc"] = "K_SELFIE_TYPE9",
+        ["Icon"] = "rbxassetid://15765953321",
+        ["New"] = ""
+    },
+    ["Frame_6"] = {
+        ["Bg"] = "rbxassetid://15606841214",
+        ["MinSize"] = 0.3,
+        ["Sort"] = 94,
+        ["MaxSize"] = 0.9,
+        ["Id"] = "Frame_6",
+        ["NameLoc"] = "K_SELFIE_TYPE6",
+        ["Icon"] = "rbxassetid://15622122850",
+        ["New"] = ""
+    },
+    ["__index"] = {
+        "Frame_1",
+        "Frame_2",
+        "Frame_3",
+        "Frame_4",
+        "Frame_5",
+        "Frame_6",
+        "Frame_7",
+        "Frame_8",
+        "Frame_9",
+        "Frame_10",
+        "Frame_11",
+        "Frame_12",
+        "Frame_13",
+        "Frame_14"
+    },
+    ["Frame_11"] = {
+        ["Bg"] = "rbxassetid://16207113177",
+        ["MinSize"] = 0.3,
+        ["Sort"] = 89,
+        ["MaxSize"] = 0.9,
+        ["Id"] = "Frame_11",
+        ["NameLoc"] = "K_SELFIE_TYPE11",
+        ["Icon"] = "rbxassetid://16161658868",
+        ["New"] = ""
+    },
+    ["Frame_5"] = {
+        ["Bg"] = "rbxassetid://15606547018",
+        ["MinSize"] = 0.3,
+        ["Sort"] = 95,
+        ["MaxSize"] = 0.9,
+        ["Id"] = "Frame_5",
+        ["NameLoc"] = "K_SELFIE_TYPE5",
+        ["Icon"] = "rbxassetid://15622122683",
+        ["New"] = ""
+    },
+    ["Frame_1"] = {
+        ["Bg"] = "rbxassetid://14036201936",
+        ["MinSize"] = 0.3,
+        ["Sort"] = 99,
+        ["MaxSize"] = 0.9,
+        ["Id"] = "Frame_1",
+        ["NameLoc"] = "K_SELFIE_TYPE1",
+        ["Icon"] = "rbxassetid://13972700256",
+        ["New"] = ""
+    },
+    ["Frame_13"] = {
+        ["Bg"] = "rbxassetid://17423556342",
+        ["MinSize"] = 0.3,
+        ["Sort"] = 87,
+        ["MaxSize"] = 0.9,
+        ["Id"] = "Frame_13",
+        ["NameLoc"] = "K_SELFIE_TYPE13",
+        ["Icon"] = "rbxassetid://17480817159",
+        ["New"] = "2024-05-24"
+    },
+    ["Frame_7"] = {
+        ["Bg"] = "rbxassetid://15606547216",
+        ["MinSize"] = 0.3,
+        ["Sort"] = 93,
+        ["MaxSize"] = 0.9,
+        ["Id"] = "Frame_7",
+        ["NameLoc"] = "K_SELFIE_TYPE7",
+        ["Icon"] = "rbxassetid://15622122793",
+        ["New"] = ""
+    },
+    ["Frame_12"] = {
+        ["Bg"] = "rbxassetid://17086151912",
+        ["MinSize"] = 0.3,
+        ["Sort"] = 88,
+        ["MaxSize"] = 0.9,
+        ["Id"] = "Frame_12",
+        ["NameLoc"] = "K_SELFIE_TYPE12",
+        ["Icon"] = "rbxassetid://17088383318",
+        ["New"] = ""
+    },
+    ["Frame_14"] = {
+        ["Bg"] = "rbxassetid://17423533691",
+        ["MinSize"] = 0.3,
+        ["Sort"] = 86,
+        ["MaxSize"] = 0.9,
+        ["Id"] = "Frame_14",
+        ["NameLoc"] = "K_SELFIE_TYPE14",
+        ["Icon"] = "rbxassetid://17480817054",
+        ["New"] = "2024-05-24"
+    },
+    ["Frame_3"] = {
+        ["Bg"] = "rbxassetid://14036200951",
+        ["MinSize"] = 0.3,
+        ["Sort"] = 97,
+        ["MaxSize"] = 0.9,
+        ["Id"] = "Frame_3",
+        ["NameLoc"] = "K_SELFIE_TYPE3",
+        ["Icon"] = "rbxassetid://13972699903",
+        ["New"] = ""
+    },
+    ["Frame_4"] = {
+        ["Bg"] = "rbxassetid://14036202410",
+        ["MinSize"] = 0.3,
+        ["Sort"] = 96,
+        ["MaxSize"] = 0.9,
+        ["Id"] = "Frame_4",
+        ["NameLoc"] = "K_SELFIE_TYPE4",
+        ["Icon"] = "rbxassetid://13972699648",
+        ["New"] = ""
+    },
+    ["Frame_10"] = {
+        ["Bg"] = "rbxassetid://16022479961",
+        ["MinSize"] = 0.3,
+        ["Sort"] = 90,
+        ["MaxSize"] = 0.9,
+        ["Id"] = "Frame_10",
+        ["NameLoc"] = "K_SELFIE_TYPE10",
+        ["Icon"] = "rbxassetid://16161658963",
+        ["New"] = ""
+    }
+}
