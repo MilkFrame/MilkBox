f955fd619408bc11e7b5bfc201c30d9efcd77a01
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:57:02 +1000

    Create ResSticker.lua

---
+++ GameData/Metro Life [12985361032]/ResSticker.lua
@@ -0,0 +1,147 @@
+data = {
+    ["Sticker_5"] = {
+        ["ModelAssetId"] = "Sticker_5",
+        ["Orientation"] = "0, 0, 0",
+        ["Sort"] = 95,
+        ["Position"] = "0.02, 0, -0.624",
+        ["Id"] = "Sticker_5",
+        ["NameLoc"] = "K_SELFIE_TYPE5",
+        ["Icon"] = "rbxassetid://15932828129",
+        ["New"] = ""
+    },
+    ["Sticker_10"] = {
+        ["ModelAssetId"] = "Sticker_10",
+        ["Orientation"] = "0, 0, 0",
+        ["Sort"] = 90,
+        ["Position"] = "0, 0.15, -0.624",
+        ["Id"] = "Sticker_10",
+        ["NameLoc"] = "K_SELFIE_TYPE10",
+        ["Icon"] = "rbxassetid://16878243509",
+        ["New"] = "2024-04-03"
+    },
+    ["Sticker_4"] = {
+        ["ModelAssetId"] = "Sticker_4",
+        ["Orientation"] = "0, 0, 0",
+        ["Sort"] = 96,
+        ["Position"] = "0.025, 0.36, -0.624",
+        ["Id"] = "Sticker_4",
+        ["NameLoc"] = "K_SELFIE_TYPE4",
+        ["Icon"] = "rbxassetid://15932828257",
+        ["New"] = ""
+    },
+    ["Sticker_1"] = {
+        ["ModelAssetId"] = "Sticker_1",
+        ["Orientation"] = "0, 0, 0",
+        ["Sort"] = 99,
+        ["Position"] = "0.02, 0.17, -0.624",
+        ["Id"] = "Sticker_1",
+        ["NameLoc"] = "K_SELFIE_TYPE1",
+        ["Icon"] = "rbxassetid://15932828666",
+        ["New"] = ""
+    },
+    ["Sticker_13"] = {
+        ["ModelAssetId"] = "Sticker_13",
+        ["Orientation"] = "0, 0, 0",
+        ["Sort"] = 87,
+        ["Position"] = "0, 0.25, -0.624",
+        ["Id"] = "Sticker_13",
+        ["NameLoc"] = "K_SELFIE_TYPE13",
+        ["Icon"] = "rbxassetid://17086151453",
+        ["New"] = "2024-04-24"
+    },
+    ["Sticker_3"] = {
+        ["ModelAssetId"] = "Sticker_3",
+        ["Orientation"] = "0, 0, 0",
+        ["Sort"] = 97,
+        ["Position"] = "0.02, 0.5, -0.624",
+        ["Id"] = "Sticker_3",
+        ["NameLoc"] = "K_SELFIE_TYPE3",
+        ["Icon"] = "rbxassetid://15932828344",
+        ["New"] = ""
+    },
+    ["Sticker_9"] = {
+        ["ModelAssetId"] = "Sticker_9",
+        ["Orientation"] = "0, 0, 0",
+        ["Sort"] = 91,
+        ["Position"] = "0, 0.05, -0.624",
+        ["Id"] = "Sticker_9",
+        ["NameLoc"] = "K_SELFIE_TYPE9",
+        ["Icon"] = "rbxassetid://16878243683",
+        ["New"] = "2024-04-03"
+    },
+    ["Sticker_12"] = {
+        ["ModelAssetId"] = "Sticker_12",
+        ["Orientation"] = "0, 0, 0",
+        ["Sort"] = 88,
+        ["Position"] = "0, 0.05, -0.624",
+        ["Id"] = "Sticker_12",
+        ["NameLoc"] = "K_SELFIE_TYPE12",
+        ["Icon"] = "rbxassetid://16890377821",
+        ["New"] = "2024-04-03"
+    },
+    ["Sticker_2"] = {
+        ["ModelAssetId"] = "Sticker_2",
+        ["Orientation"] = "0, 0, 0",
+        ["Sort"] = 98,
+        ["Position"] = "0, 0.2, -0.624",
+        ["Id"] = "Sticker_2",
+        ["NameLoc"] = "K_SELFIE_TYPE2",
+        ["Icon"] = "rbxassetid://15932828520",
+        ["New"] = ""
+    },
+    ["Sticker_6"] = {
+        ["ModelAssetId"] = "Sticker_6",
+        ["Orientation"] = "0, 0, 0",
+        ["Sort"] = 94,
+        ["Position"] = "0, 0.21, -0.624",
+        ["Id"] = "Sticker_6",
+        ["NameLoc"] = "K_SELFIE_TYPE6",
+        ["Icon"] = "rbxassetid://15932828029",
+        ["New"] = ""
+    },
+    ["Sticker_8"] = {
+        ["ModelAssetId"] = "Sticker_8",
+        ["Orientation"] = "0, 0, 0",
+        ["Sort"] = 92,
+        ["Position"] = "0, 0.05, -0.624",
+        ["Id"] = "Sticker_8",
+        ["NameLoc"] = "K_SELFIE_TYPE8",
+        ["Icon"] = "rbxassetid://16878243864",
+        ["New"] = "2024-04-03"
+    },
+    ["Sticker_11"] = {
+        ["ModelAssetId"] = "Sticker_11",
+        ["Orientation"] = "0, 0, 0",
+        ["Sort"] = 89,
+        ["Position"] = "0, 0.535, -0.624",
+        ["Id"] = "Sticker_11",
+        ["NameLoc"] = "K_SELFIE_TYPE11",
+        ["Icon"] = "rbxassetid://16890378085",
+        ["New"] = "2024-04-03"
+    },
+    ["__index"] = {
+        "Sticker_1",
+        "Sticker_2",
+        "Sticker_3",
+        "Sticker_4",
+        "Sticker_5",
+        "Sticker_6",
+        "Sticker_7",
+        "Sticker_8",
+        "Sticker_9",
+        "Sticker_10",
+        "Sticker_11",
+        "Sticker_12",
+        "Sticker_13"
+    },
+    ["Sticker_7"] = {
+        ["ModelAssetId"] = "Sticker_7",
+        ["Orientation"] = "0, 0, 0",
+        ["Sort"] = 93,
+        ["Position"] = "0, 0.47, -0.624",
+        ["Id"] = "Sticker_7",
+        ["NameLoc"] = "K_SELFIE_TYPE7",
+        ["Icon"] = "rbxassetid://15932827911",
+        ["New"] = "2024-01-17"
+    }
+}
