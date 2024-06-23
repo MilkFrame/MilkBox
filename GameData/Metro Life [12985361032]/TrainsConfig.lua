f2c18490f70267ff1e351a9f525e0ad78aeb87d9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:42:11 +1000

    Create TrainsConfig.lua

---
+++ GameData/Metro Life [12985361032]/TrainsConfig.lua
@@ -0,0 +1,76 @@
+data = {
+    ["TourBus"] = {
+        ["Type"] = "Bus",
+        ["Name"] = "TourBus",
+        ["MoveType"] = 4,
+        ["InitStation"] = 2,
+        ["TrackStation"] = {
+            {["IntervalTime"] = 10, ["StationPoint"] = 464, ["DecelerateDistance"] = 10, ["AccelerateDistance"] = 10},
+            {["IntervalTime"] = 10, ["StationPoint"] = 716, ["DecelerateDistance"] = 10, ["AccelerateDistance"] = 10},
+            {["IntervalTime"] = 10, ["StationPoint"] = 1395, ["DecelerateDistance"] = 10, ["AccelerateDistance"] = 10}
+        },
+        ["TrainCar"] = {
+            ["Friction"] = 0,
+            ["RotateAccelerate"] = 10,
+            ["SpaceBetween"] = 1,
+            ["CarsCount"] = 1,
+            ["MaxSpeed"] = 100,
+            ["Gravity"] = 0,
+            ["ParkSpeed"] = 1,
+            ["RotateSpeed"] = 3,
+            ["MinSpeed"] = 8
+        },
+        ["MaxHideDistance"] = 320
+    },
+    ["Metro"] = {
+        ["Type"] = "METRO",
+        ["Name"] = "TopiaMetro",
+        ["MoveType"] = 1,
+        ["InitStation"] = 5,
+        ["TrackStation"] = {
+            {["IntervalTime"] = 6, ["StationPoint"] = 101, ["DecelerateDistance"] = 50, ["AccelerateDistance"] = 30},
+            {["IntervalTime"] = 6, ["StationPoint"] = 379, ["DecelerateDistance"] = 50, ["AccelerateDistance"] = 30},
+            {["IntervalTime"] = 6, ["StationPoint"] = 763, ["DecelerateDistance"] = 50, ["AccelerateDistance"] = 30},
+            {["IntervalTime"] = 6, ["StationPoint"] = 998, ["DecelerateDistance"] = 50, ["AccelerateDistance"] = 30},
+            {["IntervalTime"] = 6, ["StationPoint"] = 1366, ["DecelerateDistance"] = 50, ["AccelerateDistance"] = 30},
+            {["IntervalTime"] = 6, ["StationPoint"] = 1660, ["DecelerateDistance"] = 50, ["AccelerateDistance"] = 30}
+        },
+        ["TrainCar"] = {
+            ["Friction"] = 0.18,
+            ["CarsCount"] = 3,
+            ["ParkSpeed"] = 1,
+            ["Gravity"] = 6.1,
+            ["SpaceBetween"] = 69.9,
+            ["MaxSpeed"] = 40,
+            ["MinSpeed"] = 25
+        },
+        ["MaxHideDistance"] = 320
+    },
+    ["Bus"] = {
+        ["Type"] = "Bus",
+        ["Name"] = "TopiaBus",
+        ["MoveType"] = 4,
+        ["InitStation"] = 6,
+        ["TrackStation"] = {
+            {["IntervalTime"] = 10, ["StationPoint"] = 25, ["DecelerateDistance"] = 10, ["AccelerateDistance"] = 10},
+            {["IntervalTime"] = 10, ["StationPoint"] = 161, ["DecelerateDistance"] = 10, ["AccelerateDistance"] = 10},
+            {["IntervalTime"] = 10, ["StationPoint"] = 613, ["DecelerateDistance"] = 10, ["AccelerateDistance"] = 10},
+            {["IntervalTime"] = 10, ["StationPoint"] = 995, ["DecelerateDistance"] = 10, ["AccelerateDistance"] = 10},
+            {["IntervalTime"] = 10, ["StationPoint"] = 1449, ["DecelerateDistance"] = 10, ["AccelerateDistance"] = 10},
+            {["IntervalTime"] = 10, ["StationPoint"] = 1754, ["DecelerateDistance"] = 10, ["AccelerateDistance"] = 10},
+            {["IntervalTime"] = 10, ["StationPoint"] = 1993, ["DecelerateDistance"] = 10, ["AccelerateDistance"] = 10}
+        },
+        ["TrainCar"] = {
+            ["Friction"] = 0,
+            ["RotateAccelerate"] = 10,
+            ["SpaceBetween"] = 1,
+            ["CarsCount"] = 1,
+            ["MaxSpeed"] = 100,
+            ["Gravity"] = 0,
+            ["ParkSpeed"] = 1,
+            ["RotateSpeed"] = 3,
+            ["MinSpeed"] = 8
+        },
+        ["MaxHideDistance"] = 320
+    }
+}
