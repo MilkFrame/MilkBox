3b31995bb4c3ba97ff5902a907ae061f6a12ca9e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:54:39 +1000

    Create ResCheckInRoomType.lua

---
+++ GameData/Metro Life [12985361032]/ResCheckInRoomType.lua
@@ -0,0 +1,147 @@
+data = {
+    ["Hotel_PaidA_1"] = {
+        ["AssetId"] = 14756861400,
+        ["RequirPass"] = "ResortPass",
+        ["IdName"] = "Hotel_PaidA_1",
+        ["Icon"] = "rbxassetid://14538757809",
+        ["Id"] = "Hotel_PaidA_1",
+        ["Name"] = "K_HOTEL_PASS_ROOM_1",
+        ["IsNew"] = "",
+        ["Des"] = ""
+    },
+    ["__index"] = {
+        "Apartment_FreeA_1",
+        "Apartment_FreeB_1",
+        "Apartment_FreeB_2",
+        "Apartment_PassA_1",
+        "Apartment_PassB_1",
+        "Apartment_PassC_1",
+        "Apartment2F",
+        "Hotel_FreeA_1",
+        "Hotel_FreeA_2",
+        "Hotel_FreeA_3",
+        "Hotel_PaidA_1",
+        "Hotel_PaidA_2",
+        "Hotel_PaidA_3"
+    },
+    ["Hotel_PaidA_3"] = {
+        ["AssetId"] = "",
+        ["RequirPass"] = "ResortPass",
+        ["IdName"] = "Hotel_PaidA_3",
+        ["Icon"] = "rbxassetid://16889503108",
+        ["Id"] = "Hotel_PaidA_3",
+        ["Name"] = "K_HOTEL_PASS_ROOM_3",
+        ["IsNew"] = "2024-04-03",
+        ["Des"] = ""
+    },
+    ["Apartment2F"] = {
+        ["AssetId"] = "",
+        ["RequirPass"] = "",
+        ["IdName"] = "",
+        ["Icon"] = "rbxassetid://13815174930",
+        ["Id"] = "Apartment2F",
+        ["Name"] = "",
+        ["IsNew"] = "",
+        ["Des"] = ""
+    },
+    ["Hotel_FreeA_3"] = {
+        ["AssetId"] = "",
+        ["RequirPass"] = "",
+        ["IdName"] = "Hotel_FreeA_3",
+        ["Icon"] = "rbxassetid://16889503766",
+        ["Id"] = "Hotel_FreeA_3",
+        ["Name"] = "K_HOTEL_ROOM_TYPE_3",
+        ["IsNew"] = "2024-04-03",
+        ["Des"] = ""
+    },
+    ["Hotel_FreeA_2"] = {
+        ["AssetId"] = 14756859758,
+        ["RequirPass"] = "",
+        ["IdName"] = "Hotel_FreeA_2",
+        ["Icon"] = "rbxassetid://14538757227",
+        ["Id"] = "Hotel_FreeA_2",
+        ["Name"] = "K_HOTEL_ROOM_TYPE_2",
+        ["IsNew"] = "",
+        ["Des"] = ""
+    },
+    ["Apartment_FreeA_1"] = {
+        ["AssetId"] = 14756846184,
+        ["RequirPass"] = "",
+        ["IdName"] = "Apartment_FreeA_1",
+        ["Icon"] = "rbxassetid://13805260602",
+        ["Id"] = "Apartment_FreeA_1",
+        ["Name"] = "K_APARTMENT_ROOM_TYPE_1",
+        ["IsNew"] = "",
+        ["Des"] = ""
+    },
+    ["Hotel_FreeA_1"] = {
+        ["AssetId"] = 14756858221,
+        ["RequirPass"] = "",
+        ["IdName"] = "Hotel_FreeA_1",
+        ["Icon"] = "rbxassetid://14538757435",
+        ["Id"] = "Hotel_FreeA_1",
+        ["Name"] = "K_HOTEL_ROOM_TYPE_1",
+        ["IsNew"] = "",
+        ["Des"] = ""
+    },
+    ["Apartment_PassB_1"] = {
+        ["AssetId"] = 14756853074,
+        ["RequirPass"] = "ApartmentPass",
+        ["IdName"] = "Apartment_PassB_1",
+        ["Icon"] = "rbxassetid://13805259962",
+        ["Id"] = "Apartment_PassB_1",
+        ["Name"] = "K_APARTMENT_FLOOR_7",
+        ["IsNew"] = "",
+        ["Des"] = ""
+    },
+    ["Hotel_PaidA_2"] = {
+        ["AssetId"] = 14756862191,
+        ["RequirPass"] = "ResortPass",
+        ["IdName"] = "Hotel_PaidA_2",
+        ["Icon"] = "rbxassetid://14538757612",
+        ["Id"] = "Hotel_PaidA_2",
+        ["Name"] = "K_HOTEL_PASS_ROOM_2",
+        ["IsNew"] = "",
+        ["Des"] = ""
+    },
+    ["Apartment_PassC_1"] = {
+        ["AssetId"] = 15572694951,
+        ["RequirPass"] = "ApartmentPass",
+        ["IdName"] = "Apartment_PassC_1",
+        ["Icon"] = "rbxassetid://14764208794",
+        ["Id"] = "Apartment_PassC_1",
+        ["Name"] = "K_APARTMENT_FLOOR_9",
+        ["IsNew"] = "2023-09-20",
+        ["Des"] = ""
+    },
+    ["Apartment_PassA_1"] = {
+        ["AssetId"] = 14756850358,
+        ["RequirPass"] = "ApartmentPass",
+        ["IdName"] = "Apartment_PassA_1",
+        ["Icon"] = "rbxassetid://13805260173",
+        ["Id"] = "Apartment_PassA_1",
+        ["Name"] = "K_APARTMENT_FLOOR_5",
+        ["IsNew"] = "",
+        ["Des"] = ""
+    },
+    ["Apartment_FreeB_1"] = {
+        ["AssetId"] = 14756847672,
+        ["RequirPass"] = "",
+        ["IdName"] = "Apartment_FreeB_1",
+        ["Icon"] = "rbxassetid://13805260368",
+        ["Id"] = "Apartment_FreeB_1",
+        ["Name"] = "K_APARTMENT_ROOM_TYPE_2",
+        ["IsNew"] = "",
+        ["Des"] = ""
+    },
+    ["Apartment_FreeB_2"] = {
+        ["AssetId"] = 14756848793,
+        ["RequirPass"] = "",
+        ["IdName"] = "Apartment_FreeB_2",
+        ["Icon"] = "rbxassetid://14764208923",
+        ["Id"] = "Apartment_FreeB_2",
+        ["Name"] = "K_APARTMENT_ROOM_TYPE_3",
+        ["IsNew"] = "2023-09-20",
+        ["Des"] = ""
+    }
+}
