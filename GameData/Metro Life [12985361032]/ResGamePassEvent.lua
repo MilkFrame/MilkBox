e5ef7a87e7e49af39e579483b88f913113994e56
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:54:27 +1000

    Create ResGamePassEvent.lua

---
+++ GameData/Metro Life [12985361032]/ResGamePassEvent.lua
@@ -0,0 +1,38 @@
+data = {
+    ["Christmas2022"] = {
+        ["Enabled"] = true,
+        ["TimerDesc"] = "K_PROMOTION_START_COUNTDOWN",
+        ["EndTime"] = "2022-12-30 8:00:00",
+        ["ChinaIcon"] = "rbxassetid://8306034899",
+        ["Id"] = "Christmas2022",
+        ["ChinaDetailIcon"] = "rbxassetid://8306035206",
+        ["IsNew"] = "2022-12-23",
+        ["Desc"] = "K_GAMEPASS_BLACKFRIDAY_DESC",
+        ["Sort"] = -1,
+        ["NameTag"] = "",
+        ["HideInList"] = "",
+        ["UpdateTime"] = "",
+        ["Discount"] = 0.5,
+        ["StartTime"] = "2022-12-1 8:00:00",
+        ["Icon"] = "rbxassetid://8306034899",
+        ["DetailIcon"] = "rbxassetid://8306035206"
+    },
+    ["__index"] = {"BlackFriday2022", "Christmas2022"},
+    ["BlackFriday2022"] = {
+        ["TimerDesc"] = "K_PROMOTION_START_COUNTDOWN",
+        ["EndTime"] = "2022-11-28 8:00:00",
+        ["ChinaIcon"] = "rbxassetid://8067559006",
+        ["Id"] = "BlackFriday2022",
+        ["ChinaDetailIcon"] = "rbxassetid://11632100754",
+        ["IsNew"] = "2021-11-23",
+        ["Desc"] = "K_GAMEPASS_BLACKFRIDAY_DESC",
+        ["Sort"] = -1,
+        ["NameTag"] = "",
+        ["HideInList"] = "",
+        ["UpdateTime"] = "",
+        ["Discount"] = 0.5,
+        ["StartTime"] = "2021-11-20 8:00:00",
+        ["Icon"] = "rbxassetid://8031564789",
+        ["DetailIcon"] = "rbxassetid://11632100754"
+    }
+}
