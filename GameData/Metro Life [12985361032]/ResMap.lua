fd08230f084b82d23daebd73ab3f8a5f41aff198
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:46:34 +1000

    Create ResMap.lua

---
+++ GameData/Metro Life [12985361032]/ResMap.lua
@@ -0,0 +1,484 @@
+data = {
+    ["Supermarket"] = {
+        ["IsNew"] = "",
+        ["Name"] = "Everyday Essentials",
+        ["Position"] = "1348,1534",
+        ["ID"] = "Supermarket",
+        ["NaviPosition"] = "399.893, 69.495, 299.241",
+        ["Icon"] = "rbxassetid://13400633440",
+        ["Size"] = ""
+    },
+    ["FireDepartment"] = {
+        ["IsNew"] = "",
+        ["Name"] = "Fire Department",
+        ["Position"] = "336,1206",
+        ["ID"] = "FireDepartment",
+        ["NaviPosition"] = "-1252.607, 65.495, -140.259",
+        ["Icon"] = "rbxassetid://13400632977",
+        ["Size"] = ""
+    },
+    ["ParkviewResidence"] = {
+        ["IsNew"] = "2023-6-30",
+        ["Name"] = "Parkview Residence",
+        ["Position"] = "1044,907",
+        ["ID"] = "ParkviewResidence",
+        ["NaviPosition"] = "-48.475, 95, -681",
+        ["Icon"] = "rbxassetid://13807281006",
+        ["Size"] = ""
+    },
+    ["Hospital"] = {
+        ["IsNew"] = "",
+        ["Name"] = "Harbor Health Center",
+        ["Position"] = "1428,887",
+        ["ID"] = "Hospital",
+        ["NaviPosition"] = "672.393, 65.495, -516.759",
+        ["Icon"] = "rbxassetid://13400633224",
+        ["Size"] = ""
+    },
+    ["Restaurant"] = {
+        ["IsNew"] = "",
+        ["Name"] = "La Vie en Rose",
+        ["Position"] = "1052,1418",
+        ["ID"] = "Restaurant",
+        ["NaviPosition"] = "-14.107, 65.495, 263.241",
+        ["Icon"] = "rbxassetid://13400634351",
+        ["Size"] = ""
+    },
+    ["EastShoreStation"] = {
+        ["IsNew"] = "2023-6-5",
+        ["Name"] = "East Shore Station",
+        ["Position"] = "2420,1498",
+        ["ID"] = "EastShoreStation",
+        ["NaviPosition"] = "2149.551, 64.819, 378.956",
+        ["Icon"] = "rbxassetid://13409660117",
+        ["Size"] = ""
+    },
+    ["OceanBreezeWaterPark"] = {
+        ["IsNew"] = "2024-07-23",
+        ["Name"] = "Ocean Breeze Water Park",
+        ["Position"] = "514,2369",
+        ["ID"] = "OceanBreezeWaterPark",
+        ["NaviPosition"] = "-831.715, 56.488, 1803.085",
+        ["Icon"] = "rbxassetid://17574602408",
+        ["Size"] = ""
+    },
+    ["HospitalStation"] = {
+        ["IsNew"] = "2023-11-23",
+        ["Name"] = "Hospital Station",
+        ["Position"] = "1208,962",
+        ["ID"] = "HospitalStation",
+        ["NaviPosition"] = "257.046, 68.676, -563.772",
+        ["Icon"] = "rbxassetid://15305114582",
+        ["Size"] = ""
+    },
+    ["Paradise Airport Station"] = {
+        ["IsNew"] = "2023-9-28",
+        ["Name"] = "Paradise Airport Station",
+        ["Position"] = "1680,2425",
+        ["ID"] = "Paradise Airport Station",
+        ["NaviPosition"] = "948.563, 65.127, 1830.253",
+        ["Icon"] = "rbxassetid://13409660117",
+        ["Size"] = ""
+    },
+    ["ParadiseHotelStop"] = {
+        ["IsNew"] = "2023-11-23",
+        ["Name"] = "Paradise Hotel Stop",
+        ["Position"] = "1068,2625",
+        ["ID"] = "ParadiseHotelStop",
+        ["NaviPosition"] = "48.351, 68.676, 2125.927",
+        ["Icon"] = "rbxassetid://15305114582",
+        ["Size"] = ""
+    },
+    ["GasStation"] = {
+        ["IsNew"] = "",
+        ["Name"] = "FuelPlus",
+        ["Position"] = "1308,1630",
+        ["ID"] = "GasStation",
+        ["NaviPosition"] = "342.893, 65.495, 558.741",
+        ["Icon"] = "rbxassetid://13400634566",
+        ["Size"] = ""
+    },
+    ["MetroAirport"] = {
+        ["IsNew"] = "2023-9-28",
+        ["Name"] = "Metro Airport",
+        ["Position"] = "408,183",
+        ["ID"] = "MetroAirport",
+        ["NaviPosition"] = "-1096, 88.744, -1658",
+        ["Icon"] = "rbxassetid://14696698602",
+        ["Size"] = ""
+    },
+    ["Sportclothes Shop"] = {
+        ["IsNew"] = "",
+        ["Name"] = "SwiftSport",
+        ["Position"] = "1124,1298",
+        ["ID"] = "Sportclothes Shop",
+        ["NaviPosition"] = "111.893, 65.495, 73.241",
+        ["Icon"] = "rbxassetid://13400633897",
+        ["Size"] = ""
+    },
+    ["ClothesShopDeluxe"] = {
+        ["IsNew"] = "",
+        ["Name"] = "Carter & Co. Bridal",
+        ["Position"] = "1000,1310",
+        ["ID"] = "ClothesShopDeluxe",
+        ["NaviPosition"] = "-86.607, 65.495, 95.741",
+        ["Icon"] = "rbxassetid://13400633978",
+        ["Size"] = ""
+    },
+    ["ParadiseHotel"] = {
+        ["IsNew"] = "2023-9-1",
+        ["Name"] = "Paradise Hotel",
+        ["Position"] = "884,2709",
+        ["ID"] = "ParadiseHotel",
+        ["NaviPosition"] = "-261.92, 63.988, 2204.73",
+        ["Icon"] = "rbxassetid://14466060916",
+        ["Size"] = ""
+    },
+    ["ChapelHillStation"] = {
+        ["IsNew"] = "2023-6-5",
+        ["Name"] = "Chapel Hill Station",
+        ["Position"] = "936,903",
+        ["ID"] = "ChapelHillStation",
+        ["NaviPosition"] = "-161.027, 92.627, -627.421",
+        ["Icon"] = "rbxassetid://13409660117",
+        ["Size"] = ""
+    },
+    ["School"] = {
+        ["IsNew"] = "",
+        ["Name"] = "Ascend High School",
+        ["Position"] = "1320,1242",
+        ["ID"] = "School",
+        ["NaviPosition"] = "412.893, 62.495, 55.241",
+        ["Icon"] = "rbxassetid://13400633384",
+        ["Size"] = ""
+    },
+    ["ParadiseAirportStop"] = {
+        ["IsNew"] = "2023-11-23",
+        ["Name"] = "Paradise Airport Stop",
+        ["Position"] = "1624,2537",
+        ["ID"] = "ParadiseAirportStop",
+        ["NaviPosition"] = "911.334, 68.676, 2025.461",
+        ["Icon"] = "rbxassetid://15305114582",
+        ["Size"] = ""
+    },
+    ["Coffee"] = {
+        ["IsNew"] = "",
+        ["Name"] = "Cuppa Culture",
+        ["Position"] = "836,1434",
+        ["ID"] = "Coffee",
+        ["NaviPosition"] = "-359.107, 65.495, 263.241",
+        ["Icon"] = "rbxassetid://13400634091",
+        ["Size"] = ""
+    },
+    ["Fastfoodrestaurant"] = {
+        ["IsNew"] = "",
+        ["Name"] = "FreshBites",
+        ["Position"] = "832,1142",
+        ["ID"] = "Fastfoodrestaurant",
+        ["NaviPosition"] = "-358.157, 62.25, -126.831",
+        ["Icon"] = "rbxassetid://13400633835",
+        ["Size"] = ""
+    },
+    ["ClothesShop"] = {
+        ["IsNew"] = "",
+        ["Name"] = "Aurora",
+        ["Position"] = "1068,1286",
+        ["ID"] = "ClothesShop",
+        ["NaviPosition"] = "8.893, 65.495, 95.741",
+        ["Icon"] = "rbxassetid://13400633897",
+        ["Size"] = ""
+    },
+    ["Metroairport Station"] = {
+        ["IsNew"] = "2023-9-28",
+        ["Name"] = "Metro Airport Station",
+        ["Position"] = "276,231",
+        ["ID"] = "Metroairport Station",
+        ["NaviPosition"] = "-1275.306, 91.871, -1641.503",
+        ["Icon"] = "rbxassetid://13409660117",
+        ["Size"] = ""
+    },
+    ["Revive&RenewSpa"] = {
+        ["IsNew"] = "2023-7-7",
+        ["Name"] = "Revive & Renew Spa",
+        ["Position"] = "648,1430",
+        ["ID"] = "Revive&RenewSpa",
+        ["NaviPosition"] = "-669.95, 69.35, 282.7",
+        ["Icon"] = "rbxassetid://13887952086",
+        ["Size"] = ""
+    },
+    ["BearBouquets"] = {
+        ["IsNew"] = "2023-12-07",
+        ["Name"] = "Bear Bouquets",
+        ["Position"] = "560,1430",
+        ["ID"] = "BearBouquets",
+        ["NaviPosition"] = "-790.257, 63.75, 290.125",
+        ["Icon"] = "rbxassetid://15311306857",
+        ["Size"] = ""
+    },
+    ["MetroHall"] = {
+        ["IsNew"] = "2024-04-10",
+        ["Name"] = "Metro Hall",
+        ["Position"] = "893,760",
+        ["ID"] = "MetroHall",
+        ["NaviPosition"] = "-295.977, 91.311, -845.9",
+        ["Icon"] = "rbxassetid://16823757000",
+        ["Size"] = ""
+    },
+    ["BeachfrontBlissPark"] = {
+        ["IsNew"] = "2024-05-23",
+        ["Name"] = "Beachfront Bliss Park",
+        ["Position"] = "2427,1614",
+        ["ID"] = "BeachfrontBlissPark",
+        ["NaviPosition"] = "2203.64, 62.5, 462.61",
+        ["Icon"] = "rbxassetid://17508231538",
+        ["Size"] = ""
+    },
+    ["SeagullStreet"] = {
+        ["IsNew"] = "2023-11-23",
+        ["Name"] = "Seagull Street Stop",
+        ["Position"] = "2448,1114",
+        ["ID"] = "SeagullStreet",
+        ["NaviPosition"] = "2247.059, 68.676, -276.691",
+        ["Icon"] = "rbxassetid://15305114582",
+        ["Size"] = ""
+    },
+    ["HarborStreet"] = {
+        ["IsNew"] = "2023-11-23",
+        ["Name"] = "Harbor Street Stop",
+        ["Position"] = "2748,1358",
+        ["ID"] = "HarborStreet",
+        ["NaviPosition"] = "2744.475, 68.676, 98.609",
+        ["Icon"] = "rbxassetid://15305114582",
+        ["Size"] = ""
+    },
+    ["ParkAvenue"] = {
+        ["IsNew"] = "2023-11-23",
+        ["Name"] = "Park Avenue Stop",
+        ["Position"] = "1044,1538",
+        ["ID"] = "ParkAvenue",
+        ["NaviPosition"] = "9.348, 68.676, 391.445",
+        ["Icon"] = "rbxassetid://15305114582",
+        ["Size"] = ""
+    },
+    ["StationRoad"] = {
+        ["IsNew"] = "2023-11-23",
+        ["Name"] = "Station Road Stop",
+        ["Position"] = "480,1222",
+        ["ID"] = "StationRoad",
+        ["NaviPosition"] = "-962.724, 68.676, -80.209",
+        ["Icon"] = "rbxassetid://15305114582",
+        ["Size"] = ""
+    },
+    ["Carwash"] = {
+        ["IsNew"] = "",
+        ["Name"] = "Turbo Wash Express",
+        ["Position"] = "1480,1638",
+        ["ID"] = "Carwash",
+        ["NaviPosition"] = "633.393, 65.495, 593.241",
+        ["Icon"] = "rbxassetid://13400634481",
+        ["Size"] = ""
+    },
+    ["__index"] = {
+        "Bakery",
+        "Bank",
+        "Carwash",
+        "ClothesShop",
+        "ClothesShopDeluxe",
+        "Coffee",
+        "ConvenienceStore",
+        "DayCare",
+        "Dessertcolddrink",
+        "Fastfoodrestaurant",
+        "FireDepartment",
+        "GasStation",
+        "HairSalon",
+        "Hospital",
+        "NewChurch",
+        "Police",
+        "Restaurant",
+        "School",
+        "Sportclothes Shop",
+        "Supermarket",
+        "ParadiseIslandStation",
+        "MetroParkStation",
+        "Metroairport Station",
+        "ChapelHillStation",
+        "Paradise Airport Station",
+        "EastShoreStation",
+        "MetroFit",
+        "ParkviewResidence",
+        "Revive&RenewSpa",
+        "MetroMall2F",
+        "ParadiseHotel",
+        "MetroAirport",
+        "ParadiseAirport",
+        "StarlightClub",
+        "ParkAvenue",
+        "HopeRoad",
+        "SchoolStation",
+        "StationRoad",
+        "NorthCoastalHighWay",
+        "HospitalStation",
+        "HarborStreet",
+        "SeagullStreet",
+        "ParadiseHotelStop",
+        "ParadiseAirportStop",
+        "BearBouquets",
+        "MetroHall",
+        "BeachfrontBlissPark",
+        "OceanBreezeWaterPark"
+    },
+    ["Bank"] = {
+        ["IsNew"] = "",
+        ["Name"] = "Sunrise Bank",
+        ["Position"] = "684,1286",
+        ["ID"] = "Bank",
+        ["NaviPosition"] = "-624.607, 65.495, 12.741",
+        ["Icon"] = "rbxassetid://13400633029",
+        ["Size"] = ""
+    },
+    ["DayCare"] = {
+        ["IsNew"] = "",
+        ["Name"] = "Honeycomb Kids",
+        ["Position"] = "1268,847",
+        ["ID"] = "DayCare",
+        ["NaviPosition"] = "321.393, 65.495, -670.259",
+        ["Icon"] = "rbxassetid://13400633309",
+        ["Size"] = ""
+    },
+    ["ParadiseIslandStation"] = {
+        ["IsNew"] = "2023-6-5",
+        ["Name"] = "Paradise Island Station",
+        ["Position"] = "648,2621",
+        ["ID"] = "ParadiseIslandStation",
+        ["NaviPosition"] = "-626.021, 65.115, 2113.063",
+        ["Icon"] = "rbxassetid://13409660117",
+        ["Size"] = ""
+    },
+    ["Police"] = {
+        ["IsNew"] = "",
+        ["Name"] = "Police Department",
+        ["Position"] = "380,1394",
+        ["ID"] = "Police",
+        ["NaviPosition"] = "-1120.107, 65.495, 263.241",
+        ["Icon"] = "rbxassetid://13400633106",
+        ["Size"] = ""
+    },
+    ["StarlightClub"] = {
+        ["IsNew"] = "2023-10-19",
+        ["Name"] = "Starlight Club",
+        ["Position"] = "697,1249",
+        ["ID"] = "StarlightClub",
+        ["NaviPosition"] = "-628.337, 64.336, -118.972",
+        ["Icon"] = "rbxassetid://14764208676",
+        ["Size"] = ""
+    },
+    ["HopeRoad"] = {
+        ["IsNew"] = "2023-11-23",
+        ["Name"] = "Hope Road Stop",
+        ["Position"] = "1208,1590",
+        ["ID"] = "HopeRoad",
+        ["NaviPosition"] = "195.028, 68.676, 490.754",
+        ["Icon"] = "rbxassetid://15305114582",
+        ["Size"] = ""
+    },
+    ["MetroMall2F"] = {
+        ["IsNew"] = "2023-8-5",
+        ["Name"] = "Metro Mall",
+        ["Position"] = "744,1194",
+        ["ID"] = "MetroMall2F",
+        ["NaviPosition"] = "-496, 62.5, -153.55",
+        ["Icon"] = "rbxassetid://13953164848",
+        ["Size"] = ""
+    },
+    ["NorthCoastalHighWay"] = {
+        ["IsNew"] = "2023-11-23",
+        ["Name"] = "North Coastal Stop",
+        ["Position"] = "576,559",
+        ["ID"] = "NorthCoastalHighWay",
+        ["NaviPosition"] = "-751.165, 95.501, -1191.182",
+        ["Icon"] = "rbxassetid://15305114582",
+        ["Size"] = ""
+    },
+    ["ConvenienceStore"] = {
+        ["IsNew"] = "",
+        ["Name"] = "EZMart",
+        ["Position"] = "924,1154",
+        ["ID"] = "ConvenienceStore",
+        ["NaviPosition"] = "-255.129, 62.25, -126.831",
+        ["Icon"] = "rbxassetid://13400633747",
+        ["Size"] = ""
+    },
+    ["Dessertcolddrink"] = {
+        ["IsNew"] = "",
+        ["Name"] = "Sugar Rush",
+        ["Position"] = "1132,1418",
+        ["ID"] = "Dessertcolddrink",
+        ["NaviPosition"] = "148.893, 65.495, 278.741",
+        ["Icon"] = "rbxassetid://13400634233",
+        ["Size"] = ""
+    },
+    ["MetroFit"] = {
+        ["IsNew"] = "2023-6-25",
+        ["Name"] = "MetroFit",
+        ["Position"] = "896,1302",
+        ["ID"] = "MetroFit",
+        ["NaviPosition"] = "-257.694, 62.5, 86.56",
+        ["Icon"] = "rbxassetid://13667099791",
+        ["Size"] = ""
+    },
+    ["ParadiseAirport"] = {
+        ["IsNew"] = "2023-9-28",
+        ["Name"] = "Paradise Airport",
+        ["Position"] = "1780,2645",
+        ["ID"] = "ParadiseAirport",
+        ["NaviPosition"] = "1042, 62.993, 2132",
+        ["Icon"] = "rbxassetid://14696698602",
+        ["Size"] = ""
+    },
+    ["Bakery"] = {
+        ["IsNew"] = "",
+        ["Name"] = "Flourish Bakery",
+        ["Position"] = "916,1426",
+        ["ID"] = "Bakery",
+        ["NaviPosition"] = "-219.107, 65.495, 263.241",
+        ["Icon"] = "rbxassetid://13400634162",
+        ["Size"] = ""
+    },
+    ["SchoolStation"] = {
+        ["IsNew"] = "2023-11-23",
+        ["Name"] = "School Station",
+        ["Position"] = "1364,1394",
+        ["ID"] = "SchoolStation",
+        ["NaviPosition"] = "519.053, 68.676, 171.33",
+        ["Icon"] = "rbxassetid://15305114582",
+        ["Size"] = ""
+    },
+    ["MetroParkStation"] = {
+        ["IsNew"] = "2023-6-5",
+        ["Name"] = "Metro Park Station",
+        ["Position"] = "720,1542",
+        ["ID"] = "MetroParkStation",
+        ["NaviPosition"] = "-622.985, 62.5, 387.971",
+        ["Icon"] = "rbxassetid://13409660117",
+        ["Size"] = ""
+    },
+    ["NewChurch"] = {
+        ["IsNew"] = "",
+        ["Name"] = "Grace Church",
+        ["Position"] = "1084,1190",
+        ["ID"] = "NewChurch",
+        ["NaviPosition"] = "-69.107, 62.495, -180.259",
+        ["Icon"] = "rbxassetid://13400923109",
+        ["Size"] = ""
+    },
+    ["HairSalon"] = {
+        ["IsNew"] = "",
+        ["Name"] = "Moda Hair Studio",
+        ["Position"] = "716,1422",
+        ["ID"] = "HairSalon",
+        ["NaviPosition"] = "-527.607, 65.495, 263.241",
+        ["Icon"] = "rbxassetid://13400633588",
+        ["Size"] = ""
+    }
+}
