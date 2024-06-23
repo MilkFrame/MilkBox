842fc763d6c47caabf036b0891e918cf8bd31a89
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:58:53 +1000

    Create ResFurnitureType.lua

---
+++ GameData/Metro Life [12985361032]/ResFurnitureType.lua
@@ -0,0 +1,148 @@
+data = {
+    ["DiningTable_L"] = {
+        ["ScaleMax"] = 2,
+        ["Category"] = "DiningTable",
+        ["Id"] = "DiningTable_L",
+        ["ScaleMin"] = 0.5,
+        ["Size"] = 3
+    },
+    ["TableDecor_L"] = {
+        ["ScaleMax"] = 2,
+        ["Category"] = "TableDecor",
+        ["Id"] = "TableDecor_L",
+        ["ScaleMin"] = 0.5,
+        ["Size"] = 3
+    },
+    ["Table_M"] = {["ScaleMax"] = 2.5, ["Category"] = "Table", ["Id"] = "Table_M", ["ScaleMin"] = 0.5, ["Size"] = 2},
+    ["Sofa_S"] = {["ScaleMax"] = 3, ["Category"] = "Sofa", ["Id"] = "Sofa_S", ["ScaleMin"] = 0.5, ["Size"] = 1},
+    ["WallDecor_L"] = {
+        ["ScaleMax"] = 2,
+        ["Category"] = "WallDecor",
+        ["Id"] = "WallDecor_L",
+        ["ScaleMin"] = 0.5,
+        ["Size"] = 3
+    },
+    ["FloorDecor_L"] = {
+        ["ScaleMax"] = 2,
+        ["Category"] = "FloorDecor",
+        ["Id"] = "FloorDecor_L",
+        ["ScaleMin"] = 0.5,
+        ["Size"] = 3
+    },
+    ["Bed_S"] = {["ScaleMax"] = 3, ["Category"] = "Bed", ["Id"] = "Bed_S", ["ScaleMin"] = 0.5, ["Size"] = 1},
+    ["TableDecor_M"] = {
+        ["ScaleMax"] = 2.5,
+        ["Category"] = "TableDecor",
+        ["Id"] = "TableDecor_M",
+        ["ScaleMin"] = 0.5,
+        ["Size"] = 2
+    },
+    ["Sofa_M"] = {["ScaleMax"] = 2.5, ["Category"] = "Sofa", ["Id"] = "Sofa_M", ["ScaleMin"] = 0.5, ["Size"] = 2},
+    ["DiningTable_M"] = {
+        ["ScaleMax"] = 2.5,
+        ["Category"] = "DiningTable",
+        ["Id"] = "DiningTable_M",
+        ["ScaleMin"] = 0.5,
+        ["Size"] = 2
+    },
+    ["Chair_M"] = {["ScaleMax"] = 2.5, ["Category"] = "Chair", ["Id"] = "Chair_M", ["ScaleMin"] = 0.5, ["Size"] = 2},
+    ["__index"] = {
+        "Bed_S",
+        "Bed_M",
+        "Bed_L",
+        "Chair_S",
+        "Chair_M",
+        "DiningTable_S",
+        "DiningTable_M",
+        "DiningTable_L",
+        "FloorDecor_S",
+        "FloorDecor_M",
+        "FloorDecor_L",
+        "Mail_S",
+        "Nightstand_M",
+        "Signboard_S",
+        "Sofa_L",
+        "Sofa_M",
+        "Sofa_S",
+        "Table_S",
+        "Table_M",
+        "Table_L",
+        "TableDecor_S",
+        "TableDecor_M",
+        "TableDecor_L",
+        "TrashCan_S",
+        "WallDecor_S",
+        "WallDecor_M",
+        "WallDecor_L"
+    },
+    ["TableDecor_S"] = {
+        ["ScaleMax"] = 3,
+        ["Category"] = "TableDecor",
+        ["Id"] = "TableDecor_S",
+        ["ScaleMin"] = 0.5,
+        ["Size"] = 1
+    },
+    ["Bed_M"] = {["ScaleMax"] = 2.5, ["Category"] = "Bed", ["Id"] = "Bed_M", ["ScaleMin"] = 0.5, ["Size"] = 2},
+    ["WallDecor_M"] = {
+        ["ScaleMax"] = 2.5,
+        ["Category"] = "WallDecor",
+        ["Id"] = "WallDecor_M",
+        ["ScaleMin"] = 0.5,
+        ["Size"] = 2
+    },
+    ["FloorDecor_M"] = {
+        ["ScaleMax"] = 2.5,
+        ["Category"] = "FloorDecor",
+        ["Id"] = "FloorDecor_M",
+        ["ScaleMin"] = 0.5,
+        ["Size"] = 2
+    },
+    ["WallDecor_S"] = {
+        ["ScaleMax"] = 3,
+        ["Category"] = "WallDecor",
+        ["Id"] = "WallDecor_S",
+        ["ScaleMin"] = 0.5,
+        ["Size"] = 1
+    },
+    ["Signboard_S"] = {
+        ["ScaleMax"] = 3,
+        ["Category"] = "Signboard",
+        ["Id"] = "Signboard_S",
+        ["ScaleMin"] = 0.5,
+        ["Size"] = 1
+    },
+    ["DiningTable_S"] = {
+        ["ScaleMax"] = 3,
+        ["Category"] = "DiningTable",
+        ["Id"] = "DiningTable_S",
+        ["ScaleMin"] = 0.5,
+        ["Size"] = 1
+    },
+    ["Table_L"] = {["ScaleMax"] = 2, ["Category"] = "Table", ["Id"] = "Table_L", ["ScaleMin"] = 0.5, ["Size"] = 3},
+    ["TrashCan_S"] = {
+        ["ScaleMax"] = 3,
+        ["Category"] = "TrashCan",
+        ["Id"] = "TrashCan_S",
+        ["ScaleMin"] = 0.5,
+        ["Size"] = 1
+    },
+    ["Sofa_L"] = {["ScaleMax"] = 2, ["Category"] = "Sofa", ["Id"] = "Sofa_L", ["ScaleMin"] = 0.5, ["Size"] = 3},
+    ["Table_S"] = {["ScaleMax"] = 3, ["Category"] = "Table", ["Id"] = "Table_S", ["ScaleMin"] = 0.5, ["Size"] = 1},
+    ["Nightstand_M"] = {
+        ["ScaleMax"] = 2.5,
+        ["Category"] = "Nightstand",
+        ["Id"] = "Nightstand_M",
+        ["ScaleMin"] = 0.5,
+        ["Size"] = 2
+    },
+    ["Mail_S"] = {["ScaleMax"] = 3, ["Category"] = "Mail", ["Id"] = "Mail_S", ["ScaleMin"] = 0.5, ["Size"] = 1},
+    ["FloorDecor_S"] = {
+        ["ScaleMax"] = 3,
+        ["Category"] = "FloorDecor",
+        ["Id"] = "FloorDecor_S",
+        ["ScaleMin"] = 0.5,
+        ["Size"] = 1
+    },
+    ["Chair_S"] = {["ScaleMax"] = 3, ["Category"] = "Chair", ["Id"] = "Chair_S", ["ScaleMin"] = 0.5, ["Size"] = 1},
+    ["Bed_L"] = {["ScaleMax"] = 2, ["Category"] = "Bed", ["Id"] = "Bed_L", ["ScaleMin"] = 0.5, ["Size"] = 3}
+}
