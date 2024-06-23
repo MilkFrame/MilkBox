5b4dd4138d4d9bd41560118537c6a23fbfad5329
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:57:26 +1000

    Create ResChatMemeInfo.lua

---
+++ GameData/Metro Life [12985361032]/ResChatMemeInfo.lua
@@ -0,0 +1,130 @@
+data = {
+    ["ChatMeme_VIP_05"] = {
+        ["Id"] = "ChatMeme_VIP_05",
+        ["Order"] = "5",
+        ["Icon"] = "rbxassetid://16651794406",
+        ["Group"] = "ChatMemeGroup_VIP"
+    },
+    ["ChatMeme_Product_03"] = {
+        ["Id"] = "ChatMeme_Product_03",
+        ["Order"] = "3",
+        ["Icon"] = "rbxassetid://17279471297",
+        ["Group"] = "ChatMemeGroup_Product_01"
+    },
+    ["ChatMeme_Free_01"] = {
+        ["Id"] = "ChatMeme_Free_01",
+        ["Order"] = "1",
+        ["Icon"] = "rbxassetid://16459423819",
+        ["Group"] = "ChatMemeGroup_Free_01"
+    },
+    ["ChatMeme_VIP_06"] = {
+        ["Id"] = "ChatMeme_VIP_06",
+        ["Order"] = "6",
+        ["Icon"] = "rbxassetid://16651794357",
+        ["Group"] = "ChatMemeGroup_VIP"
+    },
+    ["ChatMeme_VIP_02"] = {
+        ["Id"] = "ChatMeme_VIP_02",
+        ["Order"] = "2",
+        ["Icon"] = "rbxassetid://16651794774",
+        ["Group"] = "ChatMemeGroup_VIP"
+    },
+    ["ChatMeme_VIP_03"] = {
+        ["Id"] = "ChatMeme_VIP_03",
+        ["Order"] = "3",
+        ["Icon"] = "rbxassetid://16651794650",
+        ["Group"] = "ChatMemeGroup_VIP"
+    },
+    ["ChatMeme_Free_04"] = {
+        ["Id"] = "ChatMeme_Free_04",
+        ["Order"] = "4",
+        ["Icon"] = "rbxassetid://16459423383",
+        ["Group"] = "ChatMemeGroup_Free_01"
+    },
+    ["ChatMeme_Product_02"] = {
+        ["Id"] = "ChatMeme_Product_02",
+        ["Order"] = "2",
+        ["Icon"] = "rbxassetid://17279471533",
+        ["Group"] = "ChatMemeGroup_Product_01"
+    },
+    ["ChatMeme_Free_03"] = {
+        ["Id"] = "ChatMeme_Free_03",
+        ["Order"] = "3",
+        ["Icon"] = "rbxassetid://16459423533",
+        ["Group"] = "ChatMemeGroup_Free_01"
+    },
+    ["__index"] = {
+        "ChatMeme_Free_01",
+        "ChatMeme_Free_02",
+        "ChatMeme_Free_03",
+        "ChatMeme_Free_04",
+        "ChatMeme_Free_05",
+        "ChatMeme_Free_06",
+        "ChatMeme_VIP_01",
+        "ChatMeme_VIP_02",
+        "ChatMeme_VIP_03",
+        "ChatMeme_VIP_04",
+        "ChatMeme_VIP_05",
+        "ChatMeme_VIP_06",
+        "ChatMeme_Product_01",
+        "ChatMeme_Product_02",
+        "ChatMeme_Product_03",
+        "ChatMeme_Product_04",
+        "ChatMeme_Product_05",
+        "ChatMeme_Product_06"
+    },
+    ["ChatMeme_VIP_01"] = {
+        ["Id"] = "ChatMeme_VIP_01",
+        ["Order"] = "1",
+        ["Icon"] = "rbxassetid://16651794909",
+        ["Group"] = "ChatMemeGroup_VIP"
+    },
+    ["ChatMeme_Product_06"] = {
+        ["Id"] = "ChatMeme_Product_06",
+        ["Order"] = "6",
+        ["Icon"] = "rbxassetid://17279470983",
+        ["Group"] = "ChatMemeGroup_Product_01"
+    },
+    ["ChatMeme_Product_05"] = {
+        ["Id"] = "ChatMeme_Product_05",
+        ["Order"] = "5",
+        ["Icon"] = "rbxassetid://17279471104",
+        ["Group"] = "ChatMemeGroup_Product_01"
+    },
+    ["ChatMeme_Free_06"] = {
+        ["Id"] = "ChatMeme_Free_06",
+        ["Order"] = "6",
+        ["Icon"] = "rbxassetid://16459423073",
+        ["Group"] = "ChatMemeGroup_Free_01"
+    },
+    ["ChatMeme_Product_04"] = {
+        ["Id"] = "ChatMeme_Product_04",
+        ["Order"] = "4",
+        ["Icon"] = "rbxassetid://17279471202",
+        ["Group"] = "ChatMemeGroup_Product_01"
+    },
+    ["ChatMeme_Product_01"] = {
+        ["Id"] = "ChatMeme_Product_01",
+        ["Order"] = "1",
+        ["Icon"] = "rbxassetid://17279471397",
+        ["Group"] = "ChatMemeGroup_Product_01"
+    },
+    ["ChatMeme_Free_05"] = {
+        ["Id"] = "ChatMeme_Free_05",
+        ["Order"] = "5",
+        ["Icon"] = "rbxassetid://16459423204",
+        ["Group"] = "ChatMemeGroup_Free_01"
+    },
+    ["ChatMeme_VIP_04"] = {
+        ["Id"] = "ChatMeme_VIP_04",
+        ["Order"] = "4",
+        ["Icon"] = "rbxassetid://16651794522",
+        ["Group"] = "ChatMemeGroup_VIP"
+    },
+    ["ChatMeme_Free_02"] = {
+        ["Id"] = "ChatMeme_Free_02",
+        ["Order"] = "2",
+        ["Icon"] = "rbxassetid://16459423643",
+        ["Group"] = "ChatMemeGroup_Free_01"
+    }
+}
