6bcb935e7b6dd78c5b5ff2a8410382986f3e4097
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:51:15 +1000

    Create AnimationAsset.lua

---
+++ GameData/Metro Life [12985361032]/AnimationAsset.lua
@@ -0,0 +1,172 @@
+data = {
+    ["Sit_03"] = {
+        ["Id"] = "Sit_03",
+        ["LowerBodyAssetId"] = "Sit_03_L",
+        ["UpperBodyAssetId"] = "Sit_03_U",
+        ["FullBodyAssetId"] = ""
+    },
+    ["Sit_18"] = {
+        ["Id"] = "Sit_18",
+        ["LowerBodyAssetId"] = "",
+        ["UpperBodyAssetId"] = "",
+        ["FullBodyAssetId"] = "Sit_18"
+    },
+    ["Sit_01"] = {
+        ["Id"] = "Sit_01",
+        ["LowerBodyAssetId"] = "Sit_01_L",
+        ["UpperBodyAssetId"] = "Sit_01_U",
+        ["FullBodyAssetId"] = ""
+    },
+    ["__index"] = {
+        "Sit_00",
+        "Sit_01",
+        "Sit_02",
+        "Sit_03",
+        "Sit_04",
+        "Sit_05",
+        "Sit_06",
+        "Sit_07",
+        "Sit_08",
+        "Sit_09",
+        "Sit_10",
+        "Sit_11",
+        "Sit_12",
+        "Sit_13",
+        "Sit_14",
+        "Sit_15",
+        "Sit_16",
+        "Sit_17",
+        "Sit_18",
+        "Sit_19",
+        "Aerial_Yoga_1",
+        "Aerial_Yoga_2",
+        "Bike_Idle",
+        "Bike_Run"
+    },
+    ["Sit_17"] = {
+        ["Id"] = "Sit_17",
+        ["LowerBodyAssetId"] = "",
+        ["UpperBodyAssetId"] = "",
+        ["FullBodyAssetId"] = "Sit_17"
+    },
+    ["Sit_09"] = {
+        ["Id"] = "Sit_09",
+        ["LowerBodyAssetId"] = "Sit_09_L",
+        ["UpperBodyAssetId"] = "Sit_09_U",
+        ["FullBodyAssetId"] = ""
+    },
+    ["Aerial_Yoga_1"] = {
+        ["Id"] = "Aerial_Yoga_1",
+        ["LowerBodyAssetId"] = "",
+        ["UpperBodyAssetId"] = "",
+        ["FullBodyAssetId"] = "Aerial_Yoga_1"
+    },
+    ["Sit_15"] = {
+        ["Id"] = "Sit_15",
+        ["LowerBodyAssetId"] = "",
+        ["UpperBodyAssetId"] = "",
+        ["FullBodyAssetId"] = "Sit_15"
+    },
+    ["Bike_Run"] = {
+        ["Id"] = "Bike_Run",
+        ["LowerBodyAssetId"] = "",
+        ["UpperBodyAssetId"] = "",
+        ["FullBodyAssetId"] = "Bike_Run"
+    },
+    ["Bike_Idle"] = {
+        ["Id"] = "Bike_Idle",
+        ["LowerBodyAssetId"] = "",
+        ["UpperBodyAssetId"] = "",
+        ["FullBodyAssetId"] = "Bike_Idle"
+    },
+    ["Aerial_Yoga_2"] = {
+        ["Id"] = "Aerial_Yoga_2",
+        ["LowerBodyAssetId"] = "",
+        ["UpperBodyAssetId"] = "",
+        ["FullBodyAssetId"] = "Aerial_Yoga_2"
+    },
+    ["Sit_05"] = {
+        ["Id"] = "Sit_05",
+        ["LowerBodyAssetId"] = "Sit_05_L",
+        ["UpperBodyAssetId"] = "Sit_05_U",
+        ["FullBodyAssetId"] = ""
+    },
+    ["Sit_07"] = {
+        ["Id"] = "Sit_07",
+        ["LowerBodyAssetId"] = "Sit_07_L",
+        ["UpperBodyAssetId"] = "Sit_07_U",
+        ["FullBodyAssetId"] = ""
+    },
+    ["Sit_19"] = {
+        ["Id"] = "Sit_19",
+        ["LowerBodyAssetId"] = "",
+        ["UpperBodyAssetId"] = "",
+        ["FullBodyAssetId"] = "Sit_19"
+    },
+    ["Sit_00"] = {
+        ["Id"] = "Sit_00",
+        ["LowerBodyAssetId"] = "",
+        ["UpperBodyAssetId"] = "",
+        ["FullBodyAssetId"] = "Sit_00"
+    },
+    ["Sit_10"] = {
+        ["Id"] = "Sit_10",
+        ["LowerBodyAssetId"] = "",
+        ["UpperBodyAssetId"] = "",
+        ["FullBodyAssetId"] = "Sit_10"
+    },
+    ["Sit_16"] = {
+        ["Id"] = "Sit_16",
+        ["LowerBodyAssetId"] = "",
+        ["UpperBodyAssetId"] = "",
+        ["FullBodyAssetId"] = "Sit_16"
+    },
+    ["Sit_13"] = {
+        ["Id"] = "Sit_13",
+        ["LowerBodyAssetId"] = "",
+        ["UpperBodyAssetId"] = "",
+        ["FullBodyAssetId"] = "Sit_13"
+    },
+    ["Sit_12"] = {
+        ["Id"] = "Sit_12",
+        ["LowerBodyAssetId"] = "",
+        ["UpperBodyAssetId"] = "",
+        ["FullBodyAssetId"] = "Sit_12"
+    },
+    ["Sit_11"] = {
+        ["Id"] = "Sit_11",
+        ["LowerBodyAssetId"] = "",
+        ["UpperBodyAssetId"] = "",
+        ["FullBodyAssetId"] = "Sit_11"
+    },
+    ["Sit_04"] = {
+        ["Id"] = "Sit_04",
+        ["LowerBodyAssetId"] = "",
+        ["UpperBodyAssetId"] = "",
+        ["FullBodyAssetId"] = "Sit_04"
+    },
+    ["Sit_14"] = {
+        ["Id"] = "Sit_14",
+        ["LowerBodyAssetId"] = "",
+        ["UpperBodyAssetId"] = "",
+        ["FullBodyAssetId"] = "Sit_14"
+    },
+    ["Sit_08"] = {
+        ["Id"] = "Sit_08",
+        ["LowerBodyAssetId"] = "Sit_08_L",
+        ["UpperBodyAssetId"] = "Sit_08_U",
+        ["FullBodyAssetId"] = ""
+    },
+    ["Sit_06"] = {
+        ["Id"] = "Sit_06",
+        ["LowerBodyAssetId"] = "",
+        ["UpperBodyAssetId"] = "",
+        ["FullBodyAssetId"] = "Sit_06"
+    },
+    ["Sit_02"] = {
+        ["Id"] = "Sit_02",
+        ["LowerBodyAssetId"] = "Sit_02_L",
+        ["UpperBodyAssetId"] = "Sit_02_U",
+        ["FullBodyAssetId"] = ""
+    }
+}
