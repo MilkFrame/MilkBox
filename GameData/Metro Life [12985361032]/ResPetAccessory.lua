ea7348d5af0b0a41674b67dfac120f05b3e0df72
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:58:41 +1000

    Create ResPetAccessory.lua

---
+++ GameData/Metro Life [12985361032]/ResPetAccessory.lua
@@ -0,0 +1,25 @@
+data = {
+    ["PetAccessory_001"] = {
+        ["Id"] = "PetAccessory_001",
+        ["IsNew"] = "2024-04-25",
+        ["IsUpdate"] = "",
+        ["Sort"] = 99,
+        ["Model"] = "PetAccessory_001",
+        ["RequireProduct"] = "",
+        ["Disable"] = "",
+        ["Icon"] = "rbxassetid://17087970330",
+        ["DecorPoint"] = "Accessory_Neck"
+    },
+    ["__index"] = {"PetAccessory_001", "PetAccessory_002"},
+    ["PetAccessory_002"] = {
+        ["Id"] = "PetAccessory_002",
+        ["IsNew"] = "2024-04-25",
+        ["IsUpdate"] = "",
+        ["Sort"] = 98,
+        ["Model"] = "PetAccessory_002",
+        ["RequireProduct"] = "",
+        ["Disable"] = "",
+        ["Icon"] = "rbxassetid://17087970243",
+        ["DecorPoint"] = "Accessory_Back"
+    }
+}
