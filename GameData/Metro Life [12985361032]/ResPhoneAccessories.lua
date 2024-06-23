b1cfbb3030c7700ccec26cbb13fcfb99026a07bd
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:55:56 +1000

    Create ResPhoneAccessories.lua

---
+++ GameData/Metro Life [12985361032]/ResPhoneAccessories.lua
@@ -0,0 +1,59 @@
+data = {
+    ["PP05"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_05pendant",
+        ["Id"] = "PP05",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://15165221617",
+        ["OwnCheck"] = true
+    },
+    ["PP07"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_07pendant",
+        ["Id"] = "PP07",
+        ["IsNew"] = "2029-06-30",
+        ["Icon"] = "rbxassetid://16083646679",
+        ["OwnCheck"] = false
+    },
+    ["__index"] = {"PP01", "PP02", "PP03", "PP04", "PP05", "PP06", "PP07"},
+    ["PP06"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_06pendant",
+        ["Id"] = "PP06",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://15768046443",
+        ["OwnCheck"] = false
+    },
+    ["PP04"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_04pendant",
+        ["Id"] = "PP04",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://14537376542",
+        ["OwnCheck"] = false
+    },
+    ["PP01"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_01pendant",
+        ["Id"] = "PP01",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://14537376191",
+        ["OwnCheck"] = false
+    },
+    ["PP02"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_02pendant",
+        ["Id"] = "PP02",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://14537376320",
+        ["OwnCheck"] = false
+    },
+    ["PP03"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_03pendant",
+        ["Id"] = "PP03",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://14537376410",
+        ["OwnCheck"] = false
+    }
+}
