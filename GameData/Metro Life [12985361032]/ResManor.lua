3a823945e4804f1526b5457977c375dc11a11103
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:46:46 +1000

    Create ResManor.lua

---
+++ GameData/Metro Life [12985361032]/ResManor.lua
@@ -0,0 +1,136 @@
+data = {
+    ["NTP_Manor_001"] = {
+        ["HouseAssetId"] = "",
+        ["IsNew"] = "",
+        ["Sort"] = 99,
+        ["UpdateTime"] = "",
+        ["Id"] = "NTP_Manor_001",
+        ["Disable"] = "",
+        ["Icon"] = "rbxassetid://13256619680",
+        ["RequireProduct"] = "NTP_Manor_001"
+    },
+    ["NTP_Manor_010"] = {
+        ["HouseAssetId"] = "",
+        ["IsNew"] = "2024-03-27",
+        ["Sort"] = 90,
+        ["UpdateTime"] = "",
+        ["Id"] = "NTP_Manor_010",
+        ["Disable"] = "",
+        ["Icon"] = "rbxassetid://16804710995",
+        ["RequireProduct"] = "NTP_Manor_010"
+    },
+    ["NTP_Manor_002"] = {
+        ["HouseAssetId"] = "",
+        ["IsNew"] = "",
+        ["Sort"] = 98,
+        ["UpdateTime"] = "",
+        ["Id"] = "NTP_Manor_002",
+        ["Disable"] = "",
+        ["Icon"] = "rbxassetid://13695690032",
+        ["RequireProduct"] = "NTP_Manor_002"
+    },
+    ["NTP_Manor_009"] = {
+        ["HouseAssetId"] = "",
+        ["IsNew"] = "2024-02-21",
+        ["Sort"] = 91,
+        ["UpdateTime"] = "",
+        ["Id"] = "NTP_Manor_009",
+        ["Disable"] = "",
+        ["Icon"] = "rbxassetid://16261629506",
+        ["RequireProduct"] = "NTP_Manor_009"
+    },
+    ["NTP_Manor_011"] = {
+        ["HouseAssetId"] = "",
+        ["IsNew"] = "2024-05-01",
+        ["Sort"] = 89,
+        ["UpdateTime"] = "",
+        ["Id"] = "NTP_Manor_011",
+        ["Disable"] = "",
+        ["Icon"] = "rbxassetid://17167233284",
+        ["RequireProduct"] = "NTP_Manor_011"
+    },
+    ["NTP_Manor_008"] = {
+        ["HouseAssetId"] = "",
+        ["IsNew"] = "2024-01-10",
+        ["Sort"] = 92,
+        ["UpdateTime"] = "",
+        ["Id"] = "NTP_Manor_008",
+        ["Disable"] = "",
+        ["Icon"] = "rbxassetid://15854729658",
+        ["RequireProduct"] = "NTP_Manor_008"
+    },
+    ["NTP_Manor_005"] = {
+        ["HouseAssetId"] = "",
+        ["IsNew"] = "",
+        ["Sort"] = 95,
+        ["UpdateTime"] = "",
+        ["Id"] = "NTP_Manor_005",
+        ["Disable"] = "",
+        ["Icon"] = "rbxassetid://14895956565",
+        ["RequireProduct"] = "NTP_Manor_005"
+    },
+    ["NTP_Manor_012"] = {
+        ["HouseAssetId"] = "",
+        ["IsNew"] = "2024-05-23",
+        ["Sort"] = 88,
+        ["UpdateTime"] = "",
+        ["Id"] = "NTP_Manor_012",
+        ["Disable"] = "",
+        ["Icon"] = "rbxassetid://17500284770",
+        ["RequireProduct"] = "NTP_Manor_012"
+    },
+    ["NTP_Manor_004"] = {
+        ["HouseAssetId"] = "",
+        ["IsNew"] = "",
+        ["Sort"] = 96,
+        ["UpdateTime"] = "",
+        ["Id"] = "NTP_Manor_004",
+        ["Disable"] = "",
+        ["Icon"] = "rbxassetid://14610774121",
+        ["RequireProduct"] = "NTP_Manor_004"
+    },
+    ["__index"] = {
+        "NTP_Manor_001",
+        "NTP_Manor_002",
+        "NTP_Manor_003",
+        "NTP_Manor_004",
+        "NTP_Manor_005",
+        "NTP_Manor_006",
+        "NTP_Manor_007",
+        "NTP_Manor_008",
+        "NTP_Manor_009",
+        "NTP_Manor_010",
+        "NTP_Manor_011",
+        "NTP_Manor_012"
+    },
+    ["NTP_Manor_007"] = {
+        ["HouseAssetId"] = "",
+        ["IsNew"] = "2023-12-31",
+        ["Sort"] = 93,
+        ["UpdateTime"] = "",
+        ["Id"] = "NTP_Manor_007",
+        ["Disable"] = "",
+        ["Icon"] = "rbxassetid://15621794552",
+        ["RequireProduct"] = "NTP_Manor_007"
+    },
+    ["NTP_Manor_003"] = {
+        ["HouseAssetId"] = "",
+        ["IsNew"] = "",
+        ["Sort"] = 97,
+        ["UpdateTime"] = "",
+        ["Id"] = "NTP_Manor_003",
+        ["Disable"] = "",
+        ["Icon"] = "rbxassetid://14175967262",
+        ["RequireProduct"] = "NTP_Manor_003"
+    },
+    ["NTP_Manor_006"] = {
+        ["HouseAssetId"] = "",
+        ["IsNew"] = "",
+        ["Sort"] = 94,
+        ["UpdateTime"] = "",
+        ["Id"] = "NTP_Manor_006",
+        ["Disable"] = "",
+        ["Icon"] = "rbxassetid://15297707886",
+        ["RequireProduct"] = "NTP_Manor_006"
+    }
+}
