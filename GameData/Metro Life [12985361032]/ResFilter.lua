91a1e3e349e25aa0f2d917268ea7fe4c490db137
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:54:15 +1000

    Create ResFilter.lua

---
+++ GameData/Metro Life [12985361032]/ResFilter.lua
@@ -0,0 +1,69 @@
+data = {
+    ["Filter_6"] = {
+        ["Blur"] = 4,
+        ["FilterTransparent"] = 0.8,
+        ["Id"] = "Filter_6",
+        ["Sort"] = 1,
+        ["FilterColor"] = "#6c584b",
+        ["Lighting"] = "FilterType6",
+        ["NameLoc"] = "K_SELFIE_TYPE6",
+        ["Icon"] = "rbxassetid://16021653617",
+        ["New"] = "2024-01-24"
+    },
+    ["Filter_5"] = {
+        ["Blur"] = 0,
+        ["FilterTransparent"] = 0.8,
+        ["Id"] = "Filter_5",
+        ["Sort"] = 2,
+        ["FilterColor"] = "#000000",
+        ["Lighting"] = "FilterType5",
+        ["NameLoc"] = "K_SELFIE_TYPE5",
+        ["Icon"] = "rbxassetid://15924999018",
+        ["New"] = "2024-01-17"
+    },
+    ["Filter_3"] = {
+        ["Blur"] = 3,
+        ["FilterTransparent"] = 1,
+        ["Id"] = "Filter_3",
+        ["Sort"] = 4,
+        ["FilterColor"] = "#574b9f",
+        ["Lighting"] = "FilterType3",
+        ["NameLoc"] = "K_SELFIE_TYPE3",
+        ["Icon"] = "rbxassetid://15924998909",
+        ["New"] = ""
+    },
+    ["__index"] = {"Filter_1", "Filter_2", "Filter_3", "Filter_4", "Filter_5", "Filter_6"},
+    ["Filter_2"] = {
+        ["Blur"] = 0,
+        ["FilterTransparent"] = 1,
+        ["Id"] = "Filter_2",
+        ["Sort"] = 5,
+        ["FilterColor"] = "#302d28",
+        ["Lighting"] = "FilterType2",
+        ["NameLoc"] = "K_SELFIE_TYPE2",
+        ["Icon"] = "rbxassetid://15924999167",
+        ["New"] = ""
+    },
+    ["Filter_4"] = {
+        ["Blur"] = 2,
+        ["FilterTransparent"] = 1,
+        ["Id"] = "Filter_4",
+        ["Sort"] = 3,
+        ["FilterColor"] = "#cab0b0",
+        ["Lighting"] = "FilterType4",
+        ["NameLoc"] = "K_SELFIE_TYPE4",
+        ["Icon"] = "rbxassetid://15924966191",
+        ["New"] = ""
+    },
+    ["Filter_1"] = {
+        ["Blur"] = 0,
+        ["FilterTransparent"] = 1,
+        ["Id"] = "Filter_1",
+        ["Sort"] = 6,
+        ["FilterColor"] = "#ff7bff",
+        ["Lighting"] = "FilterType1",
+        ["NameLoc"] = "K_SELFIE_TYPE1",
+        ["Icon"] = "rbxassetid://15924999088",
+        ["New"] = ""
+    }
+}
