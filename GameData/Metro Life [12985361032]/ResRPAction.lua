35a5acaa2309fc9c846ef0baa7470d49b2d38382
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:59:04 +1000

    Create ResRPAction.lua

---
+++ GameData/Metro Life [12985361032]/ResRPAction.lua
@@ -0,0 +1,58 @@
+data = {
+    ["Adult_Dizzy"] = {
+        ["RPType"] = "Adult",
+        ["LocalKey"] = "K_ACTION_DIZZY",
+        ["Id"] = "Adult_Dizzy",
+        ["IsNew"] = "2024-06-03",
+        ["AssetId"] = 17585004375,
+        ["Sort"] = 1,
+        ["Category"] = "Adult",
+        ["Loop"] = false,
+        ["Frozen"] = ""
+    },
+    ["__index"] = {"Baby_cry", "Baby_maketrouble", "Baby_sleep", "Adult_Dizzy", "Adult_Vomit"},
+    ["Adult_Vomit"] = {
+        ["RPType"] = "Adult",
+        ["LocalKey"] = "K_ADULT_VOMIT",
+        ["Id"] = "Adult_Vomit",
+        ["IsNew"] = "2024-06-03",
+        ["AssetId"] = 17585870919,
+        ["Sort"] = 2,
+        ["Category"] = "Adult",
+        ["Loop"] = false,
+        ["Frozen"] = ""
+    },
+    ["Baby_maketrouble"] = {
+        ["RPType"] = "Baby",
+        ["LocalKey"] = "K_BABYTROUBLE",
+        ["Id"] = "Baby_maketrouble",
+        ["IsNew"] = "2024-06-03",
+        ["AssetId"] = 17413855098,
+        ["Sort"] = 2,
+        ["Category"] = "Baby",
+        ["Loop"] = false,
+        ["Frozen"] = ""
+    },
+    ["Baby_cry"] = {
+        ["RPType"] = "Baby",
+        ["LocalKey"] = "K_BABYCRY",
+        ["Id"] = "Baby_cry",
+        ["IsNew"] = "2024-06-03",
+        ["AssetId"] = 17414691554,
+        ["Sort"] = 1,
+        ["Category"] = "Baby",
+        ["Loop"] = false,
+        ["Frozen"] = ""
+    },
+    ["Baby_sleep"] = {
+        ["RPType"] = "Baby",
+        ["LocalKey"] = "K_BABYSLEEP",
+        ["Id"] = "Baby_sleep",
+        ["IsNew"] = "2024-06-03",
+        ["AssetId"] = 17414520684,
+        ["Sort"] = 3,
+        ["Category"] = "Baby",
+        ["Loop"] = false,
+        ["Frozen"] = ""
+    }
+}
