3d8fd6d678c0039532a650182183b5c6cc3d8017
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:56:07 +1000

    Create ResPhoneWallPaper.lua

---
+++ GameData/Metro Life [12985361032]/ResPhoneWallPaper.lua
@@ -0,0 +1,59 @@
+data = {
+    ["__index"] = {"W01", "W02", "W03", "W04", "W05", "W06", "W07"},
+    ["W07"] = {
+        ["Image"] = "rbxassetid://16083634500",
+        ["Name"] = "",
+        ["Id"] = "W07",
+        ["IsNew"] = "2029-06-30",
+        ["Icon"] = "rbxassetid://16083638586",
+        ["OwnCheck"] = false
+    },
+    ["W04"] = {
+        ["Image"] = "rbxassetid://14479506137",
+        ["Name"] = "",
+        ["Id"] = "W04",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://14537359345",
+        ["OwnCheck"] = false
+    },
+    ["W05"] = {
+        ["Image"] = "rbxassetid://15155881247",
+        ["Name"] = "",
+        ["Id"] = "W05",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://15165221503",
+        ["OwnCheck"] = true
+    },
+    ["W03"] = {
+        ["Image"] = "rbxassetid://14479815383",
+        ["Name"] = "",
+        ["Id"] = "W03",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://14537358739",
+        ["OwnCheck"] = false
+    },
+    ["W02"] = {
+        ["Image"] = "rbxassetid://14479830961",
+        ["Name"] = "",
+        ["Id"] = "W02",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://14537357949",
+        ["OwnCheck"] = false
+    },
+    ["W01"] = {
+        ["Image"] = "rbxassetid://14479756650",
+        ["Name"] = "",
+        ["Id"] = "W01",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://14537355504",
+        ["OwnCheck"] = false
+    },
+    ["W06"] = {
+        ["Image"] = "rbxassetid://15709885431",
+        ["Name"] = "",
+        ["Id"] = "W06",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://15768046308",
+        ["OwnCheck"] = false
+    }
+}
