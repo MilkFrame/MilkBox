62e9bfd53258a1362add452c3b997532788a0c90
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:56:41 +1000

    Create ResPhoneShell.lua

---
+++ GameData/Metro Life [12985361032]/ResPhoneShell.lua
@@ -0,0 +1,91 @@
+data = {
+    ["PC09"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_09",
+        ["Id"] = "PC09",
+        ["IsNew"] = "2029-06-30",
+        ["Icon"] = "rbxassetid://16803211298",
+        ["OwnCheck"] = false
+    },
+    ["PC10"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_10",
+        ["Id"] = "PC10",
+        ["IsNew"] = "2029-06-30",
+        ["Icon"] = "rbxassetid://16803211119",
+        ["OwnCheck"] = false
+    },
+    ["PC06"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_06",
+        ["Id"] = "PC06",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://15768046655",
+        ["OwnCheck"] = false
+    },
+    ["PC02"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_02",
+        ["Id"] = "PC02",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://14537368314",
+        ["OwnCheck"] = false
+    },
+    ["PC08"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_08",
+        ["Id"] = "PC08",
+        ["IsNew"] = "2029-06-30",
+        ["Icon"] = "rbxassetid://16803211510",
+        ["OwnCheck"] = false
+    },
+    ["PC03"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_03",
+        ["Id"] = "PC03",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://14537368433",
+        ["OwnCheck"] = false
+    },
+    ["PC05"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_05",
+        ["Id"] = "PC05",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://15165221724",
+        ["OwnCheck"] = true
+    },
+    ["__index"] = {"PC01", "PC02", "PC03", "PC04", "PC05", "PC06", "PC07", "PC08", "PC09", "PC10", "PC11"},
+    ["PC11"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_11",
+        ["Id"] = "PC11",
+        ["IsNew"] = "2029-06-30",
+        ["Icon"] = "rbxassetid://16803210898",
+        ["OwnCheck"] = false
+    },
+    ["PC04"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_04",
+        ["Id"] = "PC04",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://14537368570",
+        ["OwnCheck"] = false
+    },
+    ["PC07"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_07",
+        ["Id"] = "PC07",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://16083646560",
+        ["OwnCheck"] = false
+    },
+    ["PC01"] = {
+        ["Name"] = "",
+        ["Model"] = "PhoneCase_01",
+        ["Id"] = "PC01",
+        ["IsNew"] = "",
+        ["Icon"] = "rbxassetid://14537367224",
+        ["OwnCheck"] = false
+    }
+}
