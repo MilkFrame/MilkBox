0169676b39df24d49cfbafc26f79dc7ef1a61041
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-23 22:53:08 +1000

    Create ResDoubleAction.lua

---
+++ GameData/Metro Life [12985361032]/ResDoubleAction.lua
@@ -0,0 +1,75 @@
+data = {
+    ["__index"] = {"Carryhurt", "Piggyback", "Shoulders", "Waltz", "Hug", "CarryBaby"},
+    ["Piggyback"] = {
+        ["RequireLikes"] = "",
+        ["RequirePass"] = "",
+        ["RequireFriends"] = "",
+        ["Id"] = "Piggyback",
+        ["IsNew"] = "",
+        ["Sort"] = 2,
+        ["Enabled"] = true,
+        ["BabyOnly"] = false,
+        ["Icon"] = "rbxassetid://6731079305",
+        ["Name"] = "K_PIGGYBACK"
+    },
+    ["Carryhurt"] = {
+        ["RequireLikes"] = "",
+        ["RequirePass"] = "",
+        ["RequireFriends"] = "",
+        ["Id"] = "Carryhurt",
+        ["IsNew"] = "",
+        ["Sort"] = 1,
+        ["Enabled"] = true,
+        ["BabyOnly"] = false,
+        ["Icon"] = "rbxassetid://6731079164",
+        ["Name"] = "K_CARRYHURT"
+    },
+    ["Waltz"] = {
+        ["RequireLikes"] = "",
+        ["RequirePass"] = "",
+        ["RequireFriends"] = "",
+        ["Id"] = "Waltz",
+        ["IsNew"] = "",
+        ["Sort"] = 4,
+        ["Enabled"] = false,
+        ["BabyOnly"] = false,
+        ["Icon"] = "rbxassetid://7481630509",
+        ["Name"] = "K_WALTZ"
+    },
+    ["Hug"] = {
+        ["RequireLikes"] = "",
+        ["RequirePass"] = "",
+        ["RequireFriends"] = "",
+        ["Id"] = "Hug",
+        ["IsNew"] = "",
+        ["Sort"] = 5,
+        ["Enabled"] = false,
+        ["BabyOnly"] = false,
+        ["Icon"] = "rbxassetid://7481630576",
+        ["Name"] = "K_HUG"
+    },
+    ["Shoulders"] = {
+        ["RequireLikes"] = "",
+        ["RequirePass"] = "",
+        ["RequireFriends"] = "",
+        ["Id"] = "Shoulders",
+        ["IsNew"] = "",
+        ["Sort"] = 3,
+        ["Enabled"] = true,
+        ["BabyOnly"] = false,
+        ["Icon"] = "rbxassetid://6731079400",
+        ["Name"] = "K_SHOULDERS"
+    },
+    ["CarryBaby"] = {
+        ["RequireLikes"] = "",
+        ["RequirePass"] = "",
+        ["RequireFriends"] = "",
+        ["Id"] = "CarryBaby",
+        ["IsNew"] = "",
+        ["Sort"] = 6,
+        ["Enabled"] = true,
+        ["BabyOnly"] = true,
+        ["Icon"] = "rbxassetid://12862374503",
+        ["Name"] = "K_CARRYBABY"
+    }
+}
