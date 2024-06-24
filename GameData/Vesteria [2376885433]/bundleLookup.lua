1adb9ceecd065116ec7525b48064446ec3e0a074
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-24 19:17:25 +1000

    Create bundleLookup.lua

---
+++ GameData/Vesteria [2376885433]/bundleLookup.lua
@@ -0,0 +1,66 @@
+data = {
+    ["terul maw bundle"] = {
+        ["items"] = {
+            {["stacks"] = 25, ["itemName"] = "doubloon"},
+            {["stacks"] = 3, ["itemName"] = "black pearl"},
+            {["variation"] = "atk", ["itemName"] = "ocean scroll", ["soulbound"] = true, ["successRate"] = 1},
+            {["variation"] = "def", ["itemName"] = "ocean scroll", ["soulbound"] = true, ["successRate"] = 1},
+            {["itemName"] = "baby hermit pet"}
+        },
+        ["title"] = "Terul's Maw Release Bundle",
+        ["minLevel"] = 45,
+        ["cost"] = 200,
+        ["policyRequirements"] = {["IsPaidItemTradingAllowed"] = true},
+        ["costInfo"] = {
+            ["icon"] = "rbxgameasset://Images/ethyricon2",
+            ["costType"] = "ethyr",
+            ["textColor"] = Color3.fromRGB(115, 255, 251)
+        },
+        ["bundleName"] = "terul maw bundle",
+        ["response"] = "[Bundle] Release Bundle"
+    },
+    ["holiday gift bundle 2"] = {
+        ["isGlobal"] = true,
+        ["cost"] = 2450,
+        ["costInfo"] = {
+            ["icon"] = "rbxgameasset://Images/ethyricon2",
+            ["costType"] = "ethyr",
+            ["textColor"] = Color3.fromRGB(115, 255, 251)
+        },
+        ["bundleName"] = "holiday gift bundle 2",
+        ["response"] = "[Bundle] Holiday Gifts 2",
+        ["items"] = {
+            {["itemName"] = "festive gift", ["customTag"] = "holiday2022", ["variation"] = "giving"},
+            {["itemName"] = "festive gift", ["customTag"] = "holiday2022", ["variation"] = "brick"},
+            {["itemName"] = "festive gift", ["customTag"] = "holiday2022", ["variation"] = "shadows"},
+            {["itemName"] = "festive gift", ["customTag"] = "holiday2022", ["variation"] = "harvest"},
+            {["itemName"] = "festive gift", ["customTag"] = "holiday2022", ["variation"] = "royal"},
+            {["itemName"] = "festive gift", ["customTag"] = "holiday2022", ["variation"] = "arcane"}
+        },
+        ["title"] = "Holiday Gifts Bundle 2",
+        ["policyRequirements"] = {},
+        ["minLevel"] = 5
+    },
+    ["holiday gift bundle"] = {
+        ["isGlobal"] = true,
+        ["cost"] = 1050,
+        ["costInfo"] = {
+            ["icon"] = "rbxgameasset://Images/ethyricon2",
+            ["costType"] = "ethyr",
+            ["textColor"] = Color3.fromRGB(115, 255, 251)
+        },
+        ["bundleName"] = "holiday gift bundle",
+        ["response"] = "[Bundle] Holiday Gifts",
+        ["items"] = {
+            {["itemName"] = "festive gift", ["customTag"] = "holiday2022", ["variation"] = "grace"},
+            {["itemName"] = "festive gift", ["customTag"] = "holiday2022", ["variation"] = "frost"},
+            {["itemName"] = "festive gift", ["customTag"] = "holiday2022", ["variation"] = "slime"},
+            {["itemName"] = "festive gift", ["customTag"] = "holiday2022", ["variation"] = "sadness"},
+            {["itemName"] = "festive gift", ["customTag"] = "holiday2022", ["variation"] = "love"},
+            {["itemName"] = "cookie", ["stacks"] = 30, ["customTag"] = "holiday2022"}
+        },
+        ["title"] = "Holiday Gifts Bundle",
+        ["policyRequirements"] = {},
+        ["minLevel"] = 5
+    }
+}
