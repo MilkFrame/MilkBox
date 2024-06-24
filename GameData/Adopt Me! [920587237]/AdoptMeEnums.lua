145abe7ff2f1f35c9bd26c26e17c5bb91193bacf
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-24 19:27:02 +1000

    Create AdoptMeEnums.lua

---
+++ GameData/Adopt Me! [920587237]/AdoptMeEnums.lua
@@ -0,0 +1,60 @@
+data = {
+    ["ClaimBeesBlasterResult"] = {
+        ["DoesNotOwnAsset"] = "__Enum_ClaimBeesBlasterResult_1",
+        ["BackendError"] = "__Enum_ClaimBeesBlasterResult_4",
+        ["AlreadyClaimed"] = "__Enum_ClaimBeesBlasterResult_2",
+        ["ClaimSuccessful"] = "__Enum_ClaimBeesBlasterResult_3"
+    },
+    ["PetEntities"] = {
+        ["PerformanceExitType"] = {
+            ["Cancel"] = "__Enum_PerformanceExitType_2",
+            ["Complete"] = "__Enum_PerformanceExitType_1",
+            ["None"] = "__Enum_PerformanceExitType_3"
+        },
+        ["ReactionExitType"] = {
+            ["Cancel"] = "__Enum_ReactionExitType_2",
+            ["Complete"] = "__Enum_ReactionExitType_1",
+            ["None"] = "__Enum_ReactionExitType_3"
+        },
+        ["ObstacleType"] = {
+            ["Impassable"] = "__Enum_ObstacleType_4",
+            ["Jumpable"] = "__Enum_ObstacleType_2",
+            ["NotJumpable"] = "__Enum_ObstacleType_3",
+            ["None"] = "__Enum_ObstacleType_1"
+        },
+        ["MountType"] = {
+            ["NotMounted"] = "__Enum_MountType_1",
+            ["Riding"] = "__Enum_MountType_2",
+            ["Flying"] = "__Enum_MountType_3"
+        },
+        ["StimulusType"] = {
+            ["Ailment"] = "__Enum_StimulusType_5",
+            ["ThrowToy"] = "__Enum_StimulusType_1",
+            ["Food"] = "__Enum_StimulusType_2",
+            ["Command"] = "__Enum_StimulusType_3",
+            ["Pet"] = "__Enum_StimulusType_4",
+            ["EquippedItem"] = "__Enum_StimulusType_6",
+            ["Discosplosion"] = "__Enum_StimulusType_7"
+        },
+        ["ExclusiveStateType"] = {
+            ["Mounted"] = "__Enum_ExclusiveStateType_4",
+            ["FollowingPresetPath"] = "__Enum_ExclusiveStateType_3",
+            ["LearningTricks"] = "__Enum_ExclusiveStateType_2",
+            ["Carried"] = "__Enum_ExclusiveStateType_5",
+            ["Sitting"] = "__Enum_ExclusiveStateType_6",
+            ["Swimming"] = "__Enum_ExclusiveStateType_1"
+        },
+        ["PetObjectCreatorType"] = {
+            ["DroppableToy"] = "__Enum_PetObjectCreatorType_1",
+            ["PetFood"] = "__Enum_PetObjectCreatorType_2"
+        },
+        ["PetCommandType"] = {
+            ["DoTrick"] = "__Enum_PetCommandType_1",
+            ["CompleteAilment"] = "__Enum_PetCommandType_5",
+            ["Follow"] = "__Enum_PetCommandType_2",
+            ["Stay"] = "__Enum_PetCommandType_3",
+            ["UseFurniture"] = "__Enum_PetCommandType_4"
+        }
+    },
+    ["init"] = "function: 0x5963d527b46da329"
+}
