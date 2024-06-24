78be8d1e502cc5a6a9075693e415c7668f153f33
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-24 19:20:58 +1000

    Create gameChangelogs.lua

---
+++ GameData/Vesteria [2376885433]/gameChangelogs.lua
@@ -0,0 +1,237 @@
+data = {["Feb Update 2"]={["title"]="Happy Valentine's Day ❤️",["text"]=[[✨ New Tree of Life and Redwood Stronghold map demos have been launched
+🔥 New Deluxe Emotes pass unlocks the following emotes: griddy, pushups2, sob, fistpump, flop, worm, jazzy
+❤️ Valentines Day items have been added to the Premium Store
+👍 Enabled Monster Lures in Whispering Dunes
+⚔️ New Colosseum map launched
+⚔️ Increased damage reduction and HP/MP regeneration in Colosseum
+⚔️ Consumables can no longer be used inside the Colosseum
+⚔️ Enemies killed in Colosseum drop HP/MP restoring orbs that are used when picked up
+🔧 Reduced the tick damage of Blood Plague in PVP
+🔧 Bow basic attacks can now be used on destroyable objects
+🔧 Reduced the drop chance of Pristine, Legendary Modifiers on Super Giant enemies (Not Colossal enemies)
+🐛 Fixed Lvl.45+ giant enemies not dropping Shadow Cursed Scrolls
+🐛 Fixed ocassionally not being able to jump after respawning
+🐛 Fixed missing pages of the Lost Journal not updating
+🐛 Fixed stamina bar accidently turning itself back on
+	]]},["October Update 1"]={["title"]="October Update 1",["text"]=[[- Rebirths now grant +5% XP per Rebirth
+- Leaderboard player cards have been updated to improve readability
+- All Parasites now properly spawn as the same level as their host
+- Shield now has a category icon
+- Chain Binding has been slightly reworked, now it only activates on direct hits and then it grabs all nearby enemies and chains them to remain in a nearby area
+- Physics optimization for when there are a lot of mobs nearby
+- Falling into the void triggers death effects
+- Status Effects will now properly be removed if you fall into the void
+	]]},["September Update 2"]={["title"]="September Update 2",["text"]=[[- Released Bard
+- Added Bard quest with Melody
+- Added secret third eye cosmetic (don't tell anyone!).
+- Added secret spider eyes cosmetic.
+- Added unibrow.
+- Added raised eyebrow.
+- Added green hair.
+- New tribute wars gear sets.
+- New Tribute wars shop.
+- Fixed spiderfang dagger perk not working when dual wielded in offhand.
+- Bard instruments now work with jester armor.
+- Holiday items are now in the Stranger shop.
+	]]},["Mushroom Apocalypse"]={["title"]="Mushroom Apocalypse Update",["text"]=[[- Mushroom Apocalypse update: talk to Geologist Bridget in any city to visit the Mushroom Research Base (Mushpoc dungeon returned w/ revamped rewards, balancing, bug fixes, experience improvements, etc.)
+- Assassin update: Ethereal Strike, Shadow Walk buffed. Added new Ravenous status effect
+- Warlock update: Desecrate, Dark Pulse adjusted, Chain Binding reworked
+- Knockback update: standardized how knockback is handled by abilities, showing accurate knockback stats and affecting players/monsters consistently
+- Leviathian Hit is now Leviathian Cleave; power: 1x->2x, added powerful upwards knockback
+- Ethereal Strike's teleport attack is now Unstoppable; fixed replication issues with characters being stuck and missing projectile
+- Buffed Ornate Dagger's smite power: 2x->2.5x
+- Abyssal Dagger's Echo perk now applies its echo hits after the ability (reducing endlag), increased echo hit damage
+- You can now view status effects over other players in your party or in PVP
+- Updated main menu UX
+- Increased default night time brightness
+- Players can no longer be tracked during stealth
+- Adjusted Monster book rewards to be more consistent 
+- Uprooted Blade now caps at 250 stacks per player
+- Points invested into a special ability will now be refunded when the special ability is removed
+- Nerfed Abyssal Bow
+- Buffed scroll and item drop rates of Scarab, Sarakis, Auktufiti the Watcher, Azariah the Blood-crazed.
+- Increased or rounded drop rates of Bandit, Bandit Skirmisher, Deathsting, Dustwurm.
+- Buffed Pirate Captain's drop rates
+- War Cry, Prayer of Comfort, Inspire no longer stack
+- Holding basic attack input now allows you to continue basic attacking after an ability has ended
+- Can no longer sit in a chair while holding unclaimed Marks of Valor in Colosseum
+- Fixed Savage Shank immortality bug
+- Fixed collisions involving pets and projectiles
+- Fixed Flintlock damaging larger enemies
+	]]},["September Update 1"]={["title"]="September Update 1",["text"]=[[- Added Faction city quest badges
+- Added leaderboard badges for legend, roblox stars, roblox staff
+- Added leaderboard icon for voice chatters
+- Added custom more status effect icons for tree of life and redwood stronghold's special faction fireplaces
+- Added support for NPC dialogue translations
+- Replaced red screen damage effect with a black-and-white effect
+- Ravenous updated visual effects + damage buff, now only affects ability damage
+- Shadow Walk and Etheral reduced to max rank 5, rebalanced (all Assassin ability points reset)
+- Improvements to data saving to mitigate the risk of data issues in the future
+- Minor nerfs to dagger m1 attack speed/damage
+- Consumable status effects from the same category no longer stack. Categories: potion, fish, food, candy
+- Updated dialogue and chat bubble UIs
+- Fixed missing damage indicators on some attacks
+- Fixed animation glitches with dual wield sword/shield attacks
+- Fixed Berserker 2x Triple Slash buff applying when only one sword is equipped
+- Fixed Dark Pulse being spammable
+- Fixed Parry counterattacks not being unstoppable
+- Fixed Tuaa Staff perk
+- Fixed text clipping out of bounds in chat bubbles
+- Fixed player name tags stretching out of bounds
+- Fixed Rich Text size label being used to break item name size limits
+- Fixed War Cry voiding players or breaking their movement
+	]]},["Stamina Gameplay Update"]={["title"]="Major Gameplay Update",["text"]=[[- Stamina v3
+- Added Fall Damage
+- New cooperative monster rewards
+- New skills menu
+- Stat perk improvements 
+- New chat system
+- New character customization screen & faction fast-pass
+- Guild improvements and added functionality (renaming, icon, color select, ownership transfer)
+- Mobile improvements
+- Battle summary sorts top-to-bottom now (was bottom-to-top)
+- Monster book now caches buttons
+- Attributes no longer affect reforge price
+- Added new Mo Ko Tu Aa Forsaken Crabby variants
+- Improved yeti behavior
+- Improved chad behavior with size scaling
+- Updated aiming to use spherecasting
+- Improved falling/edge detection with spherecasting
+- Destroyable faction banners
+- Rock throw & combat roll cost stamina
+- Prayer of comfort increases stamina regeneration
+- Cursed scrolls' walkspeed rolls changed back to stamina
+- Pears increase stamina again
+- Ornate staff uses stamina
+- Rebalanced most boots
+- Rebalanced ratking bucket & captains hat to be weaker but have location-specific perk
+- Rebalanced diver headgear perk to give damage reduction & plated iron helmet to give defense
+- Spear of light now has an AoE effect and can level to 10
+- Nerfed flare damage/health but decreased cooldown
+- Buffed Heal ability
+- Nerfed tracking range for Forg Scryer Thundercall
+- Abyssal Bow changes
+- Spear of light fix
+- Fiant loot rebalancing
+- Fixed interactions getting stuck
+- Fixed portable taxi
+- Fixed shiprock pirate hat not being a golden chest
+- Fixed inconsistent Tuaa staff
+- Fixed special attacks for monsters
+- Fixed meteor strike breaking things
+- Fixed pvp not counting for onKill stuff
+- Fixed 12 hats that didn't reforge correctly
+- Fixed duplicate names & non-updating data, and randomly being kicked out in guilds
+	]]},["April Update 1"]={["title"]="April Update 1",["text"]=[[4/18/2023
+🔎 New Quest Trackers! Indicators in the world help you complete quest progress
+📱 Improved mobile menu buttons and UI layout
+📱 New tap-to-aim mobile controls. Old mobile cursor aim can be toggled in settings
+🎥 Content Creator freecam now hides name tags, health bars and other in-world UI
+💛 Rebirth title and XX eyes now visible in main menu character selection
+⬇️ The well in Nilgarf can now be used to travel to Nilgarf Sewers
+🔧 Reduced the health of Whispering Dunes minibosses, Reanimated Slime
+🔧 Reduced health, damage and attack speed of Ethera
+🔧 Removed Gift Bomb from Adventurer Book
+🔧 Mushtown quest dialogue improvements
+🔧 Scruff's quest now awards 5 Red Potion and 5 Blue Potion again
+🔧 Innkeeper's Son quest requirement changed to Lvl.12
+🔧 Mushtown Helper samples step replaced with gathering building supplies
+🔧 Catapult quest can now be started at Lvl.8
+🔧 Family Gem quest now grants Great ATK + Great DEF on complete
+🐛 Fixed Ornate Staff attack
+🐛 Fixed missing class emblem/item category icons
+🐛 Fixed faction/subclass NPC dialogue visibility
+🐛 Fixed negative equipment stats due to level ceiling in colo
+🐛 Fixed character customization selection on mobile
+🐛 Fixed full screen prompt menu on Xbox
+🐛 Fixed Mushroom Potion morph]]},["March Update 1"]={["title"]="March Update 1",["text"]=[[🍀 Limited-time St. Patrick's Day-themed items available in the ethyr shop
+🍀 Four leaf clovers have been spotted dropping across the game
+🔨 Introducing Reforging! Talk to Blacksmith Ralph to increase your item's level
+📜 Damage/defense bonus from scrolls and attributes reduced to original 1x multi
+🤔 STR/INT now increase physical/magical damage additively instead of multiplying
+🔧 Crit damage bonus reduced from 150% to 130%
+🔧 NPC status markers can now be seen through walls
+🌈 New Future Lighting being rolled out across maps
+⚙️ Lighting updates once per minute unless new Smooth Lighting setting is on
+
+	]]},["November Patch 1"]={["title"]="Fixes",["text"]=[[	- Fixed "Rubee Problem" explosions not dealing damage to enemies
+	- Gold is now displayed on the leaderboard
+	- Aevrul's Helmet now shows (lack of) hair properly
+	- A confirmation prompt now appears when trying to leave a dungeon via "Leave party"
+	
+	<b>Balancing</b>
+	- Reduced Mighty Sub cooldown from 8s to 6s
+	- "Finders, Keepers" quest start levels adjusted to 48, 49, 50 (respectively)
+	]]},["Feb Update 1"]={["title"]="February Update 1",["text"]=[[✨ Updated game lighting. Enjoy the scenery more than ever before!
+🔍 Show the location of a player's character on the map when inspecting them
+🔧 Updated giant drops:  (Level 0-14: Basic Scrolls, rarely Greats; Level 15-29: Great Scrolls, rarely Darks; Level 30-44: Dark Scrolls, rarely Curseds, super+Giants very rarely drop Pristine mod; Level 45-59: Cursed Scrolls, rarely Shadow Curseds, super+Giants very rarely drop Pristine or Legendary mod)
+ 🔧 Enemies in dungeons now give some EXP for killing them instead of nothing
+🔧 Added a triple venom bomb attack to Spider Prince
+🔧 Mobs no longer collide with eachother
+🔧 When a party leader uses a rune, the entire party teleports together
+🐛 Fixed parties being disbanded after teleporting together
+🐛 Fixed Business Trip quest entering the house step
+🐛 Fixed Whale Tale quest lab door 
+🐛Fixed input lag before ability casting
+🐛 Fixed the entrance to the Gauntlet]]},["Feb Update 3"]={["title"]="February Update 3",["text"]=[[2/24/2023
+🔥 When a boss or giant enemy is defeated, a Battle Summary page will now display showing the participation of each player and what rewards they got
+🔧 Timed world bosses now instead spawn on a chance interval like the Horseshoe Crab boss. Chad and the Yeti have a 30% chance to spawn every 3 minutes, while Possum has a 15% chance to spawn every 5 minutes. The timer begins when the server starts and then again when the last boss is killed
+🔧 Enemies have a reduced chance to spawn on empty servers. The chance is reduced to 35% when only one player is in the server, goes up to 100% when the server is half-full or at least 10 players are in the server, and can increase up to 125% for slightly fuller servers 
+🔧 Faction runes now teleport you directly into the faction base instead of at the start of the map
+🔧 Giant Bell Diver Hermit Crabby can now only be spawned once per quest run, however everyone who is doing the quest is able to gain participation from fighting the giant instead of just the person who spawned it
+	]]},["March Update 2"]={["title"]="March Update 2",["text"]=[[⚔️ Added new Brine Sabre sword with Ground Slam modification
+⚜️ Added new Moko Spear adventurer item with Rock Throw modification
+✨ Variants now spawn in the Oceanographer's shop
+✅ Added stats and updated missing data on Aevrul equipment items
+🏴‍☠ Pirate baseHealth 2->1.5, baseDamage 1.5->0.8, goldMulti 1->1.5, expMulti 1->1.25
+🏴‍☠ Anchor Pirate baseHealth 4->2, baseDamage 1.5->0.8, goldMulti 1.5->1.75, expMulti 1.5->1.25
+🏴‍☠ Updated loot drop tables for Pirate, Anchor Pirate and Sunken Savage
+🗝️ Rusted Sunken Keys now drop from above mobs, used to open Sunken Lockboxes in the future
+🗡️ Octant Dirk Lvl47->Lvl50
+🔎 Perk icons will now display on equipment tooltips
+🌈 Added atmospheric lighting regions to Terul's Maw
+💵 Doubloon/Doubloon fragment stack size increased 50->99
+💵 White Pearl stack size 24->99, sell price 80S->92S
+💵 Dark Pearl sell price 200S->350S
+💡 Lato & Miru's Outpost restocked with new items
+🔧 Fireflies no longer spawn underwater
+🔧 Set a maximum falling speed underwater
+🔧 Added advanced search filter terms to shop (i.e. tier:6,level:30-45,type:bow)
+🔧 Added new gameplay tips that can be disabled in settings
+🐛 Fixed the Stranger
+🐛 Fixed lighting glitches on Terul's Maw 
+🐛 Fixed Sawshark sword not being able to interact with objects
+	]]},["New Port Fidelio"]={["title"]="🌊 New Port Fildeio Update 🌊",["text"]=[[🌊 Introducing New Port Fidelio! Seaside Path, Scallop Shores and Port Fidelio have been merged into one epic new map, featuring new Crabby variants, a boss, new equipment items, a new resource and a new quest. We are working on similar updates for the remaining faction areas.
+🏆 Leaderboard now shows Roblox Premium, Roblox Verified, Vesteria Staff and Content Creator badges, sorts by gold, and shows HP and MP of party members.
+🔎 You can now inspect players using the interact key when you are near them, and the interact page shows more detailed stats.
+✨ Resources now glow and sparkle to attract attention. Loot tables of some resources have been updated.
+📜 Introducing the Undo Scroll - undos the effect of the last successful scroll upgrade.
+🎨 Introducing the Custom Dye - allows you to select a specific color to dye your item.
+📚 Introducing Tomes - equippable magic books which raise Magic Attack and other stats for Mages. Can be further upgraded with weapon scrolls.
+🔥 Meteor Strike now temporarily sets enemies on fire.
+🧊 Frostcall now temporarily slows enemies.
+🕊️ Flare now dulls enemies, temporarily reducing their damage.
+🌈 Prism Trap can now have its trap lifetime, root time and number of traps upgraded by STR, VIT and DEX respectively.
+🔧 Bubble Burst now functions as an AoE ability.
+🔧 Two-handed great weapons now provide additional Physical Attack.
+🔧 All monster attack damage has been reduced by 40%.
+🔧 Pristine (-25%) and Legendary (-50%) items are now less likely to be destroyed by scrolls.
+🔧 Items now show the chance of being destroyed by a scroll when hovering over them.
+🔧 XX players can now rebirth to reset their class, but with no additional rewards.
+🔧 Dropped items can now be picked up faster and from a great distance.
+🔧 Campfire status effect now lingers based on time spent by the fire + improved visuals.
+🔧 Reset Scrolls now ask for confirmation before being applied to an item.
+🔧 Mo Ko Tu Aa's spells now count as magical attacks.
+🐛 Fixed health bars over other players not updating.
+🐛 Fixed Dark Pulse, Death Plague, and Blasting Slash not working with ability radius boosts.
+🐛 Fixed Captain's Quarters spawning you in the wrong location when you return.	
+	]]},["December Event 1"]={["title"]="Holiday Event Update 2022",["text"]=[[	- Added Santy Claws Holiday Tasks and new player stats tracking
+	- Introducing Special Abilities. These replace some equipment perks and allow the effect of the equipment to be upgraded with ability points
+	- Current Special Abilities: Splinter (Uprooted Blade), Caustic Wounds (Spider Fang Dagger), Mana Thief (Spider Fang Sword), more coming soon
+	- Ethera boss battle fixed
+	- Added new Multi-Shot passive to Ranger book
+	
+	<b>Balancing</b>
+	- Ranger's Stance has been nerfed and its variants adjusted
+	- New Dissolve perk on the Forgotten Dagger
+	]]}}
