3b37679d67c515501731b6e15db9f911bc5c3aa0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-24 19:27:50 +1000

    Create AltCurrencyDB.lua

---
+++ GameData/Adopt Me! [920587237]/AltCurrencyDB.lua
@@ -0,0 +1,128 @@
+data = {
+    ["lanterns"] = {
+        ["human_readable"] = "Lanterns",
+        ["minigame_rewards_image"] = {["phone"] = "rbxassetid://12114155516", ["desktop"] = "rbxassetid://12114155309"},
+        ["indicator_color"] = Color3.fromRGB(244, 40, 40),
+        ["interaction_format"] = "%d Lanterns",
+        ["sprout_images"] = {["A"] = "rbxassetid://4508224937", ["B"] = "rbxassetid://11779651672"},
+        ["buy_icon"] = "rbxassetid://11779651672",
+        ["shop_text"] = "BUY LANTERNS",
+        ["shop_image"] = "rbxassetid://11865012109",
+        ["shop_gradient_color"] = Color3.fromRGB(255, 0, 0),
+        ["purchase_app_icon"] = "rbxassetid://11779651576",
+        ["indicator_backing"] = "rbxassetid://11815061831",
+        ["developer_products"] = {
+            {["reward_amount"] = 75, ["product_id"] = 1345955299, ["image_id"] = 11779651672, ["cost"] = 30},
+            {["reward_amount"] = 150, ["product_id"] = 1345955450, ["image_id"] = 11814876322, ["cost"] = 60},
+            {["reward_amount"] = 300, ["product_id"] = 1345955492, ["image_id"] = 11814876377, ["cost"] = 120},
+            {["reward_amount"] = 1000, ["product_id"] = 1345955540, ["image_id"] = 11814876411, ["cost"] = 320},
+            {["reward_amount"] = 3000, ["product_id"] = 1345955585, ["image_id"] = 11814876462, ["cost"] = 800},
+            {["reward_amount"] = 10000, ["product_id"] = 1345955627, ["image_id"] = 11814876514, ["cost"] = 2400}
+        }
+    },
+    ["beachballs"] = {
+        ["human_readable"] = "Beach Balls",
+        ["minigame_rewards_image"] = {["phone"] = "rbxassetid://13602641492", ["desktop"] = "rbxassetid://13602642151"},
+        ["indicator_color"] = Color3.fromRGB(244, 40, 40),
+        ["interaction_format"] = "%d 🏐",
+        ["sprout_images"] = {["A"] = "rbxassetid://4508224937", ["B"] = "rbxassetid://13682622092"},
+        ["buy_icon"] = "rbxassetid://13682622092",
+        ["shop_text"] = "BUY BEACH BALLS",
+        ["shop_image"] = "rbxassetid://13673396202",
+        ["shop_gradient_color"] = Color3.fromRGB(113, 70, 196),
+        ["purchase_app_icon"] = "rbxassetid://13690200024",
+        ["indicator_backing"] = "rbxassetid://13682467766",
+        ["developer_products"] = {
+            {["reward_amount"] = 200, ["product_id"] = 1546861034, ["image_id"] = 13682622092, ["cost"] = 7},
+            {["reward_amount"] = 800, ["product_id"] = 1546861115, ["image_id"] = 13682628376, ["cost"] = 28},
+            {["reward_amount"] = 3000, ["product_id"] = 1546861161, ["image_id"] = 13682640479, ["cost"] = 105},
+            {["reward_amount"] = 7500, ["product_id"] = 1546861210, ["image_id"] = 13682646227, ["cost"] = 210},
+            {["reward_amount"] = 16000, ["product_id"] = 1546861238, ["image_id"] = 13682650293, ["cost"] = 420},
+            {["reward_amount"] = 50000, ["product_id"] = 1546861283, ["image_id"] = 13682654283, ["cost"] = 1225}
+        }
+    },
+    ["gingerbread"] = {
+        ["human_readable"] = "Gingerbread",
+        ["minigame_rewards_image"] = {["phone"] = "rbxassetid://6040957938", ["desktop"] = "rbxassetid://6040958164"},
+        ["indicator_color"] = Color3.fromRGB(244, 40, 40),
+        ["interaction_format"] = "%d Gingerbread",
+        ["sprout_images"] = {["A"] = "rbxassetid://4508224937", ["B"] = "rbxassetid://6087886497"},
+        ["buy_icon"] = "rbxassetid://15431878046",
+        ["shop_text"] = "BUY GINGERBREAD",
+        ["shop_image"] = "rbxassetid://6048693211",
+        ["shop_gradient_color"] = Color3.fromRGB(113, 70, 196),
+        ["purchase_app_icon"] = "rbxassetid://4506300100",
+        ["indicator_backing"] = "rbxassetid://6048692831",
+        ["developer_products"] = {
+            {["reward_amount"] = 300, ["product_id"] = 1334951327, ["image_id"] = 6048722192, ["cost"] = 7},
+            {["reward_amount"] = 1750, ["product_id"] = 1334951598, ["image_id"] = 6048722443, ["cost"] = 40},
+            {["reward_amount"] = 4400, ["product_id"] = 1334951720, ["image_id"] = 6048722865, ["cost"] = 100},
+            {["reward_amount"] = 11500, ["product_id"] = 1334951940, ["image_id"] = 6048723099, ["cost"] = 250},
+            {["reward_amount"] = 31250, ["product_id"] = 1334952075, ["image_id"] = 6048723369, ["cost"] = 625},
+            {["reward_amount"] = 70000, ["product_id"] = 1334952170, ["image_id"] = 6048724179, ["cost"] = 1250}
+        }
+    },
+    ["candy"] = {
+        ["human_readable"] = "Candy",
+        ["minigame_rewards_image"] = {["phone"] = "rbxassetid://5852117599", ["desktop"] = "rbxassetid://5852117837"},
+        ["indicator_color"] = Color3.fromRGB(244, 40, 40),
+        ["interaction_format"] = "%d 🍬",
+        ["sprout_images"] = {["A"] = "rbxassetid://4508224937", ["B"] = "rbxassetid://5865710943"},
+        ["buy_icon"] = "rbxassetid://14845052425",
+        ["shop_text"] = "BUY CANDY",
+        ["shop_image"] = "rbxassetid://5865714516",
+        ["shop_gradient_color"] = Color3.fromRGB(113, 70, 196),
+        ["purchase_app_icon"] = "rbxassetid://4149469268",
+        ["indicator_backing"] = "rbxassetid://5865214349",
+        ["developer_products"] = {
+            {["reward_amount"] = 300, ["product_id"] = 1319076371, ["image_id"] = 5865710943, ["cost"] = 7},
+            {["reward_amount"] = 1750, ["product_id"] = 1319076549, ["image_id"] = 5865711471, ["cost"] = 40},
+            {["reward_amount"] = 4400, ["product_id"] = 1319076654, ["image_id"] = 5865711744, ["cost"] = 100},
+            {["reward_amount"] = 11500, ["product_id"] = 1319076815, ["image_id"] = 5865712063, ["cost"] = 250},
+            {["reward_amount"] = 31250, ["product_id"] = 1319077062, ["image_id"] = 10979947182, ["cost"] = 625},
+            {["reward_amount"] = 70000, ["product_id"] = 1319077176, ["image_id"] = 5865713276, ["cost"] = 1250}
+        }
+    },
+    ["yakgwa"] = {
+        ["human_readable"] = "Yakgwa",
+        ["minigame_rewards_image"] = {["phone"] = "rbxassetid://16100617840", ["desktop"] = "rbxassetid://16100620769"},
+        ["indicator_color"] = Color3.fromRGB(244, 40, 40),
+        ["interaction_format"] = "%d Yakgwa",
+        ["sprout_images"] = {["A"] = "rbxassetid://4508224937", ["B"] = "rbxassetid://11779651672"},
+        ["buy_icon"] = "rbxassetid://16101765735",
+        ["shop_text"] = "BUY YAKGWA",
+        ["shop_image"] = "rbxassetid://16100061800",
+        ["shop_gradient_color"] = Color3.fromRGB(133, 0, 212),
+        ["purchase_app_icon"] = "rbxassetid://16100156573",
+        ["indicator_backing"] = "rbxassetid://16100107990",
+        ["developer_products"] = {
+            {["reward_amount"] = 300, ["product_id"] = 1739761890, ["image_id"] = 16100131394, ["cost"] = 7},
+            {["reward_amount"] = 1750, ["product_id"] = 1739762694, ["image_id"] = 16100134840, ["cost"] = 40},
+            {["reward_amount"] = 4400, ["product_id"] = 1739762961, ["image_id"] = 16100125252, ["cost"] = 100},
+            {["reward_amount"] = 11500, ["product_id"] = 1739763092, ["image_id"] = 16100121713, ["cost"] = 250},
+            {["reward_amount"] = 31250, ["product_id"] = 1739763220, ["image_id"] = 16100128265, ["cost"] = 625},
+            {["reward_amount"] = 70000, ["product_id"] = 1739763373, ["image_id"] = 16100118431, ["cost"] = 1250}
+        }
+    },
+    ["eggs"] = {
+        ["human_readable"] = "Eggs",
+        ["minigame_rewards_image"] = {["phone"] = "rbxassetid://12739543504", ["desktop"] = "rbxassetid://12739543504"},
+        ["indicator_color"] = Color3.fromRGB(244, 40, 40),
+        ["interaction_format"] = "%d 🥚",
+        ["sprout_images"] = {["A"] = "rbxassetid://4508224937", ["B"] = "rbxassetid://16725730324"},
+        ["buy_icon"] = "rbxassetid://16725730324",
+        ["shop_text"] = "BUY EGGS",
+        ["shop_image"] = "rbxassetid://12790274692",
+        ["shop_gradient_color"] = Color3.fromRGB(113, 70, 196),
+        ["purchase_app_icon"] = "rbxassetid://9281808894",
+        ["indicator_backing"] = "rbxassetid://12920177170",
+        ["developer_products"] = {
+            {["reward_amount"] = 300, ["product_id"] = 1501323377, ["image_id"] = 9281667602, ["cost"] = 7},
+            {["reward_amount"] = 1750, ["product_id"] = 1501323495, ["image_id"] = 9281667704, ["cost"] = 40},
+            {["reward_amount"] = 4400, ["product_id"] = 1501323618, ["image_id"] = 9281667807, ["cost"] = 100},
+            {["reward_amount"] = 11500, ["product_id"] = 1501323686, ["image_id"] = 9281667928, ["cost"] = 250},
+            {["reward_amount"] = 31250, ["product_id"] = 1501323744, ["image_id"] = 9281668036, ["cost"] = 625},
+            {["reward_amount"] = 70000, ["product_id"] = 1501323813, ["image_id"] = 9281673267, ["cost"] = 1250}
+        }
+    }
+}
