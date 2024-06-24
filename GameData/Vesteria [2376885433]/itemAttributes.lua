269b2030b21b94b073f25bcd01dacaf83952bf90
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-24 18:51:34 +1000

    Create itemAttributes.lua

---
+++ GameData/Vesteria [2376885433]/itemAttributes.lua
@@ -0,0 +1,86 @@
+data = {
+    ["titanic"] = {
+        ["prefix"] = "Titanic",
+        ["modifier"] = "function: 0xec9dc35b406c23bd",
+        ["color"] = Color3.fromRGB(0, 170, 255),
+        ["attributes"] = {["isCriticallyResistant"] = true},
+        ["valueMulti"] = 25
+    },
+    ["trash"] = {["prefix"] = "Trash", ["attributes"] = {["rarity"] = "Common", ["tier"] = -1}},
+    ["mythical"] = {
+        ["color"] = Color3.fromRGB(27, 225, 225),
+        ["valueMulti"] = 4.5,
+        ["prefix"] = "Mythical",
+        ["modifier"] = "function: 0xb6f9218003555c7d"
+    },
+    ["keen"] = {
+        ["color"] = Color3.fromRGB(135, 186, 213),
+        ["valueMulti"] = 1.25,
+        ["prefix"] = "Keen",
+        ["modifier"] = "function: 0x88bf458242db78bd"
+    },
+    ["mythic"] = {
+        ["modifier"] = "function: 0xeb1272d6609c471d",
+        ["color"] = Color3.fromRGB(0, 225, 0),
+        ["attributes"] = {["isCriticallyResistant"] = true},
+        ["valueMulti"] = 10
+    },
+    ["poisoned"] = {["prefix"] = "Poisoned", ["attributes"] = {["soulbound"] = true, ["tier"] = -1}},
+    ["vibrant"] = {
+        ["color"] = Color3.fromRGB(194, 127, 128),
+        ["valueMulti"] = 1.25,
+        ["prefix"] = "Vibrant",
+        ["modifier"] = "function: 0x1fda1f83223e6b9d"
+    },
+    ["dull"] = {
+        ["color"] = Color3.fromRGB(148, 148, 148),
+        ["valueMulti"] = 0.75,
+        ["prefix"] = "Dull",
+        ["modifier"] = "function: 0x134abd2fa5537c1d"
+    },
+    ["swift"] = {
+        ["color"] = Color3.fromRGB(190, 130, 179),
+        ["valueMulti"] = 1.25,
+        ["prefix"] = "Swift",
+        ["modifier"] = "function: 0x80d7e76982364efd"
+    },
+    ["fierce"] = {
+        ["color"] = Color3.fromRGB(190, 143, 109),
+        ["valueMulti"] = 1.25,
+        ["prefix"] = "Fierce",
+        ["modifier"] = "function: 0x0433ce347f8165dd"
+    },
+    ["tattered"] = {
+        ["color"] = Color3.fromRGB(148, 148, 148),
+        ["valueMulti"] = 0.75,
+        ["prefix"] = "Tattered",
+        ["modifier"] = "function: 0xae46e45cff88737d"
+    },
+    ["legendary"] = {
+        ["prefix"] = "Legendary",
+        ["modifier"] = "function: 0x15e4117aa12d105d",
+        ["color"] = Color3.fromRGB(225, 176, 28),
+        ["attributes"] = {["destroyItemRateMulti_attribute"] = 0.5},
+        ["valueMulti"] = 3
+    },
+    ["original"] = {
+        ["prefix"] = "Original",
+        ["modifier"] = "function: 0x65498b2844ff6e3d",
+        ["color"] = Color3.fromRGB(0, 170, 255),
+        ["attributes"] = {["soulbound"] = true, ["isCriticallyResistant"] = true},
+        ["valueMulti"] = 3
+    },
+    ["pristine"] = {
+        ["prefix"] = "Pristine",
+        ["modifier"] = "function: 0x7aef002ffe87973d",
+        ["color"] = Color3.fromRGB(150, 45, 202),
+        ["attributes"] = {["destroyItemRateMulti_attribute"] = 0.75},
+        ["valueMulti"] = 1.75
+    },
+    ["worn"] = {
+        ["color"] = Color3.fromRGB(148, 148, 148),
+        ["valueMulti"] = 0.75,
+        ["prefix"] = "Worn",
+        ["modifier"] = "function: 0x6a5c8b85a460755d"
+    }
+}
