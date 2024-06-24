c7141780ead490bd3931117470b881903e080c49
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-24 19:18:15 +1000

    Create recipeLookup.lua

---
+++ GameData/Vesteria [2376885433]/recipeLookup.lua
@@ -0,0 +1,48 @@
+data = {
+    ["jellyfish-staff-red"] = {
+        ["recipe"] = {{["id"] = 582, ["stacks"] = 20}, {["id"] = 2061, ["stacks"] = 20, ["variation"] = "red"}},
+        ["rewards"] = {{["id"] = 2060, ["variation"] = "red"}}
+    },
+    ["jellyfish-staff-blue"] = {
+        ["recipe"] = {{["id"] = 582, ["stacks"] = 20}, {["id"] = 2061, ["stacks"] = 40, ["variation"] = "blue"}},
+        ["rewards"] = {{["id"] = 2060, ["variation"] = "blue"}}
+    },
+    ["tide-hunter-mask"] = {
+        ["recipe"] = {{["id"] = 411, ["stacks"] = 30}, {["id"] = 96, ["stacks"] = 1}},
+        ["rewards"] = {{["id"] = 502}}
+    },
+    ["copper-axe"] = {
+        ["recipe"] = {{["id"] = 750, ["stacks"] = 18}, {["id"] = 701, ["stacks"] = 6}},
+        ["rewards"] = {{["id"] = 894}}
+    },
+    ["copper-pickaxe"] = {
+        ["recipe"] = {{["id"] = 750, ["stacks"] = 18}, {["id"] = 701, ["stacks"] = 6}},
+        ["rewards"] = {{["id"] = 891}}
+    },
+    ["wavebreaker-helmet"] = {
+        ["recipe"] = {{["id"] = 411, ["stacks"] = 30}, {["id"] = 143, ["stacks"] = 1}},
+        ["rewards"] = {{["id"] = 503}}
+    },
+    ["jellyfish-staff"] = {
+        ["recipe"] = {{["id"] = 582, ["stacks"] = 20}, {["id"] = 2061, ["stacks"] = 60}},
+        ["rewards"] = {{["id"] = 2060}}
+    },
+    ["stone-pickaxe"] = {
+        ["recipe"] = {{["id"] = 750, ["stacks"] = 12}, {["id"] = 700, ["stacks"] = 6}},
+        ["rewards"] = {{["id"] = 890}}
+    },
+    ["stone-axe"] = {
+        ["recipe"] = {{["id"] = 750, ["stacks"] = 12}, {["id"] = 700, ["stacks"] = 6}},
+        ["rewards"] = {{["id"] = 893}}
+    },
+    ["gorgog-mech-pet"] = {
+        ["recipe"] = {{["id"] = 523, ["stacks"] = 15}, {["id"] = 540, ["stacks"] = 20}},
+        ["rewards"] = {{["id"] = 522}}
+    },
+    ["oak-axe"] = {["recipe"] = {{["id"] = 750, ["stacks"] = 6}}, ["rewards"] = {{["id"] = 892}}},
+    ["depth-scryer-hat"] = {
+        ["recipe"] = {{["id"] = 411, ["stacks"] = 30}, {["id"] = 31, ["stacks"] = 1}},
+        ["rewards"] = {{["id"] = 501}}
+    },
+    ["oak-pickaxe"] = {["recipe"] = {{["id"] = 750, ["stacks"] = 6}}, ["rewards"] = {{["id"] = 889}}}
+}
