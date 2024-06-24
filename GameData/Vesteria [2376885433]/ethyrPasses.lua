ecb706753e9b5249afdf1179830b4228b4221e67
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-24 19:17:38 +1000

    Create ethyrPasses.lua

---
+++ GameData/Vesteria [2376885433]/ethyrPasses.lua
@@ -0,0 +1,22 @@
+data = {
+    ["emotePack1"] = {
+        ["image"] = "rbxassetid://12447779702",
+        ["name"] = "Deluxe Emotes",
+        ["cost"] = 450,
+        ["productId"] = 135917594,
+        ["description"] = "Grants the ability to use 7 exclusive emotes"
+    },
+    ["portableSell"] = {
+        ["description"] = "Grants the ability to sell items anywhere",
+        ["image"] = "rbxassetid://7181766203",
+        ["name"] = "Sell Anywhere",
+        ["productId"] = 749970457
+    },
+    ["bountyHunter"] = {
+        ["image"] = "rbxassetid://7181766203",
+        ["name"] = "Bounty Hunter",
+        ["cost"] = 600,
+        ["productId"] = 7785243,
+        ["description"] = "Unlock the bounty hunter menu"
+    }
+}
