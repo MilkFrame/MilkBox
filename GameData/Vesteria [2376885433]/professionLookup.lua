e7470cd37ad399a6b3846dd4880ef88d61a176ba
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-24 19:14:05 +1000

    Create professionLookup.lua

---
+++ GameData/Vesteria [2376885433]/professionLookup.lua
@@ -0,0 +1,18 @@
+data = {
+    ["fishing"] = {
+        ["name"] = "Fishing",
+        ["icon"] = "rbxassetid://2585107511",
+        ["getExp"] = "function: 0x828038303432b9dd"
+    },
+    ["woodcutting"] = {
+        ["name"] = "Woodcutting",
+        ["icon"] = "rbxassetid://5344159698",
+        ["getExp"] = "function: 0x828038303432b9dd"
+    },
+    ["mining"] = {
+        ["name"] = "Mining",
+        ["icon"] = "rbxassetid://5344159811",
+        ["getExp"] = "function: 0x828038303432b9dd"
+    },
+    ["crafting"] = {["name"] = "Crafting", ["getExp"] = "function: 0x828038303432b9dd"}
+}
