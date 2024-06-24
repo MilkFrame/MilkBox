e2c6330700d52bf314e2829a0d4e19c4be32cd4a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-24 19:14:29 +1000

    Create dungeonStyles.lua

---
+++ GameData/Vesteria [2376885433]/dungeonStyles.lua
@@ -0,0 +1,47 @@
+data = {
+    ["Normal"] = {
+        ["priority"] = 3,
+        ["styleColor"] = Color3.fromRGB(222, 222, 28),
+        ["responseButtonColor"] = Color3.fromRGB(222, 222, 87)
+    },
+    ["Nightmare"] = {
+        ["priority"] = 7,
+        ["styleColor"] = Color3.fromRGB(78, 29, 222),
+        ["responseButtonColor"] = Color3.fromRGB(116, 87, 222)
+    },
+    ["Expert"] = {
+        ["priority"] = 9,
+        ["styleColor"] = Color3.fromRGB(33, 181, 255),
+        ["responseButtonColor"] = Color3.fromRGB(100, 203, 255)
+    },
+    ["Raid"] = {
+        ["priority"] = 8,
+        ["styleColor"] = Color3.fromRGB(222, 28, 28),
+        ["responseButtonColor"] = Color3.fromRGB(222, 87, 87)
+    },
+    ["Hard"] = {
+        ["priority"] = 4,
+        ["styleColor"] = Color3.fromRGB(222, 125, 28),
+        ["responseButtonColor"] = Color3.fromRGB(222, 155, 87)
+    },
+    ["Challenge"] = {
+        ["priority"] = 5,
+        ["styleColor"] = Color3.fromRGB(222, 125, 28),
+        ["responseButtonColor"] = Color3.fromRGB(222, 155, 87)
+    },
+    ["Insane"] = {
+        ["priority"] = 6,
+        ["styleColor"] = Color3.fromRGB(222, 28, 28),
+        ["responseButtonColor"] = Color3.fromRGB(222, 87, 87)
+    },
+    ["Maxed"] = {
+        ["priority"] = 9,
+        ["styleColor"] = Color3.fromRGB(33, 181, 255),
+        ["responseButtonColor"] = Color3.fromRGB(100, 203, 255)
+    },
+    ["Easy"] = {
+        ["priority"] = 2,
+        ["styleColor"] = Color3.fromRGB(28, 222, 51),
+        ["responseButtonColor"] = Color3.fromRGB(87, 222, 96)
+    }
+}
