62bf3292b45461d2c4b9a1f9de8bb65d718b3654
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-24 19:18:02 +1000

    Create tasksLookup.lua

---
+++ GameData/Vesteria [2376885433]/tasksLookup.lua
@@ -0,0 +1,294 @@
+data = {
+    ["random-gift-5"] = {
+        ["requirement"] = 5,
+        ["title"] = "Buy 5 Random Gifts",
+        ["sort"] = 0,
+        ["stat"] = "bought-random-gifts",
+        ["reward"] = 2
+    },
+    ["giant-10"] = {
+        ["requirement"] = 10,
+        ["title"] = "Defeat 10 giant monsters near your level",
+        ["sort"] = 5,
+        ["stat"] = "giant-kills",
+        ["reward"] = 2
+    },
+    ["heal-100k"] = {
+        ["requirement"] = 100000,
+        ["title"] = "Heal 100,000 HP",
+        ["sort"] = 6,
+        ["stat"] = "healing",
+        ["reward"] = 2
+    },
+    ["ethyr-gift-10k"] = {
+        ["requirement"] = 10000,
+        ["title"] = "Gift 10,000 Ethyr",
+        ["sort"] = 10,
+        ["stat"] = "ethyr-gifted",
+        ["reward"] = 4
+    },
+    ["enemy-200"] = {
+        ["requirement"] = 200,
+        ["title"] = "Defeat 200 monsters near your level",
+        ["sort"] = 3,
+        ["stat"] = "monster-kills",
+        ["reward"] = 1
+    },
+    ["scrolls-9"] = {
+        ["requirement"] = 9,
+        ["title"] = "Apply 9 scrolls to equipment",
+        ["sort"] = 3,
+        ["stat"] = "scrolls-applied",
+        ["reward"] = 1
+    },
+    ["enemy-500"] = {
+        ["requirement"] = 500,
+        ["title"] = "Defeat 500 monsters near your level",
+        ["sort"] = 5,
+        ["stat"] = "monster-kills",
+        ["reward"] = 2
+    },
+    ["playtime-2hr"] = {
+        ["requirement"] = 120,
+        ["title"] = "Play for 2 hours",
+        ["formatProgress"] = "function: 0x64846602c149f32d",
+        ["sort"] = 3,
+        ["stat"] = "playtime",
+        ["reward"] = 1
+    },
+    ["superGiant"] = {
+        ["requirement"] = 1,
+        ["title"] = "Defeat a super giant monster near your level",
+        ["sort"] = 5,
+        ["stat"] = "superGiant-kills",
+        ["reward"] = 2
+    },
+    ["enemy-50"] = {
+        ["requirement"] = 50,
+        ["title"] = "Defeat 50 monsters near your level",
+        ["sort"] = 1,
+        ["stat"] = "monster-kills",
+        ["reward"] = 1
+    },
+    ["mushtropolis-30"] = {
+        ["requirement"] = 30,
+        ["title"] = "Complete wave 30 in Mushtropolis",
+        ["sort"] = 4,
+        ["stat"] = "mushtropolis-wave",
+        ["reward"] = 2
+    },
+    ["sell-500"] = {
+        ["requirement"] = 500,
+        ["title"] = "Sell 500 items at stores",
+        ["sort"] = 1,
+        ["stat"] = "store-items-sold",
+        ["reward"] = 1
+    },
+    ["sell-5000"] = {
+        ["requirement"] = 5000,
+        ["title"] = "Sell 5,000 items at stores",
+        ["sort"] = 4,
+        ["stat"] = "store-items-sold",
+        ["reward"] = 2
+    },
+    ["giant-3"] = {
+        ["requirement"] = 3,
+        ["title"] = "Defeat 3 giant monsters near your level",
+        ["sort"] = 3,
+        ["stat"] = "giant-kills",
+        ["reward"] = 1
+    },
+    ["playtime-30min"] = {
+        ["requirement"] = 30,
+        ["title"] = "Play for 30 minutes",
+        ["formatProgress"] = "function: 0x16b942000d47d56d",
+        ["sort"] = 0,
+        ["stat"] = "playtime",
+        ["reward"] = 1
+    },
+    ["gigaGiant-1"] = {
+        ["requirement"] = 1,
+        ["title"] = "Defeat a colossal monster near your level",
+        ["sort"] = 6,
+        ["stat"] = "gigaGiant-kills",
+        ["reward"] = 2
+    },
+    ["pvp-5"] = {
+        ["requirement"] = 5,
+        ["title"] = "Defeat 5 players near your level",
+        ["sort"] = 4,
+        ["stat"] = "pvp-kills",
+        ["reward"] = 1
+    },
+    ["pvp-30"] = {
+        ["requirement"] = 30,
+        ["title"] = "Defeat 30 players near your level",
+        ["sort"] = 6,
+        ["stat"] = "pvp-kills",
+        ["reward"] = 2
+    },
+    ["heal-2500"] = {
+        ["requirement"] = 2500,
+        ["title"] = "Heal 2,500 HP",
+        ["sort"] = 1,
+        ["stat"] = "healing",
+        ["reward"] = 1
+    },
+    ["pvp-1"] = {
+        ["requirement"] = 1,
+        ["title"] = "Defeat 1 player near your level",
+        ["sort"] = 2,
+        ["stat"] = "pvp-kills",
+        ["reward"] = 1
+    },
+    ["boss-25"] = {
+        ["requirement"] = 25,
+        ["title"] = "Defeat 25 bosses near your level",
+        ["sort"] = 13,
+        ["stat"] = "boss-kills",
+        ["reward"] = 4
+    },
+    ["dark-explode"] = {
+        ["requirement"] = 1,
+        ["title"] = "Blow up an item with a Dark Scroll",
+        ["sort"] = 3,
+        ["stat"] = "dark-explode",
+        ["reward"] = 1
+    },
+    ["chess-1"] = {
+        ["requirement"] = 1,
+        ["title"] = "Win a game of Chess",
+        ["sort"] = 2,
+        ["stat"] = "chess-win",
+        ["reward"] = 1
+    },
+    ["enemy-5k"] = {
+        ["requirement"] = 5000,
+        ["title"] = "Defeat 5,000 monsters near your level",
+        ["sort"] = 15,
+        ["stat"] = "monster-kills",
+        ["reward"] = 6
+    },
+    ["playtime-1hr"] = {
+        ["requirement"] = 60,
+        ["title"] = "Play for 1 hour",
+        ["formatProgress"] = "function: 0xf9178c0361c0810d",
+        ["sort"] = 2,
+        ["stat"] = "playtime",
+        ["reward"] = 1
+    },
+    ["mushtropolis-10"] = {
+        ["requirement"] = 10,
+        ["title"] = "Complete wave 10 in Mushtropolis",
+        ["sort"] = 2,
+        ["stat"] = "mushtropolis-wave",
+        ["reward"] = 1
+    },
+    ["ethyr-gift"] = {
+        ["requirement"] = 1,
+        ["title"] = "Gift Ethyr to another player",
+        ["sort"] = 9,
+        ["stat"] = "ethyr-gifted",
+        ["reward"] = 2
+    },
+    ["captain-quarters-1"] = {
+        ["requirement"] = 1,
+        ["title"] = "Complete Captains Quarters",
+        ["sort"] = 5,
+        ["stat"] = "pirateCaptain-kills",
+        ["reward"] = 2
+    },
+    ["boss-50"] = {
+        ["requirement"] = 50,
+        ["title"] = "Defeat 50 bosses near your level",
+        ["sort"] = 14,
+        ["stat"] = "boss-kills",
+        ["reward"] = 6
+    },
+    ["boss-5"] = {
+        ["requirement"] = 5,
+        ["title"] = "Defeat 5 bosses near your level",
+        ["sort"] = 12,
+        ["stat"] = "boss-kills",
+        ["reward"] = 2
+    },
+    ["random-gift-1"] = {
+        ["requirement"] = 1,
+        ["title"] = "Buy 1 Random Gift",
+        ["sort"] = 0,
+        ["stat"] = "bought-random-gifts",
+        ["reward"] = 1
+    },
+    ["ethyr-gift-1k"] = {
+        ["requirement"] = 1000,
+        ["title"] = "Gift 1,000 Ethyr",
+        ["sort"] = 10,
+        ["stat"] = "ethyr-gifted",
+        ["reward"] = 2
+    },
+    ["playtime-8hr"] = {
+        ["requirement"] = 480,
+        ["title"] = "Play for 8 hours",
+        ["formatProgress"] = "function: 0x873f910310d1745d",
+        ["sort"] = 6,
+        ["stat"] = "playtime",
+        ["reward"] = 2
+    },
+    ["scrolls-3"] = {
+        ["requirement"] = 3,
+        ["title"] = "Apply 3 scrolls to equipment",
+        ["sort"] = 1,
+        ["stat"] = "scrolls-applied",
+        ["reward"] = 1
+    },
+    ["spider-insane-1"] = {
+        ["requirement"] = 1,
+        ["title"] = "Complete Spider Abyss - Insane",
+        ["sort"] = 5,
+        ["stat"] = "spider-insane",
+        ["reward"] = 2
+    },
+    ["scrolls-27"] = {
+        ["requirement"] = 27,
+        ["title"] = "Apply 27 scrolls to equipment",
+        ["sort"] = 5,
+        ["stat"] = "scrolls-applied",
+        ["reward"] = 2
+    },
+    ["playtime-4hr"] = {
+        ["requirement"] = 240,
+        ["title"] = "Play for 4 hours",
+        ["formatProgress"] = "function: 0x1f5cc306507a7afd",
+        ["sort"] = 5,
+        ["stat"] = "playtime",
+        ["reward"] = 2
+    },
+    ["mushtropolis-20"] = {
+        ["requirement"] = 20,
+        ["title"] = "Complete wave 20 in Mushtropolis",
+        ["sort"] = 3,
+        ["stat"] = "mushtropolis-wave",
+        ["reward"] = 1
+    },
+    ["heal-15k"] = {
+        ["requirement"] = 15000,
+        ["title"] = "Heal 15,000 HP",
+        ["sort"] = 3,
+        ["stat"] = "healing",
+        ["reward"] = 1
+    },
+    ["giant-1"] = {
+        ["requirement"] = 1,
+        ["title"] = "Defeat a giant monster near your level",
+        ["sort"] = 1,
+        ["stat"] = "giant-kills",
+        ["reward"] = 1
+    },
+    ["cursed-explode"] = {
+        ["requirement"] = 1,
+        ["title"] = "Blow up an item with a Cursed Scroll",
+        ["sort"] = 3,
+        ["stat"] = "cursed-explode",
+        ["reward"] = 1
+    }
+}
