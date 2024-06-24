a77a520b1ad4f9a95a58e56fe61e3e3f8ff4d7a3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-06-24 18:48:12 +1000

    Create CalendarRewards.lua

---
+++ GameData/Vesteria [2376885433]/CalendarRewards.lua
@@ -0,0 +1,54 @@
+data = {
+    {["itemInfo"] = {["id"] = 869, ["stacks"] = 1}, ["available"] = 1639058400},
+    {["itemInfo"] = {["id"] = 866, ["stacks"] = 6}, ["available"] = 1639144800},
+    {
+        ["ethyrReward"] = "function: 0x659789c5dea6d01d",
+        ["itemInfo"] = {
+            ["Type"] = "Currency",
+            ["Image"] = "rbxassetid://3405408525",
+            ["TierColor"] = Color3.fromRGB(112, 241, 255),
+            ["Amount"] = 20
+        },
+        ["available"] = 1639231200
+    },
+    {["itemInfo"] = {["id"] = 870, ["stacks"] = 1}, ["available"] = 1639317600},
+    {["itemInfo"] = {["id"] = 868, ["stacks"] = 8}, ["available"] = 1639404000},
+    {
+        ["ethyrReward"] = "function: 0xdd5636c55e629b2d",
+        ["itemInfo"] = {
+            ["Type"] = "Currency",
+            ["Image"] = "rbxassetid://3405408525",
+            ["TierColor"] = Color3.fromRGB(112, 241, 255),
+            ["Amount"] = 30
+        },
+        ["available"] = 1639490400
+    },
+    {["itemInfo"] = {["id"] = 108, ["stacks"] = 1}, ["available"] = 1639576800},
+    {["itemInfo"] = {["id"] = 870, ["stacks"] = 1}, ["available"] = 1639663200},
+    {
+        ["ethyrReward"] = "function: 0x923adb3a5da102fd",
+        ["itemInfo"] = {
+            ["Type"] = "Currency",
+            ["Image"] = "rbxassetid://3405408525",
+            ["TierColor"] = Color3.fromRGB(112, 241, 255),
+            ["Amount"] = 40
+        },
+        ["available"] = 1639749600
+    },
+    {["itemInfo"] = {["id"] = 867, ["stacks"] = 12}, ["available"] = 1639836000},
+    {["itemInfo"] = {["id"] = 105, ["stacks"] = 1}, ["available"] = 1639922400},
+    {["itemInfo"] = {["id"] = 870, ["stacks"] = 1}, ["available"] = 1640008800},
+    {
+        ["ethyrReward"] = "function: 0x47c34c4e1d1f954d",
+        ["itemInfo"] = {
+            ["Type"] = "Currency",
+            ["Image"] = "rbxassetid://3405408525",
+            ["TierColor"] = Color3.fromRGB(112, 241, 255),
+            ["Amount"] = 50
+        },
+        ["available"] = 1640095200
+    },
+    {["itemInfo"] = {["id"] = 542, ["stacks"] = 1}, ["available"] = 1640181600},
+    {["itemInfo"] = {["id"] = 898, ["stacks"] = 1}, ["available"] = 1640268000},
+    {["itemInfo"] = {["id"] = 897, ["stacks"] = 1}, ["available"] = 1640354400}
+}
