d372d11c875e29713109813ab3a4d57f9d6571c6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:20:27 +1000

    Create BallService.lua

---
+++ GameData/Welcome to Bloxburg [185655149]/BallService.lua
@@ -0,0 +1,12 @@
+data = {
+    ["AssertThrowable"] = "function: 0x899e845547956803",
+    ["KickBall"] = "function: 0x38f9aed3c4276c43",
+    ["InitHoop"] = "function: 0xd6d79cb77a6ea003",
+    ["AssertKickable"] = "function: 0x82199ef51b39b163",
+    ["InitKickable"] = "function: 0xe51cab7ed36d5743",
+    ["EquipBasketball"] = "function: 0x8c85b9a75a66db03",
+    ["InteractKickable"] = "function: 0x25c2c236a04dc383",
+    ["HIT_ACCURACY_OFFSET"] = 0.15,
+    ["HIT_ACCURACY_CENTER"] = 0.7,
+    ["KICK_SPEED"] = 30
+}
