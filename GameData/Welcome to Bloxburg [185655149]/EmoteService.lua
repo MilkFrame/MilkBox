d4e7949cd797c4e017fd9c86ee7e74ef7f7717f8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:21:48 +1000

    Create EmoteService.lua

---
+++ GameData/Welcome to Bloxburg [185655149]/EmoteService.lua
@@ -0,0 +1,13 @@
+data = {
+    ["AddInteractions"] = "function: 0x73c2dc41ad533283",
+    ["OnCustomEmotesChangedObject"] = "OnCustomEmotesChanged",
+    ["GetEmote"] = "function: 0xb4e5a6baf7b62523",
+    ["OnCustomEmotesChanged"] = "Signal Event",
+    ["RefreshEmotes"] = "function: 0xac03b4f2d0586983",
+    ["IsEmoteLocked"] = "function: 0xe7fbdef27ac19a43",
+    ["PlayEmoteAnimation"] = "function: 0x2761719299b6a703",
+    ["GetEmotesFlat"] = "function: 0xd6758f3913f99c83",
+    ["GetEmotes"] = "function: 0xda59200de4a4e3c3",
+    ["IsAnimationLocked"] = "function: 0x6cf1419b238216d3",
+    ["DoEmote"] = "function: 0x1416fd5b3496cbc3"
+}
