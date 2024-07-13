e9fe85ed990ebeac815999e5d2c602764dda945e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:13:58 +1000

    Create MinigameConfig.lua

---
+++ GameData/Socks Minigames [12191579104]/MinigameConfig.lua
@@ -0,0 +1,363 @@
+data = {
+    ["CarObby"] = {["Map"] = {["Default"] = "rbxassetid://17094482573"}, ["DisplayText"] = "Car Obby"},
+    ["Infection"] = {
+        ["Map"] = {["Default"] = "rbxassetid://17094755244", ["Lab Infection"] = "rbxassetid://17094755244"},
+        ["DisplayText"] = "Infection"
+    },
+    ["BennysBackroom"] = {
+        ["Map"] = {["Default"] = "rbxassetid://17684860043", ["Backrooms"] = "rbxassetid://17684860043"},
+        ["DisplayText"] = "Benny's Backrooms"
+    },
+    ["Doors"] = {
+        ["Map"] = {
+            ["Pyramid Doors"] = "rbxassetid://17822306915",
+            ["Default"] = "rbxassetid://17181930562",
+            ["Mansion Doors"] = "rbxassetid://17181930562"
+        },
+        ["DisplayText"] = "Doors"
+    },
+    ["RPGMinigame"] = {["Map"] = {["Default"] = "rbxassetid://"}, ["DisplayText"] = "RPG Battles"},
+    ["Dodgeball"] = {
+        ["Map"] = {
+            ["Meadows Dodgeball"] = "rbxassetid://17094571349",
+            ["Default"] = "rbxassetid://17094571349",
+            ["Volcano Dodgeball"] = "rbxassetid://17094571008",
+            ["Forest Dodgeball"] = "rbxassetid://17094571159",
+            ["Beach Dodgeball"] = "rbxassetid://17094571540"
+        },
+        ["DisplayText"] = "Dodgeball"
+    },
+    ["HighLow"] = {
+        ["Map"] = {
+            ["Desert Higher Or Lower"] = "rbxassetid://17094748555",
+            ["Default"] = "rbxassetid://17094748319",
+            ["Higher Or Lower"] = "rbxassetid://17094748319"
+        },
+        ["DisplayText"] = "Higher Or Lower"
+    },
+    ["BallRace"] = {
+        ["Map"] = {
+            ["Desert Sisyphus"] = "rbxassetid://17523569204",
+            ["Default"] = "rbxassetid://17094392769",
+            ["Jungle Sisyphus"] = "rbxassetid://17094392769"
+        },
+        ["DisplayText"] = "Sisyphus"
+    },
+    ["Coin"] = {
+        ["Map"] = {
+            ["Winter Coin Frenzy"] = "rbxassetid://17094524621",
+            ["Forest Coin Frenzy"] = "rbxassetid://17340552552",
+            ["Farm Coin Frenzy"] = "rbxassetid://17094525503",
+            ["Jungle Temple Coin Frenzy"] = "rbxassetid://17295043979",
+            ["Jungle Coin Frenzy"] = "rbxassetid://17094525326",
+            ["Default"] = "rbxassetid://17094524895",
+            ["Pirate Cove Coin Frenzy"] = "rbxassetid://17094524895",
+            ["Arctic Coin Frenzy"] = "rbxassetid://17295044183",
+            ["Lab Coin Frenzy"] = "rbxassetid://17094525144"
+        },
+        ["DisplayText"] = "Coin Frenzy"
+    },
+    ["PixelPainter"] = {
+        ["UseGameNameOnly"] = true,
+        ["DisplayText"] = "Pixel Painter",
+        ["Map"] = {["Default"] = "rbxassetid://17095724069", ["PixelPainter"] = "rbxassetid://17095724069"}
+    },
+    ["CannonEscape"] = {
+        ["Map"] = {["Default"] = "rbxassetid://17598955975", ["JungleCannon"] = "rbxassetid://17598955975"},
+        ["DisplayText"] = "Cannon Escape"
+    },
+    ["ZombieWaves"] = {
+        ["Map"] = {
+            ["Jungle Zombie Horde"] = "rbxassetid://17096016724",
+            ["Default"] = "rbxassetid://17096016976",
+            ["City Zombie Horde"] = "rbxassetid://17096016976"
+        },
+        ["DisplayText"] = "Zombie Horde"
+    },
+    ["Ramp"] = {
+        ["Map"] = {
+            ["Desert Avalanche"] = "rbxassetid://17095793373",
+            ["Default"] = "rbxassetid://17095793249",
+            ["Forest Avalanche"] = "rbxassetid://17095793249",
+            ["City Avalanche"] = "rbxassetid://17095793556",
+            ["Snow Avalanche"] = "rbxassetid://17095793148"
+        },
+        ["DisplayText"] = "Avalanche"
+    },
+    ["TileBattles"] = {
+        ["Map"] = {
+            ["Jungle Tile Battles"] = "rbxassetid://17095978479",
+            ["Default"] = "rbxassetid://17095978562",
+            ["CandyLand Tile Battles"] = "rbxassetid://17095978637",
+            ["Volcano Tile Battles"] = "rbxassetid://17095978349",
+            ["Tile Battles"] = "rbxassetid://17095978562"
+        },
+        ["DisplayText"] = "Tile Battles",
+        ["Predicate"] = "function: 0xed705326858da799",
+        ["OpposingGame"] = "TeamTileBattles"
+    },
+    ["CantTouchColor"] = {
+        ["Map"] = {
+            ["Dont Touch Red"] = "rbxassetid://17494012015",
+            ["Dont Touch Green"] = "rbxassetid://17094472994",
+            ["Default"] = "rbxassetid://17094473148",
+            ["Dont Touch Blue"] = "rbxassetid://17094473148"
+        },
+        ["DisplayText"] = "Don't Touch Color"
+    },
+    ["SabotageMiniGame"] = {
+        ["Map"] = {
+            ["Forest Sabotage"] = "rbxassetid://17095860622",
+            ["Graveyard Sabotage"] = "rbxassetid://17601814323",
+            ["Default"] = "rbxassetid://17095860351",
+            ["Red Vs Blue Sabotage"] = "rbxassetid://17095860351",
+            ["Desert Sabotage"] = "rbxassetid://17095860841"
+        },
+        ["DisplayText"] = "Sabotage"
+    },
+    ["CircuitRace"] = {
+        ["Map"] = {
+            ["Default"] = "rbxassetid://17094496706",
+            ["Jungle Circuit Racer"] = "rbxassetid://17094496504",
+            ["Forest Circuit Racer"] = "rbxassetid://17094496706",
+            ["Desert Circuit Racer"] = "rbxassetid://17094496706",
+            ["Winter Circuit Racer"] = "rbxassetid://17094496196"
+        },
+        ["DisplayText"] = "Circuit Racer"
+    },
+    ["RocketBall"] = {
+        ["Map"] = {
+            ["Jungle Rocket Ball"] = "rbxassetid://17095847276",
+            ["Default"] = "rbxassetid://17095847276",
+            ["Rooftop Rocket Ball"] = "rbxassetid://17095847519"
+        },
+        ["DisplayText"] = "Rocket Ball"
+    },
+    ["DropperMiniGame"] = {
+        ["UseGameNameOnly"] = true,
+        ["DisplayText"] = "Dropper",
+        ["Map"] = {["Default"] = "rbxassetid://17094590900"}
+    },
+    ["TeamTileBattles"] = {
+        ["Map"] = {
+            ["Team Tile Battles"] = "rbxassetid://17095945188",
+            ["Hills Team Tile Battles"] = "rbxassetid://17095944809",
+            ["Default"] = "rbxassetid://17095945188"
+        },
+        ["DisplayText"] = "Tile Battles (Team)",
+        ["Predicate"] = "function: 0xdad3bcec408d7c39",
+        ["OpposingGame"] = "TileBattles"
+    },
+    ["SpinnerMayhem"] = {
+        ["Map"] = {["Default"] = "rbxassetid://17522191123", ["Spinner Forest"] = "rbxassetid://17522191123"},
+        ["DisplayText"] = "Spinner Mayhem"
+    },
+    ["GlassBridge"] = {
+        ["Map"] = {
+            ["Volcano Glass Bridge"] = "rbxassetid://17094736916",
+            ["Default"] = "rbxassetid://17094737141",
+            ["Desert Glass Bridge"] = "rbxassetid://17094737247",
+            ["Space Glass Bridge"] = "rbxassetid://17094737054",
+            ["Jungle Glass Bridge"] = "rbxassetid://17094737141"
+        },
+        ["DisplayText"] = "Glass Bridge"
+    },
+    ["PressTheButtons"] = {
+        ["Map"] = {
+            ["Lab Press The Buttons"] = "rbxassetid://17095759093",
+            ["Default"] = "rbxassetid://17095760012",
+            ["Bedroom Press The Buttons"] = "rbxassetid://17095760012",
+            ["Jail Press The Buttons"] = "rbxassetid://17095759426",
+            ["Farm Press The Buttons"] = "rbxassetid://17095759840",
+            ["Kitchen Press The Buttons"] = "rbxassetid://17095759239"
+        },
+        ["UseGameNameOnly"] = true,
+        ["DisplayText"] = "Find The Buttons",
+        ["Predicate"] = "function: 0x7b8fec9e5397b1f9",
+        ["OpposingGame"] = "TeamPressTheButtons"
+    },
+    ["SledRace"] = {
+        ["Map"] = {
+            ["Winter Ski Race"] = "rbxassetid://17095893021",
+            ["Default"] = "rbxassetid://17095893021",
+            ["Ski Race Desert"] = "rbxassetid://17095893234"
+        },
+        ["DisplayText"] = "Ski Race"
+    },
+    ["Soccer"] = {
+        ["Map"] = {
+            ["Desert Soccer"] = "rbxassetid://17095916391",
+            ["MOD Winter Soccer"] = "rbxassetid://17095916051",
+            ["Forest Soccer"] = "rbxassetid://17095916556",
+            ["Cherry Blossom Soccer"] = "rbxassetid://16539537648",
+            ["Lava Soccer"] = "rbxassetid://16885115344",
+            ["Winter Soccer"] = "rbxassetid://16539536348",
+            ["Default"] = "rbxassetid://17095916556",
+            ["MOD Blossom Soccer"] = "rbxassetid://17095916728",
+            ["MOD Desert Soccer"] = "rbxassetid://16971925297",
+            ["Moon Soccer"] = "rbxassetid://16539536844",
+            ["MOD Lava Soccer"] = "rbxassetid://17095916250",
+            ["MOD Moon Soccer"] = "rbxassetid://17095916157"
+        },
+        ["DisplayText"] = "Soccer"
+    },
+    ["Sled"] = {
+        ["Map"] = {
+            ["Beach Ramp Of Doom"] = "rbxassetid://17095877594",
+            ["Moon Ramp Of Doom"] = "rbxassetid://17095877078",
+            ["Default"] = "rbxassetid://17095877213",
+            ["Forest Ramp Of Doom"] = "rbxassetid://17095877213",
+            ["Desert Ramp Of Doom"] = "rbxassetid://17095877414"
+        },
+        ["DisplayText"] = "Ramp Of Doom"
+    },
+    ["Volcano"] = {
+        ["Map"] = {["Default"] = "rbxassetid://17096008202", ["Volcano Madness"] = "rbxassetid://17096008202"},
+        ["DisplayText"] = "Volcano Madness"
+    },
+    ["Jetpack"] = {
+        ["Map"] = {["Default"] = "rbxassetid://17094761386", ["Rainbow Jetpack Mania"] = "rbxassetid://17094761386"},
+        ["DisplayText"] = "Jetpack Mania"
+    },
+    ["RobuxMiniGame"] = {
+        ["Map"] = {
+            ["City Bobux Heist"] = "rbxassetid://17095829956",
+            ["Desert Bobux Heist"] = "rbxassetid://17095829850",
+            ["Default"] = "rbxassetid://17095829701",
+            ["Forest Bobux Heist"] = "rbxassetid://17095829701"
+        },
+        ["DisplayText"] = "Bobux Heist"
+    },
+    ["CantTouchBlue"] = {
+        ["Map"] = {["Default"] = "rbxassetid://16539115182", ["BLUE"] = "rbxassetid://16539115182"},
+        ["DisplayText"] = "Don't Touch Blue"
+    },
+    ["EggHunt"] = {
+        ["Map"] = {
+            ["Default"] = "rbxassetid://17094661883",
+            ["EggHuntWinter"] = "rbxassetid://17094660762",
+            ["EggHuntOcean"] = "rbxassetid://17094661152",
+            ["EggHuntMilitary"] = "rbxassetid://17094661804",
+            ["EggHuntLava"] = "rbxassetid://17094661009",
+            ["EggHuntMagic"] = "rbxassetid://17094661883"
+        },
+        ["DisplayText"] = "Egg Hunt"
+    },
+    ["ObbyRace"] = {
+        ["Map"] = {
+            ["Pyramid Deathrun"] = "rbxassetid://17095698159",
+            ["Desert Deathrun"] = "rbxassetid://17095698669",
+            ["Volcano Deathrun"] = "rbxassetid://17095697938",
+            ["Cove Deathrun"] = "rbxassetid://17768194714",
+            ["Default"] = "rbxassetid://17095698968",
+            ["Icy Deathrun"] = "rbxassetid://17095698431",
+            ["Rainbow Deathrun"] = "rbxassetid://17095698038",
+            ["Gamer Deathrun"] = "rbxassetid://17095698968",
+            ["Jungle Deathrun"] = "rbxassetid://17095698261",
+            ["Candyland Deathrun"] = "rbxassetid://17095699166"
+        },
+        ["DisplayText"] = "Obby Race"
+    },
+    ["LastToLeaveCircle"] = {
+        ["Map"] = {
+            ["Candyland Circle Of Doom"] = "rbxassetid://17095693477",
+            ["Desert Circle Of Doom"] = "rbxassetid://17095693295",
+            ["Default"] = "rbxassetid://17095693175",
+            ["Jungle Circle Of Doom"] = "rbxassetid://17095693175"
+        },
+        ["DisplayText"] = "Circle Of Doom"
+    },
+    ["KingOfTheHill"] = {
+        ["Map"] = {
+            ["Default"] = "rbxassetid://17094775704",
+            ["Winter KOTH"] = "rbxassetid://17094774925",
+            ["Candyland KOTH"] = "rbxassetid://17094775435",
+            ["Radioactive KOTH"] = "rbxassetid://17094775068",
+            ["Volcano KOTH"] = "rbxassetid://17094774717",
+            ["Beach KOTH"] = "rbxassetid://17094775704"
+        },
+        ["DisplayText"] = "King Of The Hill"
+    },
+    ["FallDamage"] = {
+        ["Map"] = {
+            ["Desert Leap Of Faith"] = "rbxassetid://17094699374",
+            ["Default"] = "rbxassetid://17094699178",
+            ["Forest Leap Of Faith"] = "rbxassetid://17094699178",
+            ["Graveyard Leap Of Faith"] = "rbxassetid://17683065997",
+            ["Winter Leap Of Faith"] = "rbxassetid://17094698730",
+            ["Nuclear Leap Of Faith"] = "rbxassetid://17094698961"
+        },
+        ["DisplayText"] = "Leap Of Faith"
+    },
+    ["BankHeist"] = {
+        ["Map"] = {
+            ["BankHeistDefault"] = "rbxassetid://17094458257",
+            ["Default"] = "rbxassetid://17094458257",
+            ["Bank Heist Wild West"] = "rbxassetid://17094458080"
+        },
+        ["DisplayText"] = "Bank Heist"
+    },
+    ["TileTower"] = {
+        ["Map"] = {["Default"] = "rbxassetid://17095986114", ["Lava Tile Tower"] = "rbxassetid://17095986114"},
+        ["DisplayText"] = "Tile Battles (Tower)"
+    },
+    ["TapRacer"] = {
+        ["Map"] = {
+            ["Desert Tap Racer"] = "rbxassetid://17095779342",
+            ["Default"] = "rbxassetid://17095779614",
+            ["Forest Tap Racer"] = "rbxassetid://17095779195",
+            ["Plains Tap Racer"] = "rbxassetid://17095779614",
+            ["Volcano Tap Racer"] = "rbxassetid://17095779027"
+        },
+        ["DisplayText"] = "Tap Racer"
+    },
+    ["RedLightGreenLight"] = {
+        ["Map"] = {
+            ["Forest RLGL"] = "rbxassetid://17095816504",
+            ["Default"] = "rbxassetid://17095816504",
+            ["Graveyard RLGL"] = "rbxassetid://17661556180",
+            ["Snowy RLGL"] = "rbxassetid://17095816184",
+            ["Wasteland RLGL"] = "rbxassetid://17095816009",
+            ["Volcano RLGL"] = "rbxassetid://17095817575"
+        },
+        ["DisplayText"] = "Red Light, Green Light"
+    },
+    ["Plinko"] = {
+        ["Map"] = {
+            ["Arcade Plinko"] = "rbxassetid://17095726535",
+            ["Default"] = "rbxassetid://17095726423",
+            ["Basic Plinko"] = "rbxassetid://17095726423"
+        },
+        ["DisplayText"] = "Plinko"
+    },
+    ["LightChase"] = {
+        ["Map"] = {["Default"] = "rbxassetid://17845655268", ["LightChaseDefault"] = "rbxassetid://17845655268"},
+        ["DisplayText"] = "Light Chase"
+    },
+    ["ColorBlock"] = {
+        ["Map"] = {["Default"] = "rbxassetid://17094537616", ["ColorBlock"] = "rbxassetid://17094537616"},
+        ["DisplayText"] = "Color Block"
+    },
+    ["UFO"] = {
+        ["Map"] = {["Default"] = "rbxassetid://17096001852", ["Farm UFO Invasion"] = "rbxassetid://17096001852"},
+        ["DisplayText"] = "UFO Invasion"
+    },
+    ["TeamPressTheButtons"] = {
+        ["Map"] = {
+            ["Lab Press The Buttons"] = "rbxassetid://17095759093",
+            ["Default"] = "rbxassetid://17095760012",
+            ["Bedroom Press The Buttons"] = "rbxassetid://17095760012",
+            ["Jail Press The Buttons"] = "rbxassetid://17095759426",
+            ["Farm Press The Buttons"] = "rbxassetid://17095759840",
+            ["Kitchen Press The Buttons"] = "rbxassetid://17095759239"
+        },
+        ["UseGameNameOnly"] = true,
+        ["DisplayText"] = "Find The Buttons (Team)",
+        ["Predicate"] = "function: 0x2b9904070eb283d9",
+        ["OpposingGame"] = "PressTheButtons"
+    },
+    ["Delivery"] = {
+        ["Map"] = {["Default"] = "rbxassetid://17094543580", ["City Package Delivery"] = "rbxassetid://17094543580"},
+        ["DisplayText"] = "Package Delivery"
+    }
+}
