802ef3f4f3fe77bf21d3a25dc33303359cc159c6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:15:22 +1000

    Create ShopItems.lua

---
+++ GameData/Socks Minigames [12191579104]/ShopItems.lua
@@ -0,0 +1,104 @@
+data = {
+    ["Pool"] = {
+        ["CyberpunkChest"] = {
+            ["Price"] = 300,
+            ["Tier"] = 2,
+            ["Icon"] = "rbxassetid://15358757997",
+            ["DisplayName"] = "Cyberpunk"
+        },
+        ["PartyChest"] = {
+            ["Price"] = 300,
+            ["Tier"] = 2,
+            ["Icon"] = "rbxassetid://15230562353",
+            ["DisplayName"] = "Party"
+        },
+        ["TropicalChest"] = {
+            ["Price"] = 100,
+            ["Tier"] = 1,
+            ["Icon"] = "rbxassetid://17116112679",
+            ["DisplayName"] = "Tropical"
+        },
+        ["RoyalChest"] = {
+            ["Price"] = 100,
+            ["Tier"] = 1,
+            ["Icon"] = "rbxassetid://15194507432",
+            ["DisplayName"] = "Royal"
+        },
+        ["UndeadChest"] = {
+            ["Price"] = 300,
+            ["Tier"] = 2,
+            ["Icon"] = "rbxassetid://15194514668",
+            ["DisplayName"] = "Undead"
+        },
+        ["WildChest"] = {["Price"] = 300, ["Tier"] = 2, ["Icon"] = "rbxassetid://15515613009", ["DisplayName"] = "Wild"},
+        ["UnderworldChest"] = {
+            ["Price"] = 500,
+            ["Tier"] = 3,
+            ["Icon"] = "rbxassetid://16829290914",
+            ["DisplayName"] = "Underworld"
+        },
+        ["ColorfulChest"] = {
+            ["Price"] = 100,
+            ["Tier"] = 1,
+            ["Icon"] = "rbxassetid://15194517334",
+            ["DisplayName"] = "Colorful"
+        },
+        ["IcyChest"] = {["Price"] = 100, ["Tier"] = 1, ["Icon"] = "rbxassetid://16027858952", ["DisplayName"] = "Icy"},
+        ["FantasyChest"] = {
+            ["Price"] = 500,
+            ["Tier"] = 3,
+            ["Icon"] = "rbxassetid://15194509409",
+            ["DisplayName"] = "Fantasy"
+        },
+        ["ChallengerChest"] = {
+            ["Price"] = 500,
+            ["Tier"] = 3,
+            ["Icon"] = "rbxassetid://15289516424",
+            ["DisplayName"] = "Challenger"
+        },
+        ["DarkChest"] = {["Price"] = 500, ["Tier"] = 3, ["Icon"] = "rbxassetid://17660020802", ["DisplayName"] = "Dark"},
+        ["SpaceChest"] = {
+            ["Price"] = 300,
+            ["Tier"] = 2,
+            ["Icon"] = "rbxassetid://17331230080",
+            ["DisplayName"] = "Space"
+        },
+        ["FurryChest"] = {
+            ["Price"] = 300,
+            ["Tier"] = 2,
+            ["Icon"] = "rbxassetid://16746114383",
+            ["DisplayName"] = "Furry"
+        },
+        ["AdventureChest"] = {
+            ["Price"] = 100,
+            ["Tier"] = 1,
+            ["Icon"] = "rbxassetid://15571669750",
+            ["DisplayName"] = "Adventure"
+        },
+        ["HeavenlyChest"] = {
+            ["Price"] = 500,
+            ["Tier"] = 3,
+            ["Icon"] = "rbxassetid://16258070882",
+            ["DisplayName"] = "Heavenly"
+        },
+        ["GamerChest"] = {
+            ["Price"] = 300,
+            ["Tier"] = 2,
+            ["Icon"] = "rbxassetid://15194511786",
+            ["DisplayName"] = "Gamer"
+        },
+        ["CircusChest"] = {
+            ["Price"] = 300,
+            ["Tier"] = 2,
+            ["Icon"] = "rbxassetid://16101560788",
+            ["DisplayName"] = "Circus"
+        },
+        ["MedievalChest"] = {
+            ["Price"] = 500,
+            ["Tier"] = 3,
+            ["Icon"] = "rbxassetid://15940085276",
+            ["DisplayName"] = "Medieval"
+        }
+    },
+    ["NewArrival"] = {["Id"] = "DarkChest", ["EndTime"] = 1718423362}
+}
