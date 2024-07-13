98c678591000b31362e8ac6d587fdc24d3761816
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:32:47 +1000

    Create StreamingService.lua

---
+++ GameData/Welcome to Bloxburg [185655149]/StreamingService.lua
@@ -0,0 +1,14 @@
+data = {
+    ["WaitForReferencedDescendants"] = "function: 0xaf1aa8cfd92c9213",
+    ["ReparentPrimaryPart"] = "function: 0xe078efe58004a043",
+    ["GetReplicatedPrimaryPartPosition"] = "function: 0xdcbf265e69b8f863",
+    ["SeparateWorkspacePart"] = "function: 0x38f27c383d34b103",
+    ["WaitForProperty"] = "function: 0x6cbb1a8412e94fd3",
+    ["CreateReference"] = "function: 0x56c8ca89858ad103",
+    ["CreateDescendantReferences"] = "function: 0xe69e6554daec6dc3",
+    ["WaitForReferencedObject"] = "function: 0x5fb24959a37a6103",
+    ["WaitForValue"] = "function: 0xefea79a37f59d473",
+    ["WaitForPrimaryPart"] = "function: 0xbf438e10ad880b23",
+    ["ReplicatePrimaryPartPosition"] = "function: 0x9e13c7f897c692c3",
+    ["ReplicateClientClone"] = "function: 0x550213c1ef524043"
+}
