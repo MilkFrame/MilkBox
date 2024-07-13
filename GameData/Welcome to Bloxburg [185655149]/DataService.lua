86d6a1a073c0764a404da3d75dd3c0f57fba32e0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:21:30 +1000

    Create DataService.lua

---
+++ GameData/Welcome to Bloxburg [185655149]/DataService.lua
@@ -0,0 +1,12 @@
+data = {
+    ["FireLocal"] = "function: 0x74ca1294ce8f9573",
+    ["IsClient"] = "function: 0xf4306c575025b443",
+    ["HookFunction"] = "function: 0x0378bd784ec62683",
+    ["OnEvent"] = "function: 0xf4103169c2bf2e83",
+    ["HookEvent"] = "function: 0x0bf2c22fe4172ac3",
+    ["FireServer"] = "function: 0xe41170edf57e7923",
+    ["OnInvoke"] = "function: 0x2c33b32abb9c4303",
+    ["InvokeServer"] = "function: 0x06bdf9b31d6e2b83",
+    ["WaitEvent"] = "function: 0xfd6debcb0d5e6d23",
+    ["InvokeLocal"] = "function: 0xb652bfff5be7c003"
+}
