c3567bf63da97a07013f2ad0b604676bd0f3d895
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:32:59 +1000

    Create SubscriptionService.lua

---
+++ GameData/Welcome to Bloxburg [185655149]/SubscriptionService.lua
@@ -0,0 +1,47 @@
+data = {
+    ["PurchaseSubscription"] = "function: 0x051fa63ee4bc3b43",
+    ["SubscriptionDataLoad"] = "SubscriptionDataLoad",
+    ["LoadData"] = "function: 0x743c2e52406a7363",
+    ["QuerySubscriptionStatusRemote"] = "QuerySubscriptionStatus",
+    ["CalculateCurrencyRewards"] = "function: 0x3dd9925268832d83",
+    ["HasSeenSubscriptions"] = true,
+    ["ClientSupportsSubscriptions"] = "function: 0x0872b838040081b3",
+    ["QuerySubscriptionConsecutivePeriodsRemote"] = "QuerySubscriptionConsecutivePeriods",
+    ["SUBSCRIPTIONS"] = {
+        ["CurrencyPack1"] = {
+            ["Rewards"] = {
+                {
+                    ["ConsecutiveRewards"] = {
+                        {["AmountBlockbux"] = 250, ["AmountMoney"] = 20000},
+                        {["AmountBlockbux"] = 300, ["AmountMoney"] = 25000},
+                        {["AmountBlockbux"] = 350, ["AmountMoney"] = 30000}
+                    },
+                    ["Type"] = "Currency"
+                }
+            },
+            ["SubscriptionID"] = "EXP-5390182541637189742"
+        },
+        ["CurrencyPack2"] = {
+            ["Rewards"] = {
+                {
+                    ["ConsecutiveRewards"] = {
+                        {["AmountBlockbux"] = 650, ["AmountMoney"] = 60000},
+                        {["AmountBlockbux"] = 700, ["AmountMoney"] = 65000},
+                        {["AmountBlockbux"] = 750, ["AmountMoney"] = 70000}
+                    },
+                    ["Type"] = "Currency"
+                }
+            },
+            ["SubscriptionID"] = "EXP-7477809566895046766"
+        }
+    },
+    ["QuerySubscriptionHistoryRemote"] = "QuerySubscriptionHistory",
+    ["GetSubscriptionDataFromID"] = "function: 0xc76cd70cf43b3983",
+    ["QuerySubscriptionPeriod"] = "function: 0x4bf82d0eddc29743",
+    ["QuerySubscriptionInfo"] = "function: 0x12292c3443275ed3",
+    ["SetPlayerHasSeenSubs"] = "function: 0xd6c4f2e74a1ba493",
+    ["SubscriptionHasSeenRemote"] = "SubscriptionHasSeen",
+    ["QuerySubscriptionStatus"] = "function: 0xe4ecc391d3ec6fc3",
+    ["HAS_SUPPORT"] = false,
+    ["QuerySubscriptionHistory"] = "function: 0xd2721a5580ed8383"
+}
