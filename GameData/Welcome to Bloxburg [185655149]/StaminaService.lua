7843d01a908a23af2a1903df9bd687ecfdd57d5f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:32:35 +1000

    Create StaminaService.lua

---
+++ GameData/Welcome to Bloxburg [185655149]/StaminaService.lua
@@ -0,0 +1,25 @@
+data = {
+    ["GetStaminaValuePerLevel"] = "function: 0x3436db8c0277cf83",
+    ["GetDefaultMaxStaminaValue"] = "function: 0x753b7347d1174bc3",
+    ["RunButton"] = {
+        ["TouchControlFrame"] = "TouchControlFrame",
+        ["_maid"] = {["_tasks"] = {"Connection", "Connection"}},
+        ["DefaultIcon"] = "rbxassetid://13388646891",
+        ["DefaultStartAction"] = "function: 0x07079cc5017f50a3",
+        ["StartAction"] = "function: 0x07079cc5017f50a3",
+        ["DefaultEndAction"] = "function: 0x07af2c53bb94a5f3",
+        ["IsLoaded"] = true,
+        ["IsVisible"] = true,
+        ["EndAction"] = "function: 0x07af2c53bb94a5f3",
+        ["GUI"] = "RunButton"
+    },
+    ["GetMaxStaminaValueWithBoosters"] = "function: 0x78a6028f2a100c63",
+    ["ResetStaminaBoost"] = "function: 0x1946389b358751a3",
+    ["StartSprinting"] = "function: 0xbd4faf806c058543",
+    ["StopSprinting"] = "function: 0x9dbf96db884122a3",
+    ["GetSprintSpeedModifier"] = "function: 0x186bd27267d77223",
+    ["GetMaxStaminaValue"] = "function: 0x375ea7169df14f93",
+    ["BoostStamina"] = "function: 0x23154e1ff285af83",
+    ["CanSprint"] = "function: 0x260efde5603156b3",
+    ["GetStaminaMultiplier"] = "function: 0x11f0232bd8e7c1e3"
+}
