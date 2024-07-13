328e09b7cb06b0f23cea0db5d2b7bf7cfccfaf84
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:23:50 +1000

    Create IKService.lua

---
+++ GameData/Welcome to Bloxburg [185655149]/IKService.lua
@@ -0,0 +1,14 @@
+data = {
+    ["SolveIK"] = "function: 0xd982856691260053",
+    ["GetCurrentIKValues"] = "function: 0x9a19d091871ddbc3",
+    ["AddIK"] = "function: 0x9dc9adbb2dc87ec3",
+    ["_tempTargetPart"] = "Part",
+    ["RemoveIK"] = "function: 0x9d1c3e7092af0003",
+    ["GetLimbWeld"] = "function: 0xba84cef1628961a3",
+    ["InitIKSolver"] = "function: 0x9e220f2c438e7743",
+    ["CreateValuePart"] = "function: 0xe44b9ba121bfa683",
+    ["CreateIKParts"] = "function: 0x62e0070f6897d393",
+    ["CharTargets"] = {},
+    ["_addIKData"] = "function: 0xc37b49a32470c943",
+    ["AddIKFromTargets"] = "function: 0x35b34e33fb58a9f3"
+}
