55a0337f6ff3dd2cc5b7591dab0414a8865d7581
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:20:54 +1000

    Create CleaningService.lua

---
+++ GameData/Welcome to Bloxburg [185655149]/CleaningService.lua
@@ -0,0 +1,9 @@
+data = {
+    ["GetCleanLevel"] = "function: 0x93fbe28658ee1f93",
+    ["DirtyThreshold"] = 0.2,
+    ["GetCombinedStat"] = "function: 0x8b53de2fd7b57c63",
+    ["AddDirty"] = "function: 0x8c8302c07d75b1c3",
+    ["GetCombinedQuality"] = "function: 0x822badb5355de6b3",
+    ["GetDirtyLevel"] = "function: 0x44075ed267b5d143",
+    ["GetDirtyColor"] = "function: 0xaa2387724d973a83"
+}
