a47c7e6054064eb67be5cc3629bc3a6dee8c7a44
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:32:09 +1000

    Create RoofService.lua

---
+++ GameData/Welcome to Bloxburg [185655149]/RoofService.lua
@@ -0,0 +1,78 @@
+data = {
+    ["UpdateChimney"] = "function: 0x13a5abe1191d47a3",
+    ["RoofProperties"] = {
+        ["Gambrel Roof"] = {
+            {["Min"] = 4, ["Name"] = "Height", ["Max"] = 24, ["Res"] = 0.3, ["Default"] = 12},
+            {["Min"] = 0, ["Name"] = "Overhang", ["Max"] = 5, ["Res"] = 0.25, ["Default"] = 2},
+            {["Min"] = 0.25, ["Name"] = "Thickness", ["Max"] = 3, ["Res"] = 0.25, ["Default"] = 0.5}
+        },
+        ["Pyramid Roof"] = {
+            {["Min"] = 2, ["Name"] = "Height", ["Max"] = 24, ["Res"] = 0.3, ["Default"] = 4},
+            {["Min"] = 0.1, ["Name"] = "Overhang", ["Max"] = 1, ["Res"] = 0.1, ["Default"] = 1}
+        },
+        ["Hip Roof"] = {
+            {["Min"] = 4, ["Name"] = "Height", ["Max"] = 24, ["Res"] = 0.3, ["Default"] = 12},
+            {["Min"] = 0, ["Name"] = "Overhang", ["Max"] = 5, ["Res"] = 0.25, ["Default"] = 1}
+        },
+        ["Gable Roof"] = {
+            {["Min"] = 4, ["Name"] = "Height", ["Max"] = 24, ["Res"] = 0.3, ["Default"] = 10},
+            {["Min"] = 0.25, ["Name"] = "Overhang", ["Max"] = 5.25, ["Res"] = 0.25, ["Default"] = 2},
+            {["Min"] = 0.25, ["Name"] = "Thickness", ["Max"] = 3, ["Res"] = 0.25, ["Default"] = 0.5}
+        },
+        ["Flat Roof"] = {
+            {["Min"] = 0, ["Res"] = 0.3, ["Name"] = "Height", ["Max"] = 12},
+            {["Min"] = 0, ["Res"] = 0.3, ["Name"] = "BorderHeight", ["Max"] = 6},
+            {["Min"] = 0.3, ["Res"] = 0.1, ["Name"] = "Border", ["Max"] = 1.5}
+        },
+        ["Flared Mansard Roof"] = {
+            {["Min"] = 2, ["Name"] = "Height", ["Max"] = 12, ["Res"] = 0.3, ["Default"] = 4},
+            {["Min"] = 0.1, ["Name"] = "Overhang", ["Max"] = 1, ["Res"] = 0.1, ["Default"] = 1},
+            {["Min"] = 1, ["Name"] = "Offset", ["Inverted"] = true, ["Max"] = 10, ["Res"] = 0.25, ["Default"] = 4},
+            {["Min"] = -1, ["Name"] = "FlareRatio", ["Inverted"] = true, ["Max"] = 1, ["Res"] = 0.25, ["Default"] = 0.5}
+        },
+        ["Hollow Flared Mansard Roof"] = {
+            {["Min"] = 2, ["Name"] = "Height", ["Max"] = 12, ["Res"] = 0.3, ["Default"] = 4},
+            {["Min"] = 0.1, ["Name"] = "Overhang", ["Max"] = 1, ["Res"] = 0.1, ["Default"] = 1},
+            {["Min"] = 1, ["Name"] = "Offset", ["Inverted"] = true, ["Max"] = 10, ["Res"] = 0.25, ["Default"] = 4},
+            {["Min"] = -1, ["Name"] = "FlareRatio", ["Inverted"] = true, ["Max"] = 1, ["Res"] = 0.25, ["Default"] = 0.5}
+        },
+        ["Hollow Mansard Roof"] = {
+            {["Min"] = 2, ["Name"] = "Height", ["Max"] = 12, ["Res"] = 0.3, ["Default"] = 4},
+            {["Min"] = 0.1, ["Name"] = "Overhang", ["Max"] = 1, ["Res"] = 0.1, ["Default"] = 1},
+            {["Min"] = 1, ["Name"] = "Offset", ["Inverted"] = true, ["Max"] = 10, ["Res"] = 0.25, ["Default"] = 4}
+        },
+        ["Curved Roof"] = {
+            {["Min"] = 4, ["Name"] = "Height", ["Max"] = 24, ["Res"] = 0.3, ["Default"] = 5},
+            {["Min"] = 0, ["Name"] = "Overhang", ["Max"] = 5, ["Res"] = 0.25, ["Default"] = 2},
+            {["Min"] = 0.25, ["Name"] = "Thickness", ["Max"] = 3, ["Res"] = 0.25, ["Default"] = 0.5}
+        },
+        ["Mansard Skylight Roof"] = {
+            {["Min"] = 2, ["Name"] = "Height", ["Max"] = 12, ["Res"] = 0.3, ["Default"] = 4},
+            {["Min"] = 0.1, ["Name"] = "Overhang", ["Max"] = 1, ["Res"] = 0.1, ["Default"] = 0.6},
+            {["Min"] = 1, ["Name"] = "Offset", ["Inverted"] = true, ["Max"] = 10, ["Res"] = 0.25, ["Default"] = 5}
+        },
+        ["Flat Glass Roof"] = {
+            {["Min"] = 0, ["Res"] = 0.3, ["Name"] = "Height", ["Max"] = 12},
+            {["Min"] = 0, ["Res"] = 0.3, ["Name"] = "BorderHeight", ["Max"] = 6},
+            {["Min"] = 0.3, ["Res"] = 0.1, ["Name"] = "Border", ["Max"] = 1.5}
+        },
+        ["Mansard Roof"] = {
+            {["Min"] = 2, ["Name"] = "Height", ["Max"] = 12, ["Res"] = 0.3, ["Default"] = 4},
+            {["Min"] = 0.1, ["Name"] = "Overhang", ["Max"] = 1, ["Res"] = 0.1, ["Default"] = 1},
+            {["Min"] = 1, ["Name"] = "Offset", ["Inverted"] = true, ["Max"] = 10, ["Res"] = 0.25, ["Default"] = 4}
+        },
+        ["Slanted Roof"] = {
+            {["Min"] = 1, ["Name"] = "Height", ["Max"] = 18, ["Res"] = 0.3, ["Default"] = 9},
+            {["Min"] = 0, ["Name"] = "Overhang", ["Max"] = 10, ["Res"] = 0.25, ["Default"] = 1},
+            {["Min"] = 0.25, ["Name"] = "Thickness", ["Max"] = 6, ["Res"] = 0.25, ["Default"] = 0.5}
+        },
+        ["Pyramid Glass Roof"] = {
+            {["Min"] = 2, ["Name"] = "Height", ["Max"] = 24, ["Res"] = 0.3, ["Default"] = 4},
+            {["Min"] = 0.2, ["Name"] = "Border", ["Max"] = 1.6, ["Res"] = 0.1, ["Default"] = 0.4}
+        }
+    },
+    ["CreateRoof"] = "function: 0xde40b30d597b5b23",
+    ["GenerateRoof"] = "function: 0x2096190e3fcf5c43",
+    ["GetRoofPlacementFloorPos"] = "function: 0xeb0dc77670f6fb53",
+    ["CreateBorder"] = "function: 0xd8e8ae8b6e01ae23"
+}
