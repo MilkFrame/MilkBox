d2567b18d17766fb6915e4e3192097f0a673ed20
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:33:15 +1000

    Create TagService.lua

---
+++ GameData/Welcome to Bloxburg [185655149]/TagService.lua
@@ -0,0 +1,118 @@
+data = {
+    ["GetTagFromID"] = "function: 0xa74da9f9e18f5833",
+    ["GetSelectedTag"] = "function: 0x77d27a643ab7b543",
+    ["Tags"] = {
+        ["Developer"] = {
+            ["IconPosition"] = "85, 0",
+            ["Name"] = "Developer",
+            ["ID"] = 3,
+            ["Priority"] = 100,
+            ["Short"] = "Dev",
+            ["Color"] = Color3.fromRGB(255, 170, 0)
+        },
+        ["Moderator"] = {
+            ["IconPosition"] = "85, 0",
+            ["Name"] = "Moderator",
+            ["ID"] = 5,
+            ["Priority"] = 98,
+            ["Short"] = "Mod",
+            ["Color"] = Color3.fromRGB(0, 255, 255)
+        },
+        ["Early Bloxburger"] = {
+            ["IconPosition"] = "515, 3",
+            ["Name"] = "Early Bloxburger",
+            ["Color"] = Color3.fromRGB(0, 174, 255),
+            ["Priority"] = 75,
+            ["ID"] = 21
+        },
+        ["Administrator"] = {
+            ["IconPosition"] = "85, 0",
+            ["Name"] = "Administrator",
+            ["ID"] = 4,
+            ["Priority"] = 99,
+            ["Short"] = "Admin",
+            ["Color"] = Color3.fromRGB(0, 255, 0)
+        },
+        ["Contributor"] = {
+            ["IconPosition"] = "355, 0",
+            ["Name"] = "Contributor",
+            ["Color"] = Color3.fromRGB(0, 255, 127),
+            ["Priority"] = 80,
+            ["ID"] = 6
+        },
+        ["Premium"] = {
+            ["IconPosition"] = "170, 0",
+            ["Name"] = "Premium",
+            ["Color"] = Color3.fromRGB(255, 50, 50),
+            ["Priority"] = 200,
+            ["ID"] = 2
+        },
+        ["Host"] = {
+            ["IconPosition"] = "255, 0",
+            ["Name"] = "Host",
+            ["Color"] = Color3.fromRGB(195, 142, 213),
+            ["Priority"] = 999,
+            ["ID"] = 7
+        },
+        ["Content Creator"] = {
+            ["IconPosition"] = "440, 0",
+            ["Name"] = "Content Creator",
+            ["Color"] = Color3.fromRGB(124, 252, 0),
+            ["Priority"] = 79,
+            ["ID"] = 20
+        },
+        ["None"] = {["ID"] = 1, ["Priority"] = 70, ["Name"] = "None"}
+    },
+    ["GetUserRank"] = "function: 0xa0a519172796ffe3",
+    ["GetPlayerTags"] = "function: 0x4101fbdfe6c3d423",
+    ["FreeTags"] = {
+        ["Grandparent"] = {
+            ["ID"] = 11,
+            ["Priority"] = 6,
+            ["Name"] = "Grandparent",
+            ["Color"] = Color3.fromRGB(242, 122, 180)
+        },
+        ["Parent"] = {["ID"] = 10, ["Priority"] = 5, ["Name"] = "Parent", ["Color"] = Color3.fromRGB(242, 122, 180)},
+        ["Relative"] = {["ID"] = 13, ["Priority"] = 8, ["Name"] = "Relative", ["Color"] = Color3.fromRGB(242, 122, 180)},
+        ["Bloxburger"] = {
+            ["ID"] = 14,
+            ["Priority"] = 9,
+            ["Name"] = "Bloxburger",
+            ["Color"] = Color3.fromRGB(255, 255, 255)
+        },
+        ["Decorator"] = {["ID"] = 8, ["Priority"] = 3, ["Name"] = "Decorator", ["Color"] = Color3.fromRGB(78, 91, 146)},
+        ["Landscaper"] = {
+            ["ID"] = 19,
+            ["Priority"] = 14,
+            ["Name"] = "Landscaper",
+            ["Color"] = Color3.fromRGB(109, 120, 168)
+        },
+        ["Noob"] = {["ID"] = 15, ["Priority"] = 10, ["Name"] = "Noob", ["Color"] = Color3.fromRGB(255, 255, 255)},
+        ["Designer"] = {
+            ["ID"] = 18,
+            ["Priority"] = 13,
+            ["Name"] = "Designer",
+            ["Color"] = Color3.fromRGB(109, 120, 168)
+        },
+        ["Renovator"] = {
+            ["ID"] = 17,
+            ["Priority"] = 12,
+            ["Name"] = "Renovator",
+            ["Color"] = Color3.fromRGB(109, 120, 168)
+        },
+        ["Roleplayer"] = {
+            ["ID"] = 9,
+            ["Priority"] = 4,
+            ["Name"] = "Roleplayer",
+            ["Color"] = Color3.fromRGB(242, 122, 180)
+        },
+        ["Builder"] = {["ID"] = 16, ["Priority"] = 11, ["Name"] = "Builder", ["Color"] = Color3.fromRGB(109, 120, 168)},
+        ["Child"] = {["ID"] = 12, ["Priority"] = 7, ["Name"] = "Child", ["Color"] = Color3.fromRGB(242, 122, 180)}
+    },
+    ["Ranks"] = {["Administrator"] = 8, ["Developer"] = 10, ["Moderator"] = 5},
+    ["GetPlayerPermission"] = "function: 0xcc3ef0d186337ef3",
+    ["SetIcon"] = "function: 0x69a14e0f02235443",
+    ["SetNametagText"] = "function: 0x78d2472d846fcfc3",
+    ["GetPermissionFromRank"] = "function: 0x9c99af50271e4da3",
+    ["PlayerRank"] = 0
+}
