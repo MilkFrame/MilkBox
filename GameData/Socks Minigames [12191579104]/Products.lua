ab0d5c1ef933d0a98808fd370ee41bc11f812ada
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:14:41 +1000

    Create Products.lua

---
+++ GameData/Socks Minigames [12191579104]/Products.lua
@@ -0,0 +1,36 @@
+data = {
+    ["Potions"] = {
+        ["Rating"] = {["1"] = 1816786540, ["3"] = 1816787101, ["2"] = 1816786680},
+        ["Luck"] = {["1"] = 1816784861, ["3"] = 1816785256, ["2"] = 1816784981},
+        ["Coins"] = {["1"] = 1816787845, ["3"] = 1816788227, ["2"] = 1816788119}
+    },
+    ["Cosmetic"] = {
+        ["AutoOpen"] = {["Tip"] = "Allows you to automatically open chests in the lobby!"},
+        ["VIP"] = {["Type"] = "Subscription", ["Tip"] = ""},
+        ["ExtraLuck"] = {["Tip"] = "Gives you a luck boost whenever opening chests in the lobby!"},
+        ["MultiChests"] = {["Tip"] = "Allows you to open multiple chests at once in the lobby!"},
+        ["DoubleCoins"] = {["Tip"] = "Gives you double coins whenever rewarded with coins!"},
+        ["Boombox"] = {["Tip"] = "Allows you to actively play music in the lobby and during minigames!"}
+    },
+    ["Limited"] = 1681218825,
+    ["Coins"] = {
+        ["85000"] = 1676469926,
+        ["175000"] = 1676469925,
+        ["5000"] = 1676469372,
+        ["15500"] = 1676469373,
+        ["45000"] = 1676469371
+    },
+    ["Legendary"] = 1800783057,
+    ["BattlepassSkip10"] = 1708759964,
+    ["Gamepasses"] = {
+        ["AutoOpen"] = 647787232,
+        ["VIP"] = "EXP-3761134621766189157",
+        ["ExtraLuck"] = 637685796,
+        ["MultiChests"] = 637407891,
+        ["DoubleCoins"] = 637482876,
+        ["Boombox"] = 793906664
+    },
+    ["BattlepassSkip1"] = 1708759615,
+    ["Battlepass"] = 1708764156,
+    ["Trolls"] = {["Speed"] = 1816774384, ["Bat"] = 1816774016, ["Nuke"] = 1816774897, ["JumpScare"] = 1816775210}
+}
