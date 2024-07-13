2e5c3530045bf20d809535566ce3673772e97b5b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:23:38 +1000

    Create GroundService.lua

---
+++ GameData/Welcome to Bloxburg [185655149]/GroundService.lua
@@ -0,0 +1,11 @@
+data = {
+    ["GetTerrainVoxel"] = "function: 0x111a68a64fa1b543",
+    ["IsWater"] = "function: 0x9f0aea6f172e5643",
+    ["IsTerrainWater"] = "function: 0x0e7dbbca3d41a653",
+    ["Update"] = "function: 0xb138107f294acae3",
+    ["GetGridSize"] = "function: 0xdda2e310f73db183",
+    ["IsSnowPart"] = "function: 0x651bf7b0cddb1d03",
+    ["CheckGround"] = "function: 0x9fc47d26ae0ecb03",
+    ["IsGrassPart"] = "function: 0xcf4f6975b35a4313",
+    ["GetBasementItems"] = "function: 0x7c25e85bf16172a3"
+}
