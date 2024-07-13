c6fff8c6a1d40c75eb729446e50356f8b04df172
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:15:51 +1000

    Create Songs.lua

---
+++ GameData/Socks Minigames [12191579104]/Songs.lua
@@ -0,0 +1,62 @@
+data = {
+    ["Entry Of The Gladiators"] = {
+        ["Image"] = "rbxassetid://17117726840",
+        ["Owner"] = "Roblox",
+        ["Duration"] = 138,
+        ["SongID"] = 1846469019
+    },
+    ["Metal Head"] = {
+        ["Image"] = "rbxassetid://17117771907",
+        ["Owner"] = "Roblox",
+        ["Duration"] = 140,
+        ["SongID"] = 1843623527
+    },
+    ["Escape"] = {
+        ["Image"] = "rbxassetid://17117536292",
+        ["Owner"] = "Monstercat",
+        ["Duration"] = 189,
+        ["SongID"] = 5410082879
+    },
+    ["Raining Tacos"] = {
+        ["Image"] = "rbxassetid://17087497153",
+        ["Owner"] = "Parry Gripp",
+        ["Duration"] = 92,
+        ["SongID"] = 142376088
+    },
+    ["Fairy Magic"] = {
+        ["Image"] = "rbxassetid://17117555850",
+        ["Owner"] = "Roblox",
+        ["Duration"] = 100,
+        ["SongID"] = 1847611722
+    },
+    ["Feels Good"] = {
+        ["Image"] = "rbxassetid://17117513621",
+        ["Owner"] = "Roblox",
+        ["Duration"] = 317,
+        ["SongID"] = 1839850227
+    },
+    ["Tomfoolery"] = {
+        ["Image"] = "rbxassetid://17117506316",
+        ["Owner"] = "Roblox",
+        ["Duration"] = 132,
+        ["SongID"] = 1836362602
+    },
+    ["Dombummel"] = {
+        ["Image"] = "rbxassetid://17117553275",
+        ["Owner"] = "Roblox",
+        ["Duration"] = 79,
+        ["SongID"] = 1845736826
+    },
+    ["Crab Rave"] = {
+        ["Image"] = "rbxassetid://17117543609",
+        ["Owner"] = "Monstercat",
+        ["Duration"] = 161,
+        ["SongID"] = 5410086218
+    },
+    ["Fart Fart Away"] = {
+        ["Image"] = "rbxassetid://17117487164",
+        ["Owner"] = "Roblox",
+        ["Duration"] = 68,
+        ["SongID"] = 1845015288
+    }
+}
