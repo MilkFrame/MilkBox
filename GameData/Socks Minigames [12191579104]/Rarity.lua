145cca5e38e5850e2d6b439fe8a1a19864f9223b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:14:55 +1000

    Create Rarity.lua

---
+++ GameData/Socks Minigames [12191579104]/Rarity.lua
@@ -0,0 +1,10 @@
+data = {
+    ["Colors"] = {
+        ["Legendary"] = Color3.fromRGB(255, 217, 0),
+        ["Common"] = Color3.fromRGB(172, 172, 172),
+        ["Epic"] = Color3.fromRGB(173, 92, 243),
+        ["Exclusive"] = Color3.fromRGB(255, 0, 0),
+        ["Uncommon"] = Color3.fromRGB(3, 168, 0),
+        ["Rare"] = Color3.fromRGB(15, 137, 189)
+    }
+}
