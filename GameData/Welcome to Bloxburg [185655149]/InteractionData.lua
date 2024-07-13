b37f5d1a9e58fa956533a2ba055ee3576094f5f0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:24:07 +1000

    Create InteractionData.lua

---
+++ GameData/Welcome to Bloxburg [185655149]/InteractionData.lua
@@ -0,0 +1,12 @@
+data = {
+    ["GetData"] = "function: 0x5daaff7a2d75f683",
+    ["INTERACTION_DISTANCE"] = 10,
+    ["MAX_INTERACTION_DISTANCE"] = 30,
+    ["IndexData"] = "function: 0x4305dc0d16a691e3",
+    ["GetOriginPosition"] = "function: 0x42aae9e15c161f83",
+    ["UnlockAllFilters"] = "function: 0x53ec5272e01af623",
+    ["GetOrigin"] = "function: 0x49925cd5c218c523",
+    ["INTERACTIONS_ACTION_FILTERS_INCLUDE"] = {["I_Talk"] = true},
+    ["INTERACTIONS_OBJECT_FILTERS_INCLUDE"] = {["Tom"] = true},
+    ["ALL_INTERACTIONS_LOCKED"] = false
+}
