3837251f11851aea67c46bbc6c20fe0577a98e4a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:15:10 +1000

    Create TimeRewards.lua

---
+++ GameData/Socks Minigames [12191579104]/TimeRewards.lua
@@ -0,0 +1,14 @@
+data = {
+    {60, "Coins", 500},
+    {240, "Chest", "RoyalChest"},
+    {480, "Chest", "CyberpunkChest"},
+    {900, "Chest", "PartyChest"},
+    {1200, "Coins", 1500},
+    {1800, "Chest", "RoyalChest", 5},
+    {2400, "Chest", "CyberpunkChest", 5},
+    {3000, "Chest", "PartyChest", 5},
+    {3600, "Coins", 2500},
+    {2400, "Chest", "RoyalChest", 3},
+    {3000, "Chest", "CyberpunkChest", 3},
+    {3600, "Chest", "PartyChest", 3}
+}
