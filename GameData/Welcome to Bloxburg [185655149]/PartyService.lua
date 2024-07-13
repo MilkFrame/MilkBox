972d5a4563517382c3e5672ea79aaf428cc6cbc0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:30:10 +1000

    Create PartyService.lua

---
+++ GameData/Welcome to Bloxburg [185655149]/PartyService.lua
@@ -0,0 +1,7 @@
+data = {
+    ["INVITE_LEVELS"] = {["Family"] = 3, ["Friends"] = 2, ["Everyone"] = 1},
+    ["ThrowParty"] = "function: 0xeb74816783773923",
+    ["UpdateCandleState"] = "function: 0xe72fe277470be943",
+    ["GetInvitationPrice"] = "function: 0x8326690893edca03",
+    ["PARTY_DELAY"] = 300
+}
