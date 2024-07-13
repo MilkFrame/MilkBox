d434eea24b1bfdb1c216146f6766c64078cbe88e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:32:22 +1000

    Create SeasonalService.lua

---
+++ GameData/Welcome to Bloxburg [185655149]/SeasonalService.lua
@@ -0,0 +1,14 @@
+data = {
+    ["SeasonData"] = {
+        ["Spring"] = true,
+        ["Winter"] = false,
+        ["NewYears"] = false,
+        ["Halloween"] = false,
+        ["Valentines"] = false,
+        ["July4th"] = true,
+        ["Fall"] = false,
+        ["Summer"] = true,
+        ["Christmas"] = false
+    },
+    ["Is"] = "function: 0x1843ff27a1e4abc3"
+}
