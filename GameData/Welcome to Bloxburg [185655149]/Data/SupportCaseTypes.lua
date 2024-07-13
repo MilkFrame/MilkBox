761580f7de01cada2e24c85e45c391c4864cbcc8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:39:51 +1000

    Create SupportCaseTypes.lua

---
+++ GameData/Welcome to Bloxburg [185655149]/Data/SupportCaseTypes.lua
@@ -0,0 +1,15 @@
+data = {
+    {0, "T_SupportIssueSuggestion", true},
+    {1, "T_SupportIssueBug", true},
+    {2, "T_SupportIssueGameLag"},
+    {3, "T_SupportIssueHowTo"},
+    {4, "T_SupportIssueLostItems"},
+    {5, "T_SupportIssuePurchaseFailed"},
+    {6, "T_SupportIssueGamepassMissing"},
+    {7, "T_SupportIssueScammed"},
+    {8, "T_SupportIssueRobuxRefund"},
+    {9, "T_SupportIssueBanAppeal", true},
+    {10, "T_SupportIssuePlayerReport", true},
+    {11, "T_SupportIssueGameSubmission", true},
+    {12, "T_SupportActionDataWiped", true}
+}
