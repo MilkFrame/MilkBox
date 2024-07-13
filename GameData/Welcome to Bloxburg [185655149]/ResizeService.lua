41360611adf60d17ab7c57c71e36dfbfa78a7a72
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-13 22:31:55 +1000

    Create ResizeService.lua

---
+++ GameData/Welcome to Bloxburg [185655149]/ResizeService.lua
@@ -0,0 +1,11 @@
+data = {
+    ["ForceUpdateUDim3D"] = "function: 0x0f8237975aa3b243",
+    ["CostBaseMultiplier"] = 0.35,
+    ["SetModelSize"] = "function: 0xe860f578af79c783",
+    ["GetTempRoot"] = "function: 0x83bf399314e076c3",
+    ["CanResize"] = "function: 0x1c6a743dd7fc5d53",
+    ["CostAxisMultiplier"] = 0.65,
+    ["GetResizeRootSize"] = "function: 0x8a7a64374e4d32e3",
+    ["UpdateModelSize"] = "function: 0x3edb2dc4790a14f3",
+    ["GetResizeCost"] = "function: 0x4d4ec164e3a9ee43"
+}
