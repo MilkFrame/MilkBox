b3f57e0fad8bee88a476e792a229c425906996e2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-14 22:34:47 +1000

    Update crate-meta.lua

--- GameData/BedWars [6872265039]/crate-meta.lua
+++ GameData/BedWars [6872265039]/crate-meta.lua
@@ -1,5 +1,28 @@
 data = {
     ["CrateMeta"] = {
+        ["afk_crate"] = {
+            ["consumableRequirement"] = "afk_crate",
+            ["image"] = "rbxassetid://16395888088",
+            ["model"] = "AFKLuckyCrate",
+            ["raritySettings"] = {
+                {["rarity"] = 0, ["value"] = 0.45},
+                {["rarity"] = 1, ["value"] = 0.25},
+                {["rarity"] = 2, ["value"] = 0.15},
+                {["rarity"] = 3, ["value"] = 0.0985},
+                {["rarity"] = 4, ["value"] = 0.05},
+                {["rarity"] = 5, ["value"] = 0.0015}
+            },
+            ["items"] = {
+                {["rarity"] = 0, ["itemType"] = "flat_bp_xp_1", ["amount"] = 1},
+                {["rarity"] = 1, ["itemType"] = "flat_bp_xp_2", ["amount"] = 1},
+                {["rarity"] = 2, ["itemType"] = "flat_bp_xp_3", ["amount"] = 1},
+                {["rarity"] = 3, ["itemType"] = "battle_pass_2x", ["amount"] = 1},
+                {["rarity"] = 3, ["itemType"] = "level_up_crate", ["amount"] = 1},
+                {["rarity"] = 4, ["itemType"] = "diamond_lucky_crate", ["amount"] = 1},
+                {["rarity"] = 5, ["itemType"] = "random_kit", ["amount"] = 1}
+            },
+            ["displayName"] = "AFK Crate"
+        },
         ["murder_crate"] = {
             ["items"] = {
                 {["rarity"] = 1, ["itemType"] = "sleep_splash_potion", ["amount"] = 1},
@@ -29,10 +52,11 @@ data = {
             ["model"] = "DiamondLuckyCrate",
             ["raritySettings"] = {
                 {["rarity"] = 0, ["value"] = 0.2},
-                {["rarity"] = 1, ["value"] = 0.275},
-                {["rarity"] = 2, ["value"] = 0.35},
+                {["rarity"] = 1, ["value"] = 0.25},
+                {["rarity"] = 2, ["value"] = 0.3},
                 {["rarity"] = 3, ["value"] = 0.15},
-                {["rarity"] = 4, ["value"] = 0.025}
+                {["rarity"] = 4, ["value"] = 0.075},
+                {["rarity"] = 5, ["value"] = 0.025}
             },
             ["items"] = {
                 {["rarity"] = 0, ["itemType"] = "yuzi_brb", ["amount"] = 1},
@@ -63,7 +87,8 @@ data = {
                 {["rarity"] = 1, ["itemType"] = "battle_pass_2x", ["amount"] = 1},
                 {["rarity"] = 3, ["itemType"] = "battle_pass_3x", ["amount"] = 1},
                 {["rarity"] = 1, ["itemType"] = "auto_complete_daily_mission_ticket", ["amount"] = 1},
-                {["rarity"] = 2, ["itemType"] = "auto_complete_weekly_mission_ticket", ["amount"] = 1}
+                {["rarity"] = 2, ["itemType"] = "auto_complete_weekly_mission_ticket", ["amount"] = 1},
+                {["rarity"] = 5, ["itemType"] = "random_kit", ["amount"] = 1}
             },
             ["displayName"] = "Diamond Lucky Crate"
         },
@@ -73,10 +98,11 @@ data = {
             ["model"] = "RewardCrate",
             ["raritySettings"] = {
                 {["rarity"] = 0, ["value"] = 0.38},
-                {["rarity"] = 1, ["value"] = 0.35},
+                {["rarity"] = 1, ["value"] = 0.32},
                 {["rarity"] = 2, ["value"] = 0.2},
-                {["rarity"] = 3, ["value"] = 0.065},
-                {["rarity"] = 4, ["value"] = 0.005}
+                {["rarity"] = 3, ["value"] = 0.0825},
+                {["rarity"] = 4, ["value"] = 0.015},
+                {["rarity"] = 5, ["value"] = 0.0025}
             },
             ["items"] = {
                 {["rarity"] = 0, ["itemType"] = "yuzi_brb", ["amount"] = 1},
@@ -107,7 +133,8 @@ data = {
                 {["rarity"] = 1, ["itemType"] = "battle_pass_2x", ["amount"] = 1},
                 {["rarity"] = 3, ["itemType"] = "battle_pass_3x", ["amount"] = 1},
                 {["rarity"] = 1, ["itemType"] = "auto_complete_daily_mission_ticket", ["amount"] = 1},
-                {["rarity"] = 2, ["itemType"] = "auto_complete_weekly_mission_ticket", ["amount"] = 1}
+                {["rarity"] = 2, ["itemType"] = "auto_complete_weekly_mission_ticket", ["amount"] = 1},
+                {["rarity"] = 5, ["itemType"] = "random_kit", ["amount"] = 1}
             },
             ["displayName"] = "Gold Lucky Crate"
         }
