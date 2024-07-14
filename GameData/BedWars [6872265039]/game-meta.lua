8ac5c6be3297ae33b64e8bbb1b388be5d07e621e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-14 22:19:38 +1000

    Create game-meta.lua

---
+++ GameData/BedWars [6872265039]/game-meta.lua
@@ -0,0 +1,28 @@
+data = {
+    ["GameMeta"] = {
+        ["one-in-the-chamber"] = {["title"] = "One In The Chamber"},
+        ["hot-potato"] = {["title"] = "TNT Run", ["image"] = "rbxassetid://11104071738"},
+        ["gun-game"] = {["title"] = "Gun Game", ["image"] = "rbxassetid://10977265283"},
+        ["juggernaut"] = {["title"] = "Juggernaut TDM", ["image"] = "rbxassetid://11108534877"},
+        ["bedwars"] = {["title"] = "Bedwars", ["image"] = "rbxassetid://11104064080"},
+        ["infected"] = {["title"] = "Infected", ["image"] = "rbxassetid://11104063651"},
+        ["bridge-duel"] = {["title"] = "Bridge Battle"},
+        ["skywars"] = {["title"] = "Skywars", ["image"] = "rbxassetid://11108575630"},
+        ["capture_the_points"] = {["title"] = "Capture the Points"},
+        ["FLAG_CAPTURE"] = {["title"] = "Capture The Flag", ["image"] = "rbxassetid://11108528694"},
+        ["skull-drop"] = {["title"] = "Hunter"},
+        ["tnt-wars"] = {["title"] = "TNT Wars", ["image"] = "rbxassetid://11104071738"},
+        ["halloween-event"] = {["title"] = "Halloween Event 2022"},
+        ["fast_casual"] = {["title"] = "Fast & Casual"},
+        ["lasso-wars"] = {["title"] = "Lasso Wars"},
+        ["egg-hunt"] = {["title"] = "Egg Hunt"},
+        ["jugg-v2"] = {["title"] = "Juggernaut", ["image"] = "rbxassetid://11108534877"},
+        ["sandbox"] = {["title"] = "Sandbox", ["image"] = "rbxassetid://11104064080"},
+        ["training-room"] = {["title"] = "Training Room"},
+        ["survival"] = {["title"] = "Penguin Survival", ["image"] = "rbxassetid://11108541918"},
+        ["murderer-game"] = {["title"] = "Murderer"},
+        ["winter-event"] = {["title"] = "Winter Event 2022"},
+        ["block-hunt"] = {["title"] = "Block Hunt"}
+    },
+    ["getGameMeta"] = "function: 0x04b9fa736e2271e9"
+}
