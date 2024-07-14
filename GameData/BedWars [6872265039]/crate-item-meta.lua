8ff1457e5aa9b7ee6a277afbaf8f44c9e03cebbf
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-14 22:35:06 +1000

    Create crate-item-meta.lua

---
+++ GameData/BedWars [6872265039]/crate-item-meta.lua
@@ -0,0 +1,26 @@
+data = {
+    ["getCrateItemMeta"] = "function: 0xabc3f23ab1489639",
+    ["FLAT_BP_XP_REWARD"] = {1200, 3600, 9600, 28800, 86400, 750},
+    ["CrateItemMeta"] = {
+        ["flat_bp_xp_3"] = {
+            ["description"] = "Instantly gives the player a flat amount of XP",
+            ["itemDisplay"] = {["image"] = "rbxassetid://16393891370"},
+            ["displayName"] = "3600 Battlepass XP"
+        },
+        ["flat_bp_xp_1"] = {
+            ["description"] = "Instantly gives the player a flat amount of XP",
+            ["itemDisplay"] = {["image"] = "rbxassetid://16393891370"},
+            ["displayName"] = "750 Battlepass XP"
+        },
+        ["flat_bp_xp_2"] = {
+            ["description"] = "Instantly gives the player a flat amount of XP",
+            ["itemDisplay"] = {["image"] = "rbxassetid://16393891370"},
+            ["displayName"] = "1200 Battlepass XP"
+        },
+        ["random_kit"] = {
+            ["description"] = "Permenantly unlocks a random purchaseable kit!",
+            ["itemDisplay"] = {["image"] = "rbxassetid://16538276237"},
+            ["displayName"] = "Random Free Kit"
+        }
+    }
+}
