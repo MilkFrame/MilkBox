b3d5ff613b58b6e2756c69ea753e61982d96b493
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-14 22:26:14 +1000

    Create training-dummy-meta.lua

---
+++ GameData/BedWars [6872265039]/training-dummy-meta.lua
@@ -0,0 +1,132 @@
+data = {
+    ["getTrainingDummyMeta"] = "function: 0x560891602de2abb9",
+    ["TrainingDummySigns"] = {
+        ["NO_KNOCKBACK_ENEMY_DUMMY_6"] = "nkb_dummy_6",
+        ["KNOCKBACK_ENEMY_DUMMY_6"] = "kb_dummy_6",
+        ["KNOCKBACK_ENEMY_DUMMY_2"] = "kb_dummy_2",
+        ["TEAMMATE_DUMMY_2"] = "tm_dummy_2",
+        ["KNOCKBACK_ENEMY_DUMMY_5"] = "kb_dummy_5",
+        ["WALKING_ENEMY_DUMMY_START_2"] = "dummy_2_start",
+        ["NO_KNOCKBACK_ENEMY_DUMMY_1"] = "nkb_dummy_1",
+        ["WALKING_ENEMY_DUMMY_START_1"] = "dummy_1_start",
+        ["KNOCKBACK_ENEMY_DUMMY_3"] = "kb_dummy_3",
+        ["NO_KNOCKBACK_ENEMY_DUMMY_4"] = "nkb_dummy_4",
+        ["WALKING_ENEMY_DUMMY_END_2"] = "dummy_2_end",
+        ["KNOCKBACK_ENEMY_DUMMY_4"] = "kb_dummy_4",
+        ["WALKING_ENEMY_DUMMY_END_1"] = "dummy_1_end",
+        ["KNOCKBACK_ENEMY_DUMMY_1"] = "kb_dummy_1",
+        ["TEAMMATE_DUMMY_1"] = "tm_dummy_1",
+        ["NO_KNOCKBACK_ENEMY_DUMMY_2"] = "nkb_dummy_2",
+        ["NO_KNOCKBACK_ENEMY_DUMMY_3"] = "nkb_dummy_3",
+        ["NO_KNOCKBACK_ENEMY_DUMMY_5"] = "nkb_dummy_5"
+    },
+    ["TrainingDummyMeta"] = {
+        {["teammate"] = true, ["spawnSignName"] = "tm_dummy_2", ["maxHealth"] = 100, ["displayName"] = "Friendly Dummy"},
+        {
+            ["spawnSignName"] = "nkb_dummy_1",
+            ["teammate"] = false,
+            ["noKnockback"] = true,
+            ["maxHealth"] = 100,
+            ["displayName"] = "Regular Enemy Dummy"
+        },
+        {
+            ["armorType"] = "LEATHER",
+            ["spawnSignName"] = "nkb_dummy_2",
+            ["teammate"] = false,
+            ["noKnockback"] = true,
+            ["maxHealth"] = 100,
+            ["displayName"] = "Leather Enemy Dummy"
+        },
+        {
+            ["armorType"] = "IRON",
+            ["spawnSignName"] = "nkb_dummy_3",
+            ["teammate"] = false,
+            ["noKnockback"] = true,
+            ["maxHealth"] = 100,
+            ["displayName"] = "Iron Enemy Dummy"
+        },
+        {
+            ["armorType"] = "DIAMOND",
+            ["spawnSignName"] = "nkb_dummy_4",
+            ["teammate"] = false,
+            ["noKnockback"] = true,
+            ["maxHealth"] = 100,
+            ["displayName"] = "Diamond Enemy Dummy"
+        },
+        {
+            ["armorType"] = "EMERALD",
+            ["spawnSignName"] = "nkb_dummy_5",
+            ["teammate"] = false,
+            ["noKnockback"] = true,
+            ["maxHealth"] = 100,
+            ["displayName"] = "Emerald Enemy Dummy"
+        },
+        {
+            ["armorType"] = "VOID",
+            ["spawnSignName"] = "nkb_dummy_6",
+            ["teammate"] = false,
+            ["noKnockback"] = true,
+            ["maxHealth"] = 100,
+            ["displayName"] = "Void Enemy Dummy"
+        },
+        {
+            ["teammate"] = false,
+            ["spawnSignName"] = "kb_dummy_1",
+            ["maxHealth"] = 100,
+            ["displayName"] = "Regular Enemy Dummy"
+        },
+        {
+            ["armorType"] = "LEATHER",
+            ["spawnSignName"] = "kb_dummy_2",
+            ["teammate"] = false,
+            ["maxHealth"] = 100,
+            ["displayName"] = "Leather Enemy Dummy"
+        },
+        {
+            ["armorType"] = "IRON",
+            ["spawnSignName"] = "kb_dummy_3",
+            ["teammate"] = false,
+            ["maxHealth"] = 100,
+            ["displayName"] = "Iron Enemy Dummy"
+        },
+        {
+            ["armorType"] = "DIAMOND",
+            ["spawnSignName"] = "kb_dummy_4",
+            ["teammate"] = false,
+            ["maxHealth"] = 100,
+            ["displayName"] = "Diamond Enemy Dummy"
+        },
+        {
+            ["armorType"] = "EMERALD",
+            ["spawnSignName"] = "kb_dummy_5",
+            ["teammate"] = false,
+            ["maxHealth"] = 100,
+            ["displayName"] = "Emerald Enemy Dummy"
+        },
+        {
+            ["armorType"] = "VOID",
+            ["spawnSignName"] = "kb_dummy_6",
+            ["teammate"] = false,
+            ["maxHealth"] = 100,
+            ["displayName"] = "Void Enemy Dummy"
+        },
+        {
+            ["canMove"] = {["endPositionSign"] = "dummy_1_end", ["startPositionSign"] = "dummy_1_start"},
+            ["spawnSignName"] = "dummy_1_start",
+            ["teammate"] = false,
+            ["noKnockback"] = true,
+            ["maxHealth"] = 100,
+            ["displayName"] = "Regular Enemy Dummy"
+        },
+        {
+            ["armorType"] = "LEATHER",
+            ["canMove"] = {["endPositionSign"] = "dummy_2_end", ["startPositionSign"] = "dummy_2_start"},
+            ["spawnSignName"] = "dummy_2_start",
+            ["teammate"] = false,
+            ["noKnockback"] = true,
+            ["maxHealth"] = 100,
+            ["displayName"] = "Leather Enemy Dummy"
+        },
+        {["teammate"] = true, ["spawnSignName"] = "tm_dummy_1", ["maxHealth"] = 100, ["displayName"] = "Friendly Dummy"}
+    }
+}
