2e59a44927ca9aacb87cde8c433368aafc84b51a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-14 22:33:25 +1000

    Create pet-meta.lua

---
+++ GameData/BedWars [6872265039]/pet-meta.lua
@@ -0,0 +1,31 @@
+data = {
+    ["PetMeta"] = {
+        ["crab"] = {
+            ["animations"] = {
+                ["passiveActions"] = {
+                    {["id"] = 624, ["type"] = 1},
+                    {["id"] = 625, ["type"] = 1},
+                    {["id"] = 626, ["type"] = 0}
+                },
+                ["dance"] = 627,
+                ["idle"] = 622,
+                ["walk"] = 623
+            },
+            ["rotationOffset"] = -90,
+            ["model"] = "Crab"
+        },
+        ["turtle"] = {
+            ["animations"] = {
+                ["passiveActions"] = {
+                    {["id"] = 618, ["type"] = 1},
+                    {["id"] = 619, ["type"] = 1},
+                    {["id"] = 620, ["type"] = 0}
+                },
+                ["dance"] = 621,
+                ["idle"] = 616,
+                ["walk"] = 617
+            },
+            ["model"] = "VictoriousTurtle"
+        }
+    }
+}
