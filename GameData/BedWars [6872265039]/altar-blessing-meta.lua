411f7a4f575df7a64f9506ea04e8e3b901fb2045
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-14 22:14:00 +1000

    Create altar-blessing-meta.lua

---
+++ GameData/BedWars [6872265039]/altar-blessing-meta.lua
@@ -0,0 +1,144 @@
+data = {
+    ["getAltarBlessingMeta"] = "function: 0x7821a1ad6ea7aa09",
+    ["getAltarBlessingMetaFromBoonType"] = "function: 0x5c7dc510e8d932f9",
+    ["AltarBoonMeta"] = {
+        {
+            ["uiGradient"] = "0 0.752941 0.780392 0.823529 0 1 0.490196 0.521569 0.592157 0 ",
+            ["image"] = "rbxassetid://9166208240",
+            ["name"] = "TRINITY'S FAVOR",
+            ["description"] = "Reflect a percentage of melee damage taken back to the attacker, enhancing defense and deterring enemies."
+        },
+        {
+            ["uiGradient"] = "0 0.996078 0.901961 0.443137 0 1 0.976471 0.505882 0.164706 0 ",
+            ["image"] = "rbxassetid://17024640133",
+            ["name"] = "AGNI'S FAVOR",
+            ["description"] = "Increase explosive block destruction and reduce the damage and knockback from explosions, gaining mastery over explosive forces."
+        },
+        {
+            ["uiGradient"] = "0 0.858824 0.819608 0.788235 0 1 0.74902 0.423529 0.415686 0 ",
+            ["image"] = "rbxassetid://17022941869",
+            ["name"] = "BEKZAT'S FAVOR",
+            ["description"] = "Deal massive damage to non-player entities and gain additional resources by defeating diamond guardians and the titan, boosting the team's economy."
+        },
+        {
+            ["uiGradient"] = "0 0.92549 0.984314 0.992157 0 1 0.188235 0.454902 0.847059 0 ",
+            ["image"] = "rbxassetid://13841413050",
+            ["name"] = "ELEKTRA'S FAVOR",
+            ["description"] = "Stay out of combat for 5 seconds to activate a significant speed boost, allowing swift movement through the battlefield."
+        },
+        {
+            ["uiGradient"] = "0 0.647059 0.729412 0.827451 0 1 0.694118 0.494118 0.34902 0 ",
+            ["image"] = "rbxassetid://9166207341",
+            ["name"] = "ARES' FAVOR",
+            ["description"] = "Continue hitting enemies to gain a stacking damage buff, increasing your damage output.  If no attack is made within 2 seconds, the damage bonus will reset."
+        }
+    },
+    ["AltarBlessingMeta"] = {
+        ["combo_master_1"] = {
+            ["boonType"] = 0,
+            ["description"] = "+3% per stack",
+            ["level"] = 1,
+            ["statusUIDescription"] = "Gain temporary stacking damage that increases 3% more damage per hit.",
+            ["displayName"] = "Combo Master I"
+        },
+        ["swift_3"] = {
+            ["boonType"] = 4,
+            ["description"] = "+40% speed",
+            ["level"] = 3,
+            ["statusUIDescription"] = "Increase Movement Speed by 40% while out of combat for 5 seconds.",
+            ["displayName"] = "Swift III"
+        },
+        ["thorns_1"] = {
+            ["boonType"] = 1,
+            ["description"] = "+10% reflect",
+            ["level"] = 1,
+            ["statusUIDescription"] = "Reflect 10% of melee damage taken.",
+            ["displayName"] = "Thorns I"
+        },
+        ["hardened_2"] = {
+            ["boonType"] = 2,
+            ["description"] = "+40% dst -40% dmg & kb",
+            ["level"] = 2,
+            ["statusUIDescription"] = "Increase explosion block destruction by 40%. Reduce explosive damage and knockback by 40%.",
+            ["displayName"] = "Hardened II"
+        },
+        ["swift_2"] = {
+            ["boonType"] = 4,
+            ["description"] = "+30% speed",
+            ["level"] = 2,
+            ["statusUIDescription"] = "Increase Movement Speed by 30% while out of combat for 5 seconds.",
+            ["displayName"] = "Swift II"
+        },
+        ["monster_slayer_2"] = {
+            ["boonType"] = 3,
+            ["description"] = "+70% dmg +70% loot",
+            ["level"] = 2,
+            ["statusUIDescription"] = "Deal 70% more damage to non-player entities. Gain 70% more resources from diamond guardians and the titan.",
+            ["displayName"] = "Monster Slayer II"
+        },
+        ["monster_slayer_3"] = {
+            ["boonType"] = 3,
+            ["description"] = "+100% dmg +100% loot",
+            ["level"] = 3,
+            ["statusUIDescription"] = "Deal 100% more damage to non-player entities. Gain 100% more resources from diamond guardians and the titan.",
+            ["displayName"] = "Monster Slayer III"
+        },
+        ["hardened_1"] = {
+            ["boonType"] = 2,
+            ["description"] = "+20% dst -20% dmg & kb",
+            ["level"] = 1,
+            ["statusUIDescription"] = "Increase explosion block destruction by 20%. Reduce explosive damage and knockback by 20%.",
+            ["displayName"] = "Hardened I"
+        },
+        ["swift_1"] = {
+            ["boonType"] = 4,
+            ["description"] = "+20% speed",
+            ["level"] = 1,
+            ["statusUIDescription"] = "Increase Movement Speed by 20% while out of combat for 5 seconds.",
+            ["displayName"] = "Swift I"
+        },
+        ["combo_master_3"] = {
+            ["boonType"] = 0,
+            ["description"] = "+8% per stack",
+            ["level"] = 3,
+            ["statusUIDescription"] = "Gain temporary stacking damage that increases 8% more damage per hit.",
+            ["displayName"] = "Combo Master III"
+        },
+        ["hardened_3"] = {
+            ["boonType"] = 2,
+            ["description"] = "+80% dst -70% dmg & kb",
+            ["level"] = 3,
+            ["statusUIDescription"] = "Increase explosion block destruction by 80%. Reduce explosive damage and knockback by 70%.",
+            ["displayName"] = "Hardened III"
+        },
+        ["monster_slayer_1"] = {
+            ["boonType"] = 3,
+            ["description"] = "+50% dmg +50% loot",
+            ["level"] = 1,
+            ["statusUIDescription"] = "Deal 50% more damage to non-player entities. Gain 50% more resources from diamond guardians and the titan.",
+            ["displayName"] = "Monster Slayer I"
+        },
+        ["combo_master_2"] = {
+            ["boonType"] = 0,
+            ["description"] = "+5% per stack",
+            ["level"] = 2,
+            ["statusUIDescription"] = "Gain temporary stacking damage that increases 5% more damage per hit.",
+            ["displayName"] = "Combo Master II"
+        },
+        ["thorns_2"] = {
+            ["boonType"] = 1,
+            ["description"] = "+20% reflect",
+            ["level"] = 2,
+            ["statusUIDescription"] = "Reflect 20% of melee damage taken.",
+            ["displayName"] = "Thorns II"
+        },
+        ["thorns_3"] = {
+            ["boonType"] = 1,
+            ["description"] = "+30% reflect",
+            ["level"] = 3,
+            ["statusUIDescription"] = "Reflect 30% of melee damage taken.",
+            ["displayName"] = "Thorns III"
+        }
+    },
+    ["getAltarBoonMeta"] = "function: 0x9e9672f6401ac6b9"
+}
