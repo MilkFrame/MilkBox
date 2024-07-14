201d14d608f80fec6e7d78fffb2b3cc9d9e180fd
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-14 22:16:26 +1000

    Create clan-shop-item-meta.lua

---
+++ GameData/BedWars [6872265039]/clan-shop-item-meta.lua
@@ -0,0 +1,121 @@
+data = {
+    ["ClanShopItemsMeta"] = {
+        ["kit_rental_defender_7"] = {
+            ["items"] = {["consumables"] = {"kit_rental_defender_7"}},
+            ["image"] = "rbxassetid://12284610212",
+            ["name"] = "Kit Rental Ticket (Defender)",
+            ["price"] = 14000,
+            ["unlockedFromTier"] = "shop4",
+            ["maxPurchasesPerCycle"] = 1,
+            ["rarityColor"] = Color3.fromRGB(94, 197, 255)
+        },
+        ["battle_pass_1_5x"] = {
+            ["items"] = {["consumables"] = {"battle_pass_1_5x"}},
+            ["image"] = "rbxassetid://12284661708",
+            ["name"] = "1.5x BattlePass XP Boost",
+            ["price"] = 4000,
+            ["unlockedFromTier"] = "shop1",
+            ["maxPurchasesPerCycle"] = 1,
+            ["rarityColor"] = Color3.fromRGB(255, 255, 255)
+        },
+        ["battle_pass_2x"] = {
+            ["items"] = {["consumables"] = {"battle_pass_2x"}},
+            ["image"] = "rbxassetid://12284661708",
+            ["name"] = "2x BattlePass XP Boost",
+            ["price"] = 14000,
+            ["unlockedFromTier"] = "shop2",
+            ["maxPurchasesPerCycle"] = 1,
+            ["rarityColor"] = Color3.fromRGB(85, 217, 120)
+        },
+        ["kit_rental_tank_7"] = {
+            ["items"] = {["consumables"] = {"kit_rental_tank_7"}},
+            ["image"] = "rbxassetid://12284610292",
+            ["name"] = "Kit Rental Ticket (Tank)",
+            ["price"] = 14000,
+            ["unlockedFromTier"] = "shop3",
+            ["maxPurchasesPerCycle"] = 1,
+            ["rarityColor"] = Color3.fromRGB(94, 197, 255)
+        },
+        ["auto_complete_weekly_mission_ticket"] = {
+            ["items"] = {["consumables"] = {"auto_complete_weekly_mission_ticket"}},
+            ["image"] = "rbxassetid://12482272470",
+            ["name"] = "Auto-Complete Weekly Mission Ticket",
+            ["price"] = 14000,
+            ["unlockedFromTier"] = "shop4",
+            ["maxPurchasesPerCycle"] = 1,
+            ["rarityColor"] = Color3.fromRGB(94, 197, 255)
+        },
+        ["kit_rental_ranged_7"] = {
+            ["items"] = {["consumables"] = {"kit_rental_ranged_7"}},
+            ["image"] = "rbxassetid://12510116571",
+            ["name"] = "Kit Rental Ticket (Ranged)",
+            ["price"] = 14000,
+            ["unlockedFromTier"] = "shop4",
+            ["maxPurchasesPerCycle"] = 1,
+            ["rarityColor"] = Color3.fromRGB(94, 197, 255)
+        },
+        ["kit_rental_support_7"] = {
+            ["items"] = {["consumables"] = {"kit_rental_support_7"}},
+            ["image"] = "rbxassetid://12284609758",
+            ["name"] = "Kit Rental Ticket (Support)",
+            ["price"] = 14000,
+            ["unlockedFromTier"] = "shop3",
+            ["maxPurchasesPerCycle"] = 1,
+            ["rarityColor"] = Color3.fromRGB(94, 197, 255)
+        },
+        ["kit_rental_destroyer_7"] = {
+            ["items"] = {["consumables"] = {"kit_rental_destroyer_7"}},
+            ["image"] = "rbxassetid://12284610017",
+            ["name"] = "Kit Rental Ticket (Destroyer)",
+            ["price"] = 14000,
+            ["unlockedFromTier"] = "shop4",
+            ["maxPurchasesPerCycle"] = 1,
+            ["rarityColor"] = Color3.fromRGB(94, 197, 255)
+        },
+        ["kit_rental_economy_7"] = {
+            ["items"] = {["consumables"] = {"kit_rental_economy_7"}},
+            ["image"] = "rbxassetid://12284609902",
+            ["name"] = "Kit Rental Ticket (Economy)",
+            ["price"] = 14000,
+            ["unlockedFromTier"] = "shop3",
+            ["maxPurchasesPerCycle"] = 1,
+            ["rarityColor"] = Color3.fromRGB(94, 197, 255)
+        },
+        ["auto_complete_daily_mission_ticket"] = {
+            ["items"] = {["consumables"] = {"auto_complete_daily_mission_ticket"}},
+            ["image"] = "rbxassetid://12482272628",
+            ["name"] = "Auto-Complete Daily Mission Ticket",
+            ["price"] = 14000,
+            ["unlockedFromTier"] = "shop2",
+            ["maxPurchasesPerCycle"] = 1,
+            ["rarityColor"] = Color3.fromRGB(85, 217, 120)
+        },
+        ["kit_rental_movement_7"] = {
+            ["items"] = {["consumables"] = {"kit_rental_movement_7"}},
+            ["image"] = "rbxassetid://12284610107",
+            ["name"] = "Kit Rental Ticket (Movement)",
+            ["price"] = 14000,
+            ["unlockedFromTier"] = "shop3",
+            ["maxPurchasesPerCycle"] = 1,
+            ["rarityColor"] = Color3.fromRGB(94, 197, 255)
+        },
+        ["kit_rental_fighter_7"] = {
+            ["items"] = {["consumables"] = {"kit_rental_fighter_7"}},
+            ["image"] = "rbxassetid://12284610380",
+            ["name"] = "Kit Rental Ticket (Fighter)",
+            ["price"] = 14000,
+            ["unlockedFromTier"] = "shop4",
+            ["maxPurchasesPerCycle"] = 1,
+            ["rarityColor"] = Color3.fromRGB(94, 197, 255)
+        },
+        ["level_up_crate"] = {
+            ["items"] = {["consumables"] = {"level_up_crate"}},
+            ["image"] = "rbxassetid://12500112438",
+            ["name"] = "Lucky Crate",
+            ["price"] = 10000,
+            ["unlockedFromTier"] = "shop2",
+            ["maxPurchasesPerCycle"] = 1,
+            ["rarityColor"] = Color3.fromRGB(85, 217, 120)
+        }
+    }
+}
