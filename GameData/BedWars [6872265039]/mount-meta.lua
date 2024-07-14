6bfcf6582c10ae192ecd87850f99deccc3bb11a1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-14 22:33:03 +1000

    Update mount-meta.lua

--- GameData/BedWars [6872265039]/mount-meta.lua
+++ GameData/BedWars [6872265039]/mount-meta.lua
@@ -1,24 +1,60 @@
 data = {
     ["MountMeta"] = {
+        ["emerald_tinker"] = {
+            ["model"] = "emerald_tinker",
+            ["animations"] = {["walk"] = 579, ["fall"] = 584, ["idle"] = 581, ["jump"] = 583},
+            ["mountHipYOffset"] = 0.5,
+            ["controlBoneCallback"] = "function: 0xdb0ce715b8a56959",
+            ["displayName"] = "Tinker"
+        },
+        ["void_tinker"] = {
+            ["model"] = "void_tinker",
+            ["animations"] = {["walk"] = 579, ["fall"] = 584, ["idle"] = 581, ["jump"] = 583},
+            ["mountHipYOffset"] = 0.5,
+            ["controlBoneCallback"] = "function: 0x700f003ac6868279",
+            ["displayName"] = "Tinker"
+        },
+        ["diamond_tinker"] = {
+            ["model"] = "diamond_tinker",
+            ["animations"] = {["walk"] = 579, ["fall"] = 584, ["idle"] = 581, ["jump"] = 583},
+            ["mountHipYOffset"] = 0.5,
+            ["controlBoneCallback"] = "function: 0x0606c26170cdf4b9",
+            ["displayName"] = "Tinker"
+        },
+        ["iron_tinker"] = {
+            ["model"] = "iron_tinker",
+            ["animations"] = {["walk"] = 579, ["fall"] = 584, ["idle"] = 581, ["jump"] = 583},
+            ["mountHipYOffset"] = 0.5,
+            ["controlBoneCallback"] = "function: 0xaf385d4cbadd4299",
+            ["displayName"] = "Tinker"
+        },
         ["dino"] = {
             ["animations"] = {["walk"] = 170, ["fall"] = 171, ["idle"] = 175, ["jump"] = 172},
             ["displayName"] = "Dino",
-            ["controlBoneCallback"] = "function: 0x00000000068aede3",
+            ["controlBoneCallback"] = "function: 0xde39c7d17b330139",
             ["model"] = "dino"
-        },
-        ["dodo_bird"] = {
-            ["animations"] = {["walk"] = 44, ["fall"] = 46, ["idle"] = 43, ["jump"] = 47},
-            ["displayName"] = "Dodo Bird",
-            ["controlBoneCallback"] = "function: 0x000000007649685b",
-            ["model"] = "dodo_bird"
         },
         ["elk"] = {
             ["playerMountOffset"] = Vector3.new(0, -1.75, 1),
             ["model"] = "elk",
             ["animations"] = {["walk"] = 176, ["fall"] = 177, ["idle"] = 179, ["jump"] = 178},
             ["mountHipYOffset"] = 2,
-            ["controlBoneCallback"] = "function: 0x00000000f17f816b",
+            ["controlBoneCallback"] = "function: 0x033d1aacbc0734d9",
             ["displayName"] = "Elk"
+        },
+        ["dodo_bird"] = {
+            ["animations"] = {["walk"] = 44, ["fall"] = 46, ["idle"] = 43, ["jump"] = 47},
+            ["displayName"] = "Dodo Bird",
+            ["controlBoneCallback"] = "function: 0xb721a80621ecea19",
+            ["model"] = "dodo_bird"
+        },
+        ["tinker"] = {
+            ["model"] = "tinker",
+            ["animations"] = {["walk"] = 579, ["fall"] = 584, ["idle"] = 581, ["jump"] = 583},
+            ["mountHipYOffset"] = 0.5,
+            ["controlBoneCallback"] = "function: 0xd43d9d8fff232af9",
+            ["displayName"] = "Tinker"
         }
-    }
+    },
+    ["getMountMeta"] = "function: 0x5851c44a4197db69"
 }
