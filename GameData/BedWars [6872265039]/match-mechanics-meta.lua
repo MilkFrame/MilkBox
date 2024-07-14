c1d89df04f3d7690c34b5b722147a47dcc994948
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-14 22:32:28 +1000

    Create match-mechanics-meta.lua

---
+++ GameData/BedWars [6872265039]/match-mechanics-meta.lua
@@ -0,0 +1,295 @@
+data = {
+    ["getMatchMechanicMeta"] = "function: 0x5e3e48d348e44da9",
+    ["MatchMechanicsMeta"] = {
+        ["capture_points"] = {
+            ["description"] = "Diamond Generators can be captured to produce extra diamonds.",
+            ["image"] = "rbxassetid://17014527861",
+            ["name"] = "Diamond Generator Capture Points",
+            ["weight"] = 20
+        },
+        ["relic_knights_code"] = {
+            ["image"] = "rbxassetid://17014528777",
+            ["name"] = "Knight's Code",
+            ["weight"] = 10,
+            ["relic"] = "knights_code",
+            ["description"] = "All players deal more damage, but cannot use the enchant table or altar."
+        },
+        ["item_grappling_hook_stopwatch"] = {
+            ["items"] = {
+                {
+                    ["currency"] = "emerald",
+                    ["itemType"] = "grappling_hook",
+                    ["amount"] = 1,
+                    ["price"] = 7,
+                    ["lockAfterPurchase"] = true,
+                    ["category"] = "Combat"
+                },
+                {
+                    ["currency"] = "emerald",
+                    ["itemType"] = "stopwatch",
+                    ["amount"] = 1,
+                    ["price"] = 2,
+                    ["category"] = "Combat"
+                }
+            },
+            ["image"] = "rbxassetid://17014528367",
+            ["name"] = "Grappling Hooks And Stopwatches",
+            ["weight"] = 10,
+            ["description"] = "Grappling Hooks and Stopwatches are available in the shop. Use Grappling Hooks for swift movement and Stopwatches for quick returns."
+        },
+        ["blood_money"] = {
+            ["image"] = "rbxassetid://18461222062",
+            ["name"] = "Blood Money",
+            ["weight"] = 25,
+            ["disableInQueue"] = {"bedwars_ranked_s10"},
+            ["description"] = "Earn emeralds for every kill based on your kill streak!"
+        },
+        ["item_potions"] = {
+            ["items"] = {
+                {
+                    ["currency"] = "iron",
+                    ["itemType"] = "mini_shield",
+                    ["amount"] = 1,
+                    ["price"] = 60,
+                    ["category"] = "Potions"
+                },
+                {
+                    ["currency"] = "emerald",
+                    ["itemType"] = "big_shield",
+                    ["amount"] = 1,
+                    ["price"] = 1,
+                    ["category"] = "Potions"
+                },
+                {
+                    ["currency"] = "emerald",
+                    ["itemType"] = "invisibility_potion",
+                    ["amount"] = 1,
+                    ["price"] = 4,
+                    ["category"] = "Potions"
+                },
+                {
+                    ["currency"] = "iron",
+                    ["itemType"] = "jump_potion",
+                    ["amount"] = 1,
+                    ["price"] = 60,
+                    ["category"] = "Potions"
+                },
+                {
+                    ["currency"] = "iron",
+                    ["itemType"] = "speed_potion",
+                    ["amount"] = 1,
+                    ["price"] = 60,
+                    ["category"] = "Potions"
+                }
+            },
+            ["image"] = "rbxassetid://17014528477",
+            ["name"] = "Potions",
+            ["weight"] = 5,
+            ["description"] = "Potions are available in the shop, including speed, jump, shield, and invisibility potions."
+        },
+        ["glitched_enchant_table"] = {
+            ["image"] = "rbxassetid://17014528023",
+            ["name"] = "Glitched Enchant Table",
+            ["weight"] = 10,
+            ["disableInQueue"] = {"bedwars_ranked_s10"},
+            ["description"] = "The Enchant Table is glitched. Enchants are significantly more powerful and have additional effects."
+        },
+        ["vending_machine"] = {
+            ["description"] = "The Vending Machine will drop in the center as a mid match event.",
+            ["image"] = "rbxassetid://17014529513",
+            ["name"] = "Vending Machine",
+            ["weight"] = 0
+        },
+        ["relic_dragon_egg"] = {
+            ["image"] = "rbxassetid://17014528616",
+            ["name"] = "Dragon Egg",
+            ["weight"] = 5,
+            ["relic"] = "dragon_egg",
+            ["disableInQueue"] = {"bedwars_ranked_s10"},
+            ["description"] = "A Dragon Egg appears. The team that deposits the most resources will receive a dragon to help them in combat"
+        },
+        ["lightning"] = {
+            ["description"] = "When it gets stormy the global generators are faster...but take cover to avoid getting struck by lightning!",
+            ["image"] = "rbxassetid://17207588531",
+            ["name"] = "Lightning Storm",
+            ["weight"] = 0
+        },
+        ["more_ores"] = {
+            ["description"] = "Mineable ores will appear around the center of the map.",
+            ["image"] = "rbxassetid://17182452477",
+            ["name"] = "More Ores",
+            ["weight"] = 20
+        },
+        ["void_portals"] = {
+            ["items"] = {
+                {
+                    ["currency"] = "iron",
+                    ["itemType"] = "void_teleport_portal",
+                    ["amount"] = 1,
+                    ["price"] = 45,
+                    ["category"] = "Blocks"
+                }
+            },
+            ["image"] = "rbxassetid://17449924762",
+            ["name"] = "Void Portals",
+            ["weight"] = 10,
+            ["disableInQueue"] = {"bedwars_ranked_s10"},
+            ["description"] = "Portals are purchasable from shop. Jumping into the portal will teleport you to another random portal"
+        },
+        ["forge"] = {
+            ["description"] = "Select a starting forge weapon at the shop.",
+            ["image"] = "rbxassetid://17014527947",
+            ["name"] = "Forge",
+            ["weight"] = 20
+        },
+        ["relic_enchanted"] = {
+            ["image"] = "rbxassetid://17014528661",
+            ["name"] = "Enchanted",
+            ["weight"] = 10,
+            ["relic"] = "enchanted",
+            ["disableInQueue"] = {"bedwars_ranked_s10"},
+            ["description"] = "All players receive a random level 1 enchantment at the start of the game."
+        },
+        ["excaliburs_bounty"] = {
+            ["description"] = "At the start of the game, armor and weapons will appear in the center of the map. Prove yourself worthy and draw the diamond sword in the stone!",
+            ["image"] = "rbxassetid://18461222415",
+            ["name"] = "Excalibur's Bounty",
+            ["weight"] = 25
+        },
+        ["aerial_warfare"] = {
+            ["items"] = {
+                {
+                    ["currency"] = "emerald",
+                    ["itemType"] = "helicopter_deploy",
+                    ["amount"] = 1,
+                    ["price"] = 6,
+                    ["lockAfterPurchase"] = true,
+                    ["category"] = "Combat"
+                },
+                {
+                    ["currency"] = "emerald",
+                    ["itemType"] = "hot_air_balloon_deploy",
+                    ["amount"] = 1,
+                    ["price"] = 6,
+                    ["lockAfterPurchase"] = true,
+                    ["category"] = "Combat"
+                }
+            },
+            ["image"] = "rbxassetid://17449881242",
+            ["name"] = "Aerial Warfare",
+            ["weight"] = 10,
+            ["disableInQueue"] = {"bedwars_ranked_s10"},
+            ["description"] = "Minicopters and Hot Air Balloons are available in the shop! Start a dogfight in the sky or drop into an enemy base."
+        },
+        ["relic_supercharged_drill"] = {
+            ["image"] = "rbxassetid://17014529136",
+            ["name"] = "Supercharged Generator",
+            ["weight"] = 20,
+            ["relic"] = "supercharged_drill",
+            ["description"] = "All Team Generators start with a 20% hyperized bonus."
+        },
+        ["simplified"] = {
+            ["description"] = "Simplify the game. Removes the Diamond Guardians, Titans, Ores, Bed Plating, Altar, and Enchant Table.",
+            ["image"] = "rbxassetid://17014529254",
+            ["name"] = "Simplified",
+            ["weight"] = 5
+        },
+        ["increased_health"] = {
+            ["image"] = "rbxassetid://17014528184",
+            ["name"] = "Increased Health",
+            ["weight"] = 10,
+            ["disableInQueue"] = {"bedwars_ranked_s10"},
+            ["description"] = "All Players have increased max health."
+        },
+        ["golden_goose"] = {
+            ["description"] = "The Monarch will spawn in the center of the map.",
+            ["image"] = "rbxassetid://17014528096",
+            ["name"] = "Monarch",
+            ["weight"] = 0
+        },
+        ["squad_launcher"] = {
+            ["image"] = "rbxassetid://17449401358",
+            ["name"] = "Squad Launcher",
+            ["weight"] = 20,
+            ["disableInQueue"] = {"bedwars_ranked_s10", "bedwars_to1", "bedwars_to2", "bedwars_nb4"},
+            ["description"] = "Squad Launchers are placed at each base. Hop in with your team and launch straight to battle!"
+        },
+        ["relic_head_start"] = {
+            ["image"] = "rbxassetid://17014528722",
+            ["name"] = "Head Start",
+            ["weight"] = 20,
+            ["relic"] = "headstart",
+            ["disableInQueue"] = {"bedwars_ranked_s10"},
+            ["description"] = "All players start with 100 iron and Iron Era unlocked."
+        },
+        ["traveling_merchant"] = {
+            ["description"] = "The Traveling Merchant will set up shop in the center of the map, offering a variety of items.",
+            ["image"] = "rbxassetid://17014529447",
+            ["name"] = "Traveling Merchant",
+            ["weight"] = 0
+        },
+        ["relic_runic_divide"] = {
+            ["image"] = "rbxassetid://17014529007",
+            ["name"] = "Runic Divide",
+            ["weight"] = 10,
+            ["relic"] = "enchanter",
+            ["disableInQueue"] = {"bedwars_ranked_s10"},
+            ["description"] = "Using the Enchant Table grants 2 enchantments, but costs double the price."
+        },
+        ["item_balloon_snowball"] = {
+            ["items"] = {
+                {
+                    ["currency"] = "emerald",
+                    ["itemType"] = "balloon",
+                    ["amount"] = 3,
+                    ["price"] = 2,
+                    ["category"] = "Combat"
+                },
+                {
+                    ["currency"] = "iron",
+                    ["itemType"] = "snowball",
+                    ["amount"] = 8,
+                    ["price"] = 16,
+                    ["category"] = "Combat"
+                }
+            },
+            ["image"] = "rbxassetid://17014528259",
+            ["name"] = "Balloons And Snowballs",
+            ["weight"] = 10,
+            ["description"] = "Balloons and Snowballs are available in the shop. Use Balloons to float and Snowballs to knock players out of the air."
+        },
+        ["swift_finale"] = {
+            ["description"] = "All beds break at 12 minutes and block decay starts at 13 minutes.",
+            ["image"] = "rbxassetid://17182729859",
+            ["name"] = "Swift Finale",
+            ["weight"] = 5
+        },
+        ["relic_light_step"] = {
+            ["image"] = "rbxassetid://17014528873",
+            ["name"] = "Light Step",
+            ["weight"] = 20,
+            ["relic"] = "light_step",
+            ["disableInQueue"] = {"bedwars_ranked_s10"},
+            ["description"] = "All players have negated fall damage and increased movement speed."
+        },
+        ["snow_cone_machine"] = {
+            ["image"] = "rbxassetid://17014529354",
+            ["name"] = "Snow Cone Machine",
+            ["weight"] = 0,
+            ["disableInQueue"] = {"bedwars_ranked_s10"},
+            ["description"] = "Snow Cone Machines are placed at each base. Repair it and treat yourself to some tasty snow cones."
+        },
+        ["delicious_diamonds"] = {
+            ["description"] = "Diamonds can be consumed for shields!",
+            ["image"] = "rbxassetid://18461222208",
+            ["name"] = "Delicious Diamonds",
+            ["weight"] = 25
+        },
+        ["none"] = {
+            ["description"] = "A regular game of Bedwars, no mechanic is active for this match.",
+            ["image"] = "rbxassetid://17014528538",
+            ["name"] = "Standard",
+            ["weight"] = 10
+        }
+    }
+}
