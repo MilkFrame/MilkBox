9cf8d8f5fdddc564d57329903ba0361cacf12eda
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-17 21:47:26 +1000

    Create HWGems.lua

---
+++ GameData/Doodle World [5116869569]/HWGems.lua
@@ -0,0 +1,9 @@
+data = {
+    {["Name"] = "Topaz", ["Image"] = "rbxassetid://15344680521"},
+    {["Name"] = "Quartz", ["Image"] = "rbxassetid://15344680192"},
+    {["Name"] = "Amethyst", ["Image"] = "rbxassetid://15344680457"},
+    {["Name"] = "Sapphire", ["Image"] = "rbxassetid://15344680031"},
+    {["Name"] = "Ruby", ["Image"] = "rbxassetid://15344680084"},
+    {["Name"] = "Emerald", ["Image"] = "rbxassetid://15344680301"},
+    {["Name"] = "Diamond", ["Image"] = "rbxassetid://15344680394"}
+}
