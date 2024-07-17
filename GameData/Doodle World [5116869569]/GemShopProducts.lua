ff8cc4f95b704df27a2d550acdfadd6c2099339d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-17 21:37:58 +1000

    Update GemShopProducts.lua

--- GameData/Doodle World [5116869569]/GemShopProducts.lua
+++ GameData/Doodle World [5116869569]/GemShopProducts.lua
@@ -1,93 +1,138 @@
 data = {
     {
-        ["HeldItem"] = "Bionotic Rune",
         ["Type"] = "Gems",
-        ["Description"] = "Purchase this to get a Bionotic with its NEW runestone!",
-        ["Showcase"] = "Bionotic-A",
-        ["Line"] = "Bionotic",
-        ["Item"] = "Doodle",
-        ["Skin"] = 0,
-        ["Cost"] = 5000,
-        ["Name"] = "Bionotic-A",
-        ["New"] = true
-    },
-    {
-        ["HeldItem"] = "Lumiline Rune",
-        ["Type"] = "Gems",
-        ["Description"] = "Purchase this to get a Lumiline with its NEW runestone!",
-        ["Showcase"] = "Lumiline-A",
-        ["Line"] = "Lumiline",
-        ["Item"] = "Doodle",
-        ["Skin"] = 0,
-        ["Cost"] = 5000,
-        ["Name"] = "Lumiline-A",
-        ["New"] = true
-    },
-    {
-        ["Type"] = "Gems",
-        ["Description"] = "Purchase this to get a Electrician Riptorvent. This skin is only obtainable with gems.",
-        ["Showcase"] = "Riptorvent",
-        ["Line"] = "Riptorvent",
+        ["Description"] = "Have a 1% chance that this comes with a super dog Exiled instead! Guaranteed to get a super dog Exiled after 50 purchases if you did not get one by then.",
+        ["Showcase"] = "Exiled",
+        ["Special"] = true,
+        ["Line"] = "Exiled",
         ["Item"] = "Doodle",
         ["Skin"] = 7,
-        ["Premium"] = true,
-        ["Cost"] = 4600,
-        ["Name"] = "Electrician Riptorvent"
+        ["Cost"] = 2000,
+        ["Name"] = "dog Exiled"
+    },
+    {
+        ["Image"] = "rbxassetid://17452759360",
+        ["Item"] = "Item",
+        ["Cost"] = 750,
+        ["Type"] = "Robux",
+        ["Name"] = "BP 3 Premium",
+        ["Description"] = "Purchase this to get a BP 3 Premium Ticket! Using this ticket grants you Battlepass premium! You can trade this to others!",
+        ["Product"] = 1826401341
     },
     {
         ["Type"] = "Gems",
-        ["Description"] = "Purchase this to get a Digital Metalytra!",
-        ["Showcase"] = "Metalytra",
-        ["Line"] = "Metalytra",
-        ["Item"] = "Doodle",
-        ["Skin"] = 18,
-        ["Cost"] = 3600,
-        ["Name"] = "Digital Metalytra",
-        ["New"] = true
-    },
-    {
-        ["Type"] = "Gems",
-        ["Description"] = "Purchase this to get a Donut Banautilus!",
-        ["Showcase"] = "Banautilus",
-        ["Line"] = "Banautilus",
+        ["Description"] = "A legendary skin! Comes with the Doodle. 25% off!",
+        ["Showcase"] = "Zerzura",
+        ["Sale"] = true,
+        ["Line"] = "Zerzura",
         ["Item"] = "Doodle",
         ["Skin"] = 3,
-        ["Cost"] = 3600,
-        ["Name"] = "Donut Banautilus",
-        ["New"] = true
+        ["Cost"] = 6000,
+        ["Name"] = "Ocean Guardian Zerzura"
     },
     {
         ["Type"] = "Gems",
-        ["Description"] = "Purchase this to get a Strawberry Shortcake Jellupy!",
-        ["Showcase"] = "Jellupy",
-        ["Line"] = "Jellupy",
+        ["Description"] = "A legendary skin! Comes with the Doodle.",
+        ["Showcase"] = "Koriyu",
+        ["Line"] = "Koriyu",
         ["Item"] = "Doodle",
-        ["Skin"] = 26,
-        ["Cost"] = 3600,
-        ["Name"] = "Strawberry Shortcake Jellupy",
-        ["New"] = true
+        ["Skin"] = 7,
+        ["Cost"] = 8000,
+        ["Legendary"] = true,
+        ["Name"] = "Chess Koriyu"
     },
     {
         ["Type"] = "Gems",
-        ["Description"] = "Purchase this to get a Cyberpunk Tasmarauder!",
-        ["Showcase"] = "Hollihare",
-        ["Line"] = "Hollihare",
+        ["Description"] = "Comes with the Doodle. Can't be encountered in the wild. 20% off!",
+        ["Showcase"] = "Seamstress",
+        ["Sale"] = true,
+        ["Line"] = "Seamknight",
+        ["Item"] = "Doodle",
+        ["Skin"] = 7,
+        ["Cost"] = 3200,
+        ["Name"] = "Stargazer Seamstress"
+    },
+    {
+        ["Type"] = "Gems",
+        ["Description"] = "Comes with the Doodle. Can't be encountered in the wild. 20% off!",
+        ["Showcase"] = "Mawthra",
+        ["Sale"] = true,
+        ["Line"] = "Mawthra",
+        ["Item"] = "Doodle",
+        ["Skin"] = 13,
+        ["Cost"] = 3200,
+        ["Name"] = "Gilded Mawthra"
+    },
+    {
+        ["Type"] = "Gems",
+        ["Description"] = "Comes with the Doodle. Can't be encountered in the wild. 20% off!",
+        ["Showcase"] = "Humbiscus",
+        ["Sale"] = true,
+        ["Line"] = "Humbiscus",
+        ["Item"] = "Doodle",
+        ["Skin"] = 12,
+        ["Cost"] = 3200,
+        ["Name"] = "Cosmic Wonderer Humbiscus"
+    },
+    {
+        ["Type"] = "Gems",
+        ["Description"] = "Comes with the Doodle. Can't be encountered in the wild. 20% off!",
+        ["Showcase"] = "Archopos",
+        ["Sale"] = true,
+        ["Line"] = "Archopos",
         ["Item"] = "Doodle",
         ["Skin"] = 15,
-        ["Cost"] = 3600,
-        ["Name"] = "Cozy Hollihare",
-        ["New"] = true
+        ["Cost"] = 3200,
+        ["Name"] = "Sorceror Archopos"
     },
     {
         ["Type"] = "Gems",
-        ["Description"] = "Purchase this to get a Nerd Finwick!",
-        ["Showcase"] = "Finwick",
-        ["Line"] = "Finwick",
+        ["Description"] = "Comes with the Doodle. Can't be encountered in the wild. 20% off!",
+        ["Showcase"] = "Djinneko",
+        ["Sale"] = true,
+        ["Line"] = "Djinneko",
         ["Item"] = "Doodle",
-        ["Skin"] = 3,
-        ["Cost"] = 3600,
-        ["Name"] = "Nerd Finwick",
+        ["Skin"] = 10,
+        ["Cost"] = 3200,
+        ["Name"] = "Jazz Band Djinneko"
+    },
+    {
+        ["Type"] = "Gems",
+        ["Description"] = "Comes with the Doodle. Can't be encountered in the wild. 20% off!",
+        ["Showcase"] = "Spiraryu",
+        ["Sale"] = true,
+        ["Line"] = "Spirasol",
+        ["Item"] = "Doodle",
+        ["Skin"] = 20,
+        ["Cost"] = 3200,
+        ["Name"] = "Cosmic Spiraryu"
+    },
+    {
+        ["Type"] = "Gems",
+        ["Item"] = "Particle",
+        ["Cost"] = 2000,
+        ["Description"] = "You can equip this Particle on your Character by going into the Customize Menu!",
+        ["Particle"] = "2",
+        ["Name"] = "Rainy Day Particle"
+    },
+    {
+        ["Type"] = "Gems",
+        ["Item"] = "Particle",
+        ["Cost"] = 2000,
+        ["Description"] = "You can equip this Particle on your Character by going into the Customize Menu!",
+        ["Name"] = "Cleansing Pool",
+        ["Particle"] = "19",
         ["New"] = true
     },
-    ["CurrentWeek"] = 53
+    {
+        ["Type"] = "Robux",
+        ["Image"] = "rbxassetid://16053808183",
+        ["Item"] = "Bike",
+        ["Cost"] = 150,
+        ["Id"] = "21",
+        ["Name"] = "Sukikogami Motorcycle",
+        ["Description"] = "A cool spiffy motorcycle!",
+        ["Product"] = 1736769294
+    },
+    ["CurrentWeek"] = 88
 }
