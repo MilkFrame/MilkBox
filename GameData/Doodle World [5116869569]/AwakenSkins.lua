cc56c85cf7ffa076ae8b619750769afd6a941f63
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-17 21:42:33 +1000

    Create AwakenSkins.lua

---
+++ GameData/Doodle World [5116869569]/AwakenSkins.lua
@@ -0,0 +1,190 @@
+data = {
+    ["Cacmeow-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=16538079737",
+            ["Name"] = "Litterfall",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=16538082158",
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=16538080912",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=16538078518"
+        }
+    },
+    ["Malotrick-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=16907171708",
+            ["Name"] = "Fool",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=16905084631",
+            ["WaitTime"] = 0.2,
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=16905084420",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=16907171469"
+        },
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=17464257505",
+            ["Name"] = "Exalted",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=17464257157",
+            ["Contributors"] = {2211553344},
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=17464257397",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=17464257670"
+        }
+    },
+    ["Tufflaze-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=15895052802",
+            ["Name"] = "Blossom",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=15890205572",
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=15890205437",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=15895052492"
+        }
+    },
+    ["Staligant-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=16236816252",
+            ["Name"] = "Galaxy",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=16236815934",
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=16236816096",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=16236816474"
+        }
+    },
+    ["Cryotera-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=15807693042",
+            ["Name"] = "Crimson",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=15807693384",
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=15807693267",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=15919983559"
+        }
+    },
+    ["Lumiline-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=16236830747",
+            ["Name"] = "Carnelian",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=16236823614",
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=16236823781",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=16236830924"
+        }
+    },
+    ["Archopos-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=17464059629",
+            ["Name"] = "Alacritous",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=17464051817",
+            ["Contributors"] = {87921828, 153637695},
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=17464042844",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=17464051351"
+        }
+    },
+    ["Vigimantè-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=17464284309",
+            ["Name"] = "Lemondrop",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=17464283899",
+            ["Contributors"] = {1793190103},
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=17464283730",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=17464284126"
+        }
+    },
+    ["Grufflin-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=15914805582",
+            ["Name"] = "Diamond",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=15914805833",
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=15914805725",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=15914805506"
+        }
+    },
+    ["Spectatik-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=17464239896",
+            ["Name"] = "Auric",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=17464239567",
+            ["Contributors"] = {566737194},
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=17464239706",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=17464240242"
+        }
+    },
+    ["Groato-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=15933752982",
+            ["Name"] = "Zenith",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=15933754523",
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=15933753707",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=15933752129"
+        }
+    },
+    ["Theaterror-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=17464192468",
+            ["Name"] = "Academy",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=17464185277",
+            ["Contributors"] = {1243948529},
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=17464184909",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=17464185415"
+        }
+    },
+    ["Somberock-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=15890204623",
+            ["Name"] = "Celestial",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=15890205006",
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=15890204824",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=15890204502"
+        }
+    },
+    ["Thornet-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=17464052289",
+            ["Contributors"] = {131329154},
+            ["Name"] = "Pristine",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=17464016951",
+            ["BackSN"] = "http://www.roblox.com/asset/?id=17464016836",
+            ["FrontSN"] = "http://www.roblox.com/asset/?id=17464161373",
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=17464017235",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=17464059991"
+        }
+    },
+    ["Hattrix-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=15809996849",
+            ["Name"] = "Solidarity",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=15810002898",
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=15810001335",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=15809994524"
+        }
+    },
+    ["Nyanto-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=16236823275",
+            ["Name"] = "Admiral",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=16236822941",
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=16236823107",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=16236823476"
+        }
+    },
+    ["Partybug-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=16534295217",
+            ["Name"] = "Azure",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=16534297818",
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=16534296686",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=16534293698"
+        }
+    },
+    ["Bionotic-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=16477427192",
+            ["Name"] = "Kitty",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=16477432501",
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=16477429525",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=16477424661"
+        }
+    },
+    ["Maskomedy-A"] = {
+        {
+            ["SFSprite"] = "http://www.roblox.com/asset/?id=17464192603",
+            ["Name"] = "Academy",
+            ["SBSprite"] = "http://www.roblox.com/asset/?id=17464184790",
+            ["Contributors"] = {1243948529},
+            ["BackSprite"] = "http://www.roblox.com/asset/?id=17464185068",
+            ["FrontSprite"] = "http://www.roblox.com/asset/?id=17464185592"
+        }
+    }
+}
