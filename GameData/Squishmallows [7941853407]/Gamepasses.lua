7e454e46669fe59b9ea54d781073aea915bd45c4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-17 21:20:47 +1000

    Create Gamepasses.lua

---
+++ GameData/Squishmallows [7941853407]/Gamepasses.lua
@@ -0,0 +1,106 @@
+data = {
+    ["Names"] = {
+        ["Choose your Plot"] = 24683076,
+        ["VIP Pack"] = 24682635,
+        ["Hoverboard"] = 24682735,
+        ["Starter Pack"] = 24683065,
+        ["Squish Club"] = 24682725,
+        ["Celebrity Pack"] = 24682656,
+        ["+1 Equipped Squishmallows"] = 24682696,
+        ["'Mallow Mansion"] = 24682666,
+        ["Premium Emotes"] = 24682678
+    },
+    ["List"] = {
+        {
+            ["Image"] = "",
+            ["Name"] = "Hoverboard",
+            ["Robux"] = 375,
+            ["ID"] = 10,
+            ["ProductId"] = 24682735,
+            ["Category"] = "Aesthetics",
+            ["Description"] = "Travel at super speed on this exclusive Hoverboard!"
+        },
+        {
+            ["Image"] = "",
+            ["Name"] = "Choose your Plot",
+            ["Robux"] = 250,
+            ["ID"] = 6,
+            ["ProductId"] = 24683076,
+            ["Category"] = "Home",
+            ["Description"] = "Grants the ability to choose your plot in the Neighborhood through the House Menu."
+        },
+        {
+            ["Image"] = "",
+            ["Name"] = "Starter Pack",
+            ["Robux"] = 125,
+            ["ID"] = 1,
+            ["ProductId"] = 24683065,
+            ["Category"] = "Special Event",
+            ["Description"] = [[Time to get started!\n\nThis pack comes with:\n-Family Home\n-500 S$\n-1 Random Core Squishmallows]]
+        },
+        {
+            ["Image"] = "",
+            ["Name"] = "Premium Emotes",
+            ["Robux"] = 125,
+            ["ID"] = 5,
+            ["ProductId"] = 24682678,
+            ["Category"] = "Aesthetics",
+            ["Description"] = "Currently unavailable"
+        },
+        {
+            ["Image"] = "",
+            ["Name"] = "VIP Pack",
+            ["Robux"] = 1000,
+            ["ID"] = 2,
+            ["ProductId"] = 24682635,
+            ["Category"] = "Bundles",
+            ["Description"] = [[👑 VIP coming through!\n\nThis pack comes with:\n-Modern Villa\n-2500 S$\n-1 Random Game Exclusive Squishmallows]]
+        },
+        {
+            ["Image"] = "",
+            ["Name"] = "Celebrity Pack",
+            ["Robux"] = 1250,
+            ["ID"] = 3,
+            ["ProductId"] = 24682656,
+            ["Category"] = "Bundles",
+            ["Description"] = [[🌟 Roll out the red carpet!\n\nThis pack comes with:\n-Celebrity Mansion\n-3000 S$\n-1 Random Game Exclusive Squishmallows]]
+        },
+        {
+            ["Image"] = "",
+            ["Name"] = "+1 Equipped Squishmallows",
+            ["Robux"] = 250,
+            ["ID"] = 8,
+            ["ProductId"] = 24682702,
+            ["Category"] = "Squishmallows",
+            ["Description"] = [[😲This Gamepass expands the limit of equipped Squishmallows by 1. \nThis stacks with the other +1 Gamepass to allow for up to 5 if both are owned.]]
+        },
+        {
+            ["Image"] = "",
+            ["Name"] = "+1 Equipped Squishmallows",
+            ["Robux"] = 250,
+            ["ID"] = 7,
+            ["ProductId"] = 24682696,
+            ["Category"] = "Squishmallows",
+            ["Description"] = [[😲This Gamepass expands the limit of equipped Squishmallows by 1. \nThis stacks with the other +1 Gamepass to allow for up to 5 if both are owned.]]
+        },
+        {
+            ["Image"] = "",
+            ["Name"] = "Squish Club",
+            ["Robux"] = 400,
+            ["ID"] = 9,
+            ["ProductId"] = 24682725,
+            ["Category"] = "Bundles",
+            ["Description"] = [[🏅Welcome to the club! \n\nSquish Club grants: \n-Exclusive Squish Club chat tag and color \n-Special Squish Club icon next to your display name \n-1.5x Reward in all minigames]]
+        },
+        {
+            ["Image"] = "",
+            ["Name"] = "'Mallow Mansion",
+            ["Robux"] = 850,
+            ["ID"] = 4,
+            ["ProductId"] = 24682666,
+            ["Category"] = "Home",
+            ["Description"] = [[⚜️Live like royalty in the 'Mallow Mansion! \nThis home is only available through Gamepass.]]
+        }
+    },
+    ["Featured"] = {["Aesthetics"] = 24682678, ["Home"] = 24682666, ["Bundles"] = 24682725}
+}
