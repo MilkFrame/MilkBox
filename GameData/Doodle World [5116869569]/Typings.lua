7a858a31511c4c525ce09ffab4f0f599c8e396bc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-17 21:27:26 +1000

    Update Typings.lua

--- GameData/Doodle World [5116869569]/Typings.lua
+++ GameData/Doodle World [5116869569]/Typings.lua
@@ -1,56 +1,107 @@
+--require(game.Players.LocalPlayer.Packer.Database.Typings)("Get")
 data = {
     ["Visual"] = {
-        ["Mythic"] = {["Color"] = Color3.fromRGB(72, 89, 190), ["Image"] = "http://www.roblox.com/asset/?id=4604289554"},
+        ["Mythic"] = {
+            ["Color"] = Color3.fromRGB(72, 89, 190),
+            ["RT"] = "rgb(72, 89, 190)",
+            ["Image"] = "http://www.roblox.com/asset/?id=4604289554"
+        },
         ["Poison"] = {
             ["Color"] = Color3.fromRGB(180, 167, 214),
+            ["RT"] = "rgb(180, 167, 214)",
             ["Image"] = "http://www.roblox.com/asset/?id=4598176219"
         },
-        ["???"] = {["Color"] = Color3.fromRGB(255, 255, 255), ["Image"] = ""},
+        ["???"] = {["Color"] = Color3.fromRGB(255, 255, 255), ["RT"] = "rgb(255, 255, 255)", ["Image"] = ""},
         ["Water"] = {
             ["Color"] = Color3.fromRGB(109, 158, 235),
+            ["RT"] = "rgb(109, 158, 235)",
             ["Image"] = "http://www.roblox.com/asset/?id=4598175859"
         },
         ["Basic"] = {
             ["Color"] = Color3.fromRGB(204, 204, 204),
+            ["RT"] = "rgb(204,204,204)",
             ["Image"] = "http://www.roblox.com/asset/?id=4557208777"
         },
         ["Insect"] = {
             ["Color"] = Color3.fromRGB(160, 206, 131),
+            ["RT"] = "rgb(160, 206, 131)",
             ["Image"] = "http://www.roblox.com/asset/?id=4604288448"
         },
         ["Spirit"] = {
             ["Color"] = Color3.fromRGB(103, 78, 167),
+            ["RT"] = "rgb(103, 78, 167)",
             ["Image"] = "http://www.roblox.com/asset/?id=4598179077"
         },
         ["Light"] = {
             ["Color"] = Color3.fromRGB(255, 229, 153),
+            ["RT"] = "rgb(255, 229, 153)",
             ["Image"] = "http://www.roblox.com/asset/?id=4598176416"
         },
         ["Crystal"] = {
             ["Color"] = Color3.fromRGB(90, 194, 200),
+            ["RT"] = "rgb(90, 194, 200)",
             ["Image"] = "http://www.roblox.com/asset/?id=6524832960"
         },
-        ["Melee"] = {["Color"] = Color3.fromRGB(216, 74, 56), ["Image"] = "http://www.roblox.com/asset/?id=4604288204"},
-        ["Food"] = {["Color"] = Color3.fromRGB(249, 203, 156), ["Image"] = "http://www.roblox.com/asset/?id=4604283378"},
-        ["Spark"] = {["Color"] = Color3.fromRGB(255, 228, 98), ["Image"] = "http://www.roblox.com/asset/?id=4604834458"},
-        ["Dark"] = {["Color"] = Color3.fromRGB(102, 102, 102), ["Image"] = "http://www.roblox.com/asset/?id=4598175756"},
+        ["Melee"] = {
+            ["Color"] = Color3.fromRGB(216, 74, 56),
+            ["RT"] = "rgb(216, 74, 56)",
+            ["Image"] = "http://www.roblox.com/asset/?id=4604288204"
+        },
+        ["Food"] = {
+            ["Color"] = Color3.fromRGB(249, 203, 156),
+            ["RT"] = "rgb(249, 203, 156)",
+            ["Image"] = "http://www.roblox.com/asset/?id=4604283378"
+        },
+        ["Spark"] = {
+            ["Color"] = Color3.fromRGB(255, 228, 98),
+            ["RT"] = "rgb(255, 228, 98)",
+            ["Image"] = "http://www.roblox.com/asset/?id=4604834458"
+        },
+        ["Dark"] = {
+            ["Color"] = Color3.fromRGB(102, 102, 102),
+            ["RT"] = "rgb(102, 102, 102)",
+            ["Image"] = "http://www.roblox.com/asset/?id=4598175756"
+        },
         ["Metal"] = {
             ["Color"] = Color3.fromRGB(136, 160, 177),
+            ["RT"] = "rgb(136, 160, 177)",
             ["Image"] = "http://www.roblox.com/asset/?id=4604834752"
         },
         ["Earth"] = {
             ["Color"] = Color3.fromRGB(184, 137, 107),
+            ["RT"] = "rgb(184, 137, 107)",
             ["Image"] = "http://www.roblox.com/asset/?id=4604284148"
         },
-        ["Air"] = {["Color"] = Color3.fromRGB(207, 226, 243), ["Image"] = "http://www.roblox.com/asset/?id=4604289154"},
-        ["Ice"] = {["Color"] = Color3.fromRGB(159, 197, 232), ["Image"] = "http://www.roblox.com/asset/?id=4557207717"},
+        ["Air"] = {
+            ["Color"] = Color3.fromRGB(207, 226, 243),
+            ["RT"] = "rgb(207, 226, 243)",
+            ["Image"] = "http://www.roblox.com/asset/?id=4604289154"
+        },
+        ["Ice"] = {
+            ["Color"] = Color3.fromRGB(159, 197, 232),
+            ["RT"] = "rgb(159, 197, 232)",
+            ["Image"] = "http://www.roblox.com/asset/?id=4557207717"
+        },
         ["Plant"] = {
             ["Color"] = Color3.fromRGB(106, 171, 110),
+            ["RT"] = "rgb(106, 171, 110)",
             ["Image"] = "http://www.roblox.com/asset/?id=4557208188"
         },
-        ["Fire"] = {["Color"] = Color3.fromRGB(230, 145, 56), ["Image"] = "http://www.roblox.com/asset/?id=4598175977"},
-        ["Beast"] = {["Color"] = Color3.fromRGB(133, 104, 94), ["Image"] = "http://www.roblox.com/asset/?id=4557207616"},
-        ["Mind"] = {["Color"] = Color3.fromRGB(234, 153, 153), ["Image"] = "http://www.roblox.com/asset/?id=6519698515"}
+        ["Fire"] = {
+            ["Color"] = Color3.fromRGB(230, 145, 56),
+            ["RT"] = "rgb(230, 145, 56)",
+            ["Image"] = "http://www.roblox.com/asset/?id=4598175977"
+        },
+        ["Beast"] = {
+            ["Color"] = Color3.fromRGB(133, 104, 94),
+            ["RT"] = "rgb(133, 104, 94)",
+            ["Image"] = "http://www.roblox.com/asset/?id=4557207616"
+        },
+        ["Mind"] = {
+            ["Color"] = Color3.fromRGB(234, 153, 153),
+            ["RT"] = "rgb(234, 153, 153)",
+            ["Image"] = "http://www.roblox.com/asset/?id=6519698515"
+        }
     },
     ["EffectiveIcons"] = {
         {["Color"] = Color3.fromRGB(255, 0, 0), ["Image"] = "http://www.roblox.com/asset/?id=4597964542"},
@@ -59,8 +110,8 @@ data = {
         {["Color"] = Color3.fromRGB(170, 0, 0), ["Image"] = "http://www.roblox.com/asset/?id=4597964185"},
         {["Color"] = Color3.fromRGB(0, 200, 0), ["Image"] = "http://www.roblox.com/asset/?id=4597964685"}
     },
-    ["GetEffectiveness"] = "function: 0x000000000a07e8c5",
-    ["GetColor"] = "function: 0x0000000009545645",
+    ["GetEffectiveness"] = "function: 0x7fed09d7d618d0cc",
+    ["GetColor"] = "function: 0x857755489a96c49c",
     ["TypeOrder"] = {
         "Basic",
         "Fire",
@@ -83,10 +134,10 @@ data = {
         "Poison",
         "Mythic"
     },
-    ["GetImage"] = "function: 0x0000000078a326dd",
-    ["GetTypeColor"] = "function: 0x00000000db9e0455",
+    ["GetImage"] = "function: 0x7d4b7a63dab9412c",
+    ["GetTypeColor"] = "function: 0xbef8c70905b5b15c",
     ["TypeChart"] = {
-        ["Mythic"] = {["NVE"] = {"Crystal", "Food"}, ["NH"] = {}, ["SE"] = {"Mythic"}},
+        ["Mythic"] = {["NVE"] = {"Crystal"}, ["NH"] = {}, ["SE"] = {"Mythic"}},
         ["Poison"] = {
             ["NVE"] = {"Poison", "Spirit", "Earth"},
             ["NH"] = {"Crystal", "Metal"},
@@ -101,20 +152,24 @@ data = {
             ["SE"] = {"Mind", "Food", "Plant", "Dark"}
         },
         ["Spirit"] = {["NVE"] = {"Crystal", "Dark"}, ["NH"] = {"Basic", "Light"}, ["SE"] = {"Spirit", "Mind"}},
-        ["Light"] = {["NVE"] = {"Plant", "Crystal", "Light"}, ["NH"] = {}, ["SE"] = {"Mythic", "Dark", "Spirit"}},
+        ["Light"] = {
+            ["NVE"] = {"Food", "Crystal", "Light", "Plant"},
+            ["NH"] = {},
+            ["SE"] = {"Mythic", "Dark", "Spirit"}
+        },
         ["Crystal"] = {["NVE"] = {"Earth", "Ice", "Water"}, ["NH"] = {}, ["SE"] = {"Spark", "Mythic", "Spirit"}},
         ["Melee"] = {
             ["NVE"] = {"Mind", "Insect", "Air", "Poison"},
             ["NH"] = {"Spirit"},
             ["SE"] = {"Basic", "Crystal", "Ice", "Metal"}
         },
-        ["Food"] = {["NVE"] = {"Fire", "Beast", "Plant", "Spirit"}, ["NH"] = {}, ["SE"] = {"Mind", "Melee", "Mythic"}},
+        ["Food"] = {["NVE"] = {"Fire", "Beast", "Plant", "Spirit"}, ["NH"] = {}, ["SE"] = {"Mind", "Melee"}},
         ["Spark"] = {
             ["NVE"] = {"Mythic", "Plant", "Food", "Spark", "Crystal"},
             ["NH"] = {"Earth"},
             ["SE"] = {"Beast", "Air", "Water", "Insect"}
         },
-        ["Dark"] = {["NVE"] = {"Food", "Dark", "Insect"}, ["NH"] = {}, ["SE"] = {"Mind", "Light", "Spirit"}},
+        ["Dark"] = {["NVE"] = {"Beast", "Food", "Dark", "Insect"}, ["NH"] = {}, ["SE"] = {"Mind", "Light", "Spirit"}},
         ["Metal"] = {["NVE"] = {"Fire", "Spark", "Water", "Metal"}, ["NH"] = {}, ["SE"] = {"Ice", "Crystal"}},
         ["Earth"] = {
             ["NVE"] = {"Plant", "Insect"},
