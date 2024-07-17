4191a5093092fd07cc1eb815c66aa24615e86767
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-17 21:43:44 +1000

    Create EventSkin.lua

---
+++ GameData/Doodle World [5116869569]/EventSkin.lua
@@ -0,0 +1,23 @@
+data = {
+    ["VDay"] = {
+        {["ID"] = "Tufflaze", ["Rarity"] = 1, ["Skin"] = 20},
+        {["ID"] = "Hypnotl", ["Rarity"] = 1, ["Skin"] = 9},
+        {["ID"] = "Candeigon", ["Rarity"] = 1, ["Skin"] = 12},
+        {["ID"] = "Armaratus", ["Rarity"] = 1, ["Skin"] = 2},
+        {["ID"] = "Sukikogami", ["Rarity"] = 1, ["Skin"] = 6},
+        {["ID"] = "Irikil", ["Rarity"] = 1, ["Skin"] = 7},
+        {["ID"] = "Snowclowne", ["Rarity"] = 1, ["Skin"] = 8},
+        {["ID"] = "Umaisho", ["Rarity"] = 1, ["Skin"] = 7},
+        {["ID"] = "Spectatik", ["Rarity"] = 1, ["Skin"] = 12},
+        {["ID"] = "Desdog", ["Rarity"] = 1, ["Skin"] = 12},
+        {["ID"] = "Bronzoch", ["Rarity"] = 1, ["Skin"] = 7},
+        {["ID"] = "Spunny", ["Rarity"] = 1, ["Skin"] = 21},
+        {["ID"] = "Wyvernal", ["Rarity"] = 1, ["Skin"] = 8},
+        {["ID"] = "Groato", ["Rarity"] = 1, ["Skin"] = 22},
+        {["ID"] = "Jinglark", ["Rarity"] = 1, ["Skin"] = 3},
+        {["ID"] = "Zerzura", ["Rarity"] = 2, ["Skin"] = 5},
+        {["ID"] = "Qilintel", ["Rarity"] = 2, ["Skin"] = 11},
+        {["ID"] = "Seamknight", ["Rarity"] = 3, ["Skin"] = 15},
+        {["ID"] = "Vixalor", ["Rarity"] = 3, ["Skin"] = 16}
+    }
+}
