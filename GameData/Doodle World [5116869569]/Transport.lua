e31496eeadd1fdcef9d12cde91d07b4393a838a1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-17 21:42:14 +1000

    Update Transport.lua

--- GameData/Doodle World [5116869569]/Transport.lua
+++ GameData/Doodle World [5116869569]/Transport.lua
@@ -1,16 +1,10 @@
 data = {
     {["Name"] = "NONE", ["Image"] = ""},
-    ["14"] = {
-        ["Type"] = "RainbowSurfboard",
-        ["Image"] = "rbxassetid://14016477253",
-        ["Name"] = "Swirl Surfboard",
-        ["Model"] = "SwirlSurfboard"
-    },
-    ["15"] = {
-        ["Type"] = "RainbowSurfboard",
-        ["Image"] = "rbxassetid://14016477108",
-        ["Name"] = "Beach Surfboard",
-        ["Model"] = "BeachSurfboard"
+    ["29"] = {
+        ["Type"] = "Hoverboard",
+        ["Image"] = "rbxassetid://17265609211",
+        ["Name"] = "Blue Hoverboard",
+        ["Model"] = "BlueHoverboard"
     },
     ["1"] = {
         ["Image"] = "rbxassetid://12000437109",
@@ -77,11 +71,11 @@ data = {
         ["Animations"] = {["Idle"] = {12844000391}, ["Movement"] = {12844001503, 12844002772, 12844003650}},
         ["Type"] = "PattyDay"
     },
-    ["16"] = {
-        ["Type"] = "BalloonAnimal",
-        ["Image"] = "rbxassetid://14016632387",
-        ["Name"] = "King Plipo Balloon",
-        ["Model"] = "KingPlipo"
+    ["27"] = {
+        ["Type"] = "Scooter",
+        ["Image"] = "rbxassetid://17265610035",
+        ["Name"] = "Blue Scooter",
+        ["Model"] = "BlueScooter"
     },
     ["17"] = {
         ["Type"] = "Skateboard",
@@ -95,11 +89,12 @@ data = {
         ["Name"] = "Palm Surfboard",
         ["Model"] = "PalmSurfboard"
     },
-    ["12"] = {
-        ["Type"] = "RainbowSurfboard",
-        ["Image"] = "rbxassetid://14016477623",
-        ["Name"] = "Wood Surfboard",
-        ["Model"] = "WoodSurfboard"
+    ["21"] = {
+        ["Image"] = "rbxassetid://16053808183",
+        ["Name"] = "Sukikogami Motorcycle",
+        ["Model"] = "SukikogamiMotorcycle",
+        ["DevProduct"] = 1736769294,
+        ["Type"] = "OrigamiMotorcycle"
     },
     ["11"] = {
         ["Image"] = "rbxassetid://13820836909",
@@ -107,6 +102,106 @@ data = {
         ["Model"] = "StandardSkateboard",
         ["Price"] = 1400,
         ["Type"] = "Skateboard"
+    },
+    ["23"] = {
+        ["Type"] = "LoveBoat",
+        ["Image"] = "rbxassetid://16318331213",
+        ["Name"] = "Love Boat",
+        ["Model"] = "LoveBoat"
+    },
+    ["28"] = {
+        ["Type"] = "Scooter",
+        ["Image"] = "rbxassetid://17265608595",
+        ["Name"] = "Red Scooter",
+        ["Model"] = "RedScooter"
+    },
+    ["33"] = {
+        ["Type"] = "RunicRift",
+        ["Image"] = "rbxassetid://17467090844",
+        ["Name"] = "Runic Prisoner",
+        ["Model"] = "RunicRift"
+    },
+    ["32"] = {
+        ["Type"] = "WingedKlicki",
+        ["Image"] = "rbxassetid://17451959000",
+        ["Name"] = "Winged Klicki",
+        ["Model"] = "VelveyKlicki"
+    },
+    ["31"] = {
+        ["Image"] = "rbxassetid://17265609011",
+        ["Name"] = "Klicki Bumper Car",
+        ["Model"] = "KlickiBumper",
+        ["DevProduct"] = 1815469535,
+        ["Type"] = "KlickiBumper"
+    },
+    ["30"] = {
+        ["Type"] = "Hoverboard",
+        ["Image"] = "rbxassetid://17265608843",
+        ["Name"] = "Red Hoverboard",
+        ["Model"] = "RedHoverboard"
+    },
+    ["19"] = {
+        ["Type"] = "BalloonAnimal",
+        ["Image"] = "rbxassetid://15988556555",
+        ["Name"] = "Louis Balloon",
+        ["Model"] = "LouisBalloon"
+    },
+    ["18"] = {
+        ["Type"] = "BalloonAnimal",
+        ["Image"] = "rbxassetid://15599271127",
+        ["Name"] = "Exiled Balloon",
+        ["Model"] = "ExiledBalloon"
+    },
+    ["25"] = {
+        ["Type"] = "TheCoffin",
+        ["Image"] = "rbxassetid://16863990648",
+        ["Name"] = "The Coffin",
+        ["Model"] = "TheCoffin"
+    },
+    ["15"] = {
+        ["Type"] = "RainbowSurfboard",
+        ["Image"] = "rbxassetid://14016477108",
+        ["Name"] = "Beach Surfboard",
+        ["Model"] = "BeachSurfboard"
+    },
+    ["14"] = {
+        ["Type"] = "RainbowSurfboard",
+        ["Image"] = "rbxassetid://14016477253",
+        ["Name"] = "Swirl Surfboard",
+        ["Model"] = "SwirlSurfboard"
+    },
+    ["24"] = {["Type"] = "Bungo", ["Image"] = "rbxassetid://16603143630", ["Name"] = "Bungo", ["Model"] = "Bungo"},
+    ["16"] = {
+        ["Type"] = "BalloonAnimal",
+        ["Image"] = "rbxassetid://14016632387",
+        ["Name"] = "King Plipo Balloon",
+        ["Model"] = "KingPlipo"
+    },
+    ["26"] = {
+        ["Type"] = "PartyCar",
+        ["Image"] = "rbxassetid://17116946740",
+        ["Name"] = "Party Car",
+        ["Model"] = "PartybugCar"
+    },
+    ["20"] = {
+        ["Image"] = "rbxassetid://16041228823",
+        ["Name"] = "Vigimantè Glider",
+        ["Model"] = "MantaGlider",
+        ["DevProduct"] = 1736004790,
+        ["Type"] = "MantaGlider"
+    },
+    ["12"] = {
+        ["Type"] = "RainbowSurfboard",
+        ["Image"] = "rbxassetid://14016477623",
+        ["Name"] = "Wood Surfboard",
+        ["Model"] = "WoodSurfboard"
+    },
+    ["22"] = {
+        ["Image"] = "rbxassetid://16257937230",
+        ["Name"] = "Great Beast Yorus",
+        ["Model"] = "Torus",
+        ["DevProduct"] = 1748446880,
+        ["Type"] = "Torus"
     },
     ["10"] = {
         ["Type"] = "RainbowSurfboard",
