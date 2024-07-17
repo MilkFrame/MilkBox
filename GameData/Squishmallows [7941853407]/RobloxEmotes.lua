caf6c9889f3fbfe0ab9c9d419b5b4be9bd7e1799
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-17 21:18:25 +1000

    Create RobloxEmotes.lua

---
+++ GameData/Squishmallows [7941853407]/RobloxEmotes.lua
@@ -0,0 +1,179 @@
+data = {
+    ["GetRandomDance"] = "function: 0x00000000c39b1a1d",
+    ["Emotes"] = {
+        ["Cower"] = {["Id"] = "4940563117", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Disagree"] = {["Id"] = "4841401869", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Point"] = {["Id"] = "507770453", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Greatest"] = {["Id"] = "3338042785", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Bored"] = {["Id"] = "5230599789", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Dance"] = {["Id"] = "507771019", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Get Out"] = {["Id"] = "3333272779", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Shy"] = {["Id"] = "3337978742", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Haha"] = {["Id"] = "3337966527", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Sad"] = {["Id"] = "9069950268", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Agree"] = {["Id"] = "4841397952", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Wave"] = {["Id"] = "507770239", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Curtsy"] = {["Id"] = "4555816777", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Confused"] = {["Id"] = "4940561610", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Laugh"] = {["Id"] = "507770818", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Cheer"] = {["Id"] = "507770677", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Beckon"] = {["Id"] = "5230598276", ["Looped"] = false, ["RequiresGamepass"] = false}
+    },
+    ["Dances"] = {
+        ["Why Don't You Say So"] = {["Id"] = "8150507994", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Caramel"] = {["Id"] = "8150462267", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Hatchback"] = {["Id"] = "8150499979", ["Looped"] = true, ["RequiresGamepass"] = 24682678},
+        ["Two Thousand and Two"] = {["Id"] = "8150420097", ["Looped"] = true, ["RequiresGamepass"] = 24682678},
+        ["Hype"] = {["Id"] = "8150502638", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["The Renegade"] = {["Id"] = "8150505115", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Hey Now"] = {["Id"] = "8150501368", ["Looped"] = true, ["RequiresGamepass"] = 24682678},
+        ["Cat Dance"] = {["Id"] = "8150493865", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Build Up"] = {["Id"] = "8150492993", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Chicken Wing"] = {["Id"] = "8150495253", ["Looped"] = true, ["RequiresGamepass"] = 24682678},
+        ["Cat Dance 2"] = {["Id"] = "8150503677", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Don't Show Up"] = {["Id"] = "8150496783", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Pop Dance"] = {["Id"] = "8150491749", ["Looped"] = true, ["RequiresGamepass"] = 24682678},
+        ["I'm Savage"] = {["Id"] = "8150506670", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Dynamite Dance"] = {["Id"] = "8150498307", ["Looped"] = true, ["RequiresGamepass"] = 24682678}
+    },
+    ["Util"] = {
+        ["CakeThrow"] = {["Id"] = "11328661401", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["CarryAboveHead"] = {["Id"] = "11443451913", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["HoldTree"] = {
+            ["Id"] = "9109165917",
+            ["Looped"] = false,
+            ["Keyframes"] = {["Grab"] = "function: 0x000000001acbe7b5"},
+            ["RequiresGamepass"] = false
+        },
+        ["ShakeTree"] = {["Id"] = "9109191639", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["PickupLookatPutaway"] = {
+            ["Id"] = "8769890653",
+            ["Looped"] = false,
+            ["Keyframes"] = {["Start"] = "function: 0x0000000012782c4d", ["Hold"] = "function: 0x0000000052cc0cbd"},
+            ["RequiresGamepass"] = false
+        },
+        ["FishingCast"] = {
+            ["Id"] = "9037871887",
+            ["Looped"] = false,
+            ["Keyframes"] = {["LastFrame"] = "function: 0x000000000ca3cd65"},
+            ["RequiresGamepass"] = false
+        },
+        ["Tool:PickupPutaway"] = {["Id"] = "7958177247", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Putaway"] = {
+            ["Id"] = "9163211615",
+            ["Looped"] = false,
+            ["Keyframes"] = {["PocketReached"] = "function: 0x000000003dbbf4d5"},
+            ["RequiresGamepass"] = false
+        },
+        ["FireflyCatch"] = {
+            ["Id"] = "10284791748",
+            ["Looped"] = false,
+            ["Keyframes"] = {["Catch"] = "function: 0x000000008bd7cb0d"},
+            ["RequiresGamepass"] = false
+        },
+        ["Tool:Idle"] = {["Id"] = "7958173389", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Lay Down Old"] = {["Id"] = "8044713956", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["RightArmThrow"] = {["Id"] = "11714148880", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["LookatPutaway"] = {
+            ["Id"] = "9163226766",
+            ["Looped"] = false,
+            ["Keyframes"] = {
+                ["Start"] = "function: 0x00000000e71bd115",
+                ["Hold"] = "function: 0x000000006cc903a5",
+                ["PocketReached"] = "function: 0x000000001c0fc4fd"
+            },
+            ["RequiresGamepass"] = false
+        },
+        ["HoldSquishmallow"] = {["Id"] = "8648896567", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["PickupPutaway"] = {["Id"] = "8769773261", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["FishingReel"] = {["Id"] = "9037886678", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Lay Down"] = {["Id"] = "9102796848", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["CoverEyes"] = {["Id"] = "8135425565", ["Looped"] = true, ["RequiresGamepass"] = false}
+    },
+    ["Unfiltered"] = {
+        ["Cower"] = {["Id"] = "4940563117", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Point"] = {["Id"] = "507770453", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Greatest"] = {["Id"] = "3338042785", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["ShakeTree"] = {["Id"] = "9109191639", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Bored"] = {["Id"] = "5230599789", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Dance"] = {["Id"] = "507771019", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["FishingCast"] = {
+            ["Id"] = "9037871887",
+            ["Looped"] = false,
+            ["Keyframes"] = {["LastFrame"] = "function: 0x000000000ca3cd65"},
+            ["RequiresGamepass"] = false
+        },
+        ["I'm Savage"] = {["Id"] = "8150506670", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Putaway"] = {
+            ["Id"] = "9163211615",
+            ["Looped"] = false,
+            ["Keyframes"] = {["PocketReached"] = "function: 0x000000003dbbf4d5"},
+            ["RequiresGamepass"] = false
+        },
+        ["Laugh"] = {["Id"] = "507770818", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["FireflyCatch"] = {
+            ["Id"] = "10284791748",
+            ["Looped"] = false,
+            ["Keyframes"] = {["Catch"] = "function: 0x000000008bd7cb0d"},
+            ["RequiresGamepass"] = false
+        },
+        ["Hatchback"] = {["Id"] = "8150499979", ["Looped"] = true, ["RequiresGamepass"] = 24682678},
+        ["Two Thousand and Two"] = {["Id"] = "8150420097", ["Looped"] = true, ["RequiresGamepass"] = 24682678},
+        ["Sad"] = {["Id"] = "9069950268", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["The Renegade"] = {["Id"] = "8150505115", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Cat Dance"] = {["Id"] = "8150493865", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Build Up"] = {["Id"] = "8150492993", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Haha"] = {["Id"] = "3337966527", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Why Don't You Say So"] = {["Id"] = "8150507994", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Disagree"] = {["Id"] = "4841401869", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["HoldSquishmallow"] = {["Id"] = "8648896567", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["PickupPutaway"] = {["Id"] = "8769773261", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Cheer"] = {["Id"] = "507770677", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Hype"] = {["Id"] = "8150502638", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["HoldTree"] = {
+            ["Id"] = "9109165917",
+            ["Looped"] = false,
+            ["Keyframes"] = {["Grab"] = "function: 0x000000001acbe7b5"},
+            ["RequiresGamepass"] = false
+        },
+        ["Lay Down"] = {["Id"] = "9102796848", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["FishingReel"] = {["Id"] = "9037886678", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["LookatPutaway"] = {
+            ["Id"] = "9163226766",
+            ["Looped"] = false,
+            ["Keyframes"] = {
+                ["Start"] = "function: 0x00000000e71bd115",
+                ["Hold"] = "function: 0x000000006cc903a5",
+                ["PocketReached"] = "function: 0x000000001c0fc4fd"
+            },
+            ["RequiresGamepass"] = false
+        },
+        ["RightArmThrow"] = {["Id"] = "11714148880", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Shy"] = {["Id"] = "3337978742", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Get Out"] = {["Id"] = "3333272779", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Pop Dance"] = {["Id"] = "8150491749", ["Looped"] = true, ["RequiresGamepass"] = 24682678},
+        ["Tool:Idle"] = {["Id"] = "7958173389", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Dynamite Dance"] = {["Id"] = "8150498307", ["Looped"] = true, ["RequiresGamepass"] = 24682678},
+        ["Hey Now"] = {["Id"] = "8150501368", ["Looped"] = true, ["RequiresGamepass"] = 24682678},
+        ["Tool:PickupPutaway"] = {["Id"] = "7958177247", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["PickupLookatPutaway"] = {
+            ["Id"] = "8769890653",
+            ["Looped"] = false,
+            ["Keyframes"] = {["Start"] = "function: 0x0000000012782c4d", ["Hold"] = "function: 0x0000000052cc0cbd"},
+            ["RequiresGamepass"] = false
+        },
+        ["CarryAboveHead"] = {["Id"] = "11443451913", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["CakeThrow"] = {["Id"] = "11328661401", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Caramel"] = {["Id"] = "8150462267", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Confused"] = {["Id"] = "4940561610", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Lay Down Old"] = {["Id"] = "8044713956", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Beckon"] = {["Id"] = "5230598276", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Wave"] = {["Id"] = "507770239", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Chicken Wing"] = {["Id"] = "8150495253", ["Looped"] = true, ["RequiresGamepass"] = 24682678},
+        ["Cat Dance 2"] = {["Id"] = "8150503677", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Curtsy"] = {["Id"] = "4555816777", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["Don't Show Up"] = {["Id"] = "8150496783", ["Looped"] = true, ["RequiresGamepass"] = false},
+        ["Agree"] = {["Id"] = "4841397952", ["Looped"] = false, ["RequiresGamepass"] = false},
+        ["CoverEyes"] = {["Id"] = "8135425565", ["Looped"] = true, ["RequiresGamepass"] = false}
+    }
+}
