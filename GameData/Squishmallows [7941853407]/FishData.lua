419d5cbd63fccd2067e51b8be30ad0afe5c387d8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-17 21:19:05 +1000

    Create FishData.lua

---
+++ GameData/Squishmallows [7941853407]/FishData.lua
@@ -0,0 +1,18 @@
+data = {
+    ["GoldfishRig"] = true,
+    ["SunFish"] = true,
+    ["StrawberryPeacockCichlid"] = true,
+    ["AcadianRedfish"] = true,
+    ["KoiRig"] = true,
+    ["GreatWhiteShark"] = true,
+    ["Carp"] = true,
+    ["Trout"] = true,
+    ["Cod"] = true,
+    ["Eel"] = true,
+    ["Swordfish"] = true,
+    ["PinkSalmon"] = true,
+    ["ElectricYellowFish"] = true,
+    ["SmallFry"] = true,
+    ["BlueShark"] = true,
+    ["Catfish"] = true
+}
