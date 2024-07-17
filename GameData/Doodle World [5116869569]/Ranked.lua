a8857bac8c284543a6f15f0b29ddd78a4560b26f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-17 21:46:05 +1000

    Update Ranked.lua

--- GameData/Doodle World [5116869569]/Ranked.lua
+++ GameData/Doodle World [5116869569]/Ranked.lua
@@ -1,12 +1,12 @@
 data = {
     ["Season"] = {
-        ["Desc"] = "For the first official season, season 1 will have minimal restrictions.",
-        ["Usage"] = "S1R",
+        ["Desc"] = "The first ever Doubles season!",
+        ["Usage"] = "S7",
         ["SpeciesClause"] = true,
-        ["Title"] = "Ranked: Season 1",
-        ["Ban"] = {"Chronos"},
-        ["Ruleset"] = "Casual",
-        ["Format"] = "Singles",
+        ["Title"] = "Ranked: Season 7",
+        ["Ban"] = {"Any Doodle that you can only receive from Roulette."},
+        ["Ruleset"] = "Competitive",
+        ["Format"] = "Doubles",
         ["Level"] = "Level 50",
         ["ItemClause"] = true
     }
