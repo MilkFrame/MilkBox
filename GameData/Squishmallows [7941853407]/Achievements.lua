14a8a2d725d64b760fa5f7468fdec65ee17ace8f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-17 21:18:51 +1000

    Create Achievements.lua

---
+++ GameData/Squishmallows [7941853407]/Achievements.lua
@@ -0,0 +1,233 @@
+--AchievementsList
+data = {
+    {
+        ["Name"] = "Win your first minigame",
+        ["BadgeId"] = 2124862428,
+        ["Id"] = 2124862428,
+        ["RewardAction"] = "function: 0x00000000495c6645",
+        ["QuestId"] = 1,
+        ["Reward"] = {["Type"] = "Squishbux", ["Amount"] = 75}
+    },
+    {
+        ["Name"] = "Complete your first trade",
+        ["BadgeId"] = 2124862424,
+        ["Id"] = 2124862424,
+        ["RewardAction"] = "function: 0x00000000a8687a2d",
+        ["QuestId"] = 2,
+        ["Reward"] = {["Type"] = "Squishbux", ["Amount"] = 50}
+    },
+    {
+        ["Name"] = "Place your first piece of furniture",
+        ["BadgeId"] = 2124862425,
+        ["Id"] = 2124862425,
+        ["RewardAction"] = "function: 0x0000000017a37d95",
+        ["QuestId"] = 3,
+        ["Reward"] = {["Type"] = "Squishbux", ["Amount"] = 50}
+    },
+    {
+        ["Name"] = "Collect 10 Squishmallows",
+        ["BadgeId"] = 2124862431,
+        ["Id"] = 2124862431,
+        ["RewardAction"] = "function: 0x00000000158a74a5",
+        ["QuestId"] = 4,
+        ["Reward"] = {["Type"] = "Squishbux", ["Amount"] = 100}
+    },
+    {
+        ["Name"] = "Collect 50 Squishmallows",
+        ["BadgeId"] = 2124862432,
+        ["Id"] = 2124862432,
+        ["RewardAction"] = "function: 0x000000000afab315",
+        ["QuestId"] = 5,
+        ["Reward"] = {["Type"] = "Squishbux", ["Amount"] = 250}
+    },
+    {
+        ["Name"] = "Collect 100 Squishmallows",
+        ["BadgeId"] = 2124862433,
+        ["Id"] = 2124862433,
+        ["RewardAction"] = "function: 0x000000008d2726fd",
+        ["QuestId"] = 6,
+        ["Reward"] = {["Type"] = "Squishbux", ["Amount"] = 500}
+    },
+    {
+        ["Name"] = "Maintain login streak for 14 days",
+        ["BadgeId"] = 2124862434,
+        ["Id"] = 2124862434,
+        ["RewardAction"] = "function: 0x000000006450ba25",
+        ["QuestId"] = 9,
+        ["Reward"] = {["Type"] = "Squishbux", ["Amount"] = 500}
+    },
+    {
+        ["Name"] = "My First Squishmallows",
+        ["BadgeId"] = 2124862423,
+        ["Id"] = 2124862423,
+        ["RewardAction"] = "function: 0x00000000c7c8bef5",
+        ["QuestId"] = 10,
+        ["Reward"] = {["Type"] = "Squishbux", ["Amount"] = 75}
+    },
+    {
+        ["Name"] = "Welcome!",
+        ["BadgeId"] = 2124862422,
+        ["Id"] = 2124862422,
+        ["RewardAction"] = "function: 0x00000000a1a23785",
+        ["QuestId"] = 11,
+        ["Reward"] = {["Type"] = "Squishbux", ["Amount"] = 50}
+    },
+    {
+        ["Name"] = "Star-Player",
+        ["BadgeId"] = 2124862435,
+        ["Id"] = 2124862435,
+        ["RewardAction"] = "function: 0x00000000116ea0ed",
+        ["QuestId"] = 12,
+        ["Reward"] = {["Type"] = "Squishbux", ["Amount"] = 250}
+    },
+    {
+        ["Name"] = "Super Fan!",
+        ["BadgeId"] = 2124862436,
+        ["Id"] = 2124862436,
+        ["RewardAction"] = "function: 0x00000000829b0c55",
+        ["QuestId"] = 13,
+        ["Reward"] = {["Type"] = "Squishbux", ["Amount"] = 500}
+    },
+    {
+        ["Name"] = "Good Neighbor!",
+        ["BadgeId"] = 2124862426,
+        ["Id"] = 2124862426,
+        ["RewardAction"] = "function: 0x00000000ac8c22e5",
+        ["QuestId"] = 14,
+        ["Reward"] = {["Type"] = "Squishbux", ["Amount"] = 50}
+    },
+    {
+        ["Name"] = "Level Up!",
+        ["BadgeId"] = 2124862429,
+        ["Id"] = 2124862429,
+        ["RewardAction"] = "function: 0x000000006fda9145",
+        ["QuestId"] = 15,
+        ["Reward"] = {["Type"] = "Squishbux", ["Amount"] = 200}
+    },
+    {
+        ["Name"] = "Mega 'Mallow!",
+        ["BadgeId"] = 2124862430,
+        ["Id"] = 2124862430,
+        ["RewardAction"] = "function: 0x000000003d30e895",
+        ["QuestId"] = 16,
+        ["Reward"] = {["Type"] = "Squishbux", ["Amount"] = 500}
+    },
+    {
+        ["Name"] = "Welcome to the Club!",
+        ["BadgeId"] = 2124862438,
+        ["Id"] = 2124862438,
+        ["RewardAction"] = "function: 0x00000000f328cfa5",
+        ["QuestId"] = 17,
+        ["Reward"] = {["Type"] = "Squishbux", ["Amount"] = 15}
+    },
+    ["GetAchievementFromId"] = "function: 0x000000007f0145bd"
+}
+AchievementConditions = {
+    {
+        ["RepeatCount"] = 1,
+        ["Type"] = 1,
+        ["AchievementId"] = 1,
+        ["ConditionType"] = "number",
+        ["ConnectorData"] = "function: 0x00000000a054d3f5",
+        ["Condition"] = "function: 0x00000000f140cc1d"
+    },
+    {
+        ["RepeatCount"] = 1,
+        ["Type"] = 1,
+        ["AchievementId"] = 2,
+        ["ConditionType"] = "number",
+        ["ConnectorData"] = "function: 0x00000000823ca885",
+        ["Condition"] = "function: 0x0000000033e935ed"
+    },
+    {
+        ["RepeatCount"] = 1,
+        ["Type"] = 1,
+        ["AchievementId"] = 3,
+        ["ConditionType"] = "number",
+        ["ConnectorData"] = "function: 0x0000000064228155",
+        ["Condition"] = "function: 0x00000000df3d428d"
+    },
+    {
+        ["RepeatCount"] = 10,
+        ["Type"] = 1,
+        ["AchievementId"] = 4,
+        ["ConditionType"] = "number",
+        ["ConnectorData"] = "function: 0x000000006f826705",
+        ["Condition"] = "function: 0x00000000dd9948d5"
+    },
+    {
+        ["RepeatCount"] = 50,
+        ["Type"] = 1,
+        ["AchievementId"] = 5,
+        ["ConditionType"] = "number",
+        ["ConnectorData"] = "function: 0x00000000ec2979ad",
+        ["Condition"] = "function: 0x00000000832244f5"
+    },
+    {
+        ["RepeatCount"] = 100,
+        ["Type"] = 1,
+        ["AchievementId"] = 6,
+        ["ConditionType"] = "number",
+        ["ConnectorData"] = "function: 0x00000000b8be4d8d",
+        ["Condition"] = "function: 0x00000000dda31675"
+    },
+    {
+        ["RepeatCount"] = 10,
+        ["Type"] = 1,
+        ["AchievementId"] = 7,
+        ["ConditionType"] = "number",
+        ["ConnectorData"] = "function: 0x00000000199e343d",
+        ["Condition"] = "function: 0x00000000b6e3b965"
+    },
+    {
+        ["RepeatCount"] = 100,
+        ["Type"] = 1,
+        ["AchievementId"] = 8,
+        ["ConditionType"] = "number",
+        ["ConnectorData"] = "function: 0x00000000cfb73f0d",
+        ["Condition"] = "function: 0x00000000a37f88fd"
+    },
+    {
+        ["RepeatCount"] = 14,
+        ["Type"] = 1,
+        ["AchievementId"] = 9,
+        ["ConditionType"] = "number",
+        ["ConnectorData"] = "function: 0x00000000f2031425",
+        ["Condition"] = "function: 0x000000009e7b6585"
+    },
+    {
+        ["RepeatCount"] = 1,
+        ["Type"] = 1,
+        ["AchievementId"] = 10,
+        ["ConditionType"] = "number",
+        ["ConnectorData"] = "function: 0x00000000393616ed",
+        ["Condition"] = "function: 0x00000000e9c38a55"
+    },
+    {
+        ["Type"] = 1,
+        ["AchievementId"] = 11,
+        ["ConditionType"] = "boolean",
+        ["ConnectorData"] = "function: 0x00000000bba870e5",
+        ["Condition"] = "function: 0x0000000064b37835"
+    },
+    {
+        ["RepeatCount"] = 60,
+        ["Type"] = 1,
+        ["AchievementId"] = 12,
+        ["ConditionType"] = "number",
+        ["ConnectorData"] = "function: 0x00000000069a7f45",
+        ["Condition"] = "function: 0x00000000bb5a8b75"
+    },
+    {
+        ["RepeatCount"] = 600,
+        ["Type"] = 1,
+        ["AchievementId"] = 13,
+        ["ConditionType"] = "number",
+        ["ConnectorData"] = "function: 0x0000000089236405",
+        ["Condition"] = "function: 0x0000000039f7586d"
+    },
+    {},
+    {},
+    {},
+    {}
+}
