38eb95efed3f92bd6069707bc028342381539da4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-22 22:50:59 +1000

    Create Emotes.lua

---
+++ GameData/Murder Mystery 2 [142823291]/Emotes.lua
@@ -0,0 +1,109 @@
+data = {
+    ["dab"] = {
+        ["Image"] = "rbxassetid://2474247481",
+        ["Name"] = "Dab",
+        ["Command"] = "dab",
+        ["Rarity"] = "Common",
+        ["Price"] = 700,
+        ["AnimationID"] = 2445521505
+    },
+    ["zen"] = {
+        ["Rarity"] = "Rare",
+        ["Image"] = "http://www.roblox.com/asset/?id=198273518",
+        ["Name"] = "Zen",
+        ["Price"] = 800,
+        ["Command"] = "zen",
+        ["Gems"] = true,
+        ["Loop"] = true,
+        ["AnimationID"] = 2431812646
+    },
+    ["dance2"] = {
+        ["Image"] = "rbxassetid://2435882711",
+        ["Name"] = "Dance 2",
+        ["Command"] = "dance2",
+        ["Price"] = "NotForSale",
+        ["Loop"] = true
+    },
+    ["sit"] = {
+        ["Image"] = "http://www.roblox.com/asset/?id=198215446",
+        ["Name"] = "Sit",
+        ["Rarity"] = "Common",
+        ["Command"] = "sit2",
+        ["Price"] = 200,
+        ["Loop"] = true,
+        ["AnimationID"] = 2431845940
+    },
+    ["floss"] = {
+        ["Rarity"] = "Rare",
+        ["Image"] = "rbxassetid://2474247282",
+        ["Name"] = "Floss",
+        ["Price"] = 750,
+        ["Command"] = "floss",
+        ["Gems"] = true,
+        ["Loop"] = true,
+        ["AnimationID"] = 2452938820
+    },
+    ["dance3"] = {
+        ["Image"] = "rbxassetid://2435882719",
+        ["Name"] = "Dance 3",
+        ["Command"] = "dance3",
+        ["Price"] = "NotForSale",
+        ["Loop"] = true
+    },
+    ["Back"] = {["Price"] = "NotForSale", ["Image"] = ""},
+    ["headless"] = {
+        ["Image"] = "rbxassetid://2513709230",
+        ["Name"] = "Headless",
+        ["Command"] = "headless",
+        ["Price"] = "NotForSale",
+        ["AnimationID"] = 2513664073
+    },
+    ["wave"] = {
+        ["Command"] = "wave",
+        ["Image"] = "rbxassetid://2435882728",
+        ["Name"] = "Wave",
+        ["Price"] = "NotForSale"
+    },
+    ["dance1"] = {
+        ["Image"] = "rbxassetid://2435882712",
+        ["Name"] = "Dance 1",
+        ["Command"] = "dance",
+        ["Price"] = "NotForSale",
+        ["Loop"] = true
+    },
+    ["point"] = {
+        ["Command"] = "point",
+        ["Image"] = "rbxassetid://2435938718",
+        ["Name"] = "Point",
+        ["Price"] = "NotForSale"
+    },
+    ["laugh"] = {
+        ["Command"] = "laugh",
+        ["Image"] = "rbxassetid://2435995264",
+        ["Name"] = "Laugh",
+        ["Price"] = "NotForSale"
+    },
+    ["cheer"] = {
+        ["Command"] = "cheer",
+        ["Image"] = "rbxassetid://2435882701",
+        ["Name"] = "Cheer",
+        ["Price"] = "NotForSale"
+    },
+    ["ninja"] = {
+        ["Image"] = "http://www.roblox.com/asset/?id=198273910",
+        ["Name"] = "Ninja Rest",
+        ["Rarity"] = "Common",
+        ["Command"] = "ninja",
+        ["Price"] = 650,
+        ["Loop"] = true,
+        ["AnimationID"] = 2431864798
+    },
+    ["zombie"] = {
+        ["Image"] = "rbxassetid://2513709229",
+        ["Name"] = "Zombie",
+        ["Command"] = "zombie",
+        ["Price"] = "NotForSale",
+        ["Loop"] = true,
+        ["AnimationID"] = 2513692312
+    }
+}
