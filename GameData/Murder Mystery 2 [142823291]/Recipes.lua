702c6c10758500226f8f48ab3bc18cfa714431ff
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-22 22:50:10 +1000

    Create Recipes.lua

---
+++ GameData/Murder Mystery 2 [142823291]/Recipes.lua
@@ -0,0 +1,44 @@
+data = {
+    ["RandomRareWeapon"] = {
+        ["Function"] = "RandomCraft",
+        ["Image"] = "rbxassetid://582222760",
+        ["Name"] = "Random Rare Weapon",
+        ["SortPriority"] = 1100,
+        ["Materials"] = {["UncommonShards"] = 12},
+        ["Rarity"] = "Rare"
+    },
+    ["RandomLegendaryWeapon"] = {
+        ["Function"] = "RandomCraft",
+        ["Image"] = "rbxassetid://582222766",
+        ["Name"] = "Random Legendary Weapon",
+        ["SortPriority"] = 1200,
+        ["Materials"] = {["RareShards"] = 12},
+        ["Rarity"] = "Legendary"
+    },
+    ["RandomUncommonWeapon"] = {
+        ["Function"] = "RandomCraft",
+        ["Image"] = "rbxassetid://582222761",
+        ["Name"] = "Random Uncommon Weapon",
+        ["SortPriority"] = 1000,
+        ["Materials"] = {["CommonShards"] = 12},
+        ["Rarity"] = "Uncommon"
+    },
+    ["TheSeer"] = {
+        ["SortPriority"] = 1300,
+        ["Image"] = "http://www.roblox.com/asset/?id=198459100",
+        ["Name"] = "Seer",
+        ["RewardItem"] = "TheSeer",
+        ["Materials"] = {["LegendaryShards"] = 20},
+        ["RewardAmount"] = 1,
+        ["RewardType"] = "Weapons",
+        ["Rarity"] = "Godly"
+    },
+    ["PaintedSeer"] = {
+        ["Function"] = "PaintedSeer",
+        ["Image"] = "rbxassetid://3184179319",
+        ["Name"] = "Random Painted Seer",
+        ["SortPriority"] = 1400,
+        ["Materials"] = {["GodlyMetal"] = 10, ["GodlyShards"] = 10},
+        ["Rarity"] = "Godly"
+    }
+}
