8a1847cdb9df237a2b2cae851624febf1fe64bed
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-22 22:47:52 +1000

    Create EloRankInfo.lua

---
+++ GameData/Murder Mystery 2 [142823291]/EloRankInfo.lua
@@ -0,0 +1,32 @@
+data = {
+    ["RankOrder"] = {
+        ["None"] = 0
+        ["Bronze"] = 1,
+        ["Silver"] = 2,
+        ["SilverPlus"] = 3,
+        ["Gold"] = 4,
+        ["Diamond"] = 6,
+        ["GoldPlus"] = 5,
+        ["DiamondPlus"] = 7,
+    },
+    ["MaxElo"] = 1600,
+    ["RankIcons"] = {
+        ["None"] = "http://www.roblox.com/asset/?id=4801598442"
+        ["Bronze"] = "http://www.roblox.com/asset/?id=4801543290",
+        ["Silver"] = "http://www.roblox.com/asset/?id=4801543932",
+        ["SilverPlus"] = "http://www.roblox.com/asset/?id=4802472365",
+        ["Gold"] = "http://www.roblox.com/asset/?id=4801544128",
+        ["GoldPlus"] = "http://www.roblox.com/asset/?id=4802472150",
+        ["Diamond"] = "http://www.roblox.com/asset/?id=4801544484",
+        ["DiamondPlus"] = "http://www.roblox.com/asset/?id=4809009917",
+    },
+    ["MaxRank"] = "DiamondPlus",
+    ["EloRanks"] = {
+        {["Elo"] = 1150, ["RankName"] = "Bronze"},
+        {["Elo"] = 1250, ["RankName"] = "Silver"},
+        {["Elo"] = 1325, ["RankName"] = "SilverPlus"},
+        {["Elo"] = 1400, ["RankName"] = "Gold"},
+        {["Elo"] = 1500, ["RankName"] = "GoldPlus"},
+        {["Elo"] = 1600, ["RankName"] = "Diamond"}
+    }
+}
