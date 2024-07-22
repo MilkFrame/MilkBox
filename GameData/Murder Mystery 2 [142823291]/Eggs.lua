dc3ab1b2fa98d29d0a1ff30a8bf938d9575518da
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-22 22:50:47 +1000

    Create Eggs.lua

---
+++ GameData/Murder Mystery 2 [142823291]/Eggs.lua
@@ -0,0 +1,12 @@
+data = {
+    ["Egg"] = {
+        ["Rarity"] = "Common",
+        ["Image"] = "rbxassetid://3149859416",
+        ["Name"] = "Common Egg",
+        ["GodlyTag"] = "Fire",
+        ["Released"] = 0,
+        ["BoxType"] = "Pets",
+        ["Contents"] = {"Cat", "Dog", "Bunny", "Pig", "Fox", "Bear", "Bat"},
+        ["RarityChances"] = {["Rare"] = 10, ["Legendary"] = 5, ["Uncommon"] = 25, ["Common"] = 60}
+    }
+}
