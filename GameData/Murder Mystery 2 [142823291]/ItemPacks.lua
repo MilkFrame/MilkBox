bec69a971cd3e71a1d235c6776cd2bd7803466f1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-22 22:51:12 +1000

    Create ItemPacks.lua

---
+++ GameData/Murder Mystery 2 [142823291]/ItemPacks.lua
@@ -0,0 +1,96 @@
+data = {
+    ["13502690"] = {{["Type"] = "Weapons", ["ItemName"] = "Cookieblade"}},
+    ["131048950"] = {{["Type"] = "Weapons", ["ItemName"] = "Sakura_K"}},
+    ["156618096"] = {{["Type"] = "Weapons", ["ItemName"] = "Rainbow_K"}},
+    ["273009597"] = {{["Type"] = "Weapons", ["ItemName"] = "Darkshot"}},
+    ["850003963"] = {{["Type"] = "Weapons", ["ItemName"] = "Pearl_K"}},
+    ["8456434"] = {{["Type"] = "Weapons", ["ItemName"] = "Bioblade"}},
+    ["767127317"] = {
+        {["Type"] = "Weapons", ["ItemName"] = "FlowerwoodKnife"},
+        {["Type"] = "Weapons", ["ItemName"] = "FlowerwoodGun"},
+        {["Type"] = "Effects", ["ItemName"] = "Flowerwood"}
+    },
+    ["273009675"] = {{["Type"] = "Weapons", ["ItemName"] = "Darksword"}},
+    ["55292987"] = {
+        {["Type"] = "Weapons", ["ItemName"] = "Plasmablade"},
+        {["Type"] = "Weapons", ["ItemName"] = "Plasmabeam"},
+        {["Type"] = "Effects", ["ItemName"] = "PlasmaEffect"}
+    },
+    ["768389355"] = {{["Type"] = "Weapons", ["ItemName"] = "FlowerwoodGun"}},
+    ["12448341"] = {
+        {["Type"] = "Weapons", ["ItemName"] = "VampiresEdge"},
+        {["Type"] = "Effects", ["ItemName"] = "Vampire2020"}
+    },
+    ["850293409"] = {{["Type"] = "Weapons", ["ItemName"] = "Pearl_G"}},
+    ["200074115"] = {
+        {["Type"] = "Weapons", ["ItemName"] = "Waves_K"},
+        {["Type"] = "Weapons", ["ItemName"] = "Ocean_G"},
+        {["Type"] = "Effects", ["ItemName"] = "Water2023"}
+    },
+    ["16462794"] = {{["Type"] = "Weapons", ["ItemName"] = "Eggblade"}},
+    ["112956652"] = {{["ItemName"] = "IceHammer", ["Type"] = "Weapons", ["Evo"] = true}},
+    ["200074174"] = {{["Type"] = "Weapons", ["ItemName"] = "Waves_K"}},
+    ["26304644"] = {{["Type"] = "Weapons", ["ItemName"] = "Iceflake"}},
+    ["21348436"] = {{["Type"] = "Weapons", ["ItemName"] = "Nebula"}},
+    ["273009641"] = {
+        {["Type"] = "Weapons", ["ItemName"] = "Darksword"},
+        {["Type"] = "Weapons", ["ItemName"] = "Darkshot"},
+        {["Type"] = "Effects", ["ItemName"] = "Darkswirl"}
+    },
+    ["55292828"] = {{["Type"] = "Weapons", ["ItemName"] = "Plasmabeam"}},
+    ["7795820"] = {
+        {["Type"] = "Weapons", ["ItemName"] = "Frostbite"},
+        {["Type"] = "Effects", ["ItemName"] = "FrostbiteEffect"},
+        {["Type"] = "Pets", ["ItemName"] = "Icey"}
+    },
+    ["658667213"] = {{["Type"] = "Weapons", ["ItemName"] = "Turkey2023"}},
+    ["769140200"] = {{["Type"] = "Weapons", ["ItemName"] = "FlowerwoodKnife"}},
+    ["26304640"] = {
+        {["Type"] = "Weapons", ["ItemName"] = "Iceflake"},
+        {["Type"] = "Weapons", ["ItemName"] = "Icebeam"},
+        {["Type"] = "Effects", ["ItemName"] = "IceMagic"}
+    },
+    ["850387075"] = {
+        {["Type"] = "Weapons", ["ItemName"] = "Pearl_K"},
+        {["Type"] = "Weapons", ["ItemName"] = "Pearl_G"},
+        {["Type"] = "Effects", ["ItemName"] = "Glisten2024"}
+    },
+    ["13308756"] = {{["Type"] = "Weapons", ["ItemName"] = "Peppermint"}},
+    ["674830453"] = {{["ItemName"] = "Gingerscythe", ["Type"] = "Weapons", ["Evo"] = true}},
+    ["200073999"] = {{["Type"] = "Weapons", ["ItemName"] = "Ocean_G"}},
+    ["658376309"] = {
+        {["Type"] = "Weapons", ["ItemName"] = "Latte_K_2023"},
+        {["Type"] = "Weapons", ["ItemName"] = "Latte_G_2023"}
+    },
+    ["24102758"] = {{["ItemName"] = "Reaver", ["Type"] = "Weapons", ["Evo"] = true}},
+    ["156618061"] = {
+        {["Type"] = "Weapons", ["ItemName"] = "Rainbow_K"},
+        {["Type"] = "Weapons", ["ItemName"] = "Rainbow_G"},
+        {["Type"] = "Effects", ["ItemName"] = "RainbowEffect"}
+    },
+    ["156618021"] = {{["Type"] = "Weapons", ["ItemName"] = "Rainbow_G"}},
+    ["26304638"] = {{["Type"] = "Weapons", ["ItemName"] = "Icebeam"}},
+    ["93780280"] = {{["Type"] = "Weapons", ["ItemName"] = "Phantom2022"}},
+    ["5593012"] = {{["Type"] = "Weapons", ["ItemName"] = "Icewing"}},
+    ["10747452"] = {{["Type"] = "Weapons", ["ItemName"] = "Prismatic"}},
+    ["93780371"] = {
+        {["Type"] = "Weapons", ["ItemName"] = "Phantom2022"},
+        {["Type"] = "Weapons", ["ItemName"] = "Spectre2022"},
+        {["Type"] = "Effects", ["ItemName"] = "PhantomEffect2022"}
+    },
+    ["15066851"] = {{["Type"] = "Weapons", ["ItemName"] = "Heartblade"}},
+    ["7435036"] = {
+        {["Type"] = "Weapons", ["ItemName"] = "Ghostblade"},
+        {["Type"] = "Effects", ["ItemName"] = "Ethereal"},
+        {["Type"] = "Pets", ["ItemName"] = "Ghosty"}
+    },
+    ["5299081"] = {{["Type"] = "Weapons", ["ItemName"] = "Scythe"}},
+    ["93780323"] = {{["Type"] = "Weapons", ["ItemName"] = "Spectre2022"}},
+    ["131048887"] = {{["Type"] = "Weapons", ["ItemName"] = "Blossom_G"}},
+    ["55292879"] = {{["Type"] = "Weapons", ["ItemName"] = "Plasmablade"}},
+    ["131048794"] = {
+        {["Type"] = "Weapons", ["ItemName"] = "Sakura_K"},
+        {["Type"] = "Weapons", ["ItemName"] = "Blossom_G"},
+        {["Type"] = "Effects", ["ItemName"] = "Petals"}
+    }
+}
