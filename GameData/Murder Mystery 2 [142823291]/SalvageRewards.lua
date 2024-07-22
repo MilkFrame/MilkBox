e75c8ea22db5e4916a2039c0300cebd2c2d20828
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-22 22:52:57 +1000

    Create SalvageRewards.lua

---
+++ GameData/Murder Mystery 2 [142823291]/SalvageRewards.lua
@@ -0,0 +1,171 @@
+data = {
+    ["Saw"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {32, 32}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["GingerLuger"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {60, 60}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["IceDragon"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {16, 16}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Luger"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {65, 65}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Gingerblade"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {20, 20}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["RedLuger"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {230, 230}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["TheSeer"] = {
+        ["Rewards"] = {["GodlyMetal"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {1, 1}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Handsaw"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {255, 255}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Uncommon"] = {
+        ["Rewards"] = {["UncommonShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {2, 2}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Shark"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {27, 27}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Nightblade"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {3, 3}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["BattleAxe2"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {90, 90}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Hallow"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {75, 75}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Amerilaser"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {72, 72}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Sugar"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {255, 255}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Xbox"] = {},
+    ["WintersEdge"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {22, 22}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["GreenLuger"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {230, 230}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Rare"] = {
+        ["Rewards"] = {["RareShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {2, 2}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Snowflake"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {10, 10}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["HallowsBlade"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {10, 10}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Clockwork"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {135, 135}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Frostsaber"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {11, 11}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Xmas"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {38, 38}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Virtual"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {48, 48}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["IceShard"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {12, 12}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["BattleAxe"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {8, 8}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Pumpking"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {22, 22}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Deathshard"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {25, 25}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Spider"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {110, 110}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Common"] = {
+        ["Rewards"] = {["CommonShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {2, 2}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Fang"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {28, 28}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Candy"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {235, 235}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Laser"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {30, 30}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Slasher"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {35, 35}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["AmericaSword"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {72, 72}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Legendary"] = {
+        ["Rewards"] = {["LegendaryShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {2, 2}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Eternal"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {160, 160}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Blaster"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {70, 70}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Heat"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {55, 55}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Tides"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {55, 55}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Pixel"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {105, 105}}},
+        ["CountChance"] = {100, 0, 0}
+    },
+    ["Chill"] = {
+        ["Rewards"] = {["GodlyShards"] = {["Rarity"] = {100, 0, 0}, ["Amount"] = {235, 235}}},
+        ["CountChance"] = {100, 0, 0}
+    }
+}
