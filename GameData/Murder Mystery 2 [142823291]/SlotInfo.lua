9216a59c52c678c955800ea9bd769297f1f4b43f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-22 22:51:23 +1000

    Create SlotInfo.lua

---
+++ GameData/Murder Mystery 2 [142823291]/SlotInfo.lua
@@ -0,0 +1,7 @@
+data = {
+    ["Toys"] = {["Unequip"] = true, ["Prices"] = {0, 25, 100, 300}, ["Max"] = 4},
+    ["Radios"] = {["Max"] = 1, ["Unequip"] = true},
+    ["Pets"] = {["Max"] = 1, ["Unequip"] = true},
+    ["Perks"] = {["Max"] = 1},
+    ["Effects"] = {["Max"] = 1, ["Unequip"] = true}
+}
