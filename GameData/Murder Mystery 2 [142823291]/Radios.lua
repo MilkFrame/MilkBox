bbb80964786979830a2205fe37911f4dd9969636
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-22 22:49:56 +1000

    Create Radios.lua

---
+++ GameData/Murder Mystery 2 [142823291]/Radios.lua
@@ -0,0 +1,352 @@
+data = {
+    ["BluesteelRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0, ["Z"] = 0.8},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Bluesteel",
+        ["Rarity"] = "Common",
+        ["Price"] = 780,
+        ["ItemID"] = 426038132,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "426038132"
+    },
+    ["ElderwoodRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Elderwood",
+        ["ItemID"] = 4340193489,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "4340193489"
+    },
+    ["GingerbreadRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Gingerbread",
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Price"] = "NotForSale",
+        ["ItemID"] = 585672136,
+        ["ChristmasDescription"] = "Radio",
+        ["Image"] = "585672136"
+    },
+    ["CandyCane2018"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Candy Cane '18",
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Price"] = "NotForSale",
+        ["ItemID"] = 2672403759,
+        ["ChristmasDescription"] = "Radio",
+        ["Image"] = "2672403759"
+    },
+    ["Omega"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Rarity"] = "Uncommon",
+        ["Price"] = 400,
+        ["Gems"] = true,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "426037069",
+        ["Name"] = "Omega",
+        ["ItemID"] = 426037069
+    },
+    ["Elite"] = {
+        ["Offset"] = {["Y"] = 0.1, ["X"] = 0.1, ["Z"] = 1},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Elite",
+        ["Price"] = "Elite",
+        ["ItemID"] = 340601210,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.6981317007977318},
+        ["Image"] = "340601210"
+    },
+    ["RedSparkleRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Rarity"] = "Legendary",
+        ["Price"] = 1000,
+        ["Gems"] = true,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "426034746",
+        ["Name"] = "Red Sparkletime",
+        ["ItemID"] = 426034746
+    },
+    ["Gold"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Golden",
+        ["Rarity"] = "Common",
+        ["Price"] = 3000,
+        ["ItemID"] = 336413900,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "336413900"
+    },
+    ["CandyRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0, ["Z"] = 0.8},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Candy Radio",
+        ["ItemID"] = 4324197725,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "4213325191"
+    },
+    ["ReaverRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Reaver",
+        ["ItemID"] = 7805814852,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "7805814852"
+    },
+    ["SnowRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Snowy",
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Price"] = "NotForSale",
+        ["ItemID"] = 585672503,
+        ["ChristmasDescription"] = "Radio",
+        ["Image"] = "585672503"
+    },
+    ["CamoRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0, ["Z"] = 0.8},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Camo",
+        ["Rarity"] = "Common",
+        ["Price"] = 500,
+        ["ItemID"] = 426037948,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "426037948"
+    },
+    ["Flaming"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Rarity"] = "Rare",
+        ["Price"] = 900,
+        ["Gems"] = true,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "426034894",
+        ["Name"] = "Flaming",
+        ["ItemID"] = 426034894
+    },
+    ["OverseerRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Rarity"] = "Uncommon",
+        ["Price"] = 300,
+        ["Gems"] = true,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "426036356",
+        ["Name"] = "Overseer",
+        ["ItemID"] = 426036356
+    },
+    ["GreenCane"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Green Candy Cane",
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Price"] = "NotForSale",
+        ["ItemID"] = 1269026737,
+        ["ChristmasDescription"] = "Radio",
+        ["Image"] = "1269026737"
+    },
+    ["FadeRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Rarity"] = "Uncommon",
+        ["Price"] = 400,
+        ["Gems"] = true,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "426035979",
+        ["Name"] = "Fade",
+        ["ItemID"] = 426035979
+    },
+    ["PinkSparkleRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Rarity"] = "Legendary",
+        ["Price"] = 1000,
+        ["Gems"] = true,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "426036727",
+        ["Name"] = "Pink Sparkle Time",
+        ["ItemID"] = 426036727
+    },
+    ["ChristmasRadio2019"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Minty",
+        ["ItemID"] = 4528279118,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "4528279118"
+    },
+    ["JackRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Jack-o-Radio",
+        ["ItemID"] = 4217639269,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "4217639269"
+    },
+    ["RainbowRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Rarity"] = "Rare",
+        ["Price"] = 800,
+        ["Gems"] = true,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "426036850",
+        ["Name"] = "Rainbow",
+        ["ItemID"] = 426036850
+    },
+    ["DiamondRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Rarity"] = "Legendary",
+        ["Price"] = 2500,
+        ["Gems"] = true,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "426036523",
+        ["Name"] = "Diamond",
+        ["ItemID"] = 426036523
+    },
+    ["Hallowradio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Hallowradio",
+        ["ItemID"] = 5929507735,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "5929507735"
+    },
+    ["BlueCane"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Blue Candy Cane",
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Price"] = "NotForSale",
+        ["ItemID"] = 1269026952,
+        ["ChristmasDescription"] = "Radio",
+        ["Image"] = "1269026952"
+    },
+    ["Coal"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Coal",
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Price"] = "NotForSale",
+        ["ItemID"] = 1269027894,
+        ["ChristmasDescription"] = "Radio",
+        ["Image"] = "1269027894"
+    },
+    ["BlueCandyCane2018"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Blue Candy Cane '18",
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Price"] = "NotForSale",
+        ["ItemID"] = 2674776628,
+        ["ChristmasDescription"] = "Radio",
+        ["Image"] = "2674776628"
+    },
+    ["Tree"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Tree",
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Price"] = "NotForSale",
+        ["ItemID"] = 2674774833,
+        ["ChristmasDescription"] = "Radio",
+        ["Image"] = "2674774833"
+    },
+    ["Ice"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Ice",
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Price"] = "NotForSale",
+        ["ItemID"] = 1269027626,
+        ["ChristmasDescription"] = "Radio",
+        ["Image"] = "1269027626"
+    },
+    ["GhostRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Ghost",
+        ["ItemID"] = 5873199043,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "5873199043"
+    },
+    ["CandyCaneRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Candy Cane",
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Price"] = "NotForSale",
+        ["ItemID"] = 585671667,
+        ["ChristmasDescription"] = "Radio",
+        ["Image"] = "585671667"
+    },
+    ["ChristmasRadio2020"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0, ["Z"] = 0.8},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Iceplayer",
+        ["ItemID"] = 6125721029,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "6121716902"
+    },
+    ["Default"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0, ["Z"] = 0.8},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Regular",
+        ["Rarity"] = "Common",
+        ["ItemID"] = 336413959,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "336413959"
+    },
+    ["Boombastic"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Boombastic",
+        ["Rarity"] = "Common",
+        ["Price"] = 900,
+        ["ItemID"] = 426037145,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "426037145"
+    },
+    ["Dubstep"] = {
+        ["Offset"] = {["Y"] = 0.1, ["X"] = 0.1, ["Z"] = 1},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Dubstep",
+        ["Rarity"] = "Common",
+        ["Price"] = 300,
+        ["ItemID"] = 340586338,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.6981317007977318},
+        ["Image"] = "340586338"
+    },
+    ["GreenSparkleRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Rarity"] = "Legendary",
+        ["Price"] = 1000,
+        ["Gems"] = true,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "426036954",
+        ["Name"] = "Green Sparkle Time",
+        ["ItemID"] = 426036954
+    },
+    ["ChristmasRadio2021"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0, ["Z"] = 0.8},
+        ["Type"] = "Radio Skin",
+        ["Name"] = "Swirly",
+        ["ItemID"] = 8304827967,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "8304827967"
+    },
+    ["BlueSparkleRadio"] = {
+        ["Offset"] = {["Y"] = 0, ["X"] = 0.1, ["Z"] = 0.99},
+        ["Type"] = "Radio Skin",
+        ["Rarity"] = "Legendary",
+        ["Price"] = 1000,
+        ["Gems"] = true,
+        ["Rotation"] = {["Y"] = 3.141592653589793, ["X"] = 0, ["Z"] = 0.7853981633974483},
+        ["Image"] = "426035785",
+        ["Name"] = "Blue Sparkle Time",
+        ["ItemID"] = 426035785
+    }
+}
