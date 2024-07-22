92b18da981106e824d3f45406f471910619a8805
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-22 22:51:36 +1000

    Create Toys.lua

---
+++ GameData/Murder Mystery 2 [142823291]/Toys.lua
@@ -0,0 +1,225 @@
+data = {
+    ["PumpkinBucket"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Treat Bucket",
+        ["ItemID"] = 4212804497,
+        ["Image"] = "4212804497",
+        ["Description"] = ""
+    },
+    ["WaterBalloon2024"] = {
+        ["Image"] = "rbxassetid://18335937543",
+        ["Type"] = "Toy",
+        ["Name"] = "Water Balloon",
+        ["Description"] = ""
+    },
+    ["SandBucket2024"] = {
+        ["Image"] = "rbxassetid://18335937779",
+        ["Type"] = "Toy",
+        ["Name"] = "Sand Bucket",
+        ["Description"] = ""
+    },
+    ["Coconut2024"] = {
+        ["Image"] = "rbxassetid://18335937971",
+        ["Type"] = "Toy",
+        ["Name"] = "Coconut",
+        ["Description"] = ""
+    },
+    ["SnowballToy2020"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Snowball",
+        ["ItemID"] = 6124068212,
+        ["Image"] = "6123754554",
+        ["Description"] = ""
+    },
+    ["CandyToy2021"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Candy",
+        ["ItemID"] = 7808227101,
+        ["Image"] = "7808227101",
+        ["Description"] = ""
+    },
+    ["BeachBall2023"] = {
+        ["Image"] = "rbxassetid://13932988534",
+        ["Type"] = "Toy",
+        ["Name"] = "Beach Ball",
+        ["Description"] = ""
+    },
+    ["Pie2023"] = {["Image"] = "rbxassetid://15415343872", ["Type"] = "Toy", ["Name"] = "Pie", ["Description"] = ""},
+    ["CommonEgg2024"] = {
+        ["Image"] = "rbxassetid://16959887230",
+        ["Type"] = "Toy",
+        ["Name"] = "Egg Toy",
+        ["Description"] = ""
+    },
+    ["RareEgg2024"] = {
+        ["Image"] = "rbxassetid://16959891184",
+        ["Type"] = "Toy",
+        ["Name"] = "Rare Egg Toy",
+        ["Description"] = ""
+    },
+    ["SprayPaint"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Spray Paint",
+        ["Rarity"] = "Rare",
+        ["Price"] = 750,
+        ["Gems"] = true,
+        ["Image"] = "342185593",
+        ["Description"] = "Spray paint your favorite decals on the walls."
+    },
+    ["Cookie2023"] = {
+        ["Image"] = "rbxassetid://15653924406",
+        ["Type"] = "Toy",
+        ["Name"] = "Cookie",
+        ["Description"] = ""
+    },
+    ["GGSign"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "GG Sign",
+        ["Rarity"] = "Common",
+        ["ItemID"] = 4806328826,
+        ["Image"] = "4806328826",
+        ["Description"] = ""
+    },
+    ["Bunny2024"] = {
+        ["Image"] = "rbxassetid://16960933548",
+        ["Type"] = "Toy",
+        ["Name"] = "Bunny Toy",
+        ["Description"] = ""
+    },
+    ["EggToy2023"] = {
+        ["Image"] = "http://www.roblox.com/asset/?id=12966771924",
+        ["Type"] = "Toy",
+        ["Name"] = "Egg Toy",
+        ["Description"] = ""
+    },
+    ["FakeBomb"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Prank Bomb",
+        ["Rarity"] = "Common",
+        ["Price"] = 1200,
+        ["ItemID"] = 2477932633,
+        ["Image"] = "342187115",
+        ["Description"] = "Prank your friends with this confetti bomb!"
+    },
+    ["Pumpkin2023"] = {
+        ["Image"] = "rbxassetid://15092282349",
+        ["Type"] = "Toy",
+        ["Name"] = "Pumpkin Toy",
+        ["Description"] = ""
+    },
+    ["Fireflies"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Firefly Jar",
+        ["Rarity"] = "Common",
+        ["Price"] = 400,
+        ["ItemID"] = 2474323527,
+        ["Image"] = "342145382",
+        ["Description"] = "Light up the room with a jar of fireflies."
+    },
+    ["CandyToy2022"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Candy Emote",
+        ["ItemID"] = 11283836868,
+        ["Image"] = "http://www.roblox.com/asset/?id=11137521765",
+        ["Description"] = ""
+    },
+    ["ChocolateMilk"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Chocolate Milk",
+        ["Rarity"] = "Common",
+        ["Price"] = 100,
+        ["ItemID"] = 342177661,
+        ["Image"] = "342177661",
+        ["Description"] = "Relax after a long day of unboxing crates with a cool glass of chocolate milk."
+    },
+    ["Pizza"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Pizza",
+        ["Rarity"] = "Common",
+        ["Price"] = 100,
+        ["ItemID"] = 342189892,
+        ["Image"] = "342189892",
+        ["Description"] = "Pizza = delicious."
+    },
+    ["TeamZombies"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Zombie Sign",
+        ["ItemID"] = 11283762381,
+        ["Image"] = "11283762381",
+        ["Description"] = ""
+    },
+    ["TeamSurvivors"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Survivor Sign",
+        ["ItemID"] = 11283752703,
+        ["Image"] = "11283752703",
+        ["Description"] = ""
+    },
+    ["ReaverSign"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Reaver Sign",
+        ["ItemID"] = 7808229175,
+        ["Image"] = "7808229175",
+        ["Description"] = ""
+    },
+    ["CandyToy2020"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Toy Candy 2",
+        ["ItemID"] = 5872766873,
+        ["Image"] = "http://www.roblox.com/asset/?id=5872775283",
+        ["Description"] = ""
+    },
+    ["CandyToy"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Toy Candy",
+        ["ItemID"] = 4238848221,
+        ["Image"] = "http://www.roblox.com/asset/?id=4212868318",
+        ["Description"] = ""
+    },
+    ["ZombieFriend"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "ZombieFriend",
+        ["ItemID"] = 36784258,
+        ["Image"] = "36784258",
+        ["Description"] = ""
+    },
+    ["BooSign"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "BooSign",
+        ["ItemID"] = 7808228583,
+        ["Image"] = "7808228583",
+        ["Description"] = ""
+    },
+    ["ScaryKey"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Key Toy",
+        ["ItemID"] = 5924837972,
+        ["Image"] = "http://www.roblox.com/asset/?id=5924837972",
+        ["Description"] = ""
+    },
+    ["Teddy"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Teddy Bear",
+        ["Rarity"] = "Common",
+        ["Price"] = 140,
+        ["ItemID"] = 345708612,
+        ["Image"] = "345708612",
+        ["Description"] = "He will always love you forever."
+    },
+    ["SnowTokenToy"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Toy Token",
+        ["ItemID"] = 4535394532,
+        ["Image"] = "http://www.roblox.com/asset/?id=4521691392",
+        ["Description"] = ""
+    },
+    ["IceCream"] = {
+        ["Type"] = "Toy",
+        ["Name"] = "Ice Cream",
+        ["Rarity"] = "Common",
+        ["Price"] = 100,
+        ["ItemID"] = 2474138899,
+        ["Image"] = "342188950",
+        ["Description"] = "Yummy!"
+    }
+}
