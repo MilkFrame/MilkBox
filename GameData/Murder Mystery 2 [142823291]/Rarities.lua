1d90fb92b7ee15cf7ba220c4f2e143c594e1b597
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-22 22:49:32 +1000

    Create Rarities.lua

---
+++ GameData/Murder Mystery 2 [142823291]/Rarities.lua
@@ -0,0 +1,57 @@
+data = {
+    ["Ancient"] = {
+        ["Color"] = {["b"] = 255, ["g"] = 10, ["r"] = 100},
+        ["Hex"] = "#640aff",
+        ["Name"] = "Ancient",
+        ["Sort"] = 75
+    },
+    ["Common"] = {
+        ["Color"] = {["b"] = 106, ["g"] = 106, ["r"] = 106},
+        ["Hex"] = "#6a6a6a",
+        ["Name"] = "Common",
+        ["Sort"] = 100
+    },
+    ["Classic"] = {
+        ["Color"] = {["b"] = 0, ["g"] = 200, ["r"] = 230},
+        ["Hex"] = "#e6c800",
+        ["Sort"] = 110,
+        ["Name"] = "Vintage"
+    },
+    ["Uncommon"] = {
+        ["Color"] = {["b"] = 255, ["g"] = 255, ["r"] = 0},
+        ["Hex"] = "#00ffff",
+        ["Name"] = "Uncommon",
+        ["Sort"] = 95
+    },
+    ["Rare"] = {["Color"] = {["b"] = 0, ["g"] = 200, ["r"] = 0}, ["Hex"] = "#00c800", ["Name"] = "Rare", ["Sort"] = 90},
+    ["Legendary"] = {
+        ["Color"] = {["b"] = 5, ["g"] = 0, ["r"] = 220},
+        ["Hex"] = "#dc0005",
+        ["Sort"] = 85,
+        ["Name"] = "Legendary"
+    },
+    ["Halloween"] = {
+        ["Color"] = {["b"] = 0, ["g"] = 70, ["r"] = 180},
+        ["Hex"] = "#b44600",
+        ["Name"] = "Halloween",
+        ["Sort"] = 104
+    },
+    ["Christmas"] = {
+        ["Color"] = {["b"] = 190, ["g"] = 195, ["r"] = 30},
+        ["Hex"] = "#1ec3be",
+        ["Name"] = "Christmas",
+        ["Sort"] = 105
+    },
+    ["Unique"] = {
+        ["Color"] = {["b"] = 0, ["g"] = 140, ["r"] = 240},
+        ["Hex"] = "#fa8c00",
+        ["Sort"] = 70,
+        ["Name"] = "Unique"
+    },
+    ["Godly"] = {
+        ["Color"] = {["b"] = 179, ["g"] = 0, ["r"] = 255},
+        ["Hex"] = "#ff00b3",
+        ["Sort"] = 80,
+        ["Name"] = "Godly"
+    }
+}
