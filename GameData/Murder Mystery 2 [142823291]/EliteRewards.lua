a75864c25261bd508cb4e14051a3691bbf168a43
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-22 22:51:48 +1000

    Create EliteRewards.lua

---
+++ GameData/Murder Mystery 2 [142823291]/EliteRewards.lua
@@ -0,0 +1,6 @@
+data = {
+    ["EliteKnife"] = {["Type"] = "Weapon", ["Reward"] = "Elite"},
+    ["EliteEffect"] = {["Type"] = "Effects", ["Reward"] = "Elitify"},
+    ["ElitePet"] = {["Type"] = "Pets", ["Reward"] = "Elitey"},
+    ["EliteRadio"] = {["Type"] = "Radios", ["Reward"] = "Elite"}
+}
