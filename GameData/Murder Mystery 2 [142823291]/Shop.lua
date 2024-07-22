ef49ab4d0d2be1f08acd9068a4e843e53995387d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-22 22:52:23 +1000

    Create Shop.lua

---
+++ GameData/Murder Mystery 2 [142823291]/Shop.lua
@@ -0,0 +1,154 @@
+data = {
+    ["Perks"] = {
+        ["Sprint"] = {["Price"] = {["Gems"] = 400, ["Coins"] = 3000}},
+        ["Trap"] = {["Price"] = {["Gems"] = 400, ["Coins"] = 3000}},
+        ["FakeGun"] = {["Price"] = {["Gems"] = 400, ["Coins"] = 3000}},
+        ["Ghost"] = {["Price"] = {["Gems"] = 800, ["Coins"] = 6000}},
+        ["Sleight"] = {["Price"] = {["Gems"] = 100, ["Coins"] = 1000}},
+        ["Haste"] = {["Price"] = {["Gems"] = 400, ["Coins"] = 3000}},
+        ["Xray"] = {["Price"] = {["Gems"] = 800, ["Coins"] = 6000}},
+        ["Ninja"] = {["Price"] = {["Gems"] = 100, ["Coins"] = 1000}}
+    },
+    ["Emotes"] = {
+        ["dab"] = {["Description"] = "Hit a HUGE dab on your enemies!", ["Price"] = {["Coins"] = 2800}},
+        ["Fireflies"] = {
+            ["Price"] = {["Coins"] = 1500},
+            ["Description"] = "Release a jar of fireflies.",
+            ["DataType"] = "Toys"
+        },
+        ["zen"] = {
+            ["Description"] = "Experience tranquility with this relaxing zen pose.",
+            ["Price"] = {["Gems"] = 800}
+        },
+        ["ChocolateMilk"] = {
+            ["Price"] = {["Coins"] = 400},
+            ["Description"] = "Relax after a long day of unboxing crates with a cool glass of chocolate milk.",
+            ["DataType"] = "Toys"
+        },
+        ["sit"] = {["Price"] = {["Description"] = "Have a seat after a long day of coin-collecting.", ["Coins"] = 800}},
+        ["floss"] = {["Description"] = "FLOSS on your enemies!", ["Price"] = {["Gems"] = 750}},
+        ["IceCream"] = {["Price"] = {["Coins"] = 400}, ["Description"] = "Yummy!", ["DataType"] = "Toys"},
+        ["SprayPaint"] = {
+            ["Price"] = {["Gems"] = 750},
+            ["Description"] = "Spray paint your favorite decals on the walls.",
+            ["DataType"] = "Toys"
+        },
+        ["Pizza"] = {["Price"] = {["Coins"] = 400}, ["Description"] = "Pizza = delicious.", ["DataType"] = "Toys"},
+        ["GGSign"] = {["Price"] = {["Gems"] = 100}, ["Description"] = "Good game!", ["DataType"] = "Toys"},
+        ["Teddy"] = {
+            ["Price"] = {["Coins"] = 600},
+            ["Description"] = "He will always love you forever.",
+            ["DataType"] = "Toys"
+        },
+        ["ninja"] = {
+            ["Description"] = "Embrace the shadow of a ninja with this flexible pose.",
+            ["Price"] = {["Coins"] = 2600}
+        },
+        ["FakeBomb"] = {
+            ["Price"] = {["Coins"] = 4800},
+            ["Description"] = "Prank your friends with this confetti bomb!",
+            ["DataType"] = "Toys"
+        }
+    },
+    ["Pets"] = {
+        ["Egg"] = {["Price"] = {["Gems"] = 100, ["Coins"] = 1000}, ["LayoutOrder"] = 100, ["DataType"] = "Eggs"}
+    },
+    ["Weapons"] = {
+        ["KnifeBox5"] = {
+            ["Price"] = {["Gems"] = 100, ["Key"] = 1, ["Coins"] = 1000},
+            ["LayoutOrder"] = 305,
+            ["DataType"] = "MysteryBox"
+        },
+        ["KnifeBox2"] = {
+            ["Price"] = {["Gems"] = 100, ["Key"] = 1, ["Coins"] = 1000},
+            ["LayoutOrder"] = 302,
+            ["DataType"] = "MysteryBox"
+        },
+        ["Summer2024Box"] = {
+            ["Price"] = {["SummerKey2024"] = 1, ["BeachBalls2024_Box"] = 400},
+            ["LayoutOrder"] = 99,
+            ["DataType"] = "MysteryBox"
+        },
+        ["MysteryBox1"] = {
+            ["Price"] = {["Gems"] = 100, ["Key"] = 1, ["Coins"] = 1000},
+            ["LayoutOrder"] = 100,
+            ["DataType"] = "MysteryBox"
+        },
+        ["GunBox3"] = {
+            ["Price"] = {["Gems"] = 100, ["Key"] = 1, ["Coins"] = 1000},
+            ["LayoutOrder"] = 403,
+            ["DataType"] = "MysteryBox"
+        },
+        ["GunBox2"] = {
+            ["Price"] = {["Gems"] = 100, ["Key"] = 1, ["Coins"] = 1000},
+            ["LayoutOrder"] = 402,
+            ["DataType"] = "MysteryBox"
+        },
+        ["KnifeBox3"] = {
+            ["Price"] = {["Gems"] = 100, ["Key"] = 1, ["Coins"] = 1000},
+            ["LayoutOrder"] = 303,
+            ["DataType"] = "MysteryBox"
+        },
+        ["Key"] = {
+            ["LayoutOrder"] = 200,
+            ["OwnedType"] = "Weapons",
+            ["Description"] = "Can be used once to open a mystery box. Tradeable!",
+            ["Price"] = {["Gems"] = 125},
+            ["DataType"] = "Item"
+        },
+        ["KnifeBox4"] = {
+            ["Price"] = {["Gems"] = 100, ["Key"] = 1, ["Coins"] = 1000},
+            ["LayoutOrder"] = 304,
+            ["DataType"] = "MysteryBox"
+        },
+        ["MLG Box"] = {
+            ["Price"] = {["Gems"] = 100, ["Key"] = 1, ["Coins"] = 1000},
+            ["LayoutOrder"] = 306,
+            ["DataType"] = "MysteryBox"
+        },
+        ["KnifeBox1"] = {
+            ["Price"] = {["Gems"] = 100, ["Key"] = 1, ["Coins"] = 1000},
+            ["LayoutOrder"] = 301,
+            ["DataType"] = "MysteryBox"
+        },
+        ["MysteryBox2"] = {
+            ["Price"] = {["Gems"] = 100, ["Key"] = 1, ["Coins"] = 1000},
+            ["LayoutOrder"] = 100,
+            ["DataType"] = "MysteryBox"
+        },
+        ["GunBox1"] = {
+            ["Price"] = {["Gems"] = 100, ["Key"] = 1, ["Coins"] = 1000},
+            ["LayoutOrder"] = 401,
+            ["DataType"] = "MysteryBox"
+        }
+    },
+    ["Radios"] = {
+        ["BluesteelRadio"] = {["Price"] = {["Coins"] = 3200}},
+        ["Flaming"] = {["Price"] = {["Gems"] = 900}},
+        ["DiamondRadio"] = {["Price"] = {["Gems"] = 2500}},
+        ["PinkSparkleRadio"] = {["Price"] = {["Gems"] = 1000}},
+        ["CamoRadio"] = {["Price"] = {["Coins"] = 2000}},
+        ["Boombastic"] = {["Price"] = {["Coins"] = 3600}},
+        ["FadeRadio"] = {["Price"] = {["Gems"] = 400}},
+        ["RedSparkleRadio"] = {["Price"] = {["Gems"] = 1000}},
+        ["GreenSparkleRadio"] = {["Price"] = {["Gems"] = 1000}},
+        ["Gold"] = {["Price"] = {["Coins"] = 12000}},
+        ["Dubstep"] = {["Price"] = {["Coins"] = 1200}},
+        ["RainbowRadio"] = {["Price"] = {["Gems"] = 800}},
+        ["Omega"] = {["Price"] = {["Gems"] = 400}},
+        ["BlueSparkleRadio"] = {["Price"] = {["Gems"] = 1000}}
+    },
+    ["Effects"] = {
+        ["BlueFire"] = {["Price"] = {["Gems"] = 800}, ["LayoutOrder"] = 1010},
+        ["Dual"] = {["Price"] = {["Gems"] = 900}, ["LayoutOrder"] = 1300},
+        ["Radioactive"] = {["Price"] = {["Coins"] = 1600}, ["LayoutOrder"] = 1890},
+        ["Knifeception"] = {["Price"] = {["Gems"] = 300}, ["LayoutOrder"] = 900},
+        ["Burn"] = {["Price"] = {["Coins"] = 4000}, ["LayoutOrder"] = 1500},
+        ["Ghostify"] = {["Price"] = {["Coins"] = 2000, ["Gems"] = 200}, ["LayoutOrder"] = 2000},
+        ["PinkFire"] = {["Price"] = {["Gems"] = 800}, ["LayoutOrder"] = 1020},
+        ["GreenFire"] = {["Price"] = {["Gems"] = 800}, ["LayoutOrder"] = 1030},
+        ["Musical"] = {["Price"] = {["Coins"] = 2000}, ["LayoutOrder"] = 1899},
+        ["Electric"] = {["Price"] = {["Gems"] = 1000}, ["LayoutOrder"] = 1400},
+        ["Heartify"] = {["Price"] = {["Coins"] = 2400}, ["LayoutOrder"] = 2100}
+    }
+}
