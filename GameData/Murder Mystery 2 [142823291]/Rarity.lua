9f2201053354ccc45fd77b3e57b750b0d0a2609f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-22 22:49:19 +1000

    Create Rarity.lua

---
+++ GameData/Murder Mystery 2 [142823291]/Rarity.lua
@@ -0,0 +1,13 @@
+data = {
+    ["Ancient"] = Color3.fromRGB(100, 10, 255),
+    ["Common"] = Color3.fromRGB(255, 255, 255),
+    ["Classic"] = Color3.fromRGB(255, 255, 0),
+    ["Uncommon"] = Color3.fromRGB(0, 255, 255),
+    ["Rare"] = Color3.fromRGB(0, 255, 0),
+    ["Legendary"] = Color3.fromRGB(255, 0, 0),
+    ["Halloween"] = Color3.fromRGB(221, 86, 2),
+    ["Unique"] = Color3.fromRGB(255, 140, 0),
+    ["Christmas"] = Color3.fromRGB(30, 214, 205),
+    ["Victim"] = Color3.fromRGB(255, 140, 0),
+    ["Godly"] = Color3.fromRGB(255, 0, 255)
+}
