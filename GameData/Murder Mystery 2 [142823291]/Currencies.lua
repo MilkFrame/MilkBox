7bc604dba4e096bb12e323397e708675add8f2d8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-22 22:50:35 +1000

    Create Currencies.lua

---
+++ GameData/Murder Mystery 2 [142823291]/Currencies.lua
@@ -0,0 +1,11 @@
+data = {
+    ["Hearts2023"] = "rbxassetid://4521639303",
+    ["SnowTokens2022"] = "rbxassetid://4521639303",
+    ["Gems"] = "rbxassetid://377010926",
+    ["Coins"] = "rbxassetid://197012173",
+    ["SnowKey2023"] = "rbxassetid://15625795123",
+    ["Keys"] = "rbxassetid://3049472374",
+    ["SnowTokens2023"] = "rbxassetid://4521639303",
+    ["SnowTokens2020"] = "rbxassetid://4521639303",
+    ["Candies2022"] = "http://www.roblox.com/asset/?id=11137521765"
+}
