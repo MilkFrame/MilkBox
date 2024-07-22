5b85888901b92d03814fdf2eadf94fcc09154d17
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-22 22:48:07 +1000

    Create Featured.lua

---
+++ GameData/Murder Mystery 2 [142823291]/Featured.lua
@@ -0,0 +1,10 @@
+data = {
+    ["HotItems"] = {
+        {["Type"] = "Perks", ["ItemID"] = "Trap", ["New"] = true},
+        {["Type"] = "Perks", ["ItemID"] = "Sprint", ["New"] = false},
+        {["Type"] = "Radios", ["ItemID"] = "DiamondRadio", ["New"] = false},
+        {["Type"] = "Emotes", ["ItemID"] = "dab", ["New"] = false}
+    },
+    ["Box"] = "MysteryBox2",
+    ["Cover"] = "rbxassetid://2171174581"
+}
