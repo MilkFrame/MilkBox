63319efde83dd4b083320fd1f469037a02d32d62
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-22 22:48:18 +1000

    Create Perks.lua

---
+++ GameData/Murder Mystery 2 [142823291]/Perks.lua
@@ -0,0 +1,100 @@
+data = {
+    ["Sprint"] = {
+        ["Type"] = "Murderer",
+        ["Description"] = "Activate to gain a 50% speed boost temporarily.",
+        ["Active"] = true,
+        ["Price"] = 1000,
+        ["Image"] = "https://www.roblox.com/asset/?id=4970219155",
+        ["Name"] = "Sprint",
+        ["ActiveTime"] = 3,
+        ["Cooldown"] = 30,
+        ["Rarity"] = "Common"
+    },
+    ["FakeGun"] = {
+        ["Type"] = "Murderer",
+        ["Description"] = "Trick innocents by making your knife look like a gun temporarily.",
+        ["UnlockLevel"] = 79,
+        ["Price"] = 700,
+        ["Image"] = "http://www.roblox.com/asset/?id=17899774181",
+        ["Name"] = "Fake Gun",
+        ["ActiveTime"] = 10,
+        ["Rarity"] = "Common",
+        ["Cooldown"] = 20,
+        ["Active"] = true
+    },
+    ["Ghost"] = {
+        ["Type"] = "Murderer",
+        ["Description"] = "Go invisible for a few seconds.",
+        ["UnlockLevel"] = 53,
+        ["Price"] = 1100,
+        ["Image"] = "https://www.roblox.com/asset/?id=335206856",
+        ["Name"] = "Ghost",
+        ["ActiveTime"] = 5,
+        ["Rarity"] = "Common",
+        ["Cooldown"] = 30,
+        ["Active"] = true
+    },
+    ["Ninja"] = {
+        ["Image"] = "https://www.roblox.com/asset/?id=416832054",
+        ["Name"] = "Ninja",
+        ["Active"] = false,
+        ["Price"] = 600,
+        ["Rarity"] = "Common",
+        ["Type"] = "Murderer",
+        ["Description"] = "Your knife becomes silent. Coins are silent while holding knife."
+    },
+    ["Haste"] = {
+        ["Image"] = "https://www.roblox.com/asset/?id=416942717",
+        ["Name"] = "Haste",
+        ["Active"] = false,
+        ["Price"] = 1000,
+        ["Rarity"] = "Common",
+        ["Type"] = "Murderer",
+        ["Description"] = "You move 10% faster while holding the knife out. This allows you to outrun innocents."
+    },
+    ["Trap"] = {
+        ["Charges"] = 3,
+        ["Description"] = "Places an invisible trap that will stop innocents. Max 3 traps.",
+        ["Active"] = true,
+        ["Price"] = 1000,
+        ["Image"] = "https://www.roblox.com/asset/?id=4966828196",
+        ["Name"] = "Trap",
+        ["Rarity"] = "Common",
+        ["Cooldown"] = 2,
+        ["Type"] = "Murderer"
+    },
+    ["Sleight"] = {
+        ["Rarity"] = "Common",
+        ["Image"] = "https://www.roblox.com/asset/?id=340309581",
+        ["Name"] = "Sleight",
+        ["UnlockLevel"] = 22,
+        ["Price"] = 400,
+        ["Active"] = false,
+        ["Type"] = "Murderer",
+        ["Description"] = "Throw knives quicker."
+    },
+    ["Footsteps"] = {
+        ["Type"] = "Murderer",
+        ["Description"] = "Allows you to see the footprints of innocents.",
+        ["UnlockLevel"] = 1,
+        ["Price"] = 0,
+        ["Image"] = "https://www.roblox.com/asset/?id=336404773",
+        ["Name"] = "Footsteps",
+        ["Local"] = 0,
+        ["Rarity"] = "Common",
+        ["Character"] = 0,
+        ["Active"] = false
+    },
+    ["Xray"] = {
+        ["Type"] = "Murderer",
+        ["Description"] = "See through walls for a few seconds. 30s cooldown.",
+        ["UnlockLevel"] = 61,
+        ["Price"] = 1300,
+        ["Image"] = "https://www.roblox.com/asset/?id=340313896",
+        ["Name"] = "X-Ray",
+        ["ActiveTime"] = 4,
+        ["Rarity"] = "Common",
+        ["Cooldown"] = 30,
+        ["Active"] = true
+    }
+}
