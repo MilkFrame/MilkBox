a166eb40350b819eaa919502c8c6ee2582ff27c0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 22:12:39 +1000

    Create Teams.lua

---
+++ GameData/Arsenal [286090429]/Teams.lua
@@ -0,0 +1,95 @@
+data = {
+    ["shouldShowTeam"] = "function: 0x381eead4425ecefc",
+    ["rgb"] = {},
+    ["colors"] = {
+        ["TRC"] = {
+            Color3.fromRGB(216, 47, 47),
+            Color3.fromRGB(196, 40, 28),
+            Color3.fromRGB(255, 212, 212),
+            Color3.fromRGB(191, 42, 42),
+            Color3.fromRGB(126, 18, 18),
+            Color3.fromRGB(110, 0, 0),
+            Color3.fromRGB(255, 0, 0),
+            Color3.fromRGB(191, 42, 42),
+            Color3.fromRGB(255, 0, 0),
+            Color3.fromRGB(220, 48, 48),
+            Color3.fromRGB(255, 184, 184)
+        },
+        ["TPC"] = {
+            Color3.fromRGB(178, 47, 218),
+            Color3.fromRGB(107, 50, 124),
+            Color3.fromRGB(218, 201, 255),
+            Color3.fromRGB(186, 55, 218),
+            Color3.fromRGB(96, 26, 126),
+            Color3.fromRGB(110, 0, 110),
+            Color3.fromRGB(255, 0, 255),
+            Color3.fromRGB(163, 49, 191),
+            Color3.fromRGB(255, 0, 255),
+            Color3.fromRGB(186, 55, 218),
+            Color3.fromRGB(230, 190, 255)
+        },
+        ["TBC"] = {
+            Color3.fromRGB(6, 147, 255),
+            Color3.fromRGB(13, 105, 172),
+            Color3.fromRGB(207, 227, 255),
+            Color3.fromRGB(41, 131, 191),
+            Color3.fromRGB(18, 76, 126),
+            Color3.fromRGB(0, 0, 110),
+            Color3.fromRGB(0, 0, 255),
+            Color3.fromRGB(36, 118, 173),
+            Color3.fromRGB(0, 136, 255),
+            Color3.fromRGB(41, 131, 191),
+            Color3.fromRGB(143, 194, 255)
+        },
+        ["TYC"] = {
+            Color3.fromRGB(218, 193, 47),
+            Color3.fromRGB(245, 205, 48),
+            Color3.fromRGB(255, 250, 207),
+            Color3.fromRGB(213, 181, 63),
+            Color3.fromRGB(126, 105, 28),
+            Color3.fromRGB(110, 110, 0),
+            Color3.fromRGB(255, 255, 0),
+            Color3.fromRGB(213, 181, 63),
+            Color3.fromRGB(255, 238, 0),
+            Color3.fromRGB(213, 181, 63),
+            Color3.fromRGB(255, 239, 189)
+        },
+        ["TOC"] = {
+            Color3.fromRGB(255, 170, 0),
+            Color3.fromRGB(218, 133, 65),
+            Color3.fromRGB(255, 176, 0),
+            Color3.fromRGB(255, 170, 0),
+            Color3.fromRGB(255, 170, 0),
+            Color3.fromRGB(255, 170, 0),
+            Color3.fromRGB(255, 170, 0),
+            Color3.fromRGB(255, 170, 0),
+            Color3.fromRGB(255, 170, 0),
+            Color3.fromRGB(255, 170, 0),
+            Color3.fromRGB(255, 170, 127)
+        },
+        ["TGC"] = {
+            Color3.fromRGB(96, 186, 40),
+            Color3.fromRGB(75, 151, 75),
+            Color3.fromRGB(206, 255, 212),
+            Color3.fromRGB(62, 191, 55),
+            Color3.fromRGB(27, 126, 27),
+            Color3.fromRGB(0, 110, 0),
+            Color3.fromRGB(0, 255, 0),
+            Color3.fromRGB(62, 191, 55),
+            Color3.fromRGB(25, 255, 0),
+            Color3.fromRGB(68, 206, 58),
+            Color3.fromRGB(195, 255, 194)
+        }
+    },
+    ["getTeamNumbers"] = "function: 0x38500ed0e6f4351c",
+    ["canJoinTeam"] = "function: 0x2dfd85a9fe60727c",
+    ["update"] = "function: 0x1dbf4923273b9e2c",
+    ["names"] = {
+        ["TRC"] = {"Red", "Rd"},
+        ["TPC"] = {"Purple", "Pur"},
+        ["TBC"] = {"Blue", "Blu"},
+        ["TYC"] = {"Yellow", "Ylw"},
+        ["TOC"] = {"Orange", "Or"},
+        ["TGC"] = {"Green", "Grn"}
+    }
+}
