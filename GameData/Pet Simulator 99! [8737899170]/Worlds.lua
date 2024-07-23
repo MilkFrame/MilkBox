473c8a78dbcb158f72d55bf86d19a6f46ad8f2e1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 21:17:24 +1000

    Create Worlds.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/Worlds.lua
@@ -0,0 +1,36 @@
+data = {
+    ["World 1"] = {
+        ["SpawnId"] = "Spawn",
+        ["FallbackSpawnLocation"] = Vector3.new(160.33599853515625, 15, -194.1179962158203),
+        ["_id"] = "World 1",
+        ["Lighting"] = "LightingConfig",
+        ["PlaceId"] = 8737899170,
+        ["MapName"] = "Map",
+        ["WorldNumber"] = 1,
+        ["_script"] = "World 1",
+        ["WorldCurrency"] = "Coins"
+    },
+    ["World 2"] = {
+        ["SpawnId"] = "Tech Spawn",
+        ["FallbackSpawnLocation"] = Vector3.new(-9983.669921875, 13.904000282287598, -383.4020080566406),
+        ["_id"] = "World 2",
+        ["WorldCurrency"] = "TechCoins",
+        ["PlaceId"] = 16498369169,
+        ["MapName"] = "Map2",
+        ["WorldNumber"] = 2,
+        ["_script"] = "World 2",
+        ["Lighting"] = "LightingConfig",
+        ["AdditionalMusic"] = {"rbxassetid://16494370682", "rbxassetid://16494369253", "rbxassetid://16494371914"}
+    },
+    ["World 3"] = {
+        ["SpawnId"] = "Void Spawn",
+        ["FallbackSpawnLocation"] = Vector3.new(-10221.390625, -2.0999999046325684, -7376.2900390625),
+        ["_id"] = "World 3",
+        ["Lighting"] = "LightingConfig",
+        ["PlaceId"] = 17503543197,
+        ["MapName"] = "Map3",
+        ["WorldNumber"] = 3,
+        ["_script"] = "World 3",
+        ["WorldCurrency"] = "VoidCoins"
+    }
+}
