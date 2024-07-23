0c389809e5f43a3fdd75a4828f6af2cd8cbd774d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 21:34:44 +1000

    Create DuelLibrary.lua

---
+++ GameData/RIVALS [17625359962]/DuelLibrary.lua
@@ -0,0 +1,127 @@
+data = {
+    ["GetMaps"] = "function: 0x75fdf62688af43d9",
+    ["Teams"] = {
+        {
+            ["TeamName"] = "Sensei & Co.",
+            ["TeamID"] = "",
+            ["TeamIndex"] = 1,
+            ["PadColor"] = Color3.fromRGB(198, 175, 146),
+            ["Color"] = Color3.fromRGB(255, 215, 0)
+        },
+        {
+            ["TeamName"] = "Nosniy Inc.",
+            ["TeamID"] = "",
+            ["TeamIndex"] = 2,
+            ["PadColor"] = Color3.fromRGB(136, 127, 188),
+            ["Color"] = Color3.fromRGB(116, 61, 255)
+        },
+        {
+            ["TeamName"] = "Neko's R&D",
+            ["TeamID"] = "",
+            ["TeamIndex"] = 3,
+            ["PadColor"] = Color3.fromRGB(145, 166, 188),
+            ["Color"] = Color3.fromRGB(0, 150, 255)
+        }
+    },
+    ["Maps"] = {
+        ["Crossroads"] = {
+            ["Difficulty"] = 2,
+            ["Image"] = "rbxassetid://18336297791",
+            ["MaximumTeamsSupported"] = 3,
+            ["MinimumPlayersRequired"] = 1
+        },
+        ["Onyx"] = {
+            ["Difficulty"] = 3,
+            ["Image"] = "rbxassetid://17619337459",
+            ["MaximumTeamsSupported"] = 3,
+            ["MinimumPlayersRequired"] = 1
+        },
+        ["Shooting Range"] = {
+            ["Difficulty"] = 4,
+            ["Image"] = "rbxassetid://18270648407",
+            ["MaximumTeamsSupported"] = 2,
+            ["MinimumPlayersRequired"] = 9
+        },
+        ["Big Crossroads"] = {
+            ["Difficulty"] = 2,
+            ["Image"] = "rbxassetid://17619338801",
+            ["MaximumTeamsSupported"] = 3,
+            ["MinimumPlayersRequired"] = 5
+        },
+        ["Construction"] = {
+            ["Difficulty"] = 3,
+            ["Image"] = "rbxassetid://17528246378",
+            ["MaximumTeamsSupported"] = 2,
+            ["MinimumPlayersRequired"] = 1
+        },
+        ["Docks"] = {
+            ["Difficulty"] = 2,
+            ["Image"] = "rbxassetid://17619338260",
+            ["MaximumTeamsSupported"] = 2,
+            ["MinimumPlayersRequired"] = 1
+        },
+        ["Backrooms"] = {
+            ["Difficulty"] = 2,
+            ["Image"] = "rbxassetid://17619339042",
+            ["MaximumTeamsSupported"] = 2,
+            ["MinimumPlayersRequired"] = 1
+        },
+        ["Arena"] = {
+            ["Difficulty"] = 1,
+            ["Image"] = "rbxassetid://17528193900",
+            ["MaximumTeamsSupported"] = 2,
+            ["MinimumPlayersRequired"] = 1
+        },
+        ["Station"] = {
+            ["Difficulty"] = 2,
+            ["Image"] = "rbxassetid://17619336674",
+            ["MaximumTeamsSupported"] = 2,
+            ["MinimumPlayersRequired"] = 1
+        },
+        ["Big Arena"] = {
+            ["Difficulty"] = 2,
+            ["Image"] = "rbxassetid://18522249528",
+            ["MaximumTeamsSupported"] = 3,
+            ["MinimumPlayersRequired"] = 5
+        }
+    },
+    ["FakeEliminators"] = {
+        ["Explosive Barrel"] = {["Image"] = "rbxassetid://17699771882", ["DisplayName"] = "Explosive Barrel"}
+    },
+    ["TeamsByID"] = {
+        [""] = {
+            ["TeamName"] = "Sensei & Co.",
+            ["TeamID"] = "",
+            ["TeamIndex"] = 1,
+            ["PadColor"] = Color3.fromRGB(198, 175, 146),
+            ["Color"] = Color3.fromRGB(255, 215, 0)
+        },
+        [""] = {
+            ["TeamName"] = "Neko's R&D",
+            ["TeamID"] = "",
+            ["TeamIndex"] = 3,
+            ["PadColor"] = Color3.fromRGB(145, 166, 188),
+            ["Color"] = Color3.fromRGB(0, 150, 255)
+        },
+        [""] = {
+            ["TeamName"] = "Nosniy Inc.",
+            ["TeamID"] = "",
+            ["TeamIndex"] = 2,
+            ["PadColor"] = Color3.fromRGB(136, 127, 188),
+            ["Color"] = Color3.fromRGB(116, 61, 255)
+        }
+    },
+    ["MapOrder"] = {
+        "Backrooms",
+        "Arena",
+        "Construction",
+        "Station",
+        "Onyx",
+        "Crossroads",
+        "Docks",
+        "Big Arena",
+        "Big Crossroads",
+        "Shooting Range"
+    },
+    ["GetTeamColor"] = "function: 0x47690b3808f08259"
+}
