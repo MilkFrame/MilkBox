870c404efde0e4eff6668a90ad7d2002f5bf3a8d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 21:18:13 +1000

    Update Lootboxes.lua

--- GameData/Pet Simulator 99! [8737899170]/Lootboxes.lua
+++ GameData/Pet Simulator 99! [8737899170]/Lootboxes.lua
@@ -1,23 +1,4 @@
 data = {
-    ["Medium Christmas Present"] = {
-        ["_script"] = "Medium Christmas Present",
-        ["Rarity"] = {
-            ["RarityNumber"] = 3,
-            ["Lootbag"] = "Epic",
-            ["_id"] = "Epic",
-            ["Color"] = Color3.fromRGB(41, 226, 255),
-            ["DisplayName"] = "Epic",
-            ["Message"] = "function: 0x000000008ca3bb5b",
-            ["ItemSlot"] = "ItemSlot",
-            ["Gradient"] = "Gradient",
-            ["Announce"] = false,
-            ["_script"] = "3 | Epic"
-        },
-        ["_id"] = "Medium Christmas Present",
-        ["Desc"] = "Open for a bunch of goodies!",
-        ["Icon"] = "rbxassetid://15635476705",
-        ["DisplayName"] = "Medium Christmas Present"
-    },
     ["Small Christmas Present"] = {
         ["_script"] = "Small Christmas Present",
         ["Rarity"] = {
@@ -26,7 +7,7 @@ data = {
             ["_id"] = "Rare",
             ["Color"] = Color3.fromRGB(113, 255, 62),
             ["DisplayName"] = "Rare",
-            ["Message"] = "function: 0x00000000c268f24b",
+            ["Message"] = "function: 0x08b2ed8afa242a99",
             ["ItemSlot"] = "ItemSlot",
             ["Gradient"] = "Gradient",
             ["Announce"] = false,
@@ -37,24 +18,176 @@ data = {
         ["Icon"] = "rbxassetid://15635476827",
         ["DisplayName"] = "Small Christmas Present"
     },
-    ["Titanic Christmas Present"] = {
-        ["_script"] = "Titanic Christmas Present",
+    ["Hype Egg"] = {
+        ["_script"] = "Hype Egg",
+        ["Rarity"] = {
+            ["RarityNumber"] = 999,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exclusive",
+            ["Color"] = Color3.fromRGB(173, 79, 255),
+            ["DisplayName"] = "Exclusive",
+            ["Message"] = "function: 0xd28f61bee5beaff9",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "Exclusive"
+        },
+        ["_id"] = "Hype Egg",
+        ["Desc"] = "Awarded by completing the Hype Event after the update!",
+        ["Icon"] = "rbxassetid://16901624489",
+        ["DisplayName"] = "Hype Egg"
+    },
+    ["Clan Gift"] = {
+        ["_script"] = "Clan Gift",
         ["Rarity"] = {
             ["RarityNumber"] = 6,
             ["Lootbag"] = "Exotic",
             ["_id"] = "Exotic",
             ["Color"] = Color3.fromRGB(255, 29, 251),
             ["DisplayName"] = "Exotic",
-            ["Message"] = "function: 0x00000000bb5c2a6b",
+            ["Message"] = "function: 0xf7b81fb38be7d4f9",
             ["ItemSlot"] = "ItemSlot",
             ["Gradient"] = "Gradient",
             ["Announce"] = true,
             ["_script"] = "6 | Exotic"
         },
-        ["_id"] = "Titanic Christmas Present",
-        ["Desc"] = "TITANIC present with sooo many goodies! Includes chance for a Titanic & Huge pet!",
-        ["Icon"] = "rbxassetid://15635476408",
-        ["DisplayName"] = "Titanic Christmas Present"
+        ["_id"] = "Clan Gift",
+        ["Desc"] = "Rewarded to the top 500 clans in clan battles! Chance for Huge Tech Samurai Cat!",
+        ["Icon"] = "rbxassetid://17025063453",
+        ["DisplayName"] = "Clan Gift"
+    },
+    ["Arcade Egg 5"] = {
+        ["_script"] = "Arcade Egg 5",
+        ["Rarity"] = {
+            ["RarityNumber"] = 4,
+            ["Lootbag"] = "Legendary",
+            ["_id"] = "Legendary",
+            ["Color"] = Color3.fromRGB(255, 133, 34),
+            ["DisplayName"] = "Legendary",
+            ["Message"] = "function: 0x990c3c570ce73c99",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "4 | Legendary"
+        },
+        ["_id"] = "Arcade Egg 5",
+        ["Desc"] = "Obtained from the Claw Machine!",
+        ["Icon"] = "rbxassetid://16759779609",
+        ["DisplayName"] = "Arcade Egg 50x"
+    },
+    ["Evil Gift"] = {
+        ["_script"] = "Evil Gift",
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0xf7b81fb38be7d4f9",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["_id"] = "Evil Gift",
+        ["Desc"] = "Made from items in the Good vs. Evil Event. Chance for a HUGE!",
+        ["Icon"] = "rbxassetid://18100921552",
+        ["DisplayName"] = "Evil Gift"
+    },
+    ["Glitched Gift"] = {
+        ["_script"] = "Glitched Gift",
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0xf7b81fb38be7d4f9",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["_id"] = "Glitched Gift",
+        ["Desc"] = "Made from Glitch Cores in the Glitched Event. Chance for a HUGE 404 Demon!",
+        ["Icon"] = "rbxassetid://17288649241",
+        ["DisplayName"] = "Glitched Gift"
+    },
+    ["Summer Gift"] = {
+        ["_script"] = "Summer Gift",
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0xf7b81fb38be7d4f9",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["_id"] = "Summer Gift",
+        ["Desc"] = "Made from Seashells in the Summer Event. Chance for a HUGE Pineapple Monkey!",
+        ["Icon"] = "rbxassetid://17671375342",
+        ["DisplayName"] = "Summer Gift"
+    },
+    ["Medium Christmas Present"] = {
+        ["_script"] = "Medium Christmas Present",
+        ["Rarity"] = {
+            ["RarityNumber"] = 3,
+            ["Lootbag"] = "Epic",
+            ["_id"] = "Epic",
+            ["Color"] = Color3.fromRGB(41, 226, 255),
+            ["DisplayName"] = "Epic",
+            ["Message"] = "function: 0xfc814f6ff0431799",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "3 | Epic"
+        },
+        ["_id"] = "Medium Christmas Present",
+        ["Desc"] = "Open for a bunch of goodies!",
+        ["Icon"] = "rbxassetid://15635476705",
+        ["DisplayName"] = "Medium Christmas Present"
+    },
+    ["Rave Gift"] = {
+        ["_script"] = "Rave Gift",
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0xf7b81fb38be7d4f9",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["_id"] = "Rave Gift",
+        ["Desc"] = "Reward for playing Rave World's lucky tiles game. Chance for a HUGE!",
+        ["Icon"] = "rbxassetid://18336837008",
+        ["DisplayName"] = "Rave Gift"
+    },
+    ["Arcade Egg 3"] = {
+        ["_script"] = "Arcade Egg 3",
+        ["Rarity"] = {
+            ["RarityNumber"] = 4,
+            ["Lootbag"] = "Legendary",
+            ["_id"] = "Legendary",
+            ["Color"] = Color3.fromRGB(255, 133, 34),
+            ["DisplayName"] = "Legendary",
+            ["Message"] = "function: 0x990c3c570ce73c99",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "4 | Legendary"
+        },
+        ["_id"] = "Arcade Egg 3",
+        ["Desc"] = "Obtained from the Claw Machine!",
+        ["Icon"] = "rbxassetid://16759779792",
+        ["DisplayName"] = "Arcade Egg 10x"
     },
     ["Large Christmas Present"] = {
         ["_script"] = "Large Christmas Present",
@@ -64,7 +197,7 @@ data = {
             ["_id"] = "Legendary",
             ["Color"] = Color3.fromRGB(255, 133, 34),
             ["DisplayName"] = "Legendary",
-            ["Message"] = "function: 0x0000000040b6e07b",
+            ["Message"] = "function: 0x990c3c570ce73c99",
             ["ItemSlot"] = "ItemSlot",
             ["Gradient"] = "Gradient",
             ["Announce"] = false,
@@ -83,7 +216,7 @@ data = {
             ["_id"] = "Exotic",
             ["Color"] = Color3.fromRGB(255, 29, 251),
             ["DisplayName"] = "Exotic",
-            ["Message"] = "function: 0x00000000bb5c2a6b",
+            ["Message"] = "function: 0xf7b81fb38be7d4f9",
             ["ItemSlot"] = "ItemSlot",
             ["Gradient"] = "Gradient",
             ["Announce"] = true,
@@ -94,6 +227,82 @@ data = {
         ["Icon"] = "rbxassetid://15800024082",
         ["DisplayName"] = "2024 New Year's Gift"
     },
+    ["Good Gift"] = {
+        ["_script"] = "Good Gift",
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0xf7b81fb38be7d4f9",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["_id"] = "Good Gift",
+        ["Desc"] = "Made from items in the Good vs. Evil Event. Chance for a HUGE!",
+        ["Icon"] = "rbxassetid://18100921410",
+        ["DisplayName"] = "Good Gift"
+    },
+    ["Good vs Evil Gift"] = {
+        ["_script"] = "Good vs Evil Gift",
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0xf7b81fb38be7d4f9",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["_id"] = "Good vs Evil Gift",
+        ["Desc"] = "Reward for playing the Good vs. Evil Event. Chance for a HUGE!",
+        ["Icon"] = "rbxassetid://18100928783",
+        ["DisplayName"] = "Good vs. Evil Gift"
+    },
+    ["Arcade Egg 1"] = {
+        ["_script"] = "Arcade Egg 1",
+        ["Rarity"] = {
+            ["RarityNumber"] = 4,
+            ["Lootbag"] = "Legendary",
+            ["_id"] = "Legendary",
+            ["Color"] = Color3.fromRGB(255, 133, 34),
+            ["DisplayName"] = "Legendary",
+            ["Message"] = "function: 0x990c3c570ce73c99",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "4 | Legendary"
+        },
+        ["_id"] = "Arcade Egg 1",
+        ["Desc"] = "Obtained from the Claw Machine!",
+        ["Icon"] = "rbxassetid://16759780030",
+        ["DisplayName"] = "Arcade Egg 1x"
+    },
+    ["Titanic Christmas Present"] = {
+        ["_script"] = "Titanic Christmas Present",
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0xf7b81fb38be7d4f9",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["_id"] = "Titanic Christmas Present",
+        ["Desc"] = "TITANIC present with sooo many goodies! Includes chance for a Titanic & Huge pet!",
+        ["Icon"] = "rbxassetid://15635476408",
+        ["DisplayName"] = "Titanic Christmas Present"
+    },
     ["X-Large Christmas Present"] = {
         ["_script"] = "X-Large Christmas Present",
         ["Rarity"] = {
@@ -102,7 +311,7 @@ data = {
             ["_id"] = "Mythical",
             ["Color"] = Color3.fromRGB(255, 43, 100),
             ["DisplayName"] = "Mythical",
-            ["Message"] = "function: 0x000000004162f913",
+            ["Message"] = "function: 0xc03ce71914dc8399",
             ["ItemSlot"] = "ItemSlot",
             ["Gradient"] = "Gradient",
             ["Announce"] = false,
@@ -112,5 +321,81 @@ data = {
         ["Desc"] = "Huge present with a ton of goodies!",
         ["Icon"] = "rbxassetid://15635476534",
         ["DisplayName"] = "X-Large Christmas Present"
+    },
+    ["Arcade Egg 2"] = {
+        ["_script"] = "Arcade Egg 2",
+        ["Rarity"] = {
+            ["RarityNumber"] = 4,
+            ["Lootbag"] = "Legendary",
+            ["_id"] = "Legendary",
+            ["Color"] = Color3.fromRGB(255, 133, 34),
+            ["DisplayName"] = "Legendary",
+            ["Message"] = "function: 0x990c3c570ce73c99",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "4 | Legendary"
+        },
+        ["_id"] = "Arcade Egg 2",
+        ["Desc"] = "Obtained from the Claw Machine!",
+        ["Icon"] = "rbxassetid://16759779878",
+        ["DisplayName"] = "Arcade Egg 5x"
+    },
+    ["Arcade Egg 4"] = {
+        ["_script"] = "Arcade Egg 4",
+        ["Rarity"] = {
+            ["RarityNumber"] = 4,
+            ["Lootbag"] = "Legendary",
+            ["_id"] = "Legendary",
+            ["Color"] = Color3.fromRGB(255, 133, 34),
+            ["DisplayName"] = "Legendary",
+            ["Message"] = "function: 0x990c3c570ce73c99",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "4 | Legendary"
+        },
+        ["_id"] = "Arcade Egg 4",
+        ["Desc"] = "Obtained from the Claw Machine!",
+        ["Icon"] = "rbxassetid://16759779714",
+        ["DisplayName"] = "Arcade Egg 20x"
+    },
+    ["Global Event Gift"] = {
+        ["_script"] = "Global Event Gift",
+        ["Rarity"] = {
+            ["RarityNumber"] = 9,
+            ["Lootbag"] = "Celestial",
+            ["_id"] = "Celestial",
+            ["Color"] = Color3.fromRGB(187, 196, 255),
+            ["DisplayName"] = "Celestial",
+            ["Message"] = "function: 0x27fb7fdabbfa1f99",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "9 | Celestial"
+        },
+        ["_id"] = "Global Event Gift",
+        ["Desc"] = [[Special gift in global events!\nChance for the Huge Lunar Moth!]],
+        ["Icon"] = "rbxassetid://17024878528",
+        ["DisplayName"] = "Global Event Gift"
+    },
+    ["Adventurer Lootbox"] = {
+        ["_script"] = "Adventurer Lootbox",
+        ["Rarity"] = {
+            ["RarityNumber"] = 9,
+            ["Lootbag"] = "Celestial",
+            ["_id"] = "Celestial",
+            ["Color"] = Color3.fromRGB(187, 196, 255),
+            ["DisplayName"] = "Celestial",
+            ["Message"] = "function: 0x27fb7fdabbfa1f99",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "9 | Celestial"
+        },
+        ["_id"] = "Adventurer Lootbox",
+        ["Desc"] = "Adventurer's Lootbox containing a chance for the Active Huge Egg, Huge Firefly, and other high value items!",
+        ["Icon"] = "rbxassetid://16745658276",
+        ["DisplayName"] = "Adventurer's Lootbox"
     }
 }
