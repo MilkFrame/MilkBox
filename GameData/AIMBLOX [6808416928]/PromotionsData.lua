40c329fbec02aa5a0cb29c32245448b9c8a5ccdd
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 22:08:21 +1000

    Create PromotionsData.lua

---
+++ GameData/AIMBLOX [6808416928]/PromotionsData.lua
@@ -0,0 +1,108 @@
+data = {
+    ["Dormant"] = {
+        ["NewUpdate20240412Promo"] = {
+            ["ID"] = "NewUpdate20240412Promo",
+            ["OneTime"] = true,
+            ["DisplayImage"] = "rbxassetid://17372785476",
+            ["DisplayName"] = "NEW WEAPON"
+        },
+        ["NewUpdate20240322Promo"] = {
+            ["ID"] = "NewUpdate20240322Promo",
+            ["OneTime"] = true,
+            ["DisplayImage"] = "rbxassetid://16829359845",
+            ["DisplayName"] = "What's NEW"
+        },
+        ["NewUpdate92923Promo"] = {
+            ["ID"] = "NewUpdate92923Promo",
+            ["OneTime"] = true,
+            ["DisplayImage"] = "rbxassetid://14906410679",
+            ["DisplayName"] = "What's NEW"
+        },
+        ["NewUpdate102723Promo"] = {
+            ["ID"] = "NewUpdate102723Promo",
+            ["OneTime"] = true,
+            ["DisplayImage"] = "rbxassetid://15177120091",
+            ["DisplayName"] = "What's NEW"
+        },
+        ["NewUpdate111023Promo"] = {
+            ["ID"] = "NewUpdate111023Promo",
+            ["OneTime"] = true,
+            ["DisplayImage"] = "rbxassetid://15308762755",
+            ["DisplayName"] = "What's NEW"
+        },
+        ["NewUpdate72823Promo"] = {
+            ["ID"] = "NewUpdate72823Promo",
+            ["OneTime"] = true,
+            ["DisplayImage"] = "rbxassetid://14219827312",
+            ["DisplayName"] = "What's NEW"
+        },
+        ["NewUpdate72123Promo"] = {
+            ["ID"] = "NewUpdate72123Promo",
+            ["OneTime"] = true,
+            ["DisplayImage"] = "rbxassetid://14143798542",
+            ["DisplayName"] = "What's NEW"
+        },
+        ["NewProgression2023Promo"] = {
+            ["ID"] = "NewProgression2023Promo",
+            ["OneTime"] = true,
+            ["DisplayImage"] = "rbxassetid://13912206555",
+            ["DisplayName"] = "New Progression 2023"
+        },
+        ["NewUpdate20240517Promo"] = {
+            ["ID"] = "NewUpdate20240517Promo",
+            ["OneTime"] = true,
+            ["DisplayImage"] = "rbxassetid://18252418441",
+            ["DisplayName"] = "NEW WEAPON"
+        },
+        ["NewUpdate21624Promo"] = {
+            ["ID"] = "NewUpdate21624Promo",
+            ["OneTime"] = true,
+            ["DisplayImage"] = "rbxassetid://16392403420",
+            ["DisplayName"] = "What's NEW"
+        },
+        ["NewUpdate030124Promo"] = {
+            ["ID"] = "NewUpdate030124Promo",
+            ["OneTime"] = true,
+            ["DisplayImage"] = "rbxassetid://16627228033",
+            ["DisplayName"] = "What's NEW"
+        },
+        ["NewUpdate20240510Promo"] = {
+            ["ID"] = "NewUpdate20240510Promo",
+            ["OneTime"] = true,
+            ["DisplayImage"] = "rbxassetid://17447974328",
+            ["DisplayName"] = "NEW WEAPON"
+        },
+        ["_StarterPackPromo"] = {
+            ["DisplayImage"] = "rbxassetid://10087312502",
+            ["AssetInfoType"] = Enum.InfoType.Product,
+            ["ID"] = "_StarterPackPromo",
+            ["AssetID"] = 1279245432,
+            ["OneTime"] = true,
+            ["DisplayName"] = "Starter Pack"
+        },
+        ["NewUpdate12823Promo"] = {
+            ["ID"] = "NewUpdate12823Promo",
+            ["OneTime"] = true,
+            ["DisplayImage"] = "rbxassetid://15628806493",
+            ["DisplayName"] = "What's NEW"
+        },
+        ["NewUpdate101323Promo"] = {
+            ["ID"] = "NewUpdate101323Promo",
+            ["OneTime"] = true,
+            ["DisplayImage"] = "rbxassetid://15047484695",
+            ["DisplayName"] = "What's NEW"
+        },
+        ["NewUpdate11224Promo"] = {
+            ["ID"] = "NewUpdate11224Promo",
+            ["OneTime"] = true,
+            ["DisplayImage"] = "rbxassetid://15950137623",
+            ["DisplayName"] = "What's NEW"
+        }
+    },
+    ["NewUpdate20240628Promo"] = {
+        ["ID"] = "NewUpdate20240628Promo",
+        ["OneTime"] = true,
+        ["DisplayImage"] = "rbxassetid://18252418441",
+        ["DisplayName"] = "FREEZE RAY!"
+    }
+}
