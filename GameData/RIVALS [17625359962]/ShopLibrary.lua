654fe7bbd1b144aee5e33bb8760b22e265157113
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 21:35:53 +1000

    Create ShopLibrary.lua

---
+++ GameData/RIVALS [17625359962]/ShopLibrary.lua
@@ -0,0 +1,93 @@
+data = {
+    ["GetCostToLevelUp"] = "function: 0x16a8aae223cdd559",
+    ["Lootboxes"] = {
+        ["Charm Capsule"] = {
+            ["ShopEntry"] = {
+                ["EntryType"] = "Lootbox",
+                ["Rewards"] = {{["Quantity"] = 1, ["Name"] = "Charm Capsule", ["Weapon"] = "IsRandom"}},
+                ["EntryName"] = "lootbox_Charm Capsule",
+                ["KeyPrice"] = 3
+            },
+            ["KeyPricesByRarity"] = {["Legendary"] = 24, ["Rare"] = 12, ["Common"] = 6}
+        },
+        ["Skin Case"] = {
+            ["ShopEntry"] = {
+                ["ProductID"] = 1852079505,
+                ["ProductIDTriple"] = 1861165031,
+                ["EntryType"] = "Lootbox",
+                ["Rewards"] = {{["Quantity"] = 1, ["Name"] = "Skin Case", ["Weapon"] = "IsRandom"}},
+                ["EntryName"] = "lootbox_Skin Case"
+            }
+        },
+        ["Wrap Box"] = {
+            ["ShopEntry"] = {
+                ["EntryType"] = "Lootbox",
+                ["Rewards"] = {{["Quantity"] = 1, ["Name"] = "Wrap Box", ["Weapon"] = "IsRandom"}},
+                ["EntryName"] = "lootbox_Wrap Box",
+                ["KeyPrice"] = 5
+            },
+            ["KeyPricesByRarity"] = {["Legendary"] = 45, ["Rare"] = 20, ["Common"] = 10}
+        }
+    },
+    ["Weapons"] = {
+        ["Shotgun"] = {["KeyPrice"] = 20, ["Weapon"] = "Shotgun"},
+        ["Grenade Launcher"] = {["KeyPrice"] = 425, ["Weapon"] = "Grenade Launcher"},
+        ["Medkit"] = {["KeyPrice"] = 300, ["Weapon"] = "Medkit"},
+        ["Burst Rifle"] = {["KeyPrice"] = 25, ["Weapon"] = "Burst Rifle"},
+        ["Exogun"] = {["KeyPrice"] = 350, ["Weapon"] = "Exogun"},
+        ["Freeze Ray"] = {["KeyPrice"] = 45, ["Weapon"] = "Freeze Ray"},
+        ["Flashbang"] = {["KeyPrice"] = 15, ["Weapon"] = "Flashbang"},
+        ["Shorty"] = {["KeyPrice"] = 20, ["Weapon"] = "Shorty"},
+        ["Smoke Grenade"] = {["KeyPrice"] = 10, ["Weapon"] = "Smoke Grenade"},
+        ["Subspace Tripmine"] = {["KeyPrice"] = 250, ["Weapon"] = "Subspace Tripmine"},
+        ["Knife"] = {["KeyPrice"] = 20, ["Weapon"] = "Knife"},
+        ["Chainsaw"] = {["KeyPrice"] = 15, ["Weapon"] = "Chainsaw"},
+        ["Bow"] = {["KeyPrice"] = 15, ["Weapon"] = "Bow"},
+        ["Paintball Gun"] = {["KeyPrice"] = 475, ["Weapon"] = "Paintball Gun"},
+        ["Sniper"] = {["KeyPrice"] = 75, ["Weapon"] = "Sniper"},
+        ["Grenade"] = {["KeyPrice"] = 0, ["Weapon"] = "Grenade"},
+        ["Fists"] = {["KeyPrice"] = 0, ["Weapon"] = "Fists"},
+        ["Handgun"] = {["KeyPrice"] = 0, ["Weapon"] = "Handgun"},
+        ["RPG"] = {["KeyPrice"] = 25, ["Weapon"] = "RPG"},
+        ["Minigun"] = {["KeyPrice"] = 450, ["Weapon"] = "Minigun"},
+        ["Trowel"] = {["KeyPrice"] = 200, ["Weapon"] = "Trowel"},
+        ["Scythe"] = {["KeyPrice"] = 40, ["Weapon"] = "Scythe"},
+        ["Uzi"] = {["KeyPrice"] = 20, ["Weapon"] = "Uzi"},
+        ["Slingshot"] = {["KeyPrice"] = 300, ["Weapon"] = "Slingshot"},
+        ["Katana"] = {["KeyPrice"] = 45, ["Weapon"] = "Katana"},
+        ["Assault Rifle"] = {["KeyPrice"] = 0, ["Weapon"] = "Assault Rifle"},
+        ["Flare Gun"] = {["KeyPrice"] = 10, ["Weapon"] = "Flare Gun"},
+        ["Molotov"] = {["KeyPrice"] = 25, ["Weapon"] = "Molotov"},
+        ["Revolver"] = {["KeyPrice"] = 25, ["Weapon"] = "Revolver"},
+        ["Flamethrower"] = {["KeyPrice"] = 400, ["Weapon"] = "Flamethrower"}
+    },
+    ["GetWeaponKeyPriceInfo"] = "function: 0x945399057caa5b59",
+    ["Entries"] = {
+        ["lootbox_Charm Capsule"] = {
+            ["EntryType"] = "Lootbox",
+            ["Rewards"] = {{["Quantity"] = 1, ["Name"] = "Charm Capsule", ["Weapon"] = "IsRandom"}},
+            ["EntryName"] = "lootbox_Charm Capsule",
+            ["KeyPrice"] = 3
+        },
+        ["lootbox_Wrap Box"] = {
+            ["EntryType"] = "Lootbox",
+            ["Rewards"] = {{["Quantity"] = 1, ["Name"] = "Wrap Box", ["Weapon"] = "IsRandom"}},
+            ["EntryName"] = "lootbox_Wrap Box",
+            ["KeyPrice"] = 5
+        },
+        ["loose_groove"] = {
+            ["IsLimited"] = true,
+            ["EntryType"] = "Loose",
+            ["Rewards"] = {{["Name"] = "Groove", ["Weapon"] = "IsUniversal"}},
+            ["EntryName"] = "loose_groove",
+            ["KeyPrice"] = 100
+        },
+        ["lootbox_Skin Case"] = {
+            ["ProductID"] = 1852079505,
+            ["ProductIDTriple"] = 1861165031,
+            ["EntryType"] = "Lootbox",
+            ["Rewards"] = {{["Quantity"] = 1, ["Name"] = "Skin Case", ["Weapon"] = "IsRandom"}},
+            ["EntryName"] = "lootbox_Skin Case"
+        }
+    }
+}
