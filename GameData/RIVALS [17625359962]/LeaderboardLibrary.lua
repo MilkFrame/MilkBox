be6350ee82fb80dd85ea5206af33d0fda9f8c9b7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 21:36:07 +1000

    Create LeaderboardLibrary.lua

---
+++ GameData/RIVALS [17625359962]/LeaderboardLibrary.lua
@@ -0,0 +1,63 @@
+data = {
+    ["REFRESH_TIME"] = 60,
+    ["Info"] = {
+        ["Current Highest Win Streak"] = {
+            ["NumPlayers"] = 100,
+            ["IconFilled"] = "rbxassetid://17175117593",
+            ["IsAscending"] = false,
+            ["Name"] = "Current Highest Win Streak",
+            ["ValueTostring"] = "function: 0x6c9fe0c0db8e8419",
+            ["Icon"] = "rbxassetid://17175092502",
+            ["DisplayName"] = "Win Streaks                   ",
+            ["DataStoreKey"] = "WinStreakCurrent_1",
+            ["UpdateCycle"] = 86400
+        },
+        ["Highest Win Streak Ever"] = {
+            ["NumPlayers"] = 100,
+            ["IconFilled"] = "rbxassetid://17175117593",
+            ["IsAscending"] = false,
+            ["Name"] = "Highest Win Streak Ever",
+            ["ValueTostring"] = "function: 0x6c9fe0c0db8e8419",
+            ["Icon"] = "rbxassetid://17175092502",
+            ["DisplayName"] = "Highest Win Streaks",
+            ["DataStoreKey"] = "WinStreakHighest_1"
+        },
+        ["Most Wins"] = {
+            ["NumPlayers"] = 100,
+            ["IconFilled"] = "rbxassetid://17175117401",
+            ["IsAscending"] = false,
+            ["Name"] = "Most Wins",
+            ["ValueTostring"] = "function: 0x6c9fe0c0db8e8419",
+            ["Icon"] = "rbxassetid://17175095665",
+            ["DisplayName"] = "Most Wins",
+            ["DataStoreKey"] = "Wins_1"
+        },
+        ["Highest Level"] = {
+            ["NumPlayers"] = 100,
+            ["IconFilled"] = "rbxassetid://18371471156",
+            ["IsAscending"] = false,
+            ["Name"] = "Highest Level",
+            ["ValueTostring"] = "function: 0x6c9fe0c0db8e8419",
+            ["Icon"] = "rbxassetid://18270680538",
+            ["DisplayName"] = "Highest Level",
+            ["DataStoreKey"] = "Level_1"
+        },
+        ["Most Eliminations"] = {
+            ["NumPlayers"] = 100,
+            ["IconFilled"] = "rbxassetid://17736752966",
+            ["IsAscending"] = false,
+            ["Name"] = "Most Eliminations",
+            ["ValueTostring"] = "function: 0x6c9fe0c0db8e8419",
+            ["Icon"] = "rbxassetid://17736782213",
+            ["DisplayName"] = "Most Eliminations",
+            ["DataStoreKey"] = "Eliminations_1"
+        }
+    },
+    ["Order"] = {
+        "Current Highest Win Streak",
+        "Most Eliminations",
+        "Most Wins",
+        "Highest Level",
+        "Highest Win Streak Ever"
+    }
+}
