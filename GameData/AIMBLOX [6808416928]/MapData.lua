67416753a9b2b7ae550ff7d71ce76937a686610d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 22:07:53 +1000

    Create MapData.lua

---
+++ GameData/AIMBLOX [6808416928]/MapData.lua
@@ -0,0 +1,207 @@
+data = {
+    ["AirShip"] = {
+        ["DisplayImage"] = "rbxassetid://7450520631",
+        ["Description"] = "This is a map that is used in the quick paced AimBlox Combat PvP game. Don't forget to check it out in the Lobby!!",
+        ["Active"] = true,
+        ["TrainingOnly"] = true,
+        ["DisplayName"] = "Air Ship",
+        ["Stages"] = {"GridIntro", "SpiderIntro", "SixshotIntro", "MotionIntro", "TrackIntro"},
+        ["DisplayOrder"] = 3,
+        ["ID"] = "AirShip",
+        ["Tags"] = {["All"] = true}
+    },
+    ["AimCity"] = {
+        ["DisplayImage"] = "rbxassetid://6929749109",
+        ["Description"] = "This is a map that is used in the quick paced AimBlox Combat PvP game. Don't forget to check it out in the Lobby!!",
+        ["Active"] = true,
+        ["TrainingOnly"] = true,
+        ["DisplayName"] = "Aim City",
+        ["Stages"] = {"GridIntro", "SpiderIntro", "SixshotIntro", "MotionIntro", "TrackIntro"},
+        ["DisplayOrder"] = 2,
+        ["ID"] = "AimCity",
+        ["Tags"] = {["All"] = true}
+    },
+    ["Mojave"] = {
+        ["DisplayImage"] = "rbxassetid://6874634316",
+        ["Tags"] = {["All"] = true},
+        ["Description"] = "This desert themed map illustrates the harsh conditions in the Mojave Desert found in the western United States. It's a map that was originally created for the Roblox game 'Wild Revolvers' by Novaly.",
+        ["DisplayOrder"] = 7,
+        ["ID"] = "Mojave",
+        ["Stages"] = {"MicroGridshot", "SemiMicroSixshot", "Ghostbuster"},
+        ["Active"] = true,
+        ["DisplayName"] = "Mojave"
+    },
+    ["Osake"] = {
+        ["DisplayImage"] = "rbxassetid://8666161809",
+        ["Description"] = "This is a map that is used in the quick paced AimBlox Combat PvP game. Don't forget to check it out in the Lobby!!",
+        ["Active"] = true,
+        ["TrainingOnly"] = true,
+        ["DisplayName"] = "Osake",
+        ["Stages"] = {"GridIntro", "SpiderIntro", "SixshotIntro", "MotionIntro", "TrackIntro"},
+        ["DisplayOrder"] = 11,
+        ["ID"] = "Osake",
+        ["Tags"] = {["All"] = true}
+    },
+    ["ShootOutXAimBlox"] = {
+        ["DisplayImage"] = "rbxassetid://8072596043",
+        ["Description"] = "The training map for the ShootOut and AimBlox collab!!",
+        ["Active"] = false,
+        ["TrainingOnly"] = true,
+        ["DisplayName"] = "ShootOutXAimBlox",
+        ["Stages"] = {"GridIntro", "SpiderIntro", "SixshotIntro", "MotionIntro", "TrackIntro"},
+        ["DisplayOrder"] = 12,
+        ["ID"] = "ShootOutXAimBlox",
+        ["Tags"] = {["All"] = true}
+    },
+    ["Halloween"] = {
+        ["DisplayImage"] = "rbxassetid://7660000372",
+        ["Description"] = "Enter if you dare the AimBlox haunted house! Happy Halloween!!",
+        ["Active"] = false,
+        ["TrainingOnly"] = true,
+        ["DisplayName"] = "Halloween",
+        ["Stages"] = {"GridIntro", "SpiderIntro", "SixshotIntro", "MotionIntro", "TrackIntro"},
+        ["DisplayOrder"] = 10,
+        ["ID"] = "Halloween",
+        ["Tags"] = {["All"] = true}
+    },
+    ["1v1"] = {
+        ["DisplayImage"] = "rbxassetid://6874633663",
+        ["Tags"] = {["All"] = true},
+        ["Description"] = "This map strips away any environmental distractions so you can just focus on your Aim Training.",
+        ["DisplayOrder"] = 1,
+        ["ID"] = "1v1",
+        ["TrainingOnly"] = true,
+        ["Stages"] = {"GridIntro", "SpiderIntro", "SixshotIntro", "MotionIntro", "TrackIntro"},
+        ["DisplayName"] = "1v1"
+    },
+    ["Duckshire"] = {
+        ["DisplayImage"] = "rbxassetid://7112988363",
+        ["Description"] = "This is a map that is used in the quick paced AimBlox Combat PvP game. Don't forget to check it out in the Lobby!!",
+        ["Active"] = true,
+        ["TrainingOnly"] = true,
+        ["DisplayName"] = "Duckshire",
+        ["Stages"] = {"GridIntro", "SpiderIntro", "SixshotIntro", "MotionIntro", "TrackIntro"},
+        ["DisplayOrder"] = 6,
+        ["ID"] = "Duckshire",
+        ["Tags"] = {["All"] = true}
+    },
+    ["Nightlife"] = {
+        ["DisplayImage"] = "rbxassetid://7966582766",
+        ["Description"] = "This is a map that is used in the quick paced AimBlox Combat PvP game. Don't forget to check it out in the Lobby!!",
+        ["Active"] = true,
+        ["TrainingOnly"] = true,
+        ["DisplayName"] = "Nightlife",
+        ["Stages"] = {"GridIntro", "SpiderIntro", "SixshotIntro", "MotionIntro", "TrackIntro"},
+        ["DisplayOrder"] = 9,
+        ["ID"] = "Nightlife",
+        ["Tags"] = {["All"] = true}
+    },
+    ["Moroz"] = {
+        ["DisplayImage"] = "rbxassetid://7176700391",
+        ["Description"] = "This is a map that is used in the quick paced AimBlox Combat PvP game. Don't forget to check it out in the Lobby!!",
+        ["Active"] = true,
+        ["TrainingOnly"] = true,
+        ["DisplayName"] = "Moroz",
+        ["Stages"] = {"GridIntro", "SpiderIntro", "SixshotIntro", "MotionIntro", "TrackIntro"},
+        ["DisplayOrder"] = 8,
+        ["ID"] = "Moroz",
+        ["Tags"] = {["All"] = true}
+    },
+    ["Bastion"] = {
+        ["DisplayImage"] = "rbxassetid://7807123948",
+        ["Description"] = "This is a map that is used in the quick paced AimBlox Combat PvP game. Don't forget to check it out in the Lobby!!",
+        ["Active"] = true,
+        ["TrainingOnly"] = true,
+        ["DisplayName"] = "Bastion",
+        ["Stages"] = {"GridIntro", "SpiderIntro", "SixshotIntro", "MotionIntro", "TrackIntro"},
+        ["DisplayOrder"] = 4,
+        ["ID"] = "Bastion",
+        ["Tags"] = {["All"] = true}
+    },
+    ["Graybox"] = {
+        ["DisplayImage"] = "rbxassetid://6874633663",
+        ["Tags"] = {["All"] = true},
+        ["Description"] = "This map strips away any environmental distractions so you can just focus on your Aim Training.",
+        ["DisplayOrder"] = 1,
+        ["ID"] = "Graybox",
+        ["Stages"] = {"GridIntro", "SpiderIntro", "SixshotIntro", "MotionIntro", "TrackIntro"},
+        ["Active"] = true,
+        ["DisplayName"] = "Graybox"
+    },
+    ["Zanzibar"] = {
+        ["DisplayImage"] = "rbxassetid://6950362220",
+        ["Description"] = "This is a map that is used in the quick paced AimBlox Combat PvP game. Don't forget to check it out in the Lobby!!",
+        ["Active"] = true,
+        ["TrainingOnly"] = true,
+        ["DisplayName"] = "Zanzibar",
+        ["Stages"] = {"GridIntro", "SpiderIntro", "SixshotIntro", "MotionIntro", "TrackIntro"},
+        ["DisplayOrder"] = 15,
+        ["ID"] = "Zanzibar",
+        ["Tags"] = {["All"] = true}
+    },
+    ["Outpost"] = {
+        ["DisplayImage"] = "rbxassetid://6874632719",
+        ["Tags"] = {["All"] = true},
+        ["Description"] = "This wild west themed map harkens back to when bandits and outlaws roamed the western United States. It is a map that originates in the Roblox game 'Wild Revolvers' created by Novaly.",
+        ["DisplayOrder"] = 12,
+        ["ID"] = "Outpost",
+        ["Stages"] = {"Gauntlet"},
+        ["Active"] = true,
+        ["DisplayName"] = "Bandit Outpost"
+    },
+    ["XmasIsland"] = {
+        ["DisplayImage"] = "rbxassetid://8152042923",
+        ["Description"] = "This is Santa's favorite get away spot...no need for sunblock!!",
+        ["Active"] = false,
+        ["TrainingOnly"] = true,
+        ["DisplayName"] = "Xmas Island",
+        ["Stages"] = {"GridIntro", "SpiderIntro", "SixshotIntro", "MotionIntro", "TrackIntro"},
+        ["DisplayOrder"] = 14,
+        ["ID"] = "XmasIsland",
+        ["Tags"] = {["All"] = true}
+    },
+    ["Snowdown"] = {
+        ["DisplayImage"] = "rbxassetid://7176700391",
+        ["Description"] = "This is a map that is used in the quick paced AimBlox Combat PvP game as a part of out Collab with Shoot Out!!",
+        ["Active"] = true,
+        ["TrainingOnly"] = true,
+        ["DisplayName"] = "Snowdown",
+        ["Stages"] = {"GridIntro", "SpiderIntro", "SixshotIntro", "MotionIntro", "TrackIntro"},
+        ["DisplayOrder"] = 13,
+        ["ID"] = "Snowdown",
+        ["Tags"] = {["All"] = true}
+    },
+    ["Oasis"] = {
+        ["DisplayImage"] = "rbxassetid://7293181840",
+        ["Description"] = "This is a map that is used in the quick paced AimBlox Combat PvP game. Don't forget to check it out in the Lobby!!",
+        ["Active"] = true,
+        ["TrainingOnly"] = true,
+        ["DisplayName"] = "Oasis",
+        ["Stages"] = {"GridIntro", "SpiderIntro", "SixshotIntro", "MotionIntro", "TrackIntro"},
+        ["DisplayOrder"] = 10,
+        ["ID"] = "Oasis",
+        ["Tags"] = {["All"] = true}
+    },
+    ["Construct"] = {
+        ["DisplayImage"] = "rbxassetid://7553509753",
+        ["Description"] = "This is a map that is used in the quick paced AimBlox Combat PvP game. Don't forget to check it out in the Lobby!!",
+        ["Active"] = true,
+        ["TrainingOnly"] = true,
+        ["DisplayName"] = "Construct",
+        ["Stages"] = {"GridIntro", "SpiderIntro", "SixshotIntro", "MotionIntro", "TrackIntro"},
+        ["DisplayOrder"] = 5,
+        ["ID"] = "Construct",
+        ["Tags"] = {["All"] = true}
+    },
+    ["Foundry"] = {
+        ["DisplayImage"] = "rbxassetid://9017378385",
+        ["Description"] = "This is a map that is used in the quick paced AimBlox Combat PvP game. Don't forget to check it out in the Lobby!!",
+        ["Active"] = true,
+        ["TrainingOnly"] = true,
+        ["DisplayName"] = "Foundry",
+        ["Stages"] = {"GridIntro", "SpiderIntro", "SixshotIntro", "MotionIntro", "TrackIntro"},
+        ["DisplayOrder"] = 16,
+        ["ID"] = "Foundry",
+        ["Tags"] = {["All"] = true}
+    }
+}
