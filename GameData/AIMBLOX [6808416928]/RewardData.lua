722b92987e0b5a4694aafd5b21ca71219f2387d1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 22:08:34 +1000

    Create RewardData.lua

---
+++ GameData/AIMBLOX [6808416928]/RewardData.lua
@@ -0,0 +1,53 @@
+data = {
+    ["FinalStageReward"] = {
+        ["ID"] = "FinalStageReward",
+        ["TopText"] = "%s score for 5⭐ Victory",
+        ["Rewards"] = {
+            {["Value"] = 500, ["Type"] = "Currency", ["Special"] = false},
+            {["Value"] = 100, ["Type"] = "XP", ["Special"] = false}
+        },
+        ["RequiredScore"] = 4000
+    },
+    ["GauntletReward"] = {
+        ["ID"] = "GauntletReward",
+        ["TopText"] = "%s score for 5⭐ Victory",
+        ["Rewards"] = {
+            {["Value"] = "Supernova", ["Type"] = "Gun", ["Special"] = true},
+            {["Value"] = 2500, ["Type"] = "Currency", ["Special"] = false},
+            {["Value"] = 500, ["Type"] = "XP", ["Special"] = false}
+        },
+        ["RequiredScore"] = 50000
+    },
+    ["AwardCash"] = {
+        ["AwardDisplayData"] = {
+            ["ButtonText"] = "Claim!",
+            ["Type"] = "Currency",
+            ["Desc"] = "You got cash!",
+            ["ID"] = "AwardCash",
+            ["DisplayTime"] = 1,
+            ["Icon"] = "rbxassetid://7843982007",
+            ["DisplayName"] = "AIMBUX"
+        },
+        ["Type"] = "Currency",
+        ["Amount"] = 0
+    },
+    ["BasicReward"] = {
+        ["ID"] = "BasicReward",
+        ["TopText"] = "%s score for 5⭐ Victory",
+        ["Rewards"] = {
+            {["Value"] = 250, ["Type"] = "Currency", ["Special"] = false},
+            {["Value"] = 50, ["Type"] = "XP", ["Special"] = false}
+        },
+        ["RequiredScore"] = 2500
+    },
+    ["ZombieTestReward"] = {
+        ["ID"] = "ZombieTestReward",
+        ["TopText"] = "%s score for 5⭐ Victory",
+        ["Rewards"] = {
+            {["Value"] = "ScarH", ["Type"] = "Gun", ["Special"] = true},
+            {["Value"] = 2000, ["Type"] = "Currency", ["Special"] = false},
+            {["Value"] = 250, ["Type"] = "XP", ["Special"] = false}
+        },
+        ["RequiredScore"] = 8000
+    }
+}
