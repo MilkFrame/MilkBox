4fce3602b82ee7425e9da0031856b67c27db713d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 21:38:31 +1000

    Create BountyLibrary.lua

---
+++ GameData/RIVALS [17625359962]/BountyLibrary.lua
@@ -0,0 +1,29 @@
+data = {
+    ["Players"] = {
+        ["487532097"] = {["Username"] = "CarbonMeister", ["EliminationReward"] = "CarbonMeister"},
+        ["964088769"] = {["Username"] = "DVwastaken", ["EliminationReward"] = "DV"},
+        ["20349956"] = {["Username"] = "Nosniy", ["WinReward"] = "Nosnite", ["EliminationReward"] = "Nosniy"},
+        ["64516"] = {["Username"] = "Bandites", ["EliminationReward"] = "Bandites"},
+        ["301707243"] = {["Username"] = "BobbVX", ["EliminationReward"] = "BobbVX"},
+        ["84743203"] = {["Username"] = "TanqR", ["EliminationReward"] = "TanqR"},
+        ["266340204"] = {["Username"] = "Blizmid", ["EliminationReward"] = "Blizmid"},
+        ["341006533"] = {["Username"] = "oPixel", ["EliminationReward"] = "oPixel"},
+        ["780350915"] = {["Username"] = "nekoanims", ["WinReward"] = "Nekore", ["EliminationReward"] = "nekoanims"},
+        ["15941965"] = {["Username"] = "SenseiWarrior",["WinReward"] = "Sensite",["EliminationReward"] = "SenseiWarrior"}
+    },
+    ["Rewards"] = {
+        ["TanqR"] = true,
+        ["oPixel"] = true,
+        ["BobbVX"] = true,
+        ["SenseiWarrior"] = true,
+        ["nekoanims"] = true,
+        ["CarbonMeister"] = true,
+        ["Nekore"] = true,
+        ["Blizmid"] = true,
+        ["Bandites"] = true,
+        ["Nosnite"] = true,
+        ["DV"] = true,
+        ["Sensite"] = true,
+        ["Nosniy"] = true
+    }
+}
