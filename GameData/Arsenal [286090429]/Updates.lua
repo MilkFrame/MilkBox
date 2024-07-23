de95abefc8c1712ce111afc489592683cedd7fb5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 22:14:11 +1000

    Create Updates.lua

---
+++ GameData/Arsenal [286090429]/Updates.lua
@@ -0,0 +1,152 @@
+data = {
+    ["1"] = {
+        ["mobile"] = {["removals"] = {}, ["changes"] = {{"No mobile specific changes."}}, ["additions"] = {}},
+        ["title"] = "Arsenal Winter Part 2",
+        ["console"] = {["removals"] = {}, ["changes"] = {{"No console specific changes."}}, ["additions"] = {}},
+        ["computer"] = {["removals"] = {}, ["changes"] = {{"No PC specific changes."}}, ["additions"] = {}},
+        ["general"] = {
+            ["removals"] = {},
+            ["changes"] = {
+                {
+                    "Crate Preview now shows already owned items",
+                    "It's now easier than ever to figure out what items you're missing.",
+                    "Items you don't own will be sorted to the top of the case preview."
+                },
+                {
+                    "Reverted Assault",
+                    "The new version of the map was behaving in odd ways, so it's been reverted until we can resolve the issue."
+                },
+                {"Something's different...", "Something's different with this Emote..."},
+                {
+                    "Gift For You taunt fixed",
+                    "The explosion easter egg was accidentally a 9 in 10 chance rather than a 1 in 10 chance for like a year. Whoops!"
+                }
+            },
+            ["additions"] = {
+                {
+                    "Added Group Rewards",
+                    "By joining the ROLVe Roblox group you can obtain exclusive perks, such as a chat tag (💪) and 25% more Bucks and XP upon winning a round!"
+                },
+                {"Added Update Log", "Starting with this update ALL changes, additions and fixes will be logged here."},
+                {
+                    "Added Random Drops",
+                    "After each round there's a chance that a random user in the game will receive a random crate item!"
+                },
+                {
+                    "Added Victory Themes",
+                    "A blast from the past! Pick from an ever-expanding library of audio that plays whenever you win a round.",
+                    "Available for purchase now in Flair Crates. You can equip them in the Settings Menu."
+                },
+                {
+                    "Added Menu Music",
+                    "We decided to give the menu a bit of a refresh! Enjoy music that plays while you get ready for battle, or turn it off to listen to the battleground before joining in!"
+                },
+                {
+                    "New Settings!",
+                    "Customise the sounds you hear when hitting or eliminating an enemy.",
+                    "Toggle between the new menu music and background gameplay sounds."
+                },
+                {"New Character Crate", "Express yourself this season with 12 new, limited time character skins!"},
+                {
+                    "New Calling Card Pack",
+                    "10 new Calling Cards are now available for you to purchase and use to truly get under the skin of your enemies!"
+                },
+                {"New Kill Effects!", "4 new Kill Effects are now available in the Flair Crate."},
+                {"New Gamemode!", "Snowball Fight! Get to 16 kills with ONLY snowballs!"},
+                {
+                    "New Crate",
+                    "For the rest of the Winter Update users can buy a Gift Crate that will give out a crate skin to every user in their server, with a 5% chance for an unusual drop!"
+                },
+                {
+                    "Sneaky Santa",
+                    "5 Presents have been hidden around Arsenal, can you find them before December is over? (Reward will be given on January 1st)"
+                }
+            }
+        },
+        ["description"] = [[Winter is officially here!
+
+Welcome to the first update log for Arsenal, check in each update to see the various changes to the game and platform specific updates and fixes.]]
+    },
+    ["3"] = {
+        ["mobile"] = {["removals"] = {}, ["changes"] = {{"No mobile specific changes."}}, ["additions"] = {}},
+        ["title"] = "Arsenal Sainthood Update",
+        ["console"] = {["removals"] = {}, ["changes"] = {{"No console specific changes."}}, ["additions"] = {}},
+        ["computer"] = {["removals"] = {}, ["changes"] = {{"No PC specific changes."}}, ["additions"] = {}},
+        ["general"] = {
+            ["removals"] = {{"Cache"}},
+            ["changes"] = {
+                {"Galil and USP stats modified."},
+                {"Fixed change colour text outlines box not being properly ticked when enabled."},
+                {"Change Colour TextOutlines will now create drop shadows on main menu icons."},
+                {" "},
+                {"Community Credits:", "Buildingsssss: The new Emotes", "Ascorbine: The Saint Unusuals."},
+                {" "}
+            },
+            ["additions"] = {
+                {"New gamemode:", "Bombfest (Found in Extra Gamemodes)"},
+                {"New revolver:", "Python"},
+                {"New melees:", "Annihilator's Broken Sword", "Reclaimer"},
+                {"New emotes:", "Boasting Jump", "Booyah", "The Jig", "Skillful Blade"},
+                {"New calling cards:", "Dynamite", "Fearless"},
+                {"New weapon skin:", "Clover"},
+                {"New kill effect:", "Snake Bite"},
+                {[[New Menu Colour "Saint's Shamrock".]]},
+                {"New menu music theme."},
+                {"Added three new badges related to kill effects."}
+            }
+        },
+        ["description"] = [[
+Saint Patrick's Day is here, and with that comes "The Saint" character and the Sainthood event! 
+
+Complete random challenges to earn a piece of the three leaf clover. Each piece of the clover will reward you with a Saint Patrick's Day themed item. Completing the clover will earn you "The Saint" character himself. Every clover piece earned after can be used to obtain duplicates of "The Saint" to upgrade and unlock his exclusive unusuals.
+
+This isn't all though, with this update we introduce a new gamemode known as BOMBFEST! Available in the Extra Gamemodes page, participate in a 6v6 sports experience hosted by the Annihilator himself to earn Annihilator themed items.
+
+The Sainthood event ends on April 1st, good luck and have fun!]]
+    },
+    ["2"] = {
+        ["mobile"] = {["removals"] = {}, ["changes"] = {{"No mobile specific changes."}}, ["additions"] = {}},
+        ["title"] = "Arsenal Travelling Hearts",
+        ["console"] = {["removals"] = {}, ["changes"] = {{"No console specific changes."}}, ["additions"] = {}},
+        ["computer"] = {["removals"] = {}, ["changes"] = {{"No PC specific changes."}}, ["additions"] = {}},
+        ["general"] = {
+            ["removals"] = {},
+            ["changes"] = {
+                {[[Previous "Seeker of Hearts" Character Skin is now Retired]]},
+                {
+                    "Fixed player team colours in Free For All",
+                    "This also applies to Private Server exclusive modes that used special team colours such as Juggernaut and Clown Infection"
+                },
+                {"Fixed certain Private Server modes having unusable weapons"},
+                {"Fixed the map voting screen popping up when it shouldn't", "(maybe)"},
+                {
+                    "Shooting networking has been optimised",
+                    "In larger servers during intense gunfights you should notice a slight increase in performance!"
+                }
+            },
+            ["additions"] = {
+                {
+                    "Added 5 new items",
+                    [["Seeker Of Hearts" Character Skin]],
+                    [["Heart Break" Melee]],
+                    [["Heart Break" Kill Effect]],
+                    [["Heart" Emote]],
+                    [["Heartless" Calling Card]]
+                },
+                {
+                    "Added 4 new unusuals",
+                    [["Hearts 1", "Hearts 2" and "Hearts 3" as the exclusive Seeker Of Hearts crafting milestone rewards.]],
+                    [["Cherry Blossoms" as an unusual reward which you can put on any Character Skin.]]
+                },
+                {[[New Menu Colour "Seeker's Serene Pink"]]}
+            }
+        },
+        ["description"] = [[Valentine's Day is here!
+
+Love is in the air... and so is the smell of gunpowder! Earn points by killing people, collecting dropped hearts, completing contracts and winning rounds to obtain event exclusive items!
+Max out your points to obtain the "Seeker Of Hearts" skin! Max it out multiple times to earn more copies!
+The Seeker Of Hearts has unique unusuals you can obtain through crafting... but you can only level it up using copies of itself! Craft it while you can!
+
+The event will run through February 14th to March 1st (~2 weeks). Have fun!]]
+    }
+}
