65461fef24a8bad930429f1e760d6510f3837c39
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 22:15:28 +1000

    Create Challenges.lua

---
+++ GameData/Arsenal [286090429]/Challenges.lua
@@ -0,0 +1,73 @@
+data = {
+    ["Rounds II"] = {
+        ["Difficulty"] = "Medium",
+        ["Max"] = 2,
+        ["ObjectName"] = "Rounds",
+        ["Min"] = 2,
+        ["Trigger"] = "Rounds"
+    },
+    ["Wins"] = {["Difficulty"] = "Hard", ["Max"] = 1, ["ObjectName"] = "Wins", ["Min"] = 1, ["Trigger"] = "Wins"},
+    ["Kills IV"] = {
+        ["Difficulty"] = "Hard",
+        ["Tier"] = "Kills",
+        ["Max"] = 100,
+        ["ObjectName"] = "Kills",
+        ["Trigger"] = "Kills",
+        ["Min"] = 100
+    },
+    ["Rounds III"] = {
+        ["Difficulty"] = "Hard",
+        ["Max"] = 3,
+        ["ObjectName"] = "Rounds",
+        ["Min"] = 3,
+        ["Trigger"] = "Rounds"
+    },
+    ["Kills II"] = {
+        ["Difficulty"] = "Medium",
+        ["Tier"] = "Kills",
+        ["Max"] = 30,
+        ["ObjectName"] = "Kills",
+        ["Trigger"] = "Kills",
+        ["Min"] = 25
+    },
+    ["Kills"] = {["Difficulty"] = "Easy", ["Max"] = 15, ["ObjectName"] = "Kills", ["Min"] = 10, ["Trigger"] = "Kills"},
+    ["Rounds"] = {["Difficulty"] = "Easy", ["Max"] = 1, ["ObjectName"] = "Rounds", ["Min"] = 1, ["Trigger"] = "Rounds"},
+    ["Headshot Kills"] = {
+        ["Difficulty"] = "Medium",
+        ["Max"] = 25,
+        ["ObjectName"] = "Headshot Kills",
+        ["Min"] = 10,
+        ["Trigger"] = "Headshot Kills"
+    },
+    ["Headshot Kills II"] = {
+        ["Difficulty"] = "Hard",
+        ["Tier"] = "Headshot Kills",
+        ["Max"] = 45,
+        ["ObjectName"] = "Headshot Kills",
+        ["Trigger"] = "Headshot Kills",
+        ["Min"] = 30
+    },
+    ["Melee Kills II"] = {
+        ["Difficulty"] = "Hard",
+        ["Tier"] = "Melee Kills",
+        ["Max"] = 20,
+        ["ObjectName"] = "Melee Kills",
+        ["Trigger"] = "Melee Kills",
+        ["Min"] = 15
+    },
+    ["Melee Kills"] = {
+        ["Difficulty"] = "Medium",
+        ["Max"] = 10,
+        ["ObjectName"] = "Melee Kills",
+        ["Min"] = 5,
+        ["Trigger"] = "Melee Kills"
+    },
+    ["Kills III"] = {
+        ["Difficulty"] = "Hard",
+        ["Tier"] = "Kills",
+        ["Max"] = 60,
+        ["ObjectName"] = "Kills",
+        ["Trigger"] = "Kills",
+        ["Min"] = 50
+    }
+}
