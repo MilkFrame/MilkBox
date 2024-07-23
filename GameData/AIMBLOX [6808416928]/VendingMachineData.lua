b878cce7759e121fdaebd9375f48cc2ecd7a71a0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 22:09:09 +1000

    Create VendingMachineData.lua

---
+++ GameData/AIMBLOX [6808416928]/VendingMachineData.lua
@@ -0,0 +1,133 @@
+--game.ReplicatedStorage.Shared.VendingMachineData.StarterPack:Destroy()
+data = {
+    ["AimlabVestNoir"] = {
+        ["Items"] = {},
+        ["Subtitle"] = "Noir",
+        ["Title"] = "Aimlab Vest",
+        ["ProductId"] = 10048066007,
+        ["Active"] = false,
+        ["Description"] = "A Noir Vest for your character!",
+        ["ImageId"] = "https://www.roblox.com/asset-thumbnail/image?assetId=10048066007&width=420&height=420&format=png",
+        ["Currency"] = "Robux"
+    },
+    ["DuckGun"] = {
+        ["Image"] = "0",
+        ["Type"] = Enum.InfoType.GamePass,
+        ["Title"] = "Weapon!",
+        ["ProductId"] = 55026855,
+        ["Price"] = 49,
+        ["Subtitle"] = "Duck Gun",
+        ["Active"] = true,
+        ["LimitedTimeOnly"] = false
+    },
+    ["XPBoost"] = {
+        ["Image"] = "0",
+        ["Type"] = Enum.InfoType.GamePass,
+        ["Title"] = "XP Boost!",
+        ["ProductId"] = 59113079,
+        ["Price"] = 199,
+        ["Subtitle"] = "+30% XP Boost",
+        ["Active"] = false,
+        ["LimitedTimeOnly"] = false
+    },
+    ["AimlabVestCarmineSparkle"] = {
+        ["Items"] = {},
+        ["Subtitle"] = "Carmine Sparkle",
+        ["Title"] = "Aimlab Vest",
+        ["ProductId"] = 10048114888,
+        ["Active"] = false,
+        ["Description"] = "A Carmine Sparkle Vest for your character!",
+        ["ImageId"] = "https://www.roblox.com/asset-thumbnail/image?assetId=10048114888&width=420&height=420&format=png",
+        ["Currency"] = "Robux"
+    },
+    ["AimlabVestRubyRed"] = {
+        ["Items"] = {},
+        ["Subtitle"] = "Ruby Red",
+        ["Title"] = "Aimlab Vest",
+        ["ProductId"] = 10048124848,
+        ["Active"] = false,
+        ["Description"] = "A Ruby Red Vest for your character!",
+        ["ImageId"] = "https://www.roblox.com/asset-thumbnail/image?assetId=10048124848&width=420&height=420&format=png",
+        ["Currency"] = "Robux"
+    },
+    ["AimlabJacketNoirNoLogo"] = {
+        ["Items"] = {},
+        ["Subtitle"] = "Noir",
+        ["Title"] = "Aimlab Jacket",
+        ["ProductId"] = 10048073359,
+        ["Active"] = false,
+        ["Description"] = "A Noir Jacket for your character!",
+        ["ImageId"] = "https://www.roblox.com/asset-thumbnail/image?assetId=10048073359&width=420&height=420&format=png",
+        ["Currency"] = "Robux"
+    },
+    ["CatmaidOutfit"] = {
+        ["Image"] = "0",
+        ["Type"] = Enum.InfoType.GamePass,
+        ["Title"] = "Outfit!",
+        ["ProductId"] = 54721234,
+        ["Price"] = 149,
+        ["Subtitle"] = "Catmaid Outfit",
+        ["Active"] = false,
+        ["LimitedTimeOnly"] = false
+    },
+    ["SamuraiPack"] = {
+        ["Image"] = "0",
+        ["Type"] = Enum.InfoType.GamePass,
+        ["Title"] = "Outifit Pack!",
+        ["ProductId"] = 68275299,
+        ["Price"] = 750,
+        ["Subtitle"] = "Samurai Outfit Pack",
+        ["Active"] = false,
+        ["LimitedTimeOnly"] = false
+    },
+    ["SDMRWolfSkullPack"] = {
+        ["Image"] = "0",
+        ["Type"] = Enum.InfoType.GamePass,
+        ["Title"] = "Weapon Pack!",
+        ["ProductId"] = 68284352,
+        ["Price"] = 300,
+        ["Subtitle"] = "SDM-R Wolf Skull Pack",
+        ["Active"] = false,
+        ["LimitedTimeOnly"] = true
+    },
+    ["AimlabSquidGameGuard"] = {
+        ["Items"] = {},
+        ["Subtitle"] = "",
+        ["Title"] = "Squid Game Guard",
+        ["ProductId"] = 7738532886,
+        ["Active"] = false,
+        ["Description"] = "A squid game guard outfit for your character!",
+        ["ImageId"] = "https://www.roblox.com/asset-thumbnail/image?assetId=7738532886&width=420&height=420&format=png",
+        ["Currency"] = "Robux"
+    },
+    ["KVImpulsePack"] = {
+        ["Image"] = "0",
+        ["Type"] = Enum.InfoType.GamePass,
+        ["Title"] = "Weapon Pack!",
+        ["ProductId"] = 68284425,
+        ["Price"] = 350,
+        ["Subtitle"] = "Kriss Vector Impulse Pack",
+        ["Active"] = false,
+        ["LimitedTimeOnly"] = false
+    },
+    ["AimlabJacketNoir"] = {
+        ["Items"] = {},
+        ["Subtitle"] = "Noir with Logo",
+        ["Title"] = "Aimlab Jacket",
+        ["ProductId"] = 10048069320,
+        ["Active"] = false,
+        ["Description"] = "A Noir Jacket with the Aim lab logo for your character!",
+        ["ImageId"] = "https://www.roblox.com/asset-thumbnail/image?assetId=10048069314&width=420&height=420&format=png",
+        ["Currency"] = "Robux"
+    },
+    ["SnakeskinPack"] = {
+        ["Image"] = "0",
+        ["Type"] = Enum.InfoType.GamePass,
+        ["Title"] = "Weapon Pack!",
+        ["ProductId"] = 81285533,
+        ["Price"] = 350,
+        ["Subtitle"] = "P90 Snakeskin Pack",
+        ["Active"] = false,
+        ["LimitedTimeOnly"] = true
+    }
+}
