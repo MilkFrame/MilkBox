d6a0161290f819e3186f6bbb74fc6f4ed579b3dc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 21:42:01 +1000

    Create DeveloperProductMetadata.lua

---
+++ GameData/AIMBLOX [6808416928]/DeveloperProductMetadata.lua
@@ -0,0 +1,13 @@
+data = {
+    ["GetCoinDataFromReward"] = "function: 0x3dc98abebb9a8d00",
+    ["Coins"] = {
+        {["ProductID"] = 1523550375, ["Reward"] = 500},
+        {["ProductID"] = 1524567021, ["BannerText"] = "Crate Take", ["Reward"] = 1000},
+        {["ProductID"] = 1523551317, ["Reward"] = 1500},
+        {["ProductID"] = 1523552134, ["BannerText"] = "Popular", ["Reward"] = 3000},
+        {["ProductID"] = 1523552628, ["Reward"] = 5000},
+        {["ProductID"] = 1523552984, ["BannerText"] = "Most Popular", ["Reward"] = 10000},
+        {["ProductID"] = 1523553375, ["Reward"] = 50000},
+        {["ProductID"] = 1523553966, ["BannerText"] = "Best Deal!", ["Reward"] = 150000}
+    }
+}
