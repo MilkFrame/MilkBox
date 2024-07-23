09ae44fa9acf3338d777ac27de7294e08ab9ad0c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 21:20:09 +1000

    Create BoostExchange.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/BoostExchange.lua
@@ -0,0 +1,26 @@
+data = {
+    ["Diamonds"] = {
+        ["_script"] = "BoostExchange | Diamonds",
+        ["_id"] = "Diamonds",
+        ["Id"] = "Diamonds",
+        ["MaxBoost"] = 5,
+        ["Icon"] = "rbxassetid://18523402234",
+        ["DisplayName"] = "Diamond Boost"
+    },
+    ["Luck"] = {
+        ["_script"] = "BoostExchange | Luck",
+        ["_id"] = "Luck",
+        ["Id"] = "Luck",
+        ["MaxBoost"] = 100,
+        ["Icon"] = "rbxassetid://15548782732",
+        ["DisplayName"] = "Egg Luck Boost"
+    },
+    ["Drops"] = {
+        ["_script"] = "BoostExchange | Drops",
+        ["_id"] = "Drops",
+        ["Id"] = "Drops",
+        ["MaxBoost"] = 15,
+        ["Icon"] = "rbxassetid://15000940153",
+        ["DisplayName"] = "Drop Chance Boost"
+    }
+}
