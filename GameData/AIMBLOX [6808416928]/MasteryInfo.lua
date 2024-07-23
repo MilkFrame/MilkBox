e255c15359833a08b61251c86ece765475d1d330
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 21:51:23 +1000

    Create MasteryInfo.lua

---
+++ GameData/AIMBLOX [6808416928]/MasteryInfo.lua
@@ -0,0 +1,24 @@
+data = {
+    ["PVP_WEAPON_LEVELS"] = {
+        {["Bonus"] = 500, ["XP"] = 25, ["Reward"] = "MCamo.LevelOne"},
+        {["XP"] = 50, ["Reward"] = "MCamo.LevelTwo"},
+        {["Bonus"] = "Duplicate", ["XP"] = 75, ["Reward"] = "MCamo.LevelThree"},
+        {["XP"] = 100, ["Reward"] = "MCamo.LevelFour"},
+        {["Bonus"] = "Duplicate", ["XP"] = 250, ["Reward"] = "MCamo.LevelFive"},
+        {["XP"] = 400, ["Reward"] = "MCamo.LevelSix"},
+        {["Bonus"] = "Duplicate", ["XP"] = 700, ["Reward"] = "MCamo.LevelSeven"},
+        {["Bonus"] = 500, ["XP"] = 1100, ["Reward"] = "MCamo.LevelEight"},
+        {["XP"] = 1650, ["Reward"] = "MCamo.LevelNine"},
+        {["Bonus"] = 1000, ["XP"] = 2500, ["Reward"] = "MCamo.LevelTen"},
+        {["Bonus"] = "Duplicate", ["XP"] = 2750, ["Reward"] = "MCamo.LevelEleven"},
+        {["XP"] = 3000, ["Reward"] = "MCamo.LevelTwelve"},
+        {["Bonus"] = "Duplicate", ["XP"] = 3250, ["Reward"] = "MCamo.LevelThirteen"},
+        {["XP"] = 3500, ["Reward"] = "MCamo.LevelFourteen"},
+        {["Bonus"] = 500, ["XP"] = 3750, ["Reward"] = "MCamo.LevelFifteen"},
+        {["XP"] = 4000, ["Reward"] = "MCamo.LevelSixteen"},
+        {["XP"] = 4250, ["Reward"] = "MCamo.LevelSeventeen"},
+        {["Bonus"] = 500, ["XP"] = 4500, ["Reward"] = "MCamo.LevelEighteen"},
+        {["XP"] = 4750, ["Reward"] = "MCamo.LevelNineteen"},
+        {["Bonus"] = 2000, ["XP"] = 5000, ["Reward"] = "MCamo.LevelTwenty"}
+    }
+}
