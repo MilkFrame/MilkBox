acfa4b9a8aaf7585de2ef363445ad389103385f0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 22:12:23 +1000

    Create Advent23.lua

---
+++ GameData/Arsenal [286090429]/Advent23.lua
@@ -0,0 +1,14 @@
+data = {
+    ["14"] = {"Cards", "Winter Storm"},
+    ["15"] = {"KillEffects", "Wrapped Up"},
+    ["16"] = {"Skins", "Arctic Occupier"},
+    ["17"] = {"WeaponSkins", "Arctic OP"},
+    ["18"] = {"Cards", "Candy Crux"},
+    ["19"] = {"KillEffects", "Reindeer Rider"},
+    ["20"] = {"Skins", "Night Cap"},
+    ["21"] = {"WeaponSkins", "Winter OP"},
+    ["22"] = {"Cards", "Yards"},
+    ["23"] = {"Skins", "Holiday Performer"}
+    ["24"] = {"Kits", "Cryo Spell Unusual Kit"},
+    ["25"] = {"Kits", "Cryo Spell Unusual Kit"},
+}
