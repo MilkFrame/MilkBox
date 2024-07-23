326e1cbcc3832c3762f5dbb792872f5c974c640f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 21:20:39 +1000

    Create Raffles.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/Raffles.lua
@@ -0,0 +1,20 @@
+data = {
+    ["MillionaireRaffle"] = {
+        ["_id"] = "MillionaireRaffle",
+        ["HerokuInternalId"] = "MillionaireRaffle",
+        ["MaxTickets"] = 2500,
+        ["Delta"] = 600,
+        ["DisplayName"] = "Millionaire Raffle",
+        ["_script"] = "MillionaireRaffle",
+        ["TicketItem"] = {["_data"] = {["id"] = "Millionaire Ticket"}},
+        ["WinnersPerDelta"] = 5,
+        ["Rewards"] = {
+            {["Item"] = {["_data"] = {["id"] = "Huge Elegant Eagle"}}, ["Weight"] = 857},
+            {["Item"] = {["_data"] = {["id"] = "Huge Elegant Eagle", ["pt"] = 1}}, ["Weight"] = 100},
+            {["Item"] = {["_data"] = {["id"] = "Huge Elegant Eagle", ["pt"] = 2}}, ["Weight"] = 20},
+            {["Item"] = {["_data"] = {["id"] = "Huge Elegant Eagle", ["sh"] = true}}, ["Weight"] = 20},
+            {["Item"] = {["_data"] = {["id"] = "Huge Elegant Eagle", ["sh"] = true, ["pt"] = 1}}, ["Weight"] = 2},
+            {["Item"] = {["_data"] = {["id"] = "Huge Elegant Eagle", ["sh"] = true, ["pt"] = 2}}, ["Weight"] = 1}
+        }
+    }
+}
