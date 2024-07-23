9dcf8946edac0805845d788415f6d78e40042458
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 21:38:54 +1000

    Create MatchmakingLibrary.lua

---
+++ GameData/RIVALS [17625359962]/MatchmakingLibrary.lua
@@ -0,0 +1,61 @@
+data = {
+    ["QueueOrder"] = {"1v1_beginner", "1v1", "2v2", "3v3", "4v4", "5v5", "1v1v1", "2v2v2"},
+    ["Queues"] = {
+        ["2v2v2"] = {
+            ["PlayersPerTeam"] = 2,
+            ["NumTeams"] = 3,
+            ["Description"] = "Duel",
+            ["Image"] = "rbxassetid://18526010596",
+            ["DisplayName"] = "2v2v2"
+        },
+        ["1v1v1"] = {
+            ["PlayersPerTeam"] = 1,
+            ["NumTeams"] = 3,
+            ["Description"] = "Duel",
+            ["Image"] = "rbxassetid://18526010596",
+            ["DisplayName"] = "1v1v1"
+        },
+        ["3v3"] = {
+            ["PlayersPerTeam"] = 3,
+            ["NumTeams"] = 2,
+            ["Description"] = "Duel",
+            ["Image"] = "rbxassetid://18525954682",
+            ["DisplayName"] = "3v3"
+        },
+        ["1v1"] = {
+            ["PlayersPerTeam"] = 1,
+            ["NumTeams"] = 2,
+            ["Description"] = "Duel",
+            ["Image"] = "rbxassetid://18525954682",
+            ["DisplayName"] = "1v1"
+        },
+        ["4v4"] = {
+            ["PlayersPerTeam"] = 4,
+            ["NumTeams"] = 2,
+            ["Description"] = "Duel",
+            ["Image"] = "rbxassetid://18525954682",
+            ["DisplayName"] = "4v4"
+        },
+        ["5v5"] = {
+            ["PlayersPerTeam"] = 5,
+            ["NumTeams"] = 2,
+            ["Description"] = "Duel",
+            ["Image"] = "rbxassetid://18525954682",
+            ["DisplayName"] = "5v5"
+        },
+        ["1v1_beginner"] = {
+            ["PlayersPerTeam"] = 1,
+            ["NumTeams"] = 2,
+            ["Description"] = "Only available until you reach 5 wins!",
+            ["Image"] = "rbxassetid://18523763487",
+            ["DisplayName"] = "Beginner 1v1"
+        },
+        ["2v2"] = {
+            ["PlayersPerTeam"] = 2,
+            ["NumTeams"] = 2,
+            ["Description"] = "Duel",
+            ["Image"] = "rbxassetid://18525954682",
+            ["DisplayName"] = "2v2"
+        }
+    }
+}
