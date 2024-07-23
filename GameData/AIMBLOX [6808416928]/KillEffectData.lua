cb4eb8d64e59c9a1b211ed86607e4613771de1cc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 22:07:18 +1000

    Create KillEffectData.lua

---
+++ GameData/AIMBLOX [6808416928]/KillEffectData.lua
@@ -0,0 +1,230 @@
+data = {
+    ["BoomBoom"] = {
+        ["DisplayImage"] = "rbxassetid://16100032448",
+        ["Description"] = "They are Doomed",
+        ["Category"] = "KillEffect",
+        ["ID"] = "BoomBoom",
+        ["Price"] = 25000,
+        ["StarRating"] = 5,
+        ["DisplayName"] = "8Bit Kaboom"
+    },
+    ["Ghost"] = {
+        ["DisplayImage"] = "rbxassetid://9253833806",
+        ["Description"] = "Unlike Casper, this one wasn't friendly",
+        ["Category"] = "KillEffect",
+        ["ID"] = "Ghost",
+        ["Price"] = 10000,
+        ["StarRating"] = 2,
+        ["DisplayName"] = "Ghost"
+    },
+    ["Campfire"] = {
+        ["DisplayImage"] = "rbxassetid://18444777377",
+        ["Description"] = "Camp Aimblox Fire!",
+        ["Category"] = "KillEffect",
+        ["ID"] = "Campfire",
+        ["Price"] = 20000,
+        ["StarRating"] = 5,
+        ["DisplayName"] = "Campfire"
+    },
+    ["GraveStoneBunny"] = {
+        ["DisplayImage"] = "rbxassetid://15046051142",
+        ["Description"] = "Bunny will rise again!",
+        ["Category"] = "KillEffect",
+        ["ID"] = "GraveStoneBunny",
+        ["Price"] = 20000,
+        ["StarRating"] = 5,
+        ["DisplayName"] = "Bunny's Grave Stone"
+    },
+    ["Swallow"] = {
+        ["DisplayImage"] = "rbxassetid://9253838253",
+        ["Description"] = "Swallows the player up",
+        ["Category"] = "KillEffect",
+        ["ID"] = "Swallow",
+        ["Price"] = 10000,
+        ["StarRating"] = 5,
+        ["DisplayName"] = "Swallow"
+    },
+    ["Lightning"] = {
+        ["DisplayImage"] = "rbxassetid://9253835602",
+        ["Description"] = "Summon Thor's hammer",
+        ["Category"] = "KillEffect",
+        ["ID"] = "Lightning",
+        ["Price"] = 10000,
+        ["StarRating"] = 4,
+        ["DisplayName"] = "Lightning"
+    },
+    ["SnowBurst"] = {
+        ["DisplayImage"] = "rbxassetid://15616833948",
+        ["Description"] = "Where'd you go?",
+        ["Category"] = "KillEffect",
+        ["ID"] = "SnowBurst",
+        ["Price"] = 20000,
+        ["StarRating"] = 5,
+        ["DisplayName"] = "Snow Burst"
+    },
+    ["YouDied"] = {
+        ["DisplayImage"] = "rbxassetid://9253838790",
+        ["Description"] = "Like that one hard game",
+        ["Category"] = "KillEffect",
+        ["ID"] = "YouDied",
+        ["Price"] = 10000,
+        ["StarRating"] = 5,
+        ["DisplayName"] = "You Died"
+    },
+    ["GiftBurst"] = {
+        ["DisplayImage"] = "rbxassetid://15686185060",
+        ["Description"] = "Where'd you go?",
+        ["Category"] = "KillEffect",
+        ["ID"] = "GiftBurst",
+        ["Price"] = 10000,
+        ["StarRating"] = 5,
+        ["DisplayName"] = "Gift Burst"
+    },
+    ["HavocGlitch"] = {
+        ["DisplayImage"] = "rbxassetid://14882832464",
+        ["Description"] = "You will assimilate",
+        ["Category"] = "KillEffect",
+        ["ID"] = "HavocGlitch",
+        ["Price"] = 10000,
+        ["StarRating"] = 5,
+        ["DisplayName"] = "Havoc Glitch"
+    },
+    ["Leaves"] = {
+        ["DisplayImage"] = "rbxassetid://15310550523",
+        ["Description"] = "Where'd you go?",
+        ["Category"] = "KillEffect",
+        ["ID"] = "Leaves",
+        ["Price"] = 15000,
+        ["StarRating"] = 5,
+        ["DisplayName"] = "Autumn Leaves"
+    },
+    ["Sparks"] = {
+        ["DisplayImage"] = "rbxassetid://16567481525",
+        ["Price"] = 25000,
+        ["Description"] = "Where'd you go?",
+        ["Category"] = "KillEffect",
+        ["ID"] = "Sparks",
+        ["StarRating"] = 5,
+        ["Rarity"] = 5,
+        ["DisplayName"] = "Sparks"
+    },
+    ["EggBurst"] = {
+        ["DisplayImage"] = "rbxassetid://16894140484",
+        ["Description"] = "Egg them!",
+        ["Category"] = "KillEffect",
+        ["ID"] = "EggBurst",
+        ["Price"] = 25000,
+        ["StarRating"] = 5,
+        ["DisplayName"] = "Egg Burst"
+    },
+    ["Incinerate"] = {
+        ["DisplayImage"] = "rbxassetid://9253834507",
+        ["Description"] = "Burn 'em alive",
+        ["Category"] = "KillEffect",
+        ["ID"] = "Incinerate",
+        ["Price"] = 10000,
+        ["StarRating"] = 3,
+        ["DisplayName"] = "Incinerate"
+    },
+    ["GraveStoneDuck"] = {
+        ["DisplayImage"] = "rbxassetid://15046052291",
+        ["Description"] = "He was a good duck",
+        ["Category"] = "KillEffect",
+        ["ID"] = "GraveStoneDuck",
+        ["Price"] = 20000,
+        ["StarRating"] = 5,
+        ["DisplayName"] = "Ducky's Grave Stone"
+    },
+    ["Spin"] = {
+        ["DisplayImage"] = "rbxassetid://9253836803",
+        ["Description"] = "TPosing up into the air",
+        ["Category"] = "KillEffect",
+        ["ID"] = "Spin",
+        ["Price"] = 10000,
+        ["StarRating"] = 2,
+        ["DisplayName"] = "Spin"
+    },
+    ["Freezed"] = {
+        ["DisplayImage"] = "rbxassetid://18249046460",
+        ["Description"] = "",
+        ["Category"] = "KillEffect",
+        ["ID"] = "Freezed",
+        ["Price"] = 10000,
+        ["StarRating"] = 3,
+        ["DisplayName"] = "Freezed"
+    },
+    ["Berserked"] = {
+        ["DisplayImage"] = "rbxassetid://17684905699",
+        ["Description"] = "",
+        ["Category"] = "KillEffect",
+        ["ID"] = "Berserked",
+        ["Price"] = 10000,
+        ["StarRating"] = 3,
+        ["DisplayName"] = "Berserked"
+    },
+    ["TreeStump"] = {
+        ["DisplayImage"] = "rbxassetid://15393325173",
+        ["Description"] = "Stumpy the Stump Stump",
+        ["Category"] = "KillEffect",
+        ["ID"] = "TreeStump",
+        ["Price"] = 10000,
+        ["StarRating"] = 5,
+        ["DisplayName"] = "Tree Stump"
+    },
+    ["Default"] = {
+        ["DisplayImage"] = "",
+        ["Description"] = "Changes your kill effect back to the default ragdoll",
+        ["Category"] = "KillEffect",
+        ["ID"] = "Default",
+        ["Locked"] = true,
+        ["Default"] = true,
+        ["DisplayName"] = "Default"
+    },
+    ["Break"] = {
+        ["DisplayImage"] = "rbxassetid://9253832178",
+        ["Description"] = "Character falls apart",
+        ["Category"] = "KillEffect",
+        ["ID"] = "Break",
+        ["Price"] = 10000,
+        ["StarRating"] = 2,
+        ["DisplayName"] = "Break"
+    },
+    ["FumoMrAimlabBurst"] = {
+        ["DisplayImage"] = "rbxassetid://15949787237",
+        ["Description"] = "Where'd you go?",
+        ["Category"] = "KillEffect",
+        ["ID"] = "FumoMrAimlabBurst",
+        ["Price"] = 25000,
+        ["StarRating"] = 5,
+        ["DisplayName"] = "Mr. Aimlabs Fumo Burst"
+    },
+    ["FumoLiNaBurst"] = {
+        ["DisplayImage"] = "rbxassetid://15949430197",
+        ["Price"] = 25000,
+        ["Description"] = "Where'd you go?",
+        ["Category"] = "KillEffect",
+        ["ID"] = "FumoLiNaBurst",
+        ["StarRating"] = 5,
+        ["Rarity"] = 5,
+        ["DisplayName"] = "Li Na Fumo Burst"
+    },
+    ["Disintegrate"] = {
+        ["DisplayImage"] = "rbxassetid://9253833038",
+        ["Description"] = "Disappear like that purple guy's snap",
+        ["Category"] = "KillEffect",
+        ["ID"] = "Disintegrate",
+        ["Price"] = 10000,
+        ["StarRating"] = 4,
+        ["DisplayName"] = "Disintegrate"
+    },
+    ["FumoAimeeBurst"] = {
+        ["DisplayImage"] = "rbxassetid://15949788737",
+        ["Price"] = 25000,
+        ["Description"] = "Where'd you go?",
+        ["Category"] = "KillEffect",
+        ["ID"] = "FumoAimeeBurst",
+        ["StarRating"] = 5,
+        ["Rarity"] = 5,
+        ["DisplayName"] = "Aimee Fumo Burst"
+    }
+}
