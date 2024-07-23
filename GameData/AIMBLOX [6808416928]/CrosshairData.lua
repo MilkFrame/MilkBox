ab9d574f7d29f7d403c6e7dadd52acb092ac8b24
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 22:06:40 +1000

    Create CrosshairData.lua

---
+++ GameData/AIMBLOX [6808416928]/CrosshairData.lua
@@ -0,0 +1,49 @@
+data = {
+    ["SmallDot"] = {
+        ["Enabled"] = true,
+        ["Transparency"] = 0,
+        ["Id"] = "6971191916",
+        ["EnableColor"] = true,
+        ["Size"] = 96
+    },
+    ["SquareDot"] = {
+        ["Enabled"] = true,
+        ["Transparency"] = 0,
+        ["Id"] = "6972613669",
+        ["EnableColor"] = true,
+        ["Size"] = 32
+    },
+    ["CrossDotRotatedCircle"] = {
+        ["Enabled"] = true,
+        ["Transparency"] = 0,
+        ["Id"] = "6972844467",
+        ["EnableColor"] = true,
+        ["Size"] = 32
+    },
+    ["Range"] = {["Enabled"] = true, ["Transparency"] = 0, ["Id"] = "6941962099", ["EnableColor"] = true, ["Size"] = 96},
+    ["DotVerySmall"] = {
+        ["Enabled"] = true,
+        ["Transparency"] = 0,
+        ["Id"] = "6971585733",
+        ["EnableColor"] = true,
+        ["Size"] = 96
+    },
+    ["Default"] = {["Enabled"] = false, ["Transparency"] = 1, ["Id"] = "", ["EnableColor"] = true, ["Size"] = 0},
+    ["Cross"] = {["Enabled"] = true, ["Transparency"] = 0, ["Id"] = "6972658732", ["EnableColor"] = true, ["Size"] = 32},
+    ["Dot"] = {["Enabled"] = true, ["Transparency"] = 0, ["Id"] = "6941790058", ["EnableColor"] = true, ["Size"] = 196},
+    ["CrossDot"] = {
+        ["Enabled"] = true,
+        ["Transparency"] = 0,
+        ["Id"] = "6972596667",
+        ["EnableColor"] = true,
+        ["Size"] = 32
+    },
+    ["Bow"] = {["Enabled"] = true, ["Transparency"] = 0, ["Id"] = "6941789399", ["EnableColor"] = true, ["Size"] = 128},
+    ["CrossDotRotated"] = {
+        ["Enabled"] = true,
+        ["Transparency"] = 0,
+        ["Id"] = "6972582335",
+        ["EnableColor"] = true,
+        ["Size"] = 32
+    }
+}
