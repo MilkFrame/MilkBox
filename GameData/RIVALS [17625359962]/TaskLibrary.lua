c5ecb134ea40e88f3f6bbae92bc3b0276cd143cf
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 21:37:33 +1000

    Create TaskLibrary.lua

---
+++ GameData/RIVALS [17625359962]/TaskLibrary.lua
@@ -0,0 +1,113 @@
+data = {
+    ["Info"] = {
+        ["Beginner5"] = {
+            ["Title"] = "Dash with Scythe (MB2 / Aim)",
+            ["Icon"] = "rbxassetid://16828140099",
+            ["Goal"] = 3,
+            ["Reward"] = {["Name"] = "Key", ["Quantity"] = 2}
+        },
+        ["Beginner3"] = {
+            ["Title"] = "Slide by running and then crouching",
+            ["Icon"] = "rbxassetid://17735857643",
+            ["Goal"] = 3,
+            ["Reward"] = {["Name"] = "Key", ["Quantity"] = 1}
+        },
+        ["Beginner4"] = {
+            ["Title"] = "Equip your Melee",
+            ["Icon"] = "rbxassetid://17735878660",
+            ["Goal"] = 1,
+            ["Reward"] = {["Name"] = "Key", ["Quantity"] = 2}
+        },
+        ["RepeatableStreak"] = {
+            ["Title"] = "Win duels in a row",
+            ["Icon"] = "rbxassetid://18151049137",
+            ["Goal"] = 10,
+            ["Reward"] = {["Quantity"] = 1, ["Name"] = "Fire", ["Weapon"] = "IsRandom"}
+        },
+        ["RepeatableWins"] = {
+            ["Title"] = "Win duels",
+            ["Icon"] = "rbxassetid://17735868621",
+            ["Goal"] = 25,
+            ["Reward"] = {["Quantity"] = 1, ["Name"] = "Trophy", ["Weapon"] = "IsRandom"}
+        },
+        ["RepeatableStreak2"] = {
+            ["Title"] = "Win duels in a row",
+            ["Icon"] = "rbxassetid://18151049137",
+            ["Goal"] = 100,
+            ["Reward"] = {["Quantity"] = 1, ["Name"] = "Blaze", ["Weapon"] = "IsRandom"}
+        },
+        ["Beginner9"] = {
+            ["Title"] = "Win a duel",
+            ["Icon"] = "rbxassetid://17735868621",
+            ["Goal"] = 1,
+            ["Reward"] = {["Name"] = "Key", ["Quantity"] = 3}
+        },
+        ["RepeatableCore2"] = {
+            ["Title"] = "Eliminations",
+            ["Icon"] = "rbxassetid://17735857264",
+            ["Goal"] = 40,
+            ["Reward"] = {["Name"] = "Key", ["Quantity"] = 2}
+        },
+        ["Beginner8"] = {
+            ["Title"] = "Eliminations",
+            ["Icon"] = "rbxassetid://17735857264",
+            ["Goal"] = 5,
+            ["Reward"] = {["Name"] = "Key", ["Quantity"] = 3}
+        },
+        ["Core3"] = {
+            ["Title"] = "Win a duel",
+            ["Icon"] = "rbxassetid://17735868621",
+            ["Goal"] = 1,
+            ["Reward"] = {["Name"] = "Key", ["Quantity"] = 3}
+        },
+        ["Core1"] = {
+            ["Title"] = "Play duels",
+            ["Icon"] = "rbxassetid://17738769051",
+            ["Goal"] = 3,
+            ["Reward"] = {["Name"] = "Key", ["Quantity"] = 1}
+        },
+        ["Core2"] = {
+            ["Title"] = "Eliminations",
+            ["Icon"] = "rbxassetid://17735857264",
+            ["Goal"] = 10,
+            ["Reward"] = {["Name"] = "Key", ["Quantity"] = 2}
+        },
+        ["Beginner1"] = {
+            ["Title"] = "Go to the Shooting Range",
+            ["Icon"] = "rbxassetid://17539215512",
+            ["Goal"] = 1,
+            ["Reward"] = {["Name"] = "Key", ["Quantity"] = 1}
+        },
+        ["Beginner6"] = {
+            ["Title"] = "Throw a Grenade",
+            ["Icon"] = "rbxassetid://17513805716",
+            ["Goal"] = 1,
+            ["Reward"] = {["Name"] = "Key", ["Quantity"] = 2}
+        },
+        ["Beginner2"] = {
+            ["Title"] = "Break targets",
+            ["Icon"] = "rbxassetid://17738770088",
+            ["Goal"] = 5,
+            ["Reward"] = {["Name"] = "Key", ["Quantity"] = 1}
+        },
+        ["Beginner7"] = {
+            ["Title"] = "Play a duel",
+            ["Icon"] = "rbxassetid://17738769051",
+            ["Goal"] = 1,
+            ["Reward"] = {["Name"] = "Key", ["Quantity"] = 3}
+        },
+        ["RepeatableCore3"] = {
+            ["Title"] = "Win duels",
+            ["Icon"] = "rbxassetid://17735868621",
+            ["Goal"] = 12,
+            ["Reward"] = {["Name"] = "Key", ["Quantity"] = 3}
+        },
+        ["RepeatableCore1"] = {
+            ["Title"] = "Play duels",
+            ["Icon"] = "rbxassetid://17738769051",
+            ["Goal"] = 12,
+            ["Reward"] = {["Name"] = "Key", ["Quantity"] = 1}
+        }
+    },
+    ["NUM_BEGINNER_TASKS"] = 3
+}
