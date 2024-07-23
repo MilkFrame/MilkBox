1b6054175996431a15cbeb9ae225c268c23b799e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-23 22:06:51 +1000

    Create EasterEggQuests.lua

---
+++ GameData/AIMBLOX [6808416928]/EasterEggQuests.lua
@@ -0,0 +1,47 @@
+data = {
+    ["Egg"] = {
+        ["RequiredActivations"] = 12,
+        ["ObjectDisplayName"] = "<font color='#f2c9f7'>Easter eggs</font>",
+        ["RewardText"] = "Quest complete - Go into the quest menu to claim your <font color='#f2c9f7'>Bunny Outfit</font>!"
+    },
+    ["HavocPoster"] = {
+        ["RequiredActivations"] = 15,
+        ["ObjectDisplayName"] = "<font color='#FFA500'>posters</font>",
+        ["RewardText"] = "Quest complete - Go into the quest menu to claim your <font color='#FFA500'>Havoc Director Tyro outfit</font>!"
+    },
+    ["Doll"] = {
+        ["RequiredActivations"] = 5,
+        ["ObjectDisplayName"] = "<font color='#fff01d'>dolls</font>",
+        ["RewardText"] = "Quest complete - Go into the quest menu to claim your <font color='#fff01d'>Doll Gun</font>!"
+    },
+    ["Pumpkin2023"] = {
+        ["RequiredActivations"] = 20,
+        ["ObjectDisplayName"] = "<font color='#FFA500'>pumpkins</font>",
+        ["RewardText"] = "Quest complete - Go into the quest menu to claim your <font color='#FFA500'>Duck Varsity outfit</font>!"
+    },
+    ["HiddenTarget"] = {
+        ["RequiredActivations"] = 1,
+        ["ObjectDisplayName"] = "<font color='#fff01d'>Hidden Target</font>",
+        ["RewardText"] = "You found it!! - Go into the quest menu to claim your <font color='#fff01d'>Mystery Gift!</font>!"
+    },
+    ["Catmaid"] = {
+        ["RequiredActivations"] = 7,
+        ["ObjectDisplayName"] = "<font color='#fff01d'>Catmaid Coins</font>",
+        ["RewardText"] = "Quest complete - Go into the quest menu to claim your <font color='#fff01d'>Catmaid outfit</font>!"
+    },
+    ["Duck"] = {
+        ["RequiredActivations"] = 15,
+        ["ObjectDisplayName"] = "<font color='#fff01d'>ducks</font>",
+        ["RewardText"] = "Quest complete - Go into the quest menu to claim your <font color='#fff01d'>duck gun</font>!"
+    },
+    ["GingerbreadMan"] = {
+        ["RequiredActivations"] = 20,
+        ["ObjectDisplayName"] = "<font color='#fff01d'>gingerbread men</font>",
+        ["RewardText"] = "Quest complete - Go into the quest menu to claim your <font color='#fff01d'>Reward</font>!"
+    },
+    ["Pumpkin"] = {
+        ["RequiredActivations"] = 20,
+        ["ObjectDisplayName"] = "<font color='#FFA500'>pumpkins</font>",
+        ["RewardText"] = "Quest complete - Go into the quest menu to claim your <font color='#FFA500'>Duck Varsity outfit</font>!"
+    }
+}
