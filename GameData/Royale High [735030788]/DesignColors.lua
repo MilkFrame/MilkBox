2b818cc597778625e76cb42e7c1e0a9f6a6d6af2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-24 22:42:32 +1000

    Create DesignColors.lua

---
+++ GameData/Royale High [735030788]/DesignColors.lua
@@ -0,0 +1,285 @@
+--game.ReplicatedStorage.GetDesignColors:InvokeServer()
+data = {
+    {
+        ["Offset"] = 1,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Red", "Luxury", "Free Sparkles"},
+        ["Id"] = "8333466799",
+        ["Name"] = "Red",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 2,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Blue", "Luxury", "Free Sparkles"},
+        ["Id"] = "8333461958",
+        ["Name"] = "Blue",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 3,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Green", "Luxury", "Free Sparkles"},
+        ["Id"] = "8333465241",
+        ["Name"] = "Green",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 4,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Yellow", "Luxury", "Free Sparkles"},
+        ["Id"] = "8333464858",
+        ["Name"] = "Yellow",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 5,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Blue", "Luxury", "Free Sparkles"},
+        ["Id"] = "8333461457",
+        ["Name"] = "Aqua",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 6,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Orange", "Luxury", "Free Sparkles"},
+        ["Id"] = "8333465840",
+        ["Name"] = "Orange",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 7,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Purple", "Luxury", "Free Sparkles"},
+        ["Id"] = "8333466121",
+        ["Name"] = "Pink",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 8,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Pink", "Luxury", "Free Sparkles"},
+        ["Id"] = "8333759200",
+        ["Name"] = "Hot Pink",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 9,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Purple", "Luxury", "Free Sparkles"},
+        ["Id"] = "8333464506",
+        ["Name"] = "Purple",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 10,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Black", "Free Sparkles"},
+        ["Id"] = "8333461642",
+        ["Name"] = "Black",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 11,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"White", "Free Sparkles"},
+        ["Id"] = "8333467717",
+        ["Name"] = "White",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 12,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Brown", "Luxury", "Free Sparkles"},
+        ["Id"] = "8333462356",
+        ["Name"] = "Brown",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 13,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Blue", "Luxury", "Free Sparkles"},
+        ["Id"] = "8333463329",
+        ["Name"] = "darkblue1",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 14,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"White", "Luxury", "Free Sparkles"},
+        ["Id"] = "8337191067",
+        ["Name"] = "silverpink",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 15,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Green", "Luxury", "Free Sparkles"},
+        ["Id"] = "8333463071",
+        ["Name"] = "mint",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 16,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Purple", "Luxury", "Free Sparkles"},
+        ["Id"] = "8337222712",
+        ["Name"] = "lavender",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 17,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Pink", "Luxury", "Free Sparkles"},
+        ["Id"] = "8333463961",
+        ["Name"] = "lightpink",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 18,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Pink", "Luxury", "Free Sparkles"},
+        ["Id"] = "8333467054",
+        ["Name"] = "pinkishred",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 19,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Pink", "Purple", "Luxury", "Free Sparkles"},
+        ["Id"] = "8333465581",
+        ["Name"] = "lightpink2",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 20,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Purple", "Luxury", "Free Sparkles"},
+        ["Id"] = "8333463667",
+        ["Name"] = "grape",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 21,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Pink", "Luxury", "Free Sparkles"},
+        ["Id"] = "8333463506",
+        ["Name"] = "fuschia",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 22,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Pastel", "Flowering", "Free Sparkles", "Multicolored"},
+        ["Id"] = "8333466450",
+        ["Name"] = "multi",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 23,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Blue", "Luxury", "Glitterfrost", "Free Sparkles"},
+        ["Id"] = "8333464263",
+        ["Name"] = "blueandwhite",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 24,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Blue", "Luxury", "Free Sparkles"},
+        ["Id"] = "8337292927",
+        ["Name"] = "perfectshadeofdarkblue",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 25,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Pink", "Luxury", "Free Sparkles"},
+        ["Id"] = "8337855337",
+        ["Name"] = "chocospink",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 26,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Pink", "Luxury", "Free Sparkles"},
+        ["Id"] = "8337789262",
+        ["Name"] = "ballerinapink",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 27,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Green", "Luxury", "Free Sparkles"},
+        ["Id"] = "8337787147",
+        ["Name"] = "chocosmint",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 28,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"Yellow", "Luxury", "Free Sparkles"},
+        ["Id"] = "8337790997",
+        ["Name"] = "chocosgold",
+        ["Credits"] = "ixChoco"
+    },
+    {
+        ["Offset"] = 29,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"White", "Trendy"},
+        ["Id"] = "18161070764",
+        ["Name"] = "2024July1",
+        ["Pass"] = "FabricPass",
+        ["Credits"] = "senzie (senr4wr)"
+    },
+    {
+        ["Offset"] = 30,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"White", "Trendy"},
+        ["Id"] = "18160824162",
+        ["Name"] = "2024July2",
+        ["Pass"] = "FabricPass",
+        ["Credits"] = "senzie (senr4wr)"
+    },
+    {
+        ["Offset"] = 31,
+        ["Type"] = "Texture",
+        ["Colors"] = {Color3.fromRGB(248, 248, 248), Color3.fromRGB(248, 248, 248)},
+        ["Tags"] = {"White", "Trendy"},
+        ["Id"] = "18160827985",
+        ["Name"] = "2024July3",
+        ["Pass"] = "FabricPass",
+        ["Credits"] = "senzie (senr4wr)"
+    }
+}
