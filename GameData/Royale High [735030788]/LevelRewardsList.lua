935dc5be74937492562f3d1f422ca4f91cf1e6bb
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-24 22:39:31 +1000

    Create LevelRewardsList.lua

---
+++ GameData/Royale High [735030788]/LevelRewardsList.lua
@@ -0,0 +1,377 @@
+data = {
+    {
+        ["LevelRewardTitle"] = "Friend Finder",
+        ["Name"] = "Friend Finder",
+        ["Description"] = "A cute friend bubble Location Indicator so you never lose your friend again! Turn it off in Settings when playing Hide N Seek!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 5
+    },
+    {
+        ["LevelRewardTitle"] = "Locker Makeover!",
+        ["Name"] = "Locker Makeover!",
+        ["Description"] = "This reward unlocks paint for the inside of your door, walls, and shelves in your locker!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 10
+    },
+    {
+        ["LevelRewardTitle"] = "Pose Perfection",
+        ["Name"] = "Pose Perfection",
+        ["Description"] = "Choose from an array of cute animations when you sit!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 15
+    },
+    {
+        ["LevelRewardTitle"] = "Quest Hunter - Quest Slot #2",
+        ["Name"] = "QuestSlot2",
+        ["Description"] = "Unlocks an additional Quest Slot! Multitask multiple quests at once, or pick only your fav ones!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 20
+    },
+    {
+        ["LevelRewardTitle"] = "Dream Diary - Extra Notebooks! #2 and #3",
+        ["Name"] = "DiarySlots2and3",
+        ["Description"] = "2 Extra Save Slots for your Diary!",
+        ["Type"] = "OldLevels",
+        ["LevelRequirement"] = 25
+    },
+    {
+        ["LevelRewardTitle"] = "Cute Locker! Save Slots #2 and #3",
+        ["Name"] = "LockerSlots2and3",
+        ["Description"] = "Unlocks 2 new Save Slots to save more Locker Designs!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 30
+    },
+    {
+        ["LevelRewardTitle"] = "Art Studio Access!",
+        ["Name"] = "Art Studio Access!",
+        ["Description"] = "Gains entry to the prestigious Art Studio, where artists practice their drawing skills, show off their talent, and earn EXP!",
+        ["Purchasable"] = false,
+        ["Type"] = "OldLevels",
+        ["LevelRequirement"] = 25
+    },
+    {
+        ["LevelRewardTitle"] = "Elemental Studies - New Magical Element! Book 2",
+        ["Name"] = "Elemental Studies - New Magical Element! Book 2",
+        ["Description"] = "You can now study and practice a new element! Go to the Enchantraverse to begin your new magical pursuit!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 50
+    },
+    {
+        ["LevelRewardTitle"] = "Dream Diary - Extra Notebooks! #4 and #5",
+        ["Name"] = "DiarySlots4and5",
+        ["Requires"] = "DiarySlots2and3",
+        ["Description"] = "2 Extra Save Slots for your Diary!",
+        ["Type"] = "OldLevels",
+        ["LevelRequirement"] = 60
+    },
+    {
+        ["LevelRewardTitle"] = "Cute Locker! Save Slots #4 and #5",
+        ["Name"] = "LockerSlots4and5",
+        ["Requires"] = "LockerSlots2and3",
+        ["Description"] = "Unlocks 2 new Save Slots to save more Locker Designs!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 65
+    },
+    {
+        ["LevelRewardTitle"] = "Vacation Mode! Smart Streak Saver",
+        ["Name"] = "Vacation Mode! Smart Streak Saver",
+        ["Description"] = "Inform the Front Desk when you are going on vacation IRL, to pause your login streak bonus timer while you are away!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 70
+    },
+    {
+        ["LevelRewardTitle"] = "Trading Hub!",
+        ["Name"] = "Trading Hub!",
+        ["Description"] = "Unlocks access to the Trading Hub, where out-of-this-world trades of goods and treasures take place!",
+        ["Purchasable"] = false,
+        ["Type"] = "OldLevels",
+        ["LevelRequirement"] = 75
+    },
+    {
+        ["LevelRewardTitle"] = "Dream Diary - Extra Notebooks! #6 and #7",
+        ["Name"] = "DiarySlots6and7",
+        ["Requires"] = "DiarySlots4and5",
+        ["Description"] = "2 Extra Save Slots for your Diary!",
+        ["Type"] = "OldLevels",
+        ["LevelRequirement"] = 90
+    },
+    {
+        ["LevelRewardTitle"] = "Cashout Premium! Tidy Textbooks",
+        ["Name"] = "Cashout Premium! Tidy Textbooks",
+        ["Description"] = "Unlocking Cashout Premium allows you to play multiple times to cash out your preferred score for Tidy Textbooks!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 100
+    },
+    {
+        ["LevelRewardTitle"] = "Elemental Studies - New Magical Element! Book 3",
+        ["Name"] = "Elemental Studies - New Magical Element! Book 3",
+        ["Requires"] = "Elemental Studies - New Magical Element! Book 2",
+        ["Description"] = "You can now study and practice a new element! Go to the Enchantraverse to begin your new magical pursuit!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 100
+    },
+    {
+        ["LevelRewardTitle"] = "Cute Locker! Save Slots #6 and #7",
+        ["Name"] = "LockerSlots6and7",
+        ["Requires"] = "LockerSlots4and5",
+        ["Description"] = "Unlocks 2 new Save Slots to save more Locker Designs!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 110
+    },
+    {
+        ["LevelRewardTitle"] = "Dream Diary - Extra Notebooks! #8 and #9",
+        ["Name"] = "DiarySlots8and9",
+        ["Requires"] = "DiarySlots6and7",
+        ["Description"] = "2 Extra Save Slots for your Diary!",
+        ["Type"] = "OldLevels",
+        ["LevelRequirement"] = 130
+    },
+    {
+        ["LevelRewardTitle"] = "Cute Locker! Save Slots #8 and #9",
+        ["Name"] = "LockerSlots8and9",
+        ["Requires"] = "LockerSlots6and7",
+        ["Description"] = "Unlocks 2 new Save Slots to save more Locker Designs!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 135
+    },
+    {
+        ["LevelRewardTitle"] = "StyleSync Scepter",
+        ["Name"] = "StyleSync Scepter",
+        ["Description"] = "Installs StyleSync software into your Teleportation Scepter that scans what your wearing to match your outfit colors!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 150
+    },
+    {
+        ["LevelRewardTitle"] = "Elemental Studies - New Magical Element! Book 4",
+        ["Name"] = "Elemental Studies - New Magical Element! Book 4",
+        ["Requires"] = "Elemental Studies - New Magical Element! Book 3",
+        ["Description"] = "You can now study and practice a new element! Go to the Enchantraverse to begin your new magical pursuit!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 150
+    },
+    {
+        ["LevelRewardTitle"] = "Friendship Portals",
+        ["Name"] = "Friendship Portals",
+        ["Description"] = "Your powerful magic can now summon teleportation portals strong enough to let multiple people through, including your friends or strangers! (Locks portals to exclusive areas if the individual hasn't unlocked yet)",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 160
+    },
+    {
+        ["LevelRewardTitle"] = "Royale High Nature Sounds App",
+        ["Name"] = "Royale High Nature Sounds App",
+        ["Description"] = "Download on your laptop to get access to playing all your favorite nature sounds, from Sunset Island's waves to the calming hum of the Enchantraverse!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 170
+    },
+    {
+        ["LevelRewardTitle"] = "Dream Diary - Extra Notebooks! #10 and #11",
+        ["Name"] = "DiarySlots10and11",
+        ["Requires"] = "DiarySlots8and9",
+        ["Description"] = "2 Extra Save Slots for your Diary!",
+        ["Type"] = "OldLevels",
+        ["LevelRequirement"] = 180
+    },
+    {
+        ["LevelRewardTitle"] = "Quest Master - Quest Slot #3",
+        ["Name"] = "QuestSlot3",
+        ["Requires"] = "QuestSlot2",
+        ["Description"] = "Unlocks an additional Quest Slot! Multitask multiple quests at once, or pick only your fav ones!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 200
+    },
+    {
+        ["LevelRewardTitle"] = "Time Bender Wheel Enchantment",
+        ["Name"] = "Time Bender Wheel Enchantment",
+        ["Description"] = "Wheel cooldown decreased from 20 hours to 12 hours!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 225
+    },
+    {
+        ["LevelRewardTitle"] = "Elemental Studies - New Magical Element! Book 5",
+        ["Name"] = "Elemental Studies - New Magical Element! Book 5",
+        ["Requires"] = "Elemental Studies - New Magical Element! Book 4",
+        ["Description"] = "You can now study and practice a new element! Go to the Enchantraverse to begin your new magical pursuit!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 250
+    },
+    {
+        ["LevelRewardTitle"] = "Cute Locker! Slots #10 and #11",
+        ["Name"] = "LockerSlots10and11",
+        ["Requires"] = "LockerSlots8and9",
+        ["Description"] = "Unlocks 2 new Save Slots to save more Locker Designs!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 250
+    },
+    {
+        ["LevelRewardTitle"] = "Counselor's Office Owner!",
+        ["Name"] = "Counselor's Office Owner!",
+        ["Description"] = "Become a counselor in the Front Office and claim your own Office Room!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 275
+    },
+    {
+        ["LevelRewardTitle"] = "Elemental Studies - New Magical Element! Book 6",
+        ["Name"] = "Elemental Studies - New Magical Element! Book 6",
+        ["Requires"] = "Elemental Studies - New Magical Element! Book 5",
+        ["Description"] = "You can now study and practice a new element! Go to the Enchantraverse to begin your new magical pursuit!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 300
+    },
+    {
+        ["LevelRewardTitle"] = "Dream Diary - Extra Notebooks! #12 and #13",
+        ["Name"] = "DiarySlots12and13",
+        ["Requires"] = "DiarySlots10and11",
+        ["Description"] = "2 Extra Save Slots for your Diary! ",
+        ["Type"] = "OldLevels",
+        ["LevelRequirement"] = 325
+    },
+    {
+        ["LevelRewardTitle"] = "Elemental Mastery - Powerful Magic",
+        ["Name"] = "Elemental Mastery",
+        ["Description"] = "With your magical abilities growing stronger, you are now able to instantaneously change and switch your elemental powers!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 350
+    },
+    {
+        ["LevelRewardTitle"] = "Cute Locker! Slots #12 and #13",
+        ["Name"] = "LockerSlots12and13",
+        ["Requires"] = "LockerSlots10and11",
+        ["Description"] = "Unlocks 2 new Save Slots to save more Locker Designs!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 375
+    },
+    {
+        ["LevelRewardTitle"] = "Construction Mode: Architect's Key",
+        ["Name"] = "Construction Mode: Architect's Key",
+        ["Description"] = "Your key to enter Construction Mode, where you can customize the color of your room walls, floors, ceiling, and balcony! (wallpaper overlays, lights, and front door coloring sold separately)",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 400
+    },
+    {
+        ["LevelRewardTitle"] = "Diary Expansion - Perfect Planner!",
+        ["Name"] = "Diary Expansion - Perfect Planner!",
+        ["Description"] = "Stay on top of your tasks and dreams with to-do lists, dream goals tracking, weekly plans, and customizable checklists!",
+        ["Type"] = "OldLevels",
+        ["LevelRequirement"] = 450
+    },
+    {
+        ["LevelRewardTitle"] = "Spin & Flip Stickers Upgrade!",
+        ["Name"] = "Spin and Flip Stickers Upgrade!",
+        ["Description"] = "Ever wanted your turtle stickers to boop noses? Unlocks the ability to mirror and rotate your stickers!",
+        ["LevelRequirement"] = 450,
+        ["Type"] = "NewLevels",
+        ["RequiresGamepass"] = "ProfileStickers"
+    },
+    {
+        ["LevelRewardTitle"] = "Furniture Resizer!",
+        ["Name"] = "FurnitureScale",
+        ["Description"] = "Ability to change the size of your furniture!",
+        ["Purchasable"] = true,
+        ["Type"] = "OldLevels",
+        ["LevelRequirement"] = 475
+    },
+    {
+        ["LevelRewardTitle"] = "Room Designer - Overlay #1!",
+        ["Name"] = "RoomOverlay1",
+        ["Requires"] = "Construction Mode: Architect's Key",
+        ["Description"] = "Unlocks the ability to customize your own wallpaper and flooring with fabrics! Comes with 45 unique colorable fabrics specifically designed for walls & floor! (more colors accessible through the Fabrics Gamepass)",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 500
+    },
+    {
+        ["LevelRewardTitle"] = "Dream Diary - Extra Notebooks! #14 and #15",
+        ["Name"] = "DiarySlots14and15",
+        ["Requires"] = "DiarySlots12and13",
+        ["Description"] = "2 Extra Save Slots for your Diary!",
+        ["Type"] = "OldLevels",
+        ["LevelRequirement"] = 525
+    },
+    {
+        ["LevelRewardTitle"] = "Shrink 'n Grow Stickers Upgrade!",
+        ["Name"] = "Shrink 'n Grow Stickers Upgrade!",
+        ["Description"] = "Ever wanted to make a baby and mommy turtle? Now you can with the Shrink 'n Grow Stickers Upgrade!",
+        ["LevelRequirement"] = 550,
+        ["Type"] = "NewLevels",
+        ["RequiresGamepass"] = "ProfileStickers"
+    },
+    {
+        ["LevelRewardTitle"] = "Room Designer - Overlay #2!",
+        ["Name"] = "RoomOverlay2",
+        ["Requires"] = "RoomOverlay1",
+        ["Description"] = "Unlocks the 2nd Overlay layer for your wallpaper and flooring, to combine fabrics to make beautiful new designs! (ex: stars + clouds)",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 550
+    },
+    {
+        ["LevelRewardTitle"] = "Cute Locker! Save Slots #14 and #15",
+        ["Name"] = "LockerSlots14and15",
+        ["Requires"] = "LockerSlots12and13",
+        ["Description"] = "Unlocks 2 new Save Slots to save more Locker Designs!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 575
+    },
+    {
+        ["LevelRewardTitle"] = "Room Light Magic!",
+        ["Name"] = "Room Light Magic!",
+        ["Requires"] = "Construction Mode: Architect's Key",
+        ["Description"] = "Change the color of the lights in your room!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 600
+    },
+    {
+        ["LevelRewardTitle"] = "Room Designer - Overlay #3!",
+        ["Name"] = "RoomOverlay3",
+        ["Requires"] = "RoomOverlay2",
+        ["Description"] = "Unlocks the 3rd Overlay layer for your wallpaper and flooring, to combine fabrics to make beautiful new designs! (ex: stars + clouds + sparkles)",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 650
+    },
+    {
+        ["LevelRewardTitle"] = "Dream Diary - Extra Notebooks! #16 and #17",
+        ["Name"] = "DiarySlots16and17",
+        ["Requires"] = "DiarySlots14and15",
+        ["Description"] = "2 Extra Save Slots for your Diary!",
+        ["Type"] = "OldLevels",
+        ["LevelRequirement"] = 850
+    },
+    {
+        ["LevelRewardTitle"] = "Cute Locker! Save Slots #16 and #17",
+        ["Name"] = "LockerSlots16and17",
+        ["Requires"] = "LockerSlots14and15",
+        ["Description"] = "Unlocks 2 new Save Slots to save more Locker Designs!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 900
+    },
+    {
+        ["LevelRewardTitle"] = "Dorm Door Makeover!",
+        ["Name"] = "Dorm Door Makeover!",
+        ["Requires"] = "Construction Mode: Architect's Key",
+        ["Description"] = "Unlocking this achievement allows you to be able to paint your dorm door!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 1000
+    },
+    {
+        ["LevelRewardTitle"] = "Cute Locker! Save Slots #18, #19 and #20!",
+        ["Name"] = "LockerSlots18and19and20",
+        ["Requires"] = "LockerSlots16and17",
+        ["Description"] = "Unlocks 3 new Save Slots to save more Locker Designs!",
+        ["Type"] = "NewLevels",
+        ["LevelRequirement"] = 1150
+    },
+    {
+        ["LevelRewardTitle"] = "Dream Diary - Extra Notebooks! #18, #19 and #20!",
+        ["Name"] = "DiarySlots18and19and20",
+        ["Requires"] = "DiarySlots16and17",
+        ["Description"] = "3 Extra Save Slots for your Diary!",
+        ["Type"] = "OldLevels",
+        ["LevelRequirement"] = 1200
+    },
+    {
+        ["LevelRewardTitle"] = "Teacher's Lounge Keycard!",
+        ["Name"] = "Teacher's Lounge Keycard",
+        ["Description"] = "Gains you an exclusive VIP access keycard into the elusive Teacher's Lounge!",
+        ["Purchasable"] = false,
+        ["Type"] = "OldLevels",
+        ["LevelRequirement"] = 1500
+    }
+}
