d0d13ef3ebf070f192b0adb1a8b8aeadd59e2445
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-24 22:40:02 +1000

    Create DestinationInfo.lua

---
+++ GameData/Royale High [735030788]/DestinationInfo.lua
@@ -0,0 +1,148 @@
+data = {
+    {
+        ["DestinationName"] = "Lobby",
+        ["Title"] = "Castle's Heart",
+        ["Desc"] = "The Heart of the Castle, a cozy core of the school where chatter echoes across its grand stairways and connects you to your classes, dorms, and has your locker for you to grab your books before class!"
+    },
+    {
+        ["DestinationName"] = "RH3DormBathrooms",
+        ["Title"] = "Dormitory Bathing Quarters",
+        ["Desc"] = "Soft, luxuriantly replenishing waters come from our Mystical Mountain Hot Spring into our Bathing Quarters to refresh and prepare you for a productive day! After school, come unwind in our Relaxation Room with a hot cup of delicious antioxidant tea."
+    },
+    {
+        ["DestinationName"] = "RH3Gym",
+        ["Title"] = "School Gymnasium",
+        ["Desc"] = "The hot spot for pep rallies, after school basketball games, and the emergency PE destination when it rains. Many athletic legends have walked upon these squeaky floors! .. and equally as many sleepy students on the bleachers."
+    },
+    {
+        ["DestinationName"] = "RH3LockerCourtyard",
+        ["Title"] = "Locker Courtyard",
+        ["Desc"] = "The bustling Castle Courtyard to get to and from your high school classes. Stop at your locker, turn in your homework, use the Powder Rooms or grab a snack at the vending machines during break!"
+    },
+    {
+        ["DestinationName"] = "RH3Dorms",
+        ["Title"] = "RH Castle Student Dormitories",
+        ["Desc"] = "Your home away from home, experience a slice of teenage freedom by living with your friends in your RH Dormitory! It's your very own personal space to decorate and enjoy life however you see fit!"
+    },
+    {
+        ["DestinationName"] = "RH3Office",
+        ["Title"] = "School Office",
+        ["Desc"] = "The School Office is always here for you, whether you are feeling sick, want to check the Lost & Found, or need help from our School Counselors. Become an Office Volunteer to help others!"
+    },
+    {
+        ["DestinationName"] = "RH3FrontOfCastle",
+        ["Title"] = "Castle Entrance",
+        ["Desc"] = "Welcome to Royale High! The castle can be seen in the distance from the Rainy Woods and Elemental Home Portals of the Enchantraverse. Many Kings and Queens have kissed the heads of their teens at the gate as they sent them off on their own adventures to hone their magical abilities at this prestigious school."
+    },
+    {
+        ["DestinationName"] = "RH3WestWing",
+        ["Title"] = "West Wing Campus",
+        ["Desc"] = "West Wing Campus is home to the Valkyrie Gymnasium, RH Cafeteria, and the Wheel of Divinia! Get fresh air along the top walls of the castle after lunch, or go for a jog amongst the cherry trees!"
+    },
+    {
+        ["DestinationName"] = "RH3Cafeteria",
+        ["Title"] = "School Cafeteria",
+        ["Desc"] = "Currently serving Lunch & Dinner! Come refuel your body and mind while enjoying your favorite meal with friends located at our breezy outdoor lunch zone."
+    },
+    {
+        ["DestinationName"] = "RH3ThroneTower",
+        ["Title"] = "Throne Tower",
+        ["Desc"] = "A mysterious, magic-resonating tower leading to the Throne Room, a historical and opulent grand room that has stood long before Royale Castle became a school!"
+    },
+    {
+        ["DestinationName"] = "RH3Dungeons",
+        ["Title"] = "The Dungeon",
+        ["Desc"] = "A school always has it's secrets.. and a castle has even more! Hidden behind a forbidden entrance no students were ever supposed to find is The Dungeon, its origins for use is unknown - whether it was made long ago before it was a school, or specially created for bad students.."
+    },
+    {
+        ["DestinationName"] = "RH3RainyClassroom",
+        ["Title"] = "Rainy Day Classroom",
+        ["Desc"] = "A classroom cursed with an eternal rain spell... what better to do than to play cozy games? Now playing Musical Chairs!"
+    },
+    {
+        ["DestinationName"] = "RH3ArtClass",
+        ["Title"] = "Art Class",
+        ["Desc"] = "Practice & show off your drawing skills in the Art Studio! Guess other students drawings correctly to earn EXP Stars! Requires level 25 to join"
+    },
+    {
+        ["DestinationName"] = "Snowglobe Summit",
+        ["Title"] = "Snowglobe Summit",
+        ["Desc"] = "Spread seasonal spirit atop Snowglobe Summit, the real 'peak' winter holiday destination! With it's crystalline caves & serene snowfall, you won't find sights so starkly similar to enchanted trinkets encased within frosted glass anywhere else!"
+    },
+    {
+        ["DestinationName"] = "Wickery Cliffs",
+        ["Title"] = "Wickery Cliffs",
+        ["Desc"] = "Buried just off the coastline of an autumnal plane, this phantom peninsula grants a wispy flicker into the other side. With its classical structures & historical railway line, it is both locked in & lost in time... A mysterious Manor perches atop this quaint neighborhood. Who lives there?"
+    },
+    {
+        ["DestinationName"] = "Enchantix",
+        ["Title"] = "New Royale Campus",
+        ["Desc"] = "Only those who hold powers of the 12 Elementals can attend this magical high school! Dorm with other fairies and mermaids, attend magical classes, or study, swim & eat at the Student Quarters!"
+    },
+    {
+        ["DestinationName"] = "New Royale",
+        ["Title"] = "Royale High Campus 2",
+        ["Desc"] = "Arrive in your carriage and get ready for challenging classes, late night dances, fancy lunches, homework, a state of the art spa and swimming pool, & dorming with your friends!"
+    },
+    {
+        ["DestinationName"] = "Classic Royale",
+        ["Title"] = "Royale High Campus 1",
+        ["Desc"] = "Take a trip to our most classical campus! Complete a busy day of school, finish your homework, then zoom on the exhilarating zipline back to your dorm or to the bathrooms for a shower... No worries if you arrive fashionably late to the royale dance!"
+    },
+    {
+        ["DestinationName"] = "Fantasia",
+        ["Title"] = "Fantasia Getaway Resort",
+        ["Desc"] = "Need a break from school? Book a hotel room with friends at the Fantasia Getaway Resort!"
+    },
+    {
+        ["DestinationName"] = "BeachHouse",
+        ["Title"] = "Beach House",
+        ["Desc"] = "Enjoy your Summer Break at your own Beach House! Soak up the sun & surf with friends, go seashell hunting in the ocean, or roast a delicious smore on your 3rd floor balcony."
+    },
+    {
+        ["DestinationName"] = "Sunset Island",
+        ["Title"] = "Sunset Island",
+        ["Desc"] = "Cast your cares away on this tropical island that has warm waters all year around! Every night at Sunset, a Pageant Competition takes place for you to represent your home realm & bring home the grand prize!"
+    },
+    {
+        ["DestinationName"] = "Earth",
+        ["Title"] = "Earth",
+        ["Desc"] = "A bustling city of opportunity, where the Wheel awaits you with a daily spin, and high-end designers showcase dream-worthy outfits in their luxurious boutiques."
+    },
+    {
+        ["DestinationName"] = "BattleRoyale",
+        ["Title"] = "Royale Archery",
+        ["Desc"] = "🏹Test your aim and quick flexes in this team-based PvP game, where all you have is your bow & arrow, heal, and flying ability! Get rewarded EXP for your skills."
+    },
+    {
+        ["DestinationName"] = "DiviniaPark",
+        ["Title"] = "Divinia Park",
+        ["Desc"] = "Take a stroll with your parasol through Divinia Park, where you can make a wish at the Fountain of Dreams, use the city's stage and recreational center, or go for a swim in the endless clear streams!"
+    },
+    {
+        ["DestinationName"] = "TradingHub",
+        ["Title"] = "Trading Hub",
+        ["Desc"] = "The Intergalactic Trading Hub is where-out-of-this-world trades take place between beings from all kingdoms and planets! Trade your goods and exchange them for rare treasures here... Level 75+ Only!"
+    },
+    {
+        ["DestinationName"] = "ApartmentsLobby",
+        ["Title"] = "Apartments Lobby",
+        ["Desc"] = "A cozy place to make tea on the stove, login to your computer for a daily diamonds bonus, or catch some shut eye for some EXP...Or host a sleepover party & order pizza, the choice is yours!"
+    },
+    {
+        ["DestinationName"] = "DiamondBeach",
+        ["Title"] = "Diamond Beach",
+        ["Desc"] = "Welcome to your ultimate Summer Destination Spot! Diamond Beach is a bustling coastal city, with high-fashion boutiques, fine dining, and a lively Pier - all within a leisurely stroll from your Beach House!"
+    },
+    {["DestinationName"] = "Blackwood Manor", ["Title"] = "Blackwood Manor", ["Desc"] = "N/A"},
+    {["DestinationName"] = "MoonlightSquare", ["Title"] = "Moonlight Square", ["Desc"] = "N/A"},
+    {["DestinationName"] = "Apartments", ["Title"] = "Apartments", ["Desc"] = "N/A"},
+    {["DestinationName"] = "DiamondBeachShops", ["Title"] = "Diamond Beach Shops", ["Desc"] = "N/A"},
+    {["DestinationName"] = "BuildersBoutique", ["Title"] = "Builders Boutique", ["Desc"] = "N/A"},
+    {["DestinationName"] = "NewYears", ["Title"] = "New Years", ["Desc"] = "N/A"},
+    {
+        ["DestinationName"] = "NatureFairyClass",
+        ["Title"] = "Nature Fairy Class",
+        ["Desc"] = "Join the 2024 Roblox 'The Hunt' Event here! Nature Fairy Class brings us into the Woods today, where we need to rescue baby chicks eggs and return them back to their nest! Enjoy 10 new 2024 prizes! Disappears 3/30"
+    }
+}
