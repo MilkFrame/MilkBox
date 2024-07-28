81b88ee000669142ac69345ceb68d63b743bd54a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:31:22 +1000

    Create GemPurchases.lua

---
+++ GameData/Knife Ability Test [621129760]/GemPurchases.lua
@@ -0,0 +1,57 @@
+data = {
+    ["GemsT1"] = {
+        ["ItemMenuData"] = {
+            ["Rarity"] = "Gem",
+            ["Name"] = "100 Gems",
+            ["isGemPurchase"] = true,
+            ["GemPurchaseID"] = 1,
+            ["ShopPrice"] = 100,
+            ["RobuxPrice"] = 49,
+            ["ItemIcon"] = "rbxassetid://2174596855"
+        }
+    },
+    ["GemsT4"] = {
+        ["ItemMenuData"] = {
+            ["Rarity"] = "Gem",
+            ["Name"] = "750 Gems",
+            ["isGemPurchase"] = true,
+            ["GemPurchaseID"] = 4,
+            ["ShopPrice"] = 750,
+            ["RobuxPrice"] = 315,
+            ["ItemIcon"] = "rbxassetid://2174597463"
+        }
+    },
+    ["GemsT5"] = {
+        ["ItemMenuData"] = {
+            ["Rarity"] = "Gem",
+            ["Name"] = "1,000 Gems",
+            ["isGemPurchase"] = true,
+            ["GemPurchaseID"] = 5,
+            ["ShopPrice"] = 1000,
+            ["RobuxPrice"] = 420,
+            ["ItemIcon"] = "rbxassetid://2174597739"
+        }
+    },
+    ["GemsT6"] = {
+        ["ItemMenuData"] = {
+            ["Rarity"] = "Gem",
+            ["Name"] = "3,000 Gems",
+            ["isGemPurchase"] = true,
+            ["GemPurchaseID"] = 6,
+            ["ShopPrice"] = 3000,
+            ["RobuxPrice"] = 999,
+            ["ItemIcon"] = "rbxassetid://2174597977"
+        }
+    },
+    ["GemsT3"] = {
+        ["ItemMenuData"] = {
+            ["Rarity"] = "Gem",
+            ["Name"] = "425 Gems",
+            ["isGemPurchase"] = true,
+            ["GemPurchaseID"] = 3,
+            ["ShopPrice"] = 425,
+            ["RobuxPrice"] = 180,
+            ["ItemIcon"] = "rbxassetid://2174629914"
+        }
+    }
+}
