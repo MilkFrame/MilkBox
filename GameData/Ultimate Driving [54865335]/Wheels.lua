a41abc4dba5890c17bfc9187c065bb1c75af1e97
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:42:02 +1000

    Create Wheels.lua

---
+++ GameData/Ultimate Driving [54865335]/Wheels.lua
@@ -0,0 +1,34 @@
+data = {
+    {
+        ["PBR"] = {false, false, 0.95, 1, 1, 1},
+        ["Invisible"] = {false, false, 1, 1, 1, 0},
+        ["MCL36_Front_Tire"] = {false, false, 0.975, 1, 0.8, 1},
+        ["Standard2"] = {"d", 800, 0.9125, 0.8, 0.95, 1.1, "Car ★★", 2},
+        ["Truck1"] = {"a", 800, 0.9, 0.75, 1, 1.2, "Truck ★", 8},
+        ["Standard1"] = {"c", 400, 0.9, 0.75, 0.9, 1.1, "Car ★", 1},
+        ["Truck2"] = {"b", 2000, 0.925, 0.8, 1.25, 1.25, "Truck ★★", 9},
+        ["Monster"] = {false, false, 0.95, 1, 1, 1.5},
+        ["DragRadial"] = {"i", 2000, 0.9, 1.8, 0.6, 1, "Drag Radial ★", 7},
+        ["Truck1D"] = {false, false, 0.9, 0.75, 1, 1.2},
+        ["Race1"] = {"e", 2000, 0.925, 1, 0.8, 1, "Sport ★", 3},
+        ["White1"] = {"k", 1200, 0.95, 1, 0.9, 1.2, "Whitewall ★", 11},
+        ["MCL36_Rear_Tire"] = {false, false, 0.975, 1, 0.8, 1},
+        ["Race2"] = {"f", 10000, 0.95, 1.2, 0.75, 1, "Sport ★★", 4},
+        ["White2"] = {"l", 2000, 0.975, 1.05, 0.95, 1.15, "Whitewall ★★", 12},
+        ["Offroad1"] = {"j", 1200, 0.925, 1, 1.5, 1.25, "Offroad ★", 10},
+        ["Donk"] = {"m", 6000, 0.9, 1, 0.9, 1.1, "Donk ★", 13},
+        ["Race3"] = {"g", 20000, 0.975, 1.4, 0.7, 0.95, "Race ★★★", 5},
+        ["Nuke"] = {false, false, 0.95, 1, 1, 1},
+        ["Race4"] = {"h", 40000, 1, 1.75, 0.65, 0.9, "Race ★★★★", 6}
+    },
+    {
+        ["LayoutOrder"] = 8,
+        ["RoadMult"] = 4,
+        ["DataID"] = 1,
+        ["WeatherMult"] = 3,
+        ["Price"] = 2,
+        ["Description"] = 7,
+        ["Resistance"] = 6,
+        ["OffroadMult"] = 5
+    }
+}
