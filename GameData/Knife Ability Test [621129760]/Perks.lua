ded17bdfa4d192ee073b58feeb943ac85d1d3afc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:35:11 +1000

    Create Perks.lua

---
+++ GameData/Knife Ability Test [621129760]/Perks.lua
@@ -0,0 +1,41 @@
+data = {
+    ["ChillyDown"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 10000,
+            ["Name"] = "Headless Horror",
+            ["Description"] = [[You might find the results to be
+quite shocking...]],
+            ["Rarity"] = "Mythical",
+            ["isExclusive"] = true,
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://8285045202"
+        }
+    },
+    ["ReflectPerk"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 7500,
+            ["SortPrice"] = 1005,
+            ["Name"] = "Deflect",
+            ["Description"] = [[+ Passive Ability: Deflect revolver fire.
+!- Cannot spawn with Revolver.]],
+            ["Rarity"] = "Legendary",
+            ["isExclusive"] = true,
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://5145067270"
+        }
+    },
+    ["SuperKevlar"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 7500,
+            ["SortPrice"] = 1005,
+            ["Name"] = "Super Kevlar",
+            ["Description"] = [[+ Take less damage from revolver fire.
+!- 15% slower Movement Speed.]],
+            ["Rarity"] = "Legendary",
+            ["isExclusive"] = true,
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://8269125019"
+        },
+        ["PerkDataBasic"] = {["MovementModifier"] = 0.88}
+    }
+}
