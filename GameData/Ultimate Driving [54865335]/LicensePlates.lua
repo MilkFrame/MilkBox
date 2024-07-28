c9a6870d63dfe303f3f6763321c07752673687d3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:41:24 +1000

    Create LicensePlates.lua

---
+++ GameData/Ultimate Driving [54865335]/LicensePlates.lua
@@ -0,0 +1,258 @@
+data = {
+    ["Data"] = {
+        ["CA"] = {
+            ["Std"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "0ZZZ000", ["SpriteIndex"] = 1, ["SpriteX"] = 3},
+            ["Sun"] = {["SpriteY"] = 4, ["MaxDigits"] = 8, ["Format"] = "0ZZZ000", ["SpriteIndex"] = 1, ["SpriteX"] = 2},
+            ["Gld"] = {["SpriteY"] = 4, ["MaxDigits"] = 8, ["Format"] = "0ZZZ000", ["SpriteIndex"] = 1, ["SpriteX"] = 1}
+        },
+        ["HI"] = {
+            ["Std"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 2, ["SpriteX"] = 3}
+        },
+        ["DE"] = {
+            ["Std"] = {["SpriteY"] = 5, ["MaxDigits"] = 8, ["Format"] = "0000000", ["SpriteIndex"] = 1, ["SpriteX"] = 2},
+            ["Blk"] = {["SpriteY"] = 5, ["MaxDigits"] = 6, ["Format"] = "000000", ["SpriteIndex"] = 1, ["SpriteX"] = 3},
+            ["Bch"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "0000000", ["SpriteIndex"] = 2, ["SpriteX"] = 1}
+        },
+        ["BC"] = {
+            ["Std"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 7, ["SpriteX"] = 1}
+        },
+        ["GA"] = {
+            ["Pch"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "0000ZZZ", ["SpriteIndex"] = 2, ["SpriteX"] = 1},
+            ["Std"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "0000ZZZ", ["SpriteIndex"] = 2, ["SpriteX"] = 2}
+        },
+        ["DC"] = {
+            ["Std"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "ZZ0000", ["SpriteIndex"] = 2, ["SpriteX"] = 2}
+        },
+        ["WY"] = {
+            ["New"] = {["SpriteY"] = 2, ["MaxDigits"] = 5, ["Format"] = "00000", ["SpriteIndex"] = 7, ["SpriteX"] = 2},
+            ["Std"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "000000", ["SpriteIndex"] = 7, ["SpriteX"] = 1}
+        },
+        ["YT"] = {
+            ["Std"] = {["SpriteY"] = 1, ["MaxDigits"] = 5, ["Format"] = "ZZZ00", ["SpriteIndex"] = 8, ["SpriteX"] = 3}
+        },
+        ["NE"] = {
+            ["Std"] = {["SpriteY"] = 4, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 4, ["SpriteX"] = 2},
+            ["Crn"] = {["SpriteY"] = 5, ["MaxDigits"] = 6, ["Format"] = "ZZZ000", ["SpriteIndex"] = 4, ["SpriteX"] = 1},
+            ["New"] = {["SpriteY"] = 4, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 4, ["SpriteX"] = 3}
+        },
+        ["ME"] = {
+            ["Std"] = {["SpriteY"] = 3, ["MaxDigits"] = 6, ["Format"] = "0000ZZ", ["SpriteIndex"] = 3, ["SpriteX"] = 1}
+        },
+        ["AK"] = {
+            ["Cen"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 1, ["SpriteX"] = 2},
+            ["Std"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 1, ["SpriteX"] = 1}
+        },
+        ["CO"] = {
+            ["Std"] = {["SpriteY"] = 4, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 1, ["SpriteX"] = 3}
+        },
+        ["MB"] = {
+            ["Std"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "ZZZ00", ["SpriteIndex"] = 7, ["SpriteX"] = 2}
+        },
+        ["NU"] = {
+            ["Std"] = {["SpriteY"] = 5, ["MaxDigits"] = 8, ["Format"] = "00000Z", ["SpriteIndex"] = 7, ["SpriteX"] = 1}
+        },
+        ["MA"] = {
+            ["Cod"] = {["SpriteY"] = 5, ["MaxDigits"] = 6, ["Format"] = "00AAAA", ["SpriteIndex"] = 3, ["SpriteX"] = 2},
+            ["Std"] = {["SpriteY"] = 5, ["MaxDigits"] = 8, ["Format"] = "00AAAA", ["SpriteIndex"] = 3, ["SpriteX"] = 1}
+        },
+        ["LA"] = {
+            ["Ann"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 3, ["SpriteX"] = 1},
+            ["Std"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 3, ["SpriteX"] = 2},
+            ["Stk"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 3, ["SpriteX"] = 3}
+        },
+        ["QC"] = {
+            ["Std"] = {["SpriteY"] = 1, ["MaxDigits"] = 7, ["Format"] = "000ZZZ", ["SpriteIndex"] = 8, ["SpriteX"] = 1}
+        },
+        ["ON"] = {
+            ["Std"] = {["SpriteY"] = 5, ["MaxDigits"] = 8, ["Format"] = "ZZZZ000", ["SpriteIndex"] = 7, ["SpriteX"] = 2}
+        },
+        ["SC"] = {
+            ["Std"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 6, ["SpriteX"] = 1},
+            ["Brb"] = {["SpriteY"] = 2, ["MaxDigits"] = 6, ["Format"] = "ZZZ000", ["SpriteIndex"] = 6, ["SpriteX"] = 2},
+            ["New"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 6, ["SpriteX"] = 3}
+        },
+        ["PA"] = {
+            ["Std"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "ZZZ0000", ["SpriteIndex"] = 6, ["SpriteX"] = 1}
+        },
+        ["WA"] = {
+            ["Std"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "ZZZ0000", ["SpriteIndex"] = 7, ["SpriteX"] = 1}
+        },
+        ["VA"] = {
+            ["400"] = {["SpriteY"] = 5, ["MaxDigits"] = 8, ["Format"] = "ZZZ0000", ["SpriteIndex"] = 6, ["SpriteX"] = 3},
+            ["Std"] = {["SpriteY"] = 5, ["MaxDigits"] = 8, ["Format"] = "ZZZ0000", ["SpriteIndex"] = 6, ["SpriteX"] = 2}
+        },
+        ["PE"] = {
+            ["Std"] = {["SpriteY"] = 5, ["MaxDigits"] = 5, ["Format"] = "ZZ000", ["SpriteIndex"] = 7, ["SpriteX"] = 3}
+        },
+        ["WV"] = {
+            ["Std"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "0ZZ000", ["SpriteIndex"] = 7, ["SpriteX"] = 2}
+        },
+        ["OK"] = {
+            ["New"] = {["SpriteY"] = 5, ["MaxDigits"] = 8, ["Format"] = "000AAA", ["SpriteIndex"] = 5, ["SpriteX"] = 2},
+            ["Std"] = {["SpriteY"] = 5, ["MaxDigits"] = 6, ["Format"] = "000AAA", ["SpriteIndex"] = 5, ["SpriteX"] = 1}
+        },
+        ["NS"] = {
+            ["Std"] = {["SpriteY"] = 4, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 7, ["SpriteX"] = 3}
+        },
+        ["NL"] = {
+            ["Std"] = {["SpriteY"] = 4, ["MaxDigits"] = 8, ["Format"] = "ZZZ00", ["SpriteIndex"] = 7, ["SpriteX"] = 1}
+        },
+        ["TX"] = {
+            ["New"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 6, ["SpriteX"] = 3},
+            ["Std"] = {["SpriteY"] = 4, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 6, ["SpriteX"] = 1}
+        },
+        ["SK"] = {
+            ["Std"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 8, ["SpriteX"] = 2}
+        },
+        ["MO"] = {
+            ["Std"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "0ZZAAA", ["SpriteIndex"] = 4, ["SpriteX"] = 3},
+            ["Sta"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "0ZZAAA", ["SpriteIndex"] = 4, ["SpriteX"] = 2},
+            ["BiC"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "0ZZAAA", ["SpriteIndex"] = 4, ["SpriteX"] = 1}
+        },
+        ["KS"] = {
+            ["Hrt"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 3, ["SpriteX"] = 2},
+            ["Std"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 3, ["SpriteX"] = 1}
+        },
+        ["RI"] = {
+            ["Std"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "000000", ["SpriteIndex"] = 6, ["SpriteX"] = 2}
+        },
+        ["IN"] = {
+            ["Brg"] = {["SpriteY"] = 5, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 2, ["SpriteX"] = 3},
+            ["Std"] = {["SpriteY"] = 5, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 2, ["SpriteX"] = 1},
+            ["Grn"] = {["SpriteY"] = 5, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 2, ["SpriteX"] = 2}
+        },
+        ["AB"] = {
+            ["Std"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 7, ["SpriteX"] = 3}
+        },
+        ["IL"] = {
+            ["New"] = {
+                ["ShowInChina"] = false,
+                ["SpriteY"] = 4,
+                ["MaxDigits"] = 8,
+                ["Format"] = "Z000000",
+                ["SpriteIndex"] = 2,
+                ["SpriteX"] = 3
+            },
+            ["Std"] = {
+                ["ShowInChina"] = false,
+                ["SpriteY"] = 4,
+                ["MaxDigits"] = 8,
+                ["Format"] = "Z000000",
+                ["SpriteIndex"] = 2,
+                ["SpriteX"] = 2
+            }
+        },
+        ["OR"] = {
+            ["Std"] = {["SpriteY"] = 5, ["MaxDigits"] = 6, ["Format"] = "000ZZZ", ["SpriteIndex"] = 5, ["SpriteX"] = 3}
+        },
+        ["MT"] = {
+            ["Std"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "AAAAAAA", ["SpriteIndex"] = 4, ["SpriteX"] = 3},
+            ["Blk"] = {["SpriteY"] = 2, ["MaxDigits"] = 6, ["Format"] = "AAAAAA", ["SpriteIndex"] = 8, ["SpriteX"] = 2},
+            ["New"] = {["SpriteY"] = 4, ["MaxDigits"] = 6, ["Format"] = "AAAAAA", ["SpriteIndex"] = 4, ["SpriteX"] = 1}
+        },
+        ["NT"] = {
+            ["Std"] = {["SpriteY"] = 4, ["MaxDigits"] = 5, ["Format"] = "00000", ["SpriteIndex"] = 7, ["SpriteX"] = 1}
+        },
+        ["WI"] = {
+            ["Std"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 7, ["SpriteX"] = 3}
+        },
+        ["FL"] = {
+            ["Std"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "AAAAAA", ["SpriteIndex"] = 2, ["SpriteX"] = 3}
+        },
+        ["MD"] = {
+            ["Bay"] = {["SpriteY"] = 4, ["MaxDigits"] = 6, ["Format"] = "AAAAAA", ["SpriteIndex"] = 3, ["SpriteX"] = 3},
+            ["Std"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "AAAAAA", ["SpriteIndex"] = 3, ["SpriteX"] = 2},
+            ["Frm"] = {["SpriteY"] = 4, ["MaxDigits"] = 8, ["Format"] = "AAAAAA", ["SpriteIndex"] = 3, ["SpriteX"] = 2},
+            ["War"] = {["SpriteY"] = 4, ["MaxDigits"] = 6, ["Format"] = "0AA000", ["SpriteIndex"] = 3, ["SpriteX"] = 1},
+            ["New"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "AAAAAA", ["SpriteIndex"] = 3, ["SpriteX"] = 3}
+        },
+        ["ND"] = {
+            ["New"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 5, ["SpriteX"] = 3},
+            ["Std"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 5, ["SpriteX"] = 2}
+        },
+        ["AL"] = {
+            ["Std"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "AAAAAAA", ["SpriteIndex"] = 1, ["SpriteX"] = 1},
+            ["RT"] = {["SpriteY"] = 1, ["MaxDigits"] = 6, ["Format"] = "AAAAAA", ["SpriteIndex"] = 1, ["SpriteX"] = 2},
+            ["New"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "AAAAAAA", ["SpriteIndex"] = 1, ["SpriteX"] = 3}
+        },
+        ["UT"] = {
+            ["New"] = {["SpriteY"] = 4, ["MaxDigits"] = 8, ["Format"] = "Z000ZZ", ["SpriteIndex"] = 6, ["SpriteX"] = 2},
+            ["Std"] = {["SpriteY"] = 4, ["MaxDigits"] = 8, ["Format"] = "Z000ZZ", ["SpriteIndex"] = 6, ["SpriteX"] = 3}
+        },
+        ["ID"] = {
+            ["Std"] = {["SpriteY"] = 4, ["MaxDigits"] = 8, ["Format"] = "Z0AAAAA", ["SpriteIndex"] = 2, ["SpriteX"] = 1}
+        },
+        ["VT"] = {
+            ["Std"] = {["SpriteY"] = 5, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 6, ["SpriteX"] = 1}
+        },
+        ["NB"] = {
+            ["Std"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 7, ["SpriteX"] = 3}
+        },
+        ["TN"] = {
+            ["Std"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 6, ["SpriteX"] = 2}
+        },
+        ["NY"] = {
+            ["Old"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "ZZZ0000", ["SpriteIndex"] = 5, ["SpriteX"] = 2},
+            ["Std"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "ZZZ0000", ["SpriteIndex"] = 5, ["SpriteX"] = 1}
+        },
+        ["MI"] = {
+            ["Brg"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 4, ["SpriteX"] = 1},
+            ["Std"] = {["SpriteY"] = 5, ["MaxDigits"] = 8, ["Format"] = "ZZZ0000", ["SpriteIndex"] = 3, ["SpriteX"] = 3}
+        },
+        ["SD"] = {
+            ["New"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "AAZAA0", ["SpriteIndex"] = 6, ["SpriteX"] = 1},
+            ["Std"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "AAZAA0", ["SpriteIndex"] = 6, ["SpriteX"] = 3}
+        },
+        ["NV"] = {
+            ["Lad"] = {["SpriteY"] = 2, ["MaxDigits"] = 6, ["Format"] = "00Z000", ["SpriteIndex"] = 8, ["SpriteX"] = 3},
+            ["Std"] = {["SpriteY"] = 5, ["MaxDigits"] = 8, ["Format"] = "00Z000", ["SpriteIndex"] = 4, ["SpriteX"] = 2},
+            ["Ttl"] = {["SpriteY"] = 5, ["MaxDigits"] = 6, ["Format"] = "00Z000", ["SpriteIndex"] = 4, ["SpriteX"] = 3}
+        },
+        ["NM"] = {
+            ["Std"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 5, ["SpriteX"] = 1}
+        },
+        ["AR"] = {
+            ["Std"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 1, ["SpriteX"] = 2}
+        },
+        ["NC"] = {
+            ["Fly"] = {["SpriteY"] = 3, ["MaxDigits"] = 6, ["Format"] = "ZZZ000", ["SpriteIndex"] = 5, ["SpriteX"] = 1},
+            ["Std"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "ZZZ0000", ["SpriteIndex"] = 5, ["SpriteX"] = 3}
+        },
+        ["KY"] = {
+            ["Std"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 3, ["SpriteX"] = 2}
+        },
+        ["NJ"] = {
+            ["Std"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "Z00AAA", ["SpriteIndex"] = 5, ["SpriteX"] = 3}
+        },
+        ["MS"] = {
+            ["Std"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 4, ["SpriteX"] = 3},
+            ["Msc"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 4, ["SpriteX"] = 2},
+            ["Sel"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "ZZZ000", ["SpriteIndex"] = 4, ["SpriteX"] = 1}
+        },
+        ["NH"] = {
+            ["Std"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "000000", ["SpriteIndex"] = 5, ["SpriteX"] = 2}
+        },
+        ["OH"] = {
+            ["Alt"] = {["SpriteY"] = 4, ["MaxDigits"] = 8, ["Format"] = "ZZZ0000", ["SpriteIndex"] = 5, ["SpriteX"] = 3},
+            ["Std"] = {["SpriteY"] = 4, ["MaxDigits"] = 8, ["Format"] = "ZZZ0000", ["SpriteIndex"] = 5, ["SpriteX"] = 1},
+            ["Old"] = {["SpriteY"] = 4, ["MaxDigits"] = 8, ["Format"] = "ZZZ0000", ["SpriteIndex"] = 5, ["SpriteX"] = 2}
+        },
+        ["MN"] = {
+            ["Std"] = {["SpriteY"] = 1, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 4, ["SpriteX"] = 2}
+        },
+        ["IA"] = {
+            ["Std"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 2, ["SpriteX"] = 1},
+            ["Blk"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 2, ["SpriteX"] = 3},
+            ["New"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 2, ["SpriteX"] = 2}
+        },
+        ["CT"] = {
+            ["Lit"] = {["SpriteY"] = 2, ["MaxDigits"] = 6, ["Format"] = "000ZZZ", ["SpriteIndex"] = 8, ["SpriteX"] = 1},
+            ["Std"] = {["SpriteY"] = 5, ["MaxDigits"] = 8, ["Format"] = "000ZZZ", ["SpriteIndex"] = 1, ["SpriteX"] = 1}
+        },
+        ["AZ"] = {
+            ["Cen"] = {["SpriteY"] = 3, ["MaxDigits"] = 8, ["Format"] = "ZZZ0000", ["SpriteIndex"] = 1, ["SpriteX"] = 1},
+            ["Std"] = {["SpriteY"] = 2, ["MaxDigits"] = 8, ["Format"] = "ZZZ0000", ["SpriteIndex"] = 1, ["SpriteX"] = 3}
+        }
+    },
+    ["Sheets"] = {["1"] = "rbxassetid://8517327605"}
+}
