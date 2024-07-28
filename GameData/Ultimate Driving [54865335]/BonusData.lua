390c0afa37876e5e574cf7344ed27f25620af231
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:37:18 +1000

    Create BonusData.lua

---
+++ GameData/Ultimate Driving [54865335]/BonusData.lua
@@ -0,0 +1,10 @@
+data = {
+    ["Max"] = 85,
+    ["Session1"] = {["Time"] = 600, ["Percent"] = 10},
+    ["Driving2X"] = {["Percent"] = 100},
+    ["VIP"] = {["Percent"] = 30},
+    ["Group"] = {["Percent"] = 10},
+    ["Session2"] = {["Time"] = 1200, ["Percent"] = 10},
+    ["Friend"] = {["PercentPer"] = 3, ["MaxNeeded"] = 5},
+    ["Session3"] = {["Time"] = 3600, ["Percent"] = 10}
+}
