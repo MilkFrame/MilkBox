9efa7e5dd2633e90c89c2e848a726fbf545ecdba
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:41:37 +1000

    Create Quests.lua

---
+++ GameData/Ultimate Driving [54865335]/Quests.lua
@@ -0,0 +1,12 @@
+data = {
+    ["QuestTruckTask"] = {{4, 10}, 1, 3, "Finish %s truck jobs!"},
+    ["QuestShootCrim"] = {{4, 10}, 1, 4, "Kill %s criminals!"},
+    ["QuestFightFire"] = {{4, 10}, 1, 5, "Put out %s fires!"},
+    ["QuestFineCrim"] = {{4, 10}, 1, 2, "Fine %s civilians!"},
+    ["QuestEarnMiles"] = {{10, 100}, 5, 2, "Drive %s miles!"},
+    ["QuestEarnBounty"] = {{2000, 10000}, 1000, 4, "Earn %s bounty!"},
+    ["QuestSpecialRace"] = {{4, 8}, 1, 4, "Finish %s special races!"},
+    ["QuestShootCop"] = {{4, 10}, 1, 4, "Kill %s police!"},
+    ["QuestArrestCrim"] = {{4, 10}, 1, 3, "Arrest %s criminals!"},
+    ["QuestTransitTask"] = {{4, 10}, 1, 3, "Finish %s transit jobs!"}
+}
