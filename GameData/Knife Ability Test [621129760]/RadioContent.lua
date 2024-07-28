1d7e575ecebc35528baf8b80a1e774b31f2f66af
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:29:03 +1000

    Create RadioContent.lua

---
+++ GameData/Knife Ability Test [621129760]/RadioContent.lua
@@ -0,0 +1,135 @@
+data = {
+    ["RadioSkins"] = {
+        ["CakeRadio"] = {
+            ["ItemMenuData"] = {
+                ["SortPrice"] = 999.5,
+                ["Name"] = "Christmas Pudding",
+                ["Rarity"] = "Legendary",
+                ["HideFromShop"] = true,
+                ["Description"] = "[Xmas 2020 item]",
+                ["ShopPrice"] = 1000,
+                ["ItemIcon"] = "rbxassetid://4108743133"
+            }
+        },
+        ["Invisible"] = {
+            ["ItemMenuData"] = {
+                ["Rarity"] = "Legendary",
+                ["TRADABLE"] = true,
+                ["Name"] = "Invisible Radio",
+                ["Description"] = [=[There is no radio.
+Requires the Radio Pass to use.
+[Can be found in Extras]]=],
+                ["HideFromShop"] = true,
+                ["ShopPrice"] = 1000,
+                ["IsWhite"] = true,
+                ["ItemIcon"] = "rbxassetid://2188435221"
+            }
+        },
+        ["FabulousRadio"] = {
+            ["ItemMenuData"] = {
+                ["Name"] = "Fabulous Radio",
+                ["Description"] = [=[Fabulous!
+Requires the Radio Pass to use.
+[Can be found in Extras]]=],
+                ["Rarity"] = "Epic",
+                ["ShopPrice"] = 700,
+                ["TRADABLE"] = true,
+                ["ItemIcon"] = "rbxassetid://4063528908"
+            }
+        },
+        ["BlackIronRadio"] = {
+            ["ItemMenuData"] = {
+                ["Name"] = "Black Iron",
+                ["ShopPrice"] = 400,
+                ["Rarity"] = "Epic",
+                ["Description"] = " ",
+                ["TRADABLE"] = true,
+                ["ItemIcon"] = "rbxassetid://2188431540"
+            }
+        },
+        ["TheMixtape"] = {
+            ["ItemMenuData"] = {
+                ["Name"] = "The Mixtape",
+                ["Description"] = [=[Spicy hot!
+Requires the Radio Pass to use.
+[Can be found in Extras]]=],
+                ["Rarity"] = "Legendary",
+                ["ShopPrice"] = 900,
+                ["TRADABLE"] = true,
+                ["ItemIcon"] = "rbxassetid://2188432857"
+            }
+        },
+        ["DiamondRadio"] = {
+            ["ItemMenuData"] = {
+                ["TRADABLE"] = true,
+                ["Name"] = "Diamond Radio",
+                ["Description"] = " ",
+                ["Rarity"] = "Legendary",
+                ["ShopPrice"] = 1000,
+                ["Reference"] = "RadioModel",
+                ["ItemIcon"] = "rbxassetid://2188434186"
+            }
+        },
+        ["MLGRadio"] = {
+            ["ItemMenuData"] = {
+                ["ShopPrice"] = 1200,
+                ["RedirectExtra"] = "MLGPass",
+                ["Name"] = "MLG Radio",
+                ["Rarity"] = "Epic",
+                ["HideFromShop"] = true,
+                ["GamepassId"] = 2379688,
+                ["RobuxPrice"] = 350,
+                ["ItemIcon"] = "rbxassetid://2188436193"
+            }
+        },
+        ["AdminRadio"] = {
+            ["ItemMenuData"] = {
+                ["Name"] = "Stock Radio",
+                ["REDACTED"] = true,
+                ["ShopPrice"] = 99999999,
+                ["Rarity"] = "Contraband",
+                ["Description"] = "",
+                ["ItemIcon"] = "rbxassetid://2188432215"
+            }
+        },
+        ["VoidRadio"] = {
+            ["ItemMenuData"] = {
+                ["isCrate"] = true,
+                ["Name"] = "Void Radio",
+                ["ShopPrice"] = 9999999,
+                ["Rarity"] = "Epic",
+                ["Description"] = " ",
+                ["TRADABLE"] = true,
+                ["ItemIcon"] = "rbxassetid://4992264521"
+            }
+        },
+        ["YoutubeRadio"] = {
+            ["ItemMenuData"] = {
+                ["Name"] = "YouTube Radio",
+                ["Rarity"] = "Rare",
+                ["ShopPrice"] = 200,
+                ["Description"] = " ",
+                ["ItemIcon"] = "rbxassetid://2188437749"
+            }
+        },
+        ["GoldenRadio"] = {
+            ["ItemMenuData"] = {
+                ["Name"] = "Golden Radio",
+                ["ShopPrice"] = 500,
+                ["Rarity"] = "Epic",
+                ["Description"] = " ",
+                ["TRADABLE"] = true,
+                ["ItemIcon"] = "rbxassetid://2188433561"
+            }
+        },
+        ["StockRadio"] = {
+            ["ItemMenuData"] = {
+                ["Name"] = "Hidden",
+                ["IsFree"] = true,
+                ["Rarity"] = "Stock",
+                ["Description"] = " ",
+                ["ItemIcon"] = "rbxassetid://2188432215"
+            }
+        }
+    }
+}
