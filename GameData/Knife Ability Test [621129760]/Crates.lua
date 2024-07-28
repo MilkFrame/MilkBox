f583f01a02fc5b1c520e18b3564d0d6ca57d487c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:31:58 +1000

    Create Crates.lua

---
+++ GameData/Knife Ability Test [621129760]/Crates.lua
@@ -0,0 +1,112 @@
+data = {
+    ["CrateT1"] = {
+        ["ItemMenuData"] = {
+            ["Rarity"] = "Rare",
+            ["RobuxPrice"] = 59,
+            ["isConsumable"] = true,
+            ["TRADABLE"] = true,
+            ["flavorDescription"] = "Can be used in the Backpack menu.",
+            ["Name"] = "Mystery Crate #1",
+            ["ProductId"] = 1006912338,
+            ["ItemIcon"] = "rbxassetid://5022185105",
+            ["Description"] = "Open to obtain a random tradeable weapon skin!",
+            ["ShopPrice"] = 0,
+            ["ChancePool"] = "1A"
+        }
+    },
+    ["CrateT2_HW_BM"] = {
+        ["ItemMenuData"] = {
+            ["PurchaseLimit"] = 3,
+            ["SortPrice"] = 900,
+            ["ShopPrice"] = 9000,
+            ["TRADABLE"] = true,
+            ["isConsumable"] = true,
+            ["Name"] = "Halloween Crate #2",
+            ["ItemIcon"] = "rbxassetid://5917808508",
+            ["Description"] = [=[Open to obtain a random tradeable weapon skin!
+Purchased illegally from the Black Market.
+[Increased Chances]]=],
+            ["Rarity"] = "Contraband",
+            ["OverrideLootTable"] = "crateLootTable2",
+            ["ChancePool"] = "2H"
+        }
+    },
+    ["CrateT1_BM"] = {
+        ["ItemMenuData"] = {
+            ["SortPrice"] = 850,
+            ["ShopPrice"] = 1500,
+            ["TRADABLE"] = true,
+            ["Name"] = "Mystery Crate #1",
+            ["isConsumable"] = true,
+            ["ItemIcon"] = "rbxassetid://5022185105",
+            ["Description"] = [[Open to obtain a random tradeable weapon skin!
+Purchased illegally from the Black Market.]],
+            ["Rarity"] = "Contraband",
+            ["ChancePool"] = "1C"
+        }
+    },
+    ["CrateT1_HW"] = {
+        ["ItemMenuData"] = {
+            ["isConsumable"] = true,
+            ["ShopPrice"] = 0,
+            ["Rarity"] = "Epic",
+            ["PriceDisplayMessage"] = "[Collectable]",
+            ["Description"] = "Open to obtain a random tradeable weapon skin!",
+            ["RobuxPrice"] = 99,
+            ["TRADABLE"] = true,
+            ["flavorDescription"] = "Can be used in the Backpack menu.",
+            ["ItemIcon"] = "rbxassetid://5917808508",
+            ["Name"] = "Halloween Crate #2",
+            ["ProductId"] = 0,
+            ["ShowInShop"] = true,
+            ["isExclusive"] = true,
+            ["OverrideLootTable"] = "crateLootTable2",
+            ["ChancePool"] = "1H"
+        }
+    },
+    ["CrateT2_HW"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 0,
+            ["isConsumable"] = true,
+            ["Rarity"] = "Legendary",
+            ["RobuxPrice"] = 299,
+            ["TRADABLE"] = true,
+            ["flavorDescription"] = "Can be used in the Backpack menu.",
+            ["ItemIcon"] = "rbxassetid://5917808508",
+            ["Name"] = "Halloween Crate #2",
+            ["ProductId"] = 1117993585,
+            ["Description"] = [=[Open to obtain a random tradeable weapon skin!
+[Increased Chances]]=],
+            ["isExclusive"] = true,
+            ["OverrideLootTable"] = "crateLootTable2",
+            ["ChancePool"] = "2H"
+        }
+    },
+    ["CrateT0"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 200000,
+            ["TRADABLE"] = true,
+            ["isConsumable"] = true,
+            ["Name"] = "Mystery Crate #0",
+            ["flavorDescription"] = "The Black Market only exists to serve high end players who have nothing else to spend their currency on.",
+            ["ItemIcon"] = "rbxassetid://5022185105",
+            ["Description"] = "Redacted",
+            ["Rarity"] = "Contraband",
+            ["ChancePool"] = "00"
+        }
+    },
+    ["CrateT1_F"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 0,
+            ["RobuxPrice"] = 49,
+            ["isConsumable"] = true,
+            ["TRADABLE"] = true,
+            ["Name"] = "Mystery Crate",
+            ["ProductId"] = 1004342027,
+            ["flavorDescription"] = "Can be used in the Backpack menu.",
+            ["ItemIcon"] = "rbxassetid://5022185105",
+            ["Description"] = "Open to obtain a random tradeable weapon skin!",
+            ["ChancePool"] = "1B"
+        }
+    }
+}
