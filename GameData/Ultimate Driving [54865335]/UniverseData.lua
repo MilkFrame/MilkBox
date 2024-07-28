17c55ce6f781103cdc53adb6df71cfdc4fcd8f8f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:37:03 +1000

    Create UniverseData.lua

---
+++ GameData/Ultimate Driving [54865335]/UniverseData.lua
@@ -0,0 +1,62 @@
+data = {
+    ["Westover"] = {
+        ["Origin"] = Vector3.new(0, 0, 0),
+        ["SortPriority"] = 3,
+        ["ReservedSlotCount"] = 10,
+        ["MaxPlayerCount"] = 30,
+        ["TestID"] = 8515225705,
+        ["Spawns"] = {
+            ["Westover"] = {Vector3.new(5277, 61, -2095), "Westover Dealership"},
+            ["Police"] = {Vector3.new(5800, 54, -1181), "Westover Troop 1"},
+            ["Mailman"] = {Vector3.new(6529, 57, -1510), "Westover Post Office"},
+            ["Highway Worker"] = {Vector3.new(1274, 7, 4933), "Kensington DOT Depot"},
+            ["Transit"] = {Vector3.new(4953, 60, -1190), "Westover Transit Hub"},
+            ["EMT / Fire Fighter"] = {Vector3.new(1003, 8, 5043), "Kensington Fire Department"},
+            ["Trucker"] = {Vector3.new(1701, -3, 5919), "Kensington Cargo Hub"}
+        },
+        ["ID"] = 54865335,
+        ["UniqueID"] = "Westover",
+        ["ImageID"] = "rbxassetid://9598913852",
+        ["Name"] = "Westover Islands"
+    },
+    ["Hub"] = {
+        ["TestID"] = 8515309277,
+        ["Origin"] = Vector3.new(0, 0, 0),
+        ["ID"] = 9597982562,
+        ["UniqueID"] = "Hub",
+        ["Spawns"] = {},
+        ["Name"] = "Ultimate Driving Hub"
+    },
+    ["Delancydrome"] = {
+        ["Origin"] = Vector3.new(-49000, -6.199999809265137, -27000),
+        ["SortPriority"] = 1,
+        ["ReservedSlotCount"] = 10,
+        ["MaxPlayerCount"] = 35,
+        ["TestID"] = 9673011193,
+        ["Spawns"] = {["Main Festival"] = {Vector3.new(128, 493, 1146), "Main Festival"}},
+        ["ID"] = 9684034858,
+        ["UniqueID"] = "Delancydrome",
+        ["ImageID"] = "rbxassetid://9598913852",
+        ["Name"] = "Delancydrome Speed Complex"
+    },
+    ["Odessa"] = {
+        ["Origin"] = Vector3.new(-16517.00390625, -6.199999809265137, -5478.64404296875),
+        ["SortPriority"] = 2,
+        ["ReservedSlotCount"] = 10,
+        ["MaxPlayerCount"] = 35,
+        ["TestID"] = 9669189719,
+        ["Spawns"] = {
+            ["Odessa"] = {Vector3.new(1995, 103, 3981), "Odessa Dealership"},
+            ["Police"] = {Vector3.new(-1617, 116, 189), "Bordenville Police Station"},
+            ["Mailman"] = {Vector3.new(2053, 98, 4417), "Odessa Post Office"},
+            ["Highway Worker"] = {Vector3.new(-2356, 103, 9500), "Odessa DOT Barn"},
+            ["Transit"] = {Vector3.new(1573, 99, 4568), "Odessa Transit Stop"},
+            ["EMT / Fire Fighter"] = {Vector3.new(1697, 86, 5570), "Odessa Fire Department"},
+            ["Trucker"] = {Vector3.new(-2271, 93, -5477), "Odessa Warehouse"}
+        },
+        ["ID"] = 9684030749,
+        ["UniqueID"] = "Odessa",
+        ["ImageID"] = "rbxassetid://9598913852",
+        ["Name"] = "Odessa"
+    }
+}
