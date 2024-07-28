c807f12e5de3363ee053fa0fcd97779123822444
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:29:32 +1000

    Create CharacterSkins.lua

---
+++ GameData/Knife Ability Test [621129760]/CharacterSkins.lua
@@ -0,0 +1,257 @@
+data = {
+    ["XboxCharacter"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 125,
+            ["Reference"] = "ShopModel",
+            ["oldPrice"] = 0,
+            ["Name"] = "xbox",
+            ["levelRequirement"] = 30,
+            ["IsFree"] = true,
+            ["Rarity"] = "Epic",
+            ["Description"] = "Oops!?",
+            ["ItemIcon"] = "rbxassetid://1880277152"
+        }
+    },
+    ["BeastCharacter"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Beast",
+            ["ShopPrice"] = 1000,
+            ["HideFromShop"] = true,
+            ["Rarity"] = "Epic",
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://2694084025"
+        }
+    },
+    ["YukkiCharacter"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Yukki",
+            ["levelRequirement"] = 40,
+            ["IsFree"] = true,
+            ["Rarity"] = "Epic",
+            ["ShopPrice"] = 175,
+            ["ItemIcon"] = "rbxassetid://4595562386"
+        }
+    },
+    ["KasCharacter"] = {
+        ["ItemMenuData"] = {
+            ["levelRequirement"] = 35,
+            ["oldPrice"] = 0,
+            ["Name"] = "Kasodus",
+            ["IsFree"] = true,
+            ["Rarity"] = "Epic",
+            ["ShopPrice"] = 150,
+            ["Description"] = "The.",
+            ["ItemIcon"] = "rbxassetid://1879559409"
+        }
+    },
+    ["HarryCharacter"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Harry",
+            ["levelRequirement"] = 100,
+            ["IsFree"] = true,
+            ["Rarity"] = "Legendary",
+            ["ShopPrice"] = 201,
+            ["ItemIcon"] = "rbxassetid://1879519683"
+        }
+    },
+    ["NoobCharacter"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Noob",
+            ["levelRequirement"] = 1,
+            ["IsFree"] = true,
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 0,
+            ["ItemIcon"] = "rbxassetid://3091185699"
+        }
+    },
+    ["TomCharacter"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = "EUGHH?",
+            ["Reference"] = "ShopModel",
+            ["oldPrice"] = 0,
+            ["Name"] = "Tom",
+            ["levelRequirement"] = 50,
+            ["IsFree"] = true,
+            ["Rarity"] = "Epic",
+            ["ShopPrice"] = 200,
+            ["ItemIcon"] = "rbxassetid://1870551363"
+        }
+    },
+    ["WardenCharacter"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Warden",
+            ["levelRequirement"] = 49,
+            ["IsFree"] = true,
+            ["Rarity"] = "Epic",
+            ["ShopPrice"] = 199.99,
+            ["ItemIcon"] = "rbxassetid://5910701668"
+        }
+    },
+    ["SpongeyCharacter"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Spengy",
+            ["TRADABLE"] = true,
+            ["ShopPrice"] = 900,
+            ["Rarity"] = "Epic",
+            ["Reference"] = "ShopModel",
+            ["ItemIcon"] = "rbxassetid://4169769576"
+        }
+    },
+    ["TabbyCharacter"] = {
+        ["ItemMenuData"] = {
+            ["levelRequirement"] = 16,
+            ["oldPrice"] = 0,
+            ["Name"] = "Tabby",
+            ["IsFree"] = true,
+            ["Rarity"] = "Rare",
+            ["Submitter"] = "nettimato",
+            ["ShopPrice"] = 75,
+            ["ItemIcon"] = "rbxassetid://2393894414"
+        }
+    },
+    ["DaveCharacter"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Dave",
+            ["levelRequirement"] = 3,
+            ["IsFree"] = true,
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 1,
+            ["ItemIcon"] = "rbxassetid://1879182723"
+        }
+    },
+    ["HotdogScoutCharacter"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 201,
+            ["TRADABLE"] = true,
+            ["BonusUnlock"] = "BaseballBat",
+            ["isCrate"] = true,
+            ["Name"] = "Hotdog Scout",
+            ["isRare"] = true,
+            ["Description"] = "Yeeah! You just got freakin' dominated!",
+            ["Submitter"] = "Jax_Bline",
+            ["Rarity"] = "Legendary",
+            ["ItemIcon"] = "rbxassetid://5191280373"
+        }
+    },
+    ["BOFCharacter"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "BOF",
+            ["levelRequirement"] = 9,
+            ["IsFree"] = true,
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 4,
+            ["ItemIcon"] = "rbxassetid://1880306850"
+        }
+    },
+    ["RastaCharacter"] = {
+        ["ItemMenuData"] = {
+            ["levelRequirement"] = 5,
+            ["Name"] = "Rasta",
+            ["IsFree"] = true,
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 3,
+            ["Description"] = "pasta rasta",
+            ["ItemIcon"] = "rbxassetid://1875682519"
+        }
+    },
+    ["JaxCharacter"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Jax",
+            ["levelRequirement"] = 45,
+            ["IsFree"] = true,
+            ["Rarity"] = "Epic",
+            ["ShopPrice"] = 199.98,
+            ["ItemIcon"] = "rbxassetid://1876099184"
+        }
+    },
+    ["JohnCharacter"] = {
+        ["ItemMenuData"] = {
+            ["levelRequirement"] = 15,
+            ["oldPrice"] = 0,
+            ["Name"] = "John",
+            ["IsFree"] = true,
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 50,
+            ["Description"] = "Blame John.",
+            ["ItemIcon"] = "rbxassetid://1875985033"
+        }
+    },
+    ["AyanoCharacter"] = {
+        ["ItemMenuData"] = {
+            ["levelRequirement"] = 18,
+            ["Name"] = "デレデレ",
+            ["IsFree"] = true,
+            ["Rarity"] = "Epic",
+            ["ShopPrice"] = 85,
+            ["Description"] = "...",
+            ["ItemIcon"] = "rbxassetid://1919997732"
+        }
+    },
+    ["MaxCharacter"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = [[¯\_(ツ)_/¯]],
+            ["Reference"] = "ShopModel",
+            ["oldPrice"] = 0,
+            ["Name"] = "Max",
+            ["levelRequirement"] = 28,
+            ["IsFree"] = true,
+            ["Rarity"] = "Epic",
+            ["ShopPrice"] = 100,
+            ["ItemIcon"] = "rbxassetid://4169624461"
+        }
+    },
+    ["AceCharacter"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Ace",
+            ["levelRequirement"] = 10,
+            ["IsFree"] = true,
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 10,
+            ["ItemIcon"] = "rbxassetid://1880239428"
+        }
+    },
+    ["SergeiCharacter"] = {
+        ["ItemMenuData"] = {
+            ["levelRequirement"] = 4,
+            ["Name"] = "Sergei",
+            ["IsFree"] = true,
+            ["ShopPrice"] = 2,
+            ["Rarity"] = "Rare",
+            ["Reference"] = "ShopModel",
+            ["ItemIcon"] = "rbxassetid://1868645878"
+        }
+    },
+    ["IcyTeaCharacter"] = {
+        ["ItemMenuData"] = {
+            ["oldPrice"] = 0,
+            ["Name"] = "Icy",
+            ["levelRequirement"] = 12,
+            ["IsFree"] = true,
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 25,
+            ["ItemIcon"] = "rbxassetid://1876049627"
+        }
+    },
+    ["Cl0ckHandzCharacter"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Shamus",
+            ["levelRequirement"] = 20,
+            ["IsFree"] = true,
+            ["Rarity"] = "Epic",
+            ["ShopPrice"] = 100,
+            ["ItemIcon"] = "rbxassetid://1879246011"
+        }
+    },
+    ["JackCharacter"] = {
+        ["ItemMenuData"] = {
+            ["levelRequirement"] = 24,
+            ["oldPrice"] = 0,
+            ["Name"] = "Jack",
+            ["IsFree"] = true,
+            ["Rarity"] = "Epic",
+            ["ShopPrice"] = 100,
+            ["Description"] = "Hit the road, jack.",
+            ["ItemIcon"] = "rbxassetid://2691416263"
+        }
+    }
+}
