d5240f166cb217eab847166ed2b5dea19faf1628
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:34:33 +1000

    Create Consumables.lua

---
+++ GameData/Knife Ability Test [621129760]/Consumables.lua
@@ -0,0 +1,336 @@
+data = {
+    ["ShardsPassVoucher"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 230,
+            ["HideFromShop"] = true,
+            ["Rarity"] = "Epic",
+            ["RobuxPrice"] = 200,
+            ["isConsumable"] = true,
+            ["Description"] = [[Giftable voucher, can be used to obtain the shard knives.
+!Cannot be exchanged for robux equivalent]],
+            ["TRADABLE"] = true,
+            ["Name"] = "Shards Token",
+            ["ProductId"] = 1006914722,
+            ["flavorDescription"] = "Tradable, can be used once to obtain the shard knives.",
+            ["isExclusive"] = true,
+            ["VoucherPassType"] = "ShardsPass",
+            ["ItemIcon"] = "rbxassetid://5175768734"
+        }
+    },
+    ["GemUnlocker2"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = [[Gives 1,500 Gems on use.
+Double Tap to open..]],
+            ["HideFromShop"] = true,
+            ["RobuxPrice"] = 999,
+            ["GemTier"] = 2,
+            ["Name"] = "Stacked Gem Crate",
+            ["isConsumable"] = true,
+            ["Rarity"] = "Epic",
+            ["VoucherPassType"] = "GemUnlock",
+            ["ItemIcon"] = "rbxassetid://2174597739"
+        }
+    },
+    ["DescriptionTag"] = {
+        ["ItemMenuData"] = {
+            ["isConsumable"] = true,
+            ["Name"] = "Description Tag",
+            ["Description"] = [[One time use consumable, can be used to
+rename items.]],
+            ["flavorDescription"] = "403 NOT IMPLEMENTED",
+            ["Rarity"] = "Contraband",
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://5118514438"
+        }
+    },
+    ["FestiveGift2"] = {
+        ["ItemMenuData"] = {
+            ["giftPool"] = "XL",
+            ["ShopPrice"] = 999999,
+            ["HideFromShop"] = true,
+            ["RobuxPrice"] = 85,
+            ["isConsumable"] = true,
+            ["Name"] = "Large Festive Gift",
+            ["Description"] = [[Happy Holidays 2021
+Double Tap to open..]],
+            ["flavorDescription"] = "One time use consumable, can be used to rename items.",
+            ["Rarity"] = "Rare",
+            ["VoucherPassType"] = "InstantGift",
+            ["ItemIcon"] = "rbxassetid://8320018041"
+        }
+    },
+    ["MVPPassVoucher_BM"] = {
+        ["ItemMenuData"] = {
+            ["PurchaseLimit"] = 1,
+            ["SortPrice"] = 1020,
+            ["ShopPrice"] = 32500,
+            ["isShop"] = true,
+            ["TRADABLE"] = true,
+            ["Rarity"] = "Contraband",
+            ["Name"] = "MVP Token [BM]",
+            ["isConsumable"] = true,
+            ["Description"] = [[Giftable voucher, can be used to give MVP.
+Purchased illegally from the Black Market.]],
+            ["isExclusive"] = true,
+            ["VoucherPassType"] = "MVPPass",
+            ["ItemIcon"] = "rbxassetid://5209963405"
+        }
+    },
+    ["ShotgunPassVoucher"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 240,
+            ["Rarity"] = "Epic",
+            ["RobuxPrice"] = 399,
+            ["isConsumable"] = true,
+            ["Description"] = [[Giftable voucher, can be used to obtain the
+Shotgun knife ability.
+!Cannot be exchanged for robux equivalent]],
+            ["TRADABLE"] = true,
+            ["Name"] = "Shotgun Token",
+            ["ProductId"] = 1006914859,
+            ["flavorDescription"] = "Tradable, can be used once to obtain the Shotgun knife ability.",
+            ["isExclusive"] = true,
+            ["VoucherPassType"] = "ShotgunKnifePass",
+            ["ItemIcon"] = "rbxassetid://5175769431"
+        }
+    },
+    ["VIPPassVoucher"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 210,
+            ["Rarity"] = "Rare",
+            ["RobuxPrice"] = 95,
+            ["isConsumable"] = true,
+            ["Description"] = [[Giftable voucher, can be used to give VIP.
+!Cannot be exchanged for robux equivalent]],
+            ["TRADABLE"] = true,
+            ["Name"] = "VIP Token",
+            ["ProductId"] = 1006914349,
+            ["flavorDescription"] = "Tradable, can be used once for VIP Access.",
+            ["isExclusive"] = true,
+            ["VoucherPassType"] = "VIPPass",
+            ["ItemIcon"] = "rbxassetid://5175769983"
+        }
+    },
+    ["MVPPassVoucher"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 240,
+            ["Rarity"] = "Epic",
+            ["RobuxPrice"] = 300,
+            ["isConsumable"] = true,
+            ["Description"] = [[Giftable voucher, can be used to give MVP.
+!Cannot be exchanged for robux equivalent]],
+            ["TRADABLE"] = true,
+            ["Name"] = "MVP Token",
+            ["ProductId"] = 1006914582,
+            ["flavorDescription"] = "Tradable, can be used once for MVP Access.",
+            ["isExclusive"] = true,
+            ["VoucherPassType"] = "MVPPass",
+            ["ItemIcon"] = "rbxassetid://5175770455"
+        }
+    },
+    ["FestiveGift1_HW"] = {
+        ["ItemMenuData"] = {
+            ["giftPool"] = "FS",
+            ["ShopPrice"] = 999999,
+            ["HideFromShop"] = true,
+            ["RobuxPrice"] = 85,
+            ["isConsumable"] = true,
+            ["Name"] = "Small Festive Gift",
+            ["Description"] = [[Happy Holidays 2021 !
+Double Tap to open..]],
+            ["flavorDescription"] = "One time use consumable, can be used to rename items.",
+            ["Rarity"] = "Rare",
+            ["VoucherPassType"] = "InstantGift",
+            ["ItemIcon"] = "rbxassetid://8339263036"
+        }
+    },
+    ["VIPPassVoucher_BM"] = {
+        ["ItemMenuData"] = {
+            ["PurchaseLimit"] = 1,
+            ["SortPrice"] = 1010,
+            ["ShopPrice"] = 15000,
+            ["isShop"] = true,
+            ["TRADABLE"] = true,
+            ["Rarity"] = "Contraband",
+            ["Name"] = "VIP Token [BM]",
+            ["isConsumable"] = true,
+            ["Description"] = [[Giftable voucher, can be used to give VIP.
+Purchased illegally from the Black Market.]],
+            ["isExclusive"] = true,
+            ["VoucherPassType"] = "VIPPass",
+            ["ItemIcon"] = "rbxassetid://5209962831"
+        }
+    },
+    ["GemUnlocker1"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = [[Gives 400 Gems on use.
+Double Tap to open..]],
+            ["HideFromShop"] = true,
+            ["RobuxPrice"] = 999,
+            ["GemTier"] = 1,
+            ["Name"] = "Bucket o' Gems",
+            ["isConsumable"] = true,
+            ["Rarity"] = "Rare",
+            ["VoucherPassType"] = "GemUnlock",
+            ["ItemIcon"] = "rbxassetid://2174629914"
+        }
+    },
+    ["RadioPassVoucher"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 225,
+            ["Rarity"] = "Epic",
+            ["RobuxPrice"] = 200,
+            ["isConsumable"] = true,
+            ["Description"] = [[Giftable voucher, can be used to give Radio.
+!Cannot be exchanged for robux equivalent]],
+            ["TRADABLE"] = true,
+            ["Name"] = "Radio Token",
+            ["ProductId"] = 1007092836,
+            ["flavorDescription"] = "Tradable, can be used once for Radio Access.",
+            ["isExclusive"] = true,
+            ["VoucherPassType"] = "RadioPass2",
+            ["ItemIcon"] = "rbxassetid://5200128417"
+        }
+    },
+    ["RadioPassVoucher_UT"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 9999999,
+            ["isConsumable"] = true,
+            ["Name"] = "Radio Unlocker",
+            ["Description"] = "Consume to give access to radio.",
+            ["Rarity"] = "Stock",
+            ["isExclusive"] = true,
+            ["VoucherPassType"] = "RadioPass2",
+            ["ItemIcon"] = "rbxassetid://5200128417"
+        }
+    },
+    ["FestiveGift1"] = {
+        ["ItemMenuData"] = {
+            ["giftPool"] = "XS",
+            ["ShopPrice"] = 999999,
+            ["HideFromShop"] = true,
+            ["RobuxPrice"] = 85,
+            ["isConsumable"] = true,
+            ["Name"] = "Small Festive Gift",
+            ["Description"] = [[Happy Holidays 2021
+Double Tap to open..]],
+            ["flavorDescription"] = "One time use consumable, can be used to rename items.",
+            ["Rarity"] = "Rare",
+            ["VoucherPassType"] = "InstantGift",
+            ["ItemIcon"] = "rbxassetid://8339252216"
+        }
+    },
+    ["KCoal"] = {
+        ["ItemMenuData"] = {
+            ["REDACTED"] = true,
+            ["TRADABLE"] = true,
+            ["Name"] = "A Lump of Coal",
+            ["Rarity"] = "Epic",
+            ["ShopPrice"] = 999999,
+            ["Description"] = "UH oh..",
+            ["RobuxPrice"] = 85,
+            ["ItemIcon"] = "rbxassetid://8325549357"
+        }
+    },
+    ["MediumGift"] = {
+        ["ItemMenuData"] = {
+            ["PurchaseLimit"] = 3,
+            ["SortPrice"] = 2,
+            ["ShopPrice"] = 205,
+            ["isConsumable"] = true,
+            ["RobuxPrice"] = 400,
+            ["Description"] = [[Happy Holidays 2021
+Double Tap to open..]],
+            ["giftPool"] = "XM",
+            ["TRADABLE"] = true,
+            ["Name"] = "Festive Gift",
+            ["ProductId"] = 1230538801,
+            ["flavorDescription"] = "Can be used in the Backpack menu. Contains Rocks.",
+            ["Rarity"] = "Rare",
+            ["VoucherPassType"] = "InstantGift",
+            ["ItemIcon"] = "rbxassetid://8320018762"
+        }
+    },
+    ["FestiveGift2_HW"] = {
+        ["ItemMenuData"] = {
+            ["giftPool"] = "FL",
+            ["ShopPrice"] = 999999,
+            ["HideFromShop"] = true,
+            ["RobuxPrice"] = 85,
+            ["isConsumable"] = true,
+            ["Name"] = "Large Festive Gift",
+            ["Description"] = [[Happy Holidays 2021 !
+Double Tap to open..]],
+            ["flavorDescription"] = "One time use consumable, can be used to rename items.",
+            ["Rarity"] = "Rare",
+            ["VoucherPassType"] = "InstantGift",
+            ["ItemIcon"] = "rbxassetid://8329319339"
+        }
+    },
+    ["GemUnlocker3"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 99999,
+            ["HideFromShop"] = true,
+            ["RobuxPrice"] = 999,
+            ["GemTier"] = 3,
+            ["Name"] = "Treasure Vault",
+            ["isConsumable"] = true,
+            ["Description"] = [[Gives 4,000 Gems on use.
+Double Tap to open..]],
+            ["Rarity"] = "Legendary",
+            ["VoucherPassType"] = "GemUnlock",
+            ["ItemIcon"] = "rbxassetid://2174597977"
+        }
+    },
+    ["RadioPassVoucher_BM"] = {
+        ["ItemMenuData"] = {
+            ["PurchaseLimit"] = 1,
+            ["SortPrice"] = 1005,
+            ["ShopPrice"] = 10000,
+            ["isShop"] = true,
+            ["TRADABLE"] = true,
+            ["Rarity"] = "Contraband",
+            ["Name"] = "Radio Token [BM]",
+            ["isConsumable"] = true,
+            ["Description"] = [[Giftable voucher, can be used to give Radio.
+Purchased illegally from the Black Market.]],
+            ["isExclusive"] = true,
+            ["VoucherPassType"] = "RadioPass2",
+            ["ItemIcon"] = "rbxassetid://5200128417"
+        }
+    },
+    ["NameTag"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 200,
+            ["RobuxPrice"] = 125,
+            ["isConsumable"] = true,
+            ["Rarity"] = "Rare",
+            ["TRADABLE"] = true,
+            ["Name"] = "Name Tag",
+            ["ProductId"] = 1006911896,
+            ["flavorDescription"] = "One time use consumable, can be used to rename items.",
+            ["isRare"] = true,
+            ["Description"] = [[One time use consumable, can be used to
+rename items.]],
+            ["ItemIcon"] = "rbxassetid://5118514438"
+        }
+    },
+    ["TeslaPassVoucher"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 250,
+            ["Rarity"] = "Legendary",
+            ["RobuxPrice"] = 600,
+            ["isConsumable"] = true,
+            ["Description"] = [[Giftable voucher, can be used to obtain the
+Tesla knife ability.
+!Cannot be exchanged for robux equivalent]],
+            ["TRADABLE"] = true,
+            ["Name"] = "Tesla Token",
+            ["ProductId"] = 1006914983,
+            ["flavorDescription"] = "Tradable, can be used once to obtain the Tesla knife ability.",
+            ["isExclusive"] = true,
+            ["VoucherPassType"] = "TeslaKnifePass",
+            ["ItemIcon"] = "rbxassetid://5175768219"
+        }
+    }
+}
