00458fb9ad776fb663d3e537c0bc3e5fafccb3bb
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:38:59 +1000

    Create Toys.lua

---
+++ GameData/Ultimate Driving [54865335]/Toys.lua
@@ -0,0 +1,17 @@
+data = {
+    ["880452192"] = {
+        "Westover Racer",
+        "http://www.roblox.com/asset/?id=4340757388",
+        "https://www.roblox.com/Thumbs/Asset.ashx?width=420&height=420&assetId=880452192"
+    },
+    ["743706754"] = {
+        "Masters of Roblox",
+        "http://www.roblox.com/asset/?id=4340757409",
+        "https://roblox.com/outfit-thumbnail/image?userOutfitId=132923229&width=420&height=420&format=png"
+    },
+    ["1868161479"] = {
+        "Citizens of Roblox",
+        "http://www.roblox.com/asset/?id=4340757386",
+        "https://roblox.com/outfit-thumbnail/image?userOutfitId=132923230&width=420&height=420&format=png"
+    }
+}
