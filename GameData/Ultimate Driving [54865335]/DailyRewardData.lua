d8f8a429c9a2208a1984822e1984a4041a6d59dc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:36:50 +1000

    Create DailyRewardData.lua

---
+++ GameData/Ultimate Driving [54865335]/DailyRewardData.lua
@@ -0,0 +1,77 @@
+data = {
+    ["ProgrammedRewards"] = {
+        ["ProgrammedDays"] = 30,
+        ["SeriesData"] = {
+            {
+                ["SplashImage"] = "rbxassetid://11117190621",
+                ["RewardData"] = {
+                    {["Type"] = "Day1"},
+                    {["VehicleID"] = "Challenger", ["Type"] = "Vehicle"},
+                    {["VehicleID"] = "CorvetteC6", ["Type"] = "Vehicle"},
+                    {["VehicleID"] = "G80_Competition", ["Type"] = "Vehicle"},
+                    {["VehicleID"] = "GT350R", ["Type"] = "Vehicle"}
+                }
+            },
+            {
+                ["SplashImage"] = "rbxassetid://11117190621",
+                ["RewardData"] = {
+                    {["Amount"] = 3000, ["Type"] = "Credits"},
+                    {["Amount"] = 4000, ["Type"] = "Credits"},
+                    {["Amount"] = 5000, ["Type"] = "Credits"},
+                    {["Amount"] = 7500, ["Type"] = "Credits"},
+                    {["Amount"] = 10000, ["Type"] = "Credits"}
+                }
+            },
+            {
+                ["SplashImage"] = "rbxassetid://11117190621",
+                ["RewardData"] = {
+                    {["Amount"] = 3000, ["Type"] = "Credits"},
+                    {["Amount"] = 4000, ["Type"] = "Credits"},
+                    {["Amount"] = 5000, ["Type"] = "Credits"},
+                    {["Amount"] = 7500, ["Type"] = "Credits"},
+                    {["Amount"] = 10000, ["Type"] = "Credits"}
+                }
+            },
+            {
+                ["SplashImage"] = "rbxassetid://11117190621",
+                ["RewardData"] = {
+                    {["Amount"] = 3000, ["Type"] = "Credits"},
+                    {["Amount"] = 4000, ["Type"] = "Credits"},
+                    {["Amount"] = 5000, ["Type"] = "Credits"},
+                    {["Amount"] = 7500, ["Type"] = "Credits"},
+                    {["Amount"] = 10000, ["Type"] = "Credits"}
+                }
+            },
+            {
+                ["SplashImage"] = "rbxassetid://11117190621",
+                ["RewardData"] = {
+                    {["Amount"] = 3000, ["Type"] = "Credits"},
+                    {["Amount"] = 4000, ["Type"] = "Credits"},
+                    {["Amount"] = 5000, ["Type"] = "Credits"},
+                    {["Amount"] = 7500, ["Type"] = "Credits"},
+                    {["Amount"] = 10000, ["Type"] = "Credits"}
+                }
+            },
+            {
+                ["SplashImage"] = "rbxassetid://11117190621",
+                ["RewardData"] = {
+                    {["Amount"] = 3000, ["Type"] = "Credits"},
+                    {["Amount"] = 4000, ["Type"] = "Credits"},
+                    {["Amount"] = 5000, ["Type"] = "Credits"},
+                    {["Amount"] = 7500, ["Type"] = "Credits"},
+                    {["Amount"] = 10000, ["Type"] = "Credits"}
+                }
+            }
+        }
+    },
+    ["RankRewards"] = {
+        ["RewardData"] = {
+            {["Amount"] = 2000, ["Type"] = "Credits"},
+            {["Amount"] = 3000, ["Type"] = "Credits"},
+            {["Amount"] = 4000, ["Type"] = "Credits"},
+            {["Amount"] = 5000, ["Type"] = "Credits"},
+            {["Amount"] = 6000, ["Type"] = "Credits"}
+        },
+        ["RankScale"] = 0.01
+    }
+}
