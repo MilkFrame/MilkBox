0f5528a8c3ddf806877371b08a3f3869aa8ba12f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:40:24 +1000

    Create PlayerIcons.lua

---
+++ GameData/Ultimate Driving [54865335]/PlayerIcons.lua
@@ -0,0 +1,14 @@
+data = {
+    ["STAR_ICON"] = "rbxasset://textures/ui/icon_star-16.png",
+    ["VIP_ICON"] = "rbxassetid://6817916360",
+    ["UD_ADMIN_ICON"] = "http://www.roblox.com/asset/?id=4701927163",
+    ["UD_PARTNER_ICON"] = "http://www.roblox.com/asset/?id=4701986861",
+    ["UD_TTP_ICON"] = "http://www.roblox.com/asset/?id=4701927165",
+    ["ADMIN_ICON"] = "rbxasset://textures/ui/icon_admin-16.png",
+    ["UD_MODERATOR_ICON"] = "http://www.roblox.com/asset/?id=4701880974",
+    ["BLOCKED_ICON"] = "rbxasset://textures/ui/PlayerList/BlockedIcon.png",
+    ["FRIEND_ICON"] = "rbxasset://textures/ui/icon_friends_16.png",
+    ["PREMIUM_ICON"] = "rbxasset://textures/ui/PlayerList/PremiumIcon.png",
+    ["LOCALIZATION_EXPERT_ICON"] = "rbxasset://textures/ui/icon_localization-16.png",
+    ["SHEDLETSKY_ICON"] = "rbxassetid://105897927"
+}
