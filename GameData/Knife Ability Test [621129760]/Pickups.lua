67bbbacb69d386cdfd1158cfbad6cf9392a41886
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:29:59 +1000

    Create Pickups.lua

---
+++ GameData/Knife Ability Test [621129760]/Pickups.lua
@@ -0,0 +1,87 @@
+data = {
+    ["RevolverPickup"] = {
+        ["Settings"] = {
+            ["OnSpawn"] = "function: 0xfcc2ac06a48787fe",
+            ["GetModelCollisionAddress"] = "function: 0x8d35ee69546605de",
+            ["Animate"] = {["Step"] = "function: 0x3720e32cb6a82ebe"},
+            ["Overrides"] = {},
+            ["GetModelItemNamespaceAndAddress"] = "function: 0xb92b234d6fc1ee9e",
+            ["OnInteract"] = "function: 0x96b42484c217535e",
+            ["OnStepped"] = "function: 0x28aea510efa0f83e",
+            ["KillPickup"] = "function: 0xbe9e1970d859b8de"
+        }
+    },
+    ["TokenHWPickupType"] = {
+        ["Settings"] = {
+            ["OnSpawn"] = "function: 0x01a4c40f949c055e",
+            ["GetModelCollisionAddress"] = "function: 0xd3bd0ca8d337009e",
+            ["Overrides"] = {["AnimateStep"] = "function: 0xb1b77754d5e7f67e"},
+            ["GetModelItemNamespaceAndAddress"] = "function: 0xfdb29de6b1872d3e",
+            ["OnInteract"] = "function: 0x83adcd8993222e3e",
+            ["OnStepped"] = "function: 0x0d80db127150b39e",
+            ["KillPickup"] = "function: 0x8797c6657fc3877e"
+        }
+    },
+    ["GemPickupType"] = {
+        ["Settings"] = {
+            ["OnSpawn"] = "function: 0x91e58c8bf1ce807e",
+            ["GetModelCollisionAddress"] = "function: 0x9dcb87b7e96e7b3e",
+            ["Animate"] = {["Step"] = "function: 0x33cd1c05ffccd69e"},
+            ["LifeExpectancy"] = 35,
+            ["Overrides"] = {},
+            ["GetModelItemNamespaceAndAddress"] = "function: 0x77d80d65db28005e",
+            ["OnInteract"] = "function: 0xeff20711d3b1bdde",
+            ["OnStepped"] = "function: 0xb5ff9aaf156dd6be",
+            ["KillPickup"] = "function: 0x1bf10a2e36ad001e"
+        }
+    },
+    ["BananaPickup"] = {
+        ["Settings"] = {
+            ["OnSpawn"] = "function: 0xbe628d46c649813e",
+            ["GetModelCollisionAddress"] = "function: 0xea0e86d402d4ecfe",
+            ["Animate"] = {["Step"] = "function: 0x18101a36e0455a5e"},
+            ["LifeExpectancy"] = 20,
+            ["Overrides"] = {},
+            ["GetModelItemNamespaceAndAddress"] = "function: 0xa6659b9ed897d7be",
+            ["OnInteract"] = "function: 0x145f04f49ba9ac9e",
+            ["OnStepped"] = "function: 0x72449c6889e45a7e",
+            ["KillPickup"] = "function: 0x30560daebf9207de"
+        }
+    },
+    ["CratePickupType"] = {
+        ["Settings"] = {
+            ["OnSpawn"] = "function: 0x40c4f1f9f35c5dbe",
+            ["GetModelCollisionAddress"] = "function: 0xc4a0933dd7986b7e",
+            ["Animate"] = {["Step"] = "function: 0xdecfef5ff15e06de"},
+            ["Overrides"] = {},
+            ["GetModelItemNamespaceAndAddress"] = "function: 0xca9131dbda1fdf9e",
+            ["OnInteract"] = "function: 0x43351093ccc1a91e",
+            ["OnStepped"] = "function: 0x4d3e8f01ded186fe",
+            ["KillPickup"] = "function: 0x473831970b5d545e"
+        }
+    },
+    ["TokenCMPickupType"] = {
+        ["Settings"] = {
+            ["OnSpawn"] = "function: 0x20f5f0a664ae389e",
+            ["GetModelCollisionAddress"] = "function: 0xfcefb77c5e8d8e5e",
+            ["Animate"] = {["Step"] = "function: 0xa6e4d11300e8d53e"},
+            ["Overrides"] = {},
+            ["GetModelItemNamespaceAndAddress"] = "function: 0xda00321f4c0df8fe",
+            ["OnInteract"] = "function: 0x62fef027464c0e7e",
+            ["OnStepped"] = "function: 0x64fbd1f0684fd3de",
+            ["KillPickup"] = "function: 0xbee9334b9bd078be"
+        }
+    },
+    ["ItemPickupType"] = {
+        ["Settings"] = {
+            ["Offset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["GetModelCollisionAddress"] = "function: 0xd52c7529a01bd47e",
+            ["Overrides"] = {["AnimateStep"] = "function: 0x4f46144f839ba9de"},
+            ["OnSpawn"] = "function: 0x71518cc16d6282be",
+            ["GetModelItemNamespaceAndAddress"] = "function: 0xfb2d0ee3479b029e",
+            ["OnInteract"] = "function: 0x5361354a78d0d41e",
+            ["OnStepped"] = "function: 0xba979bd56ac26bfe",
+            ["KillPickup"] = "function: 0xe88c0e201455ff5e"
+        }
+    }
+}
