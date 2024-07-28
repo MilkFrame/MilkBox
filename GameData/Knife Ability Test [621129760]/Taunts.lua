b17cbdd5e1e15c858e044fa3faad86e0ffd4a796
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:34:55 +1000

    Create Taunts.lua

---
+++ GameData/Knife Ability Test [621129760]/Taunts.lua
@@ -0,0 +1,451 @@
+data = {
+    ["Dab"] = {
+        ["TauntControl"] = {
+            ["Sounds"] = {["Bass"] = "rbxassetid://577475178"},
+            ["TauntType"] = "Singular",
+            ["Animations"] = {["Start"] = 1359860552},
+            ["AttatchTauntFunctions"] = "function: 0x6a3db106767769de",
+            ["Props"] = {}
+        },
+        ["ItemMenuData"] = {
+            ["Name"] = "Dab",
+            ["TauntIcon"] = "rbxassetid://1373945813",
+            ["Description"] = "You can use Emotes with the G key on PC, or the emote button on mobile.",
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 50,
+            ["ItemIcon"] = "rbxassetid://1872030051"
+        }
+    },
+    ["MeditationTaunt"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Meditation",
+            ["TauntIcon"] = "rbxassetid://1373945980",
+            ["Description"] = "You can use Emotes with the G key on PC, or the emote button on mobile.",
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 200,
+            ["ItemIcon"] = "rbxassetid://2199206284"
+        },
+        ["TauntControl"] = {
+            ["Sounds"] = {},
+            ["TauntType"] = "Looped",
+            ["Animations"] = {["Start"] = 1504880503, ["Loop"] = 1504883832},
+            ["AttatchTauntFunctions"] = "function: 0x28d3f420e83397be",
+            ["Props"] = {
+                ["ParticlePoint"] = {["Offset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1), ["Target"] = "Torso"}
+            }
+        }
+    },
+    ["CoffinDweller2"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = "You can use Emotes with the G key on PC, or the emote button on mobile.",
+            ["ShopPrice"] = 8000,
+            ["Name"] = "Coffin Dweller II",
+            ["TauntIcon"] = "rbxassetid://5917488328",
+            ["HideFromShop"] = true,
+            ["Rarity"] = "Contraband",
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://5917505130"
+        },
+        ["TauntControl"] = {
+            ["Sounds"] = {},
+            ["TauntType"] = "Looped",
+            ["Animations"] = {["Start"] = 5931967948, ["Loop"] = 5931973444, ["Finish"] = 5931994561},
+            ["AttatchTauntFunctions"] = "function: 0x4093ca58630ef55e",
+            ["AnchorHRP"] = true,
+            ["Props"] = {
+                ["Lid"] = {["Offset"] = CFrame.new(0, 0.982161045, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1), ["Target"] = "Coffin"},
+                ["Coffin"] = {
+                    ["Offset"] = CFrame.new(
+                        0.01023072,
+                        -4.47940254,
+                        4.98492956,
+                        0.00278360886,
+                        0,
+                        -0.999996126,
+                        0,
+                        1,
+                        0,
+                        0.999996126,
+                        0,
+                        0.00278360886
+                    ),
+                    ["Target"] = "Torso"
+                }
+            }
+        }
+    },
+    ["Handstand"] = {
+        ["TauntControl"] = {
+            ["Sounds"] = {},
+            ["TauntType"] = "Singular",
+            ["Animations"] = {["Start"] = 5935609619},
+            ["AttatchTauntFunctions"] = "function: 0x50035e51fa88405e",
+            ["Props"] = {}
+        },
+        ["ItemMenuData"] = {
+            ["levelRequirement"] = 22,
+            ["Name"] = "Handstand",
+            ["TauntIcon"] = "rbxassetid://1379031379",
+            ["Description"] = "You can use Emotes with the G key on PC, or the emote button on mobile.",
+            ["Rarity"] = "Common",
+            ["ShopPrice"] = 10,
+            ["ItemIcon"] = "rbxassetid://2199200135"
+        }
+    },
+    ["SelfDestruct"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Self Destruction",
+            ["TauntIcon"] = "rbxassetid://1373945562",
+            ["Description"] = "You can use Emotes with the G key on PC, or the emote button on mobile.",
+            ["Rarity"] = "Epic",
+            ["ShopPrice"] = 500,
+            ["ItemIcon"] = "rbxassetid://2199214949"
+        },
+        ["TauntControl"] = {
+            ["Sounds"] = {},
+            ["TauntType"] = "Singular",
+            ["IsKT"] = true,
+            ["Animations"] = {["Start"] = 1371964755},
+            ["OverrideSpeedFactor"] = 1.3,
+            ["AttatchTauntFunctions"] = "function: 0xfbf8642ae8c1695e",
+            ["Props"] = {
+                ["ParticlePoint"] = {["Offset"] = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1), ["Target"] = "Torso"}
+            }
+        }
+    },
+    ["TPose"] = {
+        ["TauntControl"] = {
+            ["Sounds"] = {},
+            ["TauntType"] = "Moving",
+            ["Animations"] = {["Loop"] = 2488926936},
+            ["AttatchTauntFunctions"] = "function: 0xf319c96ca65af65e",
+            ["Props"] = {}
+        },
+        ["ItemMenuData"] = {
+            ["REDDACTED"] = true,
+            ["Name"] = "T-Pose",
+            ["TauntIcon"] = "rbxassetid://2494702311",
+            ["Description"] = "assert dominance.",
+            ["Rarity"] = "Epic",
+            ["ShopPrice"] = 500,
+            ["ItemIcon"] = "rbxassetid://2496165619"
+        }
+    },
+    ["Selfie"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Selfie",
+            ["TauntIcon"] = "rbxassetid://1373946792",
+            ["Description"] = "You can use Emotes with the G key on PC, or the emote button on mobile.",
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 45,
+            ["ItemIcon"] = "rbxassetid://2199215486"
+        },
+        ["TauntControl"] = {
+            ["Sounds"] = {["Click"] = "rbxassetid://515689563"},
+            ["TauntType"] = "Singular",
+            ["Animations"] = {["Start"] = 1368485919},
+            ["AttatchTauntFunctions"] = "function: 0x9829dd3c699e437e",
+            ["Props"] = {
+                ["Phone"] = {
+                    ["Offset"] = CFrame.new(
+                        -0.0871219635,
+                        -0.980876923,
+                        -0.96158123,
+                        -0.999992192,
+                        -0.00280799204,
+                        0.00278398977,
+                        -0.00278400071,
+                        -2.51675374e-08,
+                        -0.999996126,
+                        0.00280798133,
+                        -0.999996066,
+                        -7.79228503e-06
+                    ),
+                    ["Target"] = "Right Arm"
+                }
+            }
+        }
+    },
+    ["Hadouken"] = {
+        ["ItemMenuData"] = {
+            ["PurchaseLimit"] = 3,
+            ["ShopPrice"] = 3500,
+            ["Name"] = "Hadouken",
+            ["TauntIcon"] = "rbxassetid://4111192932",
+            ["Description"] = "You can use Emotes with the G key on PC, or the emote button on mobile.",
+            ["Rarity"] = "Legendary",
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://4111254156 "
+        },
+        ["TauntControl"] = {
+            ["Sounds"] = {["Launch"] = "rbxassetid://3288140848"},
+            ["TauntType"] = "Singular",
+            ["IsKT"] = true,
+            ["Animations"] = {["Start"] = 5932303895},
+            ["OverrideSpeedFactor"] = 1.1,
+            ["AttatchTauntFunctions"] = "function: 0x82455476b1d957ee",
+            ["Props"] = {
+                ["ChargeSpot"] = {
+                    ["Offset"] = CFrame.new(0, -1.00000048, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+                    ["Target"] = "Right Arm"
+                }
+            }
+        }
+    },
+    ["BatterUp"] = {
+        ["TauntControl"] = {
+            ["Sounds"] = {},
+            ["TauntType"] = "Singular",
+            ["IsKT"] = true,
+            ["Animations"] = {["Start"] = 4125276077},
+            ["AttatchTauntFunctions"] = "function: 0x784d61b258c53e6e",
+            ["Props"] = {
+                ["Metal"] = {["Offset"] = CFrame.new(0, 1.33599997, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1), ["Target"] = "Grip"},
+                ["Grip"] = {
+                    ["Offset"] = CFrame.new(
+                        -0.0879999995,
+                        -1,
+                        0,
+                        1,
+                        0,
+                        0,
+                        0,
+                        0.000796274282,
+                        0.999999702,
+                        0,
+                        -0.999999702,
+                        0.000796274282
+                    ),
+                    ["Target"] = "Right Arm"
+                }
+            }
+        },
+        ["ItemMenuData"] = {
+            ["Name"] = "Batter Up!",
+            ["TauntIcon"] = "rbxassetid://4125131975",
+            ["ShopPrice"] = 125,
+            ["Rarity"] = "Rare",
+            ["ItemIcon"] = "rbxassetid://4125146395"
+        }
+    },
+    ["Chirimiru"] = {
+        ["TauntControl"] = {
+            ["Animations"] = {["Start"] = 3079099121, ["Loop"] = 3079075140},
+            ["TauntType"] = "Looped",
+            ["AttatchTauntFunctions"] = "function: 0xce6c633ae213c63e"
+        },
+        ["ItemMenuData"] = {
+            ["Name"] = "Chirumiru",
+            ["TauntIcon"] = "rbxassetid://3085571366",
+            ["Description"] = "You can use Emotes with the G key on PC, or the emote button on mobile.",
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 20,
+            ["ItemIcon"] = "rbxassetid://3085667666"
+        }
+    },
+    ["KazotskyKick"] = {
+        ["TauntControl"] = {
+            ["Sounds"] = {},
+            ["TauntType"] = "Moving",
+            ["Animations"] = {["Loop"] = 1368170606},
+            ["AttatchTauntFunctions"] = "function: 0xd461e0ea152ea85e",
+            ["Props"] = {}
+        },
+        ["ItemMenuData"] = {
+            ["Name"] = "Kazotsky Kick",
+            ["TauntIcon"] = "rbxassetid://1373946200",
+            ["Description"] = "You can use Emotes with the G key on PC, or the emote button on mobile.",
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 125,
+            ["ItemIcon"] = "rbxassetid://2199201105"
+        }
+    },
+    ["StandProud"] = {
+        ["TauntControl"] = {
+            ["Sounds"] = {["Bass"] = "rbxassetid://577475178"},
+            ["TauntType"] = "Looped",
+            ["Animations"] = {
+                ["Start4"] = 4935715983,
+                ["Loop1"] = 4935461037,
+                ["Start1"] = 4935458525,
+                ["Start2"] = 4935591165,
+                ["Loop3"] = 4935687890,
+                ["Loop5"] = 4935897773,
+                ["Start5"] = 4935895968,
+                ["Loop4"] = 4935724467,
+                ["Start3"] = 4935608228,
+                ["Loop2"] = 4935885022
+            },
+            ["AttatchTauntFunctions"] = "function: 0x86ea75a98126a89e",
+            ["Props"] = {}
+        },
+        ["ItemMenuData"] = {
+            ["Name"] = "Stand Proud",
+            ["TauntIcon"] = "rbxassetid://4935718081",
+            ["Description"] = "You can use Emotes with the G key on PC, or the emote button on mobile.",
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 185,
+            ["ItemIcon"] = "rbxassetid://4935722271"
+        }
+    },
+    ["PlantC4"] = {
+        ["TauntControl"] = {
+            ["Sounds"] = {
+                ["Press6"] = "rbxassetid://5932245005",
+                ["AprilFools"] = "rbxassetid://5932245405",
+                ["Press7"] = "rbxassetid://5932245177",
+                ["Press3"] = "rbxassetid://5932244425",
+                ["TimerBeep"] = "rbxassetid://5932254847",
+                ["Press4"] = "rbxassetid://5932244652",
+                ["Press2"] = "rbxassetid://5932244243",
+                ["Press1"] = "rbxassetid://5932243907",
+                ["Press5"] = "rbxassetid://5932244859"
+            },
+            ["TauntType"] = "Singular",
+            ["Animations"] = {["Start"] = 5932104608},
+            ["AttatchTauntFunctions"] = "function: 0xab488bb6d369001e",
+            ["Props"] = {
+                ["C4"] = {
+                    ["Offset"] = CFrame.new(-0.583621979, -0.927121639, -0.580247402, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+                    ["Target"] = "Right Arm"
+                }
+            }
+        }
+    },
+    ["Boxtrot"] = {
+        ["TauntControl"] = {
+            ["Sounds"] = {},
+            ["TauntType"] = "Moving",
+            ["AttatchTauntFunctions"] = "function: 0xe91d0fa78f3fef5e",
+            ["Animations"] = {["Move"] = 4108300368, ["Loop"] = 4109951655},
+            ["HideName"] = true,
+            ["isLargePreview"] = true,
+            ["Props"] = {
+                ["FlapFront"] = {
+                    ["Offset"] = CFrame.new(-2.54999995, -2.97099996, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+                    ["Target"] = "Box"
+                },
+                ["FlapLeft"] = {
+                    ["Offset"] = CFrame.new(0, -2.97000003, 1.79700005, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+                    ["Target"] = "Box"
+                },
+                ["FlapRight"] = {
+                    ["Offset"] = CFrame.new(0, -2.97000003, -1.79999995, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+                    ["Target"] = "Box"
+                },
+                ["FlapBack"] = {
+                    ["Offset"] = CFrame.new(2.54900002, -2.97099996, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+                    ["Target"] = "Box"
+                },
+                ["Box"] = {
+                    ["Offset"] = CFrame.new(
+                        0,
+                        2.23600006,
+                        0,
+                        0.000796274282,
+                        0,
+                        -0.999999702,
+                        0,
+                        1,
+                        0,
+                        0.999999702,
+                        0,
+                        0.000796274282
+                    ),
+                    ["Target"] = "Torso"
+                }
+            }
+        },
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 1000,
+            ["Name"] = "Box Trot",
+            ["TauntIcon"] = "rbxassetid://4102237284",
+            ["Description"] = "You can use Emotes with the G key on PC, or the emote button on mobile.",
+            ["Rarity"] = "Legendary",
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://4102312473"
+        }
+    },
+    ["CoffinDweller"] = {
+        ["ItemMenuData"] = {
+            ["PurchaseLimit"] = 2,
+            ["Description"] = [=[You can use Emotes with the G key on PC, or the emote button on mobile.
+[Halloween 2020 item]]=],
+            ["HideFromShop"] = true,
+            ["TRADABLE"] = true,
+            ["Name"] = "Coffin Dweller",
+            ["TauntIcon"] = "rbxassetid://5917488328",
+            ["ShopPrice"] = 666,
+            ["Rarity"] = "Epic",
+            ["ItemIcon"] = "rbxassetid://5917505130"
+        },
+        ["TauntControl"] = {
+            ["Sounds"] = {},
+            ["TauntType"] = "Looped",
+            ["LoopTimeLimit"] = 12,
+            ["Animations"] = {["Start"] = 5931967948, ["Loop"] = 5931973444, ["Finish"] = 5931994561},
+            ["AttatchTauntFunctions"] = "function: 0x5e488c85eb73b13e",
+            ["AnchorHRP"] = true,
+            ["Props"] = {
+                ["Lid"] = {["Offset"] = CFrame.new(0, 0.982161045, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1), ["Target"] = "Coffin"},
+                ["Coffin"] = {
+                    ["Offset"] = CFrame.new(
+                        0.01023072,
+                        -4.47940254,
+                        4.98492956,
+                        0.00278360886,
+                        0,
+                        -0.999996126,
+                        0,
+                        1,
+                        0,
+                        0.999996126,
+                        0,
+                        0.00278360886
+                    ),
+                    ["Target"] = "Torso"
+                }
+            }
+        }
+    },
+    ["MoneyTaunt"] = {
+        ["ItemMenuData"] = {
+            ["REDACTED"] = true,
+            ["Name"] = "Make it rain",
+            ["TauntIcon"] = "rbxassetid://1373946617",
+            ["Description"] = "You can use Emotes with the G key on PC, or the emote button on mobile.",
+            ["Rarity"] = "Epic",
+            ["ShopPrice"] = 275,
+            ["ItemIcon"] = "rbxassetid://2500559715"
+        },
+        ["TauntControl"] = {
+            ["Sounds"] = {
+                ["Cash1"] = "rbxassetid://16720281",
+                ["Cash3"] = "rbxassetid://16720508",
+                ["Cash2"] = "rbxassetid://16720502"
+            },
+            ["TauntType"] = "Looped",
+            ["Animations"] = {["Start"] = 1366527893, ["Loop"] = 1366127941},
+            ["AttatchTauntFunctions"] = "function: 0x2dac0698bf48c51e",
+            ["Props"] = {
+                ["Dosh"] = {
+                    ["Offset"] = CFrame.new(
+                        0.0199317932,
+                        -1.47787094,
+                        -0.38406086,
+                        0.00269658631,
+                        0,
+                        0.999996364,
+                        8.74224568e-08,
+                        -1,
+                        -2.35743064e-10,
+                        0.999996364,
+                        8.74227766e-08,
+                        -0.00269658631
+                    ),
+                    ["Target"] = "Left Arm"
+                }
+            }
+        }
+    }
+}
