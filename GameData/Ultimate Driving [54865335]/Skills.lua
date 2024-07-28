c2e7315c800250a65ef22bba813ea66cde1b9c5a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:41:51 +1000

    Create Skills.lua

---
+++ GameData/Ultimate Driving [54865335]/Skills.lua
@@ -0,0 +1,329 @@
+data = {
+    ["Data"] = {
+        ["Transit"] = {
+            ["VIP"] = {
+                {
+                    "Fancy Service",
+                    50,
+                    "Your riders appreciate how dripped out you are with your white gloved tuxedo uniform. Earn an extra 10% bonus on all trips."
+                },
+                {
+                    "Fancy Beverages",
+                    250,
+                    "Offer your riders only the finest selection of sparkling apple ciders. Earn an extra 20% bonus on all trips."
+                },
+                {
+                    "Air Suspension",
+                    600,
+                    "Your riders appreciate not spilling their drinks after smashing through potholes the size of Vermont. Earn an extra 30% bonus on all trips."
+                },
+                {
+                    "Soundproof Glass",
+                    1500,
+                    "Your riders' conversations of hostile corporate takeovers, coup d'etats, and assassinations are made without fears of eavesdropping. Earn an extra 40% bonus on all trips."
+                },
+                {
+                    "Bulletproof Glass",
+                    3000,
+                    "Your more unscrupulous clients appreciate not having to worry about being assassinated. Earn an extra 50% tip bonus on all trips."
+                },
+                {
+                    "Presidential Motorcade",
+                    7000,
+                    "Join the Secret Service and transport the President to his appointments! Earn an extra 60% tip bonus on all trips."
+                }
+            },
+            ["City"] = {
+                {
+                    "Clean Seats",
+                    10,
+                    "Your riders appreciate not having to avoid 3 week old dried gum. Earn an extra 10% bonus upon completing any route."
+                },
+                {
+                    "Upgraded Seats",
+                    60,
+                    "The old seats felt like something out of a 1700s prison, NOW your riders are more willing to take longer trips on your bus. Earn an extra 20% bonus upon completing any route."
+                },
+                {
+                    "Functional Air Conditioning",
+                    500,
+                    "Your riders appreciate not being subjected to conditions akin to a Finnish sauna coming home from work. Earn an extra 30% bonus upon completing any route."
+                },
+                {
+                    "Coach Bus Routes",
+                    1500,
+                    "Steve the Manager has given you a promotion, you'll be able to drive packs of feral tourists to all the hotspots around town. Earn an extra 40% bonus upon completing any route."
+                },
+                {
+                    "Party Bus Sound System",
+                    3000,
+                    "Every city needs a Techno Bus™. Earn an extra 50% bonus upon completing any route."
+                },
+                {
+                    "Expedition Bus Routes",
+                    5000,
+                    "You've been promoted again, God help us all. You'll be piloting expedition buses through precarious terrain, so review your life insurance policy. Earn an extra 60% bonus upon completing any route."
+                }
+            },
+            ["School"] = {
+                {
+                    "Functioning PA System",
+                    5,
+                    "Yell at the clowns in the back seats with ease! WES AND CHARLES STOP THROWING ROCKS AT OTHER CARS. Earn an extra 10% bonus on all trips."
+                },
+                {
+                    "All Under Control",
+                    50,
+                    "School administration appreciates not hearing about kids throwing rocks at other cars from your bus anymore. Earn an extra 20% bonus on all trips."
+                },
+                {
+                    "Haha Long Boy",
+                    400,
+                    "Your boss trusts you with larger buses and more obnoxious kids. Earn an extra 30% bonus on all trips."
+                },
+                {
+                    "Premium Sound System",
+                    800,
+                    "Maybe some smooth jazz will calm the kids down long enough to get them home without crashing? No? It was worth a shot... Earn an extra 40% bonus on all trips."
+                },
+                {
+                    "Seat Belts",
+                    2500,
+                    "The school budget got a boost this year and we can afford to belt the dang kids into their seats. Earn an extra 50% bonus on all trips."
+                },
+                {
+                    "Air Suspension",
+                    5000,
+                    "Suddenly those spine-blasting potholes aren't so spine-blasting! Earn an extra 60% bonus on all trips."
+                }
+            },
+            ["UDer"] = {
+                {
+                    "UDer XL",
+                    50,
+                    "Unlock the ability to run trips with larger vehicles and larger parties! Earn an extra 10% bonus on all trips."
+                },
+                {
+                    "UDer Muscle",
+                    250,
+                    "Some riders just need a good send. Unlock the ability to run trips in muscle cars, and earn an extra 20% bonus on all trips."
+                },
+                {
+                    "UDer Luxury",
+                    600,
+                    "Pamper your riders with the trips done in the more luxurious segment of your collection. Earn an extra 30% bonus on all trips."
+                },
+                {
+                    "UDer Plus",
+                    1500,
+                    "Up your rideshare game with even more luxury and speed. Earn an extra 40% bonus on all trips."
+                },
+                {
+                    "UDer Sport",
+                    3000,
+                    "Unlock the ability to run trips in super cars and other top-of-the-line machines. Earn an extra 50% bonus on all trips."
+                },
+                {
+                    "UDer Hyper",
+                    7000,
+                    "Pull out all the stops and use the fastest pieces in your collection to take your passengers where they need to go. Earn an extra 60% bonus on all trips."
+                }
+            },
+            ["Taxi"] = {
+                {
+                    "Friendly Conversation",
+                    10,
+                    "Your riders think you're chill, earn an extra 10% tip bonus on all trips."
+                },
+                {
+                    "Powerful Air Fresheners",
+                    60,
+                    "Your riders appreciate not having to be subjected to the full force of the fart Tyler mistrusted... earn an extra 20% tip bonus on all trips."
+                },
+                {
+                    "Hilarious Jokes",
+                    500,
+                    "Your riders think you're legendary and will regale their friends with stories of this ride for years to come, earn an extra 30% tip bonus on all trips."
+                },
+                {
+                    "Complimentary Snacks",
+                    1500,
+                    "Food is the best way to anyone's heart, earn an extra 40% tip bonus on all trips."
+                },
+                {
+                    "Premium Sound System",
+                    3000,
+                    "Your rider's mixtape IS FIRE, and they'll make sure everyone in a three mile radius knows it too. Earn an extra 50% tip bonus on all trips."
+                },
+                {
+                    "Massage Seats",
+                    5000,
+                    "Your riders appreciate relieving the stress of work and 30 years of bad decisions while being hurtled through town at 150 MPH. Earn an extra 60% tip bonus on all trips."
+                }
+            }
+        },
+        ["Trucker"] = {
+            ["HiValue"] = {
+                {
+                    "High-Value Cargo Rank 1",
+                    25,
+                    "Unlock the first tier of jobs for transporting important cargo with the 'HIGH VALUE' attribute, such as valuable metals, expensive technology, top secret documents, and more."
+                },
+                {
+                    "High-Value Cargo Rank 2",
+                    175,
+                    "Unlock the second tier of high-value jobs, and get a 5% bonus on all rewards for shipping all types of cargo with the 'HIGH VALUE' attribute."
+                },
+                {
+                    "High-Value Cargo Rank 3",
+                    700,
+                    "Unlock the third tier of high-value jobs, and get a 10% bonus on all rewards for shipping all types of cargo with the 'HIGH VALUE' attribute."
+                },
+                {
+                    "High-Value Cargo Rank 4",
+                    1800,
+                    "Unlock the fourth tier of high-value jobs, and get a 15% bonus on all rewards for shipping all types of cargo with the 'HIGH VALUE' attribute."
+                },
+                {
+                    "High-Value Cargo Rank 5",
+                    2400,
+                    "Unlock the fifth tier of high-value jobs, and get a 20% bonus on all rewards for shipping all types of cargo with the 'HIGH VALUE' attribute."
+                },
+                {
+                    "High-Value Cargo Rank 6",
+                    5000,
+                    "Unlock the sixth tier of high-value jobs, and get a 25% bonus on all rewards for shipping all types of cargo with the 'HIGH VALUE' attribute."
+                }
+            },
+            ["Time"] = {
+                {
+                    "Time-Sensitive Delivery Rank 1",
+                    35,
+                    "Unlock jobs with time-sensitive deadlines. The earlier you make your delivery, the more of a bonus you'll get, provided your cargo isn't damaged of course."
+                },
+                {
+                    "Time-Sensitive Delivery Rank 2",
+                    100,
+                    "Get a 5% bonus on all rewards for shipping all types of cargo with the 'TIME SENSITIVE' attribute."
+                },
+                {
+                    "Time-Sensitive Delivery Rank 3",
+                    300,
+                    "Get a 10% bonus on all rewards for shipping all types of cargo with the 'TIME SENSITIVE' attribute."
+                },
+                {
+                    "Time-Sensitive Delivery Rank 4",
+                    1500,
+                    "Unlock jobs with the cement truck. Get a 15% bonus on all rewards for shipping all types of cargo with the 'TIME SENSITIVE' attribute."
+                },
+                {
+                    "Time-Sensitive Delivery Rank 5",
+                    2500,
+                    "Get a 20% bonus on all rewards for shipping all types of cargo with the 'TIME SENSITIVE' attribute."
+                },
+                {
+                    "Time-Sensitive Delivery Rank 6",
+                    4000,
+                    "Get a 25% bonus on all rewards for shipping all types of cargo with the 'TIME SENSITIVE' attribute."
+                }
+            },
+            ["HAZMAT"] = {
+                {
+                    "Flammable Cargo",
+                    10,
+                    "Unlock jobs for shipping flammable chemicals to businesses and factories. Damaging this carge could cause a firey crash and destroy your rig, drive carefully!"
+                },
+                {
+                    "Corrosive Cargo",
+                    60,
+                    "Unlock jobs for shipping corrosive chemicals to businesses and factories. Damaging this cargo could cause an environmental mess for your employers to clean up, drive carefully!"
+                },
+                {
+                    "Poisonous Cargo",
+                    500,
+                    "Unlock jobs for shipping poisonous chemicals to businesses and factories. Damaging this cargo could cause an environmental mess for your employers to clean up, drive carefully!"
+                },
+                {
+                    "Explosive Cargo",
+                    1500,
+                    "Unlock jobs for shipping explosive chemicals to businesses and factories. Damaging this cargo can have disastrous results, drive carefully!"
+                },
+                {
+                    "Biohazard Cargo",
+                    3000,
+                    "Unlock jobs for shipping biohazards to businesses, hospitals, and factories. Damaging this cargo can harm you, drive carefully!"
+                },
+                {
+                    "Radioactive Cargo",
+                    5000,
+                    "Unlock jobs for shipping radioactive chemicals to businesses and factories. Damaging this cargo can have catastrophic results, drive carefully!"
+                }
+            },
+            ["Oversize"] = {
+                {
+                    "Oversized Cargo Rank 1",
+                    300,
+                    "Unlock jobs for large cargo with the 'OVERSIZE' attribute, such as construction equipment, generators, sheds, and more."
+                },
+                {
+                    "Oversized Cargo Rank 2",
+                    600,
+                    "Get a 5% bonus on all rewards for shipping all types of cargo with the 'HIGH VALUE' attribute."
+                },
+                {
+                    "Oversized Cargo Rank 3",
+                    800,
+                    "Get a 10% bonus on all rewards for shipping all types of cargo with the 'HIGH VALUE' attribute."
+                },
+                {
+                    "Oversized Cargo Rank 4",
+                    1600,
+                    "Get a 15% bonus on all rewards for shipping all types of cargo with the 'HIGH VALUE' attribute."
+                },
+                {
+                    "Oversized Cargo Rank 5",
+                    3000,
+                    "Get a 20% bonus on all rewards for shipping all types of cargo with the 'HIGH VALUE' attribute."
+                },
+                {
+                    "Oversized Cargo Rank 6",
+                    5000,
+                    "Get a 25% bonus on all rewards for shipping all types of cargo with the 'HIGH VALUE' attribute."
+                }
+            },
+            ["Fragile"] = {
+                {
+                    "Fragile Cargo Rank 1",
+                    5,
+                    "Unlock jobs for transporting delicate cargo with the 'FRAGILE' attribute, such as foodstuffs, technology, and more."
+                },
+                {
+                    "Fragile Cargo Rank 2",
+                    50,
+                    "Unlock jobs transporting refrigerated materials, and get a 5% bonus on all rewards for shipping all types of cargo with the 'FRAGILE' attribute."
+                },
+                {
+                    "Fragile Cargo Rank 3",
+                    400,
+                    "Get a 10% bonus on all rewards for shipping all types of cargo with the 'FRAGILE' attribute."
+                },
+                {
+                    "Fragile Cargo Rank 4",
+                    800,
+                    "Unlock jobs transporting live animals, and get a 15% bonus on all rewards for shipping all types of cargo with the 'FRAGILE' attribute."
+                },
+                {
+                    "Fragile Cargo Rank 5",
+                    2500,
+                    "Get a 20% bonus on all rewards for shipping all types of cargo with the 'FRAGILE' attribute."
+                },
+                {
+                    "Fragile Cargo Rank 6",
+                    5000,
+                    "Get a 25% bonus on all rewards for shipping all types of cargo with the 'FRAGILE' attribute."
+                }
+            }
+        }
+    },
+    ["Indexes"] = {["Description"] = 3, ["SkillName"] = 1, ["PointsPrice"] = 2}
+}
