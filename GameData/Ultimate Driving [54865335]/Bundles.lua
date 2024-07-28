6d3bf8b7e22d55e2c65232634bacda90fea2514f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:41:13 +1000

    Create Bundles.lua

---
+++ GameData/Ultimate Driving [54865335]/Bundles.lua
@@ -0,0 +1,18 @@
+data = {
+    ["TruckBundle1"] = {"RebelTRX", "Silverado", "Raptor"},
+    ["GoKartBundle"] = {"GoKart", 100000},
+    ["ConceptBundle1"] = {"Saleen", "Furai", "917New"},
+    ["SupercarBundle1"] = {"488Pista", "MC20", "DBSSuper"},
+    ["DonkBundle"] = {"CullinanDonk", "HummerLimoDonk", "CountachDonk"},
+    ["HypercarBundle1"] = {"One:1", "Senna", "Regera", "Chiron"},
+    ["MantaBundle"] = {"CorvetteC8", "CorvetteC6_ZR1", "Corvette", "CorvetteC2", "CorvetteC1"},
+    ["MolsheimBundle"] = {"Royale", "Atlantic"},
+    ["StallionBundle"] = {"GT500", "Mach1_21", "Mach1_69"},
+    ["TrackToyBundle1"] = {"911_GT3RS", "CaymanGT4", "S1GT4", "AmgGT3"},
+    ["GoldenBundle"] = {"AventadorGold", 100000},
+    ["MonsterBundle"] = {"MonsterBus", "MonsterTruck", "MonsterVan"},
+    ["OffroadBundle"] = {"930Offroad", "Bronco2Door"},
+    ["StarterBundle"] = {"Wrangler", "Elise", "Model3", "Ram1500_21"},
+    ["DGBBundle"] = {"ChallengerThicc", "Trackhawk", "ChargerThicc"},
+    ["ClassicCarBundle"] = {"ModelA", "BelAir"}
+}
