d77de00fa08654574ae02717a74bc9abaf831d6c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:32:12 +1000

    Create Menu.lua

---
+++ GameData/Knife Ability Test [621129760]/Menu.lua
@@ -0,0 +1,75 @@
+data = {
+    ["RandomMythical"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = "Recieve a random LEGENDARY crate item.",
+            ["Rarity"] = "Mythical",
+            ["Name"] = "Random item",
+            ["ItemIcon"] = ""
+        }
+    },
+    ["RandomContraband"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = "Recieve a random CONTRABAND crate item.",
+            ["Rarity"] = "Contraband",
+            ["Name"] = "Random item",
+            ["ItemIcon"] = ""
+        }
+    },
+    ["RandomLegendary"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = "Recieve a random UNIQUE crate item.",
+            ["Rarity"] = "Legendary",
+            ["Name"] = "Random item",
+            ["ItemIcon"] = ""
+        }
+    },
+    ["RandomCommon"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = "Recieve a random common crate item.",
+            ["Rarity"] = "Common",
+            ["Name"] = "Random item",
+            ["ItemIcon"] = ""
+        }
+    },
+    ["RandomTrueMythical"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = "Recieve a random MYTHICAL crate item.",
+            ["Rarity"] = "TrueMythical",
+            ["Name"] = "Random item",
+            ["ItemIcon"] = ""
+        }
+    },
+    ["RandomRare"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = "Recieve a random RARE crate item.",
+            ["Rarity"] = "Rare",
+            ["Name"] = "Random item",
+            ["ItemIcon"] = ""
+        }
+    },
+    ["RdmEventItem"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Festive item",
+            ["Rarity"] = "Legendary",
+            ["Description"] = "Recieve a random FESTIVE event item.",
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://6133015374"
+        }
+    },
+    ["RdmEventItem_HW_LGCY"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = "Recieve a random HALLOWEEN crate item.",
+            ["Rarity"] = "Epic",
+            ["Name"] = "Halloween item",
+            ["ItemIcon"] = "rbxassetid://5943385905"
+        }
+    },
+    ["RandomEpic"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = "Recieve a random EPIC crate item.",
+            ["Rarity"] = "Epic",
+            ["Name"] = "Random item",
+            ["ItemIcon"] = ""
+        }
+    }
+}
