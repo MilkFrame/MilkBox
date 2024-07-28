58219067fc2c30ef6233f87a1d3acdbff2b8d160
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:30:59 +1000

    Create EXPUNGED_CONTENT.lua

---
+++ GameData/Knife Ability Test [621129760]/EXPUNGED_CONTENT.lua
@@ -0,0 +1,56 @@
+data = {
+    ["MysteryGift_01"] = {
+        ["ItemMenuData"] = {
+            ["Rarity"] = "Stock",
+            ["Description"] = [[Sorry about the hotfix disruption 02/13/2021.
+Unlocks next update!]],
+            ["Name"] = "Mystery Gift",
+            ["ItemIcon"] = "rbxassetid://4964688721"
+        }
+    },
+    ["CMToken1"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Xmas Token 2020",
+            ["isEventToken"] = true,
+            ["Rarity"] = "Rare",
+            ["Description"] = [=[Collect and craft 12 of these to unlock an 
+exclusive festive item!
+[Expires 26/12/2020!]]=],
+            ["ItemIcon"] = "rbxassetid://6132573238"
+        }
+    },
+    ["TargetUI"] = {["UpdateScale"] = "function: 0x35df9f09c087541e", ["InitializeUI"] = "function: 0x0e0e7306534caf5e"},
+    ["VictoryKnife"] = {
+        ["KnifeData"] = {},
+        ["ItemMenuData"] = {
+            ["TRADABLE"] = true,
+            ["isCrate"] = true,
+            ["Name"] = "Sakusetsu",
+            ["ShopPrice"] = 99999999,
+            ["Credit"] = "PastaFTW",
+            ["Rarity"] = "Legendary",
+            ["isWildcard"] = true,
+            ["ItemIcon"] = "rbxassetid://4997573037"
+        },
+        ["KillEffect"] = {
+            "function: 0xff4f5b74610c575e",
+            {
+                ["Headshot"] = "rbxassetid://2669271542",
+                ["Death3"] = "rbxassetid://146457047",
+                ["Death2"] = "rbxassetid://146594648",
+                ["Death1"] = "rbxassetid://146594640"
+            }
+        }
+    },
+    ["HWToken1"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Halloween Token 2020",
+            ["isEventToken"] = true,
+            ["Rarity"] = "Rare",
+            ["Description"] = [=[Collect and craft 24 of these to unlock a 
+random halloween item!
+[Expires 14/11/2020!]]=],
+            ["ItemIcon"] = "rbxassetid://5943269501"
+        }
+    }
+}
