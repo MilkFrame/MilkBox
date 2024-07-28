12596ed43ecdb8c63fc1bc0ce9dd65d31a0948b7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:30:22 +1000

    Create Gears.lua

---
+++ GameData/Knife Ability Test [621129760]/Gears.lua
@@ -0,0 +1,398 @@
+data = {
+    ["MountainDev"] = {
+        ["GearControl"] = {["CreateGearObject"] = "function: 0x5a4ebceb1cece05e"},
+        ["GearEffect"] = "function: 0xd4a729f147d2f6be",
+        ["GearData"] = {
+            ["GearAnimations"] = {["Idle"] = {2549167683, 1}, ["Drink"] = {2562494755, 1}},
+            ["GearDisplayName"] = "Mountain Dev",
+            ["GearSounds"] = {["Equip"] = {10721950, 0.75, false}, ["Slurp"] = {10722059, 2.5, false}},
+            ["GripOffset"] = CFrame.new(
+                -0.0599999987,
+                -1.05999994,
+                -0.180000007,
+                0.900435865,
+                0.00450221682,
+                -0.434965521,
+                0.434988678,
+                -0.0100285057,
+                0.900380075,
+                -0.000308347866,
+                -0.999939561,
+                -0.0109884404
+            ),
+            ["GearIcon"] = "rbxassetid://4236876649"
+        },
+        ["ItemMenuData"] = {
+            ["requirementText"] = "requires MLG Pass",
+            ["RedirectExtra"] = "MLGPass",
+            ["Name"] = "Mountain Dev",
+            ["Rarity"] = "Epic",
+            ["ShopPrice"] = 100,
+            ["GamepassId"] = 2379688,
+            ["RobuxPrice"] = 350,
+            ["ItemIcon"] = "rbxassetid://4236876649"
+        },
+        ["GearServerEffect"] = "function: 0xc3f9d6494ea65efe"
+    },
+    ["NoobSign"] = {
+        ["GearControl"] = {["CreateGearObject"] = "function: 0xbcdfe50bfcd6565e"},
+        ["GearData"] = {
+            ["GearAnimations"] = {["Idle"] = {2549167683, 1}},
+            ["GearDisplayName"] = "Sign",
+            ["GearSounds"] = {},
+            ["GripOffset"] = CFrame.new(-0.0350000001, -1.12, -1.97500002, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["GearIcon"] = "rbxassetid://22958466"
+        },
+        ["GearEffect"] = "function: 0x58fd60780e63ca1e",
+        ["ItemMenuData"] = {
+            ["Name"] = "Noob sign",
+            ["levelRequirement"] = 5,
+            ["IsFree"] = true,
+            ["Rarity"] = "Common",
+            ["ShopPrice"] = 5,
+            ["ItemIcon"] = "rbxassetid://22958466"
+        },
+        ["GearServerEffect"] = "function: 0xd00eca303e3b85fe"
+    },
+    ["Spork"] = {
+        ["GearControl"] = {["CreateGearObject"] = "function: 0x64dce58b53904ede"},
+        ["GearData"] = {
+            ["GearAnimations"] = {["Idle"] = {2549167683, 1}, ["Throw"] = {2549238936, 0.65}},
+            ["GearDisplayName"] = "Spork",
+            ["GearSounds"] = {["Slash"] = {2478100279, 1, false}},
+            ["GripOffset"] = CFrame.new(
+                -0.0299999993,
+                -1.05999994,
+                -0.419999987,
+                1,
+                0,
+                0,
+                0,
+                0.0607588924,
+                0.998152494,
+                0,
+                -0.998152494,
+                0.0607588924
+            ),
+            ["GearIcon"] = "rbxassetid://107441224"
+        },
+        ["GearEffect"] = "function: 0x0ed9c59c5db16e1e",
+        ["ItemMenuData"] = {
+            ["oldPrice"] = 0,
+            ["Name"] = "Spork",
+            ["Rarity"] = "Common",
+            ["ShopPrice"] = 25,
+            ["ItemIcon"] = "rbxassetid://107441224"
+        },
+        ["GearServerEffect"] = "function: 0x9f9d68c1a554092e"
+    },
+    ["MoneyBag"] = {
+        ["GearControl"] = {["CreateGearObject"] = "function: 0x84efb9ae1d4e2afe"},
+        ["GearData"] = {
+            ["GearAnimations"] = {["Idle"] = {2549167683, 1}},
+            ["GearDisplayName"] = "Dice",
+            ["GearSounds"] = {["MoneyDrop"] = {{16720281, 16720502, 16720508}, 1, false}},
+            ["GripOffset"] = CFrame.new(
+                0,
+                -1,
+                0.75,
+                -1,
+                8.74227766e-08,
+                0,
+                3.82137093e-15,
+                4.37113883e-08,
+                -1,
+                -8.74227766e-08,
+                -1,
+                -4.37113883e-08
+            ),
+            ["GearIcon"] = "rbxassetid://16659163"
+        },
+        ["GearEffect"] = "function: 0xb2c1b4514fef26be",
+        ["ItemMenuData"] = {
+            ["Name"] = "Money Bag",
+            ["ShopPrice"] = 100,
+            ["Rarity"] = "Rare",
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://16659163"
+        },
+        ["GearServerEffect"] = "function: 0x737fc6c203322ffe"
+    },
+    ["SporkOFDOOM"] = {
+        ["GearControl"] = {["CreateGearObject"] = "function: 0xe254d009dd138bbe"},
+        ["GearData"] = {
+            ["GearAnimations"] = {["Idle"] = {2549167683, 1}, ["Throw"] = {2549238936, 0.65}},
+            ["GearDisplayName"] = "Spork",
+            ["GearSounds"] = {["Slash"] = {2478100279, 1, false}},
+            ["GripOffset"] = CFrame.new(
+                -0.0299999993,
+                -1.05999994,
+                -0.419999987,
+                1,
+                0,
+                0,
+                0,
+                0.0607588924,
+                0.998152494,
+                0,
+                -0.998152494,
+                0.0607588924
+            ),
+            ["GearIcon"] = "rbxassetid://107441224"
+        },
+        ["GearEffect"] = "function: 0x346f2f4382d5b09e",
+        ["ItemMenuData"] = {
+            ["Name"] = "Spork OF DOOM",
+            ["ShopPrice"] = 100000,
+            ["HideFromShop"] = true,
+            ["Rarity"] = "Mythical",
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://107441224"
+        },
+        ["GearServerEffect"] = "function: 0x675d448536bf2bbe"
+    },
+    ["EpicDuck"] = {
+        ["GearControl"] = {["CreateGearObject"] = "function: 0xbd63d9b972d6ba3e"},
+        ["GearData"] = {
+            ["GearAnimations"] = {["Idle"] = {2549167683, 1}, ["Throw"] = {2549238936, 0.65}},
+            ["GearDisplayName"] = "Duck",
+            ["GearSounds"] = {["Slash"] = {281261799, 1, false}},
+            ["GripOffset"] = CFrame.new(
+                -0.0599999987,
+                -1.11000001,
+                -0.230000004,
+                1,
+                0,
+                0,
+                0,
+                0.000796274282,
+                0.999999702,
+                0,
+                -0.999999702,
+                0.000796274282
+            ),
+            ["GearIcon"] = "rbxassetid://2562583734"
+        },
+        ["GearEffect"] = "function: 0x4527bbb40414e5be",
+        ["ItemMenuData"] = {
+            ["Name"] = "Epic Duck",
+            ["ShopPrice"] = 500,
+            ["Rarity"] = "Epic",
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://2562583734"
+        },
+        ["GearServerEffect"] = "function: 0x615124097d25253e"
+    },
+    ["ThrowSanic"] = {
+        ["GearControl"] = {["CreateGearObject"] = "function: 0x7590e51717ddbabe"},
+        ["GearData"] = {
+            ["GearAnimations"] = {["Idle"] = {2549167683, 1}, ["Throw"] = {2549238936, 0.65}},
+            ["GearDisplayName"] = "Sanic",
+            ["GearSounds"] = {["Slash"] = {2478100279, 0.4, false}},
+            ["GripOffset"] = CFrame.new(
+                -0.00999999978,
+                -1.01999998,
+                -0.579999983,
+                1,
+                0,
+                0,
+                0,
+                0.0107961744,
+                0.999941707,
+                0,
+                -0.999941707,
+                0.0107961744
+            ),
+            ["GearIcon"] = "rbxassetid://135813071"
+        },
+        ["GearEffect"] = "function: 0x61e860d4625e897e",
+        ["ItemMenuData"] = {
+            ["Name"] = "Throw Sanic",
+            ["ShopPrice"] = 1000,
+            ["Rarity"] = "Epic",
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://135813071"
+        },
+        ["GearServerEffect"] = "function: 0x1827cbda8c760ade"
+    },
+    ["TurkeyLeg"] = {
+        ["GearControl"] = {["CreateGearObject"] = "function: 0x11e7eda31623d27e"},
+        ["GearData"] = {
+            ["GearAnimations"] = {["Idle"] = {2549167683, 1}, ["Throw"] = {2549238936, 0.65}},
+            ["GearDisplayName"] = "Turkey",
+            ["GearSounds"] = {["Slash"] = {2478100279, 1, false}},
+            ["GripOffset"] = CFrame.new(
+                -0.00999999978,
+                -1.01999998,
+                -0.579999983,
+                1,
+                0,
+                0,
+                0,
+                0.0107961744,
+                0.999941707,
+                0,
+                -0.999941707,
+                0.0107961744
+            ),
+            ["GearIcon"] = "rbxassetid://282614289"
+        },
+        ["GearEffect"] = "function: 0x9bcc78d9a265bd5e",
+        ["ItemMenuData"] = {
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 45,
+            ["Name"] = "Turkey Leg",
+            ["ItemIcon"] = "rbxassetid://282614289"
+        },
+        ["GearServerEffect"] = "function: 0x6492d5ee3bf47c9e"
+    },
+    ["Pizza"] = {
+        ["GearControl"] = {["CreateGearObject"] = "function: 0x8b5817d05251185e"},
+        ["GearEffect"] = "function: 0x6877177681ec181e",
+        ["GearData"] = {
+            ["GearAnimations"] = {["Idle"] = {2549167683, 1}},
+            ["GearDisplayName"] = "Pizza",
+            ["GearSounds"] = {["Pizza"] = {22593428, 1.5, false}},
+            ["GripOffset"] = CFrame.new(
+                -0.0599999987,
+                -1.29999995,
+                -0.180000007,
+                -4.37113883e-08,
+                0,
+                -1,
+                1,
+                -4.37113883e-08,
+                -4.37113883e-08,
+                -4.37113883e-08,
+                -1,
+                1.91068547e-15
+            ),
+            ["GearIcon"] = "rbxassetid://22595979"
+        },
+        ["ItemMenuData"] = {
+            ["Name"] = "Pizza",
+            ["levelRequirement"] = 7,
+            ["Rarity"] = "Common",
+            ["ShopPrice"] = 8,
+            ["ItemIcon"] = "rbxassetid://22595979"
+        },
+        ["GearServerEffect"] = "function: 0xdba8d4df2b00697e"
+    },
+    ["BloxyCola"] = {
+        ["GearControl"] = {["CreateGearObject"] = "function: 0x585d134893ef771e"},
+        ["GearEffect"] = "function: 0x7cbe0b8bad6df3de",
+        ["GearData"] = {
+            ["GearAnimations"] = {["Idle"] = {2549167683, 1}, ["Drink"] = {2562494755, 1}},
+            ["GearDisplayName"] = "Bloxy Cola",
+            ["GearSounds"] = {["Equip"] = {10721950, 0.75, false}, ["Slurp"] = {10722059, 2.5, false}},
+            ["GripOffset"] = CFrame.new(
+                -0.0599999987,
+                -1.05999994,
+                -0.180000007,
+                0.900435865,
+                0.00450221682,
+                -0.434965521,
+                0.434988678,
+                -0.0100285057,
+                0.900380075,
+                -0.000308347866,
+                -0.999939561,
+                -0.0109884404
+            ),
+            ["GearIcon"] = "rbxassetid://10472127"
+        },
+        ["ItemMenuData"] = {
+            ["Name"] = "Bloxy Cola",
+            ["levelRequirement"] = 1,
+            ["IsFree"] = true,
+            ["Rarity"] = "Common",
+            ["ShopPrice"] = 1,
+            ["ItemIcon"] = "rbxassetid://10472127"
+        },
+        ["GearServerEffect"] = "function: 0x1609ae4bd08e84be"
+    },
+    ["FakeC4"] = {
+        ["GearControl"] = {["CreateGearObject"] = "function: 0x4ad67ee39a74a6de"},
+        ["GearEffect"] = "function: 0x56e6fe2e4ce7a29e",
+        ["GearData"] = {
+            ["GearAnimations"] = {["Idle"] = {2549167683, 1}},
+            ["GearDisplayName"] = "C4 Explosive",
+            ["GearSounds"] = {["Equip"] = {5932245606, 1}},
+            ["GripOffset"] = CFrame.new(-0.583621979, -0.927121639, -0.580247402, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+            ["GearIcon"] = "rbxassetid://4191593108"
+        },
+        ["ItemMenuData"] = {
+            ["PurchaseLimit"] = 3,
+            ["Name"] = "Plastic Explosive",
+            ["ShopPrice"] = 1249,
+            ["Rarity"] = "Legendary",
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://4191593108"
+        },
+        ["GearServerEffect"] = "function: 0xe0dbafda965a83be"
+    },
+    ["HeartBalloon"] = {
+        ["GearControl"] = {["CreateGearObject"] = "function: 0xeab604f2e773ccde"},
+        ["GearEffect"] = "function: 0xf5570930b4fd4c9e",
+        ["GearData"] = {
+            ["GearAnimations"] = {["Idle"] = {2549167683, 1}},
+            ["GearDisplayName"] = "Heart Balloon",
+            ["GearSounds"] = {},
+            ["GripOffset"] = CFrame.new(
+                -0.200000003,
+                -0.800000012,
+                -1.5,
+                1,
+                0,
+                0,
+                0,
+                -4.37113883e-08,
+                1,
+                0,
+                -1,
+                -4.37113883e-08
+            ),
+            ["GearIcon"] = "rbxassetid://146063920"
+        },
+        ["ItemMenuData"] = {
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 200,
+            ["Name"] = "Heart Balloon",
+            ["ItemIcon"] = "rbxassetid://146063920"
+        },
+        ["GearServerEffect"] = "function: 0xb4d2d01a50af9bfe"
+    },
+    ["Sandvich"] = {
+        ["GearControl"] = {["CreateGearObject"] = "function: 0x096214ddc4713ade"},
+        ["GearEffect"] = "function: 0x1455108d94e2369e",
+        ["GearData"] = {
+            ["GearAnimations"] = {["Eat"] = {2560870685, 1}, ["Idle"] = {2560906957, 1}, ["Equip"] = {2560901990, 1}},
+            ["GearDisplayName"] = "Sandvich",
+            ["GearSounds"] = {["Consume"] = {151050515, 2.5, false}, ["Equip"] = {153647514, 0.75, false}},
+            ["GripOffset"] = CFrame.new(
+                -0.209999993,
+                -0.99000001,
+                -0.725000024,
+                -0.00420370838,
+                -0.999991179,
+                0,
+                -0.706255376,
+                0.00296891807,
+                0.70795089,
+                -0.707944632,
+                0.00297601917,
+                -0.706261635
+            ),
+            ["GearIcon"] = "rbxassetid://2562584175"
+        },
+        ["ItemMenuData"] = {
+            ["Name"] = "Sandvich",
+            ["Description"] = "What was that Sandvich? Kill them all? Good idea!",
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 50,
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://2562584175"
+        },
+        ["GearServerEffect"] = "function: 0x4e2f55048eeb191e"
+    }
+}
