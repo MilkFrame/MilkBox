e6ba0d5b85d5326df64b3025cab97cea2f257a16
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:31:11 +1000

    Create FollowersContent.lua

---
+++ GameData/Knife Ability Test [621129760]/FollowersContent.lua
@@ -0,0 +1,145 @@
+data = {
+    ["Unicorn"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Fluffy Unicorn",
+            ["levelRequirement"] = 4,
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 35,
+            ["ItemIcon"] = "rbxassetid://4234932968"
+        },
+        ["FollowerControl"] = "function: 0xf1244a09a70671fe"
+    },
+    ["MrTentacles"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Mr Tentacles",
+            ["levelRequirement"] = 8,
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 90,
+            ["ItemIcon"] = "rbxassetid://4234934125"
+        },
+        ["FollowerControl"] = "function: 0xedf4cdc9945bf1be"
+    },
+    ["BalloonJax"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Balloon Jax",
+            ["levelRequirement"] = 32,
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 200,
+            ["ItemIcon"] = "rbxassetid://4935528772"
+        },
+        ["FollowerControl"] = "function: 0xf7c7531a65de8dde"
+    },
+    ["AkuAku"] = {
+        ["FollowerControl"] = "function: 0xda30dbef2b9c8d9e",
+        ["ItemMenuData"] = {
+            ["Name"] = "Aku Aku",
+            ["ShopPrice"] = 350,
+            ["Rarity"] = "Epic",
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://4045896411"
+        }
+    },
+    ["DogePet"] = {
+        ["ItemMenuData"] = {
+            ["requirementText"] = "requires MLG Pass",
+            ["RedirectExtra"] = "MLGPass",
+            ["Name"] = "Doge",
+            ["Rarity"] = "Epic",
+            ["ShopPrice"] = 250,
+            ["GamepassId"] = 2379688,
+            ["RobuxPrice"] = 350,
+            ["ItemIcon"] = "rbxassetid://4236618466"
+        },
+        ["FollowerControl"] = "function: 0xc55578632c935d3e"
+    },
+    ["BongoCat"] = {
+        ["FollowerControl"] = "function: 0x228761abce1565be",
+        ["ItemMenuData"] = {
+            ["Name"] = "Bongo Cat",
+            ["ShopPrice"] = 1000,
+            ["Rarity"] = "Epic",
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://4017695395"
+        }
+    },
+    ["Ghosdeeri"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Ghosdeeri",
+            ["levelRequirement"] = 16,
+            ["ShopPrice"] = 145,
+            ["Rarity"] = "Rare",
+            ["HasParticles"] = true,
+            ["ItemIcon"] = "rbxassetid://4234926979"
+        },
+        ["FollowerControl"] = "function: 0x682ce2dde66b7a9e"
+    },
+    ["GreenBalloon"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Green Balloon",
+            ["levelRequirement"] = 1,
+            ["IsFree"] = true,
+            ["Rarity"] = "Common",
+            ["ShopPrice"] = 25,
+            ["ItemIcon"] = "rbxassetid://4148095984"
+        },
+        ["FollowerControl"] = "function: 0x16342ff612a253fe"
+    },
+    ["CatFollower"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Portable Sun",
+            ["ShopPrice"] = 450,
+            ["Rarity"] = "Epic",
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://5897308431"
+        },
+        ["FollowerControl"] = "function: 0x30e1745a1ba364be"
+    },
+    ["Phantasm"] = {
+        ["ItemMenuData"] = {
+            ["Name"] = "Phantasm",
+            ["Description"] = "[Halloween 2020 item]",
+            ["Rarity"] = "Rare",
+            ["ShopPrice"] = 250,
+            ["ItemIcon"] = "rbxassetid://5917487396"
+        },
+        ["FollowerControl"] = "function: 0xfa8891a05f658b9e"
+    },
+    ["RedBalloon"] = {
+        ["ItemMenuData"] = {
+            ["IsFree"] = true,
+            ["Name"] = "Red Balloon",
+            ["levelRequirement"] = 2,
+            ["HideFromShop"] = true,
+            ["Rarity"] = "Common",
+            ["ShopPrice"] = 30,
+            ["ItemIcon"] = "rbxassetid://4148094900"
+        },
+        ["FollowerControl"] = "function: 0x24301129553652fe"
+    },
+    ["Bombinomicon"] = {
+        ["FollowerControl"] = "function: 0x50590c79e445cebe",
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 150,
+            ["Name"] = "Bombinomicon",
+            ["PreviewSettings"] = {["ZOffset"] = 6.5, ["FOV"] = 35},
+            ["HideFromShop"] = true,
+            ["Rarity"] = "Rare",
+            ["TRADABLE"] = true,
+            ["ItemIcon"] = "rbxassetid://4017696897"
+        }
+    },
+    ["CoinCloud"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 1000,
+            ["RobuxPrice"] = 95,
+            ["requirementText"] = "requires VIP Pass",
+            ["RedirectExtra"] = "VIPPass",
+            ["Name"] = "Coin Cloud",
+            ["modelUpdateFunction"] = "function: 0x77f811f092cc5c1e",
+            ["Rarity"] = "Legendary",
+            ["GamepassId"] = 2385636,
+            ["ItemIcon"] = "rbxassetid://4116852596"
+        },
+        ["FollowerControl"] = "function: 0x7f7e09a935b0b57e"
+    }
+}
