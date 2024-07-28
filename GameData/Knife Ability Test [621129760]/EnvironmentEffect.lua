03bfa6e9376a8103bc6fb888b377acf19686f753
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:29:47 +1000

    Create EnvironmentEffect.lua

---
+++ GameData/Knife Ability Test [621129760]/EnvironmentEffect.lua
@@ -0,0 +1,22 @@
+data = {
+    ["WaterEffect"] = {["KillEffect"] = {"function: 0x1bd45358ebfe51ce", {["Death"] = "rbxasset://sounds/uuhhh.mp3"}}},
+    ["ExplosionDeath"] = {["KillEffect"] = {"function: 0x63825afb98f4b14e", {["Oof"] = "rbxasset://sounds/uuhhh.mp3"}}},
+    ["MetalBatEffect"] = {["KillEffect"] = {"function: 0xf557a17c916511de", {["Oof"] = "rbxasset://sounds/uuhhh.mp3"}}},
+    ["Combust"] = {["KillEffect"] = {"function: 0x49729df2a5ca7eae", {["Flame"] = "rbxassetid://142472270"}}},
+    ["LavaEffect"] = {
+        ["KillEffect"] = {"function: 0x4674a0362989a17e", {["Explosive"] = "rbxassetid://142070127"}, true}
+    },
+    ["Skelefy"] = {["KillEffect"] = {"function: 0xe93bbbdc4b24a1fe", {["rawr"] = "rbxassetid://248568826"}}},
+    ["BOOM"] = {["KillEffect"] = {"function: 0xb7eff3533e7c601e", {["Explosive"] = "rbxassetid://142070127"}}},
+    ["Z"] = {
+        ["KillEffect"] = {
+            "function: 0x1003e496568ee21e",
+            {["Headshot"] = "rbxassetid://2669271542", ["Groan"] = "rbxassetid://142675288"}
+        }
+    },
+    ["IceIX"] = {["KillEffect"] = {"function: 0x0f0657042a14f93e", {["b"] = "rbxassetid://5894917588"}}},
+    ["ENV_404"] = {["KillEffect"] = {"function: 0x29fa1818bcf0e26e", {["ERROR"] = "rbxassetid://160715357"}}},
+    ["KaboomTauntKillEffect"] = {
+        ["KillEffect"] = {"function: 0xbc2e3e4b8d00a2fe", {["Explosive"] = "rbxassetid://142070127"}, true}
+    }
+}
