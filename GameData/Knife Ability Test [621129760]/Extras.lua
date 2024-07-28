aa3b75a29998106fc99ae978b2014cef9377e427
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-28 22:30:11 +1000

    Create Extras.lua

---
+++ GameData/Knife Ability Test [621129760]/Extras.lua
@@ -0,0 +1,158 @@
+data = {
+    ["TeslaKnifePass"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = "Unleash the power of lightning to chain zap your victims.",
+            ["contextDescription"] = "Unlocks the Tesla Knife Power",
+            ["RefundVoucherName"] = "TeslaPassVoucher",
+            ["RobuxPrice"] = 600,
+            ["Name"] = "Tesla",
+            ["Rarity"] = "Legendary",
+            ["GamepassId"] = 2429055,
+            ["ShopPrice"] = 3000,
+            ["IsWhite"] = true,
+            ["ItemIcon"] = "rbxassetid://2172119052"
+        },
+        ["ShopModel"] = {
+            ["ReplicatedEffect[0]"] = "function: 0x46833cb3b3ad7e0e",
+            ["ClientEffect[0]"] = "function: 0x302c49210646e55e"
+        }
+    },
+    ["VIPPass"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = "Unlocks special chat emotes, an exclusive VIP Knife and a golden [VIP] chat tag!",
+            ["ShopPrice"] = 95,
+            ["Name"] = "VIP",
+            ["contextDescription"] = "Unlocks the [VIP] Chat Tag, Coin Cloud Pet, and Australium Knife",
+            ["GamepassId"] = 2385636,
+            ["RefundVoucherName"] = "VIPPassVoucher",
+            ["RobuxPrice"] = 95,
+            ["ItemIcon"] = "rbxassetid://4111130487"
+        }
+    },
+    ["EffectTransferPass"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 120,
+            ["Name"] = "Kill Effect Transfer",
+            ["contextDescription"] = "Allows your Revolver to use your selected Knife's Kill Effect",
+            ["HideFromShop"] = true,
+            ["GamepassId"] = 2343632,
+            ["RobuxPrice"] = 120,
+            ["ItemIcon"] = "rbxassetid://4113949599"
+        }
+    },
+    ["RapidFireExtra"] = {
+        ["ItemMenuData"] = {
+            ["SortPrice"] = 10000,
+            ["ShopPrice"] = 8500,
+            ["HideFromShop"] = true,
+            ["RobuxPrice"] = 3750,
+            ["Name"] = "Rapid-Fire",
+            ["Rarity"] = "Legendary",
+            ["Description"] = [[On sale until January 4th!
+This item is pending a visual overhaul]],
+            ["GamepassId"] = 5695199,
+            ["ItemIcon"] = "rbxassetid://4115104690"
+        }
+    },
+    ["RadioPass"] = {
+        ["ItemMenuData"] = {
+            ["REDACTED"] = true,
+            ["Name"] = "Radio [LEGACY]",
+            ["contextDescription"] = "Allows you to play your own music in-game!",
+            ["HideFromShop"] = true,
+            ["ShopPrice"] = 350,
+            ["Description"] = "Allows you to play your own music in-game!",
+            ["ItemIcon"] = "rbxassetid://2188432215"
+        }
+    },
+    ["SpamKnifePass"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = "Throw your knives at hyper speed!!",
+            ["contextDescription"] = "Unlocks the Spam Knife Power",
+            ["HideFromShop"] = true,
+            ["RobuxPrice"] = 9001,
+            ["Name"] = "Spam Knife",
+            ["Rarity"] = "Mythical",
+            ["GamepassId"] = 2317106,
+            ["ShopPrice"] = 9000,
+            ["IsWhite"] = true,
+            ["ItemIcon"] = "rbxassetid://1854970804"
+        }
+    },
+    ["MLGPass"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 199,
+            ["Name"] = "MLG",
+            ["contextDescription"] = "Unlocks the MLG Knife, Conspiracy Knife Effect, Doge Pet, and MLG Radio",
+            ["GamepassId"] = 2379688,
+            ["Description"] = "Unlocks the MLG Knife, MLG Radio, and the Doge pet.",
+            ["RobuxPrice"] = 400,
+            ["ItemIcon"] = "rbxassetid://4231642392"
+        }
+    },
+    ["SprayPass"] = {
+        ["ItemMenuData"] = {
+            ["GamepassId"] = 5689532,
+            ["ShopPrice"] = 60,
+            ["Name"] = "Spray",
+            ["contextDescription"] = "Allows you to spray your own images in game! (T-Key)",
+            ["HideFromShop"] = true,
+            ["Description"] = "Allows you to spray images, press T to apply to a wall!",
+            ["RobuxPrice"] = 60,
+            ["ItemIcon"] = "rbxassetid://4110364791"
+        }
+    },
+    ["MVPPass"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = "Unlocks animated chat emotes and a red [MVP] chat tag!",
+            ["ShopPrice"] = 150,
+            ["Name"] = "MVP",
+            ["contextDescription"] = "Unlocks the [MVP] Chat Tag and 2.0x Bounty rewards!",
+            ["GamepassId"] = 2431975,
+            ["RefundVoucherName"] = "MVPPassVoucher",
+            ["RobuxPrice"] = 300,
+            ["ItemIcon"] = "rbxassetid://4287853206"
+        }
+    },
+    ["ShotgunKnifePass"] = {
+        ["ItemMenuData"] = {
+            ["Description"] = "Allows you to throw up to 15 knives at once!",
+            ["contextDescription"] = "Unlocks the Shotgun Knife Power",
+            ["RefundVoucherName"] = "ShotgunPassVoucher",
+            ["RobuxPrice"] = 399,
+            ["Name"] = "Shotgun Knife",
+            ["Rarity"] = "Epic",
+            ["GamepassId"] = 2355258,
+            ["ShopPrice"] = 200,
+            ["IsWhite"] = true,
+            ["ItemIcon"] = "rbxassetid://2165439768"
+        }
+    },
+    ["ShardsPass"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 175,
+            ["contextDescription"] = "Unlocks all of the Shard Knife Skins!",
+            ["HideFromShop"] = true,
+            ["RefundVoucherName"] = "ShardsPassVoucher",
+            ["RobuxPrice"] = 200,
+            ["Name"] = "Shard Knife Bundle",
+            ["modelUpdateFunction"] = "function: 0x7db4ace67688035e",
+            ["Description"] = "Unlocks the shard knife skins.",
+            ["GamepassId"] = 2343473,
+            ["ItemIcon"] = "rbxassetid://3093648894"
+        }
+    },
+    ["RadioPass2"] = {
+        ["ItemMenuData"] = {
+            ["ShopPrice"] = 0,
+            ["contextDescription"] = "Allows you to play your own music for everyone to hear!",
+            ["RefundVoucherName"] = "RadioPassVoucher",
+            ["RobuxPrice"] = 200,
+            ["Name"] = "Radio",
+            ["REDACTED"] = true,
+            ["GamepassId"] = 10200768,
+            ["Description"] = "Allows you to play your own music for everyone to hear!",
+            ["ItemIcon"] = ""
+        }
+    }
+}
