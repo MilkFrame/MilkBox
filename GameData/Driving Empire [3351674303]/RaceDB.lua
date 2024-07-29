444ccd22d8707d615e11245e51f52c6de9539584
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-29 22:36:29 +1000

    Create RaceDB.lua

---
+++ GameData/Driving Empire [3351674303]/RaceDB.lua
@@ -0,0 +1,130 @@
+data = {
+    ["leaderboardRewards"] = {
+        ["Exhibition"] = {
+            ["CrossCountry"] = {500000, 225000, 125000, 50000, 40000, 30000, 20000, 10000},
+            ["Drag"] = {75000, 33700, 18700, 7500, 6000, 4500, 3000, 1500},
+            ["Highway"] = {150000, 67500, 37500, 15000, 12000, 9000, 6000, 3000},
+            ["Circuit"] = {450000, 202500, 112500, 45000, 36000, 27000, 18000, 9000}
+        },
+        ["TimeTrial"] = {
+            ["CrossCountry"] = {100000, 45000, 25000, 10000, 8000, 6000, 4000, 2000},
+            ["Drag"] = {15000, 7000, 4000, 2000, 1000, 1000, 1000, 0},
+            ["Highway"] = {30000, 14000, 8000, 3000, 2000, 2000, 1000, 1000},
+            ["Circuit"] = {90000, 41000, 23000, 9000, 7000, 5000, 4000, 2000}
+        }
+    },
+    ["music"] = {{9159001259, 47}, {9165608549, 67, 0.7}, {9165612171, 22, 1}},
+    ["races"] = {
+        ["ClassicRoblox"] = {
+            ["Type"] = "Sprint",
+            ["Description"] = "Race on a Classic Roblox map!",
+            ["tpID"] = "classicRoblox",
+            ["thumbnail"] = "rbxassetid://12930728381",
+            ["Chassis"] = "Land",
+            ["MinPlayers"] = 2,
+            ["Payout"] = 27500,
+            ["Name"] = "Classic Roblox",
+            ["Category"] = "Track",
+            ["TimeLimit"] = 240,
+            ["MinTime"] = 20,
+            ["MaxLaps"] = 1,
+            ["MaxPlayers"] = 10
+        },
+        ["Drawbridge"] = {
+            ["Type"] = "Sprint",
+            ["Description"] = "Race over the Drawbridge!",
+            ["tpID"] = "drawbridge",
+            ["thumbnail"] = "rbxassetid://12930728381",
+            ["Chassis"] = "Land",
+            ["MinPlayers"] = 2,
+            ["Payout"] = 22300,
+            ["Name"] = "Drawbridge Sprint",
+            ["Category"] = "Drawbridge",
+            ["TimeLimit"] = 240,
+            ["MinTime"] = 59,
+            ["MaxLaps"] = 1,
+            ["MaxPlayers"] = 10
+        },
+        ["Circuit"] = {
+            ["Type"] = "Circuit",
+            ["Description"] = "2 Laps Around Joya Costa Speedway!",
+            ["tpID"] = "circuit",
+            ["thumbnail"] = "rbxassetid://12930729190",
+            ["Chassis"] = "Land",
+            ["MinPlayers"] = 2,
+            ["Payout"] = 51200,
+            ["Name"] = "Circuit Race",
+            ["Category"] = "Track",
+            ["TimeLimit"] = 360,
+            ["MinTime"] = 150,
+            ["MaxLaps"] = 2,
+            ["MaxPlayers"] = 10
+        },
+        ["CrossCountry"] = {
+            ["Type"] = "Sprint",
+            ["Description"] = "Around the world race!",
+            ["tpID"] = "cc",
+            ["thumbnail"] = "rbxassetid://12930728984",
+            ["Chassis"] = "Land",
+            ["MinPlayers"] = 2,
+            ["Payout"] = 56500,
+            ["Name"] = "Cross Country Race",
+            ["Category"] = "Backroads",
+            ["TimeLimit"] = 360,
+            ["MinTime"] = 146,
+            ["MaxLaps"] = 1,
+            ["MaxPlayers"] = 22
+        },
+        ["Drag"] = {
+            ["Type"] = "Sprint",
+            ["Description"] = "Sprint down Vine Hill Dragway!",
+            ["tpID"] = "drag",
+            ["thumbnail"] = "rbxassetid://12930728794",
+            ["Chassis"] = "Land",
+            ["MinPlayers"] = 2,
+            ["Payout"] = 4200,
+            ["Name"] = "Drag Race",
+            ["Category"] = "Dragstrip",
+            ["TimeLimit"] = 25,
+            ["MinTime"] = 3.7,
+            ["MaxLaps"] = 1,
+            ["MaxPlayers"] = 2
+        },
+        ["Highway"] = {
+            ["Type"] = "Sprint",
+            ["Description"] = "Rip down the Highway as fast as you can!",
+            ["tpID"] = "highway",
+            ["thumbnail"] = "rbxassetid://12930727491",
+            ["Chassis"] = "Land",
+            ["MinPlayers"] = 2,
+            ["Payout"] = 9800,
+            ["Name"] = "Highway Race",
+            ["Category"] = "Highway",
+            ["TimeLimit"] = 110,
+            ["MinTime"] = 23,
+            ["MaxLaps"] = 1,
+            ["MaxPlayers"] = 8
+        },
+        ["SofiaBay"] = {
+            ["Type"] = "Sprint",
+            ["Description"] = "Dash through the Sofia Bay area",
+            ["tpID"] = "sofiabay",
+            ["IsNew"] = true,
+            ["Chassis"] = "Land",
+            ["thumbnail"] = "rbxassetid://13892062660",
+            ["MinPlayers"] = 2,
+            ["Name"] = "Sofia Bay Race",
+            ["Category"] = "Backroads",
+            ["TimeLimit"] = 200,
+            ["MinTime"] = 55,
+            ["MaxLaps"] = 1,
+            ["MaxPlayers"] = 10
+        }
+    },
+    ["colors"] = {
+        {Color3.fromRGB(255, 184, 2), Color3.fromRGB(255, 240, 164)},
+        {Color3.fromRGB(204, 204, 204), Color3.fromRGB(220, 220, 220)},
+        {Color3.fromRGB(161, 107, 0), Color3.fromRGB(182, 121, 0)},
+        {Color3.fromRGB(255, 255, 255), Color3.fromRGB(232, 232, 232)}
+    }
+}
