129354ec987d4743f7e6a0d7a534823d980f5191
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-29 22:37:33 +1000

    Create Sorts.lua

---
+++ GameData/Driving Empire [3351674303]/Sorts.lua
@@ -0,0 +1,19 @@
+data = {
+    ["brandsMap"] = {{["sorts"] = {{"LowHigh", "A to Z"}, {"HighLow", "Z to A"}}, ["div"] = "Alphabetical"}},
+    ["sortVehicle"] = "function: 0xe7d038b1a861978b",
+    ["default"] = {["sort"] = "LowHigh", ["div"] = "Price"},
+    ["map"] = {
+        {["sorts"] = {{"LowHigh", "Price - Low to High"}, {"HighLow", "Price - High to Low"}}, ["div"] = "Price"},
+        {["sorts"] = {{"LowHigh", "Year - Low to High"}, {"HighLow", "Year - High to Low"}}, ["div"] = "Year"},
+        {
+            ["sorts"] = {{"LowHigh", "Top Speed - Low to High"}, {"HighLow", "Top Speed - High to Low"}},
+            ["div"] = "TopSpeed"
+        },
+        {
+            ["sorts"] = {{"LowHigh", "Handling - Worst to Best"}, {"HighLow", "Handling - Best to Worst"}},
+            ["div"] = "Handling"
+        },
+        {["sorts"] = {{"Track", "Track Vehicles"}}, ["div"] = "Gamepasses"},
+        {["sorts"] = {{"Bodykits", "Body Customization"}, {"Doors", "Doors"}}, ["div"] = "Features"}
+    }
+}
