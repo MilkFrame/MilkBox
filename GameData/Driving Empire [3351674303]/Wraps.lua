ff3edecc3d8c749a004a7241dd5bea2e849eda09
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-29 22:33:37 +1000

    Create Wraps.lua

---
+++ GameData/Driving Empire [3351674303]/Wraps.lua
@@ -0,0 +1,449 @@
+data = {
+    ["items"] = {
+        ["80c"] = {
+            ["Icon"] = 11826816918,
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Candy Striped",
+            ["DisplayOrder"] = 10000000,
+            ["Presents"] = 160,
+            ["Wrap"] = {["tile"] = 2},
+            ["GlobalIdentifier"] = "Winter 2022",
+            ["MaterialVariant"] = "Christmas  Candy Cane"
+        },
+        ["82c"] = {
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Bedazzled",
+            ["Icon"] = 11828444285,
+            ["Wrap"] = {["tile"] = 2},
+            ["GlobalIdentifier"] = "Code",
+            ["MaterialVariant"] = "Bedazzled"
+        },
+        ["74c"] = {
+            ["MaterialOnly"] = Enum.Material.Marble,
+            ["Name"] = "Marble",
+            ["Price"] = 15750,
+            ["Wrap"] = {["transparency"] = 0, ["inheritColor"] = true, ["tile"] = 0.6},
+            ["Icon"] = 11645142310,
+            ["MaterialVariant"] = "Marble"
+        },
+        ["84c"] = {
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Eggstravagant",
+            ["Icon"] = 13018454087,
+            ["Wrap"] = {["tile"] = 2},
+            ["GlobalIdentifier"] = "Easter 2023",
+            ["MaterialVariant"] = "Eggstravagant"
+        },
+        ["76c"] = {
+            ["Icon"] = 11826816026,
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Obsidian",
+            ["DisplayOrder"] = 10000000,
+            ["Presents"] = 240,
+            ["Wrap"] = {["tile"] = 2},
+            ["GlobalIdentifier"] = "Winter 2022",
+            ["MaterialVariant"] = "Obsidian"
+        },
+        ["BackroomsWallpaper"] = {
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Backrooms Wallpaper",
+            ["Icon"] = 14686800651,
+            ["Wrap"] = {["tile"] = 2},
+            ["GlobalIdentifier"] = "Halloween 2023",
+            ["MaterialVariant"] = "BackroomsWallpaper"
+        },
+        ["68c"] = {
+            ["Name"] = "Spooky Neon",
+            ["DisplayOrder"] = 10000006,
+            ["GlobalIdentifier"] = "Halloween 2022",
+            ["Wrap"] = {["tile"] = 2},
+            ["Icon"] = 11396644439,
+            ["Candy"] = 265
+        },
+        ["78c"] = {
+            ["Icon"] = 11826816377,
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Merry to the Bone",
+            ["DisplayOrder"] = 10000000,
+            ["Presents"] = 80,
+            ["Wrap"] = {["tile"] = 2},
+            ["GlobalIdentifier"] = "Winter 2022",
+            ["MaterialVariant"] = "Christmas Skulls"
+        },
+        ["Knitted"] = {
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Knitted",
+            ["Icon"] = 14686995432,
+            ["Wrap"] = {["tile"] = 2},
+            ["GlobalIdentifier"] = "Halloween 2023",
+            ["MaterialVariant"] = "Knitted"
+        },
+        ["PurpleChameleon"] = {
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Purple Chameleon",
+            ["Icon"] = 14473981169,
+            ["Wrap"] = {["tile"] = 2},
+            ["GlobalIdentifier"] = "Halloween 2023",
+            ["MaterialVariant"] = "PurpleChameleon"
+        },
+        ["BumpyCarpet"] = {
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Bumpy Carpet",
+            ["Icon"] = 14819123830,
+            ["Wrap"] = {["tile"] = 2},
+            ["GlobalIdentifier"] = "Halloween 2023",
+            ["MaterialVariant"] = "BumpyCarpet"
+        },
+        ["AutumnCamo"] = {
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Autumn Camo",
+            ["Icon"] = 14635889623,
+            ["Wrap"] = {["tile"] = 2},
+            ["GlobalIdentifier"] = "Halloween 2023",
+            ["MaterialVariant"] = "Autumn Camo"
+        },
+        ["BulletHoles"] = {
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Bullet Holes",
+            ["Icon"] = 14686579779,
+            ["Wrap"] = {["tile"] = 2},
+            ["GlobalIdentifier"] = "Halloween 2023",
+            ["MaterialVariant"] = "BulletHoles"
+        },
+        ["72c"] = {
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Metal Weave",
+            ["Price"] = 22500,
+            ["Wrap"] = {["transparency"] = 0, ["inheritColor"] = true, ["tile"] = 0.6},
+            ["Icon"] = 11645142153,
+            ["MaterialVariant"] = "Metal Weave"
+        },
+        ["81c"] = {
+            ["Icon"] = 11826817272,
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Black Ice",
+            ["DisplayOrder"] = 10000000,
+            ["Presents"] = 320,
+            ["Wrap"] = {["tile"] = 2},
+            ["GlobalIdentifier"] = "Winter 2022",
+            ["MaterialVariant"] = "Christmas  Black Ice"
+        },
+        ["66c"] = {
+            ["Name"] = "Halloween Pattern",
+            ["DisplayOrder"] = 10000006,
+            ["GlobalIdentifier"] = "Halloween 2022",
+            ["Wrap"] = {["tile"] = 6},
+            ["Icon"] = 11396676084,
+            ["Candy"] = 170
+        },
+        ["67c"] = {
+            ["Name"] = "Candy",
+            ["DisplayOrder"] = 10000006,
+            ["Wrap"] = {["tile"] = 3},
+            ["Icon"] = 11396651427,
+            ["GlobalIdentifier"] = "Halloween 2022"
+        },
+        ["59c"] = {
+            ["Name"] = "Flowers",
+            ["DisplayOrder"] = 10000003,
+            ["Wrap"] = {["tile"] = 3},
+            ["GlobalIdentifier"] = "Code",
+            ["Icon"] = 6803181481
+        },
+        ["70c"] = {
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Oily Metal",
+            ["Price"] = 45250,
+            ["Wrap"] = {["transparency"] = 0, ["inheritColor"] = true, ["tile"] = 0.6},
+            ["Icon"] = 11645141903,
+            ["MaterialVariant"] = "Oily Metal"
+        },
+        ["1c"] = {["Name"] = "No Wrap", ["DisplayOrder"] = -1, ["Price"] = 0, ["Wrap"] = "_None", ["Icon"] = 5854320152},
+        ["57c"] = {
+            ["Name"] = "Jellybeans",
+            ["DisplayOrder"] = 10000002,
+            ["Wrap"] = {["tile"] = 3},
+            ["GlobalIdentifier"] = "Code",
+            ["Icon"] = 6604406947
+        },
+        ["3c"] = {
+            ["Price"] = 11400,
+            ["Wrap"] = {["inheritColor"] = true, ["tile"] = 6},
+            ["Name"] = "Hexagon",
+            ["Icon"] = 5995860652
+        },
+        ["2c"] = {
+            ["Price"] = 16700,
+            ["Wrap"] = {["transparency"] = 0.1, ["inheritColor"] = true, ["tile"] = 0.6},
+            ["Name"] = "Carbon Fiber",
+            ["Icon"] = 154372719
+        },
+        ["5c"] = {
+            ["Price"] = 21750,
+            ["Wrap"] = {["inheritColor"] = true, ["tile"] = 6},
+            ["Name"] = "Circles",
+            ["Icon"] = 5995831146
+        },
+        ["4c"] = {
+            ["Price"] = 12000,
+            ["Wrap"] = {["inheritColor"] = true, ["tile"] = 6},
+            ["Name"] = "Circuit",
+            ["Icon"] = 5995813624
+        },
+        ["7c"] = {
+            ["Price"] = 23250,
+            ["Wrap"] = {["inheritColor"] = true, ["tile"] = 6},
+            ["Name"] = "Diamond Disco",
+            ["Icon"] = 5995882441
+        },
+        ["6c"] = {
+            ["Price"] = 10500,
+            ["Wrap"] = {["inheritColor"] = true, ["tile"] = 6},
+            ["Name"] = "Circular Weave",
+            ["Icon"] = 5995863761
+        },
+        ["9c"] = {
+            ["Price"] = 29400,
+            ["Wrap"] = {["inheritColor"] = true, ["tile"] = 6},
+            ["Name"] = "Polys",
+            ["Icon"] = 5995792269
+        },
+        ["8c"] = {
+            ["Price"] = 13650,
+            ["Wrap"] = {["inheritColor"] = true, ["tile"] = 6},
+            ["Name"] = "Checkerboard",
+            ["Icon"] = 5995844680
+        },
+        ["73c"] = {
+            ["MaterialOnly"] = Enum.Material.Fabric,
+            ["Name"] = "Gold Flowers",
+            ["Price"] = 35700,
+            ["Wrap"] = {["transparency"] = 0, ["inheritColor"] = true, ["tile"] = 0.6},
+            ["Icon"] = 11645142706,
+            ["MaterialVariant"] = "Elegant carpet"
+        },
+        ["83c"] = {
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Pink Scales",
+            ["Icon"] = 12479543564,
+            ["Wrap"] = {["tile"] = 4},
+            ["GlobalIdentifier"] = "Code",
+            ["MaterialVariant"] = "Pink Scales"
+        },
+        ["71c"] = {
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Polished Metal",
+            ["Price"] = 10600,
+            ["Wrap"] = {["transparency"] = 0, ["inheritColor"] = true, ["tile"] = 0.6},
+            ["Icon"] = 11645141359,
+            ["MaterialVariant"] = "Polished Metal"
+        },
+        ["75c"] = {
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Sequin",
+            ["Icon"] = 11778279464,
+            ["Price"] = 0,
+            ["Wrap"] = {["transparency"] = 0, ["inheritColor"] = true, ["tile"] = 0.6},
+            ["GlobalIdentifier"] = "Code",
+            ["MaterialVariant"] = "Sequin"
+        },
+        ["65c"] = {
+            ["Name"] = "Witch's Spell",
+            ["DisplayOrder"] = 10000006,
+            ["GlobalIdentifier"] = "Halloween 2022",
+            ["Wrap"] = {["tile"] = 5},
+            ["Icon"] = 11396659424,
+            ["Candy"] = 90
+        },
+        ["77c"] = {
+            ["Icon"] = 11826816572,
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Gold Foil",
+            ["DisplayOrder"] = 10000000,
+            ["Presents"] = 520,
+            ["Wrap"] = {["tile"] = 2},
+            ["GlobalIdentifier"] = "Winter 2022",
+            ["MaterialVariant"] = "Space_Blanket"
+        },
+        ["79c"] = {
+            ["Icon"] = 11826816185,
+            ["MaterialOnly"] = Enum.Material.Metal,
+            ["Name"] = "Mistletoe",
+            ["DisplayOrder"] = 10000000,
+            ["Presents"] = 400,
+            ["Wrap"] = {["tile"] = 2},
+            ["GlobalIdentifier"] = "Winter 2022",
+            ["MaterialVariant"] = "Christmas Mistletoe"
+        },
+        ["69c"] = {
+            ["Name"] = "Pumpkin Patch",
+            ["DisplayOrder"] = 10000006,
+            ["GlobalIdentifier"] = "Halloween 2022",
+            ["Wrap"] = {["tile"] = 2},
+            ["Icon"] = 11396634896,
+            ["Candy"] = 340
+        },
+        ["64c"] = {
+            ["Name"] = "Scarecrow",
+            ["DisplayOrder"] = 10000006,
+            ["GlobalIdentifier"] = "Halloween 2022",
+            ["Wrap"] = {["tile"] = 4},
+            ["Icon"] = 11396669866,
+            ["Candy"] = 60
+        },
+        ["63c"] = {
+            ["Name"] = "Metallic Paint",
+            ["Price"] = 11780,
+            ["Wrap"] = {["transparency"] = 0, ["inheritColor"] = true, ["tile"] = 0.6},
+            ["Icon"] = 9441161254,
+            ["Material"] = Enum.Material.Metal
+        },
+        ["62c"] = {
+            ["Name"] = "Matte Stealth Wrap",
+            ["Price"] = 11300,
+            ["Wrap"] = {["transparency"] = 0, ["inheritColor"] = true, ["tile"] = 0.6},
+            ["Icon"] = 9441161254,
+            ["Material"] = Enum.Material.Pebble
+        },
+        ["61c"] = {
+            ["Name"] = "Skulls",
+            ["DisplayOrder"] = 10000005,
+            ["Wrap"] = {["tile"] = 4},
+            ["GlobalIdentifier"] = "Halloween 2021",
+            ["Icon"] = 7903028481
+        },
+        ["60c"] = {
+            ["Name"] = "Canada Day",
+            ["DisplayOrder"] = 10000004,
+            ["Wrap"] = {["tile"] = 2.5},
+            ["GlobalIdentifier"] = "Code",
+            ["Icon"] = 7027215352
+        },
+        ["13c"] = {
+            ["Price"] = 21000,
+            ["Wrap"] = {["inheritColor"] = true, ["tile"] = 6},
+            ["Name"] = "Wood",
+            ["Icon"] = 6002678718
+        },
+        ["17c"] = {
+            ["Name"] = "Cardboard",
+            ["Price"] = 8100,
+            ["Wrap"] = {["inheritColor"] = true, ["tile"] = 3},
+            ["Icon"] = 6900833792,
+            ["DisplayColor"] = Color3.fromRGB(217, 217, 217)
+        },
+        ["55c"] = {
+            ["Name"] = "Festive",
+            ["DisplayOrder"] = 10000000,
+            ["Gifts"] = 50,
+            ["Wrap"] = {["tile"] = 4},
+            ["GlobalIdentifier"] = "Winter 2020",
+            ["Icon"] = 6115092880
+        },
+        ["15c"] = {
+            ["Name"] = "Lava",
+            ["Price"] = 16300,
+            ["Wrap"] = {["inheritColor"] = true, ["tile"] = 4},
+            ["Icon"] = 6883089312,
+            ["DisplayColor"] = Color3.fromRGB(255, 136, 17)
+        },
+        ["18c"] = {
+            ["Name"] = "Brushed Metal",
+            ["Price"] = 18040,
+            ["Wrap"] = {["inheritColor"] = true, ["tile"] = 4},
+            ["Icon"] = 6901060131,
+            ["DisplayColor"] = Color3.fromRGB(148, 148, 148)
+        },
+        ["53c"] = {
+            ["Name"] = "Candy Cane",
+            ["DisplayOrder"] = 10000000,
+            ["Gifts"] = 25,
+            ["Wrap"] = {["tile"] = 2},
+            ["GlobalIdentifier"] = "Winter 2020",
+            ["Icon"] = 6115091236
+        },
+        ["11c"] = {
+            ["Name"] = "Camo",
+            ["Price"] = 25500,
+            ["Wrap"] = {["inheritColor"] = true, ["tile"] = 6},
+            ["Icon"] = 5995782645,
+            ["DisplayColor"] = Color3.fromRGB(230, 230, 230)
+        },
+        ["51c"] = {
+            ["Name"] = "Driving Empire",
+            ["DisplayOrder"] = 10000000,
+            ["Wrap"] = {["tile"] = 3},
+            ["GlobalIdentifier"] = "Group",
+            ["Icon"] = 6038415957
+        },
+        ["58c"] = {
+            ["Name"] = "Grass",
+            ["DisplayOrder"] = 10000003,
+            ["DisplayColor"] = Color3.fromRGB(77, 129, 63),
+            ["Wrap"] = {["inheritColor"] = true, ["tile"] = 8},
+            ["GlobalIdentifier"] = "Code",
+            ["Icon"] = 6803208829
+        },
+        ["56c"] = {
+            ["Name"] = "2020 Valentines",
+            ["DisplayOrder"] = 10000001,
+            ["Wrap"] = {["tile"] = 3},
+            ["GlobalIdentifier"] = "Code",
+            ["Icon"] = 6391849336
+        },
+        ["10c"] = {["Price"] = 43500, ["Wrap"] = {["tile"] = 6}, ["Name"] = "Galaxy", ["Icon"] = 5995791888},
+        ["54c"] = {
+            ["Name"] = "Ugly Sweater",
+            ["DisplayOrder"] = 10000000,
+            ["Gifts"] = 35,
+            ["Wrap"] = {["tile"] = 4},
+            ["GlobalIdentifier"] = "Winter 2020",
+            ["Icon"] = 6115091998
+        },
+        ["12c"] = {
+            ["Name"] = "Digital Camo",
+            ["Price"] = 28500,
+            ["Wrap"] = {["inheritColor"] = true, ["tile"] = 6},
+            ["Icon"] = 6002678163,
+            ["DisplayColor"] = Color3.fromRGB(120, 120, 120)
+        },
+        ["16c"] = {
+            ["MaterialOnly"] = Enum.Material.CorrodedMetal,
+            ["Name"] = "Rust",
+            ["Price"] = 5000,
+            ["Wrap"] = {["inheritColor"] = true, ["tile"] = 4},
+            ["Icon"] = 6901174825
+        },
+        ["50c"] = {
+            ["Name"] = "2020 Snowflakes",
+            ["DisplayOrder"] = 10000000,
+            ["Wrap"] = {["tile"] = 6},
+            ["GlobalIdentifier"] = "Code",
+            ["Icon"] = 6025748032
+        },
+        ["14c"] = {
+            ["Price"] = 11200,
+            ["Wrap"] = {["inheritColor"] = true, ["tile"] = 5},
+            ["Name"] = "Triangular Weave",
+            ["Icon"] = 5136542060
+        },
+        ["52c"] = {
+            ["Name"] = "First place!",
+            ["DisplayOrder"] = 10000000,
+            ["Wrap"] = {["tile"] = 4},
+            ["GlobalIdentifier"] = "Won race!",
+            ["Icon"] = 6029444499
+        }
+    },
+    ["config"] = {
+        ["itemType"] = "Wrap",
+        ["statDisplay"] = "Preview",
+        ["id"] = "8a",
+        ["layout"] = 8,
+        ["name"] = "Wraps",
+        ["category"] = "Appearance",
+        ["default"] = "1c",
+        ["icon"] = "rbxassetid://4699454450",
+        ["menu"] = "PThumb"
+    }
+}
