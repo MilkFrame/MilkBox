dc001a5a452c3fff31975393c1e9160933b56a3b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-29 22:37:09 +1000

    Create Regions.lua

---
+++ GameData/Driving Empire [3351674303]/Regions.lua
@@ -0,0 +1,27 @@
+data = {
+    ["Decals_Mainland2"] = {["Distance"] = 1000, ["Class"] = "BaseArea", ["Position"] = Vector3.new(-559, 0, -2712)},
+    ["SpawnArea"] = {["Distance"] = 1500, ["Class"] = "SpawnArea", ["Position"] = Vector3.new(-442, 0, -1807)},
+    ["Decals_Circuit2"] = {["Distance"] = 1750, ["Class"] = "BaseArea", ["Position"] = Vector3.new(-9530, 0, -5663)},
+    ["Decals_Highway4"] = {
+        ["Distance"] = 12500,
+        ["Class"] = "BaseArea",
+        ["Position"] = Vector3.new(-31205, 332, -20512)
+    },
+    ["Decals_Country2"] = {["Distance"] = 1750, ["Class"] = "BaseArea", ["Position"] = Vector3.new(5827, 0, -2306)},
+    ["City"] = {["Distance"] = 2250, ["Class"] = "BaseArea", ["Position"] = Vector3.new(-969, 0, 3184)},
+    ["Decals_City"] = {["Distance"] = 2250, ["Class"] = "BaseArea", ["Position"] = Vector3.new(-957, 0, 3047)},
+    ["Decals_Highway2"] = {["Distance"] = 1750, ["Class"] = "BaseArea", ["Position"] = Vector3.new(-8863, 332, 2100)},
+    ["Decals_Mainland1"] = {["Distance"] = 1750, ["Class"] = "BaseArea", ["Position"] = Vector3.new(-593, 0, -39)},
+    ["Decals_Country1"] = {["Distance"] = 2500, ["Class"] = "BaseArea", ["Position"] = Vector3.new(8821, 0, -6356)},
+    ["RestinUndergroundGarage"] = {
+        ["Distance"] = 1500,
+        ["Class"] = "BaseArea",
+        ["Position"] = Vector3.new(-530, 0, 1791)
+    },
+    ["Decals_Highway5"] = {["Distance"] = 1750, ["Class"] = "BaseArea", ["Position"] = Vector3.new(-12365, 332, 2100)},
+    ["Decals_Highway1"] = {["Distance"] = 1750, ["Class"] = "BaseArea", ["Position"] = Vector3.new(-6757, 0, -1998)},
+    ["Decals_Neighborhood1"] = {["Distance"] = 1000, ["Class"] = "BaseArea", ["Position"] = Vector3.new(756, 0, -3993)},
+    ["Decals_Circuit1"] = {["Distance"] = 1750, ["Class"] = "BaseArea", ["Position"] = Vector3.new(-5989, 0, -5663)},
+    ["Decals_Highway3"] = {["Distance"] = 5500, ["Class"] = "BaseArea", ["Position"] = Vector3.new(-19541, 332, -2719)},
+    ["Chinatown"] = {["Distance"] = 650, ["Class"] = "BaseArea", ["Position"] = Vector3.new(591, 0, 1954)}
+}
