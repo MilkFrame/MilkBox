a7c87718f587eaa4126232df9ba98af9cba6c666
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-29 22:36:04 +1000

    Create Popups.lua

---
+++ GameData/Driving Empire [3351674303]/Popups.lua
@@ -0,0 +1,17 @@
+data = {
+    ["Rim_Asanti_af827"] = {["Type"] = "Customization", ["CustomizationId"] = "10a", ["CustomizationItemId"] = "61c"},
+    ["Rim_Asanti_af834"] = {["Type"] = "Customization", ["CustomizationId"] = "10a", ["CustomizationItemId"] = "63c"},
+    ["Wrap_Obsidian"] = {["Type"] = "Customization", ["CustomizationId"] = "8a", ["CustomizationItemId"] = "76c"},
+    ["Wrap_Candy Striped"] = {["Type"] = "Customization", ["CustomizationId"] = "8a", ["CustomizationItemId"] = "80c"},
+    ["Rim_Avanti_af824"] = {["Type"] = "Customization", ["CustomizationId"] = "10a", ["CustomizationItemId"] = "65c"},
+    ["Rim_SnowflakeRim2"] = {["Type"] = "Customization", ["CustomizationId"] = "10a", ["CustomizationItemId"] = "68c"},
+    ["Rim_Asanti_af829"] = {["Type"] = "Customization", ["CustomizationId"] = "10a", ["CustomizationItemId"] = "62c"},
+    ["Rim_SnowflakeRim1"] = {["Type"] = "Customization", ["CustomizationId"] = "10a", ["CustomizationItemId"] = "67c"},
+    ["Wrap_Gold Foil"] = {["Type"] = "Customization", ["CustomizationId"] = "8a", ["CustomizationItemId"] = "77c"},
+    ["Wrap_Black Ice"] = {["Type"] = "Customization", ["CustomizationId"] = "8a", ["CustomizationItemId"] = "81c"},
+    ["Rim_Asanti_cx828"] = {["Type"] = "Customization", ["CustomizationId"] = "10a", ["CustomizationItemId"] = "64c"},
+    ["Wrap_Mistletoe"] = {["Type"] = "Customization", ["CustomizationId"] = "8a", ["CustomizationItemId"] = "79c"},
+    ["Wrap_Merry to the Bone"] = {["Type"] = "Customization",["CustomizationId"] = "8a",["CustomizationItemId"] = "78c"},
+    ["Spoiler_Antlers"] = {["Type"] = "Customization", ["CustomizationId"] = "7a", ["CustomizationItemId"] = "52c"},
+    ["Rim_Avanti_af832"] = {["Type"] = "Customization", ["CustomizationId"] = "10a", ["CustomizationItemId"] = "66c"}
+}
