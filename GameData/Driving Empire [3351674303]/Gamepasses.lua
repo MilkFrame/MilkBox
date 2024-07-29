d321c30197ae57f57444b3c060486745515bda76
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-29 22:33:58 +1000

    Create Gamepasses.lua

---
+++ GameData/Driving Empire [3351674303]/Gamepasses.lua
@@ -0,0 +1,10 @@
+data = {
+    ["Radio"] = {["id"] = 6695265, ["name"] = "Radio", ["customize"] = "Radio"},
+    ["PortableGarage"] = {["id"] = 6695276, ["name"] = "Portable Garage", ["customize"] = "Portable Garage"},
+    ["Police"] = {["id"] = 6695258, ["name"] = "Police", ["customize"] = "Police"},
+    ["TaxiSign"] = {["id"] = 161490671, ["name"] = "Taxi Sign Access", ["customize"] = "Taxi Sign Access"},
+    ["BusPass"] = {["id"] = 7469387, ["name"] = "Bus", ["customize"] = "Bus"},
+    ["Boost"] = {["id"] = 15129360, ["name"] = "Boost", ["customize"] = "Boost"},
+    ["VIP"] = {["id"] = 9614957, ["name"] = "VIP", ["customize"] = "VIP"},
+    ["WinterfestBooster"] = {["id"] = 111564999, ["name"] = "2x Presents", ["customize"] = "2x Presents"}
+}
