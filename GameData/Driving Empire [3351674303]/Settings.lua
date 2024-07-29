8946b8fa3dfacce3a00ae0a5de0862ac1494ff38
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-29 22:37:21 +1000

    Create Settings.lua

---
+++ GameData/Driving Empire [3351674303]/Settings.lua
@@ -0,0 +1,107 @@
+data = {
+    ["PassengerLock"] = {
+        ["tab"] = "General",
+        ["title"] = "Who can sit in my vehicles",
+        ["options"] = {"Nobody", "Friends", "Everyone"},
+        ["default"] = 3,
+        ["index"] = "passengers"
+    },
+    ["Shadow"] = {
+        ["tab"] = "Graphics",
+        ["title"] = "Shadows",
+        ["options"] = {"Off", "On"},
+        ["default"] = 2,
+        ["index"] = "shadows"
+    },
+    ["Radios"] = {
+        ["tab"] = "Sound",
+        ["title"] = "Mute radios",
+        ["options"] = {"Off", "On"},
+        ["default"] = 1,
+        ["index"] = "radios"
+    },
+    ["AccessoryHead"] = {
+        ["tab"] = "Accessories",
+        ["title"] = "Head accessories while driving",
+        ["options"] = {"Off", "On"},
+        ["default"] = 2,
+        ["index"] = "accessoryHead"
+    },
+    ["ComfortMode"] = {
+        ["tab"] = "Roleplay",
+        ["title"] = "Comfort driving mode",
+        ["options"] = {"Off", "On"},
+        ["default"] = 1,
+        ["index"] = "comfortMode"
+    },
+    ["Collisions"] = {
+        ["tab"] = "General",
+        ["title"] = "Vehicle collisions",
+        ["options"] = {"Off", "On"},
+        ["default"] = 1,
+        ["index"] = "collisions"
+    },
+    ["Units"] = {
+        ["tab"] = "General",
+        ["title"] = [[Units ("Auto" will adjust based on your region)]],
+        ["options"] = {"Auto", "MPH", "KM/H"},
+        ["default"] = 1,
+        ["index"] = "units"
+    },
+    ["DoorPrompts"] = {
+        ["tab"] = "General",
+        ["title"] = "Hide door prompts while seated",
+        ["options"] = {"Off", "On"},
+        ["default"] = 1,
+        ["index"] = "doorPrompts"
+    },
+    ["WeatherParticles"] = {
+        ["tab"] = "Graphics",
+        ["title"] = "Weather particles",
+        ["options"] = {"Off", "On"},
+        ["default"] = 2,
+        ["index"] = "weatherParticles"
+    },
+    ["VehicleRender"] = {
+        ["tab"] = "Graphics",
+        ["title"] = "Vehicle render distance",
+        ["options"] = {"Potato", "Near", "Default", "Far"},
+        ["default"] = 3,
+        ["index"] = "vehicleRender"
+    },
+    ["Lock"] = {
+        ["tab"] = "General",
+        ["title"] = "Who can drive my vehicles",
+        ["options"] = {"Nobody", "Friends", "Everyone"},
+        ["default"] = 1,
+        ["index"] = "lock"
+    },
+    ["Processing"] = {
+        ["tab"] = "Graphics",
+        ["title"] = "Post processing",
+        ["options"] = {"Off", "On"},
+        ["default"] = 2,
+        ["index"] = "processing"
+    },
+    ["Music"] = {
+        ["tab"] = "Sound",
+        ["title"] = "Mute music",
+        ["options"] = {"Off", "On"},
+        ["default"] = 1,
+        ["index"] = "music"
+    },
+    ["VehicleMax"] = {
+        ["tab"] = "Graphics",
+        ["title"] = "Maximum vehicles rendered",
+        ["options"] = {"8", "16", "Infinite", "Auto"},
+        ["default"] = 4,
+        ["index"] = "vehicleMax"
+    },
+    ["Clouds"] = {
+        ["tab"] = "Graphics",
+        ["title"] = "Clouds",
+        ["options"] = {"Off", "On"},
+        ["default"] = 2,
+        ["index"] = "clouds"
+    }
+}
