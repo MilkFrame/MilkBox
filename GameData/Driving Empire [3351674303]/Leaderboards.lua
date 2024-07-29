c8a6813c3fdba29676031e60e7750c8e99b3d64e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-29 22:34:35 +1000

    Create Leaderboards.lua

---
+++ GameData/Driving Empire [3351674303]/Leaderboards.lua
@@ -0,0 +1,48 @@
+data = {
+    ["ChristmasDailyDeliveries"] = {
+        ["formatLocal"] = "function: 0x49a6ceb95cb0874b",
+        ["key"] = "dailyDeliveries",
+        ["format"] = "function: 0x027f05993dcddccb",
+        ["defaultColor"] = Color3.fromRGB(186, 162, 101),
+        ["isDaily"] = true
+    },
+    ["ChristmasMostDeliveries"] = {
+        ["formatLocal"] = "function: 0xdb46995b60dbe67b",
+        ["isWeekly"] = false,
+        ["key"] = "mostDeliveries",
+        ["format"] = "function: 0xc6ad59b5ffe3cf8b",
+        ["defaultColor"] = Color3.fromRGB(186, 162, 101)
+    },
+    ["ChristmasDailyPresents"] = {
+        ["formatLocal"] = "function: 0xa1649b59b3f5c2ab",
+        ["key"] = "dailyPresents",
+        ["format"] = "function: 0x24d41a065cf6c96b",
+        ["defaultColor"] = Color3.fromRGB(186, 162, 101),
+        ["isDaily"] = true
+    },
+    ["money"] = {
+        ["formatLocal"] = "function: 0xb4d1e444f1d8d3fb",
+        ["isWeekly"] = false,
+        ["key"] = "money",
+        ["format"] = "function: 0xdc97b9c8624e610b"
+    },
+    ["miles"] = {
+        ["formatLocal"] = "function: 0xe96ff1ad6bbf700b",
+        ["isWeekly"] = false,
+        ["key"] = "miles",
+        ["format"] = "function: 0x5d718bde9b249aeb"
+    },
+    ["ChristmasMostPresents"] = {
+        ["formatLocal"] = "function: 0x1c0d6c2b5bc2a95b",
+        ["isWeekly"] = false,
+        ["key"] = "mostPresents",
+        ["format"] = "function: 0x9933d7385ecea62b",
+        ["defaultColor"] = Color3.fromRGB(186, 162, 101)
+    },
+    ["weekly_money"] = {
+        ["formatLocal"] = "function: 0xe9f5891ddd5f2e9b",
+        ["isWeekly"] = true,
+        ["key"] = "moneyEarned",
+        ["format"] = "function: 0x9b1091aeb0353a4b"
+    }
+}
