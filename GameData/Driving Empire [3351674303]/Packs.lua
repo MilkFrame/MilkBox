18ea8543ca6f357fd6831e5c24da852a0e0d68b4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-29 22:35:31 +1000

    Create Packs.lua

---
+++ GameData/Driving Empire [3351674303]/Packs.lua
@@ -0,0 +1,8 @@
+data = {
+    ["packs"] = {
+        ["p9"] = {["Awards"] = {"AventadorJ", "SL2008", "SVJ63"}, ["Name"] = "Lambulli Exclusives", ["Tier"] = 3},
+        ["p11"] = {["Awards"] = {"SkylineR34", "Veilside", "S2000CR"}, ["Name"] = "Japanese Legends", ["Tier"] = 2},
+        ["p10"] = {["Awards"] = {"Rocket900", "CLKRoadster", "AMGOne"},["Name"] = "Ziblinglo Super Series",["Tier"] = 4}
+    },
+    ["tiers"] = {4, 2, 1, 3}
+}
