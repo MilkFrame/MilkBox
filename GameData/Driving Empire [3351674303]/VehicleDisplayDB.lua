c5d6cc5713a22e2168ae8e56528461f03442346b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-29 22:37:58 +1000

    Create VehicleDisplayDB.lua

---
+++ GameData/Driving Empire [3351674303]/VehicleDisplayDB.lua
@@ -0,0 +1,44 @@
+data = {
+    ["NewCars_1"] = {["DefaultVehicleId"] = "Audi-A4", ["DisplayStatus"] = "Static", ["Range"] = 25},
+    ["Cars_4"] = {
+        ["DefaultVehicleId"] = "ModelS",
+        ["ChassisTypes"] = {"Generic"},
+        ["DisplayStatus"] = "Dynamic",
+        ["Range"] = 20
+    },
+    ["ExteriorCar"] = {["DefaultVehicleId"] = "Audi-RS7Sportback", ["DisplayStatus"] = "Static", ["Range"] = 25},
+    ["R22"] = {["DefaultVehicleId"] = "R22", ["DisplayStatus"] = "Static", ["Range"] = 25},
+    ["Cars_8"] = {
+        ["DefaultVehicleId"] = "GT2993",
+        ["ChassisTypes"] = {"Generic"},
+        ["DisplayStatus"] = "Dynamic",
+        ["Range"] = 20
+    },
+    ["NewCars_3"] = {["DefaultVehicleId"] = "Audi-EtronGT", ["DisplayStatus"] = "Static", ["Range"] = 25},
+    ["Heli2"] = {["DefaultVehicleId"] = "Heli2", ["DisplayStatus"] = "Static", ["Range"] = 25},
+    ["Z182"] = {["DefaultVehicleId"] = "Z182", ["DisplayStatus"] = "Static", ["Range"] = 25},
+    ["Helicopter"] = {
+        ["DefaultVehicleId"] = "Z182",
+        ["Range"] = 40,
+        ["DisplayStatus"] = "Static",
+        ["InspectDisabled"] = true
+    },
+    ["NewCars_7"] = {["DefaultVehicleId"] = "McLaren-Senna", ["DisplayStatus"] = "Static", ["Range"] = 25},
+    ["Cars_7"] = {
+        ["DefaultVehicleId"] = "TuataraAggressor",
+        ["ChassisTypes"] = {"Generic"},
+        ["DisplayStatus"] = "Dynamic",
+        ["Range"] = 20
+    },
+    ["NewCars_4"] = {["DefaultVehicleId"] = "Audi-RS7Sportback", ["DisplayStatus"] = "Static", ["Range"] = 25},
+    ["Cars_5"] = {
+        ["DefaultVehicleId"] = "GT3997",
+        ["ChassisTypes"] = {"Generic"},
+        ["DisplayStatus"] = "Dynamic",
+        ["Range"] = 20
+    },
+    ["Cars_TwitterReward"] = {["DefaultVehicleId"] = "GoofyCar", ["DisplayStatus"] = "Static", ["Range"] = 20},
+    ["NewCars_5"] = {["DefaultVehicleId"] = "Pagani-ZondaRevolucion", ["DisplayStatus"] = "Static", ["Range"] = 25},
+    ["NewCars_6"] = {["DefaultVehicleId"] = "Pagani-Utopia", ["DisplayStatus"] = "Static", ["Range"] = 25},
+    ["NewCars_2"] = {["DefaultVehicleId"] = "KTM-BowGTX", ["DisplayStatus"] = "Static", ["Range"] = 25}
+}
