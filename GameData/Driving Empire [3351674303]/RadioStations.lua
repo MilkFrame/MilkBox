d0925f528b3d3d85413a59d8fa9ae41cdae8fd0f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-29 22:36:43 +1000

    Create RadioStations.lua

---
+++ GameData/Driving Empire [3351674303]/RadioStations.lua
@@ -0,0 +1,28 @@
+data = {
+    ["DB"] = {
+        ["Monstercat"] = {
+            ["Station"] = {
+                ["PrimaryColor"] = Color3.fromRGB(103, 40, 61),
+                ["AvailableOnRadios"] = true,
+                ["DisplayName"] = "Pop-Station"
+            },
+            ["TrackList"] = {
+                {["SoundID"] = 14388457180, ["DisplayName"] = "Ooyy - Running Into"},
+                {["SoundID"] = 5410081702, ["DisplayName"] = "Mark Twain - Half an Orange"},
+                {["SoundID"] = 14388458101, ["DisplayName"] = "Phello - Quiet Please"},
+                {["SoundID"] = 14388458101, ["DisplayName"] = "Phello - Quiet Please"},
+                {["SoundID"] = 7028570258, ["DisplayName"] = "Skin - Rome in Silver & Beach Season"},
+                {["SoundID"] = 14388456342, ["DisplayName"] = "Zorro - Manners"},
+                {["SoundID"] = 7023828725, ["DisplayName"] = "Color - Grant"},
+                {["SoundID"] = 7023828725, ["DisplayName"] = "Cushy - Blinding"},
+                {["SoundID"] = 7023828725, ["DisplayName"] = "Autohacker - Monument"},
+                {["SoundID"] = 5410083549, ["DisplayName"] = "Rogue - Let's Talk"},
+                {["SoundID"] = 5410081542, ["DisplayName"] = "Rootkit - Taking Me Higher"},
+                {["SoundID"] = 7029092469, ["DisplayName"] = "Vintage & Morelli x Brandon Mignacca - Love Is"},
+                {["SoundID"] = 7024324344, ["DisplayName"] = "Odsen - In My Bones"},
+                {["SoundID"] = 7029017448, ["DisplayName"] = "Terry Zhong - Play It Cool"}
+            }
+        }
+    },
+    ["GetInfoFromSoundID"] = "function: 0x134177f66c9f7a5b"
+}
