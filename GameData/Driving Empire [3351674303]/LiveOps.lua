72a4eeebec2e88e199206ebaf7ac619e82d40883
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-29 22:38:50 +1000

    Create LiveOps.lua

---
+++ GameData/Driving Empire [3351674303]/LiveOps.lua
@@ -0,0 +1,394 @@
+data = {
+    ["ShiptDB"] = {
+        ["Sounds"] = {["CollectReward"] = "rbxassetid://5852497507"},
+        ["Rewards"] = {
+            ["Reward2"] = {
+                ["PartName"] = "Engine",
+                ["Skip"] = {["Robux"] = 299, ["DeveloperProductId"] = 1567620064},
+                ["QuestId"] = "BuildAMotorcycle_June2023_RacePromoted",
+                ["Icon"] = "rbxassetid://13884224066"
+            },
+            ["Reward8"] = {
+                ["PartName"] = "Body",
+                ["Skip"] = {["Robux"] = 499, ["DeveloperProductId"] = 1567638526},
+                ["QuestId"] = "BuildAMotorcycle_June2023_RaceForCash",
+                ["Icon"] = "rbxassetid://13884224270"
+            },
+            ["Reward5"] = {
+                ["PartName"] = "Lights",
+                ["Skip"] = {["Robux"] = 299, ["DeveloperProductId"] = 1567635014},
+                ["QuestId"] = "BuildAMotorcycle_June2023_CrossCountry",
+                ["Icon"] = "rbxassetid://13884223627"
+            },
+            ["Reward10"] = {
+                ["PartName"] = "Wheels",
+                ["Skip"] = {["Robux"] = 499, ["DeveloperProductId"] = 1567639196},
+                ["QuestId"] = "BuildAMotorcycle_June2023_Distance1",
+                ["Icon"] = "rbxassetid://13884222790"
+            },
+            ["Reward4"] = {
+                ["PartName"] = "Windshield",
+                ["Skip"] = {["Robux"] = 99, ["DeveloperProductId"] = 1567620899},
+                ["QuestId"] = "BuildAMotorcycle_June2023_NewRace",
+                ["Icon"] = "rbxassetid://13884222627"
+            },
+            ["Reward9"] = {
+                ["PartName"] = "Rims",
+                ["Skip"] = {["Robux"] = 199, ["DeveloperProductId"] = 1567638980},
+                ["QuestId"] = "BuildAMotorcycle_June2023_Rims",
+                ["Icon"] = "rbxassetid://13884223407"
+            },
+            ["Reward7"] = {
+                ["PartName"] = "Handle Bars",
+                ["Skip"] = {["Robux"] = 299, ["DeveloperProductId"] = 1567637539},
+                ["QuestId"] = "BuildAMotorcycle_June2023_RaceFinishWithTime",
+                ["Icon"] = "rbxassetid://13884223742"
+            },
+            ["Reward3"] = {
+                ["PartName"] = "Exhaust",
+                ["Skip"] = {["Robux"] = 199, ["DeveloperProductId"] = 1567620428},
+                ["QuestId"] = "BuildAMotorcycle_June2023_FindExhaust",
+                ["Icon"] = "rbxassetid://13884223897"
+            },
+            ["Reward6"] = {
+                ["PartName"] = "Fuel Tank",
+                ["Skip"] = {["Robux"] = 199, ["DeveloperProductId"] = 1567635788},
+                ["QuestId"] = "BuildAMotorcycle_June2023_FuelTank",
+                ["Icon"] = "rbxassetid://13884223022"
+            },
+            ["Reward1"] = {
+                ["PartName"] = "Seat",
+                ["Skip"] = {["Robux"] = 99, ["DeveloperProductId"] = 1567619369},
+                ["QuestId"] = "BuildAMotorcycle_June2023_RaceFinishWithFriend",
+                ["Icon"] = "rbxassetid://13884223247"
+            }
+        },
+        ["ShiptMan"] = {["PromptHoldDuration"] = 2},
+        ["CarId"] = "BCUP2"
+    },
+    ["BuildACarDB"] = {
+        ["Sounds"] = {["CollectReward"] = "rbxassetid://5852497507"},
+        ["Rewards"] = {
+            ["Reward2"] = {
+                ["PartName"] = "Lights",
+                ["Skip"] = {["Robux"] = 30, ["DeveloperProductId"] = 1567620064},
+                ["QuestId"] = "BuildACar_May2024_FindLights",
+                ["Icon"] = "rbxassetid://16897359088"
+            },
+            ["Reward8"] = {
+                ["PartName"] = "Engine",
+                ["Skip"] = {["Robux"] = 200, ["DeveloperProductId"] = 1567639196},
+                ["QuestId"] = "BuildACar_May2024_RushhourAndDrawbridge",
+                ["Icon"] = "rbxassetid://17429766424"
+            },
+            ["Reward5"] = {
+                ["PartName"] = "Logo",
+                ["Skip"] = {["Robux"] = 50, ["DeveloperProductId"] = 1567635014},
+                ["QuestId"] = "BuildACar_May2024_FindExhaust",
+                ["Icon"] = "rbxassetid://17429629085"
+            },
+            ["Reward10"] = {
+                ["PartName"] = "Front Bumper",
+                ["Skip"] = {["Robux"] = 350, ["DeveloperProductId"] = 1567637539},
+                ["QuestId"] = "BuildACar_May2024_Distance1",
+                ["Icon"] = "rbxassetid://17429766278"
+            },
+            ["Reward4"] = {
+                ["PartName"] = "Rims",
+                ["Skip"] = {["Robux"] = 30, ["DeveloperProductId"] = 1567620899},
+                ["QuestId"] = "BuildACar_May2024_FindRims",
+                ["Icon"] = "rbxassetid://16897358950"
+            },
+            ["Reward9"] = {
+                ["PartName"] = "Rear Bumper",
+                ["Skip"] = {["Robux"] = 250, ["DeveloperProductId"] = 1567638526},
+                ["QuestId"] = "BuildACar_May2024_CrossCountryMultiplayer",
+                ["Icon"] = "rbxassetid://16897203761"
+            },
+            ["Reward7"] = {
+                ["PartName"] = "Steering Wheel",
+                ["Skip"] = {["Robux"] = 200, ["DeveloperProductId"] = 1567638980},
+                ["QuestId"] = "BuildACar_May2024_CompleteLightingLoopAndCircuit",
+                ["Icon"] = "rbxassetid://17429766161"
+            },
+            ["Reward3"] = {
+                ["PartName"] = "Wheels",
+                ["Skip"] = {["Robux"] = 30, ["DeveloperProductId"] = 1567620428},
+                ["QuestId"] = "BuildACar_May2024_FindWheels",
+                ["Icon"] = "rbxassetid://16897204331"
+            },
+            ["Reward6"] = {
+                ["PartName"] = "Seats",
+                ["Skip"] = {["Robux"] = 50, ["DeveloperProductId"] = 1567635788},
+                ["QuestId"] = "BuildACar_May2024_FindDiffuser",
+                ["Icon"] = "rbxassetid://17428882355"
+            },
+            ["Reward1"] = {
+                ["PartName"] = "Doors",
+                ["Skip"] = {["Robux"] = 30, ["DeveloperProductId"] = 1567619369},
+                ["QuestId"] = "BuildACar_May2024_FindDoors",
+                ["Icon"] = "rbxassetid://16810441486"
+            }
+        },
+        ["MechanicMan"] = {["PromptHoldDuration"] = 2},
+        ["CarId"] = "Noble-m600CarbonSport"
+    },
+    ["EasterfestDB"] = {
+        ["LoopingCashReward"] = {["CashAmount"] = 100000, ["Eggs"] = 1000},
+        ["BaseEggPrizeDivisorByTotalRacers"] = {
+            ["Circuit"] = {5, 3.33, 2.5, 2, 1.67, 1.43, 1.25, 1.11, 1},
+            ["CrossCountry"] = {7, 4.67, 3.5, 2.8, 2.33, 2, 1.75, 1.56, 1.4, 1.27, 1.17, 1.08, 1},
+            ["Drag"] = {1},
+            ["Highway"] = {4, 2.67, 2, 1.6, 1.33, 1.14, 1},
+            ["Drawbridge"] = {5, 3.33, 2.5, 2, 1.67, 1.43, 1.25, 1.11, 1}
+        },
+        ["BaseEggPrizeByPosition"] = {
+            ["Circuit"] = {356, 284, 213, 142, 71, 71, 56, 56, 42, 42},
+            ["CrossCountry"] = {487, 390, 292, 195, 97, 97, 78, 78, 58, 58, 39, 39, 19, 19},
+            ["Drag"] = {3, 2},
+            ["Drawbridge"] = {180, 144, 108, 72, 36, 36, 28, 28, 21, 21},
+            ["Highway"] = {56, 45, 33, 22, 11, 11, 9, 9}
+        },
+        ["BaseCashMultiplier"] = 1,
+        ["Sounds"] = {["CollectReward"] = "rbxassetid://5852497507"},
+        ["Rewards"] = {
+            ["Reward2"] = {
+                ["Skip"] = {["Robux"] = 149, ["DeveloperProductId"] = 1511240243},
+                ["RewardType"] = "Wrap",
+                ["TrackPosition"] = 0.31,
+                ["TrackThumbnailSize"] = 0.55,
+                ["WrapId"] = "84c",
+                ["ItemName"] = "Eggstravagant Wrap",
+                ["Image"] = 13018454087,
+                ["Eggs"] = 500
+            },
+            ["Reward4"] = {
+                ["Skip"] = {["Robux"] = 499, ["DeveloperProductId"] = 1511240422},
+                ["RewardType"] = "Vehicle",
+                ["TrackPosition"] = 0.77,
+                ["VehicleId"] = "X150",
+                ["Eggs"] = 3000
+            },
+            ["Reward5"] = {
+                ["Skip"] = {["Robux"] = 999, ["DeveloperProductId"] = 1511240488},
+                ["RewardType"] = "Vehicle",
+                ["TrackPosition"] = 1,
+                ["VehicleId"] = "HalloweenCar3",
+                ["Eggs"] = 6000
+            },
+            ["Reward3"] = {
+                ["Skip"] = {["Robux"] = 299, ["DeveloperProductId"] = 1511240354},
+                ["RewardType"] = "Vehicle",
+                ["TrackPosition"] = 0.54,
+                ["VehicleId"] = "Alyse",
+                ["Eggs"] = 1500
+            },
+            ["Reward1"] = {
+                ["Skip"] = false,
+                ["RewardType"] = "Cash",
+                ["TrackPosition"] = 0.07,
+                ["Amount"] = 15000,
+                ["Eggs"] = 3
+            }
+        },
+        ["BunnyMan"] = {["PromptHoldDuration"] = 2},
+        ["Boost"] = {["DurationMinutes"] = 30, ["DeveloperProductId"] = 1511240609, ["Multiplier"] = 2, ["Robux"] = 99}
+    },
+    ["EmpireTaxiEventDB"] = {
+        ["PassengerVoicelines"] = {
+            ["DestructableCollision"] = {"Be careful!", "Watch out!", "Careful!", "Watch it!", "Whoa!"},
+            ["DropOff"] = {"That's it", "Right there", "Drop me off here", "Stop here", "Thank you!"},
+            ["VehicleEnter"] = {"Hey!", "Hi!", "Hello!", "How are you?"},
+            ["VehicleCrash"] = {
+                "Are you crazy?!",
+                "Where did you learn to drive??",
+                "What are you doing?!",
+                "Are you even a real taxi driver?!"
+            }
+        },
+        ["Passenger"] = {
+            ["DropoffTime"] = 1,
+            ["MaxTime"] = 90,
+            ["RenderDeadzone"] = 250,
+            ["PickupTime"] = 2,
+            ["DropoffDistance"] = 30,
+            ["PickupBillboardDistance"] = 300,
+            ["InteractionDistance"] = 20,
+            ["RenderDistance"] = 1000,
+            ["DropoffBillboardDistance"] = 300
+        },
+        ["AppreciationStarLevels"] = {
+            {["MinAppreciation"] = 0, ["Multiplier"] = 0.5},
+            {["MinAppreciation"] = 20, ["Multiplier"] = 1},
+            {["MinAppreciation"] = 70, ["Multiplier"] = 1.5}
+        },
+        ["TaxiMan"] = {["PromptHoldDuration"] = 2},
+        ["Difficulties"] = {
+            {
+                ["MinDistance"] = 1500,
+                ["MaxDistance"] = 2000,
+                ["Name"] = "Easy",
+                ["Color"] = Color3.fromRGB(150, 199, 158),
+                ["MaxRating"] = 5,
+                ["BaseReward"] = 300
+            },
+            {
+                ["MinDistance"] = 2200,
+                ["MaxDistance"] = 3200,
+                ["Name"] = "Medium",
+                ["Color"] = Color3.fromRGB(243, 207, 54),
+                ["MaxRating"] = 8,
+                ["BaseReward"] = 900
+            },
+            {
+                ["MinDistance"] = 3600,
+                ["MaxDistance"] = 4600,
+                ["Name"] = "Hard",
+                ["Color"] = Color3.fromRGB(216, 99, 78),
+                ["MaxRating"] = 10,
+                ["BaseReward"] = 3600
+            }
+        },
+        ["DefaultPassengerIds"] = {
+            4407810154,
+            3322429,
+            1434829778,
+            2390591390,
+            1068573686,
+            3105122505,
+            2493692584,
+            23108718,
+            5419947647,
+            1101826,
+            241063740,
+            591909929,
+            56801836,
+            1496559625,
+            1017690015,
+            3691652390,
+            4409841634,
+            1081225089,
+            455541205,
+            1925625322,
+            173181781,
+            4239499933,
+            97109886,
+            4365640110,
+            198463924,
+            2394559807,
+            1591788090,
+            167186282,
+            1260052317,
+            106064507,
+            1016578688
+        },
+        ["Sounds"] = {
+            ["LoseAppreciationStar"] = "rbxassetid://12874171945",
+            ["AngryPassenger"] = "rbxassetid://12874171234",
+            ["Cleared"] = "rbxassetid://12874171447",
+            ["Dropoff"] = "rbxassetid://12874171668",
+            ["CollectReward"] = "rbxassetid://5852497507",
+            ["Pickup"] = "rbxassetid://12874171552",
+            ["Music"] = "rbxassetid://1842227851",
+            ["Failed"] = "rbxassetid://12874171354",
+            ["Go"] = "rbxassetid://12874171816"
+        },
+        ["BaseCashMultiplier"] = 1,
+        ["Appreciation"] = {
+            ["StartWith"] = 100,
+            ["Decay"] = {
+                ["DistanceToDropoff"] = {["DefaultRate"] = 1.5},
+                ["GainedBounty"] = {["Rate"] = 0.5},
+                ["DestructableCollision"] = {["Rate"] = 5},
+                ["VehicleCrash"] = {["Rate"] = 20},
+                ["Stationary"] = {
+                    ["DecayAfterSeconds"] = 5,
+                    ["MaxRate"] = 10,
+                    ["IncreaseRateEverySecond"] = 1,
+                    ["BaseRate"] = 1
+                }
+            },
+            ["BeginDecayAfterSeconds"] = 3,
+            ["Color"] = {["Max"] = Color3.fromRGB(126, 223, 110), ["Min"] = Color3.fromRGB(109, 132, 61)}
+        },
+        ["LoopingCashReward"] = {["CashAmount"] = 100000, ["Stars"] = 100},
+        ["Rewards"] = {
+            ["Reward2"] = {
+                ["Stars"] = 210,
+                ["RewardType"] = "Vehicle",
+                ["TrackPosition"] = 0.77,
+                ["VehicleId"] = "Devil22",
+                ["Skip"] = {["Robux"] = 669, ["DeveloperProductId"] = 1497199031}
+            },
+            ["SmallCash"] = {
+                ["Stars"] = 1,
+                ["RewardType"] = "Cash",
+                ["TrackPosition"] = 0.07,
+                ["Amount"] = 15000,
+                ["Skip"] = false
+            },
+            ["Reward3"] = {
+                ["Stars"] = 450,
+                ["RewardType"] = "Vehicle",
+                ["TrackPosition"] = 1,
+                ["VehicleId"] = "TyphoonCarbonio",
+                ["Skip"] = {["Robux"] = 1329, ["DeveloperProductId"] = 1497204231}
+            },
+            ["TaxiSign"] = {
+                ["Stars"] = 30,
+                ["RewardType"] = "Custom",
+                ["TrackPosition"] = 0.31,
+                ["Skip"] = {["Robux"] = 159, ["DeveloperProductId"] = 1497186353},
+                ["ItemName"] = "Taxi Sign Customization Access",
+                ["Image"] = 12791875280,
+                ["TrackThumbnailSize"] = 0.55
+            },
+            ["Reward1"] = {
+                ["Stars"] = 90,
+                ["RewardType"] = "Vehicle",
+                ["TrackPosition"] = 0.54,
+                ["VehicleId"] = "4Z",
+                ["Skip"] = {["Robux"] = 339, ["DeveloperProductId"] = 1497195858}
+            }
+        },
+        ["PassengerSpawning"] = {
+            ["SpawnEventEverySeconds"] = 10,
+            ["DontSpawnAnotherWithinRadiusStuds"] = 800,
+            ["DisableSpawnpointAfterPickupForSeconds"] = 300,
+            ["PassengersPerEvent"] = 4
+        }
+    },
+    ["Halloween2023DB"] = {
+        ["LoopingCashReward"] = {["CashAmount"] = 100000, ["Eggs"] = 1000},
+        ["BaseEggPrizeDivisorByTotalRacers"] = {
+            ["Circuit"] = {5, 3.33, 2.5, 2, 1.67, 1.43, 1.25, 1.11, 1},
+            ["CrossCountry"] = {7, 4.67, 3.5, 2.8, 2.33, 2, 1.75, 1.56, 1.4, 1.27, 1.17, 1.08, 1},
+            ["Drag"] = {1},
+            ["Highway"] = {4, 2.67, 2, 1.6, 1.33, 1.14, 1},
+            ["Drawbridge"] = {5, 3.33, 2.5, 2, 1.67, 1.43, 1.25, 1.11, 1}
+        },
+        ["BaseEggPrizeByPosition"] = {
+            ["Circuit"] = {356, 284, 213, 142, 71, 71, 56, 56, 42, 42},
+            ["CrossCountry"] = {487, 390, 292, 195, 97, 97, 78, 78, 58, 58, 39, 39, 19, 19},
+            ["Drag"] = {3, 2},
+            ["Drawbridge"] = {180, 144, 108, 72, 36, 36, 28, 28, 21, 21},
+            ["Highway"] = {56, 45, 33, 22, 11, 11, 9, 9}
+        },
+        ["BaseCashMultiplier"] = 1,
+        ["Sounds"] = {["CollectReward"] = "rbxassetid://5852497507"},
+        ["Rewards"] = {
+            ["Reward2"] = {["CurrencyPrize"] = 50, ["RewardType"] = "Currency"},
+            ["Reward4"] = {["CurrencyPrize"] = 500, ["RewardType"] = "Currency"},
+            ["Reward5"] = {["CurrencyPrize"] = 1500, ["RewardType"] = "Currency"},
+            ["Reward3"] = {["CurrencyPrize"] = 250, ["RewardType"] = "Currency"},
+            ["Reward6"] = {["CurrencyPrize"] = 5000, ["RewardType"] = "Currency"}
+        },
+        ["BunnyMan"] = {["PromptHoldDuration"] = 2},
+        ["Boost"] = {
+            ["DurationMinutes"] = 60,
+            ["DeveloperProductId"] = 1664290704,
+            ["Multiplier"] = 1.5,
+            ["Robux"] = 50
+        }
+    }
+}
