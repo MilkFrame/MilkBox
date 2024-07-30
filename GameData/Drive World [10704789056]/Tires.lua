b687587d5543f9bc355a9bca6e9bee4cfaf119bc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-30 20:52:40 +1000

    Create Tires.lua

---
+++ GameData/Drive World [10704789056]/Tires.lua
@@ -0,0 +1,128 @@
+data = {
+    ["Peppermint"] = {
+        ["Category"] = "Tires",
+        ["Order"] = 9,
+        ["BaseColor"] = Color3.fromRGB(246, 41, 44),
+        ["Offsale"] = true,
+        ["DisplayName"] = "Peppermint Tires"
+    },
+    ["Cybernetic"] = {
+        ["Category"] = "Tires",
+        ["Order"] = 10,
+        ["BaseColor"] = Color3.fromRGB(39, 39, 39),
+        ["Offsale"] = true,
+        ["DisplayName"] = "Cybernetic Tires"
+    },
+    ["SpringRally_B"] = {
+        ["Category"] = "Tires",
+        ["Order"] = 12,
+        ["BaseColor"] = Color3.fromRGB(39, 39, 39),
+        ["Offsale"] = true,
+        ["DisplayName"] = "Spring Rally Tires '24 B"
+    },
+    ["Race"] = {
+        ["Category"] = "Tires",
+        ["Order"] = 4,
+        ["CashPrice"] = 25000,
+        ["BaseColor"] = Color3.fromRGB(255, 255, 255),
+        ["DisplayName"] = "Race Tires"
+    },
+    ["Chain"] = {
+        ["Category"] = "Tires",
+        ["Order"] = 7,
+        ["BaseColor"] = Color3.fromRGB(39, 39, 39),
+        ["Offsale"] = true,
+        ["DisplayName"] = "Chained Tires"
+    },
+    ["ColoredSmoke_Pink"] = {
+        ["Category"] = "Tires",
+        ["Order"] = 100,
+        ["BaseColor"] = Color3.fromRGB(108, 44, 79),
+        ["Offsale"] = true,
+        ["DisplayName"] = "Pink Smoke Tires"
+    },
+    ["SpringRally_A"] = {
+        ["Category"] = "Tires",
+        ["Order"] = 11,
+        ["BaseColor"] = Color3.fromRGB(8, 47, 143),
+        ["Offsale"] = true,
+        ["DisplayName"] = "Spring Rally Tires '24 A"
+    },
+    ["Monster"] = {
+        ["Category"] = "Tires",
+        ["Order"] = 13,
+        ["BaseColor"] = Color3.fromRGB(130, 13, 21),
+        ["Offsale"] = true,
+        ["DisplayName"] = "Monster Tires"
+    },
+    ["Stock"] = {
+        ["Order"] = 1,
+        ["Category"] = "Tires",
+        ["BaseColor"] = Color3.fromRGB(39, 39, 39),
+        ["DisplayName"] = "Stock Tires"
+    },
+    ["Classic"] = {
+        ["Category"] = "Tires",
+        ["Order"] = 90,
+        ["BaseColor"] = Color3.fromRGB(23, 28, 31),
+        ["Offsale"] = true,
+        ["DisplayName"] = "Retro Tires"
+    },
+    ["ColoredSmoke_Red"] = {
+        ["Category"] = "Tires",
+        ["Order"] = 101,
+        ["BaseColor"] = Color3.fromRGB(83, 4, 4),
+        ["Offsale"] = false,
+        ["DisplayName"] = "Red Smoke Tires"
+    },
+    ["ColoredSmoke_Yellow"] = {
+        ["Order"] = 103,
+        ["Category"] = "Tires",
+        ["CashPrice"] = 20000,
+        ["BaseColor"] = Color3.fromRGB(108, 104, 27),
+        ["Offsale"] = false,
+        ["DisplayName"] = "Yellow Smoke Tires"
+    },
+    ["Sport"] = {
+        ["Category"] = "Tires",
+        ["Order"] = 2,
+        ["CashPrice"] = 15000,
+        ["BaseColor"] = Color3.fromRGB(39, 39, 39),
+        ["DisplayName"] = "Sport Tires"
+    },
+    ["Slick_White"] = {
+        ["Category"] = "Tires",
+        ["Order"] = 104,
+        ["BaseColor"] = Color3.fromRGB(245, 245, 245),
+        ["Offsale"] = true,
+        ["DisplayName"] = "White Slick Tires"
+    },
+    ["Offroad"] = {
+        ["Category"] = "Tires",
+        ["Order"] = 3,
+        ["CashPrice"] = 15000,
+        ["BaseColor"] = Color3.fromRGB(39, 39, 39),
+        ["DisplayName"] = "Offroad Tires"
+    },
+    ["Airless"] = {
+        ["Category"] = "Tires",
+        ["Order"] = 6,
+        ["BaseColor"] = Color3.fromRGB(39, 39, 39),
+        ["Offsale"] = true,
+        ["DisplayName"] = "Airless Tires"
+    },
+    ["Dyed"] = {
+        ["Category"] = "Tires",
+        ["Order"] = 8,
+        ["BaseColor"] = Color3.fromRGB(0, 12, 27),
+        ["Offsale"] = true,
+        ["DisplayName"] = "Dyed Tires"
+    },
+    ["Slick"] = {
+        ["Category"] = "Tires",
+        ["Order"] = 5,
+        ["CashPrice"] = 25000,
+        ["BaseColor"] = Color3.fromRGB(245, 205, 48),
+        ["DisplayName"] = "Yellow Slick Tires"
+    }
+}
