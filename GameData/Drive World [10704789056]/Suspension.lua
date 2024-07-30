caedcba620743edcd268d25f4a48fa1024669d3c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-30 20:53:05 +1000

    Create Suspension.lua

---
+++ GameData/Drive World [10704789056]/Suspension.lua
@@ -0,0 +1,24 @@
+data = {
+    ["Stock"] = {["Order"] = 1, ["Category"] = "Suspension", ["DisplayName"] = "Stock Suspension"},
+    ["Sport"] = {["CashPrice"] = 5000, ["Category"] = "Suspension", ["Order"] = 2, ["DisplayName"] = "Sport Suspension"},
+    ["Track"] = {
+        ["CashPrice"] = 10000,
+        ["Category"] = "Suspension",
+        ["Order"] = 3,
+        ["DisplayName"] = "Track Suspension"
+    },
+    ["Race"] = {
+        ["Category"] = "Suspension",
+        ["CashPrice"] = 20000,
+        ["ProductName"] = "SmallModPurchase",
+        ["Order"] = 4,
+        ["DisplayName"] = "Race Suspension"
+    },
+    ["Custom"] = {
+        ["PremiumPrice"] = 50,
+        ["Order"] = 5,
+        ["Category"] = "Suspension",
+        ["CashPrice"] = 97500,
+        ["DisplayName"] = "Custom Suspension"
+    }
+}
