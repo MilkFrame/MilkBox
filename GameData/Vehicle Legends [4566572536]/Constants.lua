fed90b3fd4906a5bba8f9e9d747351281eb19b93
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-30 20:44:13 +1000

    Create Constants.lua

---
+++ GameData/Vehicle Legends [4566572536]/Constants.lua
@@ -0,0 +1,25 @@
+data = {
+    ["codeSystem"] = {["codeRedeemed"] = 1, ["codeInvalid"] = 3, ["codeUsed"] = 2},
+    ["vehiclePurchases"] = {["offSale"] = 4, ["vehiclePurchased"] = 1, ["anError"] = 3, ["alreadyOwnVehicle"] = 2},
+    ["radioSystem"] = {["play"] = 1, ["stop"] = 2},
+    ["vehicleChassis"] = {["exitVehicle"] = 1, ["enterVehicle"] = 2},
+    ["customizationSystem"] = {["primaryColor"] = 1, ["rimColor"] = 2},
+    ["generics"] = {["actionFailed"] = 2, ["actionSuccess"] = 1},
+    ["actions"] = {
+        ["redeemCode"] = 1,
+        ["useRadio"] = 2,
+        ["purchaseGap"] = 3,
+        ["enterGarageClientFunc"] = 4,
+        ["positionCamera"] = 5,
+        ["purchaseMod"] = 6,
+        ["exitGarage"] = 7,
+        ["autoshopPurchaseCompleted"] = 8,
+        ["Transition"] = 9,
+        ["changeColor"] = 10,
+        ["changeMaterial"] = 11,
+        ["loadDealership"] = 12,
+        ["loadKit"] = 13,
+        ["loadPhysicalDealership"] = 14,
+        ["submitTwitter"] = 15
+    }
+}
