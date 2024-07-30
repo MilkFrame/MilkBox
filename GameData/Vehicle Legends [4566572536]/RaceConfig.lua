7e0cd576c2caa76ddfba1cefe1ef388ac4bf937d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-30 20:46:58 +1000

    Create RaceConfig.lua

---
+++ GameData/Vehicle Legends [4566572536]/RaceConfig.lua
@@ -0,0 +1,110 @@
+data = {
+    ["Ice Race"] = {
+        ["players"] = 6,
+        ["laps"] = 2,
+        ["CandyRewardRatio"] = 0.003,
+        ["minimumTime"] = 40,
+        ["creditsReward"] = 11000,
+        ["timeoutLength"] = 130,
+        ["vehicleTypes"] = {"Car", "Motorcycle", "Dirtbike", "ATV", "Truck"}
+    },
+    ["Drag Race"] = {
+        ["players"] = 7,
+        ["laps"] = 1,
+        ["CandyRewardRatio"] = 0.0015,
+        ["minimumTime"] = 4.8,
+        ["creditsReward"] = 3750,
+        ["timeoutLength"] = 45,
+        ["vehicleTypes"] = {"Car", "Motorcycle", "Dirtbike", "ATV", "Truck"}
+    },
+    ["Offroad Race"] = {
+        ["players"] = 8,
+        ["laps"] = 1,
+        ["CandyRewardRatio"] = 0.0015,
+        ["minimumTime"] = 15,
+        ["creditsReward"] = 14500,
+        ["timeoutLength"] = 135,
+        ["vehicleTypes"] = {"Dirtbike", "ATV", "Truck"}
+    },
+    ["Beach Dash"] = {
+        ["players"] = 2,
+        ["laps"] = 1,
+        ["CandyRewardRatio"] = 0.0015,
+        ["minimumTime"] = 10,
+        ["creditsReward"] = 5750,
+        ["timeoutLength"] = 45,
+        ["vehicleTypes"] = {"Car", "Motorcycle", "Dirtbike", "ATV", "Truck"}
+    },
+    ["Air Race"] = {
+        ["players"] = 6,
+        ["laps"] = 1,
+        ["CandyRewardRatio"] = 0.0015,
+        ["minimumTime"] = 12,
+        ["creditsReward"] = 13000,
+        ["timeoutLength"] = 130,
+        ["vehicleTypes"] = {"Aircraft"}
+    },
+    ["Tropical Dash"] = {
+        ["players"] = 4,
+        ["laps"] = 1,
+        ["CandyRewardRatio"] = 0.0015,
+        ["minimumTime"] = 5,
+        ["creditsReward"] = 8000,
+        ["timeoutLength"] = 55,
+        ["vehicleTypes"] = {"Dirtbike", "ATV", "Truck"}
+    },
+    ["Circuit Race"] = {
+        ["players"] = 8,
+        ["laps"] = 2,
+        ["CandyRewardRatio"] = 0.001,
+        ["minimumTime"] = 45,
+        ["creditsReward"] = 31750,
+        ["timeoutLength"] = 160,
+        ["vehicleTypes"] = {"Car", "Motorcycle", "Dirtbike", "ATV", "Truck"}
+    },
+    ["Desert Race"] = {
+        ["players"] = 6,
+        ["laps"] = 1,
+        ["CandyRewardRatio"] = 0.0012,
+        ["minimumTime"] = 15,
+        ["creditsReward"] = 13000,
+        ["timeoutLength"] = 130,
+        ["vehicleTypes"] = {"Car", "Motorcycle", "Dirtbike", "ATV", "Truck"}
+    },
+    ["Boat Race"] = {
+        ["players"] = 6,
+        ["laps"] = 1,
+        ["CandyRewardRatio"] = 0.0015,
+        ["minimumTime"] = 15,
+        ["creditsReward"] = 10000,
+        ["timeoutLength"] = 100,
+        ["vehicleTypes"] = {"Boat"}
+    },
+    ["Bloxywood Circuit"] = {
+        ["players"] = 12,
+        ["laps"] = 1,
+        ["CandyRewardRatio"] = 0.001,
+        ["minimumTime"] = 45,
+        ["creditsReward"] = 32000,
+        ["timeoutLength"] = 155,
+        ["vehicleTypes"] = {"Car", "Motorcycle", "Dirtbike", "ATV", "Truck"}
+    },
+    ["Around The Map"] = {
+        ["players"] = 12,
+        ["laps"] = 1,
+        ["CandyRewardRatio"] = 0.001,
+        ["minimumTime"] = 30,
+        ["creditsReward"] = 27500,
+        ["timeoutLength"] = 140,
+        ["vehicleTypes"] = {"Car", "Motorcycle", "Dirtbike", "ATV", "Truck"}
+    },
+    ["Highway Race"] = {
+        ["players"] = 12,
+        ["laps"] = 1,
+        ["CandyRewardRatio"] = 0.0012,
+        ["minimumTime"] = 16,
+        ["creditsReward"] = 14500,
+        ["timeoutLength"] = 100,
+        ["vehicleTypes"] = {"Car", "Motorcycle", "Dirtbike", "ATV", "Truck"}
+    }
+}
