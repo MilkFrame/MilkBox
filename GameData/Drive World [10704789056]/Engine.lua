97acddc3f7668e146e17507aa2a67302b54242e2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-30 20:52:02 +1000

    Create Engine.lua

---
+++ GameData/Drive World [10704789056]/Engine.lua
@@ -0,0 +1,372 @@
+data = {
+    ["BatteryPack"] = {
+        ["Track"] = {
+            ["Subcategory"] = "BatteryPack",
+            ["Category"] = "Engine",
+            ["Order"] = 3,
+            ["PowerShare"] = 0.29700000000000004,
+            ["CashPrice"] = 650000,
+            ["DisplayName"] = "Track Battery"
+        },
+        ["Race"] = {
+            ["Subcategory"] = "BatteryPack",
+            ["Category"] = "Engine",
+            ["PremiumPrice"] = 400,
+            ["CashPrice"] = 1170000,
+            ["PowerShare"] = 0.45,
+            ["Order"] = 4,
+            ["DisplayName"] = "Race Battery"
+        },
+        ["Stock"] = {
+            ["Category"] = "Engine",
+            ["Order"] = 1,
+            ["PowerShare"] = 0,
+            ["Subcategory"] = "BatteryPack",
+            ["DisplayName"] = "Stock Battery"
+        },
+        ["Sport"] = {
+            ["Subcategory"] = "BatteryPack",
+            ["Category"] = "Engine",
+            ["Order"] = 2,
+            ["PowerShare"] = 0.14850000000000002,
+            ["CashPrice"] = 260000,
+            ["DisplayName"] = "Sport Battery"
+        }
+    },
+    ["Pistons"] = {
+        ["Track"] = {
+            ["Subcategory"] = "Pistons",
+            ["Category"] = "Engine",
+            ["Order"] = 3,
+            ["PowerShare"] = 0.198,
+            ["CashPrice"] = 130000,
+            ["DisplayName"] = "Track Pistons"
+        },
+        ["Race"] = {
+            ["Subcategory"] = "Pistons",
+            ["Category"] = "Engine",
+            ["PremiumPrice"] = 180,
+            ["CashPrice"] = 468000,
+            ["PowerShare"] = 0.3,
+            ["Order"] = 4,
+            ["DisplayName"] = "Race Pistons"
+        },
+        ["Stock"] = {
+            ["Category"] = "Engine",
+            ["Order"] = 1,
+            ["PowerShare"] = 0,
+            ["Subcategory"] = "Pistons",
+            ["DisplayName"] = "Stock Pistons"
+        },
+        ["Sport"] = {
+            ["Subcategory"] = "Pistons",
+            ["Category"] = "Engine",
+            ["Order"] = 2,
+            ["PowerShare"] = 0.099,
+            ["CashPrice"] = 91000,
+            ["DisplayName"] = "Sport Pistons"
+        }
+    },
+    ["Aspiration"] = {
+        ["SportSingleTurbo"] = {
+            ["Subcategory"] = "Aspiration",
+            ["Category"] = "Engine",
+            ["AspirationType"] = "Turbo",
+            ["CashPrice"] = 98000,
+            ["PowerShare"] = 0.11549999999999999,
+            ["Order"] = 2,
+            ["DisplayName"] = "Sport Turbo"
+        },
+        ["TrackSupercharger"] = {
+            ["Subcategory"] = "Aspiration",
+            ["Category"] = "Engine",
+            ["AspirationType"] = "Supercharger",
+            ["CashPrice"] = 195000,
+            ["PowerShare"] = 0.23099999999999998,
+            ["Order"] = 3,
+            ["DisplayName"] = "Track Supercharger"
+        },
+        ["Stock"] = {
+            ["Category"] = "Engine",
+            ["Order"] = 1,
+            ["PowerShare"] = 0,
+            ["Subcategory"] = "Aspiration",
+            ["DisplayName"] = "Stock"
+        },
+        ["RaceSupercharger"] = {
+            ["Subcategory"] = "Aspiration",
+            ["Category"] = "Engine",
+            ["Order"] = 5,
+            ["AspirationType"] = "Supercharger",
+            ["CashPrice"] = 487500,
+            ["PowerShare"] = 0.35,
+            ["PremiumPrice"] = 200,
+            ["DisplayName"] = "Race Supercharger"
+        },
+        ["RaceTwinTurbo"] = {
+            ["Subcategory"] = "Aspiration",
+            ["Category"] = "Engine",
+            ["Order"] = 4,
+            ["AspirationType"] = "TwinTurbo",
+            ["CashPrice"] = 487500,
+            ["PowerShare"] = 0.35,
+            ["PremiumPrice"] = 200,
+            ["DisplayName"] = "Race Twin Turbo"
+        }
+    },
+    ["Motor"] = {
+        ["Track"] = {
+            ["Subcategory"] = "Motor",
+            ["Category"] = "Engine",
+            ["Order"] = 3,
+            ["PowerShare"] = 0.29700000000000004,
+            ["CashPrice"] = 650000,
+            ["DisplayName"] = "Track Motor"
+        },
+        ["Race"] = {
+            ["Subcategory"] = "Motor",
+            ["Category"] = "Engine",
+            ["PremiumPrice"] = 400,
+            ["CashPrice"] = 1170000,
+            ["PowerShare"] = 0.45,
+            ["Order"] = 4,
+            ["DisplayName"] = "Race Motor"
+        },
+        ["Stock"] = {
+            ["Category"] = "Engine",
+            ["Order"] = 1,
+            ["PowerShare"] = 0,
+            ["Subcategory"] = "Motor",
+            ["DisplayName"] = "Stock Motor"
+        },
+        ["Sport"] = {
+            ["Subcategory"] = "Motor",
+            ["Category"] = "Engine",
+            ["Order"] = 2,
+            ["PowerShare"] = 0.14850000000000002,
+            ["CashPrice"] = 260000,
+            ["DisplayName"] = "Sport Motor"
+        }
+    },
+    ["Nitrous"] = {
+        ["Track"] = {
+            ["Subcategory"] = "Nitrous",
+            ["Rarity"] = "Rare",
+            ["Category"] = "Engine",
+            ["CashPrice"] = 115000,
+            ["Order"] = 4,
+            ["BoostIncrease"] = 0.5,
+            ["DisplayName"] = "Track Nitrous"
+        },
+        ["Race"] = {
+            ["Subcategory"] = "Nitrous",
+            ["Category"] = "Engine",
+            ["Rarity"] = "Epic",
+            ["PremiumPrice"] = 100,
+            ["CashPrice"] = 234000,
+            ["Order"] = 5,
+            ["BoostIncrease"] = 0.75,
+            ["DisplayName"] = "Race Nitrous"
+        },
+        ["Stock"] = {
+            ["Subcategory"] = "Nitrous",
+            ["Category"] = "Engine",
+            ["Order"] = 1,
+            ["EVUpgrade"] = true,
+            ["BoostIncrease"] = 0,
+            ["DisplayName"] = "Stock Nitrous"
+        },
+        ["Sport"] = {
+            ["Subcategory"] = "Nitrous",
+            ["Category"] = "Engine",
+            ["Order"] = 2,
+            ["CashPrice"] = 33000,
+            ["BoostIncrease"] = 0.25,
+            ["DisplayName"] = "Sport Nitrous"
+        }
+    },
+    ["Profile"] = {
+        ["ItalianV10"] = {
+            ["Subcategory"] = "Profile",
+            ["Category"] = "Engine",
+            ["DisplayOrder"] = 5,
+            ["CashPrice"] = 500000,
+            ["PowerShare"] = 0,
+            ["Order"] = 4,
+            ["DisplayName"] = "V10 Engine"
+        },
+        ["Stock"] = {
+            ["Subcategory"] = "Profile",
+            ["Category"] = "Engine",
+            ["DisplayOrder"] = 1,
+            ["Order"] = 1,
+            ["Preview"] = "rbxassetid://11309932812",
+            ["PowerShare"] = 0,
+            ["DisplayName"] = "Stock Engine"
+        },
+        ["Godzilla34"] = {
+            ["Subcategory"] = "Profile",
+            ["Category"] = "Engine",
+            ["DisplayOrder"] = 2,
+            ["CashPrice"] = 100000,
+            ["PowerShare"] = 0,
+            ["Order"] = 2,
+            ["DisplayName"] = "I6 Engine"
+        },
+        ["ItalianV12"] = {
+            ["Subcategory"] = "Profile",
+            ["Category"] = "Engine",
+            ["PremiumPrice"] = 300,
+            ["DisplayOrder"] = 5,
+            ["CashPrice"] = 1000000,
+            ["PowerShare"] = 0,
+            ["Order"] = 5,
+            ["DisplayName"] = "V12 Engine"
+        },
+        ["V8Engine"] = {
+            ["Subcategory"] = "Profile",
+            ["Category"] = "Engine",
+            ["DisplayOrder"] = 3,
+            ["CashPrice"] = 250000,
+            ["PowerShare"] = 0,
+            ["Order"] = 3,
+            ["DisplayName"] = "V8 Engine"
+        }
+    },
+    ["Exhaust"] = {
+        ["Track"] = {
+            ["Subcategory"] = "Exhaust",
+            ["Category"] = "Engine",
+            ["Order"] = 3,
+            ["PowerShare"] = 0.099,
+            ["CashPrice"] = 85000,
+            ["DisplayName"] = "Track Exhaust"
+        },
+        ["Race"] = {
+            ["Subcategory"] = "Exhaust",
+            ["Category"] = "Engine",
+            ["PremiumPrice"] = 125,
+            ["CashPrice"] = 273000,
+            ["PowerShare"] = 0.15,
+            ["Order"] = 4,
+            ["DisplayName"] = "Race Exhaust"
+        },
+        ["Stock"] = {
+            ["Category"] = "Engine",
+            ["Order"] = 1,
+            ["PowerShare"] = 0,
+            ["Subcategory"] = "Exhaust",
+            ["DisplayName"] = "Stock Exhaust"
+        },
+        ["Sport"] = {
+            ["Subcategory"] = "Exhaust",
+            ["Category"] = "Engine",
+            ["Order"] = 2,
+            ["PowerShare"] = 0.0495,
+            ["CashPrice"] = 33000,
+            ["DisplayName"] = "Sport Exhaust"
+        }
+    },
+    ["Computer"] = {
+        ["Track"] = {
+            ["Subcategory"] = "Computer",
+            ["Category"] = "Engine",
+            ["Order"] = 3,
+            ["PowerShare"] = 0.066,
+            ["CashPrice"] = 59000,
+            ["DisplayName"] = "Track Computer"
+        },
+        ["Race"] = {
+            ["Subcategory"] = "Computer",
+            ["Category"] = "Engine",
+            ["PremiumPrice"] = 85,
+            ["CashPrice"] = 175000,
+            ["PowerShare"] = 0.1,
+            ["Order"] = 4,
+            ["DisplayName"] = "Race Computer"
+        },
+        ["Stock"] = {
+            ["Category"] = "Engine",
+            ["Order"] = 1,
+            ["PowerShare"] = 0,
+            ["Subcategory"] = "Computer",
+            ["DisplayName"] = "Stock Computer"
+        },
+        ["Sport"] = {
+            ["Subcategory"] = "Computer",
+            ["Category"] = "Engine",
+            ["Order"] = 2,
+            ["PowerShare"] = 0.033,
+            ["CashPrice"] = 20000,
+            ["DisplayName"] = "Sport Computer"
+        }
+    },
+    ["Clutch"] = {
+        ["Track"] = {
+            ["Subcategory"] = "Clutch",
+            ["Category"] = "Engine",
+            ["Order"] = 3,
+            ["ShiftSpeed"] = 3,
+            ["CashPrice"] = 39000,
+            ["DisplayName"] = "Track Clutch"
+        },
+        ["Race"] = {
+            ["Subcategory"] = "Clutch",
+            ["Category"] = "Engine",
+            ["PremiumPrice"] = 75,
+            ["CashPrice"] = 156000,
+            ["ShiftSpeed"] = 5,
+            ["Order"] = 4,
+            ["DisplayName"] = "Race Clutch"
+        },
+        ["Stock"] = {
+            ["Category"] = "Engine",
+            ["Order"] = 1,
+            ["ShiftSpeed"] = 1,
+            ["Subcategory"] = "Clutch",
+            ["DisplayName"] = "Stock Clutch"
+        },
+        ["Sport"] = {
+            ["Subcategory"] = "Clutch",
+            ["Category"] = "Engine",
+            ["Order"] = 2,
+            ["ShiftSpeed"] = 2,
+            ["CashPrice"] = 20000,
+            ["DisplayName"] = "Sport Clutch"
+        }
+    },
+    ["Intake"] = {
+        ["Track"] = {
+            ["Subcategory"] = "Intake",
+            ["Category"] = "Engine",
+            ["Order"] = 3,
+            ["PowerShare"] = 0.066,
+            ["CashPrice"] = 78000,
+            ["DisplayName"] = "Track Intake"
+        },
+        ["Race"] = {
+            ["Subcategory"] = "Intake",
+            ["Category"] = "Engine",
+            ["PremiumPrice"] = 125,
+            ["CashPrice"] = 273000,
+            ["PowerShare"] = 0.1,
+            ["Order"] = 4,
+            ["DisplayName"] = "Race Intake"
+        },
+        ["Stock"] = {
+            ["Category"] = "Engine",
+            ["Order"] = 1,
+            ["PowerShare"] = 0,
+            ["Subcategory"] = "Intake",
+            ["DisplayName"] = "Stock Intake"
+        },
+        ["Sport"] = {
+            ["Subcategory"] = "Intake",
+            ["Category"] = "Engine",
+            ["Order"] = 2,
+            ["PowerShare"] = 0.033,
+            ["CashPrice"] = 52000,
+            ["DisplayName"] = "Sport Intake"
+        }
+    }
+}
