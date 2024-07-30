fa6df00ded5ff352d813e415d6930e7796ebe280
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-30 20:46:20 +1000

    Create HalloweenEvent2.lua

---
+++ GameData/Vehicle Legends [4566572536]/HalloweenEvent2.lua
@@ -0,0 +1,43 @@
+data = {
+    ["PumpkinTypes"] = {
+        ["Green"] = {["SpawnChance"] = 0.03, ["Pumpkins"] = 8},
+        ["Purple"] = {["SpawnChance"] = 0.07, ["Pumpkins"] = 5},
+        ["Yellow"] = {["SpawnChance"] = 0.35, ["Pumpkins"] = 2},
+        ["Orange"] = {["SpawnChance"] = 0.5, ["Pumpkins"] = 1},
+        ["Red"] = {["SpawnChance"] = 0.05, ["Pumpkins"] = 3}
+    },
+    ["PumpkinRespawnTime"] = 300,
+    ["Rewards"] = {
+        {["Type"] = "CarSkin", ["Index"] = "HalloweenGhosts", ["Pumpkins"] = 30},
+        {["Type"] = "CarSkin", ["Index"] = "HalloweenCamo", ["Pumpkins"] = 35},
+        {["Type"] = "CarSkin", ["Index"] = "HalloweenCrackedGround", ["Pumpkins"] = 35},
+        {["Type"] = "Customization", ["Index"] = "BatSpoiler", ["Pumpkins"] = 55},
+        {["Type"] = "Customization", ["Index"] = "BatRim", ["Pumpkins"] = 60},
+        {["Type"] = "Customization", ["Index"] = "PumpkinRim", ["Pumpkins"] = 75},
+        {["Type"] = "Customization", ["Index"] = "BladeRim", ["Pumpkins"] = 65},
+        {["IsLicence"] = true, ["Type"] = "Customization", ["Index"] = "HalloweenHousesLicencePlate", ["Pumpkins"] = 65},
+        {["IsLicence"] = true, ["Type"] = "Customization", ["Index"] = "BlueGhostsLicencePlate", ["Pumpkins"] = 65},
+        {["IsLicence"] = true, ["Type"] = "Customization", ["Index"] = "DarkGhostsLicencePlate", ["Pumpkins"] = 65},
+        {["IsLicence"] = true, ["Type"] = "Customization", ["Index"] = "HalloweenHouseLicencePlate", ["Pumpkins"] = 65},
+        {["IsLicence"] = true, ["Type"] = "Customization", ["Index"] = "GhostLicencePlate", ["Pumpkins"] = 65},
+        {["Type"] = "Customization", ["Index"] = "BladeRim2", ["Pumpkins"] = 70},
+        {["Type"] = "Customization", ["Index"] = "SpiderwebRim", ["Pumpkins"] = 65},
+        {["Type"] = "Customization", ["Index"] = "SkullRim", ["Pumpkins"] = 70}
+    },
+    ["PumpkinDevProducts"] = {{1676158299, 105}, {1676158623, 600}, {1676159084, 1300}, {1676159436, 4000}},
+    ["PumpkinToCashBaseConversionRate"] = 350,
+    ["PumpkinToCashExtraConversionRate"] = 250,
+    ["EventEnd"] = 1699851600,
+    ["Dealerships"] = {["Dealership1"] = {1}},
+    ["Cars"] = {
+        {["VehicleType"] = "HotRod", ["Pumpkins"] = 400},
+        {["VehicleType"] = "BamfordBR6", ["Pumpkins"] = 1600},
+        {["VehicleType"] = "AuroraCabrio", ["Pumpkins"] = 200},
+        {["VehicleType"] = "GrandtoureTII", ["Pumpkins"] = 1800},
+        {["VehicleType"] = "Obstructer", ["Pumpkins"] = 600},
+        {["VehicleType"] = "Exec414", ["Pumpkins"] = 600},
+        {["VehicleType"] = "Cavalla377G", ["Pumpkins"] = 1650},
+        {["VehicleType"] = "RansomSkylight", ["Pumpkins"] = 300}
+    },
+    ["PumpkinTypeEnum"] = {["Green"] = "5", ["Purple"] = "4", ["Yellow"] = "2", ["Orange"] = "1", ["Red"] = "3"}
+}
