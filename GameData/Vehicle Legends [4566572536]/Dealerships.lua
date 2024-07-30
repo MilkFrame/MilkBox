23ce8f5cf85f942b408def9ea1222599339813ad
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-30 20:45:01 +1000

    Create Dealerships.lua

---
+++ GameData/Vehicle Legends [4566572536]/Dealerships.lua
@@ -0,0 +1,50 @@
+data = {
+    {
+        ["defaultPaintColor"] = Color3.fromRGB(128, 128, 135),
+        ["fullName"] = "Vehicles",
+        ["canTestDrive"] = true,
+        ["lighting"] = {["dealershipLighting"] = true},
+        ["reflectiveSky"] = false,
+        ["performanceStats"] = true,
+        ["cameraSettings"] = {["maxZoomDistance"] = 40, ["fieldOfView"] = 60},
+        ["music"] = {["id"] = 7024324344, ["startPosition"] = 0, ["volume"] = 0.35},
+        ["dealershipIdentifier"] = 1,
+        ["iconId"] = 12386206186
+    },
+    {
+        ["defaultPaintColor"] = Color3.fromRGB(128, 128, 135),
+        ["fullName"] = "Aviation Catalog",
+        ["canTestDrive"] = false,
+        ["lighting"] = {["dealershipLighting"] = false},
+        ["reflectiveSky"] = false,
+        ["performanceStats"] = false,
+        ["cameraSettings"] = {["maxZoomDistance"] = 70, ["fieldOfView"] = 60},
+        ["music"] = {["id"] = 1836811873, ["startPosition"] = 0, ["volume"] = 0.35},
+        ["dealershipIdentifier"] = 2,
+        ["iconId"] = 8718042635
+    },
+    {
+        ["defaultPaintColor"] = Color3.fromRGB(166, 81, 16),
+        ["fullName"] = "Extra Vehicle Catalog",
+        ["canTestDrive"] = true,
+        ["lighting"] = {["dealershipLighting"] = false},
+        ["reflectiveSky"] = false,
+        ["performanceStats"] = true,
+        ["cameraSettings"] = {["maxZoomDistance"] = 40, ["fieldOfView"] = 60},
+        ["music"] = {["id"] = 1838615551, ["startPosition"] = 0, ["volume"] = 0.2},
+        ["dealershipIdentifier"] = 3,
+        ["iconId"] = 8718043978
+    },
+    {
+        ["defaultPaintColor"] = Color3.fromRGB(130, 0, 0),
+        ["fullName"] = "Boat Catalog",
+        ["canTestDrive"] = false,
+        ["lighting"] = {["dealershipLighting"] = false},
+        ["reflectiveSky"] = false,
+        ["performanceStats"] = true,
+        ["cameraSettings"] = {["maxZoomDistance"] = 70, ["fieldOfView"] = 60},
+        ["music"] = {["id"] = 1836811873, ["startPosition"] = 0, ["volume"] = 0.35},
+        ["dealershipIdentifier"] = 4,
+        ["iconId"] = 8718041319
+    }
+}
