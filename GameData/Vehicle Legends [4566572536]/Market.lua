4b1cf21274dcbd15db17df175315022740a45a36
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-30 20:44:30 +1000

    Create Market.lua

---
+++ GameData/Vehicle Legends [4566572536]/Market.lua
@@ -0,0 +1,97 @@
+data = {
+    ["Gamepasses"] = {
+        {
+            ["Visible"] = true,
+            ["Index"] = "TrackCarsPack",
+            ["serverFunc"] = "function: 0x2f0f4f9bcfd2eab5",
+            ["Name"] = "Track Cars Pack",
+            ["Features"] = {""},
+            ["Cars"] = {"OperaNobleGE", "Spende845", "760GE"},
+            ["Description"] = "",
+            ["GamepassID"] = 188496198,
+            ["IconId"] = 14543711068
+        },
+        {
+            ["Visible"] = true,
+            ["serverFunc"] = "function: 0x1214cb5b847a91d5",
+            ["Index"] = "VIP",
+            ["Features"] = {"VIP-Exclusive Car: Opera 590B!", "25% more money when driving!", "Special chat tag!"},
+            ["Description"] = "VIP Only Car, 25% more money when driving, and a special chat tag!",
+            ["GamepassID"] = 8023299,
+            ["Name"] = "VIP",
+            ["IconId"] = 14544068118
+        },
+        {
+            ["Visible"] = true,
+            ["Index"] = "PortableGarage",
+            ["Features"] = {"Customize your vehicle from anywhere!"},
+            ["Description"] = "Customize your vehicle from anywhere!",
+            ["GamepassID"] = 7926571,
+            ["Name"] = "Portable Garage",
+            ["IconId"] = 9506477548
+        },
+        {
+            ["Visible"] = false,
+            ["Index"] = "PremiumPaints",
+            ["Features"] = {"An extra set of 16 exotic colors for your vehicle!"},
+            ["Description"] = "An extra set of 16 exotic colors for your vehicle!",
+            ["GamepassID"] = 11164612,
+            ["Name"] = "Premium Paints",
+            ["IconId"] = 9506477851
+        },
+        {
+            ["Visible"] = true,
+            ["Index"] = "Radio",
+            ["Features"] = {"Play tunes on your ride!"},
+            ["Description"] = "Play tunes on your ride!",
+            ["GamepassID"] = 7926570,
+            ["Name"] = "Radio",
+            ["IconId"] = 6488691336
+        },
+        {
+            ["Visible"] = true,
+            ["Index"] = "RustMaterial",
+            ["Features"] = {
+                "Gain access to the Rust material in the autoshop!",
+                "WHEN EQUIPPED: +10% MORE MONEY AS YOU DRIVE!"
+            },
+            ["Description"] = "Rust material for your vehicle! +10% When equiped!",
+            ["GamepassID"] = 14467199,
+            ["Name"] = "Rust Material",
+            ["IconId"] = 16466061443
+        },
+        {
+            ["Visible"] = true,
+            ["Index"] = "SilverMaterial",
+            ["Features"] = {
+                "Gain access to the Silver material in the autoshop!",
+                "WHEN EQUIPPED: +15% MORE MONEY AS YOU DRIVE!"
+            },
+            ["Description"] = "Silver material for your vehicle! +15% When equiped!",
+            ["GamepassID"] = 14467192,
+            ["Name"] = "Silver Material",
+            ["IconId"] = 16466062298
+        },
+        {
+            ["Visible"] = true,
+            ["Index"] = "GoldMaterial",
+            ["Features"] = {
+                "Gain access to the Gold material in the autoshop!",
+                "WHEN EQUIPPED: +30% MORE MONEY AS YOU DRIVE!"
+            },
+            ["Description"] = "Gold material for your vehicle! +30% When equiped!",
+            ["GamepassID"] = 14467182,
+            ["Name"] = "Gold Material",
+            ["IconId"] = 16466063302
+        },
+        {
+            ["Visible"] = false,
+            ["serverFunc"] = "function: 0x7afe54dc78ef5675",
+            ["Index"] = "StarterPack",
+            ["Features"] = {"$45,000!", "A car!"},
+            ["Description"] = "Test",
+            ["GamepassID"] = 9798673,
+            ["Name"] = "Starter Pack"
+        }
+    }
+}
