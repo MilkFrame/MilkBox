0c8deb0619aada4fd5e78e259fb40712288b9df6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-30 20:53:37 +1000

    Create Plates.lua

---
+++ GameData/Drive World [10704789056]/Plates.lua
@@ -0,0 +1,294 @@
+data = {
+    ["MorikawaCarbonFiber"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(32, 32, 32), ["Image"] = "rbxassetid://13771947369"},
+        ["TextColor"] = Color3.fromRGB(255, 255, 255),
+        ["DisplayName"] = "Morikawa Carbon Fiber"
+    },
+    ["DarkScrewThread"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(0, 0, 0), ["Image"] = "rbxassetid://13771945177"},
+        ["TextColor"] = Color3.fromRGB(255, 255, 255),
+        ["DisplayName"] = "Dark Screw Thread"
+    },
+    ["Letter"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(255, 255, 255), ["Image"] = "rbxassetid://13771944705"},
+        ["TextColor"] = Color3.fromRGB(50, 50, 150),
+        ["DisplayName"] = "Letter"
+    },
+    ["Rainbow2"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(237, 67, 68), ["Image"] = "rbxassetid://13915173745"},
+        ["TextColor"] = Color3.fromRGB(255, 255, 255),
+        ["DisplayName"] = "Wavy Rainbow"
+    },
+    ["BlackOut"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {
+            ["Image"] = "",
+            ["BackgroundColor"] = Color3.fromRGB(0, 0, 0),
+            ["ImageColor"] = Color3.fromRGB(0, 0, 0)
+        },
+        ["TextColor"] = Color3.fromRGB(255, 255, 255),
+        ["DisplayName"] = "Black Out"
+    },
+    ["Paradise"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(255, 255, 255), ["Image"] = "rbxassetid://13771946030"},
+        ["TextColor"] = Color3.fromRGB(0, 0, 255),
+        ["DisplayName"] = "Golden Valley Paradise"
+    },
+    ["Tide"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(255, 255, 255), ["Image"] = "rbxassetid://13771946114"},
+        ["TextColor"] = Color3.fromRGB(0, 51, 160),
+        ["DisplayName"] = "Golden Valley Tide"
+    },
+    ["Classic"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(0, 0, 0), ["Image"] = ""},
+        ["TextColor"] = Color3.fromRGB(252, 233, 3),
+        ["DisplayName"] = "Classic Plates"
+    },
+    ["GarlandAndBaubles"] = {
+        ["Category"] = "Plates",
+        ["TextColor"] = Color3.fromRGB(241, 191, 28),
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(241, 191, 28), ["Image"] = "rbxassetid://15636418384"},
+        ["Preview"] = "rbxassetid://15636418384",
+        ["DisplayName"] = "Garland & Baubles"
+    },
+    ["NeonLove"] = {
+        ["Category"] = "Plates",
+        ["TextColor"] = Color3.fromRGB(2519, 2150, 2290),
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(111, 0, 33), ["Image"] = "rbxassetid://16289485682"},
+        ["Preview"] = "rbxassetid://16289485682",
+        ["DisplayName"] = "Neon Love Plate"
+    },
+    ["ScrewThread"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(213, 210, 205), ["Image"] = "rbxassetid://13771945275"},
+        ["TextColor"] = Color3.fromRGB(0, 0, 0),
+        ["DisplayName"] = "Screw Thread"
+    },
+    ["Snowflakes"] = {
+        ["Category"] = "Plates",
+        ["TextColor"] = Color3.fromRGB(251, 252, 253),
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(23, 34, 84), ["Image"] = "rbxassetid://15636418301"},
+        ["Preview"] = "rbxassetid://15636418301",
+        ["DisplayName"] = "Snowflakes"
+    },
+    ["LoveCard"] = {
+        ["Category"] = "Plates",
+        ["TextColor"] = Color3.fromRGB(190, 32, 72),
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(231, 227, 250), ["Image"] = "rbxassetid://16289485804"},
+        ["Preview"] = "rbxassetid://16289485804",
+        ["DisplayName"] = "Love Letter Plate"
+    },
+    ["Flames"] = {
+        ["CashPrice"] = 500,
+        ["Background"] = {
+            ["Image"] = "rbxassetid://13771946357",
+            ["BackgroundColor"] = Color3.fromRGB(0, 0, 0),
+            ["ImageColor"] = Color3.fromRGB(255, 255, 255)
+        },
+        ["Category"] = "Plates",
+        ["TextColor"] = Color3.fromRGB(255, 255, 255)
+    },
+    ["Mountain"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(91, 129, 194), ["Image"] = "rbxassetid://13771948392"},
+        ["TextColor"] = Color3.fromRGB(20, 20, 150),
+        ["DisplayName"] = "Mountains"
+    },
+    ["Icoshere"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(207, 204, 198), ["Image"] = "rbxassetid://13771945799"},
+        ["TextColor"] = Color3.fromRGB(0, 0, 0),
+        ["DisplayName"] = "Icosphere"
+    },
+    ["GoldenValleyBlue"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(223, 227, 233), ["Image"] = "rbxassetid://13771947935"},
+        ["TextColor"] = Color3.fromRGB(29, 76, 152),
+        ["DisplayName"] = "Golden Valley City"
+    },
+    ["Bubbles"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(1, 126, 240), ["Image"] = "rbxassetid://13771945411"},
+        ["TextColor"] = Color3.fromRGB(0, 0, 0),
+        ["DisplayName"] = "Bubbles"
+    },
+    ["RetroReplica"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(2, 69, 148), ["Image"] = ""},
+        ["TextColor"] = Color3.fromRGB(255, 255, 255),
+        ["DisplayName"] = "Retro Replica"
+    },
+    ["PoliceGoldenValley"] = {
+        ["TextColor"] = Color3.fromRGB(255, 185, 0),
+        ["Category"] = "Plates",
+        ["CashPrice"] = 0,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(33, 33, 33), ["Image"] = "rbxassetid://14450560192"},
+        ["RequiredPass"] = "Police",
+        ["DisplayName"] = "Golden Valley PD"
+    },
+    ["DrivingIsOurPassion"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(50, 54, 83), ["Image"] = "rbxassetid://13915173899"},
+        ["TextColor"] = Color3.fromRGB(212, 214, 221),
+        ["DisplayName"] = "Passion"
+    },
+    ["Webs"] = {
+        ["Category"] = "Plates",
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(18, 17, 19), ["Image"] = "rbxassetid://15187222852"},
+        ["TextColor"] = Color3.fromRGB(156, 156, 154),
+        ["DisplayName"] = "Webs"
+    },
+    ["Season1"] = {
+        ["Category"] = "Plates",
+        ["TextColor"] = Color3.fromRGB(2, 140, 223),
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(4, 13, 77), ["Image"] = "rbxassetid://18113257926"},
+        ["Preview"] = "rbxassetid://18113257926",
+        ["DisplayName"] = "Season 1 Victor"
+    },
+    ["PastelEvening"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(202, 167, 240), ["Image"] = "rbxassetid://13771947613"},
+        ["TextColor"] = Color3.fromRGB(239, 220, 158),
+        ["DisplayName"] = "Pastel Evening"
+    },
+    ["LegacyLogo"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(44, 44, 44), ["Image"] = "rbxassetid://13771947515"},
+        ["TextColor"] = Color3.fromRGB(44, 44, 44),
+        ["DisplayName"] = "Legacy Logo"
+    },
+    ["CMYKSynthwave"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(10, 1, 34), ["Image"] = "rbxassetid://13771948233"},
+        ["TextColor"] = Color3.fromRGB(10, 232, 236),
+        ["DisplayName"] = "CMYK Synthwave"
+    },
+    ["IcosphereDark"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(37, 35, 31), ["Image"] = "rbxassetid://13771945641"},
+        ["TextColor"] = Color3.fromRGB(255, 255, 255),
+        ["DisplayName"] = "Icosphere Dark"
+    },
+    ["Stock"] = {
+        ["Category"] = "Plates",
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(33, 33, 33), ["Image"] = "rbxassetid://13771946482"},
+        ["TextColor"] = Color3.fromRGB(255, 185, 0),
+        ["DisplayName"] = "Golden Valley Black"
+    },
+    ["Gears"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(100, 100, 100), ["Image"] = "rbxassetid://13771945937"},
+        ["TextColor"] = Color3.fromRGB(0, 0, 0),
+        ["DisplayName"] = "Gears"
+    },
+    ["Rainbow"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(255, 255, 255), ["Image"] = "rbxassetid://13771945005"},
+        ["TextColor"] = Color3.fromRGB(0, 0, 0),
+        ["DisplayName"] = "Rainbow"
+    },
+    ["Pumpkins"] = {
+        ["Category"] = "Plates",
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(31, 30, 84), ["Image"] = "rbxassetid://15187223051"},
+        ["TextColor"] = Color3.fromRGB(255, 229, 255),
+        ["DisplayName"] = "Pumpkins"
+    },
+    ["GoldenValleyClassic"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(240, 240, 240), ["Image"] = "rbxassetid://13771946825"},
+        ["TextColor"] = Color3.fromRGB(0, 24, 165),
+        ["DisplayName"] = "Golden Valley Classic"
+    },
+    ["Empire"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(0, 0, 0), ["Image"] = "rbxassetid://13771944893"},
+        ["TextColor"] = Color3.fromRGB(0, 0, 50),
+        ["DisplayName"] = "Empire"
+    },
+    ["MesaSunset"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(26, 26, 26), ["Image"] = "rbxassetid://13771947774"},
+        ["TextColor"] = Color3.fromRGB(26, 26, 26),
+        ["DisplayName"] = "Mesa Sunset"
+    },
+    ["ChocolateBar"] = {
+        ["Category"] = "Plates",
+        ["TextColor"] = Color3.fromRGB(53, 21, 10),
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(67, 33, 16), ["Image"] = "rbxassetid://16289485946"},
+        ["Preview"] = "rbxassetid://16289485946",
+        ["DisplayName"] = "Chocolate Bar Plate"
+    },
+    ["MissingTexture"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(0, 0, 0), ["Image"] = "rbxassetid://13771947037"},
+        ["TextColor"] = Color3.fromRGB(255, 255, 255),
+        ["DisplayName"] = "Missing Texture"
+    },
+    ["CrayonDrawing"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(239, 236, 230), ["Image"] = "rbxassetid://13771947199"},
+        ["TextColor"] = Color3.fromRGB(174, 58, 176),
+        ["DisplayName"] = "Crayon Drawing"
+    },
+    ["America"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(165, 22, 60), ["Image"] = "rbxassetid://13915850367"},
+        ["TextColor"] = Color3.fromRGB(0, 0, 0),
+        ["DisplayName"] = "USA"
+    },
+    ["MorikawaLight"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(208, 165, 29), ["Image"] = "rbxassetid://13772722298"},
+        ["TextColor"] = Color3.fromRGB(1, 1, 1),
+        ["DisplayName"] = "Morikawa Light"
+    },
+    ["Gambler"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(0, 0, 0), ["Image"] = "rbxassetid://13772020804"},
+        ["TextColor"] = Color3.fromRGB(255, 255, 255),
+        ["DisplayName"] = "Gambler"
+    },
+    ["MorikawaStandard"] = {
+        ["Category"] = "Plates",
+        ["CashPrice"] = 500,
+        ["Background"] = {["ImageColor"] = Color3.fromRGB(226, 226, 226), ["Image"] = "rbxassetid://13772020900"},
+        ["TextColor"] = Color3.fromRGB(1, 70, 1),
+        ["DisplayName"] = "Morikawa Standard"
+    }
+}
