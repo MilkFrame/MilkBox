a3caee9ad856a581944fac573933ad1d5439ca18
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-30 20:45:53 +1000

    Create Settings.lua

---
+++ GameData/Vehicle Legends [4566572536]/Settings.lua
@@ -0,0 +1,75 @@
+data = {
+    ["shadowsEnabled"] = {
+        ["enabled"] = true,
+        ["nameFull"] = "Shadows",
+        ["toggleKind"] = "OnOff",
+        ["toggleText"] = {"Enabled", "Disabled"},
+        ["clientfunc"] = "function: 0xa889ac9bf60c1c35"
+    },
+    ["speedometerKMH"] = {
+        ["enabled"] = true,
+        ["nameFull"] = "Unit",
+        ["toggleKind"] = "YesNo",
+        ["toggleText"] = {"KMH", "MPH"},
+        ["clientfunc"] = "function: 0xf04d8f54b2fe1735"
+    },
+    ["beaconsEnabled"] = {
+        ["enabled"] = true,
+        ["nameFull"] = "Location Beacons",
+        ["toggleKind"] = "YesNo",
+        ["gradientsReversed"] = true,
+        ["toggleText"] = {"Hidden", "Visible"},
+        ["clientfunc"] = "function: 0x98d50c58996bd755"
+    },
+    ["postProcessing"] = {
+        ["enabled"] = false,
+        ["nameFull"] = "",
+        ["toggleKind"] = "YesNo",
+        ["toggleText"] = {"Enabled", "Disabled"},
+        ["clientfunc"] = "function: 0x334c35d205cd5f95"
+    },
+    ["pvpEnabled"] = {
+        ["enabled"] = true,
+        ["nameFull"] = "PVP (Collision)",
+        ["toggleKind"] = "OnOff",
+        ["serverFunc"] = "function: 0xddfaa9c24b739cf5",
+        ["toggleText"] = {"Enabled", "Disabled"},
+        ["clientfunc"] = "function: 0xe761215b92e5d855"
+    },
+    ["raceMusic"] = {
+        ["enabled"] = true,
+        ["nameFull"] = "Race Music",
+        ["toggleKind"] = "YesNo",
+        ["toggleText"] = {"Enabled", "Muted"},
+        ["clientfunc"] = "function: 0x19ea02a112565115"
+    },
+    ["radiosMuted"] = {
+        ["enabled"] = true,
+        ["nameFull"] = "Other Radios",
+        ["toggleKind"] = "YesNo",
+        ["toggleText"] = {"Muted", "Unmuted"},
+        ["clientfunc"] = "function: 0x5423b11a40a71db5"
+    },
+    ["doorButtons"] = {
+        ["enabled"] = true,
+        ["nameFull"] = "Door Buttons",
+        ["toggleKind"] = "OnOff",
+        ["toggleText"] = {"Enabled", "Disabled"},
+        ["clientfunc"] = "function: 0x6f801b1020bf2295"
+    },
+    ["pvpMode"] = {
+        ["enabled"] = false,
+        ["nameFull"] = "Car Collisions",
+        ["toggleKind"] = "OnOff",
+        ["toggleText"] = {"Enabled", "Disabled"},
+        ["clientfunc"] = "function: 0xa2be97e46411e5f5"
+    },
+    ["carLocked"] = {
+        ["enabled"] = true,
+        ["nameFull"] = "Car Locked",
+        ["toggleKind"] = "YesNo",
+        ["serverFunc"] = "function: 0x42108412c9929b75",
+        ["toggleText"] = {"Locked", "Unlocked"},
+        ["clientfunc"] = "function: 0x8d3b3a9a873ad8d5"
+    }
+}
