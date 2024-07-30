5d215015779cf10371916e4232dc59edb658a372
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-30 20:47:13 +1000

    Create ChristmasEvent2023.lua

---
+++ GameData/Vehicle Legends [4566572536]/ChristmasEvent2023.lua
@@ -0,0 +1,33 @@
+data = {
+    ["CandyToCashExtraConversionRate"] = 250,
+    ["CandyDevProducts"] = {{1711054170, 105}, {1711054303, 600}, {1711054387, 1300}, {1711054669, 4000}},
+    ["CandyToCashBaseConversionRate"] = 350,
+    ["GiftTypes"] = {
+        ["Candy1"] = {["SpawnChance"] = 0.5, ["Candy"] = 1},
+        ["Candy5"] = {["SpawnChance"] = 0.03, ["Candy"] = 8},
+        ["Candy3"] = {["SpawnChance"] = 0.05, ["Candy"] = 3},
+        ["Candy2"] = {["SpawnChance"] = 0.35, ["Candy"] = 2},
+        ["Candy4"] = {["SpawnChance"] = 0.07, ["Candy"] = 5}
+    },
+    ["Cars"] = {
+        {["VehicleType"] = "InfernoGE", ["Candy"] = 400},
+        {["VehicleType"] = "LADRowood", ["Candy"] = 1250},
+        {["VehicleType"] = "CavallaN10", ["Candy"] = 2500}
+    },
+    ["GiftRespawnTime"] = 300,
+    ["EventEnd"] = 1704430800,
+    ["Dealerships"] = {["Dealership1"] = {1}},
+    ["GiftTypeEnum"] = {["Candy1"] = "1", ["Candy5"] = "5", ["Candy3"] = "3", ["Candy2"] = "2", ["Candy4"] = "4"},
+    ["Rewards"] = {
+        {["Type"] = "CarSkin", ["Index"] = "2023Festive1", ["Candy"] = 25},
+        {["Type"] = "CarSkin", ["Index"] = "2023Festive2", ["Candy"] = 30},
+        {["Type"] = "CarSkin", ["Index"] = "2023Festive3", ["Candy"] = 35},
+        {["Type"] = "CarSkin", ["Index"] = "2023Festive4", ["Candy"] = 45},
+        {["Type"] = "Customization", ["Index"] = "SantaBagWing", ["Candy"] = 100},
+        {["Type"] = "Customization", ["Index"] = "AntlersWing", ["Candy"] = 65},
+        {["Type"] = "Customization", ["Index"] = "ChristmasTreeRim", ["Candy"] = 50},
+        {["Type"] = "Customization", ["Index"] = "SnowflakeRim", ["Candy"] = 65},
+        {["Type"] = "Customization", ["Index"] = "GingerbreadRim", ["Candy"] = 70},
+        {["Type"] = "Customization", ["Index"] = "ChristmasTreeRoof", ["Candy"] = 125}
+    }
+}
