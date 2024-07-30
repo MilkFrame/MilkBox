bd079563ba8cfeb8612450e24f14d08d5c90811b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-30 20:53:49 +1000

    Create Accessories.lua

---
+++ GameData/Drive World [10704789056]/Accessories.lua
@@ -0,0 +1,545 @@
+data = {
+    ["FrontMod"] = {
+        ["BunnyTeeth"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "FrontMod",
+            ["AttachmentName"] = "FrontNoseAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "BunnyTeeth"
+        },
+        ["RudolphNose"] = {
+            ["Subcategory"] = "FrontMod",
+            ["AttachmentName"] = "FrontNoseAttach",
+            ["Category"] = "Accessories",
+            ["DisplayName"] = "Rudolph Nose"
+        },
+        ["Bumper1"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 5000,
+            ["AttachmentName"] = "FrontBottomAttach",
+            ["Subcategory"] = "FrontMod",
+            ["DisplayName"] = "Truck Bumper"
+        },
+        ["PresentBow"] = {
+            ["Subcategory"] = "FrontMod",
+            ["AttachmentName"] = "FrontCenterAttach",
+            ["Category"] = "Accessories",
+            ["DisplayName"] = "Present Bow (Front)"
+        },
+        ["SnowPlow"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 10000,
+            ["AttachmentName"] = "FrontBottomAttach",
+            ["Subcategory"] = "FrontMod",
+            ["DisplayName"] = "Snow Plow"
+        },
+        ["TowHook"] = {
+            ["Subcategory"] = "FrontMod",
+            ["Category"] = "Accessories",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Front Tow Hook"
+        },
+        ["HoodScoop"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "FrontMod",
+            ["AttachmentName"] = "FrontHoodAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Hood Scoop"
+        },
+        ["ChristmasWreath"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "FrontMod",
+            ["AttachmentName"] = "FrontHoodAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Holiday Wreath"
+        },
+        ["PeppermintCandy"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "FrontMod",
+            ["AttachmentName"] = "FrontCenterAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Peppermint Candy"
+        },
+        ["Heart"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "FrontMod",
+            ["AttachmentName"] = "FrontHoodAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Hood Heart"
+        },
+        ["SafetyBumper"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 5000,
+            ["AttachmentName"] = "FrontBottomAttach",
+            ["Subcategory"] = "FrontMod",
+            ["DisplayName"] = "Safety Bumper"
+        },
+        ["Mustache"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "FrontMod",
+            ["AttachmentName"] = "FrontCenterAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Mustache"
+        },
+        ["FrontLights"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 4500,
+            ["AttachmentName"] = "FrontCenterAttach",
+            ["Subcategory"] = "FrontMod",
+            ["DisplayName"] = "Front Lights"
+        },
+        ["None"] = {
+            ["CashPrice"] = 0,
+            ["Subcategory"] = "FrontMod",
+            ["Category"] = "Accessories",
+            ["DisplayName"] = "None"
+        }
+    },
+    ["Spoiler"] = {
+        ["Spoiler4"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 3500,
+            ["AttachmentName"] = "SpoilerAttach",
+            ["Subcategory"] = "Spoiler",
+            ["DisplayName"] = "GT Wing"
+        },
+        ["DoubleFlatWing"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 3500,
+            ["AttachmentName"] = "SpoilerAttach",
+            ["Subcategory"] = "Spoiler",
+            ["DisplayName"] = "Double Flat Wing"
+        },
+        ["BatWing"] = {
+            ["CashPrice"] = 5000,
+            ["AttachmentName"] = "SpoilerAttach",
+            ["Subcategory"] = "Spoiler",
+            ["Category"] = "Accessories"
+        },
+        ["DecoratedChassisWing"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "Spoiler",
+            ["AttachmentName"] = "SpoilerRearAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Christmas Chassis Wing"
+        },
+        ["Spoiler2"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 3500,
+            ["AttachmentName"] = "SpoilerAttach",
+            ["Subcategory"] = "Spoiler",
+            ["DisplayName"] = "Aero Wing"
+        },
+        ["StackedWing"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "Spoiler",
+            ["AttachmentName"] = "SpoilerAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Stacked Wing"
+        },
+        ["Spoiler1"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 3500,
+            ["AttachmentName"] = "SpoilerAttach",
+            ["Subcategory"] = "Spoiler",
+            ["DisplayName"] = "Helix Wing"
+        },
+        ["ChassisWing"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 5000,
+            ["AttachmentName"] = "SpoilerRearAttach",
+            ["Subcategory"] = "Spoiler",
+            ["DisplayName"] = "Chassis Wing"
+        },
+        ["FlatWing"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 3500,
+            ["AttachmentName"] = "SpoilerAttach",
+            ["Subcategory"] = "Spoiler",
+            ["DisplayName"] = "Flat Wing"
+        },
+        ["TeslaCoilSpoiler"] = {
+            ["Subcategory"] = "Spoiler",
+            ["Category"] = "Accessories",
+            ["AttachmentName"] = "SpoilerAttach",
+            ["Preview"] = "rbxassetid://15115116674",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Tesla Coil Spoiler"
+        },
+        ["DecoratedSmallWing"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "Spoiler",
+            ["AttachmentName"] = "SpoilerAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Christmas Small Wing"
+        },
+        ["SmallWing"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 1500,
+            ["AttachmentName"] = "SpoilerAttach",
+            ["Subcategory"] = "Spoiler",
+            ["DisplayName"] = "Small Wing"
+        },
+        ["CupidWingSpoiler"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "Spoiler",
+            ["AttachmentName"] = "SpoilerAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Cupid's Wings"
+        },
+        ["DriftWing"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "Spoiler",
+            ["AttachmentName"] = "SpoilerAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Drift Wing"
+        },
+        ["FrozenSpoiler"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "Spoiler",
+            ["AttachmentName"] = "SpoilerAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Icicle Wing"
+        },
+        ["BigWing"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 3500,
+            ["AttachmentName"] = "SpoilerAttach",
+            ["Subcategory"] = "Spoiler",
+            ["DisplayName"] = "Big Wing"
+        },
+        ["BrokenHeartSpoiler"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "Spoiler",
+            ["AttachmentName"] = "SpoilerAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Shattered Heart Spoiler"
+        },
+        ["None"] = {
+            ["CashPrice"] = 0,
+            ["Subcategory"] = "Spoiler",
+            ["Category"] = "Accessories",
+            ["DisplayName"] = "None"
+        }
+    },
+    ["RearMod"] = {
+        ["BunnyTail"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "RearMod",
+            ["AttachmentName"] = "SpoilerRearAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Bunny Tail"
+        },
+        ["RearDiffuser1"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 2500,
+            ["AttachmentName"] = "RearBottomAttach",
+            ["Subcategory"] = "RearMod",
+            ["DisplayName"] = "Smooth Rear Diffuser"
+        },
+        ["StudentDriverSticker"] = {
+            ["Subcategory"] = "RearMod",
+            ["AttachmentName"] = "RearBottomAttach",
+            ["Category"] = "Accessories",
+            ["DisplayName"] = "Student Driver Sign"
+        },
+        ["BroomRack"] = {
+            ["Subcategory"] = "RearMod",
+            ["AttachmentName"] = "RearBottomAttach",
+            ["Category"] = "Accessories",
+            ["DisplayName"] = "Broom Rack"
+        },
+        ["WheelieBar"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "RearMod",
+            ["AttachmentName"] = "RearBottomAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Wheelie Bar"
+        },
+        ["RearDiffuser2"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 2500,
+            ["AttachmentName"] = "RearBottomAttach",
+            ["Subcategory"] = "RearMod",
+            ["DisplayName"] = "Spiked Rear Diffuser"
+        },
+        ["BikeRack"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 4000,
+            ["AttachmentName"] = "RearBottomAttach",
+            ["Subcategory"] = "RearMod",
+            ["DisplayName"] = "Bike Rack"
+        },
+        ["WindUpKey"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "RearMod",
+            ["AttachmentName"] = "SpoilerRearAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Wind-Up Key"
+        },
+        ["None"] = {
+            ["CashPrice"] = 0,
+            ["Subcategory"] = "RearMod",
+            ["Category"] = "Accessories",
+            ["DisplayName"] = "None"
+        }
+    },
+    ["FrontHookMod"] = {
+        ["DemonHeart"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 3500,
+            ["AttachmentName"] = "FrontTowAttach",
+            ["Subcategory"] = "FrontHookMod",
+            ["DisplayName"] = "Demon Heart"
+        },
+        ["HeartBroken"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 3000,
+            ["AttachmentName"] = "FrontTowAttach",
+            ["Subcategory"] = "FrontHookMod",
+            ["DisplayName"] = "Heart Broken"
+        },
+        ["IcicleTowHook"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "FrontHookMod",
+            ["AttachmentName"] = "FrontTowAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Icicle Tow Hook"
+        },
+        ["Triangle"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 2500,
+            ["AttachmentName"] = "FrontTowAttach",
+            ["Subcategory"] = "FrontHookMod",
+            ["DisplayName"] = "Triangle"
+        },
+        ["Loop"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 2500,
+            ["AttachmentName"] = "FrontTowAttach",
+            ["Subcategory"] = "FrontHookMod",
+            ["DisplayName"] = "Loop"
+        },
+        ["Strap"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 3000,
+            ["AttachmentName"] = "FrontTowAttach",
+            ["Subcategory"] = "FrontHookMod",
+            ["DisplayName"] = "Strap"
+        },
+        ["Heart"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 3000,
+            ["AttachmentName"] = "FrontTowAttach",
+            ["Subcategory"] = "FrontHookMod",
+            ["DisplayName"] = "Heart"
+        },
+        ["ShotHeart"] = {
+            ["Subcategory"] = "FrontHookMod",
+            ["AttachmentName"] = "FrontTowAttach",
+            ["Category"] = "Accessories",
+            ["DisplayName"] = "Arrowed Heart Hook"
+        },
+        ["None"] = {
+            ["CashPrice"] = 0,
+            ["Subcategory"] = "FrontHookMod",
+            ["Category"] = "Accessories",
+            ["DisplayName"] = "None"
+        }
+    },
+    ["StickerMod"] = {
+        ["None"] = {
+            ["CashPrice"] = 0,
+            ["Subcategory"] = "StickerMod",
+            ["Category"] = "Accessories",
+            ["DisplayName"] = "None"
+        },
+        ["StudentDriver"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 1500,
+            ["AttachmentName"] = "RearStickerAttach",
+            ["Subcategory"] = "StickerMod",
+            ["DisplayName"] = "Student Driver"
+        }
+    },
+    ["TopMod"] = {
+        ["PresentBow"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "TopMod",
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Present Bow (Top)"
+        },
+        ["RoofRack1Filled"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 3000,
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Subcategory"] = "TopMod",
+            ["DisplayName"] = "Large Roof Rack (Full)"
+        },
+        ["TaxiRoofLight"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 2500,
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Subcategory"] = "TopMod",
+            ["DisplayName"] = "Taxi Cab Light"
+        },
+        ["Skibox"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 1500,
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Subcategory"] = "TopMod",
+            ["DisplayName"] = "Skibox"
+        },
+        ["FuturisticPoliceLights"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "TopMod",
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Hi-tech Light Bar"
+        },
+        ["RoofLights"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 2500,
+            ["AttachmentName"] = "RoofFrontAttach",
+            ["Subcategory"] = "TopMod",
+            ["DisplayName"] = "Light Bar"
+        },
+        ["ChocolateDelivery"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "TopMod",
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "ChocolateDelivery"
+        },
+        ["BunnyEars"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "TopMod",
+            ["AttachmentName"] = "RoofFrontAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Bunny Ears"
+        },
+        ["RoofLEDLightBar"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "TopMod",
+            ["AttachmentName"] = "RoofFrontAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "LED Light Bar"
+        },
+        ["RotaryLight"] = {
+            ["Subcategory"] = "TopMod",
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 1000,
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["RequiredPass"] = "Police",
+            ["DisplayName"] = "Rotary Light"
+        },
+        ["SackofPresents"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "TopMod",
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Sack O' Presents Topper"
+        },
+        ["UncleSamsHat"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "TopMod",
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Uncle Sam Hat"
+        },
+        ["TeddyBear"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "TopMod",
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Teddy Bear"
+        },
+        ["ChocolateBoxes"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "TopMod",
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Chocolate Boxes Roof"
+        },
+        ["PumpkinMouthed"] = {
+            ["Subcategory"] = "TopMod",
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Category"] = "Accessories",
+            ["DisplayName"] = "Haunted Pumpkin"
+        },
+        ["ChainedCasket"] = {
+            ["Subcategory"] = "TopMod",
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Category"] = "Accessories",
+            ["DisplayName"] = "Chained Casket"
+        },
+        ["RudolphAntlers"] = {
+            ["Subcategory"] = "TopMod",
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Category"] = "Accessories",
+            ["DisplayName"] = "Rudolph Antlers"
+        },
+        ["Cauldron"] = {
+            ["Subcategory"] = "TopMod",
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Category"] = "Accessories",
+            ["DisplayName"] = "Brewing Cauldron"
+        },
+        ["RoofRack1"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 2000,
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Subcategory"] = "TopMod",
+            ["DisplayName"] = "Large Roof Rack"
+        },
+        ["SantaHat"] = {
+            ["Subcategory"] = "TopMod",
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Category"] = "Accessories",
+            ["DisplayName"] = "Santa Hat"
+        },
+        ["ChristmasTree"] = {
+            ["Subcategory"] = "TopMod",
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Category"] = "Accessories",
+            ["DisplayName"] = "Christmas Tree"
+        },
+        ["USLightBar1"] = {
+            ["Subcategory"] = "TopMod",
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 3000,
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["RequiredPass"] = "Police",
+            ["DisplayName"] = "US Light Bar 1"
+        },
+        ["Surfboard"] = {
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 1500,
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Subcategory"] = "TopMod",
+            ["DisplayName"] = "Surfboard"
+        },
+        ["HolidayTrumpets"] = {
+            ["Category"] = "Accessories",
+            ["Subcategory"] = "TopMod",
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["Offsale"] = true,
+            ["DisplayName"] = "Holiday Trumpets Topper"
+        },
+        ["USLightBar2"] = {
+            ["Subcategory"] = "TopMod",
+            ["Category"] = "Accessories",
+            ["CashPrice"] = 3000,
+            ["AttachmentName"] = "RoofCenterAttach",
+            ["RequiredPass"] = "Police",
+            ["DisplayName"] = "US Light Bar 2"
+        },
+        ["None"] = {
+            ["CashPrice"] = 0,
+            ["Subcategory"] = "TopMod",
+            ["Category"] = "Accessories",
+            ["DisplayName"] = "None"
+        }
+    }
+}
