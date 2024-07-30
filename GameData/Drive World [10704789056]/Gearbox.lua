1635fd4100af08f4dbb8ef7ed0d1505e947ed90c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-30 20:54:02 +1000

    Create Gearbox.lua

---
+++ GameData/Drive World [10704789056]/Gearbox.lua
@@ -0,0 +1,56 @@
+data = {
+    ["Profile"] = {
+        ["SevenSpeed"] = {
+            ["GearCount"] = 7,
+            ["Subcategory"] = "Profile",
+            ["Category"] = "Gearbox",
+            ["Gears"] = {3.133, 2.083, 1.575, 1.244, 0.979, 0.788, 0.677, 0, -2.647},
+            ["CashPrice"] = 120000,
+            ["FinalDrive"] = 4.89,
+            ["Order"] = 3,
+            ["DisplayName"] = "7-speed"
+        },
+        ["Stock"] = {["Order"] = 1, ["Subcategory"] = "Profile", ["Category"] = "Gearbox", ["DisplayName"] = "Stock"},
+        ["TenSpeed"] = {
+            ["Gears"] = {6, 4, 2.87, 2.1, 1.6, 1.31, 1.1, 0.946, 0.829, 0.73, 0, -4.866},
+            ["CashPrice"] = 200000,
+            ["DisplayName"] = "10-speed",
+            ["FinalDrive"] = 2.85,
+            ["Category"] = "Gearbox",
+            ["Order"] = 6,
+            ["GearCount"] = 10,
+            ["ProductName"] = "SmallModPurchase",
+            ["Subcategory"] = "Profile"
+        },
+        ["NineSpeed"] = {
+            ["GearCount"] = 9,
+            ["Subcategory"] = "Profile",
+            ["Category"] = "Gearbox",
+            ["Gears"] = {5.354, 3.243, 2.252, 1.636, 1.211, 1, 0.865, 0.717, 0.601, 0, -4.798},
+            ["CashPrice"] = 160000,
+            ["FinalDrive"] = 3.06,
+            ["Order"] = 5,
+            ["DisplayName"] = "9-speed"
+        },
+        ["EightSpeed"] = {
+            ["GearCount"] = 8,
+            ["Subcategory"] = "Profile",
+            ["Category"] = "Gearbox",
+            ["Gears"] = {2.905, 1.759, 1.22, 0.878, 0.653, 0.508, 0.397, 0.329, 0, -2.632},
+            ["CashPrice"] = 140000,
+            ["FinalDrive"] = 5.56,
+            ["Order"] = 4,
+            ["DisplayName"] = "8-speed"
+        },
+        ["SixSpeed"] = {
+            ["GearCount"] = 6,
+            ["Subcategory"] = "Profile",
+            ["Category"] = "Gearbox",
+            ["Gears"] = {3.75, 2.38, 1.72, 1.34, 1.08, 0.88, 0, -3.42},
+            ["CashPrice"] = 100000,
+            ["FinalDrive"] = 3.96,
+            ["Order"] = 2,
+            ["DisplayName"] = "6-speed"
+        }
+    }
+}
