6cb457c740ff46e897f8171f505d06b25f49f667
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-30 20:52:24 +1000

    Create Garages.lua

---
+++ GameData/Drive World [10704789056]/Garages.lua
@@ -0,0 +1,22 @@
+data = {
+    ["Starter"] = {
+        ["Order"] = 1,
+        ["Preview"] = "rbxassetid://17197490123",
+        ["Category"] = "Garages",
+        ["DisplayName"] = "Mechanic's Garage"
+    },
+    ["Showroom"] = {
+        ["Category"] = "Garages",
+        ["Order"] = 2,
+        ["Preview"] = "rbxassetid://17197260293",
+        ["CashPrice"] = 10000000,
+        ["DisplayName"] = "Showroom Garage"
+    },
+    ["GreenScreen"] = {
+        ["Category"] = "Garages",
+        ["Order"] = 3,
+        ["Preview"] = "rbxassetid://17197490340",
+        ["CashPrice"] = 300000,
+        ["DisplayName"] = "Green-screen Garage"
+    }
+}
