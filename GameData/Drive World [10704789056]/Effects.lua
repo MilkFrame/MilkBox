06b719292453615632b101328ba07100a94f9fc2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-31 22:00:18 +1000

    Create Effects.lua

---
+++ GameData/Drive World [10704789056]/Effects.lua
@@ -0,0 +1,203 @@
+data = {
+    ["Backfire"] = {
+        ["NewDefault"] = {
+            ["Category"] = "Effects",
+            ["Subcategory"] = "Backfire",
+            ["Preview"] = "rbxassetid://13558752324",
+            ["RequiredPass"] = "NitroFlame",
+            ["DisplayName"] = "Modern"
+        },
+        ["Stock"] = {
+            ["Subcategory"] = "Backfire",
+            ["Preview"] = "rbxassetid://13558752099",
+            ["Category"] = "Effects",
+            ["DisplayName"] = "Stock"
+        },
+        ["Contrast"] = {
+            ["Subcategory"] = "Backfire",
+            ["Preview"] = "rbxassetid://16303023763",
+            ["Category"] = "Effects",
+            ["DisplayName"] = "Contrast"
+        },
+        ["Rainbow"] = {
+            ["Category"] = "Effects",
+            ["ProductName"] = "UnlockRainbowBackfire",
+            ["Preview"] = "rbxassetid://13841684353",
+            ["Subcategory"] = "Backfire",
+            ["DisplayName"] = "Rainbow"
+        },
+        ["Stars"] = {
+            ["Subcategory"] = "Backfire",
+            ["Preview"] = "rbxassetid://16042330222",
+            ["Category"] = "Effects",
+            ["DisplayName"] = "Stars"
+        },
+        ["Lightning"] = {
+            ["Category"] = "Effects",
+            ["ProductName"] = "UnlockLightningBackfire",
+            ["Preview"] = "rbxassetid://13558752517",
+            ["Subcategory"] = "Backfire",
+            ["DisplayName"] = "Lightning"
+        },
+        ["HeartsBackfire"] = {
+            ["Subcategory"] = "Backfire",
+            ["Preview"] = "rbxassetid://16303023616",
+            ["Category"] = "Effects",
+            ["DisplayName"] = "Hearts"
+        },
+        ["RomanCandle"] = {
+            ["Subcategory"] = "Backfire",
+            ["Preview"] = "rbxassetid://18296681078",
+            ["Category"] = "Effects",
+            ["DisplayName"] = "Roman Candle"
+        },
+        ["Cybernetic"] = {
+            ["Subcategory"] = "Backfire",
+            ["Preview"] = "rbxassetid://17294634715",
+            ["Category"] = "Effects",
+            ["DisplayName"] = "Cybernetic"
+        },
+        ["Stardust"] = {
+            ["Category"] = "Effects",
+            ["ProductName"] = "UnlockStardustBackfire",
+            ["Preview"] = "rbxassetid://14228422766",
+            ["Subcategory"] = "Backfire",
+            ["DisplayName"] = "Stardust"
+        },
+        ["Ethereal"] = {
+            ["Subcategory"] = "Backfire",
+            ["Preview"] = "rbxassetid://15115059485",
+            ["Category"] = "Effects",
+            ["DisplayName"] = "Ethereal"
+        }
+    },
+    ["Underglow"] = {
+        ["Stock"] = {["Subcategory"] = "Underglow", ["Category"] = "Effects"},
+        ["Equipped"] = {["Subcategory"] = "Underglow", ["Category"] = "Effects"}
+    },
+    ["Nitrous"] = {
+        ["Cybernetic"] = {
+            ["Subcategory"] = "Nitrous",
+            ["Preview"] = "rbxassetid://17294634550",
+            ["Category"] = "Effects",
+            ["DisplayName"] = "Cybernetic"
+        },
+        ["Smoke"] = {
+            ["Category"] = "Effects",
+            ["ProductName"] = "UnlockSmokeNitrous",
+            ["Preview"] = "rbxassetid://13558751674",
+            ["Subcategory"] = "Nitrous",
+            ["DisplayName"] = "Smoke"
+        },
+        ["BlackHole"] = {
+            ["Category"] = "Effects",
+            ["ProductName"] = "UnlockBlackHoleNitrous",
+            ["Preview"] = "rbxassetid://14228423190",
+            ["Subcategory"] = "Nitrous",
+            ["DisplayName"] = "Black Hole"
+        },
+        ["WildWaves"] = {
+            ["Category"] = "Effects",
+            ["ProductName"] = "UnlockWildWavesNitrous",
+            ["Preview"] = "rbxassetid://14228422225",
+            ["Subcategory"] = "Nitrous",
+            ["DisplayName"] = "Wild Waves"
+        },
+        ["Lightning"] = {
+            ["ProductName"] = "UnlockLightningNitrous",
+            ["Preview"] = "rbxassetid://13558752705",
+            ["Subcategory"] = "Nitrous",
+            ["Category"] = "Effects"
+        },
+        ["HeartsNitro"] = {
+            ["Subcategory"] = "Nitrous",
+            ["Preview"] = "rbxassetid://16303023444",
+            ["Category"] = "Effects",
+            ["DisplayName"] = "Hearts"
+        },
+        ["Ethereal"] = {
+            ["Subcategory"] = "Nitrous",
+            ["Preview"] = "rbxassetid://15115059485",
+            ["Category"] = "Effects",
+            ["DisplayName"] = "Ethereal"
+        },
+        ["NewDefault"] = {
+            ["Category"] = "Effects",
+            ["Subcategory"] = "Nitrous",
+            ["Preview"] = "rbxassetid://13558752202",
+            ["RequiredPass"] = "NitroFlame",
+            ["DisplayName"] = "Modern"
+        },
+        ["Stock"] = {
+            ["Subcategory"] = "Nitrous",
+            ["Preview"] = "rbxassetid://13558751924",
+            ["Category"] = "Effects",
+            ["DisplayName"] = "Stock"
+        },
+        ["Contrast"] = {
+            ["Subcategory"] = "Nitrous",
+            ["Preview"] = "rbxassetid://14648887101",
+            ["Category"] = "Effects",
+            ["DisplayName"] = "Contrast"
+        },
+        ["Rainbow"] = {
+            ["Category"] = "Effects",
+            ["ProductName"] = "UnlockRainbowNitrous",
+            ["Preview"] = "rbxassetid://13772354045",
+            ["Subcategory"] = "Nitrous",
+            ["DisplayName"] = "Rainbow"
+        },
+        ["Fireworks"] = {
+            ["Subcategory"] = "Nitrous",
+            ["Preview"] = "rbxassetid://18296680789",
+            ["Category"] = "Effects",
+            ["DisplayName"] = "Fireworks"
+        },
+        ["ClassicSparkles"] = {
+            ["Subcategory"] = "Nitrous",
+            ["Preview"] = "rbxassetid://17599578715",
+            ["Category"] = "Effects",
+            ["DisplayName"] = "Retro Sparkles"
+        },
+        ["ShockDiamonds"] = {
+            ["Category"] = "Effects",
+            ["ProductName"] = "UnlockShockDiamondsNitrous",
+            ["Preview"] = "rbxassetid://13558751789",
+            ["Subcategory"] = "Nitrous",
+            ["DisplayName"] = "Shock Diamonds"
+        },
+        ["Stars"] = {
+            ["Subcategory"] = "Nitrous",
+            ["Preview"] = "rbxassetid://16042330066",
+            ["Category"] = "Effects",
+            ["DisplayName"] = "Stars"
+        },
+        ["Petals"] = {
+            ["Category"] = "Effects",
+            ["ProductName"] = "UnlockPetalsNitrous",
+            ["Preview"] = "rbxassetid://14228422973",
+            ["Subcategory"] = "Nitrous",
+            ["DisplayName"] = "Petals"
+        },
+        ["Stardust"] = {
+            ["Category"] = "Effects",
+            ["ProductName"] = "UnlockStardustNitrous",
+            ["Preview"] = "rbxassetid://14228422476",
+            ["Subcategory"] = "Nitrous",
+            ["DisplayName"] = "Stardust"
+        },
+        ["Flames"] = {
+            ["Subcategory"] = "Nitrous",
+            ["Preview"] = "rbxassetid://13558753060",
+            ["Category"] = "Effects",
+            ["DisplayName"] = "Flames"
+        },
+        ["GlowLines"] = {
+            ["Category"] = "Effects",
+            ["ProductName"] = "UnlockGlowLines",
+            ["Preview"] = "rbxassetid://13558752890",
+            ["Subcategory"] = "Nitrous",
+            ["DisplayName"] = "Glow Lines"
+        }
+    }
+}
