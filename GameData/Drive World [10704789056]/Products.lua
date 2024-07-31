9b88c113b60579a937f3cbc758574326b5625dac
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-31 22:02:05 +1000

    Create Products.lua

---
+++ GameData/Drive World [10704789056]/Products.lua
@@ -0,0 +1,236 @@
+data = {
+    ["UnlockLightningBackfire"] = {["Id"] = 1512053700, ["Type"] = "ItemUnlock"},
+    ["Tier8"] = {
+        ["Type"] = "Cash",
+        ["Id"] = 1661121167,
+        ["Preview"] = "rbxassetid://13764670648",
+        ["CurrencyAmount"] = 17000000,
+        ["Hidden"] = true
+    },
+    ["ValentinesBundleRepeat"] = {
+        ["Id"] = 1754237938,
+        ["Type"] = "PackRepeat",
+        ["PackName"] = "ValentinesBundle",
+        ["DisplayName"] = "Diavolo Evo"
+    },
+    ["Tier5"] = {
+        ["Type"] = "Cash",
+        ["Id"] = 1661116597,
+        ["Preview"] = "rbxassetid://13764670537",
+        ["CurrencyAmount"] = 600000,
+        ["ExtraPercentage"] = 0.25
+    },
+    ["UnlockGlowLines"] = {["Id"] = 1551761551, ["Type"] = "ItemUnlock"},
+    ["Tier4"] = {
+        ["Type"] = "Cash",
+        ["Id"] = 1661112366,
+        ["Preview"] = "rbxassetid://13764670394",
+        ["CurrencyAmount"] = 255000,
+        ["ExtraPercentage"] = 0.15
+    },
+    ["Tier2Event"] = {
+        ["Id"] = 1750330148,
+        ["Type"] = "EventCurrency",
+        ["CurrencyAmount"] = 500,
+        ["Preview"] = "rbxassetid://16279076410"
+    },
+    ["Tier3Gold"] = {
+        ["Id"] = 1760986905,
+        ["Type"] = "PremiumCurrency",
+        ["CurrencyAmount"] = 400,
+        ["Preview"] = "rbxassetid://16481995040"
+    },
+    ["UnlockSmokeNitrous"] = {["Id"] = 1512053954, ["Type"] = "ItemUnlock"},
+    ["Tier5Gold"] = {
+        ["BundleDisplay"] = true,
+        ["Type"] = "PremiumCurrency",
+        ["Id"] = 1760987147,
+        ["Preview"] = "rbxassetid://16481974553",
+        ["CurrencyAmount"] = 2500,
+        ["ExtraPercentage"] = 0.15
+    },
+    ["GiftLegendaryPack"] = {["Id"] = 1706250384, ["Type"] = "WinterGift"},
+    ["UnlockLightningNitrous"] = {["Id"] = 1512053806, ["Type"] = "ItemUnlock"},
+    ["UnlockHarokuXtreme"] = {["Id"] = 1873432562, ["Type"] = "SeasonUnlock"},
+    ["UnlockAltus"] = {["Id"] = 1873433973, ["Type"] = "SeasonUnlock"},
+    ["SmallModPurchase"] = {["Id"] = 1697595305, ["Type"] = "CarMod"},
+    ["Missions100Races"] = {["Id"] = 1512053806, ["Type"] = "RewardPurchase"},
+    ["UnlockShockDiamondsNitrous"] = {["Id"] = 1512054024, ["Type"] = "ItemUnlock"},
+    ["UnlockRainbowBackfire"] = {["Id"] = 1564687198, ["Type"] = "ItemUnlock"},
+    ["Cirrus441RallyBundle"] = {
+        ["Id"] = 1821299327,
+        ["Type"] = "Pack",
+        ["PackName"] = "Cirrus441RallyBundle",
+        ["DisplayName"] = "Cirrus 441 Rally Bundle"
+    },
+    ["GiftUnderglowPass"] = {["Id"] = 1711900137, ["Type"] = "WinterGift"},
+    ["Tier7"] = {
+        ["Type"] = "Cash",
+        ["Id"] = 1661120840,
+        ["Preview"] = "rbxassetid://13764670648",
+        ["CurrencyAmount"] = 5000000,
+        ["Hidden"] = true
+    },
+    ["UnlockCostello"] = {["Id"] = 1873431488, ["Type"] = "SeasonUnlock"},
+    ["GiftLaunchPack"] = {["Id"] = 1706248191, ["Type"] = "WinterGift"},
+    ["Tier1Winter"] = {["Id"] = 1706315358, ["Type"] = "WinterSkip"},
+    ["LargeModPurchase"] = {["Id"] = 1697596233, ["Type"] = "CarMod"},
+    ["UnlockBlackHoleNitrous"] = {["Id"] = 1593092124, ["Type"] = "ItemUnlock"},
+    ["Tier4Gold"] = {
+        ["Type"] = "PremiumCurrency",
+        ["Id"] = 1760987023,
+        ["Preview"] = "rbxassetid://16481994836",
+        ["CurrencyAmount"] = 1000,
+        ["ExtraPercentage"] = 0.1
+    },
+    ["GiftPolicePass"] = {["Id"] = 1711899879, ["Type"] = "WinterGift"},
+    ["SkipDailyQuest"] = {["Id"] = 1873428517, ["Type"] = "SkipQuest"},
+    ["GiftNexus2"] = {["Id"] = 1711113656, ["Type"] = "WinterGift"},
+    ["UnlockDamascusSteel"] = {["Id"] = 1555807823, ["Type"] = "ItemUnlock"},
+    ["SkipWeeklyQuest"] = {["Id"] = 1873428736, ["Type"] = "SkipQuest"},
+    ["ZeusBundleRepeat"] = {
+        ["Id"] = 1789095883,
+        ["Type"] = "PackRepeat",
+        ["PackName"] = "ZeusEvoBundle",
+        ["DisplayName"] = "Zeus Evo"
+    },
+    ["GiftVIPPass"] = {["Id"] = 1711899504, ["Type"] = "WinterGift"},
+    ["UnlockVern"] = {["Id"] = 1888493902, ["Type"] = "SeasonUnlock"},
+    ["OsirisRepeat"] = {
+        ["Id"] = 1888084732,
+        ["Type"] = "PackRepeat",
+        ["PackName"] = "OsirisBundle",
+        ["DisplayName"] = "Osiris Car"
+    },
+    ["UnlockStardustNitrous"] = {["Id"] = 1593094280, ["Type"] = "ItemUnlock"},
+    ["Tier1Event"] = {
+        ["Id"] = 1750329920,
+        ["Type"] = "EventCurrency",
+        ["CurrencyAmount"] = 100,
+        ["Preview"] = "rbxassetid://16278996334"
+    },
+    ["CybervoloRepeat"] = {
+        ["Id"] = 1816045207,
+        ["Type"] = "PackRepeat",
+        ["PackName"] = "CybervoloBundle",
+        ["DisplayName"] = "Cybervolo Car"
+    },
+    ["UnlockPolygonalWrap"] = {["Id"] = 1512054098, ["Type"] = "ItemUnlock"},
+    ["ZeusBundle"] = {
+        ["Id"] = 1789095120,
+        ["Type"] = "Pack",
+        ["PackName"] = "ZeusEvoBundle",
+        ["DisplayName"] = "Zeus Evo Bundle"
+    },
+    ["ScimitarEvoBundle"] = {
+        ["Id"] = 1796027343,
+        ["Type"] = "Pack",
+        ["PackName"] = "ScimitarEvoBundle",
+        ["DisplayName"] = "Scimitar Evo Car"
+    },
+    ["UnlockWildWavesNitrous"] = {["Id"] = 1593094969, ["Type"] = "ItemUnlock"},
+    ["UnlockStardustBackfire"] = {["Id"] = 1593093399, ["Type"] = "ItemUnlock"},
+    ["UnlockPetalsNitrous"] = {["Id"] = 1593092909, ["Type"] = "ItemUnlock"},
+    ["UnlockDentedGloss"] = {["Id"] = 1512053881, ["Type"] = "ItemUnlock"},
+    ["Tier1Gold"] = {
+        ["Id"] = 1760986429,
+        ["Type"] = "PremiumCurrency",
+        ["CurrencyAmount"] = 25,
+        ["Preview"] = "rbxassetid://16480007726"
+    },
+    ["UnlockRainbowSplatterWrap"] = {["Id"] = 1555809432, ["Type"] = "ItemUnlock"},
+    ["ScimitarEvoRepeat"] = {
+        ["Id"] = 1796026615,
+        ["Type"] = "PackRepeat",
+        ["PackName"] = "ScimitarEvoBundle",
+        ["DisplayName"] = "Scimitar Evo Car"
+    },
+    ["Tier3Event"] = {
+        ["Id"] = 1750330370,
+        ["Type"] = "EventCurrency",
+        ["CurrencyAmount"] = 1600,
+        ["Preview"] = "rbxassetid://16279100981"
+    },
+    ["ABOnboardingDeal"] = {["Id"] = 1888901561, ["Type"] = "ABOnboardingDeal"},
+    ["GiftNitroPass"] = {["Id"] = 1711900345, ["Type"] = "WinterGift"},
+    ["Cirrus441RallyRepeat"] = {
+        ["Id"] = 1821299701,
+        ["Type"] = "PackRepeat",
+        ["PackName"] = "Cirrus441RallyBundle",
+        ["DisplayName"] = "Cirrus 441 Rally Car"
+    },
+    ["UnlockRainbowPolygonWrap"] = {["Id"] = 1551761178, ["Type"] = "ItemUnlock"},
+    ["ValentinesBundle"] = {
+        ["Id"] = 1750337815,
+        ["Type"] = "Pack",
+        ["PackName"] = "ValentinesBundle",
+        ["DisplayName"] = "Valentines Bundle"
+    },
+    ["UnlockRainbowNitrous"] = {["Id"] = 1559949973, ["Type"] = "ItemUnlock"},
+    ["UnlockJeweledWrap"] = {["Id"] = 1559878076, ["Type"] = "ItemUnlock"},
+    ["UnlockFlatPolygons"] = {["Id"] = 1564686626, ["Type"] = "ItemUnlock"},
+    ["UnlockCamoWrap"] = {["Id"] = 1512053566, ["Type"] = "ItemUnlock"},
+    ["UnlockNebulaWrap"] = {["Id"] = 1593044666, ["Type"] = "RewardPurchase"},
+    ["MediumModPurchase"] = {["Id"] = 1697595799, ["Type"] = "CarMod"},
+    ["Tier1"] = {
+        ["Id"] = 1661111288,
+        ["Type"] = "Cash",
+        ["CurrencyAmount"] = 12000,
+        ["Preview"] = "rbxassetid://13764670310"
+    },
+    ["Trailer200mi"] = {["Id"] = 1512053954, ["Type"] = "RewardPurchase"},
+    ["GiftKingfisher"] = {["Id"] = 1706312497, ["Type"] = "WinterGift"},
+    ["Missons500kDrift"] = {["Id"] = 1512053881, ["Type"] = "RewardPurchase"},
+    ["GiftGridiron"] = {["Id"] = 1706312040, ["Type"] = "WinterGift"},
+    ["Missions50Races"] = {["Id"] = 1512053700, ["Type"] = "RewardPurchase"},
+    ["OsirisBundle"] = {
+        ["Id"] = 1888084520,
+        ["Type"] = "Pack",
+        ["PackName"] = "OsirisBundle",
+        ["DisplayName"] = "Osiris Police Bundle"
+    },
+    ["GiftStarterPass"] = {["Id"] = 1711900598, ["Type"] = "WinterGift"},
+    ["UnlockStinger"] = {["Id"] = 1873433585, ["Type"] = "SeasonUnlock"},
+    ["Missions100Jobs"] = {["Id"] = 1512053566, ["Type"] = "RewardPurchase"},
+    ["Tier3"] = {
+        ["Id"] = 1661112142,
+        ["Type"] = "Cash",
+        ["CurrencyAmount"] = 135000,
+        ["Preview"] = "rbxassetid://13764670394"
+    },
+    ["WeeklyQuestPurchase"] = {["Id"] = 1888493902, ["Type"] = "RewardPurchase"},
+    ["Tier2Winter"] = {["Id"] = 1706316680, ["Type"] = "WinterSkip"},
+    ["Tier2"] = {
+        ["Id"] = 1661111764,
+        ["Type"] = "Cash",
+        ["CurrencyAmount"] = 55000,
+        ["Preview"] = "rbxassetid://13764670310"
+    },
+    ["Tier2Gold"] = {
+        ["Id"] = 1760986616,
+        ["Type"] = "PremiumCurrency",
+        ["CurrencyAmount"] = 175,
+        ["Preview"] = "rbxassetid://16481995040"
+    },
+    ["GiftSpeedWindPack"] = {["Id"] = 1706250884, ["Type"] = "WinterGift"},
+    ["Tier4Event"] = {
+        ["Id"] = 1750334319,
+        ["Type"] = "EventCurrency",
+        ["CurrencyAmount"] = 8900,
+        ["Preview"] = "rbxassetid://16279104359"
+    },
+    ["GiftTrackCars"] = {["Id"] = 1706249467, ["Type"] = "WinterGift"},
+    ["CybervoloBundle"] = {
+        ["Id"] = 1816044900,
+        ["Type"] = "Pack",
+        ["PackName"] = "CybervoloBundle",
+        ["DisplayName"] = "Cybervolo Bundle"
+    },
+    ["Tier6"] = {
+        ["Type"] = "Cash",
+        ["Id"] = 1661116719,
+        ["Preview"] = "rbxassetid://13764670648",
+        ["CurrencyAmount"] = 2500000,
+        ["ExtraPercentage"] = 0.45
+    }
+}
