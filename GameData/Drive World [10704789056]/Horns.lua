8934eb6579b7e494d0bf2336981dd1c857234ea7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-31 22:01:21 +1000

    Create Horns.lua

---
+++ GameData/Drive World [10704789056]/Horns.lua
@@ -0,0 +1,14 @@
+data = {
+    ["Air"] = {["CashPrice"] = 85000, ["Category"] = "Horns", ["DisplayName"] = "Air Horn"},
+    ["Stock"] = {["Category"] = "Horns", ["DisplayName"] = "Stock Horn"},
+    ["EagleScreech"] = {
+        ["CashPrice"] = 7476,
+        ["Category"] = "Horns",
+        ["Offsale"] = true,
+        ["DisplayName"] = "Freedom Horn"
+    },
+    ["Sport"] = {["CashPrice"] = 60000, ["Category"] = "Horns", ["DisplayName"] = "Sport Horn"},
+    ["WakeUp"] = {["Category"] = "Horns", ["PremiumPrice"] = 100, ["DisplayName"] = "Wakey Wakey Horn"},
+    ["Clown"] = {["Category"] = "Horns", ["PremiumPrice"] = 35, ["DisplayName"] = "Clown Horn"},
+    ["Truck"] = {["CashPrice"] = 50000, ["Category"] = "Horns", ["DisplayName"] = "Truck Horn"}
+}
