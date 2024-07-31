79237233b2e705228dd2d19effa2ae6fa1a703a0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-31 21:59:59 +1000

    Create Wraps.lua

---
+++ GameData/Drive World [10704789056]/Wraps.lua
@@ -0,0 +1,421 @@
+data = {
+    ["Checkered"] = {
+        ["PaintMultiplierCost"] = 3,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "Checkered",
+        ["Preview"] = "rbxassetid://13558137593",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Checkered"
+    },
+    ["Bluesteel"] = {
+        ["PaintMultiplierCost"] = 3,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "Bluesteel",
+        ["Preview"] = "rbxassetid://16736582692",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Bluesteel"
+    },
+    ["SparkleTime"] = {
+        ["OverlayTexture"] = {
+            ["Color3"] = Color3.fromRGB(500, 500, 500),
+            ["Texture"] = "rbxassetid://18398393936",
+            ["ZIndex"] = 1,
+            ["StudsPerTileV"] = 4,
+            ["StudsPerTileU"] = 4
+        },
+        ["PaintMultiplierCost"] = 3,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "SparkleTime",
+        ["Preview"] = "rbxassetid://18398651331",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Sparkle Time"
+    },
+    ["Obsidian"] = {
+        ["PaintMultiplierCost"] = 2,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "Obsidian",
+        ["Preview"] = "rbxassetid://15636573641",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Obsidian"
+    },
+    ["MercuryWood"] = {
+        ["PaintMultiplierCost"] = 3,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "MercuryWood",
+        ["Preview"] = "rbxassetid://14182778303",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Mercury Wood"
+    },
+    ["FlatPolygon"] = {
+        ["MaterialVariant"] = "PolygonalFlat",
+        ["PaintMultiplierCost"] = 3,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["ProductName"] = "UnlockFlatPolygons",
+        ["Preview"] = "rbxassetid://13558081664",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Flat Polygons"
+    },
+    ["VIP"] = {
+        ["MaterialVariant"] = "GoldenWeave",
+        ["PaintMultiplierCost"] = 4,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["SupportsReflectance"] = false,
+        ["Preview"] = "rbxassetid://13558310599",
+        ["RequiredPass"] = "VIP",
+        ["DisplayName"] = "Golden Weave"
+    },
+    ["DamascusSteel"] = {
+        ["MaterialVariant"] = "Damascus",
+        ["PaintMultiplierCost"] = 4,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["ProductName"] = "UnlockDamascusSteel",
+        ["Preview"] = "rbxassetid://13693769472",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Damascus Steel"
+    },
+    ["DentedGloss"] = {
+        ["MaterialVariant"] = "DentedGloss",
+        ["PaintMultiplierCost"] = 2,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["ProductName"] = "UnlockDentedGloss",
+        ["Preview"] = "rbxassetid://13558081793",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Dented Gloss"
+    },
+    ["Polygonal"] = {
+        ["MaterialVariant"] = "Polygonal",
+        ["PaintMultiplierCost"] = 3,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["ProductName"] = "UnlockPolygonalWrap",
+        ["Preview"] = "rbxassetid://13558137448",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Polygons"
+    },
+    ["Tiles"] = {
+        ["PaintMultiplierCost"] = 3,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "Tiles",
+        ["Preview"] = "rbxassetid://15636568345",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Tiles"
+    },
+    ["PaintSplatter"] = {
+        ["PaintMultiplierCost"] = 2,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "PaintSplatter",
+        ["Preview"] = "rbxassetid://13694910440",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Paint Splatter"
+    },
+    ["Jeweled"] = {
+        ["MaterialVariant"] = "Jeweled",
+        ["PaintMultiplierCost"] = 5,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["ProductName"] = "UnlockJeweledWrap",
+        ["Preview"] = "rbxassetid://13558080573",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Jeweled"
+    },
+    ["Snowy"] = {
+        ["PaintMultiplierCost"] = 2,
+        ["BaseMaterial"] = "Snow",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "Snowy",
+        ["Preview"] = "rbxassetid://15636572342",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Snowy"
+    },
+    ["Season1"] = {
+        ["PaintMultiplierCost"] = 3,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "Season1",
+        ["Preview"] = "rbxassetid://18113149684",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Season 1 Victor"
+    },
+    ["PaintSplatterRainbow"] = {
+        ["MaterialVariant"] = "PaintSplatterRainbow",
+        ["PaintMultiplierCost"] = 3,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["ProductName"] = "UnlockRainbowSplatterWrap",
+        ["Preview"] = "rbxassetid://13694910334",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Rainbow Splatter"
+    },
+    ["Vaporwave"] = {
+        ["PaintMultiplierCost"] = 2,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "Vaporwave",
+        ["Preview"] = "rbxassetid://14975899949",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Vaporwave"
+    },
+    ["Battenburg"] = {
+        ["MaterialVariant"] = "Battenburg",
+        ["PaintMultiplierCost"] = 2,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["SupportsReflectance"] = false,
+        ["Preview"] = "rbxassetid://14450886239",
+        ["RequiredPass"] = "Police",
+        ["DisplayName"] = "Battenburg Checkers"
+    },
+    ["Victory"] = {
+        ["OverlayTexture"] = {
+            ["Color3"] = Color3.fromRGB(200, 200, 2000),
+            ["Texture"] = "rbxassetid://15311169620",
+            ["ZIndex"] = 1,
+            ["StudsPerTileV"] = 4,
+            ["StudsPerTileU"] = 4
+        },
+        ["PaintMultiplierCost"] = 3,
+        ["BaseMaterial"] = "Glass",
+        ["Category"] = "Wraps",
+        ["Preview"] = "rbxassetid://15318396163",
+        ["OverrideColor"] = Color3.fromRGB(25, 0, 31),
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Victory"
+    },
+    ["CrimsonCrossed"] = {
+        ["OverlayTexture"] = {
+            ["Color3"] = Color3.fromRGB(200, 200, 200),
+            ["Texture"] = "rbxassetid://16190469883",
+            ["ZIndex"] = 1,
+            ["StudsPerTileV"] = 1,
+            ["StudsPerTileU"] = 1
+        },
+        ["PaintMultiplierCost"] = 2,
+        ["BaseMaterial"] = "Glass",
+        ["Category"] = "Wraps",
+        ["Preview"] = "rbxassetid://16302967855",
+        ["SupportsReflectance"] = true,
+        ["DisplayName"] = "Crimson Crossed"
+    },
+    ["Webs"] = {
+        ["PaintMultiplierCost"] = 2,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "Webs",
+        ["Preview"] = "rbxassetid://15114997122",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Webs"
+    },
+    ["Stars"] = {
+        ["OverlayTexture"] = {
+            ["Color3"] = Color3.fromRGB(211, 211, 211),
+            ["Texture"] = "rbxassetid://18311695397",
+            ["ZIndex"] = 1,
+            ["StudsPerTileV"] = 3,
+            ["StudsPerTileU"] = 3
+        },
+        ["PaintMultiplierCost"] = 2,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "MetallicPaint",
+        ["Preview"] = "rbxassetid://18350242680",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Stars"
+    },
+    ["HillCamo"] = {
+        ["PaintMultiplierCost"] = 3,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "HillCamo",
+        ["Preview"] = "rbxassetid://13558080910",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Woodland Camo B"
+    },
+    ["Ethereal"] = {
+        ["OverlayTexture"] = {
+            ["Color3"] = Color3.fromRGB(100, 1000, 100),
+            ["Texture"] = "rbxassetid://15029793371",
+            ["ZIndex"] = 1,
+            ["StudsPerTileV"] = 8,
+            ["StudsPerTileU"] = 8
+        },
+        ["BaseMaterial"] = "ForceField",
+        ["OverrideColor"] = Color3.fromRGB(110, 255, 110),
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Ethereal",
+        ["OverrideTransparency"] = -10,
+        ["PaintMultiplierCost"] = 4,
+        ["Category"] = "Wraps",
+        ["Preview"] = "rbxassetid://15115070643"
+    },
+    ["PolygonalAnnivesary2023"] = {
+        ["PaintMultiplierCost"] = 2,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "PolygonalAnnivesary2023",
+        ["Preview"] = "rbxassetid://15715979843",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Anniversary 2023"
+    },
+    ["RainbowPolygon"] = {
+        ["MaterialVariant"] = "PolygonalRainbow",
+        ["PaintMultiplierCost"] = 4,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["ProductName"] = "UnlockRainbowPolygonWrap",
+        ["Preview"] = "rbxassetid://13629368185",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Rainbow Polygons"
+    },
+    ["Camo"] = {
+        ["MaterialVariant"] = "Camo",
+        ["PaintMultiplierCost"] = 2,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["ProductName"] = "UnlockCamoWrap",
+        ["Preview"] = "rbxassetid://13558082500",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Woodland Camo A"
+    },
+    ["Heartbreaker"] = {
+        ["PaintMultiplierCost"] = 3,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "Heartbreaker",
+        ["Preview"] = "rbxassetid://16302967722",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Heartbreaker"
+    },
+    ["Prototype"] = {
+        ["PaintMultiplierCost"] = 2,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "CamoPrototype",
+        ["Preview"] = "rbxassetid://13629367981",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Prototype Camo"
+    },
+    ["Candy"] = {
+        ["PaintMultiplierCost"] = 2,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "Candy",
+        ["Preview"] = "rbxassetid://13558082311",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Candy"
+    },
+    ["Lucky"] = {
+        ["PaintMultiplierCost"] = 3,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "Lucky",
+        ["Preview"] = "rbxassetid://16302967593",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Card Suits"
+    },
+    ["Nebula"] = {
+        ["PaintMultiplierCost"] = 3,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "Nebula",
+        ["Preview"] = "rbxassetid://14215237001",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Nebula"
+    },
+    ["Bombastic"] = {
+        ["OverlayTexture"] = {
+            ["Color3"] = Color3.fromRGB(2000, 150, 0),
+            ["Texture"] = "rbxassetid://16747263236",
+            ["ZIndex"] = 1,
+            ["StudsPerTileV"] = 5,
+            ["StudsPerTileU"] = 5
+        },
+        ["PaintMultiplierCost"] = 2,
+        ["BaseMaterial"] = "Glass",
+        ["Category"] = "Wraps",
+        ["Preview"] = "rbxassetid://16747330434",
+        ["OverrideColor"] = Color3.fromRGB(180, 0, 0),
+        ["SupportsReflectance"] = true,
+        ["DisplayName"] = "Bombastic"
+    },
+    ["Holiday"] = {
+        ["OverlayTexture"] = {
+            ["Color3"] = Color3.fromRGB(500, 500, 400),
+            ["Texture"] = "rbxassetid://15627896546",
+            ["ZIndex"] = 1,
+            ["StudsPerTileV"] = 3,
+            ["StudsPerTileU"] = 3
+        },
+        ["PaintMultiplierCost"] = 3,
+        ["BaseMaterial"] = "SmoothPlastic",
+        ["Category"] = "Wraps",
+        ["Preview"] = "rbxassetid://15636585906",
+        ["SupportsReflectance"] = true,
+        ["DisplayName"] = "String Lights"
+    },
+    ["Hexapanels"] = {
+        ["PaintMultiplierCost"] = 4,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "Hexapanels",
+        ["Preview"] = "rbxassetid://13558081163",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Hexapanels"
+    },
+    ["WrappingPaper"] = {
+        ["Offsale"] = true,
+        ["PaintMultiplierCost"] = 3,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "WrappingPaper",
+        ["Preview"] = "rbxassetid://15636566729",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Wrapping Paper"
+    },
+    ["Glacial"] = {
+        ["PaintMultiplierCost"] = 2,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "Glacial",
+        ["Preview"] = "rbxassetid://15636575490",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Glacial"
+    },
+    ["SpaceHearts"] = {
+        ["PaintMultiplierCost"] = 2,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "SpaceHearts",
+        ["Preview"] = "rbxassetid://16302967436",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Space Hearts"
+    },
+    ["CMYK"] = {
+        ["PaintMultiplierCost"] = 2,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "CMYK",
+        ["Preview"] = "rbxassetid://15259313817",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "CMYK"
+    },
+    ["CellCamo"] = {
+        ["PaintMultiplierCost"] = 3,
+        ["BaseMaterial"] = "Plastic",
+        ["Category"] = "Wraps",
+        ["MaterialVariant"] = "CellCamo",
+        ["Preview"] = "rbxassetid://13558082160",
+        ["SupportsReflectance"] = false,
+        ["DisplayName"] = "Cell Camo"
+    }
+}
