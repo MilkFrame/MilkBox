6da53315706a05e2c5e3a82510b28735b5879e26
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-31 22:02:16 +1000

    Create Passes.lua

---
+++ GameData/Drive World [10704789056]/Passes.lua
@@ -0,0 +1,44 @@
+data = {
+    ["PremiumTrackPass"] = {
+        ["Id"] = 633112831,
+        ["ShortDescription"] = "Unlocks the premium track in Halloween Pass.",
+        ["Hidden"] = true,
+        ["DisplayName"] = "Halloween Premium Track"
+    },
+    ["CinemaClassicsPack"] = {
+        ["Id"] = 756348588,
+        ["Type"] = "Pack",
+        ["Order"] = 5,
+        ["DisplayName"] = "Cinema Classics Pack"
+    },
+    ["Police"] = {
+        ["Id"] = 236485373,
+        ["ShortDescription"] = "Includes Battenburg wrap and access to police cars & accessories",
+        ["DisplayName"] = "Police"
+    },
+    ["StarterPack"] = {
+        ["ShortDescription"] = "Includes 790 Renn and $150k Instant Cash",
+        ["Type"] = "Pack",
+        ["Id"] = 270322340,
+        ["Order"] = 0,
+        ["DisplayName"] = "Starter Pack"
+    },
+    ["SpeedWindPack"] = {["Id"] = 173012853, ["Type"] = "Pack", ["Order"] = 1, ["DisplayName"] = "Speed & Wind Pack"},
+    ["LegendaryPack"] = {["Id"] = 172372337, ["Type"] = "Pack", ["Order"] = 2, ["DisplayName"] = "Legendary Pack"},
+    ["VIP"] = {
+        ["Id"] = 113846981,
+        ["ShortDescription"] = "$500k one time cash, Golden Weave Wrap & 20% extra driving cash!",
+        ["DisplayName"] = "VIP"
+    },
+    ["LaunchPack"] = {["Id"] = 172372251, ["Type"] = "Pack", ["Order"] = 4, ["DisplayName"] = "Widebody Pack"},
+    ["Underglow"] = {
+        ["Id"] = 190052809,
+        ["ShortDescription"] = "Equip and customize your car's underglow!",
+        ["DisplayName"] = "Underglow"
+    },
+    ["NitroFlame"] = {
+        ["Id"] = 178614478,
+        ["ShortDescription"] = [[Tune exhaust flame colors! Includes "Modern" Nitrous & Backfire effects.]],
+        ["DisplayName"] = "Nitro Flame Tuning"
+    }
+}
