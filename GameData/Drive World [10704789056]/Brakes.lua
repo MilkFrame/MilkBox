79e0ebac984973b479987d8d27a392cbe072c400
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-31 22:00:43 +1000

    Create Brakes.lua

---
+++ GameData/Drive World [10704789056]/Brakes.lua
@@ -0,0 +1,28 @@
+data = {
+    ["Track"] = {
+        ["Rarity"] = "Rare",
+        ["Category"] = "Brakes",
+        ["CashPrice"] = 65000,
+        ["Force"] = 1.2,
+        ["Order"] = 3,
+        ["DisplayName"] = "Track Brakes"
+    },
+    ["Race"] = {
+        ["Category"] = "Brakes",
+        ["Rarity"] = "Epic",
+        ["PremiumPrice"] = 75,
+        ["CashPrice"] = 147000,
+        ["Force"] = 1.3,
+        ["Order"] = 4,
+        ["DisplayName"] = "Race Brakes"
+    },
+    ["Stock"] = {["Order"] = 1, ["Force"] = 1, ["Category"] = "Brakes", ["DisplayName"] = "Stock Brakes"},
+    ["Sport"] = {
+        ["Rarity"] = "Uncommon",
+        ["Category"] = "Brakes",
+        ["CashPrice"] = 33000,
+        ["Force"] = 1.1,
+        ["Order"] = 2,
+        ["DisplayName"] = "Sport Brakes"
+    }
+}
