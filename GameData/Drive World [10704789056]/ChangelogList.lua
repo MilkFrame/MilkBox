253bdebc1b2afed16fffa39983879402d770a354
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-31 22:02:57 +1000

    Create ChangelogList.lua

---
+++ GameData/Drive World [10704789056]/ChangelogList.lua
@@ -0,0 +1,483 @@
+data = {
+    ["Versions"] = {
+        {
+            ["Cars"] = {"Butler", "Crown", "KITE", "ZephyrEvo"},
+            ["Notes"] = {
+                "Boost races/events with gold for bigger payouts!",
+                "New Gamepass: Cinema Classics!",
+                "New limited Weekly Car racecar",
+                "New Weekly Missions and Rewards",
+                "Bugfixes and Optimizations"
+            }
+        },
+        {
+            ["Cars"] = {"BuckRally", "PioneerSummit"},
+            ["Notes"] = {
+                "UI Facelift!",
+                "2 New Cars",
+                "Custom Leaderboard",
+                "Visible Hands in First Person",
+                "New Weekly Missions and Rewards."
+            }
+        },
+        {
+            ["Cars"] = {"CutterXtreme", "ZeroOneR"},
+            ["Notes"] = {
+                "New Jump Ramp Traps Around GV!",
+                "Spoiler and Brakelight Trails!",
+                "New VFX for Nitrous and Car Landings.",
+                "New Weekly Missions and Rewards!",
+                "Optimizations & Bugfixes."
+            }
+        },
+        {
+            ["Cars"] = {"CometEX", "RogueS"},
+            ["Notes"] = {
+                "Car Power Revamp!",
+                "New/Changes to car mods",
+                "EV upgrades",
+                "Tires are now cosmetic",
+                "and much more!"
+            }
+        },
+        {
+            ["Cars"] = {"Cybervolo", "JupiterGTR", "Pioneer"},
+            ["Notes"] = {
+                "Paint your garages!",
+                "Invite people in server to your garage.",
+                "New Limited Time Cyberpunk Bundle.",
+                "Sunsets!",
+                "New Pioneer coupe in Dealership.",
+                "New Weekly Missions and Rewards."
+            }
+        },
+        {
+            ["Cars"] = {"Razor2Xtreme", "Haroku"},
+            ["Notes"] = {
+                "Season 1 of Racing Clubs Start!",
+                "Compete in a club to earn exclusive rewards!",
+                "Season 1 runs from May 31st to June 28th!",
+                "Updated username UI above players",
+                "Car lifts work in garages!",
+                "New Weekly Missions and Rewards."
+            }
+        },
+        {
+            ["Cars"] = {"Diavolo", "Buggy", "RossoWidebody"},
+            ["Notes"] = {
+                "Valentine's Festival Week 2!",
+                "New Rewards Available in Shop!",
+                "New Race: City Escape",
+                "Bugfixes and Optimizations",
+                "New Weekly Missions and Rewards"
+            }
+        },
+        {
+            ["Cars"] = {"RazorUTEEvo", "Condor"},
+            ["Notes"] = {
+                "Revamp Challenges System!",
+                "Earn Gold from Challenges!",
+                "New Condor sports car in dealership.",
+                "New race: Waterfall Pass.",
+                "New Weekly Missions and Rewards."
+            }
+        },
+        {
+            ["Cars"] = {"KronosMason", "AllegroEvo"},
+            ["Notes"] = {
+                "Racing Clubs Part 1 of 3!",
+                "Create and invite people to racing clubs!",
+                "New Premium Dealership supercar.",
+                "Fix Shadow Racing balancing.",
+                "New Weekly Missions and Rewards."
+            }
+        },
+        {
+            ["Cars"] = {"Nimbus936", "RazorXtreme", "RacePal"},
+            ["Notes"] = {
+                "Golden Valley GP UGC Competition!",
+                "Reach top 200, top 50, & #1 for free UGC!",
+                "2 new cars & Race Pal returns!",
+                "40% discount on Mixture!",
+                "Improved night lighting!",
+                "Optimizations, improvements, & bug fixes"
+            }
+        },
+        {
+            ["Cars"] = {"CycloneGT", "V10RSWidebody"},
+            ["Notes"] = {
+                "The Garage Update! Walk around and upgrade garages!",
+                "Purchase several different garages.",
+                "Invite friends and hangout.",
+                "New CycloneGT in Dealership!",
+                "New Weekly Missions and Rewards."
+            }
+        },
+        {
+            ["Cars"] = {"Ocelot", "Saxxon"},
+            ["Notes"] = {
+                "Valentine's Event 2024 Countdown!",
+                "New Ocelot sports car in Dealership",
+                "New Weekly Missions and Rewards",
+                "New Sound Effects for Destructibles",
+                "Optimizations, improvements, & bug fixes."
+            }
+        },
+        {
+            ["Cars"] = {"HorizonXtreme"},
+            ["Notes"] = {
+                "Auction penalties for leaving early!",
+                "Jump to top of auction queue with Gold!",
+                "Easter 2024 removed",
+                "New Weekly Missions and Rewards"
+            }
+        },
+        {
+            ["Cars"] = {"Blade", "NightlineS34Xtreme", "Mori15Xtreme"},
+            ["Notes"] = {
+                "Weekly global race leaderboards!",
+                "Challenge top racers with Shadow Racers!",
+                "Introducing racer profiles!",
+                "Flex your racing dedication w/Driver Level!"
+            }
+        },
+        {
+            ["Cars"] = {"FutureVolition", "PaletoSport", "Solar"},
+            ["Notes"] = {
+                "Race Leaderboard Placement Rewards",
+                "Spring-time Map",
+                "Better Collision Physics",
+                "2 New Dealership Cars",
+                "New Weekly Missions and Rewards",
+                "New Roblox Badges"
+            }
+        },
+        {
+            ["Cars"] = {"Vanaheim", "NexusXtreme", "TorakkuB"},
+            ["Notes"] = {
+                "Torakku Xtreme back in weekly challenge!",
+                "+1 new wrap, +4 nitrous & +1 backfire effects",
+                "Instrument clusters on 15+ cars",
+                "Stay tuned for Shadow Racers update!",
+                "Interface buttons appearance refreshed",
+                "Default Wood & Metal materials added"
+            }
+        },
+        {
+            ["Cars"] = {"LaRazza", "LRXK", "StallionRace"},
+            ["Notes"] = {
+                "Cardex - see how many cars you've collected",
+                "Remodel LaRazza and LRXK",
+                "Stat adjustments for 5 cars",
+                "New notification UI for quest and unlocks",
+                "New Weekly Missions and Rewards."
+            }
+        },
+        {
+            ["Cars"] = {"ZeroOneR", "Hellion", "RasantBlack"},
+            ["Notes"] = {
+                "+2 new cars",
+                "New Gatling Run & City Dragstrip!",
+                "New Starter Pack!",
+                "Performance & bug fixes"
+            }
+        },
+        {
+            ["Cars"] = {"CamoStock", "Tiger1", "Ranger", "Exotica", "WidebodyExotica"},
+            ["Notes"] = {
+                "UGC Event Countdown!",
+                "Golden Valley renovations",
+                "5 new cars",
+                "Weather is added back",
+                "25% off all Game Passes!"
+            }
+        },
+        {
+            ["Cars"] = {"Edler", "Nexus2", "Nimbus933Trek", "NightlineS34Xtreme"},
+            ["Notes"] = {
+                "It's the 3rd and final week of the Winter Rally 2023!",
+                "Donate in the event to unlock the Edler",
+                "New winter rally race",
+                "New holiday job event ends this week for the Holiday Inferno"
+            }
+        },
+        {
+            ["Cars"] = {"VultureVT", "Trident", "CycloneGTRS", "Quinten"},
+            ["Notes"] = {
+                "Season 1 Ends",
+                "1 new premium dealership track car",
+                "1 new dealership coupe",
+                "Stat adjustments for 7 cars",
+                "New Weekly Missions and Rewards."
+            }
+        },
+        {
+            ["Cars"] = {"Super5Xtreme", "Sideswipe", "Mori15Xtreme"},
+            ["Notes"] = {"+2 races", "+2 cars", [[Use code "BIG_W" for a surprise]], "New Challenge races in Japan"}
+        },
+        {["Cars"] = {"StallionConvert", "StallionPatrol", "HooliganNimbus"}, ["Notes"] = {"+2 cars", "Bug fixes"}},
+        {
+            ["Cars"] = {"StreetEagle", "Nimbus933Xtreme"},
+            ["Notes"] = {
+                "Low end car sounds improved",
+                "Map optimizations",
+                "New weekly challenge quests!",
+                "Stay tuned for Fall update & map expansion!"
+            }
+        },
+        {
+            ["Cars"] = {"Quicksilver", "PumpkinPal", "CamoStock"},
+            ["Notes"] = {"Halloween Part 2!", "+2 new cars", "More spooky accessories", "Pumpkin Pal", "Bug fixes"}
+        },
+        {
+            ["Cars"] = {"Cirrus441Rally", "QuintenRally", "PioneerRally"},
+            ["Notes"] = {
+                "Race in the Spring Rally!",
+                "Earn UGC trohpies for being in the top 200!",
+                "New Limited Time Cirrus 441 Rally bundle.",
+                "Purchase new DW-themed UGC",
+                "Free UGC in the Weekly quest!",
+                "New Weekly Missions and Rewards."
+            }
+        },
+        {
+            ["Cars"] = {"Kronos", "Odin6x6", "ApexGT", "Ringo"},
+            ["Notes"] = {
+                "New Redwood Forest and Coastal Highway in Golden Valley!",
+                "Nightline S35 Xtreme returns!",
+                "New weekly quests",
+                "+3 new races!",
+                "+4 new cars!"
+            }
+        },
+        {
+            ["Cars"] = {"ApexXtreme", "Hooligan"},
+            ["Notes"] = {
+                "Apex Xtreme widebody car!",
+                "Hooligan is back!",
+                "New Boulevard Circuit race",
+                "Performance and bug fixes",
+                "THANKS FOR 100 MILLION VISITS!"
+            }
+        },
+        {
+            ["Cars"] = {"Chicane", "Voyager", "Crest", "DragRanger", "RatRod"},
+            ["Notes"] = {
+                "Drag Ranger weekly challenge! Rat Rod returns!",
+                "Police Cap UGC for first 2,500 Police Pass buyers!",
+                "3 new police cars",
+                "New light bar accessories for all cars",
+                "New Battenburg wrap",
+                "New Police station in Golden Valley!",
+                "New Police Chase race"
+            }
+        },
+        {
+            ["Cars"] = {"Solace", "Cutter", "HooliganCyber"},
+            ["Notes"] = {
+                "New Mini-Games Queue rotating with Race Queue!",
+                "Particpate in Smash Zone and Jump Traps Mini-Games!",
+                "New Premium Shop Car",
+                "New Dealership Car",
+                "New Race: Dusty Drop",
+                "New Weekly Missions and Rewards"
+            }
+        },
+        {
+            ["Cars"] = {"Guzzler3000", "CenturyX", "PonyS5"},
+            ["Notes"] = {
+                "Roblox VIP server support",
+                "PlayStation Support!",
+                "+3 new cars",
+                "New Asphalt Outlaws & The Top races",
+                "Bug fixes"
+            }
+        },
+        {
+            ["Cars"] = {"BloxOne", "MixtureClassic"},
+            ["Notes"] = {
+                "Two new 'classic' cars for a limited time!",
+                "New Weekly Missions and Rewards.",
+                "Racing Clubs Season 1 begins May 31st."
+            }
+        },
+        {
+            ["Cars"] = {"ProjectRasant", "Bloxymobile"},
+            ["Notes"] = {
+                "New premium dealership hyper car!",
+                "New customizable horn feature",
+                "Customize headlight and brake light colors",
+                "Find Rims easier with the seach menu",
+                "New Weekly Missions and Rewards."
+            }
+        },
+        {
+            ["Cars"] = {"ZetaRoadster", "RotaryEvo", "Coach", "SideswipeX", "ApexGTR"},
+            ["Notes"] = {"Halloween has arrived!", "+5 new cars", "New spooky accessories", "New music", "Bug fixes"}
+        },
+        {
+            ["Cars"] = {"Osiris", "Vern", "Frontier"},
+            ["Notes"] = {"Season 1: Week 2", "Daily and Weekly Quests Reset", "New LIMITED TIME hyper car", "NEW CODE!"}
+        },
+        {
+            ["Cars"] = {"Avenger7400", "Razor", "Rogue"},
+            ["Notes"] = {
+                "New Single Player Shadow Racing!",
+                "+3 new cars",
+                "Say goodbye to Halloween :(",
+                "Performance fixes"
+            }
+        },
+        {
+            ["Cars"] = {"Badger", "SummitBeast", "Horizon"},
+            ["Notes"] = {
+                "THE HUNT: FIRST EDITION!",
+                "Complete the event to earn a limited car!",
+                "1 New Dealership Car",
+                "New limited Weekly Car",
+                "New Weekly Missions and Rewards"
+            }
+        },
+        {
+            ["Cars"] = {"DiavoloEvo", "HorizonCasanova", "SideswipeRT", "TrexBeast"},
+            ["Notes"] = {
+                "Valentine's Festival Week 1!",
+                "Earn Chocolates to Unlock Rewards",
+                "New Valentine's Themed Races",
+                "New Weekly Missions and Rewards"
+            }
+        },
+        {
+            ["Cars"] = {"RacePal", "BlipGT"},
+            ["Notes"] = {
+                "New RacePal weekly challenge car!",
+                "Blip GT is back!",
+                "Map performance improvements",
+                "Cleaned up shop and spawn menu details"
+            }
+        },
+        {
+            ["Cars"] = {"Ardente", "SolarXtreme", "PresentPal", "ApexXtreme"},
+            ["Notes"] = {
+                "New Ardente now in the dealership!",
+                "Winter Rally event has now ended.",
+                "New VFX for speed lines, brakelights, and spoiler vortex.",
+                "Claim Present Pal for a limited time.",
+                "New VFX for certain cars(support for all soon)."
+            }
+        },
+        {
+            ["Cars"] = {"ContenderHT", "Tiger1"},
+            ["Notes"] = {
+                "New Auction World",
+                "Sell or bid on rare and limited cars",
+                "Take a cruise around the new AW track",
+                "New weekly missions and rewards"
+            }
+        },
+        {
+            ["Cars"] = {"ScimitarEvo", "Bavette", "Galano"},
+            ["Notes"] = {
+                "Even more Easter rewards in Weekly Quests!",
+                "New limited-time bundle",
+                "Dealership UI changes",
+                "Purchase top tier upgrades for Gold!",
+                "New Weekly Missions and Rewards"
+            }
+        },
+        {
+            ["Cars"] = {"ArdenteGTR", "Spectra", "Nimbus933Xtreme"},
+            ["Notes"] = {
+                "Winter Countdown!",
+                "Snow has fallen in Golden Valley.",
+                "Top tier car balancing.",
+                "New sound sliders in settings.",
+                "Optimizations, improvements, & bug fixes."
+            }
+        },
+        {
+            ["Cars"] = {"Patriot", "CamoFreedom"},
+            ["Notes"] = {
+                "Build-A-Car Event",
+                "Build the Patriot Monster Truck",
+                "Freedom-based Weekly Rewards",
+                "20% Off Sale on All Cash and Gold",
+                "New Like Goal Reward Code"
+            }
+        },
+        {
+            ["Cars"] = {"Modena", "Bullet", "Gridiron", "Kingfisher", "Grant", "NexusXtreme", "FemarisWidebody"},
+            ["Notes"] = {
+                "Winter Rally 2023 has begun!!!",
+                "Donate in the event to win exclusive rewards",
+                "Reach the community donation milestone to unlock event hypercars",
+                "Gift new event cars and gamepasses at a discounted price",
+                "Bug fixes and improvements"
+            }
+        },
+        {
+            ["Cars"] = {"Luxor", "NightlineS35Xtreme"},
+            ["Notes"] = {
+                "Black Friday Sale on Car packs!",
+                "New weekly car!",
+                "Event winners will get trophy soon!",
+                "Optimizations, improvements, & bug fixes"
+            }
+        },
+        {
+            ["Cars"] = {"Prince", "EdlerRoadster", "OcelotEvo", "Buzzard"},
+            ["Notes"] = {
+                "New Premium Dealership!",
+                "New Gold Premium Currency!",
+                "New Race: Skybound Plummet",
+                "New Mission and Challenges Types",
+                "New Weekly Missions and Rewards"
+            }
+        },
+        {
+            ["Cars"] = {"StingerGTR", "Vessel", "VandelWidebody", "Trex"},
+            ["Notes"] = {
+                "Reworked delivery jobs!",
+                "Delivery jobs now have more guarantee pay for hard work!",
+                "1 New Premium Shop Car",
+                "2 New Dealership Car",
+                "New arrow above cars to help you navigate",
+                "New Weekly Missions and Rewards"
+            }
+        },
+        {
+            ["Cars"] = {"ZeusEvo", "Scimitar", "Viscount"},
+            ["Notes"] = {
+                "Checkout weekly quests for easter rewards!",
+                "New Easter destructibles",
+                "New limited-time bundle",
+                "New Weekly Missions and Rewards",
+                "Bugfixes and Optimizations"
+            }
+        },
+        {
+            ["Cars"] = {"HolidayInferno", "Regent", "HammerEV6x6", "RallyHunter"},
+            ["Notes"] = {
+                "Week 2 of the Winter Rally 2023!",
+                "Donate in the event to unlock the Regent",
+                "New gifting options",
+                "New holiday job event to win the Holiday Inferno",
+                "Bug fixes and improvements"
+            }
+        },
+        {
+            ["Cars"] = {"Vivec", "Stinger"},
+            ["Notes"] = {
+                "Car Season 1",
+                "New Questing System",
+                "Play Each Week to Unlock The Monthly Car",
+                "Pin Quests on Screen",
+                "New Dealership and Spawn"
+            }
+        },
+        {
+            ["Cars"] = {"HooliganNimbus", "CasperTRC"},
+            ["Notes"] = {"Limited Time Racing Helmet UGC!", "Bug fixes", "Police Update Coming Soon"}
+        }
+    }
+}
