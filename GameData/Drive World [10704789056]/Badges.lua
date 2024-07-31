8c4ae59b2174259f178c60507961aa9c9aa4280d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-31 22:03:08 +1000

    Create Badges.lua

---
+++ GameData/Drive World [10704789056]/Badges.lua
@@ -0,0 +1,19 @@
+data = {
+    ["win50Races"] = {["Id"] = 1071540833275320, ["Goal"] = 50, ["Stat"] = "Wins"},
+    ["earn5Mcash"] = {["Id"] = 1646885161211924, ["Goal"] = 5000000, ["Stat"] = "PlayerWallet"},
+    ["earn10Mcash"] = {["Id"] = 3689680336619217, ["Goal"] = 10000000, ["Stat"] = "PlayerWallet"},
+    ["complete100Jobs"] = {["Id"] = 3480322615038816, ["Goal"] = 100, ["Stat"] = "JobsCompleted"},
+    ["win25Races"] = {["Id"] = 544881738477538, ["Goal"] = 25, ["Stat"] = "Wins"},
+    ["patriot2024"] = {["Id"] = 3059164459297447},
+    ["earn1Mcash"] = {["Id"] = 1745855508941283, ["Goal"] = 1000000, ["Stat"] = "PlayerWallet"},
+    ["theHunt2024"] = {["Id"] = 984962670920392},
+    ["drive10000mi"] = {["Id"] = 1741708211947064, ["Goal"] = 10000, ["Stat"] = "MilesDriven"},
+    ["purchase5Cars"] = {["Id"] = 143188166337551, ["Goal"] = 5, ["Stat"] = "CarsPurchased"},
+    ["complete50Jobs"] = {["Id"] = 1125054717635938, ["Goal"] = 50, ["Stat"] = "JobsCompleted"},
+    ["drive5000mi"] = {["Id"] = 1185364550787786, ["Goal"] = 5000, ["Stat"] = "MilesDriven"},
+    ["purchase45Cars"] = {["Id"] = 1525289083320971, ["Goal"] = 45, ["Stat"] = "CarsPurchased"},
+    ["purchase20Cars"] = {["Id"] = 3772379938174647, ["Goal"] = 20, ["Stat"] = "CarsPurchased"},
+    ["win100Races"] = {["Id"] = 1127153771894232, ["Goal"] = 100, ["Stat"] = "Wins"},
+    ["complete250Jobs"] = {["Id"] = 2249957312731405, ["Goal"] = 250, ["Stat"] = "JobsCompleted"},
+    ["drive1000mi"] = {["Id"] = 2939458636670655, ["Goal"] = 1000, ["Stat"] = "MilesDriven"}
+}
