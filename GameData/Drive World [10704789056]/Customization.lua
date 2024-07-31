6ef5ccc39c7d92732eba5f68d975a4981267137e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-31 22:01:33 +1000

    Create Customization.lua

---
+++ GameData/Drive World [10704789056]/Customization.lua
@@ -0,0 +1,308 @@
+data = {
+    ["Paints"] = {
+        ["HotPink"] = {
+            ["Color"] = Color3.fromRGB(255, 46, 228),
+            ["Material"] = "Glass",
+            ["Reflectance"] = 0,
+            ["DisplayName"] = "Hot Pink"
+        },
+        ["NitrousBlue"] = {
+            ["Color"] = Color3.fromRGB(9, 137, 207),
+            ["Material"] = "SmoothPlastic",
+            ["DisplayName"] = "Nitrous Blue"
+        },
+        ["Silver"] = {["Color"] = Color3.fromRGB(148, 165, 171), ["DisplayName"] = "Silver"},
+        ["CrimsonRed"] = {
+            ["Color"] = Color3.fromRGB(147, 24, 24),
+            ["Reflectance"] = 0.05,
+            ["DisplayName"] = "Crimson Red"
+        },
+        ["GreenHell"] = {["Color"] = Color3.fromRGB(53, 94, 16), ["DisplayName"] = "Magno Green"},
+        ["RubyRed"] = {["Color"] = Color3.fromRGB(131, 45, 72), ["Reflectance"] = 0.05, ["DisplayName"] = "Ruby Red"},
+        ["CompetitionYellow"] = {["Color"] = Color3.fromRGB(255, 216, 60), ["DisplayName"] = "Competition Yellow"},
+        ["CornflowerBlue"] = {
+            ["Reflectance"] = 0.05,
+            ["Color"] = Color3.fromRGB(11, 84, 172),
+            ["Material"] = "Glass",
+            ["Rarity"] = "Common",
+            ["DisplayName"] = "Cornflower Blue"
+        },
+        ["SlimeGreen"] = {
+            ["Reflectance"] = 0,
+            ["Color"] = Color3.fromRGB(54, 255, 44),
+            ["Material"] = "Glass",
+            ["Rarity"] = "Common",
+            ["DisplayName"] = "Slime Green"
+        },
+        ["RacingYellow"] = {
+            ["Color"] = Color3.fromRGB(248, 205, 2),
+            ["IsDefault"] = false,
+            ["DisplayName"] = "Racing Yellow"
+        },
+        ["YellowAccelerate"] = {["Color"] = Color3.fromRGB(177, 197, 0), ["DisplayName"] = "Yellow Accelerate"},
+        ["Black"] = {["Color"] = Color3.fromRGB(0, 0, 0), ["IsDefault"] = true, ["DisplayName"] = "Black"},
+        ["SharkBlue"] = {
+            ["Color"] = Color3.fromRGB(89, 125, 243),
+            ["IsDefault"] = false,
+            ["DisplayName"] = "Shark Blue"
+        },
+        ["LudusBlue"] = {
+            ["Color"] = Color3.fromRGB(71, 108, 189),
+            ["IsDefault"] = false,
+            ["DisplayName"] = "Ludus Blue"
+        },
+        ["LavaOrange"] = {
+            ["Color"] = Color3.fromRGB(247, 104, 6),
+            ["IsDefault"] = false,
+            ["DisplayName"] = "Lava Orange"
+        },
+        ["RossoItalia"] = {
+            ["Reflectance"] = 0.1,
+            ["Color"] = Color3.fromRGB(193, 1, 0),
+            ["Material"] = "Glass",
+            ["Rarity"] = "Common",
+            ["DisplayName"] = "Rosso Italia"
+        },
+        ["VolcanoYellow"] = {
+            ["Color"] = Color3.fromRGB(252, 185, 48),
+            ["IsDefault"] = false,
+            ["DisplayName"] = "Volcano Yellow"
+        },
+        ["Ultraviolet"] = {
+            ["Color"] = Color3.fromRGB(42, 32, 59),
+            ["Reflectance"] = 0.05,
+            ["DisplayName"] = "Ultraviolet"
+        },
+        ["BrightOrange"] = {
+            ["Color"] = Color3.fromRGB(213, 115, 61),
+            ["Material"] = "Glass",
+            ["Reflectance"] = 0,
+            ["DisplayName"] = "Bright Orange"
+        },
+        ["OpulentBlueMetallic"] = {
+            ["Color"] = Color3.fromRGB(53, 42, 156),
+            ["IsDefault"] = false,
+            ["DisplayName"] = "Opulent Blue Metallic"
+        },
+        ["Teal"] = {["Color"] = Color3.fromRGB(104, 130, 145), ["DisplayName"] = "Teal"},
+        ["MatteBlack"] = {
+            ["Reflectance"] = 0,
+            ["Color"] = Color3.fromRGB(50, 50, 50),
+            ["Material"] = "SmoothPlastic",
+            ["Rarity"] = "Common",
+            ["DisplayName"] = "Matte Black"
+        },
+        ["IridiumGrey"] = {
+            ["Color"] = Color3.fromRGB(163, 162, 165),
+            ["Material"] = "Plastic",
+            ["Reflectance"] = 0,
+            ["DisplayName"] = "Iridium Grey"
+        },
+        ["ViolaMetallic"] = {
+            ["Reflectance"] = 0,
+            ["Color"] = Color3.fromRGB(61, 29, 77),
+            ["Material"] = "Glass",
+            ["Rarity"] = "Common",
+            ["DisplayName"] = "Viola Metallic"
+        },
+        ["GialloMidas"] = {
+            ["Color"] = Color3.fromRGB(252, 233, 3),
+            ["Reflectance"] = 0.05,
+            ["DisplayName"] = "Giallo Midas"
+        },
+        ["AppleRed"] = {["Color"] = Color3.fromRGB(142, 36, 34), ["IsDefault"] = false, ["DisplayName"] = "Apple Red"},
+        ["VerdeMantis"] = {["Color"] = Color3.fromRGB(125, 194, 59), ["DisplayName"] = "Verde Mantis"},
+        ["NardoGrey"] = {
+            ["Color"] = Color3.fromRGB(104, 106, 108),
+            ["Material"] = "Plastic",
+            ["Reflectance"] = 0,
+            ["DisplayName"] = "Nardo Grey"
+        },
+        ["AuroraBlue"] = {
+            ["Color"] = Color3.fromRGB(40, 22, 111),
+            ["IsDefault"] = false,
+            ["DisplayName"] = "Aurora Blue"
+        },
+        ["AmplifyOrange"] = {["Color"] = Color3.fromRGB(220, 92, 0), ["DisplayName"] = "Orange Amplify"},
+        ["White"] = {["Color"] = Color3.fromRGB(255, 255, 255), ["IsDefault"] = true, ["DisplayName"] = "Pure White"}
+    },
+    ["Materials"] = {
+        ["Wood"] = {
+            ["PaintMultiplierCost"] = 1.5,
+            ["Preview"] = "rbxassetid://14195038100",
+            ["CashPrice"] = 3000,
+            ["Material"] = "Wood",
+            ["SupportsReflectance"] = false,
+            ["DisplayName"] = "Wood"
+        },
+        ["Matte"] = {
+            ["PaintMultiplierCost"] = 2,
+            ["BaseMaterial"] = "Plastic",
+            ["MaterialVariant"] = "Matte",
+            ["CashPrice"] = 1000,
+            ["Preview"] = "rbxassetid://14229795111",
+            ["SupportsReflectance"] = false,
+            ["DisplayName"] = "Matte"
+        },
+        ["MetallicPaint"] = {
+            ["PaintMultiplierCost"] = 2,
+            ["BaseMaterial"] = "Plastic",
+            ["MaterialVariant"] = "MetallicPaint",
+            ["CashPrice"] = 2000,
+            ["Preview"] = "rbxassetid://12807435739",
+            ["SupportsReflectance"] = false,
+            ["DisplayName"] = "Metallic Paint"
+        },
+        ["Carpet"] = {
+            ["PaintMultiplierCost"] = 1.2,
+            ["BaseMaterial"] = "Fabric",
+            ["MaterialVariant"] = "Carpet",
+            ["CashPrice"] = 2000,
+            ["Preview"] = "rbxassetid://14229801959",
+            ["SupportsReflectance"] = false,
+            ["DisplayName"] = "Carpet"
+        },
+        ["RoughPlastic"] = {
+            ["PaintMultiplierCost"] = 1.5,
+            ["BaseMaterial"] = "Plastic",
+            ["MaterialVariant"] = "RoughPlastic",
+            ["CashPrice"] = 800,
+            ["Preview"] = "rbxassetid://14229795111",
+            ["SupportsReflectance"] = false,
+            ["DisplayName"] = "Rough Plastic"
+        },
+        ["CorrodedMetal"] = {
+            ["PaintMultiplierCost"] = 1.2,
+            ["Preview"] = "rbxassetid://12807435547",
+            ["CashPrice"] = 100,
+            ["Material"] = "CorrodedMetal",
+            ["SupportsReflectance"] = false,
+            ["DisplayName"] = "Rust"
+        },
+        ["IntakeManifold"] = {
+            ["PaintMultiplierCost"] = 1.5,
+            ["BaseMaterial"] = "Metal",
+            ["MaterialVariant"] = "IntakeManifold",
+            ["CashPrice"] = 800,
+            ["Preview"] = "rbxassetid://12807435879",
+            ["SupportsReflectance"] = false,
+            ["DisplayName"] = "Intake Manifold"
+        },
+        ["QuiltedLeather"] = {
+            ["PaintMultiplierCost"] = 3,
+            ["BaseMaterial"] = "Fabric",
+            ["MaterialVariant"] = "QuiltedLeather",
+            ["CashPrice"] = 3000,
+            ["Preview"] = "rbxassetid://15055463442",
+            ["SupportsReflectance"] = false,
+            ["DisplayName"] = "Quilted Leather"
+        },
+        ["Gloss"] = {
+            ["PaintMultiplierCost"] = 1,
+            ["BaseMaterial"] = "Plastic",
+            ["MaterialVariant"] = "Gloss",
+            ["CashPrice"] = 1000,
+            ["Preview"] = "rbxassetid://14229815042",
+            ["SupportsReflectance"] = false,
+            ["DisplayName"] = "Gloss"
+        },
+        ["Plastic"] = {
+            ["PaintMultiplierCost"] = 1.5,
+            ["Disabled"] = true,
+            ["Preview"] = "rbxassetid://14229795111",
+            ["CashPrice"] = 800,
+            ["Material"] = "Plastic",
+            ["SupportsReflectance"] = true,
+            ["DisplayName"] = "Plastic"
+        },
+        ["CarbonFiberMatte"] = {
+            ["PaintMultiplierCost"] = 3,
+            ["BaseMaterial"] = "Plastic",
+            ["MaterialVariant"] = "CarbonFiberMatte",
+            ["CashPrice"] = 4000,
+            ["Preview"] = "rbxassetid://14229786887",
+            ["SupportsReflectance"] = false,
+            ["DisplayName"] = "Matte Carbon Fiber"
+        },
+        ["Leather"] = {
+            ["PaintMultiplierCost"] = 2,
+            ["BaseMaterial"] = "Plastic",
+            ["MaterialVariant"] = "Leather",
+            ["CashPrice"] = 3000,
+            ["Preview"] = "rbxassetid://14229795111",
+            ["SupportsReflectance"] = false,
+            ["DisplayName"] = "Leather"
+        },
+        ["CarbonFiber"] = {
+            ["PaintMultiplierCost"] = 3,
+            ["BaseMaterial"] = "Plastic",
+            ["MaterialVariant"] = "CarbonFiber",
+            ["CashPrice"] = 5000,
+            ["Preview"] = "rbxassetid://14229786887",
+            ["SupportsReflectance"] = false,
+            ["DisplayName"] = "Glossy Carbon Fiber"
+        },
+        ["Alcantara"] = {
+            ["PaintMultiplierCost"] = 2,
+            ["BaseMaterial"] = "Fabric",
+            ["MaterialVariant"] = "Alcantara",
+            ["CashPrice"] = 3000,
+            ["Preview"] = "rbxassetid://14229795111",
+            ["SupportsReflectance"] = false,
+            ["DisplayName"] = "Alcantara"
+        },
+        ["Sand"] = {
+            ["PaintMultiplierCost"] = 1.2,
+            ["Preview"] = "rbxassetid://12807435611",
+            ["CashPrice"] = 800,
+            ["Material"] = "Sand",
+            ["SupportsReflectance"] = true,
+            ["DisplayName"] = "Sand"
+        },
+        ["SmoothPlastic"] = {
+            ["PaintMultiplierCost"] = 2,
+            ["Preview"] = "rbxassetid://14229815042",
+            ["CashPrice"] = 900,
+            ["Material"] = "SmoothPlastic",
+            ["SupportsReflectance"] = true,
+            ["DisplayName"] = "Smooth Plastic"
+        },
+        ["Glass"] = {
+            ["PaintMultiplierCost"] = 2,
+            ["Preview"] = "rbxassetid://14229820971",
+            ["CashPrice"] = 2000,
+            ["Material"] = "Glass",
+            ["SupportsReflectance"] = true,
+            ["DisplayName"] = "Glass"
+        },
+        ["Metal"] = {
+            ["PaintMultiplierCost"] = 2,
+            ["Preview"] = "rbxassetid://12807435739",
+            ["CashPrice"] = 2500,
+            ["Material"] = "Metal",
+            ["SupportsReflectance"] = false,
+            ["DisplayName"] = "Bare Metal"
+        }
+    },
+    ["LightColors"] = {
+        ["HotPink"] = {["Color"] = Color3.fromRGB(255, 0, 136), ["PremiumPrice"] = 15},
+        ["NeonBlue"] = {["Color"] = Color3.fromRGB(42, 23, 255), ["PremiumPrice"] = 10},
+        ["Purple"] = {["Color"] = Color3.fromRGB(123, 6, 225), ["CashPrice"] = 40000},
+        ["DarkBlue"] = {["Color"] = Color3.fromRGB(0, 41, 129), ["PremiumPrice"] = 10},
+        ["YellowOrange"] = {["Color"] = Color3.fromRGB(229, 134, 0), ["PremiumPrice"] = 10},
+        ["BrightRed"] = {["Color"] = Color3.fromRGB(255, 0, 4), ["CashPrice"] = 20000},
+        ["OffWhite"] = {["Color"] = Color3.fromRGB(255, 246, 198), ["CashPrice"] = 20000},
+        ["SkyBlue"] = {["Color"] = Color3.fromRGB(147, 208, 255), ["PremiumPrice"] = 10},
+        ["Orange"] = {["Color"] = Color3.fromRGB(238, 92, 13), ["CashPrice"] = 40000},
+        ["Red"] = {["Color"] = Color3.fromRGB(176, 0, 33), ["CashPrice"] = 35000},
+        ["Pink"] = {["Color"] = Color3.fromRGB(234, 52, 195), ["PremiumPrice"] = 15},
+        ["LightBlue"] = {["Color"] = Color3.fromRGB(0, 100, 176), ["CashPrice"] = 50000},
+        ["Crimson"] = {["Color"] = Color3.fromRGB(175, 0, 3), ["CashPrice"] = 50000},
+        ["Black"] = {["Color"] = Color3.fromRGB(0, 0, 0), ["PremiumPrice"] = 20},
+        ["Green"] = {["Color"] = Color3.fromRGB(0, 170, 54), ["CashPrice"] = 30000},
+        ["ToxicGreen"] = {["Color"] = Color3.fromRGB(132, 255, 0), ["PremiumPrice"] = 10},
+        ["Yellow"] = {["Color"] = Color3.fromRGB(255, 205, 53), ["CashPrice"] = 40000},
+        ["BrightYellow"] = {["Color"] = Color3.fromRGB(255, 248, 30), ["PremiumPrice"] = 10},
+        ["White"] = {["Color"] = Color3.fromRGB(255, 255, 255), ["CashPrice"] = 1000}
+    }
+}
