54358131a068fc87c4dc5c26518ff9c20b5f6cc4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-31 22:02:40 +1000

    Create Constants.lua

---
+++ GameData/Drive World [10704789056]/Constants.lua
@@ -0,0 +1,233 @@
+data = {
+    ["GUILD_HALL_PRICE"] = 100,
+    ["MAX_BRAKE_FORCE"] = 39,
+    ["CLUB_UPDATE_ENABLED"] = true,
+    ["MULTIPLIER_PER_PURCHASE"] = 0.25,
+    ["MOTOR_SHARE"] = 0.45,
+    ["GROUP_RBXTHUMB"] = "rbxthumb://type=GroupIcon&id=%d&w=150&h=150",
+    ["GARAGE_COLOR_GROUP_PREFIX"] = "GarageColorGroup_",
+    ["CONTENT_CREATOR_RANK"] = 4,
+    ["DEFAULT_FRONTAL_AREA"] = 2.19,
+    ["COLORS"] = {
+        ["Blue"] = Color3.fromRGB(58, 209, 242),
+        ["Money"] = Color3.fromRGB(0, 216, 0),
+        ["Grey"] = Color3.fromRGB(126, 126, 126),
+        ["Purple"] = Color3.fromRGB(211, 65, 255),
+        ["Pink"] = Color3.fromRGB(255, 138, 179),
+        ["Red"] = Color3.fromRGB(207, 30, 20),
+        ["Green"] = Color3.fromRGB(117, 203, 81),
+        ["GreyDark"] = Color3.fromRGB(107, 107, 107),
+        ["LightGrey"] = Color3.fromRGB(209, 209, 209),
+        ["Orange"] = Color3.fromRGB(255, 195, 12),
+        ["White"] = Color3.fromRGB(255, 255, 255)
+    },
+    ["INTAKE_SHARE"] = 0.1,
+    ["BATTERY_PACK_SHARE"] = 0.45,
+    ["GROUP_ID"] = 4909558,
+    ["EVENT_END_DATE"] = 1708718400,
+    ["CAR_PART_SPAWNS"] = 5,
+    ["RACE_COMPLETION_XP"] = 20,
+    ["CURRENT_WEEK_NUM"] = 77,
+    ["CURRENT_CLUB_SEASON"] = 1,
+    ["DEFAULT_COLORS"] = {
+        ["Rims"] = {
+            Color3.fromRGB(17, 17, 17),
+            Color3.fromRGB(200, 201, 203),
+            Color3.fromRGB(104, 106, 108),
+            Color3.fromRGB(159, 120, 37),
+            Color3.fromRGB(131, 112, 91)
+        },
+        ["Body"] = {
+            Color3.fromRGB(200, 201, 203),
+            Color3.fromRGB(148, 165, 171),
+            Color3.fromRGB(104, 106, 108),
+            Color3.fromRGB(17, 17, 17),
+            Color3.fromRGB(252, 233, 3),
+            Color3.fromRGB(244, 34, 31),
+            Color3.fromRGB(89, 125, 243),
+            Color3.fromRGB(40, 22, 111),
+            Color3.fromRGB(125, 194, 59),
+            Color3.fromRGB(247, 104, 6)
+        }
+    },
+    ["FORCE_FTUX"] = false,
+    ["PAINT_COST"] = 200,
+    ["PERCENT_DAY"] = 0.9,
+    ["LIGHT_ENABLE_TIME"] = 17.5,
+    ["SEASON_RUNNING"] = false,
+    ["ROBUX_STRING_FORMAT"] = "%s",
+    ["HEADSHOT_THUMBNAIL_FORMAT"] = "rbxthumb://type=AvatarHeadShot&id=%d&w=150&h=150&filters=circular",
+    ["CURRENCIES"] = {
+        ["Wood"] = {
+            ["Name"] = "Wood",
+            ["WalletCap"] = 1600,
+            ["Color"] = Color3.fromRGB(255, 255, 255),
+            ["ConversionRatio"] = 0.33,
+            ["Icon"] = "rbxassetid://17095646589",
+            ["RewardIcon"] = "rbxassetid://17095646589"
+        },
+        ["PointsReward"] = {
+            ["Name"] = "Season Points",
+            ["DisplayFormat"] = "%s PTS",
+            ["Color"] = Color3.fromRGB(0, 0, 0),
+            ["Icon"] = "rbxassetid://18565453979",
+            ["RewardIcon"] = "rbxassetid://18565453979"
+        },
+        ["ClubPoints"] = {
+            ["Color"] = Color3.fromRGB(0, 0, 0),
+            ["Icon"] = "rbxassetid://17650604592",
+            ["Name"] = "Club Points"
+        },
+        ["ClubSponsors"] = {
+            ["Color"] = Color3.fromRGB(0, 0, 0),
+            ["Icon"] = "rbxassetid://17650604686",
+            ["Name"] = "Club Sponsors"
+        },
+        ["Steel"] = {
+            ["Name"] = "Steel",
+            ["WalletCap"] = 250,
+            ["Color"] = Color3.fromRGB(255, 255, 255),
+            ["ConversionRatio"] = 0.04,
+            ["Icon"] = "rbxassetid://17095646889",
+            ["RewardIcon"] = "rbxassetid://17095646889"
+        },
+        ["PremiumCurrency"] = {
+            ["Color"] = Color3.fromRGB(255, 186, 66),
+            ["RewardIcon"] = "rbxassetid://16481974553",
+            ["Icon"] = "rbxassetid://16480007726",
+            ["Name"] = "Gold"
+        },
+        ["EventCurrency"] = {["Emoji"] = "🍫", ["Icon"] = "rbxassetid://16260982012", ["Name"] = "Chocolate"},
+        ["Cash"] = {
+            ["Color"] = Color3.fromRGB(128, 216, 74),
+            ["RewardIcon"] = "rbxassetid://16481974731",
+            ["Icon"] = "rbxassetid://16479959415",
+            ["Name"] = "Cash"
+        }
+    },
+    ["ADMIN_RANK"] = 253,
+    ["KEYBINDS"] = {
+        ["Keyboard"] = {
+            ["LightBar"] = Enum.KeyCode.K,
+            ["OpenTuning"] = Enum.KeyCode.T,
+            ["ManualToggle"] = Enum.KeyCode.N,
+            ["Downshift"] = Enum.KeyCode.F,
+            ["Continue"] = Enum.KeyCode.Return,
+            ["Upshift"] = Enum.KeyCode.R,
+            ["Horn"] = Enum.KeyCode.H,
+            ["NitrousRight"] = Enum.KeyCode.RightShift,
+            ["Drift"] = Enum.KeyCode.Space,
+            ["ToggleSpoiler"] = Enum.KeyCode.B,
+            ["FlipCar"] = Enum.KeyCode.X,
+            ["Nitrous"] = Enum.KeyCode.LeftShift,
+            ["Interact"] = Enum.KeyCode.E,
+            ["ExitBack"] = Enum.KeyCode.E,
+            ["Headlights"] = Enum.KeyCode.L,
+            ["FirstPerson"] = Enum.KeyCode.V,
+            ["OpenMap"] = Enum.KeyCode.M
+        },
+        ["Gamepad"] = {
+            ["ControllerZoom"] = Enum.KeyCode.ButtonR3,
+            ["ToggleRevEngine"] = Enum.KeyCode.DPadRight,
+            ["Doors"] = Enum.KeyCode.DPadDown,
+            ["Downshift"] = Enum.KeyCode.ButtonL1,
+            ["Continue"] = Enum.KeyCode.ButtonX,
+            ["Upshift"] = Enum.KeyCode.ButtonR1,
+            ["Horn"] = Enum.KeyCode.ButtonL3,
+            ["SwapCar"] = Enum.KeyCode.ButtonY,
+            ["Drift"] = Enum.KeyCode.ButtonX,
+            ["Nitrous"] = Enum.KeyCode.ButtonA,
+            ["RaceTeleport"] = Enum.KeyCode.DPadLeft,
+            ["ToggleEngineOn"] = Enum.KeyCode.DPadLeft,
+            ["Interact"] = Enum.KeyCode.ButtonX,
+            ["ExitBack"] = Enum.KeyCode.ButtonB,
+            ["CarContextMenu"] = Enum.KeyCode.DPadRight,
+            ["Headlights"] = Enum.KeyCode.DPadUp,
+            ["Pin"] = Enum.KeyCode.ButtonL3,
+            ["FirstPerson"] = Enum.KeyCode.DPadDown
+        }
+    },
+    ["SUMMER_GAMES_ENABLED"] = false,
+    ["GARAGE_SPACING"] = 800,
+    ["SELL_CONVERSION"] = 0.3,
+    ["LIGHTING_TRANSITION_SPEED"] = 6,
+    ["GLOBAL_POWER_TARGET"] = 1500,
+    ["LICENSE_PLATE_CHAR_LIMIT"] = 7,
+    ["MAX_SHIFT_TIME"] = 0.05,
+    ["WEATHER_DURATION"] = 180,
+    ["LIGHT_DISABLE_TIME"] = 8,
+    ["MAX_NITROUS_BOOST"] = 175,
+    ["CAR_HITBOX_ANTI_SLIDE_PHYSICS"] = "1, 1.5, 0.200000003, 100, 70",
+    ["RACE_QUEUE_CATEGORIES"] = {
+        ["Acceleration"] = 1,
+        ["TopSpeed"] = 3,
+        ["Mixed"] = 2,
+        ["Handling"] = 2,
+        ["OffRoad"] = 1
+    },
+    ["AUCTION_SELLER_FEE"] = 0.15,
+    ["WEATHER_LENGTH"] = 600,
+    ["DEFAULT_NITROUS_BOOST"] = 100,
+    ["ASPIRATION_SHARE"] = 0.35,
+    ["DEFAULT_STEER_MODIFIER"] = 0.5,
+    ["MASS_TARGET"] = 250,
+    ["MOD_RANK"] = 5,
+    ["VIP_CASH_AMOUNT"] = 500000,
+    ["GOLDIFY_PRICE_PER_PURCHASE"] = 5,
+    ["STARTER_CARS"] = {
+        ["Control"] = {
+            ["FocusedCar"] = "Stallion",
+            ["Scene"] = "FTUX_Garage",
+            ["Cars"] = {"Stallion", "Stratus", "Thunder"}
+        }
+    },
+    ["MAX_GRIP"] = 3,
+    ["CAR_CLASS_COLORS"] = {
+        ["Track"] = Color3.fromRGB(74, 2, 255),
+        ["Misc"] = Color3.fromRGB(29, 29, 29),
+        ["Semi"] = Color3.fromRGB(86, 86, 86),
+        ["Classic"] = Color3.fromRGB(114, 222, 255),
+        ["Coupe"] = Color3.fromRGB(188, 155, 93),
+        ["Hyper"] = Color3.fromRGB(255, 0, 4),
+        ["Super"] = Color3.fromRGB(45, 119, 255),
+        ["Sport"] = Color3.fromRGB(255, 174, 0),
+        ["Sedan"] = Color3.fromRGB(167, 68, 185),
+        ["Muscle"] = Color3.fromRGB(255, 115, 0),
+        ["Utility"] = Color3.fromRGB(236, 239, 255)
+    },
+    ["PISTONS_SHARE"] = 0.3,
+    ["DEFAULT_BRAKE_FORCE"] = 30,
+    ["DAY_CYCLE"] = 1800,
+    ["GARAGE_UPDATE_ENABLED"] = true,
+    ["PERCENT_NIGHT"] = 0.1,
+    ["CHALLENGER_BEAT_XP_RATE"] = 0.5,
+    ["DAY_START_HOUR"] = 6,
+    ["WEATHER_MATERIAL_OVERRIDES"] = {
+        ["Rock"] = {["Dry"] = "AsphaltStripedYellow", ["Wet"] = "AsphaltStripedYellowWet"},
+        ["Asphalt"] = {["Dry"] = "AsphaltCustom", ["Wet"] = "AsphaltCustomWet"},
+        ["Salt"] = {["Dry"] = "AsphaltCustomStripedMarking1", ["Wet"] = "AsphaltCustomStripedMarking1Wet"}
+    },
+    ["WEATHER_TRIGGER_CHANCE"] = 0.1,
+    ["TESTER_RANK"] = 2,
+    ["GAMEPAD_PRIORITIES"] = {
+        ["Controls"] = 3100,
+        ["SecondaryUI"] = 3300,
+        ["PrimaryUI"] = 3200,
+        ["PromptUI"] = 3350,
+        ["Loading"] = 3400
+    },
+    ["GARAGE_COLOR_GROUP_PREVENT_TAG"] = "GarageNoMatChange",
+    ["QUEST_RESET_INTERVAL"] = 64800,
+    ["LEVEL_CAP"] = 50,
+    ["EXHAUST_SHARE"] = 0.15,
+    ["COMPUTER_SHARE"] = 0.1,
+    ["DEFAULT_SHIFT_TIME"] = 0.25,
+    ["MAX_TOP_SPEED"] = 275,
+    ["MAX_DOWNFORCE"] = 400,
+    ["BEST_ROBUX_CONVERSION"] = 555,
+    ["STARTER_CAR_GA_NAME"] = "apr_startercars",
+    ["CAR_HITBOX_PHYSICS"] = "1, 0.200000003, 0.200000003, 100, 70",
+    ["NIGHT_START_HOUR"] = 17,
+    ["SKIP_QUEUE_GOLD_PRICE"] = 10,
+    ["GOLDIFY_BASE_PRICE"] = 15
+}
