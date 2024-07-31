e5fea17d056d2c1bf249a2228462d79f81f1d1d1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-07-31 22:00:31 +1000

    Create Chassis.lua

---
+++ GameData/Drive World [10704789056]/Chassis.lua
@@ -0,0 +1,30 @@
+data = {
+    ["Track"] = {
+        ["Category"] = "Chassis",
+        ["Order"] = 3,
+        ["CashPrice"] = 85000,
+        ["MassFloorPercentage"] = 0.66,
+        ["DisplayName"] = "Track Chassis"
+    },
+    ["Race"] = {
+        ["Category"] = "Chassis",
+        ["PremiumPrice"] = 90,
+        ["CashPrice"] = 234000,
+        ["Order"] = 4,
+        ["MassFloorPercentage"] = 1,
+        ["DisplayName"] = "Race Chassis"
+    },
+    ["Stock"] = {
+        ["Order"] = 1,
+        ["Category"] = "Chassis",
+        ["MassFloorPercentage"] = 0,
+        ["DisplayName"] = "Stock Chassis"
+    },
+    ["Sport"] = {
+        ["Category"] = "Chassis",
+        ["Order"] = 2,
+        ["CashPrice"] = 39000,
+        ["MassFloorPercentage"] = 0.33,
+        ["DisplayName"] = "Sport Chassis"
+    }
+}
