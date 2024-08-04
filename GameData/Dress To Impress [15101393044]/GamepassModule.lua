7a994da76ad345b437fb75aeaead68ee8ffc4318
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-05 01:39:18 +1000

    Create GamepassModule.lua

---
+++ GameData/Dress To Impress [15101393044]/GamepassModule.lua
@@ -0,0 +1,39 @@
+data = {
+    ["GamePassApp"] = {
+        ["IsOpen"] = false,
+        ["Blur"] = "Blur",
+        ["Tinfo"] = "Time:0.5 DelayTime:0 RepeatCount:0 Reverses:False EasingDirection:Out EasingStyle:Quint",
+        ["Locked"] = false,
+        ["CloseTween"] = {["Position"] = "{0.5, 0}, {-0.5, 0}"},
+        ["SubApps"] = {},
+        ["StateManager"] = "Event",
+        ["Button"] = "Gamepasses",
+        ["OpenTween"] = {["Position"] = "{0.5, 0}, {0.523000002, 0}"},
+        ["StartPos"] = {["Position"] = "{0.5, 0}, {-0.5, 0}"},
+        ["Gui"] = "Gamepass"
+    },
+    ["Info"] = {
+        ["Run Faster"] = {
+            ["PassId"] = 662806020,
+            ["HasPass"] = false,
+            ["Desc2"] = "You unlocked the Run Faster gamepass! Can can now run faster & change your speed from 0% to 100%.",
+            ["Price"] = "149",
+            ["Desc"] = "Change your walk speed as you wish from 0% to 100% speed!"
+        },
+        ["VIP Pass"] = {
+            ["PassId"] = 647191068,
+            ["HasPass"] = false,
+            ["Desc2"] = "You unlocked the VIP room, as well as a VIP name & chat tag and a VIP exclusive server!!",
+            ["Price"] = "299 / 799",
+            ["Desc"] = "Unlock the VIP Room, all VIP clothing and accessories, receive a VIP Chat and Name Tag!"
+        },
+        ["Custom Makeup"] = {
+            ["PassId"] = 646065677,
+            ["HasPass"] = false,
+            ["Desc2"] = "You unlocked Custom Makeup! Customize your Eyes, eyebrows, lips and touches!",
+            ["Price"] = "349",
+            ["Desc"] = "Customize your makeup as you wish! Eyes, eyebrows, and colorable lips and touches!"
+        }
+    },
+    ["Promote"] = "function: 0xf266d0b2eee2269e"
+}
