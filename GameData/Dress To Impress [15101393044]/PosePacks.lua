079f85e871f253fd0e1b1dbb9eb232b987bd8e8d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-05 01:37:07 +1000

    Update PosePacks.lua

--- GameData/Dress To Impress [15101393044]/PosePacks.lua
+++ GameData/Dress To Impress [15101393044]/PosePacks.lua
@@ -1,10 +1,18 @@
 data = {
-    ["Pop Culture Pose Pack"] = {["Price"] = 1800, ["Icon"] = "rbxassetid://17186576001"},
-    ["K-Pop Pose Pack"] = {["Price"] = 4500, ["Icon"] = "rbxassetid://17514148701"},
-    ["Fantasy Pose Pack"] = {["Price"] = 1000, ["Icon"] = "rbxassetid://17067974177"},
-    ["Supermodel Pose Pack"] = {["Price"] = 3200, ["Icon"] = "rbxassetid://17067577235"},
-    ["Sci-Fi Pose Pack"] = {["Price"] = 2000, ["Icon"] = "rbxassetid://17190530494"},
-    ["Roblox Classic Pose Pack"] = {["Price"] = 999999999999999, ["Icon"] = "rbxassetid://17579389160"},
-    ["Kawaii Pose Pack"] = {["Price"] = 4000, ["Icon"] = "rbxassetid://17058902638"},
-    ["Crazy Pose Pack"] = {["Price"] = 2000, ["Icon"] = "rbxassetid://17186565505"}
+    ["Summer Pose Pack"] = {["Price"] = 4300, ["Quantity"] = 10, ["Icon"] = "rbxassetid://18149525519"},
+    ["Editorial Pose Pack"] = {["Price"] = 2500, ["Quantity"] = 7, ["Icon"] = "rbxassetid://18748619782"},
+    ["Fantasy Pose Pack"] = {["Price"] = 1000, ["Quantity"] = 4, ["Icon"] = "rbxassetid://17067974177"},
+    ["Supermodel Pose Pack"] = {["Price"] = 3200, ["Quantity"] = 11, ["Icon"] = "rbxassetid://17067577235"},
+    ["Sci-Fi Pose Pack"] = {["Price"] = 2000, ["Quantity"] = 10, ["Icon"] = "rbxassetid://17190530494"},
+    ["One Billion Pose Pack"] = {["Price"] = 999999999999999,["Quantity"] = 2,["Icon"] = "rbxassetid://18245873119",["Deprecated"] = true},
+    ["Gala Pose Pack"] = {["Price"] = 5000, ["Quantity"] = 10, ["Icon"] = "rbxassetid://18149293270"},
+    ["Goddess Pose Pack"] = {["Price"] = 3000, ["Quantity"] = 8, ["Icon"] = "rbxassetid://18150058051"},
+    ["Masculine Pose Pack"] = {["Price"] = 2000, ["Quantity"] = 7, ["Icon"] = "rbxassetid://18748364450"},
+    ["Baddies Pose Pack"] = {["Price"] = 4000, ["Quantity"] = 4, ["Icon"] = "rbxassetid://18149207549"},
+    ["Mermaid Pose Pack"] = {["Price"] = 3500, ["Quantity"] = 10, ["Icon"] = "rbxassetid://18150099833"},
+    ["Pop Culture Pose Pack"] = {["Price"] = 1800, ["Quantity"] = 7, ["Icon"] = "rbxassetid://17186576001"},
+    ["K-Pop Pose Pack"] = {["Price"] = 4500, ["Quantity"] = 15, ["Icon"] = "rbxassetid://17514148701"},
+    ["Roblox Classic Pose Pack"] = {["Price"] = 999999999999999,["Quantity"] = 1,["Icon"] = "rbxassetid://17579389160",["Deprecated"] = true},
+    ["Kawaii Pose Pack"] = {["Price"] = 4000, ["Quantity"] = 18, ["Icon"] = "rbxassetid://17058902638"},
+    ["Crazy Pose Pack"] = {["Price"] = 2000, ["Quantity"] = 8, ["Icon"] = "rbxassetid://17186565505"}
 }
