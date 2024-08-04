10f5e2c8bf423dc4ecffa3b95c3457166951263c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-05 01:35:00 +1000

    Create Ranks.lua

---
+++ GameData/Dress To Impress [15101393044]/Ranks.lua
@@ -0,0 +1,12 @@
+data = {
+    {["Stars"] = 0, ["Rank"] = "New Model"},
+    {["Stars"] = 50, ["Rank"] = "Rising Star"},
+    {["Stars"] = 200, ["Rank"] = "Aspiring Model"},
+    {["Stars"] = 500, ["Rank"] = "Fashionista"},
+    {["Stars"] = 1000, ["Rank"] = "Glamourista"},
+    {["Stars"] = 1500, ["Rank"] = "Fashion Maven"},
+    {["Stars"] = 3000, ["Rank"] = "Runway Queen"},
+    {["Stars"] = 6000, ["Rank"] = "Trend-Setter"},
+    {["Stars"] = 15000, ["Rank"] = "Runway Diva"},
+    {["Stars"] = 25000, ["Rank"] = "Top Model"}
+}
