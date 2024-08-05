ffe556414f676676a5a5910e17ae4c341ef6cee6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-06 01:51:16 +1000

    Create MonetizationLibrary.lua

---
+++ GameData/RIVALS [17625359962]/MonetizationLibrary.lua
@@ -0,0 +1,304 @@
+data = {
+    ["Products"] = {
+        ["DailySkin2Rare"] = {["ProductID"] = 1857673748, ["DisplayName"] = "Rare Skin #2"},
+        ["DailySkin1Legendary"] = {["ProductID"] = 1852068440, ["DisplayName"] = "Legendary Skin #1"},
+        ["DailySkin2Legendary"] = {["ProductID"] = 1857673858, ["DisplayName"] = "Legendary Skin #2"},
+        ["DailySkin1Rare"] = {["ProductID"] = 1852080069, ["DisplayName"] = "Rare Skin #1"},
+        ["DailySkin2Common"] = {["ProductID"] = 1857673571, ["DisplayName"] = "Common Skin #2"},
+        ["DailySkin1Common"] = {["ProductID"] = 1852078966, ["DisplayName"] = "Common Skin #1"},
+        ["RefreshTasks"] = {["ProductID"] = 1852077788, ["DisplayName"] = "Refresh Daily Tasks"}
+    },
+    ["Bundles"] = {
+        ["keybundle_2"] = {
+            ["ProductID"] = 1851594599,
+            ["Rewards"] = {
+                {["Name"] = "Key", ["Quantity"] = 40},
+                {["Quantity"] = 1, ["Name"] = "Charm Capsule", ["Weapon"] = "IsRandom"}
+            },
+            ["DisplayName"] = "Mega Key Bundle"
+        },
+        ["keybundle_4"] = {
+            ["ProductID"] = 1851594902,
+            ["Rewards"] = {
+                {["Name"] = "Key", ["Quantity"] = 450},
+                {["Quantity"] = 20, ["Name"] = "Charm Capsule", ["Weapon"] = "IsRandom"},
+                {["Quantity"] = 5, ["Name"] = "Wrap Box", ["Weapon"] = "IsRandom"},
+                {["Quantity"] = 5, ["Name"] = "Mini Key", ["Weapon"] = "IsRandom"},
+                {["Quantity"] = 1, ["Name"] = "Wealth", ["Weapon"] = "IsRandom"},
+                {["Quantity"] = 1, ["Name"] = "Lucre", ["Weapon"] = "IsRandom"}
+            },
+            ["DisplayName"] = "Ultra Key Bundle"
+        },
+        ["starter_bundle"] = {
+            ["BubbleText"] = "x12 VALUE",
+            ["GamepassName"] = "StarterBundle",
+            ["Rewards"] = {
+                {["Name"] = "Unlock Token", ["Quantity"] = 1},
+                {["Name"] = "Shorty"},
+                {["Name"] = "Too Shorty", ["Weapon"] = "Shorty"},
+                {["Name"] = "Paint", ["Weapon"] = "IsUniversal"},
+                {["Name"] = "Emoji: Nauseated", ["Weapon"] = "IsUniversal"},
+                {["Name"] = "Key", ["Quantity"] = 10}
+            },
+            ["DisplayName"] = "Starter Bundle"
+        },
+        ["keybundle_3"] = {
+            ["ProductID"] = 1851594741,
+            ["Rewards"] = {
+                {["Name"] = "Key", ["Quantity"] = 90},
+                {["Quantity"] = 4, ["Name"] = "Charm Capsule", ["Weapon"] = "IsRandom"},
+                {["Quantity"] = 1, ["Name"] = "Wrap Box", ["Weapon"] = "IsRandom"},
+                {["Quantity"] = 1, ["Name"] = "Mini Key", ["Weapon"] = "IsRandom"}
+            },
+            ["DisplayName"] = "Super Key Bundle"
+        },
+        ["standardweapons_bundle"] = {
+            ["BubbleText"] = "x3 VALUE",
+            ["GamepassName"] = "StandardWeaponsBundle",
+            ["Rewards"] = {
+                {["Name"] = "Disco", ["Weapon"] = "IsUniversal"},
+                {["Name"] = "Mini Disco Ball", ["Weapon"] = "IsUniversal"},
+                {["Quantity"] = 5, ["Name"] = "Wrap Box", ["Weapon"] = "IsRandom"},
+                {["Quantity"] = 10, ["Name"] = "Charm Capsule", ["Weapon"] = "IsRandom"},
+                {["Name"] = "Key", ["Quantity"] = 15}
+            },
+            ["DisplayName"] = "Standard Weapons Bundle"
+        },
+        ["classic_bundle"] = {
+            ["BubbleText"] = "x7 VALUE",
+            ["GamepassName"] = "ClassicBundle",
+            ["Rewards"] = {
+                {["Name"] = "Paintball Gun"},
+                {["Name"] = "Slingshot"},
+                {["Name"] = "Trowel"},
+                {["Name"] = "Subspace Tripmine"},
+                {["Name"] = "Classic", ["Weapon"] = "IsUniversal"},
+                {["Name"] = "Mini Ban Hammer", ["Weapon"] = "IsUniversal"},
+                {["Quantity"] = 10, ["Name"] = "Wrap Box", ["Weapon"] = "IsRandom"},
+                {["Quantity"] = 20, ["Name"] = "Charm Capsule", ["Weapon"] = "IsRandom"},
+                {["Name"] = "Key", ["Quantity"] = 40}
+            },
+            ["DisplayName"] = "Classic Bundle"
+        },
+        ["keybundle_5"] = {
+            ["ProductID"] = 1851595031,
+            ["Rewards"] = {
+                {["Name"] = "Key", ["Quantity"] = 1100},
+                {["Quantity"] = 60, ["Name"] = "Charm Capsule", ["Weapon"] = "IsRandom"},
+                {["Quantity"] = 15, ["Name"] = "Wrap Box", ["Weapon"] = "IsRandom"},
+                {["Quantity"] = 15, ["Name"] = "Mini Key", ["Weapon"] = "IsRandom"},
+                {["Quantity"] = 5, ["Name"] = "Wealth", ["Weapon"] = "IsRandom"},
+                {["Quantity"] = 5, ["Name"] = "Lucre", ["Weapon"] = "IsRandom"},
+                {["Name"] = "Keythe", ["Weapon"] = "Scythe"},
+                {["Quantity"] = 1, ["Name"] = "Skin Case", ["Weapon"] = "IsRandom"}
+            },
+            ["DisplayName"] = "Legendary Key Bundle"
+        },
+        ["keybundle_1"] = {
+            ["ProductID"] = 1851594490,
+            ["Rewards"] = {{["Name"] = "Key", ["Quantity"] = 10}},
+            ["DisplayName"] = "Key Bundle"
+        },
+        ["heavyduty_bundle"] = {
+            ["BubbleText"] = "x4 VALUE",
+            ["GamepassName"] = "HeavyDutyBundle",
+            ["Rewards"] = {
+                {["Name"] = "Grenade Launcher"},
+                {["Name"] = "Minigun"},
+                {["Name"] = "Flamethrower"},
+                {["Name"] = "Magma", ["Weapon"] = "IsUniversal"},
+                {["Name"] = "Explosion", ["Weapon"] = "IsUniversal"},
+                {["Quantity"] = 10, ["Name"] = "Wrap Box", ["Weapon"] = "IsRandom"},
+                {["Quantity"] = 20, ["Name"] = "Charm Capsule", ["Weapon"] = "IsRandom"},
+                {["Name"] = "Key", ["Quantity"] = 40}
+            },
+            ["DisplayName"] = "Heavy Duty Bundle"
+        },
+        ["exogun_bundle"] = {
+            ["BubbleText"] = "x4 VALUE",
+            ["GamepassName"] = "ExogunBundle",
+            ["Rewards"] = {
+                {["Name"] = "Exogun"},
+                {["Name"] = "Wondergun", ["Weapon"] = "Exogun"},
+                {["Name"] = "Nebula", ["Weapon"] = "IsUniversal"},
+                {["Name"] = "Alien Head", ["Weapon"] = "IsUniversal"},
+                {["Quantity"] = 15, ["Name"] = "Wrap Box", ["Weapon"] = "IsRandom"},
+                {["Name"] = "Key", ["Quantity"] = 20}
+            },
+            ["DisplayName"] = "Exogun Bundle"
+        },
+        ["medkit_bundle"] = {
+            ["BubbleText"] = "x10 VALUE",
+            ["GamepassName"] = "MedkitBundle",
+            ["Rewards"] = {
+                {["Name"] = "Medkit"},
+                {["Name"] = "Briefcase", ["Weapon"] = "Medkit"},
+                {["Name"] = "Aurum", ["Weapon"] = "IsUniversal"},
+                {["Name"] = "First Aid", ["Weapon"] = "IsUniversal"},
+                {["Quantity"] = 15, ["Name"] = "Wrap Box", ["Weapon"] = "IsRandom"},
+                {["Name"] = "Key", ["Quantity"] = 20}
+            },
+            ["DisplayName"] = "Medkit Bundle"
+        }
+    },
+    ["GetBundleContainingReward"] = "function: 0xc38046a6e7d3a44e",
+    ["GetProductName"] = "function: 0x7e05feef50891cae",
+    ["NUM_KEY_BUNDLES"] = 5,
+    ["GiftOrder"] = {
+        "1892544195",
+        "1892544449",
+        "1890371681",
+        "1890372364",
+        "1890372130",
+        "1890372574",
+        "1890372831",
+        "1890371901",
+        "1890374722",
+        "1890374959",
+        "1890375172",
+        "1890375405",
+        "1890375611"
+    },
+    ["Gamepasses"] = {
+        ["StandardWeaponsBundle"] = {
+            ["BundleName"] = "standardweapons_bundle",
+            ["GamepassID"] = 838203071,
+            ["DisplayName"] = "Standard Weapons Bundle"
+        },
+        ["ClassicBundle"] = {
+            ["BundleName"] = "classic_bundle",
+            ["GamepassID"] = 838160059,
+            ["DisplayName"] = "Classic Bundle"
+        },
+        ["MedkitBundle"] = {
+            ["BundleName"] = "medkit_bundle",
+            ["GamepassID"] = 837904767,
+            ["DisplayName"] = "Medkit Bundle"
+        },
+        ["ExogunBundle"] = {
+            ["BundleName"] = "exogun_bundle",
+            ["GamepassID"] = 838087219,
+            ["DisplayName"] = "Exogun Bundle"
+        },
+        ["StarterBundle"] = {
+            ["BundleName"] = "starter_bundle",
+            ["GamepassID"] = 839491390,
+            ["DisplayName"] = "Starter Bundle"
+        },
+        ["HeavyDutyBundle"] = {
+            ["BundleName"] = "heavyduty_bundle",
+            ["GamepassID"] = 838198043,
+            ["DisplayName"] = "Heavy Duty Bundle"
+        }
+    },
+    ["GetBundleByProductID"] = "function: 0xe8a037758544bdde",
+    ["Gifts"] = {
+        ["1892544195"] = {
+            ["TargetProductID"] = 1852079505,
+            ["GiftName"] = "lootbox_Skin Case",
+            ["ProductType"] = "Product",
+            ["GiftType"] = "ShopEntry",
+            ["ProductID"] = 1892544195,
+            ["IsPaidRandomItem"] = true,
+            ["DisplayName"] = "Skin Case"
+        },
+        ["1890372574"] = {
+            ["TargetProductID"] = 838087219,
+            ["GiftName"] = "ExogunBundle",
+            ["ProductType"] = "Gamepass",
+            ["GiftType"] = "Gamepass",
+            ["ProductID"] = 1890372574,
+            ["DisplayName"] = "Exogun Bundle"
+        },
+        ["1890372831"] = {
+            ["TargetProductID"] = 837904767,
+            ["GiftName"] = "MedkitBundle",
+            ["ProductType"] = "Gamepass",
+            ["GiftType"] = "Gamepass",
+            ["ProductID"] = 1890372831,
+            ["DisplayName"] = "Medkit Bundle"
+        },
+        ["1890372364"] = {
+            ["TargetProductID"] = 838160059,
+            ["GiftName"] = "ClassicBundle",
+            ["ProductType"] = "Gamepass",
+            ["GiftType"] = "Gamepass",
+            ["ProductID"] = 1890372364,
+            ["DisplayName"] = "Classic Bundle"
+        },
+        ["1890375611"] = {
+            ["TargetProductID"] = 1851595031,
+            ["GiftName"] = "keybundle_5",
+            ["ProductType"] = "Product",
+            ["GiftType"] = "Bundle",
+            ["ProductID"] = 1890375611,
+            ["DisplayName"] = "Legendary Key Bundle"
+        },
+        ["1892544449"] = {
+            ["TargetProductID"] = 1892542763,
+            ["GiftName"] = "lootbox_Skin Case 2",
+            ["ProductType"] = "Product",
+            ["GiftType"] = "ShopEntry",
+            ["ProductID"] = 1892544449,
+            ["IsPaidRandomItem"] = true,
+            ["DisplayName"] = "Skin Case 2"
+        },
+        ["1890374722"] = {
+            ["TargetProductID"] = 1851594490,
+            ["GiftName"] = "keybundle_1",
+            ["ProductType"] = "Product",
+            ["GiftType"] = "Bundle",
+            ["ProductID"] = 1890374722,
+            ["DisplayName"] = "Key Bundle"
+        },
+        ["1890371681"] = {
+            ["TargetProductID"] = 838203071,
+            ["GiftName"] = "StandardWeaponsBundle",
+            ["ProductType"] = "Gamepass",
+            ["GiftType"] = "Gamepass",
+            ["ProductID"] = 1890371681,
+            ["DisplayName"] = "Standard Weapons Bundle"
+        },
+        ["1890375172"] = {
+            ["TargetProductID"] = 1851594741,
+            ["GiftName"] = "keybundle_3",
+            ["ProductType"] = "Product",
+            ["GiftType"] = "Bundle",
+            ["ProductID"] = 1890375172,
+            ["DisplayName"] = "Super Key Bundle"
+        },
+        ["1890375405"] = {
+            ["TargetProductID"] = 1851594902,
+            ["GiftName"] = "keybundle_4",
+            ["ProductType"] = "Product",
+            ["GiftType"] = "Bundle",
+            ["ProductID"] = 1890375405,
+            ["DisplayName"] = "Ultra Key Bundle"
+        },
+        ["1890372130"] = {
+            ["TargetProductID"] = 838198043,
+            ["GiftName"] = "HeavyDutyBundle",
+            ["ProductType"] = "Gamepass",
+            ["GiftType"] = "Gamepass",
+            ["ProductID"] = 1890372130,
+            ["DisplayName"] = "Heavy Duty Bundle"
+        },
+        ["1890374959"] = {
+            ["TargetProductID"] = 1851594599,
+            ["GiftName"] = "keybundle_2",
+            ["ProductType"] = "Product",
+            ["GiftType"] = "Bundle",
+            ["ProductID"] = 1890374959,
+            ["DisplayName"] = "Mega Key Bundle"
+        },
+        ["1890371901"] = {
+            ["TargetProductID"] = 839491390,
+            ["GiftName"] = "StarterBundle",
+            ["ProductType"] = "Gamepass",
+            ["GiftType"] = "Gamepass",
+            ["ProductID"] = 1890371901,
+            ["DisplayName"] = "Starter Bundle"
+        }
+    },
+    ["GetGamepassName"] = "function: 0x3569be5c9731444e"
+}
