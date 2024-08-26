394b0a8fb99f2879834bcaf08fb88cbaf19c5cb1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-27 02:37:52 +1000

    Create GeneralIcons.lua

---
+++ GameData/Dungeon Quest [2414851778]/GeneralIcons.lua
@@ -0,0 +1,94 @@
+data = {
+    ["ConsumableIcons"] = {
+        ["progression"] = "rbxassetid://16794749011",
+        ["combat"] = "rbxassetid://16821278345",
+        ["xp"] = "rbxassetid://16251661590",
+        ["ultimate"] = "rbxassetid://16812006550",
+        ["greggbounty"] = "rbxassetid://17267037840",
+        ["luck"] = "rbxassetid://16821900218",
+        ["health"] = "rbxassetid://16821895766",
+        ["speed"] = "rbxassetid://16821894351",
+        ["cooldown"] = "rbxassetid://16821890209",
+        ["revive"] = "rbxassetid://16821891988",
+        ["damage"] = "rbxassetid://16821898649"
+    },
+    ["EmblemBundles"] = {
+        "rbxassetid://17363325124",
+        "rbxassetid://17363324854",
+        "rbxassetid://17363324508",
+        "rbxassetid://17363324343",
+        "rbxassetid://17363324262"
+    },
+    ["GeneralImages"] = {
+        ["General XP Booster"] = "rbxassetid://15171050702",
+        ["Gift Image"] = "rbxassetid://15508980683",
+        ["Gold Bundle"] = "rbxassetid://16272097589"
+    },
+    ["Boosters"] = {
+        ["6hour"] = "rbxassetid://16181809065",
+        ["General XP Booster"] = "rbxassetid://15171050702",
+        ["4hour"] = "rbxassetid://16181809065",
+        ["2hour"] = "rbxassetid://16181808195",
+        ["1hour"] = "rbxassetid://16181805045",
+        ["12hour"] = "rbxassetid://16181809065",
+        ["30min"] = "rbxassetid://16181806598",
+        ["15min"] = "rbxassetid://16181807250",
+        ["24hour"] = "rbxassetid://16181809065"
+    },
+    ["ConfigRewards"] = {
+        ["Easter key"] = "rbxassetid://16628773596",
+        ["Speed boost"] = "rbxassetid://16873205789",
+        ["XP boost"] = "rbxassetid://16873202198",
+        ["Revive potion"] = "rbxassetid://16873215573",
+        ["Gold Package 2"] = "rbxassetid://16181706171",
+        ["Gold Package 5"] = "rbxassetid://16181703523",
+        ["Cosmetic coupon"] = "rbxassetid://16742703042",
+        ["Gold Package 3"] = "rbxassetid://16181706957",
+        ["Gold Package 4"] = "rbxassetid://16181704193",
+        ["Gold"] = "rbxassetid://16122776103",
+        ["Gold Package 1"] = "rbxassetid://16181705031",
+        ["Luck boost"] = "rbxassetid://16873210051",
+        ["XP"] = "rbxassetid://16122761705",
+        ["Health boost"] = "rbxassetid://16873213679"
+    },
+    ["Platinum"] = {
+        ["tier5"] = "rbxassetid://17438603030",
+        ["tier2"] = "rbxassetid://17438603477",
+        ["tier1"] = "rbxassetid://17438603628",
+        ["tier3"] = "rbxassetid://17438603300",
+        ["tier4"] = "rbxassetid://17438603143"
+    },
+    ["CraftingMaterials"] = {
+        ["Obsidian Chunk"] = "rbxassetid://17166875877",
+        ["Warforged Metal"] = "rbxassetid://17166874566",
+        ["Astral Amethyst"] = "rbxassetid://17166877182",
+        ["Gold Bar"] = "rbxassetid://17166876539"
+    },
+    ["GoldBundles"] = {
+        "rbxassetid://16181705031",
+        "rbxassetid://16181706171",
+        "rbxassetid://16181706957",
+        "rbxassetid://16181704193",
+        "rbxassetid://16181703523"
+    },
+    ["HighlightColors"] = {
+        ["progression"] = Color3.fromRGB(255, 255, 255),
+        ["combat"] = Color3.fromRGB(255, 255, 255),
+        ["rare"] = Color3.fromRGB(84, 124, 255),
+        ["luck"] = Color3.fromRGB(216, 170, 62),
+        ["gold"] = Color3.fromRGB(255, 200, 33),
+        ["speed"] = Color3.fromRGB(87, 219, 204),
+        ["revive"] = Color3.fromRGB(241, 209, 102),
+        ["damage"] = Color3.fromRGB(242, 124, 100),
+        ["common"] = Color3.fromRGB(255, 255, 255),
+        ["booster"] = Color3.fromRGB(119, 178, 255),
+        ["uncommon"] = Color3.fromRGB(119, 255, 153),
+        ["legendary"] = Color3.fromRGB(255, 102, 51),
+        ["health"] = Color3.fromRGB(61, 201, 67),
+        ["ultimate"] = Color3.fromRGB(255, 255, 255),
+        ["epic"] = Color3.fromRGB(160, 65, 255),
+        ["greggbounty"] = Color3.fromRGB(241, 209, 102),
+        ["cooldown"] = Color3.fromRGB(204, 90, 218),
+        ["xp"] = Color3.fromRGB(101, 186, 240)
+    }
+}
