e97d45a3430396d84e3a103d27e2180dff987295
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-27 02:13:19 +1000

    Update TierSkins.lua

--- GameData/Sol's RNG [15532962292]/TierSkins.lua
+++ GameData/Sol's RNG [15532962292]/TierSkins.lua
@@ -1,42 +1,196 @@
 data = {
-    ["Rage"] = {["Heated"] = {["Rarity"] = 1, ["CreatorId"] = 419860256}},
-    ["BOUNDED"] = {["UNBOUND"] = {["Rarity"] = 1}},
-    ["Impeached"] = {
-        ["impeach"] = {
-            ["LimitedTimestamp"] = 1714143600,
+    ["BOUNDED"] = {
+        ["UNBOUND"] = {
+            ["AudioInfo"] = {["Name"] = "Brooding Dark (a)", ["Author"] = "APM Music"},
+            ["CreatorId"] = 419860256,
             ["Rarity"] = 1,
-            ["Description"] = [=[im peach
+            ["Description"] = [[When news spread that this being had awoken, people trembled in fear. 
 
-Aura dedicated to April Fool's Day. You can get it until [April 26, 2024 11:00 AM]]=]
+However, something was odd; the creature had awoken but showed no signs of activity. 
+Yet, it was too early for assumptions. It could be just recovering its strength...]]
         }
     },
-    ["Divinus"] = {["Love"] = {["Rarity"] = 0, ["Unobtainable"] = true}},
+    ["Divinus"] = {
+        ["Love"] = {
+            ["Unobtainable"] = true,
+            ["Rarity"] = 0,
+            ["Description"] = [[Perchance, maybe <font color="rgb(245,136,255)">love</font> is the strongest emotion?]]
+        }
+    },
     ["Exotic"] = {
         ["APEX"] = {
             ["AudioInfo"] = {["Name"] = "Panic Track", ["Author"] = "Remi Gallego"},
             ["CreatorId"] = 419860256,
+            ["Rarity"] = 1,
+            ["Description"] = [[Maybe this is the source of the ancient <font color="rgb(255,0,0)">being</font>, that people were dying to know about?]]
+        }
+    },
+    ["Celestial"] = {
+        ["Divine"] = {
+            ["AudioInfo"] = {["Name"] = "Slow Down", ["Author"] = "Creo"},
+            ["CreatorId"] = 1270194733,
+            ["Rarity"] = 1,
+            ["Description"] = "EMPTY"
+        },
+        ["Cupid"] = {
+            ["Unobtainable"] = true,
+            ["CreatorId"] = 419860256,
+            ["Rarity"] = 0,
+            ["Description"] = [[Do you want your <font color="rgb(245,136,255)">love</font> to come true?? 
+I’ll make it come true!]]
+        }
+    },
+    ["StarRider"] = {
+        ["Starfish"] = {
+            ["LimitedTimestamp"] = 1724425200,
+            ["CreatorId"] = 419860256,
+            ["Rarity"] = 1,
+            ["EquipTimestampLimit"] = 1724425200
+        }
+    },
+    ["Stormal"] = {
+        ["Hurricane"] = {
+            ["AudioInfo"] = {["Name"] = "Looking In a", ["Author"] = "APM Music"},
+            ["CreatorId"] = 1188520962,
             ["Rarity"] = 1
         }
     },
-    ["Magnetic"] = {["Reverse Polarity"] = {["Rarity"] = 1, ["CreatorId"] = 500806314}},
-    ["Diaboli"] = {["Void"] = {["Rarity"] = 1, ["CreatorId"] = 419860256}},
-    ["Undefined"] = {
-        ["Defined"] = {
-            ["LimitedTimestamp"] = 1714143600,
+    ["Arcane"] = {
+        ["Legacy"] = {
+            ["CreatorId"] = 419860256,
             ["Rarity"] = 1,
-            ["Description"] = "Aura dedicated to April Fool's Day. You can get it until [April 26, 2024 11:00 AM]"
+            ["Description"] = [[A <font color="rgb(88,210,238)">powerful spell</font> created by an ancient civilization, it was originally invented as a means of salvation in war, but instead the spell drived out of control and wiped out their entire civilization from the world. 
+
+Maybe it was... too powerful.]]
+        },
+        ["Dark"] = {
+            ["CreatorId"] = 419860256,
+            ["Rarity"] = 2,
+            ["Description"] = [[This is the result of intervening <font color="rgb(53,1,48)"><stroke color="rgb(158,13,196)" thickness="1" transparency="0">darkness</stroke></font> into a <font color="rgb(88,210,238)">powerful spell</font> capable of bringing destruction that could devastate civilizations in an instant. 
+
+This dreadful entity, no longer a mere spell, now is a sentient being 
+
+Destined to bring down destruction and chaos upon the world according to its own will]]
         }
     },
-    [":Flushed:"] = {
-        ["Heart Eye"] = {["Rarity"] = 0, ["Unobtainable"] = true},
-        ["Lobotomy"] = {["CreatorId"] = 198120391, ["Rarity"] = 1, ["ConceptId"] = 1616246197}
+    ["Undead"] = {
+        ["Devil"] = {
+            ["CreatorId"] = 419860256,
+            ["Rarity"] = 1,
+            ["Description"] = "The tiny demon"
+        }
     },
-    ["Celestial"] = {["Cupid"] = {["Rarity"] = 0, ["Unobtainable"] = true}},
-    ["Arcane"] = {
-        ["Legacy"] = {["Rarity"] = 1, ["CreatorId"] = 419860256},
-        ["Dark"] = {["Rarity"] = 2, ["CreatorId"] = 419860256}
+    ["Impeached"] = {
+        ["impeach"] = {
+            ["Unobtainable"] = true,
+            ["CreatorId"] = 419860256,
+            ["Rarity"] = 1,
+            ["Description"] = [=[[HEEHEEHEE, 'I’m a peach!'
+
+im peach
+
+Aura dedicated to April Fool's Day. You can get it until [April 27, 2024 1:00 AM]]=]
+        },
+        ["LimitedTimestamp"] = 1714143600
     },
-    ["Undead"] = {["Devil"] = {["Rarity"] = 1, ["CreatorId"] = 419860256}},
+    ["Lunar"] = {
+        ["Full Moon"] = {
+            ["IsCommunityAura"] = true,
+            ["CreatorId"] = 317330352,
+            ["Rarity"] = 1,
+            ["AudioInfo"] = {["Name"] = "Shiny night", ["Author"] = "Kandera"},
+            ["Description"] = [[A <font color="rgb(240, 230, 140)">Full Moon</font> hung in the sky]]
+        }
+    },
+    ["Rage"] = {
+        ["Heated"] = {
+            ["CreatorId"] = 419860256,
+            ["Rarity"] = 1,
+            ["Description"] = "Furious rage"
+        }
+    },
+    ["Starscourge"] = {
+        ["Radiant"] = {
+            ["IsCommunityAura"] = true,
+            ["CreatorId"] = 94067972,
+            ["Rarity"] = 1,
+            ["AudioInfo"] = {["Name"] = "Crystal Drift (a)", ["Author"] = "APM Music"}
+        }
+    },
+    ["Sailor"] = {
+        ["Flying Dutchman"] = {
+            ["CreatorId"] = 1108424109,
+            ["Rarity"] = 1,
+            ["AudioInfo"] = {["Name"] = "Voyage of Lost Souls", ["Author"] = "End0_114"},
+            ["Description"] = [[<font color="rgb(81,113,80)">The Flying Dutchman</font>. The same terrible vessel whose very timbers are cut from the bodies and souls of doomed seamen.]],
+            ["HipHeight"] = 0
+        }
+    },
+    ["Solar"] = {
+        ["Solstice"] = {
+            ["IsCommunityAura"] = true,
+            ["CreatorId"] = 478424220,
+            ["Rarity"] = 1,
+            ["AudioInfo"] = {["Name"] = "White Knight", ["Author"] = "APM Music"}
+        }
+    },
+    ["Magnetic"] = {
+        ["Reverse Polarity"] = {
+            ["IsCommunityAura"] = true,
+            ["CreatorId"] = 500806314,
+            ["Rarity"] = 1
+        }
+    },
+    ["Diaboli"] = {
+        ["Void"] = {
+            ["Rarity"] = 1,
+            ["CreatorId"] = 419860256
+        }
+    },
+    ["Twilight"] = {
+        ["Iridescent Memory"] = {
+            ["AudioInfo"] = {["Name"] = "Fleecy Light", ["Author"] = "Baptiste Charvet"},
+            ["CreatorId"] = 1390696522,
+            ["Rarity"] = 1,
+            ["Description"] = [[The memories were becoming increasingly blurry.
+Under the full moon that holds many dreams high above, I sincerely wish.
+I believe that I will meet him again someday.
+I once again reminisce about the memories I have with the person I will never meet again.
+
+Her side is empty again today.]]
+        }
+    },
+    ["Overture"] = {
+        ["History"] = {
+            ["CreatorId"] = 419860256,
+            ["Rarity"] = 1,
+            ["Description"] = [[Tick* Tick* Tick* Tick* 
+
+When the clock hand started to move for the <font color="rgb(132,255,94)"><b>first time</b></font>, 
+was when everything started to move for the <font color="rgb(255,255,255)"><b>first time</b></font>.....]]
+        }
+    },
+    ["Undefined"] = {
+        ["Defined"] = {
+            ["Unobtainable"] = true,
+            ["LimitedTimestamp"] = 1714143600,
+            ["Rarity"] = 1,
+            ["Description"] = "Aura dedicated to April Fool's Day. You can get it until [April 27, 2024 1:00 AM]",
+            ["CreatorId"] = 1390696522
+        }
+    },
+    ["Ink"] = {
+        ["LEAK"] = {
+            ["Rarity"] = 1,
+            ["CreatorId"] = 419860256
+        }
+    },
+    ["Hazard"] = {
+        ["Rays"] = {
+            ["Rarity"] = 1,
+            ["CreatorId"] = 419860256
+        }
+    },
     ["Chromatic"] = {
         ["Exotic"] = {
             ["Unobtainable"] = true,
@@ -46,28 +200,46 @@ Aura dedicated to April Fool's Day. You can get it until [April 26, 2024 11:00 A
             ["Description"] = "Inside of hollow and dark place."
         },
         ["Kromat1k"] = {
-            ["AudioInfo"] = {["Name"] = "The Great Strategy", ["Author"] = "badliz"},
-            ["LimitedTimestamp"] = 1714143600,
+            ["Unobtainable"] = true,
+            ["CreatorId"] = 419860256,
             ["Rarity"] = 0.4,
-            ["Description"] = "Aura dedicated to April Fool's Day. You can get it until [April 26, 2024 11:00 AM]"
-        },
-        ["Genesis"] = {
-            ["AudioInfo"] = {["Name"] = "Something Different (Feat. Talurre) [Remastered]", ["Author"] = "Derpcat"},
-            ["Rarity"] = 1,
-            ["Description"] = [[Defining the beginning and end of the world.
+            ["AudioInfo"] = {["Name"] = "The Great Strategy", ["Author"] = "badliz"},
+            ["Description"] = [[y3S! Ffeel my unst0wpavb1e beat!?
+
+Aura dedicated to April Fool's Day. You can get it until [April 27, 2024 1:00 AM]],
+            ["LimitedTimestamp"] = 1714143600
+        }
+    },
+    ["Genesis"] = {
+        ["AudioInfo"] = {["Name"] = "Something Different (Feat. Talurre) [Remastered]", ["Author"] = "Derpcat"},
+        ["CreatorId"] = 419860256,
+        ["Rarity"] = 1,
+        ["Description"] = [[Defining the beginning and end of the world.
 
 <b><font transparency=".75">WAKE
 UP
 FROM
 AWAY</font></b>]]
+    },
+    [":Flushed:"] = {
+        ["Heart Eye"] = {
+            ["Unobtainable"] = true,
+            ["Rarity"] = 0,
+            ["Description"] = [[I’ve fallen in <font color="rgb(245,136,255)">love</font>!]]
+        },
+        ["Lobotomy"] = {
+            ["Description"] = "Fire In The Hole!!",
+            ["CreatorId"] = 198120391,
+            ["Rarity"] = 1,
+            ["ConceptId"] = 1616246197
         }
     },
-    ["Aquatic"] = {["Flame"] = {["Rarity"] = 1, ["CreatorId"] = 1390696522}},
-    ["Lunar"] = {
-        ["Full Moon"] = {
-            ["AudioInfo"] = {["Name"] = "Shiny night", ["Author"] = "Kandera"},
-            ["CreatorId"] = 198120391,
-            ["Rarity"] = 1
+    ["Aquatic"] = {
+        ["Flame"] = {
+            ["IsCommunityAura"] = true,
+            ["CreatorId"] = 1390696522,
+            ["Rarity"] = 1,
+            ["Description"] = [[It represents the flow of <font color="rgb(255,140,0)">the flames</font>]]
         }
     }
 }
