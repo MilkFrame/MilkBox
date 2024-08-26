9eae819d45cbb2f6696a144dbf84943bcb2a2705
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-27 01:57:56 +1000

    Update Buffs.lua

--- GameData/Sol's RNG [15532962292]/Buffs.lua
+++ GameData/Sol's RNG [15532962292]/Buffs.lua
@@ -1,33 +1,208 @@
 data = {
-    ["Lucky"] = {["EffectPerBuff"] = 1, ["Information"] = "+%s Luck"},
-    ["Basic Luck"] = {["EffectPerBuff"] = 0.25, ["Information"] = "+%s Luck"},
-    ["QuickRoll"] = {["EffectPerBuff"] = -25, ["Information"] = "%s%% Roll cooltime"},
-    ["Fortune III"] = {["EffectPerBuff"] = 2.5, ["Information"] = "+%s Luck"},
-    ["Fortune I"] = {["EffectPerBuff"] = 1.5, ["Information"] = "+%s Luck"},
-    ["Universe I"] = {["Information"] = "Aura in Starfall Unaffected by Breakthrough"},
-    ["Heavenly I"] = {["SaveBuffData"] = true, ["EffectPerBuff"] = 20000, ["Information"] = "Luck +%s on the next roll"},
-    ["Unstable Gravity"] = {["Information"] = "Increase bonus roll luck by 6"},
-    ["Tip's Blessing"] = {["EffectPerBuff"] = 1.2, ["Information"] = "+%s Luck"},
+    ["Lucky"] = {["EffectPerBuff"] = 100, ["Information"] = [[+%s%% <font color="rgb(84,252,124)"><b>Luck</b></font>]]},
+    ["Exoflex Blessing"] = {
+        ["IsRightGearBuff"] = true,
+        ["Information"] = [[+340% <font color="rgb(84,252,124)"><b>Luck</b></font>, +35% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]],
+        ["IsGearBuff"] = true
+    },
+    ["Sea Blessing"] = {
+        ["IsRightGearBuff"] = false,
+        ["Information"] = [[+50% <font color="rgb(84,252,124)"><b>Luck</b></font>]],
+        ["IsGearBuff"] = true
+    },
+    ["Rune of Everything"] = {
+        ["Information"] = [[Allows you to roll <font color="#FFFFFF"><b><stroke color="#000000" joins="miter" thickness="1.5" transparency="0.1">Every Biome Exclusive Auras</stroke></b></font> with <font color="rgb(255,145,115)"><b>Native Rarity</b></font> <font color="rgb(63,255,123)"><b><u>Regardless</u></b></font> of <b><font color="rgb(255,177,118)">Current Biome</font></b>]]
+    },
+    ["QuickRoll"] = {
+        ["EffectPerBuff"] = 25,
+        ["Information"] = [[%s%% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]]
+    },
+    ["Fortune III"] = {
+        ["EffectPerBuff"] = 250,
+        ["Information"] = [[+%s%% <font color="rgb(84,252,124)"><b>Luck</b></font>]]
+    },
+    ["The Knowledge II"] = {["Information"] = [[+40% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]]},
+    ["Heavenly I"] = {
+        ["SaveBuffData"] = true,
+        ["EffectPerBuff"] = 1,
+        ["Information"] = [[<font color="rgb(175,75,255)">+20,000</font> <font color="rgb(84,252,124)"><b>Luck</b></font> for <b><font color="rgb(255,53,44)"><u>Next %s Roll(s)</u></font></b>]]
+    },
+    ["Galestorm"] = {
+        ["IsRightGearBuff"] = true,
+        ["Information"] = [[+115% <font color="rgb(84,252,124)"><b>Luck</b></font>, +30% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]],
+        ["IsGearBuff"] = true
+    },
+    ["Coolness"] = {["Information"] = [[+30% <font color="rgb(84,252,124)"><b>Luck</b></font>]]},
+    ["Subzero"] = {
+        ["IsRightGearBuff"] = true,
+        ["Information"] = [[+150% <font color="rgb(84,252,124)"><b>Luck</b></font>, +30% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]],
+        ["IsGearBuff"] = true
+    },
+    ["Cosmic Enhancement"] = {
+        ["IsRightGearBuff"] = true,
+        ["Information"] = [[+250% <font color="rgb(84,252,124)"><b>Luck</b></font>, +30% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]],
+        ["IsGearBuff"] = true
+    },
+    ["Rainbow Ice"] = {
+        ["Information"] = [[+100% <font color="rgb(84,252,124)"><b>Luck</b></font>, +25% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]]
+    },
+    ["Sea Blessing+"] = {
+        ["IsRightGearBuff"] = false,
+        ["Information"] = [[2 -> <font color="rgb(255,220,96)"><b>3</b></font> <font color="rgb(75,123,255)"><b>Bonus Roll Multiply</b></font><br/>10 -> <font color="rgb(255,220,96)"><b>5</b></font> <font color="#6093FF"><b>Bonus Roll Countdown</b></font>]],
+        ["IsGearBuff"] = true
+    },
+    ["Tide"] = {
+        ["IsRightGearBuff"] = false,
+        ["Information"] = [[Every <b><font color="rgb(255,208,82)">6th Roll</font></b> makes me feel like it's <font color="rgb(82,133,255)"><b>Raining</b></font>.]],
+        ["IsGearBuff"] = true
+    },
     ["Heavenly II"] = {
         ["SaveBuffData"] = true,
-        ["EffectPerBuff"] = 200000,
-        ["Information"] = "Luck +%s on the next roll"
+        ["EffectPerBuff"] = 1,
+        ["Information"] = [[<font color="rgb(175,75,255)">+200,000</font> <font color="rgb(84,252,124)"><b>Luck</b></font> for <b><font color="rgb(255,53,44)"><u>Next %s Roll(s)</u></font></b>]]
     },
-    ["Subzero"] = {["Information"] = "+150% Luck, -30% Roll cooltime"},
-    ["Cosmic Enhancement"] = {["Information"] = "+250% Luck, -30% Roll cooltime"},
-    ["Haste III"] = {["EffectPerBuff"] = -70, ["Information"] = "%s%% Roll cooltime"},
-    ["Easter Blessing"] = {["EffectPerBuff"] = 0.5, ["Information"] = "+%s Luck"},
-    ["Haste II"] = {["EffectPerBuff"] = -50, ["Information"] = "%s%% Roll cooltime"},
-    ["We're sorry!"] = {["EffectPerBuff"] = 1, ["Information"] = "+%s Luck"},
-    ["Jackpot"] = {["Information"] = "+77% Luck, -7% Roll cooltime"},
-    ["Haste I"] = {["EffectPerBuff"] = -30, ["Information"] = "%s%% Roll cooltime"},
-    ["Exo Enhancement"] = {["Information"] = "+100% Luck, -20% Roll cooltime"},
-    ["Solar Ignite"] = {["EffectPerBuff"] = 0.5, ["Information"] = "+%s Luck"},
-    ["Galestorm"] = {["Information"] = "+115% Luck, -25% Roll cooltime"},
-    ["Developer's Return"] = {["EffectPerBuff"] = 5, ["Information"] = "+%s Luck"},
-    ["Christmas Blessing"] = {["EffectPerBuff"] = 1, ["Information"] = "+%s Luck"},
-    ["Developer's Blessing?"] = {["EffectPerBuff"] = -99.99, ["Information"] = "%s Luck"},
-    ["Basic Blessing"] = {["EffectPerBuff"] = 0.3, ["Information"] = "+%s Luck"},
-    ["Lunar Shift"] = {["EffectPerBuff"] = -15, ["Information"] = "%s%% Roll cooltime"},
-    ["Fortune II"] = {["EffectPerBuff"] = 2, ["Information"] = "+%s Luck"}
+    ["Warmth?"] = {["EffectPerBuff"] = 20, ["Information"] = [[+%s%% <font color="rgb(84,252,124)"><b>Luck</b></font>]]},
+    ["Oblivion"] = {
+        ["SaveBuffData"] = true,
+        ["EffectPerBuff"] = 1,
+        ["Information"] = [[ +500,000 <font color="rgb(175,75,255)"><b>Luck</b></font> for <b><font color="rgb(255,53,44)"><u>Next %s Roll(s)</u></font></b><br />Also allows you to obtain <font color="rgb(9,9,9)"><stroke color="#BD00FF" joins="miter" thickness="1.5" transparency="0.25"><b>Special Auras</b></stroke></font>]]
+    },
+    ["Rune of Hell"] = {
+        ["Information"] = [[Allows you to roll <font color="rgb(255, 74, 54)"><b>Hell Auras</b></font> with <font color="rgb(255,145,115)"><b>Native Rarity</b></font> <font color="rgb(63,255,123)"><b><u>Regardless</u></b></font> of <b><font color="rgb(255,177,118)">Current Biome</font></b>]]
+    },
+    ["Bank's Blessing"] = {["Information"] = [[+7% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]]},
+    ["GODLIKE"] = {
+        ["Information"] = [[+250% <font color="rgb(84,252,124)"><b>Luck</b></font>, +40% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]]
+    },
+    ["Haste II"] = {
+        ["EffectPerBuff"] = 50,
+        ["Information"] = [[+%s%% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]]
+    },
+    ["Tip's Blessing"] = {
+        ["EffectPerBuff"] = 120,
+        ["Information"] = [[+%s%% <font color="rgb(84,252,124)"><b>Luck</b></font>]]
+    },
+    ["Rune of Frost"] = {
+        ["Information"] = [[Allows you to roll <font color="rgb(170, 255, 255)"><b>Snowy Auras</b></font> with <font color="rgb(255,145,115)"><b>Native Rarity</b></font> <font color="rgb(63,255,123)"><b><u>Regardless</u></b></font> of <b><font color="rgb(255,177,118)">Current Biome</font></b>]]
+    },
+    ["Developer's Blessing?"] = {
+        ["EffectPerBuff"] = -99.99,
+        ["Information"] = [[%s <font color="rgb(84,252,124)"><b>Luck</b></font>]]
+    },
+    ["Lunar Shift"] = {
+        ["IsRightGearBuff"] = true,
+        ["Information"] = [[+15% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]],
+        ["IsGearBuff"] = true
+    },
+    ["The Wrath I"] = {
+        ["Information"] = [[<font color="rgb(255,28,51)">-50%</font> <font color="rgb(84,252,124)"><b>Luck</b></font>]]
+    },
+    ["Basic Blessing"] = {
+        ["EffectPerBuff"] = 30,
+        ["Information"] = [[+%s%% <font color="rgb(84,252,124)"><b>Luck</b></font>]]
+    },
+    ["We're sorry!"] = {
+        ["EffectPerBuff"] = 125,
+        ["Information"] = [[+%s%% <font color="rgb(84,252,124)"><b>Luck</b></font>]]
+    },
+    ["Christmas Blessing"] = {
+        ["EffectPerBuff"] = 100,
+        ["Information"] = [[+%s%% <font color="rgb(84,252,124)"><b>Luck</b></font>]]
+    },
+    ["The Sloth I"] = {
+        ["Information"] = [[<font color="rgb(255,28,51)">-15%</font> <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]]
+    },
+    ["Basic Luck"] = {
+        ["EffectPerBuff"] = 25,
+        ["Information"] = [[+%s%% <font color="rgb(84,252,124)"><b>Luck</b></font>]]
+    },
+    ["Rune of Nothing"] = {
+        ["Information"] = [[Allows you to roll <font color="#252525"><b><stroke color="#FFFFFF" joins="miter" thickness="1" transparency="0.1">Null Auras</stroke></b></font> with <font color="rgb(255,145,115)"><b>Native Rarity</b></font> <font color="rgb(63,255,123)"><b><u>Regardless</u></b></font> of <b><font color="rgb(255,177,118)">Current Biome</font></b>]]
+    },
+    ["Rune of Rainstorm"] = {
+        ["Information"] = [[Allows you to roll <font color="rgb(54, 101, 255)"><b>Rainy Auras</b></font> with <font color="rgb(255,145,115)"><b>Native Rarity</b></font> <font color="rgb(63,255,123)"><b><u>Regardless</u></b></font> of <b><font color="rgb(255,177,118)">Current Biome</font></b>]]
+    },
+    ["Volcanic"] = {
+        ["IsRightGearBuff"] = true,
+        ["Information"] = [[+290% <font color="rgb(84,252,124)"><b>Luck</b></font>, +35% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]],
+        ["IsGearBuff"] = true
+    },
+    ["VIP Luck"] = {["Information"] = [[× 1.2 <font color="rgb(255,229,136)"><b>Final Luck</b></font>]]},
+    ["Rune of Galaxy"] = {
+        ["Information"] = [[Allows you to roll <font color="#6038FF"><b>Starfall Auras</b></font> with <font color="rgb(255,145,115)"><b>Native Rarity</b></font> <font color="rgb(63,255,123)"><b><u>Regardless</u></b></font> of <b><font color="rgb(255,177,118)">Current Biome</font></b>]]
+    },
+    ["The Knowledge I"] = {["Information"] = [[+30% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]]},
+    ["Lucky Penny"] = {["Information"] = [[+77% <font color="rgb(84,252,124)"><b>Luck</b></font>]]},
+    ["Unstable Gravity"] = {
+        ["IsRightGearBuff"] = false,
+        ["Information"] = [[2 -> <font color="rgb(255,220,96)"><b>6</b></font> <font color="rgb(75,123,255)"><b>Bonus Roll Multiply</b></font>]],
+        ["IsGearBuff"] = true
+    },
+    ["Haste I"] = {
+        ["EffectPerBuff"] = 30,
+        ["Information"] = [[+%s%% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]]
+    },
+    ["Fortune I"] = {
+        ["EffectPerBuff"] = 150,
+        ["Information"] = [[+%s%% <font color="rgb(84,252,124)"><b>Luck</b></font>]]
+    },
+    ["Skip"] = {
+        ["SaveBuffData"] = true,
+        ["EffectPerBuff"] = 1,
+        ["Information"] = [[<b><font color="rgb(186,255,233)">0</font></b> <font color="rgb(56,255,191)"><b>Roll Cooltime</b></font> for <b><font color="rgb(255,53,44)"><u>Next %s Roll(s)</u></font></b>]]
+    },
+    ["Rune of Wind"] = {
+        ["Information"] = [[Allows you to roll <font color="rgb(115, 250, 178)"><b>Windy Auras</b></font> with <font color="rgb(255,145,115)"><b>Native Rarity</b></font> <font color="rgb(63,255,123)"><b><u>Regardless</u></b></font> of <b><font color="rgb(255,177,118)">Current Biome</font></b>]]
+    },
+    ["Rune of Corruption"] = {
+        ["Information"] = [[Allows you to roll <font color="rgb(167, 102, 227)"><b>Corruption Auras</b></font> with <font color="rgb(255,145,115)"><b>Native Rarity</b></font> <font color="rgb(63,255,123)"><b><u>Regardless</u></b></font> of <b><font color="rgb(255,177,118)">Current Biome</font></b>]]
+    },
+    ["Easter Blessing"] = {
+        ["EffectPerBuff"] = 50,
+        ["Information"] = [[+%s%% <font color="rgb(84,252,124)"><b>Luck</b></font>]]
+    },
+    ["The Chosen"] = {
+        ["Information"] = [[+150% <font color="rgb(84,252,124)"><b>Luck</b></font>, +30% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]]
+    },
+    ["The Power II"] = {["Information"] = [[+250% <font color="rgb(84,252,124)"><b>Luck</b></font>]]},
+    ["Jackpot"] = {
+        ["IsRightGearBuff"] = false,
+        ["Information"] = [[+77% <font color="rgb(84,252,124)"><b>Luck</b></font>, +7% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]],
+        ["IsGearBuff"] = true
+    },
+    ["Universe I"] = {["Information"] = "Aura in Starfall Unaffected by Breakthrough"},
+    ["Exo Enhancement"] = {
+        ["IsRightGearBuff"] = true,
+        ["Information"] = [[+100% <font color="rgb(84,252,124)"><b>Luck</b></font>, +20% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]],
+        ["IsGearBuff"] = true
+    },
+    ["Solar Ignite"] = {
+        ["IsRightGearBuff"] = true,
+        ["Information"] = [[+50% <font color="rgb(84,252,124)"><b>Luck</b></font>]],
+        ["IsGearBuff"] = true
+    },
+    ["The Wrath II"] = {
+        ["Information"] = [[<font color="rgb(255,28,51)">-100%</font> <font color="rgb(84,252,124)"><b>Luck</b></font>]]
+    },
+    ["Developer's Return"] = {
+        ["EffectPerBuff"] = 5,
+        ["Information"] = [[<font color="rgb(146,255,80)">+%s</font> <font color="rgb(84,252,124)"><b>Luck</b></font>]]
+    },
+    ["Fierce Spirit"] = {
+        ["IsRightGearBuff"] = false,
+        ["Information"] = [[<b><font color="rgb(75,123,255)">Bonus Roll</font></b> on <b><font color="#AD65FF">Every Rolls</font></b>,<br/><b><font color="rgb(75,123,255)">Bonus Roll Multiplier</font></b> <font color="rgb(167,51,51)"><b>Reduced</b></font> to <font color="#FFAB81"><b>1.35</b></font>]],
+        ["IsGearBuff"] = true
+    },
+    ["The Power I"] = {["Information"] = [[+150% <font color="rgb(84,252,124)"><b>Luck</b></font>]]},
+    ["The Sloth II"] = {
+        ["Information"] = [[<font color="rgb(255,28,51)">-25%</font> <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]]
+    },
+    ["Haste III"] = {
+        ["EffectPerBuff"] = 70,
+        ["Information"] = [[+%s%% <font color="rgb(84,252,205)"><b>Roll Speed</b></font>]]
+    },
+    ["Fortune II"] = {
+        ["EffectPerBuff"] = 200,
+        ["Information"] = [[+%s%% <font color="rgb(84,252,124)"><b>Luck</b></font>]]
+    }
 }
