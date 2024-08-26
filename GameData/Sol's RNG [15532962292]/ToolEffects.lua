c118d69ac6a6b2c63944fe671610ffdb234652d2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-27 01:58:10 +1000

    Update ToolEffects.lua

--- GameData/Sol's RNG [15532962292]/ToolEffects.lua
+++ GameData/Sol's RNG [15532962292]/ToolEffects.lua
@@ -1,14 +1,19 @@
 data = {
-    ["Luck Glove"] = {["Equipped"] = {"Basic Luck"}},
+    ["Tide Gauntlet"] = {["Equipped"] = {"Sea Blessing", "Tide"}},
     ["Solar Device"] = {["Equipped"] = {"Solar Ignite"}},
     ["Subzero Device"] = {["Equipped"] = {"Subzero"}},
+    ["Lucky Potion"] = {["Activated"] = {["Buffs"] = {{["Duration"] = 60, ["BuffName"] = "Lucky", ["Stack"] = 1}}}},
+    ["Lunar Device"] = {["Equipped"] = {"Lunar Shift"}},
+    ["Exoflex Device"] = {["Equipped"] = {"Exoflex Blessing"}},
+    ["Luck Glove"] = {["Equipped"] = {"Basic Luck"}},
     ["Gravitational Device"] = {["Equipped"] = {"Unstable Gravity"}},
     ["Jackpot Gauntlet"] = {["Equipped"] = {"Jackpot"}},
     ["Windstorm Device"] = {["Equipped"] = {"Galestorm"}},
     ["Exo Gauntlet"] = {["Equipped"] = {"Exo Enhancement"}},
-    ["Lucky Potion"] = {["Activated"] = {["Buffs"] = {{["Duration"] = 60, ["BuffName"] = "Lucky", ["Stack"] = 1}}}},
     ["Speed Potion"] = {["Activated"] = {["Buffs"] = {{["Duration"] = 30, ["BuffName"] = "QuickRoll", ["Stack"] = 1}}}},
-    ["Lunar Device"] = {["Equipped"] = {"Lunar Shift"}},
+    ["Blessed Tide Gauntlet"] = {["Equipped"] = {"Sea Blessing+", "Tide"}},
+    ["Flesh Device"] = {["Equipped"] = {"Fierce Spirit"}},
+    ["Volcanic Device"] = {["Equipped"] = {"Volcanic"}},
     ["Galactic Device"] = {["Equipped"] = {"Cosmic Enhancement"}},
     ["Eclipse Device"] = {["Equipped"] = {"Solar Ignite", "Lunar Shift"}}
 }
