a6bf36bd5d9bb3bdb050c7d524d2f35bf95532e1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-27 02:41:29 +1000

    Create Auras.lua

---
+++ GameData/Dungeon Quest [2414851778]/Auras.lua
@@ -0,0 +1,28 @@
+--require(game:GetService("ReplicatedStorage").Utility.DataRequester).GetAuras()
+data = {
+    ["Raindrop Aura"] = {["imageId"] = "rbxassetid://17757366871", ["rarity"] = "uncommon", ["name"] = "Raindrop Aura"},
+    ["Shadow"] = {["imageId"] = "rbxassetid://18863961953", ["rarity"] = "rare", ["name"] = "Shadow"},
+    ["Ghost"] = {["imageId"] = "rbxassetid://16582967612", ["rarity"] = "uncommon", ["name"] = "Ghost"},
+    ["Holographic"] = {["imageId"] = "rbxassetid://18346521866", ["rarity"] = "legendary", ["name"] = "Holographic"},
+    ["Pulse"] = {["imageId"] = "rbxassetid://16582967612", ["rarity"] = "legendary", ["name"] = "Pulse"},
+    ["Cyclone Aura"] = {["imageId"] = "rbxassetid://17757282234", ["rarity"] = "uncommon", ["name"] = "Cyclone Aura"},
+    ["Lumen Flame"] = {["imageId"] = "rbxassetid://18863973373", ["rarity"] = "legendary", ["name"] = "Lumen Flame"},
+    ["Waterlord"] = {["imageId"] = "rbxassetid://17756245417", ["rarity"] = "epic", ["name"] = "Waterlord"},
+    ["Windlord"] = {["imageId"] = "rbxassetid://17756244896", ["rarity"] = "epic", ["name"] = "Windlord"},
+    ["Steam"] = {["imageId"] = "rbxassetid://18346510095", ["rarity"] = "rare", ["name"] = "Steam"},
+    ["Abyssal"] = {["imageId"] = "rbxassetid://18863976763", ["rarity"] = "legendary", ["name"] = "Abyssal"},
+    ["Holy"] = {["imageId"] = "rbxassetid://18863963131", ["rarity"] = "rare", ["name"] = "Holy"},
+    ["Thunderlord"] = {["imageId"] = "rbxassetid://17756245840", ["rarity"] = "epic", ["name"] = "Thunderlord"},
+    ["Spark Aura"] = {["imageId"] = "rbxassetid://17757388455", ["rarity"] = "uncommon", ["name"] = "Spark Aura"},
+    ["Astral"] = {["imageId"] = "rbxassetid://18346498016", ["rarity"] = "rare", ["name"] = "Astral"},
+    ["Shadowed"] = {["imageId"] = "rbxassetid://18863961953", ["rarity"] = "rare", ["name"] = "Shadowed"},
+    ["Ice"] = {["imageId"] = "rbxassetid://16582967612", ["rarity"] = "rare", ["name"] = "Ice"},
+    ["Shadow Lord"] = {["imageId"] = "rbxassetid://16582967612", ["rarity"] = "epic", ["name"] = "Shadow Lord"},
+    ["Ember Aura"] = {["imageId"] = "rbxassetid://17757315322", ["rarity"] = "uncommon", ["name"] = "Ember Aura"},
+    ["Supreme Dungeoneer"] = {
+        ["imageId"] = "rbxassetid://18346514295",
+        ["rarity"] = "legendary",
+        ["name"] = "Supreme Dungeoneer"
+    },
+    ["Firelord"] = {["imageId"] = "rbxassetid://17756246036", ["rarity"] = "epic", ["name"] = "Firelord"}
+}
