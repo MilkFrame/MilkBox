a8bc00007dabb70aeb7bdf800103e01295c5e26f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 04:02:58 +1000

    Create SuspensionList.lua

---
+++ GameData/Taxi Boss [7305309231]/SuspensionList.lua
@@ -0,0 +1,5 @@
+data = {
+    {["price"] = 0, ["name"] = "Stock", ["height"] = 0},
+    {["price"] = 100, ["name"] = "Lowered", ["height"] = -0.1},
+    {["price"] = 100, ["name"] = "Lifted", ["height"] = 0.15}
+}
