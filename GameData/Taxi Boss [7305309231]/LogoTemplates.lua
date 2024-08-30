31b8f115562828894fd337d840d3916195e774f0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 03:59:30 +1000

    Create LogoTemplates.lua

---
+++ GameData/Taxi Boss [7305309231]/LogoTemplates.lua
@@ -0,0 +1,38 @@
+data = {
+    {
+        ["font"] = 26,
+        ["name"] = "name",
+        ["textPos"] = 0.2,
+        ["color"] = Color3.fromRGB(62, 68, 93),
+        ["iconPos"] = 0.65,
+        ["icon"] = "🚖",
+        ["textColor"] = Color3.fromRGB(255, 226, 81)
+    },
+    {
+        ["font"] = 20,
+        ["name"] = "name",
+        ["textPos"] = 0.85,
+        ["color"] = Color3.fromRGB(232, 228, 234),
+        ["iconPos"] = 0.45,
+        ["icon"] = "🌐",
+        ["textColor"] = Color3.fromRGB(46, 129, 183)
+    },
+    {
+        ["font"] = 6,
+        ["name"] = "name",
+        ["textPos"] = 0.3,
+        ["color"] = Color3.fromRGB(67, 69, 84),
+        ["iconPos"] = 0.6,
+        ["icon"] = "🚕",
+        ["textColor"] = Color3.fromRGB(255, 255, 255)
+    },
+    {
+        ["font"] = 12,
+        ["name"] = "name",
+        ["textPos"] = 0.85,
+        ["color"] = Color3.fromRGB(40, 45, 76),
+        ["iconPos"] = 0.45,
+        ["icon"] = "🌍",
+        ["textColor"] = Color3.fromRGB(255, 255, 255)
+    }
+}
