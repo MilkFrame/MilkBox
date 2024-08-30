f33edd343454b40e993fe1631cf50f46432322c2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 03:57:55 +1000

    Create AICarModels.lua

---
+++ GameData/Taxi Boss [7305309231]/AICarModels.lua
@@ -0,0 +1,109 @@
+data = {
+    {
+        ["colors"] = {
+            Color3.fromRGB(204, 204, 204),
+            Color3.fromRGB(178, 204, 204),
+            Color3.fromRGB(207, 201, 189),
+            Color3.fromRGB(209, 202, 179),
+            Color3.fromRGB(179, 179, 179),
+            Color3.fromRGB(153, 153, 153),
+            Color3.fromRGB(126, 126, 126),
+            Color3.fromRGB(75, 75, 75),
+            Color3.fromRGB(51, 51, 51),
+            Color3.fromRGB(128, 0, 2),
+            Color3.fromRGB(204, 51, 51),
+            Color3.fromRGB(51, 102, 255),
+            Color3.fromRGB(51, 51, 102),
+            Color3.fromRGB(53, 72, 55),
+            Color3.fromRGB(106, 132, 120),
+            Color3.fromRGB(62, 107, 132),
+            Color3.fromRGB(132, 108, 109)
+        },
+        ["model"] = "BoxTruck",
+        ["length"] = 18.7,
+        ["wheelSize"] = 2.230001926422119,
+        ["width"] = 7.85
+    },
+    {
+        ["colors"] = {
+            Color3.fromRGB(204, 204, 204),
+            Color3.fromRGB(178, 204, 204),
+            Color3.fromRGB(207, 201, 189),
+            Color3.fromRGB(209, 202, 179),
+            Color3.fromRGB(179, 179, 179),
+            Color3.fromRGB(153, 153, 153),
+            Color3.fromRGB(126, 126, 126),
+            Color3.fromRGB(75, 75, 75),
+            Color3.fromRGB(51, 51, 51),
+            Color3.fromRGB(128, 0, 2),
+            Color3.fromRGB(204, 51, 51),
+            Color3.fromRGB(51, 102, 255),
+            Color3.fromRGB(51, 51, 102),
+            Color3.fromRGB(53, 72, 55),
+            Color3.fromRGB(106, 132, 120),
+            Color3.fromRGB(62, 107, 132),
+            Color3.fromRGB(132, 108, 109)
+        },
+        ["model"] = "TinyCar",
+        ["length"] = 14.7,
+        ["wheelSize"] = 2.0265004634857178,
+        ["width"] = 6.7
+    },
+    {
+        ["colors"] = {
+            Color3.fromRGB(204, 204, 204),
+            Color3.fromRGB(178, 204, 204),
+            Color3.fromRGB(207, 201, 189),
+            Color3.fromRGB(209, 202, 179),
+            Color3.fromRGB(179, 179, 179),
+            Color3.fromRGB(153, 153, 153),
+            Color3.fromRGB(126, 126, 126),
+            Color3.fromRGB(75, 75, 75),
+            Color3.fromRGB(51, 51, 51),
+            Color3.fromRGB(128, 0, 2),
+            Color3.fromRGB(204, 51, 51),
+            Color3.fromRGB(51, 102, 255),
+            Color3.fromRGB(51, 51, 102),
+            Color3.fromRGB(53, 72, 55),
+            Color3.fromRGB(106, 132, 120),
+            Color3.fromRGB(62, 107, 132),
+            Color3.fromRGB(132, 108, 109)
+        },
+        ["model"] = "OldSuv",
+        ["length"] = 15.3,
+        ["wheelSize"] = 2.1855993270874023,
+        ["width"] = 7
+    },
+    {
+        ["colors"] = {
+            Color3.fromRGB(204, 204, 204),
+            Color3.fromRGB(178, 204, 204),
+            Color3.fromRGB(207, 201, 189),
+            Color3.fromRGB(209, 202, 179),
+            Color3.fromRGB(179, 179, 179),
+            Color3.fromRGB(153, 153, 153),
+            Color3.fromRGB(126, 126, 126),
+            Color3.fromRGB(75, 75, 75),
+            Color3.fromRGB(51, 51, 51),
+            Color3.fromRGB(128, 0, 2),
+            Color3.fromRGB(204, 51, 51),
+            Color3.fromRGB(51, 102, 255),
+            Color3.fromRGB(51, 51, 102),
+            Color3.fromRGB(53, 72, 55),
+            Color3.fromRGB(106, 132, 120),
+            Color3.fromRGB(62, 107, 132),
+            Color3.fromRGB(132, 108, 109)
+        },
+        ["model"] = "Sedan",
+        ["length"] = 16.4,
+        ["wheelSize"] = 2.223010540008545,
+        ["width"] = 6.5
+    },
+    {
+        ["colors"] = {Color3.fromRGB(239, 239, 239)},
+        ["model"] = "Police",
+        ["length"] = 16.4,
+        ["wheelSize"] = 2.4450106620788574,
+        ["width"] = 6.5
+    }
+}
