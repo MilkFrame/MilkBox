e74257face9a21175efc228c7aafab7f0d5bf7e8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 04:03:33 +1000

    Create PosterList.lua

---
+++ GameData/Taxi Boss [7305309231]/PosterList.lua
@@ -0,0 +1,8 @@
+data = {
+    {["Y"] = 0, ["X"] = 0, ["name"] = "empty", ["texture"] = ""},
+    {["Y"] = 3, ["X"] = 2, ["name"] = "Jet Speed", ["texture"] = "rbxassetid://7303288110"},
+    {["Y"] = 3, ["X"] = 2, ["name"] = "Car Service", ["texture"] = "rbxassetid://7303293420"},
+    {["Y"] = 3, ["X"] = 2, ["name"] = "Classic", ["texture"] = "rbxassetid://7303295739"},
+    {["Y"] = 2.25, ["X"] = 4, ["name"] = "Taxi Boss", ["texture"] = "rbxassetid://7929334709"},
+    {["Y"] = 2.25, ["X"] = 4, ["name"] = "Villa", ["texture"] = "rbxassetid://8034168669"}
+}
