c86838889fa51d9cc33c9c877ffc17735aa6ab90
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 04:00:49 +1000

    Create Christmas.lua

---
+++ GameData/Taxi Boss [7305309231]/Christmas.lua
@@ -0,0 +1,79 @@
+data = {
+    ["Rewards"] = {
+        {
+            ["duration"] = 179,
+            ["weight"] = 9.6,
+            ["id"] = "2xboost",
+            ["text"] = "2x🍬 (3min)",
+            ["multiplier"] = 2,
+            ["boost"] = "candy"
+        },
+        {
+            ["duration"] = 1199,
+            ["weight"] = 6,
+            ["id"] = "2xboost",
+            ["text"] = "2x🍬 (20min)",
+            ["multiplier"] = 2,
+            ["boost"] = "candy"
+        },
+        {["id"] = "candy1", ["text"] = "🍬 25 candy", ["candy"] = 25, ["weight"] = 11},
+        {["id"] = "candy2", ["text"] = "🍬 100 candy", ["candy"] = 100, ["weight"] = 10},
+        {["id"] = "candy2", ["text"] = "🍬 1000 candy", ["candy"] = 1000, ["weight"] = 2},
+        {["id"] = "pontec", ["text"] = "🚗 Pontec Firetran", ["carId"] = 89, ["weight"] = 0.25},
+        {["id"] = "crazystang", ["text"] = "🚗 Crazystang", ["carId"] = 88, ["weight"] = 0.01},
+        {["id"] = "austin", ["text"] = "🚗 Austin 077", ["carId"] = 215, ["weight"] = 0.2},
+        {["weight"] = 4.5, ["id"] = "hotrodWheels", ["text"] = "! HotRod Wheels", ["unlockable"] = true, ["wheel"] = 16},
+        {["weight"] = 3, ["id"] = "perfectWheels", ["text"] = "! P Perfect", ["unlockable"] = true, ["wheel"] = 53},
+        {["weight"] = 3, ["id"] = "pumpkin", ["text"] = "🎃Pumpkin", ["sign"] = 6, ["unlockable"] = true},
+        {["weight"] = 4, ["id"] = "pumpkin2", ["text"] = "🎃Pumpkin Taxi Sign", ["sign"] = 7, ["unlockable"] = true},
+        {["id"] = "moneyBasic1", ["text"] = "$1000", ["money"] = 1000, ["weight"] = 11},
+        {["id"] = "moneyBasic2", ["text"] = "$2500", ["money"] = 2500, ["weight"] = 14.7},
+        {["id"] = "moneyBasic3", ["text"] = "$50000", ["money"] = 50000, ["weight"] = 4}
+    },
+    ["PresentRewards"] = {
+        ["epic"] = {
+            {["id"] = "bnwakg", ["text"] = "🚗 BNW W1 AKG", ["carId"] = 126, ["weight"] = 15},
+            {["id"] = "grantt", ["text"] = "🚗 McLeren GrandT", ["carId"] = 121, ["weight"] = 20},
+            {["id"] = "alpha166", ["text"] = "🚗 Alpha 166", ["carId"] = 129, ["weight"] = 20},
+            {
+                ["weight"] = 20,
+                ["id"] = "iceflake",
+                ["text"] = "❄️ Iceflake wheels",
+                ["unlockable"] = true,
+                ["wheel"] = 21
+            },
+            {["id"] = "moneyEpic1", ["text"] = "$10000", ["money"] = 10000, ["weight"] = 15},
+            {["id"] = "moneyEpic2", ["text"] = "$5000", ["money"] = 5000, ["weight"] = 10}
+        },
+        ["legend"] = {
+            {["id"] = "civil", ["text"] = "🚗 Hunda Civil", ["carId"] = 128, ["weight"] = 5},
+            {["id"] = "abort", ["text"] = "🚗 Abort 500", ["carId"] = 127, ["weight"] = 5},
+            {["id"] = "alpha166", ["text"] = "🚗 Alpha 166", ["carId"] = 129, ["weight"] = 5},
+            {["id"] = "cory", ["text"] = "🚗 Chevelle Cory Z07", ["carId"] = 118, ["weight"] = 3},
+            {["id"] = "bnwakg", ["text"] = "🚗 BNW W1 AKG", ["carId"] = 126, ["weight"] = 2},
+            {["id"] = "homey", ["text"] = "🚗 BNW HOMEY", ["carId"] = 130, ["weight"] = 1.5},
+            {["id"] = "battista", ["text"] = "🚗 Pinata Bat", ["carId"] = 221, ["weight"] = 0.3},
+            {["weight"] = 4, ["id"] = "snowman", ["text"] = "⛄ Snowman wheels", ["unlockable"] = true, ["wheel"] = 22},
+            {["weight"] = 6, ["id"] = "iceflake", ["text"] = "⛄ Iceflake", ["unlockable"] = true, ["wheel"] = 21},
+            {["weight"] = 8, ["id"] = "sharp", ["text"] = "⛄ Sharp Ice", ["unlockable"] = true, ["wheel"] = 18},
+            {["weight"] = 10, ["id"] = "sledge", ["text"] = "🎁 Sledge", ["sign"] = 10, ["unlockable"] = true},
+            {["weight"] = 6, ["id"] = "santaHat", ["text"] = "🎅 Santa Hat", ["sign"] = 9, ["unlockable"] = true},
+            {["weight"] = 4, ["id"] = "presenthat", ["text"] = "🎁 Present Hat", ["sign"] = 14, ["unlockable"] = true},
+            {["id"] = "money1", ["text"] = "$30000", ["money"] = 30000, ["weight"] = 3},
+            {["id"] = "money2", ["text"] = "$1000", ["money"] = 1000, ["weight"] = 5},
+            {["id"] = "money3", ["text"] = "$3000", ["money"] = 3000, ["weight"] = 12},
+            {["id"] = "money4", ["text"] = "$10000", ["money"] = 10000, ["weight"] = 10},
+            {["id"] = "money5", ["text"] = "$15000", ["money"] = 15000, ["weight"] = 6},
+            {["id"] = "money6", ["text"] = "$20000", ["money"] = 20000, ["weight"] = 4}
+        },
+        ["basic"] = {
+            {["id"] = "vovo", ["text"] = "🚗 Vovo s66 racer", ["carId"] = 120, ["weight"] = 15},
+            {["id"] = "abort", ["text"] = "🚗 Abort 500", ["carId"] = 127, ["weight"] = 15},
+            {["id"] = "civil", ["text"] = "🚗 Hunda Civil", ["carId"] = 128, ["weight"] = 20},
+            {["weight"] = 15, ["id"] = "sharp", ["text"] = "❄️ Sharp Ice", ["unlockable"] = true, ["wheel"] = 18},
+            {["id"] = "moneyBasic1", ["text"] = "$1000", ["money"] = 1000, ["weight"] = 15},
+            {["id"] = "moneyBasic2", ["text"] = "$2500", ["money"] = 2500, ["weight"] = 15},
+            {["id"] = "moneyBasic3", ["text"] = "$100", ["money"] = 100, ["weight"] = 5}
+        }
+    }
+}
