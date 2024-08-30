1723a3649c1629f811ca49eb21716780887b525d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 03:59:09 +1000

    Create DriverStats.lua

---
+++ GameData/Taxi Boss [7305309231]/DriverStats.lua
@@ -0,0 +1,26 @@
+data = {
+    {
+        ["prices"] = {500, 1500, 9000, 40000, 165000},
+        ["name"] = "Experience",
+        ["times"] = {30, 120, 360, 1200, 3360},
+        ["defMaxRating"] = 3,
+        ["maxRating"] = {4, 5, 7, 9, 12},
+        ["description"] = "Drive Better Cars"
+    },
+    {
+        ["prices"] = {300, 2500, 8000, 25000, 95000},
+        ["name"] = "Skill",
+        ["times"] = {30, 90, 300, 600, 3000},
+        ["defMaxPower"] = 250,
+        ["maxPower"] = {350, 500, 700, 1000, 3000},
+        ["description"] = "Drive Faster Cars"
+    },
+    {
+        ["prices"] = {400, 4000, 15000, 50000, 165000},
+        ["defCashBonus"] = 1,
+        ["name"] = "Tips",
+        ["times"] = {60, 120, 600, 1200, 2400},
+        ["cashBonus"] = {1.1, 1.2, 1.3, 1.4, 1.5},
+        ["description"] = "Earn More Money"
+    }
+}
