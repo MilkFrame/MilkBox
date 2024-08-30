1e3ee7b77efd5d0e5524f380272f3a92e4586765
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 04:01:56 +1000

    Create TintList.lua

---
+++ GameData/Taxi Boss [7305309231]/TintList.lua
@@ -0,0 +1,30 @@
+data = {
+    {
+        ["transparency"] = 0.4,
+        ["name"] = "85% tint",
+        ["price"] = 0,
+        ["reflectance"] = 0.05,
+        ["color"] = Color3.fromRGB(86, 97, 102)
+    },
+    {
+        ["transparency"] = 0.3,
+        ["name"] = "70% tint",
+        ["price"] = 50,
+        ["reflectance"] = 0.05,
+        ["color"] = Color3.fromRGB(50, 57, 60)
+    },
+    {
+        ["transparency"] = 0.2,
+        ["name"] = "25% tint",
+        ["price"] = 80,
+        ["reflectance"] = 0.05,
+        ["color"] = Color3.fromRGB(43, 48, 51)
+    },
+    {
+        ["transparency"] = 0.1,
+        ["name"] = "5% tint",
+        ["price"] = 100,
+        ["reflectance"] = 0.05,
+        ["color"] = Color3.fromRGB(4, 4, 5)
+    }
+}
