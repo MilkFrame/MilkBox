0fb41a38ed57e8a897452fd836e6d68bb4d59dea
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 04:02:47 +1000

    Create LightsList.lua

---
+++ GameData/Taxi Boss [7305309231]/LightsList.lua
@@ -0,0 +1,12 @@
+data = {
+    {["price"] = 0, ["name"] = "Standard", ["color"] = Color3.fromRGB(255, 244, 141)},
+    {["price"] = 200, ["name"] = "White", ["color"] = Color3.fromRGB(193, 207, 255)},
+    {["price"] = 500, ["name"] = "Xenon", ["color"] = Color3.fromRGB(131, 207, 255)},
+    {["price"] = 0, ["color"] = Color3.fromRGB(253, 164, 233), ["name"] = "Cherry", ["customPass"] = true},
+    {["price"] = 0, ["color"] = Color3.fromRGB(255, 120, 237), ["name"] = "Pink", ["customPass"] = true},
+    {["price"] = 0, ["color"] = Color3.fromRGB(253, 118, 89), ["name"] = "Demon", ["customPass"] = true},
+    {["price"] = 0, ["color"] = Color3.fromRGB(133, 255, 137), ["name"] = "Green", ["customPass"] = true},
+    {["price"] = 0, ["color"] = Color3.fromRGB(73, 125, 255), ["name"] = "Blue", ["customPass"] = true},
+    {["price"] = 0, ["color"] = Color3.fromRGB(255, 187, 0), ["name"] = "Yellow", ["customPass"] = true},
+    {["price"] = 0, ["color"] = Color3.fromRGB(119, 7, 255), ["name"] = "UV", ["customPass"] = true}
+}
