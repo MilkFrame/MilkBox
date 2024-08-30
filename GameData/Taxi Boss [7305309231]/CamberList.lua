4c0417afc1e4a86b5c0aea5e9aa516883ecde1ac
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 04:05:35 +1000

    Create CamberList.lua

---
+++ GameData/Taxi Boss [7305309231]/CamberList.lua
@@ -0,0 +1,6 @@
+data = {
+    {["price"] = 0, ["angle"] = 0, ["name"] = "Stock"},
+    {["price"] = 0, ["angle"] = 3, ["name"] = "-3 degrees"},
+    {["price"] = 0, ["angle"] = 5, ["name"] = "-5 degrees"},
+    {["price"] = 0, ["angle"] = 10, ["name"] = "-10 degrees"}
+}
