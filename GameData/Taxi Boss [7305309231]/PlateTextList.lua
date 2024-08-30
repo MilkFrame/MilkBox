aad6b321310aee19325fdac3e539ccbf8c04ceb9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 04:05:11 +1000

    Create PlateTextList.lua

---
+++ GameData/Taxi Boss [7305309231]/PlateTextList.lua
@@ -0,0 +1,8 @@
+data = {
+    {["price"] = 0, ["text"] = "rating", ["name"] = "Rating"},
+    {["price"] = 0, ["text"] = "VIP", ["name"] = "VIP", ["vipPass"] = true},
+    {["price"] = 0, ["text"] = "TAXI", ["name"] = "TAXI", ["vipPass"] = true},
+    {["price"] = 0, ["text"] = "RBLX", ["name"] = "RBLX", ["vipPass"] = true},
+    {["price"] = 0, ["text"] = "RACE", ["name"] = "RACE", ["vipPass"] = true},
+    {["price"] = 0, ["text"] = ":)", ["name"] = ":)", ["vipPass"] = true}
+}
