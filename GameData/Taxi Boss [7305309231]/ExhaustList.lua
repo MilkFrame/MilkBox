ee9d064dc2abf5c8d681791650af2582ef2319cf
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 04:02:21 +1000

    Create ExhaustList.lua

---
+++ GameData/Taxi Boss [7305309231]/ExhaustList.lua
@@ -0,0 +1,32 @@
+data = {
+    {
+        ["name"] = "Twin Tip",
+        ["model"] = "exhaust1",
+        ["price"] = 250,
+        ["color"] = Color3.fromRGB(82, 82, 85),
+        ["scale"] = Vector3.new(0.3400000035762787, 0.3400000035762787, 0.3400000035762787)
+    },
+    {
+        ["name"] = "Single Tip",
+        ["model"] = "exhaust2",
+        ["price"] = 250,
+        ["color"] = Color3.fromRGB(98, 91, 100),
+        ["scale"] = Vector3.new(0.29499998688697815, 0.29499998688697815, 0.29499998688697815)
+    },
+    {
+        ["name"] = "Turbomo Twin",
+        ["model"] = "exhaust3",
+        ["price"] = 0,
+        ["customPass"] = true,
+        ["color"] = Color3.fromRGB(98, 91, 100),
+        ["scale"] = Vector3.new(0.14000000059604645, 0.14000000059604645, 0.14000000059604645)
+    },
+    {
+        ["name"] = "Bosuzuku",
+        ["model"] = "exhaust4",
+        ["price"] = 0,
+        ["customPass"] = true,
+        ["color"] = Color3.fromRGB(111, 111, 110),
+        ["scale"] = Vector3.new(0.14000000059604645, 0.14000000059604645, 0.14000000059604645)
+    }
+}
