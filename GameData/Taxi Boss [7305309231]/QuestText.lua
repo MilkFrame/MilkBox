d9be96f279a10177129a745bd757b553dcd2b64d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 03:58:56 +1000

    Create QuestText.lua

---
+++ GameData/Taxi Boss [7305309231]/QuestText.lua
@@ -0,0 +1,36 @@
+data = {
+    ["tasks"] = {
+        ["dr"] = "Give a ride to Drive Academy students!",
+        ["ts"] = "Reach a top speed of X",
+        ["pw"] = "Pickup pieces of Wood to your Office",
+        ["pc"] = "Deliver Containers to your Office",
+        ["pt"] = "Play for X minutes",
+        ["db"] = "Deliver Builders to your Office",
+        ["pr"] = "Deliver 2 Crates",
+        ["cs"] = "Deliver Customers",
+        ["ea"] = "Explore area: X"
+    },
+    ["emojis"] = {
+        ["dm"] = "👨‍💼",
+        ["dl"] = "⚖️",
+        ["dv"] = "👨‍🔧",
+        ["pw"] = "🌲",
+        ["de"] = "⚙️",
+        ["pt"] = "⏱",
+        ["db"] = "👷",
+        ["dc"] = "🏎️",
+        ["ea"] = "🗺️",
+        ["ts"] = "💨",
+        ["pm"] = "🕵",
+        ["po"] = "🛢️",
+        ["pn"] = "☢️",
+        ["pb"] = "🛢️",
+        ["pc"] = "🚚",
+        ["bo"] = "💼",
+        ["ps"] = "🏖️",
+        ["pi"] = "🧲",
+        ["dr"] = "🎓",
+        ["cs"] = "🙋‍",
+        ["pr"] = "📦"
+    }
+}
