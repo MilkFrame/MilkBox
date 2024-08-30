51c99ea5b842bdcc765971ddb5ed9d443ebbfe90
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 04:04:49 +1000

    Create ObjectList.lua

---
+++ GameData/Taxi Boss [7305309231]/ObjectList.lua
@@ -0,0 +1,518 @@
+data = {
+    ["objects"] = {
+        ["sofa"] = {
+            ["name"] = "Sofa",
+            ["category"] = "furniture",
+            ["order"] = 101,
+            ["model"] = "sofa",
+            ["modelHeight"] = 3.138927936553955
+        },
+        ["tireStack1"] = {
+            ["name"] = "Tire Stack 1",
+            ["category"] = "garage",
+            ["order"] = 6,
+            ["model"] = "tireStack1",
+            ["modelHeight"] = 4.017529010772705
+        },
+        ["bigDrawer"] = {
+            ["name"] = "Big Drawer 1",
+            ["category"] = "garage",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "garage",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(192, 79, 71)},
+                    {["color1"] = Color3.fromRGB(110, 141, 187)},
+                    {["color1"] = Color3.fromRGB(97, 100, 109)}
+                }
+            },
+            ["model"] = "bigDrawer",
+            ["modelHeight"] = 7.311368942260742
+        },
+        ["flowers"] = {
+            ["name"] = "Flowers",
+            ["category"] = "plants",
+            ["order"] = 101,
+            ["model"] = "flowers",
+            ["modelHeight"] = 3.6179158687591553
+        },
+        ["tableSetup"] = {
+            ["name"] = "Table Setup",
+            ["category"] = "furniture",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "wood",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(175, 148, 131)},
+                    {["color1"] = Color3.fromRGB(124, 92, 70)},
+                    {["color1"] = Color3.fromRGB(156, 115, 87)}
+                }
+            },
+            ["model"] = "tableSetup",
+            ["modelHeight"] = 4.040359020233154
+        },
+        ["motorLift"] = {
+            ["name"] = "Motor Lift",
+            ["category"] = "garage",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "garage",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(192, 79, 71)},
+                    {["color1"] = Color3.fromRGB(110, 141, 187)},
+                    {["color1"] = Color3.fromRGB(97, 100, 109)}
+                }
+            },
+            ["model"] = "motorLift",
+            ["modelHeight"] = 4.799882888793945
+        },
+        ["bigDrawer3"] = {
+            ["name"] = "Big Drawer 3",
+            ["category"] = "garage",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "garage",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(192, 79, 71)},
+                    {["color1"] = Color3.fromRGB(110, 141, 187)},
+                    {["color1"] = Color3.fromRGB(97, 100, 109)}
+                }
+            },
+            ["model"] = "bigDrawer3",
+            ["modelHeight"] = 7.311368465423584
+        },
+        ["poster4"] = {
+            ["name"] = "Poster",
+            ["category"] = "posters",
+            ["wall"] = true,
+            ["model"] = "poster4",
+            ["order"] = 101,
+            ["modelHeight"] = 0.10000000149011612
+        },
+        ["bin"] = {
+            ["name"] = "Bin",
+            ["category"] = "furniture",
+            ["order"] = 101,
+            ["model"] = "bin",
+            ["modelHeight"] = 2.4003119468688965
+        },
+        ["plant"] = {
+            ["name"] = "Plant",
+            ["category"] = "plants",
+            ["order"] = 101,
+            ["model"] = "plant",
+            ["modelHeight"] = 5.09200382232666
+        },
+        ["taxiboss"] = {
+            ["model"] = "taxiboss",
+            ["name"] = "Game Art",
+            ["category"] = "posters",
+            ["wall"] = true,
+            ["order"] = 101,
+            ["unlock"] = {["value"] = 2, ["data"] = "quests", ["key"] = "hr_tutorial"},
+            ["modelHeight"] = 0.10000000149011612
+        },
+        ["radio3"] = {
+            ["name"] = "Stereo",
+            ["category"] = "furniture",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "wood",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(175, 148, 131)},
+                    {["color1"] = Color3.fromRGB(124, 92, 70)},
+                    {["color1"] = Color3.fromRGB(156, 115, 87)}
+                }
+            },
+            ["model"] = "radio3",
+            ["modelHeight"] = 4.332769393920898
+        },
+        ["garagePC"] = {
+            ["name"] = "garage PC",
+            ["category"] = "garage",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "garage",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(192, 79, 71)},
+                    {["color1"] = Color3.fromRGB(110, 141, 187)},
+                    {["color1"] = Color3.fromRGB(97, 100, 109)}
+                }
+            },
+            ["model"] = "garagePC",
+            ["modelHeight"] = 5.356168270111084
+        },
+        ["drawer"] = {
+            ["name"] = "Drawer",
+            ["category"] = "garage",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "garage",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(192, 79, 71)},
+                    {["color1"] = Color3.fromRGB(110, 141, 187)},
+                    {["color1"] = Color3.fromRGB(97, 100, 109)}
+                }
+            },
+            ["model"] = "drawer",
+            ["modelHeight"] = 3.955216884613037
+        },
+        ["tulips"] = {
+            ["name"] = "Tulips",
+            ["category"] = "plants",
+            ["price"] = 0,
+            ["model"] = "tulips",
+            ["order"] = 101,
+            ["modelHeight"] = 3.602926254272461
+        },
+        ["workshop"] = {
+            ["name"] = "Workshop",
+            ["category"] = "garage",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "garage",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(192, 79, 71)},
+                    {["color1"] = Color3.fromRGB(110, 141, 187)},
+                    {["color1"] = Color3.fromRGB(97, 100, 109)}
+                }
+            },
+            ["model"] = "workshop",
+            ["modelHeight"] = 5.302926540374756
+        },
+        ["poster9"] = {
+            ["name"] = "Poster",
+            ["category"] = "posters",
+            ["wall"] = true,
+            ["model"] = "poster9",
+            ["order"] = 101,
+            ["modelHeight"] = 0.09999999403953552
+        },
+        ["bigShelf2"] = {
+            ["name"] = "Big Shelf 2",
+            ["category"] = "garage",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "wood",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(175, 148, 131)},
+                    {["color1"] = Color3.fromRGB(124, 92, 70)},
+                    {["color1"] = Color3.fromRGB(156, 115, 87)}
+                }
+            },
+            ["model"] = "bigShelf2",
+            ["modelHeight"] = 8.12529468536377
+        },
+        ["bigShelf"] = {
+            ["name"] = "Big Shelf 1",
+            ["category"] = "garage",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "wood",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(175, 148, 131)},
+                    {["color1"] = Color3.fromRGB(124, 92, 70)},
+                    {["color1"] = Color3.fromRGB(156, 115, 87)}
+                }
+            },
+            ["model"] = "bigShelf",
+            ["modelHeight"] = 8.12529468536377
+        },
+        ["boxes"] = {
+            ["name"] = "Boxes",
+            ["category"] = "garage",
+            ["order"] = 2,
+            ["model"] = "boxes",
+            ["modelHeight"] = 3.0572924613952637
+        },
+        ["drawerCart"] = {
+            ["name"] = "Drawer Cart 1",
+            ["category"] = "garage",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "garage",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(192, 79, 71)},
+                    {["color1"] = Color3.fromRGB(110, 141, 187)},
+                    {["color1"] = Color3.fromRGB(97, 100, 109)}
+                }
+            },
+            ["model"] = "drawerCart",
+            ["modelHeight"] = 3.4828102588653564
+        },
+        ["poster3"] = {
+            ["name"] = "Poster",
+            ["category"] = "posters",
+            ["wall"] = true,
+            ["model"] = "poster3",
+            ["order"] = 101,
+            ["modelHeight"] = 0.10000000149011612
+        },
+        ["tireStack2"] = {
+            ["name"] = "Tire Stack 2",
+            ["category"] = "garage",
+            ["order"] = 5,
+            ["model"] = "tireStack2",
+            ["modelHeight"] = 4.57772970199585
+        },
+        ["modernTable"] = {
+            ["name"] = "Table",
+            ["category"] = "furniture",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "wood",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(175, 148, 131)},
+                    {["color1"] = Color3.fromRGB(124, 92, 70)},
+                    {["color1"] = Color3.fromRGB(156, 115, 87)}
+                }
+            },
+            ["model"] = "modernTable",
+            ["modelHeight"] = 3.9025745391845703
+        },
+        ["poster6"] = {
+            ["name"] = "Poster",
+            ["category"] = "posters",
+            ["wall"] = true,
+            ["model"] = "poster6",
+            ["order"] = 101,
+            ["modelHeight"] = 0.10000000149011612
+        },
+        ["poster5"] = {
+            ["name"] = "Poster",
+            ["category"] = "posters",
+            ["wall"] = true,
+            ["model"] = "poster5",
+            ["order"] = 101,
+            ["modelHeight"] = 0.10000000149011612
+        },
+        ["empty"] = {
+            ["name"] = "empty",
+            ["category"] = "empty",
+            ["order"] = 0,
+            ["model"] = "empty",
+            ["modelHeight"] = 0.10000000149011612
+        },
+        ["poster7"] = {
+            ["name"] = "Poster",
+            ["category"] = "posters",
+            ["wall"] = true,
+            ["model"] = "poster7",
+            ["order"] = 101,
+            ["modelHeight"] = 0.10000000149011612
+        },
+        ["bigDrawer2"] = {
+            ["name"] = "Big Drawer 2",
+            ["category"] = "garage",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "garage",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(192, 79, 71)},
+                    {["color1"] = Color3.fromRGB(110, 141, 187)},
+                    {["color1"] = Color3.fromRGB(97, 100, 109)}
+                }
+            },
+            ["model"] = "bigDrawer2",
+            ["modelHeight"] = 7.311368942260742
+        },
+        ["poster2"] = {
+            ["name"] = "Poster",
+            ["category"] = "posters",
+            ["wall"] = true,
+            ["model"] = "poster2",
+            ["order"] = 101,
+            ["modelHeight"] = 0.10000000149011612
+        },
+        ["poster1"] = {
+            ["name"] = "Poster",
+            ["category"] = "posters",
+            ["wall"] = true,
+            ["model"] = "poster1",
+            ["order"] = 101,
+            ["modelHeight"] = 0.10000000149011612
+        },
+        ["radio"] = {
+            ["name"] = "Old Radio",
+            ["category"] = "furniture",
+            ["order"] = 101,
+            ["model"] = "radio",
+            ["modelHeight"] = 3.859382152557373
+        },
+        ["officeDesk"] = {
+            ["name"] = "Office Desk",
+            ["category"] = "furniture",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "wood",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(175, 148, 131)},
+                    {["color1"] = Color3.fromRGB(124, 92, 70)},
+                    {["color1"] = Color3.fromRGB(156, 115, 87)}
+                }
+            },
+            ["model"] = "officeDesk",
+            ["modelHeight"] = 4.622607707977295
+        },
+        ["boxes3"] = {
+            ["name"] = "Boxes",
+            ["category"] = "garage",
+            ["order"] = 3,
+            ["model"] = "boxes3",
+            ["modelHeight"] = 3.953378677368164
+        },
+        ["shelf"] = {
+            ["name"] = "Small Shelf",
+            ["category"] = "garage",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "garage",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(192, 79, 71)},
+                    {["color1"] = Color3.fromRGB(110, 141, 187)},
+                    {["color1"] = Color3.fromRGB(97, 100, 109)}
+                }
+            },
+            ["model"] = "shelf",
+            ["modelHeight"] = 3.3750007152557373
+        },
+        ["officeChair"] = {
+            ["name"] = "Office Chair",
+            ["category"] = "furniture",
+            ["order"] = 101,
+            ["model"] = "officeChair",
+            ["modelHeight"] = 3.6861610412597656
+        },
+        ["bookshelf"] = {
+            ["name"] = "Bookshelf",
+            ["category"] = "furniture",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "wood",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(175, 148, 131)},
+                    {["color1"] = Color3.fromRGB(124, 92, 70)},
+                    {["color1"] = Color3.fromRGB(156, 115, 87)}
+                }
+            },
+            ["model"] = "bookshelf",
+            ["modelHeight"] = 6.899999618530273
+        },
+        ["chair"] = {
+            ["name"] = "Chair",
+            ["category"] = "furniture",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "wood",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(175, 148, 131)},
+                    {["color1"] = Color3.fromRGB(124, 92, 70)},
+                    {["color1"] = Color3.fromRGB(156, 115, 87)}
+                }
+            },
+            ["model"] = "chair",
+            ["modelHeight"] = 2.9064886569976807
+        },
+        ["radio2"] = {
+            ["name"] = "Radio",
+            ["category"] = "furniture",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "wood",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(175, 148, 131)},
+                    {["color1"] = Color3.fromRGB(124, 92, 70)},
+                    {["color1"] = Color3.fromRGB(156, 115, 87)}
+                }
+            },
+            ["model"] = "radio2",
+            ["modelHeight"] = 3.4621903896331787
+        },
+        ["desktopPC"] = {
+            ["name"] = "Desktop PC",
+            ["category"] = "furniture",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "wood",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(175, 148, 131)},
+                    {["color1"] = Color3.fromRGB(124, 92, 70)},
+                    {["color1"] = Color3.fromRGB(156, 115, 87)}
+                }
+            },
+            ["model"] = "desktopPC",
+            ["modelHeight"] = 4.182363510131836
+        },
+        ["wheelStack"] = {
+            ["name"] = "Wheel Stack",
+            ["category"] = "garage",
+            ["order"] = 4,
+            ["model"] = "wheelStack",
+            ["modelHeight"] = 2.9637837409973145
+        },
+        ["armchair"] = {
+            ["name"] = "Armchair",
+            ["category"] = "furniture",
+            ["order"] = 101,
+            ["model"] = "armchair",
+            ["modelHeight"] = 2.913815498352051
+        },
+        ["boxes2"] = {
+            ["name"] = "Boxes",
+            ["category"] = "garage",
+            ["order"] = 1,
+            ["model"] = "boxes2",
+            ["modelHeight"] = 2.7180662155151367
+        },
+        ["drawerCart2"] = {
+            ["name"] = "Drawer Cart 2",
+            ["category"] = "garage",
+            ["order"] = 101,
+            ["colors"] = {
+                ["id"] = "garage",
+                ["showGroup"] = "color1",
+                ["colors"] = {
+                    {["color1"] = Color3.fromRGB(192, 79, 71)},
+                    {["color1"] = Color3.fromRGB(110, 141, 187)},
+                    {["color1"] = Color3.fromRGB(97, 100, 109)}
+                }
+            },
+            ["model"] = "drawerCart2",
+            ["modelHeight"] = 4.687489986419678
+        },
+        ["poster8"] = {
+            ["name"] = "Poster",
+            ["category"] = "posters",
+            ["wall"] = true,
+            ["model"] = "poster8",
+            ["order"] = 101,
+            ["modelHeight"] = 0.10000000149011612
+        }
+    },
+    ["categories"] = {
+        ["garage"] = {["order"] = 2, ["name"] = "Garage"},
+        ["furniture"] = {["order"] = 3, ["name"] = "Furniture"},
+        ["empty"] = {["order"] = 0, ["name"] = "Empty"},
+        ["plants"] = {["order"] = 4, ["name"] = "Plants"},
+        ["posters"] = {["order"] = 1, ["name"] = "Posters"}
+    }
+}
