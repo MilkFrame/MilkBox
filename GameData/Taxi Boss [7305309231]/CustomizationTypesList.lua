0d8f3d1aad098ff64b5b321de38ab015c5fdd811
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 04:01:43 +1000

    Create CustomizationTypesList.lua

---
+++ GameData/Taxi Boss [7305309231]/CustomizationTypesList.lua
@@ -0,0 +1,6 @@
+data = {
+    {["name"] = "Performance", ["id"] = 1, ["order"] = 3, ["icon"] = "speed", ["color"] = Color3.fromRGB(170, 240, 84)},
+    {["name"] = "Appearance", ["id"] = 2, ["order"] = 2, ["icon"] = "wrench", ["color"] = Color3.fromRGB(240, 84, 84)},
+    {["name"] = "Interior", ["id"] = 3, ["order"] = 4, ["icon"] = "paint", ["color"] = Color3.fromRGB(240, 84, 84)},
+    {["name"] = "Paint", ["id"] = 4, ["order"] = 1, ["icon"] = "paint", ["color"] = Color3.fromRGB(115, 194, 243)}
+}
