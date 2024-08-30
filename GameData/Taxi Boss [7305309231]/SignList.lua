066b2e15ccf93ee0c8366a55c118466f3e5fcc19
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 04:03:49 +1000

    Create SignList.lua

---
+++ GameData/Taxi Boss [7305309231]/SignList.lua
@@ -0,0 +1,83 @@
+data = {
+    {["price"] = 0, ["name"] = "Taxi", ["model"] = "basic"},
+    {["price"] = 0, ["name"] = "Taxi 2", ["model"] = "invert"},
+    {["price"] = 0, ["rating"] = 0, ["name"] = "No Sign"},
+    {["price"] = 0, ["rating"] = 0, ["name"] = "Match Primary", ["model"] = "basic"},
+    {["price"] = 0, ["rating"] = 0, ["name"] = "Big NY", ["model"] = "big"},
+    {["name"] = "Pumpkin", ["model"] = "pumpkin", ["price"] = 0, ["rating"] = 0.1, ["unlockable"] = "pumpkin"},
+    {["name"] = "Pumpkins", ["model"] = "pumpkin2", ["price"] = 0, ["rating"] = 0.1, ["unlockable"] = "pumpkin2"},
+    {["exclusive"] = {111}, ["name"] = "Desert", ["model"] = "Desert", ["price"] = 0, ["rating"] = 0.1},
+    {
+        ["unlockableText"] = "WINTER EVENT",
+        ["name"] = "Santa Hat",
+        ["model"] = "SantaHat",
+        ["price"] = 0,
+        ["rating"] = 0.1,
+        ["unlockableColor"] = Color3.fromRGB(135, 201, 255),
+        ["unlockable"] = "santaHat"
+    },
+    {
+        ["unlockableText"] = "WINTER EVENT",
+        ["name"] = "Sledge",
+        ["model"] = "sledge",
+        ["price"] = 0,
+        ["rating"] = 0,
+        ["unlockableColor"] = Color3.fromRGB(135, 201, 255),
+        ["unlockable"] = "sledge"
+    },
+    {
+        ["unlockableText"] = "WINTER EVENT",
+        ["name"] = "Present Bow",
+        ["model"] = "PresentBow",
+        ["price"] = 0,
+        ["rating"] = 0,
+        ["unlockableColor"] = Color3.fromRGB(135, 201, 255),
+        ["unlockable"] = "presentBow"
+    },
+    {["exclusive"] = {114}, ["name"] = "ChristmasTree", ["model"] = "ChTree", ["price"] = 0, ["rating"] = 0.1},
+    {
+        ["unlockableText"] = "WINTER EVENT",
+        ["name"] = "Tree",
+        ["model"] = "Tree",
+        ["price"] = 0,
+        ["rating"] = 0.1,
+        ["unlockableColor"] = Color3.fromRGB(135, 201, 255),
+        ["unlockable"] = "tree"
+    },
+    {
+        ["unlockableText"] = "WINTER EVENT",
+        ["name"] = "Present",
+        ["model"] = "Present",
+        ["price"] = 0,
+        ["rating"] = 0.1,
+        ["unlockableColor"] = Color3.fromRGB(135, 201, 255),
+        ["unlockable"] = "presenthat"
+    },
+    {
+        ["unlockableText"] = "EASTER EVENT",
+        ["name"] = "EggBasket",
+        ["model"] = "EggBasket",
+        ["price"] = 0,
+        ["rating"] = 0,
+        ["unlockableColor"] = Color3.fromRGB(251, 189, 245),
+        ["unlockable"] = "eggbasket"
+    },
+    {
+        ["unlockableText"] = "EASTER EVENT",
+        ["name"] = "Chicken",
+        ["model"] = "Chicken",
+        ["price"] = 0,
+        ["rating"] = 0,
+        ["unlockableColor"] = Color3.fromRGB(251, 189, 245),
+        ["unlockable"] = "chicken"
+    },
+    {
+        ["unlockableText"] = "EASTER EVENT",
+        ["name"] = "Moai",
+        ["model"] = "Moai",
+        ["price"] = 0,
+        ["rating"] = 0.1,
+        ["unlockableColor"] = Color3.fromRGB(251, 189, 245),
+        ["unlockable"] = "moai"
+    }
+}
