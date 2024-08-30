9a0d569daf6c52c3b4f1f67142259250e358a576
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 04:06:12 +1000

    Create MaterialList.lua

---
+++ GameData/Taxi Boss [7305309231]/MaterialList.lua
@@ -0,0 +1,9 @@
+data = {
+    ["materials"] = {
+        {["price"] = 0, ["name"] = "Paint", ["index"] = 1},
+        {["price"] = 0, ["material"] = "PaintMetallic", ["name"] = "Metallic", ["index"] = 2},
+        {["name"] = "Chrome", ["price"] = 0, ["material"] = "PaintChrome", ["index"] = 3, ["vipPass"] = true},
+        {["name"] = "Carbon Fiber", ["price"] = 0, ["material"] = "CarbonFiber", ["index"] = 4, ["vipPass"] = true},
+        {["name"] = "Forged Carbon", ["price"] = 0, ["material"] = "ForgedCarbon", ["index"] = 5, ["vipPass"] = true}
+    }
+}
