c5da92132cf4bb284c3e3881c42f4b482595f476
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 04:05:59 +1000

    Create TireList.lua

---
+++ GameData/Taxi Boss [7305309231]/TireList.lua
@@ -0,0 +1,46 @@
+data = {
+    {["price"] = 0, ["meshId"] = 12165086190, ["name"] = "Standard", ["rimScale"] = 0.75},
+    {
+        ["override"] = {["tireDriftFriction"] = 0.05, ["tireDrift"] = 0.7, ["tireOffroad"] = -0.05},
+        ["name"] = "Race",
+        ["priceType"] = "mulPower",
+        ["price"] = 0.05,
+        ["meshId"] = 12453418971,
+        ["rimScale"] = 0.7875000000000001,
+        ["textureId"] = 12164924135
+    },
+    {
+        ["override"] = {["tireDriftFriction"] = 0.06, ["tireDrift"] = 0.5, ["tireOffroad"] = -0.06},
+        ["name"] = "Top Race",
+        ["priceType"] = "mulPower",
+        ["price"] = 0.08,
+        ["meshId"] = 12453418971,
+        ["rimScale"] = 0.7875000000000001,
+        ["textureId"] = 12453419068
+    },
+    {
+        ["override"] = {["tireDriftFriction"] = -0.05, ["tireDrift"] = 1.15, ["tireOffroad"] = -0.1},
+        ["name"] = "Drift",
+        ["priceType"] = "mulPower",
+        ["price"] = 0.02,
+        ["meshId"] = 12500976217,
+        ["rimScale"] = 0.7875000000000001,
+        ["textureId"] = 12164924135
+    },
+    {
+        ["override"] = {["tireDriftFriction"] = -0.055, ["tireDrift"] = 1.225, ["tireOffroad"] = -0.12},
+        ["name"] = "Pro Drift",
+        ["priceType"] = "mulPower",
+        ["price"] = 0.04,
+        ["meshId"] = 12507822822,
+        ["rimScale"] = 0.7875000000000001,
+        ["textureId"] = 12507822887
+    },
+    {
+        ["override"] = {["tireOffroad"] = 0.3, ["tireDrift"] = 0.7},
+        ["name"] = "Offroad",
+        ["price"] = 950,
+        ["meshId"] = 12500974507,
+        ["rimScale"] = 0.675
+    }
+}
