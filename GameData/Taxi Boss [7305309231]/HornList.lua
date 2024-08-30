efa7a5481305e1b63a75ead597c74ff82fd782a6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 04:04:27 +1000

    Create HornList.lua

---
+++ GameData/Taxi Boss [7305309231]/HornList.lua
@@ -0,0 +1,28 @@
+data = {
+    {["name"] = "Horn 1", ["volume"] = 0.5, ["price"] = 0, ["startPos"] = 0.25, ["id"] = "rbxassetid://1065401384"},
+    {["name"] = "Horn 2", ["volume"] = 1.25, ["price"] = 500, ["startPos"] = 0.3, ["id"] = "rbxassetid://2606409476"},
+    {
+        ["name"] = "Horn 3",
+        ["volume"] = 0.4,
+        ["price"] = 0,
+        ["customPass"] = true,
+        ["startPos"] = 0,
+        ["id"] = "rbxassetid://6181149605"
+    },
+    {
+        ["name"] = "Train Horn",
+        ["volume"] = 0.4,
+        ["price"] = 0,
+        ["customPass"] = true,
+        ["startPos"] = 0,
+        ["id"] = "rbxassetid://2038308429"
+    },
+    {
+        ["name"] = "Loud Horn",
+        ["volume"] = 0.28,
+        ["price"] = 0,
+        ["customPass"] = true,
+        ["startPos"] = 0,
+        ["id"] = "rbxassetid://528103000"
+    }
+}
