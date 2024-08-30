4b9ee6cef5959ac62f7c82d71371438035458644
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-08-31 04:03:21 +1000

    Create PlateList.lua

---
+++ GameData/Taxi Boss [7305309231]/PlateList.lua
@@ -0,0 +1,70 @@
+data = {
+    {
+        ["price"] = 0,
+        ["primary"] = Color3.fromRGB(208, 211, 211),
+        ["name"] = "Light",
+        ["secondary"] = Color3.fromRGB(91, 98, 99)
+    },
+    {
+        ["price"] = 0,
+        ["primary"] = Color3.fromRGB(62, 63, 65),
+        ["name"] = "Dark",
+        ["secondary"] = Color3.fromRGB(164, 171, 163)
+    },
+    {
+        ["name"] = "Blue Dark",
+        ["customPass"] = true,
+        ["price"] = 0,
+        ["primary"] = Color3.fromRGB(62, 63, 65),
+        ["secondary"] = Color3.fromRGB(71, 137, 228)
+    },
+    {
+        ["name"] = "Red Dark",
+        ["customPass"] = true,
+        ["price"] = 0,
+        ["primary"] = Color3.fromRGB(62, 63, 65),
+        ["secondary"] = Color3.fromRGB(218, 69, 52)
+    },
+    {
+        ["name"] = "Green Dark",
+        ["customPass"] = true,
+        ["price"] = 0,
+        ["primary"] = Color3.fromRGB(62, 63, 65),
+        ["secondary"] = Color3.fromRGB(75, 205, 74)
+    },
+    {
+        ["name"] = "Gold",
+        ["customPass"] = true,
+        ["price"] = 0,
+        ["primary"] = Color3.fromRGB(62, 63, 65),
+        ["secondary"] = Color3.fromRGB(191, 148, 61)
+    },
+    {
+        ["name"] = "Blossom",
+        ["customPass"] = true,
+        ["price"] = 0,
+        ["primary"] = Color3.fromRGB(62, 63, 65),
+        ["secondary"] = Color3.fromRGB(212, 153, 202)
+    },
+    {
+        ["name"] = "Blue Light",
+        ["customPass"] = true,
+        ["price"] = 0,
+        ["primary"] = Color3.fromRGB(208, 211, 211),
+        ["secondary"] = Color3.fromRGB(36, 99, 186)
+    },
+    {
+        ["name"] = "Red Light",
+        ["customPass"] = true,
+        ["price"] = 0,
+        ["primary"] = Color3.fromRGB(208, 211, 211),
+        ["secondary"] = Color3.fromRGB(164, 38, 24)
+    },
+    {
+        ["name"] = "Green Light",
+        ["customPass"] = true,
+        ["price"] = 0,
+        ["primary"] = Color3.fromRGB(208, 211, 211),
+        ["secondary"] = Color3.fromRGB(31, 156, 29)
+    }
+}
