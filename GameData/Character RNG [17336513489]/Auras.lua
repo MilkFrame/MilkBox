c9e3645699cdb8f4aefcf7e2b436c90eb3a305eb
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-02 01:29:49 +1000

    Create Auras.lua

---
+++ GameData/Character RNG [17336513489]/Auras.lua
@@ -0,0 +1,128 @@
+data = {
+    ["PurpleCrusader"] = {
+        ["Price"] = 100000,
+        ["Image"] = "rbxassetid://17514663840",
+        ["Rarity"] = "Uncommon",
+        ["DisplayName"] = "Purple Crusader"
+    },
+    ["LovesAmora"] = {
+        ["Price"] = 6500,
+        ["Image"] = "rbxassetid://17515943067",
+        ["Rarity"] = "Common",
+        ["DisplayName"] = "Love's Amora"
+    },
+    ["CursedSpiritHunter"] = {
+        ["Price"] = 7500,
+        ["Image"] = "rbxassetid://17514663954",
+        ["Rarity"] = "Common",
+        ["DisplayName"] = "Cursed Spirit Hunter"
+    },
+    ["Moonwalker"] = {
+        ["Price"] = 1500000,
+        ["Image"] = "rbxassetid://17515941823",
+        ["Rarity"] = "Rare",
+        ["DisplayName"] = "Moonwalker"
+    },
+    ["HellfireHalo"] = {
+        ["Price"] = 250000000,
+        ["Image"] = "rbxassetid://17515942196",
+        ["Rarity"] = "Exotic",
+        ["DisplayName"] = "Hellfire Halo"
+    },
+    ["EmeraldGrip"] = {
+        ["Price"] = 250000,
+        ["Image"] = "rbxassetid://17515942375",
+        ["Rarity"] = "Uncommon",
+        ["DisplayName"] = "Emerald Grip"
+    },
+    ["GaleTempest"] = {
+        ["Price"] = 50000000,
+        ["Image"] = "rbxassetid://17515941918",
+        ["Rarity"] = "Epic",
+        ["DisplayName"] = "Gale Tempest"
+    },
+    ["ShadowMaster"] = {
+        ["Price"] = 1000000000,
+        ["Image"] = "rbxassetid://17515940773",
+        ["Rarity"] = "Legendary",
+        ["DisplayName"] = "Shadow Master"
+    },
+    ["Poseidon"] = {
+        ["Price"] = 100000000000,
+        ["Image"] = "rbxassetid://17515941549",
+        ["Rarity"] = "Mythical",
+        ["DisplayName"] = "Poseidon's Wrath"
+    },
+    ["PsychicMaster"] = {
+        ["Price"] = 2000000000,
+        ["Image"] = "rbxassetid://17515941720",
+        ["Rarity"] = "Legendary",
+        ["DisplayName"] = "Psychic Master"
+    },
+    ["DarkWinds"] = {
+        ["Price"] = 1000000,
+        ["Image"] = "rbxassetid://17515941216",
+        ["Rarity"] = "Rare",
+        ["DisplayName"] = "Dark Winds"
+    },
+    ["QuirklessHero"] = {
+        ["Price"] = 10000,
+        ["Image"] = "rbxassetid://17514663649",
+        ["Rarity"] = "Common",
+        ["DisplayName"] = "Quirkless Hero"
+    },
+    ["AphroditesGrace"] = {
+        ["Price"] = 500000000,
+        ["Image"] = "rbxassetid://17515940879",
+        ["Rarity"] = "Exotic",
+        ["DisplayName"] = "Aphrodite's Grace"
+    },
+    ["AtomicRazer"] = {
+        ["Price"] = 750000,
+        ["Image"] = "rbxassetid://17514542291",
+        ["Rarity"] = "Rare",
+        ["DisplayName"] = "Atomic Razer"
+    },
+    ["ElectricPowerFist"] = {
+        ["Price"] = 60000000,
+        ["Image"] = "rbxassetid://17515941351",
+        ["Rarity"] = "Epic",
+        ["DisplayName"] = "Electric Power Fist"
+    },
+    ["StinkAura"] = {
+        ["Price"] = 5000,
+        ["Image"] = "rbxassetid://17515940990",
+        ["Rarity"] = "Common",
+        ["DisplayName"] = "Stink Aura"
+    },
+    ["EternityBeam"] = {
+        ["Price"] = 150000000000,
+        ["Image"] = "rbxassetid://17515942935",
+        ["Rarity"] = "Mythical",
+        ["DisplayName"] = "Eternity Beam"
+    },
+    ["CelestialCrossings"] = {
+        ["Price"] = 100000000,
+        ["Image"] = "rbxassetid://17515942740",
+        ["Rarity"] = "Exotic",
+        ["DisplayName"] = "Celestial Crossings"
+    },
+    ["DarkPsycho"] = {
+        ["Price"] = 75000000,
+        ["Image"] = "rbxassetid://17515942542",
+        ["Rarity"] = "Epic",
+        ["DisplayName"] = "Dark Psycho"
+    },
+    ["DiamondGrip"] = {
+        ["Price"] = 250000,
+        ["Image"] = "rbxassetid://17515942464",
+        ["Rarity"] = "Uncommon",
+        ["DisplayName"] = "Diamond Grip"
+    },
+    ["TitanHunter"] = {
+        ["Price"] = 100000,
+        ["Image"] = "rbxassetid://17514663470",
+        ["Rarity"] = "Uncommon",
+        ["DisplayName"] = "Titan Hunter"
+    }
+}
