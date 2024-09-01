b45e02201c83e52d1698128159504e71266d56c6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-02 01:23:52 +1000

    Create Crafting.lua

---
+++ GameData/Character RNG [17336513489]/Crafting.lua
@@ -0,0 +1,82 @@
+data = {
+    ["LuckPotion2"] = {
+        ["Order"] = 2,
+        ["Type"] = "Potion",
+        ["Name"] = "LuckPotion2",
+        ["Materials"] = {
+            ["Gems"] = {["Order"] = 3, ["Type"] = "Money", ["Amount"] = 50000},
+            ["Custom1"] = {
+                ["Order"] = 1,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 50, ["RarityAndAbove"] = "Common"}
+            },
+            ["Custom2"] = {
+                ["Order"] = 2,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 25, ["RarityAndAbove"] = "Uncommon"}
+            }
+        }
+    },
+    ["HastePotion2"] = {
+        ["Order"] = 5,
+        ["Type"] = "Potion",
+        ["Name"] = "HastePotion2",
+        ["Materials"] = {
+            ["Custom"] = {
+                ["Order"] = 1,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 30, ["RarityAndAbove"] = "Uncommon"}
+            },
+            ["Gems"] = {["Order"] = 2, ["Type"] = "Money", ["Amount"] = 15000}
+        }
+    },
+    ["HastePotion1"] = {
+        ["Order"] = 4,
+        ["Type"] = "Potion",
+        ["Name"] = "HastePotion1",
+        ["Materials"] = {
+            ["Custom"] = {
+                ["Order"] = 1,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 3, ["RarityAndAbove"] = "Common"}
+            },
+            ["Gems"] = {["Order"] = 2, ["Type"] = "Money", ["Amount"] = 100}
+        }
+    },
+    ["LuckPotion1"] = {
+        ["Order"] = 1,
+        ["Type"] = "Potion",
+        ["Name"] = "LuckPotion1",
+        ["Materials"] = {
+            ["Custom"] = {
+                ["Order"] = 1,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 3, ["RarityAndAbove"] = "Common"}
+            },
+            ["Gems"] = {["Order"] = 2, ["Type"] = "Money", ["Amount"] = 250}
+        }
+    },
+    ["LuckPotion3"] = {
+        ["Order"] = 3,
+        ["Type"] = "Potion",
+        ["Name"] = "LuckPotion3",
+        ["Materials"] = {
+            ["Custom3"] = {
+                ["Order"] = 3,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 3, ["RarityAndAbove"] = "Epic"}
+            },
+            ["Gems"] = {["Order"] = 4, ["Type"] = "Money", ["Amount"] = 100000},
+            ["Custom1"] = {
+                ["Order"] = 1,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 50, ["RarityAndAbove"] = "Uncommon"}
+            },
+            ["Custom2"] = {
+                ["Order"] = 2,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 20, ["RarityAndAbove"] = "Rare"}
+            }
+        }
+    }
+}
