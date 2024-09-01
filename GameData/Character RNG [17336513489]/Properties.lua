3084c7c00a1b24640c9a2f8ca0ba6e8a52332b72
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-02 01:23:28 +1000

    Create Properties.lua

---
+++ GameData/Character RNG [17336513489]/Properties.lua
@@ -0,0 +1,82 @@
+data = {
+    ["RollingSpeed"] = 3.5,
+    ["RiggedFirstTimeCharacters"] = {
+        ["ObiWanKenobi"] = {
+            ["Type"] = "Character",
+            ["Image"] = "rbxassetid://17508380866",
+            ["Rarity"] = "Rare",
+            ["Value"] = 15000,
+            ["Animations"] = {["Idle"] = "LightsaberIdle", ["FillRemainingAnimations"] = "Werewolf"},
+            ["RawProbability"] = 0.00005714285714285714,
+            ["Probability"] = 17500,
+            ["DisplayName"] = "Obus-Wand Kenova"
+        },
+        ["Levi"] = {
+            ["Type"] = "Character",
+            ["Image"] = "rbxassetid://17506816324",
+            ["Rarity"] = "Rare",
+            ["Probability"] = 15000,
+            ["Value"] = 15000,
+            ["RawProbability"] = 0.00006666666666666667,
+            ["Animations"] = "Knight",
+            ["DisplayName"] = "Lewis"
+        },
+        ["Bakugo"] = {
+            ["Type"] = "Character",
+            ["Image"] = "rbxassetid://17506816799",
+            ["Rarity"] = "Rare",
+            ["Probability"] = 12500,
+            ["Value"] = 10000,
+            ["RawProbability"] = 0.00008,
+            ["Animations"] = "Knight",
+            ["DisplayName"] = "Bakura"
+        },
+        ["Sogeking"] = {
+            ["Type"] = "Character",
+            ["Image"] = "rbxassetid://17508380482",
+            ["Rarity"] = "Rare",
+            ["Probability"] = 9000,
+            ["Value"] = 7000,
+            ["RawProbability"] = 0.00011111111111111112,
+            ["Animations"] = "Werewolf",
+            ["DisplayName"] = "Sniper King"
+        },
+        ["Gigachad"] = {
+            ["Type"] = "Character",
+            ["Rarity"] = "Rare",
+            ["Animations"] = {["Idle"] = "18655234091", ["FillRemainingAnimations"] = "Knight"},
+            ["DisplayName"] = "Gigachad",
+            ["Image"] = "rbxassetid://17441288040",
+            ["ObtainedBy"] = {"DailyRewards", "Rolling"},
+            ["Value"] = 25000,
+            ["RawProbability"] = 0.000042553191489361704,
+            ["Probability"] = 23500
+        },
+        ["Todoroki"] = {
+            ["Type"] = "Character",
+            ["Image"] = "rbxassetid://17506814921",
+            ["Rarity"] = "Rare",
+            ["Probability"] = 13500,
+            ["Value"] = 10000,
+            ["RawProbability"] = 0.00007407407407407407,
+            ["Animations"] = "Toy",
+            ["DisplayName"] = "Todariki"
+        }
+    },
+    ["NearlyGetRareItem"] = {["MinimumRarity"] = "Epic", ["Probability"] = 50},
+    ["MakeCharacterUnknownChance"] = 100,
+    ["MakeCharacterUnknownAboveRarity"] = "Epic",
+    ["OnRewardClaimed"] = {["_handlerListHead"] = false},
+    ["OnAutoRollingChanged"] = {["_handlerListHead"] = false},
+    ["AnnounceCharacterAboveRarity"] = "Epic",
+    ["MaxLuckAmount"] = 10,
+    ["PlayRareMusicOnAboveRarity"] = "Epic",
+    ["GloballyAnnounceCharacterAboveRarity"] = "Legendary",
+    ["RoughAnimationTime"] = 1.25,
+    ["IsAutoRolling"] = false,
+    ["SellTimerOnCharactersAboveRarity"] = "Exotic",
+    ["FastRollSpeed"] = 1,
+    ["OnRollingActivated"] = {["_handlerListHead"] = false},
+    ["RollsUntilAdditionalLuck"] = 10,
+    ["AlwaysMakeCharacterUnknownAboveRarity"] = "Legendary"
+}
