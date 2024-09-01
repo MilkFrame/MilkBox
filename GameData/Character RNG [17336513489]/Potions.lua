b46d9109eae2b70555cb2caefc1684b8e0dd8320
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-02 01:25:25 +1000

    Create Potions.lua

---
+++ GameData/Character RNG [17336513489]/Potions.lua
@@ -0,0 +1,69 @@
+data = {
+    ["LuckPotion2"] = {
+        ["Type"] = "Potion",
+        ["Image"] = "rbxassetid://18327750784",
+        ["Rarity"] = "Uncommon",
+        ["MinimumProbability"] = {400, 700},
+        ["DurationInfo"] = {["Time"] = 120, ["Type"] = "Time"},
+        ["DisplayLuck"] = 75,
+        ["Description"] = "Boost luck by +75 for the next 2 minutes.",
+        ["DisplayName"] = "Luck Potion II"
+    },
+    ["HastePotion2"] = {
+        ["Image"] = "rbxassetid://18327751038",
+        ["Rarity"] = "Uncommon",
+        ["Type"] = "Potion",
+        ["Speed"] = 0.6,
+        ["DurationInfo"] = {["Time"] = 180, ["Type"] = "Time"},
+        ["Description"] = "Increases roll speed by 40% for 3 minutes.",
+        ["DisplayName"] = "Haste Potion II"
+    },
+    ["HastePotion1"] = {
+        ["Image"] = "rbxassetid://18327750452",
+        ["Rarity"] = "Common",
+        ["Type"] = "Potion",
+        ["Speed"] = 0.8,
+        ["DurationInfo"] = {["Rolls"] = 10, ["Type"] = "Rolls"},
+        ["Description"] = "Increases roll speed by 20% for the next 10 rolls.",
+        ["DisplayName"] = "Haste Potion I"
+    },
+    ["LuckPotion1"] = {
+        ["Type"] = "Potion",
+        ["Image"] = "rbxassetid://18327751212",
+        ["Rarity"] = "Common",
+        ["MinimumProbability"] = {0, 140},
+        ["DurationInfo"] = {["Time"] = 60, ["Type"] = "Time"},
+        ["DisplayLuck"] = 20,
+        ["Description"] = "Boost luck by +20 for the next minute.",
+        ["DisplayName"] = "Luck Potion I"
+    },
+    ["HeavenlyPotion"] = {
+        ["Type"] = "Potion",
+        ["Image"] = "rbxassetid://18225632389",
+        ["Rarity"] = "Legendary",
+        ["MinimumProbability"] = {100000, 200000},
+        ["DurationInfo"] = {["Time"] = 60, ["Type"] = "Time"},
+        ["DisplayLuck"] = 600,
+        ["Description"] = "Increases luck by +600 for 1 minute.",
+        ["DisplayName"] = "Heavenly Potion IV"
+    },
+    ["LuckPotion3"] = {
+        ["Type"] = "Potion",
+        ["Image"] = "rbxassetid://18327663833",
+        ["Rarity"] = "Epic",
+        ["MinimumProbability"] = {750, 5500},
+        ["DurationInfo"] = {["Time"] = 300, ["Type"] = "Time"},
+        ["DisplayLuck"] = 200,
+        ["Description"] = "Boost luck by +200 for the next 5 minutes.",
+        ["DisplayName"] = "Luck Potion III"
+    },
+    ["SwiftnessElixir"] = {
+        ["Image"] = "rbxassetid://18225631761",
+        ["Rarity"] = "Enchanted",
+        ["Type"] = "Potion",
+        ["Speed"] = 0.3,
+        ["DurationInfo"] = {["Time"] = 600, ["Type"] = "Time"},
+        ["Description"] = "Increases roll speed by 70% for the next 10 minutes.",
+        ["DisplayName"] = "Swiftness Elixir IV"
+    }
+}
