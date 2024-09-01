74b20d72eefcdbca60342dd83d41c62fea93a564
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-02 01:24:12 +1000

    Create KeyCrafting.lua

---
+++ GameData/Character RNG [17336513489]/KeyCrafting.lua
@@ -0,0 +1,25 @@
+data = {
+    {
+        ["Type"] = "Material",
+        ["Name"] = "WitchsKey",
+        ["Materials"] = {
+            ["Custom2"] = {
+                ["Order"] = 2,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 50, ["RarityAndAbove"] = "Rare"}
+            },
+            ["Gems"] = {["Order"] = 5, ["Type"] = "Money", ["Amount"] = 5000000},
+            ["WitchsRubble"] = {["Order"] = 4, ["Type"] = "Material", ["Amount"] = 150},
+            ["Custom1"] = {
+                ["Order"] = 1,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 150, ["RarityAndAbove"] = "Uncommon"}
+            },
+            ["Custom3"] = {
+                ["Order"] = 3,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 10, ["RarityAndAbove"] = "Epic"}
+            }
+        }
+    }
+}
