383d4ce27002fe46f43447c929df4c36526c726e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-02 01:25:13 +1000

    Create Materials.lua

---
+++ GameData/Character RNG [17336513489]/Materials.lua
@@ -0,0 +1,92 @@
+data = {
+    ["InfinityToken"] = {
+        ["Image"] = "rbxassetid://134509783622249",
+        ["Rarity"] = "Legendary",
+        ["Type"] = "Material",
+        ["Value"] = 250000000,
+        ["Description"] = "A token that grants +999 luck on the next roll.",
+        ["IsPaidItem"] = true,
+        ["DisplayName"] = "+999-Luck Token"
+    },
+    ["WitchsRubble"] = {
+        ["Image"] = "rbxassetid://18243124434",
+        ["Rarity"] = "Epic",
+        ["Value"] = 1,
+        ["Type"] = "Material",
+        ["Description"] = "A piece of rubble fallen from the Witch's Tower. Maybe could be crafted?",
+        ["DisplayName"] = "Cursed Rubble"
+    },
+    ["EpicSeasonKey"] = {
+        ["IsSeasonKey"] = true,
+        ["Rarity"] = "Epic",
+        ["Type"] = "Material",
+        ["Value"] = 750000,
+        ["Description"] = "Guarantees an epic item from the Season Pass's Chest.",
+        ["Image"] = "rbxassetid://18940494959",
+        ["DisplayName"] = "Epic Key"
+    },
+    ["WitchsKey"] = {
+        ["Image"] = "rbxassetid://18228896664",
+        ["Rarity"] = "Legendary",
+        ["Value"] = 500000,
+        ["Type"] = "Material",
+        ["Description"] = "Unlocks the door to the Witch's Tower.",
+        ["DisplayName"] = "Witch's Key"
+    },
+    ["RareSeasonKey"] = {
+        ["IsSeasonKey"] = true,
+        ["Rarity"] = "Rare",
+        ["Type"] = "Material",
+        ["Value"] = 100000,
+        ["Description"] = "Guarantees a rare item from the Season Pass's Chest.",
+        ["Image"] = "rbxassetid://18940366554",
+        ["DisplayName"] = "Rare Key"
+    },
+    ["UncommonSeasonKey"] = {
+        ["IsSeasonKey"] = true,
+        ["Rarity"] = "Uncommon",
+        ["Type"] = "Material",
+        ["Value"] = 50000,
+        ["Description"] = "Guarantees an uncommon item from the Season Pass's Chest.",
+        ["Image"] = "rbxassetid://18915930741",
+        ["DisplayName"] = "Uncommon Key"
+    },
+    ["LegendarySeasonKey"] = {
+        ["KeyRarity"] = "Legendary",
+        ["IsSeasonKey"] = true,
+        ["Rarity"] = "Mythical",
+        ["Type"] = "Material",
+        ["Value"] = 20000000000,
+        ["Description"] = "Guarantees a legendary item from the Season Pass's Chest.",
+        ["Image"] = "rbxassetid://18941318894",
+        ["DisplayName"] = "Legendary Key"
+    },
+    ["EnchantedSeasonKey"] = {
+        ["IsSeasonKey"] = true,
+        ["Rarity"] = "Enchanted",
+        ["Type"] = "Material",
+        ["Value"] = 250000000,
+        ["Description"] = "Guarantees an enchanted item from the Season Pass's Chest.",
+        ["Image"] = "rbxassetid://18940495349",
+        ["DisplayName"] = "Enchanted Key"
+    },
+    ["ExoticSeasonKey"] = {
+        ["IsSeasonKey"] = true,
+        ["Rarity"] = "Exotic",
+        ["Type"] = "Material",
+        ["Value"] = 50000000,
+        ["Description"] = "Guarantees an exotic item from the Season Pass's Chest.",
+        ["Image"] = "rbxassetid://18940495181",
+        ["DisplayName"] = "Exotic Key"
+    },
+    ["SeasonKey"] = {
+        ["Type"] = "Material",
+        ["IsSeasonKey"] = true,
+        ["Rarity"] = "Common",
+        ["Description"] = "A key to roll an item from the Season Pass's chest.",
+        ["Value"] = 15000,
+        ["Image"] = "rbxassetid://18915870260",
+        ["IsPaidItem"] = true,
+        ["DisplayName"] = "Season Key"
+    }
+}
