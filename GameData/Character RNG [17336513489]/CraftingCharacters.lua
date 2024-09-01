9c938a99ce02b1e42ccda553e725e57780f489e3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-02 01:24:23 +1000

    Create CraftingCharacters.lua

---
+++ GameData/Character RNG [17336513489]/CraftingCharacters.lua
@@ -0,0 +1,200 @@
+data = {
+    ["Batman"] = {
+        ["Type"] = "Character",
+        ["Name"] = "Batman",
+        ["Materials"] = {
+            ["BlackNoir"] = {["Order"] = 1, ["Type"] = "Character", ["Amount"] = 125},
+            ["Gems"] = {["Order"] = 4, ["Type"] = "Money", ["Amount"] = 750000},
+            ["Custom1"] = {
+                ["Order"] = 2,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 100, ["RarityAndAbove"] = "Rare"}
+            },
+            ["Custom2"] = {
+                ["Order"] = 3,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 20, ["RarityAndAbove"] = "Epic"}
+            }
+        }
+    },
+    ["TheDeep"] = {
+        ["Type"] = "Character",
+        ["Name"] = "TheDeep",
+        ["Materials"] = {
+            ["Custom1"] = {
+                ["Order"] = 1,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 75, ["RarityAndAbove"] = "Rare"}
+            },
+            ["Gems"] = {["Order"] = 2, ["Type"] = "Money", ["Amount"] = 3500000}
+        }
+    },
+    ["PeterGriffin"] = {
+        ["Type"] = "Character",
+        ["Name"] = "PeterGriffin",
+        ["Materials"] = {
+            ["Legendary"] = {
+                ["Order"] = 5,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 5, ["RarityAndAbove"] = "Legendary"}
+            },
+            ["Enchanted"] = {
+                ["Order"] = 4,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 25, ["RarityAndAbove"] = "Enchanted"}
+            },
+            ["Common"] = {
+                ["Order"] = 1,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 2500, ["RarityAndAbove"] = "Common"},
+                ["Amount"] = 1
+            },
+            ["Exotic"] = {
+                ["Order"] = 3,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 50, ["RarityAndAbove"] = "Exotic"}
+            },
+            ["Gems"] = {["Order"] = 6, ["Type"] = "Money", ["Amount"] = 75000000000},
+            ["Rare"] = {
+                ["Order"] = 2,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 500, ["RarityAndAbove"] = "Rare"},
+                ["Amount"] = 1
+            }
+        }
+    },
+    ["Dio"] = {
+        ["StarterCharacter"] = true,
+        ["Type"] = "Character",
+        ["Name"] = "Dio",
+        ["Materials"] = {
+            ["Gems"] = {["Order"] = 3, ["Type"] = "Money", ["Amount"] = 10000},
+            ["Custom1"] = {
+                ["Order"] = 1,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 50, ["RarityAndAbove"] = "Common"}
+            },
+            ["Custom2"] = {
+                ["Order"] = 2,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 10, ["RarityAndAbove"] = "Uncommon"}
+            }
+        }
+    },
+    ["Joker"] = {
+        ["Type"] = "Character",
+        ["Name"] = "Joker",
+        ["Materials"] = {
+            ["Gems"] = {["Order"] = 3, ["Type"] = "Money", ["Amount"] = 750000},
+            ["Custom1"] = {
+                ["Order"] = 1,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 100, ["RarityAndAbove"] = "Rare"}
+            },
+            ["Custom2"] = {
+                ["Order"] = 2,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 20, ["RarityAndAbove"] = "Epic"}
+            }
+        }
+    },
+    ["Builderman"] = {
+        ["StarterCharacter"] = true,
+        ["Type"] = "Character",
+        ["Name"] = "Builderman",
+        ["Materials"] = {
+            ["Guest"] = {["Order"] = 1, ["Type"] = "Character", ["Amount"] = 50},
+            ["Gems"] = {["Order"] = 3, ["Type"] = "Money", ["Amount"] = 10000},
+            ["Noob"] = {["Order"] = 2, ["Type"] = "Character", ["Amount"] = 50}
+        }
+    },
+    ["Jotaro"] = {
+        ["StarterCharacter"] = true,
+        ["Type"] = "Character",
+        ["Name"] = "Jotaro",
+        ["Materials"] = {
+            ["Gems"] = {["Order"] = 3, ["Type"] = "Money", ["Amount"] = 10000},
+            ["Custom1"] = {
+                ["Order"] = 1,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 50, ["RarityAndAbove"] = "Common"}
+            },
+            ["Custom2"] = {
+                ["Order"] = 2,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 10, ["RarityAndAbove"] = "Uncommon"}
+            }
+        }
+    },
+    ["L"] = {
+        ["Type"] = "Character",
+        ["Name"] = "L",
+        ["Materials"] = {
+            ["Custom3"] = {
+                ["Order"] = 3,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 10, ["RarityAndAbove"] = "Exotic"}
+            },
+            ["Gems"] = {["Order"] = 4, ["Type"] = "Money", ["Amount"] = 20000000},
+            ["Custom1"] = {
+                ["Order"] = 1,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 1000, ["RarityAndAbove"] = "Common"}
+            },
+            ["Custom2"] = {
+                ["Order"] = 2,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 50, ["RarityAndAbove"] = "Epic"}
+            }
+        }
+    },
+    ["CaseOh"] = {
+        ["Type"] = "Character",
+        ["Name"] = "CaseOh",
+        ["Materials"] = {
+            ["Custom4"] = {
+                ["Order"] = 4,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 3, ["RarityAndAbove"] = "Enchanted"}
+            },
+            ["Custom2"] = {
+                ["Order"] = 2,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 10, ["RarityAndAbove"] = "Epic"}
+            },
+            ["Gems"] = {["Order"] = 5, ["Type"] = "Money", ["Amount"] = 500000000},
+            ["Custom1"] = {
+                ["Order"] = 1,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 750, ["RarityAndAbove"] = "Common"}
+            },
+            ["Custom3"] = {
+                ["Order"] = 3,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 30, ["RarityAndAbove"] = "Exotic"}
+            }
+        }
+    },
+    ["Light"] = {
+        ["Type"] = "Character",
+        ["Name"] = "Light",
+        ["Materials"] = {
+            ["Custom3"] = {
+                ["Order"] = 3,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 10, ["RarityAndAbove"] = "Exotic"}
+            },
+            ["Gems"] = {["Order"] = 4, ["Type"] = "Money", ["Amount"] = 20000000},
+            ["Custom1"] = {
+                ["Order"] = 1,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 1000, ["RarityAndAbove"] = "Common"}
+            },
+            ["Custom2"] = {
+                ["Order"] = 2,
+                ["Type"] = "Custom",
+                ["Restrictions"] = {["Type"] = {"Character"}, ["Limit"] = 50, ["RarityAndAbove"] = "Epic"}
+            }
+        }
+    }
+}
