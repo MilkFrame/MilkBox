7519ceb142a29a0eb36db0eb06798505c80bcfd7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-02 01:24:35 +1000

    Create RewardPacks.lua

---
+++ GameData/Character RNG [17336513489]/RewardPacks.lua
@@ -0,0 +1,18 @@
+data = {
+    {
+        {["Value"] = 2500, ["Type"] = "Money"},
+        {["Value"] = 100000, ["Type"] = "Money", ["Image"] = "rbxassetid://17529353569"},
+        {["Value"] = "Gigachad", ["Type"] = "Character"},
+        {["Value"] = 10000000, ["Type"] = "Money", ["Image"] = "rbxassetid://17529345599"},
+        {["Value"] = 15000000, ["Type"] = "Money", ["Image"] = "rbxassetid://17539819216"},
+        {["Value"] = "Superman", ["Type"] = "Character"}
+    },
+    {
+        {["Value"] = 2500, ["Type"] = "Money"},
+        {["Value"] = 100000, ["Type"] = "Money", ["Image"] = "rbxassetid://17529353569"},
+        {["Value"] = "ATrain", ["Type"] = "Character"},
+        {["Value"] = 10000000, ["Type"] = "Money", ["Image"] = "rbxassetid://17529345599"},
+        {["Value"] = 15000000, ["Type"] = "Money", ["Image"] = "rbxassetid://17539819216"},
+        {["Value"] = "Sukuna", ["Type"] = "Character"}
+    }
+}
