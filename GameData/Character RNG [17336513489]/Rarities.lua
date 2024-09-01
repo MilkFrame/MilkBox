2d606967eb8a8ab75e19e68c0de51822b6e7e7d1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-02 01:25:37 +1000

    Create Rarities.lua

---
+++ GameData/Character RNG [17336513489]/Rarities.lua
@@ -0,0 +1,33 @@
+data = {
+    ["Mythical"] = {
+        ["RawProbabilityMultiplier"] = 100,
+        ["ValueRange"] = {75000000000, 1000000000000},
+        ["Order"] = 8,
+        ["Gradient"] = "Mythical",
+        ["RewardSound"] = {"Reward4", "Choir"}
+    },
+    ["Legendary"] = {
+        ["RawProbabilityMultiplier"] = 7.5,
+        ["ValueRange"] = {1000000000, 30000000000},
+        ["Order"] = 7,
+        ["Gradient"] = "Legendary",
+        ["RewardSound"] = {"Reward4", "Choir"}
+    },
+    ["Enchanted"] = {
+        ["RawProbabilityMultiplier"] = 3,
+        ["ValueRange"] = {100000000, 500000000},
+        ["Order"] = 6,
+        ["Gradient"] = "Enchanted",
+        ["RewardSound"] = {"Reward3", "Choir"}
+    },
+    ["Common"] = {["Order"] = 1, ["Gradient"] = "Common", ["ValueRange"] = {5, 200}},
+    ["Epic"] = {["Order"] = 4, ["Gradient"] = "Epic", ["RewardSound"] = "Reward2", ["ValueRange"] = {35000, 1000000}},
+    ["Exotic"] = {
+        ["Order"] = 5,
+        ["Gradient"] = "Exotic",
+        ["RewardSound"] = "Reward3",
+        ["ValueRange"] = {2000000, 85000000}
+    },
+    ["Uncommon"] = {["Order"] = 2, ["Gradient"] = "Uncommon", ["ValueRange"] = {500, 1000}},
+    ["Rare"] = {["Order"] = 3, ["Gradient"] = "Rare", ["RewardSound"] = "Reward2", ["ValueRange"] = {5000, 25000}}
+}
