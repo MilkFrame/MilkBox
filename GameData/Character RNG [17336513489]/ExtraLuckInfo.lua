68701d2615f49b107484c0106d4c545a71c27572
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-02 01:23:41 +1000

    Create ExtraLuckInfo.lua

---
+++ GameData/Character RNG [17336513489]/ExtraLuckInfo.lua
@@ -0,0 +1,27 @@
+data = {
+    ["InfinityLuck"] = {
+        ["RiggedFirstTimeLuck"] = "Exotic",
+        ["DisplayLuck"] = 999,
+        ["IsActive"] = "function: 0xaecf7f42d9623615",
+        ["MinimumProbability"] = {75000, 300000}
+    },
+    ["Premium"] = {
+        ["DisplayLuck"] = 20,
+        ["IsActive"] = "function: 0xa44c7ed583105055",
+        ["MinimumProbability"] = {0, 30}
+    },
+    ["InGroup"] = {["DisplayLuck"] = 5, ["IsActive"] = "function: 0x25c17c3a7eeb5175", ["MinimumProbability"] = {0, 15}},
+    ["FriendLuck"] = {
+        ["GetAmountRan"] = "function: 0x4183b574920a7765",
+        ["DisplayLuck"] = 15,
+        ["IsActive"] = "function: 0x127f4548701742d5",
+        ["MinimumProbability"] = {10, 25}
+    },
+    ["UltraLuck"] = {["IsGamepass"] = true, ["DisplayLuck"] = 150, ["MinimumProbability"] = {0, 1400}},
+    ["TenthRoll"] = {
+        ["DisplayLuck"] = 40,
+        ["IsActive"] = "function: 0x67cb068bd83e0915",
+        ["MinimumProbability"] = {100, 350}
+    },
+    ["DoubleLuck"] = {["IsGamepass"] = true, ["DisplayLuck"] = 75, ["MinimumProbability"] = {0, 850}}
+}
