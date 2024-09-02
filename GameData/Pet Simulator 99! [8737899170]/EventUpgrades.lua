222a26202fc4bc8f0ff52552b504458d71f7841f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-03 01:42:46 +1000

    Create EventUpgrades.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/EventUpgrades.lua
@@ -0,0 +1,130 @@
+data = {
+    ["LuckyTileHugeChance"] = {
+        ["TierCosts"] = {
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 100}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 250}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 500}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 1500}}
+        },
+        ["Disabled"] = true,
+        ["_id"] = "LuckyTileHugeChance",
+        ["Order"] = 1,
+        ["TierPowers"] = {30, 75, 125, 200},
+        ["_script"] = "EventUpgrade | LuckyTileHugeChance",
+        ["Name"] = "Huge Chance",
+        ["TierDescription"] = "function: 0x0e68fb6b913fbbc7",
+        ["Icon"] = "rbxassetid://18459829415"
+    },
+    ["LuckyTileTicketChance"] = {
+        ["TierCosts"] = {
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 50}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 125}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 250}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 750}}
+        },
+        ["Disabled"] = true,
+        ["_id"] = "LuckyTileTicketChance",
+        ["Order"] = 2,
+        ["TierPowers"] = {10, 20, 35, 50},
+        ["_script"] = "EventUpgrade | LuckyTileTicketChance",
+        ["Name"] = "Ticket Chance",
+        ["TierDescription"] = "function: 0x95ba958fc37ba697",
+        ["Icon"] = "rbxassetid://18459828887"
+    },
+    ["LuckyTilePotionUpgrader"] = {
+        ["TierCosts"] = {
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 20}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 50}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 100}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 250}}
+        },
+        ["Disabled"] = true,
+        ["_id"] = "LuckyTilePotionUpgrader",
+        ["Order"] = 8,
+        ["TierPowers"] = {30, 50, 75, 100},
+        ["_script"] = "EventUpgrade | LuckyTilePotionUpgrader",
+        ["Name"] = "Potion Upgrade Chance",
+        ["TierDescription"] = "function: 0xaa118282a3013d57",
+        ["Icon"] = "rbxassetid://18459829147"
+    },
+    ["LuckyTileEggLuck"] = {
+        ["TierCosts"] = {
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 75}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 200}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 400}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 1200}}
+        },
+        ["Disabled"] = true,
+        ["_id"] = "LuckyTileEggLuck",
+        ["Order"] = 5,
+        ["TierPowers"] = {50, 75, 100, 150},
+        ["_script"] = "EventUpgrade | LuckyTileEggLuck",
+        ["Name"] = "Egg Luck",
+        ["TierDescription"] = "function: 0x89aa1b145a932f47",
+        ["Icon"] = "rbxassetid://15011599749"
+    },
+    ["LuckyTileEnchantUpgrader"] = {
+        ["TierCosts"] = {
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 20}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 50}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 100}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 250}}
+        },
+        ["Disabled"] = true,
+        ["_id"] = "LuckyTileEnchantUpgrader",
+        ["Order"] = 7,
+        ["TierPowers"] = {30, 50, 75, 100},
+        ["_script"] = "EventUpgrade | LuckyTileEnchantUpgrader",
+        ["Name"] = "Enchant Upgrade Chance",
+        ["TierDescription"] = "function: 0xc0210b6f55482507",
+        ["Icon"] = "rbxassetid://18459953844"
+    },
+    ["LuckyTileDiamonds"] = {
+        ["TierCosts"] = {
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 50}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 150}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 250}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 750}}
+        },
+        ["Disabled"] = true,
+        ["_id"] = "LuckyTileDiamonds",
+        ["Order"] = 3,
+        ["TierPowers"] = {100, 200, 300, 400},
+        ["_script"] = "EventUpgrade | LuckyTileDiamonds",
+        ["Name"] = "Diamonds",
+        ["TierDescription"] = "function: 0x23d4231f22d798c7",
+        ["Icon"] = "rbxassetid://14267663644"
+    },
+    ["LuckyTileDoubleItemsChance"] = {
+        ["TierCosts"] = {
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 200}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 500}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 1000}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 3000}}
+        },
+        ["Disabled"] = true,
+        ["_id"] = "LuckyTileDoubleItemsChance",
+        ["Order"] = 4,
+        ["TierPowers"] = {20, 40, 75, 100},
+        ["_script"] = "EventUpgrade | LuckyTileDoubleItemsChance",
+        ["Name"] = "Double Items Chance",
+        ["TierDescription"] = "function: 0x56f6eb029aa1a487",
+        ["Icon"] = "rbxassetid://15011599932"
+    },
+    ["LuckyTileDamage"] = {
+        ["TierCosts"] = {
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 25}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 75}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 125}},
+            {["_data"] = {["id"] = "Rave Upgrade Ticket", ["_am"] = 375}}
+        },
+        ["Disabled"] = true,
+        ["_id"] = "LuckyTileDamage",
+        ["Order"] = 6,
+        ["TierPowers"] = {30, 50, 75, 100},
+        ["_script"] = "EventUpgrade | LuckyTileDamage",
+        ["Name"] = "Damage",
+        ["TierDescription"] = "function: 0xee3b73e4420ddc07",
+        ["Icon"] = "rbxassetid://14985114550"
+    }
+}
