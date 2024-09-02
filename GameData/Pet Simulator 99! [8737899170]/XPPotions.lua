49490f94b79afa48a29f6233b1df2d8f0f6ad41e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-03 01:26:29 +1000

    Create XPPotions.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/XPPotions.lua
@@ -0,0 +1,90 @@
+data = {
+    ["Mastery"] = {
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0xffcaf5fad211ca77",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["_id"] = "Mastery",
+        ["DisplayName"] = "Mastery XP Potion",
+        ["Recipe"] = "Recipe",
+        ["Amount"] = 10000,
+        ["_script"] = "XPPotion | Mastery",
+        ["Desc"] = "Grants 10,000 XP to a Mastery of your choice!",
+        ["Icon"] = "rbxassetid://16976302916",
+        ["ItemId"] = "Mastery XP Potion"
+    },
+    ["Titanic"] = {
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0xffcaf5fad211ca77",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["_id"] = "Titanic",
+        ["DisplayName"] = "Titanic XP Potion",
+        ["Recipe"] = "Recipe",
+        ["Amount"] = 10000,
+        ["_script"] = "XPPotion | Titanic",
+        ["Desc"] = "Grants 10,000 XP to a Titanic pet of your choice!",
+        ["Icon"] = "rbxassetid://16976302798",
+        ["ItemId"] = "Titanic XP Potion"
+    },
+    ["Ultimate"] = {
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0xffcaf5fad211ca77",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["_id"] = "Ultimate",
+        ["DisplayName"] = "Ultimate XP Potion",
+        ["Recipe"] = "Recipe",
+        ["Amount"] = 10000,
+        ["_script"] = "XPPotion | Ultimate",
+        ["Desc"] = "Grants 10,000 XP to an Ultimate of your choice!",
+        ["Icon"] = "rbxassetid://16976302626",
+        ["ItemId"] = "Ultimate XP Potion"
+    },
+    ["Huge"] = {
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0xffcaf5fad211ca77",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["_id"] = "Huge",
+        ["DisplayName"] = "Huge XP Potion",
+        ["Recipe"] = "Recipe",
+        ["Amount"] = 10000,
+        ["_script"] = "XPPotion | Huge",
+        ["Desc"] = "Grants 10,000 XP to a Huge pet of your choice!",
+        ["Icon"] = "rbxassetid://16976303066",
+        ["ItemId"] = "Huge XP Potion"
+    }
+}
