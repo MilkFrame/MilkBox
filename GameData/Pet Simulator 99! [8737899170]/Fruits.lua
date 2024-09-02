fd69f3b5e551dac41469b4227154f04547abc3cb
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-03 01:35:12 +1000

    Update Fruits.lua

--- GameData/Pet Simulator 99! [8737899170]/Fruits.lua
+++ GameData/Pet Simulator 99! [8737899170]/Fruits.lua
@@ -1,4 +1,26 @@
 data = {
+    ["Watermelon"] = {
+        ["ShinyIcon"] = "rbxassetid://18577639540",
+        ["Rarity"] = {
+            ["RarityNumber"] = 2,
+            ["Lootbag"] = "Rare",
+            ["_id"] = "Rare",
+            ["Color"] = Color3.fromRGB(113, 255, 62),
+            ["DisplayName"] = "Rare",
+            ["Message"] = "function: 0x8b8eb006ee24b377",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "2 | Rare"
+        },
+        ["_id"] = "Watermelon",
+        ["Duration"] = 300,
+        ["DisplayName"] = "Watermelon",
+        ["_script"] = "Fruit | Watermelon",
+        ["Boost"] = {{["Amount"] = 1, ["Type"] = "Bonus"}},
+        ["Icon"] = "rbxassetid://16717381270",
+        ["Desc"] = [[Tasty snack for your pets! Boosts chance of bonuses by <X>%! \n(Stacks!)]]
+    },
     ["Candycane"] = {
         ["Rarity"] = {
             ["RarityNumber"] = 3,
@@ -6,7 +28,7 @@ data = {
             ["_id"] = "Epic",
             ["Color"] = Color3.fromRGB(41, 226, 255),
             ["DisplayName"] = "Epic",
-            ["Message"] = "function: 0x00000000962d710c",
+            ["Message"] = "function: 0x644e123f30c21b57",
             ["ItemSlot"] = "ItemSlot",
             ["Gradient"] = "Gradient",
             ["Announce"] = false,
@@ -19,116 +41,123 @@ data = {
         ["IgnoreFruitMachine"] = true,
         ["Boost"] = {{["Amount"] = 1.5, ["Type"] = "Drops"}},
         ["Icon"] = "rbxassetid://15636388090",
-        ["Desc"] = [[Christmas snack for your pets! Increases chance for drops by 1.5%! \n(Stacks!)]]
+        ["Desc"] = [[Christmas snack for your pets! Increases chance for drops by <X>%! \n(Stacks!)]]
     },
     ["Apple"] = {
-        ["_script"] = "Fruit | Apple",
-        ["Boost"] = {{["Amount"] = 1.5, ["Type"] = "Damage"}},
+        ["ShinyIcon"] = "rbxassetid://18577434379",
         ["Rarity"] = {
             ["RarityNumber"] = 2,
             ["Lootbag"] = "Rare",
             ["_id"] = "Rare",
             ["Color"] = Color3.fromRGB(113, 255, 62),
             ["DisplayName"] = "Rare",
-            ["Message"] = "function: 0x00000000a25a89dc",
+            ["Message"] = "function: 0x8b8eb006ee24b377",
             ["ItemSlot"] = "ItemSlot",
             ["Gradient"] = "Gradient",
             ["Announce"] = false,
             ["_script"] = "2 | Rare"
         },
         ["_id"] = "Apple",
-        ["Desc"] = [[Tasty snack for your pets! Increases pet damage by 1.5%! \n(Stacks!)]],
         ["Duration"] = 300,
+        ["DisplayName"] = "Apple",
+        ["_script"] = "Fruit | Apple",
+        ["Boost"] = {{["Amount"] = 1.5, ["Type"] = "Damage"}},
         ["Icon"] = "rbxassetid://15030757644",
-        ["DisplayName"] = "Apple"
+        ["Desc"] = "function: 0xedb81d06793ac577"
     },
     ["Rainbow"] = {
-        ["_script"] = "Fruit | Rainbow",
-        ["Boost"] = {
-            {["Amount"] = 5, ["Type"] = "Coins"},
-            {["Amount"] = 1, ["Type"] = "Diamonds"},
-            {["Amount"] = 1.5, ["Type"] = "Damage"},
-            {["Amount"] = 1, ["Type"] = "Drops"}
-        },
+        ["ShinyIcon"] = "rbxassetid://18577433891",
         ["Rarity"] = {
             ["RarityNumber"] = 4,
             ["Lootbag"] = "Legendary",
             ["_id"] = "Legendary",
             ["Color"] = Color3.fromRGB(255, 133, 34),
             ["DisplayName"] = "Legendary",
-            ["Message"] = "function: 0x00000000cd315c2c",
+            ["Message"] = "function: 0xebf7ab0b07e59c17",
             ["ItemSlot"] = "ItemSlot",
             ["Gradient"] = "Gradient",
             ["Announce"] = false,
             ["_script"] = "4 | Legendary"
         },
         ["_id"] = "Rainbow",
-        ["Desc"] = [[All of the fruits in one! \n(Stacks!)]],
+        ["preventShiny"] = true,
         ["Duration"] = 300,
+        ["DisplayName"] = "Rainbow Fruit",
+        ["_script"] = "Fruit | Rainbow",
+        ["Boost"] = {
+            {["Amount"] = 5, ["Type"] = "Coins"},
+            {["Amount"] = 1, ["Type"] = "Diamonds"},
+            {["Amount"] = 1.5, ["Type"] = "Damage"},
+            {["Amount"] = 1, ["Type"] = "Drops"},
+            {["Amount"] = 1, ["Type"] = "Bonus"}
+        },
         ["Icon"] = "rbxassetid://15030756988",
-        ["DisplayName"] = "Rainbow Fruit"
+        ["Desc"] = "function: 0xab98c268c785e297"
     },
     ["Pineapple"] = {
-        ["_script"] = "Fruit | Pineapple",
-        ["Boost"] = {{["Amount"] = 1, ["Type"] = "Drops"}},
+        ["ShinyIcon"] = "rbxassetid://18577434101",
         ["Rarity"] = {
             ["RarityNumber"] = 2,
             ["Lootbag"] = "Rare",
             ["_id"] = "Rare",
             ["Color"] = Color3.fromRGB(113, 255, 62),
             ["DisplayName"] = "Rare",
-            ["Message"] = "function: 0x00000000a25a89dc",
+            ["Message"] = "function: 0x8b8eb006ee24b377",
             ["ItemSlot"] = "ItemSlot",
             ["Gradient"] = "Gradient",
             ["Announce"] = false,
             ["_script"] = "2 | Rare"
         },
         ["_id"] = "Pineapple",
-        ["Desc"] = [[Tasty snack for your pets! Increases chance for drops by 1%! \n(Stacks!)]],
         ["Duration"] = 300,
+        ["DisplayName"] = "Pineapple",
+        ["_script"] = "Fruit | Pineapple",
+        ["Boost"] = {{["Amount"] = 1, ["Type"] = "Drops"}},
         ["Icon"] = "rbxassetid://15030757105",
-        ["DisplayName"] = "Pineapple"
+        ["Desc"] = "function: 0xbf91937758446ed7"
     },
     ["Orange"] = {
-        ["_script"] = "Fruit | Orange",
-        ["Boost"] = {{["Amount"] = 1, ["Type"] = "Diamonds"}},
+        ["ShinyIcon"] = "rbxassetid://18577433264",
         ["Rarity"] = {
             ["RarityNumber"] = 2,
             ["Lootbag"] = "Rare",
             ["_id"] = "Rare",
             ["Color"] = Color3.fromRGB(113, 255, 62),
             ["DisplayName"] = "Rare",
-            ["Message"] = "function: 0x00000000a25a89dc",
+            ["Message"] = "function: 0x8b8eb006ee24b377",
             ["ItemSlot"] = "ItemSlot",
             ["Gradient"] = "Gradient",
             ["Announce"] = false,
             ["_script"] = "2 | Rare"
         },
         ["_id"] = "Orange",
-        ["Desc"] = [[Tasty snack for your pets! Boosts Diamonds by 1%! \n(Stacks!)]],
         ["Duration"] = 300,
+        ["DisplayName"] = "Orange",
+        ["_script"] = "Fruit | Orange",
+        ["Boost"] = {{["Amount"] = 1, ["Type"] = "Diamonds"}},
         ["Icon"] = "rbxassetid://15030757291",
-        ["DisplayName"] = "Orange"
+        ["Desc"] = "function: 0x53fda5127804a617"
     },
     ["Banana"] = {
-        ["_script"] = "Fruit | Banana",
-        ["Boost"] = {{["Amount"] = 5, ["Type"] = "Coins"}},
+        ["ShinyIcon"] = "rbxassetid://18577433706",
         ["Rarity"] = {
             ["RarityNumber"] = 2,
             ["Lootbag"] = "Rare",
             ["_id"] = "Rare",
             ["Color"] = Color3.fromRGB(113, 255, 62),
             ["DisplayName"] = "Rare",
-            ["Message"] = "function: 0x00000000a25a89dc",
+            ["Message"] = "function: 0x8b8eb006ee24b377",
             ["ItemSlot"] = "ItemSlot",
             ["Gradient"] = "Gradient",
             ["Announce"] = false,
             ["_script"] = "2 | Rare"
         },
         ["_id"] = "Banana",
-        ["Desc"] = [[Tasty snack for your pets! Boosts Coins by 5%! \n(Stacks!)]],
         ["Duration"] = 300,
+        ["DisplayName"] = "Banana",
+        ["_script"] = "Fruit | Banana",
+        ["Boost"] = {{["Amount"] = 5, ["Type"] = "Coins"}},
         ["Icon"] = "rbxassetid://15030757462",
-        ["DisplayName"] = "Banana"
+        ["Desc"] = "function: 0x17ead55298c7e257"
     }
 }
