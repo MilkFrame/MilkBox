0e69684341116583cad7e8ec8cb580afe69aead3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-03 01:42:08 +1000

    Create LeaderboardContests.lua

---
+++ GameData/Pet Simulator 99! [8737899170]/LeaderboardContests.lua
@@ -0,0 +1,142 @@
+data = {
+    ["Good"] = {
+        ["Leaderboard"] = "Good",
+        ["_script"] = "LeaderboardContests | Good",
+        ["Name"] = "Good",
+        ["Winners"] = {},
+        ["_id"] = "Good",
+        ["EndTimestamp"] = 1720281780,
+        ["RewardGroups"] = {
+            {["_data"] = {["id"] = "Huge Avenging Griffin", ["pt"] = 2}},
+            {["_data"] = {["id"] = "Huge Avenging Griffin", ["pt"] = 1}},
+            {["_data"] = {["id"] = "Huge Avenging Griffin"}}
+        },
+        ["SelectGroups"] = {25, 250, 1000}
+    },
+    ["RNGPoints"] = {
+        ["Leaderboard"] = "RNGPoints",
+        ["_script"] = "LeaderboardContests | RNGPoints",
+        ["Name"] = "RNGPoints",
+        ["Winners"] = {},
+        ["_id"] = "RNGPoints",
+        ["EndTimestamp"] = 1724428800,
+        ["RewardGroups"] = {
+            {["_data"] = {["id"] = "Huge Atomic Forged Shark", ["pt"] = 2}},
+            {["_data"] = {["id"] = "Huge Atomic Forged Shark", ["pt"] = 1}},
+            {["_data"] = {["id"] = "Huge Atomic Forged Shark"}}
+        },
+        ["SelectGroups"] = {25, 250, 1000}
+    },
+    ["EvilHuges"] = {
+        ["Leaderboard"] = "EvilHuges",
+        ["_script"] = "LeaderboardContests | EvilHuges",
+        ["Name"] = "EvilHuges",
+        ["Winners"] = {},
+        ["_id"] = "EvilHuges",
+        ["EndTimestamp"] = 1720281660,
+        ["RewardGroups"] = {
+            {["_data"] = {["id"] = "Huge Evil Imp", ["pt"] = 2}},
+            {["_data"] = {["id"] = "Huge Evil Imp", ["pt"] = 1}},
+            {["_data"] = {["id"] = "Huge Evil Imp"}}
+        },
+        ["SelectGroups"] = {25, 250, 1000}
+    },
+    ["Evil"] = {
+        ["Leaderboard"] = "Evil",
+        ["_script"] = "LeaderboardContests | Evil",
+        ["Name"] = "Evil",
+        ["Winners"] = {},
+        ["_id"] = "Evil",
+        ["EndTimestamp"] = 1720281720,
+        ["RewardGroups"] = {
+            {["_data"] = {["id"] = "Huge Evil Imp", ["pt"] = 2}},
+            {["_data"] = {["id"] = "Huge Evil Imp", ["pt"] = 1}},
+            {["_data"] = {["id"] = "Huge Evil Imp"}}
+        },
+        ["SelectGroups"] = {25, 250, 1000}
+    },
+    ["GoodRace"] = {
+        ["Leaderboard"] = "GoodRace",
+        ["_script"] = "LeaderboardContests | GoodRace",
+        ["Name"] = "GoodRace",
+        ["Winners"] = {},
+        ["_id"] = "GoodRace",
+        ["EndTimestamp"] = 1720281900,
+        ["RewardGroups"] = {
+            {["_data"] = {["id"] = "Huge Sage Axolotl", ["pt"] = 2}},
+            {["_data"] = {["id"] = "Huge Sage Axolotl", ["pt"] = 1}},
+            {["_data"] = {["id"] = "Huge Sage Axolotl"}}
+        },
+        ["SelectGroups"] = {25, 250, 1000}
+    },
+    ["RNGRolls"] = {
+        ["Leaderboard"] = "RNGRolls",
+        ["_script"] = "LeaderboardContests | RNGRolls",
+        ["Name"] = "RNGRolls",
+        ["Winners"] = {},
+        ["_id"] = "RNGRolls",
+        ["EndTimestamp"] = 1724428860,
+        ["RewardGroups"] = {
+            {["_data"] = {["id"] = "Huge Atomic Forged Shark", ["pt"] = 2}},
+            {["_data"] = {["id"] = "Huge Atomic Forged Shark", ["pt"] = 1}},
+            {["_data"] = {["id"] = "Huge Atomic Forged Shark"}}
+        },
+        ["SelectGroups"] = {25, 250, 1000}
+    },
+    ["GoodHuges"] = {
+        ["Leaderboard"] = "GoodHuges",
+        ["_script"] = "LeaderboardContests | GoodHuges",
+        ["Name"] = "GoodHuges",
+        ["Winners"] = {},
+        ["_id"] = "GoodHuges",
+        ["EndTimestamp"] = 1720281840,
+        ["RewardGroups"] = {
+            {["_data"] = {["id"] = "Huge Avenging Griffin", ["pt"] = 2}},
+            {["_data"] = {["id"] = "Huge Avenging Griffin", ["pt"] = 1}},
+            {["_data"] = {["id"] = "Huge Avenging Griffin"}}
+        },
+        ["SelectGroups"] = {25, 250, 1000}
+    },
+    ["EvilRace"] = {
+        ["Leaderboard"] = "EvilRace",
+        ["_script"] = "LeaderboardContests | EvilRace",
+        ["Name"] = "EvilRace",
+        ["Winners"] = {},
+        ["_id"] = "EvilRace",
+        ["EndTimestamp"] = 1720281960,
+        ["RewardGroups"] = {
+            {["_data"] = {["id"] = "Huge Devil Agony", ["pt"] = 2}},
+            {["_data"] = {["id"] = "Huge Devil Agony", ["pt"] = 1}},
+            {["_data"] = {["id"] = "Huge Devil Agony"}}
+        },
+        ["SelectGroups"] = {25, 250, 1000}
+    },
+    ["GoodFurthest"] = {
+        ["Leaderboard"] = "GoodFurthest",
+        ["_script"] = "LeaderboardContests | GoodFurthest",
+        ["Name"] = "GoodFurthest",
+        ["Winners"] = {},
+        ["_id"] = "GoodFurthest",
+        ["EndTimestamp"] = 1720282080,
+        ["RewardGroups"] = {
+            {["_data"] = {["id"] = "Huge Sage Axolotl", ["pt"] = 2}},
+            {["_data"] = {["id"] = "Huge Sage Axolotl", ["pt"] = 1}},
+            {["_data"] = {["id"] = "Huge Sage Axolotl"}}
+        },
+        ["SelectGroups"] = {25, 250, 1000}
+    },
+    ["EvilFurthest"] = {
+        ["Leaderboard"] = "EvilFurthest",
+        ["_script"] = "LeaderboardContests | EvilFurthest",
+        ["Name"] = "EvilRace",
+        ["Winners"] = {},
+        ["_id"] = "EvilFurthest",
+        ["EndTimestamp"] = 1720282020,
+        ["RewardGroups"] = {
+            {["_data"] = {["id"] = "Huge Devil Agony", ["pt"] = 2}},
+            {["_data"] = {["id"] = "Huge Devil Agony", ["pt"] = 1}},
+            {["_data"] = {["id"] = "Huge Devil Agony"}}
+        },
+        ["SelectGroups"] = {25, 250, 1000}
+    }
+}
