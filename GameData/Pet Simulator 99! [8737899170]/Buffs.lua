0cac851e79e0c21adbe4187ab52cfb619b99b8d6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-03 01:18:13 +1000

    Update Buffs.lua

--- GameData/Pet Simulator 99! [8737899170]/Buffs.lua
+++ GameData/Pet Simulator 99! [8737899170]/Buffs.lua
@@ -1,19 +1,22 @@
 data = {
-    ["Toy Bone"] = {
-        ["_script"] = "Buff | Toy Bone",
-        ["AssociatedItemID"] = "Toy Bone",
+    ["Lucky Dice III"] = {
+        ["_script"] = "Buff | Lucky Dice III",
+        ["AssociatedItemID"] = "Lucky Dice III",
         ["AssociatedItemClass"] = "Misc",
+        ["_id"] = "Lucky Dice III",
         ["Length"] = 300,
-        ["_id"] = "Toy Bone",
-        ["DisplayName"] = "Toy Bone"
+        ["IgnoreInstancePause"] = true,
+        ["DisplayName"] = "Lucky Dice III"
     },
-    ["Christmas Cookie"] = {
-        ["_script"] = "Buff | Christmas Cookie",
-        ["AssociatedItemID"] = "Christmas Cookie",
+    ["Mega Lucky Dice"] = {
+        ["_script"] = "Buff | Mega Lucky Dice",
+        ["_id"] = "Mega Lucky Dice",
+        ["AssociatedItemID"] = "Mega Lucky Dice",
         ["AssociatedItemClass"] = "Misc",
-        ["Length"] = 300,
-        ["_id"] = "Christmas Cookie",
-        ["DisplayName"] = "Christmas Cookie"
+        ["StackMode"] = true,
+        ["Length"] = 1,
+        ["IgnoreInstancePause"] = true,
+        ["DisplayName"] = "Mega Lucky Dice"
     },
     ["Toy Ball"] = {
         ["_script"] = "Buff | Toy Ball",
@@ -23,13 +26,77 @@ data = {
         ["_id"] = "Toy Ball",
         ["DisplayName"] = "Toy Ball"
     },
-    ["Squeaky Toy"] = {
-        ["_script"] = "Buff | Squeaky Toy",
-        ["AssociatedItemID"] = "Squeaky Toy",
+    ["Ultra Rng Chest Luck"] = {
+        ["_script"] = "Buff | Ultra Rng Chest Luck",
+        ["AssociatedItemID"] = "Ultra Rng Chest Luck",
         ["AssociatedItemClass"] = "Misc",
+        ["_id"] = "Ultra Rng Chest Luck",
+        ["Length"] = 120,
+        ["IgnoreInstancePause"] = true,
+        ["DisplayName"] = "Ultra RNG Chest Luck"
+    },
+    ["Christmas Cookie"] = {
+        ["_script"] = "Buff | Christmas Cookie",
+        ["AssociatedItemID"] = "Christmas Cookie",
+        ["AssociatedItemClass"] = "Misc",
+        ["Length"] = 300,
+        ["_id"] = "Christmas Cookie",
+        ["DisplayName"] = "Christmas Cookie"
+    },
+    ["Fire Dice"] = {
+        ["_script"] = "Buff | Fire Dice",
+        ["AssociatedItemID"] = "Fire Dice",
+        ["AssociatedItemClass"] = "Misc",
+        ["_id"] = "Fire Dice",
+        ["Length"] = 60,
+        ["IgnoreInstancePause"] = true,
+        ["DisplayName"] = "Fire Dice"
+    },
+    ["Lucky Dice II"] = {
+        ["_script"] = "Buff | Lucky Dice II",
+        ["AssociatedItemID"] = "Lucky Dice II",
+        ["AssociatedItemClass"] = "Misc",
+        ["_id"] = "Lucky Dice II",
+        ["Length"] = 300,
+        ["IgnoreInstancePause"] = true,
+        ["DisplayName"] = "Lucky Dice II"
+    },
+    ["Lucky Dice"] = {
+        ["_script"] = "Buff | Lucky Dice",
+        ["AssociatedItemID"] = "Lucky Dice",
+        ["AssociatedItemClass"] = "Misc",
+        ["_id"] = "Lucky Dice",
+        ["Length"] = 60,
+        ["IgnoreInstancePause"] = true,
+        ["DisplayName"] = "Lucky Dice"
+    },
+    ["Super Rng Chest Luck"] = {
+        ["_script"] = "Buff | Super Rng Chest Luck",
+        ["AssociatedItemID"] = "Super Rng Chest Luck",
+        ["AssociatedItemClass"] = "Misc",
+        ["_id"] = "Super Rng Chest Luck",
+        ["Length"] = 120,
+        ["IgnoreInstancePause"] = true,
+        ["DisplayName"] = "Super RNG Chest Luck"
+    },
+    ["Mega Lucky Dice II"] = {
+        ["_script"] = "Buff | Mega Lucky Dice II",
+        ["_id"] = "Mega Lucky Dice II",
+        ["AssociatedItemID"] = "Mega Lucky Dice II",
+        ["AssociatedItemClass"] = "Misc",
+        ["StackMode"] = true,
+        ["Length"] = 1,
+        ["IgnoreInstancePause"] = true,
+        ["DisplayName"] = "Mega Lucky Dice II"
+    },
+    ["Nightmare Fuel"] = {
+        ["_script"] = "Buff | Nightmare Fuel",
+        ["AssociatedItemID"] = "Nightmare Fuel",
+        ["AssociatedItemClass"] = "Misc",
+        ["_id"] = "Nightmare Fuel",
         ["Length"] = 600,
-        ["_id"] = "Squeaky Toy",
-        ["DisplayName"] = "Squeaky Toy"
+        ["IgnoreInstancePause"] = true,
+        ["DisplayName"] = "Nightmare Fuel"
     },
     ["Hot Cocoa"] = {
         ["_script"] = "Buff | Hot Cocoa",
@@ -38,5 +105,58 @@ data = {
         ["Length"] = 300,
         ["_id"] = "Hot Cocoa",
         ["DisplayName"] = "Hot Cocoa"
+    },
+    ["Glitched Drive"] = {
+        ["_script"] = "Buff | Glitched Drive",
+        ["AssociatedItemID"] = "Glitched Drive",
+        ["AssociatedItemClass"] = "Misc",
+        ["Length"] = 300,
+        ["_id"] = "Glitched Drive",
+        ["DisplayName"] = "Glitched Drive"
+    },
+    ["Rng Chest Luck"] = {
+        ["_script"] = "Buff | Rng Chest Luck",
+        ["AssociatedItemID"] = "Rng Chest Luck",
+        ["AssociatedItemClass"] = "Misc",
+        ["_id"] = "Rng Chest Luck",
+        ["Length"] = 120,
+        ["IgnoreInstancePause"] = true,
+        ["DisplayName"] = "RNG Chest Luck"
+    },
+    ["Lucky Tile"] = {
+        ["_script"] = "Buff | Lucky Tile",
+        ["_id"] = "Lucky Tile",
+        ["AssociatedItemID"] = "Lucky Tile",
+        ["AssociatedItemClass"] = "Misc",
+        ["StackMode"] = true,
+        ["Length"] = 1,
+        ["IgnoreInstancePause"] = true,
+        ["DisplayName"] = "Lucky Tile Boost"
+    },
+    ["Ultra Lucky Tile"] = {
+        ["_script"] = "Buff | Ultra Lucky Tile",
+        ["_id"] = "Ultra Lucky Tile",
+        ["AssociatedItemID"] = "Ultra Lucky Tile",
+        ["AssociatedItemClass"] = "Misc",
+        ["StackMode"] = true,
+        ["Length"] = 1,
+        ["IgnoreInstancePause"] = true,
+        ["DisplayName"] = "Ultra Lucky Tile Boost"
+    },
+    ["Squeaky Toy"] = {
+        ["_script"] = "Buff | Squeaky Toy",
+        ["AssociatedItemID"] = "Squeaky Toy",
+        ["AssociatedItemClass"] = "Misc",
+        ["Length"] = 600,
+        ["_id"] = "Squeaky Toy",
+        ["DisplayName"] = "Squeaky Toy"
+    },
+    ["Toy Bone"] = {
+        ["_script"] = "Buff | Toy Bone",
+        ["AssociatedItemID"] = "Toy Bone",
+        ["AssociatedItemClass"] = "Misc",
+        ["Length"] = 300,
+        ["_id"] = "Toy Bone",
+        ["DisplayName"] = "Toy Bone"
     }
 }
