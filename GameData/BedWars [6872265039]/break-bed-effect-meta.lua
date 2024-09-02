0246b268576ba2c74723d2bba690b929e3ec86e3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-03 01:55:14 +1000

    Create break-bed-effect-meta.lua

---
+++ GameData/BedWars [6872265039]/break-bed-effect-meta.lua
@@ -0,0 +1,9 @@
+data = {
+    ["BreakBedEffectMeta"] = {
+        ["meteor"] = {["name"] = "Meteor", ["image"] = "rbxassetid://95118495480315"},
+        ["crystallize"] = {["name"] = "Crystallized", ["image"] = "rbxassetid://18963958790"},
+        ["lightning_strike"] = {["name"] = "Lightning Strike", ["image"] = "rbxassetid://18567011312"},
+        ["void_portal"] = {["name"] = "Event Horizon", ["image"] = "rbxassetid://18963891855"},
+        ["none"] = {["name"] = "None"}
+    }
+}
