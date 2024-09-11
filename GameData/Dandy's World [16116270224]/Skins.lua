12e2b753dd5179997a24e41c10228d521bbbb079
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-12 01:36:35 +1000

    Create Skins.lua

---
+++ GameData/Dandy's World [16116270224]/Skins.lua
@@ -0,0 +1,32 @@
+data = {
+    ["Toodles"] = {["VintageToodles"] = {["Name"] = "Vintage Toodles", ["ApplySkin"] = "function: 0x0d4423630a6bda03"}},
+    ["Tisha"] = {["VintageTisha"] = {["Name"] = "Vintage Tisha", ["ApplySkin"] = "function: 0x727f295df955c843"}},
+    ["RazzleDazzle"] = {
+        ["VintageRazzleDazzle"] = {["Name"] = "Vintage Razzle & Dazzle", ["ApplySkin"] = "function: 0x67f44460fc2f6943"}
+    },
+    ["Rodger"] = {["VintageRodger"] = {["Name"] = "Vintage Rodger", ["ApplySkin"] = "function: 0x183c53d2481e2a63"}},
+    ["Boxten"] = {["VintageBoxten"] = {["Name"] = "Vintage Boxten", ["ApplySkin"] = "function: 0x18401e162f3615a3"}},
+    ["Astro"] = {["VintageAstro"] = {["Name"] = "Vintage Astro", ["ApplySkin"] = "function: 0x31b903f6da1726c3"}},
+    ["Glisten"] = {["VintageGlisten"] = {["Name"] = "Vintage Glisten", ["ApplySkin"] = "function: 0xf1b64f22de5541a3"}},
+    ["Finn"] = {["VintageFinn"] = {["Name"] = "Vintage Finn", ["ApplySkin"] = "function: 0xa3bc0fdb749948c3"}},
+    ["Brightney"] = {
+        ["VintageBrightney"] = {["Name"] = "Vintage Brightney", ["ApplySkin"] = "function: 0x53181421a50fcd43"}
+    },
+    ["Goob"] = {
+        ["VintageGoob"] = {
+            ["ApplySkin"] = "function: 0x48c674074de5cdc3",
+            ["Name"] = "Vintage Goob",
+            ["UseAbility"] = "function: 0x4e5428a16ceed8a3"
+        }
+    },
+    ["Vee"] = {["VintageVee"] = {["Name"] = "Vintage Vee", ["ApplySkin"] = "function: 0xeb4f6ff3b7ed4d83"}},
+    ["Flutter"] = {["VintageFlutter"] = {["Name"] = "Vintage Flutter", ["ApplySkin"] = "function: 0x5eb1b4be7bc6ab03"}},
+    ["Poppy"] = {["VintagePoppy"] = {["Name"] = "Vintage Poppy", ["ApplySkin"] = "function: 0x1cc4c6dd45640863"}},
+    ["Sprout"] = {["VintageSprout"] = {["Name"] = "Vintage Sprout", ["ApplySkin"] = "function: 0x144bb504c6299383"}},
+    ["Teagan"] = {["VintageTeagan"] = {["Name"] = "Vintage Teagan", ["ApplySkin"] = "function: 0x59ef07c7efde2283"}},
+    ["Scraps"] = {["VintageScraps"] = {["Name"] = "Vintage Scraps", ["ApplySkin"] = "function: 0x08c0cadfae60df43"}},
+    ["Cosmo"] = {["VintageCosmo"] = {["Name"] = "Vintage Cosmo", ["ApplySkin"] = "function: 0x23dab1353a3597c3"}},
+    ["Pebble"] = {["VintagePebble"] = {["Name"] = "Vintage Pebble", ["ApplySkin"] = "function: 0x9d5b9efb420b12c3"}},
+    ["Shrimpo"] = {["VintageShrimpo"] = {["Name"] = "Vintage Shrimpo", ["ApplySkin"] = "function: 0x137bad8da37e2d23"}},
+    ["Shelly"] = {["VintageShelly"] = {["Name"] = "Vintage Shelly", ["ApplySkin"] = "function: 0xda5428aa31fbce43"}}
+}
