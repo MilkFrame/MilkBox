0d1b302e26ef63703b7a72e8d58894c08596e1e7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-12 01:48:29 +1000

    Create GardeningConstants.lua

---
+++ GameData/MeepCity [370731277]/GardeningConstants.lua
@@ -0,0 +1,68 @@
+data = {
+    ["WATER_LEVELS"] = {
+        {["ObjectColor"] = 358},
+        {["ObjectColor"] = 345},
+        {["ObjectColor"] = 302},
+        {["ObjectColor"] = 1001}
+    },
+    ["WILTED_ASSETID"] = 238,
+    ["SPROUT_ASSETID"] = 237,
+    ["FLOWERS"] = {
+        {["ShovelLevel"] = 1, ["AssetId"] = 236},
+        {["ShovelLevel"] = 1, ["AssetId"] = 239},
+        {["ShovelLevel"] = 1, ["AssetId"] = 240},
+        {["ShovelLevel"] = 1, ["AssetId"] = 241},
+        {["ShovelLevel"] = 1, ["AssetId"] = 242},
+        {["ShovelLevel"] = 2, ["AssetId"] = 243},
+        {["ShovelLevel"] = 2, ["AssetId"] = 244},
+        {["ShovelLevel"] = 2, ["AssetId"] = 245},
+        {["ShovelLevel"] = 2, ["AssetId"] = 246},
+        {["ShovelLevel"] = 2, ["AssetId"] = 247},
+        {["ShovelLevel"] = 3, ["AssetId"] = 248},
+        {["ShovelLevel"] = 3, ["AssetId"] = 249},
+        {["ShovelLevel"] = 3, ["AssetId"] = 250},
+        {["ShovelLevel"] = 3, ["AssetId"] = 251},
+        {["ShovelLevel"] = 3, ["AssetId"] = 252},
+        {["ShovelLevel"] = 2, ["AssetId"] = 683, ["PLUSOnly"] = true},
+        {["ShovelLevel"] = 3, ["AssetId"] = 684, ["PLUSOnly"] = true},
+        {["ShovelLevel"] = 1, ["AssetId"] = 861, ["PLUSOnly"] = true}
+    },
+    ["SHOVELS"] = {
+        {
+            ["ExpRequired"] = 0,
+            ["CoinsCost"] = 5,
+            ["Title"] = "GARDENING_TIN_SHOVEL_TITLE",
+            ["ExpPerPick"] = 1,
+            ["Level"] = 1,
+            ["CoinsPerPick"] = 25,
+            ["ShovelBrickColorId"] = 311
+        },
+        {
+            ["ExpRequired"] = 28,
+            ["CoinsCost"] = 8,
+            ["Title"] = "GARDENING_BRONZE_SHOVEL_TITLE",
+            ["ExpPerPick"] = 2,
+            ["Level"] = 2,
+            ["CoinsPerPick"] = 50,
+            ["ShovelBrickColorId"] = 362
+        },
+        {
+            ["ExpRequired"] = 112,
+            ["CoinsCost"] = 10,
+            ["Title"] = "GARDENING_SILVER_SHOVEL_TITLE",
+            ["ExpPerPick"] = 3,
+            ["Level"] = 3,
+            ["CoinsPerPick"] = 75,
+            ["ShovelBrickColorId"] = 348
+        },
+        {
+            ["ExpRequired"] = 300,
+            ["CoinsCost"] = 10,
+            ["Title"] = "GARDENING_GOLD_SHOVEL_TITLE",
+            ["ExpPerPick"] = 4,
+            ["Level"] = 4,
+            ["CoinsPerPick"] = 100,
+            ["ShovelBrickColorId"] = 348
+        }
+    }
+}
