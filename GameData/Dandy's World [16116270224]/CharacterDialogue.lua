32e4a524515af2a51d30cbf66a80c950dc77ab2e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-12 01:35:51 +1000

    Create CharacterDialogue.lua

---
+++ GameData/Dandy's World [16116270224]/CharacterDialogue.lua
@@ -0,0 +1,39 @@
+data = {
+    ["Toodles"] = {"Oh Toodles! She is always looking for ways to help out!"},
+    ["Tisha"] = {"Ah Tisha! I've never seen a Toon more dedicated to their work than her!"},
+    ["DandyGamepassStore"] = {
+        "I've got some special Toon Licenses that I think you'd love to buy!",
+        "Buy Toon Licenses that aren't always available here for 'Robux'!"
+    },
+    ["RazzleDazzle"] = {"Razzle & Dazzle! Hehe!"},
+    ["Rodger"] = {"Ohhh, Rodger! That Toon has a keen eye when it comes to his research!"},
+    ["Boxten"] = {"Boxten is a great choice! He seems more comfortable with the Toons he knows."},
+    ["Dandy"] = {"...Who are you?"},
+    ["Flutter"] = {"Flutter! I wonder how it feels to fly and be free!"},
+    ["DandyStore"] = {"Hi there! Welcome to my Shop!"},
+    ["Astro"] = {". . ."},
+    ["DandyToonStore"] = {"Buy Toon Licenses here if you have the requirements for them!"},
+    ["Teagan"] = {"Oh Teagan! Hope you guys don't have a tea party without me!"},
+    ["Cosmo"] = {"Cosmo, he's sweet! Literally, tee hee!"},
+    ["Shelly"] = {"Huh? Shelly? Alright I guess..."},
+    ["Brightney"] = {"Brightney will surely be the 'light' of the party! Hehe!"},
+    ["Shrimpo"] = {"Shrimpo? Oh, how I pity you."},
+    ["Glisten"] = {"Glisten? He's pretty handy with that reflection trick of his!"},
+    ["BuyTrinket"] = {
+        "Thank you!",
+        "Enjoy your Trinket!",
+        "You'll make great use of that Trinket, I'm sure!",
+        "This Trinket should help you on your journey!"
+    },
+    ["Poppy"] = {"Poppy is a great choice! I'm always seeing her scurry off when she's needed!"},
+    ["Sprout"] = {"Sprout! He sure knows how to push people... -To do their best!"},
+    ["DandyTrinket"] = {
+        "I've got some Trinkets here that I think you'd love to use!",
+        "Trinkets give you special perks that will enhance your playstyle!"
+    },
+    ["Scraps"] = {"Scraps is a good choice! She always knows how to get out of trouble!"},
+    ["Finn"] = {"Finn! What a funny Toon! ...Good luck with his fish puns."},
+    ["Pebble"] = {"Hey! That's my pet rock! Take good care of him will ya?"},
+    ["Vee"] = {"Oh... Vee."},
+    ["Goob"] = {"Goob is such a fun Toon! Careful though, he's a hugger!"}
+}
