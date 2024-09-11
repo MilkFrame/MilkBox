a68bdc177747605a7293397c2044882380b6a427
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-12 01:48:17 +1000

    Create FishingConstants.lua

---
+++ GameData/MeepCity [370731277]/FishingConstants.lua
@@ -0,0 +1,340 @@
+data = {
+    ["MAX_RARITY"] = 10,
+    ["FISH_DICTIONARY"] = {
+        {
+            ["Title"] = "Plastic",
+            ["SPECIES"] = {
+                {
+                    ["Image"] = 431259984,
+                    ["Title"] = "Solid Bright red",
+                    ["WeightMin"] = 1,
+                    ["Rarity"] = 1,
+                    ["WeightMax"] = 3
+                },
+                {
+                    ["Image"] = 431260233,
+                    ["Title"] = "Solid Bright blue",
+                    ["WeightMin"] = 1,
+                    ["Rarity"] = 4,
+                    ["WeightMax"] = 1
+                },
+                {
+                    ["Image"] = 431260412,
+                    ["Title"] = "Solid Bright green",
+                    ["WeightMin"] = 3,
+                    ["Rarity"] = 5,
+                    ["WeightMax"] = 5
+                },
+                {
+                    ["Image"] = 431260696,
+                    ["Title"] = "Solid Bright yellow",
+                    ["WeightMin"] = 3,
+                    ["Rarity"] = 3,
+                    ["WeightMax"] = 5
+                },
+                {
+                    ["Image"] = 431263038,
+                    ["Title"] = "Solid Bright violet",
+                    ["WeightMin"] = 1,
+                    ["Rarity"] = 2,
+                    ["WeightMax"] = 5
+                }
+            }
+        },
+        {
+            ["Title"] = "Wood",
+            ["SPECIES"] = {
+                {
+                    ["Image"] = 431952173,
+                    ["Title"] = "Wooden Carnation pink",
+                    ["WeightMin"] = 2,
+                    ["Rarity"] = 1,
+                    ["WeightMax"] = 6
+                },
+                {
+                    ["Image"] = 431952430,
+                    ["Title"] = "Wooden Pastel green",
+                    ["WeightMin"] = 2,
+                    ["Rarity"] = 9,
+                    ["WeightMax"] = 6
+                },
+                {
+                    ["Image"] = 431952753,
+                    ["Title"] = "Wooden Neon orange",
+                    ["WeightMin"] = 5,
+                    ["Rarity"] = 4,
+                    ["WeightMax"] = 11
+                },
+                {
+                    ["Image"] = 431953300,
+                    ["Title"] = "Wooden Bronze",
+                    ["WeightMin"] = 2,
+                    ["Rarity"] = 3,
+                    ["WeightMax"] = 6
+                },
+                {
+                    ["Image"] = 431953704,
+                    ["Title"] = "Wooden Sea green",
+                    ["WeightMin"] = 5,
+                    ["Rarity"] = 2,
+                    ["WeightMax"] = 11
+                }
+            }
+        },
+        {
+            ["Title"] = "Pebble",
+            ["SPECIES"] = {
+                {
+                    ["Image"] = 431960103,
+                    ["Title"] = "Pebble Alder",
+                    ["WeightMin"] = 2,
+                    ["Rarity"] = 1,
+                    ["WeightMax"] = 8
+                },
+                {
+                    ["Image"] = 431960467,
+                    ["Title"] = "Pebble Cashmere",
+                    ["WeightMin"] = 2,
+                    ["Rarity"] = 4,
+                    ["WeightMax"] = 8
+                },
+                {
+                    ["Image"] = 431960984,
+                    ["Title"] = "Pebble Dirt brown",
+                    ["WeightMin"] = 2,
+                    ["Rarity"] = 2,
+                    ["WeightMax"] = 8
+                },
+                {
+                    ["Image"] = 431961550,
+                    ["Title"] = "Pebble Lily white",
+                    ["WeightMin"] = 2,
+                    ["Rarity"] = 6,
+                    ["WeightMax"] = 8
+                }
+            }
+        },
+        {
+            ["Title"] = "Concrete",
+            ["SPECIES"] = {
+                {
+                    ["Image"] = 444482693,
+                    ["Title"] = "Concrete Dark stone grey",
+                    ["WeightMin"] = 1,
+                    ["Rarity"] = 1,
+                    ["WeightMax"] = 5
+                },
+                {
+                    ["Image"] = 444482832,
+                    ["Title"] = "Concrete Medium stone grey",
+                    ["WeightMin"] = 2,
+                    ["Rarity"] = 2,
+                    ["WeightMax"] = 6
+                },
+                {
+                    ["Image"] = 444482991,
+                    ["Title"] = "Concrete Flint",
+                    ["WeightMin"] = 5,
+                    ["Rarity"] = 5,
+                    ["WeightMax"] = 10
+                },
+                {
+                    ["Image"] = 444483180,
+                    ["Title"] = "Concrete Mid gray",
+                    ["WeightMin"] = 1,
+                    ["Rarity"] = 7,
+                    ["WeightMax"] = 5
+                },
+                {
+                    ["Image"] = 444483332,
+                    ["Title"] = "Concrete Smoky gray",
+                    ["WeightMin"] = 1,
+                    ["Rarity"] = 10,
+                    ["WeightMax"] = 5
+                }
+            }
+        },
+        {
+            ["Title"] = "Neon",
+            ["SPECIES"] = {
+                {
+                    ["Image"] = 1390887532,
+                    ["Title"] = "Neon Really blue",
+                    ["WeightMin"] = 6,
+                    ["Rarity"] = 9,
+                    ["WeightMax"] = 10
+                },
+                {
+                    ["Image"] = 1390888232,
+                    ["Title"] = "Neon Hot pink",
+                    ["WeightMin"] = 6,
+                    ["Rarity"] = 9,
+                    ["WeightMax"] = 10
+                }
+            }
+        },
+        {
+            ["Title"] = "Grass",
+            ["SPECIES"] = {
+                {
+                    ["Image"] = 444488398,
+                    ["Title"] = "Grassy bright green",
+                    ["WeightMin"] = 7,
+                    ["Rarity"] = 1,
+                    ["WeightMax"] = 15
+                },
+                {
+                    ["Image"] = 444488524,
+                    ["Title"] = "Grassy Olivine",
+                    ["WeightMin"] = 18,
+                    ["Rarity"] = 6,
+                    ["WeightMax"] = 20
+                },
+                {
+                    ["Image"] = 444488761,
+                    ["Title"] = "Grassy Shamrock",
+                    ["WeightMin"] = 1,
+                    ["Rarity"] = 5,
+                    ["WeightMax"] = 5
+                },
+                {
+                    ["Image"] = 444488896,
+                    ["Title"] = "Grassy Parsley green",
+                    ["WeightMin"] = 3,
+                    ["Rarity"] = 4,
+                    ["WeightMax"] = 7
+                },
+                {
+                    ["Image"] = 444489060,
+                    ["Title"] = "Grassy Pastel green",
+                    ["WeightMin"] = 1,
+                    ["Rarity"] = 2,
+                    ["WeightMax"] = 2
+                }
+            }
+        },
+        {
+            ["Title"] = "Foil",
+            ["SPECIES"] = {
+                {["Image"] = 444490309, ["Title"] = "Foil Alder", ["WeightMin"] = 2, ["Rarity"] = 1, ["WeightMax"] = 6},
+                {
+                    ["Image"] = 444490537,
+                    ["Title"] = "Foil Mulberry",
+                    ["WeightMin"] = 2,
+                    ["Rarity"] = 3,
+                    ["WeightMax"] = 6
+                }
+            }
+        },
+        {
+            ["Title"] = "Granite",
+            ["SPECIES"] = {
+                {
+                    ["Image"] = 444492145,
+                    ["Title"] = "Granite Mulberry",
+                    ["WeightMin"] = 4,
+                    ["Rarity"] = 5,
+                    ["WeightMax"] = 12
+                },
+                {
+                    ["Image"] = 444492404,
+                    ["Title"] = "Granite Cool yellow",
+                    ["WeightMin"] = 4,
+                    ["Rarity"] = 7,
+                    ["WeightMax"] = 12
+                },
+                {
+                    ["Image"] = 444492689,
+                    ["Title"] = "Granite Cyan",
+                    ["WeightMin"] = 4,
+                    ["Rarity"] = 8,
+                    ["WeightMax"] = 12
+                }
+            }
+        },
+        {
+            ["Title"] = "Fabric",
+            ["SPECIES"] = {
+                {
+                    ["Image"] = 444497537,
+                    ["Title"] = "Fabric Cocoa",
+                    ["WeightMin"] = 2,
+                    ["Rarity"] = 3,
+                    ["WeightMax"] = 4
+                },
+                {
+                    ["Image"] = 444497354,
+                    ["Title"] = "Fabric Burgundy",
+                    ["WeightMin"] = 5,
+                    ["Rarity"] = 7,
+                    ["WeightMax"] = 8
+                },
+                {
+                    ["Image"] = 444496800,
+                    ["Title"] = "Fabric Brown",
+                    ["WeightMin"] = 4,
+                    ["Rarity"] = 8,
+                    ["WeightMax"] = 6
+                }
+            }
+        },
+        {
+            ["Title"] = "Ice",
+            ["SPECIES"] = {
+                {
+                    ["Image"] = 444481578,
+                    ["Title"] = "Icy Baby blue",
+                    ["WeightMin"] = 4,
+                    ["Rarity"] = 1,
+                    ["WeightMax"] = 6
+                },
+                {
+                    ["Image"] = 444499638,
+                    ["Title"] = "Icy Really red",
+                    ["WeightMin"] = 14,
+                    ["Rarity"] = 10,
+                    ["WeightMax"] = 18
+                },
+                {
+                    ["Image"] = 444499794,
+                    ["Title"] = "Icy Pastel green",
+                    ["WeightMin"] = 4,
+                    ["Rarity"] = 8,
+                    ["WeightMax"] = 10
+                },
+                {["Image"] = 444499942, ["Title"] = "Icy Lapis", ["WeightMin"] = 1, ["Rarity"] = 3, ["WeightMax"] = 1},
+                {
+                    ["Image"] = 444500156,
+                    ["Title"] = "Icy Cool yellow",
+                    ["WeightMin"] = 2,
+                    ["Rarity"] = 6,
+                    ["WeightMax"] = 6
+                },
+                {
+                    ["Image"] = 444500593,
+                    ["Title"] = "Icy Toothpaste",
+                    ["WeightMin"] = 10,
+                    ["Rarity"] = 4,
+                    ["WeightMax"] = 14
+                }
+            }
+        }
+    },
+    ["RODS"] = {
+        {["WeightMin"] = 0, ["FishDictionary"] = {}, ["CostPerCast"] = 1, ["WeightMax"] = 4},
+        {["WeightMin"] = 0, ["FishDictionary"] = {}, ["CostPerCast"] = 2, ["WeightMax"] = 8},
+        {["WeightMin"] = 0, ["FishDictionary"] = {}, ["CostPerCast"] = 3, ["WeightMax"] = 12},
+        {["WeightMin"] = 0, ["FishDictionary"] = {}, ["CostPerCast"] = 4, ["WeightMax"] = 16},
+        {["WeightMin"] = 0, ["FishDictionary"] = {}, ["CostPerCast"] = 5, ["WeightMax"] = 20}
+    },
+    ["RARITY_VALUE_SCALE"] = 0.2,
+    ["RodRarityFactor"] = {
+        0.23255813953488372,
+        0.2385211687537269,
+        0.24479804161566707,
+        0.25839793281653745,
+        0.27359781121751026
+    },
+    ["WEIGHT_VALUE_SCALE"] = 0.003125,
+    ["OVERALL_VALUE_SCALE"] = 15
+}
