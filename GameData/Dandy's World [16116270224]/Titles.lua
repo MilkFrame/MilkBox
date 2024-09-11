956563229f3bd2d0e582dd0e5cbb0a6a301c22c8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-12 01:35:38 +1000

    Create Titles.lua

---
+++ GameData/Dandy's World [16116270224]/Titles.lua
@@ -0,0 +1,65 @@
+data = {
+    ["CursedEmbers"] = {["Color"] = Color3.fromRGB(226, 132, 77), ["TitleName"] = "[Cursed Moth]"},
+    ["Beta"] = {
+        ["Color"] = Color3.fromRGB(0, 0, 0),
+        ["Border"] = "rbxassetid://12123424853",
+        ["TitleName"] = "[Beta Bear]"
+    },
+    ["QuestHundred"] = {
+        ["Color"] = Color3.fromRGB(167, 105, 232),
+        ["Border"] = "rbxassetid://15282702612",
+        ["TitleName"] = "[Overseer Of Adventure]"
+    },
+    ["QuestOne"] = {["Color"] = Color3.fromRGB(147, 124, 170), ["TitleName"] = "[Rookie Adventurer]"},
+    ["TenVictories"] = {["Color"] = Color3.fromRGB(136, 136, 136), ["TitleName"] = "[Victorious Rookie]"},
+    ["Sauna"] = {["Color"] = Color3.fromRGB(191, 113, 144), ["TitleName"] = "[Sauna Capybara]"},
+    ["KILLSTRACKER"] = {["Color"] = Color3.fromRGB(255, 255, 255), ["TitleName"] = "[Enemies Defeated]"},
+    ["WAVESCONQUERED"] = {
+        ["Color"] = Color3.fromRGB(226, 160, 161),
+        ["Gradient"] = "0 1 1 1 0 1 1 0.223529 0.235294 0 ",
+        ["TitleName"] = "[Waves Conquered]"
+    },
+    ["BakerStreet"] = {["Color"] = Color3.fromRGB(235, 137, 158), ["TitleName"] = "[Baker Deer]"},
+    ["SummertimeBeach"] = {["Color"] = Color3.fromRGB(158, 177, 211), ["TitleName"] = "[Frosty Narwhal]"},
+    ["Friendzy"] = {["Color"] = Color3.fromRGB(188, 216, 226), ["TitleName"] = "[A Friend To All]"},
+    ["AncientDunes"] = {["Color"] = Color3.fromRGB(226, 209, 146), ["TitleName"] = "[Desert Cobra]"},
+    ["EggHuntGarden"] = {["Color"] = Color3.fromRGB(252, 173, 99), ["TitleName"] = "[Bratty Bunny]"},
+    ["KingdomOfWhimsy"] = {["Color"] = Color3.fromRGB(138, 203, 226), ["TitleName"] = "[Royal Unicorn]"},
+    ["HalloweenCarnival"] = {["Color"] = Color3.fromRGB(180, 143, 102), ["TitleName"] = "[Spooky Werewolf]"},
+    ["HundredVictories"] = {["Color"] = Color3.fromRGB(241, 241, 241), ["TitleName"] = "[Victorious Master]"},
+    ["City"] = {["Color"] = Color3.fromRGB(255, 137, 33), ["TitleName"] = "[Mechanical Kaiju]"},
+    ["LighthouseIsle"] = {["Color"] = Color3.fromRGB(226, 200, 69), ["TitleName"] = "[Island Shrimp]"},
+    ["LunarGrotto"] = {["Color"] = Color3.fromRGB(226, 98, 107), ["TitleName"] = "[Pond Axolotl]"},
+    ["PicnicFalls"] = {["Color"] = Color3.fromRGB(226, 210, 199), ["TitleName"] = "[Picnic Fox]"},
+    ["BookwormLibrary"] = {["Color"] = Color3.fromRGB(140, 172, 34), ["TitleName"] = "[Library Bookworm]"},
+    ["QuestTen"] = {["Color"] = Color3.fromRGB(163, 140, 186), ["TitleName"] = "[Skilled Adventurer]"},
+    ["QuestTwentyFive"] = {["Color"] = Color3.fromRGB(190, 167, 213), ["TitleName"] = "[Expert Adventurer]"},
+    ["QuestFifty"] = {["Color"] = Color3.fromRGB(213, 191, 236), ["TitleName"] = "[World-Class Adventurer]"},
+    ["VelvetKitsune"] = {
+        ["Color"] = Color3.fromRGB(214, 94, 94),
+        ["Border"] = "rbxassetid://13278288300",
+        ["TitleName"] = "[Velvet Kitsune]"
+    },
+    ["ONEYEARANNIVERSARY"] = {
+        ["Color"] = Color3.fromRGB(255, 253, 207),
+        ["Gradient"] = "0 1 1 1 0 1 1 0.54902 0.321569 0 ",
+        ["TitleName"] = "🍰[One Year Anniversary]🍰"
+    },
+    ["Museum"] = {["Color"] = Color3.fromRGB(237, 214, 172), ["TitleName"] = "[Competitive Hedgehog]"},
+    ["EGGHUNT"] = {
+        ["Color"] = Color3.fromRGB(255, 253, 207),
+        ["Gradient"] = "0 1 0.803922 0.341176 0 1 1 0.701961 0.341176 0 ",
+        ["TitleName"] = "🏆[Eggcellent Hunter]🏆"
+    },
+    ["FiftyVictories"] = {["Color"] = Color3.fromRGB(201, 201, 201), ["TitleName"] = "[Victorious Champion]"},
+    ["VelvetGriffin"] = {["Color"] = Color3.fromRGB(222, 122, 122), ["TitleName"] = "[Velvet Griffin]"},
+    ["SkateNDine"] = {["Color"] = Color3.fromRGB(226, 160, 214), ["TitleName"] = "[Diner Spider]"},
+    ["Leviathan"] = {["Color"] = Color3.fromRGB(207, 79, 79), ["TitleName"] = "[Velvet Leviathan]"},
+    ["FiveHundredVictories"] = {
+        ["Color"] = Color3.fromRGB(255, 212, 82),
+        ["Border"] = "rbxassetid://12123362434",
+        ["TitleName"] = "[Overseer Of Victory]"
+    },
+    ["Swamp"] = {["Color"] = Color3.fromRGB(216, 212, 105), ["TitleName"] = "[Swamp Platypus]"},
+    ["TwentyFiveVictories"] = {["Color"] = Color3.fromRGB(160, 160, 160), ["TitleName"] = "[Victorious Challenger]"}
+}
