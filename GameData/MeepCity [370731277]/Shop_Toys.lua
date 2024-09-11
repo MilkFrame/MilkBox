0023f59b090e74d96cb19cb671bbed16c65d170e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-12 01:46:31 +1000

    Create Shop_Toys.lua

---
+++ GameData/MeepCity [370731277]/Shop_Toys.lua
@@ -0,0 +1,314 @@
+data = {
+    ["Categories"] = {
+        {["CategoryId"] = 1, ["Image"] = 638302816, ["CatTitle"] = "TEXT_TOYS"},
+        {["CategoryId"] = 2, ["Image"] = 1403169678, ["CatTitle"] = "TEXT_ITEMS"},
+        {["CategoryId"] = 3, ["Image"] = 1611956086, ["CatTitle"] = "SHOP_CATEGORY_WINGS"}
+    },
+    ["ShopDetails"] = {
+        ["ShopId"] = 4,
+        ["BackgroundColor"] = Color3.fromRGB(255, 212, 89),
+        ["Title"] = "TEXT_TOYS",
+        ["ClickColor"] = Color3.fromRGB(121, 19, 140),
+        ["InternalTitle"] = "ToyShop",
+        ["ColorMain"] = Color3.fromRGB(168, 0, 168),
+        ["HoverColor"] = Color3.fromRGB(219, 29, 255),
+        ["TextColor"] = Color3.fromRGB(244, 137, 51)
+    },
+    ["Assets"] = {
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 330,
+            ["Details"] = {["Price"] = {["Coins"] = 1000}},
+            ["CatId"] = 1,
+            ["ObjectId"] = 1
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 399,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 2000}},
+            ["CatId"] = 1,
+            ["ObjectId"] = 2
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 400,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 300}},
+            ["CatId"] = 1,
+            ["ObjectId"] = 3
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 401,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 3500}},
+            ["CatId"] = 1,
+            ["ObjectId"] = 4
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 416,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 5000}},
+            ["CatId"] = 1,
+            ["ObjectId"] = 5
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 437,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 10000}},
+            ["CatId"] = 1,
+            ["ObjectId"] = 6
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 564,
+            ["Tilt3D"] = 25,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 100}},
+            ["CatId"] = 2,
+            ["ObjectId"] = 7,
+            ["PaddingMultiplier"] = 5
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 571,
+            ["Tilt3D"] = 25,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 200}},
+            ["CatId"] = 2,
+            ["ObjectId"] = 8,
+            ["PaddingMultiplier"] = 1.25
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 576,
+            ["Tilt3D"] = 25,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 200}},
+            ["CatId"] = 2,
+            ["ObjectId"] = 9,
+            ["PaddingMultiplier"] = 1.25
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 606,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 1250}},
+            ["CatId"] = 3,
+            ["ObjectId"] = 10
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 614,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 2500}},
+            ["CatId"] = 3,
+            ["ObjectId"] = 11
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 619,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 2000}},
+            ["CatId"] = 3,
+            ["ObjectId"] = 12
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 621,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 1000}},
+            ["CatId"] = 3,
+            ["ObjectId"] = 13
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 628,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 2000}},
+            ["CatId"] = 3,
+            ["ObjectId"] = 14
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 631,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 750}},
+            ["CatId"] = 1,
+            ["ObjectId"] = 15
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 638,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 500}},
+            ["CatId"] = 3,
+            ["ObjectId"] = 16
+        },
+        {
+            ["ForSale"] = false,
+            ["AssetId"] = 645,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 2000}},
+            ["CatId"] = 3,
+            ["ObjectId"] = 17
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 652,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 200}},
+            ["CatId"] = 1,
+            ["ObjectId"] = 18
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 661,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 3000}},
+            ["CatId"] = 3,
+            ["ObjectId"] = 19
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 669,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 5000}},
+            ["CatId"] = 3,
+            ["ObjectId"] = 20
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 699,
+            ["Tilt3D"] = 25,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 350}},
+            ["CatId"] = 2,
+            ["ObjectId"] = 21,
+            ["PaddingMultiplier"] = 3
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 712,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 450}},
+            ["CatId"] = 2,
+            ["ObjectId"] = 22,
+            ["Tilt3D"] = 25
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 714,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 900}},
+            ["CatId"] = 2,
+            ["ObjectId"] = 23,
+            ["Tilt3D"] = 25
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 769,
+            ["Tilt3D"] = 25,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 150}},
+            ["CatId"] = 2,
+            ["ObjectId"] = 24,
+            ["PaddingMultiplier"] = 1.25
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 830,
+            ["Tilt3D"] = 25,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 2500}},
+            ["CatId"] = 2,
+            ["ObjectId"] = 25,
+            ["PaddingMultiplier"] = 1.25
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 846,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 100}},
+            ["CatId"] = 1,
+            ["ObjectId"] = 26
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 850,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 1000}},
+            ["CatId"] = 1,
+            ["ObjectId"] = 27,
+            ["PaddingMultiplier"] = 2
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 875,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 3000}},
+            ["CatId"] = 3,
+            ["ObjectId"] = 28
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 1013,
+            ["Tilt3D"] = 25,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 250}},
+            ["CatId"] = 2,
+            ["ObjectId"] = 29,
+            ["PaddingMultiplier"] = 1.25
+        },
+        {
+            ["ForSale"] = false,
+            ["AssetId"] = 1025,
+            ["Tilt3D"] = 25,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 450}},
+            ["CatId"] = 2,
+            ["ObjectId"] = 30,
+            ["PaddingMultiplier"] = 3
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 1102,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 1750}},
+            ["CatId"] = 3,
+            ["ObjectId"] = 31
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 1104,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 750}},
+            ["CatId"] = 3,
+            ["ObjectId"] = 32
+        },
+        {
+            ["ForSale"] = true,
+            ["AssetId"] = 1184,
+            ["Tilt3D"] = 25,
+            ["MaxAllowed"] = 1,
+            ["Details"] = {["Price"] = {["Coins"] = 450}},
+            ["CatId"] = 2,
+            ["ObjectId"] = 33,
+            ["PaddingMultiplier"] = 3
+        },
+        {
+            ["ForSale"] = false,
+            ["Details"] = {["Price"] = {["HalloweenCandy"] = 750, ["Coins"] = 1500}},
+            ["CatId"] = 1,
+            ["ObjectId"] = 34,
+            ["AssetId"] = 1244,
+            ["MaxAllowed"] = 1,
+            ["SpecialTag"] = 4,
+            ["HalloweenExclusive"] = true,
+            ["AllowCoinPurchase"] = true
+        }
+    }
+}
