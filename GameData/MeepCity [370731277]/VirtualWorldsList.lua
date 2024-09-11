eb6a238f069ab43b5327c10773065adcbfdaaabd
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-12 01:48:06 +1000

    Create VirtualWorldsList.lua

---
+++ GameData/MeepCity [370731277]/VirtualWorldsList.lua
@@ -0,0 +1,83 @@
+data = {
+    {
+        ["FishingZones"] = true,
+        ["NonReplicatedWorld"] = true,
+        ["StaticWorld"] = 1,
+        ["BackgroundMusicNight"] = 502898312,
+        ["DayNightEnabled"] = true,
+        ["BackgroundMusicVolume"] = 0.5,
+        ["ID"] = 1,
+        ["BackgroundMusic"] = 5239126251
+    },
+    {
+        ["BackgroundMusicVolume"] = 0.5,
+        ["NonReplicatedWorld"] = true,
+        ["ID"] = 2,
+        ["StaticWorld"] = 3,
+        ["UniverseOnly"] = 1,
+        ["BackgroundMusic"] = 444533100
+    },
+    {
+        ["BackgroundMusicVolume"] = 0.5,
+        ["NonReplicatedWorld"] = true,
+        ["ID"] = 4,
+        ["StaticWorld"] = 5,
+        ["UniverseOnly"] = 1,
+        ["BackgroundMusic"] = 436028561
+    },
+    {
+        ["FishingZones"] = true,
+        ["NonReplicatedWorld"] = true,
+        ["StaticWorld"] = 6,
+        ["BackgroundMusicNight"] = 498842045,
+        ["HasEstates"] = true,
+        ["DayNightEnabled"] = true,
+        ["BackgroundMusicVolume"] = 0.5,
+        ["ID"] = 5,
+        ["BackgroundMusic"] = 454963556
+    },
+    {
+        ["BackgroundMusicVolume"] = 0.5,
+        ["NonReplicatedWorld"] = true,
+        ["StaticWorld"] = 41,
+        ["ID"] = 7,
+        ["VWSpeed"] = 17,
+        ["UniverseOnly"] = 3,
+        ["BackgroundMusic"] = 5234651471
+    },
+    {
+        ["BackgroundMusicVolume"] = 0.5,
+        ["NonReplicatedWorld"] = true,
+        ["ID"] = 9,
+        ["StaticWorld"] = 11,
+        ["UniverseOnly"] = 10,
+        ["BackgroundMusic"] = 422773872
+    },
+    {
+        ["BackgroundMusicVolume"] = 0.25,
+        ["NonReplicatedWorld"] = true,
+        ["ID"] = 11,
+        ["StaticWorld"] = 14,
+        ["UniverseOnly"] = 11,
+        ["BackgroundMusic"] = 420756713
+    },
+    {
+        ["BackgroundMusicVolume"] = 0.5,
+        ["NonReplicatedWorld"] = true,
+        ["ID"] = 12,
+        ["StaticWorld"] = 15,
+        ["UniverseOnly"] = 1,
+        ["BackgroundMusic"] = 436028561
+    },
+    {
+        ["NonReplicatedWorld"] = true,
+        ["StaticWorld"] = 17,
+        ["ID"] = 13,
+        ["VWSpeed"] = 17,
+        ["UniverseOnly"] = 4,
+        ["Lighting"] = {["EnvironmentSpecularScale"] = 0.7}
+    },
+    {["NonReplicatedWorld"] = true, ["ID"] = 14, ["VWSpeed"] = 17, ["UniverseOnly"] = 8, ["StaticWorld"] = 19},
+    {["NonReplicatedWorld"] = true, ["ID"] = 18, ["UniverseOnly"] = 7, ["StaticWorld"] = 34},
+    {["NonReplicatedWorld"] = true, ["ID"] = 19, ["VWSpeed"] = 17, ["UniverseOnly"] = 12, ["StaticWorld"] = 46}
+}
