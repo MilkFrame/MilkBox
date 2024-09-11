f3fb7880c1bdf57dfb8c2d869aaf5a12bf4e2a83
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-12 01:48:42 +1000

    Create FireworksConstants.lua

---
+++ GameData/MeepCity [370731277]/FireworksConstants.lua
@@ -0,0 +1,465 @@
+data = {
+    ["FireworkType"] = {
+        ["GlowFlare"] = 1,
+        ["Saturn"] = 5,
+        ["Bees"] = 6,
+        ["DiademPeony"] = 3,
+        ["DiademChrysanthemum"] = 2
+    },
+    ["FireworkColors"] = {
+        Color3.fromRGB(255, 255, 255),
+        Color3.fromRGB(255, 25, 25),
+        Color3.fromRGB(25, 255, 25),
+        Color3.fromRGB(76, 255, 76),
+        Color3.fromRGB(51, 51, 255),
+        Color3.fromRGB(255, 255, 25),
+        Color3.fromRGB(255, 127, 25),
+        Color3.fromRGB(255, 25, 255),
+        Color3.fromRGB(25, 255, 255),
+        Color3.fromRGB(25, 127, 255)
+    },
+    ["FireworkShows"] = {
+        ["NEWYEARS_FIREWORKS"] = {
+            {
+                1,
+                Vector3.new(0, 0, 180),
+                Vector3.new(-120, 0, 0),
+                1.2454243750309775,
+                Color3.fromRGB(51, 51, 255),
+                Color3.fromRGB(255, 255, 255),
+                1.5,
+                1
+            },
+            {
+                1,
+                Vector3.new(0, 0, 120),
+                Vector3.new(-60, 0, 0),
+                1.3438854567660417,
+                Color3.fromRGB(51, 51, 255),
+                Color3.fromRGB(255, 255, 255),
+                1.5,
+                1
+            },
+            {
+                1,
+                Vector3.new(0, 0, 80),
+                Vector3.new(-10, 0, 0),
+                1.353745848659183,
+                Color3.fromRGB(25, 255, 25),
+                Color3.fromRGB(255, 255, 255),
+                1.5,
+                1
+            },
+            {
+                1,
+                Vector3.new(0, 0, 80),
+                Vector3.new(10, 0, 0),
+                1.368537114305728,
+                Color3.fromRGB(25, 255, 255),
+                Color3.fromRGB(255, 255, 255),
+                1.5,
+                1
+            },
+            {
+                1,
+                Vector3.new(0, 0, 120),
+                Vector3.new(60, 0, 0),
+                1.001343321714767,
+                Color3.fromRGB(255, 25, 25),
+                Color3.fromRGB(255, 255, 255),
+                1.5,
+                1
+            },
+            {
+                1,
+                Vector3.new(0, 0, 180),
+                Vector3.new(120, 0, 0),
+                1.2399433043249617,
+                Color3.fromRGB(255, 25, 255),
+                Color3.fromRGB(255, 255, 255),
+                1.5,
+                2
+            },
+            {
+                1,
+                Vector3.new(0, 0, 80),
+                Vector3.new(120, 0, 0),
+                1.0381608334470886,
+                Color3.fromRGB(51, 51, 255),
+                Color3.fromRGB(255, 255, 255),
+                1.5,
+                1
+            },
+            {
+                1,
+                Vector3.new(0, 0, 120),
+                Vector3.new(60, 0, 0),
+                1.3673573729336648,
+                Color3.fromRGB(255, 255, 255),
+                Color3.fromRGB(255, 255, 255),
+                1.5,
+                1
+            },
+            {
+                1,
+                Vector3.new(0, 0, 180),
+                Vector3.new(10, 0, 0),
+                1.2422828040610523,
+                Color3.fromRGB(25, 255, 255),
+                Color3.fromRGB(255, 255, 255),
+                1.5,
+                1
+            },
+            {
+                1,
+                Vector3.new(0, 0, 180),
+                Vector3.new(-10, 0, 0),
+                1.1461021904960125,
+                Color3.fromRGB(51, 51, 255),
+                Color3.fromRGB(255, 255, 255),
+                1.5,
+                1
+            },
+            {
+                1,
+                Vector3.new(0, 0, 120),
+                Vector3.new(-60, 0, 0),
+                1.436245680816763,
+                Color3.fromRGB(25, 255, 25),
+                Color3.fromRGB(255, 255, 255),
+                1.5,
+                1
+            },
+            {
+                1,
+                Vector3.new(0, 0, 80),
+                Vector3.new(-120, 0, 0),
+                1.2636132996812863,
+                Color3.fromRGB(25, 255, 255),
+                Color3.fromRGB(255, 255, 255),
+                1.5,
+                2
+            },
+            {
+                1,
+                Vector3.new(-180, 0, 180),
+                Vector3.new(-60, 0, 0),
+                1.2589079319163723,
+                Color3.fromRGB(51, 51, 255),
+                Color3.fromRGB(255, 255, 255),
+                2.5,
+                0.15
+            },
+            {
+                1,
+                Vector3.new(180, 0, 180),
+                Vector3.new(60, 0, 0),
+                1.3012825357791433,
+                Color3.fromRGB(51, 51, 255),
+                Color3.fromRGB(255, 255, 255),
+                2.5,
+                0.15
+            },
+            {
+                2,
+                Vector3.new(40, 50, 140),
+                Vector3.new(0, 0, 0),
+                1.3404377859285959,
+                Color3.fromRGB(76, 255, 76),
+                Color3.fromRGB(255, 255, 25),
+                1.5,
+                1.5
+            },
+            {
+                2,
+                Vector3.new(-40, -50, 140),
+                Vector3.new(0, 0, 0),
+                1.0462806356738545,
+                Color3.fromRGB(255, 255, 25),
+                Color3.fromRGB(25, 255, 25),
+                1.6,
+                3
+            },
+            {
+                2,
+                Vector3.new(-140, 50, 120),
+                Vector3.new(0, 0, 0),
+                1.429749870419115,
+                Color3.fromRGB(25, 255, 255),
+                Color3.fromRGB(255, 25, 25),
+                1.2,
+                0.25
+            },
+            {
+                2,
+                Vector3.new(70, -40, 90),
+                Vector3.new(0, 0, 0),
+                1.2329032486262497,
+                Color3.fromRGB(255, 127, 25),
+                Color3.fromRGB(25, 255, 255),
+                1.5,
+                0.25
+            },
+            {
+                2,
+                Vector3.new(-100, 30, 60),
+                Vector3.new(0, 0, 0),
+                1.4336283320830026,
+                Color3.fromRGB(25, 255, 255),
+                Color3.fromRGB(76, 255, 76),
+                1.2,
+                0.25
+            },
+            {
+                2,
+                Vector3.new(0, 20, 100),
+                Vector3.new(0, 0, 0),
+                1.4196314536228583,
+                Color3.fromRGB(255, 127, 25),
+                Color3.fromRGB(255, 25, 255),
+                1.5,
+                0.25
+            },
+            {
+                2,
+                Vector3.new(-70, 0, 130),
+                Vector3.new(0, 0, 0),
+                1.4444894394167174,
+                Color3.fromRGB(25, 255, 25),
+                Color3.fromRGB(25, 255, 25),
+                1.4,
+                0.5
+            },
+            {
+                2,
+                Vector3.new(120, 50, 100),
+                Vector3.new(0, 0, 0),
+                1.3875782822927518,
+                Color3.fromRGB(255, 25, 255),
+                Color3.fromRGB(25, 255, 255),
+                1.5,
+                3.5
+            },
+            {
+                3,
+                Vector3.new(90, 0, 120),
+                Vector3.new(0, 0, 0),
+                1.3755502856244006,
+                Color3.fromRGB(51, 51, 255),
+                Color3.fromRGB(255, 127, 25),
+                1.5,
+                0.15
+            },
+            {
+                3,
+                Vector3.new(-30, 0, 120),
+                Vector3.new(0, 0, 0),
+                1.1797772275136578,
+                Color3.fromRGB(255, 127, 25),
+                Color3.fromRGB(25, 255, 255),
+                1.6,
+                0.15
+            },
+            {
+                3,
+                Vector3.new(30, 0, 100),
+                Vector3.new(0, 0, 0),
+                1.130886545454249,
+                Color3.fromRGB(25, 255, 255),
+                Color3.fromRGB(25, 255, 255),
+                2,
+                0.15
+            },
+            {
+                3,
+                Vector3.new(-90, 0, 100),
+                Vector3.new(0, 0, 0),
+                1.105028479794965,
+                Color3.fromRGB(255, 127, 25),
+                Color3.fromRGB(255, 255, 25),
+                1.7,
+                3
+            },
+            {
+                2,
+                Vector3.new(9, 21, 141),
+                Vector3.new(0, 0, 0),
+                1.1710000400618004,
+                Color3.fromRGB(51, 51, 255),
+                Color3.fromRGB(255, 255, 25),
+                1.6,
+                0.15
+            },
+            {
+                2,
+                Vector3.new(-50, 11, 147),
+                Vector3.new(0, 0, 0),
+                1.4279561660042472,
+                Color3.fromRGB(255, 25, 255),
+                Color3.fromRGB(255, 25, 255),
+                1.5,
+                0.5
+            },
+            {
+                2,
+                Vector3.new(37, 24, 134),
+                Vector3.new(0, 0, 0),
+                1.1102296762901998,
+                Color3.fromRGB(255, 255, 25),
+                Color3.fromRGB(51, 51, 255),
+                1.9,
+                1.5
+            },
+            {
+                2,
+                Vector3.new(50, 14, 125),
+                Vector3.new(0, 0, 0),
+                1.217418566652853,
+                Color3.fromRGB(255, 25, 25),
+                Color3.fromRGB(255, 25, 25),
+                1.7,
+                3
+            },
+            {
+                5,
+                Vector3.new(0, 0, 100),
+                Vector3.new(-120, 0, 0),
+                1.1033498170383949,
+                Color3.fromRGB(51, 51, 255),
+                Color3.fromRGB(76, 255, 76),
+                1.4,
+                0.5
+            },
+            {
+                5,
+                Vector3.new(20, 0, 70),
+                Vector3.new(-120, 0, 0),
+                1.114099787379101,
+                Color3.fromRGB(25, 255, 255),
+                Color3.fromRGB(25, 255, 255),
+                1.5,
+                0.5
+            },
+            {
+                3,
+                Vector3.new(-30, 0, 120),
+                Vector3.new(120, 0, 0),
+                1.3518908412290624,
+                Color3.fromRGB(76, 255, 76),
+                Color3.fromRGB(255, 25, 25),
+                1.4,
+                0.5
+            },
+            {
+                3,
+                Vector3.new(0, 0, 90),
+                Vector3.new(120, 0, 0),
+                1.165172901826721,
+                Color3.fromRGB(25, 255, 25),
+                Color3.fromRGB(25, 255, 25),
+                1.8,
+                4
+            },
+            {
+                3,
+                Vector3.new(-140, 50, 120),
+                Vector3.new(0, 0, 0),
+                1.097283324068228,
+                Color3.fromRGB(255, 255, 255),
+                Color3.fromRGB(255, 25, 255),
+                1.7,
+                0.25
+            },
+            {
+                2,
+                Vector3.new(70, -40, 90),
+                Vector3.new(0, 0, 0),
+                1.3573057579121799,
+                Color3.fromRGB(255, 25, 25),
+                Color3.fromRGB(76, 255, 76),
+                1.5,
+                0.25
+            },
+            {
+                3,
+                Vector3.new(-100, 30, 60),
+                Vector3.new(0, 0, 0),
+                1.4152288332611043,
+                Color3.fromRGB(51, 51, 255),
+                Color3.fromRGB(25, 255, 25),
+                2.25,
+                0.25
+            },
+            {
+                2,
+                Vector3.new(0, 20, 100),
+                Vector3.new(0, 0, 0),
+                1.1969796061788383,
+                Color3.fromRGB(255, 255, 255),
+                Color3.fromRGB(255, 255, 255),
+                1.5,
+                2
+            },
+            {
+                3,
+                Vector3.new(-70, 0, 130),
+                Vector3.new(0, 0, 0),
+                1.105410353202754,
+                Color3.fromRGB(25, 255, 255),
+                Color3.fromRGB(255, 25, 255),
+                1.8,
+                1.5
+            },
+            {
+                2,
+                Vector3.new(120, 50, 100),
+                Vector3.new(0, 0, 0),
+                1.0212622785759626,
+                Color3.fromRGB(25, 255, 25),
+                Color3.fromRGB(255, 25, 255),
+                1.5,
+                2
+            },
+            {
+                3,
+                Vector3.new(90, 0, 120),
+                Vector3.new(0, 0, 0),
+                1.3978399700742172,
+                Color3.fromRGB(255, 25, 255),
+                Color3.fromRGB(255, 127, 25),
+                1.4,
+                0.15
+            },
+            {
+                3,
+                Vector3.new(-30, 0, 120),
+                Vector3.new(0, 0, 0),
+                1.4497494131803867,
+                Color3.fromRGB(25, 255, 255),
+                Color3.fromRGB(255, 255, 25),
+                1.5,
+                0.15
+            },
+            {
+                3,
+                Vector3.new(30, 0, 100),
+                Vector3.new(0, 0, 0),
+                1.0249694378742635,
+                Color3.fromRGB(25, 255, 255),
+                Color3.fromRGB(76, 255, 76),
+                1.5,
+                0.15
+            },
+            {
+                3,
+                Vector3.new(-90, 0, 100),
+                Vector3.new(0, 0, 0),
+                1.339087820913335,
+                Color3.fromRGB(255, 25, 25),
+                Color3.fromRGB(51, 51, 255),
+                1.9,
+                5
+            }
+        }
+    }
+}
