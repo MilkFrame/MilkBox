0f72d00d1ebe20ade0d562d10b17ce6cbd5a017d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-30 17:57:44 +1000

    Create ugc-meta.lua

---
+++ GameData/BedWars [6872265039]/ugc-meta.lua
@@ -0,0 +1,18 @@
+data = {
+    ["UgcMeta"] = {
+        ["grim_reaper_shoulder_buddy"] = {
+            ["assetId"] = 17735885308,
+            ["name"] = "Grim Reaper Shoulder Buddy",
+            ["category"] = "shoulder_accessory",
+            ["image"] = {["render"] = "rbxassetid://17737351773", ["stroke"] = "rbxassetid://17737753754"},
+            ["basePrice"] = 50
+        },
+        ["aery_shoulder_buddy"] = {
+            ["assetId"] = 139207473906297,
+            ["name"] = "Aery Shoulder Buddy",
+            ["category"] = "shoulder_accessory",
+            ["image"] = {["render"] = "rbxassetid://106491911463393", ["stroke"] = "rbxassetid://124811127697552"},
+            ["basePrice"] = 50
+        }
+    }
+}
