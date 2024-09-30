2d960bfc64ceac89f45dd17d3f480c6ffcb09399
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-30 18:10:22 +1000

    Update LTMCrateData.lua

--- GameData/Blade Ball [13772394625]/LTMCrateData.lua
+++ GameData/Blade Ball [13772394625]/LTMCrateData.lua
@@ -8,106 +8,85 @@ data = {
     ["RewardPool"] = {
         {
             ["Reward"] = {
-                ["Value"] = "Traitor's Blade",
-                ["Type"] = "Sword",
-                ["Icon"] = "rbxassetid://17673694225",
-                ["DisplayName"] = "Traitor's Blade"
-            },
-            ["ReplionPath"] = {"SwordSkins", "Unlocked", "Traitor's Blade"},
-            ["Probability"] = 30
-        },
-        {
-            ["Reward"] = {
-                ["Value"] = "Scale Blade",
-                ["Type"] = "Sword",
-                ["Icon"] = "rbxassetid://17673694502",
-                ["DisplayName"] = "Scale Blade"
-            },
-            ["ReplionPath"] = {"SwordSkins", "Unlocked", "Scale Blade"},
-            ["Probability"] = 15
-        },
-        {
-            ["Reward"] = {
-                ["Value"] = "Abyssal Fang",
-                ["Type"] = "Sword",
-                ["Icon"] = "rbxassetid://17673696562",
-                ["DisplayName"] = "Abyssal Fang"
-            },
-            ["ReplionPath"] = {"SwordSkins", "Unlocked", "Abyssal Fang"},
-            ["Probability"] = 5
-        },
-        {
-            ["Reward"] = {
-                ["Value"] = "Ember's Rage",
-                ["Type"] = "Sword",
-                ["Icon"] = "rbxassetid://17673695517",
-                ["DisplayName"] = "Ember's Rage"
-            },
-            ["ReplionPath"] = {"SwordSkins", "Unlocked", "Ember's Rage"},
-            ["Probability"] = 1
-        },
-        {
-            ["Reward"] = {
-                ["Value"] = "Molten Eruption",
+                ["Value"] = "Referee Whistle",
                 ["Type"] = "Explosion",
-                ["Icon"] = "rbxassetid://17673329963",
-                ["DisplayName"] = "Molten Eruption Explosion"
+                ["Icon"] = "rbxassetid://75752825089198",
+                ["DisplayName"] = "Referee Whistle Explosion"
             },
-            ["ReplionPath"] = {"ExplosionSkins", "Unlocked", "Molten Eruption"},
-            ["Probability"] = 25
+            ["ItemType"] = "Explosion",
+            ["Probability"] = 33
         },
         {
             ["Reward"] = {
-                ["Value"] = "Infernal Blast",
-                ["Type"] = "Explosion",
-                ["Icon"] = "rbxassetid://17693758193",
-                ["DisplayName"] = "Infernal Blast Explosion"
-            },
-            ["ReplionPath"] = {"ExplosionSkins", "Unlocked", "Infernal Blast"},
-            ["Probability"] = 15
-        },
-        {
-            ["ReplionPathFindValue"] = "Emote359",
-            ["Reward"] = {
-                ["Value"] = "Emote359",
+                ["Value"] = "Emote522",
                 ["Type"] = "Emote",
-                ["Icon"] = "rbxassetid://17673543973",
-                ["DisplayName"] = "Uncovered Emote"
+                ["Icon"] = "rbxassetid://133038295781914",
+                ["DisplayName"] = "Game Face Emote"
             },
-            ["ReplionPath"] = {"Emotes", "Unlocked"},
+            ["ItemType"] = "Emote",
+            ["Probability"] = 23
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Champion's Edge",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://75117019957384",
+                ["DisplayName"] = "Champion's Edge"
+            },
+            ["ItemType"] = "Sword",
+            ["Probability"] = 18
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Championship Confetti",
+                ["Type"] = "Explosion",
+                ["Icon"] = "rbxassetid://118269486379871",
+                ["DisplayName"] = "Championship Confetti Explosion"
+            },
+            ["ItemType"] = "Explosion",
+            ["Probability"] = 12
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Court Dominator",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://81097916254167",
+                ["DisplayName"] = "Court Dominator"
+            },
+            ["ItemType"] = "Sword",
+            ["Probability"] = 7
+        },
+        {
+            ["Reward"] = {
+                ["Value"] = "Emote523",
+                ["Type"] = "Emote",
+                ["Icon"] = "rbxassetid://113711153739750",
+                ["DisplayName"] = "Victory Lap Emote"
+            },
+            ["ItemType"] = "Emote",
             ["Probability"] = 6
         },
         {
-            ["ReplionPathFindValue"] = "Emote360",
             ["Reward"] = {
-                ["Value"] = "Emote360",
-                ["Type"] = "Emote",
-                ["Icon"] = "rbxassetid://17673584301",
-                ["DisplayName"] = "Dragon Beast Emote"
+                ["Value"] = "Speed Striker",
+                ["Type"] = "Sword",
+                ["Icon"] = "rbxassetid://112902439725535",
+                ["DisplayName"] = "Speed Striker"
             },
-            ["ReplionPath"] = {"Emotes", "Unlocked"},
-            ["Probability"] = 2.9
+            ["ItemType"] = "Sword",
+            ["Probability"] = 0.98
         },
         {
-            ["Replacement"] = {
-                ["ReplionPath"] = {"ExplosionSkins", "Unlocked", "Ferocitus’ Awakening"},
-                ["Reward"] = {
-                    ["Value"] = "Ferocitus’ Awakening",
-                    ["Type"] = "Explosion",
-                    ["Icon"] = "rbxassetid://17673252317",
-                    ["DisplayName"] = "Ferocitus’ Awakening Explosion"
-                }
-            },
             ["Reward"] = {
-                ["Value"] = "Bane of Ferocity",
+                ["Value"] = "All-Star Striker",
                 ["Type"] = "Sword",
-                ["Icon"] = "rbxassetid://17673696086",
-                ["DisplayName"] = "Bane of Ferocity"
+                ["Icon"] = "rbxassetid://89419075047144",
+                ["DisplayName"] = "All-Star Striker"
             },
-            ["ReplionPath"] = {"SwordSkins", "Unlocked", "Bane of Ferocity"},
-            ["Probability"] = 0.05
+            ["ItemType"] = "Sword",
+            ["Probability"] = 0.02
         }
     },
-    ["SpinTable"] = "function: 0x69d85bfab7d71070",
-    ["TimeLength"] = "1719072000000"
+    ["SpinTable"] = "function: 0x53ff98d2ff2f57cc",
+    ["TimeLength"] = "1726401600000"
 }
