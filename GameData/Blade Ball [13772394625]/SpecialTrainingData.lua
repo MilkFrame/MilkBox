2b5530229beff5398191985fc3f4c989a894e53f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-09-30 18:10:44 +1000

    Update SpecialTrainingData.lua

--- GameData/Blade Ball [13772394625]/SpecialTrainingData.lua
+++ GameData/Blade Ball [13772394625]/SpecialTrainingData.lua
@@ -1,62 +1,10 @@
 data = {
-    ["SPECIAL_DAILY_LOGIN_VERSION"] = 9,
-    ["MAXEVENTTIME"] = "1705771137",
+    ["SPECIAL_DAILY_LOGIN_VERSION"] = 15,
+    ["PurchaseDragonElement"] = 1749883315,
+    ["PurchaseSword"] = 1766864625,
+    ["AMOUNTOFQUESTS"] = 5,
+    ["MAXEVENTTIME"] = "1718467200000",
     ["CURRENT_RESETS_AVAILABLE"] = 1,
-    ["QuestToDiff"] = {1, 2, 2, 3},
-    ["Products"] = {
-        {["Id"] = 1676261676, ["Price"] = 99, ["Amount"] = 1},
-        {["Id"] = 1676261918, ["Price"] = 199, ["Amount"] = 1},
-        {["Id"] = 1676261963, ["Price"] = 299, ["Amount"] = 1},
-        {["Id"] = 1676262004, ["Price"] = 399, ["Amount"] = 1},
-        {["Id"] = 1676256463, ["Price"] = 1, ["Amount"] = 1}
-    },
-    ["AMOUNTOFQUESTS"] = 4,
-    ["Gifts"] = {
-        {["Id"] = 1676262354, ["Price"] = 999, ["Amount"] = 2},
-        {["Id"] = 1676262500, ["Price"] = 2999, ["Amount"] = 10},
-        {["Id"] = 1676262558, ["Price"] = 9999, ["Amount"] = 100}
-    },
-    ["CURRENT_TRAINING_QUEST"] = 6,
-    ["SpecialTrainingRewards"] = {
-        {["Required"] = 20, ["RewardType"] = "Coins", ["Icon"] = "rbxassetid://15038384377", ["Amount"] = 150},
-        {
-            ["Required"] = 40,
-            ["RewardType"] = "PremiumExplosionCrate",
-            ["Icon"] = "rbxassetid://15049303003",
-            ["Amount"] = 1
-        },
-        {
-            ["RewardType"] = "Emote",
-            ["Required"] = 60,
-            ["RewardStat"] = "Emote75",
-            ["Icon"] = "rbxassetid://15862986776",
-            ["Replacement"] = {
-                ["Required"] = 60,
-                ["RewardType"] = "Coins",
-                ["Icon"] = "rbxassetid://15038384377",
-                ["Amount"] = 500
-            }
-        },
-        {
-            ["RewardType"] = "Boost",
-            ["Required"] = 80,
-            ["RewardStat"] = "Coins",
-            ["Icon"] = "rbxassetid://14737693993",
-            ["Duration"] = 1800
-        },
-        {
-            ["RewardType"] = "Ability",
-            ["Required"] = 100,
-            ["RewardStat"] = "Continuity Zero",
-            ["Icon"] = "rbxassetid://15797061567",
-            ["Replacement"] = {
-                ["RewardStat"] = "Aether Katana",
-                ["RewardType"] = "Sword",
-                ["Icon"] = "rbxassetid://15861551107",
-                ["Required"] = 100
-            }
-        }
-    },
     ["SpecialTrainingQuests"] = {
         {
             {
@@ -83,7 +31,7 @@ data = {
             {
                 ["Currency"] = "XP",
                 ["Amount"] = 3,
-                ["CompletionArguments"] = {"Play_Time", 1800},
+                ["CompletionArguments"] = {"Play_Time", 600},
                 ["DisplayName"] = "Play for 30 minutes",
                 ["QuestType"] = "Daily"
             },
@@ -110,9 +58,18 @@ data = {
             },
             {
                 ["Currency"] = "XP",
-                ["Amount"] = 3,
-                ["CompletionArguments"] = {"Play_Round", 8, "LTM"},
-                ["DisplayName"] = "Play 8 Infected Limited Mode Matches",
+                ["IsEventQuest"] = true,
+                ["Amount"] = 5,
+                ["CompletionArguments"] = {"Kill_Player", 5, "StadiumICC"},
+                ["DisplayName"] = "Defeat 5 people in ICC Fan Zone Map",
+                ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["IsEventQuest"] = true,
+                ["Amount"] = 5,
+                ["CompletionArguments"] = {"Play_Round", 1, "StadiumICC"},
+                ["DisplayName"] = "Play a match on the ICC Fan Zone Map",
                 ["QuestType"] = "Daily"
             }
         },
@@ -144,20 +101,28 @@ data = {
                 ["CompletionArguments"] = {"Win_Duel", 2},
                 ["DisplayName"] = "Win at least 2 Duels",
                 ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["IsEventQuest"] = true,
+                ["Amount"] = 8,
+                ["CompletionArguments"] = {"Round_Won", 3, "StadiumICC"},
+                ["DisplayName"] = "Win 3 times in ICC Fan Zone Map",
+                ["QuestType"] = "Daily"
             }
         },
         {
             {
                 ["Currency"] = "XP",
                 ["Amount"] = 8,
-                ["CompletionArguments"] = {"Play_Time", 5400},
+                ["CompletionArguments"] = {"Play_Time", 1800},
                 ["DisplayName"] = "Play for 90 minutes",
                 ["QuestType"] = "Daily"
             },
             {
                 ["Currency"] = "XP",
                 ["Amount"] = 8,
-                ["CompletionArguments"] = {"Round_Kill_Count_Specific", 5},
+                ["CompletionArguments"] = {"Round_Kill_Count_Special", 5},
                 ["DisplayName"] = "Eliminate 5 enemies in a single match 5 times",
                 ["QuestType"] = "Daily"
             },
@@ -181,7 +146,79 @@ data = {
                 ["CompletionArguments"] = {"Round_Won", 1, "Dash"},
                 ["DisplayName"] = "Win 1 game using dash ability",
                 ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["IsEventQuest"] = true,
+                ["Amount"] = 10,
+                ["CompletionArguments"] = {"Round_Won", 5, "StadiumICC"},
+                ["DisplayName"] = "Win 5 games in ICC Fan Zone Map",
+                ["QuestType"] = "Daily"
+            },
+            {
+                ["Currency"] = "XP",
+                ["IsEventQuest"] = true,
+                ["Amount"] = 10,
+                ["CompletionArguments"] = {"Win_Dungeon", 1},
+                ["DisplayName"] = "Win a dungeons game",
+                ["QuestType"] = "Daily"
             }
         }
-    }
+    },
+    ["PurchaseMaxDragonElement"] = 1749883413,
+    ["Products"] = {
+        {["Id"] = 1676261676, ["Price"] = 99, ["Amount"] = 1},
+        {["Id"] = 1676261918, ["Price"] = 199, ["Amount"] = 1},
+        {["Id"] = 1676261963, ["Price"] = 299, ["Amount"] = 1},
+        {["Id"] = 1676262004, ["Price"] = 399, ["Amount"] = 1},
+        {["Id"] = 1676256463, ["Price"] = 1, ["Amount"] = 1}
+    },
+    ["SpecialTrainingRewards"] = {
+        {["Required"] = 20, ["RewardType"] = "Coins", ["Icon"] = "rbxassetid://15038384377", ["Amount"] = 250},
+        {
+            ["Required"] = 40,
+            ["RewardType"] = "PremiumExplosionCrate",
+            ["Icon"] = "rbxassetid://15049303003",
+            ["Amount"] = 1
+        },
+        {
+            ["RewardType"] = "Explosion",
+            ["Required"] = 60,
+            ["RewardStat"] = "Cowboy's Capture",
+            ["Icon"] = "rbxassetid://17693585682",
+            ["DisplayName"] = "Cowboy's Capture"
+        },
+        {
+            ["RewardType"] = "Emote",
+            ["Required"] = 80,
+            ["RewardStat"] = "Emote357",
+            ["Icon"] = "rbxassetid://17684913683",
+            ["DisplayName"] = "High Noon"
+        },
+        {
+            ["RewardType"] = "Sword",
+            ["Required"] = 100,
+            ["RewardStat"] = "Horseman's Sword",
+            ["Icon"] = "rbxassetid://17684914004",
+            ["DisplayName"] = "Horseman's Sword"
+        },
+        {
+            ["RewardType"] = "Ability",
+            ["Required"] = 120,
+            ["RewardStat"] = "Bounty",
+            ["Icon"] = "rbxassetid://17676181458",
+            ["DisplayName"] = "Bounty"
+        }
+    },
+    ["QuestToDiff"] = {1, 1, 2, 2, 3},
+    ["GiftPurchaseDragonElement"] = 1749883219,
+    ["CURRENT_TRAINING_QUEST"] = 11,
+    ["Gifts"] = {
+        {["Id"] = 1676262354, ["Price"] = 999, ["Amount"] = 2},
+        {["Id"] = 1676262500, ["Price"] = 2999, ["Amount"] = 10},
+        {["Id"] = 1676262558, ["Price"] = 9999, ["Amount"] = 100}
+    },
+    ["DISABLED"] = true,
+    ["EVENT_QUESTS"] = 1,
+    ["GiftPurchaseSword"] = 1766864749
 }
