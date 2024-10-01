820239b5cdbe955bd88a524a5b237845ca331b21
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-02 02:00:38 +1000

    Update limiteds.lua

--- GameData/Gym League [17450551531]/limiteds.lua
+++ GameData/Gym League [17450551531]/limiteds.lua
@@ -1,9 +1,33 @@
 data = {
+    ["1874372414"] = {
+        ["type"] = "SeakingPack",
+        ["key"] = "SeakingPack_01",
+        ["productId"] = 1874372414,
+        ["factor"] = 2,
+        ["limit"] = 1,
+        ["stock"] = 10000
+    },
+    ["1922975505"] = {
+        ["type"] = "KnightPack",
+        ["key"] = "KnightPack_03",
+        ["productId"] = 1922975505,
+        ["factor"] = 2,
+        ["limit"] = 1,
+        ["stock"] = 5000
+    },
     ["1841356711"] = {
         ["type"] = "GalaxyPack",
         ["key"] = "GalaxyPack_01",
         ["productId"] = 1841356711,
         ["limit"] = 1,
         ["stock"] = 5000
+    },
+    ["1853800345"] = {
+        ["type"] = "AngelPack",
+        ["key"] = "AngelPack_01",
+        ["productId"] = 1853800345,
+        ["factor"] = 2,
+        ["limit"] = 1,
+        ["stock"] = 10000
     }
 }
