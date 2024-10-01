c397e6cd36d490d1fa9fde29bcd0eb6acfc5681d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-02 02:03:40 +1000

    Create battlepassSpinRewards.lua

---
+++ GameData/Gym League [17450551531]/battlepassSpinRewards.lua
@@ -0,0 +1,61 @@
+data = {
+    {
+        ["image"] = "rbxassetid://17701691468",
+        ["value"] = 2500,
+        ["chance"] = 20.4,
+        ["type"] = "cash",
+        ["reward"] = "+2,500$"
+    },
+    {
+        ["image"] = "rbxassetid://17654121877",
+        ["count"] = 5,
+        ["value"] = "Mega Potion",
+        ["chance"] = 5,
+        ["type"] = "potion",
+        ["reward"] = "5x Mega Potion"
+    },
+    {
+        ["image"] = "rbxassetid://17701691468",
+        ["value"] = 2500,
+        ["chance"] = 20.4,
+        ["type"] = "cash",
+        ["reward"] = "+2,500$"
+    },
+    {
+        ["image"] = "rbxassetid://17735246150",
+        ["chance"] = 1,
+        ["value"] = 5,
+        ["color"] = Color3.fromRGB(255, 219, 14),
+        ["type"] = "auraPremiumRoll",
+        ["reward"] = "5x Premium Aura Reroll"
+    },
+    {
+        ["image"] = "rbxassetid://17701691468",
+        ["value"] = 2500,
+        ["chance"] = 20.4,
+        ["type"] = "cash",
+        ["reward"] = "+2,500$"
+    },
+    {
+        ["image"] = "rbxassetid://18122414391",
+        ["value"] = 50,
+        ["chance"] = 10,
+        ["type"] = "bottlecaps",
+        ["reward"] = "+50 Bottlecaps!"
+    },
+    {
+        ["image"] = "rbxassetid://17701691468",
+        ["value"] = 2500,
+        ["chance"] = 20.4,
+        ["type"] = "cash",
+        ["reward"] = "+2,500$"
+    },
+    {
+        ["image"] = "rbxassetid://113953533257556",
+        ["chance"] = 0.1,
+        ["value"] = "Ice Age",
+        ["color"] = Color3.fromRGB(255, 0, 0),
+        ["type"] = "aura",
+        ["reward"] = "Ice Age Aura"
+    }
+}
