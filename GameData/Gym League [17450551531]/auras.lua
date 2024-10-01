6dec65a48b199bdc57d216b80ad67fae5d46ec9f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-02 01:55:12 +1000

    Update auras.lua

--- GameData/Gym League [17450551531]/auras.lua
+++ GameData/Gym League [17450551531]/auras.lua
@@ -6,32 +6,12 @@ data = {
         ["multiplier"] = 1.05,
         ["color"] = Color3.fromRGB(179, 7, 7)
     },
-    ["galaxy"] = {
-        ["image"] = "rbxassetid://17704659000",
+    ["archangel"] = {
+        ["image"] = "rbxassetid://18178166179",
         ["rarity"] = "Exclusive",
-        ["display"] = "Galaxy",
+        ["display"] = "Archangel",
         ["multiplier"] = 2.5,
-        ["body"] = {
-            ["glowHair"] = true,
-            ["hairColor"] = Color3.fromRGB(165, 165, 255),
-            ["color"] = Color3.fromRGB(30, 29, 56),
-            ["material"] = Enum.Material.Neon,
-            ["pantsColor"] = Color3.fromRGB(30, 29, 56),
-            ["pantsMaterial"] = Enum.Material.Neon
-        }
-    },
-    ["yellowfog"] = {
-        ["image"] = "rbxassetid://17735773607",
-        ["rarity"] = "Common",
-        ["display"] = "Yellow Fog",
-        ["multiplier"] = 1.05,
-        ["color"] = Color3.fromRGB(255, 255, 30)
-    },
-    ["curse"] = {
-        ["display"] = "Curse",
-        ["image"] = "rbxassetid://17735747030",
-        ["rarity"] = "Rare",
-        ["multiplier"] = 1.25
+        ["body"] = {["glowHair"] = true, ["hairColor"] = Color3.fromRGB(240, 255, 107)}
     },
     ["purple"] = {
         ["display"] = "Purple",
@@ -39,19 +19,11 @@ data = {
         ["rarity"] = "Rare",
         ["multiplier"] = 1.15
     },
-    ["tealfog"] = {
-        ["image"] = "rbxassetid://17735773607",
-        ["rarity"] = "Common",
-        ["display"] = "Teal Fog",
-        ["multiplier"] = 1.05,
-        ["color"] = Color3.fromRGB(94, 255, 255)
-    },
-    ["blackfog"] = {
-        ["image"] = "rbxassetid://17735773607",
-        ["rarity"] = "Common",
-        ["display"] = "Black Fog",
-        ["multiplier"] = 1.05,
-        ["color"] = Color3.fromRGB(0, 0, 0)
+    ["snakebreathe"] = {
+        ["display"] = "Snake Breathe",
+        ["image"] = "rbxassetid://18577099560",
+        ["rarity"] = "Legendary",
+        ["multiplier"] = 1.9
     },
     ["orange"] = {
         ["image"] = "rbxassetid://17735213808",
@@ -60,22 +32,79 @@ data = {
         ["multiplier"] = 1.05,
         ["color"] = Color3.fromRGB(255, 155, 32)
     },
-    ["red"] = {
+    ["ssjb"] = {
+        ["image"] = "rbxassetid://17735745448",
+        ["rarity"] = "Legendary",
+        ["display"] = "Super Saiyan Blue",
+        ["multiplier"] = 1.8,
+        ["body"] = {["glowHair"] = true, ["hairColor"] = Color3.fromRGB(98, 236, 238)}
+    },
+    ["Ice Age"] = {
+        ["display"] = "Ice Age",
+        ["image"] = "rbxassetid://113953533257556",
+        ["rarity"] = "Mythical",
+        ["multiplier"] = 4
+    },
+    ["Hidden Legend"] = {
+        ["display"] = "Hidden Legend",
+        ["image"] = "rbxassetid://134760701307903",
+        ["rarity"] = "Exclusive",
+        ["multiplier"] = 2.6
+    },
+    ["default"] = {
         ["image"] = "rbxassetid://17735213808",
         ["rarity"] = "Common",
-        ["display"] = "Red Aura",
-        ["multiplier"] = 1.05,
-        ["color"] = Color3.fromRGB(255, 17, 17)
+        ["display"] = "Default",
+        ["gradient"] = "default",
+        ["multiplier"] = 1,
+        ["color"] = Color3.fromRGB(255, 255, 255)
     },
-    ["rage"] = {
-        ["display"] = "Rage",
-        ["image"] = "rbxassetid://17735745900",
+    ["Meteor Storm"] = {
+        ["display"] = "Meteor Storm",
+        ["image"] = "rbxassetid://117423136478861",
+        ["rarity"] = "Legendary",
+        ["multiplier"] = 2.7
+    },
+    ["Ghost Storm"] = {
+        ["display"] = "Ghost Storm",
+        ["image"] = "rbxassetid://128530724731037",
         ["rarity"] = "Rare",
-        ["multiplier"] = 1.15
+        ["multiplier"] = 2
     },
-    ["vip"] = {
-        ["display"] = "VIP Aura",
-        ["image"] = "rbxassetid://17735747293",
+    ["mastersvision"] = {
+        ["display"] = "Masters Vision",
+        ["image"] = "rbxassetid://18471206197",
+        ["rarity"] = "Mythical",
+        ["multiplier"] = 2.1
+    },
+    ["demonemperor"] = {
+        ["display"] = "Demon Emperor",
+        ["image"] = "rbxassetid://18249414368",
+        ["rarity"] = "Exclusive",
+        ["multiplier"] = 2
+    },
+    ["Gold Legend"] = {
+        ["display"] = "Gold Legend",
+        ["image"] = "rbxassetid://110174416641152",
+        ["rarity"] = "Exclusive",
+        ["multiplier"] = 2.1
+    },
+    ["tealfog"] = {
+        ["image"] = "rbxassetid://17735773607",
+        ["rarity"] = "Common",
+        ["display"] = "Teal Fog",
+        ["multiplier"] = 1.05,
+        ["color"] = Color3.fromRGB(94, 255, 255)
+    },
+    ["Sandstorm"] = {
+        ["display"] = "Rainbow Revenant",
+        ["image"] = "rbxassetid://84302011377559",
+        ["rarity"] = "Epic",
+        ["multiplier"] = 2.3
+    },
+    ["Beach Legend"] = {
+        ["display"] = "Beach Legend",
+        ["image"] = "rbxassetid://134582042805069",
         ["rarity"] = "Exclusive",
         ["multiplier"] = 1.5
     },
@@ -85,26 +114,6 @@ data = {
         ["display"] = "Psycho",
         ["multiplier"] = 2,
         ["body"] = {["hairColor"] = Color3.fromRGB(0, 0, 0)}
-    },
-    ["ui"] = {
-        ["image"] = "rbxassetid://17735745233",
-        ["rarity"] = "Mythical",
-        ["display"] = "Ultra Instinct",
-        ["multiplier"] = 2,
-        ["body"] = {["glowHair"] = true, ["hairColor"] = Color3.fromRGB(300, 310, 322)}
-    },
-    ["ssjb"] = {
-        ["image"] = "rbxassetid://17735745448",
-        ["rarity"] = "Legendary",
-        ["display"] = "Super Saiyan Blue",
-        ["multiplier"] = 1.8,
-        ["body"] = {["glowHair"] = true, ["hairColor"] = Color3.fromRGB(98, 236, 238)}
-    },
-    ["slime"] = {
-        ["display"] = "Slime",
-        ["image"] = "rbxassetid://17735745759",
-        ["rarity"] = "Legendary",
-        ["multiplier"] = 1.8
     },
     ["pink"] = {
         ["image"] = "rbxassetid://17735213808",
@@ -119,34 +128,11 @@ data = {
         ["rarity"] = "Epic",
         ["multiplier"] = 1.4
     },
-    ["ssjg"] = {
-        ["image"] = "rbxassetid://17735745308",
-        ["rarity"] = "Legendary",
-        ["display"] = "Super Saiyan God",
-        ["multiplier"] = 1.7,
-        ["body"] = {["hairColor"] = Color3.fromRGB(1000, 400, 200)}
-    },
-    ["default"] = {
-        ["image"] = "rbxassetid://17735213808",
-        ["rarity"] = "Common",
-        ["display"] = "Default",
-        ["gradient"] = "default",
-        ["multiplier"] = 1,
-        ["color"] = Color3.fromRGB(255, 255, 255)
-    },
-    ["green"] = {
-        ["image"] = "rbxassetid://17735213808",
-        ["rarity"] = "Common",
-        ["display"] = "Green Aura",
-        ["multiplier"] = 1.05,
-        ["color"] = Color3.fromRGB(145, 255, 19)
-    },
-    ["dark"] = {
-        ["image"] = "rbxassetid://17735746944",
-        ["rarity"] = "Epic",
-        ["display"] = "Dark",
-        ["multiplier"] = 1.5,
-        ["body"] = {["hairColor"] = Color3.fromRGB(0, 0, 0)}
+    ["shadowmonarch"] = {
+        ["display"] = "Shadow Monarch",
+        ["image"] = "rbxassetid://18249413000",
+        ["rarity"] = "Exclusive",
+        ["multiplier"] = 1.8
     },
     ["ssj"] = {
         ["image"] = "rbxassetid://17735745651",
@@ -155,7 +141,281 @@ data = {
         ["multiplier"] = 1.4,
         ["body"] = {["glowHair"] = true, ["hairColor"] = Color3.fromRGB(255, 238, 66)}
     },
+    ["green"] = {
+        ["image"] = "rbxassetid://17735213808",
+        ["rarity"] = "Common",
+        ["display"] = "Green Aura",
+        ["multiplier"] = 1.05,
+        ["color"] = Color3.fromRGB(145, 255, 19)
+    },
     ["nen"] = {["display"] = "Nen", ["image"] = "rbxassetid://17735746556", ["rarity"] = "Rare", ["multiplier"] = 1.2},
+    ["Charged Up"] = {
+        ["display"] = "Charged Up",
+        ["image"] = "rbxassetid://100903630110630",
+        ["rarity"] = "Exclusive",
+        ["multiplier"] = 3.33
+    },
+    ["galaxy"] = {
+        ["image"] = "rbxassetid://17704659000",
+        ["rarity"] = "Exclusive",
+        ["display"] = "Galaxy",
+        ["multiplier"] = 2.5,
+        ["body"] = {
+            ["glowHair"] = true,
+            ["hairColor"] = Color3.fromRGB(165, 165, 255),
+            ["color"] = Color3.fromRGB(30, 29, 56),
+            ["material"] = Enum.Material.Neon,
+            ["pantsColor"] = Color3.fromRGB(30, 29, 56),
+            ["pantsMaterial"] = Enum.Material.Neon
+        }
+    },
+    ["Bloodborne"] = {
+        ["display"] = "Bloodborne",
+        ["image"] = "rbxassetid://83499033873147",
+        ["rarity"] = "Mythical",
+        ["multiplier"] = 3.5
+    },
+    ["yellowfog"] = {
+        ["image"] = "rbxassetid://17735773607",
+        ["rarity"] = "Common",
+        ["display"] = "Yellow Fog",
+        ["multiplier"] = 1.05,
+        ["color"] = Color3.fromRGB(255, 255, 30)
+    },
+    ["Green God"] = {
+        ["display"] = "Green God",
+        ["image"] = "rbxassetid://18975030358",
+        ["rarity"] = "Common",
+        ["multiplier"] = 1.4
+    },
+    ["cursedmark"] = {
+        ["display"] = "Cursed Mark",
+        ["image"] = "rbxassetid://18577100323",
+        ["rarity"] = "Epic",
+        ["multiplier"] = 1.6
+    },
+    ["Red Rebel"] = {
+        ["display"] = "Red Rebel",
+        ["image"] = "rbxassetid://18975030201",
+        ["rarity"] = "Rare",
+        ["multiplier"] = 2
+    },
+    ["darkness"] = {
+        ["display"] = "Darkness",
+        ["image"] = "rbxassetid://18471207027",
+        ["rarity"] = "Legendary",
+        ["multiplier"] = 1.8
+    },
+    ["blackfog"] = {
+        ["image"] = "rbxassetid://17735773607",
+        ["rarity"] = "Common",
+        ["display"] = "Black Fog",
+        ["multiplier"] = 1.05,
+        ["color"] = Color3.fromRGB(0, 0, 0)
+    },
+    ["conqueror"] = {
+        ["image"] = "rbxassetid://18471207618",
+        ["rarity"] = "Exclusive",
+        ["display"] = "Conqueror",
+        ["multiplier"] = 2.5,
+        ["body"] = {["glowHair"] = true, ["hairColor"] = Color3.fromRGB(50, 0, 16)}
+    },
+    ["red"] = {
+        ["image"] = "rbxassetid://17735213808",
+        ["rarity"] = "Common",
+        ["display"] = "Red Aura",
+        ["multiplier"] = 1.05,
+        ["color"] = Color3.fromRGB(255, 17, 17)
+    },
+    ["Underground Legend"] = {
+        ["display"] = "Underground Legend",
+        ["image"] = "rbxassetid://122562582689759",
+        ["rarity"] = "Exclusive",
+        ["multiplier"] = 1.8
+    },
+    ["ui"] = {
+        ["image"] = "rbxassetid://17735745233",
+        ["rarity"] = "Mythical",
+        ["display"] = "Ultra Instinct",
+        ["multiplier"] = 2,
+        ["body"] = {["glowHair"] = true, ["hairColor"] = Color3.fromRGB(300, 310, 322)}
+    },
+    ["ssjg"] = {
+        ["image"] = "rbxassetid://17735745308",
+        ["rarity"] = "Legendary",
+        ["display"] = "Super Saiyan God",
+        ["multiplier"] = 1.7,
+        ["body"] = {["hairColor"] = Color3.fromRGB(1000, 400, 200)}
+    },
+    ["demonfox"] = {
+        ["display"] = "Demon Fox",
+        ["image"] = "rbxassetid://18577100042",
+        ["rarity"] = "Mythical",
+        ["multiplier"] = 2.8
+    },
+    ["godspeed"] = {
+        ["image"] = "rbxassetid://18178166429",
+        ["rarity"] = "Exclusive",
+        ["display"] = "Godspeed",
+        ["multiplier"] = 1.6,
+        ["body"] = {["glowHair"] = true, ["hairColor"] = Color3.fromRGB(252, 290, 398)}
+    },
+    ["Ghostling"] = {
+        ["display"] = "Ghostling",
+        ["image"] = "rbxassetid://99892795744273",
+        ["rarity"] = "Mythical",
+        ["multiplier"] = 2.8
+    },
+    ["curse"] = {
+        ["display"] = "Curse",
+        ["image"] = "rbxassetid://17735747030",
+        ["rarity"] = "Rare",
+        ["multiplier"] = 1.25
+    },
+    ["Prison Legend"] = {
+        ["display"] = "Prison Legend",
+        ["image"] = "rbxassetid://99225762959633",
+        ["rarity"] = "Exclusive",
+        ["multiplier"] = 2.8
+    },
+    ["Pirate Legend"] = {
+        ["display"] = "Pirate Legend",
+        ["image"] = "rbxassetid://87413927021867",
+        ["rarity"] = "Exclusive",
+        ["multiplier"] = 2.4
+    },
+    ["Rainbow Rage"] = {
+        ["display"] = "Rainbow Rage",
+        ["image"] = "rbxassetid://73174376025435",
+        ["rarity"] = "Mythical",
+        ["multiplier"] = 3.2
+    },
+    ["Cyber Legend"] = {
+        ["display"] = "Cyber Legend",
+        ["image"] = "rbxassetid://94167515642880",
+        ["rarity"] = "Exclusive",
+        ["multiplier"] = 2.2
+    },
+    ["THE Legend"] = {
+        ["display"] = "THE Legend",
+        ["image"] = "rbxassetid://102795615548811",
+        ["rarity"] = "Exclusive",
+        ["multiplier"] = 2
+    },
+    ["slime"] = {
+        ["display"] = "Slime",
+        ["image"] = "rbxassetid://17735745759",
+        ["rarity"] = "Legendary",
+        ["multiplier"] = 1.8
+    },
+    ["vip"] = {
+        ["display"] = "VIP Aura",
+        ["image"] = "rbxassetid://17735747293",
+        ["rarity"] = "Exclusive",
+        ["multiplier"] = 1.5
+    },
+    ["rage"] = {
+        ["display"] = "Rage",
+        ["image"] = "rbxassetid://17735745900",
+        ["rarity"] = "Rare",
+        ["multiplier"] = 1.15
+    },
+    ["guardian"] = {
+        ["display"] = "Guardian",
+        ["image"] = "rbxassetid://18577099794",
+        ["rarity"] = "Legendary",
+        ["multiplier"] = 1.9
+    },
+    ["Radiant Sun"] = {
+        ["display"] = "Radiant Sun",
+        ["image"] = "rbxassetid://129152430647116",
+        ["rarity"] = "Legendary",
+        ["multiplier"] = 2.4
+    },
+    ["sage"] = {
+        ["image"] = "rbxassetid://18249413301",
+        ["rarity"] = "Exclusive",
+        ["display"] = "Sage",
+        ["multiplier"] = 1.8,
+        ["body"] = {["hairColor"] = Color3.fromRGB(255, 255, 255)}
+    },
+    ["Blue Bandit"] = {
+        ["display"] = "Blue Bandit",
+        ["image"] = "rbxassetid://18975030463",
+        ["rarity"] = "Rare",
+        ["multiplier"] = 2.2
+    },
+    ["Gravity Legend"] = {
+        ["display"] = "Gravity Legend",
+        ["image"] = "rbxassetid://123555186519595",
+        ["rarity"] = "Exclusive",
+        ["multiplier"] = 2.3
+    },
+    ["Rainbow Revenant"] = {
+        ["display"] = "Rainbow Revenant",
+        ["image"] = "rbxassetid://18975055075",
+        ["rarity"] = "Mythical",
+        ["multiplier"] = 2.8
+    },
+    ["dark"] = {
+        ["image"] = "rbxassetid://17735746944",
+        ["rarity"] = "Epic",
+        ["display"] = "Dark",
+        ["multiplier"] = 1.5,
+        ["body"] = {["hairColor"] = Color3.fromRGB(0, 0, 0)}
+    },
+    ["Flame Lord"] = {
+        ["display"] = "Flame Lord",
+        ["image"] = "rbxassetid://18975029809",
+        ["rarity"] = "Mythical",
+        ["multiplier"] = 2.6
+    },
+    ["Yellow Yonko"] = {
+        ["display"] = "Yellow Yonko",
+        ["image"] = "rbxassetid://18975030032",
+        ["rarity"] = "Common",
+        ["multiplier"] = 1.6
+    },
+    ["destroyer1"] = {
+        ["image"] = "rbxassetid://18249414120",
+        ["rarity"] = "Exclusive",
+        ["display"] = "Destroyer",
+        ["multiplier"] = 1.5,
+        ["body"] = {["glowHair"] = true, ["hairColor"] = Color3.fromRGB(255, 98, 96)}
+    },
+    ["helsing"] = {
+        ["image"] = "rbxassetid://18249413794",
+        ["rarity"] = "Exclusive",
+        ["display"] = "Helsing",
+        ["multiplier"] = 3,
+        ["body"] = {
+            ["glowHair"] = false,
+            ["hairColor"] = Color3.fromRGB(150, 0, 0),
+            ["color"] = Color3.fromRGB(0, 0, 0),
+            ["material"] = Enum.Material.Neon,
+            ["pantsColor"] = Color3.fromRGB(0, 0, 0),
+            ["pantsMaterial"] = Enum.Material.Neon
+        }
+    },
+    ["sixpaths"] = {
+        ["image"] = "rbxassetid://18249413536",
+        ["rarity"] = "Exclusive",
+        ["display"] = "Six Paths",
+        ["multiplier"] = 2.5,
+        ["body"] = {["glowHair"] = true, ["hairColor"] = Color3.fromRGB(255, 102, 14)}
+    },
+    ["world"] = {
+        ["display"] = "The World",
+        ["image"] = "rbxassetid://18249412691",
+        ["rarity"] = "Exclusive",
+        ["multiplier"] = 1.2
+    },
+    ["Starchain"] = {
+        ["display"] = "Starchain",
+        ["image"] = "rbxassetid://94945619741262",
+        ["rarity"] = "Mythical",
+        ["multiplier"] = 2.8
+    },
     ["fog"] = {
         ["image"] = "rbxassetid://17735773607",
         ["rarity"] = "Common",
