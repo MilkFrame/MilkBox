369749eef3fdff418853da7c868516d79a36f005
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-02 02:05:57 +1000

    Create fortune.lua

---
+++ GameData/Gym League [17450551531]/fortune.lua
@@ -0,0 +1,44 @@
+data = {
+    {
+        ["type"] = "galaxyLevel",
+        ["name"] = "Divine Weight",
+        ["value"] = 2,
+        ["icon"] = "rbxassetid://95674687746238",
+        ["chance"] = 0.5
+    },
+    {
+        ["type"] = "bottlecaps",
+        ["name"] = "Bottlecaps",
+        ["value"] = 100,
+        ["icon"] = "rbxassetid://18122414391",
+        ["chance"] = 36
+    },
+    {
+        ["type"] = "aura",
+        ["name"] = "Ghostling",
+        ["value"] = "Ghostling",
+        ["icon"] = "rbxassetid://99892795744273",
+        ["chance"] = 10
+    },
+    {
+        ["type"] = "potion",
+        ["name"] = "Mega Potion",
+        ["value"] = 3,
+        ["icon"] = "rbxassetid://17654121877",
+        ["chance"] = 20
+    },
+    {
+        ["type"] = "potion",
+        ["name"] = "Speed Potion",
+        ["value"] = 1,
+        ["icon"] = "rbxassetid://17654121216",
+        ["chance"] = 32.5
+    },
+    {
+        ["type"] = "aura",
+        ["name"] = "Bloodborne Aura",
+        ["value"] = "Bloodborne",
+        ["icon"] = "rbxassetid://83499033873147",
+        ["chance"] = 1
+    }
+}
