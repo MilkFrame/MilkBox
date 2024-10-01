25fe5483f8fc2ba37f401b4a006b5f159f85d79a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-02 02:04:43 +1000

    Create universeInfo.lua

---
+++ GameData/Gym League [17450551531]/universeInfo.lua
@@ -0,0 +1,6 @@
+data = {
+    ["UniverseIds"] = {["Olympics"] = 18442256514, ["Starter"] = 17450551531},
+    ["GameIds"] = {["Main"] = 5972059550, ["Debug"] = 6101298150},
+    ["IsGameType"] = "function: 0x1331263e939c66c2",
+    ["IsUniverseType"] = "function: 0x895abe8911d6c652"
+}
