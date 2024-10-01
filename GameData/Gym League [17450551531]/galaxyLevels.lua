dc0e8e884d2d2198b067f982c2d8f3f17983e31e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-02 01:58:42 +1000

    Create galaxyLevels.lua

---
+++ GameData/Gym League [17450551531]/galaxyLevels.lua
@@ -0,0 +1,4 @@
+data = {
+    {["machineMultiplier"] = 2, ["name"] = "Galaxy", ["icon"] = "rbxassetid://17632654103"},
+    {["machineMultiplier"] = 3, ["name"] = "Divine", ["icon"] = "rbxassetid://95674687746238"}
+}
