feb6237966f974d9eb6ba5b1b65ac05a048f0bc0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-02 02:04:15 +1000

    Create crates.lua

---
+++ GameData/Gym League [17450551531]/crates.lua
@@ -0,0 +1,83 @@
+data = {
+    ["Premium Crate"] = {
+        ["rollProductId"] = 1897982183,
+        ["premiumRarities"] = {["Legendary"] = 10, ["Common"] = 0, ["Epic"] = 50, ["Mythical"] = 5, ["Rare"] = 35},
+        ["animStopTime"] = 1.6666666666666667,
+        ["rarities"] = {["Legendary"] = 1, ["Common"] = 63.99, ["Epic"] = 10, ["Mythical"] = 0.01, ["Rare"] = 25},
+        ["robuxCrate"] = true,
+        ["productId"] = 1874359329,
+        ["chances"] = {
+            ["Legendary"] = {["Golden Boots"] = 1},
+            ["Common"] = {},
+            ["Epic"] = {["Blue Jords"] = 1, ["Red Jords"] = 1},
+            ["Mythical"] = {["Box Shoe"] = 1},
+            ["Rare"] = {["Boots"] = 1, ["White Sneakers"] = 1}
+        },
+        ["spin_price"] = 1500,
+        ["animLockTime"] = 1.9166666666666667,
+        ["rollProductId2"] = 1897983225
+    },
+    ["Event Crate"] = {
+        ["premiumRarities"] = {["Legendary"] = 10, ["Common"] = 0, ["Epic"] = 50, ["Mythical"] = 1, ["Rare"] = 39},
+        ["animStopTime"] = 1.6666666666666667,
+        ["event"] = true,
+        ["rarities"] = {["Legendary"] = 2, ["Common"] = 0, ["Epic"] = 23.99, ["Mythical"] = 0.01, ["Rare"] = 74},
+        ["robuxCrate"] = false,
+        ["spin_price"] = 1200,
+        ["chances"] = {
+            ["Legendary"] = {["Golden Horn"] = 1},
+            ["Common"] = {},
+            ["Epic"] = {["Red Horn"] = 1},
+            ["Mythical"] = {["IronSpiderArms"] = 1, ["Olympic Wings"] = 1},
+            ["Rare"] = {["Torch"] = 1, ["Starfish"] = 1}
+        },
+        ["animLockTime"] = 1.9166666666666667
+    },
+    ["Basic Crate"] = {
+        ["rollProductId"] = 1897982183,
+        ["premiumRarities"] = {["Legendary"] = 10, ["Common"] = 0, ["Epic"] = 50, ["Mythical"] = 1, ["Rare"] = 39},
+        ["animStopTime"] = 1.6666666666666667,
+        ["rarities"] = {["Legendary"] = 1, ["Common"] = 63.99, ["Epic"] = 10, ["Mythical"] = 0.01, ["Rare"] = 25},
+        ["robuxCrate"] = false,
+        ["productId"] = 1874359329,
+        ["chances"] = {
+            ["Legendary"] = {
+                ["frogwarrior"] = 1,
+                ["onimask"] = 1,
+                ["sharkblade"] = 1,
+                ["championshipbelt"] = 1,
+                ["goldensweatband"] = 1,
+                ["angelwings"] = 1
+            },
+            ["Common"] = {
+                ["liftingbelt"] = 1,
+                ["greysweatband"] = 1,
+                ["oxygenmask"] = 1,
+                ["redbandana"] = 1,
+                ["birdwings"] = 1,
+                ["redsweatband"] = 1
+            },
+            ["Epic"] = {
+                ["serpentsservant"] = 1,
+                ["dragonwings"] = 1,
+                ["respirator"] = 1,
+                ["hiddenscroll"] = 1,
+                ["goldenliftingbelt"] = 1,
+                ["starsweatband"] = 1
+            },
+            ["Mythical"] = {["cursedwings"] = 1, ["exoskeleton"] = 0.6, ["emperorhorns"] = 1},
+            ["Rare"] = {
+                ["paintballmask"] = 1,
+                ["ninjamark"] = 1,
+                ["flamesweatband"] = 1,
+                ["copybandana"] = 1,
+                ["skeletonwings"] = 1,
+                ["skullbandana"] = 1,
+                ["cowboybelt"] = 1
+            }
+        },
+        ["spin_price"] = 1500,
+        ["animLockTime"] = 1.9166666666666667,
+        ["rollProductId2"] = 1897983225
+    }
+}
