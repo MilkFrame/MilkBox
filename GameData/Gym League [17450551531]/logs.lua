94357136797bbd73cb46ad1ba764568850f84ab7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-02 02:02:23 +1000

    Update logs.lua

--- GameData/Gym League [17450551531]/logs.lua
+++ GameData/Gym League [17450551531]/logs.lua
@@ -1,6 +1,155 @@
 data = {
     {
         ["logs"] = {
+            "Temple Minefield. In our newest minigame we added a temple minefield where players will have to find the right way through a temple in order to collect REWARDS!",
+            "Weight System Change. We’ve added support to easily and add change weights meaning in the future we’re going to be adding a bunch more stay tuned.",
+            "Multiplier Event. Players until next Sunday in accordance with a smaller update this week will receive 3x Muscles/Stamina",
+            "BALANCING CHANGES. Lower areas have been increased once  again to try and entice users to power through and unlock new Gyms!",
+            "Added new codes (Check discord for more info)"
+        },
+        ["mainUpdate"] = true,
+        ["header"] = "UPDATE 10",
+        ["thumbnail"] = "rbxassetid://18364134188"
+    },
+    {
+        ["logs"] = {
+            "New Prison Gym. Work your way up to the new PRISON gym! Complete all 3 new Body Alters, and purchase all of the brand new powerups! This world comes with new quests, and a beautiful courtyard with absolutely no danger at all... 👀",
+            "New Sisyphus Minigame. In Gym 4, we added a brand new minigame where the goal is to push a gigantic rock all the way up different slopes. The player will receive different rewards for pushing it up different slopes of different sizes!",
+            "Gyms Easier. Due to the addition of the new gym, we went ahead and SEVERELY INCREASED the amount of muscle gained and stamina gained in all previous gyms.. This should hopefully let users catch up to the new gym. ",
+            "New Packs. The Current pack has been replaced with a new pack, and the new prison gym has also received a brand new pack as well!",
+            "Added new codes (Check discord for more info)"
+        },
+        ["mainUpdate"] = true,
+        ["header"] = "UPDATE 9",
+        ["thumbnail"] = "rbxassetid://18364134188"
+    },
+    {
+        ["logs"] = {
+            [[New Weight. Obtainable "Divine" weight which works similar to the galaxy weight.]],
+            "Spin Wheel. Purchase, gift, or obtain spins for a chance at unique items!",
+            "Luck Reworked. Luck now applies to chances correctly, and you can see the chances with luck! Lucky gamepass has +50% Luck, and certain gears can also increase your luck.",
+            "Area Packs. Each Gym has a unique buyable pack with its own aura and gear!",
+            "New battlepass season."
+        },
+        ["mainUpdate"] = true,
+        ["header"] = "UPDATE 8",
+        ["thumbnail"] = "rbxassetid://18364134188"
+    },
+    {
+        ["logs"] = {
+            "Athletes have started their very own competitions in Gyms: 1, 5, 6 and 8!",
+            "Compete in swimming and track for the following rewards:",
+            "	Auras",
+            "	Premium Gear Spins",
+            "	+1 F2P Gear Slot",
+            "	FREE Powerups"
+        },
+        ["mainUpdate"] = true,
+        ["header"] = "UPDATE 7",
+        ["thumbnail"] = "rbxassetid://18577099200"
+    },
+    {
+        ["logs"] = {
+            "New Machines in Gym 2+",
+            "The Games ends TOMORROW!",
+            "Galaxy Weight added to all machines",
+            "Stamina Rebalance",
+            "Weight Rebalance",
+            "Mutated Genes: +200% -> TOTAL 2x",
+            "New Premium Crate rework",
+            "New shoe gears!",
+            "Severe bugs fixed + complete optimization on mobile",
+            "Minigame Support for next week 👀"
+        },
+        ["mainUpdate"] = true,
+        ["header"] = "UPDATE 6",
+        ["thumbnail"] = "rbxassetid://18749873182"
+    },
+    {
+        ["buttonColor"] = Color3.fromRGB(69, 184, 255),
+        ["mainUpdate"] = true,
+        ["header"] = "THE GAMES",
+        ["strokeColor"] = Color3.fromRGB(33, 98, 177),
+        ["thumbnail"] = "rbxassetid://18749873182",
+        ["logs"] = {
+            "COMPETE IN THE 3 MINIGAMES TO WIN BADGES FOR YOUR TEAM!",
+            "PORTAL ON BEACH GYM!",
+            "FIND THE CRYSTALS TO SCORE MORE POINTS!",
+            "(🔨 Various Bug Fixes)"
+        }
+    },
+    {
+        ["logs"] = {
+            "SUMMER EVENT 🏖",
+            "Join Red, Green, Blue or Yellow!",
+            "Best teams win prizes at event end",
+            "Collect stamps to buy limited time items!",
+            "New Summer Store!",
+            "New Gears (7)",
+            "New Power Ups (6)"
+        },
+        ["mainUpdate"] = true,
+        ["header"] = "UPDATE 5",
+        ["thumbnail"] = "rbxassetid://18577099200"
+    },
+    {
+        ["logs"] = {
+            "New GYM (Hidden GYM)",
+            "New Gears (7)",
+            "New body alters (3)",
+            "New power ups (4)'",
+            "New auras (4)",
+            "New poses (4)",
+            "Upgrade and Trade Up system!",
+            "Full balancing rework",
+            "Bug Fixes",
+            "Added new codes (Check discord for more info)"
+        },
+        ["mainUpdate"] = true,
+        ["header"] = "UPDATE 4",
+        ["thumbnail"] = "rbxassetid://18577099200"
+    },
+    {
+        ["logs"] = {
+            "New gear system",
+            "New GYM (Pirate GYM)",
+            "New body alters (3)",
+            "New power ups'",
+            "New auras",
+            "New poses",
+            "New music in every area",
+            "Normal & Premium crate",
+            "Skip Animation changes",
+            "New gamepass (+1 Gear slot)",
+            "Archangel boost 2 -> 2.5",
+            "Previous areas made easier",
+            "Bug Fixes",
+            "Added new codes (Check discord for more info)"
+        },
+        ["mainUpdate"] = true,
+        ["header"] = "UPDATE 3",
+        ["thumbnail"] = "rbxassetid://18471724335"
+    },
+    {
+        ["logs"] = {
+            "Free auto train",
+            "Added battlepass",
+            "Added new limited aura 'Archangel'",
+            "Added new auras",
+            "Added option to hide boosts on mobile",
+            "Added skip to pose reroll",
+            "Lucky game pass boost increased from 1.5x to 1.8x",
+            "Increased frame sizes on mobile",
+            "Fixed the FPS problem with opening frames",
+            "Fixed bugs",
+            "Added new codes (Check discord for more info)"
+        },
+        ["mainUpdate"] = true,
+        ["header"] = "UPDATE 2",
+        ["thumbnail"] = "rbxassetid://18364134188"
+    },
+    {
+        ["logs"] = {
             "New GYM (Gravity GYM)",
             "New body alters",
             "New power ups",
@@ -25,7 +174,7 @@ data = {
             "Added new codes (Check discord for more info)"
         },
         ["mainUpdate"] = false,
-        ["header"] = "v1.2 Log",
+        ["header"] = "v0.2 Log",
         ["thumbnail"] = "rbxassetid://17698119228"
     },
     {
@@ -38,7 +187,7 @@ data = {
             "Some bug fixes"
         },
         ["mainUpdate"] = false,
-        ["header"] = "v1.1 Log",
+        ["header"] = "v0.1 Log",
         ["thumbnail"] = "rbxassetid://17786274272"
     }
 }
