682a1277d6fc2b83ea069ef65be5c838876340bf
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-02 01:56:57 +1000

    Update muscles.lua

--- GameData/Gym League [17450551531]/muscles.lua
+++ GameData/Gym League [17450551531]/muscles.lua
@@ -1,10 +1,21 @@
 data = {
+    ["Track"] = {
+        ["id"] = 17462257192,
+        ["image"] = "rbxassetid://17462257192",
+        ["color"] = Color3.fromRGB(113, 167, 255)
+    },
     ["Triceps"] = {["id"] = 17462090126, ["image"] = "rbxassetid://17462090126"},
     ["Abs"] = {["id"] = 17462471412, ["image"] = "rbxassetid://17462471412"},
+    ["Boxing"] = {["id"] = 17462257192, ["image"] = "rbxassetid://17462257192", ["color"] = Color3.fromRGB(255, 61, 61)},
     ["Back"] = {["id"] = 17462471197, ["image"] = "rbxassetid://17462471197"},
     ["Chest"] = {["id"] = 17462471022, ["image"] = "rbxassetid://17462471022"},
-    ["Calves"] = {["id"] = 17462748139, ["image"] = "rbxassetid://17462748139"},
     ["Legs"] = {["id"] = 17462747943, ["image"] = "rbxassetid://17462747943"},
+    ["Calves"] = {["id"] = 17462748139, ["image"] = "rbxassetid://17462748139"},
+    ["Deadlift"] = {
+        ["id"] = 17462257192,
+        ["image"] = "rbxassetid://17462257192",
+        ["color"] = Color3.fromRGB(48, 79, 255)
+    },
     ["Stamina"] = {
         ["id"] = 17462257192,
         ["image"] = "rbxassetid://17462257192",
