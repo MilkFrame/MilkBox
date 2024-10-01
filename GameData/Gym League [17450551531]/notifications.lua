e12d9bddff4e2a4bb4ebcd052ad4367b513b9907
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-02 01:58:56 +1000

    Update notifications.lua

--- GameData/Gym League [17450551531]/notifications.lua
+++ GameData/Gym League [17450551531]/notifications.lua
@@ -1,4 +1,14 @@
 data = {
+    ["gained_gearroll"] = {
+        ["color"] = Color3.fromRGB(50, 255, 50),
+        ["text"] = "You gained +1 Gear Roll!",
+        ["sound"] = "Sucess2"
+    },
+    ["noncompliant"] = {
+        ["color"] = Color3.fromRGB(255, 39, 39),
+        ["text"] = "This feature is restricted.",
+        ["sound"] = "Sucess2"
+    },
     ["pose_equip_err"] = {
         ["color"] = Color3.fromRGB(255, 50, 50),
         ["text"] = "You don't have enough money!",
@@ -10,10 +20,32 @@ data = {
         ["sound"] = "Error"
     },
     ["nem"] = {["color"] = Color3.fromRGB(255, 50, 50), ["text"] = "Not enough money!", ["sound"] = "Error"},
+    ["fortune_reward"] = {["color"] = Color3.fromRGB(50, 255, 50), ["text"] = "You received %s!", ["sound"] = "Sucess2"},
     ["nae"] = {["color"] = Color3.fromRGB(255, 50, 50), ["text"] = "You can't use this right now!", ["sound"] = "Error"},
-    ["code_format_wrong"] = {
+    ["minigamestarting"] = {
+        ["color"] = Color3.fromRGB(10, 255, 71),
+        ["text"] = "A minigame is starting!",
+        ["sound"] = "Sucess2"
+    },
+    ["olympics_buy"] = {["color"] = Color3.fromRGB(50, 255, 50), ["text"] = "Purchase success!", ["sound"] = "Success2"},
+    ["olympic_game_not_active"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "Come back when a game is starting!",
+        ["sound"] = "Error"
+    },
+    ["olympic_overcapacity"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "This team has too many members!",
+        ["sound"] = "Error"
+    },
+    ["olympics_track"] = {
+        ["color"] = Color3.fromRGB(50, 255, 50),
+        ["text"] = "A track and field competition is starting!",
+        ["sound"] = "Success2"
+    },
+    ["blocked_ads"] = {
         ["color"] = Color3.fromRGB(255, 75, 75),
-        ["text"] = "Code format wrong!",
+        ["text"] = "You aren't eligible to view ads!",
         ["sound"] = "Error"
     },
     ["rate_2_hours"] = {
@@ -21,38 +53,97 @@ data = {
         ["text"] = "You can only send feedbacks every 2 hours!",
         ["sound"] = "Error"
     },
-    ["nep"] = {["color"] = Color3.fromRGB(255, 50, 50), ["text"] = "Not enough power!", ["sound"] = "Error"},
+    ["unlocked_gear_slot"] = {
+        ["color"] = Color3.fromRGB(255, 75, 75),
+        ["text"] = "You already unlocked this gear slot!",
+        ["sound"] = "Error"
+    },
+    ["olympics_not_enough_stamps"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "You do not have enough stamps!",
+        ["sound"] = "Error"
+    },
+    ["pfy"] = {["color"] = Color3.fromRGB(50, 255, 50), ["text"] = "%s purchased for you!", ["sound"] = "Sucess2"},
+    ["upgfail"] = {["color"] = Color3.fromRGB(255, 50, 50), ["text"] = "Upgrade failed!", ["sound"] = "Error"},
     ["like_rewards"] = {
         ["color"] = Color3.fromRGB(50, 255, 50),
         ["text"] = "Successfully redeemed the rewards!",
         ["sound"] = "Sucess2"
     },
-    ["daily_award"] = {
-        ["color"] = Color3.fromRGB(50, 255, 50),
-        ["text"] = "You have received your daily reward!",
-        ["sound"] = "Success2"
-    },
-    ["cca"] = {["color"] = Color3.fromRGB(255, 50, 50), ["text"] = "Complete current body alter!", ["sound"] = "Error"},
-    ["code_not_exist_err"] = {
-        ["color"] = Color3.fromRGB(255, 75, 75),
-        ["text"] = "Code does not exist!",
+    ["samerarity"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "All gears must be of the same rarity!",
         ["sound"] = "Error"
     },
+    ["cant_be_shiny"] = {
+        ["color"] = Color3.fromRGB(255, 75, 75),
+        ["text"] = "This gear can't be turned into a shiny",
+        ["sound"] = "Error"
+    },
+    ["cca"] = {["color"] = Color3.fromRGB(255, 50, 50), ["text"] = "Complete current body alter!", ["sound"] = "Error"},
+    ["packowned"] = {
+        ["color"] = Color3.fromRGB(255, 30, 30),
+        ["text"] = "You already own this pack!",
+        ["sound"] = "Error"
+    },
+    ["shiny_extra"] = {
+        ["color"] = Color3.fromRGB(255, 75, 75),
+        ["text"] = "You need extra shiny slot!",
+        ["sound"] = "Error"
+    },
+    ["shiny_more"] = {["color"] = Color3.fromRGB(255, 75, 75), ["text"] = "You need %s more gear!", ["sound"] = "Error"},
     ["code_success_redeem"] = {
         ["color"] = Color3.fromRGB(0, 255, 0),
         ["text"] = "Successfully redeemded!",
         ["sound"] = "Sucess2"
+    },
+    ["10gears"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "You can select up to 10 gears only!",
+        ["sound"] = "Error"
+    },
+    ["code_player_data_err"] = {
+        ["color"] = Color3.fromRGB(255, 75, 75),
+        ["text"] = "Player data not found!",
+        ["sound"] = "Error"
+    },
+    ["code_format_wrong"] = {
+        ["color"] = Color3.fromRGB(255, 75, 75),
+        ["text"] = "Code format wrong!",
+        ["sound"] = "Error"
+    },
+    ["olympics_deadlift"] = {
+        ["color"] = Color3.fromRGB(50, 255, 50),
+        ["text"] = "A deadlifting competition is starting!",
+        ["sound"] = "Success2"
+    },
+    ["powerups_max_buyable_limit"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "You have reached the maximum limit (5) of buyable powerups!",
+        ["sound"] = "Error"
+    },
+    ["upgsucc"] = {
+        ["color"] = Color3.fromRGB(50, 255, 50),
+        ["text"] = "Upgrade successful! You received: %s",
+        ["sound"] = "Sucess2"
+    },
+    ["code_not_exist_err"] = {
+        ["color"] = Color3.fromRGB(255, 75, 75),
+        ["text"] = "Code does not exist!",
+        ["sound"] = "Error"
     },
     ["dp"] = {
         ["color"] = Color3.fromRGB(255, 50, 50),
         ["text"] = "You need to continue on tutorial to occupy this!",
         ["sound"] = "Error"
     },
+    ["uw"] = {["color"] = Color3.fromRGB(255, 50, 50), ["text"] = "Unlock next world!", ["sound"] = "Error"},
     ["aura_enough_spin"] = {
         ["color"] = Color3.fromRGB(255, 50, 50),
         ["text"] = "You don't have enough spins!",
         ["sound"] = "Error"
     },
+    ["olympic_joined"] = {["color"] = Color3.fromRGB(255, 255, 255), ["text"] = "Joined team!", ["sound"] = "Success2"},
     ["code_is_too_short"] = {
         ["color"] = Color3.fromRGB(255, 75, 75),
         ["text"] = "Code is too short!",
@@ -62,6 +153,16 @@ data = {
         ["color"] = Color3.fromRGB(50, 255, 50),
         ["text"] = "Daily reward already claimed.",
         ["sound"] = "Success2"
+    },
+    ["powerups_cannot_buyable"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "This powerup cannot be bought!",
+        ["sound"] = "Error"
+    },
+    ["fortune_enough_money"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "You don't have enough wheel spins!",
+        ["sound"] = "Error"
     },
     ["limit"] = {
         ["color"] = Color3.fromRGB(255, 50, 50),
@@ -73,14 +174,25 @@ data = {
         ["text"] = "Not enough cash to buy this powerup!",
         ["sound"] = "Error"
     },
+    ["top2team"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "Your team must be in top 2 to use Premium GYM!",
+        ["sound"] = "Error"
+    },
+    ["olympic_notonteam"] = {
+        ["color"] = Color3.fromRGB(255, 50, 50),
+        ["text"] = "You are not on a team yet!",
+        ["sound"] = "Error"
+    },
+    ["nep"] = {["color"] = Color3.fromRGB(255, 50, 50), ["text"] = "Not enough power!", ["sound"] = "Error"},
     ["code_already_redeemed"] = {
         ["color"] = Color3.fromRGB(255, 75, 75),
         ["text"] = "Code already redeemed!",
         ["sound"] = "Error"
     },
-    ["rate_error"] = {
-        ["color"] = Color3.fromRGB(255, 50, 50),
-        ["text"] = "An error occured while sending!",
+    ["to_be_done"] = {
+        ["color"] = Color3.fromRGB(255, 30, 30),
+        ["text"] = "Packs are not giftable. (Coming Soon)",
         ["sound"] = "Error"
     },
     ["aura_already"] = {
@@ -88,21 +200,23 @@ data = {
         ["text"] = "You already have this aura :(",
         ["sound"] = "Error"
     },
+    ["daily_award"] = {
+        ["color"] = Color3.fromRGB(50, 255, 50),
+        ["text"] = "You have received your daily reward!",
+        ["sound"] = "Success2"
+    },
+    ["cratereward"] = {["color"] = Color3.fromRGB(50, 255, 50), ["text"] = "You obtained %s!", ["sound"] = "Sucess2"},
     ["pose_spin_err"] = {
         ["color"] = Color3.fromRGB(255, 50, 50),
         ["text"] = "You don't have enough spins!",
         ["sound"] = "Error"
     },
-    ["code_player_data_err"] = {
-        ["color"] = Color3.fromRGB(255, 75, 75),
-        ["text"] = "Player data not found!",
-        ["sound"] = "Error"
-    },
+    ["insgear"] = {["color"] = Color3.fromRGB(255, 50, 50), ["text"] = "Insufficient gears!", ["sound"] = "Error"},
     ["code_expired_err"] = {["color"] = Color3.fromRGB(255, 75, 75), ["text"] = "Code expired!", ["sound"] = "Error"},
     ["tfp"] = {["color"] = Color3.fromRGB(50, 255, 50), ["text"] = "Thanks for purchasing!", ["sound"] = "Sucess2"},
-    ["powerups_max_buyable_limit"] = {
+    ["rate_error"] = {
         ["color"] = Color3.fromRGB(255, 50, 50),
-        ["text"] = "You have reached the maximum limit (5) of buyable powerups!",
+        ["text"] = "An error occured while sending!",
         ["sound"] = "Error"
     },
     ["aura_enough_money"] = {
@@ -110,9 +224,9 @@ data = {
         ["text"] = "You don't have enough money!",
         ["sound"] = "Error"
     },
-    ["powerups_cannot_buyable"] = {
-        ["color"] = Color3.fromRGB(255, 50, 50),
-        ["text"] = "This powerup cannot be bought!",
+    ["minigamecooldown"] = {
+        ["color"] = Color3.fromRGB(255, 14, 14),
+        ["text"] = "You are on cooldown! Try again later",
         ["sound"] = "Error"
     },
     ["pose_enough_money"] = {
@@ -125,8 +239,16 @@ data = {
         ["text"] = "You don't have enough spins!",
         ["sound"] = "Error"
     },
-    ["pfy"] = {["color"] = Color3.fromRGB(50, 255, 50), ["text"] = "%s purchased for you!", ["sound"] = "Sucess2"},
-    ["uw"] = {["color"] = Color3.fromRGB(255, 50, 50), ["text"] = "Unlock next world!", ["sound"] = "Error"},
+    ["battlepass_award"] = {
+        ["color"] = Color3.fromRGB(50, 255, 50),
+        ["text"] = "Level award claimed!",
+        ["sound"] = "Success2"
+    },
+    ["unlock_gear_slot"] = {
+        ["color"] = Color3.fromRGB(255, 75, 75),
+        ["text"] = "You need to unlock the second gear slot!",
+        ["sound"] = "Error"
+    },
     ["powerups_unlock_world"] = {
         ["color"] = Color3.fromRGB(255, 50, 50),
         ["text"] = "You haven't unlocked the required world to use this powerup!",
