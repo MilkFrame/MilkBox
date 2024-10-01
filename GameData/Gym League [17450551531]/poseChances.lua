e8b3e85f46abb6e2931d9425542a524beb39f0bc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-02 02:05:43 +1000

    Update poseChances.lua

--- GameData/Gym League [17450551531]/poseChances.lua
+++ GameData/Gym League [17450551531]/poseChances.lua
@@ -1,10 +1,14 @@
 data = {
     ["rarities"] = {["Legendary"] = 1, ["Common"] = 63.9, ["Epic"] = 10, ["Mythical"] = 0.1, ["Rare"] = 25},
     ["poses"] = {
-        ["Legendary"] = {["Front Lat Spread"] = 1, ["Front Lat And Bicep"] = 1, ["Three Quarter Back Biceps"] = 1},
-        ["Common"] = {["Arm Up"] = 1},
-        ["Epic"] = {["Aduket"] = 1},
-        ["Mythical"] = {["The Kneel"] = 1, ["Most Muscular"] = 1},
-        ["Rare"] = {["Side Chest"] = 1, ["Victory"] = 1}
+        ["Legendary"] = {
+            ["Front Lat Spread"] = 1,
+            ["FloatingSpirit"] = 1,
+            ["Front Lat And Bicep"] = 1,
+            ["Three Quarter Back Biceps"] = 1
+        },
+        ["Common"] = {["Arm Up"] = 1, ["Punch"] = 1},
+        ["Epic"] = {["OldLegend"] = 1, ["Aduket"] = 1, ["Duplicate"] = 1},
+        ["Mythical"] = {["Combat Ready"] = 1, ["The Kneel"] = 1, ["RisingChampion"] = 1, ["Most Muscular"] = 1},
+        ["Rare"] = {["Side Chest"] = 1, ["Haka"] = 1, ["Fox Kick"] = 1, ["Victory"] = 1, ["Fox Punch"] = 1}
     }
-}
