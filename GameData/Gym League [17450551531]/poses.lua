f5fb5625b4a71f52c686ba5e46b985449f99e10e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-02 01:58:06 +1000

    Update poses.lua

--- GameData/Gym League [17450551531]/poses.lua
+++ GameData/Gym League [17450551531]/poses.lua
@@ -1,16 +1,25 @@
 data = {
     ["Arm Up"] = {["display"] = "Arm Up", ["image"] = 17734920277, ["rarity"] = "Common"},
+    ["RisingChampion"] = {["display"] = "RisingChampion", ["image"] = 18457392134, ["rarity"] = "Mythical"},
     ["Godlike"] = {["display"] = "Godlike", ["image"] = 17705391271, ["rarity"] = "Exclusive"},
-    ["Front Lat Spread"] = {["display"] = "Front Lat Spread", ["image"] = 17734919695, ["rarity"] = "Legendary"},
     ["The Kneel"] = {["display"] = "The Kneel", ["image"] = 17734919907, ["rarity"] = "Mythical"},
     ["Victory"] = {["display"] = "Victory", ["image"] = 17734918835, ["rarity"] = "Rare"},
     ["Side Chest"] = {["display"] = "Side Chest", ["image"] = 17734918974, ["rarity"] = "Rare"},
-    ["Most Muscular"] = {["display"] = "Most Muscular", ["image"] = 17734919530, ["rarity"] = "Mythical"},
-    ["Front Lat And Bicep"] = {["display"] = "Front Lat And Bicep", ["image"] = 17734920077, ["rarity"] = "Common"},
-    ["Aduket"] = {["display"] = "Aduket", ["image"] = 17734920428, ["rarity"] = "Epic"},
+    ["Haka"] = {["display"] = "Haka", ["image"] = 18457380344, ["rarity"] = "Rare"},
+    ["FloatingSpirit"] = {["display"] = "Floating Spirit", ["image"] = 18457388170, ["rarity"] = "Legendary"},
     ["Three Quarter Back Biceps"] = {
         ["display"] = "Three Quarter Back Biceps",
         ["image"] = 17734919200,
         ["rarity"] = "Legendary"
-    }
+    },
+    ["Front Lat Spread"] = {["display"] = "Front Lat Spread", ["image"] = 17734919695, ["rarity"] = "Legendary"},
+    ["Fox Kick"] = {["display"] = "Fox Kick", ["image"] = 18570512179, ["rarity"] = "Rare"},
+    ["Front Lat And Bicep"] = {["display"] = "Front Lat And Bicep", ["image"] = 17734920077, ["rarity"] = "Common"},
+    ["Fox Punch"] = {["display"] = "Fox Punch", ["image"] = 18570509713, ["rarity"] = "Rare"},
+    ["Combat Ready"] = {["display"] = "Combat Ready", ["image"] = 18570510397, ["rarity"] = "Mythical"},
+    ["Duplicate"] = {["display"] = "Duplicate", ["image"] = 18570510190, ["rarity"] = "Epic"},
+    ["OldLegend"] = {["display"] = "Old Legend", ["image"] = 18457385073, ["rarity"] = "Epic"},
+    ["Punch"] = {["display"] = "Punch", ["image"] = 18457370873, ["rarity"] = "Common"},
+    ["Aduket"] = {["display"] = "Aduket", ["image"] = 17734920428, ["rarity"] = "Epic"},
+    ["Most Muscular"] = {["display"] = "Most Muscular", ["image"] = 17734919530, ["rarity"] = "Mythical"}
 }
