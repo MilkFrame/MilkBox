0f8f63073ce10445cd3efcd793e9fa6ec9e4419d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-02 02:04:28 +1000

    Create olympicConfig.lua

---
+++ GameData/Gym League [17450551531]/olympicConfig.lua
@@ -0,0 +1,15 @@
+data = {
+    ["Refs"] = {},
+    ["Teams"] = {
+        ["y"] = {["Color"] = Color3.fromRGB(251, 255, 17), ["DisplayName"] = "Yellow"},
+        ["b"] = {["Color"] = Color3.fromRGB(47, 154, 255), ["DisplayName"] = "Blue"},
+        ["g"] = {["Color"] = Color3.fromRGB(46, 255, 77), ["DisplayName"] = "Green"},
+        ["r"] = {["Color"] = Color3.fromRGB(255, 28, 28), ["DisplayName"] = "Red"}
+    },
+    ["IsOlympicServer"] = false,
+    ["RBXEventEnabled"] = true,
+    ["EndTime"] = "1723402800000",
+    ["StartTime"] = "1722178800000",
+    ["TeamBalanceThreshold"] = 2000,
+    ["RBXSimulateCollect"] = false
+}
