cbd20bac603144a94ad9366851608c69bc78440f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-02 02:03:00 +1000

    Create battlepassDailyGift.lua

---
+++ GameData/Gym League [17450551531]/battlepassDailyGift.lua
@@ -0,0 +1,31 @@
+data = {
+    ["normal"] = {
+        {["value"] = 250, ["type"] = "bottlecaps", ["icon"] = "rbxassetid://17701691468", ["reward"] = "Bottlecaps"},
+        {["value"] = 1, ["type"] = "potion", ["icon"] = "rbxassetid://17654121583", ["reward"] = "Power Potion"},
+        {["value"] = 5, ["type"] = "spin", ["icon"] = "rbxassetid://17735246415", ["reward"] = "Spins"},
+        {["value"] = 1, ["type"] = "potion", ["icon"] = "rbxassetid://17654122529", ["reward"] = "Money Potion"},
+        {["value"] = 10, ["type"] = "spin", ["icon"] = "rbxassetid://17735246415", ["reward"] = "Spins"},
+        {["value"] = 1, ["type"] = "potion", ["icon"] = "rbxassetid://17654121877", ["reward"] = "Mega Potion"},
+        {["value"] = 1, ["type"] = "premiumSpin", ["icon"] = "rbxassetid://17735246150", ["reward"] = "Premium Spin"},
+        {["value"] = 250, ["type"] = "bottlecaps", ["icon"] = "rbxassetid://17701691468", ["reward"] = "Bottlecaps"},
+        {["value"] = 1, ["type"] = "potion", ["icon"] = "rbxassetid://17654121583", ["reward"] = "Power Potion"},
+        {["value"] = 5, ["type"] = "spin", ["icon"] = "rbxassetid://17735246415", ["reward"] = "Spins"},
+        {["value"] = 1, ["type"] = "potion", ["icon"] = "rbxassetid://17654122529", ["reward"] = "Money Potion"},
+        {["value"] = 10, ["type"] = "spin", ["icon"] = "rbxassetid://17735246415", ["reward"] = "Spins"},
+        {["value"] = 1, ["type"] = "potion", ["icon"] = "rbxassetid://17654121877", ["reward"] = "Mega Potion"},
+        {
+            ["thickness"] = 4,
+            ["type"] = "aura",
+            ["color"] = Color3.fromRGB(41, 169, 255),
+            ["value"] = "Ghost Storm",
+            ["alternate"] = {
+                ["value"] = 3,
+                ["type"] = "potion",
+                ["icon"] = "rbxassetid://17654121877",
+                ["reward"] = "Mega Potion"
+            },
+            ["icon"] = "rbxassetid://128530724731037",
+            ["reward"] = "Ghost Storm Aura"
+        }
+    }
+}
