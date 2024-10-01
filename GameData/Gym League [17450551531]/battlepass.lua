6eac94a7769c0a20fba31e09e88a9254e31fa537
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-02 02:02:47 +1000

    Create battlepass.lua

---
+++ GameData/Gym League [17450551531]/battlepass.lua
@@ -0,0 +1,490 @@
+data = {
+    ["id"] = 4,
+    ["rewards"] = {
+        {
+            ["Premium"] = {
+                ["value"] = 100,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 50,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 100,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 55,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = "Ghost Storm",
+                ["type"] = "aura",
+                ["name"] = "Ghost Storm",
+                ["icon"] = "rbxassetid://18249414120"
+            },
+            ["Free"] = {
+                ["value"] = 55,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 110,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 60,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 60,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 3,
+                ["type"] = "potion",
+                ["name"] = "Stamina Potion",
+                ["icon"] = "rbxassetid://17654122253"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 120,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 60,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 120,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 60,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 125,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 65,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = "Radiant Sun",
+                ["type"] = "aura",
+                ["name"] = "Radiant Sun",
+                ["icon"] = "rbxassetid://18249413000"
+            },
+            ["Free"] = {
+                ["value"] = 2,
+                ["type"] = "potion",
+                ["name"] = "Pump Potion",
+                ["icon"] = "rbxassetid://17654124000"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 135,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 65,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 135,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 65,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 135,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 3,
+                ["type"] = "potion",
+                ["name"] = "Speed Potion",
+                ["icon"] = "rbxassetid://17654121216"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 135,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 70,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 140,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18249412691"
+            },
+            ["Free"] = {
+                ["value"] = "Sandstorm",
+                ["type"] = "aura",
+                ["name"] = "Sandstorm",
+                ["icon"] = "rbxassetid://18249412691"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 145,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 70,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 150,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = "Goggles2",
+                ["type"] = "gear",
+                ["name"] = "Phelps Goggles",
+                ["icon"] = "rbxassetid://80759696845433"
+            }
+        },
+        {
+            ["Premium"] = {["value"] = 500, ["type"] = "cash", ["name"] = "Cash", ["icon"] = "rbxassetid://18122414391"},
+            ["Free"] = {
+                ["value"] = 70,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 3,
+                ["type"] = "potion",
+                ["name"] = "Power Potion",
+                ["icon"] = "rbxassetid://17654121216"
+            },
+            ["Free"] = {
+                ["value"] = 75,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 160,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 75,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 160,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 75,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 160,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 75,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 160,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 75,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 165,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = "gear",
+                ["type"] = "Mask2",
+                ["name"] = "Hero Mask",
+                ["icon"] = "rbxassetid://118996695194866"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 170,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 75,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = "Sword",
+                ["type"] = "gear",
+                ["name"] = "Chipped Blade",
+                ["icon"] = "rbxassetid://88385453666064"
+            },
+            ["Free"] = {
+                ["value"] = "Visor",
+                ["type"] = "gear",
+                ["name"] = "Hero Visor",
+                ["icon"] = "rbxassetid://115549730933925"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 170,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 2,
+                ["type"] = "potion",
+                ["name"] = "Mega Potion",
+                ["icon"] = "rbxassetid://17654121877"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = "Balaclava",
+                ["type"] = "gear",
+                ["name"] = "Mask",
+                ["icon"] = "rbxassetid://111973956338936"
+            },
+            ["Free"] = {
+                ["value"] = 80,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 3,
+                ["type"] = "potion",
+                ["name"] = "Mega Potion",
+                ["icon"] = "rbxassetid://17654121877"
+            },
+            ["Free"] = {
+                ["value"] = 80,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 175,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 80,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = "Crown",
+                ["type"] = "gear",
+                ["name"] = "Royal Crown",
+                ["icon"] = "rbxassetid://89216534017433"
+            },
+            ["Free"] = {
+                ["value"] = "Cape",
+                ["type"] = "gear",
+                ["name"] = "Hero Cape",
+                ["icon"] = "rbxassetid://115560646139983"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 185,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 85,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 190,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 85,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 195,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 90,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = 200,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            },
+            ["Free"] = {
+                ["value"] = 100,
+                ["type"] = "bottlecaps",
+                ["name"] = "Bottlecaps",
+                ["icon"] = "rbxassetid://18122414391"
+            }
+        },
+        {
+            ["Premium"] = {
+                ["value"] = "Meteor Storm",
+                ["type"] = "aura",
+                ["name"] = "Meteor Storm",
+                ["icon"] = "rbxassetid://18249414368"
+            },
+            ["Free"] = {
+                ["value"] = "Rainbow Rage",
+                ["type"] = "aura",
+                ["name"] = "Rainbow Rage",
+                ["icon"] = "rbxassetid://18249413301"
+            }
+        }
+    }
+}
