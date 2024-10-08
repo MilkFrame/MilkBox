8c4e1b915ac7687d22aeec0ec434c6f04351a272
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 23:02:34 +1100

    Create liveOps.lua

---
+++ GameData/RIOTFALL [7796842481]/liveOps.lua
@@ -0,0 +1,31 @@
+data = {
+    ["events"] = {
+        ["event_albatross"] = {
+            ["begin"] = 1666526400,
+            ["challenges"] = "liveOpsChallengeCategory_albatross",
+            ["shopBundles"] = {"bundle_beta"},
+            ["id"] = "event_albatross",
+            ["finish"] = 1666526400,
+            ["desc"] = "Celebrate the Beta Launch of RIOTFALL with limited time challenges and rewards!",
+            ["displayName"] = "Beta Launch"
+        },
+        ["event_condor"] = {
+            ["begin"] = 1666526400,
+            ["shopBundles"] = {},
+            ["battlepass"] = "battlepass_season1",
+            ["id"] = "event_condor",
+            ["finish"] = 1666526400,
+            ["desc"] = "classified",
+            ["displayName"] = "classified"
+        },
+        ["event_blueJay"] = {
+            ["begin"] = 1666526400,
+            ["shopBundles"] = {},
+            ["id"] = "event_blueJay",
+            ["finish"] = 1666526400,
+            ["desc"] = "classified",
+            ["displayName"] = "classified"
+        }
+    },
+    ["liveOps.spec"] = "function: 0xd796f9c7e8d8ecd5"
+}
