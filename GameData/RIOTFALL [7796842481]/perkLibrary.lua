ca31f5d63147a71fe8b75580228aa46a18f1fbdd
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 23:08:46 +1100

    Create perkLibrary.lua

---
+++ GameData/RIOTFALL [7796842481]/perkLibrary.lua
@@ -0,0 +1,122 @@
+data = {
+    ["perk_hacker"] = {
+        ["image"] = "rbxassetid://10705800235",
+        ["category"] = "weaponCategory_perk2",
+        ["id"] = "perk_hacker",
+        ["imageFull"] = "rbxassetid://10705800086",
+        ["desc"] = "Highlights enemy equipment in proximity. Enemy equipment becomes hackable.",
+        ["displayName"] = "Hacker"
+    },
+    ["perk_endurance"] = {
+        ["image"] = "rbxassetid://10714314021",
+        ["category"] = "weaponCategory_perk1",
+        ["id"] = "perk_endurance",
+        ["imageFull"] = "rbxassetid://10714313778",
+        ["desc"] = "No sprint or tactical sprint cooldown.",
+        ["displayName"] = "Endurance"
+    },
+    ["perk_vulture"] = {
+        ["image"] = "rbxassetid://10714312063",
+        ["category"] = "weaponCategory_perk1",
+        ["id"] = "perk_vulture",
+        ["imageFull"] = "rbxassetid://10714310293",
+        ["desc"] = "Pick up ammo from dropped weapons.",
+        ["displayName"] = "Vulture"
+    },
+    ["perk_ninja"] = {
+        ["image"] = "rbxassetid://10714312519",
+        ["category"] = "weaponCategory_perk1",
+        ["id"] = "perk_ninja",
+        ["imageFull"] = "rbxassetid://10714312325",
+        ["desc"] = "Your footsteps are silent. Your soldier speaks less.",
+        ["displayName"] = "Ninja"
+    },
+    ["perk_ghost"] = {
+        ["image"] = "rbxassetid://10705800566",
+        ["category"] = "weaponCategory_perk2",
+        ["id"] = "perk_ghost",
+        ["imageFull"] = "rbxassetid://10705800411",
+        ["desc"] = "Undetectable by enemy UAV. Invisible to enemy thermal lenses.",
+        ["displayName"] = "Ghost"
+    },
+    ["perk_striker"] = {
+        ["image"] = "rbxassetid://10705799628",
+        ["category"] = "weaponCategory_perk2",
+        ["id"] = "perk_striker",
+        ["imageFull"] = "rbxassetid://10705799370",
+        ["desc"] = "Quicker melee and longer melee initiate range.",
+        ["displayName"] = "Striker"
+    },
+    ["perk_thrifter"] = {
+        ["image"] = "rbxassetid://10705643880",
+        ["category"] = "weaponCategory_perk3",
+        ["id"] = "perk_thrifter",
+        ["imageFull"] = "rbxassetid://10705702660",
+        ["desc"] = "Power Items from streaks cost one point less.",
+        ["displayName"] = "Thrifter"
+    },
+    ["perk_lightweight"] = {
+        ["image"] = "rbxassetid://10714312865",
+        ["category"] = "weaponCategory_perk1",
+        ["id"] = "perk_lightweight",
+        ["imageFull"] = "rbxassetid://10714312703",
+        ["desc"] = "Move 10% quicker.",
+        ["displayName"] = "Lightweight"
+    },
+    ["perk_bastion"] = {
+        ["image"] = "rbxassetid://10705644644",
+        ["category"] = "weaponCategory_perk3",
+        ["id"] = "perk_bastion",
+        ["imageFull"] = "rbxassetid://10705703390",
+        ["desc"] = "Take signficantly less damage from melee.",
+        ["displayName"] = "Bastion"
+    },
+    ["perk_hunter"] = {
+        ["image"] = "rbxassetid://10714313554",
+        ["category"] = "weaponCategory_perk1",
+        ["id"] = "perk_hunter",
+        ["imageFull"] = "rbxassetid://10714313103",
+        ["desc"] = "Can sprint while reloading. Full movement speed while aimed down sight.",
+        ["displayName"] = "Hunter"
+    },
+    ["perk_hardHat"] = {
+        ["image"] = "rbxassetid://10705644230",
+        ["category"] = "weaponCategory_perk3",
+        ["id"] = "perk_hardHat",
+        ["imageFull"] = "rbxassetid://10705702933",
+        ["desc"] = "Take less damage from explosives.",
+        ["displayName"] = "Hard Hat"
+    },
+    ["perk_blink"] = {
+        ["image"] = "rbxassetid://10705644448",
+        ["category"] = "weaponCategory_perk3",
+        ["id"] = "perk_blink",
+        ["imageFull"] = "rbxassetid://10705703192",
+        ["desc"] = "Maximum resistance to flash and stun grenades.",
+        ["displayName"] = "Anti-Flash"
+    },
+    ["perk_incognito"] = {
+        ["image"] = "rbxassetid://10705799914",
+        ["category"] = "weaponCategory_perk2",
+        ["id"] = "perk_incognito",
+        ["imageFull"] = "rbxassetid://10705799787",
+        ["desc"] = "Undetectable by enemy AI-controller Power Items.",
+        ["displayName"] = "Incognito"
+    },
+    ["perk_anchor"] = {
+        ["image"] = "rbxassetid://10705644827",
+        ["category"] = "weaponCategory_perk3",
+        ["id"] = "perk_anchor",
+        ["imageFull"] = "rbxassetid://10705703567",
+        ["desc"] = "Significantly less flinch on incoming damage.",
+        ["displayName"] = "Anchor"
+    },
+    ["perk_fastHands"] = {
+        ["image"] = "rbxassetid://10705800909",
+        ["category"] = "weaponCategory_perk2",
+        ["id"] = "perk_fastHands",
+        ["imageFull"] = "rbxassetid://10705800762",
+        ["desc"] = "Quicker reload and quicker weapon swap.",
+        ["displayName"] = "Fast Hands"
+    }
+}
