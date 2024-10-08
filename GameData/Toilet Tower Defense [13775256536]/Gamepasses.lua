9b1494ee44442003cca116d6c0a5bdec3314acc3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 22:22:04 +1100

    Create Gamepasses.lua

---
+++ GameData/Toilet Tower Defense [13775256536]/Gamepasses.lua
@@ -0,0 +1,9 @@
+data = {
+    ["2xDrills"] = {["RobloxGamepassIds"] = {930624309}, ["DisplayName"] = "2x Drills"},
+    ["DoubleCoins"] = {["RobloxGamepassIds"] = {208620375}, ["DisplayName"] = "Double Coins"},
+    ["AprilFools_InfiniteUnits"] = {["RobloxGamepassIds"] = {767749542}, ["DisplayName"] = "Infinite Units"},
+    ["1000ExtraInvSpace"] = {["RobloxGamepassIds"] = {646799606}, ["DisplayName"] = "+1000 Inventory Space"},
+    ["ClanCreator"] = {["RobloxGamepassIds"] = {897195671}, ["DisplayName"] = "Clan Creator"},
+    ["Lucky"] = {["RobloxGamepassIds"] = {208619622}, ["DisplayName"] = "Lucky"},
+    ["VIP"] = {["RobloxGamepassIds"] = {257410325}, ["DisplayName"] = "VIP"}
+}
