d8da1093ac6c58b18809736620839158647ed7ed
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 22:27:29 +1100

    Create PlaytimeRewards.lua

---
+++ GameData/Toilet Tower Defense [13775256536]/PlaytimeRewards.lua
@@ -0,0 +1,60 @@
+data = {
+    {
+        ["rewards"] = {{["CurrencyAmount"] = 50, ["ItemType"] = "Currency", ["CurrencyType"] = "Coins"}},
+        ["timeNeeded"] = 300
+    },
+    {
+        ["rewards"] = {{["CurrencyAmount"] = 5, ["ItemType"] = "Currency", ["CurrencyType"] = "Gems"}},
+        ["timeNeeded"] = 600
+    },
+    {
+        ["rewards"] = {
+            {["Amount"] = 1, ["ItemType"] = "Boosts", ["BoostId"] = "2xLuckBoost"},
+            {["CurrencyAmount"] = 50, ["ItemType"] = "Currency", ["CurrencyType"] = "Coins"}
+        },
+        ["timeNeeded"] = 1800
+    },
+    {
+        ["rewards"] = {
+            {["Amount"] = 1, ["ItemType"] = "Crates", ["ItemId"] = "ActivityCrate"},
+            {["CurrencyAmount"] = 100, ["ItemType"] = "Currency", ["CurrencyType"] = "Coins"}
+        },
+        ["timeNeeded"] = 3600
+    },
+    {
+        ["rewards"] = {
+            {["Amount"] = 1, ["ItemType"] = "Boosts", ["BoostId"] = "2xCoinsBoost"},
+            {["CurrencyAmount"] = 200, ["ItemType"] = "Currency", ["CurrencyType"] = "Coins"}
+        },
+        ["timeNeeded"] = 7200
+    },
+    {
+        ["rewards"] = {
+            {["Amount"] = 2, ["ItemType"] = "Crates", ["ItemId"] = "ActivityCrate"},
+            {["CurrencyAmount"] = 300, ["ItemType"] = "Currency", ["CurrencyType"] = "Coins"}
+        },
+        ["timeNeeded"] = 10800
+    },
+    {
+        ["rewards"] = {
+            {["Amount"] = 2, ["ItemType"] = "Crates", ["ItemId"] = "ActivityCrate"},
+            {["Amount"] = 1, ["ItemType"] = "Boosts", ["BoostId"] = "2xXPBoost"}
+        },
+        ["timeNeeded"] = 14400
+    },
+    {
+        ["rewards"] = {
+            {["Amount"] = 2, ["ItemType"] = "Crates", ["ItemId"] = "ActivityCrate"},
+            {["Amount"] = 1, ["ItemType"] = "Boosts", ["BoostId"] = "2xLuckBoost"},
+            {["Amount"] = 1, ["ItemType"] = "Boosts", ["BoostId"] = "2xCoinsBoost"}
+        },
+        ["timeNeeded"] = 18000
+    },
+    {
+        ["rewards"] = {
+            {["Amount"] = 5, ["ItemType"] = "Crates", ["ItemId"] = "ActivityCrate"},
+            {["CurrencyAmount"] = 25, ["ItemType"] = "Currency", ["CurrencyType"] = "Gems"}
+        },
+        ["timeNeeded"] = 21600
+    }
+}
