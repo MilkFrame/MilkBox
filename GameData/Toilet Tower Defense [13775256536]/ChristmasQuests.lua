9f295627181d3d26b8c059f51f147c22713fe18a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 22:32:28 +1100

    Create ChristmasQuests.lua

---
+++ GameData/Toilet Tower Defense [13775256536]/ChristmasQuests.lua
@@ -0,0 +1,152 @@
+data = {
+    ["PlayForHours"] = {
+        ["Steps"] = {
+            {
+                ["Difficulty"] = "Easy",
+                ["Requirement"] = {"PlayMinute", 30},
+                ["Rewards"] = {{"XP", 500}, {"Presents", 50}}
+            },
+            {
+                ["Difficulty"] = "Medium",
+                ["Requirement"] = {"PlayMinute", 60},
+                ["Rewards"] = {{"XP", 1000}, {"Presents", 100}}
+            },
+            {
+                ["Difficulty"] = "Hard",
+                ["Requirement"] = {"PlayMinute", 120},
+                ["Rewards"] = {{"XP", 2000}, {"Presents", 200}}
+            },
+            {
+                ["Difficulty"] = "Extreme",
+                ["Requirement"] = {"PlayMinute", 240},
+                ["Rewards"] = {{"XP", 3000}, {"Presents", 300}}
+            },
+            {
+                ["Difficulty"] = "Challenging",
+                ["Requirement"] = {"PlayMinute", 480},
+                ["Rewards"] = {{"XP", 5000}, {"Presents", 500}}
+            }
+        },
+        ["DisplayName"] = "function: 0x00000000149713bf"
+    },
+    ["OpenCrates"] = {
+        ["Steps"] = {
+            {
+                ["Difficulty"] = "Easy",
+                ["Requirement"] = {"OpenSantaCrate", 1},
+                ["Rewards"] = {{"XP", 500}, {"Presents", 50}}
+            },
+            {
+                ["Difficulty"] = "Medium",
+                ["Requirement"] = {"OpenSantaCrate", 5},
+                ["Rewards"] = {{"XP", 1000}, {"Presents", 100}}
+            },
+            {
+                ["Difficulty"] = "Hard",
+                ["Requirement"] = {"OpenSantaCrate", 10},
+                ["Rewards"] = {{"XP", 2000}, {"Presents", 200}}
+            },
+            {
+                ["Difficulty"] = "Extreme",
+                ["Requirement"] = {"OpenSantaCrate", 25},
+                ["Rewards"] = {{"XP", 3000}, {"Presents", 300}}
+            },
+            {
+                ["Difficulty"] = "Challenging",
+                ["Requirement"] = {"OpenSantaCrate", 50},
+                ["Rewards"] = {{"XP", 5000}, {"Presents", 500}}
+            }
+        },
+        ["DisplayName"] = "function: 0x00000000818c50df"
+    },
+    ["KillToilets"] = {
+        ["Steps"] = {
+            {
+                ["Difficulty"] = "Easy",
+                ["Requirement"] = {"KillToilets", 500},
+                ["Rewards"] = {{"XP", 500}, {"Presents", 50}}
+            },
+            {
+                ["Difficulty"] = "Medium",
+                ["Requirement"] = {"KillToilets", 1000},
+                ["Rewards"] = {{"XP", 1000}, {"Presents", 100}}
+            },
+            {
+                ["Difficulty"] = "Hard",
+                ["Requirement"] = {"KillToilets", 2500},
+                ["Rewards"] = {{"XP", 2000}, {"Presents", 200}}
+            },
+            {
+                ["Difficulty"] = "Extreme",
+                ["Requirement"] = {"KillToilets", 5000},
+                ["Rewards"] = {{"XP", 3000}, {"Presents", 300}}
+            },
+            {
+                ["Difficulty"] = "Challenging",
+                ["Requirement"] = {"KillToilets", 10000},
+                ["Rewards"] = {{"XP", 5000}, {"Presents", 500}}
+            }
+        },
+        ["DisplayName"] = "function: 0x000000007e33dd27"
+    },
+    ["CollectPresents"] = {
+        ["Steps"] = {
+            {
+                ["Difficulty"] = "Easy",
+                ["Requirement"] = {"CollectPresent", 500},
+                ["Rewards"] = {{"XP", 500}, {"Presents", 50}}
+            },
+            {
+                ["Difficulty"] = "Medium",
+                ["Requirement"] = {"CollectPresent", 1000},
+                ["Rewards"] = {{"XP", 1000}, {"Presents", 100}}
+            },
+            {
+                ["Difficulty"] = "Hard",
+                ["Requirement"] = {"CollectPresent", 5000},
+                ["Rewards"] = {{"XP", 2000}, {"Presents", 200}}
+            },
+            {
+                ["Difficulty"] = "Extreme",
+                ["Requirement"] = {"CollectPresent", 10000},
+                ["Rewards"] = {{"XP", 3000}, {"Presents", 300}}
+            },
+            {
+                ["Difficulty"] = "Challenging",
+                ["Requirement"] = {"CollectPresent", 50000},
+                ["Rewards"] = {{"XP", 5000}, {"Presents", 500}}
+            }
+        },
+        ["DisplayName"] = "function: 0x00000000fe4a0a57"
+    },
+    ["BeatWaves"] = {
+        ["Steps"] = {
+            {
+                ["Difficulty"] = "Easy",
+                ["Requirement"] = {"BeatWave10", 1},
+                ["Rewards"] = {{"XP", 500}, {"Presents", 50}}
+            },
+            {
+                ["Difficulty"] = "Medium",
+                ["Requirement"] = {"BeatWave20", 1},
+                ["Rewards"] = {{"XP", 1000}, {"Presents", 100}}
+            },
+            {
+                ["Difficulty"] = "Hard",
+                ["Requirement"] = {"BeatWave30", 1},
+                ["Rewards"] = {{"XP", 2000}, {"Presents", 200}}
+            },
+            {
+                ["Difficulty"] = "Extreme",
+                ["Requirement"] = {"BeatWave40", 1},
+                ["Rewards"] = {{"XP", 3000}, {"Presents", 300}}
+            },
+            {
+                ["Difficulty"] = "Challenging",
+                ["Requirement"] = {"BeatWave50", 1},
+                ["Rewards"] = {{"XP", 5000}, {"Presents", 500}}
+            }
+        },
+        ["DisplayName"] = "function: 0x00000000902ca70f"
+    }
+}
