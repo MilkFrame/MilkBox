914036e4bfc516c991bb92787e363acafcfc11a8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 22:25:22 +1100

    Create PencilEventQuests.lua

---
+++ GameData/Toilet Tower Defense [13775256536]/PencilEventQuests.lua
@@ -0,0 +1,79 @@
+data = {
+    ["Solo"] = {
+        ["Steps"] = {{["Difficulty"] = "Easy", ["Requirement"] = {"Solo", 1}, ["Rewards"] = {{"XP", 400}}}},
+        ["DisplayName"] = "function: 0xcb52463f9fa24016"
+    },
+    ["Party"] = {
+        ["Steps"] = {{["Difficulty"] = "Easy", ["Requirement"] = {"Party", 1}, ["Rewards"] = {{"XP", 500}}}},
+        ["DisplayName"] = "function: 0x6819bfcde1193856"
+    },
+    ["PencilsOnly"] = {
+        ["Steps"] = {{["Difficulty"] = "Easy", ["Requirement"] = {"PencilsOnly", 1}, ["Rewards"] = {{"XP", 500}}}},
+        ["DisplayName"] = "function: 0x811c1b6686fc3616"
+    },
+    ["Duo"] = {
+        ["Steps"] = {{["Difficulty"] = "Easy", ["Requirement"] = {"Duo", 1}, ["Rewards"] = {{"XP", 500}}}},
+        ["DisplayName"] = "function: 0xbe1f671f3c114cd6"
+    },
+    ["OneUnit"] = {
+        ["Steps"] = {{["Difficulty"] = "Easy", ["Requirement"] = {"OneUnit", 1}, ["Rewards"] = {{"XP", 500}}}},
+        ["DisplayName"] = "function: 0x531ded3446b19f96"
+    },
+    ["MythicsOnly"] = {
+        ["Steps"] = {{["Difficulty"] = "Easy", ["Requirement"] = {"MythicsOnly", 1}, ["Rewards"] = {{"XP", 500}}}},
+        ["DisplayName"] = "function: 0xc41aa972696c5356"
+    },
+    ["SkibidiSigma"] = {
+        ["Steps"] = {{["Difficulty"] = "Easy", ["Requirement"] = {"SkibidiSigma", 1}, ["Rewards"] = {{"XP", 400}}}},
+        ["DisplayName"] = "function: 0x20569ae936e09e56"
+    },
+    ["SpeedRunner"] = {
+        ["Steps"] = {
+            {["Difficulty"] = "Easy", ["Requirement"] = {"BeatUnderMinutes", 20}, ["Rewards"] = {{"XP", 500}}},
+            {["Difficulty"] = "Medium", ["Requirement"] = {"BeatUnderMinutes", 19}, ["Rewards"] = {{"XP", 600}}},
+            {["Difficulty"] = "Hard", ["Requirement"] = {"BeatUnderMinutes", 18}, ["Rewards"] = {{"XP", 750}}},
+            {["Difficulty"] = "Extreme", ["Requirement"] = {"BeatUnderMinutes", 17}, ["Rewards"] = {{"XP", 1000}}}
+        },
+        ["DisplayName"] = "function: 0x56cb0266f02536d6"
+    },
+    ["Friends"] = {
+        ["Steps"] = {{["Difficulty"] = "Easy", ["Requirement"] = {"Friends", 1}, ["Rewards"] = {{"XP", 600}}}},
+        ["DisplayName"] = "function: 0x7d2335d37b697916"
+    },
+    ["ExclusivesOnly"] = {
+        ["Steps"] = {{["Difficulty"] = "Easy", ["Requirement"] = {"ExclusivesOnly", 1}, ["Rewards"] = {{"XP", 500}}}},
+        ["DisplayName"] = "function: 0x08d77e8a4da56c96"
+    },
+    ["KillToilets"] = {
+        ["Steps"] = {
+            {["Difficulty"] = "Easy", ["Requirement"] = {"KillToilets", 1000}, ["Rewards"] = {{"XP", 400}}},
+            {["Difficulty"] = "Medium", ["Requirement"] = {"KillToilets", 5000}, ["Rewards"] = {{"XP", 750}}},
+            {["Difficulty"] = "Hard", ["Requirement"] = {"KillToilets", 10000}, ["Rewards"] = {{"XP", 1000}}}
+        },
+        ["DisplayName"] = "function: 0x974fd65a34b29f16"
+    },
+    ["PlayMinute"] = {
+        ["Steps"] = {
+            {["Difficulty"] = "Easy", ["Requirement"] = {"PlayMinute", 10}, ["Rewards"] = {{"XP", 400}}},
+            {["Difficulty"] = "Medium", ["Requirement"] = {"PlayMinute", 30}, ["Rewards"] = {{"XP", 500}}},
+            {["Difficulty"] = "Hard", ["Requirement"] = {"PlayMinute", 60}, ["Rewards"] = {{"XP", 750}}},
+            {["Difficulty"] = "Extreme", ["Requirement"] = {"PlayMinute", 120}, ["Rewards"] = {{"XP", 1000}}}
+        },
+        ["DisplayName"] = "function: 0x30c23c1606facbf6"
+    },
+    ["CloseCall"] = {
+        ["Steps"] = {{["Difficulty"] = "Easy", ["Requirement"] = {"CloseCall", 1}, ["Rewards"] = {{"XP", 500}}}},
+        ["DisplayName"] = "function: 0x13e411b49a2665d6"
+    },
+    ["WinMatches"] = {
+        ["Steps"] = {
+            {["Difficulty"] = "Easy", ["Requirement"] = {"WinMatches", 1}, ["Rewards"] = {{"XP", 400}}},
+            {["Difficulty"] = "Medium", ["Requirement"] = {"WinMatches", 2}, ["Rewards"] = {{"XP", 500}}},
+            {["Difficulty"] = "Hard", ["Requirement"] = {"WinMatches", 5}, ["Rewards"] = {{"XP", 600}}},
+            {["Difficulty"] = "Extreme", ["Requirement"] = {"WinMatches", 8}, ["Rewards"] = {{"XP", 750}}},
+            {["Difficulty"] = "Challenging", ["Requirement"] = {"WinMatches", 10}, ["Rewards"] = {{"XP", 1000}}},
+            {["Difficulty"] = "Challenging", ["Requirement"] = {"WinMatches", 15}, ["Rewards"] = {{"XP", 2000}}}
+        },
+        ["DisplayName"] = "function: 0xf74d9099d916edb6"
+    }
+}
