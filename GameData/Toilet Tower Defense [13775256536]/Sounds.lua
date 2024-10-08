8254abafe07646c96c7f568c0cd5e7b2bfd9111e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 22:33:42 +1100

    Create Sounds.lua

---
+++ GameData/Toilet Tower Defense [13775256536]/Sounds.lua
@@ -0,0 +1,129 @@
+data = {
+    ["AdminTools"] = {
+        ["Sword"] = {
+            ["Handle"] = {
+                ["SwordLunge"] = "http://www.roblox.com/asset/?id=12222208",
+                ["Unsheath"] = "http://www.roblox.com/asset/?id=12222225",
+                ["SwordSlash"] = "http://www.roblox.com/asset/?id=12222216"
+            }
+        }
+    },
+    ["FrameworkDependencies"] = {
+        ["f677e310"] = {
+            ["BackgroundMusic"] = {
+                ["CameramanHQMusic"] = "rbxassetid://14145623221",
+                ["ToiletHQMusic"] = "rbxassetid://1847693096",
+                ["ToiletLabMusic"] = "rbxassetid://1837842012",
+                ["LobbyMusic"] = "rbxassetid://5409360995",
+                ["DesertMusic"] = "rbxassetid://7029031068",
+                ["ToiletCityMusic"] = "rbxassetid://7023635858",
+                ["NorthPoleMusic"] = "rbxassetid://1838667039",
+                ["PalmParadiseMusic"] = "rbxassetid://1848227887",
+                ["ToiletFortressMusic"] = "rbxassetid://9044582869",
+                ["PumpkinPatchMusic"] = "rbxassetid://1836009626",
+                ["TurkeyTownMusic"] = "rbxassetid://1836010387",
+                ["GraveyardMusic"] = "rbxassetid://1841601281"
+            },
+            ["TroopDatas"] = {
+                ["Christmas2023"] = {["SantaTVMan"] = {["Sound"] = "rbxassetid://1583819337"}},
+                ["Others"] = {
+                    ["SpeakerHelicopter"] = {["Sound"] = "rbxassetid://4212900131"},
+                    ["UpgradedTitanSpeakerman"] = {
+                        ["ShoulderGunSound"] = "rbxassetid://1019401548",
+                        ["AOESound"] = "rbxassetid://1843858128"
+                    },
+                    ["LargeTurkeySpeakerman"] = {
+                        ["AOESound"] = "rbxassetid://1843858128",
+                        ["TurkeySound"] = "rbxassetid://188831155"
+                    },
+                    ["TitanSpeakerman"] = {["Sound"] = "rbxassetid://1019401548"},
+                    ["TitanTVMan"] = {["Sound"] = "rbxassetid://1843858128"},
+                    ["PumpkinFarmer"] = {["Sound"] = "rbxassetid://1583819337"},
+                    ["EngineerCameraman"] = {["Sound"] = "rbxassetid://1583819337"},
+                    ["DarkSpeakerman"] = {["Sound"] = "rbxassetid://146163522"},
+                    ["NinjaCameraman"] = {["Sound"] = "rbxassetid://9118730507"},
+                    ["MonsterSpeakerman"] = {["Sound"] = "rbxassetid://4212900131"},
+                    ["Camerawoman"] = {["Sound"] = "rbxassetid://9119298913"},
+                    ["JetpackCameraman"] = {["Sound"] = "rbxassetid://9118730507"},
+                    ["CorruptedCameraman"] = {
+                        ["PunchSound"] = "rbxassetid://146163522",
+                        ["ShootSound"] = "rbxassetid://1583819337"
+                    },
+                    ["TVWoman"] = {["Sound"] = "rbxassetid://9112780462"},
+                    ["CameraSpider"] = {
+                        ["MinigunSound"] = "rbxassetid://341294387",
+                        ["LaserSound"] = "rbxassetid://9125767444"
+                    },
+                    ["ScientistCameraman"] = {["Sound"] = "rbxassetid://1583819337"},
+                    ["ScarySpeakerman"] = {
+                        ["PunchSound"] = "rbxassetid://146163522",
+                        ["AOESound"] = "rbxassetid://1843858128"
+                    },
+                    ["DarkCamerawoman"] = {["Sound"] = "rbxassetid://6479631005"},
+                    ["RocketCameraman"] = {["Sound"] = "rbxassetid://146163522"},
+                    ["ChefTVMan"] = {["ThrowSound"] = "rbxassetid://4622060212"},
+                    ["TitanCinemaman"] = {
+                        ["PunchSound"] = "rbxassetid://146163522",
+                        ["AOESound"] = "rbxassetid://1843858128"
+                    },
+                    ["LargeTVMan"] = {["Sound"] = "rbxassetid://4212900131"},
+                    ["CarSpeakerman"] = {["Sound"] = "rbxassetid://146163522"},
+                    ["TVMan"] = {["Sound"] = "rbxassetid://4212900131"},
+                    ["DancingSpeakerwoman"] = {["Sound"] = "rbxassetid://9113487449"},
+                    ["CameraHelicopter"] = {["Sound"] = "rbxassetid://4622060212"},
+                    ["LaserCameramanCar"] = {
+                        ["PunchSound"] = "rbxassetid://146163522",
+                        ["LaserSound"] = "rbxassetid://9125767444"
+                    },
+                    ["GlitchCameraman"] = {["Sound"] = "rbxassetid://146163522"},
+                    ["Speakerman"] = {["Sound"] = "rbxassetid://146163522"},
+                    ["SpeakerSpider"] = {["AOESound"] = "rbxassetid://1843858128"},
+                    ["SinisterCameraman"] = {["Sound"] = "rbxassetid://146163522"},
+                    ["BrownSuitCameraman"] = {["Sound"] = "rbxassetid://1583819337"},
+                    ["TurkeySpeakerman"] = {["TurkeySound"] = "rbxassetid://188831155"},
+                    ["ShieldCameraman"] = {
+                        ["NormalShieldBlock"] = "rbxassetid://9116519870",
+                        ["ForcefieldShieldBlock"] = "rbxassetid://1898322396",
+                        ["NormalShieldHit"] = "rbxassetid://9116669373",
+                        ["ForcefieldShieldHit"] = "rbxassetid://1624609598"
+                    },
+                    ["MechCameraman"] = {
+                        ["MinigunSound"] = "rbxassetid://341294387",
+                        ["TaserSound"] = "rbxassetid://7405488042",
+                        ["RailgunSound"] = "rbxassetid://6479631005",
+                        ["PunchSound"] = "rbxassetid://146163522",
+                        ["OrbsSound"] = "rbxassetid://1019401548"
+                    },
+                    ["SecretAgent"] = {["Sound"] = "rbxassetid://1283290053"},
+                    ["SpiderTV"] = {["AOESound"] = "rbxassetid://1843858128"},
+                    ["TurkeyCameraman"] = {["TurkeySound"] = "rbxassetid://188831155"},
+                    ["LargeScientistCameraman"] = {["Sound"] = "rbxassetid://4560467724"},
+                    ["CameraDrone"] = {["Sound"] = "rbxassetid://146163522"},
+                    ["JetpackSpeakerman"] = {["Sound"] = "rbxassetid://9118730507"},
+                    ["MedicCameraman"] = {["Sound"] = "rbxassetid://146163522"},
+                    ["TitanCameraman"] = {["Sound"] = "rbxassetid://9125767444"},
+                    ["UpgradedTitanCinemaman"] = {
+                        ["LaserSound"] = "rbxassetid://9125767444",
+                        ["SwordSlashSound"] = "rbxassetid://935843979",
+                        ["AOESound"] = "rbxassetid://1843858128"
+                    },
+                    ["ScientistTVMan"] = {["Sound"] = "rbxassetid://1583819337"},
+                    ["LargeSpeakerman"] = {["Sound"] = "rbxassetid://146163522"},
+                    ["CameraAttackHelicopter"] = {["Sound"] = "rbxassetid://146163522"},
+                    ["LargeCameraman"] = {["Sound"] = "rbxassetid://9113193315"},
+                    ["Cameraman"] = {["Sound"] = "rbxassetid://1583819337"}
+                },
+                ["Episode70Part1"] = {["DJSpeakerman"] = {["Sound"] = "rbxassetid://1583819337"}}
+            }
+        },
+        ["e53824e7"] = {
+            ["CustomExplosion"] = {
+                ["Templates"] = {
+                    ["SnowballExplosion"] = {["SnowballHitSound"] = "rbxassetid://9119310917"},
+                    ["Explosion"] = {["ExplosionSound"] = "rbxassetid://6654283042"},
+                    ["ToxicExplosion"] = {["ExplosionSound"] = "rbxassetid://6654283042"}
+                }
+            }
+        }
+    }
+}
