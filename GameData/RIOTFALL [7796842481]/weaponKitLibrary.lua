9fdc801cbc80a1c5468bc71a399f1b01f50a0361
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 23:09:01 +1100

    Create weaponKitLibrary.lua

---
+++ GameData/RIOTFALL [7796842481]/weaponKitLibrary.lua
@@ -0,0 +1,170 @@
+data = {
+    ["weaponKit_ak74GeminiMoon"] = {
+        ["connections"] = {},
+        ["rarity"] = "rarity_epic",
+        ["cosmeticCategory"] = "cosmeticCategory_weaponKit",
+        ["id"] = "weaponKit_ak74GeminiMoon",
+        ["displayName"] = "Gemini Moon",
+        ["image"] = "rbxassetid://13532614475",
+        ["bundleSource"] = "bundle_gemini",
+        ["baseId"] = "weapon_ak74",
+        ["desc"] = ""
+    },
+    ["weaponKit_pp2000Katana"] = {
+        ["connections"] = {},
+        ["rarity"] = "rarity_epic",
+        ["cosmeticCategory"] = "cosmeticCategory_weaponKit",
+        ["id"] = "weaponKit_pp2000Katana",
+        ["displayName"] = "Katana Queen",
+        ["image"] = "rbxassetid://13830255041",
+        ["bundleSource"] = "bundle_katanaQueen",
+        ["includedAttachments"] = {
+            ["attachmentCategory_optic"] = {["id"] = "attachment_xpsHoloSight", ["data"] = {}},
+            ["attachmentCategory_muzzle"] = {["id"] = "attachment_ribbonSilencer", ["data"] = {}}
+        },
+        ["baseId"] = "weapon_pp2000",
+        ["desc"] = ""
+    },
+    ["weaponKit_glockCarbineKit"] = {
+        ["stats"] = {},
+        ["rarity"] = "rarity_epic",
+        ["cosmeticCategory"] = "cosmeticCategory_weaponKit",
+        ["id"] = "weaponKit_glockCarbineKit",
+        ["hidden"] = true,
+        ["displayName"] = "Carbine Kit",
+        ["shadowCollection"] = true,
+        ["connections"] = {
+            ["attachmentCategory_underbarrel"] = {
+                ["connectMaxSize"] = 3,
+                ["connectSystem"] = "attachmentConnectSystem_picatinny"
+            },
+            ["attachmentCategory_magazine"] = {},
+            ["attachmentCategory_muzzle"] = {},
+            ["attachmentCategory_perk"] = {},
+            ["attachmentCategory_fireMode"] = {},
+            ["attachmentCategory_sidebarrel"] = {
+                ["connectMaxSize"] = 4,
+                ["connectSystem"] = "attachmentConnectSystem_picatinny"
+            },
+            ["attachmentCategory_optic"] = {
+                ["connectMaxSize"] = 21,
+                ["connectSystem"] = "attachmentConnectSystem_picatinny"
+            }
+        },
+        ["bonusStats"] = {["weaponCategories"] = {}, ["weapons"] = {}},
+        ["baseId"] = "weapon_g17",
+        ["desc"] = ""
+    },
+    ["weaponKit_mp412Sheriff"] = {
+        ["connections"] = {},
+        ["rarity"] = "rarity_legendary",
+        ["cosmeticCategory"] = "cosmeticCategory_weaponKit",
+        ["id"] = "weaponKit_mp412Sheriff",
+        ["displayName"] = "Sheriff",
+        ["image"] = "rbxassetid://13703176749",
+        ["bundleSource"] = "bundle_western",
+        ["baseId"] = "weapon_mp412",
+        ["desc"] = ""
+    },
+    ["weaponKit_none"] = {
+        ["connections"] = {},
+        ["rarity"] = "rarity_base",
+        ["cosmeticCategory"] = "cosmeticCategory_weaponKit",
+        ["id"] = "weaponKit_none",
+        ["displayName"] = "None",
+        ["image"] = "rbxassetid://10790682440",
+        ["shadowCollection"] = true,
+        ["baseId"] = "consumable_missing",
+        ["desc"] = ""
+    },
+    ["weaponKit_goldenHatchet"] = {
+        ["connections"] = {},
+        ["rarity"] = "rarity_legendary",
+        ["cosmeticCategory"] = "cosmeticCategory_weaponKit",
+        ["id"] = "weaponKit_goldenHatchet",
+        ["hidden"] = true,
+        ["displayName"] = "Golden Hatchet",
+        ["shadowCollection"] = true,
+        ["baseId"] = "equipment_throwingAxe",
+        ["desc"] = ""
+    },
+    ["weaponKit_m300Medusa"] = {
+        ["connections"] = {},
+        ["rarity"] = "rarity_epic",
+        ["cosmeticCategory"] = "cosmeticCategory_weaponKit",
+        ["id"] = "weaponKit_m300Medusa",
+        ["displayName"] = "Medusa",
+        ["image"] = "rbxassetid://13480579283",
+        ["bundleSource"] = "bundle_medusa",
+        ["baseId"] = "weapon_m300",
+        ["desc"] = ""
+    },
+    ["weaponKit_ak74GeminiSun"] = {
+        ["connections"] = {},
+        ["rarity"] = "rarity_epic",
+        ["cosmeticCategory"] = "cosmeticCategory_weaponKit",
+        ["id"] = "weaponKit_ak74GeminiSun",
+        ["displayName"] = "Gemini Sun",
+        ["image"] = "rbxassetid://13532614403",
+        ["bundleSource"] = "bundle_gemini",
+        ["baseId"] = "weapon_ak74",
+        ["desc"] = ""
+    },
+    ["weaponKit_ump45Outlaw"] = {
+        ["connections"] = {},
+        ["rarity"] = "rarity_epic",
+        ["cosmeticCategory"] = "cosmeticCategory_weaponKit",
+        ["id"] = "weaponKit_ump45Outlaw",
+        ["displayName"] = "Outlaw",
+        ["image"] = "rbxassetid://13703173164",
+        ["bundleSource"] = "bundle_western",
+        ["baseId"] = "weapon_ump45",
+        ["desc"] = ""
+    },
+    ["weaponKit_g17GoldenSnake"] = {
+        ["connections"] = {},
+        ["rarity"] = "rarity_legendary",
+        ["cosmeticCategory"] = "cosmeticCategory_weaponKit",
+        ["id"] = "weaponKit_g17GoldenSnake",
+        ["displayName"] = "Golden Viper",
+        ["image"] = "rbxassetid://13241766068",
+        ["bundleSource"] = "bundle_goldenSnake",
+        ["baseId"] = "weapon_g17",
+        ["desc"] = ""
+    },
+    ["weaponKit_m4a1GoldenSnake"] = {
+        ["connections"] = {},
+        ["rarity"] = "rarity_legendary",
+        ["cosmeticCategory"] = "cosmeticCategory_weaponKit",
+        ["id"] = "weaponKit_m4a1GoldenSnake",
+        ["displayName"] = "Golden Viper",
+        ["image"] = "rbxassetid://13241765909",
+        ["bundleSource"] = "bundle_goldenSnake",
+        ["baseId"] = "weapon_m4a1",
+        ["desc"] = ""
+    },
+    ["weaponKit_heroGlock"] = {
+        ["connections"] = {
+            ["attachmentCategory_underbarrel"] = {
+                ["connectSize"] = 6,
+                ["connectSystem"] = "attachmentConnectSystem_picatinny"
+            },
+            ["attachmentCategory_magazine"] = {},
+            ["attachmentCategory_muzzle"] = {},
+            ["attachmentCategory_perk"] = {},
+            ["attachmentCategory_fireMode"] = {},
+            ["attachmentCategory_optic"] = {
+                ["connectSize"] = 6,
+                ["connectSystem"] = "attachmentConnectSystem_picatinny"
+            }
+        },
+        ["rarity"] = "rarity_legendary",
+        ["cosmeticCategory"] = "cosmeticCategory_weaponKit",
+        ["id"] = "weaponKit_heroGlock",
+        ["hidden"] = true,
+        ["displayName"] = "Hero Glock",
+        ["shadowCollection"] = true,
+        ["baseId"] = "weapon_g17",
+        ["desc"] = ""
+    }
+}
