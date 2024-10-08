26dd42ac4419408a41a3b5e10b5b016ecfc1e23c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 22:32:58 +1100

    Create SettingsData.lua

---
+++ GameData/Toilet Tower Defense [13775256536]/SettingsData.lua
@@ -0,0 +1,46 @@
+data = {
+    ["GiftedNotifications"] = {
+        ["SettingMode"] = "Switch",
+        ["Order"] = 5,
+        ["Default"] = true,
+        ["Icon"] = "rbxassetid://15850106739",
+        ["DisplayName"] = "Gift Notifications"
+    },
+    ["SoundEffects"] = {
+        ["SettingMode"] = "Switch",
+        ["Order"] = 2,
+        ["Default"] = true,
+        ["Icon"] = "rbxassetid://14296239236",
+        ["DisplayName"] = "SFX"
+    },
+    ["BossFight"] = {
+        ["SettingMode"] = "Switch",
+        ["Order"] = 6,
+        ["Default"] = true,
+        ["Icon"] = "rbxassetid://15956321744",
+        ["DisplayName"] = "Boss Healthbar"
+    },
+    ["Music"] = {
+        ["SettingMode"] = "Switch",
+        ["Order"] = 1,
+        ["Default"] = true,
+        ["Icon"] = "rbxassetid://14296205091",
+        ["DisplayName"] = "Music"
+    },
+    ["LowGFX"] = {
+        ["SettingMode"] = "Switch",
+        ["Order"] = 4,
+        ["Default"] = false,
+        ["Icon"] = "rbxassetid://15460929343",
+        ["DisplayName"] = "Low GFX"
+    },
+    ["Trading"] = {
+        ["MaxNum"] = 3,
+        ["SettingMode"] = "TradeSetting",
+        ["Default"] = 1,
+        ["Order"] = 3,
+        ["MinNum"] = 1,
+        ["Icon"] = "rbxassetid://14581473133",
+        ["DisplayName"] = "Trade Requests"
+    }
+}
