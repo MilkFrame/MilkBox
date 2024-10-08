a25b1a67d9162048f7b06da8211b098a5131f95a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 23:09:14 +1100

    Create clanData.lua

---
+++ GameData/RIOTFALL [7796842481]/clanData.lua
@@ -0,0 +1,321 @@
+data = {
+    ["clanIncomingInvites"] = {
+        {
+            ["clanIcon"] = 10511855986,
+            ["weekPoints"] = 30300,
+            ["owner"] = 48485959,
+            ["members"] = {
+                {
+                    ["playerId"] = 48485959,
+                    ["playerBanner"] = "banner_malfunction",
+                    ["playerName"] = "kevin",
+                    ["playerPrestige"] = 3,
+                    ["allTimePoints"] = 200003,
+                    ["playerSoldierData"] = {},
+                    ["weekPoints"] = 39120,
+                    ["playerLevel"] = 34,
+                    ["playerEmblem"] = "emblem_knockoutShrimp"
+                }
+            },
+            ["clanTag"] = "swag",
+            ["clanName"] = "The Metaverse",
+            ["clanShout"] = "Welcome to the metaverse",
+            ["allTimePoints"] = 222050,
+            ["clanId"] = "clan_2",
+            ["clanBanner"] = 10897852997,
+            ["admins"] = {},
+            ["mods"] = {},
+            ["foundedTs"] = 1728382314
+        }
+    },
+    ["topClansWeek"] = {
+        {
+            ["clanIcon"] = 10590477428,
+            ["shoutPlayerId"] = 145345,
+            ["clanName"] = "WE DEM BOYS",
+            ["clanShout"] = "Yeah we dem boys. Yeah we make da noise",
+            ["mods"] = {},
+            ["admins"] = {},
+            ["owner"] = 145345,
+            ["members"] = {
+                {
+                    ["playerId"] = 145345,
+                    ["playerBanner"] = "banner_bulletStorm",
+                    ["playerName"] = "pablito",
+                    ["playerPrestige"] = 2,
+                    ["allTimePoints"] = 50003,
+                    ["playerSoldierData"] = {},
+                    ["weekPoints"] = 34000,
+                    ["playerLevel"] = 17,
+                    ["playerEmblem"] = "emblem_commando"
+                }
+            },
+            ["clanTag"] = "DEM",
+            ["shoutTs"] = 1728342314,
+            ["shoutPlayerName"] = "pablito",
+            ["weekPoints"] = 50000,
+            ["clanId"] = "clan_1",
+            ["allTimePoints"] = 302050,
+            ["clanBanner"] = 10897814748,
+            ["isYourClan"] = true,
+            ["foundedTs"] = 1728302314
+        }
+    },
+    ["topClansEvent"] = {},
+    ["topClansAllTime"] = {
+        {
+            ["clanIcon"] = 10590477428,
+            ["shoutPlayerId"] = 145345,
+            ["clanName"] = "WE DEM BOYS",
+            ["clanShout"] = "Yeah we dem boys. Yeah we make da noise",
+            ["mods"] = {},
+            ["admins"] = {},
+            ["owner"] = 145345,
+            ["members"] = {
+                {
+                    ["playerId"] = 145345,
+                    ["playerBanner"] = "banner_bulletStorm",
+                    ["playerName"] = "pablito",
+                    ["playerPrestige"] = 2,
+                    ["allTimePoints"] = 50003,
+                    ["playerSoldierData"] = {},
+                    ["weekPoints"] = 34000,
+                    ["playerLevel"] = 17,
+                    ["playerEmblem"] = "emblem_commando"
+                }
+            },
+            ["clanTag"] = "DEM",
+            ["shoutTs"] = 1728342314,
+            ["shoutPlayerName"] = "pablito",
+            ["weekPoints"] = 50000,
+            ["clanId"] = "clan_1",
+            ["allTimePoints"] = 302050,
+            ["clanBanner"] = 10897814748,
+            ["isYourClan"] = true,
+            ["foundedTs"] = 1728302314
+        }
+    },
+    ["serverClans"] = {
+        {
+            ["clanIcon"] = 10590477428,
+            ["shoutPlayerId"] = 145345,
+            ["clanName"] = "WE DEM BOYS",
+            ["clanShout"] = "Yeah we dem boys. Yeah we make da noise",
+            ["mods"] = {},
+            ["admins"] = {},
+            ["owner"] = 145345,
+            ["members"] = {
+                {
+                    ["playerId"] = 145345,
+                    ["playerBanner"] = "banner_bulletStorm",
+                    ["playerName"] = "pablito",
+                    ["playerPrestige"] = 2,
+                    ["allTimePoints"] = 50003,
+                    ["playerSoldierData"] = {},
+                    ["weekPoints"] = 34000,
+                    ["playerLevel"] = 17,
+                    ["playerEmblem"] = "emblem_commando"
+                }
+            },
+            ["clanTag"] = "DEM",
+            ["shoutTs"] = 1728342314,
+            ["shoutPlayerName"] = "pablito",
+            ["weekPoints"] = 50000,
+            ["clanId"] = "clan_1",
+            ["allTimePoints"] = 302050,
+            ["clanBanner"] = 10897814748,
+            ["isYourClan"] = true,
+            ["foundedTs"] = 1728302314
+        },
+        {
+            ["clanIcon"] = 10511855986,
+            ["weekPoints"] = 30300,
+            ["owner"] = 48485959,
+            ["members"] = {
+                {
+                    ["playerId"] = 48485959,
+                    ["playerBanner"] = "banner_malfunction",
+                    ["playerName"] = "kevin",
+                    ["playerPrestige"] = 3,
+                    ["allTimePoints"] = 200003,
+                    ["playerSoldierData"] = {},
+                    ["weekPoints"] = 39120,
+                    ["playerLevel"] = 34,
+                    ["playerEmblem"] = "emblem_knockoutShrimp"
+                }
+            },
+            ["clanTag"] = "swag",
+            ["clanName"] = "The Metaverse",
+            ["clanShout"] = "Welcome to the metaverse",
+            ["allTimePoints"] = 222050,
+            ["clanId"] = "clan_2",
+            ["clanBanner"] = 10897852997,
+            ["admins"] = {},
+            ["mods"] = {},
+            ["foundedTs"] = 1728382314
+        }
+    },
+    ["platinumPoints"] = 400000,
+    ["topClansYear"] = {
+        {
+            ["clanIcon"] = 10590477428,
+            ["shoutPlayerId"] = 145345,
+            ["clanName"] = "WE DEM BOYS",
+            ["clanShout"] = "Yeah we dem boys. Yeah we make da noise",
+            ["mods"] = {},
+            ["admins"] = {},
+            ["owner"] = 145345,
+            ["members"] = {
+                {
+                    ["playerId"] = 145345,
+                    ["playerBanner"] = "banner_bulletStorm",
+                    ["playerName"] = "pablito",
+                    ["playerPrestige"] = 2,
+                    ["allTimePoints"] = 50003,
+                    ["playerSoldierData"] = {},
+                    ["weekPoints"] = 34000,
+                    ["playerLevel"] = 17,
+                    ["playerEmblem"] = "emblem_commando"
+                }
+            },
+            ["clanTag"] = "DEM",
+            ["shoutTs"] = 1728342314,
+            ["shoutPlayerName"] = "pablito",
+            ["weekPoints"] = 50000,
+            ["clanId"] = "clan_1",
+            ["allTimePoints"] = 302050,
+            ["clanBanner"] = 10897814748,
+            ["isYourClan"] = true,
+            ["foundedTs"] = 1728302314
+        }
+    },
+    ["eternalPoints"] = 2000000,
+    ["yourClanData"] = {
+        ["clanIcon"] = 10590477428,
+        ["shoutPlayerId"] = 145345,
+        ["clanName"] = "WE DEM BOYS",
+        ["clanShout"] = "Yeah we dem boys. Yeah we make da noise",
+        ["mods"] = {},
+        ["admins"] = {},
+        ["owner"] = 145345,
+        ["members"] = {
+            {
+                ["playerId"] = 145345,
+                ["playerBanner"] = "banner_bulletStorm",
+                ["playerName"] = "pablito",
+                ["playerPrestige"] = 2,
+                ["allTimePoints"] = 50003,
+                ["playerSoldierData"] = {},
+                ["weekPoints"] = 34000,
+                ["playerLevel"] = 17,
+                ["playerEmblem"] = "emblem_commando"
+            }
+        },
+        ["clanTag"] = "DEM",
+        ["shoutTs"] = 1728342314,
+        ["shoutPlayerName"] = "pablito",
+        ["weekPoints"] = 50000,
+        ["clanId"] = "clan_1",
+        ["allTimePoints"] = 302050,
+        ["clanBanner"] = 10897814748,
+        ["isYourClan"] = true,
+        ["foundedTs"] = 1728302314
+    },
+    ["personalClanProgress"] = {["weekPoints"] = 90000},
+    ["topClansMonth"] = {
+        {
+            ["clanIcon"] = 10590477428,
+            ["shoutPlayerId"] = 145345,
+            ["clanName"] = "WE DEM BOYS",
+            ["clanShout"] = "Yeah we dem boys. Yeah we make da noise",
+            ["mods"] = {},
+            ["admins"] = {},
+            ["owner"] = 145345,
+            ["members"] = {
+                {
+                    ["playerId"] = 145345,
+                    ["playerBanner"] = "banner_bulletStorm",
+                    ["playerName"] = "pablito",
+                    ["playerPrestige"] = 2,
+                    ["allTimePoints"] = 50003,
+                    ["playerSoldierData"] = {},
+                    ["weekPoints"] = 34000,
+                    ["playerLevel"] = 17,
+                    ["playerEmblem"] = "emblem_commando"
+                }
+            },
+            ["clanTag"] = "DEM",
+            ["shoutTs"] = 1728342314,
+            ["shoutPlayerName"] = "pablito",
+            ["weekPoints"] = 50000,
+            ["clanId"] = "clan_1",
+            ["allTimePoints"] = 302050,
+            ["clanBanner"] = 10897814748,
+            ["isYourClan"] = true,
+            ["foundedTs"] = 1728302314
+        }
+    },
+    ["clanMap"] = {
+        ["clan_2"] = {
+            ["clanIcon"] = 10511855986,
+            ["weekPoints"] = 30300,
+            ["owner"] = 48485959,
+            ["members"] = {
+                {
+                    ["playerId"] = 48485959,
+                    ["playerBanner"] = "banner_malfunction",
+                    ["playerName"] = "kevin",
+                    ["playerPrestige"] = 3,
+                    ["allTimePoints"] = 200003,
+                    ["playerSoldierData"] = {},
+                    ["weekPoints"] = 39120,
+                    ["playerLevel"] = 34,
+                    ["playerEmblem"] = "emblem_knockoutShrimp"
+                }
+            },
+            ["clanTag"] = "swag",
+            ["clanName"] = "The Metaverse",
+            ["clanShout"] = "Welcome to the metaverse",
+            ["allTimePoints"] = 222050,
+            ["clanId"] = "clan_2",
+            ["clanBanner"] = 10897852997,
+            ["admins"] = {},
+            ["mods"] = {},
+            ["foundedTs"] = 1728382314
+        },
+        ["clan_1"] = {
+            ["clanIcon"] = 10590477428,
+            ["shoutPlayerId"] = 145345,
+            ["clanName"] = "WE DEM BOYS",
+            ["clanShout"] = "Yeah we dem boys. Yeah we make da noise",
+            ["mods"] = {},
+            ["admins"] = {},
+            ["owner"] = 145345,
+            ["members"] = {
+                {
+                    ["playerId"] = 145345,
+                    ["playerBanner"] = "banner_bulletStorm",
+                    ["playerName"] = "pablito",
+                    ["playerPrestige"] = 2,
+                    ["allTimePoints"] = 50003,
+                    ["playerSoldierData"] = {},
+                    ["weekPoints"] = 34000,
+                    ["playerLevel"] = 17,
+                    ["playerEmblem"] = "emblem_commando"
+                }
+            },
+            ["clanTag"] = "DEM",
+            ["shoutTs"] = 1728342314,
+            ["shoutPlayerName"] = "pablito",
+            ["weekPoints"] = 50000,
+            ["clanId"] = "clan_1",
+            ["allTimePoints"] = 302050,
+            ["clanBanner"] = 10897814748,
+            ["isYourClan"] = true,
+            ["foundedTs"] = 1728302314
+        }
+    },
+    ["clanRanksAllTime"] = {["clan_2"] = 456, ["clan_1"] = 99},
+    ["clanRanksWeek"] = {["clan_2"] = 200, ["clan_1"] = 3},
+    ["diamondPoints"] = 800000
+}
