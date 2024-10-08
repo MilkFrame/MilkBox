6d91d6c971ce547ca5081ef8a9f4b1f4da253278
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 22:29:19 +1100

    Create HalloweenQuests.lua

---
+++ GameData/Toilet Tower Defense [13775256536]/HalloweenQuests.lua
@@ -0,0 +1,61 @@
+data = {
+    ["CollectCandy"] = {
+        ["Steps"] = {
+            {["Rewards"] = {{"Candy", 100}}, ["Requirement"] = {"CollectCandy", 500}},
+            {["Rewards"] = {{"Candy", 200}}, ["Requirement"] = {"CollectCandy", 1000}},
+            {["Rewards"] = {{"Candy", 300}}, ["Requirement"] = {"CollectCandy", 2500}},
+            {["Rewards"] = {{"Candy", 400}}, ["Requirement"] = {"CollectCandy", 5000}},
+            {["Rewards"] = {{"Candy", 600}}, ["Requirement"] = {"CollectCandy", 10000}},
+            {["Rewards"] = {{"Candy", 2000}}, ["Requirement"] = {"CollectCandy", 50000}}
+        },
+        ["DisplayName"] = "function: 0x00000000393c306f"
+    },
+    ["PlayForHours"] = {
+        ["Steps"] = {
+            {["Rewards"] = {{"Candy", 100}}, ["Requirement"] = {"PlayMinute", 60}},
+            {["Rewards"] = {{"Candy", 200}}, ["Requirement"] = {"PlayMinute", 120}},
+            {["Rewards"] = {{"Candy", 300}}, ["Requirement"] = {"PlayMinute", 240}},
+            {["Rewards"] = {{"Candy", 400}}, ["Requirement"] = {"PlayMinute", 480}},
+            {["Rewards"] = {{"Candy", 600}}, ["Requirement"] = {"PlayMinute", 960}},
+            {["Rewards"] = {{"Candy", 1000}}, ["Requirement"] = {"PlayMinute", 1440}},
+            {["Rewards"] = {{"Candy", 2500}}, ["Requirement"] = {"PlayMinute", 2880}}
+        },
+        ["DisplayName"] = "function: 0x000000008e63ac7f"
+    },
+    ["OpenCrates"] = {
+        ["Steps"] = {
+            {["Rewards"] = {{"Candy", 100}}, ["Requirement"] = {"OpenScaryCrate", 1}},
+            {["Rewards"] = {{"Candy", 500}}, ["Requirement"] = {"OpenScaryCrate", 10}},
+            {["Rewards"] = {{"Candy", 2000}}, ["Requirement"] = {"OpenScaryCrate", 100}},
+            {["Rewards"] = {{"Candy", 5000}}, ["Requirement"] = {"OpenScaryCrate", 1000}}
+        },
+        ["DisplayName"] = "function: 0x00000000695e46f7"
+    },
+    ["KillToilets"] = {
+        ["Steps"] = {
+            {["Rewards"] = {{"Candy", 50}}, ["Requirement"] = {"KillToilets", 500}},
+            {["Rewards"] = {{"Candy", 100}}, ["Requirement"] = {"KillToilets", 1000}},
+            {["Rewards"] = {{"Candy", 200}}, ["Requirement"] = {"KillToilets", 2500}},
+            {["Rewards"] = {{"Candy", 300}}, ["Requirement"] = {"KillToilets", 5000}},
+            {["Rewards"] = {{"Candy", 400}}, ["Requirement"] = {"KillToilets", 10000}},
+            {["Rewards"] = {{"Candy", 500}}, ["Requirement"] = {"KillToilets", 15000}},
+            {["Rewards"] = {{"Candy", 600}}, ["Requirement"] = {"KillToilets", 20000}},
+            {["Rewards"] = {{"Candy", 700}}, ["Requirement"] = {"KillToilets", 25000}},
+            {["Rewards"] = {{"Candy", 800}}, ["Requirement"] = {"KillToilets", 30000}},
+            {["Rewards"] = {{"Candy", 1000}}, ["Requirement"] = {"KillToilets", 40000}},
+            {["Rewards"] = {{"Candy", 1500}}, ["Requirement"] = {"KillToilets", 60000}},
+            {["Rewards"] = {{"Candy", 2500}}, ["Requirement"] = {"KillToilets", 100000}}
+        },
+        ["DisplayName"] = "function: 0x00000000b46791e7"
+    },
+    ["BeatWaves"] = {
+        ["Steps"] = {
+            {["Rewards"] = {{"Candy", 100}}, ["Requirement"] = {"BeatWave10", 1}},
+            {["Rewards"] = {{"Candy", 200}}, ["Requirement"] = {"BeatWave20", 1}},
+            {["Rewards"] = {{"Candy", 300}}, ["Requirement"] = {"BeatWave30", 1}},
+            {["Rewards"] = {{"Candy", 400}}, ["Requirement"] = {"BeatWave40", 1}},
+            {["Rewards"] = {{"Candy", 500}}, ["Requirement"] = {"BeatWave50", 1}}
+        },
+        ["DisplayName"] = "function: 0x000000001e24d84f"
+    }
+}
