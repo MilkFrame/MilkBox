e39a7dd4ce6e24dd60563f894b10480c3f9bf0b3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 22:27:42 +1100

    Create DrillEvent2024Quests.lua

---
+++ GameData/Toilet Tower Defense [13775256536]/DrillEvent2024Quests.lua
@@ -0,0 +1,144 @@
+data = {
+    ["PlayForHours"] = {
+        ["Steps"] = {
+            {["Difficulty"] = "Easy", ["Requirement"] = {"PlayMinute", 30}, ["Rewards"] = {{"XP", 100}, {"Drills", 50}}},
+            {
+                ["Difficulty"] = "Medium",
+                ["Requirement"] = {"PlayMinute", 60},
+                ["Rewards"] = {{"XP", 250}, {"Drills", 100}}
+            },
+            {
+                ["Difficulty"] = "Hard",
+                ["Requirement"] = {"PlayMinute", 120},
+                ["Rewards"] = {{"XP", 500}, {"Drills", 250}}
+            },
+            {
+                ["Difficulty"] = "Extreme",
+                ["Requirement"] = {"PlayMinute", 240},
+                ["Rewards"] = {{"XP", 750}, {"Drills", 350}}
+            },
+            {
+                ["Difficulty"] = "Challenging",
+                ["Requirement"] = {"PlayMinute", 480},
+                ["Rewards"] = {{"XP", 1000}, {"Drills", 500}}
+            }
+        },
+        ["DisplayName"] = "function: 0x5432ebdcff285f56"
+    },
+    ["OpenUltraCrate"] = {
+        ["Steps"] = {
+            {
+                ["Difficulty"] = "Easy",
+                ["Requirement"] = {"OpenUltraCrate", 1},
+                ["Rewards"] = {{"XP", 100}, {"Drills", 50}}
+            },
+            {
+                ["Difficulty"] = "Medium",
+                ["Requirement"] = {"OpenUltraCrate", 5},
+                ["Rewards"] = {{"XP", 250}, {"Drills", 100}}
+            },
+            {
+                ["Difficulty"] = "Hard",
+                ["Requirement"] = {"OpenUltraCrate", 10},
+                ["Rewards"] = {{"XP", 500}, {"Drills", 250}}
+            },
+            {
+                ["Difficulty"] = "Extreme",
+                ["Requirement"] = {"OpenUltraCrate", 25},
+                ["Rewards"] = {{"XP", 750}, {"Drills", 350}}
+            },
+            {
+                ["Difficulty"] = "Challenging",
+                ["Requirement"] = {"OpenUltraCrate", 50},
+                ["Rewards"] = {{"XP", 1000}, {"Drills", 500}}
+            }
+        },
+        ["DisplayName"] = "function: 0xb24905509d678cb6"
+    },
+    ["KillToilets"] = {
+        ["Steps"] = {
+            {
+                ["Difficulty"] = "Easy",
+                ["Requirement"] = {"KillToilets", 500},
+                ["Rewards"] = {{"XP", 100}, {"Drills", 50}}
+            },
+            {
+                ["Difficulty"] = "Medium",
+                ["Requirement"] = {"KillToilets", 1000},
+                ["Rewards"] = {{"XP", 250}, {"Drills", 100}}
+            },
+            {
+                ["Difficulty"] = "Hard",
+                ["Requirement"] = {"KillToilets", 2500},
+                ["Rewards"] = {{"XP", 500}, {"Drills", 250}}
+            },
+            {
+                ["Difficulty"] = "Extreme",
+                ["Requirement"] = {"KillToilets", 5000},
+                ["Rewards"] = {{"XP", 750}, {"Drills", 350}}
+            },
+            {
+                ["Difficulty"] = "Challenging",
+                ["Requirement"] = {"KillToilets", 10000},
+                ["Rewards"] = {{"XP", 1000}, {"Drills", 500}}
+            }
+        },
+        ["DisplayName"] = "function: 0x0aaed298a1dc2276"
+    },
+    ["OpenDrillCrate"] = {
+        ["Steps"] = {
+            {
+                ["Difficulty"] = "Easy",
+                ["Requirement"] = {"OpenDrillCrate", 10},
+                ["Rewards"] = {{"XP", 100}, {"Drills", 50}}
+            },
+            {
+                ["Difficulty"] = "Medium",
+                ["Requirement"] = {"OpenDrillCrate", 25},
+                ["Rewards"] = {{"XP", 250}, {"Drills", 100}}
+            },
+            {
+                ["Difficulty"] = "Hard",
+                ["Requirement"] = {"OpenDrillCrate", 50},
+                ["Rewards"] = {{"XP", 500}, {"Drills", 250}}
+            },
+            {
+                ["Difficulty"] = "Extreme",
+                ["Requirement"] = {"OpenDrillCrate", 100},
+                ["Rewards"] = {{"XP", 750}, {"Drills", 350}}
+            },
+            {
+                ["Difficulty"] = "Challenging",
+                ["Requirement"] = {"OpenDrillCrate", 150},
+                ["Rewards"] = {{"XP", 1000}, {"Drills", 500}}
+            }
+        },
+        ["DisplayName"] = "function: 0x1140df05baa17a16"
+    },
+    ["WinMatches"] = {
+        ["Steps"] = {
+            {["Difficulty"] = "Easy", ["Requirement"] = {"WinMatches", 1}, ["Rewards"] = {{"XP", 100}, {"Drills", 50}}},
+            {
+                ["Difficulty"] = "Medium",
+                ["Requirement"] = {"WinMatches", 5},
+                ["Rewards"] = {{"XP", 250}, {"Drills", 100}}
+            },
+            {
+                ["Difficulty"] = "Hard",
+                ["Requirement"] = {"WinMatches", 10},
+                ["Rewards"] = {{"XP", 500}, {"Drills", 250}}
+            },
+            {
+                ["Difficulty"] = "Extreme",
+                ["Requirement"] = {"WinMatches", 25},
+                ["Rewards"] = {{"XP", 750}, {"Drills", 350}}
+            },
+            {
+                ["Difficulty"] = "Challenging",
+                ["Requirement"] = {"WinMatches", 50},
+                ["Rewards"] = {{"XP", 1000}, {"Drills", 500}}
+            }
+        },
+        ["DisplayName"] = "function: 0xaaecb4d3b3903096"
+    }
+}
