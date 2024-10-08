b74ef1f54bab21dd02c7220d4a3d36885b32dec8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 22:29:06 +1100

    Create TroopsLimitGroups.lua

---
+++ GameData/Toilet Tower Defense [13775256536]/TroopsLimitGroups.lua
@@ -0,0 +1,27 @@
+data = {
+    ["SantaTVMan"] = {["Message"] = "You have reached your limit for placing Santa TV Man.", ["Limit"] = 3},
+    ["GreenLaserCameraman"] = {
+        ["Message"] = "You have reached your limit for placing Green Laser Cameramen!",
+        ["Limit"] = 4
+    },
+    ["MedicCameraman"] = {["Message"] = "You have reached your limit for placing Medic Cameramen.", ["Limit"] = 2},
+    ["LargeScientistCameraman"] = {
+        ["Message"] = "You have reached your limit for placing Large Scientist Cameramen.",
+        ["Limit"] = 1
+    },
+    ["CameraDrone"] = {["Message"] = "You have reached your limit for placing Camera Drone.", ["Limit"] = 1},
+    ["PumpkinFarmer"] = {["Message"] = "You have reached your limit for placing Pumpkin Farmers.", ["Limit"] = 5},
+    ["SpeakerRepairDrone"] = {
+        ["Message"] = "You have reached your limit for placing Speaker Repair Drones!",
+        ["Limit"] = 2
+    },
+    ["ScientistCameraman"] = {
+        ["Message"] = "You have reached your limit for placing Scientist Cameramen.",
+        ["Limit"] = 4
+    },
+    ["EngineerCameraman"] = {["Message"] = "You have reached your limit for placing Engineer Cameramen.", ["Limit"] = 4},
+    ["CameraHelicopter"] = {["Message"] = "You have reached your limit for placing Camera Helicopter.", ["Limit"] = 2},
+    ["MoneyTroops"] = {["Message"] = "You have reached your limit for placing Money Troops!", ["Limit"] = 5},
+    ["ScientistTVMan"] = {["Message"] = "You have reached your limit for placing Scientist TV Man.", ["Limit"] = 2},
+    ["SecretAgent"] = {["Message"] = "You have reached your limit for placing Secret Agent.", ["Limit"] = 1}
+}
