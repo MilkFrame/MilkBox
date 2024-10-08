6e121ca18a35e4e3e2639842dbff533bddf752b3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 22:32:44 +1100

    Create NewYears2023Fireworks.lua

---
+++ GameData/Toilet Tower Defense [13775256536]/NewYears2023Fireworks.lua
@@ -0,0 +1,242 @@
+data = {
+    ["FireworkPack"] = {
+        ["Image"] = "rbxassetid://15753783341",
+        ["Rarity"] = "MatchNightmare",
+        ["Group"] = "Matches",
+        ["Order"] = 28,
+        ["Description"] = "yay firework",
+        ["DisplayName"] = "Firework Pack"
+    },
+    ["BenchFirework"] = {
+        ["Image"] = "rbxassetid://15737811781",
+        ["Rarity"] = "Easy",
+        ["Group"] = "TradingPlaza",
+        ["Order"] = 13,
+        ["Description"] = "Search the benches!",
+        ["DisplayName"] = "Bench Firework"
+    },
+    ["RailFirework"] = {
+        ["Image"] = "rbxassetid://15737818815",
+        ["Rarity"] = "Medium",
+        ["Group"] = "TradingPlaza",
+        ["Order"] = 15,
+        ["Description"] = "Look at the handrails!",
+        ["DisplayName"] = "Rail Firework"
+    },
+    ["SnowmanFirework"] = {
+        ["Image"] = "rbxassetid://15737163538",
+        ["Rarity"] = "Easy",
+        ["Group"] = "Lobby",
+        ["Order"] = 2,
+        ["Description"] = "He lost his nose :(",
+        ["DisplayName"] = "Snowman Firework"
+    },
+    ["CoconutFirework"] = {
+        ["Image"] = "rbxassetid://15753707855",
+        ["Rarity"] = "MatchMedium",
+        ["Group"] = "Matches",
+        ["Order"] = 24,
+        ["Description"] = "Where do coconuts grow?",
+        ["DisplayName"] = "Coconut Firework"
+    },
+    ["BarrelFirework"] = {
+        ["Image"] = "rbxassetid://15753715042",
+        ["Rarity"] = "MatchMedium",
+        ["Group"] = "Matches",
+        ["Order"] = 25,
+        ["Description"] = "Shiny yellow barrels!",
+        ["DisplayName"] = "Barrel Firework"
+    },
+    ["BuildingFirework"] = {
+        ["Image"] = "rbxassetid://15737270878",
+        ["Rarity"] = "Nightmare",
+        ["Group"] = "Lobby",
+        ["Order"] = 10,
+        ["Description"] = "he is hiding",
+        ["DisplayName"] = "Building Firework"
+    },
+    ["SnowFirework"] = {
+        ["Image"] = "rbxassetid://15737203876",
+        ["Rarity"] = "Medium",
+        ["Group"] = "Lobby",
+        ["Order"] = 5,
+        ["Description"] = "It's hiding in the snow!",
+        ["DisplayName"] = "Snow Firework"
+    },
+    ["TelephonePoleFirework"] = {
+        ["Image"] = "rbxassetid://15737224707",
+        ["Rarity"] = "Medium",
+        ["Group"] = "Lobby",
+        ["Order"] = 7,
+        ["Description"] = "This telephone pole looks different...",
+        ["DisplayName"] = "Telephone Pole Firework"
+    },
+    ["FireworkBundle"] = {
+        ["Image"] = "rbxassetid://15753789580",
+        ["Rarity"] = "MatchNightmare",
+        ["Group"] = "Matches",
+        ["Order"] = 30,
+        ["Description"] = "FOUR fireworks????",
+        ["DisplayName"] = "Firework Pack"
+    },
+    ["FountainFirework"] = {
+        ["Image"] = "rbxassetid://15737824628",
+        ["Rarity"] = "Easy",
+        ["Group"] = "TradingPlaza",
+        ["Order"] = 12,
+        ["Description"] = "Check the fountain.",
+        ["DisplayName"] = "Fountain Firework"
+    },
+    ["SandFirework"] = {
+        ["Image"] = "rbxassetid://15753698991",
+        ["Rarity"] = "MatchMedium",
+        ["Group"] = "Matches",
+        ["Order"] = 23,
+        ["Description"] = "Look in the sand!",
+        ["DisplayName"] = "Sand Firework"
+    },
+    ["GlassFirework"] = {
+        ["Image"] = "rbxassetid://15753747582",
+        ["Rarity"] = "MatchHard",
+        ["Group"] = "Matches",
+        ["Order"] = 26,
+        ["Description"] = "Don't drop it!",
+        ["DisplayName"] = "Glass Firework"
+    },
+    ["ConcreteFirework"] = {
+        ["Image"] = "rbxassetid://15737901285",
+        ["Rarity"] = "Hard",
+        ["Group"] = "TradingPlaza",
+        ["Order"] = 17,
+        ["Description"] = "how can this fly",
+        ["DisplayName"] = "Concrete Firework"
+    },
+    ["MountainFirework"] = {
+        ["Image"] = "rbxassetid://15753759413",
+        ["Rarity"] = "MatchHard",
+        ["Group"] = "Matches",
+        ["Order"] = 27,
+        ["Description"] = "It's in the mountains!",
+        ["DisplayName"] = "Mountain Firework"
+    },
+    ["BushFirework"] = {
+        ["Image"] = "rbxassetid://15737789271",
+        ["Rarity"] = "Easy",
+        ["Group"] = "TradingPlaza",
+        ["Order"] = 11,
+        ["Description"] = "Look in the bushes!",
+        ["DisplayName"] = "Bush Firework"
+    },
+    ["TripleFirework"] = {
+        ["Image"] = "rbxassetid://15753784898",
+        ["Rarity"] = "MatchNightmare",
+        ["Group"] = "Matches",
+        ["Order"] = 29,
+        ["Description"] = "THREE fireworks!!!",
+        ["DisplayName"] = "Triple Firework"
+    },
+    ["SpawnFirework"] = {
+        ["Image"] = "rbxassetid://15737158716",
+        ["Rarity"] = "Easy",
+        ["Group"] = "Lobby",
+        ["Order"] = 1,
+        ["Description"] = "This is by the spawn fountain!",
+        ["DisplayName"] = "Spawn Firework"
+    },
+    ["TreeFirework"] = {
+        ["Image"] = "rbxassetid://15737190220",
+        ["Rarity"] = "Easy",
+        ["Group"] = "Lobby",
+        ["Order"] = 3,
+        ["Description"] = "hey this isn't part of the tree trunk",
+        ["DisplayName"] = "Tree Firework"
+    },
+    ["PillarFirework"] = {
+        ["Image"] = "rbxassetid://15737230343",
+        ["Rarity"] = "Hard",
+        ["Group"] = "Lobby",
+        ["Order"] = 8,
+        ["Description"] = "woah! it glows red!",
+        ["DisplayName"] = "Pillar Firework"
+    },
+    ["RockFirework"] = {
+        ["Image"] = "rbxassetid://15737197815",
+        ["Rarity"] = "Easy",
+        ["Group"] = "Lobby",
+        ["Order"] = 4,
+        ["Description"] = "Check the rocks.",
+        ["DisplayName"] = "Rock Firework"
+    },
+    ["UndergroundFirework"] = {
+        ["Image"] = "rbxassetid://15737897738",
+        ["Rarity"] = "Nightmare",
+        ["Group"] = "TradingPlaza",
+        ["Order"] = 20,
+        ["Description"] = "can you get UNDERGROUND?",
+        ["DisplayName"] = "Underground Firework"
+    },
+    ["BrickFirework"] = {
+        ["Image"] = "rbxassetid://15737912715",
+        ["Rarity"] = "Nightmare",
+        ["Group"] = "TradingPlaza",
+        ["Order"] = 19,
+        ["Description"] = "brick walls are the best",
+        ["DisplayName"] = "Brick Firework"
+    },
+    ["DumpsterFirework"] = {
+        ["Image"] = "rbxassetid://15737206965",
+        ["Rarity"] = "Medium",
+        ["Group"] = "Lobby",
+        ["Order"] = 6,
+        ["Description"] = "Look outside the house!",
+        ["DisplayName"] = "Dumpster Firework"
+    },
+    ["MetalFirework"] = {
+        ["Image"] = "rbxassetid://15737900096",
+        ["Rarity"] = "Hard",
+        ["Group"] = "TradingPlaza",
+        ["Order"] = 18,
+        ["Description"] = "this also shouldn't be able to fly",
+        ["DisplayName"] = "Metal Firework"
+    },
+    ["ToxicFirework"] = {
+        ["Image"] = "rbxassetid://15753683900",
+        ["Rarity"] = "MatchEasy",
+        ["Group"] = "Matches",
+        ["Order"] = 22,
+        ["Description"] = "Check the toxic puddles.",
+        ["DisplayName"] = "Toxic Firework"
+    },
+    ["CrateFirework"] = {
+        ["Image"] = "rbxassetid://15737821341",
+        ["Rarity"] = "Medium",
+        ["Group"] = "TradingPlaza",
+        ["Order"] = 14,
+        ["Description"] = "Check the crates.",
+        ["DisplayName"] = "Crate Firework"
+    },
+    ["DirtFirework"] = {
+        ["Image"] = "rbxassetid://15737811781",
+        ["Rarity"] = "Medium",
+        ["Group"] = "TradingPlaza",
+        ["Order"] = 16,
+        ["Description"] = "Check the dirt.",
+        ["DisplayName"] = "Dirt Firework"
+    },
+    ["ScientistFirework"] = {
+        ["Image"] = "rbxassetid://15737233625",
+        ["Rarity"] = "Hard",
+        ["Group"] = "Lobby",
+        ["Order"] = 9,
+        ["Description"] = "woah! it glows red!",
+        ["DisplayName"] = "Scientist Firework"
+    },
+    ["RoadFirework"] = {
+        ["Image"] = "rbxassetid://15753673673",
+        ["Rarity"] = "MatchEasy",
+        ["Group"] = "Matches",
+        ["Order"] = 21,
+        ["Description"] = "It's on the street!",
+        ["DisplayName"] = "Road Firework"
+    }
+}
