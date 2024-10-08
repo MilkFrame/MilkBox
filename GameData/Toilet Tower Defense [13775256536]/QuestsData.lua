4da95af5eb04b0479bfc584437169f1296b7b37b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 22:27:55 +1100

    Create QuestsData.lua

---
+++ GameData/Toilet Tower Defense [13775256536]/QuestsData.lua
@@ -0,0 +1,106 @@
+data = {
+    ["Quests"] = {
+        ["Play60Mins"] = {
+            ["Requirements"] = {"PlayMinute", 60},
+            ["Rewards"] = {{"Gems", 5}},
+            ["DisplayName"] = "Play for 60 minutes!"
+        },
+        ["AFKFarm360Mins"] = {
+            ["Requirements"] = {"AFKFarmMinutes", 360},
+            ["Rewards"] = {{"Gems", 25}},
+            ["DisplayName"] = "AFK farm for 360 minutes!"
+        },
+        ["Summon10Units"] = {
+            ["Requirements"] = {"SummonUnit", 10},
+            ["Rewards"] = {{"Gems", 5}},
+            ["DisplayName"] = "Summon 10 units!"
+        },
+        ["Place100Units"] = {
+            ["Requirements"] = {"PlaceUnit", 100},
+            ["Rewards"] = {{"Gems", 25}},
+            ["DisplayName"] = "Place 100 units!"
+        },
+        ["AFKFarm120Mins"] = {
+            ["Requirements"] = {"AFKFarmMinutes", 120},
+            ["Rewards"] = {{"Gems", 5}},
+            ["DisplayName"] = "AFK farm for 120 minutes!"
+        },
+        ["Summon1EpicUnit"] = {
+            ["Requirements"] = {"SummonEpicUnit", 1},
+            ["Rewards"] = {{"Gems", 5}},
+            ["DisplayName"] = "Summon an epic unit!"
+        },
+        ["Place25Units"] = {
+            ["Requirements"] = {"PlaceUnit", 25},
+            ["Rewards"] = {{"Gems", 5}},
+            ["DisplayName"] = "Place 25 units!"
+        },
+        ["Play180Mins"] = {
+            ["Requirements"] = {"PlayMinute", 180},
+            ["Rewards"] = {{"Gems", 25}},
+            ["DisplayName"] = "Play for 180 minutes!"
+        },
+        ["Win10Matches"] = {
+            ["Requirements"] = {"WinMatch", 10},
+            ["Rewards"] = {{"Gems", 25}},
+            ["DisplayName"] = "Win 10 matches!"
+        },
+        ["Summon40Units"] = {
+            ["Requirements"] = {"SummonUnit", 40},
+            ["Rewards"] = {{"Gems", 25}},
+            ["DisplayName"] = "Summon 40 units!"
+        },
+        ["Summon1LegendaryUnit"] = {
+            ["Requirements"] = {"SummonLegendaryUnit", 1},
+            ["Rewards"] = {{"Gems", 25}},
+            ["DisplayName"] = "Summon a legendary unit!"
+        },
+        ["Play20Matches"] = {
+            ["Requirements"] = {"PlayMatch", 20},
+            ["Rewards"] = {{"Gems", 25}},
+            ["DisplayName"] = "Play 20 matches!"
+        },
+        ["Win3Matches"] = {
+            ["Requirements"] = {"WinMatch", 3},
+            ["Rewards"] = {{"Gems", 5}},
+            ["DisplayName"] = "Win 3 matches!"
+        },
+        ["Play5Matches"] = {
+            ["Requirements"] = {"PlayMatch", 5},
+            ["Rewards"] = {{"Gems", 5}},
+            ["DisplayName"] = "Play 5 matches!"
+        },
+        ["Kill1000Toilets"] = {
+            ["Requirements"] = {"KillToilet", 1000},
+            ["Rewards"] = {{"Gems", 5}},
+            ["DisplayName"] = "Kill 1,000 toilets!"
+        },
+        ["Kill10000Toilets"] = {
+            ["Requirements"] = {"KillToilet", 10000},
+            ["Rewards"] = {{"Gems", 25}},
+            ["DisplayName"] = "Kill 10,000 toilets!"
+        }
+    },
+    ["Randomizer"] = {
+        ["Weekly"] = {
+            ["Win10Matches"] = 1,
+            ["Summon40Units"] = 1,
+            ["AFKFarm360Mins"] = 1,
+            ["Play180Mins"] = 1,
+            ["Place100Units"] = 1,
+            ["Summon1LegendaryUnit"] = 1,
+            ["Play20Matches"] = 1,
+            ["Kill10000Toilets"] = 1
+        },
+        ["Daily"] = {
+            ["Play60Mins"] = 1,
+            ["Summon10Units"] = 1,
+            ["Kill1000Toilets"] = 1,
+            ["Summon1EpicUnit"] = 1,
+            ["AFKFarm120Mins"] = 1,
+            ["Win3Matches"] = 1,
+            ["Place25Units"] = 1,
+            ["Play5Matches"] = 1
+        }
+    }
+}
