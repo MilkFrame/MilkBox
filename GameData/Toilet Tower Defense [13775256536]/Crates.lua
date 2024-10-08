3ca3a0dfc35de71c891033fb2b6c92f6a97cf3ad
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 22:24:26 +1100

    Create Crates.lua

---
+++ GameData/Toilet Tower Defense [13775256536]/Crates.lua
@@ -0,0 +1,365 @@
+data = {
+    ["CratesList"] = {
+        ["InjuredTitanCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["StPatricksDay2024Crate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["ActivityCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["VIPCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["FoodCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["DrillSupportCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["ClockCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["PineappleCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["FanCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["NewYears2023Crate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["DevCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["ExclusiveSpiderCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["GadgetCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["ExclusiveScientistCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["PencilCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["LagoonCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["SigmaCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["SantaCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["UltraDrillCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["TurkeyCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["ValentinesDay2024Crate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["Easter2024Crate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["FutureCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["July4thCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["PrinterCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["ScaryCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true},
+        ["BoosterCrate"] = {["RecordExists"] = true, ["ShowInIndex"] = true, ["Tradable"] = true}
+    },
+    ["Crates"] = {
+        ["InjuredTitanCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 60, ["Type"] = "Troops", ["ItemId"] = "SawUpgradedTitanCameraman"},
+                {["Chance"] = 39, ["Type"] = "Troops", ["ItemId"] = "ToxicUpgradedTitanCameraman"},
+                {["Chance"] = 1, ["Type"] = "Troops", ["ItemId"] = "HyperUpgradedTitanSpeakerman"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 957,
+            ["CrateModel"] = "InjuredTitanCrate",
+            ["Icon"] = "rbxassetid://16200152317",
+            ["DisplayName"] = "Exclusive Injured Titan Crate"
+        },
+        ["StPatricksDay2024Crate"] = {
+            ["Items"] = {
+                {["Chance"] = 99.9, ["Type"] = "Troops", ["ItemId"] = "LuckySpeakerman"},
+                {["Chance"] = 0.1, ["Type"] = "Troops", ["ItemId"] = "TitanCloverMan"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 959,
+            ["CrateModel"] = "StPatricksDay2024Crate",
+            ["Icon"] = "rbxassetid://16659791555",
+            ["DisplayName"] = "St. Patrick's Day 2024 Crate"
+        },
+        ["ScaryCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 50, ["Type"] = "Troops", ["ItemId"] = "ScarySpeakerman"},
+                {["Chance"] = 40, ["Type"] = "Troops", ["ItemId"] = "MonsterSpeakerman"},
+                {["Chance"] = 9, ["Type"] = "Troops", ["ItemId"] = "PumpkinFarmer"},
+                {["Chance"] = 0.9, ["Type"] = "Troops", ["ItemId"] = "SinisterCameraman"},
+                {["Chance"] = 0.1, ["Type"] = "Troops", ["ItemId"] = "CorruptedCameraman"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 952,
+            ["CrateModel"] = "ScaryCrate",
+            ["Icon"] = "rbxassetid://15949760327",
+            ["DisplayName"] = "Scary Crate"
+        },
+        ["DrillSupportCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 90, ["Type"] = "Troops", ["ItemId"] = "CorporalDrillman"},
+                {["Chance"] = 9, ["Type"] = "Troops", ["ItemId"] = "CheeseDrillman"},
+                {["Chance"] = 1, ["Type"] = "Troops", ["ItemId"] = "SergeantDrillman"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 975,
+            ["CrateModel"] = "DrillSupportCrate",
+            ["Icon"] = "rbxassetid://127224723083174",
+            ["DisplayName"] = "Drill Support Crate"
+        },
+        ["FoodCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 60, ["Type"] = "Troops", ["ItemId"] = "HotDogSpeakerman"},
+                {["Chance"] = 39, ["Type"] = "Troops", ["ItemId"] = "HamburgerTVMan"},
+                {["Chance"] = 1, ["Type"] = "Troops", ["ItemId"] = "TitanMeatballMan"},
+                {["Chance"] = 0.1, ["Type"] = "Troops", ["ItemId"] = "TitanCactusMan"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 966,
+            ["CrateModel"] = "FoodCrate",
+            ["Icon"] = "rbxassetid://18944033399",
+            ["DisplayName"] = "Exclusive Food Crate"
+        },
+        ["VIPCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 99, ["Type"] = "Troops", ["ItemId"] = "VIPCameraman"},
+                {["Chance"] = 1, ["Type"] = "Troops", ["ItemId"] = "VeryImportantTitan"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 976,
+            ["CrateModel"] = "VIPCrate",
+            ["Icon"] = "rbxassetid://131867824207206",
+            ["DisplayName"] = "VIP Crate"
+        },
+        ["ClockCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 45, ["Type"] = "Troops", ["ItemId"] = "PendulumClockman"},
+                {["Chance"] = 35, ["Type"] = "Troops", ["ItemId"] = "CuckooClockman"},
+                {["Chance"] = 10, ["Type"] = "Troops", ["ItemId"] = "AlarmClockman"},
+                {["Chance"] = 0.1, ["Type"] = "Troops", ["ItemId"] = "LargeClockman"},
+                {["Chance"] = 0.01, ["Type"] = "Troops", ["ItemId"] = "ChiefClockman"},
+                {["Chance"] = 0.001, ["Type"] = "Troops", ["ItemId"] = "UpgradedTitanClockman"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 961,
+            ["CrateModel"] = "ClockCrate",
+            ["Icon"] = "rbxassetid://17503086005",
+            ["DisplayName"] = "Time Crate"
+        },
+        ["FanCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 60, ["Type"] = "Troops", ["ItemId"] = "Fanman"},
+                {["Chance"] = 39, ["Type"] = "Troops", ["ItemId"] = "LargeFanman"},
+                {["Chance"] = 1, ["Type"] = "Troops", ["ItemId"] = "TitanFanman"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 963,
+            ["CrateModel"] = "FanCrate",
+            ["Icon"] = "rbxassetid://18176897977",
+            ["DisplayName"] = "Exclusive Fan Crate"
+        },
+        ["ActivityCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 99.9, ["Type"] = "Troops", ["ItemId"] = "HourglassSpeakerman"},
+                {["Chance"] = 0.1, ["Type"] = "Troops", ["ItemId"] = "EternalClockTitan"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 972,
+            ["CrateModel"] = "Crate",
+            ["Icon"] = "rbxassetid://78129928585925",
+            ["DisplayName"] = "Activity Crate"
+        },
+        ["NewYears2023Crate"] = {
+            ["Items"] = {
+                {["Chance"] = 99.9, ["Type"] = "Troops", ["ItemId"] = "FireworkCameraman"},
+                {["Chance"] = 0.1, ["Type"] = "Troops", ["ItemId"] = "LargeFireworkCameraman"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 955,
+            ["CrateModel"] = "NewYears2023Crate",
+            ["Icon"] = "rbxassetid://15949760526",
+            ["DisplayName"] = "New Years (2023) Crate"
+        },
+        ["DevCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 80, ["Type"] = "Troops", ["ItemId"] = "Pool"},
+                {["Chance"] = 20, ["Type"] = "Troops", ["ItemId"] = "iamEvan"},
+                {["Chance"] = 0.1, ["Type"] = "Troops", ["ItemId"] = "Telanthric"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 977,
+            ["CrateModel"] = "DevCrate",
+            ["Icon"] = "rbxassetid://72605268660232",
+            ["DisplayName"] = "Developer Crate"
+        },
+        ["ExclusiveSpiderCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 70, ["Type"] = "Troops", ["ItemId"] = "SpeakerSpider"},
+                {["Chance"] = 29.9, ["Type"] = "Troops", ["ItemId"] = "CameraSpider"},
+                {["Chance"] = 0.1, ["Type"] = "Troops", ["ItemId"] = "SpiderTV"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 951,
+            ["CrateModel"] = "ExclusiveSpiderCrate",
+            ["Icon"] = "rbxassetid://15935972253",
+            ["DisplayName"] = "Exclusive Spider Crate"
+        },
+        ["GadgetCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 60, ["Type"] = "Troops", ["ItemId"] = "StaticSpeakerman"},
+                {["Chance"] = 39.5, ["Type"] = "Troops", ["ItemId"] = "GravityGunCameraman"},
+                {["Chance"] = 0.5, ["Type"] = "Troops", ["ItemId"] = "CageScientistClockman"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 970,
+            ["CrateModel"] = "GadgetCrate",
+            ["Icon"] = "rbxassetid://75696550793114",
+            ["DisplayName"] = "Gadget Crate"
+        },
+        ["BoosterCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 70, ["Type"] = "Troops", ["ItemId"] = "DJSpeakerman"},
+                {["Chance"] = 29.9, ["Type"] = "Troops", ["ItemId"] = "AnnouncerCameraman"},
+                {["Chance"] = 0.1, ["Type"] = "Troops", ["ItemId"] = "DJTVMan"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 956,
+            ["CrateModel"] = "BoosterCrate",
+            ["Icon"] = "rbxassetid://16028611556",
+            ["DisplayName"] = "Exclusive Booster Crate"
+        },
+        ["PencilCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 50, ["Type"] = "Troops", ["ItemId"] = "Pencilman"},
+                {["Chance"] = 39, ["Type"] = "Troops", ["ItemId"] = "GunPencilman"},
+                {["Chance"] = 10, ["Type"] = "Troops", ["ItemId"] = "PencilMech"},
+                {["Chance"] = 1, ["Type"] = "Troops", ["ItemId"] = "CreatorPencilman"},
+                {["Chance"] = 0.1, ["Type"] = "Troops", ["ItemId"] = "TitanPencilWoman"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 971,
+            ["CrateModel"] = "PencilCrate",
+            ["Icon"] = "rbxassetid://119420490348608",
+            ["DisplayName"] = "Pencil Crate"
+        },
+        ["PineappleCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 70, ["Type"] = "Troops", ["ItemId"] = "PineappleSpeakerman"},
+                {["Chance"] = 27, ["Type"] = "Troops", ["ItemId"] = "SeagullCameraman"},
+                {["Chance"] = 2.7, ["Type"] = "Troops", ["ItemId"] = "TorchTVMan"},
+                {["Chance"] = 0.3, ["Type"] = "Troops", ["ItemId"] = "VolcanicTitan"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 968,
+            ["CrateModel"] = "PineappleCrate",
+            ["Icon"] = "rbxassetid://102645897946500",
+            ["DisplayName"] = "Tropical Crate"
+        },
+        ["SigmaCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 80, ["Type"] = "Troops", ["ItemId"] = "AlphaSpeakerman"},
+                {["Chance"] = 19, ["Type"] = "Troops", ["ItemId"] = "InfluencerCameraman"},
+                {["Chance"] = 1, ["Type"] = "Troops", ["ItemId"] = "TitanMogman"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 969,
+            ["CrateModel"] = "SigmaCrate",
+            ["Icon"] = "rbxassetid://73913970677356",
+            ["DisplayName"] = "Sigma Crate"
+        },
+        ["SantaCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 50, ["Type"] = "Troops", ["ItemId"] = "ReindeerSpeakerman"},
+                {["Chance"] = 40, ["Type"] = "Troops", ["ItemId"] = "ElfCamerawoman"},
+                {["Chance"] = 9, ["Type"] = "Troops", ["ItemId"] = "SantaTVMan"},
+                {["Chance"] = 1, ["Type"] = "Troops", ["ItemId"] = "TitanPresentMan"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 954,
+            ["CrateModel"] = "SantaCrate",
+            ["Icon"] = "rbxassetid://15956892727",
+            ["DisplayName"] = "Santa Crate"
+        },
+        ["UltraDrillCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 99, ["Type"] = "Troops", ["ItemId"] = "LargeDrillman"},
+                {["Chance"] = 1, ["Type"] = "Troops", ["ItemId"] = "AssassinDrillman"},
+                {["Chance"] = 0.01, ["Type"] = "Troops", ["ItemId"] = "UpgradedTitanDrilldragon"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 974,
+            ["CrateModel"] = "UltraDrillCrate",
+            ["Icon"] = "rbxassetid://135060331059868",
+            ["DisplayName"] = "Ultra Drill Crate"
+        },
+        ["TurkeyCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 50, ["Type"] = "Troops", ["ItemId"] = "TurkeySpeakerman"},
+                {["Chance"] = 40, ["Type"] = "Troops", ["ItemId"] = "TurkeyCameraman"},
+                {["Chance"] = 9, ["Type"] = "Troops", ["ItemId"] = "LargeTurkeySpeakerman"},
+                {["Chance"] = 1, ["Type"] = "Troops", ["ItemId"] = "ChefTVMan"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 953,
+            ["CrateModel"] = "TurkeyCrate",
+            ["Icon"] = "rbxassetid://15956517206",
+            ["DisplayName"] = "Turkey Crate"
+        },
+        ["ValentinesDay2024Crate"] = {
+            ["Items"] = {
+                {["Chance"] = 60, ["Type"] = "Troops", ["ItemId"] = "LargeHeartSpeakerman"},
+                {["Chance"] = 35, ["Type"] = "Troops", ["ItemId"] = "SpeakerwomansRoseFarm"},
+                {["Chance"] = 4.5, ["Type"] = "Troops", ["ItemId"] = "CupidCamerawoman"},
+                {["Chance"] = 0.5, ["Type"] = "Troops", ["ItemId"] = "HealerTVWoman"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 958,
+            ["CrateModel"] = "ValentinesDay2024Crate",
+            ["Icon"] = "rbxassetid://16358874692",
+            ["DisplayName"] = "Valentines Day 2024 Crate"
+        },
+        ["Easter2024Crate"] = {
+            ["Items"] = {
+                {["Chance"] = 75, ["Type"] = "Troops", ["ItemId"] = "EasterSpeakerman"},
+                {["Chance"] = 23, ["Type"] = "Troops", ["ItemId"] = "LargeBunnyman"},
+                {["Chance"] = 1.8, ["Type"] = "Troops", ["ItemId"] = "EggLauncherCameraman"},
+                {["Chance"] = 0.2, ["Type"] = "Troops", ["ItemId"] = "MechBunnyTitan"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 960,
+            ["CrateModel"] = "Easter2024Crate",
+            ["Icon"] = "rbxassetid://16756599016",
+            ["DisplayName"] = "Bunny Crate"
+        },
+        ["FutureCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 99, ["Type"] = "Troops", ["ItemId"] = "FutureLargeClockman"},
+                {["Chance"] = 1, ["Type"] = "Troops", ["ItemId"] = "GoldenFutureLargeClockman"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 962,
+            ["CrateModel"] = "FutureCrate",
+            ["Icon"] = "rbxassetid://17732240569",
+            ["DisplayName"] = "Future Crate"
+        },
+        ["ExclusiveScientistCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 70, ["Type"] = "Troops", ["ItemId"] = "ScientistCameraman"},
+                {["Chance"] = 29, ["Type"] = "Troops", ["ItemId"] = "LargeScientistCameraman"},
+                {["Chance"] = 1, ["Type"] = "Troops", ["ItemId"] = "EngineerCameraman"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 950,
+            ["CrateModel"] = "ExclusiveScientistCrate",
+            ["Icon"] = "rbxassetid://14199484613",
+            ["DisplayName"] = "Exclusive Scientist Crate"
+        },
+        ["PrinterCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 50, ["Type"] = "Troops", ["ItemId"] = "PrinterMan"},
+                {["Chance"] = 40, ["Type"] = "Troops", ["ItemId"] = "DarkPrinterMan"},
+                {["Chance"] = 9, ["Type"] = "Troops", ["ItemId"] = "LargePrinterMan"},
+                {["Chance"] = 1, ["Type"] = "Troops", ["ItemId"] = "TitanPrinterMan"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 973,
+            ["CrateModel"] = "PrinterCrate",
+            ["Icon"] = "rbxassetid://125157659563488",
+            ["DisplayName"] = "Printer Crate"
+        },
+        ["July4thCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 99.95, ["Type"] = "Troops", ["ItemId"] = "SparklerCameraman"},
+                {["Chance"] = 0.05, ["Type"] = "Troops", ["ItemId"] = "TitanFireworkMan"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 964,
+            ["CrateModel"] = "July4thCrate",
+            ["Icon"] = "rbxassetid://18318660378",
+            ["DisplayName"] = "July 4th Crate"
+        },
+        ["LagoonCrate"] = {
+            ["Items"] = {
+                {["Chance"] = 74, ["Type"] = "Troops", ["ItemId"] = "IceCreamSpeakerman"},
+                {["Chance"] = 24, ["Type"] = "Troops", ["ItemId"] = "CoconutCameraman"},
+                {["Chance"] = 1, ["Type"] = "Troops", ["ItemId"] = "TitanBoomboxMan"}
+            },
+            ["Rarity"] = "Exclusive",
+            ["Order"] = 967,
+            ["CrateModel"] = "LagoonCrate",
+            ["Icon"] = "rbxassetid://18965706979",
+            ["DisplayName"] = "Coconut Crate"
+        }
+    }
+}
