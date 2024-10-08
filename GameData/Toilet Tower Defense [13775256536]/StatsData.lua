1685e3473d2e2ee31436575f4cbe65ace195082f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 22:32:17 +1100

    Create StatsData.lua

---
+++ GameData/Toilet Tower Defense [13775256536]/StatsData.lua
@@ -0,0 +1,13 @@
+data = {
+    ["HealHealth"] = {["Order"] = 4, ["DisplayName"] = "Heal Health"},
+    ["WalkRadius"] = {["Order"] = 8, ["DisplayName"] = "Walk Range"},
+    ["ExtraIncome"] = {["Order"] = 3, ["ShortDisplayName"] = "Income", ["DisplayName"] = "Extra Income"},
+    ["AttackRadius"] = {["Order"] = 1, ["ShortOrder"] = 2, ["DisplayName"] = "Range"},
+    ["EnergyPerSecond"] = {["Order"] = -1, ["DisplayName"] = "Energy Gain"},
+    ["AttackCooldown"] = {["Order"] = 2, ["ShortOrder"] = 1, ["DisplayName"] = "Cooldown"},
+    ["BlastRadius"] = {["Order"] = 7, ["ShortDisplayName"] = "Blast Range", ["DisplayName"] = "Explosion Radius"},
+    ["AllyDPS"] = {["Order"] = 6, ["DisplayName"] = "Ally DPS"},
+    ["AllyHealth"] = {["Order"] = 5, ["DisplayName"] = "Ally Health"},
+    ["WalkSpeed"] = {["Order"] = 9, ["DisplayName"] = "Walk Speed"},
+    ["Damage"] = {["Order"] = 0, ["DisplayName"] = "Damage"}
+}
