6660f08a784a402503f0d02a2ac0a2f8c24418b4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 22:28:29 +1100

    Create MapDatas.lua

---
+++ GameData/Toilet Tower Defense [13775256536]/MapDatas.lua
@@ -0,0 +1,60 @@
+data = {
+    ["TurkeyTown"] = {
+        ["MapIcon"] = "rbxassetid://15388376009",
+        ["OverrideDifficulties"] = {"Thanksgiving"},
+        ["OtherRoutes"] = {
+            ["Route3"] = {["CanRandomlyPickRoute"] = true, ["FolderName"] = "Route3"},
+            ["Route2"] = {["CanRandomlyPickRoute"] = true, ["FolderName"] = "Route2"}
+        },
+        ["MapType"] = "Thanksgiving",
+        ["DisplayName"] = "Turkey Town"
+    },
+    ["PalmParadise"] = {
+        ["MapIcon"] = "rbxassetid://14779061045",
+        ["OtherRoutes"] = {["Route2"] = {["CanRandomlyPickRoute"] = true, ["FolderName"] = "Route2"}},
+        ["MapType"] = "Normal",
+        ["DisplayName"] = "Palm Paradise"
+    },
+    ["ToiletCity"] = {["MapIcon"] = "rbxassetid://14296422767", ["MapType"] = "Normal", ["DisplayName"] = "Toilet City"},
+    ["CameramanHQ"] = {
+        ["MapIcon"] = "rbxassetid://14296423029",
+        ["MapType"] = "Normal",
+        ["DisplayName"] = "Cameraman HQ"
+    },
+    ["Desert"] = {["MapIcon"] = "rbxassetid://14296422926", ["MapType"] = "Normal", ["DisplayName"] = "Desert"},
+    ["ToiletHQ"] = {["MapIcon"] = "rbxassetid://14296422540", ["MapType"] = "Normal", ["DisplayName"] = "Toilet HQ"},
+    ["PumpkinPatch"] = {
+        ["MapIcon"] = "rbxassetid://15181132946",
+        ["OverrideDifficulties"] = {"Halloween"},
+        ["OtherRoutes"] = {["Route2"] = {["CanRandomlyPickRoute"] = true, ["FolderName"] = "Route2"}},
+        ["MapType"] = "HalloweenEvent",
+        ["DisplayName"] = "Pumpkin Patch"
+    },
+    ["ToiletFortress"] = {
+        ["MapIcon"] = "rbxassetid://15915350662",
+        ["MapType"] = "Endless",
+        ["OverrideDifficulties"] = {"Endless"},
+        ["DisplayName"] = "Toilet Fortress"
+    },
+    ["ToiletLab"] = {
+        ["MapIcon"] = "rbxassetid://15712624800",
+        ["OtherRoutes"] = {["Route2"] = {["CanRandomlyPickRoute"] = true, ["FolderName"] = "Route2"}},
+        ["MapType"] = "Normal",
+        ["DisplayName"] = "Toilet Lab"
+    },
+    ["Graveyard"] = {
+        ["MapIcon"] = "rbxassetid://15254366730",
+        ["OverrideDifficulties"] = {"Halloween"},
+        ["OtherRoutes"] = {["Route2"] = {["CanRandomlyPickRoute"] = true, ["FolderName"] = "Route2"}},
+        ["MapModifiers"] = "function: 0x000000009c49480f",
+        ["MapType"] = "HalloweenEvent",
+        ["DisplayName"] = "Graveyard"
+    },
+    ["NorthPole"] = {
+        ["MapIcon"] = "rbxassetid://15570820177",
+        ["OverrideDifficulties"] = {"Christmas"},
+        ["OtherRoutes"] = {["Route2"] = {["CanRandomlyPickRoute"] = true, ["FolderName"] = "Route2"}},
+        ["MapType"] = "ChristmasEvent",
+        ["DisplayName"] = "North Pole"
+    }
+}
