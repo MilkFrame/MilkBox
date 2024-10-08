635a6f2f3ac2f878d8a8e4f832388f65f99cf0b1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 22:24:58 +1100

    Create Rarities.lua

---
+++ GameData/Toilet Tower Defense [13775256536]/Rarities.lua
@@ -0,0 +1,11 @@
+data = {
+    ["Exclusive"] = {["Id"] = "Exclusive", ["Gradient"] = "Exclusive", ["Order"] = 8},
+    ["Godly"] = {["Id"] = "Godly", ["Gradient"] = "Godly", ["Order"] = 7},
+    ["Mythic"] = {["Id"] = "Mythic", ["Gradient"] = "Mythic", ["Order"] = 6},
+    ["Uncommon"] = {["Id"] = "Uncommon", ["Gradient"] = "Uncommon", ["Order"] = 2},
+    ["Rare"] = {["Id"] = "Rare", ["Gradient"] = "Rare", ["Order"] = 3},
+    ["Legendary"] = {["Id"] = "Legendary", ["Gradient"] = "Legendary", ["Order"] = 5},
+    ["Basic"] = {["Id"] = "Basic", ["Gradient"] = "Basic", ["Order"] = 1},
+    ["Epic"] = {["Id"] = "Epic", ["Gradient"] = "Epic", ["Order"] = 4},
+    ["Ultimate"] = {["Id"] = "Ultimate", ["Gradient"] = "Ultimate", ["Order"] = 9}
+}
