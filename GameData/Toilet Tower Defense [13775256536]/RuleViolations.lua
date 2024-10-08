5c79a8310a7fb76d6455aac2b96876a6b98f833e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 22:27:02 +1100

    Create RuleViolations.lua

---
+++ GameData/Toilet Tower Defense [13775256536]/RuleViolations.lua
@@ -0,0 +1,9 @@
+data = {
+    ["Beaming"] = {["PunishType"] = "Ban", ["DisplayReason"] = "Beaming", ["Duration"] = inf},
+    ["PromoteCrossTrade"] = {["PunishType"] = "Ban",["DisplayReason"] = "Promoting Cross Trading",["Duration"] = 604800},
+    ["Exploits"] = {["PunishType"] = "Ban", ["DisplayReason"] = "Exploiting", ["Duration"] = inf},
+    ["Scamming"] = {["PunishType"] = "Ban", ["DisplayReason"] = "Scamming", ["Duration"] = 604800},
+    ["Impersonation"] = {["PunishType"] = "Ban", ["DisplayReason"] = "Impersonation", ["Duration"] = 604800},
+    ["Bots"] = {["PunishType"] = "Ban", ["DisplayReason"] = "Botting", ["Duration"] = inf},
+    ["CrossTrade"] = {["PunishType"] = "Ban", ["DisplayReason"] = "Cross Trading", ["Duration"] = inf}
+}
