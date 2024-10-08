b9985c71c508782c751c8381acfb6dd9c552b680
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-08 22:21:51 +1100

    Create Currencies.lua

---
+++ GameData/Toilet Tower Defense [13775256536]/Currencies.lua
@@ -0,0 +1,123 @@
+data = {
+    ["Present_Christmas2023"] = {
+        ["Id"] = "Present_Christmas2023",
+        ["IsPrimary"] = false,
+        ["ShowInLeaderstats"] = "function: 0x692d89c756321976",
+        ["Default"] = 0,
+        ["Order"] = 4,
+        ["Display"] = "Present",
+        ["IsInteger"] = true,
+        ["Icon"] = "rbxassetid://0",
+        ["Max"] = inf
+    },
+    ["Wins"] = {
+        ["Id"] = "Wins",
+        ["IsPrimary"] = true,
+        ["ShowInLeaderstats"] = "function: 0xa0a1e330ee590156",
+        ["Default"] = 0,
+        ["Order"] = 1,
+        ["Display"] = "Wins",
+        ["IsInteger"] = true,
+        ["Icon"] = "rbxassetid://0",
+        ["Max"] = inf
+    },
+    ["Coins"] = {
+        ["Id"] = "Coins",
+        ["IsPrimary"] = false,
+        ["ShowInLeaderstats"] = "function: 0xb66b7f15c47ef996",
+        ["Default"] = 0,
+        ["Order"] = 2,
+        ["Display"] = "Coins",
+        ["IsInteger"] = true,
+        ["Icon"] = "rbxassetid://14655239746",
+        ["Max"] = inf
+    },
+    ["Pencils_PencilEvent"] = {
+        ["Id"] = "Pencils_PencilEvent",
+        ["IsPrimary"] = false,
+        ["ShowInLeaderstats"] = "function: 0xa5cd32928d9ebfb6",
+        ["Default"] = 0,
+        ["Order"] = 8,
+        ["Display"] = "Pencils",
+        ["IsInteger"] = true,
+        ["Icon"] = "rbxassetid://0",
+        ["Max"] = inf
+    },
+    ["Drills_DrillEvent"] = {
+        ["Id"] = "Drills_DrillEvent",
+        ["IsPrimary"] = false,
+        ["ShowInLeaderstats"] = "function: 0x1f4d74a075a8b9d6",
+        ["Default"] = 0,
+        ["Order"] = 9,
+        ["Display"] = "Drills",
+        ["IsInteger"] = true,
+        ["Icon"] = "rbxassetid://96992069734038",
+        ["Max"] = inf
+    },
+    ["Clocks_ClockEvent"] = {
+        ["Id"] = "Clocks_ClockEvent",
+        ["IsPrimary"] = false,
+        ["ShowInLeaderstats"] = "function: 0xf248908d5aef3096",
+        ["Default"] = 0,
+        ["Order"] = 7,
+        ["Display"] = "Clocks",
+        ["IsInteger"] = true,
+        ["Icon"] = "rbxassetid://0",
+        ["Max"] = inf
+    },
+    ["Clovers_StPatricks2024"] = {
+        ["Id"] = "Clovers_StPatricks2024",
+        ["IsPrimary"] = false,
+        ["ShowInLeaderstats"] = "function: 0xd04ef56eb890c136",
+        ["Default"] = 0,
+        ["Order"] = 5,
+        ["Display"] = "Clovers",
+        ["IsInteger"] = true,
+        ["Icon"] = "rbxassetid://0",
+        ["Max"] = inf
+    },
+    ["Candy_Halloween2023"] = {
+        ["Id"] = "Candy_Halloween2023",
+        ["IsPrimary"] = false,
+        ["ShowInLeaderstats"] = "function: 0xb3aadbeadd5c92d6",
+        ["Default"] = 0,
+        ["Order"] = 4,
+        ["Display"] = "Candy",
+        ["IsInteger"] = true,
+        ["Icon"] = "rbxassetid://0",
+        ["Max"] = inf
+    },
+    ["Eggs_Easter2024"] = {
+        ["Id"] = "Eggs_Easter2024",
+        ["IsPrimary"] = false,
+        ["ShowInLeaderstats"] = "function: 0xbece1eb4277c8af6",
+        ["Default"] = 0,
+        ["Order"] = 6,
+        ["Display"] = "Eggs",
+        ["IsInteger"] = true,
+        ["Icon"] = "rbxassetid://0",
+        ["Max"] = inf
+    },
+    ["Hearts_Valentines2024"] = {
+        ["Id"] = "Hearts_Valentines2024",
+        ["IsPrimary"] = false,
+        ["ShowInLeaderstats"] = "function: 0xdcd137b80930dc16",
+        ["Default"] = 0,
+        ["Order"] = 5,
+        ["Display"] = "Hearts",
+        ["IsInteger"] = true,
+        ["Icon"] = "rbxassetid://0",
+        ["Max"] = inf
+    },
+    ["Gems"] = {
+        ["Id"] = "Gems",
+        ["IsPrimary"] = false,
+        ["ShowInLeaderstats"] = "function: 0xfeebcd3c0c8cf2b6",
+        ["Default"] = 0,
+        ["Order"] = 3,
+        ["Display"] = "Gems",
+        ["IsInteger"] = true,
+        ["Icon"] = "rbxassetid://14624788198",
+        ["Max"] = 10000000
+    }
+}
