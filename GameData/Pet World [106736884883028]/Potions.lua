176e69b4fc5e6c40d33118f3502b9a429196ad79
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:42:57 +1100

    Create Potions.lua

---
+++ GameData/Pet World [106736884883028]/Potions.lua
@@ -0,0 +1,79 @@
+data = {
+    ["XP Potion"] = {
+        ["Index"] = 7,
+        ["Color"] = Color3.fromRGB(255, 185, 100),
+        ["DisplayName"] = "XP Potion",
+        ["SimpleDisplayName"] = "XP Potion",
+        ["Name"] = "XP Potion",
+        ["Rarity"] = "Godly",
+        ["Description"] = "Gives a pet of your choice +40,000 XP!",
+        ["XP"] = 40000,
+        ["SimpleDescription"] = "Gives a pet of your choice +40,000 XP!"
+    },
+    ["Strength Potion"] = {
+        ["Index"] = 6,
+        ["Color"] = Color3.fromRGB(255, 25, 125),
+        ["Duration"] = 600,
+        ["DisplayName"] = "Strength Potion",
+        ["SimpleDisplayName"] = "Strength Potion",
+        ["Name"] = "Strength Potion",
+        ["Rarity"] = "Godly",
+        ["Description"] = "Doubles strength for 10 minutes!",
+        ["SimpleDescription"] = "Double strength for 10 mins!"
+    },
+    ["Star Potion"] = {
+        ["Index"] = 2,
+        ["Color"] = Color3.fromRGB(45, 155, 255),
+        ["Duration"] = 600,
+        ["DisplayName"] = "Stars Potion",
+        ["SimpleDisplayName"] = "Stars Potion",
+        ["Name"] = "Star Potion",
+        ["Rarity"] = "Rare",
+        ["Description"] = "+50% stars for 10 minutes!",
+        ["SimpleDescription"] = "+50% stars for 10 mins"
+    },
+    ["Super Potion"] = {
+        ["Index"] = 5,
+        ["Color"] = Color3.fromRGB(255, 125, 255),
+        ["Duration"] = 300,
+        ["DisplayName"] = "Super Potion",
+        ["SimpleDisplayName"] = "Super Potion",
+        ["Name"] = "Super Potion",
+        ["Rarity"] = "Godly",
+        ["Description"] = "+75% Coins + Stars, and 20% more luck for 5 minutes!",
+        ["SimpleDescription"] = "+75% Coins + Stars and 20% more luck for 5 mins"
+    },
+    ["Luck Potion"] = {
+        ["Index"] = 4,
+        ["Color"] = Color3.fromRGB(45, 255, 85),
+        ["Duration"] = 600,
+        ["DisplayName"] = "Lucky Potion",
+        ["SimpleDisplayName"] = "Lucky Potion",
+        ["Name"] = "Luck Potion",
+        ["Rarity"] = "Rare",
+        ["Description"] = "Increases your luck by 40% for 10 minutes!",
+        ["SimpleDescription"] = "40% more luck for 10 mins"
+    },
+    ["XP Boost Potion"] = {
+        ["Index"] = 3,
+        ["Color"] = Color3.fromRGB(125, 255, 255),
+        ["Duration"] = 600,
+        ["DisplayName"] = "XP Boost Potion",
+        ["SimpleDisplayName"] = "XP Boost Potion",
+        ["Name"] = "XP Boost Potion",
+        ["Rarity"] = "Rare",
+        ["Description"] = "Doubles XP for 10 minutes!",
+        ["SimpleDescription"] = "Double XP for 10 mins"
+    },
+    ["Coin Potion"] = {
+        ["Index"] = 1,
+        ["Color"] = Color3.fromRGB(255, 255, 85),
+        ["Duration"] = 600,
+        ["DisplayName"] = "Coins Potion",
+        ["SimpleDisplayName"] = "Coins Potion",
+        ["Name"] = "Coin Potion",
+        ["Rarity"] = "Rare",
+        ["Description"] = "+50% coins for 10 minutes!",
+        ["SimpleDescription"] = "+50% coins for 10 mins"
+    }
+}
