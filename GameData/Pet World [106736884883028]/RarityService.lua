92353a0bb8da213498ce0f8502ad7674f3010421
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:40:25 +1100

    Create RarityService.lua

---
+++ GameData/Pet World [106736884883028]/RarityService.lua
@@ -0,0 +1,31 @@
+data = {
+    ["Rarities"] = {
+        ["Superstar"] = {
+            ["Color"] = "0 1 0.2 0.2 0 0.0666667 1 0.52 0.2 0 0.133333 1 0.84 0.2 0 0.2 0.84 1 0.2 0 0.266667 0.52 1 0.2 0 0.333333 0.2 1 0.2 0 0.4 0.2 1 0.52 0 0.466667 0.2 1 0.84 0 0.533333 0.2 0.84 1 0 0.6 0.2 0.52 1 0 0.666667 0.2 0.2 1 0 0.733333 0.52 0.2 1 0 0.8 0.84 0.2 1 0 0.866667 1 0.2 0.84 0 0.933333 1 0.2 0.52 0 1 1 0.2 0.2 0 ",
+            ["IsSpecial"] = true,
+            ["StaticColor"] = Color3.fromRGB(255, 85, 255),
+            ["Tier"] = 6
+        },
+        ["Unique"] = {["Color"] = Color3.fromRGB(255, 185, 85), ["Tier"] = 2},
+        ["Basic"] = {["Color"] = Color3.fromRGB(200, 200, 200), ["Tier"] = 1},
+        ["Epic"] = {["Color"] = Color3.fromRGB(255, 125, 255), ["Tier"] = 4},
+        ["Exclusive"] = {
+            ["Color"] = "0 0 1 0.392157 0 0.5 1 1 0.392157 0 1 0 1 0.392157 0 ",
+            ["IsSpecial"] = true,
+            ["StaticColor"] = Color3.fromRGB(0, 255, 135),
+            ["Tier"] = 8
+        },
+        ["Godly"] = {
+            ["Color"] = "0 1 0.2 0.2 0 0.0666667 1 0.52 0.2 0 0.133333 1 0.84 0.2 0 0.2 0.84 1 0.2 0 0.266667 0.52 1 0.2 0 0.333333 0.2 1 0.2 0 0.4 0.2 1 0.52 0 0.466667 0.2 1 0.84 0 0.533333 0.2 0.84 1 0 0.6 0.2 0.52 1 0 0.666667 0.2 0.2 1 0 0.733333 0.52 0.2 1 0 0.8 0.84 0.2 1 0 0.866667 1 0.2 0.84 0 0.933333 1 0.2 0.52 0 1 1 0.2 0.2 0 ",
+            ["IsSpecial"] = true,
+            ["StaticColor"] = Color3.fromRGB(255, 255, 85),
+            ["Tier"] = 5
+        },
+        ["Rare"] = {["Color"] = Color3.fromRGB(100, 255, 255), ["Tier"] = 3}
+    },
+    ["IsSpecial"] = "function: 0xc6fc7bd60c5c36fa",
+    ["GetColor"] = "function: 0xea5e122cf2ec725a",
+    ["GetStaticColor"] = "function: 0x0a38b87e3964ce3a",
+    ["GetTier"] = "function: 0xadd721a845e7e29a",
+    ["GetNameFromTier"] = "function: 0x4e6bfc97ae24897a"
+}
