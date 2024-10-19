5deb36cb6739f5d5f4adc2f2b2d39a4a4bbb8ea3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:49:06 +1100

    Create items.lua

---
+++ GameData/Fisch [16732694052]/items.lua
@@ -0,0 +1,42 @@
+data = {
+    ["Items"] = {
+        ["Windset Totem"] = {["Rarity"] = "Rare", ["Price"] = 2000},
+        ["Conception Conch"] = {["Rarity"] = "Mythical", ["Price"] = 444},
+        ["Glider"] = {["Price"] = 900, ["OnlyBuyOne"] = true, ["Rarity"] = "Rare"},
+        ["Tuna"] = {["Rarity"] = "Developer", ["Price"] = 99999999999999},
+        ["GPS"] = {
+            ["Price"] = 100,
+            ["OnlyBuyOne"] = true,
+            ["Rarity"] = "Uncommon",
+            ["Icon"] = "rbxassetid://92660360174055"
+        },
+        ["Aurora Totem"] = {["Rarity"] = "Mythical", ["Price"] = 30000},
+        ["Sundial Totem"] = {["Rarity"] = "Rare", ["Price"] = 2000},
+        ["Witches Ingredient"] = {["Rarity"] = "Rare", ["Price"] = 10000},
+        ["Crab Cage"] = {["Rarity"] = "Unusual", ["Price"] = 45},
+        ["Firework"] = {["Rarity"] = "Event", ["Price"] = 130},
+        ["Smokescreen Totem"] = {["Rarity"] = "Rare", ["Price"] = 2000},
+        ["Tempest Totem"] = {["Rarity"] = "Rare", ["Price"] = 2000},
+        ["Fish Radar"] = {
+            ["Price"] = 8000,
+            ["OnlyBuyOne"] = true,
+            ["Rarity"] = "Legendary",
+            ["Icon"] = "rbxassetid://132119413174655"
+        }
+    },
+    ["Rarities"] = {"Trash", "Common", "Uncommon", "Unusual", "Rare", "Legendary", "Mythical", "Event", "Developer"},
+    ["ToInteger"] = "function: 0x59481aaa2c5a189f",
+    ["RarityColours"] = {
+        ["Unusual"] = Color3.fromRGB(192, 135, 198),
+        ["Trash"] = Color3.fromRGB(145, 145, 145),
+        ["Common"] = Color3.fromRGB(142, 187, 191),
+        ["Extinct"] = Color3.fromRGB(54, 73, 159),
+        ["Uncommon"] = Color3.fromRGB(161, 255, 169),
+        ["Rare"] = Color3.fromRGB(119, 108, 181),
+        ["Legendary"] = Color3.fromRGB(240, 181, 109),
+        ["Relic"] = Color3.fromRGB(120, 255, 183),
+        ["Event"] = Color3.fromRGB(255, 130, 57),
+        ["Mythical"] = Color3.fromRGB(255, 62, 120)
+    },
+    ["ToHex"] = "function: 0xdff5b9796a940cef"
+}
