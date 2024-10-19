996a4f68eecb5baab65433cd9a721a5890bb1638
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:39:07 +1100

    Create EnchantService.lua

---
+++ GameData/Pet World [106736884883028]/EnchantService.lua
@@ -0,0 +1,15 @@
+data = {
+    ["GiveRandomEnchants"] = "function: 0x70b5b56cd45a227a",
+    ["GetRandomEnchant"] = "function: 0x5086d5bacec676da",
+    ["Enchants"] = {
+        ["Stars"] = {["MaxLevel"] = 5, ["Chance"] = 50},
+        ["AttackSpeed"] = {["MaxLevel"] = 3, ["Chance"] = 15},
+        ["Reach"] = {["MaxLevel"] = 3, ["Chance"] = 15},
+        ["Speed"] = {["MaxLevel"] = 3, ["Chance"] = 15},
+        ["Shard Collector"] = {["MaxLevel"] = 3, ["Chance"] = 15},
+        ["Strength"] = {["MaxLevel"] = 3, ["Chance"] = 10},
+        ["Coins"] = {["MaxLevel"] = 5, ["Chance"] = 50}
+    },
+    ["Initiailize"] = "function: 0x30737ae076cbb3ba",
+    ["GetEnchants"] = "function: 0x902fe06a3c5eff1a"
+}
