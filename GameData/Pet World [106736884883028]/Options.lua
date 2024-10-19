3113200d125b17ce0ed4eb614d4537ba170eb2a3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:41:39 +1100

    Create Options.lua

---
+++ GameData/Pet World [106736884883028]/Options.lua
@@ -0,0 +1,48 @@
+data = {
+    {["Name"] = "Graphics", ["Type"] = "Header"},
+    {["Value"] = "High", ["Type"] = "Switch", ["Name"] = "Quality", ["Options"] = {"Low", "High", "Ultra"}},
+    {
+        ["Confirmation"] = "function: 0x279ba3f0aecb12fa",
+        ["Type"] = "Toggle",
+        ["Name"] = "No Flashing Lights",
+        ["Value"] = false
+    },
+    {
+        ["Confirmation"] = "function: 0x1e7a9ac87f26ae1a",
+        ["Type"] = "Toggle",
+        ["Name"] = "Performance Mode",
+        ["Value"] = false
+    },
+    {["Name"] = "Audio", ["Type"] = "Header"},
+    {
+        ["Type"] = "Number",
+        ["Name"] = "Music",
+        ["Value"] = 100,
+        ["Increment"] = 5,
+        ["Range"] = "0 150 ",
+        ["Format"] = "%s%%"
+    },
+    {
+        ["Type"] = "Number",
+        ["Name"] = "Sound Effects",
+        ["Value"] = 100,
+        ["Increment"] = 5,
+        ["Range"] = "0 150 ",
+        ["Format"] = "%s%%"
+    },
+    {["Name"] = "Pets", ["Type"] = "Header"},
+    {["Value"] = true, ["Type"] = "Toggle", ["Name"] = "Pet Level Popups"},
+    {["Value"] = "All", ["Type"] = "Switch", ["Name"] = "Pets Shown", ["Options"] = {"Own", "All"}},
+    {["Name"] = "Trading", ["Type"] = "Header"},
+    {["Value"] = "All", ["Type"] = "Switch", ["Name"] = "Who Can Trade Me", ["Options"] = {"No One", "Friends", "All"}},
+    {["Name"] = "Notifications", ["Type"] = "Header"},
+    {["Value"] = true, ["Type"] = "Toggle", ["Name"] = "Server Hatch Notifications"},
+    {["Value"] = true, ["Type"] = "Toggle", ["Name"] = "Global Hatch Notifications"},
+    {["Value"] = true, ["Type"] = "Toggle", ["Name"] = "Easy Hatch Notifications"},
+    {["Name"] = "Utilities", ["Type"] = "Header"},
+    {["Value"] = true, ["Type"] = "Toggle", ["Name"] = "Display Tooltips"},
+    {["Value"] = true, ["Type"] = "Toggle", ["Name"] = "Display Item Popups"},
+    {["Value"] = true, ["Type"] = "Toggle", ["Name"] = "Currency Popups"},
+    {["Name"] = "Auto Farm", ["Type"] = "Header"},
+    {["Value"] = false, ["Type"] = "Toggle", ["Name"] = "Ignore Eggs"}
+}
