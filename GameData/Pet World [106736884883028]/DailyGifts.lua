6a8d20aec17012be6045b7154b469a00e2a83b30
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:40:39 +1100

    Create DailyGifts.lua

---
+++ GameData/Pet World [106736884883028]/DailyGifts.lua
@@ -0,0 +1,121 @@
+data = {
+    ["DailyGiftIcons"] = {
+        "rbxassetid://17191317319",
+        "rbxassetid://17191323533",
+        "rbxassetid://17191325377",
+        "rbxassetid://17191326627",
+        "rbxassetid://17191365833"
+    },
+    ["DailyGiftRewards"] = {
+        {
+            ["Rewards"] = {
+                {{"Orb", "Coins", 20}, 90},
+                {{"Orb", "Stars", 1}, 10},
+                {{"Egg", "DailyGifts Egg", 0, true}, 0.002}
+            },
+            ["Amount"] = 40
+        },
+        {
+            ["Rewards"] = {
+                {{"Orb", "Coins", 20}, 90},
+                {{"Orb", "Stars", 1}, 10},
+                {{"Egg", "DailyGifts Egg", 0, true}, 0.002}
+            },
+            ["Amount"] = 42
+        },
+        {
+            ["Rewards"] = {
+                {{"Orb", "Coins", 20}, 90},
+                {{"Orb", "Stars", 1}, 10},
+                {{"Egg", "DailyGifts Egg", 0, true}, 0.002}
+            },
+            ["Amount"] = 44
+        },
+        {
+            ["Rewards"] = {
+                {{"Orb", "Coins", 20}, 90},
+                {{"Orb", "Stars", 1}, 10},
+                {{"Egg", "DailyGifts Egg", 0, true}, 0.002}
+            },
+            ["Amount"] = 46
+        },
+        {
+            ["Rewards"] = {
+                {{"Orb", "Coins", 20}, 90},
+                {{"Orb", "Stars", 1}, 10},
+                {{"Egg", "DailyGifts Egg", 0, true}, 0.002}
+            },
+            ["Amount"] = 50
+        },
+        {
+            ["Rewards"] = {
+                {{"Orb", "Coins", 20}, 90},
+                {{"Orb", "Stars", 1}, 10},
+                {{"Egg", "DailyGifts Egg", 0, true}, 0.002}
+            },
+            ["Amount"] = 55
+        },
+        {
+            ["Rewards"] = {
+                {{"Orb", "Coins", 20}, 90},
+                {{"Orb", "Stars", 1}, 10},
+                {{"Egg", "DailyGifts Egg", 0, true}, 0.002}
+            },
+            ["Amount"] = 60
+        },
+        {
+            ["Rewards"] = {
+                {{"Orb", "Coins", 20}, 90},
+                {{"Orb", "Stars", 1}, 10},
+                {{"Egg", "DailyGifts Egg", 0, true}, 0.002}
+            },
+            ["Amount"] = 70
+        },
+        {
+            ["Rewards"] = {
+                {{"Orb", "Coins", 20}, 90},
+                {{"Orb", "Stars", 1}, 10},
+                {{"Egg", "DailyGifts Egg", 0, true}, 0.002}
+            },
+            ["Amount"] = 80
+        },
+        {
+            ["Rewards"] = {
+                {{"Orb", "Coins", 20}, 90},
+                {{"Orb", "Stars", 1}, 10},
+                {{"Egg", "DailyGifts Egg", 0, true}, 0.002}
+            },
+            ["Amount"] = 100
+        },
+        {
+            ["Rewards"] = {
+                {{"Orb", "Coins", 20}, 90},
+                {{"Orb", "Stars", 1}, 10},
+                {{"Egg", "DailyGifts Egg", 0, true}, 0.002}
+            },
+            ["Amount"] = 125
+        },
+        {
+            ["Rewards"] = {
+                {{"Orb", "Coins", 20}, 90},
+                {{"Orb", "Stars", 1}, 10},
+                {{"Egg", "DailyGifts Egg", 0, true}, 0.002}
+            },
+            ["Amount"] = 150
+        }
+    },
+    ["DailyGiftConfigs"] = {
+        {5, 1},
+        {10, 1},
+        {15, 1},
+        {20, 1},
+        {30, 2},
+        {40, 2},
+        {60, 3},
+        {80, 3},
+        {120, 4},
+        {160, 4},
+        {240, 5},
+        {320, 5}
+    }
+}
