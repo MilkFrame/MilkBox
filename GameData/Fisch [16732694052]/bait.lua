41400084ddd01e3ea88fd7794085d11b715c6a51
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:48:19 +1100

    Create bait.lua

---
+++ GameData/Fisch [16732694052]/bait.lua
@@ -0,0 +1,63 @@
+data = {
+    ["Magnet"] = {["LureSpeed"] = 0, ["Luck"] = 200, ["GenerelLuck"] = 0, ["Rarity"] = "Unusual", ["Resilience"] = 0},
+    ["Worm"] = {["LureSpeed"] = 15, ["Luck"] = 25, ["GenerelLuck"] = 0, ["Rarity"] = "Common", ["Resilience"] = 0},
+    ["Coal"] = {["LureSpeed"] = 0, ["Luck"] = 45, ["GenerelLuck"] = 0, ["Rarity"] = "Rare", ["Resilience"] = -10},
+    ["Squid"] = {["LureSpeed"] = -25, ["Luck"] = 55, ["GenerelLuck"] = 45, ["Rarity"] = "Unusual", ["Resilience"] = 0},
+    ["Shrimp"] = {["LureSpeed"] = 0, ["Luck"] = 45, ["GenerelLuck"] = 25, ["Rarity"] = "Uncommon", ["Resilience"] = -5},
+    ["Insect"] = {["LureSpeed"] = 5, ["Luck"] = 35, ["GenerelLuck"] = 0, ["Rarity"] = "Common", ["Resilience"] = 0},
+    ["Super Flakes"] = {
+        ["LureSpeed"] = 0,
+        ["Luck"] = 0,
+        ["GenerelLuck"] = 70,
+        ["Rarity"] = "Rare",
+        ["Resilience"] = -15
+    },
+    ["Seaweed"] = {["LureSpeed"] = 20, ["Luck"] = 35, ["GenerelLuck"] = 0, ["Rarity"] = "Unusual", ["Resilience"] = 10},
+    ["Instant Catcher"] = {
+        ["LureSpeed"] = 65,
+        ["Luck"] = 0,
+        ["GenerelLuck"] = -20,
+        ["Rarity"] = "Legendary",
+        ["Resilience"] = -15
+    },
+    ["Weird Algae"] = {
+        ["LureSpeed"] = -35,
+        ["Luck"] = 0,
+        ["GenerelLuck"] = 200,
+        ["Rarity"] = "Legendary",
+        ["Resilience"] = 0
+    },
+    ["Minnow"] = {["LureSpeed"] = 0, ["Luck"] = 65, ["GenerelLuck"] = 0, ["Rarity"] = "Unusual", ["Resilience"] = -10},
+    ["Give"] = "function: 0xf11becd41b4b99ff",
+    ["Fish Head"] = {
+        ["LureSpeed"] = 10,
+        ["Luck"] = 150,
+        ["GenerelLuck"] = 0,
+        ["Rarity"] = "Legendary",
+        ["Resilience"] = -10
+    },
+    ["Maggot"] = {["LureSpeed"] = -10, ["Luck"] = 0, ["GenerelLuck"] = 35, ["Rarity"] = "Uncommon", ["Resilience"] = 0},
+    ["Night Shrimp"] = {
+        ["LureSpeed"] = 15,
+        ["Luck"] = 0,
+        ["GenerelLuck"] = 90,
+        ["Rarity"] = "Legendary",
+        ["Resilience"] = 0
+    },
+    ["Rapid Catcher"] = {
+        ["LureSpeed"] = 35,
+        ["Luck"] = 0,
+        ["GenerelLuck"] = 0,
+        ["Rarity"] = "Rare",
+        ["Resilience"] = -15
+    },
+    ["Flakes"] = {["LureSpeed"] = 10, ["Luck"] = 55, ["GenerelLuck"] = 0, ["Rarity"] = "Common", ["Resilience"] = -3},
+    ["Bagel"] = {["LureSpeed"] = 0, ["Luck"] = 25, ["GenerelLuck"] = 0, ["Rarity"] = "Common", ["Resilience"] = 15},
+    ["Truffle Worm"] = {
+        ["LureSpeed"] = -10,
+        ["Luck"] = 300,
+        ["GenerelLuck"] = 0,
+        ["Rarity"] = "Legendary",
+        ["Resilience"] = 0
+    }
+}
