0202db002e397eba3ec977e944cd6ff1db7b0fe9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:41:07 +1100

    Create ZoneLighting.lua

---
+++ GameData/Pet World [106736884883028]/ZoneLighting.lua
@@ -0,0 +1,18 @@
+data = {
+    ["Wizard Tower"] = {
+        ["ClockTime"] = 0,
+        ["ColorShift_Bottom"] = Color3.fromRGB(0, 0, 0),
+        ["FogColor"] = Color3.fromRGB(150, 0, 255),
+        ["FogEnd"] = 350,
+        ["EnvironmentDiffuseScale"] = 0,
+        ["EnvironmentSpecularScale"] = 0.25,
+        ["FogStart"] = 25,
+        ["GeographicLatitude"] = 50,
+        ["Ambient"] = Color3.fromRGB(80, 80, 80),
+        ["OutdoorAmbient"] = Color3.fromRGB(155, 155, 255),
+        ["ExposureCompensation"] = 0.2,
+        ["Brightness"] = 1,
+        ["ColorShift_Top"] = Color3.fromRGB(135, 145, 155),
+        ["ShadowSoftness"] = 0.2
+    }
+}
