f5bc39d6de1c6ae23b5816280b736ad1d8a0efe6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:42:18 +1100

    Create Hoverboards.lua

---
+++ GameData/Pet World [106736884883028]/Hoverboards.lua
@@ -0,0 +1,18 @@
+data = {
+    ["Default"] = {
+        ["Configuration"] = {["Movement"] = {["Speed"] = 60}},
+        ["Name"] = "Default Hoverboard",
+        ["Object"] = "Default Hoverboard"
+    },
+    ["Skateboard"] = {
+        ["Configuration"] = {
+            ["Rotation"] = {["Limit"] = 30, ["RollLimit"] = 0},
+            ["Misc"] = {["Gravity"] = 220, ["HoverHeight"] = 0.4, ["PlayerOffset"] = 0.55},
+            ["Movement"] = {["Speed"] = 60},
+            ["Jumping"] = {["SpeedBoost"] = 0.4, ["Power"] = 0.4},
+            ["Bobbing"] = {["Rate"] = 0}
+        },
+        ["Name"] = "Skateboard",
+        ["Object"] = "Skateboard"
+    }
+}
