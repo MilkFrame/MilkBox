0e09744f7d0ee1b261a0b823148820377f38e2c8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:40:08 +1100

    Create CraftingService.lua

---
+++ GameData/Pet World [106736884883028]/CraftingService.lua
@@ -0,0 +1,76 @@
+data = {
+    ["RequestCraft"] = "function: 0x8bcbf9d5e096911a",
+    ["CanCraftItem"] = "function: 0x2d6e22704513dafa",
+    ["Recipes"] = {
+        ["XP Boost Potion"] = {
+            ["Recipe"] = {
+                {["Name"] = "Coins", ["Amount"] = 80000},
+                {["Name"] = "Stars", ["Amount"] = 3500},
+                {["Name"] = "Shards", ["Amount"] = 30},
+                {["Locked"] = false, ["Quantity"] = 4, ["Name"] = "Apple"}
+            },
+            ["Duration"] = 600,
+            ["Item"] = {["Name"] = "XP Boost Potion", ["Quantity"] = 1},
+            ["Index"] = 6
+        },
+        ["Star Potion"] = {
+            ["Recipe"] = {
+                {["Name"] = "Coins", ["Amount"] = 50000},
+                {["Name"] = "Stars", ["Amount"] = 2500},
+                {["Name"] = "Shards", ["Amount"] = 20}
+            },
+            ["Duration"] = 300,
+            ["Item"] = {["Name"] = "Star Potion", ["Quantity"] = 1},
+            ["Index"] = 4
+        },
+        ["Coin Potion"] = {
+            ["Recipe"] = {
+                {["Name"] = "Coins", ["Amount"] = 50000},
+                {["Name"] = "Stars", ["Amount"] = 2500},
+                {["Name"] = "Shards", ["Amount"] = 20}
+            },
+            ["Duration"] = 300,
+            ["Item"] = {["Name"] = "Coin Potion", ["Quantity"] = 1},
+            ["Index"] = 3
+        },
+        ["Luck Potion"] = {
+            ["Recipe"] = {
+                {["Name"] = "Coins", ["Amount"] = 80000},
+                {["Name"] = "Stars", ["Amount"] = 3500},
+                {["Name"] = "Shards", ["Amount"] = 30},
+                {["Locked"] = false, ["Quantity"] = 4, ["Name"] = "Green Apple"}
+            },
+            ["Duration"] = 480,
+            ["Item"] = {["Name"] = "Luck Potion", ["Quantity"] = 1},
+            ["Index"] = 5
+        },
+        ["Super Potion"] = {
+            ["Recipe"] = {
+                {["Name"] = "Stars", ["Amount"] = 10000},
+                {["Name"] = "Shards", ["Amount"] = 50},
+                {["Locked"] = false, ["Quantity"] = 12, ["Name"] = "Strawberry"},
+                {["Locked"] = false, ["Quantity"] = 6, ["Name"] = "Pineapple"}
+            },
+            ["Duration"] = 900,
+            ["Item"] = {["Name"] = "Super Potion", ["Quantity"] = 1},
+            ["Index"] = 1
+        },
+        ["XP Potion"] = {
+            ["Recipe"] = {
+                {["Name"] = "Coins", ["Amount"] = 50000},
+                {["Name"] = "Stars", ["Amount"] = 5000},
+                {["Name"] = "Shards", ["Amount"] = 40},
+                {["Locked"] = false, ["Quantity"] = 4, ["Name"] = "Raspberry"}
+            },
+            ["Duration"] = 900,
+            ["Item"] = {["Name"] = "XP Potion", ["Quantity"] = 1},
+            ["Index"] = 2
+        }
+    },
+    ["CraftItem"] = "function: 0xcce8bcca6dff5e5a",
+    ["ItemLimit"] = 5,
+    ["Initialize"] = "function: 0xec491a1c900d1a3a",
+    ["ClaimCraft"] = "function: 0x0c0570429e95d69a",
+    ["StartCraft"] = "function: 0x53fc1ab76734d57a",
+    ["GetItemCraftTime"] = "function: 0x745f85d34fb38dda"
+}
