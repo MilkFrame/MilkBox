98ba63c6a9700d2033eaef50a654707dc9b37f26
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:44:08 +1100

    Create CustomEggs.lua

---
+++ GameData/Pet World [106736884883028]/CustomEggs.lua
@@ -0,0 +1,17 @@
+data = {
+    ["DailyGifts Egg"] = {
+        ["Content"] = {
+            {"Present Cat", 0.002},
+            {"Present Dog", 0.002},
+            {"Present Fox", 0.002},
+            {"Present Bee", 0.002},
+            {"Present Racoon", 0.002},
+            {"Present Unicorn", 0.002},
+            {"Present Dragon", 0.002},
+            {"Present Monkey", 0.002}
+        },
+        ["Type"] = "Exclusive Egg",
+        ["Index"] = -1,
+        ["Description"] = ""
+    }
+}
