8c3ae6928e57192c0675fa304ba44390f6246554
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:39:36 +1100

    Create MerchantService.lua

---
+++ GameData/Pet World [106736884883028]/MerchantService.lua
@@ -0,0 +1,139 @@
+data = {
+    ["GetTimerSpeed"] = "function: 0x05fb446b6e84a3fa",
+    ["GetShopItems"] = "function: 0xa397c63f301d4f5a",
+    ["GetTimeUntilRestock"] = "function: 0x5d4a5812ff8d313a",
+    ["Items"] = {
+        {
+            ["Quantity"] = 6,
+            ["Name"] = "Lucky Potion",
+            ["Cost"] = 60,
+            ["Color"] = Color3.fromRGB(45, 255, 85),
+            ["Description"] = "40% more luck for 10 mins",
+            ["Icon"] = "rbxassetid://18274480791",
+            ["Reward"] = {
+                "Potions",
+                {["Id"] = "1oft7xbvpefidvp6", ["Quantity"] = 1, ["Name"] = "Luck Potion"},
+                {["Name"] = "Luck Potion", ["Quantity"] = 1}
+            }
+        },
+        {
+            ["Quantity"] = 6,
+            ["Name"] = "Coins Potion",
+            ["Cost"] = 60,
+            ["Color"] = Color3.fromRGB(255, 255, 85),
+            ["Description"] = "+50% coins for 10 mins",
+            ["Icon"] = "rbxassetid://18274481559",
+            ["Reward"] = {
+                "Potions",
+                {["Id"] = "1o8b0b1gj6ecfxyb", ["Quantity"] = 1, ["Name"] = "Coin Potion"},
+                {["Name"] = "Coin Potion", ["Quantity"] = 1}
+            }
+        },
+        {
+            ["Quantity"] = 2,
+            ["Name"] = "Super Potion",
+            ["Cost"] = 150,
+            ["Color"] = Color3.fromRGB(255, 125, 255),
+            ["Description"] = "+75% Coins + Stars and 20% more luck for 5 mins",
+            ["Icon"] = "rbxassetid://18274480238",
+            ["Reward"] = {
+                "Potions",
+                {["Id"] = "1q0nxp76cvd8sxv0", ["Quantity"] = 1, ["Name"] = "Super Potion"},
+                {["Name"] = "Super Potion", ["Quantity"] = 1}
+            }
+        },
+        {
+            ["Quantity"] = 6,
+            ["Name"] = "XP Boost Potion",
+            ["Cost"] = 80,
+            ["Color"] = Color3.fromRGB(125, 255, 255),
+            ["Description"] = "Double XP for 10 mins",
+            ["Icon"] = "rbxassetid://18274481169",
+            ["Reward"] = {
+                "Potions",
+                {["Id"] = "1o8xgbogg6dosxah", ["Quantity"] = 1, ["Name"] = "XP Boost Potion"},
+                {["Name"] = "XP Boost Potion", ["Quantity"] = 1}
+            }
+        },
+        {
+            ["Quantity"] = 6,
+            ["Name"] = "Stars Potion",
+            ["Cost"] = 60,
+            ["Color"] = Color3.fromRGB(45, 155, 255),
+            ["Description"] = "+50% stars for 10 mins",
+            ["Icon"] = "rbxassetid://18274480467",
+            ["Reward"] = {
+                "Potions",
+                {["Id"] = "2d0p12xgve0i1069", ["Quantity"] = 1, ["Name"] = "Star Potion"},
+                {["Name"] = "Star Potion", ["Quantity"] = 1}
+            }
+        },
+        {
+            ["Quantity"] = 4,
+            ["Name"] = "Strength Potion",
+            ["Cost"] = 80,
+            ["Color"] = Color3.fromRGB(255, 25, 125),
+            ["Description"] = "Double strength for 10 mins!",
+            ["Icon"] = "rbxassetid://71140542510366",
+            ["Reward"] = {
+                "Potions",
+                {["Id"] = "1gjm6xbffzf1a10d", ["Quantity"] = 1, ["Name"] = "Strength Potion"},
+                {["Name"] = "Strength Potion", ["Quantity"] = 1}
+            }
+        },
+        {
+            ["Quantity"] = 1,
+            ["Name"] = "XP Potion",
+            ["Cost"] = 240,
+            ["Color"] = Color3.fromRGB(255, 185, 100),
+            ["Description"] = "Gives a pet of your choice +40,000 XP!",
+            ["Icon"] = "rbxassetid://18336038098",
+            ["Reward"] = {
+                "Potions",
+                {["Id"] = "1646mcjuy3ebh10v", ["Quantity"] = 1, ["Name"] = "XP Potion"},
+                {["Name"] = "XP Potion", ["Quantity"] = 1}
+            }
+        },
+        {
+            ["Quantity"] = 1,
+            ["Name"] = "Red Dragon",
+            ["Cost"] = 250000,
+            ["Color"] = Color3.fromRGB(255, 80, 0),
+            ["Description"] = "This pet will always 100% as good as your best pet!",
+            ["Icon"] = "rbxassetid://120467427694809",
+            ["Reward"] = {
+                "Pets",
+                {
+                    ["EnchantInfo"] = {},
+                    ["Id"] = "1c2y4d210iyeuywq",
+                    ["TotalXp"] = 0,
+                    ["Xp"] = 0,
+                    ["Name"] = "Red Dragon",
+                    ["Tier"] = "Normal",
+                    ["Locked"] = false,
+                    ["Level"] = 0,
+                    ["Equipped"] = false,
+                    ["Modifiers"] = {}
+                },
+                {
+                    ["EnchantInfo"] = {},
+                    ["TotalXp"] = 0,
+                    ["Xp"] = 0,
+                    ["Name"] = "Red Dragon",
+                    ["Tier"] = "Normal",
+                    ["Locked"] = false,
+                    ["Level"] = 0,
+                    ["Equipped"] = false,
+                    ["Modifiers"] = {}
+                }
+            }
+        }
+    },
+    ["Currency"] = "Shards",
+    ["Initialize"] = "function: 0x7eecb658a702e99a",
+    ["GetShopVariation"] = "function: 0x9f6dd4858087c67a",
+    ["PurchaseItem"] = "function: 0x3db165cda6037ada",
+    ["CanPurchaseItem"] = "function: 0xc3802119ddbebfba",
+    ["UpdatePlayer"] = "function: 0xe473ab30510dfb1a",
+    ["RestockTime"] = 3600
+}
