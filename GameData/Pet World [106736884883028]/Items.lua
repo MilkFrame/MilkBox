ac4df5090092bb7f147b22402484614b3c467e09
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:43:11 +1100

    Create Items.lua

---
+++ GameData/Pet World [106736884883028]/Items.lua
@@ -0,0 +1,201 @@
+data = {
+    ["SilverOre"] = {
+        ["DisableUsing"] = true,
+        ["Description"] = "Can be used to craft items or be exchanged at the Crystal Merchant!",
+        ["Value"] = {["Stars"] = 4, ["Coins"] = 6},
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Rare",
+        ["DisplayName"] = "Platinum Ore"
+    },
+    ["RoseGoldOre"] = {
+        ["DisableUsing"] = true,
+        ["Description"] = "Can be used to craft items or be exchanged at the Crystal Merchant!",
+        ["Value"] = {["Shards"] = 6, ["Coins"] = 12},
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Epic",
+        ["DisplayName"] = "Rose Gold Ore"
+    },
+    ["TopazOre"] = {
+        ["DisableUsing"] = true,
+        ["Description"] = "Can be used to craft items or be exchanged at the Crystal Merchant!",
+        ["Value"] = {["Shards"] = 10, ["Coins"] = 80},
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Epic",
+        ["DisplayName"] = "Topaz Ore"
+    },
+    ["Cotton Candy"] = {
+        ["Rarity"] = "Rare",
+        ["Description"] = "Boosts pet attack speed for 2 minutes!",
+        ["DisplayName"] = "Cotton Candy"
+    },
+    ["Popcorn"] = {
+        ["Rarity"] = "Rare",
+        ["Description"] = "Boosts pet attack speed for 2 minutes!",
+        ["DisplayName"] = "Popcorn"
+    },
+    ["VoidStoneOre"] = {
+        ["DisableUsing"] = true,
+        ["Description"] = "Can be used to craft items or be exchanged at the Crystal Merchant!",
+        ["Value"] = {["Stars"] = 500, ["Shards"] = 120, ["Coins"] = 800},
+        ["IconSize"] = 1,
+        ["Rarity"] = "Godly",
+        ["DisplayName"] = "Void Stone Ore"
+    },
+    ["GoldOre"] = {
+        ["DisableUsing"] = true,
+        ["Description"] = "Can be used to craft items or be exchanged at the Crystal Merchant!",
+        ["Value"] = {["Coins"] = 16},
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Rare",
+        ["DisplayName"] = "Gold Ore"
+    },
+    ["JadeOre"] = {
+        ["DisableUsing"] = true,
+        ["Description"] = "Can be used to craft items or be exchanged at the Crystal Merchant!",
+        ["Value"] = {["Coins"] = 20},
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Rare",
+        ["DisplayName"] = "Jade Ore"
+    },
+    ["Apple"] = {
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Rare",
+        ["Description"] = "Boosts pet coin multiplier for 1 minute!.",
+        ["DisplayName"] = "Apple"
+    },
+    ["EmeraldOre"] = {
+        ["DisableUsing"] = true,
+        ["Description"] = "Can be used to craft items or be exchanged at the Crystal Merchant!",
+        ["Value"] = {["Stars"] = 10, ["Coins"] = 10},
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Rare",
+        ["DisplayName"] = "Emerald Ore"
+    },
+    ["Orange"] = {
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Rare",
+        ["Description"] = "Gives your pet +500 xp!",
+        ["DisplayName"] = "Orange"
+    },
+    ["BronzeOre"] = {
+        ["DisableUsing"] = true,
+        ["Description"] = "Can be used to craft items or be exchanged at the Crystal Merchant!",
+        ["Value"] = {["Coins"] = 12},
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Rare",
+        ["DisplayName"] = "Bronze Ore"
+    },
+    ["OpalOre"] = {
+        ["DisableUsing"] = true,
+        ["Description"] = "Can be used to craft items or be exchanged at the Crystal Merchant!",
+        ["Value"] = {["Stars"] = 40, ["Coins"] = 40},
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Epic",
+        ["DisplayName"] = "Opal Ore"
+    },
+    ["RedstoneOre"] = {
+        ["DisableUsing"] = true,
+        ["Description"] = "Can be used to craft items or be exchanged at the Crystal Merchant!",
+        ["Value"] = {["Stars"] = 20, ["Shards"] = 50, ["Coins"] = 250},
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Godly",
+        ["DisplayName"] = "Redstone Ore"
+    },
+    ["Raspberry"] = {
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Rare",
+        ["Description"] = "Gives your pet +1,000 xp!",
+        ["DisplayName"] = "Raspberry"
+    },
+    ["Grapes"] = {
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Rare",
+        ["Description"] = "Gives your pet +800 xp!",
+        ["DisplayName"] = "Grapes"
+    },
+    ["SaphireOre"] = {
+        ["DisableUsing"] = true,
+        ["Description"] = "Can be used to craft items or be exchanged at the Crystal Merchant!",
+        ["Value"] = {["Stars"] = 20},
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Rare",
+        ["DisplayName"] = "Saphire Ore"
+    },
+    ["Pineapple"] = {
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Rare",
+        ["Description"] = "Gives your pet +5,000 xp!",
+        ["DisplayName"] = "Pineapple"
+    },
+    ["PlatinumOre"] = {
+        ["DisableUsing"] = true,
+        ["Description"] = "Can be used to craft items or be exchanged at the Crystal Merchant!",
+        ["Value"] = {["Stars"] = 8, ["Coins"] = 12},
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Epic",
+        ["DisplayName"] = "Platinum Ore"
+    },
+    ["AmethystOre"] = {
+        ["DisableUsing"] = true,
+        ["Description"] = "Can be used to craft items or be exchanged at the Crystal Merchant!",
+        ["Value"] = {["Shards"] = 10},
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Rare",
+        ["DisplayName"] = "Amethyst Ore"
+    },
+    ["Spin Ticket"] = {
+        ["DisableUsing"] = true,
+        ["Description"] = "Can be used to spin the wheel in the Crazy Carnival!",
+        ["DisableLocking"] = true,
+        ["IconSize"] = 0.85,
+        ["Rarity"] = "Godly",
+        ["DisplayName"] = "Spin Ticket"
+    },
+    ["RubyOre"] = {
+        ["DisableUsing"] = true,
+        ["Description"] = "Can be used to craft items or be exchanged at the Crystal Merchant!",
+        ["Value"] = {["Shards"] = 4, ["Coins"] = 20},
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Rare",
+        ["DisplayName"] = "Ruby Ore"
+    },
+    ["Banana"] = {
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Rare",
+        ["Description"] = "Gives your pet +650 xp!",
+        ["DisplayName"] = "Banana"
+    },
+    ["Strawberry"] = {
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Rare",
+        ["Description"] = "Gives your pet +2,500 xp!",
+        ["DisplayName"] = "Strawberry"
+    },
+    ["AquamarineOre"] = {
+        ["DisableUsing"] = true,
+        ["Description"] = "Can be used to craft items or be exchanged at the Crystal Merchant!",
+        ["Value"] = {["Stars"] = 60},
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Epic",
+        ["DisplayName"] = "Aquamarine Ore"
+    },
+    ["Green Apple"] = {
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Rare",
+        ["Description"] = "Boosts pet star multiplier for 1 minute!.",
+        ["DisplayName"] = "Green Apple"
+    },
+    ["CoalOre"] = {
+        ["DisableUsing"] = true,
+        ["Description"] = "Can be used to craft items or be exchanged at the Crystal Merchant!",
+        ["Value"] = {["Stars"] = 4, ["Coins"] = 4},
+        ["IconSize"] = 0.8,
+        ["Rarity"] = "Unique",
+        ["DisplayName"] = "Coal Ore"
+    },
+    ["Clan Token"] = {
+        ["Rarity"] = "Godly",
+        ["Description"] = "Can be used to create a clan!",
+        ["DisplayName"] = "Clan Token"
+    }
+}
