4188dea0ffff27bc0067c8e8c077d4c7e649cd34
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:45:39 +1100

    Create mutations.lua

---
+++ GameData/Fisch [16732694052]/mutations.lua
@@ -0,0 +1,89 @@
+data = {
+    ["MutateModel"] = "function: 0xccd3898baf3fad9f",
+    ["Mutations"] = {
+        ["Blessed"] = {
+            ["PriceMultiply"] = 10,
+            ["Color"] = Color3.fromRGB(255, 230, 105),
+            ["Display"] = "Blessed",
+            ["Chance"] = 0
+        },
+        ["Electric"] = {
+            ["PriceMultiply"] = 1.45,
+            ["Color"] = Color3.fromRGB(255, 245, 99),
+            ["Display"] = "Electric",
+            ["Chance"] = 15
+        },
+        ["Darkened"] = {
+            ["PriceMultiply"] = 1.3,
+            ["Color"] = Color3.fromRGB(58, 61, 62),
+            ["Display"] = "Darkened",
+            ["Chance"] = 30
+        },
+        ["Negative"] = {
+            ["PriceMultiply"] = 1.3,
+            ["Color"] = Color3.fromRGB(117, 103, 226),
+            ["Display"] = "Negative",
+            ["Chance"] = 30
+        },
+        ["Sandy"] = {
+            ["PriceMultiply"] = 1.2,
+            ["Color"] = Color3.fromRGB(255, 195, 161),
+            ["Display"] = "Sandy",
+            ["Chance"] = 0
+        },
+        ["Ghastly"] = {
+            ["PriceMultiply"] = 2,
+            ["Color"] = Color3.fromRGB(97, 255, 94),
+            ["Display"] = "Ghastly",
+            ["Chance"] = 35
+        },
+        ["Silver"] = {
+            ["PriceMultiply"] = 1.6,
+            ["Color"] = Color3.fromRGB(206, 238, 255),
+            ["Display"] = "Silver",
+            ["Chance"] = 15
+        },
+        ["Frozen"] = {
+            ["PriceMultiply"] = 1.3,
+            ["Color"] = Color3.fromRGB(131, 255, 230),
+            ["Display"] = "Frozen",
+            ["Chance"] = 10
+        },
+        ["Glossy"] = {
+            ["PriceMultiply"] = 1.6,
+            ["Color"] = Color3.fromRGB(146, 226, 255),
+            ["Display"] = "Glossy",
+            ["Chance"] = 25
+        },
+        ["Mythical"] = {
+            ["PriceMultiply"] = 4.5,
+            ["Color"] = Color3.fromRGB(255, 82, 148),
+            ["Display"] = "Mythical",
+            ["Chance"] = 1
+        },
+        ["Midas"] = {
+            ["PriceMultiply"] = 2,
+            ["Color"] = Color3.fromRGB(255, 154, 71),
+            ["Display"] = "Midas",
+            ["Chance"] = 3
+        },
+        ["Translucent"] = {
+            ["PriceMultiply"] = 1.3,
+            ["Color"] = Color3.fromRGB(135, 255, 191),
+            ["Display"] = "Translucent",
+            ["Chance"] = 20
+        },
+        ["Sinister"] = {
+            ["PriceMultiply"] = 1.9,
+            ["Color"] = Color3.fromRGB(200, 88, 44),
+            ["Display"] = "Sinister",
+            ["Chance"] = 40
+        },
+        ["Albino"] = {
+            ["PriceMultiply"] = 1.1,
+            ["Color"] = Color3.fromRGB(252, 254, 255),
+            ["Display"] = "Albino",
+            ["Chance"] = 30
+        }
+    }
+}
