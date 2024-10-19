c99e41338884242b87cab7621dec71175fb40465
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:49:28 +1100

    Create locations.lua

---
+++ GameData/Fisch [16732694052]/locations.lua
@@ -0,0 +1,90 @@
+data = {
+    ["Roslit"] = {
+        ["Bobber"] = "Fake Pufferfish",
+        ["Name"] = "Roslit Bay",
+        ["Color"] = Color3.fromRGB(255, 160, 64),
+        ["Rewards"] = {["Xp"] = 2000, ["Coins"] = 2000},
+        ["Banner"] = "rbxassetid://18542406497",
+        ["BadgeId"] = 225841574756551
+    },
+    ["Terrapin"] = {
+        ["Bobber"] = "Mini Terrapin",
+        ["Name"] = "Terrapin Island",
+        ["Color"] = Color3.fromRGB(87, 255, 143),
+        ["Rewards"] = {["Xp"] = 2000, ["Coins"] = 2000},
+        ["Banner"] = "rbxassetid://18791315983",
+        ["BadgeId"] = 2994442057572213
+    },
+    ["Roslit Volcano"] = {
+        ["Bobber"] = "Lava",
+        ["Name"] = "Roslit Volcano",
+        ["Color"] = Color3.fromRGB(255, 160, 64),
+        ["Rewards"] = {["Xp"] = 2000, ["Coins"] = 2000},
+        ["Banner"] = "rbxassetid://108037427819021",
+        ["BadgeId"] = 4479912445209985
+    },
+    ["Ocean"] = {
+        ["Bobber"] = "Anchor",
+        ["Name"] = "Ocean",
+        ["Color"] = Color3.fromRGB(161, 199, 255),
+        ["Rewards"] = {["Xp"] = 2000, ["Coins"] = 2000},
+        ["Banner"] = "rbxassetid://18310945663",
+        ["BadgeId"] = 1045860405568363
+    },
+    ["Snowcap"] = {
+        ["Bobber"] = "Frozen",
+        ["Name"] = "Snowcap Island",
+        ["Color"] = Color3.fromRGB(165, 255, 214),
+        ["Rewards"] = {["Xp"] = 2000, ["Coins"] = 2000},
+        ["Banner"] = "rbxassetid://101454347375454",
+        ["BadgeId"] = 4101868524305507
+    },
+    ["Mushgrove"] = {
+        ["Bobber"] = "Mushroom",
+        ["Name"] = "Mushgrove Swamp",
+        ["Color"] = Color3.fromRGB(150, 255, 102),
+        ["Rewards"] = {["Xp"] = 2000, ["Coins"] = 2000},
+        ["Banner"] = "rbxassetid://18952253458",
+        ["BadgeId"] = 4343979385420841
+    },
+    ["Moosewood"] = {
+        ["Bobber"] = "Rubber Ducky",
+        ["Name"] = "Moosewood Island",
+        ["Color"] = Color3.fromRGB(255, 94, 94),
+        ["Rewards"] = {["Xp"] = 2000, ["Coins"] = 2000},
+        ["Banner"] = "rbxassetid://18310954523",
+        ["BadgeId"] = 3871316346306270
+    },
+    ["Keepers Altar"] = {
+        ["Bobber"] = "Skeleton",
+        ["Name"] = "Keepers Altar",
+        ["Color"] = Color3.fromRGB(63, 71, 185),
+        ["Rewards"] = {["Xp"] = 1750, ["Coins"] = 1750},
+        ["Banner"] = "rbxassetid://131504032544120",
+        ["BadgeId"] = 772547227400435
+    },
+    ["All"] = {
+        ["Bobber"] = "Aurora",
+        ["Name"] = "Every",
+        ["Color"] = Color3.fromRGB(255, 255, 255),
+        ["Rewards"] = {["Xp"] = 10000, ["Coins"] = 10000},
+        ["Banner"] = "rbxassetid://18310847126",
+        ["BadgeId"] = 635398639427099
+    },
+    ["Vertigo"] = {
+        ["Bobber"] = "Vertigo Rock",
+        ["Name"] = "Vertigo",
+        ["Color"] = Color3.fromRGB(127, 110, 255),
+        ["Rewards"] = {["Xp"] = 2000, ["Coins"] = 2000},
+        ["Banner"] = "rbxassetid://18952306740",
+        ["BadgeId"] = 3200076862852988
+    },
+    ["Sunstone"] = {
+        ["Bobber"] = "Sun",
+        ["Name"] = "Sunstone Island",
+        ["Color"] = Color3.fromRGB(255, 236, 129),
+        ["Rewards"] = {["Xp"] = 2000, ["Coins"] = 2000},
+        ["Banner"] = "rbxassetid://108032910283682",
+        ["BadgeId"] = 2903347774735091
+    }
+}
