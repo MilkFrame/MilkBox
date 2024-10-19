80a3a1ac8b731373c1d206cccb731ccdd7a9d0c4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:44:20 +1100

    Create Eggs.lua

---
+++ GameData/Pet World [106736884883028]/Eggs.lua
@@ -0,0 +1,337 @@
+data = {
+    ["Crystal Egg"] = {
+        ["Index"] = 15,
+        ["Cost"] = {["Amount"] = 4000000000, ["Currency"] = "Coins"},
+        ["LevelIndex"] = 12,
+        ["Description"] = "(Crystalline Cavern)",
+        ["Content"] = {
+            {"Crystal Doggy", 35},
+            {"Crystal Kitty", 25},
+            {"Crystal Fox", 20},
+            {"Crystal Cow", 12.5},
+            {"Crystal Bat", 5},
+            {"Crystal Dragon", 2.5},
+            {"Flying Crystal", 0.1},
+            {"Crystal Beserker", 0.008},
+            {"Amethyst Jackalope", 0.001},
+            {"Wondrous Relic", 0.000013333333333333333}
+        },
+        ["Area"] = "Crystalline Cavern"
+    },
+    ["Exclusive Oracle Egg"] = {
+        ["Content"] = {
+            {"Woodland Deer", 45},
+            {"Woodland Ram", 35},
+            {"Ancient Mushroom", 12},
+            {"Overgrown Paladin", 8},
+            {"Woodland Warden", 0.8},
+            {"Wendigo", 0.1}
+        },
+        ["Type"] = "Exclusive Egg",
+        ["Index"] = 1000,
+        ["Description"] = "(Exclusive Egg #1)"
+    },
+    ["Plush Egg"] = {
+        ["Description"] = "(Crazy Carnival)",
+        ["Index"] = 8,
+        ["Cost"] = {["Amount"] = 80000000, ["Currency"] = "Coins"},
+        ["LevelIndex"] = 8,
+        ["CanSpawnOnGround"] = true,
+        ["Content"] = {
+            {"Plushie Doggy", 35},
+            {"Plushie Kitty", 25},
+            {"Plushie Bunny", 20},
+            {"Plushie Wolf", 12.5},
+            {"Plushie Bear", 5},
+            {"Plushie Narwhal", 2.5},
+            {"Puppet", 0.15},
+            {"Puppeteer", 0.01},
+            {"Plushie Stack", 0.0000125}
+        },
+        ["Area"] = "Crazy Carnival"
+    },
+    ["Glacier Egg"] = {
+        ["Description"] = "(Frosty Isles)",
+        ["Index"] = 10,
+        ["Cost"] = {["Amount"] = 80000000, ["Currency"] = "Coins"},
+        ["LevelIndex"] = 8,
+        ["CanSpawnOnGround"] = true,
+        ["Content"] = {
+            {"Glacier Kitty", 35},
+            {"Glacier Doggy", 25},
+            {"Mammoth", 20},
+            {"Moose", 12.5},
+            {"Glacier Dragon", 5},
+            {"Glacier Unicorn", 2.5},
+            {"Snowflake", 0.15},
+            {"Snow Angel", 0.01},
+            {"Yeti", 0.002},
+            {"Glacius", 0.00001}
+        },
+        ["Area"] = "Frosty Isles"
+    },
+    ["Shroom Egg"] = {
+        ["Description"] = "(Mystical Forest)",
+        ["Index"] = 4,
+        ["Cost"] = {["Amount"] = 300000, ["Currency"] = "Coins"},
+        ["LevelIndex"] = 4,
+        ["CanSpawnOnGround"] = true,
+        ["Content"] = {
+            {"Yellow Mush", 40},
+            {"Red Mush", 25},
+            {"Blue Mush", 20},
+            {"Shroom Spider", 12.5},
+            {"Shroom Dragon", 2.5},
+            {"Luminecent Mush", 0.4},
+            {"Mushroom Sorcerer", 0.05}
+        },
+        ["Area"] = "Mystical Forest"
+    },
+    ["Sandcastle Egg"] = {
+        ["Description"] = "(Tropical Seaside)",
+        ["Index"] = 5,
+        ["Cost"] = {["Amount"] = 1500000, ["Currency"] = "Coins"},
+        ["LevelIndex"] = 5,
+        ["CanSpawnOnGround"] = true,
+        ["Content"] = {
+            {"Sand Dog", 40},
+            {"Sand Cat", 25},
+            {"Beach Ball", 20},
+            {"Turtle", 12.5},
+            {"Crab", 2.5},
+            {"Hermit Crab", 0.4},
+            {"Octopus", 0.05},
+            {"Sand Castle", 0.002}
+        },
+        ["Area"] = "Tropical Seaside"
+    },
+    ["Carnival Egg"] = {
+        ["Description"] = "(Crazy Carnival)",
+        ["Index"] = 7,
+        ["Cost"] = {["Amount"] = 15000000, ["Currency"] = "Coins"},
+        ["LevelIndex"] = 7,
+        ["CanSpawnOnGround"] = true,
+        ["Content"] = {
+            {"Pink Balloon", 35},
+            {"Red Balloon", 25},
+            {"Yellow Balloon", 20},
+            {"Lion", 12.5},
+            {"Elephant", 5},
+            {"Carousel Horse", 2.5},
+            {"Circus Seal", 0.15},
+            {"Flaming Cannon", 0.01}
+        },
+        ["Area"] = "Crazy Carnival"
+    },
+    ["Geode Egg"] = {
+        ["Index"] = 14,
+        ["Cost"] = {["Amount"] = 1250000000, ["Currency"] = "Coins"},
+        ["LevelIndex"] = 11,
+        ["Description"] = "(Crystalline Cavern)",
+        ["Content"] = {
+            {"Geode Bear", 40},
+            {"Geode Monkey", 25},
+            {"Geode Sheep", 17.5},
+            {"Geode Lion", 12.5},
+            {"Geode Ram", 5},
+            {"Crystal Creature", 0.05},
+            {"Omnipotent Geode", 0.00002857142857142857}
+        },
+        ["Area"] = "Crystalline Cavern"
+    },
+    ["Frost Egg"] = {
+        ["Description"] = "(Frosty Isles)",
+        ["Index"] = 9,
+        ["Cost"] = {["Amount"] = 15000000, ["Currency"] = "Coins"},
+        ["LevelIndex"] = 7,
+        ["CanSpawnOnGround"] = true,
+        ["Content"] = {
+            {"Arctic Deer", 35},
+            {"Polar Bear", 25},
+            {"Snow Bunny", 20},
+            {"Icy Goat", 12.5},
+            {"Arctic Fox", 5},
+            {"Snow Owl", 2.5},
+            {"Ice Pixie", 0.15},
+            {"Snowman", 0.01}
+        },
+        ["Area"] = "Frosty Isles"
+    },
+    ["Volcanic Egg"] = {
+        ["Description"] = "(Magma Ravine)",
+        ["Index"] = 12,
+        ["Cost"] = {["Amount"] = 800000000, ["Currency"] = "Coins"},
+        ["LevelIndex"] = 10,
+        ["CanSpawnOnGround"] = true,
+        ["Content"] = {
+            {"Volcanic Bee", 35},
+            {"Volcanic Bunny", 25},
+            {"Volcanic Piggy", 20},
+            {"Volcanic Crab", 12.5},
+            {"Demon", 5},
+            {"Imp", 2.5},
+            {"Devil", 0.15},
+            {"Volcanic Titan", 0.01},
+            {"Volcanic Wyvern", 0.002},
+            {"Volcanic Beast", 0.00002}
+        },
+        ["Area"] = "Magma Ravine"
+    },
+    ["Gemstone Egg"] = {
+        ["Index"] = 16,
+        ["Cost"] = {["Amount"] = 10000000000, ["Currency"] = "Coins"},
+        ["LevelIndex"] = 13,
+        ["Description"] = "(Crystalline Cavern)",
+        ["Content"] = {
+            {"Gemstone Bunny", 35},
+            {"Gemstone Elephant", 25},
+            {"Gemstone Shark", 20},
+            {"Gemstone Ladybug", 12.5},
+            {"Gemstone Racoon", 5},
+            {"Gemstone Demon", 2.5},
+            {"Gemstone Monster", 0.05},
+            {"Rogue Gemstone", 0.004},
+            {"Gemstone Colossus", 0.0005},
+            {"Brilliant Butterfly", 0.00001},
+            {"Rainbow Gem Dragon", 6.666666666666667e-07}
+        },
+        ["Area"] = "Crystalline Cavern"
+    },
+    ["Basic Egg"] = {
+        ["Description"] = "(Spawn)",
+        ["Index"] = 1,
+        ["Cost"] = {["Amount"] = 150, ["Currency"] = "Coins"},
+        ["LevelIndex"] = 1,
+        ["CanSpawnOnGround"] = true,
+        ["Content"] = {{"Dog", 35}, {"Cat", 35}, {"Pig", 15}, {"Cow", 10}, {"Dragon", 0.5}},
+        ["Area"] = "Spawn"
+    },
+    ["Exclusive Amber Egg"] = {
+        ["Content"] = {
+            {"Amber Demon", 45},
+            {"Amber Scorpion", 35},
+            {"Amber Arachnid", 12},
+            {"Amber Sentinel", 8},
+            {"Amber Pendant", 0.8},
+            {"Amber Dragon", 0.1}
+        },
+        ["Type"] = "Exclusive Egg",
+        ["Index"] = 1001,
+        ["Description"] = "(Exclusive Egg #2)"
+    },
+    ["Tropical Egg"] = {
+        ["Description"] = "(Tropical Seaside)",
+        ["Index"] = 6,
+        ["Cost"] = {["Amount"] = 4750000, ["Currency"] = "Coins"},
+        ["LevelIndex"] = 6,
+        ["CanSpawnOnGround"] = true,
+        ["Content"] = {
+            {"Monkey", 35},
+            {"Fishie", 25},
+            {"Clownfish", 20},
+            {"Shark", 12.5},
+            {"Orca", 5},
+            {"Parrot", 2.5},
+            {"Pufferfish", 0.15},
+            {"Jellyfish", 0.01},
+            {"Goldfish", 0.002},
+            {"Leviathan", 0.00001}
+        },
+        ["Area"] = "Tropical Seaside"
+    },
+    ["Global Egg"] = {
+        ["Content"] = {
+            {"Global Doggy", 45},
+            {"Global Kitty", 35},
+            {"Global Demon", 12},
+            {"Griffin", 8},
+            {"Pastel Butterfly", 0.8},
+            {"Event Horizion", 0.1}
+        },
+        ["Type"] = "Exclusive Egg",
+        ["Index"] = 999,
+        ["Description"] = "Spawns around the map."
+    },
+    ["Magic Egg"] = {
+        ["Description"] = "(Mystical Forest)",
+        ["Index"] = 3,
+        ["Cost"] = {["Amount"] = 45000, ["Currency"] = "Coins"},
+        ["LevelIndex"] = 3,
+        ["CanSpawnOnGround"] = true,
+        ["Content"] = {
+            {"Wolf", 40},
+            {"Raccoon", 25},
+            {"Black Cat", 20},
+            {"Wizard Bunny", 12.5},
+            {"Owl", 2.5},
+            {"Pixie", 0.4},
+            {"Ethereal Dryad", 0.000025}
+        },
+        ["Area"] = "Mystical Forest"
+    },
+    ["Treasure Egg"] = {
+        ["Description"] = "(Golden Cave)",
+        ["Index"] = 18,
+        ["Cost"] = {["Amount"] = 150000000000, ["Currency"] = "Coins"},
+        ["LevelIndex"] = 15,
+        ["CanSpawnOnGround"] = false,
+        ["Content"] = {
+            {"Ruby", 35},
+            {"Sapphire", 25},
+            {"Amethyst", 20},
+            {"Emerald", 12.5},
+            {"Silver Bar", 5},
+            {"Gold Bar", 2.5},
+            {"Golden Devil", 0.05},
+            {"Magic Coin", 0.004},
+            {"Mimic Chest", 0.0005},
+            {"Primordial Peridot", 0.00001},
+            {"Excalibur", 5e-07}
+        },
+        ["Area"] = "Golden Cave"
+    },
+    ["Gilded Egg"] = {
+        ["Description"] = "(Golden Cave)",
+        ["Index"] = 17,
+        ["Cost"] = {["Amount"] = 60000000000, ["Currency"] = "Coins"},
+        ["LevelIndex"] = 14,
+        ["CanSpawnOnGround"] = false,
+        ["Content"] = {
+            {"Gilded Doggy", 40},
+            {"Gilded Kitty", 25},
+            {"Gilded Monkey", 17.5},
+            {"Gilded Horse", 12.5},
+            {"Gilded Deer", 5},
+            {"Living Ore", 0.05},
+            {"Gilded Raven", 0.002}
+        },
+        ["Area"] = "Golden Cave"
+    },
+    ["Flame Egg"] = {
+        ["Description"] = "(Magma Ravine)",
+        ["Index"] = 11,
+        ["Cost"] = {["Amount"] = 200000000, ["Currency"] = "Coins"},
+        ["LevelIndex"] = 9,
+        ["CanSpawnOnGround"] = true,
+        ["Content"] = {
+            {"Flame Doggy", 35},
+            {"Flame Bird", 25},
+            {"Flame Spider", 20},
+            {"Flame Bat", 12.5},
+            {"Flame Scorpion", 5},
+            {"Infernal", 2.5},
+            {"Phoenix", 0.01},
+            {"Pyroclasm", 0.00002}
+        },
+        ["Area"] = "Magma Ravine"
+    },
+    ["Woodlands Egg"] = {
+        ["Description"] = "(Spawn)",
+        ["Index"] = 2,
+        ["Cost"] = {["Amount"] = 3000, ["Currency"] = "Coins"},
+        ["LevelIndex"] = 2,
+        ["CanSpawnOnGround"] = true,
+        ["Content"] = {{"Bee", 40}, {"Ladybug", 25}, {"Bunny", 20}, {"Bear", 12.5}, {"Fox", 2.5}, {"Unicorn", 0.5}},
+        ["Area"] = "Spawn"
+    }
+}
