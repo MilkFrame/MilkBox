fceaae50e9bedd62f84cfe891fc0c8b5959f04a7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:46:39 +1100

    Create level.lua

---
+++ GameData/Fisch [16732694052]/level.lua
@@ -0,0 +1,21 @@
+data = {
+    ["Max"] = 100,
+    ["LevelRewards"] = {
+        {["Title"] = "Trusted Explorer"},
+        {["Title"] = "Famous Voyager"},
+        {["Title"] = "Mythical Seeker"},
+        {["Title"] = "Sea Sovereign"},
+        {["Text"] = "You can now speak to the shipwright."},
+        {["Title"] = "Eternal Voyager"},
+        {["Title"] = "Novice Explorer"},
+        {["Title"] = "Rookie"},
+        {["Title"] = "Grand Pioneer"},
+        {["Title"] = "Sea Scout"},
+        {["Title"] = "Renowned Navigator"},
+        {["Title"] = "Ocean Hero"},
+        {["Text"] = "You can now speak to Merlin."},
+        {["Title"] = "Legendary Explorer"}
+    },
+    ["GiveXP"] = "function: 0x28d17e7af43af95f",
+    ["LevelUp"] = "function: 0xd56b73c2920a895f"
+}
