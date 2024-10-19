e9a11866379818b93c8b67d6e9de6a7151f5f5dd
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:42:08 +1100

    Create PetAuras.lua

---
+++ GameData/Pet World [106736884883028]/PetAuras.lua
@@ -0,0 +1,6 @@
+data = {
+    ["Test Aura"] = {["Rarity"] = "Godly", ["Strength"] = 1},
+    ["Aquatic Burst"] = {["Rarity"] = "Godly", ["Scale"] = 0.5},
+    ["Test Aura 2"] = {["Rarity"] = "Godly", ["Strength"] = 1},
+    ["Air"] = {["Rarity"] = "Godly", ["Scale"] = 0.5}
+}
