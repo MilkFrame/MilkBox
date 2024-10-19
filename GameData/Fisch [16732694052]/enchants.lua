8c50c625d8c3ce06ce139e4ced0c7ea0ebeadd9d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:48:06 +1100

    Create enchants.lua

---
+++ GameData/Fisch [16732694052]/enchants.lua
@@ -0,0 +1,124 @@
+data = {
+    ["Enchants"] = {
+        ["Sea King"] = {
+            ["StatIncrease"] = 10,
+            ["Description"] = "Makes fish 10% bigger",
+            ["Color"] = Color3.fromRGB(53, 77, 211),
+            ["Display"] = "Sea King",
+            ["StrokeColor"] = Color3.fromRGB(9, 13, 35)
+        },
+        ["Lucky"] = {
+            ["StatIncrease"] = 20,
+            ["Description"] = "Increases luck by 20%",
+            ["Color"] = Color3.fromRGB(126, 229, 164),
+            ["Display"] = "Lucky",
+            ["StrokeColor"] = Color3.fromRGB(25, 45, 32)
+        },
+        ["Scrapper"] = {
+            ["StatIncrease"] = 60,
+            ["Description"] = "60% chance to not consume bait",
+            ["Color"] = Color3.fromRGB(255, 155, 83),
+            ["Display"] = "Scrapper",
+            ["StrokeColor"] = Color3.fromRGB(36, 23, 13)
+        },
+        ["Divine"] = {
+            ["StatIncrease"] = 45,
+            ["Description"] = "Increases luck by 45%",
+            ["Color"] = Color3.fromRGB(198, 229, 220),
+            ["Display"] = "Divine",
+            ["StrokeColor"] = Color3.fromRGB(118, 135, 132)
+        },
+        ["Long"] = {
+            ["StatIncrease"] = 50,
+            ["Description"] = "Increases line distance by 50 studs",
+            ["Color"] = Color3.fromRGB(223, 165, 64),
+            ["Display"] = "Long",
+            ["StrokeColor"] = Color3.fromRGB(33, 25, 14)
+        },
+        ["Swift"] = {
+            ["StatIncrease"] = 30,
+            ["Description"] = "Decreases lure time by 30%",
+            ["Color"] = Color3.fromRGB(181, 227, 229),
+            ["Display"] = "Swift",
+            ["StrokeColor"] = Color3.fromRGB(40, 50, 50)
+        },
+        ["Steady"] = {
+            ["StatIncrease"] = 25,
+            ["Description"] = "Reduces progress loss by 25% when fishing",
+            ["Color"] = Color3.fromRGB(229, 207, 191),
+            ["Display"] = "Steady",
+            ["StrokeColor"] = Color3.fromRGB(21, 19, 18)
+        },
+        ["Controlled"] = {
+            ["StatIncrease"] = 0.05,
+            ["Description"] = "Increased rod control bar size",
+            ["Color"] = Color3.fromRGB(175, 155, 255),
+            ["Display"] = "Controlled",
+            ["StrokeColor"] = Color3.fromRGB(32, 28, 39)
+        },
+        ["Mutated"] = {
+            ["StatIncrease"] = 7,
+            ["Description"] = "Increases mutation luck by 7%",
+            ["Color"] = Color3.fromRGB(147, 229, 112),
+            ["Display"] = "Mutated",
+            ["StrokeColor"] = Color3.fromRGB(45, 58, 39)
+        },
+        ["Unbreakable"] = {
+            ["StatIncrease"] = 10000,
+            ["Description"] = "Increases strength by 10,000kg",
+            ["Color"] = Color3.fromRGB(225, 179, 229),
+            ["Display"] = "Unbreakable",
+            ["StrokeColor"] = Color3.fromRGB(16, 13, 16)
+        },
+        ["Insight"] = {
+            ["StatIncrease"] = 1.5,
+            ["Description"] = "1.5X XP after catching a fish",
+            ["Color"] = Color3.fromRGB(128, 255, 114),
+            ["Display"] = "Insight",
+            ["StrokeColor"] = Color3.fromRGB(29, 36, 27)
+        },
+        ["Wormhole"] = {
+            ["StatIncrease"] = 45,
+            ["Description"] = "45% chance to catch a fish from a random place",
+            ["Color"] = Color3.fromRGB(145, 90, 255),
+            ["Display"] = "Wormhole",
+            ["StrokeColor"] = Color3.fromRGB(13, 8, 22)
+        },
+        ["Breezed"] = {
+            ["StatIncrease"] = 65,
+            ["Description"] = "Luck is increased by 65% during a windy day",
+            ["Color"] = Color3.fromRGB(188, 208, 255),
+            ["Display"] = "Breezed",
+            ["StrokeColor"] = Color3.fromRGB(28, 30, 36)
+        },
+        ["Resilient"] = {
+            ["StatIncrease"] = 35,
+            ["Description"] = "Increased resilience by 35%",
+            ["Color"] = Color3.fromRGB(106, 255, 198),
+            ["Display"] = "Resilient",
+            ["StrokeColor"] = Color3.fromRGB(17, 29, 25)
+        },
+        ["Storming"] = {
+            ["StatIncrease"] = 95,
+            ["Description"] = "Luck is increased by 95% during a rainy day",
+            ["Color"] = Color3.fromRGB(255, 236, 131),
+            ["Display"] = "Storming",
+            ["StrokeColor"] = Color3.fromRGB(36, 36, 24)
+        },
+        ["Blessed"] = {
+            ["StatIncrease"] = 2,
+            ["Description"] = "Increases shiny & sparkling chances by 2%",
+            ["Color"] = Color3.fromRGB(255, 79, 164),
+            ["Display"] = "Blessed",
+            ["StrokeColor"] = Color3.fromRGB(42, 20, 34)
+        },
+        ["Ghastly"] = {
+            ["StatIncrease"] = 20,
+            ["Description"] = "20% chance for a fish to be Translucent",
+            ["Color"] = Color3.fromRGB(129, 223, 173),
+            ["Display"] = "Ghastly",
+            ["StrokeColor"] = Color3.fromRGB(46, 79, 61)
+        }
+    },
+    ["LoadVisual"] = "function: 0x9af8c383ed1b90ff"
+}
