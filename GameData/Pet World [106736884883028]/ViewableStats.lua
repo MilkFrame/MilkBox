81bc47e8355e12ac5f605437368435ba97fd7bb4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:41:55 +1100

    Create ViewableStats.lua

---
+++ GameData/Pet World [106736884883028]/ViewableStats.lua
@@ -0,0 +1,12 @@
+data = {
+    {["Icon"] = "Clock", ["Name"] = "Time Played", ["Get"] = "function: 0x3e1530ba170c3b7a"},
+    {["Icon"] = "Coins", ["Name"] = "Pet Coins Multiplier", ["Get"] = "function: 0x9ab7a667910bc8ea"},
+    {["Icon"] = "Stars", ["Name"] = "Pet Stars Multiplier", ["Get"] = "function: 0xf524b573eb329d5a"},
+    {["Icon"] = "Egg", ["Name"] = "Maximum Egg Hatch", ["Get"] = "function: 0xe07c038f4168efca"},
+    {["Icon"] = "XP", ["Name"] = "Godly Pets Hatched", ["Get"] = "function: 0x34dcad947ee141ba"},
+    {["Icon"] = "MagnifyingGlass", ["Name"] = "Secret Pets Hatched", ["Get"] = "function: 0x49b3aff83729ecaa"},
+    {["Icon"] = "Coins", ["Name"] = "Total Coins", ["Get"] = "function: 0x6f4709dc89351a9a"},
+    {["Icon"] = "Stars", ["Name"] = "Total Stars", ["Get"] = "function: 0x72c54bb123c4c98a"},
+    {["Icon"] = "Shards", ["Name"] = "Total Shards", ["Get"] = "function: 0x8a4aa8455946b7fa"},
+    {["Icon"] = "Egg", ["Name"] = "Eggs Hatched", ["Get"] = "function: 0x272e17765ed7a16a"}
+}
