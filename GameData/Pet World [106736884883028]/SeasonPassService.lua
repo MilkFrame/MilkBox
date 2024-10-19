67b86ee3aaf7d510fdfc7110f4f985879234a049
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:39:23 +1100

    Create SeasonPassService.lua

---
+++ GameData/Pet World [106736884883028]/SeasonPassService.lua
@@ -0,0 +1,314 @@
+data = {
+    ["GetXpForLevel"] = "function: 0xcd3a51329d2dcc1a",
+    ["CanClaimReward"] = "function: 0xe394f8948abceffa",
+    ["ClaimSeasonPassReward"] = "function: 0x8463626b6024035a",
+    ["SeasonKey"] = "s_1",
+    ["Rewards"] = {
+        ["Premium"] = {
+            {
+                "Currency",
+                {["Id"] = "qslhn713nwd5qr33", ["Name"] = "Shards", ["Amount"] = 50},
+                {["Name"] = "Shards", ["Amount"] = 50}
+            },
+            {
+                "Potions",
+                {["Id"] = "vfyh6x1570ed610y", ["Quantity"] = 1, ["Name"] = "Luck Potion"},
+                {["Name"] = "Luck Potion", ["Quantity"] = 1}
+            },
+            {
+                "Items",
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket", ["Id"] = "1lzf6o3vihf0stt8"},
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket"}
+            },
+            {
+                "Items",
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket", ["Id"] = "12165o1c6se0dtl1"},
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket"}
+            },
+            {
+                "Potions",
+                {["Id"] = "19ppj342d2dbiv4t", ["Quantity"] = 2, ["Name"] = "XP Potion"},
+                {["Name"] = "XP Potion", ["Quantity"] = 2}
+            },
+            {
+                "Potions",
+                {["Id"] = "1nrqunrp35druz8q", ["Quantity"] = 1, ["Name"] = "Super Potion"},
+                {["Name"] = "Super Potion", ["Quantity"] = 1}
+            },
+            {
+                "Currency",
+                {["Id"] = "1tma4lw5f1f1dpya", ["Name"] = "RobuxTokens", ["Amount"] = 10},
+                {["Name"] = "RobuxTokens", ["Amount"] = 10}
+            },
+            {
+                "Items",
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket", ["Id"] = "kkv21kbbdd8irv11"},
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket"}
+            },
+            {
+                "Potions",
+                {["Id"] = "1wq5igl90ad3ixiu", ["Quantity"] = 2, ["Name"] = "Star Potion"},
+                {["Name"] = "Star Potion", ["Quantity"] = 2}
+            },
+            {
+                "Potions",
+                {["Id"] = "6h5310hn5ea3wsca", ["Quantity"] = 1, ["Name"] = "XP Boost Potion"},
+                {["Name"] = "XP Boost Potion", ["Quantity"] = 1}
+            },
+            {
+                "Items",
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket", ["Id"] = "rjci5617c4fcvv6q"},
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket"}
+            },
+            {
+                "Potions",
+                {["Id"] = "xopq08162ze9brkf", ["Quantity"] = 2, ["Name"] = "Coin Potion"},
+                {["Name"] = "Coin Potion", ["Quantity"] = 2}
+            },
+            {
+                "Items",
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket", ["Id"] = "s3s5aqvo3dt410rq"},
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket"}
+            },
+            {
+                "Currency",
+                {["Id"] = "15pc0ef9kyd2fpo6", ["Name"] = "Shards", ["Amount"] = 200},
+                {["Name"] = "Shards", ["Amount"] = 200}
+            },
+            {
+                "Items",
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket", ["Id"] = "153lsl6xr5ff6pzs"},
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket"}
+            },
+            {
+                "Currency",
+                {["Id"] = "gpjfpp41ofewqnjm", ["Name"] = "RobuxTokens", ["Amount"] = 12},
+                {["Name"] = "RobuxTokens", ["Amount"] = 12}
+            },
+            {
+                "Currency",
+                {["Id"] = "3daieq2fuesysnir", ["Name"] = "Shards", ["Amount"] = 250},
+                {["Name"] = "Shards", ["Amount"] = 250}
+            },
+            {
+                "Potions",
+                {["Id"] = "rqimc912weemtvot", ["Quantity"] = 2, ["Name"] = "Coin Potion"},
+                {["Name"] = "Coin Potion", ["Quantity"] = 2}
+            },
+            {
+                "Items",
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket", ["Id"] = "1jm58h51dv6fhp11"},
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket"}
+            },
+            {
+                "Potions",
+                {["Id"] = "ydc55aejsetr10c4", ["Quantity"] = 2, ["Name"] = "Luck Potion"},
+                {["Name"] = "Luck Potion", ["Quantity"] = 2}
+            },
+            {
+                "Potions",
+                {["Id"] = "1d6tohd15p4dk3sg", ["Quantity"] = 1, ["Name"] = "Super Potion"},
+                {["Name"] = "Super Potion", ["Quantity"] = 1}
+            },
+            {
+                "Currency",
+                {["Id"] = "1muhmrlysbd4jv54", ["Name"] = "Shards", ["Amount"] = 750},
+                {["Name"] = "Shards", ["Amount"] = 750}
+            },
+            {
+                "Potions",
+                {["Id"] = "4hmpom784f0jzz72", ["Quantity"] = 3, ["Name"] = "Super Potion"},
+                {["Name"] = "Super Potion", ["Quantity"] = 3}
+            },
+            {
+                "Items",
+                {["Locked"] = false, ["Quantity"] = 2, ["Name"] = "Spin Ticket", ["Id"] = "1pp4k6r5e4d5zzai"},
+                {["Locked"] = false, ["Quantity"] = 2, ["Name"] = "Spin Ticket"}
+            },
+            {
+                "Pets",
+                {
+                    ["EnchantInfo"] = {},
+                    ["Id"] = "1rl11m015uffihzd",
+                    ["TotalXp"] = 0,
+                    ["Xp"] = 0,
+                    ["Name"] = "Nexus",
+                    ["Tier"] = "Normal",
+                    ["Locked"] = false,
+                    ["Level"] = 0,
+                    ["Equipped"] = false,
+                    ["Modifiers"] = {}
+                },
+                {
+                    ["EnchantInfo"] = {},
+                    ["TotalXp"] = 0,
+                    ["Xp"] = 0,
+                    ["Name"] = "Nexus",
+                    ["Tier"] = "Normal",
+                    ["Locked"] = false,
+                    ["Level"] = 0,
+                    ["Equipped"] = false,
+                    ["Modifiers"] = {}
+                }
+            }
+        },
+        ["Free"] = {
+            {
+                "Currency",
+                {["Id"] = "6kgjycv3hesgthq1", ["Name"] = "Shards", ["Amount"] = 20},
+                {["Name"] = "Shards", ["Amount"] = 20}
+            },
+            {
+                "Potions",
+                {["Id"] = "1eq7c9iuuzf9srjb", ["Quantity"] = 1, ["Name"] = "Coin Potion"},
+                {["Name"] = "Coin Potion", ["Quantity"] = 1}
+            },
+            {
+                "Currency",
+                {["Id"] = "1oapf6qpzrcuvw01", ["Name"] = "Shards", ["Amount"] = 30},
+                {["Name"] = "Shards", ["Amount"] = 30}
+            },
+            {
+                "Potions",
+                {["Id"] = "1o62v0o1c5tfie10", ["Quantity"] = 1, ["Name"] = "Star Potion"},
+                {["Name"] = "Star Potion", ["Quantity"] = 1}
+            },
+            {
+                "Potions",
+                {["Id"] = "p9et1or8hezsr0e9", ["Quantity"] = 1, ["Name"] = "Luck Potion"},
+                {["Name"] = "Luck Potion", ["Quantity"] = 1}
+            },
+            {
+                "Potions",
+                {["Id"] = "1lhpce4lafpbtygq", ["Quantity"] = 1, ["Name"] = "Coin Potion"},
+                {["Name"] = "Coin Potion", ["Quantity"] = 1}
+            },
+            {
+                "Potions",
+                {["Id"] = "sdpl12x62cvbtf71", ["Quantity"] = 1, ["Name"] = "XP Potion"},
+                {["Name"] = "XP Potion", ["Quantity"] = 1}
+            },
+            {
+                "Potions",
+                {["Id"] = "ph6zx6l7pf0u11tv", ["Quantity"] = 1, ["Name"] = "Star Potion"},
+                {["Name"] = "Star Potion", ["Quantity"] = 1}
+            },
+            {
+                "Items",
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket", ["Id"] = "4ksw9tnbdezqpzz2"},
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket"}
+            },
+            {
+                "Currency",
+                {["Id"] = "vr8atoo1pcw2y922", ["Name"] = "RobuxTokens", ["Amount"] = 5},
+                {["Name"] = "RobuxTokens", ["Amount"] = 5}
+            },
+            {
+                "Potions",
+                {["Id"] = "1b5dujujsbd83rxa", ["Quantity"] = 1, ["Name"] = "Coin Potion"},
+                {["Name"] = "Coin Potion", ["Quantity"] = 1}
+            },
+            {
+                "Potions",
+                {["Id"] = "14m0cd712bfp2vjd", ["Quantity"] = 2, ["Name"] = "Luck Potion"},
+                {["Name"] = "Luck Potion", ["Quantity"] = 2}
+            },
+            {
+                "Items",
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket", ["Id"] = "9i1hv0faqcrgwgz1"},
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket"}
+            },
+            {
+                "Potions",
+                {["Id"] = "z8bou1jsteh6znl1", ["Quantity"] = 1, ["Name"] = "Star Potion"},
+                {["Name"] = "Star Potion", ["Quantity"] = 1}
+            },
+            {
+                "Items",
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket", ["Id"] = "leb9lc6e2cx2106b"},
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket"}
+            },
+            {
+                "Currency",
+                {["Id"] = "uuenl15wnd53yt8q", ["Name"] = "RobuxTokens", ["Amount"] = 2},
+                {["Name"] = "RobuxTokens", ["Amount"] = 2}
+            },
+            {
+                "Potions",
+                {["Id"] = "11froiv776e1qqa5", ["Quantity"] = 2, ["Name"] = "Luck Potion"},
+                {["Name"] = "Luck Potion", ["Quantity"] = 2}
+            },
+            {
+                "Potions",
+                {["Id"] = "beno6tlnf0xsjq25", ["Quantity"] = 1, ["Name"] = "Super Potion"},
+                {["Name"] = "Super Potion", ["Quantity"] = 1}
+            },
+            {
+                "Currency",
+                {["Id"] = "1lyfden345e6czv8", ["Name"] = "RobuxTokens", ["Amount"] = 2},
+                {["Name"] = "RobuxTokens", ["Amount"] = 2}
+            },
+            {
+                "Items",
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket", ["Id"] = "pf6jli14fzeg1sot"},
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket"}
+            },
+            {
+                "Items",
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket", ["Id"] = "11ptyrg5vbf36yhl"},
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket"}
+            },
+            {
+                "Potions",
+                {["Id"] = "zv6unl11j0dw9ztu", ["Quantity"] = 2, ["Name"] = "Super Potion"},
+                {["Name"] = "Super Potion", ["Quantity"] = 2}
+            },
+            {
+                "Currency",
+                {["Id"] = "v0bsew64lfj310e6", ["Name"] = "RobuxTokens", ["Amount"] = 4},
+                {["Name"] = "RobuxTokens", ["Amount"] = 4}
+            },
+            {
+                "Items",
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket", ["Id"] = "f56vtkbkcemny2mo"},
+                {["Locked"] = false, ["Quantity"] = 1, ["Name"] = "Spin Ticket"}
+            },
+            {
+                "Pets",
+                {
+                    ["EnchantInfo"] = {},
+                    ["Id"] = "y8aibz7wfdsmzvy2",
+                    ["TotalXp"] = 0,
+                    ["Xp"] = 0,
+                    ["Name"] = "Nyx",
+                    ["Tier"] = "Normal",
+                    ["Locked"] = false,
+                    ["Level"] = 0,
+                    ["Equipped"] = false,
+                    ["Modifiers"] = {}
+                },
+                {
+                    ["EnchantInfo"] = {},
+                    ["TotalXp"] = 0,
+                    ["Xp"] = 0,
+                    ["Name"] = "Nyx",
+                    ["Tier"] = "Normal",
+                    ["Locked"] = false,
+                    ["Level"] = 0,
+                    ["Equipped"] = false,
+                    ["Modifiers"] = {}
+                }
+            }
+        }
+    },
+    ["GetRewards"] = "function: 0x24ce3c3fbbe4553a",
+    ["GetTotalXpRequired"] = "function: 0x44009aebfb64d59a",
+    ["XpRequirement"] = "100 2500 ",
+    ["GetTotalXpForLevel"] = "function: 0x6471705730ed9a7a",
+    ["MaxLevel"] = 25,
+    ["Initialize"] = "function: 0x3be599b30a1d5eda",
+    ["RequestClaimReward"] = "function: 0xddbc3cceddab5bba",
+    ["GetReward"] = "function: 0xff5e9f00c127171a",
+    ["XpPerQuest"] = 25,
+    ["SeasonName"] = "Season One"
+}
