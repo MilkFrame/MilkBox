f9d840162ee3c09919cc4a5e3c33fd711b6062cf
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:39:49 +1100

    Create RewardService.lua

---
+++ GameData/Pet World [106736884883028]/RewardService.lua
@@ -0,0 +1,292 @@
+data = {
+    ["ClaimReward"] = "function: 0x5c29a4e555a632da",
+    ["Initialize"] = "function: 0xbfc9256c9963f7ba",
+    ["GetRewards"] = "function: 0x1d8e993e82fec31a",
+    ["_Rewards"] = {
+        {
+            ["Color"] = Color3.fromRGB(255, 0, 255),
+            ["Rewards"] = {
+                {{["Name"] = "Stars", ["Amount"] = 100}, {["Name"] = "Shards", ["Amount"] = 5}},
+                {
+                    {["Name"] = "Stars", ["Amount"] = 1000},
+                    {["Name"] = "Shards", ["Amount"] = 25},
+                    {["Name"] = "XP", ["Amount"] = 10000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 5000},
+                    {["Name"] = "Shards", ["Amount"] = 80},
+                    {["Name"] = "XP", ["Amount"] = 30000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 35000},
+                    {["Name"] = "Shards", ["Amount"] = 250},
+                    {["Name"] = "XP", ["Amount"] = 100000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 80000},
+                    {["Name"] = "Shards", ["Amount"] = 1000},
+                    {["Name"] = "XP", ["Amount"] = 300000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 250000},
+                    {["Name"] = "Shards", ["Amount"] = 5000},
+                    {["Name"] = "XP", ["Amount"] = 1200000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 1000000},
+                    {["Name"] = "Shards", ["Amount"] = 27500},
+                    {["Name"] = "XP", ["Amount"] = 3750000}
+                }
+            },
+            ["DisplayName"] = "Time Played",
+            ["Levels"] = 7,
+            ["Name"] = "TimePlayed",
+            ["Requirements"] = {600, 3600, 10800, 7200, 14400, 43200, 100800},
+            ["FormatProgress"] = "function: 0xd16f1c2d561f596a",
+            ["Format"] = "function: 0xc672dcfc18bcd25a",
+            ["Icon"] = "rbxassetid://17762622843",
+            ["DataKey"] = "InGameTime"
+        },
+        {
+            ["Color"] = Color3.fromRGB(255, 127, 0),
+            ["Rewards"] = {
+                {{["Name"] = "Coins", ["Amount"] = 50}, {["Name"] = "Shards", ["Amount"] = 1}},
+                {{["Name"] = "Coins", ["Amount"] = 400}, {["Name"] = "Stars", ["Amount"] = 10}},
+                {
+                    {["Name"] = "Coins", ["Amount"] = 2000},
+                    {["Name"] = "Stars", ["Amount"] = 100},
+                    {["Name"] = "XP", ["Amount"] = 1000}
+                },
+                {
+                    {["Name"] = "Coins", ["Amount"] = 50000},
+                    {["Name"] = "Stars", ["Amount"] = 1000},
+                    {["Name"] = "Shards", ["Amount"] = 20},
+                    {["Name"] = "XP", ["Amount"] = 10000}
+                },
+                {
+                    {["Name"] = "Coins", ["Amount"] = 1000000},
+                    {["Name"] = "Stars", ["Amount"] = 25000},
+                    {["Name"] = "Shards", ["Amount"] = 150},
+                    {["Name"] = "XP", ["Amount"] = 125000}
+                },
+                {
+                    {["Name"] = "Coins", ["Amount"] = 75000000},
+                    {["Name"] = "Stars", ["Amount"] = 125000},
+                    {["Name"] = "Shards", ["Amount"] = 500},
+                    {["Name"] = "XP", ["Amount"] = 500000}
+                },
+                {
+                    {["Name"] = "Coins", ["Amount"] = 400000000},
+                    {["Name"] = "Stars", ["Amount"] = 350000},
+                    {["Name"] = "Shards", ["Amount"] = 1000},
+                    {["Name"] = "XP", ["Amount"] = 2250000}
+                }
+            },
+            ["DisplayName"] = "Eggs Hatched",
+            ["Levels"] = 8,
+            ["Name"] = "EggsHatched",
+            ["Requirements"] = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000},
+            ["FormatProgress"] = "function: 0xa6a0e2158a1e634a",
+            ["Format"] = "function: 0xcbebdf61127f8ada",
+            ["Icon"] = "rbxassetid://18145676098",
+            ["DataKey"] = "EggsHatched"
+        },
+        {
+            ["Color"] = Color3.fromRGB(255, 0, 95),
+            ["Rewards"] = {
+                {
+                    {["Name"] = "Stars", ["Amount"] = 100},
+                    {["Name"] = "Shards", ["Amount"] = 20},
+                    {["Name"] = "XP", ["Amount"] = 1000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 500},
+                    {["Name"] = "Shards", ["Amount"] = 40},
+                    {["Name"] = "XP", ["Amount"] = 5000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 10000},
+                    {["Name"] = "Shards", ["Amount"] = 80},
+                    {["Name"] = "XP", ["Amount"] = 10000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 37500},
+                    {["Name"] = "Shards", ["Amount"] = 150},
+                    {["Name"] = "XP", ["Amount"] = 40000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 100000},
+                    {["Name"] = "Shards", ["Amount"] = 350},
+                    {["Name"] = "XP", ["Amount"] = 100000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 375000},
+                    {["Name"] = "Shards", ["Amount"] = 800},
+                    {["Name"] = "XP", ["Amount"] = 375000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 1000000},
+                    {["Name"] = "Shards", ["Amount"] = 1250},
+                    {["Name"] = "XP", ["Amount"] = 1000000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 3500000},
+                    {["Name"] = "Shards", ["Amount"] = 3500},
+                    {["Name"] = "XP", ["Amount"] = 2500000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 10000000},
+                    {["Name"] = "Shards", ["Amount"] = 7500},
+                    {["Name"] = "XP", ["Amount"] = 5000000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 25000000},
+                    {["Name"] = "Shards", ["Amount"] = 20000},
+                    {["Name"] = "XP", ["Amount"] = 10000000}
+                }
+            },
+            ["DisplayName"] = "Godly Champion",
+            ["Levels"] = 10,
+            ["Name"] = "GodlyChampion",
+            ["Requirements"] = {1, 3, 10, 25, 100, 250, 1000, 3750, 10000, 25000},
+            ["FormatProgress"] = "function: 0x83f0c019be50072a",
+            ["Format"] = "function: 0x31181339e23ef03a",
+            ["Icon"] = "rbxassetid://17723945613",
+            ["DataKey"] = "GodliesHatched"
+        },
+        {
+            ["Color"] = Color3.fromRGB(0, 255, 127),
+            ["Rewards"] = {
+                {
+                    {["Name"] = "Stars", ["Amount"] = 500000},
+                    {["Name"] = "Shards", ["Amount"] = 3000},
+                    {["Name"] = "XP", ["Amount"] = 1000000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 2500000},
+                    {["Name"] = "Shards", ["Amount"] = 10000},
+                    {["Name"] = "XP", ["Amount"] = 5000000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 10000000},
+                    {["Name"] = "Shards", ["Amount"] = 40000},
+                    {["Name"] = "XP", ["Amount"] = 37500000}
+                }
+            },
+            ["DisplayName"] = "Secret Hunter",
+            ["Levels"] = 3,
+            ["Name"] = "SecretsHatched",
+            ["Requirements"] = {1, 3, 10},
+            ["FormatProgress"] = "function: 0x5c80e2212a5da90a",
+            ["Format"] = "function: 0x781f90f576f9d61a",
+            ["Icon"] = "rbxassetid://17576159520",
+            ["DataKey"] = "SecretsHatched"
+        },
+        {
+            ["Color"] = Color3.fromRGB(255, 255, 0),
+            ["Rewards"] = {
+                {
+                    {["Name"] = "Stars", ["Amount"] = 25},
+                    {["Name"] = "Shards", ["Amount"] = 1},
+                    {["Name"] = "XP", ["Amount"] = 500}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 500},
+                    {["Name"] = "Shards", ["Amount"] = 5},
+                    {["Name"] = "XP", ["Amount"] = 1500}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 1250},
+                    {["Name"] = "Shards", ["Amount"] = 15},
+                    {["Name"] = "XP", ["Amount"] = 4250}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 4000},
+                    {["Name"] = "Shards", ["Amount"] = 40},
+                    {["Name"] = "XP", ["Amount"] = 8000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 10000},
+                    {["Name"] = "Shards", ["Amount"] = 100},
+                    {["Name"] = "XP", ["Amount"] = 20000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 37500},
+                    {["Name"] = "Shards", ["Amount"] = 250},
+                    {["Name"] = "XP", ["Amount"] = 75000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 100000},
+                    {["Name"] = "Shards", ["Amount"] = 750},
+                    {["Name"] = "XP", ["Amount"] = 80000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 850000},
+                    {["Name"] = "Shards", ["Amount"] = 2500},
+                    {["Name"] = "XP", ["Amount"] = 175000}
+                }
+            },
+            ["DisplayName"] = "Break Pickups",
+            ["Levels"] = 8,
+            ["Name"] = "BreakPickups",
+            ["Requirements"] = {100, 500, 1500, 2500, 7500, 25000, 100000, 250000},
+            ["FormatProgress"] = "function: 0xd6768c4815dcd1ea",
+            ["Format"] = "function: 0x4319cd2c83bcbc7a",
+            ["Icon"] = "rbxassetid://18286423831",
+            ["DataKey"] = "PickupsBroken"
+        },
+        {
+            ["Color"] = Color3.fromRGB(0, 63, 255),
+            ["Rewards"] = {
+                {
+                    {["Name"] = "Stars", ["Amount"] = 1000},
+                    {["Name"] = "Shards", ["Amount"] = 50},
+                    {["Name"] = "XP", ["Amount"] = 2500}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 3500},
+                    {["Name"] = "Shards", ["Amount"] = 150},
+                    {["Name"] = "XP", ["Amount"] = 7500}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 8000},
+                    {["Name"] = "Shards", ["Amount"] = 250},
+                    {["Name"] = "XP", ["Amount"] = 15000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 25000},
+                    {["Name"] = "Shards", ["Amount"] = 400},
+                    {["Name"] = "XP", ["Amount"] = 27500}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 125000},
+                    {["Name"] = "Shards", ["Amount"] = 1000},
+                    {["Name"] = "XP", ["Amount"] = 80000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 450000},
+                    {["Name"] = "Shards", ["Amount"] = 1800},
+                    {["Name"] = "XP", ["Amount"] = 140000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 1000000},
+                    {["Name"] = "Shards", ["Amount"] = 4000},
+                    {["Name"] = "XP", ["Amount"] = 250000}
+                },
+                {
+                    {["Name"] = "Stars", ["Amount"] = 2250000},
+                    {["Name"] = "Shards", ["Amount"] = 10000},
+                    {["Name"] = "XP", ["Amount"] = 425000}
+                }
+            },
+            ["DisplayName"] = "Unlock Areas",
+            ["Levels"] = 7,
+            ["Name"] = "UnlockAreas",
+            ["Requirements"] = {1, 2, 3, 4, 5, 6, 7},
+            ["FormatProgress"] = "function: 0x300ff171cdccc07a",
+            ["Format"] = "function: 0xf62bed445e625e5a",
+            ["Icon"] = "rbxassetid://17884809793",
+            ["DataKey"] = "function: 0x455e30e66d2b213a"
+        }
+    }
+}
