746f26d935232a1c43699a33b72653d15604982e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:43:53 +1100

    Create Modifiers.lua

---
+++ GameData/Pet World [106736884883028]/Modifiers.lua
@@ -0,0 +1,9 @@
+data = {
+    ["Popcorn"] = {["Config"] = {["AttackSpeed"] = 0.25}, ["ModifierInfo"] = "+25% Attack Speed"},
+    ["Green Apple"] = {["Config"] = {["StarMultiplier"] = 0.1}, ["ModifierInfo"] = "+10% Star Multiplier"},
+    ["Cotton Candy"] = {["Config"] = {["AttackSpeed"] = 0.25}, ["ModifierInfo"] = "+25% Attack Speed"},
+    ["Apple"] = {["Config"] = {["CoinMultiplier"] = 0.1}, ["ModifierInfo"] = "+10% Coin Multiplier"},
+    ["Grapes"] = {["Config"] = {["StarMultiplier"] = 0.1}, ["ModifierInfo"] = "+10% Star Multiplier"},
+    ["Orange"] = {["Config"] = {["CoinMultiplier"] = 0.1}, ["ModifierInfo"] = "+10% Coin Multiplier"},
+    ["Banana"] = {["Config"] = {["CoinMultiplier"] = 0.1}, ["ModifierInfo"] = "+10% Coin Multiplier"}
+}
