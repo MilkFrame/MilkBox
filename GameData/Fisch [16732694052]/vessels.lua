1450b6f98b65fb95591062e71d835f6df97dd09d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:46:51 +1100

    Create vessels.lua

---
+++ GameData/Fisch [16732694052]/vessels.lua
@@ -0,0 +1,161 @@
+data = {
+    ["library"] = {
+        ["Flying Dutchman"] = {
+            ["Dedscriptipon"] = "Only available during FischFright-2024",
+            ["BackwardsEfficiency"] = 1,
+            ["Price"] = -1,
+            ["Bobbing"] = 6,
+            ["BobbingSpeed"] = 3,
+            ["MaxSpeed"] = 80,
+            ["StopEfficiency"] = 2,
+            ["Level"] = 0,
+            ["TurningSpeed"] = 0.3,
+            ["Accel"] = 0.2
+        },
+        ["Titan"] = {
+            ["Dedscriptipon"] = "Huh? Who would want this?",
+            ["BackwardsEfficiency"] = 1,
+            ["Price"] = 5000,
+            ["Bobbing"] = 3,
+            ["BobbingSpeed"] = 1,
+            ["MaxSpeed"] = 45,
+            ["StopEfficiency"] = 1.5,
+            ["Level"] = 40,
+            ["TurningSpeed"] = 0.2,
+            ["Accel"] = 0.2
+        },
+        ["Quack Cruiser"] = {
+            ["Dedscriptipon"] = "This duck sailed up to the lemonade stand.",
+            ["BackwardsEfficiency"] = 1,
+            ["Price"] = 5000,
+            ["Bobbing"] = 4,
+            ["BobbingSpeed"] = 0.5,
+            ["MaxSpeed"] = 45,
+            ["StopEfficiency"] = 2,
+            ["Level"] = 35,
+            ["TurningSpeed"] = 0.5,
+            ["Accel"] = 0.2
+        },
+        ["Speedboat"] = {
+            ["Dedscriptipon"] = "Sail around at remarkable speeds!",
+            ["BackwardsEfficiency"] = 1,
+            ["Price"] = 15000,
+            ["Bobbing"] = 2.5,
+            ["BobbingSpeed"] = 1,
+            ["MaxSpeed"] = 85,
+            ["StopEfficiency"] = 1.3,
+            ["Level"] = 20,
+            ["TurningSpeed"] = 0.5,
+            ["Accel"] = 0.25
+        },
+        ["Jetski"] = {
+            ["Dedscriptipon"] = "Ski around with a friend at insane speeds!",
+            ["BackwardsEfficiency"] = 1,
+            ["Price"] = 50000,
+            ["Bobbing"] = 2,
+            ["BobbingSpeed"] = 0.4,
+            ["MaxSpeed"] = 180,
+            ["StopEfficiency"] = 3.5,
+            ["Level"] = 60,
+            ["TurningSpeed"] = 0.7,
+            ["Accel"] = 0.3
+        },
+        ["Pontoon"] = {
+            ["Dedscriptipon"] = "Great for parties with some buddies!",
+            ["BackwardsEfficiency"] = 1,
+            ["Price"] = 20000,
+            ["Bobbing"] = 1,
+            ["BobbingSpeed"] = 1,
+            ["MaxSpeed"] = 40,
+            ["StopEfficiency"] = 2,
+            ["Level"] = 30,
+            ["TurningSpeed"] = 0.4,
+            ["Accel"] = 0.2
+        },
+        ["Bass Boat"] = {
+            ["Dedscriptipon"] = "Great for fishing with pals!",
+            ["BackwardsEfficiency"] = 1,
+            ["Price"] = 6000,
+            ["Bobbing"] = 2,
+            ["BobbingSpeed"] = 1,
+            ["MaxSpeed"] = 45,
+            ["StopEfficiency"] = 0.7,
+            ["Level"] = 10,
+            ["TurningSpeed"] = 0.4,
+            ["Accel"] = 0.22
+        },
+        ["Pirate Ship"] = {
+            ["Dedscriptipon"] = "Argggg Matey!!",
+            ["BackwardsEfficiency"] = 1,
+            ["Price"] = 20000,
+            ["Bobbing"] = 6,
+            ["BobbingSpeed"] = 3,
+            ["MaxSpeed"] = 80,
+            ["StopEfficiency"] = 2,
+            ["Level"] = 30,
+            ["TurningSpeed"] = 0.3,
+            ["Accel"] = 0.2
+        },
+        ["Luxury Speedboat"] = {
+            ["Dedscriptipon"] = "Sail around at unpresedented speeds!",
+            ["BackwardsEfficiency"] = 1,
+            ["Price"] = 20000,
+            ["Bobbing"] = 2,
+            ["BobbingSpeed"] = 1,
+            ["MaxSpeed"] = 150,
+            ["StopEfficiency"] = 1.5,
+            ["Level"] = 50,
+            ["TurningSpeed"] = 0.54,
+            ["Accel"] = 0.3
+        },
+        ["Surfboard"] = {
+            ["Dedscriptipon"] = "Surf the waves, catch the sharks!",
+            ["BackwardsEfficiency"] = 1,
+            ["Price"] = 1500,
+            ["Bobbing"] = 3,
+            ["BobbingSpeed"] = 0.5,
+            ["MaxSpeed"] = 30,
+            ["StopEfficiency"] = 2,
+            ["Level"] = 7,
+            ["TurningSpeed"] = 0.5,
+            ["Accel"] = 0.2
+        },
+        ["Fishing Boat"] = {
+            ["Dedscriptipon"] = "Trusty boat with built in crab cages!",
+            ["BackwardsEfficiency"] = 1,
+            ["Price"] = 20000,
+            ["Bobbing"] = 2,
+            ["BobbingSpeed"] = 1.5,
+            ["MaxSpeed"] = 75,
+            ["StopEfficiency"] = 2,
+            ["Level"] = 25,
+            ["TurningSpeed"] = 0.5,
+            ["Accel"] = 0.3
+        },
+        ["Hovercraft"] = {
+            ["Dedscriptipon"] = "Hover around the seas! [Submission; @DDkreep, @LiamGame09, @kylecat11]",
+            ["BackwardsEfficiency"] = 1,
+            ["Price"] = 7000,
+            ["Bobbing"] = 1,
+            ["BobbingSpeed"] = 1,
+            ["MaxSpeed"] = 55,
+            ["StopEfficiency"] = 0.7,
+            ["Level"] = 15,
+            ["TurningSpeed"] = 0.4,
+            ["Accel"] = 0.22
+        },
+        ["Rowboat"] = {
+            ["Dedscriptipon"] = "Sail the seas with your trusty paddles!",
+            ["BackwardsEfficiency"] = 1,
+            ["Price"] = 700,
+            ["Bobbing"] = 3,
+            ["BobbingSpeed"] = 1,
+            ["MaxSpeed"] = 25,
+            ["StopEfficiency"] = 2,
+            ["Level"] = 0,
+            ["TurningSpeed"] = 0.4,
+            ["Accel"] = 0.15
+        }
+    },
+    ["Spawn"] = "function: 0x2699fd81c33d6e1f"
+}
