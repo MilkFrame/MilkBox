371e0c3af3ecc053309133c891b5f55db84d2040
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:46:24 +1100

    Create titles.lua

---
+++ GameData/Fisch [16732694052]/titles.lua
@@ -0,0 +1,452 @@
+data = {
+    ["Web"] = {
+        ["StrokeColor"] = Color3.fromRGB(103, 103, 103),
+        ["Text"] = "🕸",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(72, 72, 72)
+    },
+    ["Moderator"] = {
+        ["StrokeColor"] = Color3.fromRGB(34, 25, 34),
+        ["Text"] = "Moderator",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 137, 155)
+    },
+    ["Doors"] = {
+        ["StrokeColor"] = Color3.fromRGB(112, 41, 47),
+        ["Text"] = "🚪👁",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(220, 80, 90)
+    },
+    ["Master Fischer"] = {
+        ["StrokeColor"] = Color3.fromRGB(41, 63, 40),
+        ["Text"] = "Master Fischer",
+        ["TextColor"] = Color3.fromRGB(165, 255, 123)
+    },
+    ["The Hallowed"] = {
+        ["StrokeColor"] = Color3.fromRGB(42, 22, 22),
+        ["Text"] = "The Hallowed",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 58, 58)
+    },
+    ["Supporter"] = {
+        ["StrokeColor"] = Color3.fromRGB(113, 78, 78),
+        ["Text"] = "Supporter",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 234, 75)
+    },
+    ["Ocean Hero"] = {
+        ["StrokeColor"] = Color3.fromRGB(9, 18, 42),
+        ["Text"] = "Ocean Hero",
+        ["TextColor"] = Color3.fromRGB(40, 169, 208)
+    },
+    ["New Moon"] = {
+        ["StrokeColor"] = Color3.fromRGB(54, 54, 75),
+        ["Text"] = "˗ˏˋ 🌑 ˎˊ˗",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(95, 108, 121)
+    },
+    ["Renowned Navigator"] = {
+        ["StrokeColor"] = Color3.fromRGB(42, 12, 12),
+        ["Text"] = "Renowned Navigator",
+        ["TextColor"] = Color3.fromRGB(208, 61, 61)
+    },
+    ["The Creator"] = {
+        ["StrokeColor"] = Color3.fromRGB(26, 23, 42),
+        ["Text"] = "Developer",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(101, 129, 255)
+    },
+    ["Veteran"] = {
+        ["StrokeColor"] = Color3.fromRGB(46, 38, 79),
+        ["Text"] = "Veteran",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(149, 123, 255)
+    },
+    ["Tested Tawler"] = {
+        ["StrokeColor"] = Color3.fromRGB(122, 57, 92),
+        ["Text"] = "Adept Angler",
+        ["TextColor"] = Color3.fromRGB(216, 136, 188)
+    },
+    ["Goddess Of The Seas"] = {
+        ["StrokeColor"] = Color3.fromRGB(47, 32, 20),
+        ["Text"] = "Goddess Of The Seas",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 129, 79)
+    },
+    ["God Of The Seas"] = {
+        ["StrokeColor"] = Color3.fromRGB(47, 32, 20),
+        ["Text"] = "God Of The Seas",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 129, 79)
+    },
+    ["C$ Collector"] = {
+        ["StrokeColor"] = Color3.fromRGB(54, 48, 36),
+        ["Text"] = "C$ Collector",
+        ["TextColor"] = Color3.fromRGB(255, 198, 64)
+    },
+    ["Tarnished"] = {
+        ["StrokeColor"] = Color3.fromRGB(58, 9, 12),
+        ["Text"] = "The Tarnished",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(159, 24, 32)
+    },
+    ["Trello Helper"] = {
+        ["StrokeColor"] = Color3.fromRGB(75, 166, 101),
+        ["Text"] = "Trello Helper",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(120, 255, 129)
+    },
+    ["Star Moon"] = {
+        ["StrokeColor"] = Color3.fromRGB(18, 18, 30),
+        ["Text"] = "˗ˏˋ ⭐🌙 ˎˊ˗",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(229, 232, 255)
+    },
+    ["Haunted"] = {
+        ["StrokeColor"] = Color3.fromRGB(75, 44, 38),
+        ["Text"] = "Haunted",
+        ["Italic"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 122, 78)
+    },
+    ["Tester"] = {
+        ["StrokeColor"] = Color3.fromRGB(61, 88, 65),
+        ["Text"] = "Tester",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(166, 255, 172)
+    },
+    ["C$ Loaded"] = {
+        ["StrokeColor"] = Color3.fromRGB(54, 49, 27),
+        ["Text"] = "C$ Loaded",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(252, 255, 48)
+    },
+    ["Enchanted"] = {
+        ["StrokeColor"] = Color3.fromRGB(29, 39, 54),
+        ["Text"] = "Enchanted",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(94, 145, 255)
+    },
+    ["The Foolish"] = {
+        ["StrokeColor"] = Color3.fromRGB(39, 42, 39),
+        ["Text"] = "The Foolish",
+        ["TextColor"] = Color3.fromRGB(127, 188, 105)
+    },
+    ["Davy Jones"] = {
+        ["StrokeColor"] = Color3.fromRGB(21, 165, 74),
+        ["Text"] = "Davy Jones",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(32, 255, 114)
+    },
+    ["Famous Voyager"] = {
+        ["StrokeColor"] = Color3.fromRGB(38, 11, 42),
+        ["Text"] = "Famous Voyager",
+        ["TextColor"] = Color3.fromRGB(156, 52, 208)
+    },
+    ["Prophet 1"] = {
+        ["StrokeColor"] = Color3.fromRGB(255, 255, 255),
+        ["Text"] = "☆",
+        ["TextColor"] = Color3.fromRGB(255, 253, 248)
+    },
+    ["Lady Of The Sea"] = {
+        ["StrokeColor"] = Color3.fromRGB(21, 27, 42),
+        ["Text"] = "Lady Of The Sea",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(64, 121, 255)
+    },
+    ["Sparkling"] = {
+        ["StrokeColor"] = Color3.fromRGB(255, 217, 67),
+        ["Text"] = "˗ˏˋ ⭐ ˎˊ˗",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 214, 103)
+    },
+    ["Initial Supporter"] = {
+        ["StrokeColor"] = Color3.fromRGB(88, 63, 64),
+        ["Text"] = "Initial Supporter",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 111, 149)
+    },
+    ["Junior Angler"] = {
+        ["StrokeColor"] = Color3.fromRGB(42, 42, 42),
+        ["Text"] = "Junior Angler",
+        ["TextColor"] = Color3.fromRGB(188, 188, 188)
+    },
+    ["None"] = {
+        ["StrokeColor"] = Color3.fromRGB(57, 57, 57),
+        ["Text"] = "",
+        ["TextColor"] = Color3.fromRGB(106, 106, 106)
+    },
+    ["Fishing Veteren"] = {
+        ["StrokeColor"] = Color3.fromRGB(32, 43, 63),
+        ["Text"] = "Fisching Veteran",
+        ["Italic"] = true,
+        ["TextColor"] = Color3.fromRGB(99, 143, 225)
+    },
+    ["Novice Fisher"] = {
+        ["StrokeColor"] = Color3.fromRGB(42, 42, 42),
+        ["Text"] = "Novice Fischer",
+        ["TextColor"] = Color3.fromRGB(188, 188, 188)
+    },
+    ["Ghostly Angler"] = {
+        ["StrokeColor"] = Color3.fromRGB(37, 57, 43),
+        ["Text"] = "Ghostly Angler",
+        ["TextColor"] = Color3.fromRGB(106, 255, 156)
+    },
+    ["Albino Vendor"] = {
+        ["StrokeColor"] = Color3.fromRGB(44, 44, 44),
+        ["Text"] = "Albino Vendor",
+        ["Italic"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 255, 255)
+    },
+    ["Sea Sovereign"] = {
+        ["StrokeColor"] = Color3.fromRGB(79, 45, 21),
+        ["Text"] = "Sea Sovereign",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 147, 69)
+    },
+    ["Unlucky Better"] = {
+        ["StrokeColor"] = Color3.fromRGB(41, 44, 36),
+        ["Text"] = "Unlucky Better",
+        ["Italic"] = true,
+        ["TextColor"] = Color3.fromRGB(78, 88, 72)
+    },
+    ["Clumsy"] = {
+        ["StrokeColor"] = Color3.fromRGB(42, 26, 21),
+        ["Text"] = "Clumsy",
+        ["Italic"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 255, 255)
+    },
+    ["The Angler"] = {
+        ["StrokeColor"] = Color3.fromRGB(59, 75, 90),
+        ["Text"] = "The Angler",
+        ["TextColor"] = Color3.fromRGB(129, 164, 197)
+    },
+    ["Give"] = "function: 0x55ad4c206df759df",
+    ["Eternal Voyager"] = {
+        ["StrokeColor"] = Color3.fromRGB(5, 5, 15),
+        ["Text"] = "Eternal Voyager",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(9, 9, 25)
+    },
+    ["Love"] = {
+        ["StrokeColor"] = Color3.fromRGB(255, 137, 139),
+        ["Text"] = "♡",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 255, 255)
+    },
+    ["Skilled Fisher"] = {
+        ["StrokeColor"] = Color3.fromRGB(132, 120, 62),
+        ["Text"] = "Skilled Fischer",
+        ["TextColor"] = Color3.fromRGB(216, 202, 97)
+    },
+    ["Woozy"] = {
+        ["StrokeColor"] = Color3.fromRGB(16, 24, 53),
+        ["Text"] = "",
+        ["CustomFont"] = Enum.Font.GothamBold,
+        ["TextColor"] = Color3.fromRGB(51, 80, 127)
+    },
+    ["Orcs Best Friend"] = {
+        ["StrokeColor"] = Color3.fromRGB(34, 42, 31),
+        ["Text"] = "Orcs Best Friend",
+        ["Italic"] = true,
+        ["TextColor"] = Color3.fromRGB(119, 145, 102)
+    },
+    ["Grand Pioneer"] = {
+        ["StrokeColor"] = Color3.fromRGB(54, 52, 33),
+        ["Text"] = "Grand Pioneer",
+        ["TextColor"] = Color3.fromRGB(255, 251, 125)
+    },
+    ["Legendary Explorer"] = {
+        ["StrokeColor"] = Color3.fromRGB(39, 27, 66),
+        ["Text"] = "Legendary Explorer",
+        ["TextColor"] = Color3.fromRGB(126, 86, 208)
+    },
+    ["Eternal Fischer"] = {
+        ["StrokeColor"] = Color3.fromRGB(20, 20, 20),
+        ["Text"] = "Eternal Fischer",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(24, 24, 24)
+    },
+    ["Nightmare Angler"] = {
+        ["StrokeColor"] = Color3.fromRGB(32, 27, 48),
+        ["Text"] = "Nightmare Angler",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(111, 98, 255)
+    },
+    ["True Hakari"] = {
+        ["StrokeColor"] = Color3.fromRGB(51, 130, 87),
+        ["Text"] = "True Hakari",
+        ["Italic"] = true,
+        ["TextColor"] = Color3.fromRGB(83, 255, 198)
+    },
+    ["Lord Of The Sea"] = {
+        ["StrokeColor"] = Color3.fromRGB(21, 27, 42),
+        ["Text"] = "Lord Of The Sea",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(64, 121, 255)
+    },
+    ["Bobber Enthusiast"] = {
+        ["StrokeColor"] = Color3.fromRGB(90, 55, 89),
+        ["Text"] = "Bobber Enthusiast",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(247, 129, 255)
+    },
+    ["Expert Angler"] = {
+        ["StrokeColor"] = Color3.fromRGB(90, 34, 35),
+        ["Text"] = "Expert Angler",
+        ["TextColor"] = Color3.fromRGB(255, 56, 59)
+    },
+    ["Rookie"] = {
+        ["StrokeColor"] = Color3.fromRGB(42, 42, 42),
+        ["Text"] = "Rookie",
+        ["TextColor"] = Color3.fromRGB(188, 188, 188)
+    },
+    ["Fish Overlord"] = {
+        ["StrokeColor"] = Color3.fromRGB(47, 24, 35),
+        ["Text"] = "Fish Overlord",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 48, 165)
+    },
+    ["Moonlit"] = {
+        ["StrokeColor"] = Color3.fromRGB(255, 217, 67),
+        ["Text"] = "˗ˏˋ 🌙 ˎˊ˗",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 214, 103)
+    },
+    ["The Lantern Keeper"] = {
+        ["StrokeColor"] = Color3.fromRGB(90, 70, 50),
+        ["Text"] = "The Holy",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 251, 193)
+    },
+    ["Sea Scout"] = {
+        ["StrokeColor"] = Color3.fromRGB(26, 42, 23),
+        ["Text"] = "Sea Scout",
+        ["TextColor"] = Color3.fromRGB(149, 255, 125)
+    },
+    ["Mythical Angler"] = {
+        ["StrokeColor"] = Color3.fromRGB(42, 20, 30),
+        ["Text"] = "Mythical Angler",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 93, 185)
+    },
+    ["Trusted Explorer"] = {
+        ["StrokeColor"] = Color3.fromRGB(42, 29, 23),
+        ["Text"] = "Trusted Explorer",
+        ["TextColor"] = Color3.fromRGB(208, 120, 102)
+    },
+    ["Famous Fischer"] = {
+        ["StrokeColor"] = Color3.fromRGB(59, 90, 71),
+        ["Text"] = "Famous Fischer",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(164, 255, 205)
+    },
+    ["Lucky Roller"] = {
+        ["StrokeColor"] = Color3.fromRGB(40, 53, 35),
+        ["Text"] = "Lucky Roller",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(76, 188, 93)
+    },
+    ["Legendary Fischer"] = {
+        ["StrokeColor"] = Color3.fromRGB(90, 55, 39),
+        ["Text"] = "Legendary Fischer",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 178, 69)
+    },
+    ["AFK"] = {
+        ["StrokeColor"] = Color3.fromRGB(165, 113, 131),
+        ["Text"] = "AFK",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 174, 237)
+    },
+    ["Smirk Cat"] = {
+        ["StrokeColor"] = Color3.fromRGB(207, 163, 32),
+        ["Text"] = "😼",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(221, 180, 33)
+    },
+    ["Unstoppable Fischer"] = {
+        ["StrokeColor"] = Color3.fromRGB(34, 63, 50),
+        ["Text"] = "Unstoppable Fischer",
+        ["Italic"] = true,
+        ["TextColor"] = Color3.fromRGB(93, 225, 126)
+    },
+    ["The Hoarder"] = {
+        ["StrokeColor"] = Color3.fromRGB(42, 32, 22),
+        ["Text"] = "The Hoarder",
+        ["TextColor"] = Color3.fromRGB(255, 129, 90)
+    },
+    ["Pro Fisherman"] = {
+        ["StrokeColor"] = Color3.fromRGB(34, 63, 50),
+        ["Text"] = "Pro Fisherman",
+        ["Italic"] = true,
+        ["TextColor"] = Color3.fromRGB(93, 225, 126)
+    },
+    ["Novice Explorer"] = {
+        ["StrokeColor"] = Color3.fromRGB(74, 79, 99),
+        ["Text"] = "Novice Explorer",
+        ["TextColor"] = Color3.fromRGB(155, 166, 208)
+    },
+    ["Prophet Angler"] = {
+        ["StrokeColor"] = Color3.fromRGB(29, 22, 42),
+        ["Text"] = "The Prophet Angler",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(135, 98, 255)
+    },
+    ["???"] = {
+        ["StrokeColor"] = Color3.fromRGB(170, 170, 170),
+        ["Text"] = "???",
+        ["Italic"] = true,
+        ["TextColor"] = Color3.fromRGB(66, 66, 66)
+    },
+    ["Lightning"] = {
+        ["StrokeColor"] = Color3.fromRGB(31, 7, 5),
+        ["Text"] = "⚡",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 192, 0)
+    },
+    ["Shark Slayer"] = {
+        ["StrokeColor"] = Color3.fromRGB(66, 77, 90),
+        ["Text"] = "Shark Slayer",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(147, 181, 255)
+    },
+    ["The Clever"] = {
+        ["StrokeColor"] = Color3.fromRGB(26, 42, 41),
+        ["Text"] = "The Clever",
+        ["TextColor"] = Color3.fromRGB(124, 253, 255)
+    },
+    ["Mythical Seeker"] = {
+        ["StrokeColor"] = Color3.fromRGB(54, 26, 38),
+        ["Text"] = "Mythical Seeker",
+        ["TextColor"] = Color3.fromRGB(255, 83, 123)
+    },
+    ["The Drowned"] = {
+        ["StrokeColor"] = Color3.fromRGB(16, 16, 16),
+        ["Text"] = "The Drowned",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(113, 135, 135)
+    },
+    ["Vigilante"] = {
+        ["StrokeColor"] = Color3.fromRGB(19, 22, 54),
+        ["Text"] = "Vigilante",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(27, 31, 76)
+    },
+    ["The Pupil"] = {
+        ["StrokeColor"] = Color3.fromRGB(42, 37, 16),
+        ["Text"] = "The Pupil",
+        ["TextColor"] = Color3.fromRGB(255, 255, 64)
+    },
+    ["Content Creator"] = {
+        ["StrokeColor"] = Color3.fromRGB(57, 21, 21),
+        ["Text"] = "Content Creator",
+        ["Italic"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 61, 61)
+    },
+    ["The Hunter"] = {
+        ["StrokeColor"] = Color3.fromRGB(57, 48, 36),
+        ["Text"] = "The Hunter",
+        ["Bold"] = true,
+        ["TextColor"] = Color3.fromRGB(255, 166, 42)
+    }
+}
