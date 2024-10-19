da282803ddda56d0703a8f9915a5bd0f77ffb38b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-20 01:47:35 +1100

    Create crabzone.lua

---
+++ GameData/Fisch [16732694052]/crabzone.lua
@@ -0,0 +1,64 @@
+data = {
+    ["Ocean"] = {
+        ["Pool"] = {
+            "Tire",
+            "Boot",
+            "Driftwood",
+            "Crab",
+            "Mussel",
+            "Sand Dollar",
+            "Sea Urchin",
+            "Shrimp",
+            "Prawn",
+            "Lobster",
+            "Sea Pickle"
+        },
+        ["Priority"] = 0
+    },
+    ["Exotic"] = {
+        ["Pool"] = {
+            "Seaweed",
+            "Driftwood",
+            "Crab",
+            "Mussel",
+            "Sea Urchin",
+            "Shrimp",
+            "Prawn",
+            "Lobster",
+            "Scallop",
+            "Sand Dollar",
+            "Sea Pickle"
+        },
+        ["Priority"] = 1
+    },
+    ["Vertigo"] = {["Pool"] = {"Rock", "Night Shrimp"}, ["Priority"] = 1},
+    ["King Oyster"] = {["Pool"] = {"Driftwood", "Rock", "Seaweed", "Boot", "King Oyster"}, ["Priority"] = 3},
+    ["Terrapin"] = {["Pool"] = {"Driftwood", "Rock", "Seaweed", "Oyster", "King Oyster"}, ["Priority"] = 2},
+    ["Docks"] = {
+        ["Pool"] = {"Tire", "Boot", "Driftwood", "Crab", "Mussel", "Sand Dollar", "Sea Urchin", "Lobster", "Oyster"},
+        ["Priority"] = 1
+    },
+    ["Default"] = {["Pool"] = {"Tire", "Boot", "Driftwood", "Rock", "Seaweed"}, ["Priority"] = 0},
+    ["Cheap Pond"] = {
+        ["Pool"] = {"Tire", "Boot", "Driftwood", "Rock", "Seaweed", "Mussel", "Sand Dollar"},
+        ["Priority"] = 0
+    },
+    ["Lobster"] = {["Pool"] = {"Seaweed", "Driftwood", "Lobster"}, ["Priority"] = 1},
+    ["Scallop"] = {["Pool"] = {"Tire", "Boot", "Driftwood", "Rock", "Scallop", "Crab"}, ["Priority"] = 3},
+    ["Swamp"] = {
+        ["Pool"] = {
+            "Tire",
+            "Boot",
+            "Driftwood",
+            "Rock",
+            "Seaweed",
+            "Crab",
+            "Mussel",
+            "Mushgrove Crab",
+            "Swamp Scallop",
+            "Fungal Cluster"
+        },
+        ["Priority"] = 1
+    },
+    ["Coast"] = {["Pool"] = {"Driftwood", "Mussel", "Oyster", "Scallop", "Sand Dollar", "Sea Pickle"}, ["Priority"] = 2}
+}
