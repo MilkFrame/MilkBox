c6a383b1493ad5b38e0f1fe116e131f410fd262d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-21 01:24:57 +1100

    Create Gamepasses.lua

---
+++ GameData/PETS GO! [18901165922]/Gamepasses.lua
@@ -0,0 +1,42 @@
+data = {
+    ["Hyper Dice"] = {
+        ["_script"] = "Gamepass | Hyper Dice",
+        ["_id"] = "Hyper Dice",
+        ["ClientTest"] = "function: 0x147649d0d9ad8a14",
+        ["ProductId"] = 923754964,
+        ["Desc"] = "Speeds up dice rolls!",
+        ["ServerTest"] = "function: 0x18f9814d754b9cb4",
+        ["Icon"] = "rbxassetid://105402001077488",
+        ["DisplayName"] = "Hyper Dice"
+    },
+    ["Lucky"] = {
+        ["_script"] = "Gamepass | Lucky",
+        ["_id"] = "Lucky",
+        ["ClientTest"] = "function: 0x425f6eadccc50534",
+        ["ProductId"] = 923677450,
+        ["Desc"] = "Increases luck!",
+        ["ServerTest"] = "function: 0x6bef51d3ed5a9014",
+        ["Icon"] = "rbxassetid://125753102677531",
+        ["DisplayName"] = "Lucky"
+    },
+    ["Ultra Lucky"] = {
+        ["_script"] = "Gamepass | Ultra Lucky",
+        ["_id"] = "Ultra Lucky",
+        ["ClientTest"] = "function: 0xf6efb0cf55f9da54",
+        ["ProductId"] = 924643143,
+        ["Desc"] = "Increases luck by a lot!",
+        ["ServerTest"] = "function: 0x275fa08576234fb4",
+        ["Icon"] = "rbxassetid://132023923368686",
+        ["DisplayName"] = "Ultra Lucky"
+    },
+    ["VIP"] = {
+        ["_script"] = "Gamepass | VIP",
+        ["_id"] = "VIP",
+        ["ClientTest"] = "function: 0x94979a9cf81b0734",
+        ["ProductId"] = 947426555,
+        ["Desc"] = "Tons of benefits & rewards!",
+        ["ServerTest"] = "function: 0xd14a509853dc6954",
+        ["Icon"] = "rbxassetid://122451126116495",
+        ["DisplayName"] = "VIP"
+    }
+}
