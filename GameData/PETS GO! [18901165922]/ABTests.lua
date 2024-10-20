a0ff5440df84581c9871ef734d6212d30ffe1cce
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-21 01:23:00 +1100

    Create ABTests.lua

---
+++ GameData/PETS GO! [18901165922]/ABTests.lua
@@ -0,0 +1,15 @@
+data = {
+    ["Test"] = {
+        ["_script"] = "ABTest | Test",
+        ["Table"] = {
+            {["Value"] = 5000, ["Position"] = 1, ["Weight"] = 1},
+            {["Value"] = 4000, ["Position"] = 10, ["Weight"] = 9},
+            {["Value"] = 3000, ["Position"] = 30, ["Weight"] = 20},
+            {["Value"] = 2000, ["Position"] = 60, ["Weight"] = 30},
+            {["Value"] = 1000, ["Position"] = 100, ["Weight"] = 40}
+        },
+        ["TotalWeight"] = 100,
+        ["_id"] = "Test",
+        ["DataType"] = "int"
+    }
+}
