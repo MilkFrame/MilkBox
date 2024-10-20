42f5ae6214a6b6b9d71e595627268ff1d5179ae6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-21 01:25:09 +1100

    Create Booths.lua

---
+++ GameData/PETS GO! [18901165922]/Booths.lua
@@ -0,0 +1,433 @@
+data = {
+    ["Egg"] = {
+        ["iconSettings"] = {
+            ["faceBottomRightPixel"] = "156, 131",
+            ["faceTopLeftPixel"] = "20, 61",
+            ["imageSize"] = "187, 250"
+        },
+        ["Rarity"] = {
+            ["RarityNumber"] = 2,
+            ["Lootbag"] = "Rare",
+            ["_id"] = "Rare",
+            ["Color"] = Color3.fromRGB(113, 255, 62),
+            ["DifficultyThreshold"] = 100,
+            ["DisplayName"] = "Rare",
+            ["Message"] = "function: 0x66722269a76c15b4",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "2 | Rare"
+        },
+        ["Tradable"] = false,
+        ["DisplayName"] = "Egg Booth",
+        ["_script"] = "Booth | Egg",
+        ["Model"] = "Model",
+        ["_id"] = "Egg",
+        ["Icon"] = "rbxassetid://14988724317",
+        ["Desc"] = "Time to get crackin'!"
+    },
+    ["Merchant"] = {
+        ["iconSettings"] = {
+            ["faceBottomRightPixel"] = "179, 116",
+            ["faceTopLeftPixel"] = "23, 35",
+            ["imageSize"] = "215, 250"
+        },
+        ["Rarity"] = {
+            ["RarityNumber"] = 3,
+            ["Lootbag"] = "Epic",
+            ["_id"] = "Epic",
+            ["Color"] = Color3.fromRGB(41, 226, 255),
+            ["DifficultyThreshold"] = 5000,
+            ["DisplayName"] = "Epic",
+            ["Message"] = "function: 0xe70de4fafc8c80f4",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "3 | Epic"
+        },
+        ["Tradable"] = false,
+        ["DisplayName"] = "Merchant Booth",
+        ["_script"] = "Booth | Merchant",
+        ["Model"] = "Model",
+        ["_id"] = "Merchant",
+        ["Icon"] = "rbxassetid://15122625881",
+        ["Desc"] = "I'm an NPC!"
+    },
+    ["Summer"] = {
+        ["iconSettings"] = {
+            ["faceBottomRightPixel"] = "160, 129",
+            ["faceTopLeftPixel"] = "27, 60",
+            ["imageSize"] = "190, 250"
+        },
+        ["Rarity"] = {
+            ["RarityNumber"] = 3,
+            ["Lootbag"] = "Epic",
+            ["_id"] = "Epic",
+            ["Color"] = Color3.fromRGB(41, 226, 255),
+            ["DifficultyThreshold"] = 5000,
+            ["DisplayName"] = "Epic",
+            ["Message"] = "function: 0xe70de4fafc8c80f4",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "3 | Epic"
+        },
+        ["Tradable"] = false,
+        ["DisplayName"] = "Summer Booth",
+        ["_script"] = "Booth | Summer",
+        ["Model"] = "Model",
+        ["_id"] = "Summer",
+        ["Icon"] = "rbxassetid://14988722572",
+        ["Desc"] = "Summer vibessss!"
+    },
+    ["Giraffe"] = {
+        ["iconSettings"] = {
+            ["faceBottomRightPixel"] = "147, 139",
+            ["faceTopLeftPixel"] = "19, 74",
+            ["imageSize"] = "175, 250"
+        },
+        ["Rarity"] = {
+            ["RarityNumber"] = 4,
+            ["Lootbag"] = "Legendary",
+            ["_id"] = "Legendary",
+            ["Color"] = Color3.fromRGB(255, 133, 34),
+            ["DifficultyThreshold"] = 50000,
+            ["DisplayName"] = "Legendary",
+            ["Message"] = "function: 0xb2fbe0cade9b2114",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "4 | Legendary"
+        },
+        ["Tradable"] = false,
+        ["DisplayName"] = "Giraffe Booth",
+        ["_script"] = "Booth | Giraffe",
+        ["Model"] = "Model",
+        ["_id"] = "Giraffe",
+        ["Icon"] = "rbxassetid://17450667779",
+        ["Desc"] = "Neck deep in trades!"
+    },
+    ["Fishing"] = {
+        ["iconSettings"] = {
+            ["faceBottomRightPixel"] = "176, 118",
+            ["faceTopLeftPixel"] = "23, 39",
+            ["imageSize"] = "210, 250"
+        },
+        ["Rarity"] = {
+            ["RarityNumber"] = 4,
+            ["Lootbag"] = "Legendary",
+            ["_id"] = "Legendary",
+            ["Color"] = Color3.fromRGB(255, 133, 34),
+            ["DifficultyThreshold"] = 50000,
+            ["DisplayName"] = "Legendary",
+            ["Message"] = "function: 0xb2fbe0cade9b2114",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "4 | Legendary"
+        },
+        ["Tradable"] = false,
+        ["DisplayName"] = "Fishing Booth",
+        ["_script"] = "Booth | Fishing",
+        ["Model"] = "Model",
+        ["_id"] = "Fishing",
+        ["Icon"] = "rbxassetid://15477647840",
+        ["Desc"] = "No bait needed! Let's fish!"
+    },
+    ["Rave Crab"] = {
+        ["iconSettings"] = {
+            ["faceBottomRightPixel"] = "159, 135",
+            ["faceTopLeftPixel"] = "28, 68",
+            ["imageSize"] = "189, 250"
+        },
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DifficultyThreshold"] = 10000000,
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0x8e4bd590b51b8674",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["Tradable"] = false,
+        ["DisplayName"] = "Rave Crab Booth",
+        ["_script"] = "Booth | Rave Crab",
+        ["Model"] = "Model",
+        ["_id"] = "Rave Crab",
+        ["Icon"] = "rbxassetid://74989206681600",
+        ["Desc"] = "Pinch a perfect deal!"
+    },
+    ["Panda"] = {
+        ["iconSettings"] = {
+            ["faceBottomRightPixel"] = "150, 136",
+            ["faceTopLeftPixel"] = "19, 69",
+            ["imageSize"] = "179, 250"
+        },
+        ["Rarity"] = {
+            ["RarityNumber"] = 5,
+            ["Lootbag"] = "Mythical",
+            ["_id"] = "Mythical",
+            ["Color"] = Color3.fromRGB(255, 43, 100),
+            ["DifficultyThreshold"] = 500000,
+            ["DisplayName"] = "Mythical",
+            ["Message"] = "function: 0x76bd95e45f4c4894",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "5 | Mythical"
+        },
+        ["Tradable"] = false,
+        ["DisplayName"] = "Panda Booth",
+        ["_script"] = "Booth | Panda",
+        ["Model"] = "Model",
+        ["_id"] = "Panda",
+        ["Icon"] = "rbxassetid://14988723018",
+        ["Desc"] = "Rollin' in deals like a lazy panda!"
+    },
+    ["Ice Cream"] = {
+        ["iconSettings"] = {
+            ["faceBottomRightPixel"] = "126, 149",
+            ["faceTopLeftPixel"] = "20, 96",
+            ["imageSize"] = "154, 250"
+        },
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DifficultyThreshold"] = 10000000,
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0x8e4bd590b51b8674",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["Tradable"] = false,
+        ["DisplayName"] = "Ice Cream Booth",
+        ["_script"] = "Booth | Ice Cream",
+        ["Model"] = "Model",
+        ["_id"] = "Ice Cream",
+        ["Icon"] = "rbxassetid://18856297202",
+        ["Desc"] = "Trading is sweet!"
+    },
+    ["Cat"] = {
+        ["iconSettings"] = {
+            ["faceBottomRightPixel"] = "147, 138",
+            ["faceTopLeftPixel"] = "19, 72",
+            ["imageSize"] = "176, 250"
+        },
+        ["Rarity"] = {
+            ["RarityNumber"] = 5,
+            ["Lootbag"] = "Mythical",
+            ["_id"] = "Mythical",
+            ["Color"] = Color3.fromRGB(255, 43, 100),
+            ["DifficultyThreshold"] = 500000,
+            ["DisplayName"] = "Mythical",
+            ["Message"] = "function: 0x76bd95e45f4c4894",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "5 | Mythical"
+        },
+        ["Tradable"] = false,
+        ["DisplayName"] = "Cat Booth",
+        ["_script"] = "Booth | Cat",
+        ["Model"] = "Model",
+        ["_id"] = "Cat",
+        ["Icon"] = "rbxassetid://14988725079",
+        ["Desc"] = "Meow! These trades are purrfect!"
+    },
+    ["VIP"] = {
+        ["iconSettings"] = {
+            ["faceBottomRightPixel"] = "173, 120",
+            ["faceTopLeftPixel"] = "23, 42",
+            ["imageSize"] = "207, 250"
+        },
+        ["Rarity"] = {
+            ["RarityNumber"] = 999,
+            ["Lootbag"] = "Exclusive",
+            ["_id"] = "Exclusive",
+            ["Color"] = Color3.fromRGB(173, 79, 255),
+            ["DisplayName"] = "Exclusive",
+            ["Message"] = "function: 0x92c72e94ee5dc7f4",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "Exclusive"
+        },
+        ["Tradable"] = false,
+        ["DisplayName"] = "VIP Booth",
+        ["_script"] = "Booth | VIP",
+        ["Model"] = "Model",
+        ["_id"] = "VIP",
+        ["Icon"] = "rbxassetid://14988722264",
+        ["Desc"] = "For VIP members!"
+    },
+    ["Default"] = {
+        ["iconSettings"] = {
+            ["faceBottomRightPixel"] = "198, 103",
+            ["faceTopLeftPixel"] = "25, 13",
+            ["imageSize"] = "236, 250"
+        },
+        ["Rarity"] = {
+            ["RarityNumber"] = 1,
+            ["Lootbag"] = "Basic",
+            ["_id"] = "Basic",
+            ["Color"] = Color3.fromRGB(148, 145, 167),
+            ["DifficultyThreshold"] = 0,
+            ["DisplayName"] = "Basic",
+            ["Message"] = "function: 0x3cb4ecb1a8230254",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "1 | Basic"
+        },
+        ["Tradable"] = false,
+        ["DisplayName"] = "Default Booth",
+        ["_script"] = "Booth | Default",
+        ["Model"] = "Model",
+        ["_id"] = "Default",
+        ["Icon"] = "rbxassetid://14988724636",
+        ["Desc"] = "Let's make a deal!"
+    },
+    ["Corgi"] = {
+        ["iconSettings"] = {
+            ["faceBottomRightPixel"] = "145, 140",
+            ["faceTopLeftPixel"] = "19, 76",
+            ["imageSize"] = "173, 250"
+        },
+        ["Rarity"] = {
+            ["RarityNumber"] = 5,
+            ["Lootbag"] = "Mythical",
+            ["_id"] = "Mythical",
+            ["Color"] = Color3.fromRGB(255, 43, 100),
+            ["DifficultyThreshold"] = 500000,
+            ["DisplayName"] = "Mythical",
+            ["Message"] = "function: 0x76bd95e45f4c4894",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "5 | Mythical"
+        },
+        ["Tradable"] = false,
+        ["DisplayName"] = "Corgi Booth",
+        ["_script"] = "Booth | Corgi",
+        ["Model"] = "Model",
+        ["_id"] = "Corgi",
+        ["Icon"] = "rbxassetid://14988724844",
+        ["Desc"] = "Everybody loves corgis. Bark!"
+    },
+    ["Parrot"] = {
+        ["iconSettings"] = {
+            ["faceBottomRightPixel"] = "162, 127",
+            ["faceTopLeftPixel"] = "21, 55",
+            ["imageSize"] = "194, 250"
+        },
+        ["Rarity"] = {
+            ["RarityNumber"] = 3,
+            ["Lootbag"] = "Epic",
+            ["_id"] = "Epic",
+            ["Color"] = Color3.fromRGB(41, 226, 255),
+            ["DifficultyThreshold"] = 5000,
+            ["DisplayName"] = "Epic",
+            ["Message"] = "function: 0xe70de4fafc8c80f4",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "3 | Epic"
+        },
+        ["Tradable"] = false,
+        ["DisplayName"] = "Parrot Booth",
+        ["_script"] = "Booth | Parrot",
+        ["Model"] = "Model",
+        ["_id"] = "Parrot",
+        ["Icon"] = "rbxassetid://14988722892",
+        ["Desc"] = "Looking for potions! Looking for potions! Looking for potions!"
+    },
+    ["Axolotl"] = {
+        ["iconSettings"] = {
+            ["faceBottomRightPixel"] = "162, 128",
+            ["faceTopLeftPixel"] = "21, 56",
+            ["imageSize"] = "193, 250"
+        },
+        ["Rarity"] = {
+            ["RarityNumber"] = 4,
+            ["Lootbag"] = "Legendary",
+            ["_id"] = "Legendary",
+            ["Color"] = Color3.fromRGB(255, 133, 34),
+            ["DifficultyThreshold"] = 50000,
+            ["DisplayName"] = "Legendary",
+            ["Message"] = "function: 0xb2fbe0cade9b2114",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "4 | Legendary"
+        },
+        ["Tradable"] = false,
+        ["DisplayName"] = "Axolotl Booth",
+        ["_script"] = "Booth | Axolotl",
+        ["Model"] = "Model",
+        ["_id"] = "Axolotl",
+        ["Icon"] = "rbxassetid://14988725263",
+        ["Desc"] = "Under da' sea!"
+    },
+    ["Pixel"] = {
+        ["iconSettings"] = {
+            ["faceBottomRightPixel"] = "194, 112",
+            ["faceTopLeftPixel"] = "28, 27",
+            ["imageSize"] = "232, 250"
+        },
+        ["Rarity"] = {
+            ["RarityNumber"] = 2,
+            ["Lootbag"] = "Rare",
+            ["_id"] = "Rare",
+            ["Color"] = Color3.fromRGB(113, 255, 62),
+            ["DifficultyThreshold"] = 100,
+            ["DisplayName"] = "Rare",
+            ["Message"] = "function: 0x66722269a76c15b4",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "2 | Rare"
+        },
+        ["Tradable"] = false,
+        ["DisplayName"] = "Pixel Booth",
+        ["_script"] = "Booth | Pixel",
+        ["Model"] = "Model",
+        ["_id"] = "Pixel",
+        ["Icon"] = "rbxassetid://17028277869",
+        ["Desc"] = "8-bit deals! beebop!"
+    },
+    ["Safety"] = {
+        ["iconSettings"] = {
+            ["faceBottomRightPixel"] = "189, 88",
+            ["faceTopLeftPixel"] = "49, 15",
+            ["imageSize"] = "250, 208"
+        },
+        ["Rarity"] = {
+            ["RarityNumber"] = 5,
+            ["Lootbag"] = "Mythical",
+            ["_id"] = "Mythical",
+            ["Color"] = Color3.fromRGB(255, 43, 100),
+            ["DifficultyThreshold"] = 500000,
+            ["DisplayName"] = "Mythical",
+            ["Message"] = "function: 0x76bd95e45f4c4894",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "5 | Mythical"
+        },
+        ["Tradable"] = false,
+        ["DisplayName"] = "Safety Booth",
+        ["_script"] = "Booth | Safety",
+        ["Model"] = "Model",
+        ["_id"] = "Safety",
+        ["Icon"] = "rbxassetid://18127818411",
+        ["Desc"] = "Secure the trade!"
+    }
+}
