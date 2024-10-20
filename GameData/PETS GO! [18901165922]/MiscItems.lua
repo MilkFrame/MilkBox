1b937ef158fa1bf0de5edd6175ab69935eb2a97e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-21 01:23:32 +1100

    Create MiscItems.lua

---
+++ GameData/PETS GO! [18901165922]/MiscItems.lua
@@ -0,0 +1,45 @@
+data = {
+    ["Rotten Potato"] = {
+        ["_script"] = "Rotten Potato",
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DifficultyThreshold"] = 10000000,
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0x8e4bd590b51b8674",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["Tradable"] = false,
+        ["_id"] = "Rotten Potato",
+        ["Desc"] = "FOR ADMIN USE - Lets you do a bunch of cool stuff.",
+        ["Icon"] = "rbxassetid://13959016084",
+        ["DisplayName"] = "Rotten Potato"
+    },
+    ["Index Token"] = {
+        ["_script"] = "Index Token",
+        ["_id"] = "Index Token",
+        ["Rarity"] = {
+            ["RarityNumber"] = 2,
+            ["Lootbag"] = "Rare",
+            ["_id"] = "Rare",
+            ["Color"] = Color3.fromRGB(113, 255, 62),
+            ["DifficultyThreshold"] = 100,
+            ["DisplayName"] = "Rare",
+            ["Message"] = "function: 0x66722269a76c15b4",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "2 | Rare"
+        },
+        ["Tradable"] = false,
+        ["Desc"] = [[Earned for every pet you index!\n\nRedeem in the Index Machine!]],
+        ["AltIcon"] = "rbxassetid://16042314493",
+        ["Icon"] = "rbxassetid://16047269848",
+        ["DisplayName"] = "Index Token"
+    }
+}
