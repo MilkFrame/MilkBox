3922cfb5ce6bc5c37236fc11b39ebe683c076c0e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-21 01:26:06 +1100

    Create PetCharms.lua

---
+++ GameData/PETS GO! [18901165922]/PetCharms.lua
@@ -0,0 +1,121 @@
+data = {
+    ["Secret"] = {
+        ["_script"] = "PetCharm | Secret",
+        ["Rarity"] = {
+            ["RarityNumber"] = 998,
+            ["Lootbag"] = "Secret",
+            ["_id"] = "Secret",
+            ["Color"] = Color3.fromRGB(173, 79, 255),
+            ["DisplayName"] = "Secret",
+            ["Message"] = "function: 0xec6838b2168c3f54",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "Secret"
+        },
+        ["_id"] = "Secret",
+        ["Boost"] = 30,
+        ["Icon"] = "rbxassetid://17822205696",
+        ["DisplayName"] = "Secret"
+    },
+    ["Exotic"] = {
+        ["_script"] = "PetCharm 6 | Exotic",
+        ["Rarity"] = {
+            ["RarityNumber"] = 6,
+            ["Lootbag"] = "Exotic",
+            ["_id"] = "Exotic",
+            ["Color"] = Color3.fromRGB(255, 29, 251),
+            ["DifficultyThreshold"] = 10000000,
+            ["DisplayName"] = "Exotic",
+            ["Message"] = "function: 0x8e4bd590b51b8674",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "6 | Exotic"
+        },
+        ["_id"] = "Exotic",
+        ["Boost"] = 5,
+        ["Icon"] = "rbxassetid://16334437019",
+        ["DisplayName"] = "Exotic"
+    },
+    ["Huge"] = {
+        ["ExclusiveLevel"] = 4,
+        ["Rarity"] = {
+            ["RarityNumber"] = 999,
+            ["Lootbag"] = "Exclusive",
+            ["_id"] = "Exclusive",
+            ["Color"] = Color3.fromRGB(173, 79, 255),
+            ["DisplayName"] = "Exclusive",
+            ["Message"] = "function: 0x92c72e94ee5dc7f4",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "Exclusive"
+        },
+        ["_id"] = "Huge",
+        ["_script"] = "PetCharm | Huge",
+        ["Boost"] = 50,
+        ["Icon"] = "rbxassetid://15200206892",
+        ["DisplayName"] = "Huge"
+    },
+    ["Superior"] = {
+        ["_script"] = "PetCharm 8 | Superior",
+        ["Rarity"] = {
+            ["RarityNumber"] = 8,
+            ["Lootbag"] = "Superior",
+            ["_id"] = "Superior",
+            ["Color"] = Color3.fromRGB(195, 255, 255),
+            ["DifficultyThreshold"] = 100000000,
+            ["DisplayName"] = "Superior",
+            ["Message"] = "function: 0x8486bf3367135fb4",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "8 | Superior"
+        },
+        ["_id"] = "Superior",
+        ["Boost"] = 15,
+        ["Icon"] = "rbxassetid://16334437151",
+        ["DisplayName"] = "Superior"
+    },
+    ["Celestial"] = {
+        ["_script"] = "PetCharm 9 | Celestial",
+        ["Rarity"] = {
+            ["RarityNumber"] = 9,
+            ["Lootbag"] = "Celestial",
+            ["_id"] = "Celestial",
+            ["Color"] = Color3.fromRGB(187, 196, 255),
+            ["DifficultyThreshold"] = 500000000,
+            ["DisplayName"] = "Celestial",
+            ["Message"] = "function: 0xdd070623df0eb534",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "9 | Celestial"
+        },
+        ["_id"] = "Celestial",
+        ["Boost"] = 20,
+        ["Icon"] = "rbxassetid://16334436886",
+        ["DisplayName"] = "Celestial"
+    },
+    ["Divine"] = {
+        ["_script"] = "PetCharm 7 | Divine",
+        ["Rarity"] = {
+            ["RarityNumber"] = 7,
+            ["Lootbag"] = "Divine",
+            ["_id"] = "Divine",
+            ["Color"] = Color3.fromRGB(255, 236, 88),
+            ["DifficultyThreshold"] = 50000000,
+            ["DisplayName"] = "Divine",
+            ["Message"] = "function: 0xd464d388c8609ed4",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = true,
+            ["_script"] = "7 | Divine"
+        },
+        ["_id"] = "Divine",
+        ["Boost"] = 10,
+        ["Icon"] = "rbxassetid://16334436751",
+        ["DisplayName"] = "Divine"
+    }
+}
