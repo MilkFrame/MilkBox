c9fb51aacd3608b5e6da1add6c650e1c4a80d227
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-21 01:22:25 +1100

    Create Currency.lua

---
+++ GameData/PETS GO! [18901165922]/Currency.lua
@@ -0,0 +1,83 @@
+data = {
+    ["Diamonds"] = {
+        ["Rarity"] = {
+            ["RarityNumber"] = 2,
+            ["Lootbag"] = "Rare",
+            ["_id"] = "Rare",
+            ["Color"] = Color3.fromRGB(113, 255, 62),
+            ["DifficultyThreshold"] = 100,
+            ["DisplayName"] = "Rare",
+            ["Message"] = "function: 0x0e3be00bd93e9785",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "2 | Rare"
+        },
+        ["Tradable"] = true,
+        ["Tiers"] = {
+            {
+                ["orbImage"] = "rbxassetid://15466421090",
+                ["imageOutline"] = "rbxassetid://14867116353",
+                ["Order"] = 1,
+                ["isBottom"] = true,
+                ["rainData"] = {["LightEmission"] = 0.2},
+                ["value"] = 1,
+                ["tierName"] = "Diamonds",
+                ["orbImageMobile"] = "rbxassetid://15466421090",
+                ["textColor"] = Color3.fromRGB(129, 253, 255)
+            }
+        },
+        ["DisplayName"] = "Diamonds",
+        ["_script"] = "Currency | Diamonds",
+        ["_index"] = 2,
+        ["_id"] = "Diamonds",
+        ["MaxAmount"] = 25000000000,
+        ["BagTiers"] = {
+            {["value"] = 0, ["image"] = "rbxassetid://15058306297"},
+            {["value"] = 1000, ["image"] = "rbxassetid://15058306546"},
+            {["value"] = 10000, ["image"] = "rbxassetid://15058306840"}
+        },
+        ["Desc"] = "Diamonds!"
+    },
+    ["Coins"] = {
+        ["Rarity"] = {
+            ["RarityNumber"] = 1,
+            ["Lootbag"] = "Basic",
+            ["_id"] = "Basic",
+            ["Color"] = Color3.fromRGB(148, 145, 167),
+            ["DifficultyThreshold"] = 0,
+            ["DisplayName"] = "Basic",
+            ["Message"] = "function: 0x0bc847da527fac65",
+            ["ItemSlot"] = "ItemSlot",
+            ["Gradient"] = "Gradient",
+            ["Announce"] = false,
+            ["_script"] = "1 | Basic"
+        },
+        ["Tradable"] = false,
+        ["Tiers"] = {
+            {
+                ["orbImage"] = "rbxassetid://15466375741",
+                ["imageOutline"] = "rbxassetid://14867116080",
+                ["isBottom"] = true,
+                ["value"] = 1,
+                ["Order"] = 1,
+                ["tierName"] = "Gold Coins",
+                ["orbImageMobile"] = "rbxassetid://84203005837826",
+                ["textColor"] = Color3.fromRGB(255, 252, 219)
+            }
+        },
+        ["DisplayName"] = "Coins",
+        ["_script"] = "Currency | Coins",
+        ["_index"] = 1,
+        ["IsWorldCurrency"] = false,
+        ["_id"] = "Coins",
+        ["BagTiers"] = {
+            {["value"] = 0, ["image"] = "rbxassetid://15058306423"},
+            {["value"] = 1000, ["image"] = "rbxassetid://15058306716"},
+            {["value"] = 10000, ["image"] = "rbxassetid://15058306965"}
+        },
+        ["MaxAmount"] = 1000000000,
+        ["PermitAutoLootScaling"] = true,
+        ["Desc"] = "Gold!"
+    }
+}
