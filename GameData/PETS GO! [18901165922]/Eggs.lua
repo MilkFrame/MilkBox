013facfe0ac845963f4b7ada7ad03cc5628260cd
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-21 01:24:00 +1100

    Create Eggs.lua

---
+++ GameData/PETS GO! [18901165922]/Eggs.lua
@@ -0,0 +1,258 @@
+data = {
+    ["Spotted Egg"] = {
+        ["Number"] = 2,
+        ["_script"] = "2 | Spotted Egg",
+        ["Name"] = "Spotted Egg",
+        ["Model"] = "Egg",
+        ["AssociatedUpgradeId"] = "Egg 2",
+        ["_id"] = "Spotted Egg",
+        ["Icon"] = "rbxassetid://15172353804",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "114, 242",
+            ["FaceTopLeftPixel"] = "96, 7",
+            ["ImageSize"] = "214, 250"
+        }
+    },
+    ["Cracked Stone Egg"] = {
+        ["Number"] = 10,
+        ["Name"] = "Cracked Stone Egg",
+        ["Model"] = "Egg",
+        ["_script"] = "10 | Cracked Stone Egg",
+        ["_id"] = "Cracked Stone Egg",
+        ["Icon"] = "rbxassetid://15172354305",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "113, 242",
+            ["FaceTopLeftPixel"] = "99, 7",
+            ["ImageSize"] = "214, 250"
+        }
+    },
+    ["Cracked Egg"] = {
+        ["Number"] = 1,
+        ["_script"] = "1 | Cracked Egg",
+        ["Name"] = "Cracked Egg",
+        ["Model"] = "Egg",
+        ["_id"] = "Cracked Egg",
+        ["MinDifficulty"] = 0,
+        ["Icon"] = "rbxassetid://15172355963",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "114, 242",
+            ["FaceTopLeftPixel"] = "96, 7",
+            ["ImageSize"] = "214, 250"
+        }
+    },
+    ["Mushroom Egg"] = {
+        ["Number"] = 9,
+        ["_script"] = "9 | Mushroom Egg",
+        ["Name"] = "Mushroom Egg",
+        ["Model"] = "Egg",
+        ["AssociatedUpgradeId"] = "Egg 9",
+        ["_id"] = "Mushroom Egg",
+        ["Icon"] = "rbxassetid://15172354843",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "112, 243",
+            ["FaceTopLeftPixel"] = "96, 7",
+            ["ImageSize"] = "214, 250"
+        }
+    },
+    ["Paw Print Egg"] = {
+        ["Number"] = 4,
+        ["_script"] = "4 | Paw Print Egg",
+        ["Name"] = "Paw Print Egg",
+        ["Model"] = "Egg",
+        ["AssociatedUpgradeId"] = "Egg 4",
+        ["_id"] = "Paw Print Egg",
+        ["Icon"] = "rbxassetid://15172354552",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "93, 244",
+            ["FaceTopLeftPixel"] = "84, 48",
+            ["ImageSize"] = "178, 250"
+        }
+    },
+    ["Geode Egg"] = {
+        ["Number"] = 11,
+        ["Name"] = "Geode Egg",
+        ["Model"] = "Egg",
+        ["_script"] = "11 | Geode Egg",
+        ["_id"] = "Geode Egg",
+        ["Icon"] = "rbxassetid://15172355592",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "133, 243",
+            ["FaceTopLeftPixel"] = "119, 38",
+            ["ImageSize"] = "242, 250"
+        }
+    },
+    ["Dog Egg"] = {
+        ["Number"] = 8,
+        ["_script"] = "8 | Dog Egg",
+        ["Name"] = "Dog Egg",
+        ["Model"] = "Egg",
+        ["AssociatedUpgradeId"] = "Egg 8",
+        ["_id"] = "Dog Egg",
+        ["Icon"] = "rbxassetid://15172355848",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "108, 243",
+            ["FaceTopLeftPixel"] = "100, 22",
+            ["ImageSize"] = "202, 250"
+        }
+    },
+    ["Crimson Egg"] = {
+        ["Number"] = 14,
+        ["Name"] = "Crimson Egg",
+        ["Model"] = "Egg",
+        ["_script"] = "14 | Crimson Egg",
+        ["_id"] = "Crimson Egg",
+        ["Icon"] = "rbxassetid://15172355905",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "114, 242",
+            ["FaceTopLeftPixel"] = "96, 7",
+            ["ImageSize"] = "214, 250"
+        }
+    },
+    ["Ice Egg"] = {
+        ["Number"] = 16,
+        ["Name"] = "Ice Egg",
+        ["Model"] = "Egg",
+        ["_script"] = "16 | Ice Egg",
+        ["_id"] = "Ice Egg",
+        ["Icon"] = "rbxassetid://15172355123",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "114, 242",
+            ["FaceTopLeftPixel"] = "96, 7",
+            ["ImageSize"] = "214, 250"
+        }
+    },
+    ["Overgrown Egg"] = {
+        ["Number"] = 13,
+        ["Name"] = "Overgrown Egg",
+        ["Model"] = "Egg",
+        ["_script"] = "13 | Overgrown Egg",
+        ["_id"] = "Overgrown Egg",
+        ["Icon"] = "rbxassetid://15172354701",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "120, 232",
+            ["FaceTopLeftPixel"] = "101, 6",
+            ["ImageSize"] = "249, 250"
+        }
+    },
+    ["Thawed Egg"] = {
+        ["Number"] = 18,
+        ["Name"] = "Thawed Egg",
+        ["Model"] = "Egg",
+        ["_script"] = "18 | Thawed Egg",
+        ["_id"] = "Thawed Egg",
+        ["Icon"] = "rbxassetid://15172353452",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "114, 242",
+            ["FaceTopLeftPixel"] = "97, 7",
+            ["ImageSize"] = "214, 250"
+        }
+    },
+    ["Magma Egg"] = {
+        ["Number"] = 19,
+        ["Name"] = "Magma Egg",
+        ["Model"] = "Egg",
+        ["_script"] = "19 | Magma Egg",
+        ["_id"] = "Magma Egg",
+        ["Icon"] = "rbxassetid://15172354975",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "114, 242",
+            ["FaceTopLeftPixel"] = "97, 7",
+            ["ImageSize"] = "214, 250"
+        }
+    },
+    ["Flowery Fields Egg"] = {
+        ["Number"] = 7,
+        ["_script"] = "7 | Flowery Fields Egg",
+        ["Name"] = "Flowery Fields Egg",
+        ["Model"] = "Egg",
+        ["AssociatedUpgradeId"] = "Egg 7",
+        ["_id"] = "Flowery Fields Egg",
+        ["Icon"] = "rbxassetid://15172355499",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "115, 243",
+            ["FaceTopLeftPixel"] = "103, 12",
+            ["ImageSize"] = "215, 250"
+        }
+    },
+    ["Cat Egg"] = {
+        ["Number"] = 3,
+        ["_script"] = "3 | Cat Egg",
+        ["Name"] = "Cat Egg",
+        ["Model"] = "Egg",
+        ["AssociatedUpgradeId"] = "Egg 3",
+        ["_id"] = "Cat Egg",
+        ["Icon"] = "rbxassetid://15172356302",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "114, 243",
+            ["FaceTopLeftPixel"] = "97, 7",
+            ["ImageSize"] = "214, 250"
+        }
+    },
+    ["Dragon Egg"] = {
+        ["Number"] = 12,
+        ["Name"] = "Dragon Egg",
+        ["Model"] = "Egg",
+        ["_script"] = "12 | Dragon Egg",
+        ["_id"] = "Dragon Egg",
+        ["Icon"] = "rbxassetid://15172355798",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "138, 184",
+            ["FaceTopLeftPixel"] = "129, 38",
+            ["ImageSize"] = "250, 189"
+        }
+    },
+    ["Wooden Egg"] = {
+        ["Number"] = 5,
+        ["_script"] = "5 | Wooden Egg",
+        ["Name"] = "Wooden Egg",
+        ["Model"] = "Egg",
+        ["AssociatedUpgradeId"] = "Egg 5",
+        ["_id"] = "Wooden Egg",
+        ["Icon"] = "rbxassetid://15172353173",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "114, 242",
+            ["FaceTopLeftPixel"] = "98, 7",
+            ["ImageSize"] = "214, 250"
+        }
+    },
+    ["Metal Egg"] = {
+        ["Number"] = 15,
+        ["Name"] = "Metal Egg",
+        ["Model"] = "Egg",
+        ["_script"] = "15 | Metal Egg",
+        ["_id"] = "Metal Egg",
+        ["Icon"] = "rbxassetid://15574219511",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "115, 243",
+            ["FaceTopLeftPixel"] = "98, 7",
+            ["ImageSize"] = "214, 250"
+        }
+    },
+    ["Icicle Egg"] = {
+        ["Number"] = 17,
+        ["Name"] = "Icicle Egg",
+        ["Model"] = "Egg",
+        ["_script"] = "17 | Icicle Egg",
+        ["_id"] = "Icicle Egg",
+        ["Icon"] = "rbxassetid://15172355062",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "133, 184",
+            ["FaceTopLeftPixel"] = "121, 5",
+            ["ImageSize"] = "250, 190"
+        }
+    },
+    ["Honeyhive Egg"] = {
+        ["Number"] = 6,
+        ["_script"] = "6 | Honeyhive Egg",
+        ["Name"] = "Honeyhive Egg",
+        ["Model"] = "Egg",
+        ["AssociatedUpgradeId"] = "Egg 6",
+        ["_id"] = "Honeyhive Egg",
+        ["Icon"] = "rbxassetid://15172355290",
+        ["IconSettings"] = {
+            ["FaceBottomRightPixel"] = "122, 243",
+            ["FaceTopLeftPixel"] = "106, 6",
+            ["ImageSize"] = "235, 250"
+        }
+    }
+}
