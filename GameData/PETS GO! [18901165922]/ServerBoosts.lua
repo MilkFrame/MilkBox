c7fc8aa4e041f7fe8f5efce3833e61320551f31c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-21 01:24:42 +1100

    Create ServerBoosts.lua

---
+++ GameData/PETS GO! [18901165922]/ServerBoosts.lua
@@ -0,0 +1,41 @@
+data = {
+    ["Lucky"] = {
+        ["RequiredUpgradeId"] = "Luck Event",
+        ["Description"] = "Server wide lucky boost of 20%!",
+        ["Power"] = 20,
+        ["Color"] = Color3.fromRGB(85, 255, 0),
+        ["DisplayName"] = "Server Lucky Event",
+        ["_script"] = "ServerBoost | Lucky",
+        ["Weight"] = 1,
+        ["_id"] = "Lucky",
+        ["StartMessage"] = "<font color='#55ff00'>%s</font> <font color='#ffffff'>is feeling</font> <font color='#55ff00'>Lucky</font><font color='#ffffff'>! Everyone gets free luck for </font><font color='#55ff00'>5 minutes!</font>",
+        ["Icon"] = "rbxassetid://111907197474761",
+        ["EndMessage"] = "<font color='#ff4245'>%s's</font> Luck has ran out! <font color='#ff4245'>Lucky Event</font> has ended!"
+    },
+    ["Faster Rolling"] = {
+        ["RequiredUpgradeId"] = "Faster Rolls Event",
+        ["Description"] = "Server wide roll speed boost of 20%!",
+        ["Power"] = 20,
+        ["Color"] = Color3.fromRGB(29, 255, 247),
+        ["DisplayName"] = "Server Faster Rolling Event",
+        ["_script"] = "ServerBoost | Faster Rolling",
+        ["Weight"] = 1,
+        ["_id"] = "Faster Rolling",
+        ["StartMessage"] = "<font color='#1dfff7'>%s</font> <font color='#ffffff'>is feeling</font> <font color='#1dfff7'>Speedy</font><font color='#ffffff'>! Everyone gets free roll speed for </font><font color='#1dfff7'>5 minutes!</font>",
+        ["Icon"] = "rbxassetid://106477285212597",
+        ["EndMessage"] = "<font color='#ff4245'>%s's</font> Speed has ran out! <font color='#ff4245'>Faster Rolling Event</font> has ended!"
+    },
+    ["Coins"] = {
+        ["RequiredUpgradeId"] = "More Coins Event",
+        ["Description"] = "Server wide coin boost of 20%!",
+        ["Power"] = 20,
+        ["Color"] = Color3.fromRGB(242, 195, 55),
+        ["DisplayName"] = "Server Coins Event",
+        ["_script"] = "ServerBoost | Coins",
+        ["Weight"] = 1,
+        ["_id"] = "Coins",
+        ["StartMessage"] = "<font color='#f2c337'>%s</font> <font color='#ffffff'>has</font> <font color='#f2c337'>Gold Fever</font><font color='#ffffff'>! Everyone gets a free coin boost for </font><font color='#f2c337'>5 minutes!</font>",
+        ["Icon"] = "rbxassetid://81352030279237",
+        ["EndMessage"] = "<font color='#ff4245'>%s's</font> fortune has ran out! <font color='#ff4245'>Coin Boost Event</font> has ended!"
+    }
+}
