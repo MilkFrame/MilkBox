747ffa5975492a86fef15e682cbc0998e7cf8853
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-27 18:25:16 +1100

    Create Debug.lua

---
+++ GameData/Wings of Glory [338574920]/Debug.lua
@@ -0,0 +1,22 @@
+data = {
+    ["Debug"] = {
+        ["Enabled"] = true,
+        ["__init"] = "function: 0x28de077577b4a6aa",
+        ["clearDrawings"] = "function: 0xb25f08923ba0effa",
+        ["warn"] = "function: 0x4c5c11aaf834e64a",
+        ["toggleDebugObjects"] = "function: 0x31dcb513aac4126a",
+        ["print"] = "function: 0x565045095713b43a",
+        ["output"] = "function: 0xceed7490efe7154a",
+        ["drawBeam"] = "function: 0xa26226dcc87d800a",
+        ["drawValue"] = "function: 0xba710b78536f50fa",
+        ["setEnabled"] = "function: 0x6ea896181177392a",
+        ["error"] = "function: 0x4ed8a65b49e848da",
+        ["drawRadius"] = "function: 0x16b83562f0419d8a"
+    },
+    ["DebugConfig"] = {
+        ["DEFAULT_KEY_ICON"] = "rbxassetid://10621456053",
+        ["PREFIXES"] = {["Error"] = "❌", ["Print"] = "🔣", ["Warn"] = "⚠️"},
+        ["DEFAULT_KEY_COLOR"] = Color3.fromRGB(255, 255, 255),
+        ["DEFAULT_VALUE_COLOR"] = Color3.fromRGB(116, 255, 3)
+    }
+}
