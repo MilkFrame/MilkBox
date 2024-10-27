926961963cba3255a99c58dc6af95411741f0f4d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-27 18:21:52 +1100

    Create BombData.lua

---
+++ GameData/Wings of Glory [338574920]/BombData.lua
@@ -0,0 +1,12 @@
+data = {
+    ["AN M57"] = {["BombRadius"] = 103, ["bomb_icon"] = 2090729205, ["BombWeight"] = 108.8},
+    ["FAB 50"] = {["BombRadius"] = 69, ["bomb_icon"] = 2090730212, ["BombWeight"] = 64},
+    ["SC250JA"] = {["BombRadius"] = 121, ["bomb_icon"] = 2090732803, ["BombWeight"] = 250},
+    ["AN M65A1"] = {["BombRadius"] = 138, ["bomb_icon"] = 2090729677, ["BombWeight"] = 439.6},
+    ["Army Type 94 GPHE"] = {["BombRadius"] = 63, ["bomb_icon"] = 2090729942, ["BombWeight"] = 50},
+    ["AN M64A1"] = {["BombRadius"] = 118, ["bomb_icon"] = 2090729416, ["BombWeight"] = 223},
+    ["G.P 500 lb Mk.IV"] = {["BombRadius"] = 106, ["bomb_icon"] = 2087489284, ["BombWeight"] = 213.1},
+    ["Navy Type 25 Model 2"] = {["BombRadius"] = 116, ["bomb_icon"] = 2090730987, ["BombWeight"] = 253},
+    ["Navy Type 97 N.6"] = {["BombRadius"] = 67, ["bomb_icon"] = 2090731293, ["BombWeight"] = 60},
+    ["AN M30"] = {["BombRadius"] = 67, ["bomb_icon"] = 2090728938, ["BombWeight"] = 45.8}
+}
