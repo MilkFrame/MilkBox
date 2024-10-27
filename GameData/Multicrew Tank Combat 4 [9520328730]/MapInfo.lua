40dd544d5626d3dd323634001bd40b39734f6bd8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-27 18:32:35 +1100

    Create MapInfo.lua

---
+++ GameData/Multicrew Tank Combat 4 [9520328730]/MapInfo.lua
@@ -0,0 +1,305 @@
+data = {
+    ["maps"] = {
+        {
+            ["Type"] = Color3.fromRGB(56, 83, 65),
+            ["DisplayName"] = "Gensokyo",
+            ["ModelID"] = 17335210188,
+            ["Image"] = "rbxassetid://12454067836",
+            ["Name"] = "JapanMap",
+            ["RigConstant"] = 1,
+            ["Background"] = "rbxassetid://12431248351",
+            ["Ingame"] = true,
+            ["Size"] = "3000x3000"
+        },
+        {
+            ["Image"] = "http://www.roblox.com/asset/?id=8425069718",
+            ["Name"] = "SunnyValley",
+            ["Type"] = Color3.fromRGB(132, 122, 101),
+            ["Ingame"] = false,
+            ["RigConstant"] = 1,
+            ["Size"] = "5000x5000",
+            ["DisplayName"] = "Sunny Valley"
+        },
+        {
+            ["Type"] = Color3.fromRGB(94, 104, 77),
+            ["DisplayName"] = "Powerplant",
+            ["ModelID"] = 16022674231,
+            ["Image"] = "rbxassetid://15907512200",
+            ["Name"] = "PowerPlant",
+            ["RigConstant"] = 1,
+            ["Background"] = "rbxassetid://18230256963",
+            ["Ingame"] = true,
+            ["Size"] = "4000x4000"
+        },
+        {
+            ["Type"] = Color3.fromRGB(76, 71, 64),
+            ["DisplayName"] = "Roinburg",
+            ["ModelID"] = 16083842928,
+            ["Image"] = "rbxassetid://12107055033",
+            ["Name"] = "Roinburg",
+            ["RigConstant"] = 1,
+            ["Background"] = "rbxassetid://17335263316",
+            ["Ingame"] = true,
+            ["Size"] = "2800x3500"
+        },
+        {
+            ["Type"] = Color3.fromRGB(106, 127, 94),
+            ["DisplayName"] = "Sokolovka",
+            ["ModelID"] = 16107453735,
+            ["Image"] = "rbxassetid://15622271020",
+            ["Name"] = "Sokolovka",
+            ["RigConstant"] = 1,
+            ["Background"] = "rbxassetid://17335263507",
+            ["Ingame"] = true,
+            ["Size"] = "5000x5000"
+        },
+        {
+            ["Image"] = "rbxassetid://165331391",
+            ["Name"] = "APMAP",
+            ["Ingame"] = false,
+            ["Type"] = Color3.fromRGB(106, 127, 94),
+            ["Size"] = "3000x3000",
+            ["DisplayName"] = "Armored Patrol"
+        },
+        {
+            ["Type"] = Color3.fromRGB(67, 86, 62),
+            ["DisplayName"] = "Radar Station",
+            ["ModelID"] = 16022660947,
+            ["Image"] = "rbxassetid://12215281124",
+            ["Name"] = "Radar",
+            ["RigConstant"] = 1,
+            ["Background"] = "rbxassetid://17335262756",
+            ["Ingame"] = true,
+            ["Size"] = "2100x6200"
+        },
+        {
+            ["Type"] = Color3.fromRGB(203, 180, 133),
+            ["DisplayName"] = "Dustbowl",
+            ["ModelID"] = 16107447664,
+            ["Image"] = "rbxassetid://12954426390",
+            ["Name"] = "Dustbowl",
+            ["RigConstant"] = 1,
+            ["Background"] = "rbxassetid://17335262017",
+            ["Ingame"] = true,
+            ["Size"] = "Hexagon (1700stud side)"
+        },
+        {
+            ["Type"] = Color3.fromRGB(33, 36, 32),
+            ["DisplayName"] = "Chernobyl",
+            ["ModelID"] = 18619515372,
+            ["Image"] = "rbxassetid://12949794091",
+            ["Name"] = "Chernobyl",
+            ["RigConstant"] = 1,
+            ["Background"] = "rbxassetid://12980851420",
+            ["Ingame"] = true,
+            ["Size"] = "4000x3500"
+        },
+        {
+            ["Type"] = Color3.fromRGB(224, 255, 249),
+            ["DisplayName"] = "Arctic Airbase",
+            ["ModelID"] = 16083727049,
+            ["Image"] = "rbxassetid://14192399561",
+            ["Name"] = "Airbase",
+            ["RigConstant"] = 1,
+            ["Background"] = "rbxassetid://17335261778",
+            ["Ingame"] = true,
+            ["Size"] = "4000x3800"
+        },
+        {
+            ["Type"] = Color3.fromRGB(55, 116, 57),
+            ["DisplayName"] = "Normandy Bocage",
+            ["ModelID"] = 18874647156,
+            ["Image"] = "rbxassetid://16206075615",
+            ["Name"] = "Normandy Bocage",
+            ["RigConstant"] = 1,
+            ["Background"] = "rbxassetid://16299159885",
+            ["Ingame"] = true,
+            ["Size"] = "3000x6000"
+        },
+        {
+            ["Type"] = Color3.fromRGB(55, 116, 57),
+            ["DisplayName"] = "Villers-Sommeil",
+            ["ModelID"] = 16163880185,
+            ["Image"] = "rbxassetid://16272300093",
+            ["Name"] = "Villers-Sommeil",
+            ["RigConstant"] = 1,
+            ["Background"] = "rbxassetid://16299170791",
+            ["Ingame"] = true,
+            ["Size"] = "3000x3000"
+        },
+        {
+            ["Type"] = Color3.fromRGB(125, 172, 88),
+            ["DisplayName"] = "Cloudy Valley",
+            ["ModelID"] = 16025077341,
+            ["Image"] = "",
+            ["Name"] = "Cloudy Valley",
+            ["RigConstant"] = 1,
+            ["Ingame"] = true,
+            ["Size"] = "3000x3000"
+        },
+        {
+            ["Type"] = Color3.fromRGB(194, 215, 221),
+            ["DisplayName"] = "Snowy Tundra",
+            ["ModelID"] = 16968559093,
+            ["Image"] = "rbxassetid://16968505710",
+            ["Name"] = "Snowy Tundra",
+            ["RigConstant"] = 1,
+            ["Ingame"] = true,
+            ["Size"] = "4000x4000"
+        },
+        {
+            ["Type"] = Color3.fromRGB(56, 83, 65),
+            ["DisplayName"] = "Old Gensokyo",
+            ["ModelID"] = 16969052355,
+            ["Image"] = "rbxassetid://10766536459",
+            ["Name"] = "OldJapanMap",
+            ["RigConstant"] = 1,
+            ["Ingame"] = true,
+            ["Size"] = "3000x3000"
+        },
+        {
+            ["Type"] = Color3.fromRGB(71, 83, 62),
+            ["DisplayName"] = "Old Reactor",
+            ["ModelID"] = 16969800978,
+            ["Image"] = "rbxassetid://10876957580",
+            ["Name"] = "OldReactor",
+            ["RigConstant"] = 1,
+            ["Ingame"] = true,
+            ["Size"] = "5000x5000"
+        },
+        {
+            ["Type"] = Color3.fromRGB(71, 83, 62),
+            ["DisplayName"] = "Zone 11",
+            ["ModelID"] = 17154388459,
+            ["Image"] = "rbxassetid://14400041423",
+            ["Name"] = "Fields",
+            ["RigConstant"] = 1,
+            ["Background"] = "rbxassetid://17335262378",
+            ["Ingame"] = true,
+            ["Size"] = "8000x8000"
+        },
+        {
+            ["Type"] = Color3.fromRGB(71, 83, 62),
+            ["DisplayName"] = "Muddy Fields",
+            ["ModelID"] = 17405547002,
+            ["Image"] = "rbxassetid://17621001196",
+            ["Name"] = "Muddy Fields",
+            ["RigConstant"] = 1,
+            ["Background"] = "rbxassetid://17621001196",
+            ["Ingame"] = true,
+            ["Size"] = "3000x6000"
+        },
+        {
+            ["Type"] = Color3.fromRGB(203, 180, 133),
+            ["DisplayName"] = "Dustbowl II",
+            ["ModelID"] = 18744323252,
+            ["Image"] = "rbxassetid://17464932063",
+            ["Name"] = "Dustbowl2",
+            ["RigConstant"] = 1,
+            ["Background"] = "rbxassetid://17621001696",
+            ["Ingame"] = true,
+            ["Size"] = "at least 5"
+        },
+        {
+            ["ModelID"] = 17694554525,
+            ["Image"] = "rbxassetid://17621001696",
+            ["Name"] = "TESTING",
+            ["Ingame"] = true,
+            ["Type"] = Color3.fromRGB(203, 180, 133),
+            ["Background"] = "rbxassetid://10781894501",
+            ["Size"] = "no fuel",
+            ["DisplayName"] = "TESTING"
+        },
+        {
+            ["Type"] = Color3.fromRGB(203, 180, 133),
+            ["DisplayName"] = "Sandy Place",
+            ["ModelID"] = 17704091191,
+            ["Image"] = "rbxassetid://17901003267",
+            ["Name"] = "Sandy Place",
+            ["RigConstant"] = 1,
+            ["Background"] = "rbxassetid://17901003267",
+            ["Ingame"] = true,
+            ["Size"] = "4000x9000"
+        },
+        {
+            ["Type"] = Color3.fromRGB(70, 80, 55),
+            ["DisplayName"] = "Fulvia Gap",
+            ["ModelID"] = 112444499053746,
+            ["Image"] = "rbxassetid://18914252548",
+            ["Name"] = "Fulvia Gap",
+            ["RigConstant"] = 1,
+            ["Background"] = "rbxassetid://18914252548",
+            ["Ingame"] = true,
+            ["Size"] = "4000x8000"
+        },
+        {
+            ["Type"] = Color3.fromRGB(86, 122, 71),
+            ["DisplayName"] = "Waterloo",
+            ["ModelID"] = 18931059333,
+            ["Image"] = "idk",
+            ["Name"] = "Waterloo",
+            ["RigConstant"] = 1,
+            ["Ingame"] = true,
+            ["Size"] = "8700x4300"
+        },
+        {
+            ["Type"] = Color3.fromRGB(86, 122, 71),
+            ["DisplayName"] = "Hougoumont",
+            ["ModelID"] = 18967161082,
+            ["Image"] = "idk",
+            ["Name"] = "Hougoumont",
+            ["RigConstant"] = 1,
+            ["Ingame"] = true,
+            ["Size"] = "8700x4300"
+        }
+    },
+    ["loadouts"] = {
+        {
+            ["Image"] = "rbxassetid://15619298206",
+            ["Name"] = "Alpha",
+            ["Ingame"] = true,
+            ["RigConstant"] = 1,
+            ["Description"] = "Delibrate and tactical",
+            ["DisplayName"] = "WW2"
+        },
+        {
+            ["Image"] = "rbxassetid://13351880915",
+            ["Name"] = "Beta",
+            ["Ingame"] = true,
+            ["RigConstant"] = 1,
+            ["Description"] = "Fast-paced and chaotic",
+            ["DisplayName"] = "Modern"
+        },
+        {
+            ["Image"] = "rbxassetid://13351880915",
+            ["Name"] = "BetaHeliTest",
+            ["Ingame"] = false,
+            ["Description"] = "Fast-paced and chaotic",
+            ["DisplayName"] = "Modern Heli"
+        },
+        {
+            ["Image"] = "rbxassetid://16244220603",
+            ["Name"] = "Alpha2",
+            ["Ingame"] = true,
+            ["RigConstant"] = 1,
+            ["Description"] = "Dadididododoooo",
+            ["DisplayName"] = "Cold War"
+        },
+        {
+            ["Image"] = "rbxassetid://16244220603",
+            ["Name"] = "Competitive (Modern)",
+            ["Ingame"] = false,
+            ["RigConstant"] = 1,
+            ["Description"] = "Dadididododoooo",
+            ["DisplayName"] = "Competitive (Modern)"
+        },
+        {
+            ["Image"] = "rbxassetid://16244220603",
+            ["Name"] = "Competitive (WW2)",
+            ["Ingame"] = false,
+            ["RigConstant"] = 1,
+            ["Description"] = "Dadididododoooo",
+            ["DisplayName"] = "Competitive (WW2)"
+        }
+    }
+}
