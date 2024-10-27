c703e2e06a52b11374f8c0636b34642fcb16a468
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-27 18:23:21 +1100

    Create SettingsList.lua

---
+++ GameData/Wings of Glory [338574920]/SettingsList.lua
@@ -0,0 +1,44 @@
+data = {
+    {
+        ["PlatformsAvailable"] = {"PC", "Mobile"},
+        ["DataName"] = "Music",
+        ["Options"] = "Toggleable",
+        ["DisplayName"] = "In-game Music"
+    },
+    {
+        ["PlatformsAvailable"] = {"PC"},
+        ["DataName"] = "LiveControlsDisplay",
+        ["Options"] = "Toggleable",
+        ["DisplayName"] = "Live Controls Display"
+    },
+    {
+        ["PlatformsAvailable"] = {"PC", "Mobile"},
+        ["DataName"] = "FoliageQuality",
+        ["CustomOptions"] = {"Disabled", "Low", "Medium", "High"},
+        ["DisplayName"] = "Foliage Quality"
+    },
+    {
+        ["PlatformsAvailable"] = {"PC"},
+        ["DataName"] = "WaterQuality",
+        ["CustomOptions"] = {"Low", "High"},
+        ["DisplayName"] = "Water Quality"
+    },
+    {
+        ["PlatformsAvailable"] = {"PC", "Mobile"},
+        ["DataName"] = "DisableSiren",
+        ["CustomOptions"] = {"Enabled", "Disabled"},
+        ["DisplayName"] = "Siren Sounds"
+    },
+    {
+        ["PlatformsAvailable"] = {"PC", "Mobile"},
+        ["DataName"] = "Unit",
+        ["CustomOptions"] = {"Metric", "Imperial"},
+        ["DisplayName"] = "Units"
+    },
+    {
+        ["PlatformsAvailable"] = {"PC"},
+        ["DataName"] = "LowEnd",
+        ["CustomOptions"] = {"No", "Yes"},
+        ["DisplayName"] = "Join Optimized Servers (Poor Terrain Quality)"
+    }
+}
