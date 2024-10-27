7b38a17f4122699abefde1e526b0be13d3773125
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-27 18:29:00 +1100

    Create teamInfo.lua

---
+++ GameData/Multicrew Tank Combat 4 [9520328730]/teamInfo.lua
@@ -0,0 +1,14 @@
+data = {
+    ["Bulangol Kitty State"] = {["Flag"] = "rbxassetid://11311790002"},
+    ["Redtail PMC"] = {["Flag"] = "rbxassetid://12864749670"},
+    ["Eagle Federation"] = {["Flag"] = "rbxassetid://12866024728"},
+    ["Celestial Dragon Dynasty"] = {["Flag"] = "rbxassetid://71265835088503"},
+    ["Hawk Republic"] = {["Flag"] = "rbxassetid://12865776517"},
+    ["Great Britain"] = {["Flag"] = "rbxassetid://18276993554"},
+    ["Nightingale Unitary State"] = {["Flag"] = "rbxassetid://17414042106"},
+    ["Pigeon Freedom Movement"] = {["Flag"] = "rbxassetid://12864752962"},
+    ["FISH State"] = {["Flag"] = "rbxassetid://12864755105"},
+    ["Schwalbenheim Kingdom"] = {["Flag"] = "http://www.roblox.com/asset/?id=13029026917"},
+    ["Schwalbenheim Republic"] = {["Flag"] = "rbxassetid://17414042223"},
+    ["Felid Empire"] = {["Flag"] = "rbxassetid://10660175599"}
+}
