8afb140927c860a9bb48a52932bb808f27f7360e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-27 18:21:36 +1100

    Create AAModule.lua

---
+++ GameData/Wings of Glory [338574920]/AAModule.lua
@@ -0,0 +1,48 @@
+data = {
+    ["Turret2"] = {
+        ["ModelName"] = "Turret2",
+        ["Accuracy"] = 2,
+        ["Velocity"] = 1000,
+        ["Range"] = 600,
+        ["Health"] = 800,
+        ["ROF"] = 1500,
+        ["ShellType"] = "Kinetic",
+        ["FiringSound"] = "11390787510",
+        ["Damage"] = 100
+    },
+    ["PumpkinDrone"] = {
+        ["ModelName"] = "PumpkinDrone",
+        ["Accuracy"] = 1,
+        ["Velocity"] = 1000,
+        ["Range"] = 600,
+        ["Health"] = 800,
+        ["ROF"] = 500,
+        ["ShellType"] = "Kinetic",
+        ["FiringSound"] = "5542443810",
+        ["Damage"] = 100
+    },
+    ["flak_88"] = {
+        ["ExplosionSound"] = {"895043658", "895043930", "895044196", "895044402", "895044618"},
+        ["ModelName"] = "flak_88",
+        ["Accuracy"] = 32,
+        ["Velocity"] = 900,
+        ["Range"] = 750,
+        ["Health"] = 1500,
+        ["ROF"] = 15,
+        ["ShellType"] = "Explosive",
+        ["FiringSound"] = "889765459",
+        ["Damage"] = 380
+    },
+    ["Turret1"] = {
+        ["ExplosionSound"] = {"895043658", "895043930", "895044196", "895044402", "895044618"},
+        ["ModelName"] = "Turret1",
+        ["Accuracy"] = 78,
+        ["Velocity"] = 900,
+        ["Range"] = 750,
+        ["Health"] = 1500,
+        ["ROF"] = 60,
+        ["ShellType"] = "Explosive",
+        ["FiringSound"] = "889765459",
+        ["Damage"] = 300
+    }
+}
