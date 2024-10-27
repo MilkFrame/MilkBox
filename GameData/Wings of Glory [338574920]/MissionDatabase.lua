0e9792dbbd695575c3fc8d2679fb7df7fc87d1df
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-27 18:20:54 +1100

    Create MissionDatabase.lua

---
+++ GameData/Wings of Glory [338574920]/MissionDatabase.lua
@@ -0,0 +1,261 @@
+data = {
+    ["Wingbreaker"] = {
+        ["Type"] = "Weekly",
+        ["Objective"] = "Shoot off 30 wings",
+        ["Requirements"] = {["WingsDestroyed"] = 30},
+        ["RewardType"] = "UpgradePoints",
+        ["Reward"] = 15
+    },
+    ["Eye for an eye"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Destroy an enemy plane that has shot you down",
+        ["Requirements"] = {["RevengedPlanes"] = 1},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 90
+    },
+    ["Pilots' Nightmare"] = {
+        ["Type"] = "Weekly",
+        ["Objective"] = "Destroy 60 planes",
+        ["Requirements"] = {["WeeklyKills"] = 60},
+        ["RewardType"] = "UpgradePoints",
+        ["Reward"] = 15
+    },
+    ["France's Air Expert"] = {
+        ["Type"] = "Weekly",
+        ["Objective"] = "Get 75 kills with a French plane",
+        ["Requirements"] = {["FrancePlaneKills"] = 75},
+        ["RewardType"] = "UpgradePoints",
+        ["Reward"] = 25
+    },
+    ["Wing lover"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Shoot off 5 wings",
+        ["Requirements"] = {["WingsDestroyed"] = 5},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 90
+    },
+    ["France's Great Nightmare"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Get 5 kills with a French plane in a round",
+        ["Requirements"] = {["FrancePlaneKillsInRound"] = 5},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 105
+    },
+    ["Ground pounder"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Destroy 50 ground targets in a round",
+        ["Requirements"] = {["GroundTargetsDestroyedInRound"] = 50},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 150
+    },
+    ["Killer"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Destroy 10 planes in a round",
+        ["Requirements"] = {["PlanesDestroyedInRound"] = 10},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 90
+    },
+    ["Team player"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Save a teammate from being shot down",
+        ["Requirements"] = {["SavedTeammates"] = 1},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 60
+    },
+    ["Japan's Great Nightmare"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Get 5 kills with a Japanese plane in a round",
+        ["Requirements"] = {["JapanPlaneKillsInRound"] = 5},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 105
+    },
+    ["USSR's Great Nightmare"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Get 5 kills with a Russian plane in a round",
+        ["Requirements"] = {["USSRPlaneKillsInRound"] = 5},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 105
+    },
+    ["Pyromaniac"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Set afire 10 planes",
+        ["Requirements"] = {["PlanesSetAfire"] = 10},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 90
+    },
+    ["USSR Air Veteran"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Get 20 kills with a Russian plane",
+        ["Requirements"] = {["USSRPlaneKills"] = 20},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 90
+    },
+    ["USSR's Air Expert"] = {
+        ["Type"] = "Weekly",
+        ["Objective"] = "Get 75 kills with a Russian plane",
+        ["Requirements"] = {["USSRPlaneKills"] = 75},
+        ["RewardType"] = "UpgradePoints",
+        ["Reward"] = 25
+    },
+    ["Hero of the Sky"] = {
+        ["Type"] = "Weekly",
+        ["Objective"] = "Get MVP 10 times",
+        ["Requirements"] = {["MVP"] = 10},
+        ["RewardType"] = "Medals",
+        ["Reward"] = 50
+    },
+    ["Support"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Get 5 kill assists in a round",
+        ["Requirements"] = {["PlaneKillAssistsInRound"] = 5},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 90
+    },
+    ["USA's Great Nightmare"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Get 5 kills with an American plane in a round",
+        ["Requirements"] = {["USAPlaneKillsInRound"] = 5},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 105
+    },
+    ["Germany Air Veteran"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Get 20 kills with a German plane",
+        ["Requirements"] = {["GermanyPlaneKills"] = 20},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 90
+    },
+    ["Ace in a flight"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Destroy 5 planes without getting shot down",
+        ["Requirements"] = {["KillStreak"] = 5},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 90
+    },
+    ["Japan's Air Expert"] = {
+        ["Type"] = "Weekly",
+        ["Objective"] = "Get 75 kills with a Japanese plane",
+        ["Requirements"] = {["JapanPlaneKills"] = 75},
+        ["RewardType"] = "UpgradePoints",
+        ["Reward"] = 25
+    },
+    ["Terror of the Sky"] = {
+        ["Type"] = "Weekly",
+        ["Objective"] = "Destroy 200 planes",
+        ["Requirements"] = {["WeeklyKills"] = 200},
+        ["RewardType"] = "Medals",
+        ["Reward"] = 50
+    },
+    ["France's Air Veteran"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Get 20 kills with a French plane",
+        ["Requirements"] = {["FrancePlaneKills"] = 20},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 90
+    },
+    ["Germany's Air Expert"] = {
+        ["Type"] = "Weekly",
+        ["Objective"] = "Get 75 kills with a German plane",
+        ["Requirements"] = {["GermanyPlaneKills"] = 75},
+        ["RewardType"] = "UpgradePoints",
+        ["Reward"] = 25
+    },
+    ["Britain's Great Nightmare"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Get 5 kills with a British plane in a round",
+        ["Requirements"] = {["BritainPlaneKillsInRound"] = 5},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 105
+    },
+    ["Saviour"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Save 5 teammates from being shot down",
+        ["Requirements"] = {["SavedTeammates"] = 5},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 90
+    },
+    ["Balancer"] = {
+        ["Type"] = "Weekly",
+        ["Objective"] = "Destroy planes 1 level higher than your current plane",
+        ["Requirements"] = {["Destroy1LevelPlane"] = 15},
+        ["RewardType"] = "UpgradePoints",
+        ["Reward"] = 15
+    },
+    ["USA's Air Expert"] = {
+        ["Type"] = "Weekly",
+        ["Objective"] = "Get 75 kills with an American plane",
+        ["Requirements"] = {["USAPlaneKills"] = 75},
+        ["RewardType"] = "UpgradePoints",
+        ["Reward"] = 25
+    },
+    ["Firestarter"] = {
+        ["Type"] = "Weekly",
+        ["Objective"] = "Set afire 30 planes",
+        ["Requirements"] = {["PlanesSetAfire"] = 30},
+        ["RewardType"] = "Medals",
+        ["Reward"] = 35
+    },
+    ["Avenger"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Destroy 3 planes that shot down a friendly plane",
+        ["Requirements"] = {["AvengedPlanes"] = 3},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 90
+    },
+    ["Underrated"] = {
+        ["Type"] = "Weekly",
+        ["Objective"] = "Destroy a plane 3 levels higher than your current plane",
+        ["Requirements"] = {["Destroy3LevelPlane"] = 1},
+        ["RewardType"] = "Medals",
+        ["Reward"] = 25
+    },
+    ["Japan Air Veteran"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Get 20 kills with a Japanese plane",
+        ["Requirements"] = {["JapanPlaneKills"] = 20},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 90
+    },
+    ["Most valuable"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Get MVP",
+        ["Requirements"] = {["MVP"] = 1},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 90
+    },
+    ["USA Air Veteran"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Get 20 kills with an American plane",
+        ["Requirements"] = {["USAPlaneKills"] = 20},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 90
+    },
+    ["Germany's Great Nightmare"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Get 5 kills with a German plane in a round",
+        ["Requirements"] = {["GermanyPlaneKillsInRound"] = 5},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 105
+    },
+    ["Britain's Air Expert"] = {
+        ["Type"] = "Weekly",
+        ["Objective"] = "Get 75 kills with a British plane",
+        ["Requirements"] = {["BritainPlaneKills"] = 75},
+        ["RewardType"] = "UpgradePoints",
+        ["Reward"] = 25
+    },
+    ["Britain Air Veteran"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Get 20 kills with a British plane",
+        ["Requirements"] = {["BritainPlaneKills"] = 20},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 90
+    },
+    ["Engine sniper"] = {
+        ["Type"] = "Daily",
+        ["Objective"] = "Disable 5 plane engines",
+        ["Requirements"] = {["EnginesDestroyed"] = 5},
+        ["RewardType"] = "Coins",
+        ["Reward"] = 90
+    }
+}
