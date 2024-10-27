25a6eb6ebb5f6b99b137df658b515e2212c4425f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-27 18:21:24 +1100

    Create BattlePassDatabase.lua

---
+++ GameData/Wings of Glory [338574920]/BattlePassDatabase.lua
@@ -0,0 +1,33 @@
+data = {
+    ["Ignition: Act 1"] = {
+        ["ExpRequirements"] = {
+            100,
+            200,
+            400,
+            500,
+            700,
+            1000,
+            1200,
+            1400,
+            1700,
+            2200,
+            2900,
+            3500,
+            4000,
+            5000,
+            6000,
+            7000,
+            8000,
+            9500,
+            12500
+        },
+        ["Rewards"] = {
+            ["Paid"] = {{["Name"] = "test2", ["Type"] = "skin"}},
+            ["Free"] = {
+                {["Amount"] = 100, ["Type"] = "Coins"},
+                {["Amount"] = 5, ["Type"] = "Medals"},
+                {["Name"] = "test", ["Type"] = "skin"}
+            }
+        }
+    }
+}
