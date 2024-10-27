63fc7f47c3167e4943f73b3937b2f78db3a42896
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-27 18:22:20 +1100

    Create MapSettings.lua

---
+++ GameData/Wings of Glory [338574920]/MapSettings.lua
@@ -0,0 +1,4 @@
+data = {
+    ["Pacific"] = {["BGColor"] = Color3.fromRGB(6, 72, 127), ["Image"] = "rbxassetid://1092291214"},
+    ["Africa"] = {["BGColor"] = Color3.fromRGB(127, 110, 76), ["Image"] = "rbxassetid://1092055774"}
+}
