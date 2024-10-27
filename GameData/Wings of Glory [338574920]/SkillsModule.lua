7fb5dde6eaf1315a883a0db91d627d18df8e3903
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-27 18:23:33 +1100

    Create SkillsModule.lua

---
+++ GameData/Wings of Glory [338574920]/SkillsModule.lua
@@ -0,0 +1,15 @@
+data = {
+    {0, "Newbie"},
+    {100, "Novice"},
+    {200, "Rookie"},
+    {400, "Talented"},
+    {600, "Skilled"},
+    {800, "Intermediate"},
+    {1000, "Seasoned"},
+    {1200, "Proficient"},
+    {1500, "Experienced"},
+    {2000, "Advanced"},
+    {2500, "Senior"},
+    {3000, "Expert"},
+    {10000, "Ace"}
+}
