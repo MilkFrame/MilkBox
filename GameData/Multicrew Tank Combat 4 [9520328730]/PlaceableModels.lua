f79801d3d5b40e51f32e91a4edfb13c6eca145ad
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-27 18:31:56 +1100

    Create PlaceableModels.lua

---
+++ GameData/Multicrew Tank Combat 4 [9520328730]/PlaceableModels.lua
@@ -0,0 +1,272 @@
+data = {
+    ["Kornet"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["anglelimits"] = {["vertical"] = {-10, 15, 45}, ["horizontal"] = {-45, 45, 45}},
+                    ["rangeFinderAccuracy"] = {["speed"] = 1, ["maxAccurateRange"] = 27775, ["unit"] = 1},
+                    ["Zoom"] = {2.8, 4.8, 0.1, 0, 0}
+                }
+            }
+        }
+    },
+    ["S-60"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["Zoom"] = {20, 40, 0.1, 10, 0},
+                    ["rangeFinderAccuracy"] = {["speed"] = 0.1, ["maxAccurateRange"] = 27775, ["unit"] = 50},
+                    ["MouseAim"] = true,
+                    ["anglelimits"] = {["vertical"] = {-7, 45, 45}, ["horizontal"] = {45}},
+                    ["weapons"] = {}
+                }
+            }
+        }
+    },
+    ["37mm M3"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["Zoom"] = {21, 40, 0.1, 8, 0},
+                    ["rangeFinderAccuracy"] = {["speed"] = 1, ["maxAccurateRange"] = 5000, ["unit"] = 50},
+                    ["MouseAim"] = true,
+                    ["anglelimits"] = {["vertical"] = {-5, 15, 16}, ["horizontal"] = {-40, 40, 14.3}},
+                    ["weapons"] = {}
+                }
+            }
+        }
+    },
+    ["UB32"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["Zoom"] = {20, 40, 0.1, 0, 0},
+                    ["anglelimits"] = {["vertical"] = {-1, 45, 45}, ["horizontal"] = {-30, 30, 45}},
+                    ["weapons"] = {},
+                    ["MouseAim"] = true
+                }
+            }
+        }
+    },
+    ["TOW Tripod"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["anglelimits"] = {["vertical"] = {-10, 15, 45}, ["horizontal"] = {-45, 45, 45}},
+                    ["rangeFinderAccuracy"] = {["speed"] = 1, ["maxAccurateRange"] = 27775, ["unit"] = 1},
+                    ["Zoom"] = {9, 18, 0.1, 0, 0}
+                }
+            }
+        }
+    },
+    ["9M14M"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["anglelimits"] = {["vertical"] = {-10, 15, 0}, ["horizontal"] = {-45, 45, 0}},
+                    ["rangeFinderAccuracy"] = {["speed"] = 0.1, ["maxAccurateRange"] = 27775, ["unit"] = 50},
+                    ["Zoom"] = {22.5, 40, 0.1, 0, 0}
+                }
+            }
+        }
+    },
+    ["SPG9"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["Zoom"] = {21, 40, 0.1, 15, 0},
+                    ["anglelimits"] = {["vertical"] = {-15, 20, 45}, ["horizontal"] = {-50, 50, 45}},
+                    ["rangeFinderAccuracy"] = {["speed"] = 0.1, ["maxAccurateRange"] = 27775, ["unit"] = 50},
+                    ["MouseAim"] = true
+                }
+            }
+        }
+    },
+    ["M134"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["Zoom"] = {40, 60, 0.1, 0, 0},
+                    ["MouseAim"] = true,
+                    ["Stabiliser"] = {["StabRatio"] = {1, 1}},
+                    ["anglelimits"] = {["vertical"] = {-25, 50, 70}, ["horizontal"] = {70}},
+                    ["weapons"] = {}
+                }
+            }
+        }
+    },
+    ["Lancet Launcher"] = {
+        ["Script"] = {
+            ["Controls"] = {
+                ["Utility"] = {
+                    ["AutoPilot"] = "function: 0x33beb5b017f3d092",
+                    ["Randomize"] = "function: 0x67e4d29636e81a82",
+                    ["ToQuaternion"] = "function: 0xd7e117acdf5b8e02",
+                    ["torquelimiter"] = "function: 0xe244840980bbbcb2",
+                    ["Randomize2"] = "function: 0x5a02eabccdaaa462",
+                    ["Interpolator"] = "function: 0x7854420d6093e122",
+                    ["GetMass"] = "function: 0x085b8f275b52c102",
+                    ["Airfoil"] = "function: 0xfdd254323eed08c2",
+                    ["SqrMagnitude"] = "function: 0x0acd41d03bfa9322",
+                    ["InverseLerp"] = "function: 0xfe338c5b9717b8a2",
+                    ["FromAxisAngle"] = "function: 0x6663f204dd7d4ae2",
+                    ["Lerp"] = "function: 0x2c2aa9ee92bd6e42"
+                }
+            }
+        }
+    },
+    ["Mortar"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["anglelimits"] = {["vertical"] = {-25, 35, 45}, ["horizontal"] = {45}},
+                    ["weapons"] = {},
+                    ["Zoom"] = {20, 40, 0.1, 20, 0}
+                }
+            }
+        }
+    },
+    ["SPG18"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["Zoom"] = {21, 40, 0.1, 15, 0},
+                    ["anglelimits"] = {["vertical"] = {-15, 20, 45}, ["horizontal"] = {-50, 50, 45}},
+                    ["rangeFinderAccuracy"] = {["speed"] = 0.1, ["maxAccurateRange"] = 27775, ["unit"] = 50},
+                    ["MouseAim"] = true
+                }
+            }
+        }
+    },
+    ["AGS-17"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["Zoom"] = {40, 60, 0.1, 0, 0},
+                    ["MouseAim"] = true,
+                    ["Stabiliser"] = {["StabRatio"] = {1, 1}},
+                    ["anglelimits"] = {["vertical"] = {-10, 45, 45}, ["horizontal"] = {-45, 45, 45}},
+                    ["weapons"] = {}
+                }
+            }
+        }
+    },
+    ["M134 test"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["Zoom"] = {40, 60, 0.1, 0, 0},
+                    ["MouseAim"] = true,
+                    ["Stabiliser"] = {["StabRatio"] = {1, 1}},
+                    ["anglelimits"] = {["vertical"] = {-25, 50, 70}, ["horizontal"] = {70}},
+                    ["weapons"] = {}
+                }
+            }
+        }
+    },
+    ["82mm 2B9 Vasilek"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["Zoom"] = {20, 40, 0.1, 20, 0},
+                    ["rangeFinderAccuracy"] = {["speed"] = 1, ["maxAccurateRange"] = 5000, ["unit"] = 50},
+                    ["MouseAim"] = true,
+                    ["anglelimits"] = {["vertical"] = {-1, 85, 12}, ["horizontal"] = {-30, 30, 12}},
+                    ["weapons"] = {}
+                }
+            }
+        }
+    },
+    ["7,5 cm PaK 40"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["weapons"] = {},
+                    ["anglelimits"] = {["vertical"] = {-5, 20, 13.2}, ["horizontal"] = {-30, 30, 13.2}},
+                    ["rangeFinderAccuracy"] = {["speed"] = 0.1, ["maxAccurateRange"] = 27775, ["unit"] = 50},
+                    ["Zoom"] = {21, 40, 0.1, 10, 0}
+                }
+            }
+        }
+    },
+    ["M2"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["Zoom"] = {40, 60, 0.1, 0, 0},
+                    ["MouseAim"] = true,
+                    ["Stabiliser"] = {["StabRatio"] = {1, 1}},
+                    ["anglelimits"] = {["vertical"] = {-25, 15, 45}, ["horizontal"] = {-30, 30, 45}},
+                    ["weapons"] = {}
+                }
+            }
+        }
+    },
+    ["DSHK"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["Zoom"] = {40, 60, 0.1, 0, 0},
+                    ["MouseAim"] = true,
+                    ["Stabiliser"] = {["StabRatio"] = {1, 1}},
+                    ["anglelimits"] = {["vertical"] = {-25, 50, 70}, ["horizontal"] = {70}},
+                    ["weapons"] = {}
+                }
+            }
+        }
+    },
+    ["Sentry"] = {
+        ["PRC"] = {["core"] = {["run"] = "function: 0xc35bd1a0c3846542"}},
+        ["Configs"] = {["vertlimit"] = {-40, 40}, ["horlimit"] = {-45, 45}, ["range"] = 100}
+    },
+    ["122mm D-30"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["Zoom"] = {21, 40, 0.1, 8, 0},
+                    ["rangeFinderAccuracy"] = {["speed"] = 1, ["maxAccurateRange"] = 5000, ["unit"] = 50},
+                    ["MouseAim"] = true,
+                    ["anglelimits"] = {["vertical"] = {-5, 45, 16}, ["horizontal"] = {-80, 80, 14.3}},
+                    ["weapons"] = {}
+                }
+            }
+        }
+    },
+    ["HellCannon"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["Zoom"] = {20, 40, 0.1, 15, 0},
+                    ["anglelimits"] = {["vertical"] = {-30, 30, 45}, ["horizontal"] = {-120, 120, 45}},
+                    ["weapons"] = {},
+                    ["MouseAim"] = true
+                }
+            }
+        }
+    },
+    ["T-54 Turret"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["Zoom"] = {19, 19, 0.1, 11, 0},
+                    ["rangeFinderAccuracy"] = {["speed"] = 0.1, ["maxAccurateRange"] = 2500, ["unit"] = 50},
+                    ["MouseAim"] = true,
+                    ["anglelimits"] = {["vertical"] = {-4, 15, 5}, ["horizontal"] = {10}},
+                    ["weapons"] = {}
+                }
+            }
+        }
+    },
+    ["ZU-23-2"] = {
+        ["Turrets"] = {
+            ["Turret1"] = {
+                ["TurretInfo"] = {
+                    ["Zoom"] = {21, 40, 0.1, 0, 0},
+                    ["anglelimits"] = {["vertical"] = {-10, 89, 38}, ["horizontal"] = {-120, 120, 38}},
+                    ["weapons"] = {},
+                    ["MouseAim"] = true
+                }
+            }
+        }
+    }
+}
