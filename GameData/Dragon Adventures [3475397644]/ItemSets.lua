79bc148836ad5126dee072854d68546fd7291c25
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-29 03:13:24 +1100

    Create ItemSets.lua

---
+++ GameData/Dragon Adventures [3475397644]/ItemSets.lua
@@ -0,0 +1,359 @@
+data = {
+    ["Season17Buildings"] = {
+        ["Items"] = {"ChineseTable", "ChineseFuton"},
+        ["Image"] = "rbxthumb://type=Asset&id=12339007685&w=150&h=150",
+        ["Rarity"] = {
+            ["Index"] = 4,
+            ["Fishing"] = {
+                ["SpinSpeed"] = {["Min"] = 350, ["Max"] = 450},
+                ["AnnounceReward"] = true,
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 7, ["Max"] = 8},
+                ["DropChance"] = 5
+            },
+            ["Hatching"] = {["AnnounceReward"] = true},
+            ["TextStrokeColor"] = Color3.fromRGB(106, 19, 149),
+            ["Auction"] = {["MinAmount"] = 25000},
+            ["AccessoryGamepassEffect"] = true,
+            ["Name"] = "Epic",
+            ["BreedCost"] = {["MinAmount"] = 25000},
+            ["Bosses"] = {["Amount"] = 1, ["AnnounceReward"] = true},
+            ["SpinnerWheel"] = {["Amount"] = 10, ["DropChance"] = 4, ["AnnounceReward"] = true},
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 10},
+            ["TextColor"] = Color3.fromRGB(182, 34, 255)
+        },
+        ["Category"] = "ItemSet",
+        ["ItemType"] = "ItemSet",
+        ["Name"] = "Season17Buildings",
+        ["DisplayName"] = "BuildingSet"
+    },
+    ["SeasonPotionSet1"] = {
+        ["Items"] = {"PrimaryPotion", "SecondaryPotion", "ColorShufflePotion", "TertiaryPotion", "ColorOrderPotion"},
+        ["Image"] = "rbxthumb://type=Asset&id=8829443860&w=150&h=150",
+        ["Rarity"] = {
+            ["Index"] = 4,
+            ["Fishing"] = {
+                ["SpinSpeed"] = {["Min"] = 350, ["Max"] = 450},
+                ["AnnounceReward"] = true,
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 7, ["Max"] = 8},
+                ["DropChance"] = 5
+            },
+            ["Hatching"] = {["AnnounceReward"] = true},
+            ["TextStrokeColor"] = Color3.fromRGB(106, 19, 149),
+            ["Auction"] = {["MinAmount"] = 25000},
+            ["AccessoryGamepassEffect"] = true,
+            ["Name"] = "Epic",
+            ["BreedCost"] = {["MinAmount"] = 25000},
+            ["Bosses"] = {["Amount"] = 1, ["AnnounceReward"] = true},
+            ["SpinnerWheel"] = {["Amount"] = 10, ["DropChance"] = 4, ["AnnounceReward"] = true},
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 10},
+            ["TextColor"] = Color3.fromRGB(182, 34, 255)
+        },
+        ["Category"] = "ItemSet",
+        ["ItemType"] = "ItemSet",
+        ["Name"] = "SeasonPotionSet1",
+        ["DisplayName"] = "ColorPotionSet"
+    },
+    ["IgnicarisSet"] = {
+        ["Items"] = {
+            "IgnicarisTrident",
+            "IgnicarisHelmet",
+            "IgnicarisWingWrap(L)",
+            "IgnicarisWingWrap(R)",
+            "IgnicarisNeckWrap",
+            "IgnicarisSaddle",
+            "IgnicarisTailWrap"
+        },
+        ["Image"] = "rbxthumb://type=Asset&id=109858795523443&w=150&h=150",
+        ["Name"] = "IgnicarisSet",
+        ["Category"] = "ItemSet",
+        ["ItemType"] = "ItemSet",
+        ["Rarity"] = {
+            ["Index"] = 5,
+            ["Fishing"] = {
+                ["SpinSpeed"] = {["Min"] = 400, ["Max"] = 500},
+                ["AnnounceReward"] = true,
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 8, ["Max"] = 9},
+                ["DropChance"] = 1
+            },
+            ["Hatching"] = {["AnnounceReward"] = true},
+            ["TextStrokeColor"] = Color3.fromRGB(156, 93, 6),
+            ["Auction"] = {["MinAmount"] = 75000},
+            ["AccessoryGamepassEffect"] = true,
+            ["Name"] = "Legendary",
+            ["BreedCost"] = {["MinAmount"] = 75000},
+            ["Bosses"] = {["Amount"] = 1, ["AnnounceReward"] = true},
+            ["SpinnerWheel"] = {["Amount"] = 1, ["DropChance"] = 1, ["AnnounceReward"] = true},
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 1},
+            ["TextColor"] = Color3.fromRGB(255, 153, 10)
+        }
+    },
+    ["DruidSet"] = {
+        ["Items"] = {"DruidicHeadPlate", "DruidicCollar", "DruidicWing(L)", "DruidicWing(R)", "DruidicTailRing"},
+        ["Image"] = "rbxthumb://type=Asset&id=14199759062&w=150&h=150",
+        ["Name"] = "DruidSet",
+        ["Category"] = "ItemSet",
+        ["ItemType"] = "ItemSet",
+        ["Rarity"] = {
+            ["Index"] = 5,
+            ["Fishing"] = {
+                ["SpinSpeed"] = {["Min"] = 400, ["Max"] = 500},
+                ["AnnounceReward"] = true,
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 8, ["Max"] = 9},
+                ["DropChance"] = 1
+            },
+            ["Hatching"] = {["AnnounceReward"] = true},
+            ["TextStrokeColor"] = Color3.fromRGB(156, 93, 6),
+            ["Auction"] = {["MinAmount"] = 75000},
+            ["AccessoryGamepassEffect"] = true,
+            ["Name"] = "Legendary",
+            ["BreedCost"] = {["MinAmount"] = 75000},
+            ["Bosses"] = {["Amount"] = 1, ["AnnounceReward"] = true},
+            ["SpinnerWheel"] = {["Amount"] = 1, ["DropChance"] = 1, ["AnnounceReward"] = true},
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 1},
+            ["TextColor"] = Color3.fromRGB(255, 153, 10)
+        }
+    },
+    ["Season8Buildings"] = {
+        ["Items"] = {"GoldPileSmall", "GoldPileLarge", "LavaPool"},
+        ["Image"] = "rbxthumb://type=Asset&id=9474840178&w=150&h=150",
+        ["Rarity"] = {
+            ["Index"] = 3,
+            ["Drops"] = {["DropChance"] = 1, ["Amount"] = 1},
+            ["Fishing"] = {
+                ["SpinSpeed"] = {["Min"] = 300, ["Max"] = 400},
+                ["AnnounceReward"] = false,
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 6, ["Max"] = 7},
+                ["DropChance"] = 10
+            },
+            ["TextStrokeColor"] = Color3.fromRGB(17, 130, 175),
+            ["Alchemy"] = {["DropChance"] = 3, ["ElixirRecyleAmount"] = 25},
+            ["Auction"] = {["MinAmount"] = 10000},
+            ["Name"] = "Rare",
+            ["BreedCost"] = {["MinAmount"] = 1500},
+            ["Bosses"] = {["Amount"] = 100},
+            ["SpinnerWheel"] = {["DropChance"] = 10, ["Amount"] = 25},
+            ["RecyleAccessory"] = true,
+            ["TextColor"] = Color3.fromRGB(26, 183, 255)
+        },
+        ["Category"] = "ItemSet",
+        ["ItemType"] = "ItemSet",
+        ["Name"] = "Season8Buildings",
+        ["DisplayName"] = "BuildingSet"
+    },
+    ["BylendarachSet"] = {
+        ["Items"] = {
+            "BylendarachHornedHelm",
+            "BylendarachNeckCarapace",
+            "BylendarachTailBlade",
+            "BylendarachWraithMask",
+            "BylendarachWraithSaddle",
+            "BylendarachWraithWing(L)",
+            "BylendarachWraithWing(R)",
+            "BladeOfTheProtector"
+        },
+        ["Image"] = "rbxthumb://type=Asset&id=111932418944673&w=150&h=150",
+        ["Name"] = "BylendarachSet",
+        ["Category"] = "ItemSet",
+        ["ItemType"] = "ItemSet",
+        ["Rarity"] = {
+            ["Index"] = 5,
+            ["Fishing"] = {
+                ["SpinSpeed"] = {["Min"] = 400, ["Max"] = 500},
+                ["AnnounceReward"] = true,
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 8, ["Max"] = 9},
+                ["DropChance"] = 1
+            },
+            ["Hatching"] = {["AnnounceReward"] = true},
+            ["TextStrokeColor"] = Color3.fromRGB(156, 93, 6),
+            ["Auction"] = {["MinAmount"] = 75000},
+            ["AccessoryGamepassEffect"] = true,
+            ["Name"] = "Legendary",
+            ["BreedCost"] = {["MinAmount"] = 75000},
+            ["Bosses"] = {["Amount"] = 1, ["AnnounceReward"] = true},
+            ["SpinnerWheel"] = {["Amount"] = 1, ["DropChance"] = 1, ["AnnounceReward"] = true},
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 1},
+            ["TextColor"] = Color3.fromRGB(255, 153, 10)
+        }
+    },
+    ["RoyaltySet"] = {
+        ["Items"] = {
+            "RoyaltyHeadPlate",
+            "RoyaltyCollar",
+            "RoyaltyWing(R)",
+            "RoyaltyWing(L)",
+            "RoyaltyGoldenSaddle",
+            "RoyaltyTailBanners"
+        },
+        ["Image"] = "rbxthumb://type=Asset&id=14351720094&w=150&h=150",
+        ["Name"] = "RoyaltySet",
+        ["Category"] = "ItemSet",
+        ["ItemType"] = "ItemSet",
+        ["Rarity"] = {
+            ["Index"] = 5,
+            ["Fishing"] = {
+                ["SpinSpeed"] = {["Min"] = 400, ["Max"] = 500},
+                ["AnnounceReward"] = true,
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 8, ["Max"] = 9},
+                ["DropChance"] = 1
+            },
+            ["Hatching"] = {["AnnounceReward"] = true},
+            ["TextStrokeColor"] = Color3.fromRGB(156, 93, 6),
+            ["Auction"] = {["MinAmount"] = 75000},
+            ["AccessoryGamepassEffect"] = true,
+            ["Name"] = "Legendary",
+            ["BreedCost"] = {["MinAmount"] = 75000},
+            ["Bosses"] = {["Amount"] = 1, ["AnnounceReward"] = true},
+            ["SpinnerWheel"] = {["Amount"] = 1, ["DropChance"] = 1, ["AnnounceReward"] = true},
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 1},
+            ["TextColor"] = Color3.fromRGB(255, 153, 10)
+        }
+    },
+    ["WarriorSet"] = {
+        ["Items"] = {
+            "WarriorHeadPlate",
+            "WarriorNeckPlate",
+            "WarriorBodyPlate",
+            "WarriorWingPlate(L)",
+            "WarriorWingPlate(R)",
+            "WarriorTailPlate"
+        },
+        ["Image"] = "rbxthumb://type=Asset&id=12400984853&w=150&h=150",
+        ["Name"] = "WarriorSet",
+        ["Category"] = "ItemSet",
+        ["ItemType"] = "ItemSet",
+        ["Rarity"] = {
+            ["Index"] = 5,
+            ["Fishing"] = {
+                ["SpinSpeed"] = {["Min"] = 400, ["Max"] = 500},
+                ["AnnounceReward"] = true,
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 8, ["Max"] = 9},
+                ["DropChance"] = 1
+            },
+            ["Hatching"] = {["AnnounceReward"] = true},
+            ["TextStrokeColor"] = Color3.fromRGB(156, 93, 6),
+            ["Auction"] = {["MinAmount"] = 75000},
+            ["AccessoryGamepassEffect"] = true,
+            ["Name"] = "Legendary",
+            ["BreedCost"] = {["MinAmount"] = 75000},
+            ["Bosses"] = {["Amount"] = 1, ["AnnounceReward"] = true},
+            ["SpinnerWheel"] = {["Amount"] = 1, ["DropChance"] = 1, ["AnnounceReward"] = true},
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 1},
+            ["TextColor"] = Color3.fromRGB(255, 153, 10)
+        }
+    },
+    ["SeasonPotionSet2"] = {
+        ["Items"] = {
+            "PrimaryMaterialPotion",
+            "SecondaryMaterialPotion",
+            "TertiaryMaterialPotion",
+            "MaterialShufflePotion",
+            "MaterialOrderPotion"
+        },
+        ["Image"] = "rbxthumb://type=Asset&id=8829446006&w=150&h=150",
+        ["Rarity"] = {
+            ["Index"] = 4,
+            ["Fishing"] = {
+                ["SpinSpeed"] = {["Min"] = 350, ["Max"] = 450},
+                ["AnnounceReward"] = true,
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 7, ["Max"] = 8},
+                ["DropChance"] = 5
+            },
+            ["Hatching"] = {["AnnounceReward"] = true},
+            ["TextStrokeColor"] = Color3.fromRGB(106, 19, 149),
+            ["Auction"] = {["MinAmount"] = 25000},
+            ["AccessoryGamepassEffect"] = true,
+            ["Name"] = "Epic",
+            ["BreedCost"] = {["MinAmount"] = 25000},
+            ["Bosses"] = {["Amount"] = 1, ["AnnounceReward"] = true},
+            ["SpinnerWheel"] = {["Amount"] = 10, ["DropChance"] = 4, ["AnnounceReward"] = true},
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 10},
+            ["TextColor"] = Color3.fromRGB(182, 34, 255)
+        },
+        ["Category"] = "ItemSet",
+        ["ItemType"] = "ItemSet",
+        ["Name"] = "SeasonPotionSet2",
+        ["DisplayName"] = "MaterialPotionSet"
+    },
+    ["LumenighSet"] = {
+        ["Items"] = {
+            "EurekaLightbulb",
+            "LightbulbLantern",
+            "LumenighNecklace",
+            "LumenighSaddle",
+            "LumenighScarf",
+            "LumenighTailLights",
+            "LumenighWingLights(L)",
+            "LumenighWingLights(R)"
+        },
+        ["Image"] = "rbxthumb://type=Asset&id=98010773214336&w=150&h=150",
+        ["Name"] = "LumenighSet",
+        ["Category"] = "ItemSet",
+        ["ItemType"] = "ItemSet",
+        ["Rarity"] = {
+            ["Index"] = 5,
+            ["Fishing"] = {
+                ["SpinSpeed"] = {["Min"] = 400, ["Max"] = 500},
+                ["AnnounceReward"] = true,
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 8, ["Max"] = 9},
+                ["DropChance"] = 1
+            },
+            ["Hatching"] = {["AnnounceReward"] = true},
+            ["TextStrokeColor"] = Color3.fromRGB(156, 93, 6),
+            ["Auction"] = {["MinAmount"] = 75000},
+            ["AccessoryGamepassEffect"] = true,
+            ["Name"] = "Legendary",
+            ["BreedCost"] = {["MinAmount"] = 75000},
+            ["Bosses"] = {["Amount"] = 1, ["AnnounceReward"] = true},
+            ["SpinnerWheel"] = {["Amount"] = 1, ["DropChance"] = 1, ["AnnounceReward"] = true},
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 1},
+            ["TextColor"] = Color3.fromRGB(255, 153, 10)
+        }
+    },
+    ["StellarisSet"] = {
+        ["Items"] = {
+            "StellarisSaddle",
+            "StellarisBustle",
+            "StellarisChestpiece",
+            "StellarisFan",
+            "StellarisHelmet",
+            "StellarisNecklace",
+            "StellarisTailRings",
+            "StellarisWingPlate(L)",
+            "StellarisWingPlate(R)"
+        },
+        ["Image"] = "rbxthumb://type=Asset&id=18521767029&w=150&h=150",
+        ["Name"] = "StellarisSet",
+        ["Category"] = "ItemSet",
+        ["ItemType"] = "ItemSet",
+        ["Rarity"] = {
+            ["Index"] = 5,
+            ["Fishing"] = {
+                ["SpinSpeed"] = {["Min"] = 400, ["Max"] = 500},
+                ["AnnounceReward"] = true,
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 8, ["Max"] = 9},
+                ["DropChance"] = 1
+            },
+            ["Hatching"] = {["AnnounceReward"] = true},
+            ["TextStrokeColor"] = Color3.fromRGB(156, 93, 6),
+            ["Auction"] = {["MinAmount"] = 75000},
+            ["AccessoryGamepassEffect"] = true,
+            ["Name"] = "Legendary",
+            ["BreedCost"] = {["MinAmount"] = 75000},
+            ["Bosses"] = {["Amount"] = 1, ["AnnounceReward"] = true},
+            ["SpinnerWheel"] = {["Amount"] = 1, ["DropChance"] = 1, ["AnnounceReward"] = true},
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 1},
+            ["TextColor"] = Color3.fromRGB(255, 153, 10)
+        }
+    }
+}
