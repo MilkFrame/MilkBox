b458f5d2f7e6e27993ebea10b46e511636e8e253
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-29 03:23:29 +1100

    Create BreedingPresets.lua

---
+++ GameData/Dragon Adventures [3475397644]/Dragons/BreedingPresets.lua
@@ -0,0 +1,472 @@
+data = {
+    ["TheHunt"] = {["TheHuntPresetColors"] = {"7F6195", "FB5E79", "4C2A4F", "6B3C34", "C3996F", "8c272d"}},
+    ["Seasons"] = {
+        ["PagulauPresetColors"] = {
+            {"FC6D68", "FFEFDC", "B13A3A"},
+            {"A7B08C", "516760", "1F281D"},
+            {"2B0D88", "5317B5", "0E1011"},
+            {"332D25", "CABBA2", "6D675B"},
+            {"0086CE", "E0FFFF", "153F4B"},
+            {"FEA1B3", "F7DEBF", "FFD297"}
+        },
+        ["WoodlumaPresetColors"] = {
+            {"9C8E8D", "F7F9F9", "D0CFD7"},
+            {"5A4534", "697135", "425035"},
+            {"3B3736", "1A1A1B", "636268"},
+            {"827A64", "FFB7B4", "FFE5E5"},
+            {"57372C", "B2560D", "CB732D"}
+        },
+        ["TempiritusPresetColors"] = {
+            {"5C415D", "A794B2", "FF839B"},
+            {"8C272D", "B13A3A", "FC6D68"},
+            {"1A1A1B", "413C40", "C1272D"},
+            {"D0CFD7", "FFFFFF", "CE000D"}
+        }
+    },
+    ["Winter"] = {
+        ["FirifellerPresetColors"] = {
+            {"20603F", "F9E255", "DF3236"},
+            {"FFFFFF", "B3E1F1", "76A8FF"},
+            {"FFE5E5", "FF839B", "FB5E79"},
+            {"302722", "FA912B", "FF5500"},
+            {"774840", "8A6059", "603E3D"}
+        },
+        ["WinterMaterials"] = {"Ice"},
+        ["WinterElements"] = {["Frostbite"] = {["P"] = 1}, ["Aurora Borealis"] = {["P"] = 1}, ["Ice"] = {["P"] = 1}},
+        ["WinterColors"] = {
+            "FFFFFF",
+            "EAEDEF",
+            "525195",
+            "757ADB",
+            "484AA1",
+            "2D237A",
+            "0D0A5B",
+            "212B5F",
+            "1C51E7",
+            "324BA9",
+            "4866D5",
+            "2F83FF",
+            "6394DD",
+            "76A8FF",
+            "AEC8FF",
+            "7895C1",
+            "444F69",
+            "556979",
+            "2F4557",
+            "0D1E25",
+            "0B2D46",
+            "0A3D67",
+            "0086CE",
+            "2B768F",
+            "00B4D5",
+            "00FFF1",
+            "9AEAEF",
+            "72C4C4",
+            "3CA2A4"
+        }
+    },
+    ["ShadowAltar"] = {
+        ["KostragulaPresetColors"] = {
+            {"1E2716", "C6FF00", "9FFF00"},
+            {"B2560D", "FFFDEA", "F2AD0C"},
+            {"451717", "2D0102", "AA0024"},
+            {"551199", "1F1A23", "DA4FFF"}
+        },
+        ["OrtaroukPresetColors"] = {
+            {"4D0F28", "FF6841", "AA0024"},
+            {"1A1A1B", "005D48", "C6FF00"},
+            {"EAEDEF", "7895C1", "91FFF7"},
+            {"22263D", "DA4FFF", "E934AA"}
+        },
+        ["NoctoriusPresetColors"] = {
+            {["Colors"] = {"233253", "9C50D3", "4D2C89"}, ["P"] = 1},
+            {["Colors"] = {"2D0102", "AA0024", "DF3236"}, ["P"] = 1},
+            {["Colors"] = {"094869", "00B4D5", "B3E1F1"}, ["P"] = 1},
+            {["Colors"] = {"FFEFDC", "1A1A1B", "888F8D"}, ["P"] = 1}
+        }
+    },
+    ["Halloween"] = {
+        ["HalloweenPresetColors"] = {
+            {"4B4420", "C18E1B", "B2560D"},
+            {"3F2B66", "FF984F", "9778BE"},
+            {"652127", "EF5C23", "2D0102"},
+            {"B2560D", "2F1B1B", "FFB53C"}
+        },
+        ["DjaevelhestPresetColors"] = {
+            {"1F1A23", "03FF7D", "20603F"},
+            {"1F1A23", "9C50D3", "4D2C89"},
+            {"3B3736", "EAEDEF", "888F8D"},
+            {"302722", "FF5500", "8B3220"},
+            {"0A3D67", "91FFF7", "0086CE"}
+        },
+        ["ParanoxElements"] = {
+            ["Grass"] = {["P"] = 28},
+            ["Fire"] = {["P"] = 28},
+            ["Vampire"] = {["P"] = 15},
+            ["Water"] = {["P"] = 28}
+        },
+        ["ParanoxPresetColors"] = {
+            {"1F1A23", "5317B5", "FF8500"},
+            {"5C415D", "735B77", "A794B2"},
+            {"1F281D", "1F483A", "AA0024"},
+            {"EAEDEF", "F7F9F9", "AEC8FF"},
+            {"0E1011", "F7F9F9", "636268"},
+            {"413C40", "545365", "03FF7D"}
+        },
+        ["GoliatombPresetColors"] = {{"0E1011", "4D484F", "26262C"}},
+        ["AraneaixPresetColors"] = {
+            {"EAEDEF", "AFAFAF", "D0CFD7"},
+            {"7B3C1D", "B24407", "302722"},
+            {"564D48", "3C3030", "977B6C"},
+            {"6D675B", "827A64", "564D48"},
+            {"1A1A1B", "302722", "0E1011"},
+            {"302722", "6B3C34", "8B3220"}
+        }
+    },
+    ["Solstice"] = {
+        ["SolarizonMaterials"] = {"Glass"},
+        ["SolsticeColors"] = {
+            "FFD297",
+            "F6BF6C",
+            "F2AD0C",
+            "FFB53C",
+            "FA912B",
+            "FF8500",
+            "BA311C",
+            "E22D18",
+            "CE000D",
+            "AA0024",
+            "850012",
+            "7A0E1E",
+            "581014",
+            "2D0102",
+            "C1272D",
+            "FEA1B3",
+            "B13A3A",
+            "EB799A",
+            "FB5E79",
+            "F7FF6F",
+            "FFE63B",
+            "EF5C23",
+            "CC6F6F",
+            "652127",
+            "FDD68B"
+        },
+        ["CasiriusPresetColors"] = {
+            {["Colors"] = {"22263D", "6A7185", "1A1A1B"}, ["P"] = 1},
+            {["Colors"] = {"EAEDEF", "FFFFFF", "D0CFD7"}, ["P"] = 1},
+            {["Colors"] = {"B3E1F1", "F7F9F9", "7895C1"}, ["P"] = 1},
+            {["Colors"] = {"263746", "AEC8FF", "0E1011"}, ["P"] = 0.1}
+        },
+        ["CosmaliskPresetColors"] = {
+            {["Colors"] = {"233253", "1F565D", "9AFFC7"}, ["P"] = 1},
+            {["Colors"] = {"FFFFFF", "2F83FF", "023AE2"}, ["P"] = 1},
+            {["Colors"] = {"2D0102", "CE000D", "850012"}, ["P"] = 1},
+            {["Colors"] = {"0D0A5B", "DA4FFF", "E1CDFE"}, ["P"] = 0.1}
+        },
+        ["SolsticeElements"] = {
+            ["Sun"] = {["P"] = 5},
+            ["Moon"] = {["P"] = 5},
+            ["Lunaris"] = {["P"] = 3},
+            ["Equinox"] = {["P"] = 5},
+            ["Light"] = {["P"] = 5},
+            ["Angel"] = {["P"] = 5}
+        },
+        ["SolarizonPresetColors"] = {
+            {"F2AD0C", "FFEC80", "00FFF1"},
+            {"2F1B1B", "8a6059", "236825"},
+            {"ffffff", "AFAFAF", "CE000D"},
+            {"4C4C4C", "D0CFD7", "DA4FFF"},
+            {"22263D", "556979", "FFE63B"}
+        },
+        ["SolsticePresetColors"] = {
+            {"C67A80", "553348", "FFD297"},
+            {"FDE9AC", "F0B392", "FFFDEA"},
+            {"CCA4E0", "525195", "FEA0A0"},
+            {"7A4D4D", "FF7360", "0D1E25"},
+            {"9AEAEF", "00B4D5", "FFEC80"},
+            {"89A4C0", "F7F9F9", "FF839B"}
+        }
+    },
+    ["GuildWars"] = {
+        ["GuilmorosPresetColors"] = {
+            {"444F69", "7895C1", "0086CE"},
+            {"850012", "FFB576", "DF3236"},
+            {"023489", "76A8FF", "FFD297"},
+            {"233253", "1F565D", "148E67"}
+        }
+    },
+    ["NewYear"] = {
+        ["NewYearPresetColors"] = {
+            {"DF3236", "F6BF6C", "652127"},
+            {"FFB576", "FDE9AC", "3CA2A4"},
+            {"236825", "D1E572", "B13A3A"},
+            {"233253", "B13A3A", "F7F9F9"},
+            {"1A1A1B", "F6BF6C", "FDE9AC"},
+            {"72C4C4", "B3FFD8", "FF984F"}
+        }
+    },
+    ["Easter"] = {
+        ["MielebeePresetColors"] = {
+            {"FFB53C", "57372C", "FFFDEA"},
+            {"B27749", "3C3030", "FFE5E5"},
+            {"766259", "413C40", "9C8E8D"},
+            {"FEA1B3", "7958B1", "F7F9F9"},
+            {"FFFFFF", "564D48", "3B3736"}
+        },
+        ["EasterPresetColors"] = {
+            {"00B4D5", "ffffff", "F2AD0C"},
+            {"005D48", "61AB89", "9AFFC7"},
+            {"0E1011", "F9E255", "00FFF1"},
+            {"564D48", "766259", "977B6C"},
+            {"FF984F", "FF7360", "9AEAEF"},
+            {"413C40", "AFAFAF", "EAEDEF"},
+            {"636268", "ffffff", "9AFFC7"},
+            {"DF3236", "FBEDFA", "581014"},
+            {"563012", "00B4D5", "9AEAEF"},
+            {"E22D18", "F9E255", "1C51E7"},
+            {"4C4C4C", "F2AD0C", "0E1011"},
+            {"FFD6F6", "6394DD", "525195"},
+            {"aa004c", "8A024A", "4D0F28"},
+            {"3CA2A4", "766259", "0E1011"},
+            {"FFB576", "FFEFDC", "0A3D67"},
+            {"7930B5", "CCA4E0", "FFEC80"},
+            {"236825", "C47149", "E8FCB4"},
+            {"DF3236", "FEA0A0", "1F483A"},
+            {"FFFFFF", "FFEFDC", "FFB576"},
+            {"0E1011", "8C272D", "FEA0A0"}
+        },
+        ["GaulyraPresetColors"] = {
+            {"2D0102", "CE000D", "652127"},
+            {"FDF1E1", "F0B392", "8A6059"},
+            {"9499F7", "AEC8FF", "757ADB"},
+            {"8E5B3F", "F7DEBF", "A9A032"},
+            {"FF984F", "FFD297", "C15A39"},
+            {"FFEC80", "FFFDEA", "FFE63B"}
+        }
+    },
+    ["World"] = {
+        ["KrekizPresetColors"] = {
+            {"0086CE", "EAEDEF", "636268"},
+            {"6a7185", "EAEDEF", "545365"},
+            {"FFEC80", "FDE9AC", "697135"},
+            {"F2AD0C", "1C51E7", "1F1A23"},
+            {"D0CFD7", "603E3D", "1a1a1b"}
+        },
+        ["DummyPresetColors"] = {
+            {"564D48", "0D1E25", "FFD297"},
+            {"CABBA2", "3B3736", "766259"},
+            {"8e5b3f", "57372c", "828335"},
+            {"c3996f", "ffd297", "1a1a1b"},
+            {"697135", "ffa147", "302722"}
+        },
+        ["RhyndacPresetColors"] = {
+            {"91FFF7", "3CA2A4", "977B6C"},
+            {"51684C", "D0CFD7", "1a1a1b"},
+            {"FFB7B4", "324BA9", "023489"},
+            {"023489", "FF5500", "8b3220"},
+            {"E29C7A", "8ECE56", "20603f"}
+        },
+        ["PalusPresetColors"] = {
+            {"51684C", "F7DEBF", "1F281D"},
+            {"b13a3a", "fc6d68", "4d0f28"},
+            {"1f281d", "20603f", "66903c"},
+            {"BEA55D", "FFB53C", "301e1a"},
+            {"c3c8cd", "8E7F9E", "FDF1E1"}
+        },
+        ["RadidonPresetColors"] = {
+            {"636268", "0E1011", "9FFF00"},
+            {"3F2B66", "1F1A23", "FF5500"},
+            {"22263D", "03FF7D", "CDFE6C"},
+            {"148E67", "301E1A", "B13A3A"},
+            {"4d0f28", "FDF1E1", "855B33"}
+        },
+        ["ChronocusPresetColors"] = {
+            {"636268", "5A4534", "FDE9AC"},
+            {"7B3C1D", "ffffff", "301E1A"},
+            {"7E7645", "1f281d", "4C4C4C"},
+            {"C3996F", "B2E2BD", "B27749"},
+            {"B3E1F1", "636268", "636268"}
+        },
+        ["NeroxidePresetColors"] = {
+            {"0D1E25", "9FFF00", "E7008B"},
+            {"233253", "00FFF1", "2F83FF"},
+            {"1E361A", "236825", "C6FF00"},
+            {"4d0f28", "D1B045", "D1E572"},
+            {"153f4b", "D0CFD7", "61AB89"}
+        },
+        ["VenidPresetColors"] = {
+            {"636268", "9BFF9D", "564d48"},
+            {"023489", "91FFF7", "2F83FF"},
+            {"26262c", "FFE63B", "fa912b"},
+            {"FDF1E1", "B2E2BD", "5A6050"},
+            {"FF984F", "C47149", "3F2B66"}
+        },
+        ["SylvaPresetColors"] = {
+            {"977B6C", "F7F9F9", "0E1011"},
+            {"ef5c23", "FFFDEA", "1a1a1b"},
+            {"2F83FF", "023489", "22263D"},
+            {"22263D", "FBEDFA", "3CA2A4"},
+            {"444F69", "F7DEBF", "FDF1E1"}
+        },
+        ["VenuPresetColors"] = {
+            {"9A534D", "636268", "0E1011"},
+            {"451717", "E22D18", "FF8500"},
+            {"0A3D67", "0086CE", "22263D"},
+            {"332D25", "8DBCB4", "0d1e25"},
+            {"2F1B1B", "ffffff", "ffa147"}
+        },
+        ["ZinthrosPresetColors"] = {
+            {"97AF8B", "0D1E25", "C18E1B"},
+            {"471A43", "C3996F", "735b77"},
+            {"FDF1E1", "1F483A", "61AB89"},
+            {"7A0E1E", "ffe2e6", "977B6C"},
+            {"855B33", "4b4420", "EAEDEF"}
+        },
+        ["TigriliaPresetColors"] = {
+            {"B2560D", "FF984F", "F7DEBF"},
+            {"FFFDEA", "FCC4AD", "E29C7A"},
+            {"22263D", "444F69", "545365"},
+            {"FFE63B", "fa912b", "581014"},
+            {"977B6C", "fdd68b", "553348"}
+        },
+        ["DexynPresetColors"] = {
+            {"9A534D", "0D1E25", "636268"},
+            {"629C3F", "567C34", "D1E572"},
+            {"3b3736", "FFEFDC", "C18E1B"},
+            {"cb732d", "fdd68b", "302722"},
+            {"0E1011", "d5602b", "4b4420"}
+        },
+        ["TaihoaPresetColors"] = {
+            {"EB799A", "9AEAEF", "863290"},
+            {"757adb", "FDE9AC", "484AA1"},
+            {"ffb53c", "F7FF6F", "fa912b"},
+            {"F7DEBF", "D1B045", "525195"},
+            {"FDF1E1", "CC6F6F", "FB5E79"}
+        },
+        ["NuminePresetColors"] = {
+            {"F7F9F9", "636268", "E22D18"},
+            {"1a1a1b", "fdd68b", "a12928"},
+            {"b24407", "fa912b", "ef5c23"},
+            {"2d0102", "451717", "652127"},
+            {"301e1a", "7b3c1d", "a44b28"}
+        },
+        ["SauriumPresetColors"] = {
+            {"B3E1F1", "F7F9F9", "636268"},
+            {"8b3220", "FFB576", "4d0f28"},
+            {"774840", "687F67", "9AFFC7"},
+            {"a1abb3", "EAEDEF", "d5602b"},
+            {"697135", "FFE63B", "1a1a1b"}
+        },
+        ["ZeiperaPresetColors"] = {
+            {"DB518D", "F7F9F9", "0D1E25"},
+            {"FDE9AC", "FFFDEA", "FFD297"},
+            {"B2E2BD", "00B4D5", "FFEC80"},
+            {"564d48", "0E1011", "FCC4AD"},
+            {"FDE9AC", "1F565D", "7ECE73"}
+        },
+        ["AgricosPresetColors"] = {
+            {"FDE9AC", "636268", "F2AD0C"},
+            {"850012", "BA311C", "FF5500"},
+            {"FDF1E1", "FF984F", "5A4534"},
+            {"A794B2", "22263D", "F7F9F9"},
+            {"F9E255", "B4CD3D", "61AB89"}
+        },
+        ["KheperaPresetColors"] = {
+            {"91532A", "F0B392", "5A4534"},
+            {"3C3030", "BEA55D", "8A6059"},
+            {"AA0024", "4d0f28", "F2AD0C"}
+        },
+        ["MotherDragonPresetColors"] = {
+            {"7F6195", "FB5E79", "4C2A4F"},
+            {"581014", "8C272D", "1a1a1b"},
+            {"FEA1B3", "FB5E79", "AA004C"},
+            {"FDF1E1", "F7FF6F", "FFB53C"},
+            {"c3996f", "FFFDEA", "6B3C34"}
+        },
+        ["FerorexPresetColors"] = {
+            {"C47149", "F0B392", "4D484F"},
+            {"0e1011", "26262c", "FF984F"},
+            {"302722", "564d48", "0e1011"},
+            {"c15a39", "fdd68b", "8b3220"},
+            {"828335", "f0b392", "4b4420"}
+        },
+        ["SoukeyiPresetColors"] = {
+            {"0D1E25", "545365", "6394DD"},
+            {"301E1A", "774840", "ef5c23"},
+            {"FDF1E1", "A7B08C", "FFD297"},
+            {"8ECE56", "495547", "1a1a1b"},
+            {"977B6C", "fdd68b", "26262c"}
+        },
+        ["AmphyllPresetColors"] = {
+            {"0A3D67", "FF7360", "91FFF7"},
+            {"ffe2e6", "fea0a0", "ffffff"},
+            {"735b77", "5c415d", "ffb53c"},
+            {"20603F", "567C34", "8ECE56"},
+            {"8E5B3F", "FFE63B", "3C3030"}
+        },
+        ["CoralinaPresetColors"] = {
+            {"E77FBF", "FBEDFA", "FB5E79"},
+            {"9BFF9D", "E2FFE6", "18d3a9"},
+            {"EAEDEF", "c3c8cd", "ffffff"},
+            {"26262C", "FF984F", "CCA4E0"},
+            {"FFB53C", "FFE5E5", "B13A3A"}
+        },
+        ["EnkylousPresetColors"] = {
+            {"094869", "FFB7B4", "72C4C4"},
+            {"1E361A", "c15a39", "1E2716"},
+            {"148E67", "FDE9AC", "114D41"},
+            {"0086CE", "FBEDFA", "094869"},
+            {"EAEDEF", "00B4D5", "2B768F"}
+        },
+        ["HowlerPresetColors"] = {
+            {"977B6C", "F7F9F9", "57372C"},
+            {"D0CFD7", "ffffff", "6a7185"},
+            {"57372C", "2F1B1B", "22110A"},
+            {"AFAFAF", "FFFDEA", "888F8D"},
+            {"302722", "766259", "F7DEBF"}
+        },
+        ["GlacegarPresetColors"] = {
+            {"9CDDD3", "CABBA2", "516760"},
+            {"91fff7", "ffffff", "c3c8cd"},
+            {"3a8684", "263746", "22263D"},
+            {"6394DD", "a1abb3", "6a7185"},
+            {"f7f9f9", "bfa18f", "977B6C"}
+        },
+        ["MagmipPresetColors"] = {
+            {"636268", "0D1E25", "FF8500"},
+            {"3b3736", "cabba2", "9A7B4F"},
+            {"E29C7A", "22263D", "553348"},
+            {"F7F9F9", "212b5f", "8E7F9E"},
+            {"51684C", "EDE8B0", "C18E1B"}
+        },
+        ["CocoviraPresetColors"] = {
+            {"6B3C34", "E29C7A", "C47149"},
+            {"8e5b3f", "cabba2", "F7F9F9"},
+            {"F0B392", "fa912b", "c15a39"},
+            {"FDE9AC", "FFB576", "D1E572"},
+            {"cb732d", "F7F9F9", "1F281D"}
+        },
+        ["TarakaPresetColors"] = {
+            {"636268", "0D1E25", "6394DD"},
+            {"212b5f", "757adb", "0d1e25"},
+            {"324BA9", "00B4D5", "FFE63B"},
+            {"0e1011", "2b0d88", "023ae2"},
+            {"233253", "153f4b", "148E67"}
+        },
+        ["GeliklenPresetColors"] = {
+            {"005D48", "B13A3A", "302722"},
+            {"413C40", "888F8D", "FDE9AC"},
+            {"72C4C4", "FBEDFA", "FFB576"},
+            {"cb732d", "FBEDFA", "564d48"},
+            {"fc6d68", "ffe2e6", "AA0024"}
+        },
+        ["AlrenothPresetColors"] = {
+            {"687F67", "636268", "1E2716"},
+            {"AA96A6", "D0CFD7", "7F6195"},
+            {"556979", "FDF1E1", "3C3030"},
+            {"6394DD", "FDF1E1", "9AEAEF"},
+            {"2F4557", "3a8684", "18d3a9"}
+        }
+    }
+}
