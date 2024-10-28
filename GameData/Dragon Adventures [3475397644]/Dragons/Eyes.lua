a2603576c9fdfcf18a4996cc4566856af463137a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-29 03:22:10 +1100

    Create Eyes.lua

---
+++ GameData/Dragon Adventures [3475397644]/Dragons/Eyes.lua
@@ -0,0 +1,10 @@
+data = {
+    ["Legendary"] = {["NoTexture"] = true, ["Texture"] = ""},
+    ["Square"] = {["P"] = 1},
+    ["Confused"] = {["P"] = 1},
+    ["Slit"] = {["P"] = 1},
+    ["Star"] = {["P"] = 1},
+    ["Empty"] = {["P"] = 1},
+    ["Sparkle"] = {["P"] = 1},
+    ["Round"] = {["P"] = 1}
+}
