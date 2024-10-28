adfe29373d0d91f3aad5a2ab752bb3b469614e73
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-29 03:20:39 +1100

    Create Materials.lua

---
+++ GameData/Dragon Adventures [3475397644]/Dragons/Materials.lua
@@ -0,0 +1,264 @@
+data = {
+    ["Wood"] = {["Material"] = "Wood", ["MaterialVariant"] = "", ["Texture"] = "rbxassetid://14177725300"},
+    ["ForceField"] = {
+        ["LightInfluence"] = 0,
+        ["Brightness"] = 0.25,
+        ["LightEmission"] = 0.5,
+        ["Material"] = "ForceField",
+        ["MaterialVariant"] = "",
+        ["Texture"] = "rbxassetid://13336329695"
+    },
+    ["Clovers"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonClovers",
+        ["Texture"] = "rbxassetid://16583325082"
+    },
+    ["MetalPlates"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonMetalPlates",
+        ["Texture"] = "rbxassetid://14266317329"
+    },
+    ["FurTufts"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonFurTufts",
+        ["Texture"] = "rbxassetid://14277476530"
+    },
+    ["Sandstone"] = {
+        ["Material"] = "Sandstone",
+        ["MaterialVariant"] = "DragonSandstone",
+        ["Texture"] = "rbxassetid://10912626792"
+    },
+    ["CrackedRock"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonCrackedRock",
+        ["Texture"] = "rbxassetid://14277773314"
+    },
+    ["Foil"] = {["Material"] = "Foil", ["MaterialVariant"] = "", ["Texture"] = "rbxassetid://13336378266"},
+    ["Patches"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonPatches",
+        ["Texture"] = "rbxassetid://14290929409"
+    },
+    ["Cobblestone"] = {
+        ["Material"] = "Cobblestone",
+        ["MaterialVariant"] = "DragonCobblestone",
+        ["Texture"] = "rbxassetid://13336352130"
+    },
+    ["WrappingPaper"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonWrappingPaper",
+        ["Texture"] = "rbxassetid://15777053773"
+    },
+    ["GatorLeather"] = {
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonLeatherGator",
+        ["Texture"] = "rbxassetid://14277314135"
+    },
+    ["Slate"] = {["Material"] = "Slate", ["MaterialVariant"] = "DragonSlate", ["Texture"] = "rbxassetid://13336453080"},
+    ["Granite"] = {["Material"] = "Granite", ["MaterialVariant"] = "", ["Texture"] = "rbxassetid://13336441259"},
+    ["Salt"] = {["Material"] = "Salt", ["MaterialVariant"] = "DragonSalt", ["Texture"] = "rbxassetid://10912626963"},
+    ["Pumpkin"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonPumpkin",
+        ["Texture"] = "rbxassetid://92186397008685"
+    },
+    ["Quilt"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonQuilt",
+        ["Texture"] = "rbxassetid://14293476108"
+    },
+    ["CrackedLava"] = {
+        ["LightInfluence"] = 0,
+        ["Brightness"] = 5,
+        ["LightEmission"] = 0,
+        ["Material"] = "CrackedLava",
+        ["MaterialVariant"] = "DragonCrackedLava",
+        ["Texture"] = "rbxassetid://10912667016"
+    },
+    ["Plagued"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonPlagued",
+        ["Texture"] = "rbxassetid://120486644791386"
+    },
+    ["Cursed"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonCursed",
+        ["Texture"] = "rbxassetid://71832829057168"
+    },
+    ["Gemstone"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonGemstone",
+        ["Texture"] = "rbxassetid://16582522864"
+    },
+    ["Pasture"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonPasture",
+        ["Texture"] = "rbxassetid://16582462231"
+    },
+    ["WallPaper"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonWallPaper",
+        ["Texture"] = "rbxassetid://15777243045"
+    },
+    ["Grass"] = {["Material"] = "Grass", ["MaterialVariant"] = "DragonGrass", ["Texture"] = "rbxassetid://13336441107"},
+    ["Glass"] = {["Material"] = "Glass", ["MaterialVariant"] = "", ["Texture"] = "rbxassetid://13336418398"},
+    ["Neon"] = {
+        ["LightInfluence"] = 0,
+        ["Brightness"] = 5,
+        ["LightEmission"] = 0,
+        ["Material"] = "Neon",
+        ["MaterialVariant"] = "",
+        ["Texture"] = "rbxassetid://13336329695"
+    },
+    ["Sand"] = {["Material"] = "Sand", ["MaterialVariant"] = "DragonSand", ["Texture"] = "rbxassetid://13336450679"},
+    ["WavyFur"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonFurWavy",
+        ["Texture"] = "rbxassetid://14277533794"
+    },
+    ["Scute"] = {
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonScute",
+        ["Texture"] = "rbxassetid://13796443461"
+    },
+    ["Weeds"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonWeeds",
+        ["Texture"] = "rbxassetid://14277859594"
+    },
+    ["ThickFur"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonFurThick",
+        ["Texture"] = "rbxassetid://14277509853"
+    },
+    ["Serpentine"] = {
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonSerpentine",
+        ["Texture"] = "rbxassetid://14277635760"
+    },
+    ["ThinFur"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonFurThin",
+        ["Texture"] = "rbxassetid://14277525231"
+    },
+    ["Marble"] = {["Material"] = "Marble", ["MaterialVariant"] = "", ["Texture"] = "rbxassetid://13336429118"},
+    ["StarPaper"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonStarPaper",
+        ["Texture"] = "rbxassetid://15775954744"
+    },
+    ["Glacier"] = {
+        ["Material"] = "Glacier",
+        ["MaterialVariant"] = "DragonGlacier",
+        ["Texture"] = "rbxassetid://10912626527"
+    },
+    ["FishScales"] = {
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonScalesFish",
+        ["Texture"] = "rbxassetid://14266386933"
+    },
+    ["Basalt"] = {
+        ["Material"] = "Basalt",
+        ["MaterialVariant"] = "DragonBasalt",
+        ["Texture"] = "rbxassetid://10912626360"
+    },
+    ["Snow"] = {["Material"] = "Snow", ["MaterialVariant"] = "DragonSnow", ["Texture"] = "rbxassetid://10912627100"},
+    ["Circuitry"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonCircuit",
+        ["Texture"] = "rbxassetid://14268163162"
+    },
+    ["ToughLeather"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonLeatherTough",
+        ["Texture"] = "rbxassetid://14277256629"
+    },
+    ["SharpScales"] = {
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonScalesSharp",
+        ["Texture"] = "rbxassetid://14266386110"
+    },
+    ["Metal"] = {["Material"] = "Metal", ["MaterialVariant"] = "", ["Texture"] = "rbxassetid://13336431949"},
+    ["Limestone"] = {
+        ["Material"] = "Limestone",
+        ["MaterialVariant"] = "DragonLimestone",
+        ["Texture"] = "rbxassetid://10912626669"
+    },
+    ["BigScales"] = {
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonScalesBig",
+        ["Texture"] = "rbxassetid://14226299758"
+    },
+    ["SmoothPlastic"] = {
+        ["Material"] = "SmoothPlastic",
+        ["MaterialVariant"] = "DragonSmoothPlastic",
+        ["Texture"] = "rbxassetid://13336329695"
+    },
+    ["Mud"] = {["Material"] = "Mud", ["MaterialVariant"] = "DragonMud", ["Texture"] = "rbxassetid://10912627216"},
+    ["RoundedScales"] = {
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonScalesRounded",
+        ["Texture"] = "rbxassetid://14226334124"
+    },
+    ["Scales"] = {
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonScales",
+        ["Texture"] = "rbxassetid://14226299758"
+    },
+    ["WornLeather"] = {
+        ["LightInfluence"] = 1,
+        ["LightEmission"] = 0,
+        ["Material"] = "Plastic",
+        ["MaterialVariant"] = "DragonLeatherWorn",
+        ["Texture"] = "rbxassetid://14277197477"
+    },
+    ["WoodPlanks"] = {["Material"] = "WoodPlanks", ["MaterialVariant"] = "", ["Texture"] = "rbxassetid://7553187142"},
+    ["Concrete"] = {["Material"] = "Concrete", ["MaterialVariant"] = "", ["Texture"] = "rbxassetid://13336367951"},
+    ["Pebble"] = {["Material"] = "Pebble", ["MaterialVariant"] = "", ["Texture"] = "rbxassetid://13336435761"},
+    ["Asphalt"] = {
+        ["Material"] = "Asphalt",
+        ["MaterialVariant"] = "DragonAsphalt",
+        ["Texture"] = "rbxassetid://10912627697"
+    },
+    ["Ice"] = {["Material"] = "Ice", ["MaterialVariant"] = "DragonIce", ["Texture"] = "rbxassetid://13336425411"}
+}
