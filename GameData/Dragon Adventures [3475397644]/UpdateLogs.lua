8cf4b9c8612dca1c98cfd10522b11d3ce2d716d4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-29 03:24:04 +1100

    Create UpdateLogs.lua

---
+++ GameData/Dragon Adventures [3475397644]/UpdateLogs.lua
@@ -0,0 +1,92 @@
+data = {
+    ["3"] = {
+        ["ReleaseTime"] = 1727740800,
+        ["ShortTitle"] = "Season 37",
+        ["Title"] = "Dragon Season 37",
+        ["Index"] = 3,
+        ["Description"] = [[🐉 <font color="#32CD32">NEW!</font> Dragon Season 37 Begins!
+🌪️ <font color="#32CD32">NEW!</font> Tempiritus Dragon Added!
+🔄 Torneidus Dragon Returns!
+	]],
+        ["Icon"] = "rbxassetid://77924023878548",
+        ["Thumbnail"] = "rbxassetid://120196356417270"
+    },
+    ["2"] = {
+        ["ReleaseTime"] = 1727467200,
+        ["ShortTitle"] = "World Missions",
+        ["Title"] = "World Missions",
+        ["Index"] = 2,
+        ["Description"] = [[🌍 <font color="#32CD32">NEW!</font> World Missions per world added!
+⚔️ <font color="#32CD32">NEW!</font> Guild Wars Double Points Extended till October 1st.
+🥚 <font color="#32CD32">NEW!</font> Spinner Wheel Egg added to spinner wheel!
+🔓 Changed world unlocking to complete a set of world missions.
+🎨 Revamped Season and Mission UI!
+🦁 Updated Dysuva Roar.
+🐉 Fixes to Numine & Magmip Dragons.
+	]],
+        ["Icon"] = "rbxassetid://131238996177580",
+        ["Thumbnail"] = "rbxassetid://134823406395203"
+    },
+    ["5"] = {
+        ["ReleaseTime"] = 1728680400,
+        ["ShortTitle"] = "Halloween Event",
+        ["Title"] = "Halloween Event",
+        ["Index"] = 5,
+        ["Description"] = [[👻 <font color="#9B59B6"><b>Halloween Castle World Added!</b></font>
+🐉 <font color="#32CD32">NEW!</font> Dratheros Dragon Added!
+🎃 <font color="#32CD32">NEW!</font> Daily Missions for Halloween added!
+💀 Halloween Treasure Chests Added!
+👹 Halloween Mobs and Elite Mobs Added!
+	]],
+        ["Icon"] = "rbxassetid://109617536570064",
+        ["Thumbnail"] = "rbxassetid://78684698846636"
+    },
+    ["4"] = {
+        ["ReleaseTime"] = 1728070579,
+        ["ShortTitle"] = "Halloween Countdown",
+        ["Title"] = "Halloween Countdown",
+        ["Index"] = 4,
+        ["Description"] = [[🎃 <font color="#32CD32">NEW!</font> Countdown to Halloween event added!
+🎃 <font color="#32CD32">NEW!</font> Pumpkin Smashing in Origins added for the duration of Halloween!
+🕊️ <font color="#32CD32">NEW!</font> Stellaris UGC Wings added!
+🐣 Hatching Cosmetic traits added to item feed when you hatch a dragon with a cosmetic trait.
+📊 Added season points counter to Dragon Season UI.
+🎡 Added the ability to spin up to 5 spin tokens at once at the spinner wheel.
+🎒 Removed bigger backpack prompt after being prompted once.
+🐉 Auto-equip your last dragon when rejoining Dragon Adventures added.
+	]],
+        ["Icon"] = "rbxassetid://86523937378525",
+        ["Thumbnail"] = "rbxassetid://134829987553308"
+    },
+    ["7"] = {
+        ["ReleaseTime"] = 1729882800,
+        ["ShortTitle"] = "Halloween Week 3",
+        ["Title"] = "Halloween Week 3 Update",
+        ["Index"] = 7,
+        ["Description"] = [[🎃 <font color="#32CD32">NEW!</font> Halloween Events Egg Hatching Leaderboard with unique rewards!
+🛍️ <font color="#32CD32">NEW!</font> Special Halloween Store Items in the Halloween World!
+📅 <font color="#32CD32">NEW!</font> Week 3 Halloween Missions Added!
+🐉 <font color="#32CD32">NEW!</font> Lumenigh Developer Dragon bundle Added!
+🧪 Ghoul Material Potion added.
+🎉 Gargoyle Hatch Party Incubator added.
+🧸 Haunted Doll and Undead Theme Potions added.
+📸 Bat and Evil Halloween camera filters added.
+	]],
+        ["Icon"] = "rbxassetid://92022892572119",
+        ["Thumbnail"] = "rbxassetid://113173573331789"
+    },
+    ["6"] = {
+        ["ReleaseTime"] = 1729267372,
+        ["ShortTitle"] = "Djaevelhest Dragon",
+        ["Title"] = "Djaevelhest Dragon",
+        ["Index"] = 6,
+        ["Description"] = [[🐉 <font color="#0000FF">NEW! Djaevelhest Dragon added to the Balefire Dragon Egg!</font>
+👹 <font color="#32CD32">NEW!</font> Halloween Paranox Boss Fight Added!
+💥 <font color="#32CD32">NEW!</font> Balefire Storm event added to Halloween Crystal!
+🧪 Potion models added to replace old color and material potions.
+👻 Halloween Mob merger changed to include juvenile & adult ages.
+	]],
+        ["Icon"] = "rbxassetid://114308516619028",
+        ["Thumbnail"] = "rbxassetid://101676278573942"
+    }
+}
