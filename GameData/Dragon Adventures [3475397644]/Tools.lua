985c131d472017c6d744e64b9173e8f1355034d5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-29 03:13:06 +1100

    Create Tools.lua

---
+++ GameData/Dragon Adventures [3475397644]/Tools.lua
@@ -0,0 +1,192 @@
+data = {
+    ["TeddyBear"] = {
+        ["Image"] = "rbxthumb://type=Asset&id=139155791025208&w=150&h=150",
+        ["Description"] = "Water your crops with this!",
+        ["Category"] = "Tools",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["Drops"] = {["DropChance"] = 12, ["Amount"] = 4},
+            ["Fishing"] = {
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 4, ["Max"] = 5},
+                ["DropChance"] = 55,
+                ["SpinSpeed"] = {["Min"] = 200, ["Max"] = 300}
+            },
+            ["TextStrokeColor"] = Color3.fromRGB(89, 89, 89),
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 250},
+            ["Auction"] = {["MinAmount"] = 1000},
+            ["Name"] = "Common",
+            ["BreedCost"] = {["MinAmount"] = 250},
+            ["Bosses"] = {["Amount"] = 100},
+            ["SpinnerWheel"] = {["DropChance"] = 50, ["Amount"] = 100},
+            ["RecyleAccessory"] = true,
+            ["TextColor"] = Color3.fromRGB(181, 181, 181)
+        },
+        ["ItemType"] = "Tools",
+        ["Name"] = "TeddyBear",
+        ["Cosmetic"] = true
+    },
+    ["Brush"] = {
+        ["Image"] = "rbxthumb://type=Asset&id=93623795472634&w=150&h=150",
+        ["Description"] = "Water your crops with this!",
+        ["Category"] = "Tools",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["Drops"] = {["DropChance"] = 12, ["Amount"] = 4},
+            ["Fishing"] = {
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 4, ["Max"] = 5},
+                ["DropChance"] = 55,
+                ["SpinSpeed"] = {["Min"] = 200, ["Max"] = 300}
+            },
+            ["TextStrokeColor"] = Color3.fromRGB(89, 89, 89),
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 250},
+            ["Auction"] = {["MinAmount"] = 1000},
+            ["Name"] = "Common",
+            ["BreedCost"] = {["MinAmount"] = 250},
+            ["Bosses"] = {["Amount"] = 100},
+            ["SpinnerWheel"] = {["DropChance"] = 50, ["Amount"] = 100},
+            ["RecyleAccessory"] = true,
+            ["TextColor"] = Color3.fromRGB(181, 181, 181)
+        },
+        ["ItemType"] = "Tools",
+        ["Name"] = "Brush",
+        ["Cosmetic"] = true
+    },
+    ["Showerhead"] = {
+        ["Image"] = "rbxthumb://type=Asset&id=109128375030669&w=150&h=150",
+        ["Description"] = "Water your crops with this!",
+        ["Category"] = "Tools",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["Drops"] = {["DropChance"] = 12, ["Amount"] = 4},
+            ["Fishing"] = {
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 4, ["Max"] = 5},
+                ["DropChance"] = 55,
+                ["SpinSpeed"] = {["Min"] = 200, ["Max"] = 300}
+            },
+            ["TextStrokeColor"] = Color3.fromRGB(89, 89, 89),
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 250},
+            ["Auction"] = {["MinAmount"] = 1000},
+            ["Name"] = "Common",
+            ["BreedCost"] = {["MinAmount"] = 250},
+            ["Bosses"] = {["Amount"] = 100},
+            ["SpinnerWheel"] = {["DropChance"] = 50, ["Amount"] = 100},
+            ["RecyleAccessory"] = true,
+            ["TextColor"] = Color3.fromRGB(181, 181, 181)
+        },
+        ["ItemType"] = "Tools",
+        ["Name"] = "Showerhead",
+        ["Cosmetic"] = true
+    },
+    ["WashingCloth"] = {
+        ["Image"] = "rbxthumb://type=Asset&id=91703362673161&w=150&h=150",
+        ["Description"] = "Water your crops with this!",
+        ["Category"] = "Tools",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["Drops"] = {["DropChance"] = 12, ["Amount"] = 4},
+            ["Fishing"] = {
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 4, ["Max"] = 5},
+                ["DropChance"] = 55,
+                ["SpinSpeed"] = {["Min"] = 200, ["Max"] = 300}
+            },
+            ["TextStrokeColor"] = Color3.fromRGB(89, 89, 89),
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 250},
+            ["Auction"] = {["MinAmount"] = 1000},
+            ["Name"] = "Common",
+            ["BreedCost"] = {["MinAmount"] = 250},
+            ["Bosses"] = {["Amount"] = 100},
+            ["SpinnerWheel"] = {["DropChance"] = 50, ["Amount"] = 100},
+            ["RecyleAccessory"] = true,
+            ["TextColor"] = Color3.fromRGB(181, 181, 181)
+        },
+        ["ItemType"] = "Tools",
+        ["Name"] = "WashingCloth",
+        ["Cosmetic"] = true
+    },
+    ["WateringCan"] = {
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["Drops"] = {["DropChance"] = 12, ["Amount"] = 4},
+            ["Fishing"] = {
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 4, ["Max"] = 5},
+                ["DropChance"] = 55,
+                ["SpinSpeed"] = {["Min"] = 200, ["Max"] = 300}
+            },
+            ["TextStrokeColor"] = Color3.fromRGB(89, 89, 89),
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 250},
+            ["Auction"] = {["MinAmount"] = 1000},
+            ["Name"] = "Common",
+            ["BreedCost"] = {["MinAmount"] = 250},
+            ["Bosses"] = {["Amount"] = 100},
+            ["SpinnerWheel"] = {["DropChance"] = 50, ["Amount"] = 100},
+            ["RecyleAccessory"] = true,
+            ["TextColor"] = Color3.fromRGB(181, 181, 181)
+        },
+        ["Image"] = "rbxthumb://type=Asset&id=108381795417327&w=150&h=150",
+        ["Description"] = "Water your crops with this!",
+        ["WateringDistance"] = 15,
+        ["ItemType"] = "Tools",
+        ["Name"] = "WateringCan",
+        ["Category"] = "Tools",
+        ["Cosmetic"] = true
+    },
+    ["MusicBox"] = {
+        ["Image"] = "rbxthumb://type=Asset&id=92761016874304&w=150&h=150",
+        ["Description"] = "Water your crops with this!",
+        ["Category"] = "Tools",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["Drops"] = {["DropChance"] = 12, ["Amount"] = 4},
+            ["Fishing"] = {
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 4, ["Max"] = 5},
+                ["DropChance"] = 55,
+                ["SpinSpeed"] = {["Min"] = 200, ["Max"] = 300}
+            },
+            ["TextStrokeColor"] = Color3.fromRGB(89, 89, 89),
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 250},
+            ["Auction"] = {["MinAmount"] = 1000},
+            ["Name"] = "Common",
+            ["BreedCost"] = {["MinAmount"] = 250},
+            ["Bosses"] = {["Amount"] = 100},
+            ["SpinnerWheel"] = {["DropChance"] = 50, ["Amount"] = 100},
+            ["RecyleAccessory"] = true,
+            ["TextColor"] = Color3.fromRGB(181, 181, 181)
+        },
+        ["ItemType"] = "Tools",
+        ["Name"] = "MusicBox",
+        ["Cosmetic"] = true
+    },
+    ["Soap"] = {
+        ["Image"] = "rbxthumb://type=Asset&id=80044741593613&w=150&h=150",
+        ["Description"] = "Water your crops with this!",
+        ["Category"] = "Tools",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["Drops"] = {["DropChance"] = 12, ["Amount"] = 4},
+            ["Fishing"] = {
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 4, ["Max"] = 5},
+                ["DropChance"] = 55,
+                ["SpinSpeed"] = {["Min"] = 200, ["Max"] = 300}
+            },
+            ["TextStrokeColor"] = Color3.fromRGB(89, 89, 89),
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 250},
+            ["Auction"] = {["MinAmount"] = 1000},
+            ["Name"] = "Common",
+            ["BreedCost"] = {["MinAmount"] = 250},
+            ["Bosses"] = {["Amount"] = 100},
+            ["SpinnerWheel"] = {["DropChance"] = 50, ["Amount"] = 100},
+            ["RecyleAccessory"] = true,
+            ["TextColor"] = Color3.fromRGB(181, 181, 181)
+        },
+        ["ItemType"] = "Tools",
+        ["Name"] = "Soap",
+        ["Cosmetic"] = true
+    }
+}
