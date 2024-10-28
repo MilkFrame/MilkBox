fdbce387e5ffc722087b4892ff2a06061f13aac4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-29 03:21:05 +1100

    Create Personalities.lua

---
+++ GameData/Dragon Adventures [3475397644]/Dragons/Personalities.lua
@@ -0,0 +1,109 @@
+data = {
+    ["Lazy"] = {
+        ["Name"] = "Lazy",
+        ["Image"] = "rbxassetid://9557627418",
+        ["Description"] = "Your dragon likes to do very little. This is your dragon's personality. You can change it with a Personality Potion.",
+        ["Effects"] = {["Speed"] = -0.05}
+    },
+    ["Hyperactive"] = {
+        ["Image"] = "rbxassetid://9557625136",
+        ["Description"] = "Your dragon has alot of energy. This is your dragon's personality. You can change it with a Personality Potion.",
+        ["Name"] = "Hyperactive",
+        ["Breeding"] = {"Lazy", "Greedy"},
+        ["Effects"] = {["Speed"] = 0.1, ["HungerRate"] = 0.05}
+    },
+    ["Enigmatic"] = {
+        ["Image"] = "rbxassetid://9557636923",
+        ["Description"] = "",
+        ["Tier"] = "Good",
+        ["Name"] = "Enigmatic",
+        ["Effects"] = {["Health"] = 0.1, ["ExpBoost"] = 0.1, ["Speed"] = 0.1, ["HungerRate"] = -0.1, ["Attack"] = 0.1}
+    },
+    ["Greedy"] = {
+        ["Name"] = "Greedy",
+        ["Image"] = "rbxassetid://9557620225",
+        ["Description"] = "Your dragon is often hungry. This is your dragon's personality. You can change it with a Personality Potion.",
+        ["Effects"] = {["HungerRate"] = 0.1}
+    },
+    ["Uninspired"] = {
+        ["Name"] = "Uninspired",
+        ["Image"] = "rbxassetid://9557631624",
+        ["Description"] = "Your dragon lacks motivation. This is your dragon's personality. You can change it with a Personality Potion.",
+        ["Effects"] = {["ExpBoost"] = -0.05}
+    },
+    ["Sad"] = {
+        ["Name"] = "Sad",
+        ["Image"] = "rbxassetid://9557628239",
+        ["Description"] = "Your dragon feels quite down. This is your dragon's personality. You can change it with a Personality Potion.",
+        ["Effects"] = {["Health"] = -0.05}
+    },
+    ["Brave"] = {
+        ["Image"] = "rbxassetid://9557615037",
+        ["Description"] = "Your dragon is quite bold. This is your dragon's personality. You can change it with a Personality Potion.",
+        ["Name"] = "Brave",
+        ["Breeding"] = {"Hyperactive", "Serene"},
+        ["Effects"] = {["Speed"] = 0.1}
+    },
+    ["Fearful"] = {
+        ["Name"] = "Fearful",
+        ["Image"] = "rbxassetid://9557617419",
+        ["Description"] = "Your dragon would rather run than fight. This is your dragon's personality. You can change it with a Personality Potion.",
+        ["Effects"] = {["Attack"] = -0.05}
+    },
+    ["Grumpy"] = {
+        ["Image"] = "rbxassetid://9557621072",
+        ["Description"] = "Your dragon has a temper. This is your dragon's personality. You can change it with a Personality Potion.",
+        ["Name"] = "Grumpy",
+        ["Breeding"] = {"Fearful", "Uninspired"},
+        ["Effects"] = {["Attack"] = 0.1, ["ExpBoost"] = -0.05}
+    },
+    ["Feisty"] = {
+        ["Image"] = "rbxassetid://9557618144",
+        ["Description"] = "Your dragon loves to fight. This is your dragon's personality. You can change it with a Personality Potion.",
+        ["Name"] = "Feisty",
+        ["Breeding"] = {"Grumpy", "Goofy"},
+        ["Effects"] = {["Attack"] = 0.1}
+    },
+    ["Serene"] = {
+        ["Image"] = "rbxassetid://9557629511",
+        ["Description"] = "Your dragon is very calm. This is your dragon's personality. You can change it with a Personality Potion.",
+        ["Name"] = "Serene",
+        ["Breeding"] = {"Lazy", "Uninspired"},
+        ["Effects"] = {["Speed"] = -0.05, ["ExpBoost"] = 0.1}
+    },
+    ["Contented"] = {
+        ["Image"] = "rbxassetid://9557615833",
+        ["Description"] = "Your dragon wants very little. This is your dragon's personality. You can change it with a Personality Potion.",
+        ["Name"] = "Contented",
+        ["Breeding"] = {"Hyperactive", "Sensitive"},
+        ["Effects"] = {["HungerRate"] = -0.1}
+    },
+    ["Goofy"] = {
+        ["Image"] = "rbxassetid://9557619452",
+        ["Description"] = "Your dragon has a sense of humor. This is your dragon's personality. You can change it with a Personality Potion.",
+        ["Name"] = "Goofy",
+        ["Breeding"] = {"Sad", "Fearful"},
+        ["Effects"] = {["Health"] = 0.1, ["Attack"] = -0.05}
+    },
+    ["Inspired"] = {
+        ["Image"] = "rbxassetid://9557625752",
+        ["Description"] = "Your dragon is super motivated. This is your dragon's personality. You can change it with a Personality Potion.",
+        ["Name"] = "Inspired",
+        ["Breeding"] = {"Serene", "Grumpy"},
+        ["Effects"] = {["ExpBoost"] = 0.1}
+    },
+    ["Happy"] = {
+        ["Image"] = "rbxassetid://9557621662",
+        ["Description"] = "Your dragon is very cheerful. This is your dragon's personality. You can change it with a Personality Potion.",
+        ["Name"] = "Happy",
+        ["Breeding"] = {"Goofy", "Sensitive"},
+        ["Effects"] = {["Health"] = 0.1}
+    },
+    ["Sensitive"] = {
+        ["Image"] = "rbxassetid://9557629097",
+        ["Description"] = "Your dragon feels hurt easily. This is your dragon's personality. You can change it with a Personality Potion.",
+        ["Name"] = "Sensitive",
+        ["Breeding"] = {"Sad", "Lazy"},
+        ["Effects"] = {["Health"] = -0.05}
+    }
+}
