67719cd116026afcbb6bf7f107c48d25ddf882ef
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-29 03:20:53 +1100

    Create MaterialTiers.lua

---
+++ GameData/Dragon Adventures [3475397644]/Dragons/MaterialTiers.lua
@@ -0,0 +1,7 @@
+data = {
+    {"Concrete", "Metal", "SmoothPlastic", "CrackedRock", "WornLeather"},
+    {"Slate", "Sand", "Grass", "Scales", "ThinFur", "FurTufts", "ToughLeather", "MetalPlates"},
+    {"Marble", "Pebble", "Scute", "Cobblestone", "BigScales", "RoundedScales", "ThickFur", "Patches"},
+    {"Glass", "Ice", "ForceField", "Serpentine", "SharpScales", "FishScales", "WavyFur", "Weeds"},
+    {"Foil", "Quilt", "GatorLeather", "Circuitry", "Granite", "Neon"}
+}
