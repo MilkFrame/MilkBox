472103324c393a4926d69ee0ce1bcbe9871af14f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-29 03:08:02 +1100

    Create Healing.lua

---
+++ GameData/Dragon Adventures [3475397644]/Healing.lua
@@ -0,0 +1,263 @@
+data = {
+    ["DragonRevivalHeart"] = {
+        ["Revival"] = {["Health"] = 1},
+        ["Rarity"] = {
+            ["Index"] = 3,
+            ["Drops"] = {["DropChance"] = 1, ["Amount"] = 1},
+            ["Fishing"] = {
+                ["SpinSpeed"] = {["Min"] = 300, ["Max"] = 400},
+                ["AnnounceReward"] = false,
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 6, ["Max"] = 7},
+                ["DropChance"] = 10
+            },
+            ["TextStrokeColor"] = Color3.fromRGB(17, 130, 175),
+            ["Alchemy"] = {["DropChance"] = 3, ["ElixirRecyleAmount"] = 25},
+            ["Auction"] = {["MinAmount"] = 10000},
+            ["Name"] = "Rare",
+            ["BreedCost"] = {["MinAmount"] = 1500},
+            ["Bosses"] = {["Amount"] = 100},
+            ["SpinnerWheel"] = {["DropChance"] = 10, ["Amount"] = 25},
+            ["RecyleAccessory"] = true,
+            ["TextColor"] = Color3.fromRGB(26, 183, 255)
+        },
+        ["GetTip"] = "function: 0xe88951fc1fcbbe60",
+        ["Image"] = "rbxthumb://type=Asset&id=8812279421&w=150&h=150",
+        ["Name"] = "DragonRevivalHeart",
+        ["Category"] = "Healing",
+        ["Sell"] = 45,
+        ["ItemType"] = "Resources",
+        ["Buy"] = 170
+    },
+    ["BasicHealingPotion"] = {
+        ["ItemType"] = "Resources",
+        ["Image"] = "rbxthumb://type=Asset&id=8812274286&w=150&h=150",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["Drops"] = {["DropChance"] = 12, ["Amount"] = 4},
+            ["Fishing"] = {
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 4, ["Max"] = 5},
+                ["DropChance"] = 55,
+                ["SpinSpeed"] = {["Min"] = 200, ["Max"] = 300}
+            },
+            ["TextStrokeColor"] = Color3.fromRGB(89, 89, 89),
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 250},
+            ["Auction"] = {["MinAmount"] = 1000},
+            ["Name"] = "Common",
+            ["BreedCost"] = {["MinAmount"] = 250},
+            ["Bosses"] = {["Amount"] = 100},
+            ["SpinnerWheel"] = {["DropChance"] = 50, ["Amount"] = 100},
+            ["RecyleAccessory"] = true,
+            ["TextColor"] = Color3.fromRGB(181, 181, 181)
+        },
+        ["Category"] = "Healing",
+        ["Name"] = "BasicHealingPotion",
+        ["GetTip"] = "function: 0xe88951fc1fcbbe60",
+        ["HealthRate"] = {["Rate"] = 2, ["Duration"] = 30},
+        ["DisplayName"] = "BasicHealingMedicine"
+    },
+    ["RevivalHeart"] = {
+        ["Revival"] = {["Health"] = 0.25},
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["Drops"] = {["DropChance"] = 12, ["Amount"] = 4},
+            ["Fishing"] = {
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 4, ["Max"] = 5},
+                ["DropChance"] = 55,
+                ["SpinSpeed"] = {["Min"] = 200, ["Max"] = 300}
+            },
+            ["TextStrokeColor"] = Color3.fromRGB(89, 89, 89),
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 250},
+            ["Auction"] = {["MinAmount"] = 1000},
+            ["Name"] = "Common",
+            ["BreedCost"] = {["MinAmount"] = 250},
+            ["Bosses"] = {["Amount"] = 100},
+            ["SpinnerWheel"] = {["DropChance"] = 50, ["Amount"] = 100},
+            ["RecyleAccessory"] = true,
+            ["TextColor"] = Color3.fromRGB(181, 181, 181)
+        },
+        ["GetTip"] = "function: 0xe88951fc1fcbbe60",
+        ["Image"] = "rbxthumb://type=Asset&id=8812284034&w=150&h=150",
+        ["Name"] = "RevivalHeart",
+        ["Category"] = "Healing",
+        ["Sell"] = 8,
+        ["ItemType"] = "Resources",
+        ["Buy"] = 30
+    },
+    ["MagicalBandages"] = {
+        ["Rarity"] = {
+            ["Index"] = 2,
+            ["Drops"] = {["DropChance"] = 6, ["Amount"] = 2},
+            ["Fishing"] = {
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 5, ["Max"] = 6},
+                ["DropChance"] = 30,
+                ["SpinSpeed"] = {["Min"] = 250, ["Max"] = 350}
+            },
+            ["TextStrokeColor"] = Color3.fromRGB(52, 127, 23),
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 50},
+            ["Auction"] = {["MinAmount"] = 5000},
+            ["Name"] = "Uncommon",
+            ["BreedCost"] = {["MinAmount"] = 700},
+            ["Bosses"] = {["Amount"] = 100},
+            ["SpinnerWheel"] = {["DropChance"] = 20, ["Amount"] = 50},
+            ["RecyleAccessory"] = true,
+            ["TextColor"] = Color3.fromRGB(82, 199, 36)
+        },
+        ["GetTip"] = "function: 0xe88951fc1fcbbe60",
+        ["Image"] = "rbxthumb://type=Asset&id=8812272051&w=150&h=150",
+        ["Health"] = 0.5,
+        ["Category"] = "Healing",
+        ["ItemType"] = "Resources",
+        ["Sell"] = 25,
+        ["Name"] = "MagicalBandages",
+        ["Buy"] = 85
+    },
+    ["DragonscaleBandages"] = {
+        ["Rarity"] = {
+            ["Index"] = 3,
+            ["Drops"] = {["DropChance"] = 1, ["Amount"] = 1},
+            ["Fishing"] = {
+                ["SpinSpeed"] = {["Min"] = 300, ["Max"] = 400},
+                ["AnnounceReward"] = false,
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 6, ["Max"] = 7},
+                ["DropChance"] = 10
+            },
+            ["TextStrokeColor"] = Color3.fromRGB(17, 130, 175),
+            ["Alchemy"] = {["DropChance"] = 3, ["ElixirRecyleAmount"] = 25},
+            ["Auction"] = {["MinAmount"] = 10000},
+            ["Name"] = "Rare",
+            ["BreedCost"] = {["MinAmount"] = 1500},
+            ["Bosses"] = {["Amount"] = 100},
+            ["SpinnerWheel"] = {["DropChance"] = 10, ["Amount"] = 25},
+            ["RecyleAccessory"] = true,
+            ["TextColor"] = Color3.fromRGB(26, 183, 255)
+        },
+        ["GetTip"] = "function: 0xe88951fc1fcbbe60",
+        ["Image"] = "rbxthumb://type=Asset&id=8812269241&w=150&h=150",
+        ["Health"] = 1,
+        ["Category"] = "Healing",
+        ["ItemType"] = "Resources",
+        ["Sell"] = 50,
+        ["Name"] = "DragonscaleBandages",
+        ["Buy"] = 200
+    },
+    ["Bandages"] = {
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["Drops"] = {["DropChance"] = 12, ["Amount"] = 4},
+            ["Fishing"] = {
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 4, ["Max"] = 5},
+                ["DropChance"] = 55,
+                ["SpinSpeed"] = {["Min"] = 200, ["Max"] = 300}
+            },
+            ["TextStrokeColor"] = Color3.fromRGB(89, 89, 89),
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 250},
+            ["Auction"] = {["MinAmount"] = 1000},
+            ["Name"] = "Common",
+            ["BreedCost"] = {["MinAmount"] = 250},
+            ["Bosses"] = {["Amount"] = 100},
+            ["SpinnerWheel"] = {["DropChance"] = 50, ["Amount"] = 100},
+            ["RecyleAccessory"] = true,
+            ["TextColor"] = Color3.fromRGB(181, 181, 181)
+        },
+        ["GetTip"] = "function: 0xe88951fc1fcbbe60",
+        ["Image"] = "rbxthumb://type=Asset&id=8812266105&w=150&h=150",
+        ["Health"] = 0.25,
+        ["Category"] = "Healing",
+        ["ItemType"] = "Resources",
+        ["Sell"] = 3,
+        ["Name"] = "Bandages",
+        ["Buy"] = 35
+    },
+    ["GoldenRevivalHeart"] = {
+        ["Revival"] = {["Health"] = 0.5},
+        ["Rarity"] = {
+            ["Index"] = 2,
+            ["Drops"] = {["DropChance"] = 6, ["Amount"] = 2},
+            ["Fishing"] = {
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 5, ["Max"] = 6},
+                ["DropChance"] = 30,
+                ["SpinSpeed"] = {["Min"] = 250, ["Max"] = 350}
+            },
+            ["TextStrokeColor"] = Color3.fromRGB(52, 127, 23),
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 50},
+            ["Auction"] = {["MinAmount"] = 5000},
+            ["Name"] = "Uncommon",
+            ["BreedCost"] = {["MinAmount"] = 700},
+            ["Bosses"] = {["Amount"] = 100},
+            ["SpinnerWheel"] = {["DropChance"] = 20, ["Amount"] = 50},
+            ["RecyleAccessory"] = true,
+            ["TextColor"] = Color3.fromRGB(82, 199, 36)
+        },
+        ["GetTip"] = "function: 0xe88951fc1fcbbe60",
+        ["Image"] = "rbxthumb://type=Asset&id=8812281238&w=150&h=150",
+        ["Name"] = "GoldenRevivalHeart",
+        ["Category"] = "Healing",
+        ["Sell"] = 20,
+        ["ItemType"] = "Resources",
+        ["Buy"] = 70
+    },
+    ["DragonBreathHealingPotion"] = {
+        ["ItemType"] = "Resources",
+        ["Image"] = "rbxthumb://type=Asset&id=8812275937&w=150&h=150",
+        ["Rarity"] = {
+            ["Index"] = 2,
+            ["Drops"] = {["DropChance"] = 6, ["Amount"] = 2},
+            ["Fishing"] = {
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 5, ["Max"] = 6},
+                ["DropChance"] = 30,
+                ["SpinSpeed"] = {["Min"] = 250, ["Max"] = 350}
+            },
+            ["TextStrokeColor"] = Color3.fromRGB(52, 127, 23),
+            ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 50},
+            ["Auction"] = {["MinAmount"] = 5000},
+            ["Name"] = "Uncommon",
+            ["BreedCost"] = {["MinAmount"] = 700},
+            ["Bosses"] = {["Amount"] = 100},
+            ["SpinnerWheel"] = {["DropChance"] = 20, ["Amount"] = 50},
+            ["RecyleAccessory"] = true,
+            ["TextColor"] = Color3.fromRGB(82, 199, 36)
+        },
+        ["Category"] = "Healing",
+        ["Name"] = "DragonBreathHealingPotion",
+        ["GetTip"] = "function: 0xe88951fc1fcbbe60",
+        ["HealthRate"] = {["Rate"] = 4, ["Duration"] = 30},
+        ["DisplayName"] = "DragonBreathHealingMedicine"
+    },
+    ["GemstoneHealingPotion"] = {
+        ["ItemType"] = "Resources",
+        ["Image"] = "rbxthumb://type=Asset&id=8812277458&w=150&h=150",
+        ["Rarity"] = {
+            ["Index"] = 3,
+            ["Drops"] = {["DropChance"] = 1, ["Amount"] = 1},
+            ["Fishing"] = {
+                ["SpinSpeed"] = {["Min"] = 300, ["Max"] = 400},
+                ["AnnounceReward"] = false,
+                ["WaitTime"] = 10,
+                ["RequiredSuccesses"] = {["Min"] = 6, ["Max"] = 7},
+                ["DropChance"] = 10
+            },
+            ["TextStrokeColor"] = Color3.fromRGB(17, 130, 175),
+            ["Alchemy"] = {["DropChance"] = 3, ["ElixirRecyleAmount"] = 25},
+            ["Auction"] = {["MinAmount"] = 10000},
+            ["Name"] = "Rare",
+            ["BreedCost"] = {["MinAmount"] = 1500},
+            ["Bosses"] = {["Amount"] = 100},
+            ["SpinnerWheel"] = {["DropChance"] = 10, ["Amount"] = 25},
+            ["RecyleAccessory"] = true,
+            ["TextColor"] = Color3.fromRGB(26, 183, 255)
+        },
+        ["Category"] = "Healing",
+        ["Name"] = "GemstoneHealingPotion",
+        ["GetTip"] = "function: 0xe88951fc1fcbbe60",
+        ["HealthRate"] = {["Rate"] = 8, ["Duration"] = 30},
+        ["DisplayName"] = "GemstoneHealingMedicine"
+    }
+}
