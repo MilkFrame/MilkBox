d448503098214d781e0d5a8e8c1746f08be2961b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-29 03:13:38 +1100

    Create ItemRarity.lua

---
+++ GameData/Dragon Adventures [3475397644]/ItemRarity.lua
@@ -0,0 +1,110 @@
+data = {
+    ["Legendary"] = {
+        ["Index"] = 5,
+        ["Fishing"] = {
+            ["SpinSpeed"] = {["Min"] = 400, ["Max"] = 500},
+            ["AnnounceReward"] = true,
+            ["WaitTime"] = 10,
+            ["RequiredSuccesses"] = {["Min"] = 8, ["Max"] = 9},
+            ["DropChance"] = 1
+        },
+        ["Hatching"] = {["AnnounceReward"] = true},
+        ["TextStrokeColor"] = Color3.fromRGB(156, 93, 6),
+        ["Auction"] = {["MinAmount"] = 75000},
+        ["AccessoryGamepassEffect"] = true,
+        ["Name"] = "Legendary",
+        ["BreedCost"] = {["MinAmount"] = 75000},
+        ["Bosses"] = {["Amount"] = 1, ["AnnounceReward"] = true},
+        ["SpinnerWheel"] = {["Amount"] = 1, ["DropChance"] = 1, ["AnnounceReward"] = true},
+        ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 1},
+        ["TextColor"] = Color3.fromRGB(255, 153, 10)
+    },
+    ["Common"] = {
+        ["Index"] = 1,
+        ["Drops"] = {["DropChance"] = 12, ["Amount"] = 4},
+        ["Fishing"] = {
+            ["WaitTime"] = 10,
+            ["RequiredSuccesses"] = {["Min"] = 4, ["Max"] = 5},
+            ["DropChance"] = 55,
+            ["SpinSpeed"] = {["Min"] = 200, ["Max"] = 300}
+        },
+        ["TextStrokeColor"] = Color3.fromRGB(89, 89, 89),
+        ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 250},
+        ["Auction"] = {["MinAmount"] = 1000},
+        ["Name"] = "Common",
+        ["BreedCost"] = {["MinAmount"] = 250},
+        ["Bosses"] = {["Amount"] = 100},
+        ["SpinnerWheel"] = {["DropChance"] = 50, ["Amount"] = 100},
+        ["RecyleAccessory"] = true,
+        ["TextColor"] = Color3.fromRGB(181, 181, 181)
+    },
+    ["Epic"] = {
+        ["Index"] = 4,
+        ["Fishing"] = {
+            ["SpinSpeed"] = {["Min"] = 350, ["Max"] = 450},
+            ["AnnounceReward"] = true,
+            ["WaitTime"] = 10,
+            ["RequiredSuccesses"] = {["Min"] = 7, ["Max"] = 8},
+            ["DropChance"] = 5
+        },
+        ["Hatching"] = {["AnnounceReward"] = true},
+        ["TextStrokeColor"] = Color3.fromRGB(106, 19, 149),
+        ["Auction"] = {["MinAmount"] = 25000},
+        ["AccessoryGamepassEffect"] = true,
+        ["Name"] = "Epic",
+        ["BreedCost"] = {["MinAmount"] = 25000},
+        ["Bosses"] = {["Amount"] = 1, ["AnnounceReward"] = true},
+        ["SpinnerWheel"] = {["Amount"] = 10, ["DropChance"] = 4, ["AnnounceReward"] = true},
+        ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 10},
+        ["TextColor"] = Color3.fromRGB(182, 34, 255)
+    },
+    ["Relic"] = {
+        ["Auction"] = {["MinAmount"] = 1000000},
+        ["AccessoryGamepassEffect"] = true,
+        ["Index"] = 6,
+        ["BreedCost"] = {["MinAmount"] = 1000000},
+        ["TextStrokeColor"] = Color3.fromRGB(141, 10, 30),
+        ["SpinnerWheel"] = {["Amount"] = 1, ["DropChance"] = 0.75, ["AnnounceReward"] = true},
+        ["Name"] = "Relic",
+        ["TextColor"] = Color3.fromRGB(255, 28, 43)
+    },
+    ["Uncommon"] = {
+        ["Index"] = 2,
+        ["Drops"] = {["DropChance"] = 6, ["Amount"] = 2},
+        ["Fishing"] = {
+            ["WaitTime"] = 10,
+            ["RequiredSuccesses"] = {["Min"] = 5, ["Max"] = 6},
+            ["DropChance"] = 30,
+            ["SpinSpeed"] = {["Min"] = 250, ["Max"] = 350}
+        },
+        ["TextStrokeColor"] = Color3.fromRGB(52, 127, 23),
+        ["Alchemy"] = {["DropChance"] = 30, ["ElixirRecyleAmount"] = 50},
+        ["Auction"] = {["MinAmount"] = 5000},
+        ["Name"] = "Uncommon",
+        ["BreedCost"] = {["MinAmount"] = 700},
+        ["Bosses"] = {["Amount"] = 100},
+        ["SpinnerWheel"] = {["DropChance"] = 20, ["Amount"] = 50},
+        ["RecyleAccessory"] = true,
+        ["TextColor"] = Color3.fromRGB(82, 199, 36)
+    },
+    ["Rare"] = {
+        ["Index"] = 3,
+        ["Drops"] = {["DropChance"] = 1, ["Amount"] = 1},
+        ["Fishing"] = {
+            ["SpinSpeed"] = {["Min"] = 300, ["Max"] = 400},
+            ["AnnounceReward"] = false,
+            ["WaitTime"] = 10,
+            ["RequiredSuccesses"] = {["Min"] = 6, ["Max"] = 7},
+            ["DropChance"] = 10
+        },
+        ["TextStrokeColor"] = Color3.fromRGB(17, 130, 175),
+        ["Alchemy"] = {["DropChance"] = 3, ["ElixirRecyleAmount"] = 25},
+        ["Auction"] = {["MinAmount"] = 10000},
+        ["Name"] = "Rare",
+        ["BreedCost"] = {["MinAmount"] = 1500},
+        ["Bosses"] = {["Amount"] = 100},
+        ["SpinnerWheel"] = {["DropChance"] = 10, ["Amount"] = 25},
+        ["RecyleAccessory"] = true,
+        ["TextColor"] = Color3.fromRGB(26, 183, 255)
+    }
+}
