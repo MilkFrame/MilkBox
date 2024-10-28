76c988ae98ff7c5b6f2985c486c1aae9438f58e5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-29 03:21:16 +1100

    Create PersonalityTiers.lua

---
+++ GameData/Dragon Adventures [3475397644]/Dragons/PersonalityTiers.lua
@@ -0,0 +1,9 @@
+data = {
+    ["Good"] = {["P"] = 5, ["Name"] = "Good", ["Personalities"] = {"Happy", "Brave", "Feisty", "Inspired", "Contented"}},
+    ["Bad"] = {["P"] = 55, ["Name"] = "Bad", ["Personalities"] = {"Sad", "Lazy", "Fearful", "Uninspired", "Greedy"}},
+    ["Neutral"] = {
+        ["P"] = 40,
+        ["Name"] = "Neutral",
+        ["Personalities"] = {"Serene", "Hyperactive", "Grumpy", "Goofy", "Sensitive"}
+    }
+}
