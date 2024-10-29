62e86b85cd20dbce96ce7941473bd3185775fb57
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-30 03:02:37 +1100

    Create The Haunt [82695214392018].lua

---
+++ Leaderboards/The Haunt [82695214392018].lua
@@ -0,0 +1,73 @@
+--26 October 2024 6:00am
+data = {
+    ["Creatures"] = {
+        {1, "@sutiu_doc", "rbxpost://f6299e9e-0733-459c-9c87-e61eab5b818b+Halloween2024Creatures", 7493100767},
+        {2, "@SkieSkyDark", "rbxpost://f5f40d3a-192b-4dea-b181-62236a77b8c0+Halloween2024Creatures", 7482296350},
+        {3, "@Rigo_g015", "rbxpost://f562a3f9-51f5-40b3-871f-6daf1edc8c87+Halloween2024Creatures", 2518596624},
+        {4, "@piggiezRcuteUwU", "rbxpost://ed2fd04d-cb06-498c-af2f-192435462f85+Halloween2024Creatures", 2386455248},
+        {5, "@playerthatissmart", "rbxpost://eb596393-7ae6-49fc-9615-67afbd692b20+Halloween2024Creatures", 4943293626},
+        {6, "@mind1793", "rbxpost://e1f151c0-67c1-4751-8b4e-58f15201bdf8+Halloween2024Creatures", 134287475},
+        {7, "@theblackandwhiteguy3", "rbxpost://da81fccd-13dd-4a06-a31c-574f212513f3+Halloween2024Creatures", 1389644219},
+        {8, "@Papyrus1545", "rbxpost://d385a9da-ebed-4533-b147-c36b16c72ca6+Halloween2024Creatures", 2211400122},
+        {9, "@Wafflepiebanana", "rbxpost://bc387f60-ba9e-48ce-8ad0-c9c30da5b014+Halloween2024Creatures", 73696727},
+        {10, "@rikbuna", "rbxpost://b2b08777-8f49-4b10-8ac3-9d2d08c44576+Halloween2024Creatures", 2874915650},
+        {11, "@mesorte", "rbxpost://b1182703-75f7-4c4c-ba5c-fc6ab55b29e1+Halloween2024Creatures", 289245888},
+        {12, "@azzypu", "rbxpost://83c65400-f6b9-4997-957d-3835a2d81317+Halloween2024Creatures", 11124462},
+        {13, "@Matflex017", "rbxpost://799b9b9f-9278-4af1-8d31-076bae73388e+Halloween2024Creatures", 2704673775},
+        {14, "@Tope462", "rbxpost://75637edf-5657-4754-9aa8-a0e59b3a152c+Halloween2024Creatures", 4845150558},
+        {15, "@holaquease234589e", "rbxpost://733c02a2-383e-42bc-82b6-f664a31a7fd2+Halloween2024Creatures", 1446791646},
+        {16, "@Skbee11", "rbxpost://448b0ece-9c45-4907-8489-5f8fb4f7c367+Halloween2024Creatures", 2308436899},
+        {17, "@gghghk6", "rbxpost://42d3fa1f-5374-460b-8639-4429d1aed19a+Halloween2024Creatures", 4576351522},
+        {18, "@lequvs", "rbxpost://37652d66-5533-436d-be7c-6c5e260d925d+Halloween2024Creatures", 592190418},
+        {19, "@LofiFoxy", "rbxpost://c917dda3-0d60-47b4-940c-4ef0c27342f4+Halloween2024Creatures", 2898372753},
+        {20, "@Ello_alias", "rbxpost://c8d08f81-f857-4730-8a0a-d9b8ea703b22+Halloween2024Creatures", 3075776441},
+        {21, "@Alxnpzz", "rbxpost://b635e446-b6cd-415a-925a-e8d6c882b9cd+Halloween2024Creatures", 4245555670},
+        {22, "@moomsrule", "rbxpost://affc78da-db43-4237-ba92-ec3ea934fdaa+Halloween2024Creatures", 2839229298},
+        {23, "@Fundog12780", "rbxpost://8c302b54-bebd-4ccc-b551-ec6ac0c68cbd+Halloween2024Creatures", 1521574087},
+        {24, "@BillyThePuppet13", "rbxpost://89dab240-8794-44a3-835e-5e9eb20e97c2+Halloween2024Creatures", 1012183570},
+        {25, "@miojo_moleKa", "rbxpost://8742c86e-2974-47d4-94fd-ec97ae8f2799+Halloween2024Creatures", 5717184450},
+        {26, "@yoyothe15thofapril", "rbxpost://678fdc05-1139-4d5b-ae0c-9253c7e6fae5+Halloween2024Creatures", 5787810034},
+        {27, "@crumblyjumbly", "rbxpost://63f11e80-a15e-4770-9149-6d81d790e793+Halloween2024Creatures", 2200560044},
+        {28, "@EnfantsBLH", "rbxpost://0a6469ee-231e-4f04-a50f-64db7fcc6e37+Halloween2024Creatures", 931846299},
+        {29, "@Dreamslime4648", "rbxpost://e3142a3e-8701-450d-bcc9-ca112458e2dc+Halloween2024Creatures", 2435336562},
+        {30, "@dududa332", "rbxpost://c0cabaed-d859-4181-a2ab-0ffd0d1eb12d+Halloween2024Creatures", 5337189255},
+        {31, "@Maxi196314", "rbxpost://b44d5de5-292d-4791-807d-984f9015318e+Halloween2024Creatures", 7453623473},
+        {32, "@xu1lv", "rbxpost://80193f78-0e5f-49d4-a864-121135ab315f+Halloween2024Creatures", 1429861512},
+        {33, "@udyfuds", "rbxpost://4e6121dc-2b9b-49e2-b9fb-a7a164430fa8+Halloween2024Creatures", 7469536730},
+        {34, "@MainorUwUXd", "rbxpost://29e363c2-5f2e-4484-8e94-d3d4bd2216b5+Halloween2024Creatures", 5034816379},
+        {35, "@Zaki_Noshi", "rbxpost://1d4466a6-93d6-419f-9ae5-fdaba910c87a+Halloween2024Creatures", 1519072461},
+        {36, "@zengi2525", "rbxpost://1b39def1-b60c-4489-8932-e49b4c1c03fd+Halloween2024Creatures", 256796073},
+        {37, "@cousin9487", "rbxpost://ba4e52af-a7e5-40d9-add9-0ace33d59528+Halloween2024Creatures", 2243736721},
+        {38, "@Playcookieplayfansie", "rbxpost://b66036bc-5a0b-45fc-811d-583ba8c2b903+Halloween2024Creatures", 2772202563},
+        {39, "@RobloxDramaPractice", "rbxpost://92e0b364-ec40-4ecd-b3c7-c79cc02e577b+Halloween2024Creatures", 7186603982},
+        {40, "@Lucia_22120", "rbxpost://9182e4b4-4835-4e7f-9ae1-80b5f329d099+Halloween2024Creatures", 1526280432},
+        {41, "@FxdedSkxez", "rbxpost://85652d89-086f-4833-9200-4f4299dc349b+Halloween2024Creatures", 436828076},
+        {42, "@mhg8234", "rbxpost://7191ac97-3e8d-40e2-a6c5-467f9451f731+Halloween2024Creatures", 2710724357},
+        {43, "@pvp_VRZO4", "rbxpost://6790aab4-73d1-4535-ada8-6e6999f6cef7+Halloween2024Creatures", 663003397},
+        {44, "@Pinkprincess6906", "rbxpost://50a071cf-c21e-48dc-b445-1a6391753f1e+Halloween2024Creatures", 7332171360},
+        {45, "@GroovyKayke", "rbxpost://4c0af5cf-2e1c-4c54-96d2-663ad4eb267a+Halloween2024Creatures", 202252792},
+        {46, "@shafycheema", "rbxpost://3722cc8d-740d-45ba-8531-61796c754774+Halloween2024Creatures", 157336851},
+        {47, "@akijoca", "rbxpost://2f34904b-71c5-4aae-9262-859e89c65afb+Halloween2024Creatures", 2231606444},
+        {48, "@Jamespro2311", "rbxpost://f47d2a57-f76b-420f-b8d7-b811f7fdd8c2+Halloween2024Creatures", 3170638234},
+        {49, "@moramme", "rbxpost://cfb28ebc-ca80-4ec2-8239-ae4a4da7e986+Halloween2024Creatures", 885704924},
+        {50, "@Yolcybri", "rbxpost://c8cd55dc-e874-4e0d-a963-bc2cc0410e30+Halloween2024Creatures", 3190222070}
+    }
+}
+p=""
+function get(board,title)
+p=p.."[\""..title.."\"]={"
+for i = 1,50 do
+if game.Players.LocalPlayer.PlayerGui.Leaderboards[board].Frame:FindFirstChild("S"..i) then
+x=game.Players.LocalPlayer.PlayerGui.Leaderboards[board].Frame["S"..i]
+id="" dd,d2=string.find(x.Creator.Image,"%d+") if dd~=nil then
+id=tonumber(string.sub(x.Creator.Image,dd,d2)) end
+p=p.."{"..i..",\""..x.Username.Text.."\",\""..x.Photo.Image.."\","..id.."},"
+end p=p.."},\n"
+end end
+get("LeaderboardCreatures","Creatures")
+get("LeaderboardCute","Cute")
+get("LeaderboardVillains","Villains")
+get("LeaderboardOrange","Orange")
+setclipboard(p:sub(1,16000)) print("Done")
+
+for i,v in pairs(game.Players.LocalPlayer.PlayerGui.Leaderboards:GetChildren()) do print(i,v.ClassName,v.Name) end
