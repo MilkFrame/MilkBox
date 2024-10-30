4ec5d69ac23b2b82bf33e8db69fdd81b2776c418
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-31 01:26:08 +1100

    Create CreatureBoosts.lua

---
+++ GameData/Creatures of Sonaria [5233782396]/Creatures/CreatureBoosts.lua
@@ -0,0 +1,18 @@
+data = {
+    ["Tikit"] = {
+        ["Color"] = Color3.fromRGB(27, 129, 218),
+        ["OnChangedSignal"] = {["_argCount"] = 1, ["_argData"] = {"Magnacetus"}, ["_bindableEvent"] = "Event"}
+    },
+    ["Mutation"] = {
+        ["Color"] = Color3.fromRGB(218, 43, 210),
+        ["OnChangedSignal"] = {["_argCount"] = 1, ["_argData"] = {"Chrysos"}, ["_bindableEvent"] = "Event"}
+    },
+    ["Stat"] = {
+        ["Color"] = Color3.fromRGB(255, 255, 24),
+        ["OnChangedSignal"] = {["_argCount"] = 1, ["_argData"] = {"Shiyaro"}, ["_bindableEvent"] = "Event"}
+    },
+    ["Growth"] = {
+        ["Color"] = Color3.fromRGB(218, 126, 60),
+        ["OnChangedSignal"] = {["_argCount"] = 1, ["_argData"] = {"Momola"}, ["_bindableEvent"] = "Event"}
+    }
+}
