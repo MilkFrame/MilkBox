31b554abe4f3a068c96b8cb71b81437e89c75a3e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-31 01:19:24 +1100

    Create Regions.lua

---
+++ GameData/Creatures of Sonaria [5233782396]/Regions.lua
@@ -0,0 +1,155 @@
+data = {
+    ["Jungle"] = {
+        ["RegionType"] = "Land",
+        ["Image"] = "rbxassetid://13343202192",
+        ["Name"] = "Jungle",
+        ["Color"] = Color3.fromRGB(2, 129, 0),
+        ["RegionPosition"] = Vector3.new(2235.39990234375, -535, -1186.800048828125),
+        ["Index"] = "Jungle",
+        ["RegionSize"] = Vector3.new(1, 1600, 1600)
+    },
+    ["Central Rockfaces"] = {
+        ["RegionType"] = "Land",
+        ["Image"] = "rbxassetid://13343200903",
+        ["Name"] = "Central Rockfaces",
+        ["Color"] = Color3.fromRGB(229, 195, 161),
+        ["RegionPosition"] = Vector3.new(145.39990234375, -535, -86.800048828125),
+        ["Index"] = "Central Rockfaces",
+        ["RegionSize"] = Vector3.new(1, 1400, 1400)
+    },
+    ["Swamp Hill"] = {
+        ["RegionType"] = "Land",
+        ["Image"] = "rbxassetid://13343199937",
+        ["Name"] = "Swamp Hill",
+        ["Color"] = Color3.fromRGB(42, 80, 41),
+        ["RegionPosition"] = Vector3.new(655.39990234375, -535, -2346.800048828125),
+        ["Index"] = "Swamp Hill",
+        ["RegionSize"] = Vector3.new(1, 1300, 1300)
+    },
+    ["Desert"] = {
+        ["RegionType"] = "Land",
+        ["Image"] = "rbxassetid://13343201346",
+        ["Name"] = "Desert",
+        ["Color"] = Color3.fromRGB(255, 230, 155),
+        ["RegionPosition"] = Vector3.new(-1454.5999755859375, -590, 1203.199951171875),
+        ["Index"] = "Desert",
+        ["RegionSize"] = Vector3.new(1, 1800, 1800)
+    },
+    ["Grassy Shoal"] = {
+        ["RegionType"] = "Sea",
+        ["Image"] = "rbxassetid://13343201993",
+        ["Name"] = "Grassy Shoal",
+        ["Color"] = Color3.fromRGB(100, 207, 107),
+        ["RegionPosition"] = Vector3.new(-644.60009765625, -535, 2108.199951171875),
+        ["Index"] = "Grassy Shoal",
+        ["RegionSize"] = Vector3.new(1, 900, 900)
+    },
+    ["Rocky Drop"] = {
+        ["RegionType"] = "Sea",
+        ["Image"] = "rbxassetid://13343199413",
+        ["Name"] = "Rocky Drop",
+        ["Color"] = Color3.fromRGB(1, 1, 1),
+        ["RegionPosition"] = Vector3.new(1065.39990234375, -535, 578.199951171875),
+        ["Index"] = "Rocky Drop",
+        ["RegionSize"] = Vector3.new(1, 900, 900)
+    },
+    ["Mountains"] = {
+        ["RegionType"] = "Land",
+        ["Image"] = "rbxassetid://13343198621",
+        ["Name"] = "Mountains",
+        ["Color"] = Color3.fromRGB(176, 176, 176),
+        ["RegionPosition"] = Vector3.new(-1549.60009765625, -535, -801.800048828125),
+        ["Index"] = "Mountains",
+        ["RegionSize"] = Vector3.new(1, 1600, 1600)
+    },
+    ["Seaweed Depths"] = {
+        ["RegionType"] = "Sea",
+        ["Image"] = "rbxassetid://13343199680",
+        ["Name"] = "Seaweed Depths",
+        ["Color"] = Color3.fromRGB(46, 120, 70),
+        ["RegionPosition"] = Vector3.new(-109.60009765625, -535, 993.199951171875),
+        ["Index"] = "Seaweed Depths",
+        ["RegionSize"] = Vector3.new(1, 800, 800)
+    },
+    ["Forgotten Shores"] = {
+        ["RegionType"] = "Land",
+        ["Image"] = "rbxassetid://111333888546523",
+        ["Name"] = "Forgotten Shores",
+        ["Color"] = Color3.fromRGB(143, 206, 255),
+        ["RegionPosition"] = Vector3.new(-1172.9500732421875, -1053.60009765625, 2955.199951171875),
+        ["Index"] = "Forgotten Shores",
+        ["RegionSize"] = Vector3.new(1, 1700, 1700)
+    },
+    ["Coral Reef"] = {
+        ["RegionType"] = "Sea",
+        ["Image"] = "rbxassetid://13343201136",
+        ["Name"] = "Coral Reef",
+        ["Color"] = Color3.fromRGB(198, 65, 255),
+        ["RegionPosition"] = Vector3.new(1250.39990234375, -535, 1103.199951171875),
+        ["Index"] = "Coral Reef",
+        ["RegionSize"] = Vector3.new(1, 700, 700)
+    },
+    ["Flower Cove"] = {
+        ["RegionType"] = "Land",
+        ["Image"] = "rbxassetid://13343201725",
+        ["Name"] = "Flower Cove",
+        ["Color"] = Color3.fromRGB(218, 24, 89),
+        ["RegionPosition"] = Vector3.new(500.39990234375, -535, 2028.199951171875),
+        ["Index"] = "Flower Cove",
+        ["RegionSize"] = Vector3.new(1, 1800, 1800)
+    },
+    ["Redwoods"] = {
+        ["RegionType"] = "Land",
+        ["Image"] = "rbxassetid://13343199206",
+        ["Name"] = "Redwoods",
+        ["Color"] = Color3.fromRGB(216, 120, 76),
+        ["RegionPosition"] = Vector3.new(-24.60009765625, -535, -1451.800048828125),
+        ["Index"] = "Redwoods",
+        ["RegionSize"] = Vector3.new(1, 1300, 1300)
+    },
+    ["Pride Rocks"] = {
+        ["RegionType"] = "Land",
+        ["Image"] = "rbxassetid://13343203160",
+        ["Name"] = "Pride Rocks",
+        ["Color"] = Color3.fromRGB(172, 182, 146),
+        ["RegionPosition"] = Vector3.new(1625.39990234375, -535, -471.800048828125),
+        ["Index"] = "Pride Rocks",
+        ["RegionSize"] = Vector3.new(1, 1500, 1500)
+    },
+    ["Algae Sandbar"] = {
+        ["RegionType"] = "Sea",
+        ["Image"] = "rbxassetid://13343200637",
+        ["Name"] = "Algae Sandbar",
+        ["Color"] = Color3.fromRGB(12, 197, 175),
+        ["RegionPosition"] = Vector3.new(1065.39990234375, -535, -1436.800048828125),
+        ["Index"] = "Algae Sandbar",
+        ["RegionSize"] = Vector3.new(1, 900, 900)
+    },
+    ["Mesa"] = {
+        ["RegionType"] = "Land",
+        ["Image"] = "rbxassetid://13343202998",
+        ["Name"] = "Mesa",
+        ["Color"] = Color3.fromRGB(217, 164, 57),
+        ["RegionPosition"] = Vector3.new(-2249.60009765625, -535, 218.199951171875),
+        ["Index"] = "Mesa",
+        ["RegionSize"] = Vector3.new(1, 1300, 1300)
+    },
+    ["Tundra"] = {
+        ["RegionType"] = "Land",
+        ["Image"] = "rbxassetid://13343200410",
+        ["Name"] = "Tundra",
+        ["Color"] = Color3.fromRGB(208, 208, 208),
+        ["RegionPosition"] = Vector3.new(-894.60009765625, -535, -2011.800048828125),
+        ["Index"] = "Tundra",
+        ["RegionSize"] = Vector3.new(1, 1300, 1300)
+    },
+    ["Volcano Island"] = {
+        ["RegionType"] = "Land",
+        ["Image"] = "rbxassetid://13343200195",
+        ["Name"] = "Volcano Island",
+        ["Color"] = Color3.fromRGB(159, 0, 3),
+        ["RegionPosition"] = Vector3.new(2150.39990234375, -535, 1183.199951171875),
+        ["Index"] = "Volcano Island",
+        ["RegionSize"] = Vector3.new(1, 1300, 1300)
+    }
+}
