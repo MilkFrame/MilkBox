90b51e1a4fa583656e54303f2f6d031ecb7be16c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-31 01:29:01 +1100

    Create Environment.lua

---
+++ GameData/Creatures of Sonaria [5233782396]/Environment.lua
@@ -0,0 +1,63 @@
+data = {
+    ["Weather"] = {
+        ["Heatwave"] = {["UIGradient"] = "0 1 0.709804 0.564706 0 1 0.67451 0.133333 0 0 ", ["Name"] = "Heatwave"},
+        ["Warden's Nebula"] = {
+            ["ForcedDisplayName"] = "Warden's Nebula",
+            ["UIGradient"] = "0 0.643137 0.239216 1 0 0.5 0.756863 0.0313726 1 0 0.75 0 0.866667 1 0 1 0 1 0.882353 0 ",
+            ["Name"] = "Warden's Nebula"
+        },
+        ["Sunny"] = {["UIGradient"] = "0 0.94902 1 0.384314 0 1 0.815686 0.945098 1 0 ", ["Name"] = "Sunny"},
+        ["Rain"] = {["UIGradient"] = "0 0.721569 1 0.898039 0 1 0 0.45098 0.827451 0 ", ["Name"] = "Rain"},
+        ["Blizzard"] = {["UIGradient"] = "0 0.0313726 0.188235 0.337255 0 1 1 1 1 0 ", ["Name"] = "Blizzard"},
+        ["Wind Storm"] = {
+            ["ForcedDisplayName"] = "Wind Storm",
+            ["UIGradient"] = "0 0.666667 0.901961 1 0 1 0.568627 0.568627 0.568627 0 ",
+            ["Name"] = "Wind Storm"
+        },
+        ["Acid Rain"] = {
+            ["ForcedDisplayName"] = "Acid Rain",
+            ["UIGradient"] = "0 0.541176 1 0.415686 0 1 0.0392157 0.4 0.0313726 0 ",
+            ["Name"] = "Acid Rain"
+        },
+        ["Cloudy"] = {["UIGradient"] = "0 0.929412 0.917647 1 0 1 0.54902 0.827451 1 0 ", ["Name"] = "Cloudy"},
+        ["Thunderstorm"] = {
+            ["UIGradient"] = "0 0.721569 1 0.898039 0 1 0 0.45098 0.827451 0 ",
+            ["Name"] = "Thunderstorm"
+        },
+        ["Flowering"] = {["UIGradient"] = "0 0.984314 1 0.72549 0 1 1 0.513726 0.831373 0 ", ["Name"] = "Flowering"},
+        ["Blood Moon"] = {
+            ["ForcedDisplayName"] = "Blood Moon",
+            ["UIGradient"] = "0 1 0 0.0156863 0 1 0.74902 0.180392 0.192157 0 ",
+            ["Name"] = "Blood Moon"
+        },
+        ["Clear Skies"] = {
+            ["ForcedDisplayName"] = "Clear Skies",
+            ["UIGradient"] = "0 0.721569 1 0.898039 0 1 0.866667 0.721569 0 0 ",
+            ["Name"] = "Clear Skies"
+        },
+        ["Fog"] = {["UIGradient"] = "0 1 1 1 0 1 0.478431 0.478431 0.478431 0 ", ["Name"] = "Fog"},
+        ["Sunshower"] = {
+            ["UIGradient"] = "0 1 0.952941 0.584314 0 1 0.27451 0.541176 0.827451 0 ",
+            ["Name"] = "Sunshower"
+        },
+        ["Blue Moon"] = {
+            ["ForcedDisplayName"] = "Blue Moon",
+            ["UIGradient"] = "0 0.129412 0.25098 0.65098 0 1 0.423529 0.67451 1 0 ",
+            ["Name"] = "Blue Moon"
+        },
+        ["Snow"] = {["UIGradient"] = "0 0.721569 1 0.898039 0 1 1 1 1 0 ", ["Name"] = "Snow"}
+    },
+    ["Seasons"] = {
+        ["Mythic"] = {["UIGradient"] = "0 0.666667 0.262745 1 0 1 0.262745 0.964706 1 0 ", ["Name"] = "Mythic"},
+        ["Sakura"] = {["UIGradient"] = "0 0.988235 0.596078 1 0 1 0.756863 0 0 0 ", ["Name"] = "Sakura"},
+        ["Spring"] = {["UIGradient"] = "0 0.00784314 0.623529 0 0 1 0.576471 0.764706 0.866667 0 ", ["Name"] = "Spring"},
+        ["Winter"] = {["UIGradient"] = "0 1 1 1 0 1 0.67451 0.831373 0.866667 0 ", ["Name"] = "Winter"},
+        ["Drought"] = {["UIGradient"] = "0 1 1 1 0 1 1 0.670588 0.00784314 0 ", ["Name"] = "Drought"},
+        ["Fall"] = {["UIGradient"] = "0 0.784314 0.219608 0 0 1 0.866667 0.521569 0 0 ", ["Name"] = "Fall"},
+        ["Famine"] = {
+            ["UIGradient"] = "0 0.623529 0.623529 0.623529 0 1 0.67451 0.831373 0.866667 0 ",
+            ["Name"] = "Famine"
+        },
+        ["Summer"] = {["UIGradient"] = "0 1 1 1 0 1 0.847059 0.568627 0.00784314 0 ", ["Name"] = "Summer"}
+    }
+}
