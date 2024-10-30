6c177e7b3049c1feb46de240a200624447d8f4ea
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-31 01:30:07 +1100

    Create UpdateLogs.lua

---
+++ GameData/Creatures of Sonaria [5233782396]/UpdateLogs.lua
@@ -0,0 +1,394 @@
+data = {
+    ["30"] = {
+        ["ReleaseTime"] = 1729899206,
+        ["Image"] = "rbxassetid://139709246692897",
+        ["Title"] = "Halloween Event PT.3, Phantejer arrives in Sonaria!",
+        ["FeaturedImage"] = "rbxassetid://88490625549912",
+        ["FeaturedDescription"] = "Obtain Phantejer by completing this weeks Halloween Event Creature Mission!",
+        ["Description"] = [[🎃 HALLOWEEN EVENT PT 3 RELEASED!
+⭐ Halloween Mega Maze released!
+💀 NEW CREATURE: Phantejer added to Halloween Event Missions!
+💚 Nindyr remake released!
+💢 NEW ABILITY: Cursed Sigil! Ward off your foes with this chaotic affliction!
+💥 NEW QOL: Public Packs! The Pack Menu now offers an option to open your pack to the public, or keep it private for friends!
+	]]
+    },
+    ["21"] = {
+        ["ReleaseTime"] = 1724454756,
+        ["Image"] = "rbxassetid://91523219998577",
+        ["Title"] = "Starlit crash lands in Sonaria!",
+        ["FeaturedImage"] = "rbxassetid://94652416351008",
+        ["FeaturedDescription"] = "Find this colorful meteor themed creature in the Celestial Gacha!",
+        ["Description"] = [[☄ New species Starlit, added to Celestial Gacha! 
+🕊 Halaqual remake added!
+🎵 Combat Music added!
+	]]
+    },
+    ["22"] = {
+        ["ReleaseTime"] = 1725059427,
+        ["Image"] = "rbxassetid://89961775001007",
+        ["Title"] = "Noxiouk arrives in Sonaria!",
+        ["FeaturedImage"] = "rbxassetid://136191876505993",
+        ["FeaturedDescription"] = "Find this decaying direwolf in the Master Gacha!",
+        ["Description"] = [[💀 New species Noxiouk, added to Master Gacha! 
+🌹 Moemoea remake added!
+💚 Toxic Trail Ability added! Leave a trail of toxic goop when under attack!
+🌧 Acid Rain Weather made more dangerous, and less frequent!
+🌊 Moisture Bar no longer depletes during Rain & Thunderstorm weather!
+	]]
+    },
+    ["27"] = {
+        ["ReleaseTime"] = 1728084839,
+        ["Image"] = "rbxassetid://116140372556067",
+        ["Title"] = "Zarrax arrives in Sonaria!",
+        ["FeaturedImage"] = "rbxassetid://97509583105889",
+        ["FeaturedDescription"] = "Obtain Zarrax by completing the Monthly Creature Mission!",
+        ["Description"] = [[💚 Zarrax added to Monthly Mission Reward!
+🧊 Euvias remake added!
+💫 Migration feature has released! Travel to marked regions on the minimap for buffs!
+🔨 Fixes to animation speeds & jaw placement on Gigantism/Dwarfism mutations!
+⭐ Halloween Mutations released for October!
+	]]
+    },
+    ["26"] = {
+        ["ReleaseTime"] = 1727484577,
+        ["Image"] = "rbxassetid://86357781421817",
+        ["Title"] = "Inkpujin arrives in Sonaria!",
+        ["FeaturedImage"] = "rbxassetid://91256867700660",
+        ["FeaturedDescription"] = "Obtain the Rift Guardian by completing all the Lore Event Missions!",
+        ["Description"] = [[⭐ LORE EVENT Part 2 released! Restore Inkpujin, the Rift Guardian!
+📜 New species Inkpujin unlockable by completing all the Lore Event Missions!
+🦋 Gloracus remake added!
+✨ Inkpujin Crafting Minigame added!
+🔨 Lore Event Parkour Obby Minigame added!
+📜 Lore Bundle updated in Premium Shop!
+	]]
+    },
+    ["25"] = {
+        ["ReleaseTime"] = 1726865452,
+        ["Image"] = "rbxassetid://121579540169028",
+        ["Title"] = "Mufolium arrives in Sonaria!",
+        ["FeaturedImage"] = "rbxassetid://112072747711904",
+        ["FeaturedDescription"] = "Find this prehistoric titan in the Login Store, via your Streak!",
+        ["Description"] = [[📜 LORE EVENT has begun! Defeat the Darkness, Find an Ancient Species and Restore Sonaria!
+🌳 New species Mufolium, unlockable via the Daily Login streak & Tokens!
+👁 Kriprik remake added!
+💚 Heal Aura ability added! A radius of healing for all in your range!
+🔨 Map anomalies fixed, Minimap updated!
+📜 Lore Bundle added to Premium Shop!
+	]]
+    },
+    ["24"] = {
+        ["ReleaseTime"] = 1726247282,
+        ["Image"] = "rbxassetid://119111301085733",
+        ["Title"] = "Xeleviprex arrives in Sonaria!",
+        ["FeaturedImage"] = "rbxassetid://80329236041075",
+        ["FeaturedDescription"] = "Find this all terrain serpent as a Group Loyalty Reward!",
+        ["Description"] = [[🐍 New species Xeleviprex, unlockable by joining the Sonar Studios Group! 
+🌿 Yama'tu remake added!
+🏹 Thorn Trap ability added! Place a defensive spike trap to ward off enemies!
+📊 Healing Hunter particles now scale with size of creature model!
+🧬 Gigantism & Dwarfism Mutations added!
+🌍 Forgotten Shores Map Biome added!
+	]]
+    },
+    ["23"] = {
+        ["ReleaseTime"] = 1725641989,
+        ["Image"] = "rbxassetid://113055530531229",
+        ["Title"] = "Idrabark leaps into Sonaria!",
+        ["FeaturedImage"] = "rbxassetid://136123902348321",
+        ["FeaturedDescription"] = "Find this tree gremlin as a Monthly Mission Reward!",
+        ["Description"] = [[🌳 New species Idrabark, added to Monthly Mission! 
+💚 Venatrox remake added!
+🦴 First Strike Ability added to some creatures! Hit first and hit hard!
+📊 "K" Menu/Stat GUI in-game now displays various stat increases/decreases from ailment changes!
+🐟 Grab returns to some Aquatic Creatures!
+	]]
+    },
+    ["28"] = {
+        ["ReleaseTime"] = 1728687867,
+        ["Image"] = "rbxassetid://134967153661370",
+        ["Title"] = "Halloween Event PT.1, Felifaux arrives in Sonaria!",
+        ["FeaturedImage"] = "rbxassetid://86861423026968",
+        ["FeaturedDescription"] = "Obtain Felifaux by completing the Halloween Event Creature Mission!",
+        ["Description"] = [[🎃 HALLOWEEN EVENT HAS BEGUN
+💀 Felifaux added to Halloween Event Missions!
+🐲 Sarchias remake added!
+🗺 Minimap overhauled! New interactive interface & Grid coordinates!
+🐺 Werewolf Fang Minigame Released!
+⚰ Halloween Obby Minigame Released!
+🧹 Potion Ingredient Minigame Released!
+👚 Skin Making Contest Minigame Released!
+	]]
+    },
+    ["29"] = {
+        ["ReleaseTime"] = 1729312876,
+        ["Image"] = "rbxassetid://90340004186804",
+        ["Title"] = "Halloween Event PT.2, Nok'Tuun arrives in Sonaria!",
+        ["FeaturedImage"] = "rbxassetid://121815677616678",
+        ["FeaturedDescription"] = "Obtain Nok'Tuun by completing this weeks Halloween Event Creature Mission!",
+        ["Description"] = [[🎃 HALLOWEEN EVENT PT 2 RELEASED!
+⭐ Pumpkin King Boss & Arena released!
+💀 Nok'Tuun added to Halloween Event Missions!
+☢ Kehmador remake added!
+🎉 Pumpkin Dash Race Minigame Released!
+🧡 Spooky Wooky Event Bundle added to Premium Shop
+	]]
+    },
+    ["20"] = {
+        ["ReleaseTime"] = 1723822305,
+        ["Image"] = "rbxassetid://95066666191649",
+        ["Title"] = "Summer Paradise Event, PT 4!",
+        ["FeaturedImage"] = "rbxassetid://73910845563349",
+        ["FeaturedDescription"] = "Last week of Summer Paradise Event! + New Death Gacha Species, Aidoneiscus!",
+        ["Description"] = [[🌤 Summer Paradise Event, PT 4!
+💀 New Death Gacha Species, Aidoneiscus!
+🌿 Lerachu remake added!
+	]]
+    },
+    ["19"] = {
+        ["ReleaseTime"] = 1723207052,
+        ["Image"] = "rbxassetid://18861694280",
+        ["Title"] = "Summer Paradise Event, PT 3!",
+        ["FeaturedImage"] = "rbxassetid://18869079700",
+        ["FeaturedDescription"] = "Summer Paradise Event Continues + New Event Mission creature Geortharoc!",
+        ["Description"] = [[🌤 Summer Paradise Event, PT 3!
+🗻 New behemoth species, Geortharoc added to Summer Paradise Event Missions!
+🐟 Chamei remake added!
+	]]
+    },
+    ["18"] = {
+        ["ReleaseTime"] = 1722553153,
+        ["Image"] = "rbxassetid://18751358986",
+        ["Title"] = "Summer Paradise PT 2 + THE GAMES!",
+        ["FeaturedImage"] = "rbxassetid://18751355493",
+        ["FeaturedDescription"] = "THE GAMES Roblox Event begins in Sonaria! + New Monthly Mission creature Aqoxturumox!",
+        ["Description"] = [[🏆 THE GAMES, Roblox Event Begins!
+🌤 Summer Paradise Event, Part 2!
+🐋 Aqoxturumox added to Monthly Creature Mission!
+🐎 Koipise remake added!
+	]]
+    },
+    ["9"] = {
+        ["ReleaseTime"] = 1716589470,
+        ["Image"] = "rbxassetid://17600830180",
+        ["Title"] = "Land Sea Sky Event!",
+        ["FeaturedImage"] = "rbxassetid://17600801914",
+        ["FeaturedDescription"] = "Light or dark, the primordial forces fight this event! Join the Light or Dark side this LSS Event!",
+        ["Description"] = [[🏝 Land Sea Sky Event is here!
+👼 Ouratum added for light team side!
+🦀 Militrua remake added
+	]]
+    },
+    ["1"] = {
+        ["ReleaseTime"] = 1711746840,
+        ["Image"] = "rbxassetid://16944485048",
+        ["Title"] = "Easter Event, Part 1!",
+        ["FeaturedImage"] = "rbxassetid://16945933351",
+        ["FeaturedDescription"] = "Easter Event begins now, play two new minigames to earn Egg currency and buy new items!",
+        ["Description"] = [[🐰 Easter Event Part 1 added
+🍫 Egg Delivery Minigame added
+🥚 Egg Smash Minigame added
+💨 Sylvesqual added as Air Artifact Creature
+🐥 Grysallmog added as Easter Creature
+💫 Shro Redesign added
+	]]
+    },
+    ["15"] = {
+        ["ReleaseTime"] = 1720216833,
+        ["Image"] = "rbxassetid://18351283336",
+        ["Title"] = "Fireworks Event, Part 1!",
+        ["FeaturedImage"] = "rbxassetid://18351129173",
+        ["FeaturedDescription"] = "Fireworks Event returns & Reeox appears, the new Monthly Mission Creature!",
+        ["Description"] = [[🎆 Fireworks Event Part 1!
+👾 Reeox added as the new Monthly Mission Creature!
+🌵 Koryser remake added!
+	]]
+    },
+    ["3"] = {
+        ["ReleaseTime"] = 1712958881,
+        ["Image"] = "rbxassetid://17095403196",
+        ["Title"] = "Easter Event, Part 3!",
+        ["FeaturedImage"] = "rbxassetid://17119620658",
+        ["FeaturedDescription"] = "Easter part 3 brings a new minigame activity and a new Easter Mission Creature!",
+        ["Description"] = [[🥚 Easter Egg Dye Ingredients Minigame added to Swamp!
+🦋 Momola Redesign Added!
+🐰 Vorpalus added as Easter Mission Creature!
+🛍️ New materials, palettes, plushies and creatures added to Easter Event Shop!
+	]]
+    },
+    ["2"] = {
+        ["ReleaseTime"] = 1712329200,
+        ["Image"] = "rbxassetid://17024967609",
+        ["Title"] = "Easter Event, Part 2!",
+        ["FeaturedImage"] = "rbxassetid://17024843582",
+        ["FeaturedDescription"] = "Easter part 2 brings two new activities as well as the new monthly mission creature!",
+        ["Description"] = [[🐰 Easter sniffing quest added to central rocks
+🍫 Memory matching minigame added
+👽 Paru-Gama monthly creature added
+💪 Buff Eulopii redesign added
+📅 Update log added
+💫 Shro Redesign added
+🛍️ New palettes, plush, signature, and creatures added to event shop
+	]]
+    },
+    ["5"] = {
+        ["ReleaseTime"] = 1714168819,
+        ["Image"] = "rbxassetid://17296425056",
+        ["Title"] = "Meet Bolatra!",
+        ["FeaturedImage"] = "rbxassetid://17296163689",
+        ["FeaturedDescription"] = "The scary zombie mushroom, Bolatra, creeps its way to infect Sonaria! Find it in the Time store!",
+        ["Description"] = [[🍄 Bolatra added to Time store
+☁️ Pero Remake added
+🐥 Easter Event removed - all previously obtainable easter items are now unobtainable
+💎 Emerald Mutations added to nesting mutation pool
+💉 Fix made to negative ailment healing, as some negative ailments were not healing
+🦘 Fyranili and Mobuushiy Jump animation added
+🦷 Teeth on Shadow mutation now appear white (compatability for no-eye creatures)
+	]]
+    },
+    ["4"] = {
+        ["ReleaseTime"] = 1713564043,
+        ["Image"] = "rbxassetid://17209495153",
+        ["Title"] = "Easter Event, FINAL!",
+        ["FeaturedImage"] = "rbxassetid://17209459658",
+        ["FeaturedDescription"] = "Easter part 4 finalizes the shop and brings a new easter creature!",
+        ["Description"] = [[🐗 Buukon remodel
+🐏 Ushrumesh new easter creature added
+💓 Healing added as an alternative option to damage in region mission
+🔥 Search creatures by abilities and ailments
+🧬 Mutation/Glimmer boosted creature added
+🛍️ Plushie, palette, and new creatures added to event store
+	]]
+    },
+    ["7"] = {
+        ["ReleaseTime"] = 1715378274,
+        ["Image"] = "rbxassetid://17444710631",
+        ["Title"] = "Disaster Event!",
+        ["FeaturedImage"] = "rbxassetid://17449776585",
+        ["FeaturedDescription"] = "The disaster event begins this week featuring the blood moon creature, Noctla'Lune!",
+        ["Description"] = [[🌪 Disaster Event part 1 added
+🌑 Noctla'Lune added to Disaster Event
+🎨 New palettes and material for Disaster Event
+🐲 Doragonix Remake added
+🥚 Eggs now have a 10min decay timer
+🌦 Weather now cannot be changed when a Disaster is present
+	]]
+    },
+    ["6"] = {
+        ["ReleaseTime"] = 1714700081,
+        ["Image"] = "rbxassetid://17373430618",
+        ["Title"] = "Meet Bäruw!",
+        ["FeaturedImage"] = "rbxassetid://17373387414",
+        ["FeaturedDescription"] = "Bäruw the bear-trapper stalks in as the May Monthly Mission creature!",
+        ["Description"] = [[🦅 Ralokai remake added!
+🐂 Morinus animation touch-ups!
+🗺 Map Expansion by Volcano!
+✨ Glimmer Mutation can now be toggled!
+🔎 Search for creatures by their event or gacha!
+🎯 Additional missions added to Boosted gacha!
+	]]
+    },
+    ["14"] = {
+        ["ReleaseTime"] = 1719578813,
+        ["Image"] = "rbxassetid://18228311805",
+        ["Title"] = "Daily Login Revamp!",
+        ["FeaturedImage"] = "rbxassetid://18241029965",
+        ["FeaturedDescription"] = "New and improved Daily Login along with a new face, Galiryn, added to Daily Login rewards!",
+        ["Description"] = [[📅 New Daily Login!
+🐱 Galiryn added to Daily Login Rewards!
+🦌 Verklixin remake added!
+	]]
+    },
+    ["8"] = {
+        ["ReleaseTime"] = 1715987084,
+        ["Image"] = "rbxassetid://17524523519",
+        ["Title"] = "Disaster Event Final!",
+        ["FeaturedImage"] = "rbxassetid://17524526783",
+        ["FeaturedDescription"] = "The disaster rumbles into its final week with the arrival of the behemoth living volcano, Caldonterrus!",
+        ["Description"] = [[🌊 Disaster Event Final Week
+🌋 Caldonterrus added
+🔥 New Heatwave and Acid Rain weather events
+🧗‍ Climbing added
+🐉 Hushike Redesign
+🍖 Meat changes to an orange color when it is near rotten
+⛰ Expanded underground cave in Tundra
+	]]
+    },
+    ["16"] = {
+        ["ReleaseTime"] = 1720800207,
+        ["Image"] = "rbxassetid://18452831437",
+        ["Title"] = "Fireworks Event, Part 2!",
+        ["FeaturedImage"] = "rbxassetid://18452547439",
+        ["FeaturedDescription"] = "New Fireworks Event Creature Firakai, and Emphines Remake release this week!",
+        ["Description"] = [[🎆 Fireworks Event Part 2!
+🔥 Firakai added as the new Fireworks Event Creature!
+🐺 Emphines remake added!
+	]]
+    },
+    ["17"] = {
+        ["ReleaseTime"] = 1721393303,
+        ["Image"] = "rbxassetid://18548348514",
+        ["Title"] = "Meet Lactarim!",
+        ["FeaturedImage"] = "rbxassetid://18566538916",
+        ["FeaturedDescription"] = "New Masters Gacha Creature Lactarim, and Exterreri Remake release this week!",
+        ["Description"] = [[🍄 Lactarim added to the Masters Gacha!
+👁 Exterreri remake added!
+🎯 Bug fixes and small QoL added
+	]]
+    },
+    ["13"] = {
+        ["ReleaseTime"] = 1719012955,
+        ["Image"] = "rbxassetid://18139795389",
+        ["Title"] = "Meorlark pounces in!",
+        ["FeaturedImage"] = "rbxassetid://18140992439",
+        ["FeaturedDescription"] = "This shark cat swims in as a new Boosted Gacha creature!",
+        ["Description"] = [[🦈 Meorlark joins the Boosted Gacha!
+⚡ Taurolystris gets an electrifying remake!
+🩺 New healing trait for Healing Hunter creatures!
+🌌 Underwater portals now have a cooldown when used!
+🧸 Edits to Swan and Goldfish plushie added!
+🏃‍ Stamina regen trait also effects glide regen!
+	]]
+    },
+    ["12"] = {
+        ["ReleaseTime"] = 1718416524,
+        ["Image"] = "rbxassetid://17849008432",
+        ["Title"] = "Land Sea Sky FINAL!",
+        ["FeaturedImage"] = "rbxassetid://17859814481",
+        ["FeaturedDescription"] = "The last week of LSS and the arrival of the entity of light and dark, Noxulumen!",
+        ["Description"] = [[🏝 Land Sea Sky Event FINAL WEEK!
+☯ Noxulumen added to the LSS event!
+🥇 Master's Gacha addition with Heiboktoruk!
+🐋 The skywhale, Opralegion gets a remake!
+🎯 Bug fixes and small QoL added
+	]]
+    },
+    ["11"] = {
+        ["ReleaseTime"] = 1717807001,
+        ["Image"] = "rbxassetid://17761404382",
+        ["Title"] = "Land Sea Sky PT3!",
+        ["FeaturedImage"] = "rbxassetid://17762107764",
+        ["FeaturedDescription"] = "LSS continues! Soaring in from high above, Lotremum appears as June's monthly Mission creature!",
+        ["Description"] = [[🏝 Land Sea Sky Event continues!
+⚔ Earn Lotremum from Monthly Mission!
+🦘 Diesura Remake added!
+🐙 New Octroma premium plushie available in event store!
+🧸 New Elemental plushie available from event store!
+📜 QoL Changes added: Breakable snowshield, Audio indicator when being attacked, and no damage from Blizzard under shelter.
+	]]
+    },
+    ["10"] = {
+        ["ReleaseTime"] = 1717173020,
+        ["Image"] = "rbxassetid://17672373789",
+        ["Title"] = "Land Sea Sky PT2!",
+        ["FeaturedImage"] = "rbxassetid://17672393977",
+        ["FeaturedDescription"] = "The forces rage on as the battle of light and dark continues. Earn the new team exclusive creatures this week!",
+        ["Description"] = [[🏝 Land Sea Sky Event continues!
+🎯 More ways to earn points!
+🏁 Complete timed races for points!
+🐱 Irizah and Ouratum available!
+⚪ New Pearl Monthly Mutation!
+	]]
+    }
+}
