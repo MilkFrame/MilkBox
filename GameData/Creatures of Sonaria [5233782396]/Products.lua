562509ebad360ac08348ad2143d57e5def9bd557
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-31 01:19:10 +1100

    Create Products.lua

---
+++ GameData/Creatures of Sonaria [5233782396]/Products.lua
@@ -0,0 +1,136 @@
+data = {
+    ["Gamepasses"] = {
+        ["RecodeSupporter"] = {["ProductId"] = 216724088},
+        ["MyloPlushie"] = {
+            ["CatalogItem"] = true,
+            ["ProductId"] = {8261915237, 12735371457, 12735364246, 17105795216, 17105771156, 17105809170}
+        },
+        ["EigionKey"] = {["CatalogItem"] = true, ["ProductId"] = 14638406891}
+    },
+    ["Shooms"] = {
+        {["Amount"] = 120, ["Currency"] = "Shooms", ["ProductType"] = "Currency", ["ProductId"] = 1059602844},
+        {["Amount"] = 360, ["Currency"] = "Shooms", ["ProductType"] = "Currency", ["ProductId"] = 1059602980},
+        {["Amount"] = 720, ["Currency"] = "Shooms", ["ProductType"] = "Currency", ["ProductId"] = 1059603138},
+        {["Amount"] = 4000, ["Currency"] = "Shooms", ["ProductType"] = "Currency", ["ProductId"] = 1059603231},
+        {["Amount"] = 7000, ["Currency"] = "Shooms", ["ProductType"] = "Currency", ["ProductId"] = 1132473487},
+        {["Amount"] = 15500, ["Currency"] = "Shooms", ["ProductType"] = "Currency", ["ProductId"] = 1132473568}
+    },
+    ["Tikits"] = {
+        {["Amount"] = 1, ["Currency"] = "Tikits", ["ProductType"] = "Currency", ["ProductId"] = 1137165835},
+        {["Amount"] = 4, ["Currency"] = "Tikits", ["ProductType"] = "Currency", ["ProductId"] = 1137165880},
+        {["Amount"] = 7, ["Currency"] = "Tikits", ["ProductType"] = "Currency", ["ProductId"] = 1137165928},
+        {["Amount"] = 15, ["Currency"] = "Tikits", ["ProductType"] = "Currency", ["ProductId"] = 1137165970},
+        {["Amount"] = 32, ["Currency"] = "Tikits", ["ProductType"] = "Currency", ["ProductId"] = 1137166048}
+    },
+    ["RobuxCreatures"] = {
+        ["Adharcaiin"] = {["ProductType"] = "Creature", ["Name"] = "Adharcaiin", ["ProductId"] = 1319949115},
+        ["Fleuralis"] = {["ProductType"] = "Creature", ["Name"] = "Fleuralis", ["ProductId"] = 1319949114},
+        ["Opralegion"] = {["ProductType"] = "Creature", ["Name"] = "Opralegion", ["ProductId"] = 1269606928},
+        ["Ghibli"] = {["ProductType"] = "Creature", ["Name"] = "Ghibli", ["ProductId"] = 1160868341},
+        ["Altulis"] = {["ProductType"] = "Creature", ["Name"] = "Altulis", ["ProductId"] = 1649692567},
+        ["Zoocuu"] = {["ProductType"] = "Creature", ["Name"] = "Zoocuu", ["ProductId"] = 1319949116},
+        ["Anutill"] = {["ProductType"] = "Creature", ["Name"] = "Anutill", ["ProductId"] = 1382167395},
+        ["Arkmetta"] = {["ProductType"] = "Creature", ["Name"] = "Arkmetta", ["ProductId"] = 1759955412},
+        ["Bazelii"] = {["ProductType"] = "Creature", ["Name"] = "Bazelii", ["ProductId"] = 1326265929},
+        ["Sar'Hingaro"] = {["ProductType"] = "Creature", ["Name"] = "Sar'Hingaro", ["ProductId"] = 1195075162},
+        ["Rukig"] = {["ProductType"] = "Creature", ["Name"] = "Rukig", ["ProductId"] = 1319949113},
+        ["Ura"] = {["ProductType"] = "Creature", ["Name"] = "Ura", ["ProductId"] = 1193479368},
+        ["Banishii"] = {["ProductType"] = "Creature", ["Name"] = "Banishii", ["ProductId"] = 1323043857},
+        ["Ani"] = {["ProductType"] = "Creature", ["Name"] = "Ani", ["ProductId"] = 1137185864},
+        ["Trutholbyeva"] = {["ProductType"] = "Creature", ["Name"] = "Trutholbyeva", ["ProductId"] = 1141937235}
+    },
+    ["SlotUpgrades"] = {
+        {["Price"] = 100, ["Currency"] = "Shooms", ["Slot"] = 3},
+        {["Price"] = 750, ["Currency"] = "Shooms", ["Slot"] = 4},
+        {["Slot"] = 5, ["ProductType"] = "Slots", ["ProductId"] = 1083156154},
+        {["Price"] = 1000, ["Currency"] = "Shooms", ["Slot"] = 6},
+        {["Slot"] = 7, ["ProductType"] = "Slots", ["ProductId"] = 1262327683},
+        {["Slot"] = 8, ["ProductType"] = "Slots", ["ProductId"] = 1262327680},
+        {["Slot"] = 9, ["ProductType"] = "Slots", ["ProductId"] = 1262327682}
+    },
+    ["DeveloperCreatures"] = {
+        ["Dowatt"] = {
+            ["ImageOffset"] = "{-0.200000003, 0}, {-0.349999994, 0}",
+            ["Description"] = "Greetings! I'm Late, a Senior Programmer for Creature of Sonaria. My favorite creatures are quick and nimble ones!",
+            ["Color"] = Color3.fromRGB(250, 231, 19),
+            ["UserId"] = 6891908,
+            ["Role"] = "Programmer",
+            ["Name"] = "Dowatt",
+            ["ProductId"] = 1555824125,
+            ["ProductType"] = "Creature",
+            ["Font"] = Enum.Font.LuckiestGuy,
+            ["TextStroke"] = true,
+            ["Image"] = "rbxassetid://13703830711"
+        },
+        ["Qurugosk"] = {
+            ["Description"] = "Hi! I'm Alertcoderf, the Lead Programmer of Creatures of Sonaria. My favorite creatures are scary carnivores!",
+            ["Color"] = Color3.fromRGB(0, 166, 255),
+            ["UserId"] = 7764425,
+            ["Image"] = "rbxassetid://6016897527",
+            ["Name"] = "Qurugosk",
+            ["ProductId"] = 1122416207,
+            ["ProductType"] = "Creature",
+            ["Role"] = "Programmer",
+            ["Font"] = Enum.Font.Bangers
+        },
+        ["Nyctoxon"] = {
+            ["Description"] = "Hey! I'm Jaxon/Jaxonbirb! I am a 3d modeler/concept artist. My favorite creatures are big brutes.",
+            ["Color"] = Color3.fromRGB(132, 215, 238),
+            ["UserId"] = 61052512,
+            ["ProductType"] = "Creature",
+            ["Role"] = "Modeler/Artist",
+            ["Name"] = "Nyctoxon",
+            ["ProductId"] = 1179007136,
+            ["Font"] = Enum.Font.Bangers,
+            ["UpperCase"] = true,
+            ["TextStroke"] = true,
+            ["Image"] = "rbxassetid://6893184921"
+        },
+        ["Tohukos"] = {
+            ["ImageOffset"] = "{-0.200000003, 0}, {0.0500000007, 0}",
+            ["Description"] = "Hello, I’m Stood! I do concept art/creature design. I love creative and strange looking creatures.",
+            ["Color"] = Color3.fromRGB(221, 204, 164),
+            ["UserId"] = 293768367,
+            ["ProductType"] = "Creature",
+            ["Role"] = "Concept Artist",
+            ["Name"] = "Tohukos",
+            ["ProductId"] = 1240908348,
+            ["Font"] = Enum.Font.PermanentMarker,
+            ["UpperCase"] = true,
+            ["TextStroke"] = true,
+            ["Image"] = "rbxassetid://8938760264"
+        },
+        ["Lucensia"] = {
+            ["ImageOffset"] = "{-0.200000003, 0}, {0, 0}",
+            ["Description"] = "Sup! I'm BlckBrry, I'm a 3d animator and am obsessed with dragons and crazy fantasy creatures!",
+            ["Color"] = Color3.fromRGB(164, 94, 233),
+            ["UserId"] = 983971750,
+            ["Role"] = "Animator",
+            ["Name"] = "Lucensia",
+            ["ProductId"] = 1630852727,
+            ["ProductType"] = "Creature",
+            ["Font"] = Enum.Font.LuckiestGuy,
+            ["TextStroke"] = true,
+            ["Image"] = "rbxassetid://14630204088"
+        },
+        ["Archalium"] = {
+            ["ImageOffset"] = "{-0.200000003, 0}, {0.0500000007, 0}",
+            ["Description"] = "Hi! I'm Erythia, co-creator/director of Creatures of Sonaria. I do 3d modeling, map design, and animation!",
+            ["Color"] = Color3.fromRGB(243, 174, 255),
+            ["UserId"] = 51645737,
+            ["ProductType"] = "Creature",
+            ["Role"] = "Designer",
+            ["Name"] = "Archalium",
+            ["ProductId"] = 1230499661,
+            ["Font"] = Enum.Font.Antique,
+            ["UpperCase"] = true,
+            ["TextStroke"] = true,
+            ["Image"] = "rbxassetid://15310814763"
+        }
+    },
+    ["Other"] = {
+        ["RebuyMissionCreature"] = {["ProductType"] = "RebuyMissionCreature", ["ProductId"] = 1756103207},
+        ["TutorialBundle"] = {["ProductType"] = "TutorialBundle", ["ProductId"] = 1628191293},
+        ["RestoreCalendarStreak"] = {["ProductType"] = "RestoreCalendarStreak", ["ProductId"] = 1697512967}
+    }
+}
