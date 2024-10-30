f9f683d1b68398cec556c8229b6ef0355c86a205
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-31 01:24:54 +1100

    Create DietTypes.lua

---
+++ GameData/Creatures of Sonaria [5233782396]/Creatures/DietTypes.lua
@@ -0,0 +1,31 @@
+data = {
+    ["Omnivore"] = {
+        ["Color"] = Color3.fromRGB(245, 205, 48),
+        ["Image"] = "rbxassetid://5486393302",
+        ["Name"] = "Omnivore"
+    },
+    ["Photocarni"] = {
+        ["Image"] = "rbxassetid://5486393083",
+        ["Name"] = "Photocarni",
+        ["IsInstanceValue"] = true,
+        ["Color"] = Color3.fromRGB(255, 255, 127),
+        ["ForcedDisplayName"] = "Photo-carni"
+    },
+    ["Photovore"] = {
+        ["Image"] = "rbxassetid://5486393202",
+        ["Name"] = "Photovore",
+        ["IsInstanceValue"] = true,
+        ["Color"] = Color3.fromRGB(255, 255, 127),
+        ["ForcedDisplayName"] = "Photo-vore"
+    },
+    ["Herbivore"] = {
+        ["Color"] = Color3.fromRGB(135, 255, 102),
+        ["Image"] = "rbxassetid://5486393202",
+        ["Name"] = "Herbivore"
+    },
+    ["Carnivore"] = {
+        ["Color"] = Color3.fromRGB(255, 88, 88),
+        ["Image"] = "rbxassetid://5486393083",
+        ["Name"] = "Carnivore"
+    }
+}
