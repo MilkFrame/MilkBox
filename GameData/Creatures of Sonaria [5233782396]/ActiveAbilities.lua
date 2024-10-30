7e3581539f52eb13986fca3a344f40806960ac3f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-31 01:28:06 +1100

    Create ActiveAbilities.lua

---
+++ GameData/Creatures of Sonaria [5233782396]/ActiveAbilities.lua
@@ -0,0 +1,363 @@
+data = {
+    ["DrowsyArea"] = {
+        ["Image"] = "rbxassetid://12735260012",
+        ["Description"] = "Causes those within your radius to have the drowsy ailment, which causes bite slowness",
+        ["Range"] = 150,
+        ["OnActivated"] = "function: 0x33cd1e01230207ec",
+        ["Name"] = "DrowsyArea",
+        ["Cooldown"] = 60,
+        ["DataValueName"] = "DrowsyArea"
+    },
+    ["ThornTrap"] = {
+        ["Image"] = "rbxassetid://12735260252",
+        ["Description"] = "Place a trap that causes those ensnared to bleed and freeze",
+        ["OnActivated"] = "function: 0x8fd0f05a368edafc",
+        ["Cooldown"] = 35,
+        ["Name"] = "ThornTrap"
+    },
+    ["Latch"] = {
+        ["Toggleable"] = true,
+        ["Image"] = "rbxassetid://12735260115",
+        ["Name"] = "Latch",
+        ["Description"] = "Latch onto another player when they are near your face for 1+ seconds",
+        ["OnDeactivated"] = "function: 0x3f2f7334028c750c",
+        ["NoParticles"] = true,
+        ["OnActivated"] = "function: 0x76b35e71bc673c6c",
+        ["CanUse"] = "function: 0xcd94b2cb2e42eacc"
+    },
+    ["WaterGale"] = {
+        ["Image"] = "rbxassetid://12735260252",
+        ["Description"] = "A ranged ability which impacts those in range by slowing them down",
+        ["Range"] = 150,
+        ["OnActivated"] = "function: 0x1514a0ca4fc3962c",
+        ["Cooldown"] = 90,
+        ["Name"] = "WaterGale"
+    },
+    ["Burrower"] = {
+        ["Image"] = "rbxassetid://14640005852",
+        ["Description"] = "Place burrows and teleports to undergound burrows in the world",
+        ["OnActivated"] = "function: 0x6badc521edbc952c",
+        ["Cooldown"] = 30,
+        ["Name"] = "Burrower"
+    },
+    ["SpeedBlitz"] = {
+        ["Image"] = "rbxassetid://12735260252",
+        ["Description"] = "An ability which requires stamina and increases your speed and stamina regeneration",
+        ["Name"] = "SpeedBlitz",
+        ["OnActivated"] = "function: 0x4f733f026f07622c",
+        ["Cooldown"] = 90,
+        ["CanUse"] = "function: 0x4192e30f5e3334bc"
+    },
+    ["Fortify"] = {
+        ["Image"] = "rbxassetid://12859524677",
+        ["Description"] = "An ability which clears ailments and grants ailment resistance to the user, also increasing weight",
+        ["OnActivated"] = "function: 0x00df90a7639bb76c",
+        ["Cooldown"] = 90,
+        ["Name"] = "Fortify"
+    },
+    ["Adrenaline"] = {
+        ["Image"] = "rbxassetid://12735260252",
+        ["Description"] = "Stops healing in trade for increased speed and damage",
+        ["OnActivated"] = "function: 0x0a9591ed057de14c",
+        ["Cooldown"] = 90,
+        ["Name"] = "Adrenaline"
+    },
+    ["HealingPulse"] = {
+        ["Image"] = "rbxassetid://12859442957",
+        ["Description"] = "Applies a healing ailment to creatures around you, and yourself",
+        ["Range"] = 175,
+        ["OnActivated"] = "function: 0xd3e3441ab1b8b19c",
+        ["Cooldown"] = 90,
+        ["Name"] = "HealingPulse"
+    },
+    ["Ambush"] = {
+        ["Image"] = "rbxassetid://12788381864",
+        ["Description"] = "Experience a burst of fast sprinting at the cost of faster consumed stamina",
+        ["Name"] = "Ambush"
+    },
+    ["Jump"] = {
+        ["NoAction"] = true,
+        ["ForcedDisplayName"] = "Jump",
+        ["Description"] = "Leap forwards, upwards, or backwards at the cost of stamina",
+        ["Name"] = "Jump",
+        ["DataValueName"] = "JumpData",
+        ["NoData"] = true
+    },
+    ["EscapeArea"] = {
+        ["Image"] = "rbxassetid://12735260252",
+        ["Description"] = "At the cost of health, gives yourself a speed boost to escape the area",
+        ["Name"] = "EscapeArea",
+        ["OnActivated"] = "function: 0x647b627a52a5a69c",
+        ["Cooldown"] = 75,
+        ["CanUse"] = "function: 0x0d65d1a1714b074c"
+    },
+    ["DazzlingFlash"] = {
+        ["Image"] = "rbxassetid://12735260012",
+        ["Description"] = "A ranged ability which impacts those in range by causing erratic controls and impacting vision",
+        ["Range"] = 150,
+        ["OnActivated"] = "function: 0x075c41978383646c",
+        ["Cooldown"] = 70,
+        ["Name"] = "DazzlingFlash"
+    },
+    ["CauseFear"] = {
+        ["Image"] = "rbxassetid://12859582084",
+        ["Description"] = "A ranged ability which impacts those in range by reducing damage but increasing speed",
+        ["Range"] = 150,
+        ["OnActivated"] = "function: 0x6265e22357c2fbac",
+        ["Cooldown"] = 120,
+        ["Name"] = "CauseFear"
+    },
+    ["UnbridledRage"] = {
+        ["Image"] = "rbxassetid://12735260252",
+        ["Description"] = "An ability which requires stamina to raise damage but prevents stamina regeneration",
+        ["Name"] = "UnbridledRage",
+        ["OnActivated"] = "function: 0xfcb9945d93dedbec",
+        ["Cooldown"] = 120,
+        ["CanUse"] = "function: 0x9ffc281fb986190c"
+    },
+    ["TotemPlacer"] = {
+        ["ForcedDisplayName"] = "Totem",
+        ["Description"] = "A placeable totem that applies an ailment to nearby players every few seconds",
+        ["OnActivated"] = "function: 0x8d9e6bf74b2acc1c",
+        ["Name"] = "TotemPlacer",
+        ["MinAge"] = 66,
+        ["Cooldown"] = 120,
+        ["Image"] = "rbxassetid://12735260252"
+    },
+    ["AreaWaterRestore"] = {
+        ["Image"] = "rbxassetid://12859442957",
+        ["Description"] = "A ranged ability which refreshes water sources in range",
+        ["OnActivated"] = "function: 0xfda10250adcf908c",
+        ["Cooldown"] = 600,
+        ["Name"] = "AreaWaterRestore"
+    },
+    ["AreaFoodRestore"] = {
+        ["Image"] = "rbxassetid://12859442957",
+        ["Description"] = "A ranged ability which refreshes food sources in range",
+        ["OnActivated"] = "function: 0xd40a9c41e7e4d42c",
+        ["Cooldown"] = 600,
+        ["Name"] = "AreaFoodRestore"
+    },
+    ["Radiation"] = {
+        ["Toggleable"] = true,
+        ["Image"] = "rbxassetid://12735260012",
+        ["Description"] = "A ranged toggleable ability that applies corrosion to creatures in your vicinity",
+        ["Range"] = 100,
+        ["OnTick"] = "function: 0x60f679d593f9faec",
+        ["Cooldown"] = 10,
+        ["Name"] = "Radiation"
+    },
+    ["Grab"] = {
+        ["Toggleable"] = true,
+        ["OnActivated"] = "function: 0x5bdcd49ee9015b8c",
+        ["NoParticles"] = true,
+        ["CanUse"] = "function: 0x1a200c2b76a000ec",
+        ["Image"] = "rbxassetid://12735260115",
+        ["MinAge"] = 33,
+        ["Description"] = "Grab another player while they are near your face for 1+ seconds",
+        ["OnDeactivated"] = "function: 0xcaf9e5e4409e93ac",
+        ["Cooldown"] = 10,
+        ["Name"] = "Grab"
+    },
+    ["WardensRage"] = {
+        ["GetRageValue"] = "function: 0x368d041c125fd1cc",
+        ["Description"] = "",
+        ["OnTick"] = "function: 0x9dfbbefdd3688f2c",
+        ["OnDamage"] = "function: 0xc2577dec7f59a29c",
+        ["Image"] = "rbxassetid://12735260012",
+        ["UpdateAilment"] = "function: 0x94084b6213c1396c",
+        ["MinAge"] = 66,
+        ["OnActivated"] = "function: 0x585358dcfe05af2c",
+        ["Toggleable"] = true,
+        ["OnDeactivated"] = "function: 0x086d07f0875592ec",
+        ["Cooldown"] = 30,
+        ["Name"] = "WardensRage"
+    },
+    ["TailDamage"] = {
+        ["NoAction"] = true,
+        ["ForcedDisplayName"] = "Tail Whip",
+        ["Description"] = "Face your tail to whip behind you",
+        ["Name"] = "TailDamage",
+        ["Image"] = "rbxassetid://6929057623",
+        ["NoData"] = true,
+        ["ValueScalesWithAge"] = true
+    },
+    ["Earthquake"] = {
+        ["Image"] = "rbxassetid://12735260012",
+        ["Description"] = "A ranged ability which applies injury to those in range",
+        ["Range"] = 100,
+        ["OnActivated"] = "function: 0x6ecd5c951f3ddd2c",
+        ["Cooldown"] = 150,
+        ["Name"] = "Earthquake"
+    },
+    ["HealAura"] = {
+        ["Toggleable"] = true,
+        ["Image"] = "rbxassetid://12859442957",
+        ["Description"] = "Toggle heal aura on and off, healing those around you every tick",
+        ["OnTick"] = "function: 0x8d815b351115ed8c",
+        ["Cooldown"] = 5,
+        ["Name"] = "HealAura"
+    },
+    ["Dart"] = {
+        ["NoAction"] = true,
+        ["ForcedDisplayName"] = "Dart",
+        ["Description"] = "Gives your creature a burst of movement in the water at the cost of stamina",
+        ["Name"] = "Dart",
+        ["DataValueName"] = "DartData",
+        ["NoData"] = true,
+        ["ValueScalesWithAge"] = true
+    },
+    ["EggStealer"] = {
+        ["NoAction"] = true,
+        ["NoData"] = true,
+        ["MinAge"] = 66,
+        ["Name"] = "EggStealer",
+        ["Description"] = "Allows the user to steal eggs from other player's nests as a food source"
+    },
+    ["CursedSigil"] = {
+        ["Image"] = "rbxassetid://12735260012",
+        ["Description"] = "A ranged ability which applies tunnel vision and bad omen to those in range",
+        ["Range"] = 100,
+        ["OnActivated"] = "function: 0xc22b852e3a0448ec",
+        ["Name"] = "CursedSigil",
+        ["Cooldown"] = 85,
+        ["MinAge"] = 66
+    },
+    ["TailDrop"] = {
+        ["Image"] = "rbxassetid://12735260252",
+        ["Description"] = "An ability where you can drop your tail as a food source to increase your speed, yet your weight is decreased. Your tail costs food to regrow",
+        ["Name"] = "TailDrop",
+        ["OnActivated"] = "function: 0x7a4f4a8ee64d584c",
+        ["Cooldown"] = 10,
+        ["CanUse"] = "function: 0x3c2b9d65746e032c"
+    },
+    ["StaminaPuddlePlacer"] = {
+        ["Image"] = "rbxassetid://12735260252",
+        ["Description"] = "An ability to drop a puddle of stamina which grants bonuses to stamina regen when rolled in",
+        ["OnActivated"] = "function: 0x4bd57f4c74baaa2c",
+        ["Name"] = "StaminaPuddlePlacer",
+        ["Cooldown"] = 120,
+        ["ForcedDisplayName"] = "Stamina Puddle"
+    },
+    ["SpeedSteal"] = {
+        ["Image"] = "rbxassetid://12735260252",
+        ["Description"] = "A ranged ability that decreases the speed of those in your vicinity and increases your own speed",
+        ["Range"] = 150,
+        ["OnActivated"] = "function: 0x92c8464836ca3f4c",
+        ["Cooldown"] = 60,
+        ["Name"] = "SpeedSteal"
+    },
+    ["MudPlacer"] = {
+        ["Image"] = "rbxassetid://12735260252",
+        ["Description"] = "An ability which allows you to place a mud pile to roll in for healing benefits",
+        ["OnActivated"] = "function: 0x9c43f96c392c96cc",
+        ["Name"] = "MudPlacer",
+        ["Cooldown"] = 120,
+        ["ForcedDisplayName"] = "Mud Pile"
+    },
+    ["HealingHunter"] = {
+        ["Toggleable"] = true,
+        ["Image"] = "rbxassetid://12859442957",
+        ["Description"] = "A toggleable ability which allows you to swap from damage to healing mode",
+        ["OnAttack"] = "function: 0x1aa3beea647b968c",
+        ["Name"] = "HealingHunter",
+        ["ValueScalesWithAge"] = true
+    },
+    ["Reflect"] = {
+        ["Image"] = "rbxassetid://12859524677",
+        ["Description"] = "An ability which allows you to reflect applied damage back onto the attacker while you are immune",
+        ["OnActivated"] = "function: 0x1f600e3faf904ddc",
+        ["OnDamage"] = "function: 0x8b468107c667dfec",
+        ["Cooldown"] = 45,
+        ["Name"] = "Reflect"
+    },
+    ["Breath"] = {
+        ["Controls"] = {["Gamepad"] = Enum.KeyCode.ButtonR1, ["Keyboard"] = Enum.KeyCode.C},
+        ["Image"] = "rbxassetid://12735259938",
+        ["MinAge"] = 66,
+        ["Name"] = "Breath",
+        ["Description"] = "An elemental long range attack that can apply various ailments and damage"
+    },
+    ["ChangeWeather"] = {
+        ["Image"] = "rbxassetid://12735260252",
+        ["Description"] = "Forces a weather to occur",
+        ["MinAge"] = 66,
+        ["OnActivated"] = "function: 0x9c3e2a3ebffa4b2c",
+        ["Name"] = "ChangeWeather",
+        ["Cooldown"] = 600,
+        ["CanUse"] = "function: 0x752a710feb2ceedc"
+    },
+    ["SnowShield"] = {
+        ["Toggleable"] = true,
+        ["OnActivated"] = "function: 0x70c7edf747804aac",
+        ["RemoveSpawnImmunity"] = true,
+        ["Image"] = "rbxassetid://12859524677",
+        ["MinAge"] = 66,
+        ["Description"] = "An ability which conceals you with a shield and increases hunger and thirst drain",
+        ["OnDeactivated"] = "function: 0xdd9e63b3127f6eac",
+        ["Cooldown"] = 120,
+        ["Name"] = "SnowShield"
+    },
+    ["Harden"] = {
+        ["Image"] = "rbxassetid://12859524677",
+        ["Description"] = "Decreases the players speed in trade of more weight and increased healing",
+        ["OnActivated"] = "function: 0x74280322cb89466c",
+        ["Cooldown"] = 120,
+        ["Name"] = "Harden"
+    },
+    ["HuntersCurse"] = {
+        ["Image"] = "rbxassetid://12859582084",
+        ["Description"] = "An ability which costs health to use and increases damage",
+        ["Name"] = "HuntersCurse",
+        ["OnActivated"] = "function: 0xcc504f4ea2afc93c",
+        ["Cooldown"] = 120,
+        ["CanUse"] = "function: 0xf50920ca9b6c4e2c"
+    },
+    ["ShockArea"] = {
+        ["Image"] = "rbxassetid://12735260012",
+        ["Description"] = "Causes disoriented movement and grounds fliers/prevents them from take-off for a short period of time",
+        ["Range"] = 150,
+        ["OnActivated"] = "function: 0xbd8b65d4f7c4b6ac",
+        ["Cooldown"] = 90,
+        ["Name"] = "ShockArea"
+    },
+    ["StickyTrap"] = {
+        ["Image"] = "rbxassetid://12735260252",
+        ["Description"] = "Place a trap that causes those ensnared to move slower",
+        ["OnActivated"] = "function: 0x30cc7fb9fca37adc",
+        ["Cooldown"] = 75,
+        ["Name"] = "StickyTrap"
+    },
+    ["ToxicTrap"] = {
+        ["Image"] = "rbxassetid://12735260252",
+        ["Description"] = "Place a trap that causes those ensnared to recieve poison",
+        ["OnActivated"] = "function: 0xd967f58688c710bc",
+        ["Cooldown"] = 75,
+        ["Name"] = "ToxicTrap"
+    },
+    ["PoisonArea"] = {
+        ["Image"] = "rbxassetid://12735260012",
+        ["Description"] = "Affects those in radius with poison",
+        ["Range"] = 100,
+        ["OnActivated"] = "function: 0xa845ccabdb8d256c",
+        ["Cooldown"] = 15,
+        ["Name"] = "PoisonArea"
+    },
+    ["LifeLeech"] = {
+        ["Image"] = "rbxassetid://12859442957",
+        ["Description"] = "Allows you to heal yourself from damaging other players while this ability is active",
+        ["OnActivated"] = "function: 0x84192f284ed633dc",
+        ["Cooldown"] = 60,
+        ["Name"] = "LifeLeech"
+    },
+    ["Invisibility"] = {
+        ["Toggleable"] = true,
+        ["Image"] = "rbxassetid://12859582084",
+        ["Description"] = "An ability where you appear fully invisible to those around you",
+        ["OnTick"] = "function: 0xf636edc3dd8aca0c",
+        ["OnActivated"] = "function: 0x56fa1774b97825cc",
+        ["OnDeactivated"] = "function: 0x4fd5c4ae2f78fd6c",
+        ["Cooldown"] = 30,
+        ["Name"] = "Invisibility"
+    }
+}
