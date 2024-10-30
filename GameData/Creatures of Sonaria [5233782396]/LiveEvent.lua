0c545d71ee460f49069dfa22c7d0577956bb20c8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-31 01:29:40 +1100

    Create LiveEvent.lua

---
+++ GameData/Creatures of Sonaria [5233782396]/LiveEvent.lua
@@ -0,0 +1,252 @@
+data = {
+    ["EventForageables"] = {
+        ["Common"] = {
+            ["Difficulty"] = 40,
+            ["WaitTimeToSpawn"] = 60,
+            ["MaxNodes"] = 52,
+            ["BonusDrops"] = {["YellowDye_2024"] = {["P"] = 25, ["Amount"] = 1}},
+            ["Drops"] = {["Aueko"] = {["P"] = 0.5, ["Amount"] = 1}, ["Eggs"] = {["P"] = 99.5, ["Amount"] = 4}}
+        }
+    },
+    ["AdventCalendar"] = {
+        {["Name"] = "RandomStoredCreatureToken", ["Amount"] = 1},
+        {["Name"] = "Shooms", ["Amount"] = 100},
+        {["Name"] = "PartialGrowToken", ["Amount"] = 1},
+        {["Name"] = "Twig", ["Amount"] = 1},
+        {["Name"] = "FreeSpinToken", ["Amount"] = 1},
+        {["Name"] = "Forest", ["Amount"] = 1},
+        {["Name"] = "MTL_Foil", ["Amount"] = 1},
+        {["Name"] = "CandyCane", ["Amount"] = 100},
+        {["Name"] = "Icicle", ["Amount"] = 1},
+        {["Name"] = "CandyCane", ["Amount"] = 150},
+        {["Name"] = "FirForest", ["Amount"] = 1},
+        {["Name"] = "CandyCane", ["Amount"] = 200},
+        {["Name"] = "Coal", ["Amount"] = 1},
+        {["Name"] = "CandyCane", ["Amount"] = 250},
+        {["Name"] = "MintyWiggler", ["Amount"] = 1},
+        {["Name"] = "CandyCane", ["Amount"] = 300},
+        {["Name"] = "MTL_Glass", ["Amount"] = 1},
+        {["Name"] = "CandyCane", ["Amount"] = 350},
+        {["Name"] = "MTL_Glacier", ["Amount"] = 1},
+        {["Name"] = "CandyCane", ["Amount"] = 400},
+        {["Name"] = "GingerSnapper", ["Amount"] = 1},
+        {["Name"] = "CandyCane", ["Amount"] = 450},
+        {["Name"] = "SIG_Present", ["Amount"] = 1},
+        {["Name"] = "CandyCane", ["Amount"] = 500},
+        {["Name"] = "Sleirnok", ["Amount"] = 1}
+    },
+    ["EventShopItems"] = {
+        ["Cryptoth"] = {
+            ["ForcedDisplayName"] = "Cryptoth",
+            ["Description"] = "The Cryptoth is a slow, lumbering beast. Despite its threatening appearance, it is quite passive and gentle, and prefers to eat from bushes rather than you!",
+            ["GetPrice"] = "function: 0x417dd79d5326c72c",
+            ["Price"] = 6250,
+            ["Currency"] = "Candy",
+            ["Name"] = "Cryptoth"
+        },
+        ["Ghost"] = {
+            ["ForcedDisplayName"] = "Ghost",
+            ["Description"] = {"Adds 7.5% Block Bleed"},
+            ["GetPrice"] = "function: 0xefb70ec606af56ec",
+            ["Price"] = 750,
+            ["Currency"] = "Candy",
+            ["Name"] = "Ghost"
+        },
+        ["WitchesBrew"] = {
+            ["ForcedDisplayName"] = "Witch's Brew",
+            ["Description"] = "A color palette consisting of vibrant greens, purples, and dark black. Pay fee on death to keep.",
+            ["GetPrice"] = "function: 0x79cd740b5ab7e9ac",
+            ["Price"] = 350,
+            ["Currency"] = "Candy",
+            ["Name"] = "WitchesBrew"
+        },
+        ["Owl"] = {
+            ["ForcedDisplayName"] = "Owl",
+            ["Description"] = {"Increases nightvision to max"},
+            ["GetPrice"] = "function: 0xf404dbdfbf3e7a6c",
+            ["Price"] = 650,
+            ["Currency"] = "Candy",
+            ["Name"] = "Owl"
+        },
+        ["Undoli"] = {
+            ["ForcedDisplayName"] = "Undoli",
+            ["Description"] = "Undoli are are believed to be demons that wear the skin of their victims. They prowl near other creatures, waiting for the right moment to attack and consume them, feeding their uncontrollable and constant urge to devour mortal beings.",
+            ["GetPrice"] = "function: 0x50443aa29c370c2c",
+            ["Price"] = 5000,
+            ["Currency"] = "Candy",
+            ["Name"] = "Undoli"
+        },
+        ["Viracniar"] = {
+            ["ForcedDisplayName"] = "Viracniar",
+            ["Description"] = "This spooky screature haunts sonarias nights. it soft paw pads make it incedibly silent and agile, and its amazing night vision makes it a great predator. These 8 legged creatures have mandibles on their face for fighting and grooming. they can be found in dense forests and cool deserts. Viracniar's are mainly nocturnal and don't like confrontation.",
+            ["GetPrice"] = "function: 0xd49d8829f0ac61ec",
+            ["Price"] = 4250,
+            ["Currency"] = "Candy",
+            ["Name"] = "Viracniar"
+        },
+        ["Demon"] = {
+            ["ForcedDisplayName"] = "Demon",
+            ["Description"] = "A color palette consisting of dark reds, purples, and grey. Pay fee on death to keep.",
+            ["GetPrice"] = "function: 0x28d7efdc2295d0ac",
+            ["Price"] = 500,
+            ["Currency"] = "Candy",
+            ["Name"] = "Demon"
+        },
+        ["Cat"] = {
+            ["ForcedDisplayName"] = "Cat",
+            ["Description"] = {"Adds 1 Bleed Attack, but lowers damage by 2.5%"},
+            ["GetPrice"] = "function: 0x02ed45830ed13f6c",
+            ["Price"] = 850,
+            ["Currency"] = "Candy",
+            ["Name"] = "Cat"
+        },
+        ["Octopus"] = {
+            ["ForcedDisplayName"] = "Octroma",
+            ["Name"] = "Octopus",
+            ["ProductId"] = 1843114879,
+            ["GetPrice"] = "function: 0x2ea25cf64b10ad2c",
+            ["Currency"] = "Candy",
+            ["Description"] = {
+                "Doubles the base chance an Age/Nesting Mutation occurs. Cannot Stack. Returns during time-limited events."
+            }
+        },
+        ["Garra Warden"] = {
+            ["ForcedDisplayName"] = "Garra Warden",
+            ["Description"] = "The Garra Warden reaps havoc upon those who oppose it every eclipse as the chaos that fuels it is spent. A mysterious and ominous presence, one of its many names, “Mors’vuhk” is inscribed in runes across Sonaria and it is worshipped by many as the true guardian of the dead. Although small in size, do not underestimate this Warden, as that will be your last mistake…",
+            ["GetPrice"] = "function: 0x5e7a031d64d218ec",
+            ["Price"] = 12000,
+            ["Currency"] = "Candy",
+            ["Name"] = "Garra Warden"
+        },
+        ["Salrahn"] = {
+            ["ForcedDisplayName"] = "Salrahn",
+            ["Description"] = "A large flying wyvern creature that appears to look undead. Do not underestimate it though, Salrahn are known to be extremely intelligent. At night they ravage through the land destroying anything until they and their family are fed.",
+            ["GetPrice"] = "function: 0xea3ea1a1b0f38bac",
+            ["Price"] = 8250,
+            ["Currency"] = "Candy",
+            ["Name"] = "Salrahn"
+        },
+        ["MTL_Ectoplasm"] = {
+            ["ForcedDisplayName"] = "Ectoplasm",
+            ["Description"] = "Make your creature look as if it is made of ectoplasm.",
+            ["GetPrice"] = "function: 0x71c740b6defbf46c",
+            ["Price"] = 1500,
+            ["Currency"] = "Candy",
+            ["Name"] = "MTL_Ectoplasm"
+        },
+        ["MagicFrog"] = {
+            ["ForcedDisplayName"] = "Magic Frog",
+            ["Name"] = "MagicFrog",
+            ["ProductId"] = 1666628451,
+            ["GetPrice"] = "function: 0xf781e71bfafa662c",
+            ["Currency"] = "Candy",
+            ["Description"] = {
+                "Adds a chance to nest a creature with the Bewitched mutation, and a chance to become Bewitched mutated at age 100"
+            }
+        },
+        ["CandyCorn"] = {
+            ["ForcedDisplayName"] = "Candy Corn",
+            ["Description"] = "A color palette consisting of light yellow, orange, and white. Pay fee on death to keep.",
+            ["GetPrice"] = "function: 0x694b9dc016c8d7ec",
+            ["Price"] = 300,
+            ["Currency"] = "Candy",
+            ["Name"] = "CandyCorn"
+        },
+        ["SIG_Vampire"] = {
+            ["ForcedDisplayName"] = "Vampire",
+            ["Description"] = "Make your creature look vampiric.",
+            ["GetPrice"] = "function: 0xdb102475448946ac",
+            ["Price"] = 1750,
+            ["Currency"] = "Candy",
+            ["Name"] = "SIG_Vampire"
+        },
+        ["LegendaryHybrid"] = {
+            ["ForcedDisplayName"] = "Haunt Dragon",
+            ["Description"] = {"Adds 0.5 Poison Attack"},
+            ["GetPrice"] = "function: 0x20d8c4da90c5b56c",
+            ["Price"] = 800,
+            ["Currency"] = "Candy",
+            ["Name"] = "LegendaryHybrid"
+        },
+        ["Morcanix"] = {
+            ["ForcedDisplayName"] = "Morcanix",
+            ["Description"] = "A temperamental creature of the dark forests and fog who hoists the head of its first kill upon its shoulders.",
+            ["GetPrice"] = "function: 0x3ce76e206d1c1b2c",
+            ["Price"] = 6350,
+            ["Currency"] = "Candy",
+            ["Name"] = "Morcanix"
+        },
+        ["Sang Toare"] = {
+            ["ForcedDisplayName"] = "Sang Toare",
+            ["Description"] = "Sang Toare are blind creatures of the night, never seen in daylight. Though seen as blood-sucking monsters, it's only females that drain life from the creatures of sonaria, draining them of all their plasma to empower themselves and rejuvenate their health.",
+            ["GetPrice"] = "function: 0x282907bb36c68aec",
+            ["Price"] = 9000,
+            ["Currency"] = "Candy",
+            ["Name"] = "Sang Toare"
+        },
+        ["GraveyardRuins"] = {
+            ["ForcedDisplayName"] = "Graveyard Ruins",
+            ["Description"] = "A color palette consisting of dark blues, browns, and greens. Pay fee on death to keep.",
+            ["GetPrice"] = "function: 0x5473d8d61a8efdac",
+            ["Price"] = 400,
+            ["Currency"] = "Candy",
+            ["Name"] = "GraveyardRuins"
+        },
+        ["Wixpectr'o"] = {
+            ["ForcedDisplayName"] = "Wixpectr'o",
+            ["Description"] = "Wixpectr'o are shy and mysterious creatures that prefer to lurk in the shadows. Very few have seen this creature without its signature white cloak.",
+            ["GetPrice"] = "function: 0xd7ba7a01fcc06e6c",
+            ["Price"] = 5250,
+            ["Currency"] = "Candy",
+            ["Name"] = "Wixpectr'o"
+        },
+        ["Dantenos"] = {
+            ["ForcedDisplayName"] = "Danténos",
+            ["Description"] = "Dantenos are shameful creatures regarded for their chaotic and destructive history. However, because of this stigma, they are highly misconceived; while their methods may be questionable and ruthless, they have an unwavering sense of justice that none can infringe upon.",
+            ["GetPrice"] = "function: 0x75fe2bfcd03fd02c",
+            ["Price"] = 10000,
+            ["Currency"] = "Candy",
+            ["Name"] = "Dantenos"
+        },
+        ["Pig-Lantern"] = {
+            ["ForcedDisplayName"] = "Pig-Lantern",
+            ["Description"] = {"Adds 0.5 Burn Attack, but lowers Damage by 2.5%"},
+            ["GetPrice"] = "function: 0xfb068c9785474dec",
+            ["Price"] = 650,
+            ["Currency"] = "Candy",
+            ["Name"] = "Pig-Lantern"
+        },
+        ["MTL_Gargoyle"] = {
+            ["ForcedDisplayName"] = "Gargoyle",
+            ["Description"] = "Theme that applies to your creature based on a Gargoyle!",
+            ["GetPrice"] = "function: 0x6d4d76236a8cbcac",
+            ["Price"] = 1250,
+            ["Currency"] = "Candy",
+            ["Name"] = "MTL_Gargoyle"
+        },
+        ["VampireBat"] = {
+            ["ForcedDisplayName"] = "Vampire Bat",
+            ["Description"] = {"Adds 1 Defensive Bleed, but lowers Damage by 2.5%"},
+            ["GetPrice"] = "function: 0xdeabdf3c6e462b6c",
+            ["Price"] = 950,
+            ["Currency"] = "Candy",
+            ["Name"] = "VampireBat"
+        },
+        ["Wraith"] = {
+            ["ForcedDisplayName"] = "Wraith",
+            ["Description"] = "A color palette consisting of blue hues, whites, and blacks. Pay fee on death to keep.",
+            ["GetPrice"] = "function: 0x2ae5b1890d19992c",
+            ["Price"] = 500,
+            ["Currency"] = "Candy",
+            ["Name"] = "Wraith"
+        },
+        ["Cucuerpum"] = {
+            ["ForcedDisplayName"] = "Cucuerpum",
+            ["Description"] = "Cucuerpums are known for their devious and annoying behavior,  enjoying bothering other creatures and making themselves a nuisance. They are capable of catapulting the pumpkins at the end of their tails at other creatures to antagonize them. Don't worry, they grow back!",
+            ["GetPrice"] = "function: 0x3adc12c2d059f4ec",
+            ["Price"] = 7500,
+            ["Currency"] = "Candy",
+            ["Name"] = "Cucuerpum"
+        }
+    }
+}
