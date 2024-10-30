14dab7f1948f66d0897f1d0eceb262596c981700
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-31 01:26:46 +1100

    Create StatsData.lua

---
+++ GameData/Creatures of Sonaria [5233782396]/Creatures/StatsData.lua
@@ -0,0 +1,123 @@
+data = {
+    ["Oxygen"] = {["Description"] = "The timed amount of oxygen a creature has before it drowns", ["Name"] = "Oxygen"},
+    ["FlySpeedMultiplier"] = {
+        ["Description"] = "A value which multiplies your fly speed while flying",
+        ["Name"] = "FlySpeedMultiplier"
+    },
+    ["ThirstAppetite"] = {["Description"] = "How much total water you can hold", ["Name"] = "ThirstAppetite"},
+    ["SprintSpeed"] = {
+        ["Name"] = "SprintSpeed",
+        ["Primary"] = Color3.fromRGB(0, 191, 255),
+        ["Description"] = "How fast your character can sprint",
+        ["GetName"] = "function: 0xd66684e33728ba2c"
+    },
+    ["BloodColor"] = {["Description"] = "The color of a creature's blood", ["Name"] = "BloodColor"},
+    ["FlySpeed"] = {
+        ["Name"] = "FlySpeed",
+        ["Primary"] = Color3.fromRGB(0, 191, 255),
+        ["Description"] = "How fast your character can fly",
+        ["GetName"] = "function: 0x1c41b338998f034c"
+    },
+    ["Tier"] = {
+        ["GetFormat"] = "function: 0x46af2d5fdbb9286c",
+        ["Primary"] = Color3.fromRGB(255, 255, 255),
+        ["Description"] = "How large your creature is in the world",
+        ["Name"] = "Tier"
+    },
+    ["BlockBleed"] = {["Description"] = "The ability to block oncoming bleed by a percent", ["Name"] = "BlockBleed"},
+    ["Damage"] = {
+        ["Primary"] = Color3.fromRGB(255, 34, 63),
+        ["Description"] = "The amount of damage you apply when hitting another player",
+        ["Name"] = "Damage"
+    },
+    ["Takeoff"] = {
+        ["ForcedDisplayName"] = "Takeoff Multiplier",
+        ["Description"] = "A percent value a creature requires to takeoff",
+        ["Name"] = "Takeoff"
+    },
+    ["FoodType"] = {
+        ["Name"] = "FoodType",
+        ["Description"] = "The preffered diet of a creature, dictating what it can and cannot eat",
+        ["DisplayName"] = "Diet"
+    },
+    ["Health"] = {
+        ["Primary"] = Color3.fromRGB(135, 255, 102),
+        ["Description"] = "The maximum amount of damage you can take before dying",
+        ["Name"] = "Health"
+    },
+    ["JumpStamina"] = {["Description"] = "The stamina cost it takes to jump", ["Name"] = "JumpStamina"},
+    ["Thirst"] = {["Description"] = "The rate at which your thirst decreases per tick", ["Name"] = "Thirst"},
+    ["StaminaRegen"] = {
+        ["Description"] = "How fast your character is able to regenerate stamina by a number value",
+        ["Name"] = "StaminaRegen"
+    },
+    ["StickyTeeth"] = {["Description"] = "Decreases attack speed", ["Name"] = "StickyTeeth"},
+    ["Appetite"] = {["Description"] = "How much total food you can hold", ["Name"] = "Appetite"},
+    ["Nightvision"] = {
+        ["GetFormat"] = "function: 0x92221f76602a53ec",
+        ["Description"] = "How well your character can see at night",
+        ["Name"] = "Nightvision"
+    },
+    ["Weight"] = {
+        ["GetFormat"] = "function: 0xe6aabdf53ac252ac",
+        ["Primary"] = Color3.fromRGB(191, 255, 162),
+        ["Description"] = "Weight describes how heavy your character is and can impact both oncoming and outcoming damage based on attacker or enemy weight",
+        ["Name"] = "Weight"
+    },
+    ["Speed"] = {
+        ["Name"] = "Speed",
+        ["Primary"] = Color3.fromRGB(0, 191, 255),
+        ["Description"] = "How fast your character can move",
+        ["GetName"] = "function: 0x4f8b5585a548f10c"
+    },
+    ["Hunger"] = {["Description"] = "The rate at which your hunger decreases per tick", ["Name"] = "Hunger"},
+    ["DartData"] = {["Name"] = "DartData", ["Description"] = "How far you can dart", ["DisplayName"] = "DartPower"},
+    ["JumpData"] = {["Name"] = "JumpData", ["Description"] = "How far you can jump", ["DisplayName"] = "JumpPower"},
+    ["Moisture"] = {
+        ["Description"] = "The timed amount of moisture an aquatic creature has when on land before it dries out.",
+        ["Name"] = "Moisture"
+    },
+    ["TurnRadius"] = {
+        ["Description"] = "The rate at which it takes you to turn, lower numbers are better turn and higher numbers are worse turn",
+        ["Name"] = "TurnRadius"
+    },
+    ["BaseType"] = {["Description"] = "The type of terrain your creature can traverse", ["Name"] = "BaseType"},
+    ["GrowthRate"] = {
+        ["ForcedDisplayName"] = "Growth Time",
+        ["Description"] = "The time it takes your character to reach adulthood or age 100",
+        ["GetFormat"] = "function: 0xb7caf70523f28e7c",
+        ["Primary"] = Color3.fromRGB(255, 136, 53),
+        ["Name"] = "GrowthRate"
+    },
+    ["FlySprintMultiplier"] = {
+        ["Description"] = "A value which multiplies your fly speed while sprinting",
+        ["Name"] = "FlySprintMultiplier"
+    },
+    ["HealPercent"] = {
+        ["ForcedDisplayName"] = "Health Regen",
+        ["Description"] = "the rate at which your character heals based on a percent",
+        ["Name"] = "HealPercent"
+    },
+    ["DartStamina"] = {["Description"] = "The amount of stamina required to dart", ["Name"] = "DartStamina"},
+    ["CreatureType"] = {["Description"] = "The type of terrain your creature can traverse", ["Name"] = "CreatureType"},
+    ["BloodMaterial"] = {["Description"] = "The texture of a creature's blood", ["Name"] = "BloodMaterial"},
+    ["Bleed"] = {
+        ["Description"] = "An ailment which damages your health by a number over time and prevents healing",
+        ["Name"] = "Bleed"
+    },
+    ["BiteCooldown"] = {["Description"] = "The rate at which you can attack", ["Name"] = "BiteCooldown"},
+    ["Stamina"] = {
+        ["Primary"] = Color3.fromRGB(255, 212, 38),
+        ["Description"] = "Stamina is a stat that allows your character to run, ambush, fly, and more",
+        ["Name"] = "Stamina"
+    },
+    ["BeachedSpeed"] = {["Description"] = "The speed at which an aquatic moves on land", ["Name"] = "BeachedSpeed"},
+    ["GlideRegen"] = {
+        ["Description"] = "The rate at which you regenerate stamina while gliding",
+        ["Name"] = "GlideRegen"
+    },
+    ["AmbushMultiplier"] = {
+        ["Description"] = "A value which multiplies your ambush speed",
+        ["Name"] = "AmbushMultiplier"
+    }
+}
