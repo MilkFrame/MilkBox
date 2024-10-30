7da18e7713d6a30c3afcad577c705d5543390947
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-31 01:25:23 +1100

    Create SpecialTypes.lua

---
+++ GameData/Creatures of Sonaria [5233782396]/Creatures/SpecialTypes.lua
@@ -0,0 +1,231 @@
+data = {
+    ["DevSupporter"] = {
+        ["ForcedDisplayName"] = "Developer Creature",
+        ["Name"] = "DevSupporter",
+        ["Priority"] = 2,
+        ["Gradient"] = "0 1 0.74902 0 0 1 0.505882 0.286275 0.0196078 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 6
+    },
+    ["Exclusive"] = {
+        ["ForcedDisplayName"] = "Role Limited",
+        ["Name"] = "Exclusive",
+        ["Priority"] = 3,
+        ["Gradient"] = "0 0.6 0.666667 0.666667 0 1 0.156863 0.156863 0.156863 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 20
+    },
+    ["DeathGacha"] = {
+        ["ForcedDisplayName"] = "Death Gacha",
+        ["Name"] = "DeathGacha",
+        ["Priority"] = 21,
+        ["Gradient"] = "0 1 1 1 0 0.5 0.533333 0 1 0 1 0 0 0 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 10
+    },
+    ["Meadows"] = {
+        ["ForcedDisplayName"] = "Spring Meadows Special",
+        ["Name"] = "Meadows",
+        ["Priority"] = 14,
+        ["Gradient"] = "0 0.658824 1 0.584314 0 1 0.686275 0.0901961 0.376471 0 ",
+        ["GradientOffset"] = "0, 0",
+        ["TrialMultiplier"] = 6
+    },
+    ["TikitGacha"] = {
+        ["ForcedDisplayName"] = "Royalty Special",
+        ["Name"] = "TikitGacha",
+        ["Priority"] = 18,
+        ["Gradient"] = "0 1 0.639216 0.0117647 0 1 0.443137 0.0392157 0.0392157 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 4
+    },
+    ["JulyFourth"] = {
+        ["ForcedDisplayName"] = "Firework Special",
+        ["Name"] = "JulyFourth",
+        ["Priority"] = 12,
+        ["Gradient"] = "0 1 1 1 0 0.5 1 0.184314 0.184314 0 1 0.0627451 0.470588 1 0 ",
+        ["GradientOffset"] = "0, 0",
+        ["TrialMultiplier"] = 3
+    },
+    ["MissionCreature"] = {
+        ["ForcedDisplayName"] = "Mission Reward",
+        ["Name"] = "MissionCreature",
+        ["Priority"] = 26,
+        ["Gradient"] = "0 0.529412 0.392157 0.321569 0 1 0 0 0 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 2
+    },
+    ["Halloween"] = {
+        ["ForcedDisplayName"] = "Halloween Special",
+        ["Name"] = "Halloween",
+        ["Priority"] = 5,
+        ["Gradient"] = "0 1 0.282353 0 0 1 0.364706 0 1 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 4
+    },
+    ["Thanksgiving"] = {
+        ["ForcedDisplayName"] = "Harvest Special",
+        ["Name"] = "Thanksgiving",
+        ["Priority"] = 13,
+        ["Gradient"] = "0 1 0.768627 0.6 0 1 0.254902 0.188235 0.145098 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 4
+    },
+    ["MonthlyCreature"] = {
+        ["ForcedDisplayName"] = "Monthly Special",
+        ["Name"] = "MonthlyCreature",
+        ["Priority"] = 27,
+        ["Gradient"] = "0 1 1 1 0 1 1 0.662745 0.188235 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 8
+    },
+    ["Christmas"] = {
+        ["ForcedDisplayName"] = "Winter Special",
+        ["Name"] = "Christmas",
+        ["Priority"] = 6,
+        ["Gradient"] = "0 1 1 1 0 0.5 0.203922 0.866667 1 0 1 0 0.0470588 0.321569 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 4
+    },
+    ["DailyLoginCreature"] = {
+        ["ForcedDisplayName"] = "Login Streak Special",
+        ["Name"] = "DailyLoginCreature",
+        ["Priority"] = 4,
+        ["Gradient"] = "0 1 0.207843 0.207843 0 0.5 1 0.94902 0.215686 0 1 0.309804 0.780392 1 0 ",
+        ["GradientOffset"] = "0, 0",
+        ["TrialMultiplier"] = 8
+    },
+    ["ArtifactCreature"] = {
+        ["ForcedDisplayName"] = "Artifact Creature",
+        ["Name"] = "ArtifactCreature",
+        ["Priority"] = 23,
+        ["Gradient"] = "0 0.337255 0.27451 0.196078 0 0.5 0.811765 0.627451 0.466667 0 1 0.337255 0.27451 0.196078 0 ",
+        ["GradientOffset"] = "0, 0",
+        ["TrialMultiplier"] = 8
+    },
+    ["LSSEvent"] = {
+        ["ForcedDisplayName"] = "Land Sea Sky Special",
+        ["Name"] = "LSSEvent",
+        ["Priority"] = 10,
+        ["Gradient"] = "0 1 0.784314 0.541176 0 0.5 0.470588 0.721569 0.262745 0 1 0.298039 0.776471 1 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 10
+    },
+    ["Easter"] = {
+        ["ForcedDisplayName"] = "Easter Special",
+        ["Name"] = "Easter",
+        ["Priority"] = 9,
+        ["Gradient"] = "0 0.780392 0.541176 1 0 1 0 0.886275 0.709804 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 4
+    },
+    ["Rarity"] = {
+        ["ForcedDisplayName"] = "Featured Rare",
+        ["Name"] = "Rarity",
+        ["Priority"] = 28,
+        ["Gradient"] = "0 0.172549 0.211765 0.937255 0 1 0.360784 0.529412 1 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 5
+    },
+    ["Lore"] = {
+        ["ForcedDisplayName"] = "Lore Event",
+        ["Name"] = "Lore",
+        ["Priority"] = 8,
+        ["Gradient"] = "0 0.254902 0.427451 0.509804 0 0.5 0.215686 0.364706 0.192157 0 1 0.105882 0.0705882 0.254902 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 6
+    },
+    ["Disaster"] = {
+        ["ForcedDisplayName"] = "Disaster Special",
+        ["Name"] = "Disaster",
+        ["Priority"] = 15,
+        ["Gradient"] = "0 0.180392 0.207843 0.223529 0 0.5 1 0.309804 0.219608 0 1 1 0.839216 0.521569 0 ",
+        ["GradientOffset"] = "0, 0",
+        ["TrialMultiplier"] = 6
+    },
+    ["RobuxCreature"] = {
+        ["ForcedDisplayName"] = "Robux Creature",
+        ["Name"] = "RobuxCreature",
+        ["Priority"] = 22,
+        ["Gradient"] = "0 0.980392 0.878431 0.635294 0 0.5 0.713726 0.631373 0.407843 0 1 0.980392 0.878431 0.635294 0 ",
+        ["GradientOffset"] = "0, 0",
+        ["TrialMultiplier"] = 8
+    },
+    ["ExplorerCreature"] = {
+        ["ForcedDisplayName"] = "Exploration Special",
+        ["Name"] = "ExplorerCreature",
+        ["Priority"] = 17,
+        ["Gradient"] = "0 0.776471 0.709804 0.686275 0 1 0.207843 0.184314 0.160784 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 3
+    },
+    ["LimitedGacha"] = {
+        ["ForcedDisplayName"] = "Limited Special",
+        ["Name"] = "LimitedGacha",
+        ["Priority"] = 20,
+        ["Gradient"] = "0 1 0 0 0 1 1 0.666667 0 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 6
+    },
+    ["Valentines"] = {
+        ["ForcedDisplayName"] = "Valentines Special",
+        ["Name"] = "Valentines",
+        ["Priority"] = 7,
+        ["Gradient"] = "0 1 1 1 0 1 1 0 0.835294 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 4
+    },
+    ["TimeCreature"] = {
+        ["TrialMultiplier"] = 2,
+        ["Name"] = "TimeCreature",
+        ["Gradient"] = "0 0.721569 0.52549 1 0 1 0.415686 0.207843 0.623529 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["Priority"] = 16
+    },
+    ["CelestialGacha"] = {
+        ["ForcedDisplayName"] = "Celestial Special",
+        ["Name"] = "CelestialGacha",
+        ["Priority"] = 19,
+        ["Gradient"] = "0 0.294118 0.847059 1 0 1 0.678431 0.0313726 0.956863 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 4
+    },
+    ["Warden"] = {
+        ["TrialMultiplier"] = 2,
+        ["Name"] = "Warden",
+        ["Gradient"] = "0 0.258824 0.447059 0.509804 0 1 0 0 0 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["Priority"] = 24
+    },
+    ["BadgeCreature"] = {
+        ["ForcedDisplayName"] = "Badge Reward",
+        ["Name"] = "BadgeCreature",
+        ["Priority"] = 25,
+        ["Gradient"] = "0 1 0.784314 0 0 1 1 0 0.784314 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 30
+    },
+    ["NowUnobtainable"] = {
+        ["ForcedDisplayName"] = "Limited Supply",
+        ["Name"] = "NowUnobtainable",
+        ["Priority"] = 29,
+        ["Gradient"] = "0 0.290196 0.423529 0.176471 0 1 0.0509804 0.164706 0.0509804 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["TrialMultiplier"] = 6
+    },
+    ["DeveloperOnly"] = {
+        ["ForcedDisplayName"] = "Developer Only",
+        ["Name"] = "DeveloperOnly",
+        ["Gradient"] = "0 1 0 0.498039 0 1 0 0 0 0 ",
+        ["GradientOffset"] = "0, 0.300000012",
+        ["Priority"] = 1
+    },
+    ["Paradise"] = {
+        ["ForcedDisplayName"] = "Paradise Special",
+        ["Name"] = "Paradise",
+        ["Priority"] = 11,
+        ["Gradient"] = "0 1 1 0 0 1 0 1 1 0 ",
+        ["GradientOffset"] = "0, 0",
+        ["TrialMultiplier"] = 6
+    }
+}
