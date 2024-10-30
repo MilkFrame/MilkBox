ff5439fde8955daabe09058cf9027b3e9270b680
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-31 01:28:48 +1100

    Create PlushieData.lua

---
+++ GameData/Creatures of Sonaria [5233782396]/PlushieData.lua
@@ -0,0 +1,407 @@
+data = {
+    ["GoldenBulb"] = {
+        ["NestingChanceBoosts"] = {["FoolsGold"] = 20},
+        ["Name"] = "GoldenBulb",
+        ["Ailments"] = {["GoldenBulb"] = 9000000000},
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {}
+    },
+    ["Icebreaker"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Icebreaker",
+        ["Ailments"] = {["Icebreaker"] = 9000000000}
+    },
+    ["Darkstar"] = {
+        ["PassiveAbilities"] = {},
+        ["Name"] = "Darkstar",
+        ["Ailments"] = {},
+        ["ActiveAbilities"] = {},
+        ["AilmentHealMultiplier"] = 1.25
+    },
+    ["Springram"] = {
+        ["PassiveAbilities"] = {},
+        ["Name"] = "Springram",
+        ["Ailments"] = {["Springram"] = 9000000000},
+        ["ActiveAbilities"] = {},
+        ["SatiatedMultiplier"] = 1.5
+    },
+    ["Octopus"] = {
+        ["PassiveAbilities"] = {},
+        ["Name"] = "Octopus",
+        ["Ailments"] = {},
+        ["GenericNestingChanceBoost"] = 1.5,
+        ["NestingChanceBoosts"] = {},
+        ["GenericAgingChanceBoost"] = 2,
+        ["ActiveAbilities"] = {}
+    },
+    ["Owl"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Owl",
+        ["Ailments"] = {["Owl"] = 9000000000}
+    },
+    ["Snowman"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Snowman",
+        ["Ailments"] = {["Snowman"] = 9000000000}
+    },
+    ["Reindeer"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Reindeer",
+        ["Ailments"] = {["Reindeer"] = 9000000000}
+    },
+    ["Palmtree"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Palmtree",
+        ["Ailments"] = {["PalmTree"] = 9000000000}
+    },
+    ["FireSpirit"] = {
+        ["PassiveAbilities"] = {["BlockFrostbite"] = -7.5, ["DefensiveBurn"] = 0.5},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "FireSpirit",
+        ["Ailments"] = {}
+    },
+    ["SpringbokChoy"] = {
+        ["PassiveAbilities"] = {["WillToLive"] = 100},
+        ["Name"] = "SpringbokChoy",
+        ["Ailments"] = {},
+        ["OnInit"] = "function: 0x14b87ab790ece7ac",
+        ["ActiveAbilities"] = {}
+    },
+    ["Arcane"] = {
+        ["PassiveAbilities"] = {},
+        ["Name"] = "Arcane",
+        ["Ailments"] = {["Arcane"] = 9000000000},
+        ["OnInit"] = "function: 0x815ba9f23e8eaecc",
+        ["ActiveAbilities"] = {}
+    },
+    ["JamSlug"] = {
+        ["PassiveAbilities"] = {["DefensiveNecropoison"] = 0.5},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "JamSlug",
+        ["Ailments"] = {["JamSlug"] = 9000000000}
+    },
+    ["Twig"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Twig",
+        ["Ailments"] = {["Stick"] = 9000000000}
+    },
+    ["Clownfish"] = {
+        ["NestingChanceBoosts"] = {["Clownfish"] = 3},
+        ["Name"] = "Clownfish",
+        ["Ailments"] = {["Clownfish"] = 9000000000},
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {}
+    },
+    ["Cow"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Cow",
+        ["Ailments"] = {["Cow"] = 9000000000}
+    },
+    ["MintyWiggler"] = {
+        ["PassiveAbilities"] = {["Frosty"] = 100},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "MintyWiggler",
+        ["Ailments"] = {}
+    },
+    ["Goldfish"] = {
+        ["PassiveAbilities"] = {["IronStomach"] = 100},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Goldfish",
+        ["Ailments"] = {["Goldfish"] = 9000000000}
+    },
+    ["Elemental"] = {
+        ["NestingChanceBoosts"] = {["Elemental"] = 10},
+        ["Name"] = "Elemental",
+        ["Ailments"] = {},
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {}
+    },
+    ["Bunny"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Bunny",
+        ["Ailments"] = {["Bunny"] = 9000000000}
+    },
+    ["Beetle"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Beetle",
+        ["Ailments"] = {["HornedBeetlefly"] = 9000000000}
+    },
+    ["LegendaryHybrid"] = {
+        ["PassiveAbilities"] = {["PoisonAttack"] = 0.5},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "LegendaryHybrid",
+        ["Ailments"] = {}
+    },
+    ["Partridge"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Partridge",
+        ["Ailments"] = {["Partridge"] = 9000000000}
+    },
+    ["Catalyst"] = {
+        ["NestingChanceBoosts"] = {["BloodMoon"] = 10},
+        ["Name"] = "Catalyst",
+        ["Ailments"] = {["Catalyst"] = 9000000000},
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {}
+    },
+    ["Bug"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Bug",
+        ["Ailments"] = {["MagichornProngbug"] = 9000000000}
+    },
+    ["Sparkler"] = {
+        ["PassiveAbilities"] = {["BlockBleed"] = -20, ["BlockPoison"] = 15, ["BlockBurn"] = 15, ["BlockFrostbite"] = 15},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Sparkler",
+        ["Ailments"] = {["Sparkler"] = 9000000000}
+    },
+    ["IceWolf"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "IceWolf",
+        ["Ailments"] = {["IceWolf"] = 9000000000}
+    },
+    ["Jackrabbit"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Jackrabbit",
+        ["Ailments"] = {["Jackrabbit"] = 9000000000}
+    },
+    ["PurpleHybrid"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "PurpleHybrid",
+        ["Ailments"] = {["Aerix"] = 9000000000}
+    },
+    ["VampireBat"] = {
+        ["PassiveAbilities"] = {["BlockBleed"] = 2.5, ["DefensiveBleed"] = 1},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "VampireBat",
+        ["Ailments"] = {["VampireBat"] = 9000000000}
+    },
+    ["Pig-Lantern"] = {
+        ["PassiveAbilities"] = {["BurnAttack"] = 0.5},
+        ["ForcedAilmentName"] = "Pig",
+        ["Ailments"] = {["Pig"] = 9000000000},
+        ["Name"] = "Pig-Lantern",
+        ["NestingChanceBoosts"] = {},
+        ["ActiveAbilities"] = {}
+    },
+    ["GingerSnapper"] = {
+        ["PassiveAbilities"] = {["DefensiveFrostbite"] = 0.5, ["BlockBurn"] = -5},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "GingerSnapper",
+        ["Ailments"] = {}
+    },
+    ["Seal"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Seal",
+        ["Ailments"] = {["Seal"] = 9000000000}
+    },
+    ["Rock"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Rock",
+        ["Ailments"] = {["Rock"] = 9000000000}
+    },
+    ["CloverBlossom"] = {
+        ["PassiveAbilities"] = {},
+        ["Name"] = "CloverBlossom",
+        ["Ailments"] = {["CloverBlossom"] = 9000000000},
+        ["SatiatedFromWater"] = 150,
+        ["ActiveAbilities"] = {}
+    },
+    ["Ghost"] = {
+        ["PassiveAbilities"] = {["BlockBleed"] = 7.5},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Ghost",
+        ["Ailments"] = {}
+    },
+    ["Heartsnake"] = {
+        ["PassiveAbilities"] = {["DefensivePoison"] = 0.75},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Heartsnake",
+        ["Ailments"] = {}
+    },
+    ["Eclipse"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Eclipse",
+        ["Ailments"] = {["Eclipse"] = 9000000000}
+    },
+    ["Coal"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Coal",
+        ["Ailments"] = {["Coal"] = 9000000000}
+    },
+    ["Rosevine"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {["HealingHunter"] = 1.1},
+        ["Name"] = "Rosevine",
+        ["Ailments"] = {}
+    },
+    ["AstralQuetzal"] = {
+        ["PassiveAbilities"] = {["BlockBleed"] = 50},
+        ["Name"] = "AstralQuetzal",
+        ["Ailments"] = {["AstralQuetzal"] = 9000000000},
+        ["OnInit"] = "function: 0x9f9c0f69dac43f8c",
+        ["ActiveAbilities"] = {}
+    },
+    ["MapleLeaflet"] = {
+        ["PassiveAbilities"] = {["BlockInjury"] = 22.5},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "MapleLeaflet",
+        ["Ailments"] = {}
+    },
+    ["Cat"] = {
+        ["PassiveAbilities"] = {["BleedAttack"] = 1},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Cat",
+        ["Ailments"] = {["Cat"] = 9000000000}
+    },
+    ["Pie"] = {
+        ["PassiveAbilities"] = {["SerratedTeeth"] = 100},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Pie",
+        ["Ailments"] = {["Pie"] = 9000000000}
+    },
+    ["TestAbility"] = {
+        ["OnInit"] = "function: 0xc40bb5091aa2192c",
+        ["Name"] = "TestAbility",
+        ["OnTick"] = "function: 0x49e4e4a4f71d4e4c",
+        ["Ailments"] = {["Sickly"] = 4},
+        ["NestingChanceBoosts"] = {["Smores"] = 1000},
+        ["CanEquip"] = "function: 0x58c1dbc244e574ec",
+        ["PassiveAbilities"] = {["Muddy"] = 1},
+        ["ActiveAbilities"] = {["Muddy"] = 1}
+    },
+    ["Mylo"] = {["PassiveAbilities"] = {}, ["ActiveAbilities"] = {}, ["Name"] = "Mylo", ["Ailments"] = {}},
+    ["EggSnake"] = {
+        ["PassiveAbilities"] = {},
+        ["Name"] = "EggSnake",
+        ["Ailments"] = {},
+        ["OnInit"] = "function: 0x33708928acea506c",
+        ["ActiveAbilities"] = {}
+    },
+    ["Sea"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Sea",
+        ["Ailments"] = {["Sea"] = 9000000000}
+    },
+    ["SmoreCat"] = {
+        ["NestingChanceBoosts"] = {["Smore"] = 20},
+        ["Name"] = "SmoreCat",
+        ["Ailments"] = {["SmoreCat"] = 9000000000},
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {}
+    },
+    ["Serpent"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Serpent",
+        ["Ailments"] = {["Serpent"] = 9000000000}
+    },
+    ["BlueHybrid"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "BlueHybrid",
+        ["Ailments"] = {["Euvatops"] = 9000000000}
+    },
+    ["Void"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Void",
+        ["Ailments"] = {["Void"] = 9000000000}
+    },
+    ["Sky"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Sky",
+        ["Ailments"] = {["Sky"] = 9000000000}
+    },
+    ["Land"] = {
+        ["PassiveAbilities"] = {},
+        ["Name"] = "Land",
+        ["Ailments"] = {},
+        ["ActiveAbilities"] = {},
+        ["MudMultiplier"] = 2
+    },
+    ["Swan"] = {
+        ["PassiveAbilities"] = {["AgileSwimmer"] = 100},
+        ["Name"] = "Swan",
+        ["Ailments"] = {["Swan"] = 9000000000},
+        ["CanEquip"] = "function: 0xa3bd33e1664b2b0c",
+        ["ActiveAbilities"] = {}
+    },
+    ["Lilypad"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Lilypad",
+        ["Ailments"] = {["Succulant"] = 9000000000}
+    },
+    ["Eggshell"] = {
+        ["PassiveAbilities"] = {},
+        ["Name"] = "Eggshell",
+        ["Ailments"] = {},
+        ["EggMultiplier"] = 1.5,
+        ["ActiveAbilities"] = {}
+    },
+    ["Butterfly"] = {
+        ["PassiveAbilities"] = {},
+        ["Name"] = "Butterfly",
+        ["Ailments"] = {},
+        ["ActiveAbilities"] = {},
+        ["MudMultiplier"] = 1.5
+    },
+    ["Chick"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Chick",
+        ["Ailments"] = {["Chick"] = 9000000000}
+    },
+    ["Heart"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Heart",
+        ["Ailments"] = {["Heart"] = 9000000000}
+    },
+    ["Tannenbaum"] = {
+        ["PassiveAbilities"] = {["FrostbiteAttack"] = 0.5},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "Tannenbaum",
+        ["Ailments"] = {["Tannenbaum"] = 9000000000}
+    },
+    ["GoldenHybrid"] = {
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "GoldenHybrid",
+        ["Ailments"] = {["Aerodon"] = 9000000000}
+    },
+    ["FrostDragon"] = {
+        ["PassiveAbilities"] = {["BlockFrostbite"] = 25},
+        ["ActiveAbilities"] = {},
+        ["Name"] = "FrostDragon",
+        ["Ailments"] = {["FrostDragon"] = 9000000000}
+    },
+    ["MagicFrog"] = {
+        ["NestingChanceBoosts"] = {["Bewitched"] = 5},
+        ["Name"] = "MagicFrog",
+        ["Ailments"] = {},
+        ["PassiveAbilities"] = {},
+        ["ActiveAbilities"] = {}
+    }
+}
