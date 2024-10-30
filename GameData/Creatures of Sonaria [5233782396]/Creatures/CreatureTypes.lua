195b165bf96da99001642e721977462d2d264a6d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-31 01:25:56 +1100

    Create CreatureTypes.lua

---
+++ GameData/Creatures of Sonaria [5233782396]/Creatures/CreatureTypes.lua
@@ -0,0 +1,12 @@
+data = {
+    ["Flier"] = {["Color"] = Color3.fromRGB(170, 255, 255), ["BaseType"] = "Sky", ["Name"] = "Flier"},
+    ["Land"] = {["Color"] = Color3.fromRGB(136, 217, 42), ["BaseType"] = "Land", ["Name"] = "Land"},
+    ["Aquatic"] = {["Color"] = Color3.fromRGB(0, 191, 255), ["BaseType"] = "Sea", ["Name"] = "Aquatic"},
+    ["SemiAquatic"] = {
+        ["Color"] = Color3.fromRGB(0, 191, 255),
+        ["ForcedDisplayName"] = "Semi-Aquatic",
+        ["BaseType"] = "Sea",
+        ["Name"] = "SemiAquatic"
+    },
+    ["Glider"] = {["Color"] = Color3.fromRGB(170, 255, 255), ["BaseType"] = "Sky", ["Name"] = "Glider"}
+}
