18175b3fc2ffee5d0d0ad8aa688b57efa63bf5b0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-10-31 01:19:55 +1100

    Create PlayerStats.lua

---
+++ GameData/Creatures of Sonaria [5233782396]/PlayerStats.lua
@@ -0,0 +1,39 @@
+data = {
+    ["Temporary"] = {
+        ["NestedPlayers"] = {["Name"] = "NestedPlayers", ["StatType"] = "IntValue"},
+        ["DieT5AquaticAsT3"] = {["Name"] = "DieT5AquaticAsT3", ["StatType"] = "IntValue"}
+    },
+    ["Saved"] = {
+        ["FlyDistance"] = {["Name"] = "FlyDistance", ["StatType"] = "IntValue"},
+        ["InflictShock"] = {["Name"] = "InflictShock", ["StatType"] = "IntValue"},
+        ["GuiltyKilled"] = {["Name"] = "GuiltyKilled", ["StatType"] = "IntValue"},
+        ["ExperienceThunderstorm"] = {["Name"] = "ExperienceThunderstorm", ["StatType"] = "IntValue"},
+        ["TimesDrowned"] = {["Name"] = "TimesDrowned", ["StatType"] = "IntValue"},
+        ["LengthLatched"] = {["Name"] = "LengthLatched", ["StatType"] = "IntValue"},
+        ["StudsTravelledAtNight"] = {["Name"] = "StudsTravelledAtNight", ["StatType"] = "IntValue"},
+        ["FirstPlaceCostumeFinishes"] = {["Name"] = "FirstPlaceCostumeFinishes", ["StatType"] = "IntValue"},
+        ["TotalDeaths"] = {["Name"] = "TotalDeaths", ["StatType"] = "IntValue"},
+        ["FortifyClearedDebuff"] = {["Name"] = "FortifyClearedDebuff", ["StatType"] = "IntValue"},
+        ["ReceivedCorrosionTotal"] = {["Name"] = "ReceivedCorrosionTotal", ["StatType"] = "IntValue"},
+        ["TimePlayed"] = {["Name"] = "TimePlayed", ["StatType"] = "IntValue"},
+        ["KillTier3AtNight"] = {["Name"] = "KillTier3AtNight", ["StatType"] = "IntValue"},
+        ["AquaticBleedOnTerrestrial"] = {["Name"] = "AquaticBleedOnTerrestrial", ["StatType"] = "IntValue"},
+        ["TimeUnderwater"] = {["Name"] = "TimeUnderwater", ["StatType"] = "IntValue"},
+        ["RottenFoodConsumed"] = {["Name"] = "RottenFoodConsumed", ["StatType"] = "IntValue"},
+        ["UseLifeLeech"] = {["Name"] = "UseLifeLeech", ["StatType"] = "IntValue"},
+        ["ExperienceEruption"] = {["Name"] = "ExperienceEruption", ["StatType"] = "IntValue"},
+        ["InvisibilityLength"] = {["Name"] = "InvisibilityLength", ["StatType"] = "IntValue"},
+        ["ExperienceTornado"] = {["Name"] = "ExperienceTornado", ["StatType"] = "IntValue"},
+        ["InflictDrowsy"] = {["Name"] = "InflictDrowsy", ["StatType"] = "IntValue"},
+        ["BreathDamage"] = {["Name"] = "BreathDamage", ["StatType"] = "IntValue"},
+        ["UsedReflect"] = {["Name"] = "UsedReflect", ["StatType"] = "IntValue"},
+        ["AreaFoodRestored"] = {["Name"] = "AreaFoodRestored", ["StatType"] = "IntValue"},
+        ["LavaDeathCount"] = {["Name"] = "LavaDeathCount", ["StatType"] = "IntValue"},
+        ["ReceivedFear"] = {["Name"] = "ReceivedFear", ["StatType"] = "IntValue"},
+        ["TimesGrabbing"] = {["Name"] = "TimesGrabbing", ["StatType"] = "IntValue"},
+        ["SurviveInLava"] = {["Name"] = "SurviveInLava", ["StatType"] = "IntValue"},
+        ["BerserkKillCount"] = {["Name"] = "BerserkKillCount", ["StatType"] = "IntValue"},
+        ["ReceivedAdrenaline"] = {["Name"] = "ReceivedAdrenaline", ["StatType"] = "IntValue"},
+        ["CreatureKills"] = {["Name"] = "CreatureKills", ["StatType"] = "IntValue"}
+    }
+}
