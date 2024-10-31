1c72aa88037e1f9c3aecf1f4a346a5a8812621df
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-01 01:39:31 +1100

    Create Products.lua

---
+++ GameData/Horse Life [15696848933]/Products.lua
@@ -0,0 +1,148 @@
+data = {
+    ["Products"] = {},
+    ["Bundles"] = {
+        {
+            ["ProductType"] = "Bundle",
+            ["Infertile"] = false,
+            ["BundleName"] = "StarterBundle",
+            ["ProductId"] = 1913054047
+        },
+        {
+            ["ProductType"] = "Bundle",
+            ["Infertile"] = true,
+            ["BundleName"] = "BreedersBundle",
+            ["ProductId"] = 1913102023
+        },
+        {["ProductType"] = "Bundle", ["Infertile"] = true, ["BundleName"] = "PinkBundle", ["ProductId"] = 1913084260},
+        {["ProductType"] = "Bundle", ["Infertile"] = false, ["BundleName"] = "PinkBundle", ["ProductId"] = 1913676981},
+        {
+            ["ProductType"] = "Bundle",
+            ["Infertile"] = false,
+            ["BundleName"] = "HalloweenBundle",
+            ["ProductId"] = 1940806847
+        },
+        {["ProductType"] = "Bundle", ["Infertile"] = false, ["BundleName"] = "ItemBundle", ["ProductId"] = 1940806899}
+    },
+    ["PlotPlacementSlots"] = {
+        {["Price"] = 1000, ["Currency"] = "Coins", ["Amount"] = 25},
+        {["Price"] = 2000, ["Currency"] = "Coins", ["Amount"] = 25},
+        {["Price"] = 4000, ["Currency"] = "Coins", ["Amount"] = 25},
+        {["Price"] = 6000, ["Currency"] = "Coins", ["Amount"] = 25}
+    },
+    ["Coins"] = {
+        {["Amount"] = 600, ["Currency"] = "Coins", ["ProductType"] = "Currency", ["ProductId"] = 1764575150},
+        {["Amount"] = 2000, ["Currency"] = "Coins", ["ProductType"] = "Currency", ["ProductId"] = 1764575301},
+        {["Amount"] = 4500, ["Currency"] = "Coins", ["ProductType"] = "Currency", ["ProductId"] = 1764575428},
+        {["Amount"] = 10500, ["Currency"] = "Coins", ["ProductType"] = "Currency", ["ProductId"] = 1764575621},
+        {["Amount"] = 30000, ["Currency"] = "Coins", ["ProductType"] = "Currency", ["ProductId"] = 1764575766},
+        {["Amount"] = 50000, ["Currency"] = "Coins", ["ProductType"] = "Currency", ["ProductId"] = 1764575938},
+        {["Amount"] = 175000, ["Currency"] = "Coins", ["ProductType"] = "Currency", ["ProductId"] = 1907366581}
+    },
+    ["InventorySlots"] = {
+        ["Robux"] = {
+            {["Amount"] = 300, ["ProductType"] = "InventorySlots", ["ProductId"] = 1769421419},
+            {["Amount"] = 300, ["ProductType"] = "InventorySlots", ["ProductId"] = 1769421569}
+        },
+        ["Free"] = {
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 250, ["Stats"] = {["NodesHarvested"] = 10}},
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 1000, ["Stats"] = {["NodesHarvested"] = 25}},
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 2000, ["Stats"] = {["NodesHarvested"] = 50}},
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 4000, ["Stats"] = {["NodesHarvested"] = 75}},
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 6500, ["Stats"] = {["NodesHarvested"] = 100}},
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 8000, ["Stats"] = {["NodesHarvested"] = 125}},
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 10000, ["Stats"] = {["NodesHarvested"] = 150}},
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 12500, ["Stats"] = {["NodesHarvested"] = 175}},
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 15000, ["Stats"] = {["NodesHarvested"] = 200}},
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 17500, ["Stats"] = {["NodesHarvested"] = 225}},
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 25000, ["Stats"] = {["NodesHarvested"] = 350}},
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 40000, ["Stats"] = {["NodesHarvested"] = 400}},
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 50000, ["Stats"] = {["NodesHarvested"] = 500}},
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 65000, ["Stats"] = {["NodesHarvested"] = 650}},
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 75000, ["Stats"] = {["NodesHarvested"] = 750}},
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 100000, ["Stats"] = {["NodesHarvested"] = 1000}},
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 125000, ["Stats"] = {["NodesHarvested"] = 2000}},
+            {["Currency"] = "Coins", ["Amount"] = 100, ["Price"] = 150000, ["Stats"] = {["NodesHarvested"] = 3000}}
+        }
+    },
+    ["ItemPacks"] = {
+        ["TutorialStarterPack"] = {
+            ["Items"] = {["AppleMuffin"] = 20, ["GoodFeed"] = 20, ["MasterLasso"] = 20, ["Coins"] = 2500},
+            ["ProductType"] = "ItemPacks",
+            ["Name"] = "TutorialStarterPack",
+            ["ProductId"] = 1888508369
+        }
+    },
+    ["EquipSlots"] = {
+        ["Robux"] = {{["Amount"] = 1, ["ProductType"] = "EquipSlots", ["ProductId"] = 1759203350}},
+        ["Free"] = {
+            {
+                ["Currency"] = "Coins",
+                ["Amount"] = 1,
+                ["Price"] = 500,
+                ["Stats"] = {["FoodTames"] = 25, ["LassoTames"] = 25}
+            },
+            {
+                ["Currency"] = "Coins",
+                ["Amount"] = 1,
+                ["Price"] = 5000,
+                ["Stats"] = {["FoodTames"] = 100, ["LassoTames"] = 100}
+            },
+            {
+                ["Currency"] = "Coins",
+                ["Amount"] = 1,
+                ["Price"] = 15000,
+                ["Stats"] = {["FoodTames"] = 200, ["LassoTames"] = 200}
+            },
+            {
+                ["Currency"] = "Coins",
+                ["Amount"] = 1,
+                ["Price"] = 50000,
+                ["Stats"] = {["FoodTames"] = 400, ["LassoTames"] = 400}
+            },
+            {
+                ["Currency"] = "Coins",
+                ["Amount"] = 1,
+                ["Price"] = 100000,
+                ["Stats"] = {["FoodTames"] = 750, ["LassoTames"] = 750}
+            }
+        }
+    },
+    ["BuildingStorageSlots"] = {
+        {["Price"] = 1000, ["Currency"] = "Coins", ["Amount"] = 100},
+        {["Price"] = 2000, ["Currency"] = "Coins", ["Amount"] = 100},
+        {["Price"] = 3000, ["Currency"] = "Coins", ["Amount"] = 100},
+        {["Price"] = 5000, ["Currency"] = "Coins", ["Amount"] = 100},
+        {["Price"] = 7000, ["Currency"] = "Coins", ["Amount"] = 100},
+        {["Price"] = 9000, ["Currency"] = "Coins", ["Amount"] = 100}
+    },
+    ["PlotSaveSlots"] = {
+        {["SaveSlotIndex"] = 3, ["ProductType"] = "PlotSaveSlot", ["ProductId"] = 1781554629},
+        {["Price"] = 10000, ["Currency"] = "Coins", ["SaveSlotIndex"] = 2}
+    },
+    ["StorageSlots"] = {
+        ["Robux"] = {
+            {["Amount"] = 5, ["ProductType"] = "StorageSlots", ["ProductId"] = 1759202935},
+            {["Amount"] = 5, ["ProductType"] = "StorageSlots", ["ProductId"] = 1759203048},
+            {["Amount"] = 5, ["ProductType"] = "StorageSlots", ["ProductId"] = 1863967316},
+            {["Amount"] = 15, ["ProductType"] = "StorageSlots", ["ProductId"] = 1759203133}
+        },
+        ["Free"] = {
+            {["Price"] = 1000, ["Currency"] = "Coins", ["Amount"] = 5},
+            {["Price"] = 2500, ["Currency"] = "Coins", ["Amount"] = 5},
+            {["Price"] = 5000, ["Currency"] = "Coins", ["Amount"] = 5},
+            {["Price"] = 7500, ["Currency"] = "Coins", ["Amount"] = 5},
+            {["Price"] = 10000, ["Currency"] = "Coins", ["Amount"] = 5},
+            {["Price"] = 15000, ["Currency"] = "Coins", ["Amount"] = 5},
+            {["Price"] = 20000, ["Currency"] = "Coins", ["Amount"] = 5},
+            {["Price"] = 35000, ["Currency"] = "Coins", ["Amount"] = 5},
+            {["Price"] = 50000, ["Currency"] = "Coins", ["Amount"] = 5},
+            {["Price"] = 75000, ["Currency"] = "Coins", ["Amount"] = 5},
+            {["Price"] = 100000, ["Currency"] = "Coins", ["Amount"] = 5},
+            {["Price"] = 150000, ["Currency"] = "Coins", ["Amount"] = 5},
+            {["Price"] = 200000, ["Currency"] = "Coins", ["Amount"] = 5},
+            {["Price"] = 350000, ["Currency"] = "Coins", ["Amount"] = 5},
+            {["Price"] = 500000, ["Currency"] = "Coins", ["Amount"] = 5},
+            {["Price"] = 750000, ["Currency"] = "Coins", ["Amount"] = 5}
+        }
+    }
+}
