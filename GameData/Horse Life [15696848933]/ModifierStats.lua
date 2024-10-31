a452f773bd8010ed5a2bd205f9d0321593b170ba
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-01 03:07:44 +1100

    Create ModifierStats.lua

---
+++ GameData/Horse Life [15696848933]/ModifierStats.lua
@@ -0,0 +1,85 @@
+data = {
+    ["TraitInheritanceMultiplier"] = {
+        ["Image"] = "rbxassetid://16450269627",
+        ["Name"] = "TraitInheritanceMultiplier",
+        ["Color"] = Color3.fromRGB(255, 185, 207),
+        ["Format"] = "function: 0x3408fee4f4cce983",
+        ["DisplayName"] = "TraitInheritance"
+    },
+    ["JumpHeight"] = {
+        ["Format"] = "function: 0x602b14b7dbf76513",
+        ["Image"] = "rbxassetid://16190568719",
+        ["Name"] = "JumpHeight",
+        ["DisplayName"] = "JumpHeight"
+    },
+    ["AgilityExperience"] = {
+        ["Format"] = "function: 0x1867be106f26b043",
+        ["Image"] = "rbxassetid://16190565520",
+        ["Name"] = "AgilityExperience",
+        ["DisplayName"] = "AgilityExperience"
+    },
+    ["MutationInheritanceMultiplier"] = {
+        ["Image"] = "rbxassetid://16450269627",
+        ["Name"] = "MutationInheritanceMultiplier",
+        ["Color"] = Color3.fromRGB(220, 79, 255),
+        ["Format"] = "function: 0xfc6d9b607c3f8353",
+        ["DisplayName"] = "MutationInheritance"
+    },
+    ["JumpExperience"] = {
+        ["Format"] = "function: 0x48cd49995747cae3",
+        ["Image"] = "rbxassetid://16190565520",
+        ["Name"] = "JumpExperience",
+        ["DisplayName"] = "JumpExperience"
+    },
+    ["BondExperience"] = {
+        ["Format"] = "function: 0xf1866f7d08970bb3",
+        ["Image"] = "rbxassetid://16190565520",
+        ["Name"] = "BondExperience",
+        ["DisplayName"] = "BondExperience"
+    },
+    ["TurnSpeed"] = {
+        ["Image"] = "rbxassetid://16450269627",
+        ["Name"] = "TurnSpeed",
+        ["Format"] = "function: 0xd53e607d118876c3"
+    },
+    ["HarvestStrength"] = {
+        ["Format"] = "function: 0x0bf25cb5b0d2a563",
+        ["ForcedDisplayName"] = "Damage to Forageables",
+        ["Name"] = "HarvestStrength",
+        ["Image"] = "rbxassetid://16190565520"
+    },
+    ["UnlimitedStamina"] = {
+        ["Image"] = "rbxassetid://16190565520",
+        ["Name"] = "UnlimitedStamina",
+        ["Format"] = "function: 0x8b894d9e67443cf3"
+    },
+    ["Stamina"] = {
+        ["Format"] = "function: 0xd170d1ccfc55c773",
+        ["Image"] = "rbxassetid://16190564664",
+        ["Name"] = "Stamina",
+        ["DisplayName"] = "Stamina"
+    },
+    ["Speed"] = {
+        ["Image"] = "rbxassetid://16450269627",
+        ["Name"] = "Speed",
+        ["Format"] = "function: 0x0f134ed892deddf3"
+    },
+    ["FertilityExperience"] = {
+        ["Format"] = "function: 0x6f1df28f758969a3",
+        ["Image"] = "rbxassetid://16190565520",
+        ["Name"] = "FertilityExperience",
+        ["DisplayName"] = "FertilityExperience"
+    },
+    ["StrengthExperience"] = {
+        ["Format"] = "function: 0xf9ab29029273c303",
+        ["Image"] = "rbxassetid://16190565520",
+        ["Name"] = "StrengthExperience",
+        ["DisplayName"] = "StrengthExperience"
+    },
+    ["GestationTimeMultiplier"] = {
+        ["Format"] = "function: 0x06b88f4dd6ba2ad3",
+        ["Image"] = "rbxassetid://16190566666",
+        ["Name"] = "GestationTimeMultiplier",
+        ["DisplayName"] = "GestationTime"
+    }
+}
