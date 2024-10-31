12a3153f1558e44dc7fcec03bff4994360873338
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-01 01:38:38 +1100

    Create ItemSets.lua

---
+++ GameData/Horse Life [15696848933]/ItemSets.lua
@@ -0,0 +1,352 @@
+data = {
+    ["LikeTarget2"] = {
+        ["Items"] = {
+            ["NameTag"] = 1,
+            ["LeafyFlowerCrown"] = 1,
+            ["InstantFoalPotion"] = 1,
+            ["IronChainTrail"] = 1,
+            ["RandomEquipment"] = 1,
+            ["ColorDye"] = 1,
+            ["RandomWingPotion"] = 1
+        },
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["Name"] = "LikeTarget2",
+        ["ItemType"] = "ItemSets",
+        ["RestoreAddedItems"] = true,
+        ["Claimable"] = {["LastUpdate"] = 11},
+        ["ExtraItems"] = {["LeafyFlowerCrown"] = {["ThemeChance"] = 1, ["Theme"] = "Random", ["Amount"] = 1}},
+        ["Text"] = "🎉 Thank you for hitting 100k likes! 🎉"
+    },
+    ["BetaSupporterPack"] = {
+        ["Items"] = {["FaeHeadwings"] = 4, ["MysticSparkleTrail"] = 2, ["FantasyWingPotion"] = 6},
+        ["ItemType"] = "ItemSets",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["RestoreAddedItems"] = true,
+        ["Claimable"] = {["Badges"] = {"BetaSupporter"}},
+        ["Name"] = "BetaSupporterPack",
+        ["Text"] = "💚 Thank you for supporting the game during Beta! 💚"
+    },
+    ["AlphaSupporterPack"] = {
+        ["Items"] = {["RainbowSparkleTrail"] = 2, ["FantasyWingPotion"] = 6, ["DragonSkull"] = 4},
+        ["ItemType"] = "ItemSets",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["RestoreAddedItems"] = true,
+        ["Claimable"] = {["Badges"] = {"AlphaSupporter"}},
+        ["Name"] = "AlphaSupporterPack",
+        ["Text"] = "💖 Thank you for supporting the game during Alpha! 💖"
+    },
+    ["LikesStarterPack"] = {
+        ["Items"] = {
+            ["TraitBoostPotion"] = 5,
+            ["WovenLasso"] = 10,
+            ["InstantFoalPotion"] = 5,
+            ["RandomWingPotion"] = 1,
+            ["Coins"] = 1500
+        },
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["Name"] = "LikesStarterPack",
+        ["ItemType"] = "ItemSets",
+        ["RestoreAddedItems"] = true,
+        ["Claimable"] = {["Badges"] = {"AlphaTester", "BetaTester"}},
+        ["ExtraItems"] = {
+            ["StarterHorse"] = {
+                ["NoMutations"] = true,
+                ["Preset"] = "function: 0xbf19c699487ce683",
+                ["Origin"] = "Wild",
+                ["Equipped"] = true,
+                ["Species"] = "Horse",
+                ["Age"] = 66,
+                ["Gender"] = "Male"
+            }
+        },
+        ["Text"] = "🎉 Thank you for hitting 60k likes! 🎉"
+    },
+    ["LikeTarget1"] = {
+        ["Items"] = {["TraitBoostPotion"] = 2, ["RandomFlair"] = 1, ["RandomWingPotion"] = 1},
+        ["ItemType"] = "ItemSets",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["RestoreAddedItems"] = true,
+        ["Claimable"] = {["LastUpdate"] = 9},
+        ["Name"] = "LikeTarget1",
+        ["Text"] = "🎉 Thank you for hitting 75k likes! 🎉"
+    },
+    ["LikeTarget6"] = {
+        ["Items"] = {
+            ["MoonglowTrail"] = 1,
+            ["GoodFeed"] = 10,
+            ["InstantFoalPotion"] = 1,
+            ["LunarLordHalo"] = 1,
+            ["NameTag"] = 1,
+            ["MoonRays"] = 1
+        },
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["Name"] = "LikeTarget6",
+        ["ItemType"] = "ItemSets",
+        ["RestoreAddedItems"] = true,
+        ["Claimable"] = {["LastUpdate"] = 27},
+        ["ExtraItems"] = {
+            ["RandomEquipment"] = {["ThemeChance"] = 1, ["Theme"] = "Moon", ["Amount"] = 1},
+            ["MoonGhoulsteed"] = {
+                ["Infertile"] = true,
+                ["MutationSlotChances"] = {0, 0, 1, 0},
+                ["Origin"] = "Developer",
+                ["MutationsToGuarantee"] = {["GlowPattern"] = 100, ["GlowHair"] = 100},
+                ["Species"] = "Ghoulsteed",
+                ["Preset"] = {"MoonL", "MoonD"},
+                ["MaxCosmetics"] = {
+                    ["MaxPatternsPerSlot"] = 4,
+                    ["MaxPaintsPerSlot"] = 2,
+                    ["AlwaysMax"] = true,
+                    ["MaxCosmeticsPerSlot"] = 4
+                }
+            },
+            ["LunarLordHalo"] = {["ThemeChance"] = 1, ["Theme"] = "Random", ["Amount"] = 1}
+        },
+        ["Text"] = "🎉LIKE REWARDS GOAL 275k!! THANK YOU 🎉"
+    },
+    ["AlphaTesterPack"] = {
+        ["Items"] = {["BlossomingBranches"] = 4, ["GoldSparkleTrail"] = 2},
+        ["ItemType"] = "ItemSets",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["RestoreAddedItems"] = true,
+        ["Claimable"] = {["Badges"] = {"AlphaTester"}},
+        ["Name"] = "AlphaTesterPack",
+        ["Text"] = "❤️ Thank you for playing the game during Alpha! ❤️"
+    },
+    ["LikeTarget5"] = {
+        ["Items"] = {
+            ["SunRays"] = 1,
+            ["SunLordHalo"] = 1,
+            ["GoodFeed"] = 10,
+            ["LiftedWingPotion"] = 1,
+            ["SunglowTrail"] = 1
+        },
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["Name"] = "LikeTarget5",
+        ["ItemType"] = "ItemSets",
+        ["RestoreAddedItems"] = true,
+        ["Claimable"] = {["LastUpdate"] = 21},
+        ["ExtraItems"] = {
+            ["RandomEquipment"] = {["ThemeChance"] = 1, ["Theme"] = "Sun", ["Amount"] = 1},
+            ["SunLordHalo"] = {["ThemeChance"] = 1, ["Theme"] = "Random", ["Amount"] = 1},
+            ["SunPony"] = {
+                ["Infertile"] = true,
+                ["MutationSlotChances"] = {0, 0, 1, 0},
+                ["Origin"] = "Developer",
+                ["MutationsToGuarantee"] = {["GlowHair"] = 100, ["GlowPattern"] = 100, ["GlowPaint"] = 100},
+                ["Species"] = "Pony",
+                ["Preset"] = {"Sun"},
+                ["MaxCosmetics"] = {
+                    ["MaxPatternsPerSlot"] = 4,
+                    ["MaxPaintsPerSlot"] = 2,
+                    ["AlwaysMax"] = true,
+                    ["MaxCosmeticsPerSlot"] = 4
+                }
+            }
+        },
+        ["Text"] = "🎉LIKE REWARDS GOAL 225k!! THANK YOU 🎉"
+    },
+    ["Reimbursement2"] = {
+        ["Items"] = {["PurpleButterflies"] = 1, ["ColorDye"] = 1, ["PurpleSparkleTrail"] = 1},
+        ["ItemType"] = "ItemSets",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["RestoreAddedItems"] = true,
+        ["Claimable"] = {["LastUpdate"] = 18},
+        ["Name"] = "Reimbursement2",
+        ["Text"] = "❤️ Reimbursement part 2!"
+    },
+    ["LikeTarget3"] = {
+        ["Items"] = {
+            ["TraitBoostPotion"] = 1,
+            ["SunriseSparkleTrail"] = 1,
+            ["LavenderCrown"] = 1,
+            ["WingUpgradePotion"] = 1,
+            ["RandomEquipment"] = 1,
+            ["ColorDye"] = 1,
+            ["MutationDeletePotion"] = 1
+        },
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["Name"] = "LikeTarget3",
+        ["ItemType"] = "ItemSets",
+        ["RestoreAddedItems"] = true,
+        ["Claimable"] = {["LastUpdate"] = 13},
+        ["ExtraItems"] = {["LavenderCrown"] = {["ThemeChance"] = 1, ["Theme"] = "Random", ["Amount"] = 1}},
+        ["Text"] = "🎉 Thank you for hitting 145k likes! 🎉"
+    },
+    ["Reimbursement1"] = {
+        ["Items"] = {
+            ["MutationDeletePotion"] = 1,
+            ["MasterLasso"] = 2,
+            ["MutationPotion"] = 1,
+            ["InstantFoalPotion"] = 2,
+            ["BasicFeed"] = 3,
+            ["RandomEquipment"] = 2,
+            ["NameTag"] = 2,
+            ["RandomWingPotion"] = 1
+        },
+        ["ItemType"] = "ItemSets",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["RestoreAddedItems"] = true,
+        ["Claimable"] = {["ClaimBeforeTimestamp"] = 1722557046, ["LastUpdate"] = 17},
+        ["Name"] = "Reimbursement1",
+        ["Text"] = "❤️ Reimbursement for purchasing issues"
+    },
+    ["LikeTarget4"] = {
+        ["Items"] = {["LiftedWingPotion"] = 1, ["RestingWingPotion"] = 1, ["InstantFoalPotion"] = 1},
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["Name"] = "LikeTarget4",
+        ["ItemType"] = "ItemSets",
+        ["RestoreAddedItems"] = true,
+        ["Claimable"] = {["ClaimBeforeTimestamp"] = 1722117600, ["LastUpdate"] = 16},
+        ["ExtraItems"] = {
+            ["LeafyFlowerCrown"] = {["ThemeChance"] = 1, ["Theme"] = "Random", ["Amount"] = 1},
+            ["DemonKelpie"] = {
+                ["Infertile"] = true,
+                ["Origin"] = "Developer",
+                ["Species"] = "Kelpie",
+                ["Preset"] = {"Demon"},
+                ["MaxCosmetics"] = {["MaxPatternsPerSlot"] = 4, ["MaxPaintsPerSlot"] = 2, ["MaxCosmeticsPerSlot"] = 4}
+            }
+        },
+        ["Text"] = "🎉 EXCLUSIVE RARE GIFT 🎉"
+    },
+    ["BetaTesterPack"] = {
+        ["Items"] = {["TeddyHat"] = 4, ["DiamondSparkleTrail"] = 2},
+        ["ItemType"] = "ItemSets",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["RestoreAddedItems"] = true,
+        ["Claimable"] = {["Badges"] = {"BetaTester"}},
+        ["Name"] = "BetaTesterPack",
+        ["Text"] = "💙 Thank you for playing the game during Beta! 💙"
+    }
+}
