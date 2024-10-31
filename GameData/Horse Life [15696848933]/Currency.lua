4672431f3164851877bf951646b741835ff44111
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-01 01:35:01 +1100

    Create Currency.lua

---
+++ GameData/Horse Life [15696848933]/Currency.lua
@@ -0,0 +1,45 @@
+data = {
+    ["Souls"] = {
+        ["ItemType"] = "Currency",
+        ["UIStrokeColor"] = Color3.fromRGB(78, 78, 117),
+        ["EventCurrency"] = true,
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["Image"] = "rbxassetid://80642573883441",
+        ["UIGradient"] = "SoulsGradient",
+        ["Name"] = "Souls",
+        ["DropParticleColor"] = Color3.fromRGB(255, 255, 255),
+        ["DropUIStrokeColor"] = Color3.fromRGB(78, 78, 117),
+        ["TextColor"] = Color3.fromRGB(255, 255, 255)
+    },
+    ["Coins"] = {
+        ["ItemType"] = "Currency",
+        ["UIStrokeColor"] = Color3.fromRGB(78, 78, 117),
+        ["DropUIStrokeColor"] = Color3.fromRGB(78, 78, 117),
+        ["Image"] = "rbxassetid://16190594558",
+        ["Name"] = "Coins",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["UIGradient"] = "CoinsGradient",
+        ["DropParticleColor"] = Color3.fromRGB(255, 255, 255),
+        ["TextColor"] = Color3.fromRGB(255, 255, 255)
+    }
+}
