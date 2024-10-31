19de931b9a613e7d41880737213368d137f7793b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-01 01:34:49 +1100

    Create StipendKits.lua

---
+++ GameData/Horse Life [15696848933]/StipendKits.lua
@@ -0,0 +1,35 @@
+data = {
+    ["ContentCreator"] = {
+        ["Items"] = {["PredictFoalPotion"] = 10},
+        ["Image"] = "rbxassetid://17514218413",
+        ["Role"] = "ContentCreator"
+    },
+    ["AlphaTester"] = {
+        ["Items"] = {["BlossomingBranches"] = {["Theme"] = "Random", ["Amount"] = 1}, ["GoldSparkleTrail"] = 1},
+        ["Image"] = "rbxassetid://98522978275548",
+        ["Role"] = "AlphaTester"
+    },
+    ["BetaSupporter"] = {
+        ["Items"] = {
+            ["FaeHeadwings"] = {["Theme"] = "Random", ["Amount"] = 1},
+            ["MysticSparkleTrail"] = 1,
+            ["FantasyWingPotion"] = 1
+        },
+        ["Image"] = "rbxassetid://94721386023070",
+        ["Role"] = "BetaSupporter"
+    },
+    ["BetaTester"] = {
+        ["Items"] = {["TeddyHat"] = {["Theme"] = "Random", ["Amount"] = 1}, ["DiamondSparkleTrail"] = 1},
+        ["Image"] = "rbxassetid://84868521375070",
+        ["Role"] = "BetaTester"
+    },
+    ["AlphaSupporter"] = {
+        ["Items"] = {
+            ["RainbowSparkleTrail"] = 1,
+            ["FantasyWingPotion"] = 1,
+            ["DragonSkull"] = {["Theme"] = "Random", ["Amount"] = 1}
+        },
+        ["Image"] = "rbxassetid://115050023683614",
+        ["Role"] = "AlphaSupporter"
+    }
+}
