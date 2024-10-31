81b4a80960638e1d2b0ad9a7019d818f078bbf5b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-01 01:36:27 +1100

    Create Miscellaneous.lua

---
+++ GameData/Horse Life [15696848933]/Miscellaneous.lua
@@ -0,0 +1,55 @@
+data = {
+    ["ColorDye"] = {
+        ["Image"] = "rbxthumb://type=Asset&id=17225855847&w=150&h=150",
+        ["Rarity"] = {
+            ["Name"] = "Uncommon",
+            ["RichTextColor"] = "rgb(152, 255, 167)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(19, 85, 63),
+                ["TextColor"] = Color3.fromRGB(152, 255, 167)
+            },
+            ["Index"] = 2,
+            ["TextColor"] = Color3.fromRGB(152, 255, 167)
+        },
+        ["ItemType"] = "Miscellaneous",
+        ["Name"] = "ColorDye",
+        ["Description"] = "🎨 Can be used to change the colors of accessories!"
+    },
+    ["NameTag"] = {
+        ["Image"] = "rbxthumb://type=Asset&id=16550352937&w=150&h=150",
+        ["Description"] = "✏️ Can be used in your Stables to change the name of your horse!",
+        ["ItemType"] = "Miscellaneous",
+        ["Name"] = "NameTag",
+        ["Rarity"] = {
+            ["Name"] = "Legendary",
+            ["RichTextColor"] = "rgb(255, 204, 103)",
+            ["ItemStream"] = {
+                ["Sound"] = "ItemStreamPickUp",
+                ["BackgroundColor"] = Color3.fromRGB(217, 104, 29),
+                ["TextColor"] = Color3.fromRGB(255, 204, 103)
+            },
+            ["Index"] = 5,
+            ["TextColor"] = Color3.fromRGB(255, 204, 103)
+        }
+    },
+    ["GhoulsteedBreedingLicense"] = {
+        ["Image"] = "rbxthumb://type=Asset&id=78741610385433&w=150&h=150",
+        ["Description"] = "👻 Grants permission to be able to breed Ghoulsteeds!",
+        ["ItemType"] = "Miscellaneous",
+        ["Name"] = "GhoulsteedBreedingLicense",
+        ["Tradelocked"] = true,
+        ["Deletelocked"] = true,
+        ["Rarity"] = {
+            ["Name"] = "Halloween",
+            ["RichTextColor"] = "rgb(213, 91, 9)",
+            ["ItemStream"] = {
+                ["Sound"] = "ItemStreamPickUp",
+                ["BackgroundColor"] = Color3.fromRGB(255, 89, 33),
+                ["TextColor"] = Color3.fromRGB(85, 85, 255)
+            },
+            ["Index"] = 7,
+            ["TextColor"] = Color3.fromRGB(85, 85, 255)
+        }
+    }
+}
