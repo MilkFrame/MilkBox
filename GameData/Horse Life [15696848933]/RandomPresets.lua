97eed315a2b1c955ffc46a545b1b100bb4b8896f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-01 01:35:31 +1100

    Create RandomPresets.lua

---
+++ GameData/Horse Life [15696848933]/RandomPresets.lua
@@ -0,0 +1,70 @@
+data = {
+    ["RandomFoal"] = {
+        ["Image"] = "rbxassetid://17858792839",
+        ["Name"] = "RandomFoal",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["ItemType"] = "RandomPresets",
+        ["Description"] = "A random foal of any non exclusive species!"
+    },
+    ["RandomEquipment"] = {
+        ["Image"] = "rbxassetid://17448250690",
+        ["Name"] = "RandomEquipment",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["ItemType"] = "RandomPresets",
+        ["Description"] = "Any droppable equipment that isn't an exclusive item!"
+    },
+    ["RandomFlair"] = {
+        ["Image"] = "rbxassetid://17448250476",
+        ["Name"] = "RandomFlair",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["ItemType"] = "RandomPresets",
+        ["Description"] = "Any droppable flair that isn't an exclusive item!"
+    },
+    ["RandomPalette"] = {
+        ["Image"] = "rbxassetid://17734736508",
+        ["Name"] = "RandomPalette",
+        ["Rarity"] = {
+            ["Index"] = 1,
+            ["RichTextColor"] = "rgb(212, 212, 212)",
+            ["ItemStream"] = {
+                ["Sound"] = "QuestIncrement",
+                ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+                ["TextColor"] = Color3.fromRGB(222, 191, 172)
+            },
+            ["Name"] = "Common",
+            ["TextColor"] = Color3.fromRGB(212, 212, 212)
+        },
+        ["ItemType"] = "RandomPresets",
+        ["Description"] = "Any droppable palette that isn't an exclusive item!"
+    }
+}
