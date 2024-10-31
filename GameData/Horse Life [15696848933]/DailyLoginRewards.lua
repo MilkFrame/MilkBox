17e3ba03fc4ede1efbd70185c8d368fe012622fb
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-01 03:08:43 +1100

    Create DailyLoginRewards.lua

---
+++ GameData/Horse Life [15696848933]/DailyLoginRewards.lua
@@ -0,0 +1,59 @@
+data = {
+    {
+        {["P"] = 25, ["Name"] = "AppleMuffin", ["Amount"] = 3},
+        {["P"] = 25, ["Name"] = "CarrotMuffin", ["Amount"] = 3},
+        {["P"] = 25, ["Name"] = "MintMuffin", ["Amount"] = 3},
+        {["P"] = 25, ["Name"] = "OatMuffin", ["Amount"] = 3},
+        {["P"] = 25, ["Name"] = "SugarMuffin", ["Amount"] = 3}
+    },
+    {
+        {["P"] = 25, ["Name"] = "WovenLasso", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "RopeLasso", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "BrightLasso", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "WeakMagicLasso", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "StoneLasso", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "CloudLasso", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "KelpLasso", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "WebLasso", ["Amount"] = 2}
+    },
+    {
+        {["P"] = 25, ["Name"] = "BasicFeed", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "GoodFeed", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "PremiumFeed", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "ExclusiveFeed", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "MysticFeed", ["Amount"] = 2}
+    },
+    {
+        {["Theme"] = "Random", ["P"] = 25, ["Name"] = "RandomEquipment", ["Amount"] = 1},
+        {["P"] = 25, ["Name"] = "GoodSoap", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "GoodBrush", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "GoodHose", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "BerryOnAStick", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "MagicFairyNet", ["Amount"] = 2}
+    },
+    {
+        {["P"] = 25, ["Name"] = "WovenLasso", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "RopeLasso", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "BrightLasso", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "WeakMagicLasso", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "StoneLasso", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "CloudLasso", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "KelpLasso", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "WebLasso", ["Amount"] = 2}
+    },
+    {
+        {["P"] = 25, ["Name"] = "BasicFeed", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "GoodFeed", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "PremiumFeed", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "ExclusiveFeed", ["Amount"] = 2},
+        {["P"] = 25, ["Name"] = "MysticFeed", ["Amount"] = 2}
+    },
+    {
+        {["P"] = 25, ["Name"] = "NameTag", ["Amount"] = 1},
+        {["P"] = 25, ["Name"] = "MutationDeletePotion", ["Amount"] = 1},
+        {["P"] = 25, ["Name"] = "RestingWingPotion", ["Amount"] = 1},
+        {["P"] = 25, ["Name"] = "LiftedWingPotion", ["Amount"] = 1},
+        {["P"] = 25, ["Name"] = "ColorDye", ["Amount"] = 1},
+        {["P"] = 25, ["Name"] = "WingUpgradePotion", ["Amount"] = 1}
+    }
+}
