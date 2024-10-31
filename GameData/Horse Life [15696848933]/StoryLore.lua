ce05b93f013b15e2809f0d51442f25a32408f758
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-01 03:07:08 +1100

    Create StoryLore.lua

---
+++ GameData/Horse Life [15696848933]/StoryLore.lua
@@ -0,0 +1,107 @@
+data = {
+    ["Clue3"] = {
+        ["Text"] = "This journal and pencil must be mine. This journal is shredded and few pages remain. The pencil also shows sign of previous use, I wonder where the rest of the pages are? might lead me to hints about who I am.",
+        ["Name"] = "Journal & Pencil",
+        ["Choice"] = "Interesting..."
+    },
+    ["Clue1"] = {
+        ["Text"] = "Owch, I remember this! When I woke up it felt like my head was pounding relentlessly. I have some bruises on my elbows to show for my tough landing. The marks appear immediately from this odd structure.",
+        ["Name"] = "Muddy Slide Marks",
+        ["Choice"] = "Interesting..."
+    },
+    ["Clue4"] = {
+        ["Text"] = "These odd purple jewels incite no memory, they are vaguely omitting a weak magic signal but it is quickly evaporating through signs of shattering from my rough tumble.",
+        ["Name"] = "Strange Jewels",
+        ["Choice"] = "Interesting..."
+    },
+    ["Statue3"] = {
+        ["Text"] = "Wiping my hand across the remnants of magic I’m overwhelmed with a memory of flying over a field of overwhelmingly bright flowers, their vivid gradients blur together while I glide across them. I sense a strong connection to magic and nature, yet this feeling quickly evaporates, something is wrong. ",
+        ["Name"] = "Magic Remnants",
+        ["Choice"] = "Interesting..."
+    },
+    ["Hoof"] = {
+        ["Text"] = "The foal's body has a few scratches and bruises as if it fell down. Hopefully nothing serious is wrong with the baby. Is this my fault?",
+        ["Name"] = "Injured Foal",
+        ["Choice"] = "I'm sorry.."
+    },
+    ["Top"] = {
+        ["Text"] = "The foal’s head looks fine, the poor foal is frightened and doesn’t trust me. Its ears are pinned back and it’s uncertain of me.",
+        ["Name"] = "Injured Foal",
+        ["Choice"] = "Poor thing.."
+    },
+    ["AetherisSpawn"] = {
+        ["Text"] = "The crystal emanates a strange, ethereal aura. The ground trembles beneath your feet as you sense an ominous presence behind you.",
+        ["Name"] = "Strange Crystal",
+        ["Choice"] = "*worried*"
+    },
+    ["LocateAetheris"] = {
+        ["Text"] = "H-human child… You must… learn to care for… this land. Heal it, and help… recover… all that has been lost. He must not attain victory",
+        ["Name"] = "Aetheris",
+        ["Choice"] = {
+            {
+                ["Choices"] = {
+                    {
+                        ["Choices"] = {
+                            {
+                                ["Choices"] = {
+                                    {
+                                        ["Choices"] = {
+                                            {
+                                                ["Choices"] = {
+                                                    {
+                                                        ["OnRun"] = "function: 0x6ad7d4c48b290ff3",
+                                                        ["Text"] = "I suppose I'll do what I can."
+                                                    }
+                                                },
+                                                ["Text"] = "Find the others…a journey, ME?",
+                                                ["Dialog"] = "The… bonds and magic we once shared… they are fading. We need… we need someone… someone who can restore…"
+                                            }
+                                        },
+                                        ["Text"] = " How can I do anything, I’m just a kid?",
+                                        ["Dialog"] = "You… have no memories, but… an undeniable… understanding of magic. We… we cannot fully… reach you. Find the others… and begin… your journey."
+                                    }
+                                },
+                                ["Text"] = " I don’t understand, What’s going on!?",
+                                ["Dialog"] = "Aetheron… is no longer… what it once was. I… I have little time… Fix the cracks… mend all bonds… broken in this world."
+                            }
+                        },
+                        ["Text"] = "A spirit horse?",
+                        ["Dialog"] = "Very few of us remain, even fewer with our minds in tact... We spirit horses are magical entities."
+                    }
+                },
+                ["Text"] = "AHHHH! WHO ARE YOU?",
+                ["Dialog"] = "Aetheris. Keeper of time and history… I am but a dying spirit horse"
+            }
+        }
+    },
+    ["HorseDialogue"] = {
+        ["Text"] = {
+            "Hey there little guy I’m… Well that’s not important, don’t be afraid I’m a friend! I’m just as scared as you are. Where’s your parents, fella? Are you lost like me?",
+            "Yeah atta horsie! I’m a little injured and lost myself, maybe we should stick together if you’ll give me a chance to be your friend!",
+            "It looks like you got an injury on your foot, I have some torn clothing I'll take care of it. I really hope this wasn't my fault.",
+            "Oh what was that, we can be friends, really? Well I’m sure I can get you some snacks to make you feel a bit better..I’ll take care of you from now on!"
+        },
+        ["Name"] = "Player",
+        ["Choice"] = "Let's find some treats!"
+    },
+    ["Bottom"] = {
+        ["Text"] = "The foals' front leg look a bit twisted, they must have fallen and landed funny. When I reach close to the foal’s hoof it pulls away and whinnies at me in contempt.",
+        ["Name"] = "Injured Foal",
+        ["Choice"] = "Oh yikes!"
+    },
+    ["Clue2"] = {
+        ["Text"] = "This must be my backpack as well, it’s been torn from my fall, it seems to have been violently ripped open and the contents are spilled out.",
+        ["Name"] = "Ripped Backpack",
+        ["Choice"] = "Interesting..."
+    },
+    ["Statue1"] = {
+        ["Text"] = "I gently place my hand on the horse totem, very vague images flash in my mind, horses racing, its so blurry. Loud voices yelling overlap, I don’t remember anything. Strong energies of magic and power overwhelm me and I fall backwards. What was that?",
+        ["Name"] = "Strange Totem",
+        ["Choice"] = "Interesting..."
+    },
+    ["Statue2"] = {
+        ["Text"] = "Placing my hand on a brick of the column, deep within I sense magical energies thrashing and darting within. The magic feels erratic and unnatural. Though no cracks can be seen in the structure, the energy seems to be seeping through tiny unseen holes.",
+        ["Name"] = "Cracked Structure",
+        ["Choice"] = "Interesting..."
+    }
+}
