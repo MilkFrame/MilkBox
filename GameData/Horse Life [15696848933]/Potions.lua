32bfa1f660a3eaae98084411f3255250f8390836
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-01 01:37:16 +1100

    Create Potions.lua

---
+++ GameData/Horse Life [15696848933]/Potions.lua
@@ -0,0 +1,375 @@
+data = {
+    ["TraitPotions"] = {},
+    ["BreedPotions"] = {
+        ["InfertilityPotion"] = {
+            ["ToolType"] = "Consumable",
+            ["Image"] = "rbxthumb://type=Asset&id=17210150322&w=150&h=150",
+            ["Rarity"] = {
+                ["Name"] = "Epic",
+                ["RichTextColor"] = "rgb(223, 160, 255)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(89, 42, 122),
+                    ["TextColor"] = Color3.fromRGB(223, 160, 255)
+                },
+                ["Index"] = 4,
+                ["TextColor"] = Color3.fromRGB(223, 160, 255)
+            },
+            ["ItemType"] = "Potions",
+            ["Name"] = "InfertilityPotion",
+            ["ConsumableType"] = "Infertility",
+            ["Description"] = "💔 Makes your horse unable to breed <b>PERMANENTLY!</b>"
+        },
+        ["InstantFoalPotion"] = {
+            ["ToolType"] = "Consumable",
+            ["Image"] = "rbxthumb://type=Asset&id=17372998233&w=150&h=150",
+            ["Rarity"] = {
+                ["Name"] = "Epic",
+                ["RichTextColor"] = "rgb(223, 160, 255)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(89, 42, 122),
+                    ["TextColor"] = Color3.fromRGB(223, 160, 255)
+                },
+                ["Index"] = 4,
+                ["TextColor"] = Color3.fromRGB(223, 160, 255)
+            },
+            ["ItemType"] = "Potions",
+            ["Name"] = "InstantFoalPotion",
+            ["ConsumableType"] = "InstantFoal",
+            ["Description"] = "⌛ Instantly reduce the birth time for your horse!"
+        },
+        ["BigenderPotion"] = {
+            ["Description"] = [[💘 Lets you use a horse in either gender slot! 
+<b>*ℹ️</b><b><i> Will have the same cooldown as Female horses. </i></b>]],
+            ["ConsumableType"] = "Bigender",
+            ["DisplayName"] = "BiGenderPotion",
+            ["ToolType"] = "Consumable",
+            ["Image"] = "rbxthumb://type=Asset&id=18879025727&w=150&h=150",
+            ["Tradelocked"] = true,
+            ["ItemType"] = "Potions",
+            ["Rarity"] = {
+                ["Name"] = "Legendary",
+                ["RichTextColor"] = "rgb(255, 204, 103)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(217, 104, 29),
+                    ["TextColor"] = Color3.fromRGB(255, 204, 103)
+                },
+                ["Index"] = 5,
+                ["TextColor"] = Color3.fromRGB(255, 204, 103)
+            },
+            ["Name"] = "BigenderPotion"
+        },
+        ["PredictFoalPotion"] = {
+            ["Image"] = "rbxthumb://type=Asset&id=17461086157&w=150&h=150",
+            ["Tradelocked"] = true,
+            ["ItemType"] = "Potions",
+            ["Name"] = "PredictFoalPotion",
+            ["Description"] = [[💘 Allows you to claim a predicted breed outcome! 
+<b>*ℹ️</b><b><i> The female horse becomes pregnant with the prediction</i></b>]],
+            ["Rarity"] = {
+                ["Name"] = "Legendary",
+                ["RichTextColor"] = "rgb(255, 204, 103)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(217, 104, 29),
+                    ["TextColor"] = Color3.fromRGB(255, 204, 103)
+                },
+                ["Index"] = 5,
+                ["TextColor"] = Color3.fromRGB(255, 204, 103)
+            }
+        },
+        ["GenderSwapPotion"] = {
+            ["Description"] = [[💘 Allows you to swap a horse gender! 
+<b>*ℹ️</b><b><i> Female horses cannot have a foal when using this. </i></b>]],
+            ["ConsumableType"] = "GenderSwap",
+            ["DisplayName"] = "GenderPotion",
+            ["ToolType"] = "Consumable",
+            ["Image"] = "rbxthumb://type=Asset&id=18879024029&w=150&h=150",
+            ["Tradelocked"] = true,
+            ["ItemType"] = "Potions",
+            ["Rarity"] = {
+                ["Name"] = "Legendary",
+                ["RichTextColor"] = "rgb(255, 204, 103)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(217, 104, 29),
+                    ["TextColor"] = Color3.fromRGB(255, 204, 103)
+                },
+                ["Index"] = 5,
+                ["TextColor"] = Color3.fromRGB(255, 204, 103)
+            },
+            ["Name"] = "GenderSwapPotion"
+        }
+    },
+    ["OtherPotions"] = {
+        ["LiftedWingPotion"] = {
+            ["ToolType"] = "Consumable",
+            ["Image"] = "rbxthumb://type=Asset&id=18669908989&w=150&h=150",
+            ["Rarity"] = {
+                ["Name"] = "Exclusive",
+                ["RichTextColor"] = "rgb(138, 102, 255)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(118, 37, 217),
+                    ["TextColor"] = Color3.fromRGB(138, 102, 255)
+                },
+                ["Index"] = 6,
+                ["TextColor"] = Color3.fromRGB(138, 102, 255)
+            },
+            ["ItemType"] = "Potions",
+            ["Name"] = "LiftedWingPotion",
+            ["ConsumableType"] = "UpliftedWings",
+            ["Description"] = "🎬 Can be used to change the idle wing animation to be <b>Uplifted!</b>"
+        },
+        ["RestingWingPotion"] = {
+            ["ToolType"] = "Consumable",
+            ["Image"] = "rbxthumb://type=Asset&id=18669911104&w=150&h=150",
+            ["Rarity"] = {
+                ["Name"] = "Exclusive",
+                ["RichTextColor"] = "rgb(138, 102, 255)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(118, 37, 217),
+                    ["TextColor"] = Color3.fromRGB(138, 102, 255)
+                },
+                ["Index"] = 6,
+                ["TextColor"] = Color3.fromRGB(138, 102, 255)
+            },
+            ["ItemType"] = "Potions",
+            ["Name"] = "RestingWingPotion",
+            ["ConsumableType"] = "UpliftedWings",
+            ["Description"] = "🎬 Can be used to change the idle wing animation to be <b>Rested!</b>"
+        }
+    },
+    ["BoostPotions"] = {
+        ["TraitBoostPotion"] = {
+            ["Description"] = "🎨 Boost the chances of traits transferring during breeding!",
+            ["ModifierStats"] = {["TraitInheritanceMultiplier"] = 1.25},
+            ["Duration"] = 1800,
+            ["ConsumableType"] = "Boost",
+            ["ToolType"] = "Consumable",
+            ["Image"] = "rbxthumb://type=Asset&id=17823143260&w=150&h=150",
+            ["Name"] = "TraitBoostPotion",
+            ["Rarity"] = {
+                ["Name"] = "Epic",
+                ["RichTextColor"] = "rgb(223, 160, 255)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(89, 42, 122),
+                    ["TextColor"] = Color3.fromRGB(223, 160, 255)
+                },
+                ["Index"] = 4,
+                ["TextColor"] = Color3.fromRGB(223, 160, 255)
+            },
+            ["ItemType"] = "Potions"
+        },
+        ["TestBoostPotion"] = {
+            ["ToolType"] = "Consumable",
+            ["Image"] = "rbxthumb://type=Asset&id=16841268147&w=150&h=150",
+            ["Rarity"] = {
+                ["Name"] = "Epic",
+                ["RichTextColor"] = "rgb(223, 160, 255)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(89, 42, 122),
+                    ["TextColor"] = Color3.fromRGB(223, 160, 255)
+                },
+                ["Index"] = 4,
+                ["TextColor"] = Color3.fromRGB(223, 160, 255)
+            },
+            ["ItemType"] = "Potions",
+            ["Name"] = "TestBoostPotion",
+            ["Duration"] = 3600,
+            ["ConsumableType"] = "Boost",
+            ["Description"] = "🎨 Boost the chances of traits transferring during breeding!"
+        }
+    },
+    ["MutationPotions"] = {
+        ["HauntedMutationPotion"] = {
+            ["Description"] = "🧬 Can be used on your horse to give the <b>Halloween</b> Mutations!",
+            ["QuickfeedDesc"] = "Rewards a random Halloween Mutation",
+            ["ConsumableType"] = "RandomMutation",
+            ["ToolType"] = "Consumable",
+            ["Image"] = "rbxthumb://type=Asset&id=80600061615152&w=150&h=150",
+            ["Name"] = "HauntedMutationPotion",
+            ["ItemType"] = "Potions",
+            ["Rarity"] = {
+                ["Name"] = "Halloween",
+                ["RichTextColor"] = "rgb(213, 91, 9)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(255, 89, 33),
+                    ["TextColor"] = Color3.fromRGB(85, 85, 255)
+                },
+                ["Index"] = 7,
+                ["TextColor"] = Color3.fromRGB(85, 85, 255)
+            },
+            ["TargetMutationType"] = "Halloween"
+        },
+        ["MutationDeletePotion"] = {
+            ["ToolType"] = "Consumable",
+            ["Image"] = "rbxthumb://type=Asset&id=18195547700&w=150&h=150",
+            ["Description"] = "🧬❌ Can be used to <b>delete</b> a mutation off your horse!",
+            ["ItemType"] = "Potions",
+            ["Name"] = "MutationDeletePotion",
+            ["Rarity"] = {
+                ["Name"] = "Legendary",
+                ["RichTextColor"] = "rgb(255, 204, 103)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(217, 104, 29),
+                    ["TextColor"] = Color3.fromRGB(255, 204, 103)
+                },
+                ["Index"] = 5,
+                ["TextColor"] = Color3.fromRGB(255, 204, 103)
+            }
+        },
+        ["SpookyWingPotion"] = {
+            ["Description"] = "🧬 Can be used on your horse to give the <b>Halloween</b> Wing Mutations!",
+            ["SpecificPullTable"] = {
+                "DemonWings",
+                "SkeletalDragonWing",
+                "TornDragonWings",
+                "ReaperWings",
+                "SkeletonWings",
+                "CreatureWings"
+            },
+            ["ConsumableType"] = "RandomMutation",
+            ["ToolType"] = "Consumable",
+            ["Image"] = "rbxthumb://type=Asset&id=70744521193368&w=150&h=150",
+            ["Name"] = "SpookyWingPotion",
+            ["ItemType"] = "Potions",
+            ["Rarity"] = {
+                ["Name"] = "Halloween",
+                ["RichTextColor"] = "rgb(213, 91, 9)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(255, 89, 33),
+                    ["TextColor"] = Color3.fromRGB(85, 85, 255)
+                },
+                ["Index"] = 7,
+                ["TextColor"] = Color3.fromRGB(85, 85, 255)
+            },
+            ["TargetMutationType"] = "Wings"
+        },
+        ["WingUpgradePotion"] = {
+            ["ToolType"] = "Consumable",
+            ["Image"] = "rbxthumb://type=Asset&id=16841268147&w=150&h=150",
+            ["Rarity"] = {
+                ["Name"] = "Epic",
+                ["RichTextColor"] = "rgb(223, 160, 255)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(89, 42, 122),
+                    ["TextColor"] = Color3.fromRGB(223, 160, 255)
+                },
+                ["Index"] = 4,
+                ["TextColor"] = Color3.fromRGB(223, 160, 255)
+            },
+            ["ItemType"] = "Potions",
+            ["Name"] = "WingUpgradePotion",
+            ["TargetMutationType"] = "Wings",
+            ["ConsumableType"] = "UpgradeMutation",
+            ["Description"] = [[🧬 Can be used to upgrade Wing Mutations! 
+<b>*⚠️</b><b><i> can only be used on horses that already have the mutation</i></b>]]
+        },
+        ["RandomWingPotion"] = {
+            ["ToolType"] = "Consumable",
+            ["Image"] = "rbxthumb://type=Asset&id=16841276193&w=150&h=150",
+            ["Rarity"] = {
+                ["Name"] = "Legendary",
+                ["RichTextColor"] = "rgb(255, 204, 103)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(217, 104, 29),
+                    ["TextColor"] = Color3.fromRGB(255, 204, 103)
+                },
+                ["Index"] = 5,
+                ["TextColor"] = Color3.fromRGB(255, 204, 103)
+            },
+            ["ItemType"] = "Potions",
+            ["Name"] = "RandomWingPotion",
+            ["TargetMutationType"] = "Wings",
+            ["ConsumableType"] = "RandomMutation",
+            ["Description"] = "🧬 Can be used on your horse to give a random Wing Mutation!"
+        },
+        ["MutationPotion"] = {
+            ["Image"] = "rbxthumb://type=Asset&id=18240700876&w=150&h=150",
+            ["Description"] = "🧬♻️ Can be used to roll a random mutation on your horse!",
+            ["ItemType"] = "Potions",
+            ["Name"] = "MutationPotion",
+            ["Rarity"] = {
+                ["Name"] = "Legendary",
+                ["RichTextColor"] = "rgb(255, 204, 103)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(217, 104, 29),
+                    ["TextColor"] = Color3.fromRGB(255, 204, 103)
+                },
+                ["Index"] = 5,
+                ["TextColor"] = Color3.fromRGB(255, 204, 103)
+            }
+        },
+        ["SakuraWingPotion"] = {
+            ["ToolType"] = "Consumable",
+            ["Image"] = "rbxthumb://type=Asset&id=108957572859329&w=150&h=150",
+            ["Rarity"] = {
+                ["Name"] = "Exclusive",
+                ["RichTextColor"] = "rgb(138, 102, 255)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(118, 37, 217),
+                    ["TextColor"] = Color3.fromRGB(138, 102, 255)
+                },
+                ["Index"] = 6,
+                ["TextColor"] = Color3.fromRGB(138, 102, 255)
+            },
+            ["ItemType"] = "Potions",
+            ["TargetMutation"] = "SakuraWings",
+            ["Name"] = "SakuraWingPotion",
+            ["ConsumableType"] = "Mutation",
+            ["Description"] = "🧬 Can be used on your horse to give the <b>Sakura Wings</b> Mutation!"
+        },
+        ["MutationSlotPotion"] = {
+            ["ToolType"] = "Consumable",
+            ["Image"] = "rbxthumb://type=Asset&id=97352271797248&w=150&h=150",
+            ["Rarity"] = {
+                ["Name"] = "Legendary",
+                ["RichTextColor"] = "rgb(255, 204, 103)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(217, 104, 29),
+                    ["TextColor"] = Color3.fromRGB(255, 204, 103)
+                },
+                ["Index"] = 5,
+                ["TextColor"] = Color3.fromRGB(255, 204, 103)
+            },
+            ["ItemType"] = "Potions",
+            ["Name"] = "MutationSlotPotion",
+            ["Tradelocked"] = true,
+            ["Description"] = "🧬➕ Can be used to add an empty mutation slot to your horse!"
+        },
+        ["FantasyWingPotion"] = {
+            ["ToolType"] = "Consumable",
+            ["Image"] = "rbxthumb://type=Asset&id=17560159838&w=150&h=150",
+            ["Rarity"] = {
+                ["Name"] = "Exclusive",
+                ["RichTextColor"] = "rgb(138, 102, 255)",
+                ["ItemStream"] = {
+                    ["Sound"] = "ItemStreamPickUp",
+                    ["BackgroundColor"] = Color3.fromRGB(118, 37, 217),
+                    ["TextColor"] = Color3.fromRGB(138, 102, 255)
+                },
+                ["Index"] = 6,
+                ["TextColor"] = Color3.fromRGB(138, 102, 255)
+            },
+            ["ItemType"] = "Potions",
+            ["TargetMutation"] = "FantasyWings",
+            ["Name"] = "FantasyWingPotion",
+            ["ConsumableType"] = "Mutation",
+            ["Description"] = "🧬 Can be used on your horse to give the <b>Fantasy Wings</b> Mutation!"
+        }
+    }
+}
