6e999f7ae5d2bf205fbb2ad52e05076bf18e2429
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-01 03:07:58 +1100

    Create ResourceData.lua

---
+++ GameData/Horse Life [15696848933]/ResourceData.lua
@@ -0,0 +1,141 @@
+data = {
+    ["Stump"] = {
+        ["Image"] = "rbxassetid://15020968482",
+        ["Drops"] = {
+            {["P"] = 50, ["Name"] = "Peppermint", ["Amount"] = 1},
+            {["P"] = 25, ["Name"] = "Strawberry", ["Amount"] = 1},
+            {["P"] = 10, ["Name"] = "Pineberry", ["Amount"] = 1},
+            {["P"] = 50, ["Name"] = "Carrot", ["Amount"] = 1},
+            {["P"] = 25, ["Name"] = "WhiteCarrot", ["Amount"] = 1}
+        },
+        ["Value"] = 35,
+        ["StrengthXP"] = 20,
+        ["Respawn"] = 180,
+        ["Color"] = Color3.fromRGB(255, 81, 0)
+    },
+    ["FoodPallet"] = {
+        ["Image"] = "rbxassetid://15020968482",
+        ["Drops"] = {
+            {["P"] = 75, ["Name"] = "Apple", ["Amount"] = 1},
+            {["P"] = 75, ["Name"] = "Mints", ["Amount"] = 1},
+            {["P"] = 50, ["Name"] = "Carrot", ["Amount"] = 1},
+            {["P"] = 25, ["Name"] = "WhiteCarrot", ["Amount"] = 1},
+            {["P"] = 10, ["Name"] = "OatTreats", ["Amount"] = 1},
+            {["P"] = 50, ["Name"] = "SugarCubes", ["Amount"] = 1},
+            {["P"] = 25, ["Name"] = "AlfalfaCubes", ["Amount"] = 1},
+            {["P"] = 50, ["Name"] = "HayCubes", ["Amount"] = 1},
+            {["P"] = 10, ["Name"] = "Peach", ["Amount"] = 1},
+            {["P"] = 5, ["Name"] = "WhitePeach", ["Amount"] = 1}
+        },
+        ["Value"] = 10,
+        ["StrengthXP"] = 15,
+        ["Respawn"] = 180,
+        ["Color"] = Color3.fromRGB(255, 81, 0)
+    },
+    ["AppleBarrel"] = {
+        ["Image"] = "rbxassetid://15020968482",
+        ["Drops"] = {
+            {["P"] = 75, ["Name"] = "Apple", ["Amount"] = 1},
+            {["P"] = 25, ["Name"] = "GreenApple", ["Amount"] = 1},
+            {["P"] = 10, ["Name"] = "PinkPrincessApple", ["Amount"] = 1},
+            {["P"] = 0.25, ["Name"] = "AppleBarrel", ["Amount"] = 1}
+        },
+        ["Value"] = 5,
+        ["StrengthXP"] = 5,
+        ["Respawn"] = 60,
+        ["Color"] = Color3.fromRGB(255, 81, 0)
+    },
+    ["StoneDeposit"] = {
+        ["Image"] = "rbxassetid://15020968482",
+        ["Drops"] = {{["P"] = 100, ["Name"] = "Stone", ["Amount"] = 1}},
+        ["Value"] = 50,
+        ["StrengthXP"] = 25,
+        ["Respawn"] = 120,
+        ["Color"] = Color3.fromRGB(255, 81, 0)
+    },
+    ["BerryBush"] = {
+        ["Image"] = "rbxassetid://15020968482",
+        ["Drops"] = {
+            {["P"] = 25, ["Name"] = "Strawberry", ["Amount"] = 1},
+            {["P"] = 10, ["Name"] = "Peach", ["Amount"] = 1},
+            {["P"] = 5, ["Name"] = "WhitePeach", ["Amount"] = 1},
+            {["P"] = 10, ["Name"] = "Pineberry", ["Amount"] = 1},
+            {["P"] = 75, ["Name"] = "Daylilies", ["Amount"] = 1},
+            {["P"] = 75, ["Name"] = "Pansies", ["Amount"] = 1},
+            {["P"] = 10, ["Name"] = "BrightPansies", ["Amount"] = 1},
+            {["P"] = 50, ["Name"] = "Peppermint", ["Amount"] = 1},
+            {["P"] = 25, ["Name"] = "VibrantDaylilies", ["Amount"] = 1}
+        },
+        ["Value"] = 20,
+        ["StrengthXP"] = 10,
+        ["Respawn"] = 180,
+        ["Color"] = Color3.fromRGB(255, 81, 0)
+    },
+    ["Treasure"] = {
+        ["Image"] = "rbxassetid://15020968482",
+        ["Drops"] = {
+            {["P"] = 95, ["Name"] = "Coins", ["Amount"] = 50},
+            {["P"] = 50, ["Name"] = "StringLasso", ["Amount"] = 1},
+            {["P"] = 25, ["Name"] = "WovenLasso", ["Amount"] = 1}
+        },
+        ["Value"] = 50,
+        ["StrengthXP"] = 15,
+        ["Respawn"] = 420,
+        ["Color"] = Color3.fromRGB(255, 81, 0)
+    },
+    ["LargeBerryBush"] = {
+        ["Image"] = "rbxassetid://15020968482",
+        ["Drops"] = {
+            {["P"] = 7.5, ["Name"] = "Peach", ["Amount"] = 1},
+            {["P"] = 3.5, ["Name"] = "WhitePeach", ["Amount"] = 1},
+            {["P"] = 15, ["Name"] = "Strawberry", ["Amount"] = 1},
+            {["P"] = 10, ["Name"] = "Pineberry", ["Amount"] = 1},
+            {["P"] = 30, ["Name"] = "Daylilies", ["Amount"] = 1},
+            {["P"] = 30, ["Name"] = "Pansies", ["Amount"] = 1},
+            {["P"] = 10, ["Name"] = "BrightPansies", ["Amount"] = 1},
+            {["P"] = 20, ["Name"] = "Peppermint", ["Amount"] = 1},
+            {["P"] = 15, ["Name"] = "VibrantDaylilies", ["Amount"] = 1}
+        },
+        ["Value"] = 75,
+        ["StrengthXP"] = 25,
+        ["Respawn"] = 240,
+        ["Color"] = Color3.fromRGB(81, 0, 255)
+    },
+    ["FallenTree"] = {
+        ["Image"] = "rbxassetid://15020968482",
+        ["Drops"] = {{["P"] = 100, ["Name"] = "Wood", ["Amount"] = 1}},
+        ["Value"] = 25,
+        ["StrengthXP"] = 20,
+        ["Respawn"] = 120,
+        ["Color"] = Color3.fromRGB(255, 81, 0)
+    },
+    ["CactusFruit"] = {
+        ["Image"] = "rbxassetid://15020968482",
+        ["Drops"] = {
+            {["P"] = 25, ["Name"] = "Strawberry", ["Amount"] = 1},
+            {["P"] = 10, ["Name"] = "Pineberry", ["Amount"] = 1},
+            {["P"] = 75, ["Name"] = "Daylilies", ["Amount"] = 1},
+            {["P"] = 75, ["Name"] = "Pansies", ["Amount"] = 1},
+            {["P"] = 10, ["Name"] = "BrightPansies", ["Amount"] = 1},
+            {["P"] = 25, ["Name"] = "VibrantDaylilies", ["Amount"] = 1}
+        },
+        ["Value"] = 60,
+        ["StrengthXP"] = 25,
+        ["Respawn"] = 360,
+        ["Color"] = Color3.fromRGB(81, 0, 255)
+    },
+    ["SilkBush"] = {
+        ["Image"] = "rbxassetid://15020968482",
+        ["Drops"] = {
+            {["P"] = 50, ["Name"] = "Peppermint", ["Amount"] = 1},
+            {["P"] = 30, ["Name"] = "Meadowsweet", ["Amount"] = 1},
+            {["P"] = 40, ["Name"] = "Larvae", ["Amount"] = 1},
+            {["P"] = 5, ["Name"] = "RareLarvae", ["Amount"] = 1},
+            {["P"] = 1, ["Name"] = "DivineLarvae", ["Amount"] = 1}
+        },
+        ["Value"] = 200,
+        ["StrengthXP"] = 25,
+        ["Respawn"] = 360,
+        ["Color"] = Color3.fromRGB(81, 0, 255)
+    }
+}
