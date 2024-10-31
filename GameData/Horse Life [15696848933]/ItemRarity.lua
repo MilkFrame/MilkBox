3509bb25fbc2b31f45e4aa1d594c15fcf54e6d20
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-01 01:35:16 +1100

    Create ItemRarity.lua

---
+++ GameData/Horse Life [15696848933]/ItemRarity.lua
@@ -0,0 +1,79 @@
+data = {
+    ["Legendary"] = {
+        ["Name"] = "Legendary",
+        ["RichTextColor"] = "rgb(255, 204, 103)",
+        ["ItemStream"] = {
+            ["Sound"] = "ItemStreamPickUp",
+            ["BackgroundColor"] = Color3.fromRGB(217, 104, 29),
+            ["TextColor"] = Color3.fromRGB(255, 204, 103)
+        },
+        ["Index"] = 5,
+        ["TextColor"] = Color3.fromRGB(255, 204, 103)
+    },
+    ["Halloween"] = {
+        ["Name"] = "Halloween",
+        ["RichTextColor"] = "rgb(213, 91, 9)",
+        ["ItemStream"] = {
+            ["Sound"] = "ItemStreamPickUp",
+            ["BackgroundColor"] = Color3.fromRGB(255, 89, 33),
+            ["TextColor"] = Color3.fromRGB(85, 85, 255)
+        },
+        ["Index"] = 7,
+        ["TextColor"] = Color3.fromRGB(85, 85, 255)
+    },
+    ["Common"] = {
+        ["Index"] = 1,
+        ["RichTextColor"] = "rgb(212, 212, 212)",
+        ["ItemStream"] = {
+            ["Sound"] = "QuestIncrement",
+            ["BackgroundColor"] = Color3.fromRGB(45, 32, 26),
+            ["TextColor"] = Color3.fromRGB(222, 191, 172)
+        },
+        ["Name"] = "Common",
+        ["TextColor"] = Color3.fromRGB(212, 212, 212)
+    },
+    ["Epic"] = {
+        ["Name"] = "Epic",
+        ["RichTextColor"] = "rgb(223, 160, 255)",
+        ["ItemStream"] = {
+            ["Sound"] = "ItemStreamPickUp",
+            ["BackgroundColor"] = Color3.fromRGB(89, 42, 122),
+            ["TextColor"] = Color3.fromRGB(223, 160, 255)
+        },
+        ["Index"] = 4,
+        ["TextColor"] = Color3.fromRGB(223, 160, 255)
+    },
+    ["Exclusive"] = {
+        ["Name"] = "Exclusive",
+        ["RichTextColor"] = "rgb(138, 102, 255)",
+        ["ItemStream"] = {
+            ["Sound"] = "ItemStreamPickUp",
+            ["BackgroundColor"] = Color3.fromRGB(118, 37, 217),
+            ["TextColor"] = Color3.fromRGB(138, 102, 255)
+        },
+        ["Index"] = 6,
+        ["TextColor"] = Color3.fromRGB(138, 102, 255)
+    },
+    ["Uncommon"] = {
+        ["Name"] = "Uncommon",
+        ["RichTextColor"] = "rgb(152, 255, 167)",
+        ["ItemStream"] = {
+            ["Sound"] = "QuestIncrement",
+            ["BackgroundColor"] = Color3.fromRGB(19, 85, 63),
+            ["TextColor"] = Color3.fromRGB(152, 255, 167)
+        },
+        ["Index"] = 2,
+        ["TextColor"] = Color3.fromRGB(152, 255, 167)
+    },
+    ["Rare"] = {
+        ["Name"] = "Rare",
+        ["RichTextColor"] = "rgb(123, 255, 255)",
+        ["ItemStream"] = {
+            ["Sound"] = "QuestIncrement",
+            ["BackgroundColor"] = Color3.fromRGB(20, 77, 112),
+            ["TextColor"] = Color3.fromRGB(123, 255, 255)
+        },
+        ["Index"] = 3,
+        ["TextColor"] = Color3.fromRGB(123, 255, 255)
+    }
+}
