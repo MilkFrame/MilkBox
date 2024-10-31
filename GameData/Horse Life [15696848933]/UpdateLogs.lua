6b06f2a12713e89c0bf261e8de9dd7b3d156def9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-01 03:09:05 +1100

    Create UpdateLogs.lua

---
+++ GameData/Horse Life [15696848933]/UpdateLogs.lua
@@ -0,0 +1,373 @@
+data = {
+    ["30"] = {
+        ["ReleaseTime"] = 1729896379,
+        ["Image"] = "rbxassetid://134185830006595",
+        ["Title"] = "PLUSH SPCIES & HALLOWEEN EVENT W5",
+        ["FeaturedDescription"] = "New plush species",
+        ["Description"] = [[🎠 New plush species and plush mission!
+🏆 New gold theme only obtained in storyline quests
+📜 Storyline CH1 Part 1 released (More to come soon!!)
+🧹 Event hind cauldron, demon trail, and cat familiar!
+🪀 New material machine to buy plush build items
+]]
+    },
+    ["22"] = {
+        ["ReleaseTime"] = 1725044027,
+        ["Image"] = "rbxassetid://103482906800081",
+        ["Title"] = "FAE HORSE RELEASE",
+        ["FeaturedDescription"] = "New Fae horse species is now released! Complete the fae journal entry to tame them!",
+        ["Description"] = [[🧝 Fae horses have been released
+🧚 Fairies roam the swamp! Catch them with nets for larvae drops!
+🦋 Butterfly, Cutlass, and Dragonfly wings are out!
+🐛 Fae Larvae can be obtained by taming fairies and by searching silk bushes
+🏇 Fae saddle added
+👧 Lyric's Fae shop, better larvae = better fae
+]]
+    },
+    ["23"] = {
+        ["ReleaseTime"] = 1725653508,
+        ["Image"] = "rbxassetid://103482906800081",
+        ["Title"] = "Plushie Machine Update",
+        ["FeaturedDescription"] = "New Fae horse species is now released! Complete the fae journal entry to tame them!",
+        ["Description"] = [[🧝 New plushie machine shop, buy one plush a cycle
+🧸 20+ plushies added
+👶 Twins mutation
+🎨 Color predictor
+😇 Halos from boss tier 1,2,3 and 4
+]]
+    },
+    ["27"] = {
+        ["ReleaseTime"] = 1728072707,
+        ["Image"] = "rbxassetid://102518984605031",
+        ["Title"] = "HALLOWEEN EVENT W2",
+        ["FeaturedDescription"] = "New minigames, ghoul traits, and accessories!",
+        ["Description"] = [[🪽 New reaper wing variant
+🧟 Ghoulsteed new traits: Corrupted vision, thin horns, Moon horns, short mane, short tail, spectre pattern, abyssal rifts pattern, weeping face, weeping mane, weeping tail
+🧙 275k Like goal rewards TYSM!!
+🎮 New corn maze defense minigame, coffee making minigame, haunted gacha machine, and wishing fountain
+🧙 New event accessories: crossbone ear clip, bow bat ear clip, crow plush, crow familiar, ghost trail, and lunar witch!
+]]
+    },
+    ["26"] = {
+        ["ReleaseTime"] = 1727463047,
+        ["Image"] = "rbxassetid://91730971046747",
+        ["Title"] = "HALLOWEEN EVENT W1",
+        ["FeaturedDescription"] = "New traits have been released along with new crossbreed species only traits!",
+        ["Description"] = [[🧟 New ghoulsteed species
+🐉 3 new dragon wings, can be added with spooky wing potion
+🧙 New halloween accessories, palettes, flairs, and more!
+🎮 Apple bobbing, pumpkin harvesting, and caramel apple design minigames for week 1
+🎃 New pumpkin and ghoul limited time themes
+]]
+    },
+    ["25"] = {
+        ["ReleaseTime"] = 1726863535,
+        ["Image"] = "rbxassetid://103482906800081",
+        ["Title"] = "NEW Traits!",
+        ["FeaturedDescription"] = "New traits have been released along with new crossbreed species only traits!",
+        ["Description"] = [[📶 New heights, Teacup and mammoth. Height breeding changes where the foal's height will be between the parents, with a chance for random
+- Gargoyle: Nose scutes, belly scutes, neck scutes, rhino horn, ore veins
+- Equus: Collie paint, Cascading dapple paint, streaking stripes, scarce chimera, scarce chimera paint, snakehead streaks, primal spots
+- Unicorn: Striped Paint, Magic scales (Crossbreed trait), Marble stripes (Crossbreed trait), whirl paint, Neck fur, okapi paint (crossbreed), okapi back (crossbreed), okapi main, okapi tail
+- Horse: Primal stripes (Crossbreed trait), Broken front chimera, Rear snowcap
+- Caprine: Jacobs Horns
+- Fae: Windswept
+- Birsorse: speckled back, tapered thigh, tapered face, tapered forelimb, tapered body
+- Pony: Freckle stars(crossbreed), streaking stars(Crossbreed)
+]]
+    },
+    ["24"] = {
+        ["ReleaseTime"] = 1726265971,
+        ["Image"] = "rbxassetid://103482906800081",
+        ["Title"] = "Bridle Sock Update",
+        ["FeaturedDescription"] = "New bridles and socks have been released for nearly all species",
+        ["Description"] = [[🏇 New socks and bridles for Gargoyle, peryton, unicorn, pony, caprine, bisorse, kelpie, and equus!
+🐧 Penguin plush added
+📅 Updated daily login, more to come in next week!
+🗙 Mass delete option added to inventory
+🗺️ Minimap interest points
+🐛 Fixed fae hoof coloring, gargoyle hind accessories, added 3 more hotbar/hand spots, log out teleport locations
+]]
+    },
+    ["28"] = {
+        ["ReleaseTime"] = 1728669251,
+        ["Image"] = "rbxassetid://102518984605031",
+        ["Title"] = "HALLOWEEN EVENT W3",
+        ["FeaturedDescription"] = "Maze minigames, ghoul traits, accessories, and QOL!!",
+        ["Description"] = [[🪽 New skeleeton wing variant
+🧟 Ghoulsteed new traits: Feral Mane, Feral Tail, Decaying ankles, decaying flesh, weeping soul pattern, spinneret Mane, Spinneret Tail, sporadic bones, Mangy back patch, messy ankle fur, drool, Ancient runes
+🎮 New corn maze activity, escape within 4 minutes and replay every 8 minutes
+🧙 New event items: Vampire palette, haunted, possessed, bewitched, Vampiric Amulet, Magic Potion, Wretched Tree
+🥇 New horse overlays for rare serials! 
+🍓 Added display reward pool to float above harvestables
+👻 New ghoul and ghost mutations that cause your horse to be see through
+🪛 QoL: Shop bulk buy, sort stables by species, search inventory for themed accessories
+]]
+    },
+    ["29"] = {
+        ["ReleaseTime"] = 1729287139,
+        ["Image"] = "rbxassetid://102518984605031",
+        ["Title"] = "HALLOWEEN EVENT W4",
+        ["FeaturedDescription"] = "Pinning missions, new event missions/limited ghoul",
+        ["Description"] = [[🪽 New creature wing variant
+🧟 Ghoulsteed new traits: Bat ears, ghost paint, poisoned pattern
+🧙 New event items: Maple leaf, potion palette, coffin ear clip, possessed witch, eye bat, crow crown, vampire trail
+👻 New halloween quests for exclusive rewards & new limited ghoulsteed
+📌 Pin quests to easily follow along
+🎠 Reduced complexity of missions by separating tracks from 1 into 3
+]]
+    },
+    ["20"] = {
+        ["ReleaseTime"] = 1723848014,
+        ["Image"] = "rbxassetid://18462310872",
+        ["Title"] = "NEW TRAITS + BONDING",
+        ["FeaturedDescription"] = "New bond trait with your horse, unlocked by leveling your horse, plus lots of new traits to discover.",
+        ["Description"] = [[🐄 Lots of new traits for the horse, peryton, unicorn, equus, bisorse, and pony!
+❤️ New "Bond" skill
+🧼 New bonding mechanics rinsing, soaping, and brushing
+🦜 Flared and raptor wings naturally spawn on wild Perytons
+	]]
+    },
+    ["19"] = {
+        ["ReleaseTime"] = 1723231943,
+        ["Image"] = "rbxassetid://18462310872",
+        ["Title"] = "TIMED QUESTS",
+        ["FeaturedDescription"] = "Daily, weekly, and monthly quests provide the player active goals to work towards and be rewarded from!",
+        ["Description"] = [[📜 Daily, weekly, and monthly timed quests
+🍪 New cookie theme, only obtained in timed quests
+👒 New accessories: Peryton saddle, basic halo, shimmering halo, boba, chocolate chip cookie
+🏘️ Updated town center
+👁️ Eye colors now display on horse inspect
+🎀 Inspect accessories when buying/trading
+	]]
+    },
+    ["18"] = {
+        ["ReleaseTime"] = 1722539217,
+        ["Image"] = "rbxassetid://18462310872",
+        ["Title"] = "THE GAMES",
+        ["FeaturedDescription"] = "The Roblox Summer Games have arrived!",
+        ["Description"] = [[🏁 The games! Complete quests to unlock rewards in the The Games Hub and in Horse Life!
+🏝️ Reimbursements for those who had purchase issues during the last update
+ - More exclusive item reimbursements next update!
+ - Assorted misc. bug fixes
+	]]
+    },
+    ["9"] = {
+        ["ReleaseTime"] = 1717176025,
+        ["Image"] = "rbxassetid://17682975043",
+        ["Title"] = "FULL RELEASE!!!",
+        ["FeaturedDescription"] = "The full release of the game is here! Along with the caprine species.",
+        ["Description"] = [[🐐 New Caprine species, placed between the bisorse and unicorn
+🎨 New palettes to discover to uniquely color accessories
+👒 New accessories
+✨ Sparkles, butterflies, fireflies, clouds, and lightning flairs added
+🦄 New unicorn and horse traits
+🪨 New spawnable coats of gargoyle and horse
+⚗️ Trait boost potion
+🎁 Group loyalty bonus
+	]]
+    },
+    ["1"] = {
+        ["ReleaseTime"] = 1711816849,
+        ["Image"] = "rbxassetid://16896116251",
+        ["Title"] = "Equus + Balance",
+        ["FeaturedDescription"] = "Added new wing and additions to the Equus/swamp region. Balanced food taming & drops! Expect performance upgrades.",
+        ["Description"] = [[🕊️ Added narrow wing type
+🦅 Improved stamina drain while flying
+🎨 Added new Equus paint, pattern, and tail/manes
+🏞️ Added resources to swamp & Zebra/Red Equus 
+🍎 Improved rare food drop rates
+💻 New leaderboard UX + update log
+🦚 Idle wing emotes play randomly
+	]]
+    },
+    ["15"] = {
+        ["ReleaseTime"] = 1720828952,
+        ["Image"] = "rbxassetid://18462310872",
+        ["Title"] = "MARKET STALLS/FOLDERS",
+        ["FeaturedDescription"] = "Sell horses and items at the market stalls! Acceess folders folders to sort horses",
+        ["Description"] = [[🛍️ Market stalls: List horses and items for sale!
+🗂️ Horse stable sorting with folders, unlock at 100 foals!
+🫐 New blueberry theme
+🔥 Tier 3 boss fight 
+🌵 Cactus fruit
+🐎 Lots of new traits for equus, pony, unicorn, gargoyle, kelppie, caprine, and horse!
+	]]
+    },
+    ["3"] = {
+        ["ReleaseTime"] = 1712956082,
+        ["Image"] = "rbxassetid://16896116251",
+        ["Title"] = "Co-Taming!",
+        ["FeaturedDescription"] = "Co-Op taming is now added to encourage player cooperation and friendliness! Help each other tame horses.",
+        ["Description"] = [[🐄 Added manes, tails, paint and pattern to bison
+🦓 Added more tails to gargoyle and equus paint
+🐎 New horse paint and tail/mane
+🧬 New barred wing mutation
+🤝 Co-op taming added, help friends for items!
+	]]
+    },
+    ["2"] = {
+        ["ReleaseTime"] = 1712350844,
+        ["Image"] = "rbxassetid://16896116251",
+        ["Title"] = "Bison Horse",
+        ["FeaturedDescription"] = "The new canyon region has been added which features the new species, bisorse! Beware, these horses are aggressive!",
+        ["Description"] = [[🐄 Added new species, Bison horse
+🦓 New traits for Horse, Unicorn, and Gargoyle
+⚗️ New Random Wing Potion
+📷 Added Photo Mode
+🥕 'WIP' Inventory quick feed interface
+	]]
+    },
+    ["5"] = {
+        ["ReleaseTime"] = 1714168959,
+        ["Image"] = "rbxassetid://16896116251",
+        ["Title"] = "Ponies!",
+        ["FeaturedDescription"] = "Ponies have been added and spawn in birchwoods, horse progression quests and more!",
+        ["Description"] = [[🐎 New pony species added
+🦄 Unicorn gloomy mane and tail added
+👜 Watering can mouth item added and horse saddle, & nature/ocean/galaxy accessory themes added
+🎯 New species progression quests to unlock new species
+	]]
+    },
+    ["4"] = {
+        ["ReleaseTime"] = 1713562967,
+        ["Image"] = "rbxassetid://16896116251",
+        ["Title"] = "Equipment!",
+        ["FeaturedDescription"] = "Equipment added, more saddles and accessories to come in the future!",
+        ["Description"] = [[🛒 Added equipment shop in the town!
+🦄 splintered, cracked, and broken unicorn horns added
+🎩 Top hat, tulip hat, bunny ears, plush bunny ears, and mushroom hat added
+❌ Infertility potion added 
+	]]
+    },
+    ["7"] = {
+        ["ReleaseTime"] = 1715373055,
+        ["Image"] = "rbxassetid://16896116251",
+        ["Title"] = "Horse Competitions",
+        ["FeaturedDescription"] = "Horse competitions are added, breed your horse to match for awesome rewards!",
+        ["Description"] = [[🐦 Alpha/Beta rewards wip (Dragon skull, head branches, Fae headpiece, fantasy wings)
+✨ New gargoyle paint layers: Vine body, head, ankle, and legs, with leafy back paint
+🎨 Fixed multi color names: Oak, Fawn, and Gobi (Tortilla, Nutmeg, Clove!)
+👜 Horse basic saddle fixed and added
+⚗️ Master lasso added (can tame ANY horse!)
+🧁 Taming muffins added to store (low gestation buff timers) & new exclusive feed with improved timers
+	]]
+    },
+    ["6"] = {
+        ["ReleaseTime"] = 1714769686,
+        ["Image"] = "rbxassetid://16896116251",
+        ["Title"] = "Release Preparation",
+        ["FeaturedDescription"] = "QoL and early preparations for release, we're focusing on game polish and final features (NPCS/Horse Comp!)",
+        ["Description"] = [[🐦 New wing type added, raven wings
+✨ Uni/Garg spawn rates increased from 1 to 2 each
+🦄 Unicorn sparse mane and tail added
+👜 New equipment themes added: Obsidian and Cloud
+🎯 Improved species journal missions
+🎨 New "Blonde" preset along with more colors
+⛅ Day night cycle & night lighting added
+⚗️ Instant foal potion
+	]]
+    },
+    ["14"] = {
+        ["ReleaseTime"] = 1719614411,
+        ["Image"] = "rbxassetid://18250530783",
+        ["Title"] = "NPC SHOP/GIFTING",
+        ["FeaturedDescription"] = "Bond with NPCs further by using gifts, and their shops are now accessible!",
+        ["Description"] = [[👫 Improve your bond with NPCs by gifting them items
+🛍️ Access special NPC shops with unique items, higher bond = more shop visibility!
+🏇 New SADDLES for pony, equus, kelpie, gargoyle, bisorse, caprine and unicorn
+🍓 New berry & heavenly theme
+🎒 Upgrade additions for horse stables & inventory
+🌿 Additional golden & sea blue kelpie presets
+	]]
+    },
+    ["8"] = {
+        ["ReleaseTime"] = 1715966981,
+        ["Image"] = "rbxassetid://16896116251",
+        ["Title"] = "NPC Bonding",
+        ["FeaturedDescription"] = "NPC Development is underway, bonding via chatting is a soft launch/wip!",
+        ["Description"] = [[✨ New gargoyle paint layers: Face moss, limb moss, and full body moss
+👧 NPC Bonding and chatting added, gifting/questing etc on the way!
+⚗️ Foal prediction claim works
+🐎 Bug fixes to the horse contest
+🏇 Exclusive horse R$ tier WIP
+	]]
+    },
+    ["16"] = {
+        ["ReleaseTime"] = 1721442119,
+        ["Image"] = "rbxassetid://18462310872",
+        ["Title"] = "HORSE RACES",
+        ["FeaturedDescription"] = "Participate in horse races for exclusive rewards and a fun experience!",
+        ["Description"] = [[🛍️ New horse races: First map live!
+🏇 New racing horse accessories, palette, and trails
+🪴 New store accessory items for Alex and lyric
+📦 New delivery mission for NPCS to boost bond and get a reward
+🌩️ New thunder theme exclusive to racing
+🪽 New wing variant, round wings!
+	]]
+    },
+    ["17"] = {
+        ["ReleaseTime"] = 1722037461,
+        ["Image"] = "rbxassetid://18462310872",
+        ["Title"] = "PERYTON",
+        ["FeaturedDescription"] = "The new Peryton horse species releases today! Complete their mission and fly high to tame them.",
+        ["Description"] = [[🦌 New Peryton species
+🏝️ Map floating island expansion
+🪽 Resting/Lifted wing potions
+🐎 New like goals + like goal special dev horse
+🐞 Bug fixes
+	]]
+    },
+    ["13"] = {
+        ["ReleaseTime"] = 1719614411,
+        ["Image"] = "rbxassetid://18250530783",
+        ["Title"] = "KELPIES",
+        ["FeaturedDescription"] = "Kelpies have been added! Tame kelpies by fishing, or finding one in lakes!",
+        ["Description"] = [[🌿 New kelpie species + journal entry
+🎣 New fishing (carrot, berry, and gold fish rods!)
+🧬 Mutation delete, and mutation rolling potions!
+🌟 New accessory themes, lemon and void
+🎀 8 brand new fishing-only acquired accessories
+	]]
+    },
+    ["12"] = {
+        ["ReleaseTime"] = 1718987810,
+        ["Image"] = "rbxassetid://17858610772",
+        ["Title"] = "TRADING",
+        ["FeaturedDescription"] = "Trading has been added to the game! Barter up to 9 unique items.",
+        ["Description"] = [[🐦 New small hummingbird wings
+🐄 New Bisorse horns and patterns
+🐐 New Caprine horns and patterns
+🦄 New Unicorn mane, tail, horns, and paint
+🪨 New Gargoyle mane, patterns, and horns
+🔄 Trading added
+🗺️ Map unlocked after completing Equus journal stage
+	]]
+    },
+    ["11"] = {
+        ["ReleaseTime"] = 1718395164,
+        ["Image"] = "rbxassetid://17858610772",
+        ["Title"] = "SPIRIT BOSS TAMES",
+        ["FeaturedDescription"] = "Spirit horses rampage with fury! Use your lassos to tame them to calm them down, avoid their attacks and be rewarded!",
+        ["Description"] = [[🎨 New luxury and oceanside palettes
+❤️ New pink and black heart aura, as well as petal aura
+🧬 Dwarfism and gigantism mutations added! Decrease or increase horse size by 2%
+👁️ Hazel, olive, and olive green eyes added
+🦌 Spirit boss tames added, along with glow mutations + spirit origin foal drops 
+	]]
+    },
+    ["10"] = {
+        ["ReleaseTime"] = 1717796549,
+        ["Image"] = "rbxassetid://17682975043",
+        ["Title"] = "MARKET WORLD AUCTIONS",
+        ["FeaturedDescription"] = "Market world joins us as it allows players to auction horses!",
+        ["Description"] = [[🐐 New Caprine manes/tails
+🌈 Rainbow camera effect & horse aura added
+🎀 New heart ear bow, ear ribbon, and tail bow accessories added
+🎨 Fixed editing not working on bridles/accessories
+♻️ Added market realm, must be at bison journal to enter
+🛍️ Auctioning for horses added to market world
+- More features to come to market world in the upcoming weeks!
+	]]
+    }
+}
