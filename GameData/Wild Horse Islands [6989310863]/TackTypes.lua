b533359d879b9fde00dd59c4dadb084212fe3217
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:35:11 +1100

    Create TackTypes.lua

---
+++ GameData/Wild Horse Islands [6989310863]/TackTypes.lua
@@ -0,0 +1,32 @@
+data = {
+    ["saddle"] = {["order"] = 1, ["image"] = "rbxassetid://6999178319", ["name"] = "Saddle"},
+    ["head"] = {["isAccessory"] = true, ["image"] = "rbxassetid://8928475527", ["name"] = "Head", ["order"] = 4},
+    ["legs"] = {["isAccessory"] = true, ["image"] = "rbxassetid://8928475316", ["name"] = "Legs", ["order"] = 7},
+    ["face"] = {["isAccessory"] = true, ["image"] = "rbxassetid://8928391643", ["name"] = "Face", ["order"] = 5},
+    ["feet"] = {["isAccessory"] = true, ["image"] = "rbxassetid://8928475211", ["name"] = "Feet", ["order"] = 8},
+    ["vehicle"] = {
+        ["image"] = "rbxassetid://10395246801",
+        ["name"] = "Vehicle",
+        ["isVehicle"] = true,
+        ["isAccessory"] = true,
+        ["order"] = 13,
+        ["dontVisualizeIn2D"] = true
+    },
+    ["aura"] = {["isAccessory"] = true, ["image"] = "rbxassetid://10097256563", ["name"] = "Aura", ["order"] = 12},
+    ["shoes"] = {["order"] = 9, ["image"] = "rbxassetid://10569611269", ["name"] = "Shoes"},
+    ["bridle"] = {["order"] = 2, ["image"] = "rbxassetid://7806727251", ["name"] = "Bridle/Harness"},
+    ["leftShoulder"] = {
+        ["isAccessory"] = true,
+        ["image"] = "rbxassetid://9803519562",
+        ["name"] = "Shoulder",
+        ["order"] = 10
+    },
+    ["mouth"] = {["isAccessory"] = true, ["image"] = "rbxassetid://9808514755", ["name"] = "Mouth", ["order"] = 11},
+    ["tail"] = {["isAccessory"] = true, ["image"] = "rbxassetid://8928475429", ["name"] = "Tail", ["order"] = 6},
+    ["saddlePad"] = {
+        ["isAccessory"] = true,
+        ["image"] = "rbxassetid://7777079545",
+        ["name"] = "Saddle Pad",
+        ["order"] = 3
+    }
+}
