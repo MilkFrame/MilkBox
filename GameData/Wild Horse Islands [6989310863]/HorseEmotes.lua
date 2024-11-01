aec9215ade7f3ae23fa0ad3803801ad6fa59249f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:27:13 +1100

    Create HorseEmotes.lua

---
+++ GameData/Wild Horse Islands [6989310863]/HorseEmotes.lua
@@ -0,0 +1,83 @@
+data = {
+    {
+        ["animationName"] = "piaffeLoop",
+        ["name"] = "Piaffe",
+        ["skillRequired"] = "piaffeEmote",
+        ["interruptOnMovement"] = true,
+        ["animationId"] = "rbxassetid://13988837720",
+        ["forceIdle"] = true
+    },
+    {
+        ["animationName"] = "rear",
+        ["name"] = "Rear",
+        ["disableInVehicle"] = true,
+        ["forceIdle"] = true,
+        ["onStartEvent"] = "sound",
+        ["animationId"] = "rbxassetid://7783202813",
+        ["onStartEventParams"] = {"horseNeigh"}
+    },
+    {
+        ["animationName"] = "layLoop",
+        ["name"] = "Lay",
+        ["disableInVehicle"] = true,
+        ["interruptOnMovement"] = true,
+        ["animationId"] = "rbxassetid://8643747136",
+        ["forceIdle"] = true
+    },
+    {
+        ["animationName"] = "sitLoop",
+        ["name"] = "Sit",
+        ["disableInVehicle"] = true,
+        ["interruptOnMovement"] = true,
+        ["animationId"] = "rbxassetid://8722446146",
+        ["forceIdle"] = true
+    },
+    {
+        ["animationName"] = "buck",
+        ["name"] = "Buck",
+        ["movingAnimationId"] = "rbxassetid://12242784462",
+        ["onStartEvent"] = "sound",
+        ["animationId"] = "rbxassetid://8651642805",
+        ["onStartEventParams"] = {"horseAngry"}
+    },
+    {
+        ["animationName"] = "danceLoop",
+        ["name"] = "Dance",
+        ["interruptOnMovement"] = true,
+        ["animationId"] = "rbxassetid://9944775433",
+        ["forceIdle"] = true
+    },
+    {
+        ["forceIdle"] = true,
+        ["animationName"] = "graze",
+        ["name"] = "Graze/Drink",
+        ["animationId"] = "rbxassetid://8651727401"
+    },
+    {["animationName"] = "sniffAir", ["name"] = "Sniff Air", ["animationId"] = "rbxassetid://8722645422"},
+    {
+        ["animationName"] = "angry",
+        ["name"] = "Angry",
+        ["onStartEvent"] = "sound",
+        ["animationId"] = "rbxassetid://7806261842",
+        ["onStartEventParams"] = {"horseAngry"}
+    },
+    {["animationName"] = "relaxed", ["name"] = "Relaxed", ["animationId"] = "rbxassetid://7783210574"},
+    {
+        ["forceIdle"] = true,
+        ["animationName"] = "pawGround",
+        ["name"] = "Paw Ground",
+        ["animationId"] = "rbxassetid://8405298283"
+    },
+    {
+        ["forceIdle"] = true,
+        ["animationName"] = "scratchFace",
+        ["name"] = "Scratch Face",
+        ["animationId"] = "rbxassetid://8405836918"
+    },
+    {
+        ["forceIdle"] = true,
+        ["animationName"] = "eatFromHand",
+        ["name"] = "Eat From Hand",
+        ["animationId"] = "rbxassetid://8311079983"
+    }
+}
