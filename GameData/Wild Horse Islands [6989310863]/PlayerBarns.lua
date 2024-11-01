cb49306a1d191177ef5fa5d5079af4d40e1989b3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:33:09 +1100

    Create PlayerBarns.lua

---
+++ GameData/Wild Horse Islands [6989310863]/PlayerBarns.lua
@@ -0,0 +1,103 @@
+data = {
+    {
+        ["styles"] = {
+            ["gambrelEnglish"] = {["name"] = "Gambrel (Fancy)", ["cost"] = 400},
+            ["gambrelRustic"] = {["name"] = "Gambrel (Rustic)", ["cost"] = 200}
+        },
+        ["name"] = "Four Stall Barn",
+        ["totalStalls"] = 4,
+        ["itemStorageSize"] = 25,
+        ["description"] = [[Stalls: 4
+Item Storage: 25
+Styles: 2]],
+        ["defaultStyle"] = "gambrelRustic"
+    },
+    {
+        ["defaultStyle"] = "gambrelRustic",
+        ["name"] = "Six Stall Barn",
+        ["cost"] = 2500,
+        ["description"] = [[Stalls: 6
+Item Storage: 50
+Styles: 4]],
+        ["itemStorageSize"] = 50,
+        ["styles"] = {
+            ["carriageEnglish"] = {["name"] = "Carriage (Fancy)", ["cost"] = 700},
+            ["carriageRustic"] = {["name"] = "Carriage (Rustic)", ["cost"] = 600},
+            ["gambrelEnglish"] = {["name"] = "Gambrel (Fancy)", ["cost"] = 500},
+            ["gambrelRustic"] = {["name"] = "Gambrel (Rustic)", ["cost"] = 400}
+        },
+        ["totalStalls"] = 6
+    },
+    {
+        ["defaultStyle"] = "gambrelRustic",
+        ["name"] = "Eight Stall Barn",
+        ["cost"] = 7500,
+        ["description"] = [[Stalls: 8
+Item Storage: 75
+Styles: 6]],
+        ["itemStorageSize"] = 75,
+        ["styles"] = {
+            ["carriageEnglish"] = {["name"] = "Carriage (Fancy)", ["cost"] = 1000},
+            ["carriageRustic"] = {["name"] = "Carriage (Rustic)", ["cost"] = 900},
+            ["monitorRustic"] = {["name"] = "Monitor (Rustic)", ["cost"] = 1100},
+            ["monitorEnglish"] = {["name"] = "Monitor (Fancy)", ["cost"] = 1200},
+            ["gambrelEnglish"] = {["name"] = "Gambrel (Fancy)", ["cost"] = 800},
+            ["gambrelRustic"] = {["name"] = "Gambrel (Rustic)", ["cost"] = 700}
+        },
+        ["totalStalls"] = 8
+    },
+    {
+        ["defaultStyle"] = "gambrelRustic",
+        ["name"] = "Ten Stall Barn",
+        ["cost"] = 12500,
+        ["description"] = [[Stalls: 10
+Item Storage: 100
+Styles: 6]],
+        ["itemStorageSize"] = 100,
+        ["styles"] = {
+            ["carriageEnglish"] = {["name"] = "Carriage (Fancy)", ["cost"] = 1000},
+            ["carriageRustic"] = {["name"] = "Carriage (Rustic)", ["cost"] = 900},
+            ["monitorRustic"] = {["name"] = "Monitor (Rustic)", ["cost"] = 1100},
+            ["monitorEnglish"] = {["name"] = "Monitor (Fancy)", ["cost"] = 1200},
+            ["gambrelEnglish"] = {["name"] = "Gambrel (Fancy)", ["cost"] = 800},
+            ["gambrelRustic"] = {["name"] = "Gambrel (Rustic)", ["cost"] = 700}
+        },
+        ["totalStalls"] = 10
+    },
+    {
+        ["defaultStyle"] = "gambrelRustic",
+        ["name"] = "14-stall Barn",
+        ["cost"] = 25000,
+        ["description"] = [[Stalls: 14
+Item Storage: 150
+Styles: 6]],
+        ["itemStorageSize"] = 150,
+        ["styles"] = {
+            ["carriageEnglish"] = {["name"] = "Carriage (Fancy)", ["cost"] = 1000},
+            ["carriageRustic"] = {["name"] = "Carriage (Rustic)", ["cost"] = 900},
+            ["monitorRustic"] = {["name"] = "Monitor (Rustic)", ["cost"] = 1100},
+            ["monitorEnglish"] = {["name"] = "Monitor (Fancy)", ["cost"] = 1200},
+            ["gambrelEnglish"] = {["name"] = "Gambrel (Fancy)", ["cost"] = 800},
+            ["gambrelRustic"] = {["name"] = "Gambrel (Rustic)", ["cost"] = 700}
+        },
+        ["totalStalls"] = 14
+    },
+    {
+        ["defaultStyle"] = "gambrelRustic",
+        ["name"] = "18-stall Barn",
+        ["cost"] = 35000,
+        ["description"] = [[Stalls: 18
+Item Storage: 250
+Styles: 6]],
+        ["itemStorageSize"] = 250,
+        ["styles"] = {
+            ["carriageEnglish"] = {["name"] = "Carriage (Fancy)", ["cost"] = 1000},
+            ["carriageRustic"] = {["name"] = "Carriage (Rustic)", ["cost"] = 900},
+            ["monitorRustic"] = {["name"] = "Monitor (Rustic)", ["cost"] = 1100},
+            ["monitorEnglish"] = {["name"] = "Monitor (Fancy)", ["cost"] = 1200},
+            ["gambrelEnglish"] = {["name"] = "Gambrel (Fancy)", ["cost"] = 800},
+            ["gambrelRustic"] = {["name"] = "Gambrel (Rustic)", ["cost"] = 700}
+        },
+        ["totalStalls"] = 18
+    }
+}
