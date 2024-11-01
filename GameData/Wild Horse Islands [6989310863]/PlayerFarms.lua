4d0ee396724f10291afbba6dd3e2a0843dce7858
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:33:43 +1100

    Create PlayerFarms.lua

---
+++ GameData/Wild Horse Islands [6989310863]/PlayerFarms.lua
@@ -0,0 +1,12 @@
+data = {
+    {["name"] = "Tier 1 Farm Area", ["description"] = "You can grow up to 6 crops at once"},
+    {["description"] = "You can grow up to 12 crops at once", ["name"] = "Tier 2 Farm Area", ["cost"] = 250},
+    {["description"] = "You can grow up to 18 crops at once", ["name"] = "Tier 3 Farm Area", ["cost"] = 500},
+    {["description"] = "You can grow up to 24 crops at once", ["name"] = "Tier 4 Farm Area", ["cost"] = 1000},
+    {["description"] = "You can grow up to 30 crops at once", ["name"] = "Tier 5 Farm Area", ["cost"] = 1500},
+    {["description"] = "You can grow up to 36 crops at once", ["name"] = "Tier 6 Farm Area", ["cost"] = 3250},
+    {["description"] = "You can grow up to 42 crops at once", ["name"] = "Tier 7 Farm Area", ["cost"] = 4000},
+    {["description"] = "You can grow up to 48 crops at once", ["name"] = "Tier 8 Farm Area", ["cost"] = 5000},
+    {["description"] = "You can grow up to 54 crops at once", ["name"] = "Tier 9 Farm Area", ["cost"] = 7500},
+    {["description"] = "You can grow up to 60 crops at once", ["name"] = "Tier 10 Farm Area", ["cost"] = 10000}
+}
