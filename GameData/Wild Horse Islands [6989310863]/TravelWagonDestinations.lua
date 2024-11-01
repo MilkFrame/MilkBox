dce52e58daf94a639a344c319eb38eda356cd97e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:40:10 +1100

    Create TravelWagonDestinations.lua

---
+++ GameData/Wild Horse Islands [6989310863]/TravelWagonDestinations.lua
@@ -0,0 +1,187 @@
+data = {
+    ["forestFlowerField"] = {
+        ["island"] = "Forest Island",
+        ["name"] = "Flower Field",
+        ["position"] = Vector3.new(-8036.373046875, 22.351999282836914, 2916.044921875)
+    },
+    ["blizzardIceField"] = {
+        ["island"] = "Blizzard Island",
+        ["name"] = "Ice Field",
+        ["position"] = Vector3.new(3021.618896484375, 144.343994140625, -6912.0029296875)
+    },
+    ["blizzardNPC"] = {
+        ["island"] = "Blizzard Island",
+        ["name"] = "Hubert's Camp",
+        ["position"] = Vector3.new(2959.197998046875, 80.08699798583984, -5955.65380859375)
+    },
+    ["forestJonesCabin"] = {
+        ["island"] = "Forest Island",
+        ["name"] = "Jones' Cabin",
+        ["position"] = Vector3.new(-7703.43212890625, 38.47100067138672, 3948.426025390625)
+    },
+    ["volcanoNPC"] = {
+        ["island"] = "Volcano Island",
+        ["name"] = "Bhurnes' Camp",
+        ["position"] = Vector3.new(3793.294921875, 295.0469970703125, -7534.19921875)
+    },
+    ["desertCanyon"] = {
+        ["island"] = "Desert Island",
+        ["name"] = "Canyon",
+        ["position"] = Vector3.new(820.5399780273438, 122.70800018310547, 4389.8740234375)
+    },
+    ["ucaAcademy"] = {
+        ["island"] = "Unicorn Island",
+        ["name"] = "Academy Entrance",
+        ["position"] = Vector3.new(-1543.35595703125, 142.5, -17581.013671875)
+    },
+    ["desertTown"] = {
+        ["island"] = "Desert Island",
+        ["name"] = "Town",
+        ["position"] = Vector3.new(615.7160034179688, 12.906000137329102, 3716.3759765625)
+    },
+    ["mountainTown"] = {
+        ["island"] = "Mountain Island",
+        ["name"] = "Town",
+        ["position"] = Vector3.new(-7025.9970703125, 6.5, 525.9520263671875)
+    },
+    ["desertPavillion"] = {
+        ["island"] = "Desert Island",
+        ["name"] = "Pavillion",
+        ["position"] = Vector3.new(-690.885009765625, 12.906000137329102, 4211.08984375)
+    },
+    ["jungleGreatFalls"] = {
+        ["island"] = "Jungle Island",
+        ["name"] = "Great Falls",
+        ["position"] = Vector3.new(3396.944091796875, 12.906000137329102, 1188.9510498046875)
+    },
+    ["mountainNPC"] = {
+        ["island"] = "Mountain Island",
+        ["name"] = "Doug's Cabin",
+        ["position"] = Vector3.new(-7186.91796875, 238.0399932861328, -649.1370239257812)
+    },
+    ["wildVillage"] = {
+        ["island"] = "Wild Island",
+        ["name"] = "Village",
+        ["position"] = Vector3.new(2440.955078125, 58.5, 3785.235107421875)
+    },
+    ["blizzardDock"] = {
+        ["island"] = "Blizzard Island",
+        ["name"] = "Dock",
+        ["position"] = Vector3.new(2505.739013671875, -9.515999794006348, -5601.14013671875)
+    },
+    ["royalQuarry"] = {
+        ["island"] = "Royal Island",
+        ["name"] = "Quarry",
+        ["position"] = Vector3.new(961.989990234375, 18.121000289916992, -5975.30908203125)
+    },
+    ["blizzardGlacialCavern"] = {
+        ["island"] = "Blizzard Island",
+        ["name"] = "Glacial Cavern",
+        ["position"] = Vector3.new(3010.7890625, -9.515999794006348, -6157.11376953125)
+    },
+    ["lunarTown"] = {
+        ["island"] = "Lunar Islands",
+        ["name"] = "Waning Isle",
+        ["position"] = Vector3.new(-3673.427978515625, 19.913999557495117, -1819.5980224609375)
+    },
+    ["mainlandDock"] = {
+        ["island"] = "Mainland",
+        ["name"] = "Outskirts",
+        ["position"] = Vector3.new(-289.9909973144531, 30.5, -753.7780151367188)
+    },
+    ["forestDock"] = {
+        ["island"] = "Forest Island",
+        ["name"] = "Dock",
+        ["position"] = Vector3.new(-7245.6748046875, 16.452999114990234, 4601.88916015625)
+    },
+    ["royalCastle"] = {
+        ["island"] = "Royal Island",
+        ["name"] = "Castle",
+        ["position"] = Vector3.new(-599.4730224609375, 282.25799560546875, -6547.8798828125)
+    },
+    ["lunarUFO"] = {
+        ["island"] = "Lunar Islands",
+        ["name"] = "Waxing Isle",
+        ["position"] = Vector3.new(-2141.342041015625, 125.2030029296875, -1083.8170166015625)
+    },
+    ["blizzardSummit"] = {
+        ["island"] = "Blizzard Island",
+        ["name"] = "Glacial Summit",
+        ["position"] = Vector3.new(1999.248046875, 252.67799377441406, -6656.5830078125)
+    },
+    ["lunarFullMoon"] = {
+        ["island"] = "Lunar Islands",
+        ["name"] = "Full Moon Isle",
+        ["position"] = Vector3.new(-3000.083984375, 13.73900032043457, -3965.989990234375)
+    },
+    ["ucaStarglowLake"] = {
+        ["island"] = "Unicorn Island",
+        ["name"] = "Starglow Lake",
+        ["position"] = Vector3.new(-478.4119873046875, 206.79200744628906, -19343.57421875)
+    },
+    ["ucaPeriwinkleField"] = {
+        ["island"] = "Unicorn Island",
+        ["name"] = "Periwinkle Field",
+        ["position"] = Vector3.new(-3544.966064453125, 174.46099853515625, -20007.6484375)
+    },
+    ["rpArena"] = {
+        ["island"] = "RP Island",
+        ["name"] = "Indoor Arena",
+        ["position"] = Vector3.new(-3953.2099609375, 50.5, 1181.3509521484375)
+    },
+    ["rpDock"] = {
+        ["island"] = "RP Island",
+        ["name"] = "Dock",
+        ["position"] = Vector3.new(-5142.7890625, 33.099998474121094, 858.6160278320312)
+    },
+    ["wildMoonHill"] = {
+        ["island"] = "Wild Island",
+        ["name"] = "Moon Hill",
+        ["position"] = Vector3.new(2946.177001953125, 394.5, 1805.073974609375)
+    },
+    ["wildDock"] = {
+        ["island"] = "Wild Island",
+        ["name"] = "Dock",
+        ["position"] = Vector3.new(3468.532958984375, 50.5, 4399.708984375)
+    },
+    ["mainlandToby"] = {
+        ["island"] = "Mainland",
+        ["name"] = "Toby's Hill",
+        ["position"] = Vector3.new(-100.6719970703125, 158.06300354003906, 625.1069946289062)
+    },
+    ["volcanoDragonsDen"] = {
+        ["island"] = "Volcano Island",
+        ["name"] = "Dragon's Den",
+        ["position"] = Vector3.new(2944.219970703125, 27.097000122070312, -8646.337890625)
+    },
+    ["volcanoGeysers"] = {
+        ["island"] = "Volcano Island",
+        ["name"] = "Geysers",
+        ["position"] = Vector3.new(4903.091796875, 24.257999420166016, -7374.923828125)
+    },
+    ["volcanoTown"] = {
+        ["island"] = "Volcano Island",
+        ["name"] = "Town",
+        ["position"] = Vector3.new(2936.612060546875, 22.5, -6916.7109375)
+    },
+    ["jungleSouth"] = {
+        ["island"] = "Jungle Island",
+        ["name"] = "South Jungle",
+        ["position"] = Vector3.new(3638.919921875, 12.906000137329102, 4323.919921875)
+    },
+    ["jungleTreehouse"] = {
+        ["island"] = "Jungle Island",
+        ["name"] = "Treehouse",
+        ["position"] = Vector3.new(3339.051025390625, 12.906000137329102, 2073.662109375)
+    },
+    ["mountainEast"] = {
+        ["island"] = "Mountain Island",
+        ["name"] = "East View",
+        ["position"] = Vector3.new(-6070.10302734375, 278.0889892578125, -94.71399688720703)
+    },
+    ["royalTown"] = {
+        ["island"] = "Royal Island",
+        ["name"] = "Town",
+        ["position"] = Vector3.new(948.6019897460938, 18.131999969482422, -5173.5390625)
+    }
+}
