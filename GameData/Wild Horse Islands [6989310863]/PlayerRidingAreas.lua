94948f48e59c32cc57da5850bced0a990ea07ee2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:32:55 +1100

    Create PlayerRidingAreas.lua

---
+++ GameData/Wild Horse Islands [6989310863]/PlayerRidingAreas.lua
@@ -0,0 +1,15 @@
+data = {
+    ["roundPen"] = {["name"] = "Round Pen", ["cost"] = 75},
+    ["mediumEmptyArena"] = {["name"] = "Medium Empty Arena", ["cost"] = 150},
+    ["indoorArena"] = {["name"] = "Large Empty Indoor Arena", ["cost"] = 500},
+    ["crossCountryJumps"] = {["name"] = "Cross Country Jumps", ["cost"] = 200},
+    ["hardJumpingArena"] = {["name"] = "Advanced Jumping Arena", ["cost"] = 250},
+    ["poleBendingArena"] = {["name"] = "Pole Bending Arena", ["cost"] = 200},
+    ["mediumJumpingArena"] = {["name"] = "Intermediate Jumping Arena", ["cost"] = 225},
+    ["smallDressageArena"] = {["name"] = "Small Dressage Arena", ["cost"] = 100},
+    ["jumpingArena"] = {["name"] = "Beginner Jumping Arena", ["cost"] = 200},
+    ["largeDressageArena"] = {["name"] = "Large Dressage Arena", ["cost"] = 200},
+    ["barrelRacingArena"] = {["name"] = "Barrel Racing Arena", ["cost"] = 200},
+    ["smallEmptyArena"] = {["name"] = "Small Empty Arena", ["cost"] = 100},
+    ["largeEmptyArena"] = {["name"] = "Large Empty Arena", ["cost"] = 200}
+}
