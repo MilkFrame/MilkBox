159f07cc7400b2ec7d911b37aff0374bcfba442e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:34:26 +1100

    Create HorseBreeds.lua

---
+++ GameData/Wild Horse Islands [6989310863]/HorseBreeds.lua
@@ -0,0 +1,18 @@
+data = {
+    "Quarter Horse",
+    "Appaloosa Horse",
+    "Paint Horse",
+    "Fjord Horse",
+    "Thoroughbred Horse",
+    "Clydesdale Horse",
+    "Arabian Horse",
+    "Friesian Horse",
+    "Mustang Horse",
+    "Andalusian Horse",
+    "Percheron Horse",
+    "Akhal-Teke Horse",
+    "Icelandic Horse",
+    "Marwari Horse",
+    "Dutch Warmblood Horse",
+    "Shire Horse"
+}
