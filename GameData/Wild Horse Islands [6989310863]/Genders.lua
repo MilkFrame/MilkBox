0f18ebcd72cdbd11421ecb5ca917181b969a5cd4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:28:55 +1100

    Create Genders.lua

---
+++ GameData/Wild Horse Islands [6989310863]/Genders.lua
@@ -0,0 +1,21 @@
+data = {
+    {
+        ["name"] = "Stallion",
+        ["speciesNames"] = {
+            ["Reindeer"] = {["name"] = "Bull"},
+            ["Horse"] = {["name"] = "Stallion", ["babyName"] = "Colt"}
+        },
+        ["colour"] = Color3.fromRGB(90, 151, 243),
+        ["genericName"] = "Boy",
+        ["icon"] = "rbxassetid://12787560891",
+        ["babyName"] = "Colt"
+    },
+    {
+        ["name"] = "Mare",
+        ["speciesNames"] = {["Reindeer"] = {["name"] = "Cow"}, ["Horse"] = {["name"] = "Mare", ["babyName"] = "Filly"}},
+        ["colour"] = Color3.fromRGB(247, 123, 148),
+        ["genericName"] = "Girl",
+        ["icon"] = "rbxassetid://12787561734",
+        ["babyName"] = "Filly"
+    }
+}
