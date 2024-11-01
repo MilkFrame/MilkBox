7244a692244193a43a00764adfbcfe0fa813bb87
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:30:37 +1100

    Create HornVariants.lua

---
+++ GameData/Wild Horse Islands [6989310863]/HornVariants.lua
@@ -0,0 +1,18 @@
+data = {
+    ["style"] = {
+        ["testUnicorn"] = {["name"] = "test unicorn"},
+        ["fiend"] = {["name"] = "fiend horns"},
+        ["tripleHorn"] = {["name"] = "triple horn"}
+    },
+    ["colour"] = {
+        ["blackVoid"] = {
+            ["name"] = "black void",
+            ["properties"] = {["Color"] = Color3.fromRGB(0, 0, 0), ["Material"] = Enum.Material.Neon}
+        },
+        ["turquoise"] = {["name"] = "turquoise", ["properties"] = {["TextureID"] = "rbxassetid://15169527790"}},
+        ["blackWhiteFade"] = {
+            ["name"] = "black/white fade",
+            ["properties"] = {["TextureID"] = "rbxassetid://15169538658"}
+        }
+    }
+}
