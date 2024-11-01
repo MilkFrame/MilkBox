b510d662e82b7e73679258754d31d153bdda7ad1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:33:26 +1100

    Create PlayerPastures.lua

---
+++ GameData/Wild Horse Islands [6989310863]/PlayerPastures.lua
@@ -0,0 +1,64 @@
+data = {
+    {
+        ["capacity"] = 2,
+        ["name"] = "Two Horse Pasture",
+        ["description"] = [[Size: 75x75
+Capacity: 2]]
+    },
+    {
+        ["capacity"] = 4,
+        ["description"] = [[Size: 90x100
+Capacity: 4]],
+        ["name"] = "Four Horse Pasture",
+        ["cost"] = 500
+    },
+    {
+        ["capacity"] = 6,
+        ["description"] = [[Size: 100x125
+Capacity: 6]],
+        ["name"] = "Six Horse Pasture",
+        ["cost"] = 2500
+    },
+    {
+        ["capacity"] = 8,
+        ["description"] = [[Size: 115x150
+Capacity: 8]],
+        ["name"] = "Eight Horse Pasture",
+        ["cost"] = 5000
+    },
+    {
+        ["capacity"] = 10,
+        ["description"] = [[Size: 125x175
+Capacity: 10]],
+        ["name"] = "10 Horse Pasture",
+        ["cost"] = 7500
+    },
+    {
+        ["capacity"] = 12,
+        ["description"] = [[Size: 140x200
+Capacity: 12]],
+        ["name"] = "12 Horse Pasture",
+        ["cost"] = 10000
+    },
+    {
+        ["capacity"] = 14,
+        ["description"] = [[Size: 160x220
+Capacity: 14]],
+        ["name"] = "14 Horse Pasture",
+        ["cost"] = 12500
+    },
+    {
+        ["capacity"] = 16,
+        ["description"] = [[Size: 180x220
+Capacity: 16]],
+        ["name"] = "16 Horse Pasture",
+        ["cost"] = 15000
+    },
+    {
+        ["capacity"] = 18,
+        ["description"] = [[Size: 200x220
+Capacity: 18]],
+        ["name"] = "18 Horse Pasture",
+        ["cost"] = 20000
+    }
+}
