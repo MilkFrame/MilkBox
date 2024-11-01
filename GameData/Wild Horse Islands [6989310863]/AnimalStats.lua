769c84d27cedb638aa3d01357f3a11c44a76b6f3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:34:45 +1100

    Create AnimalStats.lua

---
+++ GameData/Wild Horse Islands [6989310863]/AnimalStats.lua
@@ -0,0 +1,66 @@
+data = {
+    ["agility"] = {
+        ["maxLevel"] = 100,
+        ["image"] = "rbxassetid://12787562119",
+        ["minLevel"] = 0,
+        ["displayOrder"] = 5,
+        ["isBoostable"] = true,
+        ["disabled"] = false,
+        ["description"] = "The agility stat makes your horse steer faster and tighter, as well as how fast they can run down steep hills!",
+        ["colour"] = Color3.fromRGB(146, 238, 140)
+    },
+    ["height"] = {
+        ["image"] = "rbxassetid://13279597464",
+        ["untrainable"] = true,
+        ["displayOrder"] = 7,
+        ["isFloatOnly"] = true,
+        ["uncappable"] = true,
+        ["disabled"] = false,
+        ["colour"] = Color3.fromRGB(243, 202, 100)
+    },
+    ["jump"] = {
+        ["maxLevel"] = 100,
+        ["image"] = "rbxassetid://12787561060",
+        ["minLevel"] = 0,
+        ["displayOrder"] = 3,
+        ["isBoostable"] = true,
+        ["disabled"] = false,
+        ["description"] = "The jump stat determines how high your horse can jump.",
+        ["colour"] = Color3.fromRGB(120, 182, 255)
+    },
+    ["speed"] = {
+        ["image"] = "rbxassetid://12787560159",
+        ["minLevel"] = 0,
+        ["displayOrder"] = 1,
+        ["maxLevel"] = 100,
+        ["isBoostable"] = true,
+        ["description"] = "The speed stat determines how fast your horse can run!",
+        ["colour"] = Color3.fromRGB(131, 182, 203)
+    },
+    ["friendliness"] = {
+        ["image"] = "rbxassetid://12787561310",
+        ["untrainable"] = true,
+        ["displayOrder"] = 6,
+        ["minLevel"] = 0,
+        ["uncappable"] = true,
+        ["maxLevel"] = 100,
+        ["colour"] = Color3.fromRGB(254, 230, 132)
+    },
+    ["strength"] = {
+        ["image"] = "rbxassetid://12787559783",
+        ["minLevel"] = 0,
+        ["displayOrder"] = 4,
+        ["maxLevel"] = 100,
+        ["isBoostable"] = true,
+        ["description"] = "The strength stat determines how resilient your horse is when moving through tough terrain or going up hills, or when pulling vehicles!",
+        ["colour"] = Color3.fromRGB(118, 118, 125)
+    },
+    ["stamina"] = {
+        ["image"] = "rbxassetid://12787559922",
+        ["minLevel"] = 0,
+        ["displayOrder"] = 2,
+        ["maxLevel"] = 100,
+        ["description"] = "The stamina stat determines how long your horse can run without resting!",
+        ["colour"] = Color3.fromRGB(255, 236, 233)
+    }
+}
