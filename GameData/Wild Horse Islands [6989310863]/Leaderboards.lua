2ef7ef8e6fc052a1a22afec6d254272a09d7d036
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:33:58 +1100

    Create Leaderboards.lua

---
+++ GameData/Wild Horse Islands [6989310863]/Leaderboards.lua
@@ -0,0 +1,423 @@
+data = {
+    ["stats"] = {
+        ["bestSteeplechaseTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Steeplechase Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["timesReportedForScamming"] = {
+            ["restricted"] = true,
+            ["displayText"] = "Times Reported (MODS ONLY)",
+            ["timeCategories"] = {"All Time"},
+            ["order"] = 0
+        },
+        ["bestCross Country CourseTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Cross Country Course Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestJungle HillTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Jungle Hill Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["itemsHarvested"] = {
+            ["order"] = 7,
+            ["displayText"] = "Resources Harvested",
+            ["timeCategories"] = {"Weekly", "All Time"}
+        },
+        ["totalFishCaught"] = {
+            ["order"] = 9,
+            ["displayText"] = "Fish Caught",
+            ["timeCategories"] = {"Weekly", "All Time"}
+        },
+        ["timePlayed"] = {
+            ["restricted"] = true,
+            ["requireRank"] = "Owner",
+            ["displayText"] = "Time Played (STAFF ONLY)",
+            ["timeCategories"] = {"All Time"},
+            ["order"] = -5
+        },
+        ["racesCompleted"] = {
+            ["order"] = 4,
+            ["displayText"] = "Races Completed",
+            ["timeCategories"] = {"Weekly", "All Time"}
+        },
+        ["bestMountain WaterfallTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Mountain Waterfall Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestMountain HillTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Mountain Hill Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestMainland LoopTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Mainland Loop Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestGlacial CavernTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Glacial Cavern Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["itemsCollected"] = {
+            ["restricted"] = true,
+            ["displayText"] = "Items Collected (MODS ONLY)",
+            ["timeCategories"] = {"Weekly", "All Time"},
+            ["order"] = -4
+        },
+        ["bestInfernal AscensionTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Infernal Ascension Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestGrand CanyonTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Grand Canyon Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestJungle WaterfallTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Jungle Waterfall Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestRacetrackTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Racetrack Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestAround The QuarryTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Around The Quarry Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestBarrelTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Barrel Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestInto The SphinxTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Into The Sphinx Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestBeginner Show JumpingTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Beginner Show Jumping Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["trainingActivitiesCompleted"] = {
+            ["order"] = 10,
+            ["displayText"] = "Training Activities Completed",
+            ["timeCategories"] = {"Weekly", "All Time"}
+        },
+        ["bestBumpy BeachTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Bumpy Beach Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestRoyal LoopTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Royal Loop Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestSnowy CrescentTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Snowy Crescent Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["totalInventoryValue"] = {
+            ["restricted"] = true,
+            ["displayText"] = "Total Inventory Value (MODS ONLY)",
+            ["timeCategories"] = {"Weekly", "All Time"},
+            ["order"] = -2
+        },
+        ["bestForest WaterfallTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Forest Waterfall Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["totalTreasureFound"] = {
+            ["order"] = 8,
+            ["displayText"] = "Treasure Found",
+            ["timeCategories"] = {"Weekly", "All Time"}
+        },
+        ["bestFlower FieldTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Flower Field Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["QP"] = {["order"] = 2, ["displayText"] = "Quest Points", ["timeCategories"] = {"Weekly", "All Time"}},
+        ["bestField of IceTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Field of Ice Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestRocket SprintTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Rocket Sprint Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["Tokens"] = {
+            ["restricted"] = true,
+            ["displayText"] = "Tokens (MODS ONLY)",
+            ["timeCategories"] = {"Weekly", "All Time"},
+            ["order"] = -1
+        },
+        ["loginStreak"] = {
+            ["disabled"] = true,
+            ["requireRank"] = "Owner",
+            ["displayText"] = "Login Streak",
+            ["timeCategories"] = {"All Time"},
+            ["order"] = -6
+        },
+        ["bestPole BendingTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Pole Bending Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["timesCaughtCheating"] = {
+            ["restricted"] = true,
+            ["displayText"] = "Times Cheating Detected (MODS ONLY)",
+            ["timeCategories"] = {"Weekly", "All Time"},
+            ["order"] = -9
+        },
+        ["animalBondEarned"] = {
+            ["order"] = 5,
+            ["displayText"] = "Horse Bond Earned",
+            ["timeCategories"] = {"Weekly", "All Time"}
+        },
+        ["bestAdvanced Show JumpingTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Advanced Show Jumping Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestStalagmiteTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Stalagmite Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestMountain CaveTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Mountain Cave Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestReverse Cross Country CourseTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Reverse Cross Country Course Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestTrail RideTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Trail Ride Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestAround TownTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Around Town Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestGeyser SprintTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Geyser Sprint Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestForest SprintTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Forest Sprint Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["trainingReceiptsEarnedFromActivities"] = {
+            ["restricted"] = true,
+            ["displayText"] = "Training Receipts Earned (MODS ONLY)",
+            ["timeCategories"] = {"Weekly", "All Time"},
+            ["order"] = -8
+        },
+        ["horsesCaptured"] = {
+            ["order"] = 3,
+            ["displayText"] = "Wild Horses Captured",
+            ["timeCategories"] = {"Weekly", "All Time"}
+        },
+        ["XP"] = {["order"] = 1, ["displayText"] = "Player XP", ["timeCategories"] = {"Weekly", "All Time"}},
+        ["robuxSpent"] = {
+            ["restricted"] = true,
+            ["requireRank"] = "Owner",
+            ["displayText"] = "Robux Spent (STAFF ONLY)",
+            ["timeCategories"] = {"All Time"},
+            ["order"] = -3
+        },
+        ["bestLunar LoopTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Lunar Loop Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["bestDesert LoopTime"] = {
+            ["ModifyValue"] = "function: 0xf8ece448af0f3ad0",
+            ["saveOnImprovement"] = true,
+            ["lowerIsBetter"] = true,
+            ["order"] = 999,
+            ["displayText"] = "Desert Loop Race Time",
+            ["timeCategories"] = {"All Time"},
+            ["FormatValue"] = "function: 0x20c0fad04a87da00"
+        },
+        ["playerDataSize"] = {
+            ["restricted"] = true,
+            ["requireRank"] = "Owner",
+            ["displayText"] = "Data Size (bytes)",
+            ["timeCategories"] = {"All Time"},
+            ["order"] = -7
+        },
+        ["tradingRep"] = {
+            ["restricted"] = true,
+            ["order"] = -9,
+            ["displayText"] = "Trading Experience",
+            ["timeCategories"] = {"All Time"},
+            ["defaultValue"] = 0
+        },
+        ["foalsBred"] = {
+            ["restricted"] = true,
+            ["order"] = -9,
+            ["displayText"] = "Foals Bred",
+            ["timeCategories"] = {"All Time"},
+            ["defaultValue"] = 0
+        }
+    },
+    ["version"] = "0",
+    ["timeCategories"] = {
+        ["Monthly"] = {["GetScope"] = "function: 0xe1c2437b0fd87eb0", ["order"] = 3},
+        ["Daily"] = {["GetScope"] = "function: 0xf04d58468f7efc70", ["order"] = 1},
+        ["Weekly"] = {["GetScope"] = "function: 0xfb05d4dd4f89bd10", ["order"] = 2},
+        ["All Time"] = {["order"] = 4}
+    }
+}
