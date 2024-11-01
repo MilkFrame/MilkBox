d595f3d8aaf3713664857b63b2d3ffc64a3572b8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:37:43 +1100

    Create IslandUnlocks.lua

---
+++ GameData/Wild Horse Islands [6989310863]/IslandUnlocks.lua
@@ -0,0 +1,42 @@
+data = {
+    ["Desert Island"] = {
+        ["levelRequired"] = 60,
+        ["islandImage"] = "rbxassetid://16269469261",
+        ["unlockImages"] = {"rbxassetid://13127477203", "rbxassetid://8188513378"}
+    },
+    ["Blizzard Island"] = {
+        ["levelRequired"] = 5,
+        ["islandImage"] = "rbxassetid://16269469390",
+        ["unlockImages"] = {"rbxassetid://13127477203", "rbxassetid://8188513679", "rbxassetid://8188515913"}
+    },
+    ["Royal Island"] = {
+        ["levelRequired"] = 45,
+        ["islandImage"] = "rbxassetid://16269468340",
+        ["unlockImages"] = {"rbxassetid://9809422439", "rbxassetid://9809422829", "rbxassetid://9809421952"}
+    },
+    ["Volcano Island"] = {
+        ["levelRequired"] = 500,
+        ["islandImage"] = "rbxassetid://16269467744",
+        ["unlockImages"] = {"rbxassetid://10914644814", "rbxassetid://10914591310", "rbxassetid://10914590725"}
+    },
+    ["Lunar Islands"] = {
+        ["levelRequired"] = 400,
+        ["islandImage"] = "rbxassetid://16269468846",
+        ["unlockImages"] = {"rbxassetid://13127477203", "rbxassetid://8188516286", "rbxassetid://8269976519"}
+    },
+    ["Jungle Island"] = {
+        ["levelRequired"] = 250,
+        ["islandImage"] = "rbxassetid://16269468995",
+        ["unlockImages"] = {"rbxassetid://13127477203", "rbxassetid://8188516382"}
+    },
+    ["Forest Island"] = {
+        ["levelRequired"] = 20,
+        ["islandImage"] = "rbxassetid://16269469150",
+        ["unlockImages"] = {"rbxassetid://13127477203"}
+    },
+    ["Mountain Island"] = {
+        ["levelRequired"] = 150,
+        ["islandImage"] = "rbxassetid://16269468500",
+        ["unlockImages"] = {"rbxassetid://13127477203", "rbxassetid://8188513500"}
+    }
+}
