b5d649e36b65f2b3ca0c116f9d198ee0608e24bf
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:28:41 +1100

    Create AlternateHorseGaits.lua

---
+++ GameData/Wild Horse Islands [6989310863]/AlternateHorseGaits.lua
@@ -0,0 +1,34 @@
+data = {
+    ["extendedCanter"] = {["name"] = "Extended Canter", ["skillRequired"] = "extendedGaits"},
+    ["collectedCanter"] = {["name"] = "Collected Canter", ["skillRequired"] = "collectedGaits"},
+    ["collectedTrot"] = {["name"] = "Collected Trot", ["skillRequired"] = "collectedGaits"},
+    ["collectedWalk"] = {["target"] = 7, ["name"] = "Collected Walk", ["skillRequired"] = "collectedGaits"},
+    ["extendedWalk"] = {["name"] = "Extended Walk", ["skillRequired"] = "extendedGaits"},
+    ["tolt"] = {["target"] = 10, ["name"] = "Tolt", ["breedLocked"] = true},
+    ["spanishWalk"] = {
+        ["target"] = 8,
+        ["movementSpeed"] = 6,
+        ["name"] = "Spanish Walk",
+        ["skillRequired"] = "spanishWalk"
+    },
+    ["westernPleasureTrot"] = {
+        ["movementSpeed"] = 8.5,
+        ["target"] = 6,
+        ["name"] = "Jog",
+        ["skillRequired"] = "westernPleasureGaits"
+    },
+    ["flyingPace"] = {["target"] = 25, ["name"] = "Flying Pace", ["breedLocked"] = true},
+    ["extendedTrot"] = {["name"] = "Extended Trot", ["skillRequired"] = "extendedGaits"},
+    ["westernPleasureCanter"] = {
+        ["movementSpeed"] = 10,
+        ["target"] = 12,
+        ["name"] = "Lope",
+        ["skillRequired"] = "westernPleasureGaits"
+    },
+    ["westernPleasureWalk"] = {
+        ["movementSpeed"] = 4,
+        ["target"] = 6,
+        ["name"] = "Western Walk",
+        ["skillRequired"] = "westernPleasureGaits"
+    }
+}
