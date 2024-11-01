5ab2cb08d7ac407564a7ad3a91755392c748c38c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:35:57 +1100

    Create AnimalSkills.lua

---
+++ GameData/Wild Horse Islands [6989310863]/AnimalSkills.lua
@@ -0,0 +1,152 @@
+data = {
+    ["lightMagic"] = {
+        ["name"] = "Light Magic",
+        ["customLearningOnly"] = true,
+        ["abilities"] = {"lightTheWay"},
+        ["description"] = [[Perform the Light Magic ability: "Light The Way"]],
+        ["explanation"] = [[You've bonded with Wildstar! You can now perform their magic ability: Light The Way!
+
+To perform abilities, click on their image at the top right of your screen, select Abilities, then select the ability you'd like to perform.]]
+    },
+    ["spanishWalk"] = {
+        ["cost"] = 250,
+        ["description"] = "Perform the Spanish Walk movement.",
+        ["name"] = "Spanish Walk",
+        ["explanation"] = [[This horse can now perform the Spanish Walk alternate gait!
+
+When the gait icon on the left side of your screen displays a horseshoe pattern, select it to choose an alternate gait for your current speed!]]
+    },
+    ["westernPleasureGaits"] = {
+        ["cost"] = 500,
+        ["description"] = "Perform the Western Pleasure style Walk, Jog, & Lope.",
+        ["name"] = "Western Pleasure Gaits",
+        ["explanation"] = [[This horse can now perform the Western Walk, Jog, & Lope alternate gaits! To use an alternate gait:
+
+When the gait icon on the left side of your screen displays a horseshoe pattern, select it to choose an alternate gait for your current speed!]]
+    },
+    ["ridingDouble"] = {
+        ["cost"] = 625,
+        ["description"] = "Have a passenger sit on your horse with you.",
+        ["name"] = "Riding Double",
+        ["explanation"] = [[This horse can now have a secondary passenger riding on them! To ride double:
+
+- Walk up to a player who would like to ride double & interact with them
+- Select Invite to Ride Double
+- If they accept, they will begin riding on the back of your horse!]]
+    },
+    ["fastGallop"] = {
+        ["cost"] = 2250,
+        ["description"] = "Gallop at a higher speed for a shorter duration.",
+        ["name"] = "Fast Gallop",
+        ["explanation"] = "This horse can now gallop at a very high speed. Their stamina will drain very fast in the fast gallop, so use it wisely."
+    },
+    ["collectedGaits"] = {
+        ["cost"] = 1125,
+        ["description"] = "Perform the Collected Walk, Trot, & Canter.",
+        ["name"] = "Collected Gaits",
+        ["explanation"] = [[This horse can now perform the Collected Walk, Collected Trot, & Collected Canter alternate gaits!
+
+When the gait icon on the left side of your screen displays a horseshoe pattern, select it to choose an alternate gait for your current speed!]]
+    },
+    ["fireMagic"] = {
+        ["name"] = "Fire Magic",
+        ["customLearningOnly"] = true,
+        ["abilities"] = {"fireball"},
+        ["description"] = [[Perform the Fire Magic ability: "Fireball"]],
+        ["explanation"] = [[You've bonded with Cinder! You can now perform their magic ability: Fireball!
+
+To perform abilities, click on their image at the top right of your screen, select Abilities, then select the ability you'd like to perform.]]
+    },
+    ["slidingStop"] = {
+        ["cost"] = 750,
+        ["description"] = "Perform a sliding stop from a high speed.",
+        ["name"] = "Sliding Stop",
+        ["explanation"] = [[This horse can now perform a sliding stop! To do this:
+
+- Make sure you're moving at a high speed (Canter or higher)
+- Press and hold the STOP button on the keyboard or on your screen for a moment
+- Your horse will begin to slide to a stop!]]
+    },
+    ["plantMagic"] = {
+        ["name"] = "Plant Magic",
+        ["customLearningOnly"] = true,
+        ["abilities"] = {"plantSummon"},
+        ["description"] = [[Perform the Plant Magic ability: "Plant Summon"]],
+        ["explanation"] = [[You've bonded with Leaf! You can now perform their magic ability: Plant Summon!
+
+To perform abilities, click on their image at the top right of your screen, select Abilities, then select the ability you'd like to perform.]]
+    },
+    ["weatherMagic"] = {
+        ["name"] = "Weather Magic",
+        ["customLearningOnly"] = true,
+        ["abilities"] = {"thunderClap"},
+        ["description"] = [[Perform the Weather Magic ability: "Thunder Clap"]],
+        ["explanation"] = [[You've bonded with Storm! You can now perform their magic ability: Thunder Clap!
+
+To perform abilities, click on their image at the top right of your screen, select Abilities, then select the ability you'd like to perform.]]
+    },
+    ["iceMagic"] = {
+        ["name"] = "Ice Magic",
+        ["customLearningOnly"] = true,
+        ["abilities"] = {"snowStorm"},
+        ["description"] = [[Perform the Ice Magic ability: "Snow Storm"]],
+        ["explanation"] = [[You've bonded with Glacier! You can now perform their magic ability: Snow Storm!
+
+To perform abilities, click on their image at the top right of your screen, select Abilities, then select the ability you'd like to perform.]]
+    },
+    ["waterMagic"] = {
+        ["name"] = "Water Magic",
+        ["customLearningOnly"] = true,
+        ["abilities"] = {"tidalWave"},
+        ["description"] = [[Perform the Water Magic ability: "Tidal Wave"]],
+        ["explanation"] = [[You've bonded with River! You can now perform their magic ability: Tidal Wave!
+
+To perform abilities, click on their image at the top right of your screen, select Abilities, then select the ability you'd like to perform.]]
+    },
+    ["fastStaminaRegeneration"] = {
+        ["description"] = "Regenerate stamina faster.",
+        ["staminaRegenModifier"] = 1.5,
+        ["name"] = "Deep Breaths",
+        ["cost"] = 1875
+    },
+    ["vehiclePlus2"] = {
+        ["name"] = "Vehicle Speed++",
+        ["skillsRequired"] = {"vehiclePlus"},
+        ["description"] = "Gallop while pulling a vehicle.",
+        ["cost"] = 1500
+    },
+    ["illusionMagic"] = {
+        ["name"] = "Illusion Magic",
+        ["customLearningOnly"] = true,
+        ["abilities"] = {"fireworks"},
+        ["description"] = [[Perform the Illusion Magic ability: "Fireworks"]],
+        ["explanation"] = [[You've bonded with Ethera! You can now perform their magic ability: Fireworks!
+
+To perform abilities, click on their image at the top right of your screen, select Abilities, then select the ability you'd like to perform.]]
+    },
+    ["vehiclePlus"] = {["description"] = "Canter while pulling a vehicle.", ["name"] = "Vehicle Speed+", ["cost"] = 500},
+    ["extendedGaits"] = {
+        ["cost"] = 1125,
+        ["description"] = "Perform the Extended Walk, Trot, & Canter.",
+        ["name"] = "Extended Gaits",
+        ["explanation"] = [[This horse can now perform the Extended Walk, Extended Trot, & Extended Canter alternate gaits!
+
+When the gait icon on the left side of your screen displays a horseshoe pattern, select it to choose an alternate gait for your current speed!]]
+    },
+    ["trainingReceiptsPlus"] = {
+        ["description"] = "Earn more Training Receipts when completing activities on Training Island.",
+        ["trainingReceiptsModifier"] = 1.25,
+        ["name"] = "Training Efficiency",
+        ["cost"] = 175
+    },
+    ["piaffeEmote"] = {
+        ["cost"] = 125,
+        ["description"] = "Perform the Piaffe dressage emote.",
+        ["name"] = "Piaffe",
+        ["explanation"] = [[To perform the Piaffe emote:
+
+- Equip your horse
+- Click the Emotes button at the top right of the screen
+- Select Piaffe.]]
+    }
+}
