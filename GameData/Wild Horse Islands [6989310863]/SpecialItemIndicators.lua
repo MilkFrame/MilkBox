55d87b691d29da8e5db151dfbd3703c1953ed2e3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:32:26 +1100

    Create SpecialItemIndicators.lua

---
+++ GameData/Wild Horse Islands [6989310863]/SpecialItemIndicators.lua
@@ -0,0 +1,94 @@
+data = {
+    ["autumn"] = {["text"] = "Autumn Limited", ["priority"] = 0, ["colour"] = Color3.fromRGB(194, 126, 16)},
+    ["spring"] = {["colour"] = Color3.fromRGB(92, 236, 120), ["text"] = "Spring Limited"},
+    ["holiday2023"] = {["text"] = "Holiday '23 Limited", ["priority"] = 0, ["colour"] = Color3.fromRGB(120, 255, 131)},
+    ["codeUnique"] = {["text"] = "Redemption Code Unique", ["priority"] = 0, ["colour"] = Color3.fromRGB(255, 220, 151)},
+    ["spring2023"] = {["colour"] = Color3.fromRGB(92, 236, 120), ["text"] = "Spring '23 Limited"},
+    ["autumn2022"] = {["text"] = "Autumn '22 Limited", ["priority"] = 0, ["colour"] = Color3.fromRGB(194, 126, 16)},
+    ["halloween"] = {["text"] = "Halloween Limited", ["priority"] = 0, ["colour"] = Color3.fromRGB(255, 140, 0)},
+    ["stpatricksday"] = {
+        ["text"] = "St. Patrick's Limited",
+        ["priority"] = 2,
+        ["colour"] = Color3.fromRGB(104, 236, 104)
+    },
+    ["premium"] = {["text"] = "Premium", ["priority"] = 8, ["colour"] = Color3.fromRGB(236, 204, 76)},
+    ["volcanicMineralsUnique"] = {
+        ["text"] = "Volcanic Minerals Unique",
+        ["priorty"] = 15,
+        ["colour"] = Color3.fromRGB(255, 94, 14)
+    },
+    ["holiday"] = {["text"] = "Holiday Limited", ["priority"] = 0, ["colour"] = Color3.fromRGB(120, 255, 131)},
+    ["winter2023"] = {["text"] = "Winter '23 Limited", ["priority"] = 0, ["colour"] = Color3.fromRGB(212, 247, 255)},
+    ["summer"] = {["text"] = "Summer Limited", ["priority"] = 4, ["colour"] = Color3.fromRGB(236, 226, 82)},
+    ["event"] = {["text"] = "Event Limited", ["priority"] = 0, ["colour"] = Color3.fromRGB(221, 85, 255)},
+    ["loginRewardUnique"] = {
+        ["text"] = "Login Streak Unique",
+        ["priorty"] = 13,
+        ["colour"] = Color3.fromRGB(206, 60, 206)
+    },
+    ["valentines"] = {["text"] = "Valentine's Limited", ["priority"] = 1, ["colour"] = Color3.fromRGB(255, 167, 255)},
+    ["summer2023"] = {["text"] = "Summer '23 Limited", ["priority"] = 4, ["colour"] = Color3.fromRGB(236, 222, 68)},
+    ["valentines2022"] = {
+        ["text"] = "Valentine's '22 Limited",
+        ["priority"] = 1,
+        ["colour"] = Color3.fromRGB(255, 167, 255)
+    },
+    ["legacyItem"] = {["text"] = "Legacy Item", ["priority"] = 0, ["colour"] = Color3.fromRGB(228, 255, 187)},
+    ["spring2024"] = {["colour"] = Color3.fromRGB(98, 252, 126), ["text"] = "Spring '24 Limited"},
+    ["autumn2023"] = {["text"] = "Autumn '23 Limited", ["priority"] = 0, ["colour"] = Color3.fromRGB(194, 159, 17)},
+    ["winter"] = {["text"] = "Winter Limited", ["priority"] = 0, ["colour"] = Color3.fromRGB(212, 247, 255)},
+    ["halloween2022"] = {["text"] = "Halloween '22 Limited", ["priority"] = 0, ["colour"] = Color3.fromRGB(255, 140, 0)},
+    ["valentines2024"] = {
+        ["text"] = "Valentine's '24 Limited",
+        ["priority"] = 1,
+        ["colour"] = Color3.fromRGB(255, 167, 255)
+    },
+    ["legacyCoat"] = {["text"] = "Legacy Coat", ["priority"] = 11, ["colour"] = Color3.fromRGB(228, 255, 187)},
+    ["breedingUnique"] = {["text"] = "Breeding Unique", ["priority"] = 5, ["colour"] = "rainbow"},
+    ["holiday2022"] = {["text"] = "Holiday '22 Limited", ["priority"] = 0, ["colour"] = Color3.fromRGB(120, 255, 131)},
+    ["summer2022"] = {["text"] = "Summer '22 Limited", ["priority"] = 4, ["colour"] = Color3.fromRGB(236, 226, 82)},
+    ["treasureUnique"] = {["text"] = "Treasure Unique", ["priority"] = 7, ["colour"] = Color3.fromRGB(255, 42, 174)},
+    ["autumn2024"] = {["text"] = "Autumn '24 Limited", ["priority"] = 0, ["colour"] = Color3.fromRGB(194, 159, 17)},
+    ["easter2022"] = {["text"] = "Easter '22 Limited", ["priority"] = 3, ["colour"] = Color3.fromRGB(99, 236, 232)},
+    ["archaeologyUnique"] = {
+        ["text"] = "Archaeological Find",
+        ["priority"] = 6,
+        ["colour"] = Color3.fromRGB(255, 196, 46)
+    },
+    ["legacyBreed"] = {["text"] = "Legacy Breed", ["priority"] = 10, ["colour"] = Color3.fromRGB(228, 255, 187)},
+    ["winter2022"] = {["text"] = "Winter '22 Limited", ["priority"] = 0, ["colour"] = Color3.fromRGB(212, 247, 255)},
+    ["cobreedingUnique"] = {["text"] = "Co-breeding Unique", ["priority"] = 5, ["colour"] = "rainbow"},
+    ["secretItem"] = {["text"] = "Secret Item", ["priority"] = 9, ["colour"] = Color3.fromRGB(255, 129, 243)},
+    ["halloween2023"] = {["text"] = "Halloween '23 Limited", ["priority"] = 0, ["colour"] = Color3.fromRGB(239, 127, 0)},
+    ["stpatricksday2022"] = {
+        ["text"] = "St. Patrick's '22 Limited",
+        ["priority"] = 2,
+        ["colour"] = Color3.fromRGB(104, 236, 104)
+    },
+    ["trainingReceiptsUnique"] = {
+        ["text"] = "Training Island Unique",
+        ["priority"] = 0,
+        ["colour"] = Color3.fromRGB(0, 195, 255)
+    },
+    ["summer2024"] = {["text"] = "Summer '24 Limited", ["priority"] = 4, ["colour"] = Color3.fromRGB(236, 222, 68)},
+    ["newYears"] = {["text"] = "New Year's Limited", ["priority"] = 0, ["colour"] = Color3.fromRGB(255, 192, 33)},
+    ["halloween2024"] = {["text"] = "Halloween '24 Limited", ["priority"] = 0, ["colour"] = Color3.fromRGB(239, 127, 0)},
+    ["easter"] = {["text"] = "Easter Limited", ["priority"] = 3, ["colour"] = Color3.fromRGB(99, 236, 232)},
+    ["freeGift"] = {["text"] = "Free Gift", ["priority"] = 0, ["colour"] = Color3.fromRGB(152, 111, 255)},
+    ["islandUniqueCoat"] = {
+        ["text"] = "Island Unique Coat",
+        ["priority"] = 12,
+        ["colour"] = Color3.fromRGB(255, 108, 89)
+    },
+    ["islandUniqueHairColour"] = {
+        ["text"] = "Island Unique Hair",
+        ["priorty"] = 14,
+        ["colour"] = Color3.fromRGB(255, 87, 137)
+    },
+    ["UCA"] = {["text"] = "Unicorn Academy Limited", ["priority"] = 0, ["colour"] = Color3.fromRGB(135, 43, 255)},
+    ["valentines2023"] = {
+        ["text"] = "Valentine's '23 Limited",
+        ["priority"] = 1,
+        ["colour"] = Color3.fromRGB(255, 167, 255)
+    }
+}
