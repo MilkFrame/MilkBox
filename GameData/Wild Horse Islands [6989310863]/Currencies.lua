08379285b13df13feed60f4aa66ff0e67c0a14a0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:29:20 +1100

    Create Currencies.lua

---
+++ GameData/Wild Horse Islands [6989310863]/Currencies.lua
@@ -0,0 +1,20 @@
+data = {
+    ["Winter Tokens"] = {["image"] = "rbxassetid://15637084003"},
+    ["Robux"] = {["image"] = "rbxassetid://13279597747"},
+    ["Autumn Tokens"] = {["image"] = "rbxassetid://14933394965"},
+    ["Academy Tokens"] = {["image"] = "rbxassetid://89116850213429"},
+    ["Carnival Tickets"] = {
+        ["passiveEventRewardModifier"] = 7,
+        ["image"] = "rbxassetid://18446995075",
+        ["rewardPickupRewardModifier"] = 3,
+        ["tokensConversionModifier"] = 0.1,
+        ["sounds"] = {["increase"] = "rbxassetid://6283742188"}
+    },
+    ["Summer Tokens"] = {["image"] = "rbxassetid://13734752399"},
+    ["Tokens"] = {["image"] = "rbxassetid://13116451197"},
+    ["Easter Tokens"] = {["image"] = "rbxassetid://9361516940"},
+    ["Spring Tokens"] = {["image"] = "rbxassetid://13116451487"},
+    ["Unicorn Battle Tokens"] = {["image"] = "rbxassetid://15375247435"},
+    ["Clover Tokens"] = {["image"] = "rbxassetid://9119400894"},
+    ["Valentine's Tokens"] = {["image"] = "rbxassetid://16126689788"}
+}
