9f06a7dfa930318fbca6efb66131576252aa6717
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:32:41 +1100

    Create PlayerMarkets.lua

---
+++ GameData/Wild Horse Islands [6989310863]/PlayerMarkets.lua
@@ -0,0 +1,61 @@
+data = {
+    {
+        ["totalStands"] = 1,
+        ["name"] = "Tier 1 Market",
+        ["description"] = [[Market Stands: 1
+Horse Stalls: 0]]
+    },
+    {
+        ["totalStands"] = 2,
+        ["description"] = [[Market Stands: 2
+Horse Stalls: 0]],
+        ["name"] = "Tier 2 Market",
+        ["cost"] = 2500
+    },
+    {
+        ["totalStands"] = 3,
+        ["description"] = [[Market Stands: 3
+Horse Stalls: 0]],
+        ["name"] = "Tier 3 Market",
+        ["cost"] = 5000
+    },
+    {
+        ["totalStands"] = 4,
+        ["description"] = [[Market Stands: 4
+Horse Stalls: 0]],
+        ["name"] = "Tier 4 Market",
+        ["cost"] = 7500
+    },
+    {
+        ["totalStands"] = 5,
+        ["description"] = [[Market Stands: 5
+Horse Stalls: 0
++ Larry NPC]],
+        ["name"] = "Tier 5 Market",
+        ["cost"] = 15000
+    },
+    {
+        ["totalStands"] = 6,
+        ["description"] = [[Market Stands: 6
+Horse Stalls: 0
++ Larry NPC]],
+        ["name"] = "Tier 6 Market",
+        ["cost"] = 20000
+    },
+    {
+        ["totalStands"] = 7,
+        ["description"] = [[Market Stands: 6
+Horse Stalls: 1
++ Larry NPC]],
+        ["name"] = "Tier 7 Market",
+        ["cost"] = 30000
+    },
+    {
+        ["totalStands"] = 8,
+        ["description"] = [[Market Stands: 6
+Horse Stalls: 2
++ Larry NPC]],
+        ["name"] = "Tier 8 Market",
+        ["cost"] = 60000
+    }
+}
