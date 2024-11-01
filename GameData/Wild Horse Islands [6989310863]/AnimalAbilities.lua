423a27d20828962a90f8d2b8438ffc6a748ed708
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:36:29 +1100

    Create AnimalAbilities.lua

---
+++ GameData/Wild Horse Islands [6989310863]/AnimalAbilities.lua
@@ -0,0 +1,73 @@
+data = {
+    ["thunderClap"] = {
+        ["noReplicationCooldownOnIslands"] = {"Battlefield"},
+        ["description"] = "Create a roaring thunder on demand!",
+        ["replicationDistance"] = 150,
+        ["image"] = "rbxassetid://14344853891",
+        ["name"] = "Thunder Clap",
+        ["replicationCooldown"] = 60,
+        ["isFantasy"] = true,
+        ["cooldown"] = 5,
+        ["colour"] = Color3.fromRGB(102, 77, 132)
+    },
+    ["plantSummon"] = {
+        ["description"] = "Summon a variety of plants into the world right at your feet!",
+        ["noReplicationCooldownOnIslands"] = {"Battlefield"},
+        ["name"] = "Plant Summon",
+        ["image"] = "rbxassetid://14344854369",
+        ["isFantasy"] = true,
+        ["replicationCooldown"] = 30,
+        ["cooldown"] = 5,
+        ["colour"] = Color3.fromRGB(0, 147, 110)
+    },
+    ["fireworks"] = {
+        ["description"] = "Show a magical display of fireworks right overhead!",
+        ["noReplicationCooldownOnIslands"] = {"Battlefield"},
+        ["name"] = "Fireworks",
+        ["image"] = "rbxassetid://576952654",
+        ["isFantasy"] = true,
+        ["replicationCooldown"] = 60,
+        ["cooldown"] = 10,
+        ["colour"] = Color3.fromRGB(0, 55, 255)
+    },
+    ["snowStorm"] = {
+        ["description"] = "Summon a blistering snow storm right over your head!",
+        ["noReplicationCooldownOnIslands"] = {"Battlefield"},
+        ["name"] = "Snow Storm",
+        ["image"] = "rbxassetid://14344854052",
+        ["isFantasy"] = true,
+        ["replicationCooldown"] = 90,
+        ["cooldown"] = 15,
+        ["colour"] = Color3.fromRGB(78, 156, 171)
+    },
+    ["tidalWave"] = {
+        ["description"] = "Summon a mighty wave of water that surges forward!",
+        ["noReplicationCooldownOnIslands"] = {"Battlefield"},
+        ["name"] = "Tidal Wave",
+        ["image"] = "rbxassetid://14344854219",
+        ["isFantasy"] = true,
+        ["replicationCooldown"] = 60,
+        ["cooldown"] = 5,
+        ["colour"] = Color3.fromRGB(16, 60, 120)
+    },
+    ["lightTheWay"] = {
+        ["description"] = "Shine a bright light to see what's around you!",
+        ["noReplicationCooldownOnIslands"] = {"Battlefield"},
+        ["name"] = "Light The Way",
+        ["image"] = "rbxassetid://14344853723",
+        ["isFantasy"] = true,
+        ["replicationCooldown"] = 30,
+        ["cooldown"] = 5,
+        ["colour"] = Color3.fromRGB(191, 148, 54)
+    },
+    ["fireball"] = {
+        ["description"] = "Shoot a flaming ball of fire straight forward!",
+        ["noReplicationCooldownOnIslands"] = {"Battlefield"},
+        ["name"] = "Fireball",
+        ["image"] = "rbxassetid://14344853616",
+        ["isFantasy"] = true,
+        ["replicationCooldown"] = 5,
+        ["cooldown"] = 2.5,
+        ["colour"] = Color3.fromRGB(203, 105, 25)
+    }
+}
