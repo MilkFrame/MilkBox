b50737c2fc4d12ec12ea88a9ee7491136c65f532
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:26:00 +1100

    Create NPCs.lua

---
+++ GameData/Wild Horse Islands [6989310863]/NPCs.lua
@@ -0,0 +1,204 @@
+data = {
+    ["Jones"] = {
+        ["animations"] = {["idle"] = "rbxassetid://845397899"},
+        ["recurringQuestNames"] = {
+            "Scavenger Hunt",
+            "Lost & Found",
+            "Special Delivery",
+            "Racin' For Carrots",
+            "Wild Horse Round-up"
+        },
+        ["specialQuests"] = {{["name"] = "Jones' Buried Treasure", ["triggerAfter"] = 5}},
+        ["specialItems"] = {"Old Novel", "Pocket Watch"}
+    },
+    ["Ava Banji"] = {
+        ["animations"] = {["idle"] = "rbxassetid://910004836", ["enteredProximity"] = "rbxassetid://4940564896"}
+    },
+    ["Boat Captain"] = {
+        ["animations"] = {["idle"] = "rbxassetid://750781874", ["enteredProximity"] = "rbxassetid://3344650532"}
+    },
+    ["Sophia Mendoza"] = {
+        ["animations"] = {["idle"] = "rbxassetid://616136790", ["enteredProximity"] = "rbxassetid://3344650532"}
+    },
+    ["Toby"] = {
+        ["animations"] = {["enteredProximity"] = "rbxassetid://3334832150"},
+        ["recurringQuestNames"] = {"Scavenger Hunt", "Lost & Found", "Racin' For Carrots", "Wild Horse Round-up"},
+        ["specialQuests"] = {
+            {["name"] = "Fishing Rod Shipment", ["triggerAfter"] = 3},
+            {["name"] = "Toby's Wellness Check", ["triggerAfter"] = 5}
+        },
+        ["specialItems"] = {"Tackle Box", "Canoe Paddle"}
+    },
+    ["Ghost Larry"] = {["animations"] = {["idle"] = "rbxassetid://10921132962"}},
+    ["Ruby"] = {
+        ["animations"] = {["idle"] = "rbxassetid://910004836", ["enteredProximity"] = "rbxassetid://4940561610"},
+        ["recurringQuestNames"] = {
+            "Scavenger Hunt",
+            "Lost & Found",
+            "Special Delivery",
+            "Racin' For Carrots",
+            "Wild Horse Round-up"
+        },
+        ["specialQuests"] = {{["name"] = "Grand Canyon Bridge", ["triggerAfter"] = 5}},
+        ["specialItems"] = {"Silk Scarf", "Sarcophagus"}
+    },
+    ["Cupid"] = {["animations"] = {["idle"] = "rbxassetid://8764752632"}, ["cancelQuestsOnLogin"] = true},
+    ["Wesley"] = {
+        ["animations"] = {["idle"] = "rbxassetid://657595757", ["enteredProximity"] = "rbxassetid://3344650532"}
+    },
+    ["Animal Trainer"] = {
+        ["animations"] = {["idle"] = "rbxassetid://910004836", ["enteredProximity"] = "rbxassetid://5230599789"}
+    },
+    ["Nancy"] = {
+        ["animations"] = {["idle"] = "rbxassetid://616136790", ["enteredProximity"] = "rbxassetid://4940564896"}
+    },
+    ["Animal Salesman"] = {
+        ["animations"] = {["idle"] = "rbxassetid://657595757", ["enteredProximity"] = "rbxassetid://3344650532"}
+    },
+    ["Francine"] = {
+        ["animations"] = {["idle"] = "rbxassetid://6986781799", ["enteredProximity"] = "rbxassetid://9350066872"}
+    },
+    ["Hank"] = {
+        ["animations"] = {["idle"] = "rbxassetid://845397899", ["enteredProximity"] = "rbxassetid://3344650532"}
+    },
+    ["Ms. Primrose"] = {
+        ["animations"] = {["idle"] = "rbxassetid://2510196951", ["enteredProximity"] = "rbxassetid://3338042785"}
+    },
+    ["Doug"] = {
+        ["specialItems"] = {"Ice Pick", "Camping Tent"},
+        ["recurringQuestNames"] = {
+            "Scavenger Hunt",
+            "Lost & Found",
+            "Special Delivery",
+            "Racin' For Carrots",
+            "Wild Horse Round-up"
+        }
+    },
+    ["Valentina Furi"] = {
+        ["animations"] = {["idle"] = "rbxassetid://2510196951", ["enteredProximity"] = "rbxassetid://5230599789"}
+    },
+    ["Horse"] = {
+        ["animations"] = {["idle"] = "rbxassetid://7783205175", ["enteredProximity"] = "rbxassetid://8405836918"}
+    },
+    ["Fate Fairy"] = {
+        ["animations"] = {
+            ["twirl"] = "rbxassetid://14279938723",
+            ["idle"] = "rbxassetid://14279246184",
+            ["swirlLoop"] = "rbxassetid://14279778324"
+        },
+        ["animationSpeeds"] = {["swirlLoop"] = 0.8}
+    },
+    ["Armstrong"] = {
+        ["animations"] = {["idle"] = "rbxassetid://891621366"},
+        ["recurringQuestNames"] = {
+            "Scavenger Hunt",
+            "Lost & Found",
+            "Special Delivery",
+            "Racin' For Carrots",
+            "Wild Horse Round-up"
+        },
+        ["specialItems"] = {"Rocket Fuel", "Space Helmet"}
+    },
+    ["Sebastian"] = {
+        ["animations"] = {["idle"] = "rbxassetid://657595757"},
+        ["recurringQuestNames"] = {
+            "Scavenger Hunt",
+            "Lost & Found",
+            "Special Delivery",
+            "Racin' For Carrots",
+            "Wild Horse Round-up"
+        },
+        ["specialItems"] = {"Jungle Hatchet", "Binoculars"}
+    },
+    ["Mike"] = {["animations"] = {["enteredProximity"] = "rbxassetid://5915693819"}},
+    ["Stephanie"] = {
+        ["animations"] = {["idle"] = "rbxassetid://910004836", ["enteredProximity"] = "rbxassetid://5230599789"},
+        ["recurringQuestNames"] = {"Train'em Up"}
+    },
+    ["Cal"] = {
+        ["animations"] = {["idle"] = "rbxassetid://657595757", ["enteredProximity"] = "rbxassetid://3344650532"},
+        ["recurringQuestNames"] = {
+            "Scavenger Hunt",
+            "Lost & Found",
+            "Special Delivery",
+            "Racin' For Carrots",
+            "Wild Horse Round-up"
+        },
+        ["specialItems"] = {"Trowel", "Clipboard"}
+    },
+    ["Winter Hubert"] = {["animations"] = {["idle"] = "rbxassetid://11721306117"}},
+    ["Winter Steven"] = {["animations"] = {["idle"] = "rbxassetid://11721205117"}},
+    ["Bhurnes"] = {
+        ["animations"] = {["idle"] = "rbxassetid://657595757", ["enteredProximity"] = "rbxassetid://3344650532"},
+        ["recurringQuestNames"] = {
+            "Scavenger Hunt",
+            "Lost & Found",
+            "Special Delivery",
+            "Racin' For Carrots",
+            "Wild Horse Round-up"
+        },
+        ["specialItems"] = {"Book of Rituals", "Runestone"}
+    },
+    ["Easter Bunny"] = {
+        ["animations"] = {["idle"] = "rbxassetid://9350063425", ["enteredProximity"] = "rbxassetid://9350066872"}
+    },
+    ["Pearl"] = {
+        ["animations"] = {["idle"] = "rbxassetid://910004836", ["enteredProximity"] = "rbxassetid://4940561610"}
+    },
+    ["Shella"] = {
+        ["animations"] = {["idle"] = "rbxassetid://657595757", ["enteredProximity"] = "rbxassetid://4940564896"}
+    },
+    ["Hubert"] = {
+        ["animations"] = {["idle"] = "rbxassetid://845397899"},
+        ["recurringQuestNames"] = {
+            "Scavenger Hunt",
+            "Lost & Found",
+            "Special Delivery",
+            "Racin' For Carrots",
+            "Wild Horse Round-up"
+        },
+        ["specialQuests"] = {{["name"] = "Hubert's Science Experiment", ["triggerAfter"] = 5}},
+        ["specialItems"] = {"Snow Shovel", "Salt Bag"}
+    },
+    ["Isabel Armstrong"] = {
+        ["animations"] = {["idle"] = "rbxassetid://616136790", ["enteredProximity"] = "rbxassetid://4940564896"}
+    },
+    ["Roaming Fate Fairy"] = {
+        ["animations"] = {["idle"] = "rbxassetid://14693835182", ["idle2"] = "rbxassetid://11307652883"},
+        ["animationSpeeds"] = {["idle2"] = {0.5, 1}}
+    },
+    ["Rory Carmichael"] = {
+        ["animations"] = {["idle"] = "rbxassetid://2510196951", ["enteredProximity"] = "rbxassetid://5230599789"}
+    },
+    ["Eternal Soul"] = {
+        ["animations"] = {["idle"] = "rbxassetid://10921132962", ["enteredProximity"] = "rbxassetid://4841407203"}
+    },
+    ["Holiday Spirit"] = {
+        ["animations"] = {["idle"] = "rbxassetid://11307652883"},
+        ["animationSpeeds"] = {["idle"] = {0.1, 0.5}}
+    },
+    ["Larry"] = {
+        ["animations"] = {["idle"] = "rbxassetid://657595757", ["enteredProximity"] = "rbxassetid://3344650532"}
+    },
+    ["Charlie"] = {
+        ["animations"] = {["idle"] = "rbxassetid://657595757", ["enteredProximity"] = "rbxassetid://3344650532"}
+    },
+    ["Default"] = {["animations"] = {}},
+    ["Elf"] = {["animations"] = {["idle"] = "rbxassetid://616136790", ["enteredProximity"] = "rbxassetid://4940564896"}},
+    ["Sunny"] = {
+        ["animations"] = {["idle"] = "rbxassetid://657595757", ["enteredProximity"] = "rbxassetid://4940564896"},
+        ["recurringQuestNames"] = {"Coral Exchange"},
+        ["cancelQuestsOnLogin"] = true
+    },
+    ["Soul"] = {
+        ["animations"] = {["idle"] = "rbxassetid://11307652883"},
+        ["animationSpeeds"] = {["idle"] = {0.25, 1.25}}
+    },
+    ["Layla Fletcher"] = {
+        ["animations"] = {["idle"] = "rbxassetid://2510196951", ["enteredProximity"] = "rbxassetid://3344650532"}
+    },
+    ["Spring Butterfly"] = {
+        ["animations"] = {["idle"] = "rbxassetid://13071932954", ["idle2"] = "rbxassetid://13083026468"},
+        ["animationSpeeds"] = {["idle"] = {0.5, 1.5}, ["idle2"] = {0.5, 1.25}}
+    }
+}
