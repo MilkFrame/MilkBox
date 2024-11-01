a49ac756a9dec56b379817453dd1dc55cbded9c9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:26:59 +1100

    Create DynamicArenas.lua

---
+++ GameData/Wild Horse Islands [6989310863]/DynamicArenas.lua
@@ -0,0 +1,60 @@
+data = {
+    ["ucaFigureEight"] = {
+        ["name"] = "Figure Eight Track",
+        ["layouts"] = {
+            {["name"] = "Racetrack", ["activity"] = "ucaRacetrack"},
+            {["name"] = "Steeplechase", ["activity"] = "ucaSteepleChase"},
+            {["name"] = "Racetrack Show Jumping", ["activity"] = "ucaRacetrackShowJumping"}
+        }
+    },
+    ["standard"] = {
+        ["name"] = "Arena",
+        ["layouts"] = {
+            {["name"] = "Single Stake", ["activity"] = "arenaSingleStake"},
+            {["name"] = "Pole Bending", ["activity"] = "arenaPoleBending"},
+            {["name"] = "Hurry Scurry", ["activity"] = "arenaHurryScurry"},
+            {["name"] = "Quadrangle", ["activity"] = "arenaQuadrangle"},
+            {["name"] = "Figure 8 Stake", ["activity"] = "arenaFigure8Stake"},
+            {["name"] = "Speed Barrels", ["activity"] = "arenaSpeedBarrels"},
+            {["name"] = "Birangle", ["activity"] = "arenaBirangle"},
+            {["name"] = "Big T", ["activity"] = "arenaBigT"},
+            {["name"] = "Barrel Racing", ["activity"] = "arenaBarrelRacing"},
+            {["name"] = "Show Jumping", ["activity"] = "arenaShowJumping"},
+            {["name"] = "Gridwork A", ["activity"] = "arenaGridworkA"},
+            {["name"] = "Gridwork B", ["activity"] = "arenaGridworkB"},
+            {["name"] = "Gridwork C", ["activity"] = "arenaGridworkC"},
+            {["name"] = "Gridwork D", ["activity"] = "arenaGridworkD"},
+            {["name"] = "Flatwork", ["activity"] = "arenaFlatwork"},
+            {["listOrder"] = -1, ["name"] = "Autumn Gridwork", ["activity"] = "arenaAutumnGridwork"},
+            {["listOrder"] = -1, ["name"] = "Autumn Show Jumping", ["activity"] = "arenaAutumnShowJumping"},
+            {["listOrder"] = -1, ["name"] = "Giant Toadstool Weave", ["activity"] = "arenaGiantToadstoolWeave"}
+        }
+    },
+    ["winterResortArena"] = {
+        ["name"] = "Arena",
+        ["layouts"] = {
+            {["name"] = "Christmas Tree Polework", ["activity"] = "arenaChristmasTreePolework"},
+            {["name"] = "Icicle Pole Bending", ["activity"] = "arenaIciclePoleBending"},
+            {["name"] = "Christmas Show Jumping", ["activity"] = "arenaChristmasShowJumping"},
+            {["name"] = "Snowman Barrel Racing", ["activity"] = "arenaSnowmanBarrelRacing"}
+        }
+    },
+    ["uca150x200"] = {
+        ["name"] = "Round Arena",
+        ["layouts"] = {
+            {["name"] = "Show Jumping A", ["activity"] = "ucaShowJumpingA"},
+            {["name"] = "Show Jumping B", ["activity"] = "ucaShowJumpingB"},
+            {["name"] = "Polework A", ["activity"] = "ucaPoleworkA"},
+            {["name"] = "Polework B", ["activity"] = "ucaPoleworkB"},
+            {["name"] = "Polework C", ["activity"] = "ucaPoleworkC"}
+        }
+    },
+    ["racetrack"] = {
+        ["name"] = "Racetrack",
+        ["layouts"] = {
+            {["name"] = "Standard Racetrack", ["activity"] = "racetrackStandard"},
+            {["name"] = "Steeplechase", ["activity"] = "racetrackSteeplechase"},
+            {["name"] = "Obstacle Course", ["activity"] = "racetrackObstacleCourse"}
+        }
+    }
+}
