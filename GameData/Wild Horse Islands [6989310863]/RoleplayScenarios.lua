affe91d56059dfd189717f5221159d35de21ff95
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:30:56 +1100

    Create RoleplayScenarios.lua

---
+++ GameData/Wild Horse Islands [6989310863]/RoleplayScenarios.lua
@@ -0,0 +1,137 @@
+data = {
+    {
+        ["prompt"] = "Player1 fell off their horse and hurt their knee! Player2 must help them back to barn and take care of their injury."
+    },
+    {
+        ["prompt"] = "Player1 is the riding instructor, and Player2 is the student. Player2 wants to learn about cross country. Go to the Cross Country Course and do a lesson!"
+    },
+    {
+        ["prompt"] = "Player1 is the riding instructor, and Player2 is the student. Player2 wants to learn about trail riding. Go to the trails and do a lesson!"
+    },
+    {
+        ["prompt"] = "Player1 is the riding instructor, and Player2 is the student. Player2 wants to learn about barrel racing. Go to the Barrel Racing arena and do a lesson!"
+    },
+    {
+        ["prompt"] = "Player1 is the riding instructor, and Player2 is the student. Player2 wants to learn about jumping. Go to the jumping arenas and do a lesson!"
+    },
+    {
+        ["prompt"] = "Player1 is the riding instructor, and Player2 is the student. Player2 wants to learn about racing. Go to the racetrack and do a lesson!"
+    },
+    {
+        ["prompt"] = "Player1 is the riding instructor, and Player2 is the student. Player2 wants to learn about steeplechase. Go to the steeplechase track and do a lesson!"
+    },
+    {
+        ["prompt"] = "Player1 is the riding instructor, and Player2 is the student. Player2 wants to learn about pole bending. Go to the pole bending arena and do a lesson!"
+    },
+    {
+        ["prompt"] = "Player2ou’re on a trail ride and Player1 gets bucked off of their horse! Player2, take them to the doctor and make sure they’re okay."
+    },
+    {
+        ["prompt"] = "Oh no, Player1’s horse stepped on a nail and got it stuck in their hoof! Player2, help them take their horse to the vet and get the nail out!"
+    },
+    {["prompt"] = "Player1 just arrived at Wild Horse Islands! Player2, take a moment to give them a tour of RP Island"},
+    {["prompt"] = "Go trail riding at RP Island and find a nice spot to tie your horses and have a picnic."},
+    {
+        ["prompt"] = "Player1 and Player2 are a team of horse trainers, but you disagree on how to train a new horse. Player2ou must resolve the conflict and work together again!"
+    },
+    {
+        ["prompt"] = "Player1 has just arrived at Wild Horse Islands, and they are a rich and famous Olympic rider! Player2 is their biggest fan and wants to learn all about them."
+    },
+    {
+        ["prompt"] = "Player1 has offended Player2 by telling them that their horse could never win a race. Prove them wrong at the racetrack."
+    },
+    {["prompt"] = "Player1 must help Player2 with mucking out the barn."},
+    {
+        ["prompt"] = "Player1 sees Player2’s horse at Wild Horse Outfitters and wants to tell them that they like their horse, but they are too shy to say anything."
+    },
+    {
+        ["prompt"] = "Player1’s horse got out of control and is quickly running them toward the pond. Player2, ride up behind them and help them calm their horse!"
+    },
+    {
+        ["prompt"] = "Player1 bought a gift for Player2, but on their way to meet them their horse shredded the wrapping paper and ate what was inside."
+    },
+    {
+        ["prompt"] = "Player1 discovers that Player2 has never jumped their horse because they are too scared, so Player1 needs to help them get over their fear."
+    },
+    {["prompt"] = "Player1 and Player2 decide to go trail riding, but Player1’s horse is scared to go on the trail."},
+    {
+        ["prompt"] = "Player1 and Player2 are doing the cross country course, and Player1’s horse refuses a jump! He/she usually loves jumping."
+    },
+    {
+        ["prompt"] = "Player1 is about to do the advanced show jumping course, but Player2 thinks Player1’s horse isn’t ready for the bigger jumps yet."
+    },
+    {["prompt"] = "Player1 and Player2 were out trail riding, but they got separated and can’t find each other."},
+    {
+        ["prompt"] = "Player1 fell off of their horse and hit their head, getting amnesia. Player2 needs to help them remember who they are."
+    },
+    {
+        ["prompt"] = "Player1 is jealous of the bond Player2 has with their horse and wants to know how they can bond with their horse too."
+    },
+    {
+        ["prompt"] = "Player1 and Player2 are out riding but a storm quickly approaches. They need to race back to the barn and take cover!"
+    },
+    {["prompt"] = "Player1 and Player2 want to go riding together, but their horses can’t stand each other!"},
+    {["prompt"] = "Player1 and Player2 go jumping together, but Player1 isn’t very good and Player2 must help them."},
+    {
+        ["prompt"] = "Player1 and Player2 go pole bending together, but Player1 isn’t very good and Player2 must help them."
+    },
+    {
+        ["prompt"] = "Player1 and Player2 go barrel racing together, but Player1 isn’t very good and Player2 must help them."
+    },
+    {["prompt"] = "Player1 and Player2 go racing together, but Player1 isn’t very good and Player2 must help them."},
+    {
+        ["prompt"] = "Player1 and Player2 go trail riding together, but Player1 isn’t very good and Player2 must help them."
+    },
+    {
+        ["prompt"] = "Player1 and Player2 go cross country riding together, but Player1 isn’t very good and Player2 must help them."
+    },
+    {
+        ["prompt"] = "Player1 had to sell their horse to pay their bills. Player2 thinks of a way to get Player1’s horse back for them!"
+    },
+    {
+        ["prompt"] = "Player1 was out trail riding when their horse got its hoof stuck in a hole! Player2 was riding by and stops to help save Player1. "
+    },
+    {
+        ["prompt"] = "Player1 and Player2 are arch show jumping rivals, but after Player1 gets hurt at the competition Player2 ends up helping them and they become best friends. "
+    },
+    {
+        ["prompt"] = "After taking a bad fall, Player1 is scared to ride their horse again. Player2 needs to help them overcome their fear."
+    },
+    {
+        ["prompt"] = "Player1 was injured last night when they were riding their horse but is too embarrassed to tell Player2."
+    },
+    {
+        ["prompt"] = "Player1 is afraid to gallop their horse, but when Player2 falls off and gets hurt, Player1 needs to gallop back to the barn and bring help."
+    },
+    {
+        ["prompt"] = "Player1 walks in on Player2 talking to their horse, and to their surprise, Player2’s horse is talking back!"
+    },
+    {
+        ["prompt"] = "Player1’s horse spooks and Player1 is thrown off. Player2 needs to help them find their runaway horse."
+    },
+    {["prompt"] = "Player1's horse lost a shoe! Take them to see Player2 who is a farrier at Wild Horse Hoofcare"},
+    {
+        ["prompt"] = "Player1's horse needs their feet trimmed! Take them to see Player2 who is a farrier at Wild Horse Hoofcare"
+    },
+    {
+        ["prompt"] = "Player1 is a wild horse and got their hoof caught between the boards on one of the bridges on Wild Island. Player2 is a rider passing by on their horse and needs to calm them down so they can help them get their hoof unstuck."
+    },
+    {
+        ["prompt"] = "Player1 is a wild horse and has come to Wild Horse Hoofcare for help. Player2 is a farrier and needs to find out what's wrong with Player1's hoof."
+    },
+    {
+        ["prompt"] = "Player1 is Player2's foal, and while travelling across Wild Island, Player1 got lost. Player2 is frantically searching for their foal!"
+    },
+    {
+        ["prompt"] = "Player1 is a wild horse whose entire herd were caught by humans. Player2 is a stubborn horse that hasn't had a herd in years. Player1 finds Player2 and, begrudgingly, accepts them as a herd member."
+    },
+    {
+        ["prompt"] = "Player1 is a wild horse, and Player2 is a human searching for a horse to capture. Player2 tries to capture Player1, but Player1 doesn't make it easy."
+    },
+    {
+        ["prompt"] = "Player1 and Player2 are wild horses that are resting in a cave. Suddenly, a pack of wolves shows up outside of the cave and starts growling. They must escape the cave to get away from the wolves."
+    },
+    {
+        ["prompt"] = "Player1 and Player2 are wild horses. Player1 wandered to the top of a mountain and is scared. Player2 must help them find their way down."
+    }
+}
