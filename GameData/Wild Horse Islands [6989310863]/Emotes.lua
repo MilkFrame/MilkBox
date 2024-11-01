acb8926a311362dd2e17959f50f89944eb1173f3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:32:14 +1100

    Create Emotes.lua

---
+++ GameData/Wild Horse Islands [6989310863]/Emotes.lua
@@ -0,0 +1,45 @@
+data = {
+    {
+        ["animationId"] = "rbxassetid://3344650532",
+        ["onStartEvent"] = "Expression",
+        ["name"] = "Wave",
+        ["onStartEventParams"] = {"Wave"}
+    },
+    {
+        ["animationId"] = "rbxassetid://4841397952",
+        ["onStartEvent"] = "Expression",
+        ["name"] = "Agree",
+        ["onStartEventParams"] = {"Thumbs Up"}
+    },
+    {
+        ["animationId"] = "rbxassetid://4841401869",
+        ["onStartEvent"] = "Expression",
+        ["name"] = "Disagree",
+        ["onStartEventParams"] = {"Thumbs Down"}
+    },
+    {["name"] = "Shrug", ["animationId"] = "rbxassetid://3334392772"},
+    {["name"] = "Applaud", ["animationId"] = "rbxassetid://5915693819"},
+    {
+        ["animationId"] = "rbxassetid://3337966527",
+        ["onStartEvent"] = "Expression",
+        ["name"] = "Laugh",
+        ["onStartEventParams"] = {"Joy"}
+    },
+    {["name"] = "Dance", ["animationId"] = "rbxassetid://507771019"},
+    {["name"] = "Dance 2", ["animationId"] = "rbxassetid://507776043"},
+    {["name"] = "Dance 3", ["animationId"] = "rbxassetid://507777268"},
+    {["name"] = "Crazy Dance", ["animationId"] = "rbxassetid://4555782893"},
+    {["name"] = "Shy", ["animationId"] = "rbxassetid://3337978742"},
+    {["name"] = "Curtsy", ["animationId"] = "rbxassetid://4555816777"},
+    {["name"] = "Cower", ["animationId"] = "rbxassetid://4940563117"},
+    {["name"] = "Salute", ["animationId"] = "rbxassetid://3333474484"},
+    {["name"] = "Floss", ["animationId"] = "rbxassetid://5917459365"},
+    {["name"] = "Line Dance", ["animationId"] = "rbxassetid://4049037604"},
+    {["name"] = "Shuffle", ["animationId"] = "rbxassetid://4349242221"},
+    {["name"] = "Bored", ["animationId"] = "rbxassetid://5230599789"},
+    {["name"] = "Jump Wave", ["animationId"] = "rbxassetid://4940564896"},
+    {["name"] = "T-Pose", ["animationId"] = "rbxassetid://3338010159"},
+    {["name"] = "Twirl", ["animationId"] = "rbxassetid://3334968680"},
+    {["name"] = "Fishing", ["animationId"] = "rbxassetid://3334832150"},
+    {["name"] = "Sleepy", ["animationId"] = "rbxassetid://4686925579"}
+}
