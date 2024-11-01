d8776856f77f7db64596d608b62adb4214c979f1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:40:28 +1100

    Create Minigames.lua

---
+++ GameData/Wild Horse Islands [6989310863]/Minigames.lua
@@ -0,0 +1,92 @@
+data = {
+    ["dunkTank"] = {
+        ["_balancingDuration"] = 18,
+        ["scoreToWin"] = 5,
+        ["displayName"] = "Dunk Tank",
+        ["costToPlay"] = {["amt"] = 7, ["currency"] = "Tokens"},
+        ["image"] = "rbxassetid://18447004478",
+        ["_autoCarnivalBalance"] = true,
+        ["maxRewardAmount"] = 175,
+        ["GetReward"] = "function: 0xb3f0ecb8b0aa0c20",
+        ["playCooldown"] = 18
+    },
+    ["ringToss"] = {
+        ["_balancingDuration"] = 20,
+        ["maxRewardAmount"] = 192,
+        ["displayName"] = "Ring Toss",
+        ["costToPlay"] = {["amt"] = 8, ["currency"] = "Tokens"},
+        ["image"] = "rbxassetid://18447004478",
+        ["_autoCarnivalBalance"] = true,
+        ["maxScorePerThrow"] = 3,
+        ["minScorePerThrow"] = 1,
+        ["totalThrows"] = 15,
+        ["GetReward"] = "function: 0xa684da87e3207b50",
+        ["playCooldown"] = 20
+    },
+    ["balloonPopping"] = {
+        ["costToPlay"] = {["amt"] = 7, ["currency"] = "Tokens"},
+        ["_balancingDuration"] = 18,
+        ["_autoCarnivalBalance"] = true,
+        ["playCooldown"] = 18,
+        ["maxRewardAmount"] = 175,
+        ["image"] = "rbxassetid://18447004478",
+        ["GetReward"] = "function: 0x5bf3efb2f2a3f810",
+        ["displayName"] = "Balloon Burst Blitz"
+    },
+    ["spinTheWheel"] = {
+        ["image"] = "rbxassetid://18447004478",
+        ["maxRewardAmount"] = 5000,
+        ["playCooldown"] = 86400,
+        ["GetReward"] = "function: 0x3a9df3a32369f1d0",
+        ["displayName"] = "Spin The Wheel"
+    },
+    ["ringTheBell"] = {
+        ["costToPlay"] = {["amt"] = 7, ["currency"] = "Tokens"},
+        ["_balancingDuration"] = 19,
+        ["_autoCarnivalBalance"] = true,
+        ["playCooldown"] = 19,
+        ["maxRewardAmount"] = 183,
+        ["image"] = "rbxassetid://18447004478",
+        ["GetReward"] = "function: 0x8da60891c06460d0",
+        ["displayName"] = "Ring The Bell"
+    },
+    ["whackALarry"] = {
+        ["_balancingDuration"] = 40,
+        ["totalPopUps"] = 15,
+        ["displayName"] = "Whack-a-Larry",
+        ["costToPlay"] = {["amt"] = 14, ["currency"] = "Tokens"},
+        ["image"] = "rbxassetid://18447004478",
+        ["_autoCarnivalBalance"] = true,
+        ["maxRewardAmount"] = 358,
+        ["GetReward"] = "function: 0x5ea2f46181dc75d0",
+        ["playCooldown"] = 40
+    },
+    ["trappedSoul"] = {
+        ["image"] = "rbxassetid://90284448699015",
+        ["playCooldown"] = 5,
+        ["maxAttempts"] = 12,
+        ["scoreToWin"] = 5,
+        ["GetReward"] = "function: 0x5d51855e0e7ee290",
+        ["displayName"] = "Free the Trapped Soul"
+    },
+    ["horseshoeToss"] = {
+        ["_balancingDuration"] = 15,
+        ["displayName"] = "Horseshoe Ringers",
+        ["costToPlay"] = {["amt"] = 6, ["currency"] = "Tokens"},
+        ["image"] = "rbxassetid://18447004478",
+        ["_autoCarnivalBalance"] = true,
+        ["maxRewardAmount"] = 150,
+        ["totalThrows"] = 10,
+        ["GetReward"] = "function: 0x950d2184f3e6fd90",
+        ["playCooldown"] = 15
+    },
+    ["horseDancing"] = {
+        ["costToPlay"] = {["amt"] = 100, ["currency"] = "Tokens"},
+        ["image"] = "rbxassetid://137617649505179",
+        ["ReplicateScoreFX"] = "function: 0x784af47ae0acfd60",
+        ["playCooldown"] = 45,
+        ["AdjustScore"] = "function: 0xbef205e924b80830",
+        ["GetReward"] = "function: 0xe3e69bc86b5d8510",
+        ["displayName"] = "Dance, Dance!"
+    }
+}
