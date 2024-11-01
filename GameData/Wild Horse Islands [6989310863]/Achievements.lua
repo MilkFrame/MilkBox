5355247e66fd03cd932ad083bbc07a253a59531a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:29:06 +1100

    Create Achievements.lua

---
+++ GameData/Wild Horse Islands [6989310863]/Achievements.lua
@@ -0,0 +1,364 @@
+data = {
+    {
+        ["achievements"] = {
+            {["value"] = 10, ["rewards"] = {["XP"] = 20, ["Tokens"] = 50}, ["id"] = "QP1"},
+            {["value"] = 25, ["rewards"] = {["XP"] = 30, ["Tokens"] = 76}, ["id"] = "QP2"},
+            {["value"] = 50, ["rewards"] = {["XP"] = 52, ["Tokens"] = 131}, ["id"] = "QP3"},
+            {["value"] = 100, ["rewards"] = {["XP"] = 107, ["Tokens"] = 268}, ["id"] = "QP4"},
+            {["value"] = 250, ["rewards"] = {["XP"] = 330, ["Tokens"] = 825}, ["id"] = "QP5"},
+            {["value"] = 500, ["rewards"] = {["XP"] = 562, ["Tokens"] = 1406}, ["id"] = "QP6"},
+            {["value"] = 1000, ["rewards"] = {["XP"] = 1150, ["Tokens"] = 2875}, ["id"] = "QP7"},
+            {["value"] = 2500, ["rewards"] = {["XP"] = 3525, ["Tokens"] = 8812}, ["id"] = "QP8"},
+            {["value"] = 5000, ["rewards"] = {["XP"] = 6000, ["Tokens"] = 15000}, ["id"] = "QP9"}
+        },
+        ["announcement"] = "%s reached %s quest points!",
+        ["tokensPerValue"] = 5,
+        ["displayString"] = "Earn %d Quest Points.",
+        ["stat"] = "QP",
+        ["xpPerValue"] = 2
+    },
+    {
+        ["achievements"] = {
+            {["value"] = 5, ["rewards"] = {["XP"] = 50, ["Tokens"] = 75}, ["id"] = "horsesCaptured1"},
+            {["value"] = 25, ["rewards"] = {["XP"] = 204, ["Tokens"] = 307}, ["id"] = "horsesCaptured2"},
+            {["value"] = 50, ["rewards"] = {["XP"] = 262, ["Tokens"] = 393}, ["id"] = "horsesCaptured3"},
+            {
+                ["value"] = 100,
+                ["rewards"] = {["additionalInventorySlots"] = 1, ["Tokens"] = 806, ["XP"] = 537},
+                ["id"] = "horsesCaptured4"
+            },
+            {
+                ["value"] = 250,
+                ["rewards"] = {["additionalInventorySlots"] = 3, ["Tokens"] = 2475, ["XP"] = 1650},
+                ["id"] = "horsesCaptured5"
+            },
+            {
+                ["value"] = 500,
+                ["rewards"] = {["additionalInventorySlots"] = 5, ["Tokens"] = 4218, ["XP"] = 2812},
+                ["id"] = "horsesCaptured6"
+            },
+            {
+                ["value"] = 1000,
+                ["rewards"] = {["additionalInventorySlots"] = 11, ["Tokens"] = 8625, ["XP"] = 5750},
+                ["id"] = "horsesCaptured7"
+            },
+            {
+                ["value"] = 2500,
+                ["rewards"] = {["additionalInventorySlots"] = 35, ["Tokens"] = 26437, ["XP"] = 17625},
+                ["id"] = "horsesCaptured8"
+            },
+            {
+                ["value"] = 5000,
+                ["rewards"] = {["additionalInventorySlots"] = 60, ["Tokens"] = 45000, ["XP"] = 30000},
+                ["id"] = "horsesCaptured9"
+            },
+            {
+                ["value"] = 10000,
+                ["rewards"] = {["additionalInventorySlots"] = 122, ["Tokens"] = 91875, ["XP"] = 61250},
+                ["id"] = "horsesCaptured10"
+            }
+        },
+        ["announcement"] = "%s has captured %s wild horses!",
+        ["displayString"] = "Capture %d wild horses.",
+        ["tokensPerValue"] = 15,
+        ["inventorySlotsPerValue"] = 0.02,
+        ["stat"] = "horsesCaptured",
+        ["xpPerValue"] = 10
+    },
+    {
+        ["achievements"] = {
+            {["value"] = 5, ["rewards"] = {["XP"] = 15, ["Tokens"] = 25}, ["id"] = "racesCompleted1"},
+            {["value"] = 25, ["rewards"] = {["XP"] = 61, ["Tokens"] = 102}, ["id"] = "racesCompleted2"},
+            {["value"] = 50, ["rewards"] = {["XP"] = 78, ["Tokens"] = 131}, ["id"] = "racesCompleted3"},
+            {["value"] = 100, ["rewards"] = {["XP"] = 161, ["Tokens"] = 268}, ["id"] = "racesCompleted4"},
+            {
+                ["value"] = 250,
+                ["rewards"] = {["additionalInventorySlots"] = 2, ["Tokens"] = 825, ["XP"] = 495},
+                ["id"] = "racesCompleted5"
+            },
+            {
+                ["value"] = 500,
+                ["rewards"] = {["additionalInventorySlots"] = 3, ["Tokens"] = 1406, ["XP"] = 843},
+                ["id"] = "racesCompleted6"
+            },
+            {
+                ["value"] = 1000,
+                ["rewards"] = {["additionalInventorySlots"] = 7, ["Tokens"] = 2875, ["XP"] = 1724},
+                ["id"] = "racesCompleted7"
+            },
+            {
+                ["value"] = 2500,
+                ["rewards"] = {["additionalInventorySlots"] = 23, ["Tokens"] = 8812, ["XP"] = 5287},
+                ["id"] = "racesCompleted8"
+            }
+        },
+        ["announcement"] = "%s has completed %s races!",
+        ["displayString"] = "Complete %d races.",
+        ["tokensPerValue"] = 5,
+        ["inventorySlotsPerValue"] = 0.013333333333333334,
+        ["stat"] = "racesCompleted",
+        ["xpPerValue"] = 3
+    },
+    {
+        ["achievements"] = {
+            {["value"] = 25, ["rewards"] = {["XP"] = 25, ["Tokens"] = 25}, ["id"] = "itemsHarvested1"},
+            {["value"] = 100, ["rewards"] = {["XP"] = 76, ["Tokens"] = 76}, ["id"] = "itemsHarvested2"},
+            {["value"] = 250, ["rewards"] = {["XP"] = 157, ["Tokens"] = 157}, ["id"] = "itemsHarvested3"},
+            {["value"] = 500, ["rewards"] = {["XP"] = 268, ["Tokens"] = 268}, ["id"] = "itemsHarvested4"},
+            {
+                ["value"] = 1000,
+                ["rewards"] = {["additionalInventorySlots"] = 1, ["Tokens"] = 550, ["XP"] = 550},
+                ["id"] = "itemsHarvested5"
+            },
+            {
+                ["value"] = 2500,
+                ["rewards"] = {["additionalInventorySlots"] = 4, ["Tokens"] = 1687, ["XP"] = 1687},
+                ["id"] = "itemsHarvested6"
+            },
+            {
+                ["value"] = 5000,
+                ["rewards"] = {["additionalInventorySlots"] = 7, ["Tokens"] = 2875, ["XP"] = 2875},
+                ["id"] = "itemsHarvested7"
+            },
+            {
+                ["value"] = 10000,
+                ["rewards"] = {["additionalInventorySlots"] = 14, ["Tokens"] = 5875, ["XP"] = 5875},
+                ["id"] = "itemsHarvested8"
+            }
+        },
+        ["announcement"] = "%s has harvested %s resources!",
+        ["inventorySlotsPerValue"] = 0.0025,
+        ["tokensPerValue"] = 1,
+        ["displayString"] = "Harvest %d resources.",
+        ["stat"] = "itemsHarvested",
+        ["xpPerValue"] = 1
+    },
+    {
+        ["achievements"] = {
+            {["value"] = 100, ["rewards"] = {["XP"] = 10, ["Tokens"] = 30}, ["id"] = "animalBondEarned1"},
+            {["value"] = 250, ["rewards"] = {["XP"] = 15, ["Tokens"] = 46}, ["id"] = "animalBondEarned2"},
+            {["value"] = 500, ["rewards"] = {["XP"] = 26, ["Tokens"] = 78}, ["id"] = "animalBondEarned3"},
+            {["value"] = 1000, ["rewards"] = {["XP"] = 53, ["Tokens"] = 161}, ["id"] = "animalBondEarned4"},
+            {["value"] = 2500, ["rewards"] = {["XP"] = 165, ["Tokens"] = 495}, ["id"] = "animalBondEarned5"},
+            {
+                ["value"] = 5000,
+                ["rewards"] = {["additionalInventorySlots"] = 1, ["Tokens"] = 843, ["XP"] = 281},
+                ["id"] = "animalBondEarned6"
+            },
+            {
+                ["value"] = 10000,
+                ["rewards"] = {["additionalInventorySlots"] = 2, ["Tokens"] = 1724, ["XP"] = 575},
+                ["id"] = "animalBondEarned7"
+            },
+            {
+                ["value"] = 25000,
+                ["rewards"] = {["additionalInventorySlots"] = 7, ["Tokens"] = 5287, ["XP"] = 1762},
+                ["id"] = "animalBondEarned8"
+            },
+            {
+                ["value"] = 50000,
+                ["rewards"] = {["additionalInventorySlots"] = 12, ["Tokens"] = 9000, ["XP"] = 3000},
+                ["id"] = "animalBondEarned9"
+            }
+        },
+        ["announcement"] = "%s reached %s bond with their horses!",
+        ["inventorySlotsPerValue"] = 0.0004,
+        ["tokensPerValue"] = 0.3,
+        ["displayString"] = "Earn %d bond with horses.",
+        ["stat"] = "animalBondEarned",
+        ["xpPerValue"] = 0.1
+    },
+    {
+        ["achievements"] = {
+            {["value"] = 25, ["rewards"] = {["XP"] = 37, ["Tokens"] = 50}, ["id"] = "cropsHarvested1"},
+            {["value"] = 75, ["rewards"] = {["XP"] = 76, ["Tokens"] = 102}, ["id"] = "cropsHarvested2"},
+            {["value"] = 150, ["rewards"] = {["XP"] = 118, ["Tokens"] = 157}, ["id"] = "cropsHarvested3"},
+            {["value"] = 250, ["rewards"] = {["XP"] = 161, ["Tokens"] = 215}, ["id"] = "cropsHarvested4"},
+            {["value"] = 500, ["rewards"] = {["XP"] = 412, ["Tokens"] = 550}, ["id"] = "cropsHarvested5"},
+            {["value"] = 1000, ["rewards"] = {["XP"] = 843, ["Tokens"] = 1125}, ["id"] = "cropsHarvested6"},
+            {["value"] = 1500, ["rewards"] = {["XP"] = 862, ["Tokens"] = 1150}, ["id"] = "cropsHarvested7"},
+            {
+                ["value"] = 3000,
+                ["rewards"] = {["additionalInventorySlots"] = 2, ["Tokens"] = 3525, ["XP"] = 2643},
+                ["id"] = "cropsHarvested8"
+            },
+            {
+                ["value"] = 5000,
+                ["rewards"] = {["additionalInventorySlots"] = 4, ["Tokens"] = 4800, ["XP"] = 3600},
+                ["id"] = "cropsHarvested9"
+            },
+            {
+                ["value"] = 10000,
+                ["rewards"] = {["additionalInventorySlots"] = 10, ["Tokens"] = 12250, ["XP"] = 9187},
+                ["id"] = "cropsHarvested10"
+            },
+            {
+                ["value"] = 25000,
+                ["rewards"] = {["additionalInventorySlots"] = 31, ["Tokens"] = 37500, ["XP"] = 28125},
+                ["id"] = "cropsHarvested11"
+            },
+            {
+                ["value"] = 50000,
+                ["rewards"] = {["additionalInventorySlots"] = 53, ["Tokens"] = 63749, ["XP"] = 47812},
+                ["id"] = "cropsHarvested12"
+            }
+        },
+        ["announcement"] = "%s has grown %s crops at their farm!",
+        ["inventorySlotsPerValue"] = 0.0016666666666666668,
+        ["tokensPerValue"] = 2,
+        ["displayString"] = "Grow %s crops at your farm.",
+        ["stat"] = "cropsHarvested",
+        ["xpPerValue"] = 1.5
+    },
+    {
+        ["achievements"] = {
+            {["value"] = 5, ["rewards"] = {["XP"] = 10, ["Tokens"] = 75}, ["id"] = "foalsBred1"},
+            {["value"] = 25, ["rewards"] = {["XP"] = 41, ["Tokens"] = 307}, ["id"] = "foalsBred2"},
+            {
+                ["value"] = 50,
+                ["rewards"] = {["additionalInventorySlots"] = 1, ["Tokens"] = 393, ["XP"] = 52},
+                ["id"] = "foalsBred3"
+            },
+            {
+                ["value"] = 100,
+                ["rewards"] = {["additionalInventorySlots"] = 2, ["Tokens"] = 806, ["XP"] = 107},
+                ["id"] = "foalsBred4"
+            },
+            {
+                ["value"] = 250,
+                ["rewards"] = {["additionalInventorySlots"] = 6, ["Tokens"] = 2475, ["XP"] = 330},
+                ["id"] = "foalsBred5"
+            },
+            {
+                ["value"] = 500,
+                ["rewards"] = {["additionalInventorySlots"] = 11, ["Tokens"] = 4218, ["XP"] = 562},
+                ["id"] = "foalsBred6"
+            },
+            {
+                ["value"] = 1000,
+                ["rewards"] = {["additionalInventorySlots"] = 23, ["Tokens"] = 8625, ["XP"] = 1150},
+                ["id"] = "foalsBred7"
+            },
+            {
+                ["value"] = 2500,
+                ["rewards"] = {["additionalInventorySlots"] = 70, ["Tokens"] = 26437, ["XP"] = 3525},
+                ["id"] = "foalsBred8"
+            },
+            {
+                ["value"] = 5000,
+                ["rewards"] = {["additionalInventorySlots"] = 120, ["Tokens"] = 45000, ["XP"] = 6000},
+                ["id"] = "foalsBred9"
+            },
+            {
+                ["value"] = 10000,
+                ["rewards"] = {["additionalInventorySlots"] = 245, ["Tokens"] = 91875, ["XP"] = 12250},
+                ["id"] = "foalsBred10"
+            },
+            {
+                ["value"] = 15000,
+                ["rewards"] = {["additionalInventorySlots"] = 250, ["Tokens"] = 93750, ["XP"] = 12500},
+                ["id"] = "foalsBred11"
+            },
+            {
+                ["value"] = 25000,
+                ["rewards"] = {["additionalInventorySlots"] = 509, ["Tokens"] = 191250, ["XP"] = 25500},
+                ["id"] = "foalsBred12"
+            },
+            {
+                ["value"] = 50000,
+                ["rewards"] = {["additionalInventorySlots"] = 1300, ["Tokens"] = 487500, ["XP"] = 65000},
+                ["id"] = "foalsBred13"
+            }
+        },
+        ["announcement"] = "%s has bred %s foals!",
+        ["inventorySlotsPerValue"] = 0.04,
+        ["tokensPerValue"] = 15,
+        ["displayString"] = "Breed %s foals at the Breeding Centre.",
+        ["stat"] = "foalsBred",
+        ["xpPerValue"] = 2
+    },
+    {
+        ["achievements"] = {
+            {["value"] = 5, ["rewards"] = {["XP"] = 7, ["Tokens"] = 25}, ["id"] = "foalsGrown1"},
+            {["value"] = 10, ["rewards"] = {["XP"] = 7, ["Tokens"] = 25}, ["id"] = "foalsGrown2"},
+            {["value"] = 25, ["rewards"] = {["XP"] = 23, ["Tokens"] = 78}, ["id"] = "foalsGrown3"},
+            {["value"] = 50, ["rewards"] = {["XP"] = 40, ["Tokens"] = 134}, ["id"] = "foalsGrown4"},
+            {
+                ["value"] = 100,
+                ["rewards"] = {["additionalInventorySlots"] = 1, ["Tokens"] = 275, ["XP"] = 82},
+                ["id"] = "foalsGrown5"
+            },
+            {
+                ["value"] = 250,
+                ["rewards"] = {["additionalInventorySlots"] = 3, ["Tokens"] = 843, ["XP"] = 253},
+                ["id"] = "foalsGrown6"
+            },
+            {
+                ["value"] = 500,
+                ["rewards"] = {["additionalInventorySlots"] = 5, ["Tokens"] = 1437, ["XP"] = 431},
+                ["id"] = "foalsGrown7"
+            },
+            {
+                ["value"] = 1000,
+                ["rewards"] = {["additionalInventorySlots"] = 11, ["Tokens"] = 2937, ["XP"] = 881},
+                ["id"] = "foalsGrown8"
+            },
+            {
+                ["value"] = 2500,
+                ["rewards"] = {["additionalInventorySlots"] = 36, ["Tokens"] = 9000, ["XP"] = 2700},
+                ["id"] = "foalsGrown9"
+            },
+            {
+                ["value"] = 5000,
+                ["rewards"] = {["additionalInventorySlots"] = 61, ["Tokens"] = 15312, ["XP"] = 4593},
+                ["id"] = "foalsGrown10"
+            }
+        },
+        ["announcement"] = "%s has grown %s foals into adults!",
+        ["inventorySlotsPerValue"] = 0.02,
+        ["tokensPerValue"] = 5,
+        ["displayString"] = "Grow %s foals into adults.",
+        ["stat"] = "foalsGrown",
+        ["xpPerValue"] = 1.5
+    },
+    {
+        ["achievements"] = {
+            {["value"] = 25, ["rewards"] = {["XP"] = 37, ["Tokens"] = 200}, ["id"] = "trainingActivitiesCompleted1"},
+            {["value"] = 50, ["rewards"] = {["XP"] = 38, ["Tokens"] = 204}, ["id"] = "trainingActivitiesCompleted2"},
+            {["value"] = 100, ["rewards"] = {["XP"] = 78, ["Tokens"] = 420}, ["id"] = "trainingActivitiesCompleted3"},
+            {["value"] = 250, ["rewards"] = {["XP"] = 241, ["Tokens"] = 1290}, ["id"] = "trainingActivitiesCompleted4"},
+            {
+                ["value"] = 500,
+                ["rewards"] = {["additionalInventorySlots"] = 1, ["Tokens"] = 2200, ["XP"] = 412},
+                ["id"] = "trainingActivitiesCompleted5"
+            },
+            {
+                ["value"] = 1000,
+                ["rewards"] = {["additionalInventorySlots"] = 2, ["Tokens"] = 4500, ["XP"] = 843},
+                ["id"] = "trainingActivitiesCompleted6"
+            },
+            {
+                ["value"] = 2500,
+                ["rewards"] = {["additionalInventorySlots"] = 6, ["Tokens"] = 13799, ["XP"] = 2587},
+                ["id"] = "trainingActivitiesCompleted7"
+            },
+            {
+                ["value"] = 5000,
+                ["rewards"] = {["additionalInventorySlots"] = 11, ["Tokens"] = 23500, ["XP"] = 4406},
+                ["id"] = "trainingActivitiesCompleted8"
+            },
+            {
+                ["value"] = 10000,
+                ["rewards"] = {["additionalInventorySlots"] = 24, ["Tokens"] = 48000, ["XP"] = 9000},
+                ["id"] = "trainingActivitiesCompleted9"
+            }
+        },
+        ["announcement"] = "%s has completed %s activities on Training Island!",
+        ["addedToGameAtUpdateVersion"] = 50,
+        ["inventorySlotsPerValue"] = 0.004,
+        ["tokensPerValue"] = 8,
+        ["displayString"] = "Completed %s activities on Training Island.",
+        ["stat"] = "trainingActivitiesCompleted",
+        ["xpPerValue"] = 1.5
+    }
+}
