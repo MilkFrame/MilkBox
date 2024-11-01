a2923bbad91fe997bea1f6ab379ad8d11b3e3db1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-02 01:30:24 +1100

    Create Flags.lua

---
+++ GameData/Wild Horse Islands [6989310863]/Flags.lua
@@ -0,0 +1,77 @@
+data = {
+    ["flags"] = {
+        ["adsDisabled"] = true,
+        ["failedToSaveHookEnabled"] = true,
+        ["updateCountdownDuration"] = 172800,
+        ["disabledAntiCheatControllers"] = {["ProperIsland"] = false, ["Teleport"] = true},
+        ["dontDeleteUnserializablesOnSave"] = false,
+        ["codes"] = {
+            ["cute-little-foals"] = {
+                ["itemRewards"] = {{["name"] = "Foaling Voucher", ["data"] = {["amt"] = 3}}},
+                ["expiresAt"] = 1720213200
+            },
+            ["holiday-spirit-4-u-23"] = {
+                ["itemRewards"] = {{["name"] = "Holiday Spirit", ["data"] = {["amt"] = 10}}},
+                ["expiresAt"] = 1673560800
+            },
+            ["fuzzy-wuzzy-saddle-pads-23"] = {
+                ["itemRewards"] = {{["name"] = "Western Santa Saddle Pad"}, {["name"] = "English Santa Saddle Pad"}},
+                ["expiresAt"] = 1705096800
+            },
+            ["ty-4-100m-visits"] = {["itemRewards"] = {{["name"] = "100M Glasses"}}},
+            ["sunny-for-president"] = {["statRewards"] = {["Summer Tokens"] = 250}, ["expiresAt"] = 1689368400},
+            ["my-holiday-island-23"] = {
+                ["itemRewards"] = {
+                    {["name"] = "Decorative Holiday Wreath"},
+                    {["name"] = "Decorative Gift Bag"},
+                    {["name"] = "Christmas Presents Jump"}
+                },
+                ["expiresAt"] = 1705096800
+            },
+            ["winter-tokens-plz-23"] = {["statRewards"] = {["Winter Tokens"] = 125}, ["expiresAt"] = 1705096800},
+            ["a-gift-from-cupid"] = {["statRewards"] = {["Valentine's Tokens"] = 125}, ["expiresAt"] = 1677276000},
+            ["tiny-smol-winter-hat-23"] = {["itemRewards"] = {{["name"] = "Mini Beanie"}}, ["expiresAt"] = 1705096800},
+            ["some-pasture-stuffs"] = {
+                ["itemRewards"] = {
+                    {["name"] = "Small Horse Shelter"},
+                    {["name"] = "Water Trough"},
+                    {["name"] = "Round Bale Feeder"}
+                }
+            },
+            ["i-heart-velvet-hearts"] = {
+                ["itemRewards"] = {{["name"] = "Velvet Hearts", ["data"] = {["amt"] = 5}}},
+                ["expiresAt"] = 1677276000
+            },
+            ["koolie-plush"] = {["itemRewards"] = {{["name"] = "Isabella Plush"}}},
+            ["coral-care-package"] = {
+                ["itemRewards"] = {
+                    {["name"] = "Blue Coral", ["data"] = {["amt"] = 5}},
+                    {["name"] = "Green Coral", ["data"] = {["amt"] = 5}},
+                    {["name"] = "Orange Coral", ["data"] = {["amt"] = 5}}
+                }
+            },
+            ["tridents-trident"] = {["itemRewards"] = {{["name"] = "Trident's Trident"}}},
+            ["when-life-gives-you-lemons"] = {["itemRewards"] = {{["name"] = "Lemon's Basket"}}}
+        },
+        ["useAlignOrientationForMountAnglesDisabled"] = false,
+        ["useFollowBoneForTack"] = false,
+        ["leaderboardsDisabled"] = false,
+        ["nextUpdateTime"] = 1731708000,
+        ["trainingCostModifier"] = 1,
+        ["disableCharacterFollowsCamera"] = true,
+        ["ucaPortalFirstOpensAt"] = 1700539800,
+        ["vehicleSeatsDisabled"] = false,
+        ["ranks"] = {},
+        ["updateString"] = [[• The Halloween Event has begun! Visit Haunted Island to get started!
+• 4 unique limited horses and 50+ limited items!
+• The limited horses are: Magic Spell Icelandic, Flying Souls Thoroughbred, Haunted Black Marwari, & Haunted White Marwari
+• Visit the Graveyard and talk to the Eternal Soul to activate Ghost Vision - free Trapped Souls around the island for a reward from the Crypt!
+• Use your lasso to capture Corrupted Pumpkins and return them to the patch for a reward from Pearl!]],
+        ["statModifiers"] = {["XP"] = 1},
+        ["nextUpdateName"] = "Events End",
+        ["tradingDisabled"] = false
+    },
+    ["remoteFunction"] = "RemoteFunction",
+    ["updateEvent"] = "RemoteEvent",
+    ["Updated"] = "Signal Event"
+}
