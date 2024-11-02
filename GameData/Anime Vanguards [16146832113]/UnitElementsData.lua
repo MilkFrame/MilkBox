f8b41fb597d371196a968984f27c0dc0403c4d5e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:22:53 +1100

    Create UnitElementsData.lua

---
+++ GameData/Anime Vanguards [16146832113]/UnitElementsData.lua
@@ -0,0 +1,12 @@
+data = {
+    ["Holy"] = {["Description"] = "Reduces time of debuffs by 10%!"},
+    ["Spark"] = {["Description"] = "Immunity to DMG debuffs!"},
+    ["Curse"] = {["Description"] = "DMG +5% when attacking a boss"},
+    ["Nature"] = {["Description"] = "Immunity to RNG debuffs!"},
+    ["Water"] = {["Description"] = "Immunity to SPA debuffs!"},
+    ["Blast"] = {["Description"] = "Ignores 10% of the enemies' damage reduction"},
+    ["Cosmic"] = {["Description"] = "DMG +10% on placement; lost after upgrading"},
+    ["Fire"] = {["Description"] = "Immunity to snare!"},
+    ["Passion"] = {["Description"] = "Increases dodge chance by 10%"},
+    ["Unbound"] = {["Description"] = "Increases critical rate by 5%"}
+}
