af61e3c450689a61e94f68fba1c538c716ae4c00
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:22:00 +1100

    Create BannerData.lua

---
+++ GameData/Anime Vanguards [16146832113]/BannerData.lua
@@ -0,0 +1,43 @@
+data = {
+    ["Double Dungeon"] = {["Order"] = 4, ["Image"] = "rbxassetid://106361528360638", ["Cost"] = 30},
+    ["Planet Namak"] = {["Order"] = 2, ["Image"] = "rbxassetid://97440324036264", ["Cost"] = 30},
+    ["Pyseph's Dumb Cat"] = {["Order"] = 6, ["Image"] = "rbxassetid://94200388361341", ["Cost"] = 30},
+    ["Sand Village"] = {["Order"] = 3, ["Image"] = "rbxassetid://16997243396", ["Cost"] = 30},
+    ["Queer"] = {["Order"] = 1000, ["Image"] = "rbxassetid://71027714002397", ["IsDevOnly"] = true},
+    ["S-Rank Valkyrie"] = {
+        ["Order"] = 12,
+        ["Image"] = "rbxassetid://135056353465415",
+        ["Achievement"] = "Dancer In Battle"
+    },
+    ["Shadow Monarch"] = {["Order"] = 7, ["Image"] = "rbxassetid://140579330397565", ["Cost"] = 30},
+    ["Egotistical Fighter"] = {
+        ["Order"] = 14,
+        ["Image"] = "rbxassetid://128787010924617",
+        ["Achievement"] = "Prideful Prince"
+    },
+    ["Blood-Red Commander"] = {
+        ["Order"] = 9,
+        ["Image"] = "rbxassetid://86677329052388",
+        ["Achievement"] = "Shadow Commander"
+    },
+    ["Tokyo Plaza"] = {["Order"] = 1, ["Image"] = "rbxassetid://18489376920", ["Cost"] = 30},
+    ["Wall of Mino"] = {["Order"] = 1000, ["Image"] = "rbxassetid://120951000700064", ["IsDevOnly"] = true},
+    ["Eyes of Despair"] = {
+        ["Order"] = 13,
+        ["Image"] = "rbxassetid://80416471123844",
+        ["Achievement"] = "Beyond The Mask"
+    },
+    ["Tailed Beast"] = {
+        ["Order"] = 15,
+        ["Image"] = "rbxassetid://71478876454510",
+        ["Achievement"] = "Knucklehead Ninja"
+    },
+    ["Kirin of the Clouds"] = {
+        ["Order"] = 10,
+        ["Image"] = "rbxassetid://102881987016951",
+        ["Achievement"] = "Vengeful Prodigy"
+    },
+    ["Vampire King"] = {["Order"] = 11, ["Image"] = "rbxassetid://118412898644607", ["Achievement"] = "The Unkillable"},
+    ["Flame's Wrath"] = {["Order"] = 8, ["Image"] = "rbxassetid://107199568018609", ["Cost"] = 30},
+    ["Blood-Red Chamber"] = {["Order"] = 5, ["Image"] = "rbxassetid://73565271269093", ["Cost"] = 30}
+}
