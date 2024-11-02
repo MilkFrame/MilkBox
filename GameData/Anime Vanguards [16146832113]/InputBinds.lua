1fefd60e01b762b54183b7544a68826be27441a1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:23:05 +1100

    Create InputBinds.lua

---
+++ GameData/Anime Vanguards [16146832113]/InputBinds.lua
@@ -0,0 +1,9 @@
+data = {
+    ["Binds"] = {
+        ["UnitsWindow"] = {["DeviceBinds"] = {["Desktop"] = Enum.KeyCode.H}},
+        ["EmoteWheel"] = {["DeviceBinds"] = {["Desktop"] = Enum.KeyCode.G}, ["FireOnEnded"] = true},
+        ["ItemsWindow"] = {["DeviceBinds"] = {["Desktop"] = Enum.KeyCode.J}},
+        ["SummonMount"] = {["DeviceBinds"] = {["Desktop"] = Enum.KeyCode.V}},
+        ["Run"] = {["DeviceBinds"] = {["Desktop"] = Enum.KeyCode.LeftShift}}
+    }
+}
