8685bff6e6bd384e8f77a908c317aa73d7a164fc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:14:08 +1100

    Create CraftingData.lua

---
+++ GameData/Anime Vanguards [16146832113]/CraftingData.lua
@@ -0,0 +1,46 @@
+data = {
+    ["GetItemRequirements"] = "function: 0xd7d10ed3ab17c7f7",
+    ["GetItemByName"] = "function: 0x8ec788b60e4a1187",
+    ["_PreInit"] = "function: 0x285c0f73918f6b57",
+    ["Cache"] = {
+        ["Pink Essence Stone"] = {
+            ["Requirements"] = {["Green Essence Stone"] = {["Amount"] = 3}},
+            ["Cost"] = 500,
+            ["ItemGroup"] = "Evolution"
+        },
+        ["Blue Essence Stone"] = {
+            ["Requirements"] = {["Green Essence Stone"] = {["Amount"] = 3}},
+            ["Cost"] = 500,
+            ["ItemGroup"] = "Evolution"
+        },
+        ["Red Essence Stone"] = {
+            ["Requirements"] = {["Green Essence Stone"] = {["Amount"] = 3}},
+            ["Cost"] = 500,
+            ["ItemGroup"] = "Evolution"
+        },
+        ["Yellow Essence Stone"] = {
+            ["Requirements"] = {["Green Essence Stone"] = {["Amount"] = 3}},
+            ["Cost"] = 500,
+            ["ItemGroup"] = "Evolution"
+        },
+        ["Purple Essence Stone"] = {
+            ["Requirements"] = {["Green Essence Stone"] = {["Amount"] = 3}},
+            ["Cost"] = 500,
+            ["ItemGroup"] = "Evolution"
+        },
+        ["Rainbow Essence Stone"] = {
+            ["Requirements"] = {
+                ["Pink Essence Stone"] = {["Amount"] = 1},
+                ["Blue Essence Stone"] = {["Amount"] = 1},
+                ["Red Essence Stone"] = {["Amount"] = 1},
+                ["Green Essence Stone"] = {["Amount"] = 1},
+                ["Yellow Essence Stone"] = {["Amount"] = 1},
+                ["Purple Essence Stone"] = {["Amount"] = 1}
+            },
+            ["Cost"] = 500,
+            ["ItemGroup"] = "Evolution"
+        }
+    },
+    ["GetAllCraftableItems"] = "function: 0x92f60d147c9e5a97",
+    ["GetCost"] = "function: 0x7c638d1f47db8127"
+}
