302a9479707689771200d70547da090901bc1d34
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:15:09 +1100

    Create StatsData.lua

---
+++ GameData/Anime Vanguards [16146832113]/StatsData.lua
@@ -0,0 +1,50 @@
+data = {
+    ["A"] = {
+        ["Chance"] = 4.35,
+        ["WorthinessChance"] = 75,
+        ["Statistics"] = {["SPA"] = {5.5, 6.99}, ["Range"] = {5.5, 6.99}, ["Damage"] = {11, 13.99}},
+        ["Gradient"] = "Blue"
+    },
+    ["D"] = {
+        ["Chance"] = 40,
+        ["WorthinessChance"] = 0,
+        ["Statistics"] = {["SPA"] = {0, 2.49}, ["Range"] = {0, 2.49}, ["Damage"] = {0, 4.99}},
+        ["Gradient"] = "Silver"
+    },
+    ["S"] = {
+        ["Chance"] = 1,
+        ["WorthinessChance"] = 20,
+        ["Statistics"] = {["SPA"] = {7, 8.49}, ["Range"] = {7, 8.49}, ["Damage"] = {14, 16.99}},
+        ["Gradient"] = "Purple"
+    },
+    ["Z"] = {
+        ["Chance"] = 0.85,
+        ["WorthinessChance"] = 4.15,
+        ["Statistics"] = {["SPA"] = {8.5, 9.99}, ["Range"] = {8.5, 9.99}, ["Damage"] = {17, 19.99}},
+        ["Gradient"] = "Gold"
+    },
+    ["神"] = {
+        ["Chance"] = 0.1,
+        ["WorthinessChance"] = 0.25,
+        ["Statistics"] = {["SPA"] = {11.5, 12.5}, ["Range"] = {11.5, 12.5}, ["Damage"] = {23, 25}},
+        ["Gradient"] = "Omega"
+    },
+    ["Z+"] = {
+        ["Chance"] = 0.85,
+        ["WorthinessChance"] = 4.15,
+        ["Statistics"] = {["SPA"] = {10, 11.49}, ["Range"] = {10, 11.49}, ["Damage"] = {20, 22.99}},
+        ["Gradient"] = "Pink"
+    },
+    ["C"] = {
+        ["Chance"] = 30,
+        ["WorthinessChance"] = 0,
+        ["Statistics"] = {["SPA"] = {2.5, 3.99}, ["Range"] = {2.5, 3.99}, ["Damage"] = {5, 7.99}},
+        ["Gradient"] = "Silver"
+    },
+    ["B"] = {
+        ["Chance"] = 30,
+        ["WorthinessChance"] = 0,
+        ["Statistics"] = {["SPA"] = {4, 5.49}, ["Range"] = {4, 5.49}, ["Damage"] = {8, 10.99}},
+        ["Gradient"] = "Green"
+    }
+}
