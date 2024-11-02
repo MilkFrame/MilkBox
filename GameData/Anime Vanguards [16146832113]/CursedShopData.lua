53786c0f7dacd9732d80722e862db744fecfbdb4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:22:42 +1100

    Create CursedShopData.lua

---
+++ GameData/Anime Vanguards [16146832113]/CursedShopData.lua
@@ -0,0 +1,8 @@
+--require(game.ReplicatedStorage.Modules.Data.CursedShopData).GetAllData()
+data = {
+    ["Stat Chip"] = {["MaxQuantity"] = 30, ["Amount"] = 3},
+    ["Green Essence Stone"] = {["MaxQuantity"] = 60, ["Amount"] = 1},
+    ["Six Eyes"] = {["MaxQuantity"] = 999, ["Amount"] = 10},
+    ["Super Stat Chip"] = {["MaxQuantity"] = 20, ["Amount"] = 3},
+    ["TraitRerolls"] = {["Type"] = "Currency", ["MaxQuantity"] = 50, ["Amount"] = 1}
+}
