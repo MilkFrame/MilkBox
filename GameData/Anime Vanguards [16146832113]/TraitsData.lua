e98808b078c86ba6c996d53f2382b5a61bd6a33a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:13:18 +1100

    Create TraitsData.lua

---
+++ GameData/Anime Vanguards [16146832113]/TraitsData.lua
@@ -0,0 +1,13 @@
+data = {
+    ["Fortune"] = {["Income"] = 20, ["CostReduction"] = 10},
+    ["Monarch"] = {["SPA"] = 10, ["PlacementLimit"] = 1, ["Range"] = 5, ["Damage"] = 300},
+    ["Solar"] = {["SPA"] = 5, ["Range"] = 20, ["Damage"] = 10},
+    ["Range"] = {["Data"] = {5, 10, 15}, ["Statistic"] = "Range"},
+    ["Scholar"] = {["ExperienceGained"] = 50},
+    ["Vigor"] = {["Data"] = {5, 10, 15}, ["Statistic"] = "Damage"},
+    ["Swift"] = {["Data"] = {5, 7.5, 12.5}, ["Statistic"] = "SPA"},
+    ["Marksman"] = {["Range"] = 30},
+    ["Deadeye"] = {["CritDamage"] = 50, ["CritChance"] = 45},
+    ["Blitz"] = {["SPA"] = 20},
+    ["Ethereal"] = {["Damage"] = 20, ["Range"] = 5, ["SPA"] = 20}
+}
