ac9feda3d245ad42ab9f6970ab4243354fb90f9e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:23:18 +1100

    Create PassivesData.lua

---
+++ GameData/Anime Vanguards [16146832113]/PassivesData.lua
@@ -0,0 +1,210 @@
+data = {
+    ["Epic"] = {
+        ["Shinzi"] = {{["Name"] = "Parallel World", ["Description"] = "DMG +10% each wave (Up to +50%)."}},
+        ["Genitsu"] = {
+            {["Name"] = "Focused Breathing", ["Description"] = "Every 6th attack deals +200% more damage."},
+            {["Name"] = "Fine Tuned Blade", ["Description"] = "Inflicts Bleed equal to 10% of DMG."}
+        },
+        ["Genas"] = {
+            {["Name"] = "Flaming Cyborg", ["Description"] = "Attacks inflict Burn DMG equal to 20% DMG."},
+            {
+                ["Name"] = "Cybernetic Enhancements",
+                ["Description"] = "Decreases SPA by -5% with each attack (Up to -30%), Resets after not attacking for 8 seconds."
+            }
+        },
+        ["Inosake"] = {
+            {
+                ["Name"] = "Beastial Frenzy",
+                ["Description"] = "Every 10 kills, enters a Frenzy, increasing DMG by +40% and lowering SPA by -10% for 5 seconds. 6 second cooldown."
+            }
+        },
+        ["Kokashi"] = {
+            {
+                ["Name"] = "Skill Copy",
+                ["Description"] = "Every 8 seconds, gains +25% DMG for 8 seconds. Attacks during this time have a 100% chance to Stun."
+            }
+        },
+        ["Pickleo"] = {
+            {["Name"] = "Desperate Trump Card", ["Description"] = "Every 4th attack deals +300% more damage."}
+        },
+        ["Kinnua"] = {
+            {
+                ["Name"] = "Trained Assassin",
+                ["Description"] = "Every 8-16 seconds, Kinnua enters a frenzy lowering SPA -40% for 7 seconds."
+            },
+            {
+                ["Name"] = "Lightning Strike",
+                ["Description"] = "While in a frenzy Kinnua has a 50% chance to stun. (Excludes Bosses)"
+            }
+        },
+        ["Sprintwagon"] = {{["Name"] = "Entrepreneur", ["Description"] = "Range increases by +100% every round."}},
+        ["Gaari"] = {
+            {
+                ["Name"] = "Cursed Sand",
+                ["Description"] = "After attacking an enemy, lower SPA by -5% for 10 seconds. This effect is stackable (Max -50%) ."
+            }
+        },
+        ["Alligator"] = {
+            {["Name"] = "Sand Body", ["Description"] = "Every Attack Slows enemy movement by 50% for 3 seconds."}
+        }
+    },
+    ["Legendary"] = {
+        ["Agony"] = {
+            {["Name"] = "Almighty Presence", ["Description"] = "DMG & RNG +40% after repulsing an enemy."},
+            {["Name"] = "Looming Threat", ["Description"] = "Attacked enemies are Repulsed."}
+        },
+        ["Goi"] = {
+            {["Name"] = "Quartz, Parchment, Shears", ["Description"] = "33% chance to deal +50% DMG every attack."}
+        },
+        ["Roku (Dark)"] = {
+            {
+                ["Name"] = "Merciless Judgement",
+                ["Description"] = "Non-boss enemies below 20% HP have a 20% chance to be instantly executed on hit (Story Mode Only)."
+            }
+        },
+        ["Kinaru"] = {
+            {
+                ["Name"] = "Light Speed Step",
+                ["Description"] = "Immune to Boss/Enemy abilities for 10 seconds. 10 second cooldown."
+            }
+        },
+        ["Grim Wow"] = {{["Name"] = "Sixth Sword", ["Description"] = "Deals +150% DMG to enemies with overshield."}},
+        ["Itochi"] = {
+            {["Name"] = "Black Flames", ["Description"] = "Itochi inflicts Burn equal to 25% of his DMG."},
+            {["Name"] = "Eternal Flame", ["Description"] = "Burn DMG +5% every wave (Up to +100%)."}
+        }
+    },
+    ["Mythic"] = {
+        ["Sosuke (Storm)"] = {
+            {["Name"] = "Way of the Snake", ["Description"] = "DMG +2% with each consecutive attack on the same enemy."},
+            {
+                ["Name"] = "Lightning Powered Assault",
+                ["Description"] = "Attacks stun enemies; DMG +50% to enemies that have been stunned."
+            }
+        },
+        ["Cha-In"] = {
+            {
+                ["Name"] = "Pristine Hunter",
+                ["Description"] = "50% chance to dodge attacks. Upon successful dodge, ATK +10% and SPA -30% for 6 seconds. Subsequent dodges will refresh the duration."
+            }
+        },
+        ["Alocard"] = {
+            {["Name"] = "Casull & Jackal", ["Description"] = "Attacks inflict Bleed equal to 25% of this units DMG."}
+        },
+        ["Cha-In (Blade Dancer)"] = {
+            {
+                ["Name"] = "Pristine Hunter",
+                ["Description"] = "50% chance to dodge attacks. Upon successful dodge, ATK +10% and SPA -30% for 6 seconds. Subsequent dodges will refresh the duration."
+            },
+            {
+                ["Name"] = "Dance! Dance! Dance!",
+                ["Description"] = "50% chance to inflict an extra instance of DMG when attacking, equal to 65% of her DMG."
+            }
+        },
+        ["Haruka Rin"] = {
+            {["Name"] = "Love Dance", ["Description"] = "DMG +3% per wave for all units in range, maxing out at +18%."}
+        },
+        ["Vogita Super (Awakened)"] = {
+            {
+                ["Name"] = "Surprise Transformation",
+                ["Description"] = "SPA -20% if the only units in range include the name 'Vogita'."
+            },
+            {["Name"] = "Overwhelming Power", ["Description"] = "DMG +100% when hitting 1 enemy."}
+        },
+        ["Haruka Rin (Dancer)"] = {
+            {["Name"] = "Love Dance", ["Description"] = "DMG +3% per wave for all units in range, maxing out at +18%."},
+            {["Name"] = "Encore", ["Description"] = "Starting from Wave 6, SPA -8% for all units in range."}
+        },
+        ["Alocard (Vampire King)"] = {
+            {["Name"] = "Casull & Jackal", ["Description"] = "Attacks inflict Bleed equal to 25% of this units DMG."},
+            {
+                ["Name"] = "Bird of Hermes",
+                ["Description"] = "Enemies that enter this units range while Bleeding will be afflicted with Hemorrhage; Enemies afflicted with Hemorrhage take 20% more damage."
+            },
+            {
+                ["Description"] = "Ability to summon Familiars; while Familiars are present on the map, SPA decreases by 10% until they are dead; Each time a familiar hits an enemy, DMG is increased by 1%.",
+                ["Name"] = "Restraint Level 0",
+                ["UpgradeRequired"] = 11
+            }
+        },
+        ["Igros"] = {{["Name"] = "Rulers Hand", ["Description"] = "DMG +10% when dealing DMG to a boss."}},
+        ["Vogita Super"] = {
+            {
+                ["Name"] = "Surprise Transformation",
+                ["Description"] = "SPA -20% if the only units in range include the name 'Vogita'."
+            }
+        },
+        ["Noruto (Six Tails)"] = {
+            {
+                ["Name"] = "Sage Training",
+                ["Description"] = "After not attacking for 7 seconds, DMG & RNG +20% for the next 30 seconds."
+            },
+            {["Name"] = "Blistering Rage", ["Description"] = "After being Stunned, next attack deals +100% DMG."}
+        },
+        ["Noruto (Sage)"] = {
+            {
+                ["Name"] = "Sage Training",
+                ["Description"] = "After not attacking for 7 seconds, DMG & RNG +20% for the next 30 seconds."
+            }
+        },
+        ["Obita (Awakened)"] = {
+            {["Name"] = "Vanishing Step", ["Description"] = "SPA -6% per wave (down to -30%)."},
+            {
+                ["Description"] = "Attacks inflict Burn equal to 15% of DMG.",
+                ["Name"] = "Curse of Hatred",
+                ["UpgradeRequired"] = 9
+            }
+        },
+        ["Song Jinwu"] = {{["Name"] = "Job Request", ["Description"] = "DMG +1% per wave (up to 40%)."}},
+        ["Song Jinwu (Monarch)"] = {
+            {["Name"] = "Job Request", ["Description"] = "DMG +1% per wave (up to 40%)."},
+            {
+                ["Description"] = "Killing enemies adds them to your shadow army (up to 40 shadows); using Arise summons 15 shadows and summons 5 Shadow Knights.",
+                ["Name"] = "Necromancer",
+                ["UpgradeRequired"] = 8
+            },
+            {
+                ["Description"] = "DMG +2.5% for each shadow currently on the map, (up to +40%); Units in range that include the name 'Igros' also recieve this damage increase.",
+                ["Name"] = "King of the Shadows",
+                ["UpgradeRequired"] = 8
+            }
+        },
+        ["Sosuke (Hebi)"] = {
+            {["Name"] = "Way of the Snake", ["Description"] = "DMG +2% with each consecutive attack on the same enemy."}
+        },
+        ["Igros (Elite Knight)"] = {
+            {["Name"] = "Rulers Hand", ["Description"] = "DMG +10% when dealing DMG to a boss."},
+            {["Name"] = "Strongest Shadow", ["Description"] = "DMG +40% and SPA -10%; Immune to stun."},
+            {
+                ["Name"] = "Shadow Commander",
+                ["Description"] = "DMG & RNG +20% for 10 seconds after a shadow enters this units range."
+            }
+        },
+        ["Obita"] = {{["Name"] = "Vanishing Step", ["Description"] = "SPA -6% per wave (down to -30%)."}}
+    },
+    ["Rare"] = {
+        ["Jon"] = {{["Name"] = "Ripple Training", ["Description"] = "DMG +2% after each wave passed (up to +300%)"}},
+        ["Roku"] = {{["Name"] = "Raised on Earth", ["Description"] = "DMG +3% after each wave passed (up to +30%)"}},
+        ["Ichiga"] = {
+            {
+                ["Name"] = "Substitute Soul Reaper",
+                ["Description"] = "DMG +5% with each attack preformed (up to +20%). Resets after 10 seconds of not attacking"
+            }
+        },
+        ["Joe"] = {{["Name"] = "Star Lineage", ["Description"] = "DMG +25% and RNG +15%"}},
+        ["Sanjo"] = {{["Name"] = "Flaming Fury", ["Description"] = "SPA -1% after each wave passed (down to -5%)"}},
+        ["Rukio"] = {{["Name"] = "Ice Princess", ["Description"] = "DMG +50% when hitting 1 enemy"}},
+        ["Vogita"] = {{["Name"] = "Royal Might", ["Description"] = "DMG +2% after each wave passed (up to +26%)"}},
+        ["Noruto"] = {{["Name"] = "Believe It", ["Description"] = "RNG +15%"}},
+        ["Sosuke"] = {
+            {["Name"] = "Burning Resentment", ["Description"] = "Sasuke inflicts burn equal to 30% of his damage"},
+            {["Name"] = "Sole Survivor", ["Description"] = "Burn DMG +50%"}
+        },
+        ["Luffo"] = {
+            {
+                ["Name"] = "Greatest Pirate",
+                ["Description"] = "RNG +5% with each attack preformed (up to +15%). Resets after 10 seconds of not attacking"
+            }
+        }
+    }
+}
