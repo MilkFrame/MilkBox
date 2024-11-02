bdfbf1db35b52474e349e79e57e5424215d63f95
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:12:48 +1100

    Create PriceData.lua

---
+++ GameData/Anime Vanguards [16146832113]/PriceData.lua
@@ -0,0 +1,8 @@
+data = {
+    ["Legendary"] = {["Price"] = 500},
+    ["Secret"] = {["Price"] = 3000},
+    ["Epic"] = {["Price"] = 100},
+    ["Exclusive"] = {["Price"] = 1500},
+    ["Mythic"] = {["Price"] = 1500},
+    ["Rare"] = {["Price"] = 50}
+}
