9275f0cdbe194ba78171adc08bda9b01bad9b0db
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:22:18 +1100

    Create LeaderboardRewardsData.lua

---
+++ GameData/Anime Vanguards [16146832113]/LeaderboardRewardsData.lua
@@ -0,0 +1,50 @@
+data = {
+    ["Infinite"] = {
+        {
+            ["Standings"] = {1, 12},
+            ["Rewards"] = {
+                ["Currencies"] = {
+                    ["Trophies"] = {["Amount"] = 100},
+                    ["TraitRerolls"] = {["Amount"] = 30},
+                    ["Gems"] = {["Amount"] = 6000}
+                },
+                ["Units"] = {["Mohato"] = {["Shiny"] = true, ["DarkView"] = true, ["Amount"] = 1}}
+            }
+        },
+        {
+            ["Standings"] = {12, 32},
+            ["Rewards"] = {
+                ["Currencies"] = {
+                    ["Trophies"] = {["Amount"] = 50},
+                    ["TraitRerolls"] = {["Amount"] = 15},
+                    ["Gems"] = {["Amount"] = 3000}
+                },
+                ["Units"] = {["Mohato"] = {["DarkView"] = true, ["Amount"] = 1}}
+            }
+        }
+    },
+    ["Paragon"] = {
+        {
+            ["Standings"] = {1, 12},
+            ["Rewards"] = {
+                ["Currencies"] = {
+                    ["Trophies"] = {["Amount"] = 100},
+                    ["TraitRerolls"] = {["Amount"] = 30},
+                    ["Gems"] = {["Amount"] = 6000}
+                },
+                ["Units"] = {["Roku"] = {["Amount"] = 1}}
+            }
+        },
+        {
+            ["Standings"] = {12, 32},
+            ["Rewards"] = {
+                ["Currencies"] = {
+                    ["Trophies"] = {["Amount"] = 50},
+                    ["TraitRerolls"] = {["Amount"] = 15},
+                    ["Gems"] = {["Amount"] = 3000}
+                },
+                ["Units"] = {["Roku"] = {["Amount"] = 1}}
+            }
+        }
+    }
+}
