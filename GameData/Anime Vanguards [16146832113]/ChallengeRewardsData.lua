cbd8169e4b0c1597e4b035fe6cbce3bc42cd4810
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:21:30 +1100

    Create ChallengeRewardsData.lua

---
+++ GameData/Anime Vanguards [16146832113]/ChallengeRewardsData.lua
@@ -0,0 +1,54 @@
+data = {
+    ["HalfHourly"] = {
+        ["Essence Stone Challenge"] = {
+            ["Items"] = {
+                ["Green Essence Stone"] = {["Chance"] = 50, ["Amount"] = 2},
+                ["Rainbow Essence Stone"] = {["Chance"] = 10, ["Amount"] = 1},
+                ["Misc. Essence Stones"] = {
+                    ["Chance"] = 40,
+                    ["Items"] = {
+                        "Blue Essence Stone",
+                        "Red Essence Stone",
+                        "Yellow Essence Stone",
+                        "Pink Essence Stone",
+                        "Purple Essence Stone"
+                    },
+                    ["ItemSelectionMode"] = "ChooseOne",
+                    ["Amount"] = 1
+                }
+            },
+            ["ItemSelectionMode"] = "ChooseOne"
+        },
+        ["Trait Reroll Challenge"] = {
+            ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 1}},
+            ["Items"] = {["Green Essence Stone"] = {["Amount"] = 2}}
+        },
+        ["Stat Chip Challenge"] = {
+            ["Items"] = {
+                ["Green Essence Stone"] = {["Amount"] = 2},
+                ["Super Stat Chip"] = {["Amount"] = 1},
+                ["Stat Chip"] = {["Amount"] = 2}
+            }
+        }
+    },
+    ["Daily"] = {
+        ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 2}},
+        ["Items"] = {
+            ["Rainbow Essence Stone"] = {["Amount"] = 1},
+            ["Essence Stones"] = {
+                ["Items"] = {
+                    "Green Essence Stone",
+                    "Blue Essence Stone",
+                    "Red Essence Stone",
+                    "Yellow Essence Stone",
+                    "Pink Essence Stone",
+                    "Purple Essence Stone"
+                },
+                ["ItemSelectionMode"] = "ChooseOne",
+                ["Amount"] = 5
+            },
+            ["Super Stat Chip"] = {["Amount"] = 1},
+            ["Stat Chip"] = {["Amount"] = {2, 3}}
+        }
+    }
+}
