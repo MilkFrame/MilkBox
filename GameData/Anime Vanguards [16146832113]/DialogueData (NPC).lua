e096c6fb68608118f566182512493a5288f4605c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:15:36 +1100

    Create DialogueData (NPC).lua

---
+++ GameData/Anime Vanguards [16146832113]/DialogueData (NPC).lua
@@ -0,0 +1,132 @@
+data = {
+    ["RetrieveDialogueDataByName"] = "function: 0xffb8678c6d73eeb7",
+    ["_PreInit"] = "function: 0x2b7058f2fa4ffc17",
+    ["Cache"] = {
+        ["EmotesDialogue"] = {
+            ["DialogueGradient"] = "UIGradient",
+            ["Model"] = "Haruka Rin",
+            ["Options"] = {
+                {
+                    ["Cancel"] = {["Order"] = 2, ["Callback"] = "function: 0x7987236dc37e3b97"},
+                    ["Emotes"] = {["Order"] = 1, ["Callback"] = "function: 0xcf6ae8eccc1f3a17"}
+                }
+            },
+            ["ClientCallbacks"] = true,
+            ["Responses"] = {"Uwaaaah!~ I can't wait to dazzle everyone with this new sparkly expression!"},
+            ["DialogueTitle"] = "Haruka Rin"
+        },
+        ["EnemyIndexDialogue"] = {
+            ["DialogueGradient"] = "UIGradient",
+            ["Model"] = "Okabe",
+            ["Options"] = {
+                {
+                    ["Enemy Index"] = {["Order"] = 1, ["Callback"] = "function: 0x105a21ec55607d17"},
+                    ["Cancel"] = {["Order"] = 2, ["Callback"] = "function: 0x478a08fa625a4487"}
+                }
+            },
+            ["ClientCallbacks"] = true,
+            ["Responses"] = {
+                "Assistant! Our intel on the Organization's pawns expands exponentially - their downfall is but a matter of time!"
+            },
+            ["DialogueTitle"] = "Okabu"
+        },
+        ["CollectionDialogue"] = {
+            ["Options"] = {
+                {
+                    ["Collection"] = {["Order"] = 1, ["Callback"] = "function: 0x2c9a347049f5a857"},
+                    ["Cancel"] = {["Order"] = 2, ["Callback"] = "function: 0x7126e2d2ec567c97"}
+                }
+            },
+            ["Model"] = "Genas",
+            ["ClientCallbacks"] = true,
+            ["Responses"] = {
+                "Sensei, I have efficiently catalogued all acquired units in the collection interface for optimal strategic assessment."
+            },
+            ["DialogueTitle"] = "Genas"
+        },
+        ["TraitRerollDialogue"] = {
+            ["DialogueGradient"] = "UIGradient",
+            ["Model"] = "Madara",
+            ["Options"] = {
+                {
+                    ["Trait Reroll"] = {["Order"] = 1, ["Callback"] = "function: 0xc1813f4840f99c57"},
+                    ["Cancel"] = {["Order"] = 2, ["Callback"] = "function: 0xb6ef17fe1aad77d7"}
+                }
+            },
+            ["ClientCallbacks"] = true,
+            ["Responses"] = {
+                "Do you really think fate can outdo the genius of Devil Shinobi, Mandra? I'll mold these traits better than any hokage, crafting perfection with every roll."
+            },
+            ["DialogueTitle"] = "Mandra"
+        },
+        ["MilestonesDialogue"] = {
+            ["Options"] = {
+                {
+                    ["Level Milestones"] = {["Order"] = 1, ["Callback"] = "function: 0xcd5640b337b151d7"},
+                    ["Cancel"] = {["Order"] = 2, ["Callback"] = "function: 0xdc93df3256cb1bf7"}
+                }
+            },
+            ["Model"] = "Gilgamesh",
+            ["ClientCallbacks"] = true,
+            ["Responses"] = {
+                "Your pitiful struggles amuse me. Soon, you shall comprehend the true meaning of strength."
+            },
+            ["DialogueTitle"] = "Gilgamesh"
+        },
+        ["StarterUnitDialogue"] = {
+            ["Options"] = {
+                {["..."] = {["Order"] = 2}, ["Okay!"] = {["Order"] = 1, ["Callback"] = "function: 0x35648324ef48fea7"}},
+                {["Yeah!"] = {["Order"] = 1, ["Callback"] = "function: 0x62a8f0ad76285fd7"}, ["..."] = {["Order"] = 2}}
+            },
+            ["Responses"] = {
+                "Welcome to Anime Vanguards! Before you begin your journey, you need a unit to accompany you through your battles and get you started!",
+                "I'll let you choose between three starter units. You can get even stronger units by summoning them with gems, which are obtainable from beating stages. Are you ready?"
+            },
+            ["DialogueTitle"] = "Guide"
+        },
+        ["MissionsDialogue"] = {
+            ["Options"] = {
+                {
+                    ["Cancel"] = {["Order"] = 2, ["Callback"] = "function: 0x80dcc86bb2a3a4d7"},
+                    ["Missions"] = {["Order"] = 1, ["Callback"] = "function: 0xb86c51ced5692717"}
+                }
+            },
+            ["Model"] = "Okabe",
+            ["ClientCallbacks"] = true,
+            ["Responses"] = {
+                "Prepare yourselves! The machinations of SERN have presented us with an unexpected objective!."
+            },
+            ["DialogueTitle"] = "Okube"
+        },
+        ["StatRerollDialogue"] = {
+            ["DialogueGradient"] = "UIGradient",
+            ["Model"] = "Aqua",
+            ["Options"] = {
+                {
+                    ["Stat Reroll"] = {["Order"] = 1, ["Callback"] = "function: 0x47c939d6d7f4c117"},
+                    ["Stat Transfer"] = {["Order"] = 2, ["Callback"] = "function: 0x45f0e3e1592018d7"}
+                }
+            },
+            ["ClientCallbacks"] = true,
+            ["Responses"] = {
+                "Oh, you're rerolling your stats again? How typical of you! Just remember, no matter how many times you shuffle those numbers, you'll always be my adorable little adventurer!"
+            },
+            ["DialogueTitle"] = "Cyan"
+        },
+        ["AscensionsDialogue"] = {
+            ["DialogueGradient"] = "UIGradient",
+            ["Model"] = "Kokashi",
+            ["Options"] = {
+                {
+                    ["Ascend"] = {["Order"] = 1, ["Callback"] = "function: 0x60cf2757eee42197"},
+                    ["Cancel"] = {["Order"] = 2, ["Callback"] = "function: 0xc5cf1ece3e8d2a17"}
+                }
+            },
+            ["ClientCallbacks"] = true,
+            ["Responses"] = {
+                "To ascend, one must be willing to sacrifice. Offer what you hold dear, and be reborn in greater power."
+            },
+            ["DialogueTitle"] = "Kokashi"
+        }
+    }
+}
