70451e523664ecdfb6d7b78df64aeca0def4103d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:13:40 +1100

    Create BattlepassData.lua

---
+++ GameData/Anime Vanguards [16146832113]/BattlepassData.lua
@@ -0,0 +1,115 @@
+data = {
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 250}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 750}}},
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 250}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 750}}},
+    {
+        ["NormalRewards"] = {["TraitRerolls"] = {["Amount"] = 1}},
+        ["PremiumRewards"] = {["TraitRerolls"] = {["Amount"] = 4}}
+    },
+    {["NormalRewards"] = {["Gold"] = {["Amount"] = 1500}}, ["PremiumRewards"] = {["Gold"] = {["Amount"] = 4500}}},
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 250}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 750}}},
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 250}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 750}}},
+    {
+        ["NormalRewards"] = {["TraitRerolls"] = {["Amount"] = 1}},
+        ["PremiumRewards"] = {["TraitRerolls"] = {["Amount"] = 4}}
+    },
+    {["NormalRewards"] = {["Gold"] = {["Amount"] = 1500}}, ["PremiumRewards"] = {["Gold"] = {["Amount"] = 4500}}},
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 250}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 750}}},
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 250}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 750}}},
+    {
+        ["NormalRewards"] = {["TraitRerolls"] = {["Amount"] = 1}},
+        ["PremiumRewards"] = {["TraitRerolls"] = {["Amount"] = 4}}
+    },
+    {["NormalRewards"] = {["Gold"] = {["Amount"] = 1500}}, ["PremiumRewards"] = {["Gold"] = {["Amount"] = 4500}}},
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 250}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 750}}},
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 250}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 750}}},
+    {
+        ["NormalRewards"] = {["TraitRerolls"] = {["Amount"] = 1}},
+        ["PremiumRewards"] = {["TraitRerolls"] = {["Amount"] = 4}}
+    },
+    {["NormalRewards"] = {["Gold"] = {["Amount"] = 1500}}, ["PremiumRewards"] = {["Gold"] = {["Amount"] = 4500}}},
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 250}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 750}}},
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 250}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 750}}},
+    {
+        ["NormalRewards"] = {["TraitRerolls"] = {["Amount"] = 1}},
+        ["PremiumRewards"] = {["TraitRerolls"] = {["Amount"] = 4}}
+    },
+    {["NormalRewards"] = {["Gold"] = {["Amount"] = 1500}}, ["PremiumRewards"] = {["Gold"] = {["Amount"] = 4500}}},
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 250}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 750}}},
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 250}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 750}}},
+    {
+        ["NormalRewards"] = {["TraitRerolls"] = {["Amount"] = 1}},
+        ["PremiumRewards"] = {["TraitRerolls"] = {["Amount"] = 4}}
+    },
+    {["NormalRewards"] = {["Gold"] = {["Amount"] = 1500}}, ["PremiumRewards"] = {["Gold"] = {["Amount"] = 4500}}},
+    {
+        ["NormalRewards"] = {["Julias"] = {["Type"] = "Unit"}},
+        ["PremiumRewards"] = {["Julias"] = {["Shiny"] = true, ["Type"] = "Unit"}}
+    },
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 250}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 750}}},
+    {
+        ["NormalRewards"] = {["TraitRerolls"] = {["Amount"] = 1}},
+        ["PremiumRewards"] = {["TraitRerolls"] = {["Amount"] = 4}}
+    },
+    {["NormalRewards"] = {["Gold"] = {["Amount"] = 1500}}, ["PremiumRewards"] = {["Gold"] = {["Amount"] = 4500}}},
+    {
+        ["NormalRewards"] = {["Stat Chip"] = {["Amount"] = 4, ["Type"] = "Item"}},
+        ["PremiumRewards"] = {["Super Stat Chip"] = {["Amount"] = 2, ["Type"] = "Item"}}
+    },
+    {
+        ["NormalRewards"] = {["TraitRerolls"] = {["Amount"] = 2}},
+        ["PremiumRewards"] = {["TraitRerolls"] = {["Amount"] = 4}}
+    },
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 500}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 1000}}},
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 500}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 1000}}},
+    {
+        ["NormalRewards"] = {["TraitRerolls"] = {["Amount"] = 2}},
+        ["PremiumRewards"] = {["TraitRerolls"] = {["Amount"] = 4}}
+    },
+    {
+        ["NormalRewards"] = {["Stat Chip"] = {["Amount"] = 4, ["Type"] = "Item"}},
+        ["PremiumRewards"] = {["Super Stat Chip"] = {["Amount"] = 2, ["Type"] = "Item"}}
+    },
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 500}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 1000}}},
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 500}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 1000}}},
+    {
+        ["NormalRewards"] = {["TraitRerolls"] = {["Amount"] = 2}},
+        ["PremiumRewards"] = {["TraitRerolls"] = {["Amount"] = 4}}
+    },
+    {
+        ["NormalRewards"] = {["Stat Chip"] = {["Amount"] = 4, ["Type"] = "Item"}},
+        ["PremiumRewards"] = {["Super Stat Chip"] = {["Amount"] = 2, ["Type"] = "Item"}}
+    },
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 500}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 1000}}},
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 500}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 1000}}},
+    {
+        ["NormalRewards"] = {["TraitRerolls"] = {["Amount"] = 2}},
+        ["PremiumRewards"] = {["TraitRerolls"] = {["Amount"] = 5}}
+    },
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 500}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 1000}}},
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 750}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 1250}}},
+    {["NormalRewards"] = {["Gems"] = {["Amount"] = 1000}}, ["PremiumRewards"] = {["Gems"] = {["Amount"] = 1500}}},
+    {
+        ["NormalRewards"] = {["TraitRerolls"] = {["Amount"] = 2}},
+        ["PremiumRewards"] = {["TraitRerolls"] = {["Amount"] = 5}}
+    },
+    {
+        ["NormalRewards"] = {["TraitRerolls"] = {["Amount"] = 2}},
+        ["PremiumRewards"] = {["TraitRerolls"] = {["Amount"] = 5}}
+    },
+    {
+        ["NormalRewards"] = {["TraitRerolls"] = {["Amount"] = 2}},
+        ["PremiumRewards"] = {["TraitRerolls"] = {["Amount"] = 5}}
+    },
+    {
+        ["NormalRewards"] = {["TraitRerolls"] = {["Amount"] = 3}},
+        ["PremiumRewards"] = {["TraitRerolls"] = {["Amount"] = 5}}
+    },
+    {
+        ["NormalRewards"] = {["TraitRerolls"] = {["Amount"] = 3}},
+        ["PremiumRewards"] = {["TraitRerolls"] = {["Amount"] = 5}}
+    },
+    {
+        ["NormalRewards"] = {["Zion"] = {["Type"] = "Unit"}},
+        ["PremiumRewards"] = {["Zion"] = {["Shiny"] = true, ["Type"] = "Unit"}}
+    }
+}
