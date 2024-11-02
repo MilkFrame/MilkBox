6be24d80a82e26a57e71e77b0d354855c474c090
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:12:01 +1100

    Create TraitPool.lua

---
+++ GameData/Anime Vanguards [16146832113]/TraitPool.lua
@@ -0,0 +1,37 @@
+data = {
+    ["Fortune"] = {["Description"] = "Increased income or reduced cost!", ["Chance"] = 2.5},
+    ["Monarch"] = {
+        ["Description"] = "This unit stands above all else; MASSIVELY increased stats at a price...",
+        ["Chance"] = 0.1
+    },
+    ["Solar"] = {
+        ["Description"] = "This unit can see the stars; greatly increased range and slight increases to damage and attack speed!",
+        ["Chance"] = 0.5
+    },
+    ["Range"] = {
+        ["Chance"] = 26,
+        ["Tiers"] = {{["Chance"] = 50}, {["Chance"] = 30}, {["Chance"] = 10}},
+        ["Description"] = "Increases units range!"
+    },
+    ["Scholar"] = {["Description"] = "Increases experience gained by units!", ["Chance"] = 10},
+    ["Vigor"] = {
+        ["Chance"] = 26,
+        ["Tiers"] = {{["Chance"] = 60}, {["Chance"] = 30}, {["Chance"] = 10}},
+        ["Description"] = "Increases units damage!"
+    },
+    ["Swift"] = {
+        ["Chance"] = 26,
+        ["Tiers"] = {{["Chance"] = 60}, {["Chance"] = 40}, {["Chance"] = 10}},
+        ["Description"] = "Increases units attack speed!"
+    },
+    ["Marksman"] = {["Description"] = "Greatly increases units attack range!", ["Chance"] = 6.5},
+    ["Deadeye"] = {
+        ["Description"] = "This unit has deadly accuracy; increases critical chance and damage!",
+        ["Chance"] = 0.375
+    },
+    ["Blitz"] = {["Description"] = "This unit attacks a lot faster!", ["Chance"] = 1.85},
+    ["Ethereal"] = {
+        ["Description"] = "This unit is not of this world; gains greatly increased damage and attack speed!",
+        ["Chance"] = 0.175
+    }
+}
