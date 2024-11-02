b7e4d1a4395192959de982a9ff5fefbe87c6103e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:15:21 +1100

    Create TitlesData.lua

---
+++ GameData/Anime Vanguards [16146832113]/TitlesData.lua
@@ -0,0 +1,41 @@
+data = {
+    ["Titles"] = {
+        {["Name"] = "VIP", ["Description"] = "Rewarded to players who bought the VIP gamepass"},
+        {["Name"] = "Tester", ["Description"] = "Rewarded to players who have tested the game"},
+        {["Name"] = "Content Creator", ["Description"] = "Rewarded to players who create content for Anime Vanguards"},
+        {["Name"] = "Developer", ["Description"] = "Rewarded to developers who work on Anime Vanguards"},
+        {["Name"] = "Owner", ["Description"] = "Rewarded to owners of Anime Vanguards"},
+        {["Name"] = "Pretty Princess", ["Description"] = "Rewarded to the prettiest princess (Kazi)"},
+        {["Name"] = "Emo Goth Vampire", ["Description"] = "Rewarded to the most queer developer"},
+        {["Name"] = "CT", ["Description"] = "For peasants like ClyaTaksj"},
+        {["Name"] = "Infinity", ["Description"] = "For peasants balancers like Andrevious"},
+        {["Name"] = "Mikasa", ["Description"] = "Rewarded to less pretty princesses (Mina)"},
+        {["Name"] = "Sleep Deprived Artist", ["Description"] = "Rewarded to the lest queer artist"},
+        {["Name"] = "Imperium", ["Description"] = "Rewarded to the other vfx guy lol"},
+        {["Name"] = "Miku", ["Description"] = "Rewarded to Kobo's favorite artist"},
+        {["Name"] = "Admin", ["Description"] = "Rewarded to Administrators of our Discord server"},
+        {["Name"] = "Moderator", ["Description"] = "Rewarded to Moderators of our Discord server"},
+        {["Name"] = "Shadow Monarch", ["Description"] = "Obtained from evolving Song Jinwu"},
+        {["Name"] = "Strongest Shadow", ["Description"] = "Obtained from evolving Igros"},
+        {["Name"] = "Prideful Earthling", ["Description"] = "Complete all stages on Planet Namak (Normal)"},
+        {["Name"] = "Rogue Shinobi", ["Description"] = "Complete all stages on Sand Village (Normal)"},
+        {["Name"] = "Level Up Legend", ["Description"] = "Complete all stages on Double Dungeon (Normal)"},
+        {
+            ["Name"] = "Blood-Red Commander",
+            ["Description"] = "Obtained from completing Igros Boss Event achievement category"
+        },
+        {
+            ["Name"] = "Paragon Conqueror - Planet Namak",
+            ["Description"] = "Complete all Paragon Conqueror achievements for Planet Namak"
+        },
+        {
+            ["Name"] = "Paragon Conqueror - Sand Village",
+            ["Description"] = "Complete all Paragon Conqueror achievements for Sand Village"
+        },
+        {
+            ["Name"] = "Paragon Conqueror - Double Dungeon",
+            ["Description"] = "Complete all Paragon Conqueror achievements for Double Dungeon"
+        }
+    },
+    ["GetTitleDataFromName"] = "function: 0xa431781086a0a527"
+}
