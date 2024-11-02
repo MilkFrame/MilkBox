b5471a7a415d8173028d36bdba5e524c64f3c100
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:17:23 +1100

    Create AchievementsData.lua

---
+++ GameData/Anime Vanguards [16146832113]/AchievementsData.lua
@@ -0,0 +1,242 @@
+data = {
+    ["UnitCategories"] = {
+        "Prideful Prince",
+        "Shadow Commander",
+        "Shadow Monarch",
+        "Vengeful Prodigy",
+        "Prideful Prince",
+        "God of Flashiness",
+        "Knucklehead Ninja",
+        "Dancer In Battle",
+        "Bird of Hermes",
+        "Blazing Heart",
+        "Beyond The Mask",
+        "Art of Destruction"
+    },
+    ["Categories"] = {
+        ["Paragon Conqueror — Shibuya (I)"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 18}, ["Gems"] = {["Amount"] = 900}},
+                ["Titles"] = {["Paragon Conqueror - Shibuya"] = {["Amount"] = 1}},
+                ["Emotes"] = {["Honored One"] = {["Amount"] = 1}}
+            },
+            ["Background"] = "rbxassetid://17569559711"
+        },
+        ["Blazing Heart"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 10000}},
+                ["Banners"] = {["Eternal Ember"] = {["Amount"] = 1}},
+                ["Items"] = {["Super Stat Chip"] = {["Amount"] = 5}}
+            },
+            ["Background"] = "rbxassetid://106361528360638"
+        },
+        ["Vengeful Prodigy"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 10000}},
+                ["Banners"] = {["Kirin of the Clouds"] = {["Amount"] = 1}},
+                ["Items"] = {["Super Stat Chip"] = {["Amount"] = 5}}
+            },
+            ["Background"] = "rbxassetid://102881987016951"
+        },
+        ["Behold, My Arachnid Exterminator!"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 5}, ["Gems"] = {["Amount"] = 1000}},
+                ["Items"] = {["Stat Chip"] = {["Amount"] = 11}},
+                ["Emotes"] = {["California Gurls"] = {["Amount"] = 1}}
+            }
+        },
+        ["Beyond The Mask"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 10000}},
+                ["Banners"] = {["Eyes of Despair"] = {["Amount"] = 1}},
+                ["Items"] = {["Super Stat Chip"] = {["Amount"] = 5}}
+            },
+            ["Background"] = "rbxassetid://106361528360638"
+        },
+        ["Trouble on Planet Namak"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 2}, ["Gems"] = {["Amount"] = 1000}},
+                ["Mounts"] = {["Nimbus"] = {["Amount"] = 1}},
+                ["Emotes"] = {["Casey"] = {["Amount"] = 1}}
+            },
+            ["Background"] = "rbxassetid://97440324036264"
+        },
+        ["Raid on Sand Village"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 5}, ["Gems"] = {["Amount"] = 1000}},
+                ["Mounts"] = {["Elder Sage"] = {["Amount"] = 1}},
+                ["Emotes"] = {["Ric Flair"] = {["Amount"] = 1}}
+            },
+            ["Background"] = "rbxassetid://17569559711"
+        },
+        ["Blood Brother"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 10000}},
+                ["Banners"] = {["Curse of Blood"] = {["Amount"] = 1}},
+                ["Items"] = {["Super Stat Chip"] = {["Amount"] = 5}}
+            },
+            ["Background"] = "rbxassetid://106361528360638"
+        },
+        ["Battle Against An Elite Knight"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 1000}},
+                ["Titles"] = {["Blood-Red Commander"] = {["Amount"] = 1}}
+            },
+            ["Background"] = "rbxassetid://73565271269093"
+        },
+        ["Lava Incarnate"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 10000}},
+                ["Banners"] = {["Mountain of Destruction"] = {["Amount"] = 1}},
+                ["Items"] = {["Super Stat Chip"] = {["Amount"] = 5}}
+            },
+            ["Background"] = "rbxassetid://106361528360638"
+        },
+        ["Disaster in the Double Dungeon"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 5}, ["Gems"] = {["Amount"] = 1000}},
+                ["Mounts"] = {["Shadow Bear"] = {["Amount"] = 1}},
+                ["Emotes"] = {["Popular Vibe"] = {["Amount"] = 1}}
+            },
+            ["Background"] = "rbxassetid://106361528360638"
+        },
+        ["Paragon Conqueror — Namak (I)"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 18}, ["Gems"] = {["Amount"] = 900}},
+                ["Titles"] = {["Paragon Conqueror - Planet Namak"] = {["Amount"] = 1}},
+                ["Emotes"] = {["Tidy"] = {["Amount"] = 1}}
+            },
+            ["Background"] = "rbxassetid://97440324036264"
+        },
+        ["Paragon Conqueror — Sand Village (I)"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 18}, ["Gems"] = {["Amount"] = 900}},
+                ["Titles"] = {["Paragon Conqueror - Sand Village"] = {["Amount"] = 1}},
+                ["Emotes"] = {["Griddy"] = {["Amount"] = 1}}
+            },
+            ["Background"] = "rbxassetid://17569559711"
+        },
+        ["The Honored One"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 10000}},
+                ["Banners"] = {["Red and Blue"] = {["Amount"] = 1}},
+                ["Items"] = {["Super Stat Chip"] = {["Amount"] = 5}}
+            },
+            ["Background"] = "rbxassetid://106361528360638"
+        },
+        ["King of Curses"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 10000}},
+                ["Banners"] = {["Malevolant Kitchen"] = {["Amount"] = 1}},
+                ["Items"] = {["Super Stat Chip"] = {["Amount"] = 5}}
+            },
+            ["Background"] = "rbxassetid://106361528360638"
+        },
+        ["Bird of Hermes"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 10000}},
+                ["Banners"] = {["Vampire King"] = {["Amount"] = 1}},
+                ["Items"] = {["Super Stat Chip"] = {["Amount"] = 5}}
+            },
+            ["Background"] = "rbxassetid://118412898644607"
+        },
+        ["Dancer In Battle"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 10000}},
+                ["Banners"] = {["S-Rank Valkyrie"] = {["Amount"] = 1}},
+                ["Items"] = {["Super Stat Chip"] = {["Amount"] = 5}}
+            },
+            ["Background"] = "rbxassetid://106361528360638"
+        },
+        ["Sorcerer Slayer"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 10000}},
+                ["Banners"] = {["Heavenely Restriction"] = {["Amount"] = 1}},
+                ["Items"] = {["Super Stat Chip"] = {["Amount"] = 5}}
+            },
+            ["Background"] = "rbxassetid://106361528360638"
+        },
+        ["Shadow Monarch"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 10000}},
+                ["Banners"] = {["Commander of Shadows"] = {["Amount"] = 1}},
+                ["Items"] = {["Super Stat Chip"] = {["Amount"] = 5}}
+            },
+            ["Background"] = "rbxassetid://106361528360638"
+        },
+        ["Knucklehead Ninja"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 10000}},
+                ["Banners"] = {["Tailed Beast"] = {["Amount"] = 1}},
+                ["Items"] = {["Super Stat Chip"] = {["Amount"] = 5}}
+            },
+            ["Background"] = "rbxassetid://106361528360638"
+        },
+        ["Shadow Commander"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 10000}},
+                ["Banners"] = {["Blood-Red Commander"] = {["Amount"] = 1}},
+                ["Items"] = {["Super Stat Chip"] = {["Amount"] = 5}}
+            },
+            ["Background"] = "rbxassetid://86677329052388"
+        },
+        ["God of Flashiness"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 10000}},
+                ["Banners"] = {["Dazzling Blade"] = {["Amount"] = 1}},
+                ["Items"] = {["Super Stat Chip"] = {["Amount"] = 5}}
+            },
+            ["Background"] = "rbxassetid://106361528360638"
+        },
+        ["Art of Destruction"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 10000}},
+                ["Banners"] = {["Fist of Fury"] = {["Amount"] = 1}},
+                ["Items"] = {["Super Stat Chip"] = {["Amount"] = 5}}
+            },
+            ["Background"] = "rbxassetid://106361528360638"
+        },
+        ["The Collector"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {
+                    ["Gems"] = {["Amount"] = 2000},
+                    ["TraitRerolls"] = {["Amount"] = 5},
+                    ["Gold"] = {["Amount"] = 10000}
+                },
+                ["Emotes"] = {["Jabba Switchway"] = {["Amount"] = 1}}
+            },
+            ["Background"] = "rbxassetid://18232498558"
+        },
+        ["Paragon Conqueror — Double Dungeon (I)"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 18}, ["Gems"] = {["Amount"] = 900}},
+                ["Titles"] = {["Paragon Conqueror - Double Dungeon"] = {["Amount"] = 1}},
+                ["Emotes"] = {["Boogie Down"] = {["Amount"] = 1}}
+            },
+            ["Background"] = "rbxassetid://106361528360638"
+        },
+        ["Prideful Prince"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 10000}},
+                ["Banners"] = {["Egotistical Fighter"] = {["Amount"] = 1}},
+                ["Items"] = {["Super Stat Chip"] = {["Amount"] = 5}}
+            },
+            ["Background"] = "rbxassetid://106361528360638"
+        },
+        ["Boogie Woogie"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 20}, ["Gems"] = {["Amount"] = 10000}},
+                ["Banners"] = {["Brotha"] = {["Amount"] = 1}},
+                ["Items"] = {["Super Stat Chip"] = {["Amount"] = 5}}
+            },
+            ["Background"] = "rbxassetid://106361528360638"
+        },
+        ["Train Station Massacre"] = {
+            ["Rewards"] = {
+                ["Currencies"] = {["TraitRerolls"] = {["Amount"] = 5}, ["Gems"] = {["Amount"] = 1000}},
+                ["Emotes"] = {["Orange Justice"] = {["Amount"] = 1}}
+            },
+            ["Background"] = "rbxassetid://97592861844547"
+        }
+    }
+}
