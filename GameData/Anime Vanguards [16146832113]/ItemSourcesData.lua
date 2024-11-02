4d117c2e34316d4a34ebc299c190f7c345dabf66
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:24:46 +1100

    Create ItemSourcesData.lua

---
+++ GameData/Anime Vanguards [16146832113]/ItemSourcesData.lua
@@ -0,0 +1,40 @@
+data = {
+    ["MiscItems"] = {
+        ["Super Stat Chip"] = {"Daily Challenge", "Battle Pass"},
+        ["Stat Chip"] = {"Daily Challenge", "Battle Pass"}
+    },
+    ["EvolutionItems"] = {
+        ["Hardened Blood"] = {"Shibuya Legend Stage 3"},
+        ["Demon Beads"] = {"Raid Shop"},
+        ["Cursed Finger"] = {"Shibuya Infinite", "..??"},
+        ["Broken Sword"] = {"Double Dungeon Legend Stage 1"},
+        ["Shadow Traces"] = {"Double Dungeon Legend Stage 2"},
+        ["Nichirin Cleavers"] = {"Raid Shop"},
+        ["Blue Chakra"] = {"Sand Village Legend Stage 1"},
+        ["Hellsing Arms"] = {"Summon"},
+        ["Magma Stone"] = {"Shibuya Legend Stage 1"},
+        ["Six Eyes"] = {"Special Conditions"},
+        ["Cursed Worm"] = {"Special Conditions"},
+        ["Blood-Red Armor"] = {"Double Dungeon Legend Stage 3"},
+        ["Broken Pendant"] = {"Shibuya Legend Stage 2"},
+        ["Snake Scale"] = {"Sand Village Legend Stage 2"},
+        ["Slayer's Cape"] = {"Raid Shop"},
+        ["Skin Patch"] = {"Sand Village Legend Stage 3"},
+        ["Red Chakra"] = {"Sand Village Legend Stage 1"}
+    },
+    ["EssenceStones"] = {
+        ["Pink Essence Stone"] = {"Challenges", "Crafting"},
+        ["Blue Essence Stone"] = {"Challenges", "Crafting"},
+        ["Red Essence Stone"] = {"Challenges", "Crafting"},
+        ["Green Essence Stone"] = {"Challenges", "Crafting", "Legend Stages"},
+        ["Yellow Essence Stone"] = {"Challenges", "Crafting"},
+        ["Purple Essence Stone"] = {"Challenges", "Crafting"},
+        ["Rainbow Essence Stone"] = {"Challenges", "Crafting"}
+    },
+    ["UnitFood"] = {
+        ["Crystal"] = {"Double Dungeon Acts 1-6", "Double Dungeon Paragon Mode", "Double Dungeon Legend Stages"},
+        ["Transfigured Soul"] = {"Shibuya Acts 1-6", "Shibuya Paragon Mode", "Shibuya Legend Stages"},
+        ["Ramen"] = {"Sand Village Acts 1-6", "Sand Village Paragon Mode", "Sand Village Legend Stages"},
+        ["Senzu Bean"] = {"Namak Acts 1-6", "Namak Paragon Mode"}
+    }
+}
