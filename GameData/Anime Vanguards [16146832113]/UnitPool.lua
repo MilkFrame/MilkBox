6d866110e31306c4f7c85d999402013316238b1c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:14:39 +1100

    Create UnitPool.lua

---
+++ GameData/Anime Vanguards [16146832113]/UnitPool.lua
@@ -0,0 +1,56 @@
+data = {
+    ["Legendary"] = {
+        ["Inamuki"] = {["Chance"] = 50},
+        ["Roku (Dark)"] = {["Chance"] = 50},
+        ["Grim Wow"] = {["Chance"] = 50},
+        ["Itochi"] = {["Chance"] = 50},
+        ["Agony"] = {["Chance"] = 50},
+        ["Bean"] = {["Chance"] = 50},
+        ["Goi"] = {["Chance"] = 50},
+        ["Kinaru"] = {["Chance"] = 50},
+        ["Takaroda"] = {["Chance"] = 50},
+        ["Nobaba"] = {["Chance"] = 50}
+    },
+    ["Mythic"] = {
+        ["Tengon"] = {["Chance"] = 50},
+        ["Vogita Super"] = {["Chance"] = 50},
+        ["Cha-In"] = {["Chance"] = 50},
+        ["Akazo"] = {["Chance"] = 50},
+        ["Song Jinwu"] = {["Chance"] = 50},
+        ["Gujo"] = {["Chance"] = 50},
+        ["Todu"] = {["Chance"] = 50},
+        ["Jag-o"] = {["Chance"] = 50},
+        ["Chaso"] = {["Chance"] = 50},
+        ["Itaduri"] = {["Chance"] = 50},
+        ["Sosuke (Hebi)"] = {["Chance"] = 50},
+        ["Obita"] = {["Chance"] = 50},
+        ["Noruto (Sage)"] = {["Chance"] = 50}
+    },
+    ["Rare"] = {
+        ["Jon"] = {["Chance"] = 50},
+        ["Roku"] = {["Chance"] = 50},
+        ["Ichiga"] = {["Chance"] = 50},
+        ["Joe"] = {["Chance"] = 50},
+        ["Sanjo"] = {["Chance"] = 50},
+        ["Rukio"] = {["Chance"] = 50},
+        ["Vogita"] = {["Chance"] = 50},
+        ["Noruto"] = {["Chance"] = 50},
+        ["Sosuke"] = {["Chance"] = 50},
+        ["Luffo"] = {["Chance"] = 50}
+    },
+    ["Epic"] = {
+        ["Shinzi"] = {["Chance"] = 50},
+        ["Genitsu"] = {["Chance"] = 50},
+        ["Genas"] = {["Chance"] = 50},
+        ["Inosake"] = {["Chance"] = 50},
+        ["Blossom"] = {["Chance"] = 50},
+        ["Kokashi"] = {["Chance"] = 50},
+        ["Pickleo"] = {["Chance"] = 50},
+        ["Nazuka"] = {["Chance"] = 50},
+        ["Gaari"] = {["Chance"] = 50},
+        ["Sprintwagon"] = {["Chance"] = 50},
+        ["Kinnua"] = {["Chance"] = 50},
+        ["Alligator"] = {["Chance"] = 50}
+    },
+    ["Secret"] = {["Alocard"] = {["Chance"] = 100}}
+}
