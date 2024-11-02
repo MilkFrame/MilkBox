d530848b3b0128ac26ddb1d19c0f5271974fba44
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:13:52 +1100

    Create DailyRewardsData.lua

---
+++ GameData/Anime Vanguards [16146832113]/DailyRewardsData.lua
@@ -0,0 +1,9 @@
+data = {
+    {["Type"] = "Gems", ["Image"] = "rbxassetid://18543107547", ["Amount"] = 100},
+    {["Type"] = "Gems", ["Image"] = "rbxassetid://18543107547", ["Amount"] = 150},
+    {["Type"] = "Gems", ["Image"] = "rbxassetid://18543111862", ["Amount"] = 175},
+    {["Type"] = "Gems", ["Image"] = "rbxassetid://18543111862", ["Amount"] = 220},
+    {["Type"] = "Gems", ["Image"] = "rbxassetid://18543109517", ["Amount"] = 250},
+    {["Type"] = "Gems", ["Image"] = "rbxassetid://18543109517", ["Amount"] = 275},
+    {["Type"] = "Gems", ["Image"] = "rbxassetid://18543112547", ["Amount"] = 430}
+}
