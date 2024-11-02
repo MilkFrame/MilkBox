8ceebd6559dd47e0b182e570d0c3640dace56b15
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:21:15 +1100

    Create EmotesData.lua

---
+++ GameData/Anime Vanguards [16146832113]/EmotesData.lua
@@ -0,0 +1,22 @@
+--require(game.ReplicatedStorage.Modules.Data.EmotesData).GetAllEmotesData()
+data = {
+    ["Ric Flair"] = {["Description"] = "Ric Flair Drip", ["Animation"] = "Ric Flair"},
+    ["Take The L"] = {["Description"] = "LOL LOSERRRRR", ["Animation"] = "Take The L"},
+    ["Jabba Switchway"] = {["Description"] = "Become a Jabbawockee :P", ["Animation"] = "Jabba Switchway"},
+    ["Popular Vibe"] = {["Description"] = "Am I popular now?", ["Animation"] = "Popular Vibe"},
+    ["Honored One"] = {["Description"] = "🫸🔵🔴🫷🤌🫴🟣", ["Pivot"] = CFrame.new(-36.9000015, 5.5999999, -46, 1, 0, 0, 0, 0.642787635, 0.766044378, 0, -0.766044378, 0.642787635), ["Animation"] = "Honored One"},
+    ["Electro Shuffle"] = {["Description"] = "Turn it up!", ["Animation"] = "Electro Shuffle"},
+    ["Casey"] = {["Description"] = "Casey Casey Casey", ["Animation"] = "Casey"},
+    ["Smooth Criminal"] = {["Description"] = "Hee Heeee", ["Animation"] = "Smooth Criminal"},
+    ["God Requiem"] = {["Description"] = "Hey you DANCE WITH ME!", ["Animation"] = "God Requiem"},
+    ["Unforgettable"] = {["Description"] = "It's not good enough for me since I been with you", ["Animation"] = "Unforgettable"},
+    ["Blood-Red Commander"] = {["CustomAnimation"] = "Blood-Red Commander", ["Description"] = "sample description"},
+    ["California Gurls"] = {["Description"] = "jason dance", ["Animation"] = "California Gurls"},
+    ["Goofy Skeleton"] = {["Description"] = "Man I'm Dead", ["Animation"] = "Goofy Skeleton"},
+    ["Tidy"] = {["Description"] = "I learned this one from my Dogg Snoop", ["Animation"] = "Tidy"},
+    ["Orange Justice"] = {["Description"] = "OMG it's that epic game dance from 2018", ["Animation"] = "Orange Justice"},
+    ["Jubilation"] = {["Description"] = "Yippieeeeee", ["Animation"] = "Jubilation"},
+    ["Boogie Down"] = {["Description"] = "Let's get down to Bizzness", ["Animation"] = "Boogie Down"},
+    ["Griddy"] = {["Description"] = "Yo bro watch me hit the Griddy", ["Animation"] = "Griddy"},
+    ["Smug Dance"] = {["Description"] = "Smug uwu", ["Animation"] = "Smug Dance"}
+}
