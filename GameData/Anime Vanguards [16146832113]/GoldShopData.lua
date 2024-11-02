d80b5e6a5d69ae50cbf3534a2bd67cbc5e837a9b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:12:17 +1100

    Create GoldShopData.lua

---
+++ GameData/Anime Vanguards [16146832113]/GoldShopData.lua
@@ -0,0 +1,38 @@
+--require(game.ReplicatedStorage.Modules.Data.GoldShopData).GetAllData()
+data = {
+    ["Currencies"] = {},
+    ["Items"] = {
+        ["Crystal"] = {["Order"] = 3, ["MaxQuantity"] = 50, ["Name"] = "Crystal", ["Price"] = 150},
+        ["Blue Essence Stone"] = {["Order"] = 7, ["MaxQuantity"] = 3, ["Name"] = "Blue Essence Stone", ["Price"] = 2000},
+        ["Red Essence Stone"] = {["Order"] = 5, ["MaxQuantity"] = 3, ["Name"] = "Red Essence Stone", ["Price"] = 2000},
+        ["Yellow Essence Stone"] = {
+            ["Order"] = 6,
+            ["MaxQuantity"] = 3,
+            ["Name"] = "Yellow Essence Stone",
+            ["Price"] = 2000
+        },
+        ["Purple Essence Stone"] = {
+            ["Order"] = 8,
+            ["MaxQuantity"] = 3,
+            ["Name"] = "Purple Essence Stone",
+            ["Price"] = 2000
+        },
+        ["Senzu Bean"] = {["Order"] = 1, ["MaxQuantity"] = 50, ["Name"] = "Senzu Bean", ["Price"] = 50},
+        ["Pink Essence Stone"] = {["Order"] = 9, ["MaxQuantity"] = 3, ["Name"] = "Pink Essence Stone", ["Price"] = 2000},
+        ["Ramen"] = {["Order"] = 2, ["MaxQuantity"] = 50, ["Name"] = "Ramen", ["Price"] = 100},
+        ["Green Essence Stone"] = {
+            ["Order"] = 4,
+            ["MaxQuantity"] = 12,
+            ["Name"] = "Green Essence Stone",
+            ["Price"] = 750
+        },
+        ["Super Stat Chip"] = {["Order"] = 12, ["MaxQuantity"] = 1, ["Name"] = "Super Stat Chip", ["Price"] = 4000},
+        ["Rainbow Essence Stone"] = {
+            ["Order"] = 10,
+            ["MaxQuantity"] = 1,
+            ["Name"] = "Rainbow Essence Stone",
+            ["Price"] = 5000
+        },
+        ["Stat Chip"] = {["Order"] = 11, ["MaxQuantity"] = 2, ["Name"] = "Stat Chip", ["Price"] = 2500}
+    }
+}
