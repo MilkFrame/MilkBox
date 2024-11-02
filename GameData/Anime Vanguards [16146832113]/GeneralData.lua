413ee734ae3668313300a6083f5ce15961ced9b5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:21:43 +1100

    Create GeneralData.lua

---
+++ GameData/Anime Vanguards [16146832113]/GeneralData.lua
@@ -0,0 +1,16 @@
+data = {
+    ["TournamentEndDate"] = "1730246400000",
+    ["IgrosBossEventEndDate"] = "1728864000000",
+    ["CurrentTournament"] = "Tournament1",
+    ["MaxUnitLevel"] = 60,
+    ["MaxBattlePassLevel"] = 25,
+    ["BattlePassEndDate"] = "1729468800000",
+    ["BossEventGUID"] = 1,
+    ["MaxLegendaryPity"] = 50,
+    ["TournamentStartDate"] = "1729382400000",
+    ["IgrosBossEventStartDate"] = "1727049600000",
+    ["BattlePassStartDate"] = "1725840000000",
+    ["RaidGUID"] = 1,
+    ["MaxWorthiness"] = 5000,
+    ["MaxMythicPity"] = 400
+}
