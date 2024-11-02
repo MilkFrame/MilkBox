baef640808951bdfaf67336756956c9d57e30d64
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:13:03 +1100

    Create EvolveDataHandler.lua

---
+++ GameData/Anime Vanguards [16146832113]/EvolveDataHandler.lua
@@ -0,0 +1,284 @@
+data = {
+    ["GetEvolutionDataByName"] = "function: 0xc5afb1611dead597",
+    ["Cache"] = {
+        ["Obita (Awakened)"] = {
+            ["Requirements"] = {
+                ["Skin Patch"] = {["Amount"] = 12},
+                ["Green Essence Stone"] = {["Amount"] = 30},
+                ["Yellow Essence Stone"] = {["Amount"] = 11},
+                ["Purple Essence Stone"] = {["Amount"] = 12},
+                ["Rainbow Essence Stone"] = {["Amount"] = 1}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Obita (Awakened)", ["Identifier"] = "24:Evolved"},
+            ["EvolveBenefits"] = {"+30% Damage", "+Flame Twisters", "+1 New Passive"},
+            ["Cost"] = 15000
+        },
+        ["Renguko (Purgatory)"] = {
+            ["Requirements"] = {
+                ["Pink Essence Stone"] = {["Amount"] = 13},
+                ["Red Essence Stone"] = {["Amount"] = 14},
+                ["Green Essence Stone"] = {["Amount"] = 40},
+                ["Yellow Essence Stone"] = {["Amount"] = 14},
+                ["Rainbow Essence Stone"] = {["Amount"] = 4},
+                ["Slayer's Cape"] = {["Amount"] = 1}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Renguko (Purgatory)", ["Identifier"] = "51:Evolved"},
+            ["EvolveBenefits"] = {"+33% Damage", "+Scorching Spirals", "+Ninth Form", "+2 New Passives"},
+            ["Cost"] = 15000
+        },
+        ["Tuji (Sorcerer Killer)"] = {
+            ["Takedowns"] = 5000,
+            ["Cost"] = 30000,
+            ["Requirements"] = {
+                ["Blue Essence Stone"] = {["Amount"] = 15},
+                ["Red Essence Stone"] = {["Amount"] = 15},
+                ["Green Essence Stone"] = {["Amount"] = 40},
+                ["Rainbow Essence Stone"] = {["Amount"] = 4},
+                ["Purple Essence Stone"] = {["Amount"] = 15},
+                ["Cursed Worm"] = {["Amount"] = 1}
+            },
+            ["EvolveBenefits"] = {
+                "+25% Damage",
+                "+Domain Breaker",
+                "+Chain Blitz",
+                "+1 New Active Ability",
+                "+1 New Passive"
+            },
+            ["EvolvedUnit"] = {["Name"] = "Tuji (Sorcerer Killer)", ["Identifier"] = "65:Evolved"}
+        },
+        ["Todu (Unleashed)"] = {
+            ["Requirements"] = {
+                ["Blue Essence Stone"] = {["Amount"] = 10},
+                ["Broken Pendant"] = {["Amount"] = 12},
+                ["Green Essence Stone"] = {["Amount"] = 38},
+                ["Yellow Essence Stone"] = {["Amount"] = 14},
+                ["Purple Essence Stone"] = {["Amount"] = 14},
+                ["Rainbow Essence Stone"] = {["Amount"] = 3}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Todu (Unleashed)", ["Identifier"] = "56:Evolved"},
+            ["EvolveBenefits"] = {"+20% Damage", "+Dropkick", "+Brotha Special", "+1 New Passive"},
+            ["Cost"] = 15000
+        },
+        ["Cha-In (Blade Dancer)"] = {
+            ["Requirements"] = {
+                ["Blue Essence Stone"] = {["Amount"] = 12},
+                ["Broken Sword"] = {["Amount"] = 12},
+                ["Rainbow Essence Stone"] = {["Amount"] = 2},
+                ["Purple Essence Stone"] = {["Amount"] = 11},
+                ["Green Essence Stone"] = {["Amount"] = 30}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Cha-In (Blade Dancer)", ["Identifier"] = "22:Evolved"},
+            ["EvolveBenefits"] = {"+40% Damage", "+Light of the End", "+1 New Passive"},
+            ["Cost"] = 15000
+        },
+        ["Vogita Super (Awakened)"] = {
+            ["Requirements"] = {
+                ["Senzu Bean"] = {["Amount"] = 80},
+                ["Blue Essence Stone"] = {["Amount"] = 15},
+                ["Green Essence Stone"] = {["Amount"] = 35},
+                ["Yellow Essence Stone"] = {["Amount"] = 10},
+                ["Rainbow Essence Stone"] = {["Amount"] = 2}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Vogita Super (Awakened)", ["Identifier"] = "18:Evolved"},
+            ["EvolveBenefits"] = {"+20% Damage", "+Final Flash", "+1 New Passive"},
+            ["Cost"] = 15000
+        },
+        ["Haruka Rin (Dancer)"] = {
+            ["Requirements"] = {
+                ["Blue Essence Stone"] = {["Amount"] = 11},
+                ["Red Essence Stone"] = {["Amount"] = 9},
+                ["Green Essence Stone"] = {["Amount"] = 35},
+                ["Yellow Essence Stone"] = {["Amount"] = 11},
+                ["Rainbow Essence Stone"] = {["Amount"] = 1}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Haruka Rin (Dancer)", ["Identifier"] = "25:Evolved"},
+            ["EvolveBenefits"] = {"+1 New Passive"},
+            ["Cost"] = 15000
+        },
+        ["Alocard (Vampire King)"] = {
+            ["Requirements"] = {
+                ["Red Essence Stone"] = {["Amount"] = 13},
+                ["Hellsing Arms"] = {["Amount"] = 1},
+                ["Green Essence Stone"] = {["Amount"] = 35},
+                ["Yellow Essence Stone"] = {["Amount"] = 13},
+                ["Purple Essence Stone"] = {["Amount"] = 13},
+                ["Rainbow Essence Stone"] = {["Amount"] = 2}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Alocard (Vampire King)", ["Identifier"] = "16:Evolved"},
+            ["EvolveBenefits"] = {"+30% Damage", "+Hell Hounds", "+The Bird of Hermes", "+1 Active Ability"},
+            ["Cost"] = 30000
+        },
+        ["Akazo (Destructive)"] = {
+            ["Requirements"] = {
+                ["Demon Beads"] = {["Amount"] = 1},
+                ["Pink Essence Stone"] = {["Amount"] = 13},
+                ["Blue Essence Stone"] = {["Amount"] = 13},
+                ["Red Essence Stone"] = {["Amount"] = 3},
+                ["Green Essence Stone"] = {["Amount"] = 43},
+                ["Yellow Essence Stone"] = {["Amount"] = 3},
+                ["Purple Essence Stone"] = {["Amount"] = 13},
+                ["Rainbow Essence Stone"] = {["Amount"] = 3}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Akazo (Destructive)", ["Identifier"] = "50:Evolved"},
+            ["EvolveBenefits"] = {"+50% Damage", "+Annihilation", "+1 New Passive", "+1 Active Ability"},
+            ["Cost"] = 15000
+        },
+        ["Jag-o (Volcanic)"] = {
+            ["Requirements"] = {
+                ["Pink Essence Stone"] = {["Amount"] = 15},
+                ["Rainbow Essence Stone"] = {["Amount"] = 3},
+                ["Green Essence Stone"] = {["Amount"] = 40},
+                ["Yellow Essence Stone"] = {["Amount"] = 15},
+                ["Purple Essence Stone"] = {["Amount"] = 15},
+                ["Magma Stone"] = {["Amount"] = 12}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Jag-o (Volcanic)", ["Identifier"] = "63:Evolved"},
+            ["EvolveBenefits"] = {"+35% Damage", "+Volcanic Stomp", "+Meteor Maximum", "+1 New Active Ability"},
+            ["Cost"] = 15000
+        },
+        ["Tengon (Flashiness)"] = {
+            ["Requirements"] = {
+                ["Nichirin Cleavers"] = {["Amount"] = 1},
+                ["Blue Essence Stone"] = {["Amount"] = 13},
+                ["Green Essence Stone"] = {["Amount"] = 40},
+                ["Yellow Essence Stone"] = {["Amount"] = 15},
+                ["Purple Essence Stone"] = {["Amount"] = 15},
+                ["Rainbow Essence Stone"] = {["Amount"] = 3}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Tengon (Flashiness)", ["Identifier"] = "52:Evolved"},
+            ["EvolveBenefits"] = {
+                "+53% Damage",
+                "+String Performance",
+                "+Constant Resounding Slashes",
+                "+1 New Passive"
+            },
+            ["Cost"] = 15000
+        },
+        ["Noruto (Six Tails)"] = {
+            ["Requirements"] = {
+                ["Blue Chakra"] = {["Amount"] = 6},
+                ["Blue Essence Stone"] = {["Amount"] = 12},
+                ["Green Essence Stone"] = {["Amount"] = 30},
+                ["Red Chakra"] = {["Amount"] = 6},
+                ["Rainbow Essence Stone"] = {["Amount"] = 2},
+                ["Pink Essence Stone"] = {["Amount"] = 11}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Noruto (Six Tails)", ["Identifier"] = "20:Evolved"},
+            ["EvolveBenefits"] = {"+40% Damage", "+Tailed Beast Blast", "+Tailed Beast Bomb", "+1 New Passive"},
+            ["Cost"] = 15000
+        },
+        ["Sukono"] = {
+            ["Requirements"] = {
+                ["Pink Essence Stone"] = {["Amount"] = 12},
+                ["Cursed Finger"] = {["Amount"] = 20},
+                ["Red Essence Stone"] = {["Amount"] = 15},
+                ["Green Essence Stone"] = {["Amount"] = 40},
+                ["Purple Essence Stone"] = {["Amount"] = 15},
+                ["Rainbow Essence Stone"] = {["Amount"] = 3}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Sukono", ["Identifier"] = "66:Evolved"},
+            ["EvolveBenefits"] = {
+                "+40% Damage",
+                "+Dismantle Stomp",
+                "+Cleaving Dash",
+                "+Flame Release",
+                "+1 New Active Ability",
+                "+2 New Passive"
+            },
+            ["Cost"] = 30000
+        },
+        ["Chaso (Blood Curse)"] = {
+            ["Requirements"] = {
+                ["Pink Essence Stone"] = {["Amount"] = 9},
+                ["Blue Essence Stone"] = {["Amount"] = 12},
+                ["Red Essence Stone"] = {["Amount"] = 10},
+                ["Green Essence Stone"] = {["Amount"] = 40},
+                ["Rainbow Essence Stone"] = {["Amount"] = 3},
+                ["Purple Essence Stone"] = {["Amount"] = 10},
+                ["Hardened Blood"] = {["Amount"] = 12}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Chaso (Blood Curse)", ["Identifier"] = "60:Evolved"},
+            ["EvolveBenefits"] = {"+15% Damage", "+Coagulated Spikes", "+Blood Storm", "+1 New Passive"},
+            ["Cost"] = 15000
+        },
+        ["Julias (Eisplosion)"] = {
+            ["Requirements"] = {
+                ["Yellow Essence Stone"] = {["Amount"] = 4},
+                ["Pink Essence Stone"] = {["Amount"] = 12},
+                ["Blue Essence Stone"] = {["Amount"] = 12},
+                ["Red Essence Stone"] = {["Amount"] = 4},
+                ["Green Essence Stone"] = {["Amount"] = 35},
+                ["Ice Wand"] = {["Amount"] = 1},
+                ["Purple Essence Stone"] = {["Amount"] = 10},
+                ["Rainbow Essence Stone"] = {["Amount"] = 3}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Julias (Eisplosion)", ["Identifier"] = "68:Evolved"},
+            ["EvolveBenefits"] = {"+10% Damage", "+Lunia Caulis", "+1 New Passive"},
+            ["Cost"] = 15000
+        },
+        ["Zion (Burdelyon)"] = {
+            ["Requirements"] = {
+                ["Rainbow Essence Stone"] = {["Amount"] = 3},
+                ["Fire Wand"] = {["Amount"] = 1},
+                ["Blue Essence Stone"] = {["Amount"] = 4},
+                ["Red Essence Stone"] = {["Amount"] = 12},
+                ["Green Essence Stone"] = {["Amount"] = 35},
+                ["Yellow Essence Stone"] = {["Amount"] = 12},
+                ["Purple Essence Stone"] = {["Amount"] = 10},
+                ["Pink Essence Stone"] = {["Amount"] = 4}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Zion (Burdelyon)", ["Identifier"] = 82},
+            ["EvolveBenefits"] = {"+30% Damage", "+Dragons Nowl", "+1 New Passive"},
+            ["Cost"] = 15000
+        },
+        ["Song Jinwu (Monarch)"] = {
+            ["Requirements"] = {
+                ["Pink Essence Stone"] = {["Amount"] = 12},
+                ["Green Essence Stone"] = {["Amount"] = 30},
+                ["Shadow Traces"] = {["Amount"] = 12},
+                ["Rainbow Essence Stone"] = {["Amount"] = 1},
+                ["Yellow Essence Stone"] = {["Amount"] = 10}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Song Jinwu (Monarch)", ["Identifier"] = "21:Evolved"},
+            ["EvolveBenefits"] = {"+35% Damage", "+2 New Passives", "+1 Active Ability"},
+            ["Cost"] = 15000
+        },
+        ["Sosuke (Storm)"] = {
+            ["Requirements"] = {
+                ["Snake Scale"] = {["Amount"] = 12},
+                ["Pink Essence Stone"] = {["Amount"] = 12},
+                ["Red Essence Stone"] = {["Amount"] = 12},
+                ["Green Essence Stone"] = {["Amount"] = 30},
+                ["Rainbow Essence Stone"] = {["Amount"] = 1}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Sosuke (Storm)", ["Identifier"] = "17:Evolved"},
+            ["EvolveBenefits"] = {"+25% Damage", "+Chidori Strike", "+1 New Passive"},
+            ["Cost"] = 15000
+        },
+        ["Igros (Elite Knight)"] = {
+            ["Requirements"] = {
+                ["Pink Essence Stone"] = {["Amount"] = 10},
+                ["Blood-Red Armor"] = {["Amount"] = 12},
+                ["Red Essence Stone"] = {["Amount"] = 10},
+                ["Green Essence Stone"] = {["Amount"] = 32},
+                ["Rainbow Essence Stone"] = {["Amount"] = 1},
+                ["Blue Essence Stone"] = {["Amount"] = 10}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Igros (Elite Knight)", ["Identifier"] = "23:Evolved"},
+            ["EvolveBenefits"] = {"+40% Damage", "+Shadowfall Slam", "+Shadow's Embrace", "+2 New Passives"},
+            ["Cost"] = 30000
+        },
+        ["Gujo (Infinity)"] = {
+            ["Requirements"] = {
+                ["Blue Essence Stone"] = {["Amount"] = 30},
+                ["Red Essence Stone"] = {["Amount"] = 30},
+                ["Green Essence Stone"] = {["Amount"] = 40},
+                ["Six Eyes"] = {["Amount"] = 2},
+                ["Rainbow Essence Stone"] = {["Amount"] = 4}
+            },
+            ["EvolvedUnit"] = {["Name"] = "Gujo (Infinity)", ["Identifier"] = "62:Evolved"},
+            ["EvolveBenefits"] = {"+30% Damage", "+Purple", "+Max-Purple", "+1 New Active Ability", "+1 New Passive"},
+            ["Cost"] = 15000
+        }
+    }
+}
