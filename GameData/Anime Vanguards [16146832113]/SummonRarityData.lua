895358b7ea7e4f3b165b8862e3b1206f2846a0ba
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-03 03:12:34 +1100

    Create SummonRarityData.lua

---
+++ GameData/Anime Vanguards [16146832113]/SummonRarityData.lua
@@ -0,0 +1,7 @@
+data = {
+    ["Legendary"] = {["Chance"] = 4},
+    ["Epic"] = {["Chance"] = 20},
+    ["Secret"] = {["Chance"] = 0.004},
+    ["Mythic"] = {["Chance"] = 0.5},
+    ["Rare"] = {["Chance"] = 75.496}
+}
