f9deaf9581b890b8bb1912f12c7b80d60c552feb
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-06 02:22:55 +1100

    Create Achievements.lua

---
+++ GameData/Guts & Blackpowder [12334109280]/Achievements.lua
@@ -0,0 +1,291 @@
+data = {
+    ["FiftyKGBomb"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Kill 10+ zombies while holding a fused Grenade.",
+            ["Image"] = 17110800848,
+            ["Title"] = "To Hell We Go!",
+            ["Category"] = "Hidden"
+        },
+        ["Hidden"] = true,
+        ["Identity"] = "50kgBomb"
+    },
+    ["GiveEmSteel"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Kill 10 zombies with the Bayonet charge only.",
+            ["Image"] = 16719680015,
+            ["Title"] = "GIVE 'EM STEEL",
+            ["Category"] = "General"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 2150105222
+    },
+    ["LeRevenant"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Complete the Road to Sleepy Hollow map and defeat the Headless Horseman in ONE LIFE.",
+            ["Image"] = 138149958268459,
+            ["Title"] = "Le Revenant",
+            ["Category"] = "Hidden"
+        },
+        ["Hidden"] = true,
+        ["Identity"] = "LeRevenant"
+    },
+    ["RuleBritannia"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Die to the crew of the British Brig-Sloop in Copenhagen.",
+            ["Image"] = 18206895438,
+            ["Title"] = "Rule Britannia!",
+            ["Category"] = "Objective Mode"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 3440369202632995
+    },
+    ["Halloween"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = [[Defeat the headless horseman at "Sleepy Hallow".]],
+            ["Image"] = 16719754827,
+            ["Title"] = "For Whom The Bell Tolls",
+            ["Category"] = "Objective Mode"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 2153260837
+    },
+    ["Berezina"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Successfully build/defend the Berezina bridge.",
+            ["Image"] = 17124159499,
+            ["Title"] = "Beresinalied",
+            ["Category"] = "Objective Mode"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 4274021500128099
+    },
+    ["UselessNoise"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = [["You'll hurt yourself..."]],
+            ["Image"] = 18206374661,
+            ["Title"] = "Stop That Useless Noise!",
+            ["Category"] = "General"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 1019642655158068
+    },
+    ["Untouchable"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Complete a campaign map without receiving damage once, while healing your teammates five times, using the Crucifix five times, or gaining 200 kills.",
+            ["Image"] = 17056465815,
+            ["Title"] = "The Untouchable",
+            ["Category"] = "Hidden"
+        },
+        ["Hidden"] = true,
+        ["Identity"] = "Untouchable"
+    },
+    ["UndeadEmpire"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Die while traveling through Catacombes de Paris.",
+            ["Image"] = 16719705558,
+            ["Title"] = "Recruited by The Empire of The Dead",
+            ["Category"] = "Objective Mode"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 2145539312
+    },
+    ["ExplosiveRetreat"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Successfully escape Leipzig IN ONE LIFE.",
+            ["Image"] = 16719733230,
+            ["Title"] = "Explosive Retreat",
+            ["Category"] = "Objective Mode"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 2141000564
+    },
+    ["Copenhagen"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Successfully escape Copenhagen IN ONE LIFE.",
+            ["Image"] = 18242099185,
+            ["Title"] = "The Most Terrible Night",
+            ["Category"] = "Objective Mode"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 4366647684892586
+    },
+    ["EndlessSolo"] = {
+        ["Max"] = 35,
+        ["Properties"] = {
+            ["Description"] = "Complete 20 waves solo.",
+            ["Image"] = 17013852588,
+            ["Title"] = "Alone, At the End of the World",
+            ["Category"] = "Hidden"
+        },
+        ["Hidden"] = true,
+        ["Identity"] = "EndlessSolo"
+    },
+    ["Grace"] = {
+        ["Max"] = 4,
+        ["CheckFunc"] = "function: 0x24fb88e1f301c7f7",
+        ["Properties"] = {
+            ["Description"] = "Save 150 people from a grab, kill 75 runners on a team, unlock Vos Enterrement, and heal over 50 players.",
+            ["Image"] = 16719815684,
+            ["Title"] = "Soldier's Grace",
+            ["Category"] = "Unlocks"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 2152744338
+    },
+    ["GloryToTheSurvivors"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Survive a major campaign map with half of the server on their first life.",
+            ["Image"] = 16719815785,
+            ["Title"] = "Glory to the Survivors",
+            ["Category"] = "General"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 2152744362
+    },
+    ["Catacombs"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Successfully escape Catacombes de Paris IN ONE LIFE.",
+            ["Image"] = 16719733435,
+            ["Title"] = "Deliverance from the Catacombs",
+            ["Category"] = "Objective Mode"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 2145540410
+    },
+    ["SanSacrifice"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Lower the platform for your team at the end of San Sebastián",
+            ["Image"] = 16719719953,
+            ["Title"] = "Vos Enterrement",
+            ["Category"] = "Objective Mode"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 2149861668
+    },
+    ["ColdFront"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Successfully escape Vardohus Fortress IN ONE LIFE.",
+            ["Image"] = 16719733135,
+            ["Title"] = "Cold Front",
+            ["Category"] = "Objective Mode"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 2135317177
+    },
+    ["NoFrederick"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Hit (and die) to a bomber during a Bayonet charge.",
+            ["Image"] = 16719689847,
+            ["Title"] = "NO, FREDERICK!",
+            ["Category"] = "General"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 2150105286
+    },
+    ["Marston"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Complete a campaign map as John Marston. Requires admin to unlock.",
+            ["Image"] = 17004135238,
+            ["Title"] = "Evening Redness in the West",
+            ["Category"] = "Hidden"
+        },
+        ["Hidden"] = true,
+        ["Identity"] = "Marston"
+    },
+    ["OldGuard"] = {
+        ["Max"] = 8,
+        ["CheckFunc"] = "function: 0xeacfabdf8a334bb7",
+        ["Properties"] = {
+            ["Description"] = "Complete 35 rounds of Endless without dying once. Obtain Old Yeller, Cold Front, Explosive Retreat, and Deliverance from the Catacombs. Save 51 people, complete 30 campaigns, and have a KDR at or above 10.",
+            ["Image"] = 16719815891,
+            ["Title"] = "Soldats de ma Vieille Garde",
+            ["Category"] = "Unlocks"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 2149492872
+    },
+    ["OldYeller"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Put a survivor who's broken their legs out of their misery.",
+            ["Image"] = 16719669413,
+            ["Title"] = "Old Yeller",
+            ["Category"] = "General"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 2135317030
+    },
+    ["ForlornHope"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Successfully escape San Sebastián IN ONE LIFE.",
+            ["Image"] = 16719733638,
+            ["Title"] = "Forlorn Hope",
+            ["Category"] = "Objective Mode"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 2149670646
+    },
+    ["Grenadier"] = {
+        ["Max"] = 3,
+        ["CheckFunc"] = "function: 0xcd952e788fe88217",
+        ["Properties"] = {
+            ["Description"] = "10 hours of playtime, 2000 kills, 20 campaign wins.",
+            ["Image"] = 17261032296,
+            ["Title"] = "Dur à cuire",
+            ["Category"] = "Unlocks"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 3426925391633020
+    },
+    ["IfIHadABrain"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Be crushed by the falling drawbridge in Kaub.",
+            ["Image"] = 18206417234,
+            ["Title"] = "If I Had a Brain",
+            ["Category"] = "Objective Mode"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 3353623664586041
+    },
+    ["SlightMiscalc"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Get blown up by the bridge explosion in Leipzig.",
+            ["Image"] = 16719700950,
+            ["Title"] = "Slight Miscalculation",
+            ["Category"] = "Objective Mode"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 2141168889
+    },
+    ["Confederation"] = {
+        ["Max"] = 1,
+        ["Properties"] = {
+            ["Description"] = "Successfully survive Kaub IN ONE LIFE.",
+            ["Image"] = 16719733768,
+            ["Title"] = "Confederation of the Undead",
+            ["Category"] = "Objective Mode"
+        },
+        ["Hidden"] = false,
+        ["Identity"] = 4067857183776782
+    }
+}
