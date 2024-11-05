141d378922d52bc20e462f206c921ebf3835d3ca
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-06 02:21:56 +1100

    Create GameQuotes.lua

---
+++ GameData/Guts & Blackpowder [12334109280]/GameQuotes.lua
@@ -0,0 +1,13 @@
+data = {
+    "Has God punished us enough?",
+    "When there's no more room in hell...",
+    "The dead remain restless.",
+    "We are prepared for the inevitable.",
+    "Repent.",
+    "Repentez la fin est, très proche",
+    "Vous gagnerez les c'ès de l'empire des morts.",
+    "They are corrupt, their deeds are vile.",
+    "I cry by day, but you do not answer...",
+    "Do not be far from me, for trouble is near...",
+    "The Lord is my light and my salvation; whom shall I fear?"
+}
