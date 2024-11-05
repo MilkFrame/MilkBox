f0eefd6d38849a4ea5aa4723206ca5b8ef8cb268
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-06 02:22:10 +1100

    Create Classes.lua

---
+++ GameData/Guts & Blackpowder [12334109280]/Classes.lua
@@ -0,0 +1,125 @@
+data = {
+    ["Lancer_NoLace"] = {
+        ["Desc"] = {
+            "+ Forgot his Lance at home (Purchase the Lance at the shop!)",
+            "+ Long ranged class that is very useful for precise attacks.",
+            "+ Well-rounded, has no significant downsides.",
+            "+ Default weapon is equipped with the Bayonet, which can be used for melee attacks.",
+            "+ Bayonet synergizes with Charge, as it can deal instant kills."
+        },
+        ["Icon"] = 14857057984,
+        ["DisplayName"] = "Lancer (Infantry)"
+    },
+    ["Lancer"] = {
+        ["Desc"] = {
+            "+ Trades the power of the firearm for a long-ranged melee.",
+            "+ Great for controlling chokepoints, but very weak against close quarter encounters.",
+            "+ Lance synergizes with Charge, as it can deal instant kills."
+        },
+        ["Icon"] = 14857057984,
+        ["DisplayName"] = "Lancer"
+    },
+    ["Lancer2"] = {
+        ["Desc"] = {
+            "+ Trades the power of the firearm for a long-ranged melee.",
+            "+ Great for controlling chokepoints, but very weak against close quarter encounters.",
+            "+ Lance synergizes with Charge, as it can deal instant kills."
+        },
+        ["Icon"] = 14857057984,
+        ["DisplayName"] = "Pikeman"
+    },
+    ["Surgeon"] = {
+        ["Desc"] = {
+            "+ Supportive class that focuses on healing teammates.",
+            "+ Can place down a crate of bandages for others to use.",
+            "+ Healing slowly boosts health, but does not cure infection.",
+            "+ Healing takes a bit to apply."
+        },
+        ["Icon"] = 14857057171,
+        ["DisplayName"] = "Surgeon"
+    },
+    ["Seaman"] = {
+        ["Desc"] = {
+            "+ Mid-ranged class that specializes in crowd control.",
+            "+ The Blunderbuss is very useful for keeping Shamblers in check.",
+            "+ Larger spread helps with attacking more enemies."
+        },
+        ["Icon"] = 14857054899,
+        ["DisplayName"] = "Seaman"
+    },
+    ["Sapper"] = {
+        ["Desc"] = {
+            "+ Defensive class that has the special ability to build foritifcations.",
+            "+ Fortifications provide protection and blockage.",
+            "+ Alternative ability to Axe allows you to brace for attacks, and push zombies away.",
+            "+ Axe can also inflict insane headshot damage (5x base).",
+            "+ Infection will result in the Sapper Zombie spawning in... be careful!"
+        },
+        ["Icon"] = 14857054059,
+        ["DisplayName"] = "Sapper"
+    },
+    ["Engineer"] = {
+        ["Desc"] = {
+            "+ Trades speed for extra defensive capabilities.",
+            "+ Defensive class that has the special ability to build foritifcations.",
+            "+ Fortifications provide protection and blockage.",
+            "+ Alternative ability to Axe allows you to brace for attacks, and push zombies away.",
+            "+ Axe can also inflict insane headshot damage (5x base).",
+            "+ Infection will result in the Sapper Zombie spawning in... be careful!"
+        },
+        ["IgnoreAtShop"] = true,
+        ["Icon"] = 14857054059,
+        ["DisplayName"] = "Engineer"
+    },
+    ["Chaplain"] = {
+        ["Desc"] = {
+            "+ Supportive class that focuses on crowd control and blessings.",
+            "+ Blessings slowly cures infection, but it does not heal.",
+            "+ Crucifix is useful for pushing stunning all enemies excluding Bombers.",
+            "+ Very weak defensive capabilities."
+        },
+        ["Icon"] = 14857056426,
+        ["DisplayName"] = "Chaplain"
+    },
+    ["LineInfantry"] = {
+        ["Desc"] = {
+            "+ Long ranged class that is very useful for precise attacks.",
+            "+ Well-rounded, has no significant downsides.",
+            "+ Default weapon is equipped with the Bayonet, which can be used for melee attacks.",
+            "+ Bayonet synergizes with Charge, as it can deal instant kills."
+        },
+        ["Icon"] = 14857057984,
+        ["DisplayName"] = "Infantry"
+    },
+    ["Piper"] = {
+        ["Desc"] = {
+            "+ Access to the Bagpipes.",
+            "+ Supportive class that focuses on assisting teammates.",
+            "+ Instruments provide various buffs to nearby teammates.",
+            "+ Be careful while playing music around a Runner...!"
+        },
+        ["IgnoreAtShop"] = true,
+        ["Icon"] = 14857059581,
+        ["DisplayName"] = "Piper"
+    },
+    ["Musician"] = {
+        ["Desc"] = {
+            "+ Supportive class that focuses on assisting teammates.",
+            "+ Instruments provide various buffs to nearby teammates.",
+            "+ Be careful while playing music around a Runner...!"
+        },
+        ["Icon"] = 14857059581,
+        ["DisplayName"] = "Musician"
+    },
+    ["Officer"] = {
+        ["Desc"] = {
+            "+ Short-ranged class, melee is your real primary.",
+            "+ Fastest reloading gun in the game (Pistol).",
+            [[+ Attacks builds "Charge" meter. Which boosts the swing rate and running speed of nearby teammates.]],
+            "+ Extra actions (including saving teammates from grabs) builds up the meter faster.",
+            "+ Bayonet synergizes with Charge, as it can deal instant kills."
+        },
+        ["Icon"] = 14885478143,
+        ["DisplayName"] = "Officer"
+    }
+}
