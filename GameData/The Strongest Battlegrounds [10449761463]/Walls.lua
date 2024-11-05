bc41f708be00e5863d91962241cae44bd6660d81
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-06 02:17:59 +1100

    Create Walls.lua

---
+++ GameData/The Strongest Battlegrounds [10449761463]/Walls.lua
@@ -0,0 +1,212 @@
+data = {
+    ["KJ Wall"] = {
+        ["victimAnimation"] = 18447915110,
+        ["sounds"] = {
+            ["char"] = {{["SoundId"] = "rbxassetid://18443048724", ["Volume"] = 10}},
+            ["hit"] = {{["SoundId"] = "rbxassetid://18443049348", ["Volume"] = 10}}
+        },
+        ["markers"] = {
+            ["hit2"] = {["normal"] = true, ["damage"] = 6},
+            ["hit3"] = {
+                ["damage"] = 10,
+                ["final"] = {
+                    ["offset"] = -6,
+                    ["force"] = {["pushback"] = 0, ["sideways"] = -120, ["up"] = Vector3.new(0, 25, 0)}
+                }
+            },
+            ["hit1"] = {["normal"] = true, ["materialDependent"] = 2, ["damage"] = 6}
+        },
+        ["userAnimation"] = 18447913645
+    },
+    ["Blade Wall"] = {
+        ["victimAnimation"] = 15997027501,
+        ["sounds"] = {
+            ["char"] = {{["SoundId"] = "rbxassetid://15997645271", ["Volume"] = 2}},
+            ["hit"] = {{["SoundId"] = "rbxassetid://15997641783", ["Volume"] = 3}}
+        },
+        ["grabWeapon"] = true,
+        ["markers"] = {
+            ["hit2"] = {["normal"] = true, ["materialDependent"] = 1.5, ["damage"] = 1},
+            ["hit4"] = {
+                ["slash"] = true,
+                ["damage"] = 7,
+                ["final"] = {
+                    ["offset"] = -6,
+                    ["force"] = {["pushback"] = 0, ["sideways"] = 52.5, ["up"] = Vector3.new(0, 6, 0)}
+                }
+            },
+            ["hit3"] = {["normal"] = true, ["materialDependent"] = 1, ["damage"] = 2},
+            ["hit1"] = {["normal"] = true, ["slash"] = true, ["damage"] = 2}
+        },
+        ["userAnimation"] = 15997042291
+    },
+    ["Purple Wall"] = {
+        ["victimAnimation"] = 18181159825,
+        ["sounds"] = {
+            ["char"] = {{["SoundId"] = "rbxassetid://18181173614", ["Volume"] = 2}},
+            ["hit"] = {{["SoundId"] = "rbxassetid://18181173904", ["Volume"] = 4}}
+        },
+        ["markers"] = {
+            ["hit9"] = {["normal"] = true, ["damage"] = 3},
+            ["hit10"] = {
+                ["damage"] = 3,
+                ["final"] = {
+                    ["offset"] = -3,
+                    ["force"] = {["pushback"] = 0, ["sideways"] = 65, ["up"] = Vector3.new(0, 5, 0)}
+                }
+            },
+            ["hit2"] = {["normal"] = true, ["materialDependent"] = 1, ["damage"] = 1.5},
+            ["hit4"] = {["normal"] = true, ["damage"] = 0.5},
+            ["hit3"] = {["normal"] = true, ["damage"] = 0.5},
+            ["hit1"] = {["normal"] = true, ["materialDependent"] = 1.5, ["damage"] = 1.5},
+            ["hit5"] = {["normal"] = true, ["damage"] = 0.5},
+            ["hit8"] = {["normal"] = true, ["damage"] = 0.5},
+            ["hit7"] = {["normal"] = true, ["damage"] = 0.5},
+            ["hit6"] = {["normal"] = true, ["damage"] = 0.5}
+        },
+        ["userAnimation"] = 18181589384
+    },
+    ["Cyborg Wall"] = {
+        ["victimAnimation"] = 15943926967,
+        ["sounds"] = {
+            ["char"] = {{["SoundId"] = "rbxassetid://15943937672", ["Volume"] = 1.5}},
+            ["hit"] = {{["SoundId"] = "rbxassetid://15943937560", ["Volume"] = 2}}
+        },
+        ["markers"] = {
+            ["hit2"] = {["normal"] = true, ["materialDependent"] = 2, ["damage"] = 4},
+            ["hit4"] = {
+                ["damage"] = 6,
+                ["final"] = {
+                    ["offset"] = 0.5,
+                    ["force"] = {["pushback"] = 0, ["sideways"] = -52.5, ["up"] = Vector3.new(0, 6, 0)}
+                }
+            },
+            ["hit3"] = {["normal"] = true, ["materialDependent"] = 1, ["damage"] = 1},
+            ["hit1"] = {["normal"] = true, ["materialDependent"] = 1, ["damage"] = 1}
+        },
+        ["userAnimation"] = 15943915877
+    },
+    ["Bald Wall"] = {
+        ["victimAnimation"] = 15955422503,
+        ["sounds"] = {
+            ["char"] = {
+                {["SoundId"] = "rbxassetid://15955357446", ["TimePosition"] = 0.167, ["Volume"] = 2},
+                {["SoundId"] = "rbxassetid://15955559676", ["TimePosition"] = 0.2, ["Volume"] = 2}
+            },
+            ["wall"] = {{["SoundId"] = "rbxassetid://15955357060", ["TimePosition"] = 0.167, ["Volume"] = 2}}
+        },
+        ["markers"] = {
+            ["hit2"] = {
+                ["damage"] = 6,
+                ["materialDependent"] = 3.5,
+                ["final"] = {
+                    ["offset"] = -6,
+                    ["force"] = {["pushback"] = -45, ["sideways"] = 0, ["up"] = Vector3.new(0, 25, 0)}
+                }
+            },
+            ["hit1"] = {["normal"] = true, ["materialDependent"] = 3.5, ["damage"] = 6}
+        },
+        ["userAnimation"] = 15955393872
+    },
+    ["Hunter Wall"] = {
+        ["victimAnimation"] = 16310419285,
+        ["sounds"] = {
+            ["char"] = {{["SoundId"] = "rbxassetid://16310489770", ["Volume"] = 3}},
+            ["hit"] = {{["SoundId"] = "rbxassetid://16310489560", ["Volume"] = 3}}
+        },
+        ["markers"] = {
+            ["hit2"] = {["normal"] = true, ["damage"] = 1, ["materialDependent"] = 1, ["waterparticle"] = false},
+            ["hit4"] = {["normal"] = true, ["damage"] = 2, ["materialDependent"] = 1, ["waterparticle"] = false},
+            ["hit3"] = {["normal"] = true, ["damage"] = 1, ["materialDependent"] = 1, ["waterparticle"] = false},
+            ["hit1"] = {["normal"] = true, ["damage"] = 1, ["materialDependent"] = 1, ["waterparticle"] = false},
+            ["hit5"] = {["normal"] = true, ["damage"] = 4, ["materialDependent"] = 1, ["waterparticle"] = true},
+            ["hit6"] = {
+                ["damage"] = 3,
+                ["final"] = {
+                    ["offset"] = -6,
+                    ["force"] = {["pushback"] = -45, ["sideways"] = 0, ["up"] = Vector3.new(0, 25, 0)}
+                }
+            }
+        },
+        ["userAnimation"] = 16310343179
+    },
+    ["Sorcerer Wall"] = {
+        ["victimAnimation"] = 18903644685,
+        ["sounds"] = {
+            ["char"] = {{["SoundId"] = "rbxassetid://18846960753", ["Volume"] = 10}},
+            ["wall"] = {},
+            ["hit"] = {{["SoundId"] = "rbxassetid://18846960425", ["Volume"] = 10}}
+        },
+        ["markers"] = {
+            ["hit2"] = {["normal"] = true, ["materialDependent"] = 0, ["damage"] = 12.5},
+            ["hit3"] = {
+                ["damage"] = 30,
+                ["materialDependent"] = 0,
+                ["final"] = {["offset"] = -6, ["force"] = {["sideways"] = 0, ["pushback"] = 0}}
+            },
+            ["hit1"] = {["normal"] = true, ["materialDependent"] = 3.5, ["damage"] = 12.5}
+        },
+        ["userAnimation"] = 18903642853
+    },
+    ["Ninja Wall"] = {
+        ["victimAnimation"] = 16023171958,
+        ["sounds"] = {
+            ["char"] = {{["SoundId"] = "rbxassetid://16023181836", ["Volume"] = 2}},
+            ["hit"] = {{["SoundId"] = "rbxassetid://16023181747", ["Volume"] = 3}}
+        },
+        ["grabWeapon"] = true,
+        ["markers"] = {
+            ["hit2"] = {["normal"] = true, ["materialDependent"] = 1.5, ["damage"] = 4},
+            ["hit4"] = {
+                ["damage"] = 2,
+                ["final"] = {
+                    ["offset"] = -5.5,
+                    ["force"] = {["pushback"] = 0, ["sideways"] = -52.5, ["up"] = Vector3.new(0, 6, 0)}
+                }
+            },
+            ["hit3"] = {["normal"] = true, ["materialDependent"] = 2, ["explosion"] = true, ["damage"] = 4},
+            ["hit1"] = {["normal"] = true, ["slash"] = true, ["damage"] = 1}
+        },
+        ["userAnimation"] = 16023456135
+    },
+    ["Esper Wall"] = {
+        ["victimAnimation"] = 16310732064,
+        ["sounds"] = {
+            ["char"] = {{["SoundId"] = "rbxassetid://16310741248", ["Volume"] = 3}},
+            ["hit"] = {{["SoundId"] = "rbxassetid://16310741455", ["Volume"] = 3}}
+        },
+        ["markers"] = {
+            ["hit2"] = {
+                ["damage"] = 9,
+                ["materialDependent"] = 2,
+                ["final"] = {
+                    ["offset"] = -6,
+                    ["force"] = {["pushback"] = -45, ["sideways"] = 0, ["up"] = Vector3.new(0, 25, 0)}
+                }
+            },
+            ["hit1"] = {["normal"] = true, ["materialDependent"] = 2, ["damage"] = 3}
+        },
+        ["userAnimation"] = 16311141574
+    },
+    ["Batter Wall"] = {
+        ["victimAnimation"] = 15944354277,
+        ["sounds"] = {
+            ["char"] = {{["SoundId"] = "rbxassetid://15944339740", ["Volume"] = 2}},
+            ["hit"] = {{["SoundId"] = "rbxassetid://15944339899", ["Volume"] = 4}}
+        },
+        ["grabWeapon"] = true,
+        ["markers"] = {
+            ["hit2"] = {["normal"] = true, ["damage"] = 1},
+            ["hit4"] = {
+                ["damage"] = 6,
+                ["final"] = {
+                    ["offset"] = -3.5,
+                    ["force"] = {["pushback"] = 0, ["sideways"] = -60, ["up"] = Vector3.new(0, 6, 0)}
+                }
+            },
+            ["hit3"] = {["normal"] = true, ["materialDependent"] = 3.5, ["damage"] = 3},
+            ["hit1"] = {["normal"] = true, ["materialDependent"] = 3.5, ["damage"] = 2}
+        },
+        ["userAnimation"] = 15944317351
+    }
+}
