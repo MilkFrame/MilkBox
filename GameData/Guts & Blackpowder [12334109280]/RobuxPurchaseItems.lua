244a97cfafb96c4a3c41bb7296a4b5ab3e10fff4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-06 02:23:24 +1100

    Create RobuxPurchaseItems.lua

---
+++ GameData/Guts & Blackpowder [12334109280]/RobuxPurchaseItems.lua
@@ -0,0 +1,248 @@
+data = {
+    ["Cosmetics"] = {
+        {
+            ["Name"] = "Cosmetic Pack #1",
+            ["Id"] = 138110678,
+            ["IsFirst"] = true,
+            ["Description"] = "Contains: Mutton Chops, Walrus, Eyepatch, Klaus, Monocle, & Glasses.",
+            ["ItemsToDisplay"] = {"Mutton Chops", "Glasses"}
+        },
+        {
+            ["Id"] = 139899041,
+            ["Description"] = "Contains: Handlebar, Frederick, Full Beard, Bandage, Wheat, and a Scarf.",
+            ["Name"] = "Cosmetic Pack #2",
+            ["ItemsToDisplay"] = {"Full Beard", "Scarf"}
+        },
+        {
+            ["Id"] = 175328011,
+            ["Description"] = "Contains: Nasty Bruise, Carrot, Bird Eyepatch, Tidy Beard, Goatee, and La Francais.",
+            ["Name"] = "Cosmetic Pack #3",
+            ["ItemsToDisplay"] = {"La Francais", "Carrot"}
+        },
+        {
+            ["Id"] = 175329006,
+            ["Description"] = "Contains: Ney, Ponytail, Neckbeard, Piquet, Sideburns, and a Dandelion.",
+            ["Name"] = "Cosmetic Pack #4",
+            ["ItemsToDisplay"] = {"Sideburns", "Dandelion"}
+        },
+        {
+            ["Id"] = 218802785,
+            ["Description"] = "Contains: Long Beard, Imperial, Colonel, Chin Puff, Otis, and Braids.",
+            ["Name"] = "Cosmetic Pack #5",
+            ["ItemsToDisplay"] = {"Long Beard", "Braids"}
+        },
+        {
+            ["Id"] = 272635522,
+            ["Description"] = "Contains: Messy, Long Hair, Quixote, Charmer, Chevron, and Pointed Chevron.",
+            ["Name"] = "Cosmetic Pack #6",
+            ["ItemsToDisplay"] = {"Messy", "Quixote"}
+        },
+        {
+            ["Id"] = 739404663,
+            ["Description"] = "Contains: Rough Cut, Full Head, Wool Wrappings, Cattleman, Uncut, and III.",
+            ["Name"] = "Cosmetic Pack #7",
+            ["ItemsToDisplay"] = {"Full Head", "III"}
+        },
+        {
+            ["Id"] = 775603635,
+            ["Description"] = "Contains: Richard, Chops n'tache, Bad Shave, Bandholz, Earrings, and Tinted Glasses.",
+            ["Name"] = "Cosmetic Pack #8",
+            ["ItemsToDisplay"] = {"Chops n'tache", "Tinted Glasses"}
+        },
+        {
+            ["Id"] = 942492047,
+            ["Description"] = "Contains: English Sideburns, Sharpeshooter, Curly Hair, Head Bandage, Parted Pencil and Pencil w/ Sideburns.",
+            ["Name"] = "Cosmetic Pack #9",
+            ["ItemsToDisplay"] = {"Sharpeshooter", "Head Bandage", "Parted Pencil"}
+        },
+        {
+            ["Id"] = 943701512,
+            ["Description"] = "Contains: Hussar Hair, Anchor, Tied Beard, Blood Splatter, Locket and Cracked Spectacles.",
+            ["Name"] = "Cosmetic Pack #10",
+            ["ItemsToDisplay"] = {"Hussar Hair", "Anchor", "Blood Splatter", "Locket", "Cracked Spectacles"}
+        }
+    },
+    ["Blemishes"] = {
+        {
+            ["Name"] = "Blemish Pack #1",
+            ["Id"] = 269878738,
+            ["IsFirst"] = true,
+            ["Description"] = "Contains: Wolf Scarred, Runner's Victim, Dirty, and Stitches.",
+            ["ItemsToDisplay"] = {"Wolf Scarred"}
+        },
+        {
+            ["Id"] = 675715871,
+            ["Description"] = "Contains: Baggy Eyes, Cheek Bruise, Half-Face Scar, and Young Man's Wrinkles.",
+            ["Name"] = "Blemish Pack #2",
+            ["ItemsToDisplay"] = {"Baggy Eyes"}
+        },
+        {
+            ["Id"] = 675879573,
+            ["Description"] = "Contains: Mouth Scar, Nastier Bruise, Old Man Wrinkles, and Wavy Freckles.",
+            ["Name"] = "Blemish Pack #3",
+            ["ItemsToDisplay"] = {"Old Man Wrinkles"}
+        }
+    },
+    ["Regiments"] = {
+        {
+            ["Name"] = "Regiment Pack #1",
+            ["Id"] = 177727924,
+            ["RegimentToDisplay"] = "42nd Regiment of Foot",
+            ["Description"] = "Unlocks the Regiments: 3e Regiment Etranger, Freikorps, and 42nd Regiment of Foot",
+            ["IsFirst"] = true
+        },
+        {
+            ["Id"] = 219362545,
+            ["RegimentToDisplay"] = "Marins de la Garde impériale",
+            ["Name"] = "Regiment Pack #2",
+            ["Description"] = "Unlocks the Regiments: Marins de la Garde impériale, Field Hunter Battalion, and Nr. 1 Vistula Legion."
+        },
+        {
+            ["Id"] = 219365289,
+            ["RegimentToDisplay"] = "Sankt-Peterburzhskoye Opolcheniye",
+            ["Name"] = "Regiment Pack #3",
+            ["Description"] = "Unlocks the Regiments: Saint Petersburg Militia, Landwehr Regiment of Silesia, and Brunswick Ducal Corps."
+        },
+        {
+            ["Id"] = 675374363,
+            ["RegimentToDisplay"] = "1er Régiment de Chevaux-Légèrs Lanciers de la Garde",
+            ["Name"] = "Regiment Pack #4",
+            ["Description"] = "Unlocks the Regiments: Kärnten Landwehrbataillon, 1e Chevau-Légers Lanciers de Garde, and King's German Legion Second Light Battalion."
+        },
+        {
+            ["Id"] = 780971964,
+            ["RegimentToDisplay"] = "Whinyates 2nd Rocket Troop",
+            ["Name"] = "Regiment Pack #5",
+            ["Description"] = "Unlocks the Regiments: Whinyates 2nd Rocket Troop, Kongens Livjæger Corps, and 1. Chevaulegers-Regiment."
+        },
+        {
+            ["Id"] = 843898710,
+            ["RegimentToDisplay"] = "2ème Régiment de Dragons",
+            ["Name"] = "Regiment Pack #6",
+            ["Description"] = "Unlocks the Regiments: 2e Régiment de Dragons, Les Canonniers Gardes-côtes and Danske Landeværn."
+        },
+        {
+            ["Id"] = 845544180,
+            ["RegimentToDisplay"] = "Batalhão de Caçadores N.º6",
+            ["Name"] = "Regiment Pack #7",
+            ["Description"] = "Unlocks the Regiments: 1. Pommersches Landwehr Kavallerie, 6 Caçadores Batalhão and Landesbewaffnung."
+        },
+        {
+            ["Id"] = 911009867,
+            ["RegimentToDisplay"] = "Husaren-Regiment Nr.1",
+            ["Name"] = "Regiment Pack #8",
+            ["Description"] = "Unlocks the Regiments: Husaren-Regiment Nr. 1, 95th Regiment of Foot, Brunswick Avantgarde-Bataillon."
+        }
+    },
+    ["UGC"] = {
+        {
+            ["Name"] = "1e Shako",
+            ["Id"] = 14826223767,
+            ["Price"] = 65,
+            ["Description"] = "The French regiment. Plate of shako is separate from the shako itself due to triangle limit.",
+            ["IsFirst"] = true
+        },
+        {
+            ["Id"] = 14826229158,
+            ["Price"] = 65,
+            ["Name"] = "1e Shako Plate",
+            ["Description"] = "The French regiment. Shako itself is separate from the plate itself due to triangle limit."
+        },
+        {
+            ["Id"] = 14826218325,
+            ["Price"] = 110,
+            ["Name"] = "1e Regiment Kit",
+            ["Description"] = "The backpack carried by the 1e French Regiment."
+        },
+        {
+            ["Id"] = 15900141312,
+            ["Price"] = 65,
+            ["Name"] = "23rd Infantry Shako",
+            ["Description"] = "The British regiment. Comes with the plate because of it being below the triangle limit."
+        },
+        {
+            ["Id"] = 14826261620,
+            ["Price"] = 110,
+            ["Name"] = "23rd Infantry Kit",
+            ["Description"] = "The backpack carried by the 23rd Infantry Regiment."
+        },
+        {
+            ["Id"] = 15922970119,
+            ["Price"] = 65,
+            ["Name"] = "Russian Shako",
+            ["Description"] = "The Russian regiment. Comes with everything because of it being below the triangle limit."
+        },
+        {
+            ["Id"] = 15922961417,
+            ["Price"] = 110,
+            ["Name"] = "Russian Kit",
+            ["Description"] = "The backpack carried by the Russian Line Infantry."
+        },
+        {
+            ["Id"] = 14864938006,
+            ["Price"] = 65,
+            ["Name"] = "Old Guard Bearskin",
+            ["Description"] = "The French Old Guard regiment. Plate of bearskin is separate from the bearskin itself due to triangle limit."
+        },
+        {
+            ["Id"] = 14960208670,
+            ["Price"] = 65,
+            ["Name"] = "Old Guard Plate",
+            ["Description"] = "The French Old Guard regiment. Bearskin itself is separate from the plate itself due to triangle limit."
+        },
+        {
+            ["Id"] = 14687158698,
+            ["Price"] = 110,
+            ["Name"] = "Old Guard Kit",
+            ["Description"] = "The backpack carried by the French Old Guard."
+        },
+        {
+            ["Id"] = 14826246244,
+            ["Price"] = 55,
+            ["Name"] = "Old Guard Pouches",
+            ["Description"] = "The pouches seen on the French Old Guard."
+        },
+        {
+            ["Id"] = 14687142818,
+            ["Price"] = 120,
+            ["Name"] = "Ol' Blunderbuss",
+            ["Description"] = "The old Blunderbuss model, as a UGC item."
+        },
+        {
+            ["Id"] = 14687182311,
+            ["Price"] = 120,
+            ["Name"] = "Sapper's Axe",
+            ["Description"] = "The Sapper's iconic Axe, as a UGC item."
+        },
+        {
+            ["Id"] = 14687169097,
+            ["Price"] = 120,
+            ["Name"] = "Sabre",
+            ["Description"] = "The default Sabre, as a UGC item."
+        },
+        {
+            ["Id"] = 14826270815,
+            ["Price"] = 55,
+            ["Name"] = "Hammer",
+            ["Description"] = "The Sapper's iconic Hammer, as a UGC item."
+        },
+        {
+            ["Id"] = 14687150020,
+            ["Price"] = 55,
+            ["Name"] = "Fife",
+            ["Description"] = "The most annoying instrument in the world, as a UGC item."
+        },
+        {
+            ["Id"] = 16895116617,
+            ["Price"] = 65,
+            ["Name"] = "American Marine Shako",
+            ["Description"] = "The American Marines shako, as seen in Sleepy Hollow."
+        },
+        {
+            ["Id"] = 16895131669,
+            ["Price"] = 120,
+            ["Name"] = "American Marine Kit",
+            ["Description"] = "The American Marines kit, as seen in Sleepy Hollow."
+        }
+    }
+}
