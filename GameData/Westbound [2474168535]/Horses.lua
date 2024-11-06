43bc3b4d2d576707da6f5429ba0c7127c676a5cf
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-07 01:39:51 +1100

    Create Horses.lua

---
+++ GameData/Westbound [2474168535]/Horses.lua
@@ -0,0 +1,462 @@
+data = {
+    ["Red Ghost"] = {
+        ["Acceleration"] = 1,
+        ["Description"] = "A red ghostly horse. Shares the stats of your most expensive owned horse.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(161, 118, 118),
+            ["Cannon"] = Color3.fromRGB(131, 87, 87),
+            ["Rein"] = Color3.fromRGB(74, 72, 72),
+            ["Hoof"] = Color3.fromRGB(161, 118, 118),
+            ["Tail"] = Color3.fromRGB(161, 118, 118),
+            ["Eyes"] = Color3.fromRGB(180, 210, 228),
+            ["Body"] = Color3.fromRGB(131, 87, 87)
+        },
+        ["Handling"] = 0.8,
+        ["HorseModel"] = "RedGhost",
+        ["TurnSpeed"] = 21,
+        ["Loyal"] = true,
+        ["Health"] = 60,
+        ["Speed"] = 55,
+        ["Stamina"] = 1
+    },
+    ["Reindeer Horse"] = {
+        ["Acceleration"] = 1,
+        ["Description"] = "A horse dressed up like a reindeer! Shares the stats of your most expensive owned horse.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(172, 138, 118),
+            ["Cannon"] = Color3.fromRGB(205, 189, 178),
+            ["Rein"] = Color3.fromRGB(121, 68, 66),
+            ["Hoof"] = Color3.fromRGB(27, 42, 53),
+            ["Tail"] = Color3.fromRGB(172, 138, 118),
+            ["Eyes"] = Color3.fromRGB(17, 17, 17),
+            ["Body"] = Color3.fromRGB(126, 88, 70)
+        },
+        ["Handling"] = 0.8,
+        ["HorseModel"] = "ReindeerHorse",
+        ["TurnSpeed"] = 21,
+        ["Loyal"] = true,
+        ["Health"] = 60,
+        ["Speed"] = 55,
+        ["Stamina"] = 1
+    },
+    ["Dark Horse"] = {
+        ["Acceleration"] = 1,
+        ["Description"] = "The ghostly horse rode by the Dark Watchers. Shares the stats of your most expensive owned horse.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(0, 0, 0),
+            ["Cannon"] = Color3.fromRGB(0, 0, 0),
+            ["Rein"] = Color3.fromRGB(44, 43, 43),
+            ["Hoof"] = Color3.fromRGB(17, 17, 17),
+            ["Tail"] = Color3.fromRGB(0, 0, 0),
+            ["Eyes"] = Color3.fromRGB(228, 228, 228),
+            ["Body"] = Color3.fromRGB(0, 0, 0)
+        },
+        ["Handling"] = 0.8,
+        ["HorseModel"] = "DarkHorse",
+        ["TurnSpeed"] = 21,
+        ["Loyal"] = true,
+        ["Health"] = 60,
+        ["Speed"] = 55,
+        ["Stamina"] = 1
+    },
+    ["Black Draft"] = {
+        ["Acceleration"] = 1,
+        ["Description"] = "Exceptionally tall and heavy yet calm, and gentle. Extremely muscular with strong bones. High HP and stamina. Loyal.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(29, 22, 18),
+            ["Cannon"] = Color3.fromRGB(255, 244, 216),
+            ["Rein"] = Color3.fromRGB(47, 38, 32),
+            ["Hoof"] = Color3.fromRGB(207, 190, 173),
+            ["Tail"] = Color3.fromRGB(29, 22, 18),
+            ["Eyes"] = Color3.fromRGB(17, 17, 17),
+            ["Body"] = Color3.fromRGB(27, 24, 21)
+        },
+        ["Handling"] = 1.5,
+        ["HorseModel"] = "Draft",
+        ["TurnSpeed"] = 23,
+        ["Loyal"] = true,
+        ["Health"] = 160,
+        ["Speed"] = 58,
+        ["Stamina"] = 1.5
+    },
+    ["Skeleton Horse Old"] = {
+        ["Acceleration"] = 1,
+        ["Description"] = "The reanimated skeleton of a horse. Shares the stats of your most expensive owned horse.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(202, 203, 209),
+            ["Cannon"] = Color3.fromRGB(202, 203, 209),
+            ["Rein"] = Color3.fromRGB(202, 203, 209),
+            ["Hoof"] = Color3.fromRGB(202, 203, 209),
+            ["Tail"] = Color3.fromRGB(202, 203, 209),
+            ["Eyes"] = Color3.fromRGB(202, 203, 209),
+            ["Body"] = Color3.fromRGB(202, 203, 209)
+        },
+        ["Handling"] = 0.8,
+        ["HorseModel"] = "SkeletonHorseOld",
+        ["TurnSpeed"] = 21,
+        ["Loyal"] = true,
+        ["Health"] = 60,
+        ["Speed"] = 55,
+        ["Stamina"] = 1
+    },
+    ["Brown Standardbred"] = {
+        ["Acceleration"] = 1,
+        ["Description"] = "Known as fast trotters, especially over long distances. Good all-purpose horse for riding and driving.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(27, 42, 53),
+            ["Cannon"] = Color3.fromRGB(205, 205, 205),
+            ["Rein"] = Color3.fromRGB(106, 57, 9),
+            ["Hoof"] = Color3.fromRGB(27, 42, 53),
+            ["Tail"] = Color3.fromRGB(27, 42, 53),
+            ["Eyes"] = Color3.fromRGB(17, 17, 17),
+            ["Body"] = Color3.fromRGB(124, 92, 70)
+        },
+        ["Handling"] = 0.8,
+        ["HorseModel"] = "NormalSmall",
+        ["TurnSpeed"] = 21,
+        ["Loyal"] = false,
+        ["Health"] = 60,
+        ["Speed"] = 55,
+        ["Stamina"] = 1
+    },
+    ["White Arabian"] = {
+        ["Acceleration"] = 3,
+        ["Description"] = "Beautiful, intelligent, high spirited, and willing to please. A great long distance horse. Loyal.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(231, 231, 236),
+            ["Cannon"] = Color3.fromRGB(231, 231, 236),
+            ["Rein"] = Color3.fromRGB(159, 161, 172),
+            ["Hoof"] = Color3.fromRGB(66, 66, 66),
+            ["Tail"] = Color3.fromRGB(231, 231, 236),
+            ["Eyes"] = Color3.fromRGB(17, 17, 17),
+            ["Body"] = Color3.fromRGB(231, 231, 236)
+        },
+        ["Handling"] = 3,
+        ["HorseModel"] = "Normal",
+        ["TurnSpeed"] = 22,
+        ["Loyal"] = true,
+        ["Health"] = 90,
+        ["Speed"] = 62,
+        ["Stamina"] = 1.75
+    },
+    ["Red Morgan"] = {
+        ["Acceleration"] = 1,
+        ["Description"] = "Compact, refined, brave, agreeable, and versatile. Excellent all-purpose horse.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(99, 87, 87),
+            ["Cannon"] = Color3.fromRGB(112, 77, 61),
+            ["Rein"] = Color3.fromRGB(149, 137, 136),
+            ["Hoof"] = Color3.fromRGB(130, 112, 109),
+            ["Tail"] = Color3.fromRGB(99, 87, 87),
+            ["Eyes"] = Color3.fromRGB(17, 17, 17),
+            ["Body"] = Color3.fromRGB(124, 85, 72)
+        },
+        ["Handling"] = 1.5,
+        ["HorseModel"] = "Normal",
+        ["TurnSpeed"] = 23,
+        ["Loyal"] = false,
+        ["Health"] = 60,
+        ["Speed"] = 58,
+        ["Stamina"] = 1
+    },
+    ["Black Arabian"] = {
+        ["Acceleration"] = 3,
+        ["Description"] = "Beautiful, intelligent, high spirited, and willing to please. A great long distance horse. Loyal.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(17, 17, 17),
+            ["Cannon"] = Color3.fromRGB(17, 17, 17),
+            ["Rein"] = Color3.fromRGB(17, 17, 17),
+            ["Hoof"] = Color3.fromRGB(27, 42, 53),
+            ["Tail"] = Color3.fromRGB(17, 17, 17),
+            ["Eyes"] = Color3.fromRGB(17, 17, 17),
+            ["Body"] = Color3.fromRGB(17, 17, 17)
+        },
+        ["Handling"] = 3,
+        ["HorseModel"] = "Normal",
+        ["TurnSpeed"] = 22,
+        ["Loyal"] = true,
+        ["Health"] = 90,
+        ["Speed"] = 62,
+        ["Stamina"] = 1.75
+    },
+    ["Beige Kentucky Saddler"] = {
+        ["Acceleration"] = 1.5,
+        ["Description"] = "Very showy, spirited, yet gentle. Moves with a smooth gate. High handling and stamina.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(86, 66, 54),
+            ["Cannon"] = Color3.fromRGB(86, 66, 54),
+            ["Rein"] = Color3.fromRGB(84, 63, 48),
+            ["Hoof"] = Color3.fromRGB(108, 88, 75),
+            ["Tail"] = Color3.fromRGB(86, 66, 54),
+            ["Eyes"] = Color3.fromRGB(17, 17, 17),
+            ["Body"] = Color3.fromRGB(195, 148, 81)
+        },
+        ["Handling"] = 2,
+        ["HorseModel"] = "Normal",
+        ["TurnSpeed"] = 25,
+        ["Loyal"] = false,
+        ["Health"] = 85,
+        ["Speed"] = 58,
+        ["Stamina"] = 1.5
+    },
+    ["Night Mare"] = {
+        ["Acceleration"] = 1,
+        ["Description"] = "Ghost Rider's sinister horse, Night Mare. Shares the stats of your most expensive owned horse.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(184, 100, 51),
+            ["Cannon"] = Color3.fromRGB(17, 17, 17),
+            ["Rein"] = Color3.fromRGB(66, 56, 44),
+            ["Hoof"] = Color3.fromRGB(180, 94, 52),
+            ["Tail"] = Color3.fromRGB(184, 100, 51),
+            ["Eyes"] = Color3.fromRGB(213, 115, 61),
+            ["Body"] = Color3.fromRGB(17, 17, 17)
+        },
+        ["Handling"] = 0.8,
+        ["HorseModel"] = "NightMare",
+        ["TurnSpeed"] = 21,
+        ["Loyal"] = true,
+        ["Health"] = 60,
+        ["Speed"] = 55,
+        ["Stamina"] = 1
+    },
+    ["Ebony Quarterhorse"] = {
+        ["Acceleration"] = 2,
+        ["Description"] = "Athletic, intelligent, and dependable. Excellent short distance sprinter. Exceptional handling and trainability. Loyal.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(34, 26, 21),
+            ["Cannon"] = Color3.fromRGB(49, 37, 30),
+            ["Rein"] = Color3.fromRGB(108, 88, 75),
+            ["Hoof"] = Color3.fromRGB(59, 50, 43),
+            ["Tail"] = Color3.fromRGB(34, 26, 21),
+            ["Eyes"] = Color3.fromRGB(17, 17, 17),
+            ["Body"] = Color3.fromRGB(50, 34, 29)
+        },
+        ["Handling"] = 2.5,
+        ["HorseModel"] = "Normal",
+        ["TurnSpeed"] = 26,
+        ["Loyal"] = true,
+        ["Health"] = 100,
+        ["Speed"] = 58,
+        ["Stamina"] = 1.75
+    },
+    ["Skeleton Horse Dark"] = {
+        ["Acceleration"] = 1,
+        ["Description"] = "The reanimated skeleton of a horse. Shares the stats of your most expensive owned horse.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(68, 68, 70),
+            ["Cannon"] = Color3.fromRGB(68, 68, 70),
+            ["Rein"] = Color3.fromRGB(59, 54, 47),
+            ["Hoof"] = Color3.fromRGB(68, 68, 70),
+            ["Tail"] = Color3.fromRGB(68, 68, 70),
+            ["Eyes"] = Color3.fromRGB(68, 68, 70),
+            ["Body"] = Color3.fromRGB(68, 68, 70)
+        },
+        ["Handling"] = 0.8,
+        ["HorseModel"] = "SkeletonHorseDark",
+        ["TurnSpeed"] = 21,
+        ["Loyal"] = true,
+        ["Health"] = 60,
+        ["Speed"] = 55,
+        ["Stamina"] = 1
+    },
+    ["Spectral Steed"] = {
+        ["Acceleration"] = 1,
+        ["Description"] = "A ghostly horse with a mysterious origin. Shares the stats of your most expensive owned horse.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(139, 147, 161),
+            ["Cannon"] = Color3.fromRGB(97, 109, 131),
+            ["Rein"] = Color3.fromRGB(74, 72, 72),
+            ["Hoof"] = Color3.fromRGB(139, 147, 161),
+            ["Tail"] = Color3.fromRGB(139, 147, 161),
+            ["Eyes"] = Color3.fromRGB(180, 210, 228),
+            ["Body"] = Color3.fromRGB(97, 109, 131)
+        },
+        ["Handling"] = 0.8,
+        ["HorseModel"] = "SpectralSteed",
+        ["TurnSpeed"] = 21,
+        ["Loyal"] = true,
+        ["Health"] = 60,
+        ["Speed"] = 55,
+        ["Stamina"] = 1
+    },
+    ["Red Shetland Pony"] = {
+        ["Acceleration"] = 0.8,
+        ["Description"] = "Gentle, intelligent, and friendly, yet can be impatient, opinionated, and cunning. Strong with high health.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(202, 186, 170),
+            ["Cannon"] = Color3.fromRGB(231, 231, 236),
+            ["Rein"] = Color3.fromRGB(175, 148, 131),
+            ["Hoof"] = Color3.fromRGB(168, 149, 139),
+            ["Tail"] = Color3.fromRGB(150, 103, 102),
+            ["Eyes"] = Color3.fromRGB(17, 17, 17),
+            ["Body"] = Color3.fromRGB(136, 73, 62)
+        },
+        ["Handling"] = 0.8,
+        ["HorseModel"] = "Pony",
+        ["TurnSpeed"] = 20,
+        ["Loyal"] = false,
+        ["Health"] = 100,
+        ["Speed"] = 50,
+        ["Stamina"] = 1
+    },
+    ["Gray Morgan"] = {
+        ["Acceleration"] = 1,
+        ["Description"] = "Compact, refined, brave, agreeable, and versatile. Excellent all-purpose horse.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(47, 47, 47),
+            ["Cannon"] = Color3.fromRGB(74, 74, 74),
+            ["Rein"] = Color3.fromRGB(149, 137, 136),
+            ["Hoof"] = Color3.fromRGB(43, 43, 43),
+            ["Tail"] = Color3.fromRGB(47, 47, 47),
+            ["Eyes"] = Color3.fromRGB(17, 17, 17),
+            ["Body"] = Color3.fromRGB(122, 122, 122)
+        },
+        ["Handling"] = 1.5,
+        ["HorseModel"] = "Normal",
+        ["TurnSpeed"] = 23,
+        ["Loyal"] = false,
+        ["Health"] = 60,
+        ["Speed"] = 58,
+        ["Stamina"] = 1
+    },
+    ["Skeleton Horse"] = {
+        ["Acceleration"] = 1,
+        ["Description"] = "The reanimated skeleton of a horse. Shares the stats of your most expensive owned horse.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(125, 127, 130),
+            ["Cannon"] = Color3.fromRGB(125, 127, 130),
+            ["Rein"] = Color3.fromRGB(59, 54, 47),
+            ["Hoof"] = Color3.fromRGB(125, 127, 130),
+            ["Tail"] = Color3.fromRGB(125, 127, 130),
+            ["Eyes"] = Color3.fromRGB(125, 127, 130),
+            ["Body"] = Color3.fromRGB(125, 127, 130)
+        },
+        ["Handling"] = 0.8,
+        ["HorseModel"] = "SkeletonHorse",
+        ["TurnSpeed"] = 21,
+        ["Loyal"] = true,
+        ["Health"] = 60,
+        ["Speed"] = 55,
+        ["Stamina"] = 1
+    },
+    ["Light Brown Shetland Pony"] = {
+        ["Acceleration"] = 0.8,
+        ["Description"] = "Gentle, intelligent, and friendly, yet can be impatient, opinionated, and cunning. Strong with high health.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(202, 186, 170),
+            ["Cannon"] = Color3.fromRGB(145, 107, 82),
+            ["Rein"] = Color3.fromRGB(175, 148, 131),
+            ["Hoof"] = Color3.fromRGB(97, 86, 80),
+            ["Tail"] = Color3.fromRGB(202, 186, 170),
+            ["Eyes"] = Color3.fromRGB(17, 17, 17),
+            ["Body"] = Color3.fromRGB(145, 107, 82)
+        },
+        ["Handling"] = 0.8,
+        ["HorseModel"] = "Pony",
+        ["TurnSpeed"] = 20,
+        ["Loyal"] = false,
+        ["Health"] = 100,
+        ["Speed"] = 50,
+        ["Stamina"] = 1
+    },
+    ["Chestnut Thoroughbred"] = {
+        ["Acceleration"] = 2,
+        ["Description"] = "Known for agility, speed, and spirit. Excellent at racing short to medium distances. Tall with high HP. Loyal.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(86, 66, 54),
+            ["Cannon"] = Color3.fromRGB(86, 66, 54),
+            ["Rein"] = Color3.fromRGB(84, 63, 48),
+            ["Hoof"] = Color3.fromRGB(108, 88, 75),
+            ["Tail"] = Color3.fromRGB(86, 66, 54),
+            ["Eyes"] = Color3.fromRGB(17, 17, 17),
+            ["Body"] = Color3.fromRGB(111, 53, 32)
+        },
+        ["Handling"] = 2,
+        ["HorseModel"] = "Thoroughbred",
+        ["TurnSpeed"] = 22,
+        ["Loyal"] = true,
+        ["Health"] = 80,
+        ["Speed"] = 63,
+        ["Stamina"] = 1.5
+    },
+    ["Dark Brown Standardbred"] = {
+        ["Acceleration"] = 1,
+        ["Description"] = "Known as fast trotters, especially over long distances. Good all-purpose horse for riding and driving.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(34, 26, 21),
+            ["Cannon"] = Color3.fromRGB(49, 37, 30),
+            ["Rein"] = Color3.fromRGB(108, 88, 75),
+            ["Hoof"] = Color3.fromRGB(59, 50, 43),
+            ["Tail"] = Color3.fromRGB(34, 26, 21),
+            ["Eyes"] = Color3.fromRGB(17, 17, 17),
+            ["Body"] = Color3.fromRGB(85, 61, 43)
+        },
+        ["Handling"] = 0.8,
+        ["HorseModel"] = "NormalSmall",
+        ["TurnSpeed"] = 21,
+        ["Loyal"] = false,
+        ["Health"] = 60,
+        ["Speed"] = 55,
+        ["Stamina"] = 1
+    },
+    ["Chestnut Draft"] = {
+        ["Acceleration"] = 1,
+        ["Description"] = "Exceptionally tall and heavy yet calm, and gentle. Extremely muscular with strong bones. High HP and stamina. Loyal.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(29, 22, 18),
+            ["Cannon"] = Color3.fromRGB(255, 244, 216),
+            ["Rein"] = Color3.fromRGB(143, 87, 60),
+            ["Hoof"] = Color3.fromRGB(207, 190, 173),
+            ["Tail"] = Color3.fromRGB(29, 22, 18),
+            ["Eyes"] = Color3.fromRGB(17, 17, 17),
+            ["Body"] = Color3.fromRGB(111, 53, 32)
+        },
+        ["Handling"] = 1.5,
+        ["HorseModel"] = "Draft",
+        ["TurnSpeed"] = 23,
+        ["Loyal"] = true,
+        ["Health"] = 160,
+        ["Speed"] = 58,
+        ["Stamina"] = 1.5
+    },
+    ["Chestnut Tennessee Walker"] = {
+        ["Acceleration"] = 1.5,
+        ["Description"] = "Known for its smooth gate, flashy movements, and four-beat running walk. Excellent stamina.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(117, 62, 34),
+            ["Cannon"] = Color3.fromRGB(143, 76, 42),
+            ["Rein"] = Color3.fromRGB(84, 63, 48),
+            ["Hoof"] = Color3.fromRGB(108, 88, 75),
+            ["Tail"] = Color3.fromRGB(117, 62, 34),
+            ["Eyes"] = Color3.fromRGB(17, 17, 17),
+            ["Body"] = Color3.fromRGB(143, 76, 42)
+        },
+        ["Handling"] = 1.5,
+        ["HorseModel"] = "Normal",
+        ["TurnSpeed"] = 23,
+        ["Loyal"] = false,
+        ["Health"] = 80,
+        ["Speed"] = 58,
+        ["Stamina"] = 1.2
+    },
+    ["Brown Draft"] = {
+        ["Acceleration"] = 1,
+        ["Description"] = "Exceptionally tall and heavy yet calm, and gentle. Extremely muscular with strong bones. High HP and stamina. Loyal.",
+        ["Colours"] = {
+            ["Mane"] = Color3.fromRGB(29, 22, 18),
+            ["Cannon"] = Color3.fromRGB(43, 43, 43),
+            ["Rein"] = Color3.fromRGB(217, 217, 217),
+            ["Hoof"] = Color3.fromRGB(43, 43, 43),
+            ["Tail"] = Color3.fromRGB(29, 22, 18),
+            ["Eyes"] = Color3.fromRGB(17, 17, 17),
+            ["Body"] = Color3.fromRGB(109, 65, 39)
+        },
+        ["Handling"] = 1.5,
+        ["HorseModel"] = "Draft",
+        ["TurnSpeed"] = 23,
+        ["Loyal"] = true,
+        ["Health"] = 160,
+        ["Speed"] = 58,
+        ["Stamina"] = 1.5
+    }
+}
