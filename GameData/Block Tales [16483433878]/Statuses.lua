7e400e85dcbbc8c0aea0f5dc675f35b8a5fcc2b3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-07 01:21:02 +1100

    Create Statuses.lua

---
+++ GameData/Block Tales [16483433878]/Statuses.lua
@@ -0,0 +1,70 @@
+data = {
+    ["Sleep"] = {["Chance"] = 15, ["RectOffset"] = "0, 5", ["NoNeed"] = true, ["Color"] = Color3.fromRGB(132, 126, 135)},
+    ["Invisible"] = {
+        ["Color"] = Color3.fromRGB(132, 126, 135),
+        ["RectOffset"] = "4, 0",
+        ["NoNeed"] = true,
+        ["Good"] = true
+    },
+    ["FirstStrike"] = {["Color"] = Color3.fromRGB(91, 110, 225), ["RectOffset"] = "0, 0", ["Good"] = true},
+    ["Charged2"] = {
+        ["Color"] = Color3.fromRGB(91, 110, 225),
+        ["RectOffset"] = "0, 0",
+        ["NoNeed"] = true,
+        ["Good"] = true
+    },
+    ["Poison"] = {["Color"] = Color3.fromRGB(106, 190, 48), ["RectOffset"] = "4, 4"},
+    ["Bodyguard"] = {
+        ["Good"] = true,
+        ["NoNeed"] = true,
+        ["Color"] = Color3.fromRGB(132, 126, 135),
+        ["RectOffset"] = "0, 0",
+        ["Playa"] = true
+    },
+    ["Exhausted"] = {["Color"] = Color3.fromRGB(247, 218, 71), ["RectOffset"] = "2, 0", ["NoNeed"] = true},
+    ["Confused"] = {["Color"] = Color3.fromRGB(171, 130, 187), ["RectOffset"] = "4, 1"},
+    ["Dodgy"] = {["Color"] = Color3.fromRGB(91, 110, 225), ["RectOffset"] = "0, 0", ["Good"] = true},
+    ["NextBurn"] = {["Color"] = Color3.fromRGB(255, 131, 1), ["RectOffset"] = "2, 4", ["Good"] = true},
+    ["Greed"] = {["Color"] = Color3.fromRGB(200, 162, 74), ["RectOffset"] = "2, 0", ["Good"] = true},
+    ["Piercing"] = {
+        ["Color"] = Color3.fromRGB(0, 255, 255),
+        ["RectOffset"] = "0, 0",
+        ["NoNeed"] = true,
+        ["Good"] = true
+    },
+    ["Frozen"] = {
+        ["Chance"] = 8,
+        ["RectOffset"] = "0, 4",
+        ["DamageIfFail"] = true,
+        ["Color"] = Color3.fromRGB(95, 205, 228)
+    },
+    ["DefenseDown"] = {["Color"] = Color3.fromRGB(172, 50, 50), ["RectOffset"] = "0, 2"},
+    ["Dizzy"] = {["Chance"] = 20, ["RectOffset"] = "4, 2", ["Color"] = Color3.fromRGB(171, 130, 187)},
+    ["Peeling"] = {["Color"] = Color3.fromRGB(255, 131, 1), ["RectOffset"] = "2, 0", ["NoNeed"] = true},
+    ["Fast"] = {["Color"] = Color3.fromRGB(132, 126, 135), ["RectOffset"] = "2, 3", ["NoNeed"] = true, ["Good"] = true},
+    ["Immobile"] = {["NoNeed"] = true, ["RectOffset"] = "0, 0"},
+    ["ChargedUp"] = {["Color"] = Color3.fromRGB(91, 110, 225), ["RectOffset"] = "2, 1"},
+    ["Slow"] = {["Chance"] = 10, ["RectOffset"] = "2, 5", ["NoNeed"] = true, ["Color"] = Color3.fromRGB(138, 111, 48)},
+    ["Fine"] = {["Color"] = Color3.fromRGB(247, 218, 71), ["RectOffset"] = "4, 3", ["NoNeed"] = true, ["Good"] = true},
+    ["Agony"] = {["Color"] = Color3.fromRGB(35, 75, 145), ["RectOffset"] = "4, 0", ["NoNeed"] = true, ["Good"] = true},
+    ["Tiny"] = {["Chance"] = 30, ["RectOffset"] = "4, 5", ["NoNeed"] = true, ["Color"] = Color3.fromRGB(172, 50, 50)},
+    ["HPRegen"] = {["Color"] = Color3.fromRGB(172, 50, 50), ["RectOffset"] = "0, 0", ["Good"] = true},
+    ["Enraged"] = {
+        ["NoNeed"] = true,
+        ["Color"] = Color3.fromRGB(172, 50, 50),
+        ["RectOffset"] = "0, 3",
+        ["Chance"] = 50,
+        ["Playa"] = true
+    },
+    ["Burn"] = {["Color"] = Color3.fromRGB(255, 131, 1), ["RectOffset"] = "4, 0"},
+    ["ChargedDown"] = {["Color"] = Color3.fromRGB(172, 50, 50), ["RectOffset"] = "0, 1"},
+    ["Big"] = {["Color"] = Color3.fromRGB(91, 110, 225), ["RectOffset"] = "0, 0", ["NoNeed"] = true, ["Good"] = true},
+    ["NextPoison"] = {["Color"] = Color3.fromRGB(106, 190, 48), ["RectOffset"] = "2, 0", ["Good"] = true},
+    ["Spiky"] = {["Color"] = Color3.fromRGB(106, 190, 48), ["RectOffset"] = "0, 0", ["Good"] = true},
+    ["Shiny"] = {["Color"] = Color3.fromRGB(91, 110, 225), ["RectOffset"] = "0, 0", ["NoNeed"] = true, ["Good"] = true},
+    ["Silenced"] = {["Color"] = Color3.fromRGB(247, 218, 71), ["RectOffset"] = "0, 0", ["NoNeed"] = true},
+    ["DefenseUp"] = {["Color"] = Color3.fromRGB(91, 110, 225), ["RectOffset"] = "2, 2"},
+    ["SPRegen"] = {["Color"] = Color3.fromRGB(247, 218, 71), ["RectOffset"] = "2, 0", ["Good"] = true},
+    ["NextFrozen"] = {["Color"] = Color3.fromRGB(95, 205, 228), ["RectOffset"] = "0, 0", ["Good"] = true},
+    ["Focused"] = {["Color"] = Color3.fromRGB(132, 126, 135), ["RectOffset"] = "4, 0", ["Good"] = true}
+}
