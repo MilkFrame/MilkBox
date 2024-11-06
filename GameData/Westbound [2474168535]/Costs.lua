32c68c72fe69f809236bbd7a06c22c03eed950c5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-07 01:41:04 +1100

    Create Costs.lua

---
+++ GameData/Westbound [2474168535]/Costs.lua
@@ -0,0 +1,77 @@
+data = {
+    ["Items"] = {
+        ["Lasso"] = 200,
+        ["Remington"] = 4000,
+        ["LeMat Revolver"] = 875,
+        ["Colt Navy"] = 2000,
+        ["Lantern"] = 45,
+        ["Pump-action Shotgun"] = 2000,
+        ["Pick"] = 0,
+        ["Double Barrel Shotgun"] = 575,
+        ["Henry Rifle"] = 250,
+        ["Bow"] = 95,
+        ["Colt Lightning"] = 1175,
+        ["Machete"] = 250,
+        ["Gold Buffalo Rifle"] = 25000,
+        ["Winchester Model 94"] = 4400,
+        ["Gold Mosin"] = 30000,
+        ["Gold Mondragon"] = 50000,
+        ["Joalland Pistol"] = 2400,
+        ["Volcanic Pistol"] = 225,
+        ["Schofield Revolver"] = 150,
+        ["Winchester Rifle"] = 600,
+        ["Mondragon"] = 5000,
+        ["Double-Action Revolver"] = 3000,
+        ["Sharps Rifle"] = 2500,
+        ["Bergmann No. 3"] = 2200,
+        ["Mauser Pistol"] = 1300,
+        ["Mosin-Nagant"] = 3000,
+        ["Sawed-Off Shotgun"] = 325
+    },
+    ["ToolUpgrades"] = {
+        ["PickaxeLevel"] = {["1"] = 0, ["3"] = 250, ["2"] = 100, ["Max"] = 6, ["4"] = 600, ["5"] = 1000, ["6"] = 1500}
+    },
+    ["Upgrades"] = {
+        ["ThrowCapacityLevel"] = {["1"] = 400, ["3"] = 1800, ["2"] = 900, ["Max"] = 20, ["IncreaseAmount"] = 5},
+        ["AmmoCapacityLevel"] = {["1"] = 400, ["3"] = 1800, ["2"] = 900, ["Max"] = 190, ["IncreaseAmount"] = 30},
+        ["BagSizeLevel"] = {
+            ["1"] = 250,
+            ["IncreaseAmount"] = 10,
+            ["3"] = 700,
+            ["2"] = 500,
+            ["Max"] = 100,
+            ["4"] = 1200,
+            ["5"] = 2400,
+            ["6"] = 3200
+        },
+        ["InventorySizeLevel"] = {["1"] = 200, ["3"] = 600, ["2"] = 400, ["Max"] = 24, ["IncreaseAmount"] = 4},
+        ["PotionCapacityLevel"] = {["1"] = 600, ["IncreaseAmount"] = 1, ["Max"] = 3, ["2"] = 1200}
+    },
+    ["Consumables"] = {
+        ["ShotgunAmmo"] = {["Amount"] = 10},
+        ["SniperAmmo"] = {["Amount"] = 10},
+        ["BIG Dynamite"] = {["Amount"] = 1},
+        ["PistolAmmo"] = {["Amount"] = 10},
+        ["RifleAmmo"] = {["Amount"] = 10},
+        ["Arrows"] = {["Amount"] = 10},
+        ["Health Potion"] = {["Amount"] = 1},
+        ["Dynamite"] = {["Amount"] = 1}
+    },
+    ["Horses"] = {
+        ["Chestnut Thoroughbred"] = 6000,
+        ["Ebony Quarterhorse"] = 3000,
+        ["Black Draft"] = 4700,
+        ["Dark Brown Standardbred"] = 0,
+        ["Chestnut Draft"] = 4700,
+        ["Brown Standardbred"] = 0,
+        ["White Arabian"] = 15000,
+        ["Gray Morgan"] = 300,
+        ["Red Shetland Pony"] = 200,
+        ["Light Brown Shetland Pony"] = 200,
+        ["Red Morgan"] = 300,
+        ["Black Arabian"] = 15000,
+        ["Chestnut Tennessee Walker"] = 500,
+        ["Beige Kentucky Saddler"] = 1000,
+        ["Brown Draft"] = 4700
+    }
+}
