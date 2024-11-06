ef29c3493b2a790dce380c067c8a6a4c92d9840e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-07 01:38:05 +1100

    Create TrainNames.lua

---
+++ GameData/Westbound [2474168535]/TrainNames.lua
@@ -0,0 +1,26 @@
+data = {
+    ["TrainLoungeCarModelNames"] = {"LoungeCar", "Lounge"},
+    ["TrainLocomotiveModelNames"] = {"TrainLocomotive", "Locomotive", "Engine"},
+    ["IsEngineWheel"] = "function: 0x7294198e9600139e",
+    ["TrainCoalCarModelNames"] = {"CoalCar", "Coal", "Fuel", "FuelCar"},
+    ["GetSubModels"] = "function: 0xfcd353b0400b0e7e",
+    ["TrainArmouredCarModelNames"] = {"Armoured", "ArmouredCar", "Treasure", "TreasureCar"},
+    ["IsA"] = "function: 0x80715779cdeb6f4e",
+    ["IsALocomotive"] = "function: 0x3327a608ef05994e",
+    ["TrainCarModelNames"] = {"TrainCar", "Trolley", "PassengerCar", "Passenger"},
+    ["AllCarNames"] = {
+        ["Event"] = {},
+        ["Passenger"] = {"TrainCar", "Trolley", "PassengerCar", "Passenger"},
+        ["Armour"] = {"Armoured", "ArmouredCar", "Treasure", "TreasureCar"},
+        ["Locomotive"] = {"TrainLocomotive", "Locomotive", "Engine"},
+        ["Caboose"] = {"Caboose", "TrainCaboose"},
+        ["Coal"] = {"CoalCar", "Coal", "Fuel", "FuelCar"},
+        ["Lounge"] = {"LoungeCar", "Lounge"},
+        ["Freight"] = {"FreightCar", "Freight"}
+    },
+    ["TrainFreightCarModelNames"] = {"FreightCar", "Freight"},
+    ["PrimaryPartCFrameValueName"] = "PrimaryPartCFrame",
+    ["TrainEventModelNames"] = {},
+    ["TrainCabooseModelNames"] = {"Caboose", "TrainCaboose"},
+    ["EngineWheelNames"] = {"FrontWheel", "Wheel", "EngineWheel"}
+}
