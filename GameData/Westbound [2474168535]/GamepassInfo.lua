80f8ec271a33be7bd065829a60b860e5963e931a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-07 01:39:40 +1100

    Create GamepassInfo.lua

---
+++ GameData/Westbound [2474168535]/GamepassInfo.lua
@@ -0,0 +1,10 @@
+data = {
+    ["The Kitchen Crusader"] = {["ID"] = 11489792, ["Price"] = 150},
+    ["VIP"] = {["ID"] = 11104123, ["Price"] = 499},
+    ["Skeleton Horse"] = {["ID"] = 12245627, ["Price"] = 350},
+    ["The Cactus Menace"] = {["ID"] = 11225314, ["Price"] = 350},
+    ["El Charro"] = {["ID"] = 72607881, ["Price"] = 499},
+    ["Ghost Rider"] = {["ID"] = 135755310, ["Price"] = 799},
+    ["US Cavalry"] = {["ID"] = 11776145, ["Price"] = 250},
+    ["The Dark Watchers"] = {["ID"] = 672871119, ["Price"] = 599}
+}
