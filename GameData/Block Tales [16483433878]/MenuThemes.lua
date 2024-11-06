cc9b4782395c992ee9c9eb9a00ebc231045ff648
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-07 01:31:14 +1100

    Create MenuThemes.lua

---
+++ GameData/Block Tales [16483433878]/MenuThemes.lua
@@ -0,0 +1,14 @@
+data = {
+    ["Rose"] = Color3.fromRGB(230, 150, 170),
+    ["Grape"] = Color3.fromRGB(170, 80, 230),
+    ["Mint"] = Color3.fromRGB(130, 200, 130),
+    ["Coal"] = Color3.fromRGB(60, 60, 60),
+    ["Blueberry"] = Color3.fromRGB(80, 70, 230),
+    ["Club"] = Color3.fromRGB(80, 230, 230),
+    ["Chocolate"] = Color3.fromRGB(123, 63, 0),
+    ["Gold"] = Color3.fromRGB(255, 210, 0),
+    ["Apple"] = Color3.fromRGB(230, 70, 80),
+    ["Plain"] = Color3.fromRGB(135, 145, 145),
+    ["Orange"] = Color3.fromRGB(210, 150, 80),
+    ["Banana"] = Color3.fromRGB(230, 245, 115)
+}
