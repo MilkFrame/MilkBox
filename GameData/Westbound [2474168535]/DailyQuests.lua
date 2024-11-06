f578b4b764b61a6940a1a034adaea91ffabed45f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-07 01:39:26 +1100

    Create DailyQuests.lua

---
+++ GameData/Westbound [2474168535]/DailyQuests.lua
@@ -0,0 +1,18 @@
+data = {
+    ["KnifeKills"] = {["Hard"] = 10, ["ActionName"] = "Get", ["NounName"] = "Knife Kills", ["Easy"] = 5},
+    ["ShotgunKills"] = {["Hard"] = 16, ["ActionName"] = "Get", ["NounName"] = "Shotgun Kills", ["Easy"] = 8},
+    ["DynamiteKills"] = {["Hard"] = 8, ["ActionName"] = "Get", ["NounName"] = "Dynamite Kills", ["Easy"] = 4},
+    ["HuntBears"] = {["Hard"] = 8, ["ActionName"] = "Hunt", ["NounName"] = "Bears", ["Easy"] = 4},
+    ["CaptureOutlaws"] = {["Hard"] = 4, ["ActionName"] = "Capture", ["NounName"] = "Outlaws Alive", ["Easy"] = 2},
+    ["HuntDeer"] = {["Hard"] = 12, ["ActionName"] = "Hunt", ["NounName"] = "Deer", ["Easy"] = 6},
+    ["TreasureHunt"] = {["Hard"] = 8, ["ActionName"] = "Open", ["NounName"] = "Treasure Chests", ["Easy"] = 4},
+    ["SellSilver"] = {["Hard"] = 20, ["ActionName"] = "Sell", ["NounName"] = "Silver Ore", ["Easy"] = 10},
+    ["SellGold"] = {["Hard"] = 10, ["ActionName"] = "Sell", ["NounName"] = "Gold Ore", ["Easy"] = 5},
+    ["KillOutlaws"] = {["Hard"] = 16, ["ActionName"] = "Kill", ["NounName"] = "Outlaws", ["Easy"] = 8},
+    ["RobMoney"] = {["Hard"] = 300, ["ActionName"] = "Rob", ["NounName"] = "Dollars", ["Easy"] = 150},
+    ["HuntWolves"] = {["Hard"] = 12, ["ActionName"] = "Hunt", ["NounName"] = "Wolves", ["Easy"] = 6},
+    ["KillCowboys"] = {["Hard"] = 16, ["ActionName"] = "Kill", ["NounName"] = "Cowboys", ["Easy"] = 8},
+    ["BowKills"] = {["Hard"] = 10, ["ActionName"] = "Get", ["NounName"] = "Bow Kills", ["Easy"] = 5},
+    ["HuntBison"] = {["Hard"] = 8, ["ActionName"] = "Hunt", ["NounName"] = "Bison", ["Easy"] = 4},
+    ["HuntCoyotes"] = {["Hard"] = 12, ["ActionName"] = "Hunt", ["NounName"] = "Coyotes", ["Easy"] = 6}
+}
