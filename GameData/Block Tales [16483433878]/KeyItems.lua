dfd19537e78e255bcb08ad34cb9c22e98b50082c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-07 01:20:44 +1100

    Create KeyItems.lua

---
+++ GameData/Block Tales [16483433878]/KeyItems.lua
@@ -0,0 +1,37 @@
+data = {
+    ["Manor Attic Key"] = {
+        ["Description"] = "A key that opens the attic in the Manor.",
+        ["Icon"] = "rbxassetid://16404419929"
+    },
+    ["Jeep Keys"] = {
+        ["Description"] = "Keys that let you drive Thaniyel's jeep...",
+        ["Icon"] = "rbxassetid://18148520355"
+    },
+    ["Mysterious Bottle"] = {["Description"] = "He asked for it...", ["Icon"] = "rbxassetid://15052299783"},
+    ["Spider"] = {["Description"] = "The arachnid...", ["Icon"] = "rbxassetid://18433608554"},
+    ["Adventurer ID"] = {
+        ["Description"] = "ID that lets you pass through the Blackrock Mountains Toll Booth",
+        ["Icon"] = "rbxassetid://17327642328"
+    },
+    ["Blackrock Diner Key"] = {
+        ["Description"] = "A key that opens the diner room in the Blackrock Castle.",
+        ["Icon"] = "http://www.roblox.com/asset/?id=2120362329"
+    },
+    ["Dust"] = {["Description"] = "The particles...", ["Icon"] = "rbxassetid://18433612799"},
+    ["Bones"] = {["Description"] = "The calcium...", ["Icon"] = "rbxassetid://18433614777"},
+    ["Key"] = {["Description"] = "A key that opens a door in Bizville.", ["Icon"] = "rbxassetid://15116522148"},
+    ["Manor Basement Key"] = {
+        ["Description"] = "A key that opens the basement in the Manor.",
+        ["Icon"] = "rbxassetid://16404419929"
+    },
+    ["Train Ticket"] = {
+        ["Description"] = "Ticket that lets you board the train",
+        ["Icon"] = "rbxassetid://111332367135615"
+    },
+    ["Raw Turkey"] = {["Description"] = "What a pain.", ["Icon"] = "rbxassetid://16404419661"},
+    ["Big Bag"] = {["Description"] = "Allows you to carry up to 20 items.", ["Icon"] = "rbxassetid://16942684433"},
+    ["Blackrock Key"] = {
+        ["Description"] = "A key that opens doors in the Blackrock Castle.",
+        ["Icon"] = "rbxassetid://15116522148"
+    }
+}
