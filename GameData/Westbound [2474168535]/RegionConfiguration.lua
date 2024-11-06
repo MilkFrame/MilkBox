1c795db71e403f52cd7fb937252cc068ef1f1a75
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-07 01:38:40 +1100

    Create RegionConfiguration.lua

---
+++ GameData/Westbound [2474168535]/RegionConfiguration.lua
@@ -0,0 +1,17 @@
+data = {
+    ["Region1"] = {
+        ["Configuration"] = {
+            ["MinY"] = 37.13,
+            ["PostWidthAndDepth"] = 1,
+            ["ConnectorPostDepthSizeRatio"] = 0.8,
+            ["MaxY"] = 114.59
+        },
+        ["Points"] = {
+            {["Position"] = {["Y"] = 75, ["X"] = -1808, ["Z"] = -375}},
+            {["Position"] = {["Y"] = 75, ["X"] = -1828, ["Z"] = 245}},
+            {["Position"] = {["Y"] = 75, ["X"] = 174, ["Z"] = 578}},
+            {["Position"] = {["Y"] = 75, ["X"] = -872, ["Z"] = -197}},
+            {["Position"] = {["Y"] = 75, ["X"] = -1023, ["Z"] = -510}}
+        }
+    }
+}
