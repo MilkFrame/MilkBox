f507bf58038628ac34227d17a3ef7bf4cd31901a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-07 01:21:27 +1100

    Create FastTravel.lua

---
+++ GameData/Block Tales [16483433878]/FastTravel.lua
@@ -0,0 +1,23 @@
+data = {
+    ["HQBasement"] = {["Index"] = 2, ["Icon"] = "rbxassetid://16939425201", ["Title"] = "HQ Basement"},
+    ["TreeBossRoom"] = {["Index"] = 22, ["Icon"] = "rbxassetid://18183047153", ["Title"] = "Griefer's Crib"},
+    ["BlackrockCastleEntrance"] = {
+        ["Index"] = 11,
+        ["Icon"] = "rbxassetid://16939427379",
+        ["Title"] = "Blackrock Castle"
+    },
+    ["HAUNTEDManorKitchen"] = {["Index"] = 32, ["Icon"] = "rbxassetid://138002266567277", ["Title"] = "The Ghost Chef"},
+    ["Roadtown"] = {["Index"] = 10, ["Icon"] = "rbxassetid://16939426163", ["Title"] = "Roadtown"},
+    ["Hub"] = {["Index"] = 0, ["Icon"] = "rbxassetid://16939427725", ["Title"] = "Bizville"},
+    ["BizvilleBossRefight"] = {["Index"] = 1, ["Icon"] = "rbxassetid://18377557979", ["Title"] = "The Guru"},
+    ["DreamHouseRoom"] = {["Index"] = 33, ["Icon"] = "rbxassetid://128924031388466", ["Title"] = "Dream World"},
+    ["ManorEntrance"] = {["Index"] = 31, ["Icon"] = "rbxassetid://116147505357758", ["Title"] = "The Manor"},
+    ["Floor30"] = {["Icon"] = "rbxassetid://16939426554", ["Fake"] = true, ["Index"] = 103, ["Title"] = "Pit Floor 30"},
+    ["Turitopulis"] = {["Index"] = 20, ["Icon"] = "rbxassetid://18183053285", ["Title"] = "Turitopulis"},
+    ["MangoTree"] = {["Index"] = 21, ["Icon"] = "rbxassetid://18183049588", ["Title"] = "Mango Tree"},
+    ["Floor10"] = {["Icon"] = "rbxassetid://16939426554", ["Fake"] = true, ["Index"] = 101, ["Title"] = "Pit Floor 10"},
+    ["Meadows2"] = {["Index"] = 3, ["Icon"] = "rbxassetid://17478977753", ["Title"] = "Fishing Spot"},
+    ["Floor40"] = {["Icon"] = "rbxassetid://16939426554", ["Fake"] = true, ["Index"] = 104, ["Title"] = "Pit Floor 40"},
+    ["Floor20"] = {["Icon"] = "rbxassetid://16939426554", ["Fake"] = true, ["Index"] = 102, ["Title"] = "Pit Floor 20"},
+    ["Lobby"] = {["Icon"] = "rbxassetid://16939426554", ["Fake"] = true, ["Index"] = 100, ["Title"] = "The Pit"}
+}
