77706b7b1677f784b37574495b09a6417f8e7bbd
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-07 01:40:37 +1100

    Create Items.lua

---
+++ GameData/Westbound [2474168535]/Items.lua
@@ -0,0 +1,389 @@
+data = {
+    {
+        ["StackAmount"] = 10,
+        ["Description"] = "A nugget of gold (duh)",
+        ["Weight"] = 2,
+        ["ID"] = 1,
+        ["Types"] = {"Ore"},
+        ["SellValue"] = 25,
+        ["DisplayName"] = "Gold"
+    },
+    {
+        ["StackAmount"] = 10,
+        ["Description"] = "A nugget of silver (duh)",
+        ["Weight"] = 1,
+        ["ID"] = 2,
+        ["Types"] = {"Ore"},
+        ["SellValue"] = 10,
+        ["DisplayName"] = "Silver"
+    },
+    {
+        ["StackAmount"] = 10,
+        ["Description"] = "A nugget of copper (duh)",
+        ["Weight"] = 1.5,
+        ["ID"] = 3,
+        ["Types"] = {"Ore"},
+        ["SellValue"] = 3,
+        ["DisplayName"] = "Copper"
+    },
+    {
+        ["StackAmount"] = 10,
+        ["Description"] = "A nugget of Mica",
+        ["Weight"] = 2,
+        ["ID"] = 4,
+        ["Types"] = {"Ore"},
+        ["SellValue"] = 5,
+        ["DisplayName"] = "Mica"
+    },
+    {
+        ["StackAmount"] = 10,
+        ["Description"] = "A nugget of emerald",
+        ["Weight"] = 1,
+        ["ID"] = 5,
+        ["Types"] = {"Ore"},
+        ["SellValue"] = 80,
+        ["DisplayName"] = "Emerald"
+    },
+    {
+        ["StackAmount"] = 10,
+        ["Description"] = "A nugget of diamond",
+        ["Weight"] = 1.5,
+        ["ID"] = 6,
+        ["Types"] = {"Ore"},
+        ["SellValue"] = 200,
+        ["DisplayName"] = "Diamond"
+    },
+    {
+        ["StackAmount"] = 10,
+        ["Description"] = "A nugget of ruby",
+        ["Weight"] = 1.5,
+        ["ID"] = 7,
+        ["Types"] = {"Ore"},
+        ["SellValue"] = 80,
+        ["DisplayName"] = "Ruby"
+    },
+    {
+        ["StackAmount"] = 10,
+        ["Description"] = "A nugget of sapphire",
+        ["Weight"] = 1.5,
+        ["ID"] = 8,
+        ["Types"] = {"Ore"},
+        ["SellValue"] = 80,
+        ["DisplayName"] = "Sapphire"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Dire Wolf Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 209,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 100,
+        ["DisplayName"] = "Dire Wolf Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Fox Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 210,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 13,
+        ["DisplayName"] = "Fox Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Legendary Coyote Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 306,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 50,
+        ["DisplayName"] = "Legendary Coyote Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Jewelry",
+        ["Weight"] = 0.5,
+        ["ID"] = 21,
+        ["Types"] = {"Jewelry"},
+        ["SellValue"] = 15,
+        ["DisplayName"] = "Jewelry"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Gold Jewelry",
+        ["Weight"] = 0.5,
+        ["ID"] = 20,
+        ["Types"] = {"Jewelry"},
+        ["SellValue"] = 25,
+        ["DisplayName"] = "Gold Jewelry"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Legendary Bison Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 308,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 150,
+        ["DisplayName"] = "Legendary Bison Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Antlers from a deer kill",
+        ["Weight"] = 0.5,
+        ["ID"] = 223,
+        ["Types"] = {"AnimalPart"},
+        ["SellValue"] = 10,
+        ["DisplayName"] = "Deer Antler"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Rotting meat",
+        ["Weight"] = 0.5,
+        ["ID"] = 226,
+        ["Types"] = {"Food"},
+        ["SellValue"] = 5,
+        ["DisplayName"] = "Rotten Meat"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Meat from a small game animal",
+        ["Weight"] = 0.5,
+        ["ID"] = 222,
+        ["Types"] = {"Food"},
+        ["SellValue"] = 7,
+        ["DisplayName"] = "Small Meat"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Legendary Black Bear Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 305,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 120,
+        ["DisplayName"] = "Legendary Black Bear Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "A horse",
+        ["Weight"] = 0.5,
+        ["ID"] = 1000,
+        ["Types"] = {"Horse gear"},
+        ["SellValue"] = 0,
+        ["DisplayName"] = "Horse"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Antlers from a deer kill",
+        ["Weight"] = 0.5,
+        ["ID"] = 310,
+        ["Types"] = {"AnimalPart"},
+        ["SellValue"] = 25,
+        ["DisplayName"] = "Legendary Deer Antler"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Deer Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 208,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 20,
+        ["DisplayName"] = "Deer Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Legendary Deer Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 301,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 70,
+        ["DisplayName"] = "Legendary Deer Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Meat from a large game animal",
+        ["Weight"] = 0.5,
+        ["ID"] = 221,
+        ["Types"] = {"Food"},
+        ["SellValue"] = 14,
+        ["DisplayName"] = "Large Meat"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Gold Key",
+        ["Weight"] = 0.5,
+        ["ID"] = 23,
+        ["Types"] = {"Jewelry"},
+        ["SellValue"] = 20,
+        ["DisplayName"] = "Gold Key"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Silver Skull",
+        ["Weight"] = 0.5,
+        ["ID"] = 25,
+        ["Types"] = {"Jewelry"},
+        ["SellValue"] = 15,
+        ["DisplayName"] = "Silver Skull"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Treasure Chest",
+        ["Weight"] = 0.5,
+        ["ID"] = 26,
+        ["Types"] = {"Jewelry"},
+        ["SellValue"] = 20,
+        ["DisplayName"] = "Treasure Chest"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Use it to mine for ore found in rocks.",
+        ["Weight"] = 1,
+        ["ID"] = 30,
+        ["Types"] = {"Tool"},
+        ["SellValue"] = 0,
+        ["DisplayName"] = "Pickaxe"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Black Bear Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 202,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 30,
+        ["DisplayName"] = "Black Bear Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Legendary Dire Wolf Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 309,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 150,
+        ["DisplayName"] = "Legendary Dire Wolf Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Deer Buck Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 205,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 15,
+        ["DisplayName"] = "DeerBuck Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Deer Doe Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 206,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 15,
+        ["DisplayName"] = "DeerDoe Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "DeerFawn Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 207,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 10,
+        ["DisplayName"] = "DeerFawn Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Bison Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 204,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 50,
+        ["DisplayName"] = "Bison Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Bison Horn",
+        ["Weight"] = 0.5,
+        ["ID"] = 225,
+        ["Types"] = {"AnimalPart"},
+        ["SellValue"] = 10,
+        ["DisplayName"] = "Bison Horn"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Wolf Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 200,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 20,
+        ["DisplayName"] = "Wolf Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Claw from a bear kill",
+        ["Weight"] = 0.5,
+        ["ID"] = 224,
+        ["Types"] = {"AnimalPart"},
+        ["SellValue"] = 10,
+        ["DisplayName"] = "Bear Claw"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Grizzly Bear Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 203,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 35,
+        ["DisplayName"] = "Grizzly Bear Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Silver Key",
+        ["Weight"] = 0.5,
+        ["ID"] = 24,
+        ["Types"] = {"Jewelry"},
+        ["SellValue"] = 15,
+        ["DisplayName"] = "Silver Key"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Legendary Grizzly Bear Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 304,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 150,
+        ["DisplayName"] = "Legendary Grizzly Bear Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Legendary Wolf Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 307,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 100,
+        ["DisplayName"] = "Legendary Wolf Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Crown",
+        ["Weight"] = 0.5,
+        ["ID"] = 22,
+        ["Types"] = {"Jewelry"},
+        ["SellValue"] = 25,
+        ["DisplayName"] = "Crown"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Coyote Pelt",
+        ["Weight"] = 0.5,
+        ["ID"] = 201,
+        ["Types"] = {"AnimalPelt"},
+        ["SellValue"] = 17,
+        ["DisplayName"] = "Coyote Pelt"
+    },
+    {
+        ["StackAmount"] = 1,
+        ["Description"] = "Meat from a game animal",
+        ["Weight"] = 0.5,
+        ["ID"] = 220,
+        ["Types"] = {"Food"},
+        ["SellValue"] = 10,
+        ["DisplayName"] = "Meat"
+    }
+}
