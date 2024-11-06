08ac19b0d16b5e5315f3c3490e50cdbd2b67bd54
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-07 01:30:58 +1100

    Create Objectives.lua

---
+++ GameData/Block Tales [16483433878]/Objectives.lua
@@ -0,0 +1,190 @@
+data = {
+    ["World3"] = {
+        ["World3"] = {
+            ["30"] = "Reach_WhereTho",
+            ["12"] = "Reach_ManorBasement2",
+            ["15"] = "Reach_DreamHouseRoom",
+            ["14"] = "Reach_ManorWalkerRoom",
+            ["4"] = "Flag_DefeatBandit",
+            ["5"] = "Flag_TrainCleared",
+            ["17"] = "Reach_DreamPathA1",
+            ["16"] = "Reach_DreamZoneHub",
+            ["28"] = "Reach_DreamPathD3a",
+            ["29"] = "Flag_DefeatHatred",
+            ["8"] = "Reach_ManorEntrance",
+            ["9"] = "Reach_HAUNTEDManorCourtYard3",
+            ["10"] = "Reach_HAUNTEDManorKitchen",
+            ["11"] = "Flag_ManorKitchenTurkeyFound",
+            ["1"] = "Flag_Chapter3PlotDumped",
+            ["13"] = "Flag_DefeatSlasher",
+            ["3"] = "Flag_SleptInTrain",
+            ["2"] = "Reach_SeatCart",
+            ["19"] = "Flag_DefeatGreed",
+            ["18"] = "Reach_DreamPathA3a",
+            ["7"] = "Reach_ManorOutside",
+            ["6"] = "Reach_ManorStation",
+            ["25"] = "Flag_DefeatFear",
+            ["24"] = "Reach_DreamPathC3j",
+            ["27"] = "Reach_DreamPathD1",
+            ["26"] = "Reach_EVILDreamZoneHub",
+            ["20"] = "Reach_DreamPathB1",
+            ["21"] = "Reach_DreamPathB3c",
+            ["22"] = "Flag_DefeatAgony",
+            ["23"] = "Reach_DreamPathC1"
+        }
+    },
+    ["29"] = {
+        ["Location"] = "hatred",
+        ["Subtitle"] = "This tool could be very useful to avoid spikes...",
+        ["Title"] = "Unwrap the Heart."
+    },
+    ["World1"] = {
+        ["World1"] = {
+            ["1"] = "Reach_Cave0",
+            ["3"] = "Flag_BlackrockMayor",
+            ["2"] = "Reach_Roadtown",
+            ["5"] = "Weapons_Dynamite",
+            ["4"] = "Reach_SnowThicket1",
+            ["7"] = "Reach_BlackrockCastleLobby",
+            ["6"] = "Reach_BlackrockDungeons1",
+            ["9"] = "Reach_BlackrockKitchen",
+            ["8"] = "Flag_BlackrockChandelier",
+            ["13"] = "Flag_DefeatCruel King",
+            ["12"] = "Reach_BlackrockTreasureRoom",
+            ["11"] = "Weapons_Shrinking",
+            ["10"] = "Flag_BlackrockKitchenMessedUp"
+        }
+    },
+    ["1"] = {
+        ["Location"] = "East Bizville",
+        ["Subtitle"] = "This weird blue blob told me to",
+        ["Title"] = "Walk east to Roblox HQ"
+    },
+    ["3"] = {["Location"] = "Roblox HQ", ["Subtitle"] = "I hope he is okay", ["Title"] = "Talk to Shedletsky"},
+    ["2"] = {["Location"] = "Roblox HQ", ["Subtitle"] = "Ascend through ROBLOX HQ!", ["Title"] = "Find Shedletsky"},
+    ["19"] = {
+        ["Location"] = "greed",
+        ["Subtitle"] = "This tool could be very useful to avoid spikes...",
+        ["Title"] = "Confront your Desires"
+    },
+    ["4"] = {
+        ["Location"] = "East Bizville Planks",
+        ["Subtitle"] = "First time using a sword...",
+        ["Title"] = "Break the Planks outside HQ using a Sword"
+    },
+    ["7"] = {["Location"] = "Blackrock Castle", ["Subtitle"] = "Avoid the Guards!", ["Title"] = "Explore the Castle"},
+    ["6"] = {["Location"] = "Mount Blackrock", ["Subtitle"] = "Siege!", ["Title"] = "Enter the Blackrock Castle"},
+    ["25"] = {
+        ["Location"] = "fear",
+        ["Subtitle"] = "This tool could be very useful to avoid spikes...",
+        ["Title"] = "Kill your Fears"
+    },
+    ["8"] = {
+        ["Location"] = "Blackrock Castle",
+        ["Subtitle"] = "The attic is open...?",
+        ["Title"] = "Find a way around the Blockade"
+    },
+    ["27"] = {
+        ["Location"] = "evildreamhub",
+        ["Subtitle"] = "This tool could be very useful to avoid spikes...",
+        ["Title"] = "Fall into Darkness."
+    },
+    ["17"] = {
+        ["Location"] = "Big Tree Interior",
+        ["Subtitle"] = "Climb this big tree!",
+        ["Title"] = "Traverse the Sacred Hollow"
+    },
+    ["13"] = {
+        ["Location"] = "Blackrock Castle",
+        ["Subtitle"] = "This has to stop now!",
+        ["Title"] = "Confront the King"
+    },
+    ["21"] = {
+        ["Location"] = "pathb",
+        ["Subtitle"] = "This tool could be very useful to avoid spikes...",
+        ["Title"] = "Traverse your Mind"
+    },
+    ["11"] = {["Location"] = "Blackrock Castle", ["Subtitle"] = "", ["Title"] = "Reach the top of the messy pile"},
+    ["23"] = {
+        ["Location"] = "dreamhubfear",
+        ["Subtitle"] = "This tool could be very useful to avoid spikes...",
+        ["Title"] = "Give in to your Fears"
+    },
+    ["28"] = {
+        ["Location"] = "hatred",
+        ["Subtitle"] = "This tool could be very useful to avoid spikes...",
+        ["Title"] = "Find the Vein Center using Ghost Potion"
+    },
+    ["World2"] = {
+        ["World2"] = {
+            ["18"] = "Flag_DefeatBubonic Plant",
+            ["9"] = "Reach_JungleThicket1",
+            ["1"] = "Flag_Chapter2PlotDumped",
+            ["8"] = "Flag_GrieferRanAway",
+            ["3"] = "Reach_PlainsAirport",
+            ["2"] = "Reach_BizvilleAirport",
+            ["5"] = "Reach_AntHill1",
+            ["4"] = "Reach_Savana3",
+            ["7"] = "Reach_Turitopulis",
+            ["6"] = "Reach_Jungle1",
+            ["14"] = "Flag_DefeatBigfoot",
+            ["15"] = "Reach_Treepath4",
+            ["16"] = "Reach_Tree1",
+            ["17"] = "Reach_TreeBossRoom",
+            ["13"] = "Flag_FakeCH2SwordPickup",
+            ["12"] = "Flag_DefeatGriefer1",
+            ["11"] = "Weapons_Rocket Boots",
+            ["10"] = "Flag_DefeatKomodo Dragon"
+        }
+    },
+    ["World0"] = {
+        ["World0"] = {
+            ["1"] = "Reach_HQRoom1",
+            ["3"] = "Flag_GotWoodenSword",
+            ["2"] = "Reach_HQRoom5",
+            ["5"] = "Flag_ILearnedBP",
+            ["4"] = "Breakable_HQ0_1"
+        }
+    },
+    ["9"] = {["Location"] = "Blackrock Castle", ["Subtitle"] = "", ["Title"] = "Find a way to the Kitchen"},
+    ["30"] = {["Location"] = "Demo 3 Complete!", ["Subtitle"] = "???", ["Title"] = "Demo 4 Coming Soon..."},
+    ["18"] = {
+        ["Location"] = "Big Tree Interior",
+        ["Subtitle"] = "Save Mayor Thaniyel",
+        ["Title"] = "Confront the Griefer"
+    },
+    ["5"] = {["Location"] = "East Bizville", ["Subtitle"] = "Abilities??", ["Title"] = "Walk east to the Meadows"},
+    ["15"] = {
+        ["Location"] = "North Path",
+        ["Subtitle"] = "Keep watch for wild fruit!",
+        ["Title"] = "Traverse the path in the North"
+    },
+    ["14"] = {["Location"] = "MangoTree", ["Subtitle"] = "Retrieve the Iron Sword!", ["Title"] = "Defeat Bigfoot"},
+    ["24"] = {
+        ["Location"] = "pathc",
+        ["Subtitle"] = "This tool could be very useful to avoid spikes...",
+        ["Title"] = "Traverse your Mind"
+    },
+    ["16"] = {["Location"] = "Toucan", ["Subtitle"] = "Test your ears!", ["Title"] = "Solve the Toucan's puzzle"},
+    ["26"] = {
+        ["Location"] = "evildreampath",
+        ["Subtitle"] = "This tool could be very useful to avoid spikes...",
+        ["Title"] = "How Gullible."
+    },
+    ["20"] = {
+        ["Location"] = "dreamhubsolitude",
+        ["Subtitle"] = "This tool could be very useful to avoid spikes...",
+        ["Title"] = "Give in to Isolation"
+    },
+    ["12"] = {["Location"] = "Blackrock Castle", ["Subtitle"] = "", ["Title"] = "Find the Treasure Room"},
+    ["22"] = {
+        ["Location"] = "solitude",
+        ["Subtitle"] = "This tool could be very useful to avoid spikes...",
+        ["Title"] = "Confront your Insecurities"
+    },
+    ["10"] = {
+        ["Location"] = "Blackrock Castle",
+        ["Subtitle"] = "The attic is open...?",
+        ["Title"] = "Help out the Kitchen Wizard"
+    }
+}
