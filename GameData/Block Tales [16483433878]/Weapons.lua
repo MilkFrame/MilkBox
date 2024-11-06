8a14fdefc5ebac239713a95c5c851ada087f3781
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-07 01:22:02 +1100

    Create Weapons.lua

---
+++ GameData/Block Tales [16483433878]/Weapons.lua
@@ -0,0 +1,93 @@
+data = {
+    ["Ultraball"] = {
+        ["Icon"] = "rbxassetid://17502952050",
+        ["Description"] = "The attack power of your Superball increases!! On top of that, you can now do a Ground Pound by pressing JUMP again while in the air!",
+        ["Upgrade"] = true
+    },
+    ["Lantern"] = {
+        ["WhatDo"] = "Light",
+        ["Icon"] = "rbxassetid://17301540086",
+        ["Index"] = 6,
+        ["Description"] = "You can use this to discover hidden items and areas."
+    },
+    ["Venomshank"] = {["Icon"] = "rbxassetid://17301525947", ["Index"] = 2},
+    ["Rocket Boots"] = {
+        ["WhatDo"] = "Glide",
+        ["Icon"] = "rbxassetid://17301534010",
+        ["Index"] = 4,
+        ["Description"] = "You can use this to get across wide gaps."
+    },
+    ["Hyperball"] = {
+        ["Icon"] = "rbxassetid://17502952716",
+        ["Description"] = "The attack power of your Superball increases! And now you can do the Ground Pound by pressing JUMP again while in the air!",
+        ["Upgrade"] = true
+    },
+    ["Bottled Ship"] = {
+        ["WhatDo"] = "Ship",
+        ["Icon"] = "rbxassetid://17301531958",
+        ["Index"] = 7,
+        ["Description"] = "You can use this on a wooden post to get across waters."
+    },
+    ["Dynamite"] = {
+        ["WhatDo"] = "Ignite",
+        ["Icon"] = "rbxassetid://17301547724",
+        ["Index"] = 2,
+        ["Description"] = "You can now put down dynamite to destroy cracked walls or rocks!"
+    },
+    ["Iron Sword"] = {
+        ["Power"] = 2,
+        ["Description"] = "The attack power of your Sword increases! On top of that, you can now destroy dense vegetation!",
+        ["Icon"] = "rbxassetid://17502952561",
+        ["Upgrade"] = true
+    },
+    ["Sword"] = {["Icon"] = "rbxassetid://17301526934", ["Index"] = 0},
+    ["Wooden Sword"] = {
+        ["Power"] = 1,
+        ["Description"] = "You can do the Sword attack now! You can destroy wooden barricades with this Wooden Sword.",
+        ["Icon"] = "rbxassetid://17502951818",
+        ["Upgrade"] = true
+    },
+    ["Builderman"] = {["Icon"] = "rbxassetid://108265050", ["Index"] = 8},
+    ["Illumina"] = {["Icon"] = "rbxassetid://17301541415", ["Index"] = 7},
+    ["Ball"] = {
+        ["WhatDo"] = "Toss",
+        ["Icon"] = "rbxassetid://17301552597",
+        ["Index"] = 1,
+        ["Description"] = "Throw a superball to hit enemies or pick up far away items."
+    },
+    ["Hoverboard"] = {
+        ["WhatDo"] = "Hover",
+        ["Icon"] = "rbxassetid://17301542980",
+        ["Index"] = 8,
+        ["Description"] = "You can use this to fly over spikes or to just move around faster."
+    },
+    ["Ice Dagger"] = {["Icon"] = "rbxassetid://17301541683", ["Index"] = 1},
+    ["Superball"] = {
+        ["Power"] = 1,
+        ["Description"] = "Your standard superball. Can be used to pick up items from far away.",
+        ["Icon"] = "rbxassetid://17502952229",
+        ["Upgrade"] = true
+    },
+    ["Steel Sword"] = {
+        ["Power"] = 3,
+        ["Description"] = "The attack power of your Sword increases!! On top of that, you can now destroy metal barricades!",
+        ["Icon"] = "rbxassetid://17502952372",
+        ["Upgrade"] = true
+    },
+    ["Firebrand"] = {["Icon"] = "rbxassetid://17301546842", ["Index"] = 4},
+    ["Shrinking"] = {
+        ["WhatDo"] = "Shrink",
+        ["Icon"] = "rbxassetid://17301531756",
+        ["Index"] = 3,
+        ["Description"] = "You can now shrink yourself to enter tiny doors."
+    },
+    ["Windforce"] = {["Icon"] = "rbxassetid://17301525719", ["Index"] = 5},
+    ["Ghostwalker"] = {["Icon"] = "rbxassetid://17301544525", ["Index"] = 3},
+    ["Darkheart"] = {["Icon"] = "rbxassetid://17301550471", ["Index"] = 6},
+    ["Ghost Potion"] = {
+        ["WhatDo"] = "Potion",
+        ["Icon"] = "rbxassetid://17301544796",
+        ["Index"] = 5,
+        ["Description"] = "Hide yourself from enemies, go through fences and avoid damage."
+    }
+}
