230877e9296b71ec5f2aebf28746b1f3d167dbf5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:34:56 +1100

    Create PassiveList.lua

---
+++ GameData/King Legacy [4520749081]/PassiveList.lua
@@ -0,0 +1,222 @@
+data = {
+    ["Various"] = {
+        ["InfoTH"] = "เมื่อโจมตีศัตรู มีโอกาสที่จะสร้างความเสียหายได้หลายครั้ง",
+        ["Image"] = "rbxassetid://138394438327168",
+        ["Info"] = "When attacking an enemy, there is a chance of dealing damage multiple times.",
+        ["Tier"] = "Iconic",
+        ["AlwaysActive"] = true
+    },
+    ["Nightmare"] = {
+        ["InfoTH"] = "ความเสียหายทั้งหมดที่ได้รับเพิ่มขึ้น 10%",
+        ["Image"] = "rbxassetid://94718377244672",
+        ["Info"] = "All damage taken is increased by 10%.",
+        ["Tier"] = "Curse",
+        ["AlwaysActive"] = true
+    },
+    ["Sorrowspell"] = {
+        ["InfoTH"] = "คูลดาวน์สกิลนานขึ้น 10%",
+        ["Image"] = "rbxassetid://82120665403679",
+        ["Info"] = "The cooldown of all skills is 10% longer.",
+        ["Tier"] = "Curse",
+        ["AlwaysActive"] = true
+    },
+    ["Hyper Armor"] = {
+        ["InfoTH"] = "มีโอกาสได้รับ Hyper Armor เป็นเวลา 5 วินาที เมื่อได้รับความเสียหาย ลดความเสียหายที่ได้รับลง 4 เท่า",
+        ["Image"] = "rbxassetid://16857448955",
+        ["Info"] = "Has a chance to gain Hyper Armor for 5 seconds when damaged, Reduces damage taken by a factor of 4.",
+        ["Tier"] = "Iconic"
+    },
+    ["Ruin"] = {
+        ["InfoTH"] = "เพิ่มความเสียหายที่เกิดขึ้นกับผู้เล่น IncreasePlayerDamage",
+        ["Image"] = "rbxassetid://136430735036725",
+        ["Info"] = "Increases damage dealt to Player by IncreasePlayerDamage",
+        ["Tier"] = "Common",
+        ["PassiveBuff"] = {["IncreasePlayerDamage"] = {2, 4, 6, 8, 10}},
+        ["LevelRates"] = {500, 200, 100, 50, 25}
+    },
+    ["Blasting"] = {
+        ["InfoTH"] = "มีโอกาสสร้างระเบิดเล็กๆ น้อยๆ ใส่ศัตรูหลังจากโจมตี",
+        ["Image"] = "rbxassetid://106959061173514",
+        ["Info"] = "Has a chance to create a small explosion on the enemy after attacking.",
+        ["Tier"] = "Iconic",
+        ["AlwaysActive"] = true
+    },
+    ["Eternal"] = {
+        ["InfoTH"] = "เมื่อพลังชีวิตต่ำกว่า 15% เพิ่มความเสียหายผลไม้ 15% และลดความเสียหายที่ได้รับจากผลไม้ทั้งหมดลง 90%",
+        ["Image"] = "rbxassetid://16857439514",
+        ["Info"] = "When health falls below 15%, fruit damage increases, and damage received from fruits is reduced by 90%.",
+        ["Tier"] = "Celestial"
+    },
+    ["Wisdom"] = {
+        ["InfoTH"] = "เพิ่มค่าประสบการณ์ที่ดรอปจากภารกิจ IncreaseEXPDrop",
+        ["Image"] = "rbxassetid://105114325805453",
+        ["Info"] = "Increases EXP drops from Quest by IncreaseEXPDrop",
+        ["Tier"] = "Common",
+        ["PassiveBuff"] = {["IncreaseEXPDrop"] = {10, 20, 35, 50, 65}},
+        ["LevelRates"] = {500, 200, 100, 50, 25}
+    },
+    ["Dazzle"] = {
+        ["InfoTH"] = "มีโอกาสทำให้ศัตรูตาบอดเมื่อได้รับความเสียหาย",
+        ["Image"] = "rbxassetid://16857430900",
+        ["Info"] = "Has a chance to blind enemies upon dealing damage.",
+        ["Tier"] = "Iconic"
+    },
+    ["Swift"] = {
+        ["InfoTH"] = "หลังจากถูกตี ความเร็วของคุณจะเพิ่มขึ้น 200% เป็นเวลา 10 วินาที",
+        ["Image"] = "rbxassetid://111162213392334",
+        ["Info"] = "After being hit, your speed will increase by 200% for 10 seconds.",
+        ["Tier"] = "Iconic"
+    },
+    ["Sinister Hex"] = {
+        ["InfoTH"] = "ลดความเสียหายทั้งหมดของคุณลง 15%",
+        ["Image"] = "rbxassetid://134202890179441",
+        ["Info"] = "Decrease all damage by 15%.",
+        ["Tier"] = "Curse",
+        ["AlwaysActive"] = true
+    },
+    ["Sentinel"] = {
+        ["InfoTH"] = "เมื่อพลังชีวิตต่ำกว่า 15% ลดความเสียหายทั้งหมดลง 50%",
+        ["Image"] = "rbxassetid://16857461303",
+        ["Info"] = "When health falls below 15%, Reduce all damage by 50%.",
+        ["Tier"] = "Celestial"
+    },
+    ["Chance"] = {
+        ["InfoTH"] = "เพิ่มโอกาสได้รับไอเทมจาก NPC IncreaseDropRate",
+        ["Image"] = "rbxassetid://121427009756042",
+        ["Info"] = "Increases drop rates from NPCs by IncreaseDropRate",
+        ["Tier"] = "Common",
+        ["PassiveBuff"] = {["IncreaseDropRate"] = {5, 10, 15, 20, 25}},
+        ["LevelRates"] = {500, 200, 100, 50, 25}
+    },
+    ["Acropurged"] = {
+        ["InfoTH"] = "การเพิ่มความสามารถในการดูดซับพลังชีวิตของอาวุธ Acro",
+        ["Image"] = "rbxassetid://120574072630974",
+        ["Info"] = "Enhancement of the vitality-absorbing prowess of Acro weaponry",
+        ["Tier"] = "Iconic",
+        ["AlwaysActive"] = true
+    },
+    ["Luxuriant"] = {
+        ["InfoTH"] = "เพิ่มเงินที่ดรอปจากภารกิจ IncreaseMoneyDrop",
+        ["Image"] = "rbxassetid://122903708389679",
+        ["Info"] = "Increases $ Money drops from Quest by IncreaseMoneyDrop",
+        ["Tier"] = "Common",
+        ["PassiveBuff"] = {["IncreaseMoneyDrop"] = {10, 20, 35, 50, 65}},
+        ["LevelRates"] = {500, 200, 100, 50, 25}
+    },
+    ["Brawler"] = {
+        ["InfoTH"] = "เมื่อพลังชีวิตต่ำกว่า 15% เพิ่มความเสียหาย Melee 15% และลดความเสียหายที่ได้รับจาก Melee ทั้งหมดลง 90%",
+        ["Image"] = "rbxassetid://16857426894",
+        ["Info"] = "When health falls below 15%, melee damage increases, and damage received from melee is reduced by 90%.",
+        ["Tier"] = "Celestial"
+    },
+    ["Sprinter"] = {
+        ["InfoTH"] = "มีโอกาสเพิ่มความเร็วในการเคลื่อนที่ 400% เป็นเวลา 5 วินาทีหลังจากสร้างความเสียหาย",
+        ["Image"] = "rbxassetid://115095379415535",
+        ["Info"] = "Has a chance to increase movement speed by 400% for 5 seconds after dealing damage.",
+        ["Tier"] = "Iconic"
+    },
+    ["Ronin"] = {
+        ["InfoTH"] = "เมื่อพลังชีวิตต่ำกว่า 15% เพิ่มความเสียหายดาบ 15% และลดความเสียหายที่ได้รับจากดาบทั้งหมดลง 90%",
+        ["Image"] = "rbxassetid://16857457187",
+        ["Info"] = "When health falls below 15%, sword damage increases, and damage received from swords is reduced by 90%.",
+        ["Tier"] = "Celestial"
+    },
+    ["Reflect Strike"] = {
+        ["InfoTH"] = "สะท้อน 10% ของความเสียหายที่ได้รับ",
+        ["Image"] = "rbxassetid://99823444916061",
+        ["Info"] = "Reflects 10% of the damage received.",
+        ["Tier"] = "Celestial",
+        ["AlwaysActive"] = true
+    },
+    ["Nocturne"] = {
+        ["InfoTH"] = "มีโอกาสหลบการโจมตี หลังจากนั้นจะคงกระพันเป็นเวลา 1 วินาที คูลดาวน์ 30 วิ",
+        ["Image"] = "rbxassetid://138299182268461",
+        ["Info"] = "Has a chance to dodge attacks. Afterward, you will be invulnerable for 1 seconds. Cooldown: 30 seconds.",
+        ["Tier"] = "Celestial",
+        ["AlwaysActive"] = true
+    },
+    ["Difficult Healing"] = {
+        ["InfoTH"] = "การรักษาทั้งหมด รวมถึงการรักษาจากฟีนิกซ์หรือที่อื่น ๆ จะช้าลง 25%",
+        ["Image"] = "rbxassetid://125164378134248",
+        ["Info"] = "All heals, including those from Phoenix or others, are 25% slower.",
+        ["Tier"] = "Curse",
+        ["AlwaysActive"] = true
+    },
+    ["Tempest"] = {
+        ["InfoTH"] = "หลังจากโจมตีแล้ว จะเกิดพายุเล็กๆ ขึ้นรอบๆ ศัตรู ทำให้เกิดความเสียหายเล็กน้อย",
+        ["Image"] = "rbxassetid://80626210275958",
+        ["Info"] = "After attacking, a small storm will be created around the enemy, causing a slight amount of damage.",
+        ["Tier"] = "Iconic",
+        ["AlwaysActive"] = true
+    },
+    ["Skill Haste"] = {
+        ["InfoTH"] = "ลดคูลดาวน์ของทักษะทั้งหมด ReduceCooldown",
+        ["Image"] = "rbxassetid://105248433483357",
+        ["Info"] = "Reduces Cooldown of all abilities by ReduceCooldown",
+        ["Tier"] = "Common",
+        ["PassiveBuff"] = {["ReduceCooldown"] = {3, 7, 10, 12}},
+        ["LevelRates"] = {1000, 100, 10, 1}
+    },
+    ["Debility"] = {
+        ["InfoTH"] = "มีโอกาสทำให้ศัตรูติดสถานะอ่อนแอ ได้รับความเสียหายเพิ่มขึ้น 15% เป็นเวลา 10 วินาที เมื่อสร้างความเสียหาย",
+        ["Image"] = "rbxassetid://16857435849",
+        ["Info"] = "Has a chance to inflict a weak status on the enemy. Take 15% more damage for 10 seconds upon dealing damage.",
+        ["Tier"] = "Iconic"
+    },
+    ["Last Breath"] = {
+        ["InfoTH"] = "เมื่อตาย คุณจะได้รับพลังชีวิตคืน 50% ความสามารถนี้ใช้ได้เพียงครั้งเดียวเท่านั้น หากคุณตายอีกครั้ง คุณสามารถใช้มันได้อีกครั้ง",
+        ["Image"] = "rbxassetid://107642610926424",
+        ["Info"] = "When dying, you will regain 50% of your health. This ability can be used only once. If you die again, you can use it once more.",
+        ["Tier"] = "Iconic",
+        ["AlwaysActive"] = true
+    },
+    ["Critical Strikes"] = {
+        ["InfoTH"] = "ความเสียหายที่เกิดขึ้นมีโอกาสที่จะเป็นการโจมตีคริติคอล",
+        ["Image"] = "rbxassetid://118920660540611",
+        ["Info"] = "The damage dealt has a chance to be a critical hit.",
+        ["Tier"] = "Iconic",
+        ["AlwaysActive"] = true
+    },
+    ["Destruction"] = {
+        ["InfoTH"] = "เพิ่มความเสียหายที่เกิดขึ้นกับ NPC IncreaseNPCsDamage",
+        ["Image"] = "rbxassetid://140525864966411",
+        ["Info"] = "Increases damage dealt to NPCs by IncreaseNPCsDamage",
+        ["Tier"] = "Common",
+        ["PassiveBuff"] = {["IncreaseNPCsDamage"] = {4, 8, 12, 14, 16}},
+        ["LevelRates"] = {500, 200, 100, 50, 25}
+    },
+    ["Piercing"] = {
+        ["InfoTH"] = "เจาะเกราะ IncreasePiercingArmor",
+        ["Image"] = "rbxassetid://136702653770329",
+        ["Info"] = "Armor piercing by IncreasePiercingArmor",
+        ["Tier"] = "Common",
+        ["PassiveBuff"] = {["IncreasePiercingArmor"] = {3, 6, 9, 12, 15, 18, 21, 24}},
+        ["LevelRates"] = {500, 300, 100, 50, 30, 15, 8, 3}
+    },
+    ["Last Stand"] = {
+        ["InfoTH"] = "เมื่อสุขภาพต่ำกว่า 15% พลังโจมตีทั้งหมดจะเพิ่มขึ้นอย่างมาก",
+        ["Image"] = "rbxassetid://16857452268",
+        ["Info"] = "When health is below 15%, all attack power will greatly increase.",
+        ["Tier"] = "Iconic"
+    },
+    ["Stylish Heal"] = {
+        ["InfoTH"] = "เมื่อสุขภาพต่ำกว่า 50% จะฟื้นฟูเร็วขึ้นสองเท่า",
+        ["Image"] = "rbxassetid://16857487585",
+        ["Info"] = "When health is below 50%, it will be restored twice as fast.",
+        ["Tier"] = "Iconic"
+    },
+    ["Sword Sense"] = {
+        ["InfoTH"] = "เพิ่มความเสียหายของอาวุธที่เกิดขึ้น IncreaseSwordDamage",
+        ["Image"] = "rbxassetid://132189316624659",
+        ["Info"] = "Increases weapon damage dealt by IncreaseSwordDamage",
+        ["Tier"] = "Common",
+        ["PassiveBuff"] = {["IncreaseSwordDamage"] = {2, 4, 6, 8, 10}},
+        ["LevelRates"] = {500, 200, 100, 50, 25}
+    },
+    ["Healing Havoc"] = {
+        ["InfoTH"] = "มีโอกาสทำให้ศัตรูฟื้นฟูพลังชีวิตช้าลง เป็นเวลา 10 วินาที เมื่อได้รับความเสียหาย",
+        ["Image"] = "rbxassetid://16857445493",
+        ["Info"] = "Has a chance to make enemies regenerate their health slowly for 10 seconds upon dealing damage.",
+        ["Tier"] = "Iconic"
+    }
+}
