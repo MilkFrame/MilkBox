7db7bc07df6be4f98e87ed0de53b4a10d2d6bec0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:36:30 +1100

    Create WorldsId.lua

---
+++ GameData/King Legacy [4520749081]/WorldsId.lua
@@ -0,0 +1,14 @@
+data = {
+    ["KingLegacy"] = {
+        ["FirstSea"] = 4520749081,
+        ["SecondSea"] = 6381829480,
+        ["ThirdSea"] = 15759515082,
+        ["GoldenArena"] = 5931540094
+    },
+    ["Testing"] = {
+        ["FirstSea"] = 4618180932,
+        ["SecondSea"] = 6238744946,
+        ["ThirdSea"] = 11633603782,
+        ["GoldenArena"] = 5482460322
+    }
+}
