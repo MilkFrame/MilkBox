a525aca7b9953ab5aa98350f8b1503e23c92c22c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:33:00 +1100

    Create TipsTable.lua

---
+++ GameData/King Legacy [4520749081]/TipsTable.lua
@@ -0,0 +1,11 @@
+data = {
+    "Eating Legacy Fruit grants special powers. It appears randomly every 1 - 2 Hours and disappears after 15 minutes",
+    "You can follow game updates or report bugs on the King Legacy Discord.",
+    "You can submit your creative ideas to the Mods in the King Legacy Discord.",
+    "You can obtain the 'YouTuber' title If you have more than 5,000 followers on your YouTube channel and post content about King Legacy.",
+    "Sky Jump is air jumping, a special ability that can be gained every 50 levels (with a maximum of 15)",
+    "You can unlock the passive ability by talking to the passive tree. It occurs every night and disappears in the morning.",
+    "You can remove fruit powers from NPCs named Wizard",
+    "You can defeat bosses to gain a bounty of up to 3 million",
+    "You can enter the code to redeem your reward by opening the Shop menu and scrolling to the bottom."
+}
