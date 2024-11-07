b4a2c915a59e88821657bcc3caeb34513e65529b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:33:12 +1100

    Create AreaMusic.lua

---
+++ GameData/King Legacy [4520749081]/AreaMusic.lua
@@ -0,0 +1,73 @@
+data = {
+    ["ZombieIsland"] = {["IslandName"] = "Zombie Island", ["Music"] = "rbxassetid://5396368095"},
+    ["ShredEndangering"] = {["IslandName"] = "Shred Endangering", ["Music"] = "rbxassetid://9339658306"},
+    ["The Unearthly"] = {["IslandName"] = "The Unearthly", ["Music"] = "rbxassetid://16023057383"},
+    ["SantaFactory"] = {["IslandName"] = "Santa Factory", ["Music"] = "rbxassetid://8469149771"},
+    ["Torrefacio"] = {["IslandName"] = "Torrefacio", ["Music"] = "rbxassetid://6363439539"},
+    ["Tundra"] = {["IslandName"] = "Snow Island", ["Music"] = "rbxassetid://5396307073"},
+    ["Carcer"] = {["IslandName"] = "Carcer", ["Music"] = "rbxassetid://6363583003"},
+    ["SkullIsland"] = {["IslandName"] = "Skull Island", ["Music"] = "rbxassetid://6618218719"},
+    ["Luma Grove"] = {["IslandName"] = "Luma Grove", ["Music"] = "rbxassetid://5396290920"},
+    ["Fiore"] = {["IslandName"] = "Fiore", ["Music"] = "rbxassetid://12124100810"},
+    ["Fons Land"] = {["IslandName"] = "Fons Land", ["Music"] = "rbxassetid://6363591393"},
+    ["Fishland"] = {["IslandName"] = "Fish Island", ["Music"] = "rbxassetid://5544556243"},
+    ["LobbyIsland"] = {["IslandName"] = "Lobby Island", ["Music"] = "rbxassetid://5396382262"},
+    ["SoldierHeadquater"] = {["IslandName"] = "Soldier Headquater", ["Music"] = "rbxassetid://11320925197"},
+    ["HorrorPumpkin"] = {["IslandName"] = "Horror Pumpkin", ["Music"] = "rbxassetid://7527513174"},
+    ["DeadTundra"] = {["IslandName"] = "Dead Tundra", ["Music"] = "rbxassetid://6871997803"},
+    ["SoldierIsland"] = {["IslandName"] = "Soldier Island", ["Music"] = "rbxassetid://5396153626"},
+    ["WarIsland"] = {["IslandName"] = "War Island", ["Music"] = "rbxassetid://5396430663"},
+    ["PirateIsland"] = {["IslandName"] = "Pirate Island", ["Music"] = "rbxassetid://5396128864"},
+    ["Bubble"] = {["IslandName"] = "Bubble Island", ["Music"] = "rbxassetid://5396290920"},
+    ["PirateSkullIsland"] = {["IslandName"] = "Pirate Skull Island", ["Music"] = "rbxassetid://11320910258"},
+    ["HydraIslandArea"] = {
+        ["RainSound"] = "rbxassetid://9064263922",
+        ["IslandName"] = "Hydra Island",
+        ["SoundIdList"] = {
+            "rbxassetid://11327866919",
+            "rbxassetid://11327868132",
+            "rbxassetid://11327868888",
+            "rbxassetid://11327869767",
+            "rbxassetid://11327870594",
+            "rbxassetid://11327871392",
+            "rbxassetid://11327872785",
+            "rbxassetid://11327873861",
+            "rbxassetid://11327875305"
+        }
+    },
+    ["SkyIsland"] = {["IslandName"] = "Sky Island", ["Music"] = "rbxassetid://5396273425"},
+    ["Forgotten Coliseum"] = {["IslandName"] = "Forgotten Coliseum", ["Music"] = "rbxassetid://85485967543396"},
+    ["The Shallow"] = {["IslandName"] = "The Shallow", ["Music"] = "rbxassetid://16991688895"},
+    ["Sea"] = {["IslandName"] = "Sea", ["Music"] = "rbxassetid://9167507448"},
+    ["Sea of dust"] = {["IslandName"] = "Desert Island", ["Music"] = "rbxassetid://5396625790"},
+    ["Drakenhold Fortress"] = {["IslandName"] = "Drakenhold Fortress", ["Music"] = "rbxassetid://114945344245042"},
+    ["StarterIsland"] = {["IslandName"] = "Starter Island", ["Music"] = "rbxassetid://5395843188"},
+    ["Floresco"] = {["IslandName"] = "Floresco", ["Music"] = "rbxassetid://6363486195"},
+    ["LegacyIslandArea"] = {
+        ["IslandName"] = "Legacy Island",
+        ["SoundIdList"] = {
+            "rbxassetid://6862895603",
+            "rbxassetid://6862898255",
+            "rbxassetid://6862898799",
+            "rbxassetid://6862899276",
+            "rbxassetid://6862908240"
+        }
+    },
+    ["LoafIsland"] = {["IslandName"] = "Loaf Island", ["Music"] = "rbxassetid://7527386290"},
+    ["RainyStone"] = {
+        ["RainSound"] = "rbxassetid://9064263922",
+        ["Music"] = "rbxassetid://5396454358",
+        ["IslandName"] = "Rocks"
+    },
+    ["StoneArena"] = {["IslandName"] = "Stone Arena", ["Music"] = "rbxassetid://5796845156"},
+    ["MirrorRoom"] = {["IslandName"] = "Mirror Room", ["Music"] = "rbxassetid://7527513174"},
+    ["Viridans"] = {["IslandName"] = "Viridans", ["Music"] = "rbxassetid://6363594271"},
+    ["HalloweenIsland"] = {
+        ["Music"] = "rbxassetid://7527513174",
+        ["IslandName"] = "Halloween Island",
+        ["CustomFog"] = {["FogStart"] = 50, ["FogColor"] = Color3.fromRGB(0, 170, 0), ["FogEnd"] = 1000}
+    },
+    ["HibernusLand"] = {["IslandName"] = "Hibernus Land", ["Music"] = "rbxassetid://6364001315"},
+    ["SharkIsland"] = {["IslandName"] = "Shark Island", ["Music"] = "rbxassetid://5396179187"},
+    ["ChefShip"] = {["IslandName"] = "Chef Ship", ["Music"] = "rbxassetid://5396280001"}
+}
