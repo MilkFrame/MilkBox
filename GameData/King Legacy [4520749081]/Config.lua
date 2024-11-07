6e7806ea5dcae8a8476c2fff2cf635b52e2f0e3e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:32:21 +1100

    Create Config.lua

---
+++ GameData/King Legacy [4520749081]/Config.lua
@@ -0,0 +1,116 @@
+data = {
+    ["X100KSuccess"] = {
+        ["StartAt"] = "1730419200000",
+        ["Rewards"] = {["statreset"] = true},
+        ["EndAt"] = "1732924800000"
+    },
+    ["PvPArenaFun"] = {["StartAt"] = "1729555200000", ["Rewards"] = {["statreset"] = true}, ["EndAt"] = "1738281600000"},
+    ["LegendarySwordsUnlocked"] = {
+        ["StartAt"] = "1729555200000",
+        ["Rewards"] = {["Gem"] = 5},
+        ["EndAt"] = "1735603200000"
+    },
+    ["Update7.1"] = {["StartAt"] = "1730419200000", ["Rewards"] = {["Gem"] = 25}, ["EndAt"] = "1730851200000"},
+    ["WELCOMETOKINGLEGACY"] = {["Rewards"] = {["FREEX2EXP"] = 30}},
+    ["FruitAwakeningTime"] = {
+        ["StartAt"] = "1729555200000",
+        ["Rewards"] = {["Key"] = {["KeyName"] = "Copper Key", ["Amt"] = 10}},
+        ["EndAt"] = "1732924800000"
+    },
+    ["Halloween2024"] = {
+        ["StartAt"] = "1730419200000",
+        ["Rewards"] = {["Halloween2024"] = true},
+        ["EndAt"] = "1731110400000"
+    },
+    ["SKFreeGems"] = {["Requirement"] = "function: 0xf74f8fd1477072a3", ["Rewards"] = {["Gem"] = 25}},
+    ["TelekinesisFruitPower"] = {
+        ["StartAt"] = "1729555200000",
+        ["Rewards"] = {["Key"] = {["KeyName"] = "Iron Key", ["Amt"] = 1}},
+        ["EndAt"] = "1732924800000"
+    },
+    ["InfernoKingAwaits"] = {
+        ["StartAt"] = "1729555200000",
+        ["Rewards"] = {["FREEX2EXP"] = 15},
+        ["EndAt"] = "1732924800000"
+    },
+    ["Peodiz"] = {["Rewards"] = {["beli"] = 100000}},
+    ["IslandQuestRepeat"] = {
+        ["StartAt"] = "1729555200000",
+        ["Rewards"] = {["Key"] = {["KeyName"] = "Copper Key", ["Amt"] = 10}},
+        ["EndAt"] = "1732924800000"
+    },
+    ["SKGames"] = {["Requirement"] = "function: 0x4b1cd569382e3cc3", ["Rewards"] = {["FREEX2EXP"] = 30}},
+    ["DinoxLive"] = {["Rewards"] = {["beli"] = 100000}},
+    ["RainbowDragon"] = {["Requirement"] = "function: 0x560e0db7917716a3", ["Rewards"] = {["Gem"] = 100}},
+    ["Update7Release"] = {
+        ["StartAt"] = "1729555200000",
+        ["Rewards"] = {["FREEX2EXP"] = 15},
+        ["EndAt"] = "1732924800000"
+    },
+    ["OwnerMeteor"] = {["StartAt"] = "1729900800000", ["Rewards"] = {["statreset"] = true}, ["EndAt"] = "1730592000000"},
+    ["QuakeTsunamiBoost"] = {
+        ["StartAt"] = "1729555200000",
+        ["Rewards"] = {["Rewards"] = {["statreset"] = true}},
+        ["EndAt"] = "1735603200000"
+    },
+    ["EpicAdventure"] = {
+        ["StartAt"] = "1729555200000",
+        ["Rewards"] = {["statreset"] = true},
+        ["EndAt"] = "1740700800000"
+    },
+    ["DragonColorRefund"] = {["Rewards"] = {["DragonRefundGem"] = 50}},
+    ["SeaAdventure"] = {["StartAt"] = "1729555200000", ["Rewards"] = {["FREEX2EXP"] = 15}, ["EndAt"] = "1738281600000"},
+    ["2MFAV"] = {["Rewards"] = {["statreset"] = true}},
+    ["Dinosaur100kFollowers"] = {["Rewards"] = {["statreset"] = true}},
+    ["FREESTATSRESET"] = {["Rewards"] = {["statreset"] = true}},
+    ["<3LEEPUNGG"] = {["Rewards"] = {["FREEX2EXP"] = 30}},
+    ["FREE30MINUTESEXP"] = {["Rewards"] = {["FREEX2EXP"] = 30}},
+    ["PassiveMaster"] = {
+        ["StartAt"] = "1729555200000",
+        ["Rewards"] = {["PassiveBook"] = {["BookType"] = "Fate Book", ["Amount"] = 1}},
+        ["EndAt"] = "1732924800000"
+    },
+    ["ThirdSeaAdventures"] = {
+        ["StartAt"] = "1729555200000",
+        ["Rewards"] = {["Gem"] = 25},
+        ["Requirement"] = "function: 0xb3fb7804ea05d203",
+        ["EndAt"] = "1735603200000"
+    },
+    ["MaterialHunter"] = {
+        ["StartAt"] = "1729555200000",
+        ["Rewards"] = {["statreset"] = true},
+        ["EndAt"] = "1740700800000"
+    },
+    ["MysticAccessoryFind"] = {["StartAt"] = "1729555200000", ["Rewards"] = {["Gem"] = 5}, ["EndAt"] = "1735603200000"},
+    ["AwakenTheBuddha"] = {
+        ["StartAt"] = "1729555200000",
+        ["Key"] = {["KeyName"] = "Gold Key", ["Amt"] = 1},
+        ["EndAt"] = "1740700800000"
+    },
+    ["GetYourLuck"] = {
+        ["StartAt"] = "1729555200000",
+        ["Rewards"] = {["Key"] = {["KeyName"] = "Gold Key", ["Amt"] = 1}},
+        ["EndAt"] = "1738281600000"
+    },
+    ["BossSlayer"] = {["StartAt"] = "1729555200000", ["Rewards"] = {["FREEX2EXP"] = 15}, ["EndAt"] = "1740700800000"},
+    ["KrakensRevenge"] = {
+        ["StartAt"] = "1729555200000",
+        ["Rewards"] = {["FREEX2EXP"] = 15},
+        ["EndAt"] = "1740700800000"
+    },
+    ["DragonColorsUnlocked"] = {
+        ["StartAt"] = "1729555200000",
+        ["Rewards"] = {["Key"] = {["KeyName"] = "Gold Key", ["Amt"] = 1}},
+        ["EndAt"] = "1735603200000"
+    },
+    ["PlayKingLegacyFor5Gems"] = {
+        ["StartAt"] = "1729555200000",
+        ["Rewards"] = {["Gem"] = 5},
+        ["EndAt"] = "1738281600000"
+    },
+    ["KioruV2PowerUp"] = {
+        ["StartAt"] = "1729555200000",
+        ["Rewards"] = {["FREEX2EXP"] = 15},
+        ["EndAt"] = "1738281600000"
+    }
+}
