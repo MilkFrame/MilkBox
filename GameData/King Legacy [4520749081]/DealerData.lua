6f665a4be3e1365a53a8faa2766a32beb6afcd13
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:36:43 +1100

    Create DealerData.lua

---
+++ GameData/King Legacy [4520749081]/DealerData.lua
@@ -0,0 +1,10 @@
+data = {
+    ["BuyStacks"] = {["Legendary"] = 1, ["Common"] = 15, ["Epic"] = 1, ["Uncommon"] = 8, ["Rare"] = 5},
+    ["Prices"] = {
+        ["Legendary"] = {["Value"] = 100, ["Type"] = "Gem"},
+        ["Common"] = {["Value"] = 10000, ["Type"] = "Beli"},
+        ["Epic"] = {["Value"] = 30, ["Type"] = "Gem"},
+        ["Uncommon"] = {["Value"] = 25000, ["Type"] = "Beli"},
+        ["Rare"] = {["Value"] = 5, ["Type"] = "Gem"}
+    }
+}
