c14ee2da5050dab183bed429a31781872ccdb338
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:31:57 +1100

    Create Awake.lua

---
+++ GameData/King Legacy [4520749081]/Awake.lua
@@ -0,0 +1,30 @@
+data = {
+    ["IceIce"] = {["IceC"] = 125, ["IceZ"] = 15, ["IceX"] = 75, ["IceV"] = 175, ["IceE"] = 140},
+    ["GravityGravity"] = {["GravityB"] = 50, ["GravityC"] = 20, ["GravityZ"] = 10, ["GravityX"] = 15, ["GravityV"] = 30},
+    ["LoveLove"] = {["LoveV"] = 111, ["LoveC"] = 55, ["LoveX"] = 33, ["LoveZ"] = 11},
+    ["BombBomb"] = {["BombZ"] = 5, ["BombX"] = 15, ["BombE"] = 10, ["BombC"] = 25, ["BombV"] = 50},
+    ["FlameFlame"] = {["FlameX"] = 75, ["FlameZ"] = 25, ["FlameV"] = 150, ["FlameC"] = 100, ["FlameE"] = 50},
+    ["LightLight"] = {["LightZ"] = 15, ["LightE"] = 140, ["LightX"] = 75, ["LightV"] = 170, ["LightC"] = 150},
+    ["BuddhaBuddha"] = {["BuddhaZ"] = 5, ["BuddhaX"] = 35, ["BuddhaE"] = 100, ["BuddhaV"] = 200, ["BuddhaC"] = 75},
+    ["OpOp"] = {["OpV"] = 120, ["OpE"] = 200, ["OpB"] = 50, ["OpX"] = 25, ["OpC"] = 100, ["OpZ"] = 5},
+    ["DoughDough"] = {
+        ["DoughB"] = 222,
+        ["DoughE"] = 69,
+        ["DoughV"] = 144,
+        ["DoughX"] = 49,
+        ["DoughZ"] = 9,
+        ["DoughC"] = 99
+    },
+    ["SnowSnow"] = {["SnowZ"] = 15, ["SnowX"] = 35, ["SnowV"] = 140, ["SnowE"] = 65, ["SnowC"] = 70},
+    ["DarkDark"] = {["DarkB"] = 150, ["DarkC"] = 95, ["DarkV"] = 125, ["DarkZ"] = 10, ["DarkX"] = 70},
+    ["VenomVenom"] = {
+        ["VenomC"] = 20,
+        ["VenomZ"] = 5,
+        ["VenomX"] = 15,
+        ["VenomB"] = 75,
+        ["VenomV"] = 30,
+        ["VenomE"] = 40
+    },
+    ["QuakeQuake"] = {["QuakeZ"] = 20, ["QuakeB"] = 175, ["QuakeX"] = 50, ["QuakeV"] = 150, ["QuakeC"] = 100},
+    ["MagmaMagma"] = {["MagmaE"] = 140, ["MagmaX"] = 75, ["MagmaZ"] = 15, ["MagmaV"] = 140, ["MagmaC"] = 125}
+}
