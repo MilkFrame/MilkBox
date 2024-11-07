6fdc3a8b4dcb77b707f4ea3617a61aa38a4e8808
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:32:47 +1100

    Create SpecialStatusList.lua

---
+++ GameData/King Legacy [4520749081]/SpecialStatusList.lua
@@ -0,0 +1,55 @@
+data = {
+    ["Anti Jump"] = {["Info"] = "Unable to jump.", ["InfoTH"] = "ไม่สามารถกระโดดได้"},
+    ["Freezing"] = {
+        ["Info"] = "Reduces enemy speed by 200% for 6 seconds.",
+        ["InfoTH"] = "ลดความเร็วศัตรูลง 200% เป็นเวลา 6 วินาที"
+    },
+    ["Super Safe Zone"] = {
+        ["Info"] = "Super Safe Zone: Upon entering, combat status is immediately canceled, and no battles can take place in this area.",
+        ["InfoTH"] = "สุดยอดโซนปลอดภัย : เมื่อเข้ามาแล้ว สถานะการต่อสู้จะถูกยกเลิกทันที และการต่อสู้จะไม่สามารถเกิดขึ้นได้ในพื้นที่นี้"
+    },
+    ["Dizzy"] = {
+        ["Info"] = "Causes you to be stunned and unable to move for a moment.",
+        ["InfoTH"] = "ทำให้คุณติดสตันและไม่สามารถขยับตัวได้ชั่วขณะ"
+    },
+    ["Gale"] = {
+        ["Info"] = "Increase running speed, boost recovery rate, recover faster.",
+        ["InfoTH"] = "เพิ่มความเร็วในการวิ่ง เพิ่มอัตราการฟื้นตัว ฟื้นตัวได้เร็วขึ้น"
+    },
+    ["Danger"] = {
+        ["Info"] = "You are in combat. Exiting the game will result in losing your bounty.",
+        ["InfoTH"] = "คุณกำลังอยู่ในการต่อสู้ การออกเกมจะทำให้คุณเสียค่าหัวตัวเอง"
+    },
+    ["Buddha Orb"] = {
+        ["Info"] = "Buddha's special ability lasts for 30 seconds. Buddha's orb reduces all damage by a factor of 5",
+        ["InfoTH"] = "ความสามารถพิเศษของบุ้ดด้าปรากฏ 30 วินาที ลูกแก้วบุ้ดด้าจะช่วยลดดาเมททุกอย่างลง 5 เท่า"
+    },
+    ["PvP Disabled"] = {
+        ["Info"] = "You were just killed. Your PVP is disabled for 40 minutes. During this time, you cannot be attacked by any players (PVP can be re-enabled in settings).",
+        ["InfoTH"] = "คุณพึ่งถูกสังหาร PVP ของคุณถูกปิด 40 นาที ระหว่างนี้คุณจะไม่ถูกการโจมตีใดๆจากผู้เล่น (สามารถเปิด PVP ได้ในการตั้งค่า)"
+    },
+    ["Safe Zone"] = {
+        ["Info"] = "Safe zone: no fighting can take place in this area.",
+        ["InfoTH"] = "โซนปลอดภัย : การต่อสู้จะไม่สามารถเกิดขึ้นได้ในพื้นที่นี้"
+    },
+    ["Legacy Fruit"] = {
+        ["Info"] = "Legacy Fruits spawn on the map every 1 - 2 Hours / Despawn 15 Minutes.",
+        ["InfoTH"] = "ผลไม้วิเศษเกิดในแมพทุก 1-2 ชั่วโมง / คงอยู่ 15 นาที"
+    },
+    ["Conqueror"] = {
+        ["Info"] = "Creates a wide-area energy wave that stuns enemies for 5 seconds (does not work on enemies with higher bounties).",
+        ["InfoTH"] = "สร้างคลื่นพลังวงกว้างทำให้ศัตรูสตันเป็นเวลา 5 วินาที (ไร้ผลกับศัตรูที่มีค่าหัวมากกว่า)"
+    },
+    ["AntiHeal"] = {
+        ["Info"] = "Temporarily unable to use any recovery skills.",
+        ["InfoTH"] = "ไม่สามารถฟื้นฟูพลังชีวิตได้ชั่วคราว"
+    },
+    ["Teleport"] = {
+        ["Info"] = "Allows you to teleport a short distance.",
+        ["InfoTH"] = "ช่วยให้คุณสามารถเทเลพอร์ตระยะทางสั้น ๆ ได้"
+    },
+    ["Burning"] = {
+        ["Info"] = "Receive fire damage gradually over time.",
+        ["InfoTH"] = "ได้รับความเสียหายไฟไม้ตามระยะเวลาที่กำหนด"
+    }
+}
