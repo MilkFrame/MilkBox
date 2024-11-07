cc9d8243e8763a4cb831bdb9462fffd8d92038fc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:35:38 +1100

    Create CharacterPassiveRates.lua

---
+++ GameData/King Legacy [4520749081]/CharacterPassiveRates.lua
@@ -0,0 +1,42 @@
+data = {
+    ["Chronicles Lore"] = {
+        ["Level"] = 4000,
+        ["Celestial"] = {1, 50},
+        ["Chance"] = 100,
+        ["Common"] = 7,
+        ["IconicRate"] = 100,
+        ["Curse"] = {1, 300},
+        ["Iconic"] = {1, 4},
+        ["ChanceOrder"] = 0.75
+    },
+    ["Fate Book"] = {
+        ["Level"] = 1,
+        ["Celestial"] = {1, 1000},
+        ["Chance"] = 100,
+        ["Common"] = 4,
+        ["IconicRate"] = 1000,
+        ["Curse"] = {1, 7},
+        ["Iconic"] = {1, 100},
+        ["ChanceOrder"] = 0
+    },
+    ["Essence Book"] = {
+        ["Level"] = 1000,
+        ["Celestial"] = {1, 500},
+        ["Chance"] = 100,
+        ["Common"] = 5,
+        ["IconicRate"] = 500,
+        ["Curse"] = {1, 25},
+        ["Iconic"] = {1, 50},
+        ["ChanceOrder"] = 0.2
+    },
+    ["Fortune Tales"] = {
+        ["Level"] = 2250,
+        ["Celestial"] = {1, 100},
+        ["Chance"] = 100,
+        ["Common"] = 6,
+        ["IconicRate"] = 250,
+        ["Curse"] = {1, 100},
+        ["Iconic"] = {1, 10},
+        ["ChanceOrder"] = 0.5
+    }
+}
