6e389395b1aff1715721d2fc88383483e4e57699
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:29:09 +1100

    Create DFAwakeList.lua

---
+++ GameData/King Legacy [4520749081]/DFAwakeList.lua
@@ -0,0 +1,16 @@
+data = {
+    ["IceIce"] = "IceFruit",
+    ["GravityGravity"] = "GravityFruit",
+    ["LoveLove"] = "LoveFruit",
+    ["BombBomb"] = "BombFruit",
+    ["FlameFlame"] = "FlameFruit",
+    ["LightLight"] = "LightFruit",
+    ["BuddhaBuddha"] = "BuddhaFruit",
+    ["OpOp"] = "OpFruit",
+    ["DoughDough"] = "DoughFruit",
+    ["SnowSnow"] = "SnowFruit",
+    ["DarkDark"] = "DarkFruit",
+    ["VenomVenom"] = "VenomFruit",
+    ["QuakeQuake"] = "QuakeFruit",
+    ["MagmaMagma"] = "MagmaFruit"
+}
