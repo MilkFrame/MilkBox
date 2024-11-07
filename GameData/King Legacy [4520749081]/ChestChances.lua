d25e969b380380d6bec9d25c35972f4b6cdb46ba
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:31:27 +1100

    Create ChestChances.lua

---
+++ GameData/King Legacy [4520749081]/ChestChances.lua
@@ -0,0 +1,8 @@
+data = {
+    ["Platinum Key"] = {["Legendary"] = 2.5, ["Common"] = 5, ["Epic"] = 37.5, ["Uncommon"] = 15, ["Rare"] = 40},
+    ["Copper Key"] = {["Legendary"] = 0.05, ["Common"] = 60, ["Epic"] = 1, ["Uncommon"] = 30, ["Rare"] = 8.95},
+    ["Iridium Key"] = {["Legendary"] = 35, ["Common"] = 1, ["Epic"] = 45, ["Uncommon"] = 4, ["Rare"] = 15},
+    ["Iron Key"] = {["Legendary"] = 0.1, ["Common"] = 35, ["Epic"] = 2.5, ["Uncommon"] = 45, ["Rare"] = 17.4},
+    ["Gold Key"] = {["Legendary"] = 0.5, ["Common"] = 25, ["Epic"] = 12, ["Uncommon"] = 35, ["Rare"] = 27.5},
+    ["Diamond Key"] = {["Legendary"] = 10, ["Common"] = 2, ["Epic"] = 45, ["Uncommon"] = 8, ["Rare"] = 35}
+}
