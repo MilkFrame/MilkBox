5ba559fb899523b4faa36036d0b10035b6981b89
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:31:12 +1100

    Create DFTier.lua

---
+++ GameData/King Legacy [4520749081]/DFTier.lua
@@ -0,0 +1,42 @@
+data = {
+    ["Legendary"] = {"DoughFruit", "DragonFruit", "PhoenixFruit", "ToyFruit", "GateFruit"},
+    ["Common"] = {"BombFruit", "SpikeFruit", "SpinFruit"},
+    ["Epic"] = {
+        "FlameFruit",
+        "GasFruit",
+        "IceFruit",
+        "LightFruit",
+        "MagmaFruit",
+        "QuakeFruit",
+        "RumbleFruit",
+        "SnowFruit",
+        "SpiritFruit",
+        "MagnetFruit",
+        "GoldFruit"
+    },
+    ["Uncommon"] = {
+        "BarrierFruit",
+        "LoveFruit",
+        "PawFruit",
+        "ShadowFruit",
+        "SmokeFruit",
+        "StringFruit",
+        "GiraffeFruit",
+        "WolfFruit",
+        "HumanFruit"
+    },
+    ["Rare"] = {
+        "AlloFruit",
+        "BrachioFruit",
+        "BuddhaFruit",
+        "DarkFruit",
+        "GravityFruit",
+        "GumFruit",
+        "MammothFruit",
+        "OpFruit",
+        "SandFruit",
+        "SpinoFruit",
+        "VenomFruit",
+        "LeopardFruit"
+    }
+}
