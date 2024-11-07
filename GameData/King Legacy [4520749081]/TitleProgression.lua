638df1cefd8b9374adf89d89a68f174793597c3b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:35:24 +1100

    Create TitleProgression.lua

---
+++ GameData/King Legacy [4520749081]/TitleProgression.lua
@@ -0,0 +1,90 @@
+data = {
+    ["Chaos Kraken Hunter"] = {
+        {["Progress"] = 7, ["Title"] = "Krakenbane"},
+        {["Progress"] = 70, ["Title"] = "The Abyssal Reaper"},
+        {["Progress"] = 175, ["Title"] = "Serpent Slayer"},
+        {["Progress"] = 350, ["Title"] = "Fangs of the Deep"},
+        {["Progress"] = 700, ["Title"] = "Leviathan’s Bane"},
+        ["MaxProgress"] = 100
+    },
+    ["Bounty Hunter"] = {
+        {["Progress"] = 20, ["Title"] = "Rookie Bounty Hunter"},
+        {["Progress"] = 75, ["Title"] = "Player Reaper"},
+        {["Progress"] = 150, ["Title"] = "Bounty Seeker"},
+        {["Progress"] = 300, ["Title"] = "Killmonger"},
+        {["Progress"] = 1000, ["Title"] = "Slayer Supreme"},
+        {["Progress"] = 2000, ["Title"] = "PvP Overlord"},
+        {["Progress"] = 3000, ["Title"] = "PK Chronicles"},
+        {["Progress"] = 5000, ["Title"] = "Soul Reaper"},
+        {["Progress"] = 7500, ["Title"] = "Savage Strike"},
+        {["Progress"] = 10000, ["Title"] = "Deathbrand"},
+        {["Progress"] = 20000, ["Title"] = "Death's Avatar"},
+        ["MaxProgress"] = 2000
+    },
+    ["Beginner’s Dawn"] = {
+        {["Progress"] = 1, ["Title"] = "Trial Conqueror"},
+        {["Progress"] = 10, ["Title"] = "Wavestrider"},
+        {["Progress"] = 25, ["Title"] = "Raid Leader"},
+        {["Progress"] = 50, ["Title"] = "Fighting Titan"},
+        {["Progress"] = 100, ["Title"] = "Champion of the Arena"},
+        ["MaxProgress"] = 100
+    },
+    ["Sea King Hunter"] = {
+        {["Progress"] = 1, ["Title"] = "Deepstrike"},
+        {["Progress"] = 25, ["Title"] = "Sea King Exterminator"},
+        {["Progress"] = 75, ["Title"] = "Sea King's Nightmare"},
+        {["Progress"] = 150, ["Title"] = "Oceanic Reaper"},
+        ["MaxProgress"] = 150
+    },
+    ["Royal Sea Hunter"] = 100,
+    ["Drakenfyr the Inferno King Hunter"] = {
+        {["Progress"] = 1, ["Title"] = "Dragonbane"},
+        {["Progress"] = 10, ["Title"] = "Inferno Stalker"},
+        {["Progress"] = 25, ["Title"] = "Wyrmscourge"},
+        {["Progress"] = 50, ["Title"] = "Sky Reaper"},
+        {["Progress"] = 100, ["Title"] = "Ember Devourer"},
+        ["MaxProgress"] = 100
+    },
+    ["Trial of the Chosen"] = {
+        {["Progress"] = 1, ["Title"] = "Ultimate Survivor"},
+        {["Progress"] = 10, ["Title"] = "Warden of the Fallen"},
+        {["Progress"] = 25, ["Title"] = "Guardian of Redemption"},
+        {["Progress"] = 50, ["Title"] = "Lord of Hardships"},
+        {["Progress"] = 100, ["Title"] = "Avatar of Chaos"},
+        ["MaxProgress"] = 100
+    },
+    ["Deepsea Crusher Hunter"] = {
+        {["Progress"] = 1, ["Title"] = "Shellbreaker"},
+        {["Progress"] = 10, ["Title"] = "Clawbane Hunter"},
+        {["Progress"] = 25, ["Title"] = "Claw Reaper"},
+        {["Progress"] = 50, ["Title"] = "Terror of the Tides"},
+        {["Progress"] = 100, ["Title"] = "Crustacean Crusher"},
+        ["MaxProgress"] = 100
+    },
+    ["Treasure Hunter"] = {
+        {["Progress"] = 20, ["Title"] = "Novice Treasure Hunter"},
+        {["Progress"] = 200, ["Title"] = "Treasure Hunter Elite"},
+        ["MaxProgress"] = 200
+    },
+    ["Abyssal Tyrant Hunter"] = {
+        {["Progress"] = 1, ["Title"] = "Tyrant Slayer"},
+        {["Progress"] = 10, ["Title"] = "Abyss Breaker"},
+        {["Progress"] = 25, ["Title"] = "Depth Stalker"},
+        {["Progress"] = 50, ["Title"] = "Shadow Fang"},
+        {["Progress"] = 100, ["Title"] = "Tyrant's Eclipse"},
+        ["MaxProgress"] = 100
+    },
+    ["Challenger’s Path"] = {
+        {["Progress"] = 1, ["Title"] = "Gladiator"},
+        {["Progress"] = 10, ["Title"] = "Savage Warrior"},
+        {["Progress"] = 25, ["Title"] = "Nocturnal Knight"},
+        {["Progress"] = 50, ["Title"] = "Ragebringer"},
+        {["Progress"] = 100, ["Title"] = "Battlelord"},
+        ["MaxProgress"] = 100
+    },
+    ["Hydra Hunter"] = {
+        {["Progress"] = 1, ["Title"] = "Serpent's End"},
+        {["Progress"] = 35, ["Title"] = "Legendary Hydra Slayer"},
+        ["MaxProgress"] = 35
+    }
+}
