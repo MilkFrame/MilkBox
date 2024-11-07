47c8e80621f7ad11f4f0d7ab26959f0ab2f97539
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:27:55 +1100

    Create TierColor.lua

---
+++ GameData/King Legacy [4520749081]/TierColor.lua
@@ -0,0 +1,13 @@
+data = {
+    ["Exclusive"] = Color3.fromRGB(255, 255, 127),
+    ["Common"] = Color3.fromRGB(191, 191, 191),
+    ["Exotic"] = Color3.fromRGB(85, 85, 255),
+    ["Uncommon"] = Color3.fromRGB(110, 220, 0),
+    ["Rare"] = Color3.fromRGB(170, 255, 255),
+    ["Legendary"] = Color3.fromRGB(230, 0, 0),
+    ["Limited"] = Color3.fromRGB(255, 255, 0),
+    ["Epic"] = Color3.fromRGB(170, 0, 255),
+    ["Divine"] = Color3.fromRGB(255, 255, 0),
+    ["Collectible"] = Color3.fromRGB(241, 138, 36),
+    ["Mythical"] = Color3.fromRGB(255, 0, 255)
+}
