cb4d90701441faf50acf666a71f5102e82c5b632
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:30:44 +1100

    Create ArmamentColorList.lua

---
+++ GameData/King Legacy [4520749081]/ArmamentColorList.lua
@@ -0,0 +1,13 @@
+data = {
+    ["Carmine"] = {["Color"] = Color3.fromRGB(255, 0, 0), ["Rate"] = 275},
+    ["Lime"] = {["Color"] = Color3.fromRGB(85, 255, 0), ["Rate"] = 405},
+    ["Apricot"] = {["Color"] = Color3.fromRGB(255, 85, 0), ["Rate"] = 170},
+    ["Arc"] = {["Color"] = Color3.fromRGB(0, 0, 0), ["Rate"] = 1},
+    ["Indigo"] = {["Color"] = Color3.fromRGB(85, 0, 255), ["Rate"] = 170},
+    ["Ultramarine"] = {["Color"] = Color3.fromRGB(0, 0, 255), ["Rate"] = 200},
+    ["Green"] = {["Color"] = Color3.fromRGB(0, 255, 0), ["Rate"] = 400},
+    ["Azure"] = {["Color"] = Color3.fromRGB(0, 255, 255), ["Rate"] = 200},
+    ["Yellow"] = {["Color"] = Color3.fromRGB(255, 255, 0), ["Rate"] = 400},
+    ["Fuchia"] = {["Color"] = Color3.fromRGB(255, 0, 255), ["Rate"] = 275},
+    ["White"] = {["Color"] = Color3.fromRGB(255, 255, 255), ["Rate"] = 500}
+}
