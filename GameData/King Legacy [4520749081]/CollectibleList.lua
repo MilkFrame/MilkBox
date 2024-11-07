71d8f88fc695d6938527e7249be4e8d2c0702a35
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:37:09 +1100

    Create CollectibleList.lua

---
+++ GameData/King Legacy [4520749081]/CollectibleList.lua
@@ -0,0 +1,401 @@
+data = {
+    ["WolfWolf"] = {
+        ["InfoTH"] = "กลายร่างเป็นสัตว์ร้ายใต้จันทรา ด้วยกรงเล็บของมันต่างพาศัตรูหวาดกลัว",
+        ["Image"] = "rbxassetid://16741880397",
+        ["Name"] = "Permanent Wolf",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Morphs into the full moon beast, its flawless claws terrifying enemies."
+    },
+    ["SpiritSpirit"] = {
+        ["InfoTH"] = "อัญเชิญสิ่งศักดิ์สิทธิ์มาคุ้มกัน พร้อมเรียกสายฟ้าพิโรธปัดเป่าศัตรูจากภัยอันตรายทั้งปวง",
+        ["Image"] = "rbxassetid://16532809931",
+        ["Name"] = "Permanent Spirit",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Summons god's essence, brings a thunderstorm, and protects from danger."
+    },
+    ["Refund Stats"] = {
+        ["InfoTH"] = "สิ่งที่ต้องการมากที่สุดในหมู่นักผจญภัย วัตถุที่จะรีเซ็ทจำนวนพลังใหม่อีกครั้ง",
+        ["Image"] = "rbxassetid://15178390904",
+        ["Name"] = "Refund Stats",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "The most wanted item,that will reset your stats back!"
+    },
+    ["SnowSnow"] = {
+        ["InfoTH"] = "พลังหิมะมอบอำนาจให้ผู้ใช้เรียก พายุแห่งความหนาวเหน็บจู่โจมใส่ศัตรู",
+        ["Image"] = "rbxassetid://16827004958",
+        ["Name"] = "Permanent Snow",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Grants its user the ability to summon a snowstorm to harm enemies."
+    },
+    ["StringString"] = {
+        ["InfoTH"] = "นฤมิตด้ายที่คมกริบฟาดฟันใส่ศัตรู หรือ กวัดแกว่งไปทั่วอาณาจักร ",
+        ["Image"] = "rbxassetid://16532803798",
+        ["Name"] = "Permanent String",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Create a sharp string to slash at enemies or be used to swing across the realm."
+    },
+    ["GoldGold"] = {
+        ["InfoTH"] = "มอบพลังแห่งความมั่งคั่งให้ผู้ใช้สามารถที่จะคุมพลังแห่งสุวรรณทำร้ายศัตรูได้!",
+        ["Image"] = "rbxassetid://16532811501",
+        ["Name"] = "Permanent Gold",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Grants control over gold resources, dealing damage to enemies."
+    },
+    ["BuddhaBuddha"] = {
+        ["InfoTH"] = "เมื่อสิ่งศักดิ์สิทธ์หลอมรวมเป็นหนึ่ง ทำให้ผู้ใช้มีพละกำลังมหาศาล และต้านทานต่อศัตรู",
+        ["Image"] = "rbxassetid://16741874300",
+        ["Name"] = "Permanent Buddha",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Infused with divine energy, strengthens user and resists incoming damage."
+    },
+    ["DropX2"] = {
+        ["InfoTH"] = "เพิ่มจำนวนการได้ของสองเท่าภายใต้การผจญภัยในโลกานี้",
+        ["Image"] = "http://www.roblox.com/asset/?id=6886677613",
+        ["Name"] = "2x Drop Item",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Double your item or miscellaneous drop points beyond this Realm!"
+    },
+    ["ToyToy"] = {
+        ["InfoTH"] = "มอบพลังแห่งการควบคุมสิ่งสร้างสรรค์ และอสูรเครื่องเล่นทำร้ายศัตรูได้!",
+        ["Image"] = "rbxassetid://16532807987",
+        ["Name"] = "Permanent Toy",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Manipulate toys and creative items,And summon a feral beast to deal damage to enemies."
+    },
+    ["x2 EXP 15 MINS"] = {
+        ["InfoTH"] = "ได้รับค่าประสบการณ์ x2 เป็นเวลา 15 นาที",
+        ["Image"] = "rbxassetid://15119630573",
+        ["Name"] = "15 Mins",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Double your experience points for 15 minutes"
+    },
+    ["DragonDragon"] = {
+        ["InfoTH"] = "จำแลงกายเป็นสัตว์ในตำนาน และ พร้อมเผยพลังทีแท้จริงออกมา",
+        ["Image"] = "rbxassetid://16741887011",
+        ["Name"] = "Permanent Dragon",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Embodies the essence of a mythical creature, ready to unleash its might."
+    },
+    ["GumGum"] = {
+        ["InfoTH"] = "พลังยืดหยุ่น สามารถทำให้ผู้ใช้ยืดหด และ ใช้ร่างกายของตนปะทุทำร้ายต่อศัตรู",
+        ["Image"] = "rbxassetid://16285494876",
+        ["Name"] = "Permanent Rubber",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Grants flexibility and deals physical damage to enemies."
+    },
+    ["DarkDark"] = {
+        ["InfoTH"] = "พลังแห่งความมืด ใครก็ตามที่ใช้จะสามารถบัญชากาฬพลังได้",
+        ["Image"] = "rbxassetid://16396936495",
+        ["Name"] = "Permanent Dark",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Holds the essence of dark energy, granting control over darkness."
+    },
+    ["BeliX2"] = {
+        ["InfoTH"] = "เพิ่มจำนวนค่าเงิน (Beli) สองเท่าที่ได้จากการผจญภัยในโลกานี้!",
+        ["Image"] = "http://www.roblox.com/asset/?id=7253764575",
+        ["Name"] = "2x MONEY",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Double your in-game currency (Beli) beyond the Legacy Realm"
+    },
+    ["RumbleRumble"] = {
+        ["InfoTH"] = "มอบอำนาจพลังปล่อยคลื่นสายฟ้าฟาดใส่ศัตรู ให้แก่ผู้ที่ถือครองพลังนี้",
+        ["Image"] = "rbxassetid://16396953088",
+        ["Name"] = "Permanent Rumble",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Grants the ability to summon lightning strikes against enemies."
+    },
+    ["ExpX2"] = {
+        ["InfoTH"] = "เพิ่มจำนวนค่าประสบการณ์สองเท่าที่ได้จากการผจญภัยในโลกานี้!",
+        ["Image"] = "",
+        ["Name"] = "2x EXP",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Double your experience points beyond the Legacy Realm"
+    },
+    ["GateGate"] = {
+        ["InfoTH"] = "มอบพลังแห่งห้วงมิติ ฝ่าฟันศัตรูด้วยอำนาจกระชากมิติ!",
+        ["Image"] = "rbxassetid://125770398103486",
+        ["Name"] = "Permanent Gate",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Grants power to twist dimensions and deal damage to enemies."
+    },
+    ["Race Rerolls"] = {
+        ["InfoTH"] = "ไอเท็มพิเศษที่สุ่มเผ่าพันธุ์ของคุณได้อีกครั้ง!",
+        ["Image"] = "rbxassetid://11756679358",
+        ["Name"] = "Race Rerolls",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Race-changing items for your character."
+    },
+    ["OpOp"] = {
+        ["InfoTH"] = "มอบอำนาจให้ผู้ใช้สร้างอาณาเขตพร้อมพละเหนืออำนาจภายใต้รูมแห่งนี้",
+        ["Image"] = "rbxassetid://16536570939",
+        ["Name"] = "Permanent Control",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Allows its user to become powerful within the summoned area."
+    },
+    ["SpinSpin"] = {
+        ["InfoTH"] = "พลังแห่งใบพัด เรียกกังหันรอบตัวผู้ใช้ โบยบินแสนไกลเหนือเวหา",
+        ["Image"] = "rbxassetid://16532803364",
+        ["Name"] = "Permanent Spin",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Summons propeller around its user, granting them the ability to fly above the air."
+    },
+    ["SpikeSpike"] = {
+        ["InfoTH"] = "ที่ทะลวงศัตรูด้วยนิ่มหนามที่มาจากการจำแลงกายเป็นหนามมฤตยู",
+        ["Image"] = "rbxassetid://16532806908",
+        ["Name"] = "Permanent Spike",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Spike Power thrusts enemies with sharp force through the morph of Spike's spin."
+    },
+    ["GiraffeGiraffe"] = {
+        ["InfoTH"] = "พลังที่มอบการแปลงกายให้กับผู้ที่ใช้มัน อย่าได้ดูถูกความแข็งแกร่งของมัน!",
+        ["Image"] = "rbxassetid://16751612717",
+        ["Name"] = "Permanent Giraffe",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Morph into the creature from beyond, but beware its strength"
+    },
+    ["VenomVenom"] = {
+        ["InfoTH"] = "กระจายพิษร้าย พร้อมอำนาจพิษสงของไฮดร้า เอาชนะศัตรูได้ท่วงทัน",
+        ["Image"] = "rbxassetid://16532802679",
+        ["Name"] = "Permanent Venom",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Spreads poison, giving the user an edge against nearby enemies"
+    },
+    ["QuakeQuake"] = {
+        ["InfoTH"] = "พลังสั่นสะท้านโลกา สะเทือนพื้นดินก่อให้เกิดการทำลายอันน่าสะพรึงต่อศัตรู",
+        ["Image"] = "rbxassetid://16532807481",
+        ["Name"] = "Permanent Quake",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Stumbles across the land, causing the ground to quake and dealing damage to enemies."
+    },
+    ["HumanHuman"] = {
+        ["InfoTH"] = "พลังเหนือมนุษย์ที่เปลี่ยนร่างกายได้หลายประเภท พร้อมต่อสู้ และ ปรับกลยุทธ์เพื่อใช้ปราบศัตรู",
+        ["Image"] = "rbxassetid://140685087720266",
+        ["Name"] = "Permanent Telekinesis",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Enables shapeshifting for battle tactics, rotating to defeat enemies."
+    },
+    ["FruitNotifier"] = {
+        ["InfoTH"] = "อุปกรณ์ที่อำนวยความสะดวกที่เผยตำแหน่งของผลไม้มนตราภายใต้อาณาจักรแห่งนี้",
+        ["Image"] = "rbxassetid://6593613450",
+        ["Name"] = "Fruit Position",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Handheld device revealing exact locations of mythical fruits in this realm."
+    },
+    ["GravityGravity"] = {
+        ["InfoTH"] = "พลังที่มอบอำนาจให้ผู้ใช้ควบคุมกฏของแรงโน้ม ที่สามารถต่อกรกับศัตรูได้",
+        ["Image"] = "rbxassetid://16532809481",
+        ["Name"] = "Permanent Gravity",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Lets the user manipulate gravity, posing harm to enemies."
+    },
+    ["CoffinBoat"] = {
+        ["InfoTH"] = "ภาหนะที่ถูกขับเคลื่อนโดยนักดาบในตำนาน",
+        ["Image"] = "rbxassetid://6593666192",
+        ["Name"] = "Coffin Boat",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "A legendary boat sailed by the NightBlade wielder."
+    },
+    ["x2 EXP 4 HOURS"] = {
+        ["InfoTH"] = "ได้รับค่าประสบการณ์ x2 เป็นเวลา 4 ชั่วโมง",
+        ["Image"] = "rbxassetid://15119632862",
+        ["Name"] = "4 Hours",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Double your experience points for 4 Hours"
+    },
+    ["x2 EXP 24 HOURS"] = {
+        ["InfoTH"] = "ได้รับค่าประสบการณ์ x2 เป็นเวลา 24 ชั่วโมง",
+        ["Image"] = "rbxassetid://15119635421",
+        ["Name"] = "24 Hours",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Unlock the Ultimate Double your experienec points for 24 Epic Hours!"
+    },
+    ["LightLight"] = {
+        ["InfoTH"] = "พลังแห่งแสง ใครก็ตามที่ครอบครองสามารถนำ 'ภา' เปล่งอำนาจทำร้ายศัตรู",
+        ["Image"] = "rbxassetid://10475433638",
+        ["Name"] = "Permanent Light",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "A sacred force from beyond. Control it to summon light and deal damage to enemies"
+    },
+    ["MagnetMagnet"] = {
+        ["InfoTH"] = "มอบพลังให้ผู้ใช้ควบคุมขั้วอำนาจแม่เหล็กต่อกรกับศัตรู",
+        ["Image"] = "rbxassetid://16532811028",
+        ["Name"] = "Permanent Magnet",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Manipulates iron and magnets to harm enemies by controlling polarity."
+    },
+    ["MammothMammoth"] = {
+        ["InfoTH"] = "จำแลงเป็นสัตว์ตำนานที่สามารถใช้งวงสะบัด และ กำลังอันมหึมาของมันทำร้ายศัตรูได้",
+        ["Image"] = "rbxassetid://16751616002",
+        ["Name"] = "Permanent Mammoth",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Morph into mythical creatures, Swing a powerful trunk to deal damage to enemies."
+    },
+    ["SmokeSmoke"] = {
+        ["InfoTH"] = "มอบพลังให้ผู้ใช้ควบคุมกลุ่มควัน วางแผนต่อกรกับศัตรู โดยใช้ควันเหล่านี้",
+        ["Image"] = "rbxassetid://16333777320",
+        ["Name"] = "Permanent Smoke",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Allows smoke manipulation and mastery of deception, using vapor to harm enemies."
+    },
+    ["GasGas"] = {
+        ["InfoTH"] = "สร้างความเสียหายโดยปรับเปลี่ยนชั้นบรรยากาศกลายเป็นอาวุธที่อนุภาคทำลายล้างใส่ศัตรู",
+        ["Image"] = "rbxassetid://16333769732",
+        ["Name"] = "Permanent Gas",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Grants the user power to turn air into a highly destructive weapon against enemies."
+    },
+    ["x2 EXP 1 HOUR"] = {
+        ["InfoTH"] = "ได้รับค่าประสบการณ์ x2 เป็นเวลา 1 ชั่วโมง",
+        ["Image"] = "rbxassetid://15119631882",
+        ["Name"] = "1 Hour",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Double your experience points for 1 Hour"
+    },
+    ["BrachioBrachio"] = {
+        ["InfoTH"] = "พลังแบรคิโอซอรัสกลายร่างผู้ใช้เป็นไดโนเสาร์ในตำนาน ทำลายศัตรูด้วยพลังอันมหาศาล",
+        ["Image"] = "rbxassetid://16741885101",
+        ["Name"] = "Permanent Brachio",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Morphs user into the legendary beast, dealing heavy damage to enemies."
+    },
+    ["SpinoSpino"] = {
+        ["InfoTH"] = "กลายเป็นไดโนเสาร์ดุร้ายในตำนาน พร้อมศักยภาพทำลายล้างแค่ตวัดกรงเล็บน่าเกรงขามของมัน",
+        ["Image"] = "rbxassetid://16994236809",
+        ["Name"] = "Permanent Spino",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Morphs user into legendary beast, dealing high damage with terrifying scratches."
+    },
+    ["FruitStorage"] = {
+        ["InfoTH"] = "อุปกรณ์เก็บผลพลังที่จำเป็นมากที่สุดสำหรับนักผจญภัย",
+        ["Image"] = "rbxassetid://12702002594",
+        ["Name"] = "+1 Fruit Storage",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Perfect fruit storage for adventurous essentials."
+    },
+    ["BarrierBarrier"] = {
+        ["InfoTH"] = "พลังแห่งเกราะ อัญเชิญสิ่งที่ต้านทานทุกการโจมตีจากศัตรูที่กำลังเผชิญเข้ามา",
+        ["Image"] = "rbxassetid://16532809012",
+        ["Name"] = "Permanent Barrier",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Summons an unbreakable barrier to prevent dangerous attacks from enemies."
+    },
+    ["ShadowShadow"] = {
+        ["InfoTH"] = "หลังแห่งเงา แฝงตัวอยู่ในความมืดมิดรอคอยศัตรู พร้อมประเดิมพลังทมิฬพุ่งใส่",
+        ["Image"] = "rbxassetid://16532805311",
+        ["Name"] = "Permanent Shadow",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Utilizes shadows to lurk near enemies and attack them with darkness."
+    },
+    ["AlloAllo"] = {
+        ["InfoTH"] = "กลายร่างผู้ใช้เป็นหนึ่งในไดในเสาร์อันดุร้าย เพียงแค่คำรามก็ทำให้ศัตรูขวัญหนีดีฝ่อทันที",
+        ["Image"] = "rbxassetid://16741877883",
+        ["Name"] = "Permanent Allo",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Morphs its user into a feral beast, and just a single roar can make enemies flee."
+    },
+    ["LegacyPose"] = {
+        ["InfoTH"] = "เข็มทิศลึกลับที่ระบุตำแหน่งพื้นที่สำคัญต่างๆ",
+        ["Image"] = "http://www.roblox.com/asset/?id=6887227363",
+        ["Name"] = "Legacy Pose",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Mythical compass coordinates key locations."
+    },
+    ["LoveLove"] = {
+        ["InfoTH"] = "พลังแห่งรัก ไม่ได้ทำให้เจ็บปวด มีแต่จะนำพาสันติสุขมาให้ เว้นแต่ว่า..เอ็งคือศัตรูของข้า",
+        ["Image"] = "rbxassetid://16536574385",
+        ["Name"] = "Permanent Love",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "The Lover power brings love, causing no harm, only peace, unless you are the enemy."
+    },
+    ["PassiveBag"] = {
+        ["InfoTH"] = "กระเป๋าทรงพลังที่ช่วยนักผจญภัยจัดสรร และ รังสรรค์ศาสตรา",
+        ["Image"] = "rbxassetid://135246594264496",
+        ["Name"] = "+1 Slot (Passive)",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Superior bag that helps an adventurer sort their abilities."
+    },
+    ["FlameFlame"] = {
+        ["InfoTH"] = "มอบพลังเผาไหม้ศัตรูด้วยความร้อนจากนรก, ตำนานกล่าวไว้ว่าอำนาจมันเหนือขึ้นได้มากกว่านี้",
+        ["Image"] = "rbxassetid://16396939585",
+        ["Name"] = "Permanent Flame",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Scorches enemies with intense heat, legends speak of its awakening"
+    },
+    ["DoughDough"] = {
+        ["InfoTH"] = "แปรสภาพผู้ใช้เข้ากับธาตุได้ทุกสถานการณ์ ป้องกันการโจมตีจากศัตรูได้ทุกรูปแบบ",
+        ["Image"] = "rbxassetid://16285492663",
+        ["Name"] = "Permanent Dough",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Transforms its user into all elements, preventing physical attacks from enemies"
+    },
+    ["SandSand"] = {
+        ["InfoTH"] = "พลังแห่งทราย ทำให้ผู้ใช้สามารถควบคุมพลังของมันต่อกรกับศัตรู",
+        ["Image"] = "rbxassetid://16333781215",
+        ["Name"] = "Permanent Sand",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Grants the ability to control sand against enemies."
+    },
+    ["PhoenixPhoenix"] = {
+        ["InfoTH"] = "แปลงเป็นสัตว์ในตำนาน เยียวยาบาดแผล พร้อมพลังทำลายล้างด้วยเปลวเพลิงพุ่งใส่ศัตรู",
+        ["Image"] = "rbxassetid://16532810524",
+        ["Name"] = "Permanent Phoenix",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Embodying the mythical creature, heals wounds and unleashes fire flames on enemies."
+    },
+    ["x2 EXP 12 HOURS"] = {
+        ["InfoTH"] = "ได้รับค่าประสบการณ์ x2 เป็นเวลา 12 ชั่วโมง",
+        ["Image"] = "rbxassetid://15119634161",
+        ["Name"] = "12 Hours",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Double your experience points for 12 Hours"
+    },
+    ["PawPaw"] = {
+        ["InfoTH"] = "พลังแห่งกรงเล็บ สามารถอัญเชิญอุ้งมือสรรพสัตว์มาทำร้ายศัตรู",
+        ["Image"] = "rbxassetid://16532805669",
+        ["Name"] = "Permanent Paw",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "The Paw power can summon the palm of a creature to attack enemies."
+    },
+    ["MagmaMagma"] = {
+        ["InfoTH"] = "พลังแห่งแม็กม่า ไม่เพียงอันตรายที่เผาไหม้ศัตรู แต่หลอมละลายพวกมันด้วยเช่นกัน",
+        ["Image"] = "rbxassetid://16396954571",
+        ["Name"] = "Permanent Magma",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "An intensely burning force that scorches and melts enemies."
+    },
+    ["Conqueror"] = {
+        ["InfoTH"] = "สยบศัตรูด้วยทักษะจิตอันอาฆาตถึงกับอัมพาตได้ชั่วพริบตา",
+        ["Image"] = "http://www.roblox.com/asset/?id=9161806270",
+        ["Name"] = "Conqueror",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Legends speak of paralyzing enemies with mind power."
+    },
+    ["IceIce"] = {
+        ["InfoTH"] = "พลังเยือกเย็น ที่มอบอำนาจให้ผู้ใช้ควบคุมพลังอันหนาวเหน็บต่อกรกับศัตรูได้อย่างเย็นชา",
+        ["Image"] = "rbxassetid://16396951670",
+        ["Name"] = "Permanent Ice",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Control over the coldest essence, dealing damage to enemies."
+    },
+    ["NightBlade"] = {
+        ["InfoTH"] = "ศาสตราวุธของบุคคลในตำนาน",
+        ["Image"] = "http://www.roblox.com/asset/?id=9331992927",
+        ["Name"] = "Night Blade",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "A legendary noble pirate blade."
+    },
+    ["BombBomb"] = {
+        ["InfoTH"] = "พลังระเบิดที่จะมอบอำนาจแห่งการทำลายล้างศัตรูให้แก่ผู้ที่ใช้มัน",
+        ["Image"] = "rbxassetid://16532804466",
+        ["Name"] = "Permanent Bomb",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Grants the user destructive ability to eliminate enemies."
+    },
+    ["LeopardLeopard"] = {
+        ["InfoTH"] = "แปลงร่างผู้ใช้กลายเป็นเสือดาว มอบพละกำลังกำหราบศัตรูได้อย่างบ้าคลั่ง",
+        ["Image"] = "rbxassetid://16741889623",
+        ["Name"] = "Permanent Leopard",
+        ["Tier"] = "Exclusive",
+        ["Info"] = "Transforms the user into a leopard, dealing feral damage to enemies in this realm."
+    }
+}
