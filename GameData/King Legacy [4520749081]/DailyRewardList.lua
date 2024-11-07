93cfb71148893f4b70076a436be3ef09e077307f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:30:31 +1100

    Create DailyRewardList.lua

---
+++ GameData/King Legacy [4520749081]/DailyRewardList.lua
@@ -0,0 +1,37 @@
+data = {
+    ["Day1"] = {
+        ["Fruit"] = {["Legendary"] = 1, ["Common"] = 99, ["Epic"] = 1, ["Uncommon"] = 1, ["Rare"] = 1},
+        ["Gem"] = {1, 2},
+        ["Material"] = {["Legendary"] = 1, ["Common"] = 99, ["Epic"] = 1, ["Uncommon"] = 1, ["Rare"] = 1}
+    },
+    ["Day6"] = {
+        ["Fruit"] = {["Legendary"] = 2, ["Common"] = 99, ["Epic"] = 35, ["Uncommon"] = 99, ["Rare"] = 99},
+        ["Gem"] = {10, 20},
+        ["Material"] = {["Legendary"] = 2, ["Common"] = 99, ["Epic"] = 35, ["Uncommon"] = 99, ["Rare"] = 99}
+    },
+    ["Day3"] = {
+        ["Fruit"] = {["Legendary"] = 1, ["Common"] = 99, ["Epic"] = 1, ["Uncommon"] = 99, ["Rare"] = 99},
+        ["Gem"] = {3, 5},
+        ["Material"] = {["Legendary"] = 1, ["Common"] = 99, ["Epic"] = 1, ["Uncommon"] = 99, ["Rare"] = 99}
+    },
+    ["Day4"] = {
+        ["Fruit"] = {["Legendary"] = 1, ["Common"] = 99, ["Epic"] = 10, ["Uncommon"] = 99, ["Rare"] = 99},
+        ["Gem"] = {5, 8},
+        ["Material"] = {["Legendary"] = 1, ["Common"] = 99, ["Epic"] = 10, ["Uncommon"] = 99, ["Rare"] = 99}
+    },
+    ["Day5"] = {
+        ["Fruit"] = {["Legendary"] = 1, ["Common"] = 99, ["Epic"] = 25, ["Uncommon"] = 99, ["Rare"] = 99},
+        ["Gem"] = {8, 10},
+        ["Material"] = {["Legendary"] = 1, ["Common"] = 99, ["Epic"] = 25, ["Uncommon"] = 99, ["Rare"] = 99}
+    },
+    ["Day7"] = {
+        ["Fruit"] = {["Legendary"] = 25, ["Common"] = 99, ["Epic"] = 99, ["Uncommon"] = 99, ["Rare"] = 99},
+        ["Gem"] = {20, 30},
+        ["Material"] = {["Legendary"] = 25, ["Common"] = 99, ["Epic"] = 99, ["Uncommon"] = 99, ["Rare"] = 99}
+    },
+    ["Day2"] = {
+        ["Fruit"] = {["Legendary"] = 1, ["Common"] = 99, ["Epic"] = 1, ["Uncommon"] = 99, ["Rare"] = 1},
+        ["Gem"] = {2, 3},
+        ["Material"] = {["Legendary"] = 1, ["Common"] = 99, ["Epic"] = 1, ["Uncommon"] = 99, ["Rare"] = 1}
+    }
+}
