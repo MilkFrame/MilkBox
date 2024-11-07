47a9fac608ff49537ea10d79c1eb99d808944b1a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:32:35 +1100

    Create RankUpgradeList.lua

---
+++ GameData/King Legacy [4520749081]/RankUpgradeList.lua
@@ -0,0 +1,12 @@
+data = {
+    {["InfoTH"] = "ระดับ 1", ["Image"] = "rbxassetid://117231277022601", ["Info"] = "Rank 1"},
+    {["InfoTH"] = "ระดับ 2", ["Image"] = "rbxassetid://72837590058216", ["Info"] = "Rank 2"},
+    {["InfoTH"] = "ระดับ 3", ["Image"] = "rbxassetid://139017874075919", ["Info"] = "Rank 3"},
+    {["InfoTH"] = "ระดับ 4", ["Image"] = "rbxassetid://139496447199931", ["Info"] = "Rank 4"},
+    {["InfoTH"] = "ระดับ 5", ["Image"] = "rbxassetid://120443313191875", ["Info"] = "Rank 5"},
+    {["InfoTH"] = "ระดับ 6", ["Image"] = "rbxassetid://107910202040488", ["Info"] = "Rank 6"},
+    {["InfoTH"] = "ระดับ 7", ["Image"] = "rbxassetid://93770568663791", ["Info"] = "Rank 7"},
+    {["InfoTH"] = "ระดับ 8", ["Image"] = "rbxassetid://120934924901228", ["Info"] = "Rank 8"},
+    {["InfoTH"] = "ระดับ 9", ["Image"] = "rbxassetid://139095042836941", ["Info"] = "Rank 9"},
+    {["InfoTH"] = "ระดับ 10", ["Image"] = "rbxassetid://103156493772836", ["Info"] = "Rank 10"}
+}
