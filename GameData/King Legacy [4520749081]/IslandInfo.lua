b9c5575928e6c3a7c419794374752ab1551b7cfd
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:28:09 +1100

    Create IslandInfo.lua

---
+++ GameData/King Legacy [4520749081]/IslandInfo.lua
@@ -0,0 +1,56 @@
+data = {
+    ["ZombieIsland"] = {["InfoText"] = "Lv. 1500+", ["Name"] = "Zombie Island", ["Icon"] = "rbxassetid://9564786576"},
+    ["ShredEndangering"] = {
+        ["InfoText"] = "Lv. 3300+",
+        ["Name"] = "Shred Endangering",
+        ["Icon"] = "rbxassetid://9564779779"
+    },
+    ["WarIsland"] = {["InfoText"] = "Lv. 1750+", ["Name"] = "War Island", ["Icon"] = "rbxassetid://9564785994"},
+    ["PirateIsland"] = {["InfoText"] = "Lv. 50+", ["Name"] = "Pirate Island", ["Icon"] = "rbxassetid://9564769029"},
+    ["Forgotten Coliseum"] = {
+        ["InfoText"] = "Lv. 4550+",
+        ["Name"] = "Forgotten Coliseum",
+        ["Icon"] = "rbxassetid://114831141218674"
+    },
+    ["Bubble"] = {["InfoText"] = "Lv. 1000+", ["Name"] = "Bubble Island", ["Icon"] = "rbxassetid://9564754117"},
+    ["PirateSkullIsland"] = {
+        ["InfoText"] = "Lv. 3400+",
+        ["Name"] = "Pirate Skull Island",
+        ["Icon"] = "rbxassetid://9589399877"
+    },
+    ["SkullIsland"] = {["InfoText"] = "Lv. 3025+", ["Name"] = "Skull Island", ["Icon"] = "rbxassetid://9564781311"},
+    ["Tundra"] = {["InfoText"] = "Lv. 400+", ["Name"] = "Snow Island", ["Icon"] = "rbxassetid://9564782636"},
+    ["SkyIsland"] = {["InfoText"] = "Lv. 800+", ["Name"] = "Sky Island", ["Icon"] = "rbxassetid://9564782095"},
+    ["Drakenhold Fortress"] = {
+        ["InfoText"] = "Lv. 4400+",
+        ["Name"] = "Drakenhold Fortress",
+        ["Icon"] = "rbxassetid://84832381951615"
+    },
+    ["The Unearthly"] = {["InfoText"] = "Lv. 4000+", ["Name"] = "The Unearthly", ["Icon"] = "rbxassetid://15832098144"},
+    ["The Shallow"] = {["InfoText"] = "Lv. 4300+", ["Name"] = "The Shallow", ["Icon"] = "rbxassetid://15832099891"},
+    ["Sea"] = {["InfoText"] = "the vast sea", ["Name"] = "Sea", ["Icon"] = ""},
+    ["Sea of dust"] = {["InfoText"] = "Lv. 525+", ["Name"] = "Desert Island", ["Icon"] = "rbxassetid://9564774377"},
+    ["Fiore"] = {["InfoText"] = "Lv. 3600+", ["Name"] = "Fiore", ["Icon"] = "rbxassetid://12132535188"},
+    ["Luma Grove"] = {
+        ["InfoText"] = "A floating island inside a bubble.",
+        ["Name"] = "Luma Grove",
+        ["Icon"] = "rbxassetid://104224953846275"
+    },
+    ["Japan Island"] = {["InfoText"] = "Lv. 2250+", ["Name"] = "Japan Island", ["Icon"] = "rbxassetid://9564756284"},
+    ["StarterIsland"] = {["InfoText"] = "Lv. 1+", ["Name"] = "Starter Island", ["Icon"] = "rbxassetid://9564784590"},
+    ["Fishland"] = {["InfoText"] = "Lv. 2000+", ["Name"] = "Bubble Island", ["Icon"] = "rbxassetid://9564766265"},
+    ["LoafIsland"] = {["InfoText"] = "Lv. 3200+", ["Name"] = "Loaf Island", ["Icon"] = "rbxassetid://9564765572"},
+    ["LobbyIsland"] = {["InfoText"] = "Lv. 1200+", ["Name"] = "Lobby Island", ["Icon"] = "rbxassetid://9564766265"},
+    ["RainyStone"] = {["InfoText"] = "Lv. ???+", ["Name"] = "Rainy Stone", ["Icon"] = "rbxassetid://9564772595"},
+    ["DeadTundra"] = {["InfoText"] = "Lv. 3100+", ["Name"] = "Dead Tundra", ["Icon"] = "rbxassetid://9564755443"},
+    ["SoldierHeadquater"] = {
+        ["InfoText"] = "Lv. 3500+",
+        ["Name"] = "Soldier Headquater",
+        ["Icon"] = "rbxassetid://9589401651"
+    },
+    ["StoneArena"] = {["InfoText"] = "Lv. ???+", ["Name"] = "Stone Arena", ["Icon"] = "rbxassetid://9564784952"},
+    ["HalloweenIsland"] = {["InfoText"] = "Trick & Treat!", ["Name"] = "Halloween Island", ["Icon"] = ""},
+    ["ChefShip"] = {["InfoText"] = "Lv. 250+", ["Name"] = "Chef Ship", ["Icon"] = "rbxassetid://9564755013"},
+    ["SharkIsland"] = {["InfoText"] = "Lv. 180+", ["Name"] = "Shark Island", ["Icon"] = "rbxassetid://9564778798"},
+    ["SoldierIsland"] = {["InfoText"] = "Lv. 100+", ["Name"] = "Soldier Island", ["Icon"] = "rbxassetid://9564783353"}
+}
