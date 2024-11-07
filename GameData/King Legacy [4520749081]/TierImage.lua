2d48830bf4087d4f148c4f92180157d15fd44720
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-08 01:27:23 +1100

    Create TierImage.lua

---
+++ GameData/King Legacy [4520749081]/TierImage.lua
@@ -0,0 +1,13 @@
+data = {
+    ["Exclusive"] = "rbxassetid://17583659411",
+    ["Common"] = "rbxassetid://17583651946",
+    ["Exotic"] = "rbxassetid://17583657261",
+    ["Uncommon"] = "rbxassetid://17583653194",
+    ["Rare"] = "rbxassetid://17583653908",
+    ["Legendary"] = "rbxassetid://17583655286",
+    ["Limited"] = "rbxassetid://17583657261",
+    ["Epic"] = "rbxassetid://17583654583",
+    ["Divine"] = "rbxassetid://17583657261",
+    ["Collectible"] = "rbxassetid://17583657261",
+    ["Mythical"] = "rbxassetid://17583656087"
+}
