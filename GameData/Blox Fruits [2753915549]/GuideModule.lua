08cbd0a7e2fe7c6e3bc5923988344573d496b80f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-12 01:15:32 +1100

    Create GuideModule.lua

---
+++ GameData/Blox Fruits [2753915549]/GuideModule.lua
@@ -0,0 +1,121 @@
+data = {
+    ["AbandonButton"] = "Abandon",
+    ["GetNearestNPC"] = "function: 0x028e57fcafd5849f",
+    ["Properties"] = {
+        ["unknownLocationImage"] = "49009723",
+        ["defaultCompassIcon"] = "8934096355",
+        ["LocationDefaults"] = {
+            ["GuideColors"] = {"UIGradient", "Color", "0 1 0.772549 0.0784314 0 1 1 0.772549 0.0784314 0 "},
+            ["GuideIconID"] = {"GuideIcon", "Image", 7252565452}
+        }
+    },
+    ["Data"] = {
+        ["CompassLoop"] = "Connection",
+        ["LastQuestLevel"] = 0,
+        ["LastMeters"] = 22,
+        ["LastClosestNPC"] = "Bandit Quest Giver",
+        ["NPCList"] = {
+            {
+                ["Levels"] = {300, 325, 350},
+                ["NPCName"] = "The Mayor",
+                ["Position"] = Vector3.new(-5313.3701171875, 10.950008392333984, 8515.2939453125)
+            },
+            {
+                ["Levels"] = {190, 210},
+                ["NPCName"] = "Jail Keeper",
+                ["Position"] = Vector3.new(5310.60546875, 0.35001492500305176, 474.94659423828125)
+            },
+            {
+                ["Levels"] = {450, 475, 500},
+                ["NPCName"] = "Mole",
+                ["Position"] = Vector3.new(-7859.09814453125, 5544.1904296875, -381.4761962890625)
+            },
+            {
+                ["Levels"] = {220, 230, 240},
+                ["NPCName"] = "Head Jailer",
+                ["Position"] = Vector3.new(5191.861328125, 2.8402068614959717, 686.438720703125)
+            },
+            {
+                ["Levels"] = {625, 650, 675},
+                ["NPCName"] = "Freezeburg Quest Giver",
+                ["Position"] = Vector3.new(5259.81982421875, 37.35001754760742, 4050.029296875)
+            },
+            {
+                ["Levels"] = {0},
+                ["NPCName"] = "Marine Leader",
+                ["Position"] = Vector3.new(-2708.577392578125, 23.21882438659668, 2105.34814453125)
+            },
+            {
+                ["Levels"] = {10, 15, 20},
+                ["NPCName"] = "Adventurer",
+                ["Position"] = Vector3.new(-1598.089111328125, 35.55011749267578, 153.37783813476562)
+            },
+            {
+                ["Levels"] = {375, 400, 425},
+                ["NPCName"] = "King Neptune",
+                ["Position"] = Vector3.new(61121.109375, 17.953125, 1564.5263671875)
+            },
+            {
+                ["Levels"] = {30, 40, 55},
+                ["NPCName"] = "Pirate Adventurer",
+                ["Position"] = Vector3.new(-1141.0748291015625, 4.10001802444458, 3831.5498046875)
+            },
+            {
+                ["Levels"] = {0},
+                ["NPCName"] = "Bandit Quest Giver",
+                ["Position"] = Vector3.new(1059.3719482421875, 15.449506759643555, 1550.423095703125)
+            },
+            {
+                ["Levels"] = {150, 175},
+                ["NPCName"] = "Sky Adventurer",
+                ["Position"] = Vector3.new(-4839.5302734375, 716.3685913085938, -2619.441650390625)
+            },
+            {
+                ["Levels"] = {60, 75},
+                ["NPCName"] = "Desert Adventurer",
+                ["Position"] = Vector3.new(894.4886474609375, 5.140007019042969, 4392.43359375)
+            },
+            {
+                ["Levels"] = {120, 130},
+                ["NPCName"] = "Marine",
+                ["Position"] = Vector3.new(-5039.58642578125, 27.350038528442383, 4324.68017578125)
+            },
+            {
+                ["Levels"] = {90, 100, 105},
+                ["NPCName"] = "Villager",
+                ["Position"] = Vector3.new(1389.7445068359375, 86.65208435058594, -1298.907958984375)
+            },
+            {
+                ["Levels"] = {450, 475, 500},
+                ["NPCName"] = "Mole",
+                ["Position"] = Vector3.new(-4721.888671875, 843.8746948242188, -1949.9664306640625)
+            },
+            {
+                ["Levels"] = {525, 550, 575},
+                ["NPCName"] = "Sky Quest Giver 2",
+                ["Position"] = Vector3.new(-7904.6845703125, 5634.6611328125, -1409.96728515625)
+            },
+            {
+                ["Levels"] = {250, 275},
+                ["NPCName"] = "Colosseum Quest Giver",
+                ["Position"] = Vector3.new(-1580.046630859375, 6.350002765655518, -2986.475341796875)
+            }
+        },
+        ["Tracking"] = false,
+        ["Ready"] = true
+    },
+    ["IconFrame"] = "IconFrame",
+    ["GetDistance"] = "function: 0x2d70d79944146d9f",
+    ["ResetCompass"] = "function: 0xc4800b2f8fe9d6df",
+    ["ParentUI"] = "function: 0xa0f2f997dcd1da7f",
+    ["ToggleCompassShown"] = "function: 0x480301fcd4a0833f",
+    ["ChangeGuideIcon"] = "function: 0x2dd24dd091e0eedf",
+    ["PointCompass"] = "function: 0xfc5efb9b8f139d2f",
+    ["ChangeDisplayedNPC"] = "function: 0xad6657b581b36c4f",
+    ["AppendNPCData"] = "function: 0x0422de7f51dd1a1f",
+    ["TrackButton"] = "Track",
+    ["UI"] = "Guide",
+    ["NotifyTween"] = "Tween",
+    ["Viewport"] = "ViewportFrame",
+    ["SideCompass"] = "Compass"
+}
