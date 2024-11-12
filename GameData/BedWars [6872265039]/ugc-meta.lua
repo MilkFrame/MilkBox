8a583415e4363fbf8333c18a9d5867383d7f6d2c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-13 03:28:52 +1100

    Update ugc-meta.lua

--- GameData/BedWars [6872265039]/ugc-meta.lua
+++ GameData/BedWars [6872265039]/ugc-meta.lua
@@ -1,17 +1,39 @@
 data = {
     ["UgcMeta"] = {
-        ["grim_reaper_shoulder_buddy"] = {
+        ["grim_reaper_kit_shoulder_buddy"] = {
             ["assetId"] = 17735885308,
-            ["name"] = "Grim Reaper Shoulder Buddy",
+            ["name"] = "Grim Reaper Kit Shoulder Buddy",
             ["category"] = "shoulder_accessory",
             ["image"] = {["render"] = "rbxassetid://17737351773", ["stroke"] = "rbxassetid://17737753754"},
             ["basePrice"] = 50
         },
-        ["aery_shoulder_buddy"] = {
+        ["aery_kit_shoulder_buddy"] = {
             ["assetId"] = 139207473906297,
-            ["name"] = "Aery Shoulder Buddy",
+            ["name"] = "Aery Kit Shoulder Buddy",
             ["category"] = "shoulder_accessory",
             ["image"] = {["render"] = "rbxassetid://106491911463393", ["stroke"] = "rbxassetid://124811127697552"},
+            ["basePrice"] = 50
+        },
+        ["raven_kit_shoulder_buddy"] = {
+            ["assetId"] = 87517201817150,
+            ["name"] = "Raven Kit Shoulder Buddy",
+            ["category"] = "shoulder_accessory",
+            ["image"] = {["render"] = "rbxassetid://136755542381122", ["stroke"] = "rbxassetid://137585047786620"},
+            ["limited"] = true,
+            ["basePrice"] = 100
+        },
+        ["nyx_kit_shoulder_buddy"] = {
+            ["assetId"] = 137030930196196,
+            ["name"] = "Nyx Kit Shoulder Buddy",
+            ["category"] = "shoulder_accessory",
+            ["image"] = {["render"] = "rbxassetid://122973744613416", ["stroke"] = "rbxassetid://82192937730472"},
+            ["basePrice"] = 50
+        },
+        ["milo_kit_shoulder_buddy"] = {
+            ["assetId"] = 85878373312999,
+            ["name"] = "Milo Kit Shoulder Buddy",
+            ["category"] = "shoulder_accessory",
+            ["image"] = {["render"] = "rbxassetid://82238296736005", ["stroke"] = "rbxassetid://104404766192507"},
             ["basePrice"] = 50
         }
     }
