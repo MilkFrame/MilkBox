175087ea79d657b7f9bfcf1bfc4903a95e311c6e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-13 03:26:40 +1100

    Update win-effect-meta.lua

--- GameData/BedWars [6872265039]/win-effect-meta.lua
+++ GameData/BedWars [6872265039]/win-effect-meta.lua
@@ -2,6 +2,7 @@ data = {
     ["WinEffectMeta"] = {
         ["train"] = {["name"] = "Choo Choo Champion", ["image"] = "rbxassetid://15716142989"},
         ["juggernaut_barbarian"] = {["name"] = "Juggernaut Barbarian", ["image"] = "rbxassetid://14559884081"},
+        ["statue_of_triumph"] = {["name"] = "Divine Triumph", ["image"] = "rbxassetid://18961510440"},
         ["grim_reaper"] = {["name"] = "Grim Reaper", ["image"] = "rbxassetid://17762371072"},
         ["spirit_assassin"] = {["name"] = "Haunted Spirit Assassin", ["image"] = "rbxassetid://16390553155 "},
         ["none"] = {["name"] = "None"}
