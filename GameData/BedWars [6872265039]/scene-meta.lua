1e48f57964caa06c01feab35a52735a8d94f600a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-13 03:28:03 +1100

    Create scene-meta.lua

---
+++ GameData/BedWars [6872265039]/scene-meta.lua
@@ -0,0 +1,102 @@
+data = {
+    ["getSceneMeta"] = "function: 0x5b6ff9bd065fae1a",
+    ["SceneMeta"] = {
+        ["halloween_obby"] = {
+            ["deathScreen"] = {
+                ["deathMessages"] = {"Be hasty. The fog will catch up to you", "Look for ghosts on the bridge"}
+            },
+            ["loadingScreen"] = {
+                ["TitleColor"] = Color3.fromRGB(137, 255, 212),
+                ["Description"] = "RUN!",
+                ["Title"] = "CRYPT ESCAPE",
+                ["BackgroundImage"] = "rbxassetid://11349658767"
+            }
+        },
+        ["halloween_2024_stage_3"] = {
+            ["deathScreen"] = {["deathMessages"] = {"Beware of dust on the ground", "Spiders live in webs"}},
+            ["loadingScreen"] = {
+                ["TitleColor"] = Color3.fromRGB(137, 255, 212),
+                ["Description"] = "Glowing crystals fuel a dark power",
+                ["Title"] = "Queen's Lair",
+                ["BackgroundImage"] = "rbxassetid://80277335823549"
+            }
+        },
+        ["halloween_boss"] = {
+            ["deathScreen"] = {
+                ["deathMessages"] = {
+                    "Spot large clues spawned in the air",
+                    "Look for ghosts on the stone platforms",
+                    "Spot the clue when cursed by Eldric"
+                }
+            },
+            ["loadingScreen"] = {
+                ["TitleColor"] = Color3.fromRGB(137, 255, 212),
+                ["Description"] = "???",
+                ["Title"] = "FINAL TRIAL",
+                ["BackgroundImage"] = "rbxassetid://11363008804"
+            }
+        },
+        ["halloween_graveyard_2023"] = {
+            ["deathScreen"] = {
+                ["deathMessages"] = {
+                    "Keep a vigilant eye for the altar candles.",
+                    "Let the colored flames guide your path."
+                }
+            },
+            ["loadingScreen"] = {
+                ["TitleColor"] = Color3.fromRGB(137, 255, 212),
+                ["Description"] = "Find candles and satisfy the Skull Altar.",
+                ["Title"] = "GRAVEYARD",
+                ["BackgroundImage"] = "rbxassetid://11340018523"
+            }
+        },
+        ["halloween_maze"] = {
+            ["deathScreen"] = {["deathMessages"] = {"Follow the light", "Shine your teammates to the end"}},
+            ["loadingScreen"] = {
+                ["TitleColor"] = Color3.fromRGB(137, 255, 212),
+                ["Description"] = "Follow the light!",
+                ["Title"] = "MAZE",
+                ["BackgroundImage"] = "rbxassetid://11350360572"
+            }
+        },
+        ["halloween_2024_stage_1"] = {
+            ["deathScreen"] = {
+                ["deathMessages"] = {
+                    "There's no benefit to killing the guards, and they're extremely dangerous. Try to just avoid them!"
+                }
+            },
+            ["loadingScreen"] = {
+                ["TitleColor"] = Color3.fromRGB(137, 255, 212),
+                ["Description"] = "Find crystals and avoid the guards to open the gate.",
+                ["Title"] = "The Dark Forest",
+                ["BackgroundImage"] = "rbxassetid://80204639527004"
+            }
+        },
+        ["halloween_2024_stage_2"] = {
+            ["deathScreen"] = {
+                ["deathMessages"] = {
+                    "Keep an eye on the above.",
+                    "Sharp Stalactite hang from above... Perhaps a well-aimed shot could dislodge them?",
+                    "When the ground falls away, sometimes you must build your own way across."
+                }
+            },
+            ["loadingScreen"] = {
+                ["TitleColor"] = Color3.fromRGB(137, 255, 212),
+                ["Description"] = "Gather crystals to open the gate to the Queen's LAIR",
+                ["Title"] = "The Crystal CAVERN",
+                ["BackgroundImage"] = "rbxassetid://104890347764235"
+            }
+        },
+        ["halloween_graveyard"] = {
+            ["deathScreen"] = {
+                ["deathMessages"] = {"Don't get too close to the keeper.", "Be on the look out for clues."}
+            },
+            ["loadingScreen"] = {
+                ["TitleColor"] = Color3.fromRGB(137, 255, 212),
+                ["Description"] = "Beware of the grave keeper. Dig up the correct grave or die.",
+                ["Title"] = "GRAVEYARD",
+                ["BackgroundImage"] = "rbxassetid://11340018523"
+            }
+        }
+    }
+}
