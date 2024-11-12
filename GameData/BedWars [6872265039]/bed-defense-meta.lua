a6828710a6d498577e27534416b028d025da4520
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-13 03:27:24 +1100

    Create bed-defense-meta.lua

---
+++ GameData/BedWars [6872265039]/bed-defense-meta.lua
@@ -0,0 +1,9 @@
+data = {
+    ["BedDefenseMeta"] = {
+        {["cost"] = {["itemType"] = "iron", ["amount"] = 0}, ["layers"] = {"wool_white"}},
+        {["cost"] = {["itemType"] = "iron", ["amount"] = 16}, ["layers"] = {"wood_plank_oak"}},
+        {["cost"] = {["itemType"] = "iron", ["amount"] = 20}, ["layers"] = {"wood_plank_oak", "wool_white"}},
+        {["cost"] = {["itemType"] = "iron", ["amount"] = 40}, ["layers"] = {"wood_plank_oak", "stone_brick"}},
+        {["cost"] = {["itemType"] = "iron", ["amount"] = 80}, ["layers"] = {"wood_plank_oak", "stone_brick", "ceramic"}}
+    }
+}
