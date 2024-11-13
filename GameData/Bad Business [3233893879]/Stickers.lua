1f8c9bbbcb1893efb1ea85e574770a30dcdafa62
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-14 03:19:40 +1100

    Create Stickers.lua

---
+++ GameData/Bad Business [3233893879]/Stickers.lua
@@ -0,0 +1,179 @@
+data = {
+    ["Stickers"] = {
+        ["_shiguto"] = {
+            ["SGTO"] = {["Texture"] = "rbxassetid://18194192475", ["Category"] = "Event", ["Universal"] = true}
+        },
+        ["_p3"] = {
+            ["Fungus"] = {["Texture"] = "rbxassetid://6403874756", ["Category"] = "Aidan_Baidan", ["Universal"] = true},
+            ["Groovy"] = {["Texture"] = "rbxassetid://6403874182", ["Category"] = "Aidan_Baidan", ["Universal"] = true},
+            ["botPod"] = {["Texture"] = "rbxassetid://6403873175", ["Category"] = "Aidan_Baidan", ["Universal"] = true},
+            ["Chilly"] = {["Texture"] = "rbxassetid://6407247961", ["Category"] = "Aidan_Baidan", ["Universal"] = true},
+            ["Alien"] = {["Texture"] = "rbxassetid://6403875556", ["Category"] = "Aidan_Baidan", ["Universal"] = true},
+            ["GG"] = {["Texture"] = "rbxassetid://6403871684", ["Category"] = "Aidan_Baidan", ["Universal"] = true},
+            ["Melt"] = {["Texture"] = "rbxassetid://6403872701", ["Category"] = "Aidan_Baidan", ["Universal"] = true},
+            ["Scribble"] = {
+                ["Texture"] = "rbxassetid://6403872149",
+                ["Category"] = "Aidan_Baidan",
+                ["Universal"] = true
+            }
+        },
+        ["_p4"] = {
+            ["Hamburger"] = {
+                ["Texture"] = "rbxassetid://7545814981",
+                ["Category"] = "Aidan_Baidan",
+                ["Universal"] = true
+            },
+            ["Sweat"] = {["Texture"] = "rbxassetid://7545594125", ["Category"] = "Aidan_Baidan", ["Universal"] = true},
+            ["Trash"] = {["Texture"] = "rbxassetid://7545590252", ["Category"] = "Aidan_Baidan", ["Universal"] = true},
+            ["Vibe Check"] = {
+                ["Texture"] = "rbxassetid://7545590890",
+                ["Category"] = "Aidan_Baidan",
+                ["Universal"] = true
+            },
+            ["Mohawk"] = {["Texture"] = "rbxassetid://7545594688", ["Category"] = "Aidan_Baidan", ["Universal"] = true},
+            ["Heavy Weaponry"] = {
+                ["Texture"] = "rbxassetid://7545815266",
+                ["Category"] = "Aidan_Baidan",
+                ["Universal"] = true
+            },
+            ["Biggerhead"] = {
+                ["Texture"] = "rbxassetid://7545592406",
+                ["Category"] = "Aidan_Baidan",
+                ["Universal"] = true
+            },
+            ["Eyes"] = {["Texture"] = "rbxassetid://7545591725", ["Category"] = "Aidan_Baidan", ["Universal"] = true},
+            ["badPod"] = {["Texture"] = "rbxassetid://7545593522", ["Category"] = "Aidan_Baidan", ["Universal"] = true}
+        },
+        ["_p1"] = {
+            ["Popular Game Character"] = {
+                ["Texture"] = "rbxassetid://5061819102",
+                ["Category"] = "Prestige 1",
+                ["Universal"] = true
+            },
+            ["Death's Chill"] = {
+                ["Texture"] = "rbxassetid://5061818773",
+                ["Category"] = "Prestige 1",
+                ["Universal"] = true
+            },
+            ["Mz Wonky"] = {["Texture"] = "rbxassetid://5061798762", ["Category"] = "Prestige 1", ["Universal"] = true},
+            ["Bringles"] = {["Texture"] = "rbxassetid://5061817930", ["Category"] = "Prestige 1", ["Universal"] = true},
+            ["Noob"] = {["Texture"] = "rbxassetid://5061818341", ["Category"] = "Prestige 1", ["Universal"] = true},
+            ["Prestige 1"] = {
+                ["Texture"] = "rbxassetid://5103899861",
+                ["Category"] = "Prestige 1",
+                ["Universal"] = true
+            },
+            ["What"] = {["Texture"] = "rbxassetid://5061824384", ["Category"] = "Prestige 1", ["Universal"] = true},
+            ["Recharge"] = {["Texture"] = "rbxassetid://5061822053", ["Category"] = "Prestige 1", ["Universal"] = true},
+            ["Sharp Skater"] = {
+                ["Texture"] = "rbxassetid://5061823895",
+                ["Category"] = "Prestige 1",
+                ["Universal"] = true
+            }
+        },
+        ["_adoptme"] = {
+            ["Bee Nice"] = {["Texture"] = "rbxassetid://5019061594", ["Category"] = "Adopt Me", ["Universal"] = true},
+            ["I'm Baby"] = {["Texture"] = "rbxassetid://5019062543", ["Category"] = "Adopt Me", ["Universal"] = true},
+            ["Bro"] = {["Texture"] = "rbxassetid://5019061934", ["Category"] = "Adopt Me", ["Universal"] = true},
+            ["Bamboozled"] = {["Texture"] = "rbxassetid://5019061226", ["Category"] = "Adopt Me", ["Universal"] = true},
+            ["Cool"] = {["Texture"] = "rbxassetid://5019062314", ["Category"] = "Adopt Me", ["Universal"] = true}
+        },
+        ["_pride"] = {
+            ["Wonky Nonbinary"] = {
+                ["Texture"] = "rbxassetid://5135909802",
+                ["Category"] = "Universal",
+                ["Universal"] = true
+            },
+            ["Wonky Genderfluid"] = {
+                ["Texture"] = "rbxassetid://5138582725",
+                ["Category"] = "Universal",
+                ["Universal"] = true
+            },
+            ["Wonky Pan"] = {["Texture"] = "rbxassetid://5135909406", ["Category"] = "Universal", ["Universal"] = true},
+            ["Wonky Trans"] = {
+                ["Texture"] = "rbxassetid://4665097135",
+                ["Category"] = "Universal",
+                ["Universal"] = true
+            },
+            ["Wonky Ace"] = {["Texture"] = "rbxassetid://5135910690", ["Category"] = "Universal", ["Universal"] = true},
+            ["Wonky Bi"] = {["Texture"] = "rbxassetid://5135910389", ["Category"] = "Universal", ["Universal"] = true},
+            ["Wonky Pride"] = {
+                ["Texture"] = "rbxassetid://4665096731",
+                ["Category"] = "Universal",
+                ["Universal"] = true
+            },
+            ["Wonky Lesbian"] = {
+                ["Texture"] = "rbxassetid://5135910095",
+                ["Category"] = "Universal",
+                ["Universal"] = true
+            }
+        },
+        [".contractjuly24"] = {
+            ["Sadcat"] = {["Texture"] = "rbxassetid://5061823098", ["Category"] = "Contracts", ["Universal"] = true},
+            ["Troll Business"] = {
+                ["Texture"] = "rbxassetid://17648574657",
+                ["Category"] = "Contracts",
+                ["Universal"] = true
+            },
+            ["Pixel Business"] = {
+                ["Texture"] = "rbxassetid://18668450343",
+                ["Category"] = "Contracts",
+                ["Universal"] = true
+            },
+            ["Barcode"] = {["Texture"] = "rbxassetid://18445472563", ["Category"] = "Contracts", ["Universal"] = true},
+            ["Clown"] = {["Texture"] = "rbxassetid://7545593087", ["Category"] = "Contracts", ["Universal"] = true},
+            ["Hay"] = {["Texture"] = "rbxassetid://18445472723", ["Category"] = "Contracts", ["Universal"] = true},
+            ["Oh Love"] = {["Texture"] = "rbxassetid://18445472892", ["Category"] = "Contracts", ["Universal"] = true}
+        },
+        ["_p2"] = {
+            ["4.png"] = {["Texture"] = "rbxassetid://5061798379", ["Category"] = "Prestige 2", ["Universal"] = true},
+            ["Revival"] = {["Texture"] = "rbxassetid://5061822437", ["Category"] = "Prestige 2", ["Universal"] = true},
+            ["Radioactive"] = {
+                ["Texture"] = "rbxassetid://5061821375",
+                ["Category"] = "Prestige 2",
+                ["Universal"] = true
+            },
+            ["1.png"] = {["Texture"] = "rbxassetid://5061797866", ["Category"] = "Prestige 2", ["Universal"] = true},
+            ["No Signal"] = {["Texture"] = "rbxassetid://5061820981", ["Category"] = "Prestige 2", ["Universal"] = true},
+            ["Boss"] = {["Texture"] = "rbxassetid://5061817618", ["Category"] = "Prestige 2", ["Universal"] = true},
+            ["Prestige 2"] = {
+                ["Texture"] = "rbxassetid://5157364492",
+                ["Category"] = "Prestige 2",
+                ["Universal"] = true
+            },
+            ["Business Girl"] = {
+                ["Texture"] = "rbxassetid://5061799397",
+                ["Category"] = "Prestige 2",
+                ["Universal"] = true
+            },
+            ["Seer"] = {["Texture"] = "rbxassetid://5061823517", ["Category"] = "Prestige 2", ["Universal"] = true}
+        },
+        ["_event"] = {
+            ["Candy Corn"] = {["Texture"] = "rbxassetid://5861058111", ["Category"] = "Event", ["Universal"] = true},
+            ["Full Moon"] = {["Texture"] = "rbxassetid://5861057775", ["Category"] = "Event", ["Universal"] = true},
+            ["Wonky Bat"] = {["Texture"] = "rbxassetid://5861058385", ["Category"] = "Event", ["Universal"] = true}
+        },
+        ["_p0"] = {
+            ["Headshot"] = {["Texture"] = "rbxassetid://5030996773", ["Category"] = "Prestige 0", ["Universal"] = true},
+            ["Boom"] = {["Texture"] = "rbxassetid://5030996053", ["Category"] = "Prestige 0", ["Universal"] = true},
+            ["Stab"] = {["Texture"] = "rbxassetid://5030997677", ["Category"] = "Prestige 0", ["Universal"] = true},
+            ["Froggy"] = {["Texture"] = "rbxassetid://5031068500", ["Category"] = "Prestige 0", ["Universal"] = true},
+            ["Vaporwave Sun"] = {
+                ["Texture"] = "rbxassetid://5026595592",
+                ["Category"] = "Prestige 0",
+                ["Universal"] = true
+            },
+            ["Flashbang"] = {["Texture"] = "rbxassetid://5030995470", ["Category"] = "Prestige 0", ["Universal"] = true},
+            ["Prestige 0"] = {
+                ["Texture"] = "rbxassetid://5030997175",
+                ["Category"] = "Prestige 0",
+                ["Universal"] = true
+            },
+            ["Certified Bot"] = {
+                ["Texture"] = "rbxassetid://5030996394",
+                ["Category"] = "Prestige 0",
+                ["Universal"] = true
+            }
+        }
+    }
+}
