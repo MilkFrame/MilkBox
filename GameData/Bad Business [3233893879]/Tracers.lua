48aaaed9cedb2f51a037d7b2c7b8705c4f39b169
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-14 03:18:57 +1100

    Create Tracers.lua

---
+++ GameData/Bad Business [3233893879]/Tracers.lua
@@ -0,0 +1,76 @@
+data = {
+    ["Tracers"] = {
+        ["Dev"] = {["HitEffect"] = "Beam_Hit", ["Model"] = "Beam", ["Title"] = "Dev", ["Category"] = "Legendary"},
+        ["Electric"] = {
+            ["HitEffect"] = "Electric_Hit",
+            ["Model"] = "Electric",
+            ["Title"] = "Electric GREEN",
+            ["Category"] = "Legendary"
+        },
+        ["Rainbow"] = {
+            ["HitEffect"] = "Rainbow_Hit",
+            ["Model"] = "RainbowBeam",
+            ["Title"] = "Rainbow",
+            ["Category"] = "Legendary"
+        },
+        ["Beam"] = {["HitEffect"] = "Beam_Hit", ["Model"] = "Beam", ["Title"] = "Beam", ["Category"] = "Legendary"},
+        ["Classic"] = {
+            ["HitEffect"] = "Paintball_Hit",
+            ["Model"] = "ClassicPaintball",
+            ["Title"] = "Classic",
+            ["Category"] = "Legendary"
+        },
+        ["Laser"] = {["HitEffect"] = "Laser_Hit", ["Model"] = "Laser", ["Title"] = "Laser", ["Category"] = "Classic"},
+        ["Alpha"] = {
+            ["Title"] = "ALPHA",
+            ["Category"] = "Legendary",
+            ["OnCreate"] = "function: 0xddca743c46b3cfc8",
+            ["HitEffect"] = "Alpha_Hit",
+            ["Model"] = "AlphaPaintball"
+        },
+        ["Electric2"] = {
+            ["HitEffect"] = "Electric2_Hit",
+            ["Model"] = "Electric2",
+            ["Title"] = "Electric ORANGE",
+            ["Category"] = "Legendary"
+        },
+        ["Tactical"] = {
+            ["HitEffect"] = "Tactical_Hit",
+            ["Model"] = "Tactical",
+            ["Title"] = "Tactical",
+            ["Category"] = "Legendary"
+        },
+        ["Cyber"] = {["HitEffect"] = "Cyber_Hit", ["Model"] = "Cyber", ["Title"] = "Cyber", ["Category"] = "Legendary"},
+        ["Gilded"] = {
+            ["HitEffect"] = "Gilded_Hit",
+            ["Model"] = "Gilded",
+            ["Title"] = "Gilded",
+            ["Category"] = "Legendary"
+        },
+        ["Emerald"] = {
+            ["HitEffect"] = "Emerald_Hit",
+            ["Model"] = "Emerald",
+            ["Title"] = "Emerald",
+            ["Category"] = "Legendary"
+        },
+        ["Luxe"] = {["HitEffect"] = "Luxe_Hit", ["Model"] = "Luxe", ["Title"] = "Luxe", ["Category"] = "Legendary"},
+        ["Annihilator"] = {
+            ["HitEffect"] = "Annihilator_Hit",
+            ["Model"] = "Annihilator",
+            ["Title"] = "Annihilator",
+            ["Category"] = "Legendary"
+        },
+        ["Draconic"] = {
+            ["HitEffect"] = "Draconic_Hit",
+            ["Model"] = "Draconic",
+            ["Title"] = "Draconic",
+            ["Category"] = "Legendary"
+        },
+        ["Frozen"] = {
+            ["HitEffect"] = "FrozenIcicle_Hit",
+            ["Model"] = "Icicle",
+            ["Title"] = "Frozen",
+            ["Category"] = "Legendary"
+        }
+    }
+}
