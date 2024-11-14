5b6f230a2c590cd79e202dbf07872575e00484df
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-15 03:08:44 +1100

    Create VehicleCombatData.lua

---
+++ GameData/Jailbreak [606849621]/VehicleCombatData.lua
@@ -0,0 +1,580 @@
+data = {
+    ["Roadster"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 3,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["ShellMark5"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["TheAgent"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 125
+    },
+    ["Bugatti"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["Surus"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 125
+    },
+    ["OGLamborghini"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Firetruck"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 0.75,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 175
+    },
+    ["Jeep"] = {["DisableDuration"] = 7.5, ["RamDisableDuration"] = 1, ["OccupantDamageMult"] = 1, ["TireHealth"] = 100},
+    ["SUV"] = {["DisableDuration"] = 7.5, ["RamDisableDuration"] = 1, ["OccupantDamageMult"] = 1, ["TireHealth"] = 125},
+    ["Cybertruck"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 125
+    },
+    ["UFO"] = {["DisableDuration"] = 10, ["RamDisableDuration"] = 0, ["OccupantDamageMult"] = 1, ["TireHealth"] = 100},
+    ["Classic"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Volt4x4"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 125
+    },
+    ["Striker"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 0.8,
+        ["TireHealth"] = 125
+    },
+    ["XRK"] = {["DisableDuration"] = 7.5, ["RamDisableDuration"] = 2, ["OccupantDamageMult"] = 1, ["TireHealth"] = 100},
+    ["Hammerhead"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 0.8,
+        ["TireHealth"] = 125
+    },
+    ["Torero"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["IceBreaker"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Parisian"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["Challenger"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["McLaren"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["AirTail"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["Semi"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 0.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 200
+    },
+    ["Longhorn"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["NASCAR1"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Manta"] = {["DisableDuration"] = 7.5, ["RamDisableDuration"] = 2, ["OccupantDamageMult"] = 1, ["TireHealth"] = 75},
+    ["Lia"] = {["DisableDuration"] = 10, ["RamDisableDuration"] = 0, ["OccupantDamageMult"] = 1, ["TireHealth"] = 75},
+    ["ATV"] = {["DisableDuration"] = 7.5, ["RamDisableDuration"] = 1, ["OccupantDamageMult"] = 1, ["TireHealth"] = 50},
+    ["NASCAR2"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Dirtbike"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 3,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 50
+    },
+    ["Maverick"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 125
+    },
+    ["Venom"] = {["DisableDuration"] = 7.5, ["RamDisableDuration"] = 2, ["OccupantDamageMult"] = 1, ["TireHealth"] = 75},
+    ["TheRattler"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Bloxy"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["MCL36"] = {["DisableDuration"] = 7.5, ["RamDisableDuration"] = 2, ["OccupantDamageMult"] = 1, ["TireHealth"] = 75},
+    ["Ray9"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["JetSki"] = {["DisableDuration"] = 15, ["RamDisableDuration"] = 0, ["OccupantDamageMult"] = 1, ["TireHealth"] = 50},
+    ["JackRabbit"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 125
+    },
+    ["Camaro"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Wedge"] = {["DisableDuration"] = 7.5, ["RamDisableDuration"] = 2, ["OccupantDamageMult"] = 1, ["TireHealth"] = 75},
+    ["Delorean"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Ray"] = {["DisableDuration"] = 7.5, ["RamDisableDuration"] = 1, ["OccupantDamageMult"] = 1, ["TireHealth"] = 100},
+    ["Ferrari"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Default"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Volt"] = {["DisableDuration"] = 15, ["RamDisableDuration"] = 8, ["OccupantDamageMult"] = 1, ["TireHealth"] = 50},
+    ["LittleBird"] = {
+        ["DisableDuration"] = 10,
+        ["RamDisableDuration"] = 0,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["Raptor"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 125
+    },
+    ["EscapeBot"] = {
+        ["DisableDuration"] = 15,
+        ["RamDisableDuration"] = 0,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 50
+    },
+    ["CrewCapsule"] = {
+        ["DisableDuration"] = 15,
+        ["RamDisableDuration"] = 0,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 50
+    },
+    ["Shogun"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Tank"] = {
+        ["DisableDuration"] = 15,
+        ["RamDisableDuration"] = 0,
+        ["OccupantDamageMult"] = 0.6,
+        ["TireHealth"] = 200
+    },
+    ["Patrol"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 3,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 50
+    },
+    ["Successor"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["Snake"] = {["DisableDuration"] = 7.5, ["RamDisableDuration"] = 2, ["OccupantDamageMult"] = 1, ["TireHealth"] = 75},
+    ["Scorpio"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["Fiasco"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["OGMonster"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 150
+    },
+    ["Power1"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["TowTruck"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 125
+    },
+    ["R8"] = {["DisableDuration"] = 7.5, ["RamDisableDuration"] = 1.5, ["OccupantDamageMult"] = 1, ["TireHealth"] = 100},
+    ["TheCamper"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 125
+    },
+    ["MoltenM12"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["MiniCooper"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Celsior"] = {
+        ["DisableDuration"] = 15,
+        ["RamDisableDuration"] = 0,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 50
+    },
+    ["Macaron"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["Trailblazer"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 125
+    },
+    ["Javelin"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["DesertCrawler"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["Porsche"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Chiron"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["Lamborghini"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["BeamHybrid"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Aperture"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["BananaCar"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Poseidon"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["DuneBuggy"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["Sentinel"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 0.75,
+        ["OccupantDamageMult"] = 0.6,
+        ["TireHealth"] = 175
+    },
+    ["Proto8"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 3,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["Heli"] = {["DisableDuration"] = 10, ["RamDisableDuration"] = 0, ["OccupantDamageMult"] = 1, ["TireHealth"] = 75},
+    ["Sloop"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 0,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 300
+    },
+    ["Goliath"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 0.75,
+        ["OccupantDamageMult"] = 0.6,
+        ["TireHealth"] = 200
+    },
+    ["FrostCrawler"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 175
+    },
+    ["Cruiser"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 0,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["BlackHawk"] = {
+        ["DisableDuration"] = 15,
+        ["RamDisableDuration"] = 0,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["Stunt"] = {["DisableDuration"] = 15, ["RamDisableDuration"] = 0, ["OccupantDamageMult"] = 1, ["TireHealth"] = 75},
+    ["Jet"] = {["DisableDuration"] = 15, ["RamDisableDuration"] = 0, ["OccupantDamageMult"] = 1, ["TireHealth"] = 75},
+    ["Deja"] = {["DisableDuration"] = 7.5, ["RamDisableDuration"] = 1, ["OccupantDamageMult"] = 1, ["TireHealth"] = 100},
+    ["FalconS"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 50
+    },
+    ["SWATVan"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 0.8,
+        ["TireHealth"] = 150
+    },
+    ["Concept"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["Interrogator"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 125
+    },
+    ["Torpedo"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["PrisonBus"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 0.75,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 150
+    },
+    ["Beignet"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["Crown"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["TinyToy"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 50
+    },
+    ["Mustang"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Monster"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 150
+    },
+    ["Model3"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Laviolette"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Drone"] = {["DisableDuration"] = 15, ["RamDisableDuration"] = 0, ["OccupantDamageMult"] = 1, ["TireHealth"] = 50},
+    ["Boxer"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Badger"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Arachnid"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 125
+    },
+    ["Carbonara"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 2,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 75
+    },
+    ["BankTruck"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 0.75,
+        ["OccupantDamageMult"] = 0.4,
+        ["TireHealth"] = 200
+    },
+    ["NASCAR75th"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["ShellClassic"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Bandit"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1.5,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Pickup"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["SpeedYacht"] = {
+        ["DisableDuration"] = 10,
+        ["RamDisableDuration"] = 0,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 100
+    },
+    ["Wraith"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 0.8,
+        ["TireHealth"] = 125
+    },
+    ["Ambulance"] = {
+        ["DisableDuration"] = 7.5,
+        ["RamDisableDuration"] = 1,
+        ["OccupantDamageMult"] = 1,
+        ["TireHealth"] = 150
+    }
+}
