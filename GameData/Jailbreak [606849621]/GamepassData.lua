31e2030c186b21af6b92b77589ddff2cf4f861e5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-15 03:07:04 +1100

    Create GamepassData.lua

---
+++ GameData/Jailbreak [606849621]/GamepassData.lua
@@ -0,0 +1,67 @@
+data = {
+    {
+        ["PassId"] = 2725211,
+        ["Title"] = "PremiumGarage",
+        ["PriceInRobux"] = 360,
+        ["Text"] = "Tired of going to the garage? Customize and spawn vehicles ANYWHERE with this Premium Garage gamepass!",
+        ["ImageId"] = 11461151301
+    },
+    {
+        ["PassId"] = 2219040,
+        ["Maximum"] = 3500,
+        ["Text"] = "Buy this to be able to carry more cash during robberies!",
+        ["ImageId"] = 11461132911,
+        ["MaximumSmallStore"] = 1500,
+        ["Title"] = "DuffelBag",
+        ["PriceInRobux"] = 240,
+        ["MaximumJewelry"] = 5000,
+        ["MaximumTrain"] = 4000
+    },
+    {
+        ["PassId"] = 2070427,
+        ["Title"] = "SWAT",
+        ["PriceInRobux"] = 335,
+        ["Text"] = [[Buy this to unlock the riot wall and SWAT van.\nMachine gun, riot shield (25% MORE health), clothing, 20% larger paycheck, and more!]],
+        ["ImageId"] = 11461127992
+    },
+    {
+        ["PassId"] = 2218187,
+        ["Title"] = "Stereo",
+        ["PriceInRobux"] = 200,
+        ["Text"] = "Buy this to be able to play custom music and add to your own playlist!",
+        ["ImageId"] = 11461127128
+    },
+    {
+        ["PassId"] = 4974038,
+        ["Title"] = "BOSS",
+        ["PriceInRobux"] = 335,
+        ["Text"] = [[Buy this to unlock the Wraith super sedan, with minigun on top,\nand notifications when robberies open up! Plus more clothing options.]],
+        ["ImageId"] = 11461157854
+    },
+    {
+        ["PassId"] = 2296901,
+        ["Title"] = "VIP",
+        ["PriceInRobux"] = 800,
+        ["Text"] = "Unique chat color, 20% more cash, chrome color, daily rocket fuel + cash, & more!",
+        ["ImageId"] = 11461147598
+    },
+    {
+        ["PassId"] = 2068240,
+        ["Title"] = "Stash",
+        ["Text"] = "Buy this to unlock two extra drawers under your bed to hide items!",
+        ["ImageId"] = 708733660
+    },
+    {
+        ["PassId"] = 6631507,
+        ["Title"] = "XPBoost",
+        ["Text"] = "Gain 50% more XP from robberies, etc.!",
+        ["ImageId"] = 3348915254
+    },
+    {
+        ["PassId"] = 56149618,
+        ["Title"] = "TradingVIP",
+        ["PriceInRobux"] = 320,
+        ["Text"] = [[Buy this to get an instant-teleport button to go to the Trade World, rainbow trading tags, trade server hopping,\n		gold chat color, longer lasting trade requests, and a unique trade request bar so people know you're serious!]],
+        ["ImageId"] = 11461109674
+    }
+}
