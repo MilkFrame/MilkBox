01604fb33f2202209c48e3b506a697899f11c3b4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-15 03:10:40 +1100

    Create AwardData.lua

---
+++ GameData/Jailbreak [606849621]/AwardData.lua
@@ -0,0 +1,14 @@
+data = {
+    ["Bonus"] = {{["imageId"] = 95979128033902, ["type"] = "TireMesh", ["name"] = "RollerBlade", ["scoreLte"] = 0.02}},
+    ["Level"] = {
+        {["type"] = "Drift", ["name"] = "Synth", ["imageId"] = 101628751021692},
+        {["type"] = "Rim", ["name"] = "ShutterShade", ["imageId"] = 126148467065987},
+        {["type"] = "Spoiler", ["name"] = "Torero", ["imageId"] = 89658016447513},
+        {["type"] = "HomeItem", ["name"] = "80s Cassette Table", ["imageId"] = 128151821983401},
+        {["type"] = "Rim", ["name"] = "Cassette", ["imageId"] = 79146540547481},
+        {["type"] = "Spoiler", ["name"] = "GuitarRiff", ["imageId"] = 73598669549922},
+        {["type"] = "VehicleTexture", ["name"] = "Retrowave", ["imageId"] = 139221938818815},
+        {["type"] = "Rim", ["name"] = "Retrowave", ["imageId"] = 129056268919513},
+        {["type"] = "Vehicle", ["name"] = "Wedge", ["imageId"] = 106450572784202}
+    }
+}
