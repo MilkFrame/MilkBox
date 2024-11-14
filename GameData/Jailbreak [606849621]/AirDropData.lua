bf03d5cd3fbd129ee04ad79c993314c1dc4a9ee4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-15 03:10:53 +1100

    Create AirDropData.lua

---
+++ GameData/Jailbreak [606849621]/AirDropData.lua
@@ -0,0 +1,133 @@
+data = {
+    {
+        ["DurationToGetMaxScaling"] = 150,
+        ["RewardData"] = {
+            {["TypeEnum"] = 1, ["Type"] = "UniqueItem", ["ItemType"] = "Safe", ["RarityId"] = 2},
+            {["TypeEnum"] = 2, ["Type"] = "UniqueItem", ["ItemType"] = "Safe", ["RarityId"] = 2},
+            {["Type"] = "Cash", ["Name"] = "Cash Box", ["Amount"] = 2500, ["ImageId"] = 4915088923, ["RarityId"] = 1},
+            {["Type"] = "Cash", ["Name"] = "Cash Box", ["Amount"] = 4000, ["ImageId"] = 4915088923, ["RarityId"] = 1},
+            {["Type"] = "Nitro", ["Name"] = "Nitro", ["Amount"] = 100, ["ImageId"] = 10078659354, ["RarityId"] = 3}
+        },
+        ["MaxActiveDuration"] = 200,
+        ["PrimaryColor"] = Color3.fromRGB(211, 221, 240),
+        ["MaxNumGuards"] = 6,
+        ["ImageId"] = 10313418867,
+        ["BriefCaseOpenDuration"] = 25,
+        ["BeginningData"] = {
+            ["MoneyDropData"] = {["TotalMoneyDropped"] = 1000, ["NumMoneyDrops"] = 2},
+            ["RarityProbs"] = {75, 22, 3, 0, 0},
+            ["GuardData"] = {
+                ["BulletSpread"] = 0.15,
+                ["NumGuards"] = 3,
+                ["TriggerPressFreq"] = 1.4,
+                ["MaxHealth"] = 150,
+                ["WalkSpeed"] = 16
+            }
+        },
+        ["MinPressDurationForReward"] = 5,
+        ["Title"] = "Easy",
+        ["NumGuardsWithItemLimits"] = {["Sniper"] = 1, ["Shotgun"] = 1},
+        ["EndingData"] = {
+            ["MoneyDropData"] = {["TotalMoneyDropped"] = 2000, ["NumMoneyDrops"] = 2},
+            ["RarityProbs"] = {50, 40, 10, 0, 0},
+            ["GuardData"] = {
+                ["BulletSpread"] = 0.1,
+                ["NumGuards"] = 5,
+                ["TriggerPressFreq"] = 2.5,
+                ["MaxHealth"] = 200,
+                ["WalkSpeed"] = 16
+            }
+        },
+        ["TimeBetweenRandomGuards"] = 10,
+        ["MaxWaveCount"] = 5,
+        ["TimeBetweenWaves"] = 35
+    },
+    {
+        ["RewardData"] = {
+            {["TypeEnum"] = 2, ["Type"] = "UniqueItem", ["ItemType"] = "Safe", ["RarityId"] = 3},
+            {["TypeEnum"] = 3, ["Type"] = "UniqueItem", ["ItemType"] = "Safe", ["RarityId"] = 3},
+            {["Type"] = "Cash", ["Name"] = "Cash Box", ["Amount"] = 4000, ["ImageId"] = 4915088923, ["RarityId"] = 2},
+            {["Type"] = "Cash", ["Name"] = "Cash Box", ["Amount"] = 7000, ["ImageId"] = 4915088923, ["RarityId"] = 2},
+            {["Type"] = "Nitro", ["Name"] = "Nitro", ["Amount"] = 100, ["ImageId"] = 10078659354, ["RarityId"] = 4}
+        },
+        ["DurationToGetMaxScaling"] = 200,
+        ["BriefCaseOpenDuration"] = 25,
+        ["MaxActiveDuration"] = 250,
+        ["PrimaryColor"] = Color3.fromRGB(211, 221, 240),
+        ["MaxNumGuards"] = 7,
+        ["ImageId"] = 10313418867,
+        ["BeginningData"] = {
+            ["MoneyDropData"] = {["TotalMoneyDropped"] = 2000, ["NumMoneyDrops"] = 3},
+            ["RarityProbs"] = {0, 75, 22, 3, 0},
+            ["GuardData"] = {
+                ["BulletSpread"] = 0.15,
+                ["NumGuards"] = 4,
+                ["TriggerPressFreq"] = 1.4,
+                ["MaxHealth"] = 150,
+                ["WalkSpeed"] = 16
+            }
+        },
+        ["NumGuardsWithItemLimits"] = {["Sniper"] = 2, ["Shotgun"] = 2},
+        ["MaxInactiveDuration"] = 600,
+        ["Title"] = "Medium",
+        ["MinPressDurationForReward"] = 5,
+        ["EndingData"] = {
+            ["MoneyDropData"] = {["TotalMoneyDropped"] = 3000, ["NumMoneyDrops"] = 3},
+            ["RarityProbs"] = {0, 50, 40, 10, 0},
+            ["GuardData"] = {
+                ["BulletSpread"] = 0.1,
+                ["NumGuards"] = 5,
+                ["TriggerPressFreq"] = 2.5,
+                ["MaxHealth"] = 200,
+                ["WalkSpeed"] = 16
+            }
+        },
+        ["TimeBetweenRandomGuards"] = 10,
+        ["MaxWaveCount"] = 5,
+        ["TimeBetweenWaves"] = 35
+    },
+    {
+        ["RewardData"] = {
+            {["TypeEnum"] = 3, ["Type"] = "UniqueItem", ["ItemType"] = "Safe", ["RarityId"] = 3},
+            {["TypeEnum"] = 4, ["Type"] = "UniqueItem", ["ItemType"] = "Safe", ["RarityId"] = 3},
+            {["Type"] = "Cash", ["Name"] = "Cash Box", ["Amount"] = 7000, ["ImageId"] = 4915088923, ["RarityId"] = 2},
+            {["Type"] = "Cash", ["Name"] = "Cash Box", ["Amount"] = 10000, ["ImageId"] = 4915088923, ["RarityId"] = 2},
+            {["Type"] = "Nitro", ["Name"] = "Nitro", ["Amount"] = 100, ["ImageId"] = 10078659354, ["RarityId"] = 4}
+        },
+        ["DurationToGetMaxScaling"] = 250,
+        ["BriefCaseOpenDuration"] = 25,
+        ["MaxActiveDuration"] = 300,
+        ["PrimaryColor"] = Color3.fromRGB(211, 221, 240),
+        ["MaxNumGuards"] = 8,
+        ["ImageId"] = 10313418867,
+        ["BeginningData"] = {
+            ["MoneyDropData"] = {["TotalMoneyDropped"] = 3000, ["NumMoneyDrops"] = 5},
+            ["RarityProbs"] = {0, 75, 22, 3, 0},
+            ["GuardData"] = {
+                ["BulletSpread"] = 0.15,
+                ["NumGuards"] = 5,
+                ["TriggerPressFreq"] = 1.4,
+                ["MaxHealth"] = 150,
+                ["WalkSpeed"] = 16
+            }
+        },
+        ["NumGuardsWithItemLimits"] = {["Sniper"] = 3, ["Shotgun"] = 3},
+        ["MaxInactiveDuration"] = 600,
+        ["Title"] = "Hard",
+        ["MinPressDurationForReward"] = 5,
+        ["EndingData"] = {
+            ["MoneyDropData"] = {["TotalMoneyDropped"] = 4000, ["NumMoneyDrops"] = 5},
+            ["RarityProbs"] = {0, 50, 40, 10, 0},
+            ["GuardData"] = {
+                ["BulletSpread"] = 0.1,
+                ["NumGuards"] = 7,
+                ["TriggerPressFreq"] = 2.5,
+                ["MaxHealth"] = 200,
+                ["WalkSpeed"] = 16
+            }
+        },
+        ["TimeBetweenRandomGuards"] = 10,
+        ["MaxWaveCount"] = 5,
+        ["TimeBetweenWaves"] = 35
+    }
+}
