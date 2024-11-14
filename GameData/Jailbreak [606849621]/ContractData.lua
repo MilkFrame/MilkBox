5feb84a820fa29a7475131000d79954f37c776ae
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-15 03:06:50 +1100

    Create ContractData.lua

---
+++ GameData/Jailbreak [606849621]/ContractData.lua
@@ -0,0 +1,104 @@
+data = {
+    ["HeliVolcano"] = {
+        ["message"] = "Drop 2 helicopters into the criminal base volcano.",
+        ["rewardExp"] = 25,
+        ["goal"] = 2,
+        ["teamId"] = 2
+    },
+    ["TakeDownPolice"] = {
+        ["message"] = "function: 0x21ef8d62205ffae7",
+        ["rewardExp"] = 50,
+        ["goal"] = 5,
+        ["teamId"] = 2
+    },
+    ["NoDieXMins"] = {["message"] = "function: 0x4ef1c9e71c6c9de7", ["rewardExp"] = 25, ["goal"] = 7, ["teamId"] = 3},
+    ["RoadSpikeHit"] = {["message"] = "function: 0xab16f592f4f681c7", ["rewardExp"] = 50, ["goal"] = 3, ["teamId"] = 3},
+    ["ArrestBounty"] = {
+        ["message"] = "function: 0xe624f62a965e4107",
+        ["rewardExp"] = 30,
+        ["goal"] = "function: 0x4f1c0b14f39bbe07",
+        ["teamId"] = 3
+    },
+    ["JewelryXTimes"] = {["message"] = "function: 0x1a3b18fb3125a0a7", ["rewardExp"] = 50, ["goal"] = 5, ["teamId"] = 2},
+    ["AnyTrain"] = {["message"] = "function: 0x8618c18803c75ec7", ["rewardExp"] = 50, ["goal"] = 6, ["teamId"] = 2},
+    ["CargoPlaneXTimes"] = {
+        ["message"] = "function: 0x5a1e3fb73dfa5a67",
+        ["rewardExp"] = 55,
+        ["goal"] = 3,
+        ["teamId"] = 2
+    },
+    ["BountyAchieveNoDie"] = {
+        ["message"] = "Collect a bounty of $4,000 without dying.",
+        ["rewardExp"] = 25,
+        ["goal"] = 4000,
+        ["teamId"] = 2
+    },
+    ["MuseumXTimes"] = {["message"] = "function: 0x842483b3a8da7f27", ["rewardExp"] = 50, ["goal"] = 4, ["teamId"] = 2},
+    ["ArrestXCriminal"] = {
+        ["message"] = "function: 0x9058fc281574b5c7",
+        ["rewardExp"] = 50,
+        ["goal"] = "function: 0x527556f2cd0ed4e7",
+        ["teamId"] = 3
+    },
+    ["TazeXCrim"] = {["message"] = "function: 0x3fcec0b6948bdca7", ["rewardExp"] = 30, ["goal"] = 6, ["teamId"] = 3},
+    ["XDestructible"] = {
+        ["message"] = "function: 0x9971fe2edd0db287",
+        ["rewardExp"] = 45,
+        ["goal"] = "function: 0xeaa9487ca5db5b27",
+        ["teamId"] = 2
+    },
+    ["RobOilRig"] = {["message"] = "function: 0x1409c604b8608307", ["rewardExp"] = 60, ["goal"] = 3, ["teamId"] = 2},
+    ["EatDonutNear"] = {
+        ["message"] = "Eat a donut next to a Criminal, 10 times.",
+        ["rewardExp"] = 25,
+        ["goal"] = 10,
+        ["teamId"] = 3
+    },
+    ["StartBank"] = {["message"] = "function: 0x6c0e324dbb87a767", ["rewardExp"] = 35, ["goal"] = 2, ["teamId"] = 2},
+    ["BustBank"] = {
+        ["message"] = "Bust some criminals in a bank robbery!",
+        ["rewardExp"] = 50,
+        ["goal"] = 1,
+        ["teamId"] = 3
+    },
+    ["DealXDamage"] = {
+        ["message"] = "function: 0x28a8311df1cbe167",
+        ["rewardExp"] = 40,
+        ["goal"] = "function: 0x9bd9dfce223e6907",
+        ["teamId"] = 2
+    },
+    ["RobTomb"] = {["message"] = "function: 0x96534d6f504c9c87", ["rewardExp"] = 60, ["goal"] = 4, ["teamId"] = 2},
+    ["PitXPlayers"] = {
+        ["message"] = "function: 0x02c417848ff2b3a7",
+        ["rewardExp"] = 40,
+        ["goal"] = "function: 0x67562d5a57aeb547",
+        ["teamId"] = 3
+    },
+    ["RobCasino"] = {["message"] = "function: 0x2f77df4f45659bc7", ["rewardExp"] = 55, ["goal"] = 3, ["teamId"] = 2},
+    ["PowerPlantNoDie"] = {
+        ["message"] = "Rob the Power Plant twice in one life.",
+        ["rewardExp"] = 60,
+        ["goal"] = 2,
+        ["teamId"] = 2
+    },
+    ["PopCrimTires"] = {["message"] = "function: 0xbccb0de4bac03c47", ["rewardExp"] = 40, ["goal"] = 4, ["teamId"] = 3},
+    ["AccumArrestBounty"] = {
+        ["message"] = "function: 0x187ecd704a7162a7",
+        ["rewardExp"] = 60,
+        ["goal"] = "function: 0x431d84236b6b4087",
+        ["teamId"] = 3
+    },
+    ["DriveDist"] = {["message"] = "function: 0xca72ba0a0b47ffc7", ["rewardExp"] = 30, ["goal"] = 25, ["teamId"] = 2},
+    ["RobSmallStoresNoDie"] = {
+        ["message"] = "Rob two small stores (gas station, donut) without dying!",
+        ["rewardExp"] = 30,
+        ["goal"] = 2,
+        ["teamId"] = 2
+    },
+    ["BankXTimes"] = {
+        ["message"] = "function: 0x34503ebed82fd347",
+        ["rewardExp"] = 60,
+        ["goal"] = "function: 0xd35d8eda44c3f587",
+        ["teamId"] = 2
+    }
+}
