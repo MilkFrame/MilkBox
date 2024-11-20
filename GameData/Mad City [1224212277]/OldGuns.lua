bb400e277243190e6d9c8dc802ccb9292d387bd4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-21 02:28:28 +1100

    Create OldGuns.lua

---
+++ GameData/Mad City [1224212277]/OldGuns.lua
@@ -0,0 +1,298 @@
+data = {
+    ["Pistol Silenced"] = {
+        ["Settings"] = {
+            ["MinAccuracy"] = 0.1,
+            ["Type"] = "Pistol",
+            ["MaxAccuracy"] = 2,
+            ["RateOfFire"] = 0.12,
+            ["FireMode"] = "Single",
+            ["Sounds"] = {["Shoot"] = 9070599756},
+            ["AccuracySettings"] = {
+                ["ShootIncrease"] = 0.27359999999999995,
+                ["ReturnSpeed"] = 3.8,
+                ["AimDownSightAccuracyMultip"] = 0.25
+            },
+            ["BigTextureId"] = 8260393196,
+            ["ReloadTime"] = 1.376,
+            ["Damage"] = 8,
+            ["ClipSize"] = 14,
+            ["CrosshairType"] = "Regular"
+        }
+    },
+    ["Shotgun"] = {
+        ["Settings"] = {
+            ["DamageFalloffEndDamage"] = 1,
+            ["DamageFalloffEndDistance"] = 70,
+            ["RateOfFire"] = 0.65,
+            ["CustomMotor6DPreset"] = "ShotgunDB",
+            ["BurstTime"] = 0,
+            ["Damage"] = 3.3333333333333335,
+            ["MinAccuracy"] = 3,
+            ["Type"] = "Shotgun",
+            ["Burst"] = 18,
+            ["ClipSize"] = 4,
+            ["FireMode"] = "Burst",
+            ["BigTextureId"] = 8260378944,
+            ["DamageFalloffStartDistance"] = 25,
+            ["ReloadTime"] = 2.6,
+            ["MaxAccuracy"] = 8,
+            ["BulletMomentum"] = 45,
+            ["CrosshairType"] = "Regular"
+        }
+    },
+    ["Deagle"] = {
+        ["Settings"] = {
+            ["MinAccuracy"] = 0.1,
+            ["Type"] = "Pistol",
+            ["MaxAccuracy"] = 4.5,
+            ["RateOfFire"] = 0.4,
+            ["FireMode"] = "Single",
+            ["AccuracySettings"] = {
+                ["ShootIncrease"] = 2.112,
+                ["ReturnSpeed"] = 1.9800000000000002,
+                ["AimDownSightAccuracyMultip"] = 0.45
+            },
+            ["BigTextureId"] = 8260257638,
+            ["ReloadTime"] = 1.376,
+            ["Damage"] = 28,
+            ["ClipSize"] = 7,
+            ["CrosshairType"] = "Regular"
+        }
+    },
+    ["Grenade Launcher"] = {
+        ["Settings"] = {
+            ["ExplosionSound"] = "GrenadeLauncherExplode",
+            ["ZeroAngle"] = 0.04,
+            ["RateOfFire"] = 0.32,
+            ["SoundType"] = "GrenadeLauncher",
+            ["Sounds"] = {["Shoot"] = 562411353},
+            ["BulletImpactEffect_ParticleEmitters"] = "function: 0x00000000f7ab8bf7",
+            ["ExplosionRadius"] = 14,
+            ["VehicleDamageMultiplier"] = 2,
+            ["Damage"] = 20,
+            ["MinAccuracy"] = 3.5,
+            ["Type"] = "Rifle",
+            ["MaxAccuracy"] = 10,
+            ["ClipSize"] = 9,
+            ["FireMode"] = "Auto",
+            ["BuyRequirement"] = "function: 0x000000003f123a4f",
+            ["ParticleSettings"] = {
+                ["Color"] = Color3.fromRGB(130, 93, 59),
+                ["Speed"] = 220,
+                ["Gravity"] = Vector3.new(0, -112.1142807006836, 0),
+                ["Size"] = "0.25, 0.150000006",
+                ["LifeTime"] = 6
+            },
+            ["Scrutiny"] = true,
+            ["AccuracySettings"] = {
+                ["ShootIncrease"] = 2.392,
+                ["ReturnSpeed"] = 13,
+                ["AimDownSightAccuracyMultip"] = 0.66
+            },
+            ["BigTextureId"] = 8260289029,
+            ["PressurePower"] = 3,
+            ["Explosion"] = true,
+            ["CrosshairType"] = "Regular"
+        }
+    },
+    ["Famas"] = {
+        ["Settings"] = {
+            ["MinAccuracy"] = 0.7,
+            ["Type"] = "Rifle",
+            ["MaxAccuracy"] = 1.7,
+            ["RateOfFire"] = 0.4,
+            ["FireMode"] = "Burst",
+            ["ClipPerBurst"] = false,
+            ["Sounds"] = {["Shoot"] = 165946283},
+            ["BurstTime"] = 0.05,
+            ["AccuracySettings"] = {
+                ["ShootIncrease"] = 0.43200000000000005,
+                ["ReturnSpeed"] = 2,
+                ["AimDownSightAccuracyMultip"] = 0.2
+            },
+            ["Burst"] = 3,
+            ["BigTextureId"] = 6834304228,
+            ["Damage"] = 15,
+            ["ClipSize"] = 30,
+            ["CrosshairType"] = "Regular"
+        }
+    },
+    ["AK47"] = {
+        ["Settings"] = {
+            ["MinAccuracy"] = 0.7,
+            ["Type"] = "Rifle",
+            ["MaxAccuracy"] = 4,
+            ["RateOfFire"] = 0.1,
+            ["FireMode"] = "Auto",
+            ["AccuracySettings"] = {
+                ["ShootIncrease"] = 0.33,
+                ["ReturnSpeed"] = 4.949999999999999,
+                ["AimDownSightAccuracyMultip"] = 0.22
+            },
+            ["BigTextureId"] = 6816341075,
+            ["Damage"] = 10,
+            ["ClipSize"] = 30,
+            ["CrosshairType"] = "Regular"
+        }
+    },
+    ["MP5"] = {
+        ["Settings"] = {
+            ["MinAccuracy"] = 1,
+            ["Type"] = "Rifle",
+            ["MaxAccuracy"] = 6,
+            ["RateOfFire"] = 0.075,
+            ["FireMode"] = "Auto",
+            ["Sounds"] = {["Shoot"] = 1772743949},
+            ["AccuracySettings"] = {
+                ["ShootIncrease"] = 0.41250000000000003,
+                ["ReturnSpeed"] = 10,
+                ["AimDownSightAccuracyMultip"] = 0.5
+            },
+            ["BigTextureId"] = 8260376480,
+            ["Damage"] = 7,
+            ["ClipSize"] = 40,
+            ["CrosshairType"] = "Regular"
+        }
+    },
+    ["Pistol"] = {
+        ["Settings"] = {
+            ["MinAccuracy"] = 0.1,
+            ["Type"] = "Pistol",
+            ["MaxAccuracy"] = 2,
+            ["RateOfFire"] = 0.2,
+            ["FireMode"] = "Single",
+            ["Sounds"] = {["Shoot"] = 139593133},
+            ["AccuracySettings"] = {
+                ["ShootIncrease"] = 0.45599999999999996,
+                ["ReturnSpeed"] = 3.8,
+                ["AimDownSightAccuracyMultip"] = 0.25
+            },
+            ["BigTextureId"] = 8260363933,
+            ["ReloadTime"] = 1.376,
+            ["Damage"] = 11,
+            ["ClipSize"] = 14,
+            ["CrosshairType"] = "Regular"
+        }
+    },
+    ["TEC9"] = {
+        ["Settings"] = {
+            ["MinAccuracy"] = 0.45,
+            ["Type"] = "Pistol",
+            ["MaxAccuracy"] = 7.5,
+            ["RateOfFire"] = 0.09,
+            ["FireMode"] = "Auto",
+            ["AccuracySettings"] = {
+                ["ShootIncrease"] = 0.67257,
+                ["ReturnSpeed"] = 14.1,
+                ["AimDownSightAccuracyMultip"] = 0.25
+            },
+            ["BigTextureId"] = 8260395524,
+            ["Damage"] = 8,
+            ["ClipSize"] = 20,
+            ["CrosshairType"] = "Regular"
+        }
+    },
+    ["Sniper"] = {
+        ["Settings"] = {
+            ["ZoomEnd"] = "function: 0x000000009555649f",
+            ["ZoomStart"] = "function: 0x00000000cd63fbb7",
+            ["AfterEquip"] = "function: 0x00000000cbf70317",
+            ["RateOfFire"] = 1.3,
+            ["Sounds"] = {["Shoot"] = 8267667681, ["Cocking"] = 146740582},
+            ["Damage"] = 35,
+            ["MinAccuracy"] = 7,
+            ["Type"] = "Sniper",
+            ["MaxAccuracy"] = 7,
+            ["ClipSize"] = 6,
+            ["FireMode"] = "Single",
+            ["EquipTime"] = 1,
+            ["AfterShoot"] = "function: 0x0000000071e8ac7f",
+            ["CameraModeSettings"] = {
+                ["ZoomFOV"] = 19,
+                ["ZoomDistance"] = 0,
+                ["ZoomSensitivity"] = 0.2,
+                ["ZoomCameraOffset"] = CFrame.new(0.150000006, 0.150000006, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+                ["CanZoom"] = true
+            },
+            ["AccuracySettings"] = {["ShootIncrease"] = 0, ["ReturnSpeed"] = 0, ["AimDownSightAccuracyMultip"] = 0},
+            ["BigTextureId"] = 8260390499,
+            ["ReloadTime"] = 2.15,
+            ["MobileAutoFire"] = false,
+            ["HeadshotMultiplier"] = 1.7,
+            ["CrosshairType"] = "Dot"
+        }
+    },
+    ["SCAR"] = {
+        ["Settings"] = {
+            ["MinAccuracy"] = 0.6,
+            ["Type"] = "Rifle",
+            ["MaxAccuracy"] = 2.8,
+            ["RateOfFire"] = 0.17,
+            ["FireMode"] = "Auto",
+            ["Sounds"] = {["Shoot"] = 1112856880},
+            ["BuyRequirement"] = "function: 0x00000000c6055387",
+            ["AccuracySettings"] = {
+                ["ShootIncrease"] = 0.4301,
+                ["ReturnSpeed"] = 4.3999999999999995,
+                ["AimDownSightAccuracyMultip"] = 0.2
+            },
+            ["BigTextureId"] = 8260387310,
+            ["HeadshotMultiplier"] = 1.425,
+            ["Damage"] = 14,
+            ["ClipSize"] = 30,
+            ["CrosshairType"] = "Regular"
+        }
+    },
+    ["AWP"] = {
+        ["Settings"] = {
+            ["ZoomEnd"] = "function: 0x00000000edcbb98f",
+            ["ZoomStart"] = "function: 0x0000000035a94487",
+            ["AfterEquip"] = "function: 0x0000000091af6e9f",
+            ["RateOfFire"] = 1.6,
+            ["CameraShakeOnReload"] = false,
+            ["Sounds"] = {["Shoot"] = 188847826, ["Cocking"] = 146740582, ["Reload"] = 174295321},
+            ["Damage"] = 50,
+            ["MinAccuracy"] = 7,
+            ["Type"] = "Sniper",
+            ["MaxAccuracy"] = 7,
+            ["BuyRequirement"] = "function: 0x00000000d1885fef",
+            ["ClipSize"] = 4,
+            ["FireMode"] = "Single",
+            ["EquipTime"] = 1,
+            ["AfterShoot"] = "function: 0x00000000d6ba3ca7",
+            ["CameraModeSettings"] = {
+                ["ZoomFOV"] = 19,
+                ["ZoomDistance"] = 0,
+                ["ZoomSensitivity"] = 0.1,
+                ["ZoomCameraOffset"] = CFrame.new(0.150000006, 0.150000006, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+                ["CanZoom"] = true
+            },
+            ["AccuracySettings"] = {["ShootIncrease"] = 0, ["ReturnSpeed"] = 0, ["AimDownSightAccuracyMultip"] = 0},
+            ["BigTextureId"] = 6834020125,
+            ["ReloadTime"] = 2.15,
+            ["MobileAutoFire"] = false,
+            ["HeadshotMultiplier"] = 1.6,
+            ["CrosshairType"] = "Dot"
+        }
+    },
+    ["M4A1"] = {
+        ["Settings"] = {
+            ["MinAccuracy"] = 0.9,
+            ["Type"] = "Rifle",
+            ["MaxAccuracy"] = 5,
+            ["RateOfFire"] = 0.09,
+            ["FireMode"] = "Auto",
+            ["Sounds"] = {["Shoot"] = 241178067},
+            ["AccuracySettings"] = {
+                ["ShootIncrease"] = 0.4059,
+                ["ReturnSpeed"] = 8.2,
+                ["AimDownSightAccuracyMultip"] = 0.22
+            },
+            ["BuyRequirement"] = "function: 0x000000005c6272bf",
+            ["BigTextureId"] = 8260367121,
+            ["Damage"] = 10,
+            ["ClipSize"] = 30,
+            ["CrosshairType"] = "Regular"
+        }
+    }
+}
