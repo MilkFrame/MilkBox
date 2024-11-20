b49cb9d124d479207ea8f4f092935fcdb552d61a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-21 02:30:28 +1100

    Create Wallpapers.lua

---
+++ GameData/Mad City [1224212277]/Wallpapers.lua
@@ -0,0 +1,607 @@
+data = {
+    ["Wall23"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall23",
+            ["ItemName"] = "Art White",
+            ["FurnitureID"] = "Wall23",
+            ["Price"] = 15000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610970432
+        }
+    },
+    ["Wall26"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall26",
+            ["ItemName"] = "Color Breeze",
+            ["FurnitureID"] = "Wall26",
+            ["Price"] = 7000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610970899
+        }
+    },
+    ["Wall20"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall20",
+            ["ItemName"] = "Grey Illusion",
+            ["FurnitureID"] = "Wall20",
+            ["Price"] = 15000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610969979
+        }
+    },
+    ["Wall51"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall51",
+            ["ItemName"] = "Yellow",
+            ["FurnitureID"] = "Wall51",
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(236, 236, 13),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 2288897316
+        }
+    },
+    ["Wall44"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall44",
+            ["ItemName"] = "Green Geo",
+            ["FurnitureID"] = "Wall44",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610973437
+        }
+    },
+    ["Wall55"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall55",
+            ["ItemName"] = "Black",
+            ["FurnitureID"] = "Wall55",
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(0, 0, 0),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 2288897316
+        }
+    },
+    ["Wall22"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall22",
+            ["ItemName"] = "Art Lines",
+            ["FurnitureID"] = "Wall22",
+            ["Price"] = 15000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610970281
+        }
+    },
+    ["Wall9"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall9",
+            ["ItemName"] = "Geometric Blast",
+            ["FurnitureID"] = "Wall9",
+            ["Price"] = 7000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610968708
+        }
+    },
+    ["Wall54"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall54",
+            ["ItemName"] = "Grey",
+            ["FurnitureID"] = "Wall54",
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(93, 93, 93),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 2288897316
+        }
+    },
+    ["Wall21"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall21",
+            ["ItemName"] = "Black Illusion",
+            ["FurnitureID"] = "Wall21",
+            ["Price"] = 15000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610970080
+        }
+    },
+    ["Wall2"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall2",
+            ["ItemName"] = "Digital Breeze",
+            ["FurnitureID"] = "Wall2",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610967587
+        }
+    },
+    ["Wall16"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall16",
+            ["ItemName"] = "Polka Mix",
+            ["FurnitureID"] = "Wall16",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610969519
+        }
+    },
+    ["Wall18"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall18",
+            ["ItemName"] = "Red Illusion",
+            ["FurnitureID"] = "Wall18",
+            ["Price"] = 15000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610969761
+        }
+    },
+    ["Wall42"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall42",
+            ["ItemName"] = "Pink Geo",
+            ["FurnitureID"] = "Wall42",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610972956
+        }
+    },
+    ["Wall45"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall45",
+            ["ItemName"] = "Grey Geo",
+            ["FurnitureID"] = "Wall45",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610973645
+        }
+    },
+    ["Wall50"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall50",
+            ["ItemName"] = "Pink",
+            ["FurnitureID"] = "Wall50",
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(182, 42, 164),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 2288897316
+        }
+    },
+    ["Wall52"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall52",
+            ["ItemName"] = "Orange",
+            ["FurnitureID"] = "Wall52",
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(236, 153, 36),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 2288897316
+        }
+    },
+    ["Wall29"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall29",
+            ["ItemName"] = "Green Breeze",
+            ["FurnitureID"] = "Wall29",
+            ["Price"] = 7000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610971289
+        }
+    },
+    ["Wall48"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall48",
+            ["ItemName"] = "Green",
+            ["FurnitureID"] = "Wall48",
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(43, 156, 35),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 2288897316
+        }
+    },
+    ["Wall41"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall41",
+            ["ItemName"] = "Twirly Swirl",
+            ["FurnitureID"] = "Wall41",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610972734
+        }
+    },
+    ["Wall47"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall47",
+            ["ItemName"] = "Red",
+            ["FurnitureID"] = "Wall47",
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(255, 64, 67),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 2288897316
+        }
+    },
+    ["Wall32"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall32",
+            ["ItemName"] = "Rainbow",
+            ["FurnitureID"] = "Wall32",
+            ["Price"] = 7000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610971695
+        }
+    },
+    ["Wall49"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall49",
+            ["ItemName"] = "Lavender",
+            ["FurnitureID"] = "Wall49",
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(150, 123, 182),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 2288897316
+        }
+    },
+    ["Wall28"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall28",
+            ["ItemName"] = "Sea Breeze",
+            ["FurnitureID"] = "Wall28",
+            ["Price"] = 7000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610971171
+        }
+    },
+    ["Wall17"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall17",
+            ["ItemName"] = "Orange Illusion",
+            ["FurnitureID"] = "Wall17",
+            ["Price"] = 15000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610969643
+        }
+    },
+    ["Wall43"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall43",
+            ["ItemName"] = "Orange Geo",
+            ["FurnitureID"] = "Wall43",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610973190
+        }
+    },
+    ["Wall13"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall13",
+            ["ItemName"] = "Polka Blast",
+            ["FurnitureID"] = "Wall13",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610969257
+        }
+    },
+    ["Wall15"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall15",
+            ["ItemName"] = "Polka Light",
+            ["FurnitureID"] = "Wall15",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610969422
+        }
+    },
+    ["Wall53"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall53",
+            ["ItemName"] = "White",
+            ["FurnitureID"] = "Wall53",
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(255, 255, 255),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 2288897316
+        }
+    },
+    ["Wall1"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall1",
+            ["ItemName"] = "Digital Brown",
+            ["FurnitureID"] = "Wall1",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610967358
+        }
+    },
+    ["Wall3"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall3",
+            ["ItemName"] = "Digital Summer",
+            ["FurnitureID"] = "Wall3",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610967904
+        }
+    },
+    ["Wall24"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall24",
+            ["ItemName"] = "Art Blast",
+            ["FurnitureID"] = "Wall24",
+            ["Price"] = 15000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610970566
+        }
+    },
+    ["Wall38"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall38",
+            ["ItemName"] = "Blue Swirl",
+            ["FurnitureID"] = "Wall38",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610972356
+        }
+    },
+    ["Wall11"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall11",
+            ["ItemName"] = "Citric Squeeze",
+            ["FurnitureID"] = "Wall11",
+            ["Price"] = 7000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610968971
+        }
+    },
+    ["Wall39"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall39",
+            ["ItemName"] = "Black Swirl",
+            ["FurnitureID"] = "Wall39",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610972483
+        }
+    },
+    ["Wall27"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall27",
+            ["ItemName"] = "Water Breeze",
+            ["FurnitureID"] = "Wall27",
+            ["Price"] = 7000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610971049
+        }
+    },
+    ["Wall40"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall40",
+            ["ItemName"] = "Funky Swirl",
+            ["FurnitureID"] = "Wall40",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610972602
+        }
+    },
+    ["Wall37"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall37",
+            ["ItemName"] = "Pastel Fixed",
+            ["FurnitureID"] = "Wall37",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610972219
+        }
+    },
+    ["Wall36"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall36",
+            ["ItemName"] = "Blue Fixed",
+            ["FurnitureID"] = "Wall36",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610972127
+        }
+    },
+    ["Wall35"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall35",
+            ["ItemName"] = "Fixed Lines",
+            ["FurnitureID"] = "Wall35",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610972012
+        }
+    },
+    ["Wall8"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall8",
+            ["ItemName"] = "Geometric Brown",
+            ["FurnitureID"] = "Wall8",
+            ["Price"] = 7000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610968588
+        }
+    },
+    ["Wall33"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall33",
+            ["ItemName"] = "Dark Rainbow",
+            ["FurnitureID"] = "Wall33",
+            ["Price"] = 7000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610971794
+        }
+    },
+    ["Wall25"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall25",
+            ["ItemName"] = "Color Blast",
+            ["FurnitureID"] = "Wall25",
+            ["Price"] = 7000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610970706
+        }
+    },
+    ["Wall14"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall14",
+            ["ItemName"] = "Polka Colors",
+            ["FurnitureID"] = "Wall14",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610969343
+        }
+    },
+    ["Wall46"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall46",
+            ["ItemName"] = "Blue",
+            ["FurnitureID"] = "Wall46",
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(87, 65, 255),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 2288897316
+        }
+    },
+    ["Wall31"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall31",
+            ["ItemName"] = "Rainbow Breeze",
+            ["FurnitureID"] = "Wall31",
+            ["Price"] = 7000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610971526
+        }
+    },
+    ["Wall34"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall34",
+            ["ItemName"] = "Bright Rainbow",
+            ["FurnitureID"] = "Wall34",
+            ["Price"] = 7000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610971897
+        }
+    },
+    ["Wall6"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall6",
+            ["ItemName"] = "Geometry Shapes",
+            ["FurnitureID"] = "Wall6",
+            ["Price"] = 7000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610968433
+        }
+    },
+    ["Wall30"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall30",
+            ["ItemName"] = "White Breeze",
+            ["FurnitureID"] = "Wall30",
+            ["Price"] = 7000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610971407
+        }
+    },
+    ["Wall5"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall5",
+            ["ItemName"] = "Digital Purple",
+            ["FurnitureID"] = "Wall5",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610968206
+        }
+    },
+    ["Wall4"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall4",
+            ["ItemName"] = "Digital Lava",
+            ["FurnitureID"] = "Wall4",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610968087
+        }
+    },
+    ["Wall7"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall7",
+            ["ItemName"] = "Digital Grey",
+            ["FurnitureID"] = "Wall7",
+            ["Price"] = 10000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610968330
+        }
+    },
+    ["Wall10"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall10",
+            ["ItemName"] = "Citric Blast",
+            ["FurnitureID"] = "Wall10",
+            ["Price"] = 7000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610968828
+        }
+    },
+    ["Wall19"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall19",
+            ["ItemName"] = "Green Illusion",
+            ["FurnitureID"] = "Wall19",
+            ["Price"] = 15000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610969875
+        }
+    },
+    ["Wall12"] = {
+        ["Data"] = {
+            ["AssetID"] = "Wall12",
+            ["ItemName"] = "Citric Mix",
+            ["FurnitureID"] = "Wall12",
+            ["Price"] = 7000,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Type"] = "Wallpaper",
+            ["Texture"] = 4610969090
+        }
+    }
+}
