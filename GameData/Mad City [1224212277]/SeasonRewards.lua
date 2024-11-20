3ccb1b07fc254dc295832181cdb208cb21e6c324
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-21 02:27:53 +1100

    Create SeasonRewards.lua

---
+++ GameData/Mad City [1224212277]/SeasonRewards.lua
@@ -0,0 +1,55 @@
+data = {
+    ["Season"] = 2,
+    ["Data"] = {
+        {
+            ["VehicleSkins"] = "SMTD58",
+            ["Level"] = 5,
+            ["Title"] = "Aurora Borealis",
+            ["Icon"] = "rbxassetid://11883617076"
+        },
+        {["Icon"] = "rbxassetid://11883632898", ["Level"] = 10, ["Title"] = "Snow Pile", ["Toppers"] = "TOP14"},
+        {["Spoilers"] = "SP52", ["Level"] = 15, ["Title"] = "Ski Spoiler", ["Icon"] = "rbxassetid://11883641148"},
+        {["Rims"] = "RP6", ["Level"] = 20, ["Title"] = "Snowman Rims", ["Icon"] = "rbxassetid://11883648933"},
+        {["Ornaments"] = "ORN3", ["Level"] = 25, ["Title"] = "Ice Deer", ["Icon"] = "rbxassetid://11883655976"},
+        {["Icon"] = "rbxassetid://11884411176", ["Level"] = 30, ["Title"] = "Airwalk Emote", ["Emotes"] = "The Airwalk"},
+        {["WeaponSkins"] = "W22_4", ["Level"] = 35, ["Title"] = "Digital Camo", ["Icon"] = "rbxassetid://11883715442"},
+        {
+            ["VehicleBoostParticles"] = "BST47",
+            ["Level"] = 40,
+            ["Title"] = "SnowFlakes",
+            ["Icon"] = "rbxassetid://11883735102"
+        },
+        {["Ornaments"] = "ORN8", ["Level"] = 45, ["Title"] = "Horns of Winter", ["Icon"] = "rbxassetid://11883749897"},
+        {["Vehicle"] = "Odyssey", ["Level"] = 50, ["Title"] = "Odyssey", ["Icon"] = "rbxassetid://11883767756"},
+        {
+            ["VehicleSkins"] = "SMTD57",
+            ["Level"] = 55,
+            ["Title"] = "Frosted Winds",
+            ["Icon"] = "rbxassetid://11883769999"
+        },
+        {["Icon"] = "rbxassetid://11884411110", ["Level"] = 60, ["Title"] = "Lean With It", ["Emotes"] = "Lean With It"},
+        {["Spoilers"] = "SP51", ["Level"] = 65, ["Title"] = "Frosted Spoiler", ["Icon"] = "rbxassetid://11883780795"},
+        {["Icon"] = "rbxassetid://8142687230", ["Level"] = 70, ["Title"] = "Pengu", ["Toppers"] = "TOP9"},
+        {
+            ["VehicleSkins"] = "SMTD59",
+            ["Level"] = 75,
+            ["Title"] = "Odyssey Car Skin",
+            ["Icon"] = "rbxassetid://11901397593"
+        },
+        {["Rims"] = "RP9", ["Level"] = 80, ["Title"] = "Snow-Chained Tires", ["Icon"] = "rbxassetid://11893864623"},
+        {["Ornaments"] = "ORN6", ["Level"] = 85, ["Title"] = "Snowman Ornament", ["Icon"] = "rbxassetid://11883800792"},
+        {["Icon"] = "rbxassetid://11884411242", ["Level"] = 90, ["Title"] = "The Schwifty", ["Emotes"] = "The Schwifty"},
+        {
+            ["VehicleBoostParticles"] = "BST48",
+            ["Level"] = 95,
+            ["Title"] = "Purple Snow Void",
+            ["Icon"] = "rbxassetid://11883828680"
+        },
+        {
+            ["Vehicle"] = "Edgerunner V1",
+            ["Level"] = 100,
+            ["Title"] = "Edge Runner",
+            ["Icon"] = "rbxassetid://11883819186"
+        }
+    }
+}
