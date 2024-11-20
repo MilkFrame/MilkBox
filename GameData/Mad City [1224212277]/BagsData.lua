7c05c9024d7b10203cfb2103ff78ec8a82a3616f
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-21 02:27:04 +1100

    Create BagsData.lua

---
+++ GameData/Mad City [1224212277]/BagsData.lua
@@ -0,0 +1,196 @@
+data = {
+    ["Data"] = {
+        ["NBD10"] = {
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(44, 101, 29),
+            ["Name"] = "Dark Green",
+            ["Texture"] = 0
+        },
+        ["DF"] = {
+            ["WeaponID"] = "Bags",
+            ["Reflectance"] = 0,
+            ["Price"] = 0,
+            ["Transparency"] = 0,
+            ["Name"] = "Default",
+            ["Color"] = BrickColor.new("Earth green"),
+            ["Locked"] = true,
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["Offsale"] = true,
+            ["Texture"] = 0
+        },
+        ["NBD25"] = {
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(255, 152, 220),
+            ["Name"] = "Pastel Pink",
+            ["Texture"] = 0
+        },
+        ["NBD24"] = {
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(255, 102, 204),
+            ["Name"] = "Bright Pink",
+            ["Texture"] = 0
+        },
+        ["NBD22"] = {["Price"] = 5000, ["Color"] = Color3.fromRGB(108, 88, 75), ["Name"] = "Brown", ["Texture"] = 0},
+        ["NBD15"] = {
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(104, 144, 172),
+            ["Name"] = "Pastel Blue",
+            ["Texture"] = 0
+        },
+        ["NBD7"] = {
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(239, 184, 56),
+            ["Name"] = "Dark Yellow",
+            ["Texture"] = 0
+        },
+        ["BAG4"] = {
+            ["WeaponID"] = "Bags",
+            ["Reflectance"] = 0,
+            ["Price"] = 5000,
+            ["Transparency"] = 0,
+            ["Name"] = "Yellow",
+            ["Color"] = BrickColor.new("Olive"),
+            ["Locked"] = true,
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["Offsale"] = true,
+            ["Texture"] = 0
+        },
+        ["NBD2"] = {["Price"] = 5000, ["Color"] = Color3.fromRGB(196, 40, 28), ["Name"] = "Bright Red", ["Texture"] = 0},
+        ["BAG6"] = {
+            ["WeaponID"] = "Bags",
+            ["Reflectance"] = 0,
+            ["Price"] = 5000,
+            ["Transparency"] = 0,
+            ["Name"] = "Copper",
+            ["Color"] = BrickColor.new("Copper"),
+            ["Locked"] = true,
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["Offsale"] = true,
+            ["Texture"] = 0
+        },
+        ["NBD23"] = {["Price"] = 5000, ["Color"] = Color3.fromRGB(255, 0, 191), ["Name"] = "Dark Pink", ["Texture"] = 0},
+        ["NBD1"] = {["Price"] = 5000, ["Color"] = Color3.fromRGB(117, 0, 0), ["Name"] = "Dark Red", ["Texture"] = 0},
+        ["NBD21"] = {["Price"] = 5000, ["Color"] = Color3.fromRGB(204, 142, 105), ["Name"] = "Tan", ["Texture"] = 0},
+        ["NBD8"] = {
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(245, 205, 48),
+            ["Name"] = "Bright Yellow",
+            ["Texture"] = 0
+        },
+        ["NBD5"] = {
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(218, 133, 65),
+            ["Name"] = "Bright Orange",
+            ["Texture"] = 0
+        },
+        ["NBD20"] = {["Price"] = 5000, ["Color"] = Color3.fromRGB(248, 248, 248), ["Name"] = "White", ["Texture"] = 0},
+        ["NBD13"] = {["Price"] = 5000, ["Color"] = Color3.fromRGB(0, 16, 176), ["Name"] = "Dark Blue", ["Texture"] = 0},
+        ["BAG7"] = {
+            ["WeaponID"] = "Bags",
+            ["Reflectance"] = 0,
+            ["Price"] = 5000,
+            ["Transparency"] = 0,
+            ["Name"] = "Pink",
+            ["Color"] = BrickColor.new("Hot pink"),
+            ["Locked"] = true,
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["Offsale"] = true,
+            ["Texture"] = 0
+        },
+        ["NBD19"] = {["Price"] = 5000, ["Color"] = Color3.fromRGB(27, 42, 53), ["Name"] = "Black", ["Texture"] = 0},
+        ["NBD16"] = {
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(89, 34, 89),
+            ["Name"] = "Dark Purple",
+            ["Texture"] = 0
+        },
+        ["BAG3"] = {
+            ["WeaponID"] = "Bags",
+            ["Reflectance"] = 0,
+            ["Price"] = 5000,
+            ["Transparency"] = 0,
+            ["Name"] = "Blue",
+            ["Color"] = BrickColor.new("Deep blue"),
+            ["Locked"] = true,
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["Offsale"] = true,
+            ["Texture"] = 0
+        },
+        ["NBD17"] = {
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(140, 91, 159),
+            ["Name"] = "Bright Purple",
+            ["Texture"] = 0
+        },
+        ["NBD14"] = {
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(13, 105, 172),
+            ["Name"] = "Bright Blue",
+            ["Texture"] = 0
+        },
+        ["NBD18"] = {
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(154, 143, 159),
+            ["Name"] = "Pastel Purple",
+            ["Texture"] = 0
+        },
+        ["NBD9"] = {
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(245, 225, 149),
+            ["Name"] = "Pastel Yellow",
+            ["Texture"] = 0
+        },
+        ["BAG5"] = {
+            ["WeaponID"] = "Bags",
+            ["Reflectance"] = 0,
+            ["Price"] = 5000,
+            ["Transparency"] = 0,
+            ["Name"] = "Purple",
+            ["Color"] = BrickColor.new("Royal purple"),
+            ["Locked"] = true,
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["Offsale"] = true,
+            ["Texture"] = 0
+        },
+        ["BAG2"] = {
+            ["WeaponID"] = "Bags",
+            ["Reflectance"] = 0,
+            ["Price"] = 5000,
+            ["Transparency"] = 0,
+            ["Name"] = "Red",
+            ["Color"] = BrickColor.new("Maroon"),
+            ["Locked"] = true,
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["Offsale"] = true,
+            ["Texture"] = 0
+        },
+        ["NBD12"] = {
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(121, 151, 121),
+            ["Name"] = "Pastel Green",
+            ["Texture"] = 0
+        },
+        ["NBD4"] = {["Price"] = 5000, ["Color"] = Color3.fromRGB(170, 85, 0), ["Name"] = "Dark Orange", ["Texture"] = 0},
+        ["NBD6"] = {
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(218, 170, 133),
+            ["Name"] = "Pastel Orange",
+            ["Texture"] = 0
+        },
+        ["NBD3"] = {
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(196, 124, 119),
+            ["Name"] = "Pastel Red",
+            ["Texture"] = 0
+        },
+        ["NBD11"] = {
+            ["Price"] = 5000,
+            ["Color"] = Color3.fromRGB(75, 151, 75),
+            ["Name"] = "Bright Green",
+            ["Texture"] = 0
+        }
+    },
+    ["Icon"] = "rbxassetid://7234392997",
+    ["Description"] = "Steal cash-n-diamonds in style!",
+    ["DisplayName"] = "Bags"
+}
