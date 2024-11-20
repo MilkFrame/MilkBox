93d70225e56b4eb1deb1d6f894400edc159c9ef1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-21 02:28:14 +1100

    Update SeasonRewards.lua

--- GameData/Mad City [1224212277]/SeasonRewards.lua
+++ GameData/Mad City [1224212277]/SeasonRewards.lua
@@ -1,55 +1,50 @@
 data = {
-    ["Season"] = 2,
+    ["Season"] = 3,
     ["Data"] = {
+        {["Ornaments"] = "ORN25", ["Level"] = 5, ["Title"] = "Orn Spikes", ["Icon"] = "rbxassetid://15702868295"},
+        {["Spoilers"] = "SP69", ["Level"] = 10, ["Title"] = "Back Spikes", ["Icon"] = "rbxassetid://15702862125"},
         {
-            ["VehicleSkins"] = "SMTD58",
-            ["Level"] = 5,
-            ["Title"] = "Aurora Borealis",
-            ["Icon"] = "rbxassetid://11883617076"
+            ["Icon"] = "rbxassetid://15702869031",
+            ["Level"] = 15,
+            ["Title"] = "Front Spikes Of Doom",
+            ["Toppers"] = "TOP52"
         },
-        {["Icon"] = "rbxassetid://11883632898", ["Level"] = 10, ["Title"] = "Snow Pile", ["Toppers"] = "TOP14"},
-        {["Spoilers"] = "SP52", ["Level"] = 15, ["Title"] = "Ski Spoiler", ["Icon"] = "rbxassetid://11883641148"},
-        {["Rims"] = "RP6", ["Level"] = 20, ["Title"] = "Snowman Rims", ["Icon"] = "rbxassetid://11883648933"},
-        {["Ornaments"] = "ORN3", ["Level"] = 25, ["Title"] = "Ice Deer", ["Icon"] = "rbxassetid://11883655976"},
-        {["Icon"] = "rbxassetid://11884411176", ["Level"] = 30, ["Title"] = "Airwalk Emote", ["Emotes"] = "The Airwalk"},
-        {["WeaponSkins"] = "W22_4", ["Level"] = 35, ["Title"] = "Digital Camo", ["Icon"] = "rbxassetid://11883715442"},
+        {["Ornaments"] = "ORN33", ["Level"] = 20, ["Title"] = "Sawbliterator", ["Icon"] = "rbxassetid://15702863834"},
+        {["Spoilers"] = "SP70", ["Level"] = 25, ["Title"] = "Tire Holder", ["Icon"] = "rbxassetid://15702866699"},
+        {["Rims"] = "R95", ["Level"] = 30, ["Title"] = "Spike Wheels", ["Icon"] = "rbxassetid://15702866015"},
+        {["Rims"] = "R93", ["Level"] = 35, ["Title"] = "Skull rim", ["Icon"] = "rbxassetid://15702860569"},
+        {["Rims"] = "R94", ["Level"] = 40, ["Title"] = "Spike Tire", ["Icon"] = "rbxassetid://15702865270"},
+        {["Rims"] = "R92", ["Level"] = 45, ["Title"] = "Spiked Rims", ["Icon"] = "rbxassetid://15702859831"},
+        {["Icon"] = "rbxassetid://15702858979", ["Level"] = 50, ["Title"] = "Rack O Junk", ["Toppers"] = "TOP50"},
+        {["Icon"] = "rbxassetid://15702869833", ["Level"] = 55, ["Title"] = "Not A Flamethrower", ["Toppers"] = "TOP51"},
         {
-            ["VehicleBoostParticles"] = "BST47",
-            ["Level"] = 40,
-            ["Title"] = "SnowFlakes",
-            ["Icon"] = "rbxassetid://11883735102"
+            ["Ornaments"] = "ORN31",
+            ["Level"] = 60,
+            ["Title"] = "Twin Supercharger",
+            ["Icon"] = "rbxassetid://15702861393"
         },
-        {["Ornaments"] = "ORN8", ["Level"] = 45, ["Title"] = "Horns of Winter", ["Icon"] = "rbxassetid://11883749897"},
-        {["Vehicle"] = "Odyssey", ["Level"] = 50, ["Title"] = "Odyssey", ["Icon"] = "rbxassetid://11883767756"},
+        {["Icon"] = "rbxassetid://15715916456", ["Level"] = 65, ["Title"] = "Tenga Turret", ["Toppers"] = "TOP54"},
+        {["Rims"] = "R96", ["Level"] = 70, ["Title"] = "Mud Dapper", ["Icon"] = "rbxassetid://15715918519"},
+        {["Rims"] = "R97", ["Level"] = 75, ["Title"] = "Mud Ringer", ["Icon"] = "rbxassetid://15715917770"},
+        {["Spoilers"] = "SP71", ["Level"] = 80, ["Title"] = "Scrap Spoiler", ["Icon"] = "rbxassetid://15715919313"},
+        {["Icon"] = "rbxassetid://15715917041", ["Level"] = 85, ["Title"] = "Oil Well", ["Toppers"] = "TOP53"},
+        {["Ornaments"] = "ORN28", ["Level"] = 90, ["Title"] = "UnimatrixZero", ["Icon"] = "rbxassetid://15702867517"},
+        {["Ornaments"] = "ORN29", ["Level"] = 95, ["Title"] = "ServoSkull", ["Icon"] = "rbxassetid://15702864592"},
         {
-            ["VehicleSkins"] = "SMTD57",
-            ["Level"] = 55,
-            ["Title"] = "Frosted Winds",
-            ["Icon"] = "rbxassetid://11883769999"
-        },
-        {["Icon"] = "rbxassetid://11884411110", ["Level"] = 60, ["Title"] = "Lean With It", ["Emotes"] = "Lean With It"},
-        {["Spoilers"] = "SP51", ["Level"] = 65, ["Title"] = "Frosted Spoiler", ["Icon"] = "rbxassetid://11883780795"},
-        {["Icon"] = "rbxassetid://8142687230", ["Level"] = 70, ["Title"] = "Pengu", ["Toppers"] = "TOP9"},
-        {
-            ["VehicleSkins"] = "SMTD59",
-            ["Level"] = 75,
-            ["Title"] = "Odyssey Car Skin",
-            ["Icon"] = "rbxassetid://11901397593"
-        },
-        {["Rims"] = "RP9", ["Level"] = 80, ["Title"] = "Snow-Chained Tires", ["Icon"] = "rbxassetid://11893864623"},
-        {["Ornaments"] = "ORN6", ["Level"] = 85, ["Title"] = "Snowman Ornament", ["Icon"] = "rbxassetid://11883800792"},
-        {["Icon"] = "rbxassetid://11884411242", ["Level"] = 90, ["Title"] = "The Schwifty", ["Emotes"] = "The Schwifty"},
-        {
-            ["VehicleBoostParticles"] = "BST48",
-            ["Level"] = 95,
-            ["Title"] = "Purple Snow Void",
-            ["Icon"] = "rbxassetid://11883828680"
-        },
-        {
-            ["Vehicle"] = "Edgerunner V1",
+            ["Spoilers"] = "SP68",
             ["Level"] = 100,
-            ["Title"] = "Edge Runner",
-            ["Icon"] = "rbxassetid://11883819186"
-        }
+            ["Title"] = "Motor Bike Spoiler",
+            ["Icon"] = "rbxassetid://15702863000"
+        },
+        {["Vehicle"] = "RoadRager", ["Level"] = 105, ["Title"] = "Mad Max!", ["Icon"] = "rbxassetid://15702874098"},
+        {["Vehicle"] = "ScrapCar", ["Level"] = 110, ["Title"] = "Scrap Car", ["Icon"] = "rbxassetid://15702873369"},
+        {["Vehicle"] = "SlideStorm", ["Level"] = 115, ["Title"] = "Slide Storm", ["Icon"] = "rbxassetid://15702875721"},
+        {
+            ["Vehicle"] = "Combine Shredder",
+            ["Level"] = 120,
+            ["Title"] = "Combine Shredder",
+            ["Icon"] = "rbxassetid://15702889872"
+        },
+        {["Vehicle"] = "Jackalope", ["Level"] = 125, ["Title"] = "Jackalope", ["Icon"] = "rbxassetid://15702876315"}
     }
 }
