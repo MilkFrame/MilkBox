b36aabb8ca03a713e3e338b6a5fdebff607b1b7a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-21 02:30:40 +1100

    Create Homes.lua

---
+++ GameData/Mad City [1224212277]/Homes.lua
@@ -0,0 +1,47 @@
+data = {
+    ["Sea Breeze House"] = {
+        ["Data"] = {
+            ["AssetID"] = "Sea Breeze House",
+            ["ItemName"] = "Sea Breeze House",
+            ["Price"] = 2000000,
+            ["VehicleID"] = "Datas",
+            ["Type"] = "House"
+        }
+    },
+    ["Basic Home"] = {
+        ["Data"] = {
+            ["AssetID"] = "Basic Home",
+            ["ItemName"] = "Basic Home",
+            ["Price"] = 0,
+            ["VehicleID"] = "Datas",
+            ["Type"] = "House"
+        }
+    },
+    ["Casino Park Home"] = {
+        ["Data"] = {
+            ["AssetID"] = "Casino Park Home",
+            ["ItemName"] = "Resort Park Home",
+            ["Price"] = 100000,
+            ["VehicleID"] = "Datas",
+            ["Type"] = "House"
+        }
+    },
+    ["Utopia Towers Penthouse"] = {
+        ["Data"] = {
+            ["AssetID"] = "Utopia Towers Penthouse",
+            ["ItemName"] = "Utopia Towers Penthouse",
+            ["Price"] = 5000000,
+            ["VehicleID"] = "Datas",
+            ["Type"] = "House"
+        }
+    },
+    ["Grand Park Apartment"] = {
+        ["Data"] = {
+            ["AssetID"] = "Grand Park Apartment",
+            ["ItemName"] = "Grand Park Apartment",
+            ["Price"] = 750000,
+            ["VehicleID"] = "Datas",
+            ["Type"] = "House"
+        }
+    }
+}
