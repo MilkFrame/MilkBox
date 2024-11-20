dfdf988bff42f168fb6e069c687127e928a6d416
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-21 02:27:29 +1100

    Create MasksData.lua

---
+++ GameData/Mad City [1224212277]/MasksData.lua
@@ -0,0 +1,118 @@
+data = {
+    ["Data"] = {
+        ["MSK8"] = {
+            ["Reflectance"] = 0,
+            ["Transparency"] = 0,
+            ["Name"] = "Jack-O-Lantern",
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Price"] = 15000,
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["WeaponID"] = "Masks",
+            ["Texture"] = 4752971812
+        },
+        ["DF"] = {
+            ["WeaponID"] = "Masks",
+            ["Reflectance"] = 0,
+            ["Price"] = 0,
+            ["NoMask"] = true,
+            ["Transparency"] = 1,
+            ["Name"] = "No Mask",
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Texture"] = 0
+        },
+        ["MSK3"] = {
+            ["Reflectance"] = 0,
+            ["Transparency"] = 0,
+            ["Name"] = "Fire",
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Price"] = 7500,
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["WeaponID"] = "Masks",
+            ["Texture"] = 4752964115
+        },
+        ["MSK6"] = {
+            ["Reflectance"] = 0,
+            ["Transparency"] = 0,
+            ["Name"] = "Cheetah Pink",
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Price"] = 12500,
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["WeaponID"] = "Masks",
+            ["Texture"] = 3180159283
+        },
+        ["MSK5"] = {
+            ["Reflectance"] = 0,
+            ["Transparency"] = 0,
+            ["Name"] = "Blue Camo",
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Price"] = 12500,
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["WeaponID"] = "Masks",
+            ["Texture"] = 4752968779
+        },
+        ["MSK9"] = {
+            ["Reflectance"] = 0,
+            ["Transparency"] = 0,
+            ["Name"] = "Purple Lightning",
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Price"] = 20000,
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["WeaponID"] = "Masks",
+            ["Texture"] = 4752972941
+        },
+        ["MSK10"] = {
+            ["Reflectance"] = 0,
+            ["Transparency"] = 0,
+            ["Name"] = "Blue Circuit",
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Price"] = 7500,
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["WeaponID"] = "Masks",
+            ["Texture"] = 3204462145
+        },
+        ["MSK2"] = {
+            ["Reflectance"] = 0,
+            ["Transparency"] = 0,
+            ["Name"] = "Caution",
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Price"] = 10000,
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["WeaponID"] = "Masks",
+            ["Texture"] = 3180158274
+        },
+        ["MSK11"] = {
+            ["Reflectance"] = 0,
+            ["Transparency"] = 0,
+            ["Name"] = "Blue Hex",
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Price"] = 12500,
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["WeaponID"] = "Masks",
+            ["Texture"] = 4655308935
+        },
+        ["MSK7"] = {
+            ["Reflectance"] = 0,
+            ["Transparency"] = 0,
+            ["Name"] = "Black Hex",
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Price"] = 15000,
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["WeaponID"] = "Masks",
+            ["Texture"] = 4752970953
+        },
+        ["MSK4"] = {
+            ["Reflectance"] = 0,
+            ["Transparency"] = 0,
+            ["Name"] = "Yellow Diamond",
+            ["Color"] = BrickColor.new("Bright red"),
+            ["Price"] = 20000,
+            ["Material"] = Enum.Material.SmoothPlastic,
+            ["WeaponID"] = "Masks",
+            ["Texture"] = 4752998500
+        }
+    },
+    ["Icon"] = "rbxassetid://7234393580",
+    ["Description"] = "Perfect for 'peaceful protests'!",
+    ["DisplayName"] = "Masks"
+}
