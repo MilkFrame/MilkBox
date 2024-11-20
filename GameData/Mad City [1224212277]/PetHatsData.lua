f9b86d99f4205b231d334b947eca76920d3beee8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-21 02:27:40 +1100

    Create PetHatsData.lua

---
+++ GameData/Mad City [1224212277]/PetHatsData.lua
@@ -0,0 +1,15 @@
+data = {
+    ["Data"] = {
+        ["HAT2"] = {["Price"] = 15000, ["Name"] = "Bowler Hat", ["Model"] = "Bowler Hat"},
+        ["HAT4"] = {["Price"] = 20000, ["Name"] = "Clown Hat", ["Model"] = "Clown Hat"},
+        ["HAT9"] = {["Price"] = 20000, ["Name"] = "Propeller Hat", ["Model"] = "Propeller Hat"},
+        ["HAT8"] = {["Price"] = 30000, ["Name"] = "Police Hat", ["Model"] = "Police Hat"},
+        ["HAT11"] = {["Price"] = 75000, ["Name"] = "Tiara", ["Model"] = "Tiara"},
+        ["HAT6"] = {["Price"] = 100000, ["Name"] = "Crown", ["Model"] = "Crown"},
+        ["HAT3"] = {["Price"] = 17500, ["Name"] = "Chef Hat", ["Model"] = "Chef Hat"},
+        ["HAT1"] = {["Price"] = 0, ["Name"] = "Default", ["Model"] = "Default"},
+        ["HAT5"] = {["Price"] = 25000, ["Name"] = "Cowboy Hat", ["Model"] = "Cowboy Hat"},
+        ["HAT10"] = {["Price"] = 50000, ["Name"] = "Red Top Hat", ["Model"] = "Red Top Hat"},
+        ["HAT7"] = {["Price"] = 10000, ["Name"] = "Paper Hat", ["Model"] = "Paper Hat"}
+    }
+}
