6ff59576fb3b094e442d796f8b57d2f324c1051c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-22 02:41:51 +1100

    Create Auras.lua

---
+++ GameData/The Mad Murderer X [8317834514]/Auras.lua
@@ -0,0 +1,352 @@
+data = {
+    ["YellowFlame"] = {
+        ["AuraData"] = {
+            ["Image"] = "rbxassetid://12356198171",
+            ["Name"] = "Yellow Flame",
+            ["PreviewImage"] = "rbxassetid://15440317072",
+            ["Description"] = "I am not talking about Peltophorum Pterocarpum",
+            ["AuraType"] = "Flame",
+            ["AuraParams"] = {
+                ["Custom"] = "function: 0xafe6690afb602ced",
+                ["HullContainer"] = "Hull",
+                ["AuraPointContainer"] = "AuraPoint"
+            },
+            ["MetaData"] = {["CreationDate"] = "03/02/2023", ["CreatorId"] = 0}
+        }
+    },
+    ["BlueFlame"] = {
+        ["AuraData"] = {
+            ["Image"] = "rbxassetid://12356220293",
+            ["Name"] = "Blue Flame",
+            ["PreviewImage"] = "rbxassetid://15440299080",
+            ["Description"] = "Imbued with ancient energy by students of Magic Academy",
+            ["AuraType"] = "Flame",
+            ["AuraParams"] = {
+                ["Custom"] = "function: 0xd9c1d0bac7120bed",
+                ["HullContainer"] = "Hull",
+                ["AuraPointContainer"] = "AuraPoint"
+            },
+            ["MetaData"] = {["CreationDate"] = "03/02/2023", ["CreatorId"] = 0}
+        }
+    },
+    ["PinkFlame"] = {
+        ["AuraData"] = {
+            ["Image"] = "rbxassetid://12356190277",
+            ["Name"] = "Pink Flame",
+            ["PreviewImage"] = "rbxassetid://15440305090",
+            ["Description"] = "This is what happens when you burn pure fairy dust 😵",
+            ["AuraType"] = "Flame",
+            ["AuraParams"] = {
+                ["Custom"] = "function: 0x3635b4ff1a53194d",
+                ["HullContainer"] = "Hull",
+                ["AuraPointContainer"] = "AuraPoint"
+            },
+            ["MetaData"] = {["CreationDate"] = "03/02/2023", ["CreatorId"] = 0}
+        }
+    },
+    ["WhiteOutline"] = {
+        ["AuraData"] = {
+            ["Image"] = "rbxassetid://15117520250",
+            ["Name"] = "White Outline",
+            ["PreviewImage"] = "rbxassetid://15440312575",
+            ["Description"] = "Woosh, your weapon is now an important quest item!",
+            ["AuraType"] = "Line",
+            ["AuraParams"] = {
+                ["Custom"] = "function: 0x7e6496f3e6c8248d",
+                ["HullContainer"] = "Hull",
+                ["AuraPointContainer"] = "AuraPoint"
+            },
+            ["MetaData"] = {["CreationDate"] = "03/02/2023", ["CreatorId"] = 0}
+        }
+    },
+    ["Milestones"] = {
+        ["Stinky"] = {
+            ["AuraData"] = {
+                ["Image"] = "rbxassetid://12356620770",
+                ["Name"] = "Stinky",
+                ["PreviewImage"] = "rbxassetid://15440308642",
+                ["Description"] = "It doesn't smell nice...",
+                ["AuraType"] = "Flow",
+                ["AuraParams"] = {
+                    ["Custom"] = "function: 0xd1d4e03c139847cd",
+                    ["HullContainer"] = "Hull",
+                    ["AuraPointContainer"] = "AuraPoint"
+                },
+                ["MetaData"] = {["CreationDate"] = "19/03/2024", ["CreatorId"] = 0}
+            }
+        }
+    },
+    ["Starter"] = {
+        ["DivineAura"] = {
+            ["AuraData"] = {
+                ["Image"] = "rbxassetid://16594694405",
+                ["Name"] = "Divine Aura",
+                ["PreviewImage"] = "",
+                ["Description"] = "",
+                ["AuraType"] = "Particles",
+                ["AuraParams"] = {
+                    ["Custom"] = "function: 0x18ee722393dca12d",
+                    ["HullContainer"] = "Hull",
+                    ["AuraPointContainer"] = "AuraPoint"
+                },
+                ["MetaData"] = {["CreationDate"] = "02/03/2024", ["CreatorId"] = 0}
+            }
+        }
+    },
+    ["BlackOut"] = {
+        ["AuraData"] = {
+            ["Image"] = "rbxassetid://15117520710",
+            ["Name"] = "Black Fill",
+            ["PreviewImage"] = "rbxassetid://15440297958",
+            ["Description"] = "I guess your weapon's power grid went down",
+            ["AuraType"] = "Line",
+            ["AuraParams"] = {
+                ["Custom"] = "function: 0x3dcfd6788c1ac24d",
+                ["HullContainer"] = "Hull",
+                ["AuraPointContainer"] = "AuraPoint"
+            },
+            ["MetaData"] = {["CreationDate"] = "03/02/2023", ["CreatorId"] = 0}
+        }
+    },
+    ["!DAILY"] = {
+        ["Hurricane"] = {
+            ["AuraData"] = {
+                ["Image"] = "rbxassetid://12372460011",
+                ["Name"] = "Hurricane",
+                ["PreviewImage"] = "rbxassetid://15440315348",
+                ["Description"] = "",
+                ["AuraType"] = "Flow",
+                ["AuraParams"] = {
+                    ["Custom"] = "function: 0x341ae6c9890f192d",
+                    ["HullContainer"] = "Hull",
+                    ["AuraPointContainer"] = "AuraPoint"
+                },
+                ["MetaData"] = {["CreationDate"] = "20/02/2024", ["CreatorId"] = 0}
+            }
+        },
+        ["Lightning"] = {
+            ["AuraData"] = {
+                ["Image"] = "rbxassetid://12356508001",
+                ["Name"] = "Lightning",
+                ["PreviewImage"] = "rbxassetid://15440301753",
+                ["Description"] = "",
+                ["AuraType"] = "Particles",
+                ["AuraParams"] = {
+                    ["Custom"] = "function: 0xa67e17c4e262a3ed",
+                    ["HullContainer"] = "Hull",
+                    ["AuraPointContainer"] = "AuraPoint"
+                },
+                ["MetaData"] = {["CreationDate"] = "20/02/2024", ["CreatorId"] = 0}
+            }
+        }
+    },
+    ["CyanFlame"] = {
+        ["AuraData"] = {
+            ["Image"] = "rbxassetid://12356188734",
+            ["Name"] = "Cyan Flame",
+            ["PreviewImage"] = "rbxassetid://15440300594",
+            ["Description"] = "Despite looking cold, cyan flames are actually extremely hot",
+            ["AuraType"] = "Flame",
+            ["AuraParams"] = {
+                ["Custom"] = "function: 0x3d1e4a304bbe33ad",
+                ["HullContainer"] = "Hull",
+                ["AuraPointContainer"] = "AuraPoint"
+            },
+            ["MetaData"] = {["CreationDate"] = "03/02/2023", ["CreatorId"] = 0}
+        }
+    },
+    ["!PASS"] = {
+        ["Spring2024"] = {
+            ["ForceOfNature"] = {
+                ["AuraData"] = {
+                    ["Image"] = "rbxassetid://16736929135",
+                    ["Name"] = "Force Of Nature",
+                    ["PreviewImage"] = "",
+                    ["Description"] = "",
+                    ["AuraType"] = "Flow",
+                    ["AuraParams"] = {
+                        ["Custom"] = "function: 0x3520455cbc72c9ed",
+                        ["HullContainer"] = "Hull",
+                        ["AuraPointContainer"] = "AuraPoint"
+                    },
+                    ["MetaData"] = {["CreationDate"] = "19/03/2024", ["CreatorId"] = 0}
+                }
+            }
+        },
+        ["Autumn2024"] = {
+            ["AutumnLeaves"] = {
+                ["AuraData"] = {
+                    ["Image"] = "rbxassetid://94103810929494",
+                    ["Name"] = "Autumn Leaves",
+                    ["PreviewImage"] = "",
+                    ["Description"] = "",
+                    ["AuraType"] = "Flow",
+                    ["AuraParams"] = {
+                        ["Custom"] = "function: 0xdcbe304801ad132d",
+                        ["HullContainer"] = "Hull",
+                        ["AuraPointContainer"] = "AuraPoint"
+                    },
+                    ["MetaData"] = {["CreationDate"] = "03/09/2024", ["CreatorId"] = 0}
+                }
+            }
+        },
+        ["Summer2024"] = {
+            ["Wet"] = {
+                ["AuraData"] = {
+                    ["Image"] = "rbxassetid://17560365889",
+                    ["Name"] = "Wet",
+                    ["PreviewImage"] = "",
+                    ["Description"] = "",
+                    ["AuraType"] = "Flow",
+                    ["AuraParams"] = {
+                        ["Custom"] = "function: 0x499a9657edd47b8d",
+                        ["HullContainer"] = "Hull",
+                        ["AuraPointContainer"] = "AuraPoint"
+                    },
+                    ["MetaData"] = {["CreationDate"] = "11/06/2024", ["CreatorId"] = 0}
+                }
+            }
+        },
+        ["Winter2024"] = {
+            ["WinterBreeze"] = {
+                ["AuraData"] = {
+                    ["Image"] = "rbxassetid://12355288585",
+                    ["Name"] = "Winter Breeze",
+                    ["PreviewImage"] = "rbxassetid://15440316249",
+                    ["Description"] = "",
+                    ["AuraType"] = "Flow",
+                    ["AuraParams"] = {
+                        ["Custom"] = "function: 0x225d21d115125f4d",
+                        ["HullContainer"] = "Hull",
+                        ["AuraPointContainer"] = "AuraPoint"
+                    },
+                    ["MetaData"] = {["CreationDate"] = "26/01/2024", ["CreatorId"] = 0}
+                }
+            }
+        }
+    },
+    ["BlackFlame"] = {
+        ["AuraData"] = {
+            ["Image"] = "rbxassetid://12356239192",
+            ["Name"] = "Black Flame",
+            ["PreviewImage"] = "rbxassetid://15440295504",
+            ["Description"] = "Go get 'em, edgelord!",
+            ["AuraType"] = "Flame",
+            ["AuraParams"] = {
+                ["Custom"] = "function: 0xfc57ed35fa1056ed",
+                ["HullContainer"] = "Hull",
+                ["AuraPointContainer"] = "AuraPoint"
+            },
+            ["MetaData"] = {["CreationDate"] = "03/02/2023", ["CreatorId"] = 0}
+        }
+    },
+    ["WhiteOut"] = {
+        ["AuraData"] = {
+            ["Image"] = "rbxassetid://15117529041",
+            ["Name"] = "White Fill",
+            ["PreviewImage"] = "rbxassetid://15440313736",
+            ["Description"] = "Distinctive lack of hue",
+            ["AuraType"] = "Line",
+            ["AuraParams"] = {
+                ["Custom"] = "function: 0x994a76bf533934ed",
+                ["HullContainer"] = "Hull",
+                ["AuraPointContainer"] = "AuraPoint"
+            },
+            ["MetaData"] = {["CreationDate"] = "03/02/2023", ["CreatorId"] = 0}
+        }
+    },
+    ["BlackOutline"] = {
+        ["AuraData"] = {
+            ["Image"] = "rbxassetid://15117520534",
+            ["Name"] = "Black Outline",
+            ["PreviewImage"] = "rbxassetid://15440296775",
+            ["Description"] = "Your weapon has become cartoony",
+            ["AuraType"] = "Line",
+            ["AuraParams"] = {
+                ["Custom"] = "function: 0x002ba7a5fbeb51ad",
+                ["HullContainer"] = "Hull",
+                ["AuraPointContainer"] = "AuraPoint"
+            },
+            ["MetaData"] = {["CreationDate"] = "03/02/2023", ["CreatorId"] = 0}
+        }
+    },
+    ["WhiteFlame"] = {
+        ["AuraData"] = {
+            ["Image"] = "rbxassetid://12356231798",
+            ["Name"] = "White Flame",
+            ["PreviewImage"] = "rbxassetid://15440309802",
+            ["Description"] = "The cleansing power of white-hot flames at your fingertips",
+            ["AuraType"] = "Flame",
+            ["AuraParams"] = {
+                ["Custom"] = "function: 0xd9e3b1f51757244d",
+                ["HullContainer"] = "Hull",
+                ["AuraPointContainer"] = "AuraPoint"
+            },
+            ["MetaData"] = {["CreationDate"] = "03/02/2023", ["CreatorId"] = 0}
+        }
+    },
+    ["GreenFlame"] = {
+        ["AuraData"] = {
+            ["Image"] = "rbxassetid://12355346414",
+            ["Name"] = "Green Flame",
+            ["PreviewImage"] = "rbxassetid://15440303716",
+            ["Description"] = "Who cares about chemical burns when your weapon looks cool?",
+            ["AuraType"] = "Flame",
+            ["AuraParams"] = {
+                ["Custom"] = "function: 0x467d6b404eb02ead",
+                ["HullContainer"] = "Hull",
+                ["AuraPointContainer"] = "AuraPoint"
+            },
+            ["MetaData"] = {["CreationDate"] = "03/02/2023", ["CreatorId"] = 0}
+        }
+    },
+    ["Social"] = {
+        ["CommunitySparkle"] = {
+            ["AuraData"] = {
+                ["Image"] = "rbxassetid://12987027069",
+                ["Name"] = "Community Sparkle",
+                ["PreviewImage"] = "rbxassetid://15440307228",
+                ["Description"] = "",
+                ["AuraType"] = "Particles",
+                ["AuraParams"] = {
+                    ["Custom"] = "function: 0x8c4884d7813eea6d",
+                    ["HullContainer"] = "Hull",
+                    ["AuraPointContainer"] = "AuraPoint"
+                },
+                ["MetaData"] = {["CreationDate"] = "25/02/2024", ["CreatorId"] = 0}
+            }
+        }
+    },
+    ["!FRIENDS"] = {
+        ["Frien"] = {
+            ["AuraData"] = {
+                ["Image"] = "rbxassetid://17264885119",
+                ["Name"] = "Frien",
+                ["PreviewImage"] = "",
+                ["Description"] = "",
+                ["AuraType"] = "Flow",
+                ["AuraParams"] = {
+                    ["Custom"] = "function: 0x687a0f920b83c60d",
+                    ["HullContainer"] = "Hull",
+                    ["AuraPointContainer"] = "AuraPoint"
+                },
+                ["MetaData"] = {["CreationDate"] = "19/03/2024", ["CreatorId"] = 0}
+            }
+        }
+    },
+    ["RedFlame"] = {
+        ["AuraData"] = {
+            ["Image"] = "rbxassetid://12356207002",
+            ["Name"] = "Red Flame",
+            ["PreviewImage"] = "rbxassetid://15440306035",
+            ["Description"] = "The Flame™, all rights reserved!",
+            ["AuraType"] = "Flame",
+            ["AuraParams"] = {
+                ["Custom"] = "function: 0xc2ae70535136f28d",
+                ["HullContainer"] = "Hull",
+                ["AuraPointContainer"] = "AuraPoint"
+            },
+            ["MetaData"] = {["CreationDate"] = "03/02/2023", ["CreatorId"] = 0}
+        }
+    }
+}
