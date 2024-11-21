6dfd924abe8e1202c8ada2f1be29d2d75be360bb
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-22 02:35:41 +1100

    Create PremiumConfig.lua

---
+++ GameData/The Mad Murderer X [8317834514]/PremiumConfig.lua
@@ -0,0 +1,431 @@
+data = {
+    ["VirtualPasses"] = {
+        ["MurdererChance"] = {
+            ["InventoryNeeded"] = 0,
+            ["Description"] = "Increases your chance to become the murderer each round permanently!",
+            ["OnReward"] = "function: 0xc387fb4196666627",
+            ["DisplayName"] = "Evil",
+            ["Info"] = {
+                ["BasePriceRobux"] = 200,
+                ["Key"] = "MurdererChance",
+                ["DiscountPriceRobux"] = 20,
+                ["Icon"] = "rbxassetid://16591816037",
+                ["DisplayName"] = "Evil"
+            },
+            ["DiscountProductId"] = 1767485591,
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            },
+            ["WindowName"] = "StarterOffer",
+            ["DiscountTimeHours"] = 48,
+            ["Icon"] = "rbxassetid://16591816037",
+            ["BaseProductId"] = 1767485385
+        },
+        ["StarterPack1"] = {
+            ["InventoryNeeded"] = 3,
+            ["Description"] = "Receive a heavenly-themed gun, knife, pet and weapon aura! (One purchase per player)",
+            ["OnReward"] = "function: 0xed5419f85c08fec7",
+            ["DisplayName"] = "Good",
+            ["Info"] = {
+                ["BasePriceRobux"] = 400,
+                ["Key"] = "StarterPack1",
+                ["DiscountPriceRobux"] = 80,
+                ["Icon"] = "rbxassetid://16591295193",
+                ["DisplayName"] = "Good"
+            },
+            ["DiscountProductId"] = 1767485113,
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            },
+            ["WindowName"] = "StarterOffer",
+            ["DiscountTimeHours"] = 48,
+            ["Icon"] = "rbxassetid://16591295193",
+            ["BaseProductId"] = 1767484736
+        }
+    },
+    ["Products"] = {
+        ["EventCurrency5"] = {
+            ["Id"] = 1750616520,
+            ["Config"] = {["Count"] = 1350000, ["Type"] = "Event"},
+            ["Info"] = {
+                ["Id"] = 1750616520,
+                ["PriceRobux"] = 4500,
+                ["Icon"] = "rbxassetid://78207185775547",
+                ["Config"] = {["Count"] = 1350000, ["Type"] = "Event"}
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["CollectorsGun"] = {
+            ["Id"] = 1230255807,
+            ["Config"] = {
+                ["Type"] = "Collectible",
+                ["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "InfestedGun"},
+                ["EarlyCollectible"] = {["ElementType"] = "Gun", ["ElementName"] = "InfestedGunE"}
+            },
+            ["Info"] = {
+                ["Id"] = 1230255807,
+                ["PriceRobux"] = 800,
+                ["Icon"] = "rbxassetid://8322792693",
+                ["Config"] = {
+                    ["Type"] = "Collectible",
+                    ["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "InfestedGun"},
+                    ["EarlyCollectible"] = {["ElementType"] = "Gun", ["ElementName"] = "InfestedGunE"}
+                }
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["Coins1"] = {
+            ["Id"] = 1709080767,
+            ["Config"] = {["CoinCount"] = 2000, ["Type"] = "Coin"},
+            ["Info"] = {
+                ["Id"] = 1709080767,
+                ["PriceRobux"] = 80,
+                ["Icon"] = "rbxassetid://15681771233",
+                ["Config"] = {["CoinCount"] = 2000, ["Type"] = "Coin"}
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["CollectorsPet"] = {
+            ["Id"] = 1230255822,
+            ["Config"] = {
+                ["Type"] = "Collectible",
+                ["Collectible"] = {["ElementType"] = "Pet", ["ElementName"] = "Hand"},
+                ["EarlyCollectible"] = {["ElementType"] = "Pet", ["ElementName"] = "HandE"}
+            },
+            ["Info"] = {
+                ["Id"] = 1230255822,
+                ["PriceRobux"] = 200,
+                ["Icon"] = "rbxassetid://8322794094",
+                ["Config"] = {
+                    ["Type"] = "Collectible",
+                    ["Collectible"] = {["ElementType"] = "Pet", ["ElementName"] = "Hand"},
+                    ["EarlyCollectible"] = {["ElementType"] = "Pet", ["ElementName"] = "HandE"}
+                }
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["Coins2"] = {
+            ["Id"] = 1709080923,
+            ["Config"] = {["CoinCount"] = 10000, ["Type"] = "Coin"},
+            ["Info"] = {
+                ["Id"] = 1709080923,
+                ["PriceRobux"] = 400,
+                ["Icon"] = "rbxassetid://15681773119",
+                ["Config"] = {["CoinCount"] = 10000, ["Type"] = "Coin"}
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["Coins3"] = {
+            ["Id"] = 1709081026,
+            ["Config"] = {["CoinCount"] = 20000, ["Type"] = "Coin"},
+            ["Info"] = {
+                ["Id"] = 1709081026,
+                ["PriceRobux"] = 800,
+                ["Icon"] = "rbxassetid://15681778715",
+                ["Config"] = {["CoinCount"] = 20000, ["Type"] = "Coin"}
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["CollectorsKnife"] = {
+            ["Id"] = 1230255770,
+            ["Config"] = {
+                ["Type"] = "Collectible",
+                ["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "InfestedKnife"},
+                ["EarlyCollectible"] = {["ElementType"] = "Knife", ["ElementName"] = "InfestedKnifeE"}
+            },
+            ["Info"] = {
+                ["Id"] = 1230255770,
+                ["PriceRobux"] = 800,
+                ["Icon"] = "rbxassetid://8322793168",
+                ["Config"] = {
+                    ["Type"] = "Collectible",
+                    ["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "InfestedKnife"},
+                    ["EarlyCollectible"] = {["ElementType"] = "Knife", ["ElementName"] = "InfestedKnifeE"}
+                }
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["EventCurrency3"] = {
+            ["Id"] = 1750616104,
+            ["Config"] = {["Count"] = 200000, ["Type"] = "Event"},
+            ["Info"] = {
+                ["Id"] = 1750616104,
+                ["PriceRobux"] = 800,
+                ["Icon"] = "rbxassetid://97033527031080",
+                ["Config"] = {["Count"] = 200000, ["Type"] = "Event"}
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["EventCurrency2"] = {
+            ["Id"] = 1750615927,
+            ["Config"] = {["Count"] = 100000, ["Type"] = "Event"},
+            ["Info"] = {
+                ["Id"] = 1750615927,
+                ["PriceRobux"] = 400,
+                ["Icon"] = "rbxassetid://127004671788840",
+                ["Config"] = {["Count"] = 100000, ["Type"] = "Event"}
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["EventCurrency4"] = {
+            ["Id"] = 1750616395,
+            ["Config"] = {["Count"] = 490000, ["Type"] = "Event"},
+            ["Info"] = {
+                ["Id"] = 1750616395,
+                ["PriceRobux"] = 1700,
+                ["Icon"] = "rbxassetid://117706232648282",
+                ["Config"] = {["Count"] = 490000, ["Type"] = "Event"}
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["EventCurrency6"] = {
+            ["Id"] = 1750617076,
+            ["Config"] = {["Count"] = 3500000, ["Type"] = "Event"},
+            ["Info"] = {
+                ["Id"] = 1750617076,
+                ["PriceRobux"] = 10000,
+                ["Icon"] = "rbxassetid://128827722063730",
+                ["Config"] = {["Count"] = 3500000, ["Type"] = "Event"}
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["Coins6"] = {
+            ["Id"] = 1709081693,
+            ["Config"] = {["CoinCount"] = 350000, ["Type"] = "Coin"},
+            ["Info"] = {
+                ["Id"] = 1709081693,
+                ["PriceRobux"] = 10000,
+                ["Icon"] = "rbxassetid://15681784910",
+                ["Config"] = {["CoinCount"] = 350000, ["Type"] = "Coin"}
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["EventCurrency1"] = {
+            ["Id"] = 1750615783,
+            ["Config"] = {["Count"] = 20000, ["Type"] = "Event"},
+            ["Info"] = {
+                ["Id"] = 1750615783,
+                ["PriceRobux"] = 80,
+                ["Icon"] = "rbxassetid://113835619766817",
+                ["Config"] = {["Count"] = 20000, ["Type"] = "Event"}
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["Coins5"] = {
+            ["Id"] = 1709081572,
+            ["Config"] = {["CoinCount"] = 135000, ["Type"] = "Coin"},
+            ["Info"] = {
+                ["Id"] = 1709081572,
+                ["PriceRobux"] = 4500,
+                ["Icon"] = "rbxassetid://15681782947",
+                ["Config"] = {["CoinCount"] = 135000, ["Type"] = "Coin"}
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["Coins4"] = {
+            ["Id"] = 1709081243,
+            ["Config"] = {["CoinCount"] = 49000, ["Type"] = "Coin"},
+            ["Info"] = {
+                ["Id"] = 1709081243,
+                ["PriceRobux"] = 1700,
+                ["Icon"] = "rbxassetid://15681777635",
+                ["Config"] = {["CoinCount"] = 49000, ["Type"] = "Coin"}
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        }
+    },
+    ["GetGamepassKey"] = "function: 0x7d4770f42471c637",
+    ["GetGamepassId"] = "function: 0x308a2bc7cda77cd7",
+    ["GetProductInfo"] = "function: 0xd7cddffa44f68167",
+    ["GetProductKey"] = "function: 0x863546211da9fd27",
+    ["GetGamepassConfig"] = "function: 0x517ead5c735adae7",
+    ["GetGamepassInfo"] = "function: 0x546d1384a9995547",
+    ["CollectorsType"] = "(Star Set)",
+    ["GetVirtualPassKey"] = "function: 0xeedb255e5be18c57",
+    ["GetVirtualPassInfo"] = "function: 0x2e1b382d7d04eb97",
+    ["EarlyCollectibleDurationHours"] = 168,
+    ["Gamepasses"] = {
+        ["TripleOpen"] = {
+            ["Id"] = 26334383,
+            ["Config"] = {},
+            ["Info"] = {["Id"] = 26334383, ["Icon"] = "rbxassetid://8318008710", ["Config"] = {}},
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["Pets2"] = {
+            ["Id"] = 26334290,
+            ["Config"] = {
+                ["Name"] = "+2 Pets Equipped",
+                ["Description"] = "Owning this allows you to equip 2 more pets!"
+            },
+            ["Info"] = {
+                ["Id"] = 26334290,
+                ["PriceRobux"] = 80,
+                ["Icon"] = "rbxassetid://8317982880",
+                ["Config"] = {
+                    ["Name"] = "+2 Pets Equipped",
+                    ["Description"] = "Owning this allows you to equip 2 more pets!"
+                }
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["MadMythicals"] = {
+            ["Id"] = 26334320,
+            ["Config"] = {
+                ["Name"] = "Mad Mythicals",
+                ["Description"] = "Get a significantly higher chance to receive legendary and mythical items from boxes!"
+            },
+            ["Info"] = {
+                ["Id"] = 26334320,
+                ["PriceRobux"] = 800,
+                ["Icon"] = "rbxassetid://11998118525",
+                ["Config"] = {
+                    ["Name"] = "Mad Mythicals",
+                    ["Description"] = "Get a significantly higher chance to receive legendary and mythical items from boxes!"
+                }
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["Lucky"] = {
+            ["Id"] = 26334367,
+            ["Config"] = {
+                ["Name"] = "Lucky",
+                ["Description"] = "Get Rare, Epic, Legendary and Mythical items from boxes more often!"
+            },
+            ["Info"] = {
+                ["Id"] = 26334367,
+                ["PriceRobux"] = 80,
+                ["Icon"] = "rbxassetid://8318003704",
+                ["Config"] = {
+                    ["Name"] = "Lucky",
+                    ["Description"] = "Get Rare, Epic, Legendary and Mythical items from boxes more often!"
+                }
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        }
+    },
+    ["GetProductConfig"] = "function: 0xa99568c7eec96907",
+    ["EarlyCollectibleStart"] = 1729600082,
+    ["GetProductId"] = "function: 0x1a2eec213e4ceef7"
+}
