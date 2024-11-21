051d2ff97be4df5e57f12f165c02764ce490b9f8
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-22 02:38:06 +1100

    Create Rewards.lua

---
+++ GameData/The Mad Murderer X [8317834514]/Rewards.lua
@@ -0,0 +1,306 @@
+data = {
+    ["SocialRewards"] = {
+        ["Group"] = {["ElementType"] = "Knife", ["ElementName"] = "MadKnife"},
+        ["Twitter"] = {["ElementType"] = "Gun", ["ElementName"] = "MadGun"},
+        ["Discord"] = {["ElementType"] = "Aura", ["ElementName"] = "CommunitySparkle"},
+        ["Final"] = {["ElementType"] = "Ability", ["ElementName"] = "Haste"}
+    },
+    ["EventRewards"] = {
+        ["EventTitle"] = "Autumn",
+        ["CurrentEventInfo"] = {
+            ["CurrencyRewardBoost"] = 6,
+            ["IsActive"] = true,
+            ["PassTitle"] = "Autumn Pass",
+            ["Title"] = "Autumn",
+            ["CurrencyIcon"] = "rbxassetid://135549523256709",
+            ["CurrencyOrb"] = "rbxassetid://105691908476000",
+            ["CurrencyCoinRatio"] = 10,
+            ["Name"] = "Autumn 2024",
+            ["Tag"] = "Autumn2024"
+        },
+        ["EventTag"] = "Autumn2024",
+        ["EventPassTitle"] = "Autumn Pass",
+        ["EventPassPreFinalCount"] = 13,
+        ["EventPass"] = {
+            {
+                ["Price"] = 50,
+                ["Type"] = "Collectible",
+                ["Data"] = {["ElementType"] = "Knife", ["ElementName"] = "PumpkinKnife"}
+            },
+            {
+                ["Price"] = 200,
+                ["Type"] = "Collectible",
+                ["Data"] = {["ElementType"] = "Gun", ["ElementName"] = "PumpkinGun"}
+            },
+            {
+                ["Price"] = 500,
+                ["Type"] = "Collectible",
+                ["Data"] = {["ElementType"] = "Knife", ["ElementName"] = "CornucopiaKnife"}
+            },
+            {
+                ["Price"] = 1000,
+                ["Type"] = "Collectible",
+                ["Data"] = {["ElementType"] = "Gun", ["ElementName"] = "CornucopiaGun"}
+            },
+            {
+                ["Price"] = 2000,
+                ["Type"] = "Collectible",
+                ["Data"] = {["ElementType"] = "Knife", ["ElementName"] = "RainKnife"}
+            },
+            {
+                ["Price"] = 4000,
+                ["Type"] = "Collectible",
+                ["Data"] = {["ElementType"] = "Gun", ["ElementName"] = "RainGun"}
+            },
+            {
+                ["Price"] = 6000,
+                ["Type"] = "Collectible",
+                ["Data"] = {["ElementType"] = "Knife", ["ElementName"] = "CandleKnife"}
+            },
+            {
+                ["Price"] = 8000,
+                ["Type"] = "Collectible",
+                ["Data"] = {["ElementType"] = "Gun", ["ElementName"] = "CandleGun"}
+            },
+            {
+                ["Price"] = 10000,
+                ["Type"] = "Collectible",
+                ["Data"] = {["ElementType"] = "Knife", ["ElementName"] = "PlaidKnife"}
+            },
+            {
+                ["Price"] = 15000,
+                ["Type"] = "Collectible",
+                ["Data"] = {["ElementType"] = "Gun", ["ElementName"] = "PlaidGun"}
+            },
+            {
+                ["Price"] = 18000,
+                ["Type"] = "Collectible",
+                ["Data"] = {["ElementType"] = "Knife", ["ElementName"] = "ToadstoolKnife"}
+            },
+            {
+                ["Price"] = 20000,
+                ["Type"] = "Collectible",
+                ["Data"] = {["ElementType"] = "Gun", ["ElementName"] = "ToadstoolGun"}
+            },
+            {
+                ["Price"] = 30000,
+                ["Type"] = "Progress",
+                ["Data"] = {["ElementType"] = "Aura", ["ElementName"] = "AutumnLeaves"}
+            },
+            {
+                ["Price"] = 100000,
+                ["Type"] = "Collectible",
+                ["Data"] = {["ElementType"] = "Knife", ["ElementName"] = "MapleKnife"},
+                ["IsFinal"] = true
+            },
+            {
+                ["Price"] = 100000,
+                ["Type"] = "Collectible",
+                ["Data"] = {["ElementType"] = "Gun", ["ElementName"] = "MapleGun"},
+                ["IsFinal"] = true
+            },
+            {
+                ["Price"] = 50000,
+                ["Type"] = "Collectible",
+                ["Data"] = {["ElementType"] = "Pet", ["ElementName"] = "Tabby"},
+                ["IsFinal"] = true
+            }
+        },
+        ["CurrencyOrb"] = "rbxassetid://105691908476000",
+        ["EventInfo"] = {
+            {
+                ["CurrencyRewardBoost"] = 8,
+                ["IsActive"] = true,
+                ["PassTitle"] = "Winter Pass",
+                ["Title"] = "Winter",
+                ["CurrencyIcon"] = "rbxassetid://15431412065",
+                ["CurrencyOrb"] = "rbxassetid://16256510604",
+                ["CurrencyCoinRatio"] = 10,
+                ["Name"] = "Winter 2024",
+                ["Tag"] = "Winter2024"
+            },
+            {
+                ["CurrencyRewardBoost"] = 6,
+                ["IsActive"] = true,
+                ["PassTitle"] = "Spring Pass",
+                ["Title"] = "Spring",
+                ["CurrencyIcon"] = "rbxassetid://16799057606",
+                ["CurrencyOrb"] = "rbxassetid://16798040553",
+                ["CurrencyCoinRatio"] = 10,
+                ["Name"] = "Spring 2024",
+                ["Tag"] = "Spring2024"
+            },
+            {
+                ["CurrencyRewardBoost"] = 6,
+                ["IsActive"] = true,
+                ["PassTitle"] = "Summer Pass",
+                ["Title"] = "Summer",
+                ["CurrencyIcon"] = "rbxassetid://17518440758",
+                ["CurrencyOrb"] = "rbxassetid://17820835893",
+                ["CurrencyCoinRatio"] = 10,
+                ["Name"] = "Summer 2024",
+                ["Tag"] = "Summer2024"
+            },
+            {
+                ["CurrencyRewardBoost"] = 6,
+                ["IsActive"] = true,
+                ["PassTitle"] = "Autumn Pass",
+                ["Title"] = "Autumn",
+                ["CurrencyIcon"] = "rbxassetid://135549523256709",
+                ["CurrencyOrb"] = "rbxassetid://105691908476000",
+                ["CurrencyCoinRatio"] = 10,
+                ["Name"] = "Autumn 2024",
+                ["Tag"] = "Autumn2024"
+            }
+        },
+        ["CurrencyRewardBoost"] = 6,
+        ["CurrencyIcon"] = "rbxassetid://135549523256709",
+        ["CurrencyCoinRatio"] = 10,
+        ["CurrentEventIndex"] = 4,
+        ["EventName"] = "Autumn 2024",
+        ["IsEventActive"] = true
+    },
+    ["MilestoneRewards"] = {
+        ["FlagCaptured"] = {
+            ["ServerType"] = "CTF",
+            ["Levels"] = {
+                {
+                    ["RewardType"] = "Progress",
+                    ["Goal"] = 10,
+                    ["RewardData"] = {["ElementType"] = "Emote", ["ElementName"] = "TakeTheL"}
+                }
+            },
+            ["Title"] = "Flags Captured"
+        },
+        ["MurdererVictoryOldSchool"] = {
+            ["ServerType"] = "OldSchool",
+            ["Levels"] = {
+                {
+                    ["RewardType"] = "Progress",
+                    ["Goal"] = 10,
+                    ["RewardData"] = {["ElementType"] = "Emote", ["ElementName"] = "Cry"}
+                }
+            },
+            ["Title"] = "Classic Murderer Victories"
+        },
+        ["MurdererVictory"] = {
+            ["ServerType"] = "Main",
+            ["Levels"] = {
+                {
+                    ["RewardType"] = "Progress",
+                    ["Goal"] = 10,
+                    ["RewardData"] = {["ElementType"] = "Aura", ["ElementName"] = "Stinky"}
+                }
+            },
+            ["Title"] = "Murderer Victories"
+        }
+    },
+    ["FriendRewards"] = {
+        ["PlayWith4Friends"] = {
+            ["RewardType"] = "Progress",
+            ["Index"] = 1,
+            ["Goal"] = 1,
+            ["RewardData"] = {["ElementType"] = "Emote", ["ElementName"] = "Friends"},
+            ["Title"] = [[<stroke color="rgb(0, 0, 0)" thickness="2">Play with<font color="rgb(255,255,255)"> 4 friends </font>in the same server</stroke>]]
+        },
+        ["PlayWith8Friends"] = {
+            ["RewardType"] = "Progress",
+            ["Index"] = 3,
+            ["Goal"] = 1,
+            ["RewardData"] = {["ElementType"] = "Gear", ["ElementName"] = "PartyPopper"},
+            ["Title"] = [[<stroke color="rgb(0, 0, 0)" thickness="2">Play with<font color="rgb(255,255,255)"> 8 friends </font>in the same server</stroke>]]
+        },
+        ["Invite100Friend"] = {
+            ["RewardType"] = "Progress",
+            ["Index"] = 6,
+            ["Goal"] = 100,
+            ["RewardData"] = {["ElementType"] = "Aura", ["ElementName"] = "Frien"},
+            ["Title"] = [[<stroke color="rgb(0, 0, 0)" thickness="2">Invite<font color="rgb(255,255,255)"> 100 friends </font>to TMMX</stroke>]]
+        },
+        ["Invite1Friend"] = {
+            ["RewardType"] = "Collectible",
+            ["Index"] = 4,
+            ["Goal"] = 1,
+            ["RewardData"] = {["ElementType"] = "Gun", ["ElementName"] = "ChainGun"},
+            ["Title"] = [[<stroke color="rgb(0, 0, 0)" thickness="2">Invite<font color="rgb(255,255,255)"> 1 friend </font>to TMMX</stroke>]]
+        },
+        ["Invite1Newcomer"] = {
+            ["RewardType"] = "Collectible",
+            ["Index"] = 7,
+            ["Goal"] = 1,
+            ["RewardData"] = {["ElementType"] = "Pet", ["ElementName"] = "Wolf"},
+            ["Title"] = [[<stroke color="rgb(0, 0, 0)" thickness="2">Invite<font color="rgb(255,255,255)"> 1 friend who has never played TMMX</font></stroke>]]
+        },
+        ["PlayWith6Friends"] = {
+            ["RewardType"] = "Progress",
+            ["Index"] = 2,
+            ["Goal"] = 1,
+            ["RewardData"] = {["ElementType"] = "Ability", ["ElementName"] = "Stealth"},
+            ["Title"] = [[<stroke color="rgb(0, 0, 0)" thickness="2">Play with<font color="rgb(255,255,255)"> 6 friends </font>in the same server</stroke>]]
+        },
+        ["Invite10Friend"] = {
+            ["RewardType"] = "Collectible",
+            ["Index"] = 5,
+            ["Goal"] = 10,
+            ["RewardData"] = {["ElementType"] = "Knife", ["ElementName"] = "ChainKnife"},
+            ["Title"] = [[<stroke color="rgb(0, 0, 0)" thickness="2">Invite<font color="rgb(255,255,255)"> 10 friends </font>to TMMX</stroke>]]
+        },
+        ["Invite10Newcomer"] = {
+            ["RewardType"] = "Collectible",
+            ["Index"] = 8,
+            ["Goal"] = 10,
+            ["RewardData"] = {["ElementType"] = "Pet", ["ElementName"] = "Husky"},
+            ["Title"] = [[<stroke color="rgb(0, 0, 0)" thickness="2">Invite<font color="rgb(255,255,255)"> 10 friends who have never played TMMX</font></stroke>]]
+        },
+        ["Invite30Newcomer"] = {
+            ["RewardType"] = "Collectible",
+            ["Index"] = 9,
+            ["Goal"] = 30,
+            ["RewardData"] = {["ElementType"] = "Pet", ["ElementName"] = "Shiba"},
+            ["Title"] = [[<stroke color="rgb(0, 0, 0)" thickness="2">Invite<font color="rgb(255,255,255)"> 30 friends who have never played TMMX</font></stroke>]]
+        }
+    },
+    ["DailyRewards"] = {
+        ["EarliestLogin"] = 20,
+        ["PremiumExtraCoins"] = 50,
+        ["LatestLogin"] = 24,
+        ["CollectRequirement"] = 500,
+        ["DailyBoxName"] = "FlameBox",
+        ["Timeline"] = {
+            {["Data"] = {["ElementType"] = "Knife", ["ElementName"] = "PawKnife"}, ["Type"] = "Collectible"},
+            {["Data"] = {["ElementType"] = "Gear", ["ElementName"] = "Cookie"}, ["Type"] = "Progress"},
+            {["Data"] = {["ElementType"] = "Gun", ["ElementName"] = "PawGun"}, ["Type"] = "Collectible"},
+            {["Data"] = {["ElementType"] = "Gear", ["ElementName"] = "Newspaper"}, ["Type"] = "Progress"},
+            {["Data"] = {["BoxName"] = "FlameBox"}, ["Type"] = "Box"},
+            {["Data"] = {["ElementType"] = "Gear", ["ElementName"] = "Sandwich"}, ["Type"] = "Progress"},
+            {["Data"] = {["ElementType"] = "Knife", ["ElementName"] = "SnakeLadder"}, ["Type"] = "Collectible"},
+            {["Data"] = {["BoxName"] = "FlameBox"}, ["Type"] = "Box"},
+            {["Data"] = {["ElementType"] = "Gun", ["ElementName"] = "Soaker"}, ["Type"] = "Collectible"},
+            {["Data"] = {["ElementType"] = "Gear", ["ElementName"] = "Dice"}, ["Type"] = "Progress"},
+            {["Data"] = {["BoxName"] = "FlameBox"}, ["Type"] = "Box"},
+            {["Data"] = {["ElementType"] = "Knife", ["ElementName"] = "RoboKnife"}, ["Type"] = "Collectible"},
+            {["Data"] = {["BoxName"] = "FlameBox"}, ["Type"] = "Box"},
+            {["Data"] = {["ElementType"] = "Gun", ["ElementName"] = "CapGun"}, ["Type"] = "Collectible"},
+            {["Data"] = {["ElementType"] = "Aura", ["ElementName"] = "Hurricane"}, ["Type"] = "Progress"},
+            {["Data"] = {["BoxName"] = "FlameBox"}, ["Type"] = "Box"},
+            {["Data"] = {["ElementType"] = "Knife", ["ElementName"] = "BuildingBlocks"}, ["Type"] = "Collectible"},
+            {["Data"] = {["BoxName"] = "FlameBox"}, ["Type"] = "Box"},
+            {["Data"] = {["ElementType"] = "Gun", ["ElementName"] = "GelBlaster"}, ["Type"] = "Collectible"},
+            {["Data"] = {["BoxName"] = "FlameBox"}, ["Type"] = "Box"},
+            {["Data"] = {["BoxName"] = "FlameBox"}, ["Type"] = "Box"},
+            {["Data"] = {["ElementType"] = "Knife", ["ElementName"] = "CosplayKnife"}, ["Type"] = "Collectible"},
+            {["Data"] = {["BoxName"] = "FlameBox"}, ["Type"] = "Box"},
+            {["Data"] = {["ElementType"] = "Gun", ["ElementName"] = "LaserTag"}, ["Type"] = "Collectible"},
+            {["Data"] = {["ElementType"] = "Aura", ["ElementName"] = "Lightning"}, ["Type"] = "Progress"},
+            {["Data"] = {["BoxName"] = "FlameBox"}, ["Type"] = "Box"},
+            {["Data"] = {["BoxName"] = "FlameBox"}, ["Type"] = "Box"},
+            {["Data"] = {["ElementType"] = "Knife", ["ElementName"] = "ChessClub"}, ["Type"] = "Collectible"},
+            {["Data"] = {["ElementType"] = "Gun", ["ElementName"] = "Checkmate"}, ["Type"] = "Collectible"},
+            {["Data"] = {["ElementType"] = "Ability", ["ElementName"] = "GodMode"}, ["Type"] = "Progress"}
+        },
+        ["GetCoinsForDay"] = "function: 0xfdba6a4f6f2b41dd",
+        ["DFinalCoins"] = 200,
+        ["Day0Coins"] = 10,
+        ["CoinIncreasePerDay"] = 10
+    }
+}
