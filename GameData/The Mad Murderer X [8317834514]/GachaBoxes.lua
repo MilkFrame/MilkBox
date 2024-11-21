d2906e425c188284273abfae6bec2cabe38a12d4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-22 02:37:24 +1100

    Create GachaBoxes.lua

---
+++ GameData/The Mad Murderer X [8317834514]/GachaBoxes.lua
@@ -0,0 +1,53 @@
+data = {
+    ["WoodBox"] = {
+        ["Image"] = "rbxassetid://11812813836",
+        ["Pool"] = "WoodBox",
+        ["CurrencyAmount"] = 1000,
+        ["CurrencyType"] = "Coin",
+        ["DisplayName"] = "Wood Box"
+    },
+    ["FoodBox"] = {
+        ["Image"] = "rbxassetid://12164111795",
+        ["Pool"] = "FoodBox",
+        ["CurrencyAmount"] = 1000,
+        ["CurrencyType"] = "Coin",
+        ["DisplayName"] = "Food Box"
+    },
+    ["PlanetBox"] = {
+        ["Image"] = "rbxassetid://11835181858",
+        ["Pool"] = "PlanetBox",
+        ["CurrencyAmount"] = 1000,
+        ["CurrencyType"] = "Coin",
+        ["DisplayName"] = "Planet Box"
+    },
+    ["MetalBox"] = {
+        ["Image"] = "rbxassetid://11835123049",
+        ["Pool"] = "MetalBox",
+        ["CurrencyAmount"] = 1000,
+        ["CurrencyType"] = "Coin",
+        ["DisplayName"] = "Metal Box"
+    },
+    ["FriendBox"] = {["Image"] = "rbxassetid://17353616722", ["Pool"] = "FriendBox", ["DisplayName"] = "Friend Box"},
+    ["PetBox1"] = {
+        ["Image"] = "rbxassetid://11835299060",
+        ["Pool"] = "PetBox1",
+        ["CurrencyAmount"] = 2000,
+        ["CurrencyType"] = "Coin",
+        ["DisplayName"] = "Pet Box #1"
+    },
+    ["BlacksmithBox"] = {
+        ["Image"] = "rbxassetid://12115080564",
+        ["Pool"] = "BlacksmithBox",
+        ["CurrencyAmount"] = 1000,
+        ["CurrencyType"] = "Coin",
+        ["DisplayName"] = "Blacksmith Box"
+    },
+    ["ToyBox"] = {
+        ["Image"] = "rbxassetid://13443907250",
+        ["Pool"] = "ToyBox",
+        ["CurrencyAmount"] = 1000,
+        ["CurrencyType"] = "Coin",
+        ["DisplayName"] = "Toy Box"
+    },
+    ["FlameBox"] = {["Image"] = "rbxassetid://15440558003", ["Pool"] = "FlameBox", ["DisplayName"] = "Flame Box"}
+}
