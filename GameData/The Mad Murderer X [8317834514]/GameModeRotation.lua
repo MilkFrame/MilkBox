ab25df5c1974bacb27e0e20b25e81a13e06d5383
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-22 02:35:10 +1100

    Create GameModeRotation.lua

---
+++ GameData/The Mad Murderer X [8317834514]/GameModeRotation.lua
@@ -0,0 +1,35 @@
+data = {
+    ["Pools"] = {
+        ["OldSchool"] = {
+            {["Map"] = "TMM1Office", ["Weight"] = 1, ["GameMode"] = "Murder"},
+            {["Map"] = "TMM1Library", ["Weight"] = 1, ["GameMode"] = "Murder"},
+            {["Map"] = "TMM1Labs", ["Weight"] = 1, ["GameMode"] = "Murder"},
+            {["Map"] = "TMM1Mineshaft", ["Weight"] = 1, ["GameMode"] = "Murder"},
+            {["Map"] = "TMM1Corridors", ["Weight"] = 1, ["GameMode"] = "Murder"},
+            {["Map"] = "MGDarkLibrary", ["Weight"] = 1, ["GameMode"] = "Murder"},
+            {["Map"] = "MGMountainCastle", ["Weight"] = 1, ["GameMode"] = "Murder"},
+            {["Map"] = "MGOffice", ["Weight"] = 1, ["GameMode"] = "Murder"}
+        },
+        ["CTF"] = {
+            {["Map"] = "CastleCross", ["Weight"] = 1, ["GameMode"] = "CTF"},
+            {["Map"] = "Turbine", ["Weight"] = 1, ["GameMode"] = "CTF"},
+            {["Map"] = "WildWest", ["Weight"] = 1, ["GameMode"] = "TDM"},
+            {["Map"] = "SkyCastle", ["Weight"] = 1, ["GameMode"] = "TDM"}
+        },
+        ["Main"] = {
+            {["Map"] = "SpookyHouse", ["Weight"] = 1, ["GameMode"] = "Murder"},
+            {["Map"] = "SpookyMansion", ["Weight"] = 1, ["GameMode"] = "Murder"},
+            {["Map"] = "BadBusiness", ["Weight"] = 1, ["GameMode"] = "Murder"},
+            {["Map"] = "PizzaParty", ["Weight"] = 1, ["GameMode"] = "Murder"},
+            {["Map"] = "MagicAcademy", ["Weight"] = 1, ["GameMode"] = "Murder"},
+            {["Map"] = "DuckFactory", ["Weight"] = 1, ["GameMode"] = "Murder"},
+            {["Map"] = "Asylum", ["Weight"] = 1, ["GameMode"] = "Murder"}
+        }
+    },
+    ["Rotations"] = {
+        ["OldSchool"] = "function: 0x5071125095a8be07",
+        ["CTF"] = "function: 0x24a76b23c360f717",
+        ["Main"] = "function: 0x0ad941eef2992de7"
+    },
+    ["GetOptions"] = "function: 0xcc1ad32cd57bd287"
+}
