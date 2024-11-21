bebd4356572562d709eb3832adca6e05a0bf023a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-22 02:35:28 +1100

    Create ServerType.lua

---
+++ GameData/The Mad Murderer X [8317834514]/ServerType.lua
@@ -0,0 +1,120 @@
+data = {
+    ["Index"] = {
+        {
+            ["LobbyName"] = "LobbyMain",
+            ["Description"] = "A game of innocents and a sheriff against a cruel murderer!",
+            ["Leaders"] = {
+                ["Season"] = 1,
+                ["Title1"] = "MAD MURDERER",
+                ["IsActive"] = true,
+                ["Title2"] = "All Time Murderer Victories"
+            },
+            ["PlaceId"] = 8317834514,
+            ["DisplayName"] = "Mad Murderer",
+            ["Name"] = "Main",
+            ["CanJoin"] = true,
+            ["Icon"] = "rbxassetid://17112167120",
+            ["Index"] = 1
+        },
+        {
+            ["LobbyName"] = "LobbyMain",
+            ["Description"] = "An action-packed mode of two teams fighting each other!",
+            ["Leaders"] = {
+                ["Season"] = 1,
+                ["Title1"] = "CAPTURE THE FLAG",
+                ["IsActive"] = true,
+                ["Title2"] = "All Time Flag Captures"
+            },
+            ["PlaceId"] = 17199792086,
+            ["DisplayName"] = "Battle!",
+            ["Name"] = "CTF",
+            ["CanJoin"] = true,
+            ["Icon"] = "rbxassetid://17112290098",
+            ["Index"] = 2
+        },
+        {
+            ["LobbyName"] = "LobbyMadGames",
+            ["Description"] = "A trip back to the roots of MAD STUDIO! Featuring old school maps and anonymous mode!",
+            ["Leaders"] = {
+                ["Season"] = 1,
+                ["Title1"] = "OLD SCHOOL",
+                ["IsActive"] = true,
+                ["Title2"] = "All Time Murderer Victories"
+            },
+            ["PlaceId"] = 17199793946,
+            ["DisplayName"] = "Old School",
+            ["Name"] = "OldSchool",
+            ["CanJoin"] = true,
+            ["Icon"] = "rbxassetid://17112405899",
+            ["Index"] = 3
+        }
+    },
+    ["LatestVersionTag"] = "CTFMainOldSchool",
+    ["Types"] = {
+        ["OldSchool"] = {
+            ["LobbyName"] = "LobbyMadGames",
+            ["Description"] = "A trip back to the roots of MAD STUDIO! Featuring old school maps and anonymous mode!",
+            ["Leaders"] = {
+                ["Season"] = 1,
+                ["Title1"] = "OLD SCHOOL",
+                ["IsActive"] = true,
+                ["Title2"] = "All Time Murderer Victories"
+            },
+            ["PlaceId"] = 17199793946,
+            ["DisplayName"] = "Old School",
+            ["Name"] = "OldSchool",
+            ["CanJoin"] = true,
+            ["Icon"] = "rbxassetid://17112405899",
+            ["Index"] = 3
+        },
+        ["CTF"] = {
+            ["LobbyName"] = "LobbyMain",
+            ["Description"] = "An action-packed mode of two teams fighting each other!",
+            ["Leaders"] = {
+                ["Season"] = 1,
+                ["Title1"] = "CAPTURE THE FLAG",
+                ["IsActive"] = true,
+                ["Title2"] = "All Time Flag Captures"
+            },
+            ["PlaceId"] = 17199792086,
+            ["DisplayName"] = "Battle!",
+            ["Name"] = "CTF",
+            ["CanJoin"] = true,
+            ["Icon"] = "rbxassetid://17112290098",
+            ["Index"] = 2
+        },
+        ["Main"] = {
+            ["LobbyName"] = "LobbyMain",
+            ["Description"] = "A game of innocents and a sheriff against a cruel murderer!",
+            ["Leaders"] = {
+                ["Season"] = 1,
+                ["Title1"] = "MAD MURDERER",
+                ["IsActive"] = true,
+                ["Title2"] = "All Time Murderer Victories"
+            },
+            ["PlaceId"] = 8317834514,
+            ["DisplayName"] = "Mad Murderer",
+            ["Name"] = "Main",
+            ["CanJoin"] = true,
+            ["Icon"] = "rbxassetid://17112167120",
+            ["Index"] = 1
+        }
+    },
+    ["ThisData"] = {
+        ["LobbyName"] = "LobbyMain",
+        ["Description"] = "A game of innocents and a sheriff against a cruel murderer!",
+        ["Leaders"] = {
+            ["Season"] = 1,
+            ["Title1"] = "MAD MURDERER",
+            ["IsActive"] = true,
+            ["Title2"] = "All Time Murderer Victories"
+        },
+        ["PlaceId"] = 8317834514,
+        ["DisplayName"] = "Mad Murderer",
+        ["Name"] = "Main",
+        ["CanJoin"] = true,
+        ["Icon"] = "rbxassetid://17112167120",
+        ["Index"] = 1
+    },
+    ["ThisType"] = "Main"
+}
