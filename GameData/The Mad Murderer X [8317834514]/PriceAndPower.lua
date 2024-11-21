04471c69dc7fbe257d3b2b8b3e0befb09a4e479a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-22 02:36:46 +1100

    Create PriceAndPower.lua

---
+++ GameData/The Mad Murderer X [8317834514]/PriceAndPower.lua
@@ -0,0 +1,38 @@
+data = {
+    ["CanDelete"] = "function: 0x94d621b5c94f39b7",
+    ["TierPower"] = {5, 100, 1500, 17000, 149000, 1050000, 6000000, 30500000},
+    ["IsTradeable"] = "function: 0x5b26184168dbef87",
+    ["TierColor"] = {
+        Color3.fromRGB(240, 240, 240),
+        Color3.fromRGB(110, 255, 60),
+        Color3.fromRGB(255, 255, 30),
+        Color3.fromRGB(255, 60, 240),
+        Color3.fromRGB(255, 60, 60),
+        Color3.fromRGB(20, 110, 230),
+        Color3.fromRGB(60, 60, 60)
+    },
+    ["TierPrice"] = {500, 40000, 884000, 14000000, 177000000, 1700000000, 13000000000, 81000000000},
+    ["KShorten"] = "function: 0x5445f88816f0c0a7",
+    ["KRound"] = "function: 0x5d6e20611641b467",
+    ["TierColorize"] = "function: 0x807bdfe6fbba73a7",
+    ["PowerToCPM"] = "function: 0xc88be0d75ca82ed7",
+    ["GetCollectiblePower"] = "function: 0xa495e64bca753297",
+    ["FusionChances"] = {
+        {
+            ["RoundPriceTo"] = 10,
+            ["ChanceCurve"] = "function: 0x540a0e117ca4ad87",
+            ["MaxItems"] = 8,
+            ["PriceRubyPerItem"] = 30,
+            ["PriceExponent"] = 0.93,
+            ["QualityPriceExponent"] = 2
+        },
+        {
+            ["RoundPriceTo"] = 10,
+            ["ChanceCurve"] = "function: 0xdc300da9452dd347",
+            ["MaxItems"] = 8,
+            ["PriceRubyPerItem"] = 50,
+            ["PriceExponent"] = 0.93,
+            ["QualityPriceExponent"] = 2
+        }
+    }
+}
