0441ba7248954154f78623e0d8e49646b3651a9c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-22 02:37:52 +1100

    Create Recipes.lua

---
+++ GameData/The Mad Murderer X [8317834514]/Recipes.lua
@@ -0,0 +1,297 @@
+data = {
+    ["RecipeList"] = {
+        ["UltimateWoodKnife"] = {
+            ["Input"] = {
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Wood", ["Tags"] = {["Stack"] = 40}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Ash", ["Tags"] = {["Stack"] = 40}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Oak", ["Tags"] = {["Stack"] = 35}}},
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Knife",
+                        ["ElementName"] = "Darkwood",
+                        ["Tags"] = {["Stack"] = 20}
+                    }
+                },
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Knife",
+                        ["ElementName"] = "Mahogany",
+                        ["Tags"] = {["Stack"] = 10}
+                    }
+                },
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Knife",
+                        ["ElementName"] = "Yggdrasil",
+                        ["Tags"] = {["Stack"] = 5}
+                    }
+                }
+            },
+            ["Output"] = {["ElementType"] = "Knife", ["ElementName"] = "UltimateWoodKnife", ["Tags"] = {}}
+        },
+        ["UltimateFoodKnife"] = {
+            ["Input"] = {
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Honey", ["Tags"] = {["Stack"] = 40}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Banana", ["Tags"] = {["Stack"] = 40}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Bacon", ["Tags"] = {["Stack"] = 35}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Melon", ["Tags"] = {["Stack"] = 20}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Waffle", ["Tags"] = {["Stack"] = 10}}},
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Knife",
+                        ["ElementName"] = "Blueberry",
+                        ["Tags"] = {["Stack"] = 5}
+                    }
+                }
+            },
+            ["Output"] = {["ElementType"] = "Knife", ["ElementName"] = "UltimateFoodKnife", ["Tags"] = {}}
+        },
+        ["UltimatePlanetGun"] = {
+            ["Input"] = {
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Asteroid", ["Tags"] = {["Stack"] = 40}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Moon", ["Tags"] = {["Stack"] = 40}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Venus", ["Tags"] = {["Stack"] = 35}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Mars", ["Tags"] = {["Stack"] = 20}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Neptune", ["Tags"] = {["Stack"] = 10}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "PlanetX", ["Tags"] = {["Stack"] = 5}}}
+            },
+            ["Output"] = {["ElementType"] = "Gun", ["ElementName"] = "UltimatePlanetGun", ["Tags"] = {}}
+        },
+        ["UltimateMetalKnife"] = {
+            ["Input"] = {
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Iron", ["Tags"] = {["Stack"] = 40}}},
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Knife",
+                        ["ElementName"] = "Aluminum",
+                        ["Tags"] = {["Stack"] = 40}
+                    }
+                },
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Copper", ["Tags"] = {["Stack"] = 35}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Bronze", ["Tags"] = {["Stack"] = 20}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Silver", ["Tags"] = {["Stack"] = 10}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Gold", ["Tags"] = {["Stack"] = 5}}}
+            },
+            ["Output"] = {["ElementType"] = "Knife", ["ElementName"] = "UltimateMetalKnife", ["Tags"] = {}}
+        },
+        ["UltimateFriendKnife"] = {
+            ["Input"] = {
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "LockKnife", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "KeyKnife", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "CladdaghKnife", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "LotusKnife", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "InfinityKnife", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "TieDyeKnife", ["Tags"] = {}}}
+            },
+            ["Output"] = {["ElementType"] = "Knife", ["ElementName"] = "LafisKnife", ["Tags"] = {}}
+        },
+        ["UltimateMetalGun"] = {
+            ["Input"] = {
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Iron", ["Tags"] = {["Stack"] = 40}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Aluminum", ["Tags"] = {["Stack"] = 40}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Copper", ["Tags"] = {["Stack"] = 35}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Bronze", ["Tags"] = {["Stack"] = 20}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Silver", ["Tags"] = {["Stack"] = 10}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Gold", ["Tags"] = {["Stack"] = 5}}}
+            },
+            ["Output"] = {["ElementType"] = "Gun", ["ElementName"] = "UltimateMetalGun", ["Tags"] = {}}
+        },
+        ["UltimateToyGun"] = {
+            ["Input"] = {
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Jigsaw", ["Tags"] = {["Stack"] = 40}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Teddy", ["Tags"] = {["Stack"] = 40}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Merf", ["Tags"] = {["Stack"] = 35}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "PopIt", ["Tags"] = {["Stack"] = 20}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Fuzzy", ["Tags"] = {["Stack"] = 10}}},
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Gun",
+                        ["ElementName"] = "GlitterSlime",
+                        ["Tags"] = {["Stack"] = 5}
+                    }
+                }
+            },
+            ["Output"] = {["ElementType"] = "Gun", ["ElementName"] = "SqueakyPiggy", ["Tags"] = {}}
+        },
+        ["UltimatePlanetKnife"] = {
+            ["Input"] = {
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Knife",
+                        ["ElementName"] = "Asteroid",
+                        ["Tags"] = {["Stack"] = 40}
+                    }
+                },
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Moon", ["Tags"] = {["Stack"] = 40}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Venus", ["Tags"] = {["Stack"] = 35}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Mars", ["Tags"] = {["Stack"] = 20}}},
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Knife",
+                        ["ElementName"] = "Neptune",
+                        ["Tags"] = {["Stack"] = 10}
+                    }
+                },
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "PlanetX", ["Tags"] = {["Stack"] = 5}}}
+            },
+            ["Output"] = {["ElementType"] = "Knife", ["ElementName"] = "UltimatePlanetKnife", ["Tags"] = {}}
+        },
+        ["UltimateWoodGun"] = {
+            ["Input"] = {
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Stump", ["Tags"] = {["Stack"] = 40}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Ash", ["Tags"] = {["Stack"] = 40}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Oak", ["Tags"] = {["Stack"] = 35}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Darkwood", ["Tags"] = {["Stack"] = 20}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Mahogany", ["Tags"] = {["Stack"] = 10}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Yggdrasil", ["Tags"] = {["Stack"] = 5}}}
+            },
+            ["Output"] = {["ElementType"] = "Gun", ["ElementName"] = "UltimateWoodGun", ["Tags"] = {}}
+        },
+        ["UltimateBlacksmithGun"] = {
+            ["Input"] = {
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Hardened", ["Tags"] = {["Stack"] = 40}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "BigIron", ["Tags"] = {["Stack"] = 40}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Damascus", ["Tags"] = {["Stack"] = 35}}},
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Gun",
+                        ["ElementName"] = "Flintlock",
+                        ["Tags"] = {["Stack"] = 20}
+                    }
+                },
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Mithril", ["Tags"] = {["Stack"] = 10}}},
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Gun",
+                        ["ElementName"] = "Adamantium",
+                        ["Tags"] = {["Stack"] = 5}
+                    }
+                }
+            },
+            ["Output"] = {["ElementType"] = "Gun", ["ElementName"] = "UltimateBlacksmithGun", ["Tags"] = {}}
+        },
+        ["UltimateToyKnife"] = {
+            ["Input"] = {
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Jigsaw", ["Tags"] = {["Stack"] = 40}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Teddy", ["Tags"] = {["Stack"] = 40}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Merf", ["Tags"] = {["Stack"] = 35}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "PopIt", ["Tags"] = {["Stack"] = 20}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Fuzzy", ["Tags"] = {["Stack"] = 10}}},
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Knife",
+                        ["ElementName"] = "GlitterSlime",
+                        ["Tags"] = {["Stack"] = 5}
+                    }
+                }
+            },
+            ["Output"] = {["ElementType"] = "Knife", ["ElementName"] = "SqueakyMallet", ["Tags"] = {}}
+        },
+        ["UltimateBlacksmithKnife"] = {
+            ["Input"] = {
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Knife",
+                        ["ElementName"] = "Hardened",
+                        ["Tags"] = {["Stack"] = 40}
+                    }
+                },
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "Forged", ["Tags"] = {["Stack"] = 40}}},
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Knife",
+                        ["ElementName"] = "Damascus",
+                        ["Tags"] = {["Stack"] = 35}
+                    }
+                },
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Knife",
+                        ["ElementName"] = "Dragonbone",
+                        ["Tags"] = {["Stack"] = 20}
+                    }
+                },
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Knife",
+                        ["ElementName"] = "Mithril",
+                        ["Tags"] = {["Stack"] = 10}
+                    }
+                },
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Knife",
+                        ["ElementName"] = "Adamantium",
+                        ["Tags"] = {["Stack"] = 5}
+                    }
+                }
+            },
+            ["Output"] = {["ElementType"] = "Knife", ["ElementName"] = "UltimateBlacksmithKnife", ["Tags"] = {}}
+        },
+        ["UltimateFlameKnife"] = {
+            ["Input"] = {
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "WhiteFlameKnife", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "YellowFlameKnife", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "GreenFlameKnife", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "BlueFlameKnife", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "PurpleFlameKnife", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Knife", ["ElementName"] = "OrangeFlameKnife", ["Tags"] = {}}}
+            },
+            ["Output"] = {["ElementType"] = "Knife", ["ElementName"] = "ScorchTorch", ["Tags"] = {}}
+        },
+        ["UltimateFriendGun"] = {
+            ["Input"] = {
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "LockGun", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "KeyGun", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "CladdaghGun", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "LotusGun", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "InfinityGun", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "TieDyeGun", ["Tags"] = {}}}
+            },
+            ["Output"] = {["ElementType"] = "Gun", ["ElementName"] = "LafisGun", ["Tags"] = {}}
+        },
+        ["UltimateFoodGun"] = {
+            ["Input"] = {
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Citrus", ["Tags"] = {["Stack"] = 40}}},
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Gun",
+                        ["ElementName"] = "SaltPepper",
+                        ["Tags"] = {["Stack"] = 40}
+                    }
+                },
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "Bacon", ["Tags"] = {["Stack"] = 35}}},
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Gun",
+                        ["ElementName"] = "PeaShooter",
+                        ["Tags"] = {["Stack"] = 20}
+                    }
+                },
+                {
+                    ["Collectible"] = {
+                        ["ElementType"] = "Gun",
+                        ["ElementName"] = "Sweetroll",
+                        ["Tags"] = {["Stack"] = 10}
+                    }
+                },
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "IceCream", ["Tags"] = {["Stack"] = 5}}}
+            },
+            ["Output"] = {["ElementType"] = "Gun", ["ElementName"] = "UltimateFoodGun", ["Tags"] = {}}
+        },
+        ["UltimateFlameGun"] = {
+            ["Input"] = {
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "WhiteFlameGun", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "YellowFlameGun", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "GreenFlameGun", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "BlueFlameGun", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "PurpleFlameGun", ["Tags"] = {}}},
+                {["Collectible"] = {["ElementType"] = "Gun", ["ElementName"] = "OrangeFlameGun", ["Tags"] = {}}}
+            },
+            ["Output"] = {["ElementType"] = "Gun", ["ElementName"] = "Hotshot", ["Tags"] = {}}
+        }
+    },
+    ["IsSuitable"] = "function: 0x933cc290b19863bd",
+    ["GetStackRequirement"] = "function: 0xa621a21bb5df048d",
+    ["GetElementTypes"] = "function: 0x218475d60ff6abcd"
+}
