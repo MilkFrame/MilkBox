0c7994ea3bccd442e06df40d5415f2a5c9808e08
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-22 02:36:33 +1100

    Create BadgeConfig.lua

---
+++ GameData/The Mad Murderer X [8317834514]/BadgeConfig.lua
@@ -0,0 +1,177 @@
+data = {
+    ["GetBadgeId"] = "function: 0x565a663146af09e7",
+    ["GetBadgeConfig"] = "function: 0xcef1870298d05317",
+    ["GetBadgeInfo"] = "function: 0x34a7b0579a585a07",
+    ["Badges"] = {
+        ["BusinessQuest"] = {
+            ["Id"] = 2127432899,
+            ["Config"] = {},
+            ["Info"] = {
+                ["Id"] = 2127432899,
+                ["Config"] = {},
+                ["Key"] = "BusinessQuest",
+                ["Icon"] = "rbxassetid://10257044681"
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["SuspiciousBadge1"] = {
+            ["Id"] = 2127433158,
+            ["Config"] = {},
+            ["Info"] = {
+                ["Id"] = 2127433158,
+                ["Config"] = {},
+                ["Key"] = "SuspiciousBadge1",
+                ["Icon"] = "rbxassetid://10257276781"
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["TooMuchWitchcraft"] = {
+            ["Id"] = 145732336223319,
+            ["Config"] = {},
+            ["Info"] = {
+                ["Id"] = 145732336223319,
+                ["Config"] = {},
+                ["Key"] = "TooMuchWitchcraft",
+                ["Icon"] = "rbxassetid://17284491625"
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["YouPlayed"] = {
+            ["Id"] = 2124886180,
+            ["Config"] = {},
+            ["Info"] = {
+                ["Id"] = 2124886180,
+                ["Config"] = {},
+                ["Key"] = "YouPlayed",
+                ["Icon"] = "rbxassetid://8327252202"
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["QuestionableEthics"] = {
+            ["Id"] = 2130069326,
+            ["Config"] = {},
+            ["Info"] = {
+                ["Id"] = 2130069326,
+                ["Config"] = {},
+                ["Key"] = "QuestionableEthics",
+                ["Icon"] = "rbxassetid://11908413659"
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["TheSecurityGuardIncident"] = {
+            ["Id"] = 2127432867,
+            ["Config"] = {},
+            ["Info"] = {
+                ["Id"] = 2127432867,
+                ["Config"] = {},
+                ["Key"] = "TheSecurityGuardIncident",
+                ["Icon"] = "rbxassetid://10257014227"
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["TheGrillIncident"] = {
+            ["Id"] = 2127419684,
+            ["Config"] = {},
+            ["Info"] = {
+                ["Id"] = 2127419684,
+                ["Config"] = {},
+                ["Key"] = "TheGrillIncident",
+                ["Icon"] = "rbxassetid://10249817367"
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["RatCatcherEnthusiast"] = {
+            ["Id"] = 2127413371,
+            ["Config"] = {},
+            ["Info"] = {
+                ["Id"] = 2127413371,
+                ["Config"] = {},
+                ["Key"] = "RatCatcherEnthusiast",
+                ["Icon"] = "rbxassetid://10258374774"
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["GetYipeed"] = {
+            ["Id"] = 2127432872,
+            ["Config"] = {},
+            ["Info"] = {
+                ["Id"] = 2127432872,
+                ["Config"] = {},
+                ["Key"] = "GetYipeed",
+                ["Icon"] = "rbxassetid://10257016881"
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        },
+        ["OutOfBounds"] = {
+            ["Id"] = 2130251227,
+            ["Config"] = {},
+            ["Info"] = {
+                ["Id"] = 2130251227,
+                ["Config"] = {},
+                ["Key"] = "OutOfBounds",
+                ["Icon"] = "rbxassetid://12017076718"
+            },
+            ["InfoAdded"] = {
+                ["_listener_count"] = 0,
+                ["GetListenerCount"] = "function: 0x133998bb841e7027",
+                ["Fire"] = "function: 0x4184196184282a67",
+                ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+                ["Connect"] = "function: 0x1c123cbc18ad3067"
+            }
+        }
+    }
+}
