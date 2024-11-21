a5ae73bceed64ffa5054ba31dcc7b4552b1d3ad5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-22 02:35:54 +1100

    Create VirtualPassList.lua

---
+++ GameData/The Mad Murderer X [8317834514]/VirtualPassList.lua
@@ -0,0 +1,52 @@
+data = {
+    ["MurdererChance"] = {
+        ["InventoryNeeded"] = 0,
+        ["Description"] = "Increases your chance to become the murderer each round permanently!",
+        ["OnReward"] = "function: 0xc387fb4196666627",
+        ["DisplayName"] = "Evil",
+        ["Info"] = {
+            ["BasePriceRobux"] = 200,
+            ["Key"] = "MurdererChance",
+            ["DiscountPriceRobux"] = 20,
+            ["Icon"] = "rbxassetid://16591816037",
+            ["DisplayName"] = "Evil"
+        },
+        ["DiscountProductId"] = 1767485591,
+        ["InfoAdded"] = {
+            ["_listener_count"] = 0,
+            ["GetListenerCount"] = "function: 0x133998bb841e7027",
+            ["Fire"] = "function: 0x4184196184282a67",
+            ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+            ["Connect"] = "function: 0x1c123cbc18ad3067"
+        },
+        ["WindowName"] = "StarterOffer",
+        ["DiscountTimeHours"] = 48,
+        ["Icon"] = "rbxassetid://16591816037",
+        ["BaseProductId"] = 1767485385
+    },
+    ["StarterPack1"] = {
+        ["InventoryNeeded"] = 3,
+        ["Description"] = "Receive a heavenly-themed gun, knife, pet and weapon aura! (One purchase per player)",
+        ["OnReward"] = "function: 0xed5419f85c08fec7",
+        ["DisplayName"] = "Good",
+        ["Info"] = {
+            ["BasePriceRobux"] = 400,
+            ["Key"] = "StarterPack1",
+            ["DiscountPriceRobux"] = 80,
+            ["Icon"] = "rbxassetid://16591295193",
+            ["DisplayName"] = "Good"
+        },
+        ["DiscountProductId"] = 1767485113,
+        ["InfoAdded"] = {
+            ["_listener_count"] = 0,
+            ["GetListenerCount"] = "function: 0x133998bb841e7027",
+            ["Fire"] = "function: 0x4184196184282a67",
+            ["FireUntil"] = "function: 0xb1da34334fc3fb87",
+            ["Connect"] = "function: 0x1c123cbc18ad3067"
+        },
+        ["WindowName"] = "StarterOffer",
+        ["DiscountTimeHours"] = 48,
+        ["Icon"] = "rbxassetid://16591295193",
+        ["BaseProductId"] = 1767484736
+    }
+}
