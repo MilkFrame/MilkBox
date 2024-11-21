931486db119c2b1038a81bbfb716af4089fbbd9b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-22 02:29:56 +1100

    Create cleanLeagues.lua

---
+++ GameData/Gym League [17450551531]/cleanLeagues.lua
@@ -0,0 +1,39 @@
+data = {
+    {
+        ["lp"] = 0,
+        ["image"] = "rbxassetid://87715390915630",
+        ["name"] = "Bronze",
+        ["chestImage"] = "rbxassetid://112415204654861"
+    },
+    {
+        ["lp"] = 5000,
+        ["image"] = "rbxassetid://137905864663699",
+        ["name"] = "Silver",
+        ["chestImage"] = "rbxassetid://115783689026072"
+    },
+    {
+        ["lp"] = 20000,
+        ["image"] = "rbxassetid://71568235534822",
+        ["name"] = "Gold",
+        ["chestImage"] = "rbxassetid://115783689026072"
+    },
+    {
+        ["lp"] = 50000,
+        ["image"] = "rbxassetid://75772793894229",
+        ["name"] = "Platinum",
+        ["chestImage"] = "rbxassetid://116626189034031"
+    },
+    {
+        ["lp"] = 100000,
+        ["image"] = "rbxassetid://97393561013100",
+        ["name"] = "Demon",
+        ["chestImage"] = "rbxassetid://73127272574078"
+    },
+    {
+        ["image"] = "rbxassetid://110520223341581",
+        ["name"] = "Titan",
+        ["lp"] = 100000,
+        ["chestImage"] = "rbxassetid://72384518193624",
+        ["rank"] = 25
+    }
+}
