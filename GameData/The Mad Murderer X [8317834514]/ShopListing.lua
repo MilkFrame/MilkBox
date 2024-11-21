eb51494cf090960aeb552381cc1d01c695e87949
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-22 02:37:37 +1100

    Create ShopListing.lua

---
+++ GameData/The Mad Murderer X [8317834514]/ShopListing.lua
@@ -0,0 +1,132 @@
+data = {
+    ["Pets"] = {{["ElementType"] = "Gacha", ["ElementName"] = "PetBox1"}},
+    ["Abilities"] = {
+        {["ElementType"] = "Ability", ["PriceCoin"] = 1000, ["ElementName"] = "ObservingGoop"},
+        {["ElementType"] = "Ability", ["PriceCoin"] = 5000, ["ElementName"] = "BearTrap"},
+        {["ElementType"] = "Ability", ["PriceCoin"] = 5000, ["ElementName"] = "SmokeBomb"},
+        {["ElementType"] = "Ability", ["PriceCoin"] = 7000, ["ElementName"] = "Frame"},
+        {["ElementType"] = "Ability", ["PriceCoin"] = 10000, ["ElementName"] = "Radar"},
+        {["ElementType"] = "Ability", ["PriceCoin"] = 10000, ["ElementName"] = "Teleport"}
+    },
+    ["Emotes"] = {
+        {["ElementType"] = "Gear", ["PriceCoin"] = 50, ["ElementName"] = "BloxyCola"},
+        {["ElementType"] = "Gear", ["PriceCoin"] = 1500, ["ElementName"] = "CoinFlip"},
+        {["ElementType"] = "Gear", ["PriceCoin"] = 1500, ["ElementName"] = "Bubblegum"},
+        {["ElementType"] = "Gear", ["PriceCoin"] = 3000, ["ElementName"] = "Camera"},
+        {["ElementType"] = "Gear", ["PriceCoin"] = 600, ["ElementName"] = "Pizza"},
+        {["ElementType"] = "Gear", ["PriceCoin"] = 600, ["ElementName"] = "Donut"},
+        {["ElementType"] = "Gear", ["PriceCoin"] = 600, ["ElementName"] = "Cheeseburger"},
+        {["ElementType"] = "Gear", ["PriceCoin"] = 600, ["ElementName"] = "ChocolateMilk"},
+        {["ElementType"] = "Gear", ["PriceCoin"] = 600, ["ElementName"] = "IceCream"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 3000, ["ElementName"] = "MadMurderer"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 1500, ["ElementName"] = "Thriller"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 3000, ["ElementName"] = "Conga"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 3000, ["ElementName"] = "Mannrobics"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 3000, ["ElementName"] = "PillarMan"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 3000, ["ElementName"] = "TortureDance"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 6000, ["ElementName"] = "Chad"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 20000, ["ElementName"] = "BunnyHop"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 100, ["ElementName"] = "TelephoneAwoo"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 1500, ["ElementName"] = "VirtualInsanity"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 3000, ["ElementName"] = "AllNight"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 3000, ["ElementName"] = "Honkai"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 6000, ["ElementName"] = "Guitar"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 6000, ["ElementName"] = "Radio"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 200, ["ElementName"] = "Laugh"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 3000, ["ElementName"] = "Griddy"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 3000, ["ElementName"] = "AnimeStride"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 3000, ["ElementName"] = "BestMates"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 1500, ["ElementName"] = "Caramelldansen"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 1500, ["ElementName"] = "HandsDance"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 1000, ["ElementName"] = "Sit"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 1500, ["ElementName"] = "PenguinDance"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 1500, ["ElementName"] = "ElectroSwing"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 500, ["ElementName"] = "California"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 3000, ["ElementName"] = "KazotskyKick"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 3000, ["ElementName"] = "PhonkWalk"},
+        {["ElementType"] = "Emote", ["PriceCoin"] = 3000, ["ElementName"] = "MrBeast"},
+        {["ElementType"] = "Gear", ["PriceCoin"] = 120, ["ElementName"] = "SignJoy"},
+        {["ElementType"] = "Gear", ["PriceCoin"] = 500, ["ElementName"] = "SignAngry"},
+        {["ElementType"] = "Gear", ["PriceCoin"] = 500, ["ElementName"] = "SignPeace"},
+        {["ElementType"] = "Gear", ["PriceCoin"] = 500, ["ElementName"] = "SignGoodGame"},
+        {["ElementType"] = "Gear", ["PriceCoin"] = 500, ["ElementName"] = "SignFunny"},
+        {["ElementType"] = "Gear", ["PriceCoin"] = 20000, ["ElementName"] = "MoneyBag"},
+        {["ElementType"] = "Gear", ["PriceCoin"] = 15000, ["ElementName"] = "Extinguisher"}
+    },
+    ["Weapons"] = {
+        {["ElementType"] = "Gacha", ["ElementName"] = "ToyBox"},
+        {["ElementType"] = "Gacha", ["ElementName"] = "FoodBox"},
+        {["ElementType"] = "Gacha", ["ElementName"] = "BlacksmithBox"},
+        {["ElementType"] = "Gacha", ["ElementName"] = "WoodBox"},
+        {["ElementType"] = "Gacha", ["ElementName"] = "MetalBox"},
+        {["ElementType"] = "Gacha", ["ElementName"] = "PlanetBox"}
+    },
+    ["Mad"] = {
+        {["ElementType"] = "VirtualPass", ["ElementName"] = "StarterPack1"},
+        {["ElementType"] = "VirtualPass", ["ElementName"] = "MurdererChance"},
+        {["ElementType"] = "Gamepass", ["ElementName"] = "Lucky"},
+        {["ElementType"] = "Gamepass", ["ElementName"] = "MadMythicals"},
+        {["ElementType"] = "Gamepass", ["ElementName"] = "Pets2"},
+        {
+            ["ElementType"] = "Knife",
+            ["PriceCoin"] = 20000,
+            ["ElementName"] = "DiamondTester",
+            ["Tags"] = {["DeathEffect"] = "Ice"}
+        },
+        {["ElementType"] = "Knife", ["PriceCoin"] = 20000, ["ElementName"] = "MetalPipe"},
+        {
+            ["ElementType"] = "Knife",
+            ["PriceCoin"] = 20000,
+            ["ElementName"] = "RuneDagger",
+            ["Tags"] = {["DeathEffect"] = "RPGDeath"}
+        },
+        {
+            ["ElementType"] = "Knife",
+            ["PriceCoin"] = 20000,
+            ["ElementName"] = "Troll",
+            ["Tags"] = {["DeathEffect"] = "Troll"}
+        },
+        {
+            ["ElementType"] = "Gun",
+            ["PriceCoin"] = 20000,
+            ["ElementName"] = "GrenadeGun",
+            ["Tags"] = {["DeathEffect"] = "Explode"}
+        },
+        {
+            ["ElementType"] = "Knife",
+            ["PriceCoin"] = 20000,
+            ["ElementName"] = "Explosive",
+            ["Tags"] = {["DeathEffect"] = "Explode"}
+        },
+        {
+            ["ElementType"] = "Knife",
+            ["PriceCoin"] = 30000,
+            ["ElementName"] = "Baller",
+            ["Tags"] = {["DeathEffect"] = "Baller"}
+        },
+        {
+            ["ElementType"] = "Knife",
+            ["PriceCoin"] = 30000,
+            ["ElementName"] = "BallerBlue",
+            ["Tags"] = {["DeathEffect"] = "Baller"}
+        },
+        {
+            ["ElementType"] = "Knife",
+            ["PriceCoin"] = 350000,
+            ["ElementName"] = "GoldenPan",
+            ["Tags"] = {["DeathEffect"] = "Gold"}
+        }
+    },
+    ["Auras"] = {
+        {["ElementType"] = "Aura", ["PriceCoin"] = 5000, ["ElementName"] = "RedFlame"},
+        {["ElementType"] = "Aura", ["PriceCoin"] = 5000, ["ElementName"] = "YellowFlame"},
+        {["ElementType"] = "Aura", ["PriceCoin"] = 5000, ["ElementName"] = "GreenFlame"},
+        {["ElementType"] = "Aura", ["PriceCoin"] = 5000, ["ElementName"] = "CyanFlame"},
+        {["ElementType"] = "Aura", ["PriceCoin"] = 5000, ["ElementName"] = "BlueFlame"},
+        {["ElementType"] = "Aura", ["PriceCoin"] = 5000, ["ElementName"] = "PinkFlame"},
+        {["ElementType"] = "Aura", ["PriceCoin"] = 5000, ["ElementName"] = "WhiteFlame"},
+        {["ElementType"] = "Aura", ["PriceCoin"] = 5000, ["ElementName"] = "BlackFlame"},
+        {["ElementType"] = "Aura", ["PriceCoin"] = 100000, ["ElementName"] = "BlackOut"},
+        {["ElementType"] = "Aura", ["PriceCoin"] = 100000, ["ElementName"] = "WhiteOut"}
+    }
+}
