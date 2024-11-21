1f54ce98e608c4489c1116e3431e6adca5476f9b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-22 02:36:08 +1100

    Create CurrencyInfo.lua

---
+++ GameData/The Mad Murderer X [8317834514]/CurrencyInfo.lua
@@ -0,0 +1,18 @@
+data = {
+    ["Coin"] = {
+        ["NameSingular"] = "Coin",
+        ["TextGradient"] = "0 1 0.960784 0.529412 0 1 0.807843 0.72549 0.0862745 0 ",
+        ["NameMultiple"] = "Coins",
+        ["Color"] = Color3.fromRGB(206, 185, 22),
+        ["InLineImage"] = "rbxassetid://8204733868",
+        ["ItemImage"] = "rbxassetid://11376723860"
+    },
+    ["Ruby"] = {
+        ["NameSingular"] = "Ruby",
+        ["TextGradient"] = "0 1 0.537255 0.607843 0 1 1 0.203922 0.215686 0 ",
+        ["NameMultiple"] = "Rubies",
+        ["Color"] = Color3.fromRGB(255, 52, 55),
+        ["InLineImage"] = "rbxassetid://8204733622",
+        ["ItemImage"] = "rbxassetid://11376723628"
+    }
+}
