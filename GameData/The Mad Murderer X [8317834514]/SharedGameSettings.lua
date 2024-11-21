d01737466bb8a87b5a6dda99190ca11b45108ce5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-22 02:37:11 +1100

    Create SharedGameSettings.lua

---
+++ GameData/The Mad Murderer X [8317834514]/SharedGameSettings.lua
@@ -0,0 +1,51 @@
+data = {
+    ["MouseIcon"] = {["Weapon"] = {["Default"] = "rbxassetid://11929013754"}},
+    ["DefaultCharacterRules"] = {
+        ["CharacterHeight1"] = 4,
+        ["MaxSlopeAngle"] = 60,
+        ["MaxJumpPower"] = 50,
+        ["MaxWalkSpeed"] = 16,
+        ["CharacterHeight2"] = 3,
+        ["CharacterWidth"] = 4,
+        ["MaxHipHeight"] = 2,
+        ["RootPartOffset"] = 0
+    },
+    ["CharacterHittableParts"] = {
+        ["Left Leg"] = true,
+        ["LeftLowerArm"] = true,
+        ["Right Leg"] = true,
+        ["Torso"] = true,
+        ["HumanoidRootPart"] = true,
+        ["RightLowerLeg"] = true,
+        ["LeftUpperLeg"] = true,
+        ["LeftLowerLeg"] = true,
+        ["LowerTorso"] = true,
+        ["Head"] = true,
+        ["UpperTorso"] = true,
+        ["LeftHand"] = true,
+        ["RightUpperArm"] = true,
+        ["LeftUpperArm"] = true,
+        ["RightLowerArm"] = true,
+        ["Right Arm"] = true,
+        ["Left Arm"] = true,
+        ["RightHand"] = true,
+        ["RightFoot"] = true,
+        ["RightUpperLeg"] = true,
+        ["LeftFoot"] = true
+    },
+    ["DefaultGravity"] = 196.2,
+    ["CollectionServiceTags"] = {
+        ["Barrier"] = "Barrier",
+        ["ProjectileDontCare"] = "ProjectileDontCare",
+        ["ProjectileReact"] = "ProjectileReact",
+        ["ProjectileIgnore"] = "ProjectileIgnore"
+    },
+    ["MapMarkers"] = {
+        ["ProjectileReact"] = "PROJECTILE_REACT",
+        ["Barrier"] = "BARRIER",
+        ["Entity"] = "ENTITY",
+        ["EntityUniqueName"] = "UniqueName",
+        ["ProjectileDontCare"] = "PROJECTILE_DONTCARE",
+        ["ProjectileIgnore"] = "PROJECTILE_IGNORE"
+    }
+}
