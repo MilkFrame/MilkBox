f867bc74d480a3269b01a8dc4d1019f1ad8c1c87
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-22 02:36:21 +1100

    Create UserSettingsConfig.lua

---
+++ GameData/The Mad Murderer X [8317834514]/UserSettingsConfig.lua
@@ -0,0 +1,87 @@
+data = {
+    ["Options"] = {
+        {
+            ["Type"] = "Mode",
+            ["Name"] = "AnonymousMode",
+            ["Modes"] = {"Off", "On"},
+            ["DefaultValue"] = "Off",
+            ["DisplayName"] = "Anonymous Mode"
+        },
+        {
+            ["Type"] = "Mode",
+            ["Name"] = "Ragdolls",
+            ["Modes"] = {"On", "Off"},
+            ["DefaultValue"] = "On",
+            ["DisplayName"] = "Ragdolls"
+        },
+        {
+            ["Type"] = "Mode",
+            ["Name"] = "AutoStack",
+            ["Modes"] = {"On", "Off"},
+            ["DefaultValue"] = "Off",
+            ["DisplayName"] = "Auto unbox stack"
+        },
+        {
+            ["Type"] = "Mode",
+            ["Name"] = "DontHidePets",
+            ["Modes"] = {"Off", "On"},
+            ["DefaultValue"] = "Off",
+            ["DisplayName"] = "Don't hide pets"
+        },
+        {
+            ["Type"] = "Mode",
+            ["Name"] = "TradeNotifications",
+            ["Modes"] = {"Everyone", "Friends", "None"},
+            ["DefaultValue"] = "Everyone",
+            ["DisplayName"] = "Trade Notifications"
+        },
+        {
+            ["Type"] = "Mode",
+            ["Name"] = "InventoryView",
+            ["Modes"] = {"Everyone", "Friends", "Hidden"},
+            ["DefaultValue"] = "Everyone",
+            ["DisplayName"] = "Inventory View"
+        },
+        {["DefaultValue"] = 0.5, ["Type"] = "Float", ["Name"] = "Music", ["DisplayName"] = "Music"},
+        {["DefaultValue"] = 0.5, ["Type"] = "Float", ["Name"] = "SoundEffects", ["DisplayName"] = "Sound Effects"},
+        {["DefaultValue"] = 0.5, ["Type"] = "Float", ["Name"] = "UISounds", ["DisplayName"] = "UI Sounds"},
+        {["DefaultValue"] = 0.5, ["Type"] = "Float", ["Name"] = "CombatSounds", ["DisplayName"] = "Combat Sounds"},
+        {["DefaultValue"] = 0.5, ["Type"] = "Float", ["Name"] = "Emotes", ["DisplayName"] = "Emotes"},
+        {
+            ["Type"] = "Mode",
+            ["Name"] = "WeaponHints",
+            ["Modes"] = {"On", "Off"},
+            ["DefaultValue"] = "On",
+            ["DisplayName"] = "Weapon Hints"
+        },
+        {
+            ["Type"] = "Mode",
+            ["Name"] = "AutoStab",
+            ["Modes"] = {"On", "Off"},
+            ["DefaultValue"] = "On",
+            ["DisplayName"] = "Auto-stab (mobile)"
+        },
+        {
+            ["Type"] = "Mode",
+            ["Name"] = "AutoJump",
+            ["Modes"] = {"On", "Off"},
+            ["DefaultValue"] = "Off",
+            ["DisplayName"] = "Auto jump (mobile)"
+        },
+        {
+            ["Type"] = "Mode",
+            ["Name"] = "AimAssist",
+            ["Modes"] = {"On", "Off"},
+            ["DefaultValue"] = "On",
+            ["DisplayName"] = "Aim-assist"
+        },
+        {
+            ["Type"] = "Mode",
+            ["Name"] = "StatTrack",
+            ["Modes"] = {"On", "Off"},
+            ["DefaultValue"] = "On",
+            ["DisplayName"] = "Stat tracking on items"
+        }
+    },
+    ["GetOption"] = "function: 0x7c281fc97c6e9967"
+}
