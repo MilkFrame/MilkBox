c42790f73ccdc35ab70e62fcdfd30390a222eb18
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-26 02:49:50 +1100

    Create Events.lua

---
+++ GameData/Swordburst 3 [11523257493]/Events.lua
@@ -0,0 +1,25 @@
+data = {
+    ["ClaimReward"] = "function: 0xb9a9a55e74593991",
+    ["CanClaimReward"] = "function: 0xddea79206a5135e1",
+    ["REWARDS"] = {
+        {["Amount"] = 1, ["Item"] = "Shadowmourne", ["ReqItemAmount"] = 50},
+        {["Amount"] = 1, ["Item"] = "Doomthorn", ["ReqItemAmount"] = 100},
+        {["Amount"] = 1, ["Item"] = "Soulrender", ["ReqItemAmount"] = 150},
+        {["Amount"] = 1, ["Item"] = "Gravepiercer", ["ReqItemAmount"] = 200},
+        {["Amount"] = 1, ["Item"] = "Gravedigger", ["ReqItemAmount"] = 250},
+        {["Amount"] = 1, ["Item"] = "Wraithcaller", ["ReqItemAmount"] = 300},
+        {["Amount"] = 1, ["Item"] = "Nightgrave", ["ReqItemAmount"] = 350},
+        {["Amount"] = 1, ["Item"] = "HallowFishingRod", ["ReqItemAmount"] = 400},
+        {["Dye"] = "Random", ["Amount"] = 1, ["Item"] = "PumpkinKing", ["ReqItemAmount"] = 450},
+        {["Amount"] = 1, ["Item"] = "UndeadHorse", ["ReqItemAmount"] = 500}
+    },
+    ["Init"] = "function: 0x2fb031837c254301",
+    ["GetRequiredItem"] = "function: 0xc854075db4740961",
+    ["GetRewards"] = "function: 0xce452999abfd4441",
+    ["GiveAllRewards"] = "function: 0xc9fc9f23314c4ec1",
+    ["HasClaimedReward"] = "function: 0xeaef4931c0f5d4a1",
+    ["Start"] = "function: 0x0b849d0bbf19d9e1",
+    ["GetCurrentReward"] = "function: 0x66db516594878401",
+    ["SpawnEventPoints"] = "function: 0x2120ae161d6eee81",
+    ["GetEventEndTime"] = "function: 0x90c879acf57525c1"
+}
