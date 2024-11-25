7e769da6449f3da4f95317bef07d1d8286b189c7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-26 02:47:52 +1100

    Create FloorData.lua

---
+++ GameData/Swordburst 3 [11523257493]/FloorData.lua
@@ -0,0 +1,148 @@
+data = {
+    ["Floor3"] = {
+        ["Badge"] = {["DevID"] = 0, ["LiveID"] = 1059678527066805},
+        ["Image"] = "rbxassetid://16371711204",
+        ["Title"] = "Floor 3",
+        ["LayoutOrder"] = 3,
+        ["LiveID"] = 16582687738,
+        ["DevID"] = 15703878046,
+        ["UnlockCondition"] = "Clear Floor 2 to Unlock",
+        ["Description"] = "Arctic Expanse"
+    },
+    ["Guilds"] = {
+        ["LayoutOrder"] = 0,
+        ["DoNotSavePlayerLocation"] = true,
+        ["Description"] = "Guild member boss arena.",
+        ["DevID"] = 17490505313,
+        ["DoNotDisplay"] = true,
+        ["Image"] = "rbxassetid://14546207864",
+        ["Title"] = "Guild Arena",
+        ["HideQuestHUD"] = true,
+        ["DoNotReturnOnShutdown"] = true,
+        ["LiveID"] = 17856910664
+    },
+    ["Floor7"] = {
+        ["Badge"] = {["DevID"] = 0, ["LiveID"] = 2719699167563700},
+        ["Image"] = "rbxassetid://71226367443298",
+        ["Title"] = "Floor 7",
+        ["LayoutOrder"] = 7,
+        ["LiveID"] = 137200996772584,
+        ["DevID"] = 17071448674,
+        ["UnlockCondition"] = "Clear Floor 6 to Unlock",
+        ["Description"] = "Celestia"
+    },
+    ["IcyInfernum"] = {
+        ["IsDungeon"] = true,
+        ["DoNotSavePlayerLocation"] = true,
+        ["Description"] = "Icy Infernum raid.",
+        ["DevID"] = 18519784520,
+        ["DoNotDisplay"] = true,
+        ["Image"] = "",
+        ["Title"] = "Icy Infernum",
+        ["HideQuestHUD"] = true,
+        ["DoNotReturnOnShutdown"] = true,
+        ["LiveID"] = 18972110699
+    },
+    ["SoftShutdown"] = {["Locked"] = true, ["LiveID"] = 16468939104, ["DevID"] = 16467459533, ["DoNotDisplay"] = true},
+    ["Floor6"] = {
+        ["Badge"] = {["DevID"] = 0, ["LiveID"] = 358460270379375},
+        ["Image"] = "rbxassetid://18757079044",
+        ["Title"] = "Floor 6",
+        ["LayoutOrder"] = 6,
+        ["LiveID"] = 18771428196,
+        ["DevID"] = 17071448321,
+        ["UnlockCondition"] = "Clear Floor 5 to Unlock",
+        ["Description"] = "Treasure Cove"
+    },
+    ["Floor5"] = {
+        ["Badge"] = {["DevID"] = 0, ["LiveID"] = 861352186796828},
+        ["Image"] = "rbxassetid://18134484671",
+        ["Title"] = "Floor 5",
+        ["LayoutOrder"] = 5,
+        ["LiveID"] = 18147617256,
+        ["DevID"] = 17071448026,
+        ["UnlockCondition"] = "Clear Floor 4 to Unlock",
+        ["Description"] = "Enchanted Grove"
+    },
+    ["Floor4"] = {
+        ["Badge"] = {["DevID"] = 0, ["LiveID"] = 1772401420364692},
+        ["Image"] = "rbxassetid://17208885123",
+        ["Title"] = "Floor 4",
+        ["LayoutOrder"] = 4,
+        ["LiveID"] = 17206024999,
+        ["DevID"] = 16582666724,
+        ["UnlockCondition"] = "Clear Floor 3 to Unlock",
+        ["Description"] = "Dune Oasis"
+    },
+    ["CrystalCavern"] = {
+        ["IsDungeon"] = true,
+        ["DoNotSavePlayerLocation"] = true,
+        ["Description"] = "Crystal Cavern raid.",
+        ["DevID"] = 18111994260,
+        ["DoNotDisplay"] = true,
+        ["Image"] = "",
+        ["Title"] = "Crystal Cavern",
+        ["HideQuestHUD"] = true,
+        ["DoNotReturnOnShutdown"] = true,
+        ["LiveID"] = 18351082380
+    },
+    ["Town"] = {
+        ["LayoutOrder"] = 0,
+        ["Image"] = "rbxassetid://14546207864",
+        ["Title"] = "New Arcadia",
+        ["HideQuestHUD"] = true,
+        ["LiveID"] = 14819493140,
+        ["DevID"] = 14357831473,
+        ["Description"] = "A place to shop, trade, duel and relax with other adventurers"
+    },
+    ["Floor1"] = {
+        ["Badge"] = {["DevID"] = 2347922691010667, ["LiveID"] = 1615268444747580},
+        ["Image"] = "rbxassetid://14546164954",
+        ["Title"] = "Floor 1",
+        ["LayoutOrder"] = 1,
+        ["LiveID"] = 14819490378,
+        ["DevID"] = 13500789020,
+        ["Description"] = "Gemstone Woodlands"
+    },
+    ["TowerHub"] = {
+        ["LayoutOrder"] = 0,
+        ["DoNotSavePlayerLocation"] = true,
+        ["Description"] = "Rally up with other adventurers to take on the tower.",
+        ["DevID"] = 16872591443,
+        ["DoNotDisplay"] = true,
+        ["Image"] = "rbxassetid://14546207864",
+        ["Title"] = "Void Tower",
+        ["HideQuestHUD"] = true,
+        ["LiveID"] = 16928265611
+    },
+    ["HalloweenRaid"] = {
+        ["IsDungeon"] = true,
+        ["DoNotSavePlayerLocation"] = true,
+        ["Description"] = "Halloween raid.",
+        ["DevID"] = 85593274004070,
+        ["DoNotDisplay"] = true,
+        ["Image"] = "",
+        ["Title"] = "Halloween Raid",
+        ["HideQuestHUD"] = true,
+        ["DoNotReturnOnShutdown"] = true,
+        ["LiveID"] = 71680992787051
+    },
+    ["DevPlace"] = {["Locked"] = true, ["LiveID"] = 0, ["DevID"] = 10910887523, ["DoNotDisplay"] = true},
+    ["Lobby"] = {
+        ["LiveID"] = 11523257493,
+        ["Title"] = "Main Menu",
+        ["Description"] = "Swordburst 3",
+        ["DevID"] = 11753465531,
+        ["DoNotDisplay"] = true
+    },
+    ["Floor2"] = {
+        ["Badge"] = {["DevID"] = 0, ["LiveID"] = 2913095290059043},
+        ["Image"] = "rbxassetid://15688616999",
+        ["Title"] = "Floor 2",
+        ["LayoutOrder"] = 2,
+        ["LiveID"] = 15695929915,
+        ["DevID"] = 14896130071,
+        ["UnlockCondition"] = "Clear Floor 1 to Unlock",
+        ["Description"] = "Redveil Valley"
+    }
+}
