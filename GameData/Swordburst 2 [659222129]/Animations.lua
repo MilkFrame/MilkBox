ac0e237fd2a36e0fe7022200f74383cc680ffacf
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-26 02:45:25 +1100

    Create Animations.lua

---
+++ GameData/Swordburst 2 [659222129]/Animations.lua
@@ -0,0 +1,104 @@
+data = {
+    ["Ninja"] = {
+        ["Swing3"] = "rbxassetid://1060428885",
+        ["Swing1"] = "rbxassetid://1060248930",
+        ["Sheath"] = "rbxassetid://580129097",
+        ["Swing2"] = "rbxassetid://1060353316",
+        ["Unsheath"] = "rbxassetid://580131025"
+    },
+    ["Misc"] = {
+        ["DrinkSip"] = "",
+        ["Mount4Legged"] = "",
+        ["ForwardRoll"] = "rbxassetid://658909007",
+        ["DrinkHold"] = "",
+        ["CommonBlock"] = "rbxassetid://704713687"
+    },
+    ["SwissSabre"] = {
+        ["Swing3"] = "rbxassetid://18788791948",
+        ["Swing1"] = "rbxassetid://18788775439",
+        ["Swing4"] = "rbxassetid://18788795072",
+        ["Sheath"] = "rbxassetid://598842103",
+        ["Swing2"] = "rbxassetid://18788786028",
+        ["Unsheath"] = "rbxassetid://598845042"
+    },
+    ["2HSword"] = {
+        ["Swing2"] = "rbxassetid://582148607",
+        ["DownwardSmash"] = "rbxassetid://733503899",
+        ["Swing1"] = "rbxassetid://582146725",
+        ["Sheath"] = "rbxassetid://591055283",
+        ["Swing3"] = "rbxassetid://582150541",
+        ["Unsheath"] = "rbxassetid://591058686"
+    },
+    ["Berserker"] = {
+        ["Swing2"] = "rbxassetid://723026423",
+        ["Swing1"] = "rbxassetid://721175711",
+        ["Sheath"] = "rbxassetid://730339716",
+        ["Swing3"] = "rbxassetid://723027242",
+        ["Unsheath"] = "rbxassetid://730325787"
+    },
+    ["Daggers"] = {["Swing2"] = "", ["Swing3"] = "", ["Idle"] = "", ["Swing1"] = ""},
+    ["DualWield"] = {
+        ["Swing2"] = "rbxassetid://574375509",
+        ["InfinitySlash"] = "rbxassetid://2723618132",
+        ["Swing1"] = "rbxassetid://574373512",
+        ["Swing3"] = "rbxassetid://577311854",
+        ["Sheath"] = "rbxassetid://560987012",
+        ["WhirlwindSpin"] = "rbxassetid://783648710",
+        ["Unsheath"] = "rbxassetid://560931153"
+    },
+    ["Spear"] = {
+        ["Swing2"] = "rbxassetid://13965851874",
+        ["Swing1"] = "rbxassetid://13965847160",
+        ["Sheath"] = "rbxassetid://14157744204",
+        ["Swing3"] = "rbxassetid://13965854276",
+        ["Unsheath"] = "rbxassetid://14157767492"
+    },
+    ["SingleSword"] = {
+        ["Swing2"] = "rbxassetid://598607508",
+        ["Swing1"] = "rbxassetid://598603097",
+        ["SweepingStrike"] = "rbxassetid://734903427",
+        ["Sheath"] = "rbxassetid://599925671",
+        ["Swing3"] = "rbxassetid://598610106",
+        ["Unsheath"] = "rbxassetid://599929317"
+    },
+    ["Noble"] = {
+        ["Swing3"] = "rbxassetid://1154472151",
+        ["Swing1"] = "rbxassetid://1154469606",
+        ["Sheath"] = "rbxassetid://599925671",
+        ["Swing2"] = "rbxassetid://1154471188",
+        ["Unsheath"] = "rbxassetid://599929317"
+    },
+    ["Rapier"] = {
+        ["Swing2"] = "rbxassetid://598768781",
+        ["Swing1"] = "rbxassetid://598767023",
+        ["PiercingDash"] = "rbxassetid://738749572",
+        ["Sheath"] = "rbxassetid://598842103",
+        ["Swing3"] = "rbxassetid://598770829",
+        ["Unsheath"] = "rbxassetid://598845042"
+    },
+    ["Dagger"] = {["Swing2"] = "", ["Swing3"] = "", ["Idle"] = "", ["Swing1"] = ""},
+    ["Vigilante"] = {
+        ["Swing3"] = "rbxassetid://1134710633",
+        ["Swing1"] = "rbxassetid://1129153018",
+        ["Sheath"] = "rbxassetid://560987012",
+        ["Swing2"] = "rbxassetid://1129176584",
+        ["Unsheath"] = "rbxassetid://560931153"
+    },
+    ["Swiftstrike"] = {
+        ["Swing2"] = "rbxassetid://115077919638017",
+        ["Swing1"] = "rbxassetid://74705935200801",
+        ["Swing4"] = "rbxassetid://103785224525184",
+        ["Sheath"] = "rbxassetid://14157744204",
+        ["Swing3"] = "rbxassetid://100403158382315",
+        ["Unsheath"] = "rbxassetid://14157767492"
+    },
+    ["SwordShield"] = {["Swing3"] = "", ["Swing2"] = "", ["Idle"] = "", ["Swing1"] = ""},
+    ["Katana"] = {
+        ["Swing2"] = "rbxassetid://578067290",
+        ["Swing1"] = "rbxassetid://578065564",
+        ["LeapingSlash"] = "rbxassetid://736153032",
+        ["Sheath"] = "rbxassetid://580129097",
+        ["Swing3"] = "rbxassetid://578070634",
+        ["Unsheath"] = "rbxassetid://580131025"
+    }
+}
