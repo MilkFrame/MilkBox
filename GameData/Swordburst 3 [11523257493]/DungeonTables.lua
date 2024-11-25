d83012adba62e0838b0b740c64fcc14138578603
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-26 02:48:30 +1100

    Create DungeonTables.lua

---
+++ GameData/Swordburst 3 [11523257493]/DungeonTables.lua
@@ -0,0 +1,132 @@
+data = {
+    ["CrystalCavern"] = {
+        ["DropTables"] = {
+            {
+                ["SlateGreatsword"] = 0.1,
+                ["LuminaEdge"] = 0.1,
+                ["UpgradeCrystalLegendary"] = 0.5,
+                ["EnchantingStone"] = 0.5
+            },
+            {
+                ["SlateGreatsword"] = 0.11,
+                ["LuminaEdge"] = 0.11,
+                ["UpgradeCrystalLegendary"] = 0.6,
+                ["EnchantingStone"] = 0.6
+            },
+            {
+                ["LuminaEdge"] = 0.11,
+                ["UpgradeCrystalLegendary"] = 0.6,
+                ["SlateGreatsword"] = 0.11,
+                ["EnchantingStone"] = 0.6,
+                ["GoldPickaxe"] = 0.05
+            },
+            {
+                ["LuminaEdge"] = 0.12,
+                ["UpgradeCrystalLegendary"] = 0.7,
+                ["SlateGreatsword"] = 0.12,
+                ["EnchantingStone"] = 0.7,
+                ["GoldPickaxe"] = 0.06
+            },
+            {
+                ["LuminaEdge"] = 0.12,
+                ["UpgradeCrystalLegendary"] = 0.7,
+                ["SlateGreatsword"] = 0.12,
+                ["FieryCloak"] = 0.01,
+                ["EnchantingStone"] = 0.7,
+                ["GoldPickaxe"] = 0.06
+            },
+            {
+                ["LuminaEdge"] = 0.12,
+                ["UpgradeCrystalLegendary"] = 0.7,
+                ["FieryCloak"] = 0.01,
+                ["SlateGreatsword"] = 0.12,
+                ["Darkheart"] = 0.01,
+                ["EnchantingStone"] = 0.7,
+                ["GoldPickaxe"] = 0.06
+            }
+        },
+        ["Image"] = "rbxassetid://18320003377",
+        ["Title"] = "Crystal Cavern",
+        ["EntryCooldown"] = 60,
+        ["Color"] = Color3.fromRGB(0, 251, 255),
+        ["EntryType"] = "Key",
+        ["DifficultyLevels"] = {
+            {["Min"] = 60, ["Max"] = 85},
+            {["Min"] = 70, ["Max"] = 95},
+            {["Min"] = 80, ["Max"] = 105},
+            {["Min"] = 90, ["Max"] = 115},
+            {["Min"] = 100, ["Max"] = 125},
+            {["Min"] = 125, ["Max"] = 150}
+        },
+        ["EntryKeyItem"] = "CrystalKey"
+    },
+    ["HalloweenRaid"] = {
+        ["DropTables"] = {
+            {["HellHorse"] = 0.02, ["HalloweenToken"] = {["DropChance"] = 1, ["Count"] = 50}},
+            {
+                ["HalloweenCloak"] = 0.02,
+                ["HellHorse"] = 0.03,
+                ["HalloweenToken"] = {["DropChance"] = 1, ["Count"] = 150}
+            }
+        },
+        ["Color"] = Color3.fromRGB(255, 147, 39),
+        ["Image"] = "rbxassetid://99522128150449",
+        ["Title"] = "Halloween Raid",
+        ["AllDifficultiesUnlocked"] = true,
+        ["EntryCooldown"] = 0,
+        ["EntryType"] = "Open",
+        ["DifficultyLevels"] = {{["Min"] = 145, ["Max"] = 145}, {["Min"] = 300, ["Max"] = 300}},
+        ["EntryKeyItem"] = ""
+    },
+    ["IcyInfernum"] = {
+        ["DropTables"] = {
+            {["BlueInferno"] = 0.1, ["EnchantingStone"] = 0.5, ["UpgradeCrystalLegendary"] = 0.5, ["BlueBlaze"] = 0.1},
+            {["BlueInferno"] = 0.11, ["EnchantingStone"] = 0.6, ["UpgradeCrystalLegendary"] = 0.6, ["BlueBlaze"] = 0.11},
+            {
+                ["UpgradeCrystalLegendary"] = 0.6,
+                ["BlueInferno"] = 0.11,
+                ["BlueFlamePickaxe"] = 0.05,
+                ["EnchantingStone"] = 0.6,
+                ["BlueBlaze"] = 0.11
+            },
+            {
+                ["UpgradeCrystalLegendary"] = 0.7,
+                ["BlueInferno"] = 0.12,
+                ["BlueFlamePickaxe"] = 0.06,
+                ["EnchantingStone"] = 0.7,
+                ["BlueBlaze"] = 0.12
+            },
+            {
+                ["UpgradeCrystalLegendary"] = 0.7,
+                ["InfernumCloak"] = 0.01,
+                ["BlueInferno"] = 0.12,
+                ["BlueFlamePickaxe"] = 0.06,
+                ["EnchantingStone"] = 0.7,
+                ["BlueBlaze"] = 0.12
+            },
+            {
+                ["Frostbreaker"] = 0.01,
+                ["UpgradeCrystalLegendary"] = 0.7,
+                ["InfernumCloak"] = 0.01,
+                ["BlueInferno"] = 0.12,
+                ["BlueFlamePickaxe"] = 0.06,
+                ["EnchantingStone"] = 0.7,
+                ["BlueBlaze"] = 0.12
+            }
+        },
+        ["Image"] = "rbxassetid://18922532035",
+        ["Title"] = "Icy Infernum",
+        ["EntryCooldown"] = 60,
+        ["Color"] = Color3.fromRGB(2, 103, 255),
+        ["EntryType"] = "Key",
+        ["DifficultyLevels"] = {
+            {["Min"] = 60, ["Max"] = 85},
+            {["Min"] = 70, ["Max"] = 95},
+            {["Min"] = 80, ["Max"] = 105},
+            {["Min"] = 90, ["Max"] = 115},
+            {["Min"] = 100, ["Max"] = 125},
+            {["Min"] = 125, ["Max"] = 150}
+        },
+        ["EntryKeyItem"] = "IcyKey"
+    }
+}
