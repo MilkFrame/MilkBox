229200f0bbd5102dd9f51c971148cb1d24370a9a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-26 02:49:38 +1100

    Create Titles.lua

---
+++ GameData/Swordburst 3 [11523257493]/Titles.lua
@@ -0,0 +1,75 @@
+--require(game.ReplicatedStorage.Systems.Titles).GetTitleList()
+data = {
+    ["IcyInfernumTitle"] = {
+        ["Rotation"] = 90,
+        ["Description"] = "Earned by completing Icy Infernum on Nightmare Difficulty!",
+        ["Category"] = "Title",
+        ["Rarity"] = 5,
+        ["Gradient"] = "0 0 0.666667 1 0 1 0 0.333333 1 0 ",
+        ["Icon"] = "rbxassetid://75578316562845",
+        ["DisplayName"] = "Frostborn"
+    },
+    ["HalloweenRaidTitle"] = {
+        ["Rotation"] = 90,
+        ["Description"] = "Earned by completing the Halloween Raid on Nightmare difficulty!",
+        ["Category"] = "Title",
+        ["Rarity"] = 5,
+        ["Gradient"] = "0 1 0 0 0 1 0.533333 0 0 0 ",
+        ["Icon"] = "rbxassetid://75578316562845",
+        ["DisplayName"] = "Reaper"
+    },
+    ["SlayerWolfTitle"] = {
+        ["Rotation"] = 90,
+        ["Description"] = "Earned by completing Wolf Slayer Rewards!",
+        ["Category"] = "Title",
+        ["Rarity"] = 5,
+        ["Gradient"] = "0 0.47451 0.615686 1 0 1 0.333333 0.333333 1 0 ",
+        ["Icon"] = "rbxassetid://75578316562845",
+        ["DisplayName"] = "Lone Wolf"
+    },
+    ["SlayerRaptorTitle"] = {
+        ["Rotation"] = 90,
+        ["Description"] = "Earned by completing Raptor Slayer Rewards!",
+        ["Category"] = "Title",
+        ["Rarity"] = 5,
+        ["Gradient"] = "0 1 0.333333 0 0 1 1 1 1 0 ",
+        ["Icon"] = "rbxassetid://75578316562845",
+        ["DisplayName"] = "Jurassic"
+    },
+    ["HalloweenTitle"] = {
+        ["Rotation"] = 90,
+        ["Description"] = "Earned by completing all the quests from the Halloween 2024 event!",
+        ["Category"] = "Title",
+        ["Rarity"] = 5,
+        ["Gradient"] = "0 1 0.341176 0.0117647 0 0.4 1 0.321569 0.00784314 0 1 1 1 1 0 ",
+        ["Icon"] = "rbxassetid://75578316562845",
+        ["DisplayName"] = "Halloween Hero"
+    },
+    ["SlayerHellhoundTitle"] = {
+        ["Rotation"] = 90,
+        ["Description"] = "Earned by completing Hellhound Slayer Rewards!",
+        ["Category"] = "Title",
+        ["Rarity"] = 5,
+        ["Gradient"] = "0 1 0.333333 0 0 1 1 0 0 0 ",
+        ["Icon"] = "rbxassetid://75578316562845",
+        ["DisplayName"] = "Hellfire"
+    },
+    ["CrystalCavernTitle"] = {
+        ["Rotation"] = 90,
+        ["Description"] = "Earned by completing Crystal Cavern on Nightmare Difficulty!",
+        ["Category"] = "Title",
+        ["Rarity"] = 5,
+        ["Gradient"] = "0 0 1 1 0 1 0.666667 1 1 0 ",
+        ["Icon"] = "rbxassetid://75578316562845",
+        ["DisplayName"] = "Swordmaster"
+    },
+    ["AstralProtectorTitle"] = {
+        ["Rotation"] = 90,
+        ["Description"] = "Earned by completing the main story quests on Floor 7!",
+        ["Category"] = "Title",
+        ["Rarity"] = 5,
+        ["Gradient"] = "0 0.466667 0 1 0 0.3 0.501961 0 1 0 1 0 0.780392 0.937255 0 ",
+        ["Icon"] = "rbxassetid://75578316562845",
+        ["DisplayName"] = "Astral Protector"
+    }
+}
