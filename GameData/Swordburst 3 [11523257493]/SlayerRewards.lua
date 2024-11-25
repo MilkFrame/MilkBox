480a61d77df5b370489eb7d3d2350bdcf6816ffc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-26 02:49:16 +1100

    Create SlayerRewards.lua

---
+++ GameData/Swordburst 3 [11523257493]/SlayerRewards.lua
@@ -0,0 +1,79 @@
+data = {
+    ["ClaimReward"] = "function: 0x2288e9e24048b041",
+    ["CanClaimReward"] = "function: 0x442509a7deb8f931",
+    ["REWARDS"] = {
+        {
+            ["Rewards"] = {
+                {["Amount"] = 1, ["Item"] = "WolvesBane", ["ReqItemAmount"] = 5},
+                {["Amount"] = 1, ["Item"] = "WolvesMaw", ["ReqItemAmount"] = 10},
+                {["Amount"] = 1, ["Item"] = "WolfHunter", ["ReqItemAmount"] = 15},
+                {["Amount"] = 1, ["Item"] = "WolvesClaw", ["ReqItemAmount"] = 20},
+                {["Amount"] = 1, ["Item"] = "WolfFishingRod", ["ReqItemAmount"] = 25},
+                {["Amount"] = 1, ["Item"] = "TransmogTicket", ["ReqItemAmount"] = 30},
+                {["Dye"] = "Random", ["Amount"] = 1, ["Item"] = "DireHunter", ["ReqItemAmount"] = 35},
+                {["Amount"] = 1, ["Item"] = "TransmogTicket", ["ReqItemAmount"] = 40},
+                {["Amount"] = 1, ["Item"] = "AuraTicket", ["ReqItemAmount"] = 45},
+                {["Amount"] = 1, ["Item"] = "MountWolfgang", ["ReqItemAmount"] = 50}
+            },
+            ["TargetMob"] = "Wolf",
+            ["ReqItem"] = "WolfTooth"
+        },
+        {
+            ["Rewards"] = {
+                {["Amount"] = 1, ["Item"] = "RaptorRapier", ["ReqItemAmount"] = 5},
+                {["Amount"] = 1, ["Item"] = "RaptorScythe", ["ReqItemAmount"] = 10},
+                {["Amount"] = 1, ["Item"] = "RaptorArmor", ["ReqItemAmount"] = 15},
+                {["Amount"] = 1, ["Item"] = "RaptorSpear", ["ReqItemAmount"] = 20},
+                {["Amount"] = 1, ["Item"] = "RaptorPickaxe", ["ReqItemAmount"] = 25},
+                {["Amount"] = 1, ["Item"] = "TransmogTicket", ["ReqItemAmount"] = 30},
+                {["Dye"] = "Random", ["Amount"] = 1, ["Item"] = "PrimalBerserker", ["ReqItemAmount"] = 35},
+                {["Amount"] = 1, ["Item"] = "TransmogTicket", ["ReqItemAmount"] = 40},
+                {["Amount"] = 1, ["Item"] = "AuraTicket", ["ReqItemAmount"] = 45},
+                {["Amount"] = 1, ["Item"] = "MountAldebaran", ["ReqItemAmount"] = 50}
+            },
+            ["TargetMob"] = "Raptor",
+            ["ReqItem"] = "RaptorClaws"
+        },
+        {
+            ["Rewards"] = {
+                {["Amount"] = 1, ["Item"] = "HellhoundSpear", ["ReqItemAmount"] = 5},
+                {["Amount"] = 1, ["Item"] = "HellhoundArmor", ["ReqItemAmount"] = 10},
+                {["Amount"] = 1, ["Item"] = "HellhoundLongsword", ["ReqItemAmount"] = 15},
+                {["Amount"] = 1, ["Item"] = "HellhoundScythe", ["ReqItemAmount"] = 20},
+                {["Amount"] = 1, ["Item"] = "HellhoundPickaxe", ["ReqItemAmount"] = 25},
+                {["Amount"] = 1, ["Item"] = "TransmogTicket", ["ReqItemAmount"] = 30},
+                {["Dye"] = "Random", ["Amount"] = 1, ["Item"] = "HellraiserArmor", ["ReqItemAmount"] = 35},
+                {["Amount"] = 1, ["Item"] = "TransmogTicket", ["ReqItemAmount"] = 40},
+                {["Amount"] = 1, ["Item"] = "AuraTicket", ["ReqItemAmount"] = 45},
+                {["Amount"] = 1, ["Item"] = "MountVexis", ["ReqItemAmount"] = 50}
+            },
+            ["TargetMob"] = "Hell Hound",
+            ["ReqItem"] = "HellTail"
+        },
+        {
+            ["Rewards"] = {
+                {["Amount"] = 1, ["Item"] = "AxolotlGreatsword", ["ReqItemAmount"] = 5},
+                {["Amount"] = 1, ["Item"] = "AxolotlRapier", ["ReqItemAmount"] = 10},
+                {["Amount"] = 1, ["Item"] = "AxolotlArmor", ["ReqItemAmount"] = 15},
+                {["Amount"] = 1, ["Item"] = "AxolotlKatana", ["ReqItemAmount"] = 20},
+                {["Amount"] = 1, ["Item"] = "AxolotlFishingRod", ["ReqItemAmount"] = 25},
+                {["Amount"] = 1, ["Item"] = "TransmogTicket", ["ReqItemAmount"] = 30},
+                {["Dye"] = "Random", ["Amount"] = 1, ["Item"] = "AxolotlCostume", ["ReqItemAmount"] = 35},
+                {["Amount"] = 1, ["Item"] = "TransmogTicket", ["ReqItemAmount"] = 40},
+                {["Amount"] = 1, ["Item"] = "AuraTicket", ["ReqItemAmount"] = 45},
+                {["Amount"] = 1, ["Item"] = "PinkAxolotl", ["ReqItemAmount"] = 50}
+            },
+            ["TargetMob"] = "Axolotl",
+            ["ReqItem"] = "AxolotlScales"
+        }
+    },
+    ["Init"] = "function: 0x99f4e2f2dd1cda01",
+    ["TARGET_MOB_OVERRIDES"] = {4},
+    ["GetRewards"] = "function: 0x763a5b62c003e681",
+    ["GiveAllRewards"] = "function: 0xcfb643aa99370fc1",
+    ["GetCurrentReward"] = "function: 0x244492ba45a5a281",
+    ["Start"] = "function: 0xf794840a8ce6cbe1",
+    ["GetTargetMob"] = "function: 0xd2adb7d4dda06341",
+    ["HasClaimedReward"] = "function: 0x9cdf587d69449ce1",
+    ["ResetRewards"] = "function: 0x538b7bc94b363051"
+}
