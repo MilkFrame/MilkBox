aa4d51e395afceb3cc1a337e04161eb70395042a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-26 02:48:56 +1100

    Create AchievementList.lua

---
+++ GameData/Swordburst 3 [11523257493]/AchievementList.lua
@@ -0,0 +1,209 @@
+data = {
+    {["Type"] = "ReachLevel", ["Goal"] = 10, ["GoalAmount"] = 1, ["RewardAmount"] = 10, ["Reward"] = "Gems"},
+    {
+        ["Type"] = "ReachLevel",
+        ["Goal"] = 25,
+        ["Prerequisite"] = 1,
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 10,
+        ["Reward"] = "Gems"
+    },
+    {
+        ["Type"] = "ReachLevel",
+        ["Goal"] = 50,
+        ["Prerequisite"] = 2,
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 10,
+        ["Reward"] = "Gems"
+    },
+    {["Type"] = "ReachFloor", ["Goal"] = "Floor2", ["GoalAmount"] = 1, ["RewardAmount"] = 25, ["Reward"] = "Gems"},
+    {
+        ["Type"] = "ReachFloor",
+        ["Goal"] = "Floor3",
+        ["Prerequisite"] = 4,
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 25,
+        ["Reward"] = "Gems"
+    },
+    {["Type"] = "UpgradeGear", ["Goal"] = "UpgradeGear", ["GoalAmount"] = 1, ["RewardAmount"] = 10, ["Reward"] = "Gems"},
+    {
+        ["Type"] = "UpgradeGear",
+        ["Goal"] = "UpgradeGear",
+        ["Prerequisite"] = 6,
+        ["GoalAmount"] = 10,
+        ["RewardAmount"] = 15,
+        ["Reward"] = "Gems"
+    },
+    {["Type"] = "KillMobs", ["Goal"] = "Hagan", ["GoalAmount"] = 1, ["RewardAmount"] = 15, ["Reward"] = "Gems"},
+    {["Type"] = "KillMobs", ["Goal"] = "Galiard", ["GoalAmount"] = 1, ["RewardAmount"] = 15, ["Reward"] = "Gems"},
+    {["Type"] = "KillMobs", ["Goal"] = "Roaring Ogre", ["GoalAmount"] = 1, ["RewardAmount"] = 15, ["Reward"] = "Gems"},
+    {["Type"] = "KillMobs", ["Goal"] = "Erebus", ["GoalAmount"] = 1, ["RewardAmount"] = 15, ["Reward"] = "Gems"},
+    {["Type"] = "KillMobs", ["Goal"] = "Alpha Icewhal", ["GoalAmount"] = 1, ["RewardAmount"] = 15, ["Reward"] = "Gems"},
+    {["Type"] = "KillMobs", ["Goal"] = "Yeti", ["GoalAmount"] = 1, ["RewardAmount"] = 15, ["Reward"] = "Gems"},
+    {
+        ["Type"] = "KillMobs",
+        ["Goal"] = "Elron The Frenzy Lord",
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 50,
+        ["Reward"] = "Gems"
+    },
+    {
+        ["Type"] = "KillMobs",
+        ["Goal"] = "Pathos The Fire Lord",
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 50,
+        ["Reward"] = "Gems"
+    },
+    {
+        ["Type"] = "KillMobs",
+        ["Goal"] = "Elize The Siren",
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 50,
+        ["Reward"] = "Gems"
+    },
+    {
+        ["Type"] = "ReachLevel",
+        ["Goal"] = 75,
+        ["Prerequisite"] = 3,
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 10,
+        ["Reward"] = "Gems"
+    },
+    {["Type"] = "KillMobs", ["Goal"] = "Desert Spirit", ["GoalAmount"] = 1, ["RewardAmount"] = 15, ["Reward"] = "Gems"},
+    {["Type"] = "KillMobs", ["Goal"] = "Sand Terror", ["GoalAmount"] = 1, ["RewardAmount"] = 15, ["Reward"] = "Gems"},
+    {
+        ["Type"] = "ReachFloor",
+        ["Goal"] = "Floor4",
+        ["Prerequisite"] = 5,
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 25,
+        ["Reward"] = "Gems"
+    },
+    {
+        ["Type"] = "KillMobs",
+        ["Goal"] = "Nestor The Guardian",
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 50,
+        ["Reward"] = "Gems"
+    },
+    {
+        ["Type"] = "ReachLevel",
+        ["Goal"] = 100,
+        ["Prerequisite"] = 17,
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 10,
+        ["Reward"] = "Gems"
+    },
+    {
+        ["Type"] = "ReachFloor",
+        ["Goal"] = "Floor5",
+        ["Prerequisite"] = 20,
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 25,
+        ["Reward"] = "Gems"
+    },
+    {["Type"] = "KillMobs", ["Goal"] = "Cave Protector", ["GoalAmount"] = 1, ["RewardAmount"] = 15, ["Reward"] = "Gems"},
+    {["Type"] = "KillMobs", ["Goal"] = "Enchanted Owl", ["GoalAmount"] = 1, ["RewardAmount"] = 15, ["Reward"] = "Gems"},
+    {
+        ["Type"] = "KillMobs",
+        ["Goal"] = "Aurora The Tree Spirit",
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 50,
+        ["Reward"] = "Gems"
+    },
+    {
+        ["Type"] = "ReachFloor",
+        ["Goal"] = "Floor6",
+        ["Prerequisite"] = 23,
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 25,
+        ["Reward"] = "Gems"
+    },
+    {
+        ["Type"] = "ReachLevel",
+        ["Goal"] = 125,
+        ["Prerequisite"] = 22,
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 10,
+        ["Reward"] = "Gems"
+    },
+    {["Type"] = "KillMobs", ["Goal"] = "Sea Serpent", ["GoalAmount"] = 1, ["RewardAmount"] = 15, ["Reward"] = "Gems"},
+    {["Type"] = "KillMobs", ["Goal"] = "Tidal Nautilus", ["GoalAmount"] = 1, ["RewardAmount"] = 15, ["Reward"] = "Gems"},
+    {
+        ["Type"] = "KillMobs",
+        ["Goal"] = "Charon the Apex",
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 50,
+        ["Reward"] = "Gems"
+    },
+    {
+        ["Type"] = "SlayerReward",
+        ["Goal"] = "Hell Hound",
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 1,
+        ["Reward"] = "SlayerHellhoundTitle"
+    },
+    {
+        ["Type"] = "SlayerReward",
+        ["Goal"] = "Wolf",
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 1,
+        ["Reward"] = "SlayerWolfTitle"
+    },
+    {
+        ["Type"] = "SlayerReward",
+        ["Goal"] = "Raptor",
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 1,
+        ["Reward"] = "SlayerRaptorTitle"
+    },
+    {
+        ["Type"] = "ClearRaid",
+        ["Goal"] = "CrystalCavern",
+        ["RewardAmount"] = 1,
+        ["GoalAmount"] = 1,
+        ["SubGoal"] = 6,
+        ["Reward"] = "CrystalCavernTitle"
+    },
+    {
+        ["Type"] = "ClearRaid",
+        ["Goal"] = "IcyInfernum",
+        ["RewardAmount"] = 1,
+        ["GoalAmount"] = 1,
+        ["SubGoal"] = 6,
+        ["Reward"] = "IcyInfernumTitle"
+    },
+    {
+        ["Type"] = "CompleteQuest",
+        ["Goal"] = "243",
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 1,
+        ["Reward"] = "AstralProtectorTitle"
+    },
+    {
+        ["Type"] = "ReachFloor",
+        ["Goal"] = "Floor7",
+        ["Prerequisite"] = 27,
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 25,
+        ["Reward"] = "Gems"
+    },
+    {["Type"] = "KillMobs", ["Goal"] = "Callisto", ["GoalAmount"] = 1, ["RewardAmount"] = 15, ["Reward"] = "Gems"},
+    {["Type"] = "KillMobs", ["Goal"] = "Umbra", ["GoalAmount"] = 1, ["RewardAmount"] = 15, ["Reward"] = "Gems"},
+    {["Type"] = "KillMobs", ["Goal"] = "Altalune", ["GoalAmount"] = 1, ["RewardAmount"] = 50, ["Reward"] = "Gems"},
+    {
+        ["Type"] = "CompleteQuest",
+        ["Goal"] = "267",
+        ["GoalAmount"] = 1,
+        ["RewardAmount"] = 1,
+        ["Reward"] = "HalloweenTitle"
+    },
+    {
+        ["Type"] = "ClearRaid",
+        ["Goal"] = "HalloweenRaid",
+        ["RewardAmount"] = 1,
+        ["GoalAmount"] = 1,
+        ["SubGoal"] = 6,
+        ["Reward"] = "HalloweenRaidTitle"
+    }
+}
