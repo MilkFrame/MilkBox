8956018b7655a310682046b614279352fb64242a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-11-26 02:44:58 +1100

    Create Locations.lua

---
+++ GameData/Swordburst 2 [659222129]/Locations.lua
@@ -0,0 +1,100 @@
+data = {
+    ["floors"] = {
+        {["PlaceId"] = 540240728, ["Image"] = "rbxassetid://615523763", ["Name"] = "Arcadia", ["Category"] = "Main"},
+        {
+            ["PlaceId"] = 542351431,
+            ["Image"] = "rbxassetid://615506568",
+            ["Name"] = "Virhst Woodlands",
+            ["Category"] = "Main"
+        },
+        {
+            ["Image"] = "rbxassetid://825680829",
+            ["Name"] = "Battle Arena",
+            ["Category"] = "Main",
+            ["PlaceId"] = 737272595,
+            ["Unlocked"] = true
+        },
+        {
+            ["PlaceId"] = 548231754,
+            ["Image"] = "rbxassetid://730567278",
+            ["Name"] = "Redveil Grove",
+            ["Category"] = "Main"
+        },
+        {
+            ["PlaceId"] = 555980327,
+            ["Image"] = "rbxassetid://2154912275",
+            ["Name"] = "Avalanche Expanse",
+            ["Category"] = "Main"
+        },
+        {
+            ["PlaceId"] = 572487908,
+            ["Image"] = "rbxassetid://1079562005",
+            ["Name"] = "Hidden Wilds",
+            ["Category"] = "Main"
+        },
+        {
+            ["PlaceId"] = 580239979,
+            ["Image"] = "rbxassetid://1180881560",
+            ["Name"] = "Desolate Dunes",
+            ["Category"] = "Main"
+        },
+        {["PlaceId"] = 566212942, ["Image"] = "rbxassetid://1205788593", ["Name"] = "Helmfirth", ["Category"] = "Main"},
+        {
+            ["PlaceId"] = 582198062,
+            ["Image"] = "rbxassetid://1230453453",
+            ["Name"] = "Entoloma Gloomlands",
+            ["Category"] = "Main"
+        },
+        {
+            ["PlaceId"] = 548878321,
+            ["Image"] = "rbxassetid://1377821156",
+            ["Name"] = "Blooming Plateau",
+            ["Category"] = "Main"
+        },
+        {["PlaceId"] = 573267292, ["Image"] = "rbxassetid://2154937492", ["Name"] = "Va' Rok", ["Category"] = "Main"},
+        {
+            ["PlaceId"] = 2659143505,
+            ["Image"] = "rbxassetid://2676660892",
+            ["Name"] = "Transylvania",
+            ["Category"] = "Main"
+        },
+        {
+            ["PlaceId"] = 5287433115,
+            ["Image"] = "rbxassetid://5287398273",
+            ["Name"] = "Hypersiddia",
+            ["Category"] = "Main"
+        },
+        {
+            ["PlaceId"] = 6144637080,
+            ["Image"] = "rbxassetid://14180629039",
+            ["Name"] = "Sector - 235",
+            ["Category"] = "Main"
+        },
+        {
+            ["PlaceId"] = 13965775911,
+            ["Image"] = "rbxassetid://90844979757558",
+            ["Name"] = "Atheon",
+            ["Category"] = "Boss"
+        },
+        {
+            ["Image"] = "rbxassetid://16963160635",
+            ["Name"] = "Eternal Garden",
+            ["Category"] = "Rotating",
+            ["PlaceId"] = 16810524216,
+            ["Hidden"] = true
+        },
+        {
+            ["PlaceId"] = 18729767954,
+            ["Image"] = "rbxassetid://18788218658",
+            ["Name"] = "Glutton's Lair",
+            ["Category"] = "Rotating"
+        },
+        {
+            ["Image"] = "rbxassetid://121285527257744",
+            ["Name"] = "Spooky Hollow",
+            ["Category"] = "Event",
+            ["PlaceId"] = 11331145451,
+            ["Unlocked"] = true
+        }
+    }
+}
