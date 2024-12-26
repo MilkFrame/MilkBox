01873304e30ef51a5f68f223147de746bb3dc142
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-12-27 02:51:40 +1100

    Create Skills.lua

---
+++ GameData/Super Bomb Survival [164051105]/Skills.lua
@@ -0,0 +1,562 @@
+--Skills SPRITESHEET = rbxassetid://16031969701
+data = {
+    ["Defense"] = {7, 10, 11, 12, 16, 19, 20, 21, 23, 26, 50, 52, 57, 60, 64},
+    ["Info"] = {
+        {
+            ["type"] = "M",
+            ["name"] = "Super Jump",
+            ["image"] = "0, 0",
+            ["cooldown"] = {20, 20, 20, 21},
+            ["desc"] = "Jump super high, even in midair!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Bomb",
+            ["image"] = "1, 0",
+            ["cooldown"] = {15, 14, 14, 14},
+            ["desc"] = "The most basic of weapons, according to this game."
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Soccerbomb",
+            ["image"] = "2, 0",
+            ["cooldown"] = {19, 19, 19, 19},
+            ["desc"] = "A basic bomb, in a much more easy-to-kick format."
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Self-Destruct",
+            ["rarity"] = 0.5,
+            ["image"] = "3, 0",
+            ["cooldown"] = {12, 12, 12, 12},
+            ["desc"] = "Explode yourself, dealing damage to you and your surroundings."
+        },
+        {
+            ["type"] = "M",
+            ["name"] = "Super Dash",
+            ["image"] = "4, 0",
+            ["cooldown"] = {22, 20, 16, 12},
+            ["desc"] = "Gotta go fast! Gives a temporary speed boost."
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Black Hole",
+            ["rarity"] = 0.15,
+            ["image"] = "5, 0",
+            ["cooldown"] = {32, 32, 32, 32},
+            ["desc"] = "Drop a generator that spawns a temporary black hole."
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Stun Blast",
+            ["rarity"] = 0.85,
+            ["image"] = "6, 0",
+            ["cooldown"] = {20, 20, 20, 20},
+            ["desc"] = "Slow down your enemies with a burst of bright light!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Airstrike",
+            ["rarity"] = 0.5,
+            ["image"] = "7, 0",
+            ["cooldown"] = {24, 24, 20, 20},
+            ["desc"] = "Summon a basic missile to strike your position."
+        },
+        {
+            ["type"] = "M",
+            ["name"] = "Hover",
+            ["image"] = "8, 0",
+            ["cooldown"] = {20, 20, 20, 20},
+            ["desc"] = "Float around in the air majestically for a short time."
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Trip Pulse",
+            ["rarity"] = 0.45,
+            ["image"] = "9, 0",
+            ["cooldown"] = {14, 14, 14, 14},
+            ["desc"] = "Knock your enemies over with this confounding blast!"
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Confusion",
+            ["image"] = "0, 1",
+            ["cooldown"] = {22, 22, 21, 21},
+            ["desc"] = "This blast will temporarily disable enemies from using Skills."
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Blast Force",
+            ["image"] = "1, 1",
+            ["cooldown"] = {16, 16, 16, 16},
+            ["desc"] = "Blast players & bombs away from you with force!"
+        },
+        {
+            ["type"] = "M",
+            ["name"] = "Platform",
+            ["image"] = "2, 1",
+            ["cooldown"] = {22, 20, 18, 10},
+            ["desc"] = "Spawn a platform under your feet to stand on."
+        },
+        {
+            ["type"] = "M",
+            ["name"] = "Pickaxe",
+            ["image"] = "3, 1",
+            ["cooldown"] = {12, 8, 8, 3},
+            ["desc"] = "Dig the ground in front of or below you for hidden gold!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Shock Mine",
+            ["image"] = "4, 1",
+            ["cooldown"] = {24, 24, 24, 20},
+            ["desc"] = "Drop a mine that zaps terrain & opponents alike."
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Freeze Mine",
+            ["image"] = "5, 1",
+            ["cooldown"] = {20, 20, 20, 20},
+            ["desc"] = "Drop a mine that freezes terrain & opponents alike."
+        },
+        {
+            ["type"] = "M",
+            ["name"] = "Dive",
+            ["rarity"] = 0.1,
+            ["image"] = "6, 1",
+            ["cooldown"] = {6, 6, 6, 6},
+            ["desc"] = "Nyoooom!! Is this even useful, or just silly?"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Fake Coin",
+            ["image"] = "7, 1",
+            ["cooldown"] = {23, 20, 19, 19},
+            ["desc"] = "Drop a coin that LOOKS real but is actually a bomb."
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Forcefield",
+            ["rarity"] = 0.75,
+            ["image"] = "8, 1",
+            ["cooldown"] = {24, 24, 20, 15},
+            ["desc"] = "Temporarily shield yourself from all forms of damage!"
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Heal",
+            ["rarity"] = 0.2,
+            ["image"] = "9, 1",
+            ["cooldown"] = {40, 40, 40, 30},
+            ["desc"] = "Heavily damaged? Heal some of your lost HP!"
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Ink Blast",
+            ["image"] = "0, 2",
+            ["cooldown"] = {22, 22, 20, 20},
+            ["desc"] = "Blind your foes with a burst of ink!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Mini-Nuke",
+            ["rarity"] = 0.1,
+            ["image"] = "1, 2",
+            ["cooldown"] = {45, 40, 35, 40},
+            ["desc"] = "Very slow charge, but causes quite a bit of destruction."
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Gravity Pulse",
+            ["image"] = "2, 2",
+            ["cooldown"] = {21, 21, 17, 21},
+            ["desc"] = "This pulse will disable your enemies' jumping ability."
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Fireball",
+            ["rarity"] = 0.6,
+            ["image"] = "3, 2",
+            ["cooldown"] = {21, 21, 21, 21},
+            ["desc"] = "Cast a hovering fireball spell to burn your opponents!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Stealth Mine",
+            ["image"] = "4, 2",
+            ["cooldown"] = {18, 18, 18, 17},
+            ["desc"] = "Place a hard-to-see mine, then detonate it whenever you want!"
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Healing Pulse",
+            ["rarity"] = 0.8,
+            ["image"] = "5, 2",
+            ["cooldown"] = {29, 28, 26, 20},
+            ["desc"] = "Heal your pals with this feel-good energy!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Rocket",
+            ["rarity"] = 0.5,
+            ["image"] = "6, 2",
+            ["cooldown"] = {17, 17, 14, 10},
+            ["desc"] = "Shoot a rocket that launches forward and explodes!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Piano Drop",
+            ["rarity"] = 0.65,
+            ["image"] = "7, 2",
+            ["cooldown"] = {20, 20, 20, 20},
+            ["desc"] = "Send a piano hurtling down towards your position."
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Homing Missile",
+            ["image"] = "8, 2",
+            ["cooldown"] = {18, 16, 14, 20},
+            ["desc"] = "This missile will home to your current position!"
+        },
+        {
+            ["type"] = "M",
+            ["name"] = "Warp",
+            ["rarity"] = 0.5,
+            ["image"] = "9, 2",
+            ["cooldown"] = {25, 19, 18, 15},
+            ["desc"] = "Use this to place a warp point, then use it again to warp there."
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Drumsticks",
+            ["exclusive"] = true,
+            ["rarity"] = -1,
+            ["image"] = "0, 3",
+            ["cooldown"] = {25, 24, 23, 13},
+            ["desc"] = "Not the healthiest food, but it will heal your friends!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Spicy Turkey",
+            ["exclusive"] = true,
+            ["rarity"] = -1,
+            ["image"] = "1, 3",
+            ["cooldown"] = {23, 19, 17, 20},
+            ["desc"] = "Deals contact damage, and it's easy to kick around."
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Banana",
+            ["rarity"] = -1,
+            ["image"] = "2, 3",
+            ["cooldown"] = {15, 13, 10, 9},
+            ["desc"] = "Your foes will just LOVE you after they slip on this."
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Caltrops",
+            ["rarity"] = 0.3,
+            ["image"] = "3, 3",
+            ["cooldown"] = {24, 22, 18, 10},
+            ["desc"] = "Throw down some spiky spikes all around you."
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Pumpkin Bomb",
+            ["exclusive"] = true,
+            ["rarity"] = -1,
+            ["image"] = "0, 4",
+            ["cooldown"] = {19, 18, 17, 15},
+            ["desc"] = "Hides, then explodes after 10 seconds!"
+        },
+        {
+            ["type"] = "M",
+            ["name"] = "Spooky Jump",
+            ["exclusive"] = true,
+            ["rarity"] = -1,
+            ["image"] = "1, 4",
+            ["cooldown"] = {30, 28, 27, 19},
+            ["desc"] = "Jump & create a small explosion."
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "BOO!!",
+            ["exclusive"] = true,
+            ["rarity"] = -1,
+            ["image"] = "2, 4",
+            ["cooldown"] = {17, 15, 13, 12},
+            ["desc"] = "Scare everyone next to you."
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Freeze Staff",
+            ["rarity"] = -1,
+            ["image"] = "4, 4",
+            ["cooldown"] = {21, 20, 20, 16},
+            ["desc"] = "Summon a spike of ice that freezes players it hits."
+        },
+        {
+            ["type"] = "M",
+            ["name"] = "Spring",
+            ["rarity"] = 0.3,
+            ["image"] = "4, 3",
+            ["cooldown"] = {22, 20, 16, 12},
+            ["desc"] = "Create a bouncy spring to launch yourself (and others)!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Ice Spikes",
+            ["rarity"] = -1,
+            ["image"] = "3, 4",
+            ["cooldown"] = {24, 23, 21, 16},
+            ["desc"] = "Summon spikes all around yourself to damage others."
+        },
+        {
+            ["type"] = "M",
+            ["name"] = "Ice Tower",
+            ["rarity"] = -1,
+            ["image"] = "5, 4",
+            ["cooldown"] = {24, 22, 20, 12},
+            ["desc"] = "Create a sturdy ice tower below you to rise to new heights!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Ice Meteor",
+            ["rarity"] = 0.15,
+            ["image"] = "6, 4",
+            ["cooldown"] = {23, 23, 20, 12},
+            ["desc"] = "Call down an icy meteor to fly down and strike the earth!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Footbomb",
+            ["rarity"] = 1,
+            ["image"] = "7, 4",
+            ["cooldown"] = {19, 19, 18, 12},
+            ["desc"] = "Kick it, then watch it fly and explode!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Thunderbolt",
+            ["rarity"] = 0.6,
+            ["image"] = "5, 5",
+            ["cooldown"] = {28, 28, 27, 19},
+            ["desc"] = "Create a strike of lightning to shock the earth!"
+        },
+        {
+            ["type"] = "M",
+            ["name"] = "Truss",
+            ["rarity"] = 1,
+            ["image"] = "1, 5",
+            ["cooldown"] = {15, 15, 15, 12},
+            ["desc"] = "Need a quick lift? Climb this!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Tornado Bomb",
+            ["rarity"] = -1,
+            ["image"] = "0, 5",
+            ["cooldown"] = {14, 13, 13, 12},
+            ["desc"] = "Blast your foes skyward with this!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Tornado Mine",
+            ["rarity"] = -1,
+            ["image"] = "2, 5",
+            ["cooldown"] = {14, 14, 13, 12},
+            ["desc"] = "Hide a sneaky trap and detonate it to send enemies flying!"
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Heart Flower",
+            ["rarity"] = -1,
+            ["image"] = "3, 5",
+            ["cooldown"] = {16, 14, 12, 12},
+            ["desc"] = "It may take some time to grow... but when it does, it’ll heal you!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Storm Cloud",
+            ["rarity"] = -1,
+            ["image"] = "4, 5",
+            ["cooldown"] = {25, 25, 24, 18},
+            ["desc"] = "Send out a dark cloud that will strike anyone it rains on!"
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Disarm",
+            ["rarity"] = 0.1,
+            ["image"] = "9, 4",
+            ["cooldown"] = {20, 19, 18, 12},
+            ["desc"] = "Nullify any nearby bombs."
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Poison Mine",
+            ["rarity"] = 0.1,
+            ["image"] = "6, 5",
+            ["cooldown"] = {25, 24, 23, 12},
+            ["desc"] = "Creates a toxic cloud that’ll be sure to keep enemies away!"
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Super Magnet",
+            ["rarity"] = 0.85,
+            ["image"] = "8, 4",
+            ["cooldown"] = {24, 22, 20, 15},
+            ["desc"] = "Drop a magnet that pulls in objects."
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Bear Trap",
+            ["rarity"] = 0.25,
+            ["image"] = "7, 5",
+            ["cooldown"] = {25, 24, 24, 12},
+            ["desc"] = "Traps a foe in place and deals damage!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Cursed Pizza",
+            ["rarity"] = 0.25,
+            ["image"] = "8, 5",
+            ["cooldown"] = {21, 20, 19, 12},
+            ["desc"] = "This poorly-made pizza can poison & stun an enemy."
+        },
+        {
+            ["type"] = "M",
+            ["name"] = "Candy Corn",
+            ["exclusive"] = true,
+            ["rarity"] = -1,
+            ["image"] = "9, 5",
+            ["cooldown"] = {27, 27, 27, 12},
+            ["desc"] = "A waxy fall treat. Eat one for a temporary speed and jump boost."
+        },
+        {
+            ["type"] = "N",
+            ["name"] = "Magic Dice",
+            ["rarity"] = -1,
+            ["image"] = "8, 3",
+            ["cooldown"] = {27, 27, 27, 12},
+            ["desc"] = "Chooses a random Skill to use. What could happen next?"
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Smack",
+            ["rarity"] = 0.75,
+            ["image"] = "6, 3",
+            ["cooldown"] = {16, 14, 12, 10},
+            ["desc"] = "Give those in front of you a big ol' slap, knocking them back."
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Fire Meteor",
+            ["rarity"] = 0.15,
+            ["image"] = "9, 3",
+            ["cooldown"] = {30, 29, 27, 27},
+            ["desc"] = "Call down a burning meteor to scorch the terrain."
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Toxic Strike",
+            ["rarity"] = 0.45,
+            ["image"] = "1, 6",
+            ["cooldown"] = {26, 25, 24, 23},
+            ["desc"] = "Call down a poisonous missile that surrounds the area in a toxic mist!"
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Bubble Barrier",
+            ["rarity"] = 0.75,
+            ["image"] = "6, 6",
+            ["cooldown"] = {20, 19, 18, 18},
+            ["desc"] = "Create a magic bubble that keeps bombs out!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Super Bomb Egg",
+            ["rarity"] = 0.8,
+            ["image"] = "7, 6",
+            ["cooldown"] = {16, 15, 13, 12},
+            ["desc"] = "First it goes wobble, wobble... then it goes boom."
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Glitch Bomb",
+            ["rarity"] = -1,
+            ["image"] = "9, 6",
+            ["cooldown"] = {23, 22, 20, 15},
+            ["desc"] = "String not found"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Bubble Wand",
+            ["rarity"] = 0.5,
+            ["image"] = "4, 7",
+            ["cooldown"] = {20, 19, 17, 12},
+            ["desc"] = "Blow a floating bubble to trap players!"
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Cold Snap",
+            ["rarity"] = 0.5,
+            ["image"] = "4, 6",
+            ["cooldown"] = {14, 14, 14, 14},
+            ["desc"] = "Quickly freeze your surroundings with a chilling blast!"
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Duck Bomb",
+            ["rarity"] = -1,
+            ["image"] = "2, 7",
+            ["cooldown"] = {22, 22, 20, 20},
+            ["desc"] = "Drop a tricky duck that explodes on contact!"
+        },
+        {
+            ["type"] = "N",
+            ["name"] = "Randomizer",
+            ["exclusive"] = true,
+            ["rarity"] = -1,
+            ["image"] = "8, 6",
+            ["cooldown"] = {30, 29, 27, 27},
+            ["desc"] = "Picks one of your Skills randomly at the start of each round."
+        },
+        {
+            ["type"] = "O",
+            ["name"] = "Earth Bomb",
+            ["rarity"] = 0.1,
+            ["image"] = "5, 7",
+            ["cooldown"] = {28, 26, 24, 20},
+            ["desc"] = "Throw a bomb that explodes into columns of lava!"
+        },
+        {
+            ["type"] = "M",
+            ["name"] = "Bubble Ride",
+            ["rarity"] = 0.25,
+            ["image"] = "6, 7",
+            ["cooldown"] = {20, 20, 20, 15},
+            ["desc"] = "Float around in a bubble!"
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Gift Wrap",
+            ["exclusive"] = true,
+            ["rarity"] = -1,
+            ["image"] = "7, 7",
+            ["cooldown"] = {21, 19, 17, 12},
+            ["desc"] = "Wrap up a nearby object or bomb and regift it later!"
+        },
+        {
+            ["type"] = "D",
+            ["name"] = "Telekinesis",
+            ["exclusive"] = true,
+            ["rarity"] = -1,
+            ["image"] = "8, 7",
+            ["cooldown"] = {20, 20, 20, 15},
+            ["desc"] = "Use psychic power to pick up & throw nearby objects!"
+        }
+    },
+    ["Movement"] = {1, 5, 9, 13, 14, 17, 30, 39, 45, 68},
+    ["Offense"] = {2, 3, 4, 6, 8, 15, 18, 22, 24, 25, 27, 28, 29, 34, 42, 43, 44, 51, 53, 54, 58, 59, 61, 63, 67},
+    ["RandomSkill"] = "function: 0xeec743169d5113c3"
+}
