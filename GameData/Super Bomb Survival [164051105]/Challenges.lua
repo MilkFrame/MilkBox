72e0143175435dc2d46fcfddd4fc447f75107d2e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-12-27 02:52:05 +1100

    Create Challenges.lua

---
+++ GameData/Super Bomb Survival [164051105]/Challenges.lua
@@ -0,0 +1,27 @@
+data = {
+    ["Daily"] = {
+        {["num"] = 8, ["name"] = "Win 8 rounds on Intensity 3.0 or higher.", ["event"] = "int3"},
+        {["num"] = 12, ["name"] = "Win 12 rounds on Intensity 2.0 or higher.", ["event"] = "int2"},
+        {["num"] = 6, ["name"] = "Win 6 rounds on Intensity 3.0 or higher.", ["event"] = "int3"},
+        {["num"] = 10, ["name"] = "Win 10 rounds on Intensity 3.0 or higher.", ["event"] = "int3"},
+        {["num"] = 5, ["name"] = "Win 5 rounds on Intensity 4.0 or higher.", ["event"] = "int4"},
+        {["num"] = 10, ["name"] = "Win 10 rounds on Intensity 4.0 or higher.", ["event"] = "int4"},
+        {["num"] = 5, ["name"] = "Win 5 rounds with the Hardcore option enabled.", ["event"] = "hcwin"},
+        {["num"] = 7, ["name"] = "Win 7 rounds with the Hardcore option enabled.", ["event"] = "hcwin"}
+    },
+    ["VIP"] = {
+        {["num"] = 12, ["name"] = "Win 12 rounds.", ["event"] = "win"},
+        {["num"] = 15, ["name"] = "Win 15 rounds.", ["event"] = "win"},
+        {["num"] = 10, ["name"] = "Win 10 rounds.", ["event"] = "win"},
+        {["num"] = 7, ["name"] = "Win 7 rounds.", ["event"] = "win"}
+    },
+    ["Hourly"] = {
+        {["num"] = 20, ["name"] = "Grab 20 coins.", ["event"] = "coins"},
+        {["num"] = 30, ["name"] = "Grab 30 coins.", ["event"] = "coins"},
+        {["num"] = 10, ["name"] = "Eat 10 slices of pizza.", ["event"] = "pizza"},
+        {["num"] = 15, ["name"] = "Eat 15 slices of pizza.", ["event"] = "pizza"},
+        {["num"] = 3, ["name"] = "Pick up 3 Event Icons.", ["event"] = "event"},
+        {["num"] = 6, ["name"] = "Pick up 6 Charge Sodas.", ["event"] = "soda"},
+        {["num"] = 5, ["name"] = "Pick up 5 Charge Sodas.", ["event"] = "soda"}
+    }
+}
