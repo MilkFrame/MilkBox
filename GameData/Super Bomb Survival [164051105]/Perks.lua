c96f7a85c4a81cc85667f5b86d85f81b4405cf78
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2024-12-27 02:51:54 +1100

    Create Perks.lua

---
+++ GameData/Super Bomb Survival [164051105]/Perks.lua
@@ -0,0 +1,241 @@
+--Perks SPRITESHEET = rbxassetid://16030340462
+data = {
+    ["Info"] = {
+        {
+            ["image"] = "0, 0",
+            ["name"] = "BIG SAVER",
+            ["price"] = 800,
+            ["gemprice"] = 6,
+            ["desc"] = "Collecting coins gives you health!"
+        },
+        {
+            ["image"] = "1, 0",
+            ["name"] = "PANIC SHIELD",
+            ["price"] = 900,
+            ["gemprice"] = 7,
+            ["desc"] = "Gain a short forcefield on taking damage."
+        },
+        {
+            ["image"] = "2, 0",
+            ["name"] = "LAST CHANCE",
+            ["price"] = 900,
+            ["gemprice"] = 7,
+            ["desc"] = "Protects you from dying once."
+        },
+        {
+            ["image"] = "3, 0",
+            ["name"] = "UNSHAKEABLE",
+            ["price"] = 700,
+            ["gemprice"] = 5,
+            ["desc"] = "Become unable to be knocked down."
+        },
+        {
+            ["image"] = "4, 0",
+            ["name"] = "SPEED RUNNER",
+            ["price"] = 1000,
+            ["gemprice"] = 8,
+            ["desc"] = "Improves your base walking speed."
+        },
+        {
+            ["image"] = "5, 0",
+            ["name"] = "HEAVYWEIGHT",
+            ["price"] = 800,
+            ["gemprice"] = 6,
+            ["desc"] = "Bombs won't knock you around nearly as much. Also, you're super heavy and strong!"
+        },
+        {
+            ["image"] = "6, 0",
+            ["name"] = "TOUGH COOKIE",
+            ["price"] = 1500,
+            ["gemprice"] = 9,
+            ["desc"] = "Gives you +25 max HP."
+        },
+        {
+            ["image"] = "7, 0",
+            ["name"] = "FIREPROOF",
+            ["price"] = 10000,
+            ["gemprice"] = 50,
+            ["desc"] = "Take no damage from hot parts & survive longer in lava."
+        },
+        {
+            ["image"] = "8, 0",
+            ["name"] = "SECOND WIND",
+            ["price"] = 800,
+            ["gemprice"] = 6,
+            ["desc"] = "Survive for most of the round to get a last-minute heal."
+        },
+        {
+            ["image"] = "9, 0",
+            ["name"] = "QUICK CHARGE",
+            ["price"] = 5000,
+            ["gemprice"] = 25,
+            ["desc"] = "Your Skills will recharge slightly faster."
+        },
+        {
+            ["image"] = "0, 1",
+            ["name"] = "FOOLHARDY",
+            ["price"] = 1000,
+            ["gemprice"] = 8,
+            ["desc"] = "Become impervious to all forms of status effects."
+        },
+        {
+            ["image"] = "1, 1",
+            ["name"] = "TANK",
+            ["price"] = 1500,
+            ["gemprice"] = 10,
+            ["desc"] = "Gain +50 max HP, but go much slower."
+        },
+        {
+            ["image"] = "2, 1",
+            ["name"] = "DOUBLE JUMP",
+            ["price"] = 15000,
+            ["gemprice"] = 200,
+            ["desc"] = "Gives you the ability to jump again in midair."
+        },
+        {
+            ["image"] = "3, 1",
+            ["name"] = "HIGH JUMPER",
+            ["price"] = 5000,
+            ["gemprice"] = 25,
+            ["desc"] = "You'll jump much higher than usual with this equipped!"
+        },
+        {
+            ["image"] = "4, 1",
+            ["name"] = "TREASURER",
+            ["price"] = 800,
+            ["gemprice"] = 5,
+            ["desc"] = "See all coins, even through objects and terrain!"
+        },
+        {
+            ["image"] = "5, 1",
+            ["name"] = "PIZZA LOVER",
+            ["price"] = 600,
+            ["gemprice"] = 3,
+            ["desc"] = "Gain twice as much HP when eating pizza."
+        },
+        {
+            ["image"] = "6, 1",
+            ["name"] = "AIR DASH",
+            ["price"] = 30000,
+            ["gemprice"] = 200,
+            ["desc"] = "Jump in midair to quickly zoom forward."
+        },
+        {
+            ["image"] = "7, 1",
+            ["name"] = "FEATHERFALL",
+            ["price"] = 6000,
+            ["gemprice"] = 18,
+            ["desc"] = "Slows your fall to a gentle speed automatically."
+        },
+        {
+            ["image"] = "9, 1",
+            ["name"] = "SHOCK ABSORB",
+            ["price"] = 3000,
+            ["gemprice"] = 20,
+            ["desc"] = "Instead of damaging you, electrical explosions will charge your Skill."
+        },
+        {
+            ["image"] = "1, 2",
+            ["name"] = "BOMB VISION",
+            ["price"] = 300,
+            ["gemprice"] = 2,
+            ["desc"] = "Shows red dots where bombs are hiding, even through walls."
+        },
+        {
+            ["image"] = "0, 2",
+            ["name"] = "CRYSTAL WINGS",
+            ["price"] = 50000,
+            ["gemprice"] = 550,
+            ["desc"] = "Lets you glide around speedily, but hurts you if damaged."
+        },
+        {
+            ["image"] = "2, 2",
+            ["name"] = "TACTICIAN",
+            ["price"] = 500,
+            ["gemprice"] = 3,
+            ["desc"] = "Allows you to see other players' equipment and the locations of power-ups."
+        },
+        {
+            ["image"] = "3, 2",
+            ["name"] = "REGENERATION",
+            ["price"] = 9000,
+            ["gemprice"] = 40,
+            ["desc"] = "Slowly regain health after not being damaged for a while."
+        },
+        {
+            ["image"] = "4, 2",
+            ["name"] = "UNSEEN",
+            ["price"] = 5000,
+            ["gemprice"] = 30,
+            ["desc"] = "Homing rockets will no longer target you."
+        },
+        {
+            ["image"] = "5, 2",
+            ["name"] = "GAS MASK",
+            ["price"] = 2500,
+            ["gemprice"] = 10,
+            ["desc"] = "Poison clouds will heal you over time."
+        },
+        {
+            ["image"] = "8, 1",
+            ["name"] = "ANTI-FREEZE",
+            ["price"] = 2500,
+            ["gemprice"] = 10,
+            ["desc"] = "Ice explosions will increase your max HP."
+        },
+        {
+            ["image"] = "7, 2",
+            ["name"] = "HALLOW WINGS",
+            ["exclusive"] = true,
+            ["price"] = -1,
+            ["gemprice"] = 660,
+            ["limited"] = true,
+            ["desc"] = "Lets you glide around, but hurts you slightly if damaged."
+        },
+        {
+            ["image"] = "8, 2",
+            ["name"] = "AURORA WINGS",
+            ["price"] = 80000,
+            ["gemprice"] = 750,
+            ["desc"] = "Lets you glide high, but freezes you if damaged."
+        },
+        {
+            ["image"] = "0, 3",
+            ["name"] = "EXPERT'S TOUCH",
+            ["price"] = 6000,
+            ["gemprice"] = 40,
+            ["desc"] = "Become unable to activate touch-based explosives."
+        },
+        {
+            ["image"] = "5, 3",
+            ["name"] = "RANDOMIZER",
+            ["price"] = -1,
+            ["gemprice"] = -1,
+            ["exclusive"] = true,
+            ["desc"] = "Picks one of your Perks randomly at the start of each round."
+        },
+        {
+            ["image"] = "2, 3",
+            ["name"] = "OVERCHARGE",
+            ["price"] = 30000,
+            ["gemprice"] = 300,
+            ["desc"] = "Supercharges your Skill, but requires soda to charge."
+        },
+        {
+            ["image"] = "9, 2",
+            ["name"] = "CRYSTAL SHIELD",
+            ["price"] = -1,
+            ["gemprice"] = 400,
+            ["exclusive"] = true,
+            ["desc"] = "Protects you from a few hits, then shatters."
+        },
+        {
+            ["image"] = "6, 2",
+            ["name"] = "WALL CLIMBER",
+            ["price"] = -1,
+            ["gemprice"] = 150,
+            ["exclusive"] = true,
+            ["desc"] = "Gain the ability to climb on the sides of terrain."
+        }
+    }
+}
