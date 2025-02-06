7d83550c2e43e4b01db25a43de16c93251874e0d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-07 03:14:41 +1100

    Create PlrStyles.lua

---
+++ GameData/Blue Lock: Rivals [18668065416]/PlrStyles.lua
@@ -0,0 +1,55 @@
+data = {
+    ["Isagi"] = {
+        ["Rarity"] = "Rare",
+        ["Name"] = "Isagi",
+        ["Desc"] = "A strategic Playmaker, reads the game well and excels at positioning and teamwork. "
+    },
+    ["Shidou"] = {
+        ["Rarity"] = "Mythic",
+        ["Name"] = "Shidou",
+        ["Desc"] = "A wild striker thriving on chaos and unpredictability."
+    },
+    ["Bachira"] = {
+        ["Rarity"] = "Epic",
+        ["Name"] = "Bachira",
+        ["Desc"] = "A Energetic dribbler, great a unpredictable plays and ankles breakers. "
+    },
+    ["King"] = {
+        ["Rarity"] = "Legendary",
+        ["Name"] = "King",
+        ["Desc"] = "A Ruthless Striker, dominant presence with powerful shots and individual play."
+    },
+    ["Chigiri"] = {
+        ["Rarity"] = "Rare",
+        ["Name"] = "Chigiri",
+        ["Desc"] = "A speed-focused character, mastering swift dashes, enhanced sprinting, and boundless stamina."
+    },
+    ["Nagi"] = {
+        ["Rarity"] = "Legendary",
+        ["Name"] = "Nagi",
+        ["Desc"] = "Natural Talent, highly skilled at trapping and controlling difficult passes."
+    },
+    ["Yukimiya"] = {["Rarity"] = "Mythic", ["Name"] = "Yukimiya", ["Desc"] = "Street ball legend, ultimate dribbler."},
+    ["Reo"] = {["Rarity"] = "Legendary", ["Name"] = "Reo", ["Desc"] = "Jack of all trades, master of.. all?"},
+    ["Hiori"] = {["Rarity"] = "Epic", ["Name"] = "Hiori", ["Desc"] = "Perhaps the best passer in soccer history 🥶🥶🥶"},
+    ["Sae"] = {
+        ["Rarity"] = "Mythic",
+        ["Name"] = "Sae",
+        ["Desc"] = "Genius playmaker with unrivaled skills, precise passes, and unstoppable shots."
+    },
+    ["Gagamaru"] = {
+        ["Rarity"] = "Epic",
+        ["Name"] = "Gagamaru",
+        ["Desc"] = "Beastly GOALKEEPER with sharp instincts and unmatched reflexes."
+    },
+    ["Aiku"] = {
+        ["Rarity"] = "Mythic",
+        ["Name"] = "Aiku",
+        ["Desc"] = "Best defender you'll ever see and only one cause ngl we ain't making more"
+    },
+    ["Rin"] = {
+        ["Rarity"] = "Mythic",
+        ["Name"] = "Rin",
+        ["Desc"] = "Tactical striker with deadly curves, precise shots, and unmatched control."
+    }
+}
