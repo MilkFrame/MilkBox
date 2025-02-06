15d18b67203ed40e2eca1b5de2caa7aa53213333
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-07 03:13:50 +1100

    Create Locations.lua

---
+++ GameData/Zaibatsu [3524195448]/Locations.lua
@@ -0,0 +1,298 @@
+data = {
+    ["FootballClubElevatorBottom"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Zaibatsu Station Mall",
+        ["TelepadId"] = "FootballClubElevatorBottom"
+    },
+    ["HigashiDoriStation"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Higashi Dori Station",
+        ["TelepadId"] = "HigashiDoriStation"
+    },
+    ["FightClubSecondaryDoorInside"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Kendo Fight Club",
+        ["TelepadId"] = "FightClubSecondaryDoorInside"
+    },
+    ["YakazaSafehouseOutside"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Yakuza Safehouse Outside",
+        ["TelepadId"] = "YakazaSafehouseOutside"
+    },
+    ["CapsuleHotelMainDoorOutside"] = {["TelepadId"] = "CapsuleHotelMainDoorOutside", ["RegionId"] = "Zaibatsu"},
+    ["LowerClassApartments1CommonArea1"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Yokoyama Building Floor 1",
+        ["TelepadId"] = "LowerClassApartments1CommonArea1"
+    },
+    ["DanceClubMainDoorOutside"] = {["TelepadId"] = "DanceClubMainDoorOutside", ["RegionId"] = "Zaibatsu"},
+    ["PoliceStationOperationsElevatorTop"] = {
+        ["RegionId"] = "PoliceStation",
+        ["Name"] = "Operations Center",
+        ["TelepadId"] = "PoliceStationOperationsElevatorTop"
+    },
+    ["TrainCabin"] = {["RegionId"] = "TrainCabin", ["Name"] = "Train Cabin", ["TelepadId"] = "TrainCabin"},
+    ["StandardOffice1CommonArea3"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Floor 18",
+        ["TelepadId"] = "StandardOffice1CommonArea3"
+    },
+    ["HospitalOutside"] = {["RegionId"] = "Zaibatsu", ["Name"] = "Meitetsu Hospital", ["TelepadId"] = "HospitalOutside"},
+    ["UpperClassApartments1CommonArea1"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Panorama Tower Floor 6",
+        ["TelepadId"] = "UpperClassApartments1CommonArea1"
+    },
+    ["PortStation"] = {["RegionId"] = "Zaibatsu", ["Name"] = "Port Station", ["TelepadId"] = "PortStation"},
+    ["FightClubMainDoorInside"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Kendo Fight Club",
+        ["TelepadId"] = "FightClubMainDoorInside"
+    },
+    ["UpperClassApartments1CommonArea2"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Panorama Tower Floor 7",
+        ["TelepadId"] = "UpperClassApartments1CommonArea2"
+    },
+    ["ResearchLabElevatorTop"] = {
+        ["RegionId"] = "ResearchLab",
+        ["Name"] = "Surface",
+        ["TelepadId"] = "ResearchLabElevatorTop"
+    },
+    ["MiddleClassApartments2CommonArea1"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Old Town Manor Floor 1",
+        ["TelepadId"] = "MiddleClassApartments2CommonArea1"
+    },
+    ["FightClubSecondaryDoorOutside"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Sewer",
+        ["TelepadId"] = "FightClubSecondaryDoorOutside"
+    },
+    ["KanonParkStation"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Kanon Park Station",
+        ["TelepadId"] = "KanonParkStation"
+    },
+    ["HospitalElevatorTraumaWard"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Trauma Ward",
+        ["TelepadId"] = "HospitalElevatorTraumaWard"
+    },
+    ["ResearchLabElevatorBottom"] = {
+        ["RegionId"] = "ResearchLab",
+        ["Name"] = "Research Lab",
+        ["TelepadId"] = "ResearchLabElevatorBottom"
+    },
+    ["PoliceStationElevatorHQ"] = {
+        ["RegionId"] = "PoliceStation",
+        ["Name"] = "T-SEC Headquarters",
+        ["TelepadId"] = "PoliceStationElevatorHQ"
+    },
+    ["UpperClassApartments2Enterance"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Serenity Skies Outside",
+        ["TelepadId"] = "UpperClassApartments2Enterance"
+    },
+    ["TrainTunnelMaintenanceShaft"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Tunnel Maintenance Shaft",
+        ["TelepadId"] = "TrainTunnelMaintenanceShaft"
+    },
+    ["SabishiParkingArea"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Sabishi Parking Area",
+        ["TelepadId"] = "SabishiParkingArea"
+    },
+    ["ZaibatsuCrossing"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Zaibatsu Crossing",
+        ["TelepadId"] = "ZaibatsuCrossing"
+    },
+    ["PoliceStationKobanAccessStation"] = {
+        ["RegionId"] = "PoliceStation",
+        ["Name"] = "T-SEC Headquarters",
+        ["TelepadId"] = "PoliceStationKobanAccessStation"
+    },
+    ["BathhouseOutside"] = {["RegionId"] = "Zaibatsu", ["Name"] = "Onsen Outside", ["TelepadId"] = "BathhouseOutside"},
+    ["ArcadeMainDoorOutside"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Zaibatsu",
+        ["TelepadId"] = "ArcadeMainDoorOutside"
+    },
+    ["PoliceStationElevatorLobby"] = {
+        ["RegionId"] = "PoliceStation",
+        ["Name"] = "Lobby",
+        ["TelepadId"] = "PoliceStationElevatorLobby"
+    },
+    ["CityHallElevatorLobby"] = {["RegionId"] = "CityHall", ["Name"] = "Lobby", ["TelepadId"] = "CityHallElevatorLobby"},
+    ["MiddleClassApartments1Enterance"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Sakura Heights Outside",
+        ["TelepadId"] = "MiddleClassApartments1Enterance"
+    },
+    ["TechOfficeElevatorLobby"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "IBN Lobby",
+        ["TelepadId"] = "TechOfficeElevatorLobby"
+    },
+    ["LowerClassApartments1Enterance"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Yokoyama Building Outside",
+        ["TelepadId"] = "LowerClassApartments1Enterance"
+    },
+    ["CapsuleApartmentElevatorOutside"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Zaibatsu",
+        ["TelepadId"] = "CapsuleApartmentElevatorOutside"
+    },
+    ["MiddleClassApartments2CommonArea2"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Old Town Manor Floor 2",
+        ["TelepadId"] = "MiddleClassApartments2CommonArea2"
+    },
+    ["StandardOffice1CommonArea2"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Floor 17",
+        ["TelepadId"] = "StandardOffice1CommonArea2"
+    },
+    ["DanceClubMainDoorInside"] = {["TelepadId"] = "DanceClubMainDoorInside", ["RegionId"] = "DanceClub"},
+    ["StandardOffice1CommonArea1"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Floor 16",
+        ["TelepadId"] = "StandardOffice1CommonArea1"
+    },
+    ["StandardOffice1Enterance"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Waterfront Plaza Outside",
+        ["TelepadId"] = "StandardOffice1Enterance"
+    },
+    ["ZaibatsuStationPlatform"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Zaibatsu Station",
+        ["TelepadId"] = "ZaibatsuStationPlatform"
+    },
+    ["UpperClassApartments2CommonArea"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Serenity Skies",
+        ["TelepadId"] = "UpperClassApartments2CommonArea"
+    },
+    ["UpperClassApartments1Enterance"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Panorama Tower Outside",
+        ["TelepadId"] = "UpperClassApartments1Enterance"
+    },
+    ["JazzClubOutside"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Lonely Note Outside",
+        ["TelepadId"] = "JazzClubOutside"
+    },
+    ["FightClubMainDoorOutside"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Zaibatsu",
+        ["TelepadId"] = "FightClubMainDoorOutside"
+    },
+    ["ArcadeRearDoorOutside"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Zaibatsu",
+        ["TelepadId"] = "ArcadeRearDoorOutside"
+    },
+    ["MiddleClassApartments2Enterance"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Old Town Manor ",
+        ["TelepadId"] = "MiddleClassApartments2Enterance"
+    },
+    ["MiddleClassApartments2CommonArea3"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Old Town Manor Floor 3",
+        ["TelepadId"] = "MiddleClassApartments2CommonArea3"
+    },
+    ["PoliceStationKobanAccessKoban"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "T-SEC Koban",
+        ["TelepadId"] = "PoliceStationKobanAccessKoban"
+    },
+    ["TechOfficeElevatorParking"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Underground Parking",
+        ["TelepadId"] = "TechOfficeElevatorParking"
+    },
+    ["ArcadeRearDoorInside"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Grand Gamer Arcade",
+        ["TelepadId"] = "ArcadeRearDoorInside"
+    },
+    ["CapsuleApartmentElevatorInside"] = {
+        ["RegionId"] = "CapsuleApartment",
+        ["Name"] = "Capsule Apartment",
+        ["TelepadId"] = "CapsuleApartmentElevatorInside"
+    },
+    ["TechOfficeElevatorOffice"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "IBN Office",
+        ["TelepadId"] = "TechOfficeElevatorOffice"
+    },
+    ["FootballClubElevatorTop"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Zaibatsu Football Club",
+        ["TelepadId"] = "FootballClubElevatorTop"
+    },
+    ["PoliceStationOperationsElevatorBottom"] = {
+        ["RegionId"] = "PoliceStation",
+        ["Name"] = "Headquarters",
+        ["TelepadId"] = "PoliceStationOperationsElevatorBottom"
+    },
+    ["MiddleClassApartments1CommonArea"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Sakura Heights Building",
+        ["TelepadId"] = "MiddleClassApartments1CommonArea"
+    },
+    ["WaterfrontParkingArea"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Waterfront Parking Area",
+        ["TelepadId"] = "WaterfrontParkingArea"
+    },
+    ["TrainTunnelMaintenanceEntrance"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Sabishi Parking Area",
+        ["TelepadId"] = "TrainTunnelMaintenanceEntrance"
+    },
+    ["HospitalElevatorLobby"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Lobby & Emergency",
+        ["TelepadId"] = "HospitalElevatorLobby"
+    },
+    ["FastFoodOutside"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Atomic Automat Outside",
+        ["TelepadId"] = "FastFoodOutside"
+    },
+    ["KaraokeLoungeElevatorLobby"] = {
+        ["RegionId"] = "KaraokeLounge",
+        ["Name"] = "Lobby",
+        ["TelepadId"] = "KaraokeLoungeElevatorLobby"
+    },
+    ["TechOfficeOutside"] = {["RegionId"] = "Zaibatsu", ["Name"] = "IBN Outside", ["TelepadId"] = "TechOfficeOutside"},
+    ["LowerClassApartments1CommonArea2"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Yokoyama Building Floor 2",
+        ["TelepadId"] = "LowerClassApartments1CommonArea2"
+    },
+    ["CityHallStation"] = {["RegionId"] = "Zaibatsu", ["Name"] = "City Hall Station", ["TelepadId"] = "CityHallStation"},
+    ["ArcadeMainDoorInside"] = {
+        ["RegionId"] = "Zaibatsu",
+        ["Name"] = "Grand Gamer Arcade",
+        ["TelepadId"] = "ArcadeMainDoorInside"
+    },
+    ["CapsuleHotelMainDoorInside"] = {
+        ["RegionId"] = "CapsuleHotel",
+        ["Name"] = "5 to 9 Capsule Hotel",
+        ["TelepadId"] = "CapsuleHotelMainDoorInside"
+    },
+    ["CityHallElevatorOffice"] = {
+        ["RegionId"] = "CityHall",
+        ["Name"] = "Zaibatsu City Government Hall",
+        ["TelepadId"] = "CityHallElevatorOffice"
+    },
+    ["OldTown"] = {["RegionId"] = "Zaibatsu", ["Name"] = "Old Town", ["TelepadId"] = "OldTown"}
+}
