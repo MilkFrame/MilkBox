447d9c8fd9288999fbcba3386847049d70a4591c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-07 03:17:01 +1100

    Create Cosmetics.lua

---
+++ GameData/Blue Lock: Rivals [18668065416]/Cosmetics.lua
@@ -0,0 +1,17 @@
+data = {
+    ["Lanterns!"] = "Limited",
+    ["Dragon Cape"] = "Limited",
+    ["Fireworks"] = "Limited",
+    ["Cape"] = "Common",
+    ["Christmas Aura"] = "Limited",
+    ["Snowman Cape"] = "Limited",
+    ["Peppermint Cape"] = "Limited",
+    ["Gingerbread Cape"] = "Limited",
+    ["Dribblers Glasses"] = "Limited",
+    ["GLITCH"] = "Limited",
+    ["Shadow Cape"] = "Limited",
+    ["SHADOW"] = "Limited",
+    ["Ninja"] = "Limited",
+    ["Santa Hat"] = "Limited",
+    ["Santa Scarf"] = "Limited"
+}
