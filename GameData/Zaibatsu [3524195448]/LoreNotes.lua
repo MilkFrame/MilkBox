6f225dc624ad3b4e84d92b6a1bafe2e142dd52f4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-07 03:13:08 +1100

    Create LoreNotes.lua

---
+++ GameData/Zaibatsu [3524195448]/LoreNotes.lua
@@ -0,0 +1,49 @@
+data = {
+    ["ZaibatsuHistory"] = {
+        ["Title"] = "The History of Zaibatsu",
+        ["Body"] = [[ Zaibatsu's history is one of transformation and growth. Initially, it was a barren coastal rock unsuitable for long term settlement, only frequented by local fishermen. However, during the 1700s, a small naval outpost was built to protect the Tokyo Bay area from raids.
+        <br />During World War II, the landmass and fortifications were significantly expanded, and a network of tunnels was constructed to provide protection from air raids and as a secure place to store military ordnance.
+        <br />After the war, the fort was abandoned and left to decay until the early 1970s when little available land and a continuously expanding metropolis left developers to build artificial islands in the Tokyo Bay. ITO ITO construction was awarded a contract to do the bulk of the expansion of the landmass, as well as construct the port area.
+        <br />Starting in the 90s, financial incentives made Zaibatsu an attractive place for multinational companies to open East Asian divisions of their companies. Intelligent Business Networks was one of these, and more high-skilled tech workers flocked to the artificial island, leading to more development and expansion, with ITO ITO dominant in securing contracts to build further skyscrapers. Eventually, they constructed the Monolith, Zaibatsu's tallest building.
+        <br />However, the failure of many global institutions due to climate disasters, war, and energy insecurity led to hundreds of millions displaced around the world. Due to Japan's shrinking population, the government enacted a plan to increase immigration but concentrated immigrants to certain areas of Japan so as not to cause strife with the ethnic majority of Japanese.
+        <br />Due to Zaibatsu's worldwide presence, it was chosen as one of the special immigration zones, attracting the talent of well-educated workers from western countries in North America and Europe, as well as poor displaced people from areas of war in Eastern Europe, Africa, and Central Asia, making Zaibatsu one of the most multicultural places in Japan.]]
+    },
+    ["GrandGamerLore"] = {
+        ["Title"] = "Grand Gamer Advertisement",
+        ["Body"] = [[Are you looking for a great, casual place to hang out with your friends and challenge them to your favorite retro games? Look no further than the Grand Gamer, voted Zaibatsu's favorite (and only!) arcade for the past seven years! If you're looking for the more competitive scene, be sure to stop by while the Grand Gaming Gauntlet (aka G^3), Japan's premier retro video gaming championship, is in town. Bring your kids! Bring your androids! The Grand Gamer is guaranteed fun for the whole family. 
+        ]]
+    },
+    ["KappoSushiLore"] = {
+        ["Title"] = "A Customer's review of Kappo Sushi",
+        ["Body"] = [[Kappo Sushi is a hidden gem of a restaurant in the Old Town neighborhood of Zaibatsu. Despite being a hole in the wall, this place serves some of the best salmon sashimi in the city. Regulars swear by it and the lines out the door prove it. But there's more to Kappo Sushi than just great food. 
+        <br />The upstairs section of the restaurant is a popular meeting place for local yakuza to conduct their business talks. It's not uncommon to see suited men slipping up the stairs for a meeting, but don't let that deter you from enjoying the amazing food. Just be sure to mind your own business and let them mind theirs. 
+        <br />Come for the sashimi, stay for the potential yakuza sightings.
+        <br />Rating: ⭐⭐⭐⭐
+        ]]
+    },
+    ["BeanBrewLore"] = {
+        ["Title"] = "A Review on Bean Brew",
+        ["Body"] = [[<i>“Zaibatsu runs on Bean Brew.”</i><br />
+        <br />Bean Brew, a popular North American chain, is a testament to the impact of globalization in the world today. Despite its lukewarm and basic coffee, the chain has established a large following among coffee and tea lovers worldwide, including in Japan. With its extensive menu, offering its signature coffee blends, teas, and donuts and featuring Japanese favorites like Dango and bubble tea. Bean Brew has endeared itself to locals and tourists alike.
+        <br />It's not uncommon to find Bean Brew locations in most major Japanese cities, and in some, the stores have undergone a transformation to include elements of the popular "maid cafe" culture found in Akihabara, the bustling district in Tokyo known for its anime, manga, and video game culture. This infusion of Japanese pop culture adds a unique touch to the North American chain and has made Bean Brew a favorite among younger generations in Japan.
+        <br />But the question remains - is Bean Brew just another foreign brand trying to ride the wave of Japanese pop culture, or is it genuinely popular among locals? The best way to answer that question is to try the coffee and taste the difference for yourself. Whether you're a tourist exploring the bustling streets of Japan or a local looking for a quick caffeine fix, Bean Brew offers an experience that's uniquely North American, with a Japanese twist.
+        ]]
+    },
+    ["LonelyNoteLore"] = {
+        ["Title"] = "A Customer's review of The Lonely Note",
+        ["Body"] = [[The Lonely Note is a cozy jazz club that offers a warm and inviting atmosphere, perfect for winding down after a long day at work. Located in Sabishi Lane, Zaibatsu, it's a favorite hangout spot for office workers looking to drink with friends and enjoy live music performances.
+        <br />Due to its popularity with off-duty police officers, the Lonely Note has earned the nickname "the blue bar." Its reputation as a popular spot for police officers has made it a safe and welcoming environment for all patrons.
+        <br />Step inside the Lonely Note and you'll be transported to a world of smooth jazz and good company. The club offers a wide selection of drinks and snacks, making it the perfect spot for a quick bite or a leisurely evening with friends. Whether you're alone or with a group, you'll always find good company at the Lonely Note.
+        <br />With live music, great drinks, and friendly company, it's the perfect spot to unwind and relax. Remember, at the Lonely Note, "you're never alone!"
+        <br />Rating: ⭐⭐⭐⭐⭐
+        ]]
+    },
+    ["IBNLore"] = {
+        ["Title"] = "A look into Intelligent Business Networks",
+        ["Body"] = [[Intelligent Business Networks (IBN) is a global technology company that has been at the forefront of innovation for over half a century. Founded in the early days of personal computing, the company has since become a household name for smarter work-based technology solutions.
+        <br />Over the years, IBN has developed a wide range of cutting-edge products that have revolutionized the way people work and communicate. One of the company's greatest achievements is its creation of one of the largest and most secure data centers in the world. From cloud-based software to advanced hardware, the company's products have made businesses more efficient and streamlined.
+        <br />Despite the company's impressive track record, much of what IBN is currently working on remains a closely guarded secret. Many of their most promising and groundbreaking projects are kept under tight wraps, but it's clear that the company's impact on society is only growing. Their operations in Zaibatsu work closely with T-SEC, in the development of the Omni Sensing System (OSS) project. This collaboration demonstrates the importance of IBN's technological expertise in keeping Zaibatsu safe from any potential threat.
+        <br />IBN has cemented its position as a major player in the tech industry and its innovations are likely to shape the future of work and communication for years to come. Whether you're a business owner or a tech enthusiast, IBN's products are sure to impress and transform the way you work.
+        ]]
+    }
+}
