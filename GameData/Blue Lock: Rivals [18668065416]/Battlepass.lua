c4448f75bc69918efdcc7107b002af24c032f78b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-07 03:16:11 +1100

    Create Battlepass.lua

---
+++ GameData/Blue Lock: Rivals [18668065416]/Battlepass.lua
@@ -0,0 +1,334 @@
+data = {
+    ["AbbreviationToNormal"] = {
+        ["Flow"] = "FlowSpins",
+        ["LS"] = "LuckySpins",
+        ["Spins"] = "Spins",
+        ["Snow"] = "EventCurrency",
+        ["FL"] = "LuckyFlowSpins"
+    },
+    ["InventoryItems"] = {"Emotes", "GoalEffects", "Cards", "Cosmetics"},
+    ["Rebirth"] = {
+        ["Tier23"] = {
+            ["Premium"] = {["Value"] = 195, ["Reward"] = "Snow"},
+            ["Index"] = 23,
+            ["Free"] = {["Value"] = 100, ["Reward"] = "Snow"}
+        },
+        ["Tier26"] = {
+            ["Premium"] = {["Value"] = 210, ["Reward"] = "Snow"},
+            ["Index"] = 26,
+            ["Free"] = {["Value"] = 110, ["Reward"] = "Snow"}
+        },
+        ["Tier22"] = {
+            ["Premium"] = {["Value"] = 190, ["Reward"] = "Snow"},
+            ["Index"] = 22,
+            ["Free"] = {["Value"] = 95, ["Reward"] = "Snow"}
+        },
+        ["Tier8"] = {
+            ["Premium"] = {["Value"] = 130, ["Reward"] = "Snow"},
+            ["Index"] = 8,
+            ["Free"] = {["Value"] = 65, ["Reward"] = "Snow"}
+        },
+        ["Tier9"] = {
+            ["Premium"] = {["Value"] = 135, ["Reward"] = "Snow"},
+            ["Index"] = 9,
+            ["Free"] = {["Value"] = 65, ["Reward"] = "Snow"}
+        },
+        ["Tier30"] = {
+            ["Premium"] = {["Value"] = 230, ["Reward"] = "Snow"},
+            ["Index"] = 30,
+            ["Free"] = {["Value"] = 120, ["Reward"] = "Snow"}
+        },
+        ["Tier29"] = {
+            ["Premium"] = {["Value"] = 225, ["Reward"] = "Snow"},
+            ["Index"] = 29,
+            ["Free"] = {["Value"] = 120, ["Reward"] = "Snow"}
+        },
+        ["Tier5"] = {
+            ["Premium"] = {["Value"] = 115, ["Reward"] = "Snow"},
+            ["Index"] = 5,
+            ["Free"] = {["Value"] = 60, ["Reward"] = "Snow"}
+        },
+        ["Tier28"] = {
+            ["Premium"] = {["Value"] = 220, ["Reward"] = "Snow"},
+            ["Index"] = 28,
+            ["Free"] = {["Value"] = 115, ["Reward"] = "Snow"}
+        },
+        ["Tier12"] = {
+            ["Premium"] = {["Value"] = 145, ["Reward"] = "Snow"},
+            ["Index"] = 12,
+            ["Free"] = {["Value"] = 70, ["Reward"] = "Snow"}
+        },
+        ["Tier1"] = {
+            ["Premium"] = {["Value"] = 100, ["Reward"] = "Snow"},
+            ["Index"] = 1,
+            ["Free"] = {["Value"] = 50, ["Reward"] = "Snow"}
+        },
+        ["Tier15"] = {
+            ["Premium"] = {["Value"] = 160, ["Reward"] = "Snow"},
+            ["Index"] = 15,
+            ["Free"] = {["Value"] = 75, ["Reward"] = "Snow"}
+        },
+        ["Tier4"] = {
+            ["Premium"] = {["Value"] = 110, ["Reward"] = "Snow"},
+            ["Index"] = 4,
+            ["Free"] = {["Value"] = 60, ["Reward"] = "Snow"}
+        },
+        ["Tier13"] = {
+            ["Premium"] = {["Value"] = 150, ["Reward"] = "Snow"},
+            ["Index"] = 13,
+            ["Free"] = {["Value"] = 75, ["Reward"] = "Snow"}
+        },
+        ["Tier20"] = {
+            ["Premium"] = {["Value"] = 180, ["Reward"] = "Snow"},
+            ["Index"] = 20,
+            ["Free"] = {["Value"] = 85, ["Reward"] = "Snow"}
+        },
+        ["Tier25"] = {
+            ["Premium"] = {["Value"] = 205, ["Reward"] = "Snow"},
+            ["Index"] = 25,
+            ["Free"] = {["Value"] = 110, ["Reward"] = "Snow"}
+        },
+        ["Tier14"] = {
+            ["Premium"] = {["Value"] = 155, ["Reward"] = "Snow"},
+            ["Index"] = 14,
+            ["Free"] = {["Value"] = 75, ["Reward"] = "Snow"}
+        },
+        ["Tier2"] = {
+            ["Premium"] = {["Value"] = 100, ["Reward"] = "Snow"},
+            ["Index"] = 2,
+            ["Free"] = {["Value"] = 55, ["Reward"] = "Snow"}
+        },
+        ["Tier21"] = {
+            ["Premium"] = {["Value"] = 185, ["Reward"] = "Snow"},
+            ["Index"] = 21,
+            ["Free"] = {["Value"] = 90, ["Reward"] = "Snow"}
+        },
+        ["Tier17"] = {
+            ["Premium"] = {["Value"] = 170, ["Reward"] = "Snow"},
+            ["Index"] = 17,
+            ["Free"] = {["Value"] = 80, ["Reward"] = "Snow"}
+        },
+        ["Tier3"] = {
+            ["Premium"] = {["Value"] = 105, ["Reward"] = "Snow"},
+            ["Index"] = 3,
+            ["Free"] = {["Value"] = 60, ["Reward"] = "Snow"}
+        },
+        ["Tier19"] = {
+            ["Premium"] = {["Value"] = 175, ["Reward"] = "Snow"},
+            ["Index"] = 19,
+            ["Free"] = {["Value"] = 85, ["Reward"] = "Snow"}
+        },
+        ["Tier18"] = {
+            ["Premium"] = {["Value"] = 170, ["Reward"] = "Snow"},
+            ["Index"] = 18,
+            ["Free"] = {["Value"] = 80, ["Reward"] = "Snow"}
+        },
+        ["Tier16"] = {
+            ["Premium"] = {["Value"] = 165, ["Reward"] = "Snow"},
+            ["Index"] = 16,
+            ["Free"] = {["Value"] = 80, ["Reward"] = "Snow"}
+        },
+        ["Tier24"] = {
+            ["Premium"] = {["Value"] = 200, ["Reward"] = "Snow"},
+            ["Index"] = 24,
+            ["Free"] = {["Value"] = 110, ["Reward"] = "Snow"}
+        },
+        ["Tier10"] = {
+            ["Premium"] = {["Value"] = 135, ["Reward"] = "Snow"},
+            ["Index"] = 10,
+            ["Free"] = {["Value"] = 70, ["Reward"] = "Snow"}
+        },
+        ["Tier27"] = {
+            ["Premium"] = {["Value"] = 215, ["Reward"] = "Snow"},
+            ["Index"] = 27,
+            ["Free"] = {["Value"] = 115, ["Reward"] = "Snow"}
+        },
+        ["Tier11"] = {
+            ["Premium"] = {["Value"] = 140, ["Reward"] = "Snow"},
+            ["Index"] = 11,
+            ["Free"] = {["Value"] = 70, ["Reward"] = "Snow"}
+        },
+        ["Tier7"] = {
+            ["Premium"] = {["Value"] = 125, ["Reward"] = "Snow"},
+            ["Index"] = 7,
+            ["Free"] = {["Value"] = 65, ["Reward"] = "Snow"}
+        },
+        ["Tier6"] = {
+            ["Premium"] = {["Value"] = 120, ["Reward"] = "Snow"},
+            ["Index"] = 6,
+            ["Free"] = {["Value"] = 65, ["Reward"] = "Snow"}
+        }
+    },
+    ["Tiers"] = {
+        ["Tier23"] = {
+            ["Premium"] = {["Value"] = 160, ["Reward"] = "Snow"},
+            ["Index"] = 23,
+            ["Free"] = {["Value"] = 80, ["Reward"] = "Snow"}
+        },
+        ["Tier26"] = {
+            ["Premium"] = {["Value"] = 170, ["Reward"] = "Snow"},
+            ["Index"] = 26,
+            ["Free"] = {["Reward"] = "None"}
+        },
+        ["Tier22"] = {
+            ["Premium"] = {["Value"] = 160, ["Reward"] = "Snow"},
+            ["Index"] = 22,
+            ["Free"] = {["Reward"] = "None"}
+        },
+        ["Tier8"] = {
+            ["Premium"] = {["Value"] = 110, ["Reward"] = "Snow"},
+            ["Index"] = 8,
+            ["Free"] = {["Value"] = 65, ["Reward"] = "Snow"}
+        },
+        ["Tier9"] = {
+            ["Premium"] = {["Value"] = 115, ["Reward"] = "Snow"},
+            ["Index"] = 9,
+            ["Free"] = {["Value"] = 1, ["Reward"] = "FL"}
+        },
+        ["Tier30"] = {
+            ["Premium"] = {["Value"] = "Gingerbread Cape", ["Reward"] = "Cosmetics"},
+            ["Index"] = 30,
+            ["Free"] = {["Value"] = "Santa Scarf", ["Reward"] = "Cosmetics"}
+        },
+        ["Tier29"] = {
+            ["Premium"] = {["Value"] = "Steps", ["Reward"] = "Emotes"},
+            ["Index"] = 29,
+            ["Free"] = {["Value"] = 100, ["Reward"] = "Snow"}
+        },
+        ["Tier5"] = {
+            ["Premium"] = {["Value"] = "Santa Laugh", ["Reward"] = "Emotes"},
+            ["Index"] = 5,
+            ["Free"] = {["Value"] = "Wood", ["Reward"] = "Cards"}
+        },
+        ["Tier28"] = {
+            ["Premium"] = {["Value"] = "Presents", ["Reward"] = "GoalEffects"},
+            ["Index"] = 28,
+            ["Free"] = {["Value"] = 90, ["Reward"] = "Snow"}
+        },
+        ["Tier12"] = {
+            ["Premium"] = {["Value"] = 125, ["Reward"] = "Snow"},
+            ["Index"] = 12,
+            ["Free"] = {["Value"] = "Helicopter", ["Reward"] = "Emotes"}
+        },
+        ["Tier1"] = {
+            ["Premium"] = {["Value"] = 100, ["Reward"] = "Snow"},
+            ["Index"] = 1,
+            ["Free"] = {["Value"] = 50, ["Reward"] = "Snow"}
+        },
+        ["Tier15"] = {
+            ["Premium"] = {["Value"] = 135, ["Reward"] = "Snow"},
+            ["Index"] = 15,
+            ["Free"] = {["Value"] = 70, ["Reward"] = "Snow"}
+        },
+        ["Tier4"] = {
+            ["Premium"] = {["Value"] = 105, ["Reward"] = "Snow"},
+            ["Index"] = 4,
+            ["Free"] = {["Value"] = "Confused", ["Reward"] = "Emotes"}
+        },
+        ["Tier13"] = {
+            ["Premium"] = {["Value"] = 130, ["Reward"] = "Snow"},
+            ["Index"] = 13,
+            ["Free"] = {["Reward"] = "None"}
+        },
+        ["Tier20"] = {
+            ["Premium"] = {["Value"] = 150, ["Reward"] = "Snow"},
+            ["Index"] = 20,
+            ["Free"] = {["Value"] = 75, ["Reward"] = "Snow"}
+        },
+        ["Tier25"] = {
+            ["Premium"] = {["Value"] = 165, ["Reward"] = "Snow"},
+            ["Index"] = 25,
+            ["Free"] = {["Value"] = 80, ["Reward"] = "Snow"}
+        },
+        ["Tier14"] = {
+            ["Premium"] = {["Value"] = 135, ["Reward"] = "Snow"},
+            ["Index"] = 14,
+            ["Free"] = {["Value"] = 70, ["Reward"] = "Snow"}
+        },
+        ["Tier2"] = {
+            ["Premium"] = {["Value"] = 100, ["Reward"] = "Snow"},
+            ["Index"] = 2,
+            ["Free"] = {["Value"] = 55, ["Reward"] = "Snow"}
+        },
+        ["Tier21"] = {
+            ["Premium"] = {["Value"] = 155, ["Reward"] = "Snow"},
+            ["Index"] = 21,
+            ["Free"] = {["Value"] = 75, ["Reward"] = "Snow"}
+        },
+        ["Tier17"] = {
+            ["Premium"] = {["Value"] = 135, ["Reward"] = "Snow"},
+            ["Index"] = 17,
+            ["Free"] = {["Value"] = "Defender", ["Reward"] = "GoalEffects"}
+        },
+        ["Tier3"] = {["Premium"] = {["Value"] = 2, ["Reward"] = "LS"}, ["Index"] = 3, ["Free"] = {["Reward"] = "None"}},
+        ["Tier19"] = {
+            ["Premium"] = {["Value"] = 145, ["Reward"] = "Snow"},
+            ["Index"] = 19,
+            ["Free"] = {["Value"] = 75, ["Reward"] = "Snow"}
+        },
+        ["Tier18"] = {
+            ["Premium"] = {["Value"] = 140, ["Reward"] = "Snow"},
+            ["Index"] = 18,
+            ["Free"] = {["Reward"] = "None"}
+        },
+        ["Tier16"] = {
+            ["Premium"] = {["Value"] = "Moves", ["Reward"] = "Emotes"},
+            ["Index"] = 16,
+            ["Free"] = {["Value"] = 75, ["Reward"] = "Snow"}
+        },
+        ["Tier24"] = {
+            ["Premium"] = {["Value"] = 165, ["Reward"] = "Snow"},
+            ["Index"] = 24,
+            ["Free"] = {["Value"] = "Forest", ["Reward"] = "Cards"}
+        },
+        ["Tier10"] = {
+            ["Premium"] = {["Value"] = 120, ["Reward"] = "Snow"},
+            ["Index"] = 10,
+            ["Free"] = {["Reward"] = "None"}
+        },
+        ["Tier27"] = {
+            ["Premium"] = {["Value"] = 170, ["Reward"] = "Snow"},
+            ["Index"] = 27,
+            ["Free"] = {["Value"] = 85, ["Reward"] = "Snow"}
+        },
+        ["Tier11"] = {
+            ["Premium"] = {["Value"] = "Holiday", ["Reward"] = "Cards"},
+            ["Index"] = 11,
+            ["Free"] = {["Value"] = 65, ["Reward"] = "Snow"}
+        },
+        ["Tier7"] = {
+            ["Premium"] = {["Value"] = "Snowflakes", ["Reward"] = "GoalEffects"},
+            ["Index"] = 7,
+            ["Free"] = {["Value"] = 60, ["Reward"] = "Snow"}
+        },
+        ["Tier6"] = {
+            ["Premium"] = {["Value"] = 105, ["Reward"] = "Snow"},
+            ["Index"] = 6,
+            ["Free"] = {["Value"] = 60, ["Reward"] = "Snow"}
+        }
+    },
+    ["Icons"] = {
+        ["Flow"] = "rbxassetid://133558083095603",
+        ["LS"] = "rbxassetid://125339871021518",
+        ["Spins"] = "rbxassetid://125339871021518",
+        ["FL"] = "rbxassetid://133558083095603",
+        ["Cosmetics"] = "rbxassetid://109707434209411",
+        ["Emotes"] = "rbxassetid://133558083095603",
+        ["None"] = "rbxassetid://71873528065567",
+        ["GoalEffects"] = "rbxassetid://133558083095603",
+        ["Cards"] = "rbxassetid://109206154581451",
+        ["Snow"] = "rbxassetid://109206154581451"
+    },
+    ["ToUI"] = {
+        ["Flow"] = "Flow Spins",
+        ["LS"] = "Lucky Spins",
+        ["Spins"] = "Spins",
+        ["FL"] = "Lucky Flow Spins",
+        ["Cosmetics"] = "Cosmetic",
+        ["Emotes"] = "Emotes",
+        ["None"] = "None",
+        ["GoalEffects"] = "Goal Effect",
+        ["Cards"] = "Card",
+        ["Snow"] = "Snowflakes"
+    }
+}
