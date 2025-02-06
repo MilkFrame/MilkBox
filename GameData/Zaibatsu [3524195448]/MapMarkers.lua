77a74e13643a6ef0733cb677ab9804fd9848d41c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-07 03:14:02 +1100

    Create MapMarkers.lua

---
+++ GameData/Zaibatsu [3524195448]/MapMarkers.lua
@@ -0,0 +1,194 @@
+data = {
+    ["FastFoodRestaurant"] = {
+        ["Color"] = Color3.fromRGB(255, 193, 25),
+        ["Description"] = "Serving hungry patrons the world famous Chubbi Burger, with a side of fries.",
+        ["Name"] = "Atomic Automat",
+        ["Icon"] = "🍔"
+    },
+    ["FireDepartment"] = {
+        ["Color"] = Color3.fromRGB(255, 0, 0),
+        ["Description"] = "Home to the dedicated Firefighters ready to protect the city from the flames that threaten its harmony.",
+        ["Name"] = "Fire Department",
+        ["Icon"] = "🧑‍🚒"
+    },
+    ["Arcade"] = {
+        ["Color"] = Color3.fromRGB(181, 0, 0),
+        ["Description"] = "The best game center, acting as the ultimate haven for gamers.",
+        ["Name"] = "Grand Gamer",
+        ["Icon"] = "🕹️"
+    },
+    ["Hospital"] = {
+        ["Color"] = Color3.fromRGB(89, 225, 125),
+        ["Description"] = "State-of-the-art facilities and expert healthcare.",
+        ["Name"] = "Meitetsu Hospital",
+        ["Icon"] = "🏥"
+    },
+    ["MiddleClassApartment1"] = {
+        ["Color"] = Color3.fromRGB(155, 238, 124),
+        ["Description"] = "Modest middle class housing, with small balcony.",
+        ["Name"] = "Sakura Heights",
+        ["Icon"] = "🏠"
+    },
+    ["CapsuleHotel"] = {
+        ["Color"] = Color3.fromRGB(255, 186, 27),
+        ["Description"] = "A cozy capsule hotel offering cheap prices.",
+        ["Name"] = "5 to 9 Hotel",
+        ["Icon"] = "🛏️"
+    },
+    ["FightClub"] = {
+        ["Color"] = Color3.fromRGB(242, 33, 33),
+        ["Description"] = "Dimly-lit arena, home to the strongest muscle in Zaibatsu.",
+        ["Name"] = "Kendo Fight Club",
+        ["Icon"] = "👊"
+    },
+    ["UpperClassApartment1"] = {
+        ["Color"] = Color3.fromRGB(155, 238, 124),
+        ["Description"] = "Apartment offering beautiful sweeping views of Tokyo Bay.",
+        ["Name"] = "Panorama Tower",
+        ["Icon"] = "🏠"
+    },
+    ["CriminalSafeHouse"] = {
+        ["Color"] = Color3.fromRGB(97, 0, 0),
+        ["Description"] = "An inconspicuous building serving as a magnet for Yakuza activity.",
+        ["Name"] = "Yakuza Safehouse",
+        ["Icon"] = "🀄"
+    },
+    ["CarDealership"] = {
+        ["Color"] = Color3.fromRGB(0, 0, 0),
+        ["Description"] = "Car Dealership selling a wide variety of vehicles from cars to bikes.",
+        ["Name"] = "Quick",
+        ["Icon"] = "🚙"
+    },
+    ["CityHall"] = {
+        ["Color"] = Color3.fromRGB(186, 186, 186),
+        ["Description"] = "The administrative heartbeat of the city, where decisions are made, and the future is shaped.",
+        ["Name"] = "City Hall",
+        ["Icon"] = "🏛️"
+    },
+    ["LowerClassApartment1"] = {
+        ["Color"] = Color3.fromRGB(155, 238, 124),
+        ["Description"] = "Shoebox style apartments for cheap rent.",
+        ["Name"] = "Yokoyama Building",
+        ["Icon"] = "🏠"
+    },
+    ["PoliceHQ"] = {
+        ["Color"] = Color3.fromRGB(12, 25, 90),
+        ["Description"] = "Where dedicated officers work tirelessly to protect and serve the city.",
+        ["Name"] = "Police HQ",
+        ["Icon"] = "👮"
+    },
+    ["YokoyamaParkingArea"] = {
+        ["Color"] = Color3.fromRGB(0, 135, 255),
+        ["Description"] = "A rest stop and parking area. Spawn vehicles here.",
+        ["Name"] = "Yokoyama - PA",
+        ["Icon"] = "🅿️"
+    },
+    ["SabishiLaneParkingArea"] = {
+        ["Color"] = Color3.fromRGB(0, 135, 255),
+        ["Description"] = "A rest stop and parking area. Spawn vehicles here.",
+        ["Name"] = "Sabishi Lane - PA",
+        ["Icon"] = "🅿️"
+    },
+    ["Bank"] = {
+        ["Color"] = Color3.fromRGB(242, 197, 33),
+        ["Description"] = "Bank that offers a myriad of financial services and ensuring the stability and growth of the economy.",
+        ["Name"] = "Senshin Financial",
+        ["Icon"] = "🏦"
+    },
+    ["BeFashion"] = {
+        ["Color"] = Color3.fromRGB(27, 125, 255),
+        ["Description"] = "Upscale luxury goods and designer clothes.",
+        ["Name"] = "BE FASXION",
+        ["Icon"] = "💍"
+    },
+    ["KanonParkParkingArea"] = {
+        ["Color"] = Color3.fromRGB(0, 135, 255),
+        ["Description"] = "A rest stop and parking area. Spawn vehicles here.",
+        ["Name"] = "Kanon Park - PA",
+        ["Icon"] = "🅿️"
+    },
+    ["TechOffice"] = {
+        ["Color"] = Color3.fromRGB(33, 103, 242),
+        ["Description"] = "Office building that houses technology companies, the main resident being IBN.",
+        ["Name"] = "Intelligent Business Networks",
+        ["Icon"] = "🏢"
+    },
+    ["ZaibatsuStation"] = {
+        ["Color"] = Color3.fromRGB(242, 33, 172),
+        ["Description"] = "The main hub for rail transit in the Zaibatsu area.",
+        ["Name"] = "Zaibatsu Station",
+        ["Icon"] = "🚉"
+    },
+    ["DanceClub"] = {
+        ["Color"] = Color3.fromRGB(255, 111, 225),
+        ["Description"] = "Dance club offering the hottest tracks with amazing lighting effects.",
+        ["Name"] = "Neon Wave",
+        ["Icon"] = "💃"
+    },
+    ["WaterfrontParkingArea"] = {
+        ["Color"] = Color3.fromRGB(0, 135, 255),
+        ["Description"] = "A rest stop and parking area. Spawn vehicles here.",
+        ["Name"] = "Waterfront - PA",
+        ["Icon"] = "🅿️"
+    },
+    ["BayviewParkingArea"] = {
+        ["Color"] = Color3.fromRGB(0, 135, 255),
+        ["Description"] = "A rest stop and parking area. Spawn vehicles here.",
+        ["Name"] = "Bayview - PA",
+        ["Icon"] = "🅿️"
+    },
+    ["ConvenienceStore"] = {
+        ["Color"] = Color3.fromRGB(33, 179, 242),
+        ["Description"] = "Convenience Store with well-stocked shelves, quick bites, and a comforting array of everyday essentials.",
+        ["Name"] = "Yum Yum",
+        ["Icon"] = "🏪"
+    },
+    ["AutoMechanic"] = {
+        ["Color"] = Color3.fromRGB(242, 123, 33),
+        ["Description"] = "Gas station and Mechanic Shop offering vehicle repair and upgrade services.",
+        ["Name"] = "Autoitsu",
+        ["Icon"] = "⛽"
+    },
+    ["UpperClassApartment2"] = {
+        ["Color"] = Color3.fromRGB(155, 238, 124),
+        ["Description"] = "Luxury penthouses with the best view of the City.",
+        ["Name"] = "Serenity Skies",
+        ["Icon"] = "🏠"
+    },
+    ["Cafe"] = {
+        ["Color"] = Color3.fromRGB(242, 123, 33),
+        ["Description"] = "Cozy cafe with coffee, traditional tea, bubble tea and other sweet snacks.",
+        ["Name"] = "Bean Brew",
+        ["Icon"] = "☕"
+    },
+    ["PortParkingArea"] = {
+        ["Color"] = Color3.fromRGB(0, 135, 255),
+        ["Description"] = "A rest stop and parking area. Spawn vehicles here.",
+        ["Name"] = "Port - PA",
+        ["Icon"] = "🅿️"
+    },
+    ["JazzClub"] = {
+        ["Color"] = Color3.fromRGB(85, 33, 242),
+        ["Description"] = "A secluded jazz club with cozy ambiance, soul-stirring melodies, and smooth jazz rhythms",
+        ["Name"] = "Lonely Note",
+        ["Icon"] = "🎷"
+    },
+    ["HarajukuConnection"] = {
+        ["Color"] = Color3.fromRGB(155, 238, 124),
+        ["Description"] = "Affordable and trendy fast fashion.",
+        ["Name"] = "Harajuku Connection",
+        ["Icon"] = "👕"
+    },
+    ["BathHouse"] = {
+        ["Color"] = Color3.fromRGB(213, 205, 142),
+        ["Description"] = "A relaxing onsen, with beautiful views of Tokyo Bay.",
+        ["Name"] = "Hot Springs",
+        ["Icon"] = "♨️"
+    },
+    ["SushiRestaurant"] = {
+        ["Color"] = Color3.fromRGB(108, 141, 103),
+        ["Description"] = "Exquisite fusion of traditional craftsmanship and cutting-edge culinary techniques",
+        ["Name"] = "Kappo Sushi",
+        ["Icon"] = "🍣"
+    }
+}
