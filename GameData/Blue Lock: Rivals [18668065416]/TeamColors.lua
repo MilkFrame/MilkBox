bd6d6907aca44a0608aa910cd848bcd99894ad57
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-07 03:15:46 +1100

    Create TeamColors.lua

---
+++ GameData/Blue Lock: Rivals [18668065416]/TeamColors.lua
@@ -0,0 +1,16 @@
+data = {
+    ["Home"] = {
+        Color3.fromRGB(56, 110, 255),
+        Color3.fromRGB(29, 29, 29),
+        Color3.fromRGB(56, 110, 255),
+        Color3.fromRGB(42, 42, 42),
+        Color3.fromRGB(56, 93, 161)
+    },
+    ["Away"] = {
+        Color3.fromRGB(255, 255, 255),
+        Color3.fromRGB(255, 26, 26),
+        Color3.fromRGB(255, 255, 255),
+        Color3.fromRGB(255, 33, 251),
+        Color3.fromRGB(255, 209, 31)
+    }
+}
