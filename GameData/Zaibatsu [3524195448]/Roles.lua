0d3fc9c37a37a0a68405f4db6b79128503be800b
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-07 03:13:24 +1100

    Create Roles.lua

---
+++ GameData/Zaibatsu [3524195448]/Roles.lua
@@ -0,0 +1,91 @@
+data = {
+    ["EmergencyResponse"] = {
+        ["LayoutOrder"] = 5,
+        ["Subtitle"] = "Prepared for anything",
+        ["Name"] = "Emergency Response",
+        ["Symbol"] = "⛑️",
+        ["TeamColor"] = BrickColor.new("Medium green"),
+        ["Description"] = "Due to the collapse of the public service sector due to heavy automation, essential services such as firefighting and healthcare have been privatized, leading to a two-tiered system that favors the wealthy and leaves the poor to suffer. Firefighters charge exorbitant fees for their services. Doctors and nurses, who work for private hospitals, prioritize profits over patient care and Paramedic companies respond only to calls from those who have purchased expensive insurance plans. Even parking enforcement officers have been privatized, working for private companies that patrol the streets with ruthless efficiency, issuing tickets and fines to those who cannot afford to pay. The people who work in these essential services are forced to choose between their livelihoods and their morals."
+    },
+    ["SalaryWorker"] = {
+        ["LayoutOrder"] = 2,
+        ["Subtitle"] = "The 9 to 5 payday servants",
+        ["Name"] = "Salary Drones",
+        ["Symbol"] = "👔",
+        ["TeamColor"] = BrickColor.new("Institutional white"),
+        ["Description"] = "The dream of most citizens is to be financially stable, have a family, and to be perceived as a useful member of society. What better way to achieve this dream than to become a salaryman for a nondescript corporation of your choosing. Though the cost of this dream unfortunately includes incredible amounts of stress, long work hours, and to be at the mercy of those at the top of the corporate food chain. If you keep your head down and survive through the stress long enough, maybe you too can climb the corporate ladder. "
+    },
+    ["Civilian"] = {
+        ["LayoutOrder"] = 1,
+        ["Subtitle"] = "An ordinary individual",
+        ["Name"] = "Civilians",
+        ["Symbol"] = "👤",
+        ["AutoAssignable"] = true,
+        ["TeamColor"] = BrickColor.new("Fossil"),
+        ["Description"] = "The population that calls Zaibatsu their home is made up of a wide mix of nationalities from all around the world. To help curb to Country's population decline in recent times, the Government enacted sweeping reforms to immigration policy. This allowed foreigners to easily settle and work in special economic zones, the largest of which is none other than Zaibatsu. This status helped to make Zaibatsu the thriving cultural blend of East and Western culture that it is today, as the Country's citizens and foreigner population flock to the City in the spirit of collaboration."
+    },
+    ["Yakuza"] = {
+        ["LayoutOrder"] = 8,
+        ["Subtitle"] = "Welcome to the criminal underworld",
+        ["Name"] = "Syndicates",
+        ["Symbol"] = "👺",
+        ["TeamColor"] = BrickColor.new("Dark red"),
+        ["Description"] = "The Yakuza, Triads, Cartels and Mafias of the world have always toed a careful balance between order and chaos -but rapid transformations to society due to technology has forced criminals to evolve their strategy. Governments around the world have put tight restrictions on body augmentations that enhance physical strength or neuro-performance, or embedded weapons of any kind (outside of military use). These strict rules for civilians opened the doors for a new branch of criminal enterprise, where great profit is made in procuring, installing and performing maintenance on illicit augmentations. Due to the sophisticated nature of the technology and supply chains, criminal organizations across the globe formed ties with others in order to facilitate this underground operation. These global rings of crime became known as Syndicates."
+    },
+    ["Influencer"] = {
+        ["LayoutOrder"] = 10,
+        ["Subtitle"] = "Devoted to the spotlight",
+        ["Name"] = "Influencers",
+        ["Symbol"] = "🤳",
+        ["TeamColor"] = BrickColor.new("Carnation pink"),
+        ["Description"] = "Welcome to the world of Status. Rising stars, socialites, celebrities and even your next-door neighbor are all competing for your mind. Influencers seem to project a perfect life in the eyes of their adorers but don't let that persona fool you. Unlike the early stages of social influence, these individials compete in a high stakes game of public manipulation where only the most talented and ruthless rise to the top. Despite their own personal success, Influencers are merely tools in a larger war for your attention and consideration, where the titans of business compete to place a product in your life."
+    },
+    ["TokyoSecurity"] = {
+        ["LayoutOrder"] = 6,
+        ["Subtitle"] = "Safety and security, no matter the cost",
+        ["Name"] = "Tokyo Security",
+        ["Symbol"] = "🛡️",
+        ["TeamColor"] = BrickColor.new("Dark Royal blue"),
+        ["Description"] = "After amendments were made to the constitution to increase the Country's Defense apparatus, the quasi-public corporation Tokyo Security or “T-SEC” was formed with the mandate of policing and managing domestic public security for the entire Tokyo Bay Metropolis. T-SEC was able to streamline many processes around detainment, convictions, corrections and surveillance due to an ever-increasing budget and far less bureaucratic hurdles that traditional government agencies have. The reduction in red tape also meant paramilitarization began in many of the T-SEC departments, to better project their power over Syndicates and Civilians alike. The corruption penetrates the corporation deeply, from bribing street officers all the way up to the executives tampering with evidence to protect powerful individuals, but many people have mustered the motivation to join to defeat this corruption from within."
+    },
+    ["MetroSupport"] = {
+        ["LayoutOrder"] = 3,
+        ["Subtitle"] = "Keeping the metropolis running",
+        ["Name"] = "Metro Support",
+        ["Symbol"] = "🦺",
+        ["TeamColor"] = BrickColor.new("Electric blue"),
+        ["Description"] = "A group of skilled blue-collar workers who are the backbone of the city's infrastructure. They work tirelessly to ensure that the city remains operational and efficient. Their responsibilities include supporting the existing infrastructure, constructing new mega-projects, and demolishing slums to make way for new developments. These hard working individuals are always on the front lines, working in all kinds of weather and conditions to get the job done. They possess a wide range of skills, including engineering, welding, and heavy equipment operation, and they work together as a team to achieve their goals.The work of Metro Support is not always glamorous, but it is critical to the city's success, and their dedication and hard work ensure that the city remains a vibrant and thriving place to live and work."
+    },
+    ["Executive"] = {
+        ["LayoutOrder"] = 11,
+        ["Subtitle"] = "The puppet masters of the public",
+        ["Name"] = "Executives",
+        ["Symbol"] = "💎",
+        ["TeamColor"] = BrickColor.new("Gold"),
+        ["Description"] = "The wealthiest, and most powerful are known as 'Executives'. They make up a small sphere of affluence with the capacity for global influence. Some self-made Executives managed to rise to the top by harnessing disruptive technologies, lending an appearance of accessibility to this elite group -but make no mistake, the walls of the Aristocracy are higher than they have ever been. Fools will spend their lives trying to climb to the top only to die without ever seeing the sun. The uniting force of the Executives is the shared interest to protect their power from the forces below them and they will stop at nothing to achieve this. "
+    },
+    ["Vigilante"] = {
+        ["LayoutOrder"] = 9,
+        ["Subtitle"] = "Fight against the corruption",
+        ["Name"] = "Vigilantes",
+        ["Symbol"] = "💻",
+        ["TeamColor"] = BrickColor.new("Lime green"),
+        ["Description"] = "The new era of activism is gray. Increasing power of large international corporations and the eroding trust in governments has led some of the world's population to take justice into their hands. They operate in anonymous online groups called 'Networks'. Information exchange and coordination of networks allows for decentralized task forces to work together in shared vision based on freedom and fairness. Many vigilantes are highly skilled in the field of technology -often preferring to wage their fight in the digital realm of cyberspace. The tight-knit nature of the group and geographic distribution of members provides them with a form of security through obscurity. In the real world, the use of wearing facial coverings such as masks to keep their secret and confuse surveillance systems has become a broader symbol of the Vigilante movement."
+    },
+    ["Outsider"] = {
+        ["LayoutOrder"] = 7,
+        ["Subtitle"] = "Fallen through the cracks of the modern world",
+        ["Name"] = "Outsiders",
+        ["Symbol"] = "⛺",
+        ["TeamColor"] = BrickColor.new("Pine Cone"),
+        ["Description"] = "The outcasts of society have a hard life -much due to factors beyond their control. As the resource wars cascade throughout the world, hundreds of millions of civilians are displaced from their native lands and major regional climate failures have made it infeasible to go back for many of them. This has led to a worldwide diaspora of individuals with no nationality to their name, they roam nomadically, looking for some promise of prosperity. The already strained social systems of many cities means Outsiders are often unable to find a footing. So they choose to live away from the public's eyes in secluded areas within makeshift camps with the hope, perhaps someday they shall find a home."
+    },
+    ["Media"] = {
+        ["LayoutOrder"] = 4,
+        ["Subtitle"] = "Delivering your truth",
+        ["Name"] = "Media",
+        ["Symbol"] = "📺",
+        ["TeamColor"] = BrickColor.new("Olive"),
+        ["Description"] = "Working long hours in dimly lit studios and newsrooms, they churn out a constant stream of carefully crafted stories, images, and videos designed to manipulate public opinion and reinforce control of faceless corporations. Despite the dangers, some brave souls attempt to sneak in snippets of the truth or hidden messages of dissent, knowing that they risk severe punishment if caught. The people who work in the media industry are both cogs in the machine and pawns in the game, essential to the dystopian society's survival, but ultimately expendable. Their work is a constant reminder of the darker powers at play, that lies behind the curtain of prosperity."
+    }
+}
