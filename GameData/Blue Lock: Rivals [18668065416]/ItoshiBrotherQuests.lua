9834172f7d7894c34d49d89eae58698ace86101e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-07 03:15:16 +1100

    Create ItoshiBrotherQuests.lua

---
+++ GameData/Blue Lock: Rivals [18668065416]/ItoshiBrotherQuests.lua
@@ -0,0 +1,27 @@
+data = {
+    ["Sae"] = {
+        {
+            ["Value"] = 75,
+            ["Type"] = "Assists",
+            ["Title"] = "Perform %s Assists",
+            ["Redeem"] = "function: 0x4ad0264c96f6b5f5"
+        },
+        {["Value"] = 15, ["Type"] = "Wins", ["Title"] = "Get %s wins", ["Redeem"] = "function: 0x343e3c555a38c8c5"},
+        {
+            ["Value"] = 5,
+            ["Type"] = "Awakening",
+            ["Title"] = "Use an awakening %s times",
+            ["Redeem"] = "function: 0x3104af25017d2f95"
+        }
+    },
+    ["Rin"] = {
+        {["Value"] = 100, ["Type"] = "Goals", ["Title"] = "Score %s Goals", ["Redeem"] = "function: 0x2ae2a39ebf3792a5"},
+        {["Value"] = 15, ["Type"] = "Wins", ["Title"] = "Get %s wins", ["Redeem"] = "function: 0x2748e01a8bc84935"},
+        {
+            ["Value"] = 5,
+            ["Type"] = "Awakening",
+            ["Title"] = "Use an awakening %s times",
+            ["Redeem"] = "function: 0x63d4e297d4a22405"
+        }
+    }
+}
