daf18b303e19e0d0c44584c93aa9f823fe1d419c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-07 03:15:05 +1100

    Create Rarities.lua

---
+++ GameData/Blue Lock: Rivals [18668065416]/Rarities.lua
@@ -0,0 +1,6 @@
+data = {
+    ["Rare"] = {["Color"] = Color3.fromRGB(56, 66, 255), ["Odds"] = 62.5, ["Name"] = "Rare"}
+    ["Epic"] = {["Color"] = Color3.fromRGB(154, 38, 255), ["Odds"] = 35, ["Name"] = "Epic"},
+    ["Legendary"] = {["Color"] = Color3.fromRGB(255, 205, 11), ["Odds"] = 2, ["Name"] = "Legendary"},
+    ["Mythic"] = {["Color"] = Color3.fromRGB(255, 16, 16), ["Odds"] = 0.5, ["Name"] = "Mythic"},
+}
