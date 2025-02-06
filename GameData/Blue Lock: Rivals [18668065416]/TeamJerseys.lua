3f24475280ba7476fa54483cc228b6c60732f7c3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-07 03:15:57 +1100

    Create TeamJerseys.lua

---
+++ GameData/Blue Lock: Rivals [18668065416]/TeamJerseys.lua
@@ -0,0 +1,34 @@
+data = {
+    ["Home"] = {
+        ["1"] = {
+            ["Shirt"] = "http://www.roblox.com/asset/?id=129162105772973",
+            ["Pants"] = "http://www.roblox.com/asset/?id=75943438703044"
+        },
+        ["3"] = {
+            ["Shirt"] = "http://www.roblox.com/asset/?id=137726112091362",
+            ["Pants"] = "http://www.roblox.com/asset/?id=127356269352180"
+        },
+        ["2"] = {
+            ["Shirt"] = "http://www.roblox.com/asset/?id=114681563139440",
+            ["Pants"] = "http://www.roblox.com/asset/?id=127844994698209"
+        },
+        ["5"] = {["Shirt"] = "rbxassetid://103850297658944", ["Pants"] = "rbxassetid://126656559202917"},
+        ["4"] = {["Shirt"] = "rbxassetid://97647075631585", ["Pants"] = "rbxassetid://93230232924097"}
+    },
+    ["Away"] = {
+        ["1"] = {
+            ["Shirt"] = "http://www.roblox.com/asset/?id=107831092334592",
+            ["Pants"] = "http://www.roblox.com/asset/?id=71523813349780"
+        },
+        ["3"] = {
+            ["Shirt"] = "http://www.roblox.com/asset/?id=124933190910353",
+            ["Pants"] = "http://www.roblox.com/asset/?id=113290135124231"
+        },
+        ["2"] = {
+            ["Shirt"] = "http://www.roblox.com/asset/?id=135483864402737",
+            ["Pants"] = "http://www.roblox.com/asset/?id=134783873483548"
+        },
+        ["5"] = {["Shirt"] = "rbxassetid://129330011786102", ["Pants"] = "rbxassetid://136161071610679"},
+        ["4"] = {["Shirt"] = "rbxassetid://94775698496906", ["Pants"] = "rbxassetid://104939618583171"}
+    }
+}
