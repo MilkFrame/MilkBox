319f07037ba9efac58130c8b7747f9674d16d077
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-07 03:15:32 +1100

    Create Flows.lua

---
+++ GameData/Blue Lock: Rivals [18668065416]/Flows.lua
@@ -0,0 +1,70 @@
+data = {
+    ["Monster"] = {
+        ["Rarity"] = "Epic",
+        ["Name"] = "Monster",
+        ["Desc"] = "You get a shorter dribble cooldown & get no speed reduction when acquiring ball."
+    },
+    ["Gale Burst"] = {
+        ["Rarity"] = "Epic",
+        ["Name"] = "Gale Burst",
+        ["Desc"] = "Increases your Stamina Regeneration speed, and makes your shots quicker to release."
+    },
+    ["Ice"] = {
+        ["Rarity"] = "Rare",
+        ["Name"] = "Ice",
+        ["Desc"] = "Increased Pass strength and increases shooting strength of the player who catches your pass."
+    },
+    ["King's Instinct"] = {
+        ["Rarity"] = "Epic",
+        ["Name"] = "King's Instinct",
+        ["Desc"] = "Increases your shot power and tackles leave your opponents on the ground for longer"
+    },
+    ["Snake"] = {
+        ["Rarity"] = "Mythic",
+        ["Name"] = "Snake",
+        ["Desc"] = "Lowered tackle cooldown, shorter ragdoll time, increased speed and bigger tackle hitbox."
+    },
+    ["Genius"] = {
+        ["Rarity"] = "Epic",
+        ["Name"] = "Genius",
+        ["Desc"] = "Gives everyone on your team 30% flow on activation."
+    },
+    ["Trap"] = {
+        ["Rarity"] = "Legendary",
+        ["Name"] = "Trap",
+        ["Desc"] = [[Speed boost and shot power when receiving ball.
+-50% Trap & Dash cooldown.]]
+    },
+    ["Demon Wings"] = {
+        ["Rarity"] = "Legendary",
+        ["Name"] = "Demon Wings",
+        ["Desc"] = "Your wings increase your jump height, makes your headers stronger, and increases your shot speed."
+    },
+    ["Lightning"] = {["Rarity"] = "Rare", ["Name"] = "Lightning", ["Desc"] = "Shoot stronger than ever before!"},
+    ["Chameleon"] = {
+        ["Rarity"] = "Legendary",
+        ["Name"] = "Chameleon",
+        ["Desc"] = "-25% Cooldown on abilities when used flow is active, and increased movement speed"
+    },
+    ["Wild Card"] = {
+        ["Rarity"] = "Legendary",
+        ["Name"] = "Wild Card",
+        ["Desc"] = "Gives you a random boost (Either increased speed, Strength, or Stamina. Has a Chance to jackpot and give you all 3 buffs at once)"
+    },
+    ["Prodigy"] = {
+        ["Rarity"] = "Mythic",
+        ["Name"] = "Prodigy",
+        ["Desc"] = "Stronger shots, faster speed and basic shots turn into curved shots."
+    },
+    ["Puzzle"] = {["Rarity"] = "Rare", ["Name"] = "Puzzle", ["Desc"] = "Increases your general speed."},
+    ["Awakened Genius"] = {
+        ["Rarity"] = "Mythic",
+        ["Name"] = "Awakened Genius",
+        ["Desc"] = "Increased Dribble amount, stronger shots and 20% flow on activation."
+    },
+    ["Dribbler"] = {
+        ["Rarity"] = "Mythic",
+        ["Name"] = "Dribbler",
+        ["Desc"] = "Increases your shooting strength, makes you faster with the ball DRIBBLES LAST LONGER."
+    }
+}
