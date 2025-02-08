4d606ca545b856cc33e4ec252bef8cf2f0cd5480
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:38:33 +1100

    Create ModesData.lua

---
+++ GameData/Race Clicker [9285238704]/ModesData.lua
@@ -0,0 +1,16 @@
+data = {
+    ["EventWorld2"] = {
+        ["Name"] = "Anime City",
+        ["UnlockCallback"] = "function: 0xd24d0d0ca8ab468c",
+        ["Color"] = Color3.fromRGB(218, 41, 253),
+        ["Unlock"] = {
+            ["UpdateTriggers"] = {"Event.UnlockedPart2"},
+            ["Text"] = "You need to purchase this world to unlock!",
+            ["IsUnlocked"] = "function: 0x3f5d5e7f02c4e0ac"
+        },
+        ["PurchaseData"] = {["PriceText"] = "200K Anime Wins"}
+    },
+    ["Race"] = {["Color"] = Color3.fromRGB(255, 248, 52), ["Unlock"] = true},
+    ["Obby"] = {["Color"] = Color3.fromRGB(255, 65, 65), ["Unlock"] = "function: 0x9b91056c00a09ddc"},
+    ["Event"] = {["Color"] = Color3.fromRGB(0, 255, 127), ["Name"] = "Spring World", ["Unlock"] = true}
+}
