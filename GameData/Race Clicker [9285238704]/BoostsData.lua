02c3bc24ebb88ab5d70682fa00cc389a64ad5bd5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:36:45 +1100

    Create BoostsData.lua

---
+++ GameData/Race Clicker [9285238704]/BoostsData.lua
@@ -0,0 +1,75 @@
+data = {
+    ["X2Win"] = {
+        ["Name"] = "X2 Win",
+        ["Id"] = 1309101643,
+        ["Time"] = 900,
+        ["Icon"] = "rbxassetid://14387433159",
+        ["Color"] = Color3.fromRGB(255, 134, 0)
+    },
+    ["X3Win"] = {
+        ["Name"] = "X3 Win",
+        ["Id"] = 1309101831,
+        ["Time"] = 900,
+        ["Icon"] = "rbxassetid://14387433385",
+        ["Color"] = Color3.fromRGB(207, 5, 5)
+    },
+    ["X5Acceleration"] = {
+        ["Name"] = "X5 Acceleration",
+        ["Id"] = 1604387775,
+        ["Time"] = 900,
+        ["Icon"] = "rbxassetid://14387432098",
+        ["Color"] = Color3.fromRGB(13, 159, 251)
+    },
+    ["+100%Acceleration"] = {
+        ["Name"] = "+100% Acceleration",
+        ["Id"] = 1309102579,
+        ["Time"] = 900,
+        ["Icon"] = "rbxassetid://14387432436",
+        ["Color"] = Color3.fromRGB(13, 159, 251)
+    },
+    ["AutoClick"] = {
+        ["Name"] = "Auto Click",
+        ["Id"] = 1333691275,
+        ["Time"] = 900,
+        ["Icon"] = "rbxassetid://14397492425",
+        ["Color"] = Color3.fromRGB(255, 147, 0)
+    },
+    ["X3Luck"] = {
+        ["Name"] = "X3 Luck",
+        ["Id"] = 1309102317,
+        ["Time"] = 900,
+        ["Icon"] = "rbxassetid://14387432911",
+        ["Color"] = Color3.fromRGB(214, 12, 248)
+    },
+    ["X2Luck"] = {
+        ["Name"] = "X2 Luck",
+        ["Id"] = 1309102109,
+        ["Time"] = 900,
+        ["Icon"] = "rbxassetid://14387432681",
+        ["Color"] = Color3.fromRGB(209, 234, 10)
+    },
+    ["X2 Roll Speed"] = {
+        ["Color"] = Color3.fromRGB(255, 170, 0),
+        ["Time"] = 300,
+        ["Icon"] = "rbxassetid://125603201739985",
+        ["Name"] = "X2 Roll Speed"
+    },
+    ["X7Win"] = {
+        ["Color"] = Color3.fromRGB(66, 207, 56),
+        ["Time"] = 7200,
+        ["Icon"] = "rbxassetid://14316528975",
+        ["Name"] = "X7 Win"
+    },
+    ["X2 RNG Wins"] = {
+        ["Color"] = Color3.fromRGB(255, 0, 0),
+        ["Time"] = 300,
+        ["Icon"] = "rbxassetid://79233607961955",
+        ["Name"] = "X2 RNG Wins"
+    },
+    ["X2 RNG Luck"] = {
+        ["Color"] = Color3.fromRGB(0, 255, 127),
+        ["Time"] = 300,
+        ["Icon"] = "rbxassetid://95988724568219",
+        ["Name"] = "X2 RNG Luck"
+    }
+}
