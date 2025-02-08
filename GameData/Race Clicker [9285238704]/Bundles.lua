ca6ab4d596994f5e6cc163a8640ea1f108c6e2c0
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:41:40 +1100

    Create Bundles.lua

---
+++ GameData/Race Clicker [9285238704]/Bundles.lua
@@ -0,0 +1,56 @@
+data = {
+    ["GamepassGiftBundle"] = {
+        ["Countries"] = {"th", "ph", "vn", "id", "my", "kh"},
+        ["Contents"] = {
+            {["Name"] = "AutoClick", ["Type"] = "Gamepass"},
+            {["Name"] = "AnimeRun", ["Type"] = "Gamepass"},
+            {["Name"] = "Vip", ["Type"] = "Gamepass"},
+            {["Name"] = "SuperLuck", ["Type"] = "Gamepass"},
+            {["Name"] = "X3Hatch", ["Type"] = "Gamepass"}
+        },
+        ["Gift"] = true,
+        ["ProductId"] = 1534557899
+    },
+    ["SuperPremiumPass"] = {
+        ["Contents"] = {
+            {["Name"] = "Jolly Santa", ["Type"] = "Pet"},
+            {["Name"] = "Nutcracker", ["Type"] = "Pet"},
+            {["Name"] = "PremiumSeasonPass", ["Type"] = "Product"}
+        },
+        ["ProductId"] = 1556312103
+    },
+    ["GamepassBundle"] = {
+        ["Countries"] = {
+            "br",
+            "ph",
+            "id",
+            "ru",
+            "ar",
+            "co",
+            "eg",
+            "kz",
+            "ro",
+            "ua",
+            "ng",
+            "iq",
+            "in",
+            "my",
+            "mx",
+            "pe",
+            "cl",
+            "pt",
+            "gr",
+            "ag",
+            "ke"
+        },
+        ["Contents"] = {
+            {["Name"] = "X3Hatch", ["Type"] = "Gamepass"},
+            {["Name"] = "SuperLuck", ["Type"] = "Gamepass"},
+            {["Name"] = "Vip", ["Type"] = "Gamepass"},
+            {["Name"] = "AnimeRun", ["Type"] = "Gamepass"},
+            {["Name"] = "AutoClick", ["Type"] = "Gamepass"}
+        },
+        ["Gift"] = false,
+        ["ProductId"] = 1534543835
+    }
+}
