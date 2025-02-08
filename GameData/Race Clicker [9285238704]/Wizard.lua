cc8eb2bb652b86b2f8a66b0a20e68b5a57dbfe59
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:43:44 +1100

    Create Wizard.lua

---
+++ GameData/Race Clicker [9285238704]/Wizard.lua
@@ -0,0 +1,108 @@
+data = {
+    ["RefreshInterval"] = 86400,
+    ["Quests"] = {
+        ["HatchEggs"] = {
+            ["Odds"] = {{"I", 30}, {"II", 25}, {"III", 20}, {"IV", 15}, {"V", 10}, {"VI", 5}, {"VII", 3}, {"VIII", 2}},
+            ["Description"] = "function: 0x3e07127ce301580c",
+            ["Display"] = "Hatch Eggs",
+            ["Thumbnail"] = "rbxassetid://17659864587",
+            ["Quests"] = {
+                ["I"] = {5, [[+1 "x2 Luck Boost"]], "function: 0x63615457cf7b000c"},
+                ["II"] = {15, [[+1 "x2 Wins Boost"]], "function: 0xed8db66f0f94ddcc"},
+                ["IV"] = {50, [[+1 "Autoclick Boost"]], "function: 0x189d67224f73354c"},
+                ["VI"] = {200, [[+1 "x3 Wins Boost"]], "function: 0x8b4ea0946eb1aacc"},
+                ["VIII"] = {1000, "Boost Bundle", "function: 0x0eaa697f7b0fbeec"},
+                ["VII"] = {500, [[+1 "x3 Luck Boost"]], "function: 0x522666ec41529e8c"},
+                ["III"] = {30, [[+1 "100% Acceleration Boost"]], "function: 0xb689e889afad898c"},
+                ["V"] = {100, [[+1 "x5 Acceleration Boost"]], "function: 0xc1f1273b6cd0710c"}
+            }
+        },
+        ["PlayTime"] = {
+            ["Odds"] = {{"I", 30}, {"II", 25}, {"III", 20}, {"IV", 15}, {"V", 5}, {"VI", 3}, {"VII", 2}},
+            ["Description"] = "function: 0xa94cb8c629c5fd4c",
+            ["Display"] = "Play Time",
+            ["Thumbnail"] = "rbxassetid://17659874205",
+            ["Quests"] = {
+                ["I"] = {600, [[+1 "x2 Luck Boost"]], "function: 0x078215a38b437c8c"},
+                ["IV"] = {2700, [[+1 "Autoclick Boost"]], "function: 0x644b85fc858399cc"},
+                ["VI"] = {5400, [[+1 "x3 Wins Boost"]], "function: 0xb61b81af2905714c"},
+                ["II"] = {1200, [[+1 "x2 Wins Boost"]], "function: 0xce72890a320e004c"},
+                ["VII"] = {7200, "Boost Bundle", "function: 0x0eaa697f7b0fbeec"},
+                ["III"] = {1800, [[+1 "100% Acceleration Boost"]], "function: 0x94eb09d5e4c0c40c"},
+                ["V"] = {3600, [[+1 "x5 Acceleration Boost"]], "function: 0xdd300207d5de4d8c"}
+            }
+        },
+        ["ReachSpeed"] = {
+            ["Odds"] = {{"I", 30}, {"II", 25}, {"III", 20}, {"IV", 15}, {"V", 7}, {"VI", 3}},
+            ["Description"] = "Reach %s Speed",
+            ["Display"] = "Reach Speed",
+            ["Thumbnail"] = "rbxassetid://17659909436",
+            ["Quests"] = {
+                ["I"] = {100, [[+1 "x2 Luck Boost"]], "function: 0x885e6a3c25df008c"},
+                ["IV"] = {100000, [[+1 "Autoclick Boost"]], "function: 0xd505a36af8af55cc"},
+                ["VI"] = {10000000, [[+1 "x3 Wins Boost"]], "function: 0x2d546a38bb19bd4c"},
+                ["II"] = {1000, [[+1 "x2 Wins Boost"]], "function: 0x47e6e49b3a72cc4c"},
+                ["III"] = {10000, [[+1 "100% Acceleration Boost"]], "function: 0x3e71234390d5980c"},
+                ["V"] = {1000000, [[+1 "x5 Acceleration Boost"]], "function: 0x7439e391c26a718c"}
+            }
+        },
+        ["Wins"] = {
+            ["Odds"] = {{"I", 30}, {"II", 25}, {"III", 20}, {"IV", 15}, {"V", 7}, {"VI", 3}},
+            ["Description"] = "function: 0x6f9cea85a84c181c",
+            ["Display"] = "Gain Wins",
+            ["Thumbnail"] = "rbxassetid://17659879792",
+            ["Quests"] = {
+                ["I"] = {10, [[+1 "x2 Luck Boost"]], "function: 0x4f0b05b5804bb50c"},
+                ["IV"] = {10000, [[+1 "Autoclick Boost"]], "function: 0x9a398a5384182e4c"},
+                ["VI"] = {1000000, [[+1 "x3 Wins Boost"]], "function: 0x2c60b2a1f795b7cc"},
+                ["II"] = {100, [[+1 "x2 Wins Boost"]], "function: 0x09f28502768ee6cc"},
+                ["III"] = {1000, [[+1 "100% Acceleration Boost"]], "function: 0x4322097b5749e28c"},
+                ["V"] = {100000, [[+1 "x5 Acceleration Boost"]], "function: 0xf5cd1408e7ff7a0c"}
+            }
+        },
+        ["UseBoost"] = {
+            ["Odds"] = {{"I", 35}, {"II", 25}, {"III", 20}, {"IV", 10}, {"V", 5}, {"VI", 3}, {"VII", 2}},
+            ["Description"] = "Use %s Boosts",
+            ["Display"] = "Use Boosts",
+            ["Thumbnail"] = "rbxassetid://17659871406",
+            ["Quests"] = {
+                ["I"] = {1, [[+1 "x2 Luck Boost"]], "function: 0xf53fe4c529f7e24c"},
+                ["IV"] = {4, [[+1 "Autoclick Boost"]], "function: 0xea74154f2cf7af8c"},
+                ["VI"] = {6, [[+1 "x3 Wins Boost"]], "function: 0xb8b510704a3dd70c"},
+                ["II"] = {2, [[+1 "x2 Wins Boost"]], "function: 0x5c07269e914d260c"},
+                ["VII"] = {7, [[+1 "x3 Luck Boost"]], "function: 0xc0a9b65b638488cc"},
+                ["III"] = {3, [[+1 "100% Acceleration Boost"]], "function: 0xa4e8a937ca8a7bcc"},
+                ["V"] = {5, [[+1 "x5 Acceleration Boost"]], "function: 0x53c17669b31b134c"}
+            }
+        },
+        ["Rebirth"] = {
+            ["Odds"] = {{"I", 40}, {"II", 30}, {"III", 20}, {"IV", 7}, {"V", 3}},
+            ["Description"] = "Rebirth %i Times",
+            ["Display"] = "Rebirth",
+            ["Thumbnail"] = "rbxassetid://17659883711",
+            ["Quests"] = {
+                ["I"] = {1, [[+1 "x2 Luck Boost"]], "function: 0xa4fc10de9834138c"},
+                ["IV"] = {4, [[+1 "Autoclick Boost"]], "function: 0x30b7a8c575b044cc"},
+                ["II"] = {2, [[+1 "x2 Wins Boost"]], "function: 0xfd9874f6210fdf4c"},
+                ["III"] = {3, [[+1 "100% Acceleration Boost"]], "function: 0x1baf2aef56528b0c"},
+                ["V"] = {5, "Boost Bundle", "function: 0x0eaa697f7b0fbeec"}
+            }
+        },
+        ["WheelSpin"] = {
+            ["Odds"] = {{"I", 100}},
+            ["Description"] = "Spin The Wheel %s Time",
+            ["Display"] = "Spin The Wheel",
+            ["Thumbnail"] = "rbxassetid://17659903879",
+            ["Quests"] = {["I"] = {1, [[+2 "Wheel Spins"]], "function: 0xdd5cd70b03b77d2c"}}
+        }
+    },
+    ["Odds"] = {
+        {"UseBoost", 25},
+        {"HatchEggs", 20},
+        {"PlayTime", 20},
+        {"Wins", 15},
+        {"Rebirth", 10},
+        {"WheelSpin", 5},
+        {"ReachSpeed", 5}
+    }
+}
