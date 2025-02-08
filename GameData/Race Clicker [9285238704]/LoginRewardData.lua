77f75d7d971ae0fc2a84e6a692fce6cc7197d713
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:42:18 +1100

    Create LoginRewardData.lua

---
+++ GameData/Race Clicker [9285238704]/LoginRewardData.lua
@@ -0,0 +1,147 @@
+data = {
+    ["VipRewards"] = {
+        {
+            ["Desc"] = "100K",
+            ["Name"] = "Gouk",
+            ["Icon"] = "rbxassetid://14404468976",
+            ["Reward"] = {
+                ["IsPet"] = true,
+                ["Name"] = "Gouk",
+                ["IsProgressionPet"] = true,
+                ["Evolution"] = "Base",
+                ["Claim"] = "function: 0xe43e0dea2850e59c"
+            }
+        },
+        {
+            ["Reward"] = {["Name"] = "BoostBundle", ["Claim"] = "function: 0x13c692f5bcf83b1c"},
+            ["Icon"] = "rbxassetid://14397775499",
+            ["Name"] = "Boost Bundle",
+            ["Desc"] = "10x"
+        },
+        {
+            ["Desc"] = "x1",
+            ["Name"] = "Free Featured Egg",
+            ["Icon"] = "rbxassetid://13913355824",
+            ["Reward"] = {
+                ["IsEgg"] = true,
+                ["Claim"] = "function: 0x7bc455c4c7c7784c",
+                ["Name"] = "Featured Egg",
+                ["Amount"] = 1
+            }
+        },
+        {
+            ["Desc"] = "x5",
+            ["Name"] = "Spin Wheel",
+            ["Icon"] = "rbxassetid://13129415994",
+            ["Reward"] = {["Claim"] = "function: 0xd2704795eafe3fcc", ["Name"] = "Spin Wheel", ["Amount"] = 5}
+        },
+        {
+            ["Desc"] = "TIER 3 WINS",
+            ["Name"] = "Wins",
+            ["Icon"] = "rbxassetid://10958990792",
+            ["Reward"] = {
+                ["IsWins"] = true,
+                ["Name"] = "Wins",
+                ["GetDesc"] = "function: 0xbb57b59f171b002c",
+                ["Claim"] = "function: 0xb172b872352b1e8c",
+                ["Multiplier"] = 1337717904
+            }
+        },
+        {
+            ["Desc"] = "Velocity Insecta",
+            ["Name"] = "Velocity Insecta",
+            ["Icon"] = "rbxassetid://14388744261",
+            ["Reward"] = {["IsCar"] = true, ["Claim"] = "function: 0x7becc63cb0a36b8c", ["Name"] = "Velocity Insecta"}
+        },
+        {
+            ["Desc"] = "100K",
+            ["Name"] = "Naturo",
+            ["Icon"] = "rbxassetid://14385499854",
+            ["Reward"] = {
+                ["IsPet"] = true,
+                ["Name"] = "Naturo",
+                ["IsProgressionPet"] = true,
+                ["Evolution"] = "Base",
+                ["Claim"] = "function: 0xf3a6d9b3ffb5f2fc"
+            }
+        }
+    },
+    ["MinRebirths"] = 1,
+    ["Rewards"] = {
+        {
+            ["Desc"] = "x2",
+            ["Name"] = "2x Acceleration",
+            ["Icon"] = "rbxassetid://14387432436",
+            ["Reward"] = {
+                ["IsBoost"] = true,
+                ["Claim"] = "function: 0xe06a04544aa8482c",
+                ["Name"] = "+100%Acceleration",
+                ["Amount"] = 2
+            }
+        },
+        {
+            ["Desc"] = "x20",
+            ["Name"] = "Auto Clicker",
+            ["Icon"] = "rbxassetid://14397492425",
+            ["Reward"] = {
+                ["IsBoost"] = true,
+                ["Claim"] = "function: 0x4887e9616a1ef6dc",
+                ["Name"] = "AutoClicker",
+                ["Amount"] = 20
+            }
+        },
+        {
+            ["Desc"] = "x2",
+            ["Name"] = "3x Wins",
+            ["Icon"] = "rbxassetid://14387433385",
+            ["Reward"] = {
+                ["IsBoost"] = true,
+                ["Claim"] = "function: 0xb2e54a17758d678c",
+                ["Name"] = "X3Win",
+                ["Amount"] = 2
+            }
+        },
+        {
+            ["Desc"] = "x4",
+            ["Name"] = "3x Wins",
+            ["Icon"] = "rbxassetid://14387433385",
+            ["Reward"] = {
+                ["IsBoost"] = true,
+                ["Claim"] = "function: 0x2a3afd044173f83c",
+                ["Name"] = "X3Win",
+                ["Amount"] = 4
+            }
+        },
+        {
+            ["Desc"] = "TIER 2 WINS",
+            ["Name"] = "Wins",
+            ["Icon"] = "rbxassetid://10958990792",
+            ["Reward"] = {
+                ["IsWins"] = true,
+                ["Name"] = "Wins",
+                ["GetDesc"] = "function: 0x46110495f14b4cec",
+                ["Claim"] = "function: 0x2db1c10811bab54c",
+                ["Multiplier"] = 1337717875
+            }
+        },
+        {
+            ["Desc"] = "10x",
+            ["Name"] = "Boost Bundle",
+            ["Icon"] = "rbxassetid://14397775499",
+            ["Reward"] = {["Name"] = "BoostBundle", ["Claim"] = "function: 0x13c692f5bcf83b1c"}
+        },
+        {
+            ["Desc"] = "100K",
+            ["Name"] = "Dear Paw",
+            ["Icon"] = "rbxassetid://14385499703",
+            ["Reward"] = {
+                ["IsPet"] = true,
+                ["Name"] = "Dear Paw",
+                ["IsProgressionPet"] = true,
+                ["Evolution"] = "Base",
+                ["Claim"] = "function: 0x8370702934e276ec"
+            }
+        }
+    },
+    ["Calendar"] = 2
+}
