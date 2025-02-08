9532d3af76474e3b00136cd3b33eacd21a25deef
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:41:06 +1100

    Create TraitsData.lua

---
+++ GameData/Race Clicker [9285238704]/TraitsData.lua
@@ -0,0 +1,194 @@
+data = {
+    {
+        ["Image"] = "rbxassetid://12297588018",
+        ["Name"] = "+6% Acceleration",
+        ["Tier"] = 1,
+        ["Chance"] = 12,
+        ["Type"] = "Acceleration",
+        ["Reward"] = {["Amount"] = 0.06, ["Type"] = "Acceleration"}
+    },
+    {
+        ["Image"] = "rbxassetid://12297588018",
+        ["Name"] = "+7% Acceleration",
+        ["Tier"] = 1,
+        ["Chance"] = 9,
+        ["Type"] = "Acceleration",
+        ["Reward"] = {["Amount"] = 0.07, ["Type"] = "Acceleration"}
+    },
+    {
+        ["Image"] = "rbxassetid://12297588018",
+        ["Name"] = "+8% Acceleration",
+        ["Tier"] = 2,
+        ["Chance"] = 7,
+        ["Type"] = "Acceleration",
+        ["Reward"] = {["Amount"] = 0.08, ["Type"] = "Acceleration"}
+    },
+    {
+        ["Image"] = "rbxassetid://12297588018",
+        ["Name"] = "+11% Acceleration",
+        ["Tier"] = 3,
+        ["Chance"] = 1,
+        ["Type"] = "Acceleration",
+        ["Reward"] = {["Amount"] = 0.11, ["Type"] = "Acceleration"}
+    },
+    {
+        ["Image"] = "rbxassetid://12297685756",
+        ["Name"] = "+7% Wins",
+        ["Tier"] = 1,
+        ["Chance"] = 12,
+        ["Type"] = "Wins",
+        ["Reward"] = {["Amount"] = 0.07, ["Type"] = "Wins"}
+    },
+    {
+        ["Image"] = "rbxassetid://12297685756",
+        ["Name"] = "+8% Wins",
+        ["Tier"] = 1,
+        ["Chance"] = 9,
+        ["Type"] = "Wins",
+        ["Reward"] = {["Amount"] = 0.08, ["Type"] = "Wins"}
+    },
+    {
+        ["Image"] = "rbxassetid://12297685756",
+        ["Name"] = "+9% Wins",
+        ["Tier"] = 2,
+        ["Chance"] = 7,
+        ["Type"] = "Wins",
+        ["Reward"] = {["Amount"] = 0.09, ["Type"] = "Wins"}
+    },
+    {
+        ["Image"] = "rbxassetid://12297685756",
+        ["Name"] = "+11% Wins",
+        ["Tier"] = 3,
+        ["Chance"] = 1,
+        ["Type"] = "Wins",
+        ["Reward"] = {["Amount"] = 0.11, ["Type"] = "Wins"}
+    },
+    {
+        ["Image"] = "rbxassetid://11360976687",
+        ["Name"] = "+10% Clicks",
+        ["Tier"] = 1,
+        ["Chance"] = 12,
+        ["Type"] = "Clicks",
+        ["Reward"] = {["Amount"] = 0.1, ["Type"] = "Clicks"}
+    },
+    {
+        ["Image"] = "rbxassetid://11360976687",
+        ["Name"] = "+11% Clicks",
+        ["Tier"] = 1,
+        ["Chance"] = 9,
+        ["Type"] = "Clicks",
+        ["Reward"] = {["Amount"] = 0.11, ["Type"] = "Clicks"}
+    },
+    {
+        ["Image"] = "rbxassetid://11360976687",
+        ["Name"] = "+12% Clicks",
+        ["Tier"] = 2,
+        ["Chance"] = 7,
+        ["Type"] = "Clicks",
+        ["Reward"] = {["Amount"] = 0.12, ["Type"] = "Clicks"}
+    },
+    {
+        ["Image"] = "rbxassetid://11360976687",
+        ["Name"] = "+16% Clicks",
+        ["Tier"] = 3,
+        ["Chance"] = 1,
+        ["Type"] = "Clicks",
+        ["Reward"] = {["Amount"] = 0.16, ["Type"] = "Clicks"}
+    },
+    {
+        ["Image"] = "rbxassetid://11360976687",
+        ["Name"] = "+20% Clicks",
+        ["Tier"] = 3,
+        ["Chance"] = 1,
+        ["Type"] = "Clicks",
+        ["Reward"] = {["Amount"] = 0.2, ["Type"] = "Clicks"}
+    },
+    {
+        ["Image"] = "rbxassetid://11360976687",
+        ["Name"] = "+25% Clicks",
+        ["Tier"] = 3,
+        ["Chance"] = 1,
+        ["Type"] = "Clicks",
+        ["Reward"] = {["Amount"] = 0.25, ["Type"] = "Clicks"}
+    },
+    {
+        ["Image"] = "rbxassetid://12297685756",
+        ["Name"] = "+20% Wins",
+        ["Tier"] = 3,
+        ["Chance"] = 1,
+        ["Type"] = "Wins",
+        ["Reward"] = {["Amount"] = 0.2, ["Type"] = "Wins"}
+    },
+    {
+        ["Image"] = "rbxassetid://12297585220",
+        ["Name"] = "+6% Luck",
+        ["Tier"] = 1,
+        ["Chance"] = 11,
+        ["Type"] = "Luck",
+        ["Reward"] = {["Amount"] = 0.06, ["Type"] = "Luck"}
+    },
+    {
+        ["Image"] = "rbxassetid://12297585220",
+        ["Name"] = "+7% Luck",
+        ["Tier"] = 1,
+        ["Chance"] = 10,
+        ["Type"] = "Luck",
+        ["Reward"] = {["Amount"] = 0.07, ["Type"] = "Luck"}
+    },
+    {
+        ["Image"] = "rbxassetid://12297585220",
+        ["Name"] = "+8% Luck",
+        ["Tier"] = 2,
+        ["Chance"] = 7,
+        ["Type"] = "Luck",
+        ["Reward"] = {["Amount"] = 0.08, ["Type"] = "Luck"}
+    },
+    {
+        ["Image"] = "rbxassetid://12297585220",
+        ["Name"] = "+9% Luck",
+        ["Tier"] = 2,
+        ["Chance"] = 6,
+        ["Type"] = "Luck",
+        ["Reward"] = {["Amount"] = 0.09, ["Type"] = "Luck"}
+    },
+    {
+        ["Image"] = "rbxassetid://12297585220",
+        ["Name"] = "+14% Luck",
+        ["Tier"] = 3,
+        ["Chance"] = 1,
+        ["Type"] = "Luck",
+        ["Reward"] = {["Amount"] = 0.14, ["Type"] = "Luck"}
+    },
+    {
+        ["Image"] = "rbxassetid://12297588018",
+        ["Name"] = "-3% Acceleration",
+        ["Tier"] = 0,
+        ["Chance"] = 2,
+        ["Type"] = "Acceleration",
+        ["Reward"] = {["Amount"] = -0.03, ["Type"] = "Acceleration"}
+    },
+    {
+        ["Image"] = "rbxassetid://12297685756",
+        ["Name"] = "-2% Wins",
+        ["Tier"] = 0,
+        ["Chance"] = 2,
+        ["Type"] = "Wins",
+        ["Reward"] = {["Amount"] = -0.02, ["Type"] = "Wins"}
+    },
+    {
+        ["Image"] = "rbxassetid://11360976687",
+        ["Name"] = "-3% Clicks",
+        ["Tier"] = 0,
+        ["Chance"] = 2,
+        ["Type"] = "Clicks",
+        ["Reward"] = {["Amount"] = -0.03, ["Type"] = "Clicks"}
+    },
+    {
+        ["Image"] = "rbxassetid://12297585220",
+        ["Name"] = "-2% Luck",
+        ["Tier"] = 0,
+        ["Chance"] = 2,
+        ["Type"] = "Luck",
+        ["Reward"] = {["Amount"] = -0.02, ["Type"] = "Luck"}
+    }
+}
