20e3125046ba38d2b24575eda952da66587253b4
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:42:54 +1100

    Create EventShopData.lua

---
+++ GameData/Race Clicker [9285238704]/EventShopData.lua
@@ -0,0 +1,67 @@
+data = {
+    ["CurrencyName"] = "Snowflakes",
+    ["Active"] = true,
+    ["ResetCurrency"] = true,
+    ["CurrencyIcon"] = "rbxassetid://117870475394948",
+    ["Id"] = 24,
+    ["Rewards"] = {
+        ["DoubleWins"] = {
+            ["Title"] = "2X WINS!",
+            ["WorldExclusive"] = "Event",
+            ["Stock"] = 10,
+            ["Price"] = {["Value"] = 500, ["Currency"] = {"Event", "Currency"}},
+            ["Reward"] = {["Type"] = "Boost", ["Time"] = 900, ["Give"] = "function: 0xb9c0a5e9dbacfe4c"}
+        },
+        ["Acceleration"] = {
+            ["Title"] = "ACCELERATION BOOST",
+            ["WorldExclusive"] = "Event",
+            ["Stock"] = 20,
+            ["Price"] = {["Value"] = 3500, ["Currency"] = {"Event", "Currency"}},
+            ["Reward"] = {["Type"] = "Boost", ["Time"] = 900, ["Give"] = "function: 0xa5fd4a8d3249c90c"}
+        },
+        ["Car1"] = {
+            ["Title"] = "Snowman",
+            ["WorldExclusive"] = "Event",
+            ["Stock"] = 1,
+            ["Price"] = {["Value"] = 75000000, ["Currency"] = {"Event", "Currency"}},
+            ["Reward"] = {["Type"] = "Car", ["Name"] = "Snowman", ["Give"] = "function: 0xf5837aa75a404efc"}
+        },
+        ["TripleLuck"] = {
+            ["Title"] = "3X LUCK!",
+            ["WorldExclusive"] = "Event",
+            ["Stock"] = 5,
+            ["Price"] = {["Value"] = 2000, ["Currency"] = {"Event", "Currency"}},
+            ["Reward"] = {["Type"] = "Boost", ["Time"] = 900, ["Give"] = "function: 0xec9b26e09243c9cc"}
+        },
+        ["Trail1"] = {
+            ["Title"] = "Snowflake",
+            ["WorldExclusive"] = "Event",
+            ["Stock"] = 1,
+            ["Price"] = {["Value"] = 30000000, ["Currency"] = {"Event", "Currency"}},
+            ["Reward"] = {["Type"] = "Trail", ["Name"] = "Snowflake", ["Give"] = "function: 0xe5c040fcfe8bf3bc"}
+        },
+        ["TripleWins"] = {
+            ["Title"] = "3X WINS!",
+            ["WorldExclusive"] = "Event",
+            ["Stock"] = 5,
+            ["Price"] = {["Value"] = 1600, ["Currency"] = {"Event", "Currency"}},
+            ["Reward"] = {["Type"] = "Boost", ["Time"] = 900, ["Give"] = "function: 0xaf22d1610866374c"}
+        },
+        ["AutoClicker"] = {
+            ["Title"] = "PREMIUM AUTO CLICKER",
+            ["WorldExclusive"] = "Event",
+            ["Stock"] = 5,
+            ["Price"] = {["Value"] = 2500, ["Currency"] = {"Event", "Currency"}},
+            ["Reward"] = {["Type"] = "Boost", ["Time"] = 900, ["Give"] = "function: 0xc30f280c9eea71cc"}
+        },
+        ["DoubleLuck"] = {
+            ["Title"] = "2X LUCK!",
+            ["WorldExclusive"] = "Event",
+            ["Stock"] = 10,
+            ["Price"] = {["Value"] = 400, ["Currency"] = {"Event", "Currency"}},
+            ["Reward"] = {["Type"] = "Boost", ["Time"] = 900, ["Give"] = "function: 0xbd6266d0c040704c"}
+        }
+    },
+    ["DefaultCurrencyValue"] = 0,
+    ["DefaultCurrencyMultiplier"] = 0
+}
