45dda650a50b9418df9fd2d4829a9ac5f3ee5c9c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:43:16 +1100

    Create LimitedUGCEventData.lua

---
+++ GameData/Race Clicker [9285238704]/LimitedUGCEventData.lua
@@ -0,0 +1,88 @@
+data = {
+    ["UGCItems"] = {},
+    ["AssetId"] = 14894472891,
+    ["UGCEventNumber"] = 8,
+    ["TotalItems"] = 5000,
+    ["Tasks"] = {
+        {
+            ["Hard_Task3_1L"] = {
+                ["Index"] = 3,
+                ["Type"] = "Rebirth",
+                ["Goal"] = 21,
+                ["GetTitle"] = "function: 0x72d89baebb724d4c"
+            },
+            ["Hard_Task2_1L"] = {
+                ["Index"] = 2,
+                ["Type"] = "Rebirth",
+                ["Goal"] = 1,
+                ["GetTitle"] = "function: 0x5e234b9a339d236c"
+            },
+            ["Hard_Task1_1L"] = {
+                ["Index"] = 1,
+                ["Type"] = "Race",
+                ["Goal"] = 2000,
+                ["GetTitle"] = "function: 0x564f96bab8c4e19c"
+            }
+        },
+        {
+            ["Hard_Task3_3L"] = {
+                ["Type"] = "Hatch",
+                ["Goal"] = 250,
+                ["Index"] = 3,
+                ["ExtraData"] = {["Rarity"] = "Legendary", ["World"] = "Airport"},
+                ["GetTitle"] = "function: 0x790b19b720274d4c"
+            },
+            ["Hard_Task2_3L"] = {
+                ["Index"] = 2,
+                ["Type"] = "PlayWithoutRejoin",
+                ["Goal"] = 14400,
+                ["GetTitle"] = "function: 0x84d6cd11ee0f7fac"
+            },
+            ["Hard_Task1_3L"] = {
+                ["Index"] = 1,
+                ["Type"] = "Race",
+                ["Goal"] = 10000,
+                ["GetTitle"] = "function: 0x89073170650814dc"
+            }
+        },
+        ["ForAll"] = {
+            ["Free_Task1"] = {
+                ["Index"] = 1,
+                ["Type"] = "PlayWithoutRejoin",
+                ["Goal"] = 7200,
+                ["GetTitle"] = "function: 0xde64ad0a2b91452c"
+            },
+            ["Medium_Task1"] = {
+                ["Index"] = 1,
+                ["Type"] = "Race",
+                ["Goal"] = 2000,
+                ["GetTitle"] = "function: 0xdb6ac6df6b62cffc"
+            }
+        },
+        {
+            ["Hard_Task1_2L"] = {
+                ["Index"] = 1,
+                ["Type"] = "Race",
+                ["Goal"] = 5000,
+                ["GetTitle"] = "function: 0xafa83655e57a0b3c"
+            },
+            ["Hard_Task3_2L"] = {
+                ["Index"] = 3,
+                ["Type"] = "Rebirth",
+                ["Goal"] = 70,
+                ["GetTitle"] = "function: 0x1bbc32371091790c"
+            },
+            ["Hard_Task2_2L"] = {
+                ["Index"] = 2,
+                ["Type"] = "Rebirth",
+                ["Goal"] = 40,
+                ["GetTitle"] = "function: 0xb772d343ac37e72c"
+            }
+        }
+    },
+    ["AssetIDs"] = {
+        ["Hard"] = {["Id"] = 14894472891, ["Stock"] = 2000},
+        ["Medium"] = {["Id"] = 14894472891, ["Stock"] = 5000},
+        ["Free"] = {["Id"] = 14894472891, ["Stock"] = 10000}
+    }
+}
