f76282876512f7954c769f43bdc7c107cfe7829a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:38:04 +1100

    Create FreePetData.lua

---
+++ GameData/Race Clicker [9285238704]/FreePetData.lua
@@ -0,0 +1,28 @@
+data = {
+    ["EndsAt"] = 1764547200,
+    ["Title"] = "+100k Speed Pet",
+    ["FreePetId"] = 3,
+    ["Tasks"] = {
+        {
+            ["SpinTheWheel"] = {
+                ["Index"] = 3,
+                ["Type"] = "Wheel",
+                ["Goal"] = 1,
+                ["GetTitle"] = "function: 0x01aaa80263fa854c"
+            },
+            ["GetWins"] = {
+                ["Index"] = 2,
+                ["Type"] = "Wins",
+                ["Goal"] = 1000,
+                ["GetTitle"] = "function: 0x7ade7640e3b88e7c"
+            },
+            ["PlayForHome"] = {
+                ["Index"] = 1,
+                ["Type"] = "PlayFor",
+                ["Goal"] = 1800,
+                ["GetTitle"] = "function: 0x404fd4f30a7ac58c"
+            }
+        }
+    },
+    ["Pet"] = "Teddy Bear"
+}
