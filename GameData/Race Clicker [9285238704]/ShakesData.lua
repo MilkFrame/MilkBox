f069c1a53705decb6aa45e6cc227b4094f2e37d1
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:43:05 +1100

    Create ShakesData.lua

---
+++ GameData/Race Clicker [9285238704]/ShakesData.lua
@@ -0,0 +1,12 @@
+data = {
+    ["Shakes"] = {
+        ["Rainbow Grim's Shake"] = {["Image"] = "rbxassetid://14316528975"},
+        ["Green Grim's Shake"] = {["Image"] = "rbxassetid://14312857326"},
+        ["Red Grim's Shake"] = {["Image"] = "rbxassetid://14312858115"},
+        ["Blue Grim's Shake"] = {["Image"] = "rbxassetid://14312858468"},
+        ["Purple Grim's Shake"] = {["Image"] = "rbxassetid://14312857680"}
+    },
+    ["PurchaseCooldown"] = 3600,
+    ["MaxPurchases"] = 2,
+    ["ShakesDuration"] = 3600
+}
