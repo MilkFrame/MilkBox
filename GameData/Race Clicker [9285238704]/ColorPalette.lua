78fc244f3325343f10f4c73fb9e62765449c4993
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:37:22 +1100

    Create ColorPalette.lua

---
+++ GameData/Race Clicker [9285238704]/ColorPalette.lua
@@ -0,0 +1,15 @@
+data = {
+    ["Blue"] = Color3.fromRGB(3, 169, 244),
+    ["Cyan"] = Color3.fromRGB(24, 255, 255),
+    ["DarkGrey"] = Color3.fromRGB(91, 97, 113),
+    ["Grey"] = Color3.fromRGB(150, 150, 150),
+    ["Purple"] = Color3.fromRGB(103, 58, 183),
+    ["CashGreen"] = Color3.fromRGB(0, 230, 118),
+    ["White"] = Color3.fromRGB(255, 255, 255),
+    ["Black"] = Color3.fromRGB(60, 60, 60),
+    ["Green"] = Color3.fromRGB(76, 175, 80),
+    ["Pink"] = Color3.fromRGB(234, 128, 252),
+    ["Yellow"] = Color3.fromRGB(255, 235, 59),
+    ["Orange"] = Color3.fromRGB(255, 152, 0),
+    ["Red"] = Color3.fromRGB(255, 82, 82)
+}
