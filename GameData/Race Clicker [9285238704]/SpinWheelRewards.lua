1cf481cfea72af2798f7b1736cd916fac143be56
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:41:29 +1100

    Create SpinWheelRewards.lua

---
+++ GameData/Race Clicker [9285238704]/SpinWheelRewards.lua
@@ -0,0 +1,89 @@
+data = {
+    {
+        ["Name"] = "OP Pet",
+        ["Chance"] = 0.1,
+        ["Desc"] = "+20M",
+        ["Icon"] = "rbxassetid://112561595749606",
+        ["Reward"] = {
+            ["IsPet"] = true,
+            ["Claim"] = "function: 0x4a70e06bea61f59c",
+            ["Name"] = "Jetpack Doge",
+            ["Evolution"] = "Base"
+        }
+    },
+    {
+        ["Name"] = "2x Luck",
+        ["Chance"] = 25,
+        ["Desc"] = "15 Mins",
+        ["Icon"] = "rbxassetid://10798603871",
+        ["Reward"] = {
+            ["NumSeconds"] = 900,
+            ["Claim"] = "function: 0xfd3683089216684c",
+            ["Name"] = "X2Luck",
+            ["IsBoost"] = true
+        }
+    },
+    {
+        ["Name"] = "2x Acceleration",
+        ["Chance"] = 15.3,
+        ["Desc"] = "15 Mins",
+        ["Icon"] = "rbxassetid://10798973647",
+        ["Reward"] = {
+            ["NumSeconds"] = 900,
+            ["Claim"] = "function: 0x6c051a0a4384c2fc",
+            ["Name"] = "+100%Acceleration",
+            ["IsBoost"] = true
+        }
+    },
+    {
+        ["Name"] = "3x Luck",
+        ["Chance"] = 15,
+        ["Desc"] = "15 Mins",
+        ["Icon"] = "rbxassetid://10798864609",
+        ["Reward"] = {
+            ["NumSeconds"] = 900,
+            ["Claim"] = "function: 0x1d235b251b3573ac",
+            ["Name"] = "X3Luck",
+            ["IsBoost"] = true
+        }
+    },
+    {
+        ["Name"] = "Semi Truck",
+        ["Chance"] = 0.1,
+        ["Desc"] = "+18000% Wins",
+        ["Icon"] = "rbxassetid://73354230168957",
+        ["Reward"] = {["IsCar"] = true, ["Claim"] = "function: 0x2b9fb0494874196c", ["Name"] = "Semi Truck"}
+    },
+    {
+        ["Name"] = "Auto Click",
+        ["Chance"] = 15,
+        ["Desc"] = "15 Mins",
+        ["Icon"] = "rbxassetid://11466427676",
+        ["Reward"] = {
+            ["NumSeconds"] = 900,
+            ["Claim"] = "function: 0xae7eabd068bbe85c",
+            ["Name"] = "AutoClick",
+            ["IsBoost"] = true
+        }
+    },
+    {
+        ["Name"] = "Ornament",
+        ["Chance"] = 20,
+        ["Desc"] = "+50",
+        ["Icon"] = "rbxassetid://85852661768115",
+        ["Reward"] = {["Claim"] = "function: 0x3eb033c74c488d0c", ["Name"] = "SeasonPassTokens", ["Multiplier"] = 50}
+    },
+    {
+        ["Name"] = "Wins",
+        ["Chance"] = 9.5,
+        ["Desc"] = "TIER 3 WINS",
+        ["Icon"] = "rbxassetid://10958990792",
+        ["Reward"] = {
+            ["IsWins"] = true,
+            ["Name"] = "Wins",
+            ["GetDesc"] = "function: 0x3fd417c34c7846ac",
+            ["Claim"] = "function: 0x35f35746fffee30c",
+            ["Multiplier"] = 1337717822
+        }
+    }
+}
