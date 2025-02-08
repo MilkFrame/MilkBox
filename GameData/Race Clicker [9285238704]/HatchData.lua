11c95f8443a67394124e40a1cc0cd2a167d9306d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:38:13 +1100

    Create HatchData.lua

---
+++ GameData/Race Clicker [9285238704]/HatchData.lua
@@ -0,0 +1,18 @@
+data = {
+    ["Hatch"] = {
+        ["Rarity"] = {
+            ["Exclusive"] = {["Time"] = 3, ["Sound"] = "LegendaryPrize"},
+            ["Common"] = {["Time"] = 2, ["Sound"] = "CommonPrize"},
+            ["Mythic"] = {["Time"] = 3, ["Sound"] = "LegendaryPrize"},
+            ["Uncommon"] = {["Time"] = 2, ["Sound"] = "CommonPrize"},
+            ["Rare"] = {["Time"] = 2.5, ["Sound"] = "RarePrize"},
+            ["Legendary"] = {["Time"] = 3, ["Sound"] = "LegendaryPrize"},
+            ["Epic"] = {["Time"] = 2.5, ["Sound"] = "RarePrize"},
+            ["Secret"] = {["Time"] = 3, ["Sound"] = "LegendaryPrize"},
+            ["Mystery"] = {["Time"] = 3, ["Sound"] = "LegendaryPrize"},
+            ["Unique"] = {["Time"] = 3, ["Sound"] = "LegendaryPrize"}
+        },
+        ["Evolution"] = {["Shiny"] = {["Time"] = 3, ["Sound"] = "LegendaryPrize"}}
+    },
+    ["Timers"] = {{0.2, 0.4}, 0.4, 0.3, 0.2, 0.1, 0.075, 0.075, 0.05, 0.05, 0.05}
+}
