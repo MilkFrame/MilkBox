0dfa1c8f9e0167a4b6198439913a7d6113d28c60
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:41:18 +1100

    Create WorldsData.lua

---
+++ GameData/Race Clicker [9285238704]/WorldsData.lua
@@ -0,0 +1,347 @@
+data = {
+    ["Robot"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 18, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0x0e4c6979db365f5c"},
+        ["Order"] = 8,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.25, ["WinMultiplier"] = 144},
+            ["Obby"] = {["SpeedMultiplier"] = 0.01818181818181818, ["WinMultiplier"] = 2079},
+            ["Event"] = {["SpeedMultiplier"] = 0.25, ["WinMultiplier"] = 144}
+        },
+        ["ImageId"] = "rbxassetid://13922468972",
+        ["Color"] = Color3.fromRGB(6, 227, 227)
+    },
+    ["Castle"] = {
+        ["HasLimitedTimePets"] = true,
+        ["Unlock"] = {["Value"] = 15, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0xb2f0bc91cc90067c"},
+        ["Universe"] = "Home",
+        ["Order"] = 7,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.3333333333333333, ["WinMultiplier"] = 54},
+            ["Obby"] = {["SpeedMultiplier"] = 0.022222222222222223, ["WinMultiplier"] = 990},
+            ["Event"] = {["SpeedMultiplier"] = 0.3333333333333333, ["WinMultiplier"] = 54}
+        },
+        ["ImageId"] = "rbxassetid://13917233606",
+        ["Color"] = Color3.fromRGB(207, 207, 207)
+    },
+    ["Pool"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 125, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0x04b2fccdaff490cc"},
+        ["Order"] = 19,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.1111111111111111, ["WinMultiplier"] = 442368},
+            ["Obby"] = {["SpeedMultiplier"] = 0.013333333333333334, ["WinMultiplier"] = 4257792},
+            ["Event"] = {["SpeedMultiplier"] = 0.1111111111111111, ["WinMultiplier"] = 442368}
+        },
+        ["ImageId"] = "rbxassetid://15126018260",
+        ["Color"] = Color3.fromRGB(57, 143, 255)
+    },
+    ["Water Park"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 40, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0xbd0f6fd1a10be89c"},
+        ["Order"] = 14,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.16666666666666666, ["WinMultiplier"] = 13824},
+            ["Obby"] = {["SpeedMultiplier"] = 0.01818181818181818, ["WinMultiplier"] = 133056},
+            ["Event"] = {["SpeedMultiplier"] = 0.16666666666666666, ["WinMultiplier"] = 13824}
+        },
+        ["ImageId"] = "rbxassetid://13922466178",
+        ["Color"] = Color3.fromRGB(0, 170, 255)
+    },
+    ["Sky River"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 29, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0x1744af25d45482dc"},
+        ["Order"] = 12,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.2, ["WinMultiplier"] = 2880},
+            ["Obby"] = {["SpeedMultiplier"] = 0.01818181818181818, ["WinMultiplier"] = 33264},
+            ["Event"] = {["SpeedMultiplier"] = 0.2, ["WinMultiplier"] = 2880}
+        },
+        ["ImageId"] = "rbxassetid://13922468207",
+        ["Color"] = Color3.fromRGB(238, 61, 61)
+    },
+    ["Mine"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 200, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0x872ac58c9153a60c"},
+        ["Order"] = 25,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.2, ["WinMultiplier"] = 28311552},
+            ["Obby"] = {["SpeedMultiplier"] = 0.013333333333333334, ["WinMultiplier"] = 272498688},
+            ["Event"] = {["SpeedMultiplier"] = 0.2, ["WinMultiplier"] = 28311552}
+        },
+        ["ImageId"] = "rbxassetid://17787031265",
+        ["Color"] = Color3.fromRGB(95, 95, 95)
+    },
+    ["Hell"] = {
+        ["Universe"] = "Home",
+        ["Name"] = "Lava",
+        ["Unlock"] = {["Value"] = 9, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0x56491ba5286d64bc"},
+        ["Order"] = 5,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.3333333333333333, ["WinMultiplier"] = 18},
+            ["Obby"] = {["SpeedMultiplier"] = 0.022222222222222223, ["WinMultiplier"] = 330},
+            ["Event"] = {["SpeedMultiplier"] = 0.3333333333333333, ["WinMultiplier"] = 18}
+        },
+        ["ImageId"] = "rbxassetid://13922615827",
+        ["Color"] = Color3.fromRGB(255, 41, 34)
+    },
+    ["Home"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = true,
+        ["Order"] = 1,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 1, ["WinMultiplier"] = 1},
+            ["Obby"] = {["SpeedMultiplier"] = 1, ["WinMultiplier"] = 1},
+            ["Event"] = {["SpeedMultiplier"] = 1, ["WinMultiplier"] = 1}
+        },
+        ["ImageId"] = "rbxassetid://13922470374",
+        ["Color"] = Color3.fromRGB(128, 255, 69)
+    },
+    ["Pirate"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 12, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0x921cd2263ef6b19c"},
+        ["Order"] = 6,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.3333333333333333, ["WinMultiplier"] = 27},
+            ["Obby"] = {["SpeedMultiplier"] = 0.022222222222222223, ["WinMultiplier"] = 495},
+            ["Event"] = {["SpeedMultiplier"] = 0.3333333333333333, ["WinMultiplier"] = 27}
+        },
+        ["ImageId"] = "rbxassetid://13922646784",
+        ["Color"] = Color3.fromRGB(239, 184, 56)
+    },
+    ["Space"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 1, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0x9ea3a8c67554821c"},
+        ["Order"] = 2,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 1, ["WinMultiplier"] = 2},
+            ["Obby"] = {["SpeedMultiplier"] = 0.125, ["WinMultiplier"] = 60},
+            ["Event"] = {["SpeedMultiplier"] = 1, ["WinMultiplier"] = 2}
+        },
+        ["ImageId"] = "rbxassetid://13922467630",
+        ["Color"] = Color3.fromRGB(124, 77, 255)
+    },
+    ["Zoo World"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 100, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0x4452b17dbc4fc5ec"},
+        ["Order"] = 18,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.1111111111111111, ["WinMultiplier"] = 221184},
+            ["Obby"] = {["SpeedMultiplier"] = 0.013333333333333334, ["WinMultiplier"] = 2128896},
+            ["Event"] = {["SpeedMultiplier"] = 0.1111111111111111, ["WinMultiplier"] = 221184}
+        },
+        ["ImageId"] = "rbxassetid://14962049989",
+        ["Color"] = Color3.fromRGB(95, 226, 47)
+    },
+    ["Bank"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 70, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0x4c29ec5dd8bcd62c"},
+        ["Order"] = 16,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.1111111111111111, ["WinMultiplier"] = 55296},
+            ["Obby"] = {["SpeedMultiplier"] = 0.013333333333333334, ["WinMultiplier"] = 532224},
+            ["Event"] = {["SpeedMultiplier"] = 0.1111111111111111, ["WinMultiplier"] = 55296}
+        },
+        ["ImageId"] = "rbxassetid://14080509046",
+        ["Color"] = Color3.fromRGB(85, 255, 0)
+    },
+    ["Nuclear"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 26, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0xf5a07e95f6f9d1fc"},
+        ["HasLimitedTimePets"] = true,
+        ["Order"] = 11,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.2, ["WinMultiplier"] = 1440},
+            ["Obby"] = {["SpeedMultiplier"] = 0.01818181818181818, ["WinMultiplier"] = 16632},
+            ["Event"] = {["SpeedMultiplier"] = 0.2, ["WinMultiplier"] = 1440}
+        },
+        ["ImageId"] = "rbxassetid://13922469916",
+        ["Color"] = Color3.fromRGB(73, 238, 78)
+    },
+    ["Airport"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 85, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0x85f719eda227790c"},
+        ["Order"] = 17,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.1111111111111111, ["WinMultiplier"] = 110592},
+            ["Obby"] = {["SpeedMultiplier"] = 0.013333333333333334, ["WinMultiplier"] = 1064448},
+            ["Event"] = {["SpeedMultiplier"] = 0.1111111111111111, ["WinMultiplier"] = 110592}
+        },
+        ["ImageId"] = "rbxassetid://14572191747",
+        ["Color"] = Color3.fromRGB(127, 127, 127)
+    },
+    ["Wild West"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 185, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0xc51e901a97fb1f2c"},
+        ["Order"] = 24,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.14285714285714285, ["WinMultiplier"] = 14155776},
+            ["Obby"] = {["SpeedMultiplier"] = 0.013333333333333334, ["WinMultiplier"] = 136249344},
+            ["Event"] = {["SpeedMultiplier"] = 0.14285714285714285, ["WinMultiplier"] = 14155776}
+        },
+        ["ImageId"] = "rbxassetid://17124616498",
+        ["Color"] = Color3.fromRGB(255, 170, 0)
+    },
+    ["Underwater"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 155, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0xdd43eb3982ad896c"},
+        ["Order"] = 22,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.1111111111111111, ["WinMultiplier"] = 3538944},
+            ["Obby"] = {["SpeedMultiplier"] = 0.013333333333333334, ["WinMultiplier"] = 34062336},
+            ["Event"] = {["SpeedMultiplier"] = 0.1111111111111111, ["WinMultiplier"] = 3538944}
+        },
+        ["ImageId"] = "rbxassetid://15126029765",
+        ["Color"] = Color3.fromRGB(67, 70, 255)
+    },
+    ["Ocean"] = {
+        ["HasLimitedTimePets"] = true,
+        ["Unlock"] = {["Value"] = 3, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0x7381f2556b394afc"},
+        ["Universe"] = "Home",
+        ["Order"] = 3,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.5, ["WinMultiplier"] = 4},
+            ["Obby"] = {["SpeedMultiplier"] = 0.03333333333333333, ["WinMultiplier"] = 120},
+            ["Event"] = {["SpeedMultiplier"] = 0.5, ["WinMultiplier"] = 4}
+        },
+        ["ImageId"] = "rbxassetid://13922469399",
+        ["Color"] = Color3.fromRGB(0, 170, 255)
+    },
+    ["Candy"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 5, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0xf1e743342181fbdc"},
+        ["Order"] = 4,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.5, ["WinMultiplier"] = 7},
+            ["Obby"] = {["SpeedMultiplier"] = 0.025, ["WinMultiplier"] = 280},
+            ["Event"] = {["SpeedMultiplier"] = 0.5, ["WinMultiplier"] = 7}
+        },
+        ["ImageId"] = "rbxassetid://13917233796",
+        ["Color"] = Color3.fromRGB(230, 0, 255)
+    },
+    ["Event"] = {
+        ["Emoji"] = "❄️",
+        ["Order"] = -1,
+        ["ImageId"] = "rbxassetid://88983495667571",
+        ["IsEvent"] = true,
+        ["Universe"] = "Events",
+        ["Name"] = "Winter Wonderland",
+        ["Modes"] = {
+            ["Event"] = {["SpeedMultiplier"] = 1, ["WinMultiplier"] = 1},
+            ["Race"] = {["SpeedMultiplier"] = 1, ["WinMultiplier"] = 1}
+        },
+        ["Color"] = Color3.fromRGB(152, 246, 255),
+        ["Unlock"] = true
+    },
+    ["Forest"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 21, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0xde5bd181c1cce83c"},
+        ["HasLimitedTimePets"] = true,
+        ["Order"] = 9,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.25, ["WinMultiplier"] = 288},
+            ["Obby"] = {["SpeedMultiplier"] = 0.01818181818181818, ["WinMultiplier"] = 4158},
+            ["Event"] = {["SpeedMultiplier"] = 0.25, ["WinMultiplier"] = 288}
+        },
+        ["ImageId"] = "rbxassetid://13922470920",
+        ["Color"] = Color3.fromRGB(75, 255, 47)
+    },
+    ["Toy"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 215, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0xcaf505fcab34aeec"},
+        ["Order"] = 26,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.2, ["WinMultiplier"] = 56623104},
+            ["Obby"] = {["SpeedMultiplier"] = 0.013333333333333334, ["WinMultiplier"] = 544997376},
+            ["Event"] = {["SpeedMultiplier"] = 0.2, ["WinMultiplier"] = 56623104}
+        },
+        ["ImageId"] = "rbxassetid://119399874363460",
+        ["Color"] = Color3.fromRGB(85, 255, 255)
+    },
+    ["Circus"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 23, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0x73840c15f8616d1c"},
+        ["Order"] = 10,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.2, ["WinMultiplier"] = 720},
+            ["Obby"] = {["SpeedMultiplier"] = 0.01818181818181818, ["WinMultiplier"] = 8316},
+            ["Event"] = {["SpeedMultiplier"] = 0.2, ["WinMultiplier"] = 720}
+        },
+        ["ImageId"] = "rbxassetid://13916212736",
+        ["Color"] = Color3.fromRGB(238, 51, 79)
+    },
+    ["Coral"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 145, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0x8c9e13b188b9da8c"},
+        ["Order"] = 21,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.1111111111111111, ["WinMultiplier"] = 1769472},
+            ["Obby"] = {["SpeedMultiplier"] = 0.013333333333333334, ["WinMultiplier"] = 17031168},
+            ["Event"] = {["SpeedMultiplier"] = 0.1111111111111111, ["WinMultiplier"] = 1769472}
+        },
+        ["ImageId"] = "rbxassetid://15126028634",
+        ["Color"] = Color3.fromRGB(255, 223, 62)
+    },
+    ["Theme Park"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 31, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0xd2f8dfa5dbb24bbc"},
+        ["HasLimitedTimePets"] = true,
+        ["Order"] = 13,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.16666666666666666, ["WinMultiplier"] = 6912},
+            ["Obby"] = {["SpeedMultiplier"] = 0.01818181818181818, ["WinMultiplier"] = 66528},
+            ["Event"] = {["SpeedMultiplier"] = 0.16666666666666666, ["WinMultiplier"] = 6912}
+        },
+        ["ImageId"] = "rbxassetid://13922466699",
+        ["Color"] = Color3.fromRGB(86, 255, 67)
+    },
+    ["School"] = {
+        ["HasLimitedTimePets"] = true,
+        ["Unlock"] = {["Value"] = 55, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0x1c43b129d155af4c"},
+        ["Universe"] = "Home",
+        ["Order"] = 15,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.14285714285714285, ["WinMultiplier"] = 27648},
+            ["Obby"] = {["SpeedMultiplier"] = 0.015384615384615385, ["WinMultiplier"] = 266112},
+            ["Event"] = {["SpeedMultiplier"] = 0.14285714285714285, ["WinMultiplier"] = 27648}
+        },
+        ["ImageId"] = "rbxassetid://13922468596",
+        ["Color"] = Color3.fromRGB(255, 150, 30)
+    },
+    ["Prison"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 230, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0x3a9f718aa2aa19cc"},
+        ["Order"] = 27,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.25, ["WinMultiplier"] = 113246208},
+            ["Obby"] = {["SpeedMultiplier"] = 0.013333333333333334, ["WinMultiplier"] = 1089994752},
+            ["Event"] = {["SpeedMultiplier"] = 0.25, ["WinMultiplier"] = 113246208}
+        },
+        ["ImageId"] = "rbxassetid://111910139538617",
+        ["Color"] = Color3.fromRGB(229, 217, 216)
+    },
+    ["Iceberg"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 135, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0xbcf64a59911e23ac"},
+        ["Order"] = 20,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.1111111111111111, ["WinMultiplier"] = 884736},
+            ["Obby"] = {["SpeedMultiplier"] = 0.013333333333333334, ["WinMultiplier"] = 8515584},
+            ["Event"] = {["SpeedMultiplier"] = 0.1111111111111111, ["WinMultiplier"] = 884736}
+        },
+        ["ImageId"] = "rbxassetid://15126026033",
+        ["Color"] = Color3.fromRGB(161, 214, 255)
+    },
+    ["Atlantis"] = {
+        ["Universe"] = "Home",
+        ["Unlock"] = {["Value"] = 175, ["Stat"] = "Rebirths", ["IsUnlocked"] = "function: 0x0be1baa28404c44c"},
+        ["Order"] = 23,
+        ["Modes"] = {
+            ["Race"] = {["SpeedMultiplier"] = 0.14285714285714285, ["WinMultiplier"] = 7077888},
+            ["Obby"] = {["SpeedMultiplier"] = 0.013333333333333334, ["WinMultiplier"] = 68124672},
+            ["Event"] = {["SpeedMultiplier"] = 0.14285714285714285, ["WinMultiplier"] = 7077888}
+        },
+        ["ImageId"] = "rbxassetid://16509626678",
+        ["Color"] = Color3.fromRGB(0, 255, 255)
+    }
+}
