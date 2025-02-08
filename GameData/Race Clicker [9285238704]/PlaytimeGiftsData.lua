9f3a14946b95f911f8d02d44a5803a4245044c38
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:43:30 +1100

    Create PlaytimeGiftsData.lua

---
+++ GameData/Race Clicker [9285238704]/PlaytimeGiftsData.lua
@@ -0,0 +1,85 @@
+data = {
+    {
+        ["Rewards"] = {
+            {"function: 0xc9c34650bfae6f0c", 20},
+            {"function: 0x176bc40b5e6920cc", 20},
+            {"function: 0xf277e50eede1e4fc", 35},
+            {"function: 0xbe9a46a27f1ba48c", 10},
+            {"function: 0xe582d8da98bee84c", 15}
+        },
+        ["Thumbnail"] = {["Claimed"] = "rbxassetid://11709939996", ["Claimable"] = "rbxassetid://11709937317"},
+        ["Wait"] = 300
+    },
+    {
+        ["Rewards"] = {
+            {"function: 0x6bad34f53e653c0c", 25},
+            {"function: 0x2ce856ecdde471cc", 25},
+            {"function: 0x54c5420ff17259ac", 35},
+            {"function: 0x957df0c7fc2e958c", 15}
+        },
+        ["Thumbnail"] = {["Claimed"] = "rbxassetid://11709939996", ["Claimable"] = "rbxassetid://11709937317"},
+        ["Wait"] = 600
+    },
+    {
+        ["Rewards"] = {
+            {"function: 0x35915930e0167fcc", 25},
+            {"function: 0xa4e3216edcfcd65c", 25},
+            {"function: 0x3588134834f48a2c", 5},
+            {"function: 0x053e8c2db86d670c", 45}
+        },
+        ["Thumbnail"] = {["Claimed"] = "rbxassetid://11709949189", ["Claimable"] = "rbxassetid://11709947816"},
+        ["Wait"] = 900
+    },
+    {
+        ["Rewards"] = {
+            {"function: 0x98365f45b9a6844c", 30},
+            {"function: 0x7fd1563ee2a1d94c", 30},
+            {"function: 0x3588134834f48a2c", 7},
+            {"function: 0xba703ac0ab9f87bc", 33}
+        },
+        ["Thumbnail"] = {["Claimed"] = "rbxassetid://11709949189", ["Claimable"] = "rbxassetid://11709947816"},
+        ["Wait"] = 1200
+    },
+    {
+        ["Rewards"] = {
+            {"function: 0xc2851fdb7fe1eacc", 25},
+            {"function: 0x012d34963ae00d0c", 30},
+            {"function: 0x3588134834f48a2c", 10},
+            {"function: 0xd1bbbad34308166c", 40}
+        },
+        ["Thumbnail"] = {["Claimed"] = "rbxassetid://11709980187", ["Claimable"] = "rbxassetid://11709978220"},
+        ["Wait"] = 1800
+    },
+    {
+        ["Rewards"] = {
+            {"function: 0x13001f6cb9e8d34c", 30},
+            {"function: 0xc806d742901fbecc", 30},
+            {"function: 0x3588134834f48a2c", 10},
+            {"function: 0x5984b4ea72bae51c", 30}
+        },
+        ["Thumbnail"] = {["Claimed"] = "rbxassetid://11709980187", ["Claimable"] = "rbxassetid://11709978220"},
+        ["Wait"] = 2400
+    },
+    {
+        ["Rewards"] = {
+            {"function: 0xab9f1ee183d059cc", 21},
+            {"function: 0x731e676b7b228a8c", 20},
+            {"function: 0x3588134834f48a2c", 10},
+            {"function: 0xe1a510891a2b49cc", 48},
+            {"function: 0xda67a79043e4564c", 1}
+        },
+        ["Thumbnail"] = {["Claimed"] = "rbxassetid://11709976492", ["Claimable"] = "rbxassetid://11709974575"},
+        ["Wait"] = 3000
+    },
+    {
+        ["Rewards"] = {
+            {"function: 0x78dfd12d0d6f771c", 23},
+            {"function: 0x3ecf2638b46f120c", 20},
+            {"function: 0x3588134834f48a2c", 25},
+            {"function: 0x794b92b44a55d27c", 30},
+            {"function: 0x645460211b2aefcc", 2}
+        },
+        ["Thumbnail"] = {["Claimed"] = "rbxassetid://11709976492", ["Claimable"] = "rbxassetid://11709974575"},
+        ["Wait"] = 3600
+    }
+}
