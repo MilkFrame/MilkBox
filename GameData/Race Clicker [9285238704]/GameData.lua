9d1118a8d07aec34354cd2f1fefb7e9007aad48e
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:44:09 +1100

    Create GameData.lua

---
+++ GameData/Race Clicker [9285238704]/GameData.lua
@@ -0,0 +1,15 @@
+data = {
+    ["BanWhitelist"] = {true, true},
+    ["DevGameId"] = 3857528053,
+    ["PartnerRankId"] = 254,
+    ["GroupId"] = 14461694,
+    ["MainPlaceId"] = 9285238704,
+    ["TestersRankId"] = 200,
+    ["CommandsRankWhitelist"] = {true},
+    ["DevRankId"] = 253,
+    ["TestingGameId"] = 3820943248,
+    ["MainGameId"] = 3476371299,
+    ["CommandsRankId"] = 253,
+    ["ContributorsRankId"] = 252,
+    ["TimeOffset"] = 18000
+}
