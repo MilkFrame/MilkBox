8f08e3e7da0fed073761f80984beed6d9068eaa5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:42:29 +1100

    Create SpecialQueriesData.lua

---
+++ GameData/Race Clicker [9285238704]/SpecialQueriesData.lua
@@ -0,0 +1,15 @@
+data = {
+    ["EnteredAirportWorld"] = {
+        ["AwardMessage"] = "Airport Trail is now unlocked! Welcome to Airport World!!",
+        ["RequirementMessage"] = "Enter Airport World (Rebirth 100) To Unlock This!"
+    },
+    ["EnteredSchoolWorld"] = {
+        ["AwardMessage"] = "School Trail is now unlocked! Welcome to School World!!",
+        ["RequirementMessage"] = "Enter School World (Rebirth 55) To Unlock This!"
+    },
+    ["GotWaterParkOrcaFromPity"] = {["AwardMessage"] = "You received +1 Waterpark Orca pet!"},
+    ["EnteredWaterParkWorld"] = {
+        ["AwardMessage"] = "Aqua Trail is now unlocked! Welcome to Water Park World!",
+        ["RequirementMessage"] = "Enter Water Park World (Rebirth 40) To Unlock This!"
+    }
+}
