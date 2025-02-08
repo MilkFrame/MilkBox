a812a5dc0932a73144db317af69f9c132ff955ed
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:42:41 +1100

    Create KeymasterRewards.lua

---
+++ GameData/Race Clicker [9285238704]/KeymasterRewards.lua
@@ -0,0 +1,82 @@
+data = {
+    {
+        ["SuccessChance"] = 25,
+        ["Desc"] = "+1 Rainbow Dominus Pet",
+        ["Reward"] = {
+            ["IsPet"] = true,
+            ["Name"] = "Rainbow Dominus",
+            ["IsProgressionPet"] = true,
+            ["Evolution"] = "Base",
+            ["Claim"] = "function: 0xd613409f6d4f1c2c"
+        }
+    },
+    {
+        ["SuccessChance"] = 10,
+        ["Desc"] = "+1 Joyride Car",
+        ["Reward"] = {["IsCar"] = true, ["Claim"] = "function: 0xea3184b7df5b1dcc", ["Name"] = "Joyride"}
+    },
+    {
+        ["SuccessChance"] = 20,
+        ["Desc"] = "+1 Clown Egg",
+        ["Reward"] = {
+            ["IsEgg"] = true,
+            ["Claim"] = "function: 0x4a984d5c0b04a2ec",
+            ["Name"] = "Clown Egg",
+            ["Amount"] = 1
+        }
+    },
+    {
+        ["SuccessChance"] = 100,
+        ["Reward"] = {
+            ["NumSeconds"] = 900,
+            ["Claim"] = "function: 0xad187d014c1d218c",
+            ["Name"] = "X3Luck",
+            ["IsBoost"] = true
+        }
+    },
+    {
+        ["SuccessChance"] = 100,
+        ["Reward"] = {
+            ["NumSeconds"] = 900,
+            ["Claim"] = "function: 0x6d6b697f9d7a34ec",
+            ["Name"] = "X2Luck",
+            ["IsBoost"] = true
+        }
+    },
+    {
+        ["SuccessChance"] = 100,
+        ["Reward"] = {
+            ["NumSeconds"] = 900,
+            ["Claim"] = "function: 0x74b225503d7cbd1c",
+            ["Name"] = "X3Win",
+            ["IsBoost"] = true
+        }
+    },
+    {
+        ["SuccessChance"] = 100,
+        ["Reward"] = {
+            ["NumSeconds"] = 900,
+            ["Claim"] = "function: 0xd2f49a26d1ea21cc",
+            ["Name"] = "X2Win",
+            ["IsBoost"] = true
+        }
+    },
+    {
+        ["SuccessChance"] = 100,
+        ["Reward"] = {
+            ["NumSeconds"] = 900,
+            ["Claim"] = "function: 0x9a0769158d99ba7c",
+            ["Name"] = "+100%Acceleration",
+            ["IsBoost"] = true
+        }
+    },
+    {
+        ["SuccessChance"] = 100,
+        ["Reward"] = {
+            ["Claim"] = "function: 0xad325800f7a3ca6c",
+            ["IsWins"] = true,
+            ["Name"] = "Wins",
+            ["Multiplier"] = 50
+        }
+    }
+}
