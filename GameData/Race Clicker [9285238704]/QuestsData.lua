17e96b7f726f421ba429bd3442f2f15f75765e3d
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:42:06 +1100

    Create QuestsData.lua

---
+++ GameData/Race Clicker [9285238704]/QuestsData.lua
@@ -0,0 +1,15 @@
+data = {
+    ["Quest_Races_10"] = {
+        ["Goals"] = {
+            {["Type"] = "EggsHatched", ["Description"] = "Open 10 eggs!", ["Goal"] = 10},
+            {["Type"] = "TotalPlaytime", ["Description"] = "Play for 40 minutes!", ["Goal"] = 2400},
+            {["Type"] = "TotalPlaytime", ["Description"] = "Hatch a Legendary pet!", ["Goal"] = "Legendary"},
+            {["Type"] = "TimesUsedSpin", ["Description"] = "Use the Wheel Spin 2 times!", ["Goal"] = 2}
+        },
+        ["ID"] = "Quest_Races_10",
+        ["Prerequisites"] = {},
+        ["CompletionChecks"] = {{}},
+        ["DataEntries"] = {},
+        ["Reward"] = {["Amount"] = 1000, ["Type"] = "Wins"}
+    }
+}
