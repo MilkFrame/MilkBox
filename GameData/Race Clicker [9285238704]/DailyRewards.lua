c5c61a8309c7056a2e6af403df6d28b3e12464ad
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:37:33 +1100

    Create DailyRewards.lua

---
+++ GameData/Race Clicker [9285238704]/DailyRewards.lua
@@ -0,0 +1,231 @@
+data = {
+    ["Rewards"] = {
+        {
+            ["RewardOrder"] = 1,
+            ["RewardTime"] = 0,
+            ["RewardDisplayName"] = "Win Pack 1",
+            ["RewardCallback"] = "function: 0x6252ef4d64bb7d2c",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 2,
+            ["RewardTime"] = 86400,
+            ["RewardDisplayName"] = "1 Meme Egg",
+            ["RewardCallback"] = "function: 0xc3c400097878299c",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 3,
+            ["RewardTime"] = 172800,
+            ["RewardDisplayName"] = "Win Pack 2",
+            ["RewardCallback"] = "function: 0xfc4d19de0c2b944c",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 4,
+            ["RewardTime"] = 259200,
+            ["RewardDisplayName"] = "Win Pack 3",
+            ["RewardCallback"] = "function: 0x04dd2e1687769abc",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 5,
+            ["RewardTime"] = 345600,
+            ["RewardDisplayName"] = "200 Season XP",
+            ["RewardCallback"] = "function: 0x6ff7c5af1e40cf2c",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 6,
+            ["RewardTime"] = 432000,
+            ["RewardDisplayName"] = "2 Wheel Spins",
+            ["RewardCallback"] = "function: 0xd2e1ad1cb776f5fc",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 7,
+            ["RewardTime"] = 518400,
+            ["RewardDisplayName"] = "Robux Egg",
+            ["RewardCallback"] = "function: 0x877436e11d5839ac",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 8,
+            ["RewardTime"] = 604800,
+            ["RewardDisplayName"] = "Boost Bundle",
+            ["RewardCallback"] = "function: 0x249e378f64570cac",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 9,
+            ["RewardTime"] = 691200,
+            ["RewardDisplayName"] = "2 Accel Boosts",
+            ["RewardCallback"] = "function: 0xd1479782f82729ec",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 10,
+            ["RewardTime"] = 777600,
+            ["RewardDisplayName"] = "2 x2 Luck Boosts",
+            ["RewardCallback"] = "function: 0x90b2be42ff73f25c",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 11,
+            ["RewardTime"] = 864000,
+            ["RewardDisplayName"] = "2 AutoClick Boosts",
+            ["RewardCallback"] = "function: 0xbafb20d85153819c",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 12,
+            ["RewardTime"] = 950400,
+            ["RewardDisplayName"] = "Win Pack 2",
+            ["RewardCallback"] = "function: 0xe250a07bde28801c",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 13,
+            ["RewardTime"] = 1036800,
+            ["RewardDisplayName"] = "3 Wheel Spins",
+            ["RewardCallback"] = "function: 0xcc9e19c0bddcf75c",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 14,
+            ["RewardTime"] = 1123200,
+            ["RewardDisplayName"] = "400 Season XP",
+            ["RewardCallback"] = "function: 0xb3d8690d4c25daac",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 15,
+            ["RewardTime"] = 1209600,
+            ["RewardDisplayName"] = "3 x2 Luck Boosts",
+            ["RewardCallback"] = "function: 0x579a66354829bfec",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 16,
+            ["RewardTime"] = 1296000,
+            ["RewardDisplayName"] = "3 x2 Wins Boosts",
+            ["RewardCallback"] = "function: 0x1702cee556a4b9fc",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 17,
+            ["RewardTime"] = 1382400,
+            ["RewardDisplayName"] = "3 x3 Luck Boosts",
+            ["RewardCallback"] = "function: 0x78f6d766af770dac",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 18,
+            ["RewardTime"] = 1468800,
+            ["RewardDisplayName"] = "Win Pack 1",
+            ["RewardCallback"] = "function: 0x5e9bff966072809c",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 19,
+            ["RewardTime"] = 1555200,
+            ["RewardDisplayName"] = "Win Pack 2",
+            ["RewardCallback"] = "function: 0x5829b878e386936c",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 20,
+            ["RewardTime"] = 1641600,
+            ["RewardDisplayName"] = "4 Wheel Spins",
+            ["RewardCallback"] = "function: 0x178e945e63a0a90c",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 21,
+            ["RewardTime"] = 1728000,
+            ["RewardDisplayName"] = "+50 Pet Inventory",
+            ["RewardCallback"] = "function: 0xb242217db06dc0cc",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 22,
+            ["RewardTime"] = 1814400,
+            ["RewardDisplayName"] = "600 Season XP",
+            ["RewardCallback"] = "function: 0x988ca83697719c0c",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 23,
+            ["RewardTime"] = 1900800,
+            ["RewardDisplayName"] = "4 Wheel Spins",
+            ["RewardCallback"] = "function: 0xd9998dff184eaa7c",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 24,
+            ["RewardTime"] = 1987200,
+            ["RewardDisplayName"] = "4 x3 Luck Boosts",
+            ["RewardCallback"] = "function: 0xe8e8f5a4851ac0ec",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 25,
+            ["RewardTime"] = 2073600,
+            ["RewardDisplayName"] = "Win Pack 3",
+            ["RewardCallback"] = "function: 0x9ee33075f51e9abc",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 26,
+            ["RewardTime"] = 2160000,
+            ["RewardDisplayName"] = "3 x5 Acceleration Boosts",
+            ["RewardCallback"] = "function: 0x4b2acb3f172ef5fc",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 27,
+            ["RewardTime"] = 2246400,
+            ["RewardDisplayName"] = "Boost Bundle",
+            ["RewardCallback"] = "function: 0x2b5fa3809c7c146c",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 28,
+            ["RewardTime"] = 2332800,
+            ["RewardDisplayName"] = "800 Season XP",
+            ["RewardCallback"] = "function: 0xf94a83cc18811edc",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 29,
+            ["RewardTime"] = 2419200,
+            ["RewardDisplayName"] = "Win Pack 4",
+            ["RewardCallback"] = "function: 0x717c406db98f0cac",
+            ["RewardIcon"] = ""
+        },
+        {
+            ["RewardOrder"] = 30,
+            ["RewardTime"] = 2505600,
+            ["RewardDisplayName"] = "+1 Pet Equip",
+            ["RewardCallback"] = "function: 0x7a4979a456df29ec",
+            ["RewardIcon"] = ""
+        }
+    },
+    ["MissingDayTime"] = 86400,
+    ["AfterClaimedRewards"] = {
+        {
+            ["RewardOrder"] = 21,
+            ["RewardTime"] = 1728000,
+            ["RewardDisplayName"] = "Win Pack 3",
+            ["RewardCallback"] = "function: 0x06d955e85f2bf25c",
+            ["RewardIcon"] = "rbxassetid://11379636872"
+        },
+        {
+            ["RewardOrder"] = 30,
+            ["RewardTime"] = 2505600,
+            ["RewardDisplayName"] = "Win Pack 4",
+            ["RewardCallback"] = "function: 0x2ce57532ff3b819c",
+            ["RewardIcon"] = "rbxassetid://11379636872"
+        }
+    }
+}
