9397a9eca049aa04f8468c16459164926abf86c6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-09 03:39:54 +1100

    Create NitrosData.lua

---
+++ GameData/Race Clicker [9285238704]/NitrosData.lua
@@ -0,0 +1,108 @@
+data = {
+    ["Nitros"] = {
+        ["Pink"] = {
+            ["Acceleration"] = 32,
+            ["WinsPerLevel"] = 0.2,
+            ["PricePerLevel"] = 0.25,
+            ["Order"] = 6,
+            ["Wins"] = 64,
+            ["Image"] = "rbxassetid://13376155211",
+            ["Name"] = "Pink",
+            ["Price"] = {["Value"] = 250000000, ["Currency"] = "Wins"},
+            ["AccelerationPerLevel"] = 0.2,
+            ["MaxUpgrades"] = 2,
+            ["AccentColor"] = "0 1 0.145098 0.858824 0 1 1 0.145098 0.858824 0 "
+        },
+        ["Purple"] = {
+            ["Acceleration"] = 64,
+            ["WinsPerLevel"] = 0.2,
+            ["PricePerLevel"] = 0.25,
+            ["Order"] = 7,
+            ["Wins"] = 128,
+            ["Image"] = "rbxassetid://13376155211",
+            ["Name"] = "Purple",
+            ["Price"] = {["Value"] = 1000000000, ["Currency"] = "Wins"},
+            ["AccelerationPerLevel"] = 0.2,
+            ["MaxUpgrades"] = 2,
+            ["AccentColor"] = "0 0.560784 0.0627451 1 0 1 0.560784 0.0627451 1 0 "
+        },
+        ["Blue"] = {
+            ["Acceleration"] = 2,
+            ["WinsPerLevel"] = 0.2,
+            ["PricePerLevel"] = 0.25,
+            ["Order"] = 2,
+            ["Wins"] = 4,
+            ["Image"] = "rbxassetid://13376155211",
+            ["Name"] = "Blue",
+            ["Price"] = {["Value"] = 500000, ["Currency"] = "Wins"},
+            ["AccelerationPerLevel"] = 0.2,
+            ["MaxUpgrades"] = 2,
+            ["AccentColor"] = "0 0.0313726 0.533333 1 0 1 0.0313726 0.533333 1 0 "
+        },
+        ["Green"] = {
+            ["Acceleration"] = 4,
+            ["WinsPerLevel"] = 0.2,
+            ["PricePerLevel"] = 0.25,
+            ["Order"] = 3,
+            ["Wins"] = 8,
+            ["Image"] = "rbxassetid://13376155211",
+            ["Name"] = "Green",
+            ["Price"] = {["Value"] = 2500000, ["Currency"] = "Wins"},
+            ["AccelerationPerLevel"] = 0.2,
+            ["MaxUpgrades"] = 2,
+            ["AccentColor"] = "0 0.0980392 1 0.231373 0 1 0.0980392 1 0.231373 0 "
+        },
+        ["Rainbow"] = {
+            ["Acceleration"] = 128,
+            ["WinsPerLevel"] = 0.2,
+            ["PricePerLevel"] = 0.25,
+            ["Order"] = 8,
+            ["Wins"] = 256,
+            ["Image"] = "rbxassetid://13376155211",
+            ["Name"] = "Rainbow",
+            ["Price"] = {["Value"] = 10000000000, ["Currency"] = "Wins"},
+            ["AccelerationPerLevel"] = 0.2,
+            ["MaxUpgrades"] = 2,
+            ["AccentColor"] = "0 0.996078 0.0392157 0 0 0.16 1 0.615686 0 0 0.318 1 1 0 0 0.48 0 1 0 0 0.64 0 0.666667 1 0 0.941 1 0.333333 1 0 1 0.666667 0 1 0 "
+        },
+        ["Yellow"] = {
+            ["Acceleration"] = 8,
+            ["WinsPerLevel"] = 0.2,
+            ["PricePerLevel"] = 0.25,
+            ["Order"] = 4,
+            ["Wins"] = 16,
+            ["Image"] = "rbxassetid://13376155211",
+            ["Name"] = "Yellow",
+            ["Price"] = {["Value"] = 10000000, ["Currency"] = "Wins"},
+            ["AccelerationPerLevel"] = 0.2,
+            ["MaxUpgrades"] = 2,
+            ["AccentColor"] = "0 1 0.862745 0.0784314 0 1 1 0.862745 0.0784314 0 "
+        },
+        ["Red"] = {
+            ["Acceleration"] = 16,
+            ["WinsPerLevel"] = 0.2,
+            ["PricePerLevel"] = 0.25,
+            ["Order"] = 5,
+            ["Wins"] = 32,
+            ["Image"] = "rbxassetid://13376155211",
+            ["Name"] = "Red",
+            ["Price"] = {["Value"] = 50000000, ["Currency"] = "Wins"},
+            ["AccelerationPerLevel"] = 0.2,
+            ["MaxUpgrades"] = 2,
+            ["AccentColor"] = "0 1 0.0588235 0.184314 0 1 1 0.0588235 0.184314 0 "
+        },
+        ["White"] = {
+            ["Acceleration"] = 1,
+            ["WinsPerLevel"] = 0.2,
+            ["PricePerLevel"] = 0.25,
+            ["Order"] = 1,
+            ["Wins"] = 2,
+            ["Image"] = "rbxassetid://13376155211",
+            ["Name"] = "White",
+            ["Price"] = {["Value"] = 250000, ["Currency"] = "Wins"},
+            ["AccelerationPerLevel"] = 0.2,
+            ["MaxUpgrades"] = 2,
+            ["AccentColor"] = "0 1 1 1 0 1 1 1 1 0 "
+        }
+    }
+}
