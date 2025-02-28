2f4f4bc95a8cdcb2d467f28d36fce48a01cfc377
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-28 23:44:54 +1100

    Create Items.lua

---
+++ GameData/Something evil will happen [16991287194]/Items.lua
@@ -0,0 +1,145 @@
+data = {
+    ["Medkit"] = {
+        ["Price"] = 650,
+        ["Description"] = "A medical kit for all your medical needs! Heals 100% of your (or a friends) health after a short delay. 2 uses per round.",
+        ["Icon"] = "rbxassetid://117644777659323"
+    },
+    ["Banana Peel"] = {
+        ["Price"] = 250,
+        ["Description"] = "Throw this on the ground and watch people fall over like an old slapstick cartoon. 6 uses per round.",
+        ["Icon"] = "rbxassetid://103522815581412"
+    },
+    ["Snowball"] = {
+        ["Price"] = 1,
+        ["Icon"] = "rbxassetid://132633387850995",
+        ["Description"] = "'Tis the season! Slows down and obscures the vision of players who are hit. Can also extinguish them. 5 uses per round.",
+        ["Secret"] = true
+    },
+    ["Slateskin Potion"] = {
+        ["Price"] = 1500,
+        ["Description"] = "Grants you (or a friend) an 8 second immunity to fall damage & being knocked out. 3 uses per round.",
+        ["Icon"] = "http://www.roblox.com/asset/?id=11448091"
+    },
+    ["Epic Brew"] = {
+        ["Price"] = 400,
+        ["Icon"] = "rbxassetid://85311814389146",
+        ["Description"] = "Infused with pure radium. Grants you or a friend a random positive or negative status effect and heals 10% of your max health. 6 uses per round.",
+        ["New"] = true
+    },
+    ["Slap Hand"] = {
+        ["Price"] = 1250,
+        ["Description"] = "Good for giving your friends a good ol' pat on the back! Ragdolls players upon hit.",
+        ["Icon"] = "rbxassetid://131307586168176"
+    },
+    ["Teddy Bloxpin"] = {
+        ["Price"] = 700,
+        ["Icon"] = "rbxassetid://72051492906903",
+        ["Description"] = "He wuvs you! Sharable teddy bear with a 50/50 chance to heal 50% of max health or explode. 4 uses per round.",
+        ["New"] = true
+    },
+    ["Subspace Tripmine"] = {
+        ["Price"] = 650,
+        ["Description"] = "This blastmine uses hyper-advanced cloaking technology with a proximity trigger. 3 uses per round.",
+        ["Icon"] = "rbxassetid://86213378191739"
+    },
+    ["Water Balloon"] = {
+        ["Price"] = 350,
+        ["Description"] = "Summer's #1 fun balloon. Can extinguish fires and knock your friends out. 5 uses per round.",
+        ["Icon"] = "rbxassetid://94116646312121"
+    },
+    ["Hot Chocolate"] = {
+        ["Price"] = 350,
+        ["Description"] = "Keep warm on the coldest of days! Grants slow health regen and immunity to freezing to you (or a friend) for 8 seconds. 3 uses per round.",
+        ["Icon"] = "rbxassetid://86703749371715"
+    },
+    ["Crowbar"] = {
+        ["Price"] = 750,
+        ["Description"] = "Tear down structures brick by brick. Can also be used to break ice. Does not deal damage.",
+        ["Icon"] = "rbxassetid://116078691269080"
+    },
+    ["Dynamite"] = {
+        ["Price"] = 650,
+        ["Description"] = "Throwable bundle of dynamite that explodes after a short fuse. 3 uses per round.",
+        ["Icon"] = "rbxassetid://97934140327822"
+    },
+    ["Gravity Pad"] = {
+        ["Price"] = 750,
+        ["Description"] = "Buildable structure that will bounce players upwards and grant a low-gravity effect. 4 uses per round.",
+        ["Icon"] = "rbxassetid://128542585199482"
+    },
+    ["Regen Coil"] = {
+        ["Price"] = 950,
+        ["Description"] = "A magical coil that grants wielders significant health regen, which can be infinitely reused after a cooldown.",
+        ["Icon"] = "rbxassetid://132514741925091"
+    },
+    ["Stun Gun"] = {
+        ["Price"] = 2000,
+        ["Description"] = "Give your friends a good ol' shock! Applies Shocked to hit players. 3 uses per round.",
+        ["Icon"] = "rbxassetid://129984128725973"
+    },
+    ["Gravity Coil"] = {
+        ["Price"] = 1200,
+        ["Description"] = "A magical coil that grants wielders a lower gravitational pull. Depletes stamina when used.",
+        ["Icon"] = "rbxassetid://130099306585894"
+    },
+    ["Floatie"] = {
+        ["Price"] = 125,
+        ["Description"] = "Allows you swim in water. Click in order to swim whilst underwater. Unequip to replenish.",
+        ["Icon"] = "rbxassetid://96897223253740"
+    },
+    ["Speed Coil"] = {
+        ["Price"] = 1000,
+        ["Description"] = "A magical coil that grants wielders a faster sprint and walk speed. Increases stamina usage when sprinting.",
+        ["Icon"] = "rbxassetid://84826403094464"
+    },
+    ["Fusion Coil"] = {
+        ["Price"] = 2000,
+        ["Description"] = "A magical coil that contains slightly less powerful effects of all 3 coils. Depletes stamina when used.",
+        ["Icon"] = "rbxassetid://116785467915381"
+    },
+    ["Fire Extinguisher"] = {
+        ["Price"] = 400,
+        ["Description"] = "Capable of fighting fires and extinguishing yourself or other players. Unequip to replenish.",
+        ["Icon"] = "rbxassetid://110218416939963"
+    },
+    ["Throwing Spork"] = {
+        ["Price"] = 25,
+        ["Description"] = "Most powerful utensil on the planet. Does nothing special.",
+        ["Icon"] = "rbxassetid://72408159060454"
+    },
+    ["Timebomb"] = {
+        ["Price"] = 350,
+        ["Description"] = "A simple bomb that explodes after a short fuse. 5 uses per round.",
+        ["Icon"] = "rbxassetid://99206111474460"
+    },
+    ["Umbrella"] = {
+        ["Price"] = 350,
+        ["Description"] = "A large umbrella that slows your fall when held. Able to withstand acid for a short time.",
+        ["Icon"] = "rbxassetid://85984377560880"
+    },
+    ["Bloxiade"] = {
+        ["Price"] = 125,
+        ["Description"] = "It's important to stay hydrated with Bloxiade! Grants you (or a friend) unlimited stamina for 5 seconds. 3 uses per round.",
+        ["Icon"] = "rbxassetid://87969341807192"
+    },
+    ["Bloxy Cola"] = {
+        ["Price"] = 125,
+        ["Description"] = "The classic Bloxy Cola flavor, instantly heal 25% of your (or a friends) health. Refreshing! 3 uses per round.",
+        ["Icon"] = "rbxassetid://112160845917986"
+    },
+    ["Molotov"] = {
+        ["Price"] = 950,
+        ["Description"] = "A throwable bottle that explodes into a fiery inferno upon impact. 3 uses per round.",
+        ["Icon"] = "rbxassetid://121177818475752"
+    },
+    ["Exploding Pie"] = {
+        ["Price"] = 750,
+        ["Description"] = "A yummy cherry pie that sticks to players and objects. Explodes after a short fuse, won't deal direct damage. 4 uses per round.",
+        ["Icon"] = "rbxassetid://124823857383653"
+    },
+    ["Superball"] = {
+        ["Price"] = 1150,
+        ["Description"] = "A powerful ball capable of causing blunt-force trauma. Can bounce and hit you if you aren't careful. 4 uses per round.",
+        ["Icon"] = "rbxassetid://119877023240619"
+    }
+}
