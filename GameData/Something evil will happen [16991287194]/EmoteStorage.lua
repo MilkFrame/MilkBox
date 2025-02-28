48bbbb1260647bee1676c8d08d0cc64886bc79c5
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-02-28 23:45:16 +1100

    Create EmoteStorage.lua

---
+++ GameData/Something evil will happen [16991287194]/EmoteStorage.lua
@@ -0,0 +1,333 @@
+data = {
+    ["CoolSpin"] = {
+        ["Price"] = 300,
+        ["Mobile"] = true,
+        ["Name"] = "Cool Spin",
+        ["Init"] = "function: 0xcb4ca905bae0678a",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "Yay!  [Traversal]",
+        ["Credits"] = "Animation - iluvoatml22."
+    },
+    ["LoveHate"] = {
+        ["Price"] = 500,
+        ["Mobile"] = false,
+        ["Name"] = "Love Hate",
+        ["Init"] = "function: 0x548e5adfa60d5d4a",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "how he movin like dat",
+        ["Credits"] = "Original - Love Hate by XavierSoBased. Animation - sorakai. Remix - sorakai"
+    },
+    ["Caramelldansen"] = {
+        ["Description"] = "Oo oo oa oa aa...",
+        ["Init"] = "function: 0x1172d0aa6b98444a",
+        ["Animations"] = {"Animation"},
+        ["Mobile"] = false,
+        ["Name"] = "Caramelldansen",
+        ["Looping"] = false,
+        ["Price"] = 550,
+        ["New"] = true,
+        ["Credits"] = "Animation - iluvoatml22. Remix - sorakai"
+    },
+    ["Yorokonde"] = {
+        ["Price"] = 700,
+        ["Mobile"] = false,
+        ["Name"] = "Giri Giri Dansu",
+        ["Init"] = "function: 0xf9a559dbf2109d2a",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "（もっと鳴らせ）!",
+        ["Credits"] = "Original - Hai Yorokonde. Animation - iluvoatml22. Remix - vomic"
+    },
+    ["DieLit"] = {
+        ["Description"] = "Homer",
+        ["Init"] = "function: 0xb01e112b27f1346a",
+        ["Animations"] = {"Animation"},
+        ["Mobile"] = false,
+        ["Name"] = "Die Lit",
+        ["Looping"] = false,
+        ["Price"] = 400,
+        ["New"] = true,
+        ["Credits"] = "Animation - iluvoatml22 & sorakai. Remix - sorakai."
+    },
+    ["HearAttack"] = {
+        ["Price"] = 300,
+        ["Mobile"] = false,
+        ["Name"] = "Hear Attack",
+        ["Init"] = "function: 0x99c7ea8fb93dd08a",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "im having a hear attack",
+        ["Credits"] = "Animation - iluvoatml22."
+    },
+    ["Scary"] = {
+        ["Price"] = 4321,
+        ["Mobile"] = true,
+        ["Name"] = "Scary",
+        ["Init"] = "function: 0xb0998fc20e07daea",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = true,
+        ["Description"] = "They're coming...  [Traversal]",
+        ["Credits"] = "Animation - Him."
+    },
+    ["WhamWham"] = {
+        ["Price"] = 500,
+        ["Mobile"] = true,
+        ["Name"] = "Wham Wham",
+        ["Init"] = "function: 0xc0c7350ecb476a0a",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "Gap Gap Wap Wap, Wham!  [Traversal]",
+        ["Credits"] = "Original - Wham Wham. Animation - sorakai."
+    },
+    ["ChillSit"] = {
+        ["Mobile"] = false,
+        ["Name"] = "Chill Out",
+        ["Price"] = 300,
+        ["Animations"] = {"Animation"},
+        ["Looping"] = true,
+        ["Description"] = "These disasters are simply a chore! Heh!",
+        ["Credits"] = "Animation - iluvoatml22."
+    },
+    ["Territory"] = {
+        ["Price"] = 350,
+        ["Mobile"] = false,
+        ["Name"] = "Territory",
+        ["Init"] = "function: 0xcbff52c8135eaa4a",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "I'm an idol, my hair isn't drills!",
+        ["Credits"] = "Original - Teto Territory. Animation - sorakai & iluvoatml22. Remix - sorakai"
+    },
+    ["PartyHips"] = {
+        ["Price"] = 300,
+        ["Mobile"] = false,
+        ["Name"] = "Party Hips",
+        ["Init"] = "function: 0x5f8a548756c8514a",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "those who no :skull:",
+        ["Credits"] = "Original - Fortnite. Animation - sorakai. Remix - sorakai."
+    },
+    ["FollowMe"] = {
+        ["Mobile"] = false,
+        ["Name"] = "Follow Me",
+        ["Price"] = 1,
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "Come heal me!!!",
+        ["Credits"] = "Animation - iluvoatml22."
+    },
+    ["ByeByeBye"] = {
+        ["Price"] = 700,
+        ["Mobile"] = false,
+        ["Name"] = "Bye Bye Bye",
+        ["Init"] = "function: 0xf4688d3d62d271ea",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "It ain't no lie.",
+        ["Credits"] = "Animation - iluvoatml22. Remix - sorakai"
+    },
+    ["WhatUWant"] = {
+        ["Price"] = 700,
+        ["Mobile"] = false,
+        ["Name"] = "What U Want",
+        ["Init"] = "function: 0x657492851c0bdf8a",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "this'll show 'em. Yeah.",
+        ["Credits"] = "Animation - sorakai & iluvoatml22. Remix - sorakai."
+    },
+    ["ChampionofLight"] = {
+        ["Price"] = 500,
+        ["Mobile"] = false,
+        ["Name"] = "Champion of Light",
+        ["Init"] = "function: 0x27350d385b9d716a",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "And mama gave me a magic clicker! Well yes, I think it's true and fair to say.",
+        ["Credits"] = "Original - Herald of Darkness. Animation - iluvoatml22. Remix - sorakai"
+    },
+    ["logansbirthday"] = {
+        ["Description"] = "hip horray",
+        ["Init"] = "function: 0x4cb5ddc5e77b662a",
+        ["Animations"] = {"Animation"},
+        ["Secret"] = true,
+        ["Mobile"] = false,
+        ["Name"] = "Logans Birthday111",
+        ["Looping"] = false,
+        ["Price"] = 999,
+        ["Credits"] = "Original - Hai Yorokonde. Animation - iluvoatml22. Remix - vomic"
+    },
+    ["LaidBackShuffle"] = {
+        ["Price"] = 700,
+        ["Mobile"] = false,
+        ["Name"] = "Laid Back Shuffle",
+        ["Init"] = "function: 0x3a3f951201ee6e8a",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "Watch and learn buck-oh!",
+        ["Credits"] = "Animation - sorakai. Remix - sorakai"
+    },
+    ["SuperJumpy"] = {
+        ["Mobile"] = true,
+        ["Name"] = "Super Jumpy",
+        ["Price"] = 300,
+        ["Animations"] = {"Animation"},
+        ["Looping"] = true,
+        ["Description"] = "Guys I jumped...  [Traversal]",
+        ["Credits"] = "Animation - iluvoatml22."
+    },
+    ["LasPlagas"] = {
+        ["Secret"] = true,
+        ["Mobile"] = false,
+        ["Name"] = "Las Plagas",
+        ["Price"] = 1,
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "Leon!!!!!!!!!!!",
+        ["Credits"] = "Animation - iluvoatml22."
+    },
+    ["DoshBag"] = {
+        ["Description"] = "DEAR-D-D-D-DEAR MR. DOSH BAG. WE ALL AGREE THAT. YOU ARE A GRAV PAD. WHY CANT YOU SEE THAT?",
+        ["Init"] = "function: 0xdf30d0cd4e27aeca",
+        ["Animations"] = {"Animation"},
+        ["Mobile"] = false,
+        ["Name"] = "Dosh Bag",
+        ["Looping"] = false,
+        ["Price"] = 800,
+        ["New"] = true,
+        ["Credits"] = "Original - Your Favorite Martian. Animation - sorakai. Remix - sorakai."
+    },
+    ["OhNoes"] = {
+        ["Price"] = 100,
+        ["Mobile"] = false,
+        ["Name"] = "Oh Noes",
+        ["New"] = true,
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "It's a total emergency!",
+        ["Credits"] = "Animation - iluvoatml22."
+    },
+    ["WasntMe"] = {
+        ["Price"] = 150,
+        ["Mobile"] = false,
+        ["Name"] = "Wasn't Me!",
+        ["New"] = true,
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "Nuh uh.",
+        ["Credits"] = "Animation - iluvoatml22."
+    },
+    ["March"] = {
+        ["Price"] = 500,
+        ["Mobile"] = true,
+        ["Name"] = "March",
+        ["Init"] = "function: 0xfd74f2025dd25eea",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "Ugoku, Ugoku! [Traversal]",
+        ["Credits"] = "Animation - sorakai. Remix - sorakai."
+    },
+    ["LiarDancer"] = {
+        ["Price"] = 500,
+        ["Mobile"] = false,
+        ["Name"] = "Liar Dancer",
+        ["Init"] = "function: 0x56ec13c75fb8866a",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "Let's dance to the lies!",
+        ["Credits"] = "Original - masarada. Animation - iluvoatml22. Remix - sorakai"
+    },
+    ["OneTwoThree"] = {
+        ["Price"] = 800,
+        ["Mobile"] = false,
+        ["Name"] = "One Two Three",
+        ["Init"] = "function: 0xd675d29794d38b2a",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "one two three de arukidase!",
+        ["Credits"] = "Original - Girls Last Tour. Animation - sorakai. Remix - sorakai."
+    },
+    ["KillMeBaby"] = {
+        ["Price"] = 800,
+        ["Mobile"] = false,
+        ["Name"] = "Kill Me Baby",
+        ["Init"] = "function: 0xcc9b540da2948e4a",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "Best done with a friend!",
+        ["Credits"] = "Original - Kill Me Baby Ending. Animation - sorakai. Remix - sorakai"
+    },
+    ["PbjTime"] = {
+        ["Price"] = 500,
+        ["Mobile"] = false,
+        ["Name"] = "PBJ-Time",
+        ["Init"] = "function: 0xd679bdc0f5f45aaa",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "Now where he at? Where he at? Where he at? Where he at?",
+        ["Credits"] = "Animation - iluvoatml22. Remix - sorakai"
+    },
+    ["Mesmerizer"] = {
+        ["Price"] = 1000,
+        ["Mobile"] = false,
+        ["Name"] = "Mesmerizer",
+        ["Init"] = "function: 0x097b2bfb332c6c0a",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "Focus!",
+        ["Credits"] = "Original - Mesmerizer by 32ki. Animation - iluvoatml22. Remix - sorakai"
+    },
+    ["Shika"] = {
+        ["Price"] = 550,
+        ["Mobile"] = false,
+        ["Name"] = "Shika",
+        ["Init"] = "function: 0x8d9e8cf10a6883ea",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "Oh deer",
+        ["Credits"] = "Animation - iluvoatml22. Remix - sorakai"
+    },
+    ["Birthday"] = {
+        ["Price"] = 500,
+        ["Mobile"] = false,
+        ["Name"] = "birthday",
+        ["Init"] = "function: 0x26e58a0d7a9c67ea",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "ots my birthday today",
+        ["Credits"] = "Original - 𝓭𝓪𝓶𝓷why she So rude . Animation - vomic"
+    },
+    ["PointAt"] = {
+        ["Mobile"] = false,
+        ["Name"] = "Point At",
+        ["Price"] = 1,
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "Go save them!!!",
+        ["Credits"] = "Animation - iluvoatml22."
+    },
+    ["Lockstep"] = {
+        ["Price"] = 300,
+        ["Mobile"] = false,
+        ["Name"] = "Lockstep",
+        ["Init"] = "function: 0x41dada89f7fa0a2a",
+        ["Animations"] = {"Animation"},
+        ["Looping"] = false,
+        ["Description"] = "Ah, yes! Most excellent!",
+        ["Credits"] = "Original - Rhythm Heaven. Animation - iluvoatml22. Remix - vomic"
+    },
+    ["Slick"] = {
+        ["Description"] = "Like slidin' on ice. [Traversal]",
+        ["Init"] = "function: 0x63f27124950770ea",
+        ["Animations"] = {"Animation"},
+        ["Mobile"] = true,
+        ["Name"] = "Slick",
+        ["Looping"] = false,
+        ["Price"] = 500,
+        ["New"] = true,
+        ["Credits"] = "Original - Jamiroquai. Animation - sorakai. Remix - sorakai."
+    }
+}
