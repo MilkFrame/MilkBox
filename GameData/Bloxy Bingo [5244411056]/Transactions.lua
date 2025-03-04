f616462cc60c0f374782ff6a8340be6f27201705
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:17:58 +1100

    Create Transactions.lua

---
+++ GameData/Bloxy Bingo [5244411056]/Transactions.lua
@@ -0,0 +1,72 @@
+data = {
+    ["GetDailyPackName"] = "function: 0xbcb538bc88b1f0d4",
+    ["GetGiftMembershipDisplayName"] = "function: 0x406237a25ca65724",
+    ["GetGamepassId"] = "function: 0x7a2e3259e5e70cc4",
+    ["GetMembershipProduct"] = "function: 0xd001702ca070ea44",
+    ["DailyPacks"] = {
+        {["id"] = 1689737862, ["packName"] = "essential"},
+        {["id"] = 1689737860, ["packName"] = "unusual"},
+        {["id"] = 1689737861, ["packName"] = "rare"}
+    },
+    ["GetPlayerUpgradeId"] = "function: 0x314be7786b35b404",
+    ["GetGemsInfo"] = "function: 0xce28cb062e62c6f4",
+    ["GetGiftMembershipInfo"] = "function: 0xfff03e3d2bddcb34",
+    ["GetDailyPackInfo"] = "function: 0xbce5c2b7dbc81434",
+    ["Gems"] = {
+        {["id"] = 1009286325, ["amount"] = 50},
+        {["id"] = 1009286481, ["amount"] = 100},
+        {["id"] = 1009286619, ["amount"] = 250},
+        {["id"] = 1009287003, ["amount"] = 500},
+        {["id"] = 1009287156, ["amount"] = 1000},
+        {["id"] = 1009287299, ["amount"] = 6000}
+    },
+    ["GetDailyPackId"] = "function: 0xb6f16c014851ada4",
+    ["GetGemAmount"] = "function: 0x47cb4c903116ba74",
+    ["GetGamepassInfo"] = "function: 0xcf4d515c95f387f4",
+    ["GetGiftGemAmount"] = "function: 0xb8d0a7c2be9706b4",
+    ["Membership"] = {
+        {["id"] = 1009281159, ["duration"] = 2592000, ["name"] = "Month1"},
+        {["id"] = 1009281413, ["duration"] = 15724800, ["name"] = "Month6"},
+        {["id"] = 1009281642, ["duration"] = 31536000, ["name"] = "Year1"},
+        {["id"] = 1009281880, ["duration"] = 315532800, ["name"] = "Year10"}
+    },
+    ["GiftGems"] = {
+        {["id"] = 1203811675, ["amount"] = 50},
+        {["id"] = 1203811672, ["amount"] = 100},
+        {["id"] = 1203811673, ["amount"] = 250},
+        {["id"] = 1203811671, ["amount"] = 500},
+        {["id"] = 1203812157, ["amount"] = 1000},
+        {["id"] = 1203811669, ["amount"] = 6000}
+    },
+    ["PlayerUpgrades"] = {{["id"] = 1689746862, ["tag"] = "TMus"}, {["id"] = 1353982747, ["tag"] = "TablePair"}},
+    ["GetPlayerUpgradeInfo"] = "function: 0xd00869a595dcfc74",
+    ["GetMembershipInfo"] = "function: 0x1e4a6076afad2ab4",
+    ["GetInfo"] = "function: 0xab17104b2f144974",
+    ["Serverpasses"] = {{["id"] = 1029673001, ["tag"] = "CustomCalls"}, {["id"] = 1689747289, ["tag"] = "CustomMusic"}},
+    ["GetAtribute"] = "function: 0x89bc73c36810cb34",
+    ["GetServerpassTag"] = "function: 0x09fc5787f746e194",
+    ["GetPlayerUpgradeTag"] = "function: 0x2200812429967834",
+    ["GetGemProduct"] = "function: 0xeb9cd94447957ea4",
+    ["GetServerpassInfo"] = "function: 0x1c6af5fc07f65bb4",
+    ["GetServerpassId"] = "function: 0x15ce5ad186d84464",
+    ["GetGamepassTag"] = "function: 0xf519f213b4076bf4",
+    ["GetGiftGemsInfo"] = "function: 0xcfad04e34d286f74",
+    ["GetGiftMembershipProduct"] = "function: 0xaae981365e279d84",
+    ["GetGiftMembershipDuration"] = "function: 0xdea46d6e7d47d154",
+    ["Gamepasses"] = {
+        {["id"] = 10651016, ["tag"] = "AutoMarker"},
+        {["id"] = 10687721, ["tag"] = "MarkAssist"},
+        {["id"] = 26488005, ["tag"] = "DarkCards"},
+        {["id"] = 117616599, ["tag"] = "TwoMascots"},
+        {["id"] = 169514492, ["tag"] = "PastelMarkers"},
+        {["id"] = 169514801, ["tag"] = "DarkMarkers"}
+    },
+    ["GetGiftGemProduct"] = "function: 0xba97c8bd011763e4",
+    ["GetMembershipDuration"] = "function: 0xaf453c68f3f6ce14",
+    ["GiftMembership"] = {
+        {["id"] = 1203811670, ["duration"] = 2592000, ["name"] = "Month1", ["displayName"] = "1 Month"},
+        {["id"] = 1203811668, ["duration"] = 15724800, ["name"] = "Month6", ["displayName"] = "6 Months"},
+        {["id"] = 1203811697, ["duration"] = 31536000, ["name"] = "Year1", ["displayName"] = "1 Year"},
+        {["id"] = 1203811674, ["duration"] = 315532800, ["name"] = "Year10", ["displayName"] = "10 Years"}
+    }
+}
