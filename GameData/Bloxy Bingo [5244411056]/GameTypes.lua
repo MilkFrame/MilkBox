2f58ba1ce7bdf5be6b33d96435cc19c411e4f7dd
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:18:21 +1100

    Create GameTypes.lua

---
+++ GameData/Bloxy Bingo [5244411056]/GameTypes.lua
@@ -0,0 +1,7 @@
+data = {
+    ["MaxCallNo"] = {["US"] = 75, ["AC"] = 80, ["UK"] = 90, ["LT"] = 54},
+    ["GameNameOrder"] = {"UK", "US", "AC", "LT"},
+    ["GameNames"] = {["US"] = "American Bingo", ["AC"] = "Arcade Bingo", ["UK"] = "British Bingo", ["LT"] = "Lotería"},
+    ["GameOrder"] = {["US"] = 2, ["AC"] = 3, ["UK"] = 1, ["LT"] = 4},
+    ["GameTypeShortIds"] = {["UKBingo"] = "UK", ["LoteriaBingo"] = "LT", ["USBingo"] = "US", ["ArcadeBingo"] = "AC"}
+}
