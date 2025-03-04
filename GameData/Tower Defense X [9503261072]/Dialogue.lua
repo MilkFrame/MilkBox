d1dbf0cfdb6792b5959f98a7618e9942c6e107f9
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:30:45 +1100

    Create Dialogue.lua

---
+++ GameData/Tower Defense X [9503261072]/Dialogue.lua
@@ -0,0 +1,318 @@
+data = {
+    ["TutorialWave4"] = {
+        ["Dialogue"] = {
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://73668615117439",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 2.75,
+                ["SoundId"] = "rbxassetid://106007217224147",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave4_1"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://99444093702690",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 3.5,
+                ["SoundId"] = "rbxassetid://127160531923299",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave4_2"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://99444093702690",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 3.5,
+                ["SoundId"] = "rbxassetid://133210485582058",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave4_3"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://88482183967844",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 5,
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave4_4"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            }
+        }
+    },
+    ["TutorialWave8"] = {
+        ["Dialogue"] = {
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://88482183967844",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 8,
+                ["SoundId"] = "rbxassetid://123343229110223",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave8_1"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            }
+        }
+    },
+    ["TutorialWave6"] = {
+        ["Dialogue"] = {
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://88482183967844",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 3.75,
+                ["SoundId"] = "rbxassetid://97140656923308",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave6_1"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://88482183967844",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 7,
+                ["SoundId"] = "rbxassetid://93457457065712",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave6_2"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            }
+        }
+    },
+    ["TutorialWave3"] = {
+        ["Dialogue"] = {
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://98840110897586",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 2.5,
+                ["SoundId"] = "rbxassetid://86683815599743",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave3_1"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://73668615117439",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 6,
+                ["SoundId"] = "rbxassetid://111728883383523",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave3_2"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            }
+        }
+    },
+    ["DialogueTest"] = {
+        ["Dialogue"] = {
+            {
+                ["Image"] = "rbxassetid://6870585235",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Default"},
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Easy.Wave1"}
+            },
+            {
+                ["Image"] = "rbxassetid://9180622665",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Toilet.Wave3"}
+            }
+        }
+    },
+    ["TutorialWave1"] = {
+        ["Dialogue"] = {
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://99444093702690",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 3.25,
+                ["SoundId"] = "rbxassetid://96915076011784",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave1_1"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://99444093702690",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 3.25,
+                ["SoundId"] = "rbxassetid://106131852905408",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave1_2"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://99444093702690",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 3,
+                ["SoundId"] = "rbxassetid://111225659668393",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave1_3"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://98840110897586",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 6,
+                ["SoundId"] = "rbxassetid://78590275367314",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave1_4"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            }
+        }
+    },
+    ["TutorialPassed"] = {
+        ["Dialogue"] = {
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://90945897079609",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 3,
+                ["SoundId"] = "rbxassetid://138909733771371",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Win_1"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://83155287415664",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 3.75,
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Win_2"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://88482183967844",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 3.75,
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Win_3"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://97078771157329",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 5,
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Win_4"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            }
+        }
+    },
+    ["TutorialWave7"] = {
+        ["Dialogue"] = {
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://99444093702690",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 3,
+                ["SoundId"] = "rbxassetid://138519019172748",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave7_1"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://99444093702690",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 2.25,
+                ["SoundId"] = "rbxassetid://107177316494229",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave7_2"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://99444093702690",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 3,
+                ["SoundId"] = "rbxassetid://128168455413237",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave7_3"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://97078771157329",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 6,
+                ["SoundId"] = "rbxassetid://92189298464480",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave7_4"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            }
+        }
+    },
+    ["TutorialWave5"] = {
+        ["Dialogue"] = {
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://99444093702690",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 4,
+                ["SoundId"] = "rbxassetid://103255360416521",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave5_1"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://99444093702690",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 3.5,
+                ["SoundId"] = "rbxassetid://122307982603690",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave5_2"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://98840110897586",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 6,
+                ["SoundId"] = "rbxassetid://96018996768580",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave5_3"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            }
+        }
+    },
+    ["TutorialWave2"] = {
+        ["Dialogue"] = {
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://73668615117439",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 4.5,
+                ["SoundId"] = "rbxassetid://111047966663396",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave2_1"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://73668615117439",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 4.25,
+                ["SoundId"] = "rbxassetid://70562396422116",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave2_2"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            },
+            {
+                ["Pitch"] = 1.25,
+                ["Image"] = "rbxassetid://88482183967844",
+                ["Name"] = {["__PATH"] = "AdvisorNames.Xenon"},
+                ["AfterTextDuration"] = 6,
+                ["SoundId"] = "rbxassetid://127170483127441",
+                ["Text"] = {["__PATH"] = "Game.Dialogue.Tutorial.Wave2_3"},
+                ["ImageRatio"] = 1,
+                ["ImageSize"] = "{1.5, 0}, {1.20000005, 0}"
+            }
+        }
+    }
+}
