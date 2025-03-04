a5494b809a8d786510f5fe63a436aa136ff80157
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:14:34 +1100

    Create Rarities.lua

---
+++ GameData/Regretevator [4972273297]/Rarities.lua
@@ -0,0 +1,50 @@
+data = {
+    ["Rare"] = {
+        ["Color"] = Color3.fromRGB(41, 205, 255),
+        ["ColorGradient"] = "0 0.0313726 0.870588 1 0 1 0.168627 0.647059 0.901961 0 ",
+        ["Order"] = 3,
+        ["OverColor"] = Color3.fromRGB(255, 255, 255)
+    },
+    ["Legendary"] = {
+        ["Color"] = Color3.fromRGB(255, 209, 41),
+        ["ColorGradient"] = "0 1 0.894118 0.0980392 0 1 0.866667 0.643137 0.117647 0 ",
+        ["Order"] = 5,
+        ["OverColor"] = Color3.fromRGB(255, 255, 255)
+    },
+    ["PressureCollab"] = {
+        ["Color"] = Color3.fromRGB(32, 47, 103),
+        ["ColorGradient"] = "0 0.137255 0.27451 0.494118 0 1 0.121569 0.156863 0.34902 0 ",
+        ["Order"] = 6,
+        ["OverColor"] = Color3.fromRGB(255, 255, 255)
+    },
+    ["Common"] = {
+        ["Color"] = Color3.fromRGB(103, 70, 68),
+        ["ColorGradient"] = "0 0.172549 0.121569 0.113725 0 1 0.254902 0.176471 0.168627 0 ",
+        ["Order"] = 1,
+        ["OverColor"] = Color3.fromRGB(211, 202, 195)
+    },
+    ["Epic"] = {
+        ["Color"] = Color3.fromRGB(154, 53, 255),
+        ["ColorGradient"] = "0 0.54902 0.180392 1 0 1 0.356863 0.145098 0.776471 0 ",
+        ["Order"] = 4,
+        ["OverColor"] = Color3.fromRGB(255, 255, 255)
+    },
+    ["InternetSeries"] = {
+        ["Color"] = Color3.fromRGB(34, 34, 34),
+        ["ColorGradient"] = "0 1 1 1 0 1 0.34902 0.34902 0.34902 0 ",
+        ["Order"] = 6,
+        ["OverColor"] = Color3.fromRGB(255, 255, 255)
+    },
+    ["Uncommon"] = {
+        ["Color"] = Color3.fromRGB(0, 255, 38),
+        ["ColorGradient"] = "0 0.313726 0.854902 0.356863 0 1 0.156863 0.6 0.239216 0 ",
+        ["Order"] = 2,
+        ["OverColor"] = Color3.fromRGB(255, 255, 255)
+    },
+    ["DevOnly"] = {
+        ["Color"] = Color3.fromRGB(255, 69, 72),
+        ["ColorGradient"] = "0 1 0.172549 0.188235 0 1 0.866667 0.333333 0.341176 0 ",
+        ["Order"] = 9999,
+        ["OverColor"] = Color3.fromRGB(255, 255, 255)
+    }
+}
