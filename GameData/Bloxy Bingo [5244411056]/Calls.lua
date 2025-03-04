3570f2cf5597c1274eebb889a589e6aa44bdc780
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:24:14 +1100

    Create Calls.lua

---
+++ GameData/Bloxy Bingo [5244411056]/Calls.lua
@@ -0,0 +1,515 @@
+data = {
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "Joseph",
+        ["cost"] = 50,
+        ["displayIcons"] = 7336187418,
+        ["tracks"] = {
+            ["house"] = {9077891244},
+            ["line"] = {9077888329},
+            ["bingo"] = {9077884983, 9077886180, 9077886563}
+        },
+        ["description"] = "Voiced by Ciaranpixelz",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Ciaranpixelz"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Female",
+        ["name"] = "Emily",
+        ["cost"] = 50,
+        ["displayIcons"] = 7336184301,
+        ["tracks"] = {
+            ["house"] = {9077911518},
+            ["line"] = {9077910895},
+            ["bingo"] = {9077908649, 9077909105, 9077909488, 9077909793, 9077910193}
+        },
+        ["description"] = "Voiced by Cheesecake360",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Cheesecake360"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Female",
+        ["name"] = "Sophie",
+        ["cost"] = 50,
+        ["displayIcons"] = 7336193120,
+        ["tracks"] = {
+            ["house"] = {9077930661},
+            ["line"] = {9077929900, 9077930223},
+            ["bingo"] = {9077926936, 9077927628, 9077927978, 9077928369, 9077928665}
+        },
+        ["description"] = "Voiced by Lissvoices",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Lissvoices"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Female",
+        ["name"] = "Abbie",
+        ["cost"] = 50,
+        ["displayIcons"] = 7336179159,
+        ["tracks"] = {
+            ["house"] = {9077945158},
+            ["line"] = {9077944808},
+            ["bingo"] = {9077943736, 9077944014, 9077944272, 9077944487}
+        },
+        ["description"] = "Voiced by Minniekrim",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Minniekrim"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Female",
+        ["name"] = "Alice",
+        ["cost"] = 50,
+        ["displayIcons"] = 7336180246,
+        ["tracks"] = {
+            ["house"] = {9077956846, 9077957296},
+            ["line"] = {9077956214, 9077956465},
+            ["bingo"] = {9077954257, 9077954449, 9077955216, 9077955483, 9077955705, 9077955911}
+        },
+        ["description"] = "Voiced by Pisarkanna",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Pisarkanna"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "Jake",
+        ["cost"] = 50,
+        ["displayIcons"] = 7336186210,
+        ["tracks"] = {
+            ["house"] = {9077972305},
+            ["line"] = {9077972005},
+            ["bingo"] = {9077970591, 9077971042, 9077971457}
+        },
+        ["description"] = "Voiced by Zacoriot",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Zacoriot"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "Jacob",
+        ["cost"] = 50,
+        ["displayIcons"] = 7336185568,
+        ["tracks"] = {
+            ["house"] = {9077993063, 9077993524},
+            ["line"] = {9077991703, 9077992119},
+            ["bingo"] = {9077989618, 9077989904, 9077990146, 9077990512, 9077990894, 9077991298}
+        },
+        ["description"] = "Voiced by Iamsabouri",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Iamsabouri"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "Harry",
+        ["cost"] = 50,
+        ["displayIcons"] = 7336185029,
+        ["tracks"] = {
+            ["house"] = {9143975256, 9143975752},
+            ["line"] = {9143974187, 9143974530},
+            ["bingo"] = {9143958971, 9143963676, 9143964091, 9143964605}
+        },
+        ["description"] = "Voiced by Harrysaner",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Harrysaner"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "James",
+        ["cost"] = 50,
+        ["displayIcons"] = 7336186753,
+        ["tracks"] = {
+            ["house"] = {9078014145, 9078014420},
+            ["line"] = {9078013417, 9078013606},
+            ["bingo"] = {9078011473, 9078012002, 9078012420, 9078012721, 9078013033}
+        },
+        ["description"] = "Voiced by Harrisonva",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Harrisonva"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Female",
+        ["name"] = "Bethany",
+        ["cost"] = 50,
+        ["displayIcons"] = 7336181003,
+        ["tracks"] = {
+            ["house"] = {9078030083, 9078030430},
+            ["line"] = {9078029317, 9078029606},
+            ["bingo"] = {9078027366, 9078027658, 9078027960, 9078028434, 9078028753}
+        },
+        ["description"] = "Voiced by Emilyaly",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Emilyaly"}}
+    },
+    {
+        ["availability"] = "Special",
+        ["name"] = "Meoww",
+        ["cost"] = 50,
+        ["isForSale"] = false,
+        ["displayIcons"] = 7336188723,
+        ["tracks"] = {["bingo"] = {9078041038}},
+        ["description"] = ":3"
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "Samuel",
+        ["cost"] = 60,
+        ["displayIcons"] = 7336191858,
+        ["tracks"] = {
+            ["house"] = {9078048398, 9078048707},
+            ["line"] = {9078047904, 9078048086},
+            ["bingo"] = {9078046715, 9078047038, 9078047280, 9078047612}
+        },
+        ["description"] = "Voiced by Averybard",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Averybard"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Female",
+        ["name"] = "Charlotte",
+        ["cost"] = 60,
+        ["displayIcons"] = 7336182678,
+        ["tracks"] = {
+            ["house"] = {9078070581, 9078070997},
+            ["line"] = {9078070126, 9078070314},
+            ["bingo"] = {9078068189, 9078068569, 9078069198, 9078069518, 9078069792}
+        },
+        ["description"] = "Voiced by Cakcak8d",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Cakcak8d"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Female",
+        ["name"] = "Eleanor",
+        ["cost"] = 60,
+        ["displayIcons"] = 7336183172,
+        ["tracks"] = {
+            ["house"] = {9078088752, 9078089194},
+            ["line"] = {9078089472},
+            ["bingo"] = {9078087525, 9078087886, 9078088154, 9078088417}
+        },
+        ["description"] = "Voiced by Emilylinard",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Emilylinard"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Female",
+        ["name"] = "Natasha",
+        ["cost"] = 60,
+        ["displayIcons"] = 7336189352,
+        ["tracks"] = {
+            ["house"] = {9078100457, 9078100736},
+            ["line"] = {9078101151, 9078101443},
+            ["bingo"] = {9078099369, 9078099656, 9078099906, 9078100134}
+        },
+        ["description"] = "Voiced by Jazzdoesvocals",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Jazzdoesvocals"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "Alexander",
+        ["cost"] = 60,
+        ["displayIcons"] = 7336179712,
+        ["tracks"] = {
+            ["house"] = {9078112152, 9078112487},
+            ["line"] = {9078112851, 9078113094},
+            ["bingo"] = {9078111042, 9078111406, 9078111709, 9078111924}
+        },
+        ["description"] = "Vocied by Glasgown",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Glasgown"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Female",
+        ["name"] = "Ellie",
+        ["cost"] = 60,
+        ["displayIcons"] = 7336197371,
+        ["tracks"] = {
+            ["house"] = {9078126798, 9078127306},
+            ["line"] = {9078127629, 9078127932},
+            ["bingo"] = {9078125145, 9078125685, 9078126033, 9078126277, 9078126513}
+        },
+        ["description"] = "Voiced by Joyh97",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Joyh97"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Female",
+        ["name"] = "Rebecca",
+        ["cost"] = 60,
+        ["displayIcons"] = 7336190529,
+        ["tracks"] = {
+            ["house"] = {9078143656, 9078144024},
+            ["line"] = {9078144411, 9078144746},
+            ["bingo"] = {9078142230, 9078142551, 9078142803, 9078143087, 9078143368}
+        },
+        ["description"] = "Voiced by Jessicabritvo",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Jessicabritvo"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "Ryan",
+        ["cost"] = 60,
+        ["displayIcons"] = 7336191172,
+        ["tracks"] = {
+            ["house"] = {9078160992, 9078161313},
+            ["line"] = {9078161675, 9078161967},
+            ["bingo"] = {9078159429, 9078159689, 9078159936, 9078160210, 9078160427, 9078160644}
+        },
+        ["description"] = "Voiced by Britishvo4u",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Britishvo4u"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "Kieran",
+        ["cost"] = 60,
+        ["displayIcons"] = 7336187930,
+        ["tracks"] = {
+            ["house"] = {9078175640, 9078175994},
+            ["line"] = {9078176371, 9078176686},
+            ["bingo"] = {9078174093, 9078174340, 9078174555, 9078174838, 9078175207}
+        },
+        ["description"] = "Voiced by Toby_jackson",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Toby_jackson"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "Nathan",
+        ["cost"] = 60,
+        ["displayIcons"] = 7336189961,
+        ["tracks"] = {
+            ["house"] = {9078239552, 9078239870},
+            ["line"] = {9078240214, 9078240467},
+            ["bingo"] = {9078238349, 9078238726, 9078239094, 9078239320}
+        },
+        ["description"] = "Voiced by Chrissammo",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Chrissammo"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "Bob",
+        ["cost"] = 60,
+        ["displayIcons"] = 7336181917,
+        ["tracks"] = {
+            ["house"] = {9078253324, 9078253607},
+            ["line"] = {9078253820, 9078254123},
+            ["bingo"] = {9078251338, 9078251597, 9078251956, 9078252451, 9078253004}
+        },
+        ["description"] = "Voiced by jordanpeckvoice",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "jordanpeckvoice"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Female",
+        ["name"] = "Megan",
+        ["cost"] = 60,
+        ["displayIcons"] = 7161401103,
+        ["tracks"] = {
+            ["house"] = {9078269149, 9078269547},
+            ["line"] = {9078269995, 9078270320},
+            ["bingo"] = {9078267628, 9078267897, 9078268337, 9078268710}
+        },
+        ["description"] = "Voiced by rosasam",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "rosasam"}}
+    },
+    {
+        ["isForSale"] = false,
+        ["availability"] = "Male",
+        ["name"] = "Santa Claus",
+        ["cost"] = 0,
+        ["displayIcons"] = 7336192442,
+        ["tracks"] = {
+            ["house"] = {9143998107, 9143998458},
+            ["line"] = {9144002730, 9144003115},
+            ["bingo"] = {9143987836, 9143988359, 9143988673, 9143989101}
+        },
+        ["description"] = "Voiced by Dummyman",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Dummyman"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Female",
+        ["name"] = "Millie",
+        ["cost"] = 60,
+        ["displayIcons"] = 8489862795,
+        ["tracks"] = {
+            ["house"] = {9078304714},
+            ["line"] = {9078304924},
+            ["bingo"] = {9078303654, 9078304040, 9078304332}
+        },
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Comfort_forster"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "George",
+        ["cost"] = 60,
+        ["displayIcons"] = 8489859692,
+        ["tracks"] = {
+            ["house"] = {9078316090, 9078316374},
+            ["line"] = {9078316740, 9078317274},
+            ["bingo"] = {9078314687, 9078315151, 9078315526, 9078315826}
+        },
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Felixparrack"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Female",
+        ["name"] = "Sarah",
+        ["cost"] = 60,
+        ["displayIcons"] = 8489863469,
+        ["tracks"] = {
+            ["house"] = {9078335860},
+            ["line"] = {9078336268},
+            ["bingo"] = {9078334580, 9078334953, 9078335366}
+        },
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Isabelvo"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Female",
+        ["name"] = "Julie",
+        ["cost"] = 60,
+        ["displayIcons"] = 8489861224,
+        ["tracks"] = {
+            ["house"] = {9078349826, 9078350182},
+            ["line"] = {9078350506, 9078350898},
+            ["bingo"] = {9078348721, 9078349032, 9078349529}
+        },
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Jane_wing_voice"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "Lewis",
+        ["cost"] = 60,
+        ["displayIcons"] = 8489861964,
+        ["tracks"] = {
+            ["house"] = {9078368127},
+            ["line"] = {9078368565},
+            ["bingo"] = {9078367185, 9078367449, 9078367745}
+        },
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Josephcotham"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "Jordan",
+        ["cost"] = 60,
+        ["displayIcons"] = 8489860735,
+        ["tracks"] = {
+            ["house"] = {9078377835},
+            ["line"] = {9078378145},
+            ["bingo"] = {9078376699, 9078377035, 9078377314}
+        },
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Killsmeg"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Female",
+        ["name"] = "Maisie",
+        ["cost"] = 60,
+        ["displayIcons"] = 8489862345,
+        ["tracks"] = {
+            ["house"] = {9078389158},
+            ["line"] = {9078389500},
+            ["bingo"] = {9078387435, 9078387809, 9078388142}
+        },
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Meghanrosemckee"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Female",
+        ["name"] = "Ella",
+        ["cost"] = 60,
+        ["displayIcons"] = 8490203991,
+        ["tracks"] = {
+            ["house"] = {9078402373},
+            ["line"] = {9078402679},
+            ["bingo"] = {9078401491, 9078401755, 9078402059}
+        },
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Myriamoxford"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Female",
+        ["name"] = "Grace",
+        ["cost"] = 60,
+        ["displayIcons"] = 8489860173,
+        ["tracks"] = {
+            ["house"] = {9078415013},
+            ["line"] = {9078415325},
+            ["bingo"] = {9078414117, 9078414297, 9078414696}
+        },
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Rensvoiceovers"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "Alex",
+        ["cost"] = 60,
+        ["displayIcons"] = 8489857600,
+        ["tracks"] = {
+            ["house"] = {9078424748, 9078425036},
+            ["line"] = {9078425411, 9078425653},
+            ["bingo"] = {9078423961, 9078424166, 9078424431}
+        },
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Simonchester"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "Conor",
+        ["cost"] = 60,
+        ["displayIcons"] = 8489858806,
+        ["tracks"] = {
+            ["house"] = {9078445202},
+            ["line"] = {9078443102},
+            ["bingo"] = {9078436030, 9078436386, 9078436852}
+        },
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Sp00keh"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "Alfie",
+        ["cost"] = 60,
+        ["displayIcons"] = 8489858238,
+        ["tracks"] = {
+            ["house"] = {9078460095},
+            ["line"] = {9078460434},
+            ["bingo"] = {9078458965, 9078459239, 9078459642}
+        },
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Tashafossick1"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Male",
+        ["name"] = "Kyle",
+        ["cost"] = 60,
+        ["displayIcons"] = 8489861601,
+        ["tracks"] = {
+            ["house"] = {9078470738},
+            ["line"] = {9078471101},
+            ["bingo"] = {9078469584, 9078469954, 9078470345}
+        },
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "Vocalgraphics"}}
+    }
+}
