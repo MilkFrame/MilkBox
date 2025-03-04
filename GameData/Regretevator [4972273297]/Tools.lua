93e15ff64bda1ce0cd1b8c4c11ca423aaeb3ef03
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:13:35 +1100

    Create Tools.lua

---
+++ GameData/Regretevator [4972273297]/Tools.lua
@@ -0,0 +1,79 @@
+data = {
+    ["Snowball"] = {["Configuration"] = {["Price"] = 6, ["isInfinite"] = false, ["ShopItemType"] = "Coin"}},
+    ["Tomato"] = {["Configuration"] = {["Price"] = 4, ["isInfinite"] = false, ["ShopItemType"] = "Coin"}},
+    ["Magic Mirror"] = {["Configuration"] = {["Price"] = 20, ["isInfinite"] = false, ["ShopItemType"] = "Coin"}},
+    ["Spray Bottle"] = {["Configuration"] = {["Price"] = 200, ["isInfinite"] = true, ["ShopItemType"] = "Coin"}},
+    ["Fizz Jug"] = {["Configuration"] = {["Price"] = 50, ["isInfinite"] = false, ["ShopItemType"] = "Coin"}},
+    ["Plastic Karambit"] = {
+        ["Configuration"] = {
+            ["Price"] = 150,
+            ["PurchaseForbidden"] = false,
+            ["isInfinite"] = true,
+            ["ShopItemType"] = "VIP"
+        }
+    },
+    ["ifone"] = {["Configuration"] = {["Price"] = 175, ["isInfinite"] = true, ["ShopItemType"] = "Coin"}},
+    ["Petal Cone"] = {["Configuration"] = {["Price"] = 200, ["isInfinite"] = true, ["ShopItemType"] = "Coin"}},
+    ["C4"] = {["Configuration"] = {["Price"] = 20, ["isInfinite"] = false, ["ShopItemType"] = "VIP"}},
+    ["Squishy"] = {["Configuration"] = {["Price"] = 200, ["isInfinite"] = true, ["ShopItemType"] = "Coin"}},
+    ["Jerm Pop"] = {["Configuration"] = {["Price"] = 600, ["isInfinite"] = true, ["ShopItemType"] = "Coin"}},
+    ["Cheezburger"] = {["Configuration"] = {["Price"] = 2, ["isInfinite"] = false, ["ShopItemType"] = "Coin"}},
+    ["Magnet"] = {["Configuration"] = {["Price"] = 350, ["isInfinite"] = true, ["ShopItemType"] = "Coin"}},
+    ["Useless Coin"] = {["Configuration"] = {["Price"] = 50, ["isInfinite"] = true, ["ShopItemType"] = "Coin"}},
+    ["MusicPlayer"] = {["Configuration"] = {["Price"] = 250, ["isInfinite"] = true, ["ShopItemType"] = "VIP"}},
+    ["Maracas"] = {["Configuration"] = {["Price"] = 150, ["isInfinite"] = true, ["ShopItemType"] = "Coin"}},
+    ["!_BASETOOL"] = {["Configuration"] = {["Price"] = 0, ["isInfinite"] = true, ["ShopItemType"] = "Coin"}},
+    ["Wet Cement"] = {["Configuration"] = {["Price"] = 45, ["isInfinite"] = false, ["ShopItemType"] = "Coin"}},
+    ["Wooden Sign"] = {["Configuration"] = {["Price"] = 230, ["isInfinite"] = true, ["ShopItemType"] = "VIP"}},
+    ["Fidget Spinner"] = {["Configuration"] = {["Price"] = 150, ["isInfinite"] = true, ["ShopItemType"] = "VIP"}},
+    ["GomBall"] = {
+        ["Configuration"] = {
+            ["ProductType"] = "Gamepass",
+            ["Price"] = -1,
+            ["isInfinite"] = true,
+            ["ProductID"] = 675975551,
+            ["ShopItemType"] = "Robux"
+        }
+    },
+    ["RED JUICE"] = {
+        ["Configuration"] = {
+            ["ProductType"] = "Gamepass",
+            ["Price"] = -1,
+            ["isInfinite"] = true,
+            ["ProductID"] = 785322657,
+            ["ShopItemType"] = "Robux"
+        }
+    },
+    ["Generation Processing Unit"] = {
+        ["Configuration"] = {["Price"] = 899.99, ["isInfinite"] = true, ["ShopItemType"] = "Coin"}
+    },
+    ["Scope"] = {["Configuration"] = {["Price"] = 200, ["isInfinite"] = true, ["ShopItemType"] = "Coin"}},
+    ["Plastic Balisong"] = {
+        ["Configuration"] = {
+            ["Price"] = 150,
+            ["PurchaseForbidden"] = false,
+            ["isInfinite"] = true,
+            ["ShopItemType"] = "Coin"
+        }
+    },
+    ["Medkit"] = {["Configuration"] = {["Price"] = 40, ["isInfinite"] = false, ["ShopItemType"] = "Coin"}},
+    ["Red Teddy"] = {["Configuration"] = {["Price"] = 150, ["isInfinite"] = true, ["ShopItemType"] = "Coin"}},
+    ["Mini-Magnet"] = {["Configuration"] = {["Price"] = 350, ["isInfinite"] = true, ["ShopItemType"] = "VIP"}},
+    ["Anti Voomba Mechanism"] = {
+        ["Configuration"] = {["Price"] = 999, ["isInfinite"] = true, ["ShopItemType"] = "Coin"},
+        ["Hole"] = {["EffectsController"] = "function: 0xc2d2a0070aca3489"}
+    },
+    ["Groan Tube"] = {["Configuration"] = {["Price"] = 150, ["isInfinite"] = true, ["ShopItemType"] = "Coin"}},
+    ["Enchanted 8-Ball"] = {
+        ["Configuration"] = {
+            ["Price"] = 100,
+            ["PurchaseForbidden"] = false,
+            ["isInfinite"] = true,
+            ["ShopItemType"] = "VIP"
+        }
+    },
+    ["Timer"] = {["Configuration"] = {["Price"] = 200, ["isInfinite"] = true, ["ShopItemType"] = "Coin"}},
+    ["Fizz Up!"] = {["Configuration"] = {["Price"] = 5, ["isInfinite"] = false, ["ShopItemType"] = "Coin"}},
+    ["Cheese"] = {["Configuration"] = {["Price"] = 6, ["isInfinite"] = false, ["ShopItemType"] = "Coin"}},
+    ["Blogsy"] = {["Configuration"] = {["Price"] = 30, ["isInfinite"] = false, ["ShopItemType"] = "Coin"}}
+}
