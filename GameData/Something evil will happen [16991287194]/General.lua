0c2e389f7560942f0123b82c96ad6991f677a400
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:03:08 +1100

    Create General.lua

---
+++ GameData/Something evil will happen [16991287194]/General.lua
@@ -0,0 +1,23 @@
+data = {
+    ["Body Trauma"] = {
+        ["Description"] = "<font color='#FF8C4D'>Body Trauma</font> is one of the main gameplay mechanics in SEWH. <br/><br/> During gameplay, the map can be destroyed by both players and the chosen <font color='#FF005D'>Disaster.</font> This destruction can send debris flying in every direction. <br/><br/> If hit by this debris, you will be flung and damaged based on how fast the part was moving. <br/><br/> Getting hit in the head is more lethal than any other body part."
+    },
+    ["Disasters"] = {
+        ["Description"] = "<font color='#FF005D'>Disasters</font> are one of the main gameplay mechanics in SEWH. <br/><br/> Every round, a <font color='#FF005D'>Disaster</font> is randomly chosen. Each one is unique, offering varying levels of reward and difficulty in order to spice things up. <br/><br/> Certain <font color='#4DE4FF'>Round Types</font> will affect either the difficulty of a <font color='#FF005D'>Disaster</font> or the amount of them present in a round."
+    },
+    ["Status Effects"] = {
+        ["Description"] = "<font color='#FFEC4D'>Status Effects</font> are one of the main gameplay mechanics in SEWH. <br/><br/> <font color='#FFEC4D'>Status Effects</font> can be inflicted by a wide variety of <font color='#FF005D'>Disasters</font>, map hazards, and <font color='#4D8BFF'>Gears</font>. They are typically a detriment, aiming to make your survival less of a guarantee. <br/><br/> However, some <font color='#4D8BFF'>Gears</font> can provide positive <font color='#FFEC4D'>Status Effects</font>, which can help you survive tougher rounds."
+    },
+    ["Gear"] = {
+        ["Description"] = "<font color='#4D8BFF'>Gears</font> are one of the main gameplay mechanics in SEWH. <br/><br/> <font color='#4D8BFF'>Gears</font> can be purchased and equipped at <font color='#FF930E'>Builderfan's</font> shop, in exchange for <font color='#29FF0F'>Dosh</font>. The usefulness of a <font color='#4D8BFF'>Gear</font> is typically determined by its price. <br/><br/> Your equipped <font color='#4D8BFF'>Gear</font> is provided to you at the beginning of a round, and can be used in all 3 <font color='#4DE4FF'>Round Types.</font>"
+    },
+    ["Combo Rounds"] = {
+        ["Description"] = "Combo rounds are 1 of 3 <font color='#4DE4FF'>Round Types</font> currently in SEWH. <br/><br/> Combo rounds aim to expand on the typical gameplay experience. These rounds choose a combination of <font color='#FF005D'>Disasters</font>, aiming to increase risk for more reward. <br/><br/> They are the second-most common of all <font color='#4DE4FF'>Round Types.</font>"
+    },
+    ["Special Rounds"] = {
+        ["Description"] = "Special rounds are 1 of 3 <font color='#4DE4FF'>Round Types</font> currently in SEWH. <br/><br/> Special rounds aim to shake up the typical gameplay experience. These rounds have special <font color='#FF005D'>Disasters</font> that play very differently from normal ones, sometimes even being accompanied by a special map. <br/><br/> They are the most rare of all <font color='#4DE4FF'>Round Types</font>, and typically reward more than other round types."
+    },
+    ["Normal Rounds"] = {
+        ["Description"] = "Normal rounds are 1 of 3 <font color='#4DE4FF'>Round Types</font> currently in SEWH. <br/><br/> Normal rounds are your typical gameplay experience. These rounds choose 1 <font color='#FF005D'>Disaster</font>, and will change the current map every so often. <br/><br/> They are the most common of all <font color='#4DE4FF'>Round Types.</font>"
+    }
+}
