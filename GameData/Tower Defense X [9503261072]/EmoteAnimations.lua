0d4e355ac898e07b340cc5a48e872372a9b08ca3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:30:32 +1100

    Create EmoteAnimations.lua

---
+++ GameData/Tower Defense X [9503261072]/EmoteAnimations.lua
@@ -0,0 +1,24 @@
+data = {
+    ["Bench Rest"] = {["ModelName"] = "Bench", ["Animations"] = {"Animation"}, ["StartAnimation"] = "Animation"},
+    ["StockShuffle"] = {["Animations"] = {"Animation"}},
+    ["Rave"] = {["Animations"] = {"Animation"}},
+    ["Sturdy"] = {["Animations"] = {"Animation"}},
+    ["SherbertDance"] = {["Animations"] = {"Animation"}},
+    ["MonsterMash"] = {["SoundId"] = 35930009, ["Volume"] = 0.15, ["Animations"] = {"Animation"}},
+    ["Eradicator Toy"] = {
+        ["ModelName"] = "EradicatorToy",
+        ["Animations"] = {"Animation"},
+        ["StartAnimation"] = "Animation"
+    },
+    ["ChainsawDance"] = {["Animations"] = {"Animation"}},
+    ["Hot Chocolate"] = {
+        ["ModelName"] = "Hot Chocolate",
+        ["Animations"] = {"Animation"},
+        ["InteractAnimation"] = "Animation"
+    },
+    ["Conga"] = {
+        ["SoundId"] = 18961572950,
+        ["Animations"] = {"Animation", "Animation", "Animation", "Animation", "Animation"},
+        ["Partner"] = true
+    }
+}
