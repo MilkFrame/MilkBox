7b1bd763c12503fe70e7c271f4280157d3d1cf8c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:31:59 +1100

    Update MapData.lua

--- GameData/Tower Defense X [9503261072]/MapData.lua
+++ GameData/Tower Defense X [9503261072]/MapData.lua
@@ -1,86 +1,86 @@
 data = {
-    ["Chalet"] = {
-        ["Difficulty"] = "Normal",
-        ["Image"] = "http://www.roblox.com/asset/?id=15728751031",
-        ["Creator"] = "archmaestros"
+    ["Atrazine Test Map"] = {
+        ["Difficulty"] = "Easy",
+        ["IsEventOnlyMap"] = true,
+        ["Creator"] = "Atrazine",
+        ["Image"] = ""
     },
-    ["Moon Outpost"] = {["Difficulty"] = "Easy", ["Image"] = "rbxassetid://15638522013", ["Creator"] = "V_eeer"},
-    ["Assembly Line"] = {
-        ["Difficulty"] = "Normal",
-        ["Image"] = "rbxassetid://17224097562",
-        ["Creator"] = "archmaestros"
-    },
+    ["Tutorial"] = {["Difficulty"] = "Easy", ["IsEventOnlyMap"] = true, ["Creator"] = "archmaestros", ["Image"] = ""},
+    ["Dead End Valley"] = {["Difficulty"] = "Hard", ["Image"] = "rbxassetid://18570868504", ["Creator"] = "rikaforge"},
     ["Santa's Stronghold"] = {
         ["Difficulty"] = "Easy",
         ["IsEventOnlyMap"] = true,
         ["Creator"] = "archmaestros",
         ["Image"] = "rbxassetid://15657682478"
     },
-    ["Research Base"] = {
-        ["Difficulty"] = "Simple",
-        ["Image"] = "rbxassetid://17310102408",
+    ["Factorium"] = {
+        ["Difficulty"] = "Normal",
+        ["IsEventOnlyMap"] = true,
+        ["Creator"] = "archmaestros",
+        ["Image"] = "rbxassetid://114767155807126"
+    },
+    ["Simulation"] = {
+        ["Difficulty"] = "Normal",
+        ["Image"] = "rbxassetid://126572351510197",
         ["Creator"] = "archmaestros"
     },
-    ["Scorched Passage"] = {
-        ["Difficulty"] = "Hard",
-        ["Image"] = "rbxassetid://17224095196",
-        ["Creator"] = "archmaestros"
-    },
-    ["Blade Works"] = {["Difficulty"] = "Impossible", ["Image"] = "rbxassetid://17221050497", ["Creator"] = "rikaforge"},
     ["Obscure Island"] = {["Difficulty"] = "Hard", ["Image"] = "rbxassetid://14762424172", ["Creator"] = "V_eeer"},
-    ["Singularity"] = {["Difficulty"] = "Easy", ["Image"] = "rbxassetid://17224114428", ["Creator"] = "archmaestros"},
-    ["Treasure Cove"] = {["Difficulty"] = "Easy", ["Image"] = "rbxassetid://16283237864", ["Creator"] = "rikaforge"},
+    ["Baseplate"] = {
+        ["Difficulty"] = "Impossible",
+        ["Image"] = "rbxassetid://131913672135346",
+        ["Creator"] = "JOHN ROBLOX :3"
+    },
     ["Danger Beach"] = {
         ["Difficulty"] = "Normal",
         ["Image"] = "rbxassetid://12381096186",
         ["Creator"] = "ExtremelyHugeBadger"
     },
     ["Winter Fort"] = {["Difficulty"] = "Hard", ["Image"] = "rbxassetid://13747204507", ["Creator"] = "V_eeer"},
-    ["Limbo"] = {["Difficulty"] = "Hard", ["Image"] = "rbxassetid://17224114862", ["Creator"] = "rikaforge"},
-    ["Blox Out"] = {
-        ["Difficulty"] = "Easy",
-        ["Image"] = "rbxassetid://12381095833",
-        ["Creator"] = "ExtremelyHugeBadger",
-        ["DisplayName"] = "Blox Out"
-    },
-    ["Unforgiving Winter"] = {
-        ["Difficulty"] = "Hard",
-        ["Image"] = "rbxassetid://14005165044",
-        ["Creator"] = "ExtremelyHugeBadger"
-    },
-    ["Ancient Sky Island"] = {
+    ["Limbo"] = {["Difficulty"] = "Normal", ["Image"] = "rbxassetid://17224114862", ["Creator"] = "rikaforge"},
+    ["Research Base"] = {
         ["Difficulty"] = "Simple",
-        ["Image"] = "rbxassetid://14762424368",
-        ["Creator"] = "epicdjgameking"
+        ["Image"] = "rbxassetid://17310102408",
+        ["Creator"] = "archmaestros"
     },
-    ["Purgatory"] = {["Difficulty"] = "Normal", ["Image"] = "rbxassetid://17224115263", ["Creator"] = "rikaforge"},
-    ["Military Harbor"] = {["Difficulty"] = "Normal", ["Image"] = "rbxassetid://12968961228", ["Creator"] = "Scrufus"},
-    ["Carrier"] = {["Difficulty"] = "Hard", ["Image"] = "rbxassetid://16283385109", ["Creator"] = "archmaestros"},
-    ["Volcanic Mishap"] = {["Difficulty"] = "Hard", ["Image"] = "rbxassetid://14762424541", ["Creator"] = "V_eeer"},
-    ["Fort Summit"] = {
+    ["Fortress Snowlyn"] = {
+        ["Difficulty"] = "Normal",
+        ["IsEventOnlyMap"] = true,
+        ["Creator"] = "archmaestros",
+        ["Image"] = "rbxassetid://138450250998980"
+    },
+    ["Calamity"] = {
+        ["Difficulty"] = "Normal",
+        ["IsEventOnlyMap"] = true,
+        ["Creator"] = "archmaestros",
+        ["Image"] = "rbxassetid://114328197269384"
+    },
+    ["Singularity"] = {["Difficulty"] = "Easy", ["Image"] = "rbxassetid://17224114428", ["Creator"] = "archmaestros"},
+    ["Atrazine Test Map Short"] = {
         ["Difficulty"] = "Easy",
-        ["Image"] = "http://www.roblox.com/asset/?id=15728752874",
-        ["Creator"] = "rikaforge"
+        ["IsEventOnlyMap"] = true,
+        ["Creator"] = "Atrazine",
+        ["Image"] = ""
     },
-    ["SFOTH"] = {["Difficulty"] = "Hard", ["Image"] = "rbxassetid://12381095407", ["Creator"] = "ExtremelyHugeBadger"},
-    ["Vinland"] = {
-        ["Difficulty"] = "Normal",
-        ["Image"] = "http://www.roblox.com/asset/?id=15728754287",
-        ["Creator"] = "rikaforge"
-    },
-    ["Misleading Pond"] = {
+    ["Volcanic Mishap"] = {["Difficulty"] = "Hard", ["Image"] = "rbxassetid://14762424541", ["Creator"] = "V_eeer"},
+    ["Grasslands"] = {
         ["Difficulty"] = "Hard",
-        ["Image"] = "rbxassetid://15360246041",
-        ["Creator"] = "archmaestros"
+        ["IsEventOnlyMap"] = true,
+        ["Creator"] = "archmaestros",
+        ["Image"] = "rbxassetid://18229669213"
     },
-    ["Vapor City"] = {["Difficulty"] = "Normal", ["Image"] = "rbxassetid://15360245792", ["Creator"] = "V_eeer"},
-    ["Oil Rig"] = {["Difficulty"] = "Simple", ["Image"] = "rbxassetid://12381094725", ["Creator"] = "V_eeer"},
-    ["Secret Forest"] = {["Difficulty"] = "Normal", ["Image"] = "rbxassetid://14005150270", ["Creator"] = "V_eeer"},
-    ["Cow Annoyance"] = {
+    ["Arctic Research Base"] = {
         ["Difficulty"] = "Normal",
-        ["Image"] = "rbxassetid://15360245910",
-        ["Creator"] = "archmaestros"
+        ["IsEventOnlyMap"] = true,
+        ["Creator"] = "archmaestros",
+        ["Image"] = "rbxassetid://116939353847398"
     },
+    ["Outpost Forge"] = {
+        ["Difficulty"] = "Simple",
+        ["Image"] = "rbxassetid://108249868569924",
+        ["Creator"] = "rikaforge"
+    },
+    ["Secret Forest"] = {["Difficulty"] = "Normal", ["Image"] = "rbxassetid://14005150270", ["Creator"] = "V_eeer"},
+    ["Asteroid"] = {["Difficulty"] = "Impossible", ["Image"] = "rbxassetid://81519037839373", ["Creator"] = "rikaforge"},
     ["Sorrows Harbor"] = {
         ["Difficulty"] = "Hard",
         ["Image"] = "http://www.roblox.com/asset/?id=17224089112",
@@ -89,7 +89,121 @@ data = {
     ["Deserted Island"] = {
         ["Difficulty"] = "Impossible",
         ["Image"] = "rbxassetid://14762424065",
-        ["Creator"] = "JOHN ROBLOX"
+        ["Creator"] = "JOHN ROBLOX :3"
+    },
+    ["Ragnarok"] = {
+        ["Difficulty"] = "Normal",
+        ["IsEventOnlyMap"] = true,
+        ["Creator"] = "archmaestros",
+        ["Image"] = "rbxassetid://102820695103360"
+    },
+    ["Chalet"] = {
+        ["Difficulty"] = "Normal",
+        ["Image"] = "http://www.roblox.com/asset/?id=15728751031",
+        ["Creator"] = "archmaestros"
+    },
+    ["Route"] = {["Difficulty"] = "Normal", ["Image"] = "rbxassetid://89557375840987", ["Creator"] = "rikaforge"},
+    ["Moon Outpost"] = {["Difficulty"] = "Easy", ["Image"] = "rbxassetid://15638522013", ["Creator"] = "V_eeer"},
+    ["Assembly Line"] = {
+        ["Difficulty"] = "Normal",
+        ["Image"] = "rbxassetid://17224097562",
+        ["Creator"] = "archmaestros"
+    },
+    ["Military Base"] = {
+        ["Difficulty"] = "Normal",
+        ["IsEventOnlyMap"] = true,
+        ["Creator"] = "archmaestros",
+        ["Image"] = "rbxassetid://18229669382"
+    },
+    ["Borderlands"] = {
+        ["Difficulty"] = "Easy",
+        ["IsEventOnlyMap"] = true,
+        ["Creator"] = "archmaestros",
+        ["Image"] = "rbxassetid://18229668764"
+    },
+    ["Pond"] = {
+        ["Difficulty"] = "Normal",
+        ["IsEventOnlyMap"] = true,
+        ["Creator"] = "archmaestros",
+        ["Image"] = "rbxassetid://18229668930"
+    },
+    ["Blade Works"] = {["Difficulty"] = "Impossible", ["Image"] = "rbxassetid://17221050497", ["Creator"] = "rikaforge"},
+    ["Oil Rig"] = {["Difficulty"] = "Simple", ["Image"] = "rbxassetid://12381094725", ["Creator"] = "V_eeer"},
+    ["Western"] = {
+        ["Difficulty"] = "Simple",
+        ["IsEventOnlyMap"] = true,
+        ["Creator"] = "archmaestros",
+        ["Image"] = "rbxassetid://18229668570"
+    },
+    ["Treasure Cove"] = {["Difficulty"] = "Easy", ["Image"] = "rbxassetid://16283237864", ["Creator"] = "rikaforge"},
+    ["Survival 202"] = {
+        ["Difficulty"] = "Hard",
+        ["Image"] = "rbxassetid://134760156216140",
+        ["Creator"] = "JOHN ROBLOX :3"
+    },
+    ["Anchor"] = {["Difficulty"] = "Simple", ["Image"] = "rbxassetid://94390594102421", ["Creator"] = "rikaforge"},
+    ["Hellspire"] = {
+        ["Difficulty"] = "Normal",
+        ["IsEventOnlyMap"] = true,
+        ["Creator"] = "archmaestros",
+        ["Image"] = "rbxassetid://98729090058422"
+    },
+    ["Junction"] = {["Difficulty"] = "Easy", ["Image"] = "rbxassetid://83022637561332", ["Creator"] = "rikaforge"},
+    ["Unforgiving Winter"] = {
+        ["Difficulty"] = "Hard",
+        ["Image"] = "rbxassetid://14005165044",
+        ["Creator"] = "ExtremelyHugeBadger"
+    },
+    ["Ancient Sky Island"] = {
+        ["Difficulty"] = "Easy",
+        ["Image"] = "rbxassetid://14762424368",
+        ["Creator"] = "epicdjgameking"
+    },
+    ["Hakurei Shrine"] = {
+        ["Difficulty"] = "Normal",
+        ["Image"] = "rbxassetid://129342615598802",
+        ["Creator"] = "rikaforge"
+    },
+    ["Military Harbor"] = {["Difficulty"] = "Normal", ["Image"] = "rbxassetid://12968961228", ["Creator"] = "Scrufus"},
+    ["Gas Station"] = {["Difficulty"] = "Hard", ["Image"] = "rbxassetid://121077544385568", ["Creator"] = "V_eeer"},
+    ["GDI Port City"] = {
+        ["Difficulty"] = "Normal",
+        ["Image"] = "rbxassetid://85903767805845",
+        ["Creator"] = "rikaforge"
+    },
+    ["Highrise"] = {["Difficulty"] = "Simple", ["Image"] = "rbxassetid://17683669972", ["Creator"] = "rikaforge"},
+    ["Blox Out"] = {
+        ["Difficulty"] = "Easy",
+        ["Image"] = "rbxassetid://12381095833",
+        ["Creator"] = "ExtremelyHugeBadger"
+    },
+    ["Vinland"] = {
+        ["Difficulty"] = "Normal",
+        ["Image"] = "http://www.roblox.com/asset/?id=15728754287",
+        ["Creator"] = "rikaforge"
+    },
+    ["Purgatory"] = {["Difficulty"] = "Normal", ["Image"] = "rbxassetid://17224115263", ["Creator"] = "rikaforge"},
+    ["Fort Summit"] = {
+        ["Difficulty"] = "Simple",
+        ["Image"] = "http://www.roblox.com/asset/?id=15728752874",
+        ["Creator"] = "rikaforge"
+    },
+    ["Vapor City"] = {["Difficulty"] = "Normal", ["Image"] = "rbxassetid://15360245792", ["Creator"] = "V_eeer"},
+    ["Scorched Passage"] = {
+        ["Difficulty"] = "Hard",
+        ["Image"] = "rbxassetid://17224095196",
+        ["Creator"] = "archmaestros"
+    },
+    ["Cow Annoyance"] = {
+        ["Difficulty"] = "Normal",
+        ["Image"] = "rbxassetid://15360245910",
+        ["Creator"] = "archmaestros"
+    },
+    ["SFOTH"] = {["Difficulty"] = "Hard", ["Image"] = "rbxassetid://12381095407", ["Creator"] = "ExtremelyHugeBadger"},
+    ["Misleading Pond"] = {
+        ["Difficulty"] = "Hard",
+        ["Image"] = "rbxassetid://15360246041",
+        ["Creator"] = "archmaestros"
     },
     ["Apocalypse"] = {["Difficulty"] = "Easy", ["Image"] = "rbxassetid://12968961477", ["Creator"] = "V_eeer"}
 }
