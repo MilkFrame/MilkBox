df833c4d55cd03eb07fa040b9529841d6ffae276
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:30:21 +1100

    Create Advisors.lua

---
+++ GameData/Tower Defense X [9503261072]/Advisors.lua
@@ -0,0 +1,165 @@
+data = {
+    ["Xenon"] = {
+        ["Pitch"] = 1.25,
+        ["Ratio"] = 0.698,
+        ["Image"] = "rbxassetid://15897570239",
+        ["WaveOverrides"] = {
+            "rbxassetid://15897570795",
+            "rbxassetid://15897570795",
+            "rbxassetid://15897570795",
+            "rbxassetid://15897571974",
+            "rbxassetid://15897571974",
+            "rbxassetid://15897571265",
+            "rbxassetid://15897571974",
+            "rbxassetid://15897571265",
+            "rbxassetid://15897571265",
+            "rbxassetid://15897570795",
+            "rbxassetid://15897571974"
+        }
+    },
+    ["Christmas24Part2_Advisor"] = {
+        ["Pitch"] = 1,
+        ["Ratio"] = 0.7731958762886598,
+        ["Image"] = "rbxassetid://103722341093286",
+        ["WaveOverrides"] = {
+            "rbxassetid://103722341093286",
+            "rbxassetid://92413812327485",
+            "rbxassetid://88943500356527",
+            "rbxassetid://92413812327485",
+            "rbxassetid://92413812327485",
+            "rbxassetid://92413812327485",
+            "rbxassetid://88943500356527"
+        }
+    },
+    ["Christmas24Part1Nightmare_Xenon"] = {
+        ["Pitch"] = 1.25,
+        ["Ratio"] = 0.698,
+        ["Image"] = "rbxassetid://15897570239",
+        ["WaveOverrides"] = {
+            "rbxassetid://15897571265",
+            "rbxassetid://15897572538",
+            "rbxassetid://15897570795",
+            "rbxassetid://15897572538",
+            "rbxassetid://15897572538",
+            "rbxassetid://15897572538",
+            "rbxassetid://15897571265",
+            "rbxassetid://15897570795",
+            "rbxassetid://15897571974"
+        }
+    },
+    ["Heretic"] = {
+        ["Pitch"] = 0.66,
+        ["Ratio"] = 1.15,
+        ["Image"] = "rbxassetid://131222017224641",
+        ["WaveOverrides"] = {}
+    },
+    ["Christmas24Part2Nightmare_Xenon"] = {
+        ["Pitch"] = 1.25,
+        ["Ratio"] = 0.698,
+        ["Image"] = "rbxassetid://15897570239",
+        ["WaveOverrides"] = {
+            "rbxassetid://15897571265",
+            "rbxassetid://15897572538",
+            "rbxassetid://15897570795",
+            "rbxassetid://15897572538",
+            "rbxassetid://15897572538",
+            "rbxassetid://15897572538",
+            "rbxassetid://15897571265",
+            "rbxassetid://15897570795",
+            "rbxassetid://15897571974"
+        }
+    },
+    ["AdvisorHalloweenPart4"] = {
+        ["Pitch"] = 1,
+        ["Ratio"] = 0.7731958762886598,
+        ["Image"] = "rbxassetid://103722341093286",
+        ["WaveOverrides"] = {
+            "rbxassetid://103722341093286",
+            "rbxassetid://92413812327485",
+            "rbxassetid://103722341093286",
+            "rbxassetid://88943500356527"
+        }
+    },
+    ["WretchedHalloweenPart4Nightmare"] = {
+        ["Pitch"] = 0.6,
+        ["Ratio"] = 1,
+        ["Image"] = "rbxassetid://139685719743994",
+        ["WaveOverrides"] = {"rbxassetid://123089217617885", "rbxassetid://123089217617885"}
+    },
+    ["Christmas24Part1_Advisor"] = {
+        ["Pitch"] = 1,
+        ["Ratio"] = 0.7731958762886598,
+        ["Image"] = "rbxassetid://103722341093286",
+        ["WaveOverrides"] = {
+            "rbxassetid://103722341093286",
+            "rbxassetid://92413812327485",
+            "rbxassetid://88943500356527",
+            "rbxassetid://92413812327485",
+            "rbxassetid://92413812327485",
+            "rbxassetid://92413812327485",
+            "rbxassetid://88943500356527"
+        }
+    },
+    ["Commander"] = {
+        ["Pitch"] = 0.9,
+        ["Ratio"] = 1.0893617021276596,
+        ["Image"] = "rbxassetid://18401247301",
+        ["WaveOverrides"] = {
+            "rbxassetid://18401247691",
+            "rbxassetid://18401247691",
+            "rbxassetid://18401246925",
+            "rbxassetid://18401246925",
+            "rbxassetid://18401246925",
+            "rbxassetid://18401247691",
+            "rbxassetid://18401246925",
+            "rbxassetid://18401246925",
+            "rbxassetid://18401246925",
+            "rbxassetid://18401246925",
+            "rbxassetid://18401246925"
+        }
+    },
+    ["Default"] = {["Image"] = "rbxassetid://14063853703", ["Ratio"] = 0.776},
+    ["JohnHalloweenPart2"] = {
+        ["Pitch"] = 0.87,
+        ["Ratio"] = 0.7843137254901961,
+        ["Image"] = "rbxassetid://77173013788614",
+        ["WaveOverrides"] = {
+            "rbxassetid://89237802777431",
+            "rbxassetid://132903418951868",
+            "rbxassetid://132903418951868",
+            "rbxassetid://132903418951868"
+        }
+    },
+    ["Void"] = {
+        ["Pitch"] = 0.66,
+        ["Ratio"] = 1,
+        ["Image"] = "rbxassetid://18461271274",
+        ["WaveOverrides"] = {"rbxassetid://18461271643"}
+    },
+    ["OilHalloweenPart3"] = {
+        ["Pitch"] = 0.92,
+        ["Ratio"] = 0.6615003099814011,
+        ["Image"] = "rbxassetid://140300254572160",
+        ["WaveOverrides"] = {
+            "rbxassetid://121243786748178",
+            "rbxassetid://108043524931527",
+            "rbxassetid://134946544978793"
+        }
+    },
+    ["OilHalloweenPart3Nightmare"] = {
+        ["Pitch"] = 0.92,
+        ["Ratio"] = 0.6615003099814011,
+        ["Image"] = "rbxassetid://140300254572160",
+        ["WaveOverrides"] = {
+            "rbxassetid://121243786748178",
+            "rbxassetid://108043524931527",
+            "rbxassetid://134946544978793"
+        }
+    },
+    ["Engineer"] = {
+        ["Pitch"] = 0.66,
+        ["Ratio"] = 1.15,
+        ["Image"] = "rbxassetid://103266163217688",
+        ["WaveOverrides"] = {}
+    }
+}
