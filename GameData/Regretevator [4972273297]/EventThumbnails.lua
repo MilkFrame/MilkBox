44b551a6c4a49416ba04d4d919fb221eeff68d3c
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:16:53 +1100

    Create EventThumbnails.lua

---
+++ GameData/Regretevator [4972273297]/EventThumbnails.lua
@@ -0,0 +1,42 @@
+data = {
+    ["Lethal"] = {
+        ["Color"] = Color3.fromRGB(255, 0, 0),
+        ["Icon"] = "rbxassetid://3246716407",
+        ["DONT_RecolorIcon"] = false
+    },
+    ["Player Modifier"] = {
+        ["Color"] = Color3.fromRGB(111, 255, 101),
+        ["Icon"] = "rbxassetid://12049470978",
+        ["DONT_RecolorIcon"] = false
+    },
+    ["William"] = {["ImageID"] = 91852446676671},
+    ["Coins"] = {["ImageID"] = 105350811039198},
+    ["FunnyText"] = {["ImageID"] = 115331734728235},
+    ["Confetti"] = {["ImageID"] = 88581070719747},
+    ["TV"] = {["ImageID"] = 122745393723952},
+    ["Spike"] = {["ImageID"] = 127836556392675},
+    ["Freeze"] = {["ImageID"] = 132382709079377},
+    ["JAOBA"] = {["ImageID"] = 126310464080837},
+    ["Squish"] = {["ImageID"] = 109350411405721},
+    ["HotRails"] = {["ImageID"] = 132559491576686},
+    ["Buttons"] = {["ImageID"] = 93529750576703},
+    ["Fire"] = {["ImageID"] = 85763716183884},
+    ["PaintingPanels"] = {["ImageID"] = 76301801836706},
+    ["KnockKnock"] = {["ImageID"] = 95615550501007},
+    ["Conditioner"] = {["ImageID"] = 78728701940889},
+    ["NPCs"] = {
+        ["Color"] = Color3.fromRGB(47, 175, 255),
+        ["Icon"] = "rbxassetid://12049470978",
+        ["DONT_RecolorIcon"] = false
+    },
+    ["PaintingCompartment"] = {["ImageID"] = 78841575954127},
+    ["Mozelle"] = {["ImageID"] = 122423941442382},
+    ["Elevator Modifier"] = {
+        ["Color"] = Color3.fromRGB(156, 57, 255),
+        ["Icon"] = "rbxassetid://13002153057",
+        ["DONT_RecolorIcon"] = false
+    },
+    ["MorseCode"] = {["ImageID"] = 87670248225680},
+    ["Flood"] = {["ImageID"] = 112388752954506},
+    ["Freefall"] = {["ImageID"] = 97291414882721}
+}
