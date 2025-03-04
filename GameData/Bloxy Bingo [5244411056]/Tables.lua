9b739239d75906485e81b86b73e77f2a4e23fbd2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:23:16 +1100

    Create Tables.lua

---
+++ GameData/Bloxy Bingo [5244411056]/Tables.lua
@@ -0,0 +1,214 @@
+data = {
+    {
+        ["isForSale"] = true,
+        ["image"] = "rbxassetid://5422506710",
+        ["name"] = "Standard Table",
+        ["model"] = "ReservedTable",
+        ["availability"] = "Essential",
+        ["cost"] = 0,
+        ["description"] = "Standard table, just with a reserved sign because, well, it's reserved for you.",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "caramelchoc"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["image"] = "rbxassetid://5422467282",
+        ["name"] = "Low Table",
+        ["model"] = "LowTable",
+        ["availability"] = "Essential",
+        ["cost"] = 100,
+        ["description"] = "Just a low down table for testing purposes.",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "centraltrains"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["image"] = "rbxassetid://5422468538",
+        ["name"] = "Sci-Fi Table",
+        ["model"] = "SciFiTable",
+        ["availability"] = "Essential",
+        ["cost"] = 600,
+        ["description"] = "Space. There's nothing  to do. But, a bit of bingo may go down a treat.",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "caramelchoc"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["image"] = "rbxassetid://5422467843",
+        ["name"] = "Neptune",
+        ["model"] = "Neptune",
+        ["availability"] = "Essential",
+        ["cost"] = 500,
+        ["description"] = "Even the big brains in space need comfy seats.",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "caramelchoc"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["image"] = "rbxassetid://5434435454",
+        ["name"] = "Bench",
+        ["model"] = "Bench",
+        ["availability"] = "Essential",
+        ["cost"] = 350,
+        ["description"] = "Nice day for a picnic... and some Bingo!",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "caramelchoc"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["image"] = "rbxassetid://5434435817",
+        ["name"] = "Green House",
+        ["model"] = "GreenHouse",
+        ["availability"] = "Essential",
+        ["cost"] = 12000,
+        ["description"] = "Caution: Might get a bit warm, especially during the summer.",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "caramelchoc"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["image"] = "rbxassetid://5694731056",
+        ["name"] = "Tree Table",
+        ["model"] = "Tree",
+        ["availability"] = "Essential",
+        ["cost"] = 500,
+        ["description"] = "Bingo out in the sticks.",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "caramelchoc"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["image"] = "rbxassetid://5694730731",
+        ["name"] = "Castle",
+        ["model"] = "Castle",
+        ["availability"] = "Essential",
+        ["cost"] = 600,
+        ["description"] = "Ye shall never pass in.",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "caramelchoc"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["image"] = "rbxassetid://5694730904",
+        ["name"] = "School Desks",
+        ["model"] = "School",
+        ["availability"] = "Essential",
+        ["cost"] = 1600,
+        ["description"] = "Not paying attention? Detention.",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "caramelchoc"}}
+    },
+    {
+        ["isForSale"] = true,
+        ["image"] = "rbxassetid://5694730578",
+        ["name"] = "Boaty",
+        ["model"] = "Boaty",
+        ["availability"] = "Essential",
+        ["cost"] = 2400,
+        ["description"] = "Boaty means boaty, and we're going to make a titanic success of it!",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "caramelchoc"}}
+    },
+    {
+        ["description"] = "Bingo in the trees.",
+        ["cost"] = 800,
+        ["newlyAvailableFrom"] = 1683217800,
+        ["image"] = "rbxassetid://13248752177",
+        ["name"] = "Treetop Table",
+        ["model"] = "TreetopTable",
+        ["newlyAvailableUntil"] = 1683995400,
+        ["isForSale"] = true,
+        ["availability"] = "Essential",
+        ["credits"] = {
+            {
+                ["creditType"] = "Custom",
+                ["textColor"] = Color3.fromRGB(255, 255, 255),
+                ["creditName"] = "Jaunty Art Studio",
+                ["colour"] = Color3.fromRGB(0, 0, 0)
+            }
+        }
+    },
+    {
+        ["description"] = "You'll attract the bingo bees with this table!",
+        ["cost"] = 600,
+        ["newlyAvailableFrom"] = 1691253000,
+        ["image"] = "rbxassetid://14065742860",
+        ["name"] = "Flower Table",
+        ["model"] = "FlowerTable",
+        ["newlyAvailableUntil"] = 1692549000,
+        ["isForSale"] = true,
+        ["availability"] = "Essential",
+        ["credits"] = {
+            {
+                ["creditType"] = "Custom",
+                ["textColor"] = Color3.fromRGB(255, 255, 255),
+                ["creditName"] = "Jaunty Art Studio",
+                ["colour"] = Color3.fromRGB(0, 0, 0)
+            }
+        }
+    },
+    {
+        ["description"] = "Are we Bingoing yet? Are we Bingoing yet?!?!",
+        ["cost"] = 1200,
+        ["newlyAvailableFrom"] = 1699115400,
+        ["image"] = "rbxassetid://15263139033",
+        ["name"] = "Train Table",
+        ["model"] = "TrainTable",
+        ["newlyAvailableUntil"] = 1700325000,
+        ["isForSale"] = true,
+        ["availability"] = "Essential",
+        ["credits"] = {
+            {
+                ["creditType"] = "Custom",
+                ["textColor"] = Color3.fromRGB(255, 255, 255),
+                ["creditName"] = "Jaunty Art Studio",
+                ["colour"] = Color3.fromRGB(0, 0, 0)
+            }
+        }
+    },
+    {
+        ["description"] = "Nice day for a picnic, got any food? No?! Just bingo cards...",
+        ["cost"] = 600,
+        ["newlyAvailableFrom"] = 1712421000,
+        ["image"] = "rbxassetid://16778856311",
+        ["name"] = "Picnic Rug",
+        ["model"] = "PicnicRug",
+        ["newlyAvailableUntil"] = 1713630600,
+        ["isForSale"] = true,
+        ["availability"] = "Essential",
+        ["credits"] = {
+            {
+                ["creditType"] = "Custom",
+                ["textColor"] = Color3.fromRGB(255, 255, 255),
+                ["creditName"] = "Jaunty Art Studio",
+                ["colour"] = Color3.fromRGB(0, 0, 0)
+            }
+        }
+    },
+    {
+        ["description"] = "Subordinates - you are playing Bingo for me. MORE BINGO, MORRREEEEE BINGOOOOOOO!!!!",
+        ["cost"] = 8000,
+        ["newlyAvailableFrom"] = 1720283400,
+        ["image"] = "rbxassetid://17880845566",
+        ["name"] = "Office Desk",
+        ["model"] = "OfficeDesk",
+        ["newlyAvailableUntil"] = 1721493000,
+        ["isForSale"] = true,
+        ["availability"] = "Essential",
+        ["credits"] = {
+            {
+                ["creditType"] = "Custom",
+                ["textColor"] = Color3.fromRGB(255, 255, 255),
+                ["creditName"] = "Jaunty Art Studio",
+                ["colour"] = Color3.fromRGB(0, 0, 0)
+            }
+        }
+    },
+    {
+        ["isForSale"] = true,
+        ["image"] = "rbxassetid://104863573871489",
+        ["name"] = "Standard Table",
+        ["model"] = "ReservedTableV2",
+        ["availability"] = "Essential",
+        ["cost"] = 0,
+        ["description"] = "Standard table, just with a reserved sign because, well, it's reserved for you.",
+        ["credits"] = {
+            {
+                ["creditType"] = "Custom",
+                ["textColor"] = Color3.fromRGB(255, 255, 255),
+                ["creditName"] = "Jaunty Art Studio",
+                ["colour"] = Color3.fromRGB(0, 0, 0)
+            }
+        }
+    }
+}
