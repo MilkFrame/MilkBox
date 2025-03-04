32e77ed915de89ecd6e6ac96ddf64db5cfba90cc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:15:57 +1100

    Create DailyChallenge.lua

---
+++ GameData/Regretevator [4972273297]/DailyChallenge.lua
@@ -0,0 +1,85 @@
+data = {
+    ["CHALLENGE__TEMPLATE"] = {
+        ["Description"] = "Do something %s times.",
+        ["IsTaskPossible"] = "function: 0x75d14b3a44b74419",
+        ["InitializeFunc"] = "function: 0xcef4a82b01d1e439",
+        ["TrackingFunc"] = "function: 0x2788ed91ceda1dd9",
+        ["Parameters"] = {},
+        ["AmountRange"] = "2 4 "
+    },
+    ["Challenges"] = {
+        ["Jump"] = {
+            ["TrackingFunc"] = "function: 0xdb78a0d2fecd0aa9",
+            ["Description"] = "Jump %s times in a session while actively playing.",
+            ["AmountRange"] = "1000 2000 "
+        },
+        ["ChairSitting"] = {
+            ["TrackingFunc"] = "function: 0x19135f6a04a9f5c9",
+            ["Description"] = "Sit in %s chairs on floors.",
+            ["AmountRange"] = "5 15 "
+        },
+        ["EffectJunkie"] = {
+            ["TrackingFunc"] = "function: 0x3768386132a32949",
+            ["Description"] = "Have %s effects applied at the same time.",
+            ["AmountRange"] = "5 8 "
+        },
+        ["FlowerPetaledNpcs"] = {
+            ["IsTaskPossible"] = "function: 0x14e911dee36f2e59",
+            ["TrackingFunc"] = "function: 0x3ffe042d1f598ab9",
+            ["Description"] = "Use Petal Cone on %s different elevator npcs.",
+            ["AmountRange"] = "5 8 "
+        },
+        ["FloorStreak"] = {
+            ["TrackingFunc"] = "function: 0x79c436e137ec7dd9",
+            ["Description"] = "Get a floor streak of %s.",
+            ["AmountRange"] = "8 15 "
+        },
+        ["ShootVoombas"] = {
+            ["IsTaskPossible"] = "function: 0xe457e2801d77c8f9",
+            ["TrackingFunc"] = "function: 0x3d4f507f796a6f59",
+            ["Description"] = "Shoot and hit voombas %s times.",
+            ["AmountRange"] = "6 13 "
+        },
+        ["SprayNpcs"] = {
+            ["IsTaskPossible"] = "function: 0x525cf4cc6af22999",
+            ["TrackingFunc"] = "function: 0x2a9b3f9e53831939",
+            ["Description"] = "Spray %s different elevator npcs.",
+            ["AmountRange"] = "5 8 "
+        },
+        ["BuyThings"] = {
+            ["TrackingFunc"] = "function: 0x0584ccd3d15583b9",
+            ["Description"] = "Buy %s things from floors.",
+            ["AmountRange"] = "5 15 "
+        },
+        ["SurviveFloors"] = {
+            ["TrackingFunc"] = "function: 0x54ee9b38d530a9d9",
+            ["Description"] = "Survive %s floors.",
+            ["AmountRange"] = "25 30 "
+        },
+        ["KnockYourself"] = {
+            ["TrackingFunc"] = "function: 0x87e7214f0aa714a9",
+            ["Description"] = "Get knocked over %s times.",
+            ["AmountRange"] = "10 30 "
+        },
+        ["Walk"] = {
+            ["TrackingFunc"] = "function: 0x6583114fd2d00699",
+            ["Description"] = "Travel a distance of %s studs while actively playing.",
+            ["AmountRange"] = "10000 30000 "
+        }
+    },
+    ["DCHelper"] = {
+        ["GetStreakData"] = "function: 0x3af5b27f2c2d9839",
+        ["GetToday"] = "function: 0x1e69e315c7e1e3b9",
+        ["REWARD_IMAGES"] = {
+            ["Stickers"] = "Stickers",
+            ["FloorTickets"] = "FloorTickets",
+            ["Lanky"] = "Lanky",
+            ["ChallengeCredits"] = "ChallengeCredits",
+            ["Fafa"] = "Fafa",
+            ["Coins"] = "Coins"
+        },
+        ["GiveRewards"] = "function: 0x54c3497d27bb5169",
+        ["GetRewards"] = "function: 0x7b2719a91248e2f9",
+        ["GetRewardAmtForStreak"] = "function: 0x51ed413fdf781549"
+    }
+}
