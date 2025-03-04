d7ce8e8e93c20e0f94864f1ebd1b52e0571a3752
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:24:43 +1100

    Create CustomCredits.lua

---
+++ GameData/Bloxy Bingo [5244411056]/CustomCredits.lua
@@ -0,0 +1,42 @@
+data = {
+    ["HONEYIIP"] = {{["creditType"] = "Roblox", ["creditName"] = "honeyiip"}},
+    ["DYLANDILLAN"] = {{["creditType"] = "fiverr", ["creditName"] = "dylandillan"}},
+    ["DVLPR"] = {{["creditType"] = "Roblox", ["creditName"] = "Dvlpr"}},
+    ["RED"] = {{["creditType"] = "Roblox", ["creditName"] = "red"}},
+    ["SENNATHAN01"] = {{["creditType"] = "fiverr", ["creditName"] = "seannathan01"}},
+    ["VILLANELLE"] = {
+        {
+            ["creditType"] = "Custom",
+            ["textColor"] = Color3.fromRGB(255, 255, 255),
+            ["creditName"] = "villanelle",
+            ["colour"] = Color3.fromRGB(0, 0, 0)
+        }
+    },
+    ["DANYANATRINDADE"] = {{["creditType"] = "fiverr", ["creditName"] = "dayanatrindade"}},
+    ["IDIOFY3D"] = {{["creditType"] = "Roblox", ["creditName"] = "Idiofy3d"}},
+    ["DEVOURER_OF_BREAD"] = {{["creditType"] = "Roblox", ["creditName"] = "DevourerOfBread"}},
+    ["CENTRALTRAINS"] = {{["creditType"] = "Roblox", ["creditName"] = "centraltrains"}},
+    ["ZOOMCODE"] = {{["creditType"] = "Roblox", ["creditName"] = "ZoomCode"}},
+    ["ANIMALBOT99"] = {{["creditType"] = "Roblox", ["creditName"] = "AnimalBot99"}},
+    ["CARAMELCHOC"] = {{["creditType"] = "Roblox", ["creditName"] = "caramelchoc"}},
+    ["BSLICK"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}},
+    ["SIREHNS"] = {{["creditType"] = "Roblox", ["creditName"] = "sirehns"}},
+    ["JAUNTY"] = {
+        {
+            ["creditType"] = "Custom",
+            ["textColor"] = Color3.fromRGB(255, 255, 255),
+            ["creditName"] = "Jaunty Art Studio",
+            ["colour"] = Color3.fromRGB(0, 0, 0)
+        }
+    },
+    ["DOM"] = {
+        {
+            ["creditType"] = "Custom",
+            ["textColor"] = Color3.fromRGB(255, 255, 255),
+            ["creditName"] = "Dom",
+            ["colour"] = Color3.fromRGB(0, 0, 0)
+        }
+    },
+    ["EDO_UK"] = {{["creditType"] = "Roblox", ["creditName"] = "Edo_UK"}},
+    ["HHWHEAT"] = {{["creditType"] = "Roblox", ["creditName"] = "hhwheat"}}
+}
