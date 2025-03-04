e9a96915ca7aa7886bbb4bdb4dcdcc410167d0f3
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:02:25 +1100

    Create Maps.lua

---
+++ GameData/Something evil will happen [16991287194]/Maps.lua
@@ -0,0 +1,22 @@
+data = {
+    ["Normal"] = {
+        ["Outlet Alley"] = {["lighting"] = "Evening", ["environment"] = "Ocean", ["Weight"] = 1111},
+        ["Mercy Hospital"] = {
+            ["lighting"] = "Night",
+            ["survivalBadge"] = "Mercy",
+            ["environment"] = "Rocks",
+            ["Weight"] = 1
+        },
+        ["Mountain Lodge"] = {["lighting"] = "EveningAltitude", ["environment"] = "Mountain", ["Weight"] = 1},
+        ["Winter Wonderland"] = {["lighting"] = "NightAltitude", ["environment"] = "SnowMountain", ["Weight"] = 1},
+        ["Side-Street"] = {["lighting"] = "Sunny", ["environment"] = "Ocean", ["Weight"] = 1},
+        ["Haunted Memories"] = {["lighting"] = "Night", ["environment"] = "DirtyOcean", ["Weight"] = 1},
+        ["Pirate Showdown"] = {["lighting"] = "Sunny", ["environment"] = "Ocean", ["Weight"] = 1},
+        ["City Slice"] = {["lighting"] = "Sunny", ["environment"] = "Ocean", ["Weight"] = 1}
+    },
+    ["Special"] = {
+        ["Spleef"] = {["lighting"] = "Sunny", ["environment"] = "Empty"},
+        ["Wipeout"] = {["lighting"] = "Sunny", ["environment"] = "Ocean"},
+        ["Minesweeper"] = {["lighting"] = "Sunny", ["environment"] = "Empty"}
+    }
+}
