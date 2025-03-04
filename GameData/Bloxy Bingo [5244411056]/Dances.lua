1f4b83aced24c6e4980872ee84eb7c9110f6d3c2
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:23:26 +1100

    Create Dances.lua

---
+++ GameData/Bloxy Bingo [5244411056]/Dances.lua
@@ -0,0 +1,394 @@
+data = {
+    {
+        ["availability"] = "Essential",
+        ["name"] = "Cheer",
+        ["animation"] = {["R15"] = "rbxassetid://5041789588", ["R6"] = "rbxassetid://5041787201"},
+        ["isForSale"] = true,
+        ["cost"] = 120,
+        ["description"] = "The best classic way to celebreate, a good old cheer.",
+        ["supportedRigs"] = {["R15"] = true, ["R6"] = true}
+    },
+    {
+        ["availability"] = "Rare",
+        ["name"] = "Dab",
+        ["animation"] = {["R15"] = "rbxassetid://5113366419", ["R6"] = "rbxassetid://5113363450"},
+        ["isForSale"] = true,
+        ["cost"] = 1200,
+        ["description"] = "You memester. Just a quick one to make for test purposes ;)",
+        ["supportedRigs"] = {["R15"] = true, ["R6"] = true}
+    },
+    {
+        ["availability"] = "Rare",
+        ["name"] = "H4cker",
+        ["animation"] = {["R15"] = "rbxassetid://5371208882", ["R6"] = "rbxassetid://5371205367"},
+        ["isForSale"] = true,
+        ["cost"] = 800,
+        ["description"] = "You are so good at bingo. So, so good, coincidence it is not. You must an elite hax0r, exploiting the computer procedural random generation to be manipulated in your favour. H4CKERR!!!!!!",
+        ["supportedRigs"] = {["R15"] = true, ["R6"] = true}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://12081545054", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Pumped Victory",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "seannathan01"}},
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://12081549291", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://12081564104", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Broken Robot",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "seannathan01"}},
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://12081567911", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://12081589820", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Spreaded Wave",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "seannathan01"}},
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://12081594037", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://12081641041", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Celebration Workout",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "seannathan01"}},
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://12081636699", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://12081759519", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Wasit Shuffle & Wave",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "dayanatrindade"}},
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://12081754977", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Essential",
+        ["name"] = "Hip-Hip-Hooray",
+        ["animation"] = {["R15"] = "rbxassetid://12082614474", ["R6"] = "rbxassetid://12082596654"},
+        ["cost"] = 80,
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "dylandillan"}},
+        ["description"] = "",
+        ["supportedRigs"] = {["R15"] = true, ["R6"] = true}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Essential",
+        ["name"] = "Clapping",
+        ["animation"] = {["R15"] = "rbxassetid://12082625975", ["R6"] = "rbxassetid://12082610323"},
+        ["cost"] = 80,
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "dylandillan"}},
+        ["description"] = "",
+        ["supportedRigs"] = {["R15"] = true, ["R6"] = true}
+    },
+    {
+        ["isForSale"] = true,
+        ["availability"] = "Essential",
+        ["name"] = "Surprise",
+        ["animation"] = {["R15"] = "rbxassetid://12082620482", ["R6"] = "rbxassetid://12082606592"},
+        ["cost"] = 80,
+        ["credits"] = {{["creditType"] = "fiverr", ["creditName"] = "dylandillan"}},
+        ["description"] = "",
+        ["supportedRigs"] = {["R15"] = true, ["R6"] = true}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375024825", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Dual Window Washing",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375028799", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375032842", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Call Towards",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375036365", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375040506", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Zone Assignment",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375044266", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375048819", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Archery Warmup",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375053051", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375059648", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Tick Tock Forwards and Back",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375063777", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375068690", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Reaching Stretches",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375072351", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375076647", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Caterpillar Wiggle",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375080255", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375084090", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Beat Emersion",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375088381", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375093495", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Stretch & Panic",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375097758", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375102640", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Cashing In",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375106897", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375111447", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Circle and Point",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375114788", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375119099", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Salutes",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375122866", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375126498", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "The Director",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375129534", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375136424", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Take-Off Celebration",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375140569", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375145265", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Hellos",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375148948", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375153016", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Wiggle",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375157376", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375161522", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Ready to Play",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375165649", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375169558", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "This or That",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375172988", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375177260", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "All Together Now!",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375181126", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375186464", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Winning for Me",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375189758", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375194494", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Grab the Sky",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375198006", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375202099", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Rolling Pin",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375206579", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375210508", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Switch it Up",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375217529", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375221281", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Rapid Win",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375224715", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375228383", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Reach and Paint",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375231882", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375237371", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Grasp and Shake",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375241107", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375245123", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Global Party",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375248908", ["R6"] = "rbxassetid://5041787201"}
+    },
+    {
+        ["description"] = "",
+        ["animation"] = {["R15"] = "rbxassetid://13375253550", ["R6"] = "rbxassetid://5041787201"},
+        ["availability"] = "Unusual",
+        ["name"] = "Checking Around",
+        ["isForSale"] = true,
+        ["cost"] = 200,
+        ["supportedRigs"] = {["R15"] = true},
+        ["animationStanding"] = {["R15"] = "rbxassetid://13375257988", ["R6"] = "rbxassetid://5041787201"}
+    }
+}
