d5b2ec4f524cec8679243dc6ea291a64a553a2f6
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:19:34 +1100

    Create CardThemes.lua

---
+++ GameData/Bloxy Bingo [5244411056]/CardThemes.lua
@@ -0,0 +1,59 @@
+data = {
+    ["GetColoursIndex"] = "function: 0xdd3a9a59a6073470",
+    ["Default"] = {
+        ["Outline"] = Color3.fromRGB(153, 153, 153),
+        ["ArcadeStripes"] = {
+            Color3.fromRGB(240, 45, 48),
+            Color3.fromRGB(247, 247, 0),
+            Color3.fromRGB(43, 150, 243),
+            Color3.fromRGB(245, 245, 245)
+        },
+        ["ColourBackgrounds"] = {
+            ["144, 255, 89"] = Color3.fromRGB(144, 255, 89),
+            ["255, 180, 89"] = Color3.fromRGB(255, 180, 89),
+            ["89, 255, 202"] = Color3.fromRGB(89, 255, 202),
+            ["178, 89, 255"] = Color3.fromRGB(178, 89, 255),
+            ["255, 233, 89"] = Color3.fromRGB(255, 233, 89),
+            ["255, 89, 89"] = Color3.fromRGB(255, 89, 89),
+            ["255, 89, 238"] = Color3.fromRGB(255, 89, 238),
+            ["95, 89, 255"] = Color3.fromRGB(95, 89, 255),
+            ["89, 200, 255"] = Color3.fromRGB(89, 200, 255)
+        },
+        ["Background"] = Color3.fromRGB(255, 255, 255),
+        ["PhysicalCards"] = {
+            ["ArcadeCard"] = "rbxassetid://5578450191",
+            ["UKCard"] = "rbxassetid://5148161140",
+            ["LoteriaCard"] = "rbxassetid://5668077327",
+            ["USCard"] = "rbxassetid://6117269460"
+        },
+        ["Text"] = Color3.fromRGB(0, 0, 0)
+    },
+    ["Dark"] = {
+        ["Outline"] = Color3.fromRGB(11, 11, 11),
+        ["ArcadeStripes"] = {
+            Color3.fromRGB(137, 44, 44),
+            Color3.fromRGB(186, 162, 69),
+            Color3.fromRGB(52, 61, 112),
+            Color3.fromRGB(40, 40, 40)
+        },
+        ["ColourBackgrounds"] = {
+            ["144, 255, 89"] = Color3.fromRGB(58, 103, 35),
+            ["255, 180, 89"] = Color3.fromRGB(141, 98, 49),
+            ["89, 255, 202"] = Color3.fromRGB(38, 109, 84),
+            ["178, 89, 255"] = Color3.fromRGB(83, 41, 120),
+            ["255, 233, 89"] = Color3.fromRGB(130, 119, 45),
+            ["255, 89, 89"] = Color3.fromRGB(125, 50, 50),
+            ["255, 89, 238"] = Color3.fromRGB(109, 38, 102),
+            ["95, 89, 255"] = Color3.fromRGB(42, 51, 138),
+            ["89, 200, 255"] = Color3.fromRGB(40, 66, 115)
+        },
+        ["Background"] = Color3.fromRGB(54, 54, 54),
+        ["PhysicalCards"] = {
+            ["ArcadeCard"] = "rbxassetid://8292204605",
+            ["UKCard"] = "rbxassetid://8292290327",
+            ["LoteriaCard"] = "rbxassetid://8292287976",
+            ["USCard"] = "rbxassetid://8292284595"
+        },
+        ["Text"] = Color3.fromRGB(255, 255, 255)
+    }
+}
