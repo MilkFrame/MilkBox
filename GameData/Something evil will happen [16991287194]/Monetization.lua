cf17e5f48f1649f2d5698490793cc84800a05461
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:03:23 +1100

    Create Monetization.lua

---
+++ GameData/Something evil will happen [16991287194]/Monetization.lua
@@ -0,0 +1,42 @@
+data = {
+    ["500 Dosh"] = {
+        ["Description"] = "A little bit of Dosh.",
+        ["ID"] = 2667658780,
+        ["UsesRobux"] = true,
+        ["Icon"] = "rbxassetid://100477259826477",
+        ["Price"] = 150
+    },
+    ["10000 Dosh"] = {
+        ["Description"] = "A crate of Dosh! Yahoo!",
+        ["ID"] = 2667659557,
+        ["UsesRobux"] = true,
+        ["Icon"] = "rbxassetid://126070224018938",
+        ["Price"] = 1550
+    },
+    ["3000 Dosh"] = {
+        ["Description"] = "A barrel of Dosh. Yay!",
+        ["ID"] = 2667659038,
+        ["UsesRobux"] = true,
+        ["Icon"] = "rbxassetid://97967197938206",
+        ["Price"] = 700
+    },
+    ["1000 Dosh"] = {
+        ["Description"] = "A nice pile of Dosh.",
+        ["ID"] = 2667658901,
+        ["UsesRobux"] = true,
+        ["Icon"] = "rbxassetid://117502493656393",
+        ["Price"] = 275
+    },
+    ["VIP"] = {
+        ["Description"] = [[+ Greater Dosh Rewards! (x1.3)
+(Includes Purchased Dosh)
++ 500 Dosh Bonus!
++ Rainbow Name-tag!
++ Special VIP Emote!]],
+        ["UsesRobux"] = true,
+        ["ID"] = 984713659,
+        ["Gamepass"] = true,
+        ["Icon"] = "rbxassetid://92211091163867",
+        ["Price"] = 450
+    }
+}
