d0a43bbc8f3306f9c5387d52d5ce8a7abd139f7a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:24:25 +1100

    Create Tracks.lua

---
+++ GameData/Bloxy Bingo [5244411056]/Tracks.lua
@@ -0,0 +1,341 @@
+data = {
+    {
+        ["track"] = "rbxassetid://1848216987",
+        ["availability"] = "Essential",
+        ["name"] = "Can't Help But Dance",
+        ["cost"] = 100,
+        ["isForSale"] = false,
+        ["description"] = ""
+    },
+    {
+        ["track"] = "rbxassetid://1844140126",
+        ["availability"] = "Rare",
+        ["name"] = "Hurricane",
+        ["cost"] = 200,
+        ["isForSale"] = false,
+        ["description"] = ""
+    },
+    {
+        ["track"] = "rbxassetid://1837770183",
+        ["availability"] = "Essential",
+        ["name"] = "Give It All You Got",
+        ["cost"] = 100,
+        ["isForSale"] = false,
+        ["description"] = ""
+    },
+    {
+        ["track"] = "rbxassetid://1837770248",
+        ["availability"] = "Essential",
+        ["name"] = "Rise to the Top",
+        ["cost"] = 100,
+        ["isForSale"] = false,
+        ["description"] = ""
+    },
+    {
+        ["track"] = "rbxassetid://1841853368",
+        ["availability"] = "Essential",
+        ["name"] = "Breathe New Life",
+        ["cost"] = 100,
+        ["isForSale"] = false,
+        ["description"] = ""
+    },
+    {
+        ["track"] = "rbxassetid://1842145459",
+        ["availability"] = "Essential",
+        ["name"] = "With Me Now",
+        ["cost"] = 100,
+        ["isForSale"] = false,
+        ["description"] = ""
+    },
+    {
+        ["track"] = "rbxassetid://1841324330",
+        ["availability"] = "Essential",
+        ["name"] = "Making Candy",
+        ["cost"] = 100,
+        ["isForSale"] = false,
+        ["description"] = ""
+    },
+    {
+        ["track"] = "rbxassetid://1841824462",
+        ["availability"] = "Essential",
+        ["name"] = "Paint the Town with D n B",
+        ["cost"] = 100,
+        ["isForSale"] = false,
+        ["description"] = ""
+    },
+    {
+        ["track"] = "rbxassetid://1841824700",
+        ["availability"] = "Essential",
+        ["name"] = "Fronds Like These",
+        ["cost"] = 100,
+        ["isForSale"] = false,
+        ["description"] = ""
+    },
+    {
+        ["track"] = "rbxassetid://1846126416",
+        ["availability"] = "Essential",
+        ["name"] = "Overall Winner",
+        ["cost"] = 100,
+        ["isForSale"] = false,
+        ["description"] = ""
+    },
+    {
+        ["track"] = "rbxassetid://1842141178",
+        ["availability"] = "Essential",
+        ["name"] = "Make It Bounce",
+        ["cost"] = 100,
+        ["isForSale"] = false,
+        ["description"] = ""
+    },
+    {
+        ["track"] = "rbxassetid://1841488860",
+        ["availability"] = "Essential",
+        ["name"] = "Big and Bang",
+        ["cost"] = 100,
+        ["isForSale"] = false,
+        ["description"] = ""
+    },
+    {
+        ["track"] = "rbxassetid://9078493812",
+        ["availability"] = "Essential",
+        ["name"] = "Keep Going",
+        ["cost"] = 100,
+        ["isForSale"] = false,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "AnimalBot99"}}
+    },
+    {
+        ["track"] = "rbxassetid://9078495528",
+        ["availability"] = "Essential",
+        ["name"] = "The Moon Comes Quickly",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "AnimalBot99"}}
+    },
+    {
+        ["track"] = "rbxassetid://9078497272",
+        ["availability"] = "Essential",
+        ["name"] = "Relaxing by the Locks",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "AnimalBot99"}}
+    },
+    {
+        ["track"] = "rbxassetid://9078499694",
+        ["availability"] = "Essential",
+        ["name"] = "Insert 5p to Play",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "AnimalBot99"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765593204",
+        ["availability"] = "Essential",
+        ["name"] = "Young Adventures",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765594133",
+        ["availability"] = "Essential",
+        ["name"] = "Buzzing Along",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765594277",
+        ["availability"] = "Essential",
+        ["name"] = "Ecstatic Encounter",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765594538",
+        ["availability"] = "Essential",
+        ["name"] = "Follow the Dreams",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765594808",
+        ["availability"] = "Essential",
+        ["name"] = "Look at Me",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765595215",
+        ["availability"] = "Essential",
+        ["name"] = "Me & My & Me.",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765595546",
+        ["availability"] = "Essential",
+        ["name"] = "Winding Ways",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765595940",
+        ["availability"] = "Essential",
+        ["name"] = "The Low Vibes",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765596115",
+        ["availability"] = "Essential",
+        ["name"] = "End of the Day",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765596361",
+        ["availability"] = "Essential",
+        ["name"] = "Flipping Bricks",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765596576",
+        ["availability"] = "Essential",
+        ["name"] = "Western Competitors",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765596792",
+        ["availability"] = "Essential",
+        ["name"] = "After Work Party",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765597043",
+        ["availability"] = "Essential",
+        ["name"] = "It’s All Coming Together",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765597410",
+        ["availability"] = "Essential",
+        ["name"] = "Bumpy Road",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765597795",
+        ["availability"] = "Essential",
+        ["name"] = "Evenin’ Chillin’",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765597919",
+        ["availability"] = "Essential",
+        ["name"] = "Morning Groove",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765598146",
+        ["availability"] = "Essential",
+        ["name"] = "Meandering Thoughts",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765598493",
+        ["availability"] = "Essential",
+        ["name"] = "In The Party",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765598774",
+        ["availability"] = "Essential",
+        ["name"] = "Tick The Clock",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11765599153",
+        ["availability"] = "Essential",
+        ["name"] = "Frolicking Along",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "BSlickMusic"}}
+    },
+    {
+        ["track"] = "rbxassetid://11940512543",
+        ["availability"] = "Essential",
+        ["name"] = "Grand Arrival",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "AnimalBot99"}}
+    },
+    {
+        ["track"] = "rbxassetid://11940512900",
+        ["availability"] = "Essential",
+        ["name"] = "The Study",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "AnimalBot99"}}
+    },
+    {
+        ["track"] = "rbxassetid://11940513335",
+        ["availability"] = "Rare",
+        ["name"] = "William Tell Overture",
+        ["cost"] = 100,
+        ["isForSale"] = true,
+        ["description"] = "",
+        ["credits"] = {{["creditType"] = "Roblox", ["creditName"] = "AnimalBot99"}}
+    }
+}
