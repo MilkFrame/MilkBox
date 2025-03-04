76651b7d3873c8bf42d70721b9b588de7125e4bc
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:20:52 +1100

    Create Availability.lua

---
+++ GameData/Bloxy Bingo [5244411056]/Availability.lua
@@ -0,0 +1,9 @@
+data = {
+    ["Unusual"] = {["name"] = "Unusual", ["colour"] = Color3.fromRGB(63, 144, 255)},
+    ["Essential"] = {["name"] = "Essential", ["colour"] = Color3.fromRGB(255, 78, 81)},
+    ["Male"] = {["name"] = "Male", ["colour"] = Color3.fromRGB(0, 96, 255)},
+    ["Roblox"] = {["name"] = "Roblox", ["colour"] = Color3.fromRGB(225, 34, 26)},
+    ["Special"] = {["name"] = "Special", ["colour"] = Color3.fromRGB(253, 255, 234)},
+    ["Female"] = {["name"] = "Female", ["colour"] = Color3.fromRGB(255, 0, 148)},
+    ["Rare"] = {["name"] = "Rare", ["colour"] = Color3.fromRGB(143, 39, 184)}
+}
