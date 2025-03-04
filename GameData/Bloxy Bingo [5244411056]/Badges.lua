38877fe775f705ea0dbbadf13f821213f2b64e48
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:18:32 +1100

    Create Badges.lua

---
+++ GameData/Bloxy Bingo [5244411056]/Badges.lua
@@ -0,0 +1,381 @@
+--require(game.ReplicatedStorage.Scripts.Badges).GetBadges()
+data = {
+    {
+        ["badgeDescription"] = "Play a Bloxy Bingo for the first time.",
+        ["badgeId"] = "welcomeBadge",
+        ["badgeName"] = "Welcome to Bingo!",
+        ["badgeImage"] = 11984773399,
+        ["badgeIdNumber"] = 2130232148
+    },
+    {
+        ["badgeDescription"] = "Play your first game of British Bingo.",
+        ["badgeId"] = "playBritishBingo",
+        ["badgeName"] = "Play British Bingo",
+        ["badgeImage"] = 11984791303,
+        ["badgeIdNumber"] = 2130232149
+    },
+    {
+        ["badgeDescription"] = "Play your first game of American Bingo.",
+        ["badgeId"] = "playAmericanBingo",
+        ["badgeName"] = "Play American Bingo",
+        ["badgeImage"] = 11984791594,
+        ["badgeIdNumber"] = 2130232150
+    },
+    {
+        ["badgeDescription"] = "Play your first game of Arcade Bingo.",
+        ["badgeId"] = "playArcadeBingo",
+        ["badgeName"] = "Play Arcade Bingo",
+        ["badgeImage"] = 11984791838,
+        ["badgeIdNumber"] = 2130232151
+    },
+    {
+        ["badgeDescription"] = "Play your first game of Lotería.",
+        ["badgeId"] = "playLoteriaBingo",
+        ["badgeName"] = "Play Lotería Bingo",
+        ["badgeImage"] = 11984792098,
+        ["badgeIdNumber"] = 2130232153
+    },
+    {
+        ["badgeDescription"] = "Play your first game of Bloxy Bingo.",
+        ["badgeId"] = "gamesPlayed1",
+        ["badgeName"] = "1st Game",
+        ["badgeImage"] = 11984787666,
+        ["badgeIdNumber"] = 2130232155
+    },
+    {
+        ["badgeDescription"] = "Play your second game of Bloxy Bingo.",
+        ["badgeId"] = "gamesPlayed2",
+        ["badgeName"] = "2nd Game",
+        ["badgeImage"] = 11984788121,
+        ["badgeIdNumber"] = 2130232156
+    },
+    {
+        ["badgeDescription"] = "Play your fifth game of Bloxy Bingo.",
+        ["badgeId"] = "gamesPlayed5",
+        ["badgeName"] = "5th Game",
+        ["badgeImage"] = 11984788399,
+        ["badgeIdNumber"] = 2130232158
+    },
+    {
+        ["badgeDescription"] = "Play your tenth game of Bloxy Bingo.",
+        ["badgeId"] = "gamesPlayed10",
+        ["badgeName"] = "10th Game",
+        ["badgeImage"] = 11984788684,
+        ["badgeIdNumber"] = 2130232160
+    },
+    {
+        ["badgeDescription"] = "Play your twenty-fifth game of Bloxy Bingo.",
+        ["badgeId"] = "gamesPlayed25",
+        ["badgeName"] = "25th Game",
+        ["badgeImage"] = 11986743297,
+        ["badgeIdNumber"] = 2130232163
+    },
+    {
+        ["badgeDescription"] = "Play your fiftieth game of Bloxy Bingo.",
+        ["badgeId"] = "gamesPlayed50",
+        ["badgeName"] = "50th Game",
+        ["badgeImage"] = 11986743418,
+        ["badgeIdNumber"] = 2130232164
+    },
+    {
+        ["badgeDescription"] = "Play your hundredth game of Bloxy Bingo.",
+        ["badgeId"] = "gamesPlayed100",
+        ["badgeName"] = "100th Game",
+        ["badgeImage"] = 11984789034,
+        ["badgeIdNumber"] = 2130232167
+    },
+    {
+        ["badgeDescription"] = "Play your five-hundredth game of Bloxy Bingo.",
+        ["badgeId"] = "gamesPlayed500",
+        ["badgeName"] = "500th Game",
+        ["badgeImage"] = 11984789353,
+        ["badgeIdNumber"] = 2130232169
+    },
+    {
+        ["badgeDescription"] = "Play your thousandth game of Bloxy Bingo.",
+        ["badgeId"] = "gamesPlayed1000",
+        ["badgeName"] = "1,000th Game",
+        ["badgeImage"] = 11984789709,
+        ["badgeIdNumber"] = 2130232170
+    },
+    {
+        ["badgeDescription"] = "Play your ten-thousandth game of Bloxy Bingo.",
+        ["badgeId"] = "gamesPlayed10000",
+        ["badgeName"] = "10,000th Game",
+        ["badgeImage"] = 11984790087,
+        ["badgeIdNumber"] = 2130232172
+    },
+    {
+        ["badgeDescription"] = "Play your hundred-thousandth game of Bloxy Bingo.",
+        ["badgeId"] = "gamesPlayed100000",
+        ["badgeName"] = "100,000th Game",
+        ["badgeImage"] = 11984790451,
+        ["badgeIdNumber"] = 2130232174
+    },
+    {
+        ["badgeDescription"] = "Play your millionth game of Bloxy Bingo.",
+        ["badgeId"] = "gamesPlayed1000000",
+        ["badgeName"] = "1,000,000th Game",
+        ["badgeImage"] = 11984790864,
+        ["badgeIdNumber"] = 2130232176
+    },
+    {
+        ["badgeDescription"] = "Win any round in any game.",
+        ["badgeId"] = "winAny",
+        ["badgeName"] = "Win a Game!",
+        ["badgeImage"] = 11984773747,
+        ["badgeIdNumber"] = 2130232177
+    },
+    {
+        ["badgeDescription"] = "Win a Row round in British Bingo.",
+        ["badgeId"] = "winUKBingoRow",
+        ["badgeName"] = "British Bingo Row",
+        ["badgeImage"] = 11984774534,
+        ["badgeIdNumber"] = 2130232178
+    },
+    {
+        ["badgeDescription"] = "Win a Two Rows round in British Bingo.",
+        ["badgeId"] = "winUKBingoTwoRows",
+        ["badgeName"] = "British Bingo Two Rows",
+        ["badgeImage"] = 11984774972,
+        ["badgeIdNumber"] = 2130232181
+    },
+    {
+        ["badgeDescription"] = "Win a Full House round in British Bingo.",
+        ["badgeId"] = "winUKBingoFullHouse",
+        ["badgeName"] = "British Bingo Full House",
+        ["badgeImage"] = 11984775307,
+        ["badgeIdNumber"] = 2130232184
+    },
+    {
+        ["badgeDescription"] = "Win a Line round in American Bingo.",
+        ["badgeId"] = "winUSBingoLine",
+        ["badgeName"] = "American Bingo Line",
+        ["badgeImage"] = 11986404337,
+        ["badgeIdNumber"] = 2130232186
+    },
+    {
+        ["badgeDescription"] = "Win a Pattern round in American Bingo.",
+        ["badgeId"] = "winUSBingoPattern",
+        ["badgeName"] = "American Bingo Pattern",
+        ["badgeImage"] = 11986404484,
+        ["badgeIdNumber"] = 2130232190
+    },
+    {
+        ["badgeDescription"] = "Win a Blackout round in American Bingo.",
+        ["badgeId"] = "winUSBingoBlackout",
+        ["badgeName"] = "American Bingo Blackout",
+        ["badgeImage"] = 11986405959,
+        ["badgeIdNumber"] = 2130232195
+    },
+    {
+        ["badgeDescription"] = "Win a Line round in Arcade Bingo.",
+        ["badgeId"] = "winArcadeBingoLine",
+        ["badgeName"] = "Arcade Bingo Line",
+        ["badgeImage"] = 11986406193,
+        ["badgeIdNumber"] = 2130232198
+    },
+    {
+        ["badgeDescription"] = "Win a Two Columns round in Arcade Bingo.",
+        ["badgeId"] = "winArcadeBingoTwoColumns",
+        ["badgeName"] = "Arcade Bingo Two Columns",
+        ["badgeImage"] = 11986406360,
+        ["badgeIdNumber"] = 2130232201
+    },
+    {
+        ["badgeDescription"] = "Win a Full House round in Arcade Bingo.",
+        ["badgeId"] = "winArcadeBingoFullHouse",
+        ["badgeName"] = "Arcade Bingo Full House",
+        ["badgeImage"] = 11986406491,
+        ["badgeIdNumber"] = 2130232202
+    },
+    {
+        ["badgeDescription"] = "Win a Line round in Lotería.",
+        ["badgeId"] = "winLoteriaLine",
+        ["badgeName"] = "Lotería Line",
+        ["badgeImage"] = 11984777667,
+        ["badgeIdNumber"] = 2130232205
+    },
+    {
+        ["badgeDescription"] = "Win a Cluster round in Lotería.",
+        ["badgeId"] = "winLoteriaCluster",
+        ["badgeName"] = "Lotería Cluster",
+        ["badgeImage"] = 11984778107,
+        ["badgeIdNumber"] = 2130232207
+    },
+    {
+        ["badgeDescription"] = "Win a Blackout round in Lotería.",
+        ["badgeId"] = "winLoteriaBlackout",
+        ["badgeName"] = "Lotería Blackout",
+        ["badgeImage"] = 11984778442,
+        ["badgeIdNumber"] = 2130232209
+    },
+    {
+        ["badgeDescription"] = "Win with the same winning pattern on two different cards in the same round.",
+        ["badgeId"] = "multiCardWin",
+        ["badgeName"] = "Win on Multiple Cards",
+        ["badgeImage"] = 11984780379,
+        ["badgeIdNumber"] = 2130232210
+    },
+    {
+        ["badgeDescription"] = "Get a line within the minimum number of calls required to get a line.",
+        ["badgeId"] = "winQuickLine",
+        ["badgeName"] = "Quick Line",
+        ["badgeImage"] = 11986406718,
+        ["badgeIdNumber"] = 2130232212
+    },
+    {
+        ["badgeDescription"] = "Get a win within the top 5% number of calls, within: 60 (British), 60 (American), 56 (Arcade), 39 (Lotería).",
+        ["badgeId"] = "winQuick",
+        ["badgeName"] = "Quick Win",
+        ["badgeImage"] = 11986409040,
+        ["badgeIdNumber"] = 2130232213
+    },
+    {
+        ["badgeDescription"] = "Get a win within the top 0.02% number of calls, within: 44 (British), 51 (American), 42 (Arcade), 30 (Loteria).",
+        ["badgeId"] = "winSuperQuick",
+        ["badgeName"] = "Super Quick Win",
+        ["badgeImage"] = 11986410661,
+        ["badgeIdNumber"] = 2130232215
+    },
+    {
+        ["badgeDescription"] = "Mark your first number!",
+        ["badgeId"] = "numbersMarked1",
+        ["badgeName"] = "First number marked!",
+        ["badgeImage"] = 11986741902,
+        ["badgeIdNumber"] = 2130232217
+    },
+    {
+        ["badgeDescription"] = "Mark off 100 numbers (across games).",
+        ["badgeId"] = "numbersMarked100",
+        ["badgeName"] = "Marked 100 Numbers",
+        ["badgeImage"] = 11986742106,
+        ["badgeIdNumber"] = 2130232219
+    },
+    {
+        ["badgeDescription"] = "Mark off 1,000 numbers (across games).",
+        ["badgeId"] = "numbersMarked1000",
+        ["badgeName"] = "Marked 1,000 Numbers",
+        ["badgeImage"] = 11986742301,
+        ["badgeIdNumber"] = 2130232220
+    },
+    {
+        ["badgeDescription"] = "Mark off 10,000 numbers (across games).",
+        ["badgeId"] = "numbersMarked10000",
+        ["badgeName"] = "Marked 10,000 Numbers",
+        ["badgeImage"] = 11986742578,
+        ["badgeIdNumber"] = 2130232221
+    },
+    {
+        ["badgeDescription"] = "Mark off 100,000 numbers (across games).",
+        ["badgeId"] = "numbersMarked100000",
+        ["badgeName"] = "Marked 100,000 Numbers",
+        ["badgeImage"] = 11986742825,
+        ["badgeIdNumber"] = 2130232223
+    },
+    {
+        ["badgeDescription"] = "Mark off 1,000,000 numbers (across games).",
+        ["badgeId"] = "numbersMarked1000000",
+        ["badgeName"] = "Marked 1,000,000 Numbers",
+        ["badgeImage"] = 11986743031,
+        ["badgeIdNumber"] = 2130232224
+    },
+    {
+        ["badgeDescription"] = "Obtain and use a mascot in-game.",
+        ["badgeId"] = "itemMascot",
+        ["badgeName"] = "Get a mascot!",
+        ["badgeImage"] = 11984783039,
+        ["badgeIdNumber"] = 2130232225
+    },
+    {
+        ["badgeDescription"] = "Obtain and use a bingo call/shout voice in-game.",
+        ["badgeId"] = "itemVoice",
+        ["badgeName"] = "Find your Voice",
+        ["badgeImage"] = 11984783491,
+        ["badgeIdNumber"] = 2130232226
+    },
+    {
+        ["badgeDescription"] = "Obtain and use a custom celebration dance in-game.",
+        ["badgeId"] = "itemDance",
+        ["badgeName"] = "Find your Groove",
+        ["badgeImage"] = 11984784124,
+        ["badgeIdNumber"] = 2130232229
+    },
+    {
+        ["badgeDescription"] = "Obtain and use a custom marker in-game.",
+        ["badgeId"] = "itemMark",
+        ["badgeName"] = "Make your Mark",
+        ["badgeImage"] = 11984784544,
+        ["badgeIdNumber"] = 2130232231
+    },
+    {
+        ["badgeDescription"] = "Buy a Developer Product within the game.",
+        ["badgeId"] = "customThankYou",
+        ["badgeName"] = "Thank You for your Custom",
+        ["badgeImage"] = 11984792440,
+        ["badgeIdNumber"] = 2130232234
+    },
+    {
+        ["badgeDescription"] = "Buy a Gift for a player within the game.",
+        ["badgeId"] = "customGift",
+        ["badgeName"] = "Gifter",
+        ["badgeImage"] = 11984792866,
+        ["badgeIdNumber"] = 2130232237
+    },
+    {
+        ["badgeDescription"] = "Play with more than one card.",
+        ["badgeId"] = "multipleCards",
+        ["badgeName"] = "Cards Galore",
+        ["badgeImage"] = 11986411823,
+        ["badgeIdNumber"] = 2130232239
+    },
+    {
+        ["badgeDescription"] = "Meet a game mod.",
+        ["badgeId"] = "metModerator",
+        ["badgeName"] = "Game Mod",
+        ["badgeImage"] = 11984785510,
+        ["badgeIdNumber"] = 2130232241
+    },
+    {
+        ["badgeDescription"] = "Meet a game creator.",
+        ["badgeId"] = "metCreator",
+        ["badgeName"] = "Creator",
+        ["badgeImage"] = 11984785867,
+        ["badgeIdNumber"] = 2130232243
+    },
+    {
+        ["badgeDescription"] = "Awarded when you follow a friend into the game, or a friend follows you into the game.",
+        ["badgeId"] = "followFriend",
+        ["badgeName"] = "Play with Friends",
+        ["badgeImage"] = 11984786249,
+        ["badgeIdNumber"] = 2130232244
+    },
+    {
+        ["badgeDescription"] = "Play Bingo on your Roblox account's Birthday!",
+        ["badgeId"] = "birthdayBadge",
+        ["badgeName"] = "Happy Birthday!",
+        ["badgeImage"] = 11984786674,
+        ["badgeIdNumber"] = 2130232246
+    },
+    {
+        ["badgeDescription"] = "Play British Bingo in a dedicated server in a rhyming calls round.",
+        ["badgeId"] = "britishRhymes",
+        ["badgeName"] = "Quintessentially British",
+        ["badgeImage"] = 11984787319,
+        ["badgeIdNumber"] = 2130232247
+    },
+    {
+        ["badgeDescription"] = "Play while being a member's +1.",
+        ["badgeId"] = "plusOne",
+        ["badgeName"] = "Important Person",
+        ["badgeImage"] = 11984785037,
+        ["badgeIdNumber"] = 2130232252
+    },
+    {
+        ["badgeDescription"] = "Join a server named after one of the West Midlands nNetwork zonal rail stations.",
+        ["badgeId"] = "westMidlands",
+        ["badgeName"] = "West Midlands Super Region",
+        ["badgeImage"] = 11984793237,
+        ["badgeIdNumber"] = 2130232254
+    }
+}
