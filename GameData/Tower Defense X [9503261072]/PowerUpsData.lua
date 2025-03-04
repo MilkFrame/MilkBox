ebe397a9c854a4021a7827389bfad3dca0b981b7
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:33:21 +1100

    Create PowerUpsData.lua

---
+++ GameData/Tower Defense X [9503261072]/PowerUpsData.lua
@@ -0,0 +1,16 @@
+data = {
+    ["Punkbuster"] = {["MainImage"] = "rbxassetid://17678915851", ["MiniImage"] = "rbxassetid://17678916085"},
+    ["CryoBlast"] = {["MainImage"] = "rbxassetid://90090883804019", ["MiniImage"] = "rbxassetid://82102036502038"},
+    ["HealingField"] = {["MainImage"] = "rbxassetid://17190997326", ["MiniImage"] = "rbxassetid://17191076768"},
+    ["ReconDrone"] = {["MainImage"] = "rbxassetid://17190996292", ["MiniImage"] = "rbxassetid://17191075655"},
+    ["CashCrate"] = {["MainImage"] = "rbxassetid://17190997589", ["MiniImage"] = "rbxassetid://17191077072"},
+    ["HealthCrate"] = {["MainImage"] = "rbxassetid://17190997001", ["MiniImage"] = "rbxassetid://17191076158"},
+    ["NuclearMissile"] = {["MainImage"] = "rbxassetid://17308259360", ["MiniImage"] = "rbxassetid://17308249244"},
+    ["GoldenZed"] = {["MainImage"] = "rbxassetid://18453829573", ["MiniImage"] = "rbxassetid://18453830237"},
+    ["StrykerRush"] = {["MainImage"] = "rbxassetid://18453829863", ["MiniImage"] = "rbxassetid://18453830025"},
+    ["MOAB"] = {["MainImage"] = "rbxassetid://18330117856", ["MiniImage"] = "rbxassetid://18330117625"},
+    ["RaptorStorm"] = {["MainImage"] = "rbxassetid://17190996650", ["MiniImage"] = "rbxassetid://17191075823"},
+    ["AC130Support"] = {["MainImage"] = "rbxassetid://17190998147", ["MiniImage"] = "rbxassetid://17191077509"},
+    ["A10Strike"] = {["MainImage"] = "rbxassetid://17190998443", ["MiniImage"] = "rbxassetid://17191077678"},
+    ["AirStrike"] = {["MainImage"] = "rbxassetid://17190997844", ["MiniImage"] = "rbxassetid://17191077315"}
+}
