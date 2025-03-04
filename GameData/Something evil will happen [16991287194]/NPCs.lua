945e558a0cb531a200b4039391eef34ff4d0668a
Milk Frame <141397806+MilkFrame@users.noreply.github.com>
2025-03-04 19:02:00 +1100

    Create NPCs.lua

---
+++ GameData/Something evil will happen [16991287194]/NPCs.lua
@@ -0,0 +1,99 @@
+data = {
+    ["Builderfan"] = {
+        ["DelayedIdle"] = true,
+        ["InitAnims"] = "function: 0x7b480e686ea05cba",
+        ["Name"] = "Builderfan",
+        ["Chats"] = {
+            ["Attentive"] = {
+                "Hey! What's up?",
+                "What're you buyin'?",
+                "Welcome back!",
+                "Got stuff in stock, browse away!",
+                "Paused my game, what's up?",
+                "Oh! Hey there!",
+                "Welcome!",
+                "Happy New Year!",
+                "Shop away!",
+                "We're open! It's just me.",
+                "Plenty of gear waiting for you!",
+                "Hello!"
+            },
+            ["Shop"] = {
+                "You've got good taste!",
+                "Think you can afford that one?",
+                "Get it while it's in stock!",
+                "Please oh please buy something!",
+                "Do you want that one?",
+                "Hand me some dosh and it's yours!",
+                "Want that one?",
+                "It could be yours!",
+                "Something caught your eye?"
+            },
+            ["Idle"] = {
+                "See ya later!",
+                "Thanks for stopping by!",
+                "Have a great time!",
+                "Come back soon!",
+                "Happy New Year!",
+                "Good luck!",
+                "Bye-bye!",
+                "Break a leg!",
+                "Bye now!",
+                "Byeeee!"
+            },
+            ["Equip"] = {
+                "Good choice!",
+                "Switching it up?",
+                "Here you go!",
+                "Go get em'!",
+                "I've been holding onto that for you.",
+                "Take it off my hands!",
+                "Uh huh, yeah.",
+                "That's a good one!",
+                "Interesting choice!",
+                "Time for a new loadout?",
+                "I hope it serves you well.",
+                "You sure about that?"
+            }
+        },
+        ["ChatColor"] = Vector3.new(255, 147, 14),
+        ["IdleChat"] = false,
+        ["HeadFollow"] = true,
+        ["ProximityReact"] = true
+    },
+    ["H3LP3R"] = {
+        ["DelayedIdle"] = true,
+        ["InitAnims"] = "function: 0x0c00c05e1dd16e8a",
+        ["Name"] = "H3LP3R",
+        ["Chats"] = {
+            ["Attentive"] = {
+                "GREETINGS.",
+                "BR0WSE AWAY.",
+                "BEEP B00P... JUST KIDDING.",
+                "HELP Y0URSELF.",
+                "1011101000101... L0L.",
+                ".... . -.--. HA-HA.",
+                "HELL0.",
+                "EDUCATE Y0URSELF.",
+                "WHAT D0 Y0U WISH T0 LEARN?",
+                "HAPPY NEW YEAR.",
+                "WHAT IS UP?"
+            },
+            ["Idle"] = {
+                "LATER.",
+                "FAREWELL.",
+                "ADI0S.",
+                "CIA0.",
+                "BEST 0F LUCK.",
+                "SLEEP M0DE... ACTIVATING... JK.",
+                "TIRED 0F READING I SEE.",
+                "TILL WE MEET AGAIN.",
+                "G00DBYE."
+            }
+        },
+        ["ChatColor"] = Vector3.new(167, 127, 82),
+        ["IdleChat"] = false,
+        ["HeadFollow"] = true,
+        ["ProximityReact"] = true
+    }
+}
